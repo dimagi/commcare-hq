8f4a62dae43203886024147a2e3300ac093eadc6
dmyung <dmyung@0ff64689-efa9-4121-bbf9-4813ad1b4e4c>
2009-03-13 16:23:33 +0000

    adding simple tests

---
+++ django-hq/apps/ccorganization/tests/basic.py
@@ -0,0 +1,12 @@
+import unittest
+from django.contrib.auth.models import Group, User
+
+from ccorganization.models import *
+
+class BasicTestCases(unittest.TestCase):
+    def setup(selfs):
+        pass
+
+    def testCreateUserHierarchy(self):
+        
+        pass
