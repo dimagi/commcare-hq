54ae5b84e70a07208a5004e759a60cbbe57f0c32
rluk <rluk@0ff64689-efa9-4121-bbf9-4813ad1b4e4c>
2009-03-18 19:02:36 +0000

    add basic support for complex and repeatable types

--- django-hq/apps/xformmanager/formdefprovider.py
+++ django-hq/apps/xformmanager/formdefprovider.py
@@ -1,10 +1,16 @@
 # This is an interface; currently we do not inherit any functionality
-class FormDefProvider:
-  def set_input(self, stream_pointer):
-      pass
+class FormDefProvider(object):
+    """ This is a generic interface for things that provide information about form defintions.
+    
+    For now we read from xsd files. In the future we could read from xforms, xml, databases, etc.
+    
+    """
+    
+    def set_input(self, stream_pointer):
+        pass
 
-  def get_formdef(self):
-      pass
+    def get_formdef(self):
+        pass
 
 
 
@@ -14,45 +20,50 @@ import re
 
 
 class FormDefProviderFromXSD(FormDefProvider):
-  def __init__(self, stream_pointer=None):
-      if stream_pointer is not None:
-          self.parseStream(stream_pointer)          
-      pass
-
-  def set_input(self, stream_pointer):
-      self.parseStream(stream_pointer)
-      pass
-
-  def get_formdef(self):
-      #if(self.formDef == empty) report an error
-      return self.formDef
-  
-  def parseStream(self, stream_pointer):
-      #if( stream_pointer == null ) throw exception, if parent is None: 
-      self.tree = etree.parse(stream_pointer)
-
-      r = re.search('{[a-zA-Z0-9\.\/\:]*}', self.tree.getroot().tag)
-      xmlns = r.group(0).strip('{').strip('}')
-      self.formDef = FormDef(xmlns) # add date, time, etc. to creation later
-
-      self.__addAttributesAndChildElements(self.formDef, self.tree.getroot())
-      return self.formDef
+    """ This FormDefProvider translates xsd streams into form definitions"""
     
-  def __addAttributesAndChildElements(self, element, input_tree):
-      self.__populateElementFields(element, input_tree)
-      for input_node in etree.ElementChildIterator(input_tree):
-          if input_node.tag.find("element") > -1 and (input_node.get('name').find('root') == -1 ):
-            child_element = ElementDef()
-            element.addChild(child_element)     
-            self.__addAttributesAndChildElements(child_element, input_node)
-          else:
-            self.__addAttributesAndChildElements(element, input_node)            
-          #for other types of input nodes, pass in different parameters
-          #or add another level to the tree
-  
-  def __populateElementFields(self, element, input_node):
-      if not element.name: element.name = input_node.get('name')
-      element.type = input_node.get('type')
-      element.min_occurs = input_node.get('minOccurs')
-      element.tag = input_node.tag
+    def __init__(self, stream_pointer=None):
+        if stream_pointer is not None:
+            self.parseStream(stream_pointer)          
+        pass
+
+    def set_input(self, stream_pointer):
+        self.parseStream(stream_pointer)
+        pass
+
+    def get_formdef(self):
+        #if(self.formDef == empty) report an error
+        return self.formDef
+    
+    def parseStream(self, stream_pointer):
+        #if( stream_pointer == null ) throw exception, if parent is None: 
+        self.tree = etree.parse(stream_pointer)
+
+        r = re.search('{[a-zA-Z0-9\.\/\:]*}', self.tree.getroot().tag)
+        xmlns = r.group(0).strip('{').strip('}')
+        self.formDef = FormDef(xmlns) # add date, time, etc. to creation later
+
+        self.__addAttributesAndChildElements(self.formDef, self.tree.getroot())
+        return self.formDef
       
+    def __addAttributesAndChildElements(self, element, input_tree):
+        self.__populateElementFields(element, input_tree)
+        for input_node in etree.ElementChildIterator(input_tree):
+            if input_node.tag.find("element") > -1 and (input_node.get('name').find('root') == -1 ):
+                if input_node.get('maxOccurs') > 1: 
+                    child_element = ElementDef(is_repeatable=True)
+                else: 
+                    child_element = ElementDef()
+                element.addChild(child_element)     
+                self.__addAttributesAndChildElements(child_element, input_node)
+            else:
+                self.__addAttributesAndChildElements(element, input_node)            
+                #for other types of input nodes, pass in different parameters
+                #or add another level to the tree
+    
+    def __populateElementFields(self, element, input_node):
+        if not element.name: element.name = input_node.get('name')
+        element.type = input_node.get('type')
+        element.min_occurs = input_node.get('minOccurs')
+        element.tag = input_node.tag
+        
--- django-hq/apps/xformmanager/formstorageprovider.py
+++ django-hq/apps/xformmanager/formstorageprovider.py
@@ -2,36 +2,59 @@ from django.db import connection, transaction, DatabaseError
 from xformmanager.models import ElementDefData, FormDefData
 from xformmanager.formdata import *
 
+class FormStorageProvider(object):
+    """ This class handles everything that touches the database - both form and instance data."""
 
-class FormStorageProvider():
-  def add_formdef(self, formdef):
-      #should instead create two functions and pass functions to a traverse function
-      self.update_formdef_meta(formdef)
-      self.create_tables(formdef, formdef.name)
-        
-  def remove_formdef(self, name):
-      # implement later
-      # update formdef meta tables
-      # drop formdef data tables
-      pass
+    def add_formdef(self, formdef):
+        self.update_formdef_meta(formdef)
+        self.create_data_tables(formdef)
 
-  def save_form_data(self, data, formdef):
-      s = "INSERT INTO " + self.get_table_name(formdef.name) + " (";
-      fields = ''
-      values = ''
+    def remove_formdef(self, name):
+        # update formdef meta tables
+        # drop formdef data tables
+        pass
+    
+    def save_form_data(self, data, formdef):
+        self.populate_data_tables(formdef, data.child_iterator())
 
-      iter = data.child_iterator()
-      for child in formdef.child_elements:
-          #check whatever appropriate elements are in 'data
-          fields = fields + child.name + ", "
-          values = values + "'" + iter.next().text + "', "
-          #self.save_form_data(SOMETHING, child)
-      if not values: return 
-      s = s + self.__trim2chars(fields) + ") VALUES( " + self.__trim2chars(values) + ");"
+    def update_formdef_meta(self, formdef):
+        """ save form metadata """
+        ed = ElementDefData(name=str(formdef.name), datatype=str(formdef.type), 
+                            table_name=self.__table_name(formdef.xmlns))
+        ed.save()
+        fdd = FormDefData(form_name=self.__table_name(formdef.name), xmlns=formdef.xmlns, element_id=ed.id)
+        fdd.save()
+
+    def create_data_tables(self, elementdef, parent_name=''):
+        if not elementdef.child_elements : return
+
+        fields = self.__handle_children_tables(elementdef=elementdef, parent_name=parent_name )
+        fields = self.__trim2chars(fields);
+             
+        table_name = self.__table_name( self.__name(parent_name, elementdef.name) )
+        s = "CREATE TABLE "+ table_name +" ( " + fields + " );"
+        print s
+        if not fields: return # move this up later
+        cursor = connection.cursor()
+        cursor.execute(s)
+      
+
+
+
+    def populate_data_tables(self, elementdef, data_iterator, parent_name=''):
+      if not data_iterator : return
+
+      field_values = self.__populate_children_tables(elementdef, data_iterator, parent_name )
+
+      # populate the tables
+      s = "INSERT INTO " + self.__table_name( self.__name(parent_name, elementdef.name) ) + " (";
+      s = s + self.__trim2chars(field_values['fields']) + ") VALUES( " + self.__trim2chars(field_values['values']) + ");"
+      print s
+      if not field_values.values: return # move this up later
       cursor = connection.cursor()
       cursor.execute(s)
       transaction.commit_unless_managed()
-      
+    
       """except DatabaseError: To be threadsafe, updates/inserts are allowed to fail silently
       transaction.rollback()
            return False
@@ -39,40 +62,96 @@ class FormStorageProvider():
                  transaction.commit_unless_managed()
                  return True"""
 
-  def update_formdef_meta(self, formdef):
-      # save form metadata
-      ed = ElementDefData(name=str(formdef.name), datatype=str(formdef.type), 
-                          table_name=self.get_table_name(formdef.xmlns))
-      ed.save()
-      fdd = FormDefData(form_name=formdef.name, xmlns=formdef.xmlns, element_id=ed.id)
-      fdd.save()
 
-      #iterate through children and create element_meta as needed
-      #ed = ElementDefData(name=str(formdef.name), datatype=str(formdef.type), 
-      #                    table_name=self.get_table_name(formdef.name))
-      pass
-  
-  def get_table_name(self, name):
-      return str(name).replace("/","_").replace(":","").replace(".","_").lower()
-  
-  def create_tables(self, elementdef, form_name):
-    # create main form table
-    if not elementdef.child_elements : return
-    s = "CREATE TABLE "+ self.get_table_name(elementdef.name) +" ( "
-    fields = ''
-    for child in elementdef.child_elements:
-      #add_comma = False; if add_comma: s = s + ', ' else: add_coma = True
-      #if child.tag.find("element") > -1: # FIX THIS
-      fields = fields + str(child.name) + " VARCHAR(100), "
-      self.create_tables(child, form_name)
-    if not fields: return
-    s = s + self.__trim2chars(fields) + ");"
-    print s
-    cursor = connection.cursor()
-    cursor.execute(s)
+    def __handle_children_tables(self, elementdef, parent_name=''):
+      """ This is 'handle' instead of 'create'(_children_tables) because not only are we 
+      creating children tables, we are also gathering/passing children/field information back to the parent.
+      """
+      
+      if not elementdef: return
+      local_fields = '';
 
-    #once that works, iterate through children and create child tables
-    
-  def __trim2chars(self, string):
-      # ro - hack to fix recursion namespace weirdness
-      return string[0:len(string)-2]
+      for child in elementdef.child_elements:
+        # put in a check for root.isRepeatable
+        next_parent_name = self.__name(parent_name, elementdef.name)
+        if child.is_repeatable :
+            # repeatable elements must generate a new table
+            ed = ElementDefData(name=str(child.name), datatype=str(child.type), 
+                            table_name = self.__table_name( self.__name(next_parent_name, child.name) ) )
+            ed.save()
+            self.create_data_tables(child, next_parent_name )
+        else: 
+            #assume elements of complextype alwasy have <complextype> as first child
+            if len(child.child_elements) > 0 :
+                local_fields = local_fields + self.__handle_children_tables(elementdef=child, parent_name=self.__name( next_parent_name, child.name ) )
+            else:
+                local_fields = local_fields + child.name + " VARCHAR(100), "
+                local_fields = local_fields + self.__handle_children_tables(elementdef=child, parent_name=next_parent_name )
+      return local_fields
+  
+    def __populate_children_tables(self, elementdef, data_iterator, parent_name=''):
+      if not elementdef : return
+      if not data_iterator: return
+      local_fields = '';
+      values = '';
+      
+      try:
+          data = data_iterator.next()
+      except StopIteration:
+          return {'fields':local_fields, 'values':values}
+      
+      for child in elementdef.child_elements:
+        # put in a check for root.isRepeatable
+        next_parent_name = self.__name(parent_name, elementdef.name)
+        if child.is_repeatable :
+            #if repeatable type data has child elements, then just skip current xml node
+            if( len(child.child_elements)>0 ):
+                child_iterator = None
+                if data_iterator: child_iterator = data_iterator.get_child_iterator()
+                if child_iterator: self.populate_data_tables(child, child_iterator, next_parent_name )
+            else:
+                pass
+                # save singular repeatable element directly to its table
+                # store(child.name, data.text, parent_name)             
+        else:
+            if( len(child.child_elements)>0 ):
+                # get child iterator
+                child_iterator = None
+                if data_iterator: child_iterator = data_iterator.get_child_iterator()
+                if child_iterator: 
+                    field_values = self.__populate_children_tables(child, child_iterator, next_parent_name )
+                    local_fields = local_fields + field_values['fields']
+                    values  = values + field_values['values']
+                    print " fields: " + local_fields
+                    print " values: " + values
+                    #assume elements of complextype alwasy have <complextype> as first child
+            else:
+                local_fields = local_fields + child.name + ", "
+                values = values + "'" + data.text + "', "
+                print " fields: " + local_fields
+                print " values: " + values
+        try:
+            data = data_iterator.next()
+        except StopIteration:
+            return {'fields':local_fields, 'values':values}
+      # ah, python, so cool... but is this bad form?
+      return {'fields':local_fields, 'values':values}
+
+    def __trim2chars(self, string):
+        # ro - hack to fix recursion namespace weirdness
+        #add_comma = False; if add_comma: s = s + ', ' else: add_coma = True
+        return string[0:len(string)-2]
+        
+    def __name(self, parent_name, child_name):
+        if parent_name: return str(parent_name) + "_" + str(child_name)
+        else: return str(child_name)
+
+    def __table_name(self, name):
+        # check for uniqueness!
+        # 122 is mysql table limit, i think
+        MAX_LENGTH = 122
+        start = 0
+        if len(name) > MAX_LENGTH:
+            start = len(name)-MAX_LENGTH
+        return str(name[start:len(name)]).replace("/","_").replace(":","").replace(".","_").lower()
+        
