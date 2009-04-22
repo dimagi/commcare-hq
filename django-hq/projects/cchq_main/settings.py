07dc6ac23be9bc7d1adc21565c7bc737b58e8f97
dmyung <dmyung@0ff64689-efa9-4121-bbf9-4813ad1b4e4c>
2009-04-22 22:49:58 +0000

    fixing settings so it'll pass unit tests on buildserver

--- django-hq/projects/cchq_main/settings.py
+++ django-hq/projects/cchq_main/settings.py
@@ -13,11 +13,11 @@ COMMCARE_THEME = 'default'
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = os.path.join(os.path.dirname(__file__),'cchq.db')           # Or path to database file if using sqlite3.
-DATABASE_NAME = 'commcarehq'    # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'password'         # Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(os.path.dirname(__file__),'cchq.db')           # Or path to database file if using sqlite3.
+#DATABASE_NAME = 'commcarehq'    # Or path to database file if using sqlite3.
+#DATABASE_USER = 'root'             # Not used with sqlite3.
+#DATABASE_PASSWORD = 'password'         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
