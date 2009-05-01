d7e135ed3ecdd11c56742121fb849145ec718420
dmyung <dmyung@0ff64689-efa9-4121-bbf9-4813ad1b4e4c>
2009-05-01 21:19:27 +0000

    revert to use sqlite for build server

--- django-hq/projects/cchq_main/settings.py
+++ django-hq/projects/cchq_main/settings.py
@@ -13,10 +13,10 @@ COMMCARE_THEME = 'default'
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = os.path.join(os.path.dirname(__file__),'cchq.db')           # Or path to database file if using sqlite3.
-DATABASE_NAME = 'commcarehqbuild'    # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = os.path.join(os.path.dirname(__file__),'cchq.db')           # Or path to database file if using sqlite3.
+#DATABASE_NAME = 'commcarehq'    # Or path to database file if using sqlite3.
+#DATABASE_USER = 'root'             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
