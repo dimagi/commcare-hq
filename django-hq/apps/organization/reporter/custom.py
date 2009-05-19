b2bdaa94fea29d6a254265feb0100aac4598d7b6
dmyung <dmyung@0ff64689-efa9-4121-bbf9-4813ad1b4e4c>
2009-05-19 14:46:03 +0000

    flattened reports with aggregation by section

--- django-hq/apps/organization/reporter/custom.py
+++ django-hq/apps/organization/reporter/custom.py
@@ -17,6 +17,124 @@ from datetime import timedelta
 
 import inspector as repinspector
 
+def _get_flat_data_for_domain(domain, startdate, enddate):
+    
+    data = []
+    #next, let's get all the unclaimed
+    #first, let's iterate through all the data and get the usernames
+    #get all the usernames in question
+    configured_users = []
+    for datum in data:
+        if isinstance(datum[2], ExtUser):
+            configured_users.append(datum[2].report_identity.lower())        
+    
+    #next, do a query of all the forms in this domain to get an idea of all the usernames
+    defs = FormDefData.objects.all().filter(uploaded_by__domain=dom)
+    user_date_hash = {}
+    
+    for fdef in defs:        
+        table = fdef.element.table_name
+        
+        helper = dbhelper.DbHelper(table, fdef.form_display_name) 
+        #let's get the usernames
+        all_usernames = helper.get_uniques_for_column('meta_username', None, None)
+        #ok, so we got ALL usernames.  let's filter out the ones we've already seen
+        unclaimed_users = []
+        for existing in all_usernames:
+            if configured_users.count(existing.lower()) == 0:
+                unclaimed_users.append(existing.lower())
+        
+        #now that we've got ALL users, we can now the count query of their occurences in the formdef tables
+        #as in the dashboard query, we need to hash it by username and by date to do the aggregate counts
+        for user in all_usernames:                
+            userdailies = helper.get_filtered_date_count(startdate, enddate,filters={'meta_username': user})
+                 
+            for dat in userdailies:                
+                if not user_date_hash.has_key(user):
+                    user_date_hash[user] = {}
+                
+                if not user_date_hash[user].has_key(dat[1]):
+                    user_date_hash[user][date[1]] = 0                   
+                
+                user_date_hash[user][dat[1]] = user_date_hash[user][dat[1]] + int(dat[0]) #additive
+    #end for loop through all the fdefs
+    
+    
+    report_tuples = []
+    #once all the formdefs have been calculated, we need to finally append the totals to the data array.  
+    for usr, datehash in user_date_hash.items():
+        
+        #for a given user, make the 4 point array of their data
+        usertuple = []
+        usertuple.append(0)            
+        if len(report_tuples) == 0:            
+            usertuple.append('All Users')
+        else:            
+            usertuple.append(None)
+            
+        if unclaimed_users.count(usr) == 1:
+            usertuple.append(usr + "*")
+        else:
+            usertuple.append(usr)
+        udata = []
+        
+        totalspan = enddate - startdate
+        for day in range(0,totalspan.days+1):
+            delta = timedelta(days=day)
+            target_date = startdate + delta
+            datestr = target_date.strftime('%m/%d/%Y')
+            if datehash.has_key(datestr):
+                udata.append(datehash[datestr])
+            else:
+                udata.append(0)            
+        usertuple.append(udata)            
+        report_tuples.append(usertuple)                  
+    #ok, so we just did all this data, let's append the data to the already existing data
+    
+    data = data + report_tuples  
+    return data
+
+def domain_flat(report_schedule, run_frequency):
+    dom = report_schedule.organization.domain
+    rendered_text = ''
+    from organization import reporter
+    if run_frequency == 'daily':
+        run_frequency='weekly'    
+    (startdate, enddate) = reporter.get_daterange(run_frequency)
+
+    data = _get_flat_data_for_domain(dom, startdate, enddate)        
+    params = {}
+    heading = str(dom) + " report: " + startdate.strftime('%m/%d/%Y') + " - " + enddate.strftime('%m/%d/%Y')
+    params['heading'] = heading    
+    rendered_text += reporter.render_direct_email(data, run_frequency, "organization/reports/email_hierarchy_report.txt", params)
+
+    if report_schedule.report_delivery == 'email':
+        usr = report_schedule.recipient_user
+        subject = "[CommCare HQ] " + run_frequency + " report " + startdate.strftime('%m/%d/%Y') + "-" + enddate.strftime('%m/%d/%Y') + " ::  Domain Report - " + str(dom)
+        reporter.transport_email(rendered_text, usr, params={"startdate":startdate,"enddate":enddate,"email_subject":subject})
+
+
+def admin_catch_all_flat(report_schedule, run_frequency):
+    #same as admin_catch_all, but do for all users, no organizational junks
+    domains = Domain.objects.all()
+    rendered_text = ''
+    from organization import reporter
+    if run_frequency == 'daily':
+        run_frequency='weekly'    
+    (startdate, enddate) = reporter.get_daterange(run_frequency)
+    for dom in domains:
+        #get the root organization
+        data = _get_flat_data_for_domain(dom, startdate, enddate)        
+        params = {}
+        heading = str(dom) + " report: " + startdate.strftime('%m/%d/%Y') + " - " + enddate.strftime('%m/%d/%Y')
+        params['heading'] = heading    
+        rendered_text += reporter.render_direct_email(data, run_frequency, "organization/reports/email_hierarchy_report.txt", params)
+
+
+    if report_schedule.report_delivery == 'email':
+        usr = report_schedule.recipient_user
+        subject = "[CommCare HQ] " + run_frequency + " report " + startdate.strftime('%m/%d/%Y') + "-" + enddate.strftime('%m/%d/%Y') + " ::  Global Admin"
+        reporter.transport_email(rendered_text, usr, params={"startdate":startdate,"enddate":enddate,"email_subject":subject})
 
 
 def admin_catch_all(report_schedule, run_frequency):
@@ -72,7 +190,7 @@ def admin_catch_all(report_schedule, run_frequency):
                         user_date_hash[user] = {}
                     
                     if not user_date_hash[user].has_key(dat[1]):
-                        user_date_hash[user][dat[1]] = 0                   
+                        user_date_hash[user][date[1]] = 0                   
                     
                     user_date_hash[user][dat[1]] = user_date_hash[user][dat[1]] + int(dat[0]) #additive
         #end for loop through all the fdefs
