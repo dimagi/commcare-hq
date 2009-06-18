c6764c5a9fad1ebf1ec004948050377eb3c8dfad
Cory Zue <czue@dimagi.com>
2009-06-18 11:25:40 -0400

    making the admin metadata page more useful

--- django-hq/apps/xformmanager/admin.py
+++ django-hq/apps/xformmanager/admin.py
@@ -7,6 +7,10 @@ class FormDefModelAdmin(admin.ModelAdmin):
     list_display = ('id','uploaded_by', 'domain', 'form_display_name','form_name', 'submit_time',)
     list_filter = ["domain"]
 
+class MetaDataModelAdmin(admin.ModelAdmin):
+    list_display = ( 'formname','formversion','deviceid','timestart','timeend','username','chw_id','uid', 'submission', 'raw_data', 'formdefmodel')
+    list_filter = ( 'formname','formversion','deviceid','timestart','timeend','username','chw_id','formdefmodel')
+
 admin.site.register(FormDefModel,FormDefModelAdmin)
 admin.site.register(ElementDefModel)
-admin.site.register(Metadata)
+admin.site.register(Metadata, MetaDataModelAdmin)
