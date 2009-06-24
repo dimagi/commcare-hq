2a9e966a0873bac14f6562e9c8696d8cafaa0cfe
Cory Zue <czue@dimagi.com>
2009-06-24 17:42:32 -0400

    making the trailing slash optional on the organization views

--- django-hq/apps/organization/urls.py
+++ django-hq/apps/organization/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',
-    url(r'^report/$', 'organization.views.org_report', name='org_report'),    
+    url(r'^report/?$', 'organization.views.org_report', name='org_report'),    
     (r'^report/email/?$', 'organization.views.org_email_report'),
     #(r'^report/email/(?P<id>\d*)/?$', 'organization.views.org_email_report'),
     (r'^report/sms/?$', 'organization.views.org_sms_report'),
     #(r'^report/sms/(?P<id>\d*)/?$', 'organization.views.org_sms_report'),
-    (r'^charts/default/$', 'organization.views.summary_trend'),
-    (r'^charts/$', 'organization.views.domain_charts'),
+    (r'^charts/default/?$', 'organization.views.summary_trend'),
+    (r'^charts/?$', 'organization.views.domain_charts'),
     
 )
