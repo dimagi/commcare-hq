26dee5e0fc50122e1eb7af08eef3e75efb65490d
Cory Zue <czue@dimagi.com>
2009-06-25 14:31:48 -0400

    try/excepting the method that pulls the xmlns from the data so that it doesn't puke on binary data (which can easily be in a multipart)

--- django-hq/apps/xformmanager/util.py
+++ django-hq/apps/xformmanager/util.py
@@ -42,21 +42,25 @@ def sanitize(name):
 #temporary measure to get target form
 # todo - fix this to be more efficient, so we don't parse the file twice
 def get_xmlns(stream):
-    logging.debug("Trying to parse xml_file")
-    skip_junk(stream)
-    tree = etree.parse(stream)
-    root = tree.getroot()
-    logging.debug("Parsing xml file successful")
-    logging.debug("Find xmlns from " + root.tag)
-    #todo - add checks in case we don't have a well-formatted xmlns
-    r = re.search('{[a-zA-Z0-9_\-\.\/\:]*}', root.tag)
-    if r is None:
-        logging.error( "NO NAMESPACE FOUND" )
+    try:
+        logging.debug("Trying to parse xml_file")
+        skip_junk(stream)
+        tree = etree.parse(stream)
+        root = tree.getroot()
+        logging.debug("Parsing xml file successful")
+        logging.debug("Find xmlns from " + root.tag)
+        #todo - add checks in case we don't have a well-formatted xmlns
+        r = re.search('{[a-zA-Z0-9_\-\.\/\:]*}', root.tag)
+        if r is None:
+            logging.error( "NO NAMESPACE FOUND" )
+            return None
+        xmlns = get_table_name( r.group(0).strip('{').strip('}') )
+        logging.debug( "Xmlns is " + xmlns )
+        return xmlns
+    except XMLSyntaxError:
+        # this is probably just some non-xml data.
+        # not a big deal, just don't return an xmlns
         return None
-    xmlns = get_table_name( r.group(0).strip('{').strip('}') )
-    logging.debug( "Xmlns is " + xmlns )
-    return xmlns
-
 def get_target_namespace(stream):
     skip_junk(stream)
     tree = etree.parse(stream)
