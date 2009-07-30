43a41f5d254eb30be36cb4be7c6cb1c162556d88
Cory Zue <czue@dimagi.com>
2009-07-30 16:22:46 -0400

    moving the reports urls to no longer start with xforms/. Cleaning the "schemas and reports" tab, and removing the silly subtabs from the schemas.

--- django-hq/apps/reports/urls.py
+++ django-hq/apps/reports/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^reports/$', 'reports.views.reports'),
-    (r'^xforms/reports/(?P<case_id>\d+)/?$', 'reports.views.case_data'),
-    (r'^xforms/reports/(?P<case_id>\d+)/csv/?$', 'reports.views.case_export_csv'),
-    (r'^xforms/reports/(?P<domain_id>\d+)/custom/(?P<report_name>.*)/?$', 'reports.views.custom_report')
+    (r'^reports/(?P<case_id>\d+)/?$', 'reports.views.case_data'),
+    (r'^reports/(?P<case_id>\d+)/csv/?$', 'reports.views.case_export_csv'),
+    (r'^reports/(?P<domain_id>\d+)/custom/(?P<report_name>.*)/?$', 'reports.views.custom_report')
 )
--- django-hq/apps/xformmanager/config.py
+++ django-hq/apps/xformmanager/config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-title = "Schemas and Reports"
+title = "Schemas"
 tab_link = "/xforms"
 
