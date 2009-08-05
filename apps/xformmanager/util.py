f34da8ea77694a5dc8866d9cc22180241af79a70
U-rotop\rowena <rowena@rotop.(none)>
2009-08-05 13:05:17 -0400

    Revert "added comments"
    This reverts commit 1288dcc5428676709a28198522d70229bff05655.

--- apps/xformmanager/util.py
+++ apps/xformmanager/util.py
@@ -10,19 +10,16 @@ from transformers.csv import format_csv
 from xformmanager.models import FormDefModel, ElementDefModel
 
 MAX_MYSQL_TABLE_NAME_LENGTH = 64
-TABLE_PREFIX= "schema_"
-MAX_LENGTH = MAX_MYSQL_TABLE_NAME_LENGTH - len(TABLE_PREFIX)
+MAX_PREFIX_LENGTH= 7
+MAX_LENGTH = MAX_MYSQL_TABLE_NAME_LENGTH - MAX_PREFIX_LENGTH
 
 def table_name(name):
     r = re.match('http://[a-zA-Z\.]+/(?P<tail>.*)', name)
     if r:
         tail = r.group('tail')
         if tail: 
-            # table prefix is appended after sanitation because
-            # sanitation truncates to MAX_LENGTH minus len(prefix)
-            return TABLE_PREFIX + sanitize(tail)
-    return TABLE_PREFIX + sanitize(name)
-
+            return "schema_" + sanitize(tail)
+    return "schema_" + sanitize(name)
 def old_table_name(name):
     return "x_" + _old_sanitize(name)
 def _old_sanitize(name):
