d8610894fb32541c38fe5a41b9c76946adfff7f0
U-rotop\rowena <rowena@rotop.(none)>
2009-08-06 11:00:37 -0400

    more intelligent sanitation for xml.py

--- apps/transformers/xml.py
+++ apps/transformers/xml.py
@@ -22,15 +22,15 @@ def _xmlify(object_):
     if isinstance( object_, tuple):
         # the first element of tuple is attribute
         # the second is text
-        root = etree.Element( 'entry', index=sanitize(unicode(object_[0])) )
+        root = etree.Element( 'entry', index=sanitize_value(unicode(object_[0])) )
         root.text=unicode(object_[1])
     else:
-        root = etree.Element( sanitize(str(type(object_))) )
+        root = etree.Element( sanitize_name(str(type(object_))) )
         for i in object_.__dict__:
             i_val = getattr(object_,i)
             if isinstance(i_val, basestring):
                 # set strings to be attributes
-                root.set(sanitize(i),sanitize(i_val) )
+                root.set(sanitize_name(i),sanitize_value(i_val) )
             elif isinstance( i_val, list):
                 # i = string name of field
                 # i_val = actual field value
@@ -44,7 +44,7 @@ def _xmlify(object_):
                 # i_val = actual field value
                 children = etree.Element( pluralize(i) )
                 for key in i_val.keys():
-                    child = etree.etree.Element( sanitize(i) , name=sanitize(key), value=sanitize(i_val[key]) )
+                    child = etree.etree.Element( sanitize_name(i) , name=sanitize_value(key), value=sanitize_value(i_val[key]) )
                     children.append(child)
                 root.append(children)
             else:
@@ -56,8 +56,13 @@ def _xmlify(object_):
 def pluralize(string):
     return string
 
-def sanitize(string):
-    sanitized = string.replace(" ","_").replace("<","_").replace(">","_").replace("'","_").replace("-","_").replace("/","_").replace(":","").replace(".","_")
+def sanitize_name(string):
+    value_sanitized = sanitize_value(string)
+    name_sanitized = value_sanitized.replace("/","_").replace(" ","_").replace("-","_")
+    return name_sanitized
+
+def sanitize_value(string):
+    sanitized = string.replace("<","_").replace(">","_").replace("'","_").replace(":","").replace(".","_")
     stripped = sanitized.strip('_')
     tail = stripped.rsplit('_',1)[-1]
     return tail
