8d457ed4be6f0cf7a78f308b115ed54110b747cf
U-rotop\rowena <rowena@rotop.(none)>
2009-08-06 10:59:45 -0400

    update reports to reference reporters instead of extusers

--- apps/hq/reporter/api_/resources.py
+++ apps/hq/reporter/api_/resources.py
@@ -7,10 +7,13 @@ from django_rest_interface.resource import Resource
 from transformers.xml import *
 from transformers.http import *
 from xformmanager.models import FormDefModel, Metadata
-from hq.models import ExtUser
+from hq.models import ReporterProfile
 from hq.reporter.api_.reports import *
-from hq.reporter.metadata import get_user_id_count
+from hq.reporter.metadata import get_user_id_count, get_username_count
 import hq.utils as utils 
+
+# temporary hack - TODO: remove
+from hq.models import Domain
 
 # TODO - pull out authentication stuff into some generic wrapper
 # if ExtUser.objects.all().filter(id=request.user.id).count() == 0:
@@ -36,11 +39,20 @@ def user_report(request):
 # it doesn't really make sense to define any of this as a 'resource'
 # since reports are by definition read-only
 def read(request, ids=[], index='',value=[]):
+    # temporary hack to get pf api working. TODO - remove
     try:
-        extuser = ExtUser.objects.get(id=request.user.id)
-    except ExtUser.DoesNotExist:
+        domain = Domain.objects.get(name='Pathfinder')
+    except Domain.DoesNotExist:
         return HttpResponseBadRequest( \
-            "You do not have permission to use this API.")
+            "Domain 'Pathfinder' does not exist.")
+    
+    #try:
+    #    extuser = ExtUser.objects.get(id=request.user.id)
+    #except ExtUser.DoesNotExist:
+    #    return HttpResponseBadRequest( \
+    #        "You do not have permission to use this API.")
+    #domain = extuser.domain
+    
     if not ids:
         if request.REQUEST.has_key('ids'):
             ids = [v.strip() for v in request.GET['id'].split(',')]
@@ -54,17 +66,19 @@ def read(request, ids=[], index='',value=[]):
             values = [v.strip() for v in request.GET['value'].split(',')]
         else:
             return HttpResponseBadRequest("Must specify value (y-axes)")
+    start_date = None
     if request.REQUEST.has_key('start-date'):
         start_date = datetime.strptime(request.GET['start-date'],"%Y-%m-%d")
-    if request.REQUEST.has_key('end-date'):
-        end_date = datetime.strptime(request.GET['end-date'],"%Y-%m-%d")
-    if request.REQUEST.has_key('stat'):
-        stat = [v.strip() for v in request.GET['stat'].split(',')]
     if start_date is None:
         return HttpResponseBadRequest("Must specify start_date")   
+    end_date = None
+    if request.REQUEST.has_key('end-date'):
+        end_date = datetime.strptime(request.GET['end-date'],"%Y-%m-%d")
     if end_date is None:
         return HttpResponseBadRequest("Must specify end_date")   
-    report = get_report(extuser.domain, ids, index, value, \
+    if request.REQUEST.has_key('stat'):
+        stat = [v.strip() for v in request.GET['stat'].split(',')]
+    report = get_report(domain, ids, index, value, \
                     start_date, end_date,request.GET)
     xml = xmlify(report)
     response = responsify('xml', xml)
@@ -97,7 +111,7 @@ def get_user_activity_report(domain, ids, index, value, start_date, end_date, pa
     # when 'organization' is properly populated, we can start using that
     #       member_list = utils.get_members(organization)
     # for now, just use domain
-    member_list = [r.chw_id for r in ExtUser.objects.filter(domain=domain)]
+    member_list = [r.chw_id for r in ReporterProfile.objects.filter(domain=domain)]
     # get the specified forms
     for id in ids:
         dataset = DataSet( str(value) + " for " + str(index) )
@@ -116,6 +130,7 @@ def get_user_activity_report(domain, ids, index, value, start_date, end_date, pa
 
 # TODO - filter returned data by user's domain
 def get_daily_activity_report(domain, ids, index, value, start_date, end_date, params):
+    # TODO - this currrently only works for value lists of size 1. FIX. 
     # CHW Daily Activity Report
     report = Report("CHW Daily Activity Report")
     form_list = FormDefModel.objects.filter(pk__in=ids)
@@ -123,19 +138,19 @@ def get_daily_activity_report(domain, ids, index, value, start_date, end_date, p
     #       member_list = utils.get_members(organization)
     # for now, just use domain
     
-    rs = ExtUser.objects.filter(domain=domain)
+    reporters = ReporterProfile.objects.filter(domain=domain)
     member_list = []
-    for r in rs:
-        chw_id = r.chw_id
-        member_list.append( chw_id )
+    for reporter in reporters:
+        chw_username = reporter.chw_username
+        member_list.append( chw_username )
     #member_list = [r.chw.id for r in ReporterProfile.objects.filter(domain=domain)]
-    username_counts = get_user_id_count(form_list, member_list, start_date, end_date)
+    username_counts = get_username_count(form_list, member_list, start_date, end_date)
     # return2 dict of username to: [firstdaycount, seconddaycount, thirddaycount]
     
     if params.has_key('chw'): chw = params['chw']
     else: raise Exception("This reports requires a CHW parameter")
     for form in form_list:
-        dataset = DataSet( str(value) + " for " + str(index) )
+        dataset = DataSet( unicode(value[0]) + " for " + unicode(index) )
         #for param in params:
         #    dataset.params[param.name] = param.value
         # e.g. stat='sum'
