96ed1957d2e36024e1b3d4033e656432a5c0cc45
U-rotop\rowena <rowena@rotop.(none)>
2009-08-06 02:23:44 -0400

    first pass at a superrosa API

---
+++ apps/transformers/xml.py
@@ -0,0 +1,63 @@
+
+from lxml import etree
+
+def xmlify(object_):
+    root = _xmlify(object_)
+    return etree.tostring(root, pretty_print=True)
+
+def _xmlify(object_):
+    """ a generic Python function to take a python object
+    and serialize it as XML. Note that the output is entirely 
+    dependent on the data structure of the input - which makes this 
+    function useful for xmlifying a variety of python objects.
+    However, if we want to customize the xml output format at any time,
+    we can always drop in a different type of XMLify-er to 
+    reporters/api_/resources.py
+    
+    understands: python objects, strings, tuples, + dictionaries of basic types
+    plus lists of either basic types or any of the above
+    
+    """
+    # end-condition: when we receive a tuple or a custom object
+    if isinstance( object_, tuple):
+        # the first element of tuple is attribute
+        # the second is text
+        root = etree.Element( 'entry', index=sanitize(unicode(object_[0])) )
+        root.text=unicode(object_[1])
+    else:
+        root = etree.Element( sanitize(str(type(object_))) )
+        for i in object_.__dict__:
+            i_val = getattr(object_,i)
+            if isinstance(i_val, basestring):
+                # set strings to be attributes
+                root.set(sanitize(i),sanitize(i_val) )
+            elif isinstance( i_val, list):
+                # i = string name of field
+                # i_val = actual field value
+                children = etree.Element( pluralize(i) )
+                for val in i_val:
+                    child = _xmlify(val)
+                    children.append(child)
+                root.append(children)
+            elif isinstance( i_val, dict):
+                # i = string name of field
+                # i_val = actual field value
+                children = etree.Element( pluralize(i) )
+                for key in i_val.keys():
+                    child = etree.etree.Element( sanitize(i) , name=sanitize(key), value=sanitize(i_val[key]) )
+                    children.append(child)
+                root.append(children)
+            else:
+                # set custom data structures to child elements
+                child = _xmlify(i)
+                root.append(child)
+    return root
+
+def pluralize(string):
+    return string
+
+def sanitize(string):
+    sanitized = string.replace(" ","_").replace("<","_").replace(">","_").replace("'","_").replace("-","_").replace("/","_").replace(":","").replace(".","_")
+    stripped = sanitized.strip('_')
+    tail = stripped.rsplit('_',1)[-1]
+    return tail
