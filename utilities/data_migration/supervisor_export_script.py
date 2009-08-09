fbeda18c81d802da3efabc74cff4254d1718461d
Cory Zue <czue@dimagi.com>
2009-08-09 10:51:58 -0400

    slight tweaks to the supervisor export script.  allow you to specify what directory you want the new files created in, rather than assuming you want to make an "out" folder wherever you're looking.

--- utilities/data_migration/supervisor_export_script.py
+++ utilities/data_migration/supervisor_export_script.py
@@ -5,23 +5,20 @@ import os
 from datetime import datetime
 import json
 
-#directory = r'C:\Source\hq\commcare-hq\django-hq\brac'
-#directory = r'C:\Source\hq\commcare-hq\django-hq\pathfinder'
-#directory = r'C:\Source\hq\data\brac-chp-jul-24\touse2'
-#directory = r'C:\Source\hq\data\brac-chw-jul-24\touse2'
-#directory = r'C:\Source\hq\data\brac\CHP\commcare\input'
-#directory = r'C:\Source\hq\data\brac\CHW\commcare\input'
-directory = r'C:\Source\hq\data\pathfinder\commcare\input'
-
-#domain = "BRAC"
-domain = "Pathfinder"
+directory = r'C:\Source\hq\data\supervisor_export_all_08-08-2009\export\BRAC\CHW\commcare\input'
+out_directory = r'C:\Source\hq\data\supervisor_export_all_08-08-2009\brac-pf'
+ 
+domain = "BRAC"
+#domain = "Pathfinder"
 files = os.listdir(directory)
+count = 0
 for file in files:
     if "xml" in file:
         filename = (os.path.join(directory,file))
         file = open(filename, "rb")
         dir, short_filename = os.path.split(filename)
-        new_dir = os.path.join(dir, "out")
+        #new_dir = os.path.join(dir, "out")
+        new_dir = out_directory
         payload = file.read()
         file.close()
         if not os.path.exists(new_dir):
@@ -40,3 +37,6 @@ for file in files:
         fout.write("\n\n")
         fout.write(payload)
         fout.close()
+        count = count + 1
+        
+print "exported %s forms from %s to %s" % (count, directory, out_directory)
