8d17291c12782b78ed5f414847f1ebc12a371769
Cory Zue <czue@dimagi.com>
2009-08-25 15:35:05 -0400

    Adding a method to validate every xform in a jar by attempting to convert it into a schema and ensuring everything goes fine. Adding a has_error return argument to the form translation method, since it was returning a confusingly named variable called "errors" that didn't actually always have errors in it.

--- apps/xformmanager/tests/basic_util.py
+++ apps/xformmanager/tests/basic_util.py
@@ -7,9 +7,8 @@ class UtilTestCase(unittest.TestCase):
     def testTranslateXFormToSchema(self):
         """ Test basic xform to schema translation"""
         fin = open(os.path.join(os.path.dirname(__file__),"basic.xform"),'r')
-        (schema,err) = form_translate( "basic.xform", fin.read() )
-        if err is not None:
-            if err.lower().find("exception") != -1:
-                self.fail(err)
+        (schema,err, has_error) = form_translate( "basic.xform", fin.read() )
+        if has_error:
+            self.fail(err)
         
         
