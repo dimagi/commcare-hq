ddd6ab26374571122fbd1a47bd27a733804df51b
U-rotop\rowena <rowena@rotop.(none)>
2009-09-11 23:46:48 -0400

    First pass at generate_submissions: a utility which POSTs a bz2-compressed list of MD5s, as well as the REST api for receiver which reads that list of MD5s, compares it with MD5s of its submissions, and returns a bz2-compressed tar archive of all the missing submissions in xml

--- apps/receiver/api_/urls.py
+++ apps/receiver/api_/urls.py
@@ -6,7 +6,7 @@ from receiver.api_.resources import *
 urlpatterns = patterns('',
    # Retrieve all forms submitted to a specific schema
    # ?format=xml&start-id=<start-id>&end-id=<end-id>
-   (r'^api/submissions/$', Submissions() ),    
-   (r'^api/(?P<domain_id>\d+)/submissions/$', Submissions() ),    
+   (r'^api/submissions/$', get_submissions ),
+   (r'^api/(?P<domain_id>\d+)/submissions/$', get_submissions ),    
 )
 
