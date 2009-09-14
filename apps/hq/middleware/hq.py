68c4306e5df233d1c3371c7ba14f326dc7927f98
Cory Zue <czue@dimagi.com>
2009-09-14 13:06:15 -0400

    Adding a .extuser to the request in the middleware.  Eventually we'll want to handle .extuser with annotations.

--- apps/hq/middleware/hq.py
+++ apps/hq/middleware/hq.py
@@ -11,9 +11,23 @@ class HqMiddleware(object):
         if request.user:
             try:
                 extuser = ExtUser.objects.get(id=request.user.id)
+                # override the request.user property so we can call it 
+                # easily within templates and default to the standard
+                # "user" object when this fails. 
                 request.user = extuser
-            except:
-                pass
+                # also duplicate it as request.extuser so we can check
+                # this property in our views. 
+                request.extuser = extuser
+            except Exception:
+                # make sure the property is set either way to avoid 
+                # having extraneous hasattr() calls.  Most likely
+                # this was an ExtUser.DoesNotExist, but it could
+                # be anything something else and we don't want to
+                # fail hard in the middleware layer. 
+                request.extuser = None
+        else:
+            # ditto exception block
+            request.extuser = None
         return None
         
        
