ac10838a13e3969f632e3ce500b3b2c2be126932
U-rotop\rowena <rowena@rotop.(none)>
2009-09-24 17:00:02 -0400

    Fixes plus unit test to make sure we don't sync dupes.

--- apps/django_rest_interface/util.py
+++ apps/django_rest_interface/util.py
@@ -78,35 +78,6 @@ def process_folder(folder_name, callback, *additional_arg):
             print "%s %s" % (callback, pathname)
             callback(pathname, *additional_arg)
 
-def get_stack_diff(small_stack, big_stack):
-    # compare the list of received MD5's with the local mD5
-    counter_small = 0
-    counter_big = 0
-    results = []
-    while True:
-        if counter_small == len(small_stack):
-            results.extend(big_stack[counter_big:])
-            break
-        if counter_big == len(big_stack):
-            # Reached the end of the list of local submissions before reaching
-            # the end of received mD5s. This should really never happen. 
-            # But might, if you were syncing with two different cchq servers.
-            logging.warn("Local submission count less than received submission count!")
-            break
-        if small_stack[counter_small] == big_stack[counter_big]:
-            counter_small = counter_small + 1
-            counter_big = counter_big + 1
-        elif small_stack[counter_small] > big_stack[counter_big]:
-            # found an entry in local which is not in received
-            results.append( big_stack[counter_big] )
-            counter_big = counter_big + 1
-        else:
-            # found an entry in received which is not in local
-            # skip it
-            logging.error("Skipping unrecognized received MD5!")
-            counter_small = counter_small + 1
-    return results
-
 def bz2_to_list(buffer):
     string_of_values = bz2.decompress(buffer)
     stream_of_values = cStringIO.StringIO(string_of_values)
@@ -128,13 +99,73 @@ def get_field_as_bz2(django_model, field_name, debug=False):
     """
     string = cStringIO.StringIO()
     if not debug:
-        objs = django_model.objects.all().order_by(field_name)
+        objs = django_model.objects.all().distinct(field_name).order_by(field_name)
     else:
-        print "DEBUG MODE: Only generating some schemata"
+        print "DEBUG MODE: Only generating some fields"
         # arbitrarily return only 10 of the MD5s
-        objs = django_model.objects.all().order_by(field_name)[:5]
+        objs = django_model.objects.all().distinct(field_name).order_by(field_name)[:5]
     if objs.count() == 0:
         return ''
     for obj in objs:
         string.write(unicode( getattr(obj, field_name) ) + '\n')
     return bz2.compress(string.getvalue())
+
+def get_stack_diff(stack1, stack2):
+    # compare the list of received MD5's with the local mD5
+    # no guarantee of order
+    results = []
+    set_1 = set(stack1)
+    set_2 = set(stack2)
+    if len(set_1) > len(set_2):
+        big_set = set_1
+        small_set = set_2
+    else:
+        big_set = set_2
+        small_set = set_1
+    for i in big_set:
+        if i not in small_set:
+            results.append(i)
+    return results
+    
+    """ If performance ever becomes an issue, we can try the following code.
+    (since we have it anyways). For now, use the above. ""
+    counter_small = 0
+    counter_big = 0
+    results = []
+    previous_small_stack_value = 0
+    previous_big_stack_value = 0
+    while True:
+        # <skip dupes> although we should never get dupes
+        # so this is more of a sanity check than anything else
+        while counter_small != len(small_stack) and previous_small_stack_value == small_stack[counter_small]:
+            counter_small = counter_small + 1
+        while counter_big != len(big_stack) and previous_big_stack_value == big_stack[counter_big]:
+            counter_big = counter_big + 1
+        # </skip dupes>
+        if counter_small == len(small_stack):
+            results.extend(big_stack[counter_big:])
+            break
+        if counter_big == len(big_stack):
+            # Reached the end of the list of local submissions before reaching
+            # the end of received mD5s. This should really never happen. 
+            # But might, if you were syncing with two different cchq servers.
+            logging.warn("Local submission count less than received submission count!")
+            break
+        if small_stack[counter_small] == big_stack[counter_big]:
+            previous_small_stack_value = small_stack[counter_small]
+            previous_big_stack_value = big_stack[counter_big]
+            counter_small = counter_small + 1
+            counter_big = counter_big + 1
+        elif small_stack[counter_small] > big_stack[counter_big]:
+            # found an entry in local which is not in received
+            results.append( big_stack[counter_big] )
+            previous_big_stack_value = big_stack[counter_big]
+            counter_big = counter_big + 1
+        else:
+            # found an entry in received which is not in local
+            # skip it
+            logging.error("Skipping unrecognized received MD5!")
+            previous_small_stack_value = small_stack[counter_small]
+            counter_small = counter_small + 1
+    return results
+    """
