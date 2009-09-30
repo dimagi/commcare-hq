9ca86219c873282623be82c80801c20dcb3c5daf
Rowena <rluk@dimagi.com>
2009-09-30 18:14:30 -0400

    make a Formdef.from_file factory method

--- apps/xformmanager/tests/__init__.py
+++ apps/xformmanager/tests/__init__.py
@@ -9,6 +9,5 @@ from meta import *
 from api import *
 from views import *
 from versioned import *
-# todo: uncomment these when the form difference tests work
 from formdef_checker import *
 from xsd_checker import *
