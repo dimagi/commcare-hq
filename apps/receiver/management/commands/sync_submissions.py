6908a6a4f7f4eb195c9db381f7f0277c5901059e
rowenaluk <rluk@dimagi.com>
2009-10-15 22:44:08 -0400

    Updated sync scripts to take an optional argument specifying local server DNS

--- apps/receiver/management/commands/sync_submissions.py
+++ apps/receiver/management/commands/sync_submissions.py
@@ -9,33 +9,34 @@ from receiver.management.commands.generate_submissions import generate_submissio
 from receiver.management.commands.load_submissions import load_submissions
 
 class Command(LabelCommand):
-    option_list = LabelCommand.option_list + (
-        make_option('--localport', action='store', dest='localport', \
-                    default='8000', help='Port of local server'),
-    )
     help = "Synchronizes CommCareHQ with a specified remote server."
-    args = "<remote_ip username password>"
+    args = "<remote_ip username password local_dns>"
     label = 'IP address of the remote server (including port), ' + \
-            'username, and password'
+            'username, password, and DNS of local server'
     
     def handle(self, *args, **options):
-        if len(args) != 3:
+        if len(args) < 3:
             raise CommandError('Please specify %s.' % self.label)
         remote_ip = args[0]
         username = args[1]
         password = args[2]
+        local_dns = None
+        if len(args) == 4:
+            local_dns = args[3]
         print "This script assumes a local server is running. " + \
               "To launch your local server, run './manage.py runserver'"
         rest_util.are_you_sure()
         
-        localport = options.get('localport', 8000)
-        synchronize_submissions(remote_ip, username, password, localport)
+        if local_dns:
+            synchronize_submissions(remote_ip, username, password, local_dns)
+        else:
+            synchronize_submissions(remote_ip, username, password)
                 
     def __del__(self):
         pass
     
-def synchronize_submissions(remote_ip, username, password, localport=8000):
+def synchronize_submissions(remote_ip, username, password, local_server="127.0.0.1:8000"):
     file = 'submissions.tar'
     generate_submissions(remote_ip, username, password, \
                          download=True, to=file)
-    load_submissions(file, localport)
+    load_submissions(file, local_server)
