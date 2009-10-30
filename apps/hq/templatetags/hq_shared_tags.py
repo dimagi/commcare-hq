90956021874ff9ec424e4874bd0ea3c02a40c894
czue <czue@dimagi.com>
2009-10-30 16:53:41 -0400

    More fully featured (UI wise) metadata report view.  Also, removing duplicated template tags from xformmanager.

--- apps/hq/templatetags/hq_shared_tags.py
+++ apps/hq/templatetags/hq_shared_tags.py
@@ -3,7 +3,26 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dict, key):
+def dict_lookup(dict, key):
     '''Get an item from a dictionary.'''
     return dict.get(key)
+
+@register.filter
+def array_lookup(array, index):
+    '''Get an item from an array.'''
+    if index < len(array):
+        return array[index]
+    
+@register.filter
+def attribute_lookup(obj, attr):
+    '''Get an attribute from an object.'''
+    if (hasattr(obj, attr)):
+        return getattr(obj, attr)
+    
+
+@register.simple_tag
+def dict_as_query_string(dict, prefix=""):
+    '''Convert a dictionary to a query string, minus the initial ?'''
+    print prefix
+    return "&".join(["%s%s=%s" % (prefix, key, value) for key, value in dict.items()])
 
