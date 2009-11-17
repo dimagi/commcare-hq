fd726947f8552b3753fe232b6a8ad431b5559dd8
rowenaluk <rluk@dimagi.com>
2009-11-17 11:02:52 -0500

    tiny: small fix to previous hack so that it works from other linked reports

--- apps/receiver/static/javascripts/annotations.js
+++ apps/receiver/static/javascripts/annotations.js
@@ -1,16 +1,7 @@
 
-/*
+
 function show_annotations(attachment_id, div_id) {
     // uses jqModal to show annotations in an ajaxy way.  Pass in the attachment
     // to annotate, and the div where you want to display your ajax.
     $(div_id).jqm({ajax: '/receiver/annotations/' + attachment_id, trigger: 'div.annotationtrigger'});
-}
-
-rO - Nov 17 HACK
-*/
-
-function show_annotations(url, div_id) {
-    // uses jqModal to show annotations in an ajaxy way.  Pass in the attachment
-    // to annotate, and the div where you want to display your ajax.
-    $(div_id).jqm({ajax: url, trigger: 'div.annotationtrigger'});
 }
