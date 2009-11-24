e57f8b5f51e6c1a50e28c31a4a76843d1562f5a7
rowenaluk <rluk@dimagi.com>
2009-11-24 17:30:27 -0500

    Better checks for empty submission.raw_post. Also, better recovery for sync scripts.

--- apps/receiver/api_/resources.py
+++ apps/receiver/api_/resources.py
@@ -72,8 +72,17 @@ def _get_submissions(request, domain_id=0):
         # Revisit if xforms get super long
         try:
             export = submission.export()
+            # the following error types shouldn't halt submission export
         except Submission.DoesNotExist:
             logging.error("%s could not be found. Data export failed." \
+                          % submission.raw_post)
+            continue
+        except TypeError, e:
+            logging.error("Poorly formatted submission: %s. Data export failed." \
+                          % submission.raw_post)
+            continue
+        except UnicodeEncodeError, UnicodeDecodeError:
+            logging.error("Unicode error for submission: %s. Data export failed." \
                           % submission.raw_post)
             continue
         file_added = True
