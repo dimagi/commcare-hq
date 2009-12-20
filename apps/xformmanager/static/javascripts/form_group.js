519c0ead09f0ca8f28b9144d591276fcd2903739
czue <czue@dimagi.com>
2009-12-20 15:26:00 -0500

    Adding loading text for the ajax popups.

--- apps/xformmanager/static/javascripts/form_group.js
+++ apps/xformmanager/static/javascripts/form_group.js
@@ -2,5 +2,6 @@
 function show_forms(xmlns, div_id) {
     // uses jqModal to show annotations in an ajaxy way.  Pass in the attachment
     // to annotate, and the div where you want to display your ajax.
-    $(div_id).jqm({ajax: '/xforms/groups?xmlns=' + xmlns, trigger: 'div.formtrigger'});
+    $(div_id).jqm({ajax: '/xforms/groups?xmlns=' + xmlns, trigger: 'div.formtrigger', 
+                  ajaxText: 'Please wait while we load that for you'});
 }
