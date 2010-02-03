b524b818f6487e6ee1c4bb15b816c38b66267f2c
Nir Yariv <niryariv@gmail.com>
2010-02-03 13:24:21 -0500

    move image_path to local.ini

--- apps/photos/models.py
+++ apps/photos/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from imagekit.models import ImageModel
-
+import settings
 
 class Photo(ImageModel):
     name = models.CharField(max_length=100)
-    original_image = models.ImageField(upload_to='static/photos')#(upload_to='apps/photos/static')
+    original_image = models.ImageField(upload_to='/tmp') # upload_to is meaningless since we get the photos via xform, but it's required by ImageModel
     num_views   = models.PositiveIntegerField(editable=False, default=0)
     created_at  = models.DateTimeField(auto_now_add=True)
 
     class IKOptions:
         # This inner class is where we define the ImageKit options for the model
         spec_module = 'photos.specs'
-        cache_dir = 'data/photos'
+        cache_dir = settings.RAPIDSMS_APPS['photos']['image_path']
         image_field = 'original_image'
         save_count_as = 'num_views'
         
