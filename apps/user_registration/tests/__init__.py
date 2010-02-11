95a4692b46b8f15068f51eb40e8246da12db3c2a
czue <czue@dimagi.com>
2010-02-11 11:58:34 -0500

    user_registration app.  Turnkey dependency.  NOT included currently are docs and translations.

---
+++ apps/user_registration/tests/__init__.py
@@ -0,0 +1,4 @@
+from user_registration.tests.backends import *
+from user_registration.tests.forms import *
+from user_registration.tests.models import *
+from user_registration.tests.views import *
