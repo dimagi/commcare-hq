f1e68dc132c524110ad132b673ad62322fa0e394
czue <czue@dimagi.com>
2010-02-17 14:51:39 -0500

    Updating help urls to be reversible/urlable from templates and code.

--- urls.py
+++ urls.py
@@ -2,8 +2,7 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 from django.conf.urls.defaults import *
-import apps.docs.views as views
 
 urlpatterns = patterns('',
-    url(r'^docs/(?P<topic>\w+)$',  views.index),
+    url(r'^docs/(?P<topic>\w+)$',  'docs.views.index', name='index'),
 )
