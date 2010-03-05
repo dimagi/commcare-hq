2ee6e1f4b522ae5ca52099a02b7aa64615b8fe3b
Nir Yariv <niryariv@gmail.com>
2010-03-05 15:05:24 -0500

    add basic role system for intel

---
+++ apps/intel/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from intel.models import *
+
+admin.site.register(Role)
+admin.site.register(MemberRole)
--- apps/intel/models.py
+++ apps/intel/models.py
@@ -1,3 +1,47 @@
-# using the reports/graphing models. no models here - unless there's some reason in the future..
-
 from django.db import models
+from domain.models import Membership
+# from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+# ad hoc role system
+# Role holds a list of roles w/ id
+# Member role connects a domain member to a role
+# A member may have more than 1 role
+
+class Role(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Role")
+
+# MemberRole.objects.filter(member = 4)
+# [<MemberRole: brian: chw>, <MemberRole: brian: doctor>]
+
+class MemberRole(models.Model):
+    member = models.ForeignKey(Membership)
+    role = models.ForeignKey(Role)
+
+    def __unicode__(self):
+        return "%s: %s" % (self.member.member_object, self.role)
+        
+    class Meta:
+        verbose_name = _("Member Role")
+
+    
+    # helper method: get array of roles for a Member
+    # >>> MemberRole.per(4)
+    # [u'chw', u'doctor']
+    # useful for easily checking a user's role, eg:
+    # >>> if 'chw' in MemberRole.per(4): ....
+    
+    @staticmethod
+    def per(member):
+        roles = []
+        for r in MemberRole.objects.filter(member = member):
+            roles.append(r.role.name)
+        
+        return roles
