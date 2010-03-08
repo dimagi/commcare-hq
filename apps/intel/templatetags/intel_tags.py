5be9e314468c84cb3c25ce9055a3f2fef3c985c1
Nir Yariv <niryariv@gmail.com>
2010-03-08 18:58:06 -0500

    handle unknown tab gracefully (ie dont crash)

--- apps/intel/templatetags/intel_tags.py
+++ apps/intel/templatetags/intel_tags.py
@@ -8,12 +8,6 @@ register = template.Library()
 @register.simple_tag
 def tabs(current_page):
 
-    def is_current(tab_name):
-        if tab_name == current_page:
-            return "active"
-        else:
-            return ""
-
     tabs = { "all" : 
                 {
                     "text" : "All Pregnant Mothers",
@@ -37,11 +31,12 @@ def tabs(current_page):
         bgcolor = (tabs[i]["color"] if i == current_page else "white")
         t+= '<a href="/intel/%s" style="background-color: %s">%s</a>\n' % (i, bgcolor, tabs[i]["text"])
 
-    # icon from http://pixel-mixer.com/ - free for use but link required. We might want to have our own instead.
+    # icon from http://pixel-mixer.com/ - free for use but link required. We might want to get our own instead.
     t += '<a href="/intel/all" style="position: absolute; right: 0;"><img src="/static/intel/img/home.png" /></a><!-- icon by http://pixel-mixer.com/ -->'
 
     t += "</div>"
     
-    t += '<script type="text/javascript">$("#header").css("border-bottom-color", "%s");</script>' % tabs[current_page]["color"]
+    if tabs.has_key(current_page): 
+        t += '<script type="text/javascript">$("#header").css("border-bottom-color", "%s");</script>' % tabs[current_page]["color"]
 
     return t
