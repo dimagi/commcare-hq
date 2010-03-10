e8bb9507da76fae5c010a42dd1b03ddcbf9a3347
Nir Yariv <niryariv@gmail.com>
2010-03-10 16:41:09 -0500

    merged all views to one

--- apps/intel/urls.py
+++ apps/intel/urls.py
@@ -1,10 +1,19 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^intel/?$', 'intel.views_reports.all_mothers_report'),
-    (r'^intel/all/?$', 'intel.views_reports.all_mothers_report'),
-    (r'^intel/risk/?$', 'intel.views_reports.hi_risk_report'),
-    (r'^intel/details/?$', 'intel.views_reports.mother_details'),
+    (r'^intel/?$', 'intel.views.all_mothers_report'),
+    (r'^intel/all/?$', 'intel.views.all_mothers_report'),
+    (r'^intel/risk/?$', 'intel.views.hi_risk_report'),
+    (r'^intel/details/?$', 'intel.views.mother_details'),
     
-    (r'^intel/chart/?$', 'intel.views_charts.chart'),
+    (r'^intel/chart/?$', 'intel.views.chart'),
 )
+
+# urlpatterns = patterns('',
+#     (r'^intel/?$', 'intel.views_reports.all_mothers_report'),
+#     (r'^intel/all/?$', 'intel.views_reports.all_mothers_report'),
+#     (r'^intel/risk/?$', 'intel.views_reports.hi_risk_report'),
+#     (r'^intel/details/?$', 'intel.views_reports.mother_details'),
+#     
+#     (r'^intel/chart/?$', 'intel.views_charts.chart'),
+# )
--- apps/intel/views.py
+++ apps/intel/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext
 from django.core.exceptions import *
 
@@ -36,6 +35,48 @@ import string
 import reports.util as util
 from reports.custom.all.shared import get_data_by_chw, get_case_info
 from reports.models import Case, SqlReport
+
+
+######## Report Methods
+@login_and_domain_required
+def all_mothers_report(request):
+    '''View all mothers - default'''
+    return custom_report(request, 3, "chw_submission_details", "all")
+
+@login_and_domain_required
+def hi_risk_report(request):
+    '''View only hi risk'''
+    return custom_report(request, 3, "hi_risk_pregnancies", "risk")
+
+@login_and_domain_required
+def mother_details(request):
+    '''view details for a mother'''
+    return custom_report(request, 3, "_mother_summary", "single")
+    
+
+
+def custom_report(request, domain_id, report_name, page):
+    context = {}
+    context['page'] = page
+    context["report_name"] = report_name
+    report_method = util.get_report_method(request.user.selected_domain, report_name)
+    # return HttpResponse(report_method(request))
+    if not report_method:
+        return render_to_response(request, 
+                                  "report_not_found.html",
+                                  context)
+    context["report_display"] = report_method.__doc__
+    context["report_body"] = report_method(request)
+    
+    if 'search' not in request.GET.keys(): 
+        context['search_term'] = ''
+    else:
+        context['search_term'] = request.GET['search']
+
+    return render_to_response(request, "report_base.html", context)
+
+
+######## Chart Methods
 
 @login_and_domain_required
 def chart(request):
