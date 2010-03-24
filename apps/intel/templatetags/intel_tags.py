42c4844cc8d0b638e7e967a55a375fccc7704093
Nir Yariv <niryariv@gmail.com>
2010-03-24 17:35:12 -0400

    vary buttons according to role

--- apps/intel/templatetags/intel_tags.py
+++ apps/intel/templatetags/intel_tags.py
@@ -6,32 +6,15 @@ from django.template import RequestContext
 register = template.Library()
 
 @register.simple_tag
-def tabs(current_page):
 
-    tabs = { "all" : 
-                {
-                    "tooltip" : "All Pregnant Mothers", # we don't have tooltips, but maybe someday
-                    "text" : "Pregnant Mothers",
-                    "color": "#ff6100",
-                    "icon" : "/static/intel/img/icons/preg-icon.png"
-                },
-                
-             "risk":  
-                {
-                    "tootlip" : "High Risk For Follow Up",
-                    "text" : "High Risk",
-                    "color": "#82c700",
-                    "icon" : "/static/intel/img/icons/risk-icon.png"
-                },
-              "chart": 
-                {
-                    "tooltip" : "Program Trends",
-                    "text" : "Program Trends",
-                    "color": "#0099ff",
-                    "icon" : "/static/intel/img/icons/trends-icon.png"
-                }
-            }
 
+def tabs(current_page, hq_mode=False):
+
+    if hq_mode:
+        tabs = _hq_tabs()
+    else:
+        tabs = _doctor_tabs()
+    
     t = '<div class="buttons">'
     
     for i in tabs:
@@ -40,17 +23,53 @@ def tabs(current_page):
         but = '<a href="/intel/%s"><img class="left" src="%s"/><span style="vertical-align:top">%s</span></a>' % (i, tabs[i]["icon"], tabs[i]["text"])
         if i == current_page or current_page == "home":
             t += but 
-            
-        #but = '<span style="border: 5px solid %s">%s</span>' % (tabs[i]["color"], but)
         
 
     if current_page != "home":
-        # icon from http://pixel-mixer.com/ - free for use but link required. We might want to get our own instead.
-        #t += '<a href="/intel/" style="position: absolute; right: 0;"><img src="/static/intel/img/home.png" /></a><!-- icon by http://pixel-mixer.com/ -->'
-        t += '<a href="/intel/" style="position: absolute; right: 0; width: 90px"><img src="/static/intel/img/icons/home-icon.png" /></a>'
+        t += '<a href="/intel/" style="float: right; width: 90px"><img src="/static/intel/img/icons/home-icon.png" /></a>'
     t += "</div>"
     
-    if tabs.has_key(current_page): 
-        t += '<script type="text/javascript">$("#header").css("border-bottom-color", "%s");</script>' % tabs[current_page]["color"]
+    # if tabs.has_key(current_page): 
+    #     t += '<script type="text/javascript">$("#header").css("border-bottom-color", "%s");</script>' % tabs[current_page]["color"]
 
     return t
+
+
+
+def _doctor_tabs():
+    return { "all" : 
+                {
+                    "text" : "Pregnant Mothers",
+                    # "color": "#ff6100",
+                    "icon" : "/static/intel/img/icons/preg-icon.png"
+                },
+
+             "risk":  
+                {
+                    "text" : "High Risk",
+                    # "color": "#82c700",
+                    "icon" : "/static/intel/img/icons/risk-icon.png"
+                },
+              "chart": 
+                {
+                    "text" : "Program Trends",
+                    # "color": "#0099ff",
+                    "icon" : "/static/intel/img/icons/trends-icon.png"
+                }
+            }
+
+def _hq_tabs():
+    return { "hq_hi_risk":  
+                {
+                    "text" : "High Risk",
+                    # "color": "#82c700",
+                    "icon" : "/static/intel/img/icons/risk-icon.png"
+                },
+              "hq_chart": 
+                {
+                    "text" : "Program Trends",
+                    # "color": "#0099ff",
+                    "icon" : "/static/intel/img/icons/trends-icon.png"
+                }
+            }
+    
