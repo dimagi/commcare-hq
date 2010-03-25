193a763783ad6746abb5faba2f297f82ad091425
Nir Yariv <niryariv@gmail.com>
2010-03-25 17:03:51 -0400

    same font on buttons

--- apps/intel/templatetags/intel_tags.py
+++ apps/intel/templatetags/intel_tags.py
@@ -30,9 +30,6 @@ def tabs(current_page, hq_mode=False):
         t += '<a href="/intel/" style="float: right; width: 90px"><img src="/static/intel/img/icons/home-icon.png" /></a>'
     t += "</div>"
     
-    # if tabs.has_key(current_page): 
-        # t += '<script type="text/javascript">$("#header").css("border-bottom-color", "%s");</script>' % tabs[current_page]["color"]
-
     return t
 
 
@@ -41,20 +38,17 @@ def _doctor_tabs():
     return { "all" : 
                 {
                     "text" : "Pregnant Mothers",
-                    # "color": "#ff6100",
                     "icon" : "/static/intel/img/icons/preg-icon.png"
                 },
 
              "risk":  
                 {
                     "text" : "High Risk",
-                    # "color": "#82c700",
                     "icon" : "/static/intel/img/icons/risk-icon.png"
                 },
               "chart": 
                 {
                     "text" : "Program Trends",
-                    # "color": "#0099ff",
                     "icon" : "/static/intel/img/icons/trends-icon.png"
                 }
             }
