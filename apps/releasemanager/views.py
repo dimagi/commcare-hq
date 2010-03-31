3b5b715d6aca0dc820167388a312d02d7c0491e6
Nir Yariv <niryariv@gmail.com>
2010-03-31 18:41:16 -0400

    adding builds and resources views

--- apps/releasemanager/views.py
+++ apps/releasemanager/views.py
@@ -31,52 +31,135 @@ import mimetypes
 import urllib
 
 
-def download(request, filename):
-    print "DOWNLOAD: ", filename
+@login_and_domain_required
+def jarjad_set_release(request, id, set_to):
+    if set_to == "true" or set_to == "false":
+        Jarjad.objects.filter(id=id).update(is_release=(set_to == "true"))
+    return HttpResponseRedirect(reverse('releasemanager.views.jarjad'))
     
-    
-def list(request, template_name="releasemanager/home.html"): 
-    context = {'form' : BuildForm(), 'builds': {}}
-    context['builds']['unreleased'] = Build.objects.filter(released_by__isnull=True).order_by('-created_at')
-    context['builds']['released']   = Build.objects.filter(released_by__isnull=False).order_by('-created_at')
+@login_and_domain_required
+def jarjad(request, template_name="releasemanager/jarjad.html"): 
+    context = {'form' : JarjadForm(), 'items': {}}
+    context['items']['unreleased'] = Jarjad.objects.filter(is_release=False).order_by('-created_at')
+    context['items']['released']   = Jarjad.objects.filter(is_release=True).order_by('-created_at')
     
     return render_to_response(request, template_name, context)
 
-@login_required()
+
+@login_and_domain_required
 def make_release(request, id):
-    build = Build.objects.get(id=id)
-    build.released_by = request.user
-    build.save()
-    return HttpResponseRedirect(reverse('releasemanager.views.list'))
+    Jarjad.objects.all().update(is_release=False)
+    Jarjad.objects.filter(id=id).update(is_release=True)
+    return HttpResponseRedirect(reverse('releasemanager.views.jarjad'))
     
-@login_required()
-def new(request, template_name="releasemanager/home.html"):
+
+@login_and_domain_required
+def new(request, template_name="releasemanager/jarjad.html"):
     '''save new Jad & Jar into the system'''
     
     context = {}
-    form = BuildForm()    
+    form = JarjadForm()    
     if request.method == 'POST':
-        form = BuildForm(request.POST, request.FILES)                
+        form = JarjadForm(request.POST, request.FILES)                
         if form.is_valid():
             try:                      
-                Build = form.save(commit=False)
-                Build.uploaded_by=request.user
-                Build.description = urllib.unquote(Build.description)
+                jj = form.save(commit=False)
+                jj.uploaded_by=request.user
+                jj.description = urllib.unquote(jj.description)
 
-                Build.save_file(request.FILES['jad_file_upload'])
-                Build.save_file(request.FILES['jar_file_upload'])
+                jj.save_file(request.FILES['jad_file_upload'])
+                jj.save_file(request.FILES['jar_file_upload'])
 
-                Build.save()
+                jj.save()
                 # _log_build_upload(request, newbuild)
-                return HttpResponseRedirect(reverse('releasemanager.views.Build_list'))
+                return HttpResponseRedirect(reverse('releasemanager.views.jarjad'))
             except Exception, e:
                 logging.error("Build upload error.", 
                               extra={'exception':e, 
                                      'request.POST': request.POST, 
                                      'request.FILES': request.FILES, 
                                      'form':form})
-                context['errors'] = "Could not commit Build: " + str(e)
+                context['errors'] = "Could not commit: " + str(e)
     
     context['form'] = form
     return render_to_response(request, template_name, context)
     
+
+@login_and_domain_required
+def builds(request, template_name="releasemanager/builds.html"): 
+    context = {'form' : BuildForm(), 'items': {}}
+    context['items']['unreleased'] = Build.objects.filter(is_release=False).order_by('-created_at')
+    context['items']['released']   = Build.objects.filter(is_release=True).order_by('-created_at')
+
+    return render_to_response(request, template_name, context)
+
+
+@login_and_domain_required
+def build_set_release(request, id, set_to):
+    if set_to == "true" or set_to == "false":
+        Build.objects.filter(id=id).update(is_release=(set_to == "true"))
+    return HttpResponseRedirect(reverse('releasemanager.views.builds'))
+
+
+@login_and_domain_required
+def new_build(request, template_name="releasemanager/builds.html"):
+    context = {}
+    form = BuildForm()    
+    if request.method == 'POST':
+        form = BuildForm(request.POST)
+        if form.is_valid():
+            try:                      
+                b = form.save(commit=False)
+                b.domain = request.user.selected_domain
+                b.save()
+                return HttpResponseRedirect(reverse('releasemanager.views.builds'))
+            except Exception, e:
+                logging.error("Build upload error.", 
+                              extra={'exception':e, 
+                                     'request.POST': request.POST, 
+                                     'form':form})
+                context['errors'] = "Could not commit: " + str(e)
+
+    context['form'] = form
+    return render_to_response(request, template_name, context)
+
+
+
+
+@login_and_domain_required
+def resource_sets(request, template_name="releasemanager/resource_sets.html"): 
+    context = {'form' : ResourceSetForm(), 'items': {}}
+    context['items']['unreleased'] = ResourceSet.objects.filter(is_release=False).order_by('-created_at')
+    context['items']['released']   = ResourceSet.objects.filter(is_release=True).order_by('-created_at')
+
+    return render_to_response(request, template_name, context)
+
+
+@login_and_domain_required
+def resource_set_set_release(request, id, set_to):
+    if set_to == "true" or set_to == "false":
+        ResourceSet.objects.filter(id=id).update(is_release=(set_to == "true"))
+    return HttpResponseRedirect(reverse('releasemanager.views.resource_sets'))
+
+
+@login_and_domain_required
+def new_resource_set(request, template_name="releasemanager/resource_sets.html"):
+    context = {}
+    form = ResourceSetForm()    
+    if request.method == 'POST':
+        form = ResourceSetForm(request.POST)
+        if form.is_valid():
+            try:                      
+                b = form.save(commit=False)
+                b.domain = request.user.selected_domain
+                b.save()
+                return HttpResponseRedirect(reverse('releasemanager.views.resource_sets'))
+            except Exception, e:
+                logging.error("Build upload error.", 
+                              extra={'exception':e, 
+                                     'request.POST': request.POST, 
+                                     'form':form})
+                context['errors'] = "Could not commit: " + str(e)
+
+    context['form'] = form
+    return render_to_response(request, template_name, context)
