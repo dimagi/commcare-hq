4c4348258d551964c515ccb45b37829d9e3b5c3d
Nir Yariv <niryariv@gmail.com>
2010-04-09 13:55:10 -0400

    fix CSV

--- apps/intel/urls.py
+++ apps/intel/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
-    (r'^intel/?$', 'intel.views.homepage'),
-    (r'^intel/all/?$', 'intel.views.all_mothers_report'),
-    (r'^intel/risk/?$', 'intel.views.hi_risk_report'),
-    (r'^intel/details/?$', 'intel.views.mother_details'),
+urlpatterns = patterns('intel.views',
+    (r'^intel/?$', 'homepage'),
+    (r'^intel/all\.?(?P<format>[a-zA-Z0-9\.].*)?/?$', 'all_mothers_report'),
+    (r'^intel/risk\.?(?P<format>[a-zA-Z0-9\.].*)?/?$', 'hi_risk_report'),
+    (r'^intel/details/?$', 'mother_details'),
     
-    (r'^intel/chart/?$', 'intel.views.chart'),
+    (r'^intel/chart/?$', 'chart'),
     
-    (r'^intel/hq_chart/?$', 'intel.views.hq_chart'),
-    (r'^intel/hq_risk/?$', 'intel.views.hq_risk'),
+    (r'^intel/hq_chart/?$', 'hq_chart'),
+    (r'^intel/hq_risk/?$', 'hq_risk'),
 )
