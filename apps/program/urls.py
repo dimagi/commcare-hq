481cd279d9f3ad72bc03a684a4aa9a81892bfdc0
Nir Yariv <niryariv@gmail.com>
2010-06-11 14:06:11 -0400

    add programs app

---
+++ apps/program/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    url(r'^programs/list/?$', 'program.views.list_programs', name="list_programs"),
+    url(r'^programs/add/?$', 'program.views.add_program', name="add_program"),
+    url(r'^programs/edit/(?P<program_id>\d+)/?$', 'program.views.edit_program', name="edit_program"),
+    url(r'^programs/delete/(?P<program_id>\d+)/?$', 'program.views.delete_program', name="delete_program"),
+)
