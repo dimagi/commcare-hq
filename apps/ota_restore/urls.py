cccc4bf4002eaeb9df08c666720a3d51fafdae19
Nir Yariv <niryariv@gmail.com>
2010-06-26 20:07:11 -0400

    view method name == url

--- apps/ota_restore/urls.py
+++ apps/ota_restore/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 
 urlpatterns = patterns('',                       
-    url(r'^ota_restore/?$', 'ota_restore.views.restore'),
+    url(r'^ota_restore/?$', 'ota_restore.views.ota_restore'),
     (r'^digest_test/?$', 'ota_restore.views.digest_test'),
 
 )
