9e65a74ab77486799782494ec008179e0fbbb5fa
Nir Yariv <niryariv@gmail.com>
2010-07-09 19:09:12 -0400

    redirect to groups list after adding a group

--- apps/xformmanager/views.py
+++ apps/xformmanager/views.py
@@ -196,8 +196,10 @@ def new_form_data_group(req):
                     new_group.update_view()
                     
                     # when done, take them to the edit page for further tweaking
-                    return HttpResponseRedirect(reverse('xformmanager.views.edit_form_data_group', 
-                                            kwargs={"group_id": new_group.id }))
+                    return HttpResponseRedirect(reverse('xformmanager.views.manage_groups'))
+                    
+                    # return HttpResponseRedirect(reverse('xformmanager.views.edit_form_data_group', 
+                    #                         kwargs={"group_id": new_group.id }))
                     
                         
             else: 
