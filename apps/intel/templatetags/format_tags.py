13b22d979e7893086a3013da929c923158c16a5e
Nir Yariv <niryariv@gmail.com>
2010-07-26 16:56:06 -0400

    template tags in one place

---
+++ apps/intel/templatetags/format_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from django.http import HttpRequest
+from django.template import RequestContext
+
+
+register = template.Library()
+
+@register.simple_tag
+def url_for(request, format):
+    ''' adds a .format to the URL, taking query string into account
+        eg, convert '/intel/all?search=xy' to '/intel/all.csv?search=xy'
+    '''
+    path = request.path.rstrip('/')
+    full_path = request.get_full_path()
+
+    # if format already exists, do nothing
+    if path.endswith(".%s" % format):
+        return full_path
+    else:
+        return full_path.replace(path, path + "." + format, 1)
+        
+        
+@register.simple_tag
+def attr_name(attr):
+    ''' "sampledata_some_crap" => "some crap" '''
+    return attr.replace("sampledata_", "").replace("_", " ")
