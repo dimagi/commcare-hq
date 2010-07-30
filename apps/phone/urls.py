e25038346e1dffc49afb2d913c6c9a29e28da0db
Nir Yariv <niryariv@gmail.com>
2010-07-30 14:16:39 -0400

    add user delete option

--- apps/phone/urls.py
+++ apps/phone/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^phones/phone_users/(?P<user_id>\d+)/?$', 'phone.views.single_user', name='single_user'),
     url(r'^phones/phone_users/(?P<user_id>\d+)/new_user/?$', 'phone.views.create_user', name='create_user'),
     url(r'^phones/phone_users/(?P<user_id>\d+)/link_user/?$', 'phone.views.link_user', name='link_user'),
+    url(r'^phones/phone_users/(?P<user_id>\d+)/delete_user/?$', 'phone.views.delete_user', name='delete_user'),
     url(r'^phones/users/(?P<user_id>\d+)/?$', 'phone.views.single_django_user', name='single_django_user'),
 )
