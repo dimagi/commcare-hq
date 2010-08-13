d63b728f832bb4427bdbbd99f1db29b2165d5a9c
Rowena <rluk@dimagi.com>
2010-08-12 22:53:17 -0600

    udate selenium tests with right username and password for pathfinder

--- tests/deployment/selenium/hq_test.py
+++ tests/deployment/selenium/hq_test.py
@@ -7,7 +7,7 @@ import time
 
 
 local   = {"http://localhost:8000":     ["brian", "test", "localhost:8000", "Pathfinder"]}
-test = {"http://test.commcarehq.org": ["brian", "test", "test.commcarehq.org", "Pathfinder"]}
+test = {"http://test.commcarehq.org": ["pfadmin", "commcare123", "test.commcarehq.org", "Pathfinder"]}
 dev     = {"http://dev.commcarehq.org": ["brian", "test", "dev.commcarehq.org", "BRAC"]}
 data     = {"http://data.commcarehq.org": ["brian", "test", "data.commcarehq.org", "BRAC"]}
 staging = {"http://staging.commcarehq.org": ["brian", "test", "staging.commcarehq.org", "Pathfinder"]}
