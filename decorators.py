13578431d8d625f0440bb04940cd87d6aa268b33
Dan Myung <dmyung@dimagi.com>
2010-08-20 20:08:37 -0400

    vastly revised and simplified auditing framework for centralized audit handling for views and model events

--- decorators.py
+++ decorators.py
@@ -1,24 +1,16 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse,Http404
-from ashandapp.models import CareTeam
 import logging
+
+from models import AuditEvent
 
 def log_access(view_func):
     """
     Decorator for view - log each url and all params you're looking at.
     """
     def _log_access(request, *args, **kwargs):
-        print args
-        print kwargs
-        print request.path
-        print request.GET
-        print view_func.__code__        
-        print view_func.func_name
-        print request.user
-        print request.is_caregiver
-        print request.is_provider
-        print request.is_patient
+        AuditEvent.objects.audit_view(request, request.user, view_func)         
         ret = view_func(request, * args, **kwargs)        
         return ret
     return _log_access
--- models.py
+++ models.py
@@ -5,29 +5,106 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 from django.contrib.sessions.backends.db import SessionStore
 from django.contrib.sessions.models import Session
-from tracking.models import Visitor
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.db.models.query import QuerySet
+from django.utils.http import urlquote
+
+import utils
 
 def make_uuid():
     return uuid.uuid1().hex
 def getdate():
     return datetime.utcnow()
 
-class DeepTracking(models.Model):
-    """
-    A persistent session store for the access logging system for your site. 
-    """    
-    visitor = models.ForeignKey(Visitor)
+#class AuditManager(models.Manager):
+#    pass
 
-class AuditEvent(models.Model):    
-    id = models.CharField(_('Access guid'), max_length=32, unique=True, default=make_uuid, primary_key=True) #primary_key override?
+
+class AuditEvent(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True)
+    event_date = models.DateTimeField(default = getdate)
+    event_class = models.CharField(max_length=32, db_index=True, editable=False) #allow user defined classes
+    description = models.CharField(max_length=160)
+
+class ModelActionAudit(AuditEvent):
+    """
+    Audit event to track the modification or editing of an auditable model
+    """    
     object_type = models.ForeignKey(ContentType, verbose_name='Case linking content type', blank=True, null=True)
     object_uuid = models.CharField('object_uuid', max_length=32, db_index=True, blank=True, null=True)
-    content_object = generic.GenericForeignKey('object_type', 'object_uuid')
+    content_object = generic.GenericForeignKey('object_type', 'object_uuid')    
     
-    user = models.ForeignKey(User)
-    accessed = models.DateTimeField(default = getdate())
+    @classmethod
+    def audit_save(cls, model_class, instance, user):
+        audit = cls()
+        audit.user = user 
+        audit.description = "Save %s" % (model_class.__name__)        
+        audit.event_class= cls.__name__
+        audit.content_object = instance        
+        audit.save()                
+setattr(AuditEvent.objects, 'audit_save', ModelActionAudit.audit_save)
+    
+   
+
+class NavigationEventAudit(AuditEvent):
+    """
+    Audit event to track happenings within the system, ie, view access
+    """ 
+    request_path = models.TextField()
+    ip_address = models.IPAddressField()
+    
+    view = models.CharField(max_length=255) #the fully qualifid view name
+    headers = models.TextField(null=True, blank=True) #the request.META?
+    session = models.ForeignKey(Session)
+    
+    @classmethod
+    def audit_view(cls, request, user, view_func):
+        '''Creates an instance of a Access log.
+        '''
+        audit = NavigationEventAudit()
+        audit.user = request.user 
+        audit.description = "View"
+        audit.event_class= cls.__name__       
+        if len(request.GET.keys()) > 0: 
+            audit.request_path = "%s?%s" % (request.path, '&'.join(["%s=%s" % (x, request.GET[x]) for x in request.GET.keys()]))
+        else:
+            audit.request_path = request.path        
+        audit.ip_address = utils.get_ip(request)
+        audit.view = "%s.%s" % (view_func.__module__, view_func.func_name)        
+        #audit.headers = unicode(request.META) #it's a bit verbose to go to that extreme, TODO: need to have targeted fields in the META, but due to server differences, it's hard to make it universal.
+        audit.session = Session.objects.get(pk=request.session.session_key)
+        audit.save()        
+setattr(AuditEvent.objects, 'audit_view', NavigationEventAudit.audit_view)
+    
+class AccessAudit(AuditEvent):
+    ACCESS_CHOICES = (
+                      ('login', "Login"),
+                      ('logout', "Logout"),
+                      ('failed', "Failed Login"),
+                      ('password', "Password Change"),
+                      )
+    access_type = models.CharField(choices=ACCESS_CHOICES, max_length=12)
+    ip_address = models.IPAddressField()
+    session = models.ForeignKey(Session)    
+    
+    @classmethod
+    def audit_login(cls, request, user, success):
+        '''Creates an instance of a Access log.
+        '''
+        audit = AccessAudit() 
+        audit.event_class= cls.__name__
+        audit.user = user
+        audit.ip_address = utils.get_ip(request)
+        if success:
+            audit.description = "Login Success"
+        else:
+            audit.description = "Login Failure"
+             
+        audit.session = Session.objects.get(pk=request.session.session_key)
+        audit.save()
+setattr(AuditEvent.objects, 'audit_login', AccessAudit.audit_login)
+
     
 class FieldAccess(models.Model):
     object_type = models.ForeignKey(ContentType, verbose_name='Case linking content type', blank=True, null=True)
@@ -36,7 +113,7 @@ class FieldAccess(models.Model):
     class Meta:
         unique_together = ('object_type', 'field')    
 
-class ModelAccessLog(models.Model):    
+class ModelAuditEvent(models.Model):    
     id = models.CharField(_('Access guid'), max_length=32, unique=True, default=make_uuid, primary_key=True) #primary_key override?
     
     object_type = models.ForeignKey(ContentType, verbose_name='Case linking content type', blank=True, null=True)
@@ -48,6 +125,5 @@ class ModelAccessLog(models.Model):
     
     user = models.ForeignKey(User)
     accessed = models.DateTimeField(default = getdate())
-
 
 import signals
