3a88c76ab99f61fe6c47d236de1c4c2b7ae9db34
Dan Myung <dmyung@dimagi.com>
2010-09-02 18:21:33 -0400

    removing corehq.lib prefix for 3rd party apps.  removing audit log from settings.py till that can be fixed

--- management/commands/load_submissions.py
+++ management/commands/load_submissions.py
@@ -5,7 +5,7 @@ import tarfile
 from optparse import make_option
 from django.core.management.base import LabelCommand, CommandError
 from corehq.apps.receiver.management.commands.util import submit_form
-from corehq.lib.django_rest_interface import util as rest_util
+from django_rest_interface import util as rest_util
 
 class Command(LabelCommand):
     help = "Load data into CommCareHQ.\n" + \
