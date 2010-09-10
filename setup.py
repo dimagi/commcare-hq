fdb524ba77b620ed08899fd09d4e17efdbe7131f
Cory Zue <czue@dimagi.com>
2010-09-10 16:03:23 -0400

    adding setup.py installer

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Dimagi Utilities',
+      version='0.0.1',
+      description='Dimagi Shared Utilities',
+      author='Dimagi',
+      author_email='information@dimagi.com',
+      url='http://www.dimagi.com/',
+      packages=['dimagi', 'dimagi.utils'],
+     )
