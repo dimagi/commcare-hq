22ae692861690bbdce7ebccf2ad4c0c9e608dda8
Cory Zue <czue@dimagi.com>
2010-09-10 17:40:05 -0400

    updating setup.py to include python-dateutil dependency, use setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
 
-setup(name='Dimagi Utilities',
-      version='0.0.1',
-      description='Dimagi Shared Utilities',
-      author='Dimagi',
-      author_email='information@dimagi.com',
-      url='http://www.dimagi.com/',
-      packages=['dimagi', 'dimagi.utils'],
-     )
+setup(
+    name='dimagi-utils',
+    version='0.0.1',
+    description='Dimagi Shared Utilities',
+    author='Dimagi',
+    author_email='information@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages=['dimagi', 'dimagi.utils'],
+    install_requires = [
+        "python-dateutil"
+    ],
+)
