4e17d82a8253df58999cd842eb0d302603ed1178
Daniel Roberts <droberts@dimagi.com>
2010-10-20 14:17:34 -0400

    deleted "remote_apps" (because I ended up putting that functionality in app_manager)

--- corehq/apps/remote_apps/_design/views/by_domain/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) {
-    if(doc.doc_type == "RemoteApp") {
-        emit(doc.domain, doc);
-    }
-}
--- corehq/apps/remote_apps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-urlpatterns = patterns('corehq.apps.remote_apps.views',
-    (r'view/(?P<app_id>\w+)/$',   'app_view'),
-    (r'view/$',                 'view'),
-
-    (r'download/(?P<app_id>\w+)/CommCare.jad$',   'download_jad'),
-)
