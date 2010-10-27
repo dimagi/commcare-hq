0d1dd5841d41a57c7ecae6887900bd454bb9f4e2
Dan Myung <dmyung@dimagi.com>
2010-10-27 14:26:20 -0400

    updating urls to point to right view, getting export to sha1 the excel table name due to 20 character collisions on deep namespaces

--- couchexport/urls.py
+++ couchexport/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    url(r'^model/$', 'couchexport.views.download_model', name='model_download_excel'),
+    url(r'^model/$', 'couchexport.views.export_data', name='model_download_excel'),
 )
