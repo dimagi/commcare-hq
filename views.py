0e82b5808d1ef0c58bc92c6cc953603310da8a17
Rowena <rluk@dimagi.com>
2010-11-03 09:11:32 -0400

    fix indentation

--- views.py
+++ views.py
@@ -2,10 +2,10 @@ from django.http import HttpResponse
 from couchforms.views import post as couchforms_post
 
 def post(request, domain):
-     def callback(doc):
-         doc['#export_tag'] = ["domain", "xmlns"]
-         doc['submit_ip'] = request.META['REMOTE_ADDR']
-         doc['domain'] = domain
-         doc.save()
-         return HttpResponse("%s\n" % doc['_id'])
-     return couchforms_post(request, callback)
+    def callback(doc):
+        doc['#export_tag'] = ["domain", "xmlns"]
+        doc['submit_ip'] = request.META['REMOTE_ADDR']
+        doc['domain'] = domain
+        doc.save()
+        return HttpResponse("%s\n" % doc['_id'])
+    return couchforms_post(request, callback)
