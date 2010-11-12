d67dd65d6ee8242144ed6e79755bccd30ffdd027
Cory Zue <czue@dimagi.com>
2010-11-12 15:06:05 -0500

    bump minor minor version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dimagi-utils',
-    version='0.0.1',
+    version='0.0.2',
     description='Dimagi Shared Utilities',
     author='Dimagi',
     author_email='information@dimagi.com',
