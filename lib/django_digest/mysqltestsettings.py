b8ed94aeb6ecb8acda6e37d68aae7cfd4fdd8ef2
Rowena <rluk@dimagi.com>
2010-11-21 18:05:50 -0500

    add django_digest (temporarily, while figuring out httpdigest login issues)

---
+++ lib/django_digest/mysqltestsettings.py
@@ -0,0 +1,15 @@
+from django_digest.testsettings import *
+INSTALLED_APPS += ('south',)
+
+DATABASE_ENGINE = 'mysql' 
+DATABASE_STORAGE_ENGINE = 'InnoDB'
+DATABASE_HOST = 'localhost'
+DATABASE_PORT = 3306
+DATABASE_NAME = 'django_digest'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = ''
+
+try:
+    from django_digest.developer_settings import *
+except ImportError:
+    pass
