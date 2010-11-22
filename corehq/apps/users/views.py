103b6c07df9c508cc7335f1b63ae792cbb156195
Rowena <rluk@dimagi.com>
2010-11-22 18:46:36 -0500

    Add a basic user/settings landing page

--- corehq/apps/users/views.py
+++ corehq/apps/users/views.py
@@ -1,7 +1,8 @@
 from django.http import Http404
 from corehq.util.webutils import render_to_response
 
-def users(req, domain, template="users/users.html"):
+def users(req, domain, template="users/users_base.html"):
     return render_to_response(req, template, {
         'domain': domain,
     })
+
