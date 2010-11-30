c19f1ec6c82b778323df7bf28dac9c15900583c0
Rowena <rluk@dimagi.com>
2010-11-30 08:06:21 -0500

    add a url for 'add a new user'

--- corehq/apps/users/urls.py
+++ corehq/apps/users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('corehq.apps.users.views',
     (r'^$', 'users'),
     url(r'my_account$', 'my_account', name='my_account'),
     url(r'edit/(?P<user_id>[\w-]+)/?$', 'edit', name='edit_user'),
+    url(r'add/?$', 'add', name='add_user'),
 )
--- corehq/apps/users/views.py
+++ corehq/apps/users/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.contrib.auth.models import User
 from corehq.util.webutils import render_to_response
 from corehq.apps.users.forms import UserForm
 
@@ -8,28 +9,33 @@ def users(req, domain, template="users/users_base.html"):
     })
 
 def my_account(request, domain, template="users/single_user.html"):
-    return edit(request, domain, request.user.get_profile().get_couch_user().id, template)
+    return edit(request, domain, template, request.user)
 
-def edit(request, domain, user_id, template="users/single_user.html"):
+def add(request, domain, template="users/single_user.html"):
+    return edit(request, domain, template, None)
+
+def edit(request, domain, template="users/single_user.html", django_user = None):
     """
     Edit a user
     """
     context = {}
+    if django_user is None:
+        django_user = User()
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            request.user.username = form.cleaned_data['username']
-            request.user.first_name = form.cleaned_data['first_name']
-            request.user.last_name = form.cleaned_data['last_name']
-            request.user.email = form.cleaned_data['email']
-            request.user.save()
+            django_user.username = form.cleaned_data['username']
+            django_user.first_name = form.cleaned_data['first_name']
+            django_user.last_name = form.cleaned_data['last_name']
+            django_user.email = form.cleaned_data['email']
+            django_user.save()
             context['status'] = 'changes saved'
     else:
         form = UserForm()
-        form.initial['username'] = request.user.username
-        form.initial['first_name'] = request.user.first_name
-        form.initial['last_name'] = request.user.last_name
-        form.initial['email'] = request.user.email
+        form.initial['username'] = django_user.username
+        form.initial['first_name'] = django_user.first_name
+        form.initial['last_name'] = django_user.last_name
+        form.initial['email'] = django_user.email
         # TODO: password, phone numbers, commcare users
     context.update({"form": form, "domain": domain })
     return render_to_response(request, template, context)
