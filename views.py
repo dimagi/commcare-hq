91b34f165ae2d805433a3cac0b700524865723f4
Rowena <rluk@dimagi.com>
2010-12-02 15:18:54 -0500

    get receiver app to send a 'post_received' signal for things (like the user app) which want to listen for xforms which are *domain-aware*

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
+import logging
 from django.http import HttpResponse
+from dimagi.utils.logging import log_exception
 from couchforms.views import post as couchforms_post
+from corehq.apps.receiver.signals import post_received
 
 def post(request, domain):
     def callback(doc):
@@ -7,5 +10,11 @@ def post(request, domain):
         doc['submit_ip'] = request.META['REMOTE_ADDR']
         doc['domain'] = domain
         doc.save()
+        feedback = post_received.send_robust(sender="receiver", xform=doc)
+        for func, errors in feedback:
+            if errors:
+                logging.error("Receiver app: problem sending post-save signal %s for xform %s" \
+                              % (func, doc.id))
+                log_exception(errors)
         return HttpResponse("Success! Received XForm id is: %s\n" % doc['_id'])
     return couchforms_post(request, callback)
