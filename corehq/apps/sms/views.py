e75771c8ffd4534aeb4faed4f001bc65f650c503
Rowena <rluk@dimagi.com>
2010-12-27 17:09:38 -0500

    pull messaging tabs out into template partials instead of ajax calls

--- corehq/apps/sms/views.py
+++ corehq/apps/sms/views.py
@@ -5,19 +5,8 @@ from corehq.apps.users.models import CouchUser
 from corehq.util.webutils import render_to_response
 
 def messaging(request, domain, template="sms/default.html"):
-    return render_to_response(request, template, {
-        'domain': domain,
-    })
-
-def user_messaging(request, domain, template="sms/user_messaging.html"):
     phone_users = CouchUser.view("users/phone_users_by_domain", key=domain)
     return render_to_response(request, template, {
         'domain': domain,
         'phone_users': [phone_user['value'] for phone_user in phone_users]
     })
-
-def group_messaging(request, domain, template="sms/group_messaging.html"):
-    return render_to_response(request, template, {
-        'domain': domain,
-    })
-
