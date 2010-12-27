e6e2193859bd5a10f34836f90f0dd3d97ba7a965
Rowena <rluk@dimagi.com>
2010-12-27 15:38:48 -0500

    populate list of recipients

--- corehq/apps/sms/views.py
+++ corehq/apps/sms/views.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
 
+from corehq.apps.users.models import CouchUser
 from corehq.util.webutils import render_to_response
 
 def messaging(request, domain, template="sms/default.html"):
@@ -9,8 +10,10 @@ def messaging(request, domain, template="sms/default.html"):
     })
 
 def user_messaging(request, domain, template="sms/user_messaging.html"):
+    phone_users = CouchUser.view("users/phone_users_by_domain", key=domain)
     return render_to_response(request, template, {
         'domain': domain,
+        'phone_users': [phone_user['value'] for phone_user in phone_users]
     })
 
 def group_messaging(request, domain, template="sms/group_messaging.html"):
