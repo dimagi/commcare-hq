6d556f81625d6d055b456765aa24543d73b28a0f
Rowena <rluk@dimagi.com>
2010-12-27 14:37:46 -0500

    added a view for all phone users

---
+++ corehq/apps/users/_design/views/all_phone_users/map.js
@@ -0,0 +1,31 @@
+function(doc){ 
+    if (doc.django_type == "users.hquserprofile" && doc.phone_numbers != null && doc.phone_numbers.length > 0)
+        var name = '';
+        var phone_number = '';
+        for (var i=0;i<doc.phone_numbers.length;i++)
+        {
+            if (doc.phone_numbers[i].is_default == true){
+                phone_number = doc.phone_numbers[i].number;
+                break;
+            }
+        }
+        if (phone_number==''){
+            // default to the last number added;
+            phone_number = doc.phone_numbers[doc.phone_numbers.length-1].number;
+        }
+        if (doc.django_user.username != null && doc.django_user.username.length > 0)
+        {
+            name = doc.django_user.username
+        }
+        else 
+        {
+            for (var i=0;i<doc.commcare_accounts.length;i++)
+            {
+                if (doc.commcare_accounts[i].django_user.username.length > 0){
+                    name = doc.commcare_accounts[i].django_user.username
+                    break
+                }
+            } 
+        }
+        emit(doc._id,[name, phone_number])
+}
