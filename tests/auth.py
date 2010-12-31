e51bd5a198264591d32fcbdedb464e61298551f1
Dan Myung <dmyung@dimagi.com>
2010-12-31 16:00:50 -0500

    initial checkin of auditor code.  This is destabilizing, but needing to update the initial changes before pulling auditor out of the corehq into its own repo

--- tests/auth.py
+++ tests/auth.py
@@ -6,12 +6,40 @@ import settings
 from corehq.util.threadlocals import _thread_locals
 
 
+def delete_all(couchmodel, view_name, key=None, startkey=None, endkey=None):
+    """Helper function to help delete/clear documents from the database of a certain type.
+    Will call the view function opon a given couchdbkit model you specify (couchmodel), on the given view.  It will do an include_docs on the view request
+    to get the entire document, it must return the actual couchmodel instances for the view for this to work.
+
+    After that, it'll iterate through all the elements to delete the items in the resultset.
+    """
+    params = {}
+    if key != None:
+        params['key'] = key
+    if startkey != None and endkey != None:
+        params['startkey'] = startkey
+        params['endkey'] = endkey
+    params['include_docs'] = True
+    data = couchmodel.view(view_name, **params).all()
+    total_rows = len(data)
+
+    for dat in data:
+        try:
+            dat.delete()
+        except:
+            pass
+    return total_rows
+
+
+
+
+
 class authenticationTestCase(unittest.TestCase):
     def setUp(self):
         if hasattr(_thread_locals, 'user'):
             delattr(_thread_locals, 'user')
         User.objects.all().delete()
-        AuditEvent.objects.all().delete()
+        delete_all(AuditEvent, 'auditor/all_events')
         self.client = Client()
         self._createUser()
             
