a6c34e536b4941a46e6f8818726a714c00cd6298
Cory Zue <czue@dimagi.com>
2011-01-04 16:22:28 -0500

    add bootstrap management command

---
+++ corehq/apps/domain/management/commands/bootstrap.py
@@ -0,0 +1,17 @@
+from django.core.management.base import LabelCommand, CommandError
+from corehq.apps.domain.shortcuts import create_domain, create_user,\
+    add_user_to_domain
+
+class Command(LabelCommand):
+    help = "Bootstrap a domain and user who owns it."
+    args = "<domain> <user> <pass>"
+    label = ""
+     
+    def handle(self, *args, **options):
+        if len(args) != 3:
+            raise CommandError('Usage: manage.py submit_forms <domain> <user> <password>')
+        domain_name, username, passwd = args
+        domain = create_domain(domain_name)
+        user = create_user(username, passwd)
+        add_user_to_domain(user, domain)
+        
