1eb36dd1ecd9b43fdfdf11ec05d91f93cfb1dffb
Cory Zue <czue@dimagi.com>
2011-01-05 17:59:19 -0500

    initial checkin of user refactor stuff.  kind of a messy state of affairs. attempting to standardize how we store django user credentials in couch and do linking just by ids in the couch user objects.

--- signals.py
+++ signals.py
@@ -1,3 +1,25 @@
 from django.dispatch import Signal
 
+
+class ReceiverResult(object):
+    """
+    This class should be used to return from signals if
+    you want to communicate something back to receiver.
+    The most common (okay, only current) scenario for this
+    is overriding the response to the user based on an 
+    application's specific logic.
+    """ 
+    def __init__(self, response):
+        self._response = response
+    
+    @property
+    def response(self):
+        """
+        The response to return to the user.
+        """ 
+        return self._response
+    
+    def __str__(self):
+        return self.response
+    
 post_received = Signal(providing_args=["posted"])
