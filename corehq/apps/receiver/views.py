da862c4c7fb47e2ffd73d8c01ddb31346abe0693
Cory Zue <czue@dimagi.com>
2011-01-06 11:06:39 -0500

    properly wire the receiver response so it goes back to the phone

--- corehq/apps/receiver/views.py
+++ corehq/apps/receiver/views.py
@@ -1,8 +1,7 @@
 import logging
 from django.http import HttpResponse
-from dimagi.utils.logging import log_exception
 from couchforms.views import post as couchforms_post
-from corehq.apps.receiver.signals import post_received
+from corehq.apps.receiver.signals import post_received, ReceiverResult
 
 def post(request, domain):
     def callback(doc):
@@ -11,10 +10,17 @@ def post(request, domain):
         doc['domain'] = domain
         doc.save()
         feedback = post_received.send_robust(sender="receiver", xform=doc)
-        for func, errors in feedback:
-            if errors:
+        to_respond = None
+        for func, resp in feedback:
+            if resp and isinstance(resp, Exception):
                 logging.error("Receiver app: problem sending post-save signal %s for xform %s" \
                               % (func, doc._id))
-                log_exception(errors)
+                logging.exception(resp)
+            elif resp and not to_respond and isinstance(resp, ReceiverResult):
+                # use the first valid response if we get one 
+                to_respond = resp
+        if to_respond:
+            return HttpResponse(to_respond.response)
+        # default to something generic
         return HttpResponse("Success! Received XForm id is: %s\n" % doc['_id'])
     return couchforms_post(request, callback)
