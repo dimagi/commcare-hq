c0027a0120f623766005a213b9394ca216d5e350
Cory Zue <czue@dimagi.com>
2011-01-10 17:13:43 -0500

    fix the couch views to work with non-bhoma forms

--- couchforms/_design/views/by_user/map.js
+++ couchforms/_design/views/by_user/map.js
@@ -1,28 +1,10 @@
 function(doc) { 
-    // parse a date in yyyy-mm-dd format
-    function parse_date(date_string) {
-        if (!date_string) return new Date(1970,1,1);
-        // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse    
-        var parts = date_string.match(/(\d+)/g);
-        // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-        return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
-    }
-     
-    function get_date(xform_doc) {
-        function get_date_string(xform_doc) {
-            // check some expected places for a date
-            var meta = xform_doc.form.meta;
-            if (xform_doc.form.encounter_date) return xform_doc.form.encounter_date;
-            if (meta && meta.TimeEnd) return meta.TimeEnd;
-            if (meta && meta.TimeStart) return meta.TimeStart;
-            return null;
-        }
-        return parse_date(get_date_string(xform_doc));
-    }
+    // these lines magically import our other javascript files.  DON'T REMOVE THEM!
+    // !code util/dates.js
     
     function get_user_id(xform_doc) {
         var meta = xform_doc.form.meta;
-        if (meta) return meta.user_id;
+        if (meta) return meta.userID;
     }
     if (doc.doc_type == "XFormInstance" && get_user_id(doc) != null) {
         date = get_date(doc);
--- couchforms/_design/views/counts_by_type/map.js
+++ couchforms/_design/views/counts_by_type/map.js
@@ -1,30 +1,12 @@
 function(doc) { 
-    // parse a date in yyyy-mm-dd format
-	function parse_date(date_string) {
-	    if (!date_string) return new Date(1970,1,1);
-	    // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse    
-	    var parts = date_string.match(/(\d+)/g);
-	    // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-	    return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
-	}
-     
-    function get_encounter_date(xform_doc) {
-        function get_date_string(xform_doc) {
-            // check some expected places for a date
-            var meta = xform_doc.form.meta;
-            if (xform_doc.form.encounter_date) return xform_doc.form.encounter_date;
-            if (meta && meta.TimeEnd) return meta.TimeEnd;
-            if (meta && meta.TimeStart) return meta.TimeStart;
-            return null;
-        }
-        return parse_date(get_date_string(xform_doc));
-    }
+    // these lines magically import our other javascript files.  DON'T REMOVE THEM!
+    // !code util/dates.js
     
     if (doc["doc_type"] == "XFormInstance") {
-        date = get_encounter_date(doc);
+        date = get_date(doc);
         if (!date) {
             date = Date();
         }
-        emit([doc.xmlns, doc.form.meta.clinic_id, date.getFullYear(), date.getMonth(), date.getDate()], 1);
+        emit([doc.xmlns, date.getFullYear(), date.getMonth(), date.getDate()], 1);
     } 
 }
