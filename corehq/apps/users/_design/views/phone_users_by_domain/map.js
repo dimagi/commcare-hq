f02776e843c5b26c7dce3ac7ad9a8fdba82d3a34
Rowena <rluk@dimagi.com>
2011-01-12 20:04:51 -0800

    fix the phone_users_by_domain view to match the user refactor. a *bunch* of the user views are broken right now.

--- corehq/apps/users/_design/views/phone_users_by_domain/map.js
+++ corehq/apps/users/_design/views/phone_users_by_domain/map.js
@@ -1,42 +1,35 @@
 function(doc){ 
-    if (doc.django_type == "users.hquserprofile" && doc.phone_numbers != null && doc.phone_numbers.length > 0)
+    if (doc.doc_type == "CouchUser" && doc.phone_numbers != null && doc.phone_numbers.length > 0)
         var name = '';
         var phone_number = '';
         // get default phone number, if set
-        for (var i=0;i<doc.phone_numbers.length;i++)
-        {
-            if (doc.phone_numbers[i].is_default == true){
-                phone_number = doc.phone_numbers[i].number;
-                break;
-            }
-        }
-        if (phone_number==''){
-            // if default not set, use last number added;
-            phone_number = doc.phone_numbers[doc.phone_numbers.length-1].number;
-        }
+        phone_number = doc.phone_numbers[0];
         // get username from web user account
-        if (doc.django_user_id != null)
+        if (doc.web_account != null)
         {
-            name = doc.django_user_id
+            django_id = doc.web_account.login_id
+        	for (var i=0;i<doc.web_account.domain_memberships.length;i++)
+        	{
+                emit(doc.web_account.domain_memberships[i].domain,  {
+                    django_id: django_id,
+                    phone_number: phone_number,
+                    id: doc['_id']
+                });
+        	}
         }
         else 
         {
-        	// if no web user, use the latest commcare account username
-           for (var i=doc.commcare_accounts.length-1;i>=0;i--)
-            {
-                if (doc.commcare_accounts[i].django_user_id != null){
-                    name = doc.commcare_accounts[i].django_user_id
-                    break
-                }
-            } 
+        	// if no web user, use the latest commcare account
+        	if (doc.commcare_accounts != null && doc.commcare_accounts.length > 0){
+        		django_id = doc.commcare_accounts[0].django_user_id
+            	for (var i=0;i<doc.commcare_accounts[0].domain_memberships.length;i++)
+            	{
+                    emit(doc.commcare_accounts[0].domain_memberships[i].domain,  {
+                        django_id: django_id,
+                        phone_number: phone_number,
+                        id: doc['_id']
+                    });
+            	}
+        	}
         }
-        // if no usernames found, return name as '' 
-    	for (var i=0;i<doc.domain_memberships.length;i++)
-    	{
-            emit(doc.domain_memberships[i].domain,  {
-                name: name,
-                phone_number: phone_number,
-                id: doc['_id']
-            });
-    	}
 }
