2097819cd47a288c0418ec4f4d077f6727d55683
Dan Myung <dmyung@dimagi.com>
2011-02-09 14:27:13 -0500

    Updating couchforms to better handle variable submission methods

---
+++ couchforms/management/commands/submit_form.py
@@ -0,0 +1,44 @@
+from django.core.management.base import CommandError, BaseCommand
+from optparse import make_option
+import os
+from dimagi.utils.post import post_file
+
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+    #       make_option('--file', action='store', dest='file', default=None, help='File to upload REQUIRED', type='string'),
+    #       make_option('--url', action='store', dest='url', default=None, help='URL to upload to*', type='string'),
+    make_option('--method', action='store',
+                dest='method', default='curl',
+                help='Method to upload (curl, pyton), defaults to curl', type='string'),
+    make_option('--chunked', action='store_true', dest='use_chunked', default=False,
+                help='Use chunked encoding (default=False)'),
+    make_option('--odk', action='store_true', dest='is_odk', default=False,
+                help='Simulate ODK submission (default=False)'),
+    )
+    help = "Submits a single form to a url with options."
+    args = '[<filename> <url>]'#"[--file <filename> --url <url> [optional --method {curl | python} --chunked --odk]]"
+    label = "Submit a single form with various options"
+
+    def handle(self, *args, **options):
+        print os.path.abspath(os.path.curdir)
+        verbosity = int(options.get('verbosity', 1))
+        if len(args) != 2:
+            raise CommandError('Usage is submit_form %s' % self.args)
+        file = args[0]
+        url = args[1]
+        method = options.get('method', 'curl')
+        use_chunked = options.get('use_chunked', False)
+        is_odk = options.get('is_odk', False)
+        if file == None or url == None:
+            raise CommandError('Usage is submit_form %s' % self.args)
+        if not os.path.exists(file):
+            raise CommandError("File does not exist")
+
+        if method == 'curl':
+            use_curl = True
+        elif method == 'python':
+            use_curl = False
+
+        print post_file(file, url, use_curl=use_curl, use_chunked=use_chunked, is_odk=is_odk)
+
--- couchforms/views.py
+++ couchforms/views.py
@@ -1,9 +1,11 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from couchforms.util import post_xform_to_couch
 from django.http import HttpResponse, HttpResponseServerError
 import logging
 
 @require_POST
+@csrf_exempt
 def post(request, callback=None):
     """
     XForms can get posted here.  They will be forwarded to couch.
@@ -16,7 +18,15 @@ def post(request, callback=None):
     # just forward the post request to couch
     # this won't currently work with ODK
     # post to couch
-    instance = request.raw_post_data
+    if request.META['CONTENT_TYPE'].startswith('multipart/form-data'):
+        #it's an standard form submission (eg ODK)
+        #this does an assumption that ODK submissions submit using the form parameter xml_submission_file
+        #todo: this should be made more flexibly
+        instance = request.FILES['xml_submission_file'].read()
+    else:
+        #else, this is a raw post via a j2me client of xml.
+        #todo, multipart raw submissions need further parsing capacity.
+        instance = request.raw_post_data
     try:
         doc = post_xform_to_couch(instance)
         if callback:
