86f4845e79c9868d43d632be6ac7335e57461af4
Daniel Roberts <droberts@dimagi.com>
2011-02-11 09:57:44 -0500

    fixed url slashes

--- corehq/apps/receiver/urls.py
+++ corehq/apps/receiver/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('corehq.apps.receiver.views',
     url(r'^$', 'post', name='receiver_post'),
-    url(r'^/submission$',  'post', name="receiver_odk_post"), 
+    url(r'^submission/$',  'post', name="receiver_odk_post"),
 
 )
