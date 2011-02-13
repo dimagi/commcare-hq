e9fa2def09071752d4fd1a1944d5be3e0d9ffc75
Cory Zue <czue@dimagi.com>
2011-02-13 17:00:17 -0500

    removing more extraneous unused things from corehq

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -15,7 +15,6 @@ from django.utils.http import urlencode
 
 from django.views.decorators.http import require_POST
 from django.conf import settings
-from corehq.util.xforms import readable_form
 from dimagi.utils.web import get_url_base
 from BeautifulSoup import BeautifulStoneSoup
 from lxml import etree as ET
@@ -176,7 +175,6 @@ def _apps_context(req, domain, app_id='', module_id='', form_id=''):
         pass
     if xform:
         xform_contents = form.contents
-        #xform_contents, err, has_err = readable_form(xform_contents)
 
     if app:
         saved_apps = [x['value'] for x in get_db().view('app_manager/saved_app',
