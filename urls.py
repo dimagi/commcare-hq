269f0098fe6794db33976934f130ce7f276d812a
Cory Zue <czue@dimagi.com>
2011-02-14 14:31:31 -0500

    support slash and slashless posts to receiver

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('corehq.apps.receiver.views',
-    url(r'^$', 'post', name='receiver_post'),
-    url(r'^submission/$',  'post', name="receiver_odk_post"),
+    url(r'^$', 'post/?$', name='receiver_post'),
+    url(r'^submission/?$',  'post', name="receiver_odk_post"),
 
 )
