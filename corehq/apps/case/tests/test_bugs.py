5426876589df8db670e947f68b849dca4cdad06e
Cory Zue <czue@dimagi.com>
2011-02-17 17:50:11 -0500

    Update reciever to process failures properly and send feedback to the phone
    Also fixes the bug where a conflicting case id and form id caused it to
    fail hard

---
+++ corehq/apps/case/tests/test_bugs.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+import os
+from corehq.apps.case.models.couch import CommCareCase
+from couchforms.util import post_xform_to_couch
+
+class CaseBugTest(TestCase):
+    """
+    Tests bugs that come up in case processing
+    """
+    
+    def setUp(self):
+        for item in CommCareCase.view("case/by_xform_id", include_docs=True).all():
+            item.delete()
+        
+    def testConflictingIds(self):
+        """
+        If two forms share an ID it's a conflict
+        """
+        self.assertEqual(0, len(CommCareCase.view("case/by_xform_id").all()))
+        file_path = os.path.join(os.path.dirname(__file__), "data", "bugs", "id_conflicts.xml")
+        with open(file_path, "rb") as f:
+            xml_data = f.read()
+        form = post_xform_to_couch(xml_data)
+        self.assertEqual("XFormError", form.doc_type)
+        
+    
