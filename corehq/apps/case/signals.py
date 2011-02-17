aa6e64eaf11b42e28a1db1948f209cb866f0b4c6
Cory Zue <czue@dimagi.com>
2011-02-17 15:09:07 -0500

    add multi-case support+test

--- corehq/apps/case/signals.py
+++ corehq/apps/case/signals.py
@@ -1,11 +1,16 @@
 from couchforms.signals import xform_saved
+import logging
 
 def process_cases(sender, xform, **kwargs):
     """Creates or updates case objects which live outside of the form"""
     # recursive import fail
     from corehq.apps.case.xform import get_or_update_cases
-    cases = get_or_update_cases(xform)
-    map(lambda pair: pair[1].save(), cases.items())
+    try:
+        cases = get_or_update_cases(xform)
+        map(lambda pair: pair[1].save(), cases.items())
+    except Exception, e:
+        logging.exception(e)
+        raise
     
 # demo for showing how this can be done
 # set to True or just remove check to do this always
