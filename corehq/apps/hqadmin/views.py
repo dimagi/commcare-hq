d7970ab28941d5d397cc694bfdaaa4c4381e281a
Cory Zue <czue@dimagi.com>
2011-02-18 14:37:41 -0500

    clean up admin domain list report

--- corehq/apps/hqadmin/views.py
+++ corehq/apps/hqadmin/views.py
@@ -6,6 +6,7 @@ from django.template.context import RequestContext
 from corehq.apps.domain.models import Domain
 from dimagi.utils.couch.database import get_db
 from collections import defaultdict
+from corehq.apps.domain.decorators import login_and_domain_required
 
 @permission_required("is_superuser")
 def default(request):
@@ -27,6 +28,7 @@ def domain_list(request):
         dom.forms = form_counts[dom.name]
         dom.admins = [row["doc"]["email"] for row in get_db().view("users/admins_by_domain", key=dom.name, reduce=False, include_docs=True).all()]
     return render_to_response("hqadmin/domain_list.html", 
-                              {"domains": domains},
+                              {"domains": domains,
+                               "domain": request.user.selected_domain.name},
                               context_instance=RequestContext(request))
     
