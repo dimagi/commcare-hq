a5ee17b1a74618e519edfd9d9667c687a180f79d
Cory Zue <czue@dimagi.com>
2011-02-22 09:41:35 -0500

    fix receiver urls

--- corehq/apps/receiver/urls.py
+++ corehq/apps/receiver/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('corehq.apps.receiver.views',
     url(r'^/?$', 'post', name='receiver_post'),
     # odk urls
-    url(r'^submission/?$',  'post', name="receiver_odk_post"),
-    url(r'^formList/$', 'form_list', name='form_list'),
+    url(r'^/submission/?$',  'post', name="receiver_odk_post"),
+    url(r'^/formList/$', 'form_list', name='form_list'),
 
 )
