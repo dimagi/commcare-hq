6ed04cc34774d3ed4f5116b09f308f792421c9c9
Christian Ternus <cternus@dimagi.com>
2011-02-24 17:11:39 -0500

    Audit login and logout.

--- auditcare/signals.py
+++ auditcare/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_init, post_save, pre_save, pre_delete
 from couchdbkit.ext.django.schema import Document
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.db.models import Q
 import logging
 from datetime import datetime
@@ -15,6 +16,11 @@ except:
     from auditcare.utils import get_current_user
 
 from django.db import transaction
+from models import *
+from django.dispatch import Signal
+
+user_login_failed = Signal(providing_args=['request', 'username'])
+
 
 json_serializer = serializers.get_serializer("json")()
 
