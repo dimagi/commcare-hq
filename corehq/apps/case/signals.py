1fd9c5db982b081b09d069cc0583977ea1689913
Cory Zue <czue@dimagi.com>
2011-02-27 11:09:37 -0500

    take 2 on fixing case processing

--- corehq/apps/case/signals.py
+++ corehq/apps/case/signals.py
@@ -4,14 +4,14 @@ def process_cases(sender, xform, **kwargs):
     """Creates or updates case objects which live outside of the form"""
     # recursive import fail
     from corehq.apps.case.xform import get_or_update_cases
-    cases = get_or_update_cases(xform)
+    cases = get_or_update_cases(xform).values()
     # attach domain if it's there
     if hasattr(xform, "domain"):
         domain = xform.domain
         def attach_domain(case):
             case.domain = domain
             return case
-        cases = [attach_domain(case) for case in cases.values()]
-    map(lambda case: case.save(), cases.values())
+        cases = [attach_domain(case) for case in cases]
+    map(lambda case: case.save(), cases)
     
 successful_form_received.connect(process_cases)
