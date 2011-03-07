fc6cf7282f3051f78bd356953eb8b13a3310ddf5
Christian Ternus <cternus@dimagi.com>
2011-03-07 17:09:59 -0500

    audit_login view that checks failed passwords

--- auditcare/views.py
+++ auditcare/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
-
+from django.contrib.auth import authenticate
+from django.contrib.auth.views import login as django_login
+from signals import *
 from models import *
 import datetime
 
@@ -18,3 +20,12 @@ def auditAll(request, template="auditcare/index.html"):
                                       second=a["key"][8])} for a in auditEvents]
     context['auditEvents'] = realEvents
     return render_to_response(template, context)
+
+def audit_login(request, template_name, *args, **kwargs):
+    username=request.POST.get('username')
+    u = authenticate(username=username,password=request.POST.get('password'))
+    if u is None or not u.is_active:
+        user_login_failed.send(sender=None,request=request,username=username) # This is new
+    return django_login(request, *args, **kwargs)
+
+#django.contrib.auth.views.login = audit_login
