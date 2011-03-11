3b3b44dd834f403a0bd65d0832fb9257f5182e30
Christian Ternus <cternus@dimagi.com>
2011-03-11 14:44:24 -0500

    fix auditAll view

--- auditcare/models/_design/util/shared_funcs.js
+++ auditcare/models/_design/util/shared_funcs.js
@@ -7,6 +7,6 @@ function parse_date(date_string) {
             // hat tip: http://stackoverflow.com/questions/2587345/javascript-date-parse
             var parts = date_string.match(/(\d+)/g);
             // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-            return new Date(parts[0], parts[1]-1, parts[2]); // months are 0-based
+            return new Date(parts[0], parts[1]-1, parts[2], parts[3], parts[4], parts[5]); // months are 0-based
         }
 }
--- auditcare/views.py
+++ auditcare/views.py
@@ -1,31 +1,35 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.contrib.auth import authenticate
-from django.contrib.auth.views import login as django_login
+from django.contrib.auth.views import login as django_login, logout as django_logout
 from signals import *
 from models import *
 import datetime
 
 
 def auditAll(request, template="auditcare/index.html"):
-    auditEvents = couchmodels.AuditEvent.view("auditcare/by_date").all()
+    auditEvents = couchmodels.AuditEvent.view("auditcare/by_user_date").all()
     context = RequestContext(request)
 
-    realEvents = [{"user":a["key"][0], "type":a["key"][1], "path":a["key"][2], "date":
-                    datetime.datetime(year=int(a["key"][3]),
-                                      month=int(a["key"][4]),
-                                      day=int(a["key"][5]),
-                                      hour=a["key"][6],
-                                      minute=a["key"][7],
-                                      second=a["key"][8])} for a in auditEvents]
+    realEvents = [{"user":a["key"][0], "path":a["value"], "date":
+                    datetime.datetime(year=int(a["key"][1]),
+                                      month=int(a["key"][2]),
+                                      day=int(a["key"][3]),
+                                      hour=a["key"][4],
+                                      minute=a["key"][5],
+                                      second=a["key"][6])} for a in auditEvents]
     context['auditEvents'] = realEvents
     return render_to_response(template, context)
 
-def audit_login(request, template_name, *args, **kwargs):
+def audit_login_view(request, template_name, *args, **kwargs):
     username=request.POST.get('username')
     u = authenticate(username=username,password=request.POST.get('password'))
     if u is None or not u.is_active:
         user_login_failed.send(sender=None,request=request,username=username) # This is new
+    elif u.is_active:
+        pass
     return django_login(request, *args, **kwargs)
 
-#django.contrib.auth.views.login = audit_login
+def audit_logout_view(request, *args, **kwargs):
+
+    return django_login(request, *args, **kwargs)
