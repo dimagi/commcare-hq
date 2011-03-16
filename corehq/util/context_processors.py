9b1a7d0e02d46f32a03ed8dd3bfb5ffbe6b93378
Daniel Roberts <droberts@dimagi.com>
2011-03-16 11:09:11 -0400

    added google analytics support

--- corehq/util/context_processors.py
+++ corehq/util/context_processors.py
@@ -6,3 +6,6 @@ def base_template( request ):
        into the request context, so that we don't have to do it in 
        our render_to_response override."""    
     return {"base_template" : settings.BASE_TEMPLATE, 'login_template': settings.LOGIN_TEMPLATE}
+
+def google_analytics(request):
+    return {"GOOGLE_ANALYTICS_ID": settings.GOOGLE_ANALYTICS_ID}
