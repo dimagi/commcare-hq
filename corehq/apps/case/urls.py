6520dfe29b130fd2fc953a7fc96bfeff6ff0f90a
Daniel Roberts <droberts@dimagi.com>
2011-03-30 15:53:16 -0400

    Created basic browser for cases; still unexposed

--- corehq/apps/case/urls.py
+++ corehq/apps/case/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('',
+urlpatterns = patterns('corehq.apps.case.views',
     # stub urls
+    (r'open_cases.json', 'open_cases_json'),
+    (r'open_cases', 'open_cases'),
 )
