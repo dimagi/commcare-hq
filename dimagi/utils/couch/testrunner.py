599f17d56f570d31c4459198087e75a339ac5986
Dan Myung <dmyung@dimagi.com>
2011-03-31 15:51:18 -0400

    updating dimagi utils to use a bhoma-style settingshelper as well as being reliant upon the couchdbkit's test runner.  Also retaining the xmltest runner output option via subclassing the subclassed testrunner.

--- dimagi/utils/couch/testrunner.py
+++ dimagi/utils/couch/testrunner.py
@@ -1,10 +1,11 @@
 import sys
-from django.test.simple import DjangoTestSuiteRunner
 from django.conf import settings
 from couchdbkit.ext.django import loading as loading
+from couchdbkit.ext.django.testrunner import CouchDbKitTestSuiteRunner
 from couchdbkit.resource import ResourceNotFound
+import settingshelper
 
-class CouchDbKitTestSuiteRunner(DjangoTestSuiteRunner):
+class DimagiCouchTestSuiteRunner(CouchDbKitTestSuiteRunner):
     """
     A test suite runner for couchdbkit.  This offers the exact same functionality
     as the default django test suite runner, except that it connects all the couchdbkit
@@ -12,76 +13,33 @@ class CouchDbKitTestSuiteRunner(DjangoTestSuiteRunner):
     end of the tests.  To use this, just add this file to your project and the following 
     line to your settings.py file:
     
-    TEST_RUNNER = 'myproject.testrunner.CouchDbKitTestSuiteRunner'
+    TEST_RUNNER = 'myproject.testrunner.DimagiCouchTestSuiteRunner'
     """
-    
     dbs = []
-    
-    def get_test_db_name(self, dbname):
-        return "%s_test" % dbname
-        
+
     def setup_databases(self, **kwargs):
-        print "overridding the couchdbkit database settings to use a test database!"
-                 
-        # first pass: just implement this as a monkey-patch to the loading module
-        # overriding all the existing couchdb settings
-        self.dbs = [(app, self.get_test_db_name(url)) for app, url in getattr(settings, "COUCHDB_DATABASES", [])]
-        settings.COUCH_DATABASE = self.get_test_db_name(settings.COUCH_DATABASE)
-        old_handler = loading.couchdbkit_handler
-        couchdbkit_handler = loading.CouchdbkitHandler(self.dbs)
-        loading.couchdbkit_handler = couchdbkit_handler
-        loading.register_schema = couchdbkit_handler.register_schema
-        loading.get_schema = couchdbkit_handler.get_schema
-        loading.get_db = couchdbkit_handler.get_db
+        returnval = super(DimagiCouchTestSuiteRunner, self).setup_databases(**kwargs)
+        self.newdbname = self.get_test_db_name(settings.COUCH_DATABASE_NAME)
+        print "overridding the couch settings!"
+        new_db_settings = settingshelper.get_dynamic_db_settings(settings.COUCH_SERVER_ROOT,
+                                                                 settings.COUCH_USERNAME,
+                                                                 settings.COUCH_PASSWORD,
+                                                                 self.newdbname,
+                                                                 settings.INSTALLED_APPS)
+        settings.COUCH_DATABASE_NAME = self.newdbname
+        for (setting, value) in new_db_settings.items():
+            setattr(settings, setting, value)
+            print "set %s settting to %s" % (setting, value)
+        # hack - set the other values too
+        return returnval
 
-        #if couchforms exists, monkey patch the XFORMS_POST_URL
-        for app, url in self.dbs:
-            if app == "couchforms":
-                settings.XFORMS_POST_URL = "%s/_design/couchforms/_update/xform/" % (url)
-                break
-
-        # register our dbs with the extension document classes
-        for app, value in old_handler.app_schema.items():
-            for name, cls in value.items():
-                cls.set_db(loading.get_db(app))
-                                
-                
-        return super(CouchDbKitTestSuiteRunner, self).setup_databases(**kwargs)
-    
-    def teardown_databases(self, old_config, **kwargs):
-        deleted_databases = []
-        skipcount = 0
-        for app, item in self.dbs:
-            app_label = app.split('.')[-1]
-            db = loading.get_db(app_label)
-            if db.dbname in deleted_databases: 
-                skipcount += 1
-                continue
-            try:
-                db.server.delete_db(db.dbname)
-                deleted_databases.append(db.dbname)
-                print "deleted database %s for %s" % (db.dbname, app_label)
-            except ResourceNotFound:
-                print "database %s not found for %s! it was probably already deleted." % (db.dbname, app_label)
-        if skipcount:
-            print "skipped deleting %s app databases that were already deleted" % skipcount
-        return super(CouchDbKitTestSuiteRunner, self).teardown_databases(old_config, **kwargs)
 
 try:
-
     import xmlrunner
     from django.test.simple import *
-    class CouchDbKitXMLTestSuiteRunner(CouchDbKitTestSuiteRunner):
-        def __init__(self, *args, **kwargs):
-            CouchDbKitTestSuiteRunner.__init__(self)
-
-        def setup_databases(self, **kwargs):
-            return CouchDbKitTestSuiteRunner.setup_databases(self, **kwargs)
-            #return super(CouchDbKitXMLTestSuiteRunner, self).setup_databases(**kwargs)
-        def teardown_databases(self, old_config, **kwargs):
-            #return super(CouchDbKitXMLTestSuiteRunner, self).teardown_databases(old_config, **kwargs)
-            return CouchDbKitTestSuiteRunner.teardown_databases(self, old_config, **kwargs)
-
+    class DimagiCouchXMLTestSuiteRunner(DimagiCouchTestSuiteRunner):
+        """XML Runner for running this in a build server with pretty xml report output
+        """
         def run_tests(self, test_labels, verbosity=1, interactive=True, extra_tests=[]):
             """
             adapted from xmlrunner.extra.djangotestrunner.run_tests
