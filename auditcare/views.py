8d60063657024e1b333cef5438e5c534555bbd27
Rowena <rluk@dimagi.com>
2011-05-04 14:52:27 -0400

    small fix to previous

--- auditcare/views.py
+++ auditcare/views.py
@@ -10,7 +10,7 @@ def auditAll(request, template="auditcare/index.html"):
     auditEvents = couchmodels.AccessAudit.view("auditcare/by_date_access_events", descending=True, include_docs=True).all()
     context = RequestContext(request)
 
-    realEvents = [{'user': a.user, 'date': a.event_date, 'type': a.doc_type} for a in auditEvents]
+    realEvents = [{'user': a.user, 'date': a.event_date, 'class': a.event_class, 'access_type': a.access_type } for a in auditEvents]
 
 #    realEvents = [{"user":a["key"][0], "path":a["value"], "date":
 #                    datetime.datetime(year=int(a["key"][1]),
