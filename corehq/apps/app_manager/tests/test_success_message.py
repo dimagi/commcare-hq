fdeb98e68d2cad493fe300ae20c6ff95df476f40
Daniel Roberts <droberts@dimagi.com>
2011-05-05 17:36:13 -0400

    complete and correct, minus exposing timezone support in UI

--- corehq/apps/app_manager/tests/test_success_message.py
+++ corehq/apps/app_manager/tests/test_success_message.py
@@ -1,7 +1,9 @@
 from StringIO import StringIO
+import re
 from django.test.client import Client
 from corehq.apps.app_manager.models import Application
 from corehq.apps.app_manager.util import SuccessMessage, format_time
+from corehq.apps.phone.xml import get_response
 from corehq.apps.users.models import create_hq_user_from_commcare_registration_info
 from lib.django_digest.tests import TestCase
 from datetime import datetime, timedelta
@@ -25,7 +27,7 @@ class SuccessMessageTest(TestCase):
     last_name = "Roberts"
     password = "123"
     xmlns = "http://dimagi.com/does_not_matter"
-    tz = timedelta(hours=-4)
+    tz = timedelta(hours=0)
     def setUp(self):
         couch_user = create_hq_user_from_commcare_registration_info(self.domain, self.username, self.password)
         userID = couch_user.default_commcare_account.login_id
@@ -55,7 +57,7 @@ class SuccessMessageTest(TestCase):
             response = c.post("/a/{self.domain}/receiver/".format(self=self), {
                 'xml_submission_file': f,
             }, **kwargs)
-            print response
+            return response
 
 
         self.num_forms_today = 0
@@ -65,13 +67,20 @@ class SuccessMessageTest(TestCase):
         week_start = tznow - timedelta(days=tznow.weekday())
         week_start = datetime(week_start.year, week_start.month, week_start.day) - self.tz
         day_start = datetime(tznow.year, tznow.month, tznow.day) - self.tz
-        for h in xrange(4*8):
-            time = now-timedelta(hours=6*h)
-            fake_form_submission(time=time)
+        spacing = 6
+        for h in xrange((24/spacing)*8):
+            time = now-timedelta(hours=spacing*h)
+            response = fake_form_submission(time=time)
             if time > week_start:
                 self.num_forms_this_week += 1
             if time > day_start:
                 self.num_forms_today += 1
+            self.failUnlessEqual(
+                response.content,
+                get_response(("Thanks {self.first_name} ({self.first_name} {self.last_name})! "
+                "You have submitted {self.num_forms_today} forms today "
+                "and {self.num_forms_this_week} forms since Monday.").format(self=self))
+            )
             
     def testRender(self):
         self.failUnlessEqual(
