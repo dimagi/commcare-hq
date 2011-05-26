e353ed8c7c507d23c7685366cb394af329dff3a8
Cory Zue <czue@dimagi.com>
2011-05-26 12:17:06 -0400

    fix subtle bug where same dict was getting passed to mutiple report configs

--- corehq/apps/reports/schedule/__init__.py
+++ corehq/apps/reports/schedule/__init__.py
@@ -9,10 +9,14 @@ class ReportSchedule(object):
     A basic report scedule, fully customizable, but requiring you to 
     understand exactly what to pass to the view at runtime.
     """
-    def __init__(self, view_func, view_args={}, title="unspecified", 
+    
+    def __init__(self, view_func, view_args=None, title="unspecified", 
                  processor=RequestProcessor(), auth=None):
         self._view_func = view_func
-        self._view_args = view_args
+        if view_args is not None:
+            self._view_args = view_args
+        else: 
+            self._view_args = {}
         self._processor = processor
         self._title = title
         self.auth = auth if auth else (lambda user: True)
