3603cfb7571709ad505e4dd34325446cb2695a30
Cory Zue <czue@dimagi.com>
2011-06-01 09:48:16 -0400

    Add DateSpan object and datespan_in_request decorator
    This helps you manage date bound reports in a clean way.

--- dimagi/utils/dates.py
+++ dimagi/utils/dates.py
@@ -35,3 +35,83 @@ def add_months(year, months, offset):
 def delta_secs(td):
     """convert a timedelta to seconds"""
     return 86400. * td.days + td.seconds + 1.0e-6 * td.microseconds
+
+
+DEFAULT_DATE_FORMAT = "%m/%d/%Y"
+    
+class DateSpan(object):
+    """
+    A useful class for representing a date span
+    """
+    
+    def __init__(self, startdate, enddate, format=DEFAULT_DATE_FORMAT):
+        self.startdate = startdate
+        self.enddate = enddate
+        self.format = format
+    
+    @property
+    def startdate_param(self):
+        if self.startdate:
+            return self.startdate.strftime(self.format)
+    
+    @property
+    def enddate_param(self):
+        if self.enddate:
+            return self.enddate.strftime(self.format)
+        
+    
+    def is_valid(self):
+        # this is a bit backwards but keeps the logic in one place
+        return not bool(self.get_validation_reason())
+    
+    def get_validation_reason(self):
+        if self.startdate is None or self.enddate is None:
+            return "You have to specify both dates!"
+        else:
+            if self.enddate < self.startdate:
+                return "You can't have an end date of %s after start date of %s" % (self.enddate, self.startdate)
+        return ""
+    
+    def __str__(self):
+        if not self.is_valid():
+            return "Invalid date span %s - %s" % (self.startdate_param, self.enddate_param)
+        
+        # if the end date is today or tomorrow, use "last N days syntax"  
+        today = datetime.combine(datetime.today(), time())
+        day_after_tomorrow = today + timedelta (days=2)
+        if today <= self.enddate < day_after_tomorrow:
+            return "last %s days" % (self.enddate - self.startdate).days 
+        
+        return "%s to %s" % (self.startdate.strftime(self.format), 
+                             self.enddate.strftime(self.format))
+        
+    @classmethod
+    def since(cls, days, enddate=None, format=DEFAULT_DATE_FORMAT):
+        """
+        Generate a DateSpan ending with a certain date, and going back 
+        N days. The enddate defaults to tomorrow midnight 
+        (so it's inclusive of today).
+        
+        Will always ignore times.
+        """
+        if enddate is None:
+            enddate = datetime.utcnow() + timedelta(days=1)
+        end = datetime(enddate.year, enddate.month, enddate.day)
+        start = end - timedelta(days=days)
+        return DateSpan(start, end, format)
+                    
+    
+    def parse(self, startdate_str, enddate_str, parse_format, display_format=None):
+        """
+        Generate a DateSpan with string formats. 
+        """
+        if display_format is None:
+            display_format = format
+        
+        def date_or_nothing(param):
+            return datetime.strptime(dict[param], parse_format)\
+                        if param in dict and dict[param] else None
+        startdate = date_or_nothing(startdate_str)
+        enddate = date_or_nothing(enddate_str)
+        return DateSpan(startdate, enddate, format)
+        
---
+++ dimagi/utils/decorators/datespan.py
@@ -0,0 +1,54 @@
+from django.http import HttpRequest
+from dimagi.utils.dates import DateSpan
+from datetime import datetime
+
+def datespan_in_request(from_param="from", to_param="to", 
+                        format_string="%m/%d/%Y", default_days=30):
+    """
+    Wraps a request with dates based on url params or defaults and
+    Checks date validity.
+    """
+    
+    # this is loosely modeled after example number 4 of decorator
+    # usage here: http://www.python.org/dev/peps/pep-0318/
+    def get_dates(f):
+        def wrapped_func(*args, **kwargs):
+            # attempt to find the request object from all the argument
+            # values, checking first the args and then the kwargs 
+            req = None
+            for arg in args:
+                if _is_http_request(arg):
+                    req = arg
+                    break
+            if not req:
+                for arg in kwargs.values():
+                    if _is_http_request(arg):
+                        req = arg
+                        break
+            if req:
+                dict = req.POST if req.method == "POST" else req.GET
+                def date_or_nothing(param):
+                    return datetime.strptime(dict[param], format_string)\
+                             if param in dict and dict[param] else None
+                startdate = date_or_nothing(from_param)
+                enddate = date_or_nothing(to_param)
+                if startdate or enddate:
+                    req.datespan = DateSpan(startdate, enddate, format_string)
+                else:        
+                    # default to the last N days
+                    req.datespan = DateSpan.since(default_days, format=format_string)
+                    
+            return f(*args, **kwargs) 
+        if hasattr(f, "func_name"):
+            wrapped_func.func_name = f.func_name
+            # preserve doc strings
+            wrapped_func.__doc__ = f.__doc__  
+            
+            return wrapped_func
+        else:
+            # this means it wasn't actually a view.  
+            return f 
+    return get_dates
+
+def _is_http_request(obj):
+    return obj and isinstance(obj, HttpRequest)
