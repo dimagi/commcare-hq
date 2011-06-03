2a09ed7de05a52a90c6904ef6d25cbb04488a2f8
Cory Zue <czue@dimagi.com>
2011-06-03 15:57:15 -0400

    rename punchcard app to formtrends

--- corehq/apps/reports/calc/punchcard.py
+++ corehq/apps/reports/calc/punchcard.py
@@ -1,15 +1,11 @@
 from collections import defaultdict
 from dimagi.utils.couch.database import get_db
-from random import randint
-from couchdbkit.resource import ResourceNotFound
-import logging
-
 
 def get_data(domain, individual=None):
     data = defaultdict(lambda: 0)
     startkey = [domain, individual] if individual else [domain]
     endkey = [domain, individual, {}] if individual else [domain, {}]
-    view = get_db().view("punchcard/form_time_by_user", 
+    view = get_db().view("formtrends/form_time_by_user", 
                          startkey=startkey,
                          endkey=endkey,
                          group=True)
