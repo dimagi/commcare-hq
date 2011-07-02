3d4740d703e0adc52985d6509152e873d9372928
Rowena <rluk@dimagi.com>
2011-07-02 21:59:53 +0300

    paginate auditlog view. ADD DJTABLES DEPENDENCY.

--- auditcare/views.py
+++ auditcare/views.py
@@ -13,6 +13,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from auditcare import models
 from auditcare.models import AccessAudit, couchmodels
+from auditcare.tables import AuditLogTable
 
 import logging
 
@@ -21,27 +22,15 @@ LOCKOUT_TEMPLATE = getattr(settings, 'AXES_LOCKOUT_TEMPLATE', None)
 LOCKOUT_URL = getattr(settings, 'AXES_LOCKOUT_URL', None)
 VERBOSE = getattr(settings, 'AXES_VERBOSE', True)
 
-
 def auditAll(request, template="auditcare/index.html"):
     auditEvents = couchmodels.AccessAudit.view("auditcare/by_date_access_events", descending=True, include_docs=True).all()
-    context = RequestContext(request)
-
     realEvents = [{'user': a.user, 
                    'date': a.event_date, 
                    'class': a.doc_type, 
                    'access_type': a.access_type } for a in auditEvents]
-
-#    realEvents = [{"user":a["key"][0], "path":a["value"], "date":
-#                    datetime.datetime(year=int(a["key"][1]),
-#                                      month=int(a["key"][2]),
-#                                      day=int(a["key"][3]),
-#                                      hour=a["key"][4],
-#                                      minute=a["key"][5],
-#                                      second=a["key"][6])
-#                  }
-#                  for a in auditEvents]
-    context['auditEvents'] = realEvents
-    return render_to_response(template, context)
+    return render_to_response(template, 
+                              {"audit_table": AuditLogTable(realEvents, request=request)}, 
+                              context_instance=RequestContext(request))
 
 from django.contrib.auth import views as auth_views
 
