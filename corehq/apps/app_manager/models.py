73e8cbda14a88d8200d9f77dbae7f671f17a9d0e
Cory Zue <czue@dimagi.com>
2011-07-08 21:30:34 +0530

    allow app to build even if bitly is unavailable (for offline use)

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -20,7 +20,7 @@ from corehq.apps.domain.models import Domain
 from BeautifulSoup import BeautifulStoneSoup
 import hashlib
 from django.template.loader import render_to_string
-from urllib2 import urlopen
+from urllib2 import urlopen, URLError
 from urlparse import urljoin
 from corehq.apps.domain.decorators import login_and_domain_required
 import langcodes
@@ -34,6 +34,7 @@ from couchdbkit.resource import ResourceNotFound
 import tempfile
 import os
 from utilities.profile import profile as profile_decorator
+import logging
 
 MISSING_DEPENDECY = \
 """Aw shucks, someone forgot to install the google chart library 
@@ -717,9 +718,15 @@ class ApplicationBase(VersionedDoc):
         copy = super(ApplicationBase, self).save_copy()
         copy.create_jadjar()
 
-        copy.short_url = bitly.shorten(
-            get_url_base() + reverse('corehq.apps.app_manager.views.download_jad', args=[copy.domain, copy._id])
-        )
+        try:
+            copy.short_url = bitly.shorten(
+                get_url_base() + reverse('corehq.apps.app_manager.views.download_jad', args=[copy.domain, copy._id])
+            )
+        except URLError:
+            # for offline only
+            logging.exception("Problem creating bitly url for app %s. Do you have network?" % self.get_id)
+            copy.short_url = None
+        
         copy.save(increment_version=False)
 
         return copy
