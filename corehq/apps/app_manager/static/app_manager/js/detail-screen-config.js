066861a78fe968f1cf0ad6a6aa94a7ceaee4f1d7
Daniel Roberts <droberts@dimagi.com>
2011-07-11 10:33:43 -0400

    continued work on detail-screen ui

--- corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
+++ corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
@@ -2,15 +2,20 @@ var DetailScreenConfig = (function () {
     "use strict";
     var DetailScreenConfig, Screen, Column;
     Column = (function () {
-        function Column(col, lang) {
+        function Column(col, screen) {
+            this.screen = screen;
             this.model = col.model;
             this.field = col.field;
-            this.lang = lang;
+            this.lang = screen.lang;
+            
             this.setHeader(col.header ? col.header[this.lang] : "");
             this.setSelected(col.selected);
+            this.setEnum((col['enum'] || {})[this.lang]);
+            this.setFormat(col.format);
+            console.log(col);
         }
-        Column.init = function (col, lang) {
-            return new Column(col, lang);
+        Column.init = function (col, screen) {
+            return new Column(col, screen);
         };
         Column.prototype = {
             setHeader: function (header) {
@@ -19,13 +24,79 @@ var DetailScreenConfig = (function () {
             },
             setSelected: function (selected) {
                 var gripClass = "ui-icon-grip-dotted-horizontal";
-                this.selected = selected;
-                this.$selected = (this.$selected || $('<input type="checkbox"/>')).prop("checked", selected);
-                if (selected) {
-                    this.$grip = $('<div class="grip ui-icon"/>').addClass(gripClass);
-                } else {
-                    this.$grip = $('<span class="sort-disabled"></span>');
+                if (this.selected !== selected) {
+                    this.selected = selected;
+                    this.$selected = (this.$selected || $('<input type="checkbox"/>')).prop("checked", selected);
+                    if (selected) {
+                        this.$grip = $('<div class="grip ui-icon"/>').addClass(gripClass);
+                    } else {
+                        this.$grip = $('<span class="sort-disabled"></span>');
+                    }
                 }
+            },
+            setFormat: function (format) {
+                if (!this.hasOwnProperty('$format')) {
+                    var i, option, options = [
+                        {"label": "Plain", "value": "plain"},
+                        {"label": "Date", "value": "date"},
+                        {"label": "Years Ago", "value": "years-ago"},
+                        {"label": "Phone Number", "value": "phone"},
+                        {"label": "Enum", "value": "enum"},
+                        {"label": "Late Flag", "value": "late-flag"},
+                    ];
+                    this.$format = $('<select/>');
+                    for (i = 0; i < options.length; i += 1) {
+                        option = options[i];
+                        $('<option/>').text(option.label).val(option.value).appendTo(this.$format);
+                    }
+                }
+                this.format = format;
+                this.$format.val(format);
+                if (format === "enum") {
+                    this.$extra = this.$enum;
+                } else {
+                    this.$extra = $("<span/>");
+                }
+            },
+            setEnum: function (_enum) {
+                this.$enum = (this.$enum || $('<input type="text"/>')).val(_enum);
+                this['enum'] = _enum;
+            },
+            handleSelectedChange: function (e) {
+                var selected = this.$selected.prop("checked");
+                console.log(selected);
+                if (this.selected !== selected) {
+                    if (this.selected) {
+                        this.screen.columns.splice(this.screen.columns.indexOf(this), 1);
+                        this.screen.unselectedColumns.push(this);
+                    } else {
+                        this.screen.unselectedColumns.splice(this.screen.unselectedColumns.indexOf(this), 1);
+                        this.screen.columns.push(this);
+                    }
+                    this.setSelected(selected);
+                    this.screen.render();
+                }
+            },
+            handleFormatChange: function (e) {
+                var format = this.$format.val();
+                this.setFormat(format);
+                this.screen.render();
+            },
+            initUI: function () {
+                var that = this;
+                this.initSelected();
+                this.$format.change(function (e) {
+                    that.handleFormatChange(e);
+                });
+                this.$enum.change(function (e) {
+                    that.setEnum(that.$enum.val());
+                });
+            },
+            initSelected: function () {
+                var that = this;
+                this.$selected.change(function (e) {
+                    that.handleSelectedChange(e);
+                });
             }
         };
         return Column;
@@ -38,9 +109,8 @@ var DetailScreenConfig = (function () {
             ref_long: "Referral Full View"
         };
         function Screen($home, spec, options) {
-            var i,
-                selectedProperties = {"case": {}, referral: {}},
-                column, model, property;
+            var i, column, model, property,
+                selectedProperties = {"case": {}, referral: {}};
             this.$home = $home;
             this.columns = [];
             this.unselectedColumns = [];
@@ -52,7 +122,7 @@ var DetailScreenConfig = (function () {
             for (i = 0; i < spec.columns.length; i += 1) {
                 column = spec.columns[i];
                 column.selected = true;
-                this.columns[i] = Column.init(column, this.lang);
+                this.columns[i] = Column.init(column, this);
                 selectedProperties[column.model][column.field] = true;
             }
             for (model in this.properties) {
@@ -64,24 +134,22 @@ var DetailScreenConfig = (function () {
                                 model: model,
                                 field: property,
                                 selected: false
-                            }));
+                            }, this));
                         }
                     }
                 }
             }
             this.render();
-            this.$columns.sortable({
-                handle: '.grip',
-                items: ">*:not(:has(.sort-disabled))"
-            });
         }
         Screen.init = function ($home, spec, lang) {
             return new Screen($home, spec, lang);
         };
         Screen.prototype = {
             render: function () {
-                var $table, $columns, $unselectedColumns, $tr, i, column, showColumn,
-                    that = this;
+                var $table, $columns, $unselectedColumns, $tr, i, showColumn,
+                    that = this,
+                    rows = [];
+                this.$home.html("");
                 $("<h1/>").text(sectionLabels[this.type]).appendTo(this.$home);
                 
                 $table = $("<table/>").appendTo(this.$home);
@@ -89,34 +157,74 @@ var DetailScreenConfig = (function () {
                 $tr = $("<tr/>").appendTo($table);
 
                 $("<th/>").appendTo($tr);
-                $("<th/>").text("Selected").appendTo($tr);
+                $("<th/>").appendTo($tr);
                 if (this.model === "referral") {
                     $("<th/>").text("Model").appendTo($tr);
                 }
                 $("<th/>").text("Property").appendTo($tr);
                 $("<th/>").text("Header").appendTo($tr);
+                $("<th/>").text("Format").appendTo($tr);
+                $("<th/>").appendTo($tr); // Extra
 
                 $columns = $("<tbody/>").appendTo($table);
                 $unselectedColumns = $("<tbody/>").appendTo($table);
 
-                showColumn = function (column, $tbody) {
-                    $tr = $("<tr/>").appendTo($tbody);
+                this.unselectedColumns.sortBy(function () {
+                    return [this.model, this.field];
+                });
+
+                showColumn = function (column, $tbody, i) {
+                    $tr = $("<tr/>").data('index', i).appendTo($tbody);
 
                     $("<td/>").append(column.$grip).appendTo($tr);
                     $("<td/>").append(column.$selected).appendTo($tr);
                     if (that.model === 'referral') {
                         $("<td/>").text(column.model).appendTo($tr);
                     }
-                    $("<td/>").text(column.field).appendTo($tr);
+                    $("<td/>").html("<code>" + column.field + "</code>").appendTo($tr);
                     $("<td/>").append(column.$header).appendTo($tr);
+                    $("<td/>").append(column.$format).appendTo($tr);
+                    $("<td/>").append(column.$extra).appendTo($tr);
+                    rows.push($tr.elem);
                 };
                 for (i = 0; i < this.columns.length; i += 1) {
-                    showColumn(this.columns[i], $columns);
+                    showColumn(this.columns[i], $columns, i);
                 }
                 for (i = 0; i < this.unselectedColumns.length; i += 1) {
-                    showColumn(this.unselectedColumns[i], $unselectedColumns);
+                    showColumn(this.unselectedColumns[i], $unselectedColumns, i);
                 }
                 this.$columns = $columns;
+
+                // init UI events
+                this.initUI(rows);
+            },
+            initUI: function (rows) {
+                var i, column,
+                    that = this;
+                this.$columns.sortable({
+                    handle: '.grip',
+                    items: ">*:not(:has(.sort-disabled))",
+                    update: function (e, ui) {
+                        var fromIndex = ui.item.data('index'),
+                            // rows is a closure variable
+                            toIndex = rows.indexOf(ui.item);
+
+                        function reorder(list) {
+                            var tmp = list.splice(fromIndex, 1)[0];
+                            list.splice(toIndex, 0, tmp);
+                        }
+                        reorder(rows);
+                        reorder(that.columns);
+                    }
+                });
+                for (i = 0; i < this.columns.length; i += 1) {
+                    column = this.columns[i];
+                    column.initUI();
+                }
+                for (i = 0; i < this.unselectedColumns.length; i += 1) {
+                    column = this.unselectedColumns[i];
+                    column.initUI();
+                }
             }
         };
         return Screen;
