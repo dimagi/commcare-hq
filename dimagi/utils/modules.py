dc9f10e00bdda6e9c371c7cd95dca258f7009c44
Cory Zue <czue@dimagi.com>
2011-08-25 10:42:44 -0400

    add to_function utility

--- dimagi/utils/modules.py
+++ dimagi/utils/modules.py
@@ -7,6 +7,23 @@ import sys
 import traceback
 
 
+def to_function(function_path, failhard=False):
+    """
+    Convert a string like foo.bar.baz into a function (assumes that
+    baz is a function defined in foo/bar.py).
+    """
+    try:
+        # TODO: make this less brittle if imports or args don't line up
+        module, func = function_path.rsplit(".", 1)
+        module = __import__(module, globals(), locals(), [func])
+        actual_func = getattr(module, func)
+        return actual_func
+    except ImportError:
+        if failhard:
+            raise
+        
+
+    
 def try_import(module_name):
     """
     Import and return *module_name*.
