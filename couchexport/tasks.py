37603fc6989924814888c118b7abe040c59ed566
Cory Zue <czue@dimagi.com>
2011-08-29 09:23:26 -0400

    bugfix for when no filename is specified

--- couchexport/tasks.py
+++ couchexport/tasks.py
@@ -16,6 +16,9 @@ def export_async(download_id, export_tag, format=None, filename=None,
                  previous_export_id=None, filter=None, 
                  expiry=10*60*60):
     
+    if not filename:
+        filename = export_tag
+    
     (tmp, checkpoint) = get_export_files(export_tag, format, previous_export_id, filter)
     if checkpoint:
         temp_id = uuid.uuid4().hex
