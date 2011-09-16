c385747a2dc0f0948a8d12506cae191917b696d2
Daniel Roberts <droberts@dimagi.com>
2011-09-16 12:01:08 -0400

    extended app_manager/applications view

--- corehq/apps/app_manager/_design/views/applications/map.js
+++ corehq/apps/app_manager/_design/views/applications/map.js
@@ -1,5 +1,7 @@
 function(doc){
     if(doc.doc_type == 'Application' || doc.doc_type == 'RemoteApp') {
         emit([doc.domain, doc.copy_of, doc.version], null);
+        // to search accross all domains
+        emit([null, doc.copy_of, doc.version], null);
     }
 }
