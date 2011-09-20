3bb9669b123081549cdec82b285ed62ded85a197
Daniel Roberts <droberts@dimagi.com>
2011-09-20 15:29:19 -0400

    tried to fix bug in previous commit

--- corehq/couchapps/formtrends/views/form_duration_by_user/map.js
+++ corehq/couchapps/formtrends/views/form_duration_by_user/map.js
@@ -1,20 +1,22 @@
 function (doc) {
-    var get_form_filled_duration = function (xform_doc) {
+    function get_form_filled_duration(xform_doc) {
         // in milliseconds
-        if (xform_doc.form.meta && xform_doc.form.meta.timeEnd && xform_doc.form.meta.timeStart) {
+        if (xform_doc.form && xform_doc.form.meta && xform_doc.form.meta.timeEnd && xform_doc.form.meta.timeStart) {
             return new Date(xform_doc.form.meta.timeEnd).getTime() -
                 new Date(xform_doc.form.meta.timeStart).getTime();
         }
         return null;
-    },
-        duration, form_date, xmlns, userID, domain;
+    }
+
+    var duration, form_date, xmlns, userID, domain;
 
     // for now incude all non-device-reports in this report
     if (doc.doc_type === "XFormInstance" &&
-            doc.xmlns !== "http://code.javarosa.org/devicereport") {
+            doc.xmlns !== "http://code.javarosa.org/devicereport" &&
+            doc.xmlns !== "http://openrosa.org/user-registration") {
+        duration = get_form_filled_duration(doc);
         if (duration) {
             domain = doc.domain;
-            duration = get_form_filled_duration(doc);
             xmlns = doc.xmlns;
             userID = doc.form.meta.userID;
             form_date = doc.received_on;
