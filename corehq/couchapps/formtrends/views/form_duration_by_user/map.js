8869f8227045f6901db40debaa22932a93dedf0d
Daniel Roberts <droberts@dimagi.com>
2011-09-20 14:43:50 -0400

    made form_duration_by_user index by submit time rather than complete time

--- corehq/couchapps/formtrends/views/form_duration_by_user/map.js
+++ corehq/couchapps/formtrends/views/form_duration_by_user/map.js
@@ -1,31 +1,26 @@
-function(doc) {
-    function get_form_filled_date(xform_doc) {
-        var meta = xform_doc.form.meta;
-        if (meta && meta.timeEnd) return new Date(meta.timeEnd);
-        if (meta && meta.timeStart) return new Date(meta.timeStart);
-        return null;
-    }
-    
-    function get_form_filled_duration(xform_doc) {
+function (doc) {
+    var get_form_filled_duration = function (xform_doc) {
         // in milliseconds
-        if (xform_doc.form.meta && xform_doc.form.meta.timeEnd && xform_doc.form.meta.timeStart) 
-            return new Date(xform_doc.form.meta.timeEnd).getTime() - 
-                    new Date(xform_doc.form.meta.timeStart).getTime(); 
+        if (xform_doc.form.meta && xform_doc.form.meta.timeEnd && xform_doc.form.meta.timeStart) {
+            return new Date(xform_doc.form.meta.timeEnd).getTime() -
+                new Date(xform_doc.form.meta.timeStart).getTime();
+        }
         return null;
-    }
-    
+    },
+        duration, form_date, xmlns, userID, domain;
+
     // for now incude all non-device-reports in this report
-    if (doc.doc_type == "XFormInstance" &&
-        doc.xmlns != "http://code.javarosa.org/devicereport")
-    {
-        var filled_on = get_form_filled_date(doc);
-        var duration = get_form_filled_duration(doc);
-        if (filled_on && duration) {
-            var form_date = new Date(filled_on.getFullYear(), filled_on.getMonth(), filled_on.getDate());
-            emit(["dx", doc.domain, form_date, doc.xmlns], duration);
-            emit(["udx", doc.domain, doc.form.meta.userID, form_date, doc.xmlns], duration);
-            emit(["xdu", doc.domain, doc.xmlns, form_date, doc.form.meta.userID], duration);
-            
+    if (doc.doc_type === "XFormInstance" &&
+            doc.xmlns !== "http://code.javarosa.org/devicereport") {
+        if (duration) {
+            domain = doc.domain;
+            duration = get_form_filled_duration(doc);
+            xmlns = doc.xmlns;
+            userID = doc.form.meta.userID;
+            form_date = doc.received_on;
+            emit(["dx", domain, form_date, xmlns], duration);
+            emit(["udx", domain, userID, form_date, xmlns], duration);
+            emit(["xdu", domain, xmlns, form_date, userID], duration);
         }
     }
 }
