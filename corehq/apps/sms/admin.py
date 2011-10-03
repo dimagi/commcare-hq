ea4559b2b8455677e47fa26197306fc23c1295e6
Cory Zue <czue@dimagi.com>
2011-10-03 08:28:33 -0400

    add better message log admin

--- corehq/apps/sms/admin.py
+++ corehq/apps/sms/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from corehq.apps.sms.models import MessageLog
 
-admin.site.register(MessageLog)
+class MessageLogAdmin(admin.ModelAdmin):
+    model = MessageLog
+    list_display = ["domain", "phone_number", "date", "direction", "text"]
+    list_filter = ["domain", "phone_number", "date", "direction"]
+
+admin.site.register(MessageLog, MessageLogAdmin)
