259058b26853beb119b923365e7b623863bf138e
Daniel Roberts <droberts@dimagi.com>
2011-10-03 15:22:40 -0400

    removed cruft in export.py

--- dimagi/utils/export.py
+++ dimagi/utils/export.py
@@ -1,6 +1,4 @@
-import csv
 from StringIO import StringIO
-import zipfile
 from couchexport.export import export_from_tables
 
 class WorkBook(object):
@@ -29,33 +27,3 @@ class WorkBook(object):
         f = StringIO()
         export_from_tables(tables, f, format)
         return f.getvalue()
-
-class CsvWorkBook(WorkBook):
-    def __init__(self):
-        self._writers = {}
-
-    def open(self, table, headers):
-        f = StringIO()
-        writer = csv.DictWriter(f, headers)
-        try:
-            writer.writeheader()
-        except AttributeError:
-            f.write(",".join([h.encode('utf-8') for h in headers]))
-            f.write("\n")
-
-        self._writers[table] = (f, writer)
-
-    def write_row(self, table, row):
-        _, writer = self._writers[table]
-        for key in row:
-            if isinstance(row[key], unicode):
-                row[key] = row[key].encode('utf-8')
-        writer.writerow(row)
-
-    def to_zip(self):
-        zip_io = StringIO()
-        zip = zipfile.ZipFile(zip_io, 'w')
-        for (table, (f, writer)) in self._writers.items():
-            zip.writestr('{table}.csv'.format(table=table), f.getvalue())
-        zip.close()
-        return zip_io.getvalue()
