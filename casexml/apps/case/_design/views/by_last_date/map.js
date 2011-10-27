7e9f87fc9002412ef876e7ee77755f89a02d4a29
Daniel Roberts <droberts@dimagi.com>
2011-10-27 18:03:08 -0400

    index case/by_last_date by case_type as well

--- casexml/apps/case/_design/views/by_last_date/map.js
+++ casexml/apps/case/_design/views/by_last_date/map.js
@@ -1,9 +1,10 @@
 function(doc) {
-    DAY = 86400000;
+    var DAY = 86400000;
     function mkdate(d) {
         return new Date(d.replace(/-/g, '/').replace('T', ' ').replace('Z', ''));
     }
     if (doc.doc_type == "CommCareCase" && !doc.closed) {
-        emit([doc.domain, doc.user_id, doc.modified_on], mkdate(doc.modified_on).getTime()/DAY);
+        emit([doc.domain, doc.type, doc.user_id, doc.modified_on], mkdate(doc.modified_on).getTime()/DAY);
+        emit([doc.domain, {}, doc.user_id, doc.modified_on], mkdate(doc.modified_on).getTime()/DAY);
     }
 }
