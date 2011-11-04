7da65e648136f9524aaf7935583ebee60d201559
dmyung <dmyung@dimagi.com>
2011-11-04 10:12:35 -0400

    removing unused views, updating model history view

--- auditcare/models/_design/views/login_failures/map.js
+++
@@ -1,9 +0,0 @@
-function(doc) {
-    //Basic emission of login failure events
-    if(doc.base_type == 'AuditEvent' && doc.doc_type == 'AccessAudit' && doc.access_type == "login_failed") {
-        //all audit events
-        emit(['ip', doc.ip_address], {'event_date': doc.event_date, 'count': 1});
-        emit(['ip_ua', doc.ip_address, doc.user_agent], {'event_date': doc.event_date, 'count': 1});
-        emit(['user', doc.user], {'event_date': doc.event_date, 'count': 1});
-    }
-}
