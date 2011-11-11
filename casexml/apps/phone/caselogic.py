8ed3d862c644074dc3d9023aa8ae1b439276cf9d
Cory Zue <czue@dimagi.com>
2011-11-11 16:06:58 -0500

    remove reduce param from non-reduce view

--- casexml/apps/phone/caselogic.py
+++ casexml/apps/phone/caselogic.py
@@ -26,7 +26,7 @@ def get_open_cases_to_send(user, last_sync):
     
     # first pull down the full list (TODO: we could in the future only pull down 
     # IDs for speed improvements during sync mode)
-    cases = CommCareCase.view("case/by_owner", keys=keys, reduce=False,
+    cases = CommCareCase.view("case/by_owner", keys=keys,
                               include_docs=True).all()
     
     
