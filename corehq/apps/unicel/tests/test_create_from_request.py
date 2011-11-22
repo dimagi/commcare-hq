40e01266725f98d2836740fdf50217f46b7bb3b8
Biyeun Buczyk <biyeun@dimagi.com>
2011-11-22 16:51:28 -0500

    testing create_from_request in api.py

--- corehq/apps/unicel/tests/test_create_from_request.py
+++ corehq/apps/unicel/tests/test_create_from_request.py
@@ -1,20 +1,49 @@
 from datetime import datetime
 from django.test import TestCase
-from corehq.apps.unicel.api import InboundParams
+from django.test.client import Client
+from corehq.apps.sms.models import MessageLog
+from corehq.apps.users.models import CouchUser, WebUser
+from corehq.apps.unicel.api import InboundParams, create_from_request
 
 class IncomingPostTest(TestCase):
 
-    def SetUp(self):
+    def setUp(self):
+        all_logs = MessageLog.all()
+        for log in all_logs:
+            log.delete()
         self.user = 'username'
         self.password = 'password'
         self.domain = 'mockdomain'
-        self.phone_number = '5555551234'
-        self.message = 'Test Message'
-        self.couch_user = WebUser.create(self.domain, self.username, self.password);
+        self.number = 5555551234
+        self.couch_user = WebUser.create(self.domain, self.user, self.password)
+        self.couch_user.add_phone_number(self.number)
         self.couch_user.save()
+        self.dcs = '8'
+        self.message_ascii = 'Test Message'
+        self.message_utf_hex = '0939093F0928094D092609400020091509300924093E00200939094800200907093800200938092E092F00200915093E092E002009390948003F'
 
+    def tearDown(self):
+        self.couch_user.delete()
+        
+    def testPostToIncomingAscii(self):
+        fake_post = {InboundParams.SENDER: str(self.number),
+                     InboundParams.MESSAGE: self.message_ascii,
+                     InboundParams.TIMESTAMP: datetime.now().strftime("%m/%d/%Y %H:%M:%S %p"),
+                     InboundParams.DCS: self.dcs,
+                     InboundParams.UDHI: '0'}
+        client = Client()
+        response = client.post('/unicel/in/', fake_post)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, MessageLog.count_by_domain(self.domain))
 
-    def testPostToIncoming(self):
-        fake_post = {InboundParams.SENDER: self.phone_number,
-                     InboundParams.MESSAGE: self.message,
-                     InboundParams.TIMESTAMP: datetime.now()}
+    def testPostToIncomingUtf(self):
+        fake_post = {InboundParams.SENDER: str(self.number),
+                     InboundParams.MESSAGE: self.message_utf_hex,
+                     InboundParams.TIMESTAMP: datetime.now().strftime("%m/%d/%Y %H:%M:%S %p"),
+                     InboundParams.DCS: self.dcs,
+                     InboundParams.UDHI: '2'}
+        client = Client()
+        response = client.get('/unicel/in/', fake_post)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(1, MessageLog.count_by_domain(self.domain))
+
