4ff5132eb4445d969eea25537c9c34ff0d8f99c0
Daniel Roberts <droberts@dimagi.com>
2011-12-07 13:40:26 -0500

    expose SaveButton as a js global variable so it'll work in formdesigner

--- corehq/apps/hqwebapp/static/hqwebapp/js/main.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/main.js
@@ -18,7 +18,6 @@ var eventize = function (that) {
         var i;
         if (events[tag] !== undefined) {
             for (i = 0; i < events[tag].length; i += 1) {
-                console.log(events[tag][i]);
                 events[tag][i].apply(that, [e]);
             }
         }
@@ -26,6 +25,119 @@ var eventize = function (that) {
     };
     return that;
 };
+
+var SaveButton = {
+    /*
+        options: {
+            save: "Function to call when the user clicks Save",
+            unsavedMessage: "Message to display when there are unsaved changes and the user leaves the page"
+        }
+    */
+    init: function (options) {
+        var button = {
+            $save: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVE).click(function () {
+                button.fire('save');
+            }).button(),
+            $retry: $('<span/>').text(COMMCAREHQ.SaveButton.message.RETRY).click(function () {
+                button.fire('save');
+            }).button(),
+            $saving: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVING).button().button('disable'),
+            $saved: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVED).button().button('disable'),
+            ui: $('<div/>').css({textAlign: 'right'}),
+            setStateWhenReady: function (state) {
+                if (this.state === 'saving') {
+                    this.nextState = state;
+                } else {
+                    this.setState(state);
+                }
+            },
+            setState: function (state) {
+                if (this.state === state) {
+                    return;
+                }
+                this.state = state;
+                this.$save.detach();
+                this.$saving.detach();
+                this.$saved.detach();
+                this.$retry.detach();
+                if (state === 'save') {
+                    this.ui.append(this.$save);
+                } else if (state === 'saving') {
+                    this.ui.append(this.$saving);
+                } else if (state === 'saved') {
+                    this.ui.append(this.$saved);
+                } else if (state === 'retry') {
+                    this.ui.append(this.$retry);
+                }
+            },
+            ajax: function (options) {
+                var beforeSend = options.beforeSend || function () {},
+                    success = options.success || function () {},
+                    error = options.error || function () {},
+                    that = this;
+                options.beforeSend = function () {
+                    that.setState('saving');
+                    that.nextState = 'saved';
+                    beforeSend.apply(this, arguments);
+                };
+                options.success = function (data) {
+                    that.setState(that.nextState);
+                    success.apply(this, arguments);
+                };
+                options.error = function (data) {
+                    that.nextState = null;
+                    that.setState('retry');
+                    alert(COMMCAREHQ.SaveButton.message.ERROR_SAVING);
+                    error.apply(this, arguments);
+                };
+                $.ajax(options);
+            }
+        };
+        eventize(button);
+        button.setState('saved');
+        button.on('change', function () {
+            this.setStateWhenReady('save');
+        });
+        if (options.save) {
+            button.on('save', options.save);
+        }
+        $(window).bind('beforeunload', function () {
+            if (button.state !== 'saved') {
+                return options.unsavedMessage || "";
+            }
+        });
+        return button;
+    },
+    initForm: function ($form, options) {
+        var url = $form.attr('action'),
+            button = COMMCAREHQ.SaveButton.init({
+                unsavedMessage: options.unsavedMessage,
+                save: function () {
+                    this.ajax({
+                        url: url,
+                        type: 'POST',
+                        dataType: 'json',
+                        data: $form.serialize(),
+                        success: options.success
+                    });
+                }
+            }),
+            fireChange = function () {
+                button.fire('change');
+            };
+        $form.find('*').change(fireChange);
+        $form.find('input, textarea').bind('textchange', fireChange);
+        return button;
+    },
+    message: {
+        SAVE: 'Save',
+        SAVING: 'Saving...',
+        SAVED: 'Saved',
+        RETRY: 'Try Again',
+        ERROR_SAVING: 'There was an error saving'
+    }
+};
+
 
 var COMMCAREHQ = (function () {
     'use strict';
@@ -138,117 +250,7 @@ var COMMCAREHQ = (function () {
                 }]
             });
         },
-        SaveButton: {
-            /*
-                options: {
-                    save: "Function to call when the user clicks Save",
-                    unsavedMessage: "Message to display when there are unsaved changes and the user leaves the page"
-                }
-            */
-            init: function (options) {
-                var button = {
-                    $save: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVE).click(function () {
-                        button.fire('save');
-                    }).button(),
-                    $retry: $('<span/>').text(COMMCAREHQ.SaveButton.message.RETRY).click(function () {
-                        button.fire('save');
-                    }).button(),
-                    $saving: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVING).button().button('disable'),
-                    $saved: $('<span/>').text(COMMCAREHQ.SaveButton.message.SAVED).button().button('disable'),
-                    ui: $('<div/>').css({textAlign: 'right'}),
-                    setStateWhenReady: function (state) {
-                        if (this.state === 'saving') {
-                            this.nextState = state;
-                        } else {
-                            this.setState(state);
-                        }
-                    },
-                    setState: function (state) {
-                        if (this.state === state) {
-                            return;
-                        }
-                        this.state = state;
-                        this.$save.detach();
-                        this.$saving.detach();
-                        this.$saved.detach();
-                        this.$retry.detach();
-                        if (state === 'save') {
-                            this.ui.append(this.$save);
-                        } else if (state === 'saving') {
-                            this.ui.append(this.$saving);
-                        } else if (state === 'saved') {
-                            this.ui.append(this.$saved);
-                        } else if (state === 'retry') {
-                            this.ui.append(this.$retry);
-                        }
-                    },
-                    ajax: function (options) {
-                        var beforeSend = options.beforeSend || function () {},
-                            success = options.success || function () {},
-                            error = options.error || function () {},
-                            that = this;
-                        options.beforeSend = function () {
-                            that.setState('saving');
-                            that.nextState = 'saved';
-                            beforeSend.apply(this, arguments);
-                        };
-                        options.success = function (data) {
-                            that.setState(that.nextState);
-                            success.apply(this, arguments);
-                        };
-                        options.error = function (data) {
-                            that.nextState = null;
-                            that.setState('retry');
-                            alert(COMMCAREHQ.SaveButton.message.ERROR_SAVING);
-                            error.apply(this, arguments);
-                        };
-                        $.ajax(options);
-                    }
-                };
-                eventize(button);
-                button.setState('saved');
-                button.on('change', function () {
-                    this.setStateWhenReady('save');
-                });
-                if (options.save) {
-                    button.on('save', options.save);
-                }
-                $(window).bind('beforeunload', function () {
-                    if (button.state !== 'saved') {
-                        return options.unsavedMessage || "";
-                    }
-                });
-                return button;
-            },
-            initForm: function ($form, options) {
-                var url = $form.attr('action'),
-                    button = COMMCAREHQ.SaveButton.init({
-                        unsavedMessage: options.unsavedMessage,
-                        save: function () {
-                            this.ajax({
-                                url: url,
-                                type: 'POST',
-                                dataType: 'json',
-                                data: $form.serialize(),
-                                success: options.success
-                            });
-                        }
-                    }),
-                    fireChange = function () {
-                        button.fire('change');
-                    };
-                $form.find('*').change(fireChange);
-                $form.find('input, textarea').bind('textchange', fireChange);
-                return button;
-            },
-            message: {
-                SAVE: 'Save',
-                SAVING: 'Saving...',
-                SAVED: 'Saved',
-                RETRY: 'Try Again',
-                ERROR_SAVING: 'There was an error saving'
-            }
-        }
+        SaveButton: SaveButton
     };
 }());
 
