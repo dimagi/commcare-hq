8bed48500c8286f568f126a727c3bd957c572c8d
Cory Zue <czue@dimagi.com>
2011-12-20 14:46:38 -0500

    add actual case purging test

--- casexml/apps/phone/tests/test_case_purging.py
+++ casexml/apps/phone/tests/test_case_purging.py
@@ -14,6 +14,7 @@ from couchforms.models import XFormInstance
 from dimagi.utils.couch import uid
 from casexml.apps.phone.xml import date_to_xml_string
 from casexml.apps.case.util import post_case_blocks
+from casexml.apps.case.xml import V2
 
 class CasePurgeTest(TestCase):
     """
@@ -22,11 +23,13 @@ class CasePurgeTest(TestCase):
         
     def setUp(self):
         # clear cases
-        for item in XFormInstance.view("couchforms/by_xmlns", include_docs=True, reduce=False).all():
+        for item in XFormInstance.view("couchforms/by_xmlns", include_docs=True, 
+                                       reduce=False).all():
             item.delete()
         for case in CommCareCase.view("case/by_xform_id", include_docs=True).all():
             case.delete()
-        for log in SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all():
+        for log in SyncLog.view("phone/sync_logs_by_user", include_docs=True, 
+                                reduce=False).all():
             log.delete()
     
     def testBasicPurge(self):
@@ -41,33 +44,39 @@ class CasePurgeTest(TestCase):
             case_id=case_id,
             case_name="purgeme",
             case_type="sometype",
-            user_id=user.user_id, # must match
-            external_id=uid.new(), # must match
+            user_id=user.user_id,  # must match
             update={'foo': "bar"},
+            version=V2             # these only work on v2 cases
         ).as_xml(format_datetime=date_to_xml_string)
         
         post_case_blocks([case_block])
         
-        # have to sleep for annyoing timestamp reasons
+        # have to sleep for annoying timestamp reasons
         time.sleep(1)
-        check_user_has_case(self, user, case_block, line_by_line=True)
+        check_user_has_case(self, user, case_block, line_by_line=True, 
+                            version=V2)
         
-        [sync_log] = SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all()
-        check_user_has_case(self, user, case_block, should_have=False, restore_id=sync_log.get_id)
+        [sync_log] = SyncLog.view("phone/sync_logs_by_user", include_docs=True,
+                                   reduce=False).all()
+        check_user_has_case(self, user, case_block, should_have=False, 
+                            restore_id=sync_log.get_id, version=V2)
         
         # close it from someone else.
+        second_user = uid.new()
         close_block = CaseBlock(
             case_id=case_id,
-            update={"user_id": uid.new()},
-            close=True
+            user_id=second_user,
+            close=True,
+            version=V2
         ).as_xml(format_datetime=date_to_xml_string)
         
         post_case_blocks([close_block])
         
         # make sure the case comes back to be purged
-        expected = CaseBlock(case_id=case_id, close=True).as_xml(format_datetime=date_to_xml_string)
-        # TODO: this is currently broken until we have a way to tie 
-        # it to a different user
-#        check_user_has_case(self, user, expected, should_have=True, line_by_line=True,
-#                            restore_id=sync_log.get_id)
-#        
+        expected = CaseBlock(case_id=case_id, close=True, version=V2, 
+                             user_id=second_user).as_xml\
+                    (format_datetime=date_to_xml_string)
+        check_user_has_case(self, user, expected, should_have=True, 
+                            line_by_line=True, restore_id=sync_log.get_id, 
+                            version=V2)
+
