01f7d5b4e71f0e0ef02bed2c8072c7a3aa4d64b6
Cory Zue <czue@dimagi.com>
2011-12-20 15:57:51 -0500

    fix bad assert that wasn't always valid (mainly in tests)

--- corehq/apps/users/fixturegenerators.py
+++ corehq/apps/users/fixturegenerators.py
@@ -9,5 +9,6 @@ def user_groups(user, version=V1, last_sync=None):
     For a given user, return a fixture containing all the groups
     they are a part of.
     """
-    assert(user._hq_user is not None) # HQ sets this on the casexml user
-    return [user._hq_user.get_group_fixture()]
+    if hasattr(user, "_hq_user") and user._hq_user is not None:
+        return [user._hq_user.get_group_fixture()]
+    return [] # this user isn't made on HQ, and this also keeps tests happy
