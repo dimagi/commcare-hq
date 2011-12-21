2ac64422203abcc66b2fa225b6be007ef71e096b
Daniel Roberts <droberts@dimagi.com>
2011-12-21 17:27:48 -0500

    a test wasn't getting called and now is

--- corehq/apps/receiverwrapper/tests/__init__.py
+++ corehq/apps/receiverwrapper/tests/__init__.py
@@ -2,6 +2,7 @@ from dimagi.utils.logging import log_exception
 try:
     from .test_submissions import *
     from .test_repeater import *
+    from .test_url_regex import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
     # otherwise debugging is a pain
