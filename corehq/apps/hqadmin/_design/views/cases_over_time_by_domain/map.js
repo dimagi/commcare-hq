e96b9d1eafddcfca9d8750f312e10a58f00b98aa
Christian Ternus <ternus@mit.edu>
2012-01-17 12:03:03 -0500

    Active user report, and cleanup unused views.

--- corehq/apps/hqadmin/_design/views/cases_over_time_by_domain/map.js
+++
@@ -1,6 +0,0 @@
-function(doc) {
-    if(doc.doc_type == "CommCareCase" && doc.user_id ) {
-        var d = new Date(doc.opened_on);
-        emit([doc.domain, d.getFullYear(), d.getMonth()+1], null);
-    }
-}
