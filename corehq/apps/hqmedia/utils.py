2ef41263f30127ed5936ea897c5badbf80e1ea66
Biyeun Buczyk <biyeun@dimagi.com>
2012-02-02 09:52:42 -0200

    fixed get_modules bug and compressed the hqmedia images a little more

--- corehq/apps/hqmedia/utils.py
+++ corehq/apps/hqmedia/utils.py
@@ -7,21 +7,22 @@ def get_sorted_multimedia_refs(app):
     parsed_forms = {}
     images = {}
     audio_files = {}
-    for m in app.get_modules():
-        for f in m.get_forms():
-            parsed = f.wrapped_xform()
-            if not parsed.exists():
-                continue
-            parsed.validate()
-            parsed_forms[f] = parsed
-            for i in parsed.image_references:
-                if i not in images:
-                    images[i] = []
-                images[i].append((m,f))
-            for i in parsed.audio_references:
-                if i not in audio_files:
-                    audio_files[i] = []
-                audio_files[i].append((m,f))
+    if app:
+        for m in app.get_modules():
+            for f in m.get_forms():
+                parsed = f.wrapped_xform()
+                if not parsed.exists():
+                    continue
+                parsed.validate()
+                parsed_forms[f] = parsed
+                for i in parsed.image_references:
+                    if i not in images:
+                        images[i] = []
+                    images[i].append((m,f))
+                for i in parsed.audio_references:
+                    if i not in audio_files:
+                        audio_files[i] = []
+                    audio_files[i].append((m,f))
     sorted_images = SortedDict()
     sorted_audio = SortedDict()
     for k in sorted(images):
@@ -34,17 +35,18 @@ def get_multimedia_filenames(app):
     parsed_forms = {}
     images = []
     audio_files = []
-    for m in app.get_modules():
-        for f in m.get_forms():
-            parsed = f.wrapped_xform()
-            if not parsed.exists():
-                continue
-            parsed.validate()
-            parsed_forms[f] = parsed
-            for i in parsed.image_references:
-                if i not in images:
-                    images.append(i)
-            for a in parsed.audio_references:
-                if a not in audio_files:
-                    audio_files.append(a)
+    if app:
+        for m in app.get_modules():
+            for f in m.get_forms():
+                parsed = f.wrapped_xform()
+                if not parsed.exists():
+                    continue
+                parsed.validate()
+                parsed_forms[f] = parsed
+                for i in parsed.image_references:
+                    if i not in images:
+                        images.append(i)
+                for a in parsed.audio_references:
+                    if a not in audio_files:
+                        audio_files.append(a)
     return images, audio_files
