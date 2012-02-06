6b280200b13a55bf5a09e0193963b090f2f417d2
Biyeun Buczyk <biyeun@dimagi.com>
2012-02-06 16:22:08 -0200

    fixed util function to grab sorted images

--- corehq/apps/hqmedia/utils.py
+++ corehq/apps/hqmedia/utils.py
@@ -15,19 +15,21 @@ def get_sorted_multimedia_refs(app):
             parsed.validate()
             parsed_forms[f] = parsed
             for i in parsed.image_references:
-                if i and i not in images:
+                if i not in images:
                     images[i] = []
                 images[i].append((m,f))
             for i in parsed.audio_references:
-                if i and i not in audio_files:
+                if i not in audio_files:
                     audio_files[i] = []
                 audio_files[i].append((m,f))
     sorted_images = SortedDict()
     sorted_audio = SortedDict()
     for k in sorted(images):
-        sorted_images[k] = images[k]
+        if k:
+            sorted_images[k] = images[k]
     for k in sorted(audio_files):
-        sorted_audio[k] = audio_files[k]
+        if k:
+            sorted_audio[k] = audio_files[k]
     return sorted_images, sorted_audio
 
 def get_multimedia_filenames(app):
