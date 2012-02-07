d55142ed6cbd7acd04796d0e23986dff6eeb4581
Daniel Roberts <droberts@dimagi.com>
2012-02-07 14:02:10 -0200

    added get_ip util

--- dimagi/utils/web.py
+++ dimagi/utils/web.py
@@ -178,3 +178,35 @@ def json_request(params, lenient=True):
             else:
                 raise
     return d
+
+
+# get_ip was stolen verbatim from auditcare.utils
+# this is not intended to be an all-knowing IP address regex
+IP_RE = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+
+def get_ip(request):
+    """
+    Retrieves the remote IP address from the request data.  If the user is
+    behind a proxy, they may have a comma-separated list of IP addresses, so
+    we need to account for that.  In such a case, only the first IP in the
+    list will be retrieved.  Also, some hosts that use a proxy will put the
+    REMOTE_ADDR into HTTP_X_FORWARDED_FOR.  This will handle pulling back the
+    IP from the proper place.
+    """
+
+    # if neither header contain a value, just use local loopback
+    ip_address = request.META.get('HTTP_X_FORWARDED_FOR',
+        request.META.get('REMOTE_ADDR', '127.0.0.1'))
+    if ip_address:
+        # make sure we have one and only one IP
+        try:
+            ip_address = IP_RE.match(ip_address)
+            if ip_address:
+                ip_address = ip_address.group(0)
+            else:
+                # no IP, probably from some dirty proxy or other device
+                # throw in some bogus IP
+                ip_address = '10.0.0.1'
+        except IndexError:
+            pass
+    return ip_address
