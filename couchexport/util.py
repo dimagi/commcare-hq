970c52f5773504ecb82390aa5680273836cc5b96
Daniel Roberts <droberts@dimagi.com>
2012-02-13 16:30:24 -0200

    added a wrapper class for filter functions; makes it possibly for celery to serialize

--- couchexport/util.py
+++ couchexport/util.py
@@ -1,3 +1,5 @@
+import functools
+
 def intersect_filters(*args):
     filters = [fn for fn in args if fn]
     if filters:
@@ -9,3 +11,28 @@ def intersect_filters(*args):
     else:
         filter = None
     return filter
+
+class FilterFunction(object):
+    def __init__(self, function=None, **kwargs):
+        self.functions = []
+        if function:
+            self.add(function, **kwargs)
+
+    def add(self, function, **kwargs):
+        self.functions.append((function, kwargs))
+
+    def __iand__(self, other):
+        self.functions.extend(other.functions)
+        return self
+
+    def __and__(self, other):
+        f = FilterFunction()
+        f &= self
+        f &= other
+        return f
+
+    def __call__(self, doc):
+        if self.functions:
+            return intersect_filters(*[functools.partial(f, **kwargs) for (f, kwargs) in self.functions])(doc)
+        else:
+            return True
