f94b9e576e986a69020c875d1eff8084d54d2d05
Cory Zue <czue@dimagi.com>
2012-02-16 14:47:23 -0200

    don't explode if you pass a web user to an api case view

--- corehq/apps/cloudcare/api.py
+++ corehq/apps/cloudcare/api.py
@@ -5,7 +5,10 @@ from casexml.apps.case.models import CommCareCase
 
 def get_owned_cases(domain, user_id):
     user = CouchUser.get_by_user_id(user_id, domain)
-    owner_ids = user.get_owner_ids()
+    try:
+        owner_ids = user.get_owner_ids()
+    except AttributeError:
+        owner_ids = [user_id]
     keys = [[owner_id, False] for owner_id in owner_ids]
     cases = CommCareCase.view('case/by_owner', keys=keys, include_docs=True)
     return [case.get_json() for case in cases]
