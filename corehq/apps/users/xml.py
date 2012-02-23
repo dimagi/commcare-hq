3a5fc3b189c569104374432fbc8b399d0e1175a4
Cory Zue <czue@dimagi.com>
2012-02-23 12:12:37 -0200

    add "nature" attribute to phone responses

--- corehq/apps/users/xml.py
+++ corehq/apps/users/xml.py
@@ -9,12 +9,15 @@ Spec: https://bitbucket.org/javarosa/javarosa/wiki/UserRegistrationAPI
 # has been implemented quick and dirty
 from casexml.apps.phone import xml as phone_xml 
 from receiver import xml as receiver_xml 
+from receiver.xml import ResponseNature
 
 def get_response(user, created):
     if created:   text = "Thanks for registering! Your username is %s" % user.username
     else:         text = "Thanks for updating your information, %s." % user.username
         
-    response = receiver_xml.get_response_element(text)
+    nature = ResponseNature.SUBMIT_USER_REGISTERED if created else \
+             ResponseNature.SUBMIT_USER_UPDATED
+    response = receiver_xml.get_response_element(text, nature=nature)
     response.append(phone_xml.get_registration_element(user.to_casexml_user()))
     return phone_xml.tostring(response)
 
