93537ff6d0a1edfc58d9dbd0fe10fd7fcdfc1ce0
Cory Zue <czue@dimagi.com>
2012-02-23 18:03:44 -0200

    hopefully last fix for staticfiles

--- settings.py
+++ settings.py
@@ -92,7 +92,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.media",
     "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
-    'django.contrib.staticfiles.context_processors.static',
+    'django.core.context_processors.static',
     "corehq.util.context_processors.base_template", # sticks the base template inside all responses
     "corehq.util.context_processors.google_analytics",
 ]
