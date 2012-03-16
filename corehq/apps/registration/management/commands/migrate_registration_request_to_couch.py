21c29987437e2f2c20859df7f67b50dc3367c29d
Biyeun Buczyk <biyeun@dimagi.com>
2012-03-16 15:14:41 -0400

    changed the registration request migration script just a wee little bit

--- corehq/apps/registration/management/commands/migrate_registration_request_to_couch.py
+++ corehq/apps/registration/management/commands/migrate_registration_request_to_couch.py
@@ -14,19 +14,23 @@ class Command(LabelCommand):
         print "Migrating RegistrationRequest Model from django to couch"
 
         for request in django_requests:
+            existing_request = None
             try:
                 existing_request = RegistrationRequest.get_by_guid(request.activation_guid)
+            except Exception:
+                pass
+            try:
                 if not existing_request:
                     new_req = RegistrationRequest(tos_confirmed=request.tos_confirmed,
-                                                request_time=request.request_time,
-                                                request_ip=request.request_ip,
-                                                activation_guid=request.activation_guid,
-                                                confirm_time=request.confirm_time,
-                                                confirm_ip=request.confirm_ip,
-                                                domain=request.domain.name,
-                                                new_user_username=request.new_user.username,
-                                                requesting_user_username=request.requesting_user.username)
+                        request_time=request.request_time,
+                        request_ip=request.request_ip,
+                        activation_guid=request.activation_guid,
+                        confirm_time=request.confirm_time,
+                        confirm_ip=request.confirm_ip,
+                        domain=request.domain.name,
+                        new_user_username=request.new_user.username,
+                        requesting_user_username=request.requesting_user.username)
                     new_req.save()
             except Exception as e:
                 print "There was an error migrating a registration request with guid %s." % request.activation_guid
-                print "Error: %s", e
+                print "Error: %s" % e
