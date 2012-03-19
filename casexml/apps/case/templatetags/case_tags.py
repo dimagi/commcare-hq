f9fa38c01a36b7793a4a6ae92756158625ec55fb
Biyeun Buczyk <biyeun@dimagi.com>
2012-03-19 14:51:23 -0400

    modified case tags to deal with timezones

--- casexml/apps/case/templatetags/case_tags.py
+++ casexml/apps/case/templatetags/case_tags.py
@@ -2,6 +2,7 @@ import types
 from datetime import date, datetime
 from django import template
 from django.utils.html import escape
+import pytz
 from casexml.apps.case.models import CommCareCase
 from django.template.loader import render_to_string
 from couchdbkit.exceptions import ResourceNotFound
@@ -9,12 +10,12 @@ from couchdbkit.exceptions import ResourceNotFound
 register = template.Library()
 
 @register.simple_tag
-def render_case(case):
+def render_case(case, timezone=pytz.utc):
     if isinstance(case, basestring):
         # we were given an ID, fetch the case
         case = CommCareCase.get(case)
     
-    return render_to_string("case/partials/single_case.html", {"case": case})
+    return render_to_string("case/partials/single_case.html", {"case": case, "timezone": timezone})
     
     
 @register.simple_tag
