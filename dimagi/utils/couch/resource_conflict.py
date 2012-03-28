5a697c216643c6e632a4da4cc97b9f38879fde99
Daniel Roberts <droberts@dimagi.com>
2012-03-28 16:46:36 -0400

    add retry_resource decorator

--- dimagi/utils/couch/resource_conflict.py
+++ dimagi/utils/couch/resource_conflict.py
@@ -1,4 +1,5 @@
 from couchdbkit.exceptions import ResourceConflict
+from django.utils.functional import wraps
 
 def repeat(fn, n):
     for _ in range(n):
@@ -6,3 +7,16 @@ def repeat(fn, n):
             return fn()
         except ResourceConflict:
             pass
+
+
+def retry_resource(n):
+    def decorator(fn):
+        @wraps(fn)
+        def new_fn(*args, **kwargs):
+            for _ in range(n):
+                try:
+                    return fn(*args, **kwargs)
+                except ResourceConflict:
+                    pass
+        return new_fn
+    return decorator
