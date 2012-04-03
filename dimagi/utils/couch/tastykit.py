0ca8b2db16846899facd89bc408ece69569ac903
dmyung <dmyung@dimagi.com>
2012-04-03 14:58:42 -0400

    updating tastykit library for couchdbkit tastypie resources.  unified it into a single class to support datatables as well as provide more flexibility for urls and couch view access

--- dimagi/utils/couch/tastykit.py
+++ dimagi/utils/couch/tastykit.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from tastypie.bundle import Bundle
 from tastypie.paginator import Paginator
 from tastypie.resources import Resource
@@ -6,6 +7,9 @@ class CouchdbkitTastyPaginator(Paginator):
     """
     Override of the default Paginator for TastyPie - using couchdbkit, we can limit/paginate using the
     view API, but the slicing assumes we have a queryset, so to be more efficient, we do that in the get_list
+
+    Note, page() as it stands right now is handled by the resource itself.  page() in the tastypie sense assumes a lazily loaded queryset
+    while for couchdbkit, the data returned from the DB is exactly the data being shown.
     """
     def get_slice(self, limit, offset):
         """
@@ -13,34 +17,35 @@ class CouchdbkitTastyPaginator(Paginator):
         """
         return self.objects
 
-
-class DataTablesMixin(object):
+class CouchdbkitViewExecutor(object):
     """
-    Mixin to override the get_list functionality of get_list to support a datatables output
+    Helper class to pass into the meta of our Couchdbkit Resource to help run queries, be it map or reduce views,
+    as well as providing ways to manage parameters and special key values.
     """
-    def get_list(self, request, **kwargs):
-        """
-        Returns a serialized list of resources.
 
-        Calls ``obj_get_list`` to provide the data, then handles that result
-        set and serializes it.
+    def __init__(self):
 
-        Should return a HttpResponse (200 OK).
-        """
-        # TODO: Uncached for now. Invalidation that works for everyone may be
-        #       impossible.
-        objects = self.obj_get_list(request=request, **self.remove_api_resource_names(kwargs))
-        sorted_objects = self.apply_sorting(objects, options=request.GET)
+        #is reduce
+        #reduce level
+        #startkey, endkey
+        #key
+        #include_docs
+        #skip
+        #limit
+        #view_nme
 
-        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=self.get_resource_list_uri(), limit=self._meta.limit)
-        to_be_serialized = paginator.page()
+        pass
 
-        # Dehydrate the bundles in preparation for serialization.
-        bundles = [self.build_bundle(obj=obj, request=request) for obj in to_be_serialized['objects']]
-        to_be_serialized['aaData'] = [self.full_dehydrate(bundle) for bundle in bundles]
-        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
-        return self.create_response(request, to_be_serialized)
 
+
+    def run_query(self, request, db, view_name, skip, limit):
+        # results = self._meta.doc_class.view(self._meta.view_name, include_docs=True, skip=skip_option, limit=limit_option).all()
+
+        return None
+
+
+    def get_total(self):
+        pass
 
 class CouchdbkitResource(Resource):
     """
@@ -57,12 +62,19 @@ class CouchdbkitResource(Resource):
 
     Note: This assumes you want to use a CouchdbKit backed Document class, if you need to just use pure json from couch,
     or call reduced views, then you'll need to adjust how the deserialization works.
+
+
+    In order for this to work with datatables, you must override the sAjaxDataProp from aaData to objects
     """
     def _db(self):
         """
         Using the doc_class, get the underlying db.
         """
-        return self._meta.doc_class.get_db()
+
+        if hasattr(self._meta, 'couchdb'):
+            return self._meta.couchdb
+        else:
+            return self._meta.doc_class.get_db()
 
     def _doctype(self):
         """
@@ -88,32 +100,106 @@ class CouchdbkitResource(Resource):
 
         return self._build_reverse_url("api_dispatch_detail", kwargs=kwargs)
 
+    def call_view(self, request, **kwargs):
+        """
+        Returns the db.view() function, but with all the custom logic necessary to run.
+
+        This calls the default view in its entirety, your implementation should call it with more precision.
+        """
+        limit_option, skip_option = self._get_limit_skip(request)
+        view_call = self._meta.doc_class.view(self._meta.view_name, include_docs=True, skip=skip_option, limit=limit_option)
+        return view_call
+
+
     def _get_limit_skip(self, request):
         """
-        Tastypie api assumes offset as limit, but couchland it's skip
+        Tastypie api assumes offset as limit, but couchland it's skip.  This checks DataTables inbound data
+        but also does regular params as well.
         """
         if request is not None:
-            limit_option = request.GET.get('limit', self._meta.limit)
-            skip_option = request.GET.get('offset', 0)
+            if request.GET.get('iDisplayLength', None) is not None:
+                limit_option = request.GET['iDisplayLength']
+            else:
+                limit_option = request.GET.get('limit', self._meta.limit)
+
+            if request.GET.get('iDisplayStart', None) is not None:
+                skip_option = request.GET.get('iDisplayStart', 0)
+            else:
+                skip_option = request.GET.get('offset', 0)
         else:
             limit_option = self._meta.limit
             skip_option = 0
         return limit_option, skip_option
 
 
-    def get_object_list(self, request):
+    def ms_from_timedelta(self, td):
+        """
+        Given a timedelta object, returns a float representing milliseconds
+        """
+        return (td.seconds * 1000) + (td.microseconds / 1000.0)
+
+    def compute_totals(self, request, **kwargs):
+        """
+        For computing the "x out of y entries" section, the view needs to be called again
+
+        This ought to be overriden too
+        """
+
+        print self.generate_cache_key(**kwargs)
+
+        if hasattr(self, '_totals'):
+            return self._totals
+        else:
+            if request.GET.get('username', None) is not None:
+                ret =  self._meta.doc_class.view(self._meta.view_name, key=request.GET['username']).count()
+            else:
+                ret =  self._meta.doc_class.view(self._meta.view_name).count()
+            self._totals = ret
+            return ret
+
+    def obj_get_list(self, request=None, **kwargs):
         """
         Assumes that the document type and the view correspond
         """
         #tune the limits for couch instead of grabbing everything
 
-        limit_option, skip_option = self._get_limit_skip(request)
-        results = self._meta.doc_class.view(self._meta.view_name, include_docs=True, skip=skip_option, limit=limit_option).all()
-        return [x for x in results]
+        results = self.call_view(request, **kwargs).all()
+        return list(results)
 
-    def obj_get_list(self, request=None, **kwargs):
-        #no filtering
-        return self.get_object_list(request)
+
+
+    def get_list(self, request, **kwargs):
+        """
+        Returns a serialized list of resources.
+
+        Calls ``obj_get_list`` to provide the data, then handles that result
+        set and serializes it.
+
+        Should return a HttpResponse (200 OK).
+        """
+        # TODO: Uncached for now. Invalidation that works for everyone may be
+        #       impossible.
+        objects = self.obj_get_list(request=request, **self.remove_api_resource_names(kwargs))
+        sorted_objects = self.apply_sorting(objects, options=request.GET)
+
+        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=self.get_resource_list_uri(), limit=self._meta.limit)
+        to_be_serialized = paginator.page()
+
+        # Dehydrate the bundles in preparation for serialization.
+        bundles = [self.build_bundle(obj=obj, request=request) for obj in to_be_serialized['objects']]
+        to_be_serialized['objects'] = [self.full_dehydrate(bundle) for bundle in bundles]
+        total = len(to_be_serialized['objects'])
+        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
+        display = len(to_be_serialized['objects'])
+        #print to_be_serialized.keys()
+
+
+        #These are properties traditionally set by the paginator.page() call
+        to_be_serialized['iTotalRecords'] = self.compute_totals(request, **kwargs)
+        to_be_serialized['iTotalDisplayRecords'] = self.compute_totals(request, **kwargs)
+        to_be_serialized['sEcho'] = request.GET.get('sEcho',1)
+
+        return self.create_response(request, to_be_serialized)
 
     def obj_get(self, request=None, **kwargs):
         """
@@ -138,54 +224,3 @@ class CouchdbkitResource(Resource):
 
     def rollback(self, bundles):
         pass
-
-
-class ParameterizedResource(CouchdbkitResource):
-
-    """
-    More refined CouchDBkitResource, converts return values to datatables.net compatible json. Work in progress, as this class was created because DataTables mixin didn't seem to work.
-    Also, datatables params can be tweaked so serialization alteration here needn't be necessary.
-    """
-    def get_object_list(self, request):
-        """
-        Assumes that the document type and the view correspond
-        """
-        limit_option, skip_option = self._get_limit_skip(request)
-        results = self._meta.doc_class.view(self._meta.view_name, include_docs=True, limit=limit_option, skip=skip_option).all()
-        return results
-
-    def obj_get_list(self, request=None, **kwargs):
-        limit_option, skip_option = self._get_limit_skip(request)
-        results = self._meta.doc_class.view(self._meta.view_name, include_docs=True, key=request.user.username,  skip=skip_option, limit=limit_option).all()
-        return results
-
-    def obj_get(self, request=None, **kwargs):
-        """
-        Assume a fully formed couchdbkit document.
-        """
-        document = self._doctype().get(kwargs['pk'])
-        return document
-    def get_list(self, request, **kwargs):
-        """
-        Returns a serialized list of resources.
-
-        Calls ``obj_get_list`` to provide the data, then handles that result
-        set and serializes it.
-
-        Should return a HttpResponse (200 OK).
-        """
-        # TODO: Uncached for now. Invalidation that works for everyone may be
-        #       impossible.
-        objects = self.obj_get_list(request=request, **self.remove_api_resource_names(kwargs))
-        sorted_objects = self.apply_sorting(objects, options=request.GET)
-
-        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=self.get_resource_list_uri(), limit=self._meta.limit)
-        to_be_serialized = paginator.page()
-        to_be_serialized['aaData'] = to_be_serialized['objects']
-        del(to_be_serialized['objects'])
-
-        # Dehydrate the bundles in preparation for serialization.
-        bundles = [self.build_bundle(obj=obj, request=request) for obj in to_be_serialized['aaData']]
-        to_be_serialized['aaData'] = [self.full_dehydrate(bundle) for bundle in bundles]
-        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
-        return self.create_response(request, to_be_serialized)
