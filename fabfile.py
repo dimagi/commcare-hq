f5cbfe88c233a3d9622f3d4d2688229d93d424c7
Cory Zue <czue@dimagi.com>
2012-05-16 16:22:47 +0530

    update fabfile for india server config. deploy now fully functional

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,11 @@ from fabric.contrib import console, files
 from fabric import utils
 import os
 
+# these defaults can be overridden if necessary
 env.code_repo = 'git://github.com/dimagi/commcare-hq.git'
 env.jython_home = "/usr/bin/jython"
+env.restart_server = True
+env.service_manager = "upstart"
 
 def _join(*args):
     """
@@ -24,7 +27,7 @@ def production():
     env.hosts = ['10.84.168.241']
     env.environment = 'production'
     env.user = prompt("Username: ", default=env.user)
-    env.restart_server = True
+    env.make_bootstrap_command = 'python manage.py make_bootstrap direct-lessc'
 
 def migration():
     """pull from staging branch into production to do a data migration"""
@@ -43,6 +46,7 @@ def staging():
     env.hosts = ['192.168.7.223']
     env.environment = 'staging'
     env.user = prompt("Username: ", default='dimagivm')
+    env.make_bootstrap_command = 'python manage.py make_bootstrap direct-lessc'
 
 def india():
     """Our production server in India."""
@@ -56,6 +60,8 @@ def india():
     env.hosts = ['220.226.209.82']
     env.environment = 'india'
     env.user = prompt("Username: ", default=env.user)
+    env.service_manager = "supervisor"
+    env.make_bootstrap_command = 'python manage.py make_bootstrap'
 
 def enter_virtualenv():
     """
@@ -71,11 +77,11 @@ def enter_virtualenv():
 
 def preindex_views():
     with cd(env.pre_code_root):
-        _update_code()
+        update_code()
         with enter_virtualenv():
             sudo('nohup python manage.py sync_prepare_couchdb > preindex_views.out 2> preindex_views.err', user=env.sudo_user)
 
-def _update_code():
+def update_code():
     sudo('git pull', user=env.sudo_user)
     sudo('git checkout %(code_branch)s' % env, user=env.sudo_user)
     sudo('git pull', user=env.sudo_user)
@@ -118,6 +124,17 @@ def upload_supervisor_conf():
     sudo('chmod -R g+w %s' % tmp_destination)
     sudo('mv -f %s %s' % (tmp_destination, destination), user=env.sudo_user)
 
+def update_env():
+    require('root', provided_by=('staging', 'production', 'india'))
+    with enter_virtualenv():
+        sudo('pip install -r requirements.txt', user=env.sudo_user)
+        sudo(env.make_bootstrap_command, user=env.sudo_user)
+        sudo('python manage.py sync_finish_couchdb', user=env.sudo_user)
+        sudo('python manage.py syncdb --noinput', user=env.sudo_user)
+        sudo('python manage.py migrate --noinput', user=env.sudo_user)
+        sudo('python manage.py collectstatic --noinput', user=env.sudo_user)
+        sudo('rm -f tmp.sh resource_versions.py; python manage.py printstatic > tmp.sh; bash tmp.sh > resource_versions.py', user=env.sudo_user)
+
 def deploy():
     """ deploy code to remote host by checking out the latest via git """
     require('root', provided_by=('staging', 'production', 'india'))
@@ -126,34 +143,32 @@ def deploy():
             utils.abort('Deployment aborted.')
 
     with cd(env.code_root):
-        _update_code()
-        with enter_virtualenv():
-            sudo('pip install -r requirements.txt', user=env.sudo_user)
-            sudo('python manage.py make_bootstrap direct-lessc', user=env.sudo_user)
-            sudo('python manage.py sync_finish_couchdb', user=env.sudo_user)
-            sudo('python manage.py syncdb --noinput', user=env.sudo_user)
-            sudo('python manage.py migrate --noinput', user=env.sudo_user)
-            sudo('python manage.py collectstatic --noinput', user=env.sudo_user)
-            sudo('rm -f tmp.sh resource_versions.py; python manage.py printstatic > tmp.sh; bash tmp.sh > resource_versions.py', user=env.sudo_user)
+        update_code()
+        update_env()
         # remove all .pyc files in the project
         sudo("find . -name '*.pyc' -delete")
         
-    if env.environment == "production" and env.restart_server:
+    if env.restart_server:
         service_restart()
-
-
 
 def service_restart():
     """
-    restart cchq_www service on remote host.  This will call a stop, reload the initctl to
-    have any config file updates be reloaded into intictl, then start cchqwww again.
-
+    Restart cchq services on remote host.
     """
-    require('root', provided_by=('staging', 'production'))
-    with settings(sudo_user="root"):
-        sudo('stop cchq_www', user=env.sudo_user)
-        sudo('initctl reload-configuration', user=env.sudo_user)
-        sudo('start cchq_www', user=env.sudo_user)
+    require('service_manager', provided_by=('staging', 'production', 'india'))
+    assert env.service_manager in ("upstart", "supervisor")
+    
+    if env.service_manager == "upstart":
+        with settings(sudo_user="root"):
+            sudo('stop cchq_www', user=env.sudo_user)
+            sudo('initctl reload-configuration', user=env.sudo_user)
+            sudo('start cchq_www', user=env.sudo_user)
+    else:
+        # for supervisor we update the templates each time
+        upload_supervisor_conf()
+        with settings(sudo_user="root"):
+            sudo('supervisorctl update', user=env.sudo_user)
+            sudo('supervisorctl restart all', user=env.sudo_user)
 
 def service_stop():
     """
