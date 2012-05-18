e4f9a31374ce51a85a9f8369fb3d0c05656b89b7
Daniel Roberts <droberts@dimagi.com>
2012-05-18 17:36:11 +0530

    tweak users

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -391,6 +391,7 @@ class CouchUser(Document, DjangoUserMixin, UnicodeMixIn):
             return {
                 'WebUser': WebUser,
                 'CommCareUser': CommCareUser,
+                'FakeUser': FakeUser,
             }[source['doc_type']].wrap(source)
 
     @classmethod
