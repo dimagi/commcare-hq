ffe08415b1a8a7bcc3257999608e1e0e99790162
Biyeun Buczyk <biyeun@dimagi.com>
2012-05-22 00:42:35 -0400

    hide case management report

--- settings.py
+++ settings.py
@@ -488,8 +488,7 @@ STANDARD_REPORT_MAP = {
         'corehq.apps.reports.standard.ApplicationStatusReport',
         'corehq.apps.receiverwrapper.reports.SubmissionErrorReport',
         'phonelog.reports.FormErrorReport',
-        'phonelog.reports.DeviceLogDetailsReport',
-        'corehq.apps.reports.type.deployments.CaseManagementReport'
+        'phonelog.reports.DeviceLogDetailsReport'
     ]
 }
 
