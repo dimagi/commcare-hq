4637978053a359f34c50a93736fc6d570d249016
Daniel Roberts <droberts@dimagi.com>
2012-05-22 18:27:54 +0530

    jshint'd case_management.js

--- corehq/apps/data_interfaces/static/ko/data_interfaces/case_management.js
+++ corehq/apps/data_interfaces/static/ko/data_interfaces/case_management.js
@@ -1,4 +1,5 @@
 var CaseManagement = function (owners, cases, receiverUrl, enddate) {
+    'use strict';
     var self = this;
     self.selected_cases = ko.observableArray();
     self.selected_owners = ko.observableArray();
@@ -9,25 +10,26 @@ var CaseManagement = function (owners, cases, receiverUrl, enddate) {
 
     enddate = new Date(enddate);
     var now = new Date();
-    self.on_today = (enddate.toDateString() == now.toDateString());
+    self.on_today = (enddate.toDateString() === now.toDateString());
 
     function getUsername(userid) {
-        for (var i in self.available_owners) {
-            if (self.available_owners[i].userid == userid)
+        for (var i = 0; i < self.available_owners.length; i++) {
+            if (self.available_owners[i].userid === userid) {
                 return self.available_owners[i].username;
+            }
         }
         return "Unknown";
     }
 
-    var updateCaseRow = function(case_id, user_id) {
+    var updateCaseRow = function (case_id, user_id) {
         return function(data, textStatus) {
-            var $checkbox = $('#case-management input[data-caseid="'+case_id+'"].selected-commcare-case'),
+            var $checkbox = $('#case-management input[data-caseid="' + case_id + '"].selected-commcare-case'),
                 username = getUsername(user_id),
                 date_message = (self.on_today) ? '<span title="0"></span>' : '<span class="label label-warning" title="0">Out of range of filter. Will not appear on page refresh.</span>';
             $checkbox.data('owner', user_id);
             var $row = $checkbox.parent().parent();
-            $row.find('td:nth-child(4)').html(username+' <span class="label label-info" title="'+username+'">updated</span>');
-            $row.find('td:nth-child(5)').html('Today '+date_message);
+            $row.find('td:nth-child(4)').html(username + ' <span class="label label-info" title="' + username + '">updated</span>');
+            $row.find('td:nth-child(5)').html('Today ' + date_message);
             $checkbox.attr("checked", false).change();
         };
     };
@@ -39,13 +41,15 @@ var CaseManagement = function (owners, cases, receiverUrl, enddate) {
         var ind = self.selected_cases().indexOf(caseID);
         if ($checkbox.is(':checked')) {
             $checkbox.parent().parent().addClass('active');
-            if (ind < 0)
+            if (ind < 0) {
                 self.selected_cases.push(caseID);
+            }
             self.selected_owners.push(ownerID);
-        } else{
+        } else {
             $checkbox.parent().parent().removeClass('active');
-            if (ind >= 0)
-                self.selected_cases.splice(ind,1);
+            if (ind >= 0) {
+                self.selected_cases.splice(ind, 1);
+            }
             self.selected_owners.splice(self.selected_owners().indexOf(ownerID), 1);
         }
     };
@@ -59,7 +63,7 @@ var CaseManagement = function (owners, cases, receiverUrl, enddate) {
             return false;
         }
 
-        for (var i in self.selected_cases()) {
+        for (var i = 0; i < self.selected_cases().length; i++) {
             var case_id = self.selected_cases()[i];
             var selected_case = self.cases[case_id],
                 xform;
@@ -83,10 +87,11 @@ var CaseManagement = function (owners, cases, receiverUrl, enddate) {
 ko.bindingHandlers.caseReassignmentForm = {
     update: function(element, valueAccessor) {
         var value = valueAccessor()();
-        if (value.length > 0)
+        if (value.length > 0) {
             $(element).slideDown();
-        else
+        } else {
             $(element).slideUp();
+        }
     }
 };
 
@@ -94,9 +99,10 @@ ko.bindingHandlers.caseReassignmentOwners = {
     update: function(element, valueAccessor) {
         var value = valueAccessor()();
         var unique_owners = _.unique(value);
-        if (unique_owners.length == 1)
+        if (unique_owners.length === 1) {
             $(element).val(unique_owners[0]);
-        else
+        } else {
             $(element).val("");
+        }
     }
 };
