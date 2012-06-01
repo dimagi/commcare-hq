429eb2b8186ff65de041b755675f40702802b221
Biyeun Buczyk <biyeun@dimagi.com>
2012-06-01 17:48:03 +0530

    renamed hq bootstrap folder, static tag now handles bootstrap files properly for versioning

--- corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
+++ corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
@@ -72,7 +72,10 @@ except ImportError:
     resource_versions = {}
 @register.simple_tag
 def static(url):
-    version = resource_versions.get(url)
+    resource_url = url
+    if "hq_bootstrap" in url:
+        resource_url = resource_url.replace("/css/", "/less/").replace(".css", ".less").replace("hq_bootstrap/", "hq-bootstrap/")
+    version = resource_versions.get(resource_url)
     url = settings.STATIC_URL + url
     if version:
         url += "?version=%s" % version
