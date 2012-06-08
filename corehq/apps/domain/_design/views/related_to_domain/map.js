8ada8e9dfde1b73dd8593a14be32feabd5b601cd
timbauman <tbauman@dimagi.com>
2012-06-08 17:25:53 -0400

    Adding url/view hooks

--- corehq/apps/domain/_design/views/related_to_domain/map.js
+++ corehq/apps/domain/_design/views/related_to_domain/map.js
@@ -1,5 +1,18 @@
 function(doc) {
-    if (doc.domain) {
+    var DOC_TYPES = { // use this object to look up case types
+        'ExceptionRecord': false,
+        'MessageLog': false,
+        'RegistrationRequest': false,
+        'SMSLog': false,
+        'XFormInstance': false,
+        'CommCareUser': false,
+        'CommCareCase': false,
+        'UserRole': true,
+        'Application': true,
+        'RemoteApp': true
+    };
+
+    if (doc.domain && DOC_TYPES[doc.doc_type]) {
         emit(doc.domain, doc);
     }
 }
