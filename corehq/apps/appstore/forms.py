75bcb77b1aea97e00fd7430e40abb9d6dcd1405b
Spencer <stank@dimagi.com>
2012-06-18 16:48:40 -0400

    added a prototype of the review system to the app store info pages

--- corehq/apps/appstore/forms.py
+++ corehq/apps/appstore/forms.py
@@ -6,5 +6,29 @@ from corehq.apps.domain.utils import new_domain_re
 class AddReviewForm(forms.Form):
 
     review_rating = forms.IntegerField(max_value=5, min_value=0)
+    review_name = forms.CharField(label="nickname", max_length=25)
     review_title = forms.CharField(label="Title", max_length=35)
     review_info = forms.CharField(label="Review (Optional)", max_length=500, required=False)
+
+    def clean_review_name(self):
+        data = self.cleaned_data['review_name'].strip()
+        return data
+
+    def clean_review_info(self):
+        data = self.cleaned_data['review_info']
+        return data
+
+    def clean_review_rating(self):
+      data = self.cleaned_data['review_rating']
+      return data
+
+    def clean_review_title(self):
+      data = self.cleaned_data['review_title']
+      return data
+
+    def clean(self):
+        for field in self.cleaned_data:
+            if isinstance(self.cleaned_data[field], basestring):
+                self.cleaned_data[field] = self.cleaned_data[field].strip()
+        return self.cleaned_data
+
--- corehq/apps/appstore/models.py
+++ corehq/apps/appstore/models.py
@@ -3,8 +3,19 @@ import util
 
 
 class Review(Document):
+
+    domain = StringProperty()
     title = StringProperty() # for example "Great App"
     rating = IntegerProperty() # for example "4 (out of 5)"
     user = StringProperty() # for example "stank"
     date_published = DateTimeProperty()
     info = StringProperty() # for example "this app has a few bugs, but it works great overall!"
+    nickname = StringProperty()
+
+    @classmethod
+    def get_by_app(cls, name):
+        result = cls.view("appstore/by_app",
+            key=name,
+            reduce=False,
+            include_docs=True).all()
+        return result
