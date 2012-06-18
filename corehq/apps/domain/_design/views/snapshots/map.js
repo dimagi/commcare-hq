c3cc1d869e1159ade0f477e66fdb20c6c6d6043b
timbauman <tbauman@dimagi.com>
2012-06-18 09:07:58 -0400

    sorting snapshots by date

--- corehq/apps/domain/_design/views/snapshots/map.js
+++ corehq/apps/domain/_design/views/snapshots/map.js
@@ -1,5 +1,5 @@
 function (doc) {
-    if (doc.doc_type == 'Domain' && doc.is_snapshot) {
-        emit(doc.original_doc, doc);
+    if (doc.doc_type == 'Domain' && doc.is_snapshot && doc.published) {
+        emit([doc.original_doc, Date.parse(doc.snapshot_time).getTime()], null);
     }
 }
