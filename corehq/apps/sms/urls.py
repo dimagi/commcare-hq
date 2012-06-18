b50b845ff876b5a624c85c6f9a3cbeb9c6044dfb
Giovanni Capalbo <gcapalbo@dimagi.com>
2012-06-18 14:20:12 -0400

    Adds basic message testing capabilities

--- corehq/apps/sms/urls.py
+++ corehq/apps/sms/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns('corehq.apps.sms.views',
     url(r'^post/?$', 'post', name='sms_post'),
     url(r'^send_to_recipients/$', 'send_to_recipients'),
     url(r'^compose/$', 'compose_message', name='sms_compose_message'),
+    url(r'^message_test/(?P<phone_number>\d+)/$', 'message_test', name='message_test'),
     url(r'^$', 'messaging', name='messaging'),
 )
