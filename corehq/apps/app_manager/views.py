e498b68e69fe894021d76f243079bb694bd664df
Daniel Roberts <droberts@dimagi.com>
2012-07-06 14:11:36 -0400

    display error if success message contains bad thing

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -5,6 +5,7 @@ import uuid
 from wsgiref.util import FileWrapper
 import zipfile
 from corehq.apps.app_manager.const import APP_V1
+from corehq.apps.app_manager.success_message import SuccessMessage
 from corehq.apps.domain.models import Domain
 from couchexport.export import FormattedRow
 from couchexport.models import Format
@@ -360,6 +361,12 @@ def get_apps_base_context(request, domain, app):
             limit=1,
         ).one()
         latest_app_version = latest_app_version.version if latest_app_version else -1
+        for lang in app.success_message:
+            try:
+                SuccessMessage(app.success_message.get(lang), '').check_message()
+            except Exception as e:
+                messages.error(request, "Your success message is malformed: %s is not a keyword" % e)
+
     else:
         latest_app_version = -1
     context = locals()
