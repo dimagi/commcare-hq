2029d17b31d2fa0930c7f36fcf1760bc14047527
Spencer <stank@dimagi.com>
2012-07-09 11:55:39 -0400

    took out organizations and advanced search from project store

--- corehq/apps/appstore/urls.py
+++ corehq/apps/appstore/urls.py
@@ -7,14 +7,16 @@ urlpatterns = patterns('corehq.apps.appstore.views',
     url(r'^filter/(?P<filter_by>[\w]+)/', 'filter_choices', name='appstore_filter_choices'),
     url(r'^$', 'appstore', name='appstore'),
 #    url(r'^$', "default", name="appstore_interfaces_default"),
-    url(r'^async/filters/store/(?P<slug>[\w_]+)/$', 'report_dispatcher', name="appstore_interface_dispatcher", kwargs={
+    url(r'^store/appstore/async/filters/(?P<slug>[\w_]+)/$', 'report_dispatcher', name="appstore_interface_dispatcher", kwargs={
         'async_filters': True
     }),
-    url(r'^async/store/(?P<slug>[\w_]+)/$', 'report_dispatcher', name="async_report_dispatcher", kwargs={
+    url(r'^store/appstore/async/(?P<slug>[\w_]+)/$', 'report_dispatcher', name="async_report_dispatcher", kwargs={
         'async': True
     }),
     url(r'^store/(?P<slug>[\w_]+)/$', 'report_dispatcher', name="appstore_interface_dispatcher"),
-    url(r'^store/appstore/$', 'report_dispatcher', name="appstore_interfaces_default", kwargs={'slug':'appstore'}),
+    url(r'^store/advanced$', 'report_dispatcher', name="appstore_interfaces_default", kwargs={
+        'slug': 'advanced'
+    }),
     url(r'^(?P<domain>[\w\.-]+)/approve/$', 'approve_app', name='approve_appstore_app'),
     url(r'^(?P<domain>[\w\.-]+)/copyapp/', 'copy_snapshot_app', name='copy_snapshot_app'),
     url(r'^(?P<domain>[\w\.-]+)/copy/$', 'copy_snapshot', name='domain_copy_snapshot'),
