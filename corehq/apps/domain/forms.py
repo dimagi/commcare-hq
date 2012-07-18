20fb1299adf2f91b5c1dffa6b8ce3a9bc5d5efd9
Tim Bauman <tbauman@princeton.edu>
2012-07-18 11:02:12 -0400

    Fixing settings bug

--- corehq/apps/domain/forms.py
+++ corehq/apps/domain/forms.py
@@ -134,12 +134,12 @@ public domain.
             domain.is_test = self.cleaned_data['is_test'] == 'true'
             domain.is_shared = self.cleaned_data['is_shared']
             domain.description = self.cleaned_data['description']
-            if self.cleaned_data['deployment_date'] != '':
+            if 'deployment_date' in self.cleaned_data and self.cleaned_data['deployment_date'] != '':
                 year, month, day = map(int, self.cleaned_data['deployment_date'].split('-'))
                 domain.deployment_date = datetime.datetime(year, month, day)
             else:
                 domain.deployment_date = None
-            domain.phone_model = self.cleaned_data['phone_model']
+            domain.phone_model = self.cleaned_data.get('phone_model', '')
             domain.save()
             return True
         except Exception:
