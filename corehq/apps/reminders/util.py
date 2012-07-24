685e0308cc27e4a6cdaee9b8617fc98c61b06c6a
Giovanni Capalbo <gcapalbo@dimagi.com>
2012-07-24 12:33:40 -0400

    Excludes remote apps from survey list for now

--- corehq/apps/reminders/util.py
+++ corehq/apps/reminders/util.py
@@ -4,17 +4,18 @@ def get_form_list(domain):
     form_list = []
     for app in ApplicationBase.view("app_manager/applications_brief", startkey=[domain], endkey=[domain, {}]):
         latest_app = get_app(domain, app._id, latest=True)
-        lang = latest_app.langs[0]
-        for m in latest_app.get_modules():
-            for f in m.get_forms():
-                try:
-                    module_name = m.name[lang]
-                except Exception:
-                    module_name = m.name.items()[0][1]
-                try:
-                    form_name = f.name[lang]
-                except Exception:
-                    form_name = f.name.items()[0][1]
-                form_list.append({"code" :  f.unique_id, "name" : app.name + "/" + module_name + "/" + form_name})
+        if latest_app.doc_type == "Application":
+            lang = latest_app.langs[0]
+            for m in latest_app.get_modules():
+                for f in m.get_forms():
+                    try:
+                        module_name = m.name[lang]
+                    except Exception:
+                        module_name = m.name.items()[0][1]
+                    try:
+                        form_name = f.name[lang]
+                    except Exception:
+                        form_name = f.name.items()[0][1]
+                    form_list.append({"code" :  f.unique_id, "name" : app.name + "/" + module_name + "/" + form_name})
     return form_list
 
