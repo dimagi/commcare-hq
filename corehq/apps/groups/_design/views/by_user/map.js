4a7da921a5a01951156512490522f7d475fb89da
Tim Bauman <tbauman@princeton.edu>
2012-08-01 14:36:29 -0400

    Javascript has a triple equals sign

--- corehq/apps/groups/_design/views/by_user/map.js
+++ corehq/apps/groups/_design/views/by_user/map.js
@@ -1,6 +1,6 @@
 function(doc){
     var i;
-    if (doc.doc_type == "Group") {
+    if (doc.doc_type === "Group") {
     	for (i = 0; i < doc.users.length; i++) {
     		emit(doc.users[i], [doc.domain, doc.name]);
     	}
