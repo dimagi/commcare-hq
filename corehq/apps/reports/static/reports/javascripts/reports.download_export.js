b7743be5697ea626381dfa1d52d8062a64e01f05
Biyeun Buczyk <biyeun@dimagi.com>
2012-08-01 12:14:14 -0400

    reset this to what it was originally, still need it for case export

--- corehq/apps/reports/static/reports/javascripts/reports.download_export.js
+++ corehq/apps/reports/static/reports/javascripts/reports.download_export.js
@@ -4,7 +4,6 @@ var HQExportDownloader = function (options) {
     self.modal = options.modal || "#export-download-status";
     self.loadingIndicator = options.loadingIndicator || ".loading-indicator";
     self.loadedData = options.loadedData || ".loaded-data";
-    self.domain = options.domain;
 
     self.init = function () {
         $(function () {
@@ -12,15 +11,9 @@ var HQExportDownloader = function (options) {
                 var $modal = $(self.modal);
                 $modal.find(self.loadingIndicator).removeClass('hide');
                 $modal.find(self.loadedData).empty();
-                $modal.find('.modal-header h3 span').text($(this).data('appname')+$(this).data("formname"));
+                $modal.find('.modal-header h3 span').text($(this).data("formname"));
 
-                console.log($(this).data('exporttag'));
-                var download_url = $(this).data('dlocation') + '&export_tag=["' +
-                        self.domain + '","'+$(this).data('xmlns')+'"]'+ "&app_id=" + $(this).data('appid') +
-                        "&filename="+$(this).data('formname');
-                console.log(download_url);
-
-                $.getJSON(download_url, function (d) {
+                $.getJSON($(this).data('dlocation'), function (d) {
                     var autoRefresh = '';
                     var pollDownloader = function () {
                         if ($('#ready_'+d.download_id).length == 0)
@@ -43,4 +36,36 @@ var HQExportDownloader = function (options) {
             });
         });
     };
+
+//    $('.export-action-download').click(function() {
+//        var $modalBody = $("#export-download-status .modal-body");
+//        $modalBody.find('.loading-indicator').removeClass('hide');
+//        $modalBody.find('.loaded-data').empty();
+//        $("#export-download-status .modal-header h3 span").text($(this).data("formname"));
+//        $.ajax({
+//            type: "GET",
+//            dataType: "json",
+//            url: $(this).data('dlocation'),
+//            success: function(d) {
+//                var autoRefresh = '';
+//                var pollDownloader = function () {
+//                    console.log($('#ready_'+d.download_id).length);
+//                    if ($('#ready_'+d.download_id).length == 0)
+//                    {
+//                        $.get(d.download_url, function(data) {
+//                            $modalBody.find('.loaded-data').html(data);
+//                        });
+//                    } else {
+//                        $modalBody.find('.loading-indicator').addClass('hide');
+//                        clearInterval(autoRefresh);
+//                    }
+//                };
+//                $("#export-download-status").on('hide', function () {
+//                    clearInterval(autoRefresh);
+//                });
+//
+//                autoRefresh = setInterval(pollDownloader, 2000);
+//            }
+//        });
+//    });
 };
