34738086d04e9400080580d3dcd77a40bfa37356
Daniel Roberts <droberts@dimagi.com>
2012-08-02 13:52:31 -0400

    patch appstore views: I think they were breaking because of how _stats was processing the output

--- corehq/apps/appstore/_design/views/by_app/map.js
+++ corehq/apps/appstore/_design/views/by_app/map.js
@@ -1,5 +1,5 @@
-function(doc) {
-    if (doc.doc_type === 'Review'){
-        emit(doc.original_doc, doc.rating);
+function (doc) {
+    if (doc.doc_type === 'Review') {
+        emit(doc.original_doc, doc.rating || 0);
     }
 }
