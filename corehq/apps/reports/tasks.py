1c2335f84a2e072fbb7fcd58c0fe4fa7453ef30f
Biyeun Buczyk <biyeun@dimagi.com>
2012-08-15 14:35:38 -0400

    fix bug where user has been removed from a domain that they still have scheduled reports for

--- corehq/apps/reports/tasks.py
+++ corehq/apps/reports/tasks.py
@@ -1,7 +1,10 @@
 from datetime import datetime
+import logging
 from smtplib import SMTPRecipientsRefused
 from celery.schedules import crontab
 from celery.decorators import periodic_task, task
+from django.http import Http404
+from corehq.apps.domain.models import Domain
 from corehq.apps.reports.models import DailyReportNotification
 from corehq.apps.users.models import CouchUser
 from corehq.apps.reports.schedule import config
@@ -30,14 +33,30 @@ def weekly_reports():
 def send_report(scheduled_report, user):
     if isinstance(user, dict):
         user = CouchUser.wrap_correctly(user)
-    report = ScheduledReportFactory.get_report(scheduled_report.report_slug)
-    body = report.get_response(user, scheduled_report.domain)
-    email = user.get_email()
-    if email:
-        send_HTML_email("%s [%s]" % (report.title, scheduled_report.domain), email,
-                    html2text(body), body)
-    else:
-        raise SMTPRecipientsRefused(None)
+    try:
+        report = ScheduledReportFactory.get_report(scheduled_report.report_slug)
+        body = report.get_response(user, scheduled_report.domain)
+        email = user.get_email()
+        if email:
+            send_HTML_email("%s [%s]" % (report.title, scheduled_report.domain), email,
+                        html2text(body), body)
+        else:
+            raise SMTPRecipientsRefused(None)
+    except Http404:
+        # Scenario: user has been removed from the domain that they have scheduled reports for.
+        # Do a scheduled report cleanup
+        user_id = unicode(user.get_id)
+        domain = Domain.get_by_name(scheduled_report.domain)
+        user_ids = [user.get_id for user in domain.all_users()]
+        if user_id not in user_ids:
+            # remove the offending user from the scheduled report
+            scheduled_report.user_ids.remove(user_id)
+            if len(scheduled_report.user_ids) == 0:
+                # there are no users with appropriate permissions left in the scheduled report,
+                # so remove it
+                scheduled_report.delete()
+            else:
+                scheduled_report.save()
 
 def _run_reports(reps):
     for rep in reps:
