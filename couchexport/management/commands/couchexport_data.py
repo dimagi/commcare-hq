14f0f4e6627057b625d3929e4bce90fd09a6865c
Cory Zue <czue@dimagi.com>
2012-08-20 22:17:44 +0200

    move the group export logic from mgmt command to a separate function

--- couchexport/management/commands/couchexport_data.py
+++ couchexport/management/commands/couchexport_data.py
@@ -1,11 +1,5 @@
 from django.core.management.base import LabelCommand, CommandError
-from couchexport.models import GroupExportConfiguration, SavedBasicExport
-from couchdbkit.exceptions import ResourceNotFound
-from couchexport.export import export
-from datetime import datetime
-import os
-import json
-import tempfile
+from couchexport.groupexports import export_for_group
 
 class Command(LabelCommand):
     help = "Runs an export based on a supplied configuration."
@@ -17,32 +11,5 @@ class Command(LabelCommand):
             
         export_id = args[0]
         output_dir = args[1]
-        try:
-            config = GroupExportConfiguration.get(export_id)
-        except ResourceNotFound:
-            raise CommandError("Couldn't find an export with id %s" % export_id)
-        
-        for export_config in config.full_exports:
-            print "exporting %s to %s" % (export_config.name, output_dir)
-            # special case couch storage
-            if output_dir == "couch":
-                fd, path = tempfile.mkstemp()
-                with os.fdopen(fd, 'wb') as f:
-                    export(export_config.index, f, format=export_config.format)
-                # got the file, now rewrite it to couch
-                saved = SavedBasicExport.view("couchexport/saved_exports", 
-                                              key=json.dumps(export_config.index),
-                                              include_docs=True,
-                                              reduce=False).one()
-                if not saved: 
-                    saved = SavedBasicExport(configuration=export_config)
-                    saved.save()
-                with open(path, "rb") as f:
-                    saved.put_attachment(f.read(), export_config.filename)
-                    saved.last_updated = datetime.utcnow()
-                    saved.save()
-                os.remove(path)
-            else:
-                with open(os.path.join(output_dir, export_config.filename), "wb") as f:
-                    export(export_config.index, f, format=export_config.format)
+        export_for_group(export_id, output_dir)
                 
