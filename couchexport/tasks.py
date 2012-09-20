8ee68b0820ecfd4f54990331da1c3819482a6e0c
dmyung <dmyung@dimagi.com>
2012-09-19 20:20:25 -0400

    adding option to use file download as well. defaulting to cached download

--- couchexport/tasks.py
+++ couchexport/tasks.py
@@ -9,10 +9,14 @@ from couchexport.models import Format, FakeCheckpoint
 import tempfile
 import os
 import stat
+from django.conf import settings
 
 logging = get_task_logger()
 
 GLOBAL_RW = stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH
+
+EXPORT_METHOD = getattr(settings, 'COUCHEXPORT_METHOD', 'cached')
+
 
 @task
 def export_async(custom_export, download_id, format=None, filename=None, **kwargs):
@@ -41,14 +45,24 @@ def bulk_export_async(bulk_export_helper, download_id,
             except Exception as e:
                 logging.error("FAILED to add file to bulk export archive. %s" % e)
         zf.close()
-        download_stream = "%s_stream" % download_id
-        cache.set(download_stream, open(path, 'rb').read())
 
-        cache.set(download_id, CachedDownload(download_stream, mimetype='application/zip',
+
+        if EXPORT_METHOD == 'cached':
+            download_stream = "%s_stream" % download_id
+            cache.set(download_stream, open(path, 'rb').read())
+            cache.set(download_id, CachedDownload(download_stream, mimetype='application/zip',
                                         content_disposition='attachment; filename=%s.zip' %\
                                                             filename,
                                         extras={'X-CommCareHQ-Export-Token': bulk_export_helper.get_id}),
                                         expiry)
+        elif EXPORT_METHOD == 'tmpfile':
+            os.chmod(path, GLOBAL_RW)
+
+            cache.set(download_id, FileDownload(path, mimetype='application/zip',
+                content_disposition='attachment; filename=%s.zip' %\
+                                    filename,
+                extras={'X-CommCareHQ-Export-Token': bulk_export_helper.get_id}),
+                expiry)
     else:
         export_object = bulk_export_helper.bulk_files[0]
         return cache_file_to_be_served(export_object.generate_bulk_file(),
@@ -65,14 +79,30 @@ def cache_file_to_be_served(tmp, checkpoint, download_id, format=None, filename=
             filename = unidecode(filename)
         except Exception: 
             pass
-        download_stream = "%s_stream" % download_id
-        cache.set(download_stream, tmp.getvalue())
-        CachedDownload(download_stream,
-            mimetype=format.mimetype,
-            content_disposition='attachment; filename=%s.%s' % (filename, format.extension),
-            extras={'X-CommCareHQ-Export-Token': checkpoint.get_id},
-            download_id=download_id
-        ).save(expiry)
+
+
+        if EXPORT_METHOD == "cached":
+            download_stream = "%s_stream" % download_id
+            cache.set(download_stream, tmp.getvalue())
+
+            CachedDownload(download_stream,
+                mimetype=format.mimetype,
+                content_disposition='attachment; filename=%s.%s' % (filename, format.extension),
+                extras={'X-CommCareHQ-Export-Token': checkpoint.get_id},
+                download_id=download_id
+            ).save(expiry)
+        elif EXPORT_METHOD == 'tmpfile':
+            fd, path = tempfile.mkstemp()
+            with os.fdopen(fd, 'wb') as file:
+                file.write(tmp.getvalue())
+                # make file globally read/writeable in case celery runs as root
+            os.chmod(path, GLOBAL_RW)
+            FileDownload(path,
+                mimetype=format.mimetype,
+                content_disposition='attachment; filename=%s.%s' % (filename, format.extension),
+                extras={'X-CommCareHQ-Export-Token': checkpoint.get_id},
+                download_id=download_id
+            ).save(expiry)
     else:
         temp_id = uuid.uuid4().hex
         cache.set(temp_id, "Sorry, there wasn't any data.", expiry)
