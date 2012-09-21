090148d9a81a7ae69baaf905144a04bcf0505636
Biyeun Buczyk <biyeun@dimagi.com>
2012-09-21 14:06:54 -0400

    indicators g2qa

--- corehq/apps/indicators/_design/views/indicator_definitions/map.js
+++ corehq/apps/indicators/_design/views/indicator_definitions/map.js
@@ -1,11 +1,8 @@
 function (doc) {
     if (doc.base_doc === "CaseIndicatorDefinition" ||
         doc.base_doc === "FormIndicatorDefinition") {
-
         var class_path = doc.class_path + "." + doc.doc_type;
-
         emit(["namespace domain slug", doc.namespace, doc.domain, doc.slug, doc.version], class_path);
-
         if (doc.base_doc === "CaseIndicatorDefinition")
             emit(["namespace domain case_type slug", doc.namespace, doc.domain, doc.case_type, doc.slug, doc.version], class_path);
         if (doc.base_doc === "FormIndicatorDefinition")
--- corehq/apps/indicators/models.py
+++ corehq/apps/indicators/models.py
@@ -5,184 +5,32 @@ import datetime
 from couchdbkit.schema.properties import LazyDict
 from casexml.apps.case.models import CommCareCase
 from couchforms.models import XFormInstance
-from dimagi.utils.couch.database import get_db
 from dimagi.utils.modules import to_function
-
-OPERATOR_CHOICES = [('<','Less Than'), ('<=','Less Than or Equal To'),
-    ('>', 'Greater Than'), ('=>','Greater Than or Equal To'),
-    ('==','Equal To'), ('!=', 'Not Equal To'),
-    ('in','Contains'), ('not in', 'Does Not Contain')]
 
 class DocumentNotInDomainError(Exception):
     pass
 
+class DocumentMistmatchError(Exception):
+    pass
 
-class IndicatorBase(Document):
+
+class IndicatorDefinition(Document):
+    """
+        An Indicator Definition defines how to compute the indicator that lives
+        in the namespaced computed_ property of a case or form.
+    """
+    namespace = StringProperty()
     domain = StringProperty()
     slug = StringProperty()
     version = IntegerProperty()
     class_path = StringProperty()
 
     _class_path = "corehq.apps.indicators.models"
+    _returns_multiple = False
 
     def __init__(self, _d=None, **kwargs):
-        super(IndicatorBase, self).__init__(_d, **kwargs)
+        super(IndicatorDefinition, self).__init__(_d, **kwargs)
         self.class_path = self._class_path
-
-    @classmethod
-    def get_current(cls, **kwargs):
-        raise NotImplementedError
-
-    @classmethod
-    def all_slugs(cls, **kwargs):
-        raise NotImplementedError
-
-    @classmethod
-    def get_all(cls, **kwargs):
-        all_slugs = cls.all_slugs(**kwargs)
-        all_indicators = list()
-        for slug in all_slugs:
-            doc = cls.get_current(include_docs=False, slug=slug, **kwargs)
-            try:
-                doc_class = to_function(doc.get('value', "%s.%s" % (cls._class_path, cls.__name__)))
-                all_indicators.append(doc_class.get(doc.get('id')))
-            except Exception as e:
-                print "Error", e
-        return all_indicators
-
-
-class Indicator(IndicatorBase):
-    topic = StringProperty()
-    description = StringProperty()
-
-    base_doc = 'Indicator'
-
-    def get_value(self, **kwargs):
-        return NotImplemented
-
-    @classmethod
-    def get_or_create_by_domain_slug_version(cls, domain, slug, version):
-        indicator = cls.get_by_domain_slug_version(domain, slug, version)
-        if not indicator:
-            indicator = cls(domain=domain, slug=slug, version=version)
-            indicator.save()
-        return indicator
-
-    @classmethod
-    def get_by_domain_slug_version(cls, domain, slug, version):
-        return cls.view('indicators/all_indicators',
-            reduce=False,
-            include_docs=True,
-            key=["all", domain, slug, version]
-        ).first()
-
-    @classmethod
-    def get_current(cls, **kwargs):
-        domain = kwargs.get('domain')
-        slug = kwargs.get('slug')
-        version = kwargs.get('version')
-        include_docs = kwargs.get('include_docs', True)
-
-        startkey_suffix = [version] if version else [{}]
-        key=["all", domain, slug]
-        return cls.view('indicators/all_indicators',
-            reduce=False,
-            include_docs=include_docs,
-            startkey=key+startkey_suffix,
-            endkey=key,
-            descending=True
-        ).first()
-
-    @classmethod
-    def all_slugs(cls, **kwargs):
-        domain = kwargs.get('domain')
-        type = kwargs.get('type', 'all')
-
-        key = [type, domain]
-        data = cls.view("indicators/all_indicators",
-            group=True,
-            group_level=2,
-            descending=True,
-            startkey=key+[{}],
-            endkey=key
-        ).all()
-        return [item.get('key',[])[-1] for item in data]
-
-
-class HistoricalIndicator(Indicator):
-    original_indicator_id = StringProperty()
-    computed_for_date = DateTimeProperty()
-    computed_on_date = DateTimeProperty()
-    is_numeric = BooleanProperty(default=False)
-    computed_value = StringProperty()
-
-    def get_value(self, **kwargs):
-        return float(self.computed_value) if self.is_numeric else self.computed_value
-
-
-class ReducedCouchViewIndicator(Indicator):
-    """
-        Assumes that the couch view is keyed by the domain.
-        Start and end dates are appended to the last part of the key.
-    """
-    couch_view = StringProperty()
-    key_prefix = ListProperty()
-    key_suffix = ListProperty()
-    is_numeric = BooleanProperty(default=True)
-
-    def get_value(self, **kwargs):
-        default_value = 0 if self.is_numeric else None
-
-        startdate = kwargs.get('startdate')
-        enddate = kwargs.get('enddate')
-        startkey_suffix = [startdate] if startdate else []
-        endkey_suffix = [enddate] if enddate else [{}]
-        key = self.key_prefix + [self.domain] + self.key_suffix
-
-        data = get_db().view(self.couch_view,
-            reduce=True,
-            startkey=key+startkey_suffix,
-            endkey=key+endkey_suffix
-        ).first()
-
-        if not data:
-            return default_value
-
-        return data.get('value', default_value)
-
-
-class ProportionalIndicator(Indicator):
-    """
-        numerator and denominator are path.to.IndicatorClass strings.
-    """
-    numerator = StringProperty()
-    denominator = StringProperty()
-
-    @property
-    def value(self, **kwargs):
-        try:
-            numerator = to_function(self.numerator).get_current(self.domain, self.slug, self.version)
-            denominator = to_function(self.denominator).get_current(self.domain, self.slug, self.version)
-            return numerator.get_value(**kwargs)/denominator.get_value(**kwargs)
-        except ZeroDivisionError:
-            pass
-        except Exception as e:
-            logging.error("Could not compute %s with id %s due to error: %s" %
-                (self.__class__.__name__, self.get_id, e)
-            )
-        return 0
-
-
-class IndicatorDefinition(IndicatorBase):
-    """
-        An Indicator Definition lives in the namespaced computed_ property of a
-        case or form. It's purpose is to provide additional parameters that are computed
-        on form submission or case modification. These properties can then be used in
-        the full Indicator calculation.
-    """
-    namespace = StringProperty()
-
-    _returns_multiple = False
 
     def __str__(self):
         return "%s %s in namespace %s." % (self.__class__.__name__, self.slug, self.namespace)
@@ -274,6 +122,19 @@ class IndicatorDefinition(IndicatorBase):
         ).all()
         return [item.get('key',[])[-1] for item in data]
 
+    @classmethod
+    def get_all(cls, **kwargs):
+        all_slugs = cls.all_slugs(**kwargs)
+        all_indicators = list()
+        for slug in all_slugs:
+            doc = cls.get_current(include_docs=False, slug=slug, **kwargs)
+            try:
+                doc_class = to_function(doc.get('value', "%s.%s" % (cls._class_path, cls.__name__)))
+                all_indicators.append(doc_class.get(doc.get('id')))
+            except Exception as e:
+                print "Error", e
+        return all_indicators
+
 
 class FormDataIndicatorDefinitionMixin(DocumentSchema):
     xmlns = StringProperty()
@@ -292,7 +153,7 @@ class FormIndicatorDefinition(IndicatorDefinition, FormDataIndicatorDefinitionMi
         if not isinstance(doc, XFormInstance):
             raise ValueError("The document provided must be an instance of XFormInstance.")
         if not doc.xmlns == self.xmlns:
-            raise ValueError("The xmlns of the form provided does not match the one for this definition.")
+            raise DocumentMistmatchError("The xmlns of the form provided does not match the one for this definition.")
         return super(FormIndicatorDefinition, self).get_clean_value(doc)
 
     @classmethod
@@ -308,12 +169,25 @@ class CaseIndicatorDefinition(IndicatorDefinition):
         if not isinstance(doc, CommCareCase):
             raise ValueError("The document provided must be an instance of CommCareCase.")
         if not doc.type == self.case_type:
-            raise ValueError("The case provided should be a 'child' type case.")
+            raise DocumentMistmatchError("The case provided should be a '%s' type case." % self.case_type)
         return super(CaseIndicatorDefinition, self).get_clean_value(doc)
 
     @classmethod
     def key_params_order(cls):
         return ["namespace", "domain", "case_type", "slug"]
+
+
+class CaseDataInFormIndicatorDefinition(FormIndicatorDefinition):
+    case_property = StringProperty()
+
+    def get_value(self, doc):
+        form_data = doc.get_form
+        related_case_id = form_data.get('case', {}).get('@case_id')
+        if related_case_id:
+            case = CommCareCase.get(related_case_id)
+            if isinstance(case, CommCareCase) and hasattr(case, str(self.case_property)):
+                return getattr(case, str(self.case_property))
+        return None
 
 
 class FormDataInCaseIndicatorDefinition(CaseIndicatorDefinition, FormDataIndicatorDefinitionMixin):
@@ -350,54 +224,19 @@ class FormDataInCaseIndicatorDefinition(CaseIndicatorDefinition, FormDataIndicat
         raise NotImplementedError
 
 
-class BooleanIndicatorDefinitionMixin(DocumentSchema):
-    compared_property = StringProperty()
-    expression = StringProperty() # example: '%(value)s' not in 'foo bar'
-
-    _compared_property_value = None
-    def evaluate_expression(self):
-        print self.expression % dict(value=self._compared_property_value)
-        try:
-            return eval(self.expression % dict(value=self._compared_property_value))
-        except Exception:
-            return False
-
-
-class BooleanFormIndicatorDefinition(FormIndicatorDefinition, BooleanIndicatorDefinitionMixin):
-    """
-        Evaluate boolean expression from a form property.
-    """
-    def get_value(self, doc):
-        try:
-            self._compared_property_value = self.get_from_form(doc.get_form,
-                self.compared_property.split('.'))
-            return self.evaluate_expression()
-        except Exception:
-            return False
-
-
-class BooleanCaseIndicatorDefinition(CaseIndicatorDefinition, BooleanIndicatorDefinitionMixin):
-    """
-        Evaluate boolean expression referencing a case property.
-    """
-    def get_value(self, doc):
-        if not hasattr(doc, str(self.compared_property)):
-            return False
-        self._compared_property_value = getattr(doc, str(self.compared_property))
-        return self.evaluate_expression()
-
-
-class BooleanFormDataCaseIndicatorDefinition(FormDataInCaseIndicatorDefinition, BooleanIndicatorDefinitionMixin):
-    """
-        Evaluate boolean expression referencing a form property related to a form in a case.
-    """
-    def get_value_for_form(self, form_data):
-        try:
-            self._compared_property_value = self.get_from_form(form_data,
-                self.compared_property.split('.'))
-            return self.evaluate_expression()
-        except Exception:
-            return False
+#class BooleanIndicatorDefinitionMixin(DocumentSchema):
+#    compared_property = StringProperty()
+#    expression = StringProperty() # example: '%(value)s' not in 'foo bar'
+#
+#    _compared_property_value = None
+#    def evaluate_expression(self):
+#        print self.expression % dict(value=self._compared_property_value)
+#        try:
+#            return eval(self.expression % dict(value=self._compared_property_value))
+#        except Exception:
+#            return False
+#
+#
 
 
 class PopulateRelatedCasesWithIndicatorDefinitionMixin(DocumentSchema):
