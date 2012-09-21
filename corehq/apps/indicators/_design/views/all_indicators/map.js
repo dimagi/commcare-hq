fd8d63625d52fc7eda769200fbb9cb52fbd18cdf
Daniel Roberts <droberts@dimagi.com>
2012-09-21 16:16:02 -0300

    Update corehq/apps/indicators/_design/views/all_indicators/map.js

--- corehq/apps/indicators/_design/views/all_indicators/map.js
+++ corehq/apps/indicators/_design/views/all_indicators/map.js
@@ -2,9 +2,10 @@ function (doc) {
     if (doc.base_doc === "Indicator") {
         emit(["all", doc.domain, doc.slug, doc.version], doc.doc_type);
         var class_path = doc.class_path + "." + doc.doc_type;
-        if (doc.doc_type === 'HistoricalIndicator')
+        if (doc.doc_type === 'HistoricalIndicator') {
             emit(["historical", doc.domain, doc.slug, doc.version], class_path);
-        else
+        } else {
             emit(["active", doc.domain, doc.slug, doc.version], class_path);
+        }
     }
 }
