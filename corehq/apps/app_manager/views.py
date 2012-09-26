3b01edf0eaed5a76d0273dd9d7b22df497f84957
yedi <yedispaghetti@gmail.com>
2012-09-26 17:13:32 -0400

    refactor bail() so it isn't in multiple places

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -128,6 +128,13 @@ def back_to_main(req, domain, app_id=None, module_id=None, form_id=None, unique_
         "?%s" % urlencode(params) if params else ""
         ))
 
+def bail(req, domain, app_id, not_found=""):
+    if not_found:
+        messages.error(req, 'Oops! We could not find that %s. Please try again' % not_found)
+    else:
+        messages.error(req, 'Oops! We could not complete your request. Please try again')
+    return back_to_main(req, domain, app_id)
+
 def _get_xform_source(request, app, form, filename="form.xml"):
     download = json.loads(request.GET.get('download', 'false'))
     lang = request.COOKIES.get('lang', app.langs[0])
@@ -459,15 +466,8 @@ def view_generic(req, domain, app_id=None, module_id=None, form_id=None, is_user
     This is the main view for the app. All other views redirect to here.
 
     """
-
-    def bail():
-        module_id=None
-        form_id=None
-        messages.error(req, 'Oops! We could not complete your request. Please try again')
-        return back_to_main(req, domain, app_id)
-
     if form_id and not module_id:
-        return bail()
+        return bail(req, domain, app_id)
 
     app = module = form = None
     try:
@@ -480,7 +480,7 @@ def view_generic(req, domain, app_id=None, module_id=None, form_id=None, is_user
         if form_id:
             form = module.get_form(form_id)
     except IndexError:
-        return bail()
+        return bail(req, domain, app_id)
 
     base_context = get_apps_base_context(req, domain, app)
     edit = base_context['edit']
@@ -623,13 +623,6 @@ def user_registration_source(req, domain, app_id):
 
 @login_and_domain_required
 def form_designer(req, domain, app_id, module_id=None, form_id=None, is_user_registration=False):
-    def bail(not_found=""):
-        if not_found:
-            messages.error(req, 'Oops! We could not find that %s. Please try again' % not_found)
-        else:
-            messages.error(req, 'Oops! We could not complete your request. Please try again')
-        return back_to_main(req, domain, app_id)
-
     app = get_app(domain, app_id)
 
     if is_user_registration:
@@ -638,11 +631,11 @@ def form_designer(req, domain, app_id, module_id=None, form_id=None, is_user_reg
         try:
             module = app.get_module(module_id)
         except IndexError:
-            return bail("module")
+            return bail(req, domain, app_id, not_found="module")
         try:
             form = module.get_form(form_id)
         except IndexError:
-            return bail("form")
+            return bail(req, domain, app_id, not_found="form")
 
 
 
