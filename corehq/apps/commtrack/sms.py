91d9565a7311bc436a2a5b0adefb903ccd3c2d32
Drew Roos <droos@dimagi.com>
2012-09-28 18:06:21 -0400

    figuring out sms communication

--- corehq/apps/commtrack/sms.py
+++ corehq/apps/commtrack/sms.py
@@ -1,8 +1,18 @@
-
+from corehq.apps.domain.models import Domain
 
 
 
 def handle(v, text):
 
-    # check if a commtrack enabled domain
-    pass
+    print Domain.get_by_name(v.domain).__dict__
+
+    # look up domain
+    # is it commtrack enabled
+    # if not, return false
+
+    # if so, parse and process message
+
+
+#get_db().view('domain/domains', key='test', include_docs=True, reduce=False).one()['doc']
+
+    return False
