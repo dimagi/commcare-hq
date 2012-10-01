feb63d1f1a3e86b6fcdbd546b6773d6c159244a9
Biyeun Buczyk <biyeun@dimagi.com>
2012-10-01 17:12:48 -0400

    views emit data necessary for adm, without needing the whole doc

--- casexml/apps/case/_design/views/by_date_modified_owner/map.js
+++ casexml/apps/case/_design/views/by_date_modified_owner/map.js
@@ -2,11 +2,15 @@ function (doc) {
     if (doc.doc_type === "CommCareCase") {
         var date = doc.closed_on || doc.modified_on;
         var owner_id = doc.owner_id || doc.user_id;
-        var closed_status = (doc.closed) ? "closed" : "open";
+        var closed_status = (doc.closed) ? "closed" : "open",
+            emit_entry = {
+                closed: doc.closed,
+                type: doc.type
+            };
 
-        emit([doc.domain, closed_status, doc.type, owner_id, date], null);
-        emit([doc.domain, closed_status, {}, owner_id, date], null);
-        emit([doc.domain, {}, doc.type, owner_id, date], null);
-        emit([doc.domain, {}, {}, owner_id, date], null);
+        emit([doc.domain, closed_status, doc.type, owner_id, date], emit_entry);
+        emit([doc.domain, closed_status, {}, owner_id, date], emit_entry);
+        emit([doc.domain, {}, doc.type, owner_id, date], emit_entry);
+        emit([doc.domain, {}, {}, owner_id, date], emit_entry);
     }
 }
