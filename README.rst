82cf8ade84460c56e70fd34d570b2067bebb3e3d
Cory Zue <czue@dimagi.com>
2012-10-05 13:16:30 -0400

    add storage defaults and utility functions

--- README.rst
+++ README.rst
@@ -2,3 +2,8 @@ This is a simple app to help you schedule long running tasks for retrieval later
 Two modes of operation:  1:  sit and refresh page till it's done with a download link, or 2: send a "done" email with a link to a page with the resulting download.
 
 If you want to server large binaries over a cache, you must have django-redis installed, otherwise python-memcached is all you need
+
+There are a few settings you can use to tweak the default cache storage:
+
+SOIL_DEFAULT_CACHE = 'soil.FileDownload' # or 'soil.CachedDownload'
+SOIL_DEFAULT_CACHE = 'default'           # or 'memcached', 'redis', etc. 
---
+++ soil/util.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.utils import importlib
+from soil import FileDownload
+
+def get_default_backend():
+    """
+    Get default export class. To override set it in your settings.
+    
+    This defaults to FileDownload.
+    """
+    if hasattr(settings, "SOIL_BACKEND"):
+        # Trying to import the given backend, in case it's a dotted path
+        backend = settings.SOIL_BACKEND 
+        mod_path, cls_name = backend.rsplit('.', 1)
+        try:
+            mod = importlib.import_module(mod_path)
+            return getattr(mod, cls_name)
+        except (AttributeError, ImportError):
+            raise ValueError("Could not find soil backend '%s'" % backend)
+    
+    return FileDownload
+        
+def expose_download(payload, expiry, backend=None, **kwargs):
+    """
+    Expose a download object. Fully customizable, but allows 
+    you to rely on global defaults if you don't care how things
+    are stored.
+    """
+    if backend == None:
+        backend = get_default_backend()
+    
+    ref = backend.create(payload, expiry, **kwargs)
+    ref.save(expiry)
+    return ref
