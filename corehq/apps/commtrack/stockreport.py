383384ffc50ebdb38a03ae9ad4dae949bff1936b
Drew Roos <droos@dimagi.com>
2012-10-08 13:30:16 -0700

    refactor updating of current stock state

--- corehq/apps/commtrack/stockreport.py
+++ corehq/apps/commtrack/stockreport.py
@@ -15,6 +15,9 @@ def process(domain, instance):
     def _(tag, ns=XMLNS):
         return '{%s}%s' % (ns, tag)
 
+    # what order should stuff be processed in? if both a stock-on-hand and
+    # receipts come in, the order in which applied matters
+
     case_ids = [e.text for e in root.findall('.//%s' % _('product_entry'))]
     cases = dict((c._id, c) for c in CommCareCase.view('_all_docs', keys=case_ids, include_docs=True))
 
@@ -41,63 +44,59 @@ def process_transaction(tx, case):
     * submit the annotated instance to HQ for processing
     """
     action, value = tx
+    current_state = StockState(case)
+    current_state.update(action, value)
+    return current_state.to_case_block()
 
-    prop = lambda k, default: case.dynamic_properties().get(k, default)
-    current_state = {
-        'current_stock': prop('current_stock', 0),
-        'stocked_out_since': prop('stocked_out_since', None),
-    }
-    # worry about consumption rates later
-
-    update_stock_info(current_state, action, value)
-
-    # generate case block
-    case_update = CaseBlock(
-        version=V2,
-        case_id=case._id,
-        user_id='fixme',
-        update=dict((k, str(v) if v is not None else '') for k, v in current_state.iteritems())
-    ).as_xml()
-    # convert xml.etree to lxml
-    from xml.etree import ElementTree
-    case_update = etree.fromstring(ElementTree.tostring(case_update))
-
-    return case_update
-
-def update_stock_info(s, action, value):
-    """given the current stock state for a product at a location, update
-    with the incoming datapoint
-
-    fancy business logic to reconcile stock reports lives HERE
-    """
-
-    s['current_stock'] = int(s['current_stock'])
-    #if s['stocked_out_since']:
-    #    s['stocked_out_since'] = datetime.strptime(s['stocked_out_since'], '%Y-%m-%d').date()
-
-    # what order should stuff be processed in? if both a stock-on-hand and
-    # receipts come in, the order in which applied matters
-
-    if action == 'stockout':
-        s['current_stock'] = 0
-        s['stocked_out_since'] = date.today() - timedelta(days=value)
-    else:
-
-        if action == 'stockonhand':
-            s['current_stock'] = value
-        elif action == 'receipts':
-            s['current_stock'] += value
-        elif action == 'consumption':
-            s['current_stock'] -= value
-
-        if s['current_stock'] > 0:
-            s['stocked_out_since'] = None
+class StockState(object):
+    def __init__(self, case):
+        self.case = case
+        props = case.dynamic_properties()
+        self.current_stock = int(props.get('current_stock', 0)) # int
+        self.stocked_out_since = props.get('stocked_out_since') # date
+        # worry about consumption rates later
+        
+    def update(self, action, value):
+        """given the current stock state for a product at a location, update
+        with the incoming datapoint
+        
+        fancy business logic to reconcile stock reports lives HERE
+        """
+        if action == 'stockout':
+            self.current_stock = 0
+            self.stocked_out_since = date.today() - timedelta(days=value)
         else:
-            s['current_stock'] = 0 # handle if negative
-            if not s['stocked_out_since']: # handle if stocked out date already set
-                s['stocked_out_since'] = date.today()
 
-    #if s['stocked_out_since']:
-    #    s['stocked_out_since'] = datetime.strftime(s['stocked_out_since'], '%Y-%m-%d')
-    #else:
-    #    s['stocked_out_since'] = ''
+            if action == 'stockonhand':
+                self.current_stock = value
+            elif action == 'receipts':
+                self.current_stock += value
+            elif action == 'consumption':
+                self.current_stock -= value
+
+            if self.current_stock > 0:
+                self.stocked_out_since = None
+            else:
+                self.current_stock = 0 # handle if negative
+                if not self.stocked_out_since: # handle if stocked out date already set
+                    self.stocked_out_since = date.today()
+
+    def to_case_block(self, user=None):
+        def convert_prop(val):
+            return str(val) if val is not None else ''
+
+        props = ['current_stock', 'stocked_out_since']
+
+        case_update = CaseBlock(
+            version=V2,
+            case_id=self.case._id,
+            user_id=user or 'FIXME',
+            update=dict((k, convert_prop(getattr(self, k))) for k in props)
+        ).as_xml()
+        # convert xml.etree to lxml
+        from xml.etree import ElementTree
+        case_update = etree.fromstring(ElementTree.tostring(case_update))
+
+        return case_update
+
+
