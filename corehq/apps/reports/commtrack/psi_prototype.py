3757b1643e90860ea85da7cdeb12e5166b00369b
Drew Roos <droos@dimagi.com>
2012-10-16 18:36:59 -0700

    iso date format - use existing helper functions

--- corehq/apps/reports/commtrack/psi_prototype.py
+++ corehq/apps/reports/commtrack/psi_prototype.py
@@ -6,6 +6,7 @@ from corehq.apps.commtrack.models import *
 from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn, DTSortType
 from dimagi.utils.couch.database import get_db
 from dimagi.utils.couch.loosechange import map_reduce
+from dimagi.utils import parsing as dateparse
 import itertools
 from datetime import datetime, date, timedelta
 
@@ -37,23 +38,14 @@ def get_transactions(form_doc):
     return txs
 
 def get_stock_reports(domain, datespan):
-    timestamp_start = to_iso(datespan.startdate)
-    timestamp_end =  to_iso(datespan.end_of_end_day)
-
-    # for debugging; add a threshold to hide incompatible instances
-    timestamp_start = min(max(timestamp_start, '2012-10-15T22:30:00Z'), timestamp_end)
+    timestamp_start = dateparse.json_format_datetime(datespan.startdate)
+    timestamp_end =  dateparse.json_format_datetime(datespan.end_of_end_day)
 
     start_key = [domain, timestamp_start]
     end_key = [domain, timestamp_end]
 
     query = get_db().view('commtrack/stock_reports', start_key=start_key, end_key=end_key, include_docs=True)
     return [e['doc'] for e in query]
-
-def parse_iso(dt):
-    return datetime.strptime(dt, '%Y-%m-%dT%H:%M:%SZ')
-
-def to_iso(dt):
-    return dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 class VisitReport(GenericTabularReport, CommtrackReportMixin, DatespanMixin):
     name = 'Visit Report'
@@ -86,7 +78,7 @@ class VisitReport(GenericTabularReport, CommtrackReportMixin, DatespanMixin):
 
             data = [
                 doc['form']['location'],
-                parse_iso(doc['received_on']).strftime('%Y-%m-%d'),
+                dateparse.string_to_datetime(doc['received_on']).strftime('%Y-%m-%d'),
                 CommCareUser.get(doc['form']['meta']['userID']).username_in_report,
             ]
             for p in products:
@@ -135,14 +127,14 @@ class SalesAndConsumptionReport(GenericTabularReport, CommtrackReportMixin, Date
             for p in products:
                 tx_by_action = map_reduce(lambda tx: [(tx['action'], int(tx['value']))], data=tx_by_product.get(p['_id'], []))
 
-                start_key = [str(self.domain), site, p['_id'], to_iso(self.datespan.startdate)]
-                end_key =   [str(self.domain), site, p['_id'], to_iso(self.datespan.end_of_end_day)]
+                start_key = [str(self.domain), site, p['_id'], dateparse.json_format_datetime(self.datespan.startdate)]
+                end_key =   [str(self.domain), site, p['_id'], dateparse.json_format_datetime(self.datespan.end_of_end_day)]
                 # list() is necessary or else get a weird error
                 product_states = list(get_db().view('commtrack/stock_product_state', start_key=start_key, end_key=end_key))
                 latest_state = product_states[-1]['value'] if product_states else None
                 if latest_state:
                     stock = latest_state['updated_unknown_properties']['current_stock']
-                    as_of = parse_iso(latest_state['server_date']).strftime('%Y-%m-%d')
+                    as_of = dateparse.string_to_datetime(latest_state['server_date']).strftime('%Y-%m-%d')
 
                 stockout_dates = set()
                 for state in product_states:
@@ -150,7 +142,7 @@ class SalesAndConsumptionReport(GenericTabularReport, CommtrackReportMixin, Date
                     stocked_out_since = doc['updated_unknown_properties']['stocked_out_since']
                     if stocked_out_since:
                         so_start = max(datetime.strptime(stocked_out_since, '%Y-%m-%d').date(), self.datespan.startdate.date())
-                        so_end = parse_iso(doc['server_date']).date() # time zone issues
+                        so_end = dateparse.string_to_datetime(doc['server_date']).date() # TODO deal with time zone issues
                         dt = so_start
                         while dt < so_end:
                             stockout_dates.add(dt)
