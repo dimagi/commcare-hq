99722c262dbb24ca93f3c562a72d4e662d7b5e14
Mike White <mwhite@dimagi.com>
2012-10-28 01:31:17 -0400

    send_HTML_email() changes
    Remove text_content as required parameter and add helpful user message for when
    no text_content is supplied; our plaintext report output was unusable and
    pretty much everyone has HTML email clients.

--- dimagi/utils/django/email.py
+++ dimagi/utils/django/email.py
@@ -2,7 +2,16 @@ from django.conf import settings
 from django.core.mail import SMTPConnection
 from django.core.mail.message import EmailMultiAlternatives
 
-def send_HTML_email( subject, recipient, text_content, html_content ):
+def send_HTML_email(subject, recipient, html_content, text_content=None):
+    if not text_content:
+        text_content = """
+This is an email from CommCare HQ. You're seeing this message because your
+email client chose to display the plaintext version of an email that CommCare
+HQ can only provide in HTML.  Please set your email client to view this email
+in HTML or read this email in a client that supports HTML email.
+
+Thanks,
+The CommCare HQ Team"""
 
     # If you get the return_path header wrong, this may impede mail delivery. It appears that the SMTP server
     # has to recognize the return_path as being valid for the sending host. If we set it to, say, our SMTP
@@ -11,7 +20,7 @@ def send_HTML_email( subject, recipient, text_content, html_content ):
     if email_return_path is None: 
         # Get last two parts of the SMTP server as a proxy for the domain name from which this mail is sent.
         # This works for gmail, anyway.
-        email_return_path =  settings.EMAIL_LOGIN
+        email_return_path = settings.EMAIL_LOGIN
     
     email_from = getattr(settings, 'EMAIL_FROM', None)
     if email_from is None:
