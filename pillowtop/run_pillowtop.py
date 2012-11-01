96fb67fcb305bd286221058962c6480c12d7f122
dmyung <dmyung@dimagi.com>
2012-11-01 17:37:13 -0400

    updating pillow type to include an aliased elastic pillow for aliasing/preindexing indices.

--- pillowtop/run_pillowtop.py
+++ pillowtop/run_pillowtop.py
@@ -1,3 +1,4 @@
+from gevent import monkey; monkey.patch_all()
 import gevent
 from gevent.pool import Pool
 
@@ -26,7 +27,9 @@ def import_pillows():
 #standalone pillowtop runner
 def start_pillows():
     #gevent patching: logging doesn't seem to work unless thread is not patched
-    from gevent import monkey; monkey.patch_all(thread=False)
+    import logging
+    requests_log = logging.getLogger("requests")
+    requests_log.setLevel(logging.WARNING)
     pillows = import_pillows()
     pool = Pool(len(pillows))
     while True:
