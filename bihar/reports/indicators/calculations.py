6bbf6f5089434e84dc44d42eae1447da032bb714
Cory Zue <czue@dimagi.com>
2012-11-03 12:06:43 -0400

    implement proper done / due calculations, lots of code cleanup

--- bihar/reports/indicators/calculations.py
+++ bihar/reports/indicators/calculations.py
@@ -1,9 +1,11 @@
 from bihar.reports.indicators.filters import A_MONTH, is_pregnant_mother,\
     in_second_trimester, in_third_trimester
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 EMPTY = (0,0)
+GRACE_PERIOD = timedelta(days=7)
+
 
 def _done_due(done, due):
     return "%s/%s" % (done, due)
@@ -29,84 +31,74 @@ def _has_eb_visit(action):
 def _has_cf_visit(action):
     return _any_action_property(action, ('date_eb_%s' % i for i in range(1, 8)))
 
+def _in_last_month(date):
+    today = datetime.today().date()
+    return date < today - A_MONTH < today
+
+def _mother_due_in_window(case, days):
+    get_edd = lambda case: getattr(case, 'edd', None)
+    get_duedate = lambda case: case.edd - timedelta(days=days) + GRACE_PERIOD
+    return is_pregnant_mother(case) and get_edd(case) and _in_last_month(get_duedate(case))
+        
+def _mother_delivered_in_window(case, days):
+    get_add = lambda case: getattr(case, 'add', None)
+    get_duedate = lambda case: case.add + timedelta(days=days) + GRACE_PERIOD
+    return is_pregnant_mother(case) and get_add(case) and _in_last_month(get_duedate(case))
+        
+def _done_due_count(cases, done_func, due_func):
+    done = due = 0
+    for case in cases:
+        due_diff = due_func(case)
+        if due_diff:
+            due += due_diff
+            done_diff = done_func(case)
+            assert done_diff <= due_diff
+            # this is to prevent the numerator from ever passing the denominator
+            # though is probably not totally accurate
+            done += done_diff
+    return _done_due(done, due)
+
+def _visits_due(case, schedule):
+    return [i +1 for i, days in enumerate(schedule) \
+            if _mother_delivered_in_window(case, days)]
+
+def _visits_done(case, schedule, type):
+    due = _visits_due(case, schedule)
+    count = len(filter(lambda a: _visit_is(a, type), case.actions))
+    return len(v for v in due if count > v)
+    
 # NOTE: cases in, values out might not be the right API
 # but it's what we need for the first set of stuff.
 # might want to revisit.
 
-# NOTE: there is currently a lot of copy/paste here, but 
-# until the indicators are fully defined that's fine.
+# NOTE: this is going to be slooooow
 def bp2_last_month(cases):
-    # NOTE: this is going to be slooooow
-    done = due = 0
-    for case in cases:
-        if is_pregnant_mother(case):
-            edd = getattr(case, 'edd', None)
-            if edd:
-                for a in case.actions:
-                    # relevant
-                    if a.date > datetime.today() - A_MONTH \
-                        and in_second_trimester(edd, a.date.date()) \
-                        and _has_bp_visit(a) \
-                        and _visit_is(a, 'bp'):
-                        done += 1
-                        due += 1 # NOTE: this isn't right but i'm not sure how it should be done.
-                        break
+    due = lambda case: 1 if _mother_due_in_window(case, 75) else 0
+    # make sure they've done 2 bp visits
+    done = lambda case: 1 if len(filter(lambda a: _visit_is(a, 'bp'), case.actions)) >= 2 else 0
+    return _done_due_count(cases, due, done)    
     
-    return _done_due(done, due)
-
 def bp3_last_month(cases):
-    done = due = 0
-    for case in cases:
-        if is_pregnant_mother(case):
-            edd = getattr(case, 'edd', None)
-            if edd:
-                for a in case.actions:
-                    # relevant
-                    if a.date > datetime.today() - A_MONTH \
-                        and in_third_trimester(edd, a.date.date()) \
-                        and _has_bp_visit(a) \
-                        and _visit_is(a, 'bp'):
-                        done += 1
-                        due += 1 # NOTE: this isn't right but i'm not sure how it should be done.
-                        break
+    due = lambda case: 1 if _mother_due_in_window(case, 45) else 0
+    # make sure they've done 2 bp visits
+    done = lambda case: 1 if len(filter(lambda a: _visit_is(a, 'bp'), case.actions)) >= 3 else 0
+    return _done_due_count(cases, due, done)    
     
-    return _done_due(done, due)
-
 def pnc_last_month(cases):
-    done = due = 0
-    for case in cases:
-        if is_pregnant_mother(case):
-            for a in case.actions:
-                if a.date > datetime.today() - A_MONTH \
-                    and _has_pnc_visit(a) \
-                    and _visit_is(a, 'pnc'):
-                    done += 1
-                    due += 1 # NOTE: this isn't right but i'm not sure how it should be done.
-
-    return _done_due(done, due)
+    pnc_schedule = (1, 3, 6)
+    due = lambda case: len(_visits_due(case, pnc_schedule))
+    done = lambda case: _visits_done(case, pnc_schedule, "pnc")
+    return _done_due_count(cases, done, due)
 
 def eb_last_month(cases):
-    done = due = 0
-    for case in cases:
-        if is_pregnant_mother(case):
-            for a in case.actions:
-                if a.date > datetime.today() - A_MONTH \
-                    and _has_eb_visit(a) \
-                    and _visit_is(a, 'eb'):
-                    done += 1
-                    due += 1 # NOTE: this isn't right but i'm not sure how it should be done.
-
-    return _done_due(done, due)
+    eb_schedule = (14, 28, 60, 90, 120, 150)
+    due = lambda case: len(_visits_due(case, eb_schedule))
+    done = lambda case: _visits_done(case, eb_schedule, "eb")
+    return _done_due_count(cases, done, due)
 
 def cf_last_month(cases):
-    done = due = 0
-    for case in cases:
-        if is_pregnant_mother(case):
-            for a in case.actions:
-                if a.date > datetime.today() - A_MONTH \
-                    and _has_cf_visit(a) \
-                    and _visit_is(a, 'cf'):
-                    done += 1
-                    due += 1 # NOTE: this isn't right but i'm not sure how it should be done.
-
-    return _done_due(done, due)
+    cf_schedule_in_months = (6, 7, 8, 9, 12, 15, 18)
+    cf_schedule = (m * 30 for m in cf_schedule_in_months)
+    due = lambda case: len(_visits_due(case, cf_schedule))
+    done = lambda case: _visits_done(case, cf_schedule, "cf")
+    return _done_due_count(cases, done, due)
