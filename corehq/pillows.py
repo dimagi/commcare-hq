7dc19a29e91b39709971f2f615fe08677899b528
dmyung <dmyung@dimagi.com>
2012-11-17 13:31:48 -0500

    adding xform pillow

--- corehq/pillows.py
+++ corehq/pillows.py
@@ -12,6 +12,278 @@ import settings
 from datetime import datetime
 
 
+date_format_arr = ["yyyy-MM-dd",
+                   #"date_time_no_millis",
+                   #                      'date_optional_time',
+                   "yyyy-MM-dd'T'HH:mm:ssZZ",
+                   "yyyy-MM-dd'T'HH:mm:ss.SSSSSS",
+                   "yyyy-MM-dd'T'HH:mm:ss'Z'",
+                   "yyyy-MM-dd'T'HH:mm:ssZ",
+                   "yyyy-MM-dd'T'HH:mm:ssZZ'Z'",
+                   "yyyy-MM-dd'T'HH:mm:ss.SSSZZ",
+                   "yyyy-MM-dd'T'HH:mm:ss",
+                   "yyyy-MM-dd' 'HH:mm:ss",
+                   "yyyy-MM-dd' 'HH:mm:ss.SSSSSS",
+]
+#https://github.com/elasticsearch/elasticsearch/issues/2132
+#elasticsearch Illegal pattern component: t
+#no builtin types for || joins
+formats_string = '||'.join(date_format_arr)
+print formats_string
+
+class XFormPillow(AliasedElasticPillow):
+    couch_db = XFormInstance.get_db()
+    couch_filter = "couchforms/xforms"
+    es_host = settings.ELASTICSEARCH_HOST
+    es_port = settings.ELASTICSEARCH_PORT
+    es_index_prefix = "xforms"
+    es_alias = "xforms"
+    es_type = "xform"
+
+    seen_types = {}
+    es_meta = {
+        "date_formats": date_format_arr
+    }
+
+    def calc_meta(self):
+        """
+        override of the meta calculator since we're separating out all the types,
+        so we just do a hash of the "prototype" instead to determind md5
+        """
+        if not hasattr(self, '_calc_meta'):
+            self._calc_meta = hashlib.md5(simplejson.dumps(
+                self.get_mapping_from_type({'domain': 'default', 'type': 'default'}))).hexdigest()
+        return self._calc_meta
+
+    def get_mapping_from_type(self, doc_dict):
+        """
+        Define mapping uniquely to the domain_type document.
+        See below on why date_detection is False
+        """
+        #the meta here is defined for when the case index + type is created for the FIRST time
+        #subsequent data added to it will be added automatically, but the date_detection is necessary
+        # to be false to prevent indexes from not being created due to the way we store dates
+        #all will be strings EXCEPT the core case properties which we need to explicitly define below.
+        #that way date sort and ranges will work with canonical date formats for queries.
+        return {
+            self.get_type_string(doc_dict): {
+                "date_detection": False,
+                "date_formats": date_format_arr,
+                'ignore_malformed': True,
+                "_meta": {
+                    "created": '',
+                },
+                "properties": {
+                    "xmlns": {
+                        "type": "multi_field",
+                        "fields": {
+                            "xmlns": {"type": "string", "index": "analyzed"},
+                            "exact": {"type": "string", "index": "not_analyzed"}
+                        }
+                    },
+                    "path": {"type": "string", "index": "not_analyzed"},
+                    "submit_ip": {"type": "ip"},
+                    "app_id": {"type": "string", "index": "not_analyzed"},
+                    "received_on": {
+                        "type": "date",
+                        "format": formats_string
+                    },
+
+                    #                    "encounter_date": {"type": "string", "index": "not_analyzed"},
+                    'form': {
+                        'properties': {
+                            #                            "encounter_date": {
+                            #                                "format": "dateOptionalTime",
+                            #                                "type": "date"
+                            #                            },
+                            'case': {
+                                'properties': {
+                                    'date_modified': {
+                                        "type": "date",
+                                        "format": formats_string
+                                    },
+                                    '@date_modified': {
+                                        "type": "date",
+                                        "format": formats_string
+                                    },
+
+                                    "@case_id": {"type": "string", "index": "not_analyzed"},
+                                    "@user_id": {"type": "string", "index": "not_analyzed"},
+                                    "@xmlns": {"type": "string", "index": "not_analyzed"},
+
+
+                                    "case_id": {"type": "string", "index": "not_analyzed"},
+                                    "user_id": {"type": "string", "index": "not_analyzed"},
+                                    "xmlns": {"type": "string", "index": "not_analyzed"},
+                                }
+                            },
+                            'meta': {
+                                'properties': {
+                                    "timeStart": {
+                                        "type": "date",
+                                        "format": formats_string
+                                    },
+                                    "timeEnd": {
+                                        "type": "date",
+                                        "format": formats_string
+                                    },
+                                    "userID": {"type": "string", "index": "not_analyzed"},
+                                    "deviceID": {"type": "string", "index": "not_analyzed"},
+                                    "instanceID": {"type": "string", "index": "not_analyzed"},
+                                    "username": {"type": "string", "index": "not_analyzed"}
+                                }
+                            },
+                        },
+                    },
+
+                }
+            }
+        }
+
+
+    def get_type_string(self, doc_dict):
+    #        print doc_dict
+        domain = doc_dict.get('domain', None)
+        if domain is None:
+            domain = "unknowndomain"
+        ui_version = doc_dict.get('form', {}).get('@uiVersion', 'unknown_ui')
+        version = doc_dict.get('form', {}).get('@version', 'unknown_ver')
+        xmlns = doc_dict.get('xmlns', 'http://noxmlns')
+        return "%(type)s.%(domain)s.%(xmlns_suffix)s.u-%(ui_version)s-v-%(version)s" % {
+            'type': self.es_type,
+            'domain': domain.lower(),
+            'xmlns_suffix': xmlns.split('/')[-1],
+            'ui_version': ui_version,
+            'version': version
+        }
+
+    def get_doc_path_typed(self, doc_dict):
+        return "%(index)s/%(type_string)s/%(id)s" % (
+            {
+                'index': self.es_index,
+                'type_string': self.get_type_string(doc_dict),
+                'id': doc_dict['_id']
+            })
+
+    def type_exists(self, doc_dict):
+        es = self.get_es()
+
+        type_string = self.get_type_string(doc_dict)
+
+        ##################
+        #ES 0.20 has the index HEAD API.  While we're on 0.19, we will need to poll the index
+        # metadata
+        #type_path = "%(index)s/%(type_string)s" % ( { 'index': self.es_index, 'type_string': type_string, })
+
+        #if we don't want to server confirm it for both .19 or .20, then this hash is enough
+        #if self.seen_types.has_key(type_string):
+        #return True
+        #else:
+        #self.seen_types[type_string] = True
+        #head_result = es.head(type_path)
+        #return head_result
+        ##################
+
+        #####
+        #0.19 method, get the mapping from the index
+        return self.seen_types.has_key(type_string)
+
+    def doc_exists(self, doc_dict):
+        """
+        Overrided based upon the doc type
+        """
+        es = self.get_es()
+        doc_path = "%(index)s/%(type_string)s/%(id)s" % (
+            {
+                'index': self.es_index,
+                'type_string': self.get_type_string(doc_dict),
+                'id': doc_dict['_id']
+            })
+        head_result = es.head(doc_path)
+        return head_result
+
+
+    def change_transport(self, doc_dict):
+        """
+        Override the elastic transport to go to the index + the type being a string between the
+        domain and case type
+        """
+        try:
+            es = self.get_es()
+
+            if not self.type_exists(doc_dict):
+                #if type is never seen, apply mapping for said type
+                type_mapping = self.get_mapping_from_type(doc_dict)
+                #update metadata
+                type_mapping[self.get_type_string(doc_dict)]['_meta'][
+                'created'] = datetime.isoformat(datetime.utcnow())
+                mapping_res = es.put(
+                    "%s/%s/_mapping" % (self.es_index, self.get_type_string(doc_dict)),
+                    data=type_mapping)
+                print mapping_res
+                #this server confirm is a modest overhead but it tells us whether or not the type
+                # is successfully mapped into the index.
+                #0.19 mapping - retrieve the mapping to confirm that it's been seen
+                #print "Setting mapping for: %s" % self.get_type_string(doc_dict)
+                self.seen_types = es.get('%s/_mapping' % self.es_index)[self.es_index]
+
+            doc_path = self.get_doc_path_typed(doc_dict)
+
+            #sanity check for date_modified
+            if doc_dict.get('form', {}).has_key('case'):
+                #old and new check
+                date_modified_keys = ['date_modified', '@date_modified']
+
+                for k in date_modified_keys:
+                    if doc_dict['form']['case'].has_key(k):
+                        if doc_dict['form']['case'][k] == "":
+                            print "fixing null date modified: %s" % doc_dict['_id']
+                            doc_dict['form']['case'][k] = None
+
+                if doc_dict['form']['meta'].get('timeEnd', None) == "":
+                    doc_dict['form']['meta']['timeEnd'] = None
+                    print "fixing null timeEnd"
+
+                if doc_dict['form']['meta'].get('timeStart', None) == "":
+                    doc_dict['form']['meta']['timeStart'] = None
+                    print "fixing null timeStart"
+
+
+                if doc_dict.get('received_on', None) == "":
+                    doc_dict['received_on'] = None
+                    print "fixing null received_on"
+                if doc_dict['xmlns'] == "http://dev.commcarehq.org/pact/dots_form":
+                    if doc_dict['form'].get('question', None) == "":
+                        doc_dict['form']['question'] = {}
+                        print "fixing blank dots.form.question"
+
+
+                #dots blank case update (it's dynamic)
+                #bw results blank, it's dynamic
+
+
+            if self.allow_updates:
+                can_put = True
+            else:
+                can_put = not self.doc_exists(doc_dict['_id'])
+
+            if can_put:
+                res = es.put(doc_path, data=doc_dict)
+                if res.get('status', 0) == 400:
+                    print "xform error: %s\n%s" % (doc_dict['_id'], simplejson.dumps(res,  indent=4))
+                    #                    logging.error("Pillowtop Error [%s]:\n%s\n\tDoc id: %s\n\t%s" % (self.get_name(),
+                    #                                                                           res.get('error',
+                    #                                                                               "No error message"),
+                    #                                                                           doc_dict['_id'],
+                    #                                                                           doc_dict.keys()))
+        except Exception, ex:
+            print 'xforms error: %s' % ex
+            logging.error(
+                "PillowTop [%s]: transporting change data doc_id: %s to elasticsearch error: %s",
+                (self.get_name(), doc_dict['_id'], ex))
+            return None
+
+
 class CasePillow(AliasedElasticPillow):
     couch_db = CommCareCase.get_db()
     couch_filter = "case/casedocs"
@@ -48,8 +320,8 @@ class CasePillow(AliasedElasticPillow):
         return {
             self.get_type_string(doc_dict): {
                 "date_detection": False,
-                "_meta" : {
-                    "created" : '',
+                "_meta": {
+                    "created": '',
                 },
                 "properties": {
                     "name": {
@@ -154,9 +426,9 @@ class CasePillow(AliasedElasticPillow):
 
         #if we don't want to server confirm it for both .19 or .20, then this hash is enough
         #if self.seen_types.has_key(type_string):
-            #return True
+        #return True
         #else:
-            #self.seen_types[type_string] = True
+        #self.seen_types[type_string] = True
         #head_result = es.head(type_path)
         #return head_result
         ##################
@@ -194,8 +466,10 @@ class CasePillow(AliasedElasticPillow):
                 #if type is never seen, apply mapping for said type
                 type_mapping = self.get_mapping_from_type(doc_dict)
                 #update metadata
-                type_mapping[self.get_type_string(doc_dict)]['_meta']['created'] = datetime.isoformat(datetime.utcnow())
-                es.put("%s/%s/_mapping" % (self.es_index, self.get_type_string(doc_dict)), data=type_mapping)
+                type_mapping[self.get_type_string(doc_dict)]['_meta'][
+                'created'] = datetime.isoformat(datetime.utcnow())
+                es.put("%s/%s/_mapping" % (self.es_index, self.get_type_string(doc_dict)),
+                    data=type_mapping)
                 #this server confirm is a modest overhead but it tells us whether or not the type
                 # is successfully mapped into the index.
                 #0.19 mapping - retrieve the mapping to confirm that it's been seen
@@ -219,7 +493,8 @@ class CasePillow(AliasedElasticPillow):
                                                                            doc_dict['_id'],
                                                                            doc_dict.keys()))
         except Exception, ex:
-            logging.error("PillowTop [%s]: transporting change data doc_id: %s to elasticsearch error: %s",
+            logging.error(
+                "PillowTop [%s]: transporting change data doc_id: %s to elasticsearch error: %s",
                 (self.get_name(), doc_dict['_id'], ex))
             return None
 
@@ -286,9 +561,9 @@ class ExchangePillow(ElasticPillow):
                         "type": "custom",
                         "tokenizer": "keyword",
                         "filter": ["lowercase"]},
-                    "comma":{
+                    "comma": {
                         "type": "pattern",
-                        "pattern":"\s*,\s*"}}}},
+                        "pattern": "\s*,\s*"}}}},
         "mappings": {
             "domain": {
                 "properties": {
