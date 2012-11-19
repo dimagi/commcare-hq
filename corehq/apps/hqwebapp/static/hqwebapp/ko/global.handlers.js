9b106a90eed1f388822fdfee462676ab1ea9f00c
Biyeun Buczyk <biyeun@dimagi.com>
2012-11-19 13:49:07 +0530

    added some global handlers that fade the bound element in/out based on the boolean passed to it

--- corehq/apps/hqwebapp/static/hqwebapp/ko/global.handlers.js
+++ corehq/apps/hqwebapp/static/hqwebapp/ko/global.handlers.js
@@ -7,3 +7,27 @@ ko.bindingHandlers.hqbSubmitReady = {
             $(element).addClass("disabled").removeClass("btn-primary");
     }
 };
+
+ko.bindingHandlers.fadeVisible = {
+    // from knockout.js examples
+    init: function(element, valueAccessor) {
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value));
+    },
+    update: function(element, valueAccessor) {
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+    }
+};
+
+ko.bindingHandlers.fadeVisibleInOnly = {
+    // from knockout.js examples
+    init: function(element, valueAccessor) {
+        var value = valueAccessor();
+        $(element).toggle(ko.utils.unwrapObservable(value));
+    },
+    update: function(element, valueAccessor) {
+        var value = valueAccessor();
+        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).hide();
+    }
+};
