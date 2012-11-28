4a77cc2a9eb7be27813cb04a6ff90d88da527142
Cory Zue <czue@dimagi.com>
2012-11-28 16:45:18 +0530

    fix test

--- corehq/apps/receiverwrapper/tests/test_repeater.py
+++ corehq/apps/receiverwrapper/tests/test_repeater.py
@@ -8,6 +8,7 @@ from couchforms.models import XFormInstance
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.test.client import Client
+from couchdbkit.exceptions import ResourceNotFound
 
 case_id = "ABC123CASEID"
 instance_id = "XKVB636DFYL38FNX3D38WV5EH"
@@ -76,7 +77,12 @@ class RepeaterTest(TestCase):
         XFormInstance.get(instance_id).delete()
         repeat_records = RepeatRecord.all()
         for repeat_record in repeat_records:
-            repeat_record.delete()
+            try:
+                repeat_record.delete()
+            except ResourceNotFound:
+                # the above view can return the same object
+                # multiple times
+                pass
 
 
     def test_repeater(self):
