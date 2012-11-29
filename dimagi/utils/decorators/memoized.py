24d3583236459cf0d887a4c597af056b6a8bd4c4
Daniel Roberts <droberts@dimagi.com>
2012-11-30 03:45:12 +0530

    improve memoize so it can take any argument structure

--- dimagi/utils/decorators/memoized.py
+++ dimagi/utils/decorators/memoized.py
@@ -10,18 +10,18 @@ class memoized(object):
         self.func = func
         self.cache = {}
 
-    def __call__(self, *args):
-        return self.call(None, self.cache, *args)
+    def __call__(self, *args, **kwargs):
+        return self.call(None, self.cache, *args, **kwargs)
 
-    def call(self, obj, cache, *args):
+    def call(self, obj, cache, *args, **kwargs):
+        if obj:
+            args = (obj,) + args
+        key = self.get_args_tuple(*args, **kwargs)
         try:
-            return cache[args]
+            return cache[key]
         except KeyError:
-            if obj:
-                value = self.func(obj, *args)
-            else:
-                value = self.func(*args)
-            cache[args] = value
+            value = self.func( *args, **kwargs)
+            cache[key] = value
             return value
 
     def __repr__(self):
@@ -37,3 +37,18 @@ class memoized(object):
             cache = {}
             setattr(obj, cache_attr, cache)
         return functools.partial(self.call, obj, cache)
+
+    def get_args_tuple(self, *args, **kwargs):
+        """
+        Take a function and the arguments you'd call it with
+        and return a tuple
+        """
+        import inspect
+        arg_names, args_name, kwargs_name, ___ = inspect.getargspec(self.func)
+        values = inspect.getcallargs(self.func, *args, **kwargs)
+        in_order = [values[arg_name] for arg_name in arg_names]
+        if args_name:
+            in_order.append(values[args_name])
+        if kwargs_name:
+            in_order.append(tuple(sorted(values[kwargs_name].items())))
+        return tuple(in_order)
