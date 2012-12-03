4ad631c5410ca89b2dc2828bf85cb371b877486f
yedi <yedispaghetti@gmail.com>
2012-12-03 15:19:30 +0530

    use num and denom instead of done and due

--- bihar/reports/indicators/calculations.py
+++ bihar/reports/indicators/calculations.py
@@ -7,8 +7,8 @@ EMPTY = (0,0)
 GRACE_PERIOD = timedelta(days=7)
 
 
-def _done_due(done, due):
-    return "%s/%s" % (done, due)
+def _num_denom(num, denom):
+    return "%s/%s" % (num, denom)
 
 def _in_last_month(date):
     today = datetime.today().date()
@@ -25,19 +25,19 @@ def _mother_due_in_window(case, days):
 def _mother_delivered_in_window(case, days):
     get_visitduedate = lambda case: case.add + timedelta(days=days) + GRACE_PERIOD
     return is_pregnant_mother(case) and get_add(case) and _in_last_month(get_visitduedate(case))
-        
-def _done_due_count(cases, done_func, due_func):
-    done = due = 0
+
+def _num_denom_count(cases, num_func, denom_func):
+    num = denom = 0
     for case in cases:
-        due_diff = due_func(case)
-        if due_diff:
-            due += due_diff
-            done_diff = done_func(case)
-            assert done_diff <= due_diff
+        denom_diff = denom_func(case)
+        if denom_diff:
+            denom += denom_diff
+            num_diff = num_func(case)
+            assert num_diff <= denom_diff
             # this is to prevent the numerator from ever passing the denominator
             # though is probably not totally accurate
-            done += done_diff
-    return _done_due(done, due)
+            num += num_diff
+    return _num_denom(num, denom)
 
 def _visits_due(case, schedule):
     return [i + 1 for i, days in enumerate(schedule) \
@@ -80,35 +80,35 @@ def bp2_last_month(cases):
     due = lambda case: 1 if _mother_due_in_window(case, 75) else 0
     # make sure they've done 2 bp visits
     done = lambda case: 1 if len(filter(lambda a: visit_is(a, 'bp'), case.actions)) >= 2 else 0
-    return _done_due_count(cases, due, done)    
+    return _num_denom_count(cases, due, done)    
     
 def bp3_last_month(cases):
     due = lambda case: 1 if _mother_due_in_window(case, 45) else 0
     # make sure they've done 2 bp visits
     done = lambda case: 1 if len(filter(lambda a: visit_is(a, 'bp'), case.actions)) >= 3 else 0
-    return _done_due_count(cases, due, done)    
+    return _num_denom_count(cases, due, done)    
     
 def pnc_last_month(cases):
     pnc_schedule = (1, 3, 6)
     due = lambda case: len(_visits_due(case, pnc_schedule))
     done = lambda case: _visits_done(case, pnc_schedule, "pnc")
-    return _done_due_count(cases, done, due)
+    return _num_denom_count(cases, done, due)
 
 def eb_last_month(cases):
     eb_schedule = (14, 28, 60, 90, 120, 150)
     due = lambda case: len(_visits_due(case, eb_schedule))
     done = lambda case: _visits_done(case, eb_schedule, "eb")
-    return _done_due_count(cases, done, due)
+    return _num_denom_count(cases, done, due)
 
 def cf_last_month(cases):
     cf_schedule_in_months = (6, 7, 8, 9, 12, 15, 18)
     cf_schedule = (m * 30 for m in cf_schedule_in_months)
     due = lambda case: len(_visits_due(case, cf_schedule))
     done = lambda case: _visits_done(case, cf_schedule, "cf")
-    return _done_due_count(cases, done, due)
+    return _num_denom_count(cases, done, due)
 
 def hd_day(cases):
     valid_cases = filter(lambda case: _delivered_at_in_timeframe(case, 'home', 30), cases)
-    due = len(valid_cases)
-    done = len(filter(lambda case:_visited_in_timeframe_of_birth(case, 1) , valid_cases))
-    return _done_due(done, due)
+    denom = len(valid_cases)
+    num = len(filter(lambda case:_visited_in_timeframe_of_birth(case, 1) , valid_cases))
+    return _num_denom(num, denom)
