3ce1121a977ce5c9708b31122ba462239ed3ad91
Biyeun Buczyk <biyeun@dimagi.com>
2012-12-06 20:06:58 +0100

    to help reports (specifically the app/module/form) filter know about remote apps.

---
+++ corehq/couchapps/reports_apps/views/remote/map.js
@@ -0,0 +1,17 @@
+function (doc) {
+    if ((doc.doc_type === "RemoteApp" || doc.doc_type === "RemoteApp-Deleted")
+        && doc.copy_of === null) {
+        var emit_entry = {
+            app: {
+                names: doc.name,
+                id: doc._id,
+                langs: doc.langs,
+            },
+            profile_url: doc.profile_url,
+            is_deleted: !!(doc.doc_type === "RemoteApp-Deleted"),
+        };
+        var status = (emit_entry.is_deleted) ? "deleted" : "active";
+        emit(["", doc.domain, doc._id], emit_entry);
+        emit(["status", doc.domain, status, doc._id], emit_entry);
+    }
+}
