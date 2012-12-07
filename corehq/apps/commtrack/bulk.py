0558f4af877fd4646a4b89e79b4f5734107538a5
Drew Roos <droos@dimagi.com>
2012-12-07 16:34:32 -0500

    notify on exceptions during bulk import

--- corehq/apps/commtrack/bulk.py
+++ corehq/apps/commtrack/bulk.py
@@ -9,6 +9,7 @@ from casexml.apps.case.models import CommCareCase
 from dimagi.utils.couch.database import get_db
 from dimagi.utils.couch.loosechange import map_reduce
 from corehq.apps.commtrack import sms
+from dimagi.utils.logging import notify_exception
 
 def set_error(row, msg, override=False):
     """set an error message on a stock report to be imported"""
@@ -211,6 +212,7 @@ def process_loc(domain, loc, rows, data_cols):
             import_row(row, data_cols, domain)
             set_error(row, 'SUCCESS row imported')
         except Exception, e:
+            notify_exception(None, 'error during bulk stock report import')
             set_error(row, 'ERROR during import: %s' % str(e))
             set_error_bulk(rows, 'SKIPPED remaining rows due to unexpected error')
             break
