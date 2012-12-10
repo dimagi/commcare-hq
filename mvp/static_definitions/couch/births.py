6573999c6cf82f9fb7afbd463b76bed558adc7e5
Biyeun Buczyk <biyeun@dimagi.com>
2012-12-10 21:41:06 +0100

    changes due to changes with the mvp models

--- mvp/static_definitions/couch/births.py
+++ mvp/static_definitions/couch/births.py
@@ -3,13 +3,6 @@ APP_NAME = "mvp_births"
 BIRTH_INDICATORS = dict(
     app=APP_NAME,
     indicators=dict(
-        child_cases_by_dob=dict(
-            num_newborns=dict(
-                description="No. of newborns",
-                title="# Newborns",
-                indicator_key="dob",
-            )
-        ),
         child_registrations=dict(
             num_births_registered=dict(
                 description="No. of Births Registered",
@@ -44,34 +37,41 @@ ACTIVE_CHILD_CASES_BY_AGE_INDICATORS = dict(
                 description="No. of children Under 1 year of age.",
                 title="# Under-1s",
                 indicator_key="",
-                age_in_days=365,
+                max_age_in_days=365,
             ),
             num_births_recorded=dict(
                 description="Number of births recorded during the time period.",
                 title="# Births",
-                age_in_days=31,
-                filter_by_active=False,
+                max_age_in_days=31,
+                show_active_only=False,
                 indicator_key="opened_on"
             ),
             num_active_under5=dict(
                 description="No. of Under-5 Children during this timespan",
                 title="# Under-5s",
-                age_in_days=1827,
+                max_age_in_days=1827,
                 indicator_key="",
             ),
             under5_cases_30days=dict(
                 description="No. of Under-5 Children in the past 30 days",
                 title="# Under-5s",
-                age_in_days=1827,
+                max_age_in_days=1827,
                 indicator_key="",
                 fixed_datespan_months=1,
             ),
             neonate_cases_7days=dict(
                 description="No. of Neonate Newborns in the past 7 days",
                 title="# Under-5s",
-                age_in_days=31,
+                max_age_in_days=31,
                 indicator_key="",
                 fixed_datespan_days=7,
+            ),
+            num_newborns=dict(
+                description="No. of newborns",
+                title="# Newborns",
+                indicator_key="opened_on",
+                is_dob_in_datespan=True,
+                show_active_only=False,
             ),
         )
     )
