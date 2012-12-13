bb6dd8e68094764f9accc795eba341c2200c04b7
Biyeun Buczyk <biyeun@dimagi.com>
2012-12-13 12:26:04 +0100

    separate the prime all views part so that i can call this command elsewhere...say in the mvp update script

--- dimagi/utils/management/commands/prime_views.py
+++ dimagi/utils/management/commands/prime_views.py
@@ -1,4 +1,5 @@
 from gevent import monkey; monkey.patch_all()
+import sys
 import gevent
 from restkit.session import set_session
 set_session("gevent")
@@ -42,43 +43,53 @@ def get_unique_dbs():
     return ret
 
 
-def do_prime(app_label, design_doc_name, view_name):
+def do_prime(app_label, design_doc_name, view_name, verbose=False):
     db = get_db(app_label)
+    if verbose:
+        sys.stdout.write('<')
+        sys.stdout.flush()
 #    print "start priming %s:%s/%s" % (app_label, design_doc_name, view_name)
     list(db.view('%s/%s' % (design_doc_name, view_name), limit=0))
+    if verbose:
+        sys.stdout.write('>')
+        sys.stdout.flush()
 #    print "done priming %s:%s/%s" % (app_label, design_doc_name, view_name)
 
 class Command(BaseCommand):
     help = 'Sync live design docs with gevent'
 
     def handle(self, *args, **options):
+#        print "priming views"
         pool = Pool(POOL_SIZE)
 
-
-
         while True:
-            unique_dbs = get_unique_dbs()
-            for app in unique_dbs:
-                try:
-                    db = get_db(app)
-                    design_docs = db.view(DESIGN_DOC_VIEW, startkey=DESIGN_SK, endkey=DESIGN_EK, include_docs=True).all()
-                    for res in design_docs:
-                        design_doc = res['doc']
-                        design_doc_name = design_doc['_id'].split('/')[-1] # _design/app_name
-                        if design_doc_name.endswith('-tmp'):
-                            #it's a dangling -tmp preindex view, skip
-                            continue
-                        else:
-                            views = design_doc.get('views', {})
-                            #get the first view
-                            for view_name in views.keys():
-                                pool.spawn(do_prime, app, design_doc_name, view_name)
-                                #and that's it for that design doc
-                                break
-                except Exception, ex:
-                    #print "Got an exception but ignoring: %s" % ex
-                    pass
+            self.prime_everything(pool)
             gevent.sleep(REPEAT_INTERVAL)
 #            print "exited main app loop - repeating after %s pause" % REPEAT_INTERVAL
 #        print "done!"
 
+    def prime_everything(self, pool=None):
+        unique_dbs = get_unique_dbs()
+        for app in unique_dbs:
+            try:
+                db = get_db(app)
+                design_docs = db.view(DESIGN_DOC_VIEW, startkey=DESIGN_SK, endkey=DESIGN_EK, include_docs=True).all()
+                for res in design_docs:
+                    design_doc = res['doc']
+                    design_doc_name = design_doc['_id'].split('/')[-1] # _design/app_name
+                    if design_doc_name.endswith('-tmp'):
+                        #it's a dangling -tmp preindex view, skip
+                        continue
+                    else:
+                        views = design_doc.get('views', {})
+                        #get the first view
+                        for view_name in views.keys():
+                            if pool:
+                                pool.spawn(do_prime, app, design_doc_name, view_name)
+                            else:
+                                do_prime(app,design_doc_name, view_name, verbose=True)
+                            #and that's it for that design doc
+                            break
+            except Exception, ex:
+                #print "Got an exception but ignoring: %s" % ex
+                pass
