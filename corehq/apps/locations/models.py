2ba743a160b17d151d7360a87d77d468c2fe5580
Cory Zue <czue@dimagi.com>
2012-12-17 16:18:55 +0530

    comment about slowness, pep 8

--- corehq/apps/locations/models.py
+++ corehq/apps/locations/models.py
@@ -85,7 +85,9 @@ class Location(Document):
 
 def location_tree(domain):
     """build a hierarchical tree of the entire location structure for a domain"""
-    locs = Location.view('locations/hierarchy', startkey=[domain], endkey=[domain, {}], reduce=False, include_docs=True).all()
+    # this is going to be extremely slow as the number of locations gets big
+    locs = Location.view('locations/hierarchy', startkey=[domain], endkey=[domain, {}],
+                         reduce=False, include_docs=True).all()
     locs.sort(key=lambda l: l.path) # parents must appear before their children; couch should
     # return docs in the correct order, but, just to be safe...
     locs_by_id = dict((l._id, l) for l in locs)
