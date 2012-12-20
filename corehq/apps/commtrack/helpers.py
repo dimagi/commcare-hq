3c542d2e5f62102d9eb458cb547ca9052cce336b
Drew Roos <droos@dimagi.com>
2012-12-20 15:14:24 +0200

    create supply point product sub-cases on demand

--- corehq/apps/commtrack/helpers.py
+++ corehq/apps/commtrack/helpers.py
@@ -18,32 +18,30 @@ def make_product(domain, name, code):
     p.save()
     return p
 
-def make_supply_point(domain, location, code, all_products=True):
+# TODO use case-xml case creation workflow
+def make_supply_point(domain, location, code):
     c = CommCareCase()
     c.domain = domain
     c.site_code = code
     c.type = 'supply-point'
     c.bind_to_location(location)
     c.save()
-
-    products = []
-    if all_products:
-        products = get_db().view('commtrack/products', startkey=[domain], endkey=[domain, {}])
-    for p in products:
-        prod_id = p['id']
-        pc = CommCareCase()
-        pc.domain = domain
-        pc.type = 'supply-point-product'
-        pc.product = prod_id
-        ix = CommCareCaseIndex()
-        ix.identifier = 'parent'
-        ix.referenced_type = 'supply-point'
-        ix.referenced_id = c._id
-        pc.indices = [ix]
-        pc.bind_to_location(location)
-        pc.save()
-
     return c
+
+# TODO use case-xml case creation workflow
+def make_supply_point_product(supply_point_case, product_uuid):
+    pc = CommCareCase()
+    pc.domain = supply_point_case.domain
+    pc.type = 'supply-point-product'
+    pc.product = product_uuid
+    ix = CommCareCaseIndex()
+    ix.identifier = 'parent'
+    ix.referenced_type = 'supply-point'
+    ix.referenced_id = supply_point_case._id
+    pc.indices = [ix]
+    pc.location_ = supply_point_case.location_
+    pc.save()
+    return pc
 
 def make_psi_config(domain):
     c = CommtrackConfig(
