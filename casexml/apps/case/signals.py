e1491992749dacf9db46c28e6ef74180191ea946
Biyeun Buczyk <biyeun@dimagi.com>
2012-12-21 01:39:36 +0100

    reduce document update conflict with pillows

--- casexml/apps/case/signals.py
+++ casexml/apps/case/signals.py
@@ -6,6 +6,7 @@ def process_cases(sender, xform, **kwargs):
     """Creates or updates case objects which live outside of the form"""
     # recursive import fail
     from casexml.apps.case.xform import get_or_update_cases
+    # avoid Document conflicts
     cases = get_or_update_cases(xform).values()
     # attach domain if it's there
     if hasattr(xform, "domain"):
@@ -20,12 +21,16 @@ def process_cases(sender, xform, **kwargs):
     map(lambda case: case.save(), cases)
     
     # HACK -- figure out how to do this more properly
+    # todo: create a pillow for this
     if cases:
         case = cases[0]
         if case.location_ is not None:
             # should probably store this in computed_
-            xform.location_ = list(case.location_)
-            xform.save()
+            # re-fetch the xform to avoid document update conflicts
+            from couchforms.models import XFormInstance
+            xform_new = XFormInstance.get(xform._id)
+            xform_new.location_ = list(case.location_)
+            xform_new.save()
 
     # handle updating the sync records for apps that use sync mode
     if hasattr(xform, "last_sync_token") and xform.last_sync_token:
