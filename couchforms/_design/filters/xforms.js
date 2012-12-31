cc9b62194ae3b3777d36ff0d059c28534dd5c750
dmyung <dmyung@dimagi.com>
2012-12-31 02:51:34 -0500

    adding check to remove devicelog from the xforms.js filter

--- couchforms/_design/filters/xforms.js
+++ couchforms/_design/filters/xforms.js
@@ -2,6 +2,6 @@
  * Filter that only returns xforms.  Used by the change listener.  
  */
 function(doc, req)
-{   
-    return (doc["doc_type"] == "XFormInstance");
+{
+    return (doc["doc_type"] == "XFormInstance" && doc['xmlns'] !== "http://code.javarosa.org/devicereport");
 }
