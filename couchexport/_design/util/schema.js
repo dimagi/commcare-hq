d099b604c9741a02432ce4113c79c42897ed269a
Cory Zue <czue@dimagi.com>
2013-01-04 12:03:15 -0500

    clean javascript view code and remove dead stuff

--- couchexport/_design/util/schema.js
+++ couchexport/_design/util/schema.js
@@ -1,16 +1,30 @@
-function get_kind(obj) {
-    var c = uneval(obj)[0];
-    if (obj == null || obj == "") return 'null';
-    else if (c == '(') return 'dict';
-    else if (c == '[') return 'list';
-    else return "string";
+function typeOf(value) {
+    var s = typeof value;
+    if (s === 'object') {
+        if (value) {
+            if (Object.prototype.toString.call(value) === '[object Array]') {
+                s = 'array';
+            }
+        } else {
+            s = 'null';
+        }
+    }
+    return s;
 }
 
-function get_export_tag_value(doc) {
+function isArray(obj) {
+    return typeOf(obj) === "array";
+}
+
+function isString(obj) {
+    return typeOf(obj) === "string";
+}
+
+function getExportTagValue(doc) {
     if (doc["#export_tag"]) {
         var pattern = doc["#export_tag"];
         var key;
-        if (is_string(pattern)) {
+        if (isString(pattern)) {
             key = doc[pattern]
         }
         else {
@@ -25,7 +39,7 @@ function get_export_tag_value(doc) {
     return null;
 }
 
-function get_export_date_value(doc) {
+function getExportDateValue(doc) {
     if (doc["#export_tag"]) {
         var property;
         // this is the "generic" way to do this
@@ -55,37 +69,4 @@ function get_export_date_value(doc) {
         }
     }
     return null;
-}
-    
-function get_schema(doc) {
-    var kind = get_kind(doc);
-    var schema;
-    if (kind == 'dict') {
-        schema = {};
-        for (var key in doc) {
-            schema[key] = get_schema(doc[key]);
-        }
-    }
-    else if (kind == 'list') {
-        schema = [];
-        for (var i in doc) {
-            schema[i] = get_schema(doc[i]);
-        }
-    }
-    else if (kind == 'string'){
-        schema = "string";
-    }
-    else if (kind == 'null') {
-        schema = null;
-    }
-    return reconcile(null, schema, 0);
-}
-
-function get_doc_schema(doc) {
-    var export_tag_val = get_export_tag_value(doc);
-    if (export_tag_val) {
-        schema = get_schema(doc);
-        schema["#export_tag_value"] = export_tag_val;
-        return schema;
-    }
 }
--- couchexport/_design/views/schema_index/map.js
+++ couchexport/_design/views/schema_index/map.js
@@ -1,18 +1,16 @@
 function(doc) {
-    // !code util/types.js
     // !code util/schema.js
-    tag = get_export_tag_value(doc); 
+    tag = getExportTagValue(doc);
     if (tag) {
         var key = [];
-        if (get_kind(tag) == 'list') {
+        if (isArray(tag)) {
             for (var i = 0; i < tag.length; i++) {
                 key.push(tag[i]);
             }
         } else {
             key.push(tag);
         }
-        key.push(get_export_date_value(doc));
+        key.push(getExportDateValue(doc));
         emit(key, null);
     }
-    
 }
