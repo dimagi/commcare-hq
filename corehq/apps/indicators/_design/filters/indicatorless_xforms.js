1494ad593725a018fb2df2c6bca6deccff0f44a7
Biyeun Buczyk <biyeun@dimagi.com>
2013-01-11 16:31:55 +0100

    check document type at beginning of filter

--- corehq/apps/indicators/_design/filters/indicatorless_xforms.js
+++ corehq/apps/indicators/_design/filters/indicatorless_xforms.js
@@ -1,30 +1,30 @@
 /*
  * Filter that only returns cases without indicators in the specified namespace.  Used by the change listener.
  */
-function(doc, req)
-{
-    var namespaces = [];
-    if (req.query.namespaces) {
-        namespaces = req.query.namespaces.split(',');
-    }
+function(doc, req) {
+    if (doc["doc_type"] === "XFormInstance") {
+        var namespaces = [];
+        if (req.query.namespaces) {
+            namespaces = req.query.namespaces.split(',');
+        }
 
-    var computed_namespaces = [];
-    if (doc.computed_) {
-        computed_namespaces = Object.keys(doc.computed_);
-    }
+        var computed_namespaces = [];
+        if (doc.computed_) {
+            computed_namespaces = Object.keys(doc.computed_);
+        }
 
-    var domains = [];
-    if (req.query.domains) {
-        domains = req.query.domains.split(',');
-    }
+        var domains = [];
+        if (req.query.domains) {
+            domains = req.query.domains.split(',');
+        }
 
-    if (domains && namespaces) {
-        for (var n in namespaces) {
-            var namespace = namespaces[n];
-            if (doc["doc_type"] === "XFormInstance"
-                && computed_namespaces.indexOf(namespace) < 0
-                && domains.indexOf(doc.domain) >= 0) {
-                return true;
+        if (domains && namespaces) {
+            for (var n in namespaces) {
+                var namespace = namespaces[n];
+                if (computed_namespaces.indexOf(namespace) < 0
+                    && domains.indexOf(doc.domain) >= 0) {
+                    return true;
+                }
             }
         }
     }
