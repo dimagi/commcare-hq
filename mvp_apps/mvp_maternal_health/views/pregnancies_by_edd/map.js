6200b3fc9de1f4b01f6cf558d6eb4c1a0302efa8
Biyeun Buczyk <biyeun@dimagi.com>
2013-01-21 12:34:04 -0500

    removed unused view

--- mvp_apps/mvp_maternal_health/views/pregnancies_by_edd/map.js
+++
@@ -1,9 +0,0 @@
-function (doc) {
-    // !code util/mvp.js
-    if (isPregnancyCase(doc)) {
-        if (doc.edd_calc) {
-            var edd_date = new Date(doc.edd_calc);
-            emit_standard(doc, edd_date, ["pregnancy"], []);
-        }
-    }
-}
