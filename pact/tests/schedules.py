5ea49eb1cd3425bb3331e0177fdea45acd71d55d
dmyung <dmyung@dimagi.com>
2013-01-22 13:48:05 -0500

    initial work on schedule unit tests

--- pact/tests/schedules.py
+++ pact/tests/schedules.py
@@ -1,7 +1,104 @@
 from django.test import TestCase
 from pact.lib.quicksect import IntervalNode
 from datetime import datetime, timedelta
+from pact.models import PactPatientCase, CDotWeeklySchedule
 
+WEEKLY_SCHEDULE_KEY = 'pact_weekly_schedule'
+WEEKLY_SCHEDULE_EXAMPLES = [
+        {
+            "comment": "",
+            "doc_type": "CDotWeeklySchedule",
+            "edited_by": None,
+            "monday": "godfrey",
+            "started": "2010-11-19T23:37:48Z",
+            "deprecated": True,
+            "_rev": "4-94fb80c843e86b4c04d8d2e88c286b6d",
+            "friday": "godfrey",
+            "wednesday": "godfrey",
+            "created_by": "admin",
+            "sunday": "godfrey",
+            "ended": "2010-11-22T13:27:02Z",
+            "schedule_id": "04b34f9cf43611df8f35005056977568",
+            "thursday": "godfrey",
+            "tuesday": "godfrey",
+            "_id": "66a4f2d0e9d5467e34122514c36145f1",
+            "saturday": "godfrey"
+        },
+        {
+            "comment": "",
+            "doc_type": "CDotWeeklySchedule",
+            "edited_by": None,
+            "monday": "cm326",
+            "started": "2010-11-22T13:27:02Z",
+            "deprecated": True,
+            "_rev": "4-fb554c24b08a8bed69eb36cdbb14fdf5",
+            "friday": "godfrey",
+            "wednesday": "cm326",
+            "created_by": "cm326",
+            "sunday": None,
+            "ended": "2011-01-05T15:45:01Z",
+            "schedule_id": "30af0a20f63c11df8f35005056977568",
+            "thursday": "cm326",
+            "tuesday": "cm326",
+            "_id": "55888063e298887dfe2ba223a03ef7d0",
+            "saturday": None
+        },
+        {
+            "comment": "",
+            "doc_type": "CDotWeeklySchedule",
+            "edited_by": None,
+            "monday": None,
+            "started": "2011-01-05T15:45:01Z",
+            "deprecated": True,
+            "_rev": "4-54e7f76747ae901526c9d1b5f4ea757e",
+            "friday": "ctsims",
+            "wednesday": None,
+            "created_by": "cm326",
+            "sunday": None,
+            "ended": "2011-01-26T19:04:05Z",
+            "schedule_id": "c213316218e211e08f35005056977568",
+            "thursday": None,
+            "tuesday": None,
+            "_id": "55888063e298887dfe2ba223a0acc575",
+            "saturday": None
+        },
+        {
+            "comment": "testing missing submission flags",
+            "doc_type": "CDotWeeklySchedule",
+            "edited_by": None,
+            "monday": None,
+            "started": "2011-01-26T19:04:05Z",
+            "deprecated": True,
+            "_rev": "2-94839dae8e39173f30471b1c8a60a3dc",
+            "friday": "cm326",
+            "wednesday": "cm326",
+            "created_by": "cm326",
+            "sunday": "ctsims",
+            "ended": "2011-02-25T14:05:32Z",
+            "schedule_id": "0b72b8c6297f11e08f35005056977568",
+            "thursday": "cm326",
+            "tuesday": "cm326",
+            "_id": "93bb5dd2590222718ee975c11322a502",
+            "saturday": None
+        },
+        {
+            "comment": "",
+            "doc_type": "CDotWeeklySchedule",
+            "schedule_id": "2481c602401f11e0af2e005056977568",
+            "edited_by": None,
+            "monday": None,
+            "started": "2011-02-25T14:05:32Z",
+            "deprecated": False,
+            "tuesday": "ctsims",
+            "friday": None,
+            "wednesday": None,
+            "thursday": None,
+            "ended": None,
+            "sunday": None,
+            "created_by": "cm326",
+            "saturday": None
+        }
+    ]
 
 class BasicCaseTests(TestCase):
     def setUp(self):
@@ -42,4 +139,25 @@ class BasicCaseTests(TestCase):
                 #print "node hits per interval: %d" % self.node_hits
                 self.node_hits = 0
                 start_check = check_time
+
+
+
+    def testPatientSchedule(self):
+
+        test_patient = PactPatientCase()
+        test_patient.computed_ = {}
+
+        test_patient.computed_[WEEKLY_SCHEDULE_KEY] = [CDotWeeklySchedule.wrap(x) for x in WEEKLY_SCHEDULE_EXAMPLES]
+
+
+        #add a new schedule, verify tail is x-present, and [-2] is x-x
+
+        new_schedule = None
+
+
+
+
+
+
+        pass
 
