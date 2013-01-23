871bd9241f32d336b721f8c9f2cb65c512cf6a39
Biyeun Buczyk <biyeun@dimagi.com>
2013-01-23 14:47:11 -0500

    pull from aliased indicators instead of directly from the form

--- mvp_apps/mvp_births/views/child_registrations/map.js
+++ mvp_apps/mvp_births/views/child_registrations/map.js
@@ -2,19 +2,27 @@ function (doc) {
     // !code util/mvp.js
     if (isChildRegistrationForm(doc)) {
         var meta = doc.form.meta,
+            indicators = get_indicators(doc),
             indicator_keys = new Array();
 
         var reg_date = new Date(meta.timeEnd),
-            birth_weight = doc.form.weight_at_birth;
+            birth_weight = null;
+
+        try {
+            birth_weight = (indicators.weight_at_birth && indicators.weight_at_birth.value) ? parseInt(indicators.weight_at_birth.value) : 0;
+        } catch (e) {
+            // do nothing
+        }
+
 
         indicator_keys.push("registration");
-        if (doc.form.delivered_in_facility === 'yes') {
+        if (indicators.delivered_in_facility
+            && indicators.delivered_in_facility.value === 'yes') {
             indicator_keys.push("facility_delivery");
         }
-        if (birth_weight) {
+        if (birth_weight > 0) {
             indicator_keys.push("birth_weight");
-            var weight = parseInt(birth_weight);
-            if (weight < 2.5) {
+            if (birth_weight < 2.5) {
                 indicator_keys.push("birth_weight low");
             }
         }
