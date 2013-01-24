0954992e57418001de70693fc48ffa3ef712a3e7
yedi <yedispaghetti@gmail.com>
2013-01-24 15:47:25 -0500

    fix sensitization report and add repeat filters to all my repeat-functions

--- psi/_design/views/sensitization/map.js
+++ psi/_design/views/sensitization/map.js
@@ -9,22 +9,29 @@ function (doc) {
             return;
         }
 
-        var data = {
-            sessions: count_in_repeats(form.training_sessions, "number_of_blm_attended") +
-                        get_repeats(form.training_sessions, function(data) {
-                            return data['type_of_sensitization'] === 'vhnd';
-                        }).length,
-            ayush_doctors: count_in_repeats(form.training_sessions, "num_ayush_doctors"),
-            mbbs_doctors: count_in_repeats(form.training_sessions, "num_mbbs_doctors"),
-            asha_supervisors: count_in_repeats(form.training_sessions, "num_asha_supervisors"),
-            ashas: count_in_repeats(form.training_sessions, "num_ashas"),
-            awws: count_in_repeats(form.training_sessions, "num_awws"),
-            other: count_in_repeats(form.training_sessions, "num_other"),
-            attendees: count_in_repeats(form.training_sessions, "number_attendees")
-        };
+        var blocks = get_from_repeats(form.training_sessions, "training_block");
+        for (var i = 0; i < blocks.length; i++) {
+            var block = blocks[i];
+            var rf_func = function(repeat) {
+                return repeat['training_block'] === block;
+            };
+            var data = {
+                sessions: count_in_repeats(form.training_sessions, "number_of_blm_attended", rf_func) +
+                    get_repeats(form.training_sessions, function(data) {
+                        return rf_func(data) && data['type_of_sensitization'] === 'vhnd'
+                    }).length,
+                ayush_doctors: count_in_repeats(form.training_sessions, "num_ayush_doctors", rf_func),
+                mbbs_doctors: count_in_repeats(form.training_sessions, "num_mbbs_doctors", rf_func),
+                asha_supervisors: count_in_repeats(form.training_sessions, "num_asha_supervisors", rf_func),
+                ashas: count_in_repeats(form.training_sessions, "num_ashas", rf_func),
+                awws: count_in_repeats(form.training_sessions, "num_awws", rf_func),
+                other: count_in_repeats(form.training_sessions, "num_other", rf_func),
+                attendees: count_in_repeats(form.training_sessions, "number_attendees", rf_func)
+            };
 
-        var opened_on = form.meta.timeEnd;
+            var opened_on = form.meta.timeEnd;
 
-        emit_array([form.training_state, form.training_district, form.training_block], [opened_on], data);
+            emit_array([form.training_state, form.training_district, block], [opened_on], data);
+        }
     }
 }
