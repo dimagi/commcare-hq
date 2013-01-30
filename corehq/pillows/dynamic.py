694a165eda02245a329cb9974e80be4082acf8d6
dmyung <dmyung@dimagi.com>
2013-01-30 15:11:58 -0500

    update case_mapping to have case.name be dual field mapped.

--- corehq/pillows/dynamic.py
+++ corehq/pillows/dynamic.py
@@ -64,6 +64,7 @@ default_special_types = {
 
 case_special_types = {
     "domain":type_exact_match_string("domain", dual=True),
+    "name":type_exact_match_string("name", dual=True),
     "xform_ids": {"type": "string", "index": "not_analyzed"},
     "xform_id": {"type": "string", "index": "not_analyzed"},
     #to extend, use this and add special date formats here...
