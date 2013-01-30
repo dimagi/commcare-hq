80efd7139b079963914b6ba670cb6c55c0f6012c
Biyeun Buczyk <biyeun@dimagi.com>
2013-01-30 18:20:52 -0500

    updates for A2-10 and A2-11 indicators

--- mvp_apps/mvp_child_health/views/child_muac/map.js
+++ mvp_apps/mvp_child_health/views/child_muac/map.js
@@ -3,36 +3,29 @@ function (doc) {
     if(isChildVisitForm(doc)) {
         var indicators = get_indicators(doc);
 
-        if (indicators.child_dob && indicators.child_dob.value) {
+        if (indicators.child_dob && indicators.child_dob.value &&
+            indicators.last_muac && indicators.last_muac.value) {
             var meta = doc.form.meta,
                 indicator_entries = {},
                 age = get_age_from_dob(indicators.child_dob.value, meta.timeEnd),
-                visit_date = new Date(meta.timeEnd),
+                last_muac_date = new Date(indicators.last_muac.value),
                 case_id = get_case_id(doc);
 
-            if (age > 180*MS_IN_DAY && age < 1770*MS_IN_DAY && indicators.muac.value) {
-                // MUAC reading taken during visit
-                indicator_entries["muac_reading"] = 1;
-                try {
-                    var muac_value = parseFloat(indicators.muac.value);
-                    if (muac_value < 125.0) {
-                        indicator_entries["muac_wasting"] = 1;
-                    }
-                } catch (err) {
-                    log("MUAC value could not be obtained");
-                }
-
-                if (doc.form.last_muac) {
-                    var last_muac = new Date(doc.form.last_muac);
-                    if (last_muac <= visit_date) {
-                        var difference = visit_date.getTime() - last_muac.getTime();
-                        if (difference <= 90*MS_IN_DAY){
-                            indicator_entries["routine_muac"] = case_id;
+            if (age > 180*MS_IN_DAY && age < 1770*MS_IN_DAY) {
+                if (indicators.cur_muac && indicators.cur_muac.value) {
+                    try {
+                        var cur_muac = parseFloat(indicators.cur_muac.value);
+                        indicator_entries["muac_reading"] = case_id;
+                        if (cur_muac < 125.0) {
+                            indicator_entries["muac_wasting"] = case_id;
                         }
+                    } catch (e) {
+                        // do nothing
                     }
                 }
+                indicator_entries["routine_muac"] = case_id;
             }
-            emit_special(doc, visit_date, indicator_entries, [doc._id]);
+            emit_special(doc, last_muac_date, indicator_entries, [doc._id]);
         }
 
     }
