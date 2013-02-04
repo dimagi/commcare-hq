b71ac85f6e77dd59043c2286df94f3a9f13ddf88
Kenn Knowles <kenn.knowles@gmail.com>
2013-02-04 16:11:41 -0500

    Include all associated users as field device_users in phonelog view

--- phonelog/_design/views/devicelog_data/map.js
+++ phonelog/_design/views/devicelog_data/map.js
@@ -10,10 +10,18 @@ function(doc) {
     var error_tags = ['exception', 'rms-repair', 'rms-spill'],
         warning_tags = ['case-recreate', 'permissions_notify', 'time message'];
     if (doc.xmlns == 'http://code.javarosa.org/devicereport') {
+        var user_subreport_usernames = [];
+        if (doc.form.user_subreport) {
+            for (var i in doc.form.user_subreport.user) {
+                user_subreport_usernames.push(doc.form.user_subreport.user[i].username);
+            }
+        }
+
         var logged_in_user = "unknown";
         for (var i in doc.form.log_subreport.log) {
             // need to clone because you can't set the property on the actual doc
             var entry = clone(doc.form.log_subreport.log[i]);
+            entry.device_users = user_subreport_usernames;
             entry.version = doc.form.app_version;
             entry.device_id = doc.form.device_id;
 
