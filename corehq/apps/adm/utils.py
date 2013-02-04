e1825ddd755b52e2aba831751a6c8ee823f99108
Biyeun Buczyk <biyeun@dimagi.com>
2013-02-04 10:51:02 -0500

    adm viewable same as reports, though i think we should add in an ADM permission...

--- corehq/apps/adm/utils.py
+++ corehq/apps/adm/utils.py
@@ -1,10 +1,9 @@
-from django.conf import settings
 
 def show_adm_nav(domain, request):
-    enabled_projects = []
-    if hasattr(settings, 'ADM_ENABLED_PROJECTS'):
-        enabled_projects = settings.ADM_ENABLED_PROJECTS
-    return request.couch_user.is_superuser or (domain in enabled_projects)
+    print request.couch_user.can_view_reports()
+    return domain and\
+           (hasattr(request, 'project') and not request.project.is_snapshot) and\
+           (request.couch_user.can_view_reports() or request.couch_user.get_viewable_reports())
 
 
 def standard_start_end_key(key, datespan=None):
