e208e7ad4f74782cb6f58473b2f6edd7abc976a7
Kenn Knowles <kenn.knowles@gmail.com>
2013-02-14 16:24:28 -0500

    Fix name reverse lookup in resources taking kwargs

--- corehq/apps/api/resources/v0_4.py
+++ corehq/apps/api/resources/v0_4.py
@@ -1,4 +1,5 @@
 
+from django.core.urlresolvers import NoReverseMatch
 from tastypie import fields
 from couchforms.models import XFormInstance
 
@@ -12,11 +13,43 @@ class XFormInstanceResource(v0_3.XFormInstanceResource):
     uiversion = fields.CharField(attribute='uiversion', blank=True, null=True)
     metadata = fields.DictField(attribute='metadata', blank=True, null=True)
 
+    def get_list(self, request, **kwargs):
+        """
+        Exactly copied from https://github.com/toastdriven/django-tastypie/blob/v0.9.11/tastypie/resources.py#L1018
+        (BSD licensed) and modified to pass the kwargs to `get_resource_list_uri`
+        """        
+        objects = self.obj_get_list(request=request, **self.remove_api_resource_names(kwargs))
+        sorted_objects = self.apply_sorting(objects, options=request.GET)
+
+        paginator = self._meta.paginator_class(request.GET, sorted_objects, resource_uri=self.get_resource_list_uri(request, **kwargs), limit=self._meta.limit)
+        to_be_serialized = paginator.page()
+
+        # Dehydrate the bundles in preparation for serialization.
+        bundles = [self.build_bundle(obj=obj, request=request) for obj in to_be_serialized['objects']]
+        to_be_serialized['objects'] = [self.full_dehydrate(bundle) for bundle in bundles]
+        to_be_serialized = self.alter_list_data_to_serialize(request, to_be_serialized)
+        return self.create_response(request, to_be_serialized)
+
     def obj_get_list(self, request, domain, **kwargs):
-        
         return ESQuerySet(payload = es_search(request, domain),
                           model = XFormInstance, 
                           es_client=XFormES(domain)) # Not that XFormES is used only as an ES client, for `run_query` against the proper index
+
+    def get_resource_list_uri(self, request=None, **kwargs):
+        """
+        Exactly copied from https://github.com/toastdriven/django-tastypie/blob/v0.9.11/tastypie/resources.py#L601
+        (BSD licensed) and modified to use the kwargs
+        """
+        kwargs = dict(kwargs)
+        kwargs['resource_name'] = self._meta.resource_name
+
+        if self._meta.api_name is not None:
+            kwargs['api_name'] = self._meta.api_name
+            
+        try:
+            return self._build_reverse_url("api_dispatch_list", kwargs=kwargs)
+        except NoReverseMatch:
+            return None
 
     class Meta(v0_3.XFormInstanceResource.Meta):
         list_allowed_methods = ['get']
