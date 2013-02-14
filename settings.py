e3f1614b1a880e2ee3d25a2c87b38be5b4705344
Cory Zue <czue@dimagi.com>
2013-02-14 10:56:51 -0500

    switch auth context processor
    the one we reference was deprecated in django 1.2, and removed in 1.4

--- settings.py
+++ settings.py
@@ -93,7 +93,7 @@ MIDDLEWARE_CLASSES = [
 ROOT_URLCONF = "urls"
 
 TEMPLATE_CONTEXT_PROCESSORS = [
-    "django.core.context_processors.auth",
+    "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
