104895fcf5ef76bd8958a57b4b44526d71e9a6c0
Kenn Knowles <kenn.knowles@gmail.com>
2013-02-15 15:10:27 -0500

    CC the inviter of a web user

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -1664,7 +1664,7 @@ class DomainInvitation(Invitation):
         text_content = render_to_string("domain/email/domain_invite.txt", params)
         html_content = render_to_string("domain/email/domain_invite.html", params)
         subject = 'Invitation from %s to join CommCareHQ' % self.get_inviter().formatted_name
-        send_HTML_email(subject, self.email, html_content, text_content=text_content)
+        send_HTML_email(subject, self.email, html_content, text_content=text_content, cc=[self.get_inviter().get_email()])
 
     @classmethod
     def by_domain(cls, domain, is_active=True):
