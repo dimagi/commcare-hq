278d103c68ae8b05d2e005883332b6edaff437a7
Mike White <m@mwhite.info>
2013-02-25 17:21:20 -0500

    Revert "Merge pull request #639 from dimagi/ui-config"
    This reverts commit 6f7bff59a3980085637467ddc100627935767995, reversing
    changes made to 7f3e1197367a75d489521dc4c45a7fd47212530e.

--- corehq/__init__.py
+++ corehq/__init__.py
@@ -7,13 +7,6 @@ from corehq.apps.reports.commtrack import psi_prototype
 from django.utils.translation import ugettext_noop as _
 
 REPORTS = (
-    (_("Commtrack"), (
-        psi_prototype.VisitReport,
-        psi_prototype.SalesAndConsumptionReport,
-        psi_prototype.CumulativeSalesAndConsumptionReport,
-        psi_prototype.StockOutReport,
-        psi_prototype.StockReportExport,
-    )),
     (_("Monitor Workers"), (
         monitoring.DailyFormStatsReport,
         monitoring.SubmissionsByFormReport,
@@ -43,6 +36,13 @@ REPORTS = (
         sms.MessageLogReport,
         ivr.CallLogReport,
     )),
+    (_("Commtrack"), (
+        psi_prototype.VisitReport,
+        psi_prototype.SalesAndConsumptionReport,
+        psi_prototype.CumulativeSalesAndConsumptionReport,
+        psi_prototype.StockOutReport,
+        psi_prototype.StockReportExport,
+    ))
 )
 
 from corehq.apps.data_interfaces.interfaces import CaseReassignmentInterface
@@ -105,7 +105,6 @@ TABS = (
     ApplicationsTab,
     CloudcareTab,
     MessagesTab,
-    RemindersTab,
     ProjectSettingsTab,
     AdminTab,
     ExchangeTab,
