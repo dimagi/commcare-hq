01a1fc34abaee441e00b6c71b77a4c8d8457521b
Cory Zue <czue@dimagi.com>
2013-02-25 11:02:27 -0800

    make elastic timeout configurable and bump it up for case reindex

--- corehq/apps/hqcase/management/commands/ptop_fast_reindex_cases.py
+++ corehq/apps/hqcase/management/commands/ptop_fast_reindex_cases.py
@@ -1,6 +1,3 @@
-import logging
-from django.core.mail import send_mail
-from django.core.management.base import  BaseCommand
 from casexml.apps.case.models import CommCareCase
 from corehq.apps.hqcase.management.commands.ptop_fast_reindexer import PtopReindexer
 from corehq.pillows import CasePillow
@@ -8,12 +5,10 @@ from corehq.pillows import CasePillow
 CHUNK_SIZE=500
 POOL_SIZE = 15
 
-
-
-
 class Command(PtopReindexer):
     help = "Fast reindex of case elastic index by using the case view and reindexing cases"
 
     doc_class = CommCareCase
     view_name = 'case/by_owner'
     pillow_class = CasePillow
+    es_timeout = 600
