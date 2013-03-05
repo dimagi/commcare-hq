b4f161cc7fb29ef1a41984aad1890b524fd69be9
dmyung <dmyung@dimagi.com>
2013-03-05 15:54:18 -0500

    updating pillow inits to include FullCasePillow

--- corehq/pillows/__init__.py
+++ corehq/pillows/__init__.py
@@ -1,4 +1,5 @@
 from .case import CasePillow
+from .fullcase import FullCasePillow
 from .core import DevicelogPillow, CouchlogPillow, AuditcarePillow
 from .xform import XFormPillow
 from .exchange import ExchangePillow
