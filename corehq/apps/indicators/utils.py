a218433a210c8076f0148e9f762180398c5e734d
Biyeun Buczyk <biyeun@dimagi.com>
2013-03-15 13:09:24 -0400

    response to pr

--- corehq/apps/indicators/utils.py
+++ corehq/apps/indicators/utils.py
@@ -2,13 +2,17 @@ from couchdbkit import ResourceNotFound
 from dimagi.utils.couch.database import get_db
 
 
-def get_namespaces(domain, as_choices=False):
+def get_indicator_config():
     try:
-        available_namespaces = get_db().get('INDICATOR_CONFIGURATION').get('namespaces', {})
-        return available_namespaces.get(domain, ()) if as_choices else [n[0] for n in available_namespaces.get(domain, [])]
+        return get_db().get('INDICATOR_CONFIGURATION').get('namespaces', {})
     except ResourceNotFound:
         pass
-    return []
+    return {}
+
+
+def get_namespaces(domain, as_choices=False):
+    available_namespaces = get_indicator_config()
+    return available_namespaces.get(domain, ()) if as_choices else [n[0] for n in available_namespaces.get(domain, [])]
 
 
 def get_namespace_name(domain, namespace):
@@ -18,8 +22,4 @@ def get_namespace_name(domain, namespace):
 
 
 def get_indicator_domains():
-    try:
-        return get_db().get('INDICATOR_CONFIGURATION').get('enabled_domains', [])
-    except ResourceNotFound:
-        pass
-    return []
+    return get_indicator_config().keys()
