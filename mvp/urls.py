9d33cbdc1cd8831c069927a1c2f19e761e90a178
Biyeun Buczyk <biyeun@dimagi.com>
2013-03-18 10:30:16 -0400

    one new indicator and some view updates: added num_under5 indicator also, bulk copy view is now accessible...sorry.

--- mvp/urls.py
+++ mvp/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import patterns, url
-from mvp.views import MVPIndicatorAdminCRUDFormView
+from mvp.views import MVPIndicatorAdminCRUDFormView, MVPBulkCopyIndicatorsView
 
 urlpatterns = patterns('mvp.views',
-                       url(r'^form/(?P<form_type>[\w_]+)/(?P<action>[(update)|(new)|(delete)]+)/((?P<item_id>[\w_]+)/)?$',
-                           MVPIndicatorAdminCRUDFormView.as_view(), name="mvp_indicator_def_form"),
-                       )
+    url(r'^copy/(?P<indicator_type>[\w_]+)/$', MVPBulkCopyIndicatorsView.as_view(), name="mvp_indicator_bulk_copy"),
+    url(r'^form/(?P<form_type>[\w_]+)/(?P<action>[(update)|(new)|(delete)]+)/((?P<item_id>[\w_]+)/)?$',
+        MVPIndicatorAdminCRUDFormView.as_view(), name="mvp_indicator_def_form"),
+)
