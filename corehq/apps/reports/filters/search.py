24db753250cf4777c4ec6c9cb9a55ccf24017129
dmyung <dmyung@dimagi.com>
2013-03-20 12:08:14 -0400

    setting help text option for search field, making subclassed version for inspect for case list report for search query help text, doesn't quite owrk yet htough

--- corehq/apps/reports/filters/search.py
+++ corehq/apps/reports/filters/search.py
@@ -4,11 +4,15 @@ from corehq.apps.reports.filters.base import BaseReportFilter
 class SearchFilter(BaseReportFilter):
     slug = "search_query"
     template = "reports/filters/search.html"
-    label= "Search"
+    label = "Search"
+    search_help_title = None
+    search_help_content = None
 
 
     @property
     def filter_context(self):
         return {
-            'search_query': self.request.GET.get('search_query', "")
+            'search_query': self.request.GET.get('search_query', ""),
+            'search_help_title': self.search_help_title,
+            'search_help_content': self.search_help_content
         }
