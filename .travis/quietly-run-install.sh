0de0d4eab52424f31c698809cd578548a1209986
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-22 16:55:55 -0400

    Fixes to quiet scripts

--- .travis/quietly-run-install.sh
+++ .travis/quietly-run-install.sh
@@ -1,7 +1,2 @@
 LOG=install.log
-
-(bash -ex install.sh > $LOG 2>&1 || cat $LOG) &
-
-set +x
-until [ -z "$(ps aux | egrep '[^-]install.sh')" ]; do echo -n '.'; sleep 1; done
-set -x
+(bash -ex install.sh > $LOG 2>&1) || (cat $LOG; exit 1)
