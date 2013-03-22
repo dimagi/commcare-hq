c3186b04d8f42dd0112cbdc1e2c16b5d22b05c3c
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-22 16:43:09 -0400

    Add heartbeat to install.sh as well

--- .travis/quietly-run-install.sh
+++ .travis/quietly-run-install.sh
@@ -1,2 +1,7 @@
 LOG=install.log
-sh -ex install.sh > $LOG 2>&1 || cat $LOG
+
+(bash -ex install.sh > $LOG 2>&1 || cat $LOG) &
+
+set +x
+until [ -z "$(ps aux | egrep '[^-]install.sh')" ]; do echo -n '.'; sleep 1; done
+set -x
