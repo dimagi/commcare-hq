c2a59eba4e95fe04a237d1baf6d8dd05e6f87075
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-22 15:47:34 -0400

    Adjust paths

--- .travis/quietly-install-requirements.sh
+++ .travis/quietly-install-requirements.sh
@@ -1,7 +1,3 @@
 LOG=pip.log
-
-echo Installing requirements/requirements.txt
-pip install -r ../requirements/requirements.txt > $LOG 2>&1 || cat $LOG
-
-echo Installing requirements/dev-requirements.txt
-pip install -r ../requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG
+pip install -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG
+pip install -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG
--- .travis/quietly-run-install.sh
+++ .travis/quietly-run-install.sh
@@ -1,4 +1,2 @@
 LOG=install.log
-
-echo Running install.sh
-sh -ex ../install.sh > $LOG 2>&1 || cat $LOG
+sh -ex install.sh > $LOG 2>&1 || cat $LOG
