5f41066b7fed6caa53b1970df79b5788bfeaeba5
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-22 16:36:00 -0400

    Add heartbeat to pip install

--- .travis/quietly-install-requirements.sh
+++ .travis/quietly-install-requirements.sh
@@ -1,3 +1,8 @@
 LOG=pip.log
-pip install -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG
-pip install -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG
+
+(pip install -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG) &
+(pip install -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG) &
+
+set +x
+until [ -z "$(ps aux | grep 'pip install')" ]; do echo -n '.'; sleep 1; done
+set -x
