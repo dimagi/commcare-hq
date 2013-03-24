fae459bb9a190e254aa86850982b5bab1d13586d
dmyung <dmyung@dimagi.com>
2013-03-24 14:37:08 -0400

    updating refresh_interval on live index back to default

--- pillowtop/listener.py
+++ pillowtop/listener.py
@@ -35,7 +35,7 @@ INDEX_REINDEX_SETTINGS = {"index": {"refresh_interval": "900s",
                                     "store.throttle.type": "merge",
                                     "number_of_replicas": "0"}
 }
-INDEX_STANDARD_SETTINGS = {"index": {"refresh_interval": "5s",
+INDEX_STANDARD_SETTINGS = {"index": {"refresh_interval": "1s",
                                      "merge.policy.merge_factor": 10,
                                      "store.throttle.max_bytes_per_sec": "5mb",
                                      "store.throttle.type": "node",
