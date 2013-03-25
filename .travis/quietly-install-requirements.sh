012396e39c8c67de6cd76d540e5a64768e70c307
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-25 11:22:39 -0400

    Add --use-mirrors to pip installs

--- .travis/quietly-install-requirements.sh
+++ .travis/quietly-install-requirements.sh
@@ -1,7 +1,7 @@
 LOG=pip.log
 
-(pip install -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG) &
-(pip install -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG) &
+(pip install --use-mirrors -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG) &
+(pip install --use-mirrors -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG) &
 
 set +x
 until [ -z "$(ps aux | grep 'pip install')" ]; do echo -n '.'; sleep 1; done
