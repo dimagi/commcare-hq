7343362ee74d3a868da91916ce4dd62fbfcbf9d4
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-25 13:04:59 -0400

    Remove buggy dot-dot-dot output for pip install

--- .travis/quietly-install-requirements.sh
+++ .travis/quietly-install-requirements.sh
@@ -1,8 +1,3 @@
 LOG=pip.log
-
-(pip install --use-mirrors -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG) &
-(pip install --use-mirrors -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG) &
-
-set +x
-until [ -z "$(ps aux | grep 'pip install')" ]; do echo -n '.'; sleep 1; done
-set -x
+pip install --use-mirrors -r requirements/requirements.txt > $LOG 2>&1 || cat $LOG
+pip install --use-mirrors -r requirements/dev-requirements.txt > $LOG 2>&1 || cat $LOG
