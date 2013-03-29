d880ab292716e5d3c1000ed9bb5cd33b9bde9757
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-29 17:15:18 -0400

    Suppress some spurious errors in pip install process

--- .travis/quietly-install-requirements.sh
+++ .travis/quietly-install-requirements.sh
@@ -2,7 +2,7 @@ LOG=pip.log
 
 # --quiet is a bit too quiet because everything times out on travis
 
-cat requirements/requirements.txt requirements/dev-requirements.txt | \
+cat requirements/requirements.txt requirements/dev-requirements.txt | cut -d '#' -f 1 | egrep -v '^$' \
 while read line; do 
     if [ ! -z "$line" ]; then
         echo pip install --use-mirrors "$line" 
