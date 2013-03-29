968acc21679c00bdb35605dae6e104a140aa4b45
Kenn Knowles <kenn.knowles@gmail.com>
2013-03-29 15:31:40 -0400

    Do the keystore bit in a friendly bash shell and fail early

--- .travis/quietly-run-install.sh
+++ .travis/quietly-run-install.sh
@@ -1,2 +1,15 @@
 LOG=install.log
+
+set -ex
+
+keytool -genkey \
+  -keyalg RSA \
+  -keysize 2048 \
+  -validity 10000 \
+  -alias javarosakey \
+  -keypass onetwothreefourfive \
+  -keystore InsecureTestingKeyStore \
+  -storepass onetwothreefourfive \
+  -dname 'CN=Foo, OU=Bar, O=Bizzle, L=Bazzle, ST=Bingle, C=US'
+
 (bash -ex install.sh > $LOG 2>&1) || (cat $LOG; exit 1)
