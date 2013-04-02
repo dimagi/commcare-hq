f45acf5ea4e3428cb640e6f5d846e281d8749028
Mike White <m@mwhite.info>
2013-04-02 19:24:11 -0400

    use property display for dynamic properties too (Case:56792)
    display any case properties that weren't displayed in the default tables
    in one below
    
    an issue is that we need to explicitly set the th and td widths to make
    it look good, but the case property names might be kind of long and lead
    to ugly wrapping.
    
    Also, we need to have really narrow tds in order to fit two groups of
    two columns side-by-side on a 1280-pixel wide display, don't know what
    works best here.

--- casexml/apps/case/templatetags/case_tags.py
+++ casexml/apps/case/templatetags/case_tags.py
@@ -9,9 +9,23 @@ from casexml.apps.case.models import CommCareCase
 from django.template.loader import render_to_string
 from corehq.apps.reports.templatetags.timezone_tags import utc_to_timezone
 from django.template.defaultfilters import yesno
-import itertools
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in xrange(0, len(l), n):
+        yield l[i:i+n]
 
 register = template.Library()
+
+DYNAMIC_PROPERTIES_COLUMNS = 4
+
+@register.simple_tag
+def render_table(name, rows, collapsible=False):
+    return render_to_string("case/partials/property_table.html", {
+        "name": name,
+        "rows": rows,
+        "num_columns": max(map(len, rows)) if rows else None
+    })
 
 @register.simple_tag
 def render_case(case, timezone=pytz.utc, display=None):
@@ -41,14 +55,16 @@ def render_case(case, timezone=pytz.utc, display=None):
             # second box
             ("Submission Info", [
                 [
-                    ("Opened On", "opened_on"),
-                    ("Modified On", "modified_on"),
-                    ("Closed On", "closed_on"),
-                ],
-                [
                     ("User ID", "user_id"),
                     ("Owner ID", "owner_id")
-                ]
+                ],
+                [
+                    ("Opened On", "opened_on"),
+                    ("Modified On", "modified_on"),
+                ],
+                [
+                    ("Closed On", "closed_on"),
+                ],
             ])
         ],
         'meta': {
@@ -77,8 +93,42 @@ def render_case(case, timezone=pytz.utc, display=None):
 
     layout = display['layout']
     meta = display['meta']
-    
-    processed_layout = []
+
+
+    def display_value(case, prop, meta):
+        val = getattr(case, prop, None)
+
+        if prop in meta:
+            prop_meta = meta[prop]
+        elif isinstance(val, datetime.datetime):
+            prop_meta = meta.get('_date', {})
+        else:
+            prop_meta = {}
+
+        if 'process' in prop_meta:
+            val = processors[prop_meta['process']](val)
+        val = escape(val)
+
+        if 'format' in prop_meta:
+            val = mark_safe(prop_meta['format'].format(val))
+
+        return val
+
+
+    def set_colspan(rows):
+        if not rows:
+            return
+
+        max_row_length = max(map(len, rows))
+
+        # set colspan for last element in row if necessary
+        for row in rows:
+            if len(row) < max_row_length:
+                colspan = (max_row_length - len(row)) * 2 + 1
+                row.append([None, colspan, None])
+   
+    seen_properties = []
+    default_properties = []
 
     for section_name, rows in layout:
         processed_rows = []
@@ -86,41 +136,37 @@ def render_case(case, timezone=pytz.utc, display=None):
         for row in rows:
             processed_row = []
 
-            for name, attr in row:
-                val = getattr(case, attr, None)
-
-                if attr in meta:
-                    attr_meta = meta[attr]
-                elif isinstance(val, datetime.datetime):
-                    attr_meta = meta.get('_date', {})
-                else:
-                    attr_meta = {}
-
-                if 'process' in attr_meta:
-                    val = processors[attr_meta['process']](val)
-                val = escape(val)
-
-                if 'format' in attr_meta:
-                    val = mark_safe(attr_meta['format'].format(val))
-               
+            for name, prop in row:
                 # raw property name, property name, property value, colspan
-                processed_row.append([attr, _(name), val, None])
+                processed_row.append(
+                    [prop, _(name), display_value(case, prop, meta)])
+                seen_properties.append(prop)
 
             processed_rows.append(processed_row)
-        
-        max_row_length = max(len(row) for row in processed_rows) 
-        table_span = max_row_length * 2
 
-        # set colspan for last element in row if necessary
-        for row in processed_rows:
-            if len(row) < max_row_length:
-                row[-1][3] = (max_row_length - len(row) + 1) * 2
+        set_colspan(processed_rows)
 
-        processed_layout.append((
-            _(section_name) if section_name else "", table_span, processed_rows))
+        default_properties.append((
+            _(section_name) if section_name else "", processed_rows))
+
+    unseen_dynamic_properties = [k for (k, v) 
+            in case.dynamic_case_properties() 
+            if k not in seen_properties]
+
+    dynamic_properties_rows = []
+    for properties in chunks(unseen_dynamic_properties, DYNAMIC_PROPERTIES_COLUMNS):
+        row = []
+
+        for prop in properties:
+            row.append([prop, prop, display_value(case, prop, meta)])
+
+        dynamic_properties_rows.append(row)
+
+    set_colspan(dynamic_properties_rows)
     
     return render_to_string("case/partials/single_case.html", {
-        "layout": processed_layout,
+        "default_properties": default_properties,
+        "dynamic_properties": dynamic_properties_rows,
         "case": case, 
         "timezone": timezone
     })
