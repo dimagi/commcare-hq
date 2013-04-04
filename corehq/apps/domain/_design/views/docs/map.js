9a0270d70f115dd7bddb2526a3c6eaaebc0183e2
Simon Kelly <skelly@dimagi.com>
2013-04-04 17:38:23 +0200

    add ability to only copy specify doc types and only docs since a given date

--- corehq/apps/domain/_design/views/docs/map.js
+++ corehq/apps/domain/_design/views/docs/map.js
@@ -6,8 +6,38 @@ function (doc) {
         }
         if (doc.domains && doc.domains.length) {
             for (i = 0; i < doc.domains.length; i += 1) {
-                emit([doc.domains[i], doc.doc_type, doc._id], null);
+                emit([doc.domains[i], doc.doc_type, get_date(doc), doc._id], null);
             }
+        }
+    }
+
+    function get_date(doc){
+        // TODO: list is incomplete
+        switch (doc.doc_type){
+            case "CommCareCase":
+            case "CommCareCase-Deleted":
+                return doc.opened_on;
+            case "XFormInstance":
+            case "XFormInstance-Deleted":
+            case "XFormError":
+            case "XFormDuplicate":
+            case "XFormDeprecated":
+            case "XFormArchived":
+            case "SubmissionErrorLog":
+                return doc.received_on
+            case "CommCareUser":
+            case "WebUser":
+                return doc.created_on;
+            case "MessageLog":
+            case "CallLog":
+            case "SMSLog":
+                return doc.date;
+            case "EventLog":
+                return doc.date;
+            case "Application":
+                return doc.built_on;
+            default:
+                return null;
         }
     }
 }
--- corehq/apps/domainsync/management/commands/copy_domain.py
+++ corehq/apps/domainsync/management/commands/copy_domain.py
@@ -1,35 +1,82 @@
-from django.core.management.base import LabelCommand, CommandError
+import sys
+from django.core.management.base import BaseCommand, CommandError
 from dimagi.utils.couch.database import get_db
 from corehq.apps.domainsync.config import DocumentTransform, save
 from couchdbkit.client import Database
+from optparse import make_option
+from datetime import datetime
 
-class Command(LabelCommand):
+class Command(BaseCommand):
     help = "Copies the contents of a domain to another database."
     args = '<sourcedb> <domain>'
-    label = ""
-     
+    option_list = BaseCommand.option_list + (
+        make_option('--include',
+                    action='store',
+                    dest='doc_types',
+                    default='',
+                    help='Comma-separated list of Document Types to copy'),
+        make_option('--since',
+                    action='store',
+                    dest='since',
+                    default='',
+                    help='Only copy documents newer thank this date. Format: yyyy-MM-dd. Only '),
+        make_option('--list-types',
+                    action='store_true',
+                    dest='list_types',
+                    default=False,
+                    help='Don\'t copy anything, just list all the available document types.'),
+    )
+
     def handle(self, *args, **options):
         
         if len(args) != 2:
             raise CommandError('Usage is copy_domain %s' % self.args)
-        
+
         sourcedb = Database(args[0])
         domain = args[1].strip()
-        
-        all_docs = sourcedb.view("domain/docs", startkey=[domain], 
+
+        since = datetime.strptime(options['since'], '%y-%m-%d') if options['since'] else None
+
+        if options['list_types']:
+            self.list_types(sourcedb, domain)
+
+        if options['doc_types']:
+            doc_types = options['doc_types'].split(',')
+            for type in doc_types:
+                docs = self.get_docs_by_type(sourcedb, domain, type=type, since=since)
+                self.copy_docs(sourcedb, domain, docs, type=type, since=since)
+        else:
+            all_docs = sourcedb.view("domain/docs", startkey=[domain],
                                  endkey=[domain, {}], reduce=False)
-        
-        total = len(all_docs)
+            self.copy_docs(sourcedb, domain, all_docs)
+
+    def list_types(self, sourcedb, domain):
+        doc_types = sourcedb.view("domain/docs", startkey=[domain],
+                                  endkey=[domain, {}], reduce=True, group=True, group_level=2)
+        for row in doc_types:
+            print "{:<30}- {}".format(row['key'][1], row['value'])
+        sys.exit(0)
+
+    def get_docs_by_type(self, sourcedb, domain, type=None, since=None):
+        startkey = [x for x in [domain, type, since] if x is not None]
+        endkey = [x for x in [domain, type, {}] if x is not None]
+        docs = sourcedb.view("domain/docs", startkey=startkey,
+                             endkey=endkey, reduce=False)
+        return docs
+
+    def copy_docs(self, sourcedb, domain, docs, type=None, since=None):
+        total = len(docs)
         count = 0
         targetdb = get_db()
-        print "found %s matching documents in domain: %s" % (total, domain)
-        for row in all_docs:
+        msg = "Found %s matching documents in domain: %s" % (total, domain)
+        msg += " of type: %s" % (type) if type else ""
+        msg += " since: %s" % (since) if since else ""
+        print msg
+        for row in docs:
             try:
                 count += 1
                 dt = DocumentTransform(sourcedb.get(row["id"]), sourcedb)
                 save(dt, targetdb)
-                print "Synced %s/%s docs (%s: %s)" % (count, total, row["key"][1], row["id"])
+                print "     Synced %s/%s docs (%s: %s)" % (count, total, row["key"][1], row["id"])
             except Exception, e:
-                print "Document %s failed! Error is: %s" % (row["id"], e)
-            
-        
+                print "     Document %s failed! Error is: %s" % (row["id"], e)
