a353164427d993cf4a7a6fafc124ff1db30dfd5f
Simon Kelly <skelly@dimagi.com>
2013-04-04 17:43:30 +0200

    fix view

--- corehq/apps/domain/_design/views/docs/map.js
+++ corehq/apps/domain/_design/views/docs/map.js
@@ -2,7 +2,7 @@ function (doc) {
     var i;
     if (doc.doc_type) {
         if (doc.domain) {
-            emit([doc.domain, doc.doc_type, doc._id], null);
+            emit([doc.domain, doc.doc_type, get_date(doc), doc._id], null);
         }
         if (doc.domains && doc.domains.length) {
             for (i = 0; i < doc.domains.length; i += 1) {
