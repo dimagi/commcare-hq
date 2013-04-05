2c03be876471d6a12afea1c9812519ff828e576d
Simon Kelly <skelly@dimagi.com>
2013-04-05 16:28:24 +0200

    remove doc._id from domain/docs view

--- corehq/apps/domain/_design/views/docs/map.js
+++ corehq/apps/domain/_design/views/docs/map.js
@@ -2,11 +2,11 @@ function (doc) {
     var i;
     if (doc.doc_type) {
         if (doc.domain) {
-            emit([doc.domain, doc.doc_type, get_date(doc)], doc._id);
+            emit([doc.domain, doc.doc_type, get_date(doc)], null);
         }
         if (doc.domains && doc.domains.length) {
             for (i = 0; i < doc.domains.length; i += 1) {
-                emit([doc.domains[i], doc.doc_type, get_date(doc)], doc._id);
+                emit([doc.domains[i], doc.doc_type, get_date(doc)], null);
             }
         }
     }
