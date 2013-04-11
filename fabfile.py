3b81036ba51e9be8e5b182355bb3f103c665edd1
dmyung <dmyung@dimagi.com>
2013-04-11 14:05:13 -0400

    updating realeststaging fabfile to use realeststaging branch

--- fabfile.py
+++ fabfile.py
@@ -187,7 +187,7 @@ def production():
 @task
 def realstaging():
     """ use production environment on remote host"""
-    env.code_branch = 'master'
+    env.code_branch = 'realeststaging'
     env.sudo_user = 'cchq'
     env.environment = 'staging'
     env.django_port = '9010'
