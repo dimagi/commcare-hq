01432a35a7612ec70a5fce76d5708686f7db8ee3
Mike White <m@mwhite.info>
2013-04-19 02:09:13 -0400

    use new form rendering for standalone form page, some work fixing abstraction barriers

--- couchforms/templatetags/xform_tags.py
+++ couchforms/templatetags/xform_tags.py
@@ -1,9 +1,7 @@
-import types
-from datetime import date, datetime
 from django import template
 import pytz
-from couchforms import util
 from django.utils.html import escape
+from django.utils.translation import ugettext as _
 from couchforms.models import XFormInstance
 from dimagi.utils.timezones import utils as tz_utils
 from couchdbkit.exceptions import ResourceNotFound
@@ -15,84 +13,11 @@ SYSTEM_FIELD_NAMES = (
 
 register = template.Library()
 
-@register.simple_tag
-def value_for_display(value):
-    return util.value_for_display(value)
-
-@register.simple_tag
-def render_form_data(form):
-
-    def render_node(nodekey, nodevalue, show_hidden=True):
-
-        def is_hidden_field(field_key):
-            # hackity hack this static list of things we don't actually
-            # want to display
-            if show_hidden: return False
-            return field_key.startswith("#") or field_key.startswith("@") or field_key.startswith("_")\
-            or field_key.lower() in SYSTEM_FIELD_NAMES
-
-        def format_name(value):
-            if not isinstance(value, basestring):
-                value = unicode(value)
-            return value.replace("_", " ")
-
-        def render_base_type(key, value):
-            if not value: return ""
-            return '<dt>%s</dt><dd>%s</dd>' % (format_name(key), format_name(value))
-
-
-        def is_base_type(value):
-            return isinstance(value, basestring) or\
-                   isinstance(value, date) or\
-                   isinstance(value, datetime)
-
-        if not nodevalue or is_hidden_field(nodekey): return ""
-        if is_base_type(nodevalue):
-            return render_base_type(nodekey, nodevalue)
-        else:
-            header = '<dt class="nest-head">%s</dt>' % format_name(nodekey)
-            # process a dictionary
-            if isinstance(nodevalue, types.DictionaryType):
-                node_list = []
-                for key, value in nodevalue.items() :
-                    # recursive call
-                    node = render_node(key, value)
-                    if node: node_list.append(node)
-
-                if node_list:
-                    return '%(header)s<dd class="nest-body"><dl>%(body)s</dl></dd>' %\
-                           {"header": header,
-                            "body": "".join(node_list)}
-                else:
-                    return ""
-            elif isinstance(nodevalue, types.ListType) or\
-                 isinstance(nodevalue, types.TupleType):
-                # the only thing we are allowed to have lists of
-                # is dictionaries
-                full_list = []
-                for item in nodevalue:
-                    node_list = []
-                    if is_base_type(item):
-                        node_list.append("<li>%s</li>" % format_name(item))
-                    elif isinstance(item, types.DictionaryType):
-                        for key, value in item.items():
-                            node = render_node(key, value)
-                            if node:
-                                node_list.append(node)
-                    else:
-                        node_list.append("<li>%s</li>" % format_name(str(item)))
-                    full_list.append("%(header)s<dd><ul>%(body)s</ul></dd>" %\
-                                     {"header": header,
-                                      "body": "".join(node_list)})
-                return "".join(full_list)
-            else:
-                return render_base_type(nodekey, nodevalue)
-
-    return '<dl class="def-form-data">%s</dl>' % "".join(render_node(key, val) for key, val in form.top_level_tags().items())
 
 @register.simple_tag
 def render_form_xml(form):
     return '<pre class="fancy-code prettyprint linenums"><code class="language-xml">%s</code></pre>' % escape(form.get_xml().replace("><", ">\n<"))
+
 
 @register.simple_tag
 def form_inline_display(form_id, timezone=pytz.utc):
@@ -105,5 +30,5 @@ def form_inline_display(form_id, timezone=pytz.utc):
                                    form.xmlns)
         except ResourceNotFound:
             pass
-        return "missing form: %s" % form_id
-    return "empty form id found"
+        return "%s: %s" % (_("missing form"), form_id)
+    return _("empty form id found")
