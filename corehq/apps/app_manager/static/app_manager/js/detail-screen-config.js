fea9b0260bff104ac218f193a4e546ca16a6331a
yedi <yedispaghetti@gmail.com>
2013-04-25 10:09:36 -0400

    show the user when they input an invalid case property name

--- corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
+++ corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
@@ -65,6 +65,11 @@ var DetailScreenConfig = (function () {
         }
         return orig;
     }
+
+    var field_val_re = /^[a-zA-Z][\w_-]*(\/[a-zA-Z][\w_-]*)*$/;
+    var field_format_warning = $('<span/>').addClass('label label-warning').html("Invalid Property Name:" +
+        "<br /> Must begin with a letter and contain only letters, numbers, '-', and '_'");
+
     Column = (function () {
         function Column(col, screen) {
             /*
@@ -102,6 +107,7 @@ var DetailScreenConfig = (function () {
                 {label: "Referral", value: "referral"}
             ]).val(this.original.model);
             this.field = uiElement.input().val(this.original.field);
+            this.format_warning = field_format_warning.clone().hide();
 
             (function () {
                 var i, lang, visibleVal = "", invisibleVal = "";
@@ -320,6 +326,11 @@ var DetailScreenConfig = (function () {
             this.customColumn = Column.init({model: "case", format: "plain"}, this);
             this.customColumn.field.on('change', function () {
                 that.customColumn.header.val(toTitleCase(this.val()));
+                if (this.val() && !field_val_re.test(this.val())) {
+                    that.customColumn.format_warning.show();
+                } else {
+                    that.customColumn.format_warning.hide();
+                }
             }).$edit_view.autocomplete({
                 source: function (request, response) {
                     var availableTags = that.properties[that.customColumn.model.val()];
@@ -483,7 +494,13 @@ var DetailScreenConfig = (function () {
                     parts[j] = '<code style="display: inline-block;">' + parts[j] + '</code>'
                     column.field.ui.html(parts.join('<span style="color: #DDD;">/</span>'));
                 }
-                $('<td/>').addClass('detail-screen-field').append(column.field.ui).appendTo($tr);
+                var dsf = $('<td/>').addClass('detail-screen-field').append(column.field.ui)
+                dsf.append(column.format_warning);
+                if (column.field.value && !field_val_re.test(column.field.value)) {
+                    column.format_warning.show();
+                }
+                dsf.appendTo($tr);
+
                 $('<td/>').addClass('detail-screen-header').append(column.header.ui).appendTo($tr);
                 $('<td/>').addClass('detail-screen-format').append(column.format.ui).appendTo($tr);
                 $('<td/>').addClass('detail-screen-extra').append(column.$extra).appendTo($tr);
