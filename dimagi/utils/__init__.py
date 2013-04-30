31dba7f0bd4f3f55ed305f0257d125fadf69ae23
Cory Zue <czue@dimagi.com>
2013-04-30 16:11:34 -0400

    add create_unique_filter and tests
    (is there a better existing way to do this? i couldn't find one)

--- dimagi/utils/__init__.py
+++ dimagi/utils/__init__.py
@@ -1,2 +1,3 @@
-from make_uuid import make_uuid
-from make_time import make_time
+from .make_uuid import make_uuid
+from .make_time import make_time
+from .create_unique_filter import create_unique_filter
