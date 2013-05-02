109e61230c8226446f0cb866a7fa9aaa18083e8b
Kenn Knowles <kenn.knowles@gmail.com>
2013-05-02 18:25:55 -0400

    Basic test for due list ES query. Add `models.py` so this registers as an app.

---
+++ bihar/tests.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from bihar.reports.indicators.due_list import due_list_by_task_name
+
+class FakeES(object):
+    """
+    A mock of ES that will return the docs that have been
+    added regardless of the query, and some hardcoded
+    facets just to sanity check the facet handling code
+    """
+    
+    def __init__(self):
+        self.docs = []
+
+    def add_doc(self, id, doc):
+        self.docs.append(doc)
+
+    def base_query(self, start=0, size=None):
+        return {'filter': {'and': []}}
+    
+    def run_query(self, query, es_type=None):
+        return {
+            'hits': {
+                'total': len(self.docs),
+                'hits': [{'_source': doc} for doc in self.docs]
+            },
+            'facets': {
+                'vaccination_names': {
+                    'terms': [
+                        {'term': 'foo', 'count': 10}
+                    ]
+                }
+            }
+        }
+
+class TestDueList(TestCase):
+    """
+    Tests the Due List function superficially 
+    """
+    
+    def test_due_list_by_task_name(self):
+        es = FakeES()
+
+        due_list = due_list_by_task_name(datetime.utcnow(), case_es=es)
+
+        self.assertEquals(due_list, {'foo': 10})
