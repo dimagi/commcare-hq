d9a3a879f8608810eaf1dccad2d21889d3ce6fcc
Giovanni Capalbo <gcapalbo@dimagi.com>
2013-05-06 20:51:27 -0400

    Update settings

--- settings.py
+++ settings.py
@@ -612,6 +612,7 @@ DEFAULT_CURRENCY = "USD"
 SMS_HANDLERS = [
     'corehq.apps.sms.api.forwarding_handler',
     'corehq.apps.commtrack.sms.handle',
+    'corehq.apps.sms.api.structured_sms_handler',
     'corehq.apps.sms.api.form_session_handler',
     'corehq.apps.sms.api.fallback_handler',
 ]
