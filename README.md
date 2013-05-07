a19a0a9d72a1ce30ec276e213fb96298480ccc13
Kenn Knowles <kenn.knowles@gmail.com>
2013-05-07 15:44:01 -0400

    Add travis-ci stuff

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+Receiver [![Build Status](https://travis-ci.org/dimagi/receiver.png)](https://travis-ci.org/dimagi/receiver)
+========
+
 The receiver app is for receiving submissions from a mobile phone application
 like JavaRosa, ODK, or CommCare and processing them with couchforms.
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+
+setup(
+    name='receiver',
+    version='1.0.0',
+    description='Dimagi XForm Receiver (an installable Django app)',
+    author='Dimagi',
+    author_email='dev@dimagi.com',
+    url='http://www.dimagi.com/',
+    install_requires = [
+        "couchdbkit",
+        "couchexport",
+        "couchforms>=1.0.0",
+        "dimagi-utils>=1.0.0",
+        "django",
+        "lxml",
+        "pytz",
+        "requests",
+        "restkit",
+        "simplejson",
+    ],
+    tests_require = [
+        'coverage',
+        'django-coverage',    
+    ],
+    packages = find_packages(exclude=['*.pyc']),
+    include_package_data=True
+)
+
