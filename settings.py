8a39563fd9f1dc2fecfc4967160efe674b0cbb99
Kenn Knowles <kenn.knowles@gmail.com>
2013-05-07 14:50:28 -0400

    Add testing settings.py

---
+++ settings.py
@@ -0,0 +1,50 @@
+# vim: ai ts=4 sts=4 et sw=4 encoding=utf-8
+
+SECRET_KEY = 'this is not a secret key'
+
+INSTALLED_APPS = (
+    'casexml',
+    'couchdbkit.ext.django',
+    'couchforms',
+    'coverage',
+)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'casexml',
+    }
+}
+
+
+
+####### Couch Config ######
+COUCH_HTTPS = False # recommended production value is True if enabling https
+COUCH_SERVER_ROOT = '127.0.0.1:5984' #6984 for https couch
+COUCH_USERNAME = ''
+COUCH_PASSWORD = ''
+COUCH_DATABASE_NAME = 'casexml'
+
+COUCHDB_DATABASES = [
+    ('couchforms', 'http://127.0.0.1:5984/casexml'),
+    ('couch', 'http://127.0.0.1:5984/casexml'), # Why?
+]
+
+
+# Hardcode the test database?
+XFORMS_POST_URL = "http://127.0.0.1:5984/casexml_test/_design/couchforms/_update/xform/" 
+
+TEST_RUNNER = 'couchdbkit.ext.django.testrunner.CouchDbKitTestSuiteRunner'
+
+####### # Email setup ########
+# Print emails to console so there is no danger of spamming, but you can still get registration URLs
+EMAIL_BACKEND='django.core.mail.backends.console.EmailBackend'
+EMAIL_LOGIN = "nobody@example.com"
+EMAIL_PASSWORD = "******"
+EMAIL_SMTP_HOST = "smtp.example.com"
+EMAIL_SMTP_PORT = 587
+
+COVERAGE_REPORT_HTML_OUTPUT_DIR='coverage-html'
+COVERAGE_MODULE_EXCLUDES= ['tests$', 'settings$', 'urls$', 'locale$',
+                           'common.views.test', '^django', 'management', 'migrations',
+                           '^south', '^djcelery', '^debug_toolbar', '^rosetta']
