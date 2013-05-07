bb5f738c334e3a1fb3cccf58793c66375ed070da
Simon Kelly <skelly@dimagi.com>
2013-05-07 13:29:00 +0200

    setup.py and travis config

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+import sys
+
+setup(
+    name='fluff',
+    version='0.0.1',
+    description='Map over CouchDB changes feed',
+    author='Dimagi',
+    author_email='information@dimagi.com',
+    url='http://www.dimagi.com/',
+    packages=find_packages(exclude=['*.pyc']),
+    test_suite='fluff.tests',
+    test_loader='unittest2:TestLoader',
+    install_requires=[
+        'couchdbkit',
+        'pillowtop==0.0.1',
+        'dimagi-utils==0.0.2'
+    ],
+    dependency_links=[
+        'http://github.com/dimagi/dimagi-utils/tarball/master#egg=dimagi-utils-0.0.2',
+        'http://github.com/dimagi/pillowtop/tarball/master#egg=pillowtop-0.0.1',
+    ],
+    tests_require=[
+        'django',
+        'unittest2'
+    ]
+)
