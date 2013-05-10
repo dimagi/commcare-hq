99254930e4e0eb0c667197c5323b1596e41f3660
Tyler Wymer <twymer@gmail.com>
2013-05-10 15:17:01 -0400

    Gracefully handle invalid invite resend request

--- corehq/apps/users/views/__init__.py
+++ corehq/apps/users/views/__init__.py
@@ -191,12 +191,12 @@ def accept_invitation(request, domain, invitation_id):
 @require_can_edit_web_users
 def reinvite_web_user(request, domain):
     invitation_id = request.POST['invite']
-    if invitation_id:
+    try:
         invitation = DomainInvitation.get(invitation_id)
         invitation.send_activation_email()
         return json_response({'response': _("Invitation resent"), 'status': 'ok'})
-    else:
-        raise Http404
+    except ResourceNotFound:
+        return json_response({'response': _("Error while attempting resend"), 'status': 'error'})
 
 @require_can_edit_web_users
 def invite_web_user(request, domain, template="users/invite_web_user.html"):
