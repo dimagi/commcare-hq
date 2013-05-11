6168a49c86208474f5e747531fb27fd658cfca16
Cory Zue <czue@dimagi.com>
2013-05-11 15:46:12 -0400

    be more lenient with case types in the app builder

--- corehq/apps/app_manager/util.py
+++ corehq/apps/app_manager/util.py
@@ -1,4 +1,4 @@
-
+import re
 
 def get_app_id(form):
     """
@@ -14,3 +14,22 @@ def split_path(path):
     name = path_parts.pop(-1)
     path = '/'.join(path_parts)
     return path, name
+
+
+CASE_TYPE_REGEX = r'^[\w-]+$'
+_case_type_regex = re.compile(CASE_TYPE_REGEX)
+
+def is_valid_case_type(case_type):
+    """
+    >>> is_valid_case_type('foo')
+    True
+    >>> is_valid_case_type('foo-bar')
+    True
+    >>> is_valid_case_type('foo bar')
+    False
+    >>> is_valid_case_type('')
+    False
+    >>> is_valid_case_type(None)
+    False
+    """
+    return bool(_case_type_regex.match(case_type or ''))
