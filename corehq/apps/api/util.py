2c75a0d0690209567bbbbe4c49838c6eb38b9c78
Cory Zue <czue@dimagi.com>
2013-05-13 17:57:32 -0400

    add APIs to create and update form repeaters + tests

--- corehq/apps/api/util.py
+++ corehq/apps/api/util.py
@@ -2,16 +2,18 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.utils.translation import ugettext as _
 from couchdbkit.exceptions import ResourceNotFound
 
-def get_object_or_not_exist(cls, doc_id, domain):
+def get_object_or_not_exist(cls, doc_id, domain, additional_doc_types=None):
     """
     Given a Document class, id, and domain, get that object or raise
     an ObjectDoesNotExist exception if it's not found, not the right
     type, or doesn't belong to the domain.
     """
+    additional_doc_types = additional_doc_types or []
     doc_type = getattr(cls, '_doc_type', cls.__name__)
+    additional_doc_types.append(doc_type)
     try:
         doc = cls.get(doc_id)
-        if doc and doc.domain == domain and doc.doc_type == doc_type:
+        if doc and doc.domain == domain and doc.doc_type in additional_doc_types:
             return doc
     except ResourceNotFound:
         pass # covered by the below
