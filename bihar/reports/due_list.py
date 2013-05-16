c9d12330951530dc14aff67d8fa47078eea931ea
Cory Zue <czue@dimagi.com>
2013-05-16 19:20:56 -0400

    add classes for various days of the due list schedule

--- bihar/reports/due_list.py
+++ bihar/reports/due_list.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_noop, ugettext as _
 from bihar.reports.supervisor import SubCenterSelectionReport, BiharNavReport, GroupReferenceMixIn, shared_bihar_context, team_member_context, BiharSummaryReport
 from corehq.apps.reports.generic import summary_context
 from corehq.apps.api.es import FullCaseES
-from datetime import datetime
+from datetime import datetime, timedelta
 
 BIHAR_DOMAIN = 'care-bihar' # TODO: where should this go?
 
@@ -17,19 +17,23 @@ class DueListNav(GroupReferenceMixIn, BiharNavReport):
     extra_context_providers = [shared_bihar_context, summary_context, team_member_context]
     @property
     def reports(self):
-        return [VaccinationSummary]
+        return [VaccinationSummaryToday, VaccinationSummaryTomorrow,
+                VaccinationSummary2Days, VaccinationSummary3Days]
 
     @property
     def rendered_report_title(self):
         return self.group_display
 
 class VaccinationSummary(GroupReferenceMixIn, BiharSummaryReport):
-
     name = ugettext_noop("Care Due")
     slug = "vaccinationsummary"
     description = "Vaccination summary report"
     base_template_mobile = "bihar/bihar_summary.html"
     is_cacheable = True
+
+    def get_date(self):
+        # override
+        raise NotImplemented()
 
     @property
     def _headers(self):
@@ -38,9 +42,6 @@ class VaccinationSummary(GroupReferenceMixIn, BiharSummaryReport):
     @property
     def data(self):
         return [_("# Due")] + [res[1] for res in self.due_list_by_task_name()]
-
-    def get_date(self):
-        return datetime.today()
 
     @memoized
     def due_list_by_task_name(self, case_es=None, size=0, case_type='task'):
@@ -82,6 +83,38 @@ class VaccinationSummary(GroupReferenceMixIn, BiharSummaryReport):
         es_result = case_es.run_query(base_query, es_type=es_type)
         return sorted([(facet['term'], facet['count']) for facet in es_result['facets'][facet_name]['terms'] ],
                       key=lambda tup: tup[0])
+
+# TODO: this is pretty silly but doing this without classes would be a bit of extra work
+class VaccinationSummaryToday(VaccinationSummary):
+    name = ugettext_noop("Care Due Today")
+    slug = "vacstoday"
+    def get_date(self):
+        return datetime.today()
+
+class VaccinationSummaryTomorrow(VaccinationSummary):
+    name = ugettext_noop("Care Due Tomorrow")
+    slug = "vacstomorrow"
+    def get_date(self):
+        return datetime.today() + timedelta(days=1)
+
+class VaccinationSummaryTomorrow(VaccinationSummary):
+    name = ugettext_noop("Care Due Tomorrow")
+    slug = "vacstomorrow"
+    def get_date(self):
+        return datetime.today() + timedelta(days=1)
+
+class VaccinationSummary2Days(VaccinationSummary):
+    name = ugettext_noop("Care Due In 2 Days")
+    slug = "vacs2days"
+    def get_date(self):
+        return datetime.today() + timedelta(days=2)
+
+class VaccinationSummary3Days(VaccinationSummary):
+    name = ugettext_noop("Care Due In 3 Days")
+    slug = "vacs3days"
+    def get_date(self):
+        return datetime.today() + timedelta(days=3)
+
 
 class DueListSelectionReport(SubCenterSelectionReport):
     slug = "duelistselect"
