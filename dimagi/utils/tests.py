f9644fdf63a7d1d9426bdb5f71296ea8bdc6a141
Daniel Roberts <droberts@dimagi.com>
2013-05-23 15:59:17 +0530

    ...and remove doctest ref

--- dimagi/utils/tests.py
+++ dimagi/utils/tests.py
@@ -1,5 +1,4 @@
 from dimagi.utils.create_unique_filter import create_unique_filter
-from dimagi.utils.read_only import ReadOnlyObject
 from excel import IteratorJSONReader
 from decorators.memoized import Memoized
 from dimagi.utils.chunked import chunked
@@ -9,5 +8,4 @@ __test__ = {
     'memoized': Memoized,
     'chunked': chunked,
     'create_unique_filter': create_unique_filter,
-    'read_only': ReadOnlyObject,
 }
