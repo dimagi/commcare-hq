2f297bfe7ff095417ac47f5e99f530cae772eb6f
Simon Kelly <skelly@dimagi.com>
2013-05-27 08:46:58 +0200

    qa touchups

--- fluff/sync_couchdb.py
+++ fluff/sync_couchdb.py
@@ -1,13 +1,11 @@
 from django.db.models import signals
 import os
 from couchdbkit.ext.django.loading import get_db
-from couchdbkit import push
-from couchdbkit.exceptions import ResourceNotFound
 from pillowtop.run_pillowtop import import_pillows
 from dimagi.utils.couch.sync_docs import sync_design_docs as sync_docs
 
 
-fluff = 'fluff'
+FLUFF = 'fluff'
 
 
 def sync_design_docs(temp=None):
@@ -16,14 +14,14 @@ def sync_design_docs(temp=None):
         app_label = pillow.indicator_class._meta.app_label
         db = get_db(app_label)
 
-        sync_docs(db, os.path.join(dir, "_design"), fluff, temp=temp)
+        sync_docs(db, os.path.join(dir, "_design"), FLUFF, temp=temp)
 
 
 def catch_signal(app, **kwargs):
     """Function used by syncdb signal"""
     app_name = app.__name__.rsplit('.', 1)[0]
     app_label = app_name.split('.')[-1]
-    if app_label == fluff:
+    if app_label == FLUFF:
         sync_design_docs()
 
 
@@ -32,7 +30,7 @@ def copy_designs(temp='tmp', delete=True):
         app_label = pillow.indicator_class._meta.app_label
         db = get_db(app_label)
 
-        copy_designs(db, fluff)
+        copy_designs(db, FLUFF)
 
 
 signals.post_syncdb.connect(catch_signal)
