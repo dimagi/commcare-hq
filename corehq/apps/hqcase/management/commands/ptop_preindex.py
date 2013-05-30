63df9b21bc2365d91ddf02bd6b319b4306681c98
dmyung <dmyung@dimagi.com>
2013-05-30 10:28:02 -0400

    fail hard on misconfigured pillows before greenlets start.

--- corehq/apps/hqcase/management/commands/ptop_preindex.py
+++ corehq/apps/hqcase/management/commands/ptop_preindex.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from django.core.mail import mail_admins
+import sys
 from gevent import monkey;monkey.patch_all()
 import gevent
 from gevent import Greenlet
@@ -11,9 +12,7 @@ from django.core.management import call_command
 from django.conf import settings
 
 
-
-def do_reindex(pillow_class_name):
-    print "Starting pillow preindex %s" % pillow_class_name
+def get_reindex_command(pillow_class_name):
     pillow_command_map = {
         'DomainPillow': 'ptop_fast_reindex_domains',
         'CasePillow': 'ptop_fast_reindex_cases',
@@ -21,14 +20,15 @@ def do_reindex(pillow_class_name):
         'XFormPillow': 'ptop_fast_reindex_xforms',
         'FullXFormPillow': 'ptop_fast_reindex_fullxforms',
     }
-
     reindex_command = pillow_command_map.get(pillow_class_name, None)
+    return reindex_command
 
+def do_reindex(pillow_class_name):
+    print "Starting pillow preindex %s" % pillow_class_name
+    reindex_command = get_reindex_command(pillow_class_name)
     if reindex_command:
         call_command(reindex_command, **{'noinput': True, 'bulk': True})
         print "Pillow preindex finished %s" % pillow_class_name
-    else:
-        print "unrecognized reindex command: %s - this should never happen" % reindex_command
 
 
 class Command(BaseCommand):
@@ -65,6 +65,13 @@ class Command(BaseCommand):
 
 
         start = datetime.utcnow()
+        for pillow in reindex_pillows:
+            #loop through pillows once before running greenlets to fail hard on misconfigured pillows
+            pillow_class_name = pillow.__class__.__name__
+            reindex_command = get_reindex_command(pillow_class_name)
+            if not reindex_command:
+                raise Exception("Error, pillow [%s] is not configured with its own management command reindex command - it needs one" % pillow_class_name)
+
         for pillow in reindex_pillows:
             print pillow.__class__.__name__
             g = gevent.spawn(do_reindex, pillow.__class__.__name__)
