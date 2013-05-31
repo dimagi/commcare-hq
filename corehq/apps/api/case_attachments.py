03bfaa201d925c610d9a1212903d32272f2b6826
dmyung <dmyung@dimagi.com>
2013-05-31 15:49:45 -0400

    initial commit for case_attachment multisize handling

--- corehq/apps/api/case_attachments.py
+++ corehq/apps/api/case_attachments.py
@@ -4,6 +4,12 @@ from django.utils.decorators import method_decorator, classonlymethod
 from django.views.generic import View
 from casexml.apps.case.models import CommCareCase
 from corehq.apps.domain.decorators import login_and_domain_required
+from django.core import cache
+
+
+#following flickr's sizing convention with some additions
+
+
 
 
 class CaseAttachmentAPI(View):
@@ -17,9 +23,18 @@ class CaseAttachmentAPI(View):
         """
         print "KWARGS"
         print kwargs
-        max_size = self.request.GET.get('max_size', 0)
-        max_width = self.request.GET.get('max_image_width', 0)
-        max_height = self.request.GET.get('max_image_height', 0)
+        max_size = self.request.GET.get('max_size', None)
+        max_width = self.request.GET.get('max_image_width', None)
+        max_height = self.request.GET.get('max_image_height', None)
+        size = self.request.GET('size', None) #alternative to abs size
+
+        if size:
+            size_key = size
+        else:
+            if max_size:
+                pass
+            else:
+                pass
 
         domain = self.request.domain
         print args
@@ -27,6 +42,15 @@ class CaseAttachmentAPI(View):
         case_exists = CommCareCase.get_db().doc_exist(case_id)
         if not case_exists:
             raise Http404
+
+
+        rcache = cache.get_cache('redis')
+
+        attachment_data_key = "%(case_id)_%(attachment)s_%(size_key)s" % {
+            "case_id": case_id,
+            "attachment": attachment_key,
+            "size_key": size_key
+        }
 
         case_doc = CommCareCase.get(case_id)
         attachment_key = kwargs.get('attachment_id', None)
