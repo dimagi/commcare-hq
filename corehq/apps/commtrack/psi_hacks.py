b6497a0ef845ec435788ef9a77e37d466f96f947
Cory Zue <czue@dimagi.com>
2013-06-01 13:16:48 -0400

    whitespace

--- corehq/apps/commtrack/psi_hacks.py
+++ corehq/apps/commtrack/psi_hacks.py
@@ -8,4 +8,4 @@ def is_psi_domain(domain):
     if isinstance(domain, Domain):
         domain = domain.name
     # we can eventually make this explicit but for now this should be sufficient
-    return (domain or  '').startswith("psi-")
+    return (domain or '').startswith("psi-")
