b775f94223d3596dbc88a20b9643b13943947537
dmyung <dmyung@dimagi.com>
2013-06-10 14:47:05 -0400

    adding generic cached url for cached objects

--- corehq/apps/api/cached_object.py
+++ corehq/apps/api/cached_object.py
@@ -10,7 +10,6 @@ except:
 
 from django.core import cache
 from PIL import Image, ImageOps
-
 CACHE_PREFIX = 'ocache_'
 
 IMAGE_SIZE_SQUARE = 'square'
@@ -26,8 +25,8 @@ IMAGE_SIZE_720 = '720'
 IMAGE_SIZE_1080 = '1080'
 IMAGE_SIZE_HUGE = 'huge'
 
-IMAGE_SIZE_ORIGINAL = 'original'
-IMAGE_SIZE_WILDCARD = '*'
+OBJECT_ORIGINAL = 'original'
+WILDCARD = '*'
 
 #grr, since ordered_dict is 2.7 only
 IMAGE_SIZE_ORDERING = [
@@ -43,7 +42,7 @@ IMAGE_SIZE_ORDERING = [
     IMAGE_SIZE_720,
     IMAGE_SIZE_1080,
     IMAGE_SIZE_HUGE,
-    IMAGE_SIZE_ORIGINAL
+    OBJECT_ORIGINAL
 ]
 
 OBJECT_SIZE_MAP = {
@@ -61,7 +60,7 @@ OBJECT_SIZE_MAP = {
     IMAGE_SIZE_1080: {"width": 1920, "height": 1080},
     IMAGE_SIZE_HUGE: {"width": 3000, "height": 2000}, #largest
 
-    IMAGE_SIZE_ORIGINAL: {"width": 0, "height": 0} #cache the original anyway - but generally don't serve this
+    OBJECT_ORIGINAL: {"width": 0, "height": 0} #cache the original anyway - but generally don't serve this
 }
 
 class CachedObjectMeta(dict):
@@ -75,6 +74,9 @@ class CachedObjectMeta(dict):
         self.content_length = content_length
         self.content_type = content_type
 
+    def to_json(self):
+        return self.__dict__
+
     @staticmethod
     def get_extension(content_type):
         splits = content_type.split('/')
@@ -84,23 +86,33 @@ class CachedObjectMeta(dict):
             return 'txt'
 
     @classmethod
-    def make_meta(cls, file_obj, size_key):
+    def make_meta(cls, file_obj, size_key, metadata={}):
         """
         giventhe image object and the size key, prepare the metadata calculations
         image_obj: Image object
         size_key: matching the image_size_map - if original calculate from origina lfilesize
         """
 
-        cls.size_key = size_key
-
         file_obj.seek(0, os.SEEK_END)
-        cls.content_length = file_obj.tell()
-        file_obj.seek(0)
+        print "make meta object"
+        print metadata
+        content_type = None
+        for ctype_key in ['content_type', 'Content-Type']:
+            content_type = metadata.get(ctype_key, None)
+            if content_type is not None:
+                break
+            print "not breaking"
+        foo
+        if content_type is None:
+            content_type = 'application/octet-stream'
 
-        file_format = file_obj.format
-        guessed_type = mimetypes.guess_type("foo.%s" % file_format)
-        if guessed_type[0] is not None:
-            cls.content_type = guessed_type[0]
+        ret = cls(
+                size_key,
+                file_obj.tell(),
+                content_type
+                  )
+        file_obj.seek(0)
+        return ret
 
 
 class CachedImageMeta(CachedObjectMeta):
@@ -108,48 +120,79 @@ class CachedImageMeta(CachedObjectMeta):
     width = 0
     meta_type = "image"
 
-    def __init__(self, size_key, height, width, content_length, content_type):
+    def __init__(self, size_key, width, height, content_length, content_type):
         super(CachedImageMeta, self).__init__(size_key, content_length, content_type)
-        self.height = height
         self.width = width
+        self.height = height
 
     def get_size(self):
         return self.width, self.height
 
     @classmethod
-    def make_meta(cls, image_obj, size_key):
+    def make_meta(cls, image_obj, image_stream, size_key, metadata={}):
         """
         giventhe image object and the size key, prepare the metadata calculations
         image_obj: Image object
         size_key: matching the image_size_map - if original calculate from origina lfilesize
         """
-        assert isinstance(image_obj, Image), "CachedImageMeta needs to be a PIL Image instance"
+        #assert isinstance(image_obj, Image), "CachedImageMeta needs to be a PIL Image instance"
 
-        if size_key == IMAGE_SIZE_ORIGINAL:
+        if size_key == OBJECT_ORIGINAL:
             width, height = image_obj.size
         else:
             width = OBJECT_SIZE_MAP[size_key]['width']
             height = OBJECT_SIZE_MAP[size_key]['height']
 
-        cls.size_key = size_key
-        cls.width = width
-        cls.height = height
-        cls.content_length = image_obj.fp.len
-        file_format = image_obj.format
-        guessed_type = mimetypes.guess_type("foo.%s" % file_format)
-        if guessed_type[0] is not None:
-            cls.content_type = guessed_type[0]
+        image_stream.seek(0, os.SEEK_END)
+        content_length = image_stream.tell()
+        image_stream.seek(0)
+
+        print "make meta image"
+        print metadata
+        content_type = metadata.get('content_type', None)
+        if content_type is None:
+            file_format = image_obj.format
+            guessed_type = mimetypes.guess_type("foo.%s" % file_format)
+            if guessed_type[0] is not None:
+                content_type = guessed_type[0]
+            else:
+                content_type = 'image/png'
+
+        ret = cls(
+            size_key,
+            width,
+            height,
+            content_length,
+            content_type
+        )
+        return ret
 
 
 class CachedObject(object):
-    def __init__(self, filename, object_stream):
+    def __init__(self, filename):
         """
-
+        Filename for a cached object is a user defined, unique, yet human readable identifier.
         """
         self.filename = filename
         #hashed_name is for future use by creating filenames based upon hashes and saving these versions to the filesystem for serving via staticfiles.
         self.hashed_name = hashlib.md5(filename).hexdigest()
-        self.object_stream = object_stream
+        stream_keys, meta_keys = self.get_all_keys()
+
+        self.stream_keys = stream_keys
+        self.meta_keys = meta_keys
+
+
+
+    @classmethod
+    def get_cached(cls, filename):
+        pass
+
+    def is_cached(self):
+        metas, streams = self.get_all_keys()
+        if len(metas) == 0:
+            return False
+        else:
+            return True
 
     @property
     def key_prefix(self):
@@ -169,44 +212,56 @@ class CachedObject(object):
                          {"prefix": self.key_prefix, "size": size_key}
         return cache_meta_key
 
-    def rcache(self):
-        return cache.get_cache('redis')
+    def fetch_stream(self, key):
+        stream = self.rcache.get(self.stream_key(key))
+        if stream is not None:
+            return StringIO.StringIO(stream)
+        else:
+            return None
 
-    @classmethod
-    def get_size(cls, filename, size_key):
+    def fetch_meta(self, key):
+        meta = self.rcache.get(self.meta_key(key))
+        print "fetch meta %s" % key
+        print meta
+        if meta is not None:
+            return simplejson.loads(meta)
+        else:
+            return {}
+
+    #retrieval methods
+    def get(self):
+        return self.get_size(OBJECT_ORIGINAL)
+
+    def get_size(self, size_key):
         """
         Return the stream of the filename and size_key you want
         """
-        pass
+        #stream_key = self.stream_key(size_key)
+        #meta_key = self.meta_key(size_key)
 
-    def get_sizes(self):
-        rcache = self.rcache()
-        stream_keys = rcache.get(self.stream_key(IMAGE_SIZE_WILDCARD))
-        meta_keys = rcache.get(self.meta_key(IMAGE_SIZE_WILDCARD))
+        stream = self.fetch_stream(size_key)
+        meta = self.fetch_meta(size_key)
+
+        return meta, stream
+
+    def cache_put(self, object_stream, metadata={}):
+        object_meta = CachedObjectMeta.make_meta(object_stream, OBJECT_ORIGINAL, metadata=metadata)
+
+        rcache = self.rcache
+        object_stream.seek(0)
+        rcache.set(self.meta_key(OBJECT_ORIGINAL), simplejson.dumps(object_meta.to_json()))
+        rcache.set(self.stream_key(OBJECT_ORIGINAL), object_stream.read())
+
+    @property
+    def rcache(self):
+        return cache.get_cache('redis')
+
+    def get_all_keys(self):
+        stream_keys = self.rcache.keys(self.stream_key(WILDCARD))
+        meta_keys = self.rcache.keys(self.meta_key(WILDCARD))
 
         assert len(stream_keys) == len(meta_keys), "Error stream and meta keys must be 1:1 - something went wrong in the configuration"
         return stream_keys, meta_keys
-
-    def get_sizes(self):
-        rcache = self.rcache()
-        all_keys = rcache.get("%s*" % self.key_prefix)
-        stream_keys = filter(lambda x: x[len(self.key_prefix):].startswith('_image'), all_keys)
-        meta_keys = filter(lambda x: x[len(self.key_prefix):].startswith('_meta'), all_keys)
-        return stream_keys, meta_keys
-
-    @classmethod
-    def cache_object(cls, filename, object_stream):
-        ret = cls(filename, object_stream)
-
-        image_meta = CachedObjectMeta.make_meta(orig_image_obj, IMAGE_SIZE_ORIGINAL)
-
-        rcache = ret.rcache()
-
-        image_stream.fp.seek(0)
-        rcache.set(ret.stream_key(IMAGE_SIZE_ORIGINAL), image_stream.fp.read())
-        rcache.set(ret.meta_key(IMAGE_SIZE_ORIGINAL), simplejson.dumps(image_meta))
-        return ret
-
 
 
 class CachedImage(CachedObject):
@@ -219,65 +274,79 @@ class CachedImage(CachedObject):
     #generate
     #return stream
 
-
-    @classmethod
-    def cache_image(cls, filename, image_stream):
+    def cache_image(self, image_stream, metadata={}):
         """
         Create a cached image
         For a given original sized image - cache it initially to speed up small size generation
         """
-        orig_image_obj = Image.open(StringIO.StringIO(image_stream))
-        ret = cls(filename, image_stream)
+        orig_image_obj = Image.open(image_stream)
+        image_meta = CachedImageMeta.make_meta(orig_image_obj, image_stream, OBJECT_ORIGINAL, metadata=metadata)
 
-        image_meta = CachedImageMeta.make_meta(orig_image_obj, IMAGE_SIZE_ORIGINAL)
+        rcache = self.rcache
+        image_stream.seek(0)
 
-        rcache = ret.rcache()
+        rcache.set(self.stream_key(OBJECT_ORIGINAL), image_stream.read())
+        rcache.set(self.meta_key(OBJECT_ORIGINAL), simplejson.dumps(image_meta.to_json()))
 
-        image_stream.fp.seek(0)
-        rcache.set(ret.stream_key(IMAGE_SIZE_ORIGINAL), image_stream.fp.read())
-        rcache.set(ret.meta_key(IMAGE_SIZE_ORIGINAL), simplejson.dumps(image_meta))
-        return ret
+    def get_size(self, size_key):
+        """
+        Return the stream of the filename and size_key you want
+        """
+        if not self.has_size(size_key):
+            self.make_size(size_key)
+        return super(CachedImage, self).get_size(size_key)
+
+
+    def has_size(self, skey):
+        stream_keys, meta_keys = self.get_all_keys()
+        if self.stream_key(skey) in stream_keys and self.meta_key(skey) in meta_keys:
+            return True
+        else:
+            return False
+
+    def fetch_image(self, key):
+        stream = self.rcache.get(self.stream_key(key))
+        if stream is not None:
+            source_image_obj = Image.open(StringIO.StringIO(stream))
+            return source_image_obj
+        else:
+            return None
 
     def make_size(self, size_key):
         """
         size_key = target key size
         """
-        rcache = self.rcache()
-
-        stream_keys, meta_keys = self.get_sizes()
-
-        def has_size(skey):
-            if self.stream_key(skey) in stream_keys and self.meta_key(skey) in meta_keys:
-                return True
-            else:
-                return False
-
-        if has_size(size_key):
+        rcache = self.rcache
+        if self.has_size(size_key):
             #do nothing, already exists
             return 204
         else:
             #make size from the next available largest size (so if there's a small_320 and you want small, generate it from small_320
             size_seq = IMAGE_SIZE_ORDERING.index(size_key)
-            source_key = IMAGE_SIZE_ORIGINAL
+            source_key = OBJECT_ORIGINAL
             for source_size_key in IMAGE_SIZE_ORDERING[size_seq+1:]:
-                if has_size(source_size_key):
+                if self.has_size(source_size_key):
                     source_key = source_size_key
                     break
-            source_size = (OBJECT_SIZE_MAP[source_key]['width'], OBJECT_SIZE_MAP[source_key]['height'])
-            target_size = (OBJECT_SIZE_MAP[size_key]['width'], OBJECT_SIZE_MAP[size_key]['height'])
 
-            source_image_obj = Image.open(StringIO.StringIO(rcache.get(self.stream_key(source_key))))
+            source_size = (OBJECT_SIZE_MAP[source_key]['width'], OBJECT_SIZE_MAP[source_key]['height'])
+            source_meta = self.fetch_meta(OBJECT_ORIGINAL)
+            target_size = (OBJECT_SIZE_MAP[size_key]['width'], OBJECT_SIZE_MAP[size_key]['height'])
+            source_image_obj = self.fetch_image(source_key)
+
+
             target_image_obj = ImageOps.fit(source_image_obj, target_size, method=Image.BICUBIC)
-            mime_ext = CachedImageMeta.get_extension(meta_keys[0])
+            mime_ext = CachedImageMeta.get_extension(source_meta['content_type'])
 
             #output to buffer
             target_handle = StringIO.StringIO()
+            #target_image_obj.save(target_handle, mime_ext)
             target_image_obj.save(target_handle, mime_ext)
             target_handle.seek(0)
-            target_meta = CachedImageMeta.make_meta(target_image_obj, size_key)
+            target_meta = CachedImageMeta.make_meta(target_image_obj, target_handle, size_key)
 
-            rcache.set(self.stream_key(size_key), target_image_obj.fp.read())
-            rcache.set(self.meta_key(size_key), simplejson.dumps(target_meta))
+            rcache.set(self.stream_key(size_key), target_handle.read())
+            rcache.set(self.meta_key(size_key), simplejson.dumps(target_meta.to_json()))
             return 201
 
 
--- corehq/apps/api/object_fetch_api.py
+++ corehq/apps/api/object_fetch_api.py
@@ -2,6 +2,8 @@ from django.core.servers.basehttp import FileWrapper
 from django.http import HttpResponse, Http404
 from django.utils.decorators import method_decorator, classonlymethod
 from django.views.generic import View
+import ipdb
+from corehq.apps.api.cached_object import IMAGE_SIZE_ORDERING
 from casexml.apps.case.models import CommCareCase
 from corehq.apps.domain.decorators import login_and_domain_required
 from django.core import cache
@@ -24,43 +26,57 @@ class CaseAttachmentAPI(View):
         max_image_width	The largest width in pixels for an an image attachment
         max_image_height	The largest width in pixels for an an image attachment
         """
-        print "KWARGS"
-        print kwargs
+
         max_size = self.request.GET.get('max_size', None)
+
+        img = self.request.GET.get('img', None)
+        size = self.request.GET.get('size', None) #alternative to abs size
         max_width = self.request.GET.get('max_image_width', None)
         max_height = self.request.GET.get('max_image_height', None)
-        size = self.request.GET('size', None) #alternative to abs size
 
         if size:
             size_key = size
         else:
-            if max_size:
-                pass
-            else:
-                pass
+            size_key = None
 
         domain = self.request.domain
-        print args
         case_id = kwargs.get('case_id', None)
         case_exists = CommCareCase.get_db().doc_exist(case_id)
         if not case_exists:
             raise Http404
 
         attachment_key = kwargs.get('attachment_id', None)
-        rcache = cache.get_cache('redis')
 
-        attachment_data_key = "%(case_id)_%(attachment)s_%(size_key)s" % {
-            "case_id": case_id,
-            "attachment": attachment_key,
-            "size_key": size_key
-        }
+        if img is not None:
+            if size == "debug_all":
+                case_doc = CommCareCase.get(case_id)
+                r = HttpResponse(content_type="text/html")
+                r.write('<html><body>')
+                r.write('<ul>')
+                for fsize in IMAGE_SIZE_ORDERING:
+                    meta, stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=size, width_limit=max_width, height_limit=max_height, fixed_size=fsize)
 
-        case_doc = CommCareCase.get(case_id)
-        attach_stream = CommCareCase.get_db().fetch_attachment(case_id, attachment_key, stream=True)
-        wrapper = FileWrapper(attach_stream)
-        attachment_meta = case_doc.case_attachments.get(attachment_key, {})
-        mime_type = attachment_meta.server_mime
+                    r.write('<li>')
+                    r.write('Size: %s<br>' % fsize)
+                    r.write('Resolution: %d x %d<br>' % (meta['width'], meta['height']))
+                    r.write('Filesize: %d<br>' % ( meta['content_length']))
+                    r.write('<img src="/a/%s/api/case/attachment/%s/%s?img&size=%s">' % (case_doc.domain, case_id, attachment_key, fsize))
+                    r.write('</li>')
+                r.write('</ul></body></html>')
+                return r
+            else:
+                #image workflow
 
+                attachment_meta, attachment_stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=size, width_limit=max_width, height_limit=max_height, fixed_size=size)
+
+
+
+        else:
+            #default stream
+            attachment_meta, attachment_stream = CommCareCase.fetch_case_attachment(case_id, attachment_key)#, filesize_limit=max_size, width_limit=max_width, height_limit=max_height)
+
+        wrapper = FileWrapper(attachment_stream)
+        mime_type = attachment_meta['content_type']
         response = HttpResponse(wrapper, mimetype=mime_type)
         return response
 
