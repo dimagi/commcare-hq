7a42698f0132dfecb89b40f96735ecad7f39ca72
Sravan <sravfeyn@serenity3.(none)>
2013-06-11 18:40:00 +0530

    modified headers parameter.

--- corehq/apps/receiverwrapper/models.py
+++ corehq/apps/receiverwrapper/models.py
@@ -217,7 +217,7 @@ class RepeatRecord(Document, LockableMixIn):
             headers = {
                 "content-type": "text/xml",
                 "content-length": len(payload),
-                "received_on": form.received_on,
+                "received-on": form.received_on,
             }
             post_fn = post_fn or simple_post_with_cached_timeout(headers=headers)
         if self.try_now():
