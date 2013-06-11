2de2464421136b05119b33d181a28893996d2b39
yedi <yedispaghetti@gmail.com>
2013-06-11 11:58:46 -0400

    use more explicit way of getting the opening and closing forms

--- corehq/apps/cleanup/management/commands/add_case_properties.py
+++ corehq/apps/cleanup/management/commands/add_case_properties.py
@@ -14,16 +14,22 @@ def add_to_case(case):
             forms[xid] = xform
         return xform
 
-    if not case.opened_by:
-        creating_xform = xforms(case.xform_ids[0])
-        case.opened_by = creating_xform.metadata.userID
-    if case.closed and not case.closed_by:
-        closing_xform = xforms(case.xform_ids[-1])
-        case.closed_by = closing_xform.metadata.userID
+    should_save = False
 
     for action in case.actions:
         if not action.user_id:
             action.user_id = xforms(action.xform_id).metadata.userID
+            should_save = True
+
+        if not case.opened_by and action.action_type == 'create':
+            case.opened_by = xforms(action.xform_id).metadata.userID
+            should_save = True
+
+        if case.closed and not case.closed_by and action.action_type == 'close':
+            case.closed_by = xforms(action.xform_id).metadata.userID
+            should_save = True
+
+    return should_save
 
 class Command(BaseCommand):
     args = '<domain_name domain_name ...>'
@@ -37,5 +43,5 @@ class Command(BaseCommand):
         for d in domain_names:
             print "Migrating cases in project space: %s" % d
             for case in CommCareCase.by_domain(d):
-                add_to_case(case)
-                case.save()
+                if add_to_case(case):
+                    case.save()
