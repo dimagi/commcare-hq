75bd74f683ac0f69a3038cdd62b2b069bc1e508e
dmyung <dmyung@dimagi.com>
2013-06-13 10:00:50 -0400

    cleanup api to do constraints

--- corehq/apps/api/object_fetch_api.py
+++ corehq/apps/api/object_fetch_api.py
@@ -2,7 +2,7 @@ from django.core.servers.basehttp import FileWrapper
 from django.http import HttpResponse, Http404
 from django.utils.decorators import method_decorator
 from django.views.generic import View
-from dimagi.utils.django.cached_object import IMAGE_SIZE_ORDERING
+from dimagi.utils.django.cached_object import IMAGE_SIZE_ORDERING, OBJECT_ORIGINAL
 from casexml.apps.case.models import CommCareCase
 from corehq.apps.domain.decorators import login_and_domain_required
 
@@ -26,12 +26,12 @@ class CaseAttachmentAPI(View):
         max_image_height	The largest width in pixels for an an image attachment
         """
 
-        max_size = self.request.GET.get('max_size', None)
+        max_filesize = int(self.request.GET.get('max_size', 0)) #todo
 
         img = self.request.GET.get('img', None)
-        size = self.request.GET.get('size', None) #alternative to abs size
-        max_width = self.request.GET.get('max_image_width', None)
-        max_height = self.request.GET.get('max_image_height', None)
+        size = self.request.GET.get('size', OBJECT_ORIGINAL) #alternative to abs size
+        max_width = int(self.request.GET.get('max_image_width', 0))
+        max_height = int(self.request.GET.get('max_image_height', 0))
 
         if size:
             size_key = size
@@ -53,26 +53,43 @@ class CaseAttachmentAPI(View):
                 r.write('<html><body>')
                 r.write('<ul>')
                 for fsize in IMAGE_SIZE_ORDERING:
-                    meta, stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=size, width_limit=max_width, height_limit=max_height, fixed_size=fsize)
+                    meta, stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=max_filesize, width_limit=max_width, height_limit=max_height, fixed_size=fsize)
 
                     r.write('<li>')
                     r.write('Size: %s<br>' % fsize)
-                    r.write('Resolution: %d x %d<br>' % (meta['width'], meta['height']))
-                    r.write('Filesize: %d<br>' % ( meta['content_length']))
-                    r.write('<img src="/a/%s/api/case/attachment/%s/%s?img&size=%s">' % (case_doc.domain, case_id, attachment_key, fsize))
+                    r.write("Limit: max_size: %s" % max_filesize)
+                    if max_width > 0:
+                        r.write(", max_width: %s" % max_width)
+                    if max_height > 0:
+                        r.write(", max_height: %s" % max_height)
+                    r.write("<br>")
+                    if meta is not None:
+                        r.write('Resolution: %d x %d<br>' % (meta['width'], meta['height']))
+                        r.write('Filesize: %d<br>' % ( meta['content_length']))
+                        r.write('<img src="/a/%(domain)s/api/case/attachment/%(case_id)s/%(attachment_key)s?img&size=%(size_key)s&max_size=%(max_filesize)s&max_image_width=%(max_width)s&max_image_height=%(max_height)s">' %
+                                {"domain":case_doc.domain, "case_id":case_id,
+                                 "attachment_key": attachment_key,
+                                 "size_key": fsize,
+                                 "max_filesize":max_filesize,
+                                 "max_width": max_width,
+                                 "max_height": max_height})
+                    else:
+                        r.write('Not available')
                     r.write('</li>')
                 r.write('</ul></body></html>')
                 return r
             else:
                 #image workflow
-
-                attachment_meta, attachment_stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=size, width_limit=max_width, height_limit=max_height, fixed_size=size)
+                attachment_meta, attachment_stream = CommCareCase.fetch_case_image(case_id, attachment_key, filesize_limit=max_filesize, width_limit=max_width, height_limit=max_height, fixed_size=size)
         else:
             #default stream
             attachment_meta, attachment_stream = CommCareCase.fetch_case_attachment(case_id, attachment_key)#, filesize_limit=max_size, width_limit=max_width, height_limit=max_height)
 
         wrapper = FileWrapper(attachment_stream)
-        mime_type = attachment_meta['content_type']
+        if attachment_meta is not None:
+            mime_type = attachment_meta['content_type']
+        else:
+            mime_type = "plain/text"
         response = HttpResponse(wrapper, mimetype=mime_type)
         return response
 
