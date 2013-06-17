478fcc5193105ffcbf2ffc4f482476dfd93755aa
Daniel Roberts <droberts@dimagi.com>
2013-06-17 11:33:33 -0400

    expose a way to view all apps with certain commcare settings (as superuser)

--- corehq/apps/app_manager/util.py
+++ corehq/apps/app_manager/util.py
@@ -53,3 +53,29 @@ def is_valid_case_type(case_type):
     False
     """
     return bool(_case_type_regex.match(case_type or ''))
+
+
+def get_settings_values(app):
+    try:
+        profile = app.profile
+    except AttributeError:
+        profile = {}
+    hq_settings = dict([
+        (attr, app[attr])
+        for attr in app.properties() if not hasattr(app[attr], 'pop')
+    ])
+    if hasattr(app, 'custom_suite'):
+        hq_settings.update({'custom_suite': app.custom_suite})
+
+    hq_settings['build_spec'] = app.build_spec.to_string()
+
+    return {
+        'properties': profile.get('properties', {}),
+        'features': profile.get('features', {}),
+        'hq': hq_settings,
+        '$parent': {
+            'doc_type': app.get_doc_type(),
+            '_id': app.get_id,
+            'domain': app.domain,
+        }
+    }
