61e77a7467e469bac50c3da061b0bac2023f2b5a
Giovanni Capalbo <gcapalbo@dimagi.com>
2013-06-23 17:41:43 -0400

    Add automatically-trimmed CharField

---
+++ dimagi/utils/django/fields.py
@@ -0,0 +1,8 @@
+from django.forms.fields import CharField
+
+class TrimmedCharField(CharField):
+    def clean(self, value):
+        if value is not None:
+            value = value.strip()
+        return super(TrimmedCharField, self).clean(value)
+
