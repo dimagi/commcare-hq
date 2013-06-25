bd5f7c40a95c10343a63f235f76852f5227b8f39
yedi <yedispaghetti@gmail.com>
2013-06-24 23:46:43 -0400

    initial checkpoint for leveraging hash-tab instead of tab

--- corehq/apps/hqwebapp/static/hqwebapp/js-custom/hq.helpers.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js-custom/hq.helpers.js
@@ -27,6 +27,18 @@ $(function() {
         form.find('.disable-on-submit').prop('disabled',true).addClass('disabled');
     });
 
+    // http://stackoverflow.com/questions/12131273/twitter-bootstrap-tabs-url-doesnt-change#answer-12138756
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    $('.nav-tabs a[data-toggle="hash-tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+
     $(document).on('click', '.notification-close-btn', function() {
         var note_id = $(this).data('note-id');
         var post_url = $(this).data('url');
