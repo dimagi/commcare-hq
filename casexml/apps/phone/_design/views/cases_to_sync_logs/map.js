21b7a7e48144d3f9745c737ead4d4a7ff64e0558
Cory Zue <czue@dimagi.com>
2013-07-03 14:05:35 -0400

    hit couch many fewer times during tokenized ota restore

---
+++ casexml/apps/phone/_design/views/cases_to_sync_logs/map.js
@@ -0,0 +1,10 @@
+// Emit the most recent date a case has been modified on by each sync token
+function (doc) {
+    if (doc.doc_type === 'CommCareCase') {
+        var actions = doc.actions;
+        var server_date, sync_log_id;
+        for (var i = 0; i < actions.length; i += 1) {
+            emit(doc._id, actions[i]['sync_log_id']);
+        }
+    }
+}
