240c44ac7c12d5ad02d9879545ee2395bdc4ebab
Tyler Wymer <twymer@gmail.com>
2013-07-09 14:45:13 -0400

    Prevent error with partially created user data
    Previously if an error happened during couch user submission the
    existence of a django user trail would cause a 500

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -965,7 +965,8 @@ class CouchUser(Document, DjangoUserMixin, IsMemberOfMixin, UnicodeMixIn):
     @classmethod
     def create(cls, domain, username, password, email=None, uuid='', date='',
                first_name='', last_name='', **kwargs):
-        django_user = create_user(username, password=password, email=email,
+        django_user = User(username=username) or \
+                      create_user(username, password=password, email=email,
                                   first_name=first_name, last_name=last_name)
         if uuid:
             if not re.match(r'[\w-]+', uuid):
