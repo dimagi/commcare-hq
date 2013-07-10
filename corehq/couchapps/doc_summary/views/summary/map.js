bcdad6495df2f846d124b4b520192b50bffb46bb
Simon Kelly <skelly@dimagi.com>
2013-07-10 17:48:04 +0200

    include multi domain docs in doc_summary

--- corehq/couchapps/doc_summary/views/summary/map.js
+++ corehq/couchapps/doc_summary/views/summary/map.js
@@ -1,6 +1,14 @@
 function (doc) {
-    if (doc.doc_type && doc.domain) {
-        emit(["by_type", doc.doc_type, doc.domain], null);
-        emit(["by_domain", doc.domain, doc.doc_type], null);
+    if (doc.doc_type) {
+        if (doc.domain) {
+            emit(["by_type", doc.doc_type, doc.domain], null);
+            emit(["by_domain", doc.domain, doc.doc_type], null);
+        } else if (doc.domains) {
+            for (var i = 0; i < doc.domains.length; i += 1) {
+                var domain = doc.domains[i];
+                emit(["by_type", doc.doc_type, domain], null);
+                emit(["by_domain", domain, doc.doc_type], null);
+            }
+        }
     }
 }
