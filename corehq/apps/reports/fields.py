33af9ea1b735d40419a03272aa208bd590c20aa5
Biyeun Buczyk <biyeun@dimagi.com>
2013-07-17 22:27:01 +0530

    SelectFilteredMobileWorker field is now reports.filters.users.BaseGroupedMobileWorkerFilter - removed all old user fields and templates from reports.fields

--- corehq/apps/reports/fields.py
+++ corehq/apps/reports/fields.py
@@ -72,39 +72,6 @@ class BooleanField(ReportField):
         self.context['checked'] = self.request.GET.get(self.slug, False)
 
 
-class FilterUsersField(ReportField):
-    slug = "ufilter"
-    template = "reports/filters/filter_users.html"
-    always_show_filter = False
-    can_be_empty = False
-
-    def update_context(self):
-        toggle, show_filter = self.get_user_filter(self.request)
-        self.context['show_user_filter'] = show_filter
-        self.context['toggle_users'] = toggle
-        self.context['can_be_empty'] = self.can_be_empty
-
-    @classmethod
-    def get_user_filter(cls, request):
-        ufilter = group = individual = None
-        try:
-            if request.GET.get('ufilter', ''):
-                ufilter = request.GET.getlist('ufilter')
-            group = request.GET.get('group', '')
-            individual = request.GET.get('individual', '')
-        except KeyError:
-            pass
-        except AttributeError:
-            pass
-
-        show_filter = True
-        toggle = HQUserType.use_defaults()
-
-        if not cls.always_show_filter and (group or individual):
-            show_filter = False
-        elif ufilter:
-            toggle = HQUserType.use_filter(ufilter)
-        return toggle, show_filter
 
 class StrongFilterUsersField(FilterUsersField):
     """
@@ -115,90 +82,6 @@ class StrongFilterUsersField(FilterUsersField):
     """
     always_show_filter = True
     can_be_empty = True
-
-
-class SelectMobileWorkerMixin(object):
-    slug = "select_mw"
-    name = ugettext_noop("Select Mobile Worker")
-
-    @classmethod
-    def get_default_text(cls, user_filter, default_option=None):
-        default = default_option or cls.default_option
-        if user_filter[HQUserType.ADMIN].show or \
-           user_filter[HQUserType.DEMO_USER].show or user_filter[HQUserType.UNKNOWN].show:
-            default = _('%s & Others') % _(default)
-        return default
-
-class SelectMobileWorkerField(SelectMobileWorkerMixin, ReportField):
-    template = "reports/filters/select_mobile_worker.html"
-    default_option = ugettext_noop("All Mobile Workers")
-    filter_users_field_class = FilterUsersField
-
-    def __init__(self, request, domain=None, timezone=pytz.utc, parent_report=None, filter_users_field_class=None):
-        super(SelectMobileWorkerField, self).__init__(request, domain, timezone, parent_report)
-        if filter_users_field_class:
-            self.filter_users_field_class = filter_users_field_class
-
-    def update_params(self):
-        pass
-
-    def update_context(self):
-        self.user_filter, _ = self.filter_users_field_class.get_user_filter(self.request)
-        self.individual = self.request.GET.get('individual', '')
-        self.default_option = self.get_default_text(self.user_filter)
-        self.users = util.user_list(self.domain)
-
-        self.update_params()
-
-        self.context['field_name'] = self.name
-        self.context['default_option'] = self.default_option
-        self.context['users'] = self.users
-        self.context['individual'] = self.individual
-
-
-
-class SelectFilteredMobileWorkerField(SelectMobileWorkerField):
-    """
-        This is a little field for use when a client really wants to filter by
-        individuals from a specific group.  Since by default we still want to
-        show all the data, no filtering is done unless the special group filter
-        is selected.
-    """
-    slug = "select_filtered_mw"
-    name = ugettext_noop("Select Mobile Worker")
-    template = "reports/fields/select_filtered_mobile_worker.html"
-    default_option = ugettext_noop("All Mobile Workers...")
-
-    # Whether to display both the default option and "Only <group> Mobile
-    # Workers" or just the default option (useful when using a single
-    # group_name and changing default_option to All <group> Workers)
-    show_only_group_option = True
-
-    group_names = []
-
-    def update_params(self):
-        if not self.individual:
-            self.individual = self.request.GET.get('filtered_individual', '')
-        self.users = []
-        self.group_options = []
-        for group in self.group_names:
-            filtered_group = Group.by_name(self.domain, group)
-            if filtered_group:
-                if self.show_only_group_option:
-                    self.group_options.append(dict(group_id=filtered_group._id,
-                        name=_("Only %s Mobile Workers") % group))
-                self.users.extend(filtered_group.get_users(is_active=True, only_commcare=True))
-
-    def update_context(self):
-        super(SelectFilteredMobileWorkerField, self).update_context()
-        self.context['users'] = self.users_to_options(self.users)
-        self.context['group_options'] = self.group_options
-
-    @staticmethod
-    def users_to_options(user_list):
-        return [dict(val=user.user_id,
-            text=user.raw_username,
-            is_active=user.is_active) for user in user_list]
 
 
 class DeviceLogTagField(ReportField):
