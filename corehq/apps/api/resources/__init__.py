37866de83fa9823e472ccddbe2a4b6f91c98b5ce
Kenn Knowles <kknowles@dimagi.com>
2013-07-25 17:44:06 -0400

    Import the entire future

--- corehq/apps/api/resources/__init__.py
+++ corehq/apps/api/resources/__init__.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, absolute_import
+from __future__ import print_function, absolute_import, unicode_literals, division
 
 from tastypie.resources import Resource
 from tastypie.exceptions import InvalidSortError
