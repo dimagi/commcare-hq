c519d1b6654b7b29ed27105aac687156852bad74
dmyung <dmyung@dimagi.com>
2013-07-26 10:54:32 -0400

    fully popen based git info extraction

--- dimagi/utils/gitinfo.py
+++ dimagi/utils/gitinfo.py
@@ -1,73 +1,80 @@
 from __future__ import absolute_import
-import pdb
-from django.conf import settings
-import git
-import time
 from subprocess import Popen, PIPE
-import os
-from datetime import datetime
-import pytz
 import simplejson
 
-
-
-
-def get_project_repo():
-    """
-    Get the root git repo object for the project's root settings.
-    """
-    repo = git.Repo(settings.FILEPATH)
-    return repo
-
 def sub_git_remote_url(git_dir):
-    p = Popen(
-        [
-            'git',
-            '--git-dir',
-            git_dir,
-            'config',
-            '--get',
-            "remote.origin.url"
-        ],
-        stdout=PIPE,
-        stderr=PIPE)
+    args = ['config', '--get', "remote.origin.url"]
+    p = sub_git_cmd(git_dir, args)
     gitout = p.stdout.read().strip()
     return gitout
 
-def sub_git_info(git_dir):
+def sub_git_cmd(git_dir, args):
+    """
+    run git command
+    args are the full command with args
+
+    git_dir, the actual full path of the .git/ repo directory to run commands against
+
+    returns popen object for access to stdout+stderr
+    """
     p = Popen(
         [
             'git',
             '--git-dir',
-            git_dir,
-            'log',
-            '-1',
-            "--pretty=format:{%n  \"sha\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ai\",%n  \"subject\": \"%s\"%n, \"message\": \"%b\"%n}"
-        ],
-        stdout=PIPE,
-        stderr=PIPE)
-    gitout = p.stdout.read().strip()
-    url = sub_git_remote_url(git_dir)
+            git_dir
+        ] + args,
+        stdout=PIPE, stderr=PIPE
+    )
+    return p
 
-    commit_dict = simplejson.loads(gitout)
-    commit_dict['commit_url'] = get_commit_url(url, commit_dict['sha'])
-    commit_dict['compare_master'] = get_compare_url(url, commit_dict['sha'], 'master')
-    return commit_dict
+
+def sub_get_current_branch(git_dir):
+    args = [
+        'rev-parse',
+        '--abbrev-ref',
+        'HEAD'
+    ]
+    p = sub_git_cmd(git_dir, args)
+    return p.stdout.read().strip()
+
+
+def get_project_snapshot(git_dir, submodules=False):
+    root_info = sub_git_info(git_dir)
+    root_info['current_branch'] = sub_get_current_branch(git_dir)
+    if submodules:
+        root_info['submodules'] = list(sub_git_submodules(git_dir))
+    return root_info
+
+
+
+def sub_git_info(git_dir, ref_count=1):
+    info_dict = {}
+
+    args = ['log',
+            '-%s' % ref_count,
+            #"""--pretty=format:{%n \"sha\": \"%H\",%n  \"author\": \"%an <%ae>\",%n \"date\": \"%ai\",%n \"subject\": \"%s\",%n \"message\": \"%b\"%n}"""
+            """--pretty=format:{ \"sha\": \"%H\",  \"author\": \"%an <%ae>\", \"date\": \"%ai\", \"subject\": \"%s\", \"message\": \"%b\"}"""
+        ]
+    p = sub_git_cmd(git_dir, args)
+    gitout = p.stdout.read().strip()
+    revs = gitout.split('\n')
+    url = sub_git_remote_url(git_dir)
+    revsstring = '[%s]' % ','.join(revs)
+
+    commit_list = simplejson.loads(revsstring)
+    info_dict['commits'] = commit_list
+    info_dict['commit_url'] = get_commit_url(url, commit_list[0]['sha'])
+    info_dict['compare_master'] = get_compare_url(url, commit_list[0]['sha'], 'master')
+    return info_dict
 
 
 
 def sub_git_submodules(git_dir):
-    p = Popen(
-        [
-            'git',
-            '--git-dir',
-            git_dir,
-            'submodule',
-            'status'
-        ],
-        stdout=PIPE,
-        stderr=PIPE
-    )
+    """
+    Using shell, get the active submodule info
+    """
+    args =['submodule', 'status' ]
+    p = sub_git_cmd(git_dir, args)
     gitout = p.stdout.read().split('\n')
 
     for x in gitout:
@@ -79,66 +86,77 @@ def sub_git_submodules(git_dir):
                 'branch': splits[2],
             }
 
-def submodule_info_hack(repo, prior_project=None):
-    """
-    A hacky thing to get at least the submodule commit URL from the submodule info.
-    It's a bit of a mess due to how we submodule call, gitpython has issues figuring out what's
-    going on.
-    """
 
-    ret = {}
-    for s in repo.iter_submodules():
-        git_dir = os.path.join(settings.FILEPATH, '.git', 'modules', s.name)
-
-        subm = {}
-        sub_key = s.url.split('/')[-1].split('.')[0]
-        subm['local_name'] = s.name.replace('submodules/', '')
-        #p = Popen(['git', '--git-dir', git_dir, 'log', '-1'], stdout=PIPE, stderr=PIPE)
-        #http://cfmumbojumbo.com/cf/index.cfm/coding/convert-your-git-log-to-json/
-        p = Popen(['git', '--git-dir', git_dir, 'log', '-1', "--pretty=format:{%n  \"sha\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ai\",%n  \"subject\": \"%s\"%n, \"message\": \"%b\"%n}"], stdout=PIPE, stderr=PIPE)
-        gitout = p.stdout.read().strip()
-        commit_dict = simplejson.loads(gitout)
-        commit_dict['commit_url'] = get_commit_url(s.url, s.hexsha)
-        commit_dict['compare_master'] = get_compare_url(s.url, s.hexsha, 'master')
-
-        subm['commit'] = commit_dict
-
-        # if prior_project:
-        #     prev_submodules = prior_project['submodules']
-        #     if sub_key in prev_submodules:
-        #         subm['prior_commit'] =
-
-        ret[sub_key] = subm
-    return ret
+######
+# bad gitypython stuff
+# def get_project_repo():
+#     """
+#     Get the root git repo object for the project's root settings.
+#     """
+#     repo = git.Repo(settings.FILEPATH)
+#     return repo
 
 
-def get_latest_commit_info(repo, commit_limit=5, from_sha=None):
-    """
-    Snapshot the commit info for current running repo.
-    Args:
-    commit_limit: basic check for last <int> commits
-    from_sha: compare directly via a
-    """
-    commits = repo.iter_commits()
-    commit_count = 0
+# def submodule_info_hack(repo, prior_project=None):
+#     """
+#     A hacky thing to get at least the submodule commit URL from the submodule info.
+#     It's a bit of a mess due to how we submodule call, gitpython has issues figuring out what's
+#     going on.
+#     """
+#
+#     ret = {}
+#     for s in repo.iter_submodules():
+#         git_dir = os.path.join(settings.FILEPATH, '.git', 'modules', s.name)
+#
+#         subm = {}
+#         sub_key = s.url.split('/')[-1].split('.')[0]
+#         subm['local_name'] = s.name.replace('submodules/', '')
+#         #p = Popen(['git', '--git-dir', git_dir, 'log', '-1'], stdout=PIPE, stderr=PIPE)
+#         #http://cfmumbojumbo.com/cf/index.cfm/coding/convert-your-git-log-to-json/
+#         p = Popen(['git', '--git-dir', git_dir, 'log', '-1', "--pretty=format:{%n  \"sha\": \"%H\",%n  \"author\": \"%an <%ae>\",%n  \"date\": \"%ai\",%n  \"subject\": \"%s\"%n, \"message\": \"%b\"%n}"], stdout=PIPE, stderr=PIPE)
+#         gitout = p.stdout.read().strip()
+#         commit_dict = simplejson.loads(gitout)
+#         commit_dict['commit_url'] = get_commit_url(s.url, s.hexsha)
+#         commit_dict['compare_master'] = get_compare_url(s.url, s.hexsha, 'master')
+#
+#         subm['commit'] = commit_dict
+#
+#         # if prior_project:
+#         #     prev_submodules = prior_project['submodules']
+#         #     if sub_key in prev_submodules:
+#         #         subm['prior_commit'] =
+#
+#         ret[sub_key] = subm
+#     return ret
 
-    if compare_previous:
-        # find last commit for given type
-        pass
 
-
-    while True:
-        commit = commit_info(commits.next())
-        yield commit
-        commit_count += 1
-        if from_sha is None:
-            if commit_count == commit_limit:
-                break
-        if from_sha == commit['sha']:
-            #print "it's equal, break"
-            break
-        # else:
-        #     print "#%s# != #%s#" % (from_sha, commit['sha'])
+# def get_latest_commit_info(repo, commit_limit=5, from_sha=None):
+#     """
+#     Snapshot the commit info for current running repo.
+#     Args:
+#     commit_limit: basic check for last <int> commits
+#     from_sha: compare directly via a
+#     """
+#     commits = repo.iter_commits()
+#     commit_count = 0
+#
+#     if from_sha:
+#         # find last commit for given type
+#         pass
+#
+#
+#     while True:
+#         commit = commit_info(commits.next())
+#         yield commit
+#         commit_count += 1
+#         if from_sha is None:
+#             if commit_count == commit_limit:
+#                 break
+#         if from_sha == commit['sha']:
+#             #print "it's equal, break"
+#             break
+#         # else:
+#         #     print "#%s# != #%s#" % (from_sha, commit['sha'])
 
 
 def split_repo_url(repo_url):
@@ -170,42 +188,3 @@ def get_compare_url(repo_url, start_cmp, end_cmp):
     }
     return url
 
-
-def split_commit_info(commit):
-    """
-    helper function to get the relevant commit repo url and commit hexsha
-    commit is a Gitpython commit object
-    """
-    return commit.repo.remote().url, commit.hexsha
-
-
-def commit_info(commit):
-    """
-    Return json object of relevant info on the commit for the given repo
-    """
-    ret = {}
-    ret['author'] = "%s" % commit.author.name if commit.author.name != "" else commit.author.email
-    ret['branch'] = commit.repo.active_branch.name
-    ret['date'] = datetime.isoformat(datetime.fromtimestamp(commit.committed_date, tz=pytz.utc))
-    ret['message'] = commit.message.strip()
-    ret['sha'] = commit.hexsha
-    ret['commit_url'] = get_commit_url(*split_commit_info(commit))
-
-    commit_repo_url, commit_hash = split_commit_info(commit)
-
-    ret['compare_master'] = get_compare_url(commit_repo_url, commit_hash, 'master')
-
-    #since last deploy
-    #ret['compare_url'] = get_compare_url(*split_commit_info(commit))
-    return ret
-
-
-def get_project_info(commits=5, from_sha=None, compare_previous=False, environment=None):
-    repo = get_project_repo()
-    return {
-        #'repo': repo,
-        'project_branch': repo.active_branch.name,
-
-        'last_commits': list(get_latest_commit_info(repo, commits, from_sha=from_sha)),
-        'submodules': submodule_info_hack(repo)
-    }
