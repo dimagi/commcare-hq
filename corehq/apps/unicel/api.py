38423738a2289faa10d4476ca55b98f7898049f1
Ethan Soergel <esoergel@gmail.com>
2013-07-26 18:35:12 -0400

    Added two additional date formats to check for, moved conversion to its own function.  Also added a unit test. Is there a prettier way to do this than these nested try/except blocks?

--- corehq/apps/unicel/api.py
+++ corehq/apps/unicel/api.py
@@ -20,8 +20,8 @@ class InboundParams(object):
     TIMESTAMP = "stime"
     UDHI = "udhi"
     DCS = "dcs"
-    
-class OutboundParams(object):    
+
+class OutboundParams(object):
     """
     A constant-defining class for outbound sms params
     """
@@ -35,8 +35,6 @@ class OutboundParams(object):
 UNICODE_PARAMS = [("udhi", 0),
                   ("dcs", 8)]
 
-DATE_FORMAT = "%m/%d/%y %I:%M:%S %p"
-
 def _check_environ():
     if not hasattr(settings, "UNICEL_CONFIG") \
     or not settings.UNICEL_CONFIG.get("username", "") \
@@ -47,12 +45,33 @@ def _check_environ():
                         "called UNICEL_CONFIG to use this backend")
 
 def _config():
-    _check_environ() 
+    _check_environ()
     return settings.UNICEL_CONFIG
+
+DATE_FORMAT = "%m/%d/%y %I:%M:%S %p"
+DATE_FORMAT2 = "%Y-%m-%d %H:%M:%S"
+DATE_FORMAT3 = "%Y-%m-%d%%20%H:%M:%S"
+
+def convert_timestamp(timestamp):
+    actual_timestamp = None
+    try:
+        actual_timestamp = datetime.strptime(timestamp, DATE_FORMAT)
+    except ValueError:
+        try:
+            actual_timestamp = datetime.strptime(timestamp, DATE_FORMAT2)
+        except ValueError:
+            try:
+                actual_timestamp = datetime.strptime(timestamp, DATE_FORMAT3)
+            except ValueError, e:
+                logging.warning('could not parse unicel inbound timestamp [%s]' % timestamp)
+    if actual_timestamp:
+        actual_timestamp = pytz.timezone('Asia/Kolkata').localize(actual_timestamp).astimezone(pytz.utc)
+    return actual_timestamp
+
 
 def create_from_request(request, delay=True):
     """
-    From an inbound request (representing an incoming message), 
+    From an inbound request (representing an incoming message),
     create a message (log) object with the right fields populated.
     """
     sender = request.REQUEST[InboundParams.SENDER]
@@ -64,14 +83,9 @@ def create_from_request(request, delay=True):
         sender = '91' + sender
 
     # parse date or default to current utc time
-    actual_timestamp = None
     if timestamp:
-        try:
-            actual_timestamp = datetime.strptime(timestamp, DATE_FORMAT)
-            actual_timestamp = pytz.timezone('Asia/Kolkata').localize(actual_timestamp).astimezone(pytz.utc)
-        except Exception, e:
-            logging.warning('could not parse unicel inbound timestamp [%s]' % timestamp)
-    
+        actual_timestamp = convert_timestamp(timestamp)
+
     # not sure yet if this check is valid
     is_unicode = request.REQUEST.get(InboundParams.UDHI, "") == "1"
     if is_unicode:
@@ -80,7 +94,7 @@ def create_from_request(request, delay=True):
     log = incoming(sender, message, API_ID, timestamp=actual_timestamp, delay=delay)
 
     return log
-    
+
 def receive_phone_number():
     return _config().get('receive_phone')
 
@@ -89,14 +103,14 @@ def send(message, delay=True):
     Send an outbound message using the Unicel API
     """
     config = _config()
-    
+
     phone_number = clean_phone_number(message.phone_number).replace("+", "")
     # these are shared regardless of API
     params = [(OutboundParams.DESTINATION, phone_number),
               (OutboundParams.USERNAME, config["username"]),
               (OutboundParams.PASSWORD, config["password"]),
               (OutboundParams.SENDER, config["sender"])]
-    try: 
+    try:
         text = str(message.text)
         # it's ascii
         params.append((OutboundParams.MESSAGE, text))
@@ -115,5 +129,5 @@ def send(message, delay=True):
 
     return data
 
-    
-    
+
+
