cd65d06f7490696e2f0caffd144ad84986f5a425
dmyung <dmyung@dimagi.com>
2013-07-27 22:08:55 -0400

    adding more information to the output so fab info is decipherable

--- corehq/apps/hqadmin/management/commands/clear_supervisor_confs.py
+++ corehq/apps/hqadmin/management/commands/clear_supervisor_confs.py
@@ -3,6 +3,7 @@ from optparse import make_option
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
+import sys
 
 
 class Command(BaseCommand):
@@ -16,8 +17,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         conf_dir = options['conf_location']
         environment = settings.SERVER_ENVIRONMENT
+        if not os.path.exists(conf_dir):
+            sys.exit("[clear_supervisor_confs] Error: the path %s is not reachable by this process" % conf_dir)
         files = os.listdir(conf_dir)
         env_confs = filter(lambda x: x.startswith('%s_' % environment) and x.endswith('.conf'), files)
         for c in env_confs:
             os.remove(os.path.join(conf_dir, c))
+            print "\t[clear_supervisor_confs] Removed supervisor configuration file: %s" % c
 
--- corehq/apps/hqadmin/management/commands/make_supervisor_conf.py
+++ corehq/apps/hqadmin/management/commands/make_supervisor_conf.py
@@ -42,16 +42,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         conf_dest = options['conf_destination']
         conf_file = options['conf_file']
+
+
         root_dir = settings.FILEPATH
         params = options['params']
         environment = settings.SERVER_ENVIRONMENT
-        print params
 
-        with open(os.path.join(root_dir, 'services', 'templates', conf_file), 'r') as fin:
+        conf_file_fullpath = os.path.join(root_dir, 'services', 'templates', conf_file)
+        if not os.path.isfile(conf_file_fullpath):
+            sys.exit("[make_supervisor_conf] Error: file %s does not exist as a template to use - you're doing something wrong" % conf_file_fullpath) #needs to be in source control moron!
+
+        if not os.path.exists(conf_dest):
+            sys.exit("[clear_supervisor_confs] Error: the destination path %s is not reachable by this process" % conf_dest)
+
+        with open(conf_file_fullpath, 'r') as fin:
             conf_string = fin.read()
             rendered_conf = conf_string % params
-            with open(os.path.join(conf_dest, '%s_%s' % (environment, conf_file)), 'w') as fout:
+            dest_filepath = os.path.join(conf_dest, '%s_%s' % (environment, conf_file))
+            with open(dest_filepath, 'w') as fout:
                 fout.write(rendered_conf)
-            print rendered_conf
+                print "\t[clear_supervisor_conf] Wrote supervisor configuration: %s" % dest_filepath
 
 
