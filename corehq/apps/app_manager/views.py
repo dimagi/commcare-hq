0be3a090a25b0403ee9de09d67890bc1d6bc90b3
Daniel Roberts <droberts@dimagi.com>
2013-07-29 18:57:26 -0400

    move the form validation error messages to be part of validate_for_build
    showing up both on the form view page and showing up in the list of build errors with a link to the correct form

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -344,10 +344,8 @@ def get_form_view_context(request, form, langs, is_user_registration, messages=m
             form_errors.append("Syntax Error: %s" % e)
         except AppError as e:
             form_errors.append("Error in application: %s" % e)
-        except XFormValidationError as e:
-            message = unicode(e)
-            form_errors.append((html.escape(message).replace('\n', '<br/>'), {'extra_tags': 'html'}))
-
+        except XFormValidationError:
+            pass
         except XFormError as e:
             form_errors.append("Error in form: %s" % e)
         # any other kind of error should fail hard, but for now there are too many for that to be practical
