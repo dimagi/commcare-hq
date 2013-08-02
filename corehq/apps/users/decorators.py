cefe7aedcc4ca8eea180d97903ced1295c140910
Cory Zue <czue@dimagi.com>
2013-08-02 10:43:35 -0400

    redirect people to domain specific login from user area (Case:68870)

--- corehq/apps/users/decorators.py
+++ corehq/apps/users/decorators.py
@@ -1,7 +1,5 @@
-from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
-from corehq.apps.domain.decorators import login_and_domain_required, _redirect_for_login_or_domain
-from corehq.apps.domain.models import Domain
+from corehq.apps.domain.decorators import login_and_domain_required, domain_specific_login_redirect
 
 
 def require_permission(permission, data=None, login_decorator=login_and_domain_required):
@@ -16,9 +14,7 @@ def require_permission(permission, data=None, login_decorator=login_and_domain_r
     def decorator(view_func):
         def _inner(request, domain, *args, **kwargs):
             if not hasattr(request, "couch_user"):
-                project = Domain.get_by_name(domain)
-                login_url = reverse('login', kwargs={'domain_type': project.domain_type})
-                return _redirect_for_login_or_domain(request, 'next', login_url)
+                return domain_specific_login_redirect(request, domain)
             elif request.user.is_superuser or request.couch_user.has_permission(domain, permission, data=data):
                 return view_func(request, domain, *args, **kwargs)
             else:
