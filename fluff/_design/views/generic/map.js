bc60769df7c7ef837ec89f987bed3044bb8c23b3
Simon Kelly <skelly@dimagi.com>
2013-08-05 15:24:20 +0200

    allow per emitter group_by

--- fluff/_design/views/generic/map.js
+++ fluff/_design/views/generic/map.js
@@ -1,4 +1,20 @@
 function (doc) {
+    function typeOf(value) {
+        // from Crockford himself: http://javascript.crockford.com/remedial.html
+        var s = typeof value;
+        if (s === 'object') {
+            if (value) {
+                if (Object.prototype.toString.call(value) == '[object Array]') {
+                    s = 'array';
+                }
+            } else {
+                s = 'null';
+            }
+        }
+        return s;
+    }
+
+
     if (doc.base_doc === 'IndicatorDocument') {
         var key = [doc.doc_type], i;
         for (i = 0; i < doc.group_by.length; i++) {
@@ -12,7 +28,12 @@ function (doc) {
                         if (doc[calcName].hasOwnProperty(emitterName)) {
                             for (i = 0; i < doc[calcName][emitterName].length; i++) {
                                 var value = doc[calcName][emitterName][i];
-                                emit(key.concat([calcName, emitterName, value[0]]), value[1]);
+                                if (typeOf(value) === 'object') {
+                                    var custom_key = value['group_by']
+                                    emit(custom_key.concat([calcName, emitterName, value['date']]), value['value']);
+                                } else {
+                                    emit(key.concat([calcName, emitterName, value[0]]), value[1]);
+                                }
                             }
                         }
                     }
