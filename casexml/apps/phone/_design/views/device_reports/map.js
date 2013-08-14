2ac38f8f8c0db2c7ea6fb502a7ae22f68432796b
Cory Zue <czue@dimagi.com>
2013-08-14 15:07:25 -0700

    remove dead views

--- casexml/apps/phone/_design/views/device_reports/map.js
+++
@@ -1,7 +0,0 @@
-function(doc) { 
-    XMLNS = "http://code.javarosa.org/devicereport";
-    if (doc["#doc_type"] == "XForm" && doc["@xmlns"]==XMLNS) {
-        date = new Date(doc.report_date);
-        emit([doc.device_id, date], null);
-    }
-}
