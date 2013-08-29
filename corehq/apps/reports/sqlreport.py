6350fe626381547238d2b3f849b6783c0a137592
Simon Kelly <skelly@dimagi.com>
2013-08-29 12:21:34 +0200

    fix report header groups for sql reports

--- corehq/apps/reports/sqlreport.py
+++ corehq/apps/reports/sqlreport.py
@@ -236,7 +236,16 @@ class SqlTabularReport(SqlData, GenericTabularReport):
 
     @property
     def headers(self):
-        return DataTablesHeader(*[c.data_tables_column for c in self.columns])
+        datatables_columns = []
+        groups = []
+        for column in self.columns:
+            if column.header_group and column.header_group not in groups:
+                datatables_columns.append(column.header_group)
+                groups.append(column.header_group)
+            elif not column.header_group:
+                datatables_columns.append(column.data_tables_column)
+
+        return DataTablesHeader(*datatables_columns)
 
     @property
     def rows(self):
