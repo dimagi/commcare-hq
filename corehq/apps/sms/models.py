4651e489fa9a7713d63603caf8d40bd1ee4be510
Giovanni Capalbo <gcapalbo@dimagi.com>
2013-09-16 15:08:27 -0400

    Rename xforms_session_id to prevent conflict

--- corehq/apps/sms/models.py
+++ corehq/apps/sms/models.py
@@ -37,7 +37,7 @@ class MessageLog(Document, UnicodeMixIn):
     billed                      = BooleanProperty(default=False)
     billing_errors              = ListProperty()
     workflow = StringProperty() # One of the WORKFLOW_* constants above describing what kind of workflow this sms was a part of
-    xforms_session_id = StringProperty() # Points to the _id of an instance of corehq.apps.smsforms.models.XFormsSession that this sms is tied to
+    xforms_session_couch_id = StringProperty() # Points to the _id of an instance of corehq.apps.smsforms.models.XFormsSession that this sms is tied to
     reminder_id = StringProperty() # Points to the _id of an instance of corehq.apps.reminders.models.CaseReminder that this sms is tied to
 
     def __unicode__(self):
