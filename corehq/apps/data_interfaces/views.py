65430c162ef556c8b2e6b8615aeacf8b35665bd5
Biyeun Buczyk <biyeun@dimagi.com>
2013-09-15 23:20:55 -0400

    Beginnings of the Top-Level Case Group View

--- corehq/apps/data_interfaces/views.py
+++ corehq/apps/data_interfaces/views.py
@@ -1,9 +1,16 @@
+from casexml.apps.case.models import CommCareCaseGroup
 from corehq import CaseReassignmentInterface
+from corehq.apps.data_interfaces.forms import AddCaseGroupForm
 from corehq.apps.domain.decorators import login_and_domain_required
+from corehq.apps.domain.views import BaseDomainView
+from corehq.apps.hqwebapp.views import CRUDPaginatedViewMixin, FetchCRUDPaginatedDataView
 from corehq.apps.reports.standard.export import ExcelExportReport
 from corehq.apps.data_interfaces.dispatcher import DataInterfaceDispatcher, EditDataInterfaceDispatcher
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404
+from dimagi.utils.decorators.memoized import memoized
+from django.utils.translation import ugettext as _, ugettext_noop
+
 
 @login_and_domain_required
 def default(request, domain):
@@ -26,3 +33,73 @@ def default(request, domain):
         return HttpResponseRedirect(reverse(EditDataInterfaceDispatcher.name(),
                                             args=[domain, CaseReassignmentInterface.slug]))
     raise Http404()
+
+
+class CaseGroupListView(BaseDomainView, CRUDPaginatedViewMixin):
+    template_name = "data_interfaces/list_case_groups.html"
+    urlname = 'case_group_list'
+    page_title = ugettext_noop("Case Groups")
+    section_name = ugettext_noop("Data")
+
+    @property
+    def section_url(self):
+        return reverse("data_interfaces_default", args=[self.domain])
+
+    @property
+    def page_url(self):
+        return reverse(self.urlname, args=[self.domain])
+
+    @property
+    @memoized
+    def total(self):
+        return len(CommCareCaseGroup.get_all(self.domain))
+
+    @property
+    def crud_url(self):
+        return reverse(CRUDCaseGroupListView.urlname, args=[self.domain])
+
+    def get_create_form(self, is_blank=False):
+        if self.request.method == 'POST' and not is_blank:
+            return AddCaseGroupForm(self.request.POST)
+        return AddCaseGroupForm()
+
+    def get_new_item_data(self, create_form):
+        return {
+            'itemData': {
+                'id': 'ewreawr121342',
+                'name': 'All New Group',
+                'numCases': 2,
+            },
+            'templateSelector': '#new-group-row',
+        }
+
+    @property
+    def column_names(self):
+        return [
+            _("Group Name"),
+            _("Number of Cases"),
+            _("Action"),
+        ]
+
+    @property
+    def page_context(self):
+        return self.pagination_context
+
+
+class CRUDCaseGroupListView(FetchCRUDPaginatedDataView, CaseGroupListView):
+    urlname = 'fetch_case_group_list'
+
+    @property
+    def paginated_list(self):
+        return [
+            {
+                'itemData': {
+                    'id': '313235dsfa',
+                    'name': 'New Group',
+                    'numCases': 0,
+                },
+                'templateSelector': '#existing-group-row',
+            }
+        ]
+
+
