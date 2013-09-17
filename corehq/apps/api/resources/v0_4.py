2b341437cb8c9c1029f4d1e59d1d8299b9664879
Kenn Knowles <kknowles@dimagi.com>
2013-09-17 19:03:45 -0400

    remove unused import

--- corehq/apps/api/resources/v0_4.py
+++ corehq/apps/api/resources/v0_4.py
@@ -1,5 +1,4 @@
 from collections import defaultdict
-from itertools import groupby
 
 from django.core.urlresolvers import NoReverseMatch, reverse
 from django.http import HttpResponseForbidden, HttpResponse, HttpResponseBadRequest
