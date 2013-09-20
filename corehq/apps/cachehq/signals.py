09b0cbf1a33416b101442a1b26c3d15ab8fb9d83
dmyung <dmyung@dimagi.com>
2013-09-20 13:45:27 -0400

    enable couch_user signal for cache invalidation

--- corehq/apps/cachehq/signals.py
+++ corehq/apps/cachehq/signals.py
@@ -1,6 +1,6 @@
 from corehq.apps.domain.signals import commcare_domain_post_save
-from corehq.apps.groups.signals import commcare_group_post_save
-from corehq.apps.users.signals import commcare_user_post_save, couch_user_post_save
+from corehq.apps.users.signals import couch_user_post_save
+#from corehq.apps.groups.signals import commcare_group_post_save
 
 from corehq.pillows import cacheinvalidate
 
@@ -14,9 +14,10 @@ commcare_domain_post_save.connect(invalidate_cached_domain)
 
 def invalidate_cached_user(sender, **kwargs):
     cache_pillow.change_trigger({'doc': kwargs['couch_user'].to_json(), 'id': kwargs['couch_user']['_id']})
-# couch_user_post_save.connect(invalidate_cached_user)
+couch_user_post_save.connect(invalidate_cached_user)
 
 
 def invalidate_cached_group(sender, **kwargs):
     cache_pillow.change_trigger({'doc': kwargs['group'].to_json(), 'id': kwargs['group']._id})
+# TODO for phase2 cache
 # commcare_group_post_save.connect(invalidate_cached_group)
