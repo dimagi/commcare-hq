f1b22c43b91e8dcc3d801169fe4197e45496d771
Daniel Roberts <droberts@dimagi.com>
2013-09-21 00:23:05 -0400

    add regression tests for xml-to-json conversion

--- couchforms/tests/__init__.py
+++ couchforms/tests/__init__.py
@@ -6,6 +6,7 @@ try:
     from .test_edits import *
     from .test_namespaces import *
     from .test_auth import *
+    from .test_post import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
     # otherwise debugging is a pain
