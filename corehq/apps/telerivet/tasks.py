4a49d68d7ee89d8e879dd28a59dadce381cc8b7e
Giovanni Capalbo <gcapalbo@dimagi.com>
2013-09-24 21:26:28 -0400

    Change parameters to task

--- corehq/apps/telerivet/tasks.py
+++ corehq/apps/telerivet/tasks.py
@@ -9,26 +9,13 @@ MESSAGE_TYPE_USSD = "ussd"
 MESSAGE_TYPE_CALL = "call"
 
 @task
-def process_incoming_message(request):
-    event = request.POST.get("event")
-    message_id = request.POST.get("id")
-    message_type = request.POST.get("message_type")
-    from_number = request.POST.get("from_number")
-    contact_id = request.POST.get("contact_id")
-    phone_id = request.POST.get("phone_id")
-    to_number = request.POST.get("to_number")
-    time_created = request.POST.get("time_created")
-    time_sent = request.POST.get("time_sent")
-    content = request.POST.get("content")
-    project_id = request.POST.get("project_id")
-    secret = request.POST.get("secret")
-
-    backend = TelerivetBackend.by_webhook_secret(secret)
+def process_incoming_message(*args, **kwargs):
+    backend = TelerivetBackend.by_webhook_secret(kwargs["secret"])
     if backend is None:
         # Ignore the message if the webhook secret is not recognized
         return
 
-    if event == EVENT_INCOMING:
-        if message_type == MESSAGE_TYPE_SMS:
-            incoming_sms(from_number, content, TelerivetBackend.get_api_id())
+    if kwargs["event"] == EVENT_INCOMING:
+        if kwargs["message_type"] == MESSAGE_TYPE_SMS:
+            incoming_sms(kwargs["from_number"], kwargs["content"], TelerivetBackend.get_api_id())
 
--- corehq/apps/telerivet/views.py
+++ corehq/apps/telerivet/views.py
@@ -6,6 +6,21 @@ from corehq.apps.telerivet.tasks import process_incoming_message
 @require_POST
 @csrf_exempt
 def incoming_message(request):
-    process_incoming_message.delay(request)
+    kwargs = {
+        "event" : request.POST.get("event"),
+        "message_id" : request.POST.get("id"),
+        "message_type" : request.POST.get("message_type"),
+        "from_number" : request.POST.get("from_number"),
+        "contact_id" : request.POST.get("contact_id"),
+        "phone_id" : request.POST.get("phone_id"),
+        "to_number" : request.POST.get("to_number"),
+        "time_created" : request.POST.get("time_created"),
+        "time_sent" : request.POST.get("time_sent"),
+        "content" : request.POST.get("content"),
+        "project_id" : request.POST.get("project_id"),
+        "secret" : request.POST.get("secret"),
+    }
+
+    process_incoming_message.delay(**kwargs)
     return HttpResponse()
 
