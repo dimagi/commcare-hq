584b5a42805dd9a969bbf9186393c22e0f8b6548
Marcin Sienkiewicz <msienkiewicz@soldevelo.com>
2013-09-26 16:59:28 +0200

    Revert "Moved url's loading into module __init__ file"
    This reverts commit cdea227b1b42b26f5796da2fc930909432e01ed8.

--- custom/apps/crs_reports/__init__.py
+++ custom/apps/crs_reports/__init__.py
@@ -1,12 +1,7 @@
 from django.utils.translation import ugettext_noop as _
-from django.conf.urls.defaults import *
-from corehq.apps.reports.urls import custom_report_urls
 
 from custom.apps.crs_reports.reports import HBNCMotherReport, HBNCInfantReport
 
-custom_report_urls += patterns('',
-    (r"^crs-report/", include("custom.apps.crs_reports.urls")),
-)
 
 CUSTOM_REPORTS = (
     (_('Custom Reports'), (
@@ -14,3 +9,4 @@ CUSTOM_REPORTS = (
        HBNCInfantReport,
     )),
 )
+
