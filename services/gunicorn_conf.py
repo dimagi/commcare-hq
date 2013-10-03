83c257b6cb44813dc52d1023cfed0a1a19ae0e91
dmyung <dmyung@dimagi.com>
2013-10-03 10:16:27 -0400

    renaming gunicorn.cfg to python script
    updating django conf file to reflect it, also implementing post_load signal for worker load to address url bootstrap issue

--- services/gunicorn_conf.py
+++ services/gunicorn_conf.py
@@ -12,3 +12,9 @@ graceful_timeout = 5*60
 # http://docs.gunicorn.org/en/latest/configure.html#secure-scheme-headers
 # http://docs.gunicorn.org/en/latest/configure.html#forwarded-allow-ips
 forwarded_allow_ips = '10.176.162.109'
+
+def post_fork(server, worker):
+    # hacky way to address gunicorn gevent requests hitting django too early before urls are loaded
+    # see: https://github.com/benoitc/gunicorn/issues/527#issuecomment-19601046
+    from django.core.urlresolvers import resolve
+    resolve('/')
