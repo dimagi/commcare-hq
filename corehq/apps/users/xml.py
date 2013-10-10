6653e1e7252968f735f3f9f60a7015db99c7f0c2
Cory Zue <czue@dimagi.com>
2013-10-10 10:05:33 -0400

    pep8

--- corehq/apps/users/xml.py
+++ corehq/apps/users/xml.py
@@ -5,15 +5,17 @@ from xml.etree import ElementTree
 This module is used to generate xml responses for user registrations.
 Spec: https://bitbucket.org/javarosa/javarosa/wiki/UserRegistrationAPI
 """
-# this should eventually be harmonized with the other responses, but 
+# this should eventually be harmonized with the other responses, but
 # has been implemented quick and dirty
 from casexml.apps.phone import xml as phone_xml
 from receiver import xml as receiver_xml 
 from receiver.xml import ResponseNature
 
 def get_response(user, created):
-    if created:   text = "Thanks for registering! Your username is %s" % user.username
-    else:         text = "Thanks for updating your information, %s." % user.username
+    if created:
+        text = "Thanks for registering! Your username is %s" % user.username
+    else:
+        text = "Thanks for updating your information, %s." % user.username
         
     nature = ResponseNature.SUBMIT_USER_REGISTERED if created else \
              ResponseNature.SUBMIT_USER_UPDATED
