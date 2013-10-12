22cbe156cdfdcc65c9dbcb1de88fa025550d5157
Cory Zue <czue@dimagi.com>
2013-10-12 15:27:58 -0400

    unused imports

--- custom/openlmis/tests/test_facility_sync.py
+++ custom/openlmis/tests/test_facility_sync.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from casexml.apps.case.tests import delete_all_cases
 from corehq.apps.commtrack.tests import bootstrap_domain
 from corehq.apps.locations.models import Location
-from custom.openlmis.api import get_recent_facilities, Facility, get_facility_programs, FacilityProgramLink
+from custom.openlmis.api import get_recent_facilities
 from custom.openlmis.commtrack import sync_facility_to_supply_point
 
 
