71499744d0c24e37f326d2cd15df67c1e0c4a17b
dmyung <dmyung@dimagi.com>
2013-10-14 11:39:44 -0400

    wip for segmenting pillows out

--- pillowtop/management/commands/run_ptop.py
+++ pillowtop/management/commands/run_ptop.py
@@ -4,7 +4,7 @@ from django.core.management.base import NoArgsCommand
 from pillowtop.run_pillowtop import start_pillows
 
 class Command(NoArgsCommand):
-    help = "Run the pillowtop management command to scan all _changes feeds"
+    help = "Run all pillows listed in settings."
     option_list = NoArgsCommand.option_list + (
     )
     def handle_noargs(self, **options):
