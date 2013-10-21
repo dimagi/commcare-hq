8c64c8357f120e35889c9bb19b4bdd1ebbad91c7
Ethan Soergel <esoergel@gmail.com>
2013-10-20 22:53:59 -0400

    Dim the `make new version` button while querying

--- corehq/apps/app_manager/static/app_manager/js/releases.js
+++ corehq/apps/app_manager/static/app_manager/js/releases.js
@@ -61,6 +61,13 @@ function ReleasesMain(o) {
     self.deployAnyway = {};
     self.currentAppVersion = ko.observable(self.options.currentAppVersion);
     self.lastAppVersion = ko.observable();
+    self.buildButtonEnabled = ko.computed(function () {
+        if (self.buildState() === 'pending' || self.fetchState() === 'pending') {
+            return false;
+        } else {
+            return true;
+        }
+    });
     self.brokenBuilds = ko.computed(function () {
         var apps = self.savedApps();
         return _.some(apps, function (app) {
@@ -166,9 +173,11 @@ function ReleasesMain(o) {
         }
 
         var url = self.url('currentVersion');
+        self.fetchState('pending');
         $.get(
             self.url('currentVersion')
         ).success(function (data) {
+            self.fetchState('');
             self.currentAppVersion(data.currentVersion);
             if (!data.latestRelease) {
                 self.actuallyMakeBuild();
@@ -181,7 +190,7 @@ function ReleasesMain(o) {
                 window.alert("No new changes to deploy!");
             }
         }).error(function () {
-            self.buildState('error');
+            self.fetchState('error');
             window.alert(self.reload_message);
         });
     };
