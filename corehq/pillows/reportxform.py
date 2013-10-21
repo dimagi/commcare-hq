81e0a52f40d2a706ff5f7a347af48637f1b439f6
dmyung <dmyung@dimagi.com>
2013-10-21 15:09:40 -0400

    revert _case_blocks functionality in reportxform pillow

--- corehq/pillows/reportxform.py
+++ corehq/pillows/reportxform.py
@@ -1,9 +1,12 @@
+import copy
 from django.conf import settings
+from casexml.apps.case.xform import extract_case_blocks
 
 from corehq.pillows.base import convert_property_dict
 from .mappings.reportxform_mapping import REPORT_XFORM_INDEX, REPORT_XFORM_MAPPING
 from .xform import XFormPillow
 
+COMPUTED_CASEBLOCKS_KEY = '_case_blocks'
 
 class ReportXFormPillow(XFormPillow):
     """
@@ -14,6 +17,7 @@ class ReportXFormPillow(XFormPillow):
     es_alias = "report_xforms"
     es_type = "report_xform"
     es_index = REPORT_XFORM_INDEX
+
 
     #type level mapping
     default_mapping = REPORT_XFORM_MAPPING
@@ -27,8 +31,6 @@ class ReportXFormPillow(XFormPillow):
             if domain not in getattr(settings, 'ES_XFORM_FULL_INDEX_DOMAINS', []):
                 #full indexing is only enabled for select domains on an opt-in basis
                 return None
-
-            #after basic transforms for stupid type mistakes are done, walk all properties.
             convert_property_dict(doc_ret['form'], self.default_mapping['properties']['form'], override_root_keys=['case'])
             return doc_ret
         else:
