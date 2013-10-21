899b5399d8d529802045fcfdeeba16d9812e5439
Cory Zue <czue@dimagi.com>
2013-10-21 16:39:17 +0530

    fix broken call

--- custom/openlmis/commtrack.py
+++ custom/openlmis/commtrack.py
@@ -151,4 +151,4 @@ def sync_supply_point_to_openlmis(supply_point, openlmis_endpoint, create=True):
     if create:
         return openlmis_endpoint.create_virtual_facility(json_sp)
     else:
-        return openlmis_endpoint.update_virtual_facility(json_sp)
+        return openlmis_endpoint.update_virtual_facility(supply_point.external_id, json_sp)
