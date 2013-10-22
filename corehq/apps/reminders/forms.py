22c6d5e7f3e9ce8cc9d3cbfd3341ca819c28f0fe
Biyeun Buczyk <biyeun@dimagi.com>
2013-10-22 13:46:52 -0400

    add help bubble to stop condition in reminders form

--- corehq/apps/reminders/forms.py
+++ corehq/apps/reminders/forms.py
@@ -1045,6 +1045,11 @@ class SimpleScheduleCaseReminderForm(forms.Form):
                     data_bind="visible: isUntilVisible",
                 ),
                 data_bind="visible: isStopConditionVisible",
+                help_bubble_text=("Reminders can be stopped after a date set in the case, or if a particular "
+                                   "case property is set to OK.  Choose either a case property that is a date or "
+                                   "a case property that is going to be set to Ok.  Reminders will always stop if "
+                                   "the start condition is no longer true."),
+                css_id="stop-condition-group",
             )
         )
 
