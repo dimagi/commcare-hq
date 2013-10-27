e521727a102361f517517a84f89c15955e990aee
yedi <yedispaghetti@gmail.com>
2013-10-27 16:08:17 -0400

    create navigator for the sidebar

--- corehq/apps/hqwebapp/static/hqwebapp/js/keyboard_navigator.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/keyboard_navigator.js
@@ -6,12 +6,16 @@ var KeyboardNavigator = function() {
     self.nav_scope = 'in navigation';
 
     self.init = function(options) {
-        self.element_list = options.element_list;
-        self.handle_hover = options.hover_fn || self.handle_hover ;
-        self.handle_action = options.action_fn || self.handle_action ;
+        self.handle_focus_in = options.handle_focus_in || self.handle_focus_in;
+        self.handle_focus_out = options.handle_focus_out || self.handle_focus_out;
+        self.handle_action = options.action_fn || self.handle_action;
+        self.gen_elements = options.element_list_generator;
+        self.reset_index = options.reset_index !== false;
 
         self.nav_key = options.nav_key || 'option';
         self.action_key = options.action_key || 'enter';
+        self.forward_key = options.forward_key || 'right';
+        self.back_key = options.back_key || 'left';
 
         self.macchrome_keycodes = {
             shift: 16,
@@ -21,6 +25,9 @@ var KeyboardNavigator = function() {
         };
 
         $(function() {
+            self.$elements = self.gen_elements();
+            self.set_index(0);
+
             $(document).keyup(function(event) {
                 if (key.getScope() === self.nav_scope) {
                     if ( event.which === self.macchrome_keycodes[self.nav_key] ) {
@@ -31,20 +38,24 @@ var KeyboardNavigator = function() {
 
             key.setScope(self.ready_scope);
             key(self.nav_key, self.ready_scope, self.enter_nav);
-            key(self.nav_key + '+up', self.handle_nav('up'));
-            key(self.nav_key + '+down', self.handle_nav('down'));
+            key(self.nav_key + '+' + self.forward_key, self.handle_nav(self.forward_key));
+            key(self.nav_key + '+' + self.back_key, self.handle_nav(self.back_key));
             key(self.nav_key + '+' + self.action_key, self.handle_action);
-
         });
     };
 
     self.enter_nav = function() {
+        if (self.reset_index) {
+            self.set_index(0);
+        }
         console.log('Entering navigation');
+        console.log('Index set to: ' + self.index);
         key.setScope(self.nav_scope);
     };
 
     self.leave_nav = function() {
         console.log('Leaving navigation');
+        self.handle_focus_out();
         key.setScope(self.ready_scope);
     };
 
@@ -53,16 +64,36 @@ var KeyboardNavigator = function() {
             if (key.getScope() === self.ready_scope) {
                 self.enter_nav();
             }
+            self.handle_focus_out();
+            self.set_index(self.index + (direction === self.forward_key ? 1 : -1));
             console.log('Just went ' + direction);
+            console.log('index: ' + self.index);
+            self.handle_focus_in();
+            return false;
         }
     };
 
-    self.handle_hover = function() {
-        console.log('hovering over')
+    self.handle_focus_in = function() {
+        console.log('hovering over...');
+        console.log(self.$active_element);
+        self.$active_element.addClass('hovered');
+        self.$active_element.trigger('mouseenter');
+    };
+
+    self.handle_focus_out = function() {
+        self.$active_element.removeClass('hovered');
+        self.$active_element.trigger('mouseleave');
     };
 
     self.handle_action = function() {
+        $mock_ele = $('');
         console.log('action button pressed');
+        self.$active_element.click();
         self.leave_nav();
     };
+
+    self.set_index = function(num) {
+        self.index = num;
+        self.$active_element = $(self.$elements.get(self.index));
+    }
 };
