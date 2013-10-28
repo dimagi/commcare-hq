25793ab49dda00142baf1c151eb3ce932c8b9bcf
Daniel Roberts <droberts@dimagi.com>
2013-10-28 15:01:06 +0200

    bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pillowtop',
-    version='0.1.3.1',
+    version='0.1.4',
     description='A couchdbkit changes listener for doing backend processing',
     author='Dimagi',
     author_email='dev@dimagi.com',
