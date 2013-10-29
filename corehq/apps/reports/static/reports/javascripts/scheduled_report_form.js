9cc96aefb9cd74e5ba5d127cb7b123f25268407f
yedi <yedispaghetti@gmail.com>
2013-10-29 13:19:40 -0400

    Revert "Revert "Merge pull request #1585 from dimagi/monthly-after""
    This reverts commit 6ae48f12a0041d8b619e577cb01aa3fd75397a50.

---
+++ corehq/apps/reports/static/reports/javascripts/scheduled_report_form.js
@@ -0,0 +1,40 @@
+function add_options_to_select($select, opt_list, selected_val) {
+    for (var i = 0; i < opt_list.length; i++) {
+        var $opt = $('<option />').val(opt_list[i][0]).text(opt_list[i][1]);
+        if (opt_list[i][0] === selected_val) {
+            $opt.attr("selected", "true");
+        }
+        $select.append($opt);
+    }
+    return $select;
+}
+
+function update_day_input(weekly_options, monthly_options, day_value) {
+    var $interval = $interval || $('[name="interval"]');
+    $('#div_id_day').remove();
+    if ($interval.val() !== 'daily') {
+        var opts = $interval.val() === 'weekly' ? weekly_options : monthly_options
+        var $day_select = $('<select id="id_day" class="select" name="day" />');
+        $day_select = add_options_to_select($day_select, opts, day_value);
+        var $day_control_group = $('<div id="div_id_day" class="control-group" />')
+                .append($('<label for="id_day" class="control-label requiredField">Day<span class="asteriskField">*</span></label>'))
+                .append($('<div class="controls" />').append($day_select));
+        $interval.parents('.control-group').after($day_control_group);
+    }
+}
+
+var ScheduledReportFormHelper = function(options) {
+    var self = this;
+    self.weekly_options = options.weekly_options;
+    self.monthly_options = options.monthly_options;
+    self.day_value = options.day_value;
+
+    self.init = function() {
+        $(function() {
+            update_day_input(self.weekly_options, self.monthly_options, self.day_value);
+            $('[name="interval"]').change(function() {
+                update_day_input(self.weekly_options, self.monthly_options);
+            })
+        });
+    };
+};
