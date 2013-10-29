6c42e2052f7b87737260362344d0fe97e131007d
Ethan Soergel <esoergel@gmail.com>
2013-10-29 12:08:25 +0530

    Push back snapshot time for smiley reports

--- custom/penn_state/tasks.py
+++ custom/penn_state/tasks.py
@@ -109,7 +109,7 @@ def save_report(date=None):
         )
 
 
-@periodic_task(run_every=crontab(hour=1, day_of_week=6),
+@periodic_task(run_every=crontab(hour=10, day_of_week=6),
         queue=getattr(settings, 'CELERY_PERIODIC_QUEUE','celery'))
 def run_report():
     save_report()
