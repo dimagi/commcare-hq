44da4dde76e092bb6aea5300e43c907f476bd9f5
Cory Zue <czue@dimagi.com>
2013-10-30 14:51:16 +0530

    add global transform for custom exports

---
+++ couchexport/transforms.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+COUCH_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+EXCEL_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+
+def identity(val, doc):
+    return val
+
+
+def auto_format_datetime(expected_format, output_format, val, doc):
+    try:
+        return datetime.strptime(val, expected_format).strftime(output_format)
+    except ValueError:
+        return val
+
+
+def couch_to_excel_datetime(val, doc):
+    return auto_format_datetime(COUCH_FORMAT, EXCEL_FORMAT, val, doc)
