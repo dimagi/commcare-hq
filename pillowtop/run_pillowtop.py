44a4f6d6ecfb339342398479674c294e4fba0217
dmyung <dmyung@dimagi.com>
2013-10-31 13:55:04 -0400

    multiprocess pillowtop and single pillow caling capability

--- pillowtop/run_pillowtop.py
+++ pillowtop/run_pillowtop.py
@@ -1,17 +1,41 @@
-from gevent.pool import Pool
-from restkit.session import set_session; set_session("gevent")
+import sys
 from pillowtop import get_all_pillows
+import multiprocessing
 
+def _do_run_pillow(pillow_class):
+    try:
+        print "running %s" % pillow_class
+        pillow_class.run()
+    except Exception, ex:
+        print "Some pillow error: %s: %s" % (pillow_class.__class__.__name__, ex)
 
 def start_pillows(pillows=None):
     """
     Actual runner for running pillow processes. Use this to run pillows.
     """
-    start_pillows = pillows or get_all_pillows()
-    pool = Pool(len(start_pillows))
+    run_pillows = pillows or get_all_pillows()
+
+    try:
+        while True:
+            jobs = []
+            print "[pillowtop] Starting pillow processes"
+            for pillow_class in run_pillows:
+                p = multiprocessing.Process(target=pillow_class.run)
+                p.start()
+                jobs.append(p)
+            print "[pillowtop] all processes started, pids: %s" % ([x.pid for x in jobs])
+            for j in jobs:
+                j.join()
+            print "[pillowtop] All processes complete, restarting"
+    except KeyboardInterrupt:
+        sys.exit()
+
+
+def start_pillow(pillow_instance):
     while True:
-        for p in start_pillows:
-            pool.spawn(p.run)
-        pool.join()
-        # Pillows if they ever finish will need to restart continue forever.
+        print "Starting pillow %s.run()" % pillow_instance.__class__
+        pillow_instance.run()
+        print "Pillow %s.run() completed, restarting" % pillow_instance.__class__
+
+
 
