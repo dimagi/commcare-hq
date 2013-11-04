5ea95b21ab274a7d935d30de33b0fc6ea40fd7f9
Cory Zue <czue@dimagi.com>
2013-11-04 15:16:53 +0530

    additional argument to not force date_modified to a date

--- custom/bihar/getters.py
+++ custom/bihar/getters.py
@@ -9,9 +9,9 @@ def days_visit_overdue(form):
         return val
 
 
-def date_modified(form):
+def date_modified(form, force_to_date=True):
     val = form.xpath('form/case/@date_modified')
-    if isinstance(val, datetime.datetime):
+    if force_to_date and isinstance(val, datetime.datetime):
         return val.date()
     else:
         return val
