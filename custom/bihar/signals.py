94ed6f5bc05885a7fd95cd8fe57d014ad2481bce
Cory Zue <czue@dimagi.com>
2013-11-06 17:51:06 +0530

    hotfix bihar signal

--- custom/bihar/signals.py
+++ custom/bihar/signals.py
@@ -14,7 +14,7 @@ class BiharMockUser(object):
 
 
 def bihar_reassignment(sender, xform, cases, **kwargs):
-    if xform.domain in BIHAR_DOMAINS and xform.metadata.userID != SYSTEM_USERID:
+    if xform.domain in BIHAR_DOMAINS and xform.metadata and xform.metadata.userID != SYSTEM_USERID:
         owner_ids = set(c.owner_id for c in cases)
         if len(owner_ids) != 1:
             logging.error('form {form} had mismatched case owner ids'.format(form=xform._id))
