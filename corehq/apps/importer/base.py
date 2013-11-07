b8bd5fc044d3dd3b273cba4443c771e60036b6c0
Tyler Wymer <twymer@gmail.com>
2013-11-07 13:06:48 -0500

    Use lazy translation for importer strings

--- corehq/apps/importer/base.py
+++ corehq/apps/importer/base.py
@@ -1,10 +1,10 @@
 from corehq.apps.data_interfaces.interfaces import DataInterface
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy
 
 class ImportCases(DataInterface):
-    name = _("Import Cases from Excel")
+    name = ugettext_lazy("Import Cases from Excel")
     slug = "import_cases"
-    description = _("Import case data from an external Excel file")
+    description = ugettext_lazy("Import case data from an external Excel file")
     report_template_path = "importer/import_cases.html"
     gide_filters = True
     asynchronous = False
