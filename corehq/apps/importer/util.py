761353e88a01c627c6bf5484cad8ef15d473576e
Tyler Wymer <twymer@gmail.com>
2013-11-14 15:39:15 -0500

    Don't fail to convert 0 on import

--- corehq/apps/importer/util.py
+++ corehq/apps/importer/util.py
@@ -264,7 +264,7 @@ def populate_updated_fields(config, columns, row):
             # existing case field was chosen
             update_field_name = field_map[key]['case']
 
-        if update_value:
+        if update_value is not None:
             if field_map[key]['type_field'] == 'date':
                 update_value = parse_excel_date(update_value)
             elif field_map[key]['type_field'] == 'integer':
