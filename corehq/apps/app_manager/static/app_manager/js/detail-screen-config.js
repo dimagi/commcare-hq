16038c5d006016ba8c9402fee40383ebdd004851
Simon Kelly <skelly@dimagi.com>
2013-11-20 14:41:53 +0200

    careplan prep refactor of edit module details view method

--- corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
+++ corehq/apps/app_manager/static/app_manager/js/detail-screen-config.js
@@ -517,6 +517,7 @@ var DetailScreenConfig = (function () {
                     url: this.saveUrl,
                     type: "POST",
                     data: {
+                        type: this.type,
                         screens: JSON.stringify(this.serialize()),
                         parent_select: JSON.stringify({
                             module_id: parentSelect.moduleId(),
