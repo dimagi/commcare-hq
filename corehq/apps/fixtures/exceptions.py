e8c2a7705f2705e380fd331f9ef2ce9f1dc6fcfc
Sravan Reddy <sravfeyn@gmail.com>
2013-11-20 18:28:49 +0530

    better exception handling

--- corehq/apps/fixtures/exceptions.py
+++ corehq/apps/fixtures/exceptions.py
@@ -1,4 +1,17 @@
-
-
 class FixtureException(Exception):
+    pass
+
+class ExcelMalformatException(Exception):
+    pass
+
+
+class FixtureAPIException(Exception):
+    pass
+
+
+class FixtureTypeCheckError(Exception):
+    pass
+
+
+class FixtureVersionError(Exception):
     pass
