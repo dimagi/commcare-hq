f3b06e1786a00ab546e63069a94273445c402ed8
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-21 17:44:56 -0500

    Creation of accounting app in corehq, adding to settings
    Conflicts:
    	settings.py

--- settings.py
+++ settings.py
@@ -173,6 +173,7 @@ HQ_APPS = (
     'corehq.apps.cleanup',
     'corehq.apps.cloudcare',
     'corehq.apps.smsbillables',
+    'corehq.apps.accounting',
     'corehq.apps.appstore',
     'corehq.apps.domain',
     'corehq.apps.domainsync',
