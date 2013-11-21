69228f92906c7e456d65def1fb1a1105496fdd7e
Simon Kelly <skelly@dimagi.com>
2013-11-21 15:44:00 +0200

    refactor careplan form models

--- corehq/apps/app_manager/const.py
+++ corehq/apps/app_manager/const.py
@@ -7,29 +7,3 @@ CAREPLAN_CASE_NAMES = {
     CAREPLAN_GOAL: 'Goal',
     CAREPLAN_TASK: 'Task'
 }
-
-CAREPLAN_DEFAULT_CASE_PROPERTIES = {
-    CAREPLAN_GOAL: {
-        'create': {
-            'description': '/data/description',
-            'date_followup': '/data/date_followup',
-        },
-        'update': {
-            'description': '/data/description_group/description',
-            'date_followup': '/data/date_followup',
-        },
-    },
-    CAREPLAN_TASK: {
-        'create': {
-            'description': '../../../description',
-            'date_followup': '../../../description',
-        },
-        'update': {
-            'description': '/data/description',
-            'date_followup': '/data/date_followup',
-            'latest_report': '/data/progress_group/progress_update'
-        }
-    },
-}
-
-CAREPLAN_NAME_PATH = '/data/name'
