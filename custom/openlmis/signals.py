30d2007694b61a46add2f2003381324dc995ca2b
Marcin Sienkiewicz <msienkiewicz@soldevelo.com>
2013-11-21 16:39:56 +0100

    Updated 'supply_point_modified' signal conditions.

--- custom/openlmis/signals.py
+++ custom/openlmis/signals.py
@@ -8,6 +8,12 @@ from custom.openlmis.commtrack import sync_supply_point_to_openlmis
 @receiver(supply_point_modified)
 def supply_point_updated(sender, supply_point, created, **kwargs):
     project = Domain.get_by_name(supply_point.domain)
-    if project.commtrack_enabled and project.commtrack_settings.openlmis_enabled:
-        endpoint = OpenLMISEndpoint.from_config(project.commtrack_settings.openlmis_config)
-        sync_supply_point_to_openlmis(supply_point, endpoint)
+    if project.commtrack_enabled and project.commtrack_settings.openlmis_enabled :
+
+        # check if supply_point is of 'chw type'
+        if supply_point.location and supply_point.location.type is 'chw':
+
+            #check if supply_point is linked to an OpenLMIS facility
+            if supply_point.location.parent and supply_point.location.parent.external_id:
+                endpoint = OpenLMISEndpoint.from_config(project.commtrack_settings.openlmis_config)
+                sync_supply_point_to_openlmis(supply_point, endpoint)
