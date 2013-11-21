37df35cbcfa1836c4162cd40b59507a925c1f9fe
Simon Kelly <skelly@dimagi.com>
2013-11-21 10:15:09 +0200

    add careplan models

--- corehq/apps/app_manager/const.py
+++ corehq/apps/app_manager/const.py
@@ -1,2 +1,35 @@
 APP_V1 = '1.0'
 APP_V2 = '2.0'
+
+CAREPLAN_GOAL = 'careplan_goal'
+CAREPLAN_TASK = 'careplan_task'
+CAREPLAN_CASE_NAMES = {
+    CAREPLAN_GOAL: 'Goal',
+    CAREPLAN_TASK: 'Task'
+}
+
+CAREPLAN_DEFAULT_CASE_PROPERTIES = {
+    CAREPLAN_GOAL: {
+        'create': {
+            'description': '/data/description',
+            'date_followup': '/data/date_followup',
+        },
+        'update': {
+            'description': '/data/description_group/description',
+            'date_followup': '/data/date_followup',
+        },
+    },
+    CAREPLAN_TASK: {
+        'create': {
+            'description': '../../../description',
+            'date_followup': '../../../description',
+        },
+        'update': {
+            'description': '/data/description',
+            'date_followup': '/data/date_followup',
+            'latest_report': '/data/progress_group/progress_update'
+        }
+    },
+}
+
+CAREPLAN_NAME_PATH = '/data/name'
