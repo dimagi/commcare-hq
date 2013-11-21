ec8681cc18eb5aa790a2dd322c862e5a3731b21b
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-21 17:46:14 -0500

    initial models for Accounting (BillingAccount, BillingContact, and Currency)

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -3,6 +3,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from corehq.apps.accounting.exceptions import HQAccountingSetupError
 from corehq.apps.accounting.utils import EXCHANGE_RATE_DECIMAL_PLACES
+from corehq.apps.accounting.utils import BillingAccountType
 
 
 class Currency(models.Model):
@@ -20,3 +21,72 @@ class Currency(models.Model):
     def get_default(cls):
         default, _ = Currency.objects.get_or_create(code=settings.DEFAULT_CURRENCY)
         return default
+
+
+class BillingContact(models.Model):
+    """
+    This is the information we use to bill the client. It links to an existing WebUser
+    by username, as WebUser currently lives in Couch.
+    """
+    web_user = models.CharField(
+        max_length=80,
+    )
+    phone_number = models.CharField(
+        max_length=40,
+    )
+    first_line = models.CharField(
+        max_length=40,
+    )
+    second_line = models.CharField(
+        max_length=40,
+        blank=True,
+    )
+    city = models.CharField(
+        max_length=40,
+    )
+    state_province_region = models.CharField(
+        max_length=40,
+        blank=True,
+    )
+    postal_code = models.CharField(
+        max_length=40,
+        blank=True,
+    )
+    country = models.CharField(
+        max_length=40,
+    )
+
+
+class BillingAccount(models.Model):
+    """
+    The key model that links a Subscription to its financial source and methods of payment.
+    """
+    name = models.CharField(
+        max_length=40,
+    )
+    salesforce_account_id = models.CharField(
+        max_length=80,
+        blank=True,
+        help_text="This is the organization name in Salesforce",
+    )
+    created_by = models.CharField(
+        max_length=80,
+    )
+    date_created = models.DateField(
+        auto_now_add=True,
+    )
+    contact = models.ForeignKey(BillingContact)
+    currency = models.ForeignKey(Currency)
+    is_auto_invoiceable = models.BooleanField(
+        default=False,
+    )
+    account_type = models.CharField(
+        max_length=25,
+        default=BillingAccountType.CONTRACT,
+        choices=BillingAccountType.get_choices(),
+    )
+
+    @property
+    def balance(self):
+        # todo compute
+        return 0.0
