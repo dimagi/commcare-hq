01f895539cf99252195a55df7d1db69af3cefc65
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2013-11-22 09:03:00 +0100

    removed skipped conflict

--- corehq/apps/commtrack/signals.py
+++ corehq/apps/commtrack/signals.py
@@ -12,11 +12,7 @@ from corehq.apps.domain.signals import commcare_domain_post_save
 from corehq.apps.locations.models import Location
 from corehq.apps.sms.api import send_sms_to_verified_number
 from dimagi.utils import create_unique_filter
-<<<<<<< HEAD
-from custom.openlmis.commtrack import approved_requisition
-=======
-from custom.openlmis.commtrack import requisition_receipt
->>>>>>> master
+from custom.openlmis.commtrack import requisition_receipt, requisition_approved
 
 
 supply_point_modified = Signal(providing_args=['supply_point', 'created'])
@@ -116,7 +112,7 @@ def raise_events(xform, cases):
         supply_point_modified.send(sender=None, supply_point=sp, created=created)
     requisition_cases = [RequisitionCase.wrap(c._doc) for c in cases if c.type == const.REQUISITION_CASE_TYPE]
     if requisition_cases and requisition_cases[0].requisition_status is RequisitionStatus.APPROVED:
-        approved_requisition.send(sender=None, requisitions=requisition_cases)
+        requisition_approved.send(sender=None, requisitions=requisition_cases)
     if requisition_cases and requisition_cases[0].requisition_status is RequisitionStatus.RECEIVED:
         requisition_receipt.send(sender=None, requisitions=requisition_cases)
 
