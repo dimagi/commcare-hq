22cc62a3b429c33c167de41c4780eba6cad8bc66
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-22 13:55:42 -0500

    whitespace, where you come from?

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -11,11 +11,7 @@ class BillingAccountType(object):
         (CONTRACT, "Created by contract"),
         (USER_CREATED, "Created by user"),
     )
-
-
-
-
-
+    
 
 class Currency(models.Model):
     """
