59d95b56d6adadfbcfb69f11f46828bb50e3a3cf
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-25 11:25:50 -0500

    software product, subscription, and rate models added to accounting

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -11,7 +11,26 @@ class BillingAccountType(object):
         (CONTRACT, "Created by contract"),
         (USER_CREATED, "Created by user"),
     )
-    
+
+
+class FeatureType(object):
+    USER = "USER"
+    SMS = "SMS"
+    API = "API"
+    CHOICES = (
+        (USER, "Users"),
+        (SMS, "SMS"),
+        (API, "API"),
+    )
+
+
+class SoftwarePlanVisibility(object):
+    PUBLIC = "PUBLIC"
+    INTERNAL = "INTERNAL"
+    CHOICES = (
+        (PUBLIC, "Anyone can subscribe"),
+        (INTERNAL, "Dimagi must create subscription"),
+    )
 
 class Currency(models.Model):
     """
@@ -59,3 +78,94 @@ class BillingAccount(models.Model):
     def balance(self):
         # todo compute
         return 0.0
+
+
+class SoftwareProduct(models.Model):
+    """
+    Specifies a product name that can be included in a subscription. e.g. CommTrack Pro, CommCare Community, etc.
+    """
+    name = models.CharField(max_length=40, unique=True)
+
+
+class SoftwareProductRate(models.Model):
+    """
+    Links a SoftwareProduct to a monthly fee.
+    Once created, ProductRates cannot be modified. Instead, a new ProductRate must be created.
+    """
+    product = models.ForeignKey(SoftwareProduct, on_delete=models.PROTECT)
+    monthly_fee = models.FloatField()
+    date_created = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Feature(models.Model):
+    """
+    This is what will link a feature type (USER, API, etc.) to a name (Users Pro, API Standard, etc.) and will be what
+    the FeatureRate references to provide a monthly fee, limit and per-excess fee.
+    """
+    name = models.CharField(max_length=40, unique=True)
+    feature_type = models.CharField(max_length=10, choices=FeatureType.CHOICES)
+
+
+class FeatureRate(models.Model):
+    """
+    Links a feature to a monthly fee, monthly limit, and a per-excess fee for exceeding the monthly limit.
+    Once created, Feature Rates cannot be modified. Instead, a new Feature Rate must be created.
+    """
+    feature = models.ForeignKey(Feature, on_delete=models.PROTECT)
+    monthly_fee = models.FloatField(default=0.0)
+    monthly_limit = models.IntegerField(default=0)
+    per_excess_fee = models.FloatField(default=0.0)
+    date_created = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class SoftwarePlan(models.Model):
+    """
+    Subscriptions are created for Software Plans. Software Plans can have many Software Plan Versions, which
+    link the Software Plan to a set of permissions roles.
+    """
+    name = models.CharField(max_length=80, unique=True)
+    description = models.TextField()
+    visibility = models.CharField(
+        max_length=10,
+        default=SoftwarePlanVisibility.INTERNAL,
+        choices=SoftwarePlanVisibility.CHOICES,
+    )
+
+
+class SoftwarePlanVersion(models.Model):
+    """
+    Links a plan to its rates and provides versioning information.
+    Once a new SoftwarePlanVersion is created, it cannot be modified. Instead, a new SofwarePlanVersion
+    must be created.
+    """
+    plan = models.ForeignKey(SoftwarePlan, on_delete=models.PROTECT)
+    product_rates = models.ManyToManyField(SoftwareProductRate, blank=True)
+    feature_rates = models.ManyToManyField(FeatureRate, blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+
+class Subscriber(models.Model):
+    """
+    The objects that can be subscribed to a Subscription.
+    """
+    domain = models.CharField(max_length=25, null=True, db_index=True)
+    organization = models.CharField(max_length=25, null=True, db_index=True)
+
+
+class Subscription(models.Model):
+    """
+    Links a Subscriber to a SoftwarePlan and BillingAccount, necessary for invoicing.
+    """
+    account = models.ForeignKey(BillingAccount, on_delete=models.PROTECT)
+    plan = models.ForeignKey(SoftwarePlanVersion, on_delete=models.PROTECT)
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
+    # todo: hook in roles = models.ForeignKey(Roles)
+    salesforce_contract_id = models.CharField(blank=True, max_length=80)
+    date_start = models.DateField()
+    date_end = models.DateField(blank=True)
+    date_delay_invoicing = models.DateField(blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)
