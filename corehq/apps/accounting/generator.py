d75ed080d8d4d8583f501ba0b0e62f126d4bfe3b
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-25 11:28:35 -0500

    generators useful for tests in accounting

---
+++ corehq/apps/accounting/generator.py
@@ -0,0 +1,48 @@
+from __future__ import unicode_literals, absolute_import, print_function
+
+from dimagi.utils.data import generator
+from corehq.apps.accounting.models import *
+from corehq.apps.users.models import WebUser
+
+
+def currency_usd(save=True):
+    currency_usd = Currency(
+        code="USD",
+        name="US Dollar",
+    )
+
+    if save:
+        currency_usd.save()
+
+    return currency_usd
+
+
+def arbitrary_web_user(save=True):
+    domain = generator.arbitrary_unique_name().lower()
+    username = generator.arbitrary_unique_name()
+
+    web_user = WebUser.create(domain, username, 'test123')
+
+    if save:
+        web_user.save()
+
+    return web_user
+
+
+def billing_account(web_user_creator, web_user_contact, currency=None, save=True):
+    account_name = generator.arbitrary_unique_name(prefix="BA")[:40]
+
+    currency = currency or Currency.objects.get(code="USD")
+
+    billing_account = BillingAccount(
+        name=account_name,
+        created_by=web_user_creator.username,
+        web_user_contact=web_user_contact.username,
+        currency=currency,
+    )
+
+    if save:
+        billing_account.save()
+
+    return billing_account
+
