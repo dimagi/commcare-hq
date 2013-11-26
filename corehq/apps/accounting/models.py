b9dd76f664f596d12bbbecf8765a3ee310178245
Biyeun Buczyk <biyeun@dimagi.com>
2013-11-26 17:09:46 -0500

    linking subscription to prbac Role

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -4,6 +4,7 @@ from couchdbkit.ext.django.schema import DateTimeProperty, StringProperty
 from django.core.exceptions import ValidationError
 from django.db import models
 
+from django_prbac.models import Role
 from dimagi.utils.couch.database import SafeSaveDocument
 from dimagi.utils.decorators.memoized import memoized
 
@@ -208,7 +209,7 @@ class Subscription(models.Model):
     account = models.ForeignKey(BillingAccount, on_delete=models.PROTECT)
     plan = models.ForeignKey(SoftwarePlanVersion, on_delete=models.PROTECT)
     subscriber = models.ForeignKey(Subscriber, on_delete=models.PROTECT)
-    # todo: hook in roles = models.ForeignKey(Roles)
+    role = models.ForeignKey(Role)
     salesforce_contract_id = models.CharField(blank=True, max_length=80)
     date_start = models.DateField()
     date_end = models.DateField(blank=True)
