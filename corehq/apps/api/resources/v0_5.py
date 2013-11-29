f821f5f0cd5ddb76e8bc745a049427e9725e4718
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2013-11-29 15:34:45 +0100

    Added create and edit APIs for CommCareUser(mobile-worker)

---
+++ corehq/apps/api/resources/v0_5.py
@@ -0,0 +1,63 @@
+from django.core.urlresolvers import reverse
+from tastypie.bundle import Bundle
+from corehq.apps.api.resources import v0_1
+from corehq.apps.sms.util import strip_plus
+from corehq.apps.users.models import CommCareUser
+
+
+class CommCareUserResource(v0_1.CommCareUserResource):
+
+    class Meta(v0_1.CommCareUserResource.Meta):
+        detail_allowed_methods = ['get', 'put', 'delete']
+        list_allowed_methods = ['get', 'post']
+
+    def get_resource_uri(self, bundle_or_obj=None, url_name='api_dispatch_detail'):
+        if isinstance(bundle_or_obj, Bundle):
+            obj = bundle_or_obj.obj
+        elif bundle_or_obj is None:
+            return None
+        else:
+            obj = bundle_or_obj
+
+        return reverse('api_dispatch_detail', kwargs=dict(resource_name=self._meta.resource_name,
+                                                          domain=obj.domain,
+                                                          api_name=self._meta.api_name,
+                                                          pk=obj._id))
+
+    def _update(self, bundle):
+        should_save = False
+        for key, value in bundle.data.items():
+            if key == 'phone_numbers' and getattr(bundle.obj, key, None) != value:
+                bundle.obj.phone_numbers = []
+                for idx, phone_number in enumerate(bundle.data.get('phone_numbers', [])):
+
+                    bundle.obj.add_phone_number(strip_plus(phone_number))
+                    if idx == 0:
+                        bundle.obj.set_default_phone_number(strip_plus(phone_number))
+                    should_save = True
+            if key == 'groups' and getattr(bundle.obj, key, None) != value:
+                bundle.obj.set_groups(bundle.data.get("groups", []))
+                should_save = True
+            elif getattr(bundle.obj, key, None) != value:
+                setattr(bundle.obj, key, value)
+                should_save = True
+        return should_save
+
+    def obj_create(self, bundle, request=None, **kwargs):
+        try:
+            bundle.obj = CommCareUser.create(domain=kwargs['domain'], username=bundle.data['username'],
+                                             password=bundle.data['password'], email=bundle.data.get('email', ''))
+            del bundle.data['password']
+            self._update(bundle)
+            bundle.obj.save()
+        except Exception:
+            bundle.obj.delete()
+        return bundle
+
+    def obj_update(self, bundle, **kwargs):
+        bundle.obj = CommCareUser.get(kwargs['pk'])
+        assert bundle.obj.domain == kwargs['domain']
+        if self._update(bundle):
+            assert bundle.obj.domain == kwargs['domain']
+            bundle.obj.save()
+        return bundle
--- corehq/apps/api/tests.py
+++ corehq/apps/api/tests.py
@@ -8,6 +8,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from tastypie.resources import Resource
 from tastypie import fields
+from corehq.apps.groups.models import Group
 from corehq.pillows.reportxform import ReportXFormPillow
 
 from couchforms.models import XFormInstance
@@ -18,7 +19,7 @@ from corehq.pillows.case import CasePillow
 from corehq.apps.users.models import CommCareUser, WebUser
 from corehq.apps.domain.models import Domain
 from corehq.apps.receiverwrapper.models import FormRepeater, CaseRepeater, ShortFormRepeater
-from corehq.apps.api.resources import v0_1, v0_4
+from corehq.apps.api.resources import v0_1, v0_4, v0_5
 from corehq.apps.api.fields import ToManyDocumentsField, ToOneDocumentField, UseIfRequested, ToManyDictField
 from corehq.apps.api.es import ESQuerySet
 from django.conf import settings
@@ -314,8 +315,9 @@ class TestCommCareUserResource(APIResourceTest):
     """
     Basic sanity checking of v0_1.CommCareUserResource
     """
-    resource = v0_1.CommCareUserResource
-        
+    resource = v0_5.CommCareUserResource
+    api_name = 'v0.5'
+
     def test_get_list(self):
         self.client.login(username=self.username, password=self.password)
 
@@ -342,6 +344,90 @@ class TestCommCareUserResource(APIResourceTest):
 
         api_user = simplejson.loads(response.content)
         self.assertEqual(api_user['id'], backend_id)
+
+    def test_create(self):
+        self.client.login(username=self.username, password=self.password)
+
+        group = Group({"name": "test"})
+        group.save()
+
+        self.assertEqual(0, len(CommCareUser.by_domain(self.domain.name)))
+
+        user_json = {
+            "username": "jdoe",
+            "password": "qwer1234",
+            "first_name": "John",
+            "last_name": "Doe",
+            "email": "jdoe@example.org",
+            "language": "en",
+            "phone_numbers": [
+                "+50253311399",
+                "50253314588"
+            ],
+            "groups": [
+                group._id
+            ],
+            "user_data": {
+                "chw_id": "13/43/DFA"
+            }
+        }
+        response = self.client.post(self.list_endpoint,
+                                    simplejson.dumps(user_json),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        [user_back] = CommCareUser.by_domain(self.domain.name)
+        self.assertEqual(user_back.username, "jdoe")
+        self.assertEqual(user_back.first_name, "John")
+        self.assertEqual(user_back.last_name, "Doe")
+        self.assertEqual(user_back.email, "jdoe@example.org")
+        self.assertEqual(user_back.language, "en")
+        self.assertEqual(user_back.get_group_ids()[0], group._id)
+        self.assertEqual(user_back.user_data["chw_id"], "13/43/DFA")
+        self.assertEqual(user_back.default_phone_number, "+50253311399")
+        user_back.delete()
+        group.delete()
+
+    def test_update(self):
+        self.client.login(username=self.username, password=self.password)
+
+        user = CommCareUser.create(domain=self.domain.name, username="test", password="qwer1234")
+        group = Group({"name": "test"})
+        group.save()
+
+        user_json = {
+            "first_name": "test",
+            "last_name": "last",
+            "email": "tlast@example.org",
+            "language": "pol",
+            "phone_numbers": [
+                "+50253311399",
+                "50253314588"
+            ],
+            "groups": [
+                group._id
+            ],
+            "user_data": {
+                "chw_id": "13/43/DFA"
+            }
+        }
+
+        backend_id = user._id
+        response = self.client.put(self.single_endpoint(backend_id),
+                                   simplejson.dumps(user_json),
+                                   content_type='application/json')
+        self.assertEqual(response.status_code, 204, response.content)
+        self.assertEqual(1, len(CommCareUser.by_domain(self.domain.name)))
+        modified = CommCareUser.get(backend_id)
+        self.assertEqual(modified.username, "test")
+        self.assertEqual(modified.first_name, "test")
+        self.assertEqual(modified.last_name, "last")
+        self.assertEqual(modified.email, "tlast@example.org")
+        self.assertEqual(modified.language, "pol")
+        self.assertEqual(modified.get_group_ids()[0], group._id)
+        self.assertEqual(modified.user_data["chw_id"], "13/43/DFA")
+        self.assertEqual(modified.default_phone_number, "+50253311399")
+        modified.delete()
+        group.delete()
 
 
 class TestWebUserResource(APIResourceTest):
