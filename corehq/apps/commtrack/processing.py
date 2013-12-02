d4a134416c49222fb42e07df5a51837c34d093ba
Drew Roos <mrgriscom@gmail.com>
2013-12-02 10:46:26 +0200

    case blocks

--- corehq/apps/commtrack/processing.py
+++ corehq/apps/commtrack/processing.py
@@ -4,13 +4,18 @@ from corehq.apps.commtrack.models import CommtrackConfig, StockTransaction, Supp
 from corehq.apps.commtrack import const
 import collections
 from dimagi.utils.couch.loosechange import map_reduce
+from corehq.apps.commtrack.xmlutil import XML
 
 from casexml.apps.case.mock import CaseBlock
 from casexml.apps.case.models import CommCareCase
 from casexml.apps.case.xml import V2
 from xml import etree as legacy_etree
+from datetime import datetime, date
+from lxml import etree
 
 logger = logging.getLogger('commtrack.incoming')
+
+COMMTRACK_LEGACY_REPORT_XMLNS = 'http://commtrack.org/legacy/stock_report'
 
 # FIXME this decorator is causing me bizarre import issues
 #@log_exception()
@@ -34,27 +39,26 @@ def process_stock(sender, xform, config=None, **kwargs):
                                               include_docs=True)
     supply_point_product_subcases = dict((sp._id, product_subcases(sp)) for sp in supply_point_cases)
 
+    E = XML(ns=COMMTRACK_LEGACY_REPORT_XMLNS)
+    user_id = 'asdf' ### FIXME
     submit_time = xform['received_on']
+    root = E.commtrack_data()
     post_processed_transactions = list(transactions)
     for (supply_point_id, product_id), txs in grouped_tx.iteritems():
         subcase = supply_point_product_subcases[supply_point_id][product_id]
 
-        print subcase
-
-        """
-            case_block, reconciliations = process_product_transactions(user_id, submit_time, product_case, stock_txs)
-            root.append(case_block)
-            post_processed_transactions.extend(reconciliations)
-        """
+        case_block, reconciliations = process_product_transactions(user_id, submit_time, subcase, txs)
+        root.append(case_block)
+        post_processed_transactions.extend(reconciliations)
 
         #req_txs = requisition_transactions.get(product_id, [])
         #if req_txs and config.requisitions_enabled:
         #    req = RequisitionState.from_transactions(user_id, product_case, req_txs)
         #    case_block = etree.fromstring(req.to_xml())
         #    root.append(case_block)
-    replace_transactions(root, post_processed_transactions)
+    set_transactions(root, post_processed_transactions)  # todo convert to legacy style
 
-
+    print etree.tostring(root, pretty_print=True)
     # argh, need to make a submission here again
 
     submission = etree.tostring(root)
@@ -64,41 +68,22 @@ def process_stock(sender, xform, config=None, **kwargs):
 
 
     return
-    #####
-
-    """
-    x get da stock transactions out of the form
-    x convert to stocktransaction objects
-
-    operate on stocktx thereon
-
-    x group stocktx by (loc, product)
-
-    x fetch subcases
-
-    for each subcase, order tx appropriately
-
-    update and reconcile stock (case blocks as output)
-
-    submit case update form
-    """
-
 
 
 # TODO retire this with move to new data model
 def product_subcases(supply_point):
     """given a supply point, return all the sub-cases for each product stocked at that supply point
-    actually returns a mapping: product doc id => sub-case id
+    actually returns a mapping: product doc id => sub-case
     ACTUALLY returns a dict that will create non-existent product sub-cases on demand
     """
     from helpers import make_supply_point_product
 
     product_subcase_uuids = [ix.referenced_id for ix in supply_point.reverse_indices if ix.identifier == const.PARENT_CASE_REF]
     product_subcases = CommCareCase.view('_all_docs', keys=product_subcase_uuids, include_docs=True)
-    product_subcase_mapping = dict((subcase.dynamic_properties().get('product'), subcase._id) for subcase in product_subcases)
+    product_subcase_mapping = dict((subcase.dynamic_properties().get('product'), subcase) for subcase in product_subcases)
 
     def create_product_subcase(product_uuid):
-        return make_supply_point_product(supply_point, product_uuid)._id
+        return make_supply_point_product(supply_point, product_uuid)
 
     class DefaultDict(dict):
         """similar to collections.defaultdict(), but factory function has access
@@ -138,3 +123,97 @@ def unpack_commtrack(xform, config):
             products = [products]
         for prod_entry in products:
             yield StockTransaction.from_xml(config, global_context, tag, node, prod_entry)
+
+def set_transactions(root, new_tx):
+    for tx in new_tx:
+        root.append(tx.to_xml())
+
+def process_product_transactions(user_id, timestamp, case, txs):
+    """process all the transactions from a stock report for an individual
+    product. we have to apply them in bulk because each one may update
+    the case state that the next one works off of. therefore we have to
+    keep track of the updated case state ourselves
+    """
+    current_state = StockState(case, timestamp)
+    reconciliations = []
+
+    i = [0] # annoying python 2.x scope issue
+    def set_order(tx):
+        tx.processing_order = i[0]
+        i[0] += 1
+
+    for tx in txs:
+        recon = current_state.update(tx.action, tx.quantity)
+        if recon:
+            set_order(recon)
+            reconciliations.append(recon)
+        set_order(tx)
+    return current_state.to_case_block(user_id=user_id), reconciliations
+
+class StockState(object):
+    def __init__(self, case, reported_on):
+        self.case = case
+        self.last_reported = reported_on
+        props = case.dynamic_properties()
+        self.current_stock = int(props.get('current_stock') or 0)  # int
+        self.stocked_out_since = props.get('stocked_out_since')  # date
+
+    def update(self, action_type, value):
+        """given the current stock state for a product at a location, update
+        with the incoming datapoint
+        
+        fancy business logic to reconcile stock reports lives HERE
+        """
+        reconciliation_transaction = None
+        def mk_reconciliation(diff):
+            return StockTransaction(
+                product_id=self.case.product,
+                case_id=self.case._id, # note: wrong case type
+                action=const.StockActions.RECEIPTS if diff > 0 else const.StockActions.CONSUMPTION,
+                quantity=abs(diff),
+                inferred=True,
+            )
+
+        if action_type == const.StockActions.STOCKOUT:
+            if self.current_stock > 0:
+                reconciliation_transaction = mk_reconciliation(-self.current_stock)
+
+            self.current_stock = 0
+            self.stocked_out_since = date.today()
+
+        else:
+            if action_type == const.StockActions.STOCKONHAND:
+                if self.current_stock != value:
+                    reconciliation_transaction = mk_reconciliation(value - self.current_stock)
+                self.current_stock = value
+            elif action_type == const.StockActions.RECEIPTS:
+                self.current_stock += value
+            elif action_type == const.StockActions.CONSUMPTION:
+                self.current_stock -= value
+
+            # data normalization
+            if self.current_stock > 0:
+                self.stocked_out_since = None
+            else:
+                self.current_stock = 0 # handle if negative
+                if not self.stocked_out_since: # handle if stocked out date already set
+                    self.stocked_out_since = date.today()
+
+        return reconciliation_transaction
+
+    def to_case_block(self, user_id=None):
+        def convert_prop(val):
+            return str(val) if val is not None else ''
+
+        props = ['current_stock', 'stocked_out_since', 'last_reported']
+
+        case_update = CaseBlock(
+            version=V2,
+            case_id=self.case._id,
+            user_id=user_id or 'FIXME',
+            update=dict((k, convert_prop(getattr(self, k))) for k in props)
+        ).as_xml()
+        # convert xml.etree to lxml
+        case_update = etree.fromstring(legacy_etree.ElementTree.tostring(case_update))
+
+        return case_update
