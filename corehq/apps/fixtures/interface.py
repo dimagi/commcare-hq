20c3f3fe19e2bc32e2568c2c82c553c3c12c4b1a
Sravan Reddy <sravfeyn@gmail.com>
2013-12-10 17:08:26 +0530

    minor fix in optional param name

---
+++ corehq/apps/fixtures/interface.py
@@ -0,0 +1,8 @@
+from corehq.apps.reports.generic import GenericReportView
+from corehq.apps.data_interfaces.dispatcher import FixtureIterfaceDispatcher
+
+class FixtureInterface(GenericReportView):
+	pass
+
+
+class FixtureEditInterface(FixtureInterface):
