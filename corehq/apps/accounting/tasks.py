2d09200eb05a450e4555049255d5a5c3de31eb78
Biyeun Buczyk <biyeun@dimagi.com>
2013-12-13 16:07:57 -0500

    test subscriptions

---
+++ corehq/apps/accounting/tasks.py
@@ -0,0 +1,36 @@
+from celery.schedules import crontab
+from celery.task import task, periodic_task
+from celery.utils.log import get_task_logger
+import datetime
+from corehq import Domain
+from corehq.apps.accounting import utils
+from corehq.apps.accounting.invoicing import InvoiceFactory
+
+from corehq.apps.accounting.models import Subscription
+
+logging = get_task_logger(__name__)
+
+
+@periodic_task(run_every=crontab(minute=0, hour=0))
+def activate_subscriptions(based_on_date=None):
+    """
+    Activates all subscriptions starting today (or, for testing, based on the date specified)
+    """
+    starting_date = based_on_date or datetime.date.today()
+    starting_subscriptions = Subscription.objects.filter(date_start=starting_date)
+    for subscription in starting_subscriptions:
+        subscription.is_active = True
+        subscription.save()
+
+
+@periodic_task(run_every=crontab(minute=0, hour=0))
+def deactivate_subscriptions(based_on_date=None):
+    """
+    Deactivates all subscriptions ending yesterday (or, for testing, based on the date specified)
+    """
+    ending_date = based_on_date or datetime.date.today()
+    ending_subscriptions = Subscription.objects.filter(date_end=ending_date)
+    for subscription in ending_subscriptions:
+        subscription.is_active = False
+        subscription.save()
+
