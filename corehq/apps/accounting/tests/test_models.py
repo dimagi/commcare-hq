4b4d5eaadda4b1de305893a46a6f5bb4b31bd50c
Biyeun Buczyk <biyeun@dimagi.com>
2013-12-13 16:06:43 -0500

    test billing account updates from generator

--- corehq/apps/accounting/tests/test_models.py
+++ corehq/apps/accounting/tests/test_models.py
@@ -2,23 +2,25 @@ from django.db import models
 from django.test import TestCase
 
 from corehq.apps.accounting import generator
+from corehq.apps.accounting.models import BillingAccount, Currency, Subscription
 
 
 class TestBillingAccount(TestCase):
 
     def setUp(self):
         self.billing_contact = generator.arbitrary_web_user()
-        self.dimagi_user = generator.arbitrary_web_user()
-        self.currency = generator.currency_usd()
+        self.dimagi_user = generator.arbitrary_web_user(is_dimagi=True)
+        self.currency = generator.init_default_currency()
+        self.billing_account = generator.billing_account(self.dimagi_user, self.billing_contact)
 
-    # todo: validation testing
+    def test_creation(self):
+        self.assertIsNotNone(self.billing_account)
 
     def test_deletions(self):
-        billing_account = generator.billing_account(self.dimagi_user, self.billing_contact)
         self.assertRaises(models.ProtectedError, self.currency.delete)
-        billing_account.delete()
 
     def tearDown(self):
         self.billing_contact.delete()
         self.dimagi_user.delete()
-        self.currency.delete()
+        BillingAccount.objects.all().delete()
+        Currency.objects.all().delete()
