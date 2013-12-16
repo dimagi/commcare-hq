42e7af70b7a91aea93812ae2c14d0bee6b731816
Nick Pellegrino <npellegrino@dimagi.com>
2013-12-16 18:06:45 -0500

    skeleton of usage fee tests

---
+++ corehq/apps/smsbillables/tests/test_usage_fees.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+
+class TestUsageFee(TestCase):
+    def setUp(self):
+        pass
+
+    def test_only_direction(self):
+        raise NotImplementedError
+
+    def test_domain_and_direction(self):
+        raise NotImplementedError
+
+    def test_log_no_usage_fee(self):
+        raise NotImplementedError
+
+    def tearDown(self):
+        pass
