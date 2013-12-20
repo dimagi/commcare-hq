cfb928ff0e0af26caafd5d70fb9002f489566bcd
Cory Zue <czue@dimagi.com>
2013-12-20 15:11:47 -0500

    remove default date, change report field, add commented out fields

--- casexml/apps/stock/models.py
+++ casexml/apps/stock/models.py
@@ -4,11 +4,17 @@ from django.db import models
 
 class StockReport(models.Model):
     form_id = models.CharField(max_length=100, db_index=True)
-    date = models.DateTimeField(default=datetime.utcnow, db_index=True)
+    date = models.DateTimeField(db_index=True)
+    # todo: there are properties like these that could be really useful for queries
+    # and reports - should decide which ones we want to add if any.
+    # they are all redundant via form_id linkage
+
+    # server_date = models.DateTimeField(default=datetime.utcnow, db_index=True)
+    # user_id = models.CharField(max_length=100, db_index=True)
 
 
 class StockTransaction(models.Model):
-    report_id = models.ForeignKey(StockReport)
+    report = models.ForeignKey(StockReport)
     # for now, a supply point or requisition case
     case_id = models.CharField(max_length=100, db_index=True)
     product_id = models.CharField(max_length=100, db_index=True)
