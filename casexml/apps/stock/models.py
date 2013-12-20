c33c4721e2eab72bb678eb969d56acbeffc03396
Cory Zue <czue@dimagi.com>
2013-12-20 16:18:58 -0500

    add additional fields and __repr__ functions

--- casexml/apps/stock/models.py
+++ casexml/apps/stock/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class StockReport(models.Model):
     form_id = models.CharField(max_length=100, db_index=True)
     date = models.DateTimeField(db_index=True)
+    type = models.CharField(max_length=10)  # currently "balance" or "transfer"
+
     # todo: there are properties like these that could be really useful for queries
     # and reports - should decide which ones we want to add if any.
     # they are all redundant via form_id linkage
@@ -12,11 +14,26 @@ class StockReport(models.Model):
     # server_date = models.DateTimeField(default=datetime.utcnow, db_index=True)
     # user_id = models.CharField(max_length=100, db_index=True)
 
+    def __repr__(self):
+        return '{type} on {date} ({form})'.format(type=self.type, date=self.date, form=self.form_id)
 
 class StockTransaction(models.Model):
     report = models.ForeignKey(StockReport)
     # for now, a supply point or requisition case
     case_id = models.CharField(max_length=100, db_index=True)
     product_id = models.CharField(max_length=100, db_index=True)
-    quantity = models.FloatField()
 
+    # often one of these two will be derived based on the other one
+    quantity = models.FloatField(null=True)
+    stock_on_hand = models.FloatField()
+
+    def __repr__(self):
+        return 'stock transfer of {quantity} to {soh} (case: {case}, product: {product})'.format(
+            quantity=self.quantity, soh=self.stock_on_hand, case=self.case_id, product=self.product_id,
+        )
+
+    def get_previous_transaction(self):
+        siblings = StockTransaction.objects.filter(case_id=self.case_id, product_id=self.product_id)
+        if siblings.count():
+            return siblings.order_by('-report__date')[0]
+        return None
