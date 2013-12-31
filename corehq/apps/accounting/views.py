decfdd3037e2685ad9c8b4f1353f01812c225dbf
Nick Pellegrino <npellegrino@dimagi.com>
2013-12-31 16:23:07 -0500

    add table of BillingAccounts

--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -1,9 +1,12 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from corehq.apps.accounting.models import BillingAccount, Currency
 from corehq.apps.domain.decorators import require_superuser
 
 
 @require_superuser
 def view_billing_accounts(request):
-    return render(request, "view_billing_accounts.html", {})
+    return render(request, "view_billing_accounts.html",
+                  {'account_list': BillingAccount.objects.filter(),
+                   })
