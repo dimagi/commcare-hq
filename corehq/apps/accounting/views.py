96192544e579e230986613dc19a21cba36a480ac
Nick Pellegrino <npellegrino@dimagi.com>
2013-12-31 13:43:36 -0500

    view billing accounts page, hello world

--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from corehq.apps.domain.decorators import require_superuser
 
 
 @require_superuser
 def view_billing_accounts(request):
-    return HttpResponse()
+    return render(request, "list_backends.html", {})
