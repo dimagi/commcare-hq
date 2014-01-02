e37406b622686fb3dba9087e9310185842490949
yedi <yedispaghetti@gmail.com>
2014-01-02 12:19:23 -0500

    fix bug with legacy scheduled reports

--- corehq/apps/reports/views.py
+++ corehq/apps/reports/views.py
@@ -660,8 +660,8 @@ def _render_report_configs(request, configs, domain, owner_id, couch_user, email
         "owner_name": couch_user.full_name or couch_user.get_email(),
         "email": email,
         "notes": notes,
-        "startdate": date_range["startdate"],
-        "enddate": date_range["enddate"],
+        "startdate": date_range["startdate"] if date_range else "",
+        "enddate": date_range["enddate"] if date_range else "",
     }), excel_attachments
 
 @login_and_domain_required
