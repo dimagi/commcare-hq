63d13eac9bd2f5763eaddfbcd2587d8d50c68f62
Cory Zue <czue@dimagi.com>
2014-01-03 17:35:34 -0500

    allow specifying a new domain for forms and cases

--- corehq/apps/domainsync/management/commands/copy_case.py
+++ corehq/apps/domainsync/management/commands/copy_case.py
@@ -6,17 +6,20 @@ from couchforms.models import XFormInstance
 
 class Command(LabelCommand):
     help = "Copy a case and all related forms"
-    args = '<sourcedb> <case_id>'
+    args = '<sourcedb> <case_id> <domain>'
 
     def handle(self, *args, **options):
-        if len(args) != 2:
+        if len(args) < 2:
             raise CommandError('Usage is copy_case, %s' % self.args)
 
         sourcedb = Database(args[0])
         case_id = args[1]
+        domain = args[2] if len(args) > 2 else None
 
         print 'getting case'
         case = CommCareCase.wrap(sourcedb.get(case_id))
+        if domain is not None:
+            case.domain = domain
         case.save(force_update=True)
 
         print 'copying %s xforms' % len(case.xform_ids)
@@ -32,4 +35,7 @@ class Command(LabelCommand):
             wrapper=form_wrapper,
         ).all()
         for form in xforms:
+            if domain is not None:
+                form.domain = domain
             form.save(force_update=True)
+            print 'saved %s' % form._id
