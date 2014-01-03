4c0917401c6fb7030ccc6768d9fee5b8462862ce
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-03 17:12:45 -0500

    link to Manage Billing Account page

--- corehq/apps/accounting/urls.py
+++ corehq/apps/accounting/urls.py
@@ -6,6 +6,7 @@ from corehq.apps.accounting.dispatcher import SubscriptionAdminInterfaceDispatch
 urlpatterns = patterns('corehq.apps.accounting.views',
     url(r'^view_billing_accounts/$', 'view_billing_accounts', name='view_billing_accounts'),
     url(r'^accounting_default/$', 'accounting_default', name='accounting_default'),
+    url(r'^accounts/(\d+)/', 'manage_billing_account', name='manage_billing_account'),
     url(AccountingAdminInterfaceDispatcher.pattern(), AccountingAdminInterfaceDispatcher.as_view(),
         name=AccountingAdminInterfaceDispatcher.name()),
     url(SubscriptionAdminInterfaceDispatcher.pattern(), SubscriptionAdminInterfaceDispatcher.as_view(),
--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -11,6 +11,16 @@ def view_billing_accounts(request):
                   {'account_list': BillingAccount.objects.filter(),
                    })
 
+
 @require_superuser
 def accounting_default(request):
     return HttpResponseRedirect(AccountingInterface.get_url())
+
+
+@require_superuser
+def manage_billing_account(request, account_id):
+    parent_link = '<a href="%s">%s<a>' % (AccountingInterface.get_url(), AccountingInterface.name)
+    return render(request,
+                  'manage_account.html',
+                  dict(account_name=BillingAccount.objects.get(id=account_id).name,
+                       parent_link=parent_link))
