7796e13f35f38cd6ead4237dcf14813af6dbe120
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-03 15:52:00 -0500

    created View Subscriptions page

--- corehq/apps/accounting/interface.py
+++ corehq/apps/accounting/interface.py
@@ -1,5 +1,5 @@
 from django.utils.safestring import mark_safe
-from corehq.apps.accounting.dispatcher import AccountingAdminInterfaceDispatcher
+from corehq.apps.accounting.dispatcher import AccountingAdminInterfaceDispatcher, SubscriptionAdminInterfaceDispatcher
 from corehq.apps.accounting.models import BillingAccount, Subscription
 from corehq.apps.announcements.forms import HQAnnouncementForm
 from corehq.apps.announcements.models import HQAnnouncement
@@ -63,3 +63,61 @@ class AccountingInterface(BaseCRUDAdminInterface):
     form_class = HQAnnouncementForm
 
     crud_item_type = "Billing Account"
+
+
+class SubscriptionInterface(BaseCRUDAdminInterface):
+    section_name = "Accounting"
+    base_template = 'reports/base_template.html'
+    dispatcher = SubscriptionAdminInterfaceDispatcher
+
+    crud_form_update_url = "/accounting/form/"
+
+    def validate_document_class(self):
+        return True
+        #if self.document_class is None or not issubclass(self.document_class, HQAnnouncement):
+        #    raise NotImplementedError("document_class must be an HQAnnouncement")
+
+    """
+    @property
+    def default_report_url(self):
+        return reverse("default_announcement_admin")
+    """
+
+    @property
+    def headers(self):
+        return DataTablesHeader(
+            DataTablesColumn("Client/Account Name"),
+            DataTablesColumn("Billing Contact Name (Email)"),
+            DataTablesColumn("Plan Credit"),
+            DataTablesColumn("SMS Credit"),
+            DataTablesColumn("Users Credit"),
+            DataTablesColumn("Account Balance"),
+            DataTablesColumn("Active Subscriptions"),
+            DataTablesColumn("Edit"),
+        )
+
+    @property
+    def rows(self):
+        rows = []
+        for account in BillingAccount.objects.all():
+            rows.append([account.name,
+                         account.web_user_contact,
+                         3,
+                         4,
+                         5,
+                         account.balance,
+                         Subscription.objects.filter(account=account,
+                                                     is_active=True).count(),
+                         mark_safe('<a href="./%d" class="btn"></i>Edit</a>' % account.id)])
+        return rows
+
+    #######
+
+    name = "View Subscriptions"
+    description = "description of view subscriptions"
+    slug = "subscriptions"
+
+    document_class = HQAnnouncement
+    form_class = HQAnnouncementForm
+
+    crud_item_type = "Subscription"
