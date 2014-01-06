84db75f037e2ba90b51167746f7220a4aff0ddbb
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-06 10:08:43 -0500

    list currency codes in choice field

--- corehq/apps/accounting/forms.py
+++ corehq/apps/accounting/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from corehq.apps.accounting.models import Currency
 
 
 class BillingAccountForm(forms.Form):
@@ -6,3 +7,8 @@ class BillingAccountForm(forms.Form):
     salesforce_id = forms.CharField(label="Salesforce ID")
     currency = forms.ChoiceField(label="Currency")
     autosend_invoices = forms.BooleanField(label="Send invoices automatically")
+
+    def __init__(self, *args, **kwargs):
+        super(BillingAccountForm, self).__init__(*args, **kwargs)
+        self.fields['currency'].choices =\
+            [(cur.code, cur.code) for cur in Currency.objects.order_by('code')]
