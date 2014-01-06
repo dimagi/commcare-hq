6fa1997ca61caccecb4f6c0f325831e9a376592c
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-06 15:21:59 -0500

    Adding Edit Subscription form

--- corehq/apps/accounting/forms.py
+++ corehq/apps/accounting/forms.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import ButtonHolder, Div, Fieldset, HTML, Layout, Submit
+from crispy_forms.layout import ButtonHolder, Fieldset, Layout, Submit
 from django import forms
+from django.forms.extras.widgets import SelectDateWidget
+
 from corehq.apps.accounting.models import Currency
 
 
@@ -30,7 +33,36 @@ class BillingAccountForm(forms.Form):
                     'autosend_invoices',
                 ),
                 ButtonHolder(
-                    Submit('submit', 'Create Mobile Worker')
+                    Submit('submit', 'Create Mobile Worker') #TODO fix
+                )
+            )
+        )
+
+
+class SubscriptionForm(forms.Form):
+    start_date = forms.DateField(label="Start Date", widget=SelectDateWidget())
+    end_date = forms.DateField(label="End Date", widget=SelectDateWidget())
+    delay_invoice_until = forms.DateField(label="Delay Invoice Until", widget=SelectDateWidget())
+    #line_items = forms.SelectMultiple(choices=(('a', 'b')))
+    note = forms.CharField(required=False)
+
+    def __init__(self, subscription, *args, **kwargs):
+        super(SubscriptionForm, self).__init__(*args, **kwargs)
+        self.fields['start_date'].initial = subscription.date_start
+        self.fields['end_date'].initial = subscription.date_end
+        self.fields['delay_invoice_until'].initial = subscription.date_delay_invoicing
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            FormActions(
+                Fieldset(
+                'Edit Subscription',
+                    'start_date',
+                    'end_date',
+                    'delay_invoice_until',
+                    'note',
+                ),
+                ButtonHolder(
+                    Submit('submit', 'Submit')
                 )
             )
         )
