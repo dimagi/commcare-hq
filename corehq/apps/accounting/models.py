3d58416b2c13a92d849004882098e422a89889ef
Biyeun Buczyk <biyeun@dimagi.com>
2014-01-07 17:27:53 -0500

    added str statements for Software Plans and related rate objects

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -147,6 +147,9 @@ class SoftwareProduct(models.Model):
     name = models.CharField(max_length=40, unique=True)
     product_type = models.CharField(max_length=25, db_index=True, choices=SoftwareProductType.CHOICES)
 
+    def __str__(self):
+        return "Software Product '%s' of type '%s'" % (self.name, self.product_type)
+
 
 class SoftwareProductRate(models.Model):
     """
@@ -175,6 +178,9 @@ class Feature(models.Model):
     name = models.CharField(max_length=40, unique=True)
     feature_type = models.CharField(max_length=10, db_index=True, choices=FeatureType.CHOICES)
 
+    def __str__(self):
+        return "Feature '%s' of type '%s'" % (self.name, self.feature_type)
+
 
 class FeatureRate(models.Model):
     """
@@ -187,6 +193,11 @@ class FeatureRate(models.Model):
     per_excess_fee = models.DecimalField(default=Decimal('0.0'), max_digits=10, decimal_places=2)
     date_created = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return 'Feature Rate: $%s /month, $%s /excess, limit: %d' % (
+            self.monthly_fee, self.per_excess_fee, self.monthly_limit
+        )
 
     @classmethod
     def new_rate(cls, feature_name, feature_type,
@@ -248,6 +259,9 @@ class SoftwarePlanVersion(models.Model):
     date_created = models.DateField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     role = models.ForeignKey(Role)
+
+    def __str__(self):
+        return "Software Plan Version For Plan '%s' with Role '%s'" % (self.plan.name, self.role.slug)
 
 
 class Subscriber(models.Model):
