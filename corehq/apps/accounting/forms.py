5c22b49d2c576c25aaeaff901da84c53dd514483
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-08 17:19:36 -0500

    Update Account form

--- corehq/apps/accounting/forms.py
+++ corehq/apps/accounting/forms.py
@@ -10,34 +10,27 @@ from corehq.apps.users.models import WebUser
 
 
 class BillingAccountForm(forms.Form):
-    client_name = forms.CharField(label="Client Name")
+    name = forms.CharField(label="Name")
     salesforce_account_id = forms.CharField(label="Salesforce ID")
     currency = forms.ChoiceField(label="Currency")
-    autosend_invoices = forms.BooleanField(label="Send invoices automatically")
-    web_user_contact = forms.ChoiceField(label="Billing Contact", required=False)
 
     def __init__(self, account, *args, **kwargs):
         if account is not None:
-            kwargs['initial'] = {'client_name': account.name,
+            kwargs['initial'] = {'name': account.name,
                                  'salesforce_account_id': account.salesforce_account_id,
                                  'currency': account.currency.code,
-                                 'web_user_contact': account.web_user_contact,
                                  }
         super(BillingAccountForm, self).__init__(*args, **kwargs)
         self.fields['currency'].choices =\
             [(cur.code, cur.code) for cur in Currency.objects.order_by('code')]
-        self.fields['web_user_contact'].choices =\
-            [('', '')] + [(web_user.username, web_user.username) for web_user in WebUser.all()]
         self.helper = FormHelper()
         self.helper.layout = Layout(
             FormActions(
                 Fieldset(
-                '%s Billing Account' % ('Manage' if account is not None else 'New'),
-                    'client_name',
+                'Account',
+                    'name',
                     'salesforce_account_id',
                     'currency',
-                    'autosend_invoices',
-                    'web_user_contact',
                 ),
                 ButtonHolder(
                     Submit('submit', 'Update Account' if account is not None else 'Add New Account')
