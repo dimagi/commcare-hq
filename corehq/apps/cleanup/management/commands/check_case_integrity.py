5e7f7d8034c7feeb88f8878739f8c17249aa2963
yedi <yedispaghetti@gmail.com>
2014-01-08 22:06:52 -0500

    write the data to a csv file

--- corehq/apps/cleanup/management/commands/check_case_integrity.py
+++ corehq/apps/cleanup/management/commands/check_case_integrity.py
@@ -5,6 +5,7 @@ from casexml.apps.case.cleanup import rebuild_case
 from casexml.apps.case.models import CommCareCase
 from corehq.elastic import stream_es_query, ES_URLS
 import dateutil.parser as dparser
+import csv
 
 
 def forms_with_cases(domain=None, since=None):
@@ -19,10 +20,11 @@ def forms_with_cases(domain=None, since=None):
         q["filter"]["and"][0]["bool"]["must"] = {
             "range": {
                 "received_on": {"from": since.strftime("%Y-%m-%d")}}}
-    return stream_es_query(params=params, q=q, es_url=ES_URLS["forms"], fields=["__retrieved_case_ids"])
+    return stream_es_query(params=params, q=q, es_url=ES_URLS["forms"], fields=["__retrieved_case_ids", "domain"])
 
-def check_case_for_form_updates(form_id, case_id):
-    print "checking case (%s) for form (%s)" % (case_id, form_id)
+def check_case_for_form_updates(form_id, case_id, verbose=False):
+    if verbose:
+        print "checking case (%s) for form (%s)" % (case_id, form_id)
     ret, error, case = False, "", None
     try:
         case = CommCareCase.get(case_id)
@@ -31,7 +33,7 @@ def check_case_for_form_updates(form_id, case_id):
             error = "action_missing"
     except ResourceNotFound:
         error = "nonexistent_case"
-    return { "valid": ret, "error": error, "case": case}
+    return {"valid": ret, "error": error, "case": case}
 
 
 class Command(BaseCommand):
@@ -41,28 +43,42 @@ class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option('-s', '--since',
             help="Begin check at this date."),
+        make_option('-f', '--filename',
+            help="Begin check at this date."),
         make_option('-r', '--rebuild', action="store_true",
             help="Rebuild cases that were found to be corrupt"),
+        make_option('--verbose', action="store_true",
+            help="Verbose"),
         )
 
     def handle(self, *args, **options):
         domain = args[0] if len(args) == 1 else None
         since = dparser.parse(options["since"], fuzzy=True) if options.get("since") else None
-        rebuild = options.get("rebuild")
+        filename = options.get("filename") or ("case_integrity" + "_%s" % domain if domain else "")
+        if not filename.endswith(".csv"):
+            filename = "%s.csv" % filename
+        rebuild, verbose = options.get("rebuild"), options.get("verbose")
 
-        for form in forms_with_cases(domain, since):
-            form_id, case_ids = form["_id"], form["fields"]["__retrieved_case_ids"]
-            case_ids = [case_ids] if isinstance(case_ids, basestring) else case_ids
-            for case_id in case_ids:
-                validation = check_case_for_form_updates(form_id, case_id)
-                if not validation["valid"]:
-                    if validation["error"] == "nonexistent_case":
-                        print "Case (%s) from form (%s) does not exist" % (case_id, form_id)
-                    elif validation["error"] == "action_missing":
-                        print "Case (%s) missing action for form (%s)" % (case_id, form_id)
-                    if rebuild:
-                        print "rebuilding case (%s) from scratch" % case_id
-                        try:
-                            rebuild_case(validation["case"] or case_id)
-                        except Exception as e:
-                            print "Case Rebuild Failure: %s" % e
+        with open(filename, 'wb+') as csvfile:
+            csv_writer = csv.writer(csvfile, delimiter=' ',
+                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            csv_writer.writerow(['Domain', 'Case ID', 'Form ID', 'Error'])
+
+            for form in forms_with_cases(domain, since):
+                form_id, case_ids, f_domain = form["_id"], form["fields"]["__retrieved_case_ids"], form["fields"]["domain"]
+                case_ids = [case_ids] if isinstance(case_ids, basestring) else case_ids
+                for case_id in case_ids:
+                    validation = check_case_for_form_updates(form_id, case_id, verbose)
+                    if not validation["valid"]:
+                        csv_writer.writerow([f_domain, case_id, form_id, validation["error"]])
+                        if verbose and validation["error"] == "nonexistent_case":
+                            print "Case (%s) from form (%s) does not exist" % (case_id, form_id)
+                        elif verbose and validation["error"] == "action_missing":
+                            print "Case (%s) missing action for form (%s)" % (case_id, form_id)
+                        if rebuild:
+                            if verbose:
+                                print "rebuilding case (%s) from scratch" % case_id
+                            try:
+                                rebuild_case(validation["case"] or case_id)
+                            except Exception as e:
+                                print "Case Rebuild Failure: %s" % e
