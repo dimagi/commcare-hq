2ae7cdde0acce5f2e3a8eb5fc9ba395edd4f20c2
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-10 11:26:41 -0500

    move account subscription management to tab in Manage Billing Account

--- corehq/apps/accounting/urls.py
+++ corehq/apps/accounting/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 from corehq import AccountingAdminInterfaceDispatcher
 from corehq.apps.accounting.dispatcher import SubscriptionAdminInterfaceDispatcher
-from corehq.apps.accounting.views import ManageBillingAccountView, EditSubscriptionView, NewBillingAccountView, ManageAccountSubscriptions, NewSubscriptionView
+from corehq.apps.accounting.views import *
 
 
 urlpatterns = patterns('corehq.apps.accounting.views',
@@ -9,8 +9,6 @@ urlpatterns = patterns('corehq.apps.accounting.views',
     url(r'^accounting_default/$', 'accounting_default', name='accounting_default'),
     url(r'^accounts/(\d+)/$', ManageBillingAccountView.as_view(), name=ManageBillingAccountView.name),
     url(r'^accounts/new/', NewBillingAccountView.as_view(), name=NewBillingAccountView.name),
-    url(r'^accounts/(\d+)/manage_subscriptions/', ManageAccountSubscriptions.as_view(),
-        name=ManageAccountSubscriptions.name),
     url(r'^subscriptions/(\d+)/', EditSubscriptionView.as_view(), name=EditSubscriptionView.name),
     url(r'^subscriptions/new/(\d+)/', NewSubscriptionView.as_view(), name=NewSubscriptionView.name),
     url(AccountingAdminInterfaceDispatcher.pattern(), AccountingAdminInterfaceDispatcher.as_view(),
