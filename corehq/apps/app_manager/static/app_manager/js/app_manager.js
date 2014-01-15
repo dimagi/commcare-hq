1ae9c3c05498fbd75dc9c7f9828c396bc1ff3b39
Simon <simongdkelly@gmail.com>
2014-01-15 17:41:34 +0200

    CommTrack module view

--- corehq/apps/app_manager/static/app_manager/js/app_manager.js
+++ corehq/apps/app_manager/static/app_manager/js/app_manager.js
@@ -77,8 +77,9 @@
 	                                $(document).trigger('correction', [key, data.corrections[key]]);
 	                            }
 	                        }
-                            if (data.hasOwnProperty('case_list-show')){
-                                COMMCAREHQ.app_manager.module_view.requires_case_details(data['case_list-show'])
+                            if (data.hasOwnProperty('case_list-show')
+                                && COMMCAREHQ.app_manager.hasOwnProperty('module_view')){
+                                COMMCAREHQ.app_manager.module_view.requires_case_details(data['case_list-show']);
                             }
 	                    }
 	                }).ui.appendTo($buttonHolder);
