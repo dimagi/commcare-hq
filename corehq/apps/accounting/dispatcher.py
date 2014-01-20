1ccb7f52371d948f5ae5bc1213fb2a4c7569aa76
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-20 15:38:00 -0500

    refactoring

--- corehq/apps/accounting/dispatcher.py
+++ corehq/apps/accounting/dispatcher.py
@@ -1,5 +1,6 @@
 from corehq.apps.crud.dispatcher import BaseCRUDAdminInterfaceDispatcher
 
+
 class AccountingAdminInterfaceDispatcher(BaseCRUDAdminInterfaceDispatcher):
     prefix = 'accounting_admin_interface'
     map_name = "ACCOUNTING_ADMIN_INTERFACES"
