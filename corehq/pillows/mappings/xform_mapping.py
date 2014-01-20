e73f4838e57c26b1ed5a137d8facb2db9471cfe3
yedi <yedispaghetti@gmail.com>
2014-01-19 20:17:22 -0500

    fix bug with docs with bad dates not being indexed

--- corehq/pillows/mappings/xform_mapping.py
+++ corehq/pillows/mappings/xform_mapping.py
@@ -1,6 +1,6 @@
 from corehq.pillows.core import DATE_FORMATS_STRING, DATE_FORMATS_ARR
 
-XFORM_INDEX="xforms_9ec79af5c2225e08012ecc5b2a992a4f"
+XFORM_INDEX="xforms_9ec79af5c678ye12p129dd5b2a992a4f"
 
 
 XFORM_MAPPING = {
