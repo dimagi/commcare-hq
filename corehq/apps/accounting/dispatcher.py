9226f6c24ac3bdb4b342d7ee237d2e5046e9876a
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-21 11:48:05 -0500

    show account datespan filter

--- corehq/apps/accounting/dispatcher.py
+++ corehq/apps/accounting/dispatcher.py
@@ -1,11 +1,20 @@
 from corehq.apps.crud.dispatcher import BaseCRUDAdminInterfaceDispatcher
+from corehq.apps.reports.dispatcher import datespan_default
 
 
 class AccountingAdminInterfaceDispatcher(BaseCRUDAdminInterfaceDispatcher):
     prefix = 'accounting_admin_interface'
     map_name = "ACCOUNTING_ADMIN_INTERFACES"
 
+    @datespan_default
+    def dispatch(self, request, *args, **kwargs):
+        return super(AccountingAdminInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
+
 
 class SubscriptionAdminInterfaceDispatcher(BaseCRUDAdminInterfaceDispatcher):
     prefix = 'subscription_admin_interface'
     map_name = "SUBSCRIPTION_ADMIN_INTERFACES"
+
+    @datespan_default
+    def dispatch(self, request, *args, **kwargs):
+        return super(SubscriptionAdminInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
