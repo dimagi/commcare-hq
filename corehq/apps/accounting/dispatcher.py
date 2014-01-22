18a23d0230fc5df2327d1933a299bad4d144b745
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-22 15:34:48 -0500

    remove datespan_default tags

--- corehq/apps/accounting/dispatcher.py
+++ corehq/apps/accounting/dispatcher.py
@@ -6,15 +6,7 @@ class AccountingAdminInterfaceDispatcher(BaseCRUDAdminInterfaceDispatcher):
     prefix = 'accounting_admin_interface'
     map_name = "ACCOUNTING_ADMIN_INTERFACES"
 
-    @datespan_default
-    def dispatch(self, request, *args, **kwargs):
-        return super(AccountingAdminInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
-
 
 class SubscriptionAdminInterfaceDispatcher(BaseCRUDAdminInterfaceDispatcher):
     prefix = 'subscription_admin_interface'
     map_name = "SUBSCRIPTION_ADMIN_INTERFACES"
-
-    @datespan_default
-    def dispatch(self, request, *args, **kwargs):
-        return super(SubscriptionAdminInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
