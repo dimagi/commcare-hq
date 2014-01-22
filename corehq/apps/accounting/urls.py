35e001eebc039b8282bb8a9553bab57d10d3a174
Nick Pellegrino <npellegrino@dimagi.com>
2014-01-22 17:03:52 -0500

    refactoring views

--- corehq/apps/accounting/urls.py
+++ corehq/apps/accounting/urls.py
@@ -5,10 +5,10 @@ from corehq.apps.accounting.views import *
 
 urlpatterns = patterns('corehq.apps.accounting.views',
     url(r'^accounting_default/$', 'accounting_default', name='accounting_default'),
-    url(r'^accounts/(\d+)/$', ManageBillingAccountView.as_view(), name=ManageBillingAccountView.name),
-    url(r'^accounts/new/', NewBillingAccountView.as_view(), name=NewBillingAccountView.name),
-    url(r'^subscriptions/(\d+)/', EditSubscriptionView.as_view(), name=EditSubscriptionView.name),
-    url(r'^accounts/new_subscription/(\d+)/', NewSubscriptionView.as_view(), name=NewSubscriptionView.name),
+    url(r'^accounts/(\d+)/$', ManageBillingAccountView.as_view(), name=ManageBillingAccountView.urlname),
+    url(r'^accounts/new/', NewBillingAccountView.as_view(), name=NewBillingAccountView.urlname),
+    url(r'^subscriptions/(\d+)/', EditSubscriptionView.as_view(), name=EditSubscriptionView.urlname),
+    url(r'^accounts/new_subscription/(\d+)/', NewSubscriptionView.as_view(), name=NewSubscriptionView.urlname),
     url(AccountingAdminInterfaceDispatcher.pattern(), AccountingAdminInterfaceDispatcher.as_view(),
         name=AccountingAdminInterfaceDispatcher.name()),
 )
