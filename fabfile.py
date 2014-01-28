44b947437cd746f7e91021bba1a5e7b0b3cbc54e
Daniel Roberts <droberts@dimagi.com>
2014-01-27 23:13:55 -0500

    replace kafka with celery for couchpulse

--- fabfile.py
+++ fabfile.py
@@ -991,8 +991,6 @@ def set_pillowtop_supervisorconf():
         # preview environment should not run pillowtop and index stuff
         # just rely on what's on staging
         _rebuild_supervisor_conf_file('make_supervisor_pillowtop_conf', 'supervisor_pillowtop.conf')
-    _rebuild_supervisor_conf_file('make_supervisor_conf', 'supervisor_kafka.conf')
-    _rebuild_supervisor_conf_file('make_supervisor_conf', 'supervisor_couchpulse_consumer.conf')
 
 
 @roles(*ROLES_DJANGO)
