ee37a849fbd1afcc7587f692c6348e1ae8ab8265
Daniel Roberts <droberts@dimagi.com>
2014-01-29 18:19:13 -0500

    cp instead of ln git-hooks, so they don't change depending on branch

--- git-hooks/install.sh
+++ git-hooks/install.sh
@@ -4,5 +4,5 @@ function git-submodule-list() {
     git submodule | sed 's/^+/ /' | cut -f3 -d' '
 }
 
-ln -s $(pwd)/git-hooks/pre-commit.sh .git/hooks/pre-commit
-git-submodule-list | xargs -n1 -I% ln -s $(pwd)/git-hooks/pre-commit.sh .git/modules/%/hooks/pre-commit
+cp git-hooks/pre-commit.sh .git/hooks/pre-commit
+git-submodule-list | xargs -n1 -I% cp git-hooks/pre-commit.sh .git/modules/%/hooks/pre-commit
