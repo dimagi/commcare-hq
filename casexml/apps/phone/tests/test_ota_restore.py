cfee2bcffd1a2a62e6cf0c85dc48fa89ef593d0a
Daniel Roberts <droberts@dimagi.com>
2014-01-29 01:51:43 -0500

    make items an optional parameter, and fix tests

--- casexml/apps/phone/tests/test_ota_restore.py
+++ casexml/apps/phone/tests/test_ota_restore.py
@@ -38,14 +38,26 @@ class OtaRestoreTest(TestCase):
                                xml.get_registration_xml(dummy_user()))
 
     def testUserRestore(self):
-        self.assertEqual(0, len(SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all()))
-        restore_payload = generate_restore_payload(dummy_user())
-        # implicit length assertion
-        [sync_log] = SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all()
-        check_xml_line_by_line(self, dummy_restore_xml(sync_log.get_id, items=3), restore_payload)
+        self.assertEqual(0, SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False,
+        ).count())
+        restore_payload = generate_restore_payload(dummy_user(), items=True)
+        sync_log = SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False,
+        ).one()
+        check_xml_line_by_line(
+            self,
+            dummy_restore_xml(sync_log.get_id, items=3),
+            restore_payload,
+        )
 
     def testUserRestoreWithCase(self):
-        file_path = os.path.join(os.path.dirname(__file__), "data", "create_short.xml")
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "create_short.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
@@ -81,28 +93,51 @@ class OtaRestoreTest(TestCase):
                 <external_id>someexternal</external_id>
             </update>
         </case>"""
-        check_xml_line_by_line(self, expected_v2_case_block, xml.get_case_xml\
-                               (newcase, [case_const.CASE_ACTION_CREATE,
-                                          case_const.CASE_ACTION_UPDATE],
-                                version="2.0"))
-        
-        
-        restore_payload = generate_restore_payload(dummy_user())
-        # implicit length assertion
-        [sync_log] = SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all()
-        check_xml_line_by_line(self, dummy_restore_xml(sync_log.get_id, expected_case_block, items=4),
-                               restore_payload)
-        
+        check_xml_line_by_line(
+            self,
+            expected_v2_case_block,
+            xml.get_case_xml(
+                newcase,
+                [case_const.CASE_ACTION_CREATE, case_const.CASE_ACTION_UPDATE],
+                version="2.0",
+            ),
+        )
+
+        restore_payload = generate_restore_payload(
+            user=dummy_user(),
+            items=True,
+        )
+        sync_log_id = SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False,
+        ).one().get_id
+        check_xml_line_by_line(
+            self,
+            dummy_restore_xml(sync_log_id, expected_case_block, items=4),
+            restore_payload
+        )
+
     def testWithReferrals(self):
-        self.assertEqual(0, len(CommCareCase.view("case/by_user", reduce=False, include_docs=True).all()))
-        file_path = os.path.join(os.path.dirname(__file__), "data", "case_create.xml")
+        self.assertEqual(0, CommCareCase.view(
+            "case/by_user",
+            reduce=False,
+            include_docs=True,
+        ).count())
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "case_create.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
         process_cases(form)
-        [newcase] = CommCareCase.view("case/by_user", reduce=False, include_docs=True).all()
+        newcase = CommCareCase.view(
+            "case/by_user",
+            reduce=False,
+            include_docs=True,
+        ).one()
         self.assertEqual(0, len(newcase.referrals))
-        file_path = os.path.join(os.path.dirname(__file__), "data", "case_refer.xml")
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "case_refer.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
@@ -259,58 +294,99 @@ class OtaRestoreTest(TestCase):
     </update>
 </case>"""
 
-        v2response = views.xml_for_case(HttpRequest(), updated_case.get_id, version="2.0")
+        v2response = views.xml_for_case(HttpRequest(), updated_case.get_id,
+                                        version="2.0")
         check_xml_line_by_line(self, expected_v2_response, v2response.content)
 
-    def testSyncToken(self):
+    def testSyncTokenWithItems(self):
+        self._test_sync_token(items=True)
+
+    def testSyncTokenWithoutItems(self):
+        self._test_sync_token(items=False)
+
+    def _test_sync_token(self, items):
         """
         Tests sync token / sync mode support
         """
-
-        file_path = os.path.join(os.path.dirname(__file__), "data", "create_short.xml")
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "create_short.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
         process_cases(form)
 
         time.sleep(1)
-        restore_payload = generate_restore_payload(dummy_user())
-        # implicit length assertion
-        [sync_log] = SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all()
-        check_xml_line_by_line(self, dummy_restore_xml(sync_log.get_id, const.CREATE_SHORT, items=4),
-                               restore_payload)
+        restore_payload = generate_restore_payload(dummy_user(), items=items)
+
+        sync_log_id = SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False
+        ).one().get_id
+        expected_restore_payload = dummy_restore_xml(
+            sync_log_id,
+            const.CREATE_SHORT,
+            items=4 if items else None,
+        )
+        check_xml_line_by_line(self, expected_restore_payload, restore_payload)
 
         time.sleep(1)
-        sync_restore_payload = generate_restore_payload(dummy_user(), sync_log.get_id)
-        [latest_log] = [log for log in \
-                        SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all() \
-                        if log.get_id != sync_log.get_id]
+        sync_restore_payload = generate_restore_payload(
+            user=dummy_user(),
+            restore_id=sync_log_id,
+            items=items,
+        )
+        all_sync_logs = SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False,
+        ).all()
+        [latest_log] = [log for log in all_sync_logs
+                        if log.get_id != sync_log_id]
 
         # should no longer have a case block in the restore XML
-        check_xml_line_by_line(self, dummy_restore_xml(latest_log.get_id, items=3),
-                               sync_restore_payload)
+        check_xml_line_by_line(
+            self,
+            dummy_restore_xml(latest_log.get_id, items=3 if items else None),
+            sync_restore_payload,
+        )
 
         # apply an update
         time.sleep(1)
-        file_path = os.path.join(os.path.dirname(__file__), "data", "update_short.xml")
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "update_short.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
         process_cases(form)
-        
+
         time.sleep(1)
-        sync_restore_payload = generate_restore_payload(dummy_user(), latest_log.get_id)
-        [even_latest_log] = [log for log in \
-                             SyncLog.view("phone/sync_logs_by_user", include_docs=True, reduce=False).all() \
-                             if log.get_id != sync_log.get_id and log.get_id != latest_log.get_id]
-        
+        sync_restore_payload = generate_restore_payload(
+            user=dummy_user(),
+            restore_id=latest_log.get_id,
+            items=items,
+        )
+        all_sync_logs = SyncLog.view(
+            "phone/sync_logs_by_user",
+            include_docs=True,
+            reduce=False,
+        ).all()
+        [even_latest_log] = [log for log in all_sync_logs
+                             if log.get_id != sync_log_id and
+                             log.get_id != latest_log.get_id]
+
         # case block should come back
-        check_xml_line_by_line(self, dummy_restore_xml(even_latest_log.get_id, const.UPDATE_SHORT, items=4),
+        expected_sync_restore_payload = dummy_restore_xml(
+            even_latest_log.get_id,
+            const.UPDATE_SHORT,
+            items=4 if items else None,
+        )
+        check_xml_line_by_line(self, expected_sync_restore_payload,
                                sync_restore_payload)
-        
-        
+
     def testRestoreAttributes(self):
-        file_path = os.path.join(os.path.dirname(__file__), "data", "attributes.xml")
+        file_path = os.path.join(os.path.dirname(__file__),
+                                 "data", "attributes.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
