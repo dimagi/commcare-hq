93c9ed2c2c1039684730fb390640aabed21183a6
yedi <yedispaghetti@gmail.com>
2014-02-03 18:42:40 -0600

    emit useful fields in the by_user view

--- couchforms/_design/views/by_user/map.js
+++ couchforms/_design/views/by_user/map.js
@@ -7,10 +7,18 @@ function (doc) {
         if (meta) return meta.userID;
     }
 
+    function get_username(xform_doc) {
+        var meta = xform_doc.form.meta;
+        if (meta) return meta.userID;
+    }
+
     if (doc.doc_type == "XFormInstance" && get_user_id(doc) != null) {
         var date = parse_date(doc.received_on);
+        var user_id = get_user_id(doc);
+        var username = get_username(doc);
+        var emitting_doc = {"domain": doc.domain, "user_id": user_id, "username": username};
         if (date) {
-            emit([get_user_id(doc), date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), doc.xmlns], 1);
+            emit([user_id, date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), doc.xmlns], emitting_doc);
         }
     }
 }
