bf401d1d6255131ceecd5bbf83254f844774b149
Cory Zue <czue@dimagi.com>
2014-02-04 18:10:11 -0600

    temporarily hide reporting rates report

--- corehq/__init__.py
+++ corehq/__init__.py
@@ -44,7 +44,7 @@ def REPORTS(project):
     
     if project.commtrack_enabled:
         reports.insert(0, (ugettext_lazy("Commtrack"), (
-            commtrack_reports.ReportingRatesReport,
+            # commtrack_reports.ReportingRatesReport,
             commtrack_reports.CurrentStockStatusReport,
             commtrack_reports.AggregateStockStatusReport,
             # commtrack_reports.RequisitionReport,
