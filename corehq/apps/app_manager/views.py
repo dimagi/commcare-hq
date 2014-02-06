059fbeeb9ca0508c936534fd9437891e6b757515
Simon <simongdkelly@gmail.com>
2014-02-06 15:42:55 +0200

    refactor new_form

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -900,14 +900,8 @@ def _new_advanced_module(req, domain, app, name, lang):
             commtrack_enabled=app.commtrack_enabled
         )
     )
-    form = AdvancedForm(
-        name={lang if lang else "en": _("Untitled Form")},
-    )
-    module.forms.append(form)
-    form = module.get_form(-1)
-    form.source = ''
-
     module_id = module.id
+    app.new_form(module_id, _("Untitled Form"), lang)
 
     app.save()
     response = back_to_main(req, domain, app_id=app.id, module_id=module_id)
