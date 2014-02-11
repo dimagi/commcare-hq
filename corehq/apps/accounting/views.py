a6ba5220bed4983fae61f5c7c801f77123f2a28b
Nick Pellegrino <npellegrino@dimagi.com>
2014-02-11 15:20:25 -0600

    move async handler code to its own class

--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -52,6 +52,25 @@ class BillingAccountsSectionView(AccountingSectionView):
         }]
 
 
+class AsyncHandlerMixin():
+    async_handlers = []
+
+    @property
+    def handler_slug(self):
+        return self.request.POST.get('handler')
+
+    def get_async_handler(self):
+        handler_class = dict([(h.slug, h) for h in self.async_handlers])[self.handler_slug]
+        return handler_class(self.request)
+
+    @property
+    @memoized
+    def get_response(self):
+        if self.handler_slug in [h.slug for h in self.async_handlers]:
+            return self.get_async_handler().get_response()
+        return None
+
+
 class NewBillingAccountView(BillingAccountsSectionView):
     page_title = 'New Billing Account'
     template_name = 'accounting/accounts_base.html'
@@ -82,7 +101,7 @@ class NewBillingAccountView(BillingAccountsSectionView):
             return self.get(request, *args, **kwargs)
 
 
-class ManageBillingAccountView(BillingAccountsSectionView):
+class ManageBillingAccountView(BillingAccountsSectionView, AsyncHandlerMixin):
     page_title = 'Manage Billing Account'
     template_name = 'accounting/accounts.html'
     urlname = 'manage_billing_account'
@@ -132,17 +151,9 @@ class ManageBillingAccountView(BillingAccountsSectionView):
     def page_url(self):
         return reverse(self.urlname, args=(self.args[0],))
 
-    @property
-    def handler_slug(self):
-        return self.request.POST.get('handler')
-
-    def get_async_handler(self):
-        handler_class = dict([(h.slug, h) for h in self.async_handlers])[self.handler_slug]
-        return handler_class(self.request)
-
     def post(self, request, *args, **kwargs):
-        if self.handler_slug in [h.slug for h in self.async_handlers]:
-            return self.get_async_handler().get_response()
+        if self.get_response is not None:
+            return self.get_response
         if 'account' in self.request.POST and self.account_form.is_valid():
             self.account_form.update_account_and_contacts(self.account)
         elif 'adjust_credit' in self.request.POST and self.credit_form.is_valid():
@@ -321,7 +332,7 @@ class NewSoftwarePlanView(AccountingSectionView):
         return self.get(request, *args, **kwargs)
 
 
-class EditSoftwarePlanView(AccountingSectionView):
+class EditSoftwarePlanView(AccountingSectionView, AsyncHandlerMixin):
     template_name = 'accounting/plans.html'
     urlname = 'edit_software_plan'
     page_title = "Edit Software Plan"
@@ -379,17 +390,9 @@ class EditSoftwarePlanView(AccountingSectionView):
             'url': SoftwarePlanInterface.get_url(),
         }]
 
-    @property
-    def handler_slug(self):
-        return self.request.POST.get('handler')
-
-    def get_async_handler(self):
-        handler_class = dict([(h.slug, h) for h in self.async_handlers])[self.handler_slug]
-        return handler_class(self.request)
-
     def post(self, request, *args, **kwargs):
-        if self.handler_slug in [h.slug for h in self.async_handlers]:
-            return self.get_async_handler().get_response()
+        if self.get_response is not None:
+            return self.get_response
         if 'update_version' in request.POST:
             if self.software_plan_version_form.is_valid():
                 self.software_plan_version_form.save(request)
