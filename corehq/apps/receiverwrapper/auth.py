c121664f5292788fe97b7042e0df8ba504737a09
Daniel Roberts <droberts@dimagi.com>
2014-02-12 12:47:19 -0600

    tighten noauth behavior to be safe, while accepting demo_user submissions
    expand tests

--- corehq/apps/receiverwrapper/auth.py
+++ corehq/apps/receiverwrapper/auth.py
@@ -4,10 +4,10 @@ from corehq.apps.domain.models import Domain
 
 class AuthContext(DocumentSchema):
     authenticated = BooleanProperty(default=False)
-    domain = StringProperty()
+    domain = StringProperty(required=True)
     user_id = StringProperty()
 
-    def auth_required(self):
+    def _auth_required(self):
         domain = Domain.get_by_name(self.domain, strict=True)
         if domain:
             return domain.secure_submissions
@@ -16,6 +16,12 @@ class AuthContext(DocumentSchema):
 
     def is_valid(self):
         try:
-            return self.authenticated or not self.auth_required()
+            return self.authenticated or not self._auth_required()
         except ResourceNotFound:
             return False
+
+
+class WaivedAuthContext(AuthContext):
+
+    def _auth_required(self):
+        return False
