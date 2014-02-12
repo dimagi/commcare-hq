77e5efd1e212af81b393d3fa0bd670fff6fefbee
Daniel Roberts <droberts@dimagi.com>
2014-02-12 12:55:10 -0600

    fix deprecation error on every manage.py call
    http://docs.djangoproject.com/en/dev/releases/1.4/#request-exceptions-are-now-always-logged

--- settings.py
+++ settings.py
@@ -623,6 +623,11 @@ LOGGING = {
             'format': '%(asctime)s %(levelname)s %(module)s %(message)s'
         },
     },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
         'pillowtop': {
             'level': 'INFO',
@@ -646,6 +651,7 @@ LOGGING = {
         },
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler',
         },
         'sentry': {
