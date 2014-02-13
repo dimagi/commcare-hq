dimagi/commcare-hq

commit c62a0a5020dc109124b497662c0be0c2e98d997f

Revert "Merge pull request #1674 from dimagi/reverting"
This reverts commit a238faeb8d6a4124a6a5364066b4db51201f2ebc, reversing
changes made to a92e5779c6adf581cd98401933844e6588e69916.

corehq/apps/reports/tasks.py
@@ -80,8 +80,8 @@ def create_metadata_export(download_id, domain, format, filename, datespan=None,
 @periodic_task(run_every=crontab(hour="*", minute="0", day_of_week="*"), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE','celery'))
 def daily_reports():    
     # this should get called every hour by celery
-    reps = ReportNotification.view("reportconfig/daily_notifications",
-                                   key=datetime.utcnow().hour,
+    reps = ReportNotification.view("reportconfig/all_notifications",
+                                   key=["daily", datetime.utcnow().hour, None],
                                    include_docs=True).all()
     for rep in reps:
         send_report.delay(rep._id)
@@ -90,8 +90,17 @@ def daily_reports():
 def weekly_reports():    
     # this should get called every hour by celery
     now = datetime.utcnow()
-    reps = ReportNotification.view("reportconfig/weekly_notifications",
-                                   key=[now.weekday(), now.hour],
+    reps = ReportNotification.view("reportconfig/all_notifications",
+                                   key=["weekly", now.hour, now.weekday()],
+                                   include_docs=True).all()
+    for rep in reps:
+        send_report.delay(rep._id)
+
+@periodic_task(run_every=crontab(hour="*", minute="1", day_of_week="*"), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE','celery'))
+def monthly_reports():
+    now = datetime.utcnow()
+    reps = ReportNotification.view("reportconfig/all_notifications",
+                                   key=["monthly", now.hour, now.day],
                                    include_docs=True).all()
     for rep in reps:
         send_report.delay(rep._id)

corehq/apps/reports/forms.py
@@ -13,15 +13,16 @@ class ScheduledReportForm(forms.Form):
         help_text='Note: not all built-in reports support email delivery, so'
                   ' some of your saved reports may not appear in this list')
 
-    day_of_week = forms.TypedChoiceField(
-        label='Day',
-        coerce=int,
-        choices=(('Daily',
-                        ((-1, 'Every Day'),)),
-                 ('Once a week on...',
-                        ReportNotification.day_choices())))
+    interval = forms.TypedChoiceField(
+        label='Interval',
+        choices=[("daily", "Daily"), ("weekly", "Weekly"), ("monthly", "Monthly")])
 
-    hours = forms.TypedChoiceField(
+    day = forms.TypedChoiceField(
+        label="Day",
+        coerce=int,
+        choices=[(i, i) for i in range(1, 32)])
+
+    hour = forms.TypedChoiceField(
         label='Time',
         coerce=int,
         choices=ReportNotification.hour_choices(),

corehq/apps/reports/management/commands/migrate_report_notifications.py
@@ -4,7 +4,7 @@ from dimagi.utils.couch.database import iter_docs
 
 
 
-WHICH_MIGRATION = "first"  # this should be changed to "second" after the first migration has ran.
+WHICH_MIGRATION = "second"  # this should be changed to "second" after the first migration has ran.
                            # This should happen in the PR that introduces the schedule report refactor
 
 def first_migrate(notification):

corehq/apps/reports/models.py
@@ -368,33 +368,17 @@ class UnsupportedScheduledReportError(Exception):
 
 
 class ReportNotification(Document):
-    # 11/12: removed WeeklyNotification and DailyNotification subclasses
-
     domain = StringProperty()
     owner_id = StringProperty()
 
     recipient_emails = StringListProperty()
-    config_ids = StringListProperty()  # added 11/2012
-    send_to_owner = BooleanProperty()  # added 11/2012
+    config_ids = StringListProperty()
+    send_to_owner = BooleanProperty()
 
-    # should be named hour, actually.  moved from subclasses
-    hours = IntegerProperty(default=8)
-    # moved from WeeklyNotification subclass.  Will now be -1 for daily
-    # notifications
-    day_of_week = IntegerProperty(default=-1)
+    hour = IntegerProperty(default=8)
+    day = IntegerProperty(default=1)
+    interval = StringProperty(choices=["daily", "weekly", "monthly"])
 
-    # report_slug = StringProperty()  # removed 11/2012
-    # removed 11/2012, only ever contained the user_id of the owner
-    # user_ids = StringListProperty()
-
-    @classmethod
-    def wrap(cls, data):
-        from corehq.apps.reports.management.commands.migrate_report_notifications import first_migrate
-        should_save = first_migrate(data)
-        self = super(ReportNotification, cls).wrap(data)
-        if should_save:
-            self.save()
-        return self
 
     @property
     def is_editable(self):
@@ -439,7 +423,7 @@ class ReportNotification(Document):
     @property
     @memoized
     def owner(self):
-        id = self.owner_id if self.owner_id else self.user_ids[0]
+        id = self.owner_id
         try:
             return WebUser.get_by_user_id(id)
         except CouchUser.AccountTypeError:
@@ -473,17 +457,19 @@ class ReportNotification(Document):
             config.report_type = ProjectReportDispatcher.prefix
             config.report_slug = self.report_slug
             config.domain = self.domain
-            config.owner_id = self.user_ids[0]
+            config.owner_id = self.owner_id
             configs = [config]
 
         return configs
 
     @property
     def day_name(self):
-        if self.day_of_week != -1:
-            return calendar.day_name[self.day_of_week]
-        else:
-            return "Every day"
+        if self.interval == 'weekly':
+            return calendar.day_name[self.day]
+        return {
+            "daily": _("Every day"),
+            "monthly": _("Day %s of every month" % self.day),
+        }[self.interval]
 
     @classmethod
     def day_choices(cls):

corehq/apps/reports/static/reports/javascripts/scheduled_report_form.js
@@ -1,0 +1,40 @@
+function add_options_to_select($select, opt_list, selected_val) {
+    for (var i = 0; i < opt_list.length; i++) {
+        var $opt = $('<option />').val(opt_list[i][0]).text(opt_list[i][1]);
+        if (opt_list[i][0] === selected_val) {
+            $opt.attr("selected", "true");
+        }
+        $select.append($opt);
+    }
+    return $select;
+}
+
+function update_day_input(weekly_options, monthly_options, day_value) {
+    var $interval = $interval || $('[name="interval"]');
+    $('#div_id_day').remove();
+    if ($interval.val() !== 'daily') {
+        var opts = $interval.val() === 'weekly' ? weekly_options : monthly_options
+        var $day_select = $('<select id="id_day" class="select" name="day" />');
+        $day_select = add_options_to_select($day_select, opts, day_value);
+        var $day_control_group = $('<div id="div_id_day" class="control-group" />')
+                .append($('<label for="id_day" class="control-label requiredField">Day<span class="asteriskField">*</span></label>'))
+                .append($('<div class="controls" />').append($day_select));
+        $interval.parents('.control-group').after($day_control_group);
+    }
+}
+
+var ScheduledReportFormHelper = function(options) {
+    var self = this;
+    self.weekly_options = options.weekly_options;
+    self.monthly_options = options.monthly_options;
+    self.day_value = options.day_value;
+
+    self.init = function() {
+        $(function() {
+            update_day_input(self.weekly_options, self.monthly_options, self.day_value);
+            $('[name="interval"]').change(function() {
+                update_day_input(self.weekly_options, self.monthly_options);
+            })
+        });
+    };
+};

corehq/apps/reports/templates/reports/edit_scheduled_report.html
@@ -5,12 +5,21 @@
 {% block reports-js %}
 <script src="{% static 'hqwebapp/js/lib/jquery-ui/jquery-ui-1.9.2.multiselect-deps.custom.min.js' %}"></script>
 <script src="{% static 'hqwebapp/js/lib/jquery-ui/multiselect/ui.multiselect.js' %}"></script>
+<script src="{% static 'reports/javascripts/scheduled_report_form.js' %}"></script>
 {% endblock %}
 
 
 {% block reports-js-inline %}
 <script>
     $("#id_config_ids").width(600).height(200).multiselect();
+</script>
+<script type="text/javascript">
+    var scheduled_report_form_helper = new ScheduledReportFormHelper({
+        weekly_options: {{ weekly_day_options|JSON }},
+        monthly_options: {{ monthly_day_options|JSON }},
+        day_value: {{ day_value }}
+    });
+    scheduled_report_form_helper.init();
 </script>
 {% endblock %}
 

corehq/apps/reports/templates/reports/reports_home.html
@@ -98,7 +98,7 @@ $(function() {
                         </ul>
                         {% endif %}
                     </td>
-                    <td>{{ report.day_name }} at {{ report.hours }}:00</td>
+                    <td>{{ report.day_name }} at {{ report.hour }}:00</td>
                     <td>
                         {# handles old documents #}
                         {% if report.user_ids or report.send_to_owner %}

corehq/apps/reports/views.py
@@ -503,7 +503,7 @@ def edit_scheduled_report(request, domain, scheduled_report_id=None,
             raise HttpResponseBadRequest()
     else:
         instance = ReportNotification(owner_id=user_id, domain=domain,
-                                      config_ids=[], day_of_week=-1, hours=8,
+                                      config_ids=[], hour=8,
                                       send_to_owner=True, recipient_emails=[])
 
     is_new = instance.new_document
@@ -531,6 +531,9 @@ def edit_scheduled_report(request, domain, scheduled_report_id=None,
         return HttpResponseRedirect(reverse('reports_home', args=(domain,)))
 
     context['form'] = form
+    context['day_value'] = getattr(instance, "day", 1)
+    context['weekly_day_options'] = ReportNotification.day_choices()
+    context['monthly_day_options'] = [(i, i) for i in range(1, 32)]
     if is_new:
         context['form_action'] = "Create a new"
         context['report']['title'] = "New Scheduled Report"

commit cdb32053f5ad7b3e4c4e1e40273444154ad2a2fe

fix scheduled reports not sending bug
it looks like c62a0a5020dc109124b497662c0be0c2e98d997f introduced
an issue where the default day for daily reports would sometimes get set
to `1` which would then not get returned by the couch view query which
was explicitly checking for `None`. this just ignores the day entirely.

corehq/apps/reports/tasks.py
@@ -84,7 +84,8 @@ def create_metadata_export(download_id, domain, format, filename, datespan=None,
 def daily_reports():    
     # this should get called every hour by celery
     reps = ReportNotification.view("reportconfig/all_notifications",
-                                   key=["daily", datetime.utcnow().hour, None],
+                                   startkey=["daily", datetime.utcnow().hour],
+                                   endkey=["daily", datetime.utcnow().hour, {}],
                                    reduce=False,
                                    include_docs=True).all()
     for rep in reps: