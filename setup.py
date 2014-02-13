d9fc16ae6ad11a994844d1c1a3905576c43bcf85
Daniel Roberts <droberts@dimagi.com>
2014-02-13 11:10:12 -0600

    bump version to 1.0.9

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dimagi-utils',
-    version='1.0.8',
+    version='1.0.9',
     description='Dimagi Shared Utilities',
     author='Dimagi',
     author_email='dev@dimagi.com',
