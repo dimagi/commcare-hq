1401fe39cba29ace0a89e77799ef72d136b1f90f
Biyeun Buczyk <biyeun@dimagi.com>
2014-02-17 15:26:03 -0600

    remove print

--- corehq/apps/accounting/decorators.py
+++ corehq/apps/accounting/decorators.py
@@ -10,7 +10,6 @@ def require_billing_admin():
         def wrapped(request, *args, **kwargs):
             if not hasattr(request, 'couch_user') and not hasattr(request, 'domain'):
                 raise Http404()
-            print request.domain
             is_billing_admin = BillingAccountAdmin.get_admin_status_and_account(request.couch_user, request.domain)[0]
             if not (is_billing_admin or request.couch_user.is_superuser):
                 raise Http404()
