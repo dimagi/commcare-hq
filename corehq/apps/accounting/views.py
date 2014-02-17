fc6704fdd13d17de9477d11bce9bfd019dec0097
Biyeun Buczyk <biyeun@dimagi.com>
2014-02-17 15:09:08 -0600

    allow cross-domain access to pricing table

--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -405,4 +405,9 @@ def pricing_table_json(request, product, locale):
     table = PricingTable.get_table_by_product(product)
     table_json = json.dumps(table, cls=LazyEncoder)
     translation.deactivate()
-    return HttpResponse(table_json, content_type='application/json')
+    response = HttpResponse(table_json, content_type='application/json')
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
+    response["Access-Control-Max-Age"] = "1000"
+    response["Access-Control-Allow-Headers"] = "*"
+    return response
