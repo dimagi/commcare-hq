f4fc0ebadd0260dd46fcbbcb8048ffb34f5b6f20
yedi <yedispaghetti@gmail.com>
2014-02-18 17:19:40 -0600

    add real sql models for phonelog; add filter

--- phonelog/models.py
+++ phonelog/models.py
@@ -1,8 +1,21 @@
-from couchdbkit.ext.django.schema import Document, StringProperty,\
-    BooleanProperty, DateTimeProperty, IntegerProperty
+from django.db import models
 
-class _(Document): pass
+COUCH_UUID_MAX_LEN = 50
 
-#class DeviereportLogTag(Document):
-#    name = StringProperty()
-#    level = StringProperty()
+class Log(models.Model):
+    xform_id = models.CharField(max_length=COUCH_UUID_MAX_LEN)
+    msg = models.TextField()
+    id = models.CharField(max_length=50, primary_key=True)
+    type = models.CharField(max_length=32)
+    date = models.DateTimeField()
+    domain = models.CharField(max_length=100)
+    device_id = models.CharField(max_length=COUCH_UUID_MAX_LEN)
+    app_version = models.TextField()
+    username = models.CharField(max_length=100)
+
+
+class UserLog(models.Model):
+    xform_id = models.CharField(max_length=COUCH_UUID_MAX_LEN)
+    user_id = models.CharField(max_length=COUCH_UUID_MAX_LEN)
+    sync_token = models.CharField(max_length=COUCH_UUID_MAX_LEN)
+    username = models.CharField(max_length=100)
