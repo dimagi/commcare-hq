07e8f1df50eadcc57a46e3058961e6ce012e9a0a
Biyeun Buczyk <biyeun@dimagi.com>
2014-02-21 10:52:33 -0600

    also remove ImportCases from side navigation if privilege is not granted (under feature flag)

--- corehq/apps/data_interfaces/dispatcher.py
+++ corehq/apps/data_interfaces/dispatcher.py
@@ -1,9 +1,12 @@
 from django.utils.decorators import method_decorator
-from corehq import privileges
+from corehq import privileges, toggles
 from corehq.apps.accounting.decorators import requires_privilege_alert
 from corehq.apps.reports.dispatcher import ReportDispatcher, ProjectReportDispatcher, datespan_default
 from corehq.apps.users.decorators import require_permission
 from corehq.apps.users.models import Permissions
+from django_prbac.exceptions import PermissionDenied
+from django_prbac.utils import ensure_request_has_privilege
+import toggle
 
 require_can_edit_data = require_permission(Permissions.edit_data)
 
@@ -30,4 +33,11 @@ class EditDataInterfaceDispatcher(ReportDispatcher):
         return super(EditDataInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
 
     def permissions_check(self, report, request, domain=None):
+        if toggle.shortcuts.toggle_enabled(toggles.ACCOUNTING_PREVIEW, request.user.username):
+            from corehq.apps.importer.base import ImportCases
+            if report.split('.')[-1] in [ImportCases.__name__]:
+                try:
+                    ensure_request_has_privilege(request, privileges.BULK_CASE_MANAGEMENT)
+                except PermissionDenied:
+                    return False
         return request.couch_user.can_edit_data(domain)
