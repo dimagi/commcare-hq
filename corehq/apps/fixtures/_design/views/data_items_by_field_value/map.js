1fa604741e500fccc9ec30f540d8ba67649d9ee3
Sravan Reddy <sravfeyn@gmail.com>
2014-02-21 15:11:30 +0530

    adds null check for fixtures couch-view

--- corehq/apps/fixtures/_design/views/data_items_by_field_value/map.js
+++ corehq/apps/fixtures/_design/views/data_items_by_field_value/map.js
@@ -2,7 +2,7 @@ function (doc) {
     if (doc.doc_type === 'FixtureDataItem') {
         for (var key in doc.fields) {
             if (doc.fields.hasOwnProperty(key)) {
-                if (doc.fields[key].doc_type === 'FieldList'){
+                if (doc.fields[key] && doc.fields[key].doc_type === 'FieldList'){
                     for (var field_for_attribute in doc.fields[key].field_list){
                         emit([doc.domain, doc.data_type_id, key, field_for_attribute.field_value]);
                     }
