5be08c3f00334eb09debe00b529c1f6c19f395e0
Biyeun Buczyk <biyeun@dimagi.com>
2014-02-21 10:24:49 -0600

    apply privilege check to case importer

--- corehq/apps/data_interfaces/dispatcher.py
+++ corehq/apps/data_interfaces/dispatcher.py
@@ -1,4 +1,6 @@
 from django.utils.decorators import method_decorator
+from corehq import privileges
+from corehq.apps.accounting.decorators import requires_privilege_alert
 from corehq.apps.reports.dispatcher import ReportDispatcher, ProjectReportDispatcher, datespan_default
 from corehq.apps.users.decorators import require_permission
 from corehq.apps.users.models import Permissions
@@ -18,6 +20,13 @@ class EditDataInterfaceDispatcher(ReportDispatcher):
     @method_decorator(require_can_edit_data)
     @datespan_default
     def dispatch(self, request, *args, **kwargs):
+        from corehq.apps.importer.base import ImportCases
+        if kwargs.get('report_slug') in [ImportCases.slug]:
+            return self.bulk_dispatch(request, *args, **kwargs)
+        return super(EditDataInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
+
+    @method_decorator(requires_privilege_alert(privileges.BULK_CASE_MANAGEMENT))
+    def bulk_dispatch(self, request, *args, **kwargs):
         return super(EditDataInterfaceDispatcher, self).dispatch(request, *args, **kwargs)
 
     def permissions_check(self, report, request, domain=None):
