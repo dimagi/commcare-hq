05857df47bef0e61e59994f43b9433d4300df6f8
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-02-24 14:29:35 +0100

    WIP - Patient list report

--- custom/succeed/__init__.py
+++ custom/succeed/__init__.py
@@ -1,4 +1,8 @@
 from django.utils.translation import ugettext_noop as _
+from custom.succeed.reports import PatientListReport
 
 CUSTOM_REPORTS = (
+    (_('Custom Reports'), (
+       PatientListReport,
+    )),
 )
--- custom/succeed/reports.py
+++ custom/succeed/reports.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import NoReverseMatch, reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext as _, ugettext_noop
 from dimagi.utils.decorators.memoized import memoized
 from corehq.apps.api.es import ReportCaseES
 from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
@@ -8,9 +8,44 @@ from corehq.apps.reports.standard.cases.basic import CaseListReport
 from corehq.apps.reports.standard.cases.data_sources import CaseDisplay
 from corehq.pillows.base import restore_property_dict
 from django.utils import html
+from casexml.apps.case.models import CommCareCase
+
+
+PM1 = 'http://openrosa.org/formdesigner/111B09EB-DFFA-4613-9A16-A19BA6ED7D04'
+PM2 = 'http://openrosa.org/formdesigner/4B52ADB2-AA79-4056-A13E-BB34871876A1'
+PM3 = 'http://openrosa.org/formdesigner/5250590B-2EB2-46A8-9943-B7008CDA2BB9'
+PM4 = 'http://openrosa.org/formdesigner/876cec8f07c0e29b9f9e2bd0b33c5c85bf0192ee'
+CM1 = 'http://openrosa.org/formdesigner/9946952C-A2EB-43D5-A500-B386C56A49A7'
+CM2 = 'http://openrosa.org/formdesigner/BCFFFE7E-8C93-4B4E-9589-FF12C710C255'
+CM3 = 'http://openrosa.org/formdesigner/4EA3D459-7FB6-414F-B106-05E6E707568B'
+CM4 = 'http://openrosa.org/formdesigner/263cc99e9f0cdbc55d307359c7b45a1e555f35d1'
+CM5 = 'http://openrosa.org/formdesigner/8abd54794d8c5d592100b8cdf1f642903b7f4abe'
+CM6 = 'http://openrosa.org/formdesigner/9b47556945c6476438c2ac2f0583e2ca0055e46a'
+CM7 = 'http://openrosa.org/formdesigner/4b924f784e8dd6a23045649730e82f6a2e7ce7cf'
+HUD1 = 'http://openrosa.org/formdesigner/24433229c5f25d0bd3ceee9bf70c72093056d1af'
+HUD2 = 'http://openrosa.org/formdesigner/63f8287ac6e7dce0292ebac9b232b0d3bde327dc'
+PD1 = 'http://openrosa.org/formdesigner/9eb0eaf6954791425d6d5f0b66db9a484cacd264'
+PD2 = 'http://openrosa.org/formdesigner/69751bf3078369491e1c2f1e3c874895f762a4c1'
+CHW1 = 'http://openrosa.org/formdesigner/4b368b1d73862abeca3bce67b6e09724b8dca850'
+CHW2 = 'http://openrosa.org/formdesigner/cbc4e37437945bfda04e391d11006b6d02c24fc2'
+CHW3 = 'http://openrosa.org/formdesigner/5d77815bf7631a527d8647cdbaa5971e367f6548'
+CHW4 = 'http://openrosa.org/formdesigner/f8a741808584d772c4b899ef84db197da5b4d12a'
+
+
 
 
 class PatientListReportDisplay(CaseDisplay):
+
+    def __init__(self, report, case_dict):
+        case = CommCareCase.get(case_dict["_id"])
+        forms = case.get_forms()
+        super(PatientListReportDisplay, self).__init__(report, case_dict)
+
+    def get_property(self, key):
+        if key in self.case:
+            return self.case[key]
+        else:
+            return "---"
 
     @property
     def edit_link(self):
@@ -28,35 +63,38 @@ class PatientListReportDisplay(CaseDisplay):
 
     @property
     def mrn(self):
-        return self.case.create.mrn
+        return self.get_property("mrn")
 
     @property
     def randomization_date(self):
-        return "randomization_date"
+        return self.get_property("randomization_date")
 
     @property
     def visit_name(self):
-        return "visit_name"
+        return self.get_property("visit_name")
 
     @property
-    def target_Date(self):
-        return "target_Date"
+    def target_date(self):
+        return self.get_property("target_date")
 
     @property
     def most_recent(self):
-        return "most_recent"
+        return self.get_property("most_recent")
 
     @property
     def discuss(self):
-        return "discuss"
+        return self.get_property("discuss")
 
     @property
     def patient_info(self):
-        return "patient_info"
+        return self.get_property("patient_info")
 
 
 
 class PatientListReport(CustomProjectReport, CaseListReport):
+
+    name = ugettext_noop('Patient List')
+    slug = 'patient_list'
 
     fields = ['custom.succeed.fields.CareSite',
               'custom.succeed.fields.ResponsibleParty',
@@ -95,7 +133,7 @@ class PatientListReport(CustomProjectReport, CaseListReport):
                 disp.mrn,
                 disp.randomization_date,
                 disp.visit_name,
-                disp.target_Date,
+                disp.target_date,
                 disp.most_recent,
                 disp.discuss,
                 disp.patient_info
