5d669c47cd3f494ad3ee527229df7889bbca2699
Lukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-03-07 14:19:49 +0100

    code refactored

--- custom/succeed/__init__.py
+++ custom/succeed/__init__.py
@@ -1,5 +1,5 @@
 from django.utils.translation import ugettext_noop as _
-from custom.succeed.reports import PatientListReport
+from custom.succeed.reports.all_patients import PatientListReport
 
 CUSTOM_REPORTS = (
     (_('Custom Reports'), (
--- custom/succeed/utils.py
+++ custom/succeed/utils.py
@@ -18,31 +18,6 @@ CONFIG = {
     'chw_role': 'CHW'
 }
 
-def get_cloudcare_app(module_id):
-    from corehq.apps.cloudcare import api
-
-    def get_latest_build(domain, app_id):
-        build = ApplicationBase.view('app_manager/saved_app',
-                                     startkey=[domain, app_id, {}],
-                                     endkey=[domain, app_id],
-                                     descending=True,
-                                     limit=1).one()
-        return build._doc if build else None
-
-    apps = api.get_cloudcare_apps(SUCCEED_DOMAIN)
-    filtered_app = filter(lambda x: x['name'] == SUCCEED_CLOUD_APPNAME, apps)
-    app = api.look_up_app_json(SUCCEED_DOMAIN, filtered_app[0]['_id'])
-    app_id = app['_id']
-    module = app['modules'][module_id]
-    forms = module['forms']
-    ret = dict((f['xmlns'], ix) for (ix, f) in enumerate(forms))
-    ret['domain'] = SUCCEED_DOMAIN
-    latest_build = get_latest_build(SUCCEED_DOMAIN, app_id)
-    if latest_build is not None:
-        latest_build_id = latest_build['_id']
-        ret['build_id'] = latest_build_id
-    return ret
-
 
 def _is_succeed_admin(user):
     return True if user.get_role()['name'] == CONFIG['succeed_admin'] else False
