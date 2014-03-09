52442f21dab1e4e704ed5805ec0c2a7e49832457
Cory Zue <czue@dimagi.com>
2014-03-09 16:06:28 -0400

    move download status template to a shared place

--- corehq/apps/locations/views.py
+++ corehq/apps/locations/views.py
@@ -227,7 +227,7 @@ class LocationImportView(BaseLocationView):
         )
 
 @login_and_domain_required
-def location_importer_job_poll(request, domain, download_id, template="locations/manage/partials/status.html"):
+def location_importer_job_poll(request, domain, download_id, template="hqwebapp/partials/download_status.html"):
     context = get_download_context(download_id, check_state=True)
     context.update({
         'on_complete_short': _('Import complete.'),
