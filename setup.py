5881d05a4de6d8de6e82408461b8369e0d0a56c3
Daniel Roberts <droberts@dimagi.com>
2014-03-10 10:49:17 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dimagi-utils',
-    version='1.0.9',
+    version='1.0.10',
     description='Dimagi Shared Utilities',
     author='Dimagi',
     author_email='dev@dimagi.com',
