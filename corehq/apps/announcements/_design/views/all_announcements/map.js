a542fbd6a0ec89e0a216d5ecbc42aae1c801c73d
Simon <simongdkelly@gmail.com>
2014-03-10 14:21:09 +0200

    fix js syntax and unicode spaces

--- corehq/apps/announcements/_design/views/all_announcements/map.js
+++ corehq/apps/announcements/_design/views/all_announcements/map.js
@@ -2,7 +2,7 @@ function (doc) {
     if (doc.base_doc === "HQAnnouncement") {
         var emit_entry = {
             doc_type: doc.doc_type,
-            title: doc.title,
+            title: doc.title
         };
         emit(["all", doc.valid_until], emit_entry);
         emit(["type", doc.doc_type, doc.valid_until], emit_entry);
