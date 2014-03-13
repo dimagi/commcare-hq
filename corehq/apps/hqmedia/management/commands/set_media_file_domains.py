d43982187ef5d412ade2f75439635c36e430bdcf
yedi <yedispaghetti@gmail.com>
2014-03-13 13:26:13 -0400

    more robust set_media_domains script

--- corehq/apps/hqmedia/management/commands/set_media_file_domains.py
+++ corehq/apps/hqmedia/management/commands/set_media_file_domains.py
@@ -1,3 +1,4 @@
+from optparse import make_option
 from django.core.management.base import BaseCommand
 from corehq import Domain
 
@@ -5,13 +6,36 @@ from corehq import Domain
 class Command(BaseCommand):
     help = 'Adds domains that have apps w/ media to that media files list of valid domains'
 
+    option_list = BaseCommand.option_list + (
+        make_option('-s', '--since',
+            help="Begin at this domain. (Alphabetically Descending"),
+        make_option('-d', '--domain',
+            help="Check only this domain"),
+        )
+
     def handle(self, *args, **options):
-        for d in Domain.get_all():
-            for app in d.full_applications():
-                if app.is_remote_app():
+        if options.get('domain'):
+            domains = [Domain.get_by_name(options.get('domain'))]
+        else:
+            domains = Domain.get_all()
+
+        since = options.get('since')
+        seen_since = False
+        for d in domains:
+            if since and not seen_since:
+                if d.name == since:
+                    seen_since = True
+                else:
                     continue
-                for _, m in app.get_media_objects():
-                    if app.domain not in m.valid_domains:
-                        m.valid_domains.append(app.domain)
-                        self.stdout.write("adding domain %s to media file %s" % (app.domain, m._id))
-                        m.save()
+
+            for app in d.full_applications():
+                try:
+                    if app.is_remote_app():
+                        continue
+                    for _, m in app.get_media_objects():
+                        if app.domain not in m.valid_domains:
+                            m.valid_domains.append(app.domain)
+                            self.stdout.write("adding domain %s to media file %s" % (app.domain, m._id))
+                            m.save()
+                except Exception as e:
+                    self.stdout.write("Error in app %s-%s: %s" % (app.domain, app._id, e))
