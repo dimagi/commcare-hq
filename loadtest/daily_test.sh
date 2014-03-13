84c1e79102149e6618696a38390a2454df85bbbc
Ethan Soergel <esoergel@gmail.com>
2014-03-13 15:57:13 -0400

    update loadtest settings

--- loadtest/daily_test.sh
+++ loadtest/daily_test.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 DIR=`dirname $0`
+CODE_ROOT=`dirname $DIR`
 
-pip install -r requirements/loadtest-requirements.txt
+source $CODE_ROOT/../python_env/bin/activate
+
+# I would love to get rid of this:
+sudo apt-get build-dep python-matplotlib
+pip install -r $CODE_ROOT/requirements/loadtest-requirements.txt
 
 cp $DIR/config.cfg.example $DIR/config.cfg
+cd $CODE_ROOT
 python -c '
 import settings
+db_settings = settings.DATABASES["default"]
+db_settings["PORT"] = db_settings.get("PORT", "") or "5432"
 print "results_database = postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}".format(
-    **settings.DATABASES["default"]
+    **db_settings
 )
-' >> loadtest/config.cfg
+' >> $DIR/config.cfg
 
-multimech-run $DIR
+multimech-run `basename $DIR`
