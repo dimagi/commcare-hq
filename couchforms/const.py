7fe5207b9d2fa7272153312eb34a45f0dc8bcb19
Daniel Roberts <droberts@dimagi.com>
2014-03-14 11:47:25 -0400

    have a better response for an empty payload

--- couchforms/const.py
+++ couchforms/const.py
@@ -12,5 +12,21 @@ TAG_FORM = 'form'
 
 ATTACHMENT_NAME = "form.xml"
 
-MULTIPART_FILENAME_ERROR = object()
 MAGIC_PROPERTY = 'xml_submission_file'
+
+
+class BadRequest(object):
+    def __init__(self, message):
+        self.message = message
+
+
+MULTIPART_FILENAME_ERROR = BadRequest((
+    'If you use multipart/form-data, please name your file %s.\n'
+    'You may also do a normal (non-multipart) post '
+    'with the xml submission as the request body instead.\n'
+) % MAGIC_PROPERTY)
+MULTIPART_EMPTY_PAYLOAD_ERROR = BadRequest((
+    'If you use multipart/form-data, the file %s'
+    'must not have an empty payload\n'
+) % MAGIC_PROPERTY)
+EMPTY_PAYLOAD_ERROR = BadRequest('Post may not have an empty body\n')
