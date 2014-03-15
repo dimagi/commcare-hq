5f070dd10950345374ba640b6ff11d74e82d6e56
Cory Zue <czue@dimagi.com>
2014-03-15 10:43:20 -0400

    remove unused arguments

--- corehq/apps/sms/phonenumbers_helper.py
+++ corehq/apps/sms/phonenumbers_helper.py
@@ -9,11 +9,10 @@ class PhoneNumber(object):
         self.country_code = country_code
         self.national_number = national_number
 
-def parse_phone_number(number, region=None, keep_raw_input=False,
-                      numobj=None, _check_region=True, failhard=True):
+def parse_phone_number(number, region=None, failhard=True):
     from phonenumbers.phonenumberutil import parse as phonenumbers_parse, NumberParseException
     try:
-        phone = phonenumbers_parse(number, region, keep_raw_input, numobj, _check_region)
+        phone = phonenumbers_parse(number, region)
         if phone:
             return PhoneNumber(phone.country_code, phone.national_number)
     except NumberParseException:
