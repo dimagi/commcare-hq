123fd3e27355f2e5830da0d33c6b6accfe7cf0a6
Simon <simongdkelly@gmail.com>
2014-03-18 14:06:44 +0200

    extract elastic reindexer from base reindex command

--- corehq/apps/hqcase/management/commands/ptop_fast_reindex_xforms.py
+++ corehq/apps/hqcase/management/commands/ptop_fast_reindex_xforms.py
@@ -1,4 +1,4 @@
-from corehq.apps.hqcase.management.commands.ptop_fast_reindexer import PtopReindexer
+from corehq.apps.hqcase.management.commands.ptop_fast_reindexer import ElasticReindexer
 from corehq.pillows.xform import XFormPillow
 from couchforms.models import XFormInstance
 
@@ -6,7 +6,7 @@ CHUNK_SIZE = 500
 POOL_SIZE = 15
 
 
-class Command(PtopReindexer):
+class Command(ElasticReindexer):
     help = "Fast reindex of case elastic index by using the case view and reindexing cases"
 
     doc_class = XFormInstance
--- custom/trialconnect/management/commands/ptop_fast_reindex_tc_smslogs.py
+++ custom/trialconnect/management/commands/ptop_fast_reindex_tc_smslogs.py
@@ -1,12 +1,12 @@
 from corehq.apps.sms.models import SMSLog
-from corehq.apps.hqcase.management.commands.ptop_fast_reindexer import PtopReindexer
+from corehq.apps.hqcase.management.commands.ptop_fast_reindexer import ElasticReindexer
 from custom.trialconnect.smspillow import TCSMSPillow
 
 CHUNK_SIZE = 500
 POOL_SIZE = 15
 
 
-class Command(PtopReindexer):
+class Command(ElasticReindexer):
     help = "Fast reindex of trialconnect's sms elastic index by using the custom sms view and reindexing smslogs"
 
     doc_class = SMSLog
