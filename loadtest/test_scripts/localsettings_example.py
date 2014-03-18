e296d2070c6a506ce5092c091b542320ca988bbf
Ethan Soergel <esoergel@gmail.com>
2014-03-18 18:29:38 -0400

    Clarify loadtest localsettings business

---
+++ loadtest/test_scripts/localsettings_example.py
@@ -0,0 +1,11 @@
+BASE_URL = 'http://localhost:8000'
+DOMAIN = "demo"
+USERNAME = "changeme@dimagi.com"
+PASSWORD = "***"
+MOBILE_USERNAME = "user@demo.commcarehq.org"
+MOBILE_PASSWORD = "***"
+
+
+class Transaction(object):
+    def run(self):
+        pass
