6c5dcfeefa7e46f353a4dcdcec85aa92b02c1ed9
Biyeun Buczyk <biyeun@dimagi.com>
2014-03-18 14:42:54 -0400

    formatting

--- corehq/apps/domain/forms.py
+++ corehq/apps/domain/forms.py
@@ -793,8 +793,8 @@ class ConfirmNewSubscriptionForm(EditBillingAccountInfoForm):
             else:
                 subscription = Subscription.new_domain_subscription(
                     self.account, self.domain, self.plan_version,
-                    web_user=self.creating_user, adjustment_method=SubscriptionAdjustmentMethod.USER
-                )
+                    web_user=self.creating_user,
+                    adjustment_method=SubscriptionAdjustmentMethod.USER)
                 subscription.is_active = True
                 if subscription.plan_version.plan.edition == SoftwarePlanEdition.ENTERPRISE:
                     # this point can only be reached if the initiating user was a superuser
