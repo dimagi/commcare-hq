5fae0f5b51c2a48a1663e606632daeccd06ea330
Damian Szafranek <dszafranek@soldevelo.com>
2014-03-19 10:59:14 +0100

    mCCT client Log Page view

--- custom/m4change/reports/mcct_project_review.py
+++ custom/m4change/reports/mcct_project_review.py
@@ -39,7 +39,6 @@ class McctProjectReview(CustomProjectReport, ElasticProjectInspectionReport, Pro
     report_template_path = 'reports/selectTemplate.html'
     filter_users_field_class = StrongFilterUsersField
 
-
     @property
     def headers(self):
         headers = DataTablesHeader(
@@ -275,3 +274,142 @@ class McctRejectedClientPage(McctClientApprovalPage):
     slug = 'mcct_rejected_clients_page'
     report_template_path = 'reports/activateStatus.html'
     display_status = 'rejected'
+
+
+class McctClientLogPage(McctProjectReview):
+    name = 'mCCT client Log Page'
+    slug = 'mcct_client_log_page'
+
+    @property
+    def headers(self):
+        headers = DataTablesHeader(
+            DataTablesColumn(_("Date of service"), prop_name="form.meta.timeEnd"),
+            DataTablesColumn(_("Client Name"), sortable=False),
+            DataTablesColumn(_("Service Type"), sortable=False),
+            DataTablesColumn(_("Health Facility"), sortable=False),
+            DataTablesColumn(_("Card No."), sortable=False),
+            DataTablesColumn(_("LGA"), sortable=False),
+            DataTablesColumn(_("Phone No."), sortable=False),
+            DataTablesColumn(_("Amount due"), sortable=False),
+            DataTablesColumn(_("Status"), sortable=False),
+            DataTablesColumn(_("User"), sortable=False))
+        return headers
+
+    def es_query(self):
+        if not getattr(self, 'es_response', None):
+            range = self.request_params.get('range', None)
+            start_date = None
+            end_date = None
+            if range is not None:
+                dates = str(range).split(_(" to "))
+                start_date = dates[0]
+                end_date = dates[1]
+            filtered_case_ids = self._get_filtered_cases()
+            q = {
+                "query": {
+                    "range": {
+                        "form.meta.timeEnd": {
+                            "from": start_date,
+                            "to": end_date,
+                            "include_upper": False
+                        }
+                    }
+                },
+                "filter": {
+                    "and": [
+                        {"term": {"doc_type": "xforminstance"}},
+                        {"not": {"missing": {"field": "xmlns"}}},
+                        {"not": {"missing": {"field": "form.meta.userID"}}}
+                    ]
+                }
+            }
+
+            if len(filtered_case_ids) > 0:
+                case_ids_str = " OR ".join(filtered_case_ids)
+                q["filter"]["and"].append({
+                    "query": {
+                        "query_string": {
+                            "default_field": "form.case.@case_id",
+                            "query": case_ids_str
+                        }
+                    }
+                })
+
+            allforms = BOOKING_FORMS + FOLLOW_UP_FORMS + BOOKED_AND_UNBOOKED_DELIVERY_FORMS + IMMUNIZATION_FORMS
+            xmlnss = filter(None, [form for form in allforms])
+            if xmlnss:
+                q["filter"]["and"].append({"terms": {"xmlns.exact": xmlnss}})
+
+            modify_close = filter(None, [u'Modify/Close Client'])
+            q["filter"]["and"].append({"not": {"terms": {"form.@name": modify_close}}})
+
+            q["sort"] = self.get_sorting_block() \
+                if self.get_sorting_block() else [{"form.meta.timeEnd": {"order": "desc"}}]
+            self.es_response = es_query(params={"domain.exact": self.domain}, q=q, es_url=ES_URLS.get('forms'),
+                                        start_at=self.pagination.start, size=self.pagination.count)
+        return self.es_response
+
+    @property
+    def rows(self):
+        submissions = [res['_source'] for res in self.es_results.get('hits', {}).get('hits', [])]
+
+        for form in submissions:
+            try:
+                case_id = form["form"]["case"]["@case_id"]
+                case = get_case_by_id(case_id)
+            except KeyError:
+                case = EMPTY_FIELD
+
+            amount_due = EMPTY_FIELD
+            service_type = EMPTY_FIELD
+            visits = form["form"].get("visits")
+            form_id = form["_id"]
+            location_name = EMPTY_FIELD
+            location_parent_name = EMPTY_FIELD
+
+            if case is not EMPTY_FIELD:
+                user_id = get_property(case, "user_id", EMPTY_FIELD)
+                user = get_user_by_id(user_id)
+                location_id = get_commtrack_location_id(user, Domain.get_by_name(self.domain))
+                if location_id is not None:
+                    location = Location.get(location_id)
+                    location_name = location.name
+                    location_parent = location.parent
+                    location_parent_name = location_parent.name if location_parent is not None else EMPTY_FIELD
+
+            if form["xmlns"] in IMMUNIZATION_FORMS:
+                service_type = _("Immunization")
+                amount_due = 1000
+            elif form["xmlns"] in BOOKED_AND_UNBOOKED_DELIVERY_FORMS:
+                service_type = _("Delivery")
+                amount_due = 2000
+            elif visits == "1":
+                service_type = _("First Antenatal")
+                amount_due = 1000
+            elif visits == "2":
+                service_type = _("Second Antenatal")
+                amount_due = 300
+            elif visits == "3":
+                service_type = _("Third Antenatal")
+                amount_due = 300
+            elif visits == "4":
+                service_type = _("Fourth Antenatal")
+                amount_due = 400
+            try:
+                status = McctStatus.objects.filter(domain=self.domain, form_id=form_id)[:1].get()
+                status = status.status
+            except McctStatus.DoesNotExist:
+                status = 'eligible'
+
+            yield [
+                DateTimeProperty().wrap(form["form"]["meta"]["timeEnd"]).strftime("%Y-%m-%d %H:%M"),
+                get_property(case, "full_name", EMPTY_FIELD),
+                service_type,
+                location_name,
+                get_property(case, "card_number", EMPTY_FIELD),
+                location_parent_name,
+                get_property(case, "phone_number", EMPTY_FIELD),
+                amount_due,
+                status,
+                form["form"]["meta"]["username"]
+            ]
