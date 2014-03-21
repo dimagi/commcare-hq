cd86a79d48cb5b843e1ddb201882bbd0cb74d263
Ethan Soergel <esoergel@gmail.com>
2014-03-21 11:11:21 -0400

    Move fields to dont_use directory

--- corehq/apps/reports/dont_use/fields.py
+++ corehq/apps/reports/dont_use/fields.py
@@ -1,3 +1,7 @@
+"""
+DO NOT WRITE ANY NEW FUNCTIONALITY BASED ON THIS FILE
+This is being kept around only to support legacy reports
+"""
 import datetime
 from django.template.context import Context
 from django.template.loader import render_to_string
@@ -25,14 +29,7 @@ from django.core.urlresolvers import reverse
 import uuid
 from corehq.apps.users.models import WebUser
 
-"""
-    Note: Fields is being phased out in favor of filters.
-    The only reason it still exists is because admin reports needs to get moved over to the new
-    reporting structure.
-"""
 
-# TODO: Delete this file and corehq/apps/reports/fields.py.orig
-# TODO: Delete from here
 class ReportField(CacheableRequestMixIn):
     slug = ""
     template = ""
@@ -65,7 +62,7 @@ class ReportField(CacheableRequestMixIn):
 class ReportSelectField(ReportField):
     slug = "generic_select"
     name = ugettext_noop("Generic Select")
-    template = "reports/fields/select_generic.html"
+    template = "reports/dont_use_fields/select_generic.html"
     default_option = ugettext_noop("Select Something...")
     options = [dict(val="val", text="text")]
     cssId = "generic_select_box"
@@ -102,9 +99,93 @@ class ReportSelectField(ReportField):
         )
 
 
+class SelectMobileWorkerMixin(object):
+    slug = "select_mw"
+    name = ugettext_noop("Select Mobile Worker")
+
+    @classmethod
+    def get_default_text(cls, user_filter, default_option=None):
+        default = default_option or cls.default_option
+        if user_filter[HQUserType.ADMIN].show or \
+           user_filter[HQUserType.DEMO_USER].show or user_filter[HQUserType.UNKNOWN].show:
+            default = _('%s & Others') % _(default)
+        return default
+
+
+class SelectMobileWorkerField(SelectMobileWorkerMixin, ReportField):
+    template = "reports/dont_use_fields/select_mobile_worker.html"
+    default_option = ugettext_noop("All Mobile Workers")
+    filter_users_field_class = FilterUsersField
+
+    def __init__(self, request, domain=None, timezone=pytz.utc, parent_report=None, filter_users_field_class=None):
+        super(SelectMobileWorkerField, self).__init__(request, domain, timezone, parent_report)
+        if filter_users_field_class:
+            self.filter_users_field_class = filter_users_field_class
+
+    def update_params(self):
+        pass
+
+    def update_context(self):
+        self.user_filter, _ = self.filter_users_field_class.get_user_filter(self.request)
+        self.individual = self.request.GET.get('individual', '')
+        self.default_option = self.get_default_text(self.user_filter)
+        self.users = util.user_list(self.domain)
+
+        self.update_params()
+
+        self.context['field_name'] = self.name
+        self.context['default_option'] = self.default_option
+        self.context['users'] = self.users
+        self.context['individual'] = self.individual
+
+
+class SelectFilteredMobileWorkerField(SelectMobileWorkerField):
+    """
+        This is a little field for use when a client really wants to filter by
+        individuals from a specific group.  Since by default we still want to
+        show all the data, no filtering is done unless the special group filter
+        is selected.
+    """
+    slug = "select_filtered_mw"
+    name = ugettext_noop("Select Mobile Worker")
+    template = "reports/dont_use_fields/select_filtered_mobile_worker.html"
+    default_option = ugettext_noop("All Mobile Workers...")
+
+    # Whether to display both the default option and "Only <group> Mobile
+    # Workers" or just the default option (useful when using a single
+    # group_name and changing default_option to All <group> Workers)
+    show_only_group_option = True
+
+    group_names = []
+
+    def update_params(self):
+        if not self.individual:
+            self.individual = self.request.GET.get('filtered_individual', '')
+        self.users = []
+        self.group_options = []
+        for group in self.group_names:
+            filtered_group = Group.by_name(self.domain, group)
+            if filtered_group:
+                if self.show_only_group_option:
+                    self.group_options.append(dict(group_id=filtered_group._id,
+                        name=_("Only %s Mobile Workers") % group))
+                self.users.extend(filtered_group.get_users(is_active=True, only_commcare=True))
+
+    def update_context(self):
+        super(SelectFilteredMobileWorkerField, self).update_context()
+        self.context['users'] = self.users_to_options(self.users)
+        self.context['group_options'] = self.group_options
+
+    @staticmethod
+    def users_to_options(user_list):
+        return [dict(val=user.user_id,
+            text=user.raw_username,
+            is_active=user.is_active) for user in user_list]
+
+
 class FilterUsersField(ReportField):
     slug = "ufilter"
-    template = "reports/fields/filter_users.html"
+    template = "reports/dont_use_fields/filter_users.html"
     always_show_filter = False
     can_be_empty = False
 
@@ -135,8 +216,6 @@ class FilterUsersField(ReportField):
         elif ufilter:
             toggle = HQUserType.use_filter(ufilter)
         return toggle, show_filter
-
-# TODO: Delete to here
 
 
 class BooleanField(ReportField):
