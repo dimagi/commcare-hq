7f07d149489b69a26ffb702597eeeaddef356b89
yedi <yedispaghetti@gmail.com>
2014-03-26 18:47:41 -0400

    supported nested properties

--- corehq/pillows/xform.py
+++ corehq/pillows/xform.py
@@ -1,3 +1,4 @@
+import collections
 import copy
 from casexml.apps.case.xform import extract_case_blocks, get_case_ids_from_form
 from corehq.pillows.mappings.xform_mapping import XFORM_MAPPING, XFORM_INDEX
@@ -17,6 +18,22 @@ def is_valid_date(txt):
     except Exception:
         pass
     return False
+
+RESERVED = RESERVED_WORDS[:]
+RESERVED.remove('case')
+
+# modified from: http://stackoverflow.com/questions/6027558/flatten-nested-python-dictionaries-compressing-keys
+def flatten(d, parent_key='', delimiter='/'):
+    items = []
+    for k, v in d.items():
+        if k in RESERVED:
+            continue
+        new_key = parent_key + delimiter + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten(v, new_key, delimiter).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 class XFormPillow(HQPillow):
     document_class = XFormInstance
@@ -69,7 +86,7 @@ class XFormPillow(HQPillow):
                         case_dict[object_key] = None
 
             doc_ret["__retrieved_case_ids"] = list(get_case_ids_from_form(doc_dict))
-            form_props = ["%s:%s" % (k, v) for k, v in doc_ret['form'].iteritems() if k not in RESERVED_WORDS]
+            form_props = ["%s:%s" % (k, v) for k, v in flatten(doc_ret['form']).iteritems()]
             doc_ret["__props_for_querying"] = form_props
             return doc_ret
 
