39828d32c652fdf4799db0e4d2b0930194945269
Biyeun Buczyk <biyeun@dimagi.com>
2014-03-27 08:37:37 -0400

    add BOOKKEEPER_CONTACT_EMAILS to settings

--- settings.py
+++ settings.py
@@ -403,6 +403,7 @@ SUPPORT_EMAIL = "commcarehq-support@dimagi.com"
 CCHQ_BUG_REPORT_EMAIL = 'commcarehq-bug-reports@dimagi.com'
 BILLING_EMAIL = 'billing-comm@dimagi.com'
 INVOICING_CONTACT_EMAIL = 'accounts@dimagi.com'
+BOOKKEEPER_CONTACT_EMAILS = []
 EMAIL_SUBJECT_PREFIX = '[commcarehq] '
 
 SERVER_ENVIRONMENT = 'localdev'
