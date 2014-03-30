19fc3d65e06bbef916a5d35f2359247e70000563
Daniel Roberts <droberts@dimagi.com>
2014-03-30 15:09:41 -0400

    pep8 and @memoized

--- corehq/ex-submodules/phonelog/models.py
+++ corehq/ex-submodules/phonelog/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 COUCH_UUID_MAX_LEN = 50
 
+
 class Log(models.Model):
-    xform_id = models.CharField(max_length=COUCH_UUID_MAX_LEN, db_index=True,)
+    xform_id = models.CharField(max_length=COUCH_UUID_MAX_LEN, db_index=True)
     msg = models.TextField()
     id = models.CharField(max_length=50, primary_key=True)
     type = models.CharField(max_length=32, db_index=True)
-    date = models.DateTimeField(db_index=True,)
+    date = models.DateTimeField(db_index=True)
     domain = models.CharField(max_length=100, db_index=True)
     device_id = models.CharField(max_length=COUCH_UUID_MAX_LEN, db_index=True)
     app_version = models.TextField()
@@ -26,6 +27,7 @@ class UserLog(models.Model):
     user_id = models.CharField(max_length=COUCH_UUID_MAX_LEN)
     sync_token = models.CharField(max_length=COUCH_UUID_MAX_LEN)
     username = models.CharField(max_length=100, db_index=True)
+
 
 class _(Document):
     pass
