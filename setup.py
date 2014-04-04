daa4ed43c57a1f2afcc523536b58fea03dfdc66c
Daniel Roberts <droberts@dimagi.com>
2014-04-04 17:30:06 -0400

    use jsonobject-couchdbkit

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='couchexport',
-    version='0.0.1.2',
+    version='0.0.2',
     description='Dimagi Couch Exporter for Django',
     author='Dimagi',
     author_email='information@dimagi.com',
     url='http://www.dimagi.com/',
     install_requires = [
         "django",
-        "couchdbkit",
+        "jsonobject-couchdbkit",
         "dimagi-utils",
         'django-soil',
         "openpyxl",
