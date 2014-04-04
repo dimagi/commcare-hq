bcd5ab6b4a836740fee10d8f4ede5365c7d7e228
Cory Zue <czue@dimagi.com>
2014-04-04 18:21:42 -0400

    fix url that was only needed in tests

--- casexml/apps/case/urls.py
+++ casexml/apps/case/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from .views import reference_case_attachment_view
 
 urlpatterns = patterns('',
-    url(r'^casexml/attachment/(?P<domain>[\w\.:-]+)/(?P<case_id>[\w\-]+)/(?P<attachment_id>.*)/(?P<attachment_src>.*)$',
+    url(r'^casexml/attachment/(?P<domain>[\w\.:-]+)/(?P<case_id>[\w\-]+)/(?P<attachment_id>.*)$',
         reference_case_attachment_view, name="api_case_attachment")
 )
 
