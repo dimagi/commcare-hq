427db5852d9c22523d752a2f18cbcb8efa694efb
Cory Zue <czue@dimagi.com>
2014-04-04 18:22:05 -0400

    remove unused attachment_src flag in attachment view

--- corehq/apps/api/urls.py
+++ corehq/apps/api/urls.py
@@ -91,7 +91,7 @@ def api_url_patterns():
         pass # maybe pact isn't installed
     for view_class in DomainAPI.__subclasses__():
         yield url(r'^custom/%s/v%s/$' % (view_class.api_name(), view_class.api_version()), view_class.as_view(), name="%s_%s" % (view_class.api_name(), view_class.api_version()))
-    yield url(r'^case/attachment/(?P<case_id>[\w\-]+)/(?P<attachment_id>.*)/(?P<attachment_src>.*)$', CaseAttachmentAPI.as_view(), name="api_case_attachment")
+    yield url(r'^case/attachment/(?P<case_id>[\w\-]+)/(?P<attachment_id>.*)$', CaseAttachmentAPI.as_view(), name="api_case_attachment")
     yield url(r'^redis_assets/$', RedisAssetsAPI.as_view())
 
 
