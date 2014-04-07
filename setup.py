98e9da051e4d750b14208444c5c2064757b7885b
Daniel Roberts <droberts@dimagi.com>
2014-04-07 15:43:29 -0400

    bump version to 3.0.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='couchforms',
-    version='3.0.0',
+    version='3.0.1',
     description='Dimagi Couch Forms for Django',
     author='Dimagi',
     author_email='dev@dimagi.com',
