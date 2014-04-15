aec995618222b6e6caf84551635d9814d78216c2
Biyeun Buczyk <biyeun@dimagi.com>
2014-04-15 15:13:51 -0400

    split payment method handler into separate cost item and stripe card models for cleaner implementation and prep for feature 14

--- corehq/apps/accounting/static/accounting/ko/accounting.payment_method_handler.js
+++ corehq/apps/accounting/static/accounting/ko/accounting.payment_method_handler.js
@@ -1,94 +1,177 @@
-var PaymentMethodHandler = function (paginatedList) {
+var PaymentMethodHandler = function (errorMessages) {
     'use strict';
     var self = this;
 
-    self.currentBalance = ko.observable(0);
-    self.statementNo = ko.observable();
-    self.invoiceId = ko.observable();
-    self.customPaymentAmount = ko.observable(0);
-    self.paymentAmountType = ko.observable('full');
-    self.stripeToken = ko.observable();
-    self.displayThankYou = ko.observable(false);
-    self.paginatedItem = ko.observable();
-    self.paginatedList = paginatedList;
+    self.errorMessages = errorMessages;
 
-    self.stripeErrorMsg = ko.observable('');
-    self.showStripeErrors = ko.computed(function () {
-        return !! self.stripeErrorMsg();
+    self.costItem = ko.observable();
+    self.hasCostItem = ko.computed(function () {
+        return !! self.costItem();
+    });
+    self.stripeCard = ko.observable(new StripeCard());
+
+    self.paymentIsComplete = ko.observable(false);
+    self.paymentIsNotComplete = ko.computed(function () {
+        return ! self.paymentIsComplete();
     });
 
-    self.showServerError = ko.observable(false);
-
-    self.isCustomPaymentValid = ko.computed(function () {
-        try {
-            var balance = parseFloat(self.currentBalance()),
-            customAmount = parseFloat(self.customPaymentAmount());
-            return balance >= customAmount && customAmount >= 0.5;
-        } catch (e) {
-            return false;
-        }
+    self.isSubmitDisabled = ko.computed(function () {
+        return !(!! self.costItem() && self.costItem().isValid()) || self.stripeCard().isProcessing();
     });
 
-    self.showCustomAmountError = ko.computed(function () {
-        return !self.isCustomPaymentValid();
+    self.serverErrorMsg = ko.observable();
+    self.showServerErrorMsg = ko.computed(function () {
+        return !! self.serverErrorMsg();
     });
 
-    self.update = function (invoiceId, statementNo, balance, paginatedItem) {
-        self.invoiceId(invoiceId);
-        self.statementNo(statementNo);
-        self.currentBalance(balance);
-        self.customPaymentAmount(balance);
-        self.paymentAmountType('full');
-        self.stripeErrorMsg('');
-        self.stripeToken(null);
-        self.paginatedItem(paginatedItem);
-        self.clearStripeValues();
-        self.displayThankYou(false);
-        self.showServerError(false);
+    self.reset = function () {
+        self.paymentIsComplete(false);
+        self.serverErrorMsg('');
     };
 
     self.processPayment = function () {
-        self.processStripeCard();
-    };
-
-    self.clearStripeValues = function () {
-        $('input[data-stripe="number"]').val('');
-        $('input[data-stripe="cvc"]').val('');
-        $('input[data-stripe="exp-month"]').val('');
-        $('input[data-stripe="exp-year"]').val('');
-    };
-
-    self.processStripeCard = function () {
-        Stripe.card.createToken({
-            number: $('input[data-stripe="number"]').val(),
-            cvc: $('input[data-stripe="cvc"]').val(),
-            exp_month: $('input[data-stripe="exp-month"]').val(),
-            exp_year: $('input[data-stripe="exp-year"]').val()
-        }, function (status, response) {
-            if (response.error) {
-                self.stripeErrorMsg(response.error.message);
-            } else {
-                self.stripeErrorMsg('');
-                self.stripeToken(response['id']);
-                self.submitForm();
-            }
-        });
+        if (self.costItem().isValid()) {
+            self.stripeCard().process(self.submitForm);
+        }
     };
 
     self.submitForm = function () {
         $('#payment-form').ajaxSubmit({
             success: function (response) {
                 if (response.success) {
-                    self.paginatedList.refreshList(self.paginatedItem());
-                    self.clearStripeValues();
-                    self.displayThankYou(true);
+                    self.costItem().reset();
+                    self.stripeCard().reset();
+                    self.paymentIsComplete(true);
                 } else {
-                    self.showServerError(true);
+                    self.serverErrorMsg(response);
                 }
+                self.stripeCard().isProcessing(false);
             },
-            error: function (data) {
-                self.showServerError(true);
+            error: function (response, textStatus, errorThrown) {
+                errorThrown = errorThrown || 500;
+                self.serverErrorMsg(self.errorMessages[errorThrown]);
+                self.stripeCard().isProcessing(false);
             }
         });
-    }
+    };
+};
+
+var BaseCostItem = function (initData) {
+    'use strict';
+    var self = this;
+
+    self.reset = function () {
+        throw new Error("Missing implementation for reset");
+    };
+
+    self.isValid = function () {
+        throw new Error("missing implementation for isValid");
+    };
+};
+
+var Invoice = function (initData) {
+    'use strict';
+    BaseCostItem.call(this, initData);
+    var self = this;
+    self.paginatedItem = initData.paginatedItem;
+    self.paginatedList = initData.paginatedList;
+    self.id = ko.computed(function () {
+        return self.paginatedItem.itemData().id;
+    });
+    self.balance = ko.computed(function () {
+        return self.paginatedItem.itemData().balance;
+    });
+    self.invoiceNumber = ko.computed(function () {
+        return self.paginatedItem.itemData().invoice_number;
+    });
+    self.customPaymentAmount = ko.observable(self.balance());
+    self.paymentAmountType = ko.observable('full');
+
+    self.isAmountValid = ko.computed(function () {
+        try {
+            var balance = parseFloat(self.balance()),
+                customAmount = parseFloat(self.customPaymentAmount());
+            return balance >= customAmount && customAmount >= 0.5;
+        } catch (e) {
+            return false;
+        }
+    });
+
+    self.showAmountError = ko.computed(function () {
+        return ! self.isAmountValid();
+    });
+
+    self.selectPartialPayment = function () {
+        self.paymentAmountType('partial');
+    };
+
+    self.reset =  function () {
+        self.customPaymentAmount(self.balance());
+        self.paymentAmountType('full');
+        self.paginatedList.refreshList(self.paginatedItem);
+    };
+
+    self.isValid = function () {
+        return self.isAmountValid();
+    };
+};
+
+Invoice.prototype = Object.create( BaseCostItem.prototype );
+Invoice.prototype.constructor = Invoice;
+
+var StripeCard = function () {
+    'use strict';
+    var self = this;
+
+    self.number = ko.observable();
+    self.cvc = ko.observable();
+    self.expMonth = ko.observable();
+    self.expYear = ko.observable();
+    self.errorMsg = ko.observable();
+    self.token = ko.observable();
+    self.isTestMode = ko.observable(false);
+    self.isProcessing = ko.observable(false);
+
+    self.showErrors = ko.computed(function () {
+        return !! self.errorMsg();
+    });
+    self.cleanedNumber = ko.computed(function () {
+        if (self.number()) return self.number().split('-').join('');
+        return null;
+    });
+
+    self.process = function (callbackOnSuccess) {
+        self.isProcessing(true);
+        Stripe.card.createToken({
+            number: self.number(),
+            cvc: self.cvc(),
+            exp_month: self.expMonth(),
+            exp_year: self.expYear()
+        }, function (status, response) {
+            if (response.error) {
+                self.errorMsg(response.error.message);
+                self.isProcessing(false);
+            } else {
+                self.errorMsg('');
+                self.token(response.id);
+                self.isTestMode(!response.livemode);
+                if (self.token()) {
+                    callbackOnSuccess();
+                } else {
+                    self.isProcessing(false);
+                    self.errorMsg('Response from Stripe did not complete properly.');
+                }
+            }
+        });
+    };
+
+    self.reset = function () {
+        self.number('');
+        self.cvc('');
+        self.expMonth('');
+        self.expYear('');
+        self.errorMsg('');
+        self.token(null);
+        self.isTestMode(false);
+    };
 };
