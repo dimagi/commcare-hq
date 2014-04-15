9d757c2f591fbca069970e1fafd97cb700b78e05
Sravan Reddy <sravfeyn@gmail.com>
2014-04-15 13:16:27 +0530

    handle warnings and new_table correctly

--- corehq/apps/fixtures/views.py
+++ corehq/apps/fixtures/views.py
@@ -229,8 +229,8 @@ def download_item_lists(request, domain, html_response=False):
             data_types_view = [FixtureDataType.get(id) for id in request.GET.getlist("table_id")]
         except ResourceNotFound as Ex:
             if html_response:
+                messages.info(request, _("Sorry, we couldn't find that table. If you think this is a mistake please report an issue."))
                 raise Ex
-            messages.info(request, _("Sorry, we couldn't find that table. If you think this is a mistake please report an issue."))
             data_types_view = FixtureDataType.by_domain(domain)
     else:
         data_types_view = FixtureDataType.by_domain(domain)
