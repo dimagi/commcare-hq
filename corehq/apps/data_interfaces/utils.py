f7f3dc3993ea6bf704c1c1771e06a328bea6fda7
Nick Pellegrino <npellegrino@dimagi.com>
2014-04-15 12:22:11 -0400

    don't 500 when case identifier is not a string

--- corehq/apps/data_interfaces/utils.py
+++ corehq/apps/data_interfaces/utils.py
@@ -16,8 +16,11 @@ def add_cases_to_case_group(domain, case_group_id, uploaded_data):
         return response
 
     for row in uploaded_data:
-        identifier = row.get('case_identifier')
-        case = get_case_by_identifier(domain, identifier)
+        try:
+            identifier = row.get('case_identifier')
+            case = get_case_by_identifier(domain, identifier)
+        except AttributeError:
+            case = None
         if not case:
             response['errors'].append(_("Could not find case with identifier '%s'." % identifier))
         elif case.doc_type != 'CommCareCase':
