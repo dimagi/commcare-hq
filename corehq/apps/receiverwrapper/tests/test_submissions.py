07c5b7b92c307cb779995da69371329a26741096
Daniel Roberts <droberts@dimagi.com>
2014-04-16 14:20:01 -0400

    clean up test_submissions
    - cleaner formatting
    - use assertIn instead of ghetto version of that

--- corehq/apps/receiverwrapper/tests/test_submissions.py
+++ corehq/apps/receiverwrapper/tests/test_submissions.py
@@ -6,6 +6,7 @@ from django.test.client import Client
 from django.core.urlresolvers import reverse
 import os
 
+
 class SubmissionTest(TestCase):
     def setUp(self):
         # bit of a hack, but the tests optimize around this flag to run faster
@@ -19,49 +20,51 @@ class SubmissionTest(TestCase):
         self.client = Client()
         self.client.login(**{'username': 'test', 'password': 'foobar'})
         self.url = reverse("receiver_post", args=[self.domain])
-        
+
     def tearDown(self):
         self.couch_user.delete()
         self.domain.delete()
-        
+
     def _submit(self, formname):
         file_path = os.path.join(os.path.dirname(__file__), "data", formname)
         with open(file_path, "rb") as f:
             return self.client.post(self.url, {
                 "xml_submission_file": f
             })
-        
-    def _check_for_message(self, msg, response):
-        # ghetto
-        if msg in str(response):
-            return True
-        else:
-            # just so there's a printout
-            self.assertEqual(msg, str(response))
-        
-    
-    def testSubmitSimpleForm(self):
-        self.assertTrue(self._check_for_message("Thanks for submitting!",
-                                                self._submit("simple_form.xml")),
-                        "Basic form successfully parsed")
-    
-    def testSubmitBareForm(self):
-        self.assertTrue(self._check_for_message("Thanks for submitting!", 
-                                                self._submit("bare_form.xml")),
-                        "Bare form successfully parsed")
-    
-    def testSubmitUserRegistration(self):
-        self.assertTrue(self._check_for_message("Thanks for registering! Your username is mealz@", 
-                                                self._submit("user_registration.xml")),
-                        "User registration form successfully parsed")
-    
-    def testSubmitWithCase(self):
-        self.assertTrue(self._check_for_message("Thanks for submitting!",
-                                                self._submit("form_with_case.xml")),
-                        "Form with case successfully parsed")
-    
-    def testSubmitWithNamespacedMeta(self):
-        self.assertTrue(self._check_for_message("Thanks for submitting!",
-                                                self._submit("namespace_in_meta.xml")),
-                        "Form with namespace in meta successfully parsed")
-        
+
+    def _check_for_message(self, excerpt, response, msg=None):
+        return self.assertIn(excerpt, str(response), msg)
+
+    def test_submit_simple_form(self):
+        self._check_for_message(
+            "Thanks for submitting!",
+            self._submit("simple_form.xml"),
+        )
+
+    def test_submit_bare_form(self):
+        self._check_for_message(
+            "Thanks for submitting!",
+            self._submit("bare_form.xml"),
+            "Bare form successfully parsed",
+        )
+
+    def test_submit_user_registration(self):
+        self._check_for_message(
+            "Thanks for registering! Your username is mealz@",
+            self._submit("user_registration.xml"),
+            "User registration form successfully parsed",
+        )
+
+    def test_submit_with_case(self):
+        self._check_for_message(
+            "Thanks for submitting!",
+            self._submit("form_with_case.xml"),
+            "Form with case successfully parsed",
+        )
+
+    def test_submit_with_namespaced_meta(self):
+        self._check_for_message(
+            "Thanks for submitting!",
+            self._submit("namespace_in_meta.xml"),
+            "Form with namespace in meta successfully parsed",
+        )
