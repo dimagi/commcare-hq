38932fcf5a512ac92ef7db861908e4edb06c9a75
Cory Zue <czue@dimagi.com>
2014-04-28 16:01:33 -0700

    bump version to 3.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='couchforms',
-    version='3.0.1',
+    version='3.0.2',
     description='Dimagi Couch Forms for Django',
     author='Dimagi',
     author_email='dev@dimagi.com',
