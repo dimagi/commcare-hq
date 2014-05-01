2e42206e6ebd58aa8f676568894d4747df703d04
Cory Zue <czue@dimagi.com>
2014-05-01 11:45:29 -0700

    formatting

--- corehq/apps/reports/models.py
+++ corehq/apps/reports/models.py
@@ -770,7 +770,12 @@ class HQGroupExportConfiguration(GroupExportConfiguration):
 
     @classmethod
     def by_domain(cls, domain):
-        return cache_core.cached_view(cls.get_db(), "groupexport/by_domain", key=domain, reduce=False, include_docs=True, wrapper=cls.wrap)
+        return cache_core.cached_view(cls.get_db(), "groupexport/by_domain",
+            key=domain,
+            reduce=False,
+            include_docs=True,
+            wrapper=cls.wrap,
+        )
 
     @classmethod
     def get_for_domain(cls, domain):
