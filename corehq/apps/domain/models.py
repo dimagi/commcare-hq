dfd808237c8dd6df39c1a004c6bd660c37e03398
Daniel Roberts <droberts@dimagi.com>
2014-05-12 10:50:59 -0400

    make create_domain create insecure domain, for testing

--- corehq/apps/domain/models.py
+++ corehq/apps/domain/models.py
@@ -617,7 +617,8 @@ class Domain(Document, HQBillingDomainMixin, SnapshotMixin):
         return result
 
     @classmethod
-    def get_or_create_with_name(cls, name, is_active=False):
+    def get_or_create_with_name(cls, name, is_active=False,
+                                secure_submissions=True):
         result = cls.view("domain/domains", key=name, reduce=False, include_docs=True).first()
         if result:
             return result
@@ -626,7 +627,7 @@ class Domain(Document, HQBillingDomainMixin, SnapshotMixin):
                 name=name,
                 is_active=is_active,
                 date_created=datetime.utcnow(),
-                secure_submissions=True,
+                secure_submissions=secure_submissions,
             )
             new_domain.migrations = DomainMigrations(has_migrated_permissions=True)
             new_domain.save(**get_safe_write_kwargs())
