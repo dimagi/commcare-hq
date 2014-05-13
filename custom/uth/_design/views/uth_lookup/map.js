2b6c5419aa7befeb0a425ec54746864b8bba75a1
Tyler Wymer <twymer@gmail.com>
2014-05-13 15:03:07 +0200

    Fix scan time ordering for tests

--- custom/uth/_design/views/uth_lookup/map.js
+++ custom/uth/_design/views/uth_lookup/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.type === "child" && doc.scan_status !== 'scan_complete' && doc.domain === "uth-rhd") {
-        emit([doc.domain, 'VH014466XK', doc.exam_number, doc.date], doc._id);
+        emit([doc.domain, 'VH014466XK', doc.exam_number, doc.scan_time], doc._id);
     }
 }
