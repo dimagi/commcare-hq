6718e806cc2e3698872bed20eff3ae298e90db93
Tyler Wymer <twymer@gmail.com>
2014-05-16 11:11:59 +0200

    Add pendings exam end point

--- custom/uth/views.py
+++ custom/uth/views.py
@@ -1,5 +1,5 @@
 from corehq.apps.domain.decorators import login_or_digest
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_POST, require_GET
 import json
 from django.http import HttpResponse, Http404
 from custom.uth.utils import (
@@ -9,12 +9,14 @@ from custom.uth.utils import (
 )
 from custom.uth.models import SonositeUpload, VscanUpload
 from custom.uth.tasks import async_create_case, async_find_and_attach
+from dimagi.utils.couch.database import get_db
+from custom.uth.const import UTH_DOMAIN
 
 
 @require_POST
 @login_or_digest
 def vscan_upload(request, domain, **kwargs):
-    if domain != 'uth-rhd':
+    if domain != UTH_DOMAIN:
         raise Http404()
 
     scanner_serial = request.POST.get('scanner_serial', None)
@@ -42,10 +44,36 @@ def vscan_upload(request, domain, **kwargs):
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
+@require_GET
+@login_or_digest
+def pending_exams(request, domain, scanner_serial, **kwargs):
+    if domain != UTH_DOMAIN:
+        raise Http404()
+
+    if not scanner_serial:
+        response_data = {}
+        response_data['result'] = 'failed'
+        response_data['message'] = 'Missing scanner serial'
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+    results = get_db().view(
+        'uth/uth_lookup',
+        startkey=[UTH_DOMAIN, scanner_serial],
+        endkey=[UTH_DOMAIN, scanner_serial, {}],
+    ).all()
+
+    exam_ids = [r['key'][-1] for r in results]
+
+    response_data = {}
+    response_data['result'] = 'success'
+    response_data['exam_ids'] = exam_ids
+    return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+
 @require_POST
 @login_or_digest
 def sonosite_upload(request, domain, **kwargs):
-    if domain != 'uth-rhd':
+    if domain != UTH_DOMAIN:
         raise Http404()
 
     response_data = {}
