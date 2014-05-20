bf29bcf0ec5205e50764e47dc78d3af6b8690abd
Tyler Wymer <twymer@gmail.com>
2014-05-20 20:45:21 +0200

    Add scanner serial to view

--- custom/uth/_design/views/uth_lookup/map.js
+++ custom/uth/_design/views/uth_lookup/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.type === "child" && doc.scan_status !== 'scan_complete' && doc.domain === "uth-rhd") {
-        emit([doc.domain, 'VH014466XK', doc.exam_number, doc.scan_time], doc._id);
+        emit([doc.domain, doc.scanner_serial, doc.exam_number], doc._id);
     }
 }
