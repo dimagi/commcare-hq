a3506d2f46c918d828a60de5c010e792f1740eec
Nick Pellegrino <npellegrino@dimagi.com>
2014-05-29 16:07:03 -0400

    implement load_wholesaler_case_ids

--- custom/colalife/management/commands/load_wholesaler_case_ids.py
+++ custom/colalife/management/commands/load_wholesaler_case_ids.py
@@ -1,3 +1,5 @@
+from corehq.apps.commtrack.models import SupplyPointCase
+from corehq.apps.locations.models import Location
 from django.core.management import BaseCommand
 
 
@@ -5,4 +7,12 @@ class Command(BaseCommand):
     help = 'Assign wholesaler case IDs to child cases.'
 
     def handle(self, *args, **options):
-        pass
+        for retailer_location in Location.filter_by_type(
+                "colalifezambia", "retailer"):
+            retailer_case = SupplyPointCase.get_by_location(retailer_location)
+            wholesaler_location = retailer_location.parent
+            wholesaler_case = SupplyPointCase.get_by_location(
+                wholesaler_location
+            )
+            retailer_case.wholesaler_case_id = wholesaler_case._id
+            retailer_case.save()
