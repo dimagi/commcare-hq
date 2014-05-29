0fdb10f9d7fa0ee596a41c2ccb25750143e3c15e
Nick Pellegrino <npellegrino@dimagi.com>
2014-05-29 14:51:55 -0400

    add help string

--- custom/colalife/management/commands/load_wholesaler_case_ids.py
+++ custom/colalife/management/commands/load_wholesaler_case_ids.py
@@ -2,6 +2,7 @@ from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
+    help = 'Assign wholesaler case IDs to child cases.'
 
     def handle(self, *args, **options):
         pass
