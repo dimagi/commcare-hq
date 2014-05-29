2955147e20aab5f6fcdba3a2d7d7a8d5e1868636
Simon <simongdkelly@gmail.com>
2014-05-29 11:49:34 +0200

    normalize xml before comparison in tests

--- corehq/apps/app_manager/tests/util.py
+++ corehq/apps/app_manager/tests/util.py
@@ -1,9 +1,10 @@
 import json
 import os
+import lxml
+from lxml.doctestcompare import LXMLOutputChecker
 import mock
 from corehq.apps.builds.models import CommCareBuild, CommCareBuildConfig, \
     BuildSpec
-from lxml.doctestcompare import LXMLOutputChecker
 import difflib
 
 
@@ -25,14 +26,36 @@ class TestFileMixin(object):
     def get_xml(self, name):
         return self.get_file(name, 'xml')
 
-    def assertXmlEqual(self, want, got):
+    def assertXmlEqual(self, expected, actual, normalize=True):
+        if normalize:
+            parser = lxml.etree.XMLParser(remove_blank_text=True)
+            parse = lambda *args: normalize_attributes(lxml.etree.XML(*args))
+            expected = lxml.etree.tostring(parse(expected, parser), pretty_print=True)
+            actual = lxml.etree.tostring(parse(actual, parser), pretty_print=True)
+
         # snippet from http://stackoverflow.com/questions/321795/comparing-xml-in-a-unit-test-in-python/7060342#7060342
         checker = LXMLOutputChecker()
-        if not checker.check_output(want, got, 0):
+        if not checker.check_output(expected, actual, 0):
             message = "XML mismatch\n\n"
-            for line in difflib.unified_diff(want.splitlines(1), got.splitlines(1), fromfile='want.xml', tofile='got.xml'):
+            diff = difflib.unified_diff(
+                expected.splitlines(1),
+                actual.splitlines(1),
+                fromfile='want.xml',
+                tofile='got.xml'
+            )
+            for line in diff:
                 message += line + '\n'
             raise AssertionError(message)
+
+
+def normalize_attributes(xml):
+    """Sort XML attributes to make it easier to find differences"""
+    for node in xml.iterfind(".//*"):
+        if node.attrib:
+            attrs = sorted(node.attrib.iteritems())
+            node.attrib.clear()
+            node.attrib.update(attrs)
+    return xml
 
 
 def add_build(version, build_number):
