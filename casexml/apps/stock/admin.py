cb7170785af4bf853ff8495aaade520d3b133332
Tyler Wymer <twymer@gmail.com>
2014-06-20 12:21:06 -0400

    Add search fields to stock models

--- casexml/apps/stock/admin.py
+++ casexml/apps/stock/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+
 class StockReportAdmin(admin.ModelAdmin):
     model = StockReport
     list_display = ['date', 'type', 'form_id']
     list_filter = ['date', 'type']
+    search_fields = ['form_id']
+
 
 class StockTransactionAdmin(admin.ModelAdmin):
     model = StockTransaction
     list_display = ['report_date', 'section_id', 'type', 'subtype', 'case_id', 'product_id', 'quantity', 'stock_on_hand']
     list_filter = ['report__date', 'section_id', 'type', 'subtype']
+    search_fields = ['case_id', 'product_id']
 
     def report_date(self, obj):
         return obj.report.date
