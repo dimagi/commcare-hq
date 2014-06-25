7fa40c19a6df304ffaab76dccfad216d9bf377f3
Simon Kelly <skelly@dimagi.com>
2014-06-25 11:02:54 +0200

    add remove option to hooks

--- git-hooks/install.sh
+++ git-hooks/install.sh
@@ -10,12 +10,14 @@ This script installs CommCare HQ's default git hooks on all submodules
 OPTIONS:
    -h      Show this message
    -f      don't ask before overwriting your current git hooks
+   -r      remove existing hooks but do not add the new ones
 EOF
 }
 
-CP_OPT='-i'
+OPT='-i'
+FUNC='copy_hooks'
 
-while getopts "hf" OPTION
+while getopts "hfr" OPTION
 do
     case $OPTION in
         h)
@@ -23,7 +25,10 @@ do
             exit
             ;;
         f)
-            CP_OPT='-f'
+            OPT='-f'
+            ;;
+        r)
+            FUNC='remove_hooks'
             ;;
         ?)
             usage
@@ -32,6 +37,17 @@ do
     esac
 done
 
+function copy_hooks() {
+    hook=$1
+    base_path=$2
+    cp $OPT git-hooks/$hook.sh $base_path/hooks/$hook
+}
+
+function remove_hooks() {
+    hook=$1
+    base_path=$2
+    rm $OPT $base_path/hooks/$hook
+}
 
 function git-submodule-list() {
     git submodule | sed 's/^+/ /' | cut -f3 -d' '
@@ -40,9 +56,9 @@ function git-submodule-list() {
 for hook in 'pre-commit' 'post-checkout'
 do
 
-    cp $CP_OPT git-hooks/$hook.sh .git/hooks/$hook
+    ${FUNC} $hook '.git'
     for submodule in $(git-submodule-list)
     do
-        cp $CP_OPT git-hooks/$hook.sh .git/modules/$submodule/hooks/$hook
+        ${FUNC} $hook ".git/modules/$submodule"
     done
 done
