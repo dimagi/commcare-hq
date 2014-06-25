ec7e672769f7c698abdd9425ca3b324b0b4fda84
Nick Pellegrino <npellegrino@dimagi.com>
2014-06-25 14:53:37 -0400

    don't automatically show all reports to a mobile worker who has access to reports

--- corehq/apps/reports/dispatcher.py
+++ corehq/apps/reports/dispatcher.py
@@ -176,9 +176,7 @@ class ReportDispatcher(View):
             report_contexts = []
             for report in report_group:
                 class_name = report.__module__ + '.' + report.__name__
-                from corehq.apps.reports.util import is_mobile_worker_with_report_access
-                if not (dispatcher.permissions_check(class_name, request, domain=domain, is_navigation_check=True)
-                        or is_mobile_worker_with_report_access(couch_user, domain)):
+                if not (dispatcher.permissions_check(class_name, request, domain=domain, is_navigation_check=True)):
                     continue
                 if report.show_in_navigation(
                         domain=domain, project=project, user=couch_user):
