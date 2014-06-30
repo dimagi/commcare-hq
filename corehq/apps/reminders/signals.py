edd42af7a897876dd4cdb2e7b15e5f01fceaf3a0
Giovanni Capalbo <gcapalbo@dimagi.com>
2014-06-30 11:03:56 -0400

    Put reminders case changed receiver in a try except

--- corehq/apps/reminders/signals.py
+++ corehq/apps/reminders/signals.py
@@ -3,11 +3,18 @@ from casexml.apps.case.models import CommCareCase
 from casexml.apps.case.signals import case_post_save
 from corehq.apps.reminders.models import CaseReminderHandler
 from corehq.apps.reminders.tasks import case_changed
+from dimagi.utils.logging import notify_exception
 
 def case_changed_receiver(sender, case, **kwargs):
-    handler_ids = CaseReminderHandler.get_handlers(case.domain, ids_only=True)
-    if len(handler_ids) > 0:
-        case_changed.delay(case._id, handler_ids)
+    try:
+        handler_ids = CaseReminderHandler.get_handlers(case.domain,
+            ids_only=True)
+        if len(handler_ids) > 0:
+            case_changed.delay(case._id, handler_ids)
+    except Exception:
+        notify_exception(None,
+            message="Error in reminders case changed receiver for case %s" %
+            case._id)
 
 case_post_save.connect(case_changed_receiver, CommCareCase)
 
