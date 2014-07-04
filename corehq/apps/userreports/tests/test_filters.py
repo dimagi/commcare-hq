fd18f371f812fd8b9d0c7b0be13739ac9e265ec0
Cory Zue <czue@dimagi.com>
2014-07-04 15:40:33 +0530

    add factory, AND, OR filters, and tests

--- corehq/apps/userreports/tests/test_filters.py
+++ corehq/apps/userreports/tests/test_filters.py
@@ -1,17 +1,20 @@
 from couchdbkit import Document
 from django.test import SimpleTestCase
 from corehq.apps.userreports.exceptions import BadSpecError
+from corehq.apps.userreports.factory import FilterFactory
 from corehq.apps.userreports.filters import PropertyMatchFilter
+from fluff.filters import ANDFilter, ORFilter
 
 
 class PropertyMatchFilterTest(SimpleTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.filter = PropertyMatchFilter.from_spec({
+    def setUp(self):
+        self.filter = PropertyMatchFilter.from_spec({
             'property_name': 'foo',
             'property_value': 'bar',
         })
+        self.assertEqual('foo', self.filter.property_name)
+        self.assertEqual('bar', self.filter.property_value)
 
     def testNoName(self):
         self.assertRaises(BadSpecError, PropertyMatchFilter.from_spec, {
@@ -43,3 +46,84 @@ class PropertyMatchFilterTest(SimpleTestCase):
 
     def testFilterMissing(self):
         self.assertFalse(self.filter.filter(Document(not_foo='bar')))
+
+    def testFromFactory(self):
+        from_factory = FilterFactory.from_spec({
+            'type': 'property_match',
+            'property_name': 'foo',
+            'property_value': 'bar',
+        })
+        self.assertTrue(isinstance(from_factory, PropertyMatchFilter))
+        self.assertEqual('foo', from_factory.property_name)
+        self.assertEqual('bar', from_factory.property_value)
+
+
+class ConfigurableANDFilterTest(SimpleTestCase):
+
+    def setUp(self):
+        self.filter = FilterFactory.from_spec({
+            "type": "and",
+            "filters": [
+                {
+                    "type": "property_match",
+                    "property_name": "foo",
+                    "property_value": "bar"
+                },
+                {
+                    "type": "property_match",
+                    "property_name": "foo2",
+                    "property_value": "bar2"
+                }
+            ]
+        })
+        self.assertTrue(isinstance(self.filter, ANDFilter))
+
+    def testFilterMatch(self):
+        self.assertTrue(self.filter.filter(Document(foo='bar', foo2='bar2')))
+
+    def testFilterPartialMatch(self):
+        self.assertFalse(self.filter.filter(Document(foo='bar', foo2='not bar2')))
+
+    def testFilterNoMatch(self):
+        self.assertFalse(self.filter.filter(Document(foo='not bar', foo2='not bar2')))
+
+    def testFilterMissingPartialMatch(self):
+        self.assertFalse(self.filter.filter(Document(foo='bar')))
+
+    def testFilterMissingAll(self):
+        self.assertFalse(self.filter.filter(Document(notfoo='not bar')))
+
+class ConfigurableORFilterTest(SimpleTestCase):
+
+    def setUp(self):
+        self.filter = FilterFactory.from_spec({
+            "type": "or",
+            "filters": [
+                {
+                    "type": "property_match",
+                    "property_name": "foo",
+                    "property_value": "bar"
+                },
+                {
+                    "type": "property_match",
+                    "property_name": "foo2",
+                    "property_value": "bar2"
+                }
+            ]
+        })
+        self.assertTrue(isinstance(self.filter, ORFilter))
+
+    def testFilterMatch(self):
+        self.assertTrue(self.filter.filter(Document(foo='bar', foo2='bar2')))
+
+    def testFilterPartialMatch(self):
+        self.assertTrue(self.filter.filter(Document(foo='bar', foo2='not bar2')))
+
+    def testFilterNoMatch(self):
+        self.assertFalse(self.filter.filter(Document(foo='not bar', foo2='not bar2')))
+
+    def testFilterMissingPartialMatch(self):
+        self.assertTrue(self.filter.filter(Document(foo='bar')))
+
+    def testFilterMissingAll(self):
+        self.assertFalse(self.filter.filter(Document(notfoo='not bar')))
