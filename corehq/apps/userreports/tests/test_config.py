b67e264c09aa07283e36fa1420c12c4dfc06d469
Cory Zue <czue@dimagi.com>
2014-07-04 20:12:21 +0530

    add stub model to hold the config + simple test and sample config

---
+++ corehq/apps/userreports/tests/test_config.py
@@ -0,0 +1,25 @@
+import json
+import os
+from couchdbkit import Document
+from django.test import SimpleTestCase
+from corehq.apps.userreports.models import IndicatorConfiguration
+
+
+class IndicatorConfigurationTest(SimpleTestCase):
+
+
+    def testLoadConfig(self):
+        folder = os.path.join(os.path.dirname(__file__), 'data', 'configs')
+        sample_file = os.path.join(folder, 'sample_config.json')
+        with open(sample_file) as f:
+            structure = json.loads(f.read())
+            config = IndicatorConfiguration.wrap(structure)
+            self.assertEqual('user-reports', config.domain)
+            self.assertEqual('sample', config.table_id)
+            self.assertTrue(config.filter.filter(Document(doc_type="CommCareCase", domain='user-reports', type='ticket')))
+            self.assertFalse(config.filter.filter(Document(doc_type="NotCommCareCase", domain='user-reports', type='ticket')))
+            self.assertFalse(config.filter.filter(Document(doc_type="CommCareCase", domain='not-user-reports', type='ticket')))
+            self.assertFalse(config.filter.filter(Document(doc_type="CommCareCase", domain='user-reports', type='not-ticket')))
+
+            # tbd, indicator checks
+            indicators = config.indicators
