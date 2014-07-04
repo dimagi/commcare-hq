951767a121d6ba90af901594c0160736e45611de
Cory Zue <czue@dimagi.com>
2014-07-04 15:53:38 +0530

    add test demonstrating ability to nest compound filters

--- corehq/apps/userreports/tests/test_filters.py
+++ corehq/apps/userreports/tests/test_filters.py
@@ -127,3 +127,61 @@ class ConfigurableORFilterTest(SimpleTestCase):
 
     def testFilterMissingAll(self):
         self.assertFalse(self.filter.filter(Document(notfoo='not bar')))
+
+class ComplexFilterTest(SimpleTestCase):
+
+    def testComplexStructure(self):
+        # in slightly more compact format:
+        # ((foo=bar) or (foo1=bar1 and foo2=bar2 and (foo3=bar3 or foo4=bar4)))
+        filter = FilterFactory.from_spec({
+            "type": "or",
+            "filters": [
+                {
+                    "type": "property_match",
+                    "property_name": "foo",
+                    "property_value": "bar"
+                },
+                {
+                    "type": "and",
+                    "filters": [
+                        {
+                            "type": "property_match",
+                            "property_name": "foo1",
+                            "property_value": "bar1"
+                        },
+                        {
+                            "type": "property_match",
+                            "property_name": "foo2",
+                            "property_value": "bar2"
+                        },
+                        {
+                            "type": "or",
+                            "filters": [
+                                {
+                                    "type": "property_match",
+                                    "property_name": "foo3",
+                                    "property_value": "bar3"
+                                },
+                                {
+                                    "type": "property_match",
+                                    "property_name": "foo4",
+                                    "property_value": "bar4"
+                                }
+                            ]
+                        },
+                    ]
+                },
+            ]
+        })
+        # first level or
+        self.assertTrue(filter.filter(Document(foo='bar')))
+        # first level and with both or's
+        self.assertTrue(filter.filter(Document(foo1='bar1', foo2='bar2', foo3='bar3')))
+        self.assertTrue(filter.filter(Document(foo1='bar1', foo2='bar2', foo4='bar4')))
+
+        # first and not right
+        self.assertFalse(filter.filter(Document(foo1='not bar1', foo2='bar2', foo3='bar3')))
+        # second and not right
+        self.assertFalse(filter.filter(Document(foo1='bar1', foo2='not bar2', foo3='bar3')))
+        # last and not right
+        self.assertFalse(filter.filter(Document(foo1='bar1', foo2='bar2', foo3='not bar3', foo4='not bar4')))
