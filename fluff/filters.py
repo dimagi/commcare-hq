b80ca85d6ef6244d6090751ac3e409c1a4ed179a
Cory Zue <czue@dimagi.com>
2014-07-04 18:43:17 +0530

    move CustomFilter into fluff

--- fluff/filters.py
+++ fluff/filters.py
@@ -38,3 +38,15 @@ class ORFilter(Filter):
     def filter(self, item):
         return any(filter.filter(item) for filter in self.filters)
 
+
+class CustomFilter(Filter):
+    """
+    This filter allows you to pass in a function reference to use as the filter
+
+    e.g. CustomFilter(lambda f: f.gender in ['male', 'female'])
+    """
+    def __init__(self, filter):
+        self._filter = filter
+
+    def filter(self, item):
+        return self._filter(item)
