a9508f3709365e47e5c1429026d5676afb476ae0
Cory Zue <czue@dimagi.com>
2014-07-08 17:10:06 -0400

    fix test that should have caught consumption issue

--- corehq/apps/commtrack/tests/test_stock_state.py
+++ corehq/apps/commtrack/tests/test_stock_state.py
@@ -64,7 +64,7 @@ class StockStateConsumptionTest(StockStateTest):
         self.assertEqual(None, state.get_daily_consumption())
 
     def test_pre_set_defaults(self):
-        set_default_monthly_consumption_for_domain(self.domain.name, 50)
+        set_default_monthly_consumption_for_domain(self.domain.name, 5 * 30)
         self.report(25, 0)
 
         state = StockState.objects.get(
@@ -73,11 +73,11 @@ class StockStateConsumptionTest(StockStateTest):
             product_id=self.products[0]._id,
         )
 
-        self.assertEqual(50, float(state.get_daily_consumption()))
+        self.assertEqual(5, float(state.get_daily_consumption()))
 
     def test_defaults_set_after_report(self):
         self.report(25, 0)
-        set_default_monthly_consumption_for_domain(self.domain.name, 50)
+        set_default_monthly_consumption_for_domain(self.domain.name, 5 * 30)
 
         state = StockState.objects.get(
             section_id='stock',
@@ -85,4 +85,4 @@ class StockStateConsumptionTest(StockStateTest):
             product_id=self.products[0]._id,
         )
 
-        self.assertEqual(50, float(state.get_daily_consumption()))
+        self.assertEqual(5, float(state.get_daily_consumption()))
