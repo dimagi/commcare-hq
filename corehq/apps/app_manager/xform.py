b6f028cc47c17c200c25a69b600675e735594409
Daniel Roberts <droberts@dimagi.com>
2014-07-14 16:50:20 -0400

    add pollsensor in xforms for auto-gps and warm up

--- corehq/apps/app_manager/xform.py
+++ corehq/apps/app_manager/xform.py
@@ -832,11 +832,11 @@ class XForm(WrappedNode):
             self.add_case_and_meta_1(form)
         else:
             self.create_casexml_2(form)
-            self.add_meta_2()
+            self.add_meta_2(form)
 
     def add_case_and_meta_advanced(self, form):
         self.create_casexml_2_advanced(form)
-        self.add_meta_2()
+        self.add_meta_2(form)
 
     def already_has_meta(self):
         meta_blocks = set()
@@ -847,7 +847,7 @@ class XForm(WrappedNode):
 
         return meta_blocks
 
-    def add_meta_2(self):
+    def add_meta_2(self, form):
         case_parent = self.data_node
 
         # Test all of the possibilities so that we don't end up with two "meta" blocks
@@ -860,7 +860,7 @@ class XForm(WrappedNode):
         nsmap = {None: orx, 'cc': namespaces['cc'][1:-1]}
 
         meta = ET.Element("{orx}meta".format(**namespaces), nsmap=nsmap)
-        for tag in (
+        tags = (
             '{orx}deviceID',
             '{orx}timeStart',
             '{orx}timeEnd',
@@ -868,7 +868,10 @@ class XForm(WrappedNode):
             '{orx}userID',
             '{orx}instanceID',
             '{cc}appVersion',
-        ):
+        )
+        if form.get_auto_gps_capture():
+            tags += ('{cc}location',)
+        for tag in tags:
             meta.append(ET.Element(tag.format(**namespaces), nsmap=nsmap))
 
         case_parent.append(meta)
@@ -904,6 +907,11 @@ class XForm(WrappedNode):
             ref="meta/appVersion",
             value="instance('commcaresession')/session/context/appversion"
         )
+
+        if form.get_auto_gps_capture():
+            self.add_pollsensor(ref="meta/location")
+        elif self.model_node.findall("{f}bind[@type='geopoint']"):
+            self.add_pollsensor()
 
     def add_case_and_meta_1(self, form):
         case = self.case_node
@@ -1018,6 +1026,18 @@ class XForm(WrappedNode):
         self.model_node.append(_make_elem('setvalue', {'ref': ref, 'value': value, 'event': event}))
         if type:
             self.add_bind(nodeset=ref, type=type)
+
+    def add_pollsensor(self, event="xforms-ready", ref=None):
+        """
+        <pollsensor event="xforms-ready" ref="/data/meta/location" />
+        <bind nodeset="/data/meta/location" type="geopoint"/>
+        """
+        if ref:
+            self.model_node.append(_make_elem('pollsensor',
+                                              {'event': event, 'ref': ref}))
+            self.add_bind(nodeset=ref, type="geopoint")
+        else:
+            self.model_node.append(_make_elem('pollsensor', {'event': event}))
 
     def action_relevance(self, condition):
         if condition.type == 'always':
@@ -1653,7 +1673,7 @@ class XForm(WrappedNode):
     def add_care_plan(self, form):
         from const import CAREPLAN_GOAL, CAREPLAN_TASK
         from corehq.apps.app_manager.util import split_path
-        self.add_meta_2()
+        self.add_meta_2(form)
         self.add_instance('casedb', src='jr://instance/casedb')
 
         for property, nodeset in form.case_preload.items():
