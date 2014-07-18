f0b1736a7b20474af95091620dbe46d59b6084cf
Cory Zue <czue@dimagi.com>
2014-07-18 09:14:21 -0400

    add guess of date to recent changes feed

--- corehq/apps/hqadmin/history.py
+++ corehq/apps/hqadmin/history.py
@@ -10,4 +10,19 @@ def get_recent_changes(db, limit=500):
             'rev': row['changes'][0]['rev'],
             'domain': row['doc'].get('domain', '[no domain]'),
             'doc_type': row['doc'].get('doc_type', '[no doc_type]'),
+            'date': _guess_date(row['doc']),
         }
+
+def _guess_date(doc):
+    # note: very quick and dirty approach to this
+    properties_to_check = (
+        'server_modified_on',  # cases
+        'received_on',  # forms
+        'built_on',  # built apps
+        'date',  # shotgun
+    )
+    for p in properties_to_check:
+        val = doc.get(p)
+        if val:
+            return val
+    return '[no date]'
