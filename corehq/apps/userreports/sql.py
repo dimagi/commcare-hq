72b4caaf68380156bfc675f4471562fdf09b8f68
Cory Zue <czue@dimagi.com>
2014-07-21 16:06:34 -0400

    refactoring to split out sql stuff from pillow and remove 1:1 mapping

--- corehq/apps/userreports/sql.py
+++ corehq/apps/userreports/sql.py
@@ -1,8 +1,43 @@
 import sqlalchemy
 from django.conf import settings
+from dimagi.utils.decorators.memoized import memoized
 from fluff.util import get_column_type
 
 metadata = sqlalchemy.MetaData()
+
+
+class IndicatorSqlAdapter(object):
+
+    def __init__(self, engine, config):
+        self.engine = engine
+        self.config = config
+
+    @memoized
+    def get_table(self):
+        return get_indicator_table(self.config)
+
+    def rebuild_table(self):
+        rebuild_table(self.engine, self.get_table())
+
+    def drop_table(self):
+        with self.engine.begin() as connection:
+            self.get_table().drop(connection, checkfirst=True)
+
+    def save(self, doc):
+        indicators = self.config.get_values(doc)
+        table = self.get_table()
+        if indicators:
+            connection = self.engine.connect()
+            try:
+                # delete all existing rows for this doc to ensure we aren't left with stale data
+                delete = table.delete(table.c.doc_id == doc['_id'])
+                connection.execute(delete)
+
+                all_values = {i.column.id: i.value for i in indicators}
+                insert = table.insert().values(**all_values)
+                connection.execute(insert)
+            finally:
+                connection.close()
 
 
 def get_indicator_table(indicator_config):
@@ -31,8 +66,7 @@ def get_engine():
     return sqlalchemy.create_engine(settings.SQL_REPORTING_DATABASE_URL)
 
 
-def rebuild_table(table):
-    engine = get_engine()
+def rebuild_table(engine, table):
     with engine.begin() as connection:
         table.drop(connection, checkfirst=True)
         table.create(connection)
--- corehq/apps/userreports/tests/test_pillow.py
+++ corehq/apps/userreports/tests/test_pillow.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 import sqlalchemy
 from corehq.apps.userreports.models import IndicatorConfiguration
 from corehq.apps.userreports.pillow import ConfigurableIndicatorPillow
-from corehq.apps.userreports.sql import rebuild_table
+from corehq.apps.userreports.sql import IndicatorSqlAdapter
 from corehq.apps.userreports.tests import get_sample_doc_and_indicators
 
 
@@ -13,29 +13,29 @@ class IndicatorPillowTest(TestCase):
     def setUp(self):
         folder = os.path.join(os.path.dirname(__file__), 'data', 'configs')
         sample_file = os.path.join(folder, 'sample_config.json')
+        self.pillow = ConfigurableIndicatorPillow()
+        self.engine = self.pillow.get_sql_engine()
         with open(sample_file) as f:
             structure = json.loads(f.read())
             self.config = IndicatorConfiguration.wrap(structure)
-            self.pillow = ConfigurableIndicatorPillow(self.config)
+            self.pillow.bootstrap(configs=[self.config])
 
-        # todo: figure out the right place for this to get called
-        rebuild_table(self.pillow._table)
-        self.engine = self.pillow.get_sql_engine()
+        self.adapter = IndicatorSqlAdapter(self.engine, self.config)
+        self.adapter.rebuild_table()
 
     def tearDown(self):
-        # todo: this is a little sketchy/coupled
-        with self.engine.begin() as connection:
-            self.pillow._table.drop(connection, checkfirst=True)
-        self.pillow._engine.dispose()
+        self.adapter.drop_table()
+        self.engine.dispose()
 
     def testFilter(self):
+        # note: this is a silly test now that python_filter always returns true
         not_matching = [
             dict(doc_type="NotCommCareCase", domain='user-reports', type='ticket'),
             dict(doc_type="CommCareCase", domain='not-user-reports', type='ticket'),
             dict(doc_type="CommCareCase", domain='user-reports', type='not-ticket'),
         ]
         for document in not_matching:
-            self.assertFalse(self.pillow.python_filter(document))
+            self.assertTrue(self.pillow.python_filter(document))
 
         self.assertTrue(self.pillow.python_filter(dict(doc_type="CommCareCase", domain='user-reports', type='ticket')))
 
@@ -44,7 +44,7 @@ class IndicatorPillowTest(TestCase):
         sample_doc, expected_indicators = get_sample_doc_and_indicators()
         self.pillow.change_transport(sample_doc)
         with self.engine.begin() as connection:
-            rows = connection.execute(sqlalchemy.select([self.pillow._table]))
+            rows = connection.execute(sqlalchemy.select([self.adapter.get_table()]))
             self.assertEqual(1, rows.rowcount)
             row = rows.fetchone()
             for k, v in row.items():
