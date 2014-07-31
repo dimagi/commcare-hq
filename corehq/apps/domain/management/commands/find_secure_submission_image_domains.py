8f24d774227dca13500c0db0c10771d6b4ee7141
Tyler Wymer <twymer@gmail.com>
2014-07-31 13:31:10 -0400

    Add audio/video support and bail on findings

--- corehq/apps/domain/management/commands/find_secure_submission_image_domains.py
+++ corehq/apps/domain/management/commands/find_secure_submission_image_domains.py
@@ -6,6 +6,20 @@ import csv
 class Command(BaseCommand):
     help = 'Find domains with secure submissions and image questions'
 
+    def check_domain(self, domain, csv_writer):
+        if domain.secure_submissions:
+            for app in domain.full_applications(include_builds=False):
+                for module in app.modules:
+                    for form in module.forms:
+                        for question in form.get_questions(app.langs):
+                            if question['type'] in ('Image', 'Audio', 'Video'):
+                                csv_writer.writerow([
+                                    domain.name,
+                                    app.name,
+                                    domain.creating_user
+                                ])
+                                return
+
     def handle(self, *args, **options):
         with open('domain_results.csv', 'wb+') as csvfile:
             csv_writer = csv.writer(
@@ -18,14 +32,4 @@ class Command(BaseCommand):
             csv_writer.writerow(['domain', 'app', 'domain_creator'])
 
             for domain in Domain.get_all(include_docs=True):
-                if domain.secure_submissions:
-                    for app in domain.full_applications(include_builds=False):
-                        for module in app.modules:
-                            for form in module.forms:
-                                for question in form.get_questions(app.langs):
-                                    if question['type'] == 'Image':
-                                        csv_writer.writerow([
-                                            domain.name,
-                                            app.name,
-                                            domain.creating_user
-                                        ])
+                self.check_domain(domain, csv_writer)
