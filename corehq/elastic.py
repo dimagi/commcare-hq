23159a2dd680eb91d36e13311def5ba1bf47383e
Nick Pellegrino <npellegrino@dimagi.com>
2014-07-31 14:54:19 -0400

    fix query

--- corehq/elastic.py
+++ corehq/elastic.py
@@ -83,16 +83,16 @@ def get_stats_data(domains, histo_type, datespan, interval="day", user_type_mobi
         q["filter"]["and"].extend(ADD_TO_ES_FILTER.get(histo_type, [])[:])
         if user_type_mobile is not None:
             from corehq.apps.users.models import WebUser, CommCareUser
-            q["filter"]["and"].extend(
-                {
-                    "terms": {
-                        "form.meta.userID":
-                            [mobile_user._id for mobile_user in CommCareUser.all() if mobile_user.doc_type == "CommCareUser"]
-                            if user_type_mobile else
-                            [web_user._id for web_user in WebUser.all() if web_user.doc_type == "WebUser"]
-                    }
+            q["filter"]["and"].extend([{
+                "terms": {
+                    "form.meta.userID":
+                        [mobile_user._id for mobile_user in CommCareUser.all()
+                         if mobile_user.doc_type == "CommCareUser"]
+                        if user_type_mobile else
+                        [web_user._id for web_user in WebUser.all()
+                         if web_user.doc_type == "WebUser"]
                 }
-            )
+            }])
 
         return es_query(q=q, es_url=ES_URLS[histo_type], size=0)["hits"]["total"]
 
