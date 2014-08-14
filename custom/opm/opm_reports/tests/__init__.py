7876aef319d6dc830b24cf5c1fa99d6180058cc6
Ethan Soergel <esoergel@gmail.com>
2014-08-14 16:10:49 -0400

    Add birth spacing tests and logic rewrite

--- custom/opm/opm_reports/tests/__init__.py
+++ custom/opm/opm_reports/tests/__init__.py
@@ -1,7 +1,8 @@
 from .case_reports import *
+from .test_birth_spacing import *
+from .test_child_conditions import *
+from .test_preg_conditions import *
 from .test_status import *
 from .test_vhnd import *
 from .test_windows_and_months import *
-from .test_child_conditions import *
-from .test_preg_conditions import *
 # from .old_regression_tests import TestIncentive
