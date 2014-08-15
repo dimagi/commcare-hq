ad280f752320ba204483b4aefaab6bc53818edcb
Cory Zue <czue@dimagi.com>
2014-08-15 16:52:45 -0400

    start refactoring stuff out into a base class

--- custom/opm/opm_reports/tests/test_child_condition_four.py
+++ custom/opm/opm_reports/tests/test_child_condition_four.py
@@ -5,7 +5,26 @@ from custom.opm.opm_reports.tests.case_reports import OPMCaseReportTestBase, OPM
     offset_date
 
 
-class TestChildMeasles(OPMCaseReportTestBase):
+class ConditionFourTestMixin(object):
+    """
+    Shared test class for two conditions that have significant overlap
+    """
+    expected_window = None
+
+    @property
+    def valid_dod(self):
+        return offset_date(self.report_date, -(self.expected_window - 1))
+
+    def valid_form(self, received_on):
+        return self.valid_form_function(received_on)
+
+
+class TestChildMeasles(OPMCaseReportTestBase, ConditionFourTestMixin):
+    expected_window = 12
+
+    def setUp(self):
+        super(TestChildMeasles, self).setUp()
+        self.valid_form_function = _valid_measles_form
 
     def test_not_in_window(self):
         for dod in (date(2014, 3, 10), date(2013, 3, 10)):
@@ -19,54 +38,50 @@ class TestChildMeasles(OPMCaseReportTestBase):
     def test_in_window_no_data(self):
         case = OPMCase(
             forms=[],
-            dod=offset_date(self.report_date, -11)
+            dod=self.valid_dod
         )
         row = MockCaseRow(case, self.report)
         self.assertEqual(False, row.child_received_measles_vaccine)
 
     def test_in_window_with_data(self):
-        dod = offset_date(self.report_date, -11)
         for month in (10, 11, 12):
-            form_date = datetime.combine(offset_date(dod, month), time())
+            form_date = datetime.combine(offset_date(self.valid_dod, month), time())
             case = OPMCase(
-                forms=[_valid_form(form_date)],
-                dod=dod,
+                forms=[self.valid_form(form_date)],
+                dod=self.valid_dod,
             )
             row = MockCaseRow(case, self.report)
             self.assertEqual(True, row.child_received_measles_vaccine)
 
     def test_one_month_extension_valid(self):
-        dod = offset_date(self.report_date, -11)
         form_date = offset_date(self.report_datetime, 1)
         case = OPMCase(
-            forms=[_valid_form(form_date)],
-            dod=dod,
+            forms=[self.valid_form(form_date)],
+            dod=self.valid_dod,
         )
         row = MockCaseRow(case, self.report)
         self.assertEqual(True, row.child_received_measles_vaccine)
 
     def test_two_month_extension_not_valid(self):
-        dod = offset_date(self.report_date, -11)
         form_date = offset_date(self.report_datetime, 2)
         case = OPMCase(
-            forms=[_valid_form(form_date)],
-            dod=dod,
+            forms=[self.valid_form(form_date)],
+            dod=self.valid_dod,
         )
         row = MockCaseRow(case, self.report)
         self.assertEqual(False, row.child_received_measles_vaccine)
 
     def test_before_window_not_valid(self):
-        dod = offset_date(self.report_date, -11)
-        form_date = datetime.combine(offset_date(dod, 9), time())
+        form_date = datetime.combine(offset_date(self.valid_dod, 9), time())
         case = OPMCase(
-            forms=[_valid_form(form_date)],
-            dod=dod,
+            forms=[self.valid_form(form_date)],
+            dod=self.valid_dod,
         )
         row = MockCaseRow(case, self.report)
         self.assertEqual(False, row.child_received_measles_vaccine)
 
 
-def _valid_form(received_on):
+def _valid_measles_form(received_on):
     return XFormInstance(
         form={
             'child1': {
@@ -76,3 +91,4 @@ def _valid_form(received_on):
         received_on=received_on,
         xmlns=CFU1_XMLNS,
     )
+
