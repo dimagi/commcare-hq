b47294fda652f1f57fd9c58208f9d2c016529c0c
Cory Zue <czue@dimagi.com>
2014-08-16 18:23:39 -0400

    only return vhnd info if the mother meets the right status

--- custom/opm/opm_reports/beneficiary.py
+++ custom/opm/opm_reports/beneficiary.py
@@ -168,35 +168,37 @@ class OPMCaseRow(object):
 
     @property
     def preg_attended_vhnd(self):
-        # in month 9 they always meet this condition
-        if self.preg_month == 9:
-            return True
-        elif not self.vhnd_available:
-            return True
-        elif 9 > self.preg_month > 3:
-            vhnd_attendance = {
-                4: self.case_property('attendance_vhnd_1', 0),
-                5: self.case_property('attendance_vhnd_2', 0),
-                6: self.case_property('attendance_vhnd_3', 0),
-                7: self.case_property('month_7_attended', 0),
-                8: self.case_property('month_8_attended', 0)
-            }
-            return vhnd_attendance[self.preg_month] == '1'
-        else:
-            return False
+        if self.status == 'pregnant':
+            # in month 9 they always meet this condition
+            if self.preg_month == 9:
+                return True
+            if not self.vhnd_available:
+                return True
+            elif 9 > self.preg_month > 3:
+                vhnd_attendance = {
+                    4: self.case_property('attendance_vhnd_1', 0),
+                    5: self.case_property('attendance_vhnd_2', 0),
+                    6: self.case_property('attendance_vhnd_3', 0),
+                    7: self.case_property('month_7_attended', 0),
+                    8: self.case_property('month_8_attended', 0)
+                }
+                return vhnd_attendance[self.preg_month] == '1'
+            else:
+                return False
 
     @property
     def child_attended_vhnd(self):
-        if self.child_age == 1:
-            return True
-        elif not self.vhnd_available:
-            return True
-        else:
-            return any(
-                form.form.get(indexed_child('child1_vhndattend_calc', self.child_index)) == 'received'
-                for form in self.forms
-                if form.xmlns in CHILDREN_FORMS and self.form_in_range(form)
-            )
+        if self.status == 'mother':
+            if self.child_age == 1:
+                return True
+            elif not self.vhnd_available:
+                return True
+            else:
+                return any(
+                    form.form.get(indexed_child('child1_vhndattend_calc', self.child_index)) == 'received'
+                    for form in self.forms
+                    if form.xmlns in CHILDREN_FORMS and self.form_in_range(form)
+                )
 
     @property
     def preg_weighed(self):
