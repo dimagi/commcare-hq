c143b3393be418ea3ae2d94c9c67c72ddfd0080c
Tyler Wymer <twymer@gmail.com>
2014-08-20 17:43:15 -0400

    Selective sync for locations

---
+++ corehq/apps/locations/management/commands/location_last_modified.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from corehq.apps.locations.models import Location
+from dimagi.utils.couch.database import iter_docs
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Populate last_modified field for locations'
+
+    def handle(self, *args, **options):
+        self.stdout.write("Processing locations...\n")
+
+        relevant_ids = set([r['id'] for r in Location.get_db().view(
+            'commtrack/locations_by_code',
+            reduce=False,
+        ).all()])
+
+        to_save = []
+
+        for location in iter_docs(Location.get_db(), relevant_ids):
+            if 'last_modified' not in location or not location['last_modified']:
+                location['last_modified'] = datetime.now().isoformat()
+                to_save.append(location)
+
+                if len(to_save) > 500:
+                    Location.get_db().bulk_save(to_save)
+                    to_save = []
+
+        if to_save:
+            Location.get_db().bulk_save(to_save)
