027c1460ef35d0d0573b975405231539026426f8
Kamil Krampa <kkrampa@soldevelo.com>
2014-08-26 09:44:11 +0200

    Added decorator to produce structured response

--- corehq/util/view_utils.py
+++ corehq/util/view_utils.py
@@ -1,2 +1,23 @@
+import json
+import traceback
+from django.http import HttpResponse
+
+
 def set_file_download(response, filename):
     response["Content-Disposition"] = 'attachment; filename="%s"' % filename
+
+
+def produce_structured_response(f):
+    def inner(request, *args, **kwargs):
+        try:
+            response = f(request, *args, **kwargs)
+        except Exception as e:
+            import sys
+            the_trace = '\n'.join(traceback.format_exception(*(sys.exc_info())))
+            data = {
+                'error_message': e.message,
+                'traceback': the_trace
+            }
+            return HttpResponse(status=500, content=json.dumps(data), content_type='application/json')
+        return response
+    return inner
