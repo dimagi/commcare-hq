ca2b02d551e9bb4c8625ae79f7878892673fa731
Jonathan Emord <jonathan.emord@gmail.com>
2014-08-27 15:19:37 -0400

    Add CommCare, CommTrack filters for DomainES

--- corehq/apps/es/domains.py
+++ corehq/apps/es/domains.py
@@ -9,7 +9,9 @@ class DomainES(HQESQuery):
     def builtin_filters(self):
         return [
             real_domains,
+            commcare_domains,
             commconnect_domains,
+            commtrack_domains,
             created,
         ] + super(DomainES, self).builtin_filters
 
@@ -18,8 +20,17 @@ def real_domains():
     return filters.term("is_test", False)
 
 
+def commcare_domains():
+    return filters.AND(filters.term("commconnect_enabled", False),
+                       filters.term("commtrack_enabled", False))
+
+
 def commconnect_domains():
     return filters.term("commconnect_enabled", True)
+
+
+def commtrack_domains():
+    return filters.term("commtrack_enabled", True)
 
 
 def created(gt=None, gte=None, lt=None, lte=None):
