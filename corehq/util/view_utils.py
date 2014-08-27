3be40ad033c6a6b8b16bc559bed1f0e0afeb54fe
Kamil Krampa <kkrampa@soldevelo.com>
2014-08-27 08:50:42 +0200

    Removed decorator, added jsonview module

--- corehq/util/view_utils.py
+++ corehq/util/view_utils.py
@@ -1,23 +1,2 @@
-import json
-import traceback
-from django.http import HttpResponse
-
-
 def set_file_download(response, filename):
     response["Content-Disposition"] = 'attachment; filename="%s"' % filename
-
-
-def json_error(f):
-    def inner(request, *args, **kwargs):
-        try:
-            response = f(request, *args, **kwargs)
-        except Exception as e:
-            import sys
-            the_trace = '\n'.join(traceback.format_exception(*(sys.exc_info())))
-            data = {
-                'error_message': unicode(e),
-                'traceback': the_trace
-            }
-            return HttpResponse(status=500, content=json.dumps(data), content_type='application/json')
-        return response
-    return inner
