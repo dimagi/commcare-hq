8ee56e020a6e8110cf375781d3eabea2c87eefe5
Kamil Krampa <kkrampa@soldevelo.com>
2014-08-28 09:34:20 +0200

    Revert "Removed decorator, added jsonview module"
    This reverts commit 3be40ad033c6a6b8b16bc559bed1f0e0afeb54fe.

--- corehq/apps/ota/views.py
+++ corehq/apps/ota/views.py
@@ -1,12 +1,12 @@
-from jsonview.decorators import json_view
 from corehq.apps.domain.models import Domain
 from corehq.apps.users.models import CouchUser
+from corehq.util.view_utils import json_error
 from django_digest.decorators import *
 from casexml.apps.phone.restore import RestoreConfig
 from django.http import HttpResponse
 
 
-@json_view
+@json_error
 @httpdigest
 def restore(request, domain):
     """
