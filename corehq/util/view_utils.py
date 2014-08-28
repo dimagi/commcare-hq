104c0cc387b8739288d0aa3c77cbfd39d1eb2519
Kamil Krampa <kkrampa@soldevelo.com>
2014-08-28 15:32:33 +0200

    Code tweak

--- corehq/util/view_utils.py
+++ corehq/util/view_utils.py
@@ -10,13 +10,12 @@ def set_file_download(response, filename):
 def json_error(f):
     def inner(request, *args, **kwargs):
         try:
-            response = f(request, *args, **kwargs)
+            return f(request, *args, **kwargs)
         except Exception as e:
             data = {
                 'error_message': unicode(e),
             }
             return HttpResponse(status=500, content=json.dumps(data), content_type='application/json')
-        return response
     return inner
 
 
