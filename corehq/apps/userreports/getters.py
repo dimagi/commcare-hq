c9095cc6b533683b51d630e241d1919d6e8d158d
Cory Zue <czue@dimagi.com>
2014-08-29 13:40:27 -0400

    add support for nested properties, start refactoring to jsonobject, tests

--- corehq/apps/userreports/getters.py
+++ corehq/apps/userreports/getters.py
@@ -31,3 +31,42 @@ class DictGetter(object):
             return item[self.property_name]
         except KeyError:
             return None
+
+
+class NestedDictGetter(object):
+    """
+    Gets a property from a series of nested dicts. Takes in a fully qualified
+    path to the value in question in the form of a list. Returns None if the path
+    does not exist in the dict.
+    """
+
+    def __init__(self, property_path):
+        self.property_path = property_path
+
+    def __call__(self, item):
+        if not isinstance(item, dict):
+            return None
+        try:
+            return recursive_lookup(item, self.property_path)
+        except KeyError:
+            return None
+
+
+def recursive_lookup(dict_object, keys):
+    """
+    Given a dict object and list of keys, nest into those keys.
+    Raises KeyError if the path isn't found.
+    >>> recursive_lookup({'foo': 1}, ['foo'])
+    1
+    >>> recursive_lookup({'foo': {'bar': 1}}, ['foo'])
+    {'bar': 1}
+    >>> recursive_lookup({'foo': {'bar': 1}}, ['foo', 'bar'])
+    1
+    """
+    if not keys or not isinstance(keys, list):
+        raise ValueError('Keys must be a non-empty list!')
+
+    if len(keys) == 1:
+        return dict_object[keys[0]]
+    else:
+        return recursive_lookup(dict_object[keys[0]], keys[1:])
