d659bb83f611e7b31a1464040b70ba59157293b9
Cory Zue <czue@dimagi.com>
2014-08-29 14:45:36 -0400

    end to end wiring of filters through to the database

--- corehq/apps/userreports/reports/data_source.py
+++ corehq/apps/userreports/reports/data_source.py
@@ -1,4 +1,3 @@
-import random
 from corehq.apps.reports.sqlreport import SqlData
 from corehq.apps.userreports.sql import get_table_name
 from dimagi.utils.decorators.memoized import memoized
@@ -6,15 +5,24 @@ from dimagi.utils.decorators.memoized import memoized
 
 class ConfigurableReportDataSource(SqlData):
 
-    def __init__(self, domain, table_id, aggregation_columns, columns):
+    def __init__(self, domain, table_id, filters, aggregation_columns, columns):
         self.table_name = get_table_name(domain, table_id)
+        self._filters = {f.slug: f for f in filters}
+        self._filter_values = {}
         self.aggregation_columns = aggregation_columns
         self.column_configs = columns
 
     @property
     def filters(self):
-        # todo: figure out how to wire these in
-        return []
+        return [fv.to_sql_filter() for _, fv in self._filter_values.items()]
+
+    def set_filter_values(self, filter_values):
+        for filter_slug, value in filter_values.items():
+            self._filter_values[filter_slug] = self._filters[filter_slug].create_filter_value(value)
+
+    @property
+    def filter_values(self):
+        return {k: v for _, fv in self._filter_values.items() for k, v in fv.to_sql_values().items()}
 
     @property
     def group_by(self):
