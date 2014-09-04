82b2e01de69312e6436760c5eedc759b4fa04ce2
Ethan Soergel <esoergel@gmail.com>
2014-09-04 17:19:08 -0400

    Add validation for Custom Data Fields

--- corehq/apps/custom_fields/views.py
+++ corehq/apps/custom_fields/views.py
@@ -1,6 +1,8 @@
 import json
 
 from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_slug
 from django.utils.translation import ugettext as _, ugettext_noop
 from django import forms
 
@@ -24,20 +26,29 @@ class CustomDataFieldsForm(forms.Form):
             data_field_form = CustomDataFieldForm(raw_data_field)
             data_field_form.is_valid()
             data_fields.append(data_field_form.cleaned_data)
-            errors.append(data_field_form.errors)
+            if data_field_form.errors:
+                errors.append(data_field_form.errors)
+        if errors:
+            # TODO use actual error message(s)
+            raise ValidationError(_("All fields are required"))
         return data_fields
 
 
 class CustomDataFieldForm(forms.Form):
-    label = forms.CharField()
-    slug = forms.CharField()
-    is_required = forms.BooleanField()
+    label = forms.CharField(required=True)
+    slug = forms.CharField(required=True, validators=[validate_slug])
+    is_required = forms.BooleanField(required=False)
+
+    def clean_label(self):
+        return self.cleaned_data['label']
+
+    def clean_slug(self):
+        return self.cleaned_data['slug']
 
 
 class CustomFieldsMixin(object):
     urlname = None
     template_name = "custom_fields/custom_fields.html"
-    # form_class = CustomFieldsForm
     page_name = ugettext_noop("Edit Custom Fields")
     doc_type = None
 
@@ -49,10 +60,7 @@ class CustomFieldsMixin(object):
         if definition:
             return definition.fields
         else:
-            return [
-                {"slug": "dob", "label": "not found", "is_required": True},
-                {"slug": "gender", "label": "Gender", "is_required": False},
-            ]
+            return []
 
     def save_custom_fields(self):
         definition = self.get_definition() or CustomFieldsDefinition()
@@ -74,15 +82,23 @@ class CustomFieldsMixin(object):
     @property
     def page_context(self):
         return {
-            "custom_fields": self.get_custom_fields(),
+            "custom_fields": json.loads(self.form.data['data_fields']),
+            "custom_fields_form": self.form,
         }
 
     @property
     @memoized
     def form(self):
-        return CustomDataFieldsForm(self.request.POST)
+        if self.request.method == "POST":
+            return CustomDataFieldsForm(self.request.POST)
+        else:
+            serialized = json.dumps([field.to_json() for field in self.get_custom_fields()])
+            return CustomDataFieldsForm({'data_fields': serialized})
 
     def post(self, request, *args, **kwargs):
         if self.form.is_valid():
             self.save_custom_fields()
-        return self.get(request, success=True, *args, **kwargs)
+            return self.get(request, success=True, *args, **kwargs)
+        else:
+            errors = self.form.errors.get('data_fields')
+            return self.get(request, *args, **kwargs)
