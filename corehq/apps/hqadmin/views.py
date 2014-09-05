432ca0754b33efbf6b0f27e9231cc6485861a775
Nick Pellegrino <npellegrino@dimagi.com>
2014-09-05 14:45:16 -0400

    add get_all_subscriptions_stats_data function

--- corehq/apps/hqadmin/views.py
+++ corehq/apps/hqadmin/views.py
@@ -32,7 +32,6 @@ from django.http import (
 from restkit import Resource
 
 from casexml.apps.case.models import CommCareCase
-from corehq.apps.accounting.models import SoftwarePlanEdition
 from couchexport.export import export_raw, export_from_tables
 from couchexport.shortcuts import export_response
 from couchexport.models import Format
@@ -69,7 +68,7 @@ from corehq.apps.hqadmin.reporting.reports import (
     get_domain_stats_data,
     get_total_clients_data,
     commtrack_form_submissions,
-    get_subscription_stats_data,
+    get_all_subscriptions_stats_data,
 )
 from corehq.apps.ota.views import get_restore_response, get_restore_params
 from corehq.apps.reports.datatables import DataTablesColumn, DataTablesHeader, DTSortType
@@ -935,27 +934,7 @@ def stats_data(request):
 
     if histo_type == "subscriptions":
         params.update(params_es)
-        return json_response({
-            'histo_data': {
-                software_plan_edition_tuple[0]: add_blank_data(
-                    get_subscription_stats_data(
-                        params,
-                        request.datespan,
-                        interval=interval,
-                        software_plan_edition=software_plan_edition_tuple[0],
-                    ),
-                    request.datespan.startdate,
-                    request.datespan.enddate
-                )
-                for software_plan_edition_tuple in SoftwarePlanEdition.CHOICES
-            },
-            'initial_values': {
-                software_plan_edition_tuple[0]: 0
-                for software_plan_edition_tuple in SoftwarePlanEdition.CHOICES
-            },
-            'startdate': request.datespan.startdate_key_utc,
-            'enddate': request.datespan.enddate_key_utc,
-        })
+        return json_response(get_all_subscriptions_stats_data(params, request.datespan, interval=interval))
 
     if histo_type == "active_domains":
         stats_data = get_active_domain_stats_data(
