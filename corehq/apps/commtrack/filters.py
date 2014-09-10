164594b6f8bdf6b2ff35f56c7ccf2957b58a96c1
Cory Zue <czue@dimagi.com>
2014-09-10 16:07:10 -0400

    kill dead filter

--- corehq/apps/commtrack/filters.py
+++ corehq/apps/commtrack/filters.py
@@ -6,31 +6,6 @@ from corehq.apps.locations.util import defined_location_types
 from corehq.apps.reports.filters.base import BaseSingleOptionFilter, BaseMultipleOptionFilter
 
 
-class SupplyPointTypeFilter(BaseMultipleOptionFilter):
-    slug = 'outlet_type'
-    label = ugettext_noop('Outlet Type')
-    default_options = ['_all']
-
-    @property
-    def options(self):
-        categories = supply_point_type_categories(self.domain)
-
-        def indent(type):
-            return (type, u'\u2013 %s' % type)
-
-        choices = [('_all', _('All Outlet Types'))]
-        for k, v in sorted(categories.items()):
-            if k.startswith('_'):
-                continue
-            choices.append(('cat:%s' % k, k))
-            for t in sorted(v):
-                choices.append(indent(t))
-        choices.append(('_oth', 'Other'))
-        for t in sorted(categories['_oth']):
-            choices.append(indent(t))
-        return choices
-
-
 class ProductFilter(BaseMultipleOptionFilter):
     slug = 'product'
     label = ugettext_noop('Product')
