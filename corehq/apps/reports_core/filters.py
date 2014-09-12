7b23750f1deaec03e08722bf388cebb8d45b420d
Cory Zue <czue@dimagi.com>
2014-09-12 11:48:57 -0400

    move filter templates to subdirectory

--- corehq/apps/reports_core/filters.py
+++ corehq/apps/reports_core/filters.py
@@ -80,7 +80,8 @@ class BaseFilter(object):
 
 class DatespanFilter(BaseFilter):
 
-    def __init__(self, name, required=True, label='Datespan Filter', template='reports_core/datespan_filter.html',
+    def __init__(self, name, required=True, label='Datespan Filter',
+                 template='reports_core/filters/datespan_filter.html',
                  css_id=None):
         # todo: should these be in the constructor as well?
         params = [
