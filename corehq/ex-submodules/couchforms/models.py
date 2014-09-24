a730081b39c4219f95817990efb7ba9b253aff33
Daniel Roberts <droberts@dimagi.com>
2014-09-24 11:42:12 -0400

    fix case and form tests

--- corehq/ex-submodules/couchforms/models.py
+++ corehq/ex-submodules/couchforms/models.py
@@ -6,11 +6,12 @@ import hashlib
 import logging
 import time
 from copy import copy
+from jsonobject.api import re_date
 from jsonobject.base import DefaultProperty
 from lxml import etree
 
 from django.utils.datastructures import SortedDict
-from couchdbkit.exceptions import PreconditionFailed
+from couchdbkit.exceptions import PreconditionFailed, BadValueError
 from dimagi.ext.couchdbkit import *
 from couchdbkit import ResourceNotFound
 from lxml.etree import XMLSyntaxError
@@ -181,12 +182,18 @@ class XFormInstance(SafeSaveDocument, UnicodeMixIn, ComputedDocumentMixin,
                     for key in ("timeStart", "timeEnd"):
                         if key in meta_block:
                             if meta_block[key]:
+                                if re_date.match(meta_block[key]):
+                                    # this kind of leniency is pretty bad
+                                    # and making it midnight in UTC
+                                    # is totally arbitrary
+                                    # here for backwards compatibility
+                                    meta_block[key] += 'T00:00:00.000000Z'
                                 try:
                                     # try to parse to ensure correctness
-                                    parsed = string_to_datetime(meta_block[key])
+                                    parsed = DateTimeProperty().wrap(meta_block[key])
                                     # and set back in the right format in case it was a date, not a datetime
-                                    ret[key] = json_format_datetime(parsed)
-                                except ValueError:
+                                    _, ret[key] = DateTimeProperty().unwrap(parsed)
+                                except BadValueError:
                                     # we couldn't parse it
                                     del ret[key]
                             else:
