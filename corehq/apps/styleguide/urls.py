8df1cc155186acb009b09372d3156ff2699656ad
Biyeun Buczyk <biyeun@dimagi.com>
2014-09-24 15:36:04 -0400

    styleguide urls

--- corehq/apps/styleguide/urls.py
+++ corehq/apps/styleguide/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import *
 from corehq.apps.styleguide.views import *
 
+doc_urlpatterns = patterns('corehq.apps.styleguide.views.docs',
+    url(r'^$', 'default', name='sg_examples_default'),
+    url(r'^simple_crispy/',
+        include('corehq.apps.styleguide.examples.simple_crispy_form.urls')),
+)
 
 urlpatterns = patterns('corehq.apps.styleguide.views',
     url(r'^$', MainStyleGuideView.as_view(), name=MainStyleGuideView.urlname),
-    url(r'^forms/$', FormsStyleGuideView.as_view(), name=FormsStyleGuideView.urlname),
+    url(r'^forms/$', FormsStyleGuideView.as_view(),
+        name=FormsStyleGuideView.urlname),
+    (r'^docs/', include(doc_urlpatterns)),
 )
+
+
