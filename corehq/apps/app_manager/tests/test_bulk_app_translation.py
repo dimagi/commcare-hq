c6e687b52e449bf56954a0bce3b1e728626a4f73
NoahCarnahan <noah.carnahan@gmail.com>
2014-09-28 15:20:45 +0300

    Straightened out tests

--- corehq/apps/app_manager/tests/test_bulk_app_translation.py
+++ corehq/apps/app_manager/tests/test_bulk_app_translation.py
@@ -1,91 +1,51 @@
-import os
 import codecs
+import json
+import os
 
-from django.test.client import Client
-from django.test import TestCase
-from corehq import Domain
-
-from corehq.apps.app_manager.tests.util import add_build
-from corehq.apps.app_manager.models import Application, DetailColumn, APP_V1, Module, \
-    get_app
-from corehq.apps.users.models import WebUser
+from django.test import SimpleTestCase
+from corehq.apps.app_manager.models import Application
+from corehq.apps.app_manager.views import _process_bulk_app_translation_upload
 
 
-class BulkAppTranslationTest(TestCase):
-    with codecs.open(os.path.join(os.path.dirname(__file__), "data", "bulk_translate_test_form.xml"), encoding='utf-8') as f:
-        xform_str = f.read()
+class BulkAppTranslationTest(SimpleTestCase):
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        with codecs.open(os.path.join(os.path.dirname(__file__), "data", "bulk_translate_app.json"), encoding='utf-8') as f:
+            cls.app = Application.wrap(json.load(f))
 
-        self.client = Client()
-
-        self.domain = Domain(name="test-domain", is_active=True)
-        self.domain.save()
-
-        self.username = 'bulk-jhghgjdfdfg-user'
-        self.password = 'my-password'
-        self.user = WebUser.create(self.domain.name, self.username, self.password)
-        self.user.set_role(self.domain.name, 'admin')
-        self.user.save()
-
-        self.app = Application.new_app(self.domain.name, "TestApp", application_version=APP_V1)
-        self.app.build_langs.append("fra")
-        self.app.langs.append("fra")
-
-        for i in range(2):
-            module = self.app.add_module(Module.new_module("Module%d" % i, "en"))
-            for j in range(2):
-                self.app.new_form(module.id, name="Form%s-%s" % (i,j), attachment=self.xform_str, lang="en")
-            module = self.app.get_module(i)
-            short_detail = module.case_details.short
-            long_detail = module.case_details.long
-
-            if i == 0 and j == 0:
-                e = [{
-                    'key': 'mykey1',
-                    'value': {
-                        "en": 'myval1',
-                    }
-                },
-                {
-                    'key': 'mykey2',
-                    'value': {
-                        "en": 'myval2',
-                }}]
-                d1 = DetailColumn(header={"en": "test1"}, model="case", field="test1", format="plain")
-                d2 = DetailColumn(header={"en": "test2"}, modul="case", field="test2", format="enum", enum=e)
-                short_detail.append(d1)
-                short_detail.append(d2)
-                long_detail.append(d1)
-                long_detail.append(d2)
-
-        self.build = {'version': "2.7.0", 'build_number': 20655}
-        add_build(**self.build)
-        self.app.save()
-
-    def test_download(self):
-        # Download the sheet
-        # Somehow compare the sheets
-        pass
-
-    def test_upload(self):
-        with codecs.open(os.path.join(os.path.dirname(__file__), "data", "bulk_app_translation.xlsx")) as f:
-            # TODO: Reverse the url instead of hard coding
-            self.client.login(username=self.username, password=self.password)
-            response = self.client.post('/a/%s/apps/view/%s/languages/bulk_app_translations/upload/' % (self.app.domain, self.app.id),
-                   {'bulk_upload_file': f},
-                   follow=True
-            )
-            import ipdb; ipdb.set_trace()
-            print response
-        app = get_app(self.app.domain, self.app.id)
-        form = app.get_module(0).get_form(0)
+    def test_set_up(self):
+        form = self.app.get_module(0).get_form(0)
 
         labels = {}
-        for lang in app.langs:
+        for lang in self.app.langs:
             for question in form.get_questions([lang], include_triggers=True, include_groups=True):
                 labels[(question['value'], lang)] = question['label']
 
-        # Test new translation (of question and prop and form and module)
+        self.assertEqual(labels["/data/question1", "en"], "question1")
+
+    def test_no_change_upload(self):
+        with codecs.open(os.path.join(os.path.dirname(__file__), "data", "bulk_app_translations_no_change.xlsx")) as f:
+            messages = _process_bulk_app_translation_upload(self.app, f)
+
+        self.assertListEqual([m[1] for m in messages], ["App Translations Updated!"])
+
+
+    def test_upload(self):
+        with codecs.open(os.path.join(os.path.dirname(__file__), "data", "bulk_app_translations.xlsx")) as f:
+            messages = _process_bulk_app_translation_upload(self.app, f)
+            self.assertListEqual([m[1] for m in messages], ["App Translations Updated!"])
+
+        form = self.app.get_module(0).get_form(0)
+
+        labels = {}
+        for lang in self.app.langs:
+            for question in form.get_questions([lang], include_triggers=True, include_groups=True):
+                labels[(question['value'], lang)] = question['label']
+
         self.assertEqual(labels[("/data/question1", "en")], "in english")
-        self.assertEqual(labels[("/data/question1", "fr")], "in french")
+        self.assertEqual(labels[("/data/question1", "fra")], "in french")
+
+        module = self.app.get_module(0)
+        self.assertEqual(module.case_details.long.columns[1].enum[0].value['fra'], 'french bar')
+        self.assertEqual(module.case_details.short.columns[0].header['fra'], 'Nom')
