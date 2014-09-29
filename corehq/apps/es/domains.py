665701a51b9842f1cb1ae181f36987eb0ebfaf8e
Jonathan Emord <jonathan.emord@gmail.com>
2014-09-29 17:19:32 -0400

    Add filters to DomainES

--- corehq/apps/es/domains.py
+++ corehq/apps/es/domains.py
@@ -1,5 +1,31 @@
 from .es_query import HQESQuery
+from . import filters
 
 
 class DomainES(HQESQuery):
     index = 'domains'
+
+    @property
+    def builtin_filters(self):
+        return [
+            non_test_domains,
+            incomplete_domains,
+        ] + super(DomainES, self).builtin_filters
+
+
+def non_test_domains():
+    return filters.term("is_test", [False, "none"])
+
+
+def incomplete_domains():
+    return filters.OR(filters.missing("country"),
+                      filters.missing("internal.area"),
+                      filters.missing("internal.initiative"),
+                      filters.missing("internal.notes"),
+                      filters.missing("internal.organization_name"),
+                      filters.missing("internal.platform"),
+                      filters.missing("internal.phone_model"),
+                      filters.missing("internal.project_manager"),
+                      filters.missing("internal.project_state"),
+                      filters.missing("internal.sub_area"),
+                      )
