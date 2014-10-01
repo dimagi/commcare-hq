0e81454c60e689b258782914764540e18cb19e93
Marcin Sienkiewicz <msienkiewicz@soldevelo.com>
2014-10-01 15:22:13 +0200

    Changed GrowthMonitoring calculation.

--- custom/opm/user_calcs.py
+++ custom/opm/user_calcs.py
@@ -77,48 +77,11 @@ class GrowthMonitoring(fluff.Calculator):
     @fluff.date_emitter
     def total(self, form):
         if form.xmlns == CHILD_FOLLOWUP_XMLNS:
-            # child<n>_child_growthmon == 1 if weight was monitored this month
-            total = 0
-            for child_num in list('123'):
-                child = form.form.get('child_%s' % child_num)
-                if child:
-                    try:
-                        total += int(child.get('child%s_child_growthmon' % child_num))
-                    except:
-                        pass
-            if total:
-                yield {
-                    'date': form.received_on,
-                    'value': total,
-                    'group_by': [
-                        form.domain,
-                        form.metadata.userID,
-                    ],
-                }
 
-
-    def get_result(self, key, date_range=None, reduce=True):
-        # This block is pretty much a stripped copy-paste from fluff
-        # except I needed to make sure the results were unique by case
-        assert isinstance(date_range, tuple)
-        start, end = date_range
-        shared_key = [self.fluff._doc_type] + key + [self.slug, 'total']
-        q = self.fluff.view(
-            'fluff/generic',
-            startkey=shared_key + [json_format_date(start)],
-            endkey=shared_key + [json_format_date(end)],
-            reduce=False,
-        ).all()
-
-        def strip(id_string):
-            prefix = '%s-' % self.fluff.__name__
-            assert id_string.startswith(prefix)
-            return id_string[len(prefix):]
-
-        cases = {}
-        for form in q:
-            form_id = strip(form['id'])
-            case_id = XFormInstance.get(form_id).form['case']['@case_id']
-            cases[case_id] = max(cases.get(case_id, 0), form['value'])
-        return {'total': sum(cases.values())}
-
+            yield {
+                'date': form.received_on,
+                'group_by': [
+                    form.domain,
+                    form.metadata.userID,
+                ],
+            }
