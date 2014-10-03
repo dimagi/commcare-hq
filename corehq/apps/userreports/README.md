8ddc9f0c58a696df47e5a686c12c277146551af4
Cory Zue <czue@dimagi.com>
2014-10-03 16:06:39 -0400

    add samples and management command to load them up + docs

--- corehq/apps/userreports/README.md
+++ corehq/apps/userreports/README.md
@@ -3,11 +3,36 @@ User Configurable Reports
 
 Some rough notes for working with user configurable reports.
 
-To rebuild a table just call the `rebuild_indicators` function in `tasks.py`.
+Getting Started
+---------------
 
-The easiest way to create a table configuration is to do it manually in couch or via a python function.
-This will hopefully get easier soon.
-For inspiration, look at the example configuration files in tests/data/configs/.
+The easiest way to get started is to start with sample data and reports.
+
+First copy the dimagi domain to your developer machine.
+You only really need forms, users, and cases:
+
+```
+./manage.py copy_domain https://<your_username>:<your_password>@commcarehq.cloudant.com/commcarehq dimagi --include=CommCareCase,XFormInstance,CommCareUser
+```
+
+Then load and rebuild the data table:
+
+```
+./manage.py load_spec corehq/apps/userreports/examples/dimagi/dimagi-case-data-source.json --rebuild
+```
+
+Then load the report:
+
+```
+./manage.py load_spec corehq/apps/userreports/examples/dimagi/dimagi-chart-report.json
+```
+
+Fire up a browser and you should see the new report in your domain.
+You should also be able to navigate to the edit UI, or look at and edit the example JSON files.
+There is a second example based off the "gsid" domain as well using forms.
+
+The tests are also a good source of documentation for the various filter and indicator formats that are supported.
+
 
 Inspecting database tables
 --------------------------
---
+++ corehq/apps/userreports/management/commands/load_spec.py
@@ -0,0 +1,50 @@
+import json
+from optparse import make_option
+from couchdbkit import ResourceNotFound
+from django.core.management.base import LabelCommand, CommandError
+from corehq.apps.userreports.models import IndicatorConfiguration, ReportConfiguration
+from corehq.apps.userreports.tasks import rebuild_indicators
+from dimagi.utils.decorators.log_exception import log_exception
+
+
+class Command(LabelCommand):
+    help = "Load a user configurable report data source or report spec from a json file"
+    args = '<filename>'
+    label = ""
+    option_list = LabelCommand.option_list + (
+        make_option('--rebuild',
+                    action='store_true',
+                    dest='rebuild',
+                    default=False,
+                    help='Also rebuild indicator tables (has no affect for reports).'),
+    )
+    @log_exception()
+    def handle(self, *args, **options):
+        if len(args) < 1:
+            raise CommandError('Usage is load_data_source %s' % self.args)
+
+        filename = args[0]
+        with open(filename) as f:
+            body = json.loads(f.read())
+
+            # intentionally fails hard if bad or missing doc_type
+            cls = {
+                'IndicatorConfiguration': IndicatorConfiguration,
+                'ReportConfiguration': ReportConfiguration,
+            }[body['doc_type']]
+
+            if '_id' in body:
+                try:
+                    rev = cls.get_db().get_rev(body['_id'])
+                    body['_rev'] = rev
+                except ResourceNotFound:
+                    pass
+            to_save = cls.wrap(body)
+            to_save.validate()
+            to_save.save()
+
+            print 'updated {}: "{}"'.format(to_save.doc_type, to_save.display_name)
+            if options['rebuild'] and isinstance(to_save, IndicatorConfiguration):
+                print 'rebuilding table...'
+                rebuild_indicators(to_save._id)
+                print '...done!'
