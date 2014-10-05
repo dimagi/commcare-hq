f80c90b8224105c2c5ec44942124627c76f3bd7f
Daniel Roberts <droberts@dimagi.com>
2014-10-05 16:55:58 -0400

    remove unused ConfigurableFilter

--- corehq/apps/userreports/filters.py
+++ corehq/apps/userreports/filters.py
@@ -1,18 +1,7 @@
 from fluff.filters import Filter
 
 
-class ConfigurableFilter(Filter):
-    # this currently has the exact same API as fluff.filters.Filter
-    # but adds a function (from_spec)
-    def filter(self, item):
-        raise NotImplementedError()
-
-    @classmethod
-    def from_spec(cls, spec):
-        raise NotImplementedError()
-
-
-class SinglePropertyValueFilter(ConfigurableFilter):
+class SinglePropertyValueFilter(Filter):
 
     def __init__(self, getter, operator, reference_value):
         self.getter = getter
