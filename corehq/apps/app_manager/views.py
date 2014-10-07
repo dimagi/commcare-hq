5da7c714095136293ad299f7e031c5bd2787d149
NoahCarnahan <ncarnahan@dimagi.com>
2014-10-07 11:06:03 +0300

    Fix missing import

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -32,7 +32,8 @@ from corehq.apps.app_manager import id_strings
 from corehq.apps.app_manager.templatetags.xforms_extras import trans
 from corehq.apps.app_manager.translations import (
     expected_bulk_app_sheet_headers,
-    get_translation
+    get_translation,
+    process_bulk_app_translation_upload
 )
 from corehq.apps.commtrack.models import Program
 from corehq.apps.hqmedia.views import DownloadMultimediaZip
