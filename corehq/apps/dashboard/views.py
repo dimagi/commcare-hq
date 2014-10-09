5ce537c430009e118f92f8f6d903625568466ce1
Biyeun Buczyk <biyeun@dimagi.com>
2014-10-09 17:36:04 -0400

    so that of the rest of the bootstrap 3 template renders properly

--- corehq/apps/dashboard/views.py
+++ corehq/apps/dashboard/views.py
@@ -42,6 +42,7 @@ class BaseDashboardView(LoginAndDomainMixin, BasePageView, DomainViewMixin):
     @method_decorator(preview_boostrap3())
     @method_decorator(toggles.DASHBOARD_PREVIEW.required_decorator())
     def dispatch(self, request, *args, **kwargs):
+        request.preview_bootstrap3 = True
         return super(BaseDashboardView, self).dispatch(request, *args, **kwargs)
 
     @property
