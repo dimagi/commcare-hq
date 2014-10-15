69e1bbd0238c05300461aa7f8370e64866ba7185
Ethan Soergel <esoergel@gmail.com>
2014-10-15 16:01:30 -0400

    Genericize field data type representation

--- corehq/apps/users/views/mobile/custom_data_fields.py
+++ corehq/apps/users/views/mobile/custom_data_fields.py
@@ -1,5 +1,4 @@
 from django.utils.decorators import method_decorator
-from django.utils.translation import ugettext_noop
 from corehq.apps.custom_data_fields.views import CustomDataFieldsMixin
 
 from corehq.apps.users.decorators import require_can_edit_commcare_users
@@ -7,10 +6,9 @@ from corehq.apps.users.views import BaseUserSettingsView
 
 
 class UserFieldsView(CustomDataFieldsMixin, BaseUserSettingsView):
-    urlname = "user_fields_view"
-    page_name = ugettext_noop("Edit User Fields")
+    urlname = 'user_fields_view'
     field_type = 'UserFields'
-    form_label = 'User Fields'
+    entity_string = "User"
 
     @method_decorator(require_can_edit_commcare_users)
     def dispatch(self, request, *args, **kwargs):
