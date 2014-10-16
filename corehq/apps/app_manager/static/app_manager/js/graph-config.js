41c63f55f9d8b91d16ef4ab6db8407ecd9a0fe4a
NoahCarnahan <ncarnahan@dimagi.com>
2014-10-16 13:33:20 +0300

    Graph config modal additions

--- corehq/apps/app_manager/static/app_manager/js/graph-config.js
+++ corehq/apps/app_manager/static/app_manager/js/graph-config.js
@@ -1,6 +1,7 @@
 var openGraphConfigurationModal = function(){
     var $modalDiv = $('<div data-bind="template: \'graph_configuration_modal\'"></div>');
-    ko.applyBindings({}, $modalDiv.get(0));
+    var myGraphViewModel = new GraphViewModel();
+    ko.applyBindings(myGraphViewModel, $modalDiv.get(0));
     var $modal = $modalDiv.find('.modal');
 
     $modal.appendTo('body');
@@ -8,4 +9,20 @@ var openGraphConfigurationModal = function(){
     $modal.on('hidden', function () {
         $modal.remove();
     });
+};
+
+var GraphViewModel = function(){
+    var self = this;
+
+    self.graphDisplayName =  ko.observable("My Partograph");
+    self.availableGraphTypes =  ko.observableArray(["xy", "line", "scatter", "bubble"]);
+    self.annotations = ko.observableArray([new Annotation()]);
+};
+
+var Annotation = function(){
+    var self = this;
+
+    self.x = ko.observable(0);
+    self.y = ko.observable(0);
+    self.displayText = ko.observable("");
 };
