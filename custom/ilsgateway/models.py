cfe212df647a0daee2d4668bc3ac27ef3776613c
Kamil Krampa <kkrampa@soldevelo.com>
2014-10-20 13:45:37 +0200

    Migration checkpoint tweaks

--- custom/ilsgateway/models.py
+++ custom/ilsgateway/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class ILSMigrationCheckpoint(models.Model):
     domain = models.CharField(max_length=100)
     date = models.DateTimeField(null=True)
+    start_date = models.DateTimeField(null=True)
     api = models.CharField(max_length=100)
     limit = models.PositiveIntegerField()
     offset = models.PositiveIntegerField()
