4caa844279fd90567ed2c66512fcda2938e28878
Simon Kelly <skelly@dimagi.com>
2014-10-21 12:02:28 +0200

    remove dubious tests

--- corehq/apps/receiverwrapper/tests/__init__.py
+++ corehq/apps/receiverwrapper/tests/__init__.py
@@ -8,7 +8,6 @@ try:
     from .test_auth import *
     from .test_app_id import *
     from .test_guess_phone_type import *
-    from .test_forwarding_forms import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
     # otherwise debugging is a pain
