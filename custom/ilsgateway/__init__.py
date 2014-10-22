d60bb0a6bc5b1b8c670cca1bc160c60fa3513e4b
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-10-22 10:43:34 +0200

    ILS Dashboard report

--- custom/ilsgateway/__init__.py
+++ custom/ilsgateway/__init__.py
@@ -1,7 +1,9 @@
 from custom.ilsgateway.alerts import AlertReport
+from custom.ilsgateway.reports.dashboard_report import DashboardReport
 
 CUSTOM_REPORTS = (
     ('Custom reports', (
+        DashboardReport,
         AlertReport,
     )),
 )
