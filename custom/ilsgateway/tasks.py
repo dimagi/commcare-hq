9c3620e7707ab3b3f93409c340a85ffee19d401f
Kamil Krampa <kkrampa@soldevelo.com>
2014-10-23 12:50:20 +0200

    Added support for data portioning in ils integration

--- custom/ilsgateway/tasks.py
+++ custom/ilsgateway/tasks.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from decimal import Decimal
 from celery import group
-from celery.task import task, periodic_task
+from celery.task import task
 from couchdbkit.exceptions import ResourceNotFound
 from django.db import transaction
 from psycopg2._psycopg import DatabaseError
@@ -12,15 +12,14 @@ from custom.ilsgateway.api import ILSGatewayEndpoint, Location
 from custom.ilsgateway.commtrack import bootstrap_domain, sync_ilsgateway_location, commtrack_settings_sync,\
     sync_ilsgateway_product
 
-
-#@periodic_task(run_every=timedelta(days=1), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 from custom.ilsgateway.models import ILSGatewayConfig, SupplyPointStatus, DeliveryGroupReport, GroupSummary, \
-ProductAvailabilityData, ReportRun
-from custom.ilsgateway.run_reports import process_facility_warehouse_data, populate_report_data
+    ProductAvailabilityData, ReportRun
+from custom.ilsgateway.run_reports import populate_report_data
 
 from dimagi.utils.dates import force_to_datetime
 
 
+#@periodic_task(run_every=timedelta(days=1), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def migration_task():
     configs = ILSGatewayConfig.get_all_configs()
     for config in configs:
@@ -50,24 +49,33 @@ def get_locations(domain, endpoint):
 @task
 def product_stock_task(domain, endpoint):
     for facility in FACILITIES:
-        product_stocks = endpoint.get_productstocks(filters=dict(supply_point=facility))[1]
-        for product_stock in product_stocks:
-            case = SupplyPointCase.view('hqcase/by_domain_external_id',
-                                        key=[domain, str(product_stock.supply_point_id)],
-                                        reduce=False,
-                                        include_docs=True,
-                                        limit=1).first()
-            product = Product.get_by_code(domain, product_stock.product_code)
-            try:
-                StockState.objects.get(section_id='stock', case_id=case._id, product_id=product._id)
-            except StockState.DoesNotExist:
-                StockState.objects.create(section_id='stock',
-                                          case_id=case._id,
-                                          product_id=product._id,
-                                          stock_on_hand=product_stock.quantity or 0,
-                                          daily_consumption=product_stock.auto_monthly_consumption or 0,
-                                          last_modified_date=product_stock.last_modified,
-                                          sql_product=SQLProduct.objects.get(product_id=product._id))
+        has_next = True
+        next_url = ""
+
+        while has_next:
+            meta, product_stocks = endpoint.get_productstocks(next_url_params=next_url,
+                                                              filters=dict(supply_point=facility))
+            for product_stock in product_stocks:
+                case = SupplyPointCase.view('hqcase/by_domain_external_id',
+                                            key=[domain, str(product_stock.supply_point_id)],
+                                            reduce=False,
+                                            include_docs=True,
+                                            limit=1).first()
+                product = Product.get_by_code(domain, product_stock.product_code)
+                try:
+                    StockState.objects.get(section_id='stock', case_id=case._id, product_id=product._id)
+                except StockState.DoesNotExist:
+                    StockState.objects.create(section_id='stock',
+                                              case_id=case._id,
+                                              product_id=product._id,
+                                              stock_on_hand=product_stock.quantity or 0,
+                                              daily_consumption=product_stock.auto_monthly_consumption or 0,
+                                              last_modified_date=product_stock.last_modified,
+                                              sql_product=SQLProduct.objects.get(product_id=product._id))
+            if not meta.get('next', False):
+                has_next = False
+            else:
+                next_url = meta['next'].split('?')[1] if meta['next'] else None
 
 
 @task
@@ -80,48 +88,86 @@ def stock_transaction_task(domain, endpoint):
         xform.save()
 
     for facility in FACILITIES:
-        stocktransactions = endpoint.get_stocktransactions(filters=(dict(supply_point=facility, order_by='date')))[1]
-        for stocktransaction in stocktransactions:
-            case = SupplyPointCase.view('hqcase/by_domain_external_id',
-                                        key=[domain, str(stocktransaction.supply_point_id)],
-                                        reduce=False,
-                                        include_docs=True,
-                                        limit=1).first()
-            product = Product.get_by_code(domain, stocktransaction.product_code)
-            try:
-                StockTransaction.objects.get(case_id=case._id,
-                                             product_id=product._id, report__date=force_to_datetime(stocktransaction.date),
-                                             stock_on_hand=Decimal(stocktransaction.ending_balance),
-                                             type='stockonhand', report__domain=domain)
-            except StockTransaction.DoesNotExist:
-                r = StockReport.objects.create(form_id=xform._id, date=force_to_datetime(stocktransaction.date),
-                                               type='balance', domain=domain)
-                StockTransaction.objects.create(report=r, section_id='stock', case_id=case._id, product_id=product._id,
-                                                type='stockonhand', stock_on_hand=Decimal(stocktransaction.ending_balance))
+        has_next = True
+        next_url = ""
+
+        while has_next:
+            meta, stocktransactions = endpoint.get_stocktransactions(next_url_params=next_url,
+                                                                     filters=(dict(supply_point=facility,
+                                                                                   order_by='date')))
+            for stocktransaction in stocktransactions:
+                case = SupplyPointCase.view('hqcase/by_domain_external_id',
+                                            key=[domain, str(stocktransaction.supply_point_id)],
+                                            reduce=False,
+                                            include_docs=True,
+                                            limit=1).first()
+                product = Product.get_by_code(domain, stocktransaction.product_code)
+                try:
+                    StockTransaction.objects.get(case_id=case._id,
+                                                 product_id=product._id,
+                                                 report__date=force_to_datetime(stocktransaction.date),
+                                                 stock_on_hand=Decimal(stocktransaction.ending_balance),
+                                                 type='stockonhand', report__domain=domain)
+                except StockTransaction.DoesNotExist:
+                    r = StockReport.objects.create(form_id=xform._id,
+                                                   date=force_to_datetime(stocktransaction.date),
+                                                   type='balance',
+                                                   domain=domain)
+                    StockTransaction.objects.create(report=r,
+                                                    section_id='stock',
+                                                    case_id=case._id,
+                                                    product_id=product._id,
+                                                    type='stockonhand',
+                                                    stock_on_hand=Decimal(stocktransaction.ending_balance))
+            if not meta.get('next', False):
+                has_next = False
+            else:
+                next_url = meta['next'].split('?')[1] if meta['next'] else None
 
 
 @task
 def supply_point_statuses_task(domain, endpoint):
     for facility in FACILITIES:
-        supply_point_statuses = endpoint.get_supplypointstatuses(domain, filters=dict(supply_point=facility),
-                                                                 facility=facility)[1]
-        for sps in supply_point_statuses:
-            try:
-                SupplyPointStatus.objects.get(external_id=sps.external_id)
-            except SupplyPointStatus.DoesNotExist:
-                sps.save()
+        has_next = True
+        next_url = ""
+
+        while has_next:
+            meta, supply_point_statuses = endpoint.get_supplypointstatuses(domain,
+                                                                           next_url_params=next_url,
+                                                                           filters=dict(supply_point=facility),
+                                                                           facility=facility)
+            for sps in supply_point_statuses:
+                try:
+                    SupplyPointStatus.objects.get(external_id=sps.external_id)
+                except SupplyPointStatus.DoesNotExist:
+                    sps.save()
+
+            if not meta.get('next', False):
+                has_next = False
+            else:
+                next_url = meta['next'].split('?')[1] if meta['next'] else None
 
 
 @task
 def delivery_group_reports_task(domain, endpoint):
     for facility in FACILITIES:
-        delivery_group_reports = endpoint.get_deliverygroupreports(domain, filters=dict(supply_point=facility),
-                                                                   facility=facility)[1]
-        for dgr in delivery_group_reports:
-            try:
-                DeliveryGroupReport.objects.get(external_id=dgr.external_id)
-            except DeliveryGroupReport.DoesNotExist:
-                dgr.save()
+        has_next = True
+        next_url = ""
+        while has_next:
+            delivery_group_reports, meta = endpoint.get_deliverygroupreports(domain,
+                                                                             next_url_params=next_url,
+                                                                             filters=dict(supply_point=facility),
+                                                                             facility=facility)
+            for dgr in delivery_group_reports:
+                try:
+                    DeliveryGroupReport.objects.get(external_id=dgr.external_id)
+                except DeliveryGroupReport.DoesNotExist:
+                    dgr.save()
+
+            if not meta.get('next', False):
+                has_next = False
+            else:
+                next_url = meta['next'].split('?')[1] if meta['next'] else None
 
 
 @task
@@ -193,5 +239,4 @@ def report_run(domain):
         new_run.end_run = datetime.utcnow()
         new_run.complete = True
         new_run.save()
-        print "End time: %s" % datetime.now()
-
+        logging.info("ILSGateway report runner end time: %s" % datetime.now())
