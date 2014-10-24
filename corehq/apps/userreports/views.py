5ea48e181ba1aefe09c2cae8992e8b0c2e3ae5ad
Cory Zue <czue@dimagi.com>
2014-10-24 09:24:36 -0400

    add import report view

--- corehq/apps/userreports/views.py
+++ corehq/apps/userreports/views.py
@@ -1,3 +1,4 @@
+import json
 from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
@@ -6,6 +7,7 @@ from django.utils.translation import ugettext as _
 from django.views.decorators.http import require_POST
 from corehq import Session
 from corehq import toggles
+from corehq.apps.userreports.exceptions import BadSpecError
 from corehq.apps.userreports.models import ReportConfiguration, DataSourceConfiguration
 from corehq.apps.userreports.sql import get_indicator_table, IndicatorSqlAdapter, get_engine
 from corehq.apps.userreports.tasks import rebuild_indicators
@@ -54,6 +56,28 @@ def delete_report(request, domain, report_id):
     config.delete()
     messages.success(request, _(u'Report "{}" deleted!').format(config.title))
     return HttpResponseRedirect(reverse('configurable_reports_home', args=[domain]))
+
+
+@toggles.USER_CONFIGURABLE_REPORTS.required_decorator()
+def import_report(request, domain):
+    if request.method == "POST":
+        spec = request.POST['report_spec']
+        try:
+            json_spec = json.loads(spec)
+            if '_id' in json_spec:
+                del json_spec['_id']
+            report = ReportConfiguration.wrap(json_spec)
+            report.validate()
+            report.save()
+            messages.success(request, _('Report created!'))
+            return HttpResponseRedirect(reverse('edit_configurable_report', args=[domain, report._id]))
+        except (ValueError, BadSpecError) as e:
+            messages.error(request, _('Bad report source: {}').format(e))
+    else:
+        spec = _('paste report source here')
+    context = _shared_context(domain)
+    context['spec'] = spec
+    return render(request, "userreports/import_report.html", context)
 
 
 @toggles.USER_CONFIGURABLE_REPORTS.required_decorator()
