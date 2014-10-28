1904a0ec86f81cfff547c0a0f8f5280a282c5155
Cory Zue <czue@dimagi.com>
2014-10-28 11:28:34 -0400

    fix choice list filter tests

--- corehq/apps/userreports/tests/test_report_filters.py
+++ corehq/apps/userreports/tests/test_report_filters.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 from corehq.apps.reports_core.filters import DatespanFilter, ChoiceListFilter
 from corehq.apps.userreports.exceptions import BadSpecError
 from corehq.apps.userreports.reports.factory import ReportFilterFactory
+from corehq.apps.userreports.reports.filters import SHOW_ALL_CHOICE
 
 
 class FilterTestCase(SimpleTestCase):
@@ -55,29 +56,46 @@ class DateFilterTestCase(SimpleTestCase):
 
 
 class ChoiceListFilterTestCase(SimpleTestCase):
+    CHOICES = [
+        {
+            "value": "NEGATIVE",
+            "display": "Negative"
+        },
+        {
+            "value": "POSITIVE",
+            "display": "positive"
+        }
+    ]
 
     def test_choice_list_filter(self):
-        choices = [
-            {
-                "value": "NEGATIVE",
-                "display": "Negative"
-            },
-            {
-                "value": "POSITIVE",
-                "display": "positive"
-            }
-        ]
         filter = ReportFilterFactory.from_spec({
             "type": "choice_list",
             "slug": "diagnosis_slug",
             "field": "diagnosis_field",
             "display": "Diagnosis",
-            "choices": choices
+            "choices": self.CHOICES,
+            "show_all": False,
         })
         self.assertEqual(ChoiceListFilter, type(filter))
         self.assertEqual('diagnosis_slug', filter.name)
         self.assertEqual('Diagnosis', filter.label)
         self.assertEqual(2, len(filter.choices))
-        for i, choice in enumerate(choices):
+        for i, choice in enumerate(self.CHOICES):
             self.assertEqual(filter.choices[i].value, choice['value'])
             self.assertEqual(filter.choices[i].display, choice['display'])
+
+    def test_choice_list_filter_show_all(self):
+        filter = ReportFilterFactory.from_spec({
+            "type": "choice_list",
+            "slug": "diagnosis_slug",
+            "field": "diagnosis_field",
+            "display": "Diagnosis",
+            "choices": self.CHOICES,
+            "show_all": True,
+        })
+        self.assertEqual(3, len(filter.choices))
+
+        self.assertEqual(SHOW_ALL_CHOICE, filter.choices[0].value)
+        for i, choice in enumerate(self.CHOICES):
+            self.assertEqual(filter.choices[i + 1].value, choice['value'])
+            self.assertEqual(filter.choices[i + 1].display, choice['display'])
