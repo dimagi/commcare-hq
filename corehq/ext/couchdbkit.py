2f9948f8eced21d92d24a7217fb499dc5af5ee81
Daniel Roberts <droberts@dimagi.com>
2014-10-29 11:57:54 -0400

    fix pact tests
    exposed an issue with ISODocument

--- corehq/ext/couchdbkit.py
+++ corehq/ext/couchdbkit.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from corehq.ext.jsonobject import ISOMeta, GeoPointProperty, UTCDateTimeProperty
-from couchdbkit import Document, DocumentSchema
+from couchdbkit.ext.django.schema import Document, DocumentSchema
 from dimagi.utils.couch.database import SafeSaveDocument
 
 __all__ = [
