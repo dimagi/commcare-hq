233a384df3a34b304a0f8f5eb2451c38e6276513
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2014-11-03 11:23:46 +0100

    SoH report for District

--- custom/ilsgateway/reports/stock_on_hand.py
+++ custom/ilsgateway/reports/stock_on_hand.py
@@ -1,5 +1,8 @@
 from casexml.apps.stock.models import StockTransaction
-from corehq.apps.commtrack.models import SQLProduct
+from datetime import timedelta, datetime, time
+from django.db.models import Q
+from django.template.defaultfilters import floatformat
+from corehq.apps.commtrack.models import SQLProduct, StockState
 from corehq.apps.locations.models import Location
 from corehq.apps.reports.commtrack.util import get_relevant_supply_point_ids
 from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
@@ -7,11 +10,12 @@ from corehq.apps.reports.filters.fixtures import AsyncLocationFilter
 from corehq.apps.reports.filters.select import MonthFilter, YearFilter
 from django.utils import html
 from custom.ilsgateway.models import GroupSummary, SupplyPointStatusTypes, ProductAvailabilityData, \
-    OrganizationSummary
+    OrganizationSummary, SupplyPointStatus, SupplyPointStatusValues
 from custom.ilsgateway.reports import ProductAvailabilitySummary, ILSData, format_percent, link_format
 from custom.ilsgateway.reports.base_report import MultiReport
 from custom.ilsgateway.reports.dashboard_report import SohSubmissionData
 from django.utils.translation import ugettext as _
+from dimagi.utils.dates import get_business_day_of_month, get_day_of_month
 
 
 class DetailsReport(MultiReport):
@@ -136,6 +140,37 @@ class SohPercentageTableData(ILSData):
         return rows
 
 
+class OnTimeStates(object):
+    ON_TIME = "on time"
+    LATE = "late"
+    NO_DATA = "no data"
+    INSUFFICIENT_DATA = "insufficient data"
+
+
+def _reported_on_time(reminder_date, last_report_date):
+    cutoff_date = reminder_date + timedelta(days=5)
+    if last_report_date < reminder_date:
+        return OnTimeStates.INSUFFICIENT_DATA
+    elif last_report_date < cutoff_date:
+        return OnTimeStates.ON_TIME
+    else:
+        return OnTimeStates.LATE
+
+
+def icon_format(status, val):
+    if status == OnTimeStates.ON_TIME:
+        return '<span class="icon-ok" style="color:green"/>%s' % val
+    if status == OnTimeStates.LATE or status == OnTimeStates.INSUFFICIENT_DATA:
+        return '<span class="icon-warning-sign" style="color:orange"/>%s' % val
+
+
+def _months_or_default(val, default_value):
+    try:
+        return "%0.2f" % val
+    except TypeError:
+        return default_value
+
+
 class DistrictSohPercentageTableData(ILSData):
     title = 'Percentage'
     slug = 'district_percentage_table'
@@ -144,12 +179,137 @@ class DistrictSohPercentageTableData(ILSData):
 
     @property
     def headers(self):
-        return []
+        if self.config['products']:
+            products = SQLProduct.objects.filter(product_id__in=self.config['products'],
+                                                 domain=self.config['domain']).order_by('code')
+        else:
+            products = SQLProduct.objects.filter(domain=self.config['domain']).order_by('code')
+        headers = DataTablesHeader(*[
+            DataTablesColumn(_('MSD Code')),
+            DataTablesColumn(_('Facility Name')),
+            DataTablesColumn(_('D G')),
+            DataTablesColumn(_('Last Reported')),
+            DataTablesColumn(_('Hist. Resp. Rate')),
+        ])
+
+        for p in products:
+            headers.add_column(DataTablesColumn(_(p.code)))
+
+        return headers
 
     @property
     def rows(self):
         rows = []
+        if not self.config['products']:
+            products = SQLProduct.objects.filter(domain=self.config['domain']).order_by('code')
+        else:
+            products = SQLProduct.objects.filter(product_id__in=self.config['products'],
+                                                 domain=self.config['domain']).order_by('code')
+
+        if self.config['location_id']:
+            location = Location.get(self.config['location_id'])
+            for loc in location.children:
+                supply_point = loc.linked_supply_point()
+
+                def get_last_reported():
+                    last_bd_of_the_month = get_business_day_of_month(int(self.config['year']),
+                                                                     int(self.config['month']),
+                                                                     -1)
+                    st = StockTransaction.objects.filter(
+                        case_id=supply_point._id,
+                        type='stockonhand',
+                        report__date__lte=last_bd_of_the_month
+                    ).order_by('-report__date')
+
+                    last_of_last_month = datetime(int(self.config['year']),
+                                                  int(self.config['month']),
+                                                  1) - timedelta(days=1)
+                    last_bd_of_last_month = datetime.combine(get_business_day_of_month(last_of_last_month.year,
+                                                             last_of_last_month.month,
+                                                             -1), time())
+                    if st:
+                        status = _reported_on_time(last_bd_of_last_month, st[0].report.date)
+                        return status, st[0].report.date.date()
+                    else:
+                        status = OnTimeStates.NO_DATA
+                        return status, None
+
+                def get_hisp_resp_rate():
+                    statuses = SupplyPointStatus.objects.filter(supply_point=loc._id,
+                                                                status_type=SupplyPointStatusTypes.SOH_FACILITY)
+                    if not statuses: return None
+                    status_month_years = set([(x.status_date.month, x.status_date.year) for x in statuses])
+                    denom = len(status_month_years)
+                    num = 0
+                    for s in status_month_years:
+                        f = statuses.filter(status_date__month=s[0], status_date__year=s[1]).filter(
+                            Q(status_value=SupplyPointStatusValues.SUBMITTED) |
+                            Q(status_value=SupplyPointStatusValues.NOT_SUBMITTED) |
+                            Q(status_value=SupplyPointStatusValues.RECEIVED) |
+                            Q(status_value=SupplyPointStatusValues.NOT_RECEIVED)).order_by("-status_date")
+                        if f.count(): num += 1
+
+                    return float(num)/float(denom), num, denom
+
+                status, last_reported = get_last_reported()
+                hisp = get_hisp_resp_rate()
+                row_data = [
+                    loc.site_code,
+                    loc.name,
+                    loc.metadata['groups'][0] if 'groups' in loc.metadata else '?',
+                    icon_format(status, last_reported),
+                    "<span title='%d of %d'>%s%%</span>" % (hisp[1], hisp[2], floatformat(hisp[0]*100.0)) if hisp else "No data"
+                ]
+
+                for product in products:
+                    last_of_the_month = get_day_of_month(int(self.config['year']), int(self.config['month']), -1)
+                    first_of_the_next_month = last_of_the_month + timedelta(days=1)
+                    srs = StockTransaction.objects.filter(report__date__lt=first_of_the_next_month).order_by("-report__date")\
+                        .filter(case_id=supply_point._id, product_id=product.product_id)
+
+                    if srs.exists():
+                        ss = StockState.objects.get(case_id=supply_point._id, product_id=product.product_id)
+
+                        def calculate_months_remaining(stock_state, quantity):
+                            consumption = stock_state.get_monthly_consumption()
+                            if consumption is not None and consumption > 0 \
+                              and quantity is not None:
+                                return float(quantity) / float(consumption)
+                            elif quantity == 0:
+                                return 0
+                            return None
+
+                        val = calculate_months_remaining(ss, srs[0].stock_on_hand)
+                        ret = _months_or_default(val, -1)
+                    else:
+                        ret = -1
+
+                    row_data.append(product_format(ret, srs))
+
+                rows.append(row_data)
+
         return rows
+
+
+def product_format(ret, srs):
+    NO_DATA = -1
+    STOCKOUT = 0.00
+    LOW = 3
+    ADEQUATE = 6
+
+    mos = float(ret)
+    if mos == NO_DATA:
+        if srs.exists():
+            return '<span style="color:grey">%.0f</span>' % srs[0].stock_on_hand
+        return '<span style="color:grey">%s</span>' % 'No Data'
+    elif mos == STOCKOUT:
+        return '<span class="icon-remove" style="color:red"/>%.0f' % srs[0].stock_on_hand
+    elif mos < LOW:
+        return '<span class="icon-warning-sign" style="color:orange"/>%.0f' % srs[0].stock_on_hand
+    elif mos <= ADEQUATE:
+        return '<span class="icon-ok" style="color:green"/>%.0f' % srs[0].stock_on_hand
+    elif mos > ADEQUATE:
+        return '<span class="icon-arrow-up" style="color:purple"/>%.0f' % srs[0].stock_on_hand
 
 
 class StockOnHandReport(DetailsReport):
@@ -170,18 +330,19 @@ class StockOnHandReport(DetailsReport):
     @property
     def data_providers(self):
         config = self.report_config
+        data_providers = []
+        if config['org_summary']:
+            location = Location.get(config['org_summary'].supply_point)
 
-        location = Location.get(config['org_summary'].supply_point)
+            data_providers = [
+                SohSubmissionData(config=config, css_class='row_chart_all'),
+                ProductAvailabilitySummary(config=config, css_class='row_chart_all', chart_stacked=False),
+            ]
 
-        data_providers = [
-            SohSubmissionData(config=config, css_class='row_chart_all'),
-            ProductAvailabilitySummary(config=config, css_class='row_chart_all', chart_stacked=False),
-        ]
-
-        if location.location_type.upper() == 'DISTRICT':
-            data_providers.append(DistrictSohPercentageTableData(config=config, css_class='row_chart_all'))
-        else:
-            data_providers.append(SohPercentageTableData(config=config, css_class='row_chart_all'))
+            if location.location_type.upper() == 'DISTRICT':
+                data_providers.append(DistrictSohPercentageTableData(config=config, css_class='row_chart_all'))
+            else:
+                data_providers.append(SohPercentageTableData(config=config, css_class='row_chart_all'))
         return data_providers
 
     @property
