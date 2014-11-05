57adb16468b558b36545269a490f44000d0a12d3
Simon Kelly <skelly@dimagi.com>
2014-11-05 17:53:48 +0200

    add line profile decorator

--- dimagi/utils/decorators/profile.py
+++ dimagi/utils/decorators/profile.py
@@ -44,3 +44,30 @@ def profile(log_file):
 
         return _inner
     return _outer
+
+try:
+    from line_profiler import LineProfiler
+
+    def line_profile(follow=[]):
+        def inner(func):
+            def profiled_func(*args, **kwargs):
+                try:
+                    profiler = LineProfiler()
+                    profiler.add_function(func)
+                    for f in follow:
+                        profiler.add_function(f)
+                    profiler.enable_by_count()
+                    return func(*args, **kwargs)
+                finally:
+                    profiler.print_stats()
+            return profiled_func
+        return inner
+
+except ImportError:
+    def line_profile(follow=[]):
+        "Helpful if you accidentally leave in production!"
+        def inner(func):
+            def nothing(*args, **kwargs):
+                return func(*args, **kwargs)
+            return nothing
+        return inner
