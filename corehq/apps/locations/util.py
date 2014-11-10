379614d37dfe9ca50ccb5e42f568d3d861912713
Ethan Soergel <esoergel@gmail.com>
2014-11-10 15:00:02 +0100

    Refactor location export to avoid duplicate calls

--- corehq/apps/locations/util.py
+++ corehq/apps/locations/util.py
@@ -2,6 +2,7 @@ from corehq.apps.commtrack.models import Product, SupplyPointCase
 from corehq.apps.custom_data_fields.models import CustomDataFieldsDefinition
 from corehq.apps.locations.models import Location, root_locations
 from corehq.apps.domain.models import Domain
+from dimagi.utils.decorators.memoized import memoized
 from dimagi.utils.excel import flatten_json, json_to_headers
 from couchdbkit import ResourceNotFound
 from dimagi.utils.couch.loosechange import map_reduce
@@ -65,10 +66,6 @@ def allowed_child_types(domain, parent):
     return parent_child(domain).get(parent_type, [])
 
 
-def get_loc_config(domain):
-    return dict((lt.name, lt) for lt in Domain.get_by_name(domain).commtrack_settings.location_types)
-
-
 def lookup_by_property(domain, prop_name, val, scope, root=None):
     if root and not isinstance(root, basestring):
         root = root._id
@@ -95,53 +92,6 @@ def lookup_by_property(domain, prop_name, val, scope, root=None):
     return set(row['id'] for row in Location.get_db().view(index_view, startkey=startkey, endkey=startkey + [{}]))
 
 
-def get_default_column_data(domain, location_types):
-    data = {
-        'headers': {},
-        'values': {}
-    }
-
-    if Domain.get_by_name(domain).commtrack_settings.individual_consumption_defaults:
-        products = Product.by_domain(domain)
-
-        supply_point_map = SupplyPointCase.get_location_map_by_domain(domain)
-
-        consumption_dict = build_consumption_dict(domain)
-
-        if not consumption_dict:
-            return data
-
-        for loc_type in location_types:
-            loc = get_loc_config(domain)[loc_type]
-            if not loc.administrative:
-                data['headers'][loc_type] = [
-                    'default_' +
-                    p.code for p in products
-                ]
-
-                locations = Location.filter_by_type(domain, loc_type)
-                for loc in locations:
-                    if loc._id in supply_point_map:
-                        sp_id = supply_point_map[loc._id]
-                    else:
-                        # this only happens if the supply point case did
-                        # not already exist
-                        sp_id = SupplyPointCase.get_or_create_by_location(loc)._id
-
-                    data['values'][loc._id] = [
-                        get_loaded_default_monthly_consumption(
-                            consumption_dict,
-                            domain,
-                            p._id,
-                            loc_type,
-                            sp_id
-                        ) or '' for p in products
-                    ]
-            else:
-                data['headers'][loc_type] = []
-    return data
-
-
 def get_location_data_model(domain):
     from .views import LocationFieldsView
     return CustomDataFieldsDefinition.get_or_create(
@@ -150,65 +100,111 @@ def get_location_data_model(domain):
     )
 
 
-def _loc_type_dict(domain, loc_type, data_model):
-    uncategorized_keys = set()
-    tab_rows = []
-    for loc in Location.filter_by_type(domain, loc_type):
+class LocationExporter(object):
+    def __init__(self, domain, include_consumption=False):
+        self.domain = domain
+        self.commtrack_settings = Domain.get_by_name(domain).commtrack_settings
+        self._include_consumption = include_consumption
+        self.data_model = get_location_data_model(domain)
 
-        model_data, uncategorized_data = \
-            data_model.get_model_and_uncategorized(loc.metadata)
+    @property
+    @memoized
+    def consumption_dict(self):
+        return build_consumption_dict(self.domain)
 
-        uncategorized_keys.add(uncategorized_data.keys())
+    @property
+    @memoized
+    def include_consumption(self):
+        if bool(
+            self._include_consumption
+            and self.commtrack_settings.individual_consumption_defaults
+            # FIXME I'm pretty sure this is a bug, but it's consistent with
+            # master - if build_consumption_dict returns {}, return nothing.
+            # I expect the intended behavior is to return headers for
+            # consumption defaults, just don't bother trying to look up values
+            and self.consumption_dict
+        ):
+            # we'll be needing these, so init 'em:
+            self.products = Product.by_domain(self.domain)
+            self.product_codes = [p.code for p in self.products]
+            self.supply_point_map = SupplyPointCase.get_location_map_by_domain(self.domain)
+            self.administrative_types = {
+                lt.name for lt in self.commtrack_settings.location_types
+                if lt.administrative
 
-        loc_dict = {
-            'site_code': loc.site_code,
-            'name': loc.name,
-            'parent_site_code': loc.parent.site_code if loc.parent else '',
-            'latitude': loc.latitude or '',
-            'longitude': loc.longitude or '',
-            'data': model_data,
-            'uncategorized_data': uncategorized_data,
+            }
+            return True
+        return False
+
+    def get_consumption(self, loc):
+        if (
+            not self.include_consumption
+            or loc.location_type in self.administrative_types
+        ):
+            return {}
+        if loc._id in self.supply_point_map:
+            sp_id = self.supply_point_map[loc._id]
+        else:
+            # this only happens if the supply point case did
+            # not already exist
+            sp_id = SupplyPointCase.get_or_create_by_location(loc)._id
+        return {
+            p.code: get_loaded_default_monthly_consumption(
+                self.consumption_dict,
+                self.domain,
+                p._id,
+                loc.location_type,
+                sp_id
+            ) or ''
+            for p in self.products
         }
 
-        # TODO format defaults to be a proper k/v mapping
-        # Does it need a prefix?  How do we avoid name conflicts?
-        # if loc._id in defaults['values']:
-            # loc_row.update(defaults['values'][loc._id])
+    def _loc_type_dict(self, loc_type):
+        uncategorized_keys = set()
+        tab_rows = []
+        for loc in Location.filter_by_type(self.domain, loc_type):
 
-        tab_rows.append(dict(flatten_json(loc_dict)))
+            model_data, uncategorized_data = \
+                self.data_model.get_model_and_uncategorized(loc.metadata)
 
-    tab_headers = ['site_code', 'name', 'parent_site_code', 'latitude', 'longitude']
-    tab_headers.extend(json_to_headers(
-        {'data': {field.slug: None for field in data_model.fields}}
-    ))
-    # TODO
-    # tab_headers.extent(defaults['headers'].get(loc_type, []))
-    tab_headers.extend(json_to_headers(
-        {'uncategorized_data': {key: None for key in uncategorized_keys}}
-    ))
+            uncategorized_keys.update(uncategorized_data.keys())
 
-    return (loc_type, {
-        'headers': tab_headers,
-        'rows': tab_rows,
-    })
+            loc_dict = {
+                'site_code': loc.site_code,
+                'name': loc.name,
+                'parent_site_code': loc.parent.site_code if loc.parent else '',
+                'latitude': loc.latitude or '',
+                'longitude': loc.longitude or '',
+                'data': model_data,
+                'uncategorized_data': uncategorized_data,
+                'consumption': self.get_consumption(loc),
+            }
+
+            tab_rows.append(dict(flatten_json(loc_dict)))
+
+        tab_headers = ['site_code', 'name', 'parent_site_code', 'latitude', 'longitude']
+        def _extend_headers(prefix, headers):
+            tab_headers.extend(json_to_headers(
+                {prefix: {header: None for header in headers}}
+            ))
+        _extend_headers('data', (f.slug for f in self.data_model.fields))
+        _extend_headers('uncategorized_data', uncategorized_keys)
+        if self.include_consumption and loc_type not in self.administrative_types:
+            _extend_headers('consumption', self.product_codes)
+
+        return (loc_type, {
+            'headers': tab_headers,
+            'rows': tab_rows,
+        })
+
+    def get_export_dict(self):
+        return [self._loc_type_dict(loc_type.name)
+                for loc_type in self.commtrack_settings.location_types]
 
 
 def dump_locations(response, domain, include_consumption=False):
-    location_types = defined_location_types(domain)
-    data_model = get_location_data_model(domain)
-
-    # if include_consumption:
-        # defaults = get_default_column_data(domain, location_types)
-    # else:
-        # defaults = {
-            # 'headers': {},
-            # 'values': {}
-        # }
-
-    locations = [_loc_type_dict(domain, loc_type, data_model)
-                 for loc_type in location_types]
-
-    result = write_to_file(locations)
+    exporter = LocationExporter(domain, include_consumption=include_consumption)
+    result = write_to_file(exporter.get_export_dict())
     response.write(result)
 
 
@@ -229,21 +225,12 @@ def write_to_file(locations):
     """
     outfile = StringIO()
     writer = Excel2007ExportWriter()
-    writer.open(
-        header_table=[
-            (loc_type, tab['headers'])
-            for loc_type, tab in locations
-        ],
-        file=outfile,
-    )
+    header_table = [(loc_type, [tab['headers']]) for loc_type, tab in locations]
+    writer.open(header_table=header_table, file=outfile)
     for loc_type, tab in locations:
         headers = tab['headers']
-        writer.write([(
-            loc_type,
-            [
-                [row.get(header, '') for header in headers]
-                for row in tab['rows']
-            ]
-        )])
+        tab_rows = [[row.get(header, '') for header in headers]
+                    for row in tab['rows']]
+        writer.write([(loc_type, tab_rows)])
     writer.close()
     return outfile.getvalue()
