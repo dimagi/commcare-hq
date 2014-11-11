18521299cf3ac722744fe7479787258a5889517a
Jonathan Emord <jonathan.emord@gmail.com>
2014-11-11 09:25:52 -0500

    Look for list of domains in get_document_or_404

--- corehq/util/couch.py
+++ corehq/util/couch.py
@@ -14,7 +14,7 @@ def get_document_or_404(cls, domain, doc_id, additional_doc_types=None):
     except ResourceNotFound:
         raise Http404()
 
-    if unwrapped['domain'] != domain or unwrapped['doc_type'] not in allowed_doc_types:
+    if domain not in unwrapped['domains'] or unwrapped['doc_type'] not in allowed_doc_types:
         raise Http404()
 
     try:
