d9adb969aab4b02af9b4fda4ad115bc8d17d4c23
Nick Pellegrino <npellegrino@dimagi.com>
2014-11-12 15:58:32 -0500

    add tests for default case properties

--- corehq/apps/userreports/tests/test_app_manager_integration.py
+++ corehq/apps/userreports/tests/test_app_manager_integration.py
@@ -2,7 +2,8 @@ import json
 import os
 from django.test import SimpleTestCase
 from corehq.apps.app_manager.models import Application
-from corehq.apps.userreports.app_manager import get_case_data_sources
+from corehq.apps.userreports.app_manager import get_case_data_sources, \
+    get_default_case_property_datatypes
 
 
 class AppManagerDataSourceConfigTest(SimpleTestCase):
@@ -29,16 +30,17 @@ class AppManagerDataSourceConfigTest(SimpleTestCase):
         self.assertFalse(generated_filter.filter({'doc_type': 'CommCareCase', 'domain': app.domain, 'type': 'not-ticket'}))
 
         # check the indicators
-        expected_columns = ["doc_id", "category", "priority", "starred", "estimate"]
-        for i, col in enumerate(expected_columns):
-            col_back = data_source.get_columns()[i]
-            self.assertEqual(col, col_back.id)
+        expected_columns = set(["doc_id", "modified_on", "user_id", "opened_on", "owner_id", "name", "category", "priority", "starred", "estimate"])
+        self.assertEqual(expected_columns, set(col_back.id for col_back in data_source.get_columns()))
 
         sample_doc = dict(
             _id='some-doc-id',
-            # opened_on=date_opened,
-            # owner_id='some-user-id',
             doc_type="CommCareCase",
+            modified_on="2014-11-12T15:37:49",
+            user_id="23407238074",
+            opened_on="2014-11-11T23:34:34",
+            owner_id="0923409230948",
+            name="priority ticket",
             domain=app.domain,
             type='ticket',
             category='bug',
@@ -47,7 +49,14 @@ class AppManagerDataSourceConfigTest(SimpleTestCase):
             estimate=2,
         )
         def _get_column_property(column):
-            return result.column.id if result.column.id != 'doc_id' else '_id'
+            return column.id if column.id != 'doc_id' else '_id'
 
+
+        default_case_property_datatypes = get_default_case_property_datatypes()
         for result in data_source.get_values(sample_doc):
             self.assertEqual(sample_doc[_get_column_property(result.column)], result.value)
+            if result.column.id in default_case_property_datatypes:
+                self.assertEqual(
+                    result.column.datatype,
+                    default_case_property_datatypes[result.column.id]
+                )
