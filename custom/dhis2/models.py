57556b6cf8b1bbbc339d47dc97aa4404f06afc7a
Norman Hooper <nhooper@dimagi.com>
2014-11-20 17:30:26 +0200

    WIP: Models -- move to lookup tables. Tasks -- Move API stuff out

--- custom/dhis2/models.py
+++ custom/dhis2/models.py
@@ -1,12 +1,46 @@
 from django.db import models
 
 
+# TODO: Do we want all this? We don't care. We want the ID and the name, and maybe the parent/children
+
+
+class Dhis2OrgUnitAccess(models.Model):
+    update = models.BooleanField()
+    externalize = models.BooleanField()
+    delete = models.BooleanField()
+    write = models.BooleanField()
+    read = models.BooleanField()
+    manage = models.BooleanField()
+
+
 class Dhis2OrgUnit(models.Model):
-    remote_id = models.IntegerField(
+    # List/summary attributes
+    dhis2_id = models.CharField(
+        max_length=64,  # Only ever seen 11-char IDs, but who knows, right?
         help_text='Stored as a custom field on a mobile worker called dhis2_organization_unit_id',
         db_index=True, unique=True)
     name = models.CharField(max_length=255)
-    parent = models.ForeignKey('Dhis2OrgUnit', null=True, blank=True)
+    created = models.DateTimeField()
+    last_updated = models.DateTimeField(null=True, blank=True)
+    code = models.CharField(max_length=128, blank=True)  # Code is user-defined
+    href = models.CharField(max_length=255, blank=True)
+    # Detail attributes
+    # level = models.IntegerField()
+    # uuid = models.CharField(max_length=36)
+    # short_name = models.CharField()
+    # feature_type = models.CharField()
+    # external_access = models.BooleanField()
+    # coordinates = models.CharField()
+    # active = models.BooleanField()
+    # display_name = models.CharField()
+    parent = models.ForeignKey('Dhis2OrgUnit', null=True, blank=True, related_name='children')
+    access = models.OneToOneField(Dhis2OrgUnitAccess)
+    # users
+    # organisation_unit_groups
+    # children  # See parent related_name
+    # data_sets
+    # attribute_values
+    # user_group_accesses
 
     def delete(self, *args, **kwargs):
         # TODO: Make sure this organization unit is not in use
--- custom/dhis2/tasks.py
+++ custom/dhis2/tasks.py
@@ -1,6 +1,47 @@
+from datetime import datetime
 from celery.schedules import crontab
 from celery.task import periodic_task
+from custom.dhis2.models import Dhis2OrgUnit
+from django.conf import settings
 import requests
+
+
+class JsonApiError(Exception):
+    pass
+
+
+class JsonApiRequest(object):
+    """
+    Wrap requests with URL, header and authentication for DHIS2 API
+
+    Return HTTP status, JSON
+    """
+
+    def __init__(self, host, username, password):
+        self.baseurl = host + '/api/'
+        self.header = {'Accept': 'application/json'}
+        self.auth = (username, password)
+
+    @staticmethod
+    def json_or_error(response):
+        if not 200 <= response.status_code < 300:
+            raise JsonApiError('API request failed with HTTP status %s: %s' %
+                               (response.status_code, response.text))
+        return response.status_code, response.json()
+
+    def get(self, path, **kwargs):
+        try:
+            response = requests.get(self.baseurl + path, header=self.header, auth=self.auth, **kwargs)
+        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
+            raise JsonApiError(str(err))
+        return JsonApiRequest.json_or_error(response)
+
+    def post(self, path, data, **kwargs):
+        try:
+            response = requests.post(self.baseurl + path, data, header=self.header, auth=self.auth, **kwargs)
+        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
+            raise JsonApiError(str(err))
+        return JsonApiRequest.json_or_error(response)
 
 
 @periodic_task(run_every=crontab(minute=3, hour=3))  # Run daily at 03h03
@@ -8,7 +49,35 @@ def sync_org_units():
     """
     Synchronize DHIS2 Organization Units with local data
     """
-    pass
+    request = JsonApiRequest(settings.DHIS2_HOST, settings.DHIS2_USERNAME, settings.DHIS2_PASSWORD)
+    try:
+        __, json = request.get('organisationUnits', params={'paging': 'false', 'links': 'false'})
+    except JsonApiError:
+        # TODO: Task failed. Try again, or abort
+        raise
+    their_org_units = {ou['id']: ou for ou in json['organisationUnits']}
+    our_org_units = do_some_magic_here()  # Dict keyed on ID
+    # Add new org units
+    for id_ in their_org_units:
+        if id_ not in our_org_units:
+            last_updated = datetime.strptime(their_org_units[id_]['lastUpdated'], '%Y-%m-%dT%H:%M:%S.%f+0000') \
+                if their_org_units[id_].get('lastUpdated') \
+                else None
+            # TODO: Get details
+            # TODO: Assert parent
+            # TODO: Add parent if necessary, recurse.
+            ou = Dhis2OrgUnit(
+                dhis2_id=id_,
+                name=their_org_units[id_]['name'],
+                created=datetime.strptime(their_org_units[id_]['created'], '%Y-%m-%dT%H:%M:%S.%f+0000'),
+                last_updated=last_updated,
+                code=their_org_units[id_]['code'])
+            ou.save()
+        # TODO: Sync other details, esp parent/children
+    for id_ in our_org_units:
+        if id_ not in their_org_units:
+            ou = Dhis2OrgUnit.objects.get(id_)
+            ou.delete()  # TODO: Safely
 
 
 def push_child_entities():
