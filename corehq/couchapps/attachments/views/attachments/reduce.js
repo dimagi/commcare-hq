7380e4c6f7d2d03d87602f56b638cbc0d3cbd452
Jonathan Emord <jemord@dimagi.com>
2014-11-20 17:38:26 -0500

    Fix indentation

--- corehq/couchapps/attachments/views/attachments/reduce.js
+++ corehq/couchapps/attachments/views/attachments/reduce.js
@@ -7,11 +7,11 @@ function (key, values, rereduce) {
     }
     for(i = 0; i < values.length; i += 1) {
         if (values[i].hasOwnProperty('attachments')) {
-        for (var j in values[i].attachments) {
-            if (values[i].attachments.hasOwnProperty(j)) {
-                value += values[i].attachments[j].length;
+            for (var j in values[i].attachments) {
+                if (values[i].attachments.hasOwnProperty(j)) {
+                    value += values[i].attachments[j].length;
+                }
             }
-        }
         }
     }
     return value;
