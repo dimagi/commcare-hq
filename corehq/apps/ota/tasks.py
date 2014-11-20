0979749df382cccc391836075f770148db4338d7
Simon Kelly <skelly@dimagi.com>
2014-11-20 17:32:04 +0200

    add task to prime restore cache

--- corehq/apps/ota/tasks.py
+++ corehq/apps/ota/tasks.py
@@ -1,1 +1,23 @@
-__author__ = 'skelly'
+from celery.task import task
+from couchdbkit.exceptions import ResourceNotFound
+from corehq.apps.ota.views import get_restore_response
+from corehq.apps.users.models import CommCareUser
+
+
+@task
+def prime_restore(user_id, since=None, version='1.0', state=None, items=False, cache_timeout=None):
+    try:
+        couch_user = CommCareUser.get(user_id)
+    except ResourceNotFound:
+        return
+
+    get_restore_response(
+        couch_user.domain,
+        couch_user,
+        since=since,
+        version=version,
+        state=state,
+        items=items,
+        force_cache=True,
+        cache_timeout=cache_timeout
+    )
