87ebba86977239ac559082d8f5d61d35b590ec6a
Jonathan Emord <jemord@dimagi.com>
2014-11-21 14:40:14 -0500

    Update domain index

--- corehq/pillows/mappings/domain_mapping.py
+++ corehq/pillows/mappings/domain_mapping.py
@@ -1,4 +1,4 @@
-DOMAIN_INDEX="hqdomains_da273aa8d2cf04c231535fed72f0f778"
+DOMAIN_INDEX="hqdomains_06c30d81502bd6a7afe71946abbad437"
 DOMAIN_MAPPING={'_meta': {'comment': 'Yedi modified on 3/20/2014',
            'created': None},
  'date_detection': False,
