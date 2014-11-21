871fa2c47dd46f3bfbbde441a12dd0bf04b7502a
Simon Kelly <skelly@dimagi.com>
2014-11-21 17:07:53 +0200

    actually check if task has succeeded

--- soil/util.py
+++ soil/util.py
@@ -42,18 +42,20 @@ def get_download_context(download_id, check_state=False):
     download_data = DownloadBase.get(download_id)
     if download_data is None:
         download_data = DownloadBase(download_id=download_id)
-        try:
-            if download_data.task.failed():
-                raise TaskFailedError()
-        except (TypeError, NotImplementedError):
-            # no result backend / improperly configured
-            pass
+
+    try:
+        if download_data.task.failed():
+            raise TaskFailedError()
+    except (TypeError, NotImplementedError):
+        # no result backend / improperly configured
+        pass
     else:
         if not check_state:
-            is_ready=True
+            is_ready = True
         elif download_data.task.state == 'SUCCESS':
             is_ready = True
-            context['result'] = download_data.task.result.get('messages')
+            result = download_data.task.result
+            context['result'] = result and result.get('messages')
 
     alive = True
     if heartbeat_enabled():
