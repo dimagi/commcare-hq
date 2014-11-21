6299105a1119fd1a69fe03f4b137433a33542205
Norman Hooper <nhooper@dimagi.com>
2014-11-21 15:55:56 +0200

    DHIS2: Models use lookup tables

--- custom/dhis2/models.py
+++ custom/dhis2/models.py
@@ -1,48 +1,98 @@
-from django.db import models
+# from django.db import models
+from corehq.apps.fixtures.models import FixtureDataItem, FixtureDataType
+import requests
 
 
-# TODO: Do we want all this? We don't care. We want the ID and the name, and maybe the parent/children
+class JsonApiError(Exception):
+    pass
 
 
-class Dhis2OrgUnitAccess(models.Model):
-    update = models.BooleanField()
-    externalize = models.BooleanField()
-    delete = models.BooleanField()
-    write = models.BooleanField()
-    read = models.BooleanField()
-    manage = models.BooleanField()
+class JsonApiRequest(object):
+    """
+    Wrap requests with URL, header and authentication for DHIS2 API
+    """
+
+    def __init__(self, host, username, password):
+        self.baseurl = host + '/api/'
+        self.header = {'Accept': 'application/json'}
+        self.auth = (username, password)
+
+    @staticmethod
+    def json_or_error(response):
+        """
+        Return HTTP status, JSON
+
+        :raises JsonApiError if HTTP status is not in the 200 (OK) range
+        """
+        if not 200 <= response.status_code < 300:
+            raise JsonApiError('API request failed with HTTP status %s: %s' %
+                               (response.status_code, response.text))
+        return response.status_code, response.json()
+
+    def get(self, path, **kwargs):
+        try:
+            response = requests.get(self.baseurl + path, header=self.header, auth=self.auth, **kwargs)
+        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
+            raise JsonApiError(str(err))
+        return JsonApiRequest.json_or_error(response)
+
+    def post(self, path, data, **kwargs):
+        try:
+            response = requests.post(self.baseurl + path, data, header=self.header, auth=self.auth, **kwargs)
+        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
+            raise JsonApiError(str(err))
+        return JsonApiRequest.json_or_error(response)
 
 
-class Dhis2OrgUnit(models.Model):
-    # List/summary attributes
-    dhis2_id = models.CharField(
-        max_length=64,  # Only ever seen 11-char IDs, but who knows, right?
-        help_text='Stored as a custom field on a mobile worker called dhis2_organization_unit_id',
-        db_index=True, unique=True)
-    name = models.CharField(max_length=255)
-    created = models.DateTimeField()
-    last_updated = models.DateTimeField(null=True, blank=True)
-    code = models.CharField(max_length=128, blank=True)  # Code is user-defined
-    href = models.CharField(max_length=255, blank=True)
-    # Detail attributes
-    # level = models.IntegerField()
-    # uuid = models.CharField(max_length=36)
-    # short_name = models.CharField()
-    # feature_type = models.CharField()
-    # external_access = models.BooleanField()
-    # coordinates = models.CharField()
-    # active = models.BooleanField()
-    # display_name = models.CharField()
-    parent = models.ForeignKey('Dhis2OrgUnit', null=True, blank=True, related_name='children')
-    access = models.OneToOneField(Dhis2OrgUnitAccess)
-    # users
-    # organisation_unit_groups
-    # children  # See parent related_name
-    # data_sets
-    # attribute_values
-    # user_group_accesses
+class FixtureManager(object):
+    """
+    Reuses the Django manager pattern for fixtures
+    """
 
-    def delete(self, *args, **kwargs):
-        # TODO: Make sure this organization unit is not in use
-        # TODO: Try to delete children
-        super(Dhis2OrgUnit, self).delete(*args, **kwargs)
+    def __init__(self, model_class, domain, tag):
+        self.model_class = model_class
+        self.domain = domain
+        self.tag = tag
+
+    def get(self, id_):
+        data_type = FixtureDataType.by_domain_tag(self.domain, self.tag).one()
+        item = FixtureDataItem.by_field_value(self.domain, data_type, 'id_', id_).one()
+        return self.model_class(_fixture_id=item.get_id, **item.fields)
+
+    def all(self):
+        for item in FixtureDataItem.get_item_list(self.domain, self.tag):
+            yield self.model_class(_fixture_id=item.get_id, **item.fields)
+
+
+class Dhis2OrgUnit(object):
+    """
+    Simplify the management of DHIS2 Organisation Units, which are
+    stored in a lookup table.
+    """
+
+    objects = None  # Manager is set outside of class definition so that we can pass the class to the manager
+
+    def __init__(self, id_, name, _fixture_id=None):
+        self.id_ = id_  # param is called "id_" because "id" is a built-in
+        self.name = name
+        self._fixture_id = _fixture_id
+
+    def save(self):
+        data_type = FixtureDataType.by_domain_tag(self.objects.domain, self.objects.tag).one()
+        data_item = FixtureDataItem()
+        data_item.data_type_id = data_type.get_id
+        data_item.domain = self.objects.domain
+        data_item.fields = {
+            'id_': self.id_,   # Use key "id_" instead of "id" so we have the option of passing as kwargs to __init__
+            'name': self.name  # ... which is exactly what FixtureManager does in objects.get() and objects.all()
+        }
+        data_item.save()
+        self._fixture_id = data_item.get_id
+
+    def delete(self):
+        if self._fixture_id is None:
+            return
+        item = FixtureDataItem.get(self._fixture_id)
+        item.delete()
+
+Dhis2OrgUnit.objects = FixtureManager(Dhis2OrgUnit, 'dhis2', 'dhis2_org_unit')
--- custom/dhis2/tasks.py
+++ custom/dhis2/tasks.py
@@ -1,47 +1,7 @@
-from datetime import datetime
 from celery.schedules import crontab
 from celery.task import periodic_task
-from custom.dhis2.models import Dhis2OrgUnit
+from custom.dhis2.models import JsonApiRequest, JsonApiError, Dhis2OrgUnit
 from django.conf import settings
-import requests
-
-
-class JsonApiError(Exception):
-    pass
-
-
-class JsonApiRequest(object):
-    """
-    Wrap requests with URL, header and authentication for DHIS2 API
-
-    Return HTTP status, JSON
-    """
-
-    def __init__(self, host, username, password):
-        self.baseurl = host + '/api/'
-        self.header = {'Accept': 'application/json'}
-        self.auth = (username, password)
-
-    @staticmethod
-    def json_or_error(response):
-        if not 200 <= response.status_code < 300:
-            raise JsonApiError('API request failed with HTTP status %s: %s' %
-                               (response.status_code, response.text))
-        return response.status_code, response.json()
-
-    def get(self, path, **kwargs):
-        try:
-            response = requests.get(self.baseurl + path, header=self.header, auth=self.auth, **kwargs)
-        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
-            raise JsonApiError(str(err))
-        return JsonApiRequest.json_or_error(response)
-
-    def post(self, path, data, **kwargs):
-        try:
-            response = requests.post(self.baseurl + path, data, header=self.header, auth=self.auth, **kwargs)
-        except Exception as err:  # TODO: What exception?! (socket error; authentication error; ...?)
-            raise JsonApiError(str(err))
-        return JsonApiRequest.json_or_error(response)
 
 
 @periodic_task(run_every=crontab(minute=3, hour=3))  # Run daily at 03h03
@@ -53,31 +13,20 @@ def sync_org_units():
     try:
         __, json = request.get('organisationUnits', params={'paging': 'false', 'links': 'false'})
     except JsonApiError:
-        # TODO: Task failed. Try again, or abort
+        # TODO: Task failed. Try again later
         raise
     their_org_units = {ou['id']: ou for ou in json['organisationUnits']}
-    our_org_units = do_some_magic_here()  # Dict keyed on ID
+    our_org_units = {ou.id_: ou for ou in Dhis2OrgUnit.objects.all()}
     # Add new org units
-    for id_ in their_org_units:
+    for id_, ou in their_org_units.iteritems():
         if id_ not in our_org_units:
-            last_updated = datetime.strptime(their_org_units[id_]['lastUpdated'], '%Y-%m-%dT%H:%M:%S.%f+0000') \
-                if their_org_units[id_].get('lastUpdated') \
-                else None
-            # TODO: Get details
-            # TODO: Assert parent
-            # TODO: Add parent if necessary, recurse.
-            ou = Dhis2OrgUnit(
-                dhis2_id=id_,
-                name=their_org_units[id_]['name'],
-                created=datetime.strptime(their_org_units[id_]['created'], '%Y-%m-%dT%H:%M:%S.%f+0000'),
-                last_updated=last_updated,
-                code=their_org_units[id_]['code'])
-            ou.save()
-        # TODO: Sync other details, esp parent/children
+            org_unit = Dhis2OrgUnit(id_=id_, name=ou['name'])
+            org_unit.save()
+    # Delete former org units
     for id_ in our_org_units:
         if id_ not in their_org_units:
-            ou = Dhis2OrgUnit.objects.get(id_)
-            ou.delete()  # TODO: Safely
+            org_unit = Dhis2OrgUnit.objects.get(id_)
+            org_unit.delete()
 
 
 def push_child_entities():
