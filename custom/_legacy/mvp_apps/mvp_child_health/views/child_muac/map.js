b373945e95c68aaeb91bdb200c452ea23b060726
Nick Pellegrino <npellegrino@dimagi.com>
2014-12-03 17:40:59 -0500

    Revert "Revert "Revert "Lengthindicator fixbug"""

--- custom/_legacy/mvp_apps/mvp_child_health/views/child_muac/map.js
+++ custom/_legacy/mvp_apps/mvp_child_health/views/child_muac/map.js
@@ -1,7 +1,6 @@
 function (doc) {
     // !code util/mvp.js
-    if(isChildVisitForm(doc) ||
-       isChildWelfareForm(doc)) {
+    if(isChildVisitForm(doc)) {
         var indicators = get_indicators(doc);
 
         if (indicators.child_dob && indicators.child_dob.value &&
@@ -63,12 +62,6 @@ function (doc) {
                 } catch (e) {
                     // do nothing
                 }
-            }
-
-            //LengthIndicator
-            if (age >= 90*MS_IN_DAY && age < 730*MS_IN_DAY && indicators.child_length && indicators.child_length.value) {
-                var length_date  =  (indicators.length_date && indicators.length_date.value) ? new Date(indicators.length_date.value) : visit_date;
-                emit_standard(doc, length_date, {length_reading: case_id}, [doc._id]);
             }
 
             emit_special(doc, last_muac_date, last_muac_indicators, [doc._id]);
