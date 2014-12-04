7f825ac9f773e80e3299e2ac6c0a05cb96c151c8
Simon Kelly <skelly@dimagi.com>
2014-12-04 08:38:56 +0200

    allow usernames or user_ids

--- corehq/apps/ota/tasks.py
+++ corehq/apps/ota/tasks.py
@@ -6,17 +6,16 @@ from soil import DownloadBase
 
 
 @task
-def prime_restore(user_ids, version=V1, cache_timeout=None, overwrite_cache=False):
+def prime_restore(usernames_or_ids, version=V1, cache_timeout=None, overwrite_cache=False):
     from corehq.apps.ota.views import get_restore_response
-    total = len(user_ids)
+    total = len(usernames_or_ids)
     DownloadBase.set_progress(prime_restore, 0, total)
 
     ret = {'messages': []}
-    for i, user_id in enumerate(user_ids):
-        try:
-            couch_user = CommCareUser.get(user_id)
-        except ResourceNotFound:
-            ret['messages'].append('User not found: {}'.format(user_id))
+    for i, username_or_id in enumerate(usernames_or_ids):
+        couch_user = get_user(username_or_id)
+        if not couch_user:
+            ret['messages'].append('User not found: {}'.format(username_or_id))
             continue
 
         try:
@@ -36,3 +35,15 @@ def prime_restore(user_ids, version=V1, cache_timeout=None, overwrite_cache=Fals
         DownloadBase.set_progress(prime_restore, i + 1, total)
 
     return ret
+
+
+def get_user(username_or_id):
+    try:
+        couch_user = CommCareUser.get(username_or_id)
+    except ResourceNotFound:
+        try:
+            couch_user = CommCareUser.get_by_username(username_or_id)
+        except ResourceNotFound:
+            return None
+
+    return couch_user
