ec934bcb3401ed92a3f58b223baacf20bdc4ce6a
Cory Zue <czue@dimagi.com>
2014-12-08 18:06:22 -0500

    lint

--- corehq/ex-submodules/casexml/apps/case/models.py
+++ corehq/ex-submodules/casexml/apps/case/models.py
@@ -1198,11 +1198,13 @@ def _action_sort_key_function(case):
             return cmp(first_action.server_date, second_action.server_date)
         else:
             form_ids = list(case.xform_ids)
+
             def _sortkey(action):
                 if not action.server_date or not action.date:
                     raise MissingServerDate()
 
-                form_cmp = lambda form_id: (form_ids.index(form_id) if form_id in form_ids else sys.maxint, form_id)
+                form_cmp = lambda form_id: (form_ids.index(form_id)
+                                            if form_id in form_ids else sys.maxint, form_id)
                 # if the user is the same you should compare with the special logic below
                 # if the user is not the same you should compare just using received_on
                 return (
