7b4cb766a3775664bb13e587f6baf9767d6613ae
Tyler Wymer <twymer@dimagi.com>
2014-12-09 16:33:34 -0500

    Refactor

--- custom/ewsghana/util.py
+++ custom/ewsghana/util.py
@@ -3,7 +3,4 @@ from custom.ewsghana.models import EWSGhanaConfig
 
 def domain_has_ews_enabled(domain):
     config = EWSGhanaConfig.for_domain(domain)
-    if config and config.enabled:
-        return True
-    else:
-        return False
+    return config and config.enabled
