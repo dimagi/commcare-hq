2ce15f9bc89da625390265eeea685d190c6263f2
Norman Hooper <nhooper@dimagi.com>
2014-12-09 13:53:55 +0200

    DHIS2: WIP: Cases use external_id instead of dhis2_te_inst_id

--- custom/dhis2/tasks.py
+++ custom/dhis2/tasks.py
@@ -12,8 +12,10 @@ Required CommCare case attributes:
 
 1. dhis2_organization_unit_id: The organisation unit ID of the owner of the
    case.
-2. dhis2_te_inst_id: The ID of the DHIS2 tracked entity instance that
-   corresponds with this case.
+
+Instead of creating an attribute for the DHIS2 tracked entity instance ID, use
+external_id. This is indexed, and will allow us to fetch cases by their DHIS2
+ID efficiently.
 
 CommCare child growth monitoring forms must include:
 
@@ -21,11 +23,15 @@ CommCare child growth monitoring forms must include:
 * weight
 * mobile-calculated BMI
 * age at time of visit
-* hidden value "dhis2_processed" to indicate that the form has been sent to DHIS2 as an event
+* hidden value "dhis2_te_inst_id" whose value is taken from the case's
+  external_id
+* hidden value "dhis2_processed" to indicate that the form has been sent to
+  DHIS2 as an event
 
 DHIS2 tracked entity attributes:
 
-1. cchq_case_id: Used to refer to the corresponding CommCareHQ case. This is a hexadecimal UUID.
+1. cchq_case_id: Used to refer to the corresponding CommCareHQ case. This is a
+   hexadecimal UUID.
 
 DHIS2 needs the following two projects:
 
@@ -44,7 +50,7 @@ from celery.schedules import crontab
 from celery.task import periodic_task
 from corehq.apps.es.cases import CaseES
 from corehq.apps.es.forms import FormES
-from corehq.apps.hqcase.utils import submit_case_blocks
+from corehq.apps.hqcase.utils import submit_case_blocks, get_case_by_identifier
 from corehq.apps.users.models import CommCareUser
 from custom.dhis2.models import Dhis2Api, Dhis2OrgUnit, JsonApiRequest, JsonApiError
 from django.conf import settings
@@ -106,10 +112,10 @@ def push_child_entities(children):
     Register child entities in DHIS2 and enroll them in the Pediatric
     Nutrition Assessment and Underlying Risk Assessment programs.
 
-    :param children: A generator of cases that have properties named
-                     dhis2_organization_unit_id and dhis2_te_inst_id.
+    :param children: A generator of cases that include a properties named
+                     dhis2_organization_unit_id.
 
-    .. Note:: Once pushed, dhis2_te_inst_id is set to the ID of the
+    .. Note:: Once pushed, external_id is set to the ID of the
               tracked entity instance.
 
     This fulfills the second requirement of `DHIS2 Integration`_.
@@ -149,7 +155,7 @@ def push_child_entities(children):
         # Enroll in Underlying Risk Assessment
         dhis2_api.enroll_in_id(dhis2_child, risk_id, today)
 
-        # Set dhis2_te_inst_id in CCHQ to flag the case as pushed.
+        # Set external_id in CCHQ to flag the case as pushed.
         commcare_user = CommCareUser.get(child['owner_id'])
         close = commcare_user.to_be_deleted() or not commcare_user.is_active
         caseblock = CaseBlock(
@@ -160,7 +166,7 @@ def push_child_entities(children):
             # commcare_user.project.call_center_config.case_type
             close=close,
             update={
-                'dhis2_te_inst_id': dhis2_child['id'],
+                'external_id': dhis2_child['id'],
                 # TODO: ...
             }
         )
@@ -172,7 +178,7 @@ def pull_child_entities(domain, commcare_user_id, dhis2_children):
     """
     Create new child cases for nutrition tracking in CommCare.
 
-    Sets dhis2_te_inst_id on new child cases, and cchq_case_id on DHIS2
+    Sets external_id on new child cases, and cchq_case_id on DHIS2
     tracked entity instances. (cchq_case_id is initially unset because the
     case is new and does not exist in CommCare.)
 
@@ -191,7 +197,7 @@ def pull_child_entities(domain, commcare_user_id, dhis2_children):
     dhis2_api = Dhis2Api(settings.DHIS2_HOST, settings.DHIS2_USERNAME, settings.DHIS2_PASSWORD)
     for dhis2_child in dhis2_children:
         # Add each child separately. Although this is slower, it avoids problems if a DHIS2 API call fails
-        case = get_case_by_dhis2_te_inst_id(dhis2_child['id'])
+        case = get_case_by_external_id(domain, dhis2_child['id'])
         if case:
             case_id = case['case_id']
         else:
@@ -204,7 +210,7 @@ def pull_child_entities(domain, commcare_user_id, dhis2_children):
                 version=V2,
                 case_type=commcare_user.project.SOME_ATTRIBUTE.case_type,  # <-- TODO: What attribute?
                 update={
-                    'dhis2_te_inst_id': dhis2_child['id'],
+                    'external_id': dhis2_child['id'],
                     # TODO: ...
                 }
             )
@@ -212,6 +218,13 @@ def pull_child_entities(domain, commcare_user_id, dhis2_children):
             submit_case_blocks(casexml, commcare_user.project.name)
         dhis2_child['cchq_case_id'] = case_id
         dhis2_api.update_te_inst(dhis2_child)
+
+
+def get_case_by_external_id(domain, id_):
+    """
+    Filter cases by external_id
+    """
+    return get_case_by_identifier(domain, id_)
 
 
 def get_children_only_theirs():
@@ -226,7 +239,7 @@ def gen_children_only_ours(domain):
     """
     Returns a list of new child cases which don't have dhis2_organization_unit_id set
     """
-    # Fetch cases where dhis2_organisation_unit_id is set and dhis2_te_inst_id is empty
+    # Fetch cases where dhis2_organisation_unit_id is set and external_id is empty
 
     # query = CaseES().domain(domain).filter({
     #     # dhis2_organisation_unit_id is not empty
@@ -237,10 +250,10 @@ def gen_children_only_ours(domain):
     #         ]
     #     }
     # }).filter({
-    #     # dhis2_te_inst_id is empty
+    #     # external_id is empty
     #     'or': [
-    #         {'dhis2_te_inst_id': None},
-    #         {'dhis2_te_inst_id': ''}
+    #         {'external_id': None},
+    #         {'external_id': ''}
     #     ]
     # })
 
@@ -248,11 +261,11 @@ def gen_children_only_ours(domain):
     #     # dhis2_organisation_unit_id is not empty
     #     'not': {'dhis2_organisation_unit_id': ''}
     # }).filter({
-    #     # dhis2_te_inst_id is empty
-    #     'dhis2_te_inst_id': ''
+    #     # external_id is empty
+    #     'external_id': ''
     # })
 
-    query = CaseES().domain(DOMAIN).filter({'missing': {'field': 'dhis2_organization_unit_id'}})
+    query = CaseES().domain(domain).filter({'missing': {'field': 'dhis2_organization_unit_id'}})
     result = query.run()
     if result.total:
         for doc in result.hits:
@@ -282,43 +295,27 @@ def send_nutrition_data():
     .. _DHIS2 Integration: https://www.dropbox.com/s/8djk1vh797t6cmt/WV Sri Lanka Detailed Requirements.docx
 
     """
-    #
-    # CommCareHQ will use the DHIS API to send received nutrition data to DHIS2
-    # as an event that is associated with the correct entity, program, DHIS2
-    # user and organization.
-    #
-    # 1. On a periodic basis, CommCareHQ will submit an appropriate event using
-    #    the DHIS2 events API (Multiple Event with Registration) for any
-    #    unprocessed Growth Monitoring forms.
-    #
     dhis2_api = Dhis2Api(settings.DHIS2_HOST, settings.DHIS2_USERNAME, settings.DHIS2_PASSWORD)
     nutrition_id = dhis2_api.get_resource_id('program', 'Pediatric Nutrition Assessment')
     forms = []
     events = {'eventList': []}
     for form in get_unprocessed_growth_monitoring_forms():
         forms.append(form)
-        event = dhis2_api.form_to_event(nutrition_id, form, DATA_ELEMENT_NAMES)
+        event = dhis2_api.form_to_event(nutrition_id, form.form, DATA_ELEMENT_NAMES)
         events['eventList'].append(event)
     dhis2_api.send_events(events)
     mark_as_processed(forms)
 
 
 def get_unprocessed_growth_monitoring_forms():
-    #
-    # 2. The event will only be submitted if the corresponding case has a DHIS2
-    #    mapping (case properties that indicate the DHIS2 tracked entity
-    #    instance and programs for the case). If the case is not yet mapped to
-    #    DHIS2, the Growth Monitoring form will not be processed and could be
-    #    processed in the future (if the case is later associated with a DHIS
-    #    entity).
-    #
-    query = FormES().filter(
-        # TODO: case must have dhis2_te_inst_id populated; this indicates that it's
-        # been enrolled in both programs by push_child_entities()
-
-        # TODO: and it must not have been processed before
-
-    )
+    query = FormES().filter({
+        # dhis2_te_inst_id indicates that the case has been enrolled in both
+        # programs by push_child_entities()
+        'not': {'dhis2_te_inst_id': None}
+    }).filter({
+        # and it must not have been processed before
+        'dhis2_processed': None
+    })
     result = query.run()
     if result.total:
         for doc in result.hits:
@@ -327,5 +324,5 @@ def get_unprocessed_growth_monitoring_forms():
 
 def mark_as_processed(forms):
     for form in forms:
-        form.dhis2_processed = True
+        form.form['dhis2_processed'] = True
         form.save()
