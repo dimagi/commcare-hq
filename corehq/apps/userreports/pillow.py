413a697ed95650cec16694aafc100fc5f88bff6c
Nick Pellegrino <npellegrino@dimagi.com>
2014-12-11 13:57:43 -0500

    use deleted_filter in pillow

--- corehq/apps/userreports/pillow.py
+++ corehq/apps/userreports/pillow.py
@@ -44,12 +44,7 @@ class ConfigurableIndicatorPillow(PythonPillow):
         for table in self.tables:
             if table.config.filter.filter(doc):
                 table.save(doc)
-            if doc['doc_type'] in [
-                'CommCareCase-Deleted',
-                'XFormInstance-Deleted',
-                'XFormArchived',
-                'XFormDeprecated',
-            ]:
+            elif table.config.deleted_filter.filter(doc):
                 table.delete(doc)
 
     def set_checkpoint(self, change):
