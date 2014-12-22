3d912b5b42b53945010f587de5c2fd502bf468df
Norman Hooper <nhooper@dimagi.com>
2014-12-22 18:28:50 +0200

    DHIS2: More tests

--- custom/dhis2/tests.py
+++ custom/dhis2/tests.py
@@ -15,59 +15,6 @@ from django.test import TestCase
 from mock import patch, Mock
 
 
-class Dhis2OrgUnitTest(TestCase):
-
-    @skip('Finish writing this test')
-    def test_delete_dhis2_org_unit(self):
-        """
-        Dhis2OrgUnit.delete should succeed if it is not in use
-        """
-
-    @skip('Finish writing this test')
-    def test_delete_dhis2_org_unit_fail(self):
-        """
-        Dhis2OrgUnit.delete should fail if it is in use
-        """
-
-
-class TaskTest(TestCase):
-
-    @skip('Finish writing this test')
-    def test_push_child_entities(self):
-        """
-        push_child_entities should call the DHIS2 API for applicable child entities
-        """
-        pass
-
-    @skip('Finish writing this test')
-    def test_pull_child_entities(self):
-        """
-        pull_child_entities should fetch applicable child entities from the DHIS2 API
-        """
-        pass
-
-    @skip('Finish writing this test')
-    def test_sync_child_entities(self):
-        """
-        sync_child_entities should pull and push child entities appropriately
-        """
-        pass
-
-    @skip('Finish writing this test')
-    def test_sync_org_units(self):
-        """
-        sync_org_units should import new DHIS organization units and remove deleted ones safely.
-        """
-        pass
-
-    @skip('Finish writing this test')
-    def test_send_nutrition_data(self):
-        """
-        send_nutrition_data should update DHIS2 with received nutrition data
-        """
-        pass
-
-
 @contextmanager
 def fixture_type_context():
     fixture_type = FixtureDataType(
@@ -96,14 +43,74 @@ def org_unit_context():
             pass
 
 
-class MockOutThisTest(TestCase):
+class JsonApiRequestTest(TestCase):
+    pass
 
-    # host = 'http://dhis1.internal.commcarehq.org:8080/dhis'
-    host = 'http://localhost:8082'
-    username = 'admin'
-    password = 'district'
 
-    domain = DOMAIN
+class Dhis2ApiTest(TestCase):
+    pass
+
+
+class FixtureManagerTest(TestCase):
+    pass
+
+
+class Dhis2OrgUnitTest(TestCase):
+
+    def test_save(self):
+        """
+        Dhis2OrgUnit.save should save a FixtureDataItem
+        """
+        with fixture_type_context(), \
+                patch('corehq.apps.fixtures.models.FixtureDataItem.__init__') as mock_init:
+            data_item_mock = Mock()
+            data_item_mock.get_id.return_value = '123'
+            mock_init.return_value = data_item_mock
+
+            org_unit = Dhis2OrgUnit(id='QXOOG2Foong', name='Somerset West')
+            id_ = org_unit.save()
+
+            mock_init.assert_called()
+            data_item_mock.save.assert_called()
+            self.assertEqual(id_, '123')
+            self.assertEqual(org_unit._fixture_id, '123')
+
+    def test_delete_dhis2_org_unit_does_nothing(self):
+        """
+        Dhis2OrgUnit.delete should do nothing if it's not saved
+        """
+        with fixture_type_context(), \
+                patch('corehq.apps.fixtures.models.FixtureDataItem.get') as mock_get:
+            data_item_mock = Mock()
+            mock_get.return_value = data_item_mock
+
+            org_unit = Dhis2OrgUnit(id='QXOOG2Foong', name='Somerset West')
+            org_unit.delete()
+
+            self.assertFalse(mock_get.called)
+            self.assertFalse(data_item_mock.delete.called)
+
+    def test_delete_dhis2_org_unit_deletes(self):
+        """
+        Dhis2OrgUnit.delete should delete if it's saved
+        """
+        with fixture_type_context(), \
+                patch('corehq.apps.fixtures.models.FixtureDataItem.__init__') as mock_init, \
+                patch('corehq.apps.fixtures.models.FixtureDataItem.get') as mock_get:
+            data_item_mock = Mock()
+            data_item_mock.get_id.return_value = '123'
+            mock_init.return_value = data_item_mock
+            mock_get.return_value = data_item_mock
+
+            org_unit = Dhis2OrgUnit(id='QXOOG2Foong', name='Somerset West')
+            org_unit.save()
+            org_unit.delete()
+
+            mock_get.assert_called()
+            data_item_mock.delete.assert_called()
+
+
+class TaskTest(TestCase):
 
     def test_sync_org_units_dict_comps(self):
         """
@@ -152,6 +159,54 @@ class MockOutThisTest(TestCase):
 
             delete_mock.assert_called()
 
+    @skip('Finish writing this test')
+    def test_push_child_entities(self):
+        """
+        push_child_entities should call the DHIS2 API for applicable child entities
+        """
+        pass
+
+    @skip('Finish writing this test')
+    def test_pull_child_entities(self):
+        """
+        pull_child_entities should fetch applicable child entities from the DHIS2 API
+        """
+        pass
+
+    def test_sync_child_entities(self):
+        with patch('custom.dhis2.tasks.get_children_only_theirs') as only_theirs_mock, \
+                patch('custom.dhis2.tasks.pull_child_entities') as pull_mock, \
+                patch('custom.dhis2.tasks.gen_children_only_ours') as only_ours_mock, \
+                patch('custom.dhis2.tasks.push_child_entities') as push_mock:
+            foo = object()
+            bar = object()
+            only_theirs_mock.return_value = foo
+            only_ours_mock.return_value = bar
+
+            sync_child_entities()
+
+            only_theirs_mock.assert_called()
+            pull_mock.assert_called_with(DOMAIN, foo)
+            only_ours_mock.assert_called_with(DOMAIN)
+            push_mock.assert_called_with(bar)
+
+    @skip('Finish writing this test')
+    def test_send_nutrition_data(self):
+        """
+        send_nutrition_data should update DHIS2 with received nutrition data
+        """
+        pass
+
+
+class MockOutThisTest(TestCase):
+
+    # host = 'http://dhis1.internal.commcarehq.org:8080/dhis'
+    host = 'http://localhost:8082'
+    username = 'admin'
+    password = 'district'
+
+    domain = DOMAIN
+
     def step_into_sync_org_units(self):
         import ipdb; ipdb.set_trace()
         with fixture_type_context(), org_unit_context():
@@ -185,26 +240,3 @@ class MockOutThisTest(TestCase):
     def step_into_sync_child_entities(self):
         import ipdb; ipdb.set_trace()
         sync_child_entities()
-
-    def test_sync_child_entities(self):
-        with patch('custom.dhis2.tasks.get_children_only_theirs') as only_theirs_mock, \
-                patch('custom.dhis2.tasks.pull_child_entities') as pull_mock, \
-                patch('custom.dhis2.tasks.gen_children_only_ours') as only_ours_mock, \
-                patch('custom.dhis2.tasks.push_child_entities') as push_mock:
-            foo = object()
-            bar = object()
-            only_theirs_mock.return_value = foo
-            only_ours_mock.return_value = bar
-
-            sync_child_entities()
-
-            only_theirs_mock.assert_called()
-            pull_mock.assert_called_with(DOMAIN, foo)
-            only_ours_mock.assert_called_with(DOMAIN)
-            push_mock.assert_called_with(bar)
-
-    def test_pull_child_entities(self):
-        pass
-
-    def test_push_child_entities(self):
-        pass
