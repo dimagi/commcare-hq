c388625b3540684cfa3d25ed41e6a11d0a9c47ac
Kamil Krampa <kkrampa@soldevelo.com>
2015-01-15 14:37:10 +0100

    Refactored and modified migration for ILS

--- custom/ilsgateway/api.py
+++ custom/ilsgateway/api.py
@@ -1,9 +1,19 @@
+import logging
 from jsonobject import JsonObject
-from jsonobject.properties import StringProperty, BooleanProperty, DecimalProperty, ListProperty, IntegerProperty, \
+from jsonobject.properties import StringProperty, BooleanProperty, DecimalProperty, ListProperty, IntegerProperty,\
     FloatProperty, DictProperty
-from corehq.apps.commtrack.models import SupplyPointCase
-from custom.ilsgateway.models import SupplyPointStatus, DeliveryGroupReport
-from custom.logistics.api import LogisticsEndpoint
+from requests.exceptions import ConnectionError
+from corehq import Domain
+from corehq.apps.commtrack.models import SupplyPointCase, CommtrackConfig, CommtrackActionConfig
+from corehq.apps.locations.models import SQLLocation
+from corehq.apps.locations.schema import LocationType
+from corehq.apps.users.models import UserRole
+from custom.api.utils import apply_updates
+from custom.ilsgateway.models import SupplyPointStatus, DeliveryGroupReport, HistoricalLocationGroup
+from custom.logistics.api import LogisticsEndpoint, APISynchronization
+from corehq.apps.locations.models import Location as Loc
+
+LOCATION_TYPES = ["MOHSW", "REGION", "DISTRICT", "FACILITY"]
 
 
 class Product(JsonObject):
@@ -28,23 +38,6 @@ class ILSUser(JsonObject):
     location = DecimalProperty()
     supply_point = IntegerProperty()
 
-    @classmethod
-    def from_json(cls, json_rep):
-        return cls(
-            username=json_rep['username'],
-            first_name=json_rep['first_name'],
-            last_name=json_rep['last_name'],
-            email=json_rep['email'],
-            password=json_rep['password'],
-            is_staff=json_rep['is_staff'],
-            is_active=json_rep['is_active'],
-            is_superuser=json_rep['is_superuser'],
-            last_login=json_rep['last_login'],
-            date_joined=json_rep['date_joined'],
-            location=json_rep['location'],
-            supply_point=json_rep['supply_point']
-        )
-
 
 class SMSUser(JsonObject):
     id = IntegerProperty()
@@ -56,19 +49,6 @@ class SMSUser(JsonObject):
     phone_numbers = ListProperty()
     backend = StringProperty()
     date_updated = StringProperty()
-
-    @classmethod
-    def from_json(cls, json_rep):
-        return cls(
-            id=json_rep['id'],
-            name=json_rep['name'],
-            role=json_rep['role'],
-            is_active=json_rep['is_active'],
-            supply_point=json_rep['supply_point'],
-            email=json_rep['email'],
-            phone_numbers=json_rep['phone_numbers'],
-            backend=json_rep['backend']
-        )
 
 
 class Location(JsonObject):
@@ -136,3 +116,138 @@ class ILSGatewayEndpoint(LogisticsEndpoint):
         location_id = _get_location_id(facility, domain)
         return meta, [DeliveryGroupReport.wrap_from_json(deliverygroupreport, location_id)
                       for deliverygroupreport in deliverygroupreports]
+
+
+class ILSGatewayAPI(APISynchronization):
+
+    def prepare_commtrack_config(self):
+        domain = Domain.get_by_name(self.domain)
+        domain.location_types = []
+        for i, value in enumerate(LOCATION_TYPES):
+            if not any(lt.name == value
+                       for lt in domain.location_types):
+                allowed_parents = [LOCATION_TYPES[i - 1]] if i > 0 else [""]
+                domain.location_types.append(
+                    LocationType(name=value, allowed_parents=allowed_parents,
+                                 administrative=(value.lower() != 'facility')))
+        domain.save()
+        config = CommtrackConfig.for_domain(self.domain)
+        config.location_types = []
+        actions = [action.keyword for action in config.actions]
+        if 'delivered' not in actions:
+            config.actions.append(
+                CommtrackActionConfig(
+                    action='receipts',
+                    keyword='delivered',
+                    caption='Delivered')
+            )
+        config.save()
+
+    def web_users_sync(self, ilsgateway_webuser):
+        web_user = super(ILSGatewayAPI, self).web_users_sync(ilsgateway_webuser)
+        if not web_user:
+            return None
+        dm = web_user.get_domain_membership(self.domain)
+        dm.role_id = UserRole.get_read_only_role_by_domain(self.domain).get_id
+        web_user.save()
+        return web_user
+
+    def sms_users_sync(self, ilsgateway_smsuser, **kwargs):
+        from custom.logistics.commtrack import add_location
+        sms_user = super(ILSGatewayAPI, self).sms_users_sync(ilsgateway_smsuser, **kwargs)
+        if not sms_user:
+            return None
+        sp = SupplyPointCase.view('hqcase/by_domain_external_id',
+                                  key=[self.domain, str(ilsgateway_smsuser.supply_point)],
+                                  reduce=False,
+                                  include_docs=True,
+                                  limit=1).first()
+        location_id = sp.location_id if sp else None
+        dm = sms_user.get_domain_membership(self.domain)
+        dm.location_id = location_id
+        sms_user.save()
+        add_location(sms_user, location_id)
+        return sms_user
+
+    def locations_sync(self, ilsgateway_location, fetch_groups=False):
+        try:
+            sql_loc = SQLLocation.objects.get(
+                domain=self.domain,
+                external_id=int(ilsgateway_location.id)
+            )
+            location = Loc.get(sql_loc.location_id)
+        except SQLLocation.DoesNotExist:
+            location = None
+        except SQLLocation.MultipleObjectsReturned:
+            return
+
+        if not location:
+            if ilsgateway_location.parent_id:
+                loc_parent = SupplyPointCase.view('hqcase/by_domain_external_id',
+                                                  key=[self.domain, str(ilsgateway_location.parent_id)],
+                                                  reduce=False,
+                                                  include_docs=True).first()
+                if not loc_parent:
+                    parent = self.endpoint.get_location(ilsgateway_location.parent_id)
+                    loc_parent = self.locations_sync(Location(parent))
+                else:
+                    loc_parent = loc_parent.location
+                location = Loc(parent=loc_parent)
+            else:
+                location = Loc()
+                location.lineage = []
+            location.domain = self.domain
+            location.name = ilsgateway_location.name
+            if ilsgateway_location.groups:
+                location.metadata = {'groups': ilsgateway_location.groups}
+            if ilsgateway_location.latitude:
+                location.latitude = float(ilsgateway_location.latitude)
+            if ilsgateway_location.longitude:
+                location.longitude = float(ilsgateway_location.longitude)
+            location.location_type = ilsgateway_location.type
+            location.site_code = ilsgateway_location.code
+            location.external_id = str(ilsgateway_location.id)
+            location.save()
+            if not SupplyPointCase.get_by_location(location):
+                SupplyPointCase.create_from_location(self.domain, location)
+        else:
+            location_dict = {
+                'name': ilsgateway_location.name,
+                'latitude': float(ilsgateway_location.latitude) if ilsgateway_location.latitude else None,
+                'longitude': float(ilsgateway_location.longitude) if ilsgateway_location.longitude else None,
+                'location_type': ilsgateway_location.type,
+                'site_code': ilsgateway_location.code.lower(),
+                'external_id': str(ilsgateway_location.id),
+                'metadata': {}
+            }
+            if ilsgateway_location.groups:
+                location_dict['metadata']['groups'] = ilsgateway_location.groups
+            case = SupplyPointCase.get_by_location(location)
+            if apply_updates(location, location_dict):
+                location.save()
+                if case:
+                    case.update_from_location(location)
+                else:
+                    SupplyPointCase.create_from_location(self.domain, location)
+        if ilsgateway_location.historical_groups:
+            historical_groups = ilsgateway_location.historical_groups
+        else:
+            counter = 0
+            historical_groups = {}
+            while counter != 5:
+                try:
+                    location_object = self.endpoint.get_location(
+                        ilsgateway_location.id,
+                        params=dict(with_historical_groups=1)
+                    )
+                    historical_groups = Location(**location_object).historical_groups
+                    break
+                except ConnectionError as e:
+                    logging.error(e)
+                    counter += 1
+
+        for date, groups in historical_groups.iteritems():
+            for group in groups:
+                HistoricalLocationGroup.objects.get_or_create(date=date, group=group,
+                                                              location_id=location.sql_location)
+        return location
