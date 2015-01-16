8bba3e6960685d5f8deef4e4371114ed4c7f12f4
Simon Kelly <skelly@dimagi.com>
2015-01-16 15:51:40 +0200

    Use hashlib instead of deprecated django.utils.hashcompat

--- dimagi/utils/django/cache.py
+++ dimagi/utils/django/cache.py
@@ -1,9 +1,9 @@
-from django.utils.hashcompat import md5_constructor
+import hashlib
 from django.utils.http import urlquote
 
 
 def make_template_fragment_key(fragment_name, vary_on):
     # Build a unicode key for this fragment and all vary-on's.
-    args = md5_constructor(u':'.join([urlquote(var) for var in vary_on]))
+    args = hashlib.md5(u':'.join([urlquote(var) for var in vary_on]))
     cache_key = 'template.cache.%s.%s' % (fragment_name, args.hexdigest())
     return cache_key
