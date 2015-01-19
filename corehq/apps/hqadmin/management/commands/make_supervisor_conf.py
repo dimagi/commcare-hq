aea350451abaaac4ac59c0b2f043445d49aa7fbc
Simon Kelly <skelly@dimagi.com>
2015-01-19 11:25:55 +0200

    add new-relic to hqdjango3

--- corehq/apps/hqadmin/management/commands/make_supervisor_conf.py
+++ corehq/apps/hqadmin/management/commands/make_supervisor_conf.py
@@ -1,4 +1,3 @@
-import json
 import os
 import sys
 from optparse import make_option
--- fab/fabfile.py
+++ fab/fabfile.py
@@ -135,9 +135,20 @@ def format_env(current_env):
         'django_port',
         'django_bind',
         'flower_port',
-        'django_command_prefix',
-        'supervisor_env_vars'
     ]
+
+    host = current_env.get('host_string')
+    command_prefix = current_env.get('django_command_prefix', {})
+    if isinstance(command_prefix, dict):
+        ret['django_command_prefix'] = command_prefix.get(host, '')
+    else:
+        ret['django_command_prefix'] = command_prefix
+
+    env_vars = current_env.get('supervisor_env_vars', {})
+    if isinstance(env_vars, dict):
+        ret['supervisor_env_vars'] = env_vars.get(host, '')
+    else:
+        ret['supervisor_env_vars'] = env_vars
 
     for prop in important_props:
         ret[prop] = current_env.get(prop, '')
@@ -329,6 +340,13 @@ def production():
     env.flower_port = 5555
 
     _setup_path()
+
+    env.django_command_prefix = {
+        'hqdjango3.internal.commcarehq.org': '%(virtualenv_root)s/bin/newrelic-admin run-program ' % env
+    }
+    env.supervisor_env_vars = {
+        'hqdjango3.internal.commcarehq.org': 'NEW_RELIC_CONFIG_FILE=../newrelic.ini,NEW_RELIC_ENVIRONMENT=production'
+    }
 
 
 @task
