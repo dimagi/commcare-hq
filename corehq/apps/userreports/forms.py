95ce419437865944dd24adb893ac872a64f0b998
Nick Pellegrino <npellegrino@dimagi.com>
2015-01-21 13:59:27 -0500

    add create new report form class

---
+++ corehq/apps/userreports/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from django.utils.translation import ugettext_noop as _
+
+from crispy_forms import layout as crispy
+from crispy_forms.bootstrap import FormActions
+from crispy_forms.helper import FormHelper
+
+from corehq.apps.app_manager.models import get_apps_in_domain
+
+
+class CreateNewReportBuilderForm(forms.Form):
+    report_type = forms.ChoiceField(
+        choices=[
+            ('bar_chart', _("Bar Chart")),
+            ('pie_chart', _("Pie Chart")),
+            ('table', _("Table")),
+        ],
+    )
+
+    report_source = forms.ChoiceField()
+
+    def __init__(self, domain, *args, **kwargs):
+        super(CreateNewReportBuilderForm, self).__init__(*args, **kwargs)
+
+        apps = get_apps_in_domain(domain, full=True, include_remote=False)
+        self.fields['report_source'].choices = [
+            (c, c) for c in
+            set([case_type for app in apps for case_type in app.get_case_types() if case_type])
+        ]
+
+        self.helper = FormHelper()
+        self.helper.form_class = "form-horizontal"
+        self.helper.layout = crispy.Layout(
+            crispy.Fieldset(
+                _('Create New Report'),
+                'report_type',
+                'report_source',
+            ),
+            FormActions(
+                crispy.ButtonHolder(
+                    crispy.Submit(
+                        'create_new_report_builder_btn',
+                        _('Next'),
+                    )
+                ),
+            ),
+        )
