7c542646eace4a6c698e2a1fcd427f0dbebb8753
Moses Katembu <mose.kate@gmail.com>
2015-02-05 09:48:58 +0300

    make va report to use new db and map it

--- custom/_legacy/mvp/reports/va.py
+++ custom/_legacy/mvp/reports/va.py
@@ -59,7 +59,7 @@ class VerbalAutopsyReport(MVPIndicatorReport):
                     indicator = DynamicIndicatorDefinition.get_current(
                         MVP.NAMESPACE, self.domain, slug,
                         wrap_correctly=True,
-                        requesting_user=self.request.couch_user.username,
+                        use_new_db=True,
                     )
                     if self.is_rendered_as_email:
                         retrospective = indicator.get_monthly_retrospective(user_ids=self.user_ids)
