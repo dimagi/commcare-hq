4ebadec09ccc7ae32a9ddad25b87d289c1267c14
Simon Kelly <skelly@dimagi.com>
2015-02-12 17:59:03 +0200

    custom query for asha facilitators

--- custom/up_nrhm/sql_data.py
+++ custom/up_nrhm/sql_data.py
@@ -1,6 +1,11 @@
 from sqlagg.columns import CountUniqueColumn
 from sqlagg.filters import BETWEEN, EQ, GTE, LTE
+from sqlagg.queries.alchemy_extensions import InsertFromSelect
+import sqlalchemy
 from corehq.apps.reports.sqlreport import SqlData, DatabaseColumn
+from corehq.apps.userreports.models import CustomDataSourceConfiguration
+from corehq.apps.userreports.sql import get_table_name, get_indicator_table
+from corehq.db import Session
 
 
 class ASHAFacilitatorsData(SqlData):
@@ -126,3 +131,107 @@ class ASHAFacilitatorsData(SqlData):
     @property
     def filters(self):
         return [BETWEEN("date", "startdate", "enddate"), EQ('owner_id', 'af')]
+
+
+class ASHAFacilitatorsDataNew(SqlData):
+    """
+    CREATE TEMPORARY TABLE asha AS
+    select case_id, hv_fx_vhnd = 1 as hv_fx_vhnd from "config_report_up-nrhm_asha_facilitators_faeb40fd" rd,
+    (
+        select max(date) date, case_id as cid
+        from "config_report_up-nrhm_asha_facilitators_faeb40fd"
+        where date between '2015-01-01' and '2015-02-01'
+        group by case_id
+        ) as max
+
+    where case_id = max.cid and rd.date = max.date and case_id = 'b77bc08a-6453-43b1-8d17-6d9d9e45feac'
+
+
+    select case_id, count(case_id)
+        from "config_report_up-nrhm_asha_facilitators_faeb40fd"
+        where date between '2015-01-01' and '2015-02-01'
+        group by case_id
+
+    select *
+    from "config_report_up-nrhm_asha_facilitators_faeb40fd"
+    where case_id = 'b77bc08a-6453-43b1-8d17-6d9d9e45feac'
+
+    "2015-01-03", hv_fx_vhnd = 0
+    "2015-01-21", hv_fx_vhnd = 88
+
+
+    select 1 FROM pg_catalog.pg_class c LEFT JOIN pg_catalog.pg_namespace n ON n.oid
+    = c.relnamespace
+    where n.nspname like 'pg_temp_%' AND pg_catalog.pg_table_is_visible(c.oid)
+    AND Upper(relname) = Upper('asaha');
+
+    """
+    slug = 'asha_facilitators'
+    title = 'ASHA Facilitators report'
+    show_total = False
+
+    @property
+    def table_name(self):
+        return get_table_name(self.config['domain'], 'asha_facilitators')
+
+    def _get_temp_table_name(self):
+        start_date = self.config["startdate"].strftime("%Y-%m-%d")
+        end_date = self.config["enddate"].strftime("%Y-%m-%d")
+        return "tmp_{name}_{start}_{end}".format(
+            name=self.table_name,
+            start=start_date,
+            end=end_date
+        )
+
+    @property
+    def columns(self):
+        return [
+            sqlalchemy.Column('hv_fx_vhnd', sqlalchemy.BOOLEAN)
+        ]
+
+    def get_sql_table(self, metadata):
+        config = CustomDataSourceConfiguration.by_id(CustomDataSourceConfiguration.get_doc_id(self.slug))
+        return get_indicator_table(config, custom_metadata=metadata)
+
+    def data(self):
+        session = Session()
+        try:
+            data = self._get_sql_data(session)
+        except:
+            session.rollback()
+            raise
+
+    def _get_sql_data(self, session):
+        metadata = sqlalchemy.MetaData()
+        metadata.bind = session.connection()
+
+        # todo: check if temp table exists
+        temp_table = sqlalchemy.Table(self._get_temp_table_name(), metadata,
+                             sqlalchemy.Column("case_id", sqlalchemy.VARCHAR, primary_key=True),
+                             prefixes=['TEMPORARY'])
+
+        for column in self.columns:
+            temp_table.append_column(column)
+
+        asha_table = self.get_sql_table(metadata)
+        max_date_query = sqlalchemy.select(
+            sqlalchemy.func.max(asha_table.c.date).label('date'),
+            asha_table.c.case_id.label('case_id')
+        ).where(
+            asha_table.date.between(':startdate', ':enddate')
+        ).group_by(
+            asha_table.c.case_id
+        )
+
+        temp_table_query = sqlalchemy.select(
+            asha_table.c.case_id,
+            (asha_table.c.hv_fx_vhnd == 1).label('hv_fx_vhnd')
+        ).join(
+            max_date_query,
+            asha_table.c.case_id == max_date_query.c.case_id,
+            asha_table.c.date == max_date_query.c.date,
+        )
+
+        # create temp table
+        from_select = InsertFromSelect(temp_table, temp_table_query, self.columns)
+        session.execute(from_select)
