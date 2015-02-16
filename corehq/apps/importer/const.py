26fff5bd2eb530f7f45d3165e06ff7b2efa01042
Ben <rudolphben@gmail.com>
2015-02-16 14:38:05 -0500

    (FB: 157140) - Show missing parent errors
    Small refactor of error handling in bulk importer

--- corehq/apps/importer/const.py
+++ corehq/apps/importer/const.py
@@ -1,2 +1,10 @@
 class LookupErrors:
     NotFound, MultipleResults = range(2)
+
+class ImportErrors:
+    InvalidOwnerName = 'Invalid Owner Name'
+    InvalidOwnerId = 'Invalid Owner ID'
+    InvalidParentId = 'Invalid Parent ID'
+    InvalidDate = 'Invalid Date'
+    BlankExternalId = 'Blank External ID'
+    CaseGeneration = 'Case Generation Error'
