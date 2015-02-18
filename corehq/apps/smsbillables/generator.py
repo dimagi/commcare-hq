627b9b409fd5a7f37a89e3b8fedaf5a900538a0a
Nick Pellegrino <npellegrino@dimagi.com>
2015-02-18 15:08:54 -0500

    add tests

--- corehq/apps/smsbillables/generator.py
+++ corehq/apps/smsbillables/generator.py
@@ -40,6 +40,41 @@ def arbitrary_fee():
     return Decimal(str(round(random.uniform(0.0, 1.0), 4)))
 
 
+def arbitrary_country_code_and_prefixes(country_codes=TEST_COUNTRY_CODES):
+    country_codes_and_prefixes = []
+    for country_code in country_codes:
+        prefixes = [""]
+        for _ in range(8):
+            for i in range(4):
+                while True:
+                    prefix = prefixes[-1 - i] + str(random.randint(0 if country_code != 1 else 2, 9))
+                    if prefix not in prefixes:
+                        prefixes.append(prefix)
+                        break
+        for prefix in prefixes:
+            country_codes_and_prefixes.append((str(country_code), prefix))
+    return country_codes_and_prefixes
+
+
+def arbitrary_fees_by_prefix(backend_ids, country_codes_and_prefixes):
+    fees = {}
+    for direction in DIRECTIONS:
+        fees_by_backend = {}
+        for backend in get_available_backends().values():
+            fees_by_country_code = {}
+            for country_code, _ in country_codes_and_prefixes:
+                fees_by_country_code[country_code] = {}
+            for country_code, prefix in country_codes_and_prefixes:
+                fees_by_prefix = {
+                    backend_instance: arbitrary_fee()
+                    for backend_instance in [backend_ids[backend.get_api_id()], None]
+                }
+                fees_by_country_code[country_code][prefix] = fees_by_prefix
+            fees_by_backend[backend.get_api_id()] = fees_by_country_code
+        fees[direction] = fees_by_backend
+    return fees
+
+
 def arbitrary_phone_number(country_codes=TEST_COUNTRY_CODES):
     return str(random.choice(country_codes)) + str(random.randint(10**9, 10**10 - 1))
 
@@ -146,3 +181,32 @@ def arbitrary_currency():
     return Currency.objects.get_or_create(
         rate_to_default=Decimal('%5.f' % random.uniform(0.5, 2.0)),
     )[0]
+
+
+def arbitrary_phone_numbers_and_prefixes(country_code_and_prefixes):
+    country_code_to_prefixes = {}
+    for country_code, prefix in country_code_and_prefixes:
+        if country_code not in country_code_to_prefixes:
+            country_code_to_prefixes[country_code] = []
+        country_code_to_prefixes[country_code].append(prefix)
+    for country_code, prefix in country_code_and_prefixes:
+        remainder_len = 10 - len(prefix)
+
+        def _get_national_number():
+            return prefix + str(random.randint(
+                (1 if prefix else 2) * (10 ** (remainder_len - 1)),
+                (10 ** remainder_len) - 1
+            ))
+
+        while True:
+            national_number = _get_national_number()
+            if not any(
+                national_number.startswith(another_prefix) and another_prefix.startswith(prefix)
+                for another_prefix in country_code_to_prefixes[country_code]
+                if another_prefix and another_prefix != prefix
+            ):
+                yield (
+                    country_code + national_number,
+                    prefix
+                )
+                break
