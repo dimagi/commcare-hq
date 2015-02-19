5acf30c5aec09bf1921e1f56787cb0a47c45f1a8
Norman Hooper <nhooper@dimagi.com>
2015-02-19 16:38:35 +0200

    DHIS2: Added logging to payload generator, fixed AttributeError

--- custom/dhis2/payload_generators.py
+++ custom/dhis2/payload_generators.py
@@ -3,22 +3,25 @@ We use form forwarding to create DHIS2 program events from forms.
 
 Set up form forwarding as follows:
 
-  1. Navigate to Project Settings > Project Administration - Data Forwarding >
-     Forward Forms
+  1. Navigate to "Project Settings" > "Project Administration":
+     "Data Forwarding" > "Forward Forms"
 
   2. Click "Add a forwarding location".
 
   3. Set "URL to forward to" to the DHIS2 API events url. e.g.
      `http://dhis1.internal.commcarehq.org:8080/dhis/api/events.json`
 
-  4. Enable basic authentication and set the username and password.
+  4. Enable basic authentication and set "Username" and "Password".
 
-  5. Choose form format
+  5. Set "Payload Format" to "DHIS2 Event JSON"
+
+  6. Click "Start Forwarding"
 
 
 """
 from datetime import date
 import json
+import logging
 from corehq.apps.receiverwrapper.models import RegisterGenerator, FormRepeater
 from corehq.apps.receiverwrapper.repeater_generators import BasePayloadGenerator
 from custom.dhis2.models import Dhis2Api, json_serializer, Dhis2Settings, Dhis2IntegrationError
@@ -26,6 +29,9 @@ from custom.dhis2.const import NUTRITION_ASSESSMENT_EVENT_FIELDS, RISK_ASSESSMEN
     RISK_ASSESSMENT_PROGRAM_FIELDS, REGISTER_CHILD_XMLNS, GROWTH_MONITORING_XMLNS, RISK_ASSESSMENT_XMLNS, \
     NUTRITION_ASSESSMENT_PROGRAM_FIELDS
 from custom.dhis2.tasks import push_case
+
+
+logger = logging.getLogger(__name__)
 
 
 @RegisterGenerator(FormRepeater, 'dhis2_event_json', 'DHIS2 Event JSON')
@@ -39,6 +45,7 @@ class FormRepeaterDhis2EventPayloadGenerator(BasePayloadGenerator):
         return {'Content-type': 'application/json'}
 
     def get_payload(self, repeat_record, form):
+        logger.debug('DHIS2: Form domain "%s" XMLNS "%s"', form['domain'], form['xmlns'])
         if not Dhis2Settings.is_enabled_for_domain(form['domain']):
             return
 
@@ -58,11 +65,13 @@ class FormRepeaterDhis2EventPayloadGenerator(BasePayloadGenerator):
         if form['xmlns'] == REGISTER_CHILD_XMLNS:
             # Create a DHIS2 tracked entity instance from the form's case and
             # enroll in the nutrition assessment programme.
+            logger.debug('DHIS2: Processing Register Child form')
             push_case(case, dhis2_api)
             # We just need to enroll. No event to create
 
         elif form['xmlns'] == GROWTH_MONITORING_XMLNS:
-            if not case.get('external_id'):
+            logger.debug('DHIS2: Processing Growth Monitoring form')
+            if not getattr(case, 'external_id', None):
                 raise Dhis2IntegrationError('Register Child form must be processed before Growth Monitoring form')
             # Update tracked entity instance
             instance = dhis2_api.get_te_inst(case['external_id'])
@@ -75,8 +84,8 @@ class FormRepeaterDhis2EventPayloadGenerator(BasePayloadGenerator):
                                             case['external_id'])
 
         elif form['xmlns'] == RISK_ASSESSMENT_XMLNS:
-            # Create a risk assessment event.
-            if not case.get('external_id'):
+            logger.debug('DHIS2: Processing Risk Assessment form')
+            if not getattr(case, 'external_id', None):
                 raise Dhis2IntegrationError('Register Child form must be processed before Risk Assessment form')
             # Update tracked entity instance
             instance = dhis2_api.get_te_inst(case['external_id'])
@@ -91,6 +100,7 @@ class FormRepeaterDhis2EventPayloadGenerator(BasePayloadGenerator):
                 program_data = {dhis2_attr: case[cchq_attr]
                                 for cchq_attr, dhis2_attr in RISK_ASSESSMENT_PROGRAM_FIELDS.iteritems()}
                 dhis2_api.enroll_in_id(case['external_id'], risk_id, today, program_data)
+            # Create a risk assessment event.
             event = dhis2_api.form_to_event(risk_id, form, RISK_ASSESSMENT_EVENT_FIELDS, case['external_id'])
 
         # If the form is not to be forwarded, the event will be None
