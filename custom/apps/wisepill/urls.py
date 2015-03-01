a95462b5e7d5e00dcdd2d8814fd479a03288329b
Giovanni Capalbo <gcapalbo@dimagi.com>
2015-03-01 12:37:59 -0500

    Add WisePill Device Event Export

--- custom/apps/wisepill/urls.py
+++ custom/apps/wisepill/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import *
 
 urlpatterns = patterns('custom.apps.wisepill.views',
     url(r'^device/?$', 'device_data', name='device_data'),
+    url(r'^export/events/', 'export_events', name='export_wisepill_events'),
 )
