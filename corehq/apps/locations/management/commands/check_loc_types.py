ce7dbc2af2be339a68b7a4355edac3dadacb048c
Ethan Soergel <esoergel@gmail.com>
2015-03-03 15:52:39 -0500

    Include some statistics about the domain

--- corehq/apps/locations/management/commands/check_loc_types.py
+++ corehq/apps/locations/management/commands/check_loc_types.py
@@ -1,23 +1,40 @@
 import csv
 import datetime
 
+from couchforms.models import XFormInstance
 from django.core.management.base import BaseCommand
 
 from corehq.apps.commtrack.models import StockState
 from corehq.apps.domain.models import Domain
-from corehq.apps.locations.models import SQLLocation
+from corehq.apps.locations.models import SQLLocation, Location
 
 
-EXCLUDE_DOMAINS = (
-    "drewpsi",
-    "psi",
-    "psi-ors",
-    "psi-test",
-    "psi-test2",
-    "psi-test3",
-    "psi-unicef",
-    "psi-unicef-wb",
-)
+def _stock_states_by_domain(domain):
+    return (StockState.objects
+            .filter(sql_location__domain=domain)
+            .count())
+
+
+def _forms_by_domain(domain):
+    res = XFormInstance.view(
+        'couchforms/all_submissions_by_domain',
+        startkey=[domain, "by_type", "XFormInstance"],
+        endkey=[domain, "by_type", "XFormInstance", {}],
+        reduce=True,
+        include_docs=False,
+    ).all()
+    return res[0].get('value', 0) if res else 0
+
+
+def _locs_by_domain(domain):
+    res = Location.view(
+        'locations/by_name',
+        startkey=[domain],
+        endkey=[domain, {}],
+        reduce=True,
+        include_docs=False,
+    ).all()
+    return res[0].get('value', 0) if res else 0
 
 
 class Command(BaseCommand):
@@ -88,15 +105,6 @@ class Command(BaseCommand):
                     .format(loc_type, count)
                 )
 
-    def get_stock_states(self, domain_info):
-        stock_states = (StockState.objects
-                        .filter(sql_location__domain=domain_info.name)
-                        .count())
-        if stock_states > 0:
-            domain_info.num_stock_states = stock_states
-            self.stdout.write("  This domain has {} stock states!"
-                              .format(stock_states))
-
     def _write_to_csv(self, headers, domains):
         filename = "check_loc_types-{}.csv".format(datetime.date.today())
         with open(filename, 'wb') as f:
@@ -112,6 +120,9 @@ class Command(BaseCommand):
         headers = [
             'name',
             'num_stock_states',
+            'is_test',
+            'num_forms',
+            'num_locations',
             'has_problems',
             'duplicate_loc_type_codes',
             'incorrect_number_of_parents',
@@ -132,12 +143,15 @@ class Command(BaseCommand):
             domain_info.name = domain_obj.name
             self.stdout.write(u"[{}] - {}\n".format(domain_obj.name,
                                                     domain_obj._id))
-            self.get_stock_states(domain_info)
             self.check_for_duplicate_codes(domain_info, loc_types)
             self.check_single_parentage(domain_info, loc_types)
             self.check_parentage(domain_info, loc_types)
             self.check_loc_types(domain_info, loc_types)
             if domain_info.has_problems:
+                domain_info.num_stock_states = _stock_states_by_domain(domain_obj.name)
+                domain_info.is_test = domain_obj.is_test == u'true'
+                domain_info.num_forms = _forms_by_domain(domain_obj.name)
+                domain_info.num_locations = _locs_by_domain(domain_obj.name)
                 domains.append(domain_info)
 
         self._write_to_csv(headers, domains)
