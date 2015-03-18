1b4d30265c231df2d53b66dc60011105ebbac506
Kamil Krampa <kkrampa@soldevelo.com>
2015-03-18 08:38:16 +0100

    Generate consumption and receipts during migration
    (cherry picked from commit cdf724dfe67b130ec23a3cf739c40c2251b819a2)

--- custom/logistics/tasks.py
+++ custom/logistics/tasks.py
@@ -170,7 +170,17 @@ def sync_stock_transactions_for_facility(domain, endpoint, facility, xform, chec
                     except SQLProduct.DoesNotExist:
                         # todo: kkrampa what's the deal with this logic? this should never be true
                         continue
-
+                    if stocktransaction.quantity != 0:
+                        transactions_to_add.append(StockTransaction(
+                            case_id=case._id,
+                            product_id=sql_product.product_id,
+                            sql_product=sql_product,
+                            section_id=section_id,
+                            type='receipts' if stocktransaction.quantity > 0 else 'consumption',
+                            stock_on_hand=Decimal(stocktransaction.ending_balance),
+                            quantity=Decimal(stocktransaction.quantity),
+                            report=report
+                        ))
                     transactions_to_add.append(StockTransaction(
                         case_id=case._id,
                         product_id=sql_product.product_id,
