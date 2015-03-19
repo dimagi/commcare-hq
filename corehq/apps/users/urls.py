7964608e7bee7b1b16cd601ec6e6990795a749bc
Giovanni Capalbo <gcapalbo@dimagi.com>
2015-03-18 23:13:25 -0400

    Lint feedback

--- corehq/apps/users/urls.py
+++ corehq/apps/users/urls.py
@@ -74,5 +74,6 @@ patterns("corehq.apps.users.views.mobile.users",
 patterns("corehq.apps.users.views.mobile.groups",
     url(r'^groups/$', EditGroupsView.as_view(), name=EditGroupsView.urlname),
     url(r'^groups/(?P<group_id>[ \w-]+)/$', EditGroupMembersView.as_view(), name=EditGroupMembersView.urlname),
-    url(r'^groups/sms_verification/(?P<group_id>[ \w-]+)$', BulkSMSVerificationView.as_view(), name=BulkSMSVerificationView.urlname),
+    url(r'^groups/sms_verification/(?P<group_id>[ \w-]+)$', BulkSMSVerificationView.as_view(),
+        name=BulkSMSVerificationView.urlname),
 )
