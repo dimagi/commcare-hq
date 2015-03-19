7321dad07bdad1beda3928e78c7d0806dd1a7ada
Kamil Krampa <kkrampa@soldevelo.com>
2015-03-19 16:11:25 +0100

    Make domains list based on command args

--- custom/logistics/management/commands/clear_verified_numbers.py
+++ custom/logistics/management/commands/clear_verified_numbers.py
@@ -5,8 +5,7 @@ from corehq.apps.sms.mixin import VerifiedNumber
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        logistics_domains = ['ewsghana-test-1', 'ilsgateway-test-1', 'ilsgateway-test-2']
-        for domain in logistics_domains:
+        for domain in args:
             verified_numbers = VerifiedNumber.by_domain(domain)
             print "Deleting verified numbers for domain: %s" % domain
             for verified_number in verified_numbers:
