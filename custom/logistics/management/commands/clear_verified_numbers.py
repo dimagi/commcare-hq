cbb2e3c7d8d7eacf2b079486cec0c8181070e4d7
Kamil Krampa <kkrampa@soldevelo.com>
2015-03-19 15:35:19 +0100

    Changed list of domains

--- custom/logistics/management/commands/clear_verified_numbers.py
+++ custom/logistics/management/commands/clear_verified_numbers.py
@@ -1,13 +1,11 @@
 from django.core.management import BaseCommand
 from corehq.apps.sms.mixin import VerifiedNumber
-from custom.ewsghana.models import EWSGhanaConfig
-from custom.ilsgateway.models import ILSGatewayConfig
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        logistics_domains = ILSGatewayConfig.get_all_enabled_domains() + EWSGhanaConfig.get_all_enabled_domains()
+        logistics_domains = ['ewsghana-test-1', 'ilsgateway-test-1', 'ilsgateway-test-2']
         for domain in logistics_domains:
             verified_numbers = VerifiedNumber.by_domain(domain)
             print "Deleting verified numbers for domain: %s" % domain
