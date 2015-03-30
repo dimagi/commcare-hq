f5ba407631660890fc28794640067bae92e37f6e
Cory Zue <czue@dimagi.com>
2015-03-30 12:04:56 -0400

    update tests and docs to use the new format

--- corehq/apps/userreports/tests/test_columns.py
+++ corehq/apps/userreports/tests/test_columns.py
@@ -141,11 +141,10 @@ class TestExpandedColumn(TestCase):
             title='foo',
             aggregation_columns=['doc_id'],
             columns=[{
-                "type": "field",
+                "type": "expanded",
                 "field": field,
                 "display": field,
                 "format": "default",
-                "aggregation": "expand",
             }],
             filters=[],
             configured_charts=[]
@@ -189,11 +188,10 @@ class TestExpandedColumn(TestCase):
 
     def test_expansion(self):
         column = ReportColumnFactory.from_spec(dict(
-            type="field",
+            type="expanded",
             field="lab_result",
             display="Lab Result",
             format="default",
-            aggregation="expand",
             description="foo"
         ))
         cols = _expand_column(column, ["positive", "negative"])
