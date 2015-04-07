31e5f9318a25abd36f2e16cedd877d9c17bda33c
Cory Zue <czue@dimagi.com>
2015-04-07 15:12:29 -0400

    optimization for steady state syncs of cases with many updates
    http://manage.dimagi.com/default.asp?164405

--- corehq/ex-submodules/casexml/apps/phone/caselogic.py
+++ corehq/ex-submodules/casexml/apps/phone/caselogic.py
@@ -11,6 +11,7 @@ from casexml.apps.case.util import reverse_indices
 from casexml.apps.case.xform import CaseDbCache
 from casexml.apps.phone.models import CaseState
 from dimagi.utils.decorators.memoized import memoized
+from dimagi.utils.parsing import string_to_utc_datetime
 
 logger = logging.getLogger(__name__)
 
@@ -569,24 +570,49 @@ def get_case_updates(user, last_sync):
     """
     return CaseSyncOperation(user, last_sync)
 
-def filter_cases_modified_elsewhere_since_sync(cases, last_sync):
-    # this function is pretty ugly and is heavily optimized to reduce the number
+
+def filter_cases_modified_elsewhere_since_sync(cases, last_sync_token):
+    """
+    This function takes in a list of unwrapped case dicts and a last_sync token and
+    returns the set of cases that should be applicable to be sent down on top of that
+    sync token.
+
+    This includes:
+
+      1. All cases that were modified since the last sync date by any phone other
+         than the phone that is associated with the sync token.
+      2. All cases that were not on the phone at the time of last sync that are
+         now on the phone.
+    """
+    # todo: this function is pretty ugly and is heavily optimized to reduce the number
     # of queries to couch.
-    if not last_sync:
+    if not last_sync_token:
         return cases
     else:
-        # todo: if this case list is huge i'm guessing this query is pretty expensive
-        case_ids = [case['_id'] for case in cases]
+        # we can start by filtering out our base set of cases to check for only
+        # things that have been modified since we last synced
+        recently_modified_case_ids = [
+            case['_id'] for case in cases
+            if case and string_to_utc_datetime(case['server_modified_on']) >= last_sync_token.date
+        ]
+
+        # create a mapping of all cases to sync logs for all cases that were modified
+        # in the appropriate ranges.
+        # todo: this should really have a better way to filter out updates from sync logs
+        # that we already have in a better way.
+        # todo: if this recently modified case list is huge i'm guessing this query is
+        # pretty expensive
         case_log_map = CommCareCase.get_db().view(
             'phone/cases_to_sync_logs',
-            keys=case_ids,
+            keys=recently_modified_case_ids,
             reduce=False,
         )
 
-        # create a set of tuples of the format (case_id, log_id)
         unique_combinations = set((row['key'], row['value']) for row in case_log_map)
 
-        # todo: and this could arguably be even worse
+        # todo: and this one is also going to be very bad. see note above about how we might
+        # be able to reduce it - by finding a way to only query for sync tokens that are more
+        # likely to be relevant.
         modification_dates = CommCareCase.get_db().view(
             'phone/case_modification_status',
             keys=[list(combo) for combo in unique_combinations],
@@ -597,23 +623,27 @@ def filter_cases_modified_elsewhere_since_sync(cases, last_sync):
         # { case_id: [{'token': 'token value', 'date': 'date value'}, ...]}
         all_case_updates_by_sync_token = defaultdict(list)
         for row in modification_dates:
-            # incoming format is a list of objects that look like this:
+            # format from couch is a list of objects that look like this:
             # {
             #   'value': '2012-08-22T08:55:14Z', (most recent date updated)
-            #   'key': ['[case id]', '[sync token id]']
+            #   'key': ['case-id', 'sync-token-id']
             # }
             if row['value']:
-                all_case_updates_by_sync_token[row['key'][0]].append(
-                    {'token': row['key'][1], 'date': datetime.strptime(row['value'], '%Y-%m-%dT%H:%M:%SZ')}
-                )
+                modification_date = datetime.strptime(row['value'], '%Y-%m-%dT%H:%M:%SZ')
+                if modification_date >= last_sync_token.date:
+                    case_id, sync_token_id = row['key']
+                    all_case_updates_by_sync_token[case_id].append(
+                        {'token': sync_token_id, 'date': modification_date}
+                    )
 
         def case_modified_elsewhere_since_sync(case_id):
             # NOTE: uses closures
-            return any([row['date'] >= last_sync.date and row['token'] != last_sync._id
+            return any([row['date'] >= last_sync_token.date and row['token'] != last_sync_token._id
                         for row in all_case_updates_by_sync_token[case_id]])
 
         def relevant(case):
             case_id = case['_id']
-            return case_modified_elsewhere_since_sync(case_id) or not last_sync.phone_is_holding_case(case_id)
+            return (case_modified_elsewhere_since_sync(case_id)
+                    or not last_sync_token.phone_is_holding_case(case_id))
 
         return filter(relevant, cases)
