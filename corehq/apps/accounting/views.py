7c0b81b0dc57dc6498c7f0de64e55fa1e71d3b45
Nick Pellegrino <npellegrino@dimagi.com>
2015-04-08 16:02:23 -0400

    remove TODO

--- corehq/apps/accounting/views.py
+++ corehq/apps/accounting/views.py
@@ -172,7 +172,7 @@ class ManageBillingAccountView(BillingAccountsSectionView, AsyncHandlerMixin):
             'basic_form': self.basic_account_form,
             'contact_form': self.contact_form,
             'subscription_list': [
-                (sub, Invoice.objects.filter(subscription=sub).latest('date_due').date_due # TODO - check query
+                (sub, Invoice.objects.filter(subscription=sub).latest('date_due').date_due
                       if Invoice.objects.filter(subscription=sub).count() else 'None on record',
                 ) for sub in Subscription.objects.filter(account=self.account)
             ],
