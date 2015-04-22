92be23f40b5d792e995a0b71424f0ebad56d051c
Cory Zue <czue@dimagi.com>
2015-04-22 09:54:33 -0400

    add ability to add documentation links to feature flags

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -6,9 +6,10 @@ from toggle.shortcuts import toggle_enabled, set_toggle
 
 
 class StaticToggle(object):
-    def __init__(self, slug, label, namespaces=None):
+    def __init__(self, slug, label, namespaces=None, documentation_link=None):
         self.slug = slug
         self.label = label
+        self.documentation_link = documentation_link
         if namespaces:
             self.namespaces = [None if n == NAMESPACE_USER else n for n in namespaces]
         else:
@@ -403,5 +404,6 @@ STREAM_RESTORE_CACHE = StaticToggle(
 ENABLE_LOADTEST_USERS = StaticToggle(
     'enable_loadtest_users',
     'Enable creating loadtest users on HQ',
-    [NAMESPACE_DOMAIN]
+    namespaces=[NAMESPACE_DOMAIN],
+    documentation_link='https://confluence.dimagi.com/display/ccinternal/Loadtest+Users'
 )
