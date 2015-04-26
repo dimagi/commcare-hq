e39c6e6eccebd3e74d6c2fc11d1fb59179bd83d1
NoahCarnahan <ncarnahan@dimagi.com>
2015-04-26 15:57:57 +0300

    specify flags in yaml

--- custom/abt/reports/expressions.py
+++ custom/abt/reports/expressions.py
@@ -1,5 +1,6 @@
-from collections import namedtuple
+import os
 from jsonobject import JsonObject
+import yaml
 from corehq.apps.userreports.specs import TypeProperty
 from dimagi.utils.decorators.memoized import memoized
 
@@ -10,76 +11,44 @@ class AbtSupervisorExpressionSpec(JsonObject):
     @property
     @memoized
     def _flag_specs(self):
-        FlagSpec = namedtuple("FlagSpec", [
-            "form_xmlns",
-            "flag_id",
-            "path",
-            "danger_value",
-            "warning_string",
-            "warning_property_path"
-        ])
-        return [
-            FlagSpec(
-                form_xmlns="http://openrosa.org/formdesigner/BB2BF979-BD8F-4B8D-BCF8-A46451228BA9",
-                flag_id="adequate_distance",
-                path=["q2"],
-                danger_value="No",
-                warning_string="The nearest sensitive receptor is {msg} meters away",
-                warning_property_path=['q2_next']
-            ),
-            FlagSpec(
-                form_xmlns="http://openrosa.org/formdesigner/BB2BF979-BD8F-4B8D-BCF8-A46451228BA9",
-                flag_id="leak_free",
-                path=["q5"],
-                danger_value="No",
-                warning_string="The leak will be repaired on {msg}",
-                warning_property_path=['q5_action_two']
-            ),
-            FlagSpec(
-                form_xmlns="dummy",
-                flag_id="dummy flag",
-                path=["dummy"],
-                danger_value="dummy",
-                warning_string="foo{msg}",
-                warning_property_path=['bloop']
-            ),
-            FlagSpec(
-                form_xmlns="http://openrosa.org/formdesigner/54338047-CFB6-4D5B-861B-2256A10BBBC8",
-                flag_id="eaten_breakfast",
-                path=["q2"],
-                danger_value="No",
-                warning_string="{msg}",
-                warning_property_path=['nothing_pls']
-            )
-        ]
-
-    def __call__(self, item, context=None):
         """
-        Given a document (item), return a list of documents representing each
-        of the flagged questions.
+        Return a dict where keys are form xmlns and values are lists of FlagSpecs
         """
-        # TODO: Don't define FlagSpec and get_val for every call
+        path = os.path.join(os.path.dirname(__file__), 'flagspecs.yaml')
+        with open(path) as f:
+            return yaml.load(f)
 
-        def get_val(item, path):
+    @classmethod
+    def _get_val(cls, item, path):
+        '''
+        Return empty tuple if path is not in item
+        '''
+        if path:
             try:
                 v = item['form']
                 for key in path:
                     v = v[key]
                 return v
             except KeyError:
-                return None
+                return ()
+
+    def __call__(self, item, context=None):
+        """
+        Given a document (item), return a list of documents representing each
+        of the flagged questions.
+        """
 
         docs = []
-        print item['xmlns']
-        for spec in self._flag_specs:
-            if item['xmlns'] == spec.form_xmlns:
-                if get_val(item, spec.path) == spec.danger_value:
-                    docs.append({
-                        'flag': spec.flag_id,
-                        'warning': spec.warning_string.format(
-                            msg=get_val(item, spec.warning_property_path) or ""
-                        )
-                    })
+        for spec in self._flag_specs.get(item['xmlns'], []):
+            form_value = self._get_val(item, spec['question'])
+            danger_value = spec.get('answer', [])
+            if form_value == danger_value or (form_value != () and danger_value == []):
+                docs.append({
+                    'flag': spec['question'][-1],
+                    'warning': spec['warning'].format(
+                        msg=self._get_val(item, spec.get('warning_question', None)) or ""
+                    )
+                })
         return docs
 
 
