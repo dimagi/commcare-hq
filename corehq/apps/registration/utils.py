345792d1f1092d4d6d2c594791444cc0b45f978c
Jenny Schweers <jschweers@dimagi.com>
2015-05-11 11:21:18 -0400

    Re-blank form name

--- corehq/apps/registration/utils.py
+++ corehq/apps/registration/utils.py
@@ -180,7 +180,7 @@ def request_new_domain(request, org, domain_type=None, new_user=True):
         dom_req.activation_guid = uuid.uuid1().hex
 
     new_domain = Domain(
-        name=form.cleaned_data['domain_name'],
+        name='',
         hr_name='Untitled Project',
         is_active=False,
         date_created=datetime.utcnow(),
