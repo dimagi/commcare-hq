74d91857f566a6cee10e2cbbb5c892f1c996a823
Farid Rener <proteusvacuum@gmail.com>
2015-05-14 10:41:37 +0200

    allow multiple async downloaders per page

--- corehq/apps/app_manager/static/app_manager/js/download_async_modal.js
+++ corehq/apps/app_manager/static/app_manager/js/download_async_modal.js
@@ -1,107 +1,71 @@
 $(function(){
     "use strict";
-    function AsyncDownloader($el, download_url){
+
+    COMMCAREHQ.AsyncDownloader = function($el, download_url){
         var self = this;
-        self.POLL_FREQUENCY = 500; //ms
+        self.POLL_FREQUENCY = 1500; //ms
+        self.ERROR_MESSAGE = "Sorry, something went wrong with the download. " +
+                             "If you see this repeatedly please report an issue.";
         self.download_url = download_url;
+
+        self.$el = $el;
+        self.el_id = $el.attr("id");
+        self.$download_progress = self.$el.find("#" + self.el_id + "-download-progress");
+        self.$downloading = self.$el.find("#" + self.el_id + "-downloading");
+
+        self.init = function(){
+            self.$download_progress.hide();
+            self.$downloading.show();
+        };
 
         self.startPollDownloadStatus = function(data){
             var pollDownloadStatus = function(){
                 $.ajax({
                     url: data.download_url,
                     success: function(resp) {
-                        var progress = $("#download-progress");
-                        if (resp.replace(/[ \t\n]/g,'')) {
-                            $("#downloading").hide();
-                            progress.show().html(resp);
-                            if (progress.find(".alert-success").length) {
+                        if (resp.trim().length) {
+                            self.$downloading.hide();
+                            self.$download_progress.show().html(resp);
+                            if (self.$download_progress.find(".alert-success").length) {
                                 clearInterval(interval);
                             }
                         }
                     },
                     error: function() {
-                        // self.downloadError();
-                        // clearInterval(interval);
+                        self.downloadError();
+                        clearInterval(interval);
                     }
                 });
             };
-            console.log(data);
+
             var interval = setInterval(pollDownloadStatus, self.POLL_FREQUENCY);
+
+            self.$el.on("hidden", function(){
+                clearInterval(interval);
+                self.init();
+            });
         };
 
         self.generateDownload = function(){
             $.ajax({
                 url: self.download_url,
-                type: 'GET',
-                dataType: 'json',
+                type: "GET",
+                dataType: "json",
                 success: function(data){
                     self.startPollDownloadStatus(data);
                 },
                 error: function(){
-                    console.log('sad face');
+                    self.downloadError();
                 }
             });
         };
 
-        $el.on("show", self.generateDownload);
-    }
-    var downloader = new AsyncDownloader($("#async-file-download"), $("#async-file-download").data('download-url'));
+        self.downloadError = function(){
+            self.$downloading.hide();
+            self.$download_progress.show().html(self.ERROR_MESSAGE);
+        };
 
+        self.$el.on("show", self.generateDownload);
+        self.init();
+    };
 }());
-
-
-// self.downloadExcels = function(element, event) {
-//     var tables = [];
-//     if (self.selectedTables().length < 1)
-//         return;
-//     for (var i in self.selectedTables()) {
-//         tables.push(self.selectedTables()[i]);
-//     }
-//     $("#fixture-download").modal();
-//     if (tables.length > 0){
-//         // POST, because a long querystring can overflow the request
-//         $.ajax({
-//             url: FixtureDownloadUrl,
-//             type: 'POST',
-//             data: {'table_ids': tables},
-//             dataType: 'json',
-//             success: function (response) {
-//                 self.setupDownload(response);
-//             },
-//             error: function (response) {
-//                 self.downloadError();
-//             }
-//         });
-//     }
-// };
-
-// self.setupDownload = function (response) {
-//     function poll() {
-//         $.ajax({
-//             url: response.download_url,
-//             dataType: 'text',
-//             success: function (resp) {
-//                 var progress = $("#download-progress");
-//                 if (resp.replace(/[ \t\n]/g,'')) {
-//                     $("#downloading").hide();
-//                     progress.show().html(resp);
-//                     if (progress.find(".alert-success").length) {
-//                         clearInterval(interval);
-//                     }
-//                 }
-//             },
-//             error: function () {
-//                 self.downloadError();
-//                 clearInterval(interval);
-//             }
-//         });
-//     }
-//     var interval = setInterval(poll, 2000);
-//     $("#fixture-download").one("hidden", function() {
-//         // stop polling if dialog is closed
-//         clearInterval(interval);
-//     });
-//     $("#download-progress").hide();
-//     $("#downloading").show();
-//     poll();
-// };
