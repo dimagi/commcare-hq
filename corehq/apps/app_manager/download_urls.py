5084b4614dfdb4b98fa92b593b5455a2ebac9270
Simon Kelly <skelly@dimagi.com>
2015-05-18 20:59:38 +0200

    give preference to multimedia download urls

--- corehq/apps/app_manager/download_urls.py
+++ corehq/apps/app_manager/download_urls.py
@@ -12,7 +12,6 @@ urlpatterns = patterns('corehq.apps.app_manager.views',
     url(r'^(?P<lang>[\w-]+)/app_strings.txt$', 'download_app_strings', {}, 'download_app_strings'),
 
     url(r'^user_registration.xml$', 'download_user_registration', {}, 'download_user_registration'),
-    url(r'^multimedia/', include(media_download_urls)),
     url(r'^modules-(?P<module_id>\d+)/forms-(?P<form_id>\d+).xml$', 'download_xform', {}, 'download_xform'),
     url(r'^CommCare.jad$', 'download_jad', {}, 'download_jad'),
     url(r'^CommCare_raw.jar$', 'download_raw_jar', {}, 'download_raw_jar'),
