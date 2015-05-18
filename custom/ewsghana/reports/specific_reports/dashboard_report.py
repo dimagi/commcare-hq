00874f252091f0e82134da4ca0d617dcb9d47875
Adrian Grzywinski <agrzywinski@soldevelo.com>
2015-05-18 09:12:19 +0200

    FPR in dashboard when facility is selected

--- custom/ewsghana/reports/specific_reports/dashboard_report.py
+++ custom/ewsghana/reports/specific_reports/dashboard_report.py
@@ -1,8 +1,10 @@
 from corehq.apps.reports.filters.dates import DatespanFilter
 from custom.ewsghana.filters import EWSLocationFilter
-from custom.ewsghana.reports import MultiReport
+from custom.ewsghana.reports import MultiReport, ProductSelectionPane
 from custom.ewsghana.reports.specific_reports.reporting_rates import ReportingRates, ReportingDetails
 from custom.ewsghana.reports.specific_reports.stock_status_report import ProductAvailabilityData
+from custom.ewsghana.reports.stock_levels_report import FacilityReportData, StockLevelsLegend, InputStock, \
+    FacilitySMSUsers, FacilityUsers, FacilityInChargeUsers, InventoryManagementData
 from custom.ewsghana.utils import get_country_id
 
 
@@ -21,12 +23,30 @@ class DashboardReport(MultiReport):
             startdate=self.datespan.startdate_utc,
             enddate=self.datespan.enddate_utc,
             location_id=self.request.GET.get('location_id') or get_country_id(self.domain),
+            user=self.request.couch_user,
             program=None,
             products=None
         )
 
     @property
     def data_providers(self):
+        config = self.report_config
+        if self.is_reporting_type():
+            self.split = True
+            if self.is_rendered_as_email:
+                return [FacilityReportData(config)]
+            else:
+                return [
+                    FacilityReportData(config),
+                    StockLevelsLegend(config),
+                    InputStock(config),
+                    FacilitySMSUsers(config),
+                    FacilityUsers(config),
+                    FacilityInChargeUsers(config),
+                    InventoryManagementData(config),
+                    ProductSelectionPane(config)
+                ]
+        self.split = False
         return [
             ProductAvailabilityData(config=self.report_config),
             ReportingRates(config=self.report_config),
