c5cad5e3f1288733e2143a284e2dc335a29ef5df
Simon Kelly <skelly@dimagi.com>
2015-05-18 11:36:53 +0200

    update soil dependency for tests

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
         "django<1.7",
         "jsonobject-couchdbkit",
         "dimagi-utils>=1.3usec",
-        'django-soil',
+        'django-soil==0.10.0',
         "openpyxl",
         "unidecode",
         "xlwt",
@@ -22,5 +22,6 @@ setup(
     include_package_data=True,
     dependency_links=[
         'git+git://github.com/dimagi/dimagi-utils.git@24e1eaad37ac735ab9309253011f09835c9ee67e#egg=dimagi-utils-1.3usec',
+        'git+git://github.com/dimagi/django-soil.git@ad98c51dd95baec568c5f83af0807ef7965bf201#egg=django-soil-0.10.0',
     ]
 )
