3e73bbb51c7b5b214dc2c79e5a1cb8b00f243855
Tyler Wymer <twymer@dimagi.com>
2015-05-19 18:21:10 -0400

    Fix tests

--- corehq/apps/locations/tests/test_location_fixtures.py
+++ corehq/apps/locations/tests/test_location_fixtures.py
@@ -1,16 +1,22 @@
 from django.test import SimpleTestCase
-from corehq.apps.locations.models import Location
+from corehq.apps.locations.models import SQLLocation, LocationType
 from ..fixtures import _location_to_fixture, _location_footprint
 
 
 class LocationFixturesTest(SimpleTestCase):
 
     def test_metadata(self):
-        location = Location(
-            _id="unique-id",
+        state = LocationType(
+            domain="test-domain",
+            name="state",
+            code="state",
+        )
+
+        location = SQLLocation(
+            location_id="unique-id",
             domain="test-domain",
             name="Braavos",
-            location_type="state",
+            location_type=state,
             metadata={'best_swordsman': "Sylvio Forel",
                       'in_westeros': "false"},
         )
