cc85a5fa01990f7a5b4e06f8fe3a83fcabcd5c7e
Kamil Krampa <kkrampa@soldevelo.com>
2015-05-19 10:28:59 +0200

    Added receivers in logistics custom modules

--- custom/ilsgateway/models.py
+++ custom/ilsgateway/models.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from django.dispatch.dispatcher import receiver
+from corehq.apps.domain.signals import commcare_domain_pre_delete
 
 from dimagi.ext.couchdbkit import Document, BooleanProperty, StringProperty
-from django.db import models
+from django.db import models, connection
 
 from casexml.apps.stock.models import DocDomainMapping
 from corehq.apps.products.models import Product
@@ -460,3 +462,47 @@ class ILSNotes(models.Model):
     user_phone = models.CharField(max_length=20, null=True)
     date = models.DateTimeField()
     text = models.TextField()
+
+
+@receiver(commcare_domain_pre_delete)
+def domain_pre_delete_receiver(domain, **kwargs):
+    domain_name = domain.name
+    locations_ids = SQLLocation.objects.filter(domain=domain_name).values_list('location_id', flat=True)
+    if locations_ids:
+        DeliveryGroupReport.objects.filter(location_id__in=locations_ids).delete()
+        SupplyPointWarehouseRecord.objects.filter(supply_point__in=locations_ids)
+
+        cursor = connection.cursor()
+        cursor.execute(
+            "DELETE FROM ilsgateway_alert WHERE location_id IN "
+            "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+        )
+        cursor.execute(
+            "DELETE FROM ilsgateway_groupsummary WHERE org_summary_id IN "
+            "(SELECT id FROM ilsgateway_organizationsummary WHERE location_id IN "
+            "(SELECT location_id FROM locations_sqllocation WHERE domain=%s))", [domain_name]
+        )
+
+        cursor.execute(
+            "DELETE FROM ilsgateway_organizationsummary WHERE location_id IN "
+            "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+        )
+
+        cursor.execute(
+            "DELETE FROM ilsgateway_productavailabilitydata WHERE location_id IN "
+            "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+        )
+
+        cursor.execute(
+            "DELETE FROM ilsgateway_supplypointstatus WHERE location_id IN "
+            "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+        )
+
+        cursor.execute(
+            "DELETE FROM ilsgateway_historicallocationgroup WHERE location_id_id IN "
+            "(SELECT id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+        )
+
+    ReportRun.objects.filter(domain=domain_name).delete()
+    ILSNotes.objects.filter(domain=domain_name).delete()
+    SupervisionDocument.objects.filter(domain=domain_name).delete()
