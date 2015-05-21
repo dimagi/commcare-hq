d89431e296de9b314b25ade853ee67d857459f5f
Cory Zue <czue@dimagi.com>
2015-05-21 10:59:20 +0200

    use total case count

--- corehq/ex-submodules/casexml/apps/case/tests/test_ota_restore.py
+++ corehq/ex-submodules/casexml/apps/case/tests/test_ota_restore.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 import os
 from django.test.utils import override_settings
+from casexml.apps.case.dbaccessors import get_total_case_count
 from casexml.apps.case.models import CommCareCase
 from casexml.apps.case.xform import process_cases
 from casexml.apps.case.tests import delete_all_cases
@@ -12,16 +13,16 @@ class TestOTARestore(TestCase):
     
     def setUp(self):
         delete_all_cases()
+        self.assertEqual(0, get_total_case_count())
 
     def testOTARestore(self):
-        self.assertEqual(0, len(CommCareCase.view("case/by_user", reduce=False).all()))
         file_path = os.path.join(os.path.dirname(__file__), "data", "multicase", "parallel_cases.xml")
         with open(file_path, "rb") as f:
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
         process_cases(form)
-        self.assertEqual(4, len(CommCareCase.view("case/by_user", reduce=False).all()))
-        
+        self.assertEqual(4, get_total_case_count())
+
     def testMixed(self):
         self.assertEqual(0, len(CommCareCase.view("case/by_user", reduce=False).all()))
         file_path = os.path.join(os.path.dirname(__file__), "data", "multicase", "mixed_cases.xml")
@@ -29,4 +30,4 @@ class TestOTARestore(TestCase):
             xml_data = f.read()
         form = post_xform_to_couch(xml_data)
         process_cases(form)
-        self.assertEqual(4, len(CommCareCase.view("case/by_user", reduce=False).all()))
+        self.assertEqual(4, get_total_case_count())
