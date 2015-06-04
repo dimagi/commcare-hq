d1b8aa02b03215d4df6f915c487f05acdb1fa05f
Cory Zue <czue@dimagi.com>
2015-06-04 12:14:39 +0200

    pull out make_loc_type into its own function

--- corehq/apps/locations/tests/test_location_types.py
+++ corehq/apps/locations/tests/test_location_types.py
@@ -17,15 +17,15 @@ class TestLocationTypes(TestCase):
         LocationType.objects.filter(domain=self.domain.name).delete()
 
     def test_hierarchy(self):
-        state = self.make_loc_type('state')
+        state = make_loc_type('state')
 
-        district = self.make_loc_type('district', state)
-        section = self.make_loc_type('section', district)
-        block = self.make_loc_type('block', district)
-        center = self.make_loc_type('center', block)
+        district = make_loc_type('district', state)
+        section = make_loc_type('section', district)
+        block = make_loc_type('block', district)
+        center = make_loc_type('center', block)
 
-        county = self.make_loc_type('county', state)
-        city = self.make_loc_type('city', county)
+        county = make_loc_type('county', state)
+        city = make_loc_type('city', county)
 
         hierarchy = LocationType.objects.full_hierarchy(self.domain.name)
         desired_hierarchy = {
@@ -50,10 +50,14 @@ class TestLocationTypes(TestCase):
         }
         self.assertEqual(hierarchy, desired_hierarchy)
 
-    def make_loc_type(self, name, parent_type=None):
-        return LocationType.objects.create(
-            domain=self.domain.name,
-            name=name,
-            code=name,
-            parent_type=parent_type,
-        )
+
+def make_loc_type(name, parent_type=None, domain='locations-test',
+                  shares_cases=False, view_descendants=False):
+    return LocationType.objects.create(
+        domain=domain,
+        name=name,
+        code=name,
+        parent_type=parent_type,
+        shares_cases=shares_cases,
+        view_descendants=view_descendants
+    )
