d69d57e303ad4a8d54f08fe975f23a0ba610a465
Ethan Soergel <esoergel@gmail.com>
2015-06-05 16:07:00 -0400

    Move setUp to setUpClass

--- corehq/apps/locations/tests/test_reupholster.py
+++ corehq/apps/locations/tests/test_reupholster.py
@@ -1,19 +1,33 @@
+from django.test import TestCase
+
+from corehq.apps.commtrack.tests.util import bootstrap_location_types
+from corehq.apps.domain.shortcuts import create_domain
+
 from ..models import Location, SQLLocation
 from .test_locations import LocationTestBase
-from .util import make_loc
+from .util import make_loc, delete_all_locations
 
 
-class TestReupholster(LocationTestBase):
+class TestReupholster(TestCase):
     """
     These tests were written to drive removal of sepecific queries. It
     is safe to delete this when the reuholstering of Location is done
     and somone has written test coverage for the methods used in here.
     """
-    def setUp(self):
-        super(TestReupholster, self).setUp()
-        self.state = make_loc("Florida", type='state')
-        self.district = make_loc("Duval", type='district', parent=self.state)
-        self.block = make_loc("Jacksonville", type='block', parent=self.district)
+    @classmethod
+    def setUpClass(cls):
+        delete_all_locations()
+        cls.domain = create_domain('locations-test')
+        cls.domain.locations_enabled = True
+        bootstrap_location_types(cls.domain.name)
+
+        cls.state = make_loc("Florida", type='state')
+        cls.district = make_loc("Duval", type='district', parent=cls.state)
+        cls.block = make_loc("Jacksonville", type='block', parent=cls.district)
+
+    @classmethod
+    def tearDownClass(cls):
+        delete_all_locations()
 
     def test_replace_all_ids(self):
         original_result = set([r['id'] for r in Location.get_db().view(
--- corehq/apps/locations/tests/util.py
+++ corehq/apps/locations/tests/util.py
@@ -1,4 +1,7 @@
-from corehq.apps.locations.models import Location
+from dimagi.utils.couch.database import iter_bulk_delete
+from couchdbkit.exceptions import BulkSaveError
+from corehq.util.test_utils import unit_testing_only
+from corehq.apps.locations.models import Location, SQLLocation
 
 TEST_DOMAIN = 'locations-test'
 TEST_LOCATION_TYPE = 'location'
@@ -9,3 +12,17 @@ def make_loc(code, name=None, domain=TEST_DOMAIN, type=TEST_LOCATION_TYPE, paren
     loc = Location(site_code=code, name=name, domain=domain, location_type=type, parent=parent)
     loc.save()
     return loc
+
+
+@unit_testing_only
+def delete_all_locations():
+    loc_ids = [
+        loc['id'] for loc in
+        Location.get_db().view('locations/by_name', reduce=False).all()
+    ]
+    try:
+        iter_bulk_delete(Location.get_db(), loc_ids)
+    except BulkSaveError:
+        pass
+
+    SQLLocation.objects.all().delete()
