991f3828133996f0bf8cf9328aa3a0a863f91244
Ethan Soergel <esoergel@gmail.com>
2015-06-08 12:12:23 -0400

    Also clear supply point cases

--- corehq/apps/locations/tests/util.py
+++ corehq/apps/locations/tests/util.py
@@ -1,6 +1,7 @@
 from dimagi.utils.couch.database import iter_bulk_delete
 from couchdbkit.exceptions import BulkSaveError
 from corehq.util.test_utils import unit_testing_only
+from corehq.apps.commtrack.models import SupplyPointCase
 from corehq.apps.locations.models import Location, SQLLocation
 
 TEST_DOMAIN = 'locations-test'
@@ -16,13 +17,16 @@ def make_loc(code, name=None, domain=TEST_DOMAIN, type=TEST_LOCATION_TYPE, paren
 
 @unit_testing_only
 def delete_all_locations():
-    loc_ids = [
-        loc['id'] for loc in
-        Location.get_db().view('locations/by_name', reduce=False).all()
-    ]
-    try:
-        iter_bulk_delete(Location.get_db(), loc_ids)
-    except BulkSaveError:
-        pass
+    to_delete = [(Location, 'locations/by_name'),
+                 (SupplyPointCase, 'commtrack/supply_point_by_loc')]
+    for model, view in to_delete:
+        ids = [
+            doc['id'] for doc in
+            model.get_db().view(view, reduce=False).all()
+        ]
+        try:
+            iter_bulk_delete(model.get_db(), ids)
+        except BulkSaveError:
+            pass
 
     SQLLocation.objects.all().delete()
