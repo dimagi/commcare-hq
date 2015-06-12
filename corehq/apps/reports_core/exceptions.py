8f2a484c10d17cde4acd354f87f1ad95f6fd16b5
Cory Zue <czue@dimagi.com>
2015-06-12 10:50:44 -0400

    move exceptions to their own module

---
+++ corehq/apps/reports_core/exceptions.py
@@ -0,0 +1,17 @@
+
+
+class FilterException(Exception):
+    """
+    Exceptions involving report filters.
+    """
+    pass
+
+
+class MissingParamException(FilterException):
+    pass
+
+
+class FilterValueException(FilterException):
+    pass
+
+
