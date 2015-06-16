39afeb64660412bcc937686f4afef4ba4fe40643
Jenny Schweers <jschweers@dimagi.com>
2015-06-16 09:04:54 -0400

    Stop calling generate_name twice

--- corehq/apps/registration/forms.py
+++ corehq/apps/registration/forms.py
@@ -139,26 +139,18 @@ class DomainRegistrationForm(forms.Form):
     domain_type = forms.CharField(widget=forms.HiddenInput(), required=False,
                                   initial='commcare')
 
-    def clean_domain_name(self):
-        data = self.cleaned_data.get('hr_name', None)
-        if data is not None:
-            data = Domain.generate_name(data, self.max_name_length)
-        return data
-
     def clean_domain_type(self):
         data = self.cleaned_data.get('domain_type', '').strip().lower()
         return data if data else 'commcare'
 
-    def clean_hr_name(self):
-        data = self.cleaned_data.get('hr_name', None)
-        name = Domain.generate_name(data, self.max_name_length)
-        if name is None:
-            # Worst case, force user to pick something new
-            raise forms.ValidationError("Project name already taken---please try another")
-
-        return data
-
     def clean(self):
+        name = self.cleaned_data.get('hr_name', None)
+        if name is not None:
+            name = Domain.generate_name(name, self.max_name_length)
+            if name is None:
+                raise forms.ValidationError("Project name already taken---please try another")
+        self.cleaned_data['domain_name'] = name
+
         for field in self.cleaned_data:
             if isinstance(self.cleaned_data[field], basestring):
                 self.cleaned_data[field] = self.cleaned_data[field].strip()
