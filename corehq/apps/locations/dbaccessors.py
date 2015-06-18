c568f4d3ea475f341490bc81e89c28016e8412a2
Ethan Soergel <esoergel@gmail.com>
2015-06-18 16:33:23 -0400

    Allow getting the unwrapped doc

--- corehq/apps/locations/dbaccessors.py
+++ corehq/apps/locations/dbaccessors.py
@@ -1,8 +1,9 @@
 from corehq.apps.users.models import CommCareUser
 
 
-def _users_by_location(location_id, include_docs):
-    return CommCareUser.view(
+def _users_by_location(location_id, include_docs, wrap):
+    view = CommCareUser.view if wrap else CommCareUser.get_db().view
+    return view(
         'locations/users_by_location_id',
         startkey=[location_id],
         endkey=[location_id, {}],
@@ -10,13 +11,13 @@ def _users_by_location(location_id, include_docs):
     ).all()
 
 
-def get_users_by_location_id(location_id):
+def get_users_by_location_id(location_id, wrap=True):
     """
     Get all users for a given location
     """
-    return _users_by_location(location_id, include_docs=True)
+    return _users_by_location(location_id, include_docs=True, wrap=wrap)
 
 
 def get_user_ids_by_location(location_id):
     return [user['id'] for user in
-            _users_by_location(location_id, include_docs=False)]
+            _users_by_location(location_id, include_docs=False, wrap=False)]
