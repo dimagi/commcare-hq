7c1e207fb13d9db6959575ff06f15ff2bb166f8a
Ethan Soergel <esoergel@gmail.com>
2015-06-18 16:11:19 -0400

    Make dbaccessors for users_by_location

--- corehq/apps/locations/dbaccessors.py
+++ corehq/apps/locations/dbaccessors.py
@@ -1,13 +1,22 @@
 from corehq.apps.users.models import CommCareUser
 
 
-def get_users_by_location_id(location_id):
-    """
-    Get all users for a given location
-    """
+def _users_by_location(location_id, include_docs):
     return CommCareUser.view(
         'locations/users_by_location_id',
         startkey=[location_id],
         endkey=[location_id, {}],
-        include_docs=True
+        include_docs=include_docs,
     ).all()
+
+
+def get_users_by_location_id(location_id):
+    """
+    Get all users for a given location
+    """
+    return _users_by_location(location_id, include_docs=True)
+
+
+def get_user_ids_by_location(location_id):
+    return [user['id'] for user in
+            _users_by_location(location_id, include_docs=False)]
