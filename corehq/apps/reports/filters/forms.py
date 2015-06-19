b616813923c1782136a2d4d14f0bfa3cf2d2e117
Norman Hooper <nhooper@dimagi.com>
2015-06-20 01:30:50 +0200

    Check for request.couch_user Just In Case
    FormsByApplicationFilter extends BaseReportFilter, and request is a mandatory parameter, so we know it will always be set. I'm pretty sure reports are only available to authenticated users, but Just In Case, check couch_user is set.

--- corehq/apps/reports/filters/forms.py
+++ corehq/apps/reports/filters/forms.py
@@ -45,7 +45,7 @@ class FormsByApplicationFilter(BaseDrilldownOptionFilter):
         """
             This should return the lang code of the language being used to view this form.
         """
-        return self.request.couch_user.language or 'en'
+        return hasattr(self.request, 'couch_user') and self.request.couch_user.language or 'en'
 
     @property
     def rendered_labels(self):
