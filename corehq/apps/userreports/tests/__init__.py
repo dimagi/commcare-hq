ed79a914801bba8d22cb866b03f4f39be5abb481
NoahCarnahan <ncarnahan@dimagi.com>
2015-06-19 10:57:56 -0400

    Add test for export_tables bug

--- corehq/apps/userreports/tests/__init__.py
+++ corehq/apps/userreports/tests/__init__.py
@@ -15,6 +15,7 @@ from .test_report_config import *
 from .test_report_filters import *
 from .test_transforms import *
 from .test_utils import *
+from .test_view import *
 
 from corehq.apps.userreports.expressions.getters import recursive_lookup
 
