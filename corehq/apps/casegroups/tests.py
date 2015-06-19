6a4d0de85fa3dc46524f3615bda22ed4211e9cd3
Daniel Roberts <droberts@dimagi.com>
2015-06-19 14:09:50 -0400

    add casegroups tests

---
+++ corehq/apps/casegroups/tests.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from casexml.apps.case.models import CommCareCase
+from corehq.apps.casegroups.dbaccessors import get_case_groups_in_domain, \
+    get_number_of_case_groups_in_domain
+from corehq.apps.casegroups.models import CommCareCaseGroup
+
+
+class DBAccessorsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.domain = 'skbanskdjoasdkng'
+        cls.cases = [
+            CommCareCase(name='A', domain=cls.domain),
+            CommCareCase(name='B', domain=cls.domain),
+            CommCareCase(name='C', domain=cls.domain),
+            CommCareCase(name='D', domain=cls.domain),
+            CommCareCase(name='X', domain='bunny'),
+        ]
+        CommCareCase.get_db().bulk_save(cls.cases)
+        cls.case_groups = [
+            CommCareCaseGroup(name='alpha', domain=cls.domain,
+                              cases=[cls.cases[0]._id, cls.cases[1]._id]),
+            CommCareCaseGroup(name='beta', domain=cls.domain,
+                              cases=[cls.cases[2]._id, cls.cases[3]._id]),
+        ]
+        CommCareCaseGroup.get_db().bulk_save(cls.case_groups)
+
+    @classmethod
+    def tearDownClass(cls):
+        CommCareCase.get_db().bulk_delete(cls.cases)
+        CommCareCaseGroup.get_db().bulk_delete(cls.case_groups)
+
+    def assert_doc_lists_equal(self, docs1, docs2):
+        self.assertEqual(
+            [(doc._id, doc.to_json()) for doc in docs1],
+            [(doc._id, doc.to_json()) for doc in docs2],
+        )
+
+    def test_get_case_groups_in_domain(self):
+        self.assert_doc_lists_equal(
+            get_case_groups_in_domain(self.domain),
+            self.case_groups,
+        )
+
+    def test_get_number_of_case_groups_in_domain(self):
+        self.assertEqual(
+            get_number_of_case_groups_in_domain(self.domain),
+            len(self.case_groups)
+        )
