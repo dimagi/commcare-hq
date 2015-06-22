26b0047aaa13e6887d572e15ea8c4de33ab2730d
Daniel Roberts <droberts@dimagi.com>
2015-06-22 16:38:01 -0400

    add test scaffold for commtrack dbaccessors

---
+++ corehq/apps/commtrack/tests/test_dbaccessors.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+
+
+class RequisitionDBAccessorsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_get_open_requisition_cases_for_supply_point_id(self):
+        self.fail()
+
+    def test_get_open_requisition_cases_for_location(self):
+        self.fail()
+
+
+class SupplyPointDBAccessorsTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_get_supply_point_ids_in_domain_by_location(self):
+        self.fail()
+
+    def test_get_supply_points_json_in_domain_by_location(self):
+        self.fail()
+
+    def test_get_supply_point_case_by_location_id(self):
+        self.fail()
+
+    def test_get_supply_point_case_by_location(self):
+        self.fail()
