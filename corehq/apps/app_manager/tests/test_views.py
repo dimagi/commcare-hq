2655d36dda9ffa7e6602db273ba6f647e1eb363b
Jenny Schweers <jschweers@dimagi.com>
2015-06-23 14:51:27 -0400

    use app API

--- corehq/apps/app_manager/tests/test_views.py
+++ corehq/apps/app_manager/tests/test_views.py
@@ -3,14 +3,15 @@ import json
 
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from corehq.apps.app_manager.tests import add_build
+from corehq.apps.app_manager.tests import add_build, TestFileMixin
 from corehq.apps.app_manager.views import AppSummaryView
 from corehq.apps.builds.models import BuildSpec
 
 from corehq import toggles
 from corehq.apps.users.models import WebUser
 from corehq.apps.domain.shortcuts import create_domain
-from corehq.apps.app_manager.models import Application, APP_V1, APP_V2, import_app, Module
+from corehq.apps.app_manager.models import AdvancedModule, Application, APP_V1, APP_V2, import_app, Module, ReportModule
+from .test_form_versioning import BLANK_TEMPLATE
 
 
 class TestViews(TestCase):
@@ -26,6 +27,8 @@ class TestViews(TestCase):
         cls.user.is_superuser = True
         cls.user.save()
         cls.build = add_build(version='2.7.0', build_number=20655)
+        cls.app = Application.new_app(cls.domain, "TestApp", application_version=APP_V1)
+        cls.app.build_spec = BuildSpec.from_string('2.7.0/latest')
         toggles.CUSTOM_PROPERTIES.set("domain:{domain}".format(domain=cls.domain), True)
 
     def setUp(self):
@@ -45,8 +48,7 @@ class TestViews(TestCase):
         in some sense it does not exist.
         '''
 
-        app = Application.new_app(self.domain, "TestApp", application_version=APP_V1)
-        module = app.add_module(Module.new_module("Module0", "en"))
+        module = self.app.add_module(Module.new_module("Module0", "en"))
 
         # These builds are checked in to the repo for use in tests
         build1 = {'version': '1.2.dev', 'build_number': 7106}
@@ -57,11 +59,11 @@ class TestViews(TestCase):
 
         with open(os.path.join(os.path.dirname(__file__), "data", "invalid_form.xml")) as f:
             xform_str = f.read()
-        app.new_form(module.id, name="Form0-0", attachment=xform_str, lang="en")
-        app.save()
+        self.app.new_form(module.id, name="Form0-0", attachment=xform_str, lang="en")
+        self.app.save()
 
         response = self.client.get(reverse('app_download_file', kwargs=dict(domain=self.domain,
-                                                                            app_id=app.get_id,
+                                                                            app_id=self.app.get_id,
                                                                             path='modules-0/forms-0.xml')))
         self.assertEqual(response.status_code, 404)
 
@@ -100,31 +102,57 @@ class TestViews(TestCase):
 
         self.assertEqual(custom_properties["random"], "changed")
 
-    def _load_app(self, filename):
-        with open(os.path.join(os.path.dirname(__file__), 'data', filename)) as f:
-            self.app = import_app(json.loads(f.read()), self.domain)
-            self.app.build_spec = BuildSpec.from_string('2.7.0/latest')
-            self.build = self.app.make_build()
-            self.build.save()
-
     def _test_status_codes(self, names, kwargs):
         for name in names:
             response = self.client.get(reverse(name, kwargs=kwargs), follow=False)
             self.assertEqual(response.status_code, 200)
 
+    def _json_content_from_get(self, name, kwargs, data={}):
+        response = self.client.get(reverse(name, kwargs=kwargs), data)
+        self.assertEqual(response.status_code, 200)
+        return json.loads(response.content)
+
     def test_basic_app(self):
-        self._load_app('basic_app.json')
+        module = self.app.add_module(Module.new_module("Module0", "en"))
+        form = self.app.new_form(module.id, "Form0", "en", attachment=BLANK_TEMPLATE.format(xmlns='xmlns-0.0'))
+        self.app.save()
+
+        kwargs = {
+            'domain': self.domain,
+            'app_id': self.app.id,
+        }
         self._test_status_codes([
             'view_app',
             'release_manager',
             AppSummaryView.urlname,
-        ], {
+        ], kwargs)
+
+        self.build = self.app.make_build()
+        self.build.save()
+        content = self._json_content_from_get('current_app_version', {
             'domain': self.domain,
             'app_id': self.app.id,
         })
+        self.assertEqual(content['currentVersion'], 2)
+
+        content = self._json_content_from_get('paginate_releases', {
+            'domain': self.domain,
+            'app_id': self.app.id,
+        }, {'limit': 5})
+        self.assertEqual(len(content), 1)
+        content = content[0]
+        self.assertEqual(content['copy_of'], self.app.id)
+
+        kwargs['module_id'] = module.id
+        self._test_status_codes(['view_module'], kwargs)
+
+        kwargs['form_id'] = form.id
+        self._test_status_codes(['view_form', 'form_source'], kwargs)
+
 
     def test_user_registration(self):
-        self._load_app('user_registration.json')
+        self.app.show_user_registration = True
+        self.app.save()
         self._test_status_codes([
             'view_user_registration',
             'user_registration_source',
@@ -133,58 +161,20 @@ class TestViews(TestCase):
             'app_id': self.app.id,
         })
 
-    def test_module(self):
-        self._load_app('basic_app.json')
-        self._test_status_codes(['view_module'], {
-            'domain': self.domain,
-            'app_id': self.app.id,
-            'module_id': 0,
-        })
-
     def test_advanced_module(self):
-        self._load_app('advanced_module.json')
+        module = self.app.add_module(AdvancedModule.new_module("Module0", "en"))
+        self.app.save()
+        self._test_status_codes(['view_module'], {
+            'domain': self.domain,
+            'app_id': self.app.id,
+            'module_id': module.id,
+        })
+
+    def test_report_module(self):
+        module = self.app.add_module(ReportModule.new_module("Module0", "en"))
+        self.app.save()
         self._test_status_codes(['view_module'], {
             'domain': self.domain,
             'app_id': self.app.id,
             'module_id': 0,
         })
-
-    def test_reporting_module(self):
-        self._load_app('reporting_module.json')
-        self._test_status_codes(['view_module'], {
-            'domain': self.domain,
-            'app_id': self.app.id,
-            'module_id': 0,
-        })
-
-    def test_view_form(self):
-        self._load_app('basic_app.json')
-        self._test_status_codes(['view_form', 'form_source'], {
-            'domain': self.domain,
-            'app_id': self.app.id,
-            'module_id': 0,
-            'form_id': 0,
-        })
-
-    def _json_content_from_get(self, name, kwargs, data={}):
-        response = self.client.get(reverse(name, kwargs=kwargs), data)
-        self.assertEqual(response.status_code, 200)
-        return json.loads(response.content)
-
-    def test_current_build(self):
-        self._load_app('basic_app.json')
-        content = self._json_content_from_get('current_app_version', {
-            'domain': self.domain,
-            'app_id': self.app.id,
-        })
-        self.assertEqual(content['currentVersion'], 1)
-
-    def test_pagination(self):
-        self._load_app('basic_app.json')
-        content = self._json_content_from_get('paginate_releases', {
-            'domain': self.domain,
-            'app_id': self.app.id,
-        }, {'limit': 5})
-        self.assertEqual(len(content), 1)
-        content = content[0]
-        self.assertEqual(content['copy_of'], self.app.id)
