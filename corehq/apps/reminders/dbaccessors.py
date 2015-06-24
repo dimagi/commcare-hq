df69a108fe7766b172777169eaa1534d54f0425f
Daniel Roberts <droberts@dimagi.com>
2015-06-24 17:46:09 -0400

    remove calls to reminders/survey_by_domain

--- corehq/apps/reminders/dbaccessors.py
+++ corehq/apps/reminders/dbaccessors.py
@@ -1,8 +1,9 @@
 def get_surveys_in_domain(domain):
     from corehq.apps.reminders.models import Survey
-    return Survey.view(
-        'reminders/survey_by_domain',
-        startkey=[domain],
-        endkey=[domain, {}],
-        include_docs=True
-    ).all()
+    return sorted(Survey.view(
+        'domain/docs',
+        startkey=[domain, 'Survey'],
+        endkey=[domain, 'Survey', {}],
+        include_docs=True,
+        reduce=False,
+    ), key=lambda survey: survey.name)
