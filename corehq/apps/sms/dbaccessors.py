91f609750d62113b5c6ebab32daa4a6dc3f70325
Daniel Roberts <droberts@dimagi.com>
2015-06-24 17:07:02 -0400

    remove calls to sms/forwarding_rule

--- corehq/apps/sms/dbaccessors.py
+++ corehq/apps/sms/dbaccessors.py
@@ -3,7 +3,9 @@ from corehq.apps.sms.models import ForwardingRule
 
 def get_forwarding_rules_for_domain(domain):
     return ForwardingRule.view(
-        "sms/forwarding_rule",
-        key=[domain],
+        "domain/docs",
+        startkey=[domain, 'ForwardingRule'],
+        endkey=[domain, 'ForwardingRule', {}],
         include_docs=True,
+        reduce=False,
     ).all()
