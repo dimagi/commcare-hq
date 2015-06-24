0a8ab2108d25ef6dd4df4a89ae2cd00fc3f3791c
Daniel Roberts <droberts@dimagi.com>
2015-06-24 12:40:17 -0400

    remove calls to fixtures/data_types_by_domain

--- corehq/apps/fixtures/dbaccessors.py
+++ corehq/apps/fixtures/dbaccessors.py
@@ -3,17 +3,20 @@ from corehq.apps.fixtures.models import FixtureDataType
 
 def get_number_of_fixture_data_types_in_domain(domain):
     num_fixtures = FixtureDataType.get_db().view(
-        'fixtures/data_types_by_domain',
+        'domain/docs',
+        startkey=[domain, 'FixtureDataType'],
+        endkey=[domain, 'FixtureDataType', {}],
         reduce=True,
-        key=domain,
+        group_level=2,
     ).first()
     return num_fixtures['value'] if num_fixtures is not None else 0
 
 
 def get_fixture_data_types_in_domain(domain):
     return FixtureDataType.view(
-        'fixtures/data_types_by_domain',
-        key=domain,
+        'domain/docs',
+        endkey=[domain, 'FixtureDataType'],
+        startkey=[domain, 'FixtureDataType', {}],
         reduce=False,
         include_docs=True,
         descending=True,
