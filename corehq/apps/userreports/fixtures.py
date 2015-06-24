85f0b8af5afc30b5d3b39cb83228358bbe3e8460
Norman Hooper <nhooper@dimagi.com>
2015-06-24 16:54:49 +0200

    Move functions to methods

--- corehq/apps/userreports/fixtures.py
+++ corehq/apps/userreports/fixtures.py
@@ -23,43 +23,38 @@ class ReportFixturesProvider(object):
         root = ElementTree.Element('fixture', attrib={'id': self.id})
         reports_elem = ElementTree.Element('reports')
         for report in reports:
-            reports_elem.append(_report_to_fixture(report))
+            reports_elem.append(self._report_to_fixture(report))
         root.append(reports_elem)
         return [root]
 
+    def _report_to_fixture(self, report):
+        report_elem = ElementTree.Element('report', attrib={'id': report._id})
+        report_elem.append(self._element('name', report.title))
+        report_elem.append(self._element('description', report.description))
+        # todo: set filter values properly?
+        data_source = ReportFactory.from_spec(report)
+
+        rows_elem = ElementTree.Element('rows')
+
+        for i, row in enumerate(data_source.get_data()):
+            row_elem = ElementTree.Element('row', attrib={'index': str(i)})
+            for k in sorted(row.keys()):
+                row_elem.append(self._element('column', self._serialize(row[k]), attrib={'id': k}))
+            rows_elem.append(row_elem)
+
+        report_elem.append(rows_elem)
+        return report_elem
+
+    @staticmethod
+    def _element(name, text, attrib=None):
+        attrib = attrib or {}
+        element = ElementTree.Element(name, attrib=attrib)
+        element.text = text
+        return element
+
+    @staticmethod
+    def _serialize(value):
+        # todo: be smarter than this
+        return '' if value is None else unicode(value)
+
 report_fixture_generator = ReportFixturesProvider()
-
-
-def _report_to_fixture(report):
-    report_elem = ElementTree.Element('report',
-        attrib={
-            'id': report._id,
-        },
-    )
-    report_elem.append(_element('name', report.title))
-    report_elem.append(_element('description', report.description))
-    # todo: set filter values properly?
-    data_source = ReportFactory.from_spec(report)
-
-    rows_elem = ElementTree.Element('rows')
-
-    for i, row in enumerate(data_source.get_data()):
-        row_elem = ElementTree.Element('row', attrib={'index': str(i)})
-        for k in sorted(row.keys()):
-            row_elem.append(_element('column', _serialize(row[k]), attrib={'id': k}))
-        rows_elem.append(row_elem)
-
-    report_elem.append(rows_elem)
-    return report_elem
-
-
-def _element(name, text, attrib=None):
-    attrib = attrib or {}
-    element = ElementTree.Element(name, attrib=attrib)
-    element.text = text
-    return element
-
-
-def _serialize(value):
-    # todo: be smarter than this
-    return '' if value is None else unicode(value)
