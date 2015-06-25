9af0c861f5f5ba8c8ceed6aedc77b05efd2b0add
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2015-06-25 14:33:09 +0200

    show child list in hierarchy when parent have only one option

--- custom/up_nrhm/reports/asha_reports.py
+++ custom/up_nrhm/reports/asha_reports.py
@@ -35,20 +35,22 @@ class ASHAReports(GenericTabularReport, NRHMDatespanMixin, CustomProjectReport):
     @property
     def report_subtitles(self):
         sf = self.report_config.get('sf')
-        selected_af = self.request.GET.get('hierarchy_af')
-        user = CommCareUser.get(selected_af)
-        if sf in ['sf5', 'sf4', 'sf3']:
-            return [
-                "Selected AF: {0} {1}".format(user.first_name, user.last_name),
-                "Last Month ff the Quarter: {0} {1}".format(calendar.month_name[int(self.request.GET.get('month'))],
-                                                            self.request.GET.get('year'))
-            ]
-        else:
-            return [
-                "Selected AF: {0} {1}".format(user.first_name, user.last_name),
-                "For Date: {0} to {1}".format(self.datespan.startdate.strftime("%Y-%m-%d"),
-                                              self.datespan.enddate.strftime("%Y-%m-%d"))
-            ]
+        if sf:
+            selected_af = self.request.GET.get('hierarchy_af')
+            user = CommCareUser.get(selected_af)
+            if sf in ['sf5', 'sf4', 'sf3']:
+                return [
+                    "Selected AF: {0} {1}".format(user.first_name, user.last_name),
+                    "Last Month ff the Quarter: {0} {1}".format(calendar.month_name[int(self.request.GET.get('month'))],
+                                                                self.request.GET.get('year'))
+                ]
+            else:
+                return [
+                    "Selected AF: {0} {1}".format(user.first_name, user.last_name),
+                    "For Date: {0} to {1}".format(self.datespan.startdate.strftime("%Y-%m-%d"),
+                                                  self.datespan.enddate.strftime("%Y-%m-%d"))
+                ]
+
 
     @property
     def report_config(self):
