cad5c0a5aa263d6babb6ec950aef95dcefef54ee
Sravan Reddy <sravfeyn@gmail.com>
2015-07-08 11:12:04 +0200

    use quickcache

--- corehq/apps/data_analytics/malt_generator.py
+++ corehq/apps/data_analytics/malt_generator.py
@@ -3,6 +3,7 @@ from corehq.apps.data_analytics.models import MALTRow
 from corehq.apps.domain.models import Domain
 from corehq.apps.smsforms.app import COMMCONNECT_DEVICE_ID
 from corehq.apps.sofabed.models import FormData
+from corehq.util.quickcache import quickcache
 
 
 class MALTTableGenerator(object):
@@ -10,7 +11,6 @@ class MALTTableGenerator(object):
         Populates SQL table with data for given datespan
         See .models.MALTRow
     """
-    _wam_pam_cache = {}
 
     def __init__(self, datespan_object):
         self.datespan = datespan_object
@@ -41,7 +41,6 @@ class MALTTableGenerator(object):
                     )
                     malt_rows_to_save.append(db_row)
             MALTRow.objects.bulk_create(malt_rows_to_save)
-            self._destroy_wam_pam_cache(domain.name)
 
     def get_forms_queryset(self, user_id, domain_name):
         start_date = self.datespan.startdate
@@ -54,21 +53,10 @@ class MALTTableGenerator(object):
         )
 
     @classmethod
+    @quickcache(['domain', 'app_id'])
     def _wam_pams(cls, domain, app_id):
-        if (domain in cls._wam_pam_cache and
-                app_id in cls._wam_pam_cache[domain]):
-            return cls._wam_pam_cache[domain][app_id]
-
-        elif domain not in cls._wam_pam_cache:
-            cls._wam_pam_cache[domain] = {}
 
         app = Application.get(app_id)
         wam, pam = (getattr(app, 'amplifies_workers', 'not_set'),
                     getattr(app, 'amplifies_project', 'not_set'))
-        cls._wam_pam_cache[domain][app_id] = (wam, pam)
         return wam, pam
-
-    @classmethod
-    def _destroy_wam_pam_cache(cls, domain):
-        if domain in cls._wam_pam_cache:
-            del cls._wam_pam_cache[domain]
