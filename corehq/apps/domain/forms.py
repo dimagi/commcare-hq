b0bb3ec91da762197bf74a22205c5d3ead74f447
Daniel Roberts <droberts@dimagi.com>
2015-07-09 14:17:08 -0400

    make probono application email a zapier address

--- corehq/apps/domain/forms.py
+++ corehq/apps/domain/forms.py
@@ -1186,7 +1186,7 @@ class ProBonoForm(forms.Form):
             }
             html_content = render_to_string("domain/email/pro_bono_application.html", params)
             text_content = render_to_string("domain/email/pro_bono_application.txt", params)
-            recipient = settings.SUPPORT_EMAIL
+            recipient = settings.PROBONO_SUPPORT_EMAIL
             subject = "[Pro-Bono Application]"
             if domain is not None:
                 subject = "%s %s" % (subject, domain)
