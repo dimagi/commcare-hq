c026de1bb6275dfe92cba55c2ec05ddf05656411
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-10 12:30:47 -0400

    update thread local set / get for bootstrap version

--- corehq/apps/style/utils.py
+++ corehq/apps/style/utils.py
@@ -1,19 +1,22 @@
-from thread import _local
+import threading
+
+_thread_local = threading.local()
 
 BOOTSTRAP_2 = 'bootstrap-2'
 BOOTSTRAP_3 = 'bootstrap-3'
 
 
 def get_bootstrap_version():
-    bootstrap_version = _local.BOOTSTRAP_VERSION
-    if bootstrap_version is None:
+    try:
+        bootstrap_version = _thread_local.BOOTSTRAP_VERSION
+    except AttributeError:
         bootstrap_version = BOOTSTRAP_2
     return bootstrap_version
 
 
 def set_bootstrap_version3():
-    _local.BOOTSTRAP_VERSION = BOOTSTRAP_3
+    _thread_local.BOOTSTRAP_VERSION = BOOTSTRAP_3
 
 
 def set_bootstrap_version2():
-    _local.BOOTSTRAP_VERSION = BOOTSTRAP_2
+    _thread_local.BOOTSTRAP_VERSION = BOOTSTRAP_2
