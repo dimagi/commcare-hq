7d10b39c18bcce6a3e2249bb02005a950b7bac33
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-09 20:20:14 -0400

    use_bootstrap3 decorator instead of preview_bootstrap3, and remove parallel b2/b3 view for now

--- corehq/apps/style/decorators.py
+++ corehq/apps/style/decorators.py
@@ -18,20 +18,6 @@ def use_bootstrap3():
     return decorate
 
 
-def check_preview_bootstrap3():
-    def decorate(fn):
-        """
-        Decorator to Toggle on the use of bootstrap 3.
-        """
-        def wrapped(request, *args, **kwargs):
-            request.preview_bootstrap3 = (
-                hasattr(request, 'user')
-                and toggles.BOOTSTRAP3_PREVIEW.enabled(request.user.username))
-            return fn(request, *args, **kwargs)
-        return wrapped
-    return decorate
-
-
 def use_select2():
     def decorate(fn):
         """
