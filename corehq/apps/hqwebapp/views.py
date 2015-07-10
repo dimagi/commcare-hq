365bd9807cdd7fbc4d7b3afffd42bf8b49a99665
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-10 16:00:52 -0400

    make more explicit that maintenance alerts are a bootstrap2 template

--- corehq/apps/hqwebapp/views.py
+++ corehq/apps/hqwebapp/views.py
@@ -955,7 +955,7 @@ def osdd(request, template='osdd.xml'):
     return response
 
 @require_superuser
-def maintenance_alerts(request, template='hqwebapp/maintenance_alerts.html'):
+def maintenance_alerts(request, template='style/bootstrap2/maintenance_alerts.html'):
     from corehq.apps.hqwebapp.models import MaintenanceAlert
 
     return render(request, template, {
