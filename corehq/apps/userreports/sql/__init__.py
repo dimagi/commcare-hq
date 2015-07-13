4d6f1eb80b1c879daa7f9dd746b50035e8dafa0b
Cory Zue <czue@dimagi.com>
2015-07-13 14:23:37 +0200

    [ci skip] whitespace

--- corehq/apps/userreports/sql/__init__.py
+++ corehq/apps/userreports/sql/__init__.py
@@ -2,4 +2,3 @@ from .adapter import get_indicator_table, IndicatorSqlAdapter, metadata
 from .columns import get_expanded_column_config, SqlColumnConfig
 from .connection import create_engine
 from .util import get_column_name, get_table_name, truncate_value
-
