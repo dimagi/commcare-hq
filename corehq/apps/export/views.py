24b10a0d933b634f5a5832247ed1f9c190e6e026
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-13 18:24:43 -0400

    breadcrumbs and navigation

--- corehq/apps/export/views.py
+++ corehq/apps/export/views.py
@@ -353,6 +353,13 @@ class CreateFormExportView(BaseProjectDataView):
         }
 
     @property
+    def breadcrumbs(self):
+        return [{
+            'link': FormExportInterface.get_url(domain=self.domain),
+            'title': ugettext_lazy("Form Exports")
+        }]
+
+    @property
     def module_to_form_options(self):
         return {
             module.unique_id: [{
@@ -362,6 +369,13 @@ class CreateFormExportView(BaseProjectDataView):
             for app in get_apps_in_domain(self.domain)
             for module in app.modules
         }
+
+    @property
+    def parent_pages(self):
+        return [{
+            'link': FormExportInterface.get_url(domain=self.domain),
+            'title': ugettext_lazy("Form Exports")
+        }]
 
 
 class CreateCaseExportView(BaseProjectDataView):
