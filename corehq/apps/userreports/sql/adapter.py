f4b895126e734ea27c29c4c390d88da93c19335f
Cory Zue <czue@dimagi.com>
2015-07-16 14:01:34 +0200

    remove no-longer-used engine param

--- corehq/apps/userreports/sql/adapter.py
+++ corehq/apps/userreports/sql/adapter.py
@@ -13,9 +13,9 @@ metadata = sqlalchemy.MetaData()
 
 class IndicatorSqlAdapter(object):
 
-    def __init__(self, config, engine=None):
+    def __init__(self, config):
         self.config = config
-        self.engine = engine or connection_manager.get_engine(get_engine_id(config))
+        self.engine = connection_manager.get_engine(get_engine_id(config))
 
     @memoized
     def get_table(self):
