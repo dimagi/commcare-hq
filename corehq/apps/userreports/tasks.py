7f2fbade914db04d0e8d73296d0da8d491ede22f
Cory Zue <czue@dimagi.com>
2015-07-16 12:01:18 +0200

    [ci skip] unused import

--- corehq/apps/userreports/tasks.py
+++ corehq/apps/userreports/tasks.py
@@ -6,7 +6,7 @@ from casexml.apps.case.models import CommCareCase
 from corehq.apps.domain.utils import get_doc_ids
 from corehq.apps.userreports.models import DataSourceConfiguration, CustomDataSourceConfiguration
 from corehq.apps.userreports.sql import IndicatorSqlAdapter
-from corehq.db import connection_manager, create_engine
+from corehq.db import create_engine
 from couchforms.models import XFormInstance
 from dimagi.utils.couch.database import iter_docs
 
