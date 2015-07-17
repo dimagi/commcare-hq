91f663152f61d24b11082944fbcb395939deba3f
Norman Hooper <nhooper@dimagi.com>
2015-07-17 15:29:37 +0200

    "commcare-user" is a reserved case type
    Don't allow users to set a module case type to "commcare-user"

--- corehq/apps/app_manager/util.py
+++ corehq/apps/app_manager/util.py
@@ -91,8 +91,11 @@ def is_valid_case_type(case_type):
     False
     >>> is_valid_case_type(None)
     False
+    >>> is_valid_case_type('commcare-user')
+    False
+
     """
-    return bool(_case_type_regex.match(case_type or ''))
+    return bool(_case_type_regex.match(case_type or '')) and case_type != USERCASE_TYPE
 
 
 class ParentCasePropertyBuilder(object):
