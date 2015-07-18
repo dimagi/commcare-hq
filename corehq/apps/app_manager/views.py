37b56a6a3262c3db2141269887bc4a61bc1893fa
Jenny Schweers <jschweers@dimagi.com>
2015-07-17 20:14:11 -0400

    redirect when displaying copied app

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -359,8 +359,8 @@ def fast_import_app(request, domain, app_id):
     try:
         app.get_module(module_id).get_form(form_id)
     except (ModuleNotFoundException, FormNotFoundException):
-        return back_to_main(request, domain, app_id=app._id)
-    return form_designer(request, domain, app._id, module_id, form_id)
+        return HttpResponseRedirect(reverse('view_app', args=[domain, app._id]))
+    return HttpResponseRedirect(reverse('form_source', args=[domain, app._id, module_id, form_id]))
 
 
 @require_can_edit_apps
