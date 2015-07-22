081bd7db6c28b2287a5bfc1a3a250109ded24198
Sravan Reddy <sravfeyn@gmail.com>
2015-07-22 13:44:23 +0200

    refactor a test

--- corehq/apps/receiverwrapper/tests/test_app_id.py
+++ corehq/apps/receiverwrapper/tests/test_app_id.py
@@ -8,35 +8,44 @@ from couchforms.util import spoof_submission
 
 
 class TestAppId(TestCase):
+    form_instances = []
+
+    @classmethod
+    def setUpClass(cls):
+        cls.domain = 'alskdjfablasdkffsdlkfjabas'
+        cls.project = create_domain(name=cls.domain)
+        cls.app = Application(domain=cls.domain, version=4)
+        cls.app.save()
+        cls.app_id = cls.app.get_id
+        cls.build = Application(domain=cls.domain, version=3)
+        cls.build.copy_of = cls.app_id
+        cls.build.save()
+        cls.build_id = cls.build.get_id
+
+    @classmethod
+    def tearDownClass(cls):
+        for form in cls.form_instances:
+            form.delete()
+        cls.project.delete()
+
     def test(self):
-        self.domain = 'alskdjfablasdkffsdlkfjabas'
-        project = create_domain(name=self.domain)
-        app = Application(domain=self.domain, version=4)
-        app.save()
-        app_id = app.get_id
-        build = Application(domain=self.domain, version=3)
-        build.copy_of = app_id
-        build.save()
-        build_id = build.get_id
         cache.clear()
-        try:
-            self._test(build_id, app_id, build_id)
-            self._test(app_id, app_id, None)
-            self._test('alskdjflaksdjf', 'alskdjflaksdjf', None)
-            app.delete_app()
-            # does this work just as well for a deleted app?
-            cache.clear()
-            self._test(build_id, app_id, build_id)
-            build.delete_app()
-            cache.clear()
-            form = self._test(build_id, app_id, build_id)
-            self._test_app_version(self.domain, form, build)
-            # (test cache hit)
-            self._test_app_version(self.domain, form, build)
-        finally:
-            project.delete()
+        self._test(self.build_id, self.app_id, self.build_id)
+        self._test(self.app_id, self.app_id, None)
+        self._test('alskdjflaksdjf', 'alskdjflaksdjf', None)
+        self.app.delete_app()
+        # does this work just as well for a deleted app?
+        cache.clear()
+        self._test(self.build_id, self.app_id, self.build_id)
+        self.build.delete_app()
+        cache.clear()
+        form = self._test(self.build_id, self.app_id, self.build_id)
+        self._test_app_version(self.domain, form, self.build)
+        # (test cache hit)
+        self._test_app_version(self.domain, form, self.build)
 
     def _test(self, id, expected_app_id, expected_build_id):
+        # this should be renamed to reflect what it is testing
         r = spoof_submission(
             '/a/{domain}/receiver/{id}/'.format(domain=self.domain, id=id),
             '<data xmlns="http://example.com/"><question1/></data>',
@@ -46,6 +55,7 @@ class TestAppId(TestCase):
         form = XFormInstance.get(form_id)
         self.assertEqual(form.app_id, expected_app_id)
         self.assertEqual(form.build_id, expected_build_id)
+        self.form_instances.append(form)
         return form
 
     def _test_app_version(self, domain, form, build):
