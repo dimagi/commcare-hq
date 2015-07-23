e7ae296fa5262e6acf9ba4bb297e53e34c403c31
Sravan Reddy <sravfeyn@gmail.com>
2015-07-23 11:55:57 +0200

    replace hardcoded val with const

--- corehq/apps/data_analytics/tests/test_malt_generator.py
+++ corehq/apps/data_analytics/tests/test_malt_generator.py
@@ -1,7 +1,7 @@
 import datetime
 from django.test import TestCase
 
-from corehq.apps.app_manager.const import APP_V2
+from corehq.apps.app_manager.const import APP_V2, AMPLIFIES_YES
 from corehq.apps.app_manager.models import Application
 from corehq.apps.data_analytics.malt_generator import MALTTableGenerator
 from corehq.apps.data_analytics.models import MALTRow
@@ -49,7 +49,7 @@ class MaltGeneratorTest(TestCase):
     def _setup_apps(cls):
         cls.app = Application.new_app(cls.DOMAIN_NAME, "app 1", APP_V2)
         cls.wam_app = Application.new_app(cls.DOMAIN_NAME, "app 2", APP_V2)
-        cls.wam_app.amplifies_workers = 'yes'
+        cls.wam_app.amplifies_workers = AMPLIFIES_YES
         cls.app.save()
         cls.wam_app.save()
         cls.app_id = cls.app._id
