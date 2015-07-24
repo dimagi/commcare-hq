6bf3f1878e36d7ec354b307a06ba3c8d7845590e
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-24 13:53:12 -0400

    validate field update submissions

--- corehq/apps/fixtures/views.py
+++ corehq/apps/fixtures/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponseBadRequest, HttpResponseRedirect, Http404, H
 from django.http.response import HttpResponseServerError
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
+from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _, ugettext_noop
 from django.views.decorators.http import require_POST
 from django.views.generic.base import TemplateView
@@ -27,6 +28,7 @@ from corehq.apps.fixtures.exceptions import (
 from corehq.apps.fixtures.models import FixtureDataType, FixtureDataItem, FieldList, FixtureTypeField
 from corehq.apps.fixtures.upload import run_upload, validate_file_format, get_workbook
 from corehq.apps.fixtures.fixturegenerators import item_lists_by_domain
+from corehq.apps.fixtures.utils import is_field_name_invalid
 from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
 from corehq.apps.reports.util import format_datatables_data
 from corehq.apps.users.models import Permissions
@@ -105,6 +107,30 @@ def update_tables(request, domain, data_type_id, test_patch=None):
 
     if request.method == 'POST' or request.method == "PUT":
         fields_update = test_patch or _to_kwargs(request)
+
+        # validate fields
+        validation_errors = []
+        for field_name, options in fields_update['fields'].items():
+            method = options.keys()
+            if 'update' in method:
+                field_name = options['update']
+            if field_name.startswith('xml') and not 'remove' in method:
+                validation_errors.append(
+                    _("Field name \"%s\" cannot begin with 'xml'.") % field_name
+                )
+            if is_field_name_invalid(field_name) and not 'remove' in method:
+                validation_errors.append(
+                    _("Field name \"%s\" cannot include /, "
+                      "\\, <, >, or spaces.") % field_name
+                )
+        if validation_errors:
+            return json_response({
+                'validation_errors': validation_errors,
+                'error_msg': _(
+                    "Could not update table because field names were not "
+                    "correctly formatted"),
+            })
+
         fields_patches = fields_update["fields"]
         data_tag = fields_update["tag"]
         is_global = fields_update["is_global"]
