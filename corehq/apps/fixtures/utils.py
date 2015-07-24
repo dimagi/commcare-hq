9cb2f24a17d8036d0d31c894c02a1285dcbd8c38
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-24 12:16:04 -0400

    validate field name

--- corehq/apps/fixtures/utils.py
+++ corehq/apps/fixtures/utils.py
@@ -1,10 +1,15 @@
 import re
 
+BAD_SLUG_PATTERN = '([/\\<>\s])'
+
 
 def clean_fixture_field_name(field_name):
     """Effectively slugifies a fixture's field name so that we don't send
     bad XML back from the phone. Ideally, the fixture name should be
     verified as a good slug before using it.
     """
-    pattern = '([/\\<> ])'
-    return re.sub(pattern, '_', field_name)
+    return re.sub(BAD_SLUG_PATTERN, '_', field_name)
+
+
+def is_field_name_invalid(field_name):
+    return bool(re.search(BAD_SLUG_PATTERN, field_name))
