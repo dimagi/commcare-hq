ed9604a80703c98e669cd25cae2a609de43fa26b
Biyeun Buczyk <biyeun@dimagi.com>
2015-07-27 14:29:48 -0400

    trap deleted xforms error and retry in 5 min to avoid race condition

--- corehq/apps/users/tasks.py
+++ corehq/apps/users/tasks.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from celery.exceptions import MaxRetriesExceededError
 from celery.schedules import crontab
 from celery.task import task
 from celery.task.base import periodic_task
+from celery.utils.log import get_task_logger
 from corehq.util.log import SensitiveErrorMail
+from couchforms.exceptions import UnexpectedDeletedXForm
 import settings
 from corehq.apps.domain.models import Domain
 from dimagi.utils.couch.undo import DELETED_SUFFIX
@@ -12,6 +15,9 @@ from soil import CachedDownload, DownloadBase
 
 from casexml.apps.case.xform import get_case_ids_from_form
 from couchforms.models import XFormInstance
+
+logger = get_task_logger(__name__)
+
 
 @task(ErrorMail=SensitiveErrorMail)
 def bulk_upload_async(domain, user_specs, group_specs, location_specs):
@@ -38,8 +44,16 @@ def tag_docs_as_deleted(cls, docs, deletion_id):
     cls.get_db().bulk_save(docs)
 
 
-@task(rate_limit=2, queue='background_queue', ignore_result=True)  # 2 saves/sec for cloudant slowness
-def tag_forms_as_deleted_rebuild_associated_cases(formlist, deletion_id, deleted_cases=None):
+@task(bind=True, rate_limit=2, queue='background_queue', ignore_result=True,
+      default_retry_delay=5 * 60, max_retries=3)
+def tag_forms_as_deleted_rebuild_associated_cases(self, formlist, deletion_id,
+                                                  deleted_cases=None):
+    """ Upon user deletion, mark associated forms as deleted and prep cases
+    for a rebuild.
+    - 2 saves/sec for cloudant slowness (rate_limit)
+    - retry in 5 min if failure occurs after (default_retry_delay)
+    - retry a total of 3 times
+    """
     from casexml.apps.case.cleanup import rebuild_case
     if deleted_cases is None:
         deleted_cases = set()
@@ -51,8 +65,16 @@ def tag_forms_as_deleted_rebuild_associated_cases(formlist, deletion_id, deleted
         cases_to_rebuild.update(get_case_ids_from_form(form))
     XFormInstance.get_db().bulk_save(formlist)
 
-    for case in cases_to_rebuild - deleted_cases:
-        rebuild_case(case)
+    try:
+        for case in cases_to_rebuild - deleted_cases:
+            rebuild_case(case)
+    except UnexpectedDeletedXForm as exc:
+        try:
+            self.retry(exc=exc)
+        except MaxRetriesExceededError:
+            logger.error("Maximum Retries Exceeded for tag_forms_as_deleted. "
+                         "Deleted XForms found preventing cases from being "
+                         "rebuilt.")
 
 @periodic_task(
     run_every=crontab(hour=23, minute=55),
