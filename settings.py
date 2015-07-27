1c17f62e43367b8844a8d0d3ce1e572ff6ff4ad3
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2015-07-27 16:20:58 +0200

    WIP CustomDataSource

--- settings.py
+++ settings.py
@@ -1294,6 +1294,7 @@ CUSTOM_DATA_SOURCES = [
     os.path.join('custom', 'up_nrhm', 'data_sources', 'location_hierarchy.json'),
     os.path.join('custom', 'up_nrhm', 'data_sources', 'asha_facilitators.json'),
     os.path.join('custom', 'succeed', 'data_sources', 'submissions.json'),
+    os.path.join('custom', 'succeed', 'data_sources', 'tasks.json'),
     os.path.join('custom', 'apps', 'gsid', 'data_sources', 'patient_summary.json'),
     os.path.join('custom', 'abt', 'reports', 'data_sources', 'sms.json'),
     os.path.join('custom', 'abt', 'reports', 'data_sources', 'supervisory.json'),
