707c550c7e2f251c9ed1766db4438ff19054d1e2
Ben Rudolph <brudolph@dimagi.com>
2015-07-27 14:25:55 +0530

    Documentation to decorator

--- corehq/apps/dropbox/decorators.py
+++ corehq/apps/dropbox/decorators.py
@@ -7,6 +7,14 @@ from .views import DROPBOX_ACCESS_TOKEN
 
 
 def require_dropbox_session(next_url=None):
+    """
+    Any view that is wrapped with this will redirect to the dropbox authorization site if the user does not
+    have an access_token in their session.
+
+    @params
+    next_url - The next_url to go to after redirect. Can be a function or string. If it's a function it will
+    receive the same parameters as the view
+    """
     def decorator(view_func):
         @wraps(view_func)
         def inner(request, *args, **kwargs):
@@ -19,7 +27,6 @@ def require_dropbox_session(next_url=None):
                 request.session['dropbox_next_url'] = url
                 authorize_url = get_dropbox_auth_flow(request.session).start()
                 return HttpResponseRedirect(authorize_url)
-
             return view_func(request, *args, **kwargs)
         return inner
     return decorator
