dfa5fdaa0ac307c968bcb9bd25636fbbb9ac98b4
Ethan Soergel <esoergel@gmail.com>
2015-07-30 10:44:39 -0400

    Add `domain` to users_by_location_id

--- corehq/apps/locations/_design/views/users_by_location_id/map.js
+++ corehq/apps/locations/_design/views/users_by_location_id/map.js
@@ -2,13 +2,18 @@ function(doc) {
     if (doc.base_doc === "CouchUser") {
 
         if (doc.doc_type === "CommCareUser" && doc.location_id) {
-            emit([doc.location_id, doc.doc_type, doc._id], null);
+            emit([doc.domain, doc.location_id, doc.doc_type, doc._id], null);
 
         } else if (doc.doc_type === "WebUser") {
-            for (i = 0; i < doc.domain_memberships.length; i += 1) {
+            for (var i = 0; i < doc.domain_memberships.length; i += 1) {
                 var domain_membership = doc.domain_memberships[i];
                 if (domain_membership.location_id) {
-                    emit([domain_membership.location_id, doc.doc_type, doc._id], null);
+                    emit([
+                        domain_membership.domain,
+                        domain_membership.location_id,
+                        doc.doc_type,
+                        doc._id
+                    ], null);
                 }
             }
         }
