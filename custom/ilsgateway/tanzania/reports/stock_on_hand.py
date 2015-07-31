f9944f6e4cc2e573ad94eb15f1a749eae6ee54f1
kkrampa <kkrampa@soldevelo.com>
2015-07-31 13:15:38 +0200

    Make sure that denominator isn't equal to 0

--- custom/ilsgateway/tanzania/reports/stock_on_hand.py
+++ custom/ilsgateway/tanzania/reports/stock_on_hand.py
@@ -230,7 +230,7 @@ class SohPercentageTableData(ILSData):
             )
             for product_id in products_ids:
                 product_availability = product_availabilities.get((sql_location.location_id, product_id))
-                if product_availability:
+                if product_availability and product_availability[1] != 0:
                     row_data.append(
                         format_percent(
                             product_availability[0] * 100 / float(product_availability[1])
