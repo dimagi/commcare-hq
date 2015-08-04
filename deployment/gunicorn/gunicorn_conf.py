1e0a771d94d6510b25fbce5e0655bd4b20c8ce00
Daniel Roberts <droberts@dimagi.com>
2015-08-04 15:10:08 -0400

    kill all references to hqproxy0

--- deployment/gunicorn/gunicorn_conf.py
+++ deployment/gunicorn/gunicorn_conf.py
@@ -7,11 +7,7 @@ timeout = 900
 max_requests = 240
 # defaults to 30 sec, setting to 5 minutes to fight `GreenletExit`s
 graceful_timeout = 5*60
-# cryptically, setting forwarded_allow_ips (to the ip of the hqproxy0)
-# gets gunicorn to set https on redirects when appropriate. See:
-# http://docs.gunicorn.org/en/latest/configure.html#secure-scheme-headers
-# http://docs.gunicorn.org/en/latest/configure.html#forwarded-allow-ips
-forwarded_allow_ips = '10.176.162.109'
+
 
 def post_fork(server, worker):
     # hacky way to address gunicorn gevent requests hitting django too early before urls are loaded
