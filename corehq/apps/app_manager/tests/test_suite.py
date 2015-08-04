4868e000d7bd56709fca2ae807a721be7f871efd
Norman Hooper <nhooper@dimagi.com>
2015-08-04 12:10:19 +0200

    Updated app_manager tests to use ParentIndex

--- corehq/apps/app_manager/tests/test_suite.py
+++ corehq/apps/app_manager/tests/test_suite.py
@@ -8,7 +8,8 @@ from corehq.apps.app_manager.models import (
     AUTO_SELECT_RAW, WORKFLOW_MODULE, DetailColumn, ScheduleVisit, FormSchedule, Module, AdvancedModule,
     WORKFLOW_ROOT, AdvancedOpenCaseAction, SortElement, PreloadAction, MappingItem, OpenCaseAction,
     OpenSubCaseAction, FormActionCondition, UpdateCaseAction, WORKFLOW_FORM, FormLink, AUTO_SELECT_USERCASE,
-    ReportModule, ReportAppConfig, ParentSelect)
+    ReportModule, ReportAppConfig, ParentSelect, ParentIndex
+)
 from corehq.apps.app_manager.tests.util import TestFileMixin, commtrack_enabled
 from corehq.apps.app_manager.xpath import (dot_interpolate, UserCaseXPath,
                                            interpolate_xpath, session_var)
@@ -1042,7 +1043,7 @@ class AdvancedModuleAsChildTest(ModuleAsChildTestBase, SimpleTestCase):
     def _load_case(self, child_module_form, case_type, parent_module=None):
         action = LoadUpdateAction(case_tag=case_type, case_type=case_type)
         if parent_module:
-            action.parent_tag = parent_module.case_type
+            action.parents = [ParentIndex(tag=parent_module.case_type)]
 
         child_module_form.actions.load_update_cases.append(action)
 
@@ -1307,7 +1308,7 @@ class TestFormLinking(SimpleTestCase, TestFileMixin):
                             form.actions.load_update_cases.append(LoadUpdateAction(
                                 case_type=case_type,
                                 case_tag='update_{}'.format(case_type),
-                                parent_tag=parent,
+                                parents=[ParentIndex(tag=parent)] if parent else [],
                             ))
                     elif 'open_subacse':
                         if m_type == "basic":
@@ -1321,7 +1322,7 @@ class TestFormLinking(SimpleTestCase, TestFileMixin):
                                 case_type=case_type,
                                 case_tag='subcase_{}'.format(case_type),
                                 name_path='/data/name',
-                                parent_tag=parent
+                                parents=[ParentIndex(tag=parent)] if parent else []
                             ))
 
         return app
