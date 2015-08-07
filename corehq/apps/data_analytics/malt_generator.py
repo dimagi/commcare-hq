45887f792684fa2b813d3ddfa1ca82a36a229a1b
Sravan Reddy <sravfeyn@gmail.com>
2015-08-07 21:55:35 +0200

    include demo, unknown, admin users

--- corehq/apps/data_analytics/malt_generator.py
+++ corehq/apps/data_analytics/malt_generator.py
@@ -31,36 +31,44 @@ class MALTTableGenerator(object):
         for domain in Domain.get_all():
             malt_rows_to_save = []
             logger.info("Building MALT for {}".format(domain.name))
-            for user in domain.all_users():
-                for monthspan in self.monthspan_list:
-                    try:
-                        malt_rows_to_save.extend(self._get_malt_row_dicts(user, domain.name, monthspan))
-                    except Exception as ex:
-                        logger.error("Failed to get rows for user {id}. Exception is {ex}".format
-                                     (id=user._id, ex=str(ex)), exc_info=True)
+            all_users_by_id = {user._id: user for user in domain.all_users()}
+            for monthspan in self.monthspan_list:
+                try:
+                    malt_rows_to_save.extend(self._get_malt_row_dicts(domain.name, monthspan, all_users_by_id))
+                except Exception as ex:
+                    logger.error("Failed to get rows for domain {name}. Exception is {ex}".format
+                                 (name=domain.name, ex=str(ex)), exc_info=True)
             self._save_to_db(malt_rows_to_save, domain._id)
 
-    def _get_malt_row_dicts(self, user, domain_name, monthspan):
+    def _get_malt_row_dicts(self, domain_name, monthspan, all_users_by_id):
         malt_row_dicts = []
-        forms_query = self._get_forms_queryset(user._id, domain_name, monthspan)
-        apps_submitted_for = forms_query.values('app_id').annotate(num_of_forms=Count('instance_id'))
+        forms_query = self._get_forms_queryset(domain_name, monthspan)
+        apps_submitted_for = forms_query.values('app_id', 'user_id', 'username').annotate(
+            num_of_forms=Count('instance_id')
+        )
 
         for app_row_dict in apps_submitted_for:
             app_id = app_row_dict['app_id']
             num_of_forms = app_row_dict['num_of_forms']
+            user_id, username, user_type, email = self._user_data(
+                app_row_dict['user_id'],
+                app_row_dict['username'],
+                all_users_by_id
+            )
+
             try:
                 wam, pam, is_app_deleted = self._app_data(domain_name, app_id)
             except Exception as ex:
                 logger.error("Failed to get rows for user {id}, app {app_id}. Exception is {ex}".format
-                             (id=user._id, app_id=app_id, ex=str(ex)), exc_info=True)
+                             (id=user_id, app_id=app_id, ex=str(ex)), exc_info=True)
                 continue
 
             malt_dict = {
                 'month': monthspan.startdate,
-                'user_id': user._id,
-                'username': user.username,
-                'email': user.email,
-                'user_type': user.doc_type,
+                'user_id': user_id,
+                'username': username,
+                'email': email,
+                'user_type': user_type,
                 'domain_name': domain_name,
                 'num_of_forms': num_of_forms,
                 'app_id': app_id,
@@ -111,14 +119,13 @@ class MALTTableGenerator(object):
                 str(ex)
             ), exc_info=True)
 
-    def _get_forms_queryset(self, user_id, domain_name, monthspan):
+    def _get_forms_queryset(self, domain_name, monthspan):
         start_date = monthspan.computed_startdate
         end_date = monthspan.computed_enddate
 
         return FormData.objects.exclude(
             device_id=COMMCONNECT_DEVICE_ID,
         ).filter(
-            user_id=user_id,
             domain=domain_name,
             received_on__range=(start_date, end_date)
         )
@@ -135,3 +142,15 @@ class MALTTableGenerator(object):
         return (getattr(app, 'amplifies_workers', AMPLIFIES_NOT_SET),
                 getattr(app, 'amplifies_project', AMPLIFIES_NOT_SET),
                 app.is_deleted())
+
+    @classmethod
+    def _user_data(cls, user_id, username, all_users_by_id):
+        if user_id in all_users_by_id:
+            user = all_users_by_id[user_id]
+            return (user._id, user.username, user.doc_type, user.email)
+        elif user_id == 'demo_user':
+            return (user_id, username, 'DemoUser', None)
+        elif username == 'admin':
+            return (user_id, username, 'AdminUser', None)
+        else:
+            return (user_id, username, 'UnknownUser', None)
