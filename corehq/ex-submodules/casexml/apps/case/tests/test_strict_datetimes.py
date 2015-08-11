7cf35ee6807969cdaab4b858e8f70009a9979336
Daniel Roberts <droberts@dimagi.com>
2015-08-11 14:29:13 -0400

    add case.StrictDatetimesTest that we expect to fail
    for bad @date_modified values

---
+++ corehq/ex-submodules/casexml/apps/case/tests/test_strict_datetimes.py
@@ -0,0 +1,39 @@
+from unittest import expectedFailure
+from xml.etree import ElementTree
+from django.test import TestCase
+from casexml.apps.case.util import make_form_from_case_blocks
+from casexml.apps.case.xform import process_cases_with_casedb, CaseDbCache
+from couchforms.exceptions import PhoneDateValueError
+from couchforms.util import process_xform
+
+
+CASE_ID = 'a0cd5e6c5fb84695a4f729d3b1996a93'
+# the bad date in date_modified is what we're interested in
+CASE_BLOCK = """
+<n0:case case_id="{case_id}"
+         date_modified="2015-08-06T14:33:12.077 05:30"
+         user_id="d37bf37ee0ff4ec4a8aa114baa046a25"
+         xmlns:n0="http://commcarehq.org/case/transaction/v2">
+    <n0:create>
+        <n0:case_name>Forple Snord</n0:case_name>
+        <n0:owner_id>d37bf37ee0ff4ec4a8aa114baa046a25</n0:owner_id>
+        <n0:case_type>footron</n0:case_type>
+    </n0:create>
+</n0:case>
+""".format(case_id=CASE_ID)
+
+
+class StrictDatetimesTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.domain = 'strict-datetimes-test-domain'
+
+    @expectedFailure
+    def test(self):
+        form = make_form_from_case_blocks([ElementTree.fromstring(CASE_BLOCK)])
+        lock_manager = process_xform(form, domain=self.domain)
+        with lock_manager as xforms:
+            with CaseDbCache(domain=self.domain) as case_db:
+                with self.assertRaises(PhoneDateValueError):
+                    process_cases_with_casedb(xforms, case_db)
