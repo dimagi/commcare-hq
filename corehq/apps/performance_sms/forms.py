4e9a7e3b80b5da7a13dcb0378a52f45b4dcd7709
Cory Zue <czue@dimagi.com>
2015-08-13 12:00:05 +0200

    cleanup/pare down the UI to make a new config

--- corehq/apps/performance_sms/forms.py
+++ corehq/apps/performance_sms/forms.py
@@ -1,9 +1,7 @@
-from corehq.apps.hqwebapp.crispy import FieldWithHelpBubble
-from crispy_forms import layout as crispy
+from corehq.apps.groups.fields import GroupField
 from django import forms
 from django.utils.translation import ugettext as _
 from corehq.apps.app_manager.fields import ApplicationDataSourceUIHelper
-from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
 
 
@@ -12,14 +10,11 @@ class PerformanceMessageEditForm(forms.Form):
     schedule = forms.CharField()  # todo
     template = forms.CharField()
 
-
     def __init__(self, domain, *args, **kwargs):
         super(PerformanceMessageEditForm, self).__init__(*args, **kwargs)
         self.domain = domain
 
-        self.helper = FormHelper()
-        self.helper.form_class = "form-horizontal"
-        self.helper.form_id = "performance-form"
+        self.fields['recipient_id'] = GroupField(domain=domain, label=_('Recipient Group'))
 
         self.app_source_helper = ApplicationDataSourceUIHelper(enable_cases=False)
         self.app_source_helper.bootstrap(self.domain)
@@ -30,26 +25,7 @@ class PerformanceMessageEditForm(forms.Form):
             "source": _("Choose form you want to count in the message template."),
         }
         self.fields.update(data_source_fields)
-        data_source_crispy_fields = []
-        for k in data_source_fields.keys():
-            if k in data_source_help_texts:
-                data_source_crispy_fields.append(FieldWithHelpBubble(
-                    k, help_bubble_text=data_source_help_texts[k]
-                ))
-            else:
-                data_source_crispy_fields.append(k)
 
-
-        self.helper.layout = crispy.Layout(
-            crispy.Fieldset(
-                _('Data Source'), *data_source_crispy_fields
-            ),
-            FormActions(
-                crispy.ButtonHolder(
-                    crispy.Submit(
-                        'create_new_report_builder_btn',
-                        _('Next'),
-                    )
-                ),
-            ),
-        )
+        self.helper = FormHelper()
+        self.helper.form_class = "form-horizontal"
+        self.helper.form_id = "performance-form"
