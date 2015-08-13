dca61a1e0ea06b14e4ce694f7179d5af85ac3736
Sravan Reddy <sravfeyn@gmail.com>
2015-08-13 14:52:59 +0200

    refactor

--- corehq/apps/performance_sms/utils.py
+++ corehq/apps/performance_sms/utils.py
@@ -1,28 +1,24 @@
 from datetime import datetime
 from . import dbaccessors
-from .models import DAILY, WEEKLY, MONTHLY
+from .models import DAILY, WEEKLY, MONTHLY, DEFAULT_HOUR, DEFAULT_WEEK_DAY, DEFAULT_MONTH_DAY
 
 
 def get_message_configs_at_this_hour(as_of=None):
-
     now = as_of or datetime.utcnow()
 
-    def _keys(period, now):
-        if period == DAILY:
-            yield {
-                'key': [period, now.hour],
-            }
-        elif period == WEEKLY:
-            yield {
-                'key': [period, 1, now.weekday()],
-            }
-        else:
-            # monthly
-            yield {
-                'key': [period, 1, 1, now.day]
-            }
+    def _get_daily_messages():
+        return dbaccessors.by_interval({
+            'key': [DAILY, now.hour, DEFAULT_WEEK_DAY, DEFAULT_MONTH_DAY]
+        })
 
-    for period in (DAILY, WEEKLY, MONTHLY):
-        for keys in _keys(period, now):
-            for config in dbaccessors.by_interval(keys):
-                yield config
+    def _get_weekly_messages():
+        return dbaccessors.by_interval({
+            'key': [WEEKLY, DEFAULT_HOUR, now.weekday(), DEFAULT_MONTH_DAY]
+        })
+
+    def _get_monthly_messages():
+        return dbaccessors.by_interval({
+            'key': [MONTHLY, DEFAULT_HOUR, DEFAULT_WEEK_DAY, now.day]
+        })
+
+    return _get_daily_messages() + _get_weekly_messages() + _get_monthly_messages()
