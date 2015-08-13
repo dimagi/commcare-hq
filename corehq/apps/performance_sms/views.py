b09c3b321eb0ecf411925c8ba1d3b6f702366c4c
Cory Zue <czue@dimagi.com>
2015-08-13 10:46:00 +0200

    actually wire up add button and fix template

--- corehq/apps/performance_sms/views.py
+++ corehq/apps/performance_sms/views.py
@@ -16,9 +16,8 @@ def list_performance_configs(request, domain):
 @reminders_framework_permission
 @toggles.SMS_PERFORMANCE_FEEDBACK.required_decorator()
 def add_performance_config(request, domain):
-    return render(request, "performance_sms/list_performance_configs.html", {
+    return render(request, "performance_sms/add_performance_config.html", {
         'domain': domain,
-        'performance_configs': dbaccessors.by_domain(domain)
     })
 
 
