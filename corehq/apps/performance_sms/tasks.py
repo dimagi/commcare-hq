b58676507924d02c2cb910d28c743a0163a90f3b
Sravan Reddy <sravfeyn@gmail.com>
2015-08-13 14:38:03 +0200

    rename

--- corehq/apps/performance_sms/tasks.py
+++ corehq/apps/performance_sms/tasks.py
@@ -5,6 +5,6 @@ import settings
 
 
 @periodic_task(run_every=crontab(hour="*/1", minute="0"), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
-def daily_reports():
+def fire_performence_sms():
     for config in get_message_configs_at_this_hour():
         config.fire_messages()
