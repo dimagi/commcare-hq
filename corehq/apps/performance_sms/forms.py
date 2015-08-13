b922fa06f73920613d8a0675a4b61c66953da3e5
Cory Zue <czue@dimagi.com>
2015-08-13 12:04:34 +0200

    more cleanup

--- corehq/apps/performance_sms/forms.py
+++ corehq/apps/performance_sms/forms.py
@@ -3,12 +3,13 @@ from django import forms
 from django.utils.translation import ugettext as _
 from corehq.apps.app_manager.fields import ApplicationDataSourceUIHelper
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class PerformanceMessageEditForm(forms.Form):
     recipient_id = forms.CharField()
     schedule = forms.CharField()  # todo
-    template = forms.CharField()
+    template = forms.CharField(widget=forms.Textarea)
 
     def __init__(self, domain, *args, **kwargs):
         super(PerformanceMessageEditForm, self).__init__(*args, **kwargs)
@@ -19,13 +20,10 @@ class PerformanceMessageEditForm(forms.Form):
         self.app_source_helper = ApplicationDataSourceUIHelper(enable_cases=False)
         self.app_source_helper.bootstrap(self.domain)
         data_source_fields = self.app_source_helper.get_fields()
-        # todo: help texts?
-        data_source_help_texts = {
-            "application": _("Which application should the data come from?"),
-            "source": _("Choose form you want to count in the message template."),
-        }
         self.fields.update(data_source_fields)
 
         self.helper = FormHelper()
         self.helper.form_class = "form-horizontal"
         self.helper.form_id = "performance-form"
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', _('Save Changes')))
