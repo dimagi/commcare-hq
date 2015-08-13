5359c80665a9a92596271b5a146325fd06ac774a
Cory Zue <czue@dimagi.com>
2015-08-13 14:40:23 +0200

    alphabetize tests

--- corehq/apps/reports/tests/__init__.py
+++ corehq/apps/reports/tests/__init__.py
@@ -1,19 +1,19 @@
 try:
     from .test_case_export import *
     from .test_cache import *
-    from .test_export_api import *
-    from .test_sql_reports import *
-    from .test_form_export import *
-    from .test_report_api import *
     from .test_data_sources import *
-    from .test_readable_formdata import *
-    from .test_time_and_date_manipulations import *
+    from .test_export_api import *
+    from .test_form_export import *
     from .test_generic import *
-    from .test_util import *
     from .test_filters import *
-    from .test_pillows_xforms import *
     from .test_pillows_cases import *
+    from .test_pillows_xforms import *
+    from .test_readable_formdata import *
+    from .test_report_api import *
     from .test_scheduled_reports import *
+    from .test_sql_reports import *
+    from .test_time_and_date_manipulations import *
+    from .test_util import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
     # otherwise debugging is a pain
