c0b809734ab8fac329dabe251204f5651bf2f5f9
Cory Zue <czue@dimagi.com>
2015-08-17 12:39:23 +0200

    fix tests for reduce view

--- corehq/apps/performance_sms/dbaccessors.py
+++ corehq/apps/performance_sms/dbaccessors.py
@@ -2,8 +2,17 @@ from corehq.apps.performance_sms.models import PerformanceConfiguration
 
 
 def by_domain(domain):
-    return list(PerformanceConfiguration.view('performance_sms/by_domain', key=domain, include_docs=True))
+    return list(PerformanceConfiguration.view(
+        'performance_sms/by_domain',
+        key=domain,
+        reduce=False,
+        include_docs=True
+    ))
 
 
 def by_interval(interval_keys):
-    return list(PerformanceConfiguration.view('performance_sms/all_configs', key=interval_keys, include_docs=True))
+    return list(PerformanceConfiguration.view(
+        'performance_sms/all_configs',
+        key=interval_keys,
+        include_docs=True
+    ))
