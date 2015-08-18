956f36d83f1e4a5627c5c0a28f90d0b4b7f843e0
Jenny Schweers <jschweers@dimagi.com>
2015-08-18 09:40:23 -0400

    more debug code

--- corehq/apps/domain/decorators.py
+++ corehq/apps/domain/decorators.py
@@ -72,10 +72,13 @@ def login_and_domain_required(view_func):
                     ).format(domain=domain_name))
                     messages.info(req, msg)
                     return HttpResponseRedirect(reverse("domain_select"))
+                debug = ""
                 if hasattr(req, "couch_user"):
+                    debug = "req, and django's membership is %s" % CouchUser.from_django_user(user).is_member_of(domain)
                     couch_user = req.couch_user # set by user middleware
                 else:
                     # some views might not have this set
+                    debug = "django"
                     couch_user = CouchUser.from_django_user(user)
                 if couch_user.is_member_of(domain) or domain.is_public:
                     return view_func(req, domain_name, *args, **kwargs)
@@ -89,8 +92,8 @@ def login_and_domain_required(view_func):
                     from corehq.apps.users.views import DomainRequestView
                     return DomainRequestView.as_view()(req, *args, **kwargs)
                 else:
-                    raise Exception("404 in login_and_domain_required, user %s is not a member of %s" %
-                                    (couch_user.username, domain))
+                    raise Exception("404 in login_and_domain_required, user %s is not a member of %s, debug: %s" %
+                                    (couch_user.username, domain, debug))
             else:
                 login_url = reverse('domain_login', kwargs={'domain': domain})
                 return _redirect_for_login_or_domain(req, REDIRECT_FIELD_NAME, login_url)
