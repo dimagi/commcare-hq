16ee57bbbe73ab0f1104f997b1f7ea431f7b48e3
kkrampa <kkrampa@soldevelo.com>
2015-08-19 08:45:49 +0200

    Added message when sms is invalid

---
+++ custom/ewsghana/handlers/__init__.py
@@ -0,0 +1,4 @@
+from django.utils.translation import ugettext_lazy as _
+
+INVALID_MESSAGE = _('Sorry, I could not understand your message.'
+                  ' Please contact your DHIO for help, or visit http://www.ewsghana.com')
