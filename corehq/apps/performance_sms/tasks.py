4ae6d05ee796cf62d4438475295476733777d50c
Cory Zue <czue@dimagi.com>
2015-08-19 14:55:39 -0400

    [ci skip] lint

--- corehq/apps/performance_sms/tasks.py
+++ corehq/apps/performance_sms/tasks.py
@@ -5,7 +5,8 @@ from .utils import get_message_configs_at_this_hour
 import settings
 
 
-@periodic_task(run_every=crontab(hour="*/1", minute="0"), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
+@periodic_task(run_every=crontab(hour="*/1", minute="0"),
+               queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def fire_performence_sms():
     for config in get_message_configs_at_this_hour():
         send_messages_for_config(config)
