fa996b436facc4e986b4276864130c72418eaf85
Biyeun Buczyk <biyeun@dimagi.com>
2015-08-19 17:47:10 -0400

    handle error checking according to angular's current best practices via the ngMessages framework

--- corehq/apps/users/static/users/js/mobile_workers.ng.js
+++ corehq/apps/users/static/users/js/mobile_workers.ng.js
@@ -4,14 +4,66 @@
     var mobileWorkers = angular.module('hq.mobile_workers', [
         'ngResource',
         'ngRoute',
-        'ng.django.rmi'
+        'ng.django.rmi',
+        'ngMessages'
     ]);
+
+    var $formElements = {
+        password: function () {
+            return $('#id_password_2').parent();
+        },
+        username: function () {
+            return $('#id_username').parent();
+        }
+    };
+
+    var visualFormCtrl = {
+        passwordClear : function () {
+            $formElements.password()
+                .removeClass('has-error has-success');
+        },
+        passwordSuccess: function () {
+            $formElements.password()
+                .removeClass('has-error')
+                .addClass('has-success');
+        },
+        passwordError: function () {
+            $formElements.password()
+                .removeClass('has-success')
+                .addClass('has-error');
+        },
+        usernameClear: function () {
+            $formElements.username()
+                .removeClass('has-error has-pending has-success');
+        },
+        usernameSuccess: function () {
+            $formElements.username()
+                .removeClass('has-error has-pending')
+                .addClass('has-success');
+        },
+        usernamePending: function () {
+            $formElements.username()
+                .removeClass('has-error has-success')
+                .addClass('has-pending');
+        },
+        usernameError: function () {
+            $formElements.username()
+                .removeClass('has-success has-pending')
+                .addClass('has-error');
+        }
+    };
 
     var STATUS = {
         NEW: 'new',
         PENDING: 'pending',
         ERROR: 'error',
         SUCCESS: 'success'
+    };
+
+    var USERNAME_STATUS = {
+        PENDING: 'pending',
+        TAKEN: 'taken',
+        AVAILABLE: 'available'
     };
 
     mobileWorkers.constant('formStrings', {
@@ -23,19 +75,19 @@
         var self = this;
         self.creationStatus = STATUS.NEW;
 
-        self.fields = {
-            username: data.username || '',
-            password: data.password || '',
-            password2: data.password2 || '',
-        };
+        self.username = data.username || '';
+        self.password = data.password || '';
+        self.password2 = data.password2 || '';
 
         self.isPending = function () {
             return self.creationStatus === STATUS.PENDING;
         };
 
-        _.each(data.customFields, function (key) {
-            self.fields[key] = '';
-        });
+        if (_.isArray(data.customFields)) {
+            _.each(data.customFields, function (key) {
+                self[key] = '';
+            });
+        }
     };
 
     var mobileWorkerControllers = {};
@@ -44,73 +96,23 @@
             $scope, workerCreationService, djangoRMI, formStrings, customFields
     ) {
         $scope.mobileWorker = {};
-
-        var $usernameElement = $('#id_username').parent();
-        var $passwordElement = $('#id_password_2').parent();
-
-        var clearUsernameStatus = function () {
-            $scope.usernameAvailable = null;
-            $scope.usernameTaken = null;
-            $scope.usernamePending = null;
-            $usernameElement.removeClass("has-warning has-error has-success");
-        };
+        $scope.usernameAvailabilityStatus = null;
 
         $scope.initializeMobileWorker = function () {
-            clearUsernameStatus();
-            $passwordElement.removeClass("has-error has-success");
-            $scope.isPasswordValid = true;
+            visualFormCtrl.passwordClear();
+            visualFormCtrl.usernameClear();
+            $scope.usernameAvailabilityStatus = null;
+
+            // clears select 2 widget from old data
             $(".select2multiplechoicewidget").select2('data', null);
+
+            // initialize mobile worker model
             $scope.mobileWorker = new MobileWorker({customFields: customFields});
         };
 
         $scope.submitNewMobileWorker = function () {
             workerCreationService.stageNewMobileWorker($scope.mobileWorker);
             $("#newMobileWorkerModal").modal('hide');
-        };
-
-        $scope.checkUsername = function () {
-            clearUsernameStatus();
-            $scope.usernamePending = formStrings.checkingUsername;
-            $usernameElement.addClass("has-warning");
-            djangoRMI.check_username({
-                username: $scope.mobileWorker.fields.username
-            })
-            .success(function (data) {
-                clearUsernameStatus();
-                if (!!data.success) {
-                    $scope.usernameAvailable = data.success;
-                    $usernameElement.addClass("has-success");
-                } else {
-                    $usernameElement.addClass("has-error");
-                    $scope.usernameTaken = data.error;
-                }
-            })
-            .error(function (data) {
-                // TODO
-                clearUsernameStatus();
-                console.log(data);
-            });
-        };
-
-        $scope.checkPassword = function () {
-            if ($scope.mobileWorker.fields.password !== $scope.mobileWorker.fields.password2) {
-                $scope.isPasswordValid = false;
-                $passwordElement
-                    .removeClass("has-success")
-                    .addClass("has-error");
-            } else {
-                $scope.isPasswordValid = true;
-                $passwordElement
-                    .removeClass("has-error")
-                    .addClass("has-success");
-            }
-        };
-
-        $scope.validateForm = function () {
-            var areFieldsSet = _.every($scope.mobileWorker.fields, function (val, key) {
-                return !_.isEmpty(val);
-            });
-            return areFieldsSet && $scope.isPasswordValid && !!$scope.usernameAvailable;
         };
 
     };
@@ -158,7 +160,66 @@
         return self;
     };
 
-    mobileWorkers.service(mobileWorkerServices);
+    var mobileWorkerDirectives = {};
+    mobileWorkerDirectives.validateUsername = function ($http, $q, djangoRMI) {
+        return {
+            restrict: 'AE',
+            require: 'ngModel',
+            link: function ($scope, $elem, $attr, ctrl) {
+                ctrl.$validators.validateUsername = function (username) {
+                    var deferred = $q.defer();
+                    if (_.isUndefined(username) || _.isEmpty(username)) {
+                        $scope.usernameAvailabilityStatus = null;
+                        deferred.resolve();
+                        visualFormCtrl.usernameClear();
+                    } else {
+                        $scope.usernameAvailabilityStatus = USERNAME_STATUS.PENDING;
+                        visualFormCtrl.usernamePending();
+                        djangoRMI.check_username({
+                            username: username
+                        })
+                        .success(function (data) {
+                            if (!!data.success) {
+                                visualFormCtrl.usernameSuccess();
+                                $scope.usernameAvailabilityStatus = USERNAME_STATUS.AVAILABLE;
+                                deferred.resolve();
+                            } else {
+                                visualFormCtrl.usernameError();
+                                $scope.usernameAvailabilityStatus = USERNAME_STATUS.TAKEN;
+                                deferred.reject();
+                            }
+                        })
+                        .error(function (data) {
+                            deferred.reject();
+                        });
+                    }
+                    return deferred.promise;
+                }
+            }
+        };
+    };
+    mobileWorkerDirectives.confirmPassword = function ($q) {
+        return {
+            restrict: 'AE',
+            require: 'ngModel',
+            link: function ($scope, $attr, $elem, ctrl) {
+                ctrl.$validators.confirmPassword = function (modelValue) {
+                    var isConfirmed = $scope.mobileWorker.password === modelValue;
+                    if (_.isUndefined(modelValue) || _.isEmpty(modelValue)) {
+                        visualFormCtrl.passwordClear();
+                    } else if (isConfirmed) {
+                        visualFormCtrl.passwordSuccess();
+                    } else {
+                        visualFormCtrl.passwordError();
+                    }
+                    return isConfirmed;
+                };
+            }
+        };
+    };
 
+    mobileWorkers.directive(mobileWorkerDirectives);
+    mobileWorkers.service(mobileWorkerServices);
     mobileWorkers.controller(mobileWorkerControllers);
+    
 }(window.angular));
