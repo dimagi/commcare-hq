ea47d35977667a72b729792ab34d28e481d7a5b2
Ben Rudolph <brudolph@dimagi.com>
2015-08-20 17:25:41 -0400

    hsph hack

--- corehq/apps/cloudcare/views.py
+++ corehq/apps/cloudcare/views.py
@@ -266,11 +266,16 @@ def get_cases(request, domain):
     case_id = request.REQUEST.get("case_id", "")
     footprint = string_to_boolean(request.REQUEST.get("footprint", "false"))
 
-    hsph_case_id = request.REQUEST.get('hsph_hack', None)
-    if hsph_case_id:
-        case = CommCareCase.get(hsph_case_id)
-        cases = [case, case.parent, ]
-    import ipdb; ipdb.set_trace()
+    if toggles.HSPH_HACK.enabled(domain):
+        hsph_case_id = request.REQUEST.get('hsph_hack', None)
+        if hsph_case_id != 'None' and hsph_case_id:
+            case = CommCareCase.get(hsph_case_id)
+            usercase_id = CommCareUser.get_by_user_id(user_id).get_usercase_id()
+            usercase = CommCareCase.get(usercase_id) if usercase_id else None
+            return json_response(map(
+                lambda case: CaseAPIResult(id=case['_id'], couch_doc=case, id_only=ids_only),
+                filter(None, [case, case.parent, usercase])
+            ))
 
     if case_id and not footprint:
         # short circuit everything else and just return the case
