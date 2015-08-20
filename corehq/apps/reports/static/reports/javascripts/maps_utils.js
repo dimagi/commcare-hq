a64101e5c7de5a5e59be0c2a13849ecf59f642a4
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2015-08-20 08:53:02 +0200

    refactor

--- corehq/apps/reports/static/reports/javascripts/maps_utils.js
+++ corehq/apps/reports/static/reports/javascripts/maps_utils.js
@@ -403,7 +403,7 @@ function makeDisplayContext(metric, setActiveFeature) {
             // store visibility on the feature object so datatables can access it
             feature.visible = (feature._conf != null);
             if (!feature.visible) {
-                if(feature.$tr === void(0)){
+                if(feature.$tr){
                     return feature.visible;
                 }
                 feature.$tr.addClass('inactive-row');
