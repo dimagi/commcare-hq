50e1be780663b4abd4f2c29ced4db0cac9fc970b
Cory Zue <czue@dimagi.com>
2015-08-20 15:17:09 -0400

    fix spelling

--- corehq/apps/performance_sms/tasks.py
+++ corehq/apps/performance_sms/tasks.py
@@ -7,6 +7,6 @@ import settings
 
 @periodic_task(run_every=crontab(hour="*/1", minute="0"),
                queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
-def fire_performence_sms():
+def fire_performance_sms():
     for config in get_message_configs_at_this_hour():
         send_messages_for_config(config)
