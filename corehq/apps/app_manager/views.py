69141a66c6c2d9483446856d7f6150a23214ef86
Norman Hooper <nhooper@dimagi.com>
2015-08-21 09:20:46 +0200

    Enable user-as-a-case if used in module/form filter

--- corehq/apps/app_manager/views.py
+++ corehq/apps/app_manager/views.py
@@ -1716,6 +1716,8 @@ def edit_module_attr(request, domain, app_id, module_id, attr):
             app.enable_module_filtering and
             should_edit('module_filter')):
         module['module_filter'] = request.POST.get('module_filter')
+        if '#user' in module['module_filter'] and not is_usercase_in_use(domain):
+            enable_usercase(domain)
 
     if should_edit('case_list_form_id'):
         module.case_list_form.form_id = request.POST.get('case_list_form_id')
@@ -2045,6 +2047,8 @@ def edit_form_attr(request, domain, app_id, unique_form_id, attr):
         form.put_in_root = True if put_in_root == "True" else False
     if should_edit('form_filter'):
         form.form_filter = request.POST['form_filter']
+        if '#user' in form.form_filter and not is_usercase_in_use(domain):
+            enable_usercase(domain)
     if should_edit('post_form_workflow'):
         form.post_form_workflow = request.POST['post_form_workflow']
     if should_edit('auto_gps_capture'):
