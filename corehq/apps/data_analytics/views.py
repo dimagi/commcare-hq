63d0f6d8890204b7560a0a33ee363f98370f810b
Sravan Reddy <sravfeyn@gmail.com>
2015-08-24 10:08:12 +0200

    move code around

--- corehq/apps/data_analytics/views.py
+++
@@ -1,9 +0,0 @@
-from corehq.apps.data_analytics.models import MALTRow
-from corehq.apps.data_analytics.admin import MALTRowAdmin
-from dimagi.utils.django.management import export_as_csv_action
-
-
-def malt_csv_response(month, year):
-    query_month = "{year}-{month}-01".format(year=year, month=month)
-    queryset = MALTRow.objects.filter(month=query_month)
-    return export_as_csv_action(exclude=['id'])(MALTRowAdmin, None, queryset)
