cb73564d61354b35296f4a3f4a533ecaa40fdd49
Biyeun Buczyk <biyeun@dimagi.com>
2015-08-25 11:08:10 -0400

    fix cleanup script to handle new prelogin lessjs additions

--- corehq/apps/style/management/commands/fix_less_imports_collectstatic.py
+++ corehq/apps/style/management/commands/fix_less_imports_collectstatic.py
@@ -1,10 +1,8 @@
 import codecs
 import os
+import re
 from django.core.management.base import LabelCommand
 from django.conf import settings
-
-NON_COLLECTED_PATH = '../../../../../style/static/style/less/'
-COLLECTED_PATH = '../../style/less/'
 
 
 class Command(LabelCommand):
@@ -23,11 +21,25 @@ class Command(LabelCommand):
         for less_file in collectedless:
             with codecs.open(less_file, 'r', 'utf-8') as fd:
                 content = fd.read()
-                if NON_COLLECTED_PATH in content:
-                    print "[FIXING] bad less import in %s" % less_file
-                    content = content.replace(NON_COLLECTED_PATH, COLLECTED_PATH)
-                else:
-                    content = None
+                if content is not None:
+                    B3_REGEX = r"@\{b3-import-[a-z]+-[a-z]+\}"
+                    p = re.search(B3_REGEX, content)
+                    if p is not None:
+                        path_def = p.group(0)
+                        path_file = path_def.split('-')[-2]
+                        new_path = settings.LESS_B3_PATHS.get(path_file)
+                        print ("[CORRECTING] %(f_name)s path in %(less_file)s: "
+                               "%(new_path)s" % {
+                                   'f_name': path_file,
+                                   'less_file': less_file,
+                                   'new_path': new_path,
+                               })
+                        if new_path is not None:
+                            content = re.sub(B3_REGEX, new_path, content)
+                        else:
+                            content = None
+                    else:
+                        content = None
             if content is not None:
                 with open(less_file, 'w') as fd:
                     fd.write(content)
