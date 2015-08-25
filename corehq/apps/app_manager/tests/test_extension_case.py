c6b03fd70c06f3fae6e2a0c4566b9a7b713e42ab
Norman Hooper <nhooper@dimagi.com>
2015-08-25 13:19:39 +0200

    form.session_var_for_action only takes one param

--- corehq/apps/app_manager/tests/test_extension_case.py
+++ corehq/apps/app_manager/tests/test_extension_case.py
@@ -159,7 +159,7 @@ class CaseBlockIndexRelationshipTest(SimpleTestCase, TestFileMixin):
     def add_subcase_block(self):
         ### messy messy messy
         parent_node = self.xform.data_node
-        case_id = session_var(self.form.session_var_for_action('subcases', 0))
+        case_id = session_var(self.form.session_var_for_action('subcases'))
         #name = 'subcase_0'
         subcase_node = _make_elem('{x}subcase_0')
         parent_node.append(subcase_node)
