7ea1e4ee530ab9ee93685e0df0a7f38a63c3ada3
Farid Rener <proteusvacuum@gmail.com>
2015-08-26 16:07:22 -0400

    Front end validation

--- corehq/apps/app_manager/static/app_manager/js/visit-scheduler.js
+++ corehq/apps/app_manager/static/app_manager/js/visit-scheduler.js
@@ -104,18 +104,21 @@ var VisitScheduler = (function () {
         self.saveButton = COMMCAREHQ.SaveButton.init({
             unsavedMessage: "You have unsaved schedule settings",
             save: function () {
-                var schedule = JSON.stringify(FormSchedule.unwrap(self.formSchedule));
-                self.saveButton.ajax({
-                    type: 'post',
-                    url: self.save_url,
-                    data: {
-                        schedule: schedule
-                    },
-                    dataType: 'json',
-                    success: function (data) {
-                        COMMCAREHQ.app_manager.updateDOM(data.update);
-                    }
-                });
+                var isValid = self.validate();
+                if (isValid){
+                    var schedule = JSON.stringify(FormSchedule.unwrap(self.formSchedule));
+                    self.saveButton.ajax({
+                        type: 'post',
+                        url: self.save_url,
+                        data: {
+                            schedule: schedule
+                        },
+                        dataType: 'json',
+                        success: function (data) {
+                            COMMCAREHQ.app_manager.updateDOM(data.update);
+                        }
+                    });
+                }
             }
         });
 
@@ -128,6 +131,33 @@ var VisitScheduler = (function () {
 
         self.change = function () {
             self.saveButton.fire('change');
+        };
+
+        self.validate = function(){
+            var errors = 0;
+            var required = self.home.find(":required").not(":disabled");
+            required.each(function(i, req){
+                var $req = $(req);
+                if ($req.val().trim().length === 0){
+                    $req.closest(".control-group").addClass("error");
+                    $req.siblings(".error-text").show();
+                    errors += 1;
+                }
+                else{
+                    $req.closest(".control-group").removeClass("error");
+                    $req.siblings(".error-text").hide();
+                }
+            });
+
+            if (errors || !self.formSchedule.scheduleEnabled()){
+                self.home.find("#form-errors").show();
+                return false;
+            }
+            else{
+                self.home.find("#form-errors").hide();
+                return true;
+            }
+
         };
 
         self.schedulePhase = SchedulePhase.wrap(params.phase, self);
