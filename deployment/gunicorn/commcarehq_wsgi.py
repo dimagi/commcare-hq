1afbe4b12f09b278804c2988fef3544e949a38d5
Jonathan Emord <jemord@dimagi.com>
2015-08-26 13:00:30 -0400

    monkey patch gevent

--- deployment/gunicorn/commcarehq_wsgi.py
+++ deployment/gunicorn/commcarehq_wsgi.py
@@ -7,5 +7,9 @@ _set_source_root_parent('submodules')
 _set_source_root(os.path.join('corehq', 'ex-submodules'))
 _set_source_root(os.path.join('custom', '_legacy'))
 
+from restkit.session import set_session; set_session("gevent")
+from gevent.monkey import patch_all; patch_all(subprocess=True)
+from psycogreen.gevent import patch_psycopg; patch_psycopg()
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
