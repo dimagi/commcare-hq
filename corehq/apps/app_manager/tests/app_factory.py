17c5b7364f6c6e5f8627d908abd4fa49cd04f206
Simon Kelly <skelly@dimagi.com>
2015-08-27 14:15:02 +0200

    update tests to use app factory

--- corehq/apps/app_manager/tests/app_factory.py
+++ corehq/apps/app_manager/tests/app_factory.py
@@ -60,15 +60,16 @@ class AppFactory(object):
     def new_form(self, module):
         slug = self.slugs[module.unique_id]
         index = len(module.forms)
-        form = module.new_form('{} form {}'.format(slug, index), lang='en')
+        form = module.new_form('{} form {}'.format(slug, index), None)
         form.unique_id = '{}_form_{}'.format(slug, index)
+        form.source = ''  # set form source since we changed the unique_id
         return form
 
     @staticmethod
-    def form_updates_case(form, case_type=None, parent_case_type=None):
+    def form_updates_case(form, case_type=None, parent_case_type=None, update=None):
         if form.form_type == 'module_form':
             form.requires = 'case'
-            form.actions.update_case = UpdateCaseAction(update={'question1': '/data/question1'})
+            form.actions.update_case = UpdateCaseAction(update=update)
             form.actions.update_case.condition.type = 'always'
 
             if parent_case_type:
@@ -85,7 +86,7 @@ class AppFactory(object):
             action = LoadUpdateAction(
                 case_type=case_type,
                 case_tag='load_{}_{}'.format(case_type, index),
-                case_properties={'question1': '/data/question1'},
+                case_properties=update,
             )
 
             if parent_case_type:
