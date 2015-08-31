a4f3060fcc38f2c2132043520fa7e5091c030563
Sravan Reddy <sravfeyn@gmail.com>
2015-08-31 11:48:29 +0530

    email and pr response

--- corehq/apps/data_analytics/tasks.py
+++ corehq/apps/data_analytics/tasks.py
@@ -6,13 +6,13 @@ import datetime
 from corehq.apps.data_analytics.malt_generator import MALTTableGenerator
 from dimagi.utils.dates import DateSpan
 from dimagi.utils.django.email import send_HTML_email
-
+from settings import DATA_EMAIL
 
 logger = get_task_logger(__name__)
 
 
-@periodic_task(run_every=crontab(hour=1, minute=0, day_of_month='2'))
-def build_las_month_MALT():
+@periodic_task(queue='background_queue', run_every=crontab(hour=1, minute=0, day_of_month='2'))
+def build_last_month_MALT():
     def _last_month_datespan():
         today = datetime.date.today()
         first_of_this_month = datetime.date(day=1, month=today.month, year=today.year)
@@ -23,14 +23,13 @@ def build_las_month_MALT():
     generator = MALTTableGenerator([last_month])
     generator.build_table()
 
-    data_team_email = 'jwackman@dimagi.com'
     message = 'MALT generation for month {} is now ready. To download go to'\
               ' http://www.commcarehq.org/hq/admin/download_malt/'.format(
                   last_month
               )
     send_HTML_email(
         'MALT is ready',
-        data_team_email,
+        DATA_EMAIL,
         message,
         text_content=message
     )
