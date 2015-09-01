4445c6bd3bc3e26b788bb5d9ac057f986a956ec9
Daniel Roberts <droberts@dimagi.com>
2015-09-01 12:01:40 -0400

    define exports dbaccessors interface better

--- corehq/apps/reports/dbaccessors.py
+++ corehq/apps/reports/dbaccessors.py
@@ -3,11 +3,17 @@ import settings
 from corehq.apps.reports.models import HQExportSchema
 
 
-def get_exports(domain, include_docs=True, **kwargs):
+def get_exports(domain, include_docs=True, limit=None, stale=False):
     # add saved exports. because of the way in which the key is stored
     # (serialized json) this is a little bit hacky, but works.
     startkey = json.dumps([domain, ""])[:-3]
     endkey = "%s{" % startkey
+
+    kwargs = {}
+    if limit is not None:
+        kwargs['limit'] = limit
+    if stale:
+        kwargs['stale'] = settings.COUCH_STALE_QUERY
     return HQExportSchema.view(
         "couchexport/saved_export_schemas",
         startkey=startkey,
@@ -17,12 +23,12 @@ def get_exports(domain, include_docs=True, **kwargs):
     )
 
 
-def stale_get_exports(domain, include_docs=True, **kwargs):
+def stale_get_exports(domain, include_docs=True, limit=None):
     return get_exports(
         domain,
         include_docs=include_docs,
-        stale=settings.COUCH_STALE_QUERY,
-        **kwargs
+        stale=True,
+        limit=limit,
     )
 
 
