f3d5ac73f8e1bb9c446fe8110c3c89d18e58dfe7
Biyeun Buczyk <biyeun@dimagi.com>
2015-09-02 17:48:07 -0400

    end-to-end single form export works

--- corehq/apps/export/static/export/js/download_export.ng.js
+++ corehq/apps/export/static/export/js/download_export.ng.js
@@ -11,6 +11,15 @@
         'ngMessages'
     ]);
 
+    var $element = {
+        progress: function () {
+            return $('#download-progress-bar');
+        },
+        group: function () {
+            return $('#id_group');
+        }
+    };
+
     download_export.constant('exportList', []);
 
     var exportsControllers = {};
@@ -20,12 +29,19 @@
         var self = {};
         $scope._ = _;   // make underscore.js available
         $scope.formData = {};
-        $scope.exportList = exportList;
+        $scope.exportList = _.map(exportList, function (exportData) {
+            exportData['filename'] = encodeURIComponent(exportData['name']);
+            return exportData;
+        });
 
-        $scope.showSelectGroups = false;
+
+        $scope.isSelectGroups = false;
         $scope.hasGroups = false;
+
         $scope.groupsLoading = true;
         $scope.groupsError = false;
+        $scope.preparingExport = false;
+
         self._groupRetries = 0;
 
         self._handleGroupError = function () {
@@ -46,7 +62,7 @@
             if (data.success) {
                 $scope.groupsLoading = false;
                 $scope.hasGroups = data.groups.length > 0;
-                $('#id_groups').select2({
+                $element.group().select2({
                     data: data.groups
                 });
             } else {
@@ -62,9 +78,96 @@
 
         self._getGroups();
 
-        $scope.prepareExport = function () {
-            exportDownloadService.startDownload($scope.exportList, $scope.formData);
+        $scope.isFormInvalid = function () {
+            if ($scope.isSelectGroups) {
+                return _.isEmpty($scope.formData.group);
+            }
+            return _.isEmpty($scope.formData.user_types);
         };
+
+        $scope.prepareExport = function () {
+            $scope.preparingExport = true;
+            djangoRMI.prepare_custom_export({
+                exports: $scope.exportList,
+                form_data: $scope.formData,
+                use_group: !!$scope.isSelectGroups
+            })
+                .success(function (data) {
+                    if (data.success) {
+                        $scope.preparingExport = false;
+                        $scope.downloadInProgress = true;
+                        exportDownloadService.startDownload(data.download_id);
+                    } else {
+                        // todo deal with error
+                    }
+                })
+                .error(function () {
+                        // todo deal with error
+                });
+        };
+
+        $scope.$watch(function () {
+            return exportDownloadService.showDownloadStatus;
+        }, function (status) {
+            $scope.downloadInProgress = status;
+        });
+        $scope.downloadInProgress = false;
+    };
+
+    exportsControllers.DownloadProgressController = function (
+        $scope, exportDownloadService
+    ) {
+        var self = {};
+
+        self._reset = function () {
+            $scope.showProgress = false;
+            $scope.showDownloadStatus = false;
+            $scope.isDownloadReady = false;
+            $scope.isDownloaded = false;
+            $scope.dropboxUrl = null;
+            $scope.downloadUrl = null;
+            $scope.progress = {};
+            $element.progress().css('width', '0%');
+            $element.progress().removeClass('progress-bar-success');
+        };
+
+        self._reset();
+
+        $scope.$watch(function () {
+            return exportDownloadService.downloadStatusData;
+        }, function (data) {
+            if (!_.isEmpty(data)) {
+                $scope.progress = data.progress;
+                self._updateProgressBar(data);
+                $scope.showProgress = true;
+            }
+        });
+
+        self._updateProgressBar = function (data) {
+            var progressPercent = 0;
+            if (data.is_ready && data.has_file) {
+                progressPercent = 100;
+                $scope.isDownloadReady = true;
+                $scope.dropboxUrl = data.dropbox_url;
+                $scope.downloadUrl = data.download_url;
+                $element.progress().addClass('progress-bar-success');
+            } else if (_.isNumber(data.progress.percent)) {
+                progressPercent = data.progress.percent;
+            }
+            $scope.progress.percent = progressPercent;
+            $element.progress().css('width', progressPercent + '%');
+        };
+
+        $scope.resetDownload = function () {
+            self._reset();
+            exportDownloadService.resetDownload();
+        };
+
+        $scope.$watch(function () {
+            return exportDownloadService.showDownloadStatus;
+        }, function (status) {
+            $scope.showDownloadStatus = status;
+        });
     };
     download_export.controller(exportsControllers);
 
@@ -72,34 +175,43 @@
     downloadExportServices.exportDownloadService = function ($interval, djangoRMI) {
         var self = {};
 
-        self.exportList = [];
-        self.formData = {};
-
-        self._numErrors = 0;
-
-        self._testPolls = 0;
-
-        self._checkDownloadProgress = function () {
-            djangoRMI.check_export_prep({})
-                .success(function (data) {
-                    console.log('update');
-                    self._testPolls ++;
-                    if (self._testPolls > 10) {
-                        $interval.cancel(self._promise);
-                    }
-
-                })
-                .error(function () {
-                    if (self._numErrors > 3) {
-                        console.log('deal with error');
-                    }
-                    self._numErrors ++;
-                });
+        self.resetDownload = function () {
+            self.downloadId = null;
+            self._numErrors = 0;
+            self.downloadStatusData = null;
+            self.showDownloadStatus = false;
         };
 
-        self.startDownload = function (exportList, formData) {
-            self.exportList = exportList;
-            self.formData = formData;
+        self.resetDownload();
+
+        self._checkDownloadProgress = function () {
+            djangoRMI.poll_custom_export_download({
+                download_id: self.downloadId
+            })
+                .success(function (data) {
+                    if (data.is_poll_successful) {
+                        self.downloadStatusData = data;
+                        if (data.has_file && data.is_ready) {
+                            $interval.cancel(self._promise);
+                        }
+                    } else {
+                        self._dealWithErrors();
+                    }
+                })
+                .error(self._dealWithErrors);
+        };
+
+        self._dealWithErrors = function () {
+            if (self._numErrors > 3) {
+                console.log('deal with error');
+                $interval.cancel(self._promise);
+            }
+            self._numErrors ++;
+        };
+
+        self.startDownload = function (downloadId) {
+            self.showDownloadStatus = true;
+            self.downloadId = downloadId;
             self._promise = $interval(self._checkDownloadProgress, 2000);
         };
 
