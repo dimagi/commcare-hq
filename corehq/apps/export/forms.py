999c07e0a12cc1d36799919038783cbdc01b0a0b
Biyeun Buczyk <biyeun@dimagi.com>
2015-09-02 17:07:45 -0400

    start splitting out form / case export filter functionality and prepare export filters

--- corehq/apps/export/forms.py
+++ corehq/apps/export/forms.py
@@ -1,18 +1,24 @@
 import json
+import dateutil
 from django import forms
-from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _, ugettext_noop
-from corehq.apps.hqwebapp.crispy import BootstrapMultiField
+from corehq.apps.groups.models import Group
+from corehq.apps.reports.models import HQUserType
+from corehq.apps.reports.util import group_filter, users_matching_filter, \
+    users_filter, datespan_export_filter, app_export_filter, case_group_filter, \
+    case_users_filter
 from corehq.apps.style.crispy import B3MultiField, CrispyTemplate
 from corehq.apps.style.forms.widgets import Select2MultipleChoiceWidget, \
     DateRangePickerWidget
+from couchexport.util import SerializableFunction
 
 from crispy_forms.bootstrap import FormActions, InlineField
 from crispy_forms.helper import FormHelper
 from crispy_forms import layout as crispy
 
 from corehq.apps.app_manager.dbaccessors import get_apps_in_domain
-from crispy_forms.layout import Layout, Fieldset
+from crispy_forms.layout import Layout
+from dimagi.utils.dates import DateSpan
 
 
 class CreateFormExportForm(forms.Form):
@@ -145,37 +151,41 @@ class CreateCaseExportForm(forms.Form):
             ),
         )
 
-_USER_TYPES_CHOICES = [
-    ('mobile', ugettext_noop("Mobile Worker")),
-    ('demo_user', ugettext_noop("Demo User")),
-    ('admin', ugettext_noop("Admin")),
-    ('unknown', ugettext_noop("Unknown")),
-    ('supply', ugettext_noop("CommCare Supply")),
-]
+
+
+
 
 
 class FilterExportDownloadForm(forms.Form):
 
+    DEFAULT_GROUP_OPTION = ('everyone', ugettext_noop("Everyone (All Groups)"))
+    _USER_MOBILE = 'mobile'
+    _USER_DEMO = 'demo_user'
+    _USER_ADMIN = 'admin'
+    _USER_UNKNOWN = 'unknown'
+    _USER_SUPPLY = 'supply'
+
+    _USER_TYPES_CHOICES = [
+        (_USER_MOBILE, ugettext_noop("All Mobile Workers")),
+        (_USER_DEMO, ugettext_noop("Demo User")),
+        (_USER_ADMIN, ugettext_noop("Admin User")),
+        (_USER_UNKNOWN, ugettext_noop("Unknown Users")),
+        (_USER_SUPPLY, ugettext_noop("CommCare Supply")), # todo filter out supply
+    ]
     user_types = forms.MultipleChoiceField(
-        label=ugettext_noop("User Type"),
-        required=True,
-        choices=_USER_TYPES_CHOICES,
-        widget=Select2MultipleChoiceWidget
-    )
-    groups = forms.MultipleChoiceField(
-        label=ugettext_noop("Groups"),
-        required=False,
-        choices=[],  # filled dynamically
+        label=ugettext_noop("Select User Types"),
         widget=Select2MultipleChoiceWidget,
+        choices=_USER_TYPES_CHOICES,
+        required=False,
+    )
+    group = forms.CharField(
+        label=ugettext_noop("Select Group"),
+        required=False,
     )
     date_range = forms.CharField(
         label=ugettext_noop("Date Range"),
         required=True,
         widget=DateRangePickerWidget(),
-    )
-    exports = forms.CharField(
-        widget=forms.HiddenInput(),
-        required=True,
     )
 
     def __init__(self, domain, timezone, *args, **kwargs):
@@ -202,26 +212,34 @@ class FilterExportDownloadForm(forms.Form):
                 ng_required='true',
                 ng_model='formData.export_id',
             ),
-            crispy.Field(
-                'user_types',
-                ng_model='formData.user_types',
-                ng_required='true',
-                placeholder=_("Everyone"),
-                ng_init="formData.user_types = [{}]".format(
-                    json.dumps(_USER_TYPES_CHOICES[0])
-                )
-            ),
             B3MultiField(
-                _("Groups"),
+                _("User Types"),
                 crispy.Div(
                     crispy.Div(
                         InlineField(
-                            'groups',
-                            ng_model='formData.groups',
+                            'user_types',
+                            ng_model='formData.user_types',
                             ng_required='false',
                             style="width: 98%",
                         ),
-                        ng_show="showSelectGroups && hasGroups",
+                        ng_show="!isSelectGroups || !hasGroups",
+                    ),
+                ),
+                CrispyTemplate('export/crispy_html/user_types_help.html', {
+                    'domain': self.domain,
+                }),
+            ),
+            B3MultiField(
+                _("Group"),
+                crispy.Div(
+                    crispy.Div(
+                        InlineField(
+                            'group',
+                            ng_model='formData.group',
+                            ng_required='false',
+                            style="width: 98%",
+                        ),
+                        ng_show="isSelectGroups && hasGroups",
                     ),
                 ),
                 CrispyTemplate('export/crispy_html/groups_help.html', {
@@ -230,18 +248,88 @@ class FilterExportDownloadForm(forms.Form):
             ),
             crispy.Field(
                 'date_range',
-                ng_model='formData.date_rage',
+                ng_model='formData.date_range',
                 ng_required='true',
             ),
         )
 
-    def format_form_export(self, export):
+    def _get_filtered_users(self):
+        user_types = self.cleaned_data['user_types']
+        # this STUPID. todo fix
+        user_filter_toggles = [
+            self._USER_MOBILE in user_types,
+            self._USER_DEMO in user_types,
+            self._USER_ADMIN in user_types,
+            self._USER_UNKNOWN in user_types,
+            self._USER_SUPPLY in user_types
+        ]
+        user_filters = HQUserType._get_manual_filterset(
+            (True,) * HQUserType.count,
+            user_filter_toggles
+        )
+        return users_matching_filter(self.domain, user_filters)
+
+    def _get_group(self):
+        group = self.cleaned_data['group']
+        if group:
+            return Group.get(group)
+
+
+class FilterFormExportDownloadForm(FilterExportDownloadForm):
+    """The filters for Form Export Download
+    """
+
+    def _get_user_or_group_filter(self):
+        group = self._get_group()
+        if group:
+            # filter by groups
+            return SerializableFunction(group_filter, group=group)
+        # filter by users
+        return SerializableFunction(users_filter,
+                                    users=self._get_filtered_users())
+
+    def _get_datespan_filter(self):
+        date_range = self.cleaned_data['date_range']
+        print "date_range", date_range
+        dates = date_range.split(DateRangePickerWidget.separator)
+        print "dates", dates
+        startdate = dateutil.parser.parse(dates[0])
+        enddate = dateutil.parser.parse(dates[1])
+        datespan = DateSpan(startdate, enddate)
+        if datespan.is_valid():
+            datespan.set_timezone(self.timezone)
+            return SerializableFunction(datespan_export_filter,
+                                        datespan=datespan)
+
+    def get_form_filter(self):
+        # todo is app_id ever not None for custom exports?
+        form_filter = SerializableFunction(app_export_filter, app_id=None)
+
+        datespan_filter = self._get_datespan_filter()
+        if datespan_filter:
+            form_filter &= datespan_filter
+        form_filter &= self._get_user_or_group_filter()
+        # '&export_tag=["'+self.domain+'","'+xmlns+'","' + fileName +'"]' +
+        return form_filter
+
+    def format_export_data(self, export):
         return {
             'export_id': export.get_id,
             'xmlns': export.xmlns if hasattr(export, 'xmlns') else '',
             'export_type': 'form',
             'format':  export.default_format,
-            'dlocation': reverse('export_custom_data',
-                                 args=[self.domain, export.get_id]),
             'name': export.name,
         }
+
+class FilterCaseExportDownloadForm(FilterExportDownloadForm):
+
+    def get_case_filter(self):
+        group = self._get_group()
+        if group:
+            return SerializableFunction(case_group_filter, group=group)
+        case_sharing_groups = [g.get_id for g in
+                               Group.get_case_sharing_groups(self.domain)]
+        return SerializableFunction(case_users_filter,
+                                    users=self._get_filtered_users(),
+                                    groups=case_sharing_groups)
+
