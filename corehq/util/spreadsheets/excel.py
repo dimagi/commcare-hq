81541ce5a543379fda91ca400a18fccc0c2e23e4
Daniel Roberts <droberts@dimagi.com>
2015-09-03 16:53:15 -0400

    remove unused class

--- corehq/util/spreadsheets/excel.py
+++ corehq/util/spreadsheets/excel.py
@@ -2,40 +2,6 @@ from tempfile import NamedTemporaryFile
 import openpyxl
 
 
-# a *DictReader responds to __init__(self, file), __iter__, and fieldnames
-
-
-class Excel2007DictReader(object):
-    def __init__(self, f):
-        if isinstance(f, basestring):
-            filename = f
-        elif not isinstance(f, file):
-            tmp = NamedTemporaryFile(mode='wb', suffix='xlsx', delete=False)
-            filename = tmp.name
-            tmp.write(f.read())
-            tmp.close()
-        else:
-            filename = f
-
-        self.wb = openpyxl.load_workbook(filename, use_iterators=True)
-        self.worksheet = self.wb.worksheets[0]
-        self.fieldnames = []
-        self.fieldnames = [cell.value for cell in self.worksheet.iter_rows().next()]
-
-    def __iter__(self):
-        rows = self.worksheet.iter_rows()
-        rows.next()
-
-        def to_string(thing):
-            if isinstance(thing, int):
-                return unicode(thing)
-            elif isinstance(thing, float):
-                return unicode(int(thing))
-            return thing
-        for row in rows:
-            yield dict(zip(self.fieldnames, [to_string(cell.value) for cell in row]))
-
-
 class JSONReaderError(Exception):
     pass
 
