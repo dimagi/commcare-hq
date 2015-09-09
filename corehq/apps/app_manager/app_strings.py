b7f2ff9eedec0fec68847a4840d11f073e3704e1
Nick Pellegrino <npellegrino@dimagi.com>
2015-09-09 18:49:31 -0400

    use report name given in app manager

--- corehq/apps/app_manager/app_strings.py
+++ corehq/apps/app_manager/app_strings.py
@@ -81,7 +81,7 @@ def _create_custom_app_strings(app, lang, for_default=False):
             yield id_strings.report_last_sync(), 'Last Sync'
             for config in module.report_configs:
                 yield id_strings.report_command(config.uuid), trans(config.header)
-                yield id_strings.report_name(config.uuid), config.report.title
+                yield id_strings.report_name(config.uuid), trans(config.header)
                 for column in config.report.report_columns:
                     yield (
                         id_strings.report_column_header(config.uuid, column.column_id),
