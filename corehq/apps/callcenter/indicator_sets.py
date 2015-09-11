156e71a292fe0e19ec35728cc99902642dece2cf
Simon Kelly <skelly@dimagi.com>
2015-09-11 14:53:45 +0200

    allow specifying which indicators should be included

--- corehq/apps/callcenter/indicator_sets.py
+++ corehq/apps/callcenter/indicator_sets.py
@@ -1,8 +1,10 @@
 from collections import defaultdict
 from datetime import timedelta, datetime
+import functools
 from django.core.cache import cache
 from django.db.models.aggregates import Count, Avg
 from django.db.models.query_utils import Q
+from corehq.apps.callcenter.models import CallCenterIndicatorConfig
 from corehq.apps.hqcase.dbaccessors import get_case_types_for_domain
 from dimagi.ext.jsonobject import JsonObject, DictProperty, StringProperty
 import pytz
@@ -83,6 +85,8 @@ class CallCenterIndicators(object):
         self.override_cases = override_cases
         self.override_cache = override_cache
 
+        self.config = CallCenterIndicatorConfig.for_domain(domain_name)
+
         try:
             self.timezone = pytz.timezone(domain_timezone)
         except pytz.UnknownTimeZoneError:
@@ -94,17 +98,18 @@ class CallCenterIndicators(object):
         self.reference_date = override_date or datetime.now(self.timezone).date()
 
     @property
+    @memoized
     def date_ranges(self):
         weekago = self.reference_date - timedelta(days=7)
         weekago2 = self.reference_date - timedelta(days=14)
         daysago30 = self.reference_date - timedelta(days=30)
         daysago60 = self.reference_date - timedelta(days=60)
-        return [
-            (WEEK0, weekago, self.reference_date),
-            (WEEK1, weekago2, weekago),
-            (MONTH0, daysago30, self.reference_date),
-            (MONTH1, daysago60, daysago30),
-        ]
+        return {
+            WEEK0: (weekago, self.reference_date),
+            WEEK1: (weekago2, weekago),
+            MONTH0: (daysago30, self.reference_date),
+            MONTH1: (daysago60, daysago30),
+        }
 
     def _date_filters(self, date_field, lower, upper):
         return {
@@ -222,71 +227,157 @@ class CallCenterIndicators(object):
         for user_id in unseen_users:
             self.data[user_id].setdefault(indicator_name, self.no_value)
 
-    def _add_case_data(self, queryset, indicator_prefix, range_name, legacy_prefix=None):
+    def _update_dataset(self, dataset, owner_id, cnt):
         """
-        Given a QuerySet containing data for case based indicators generate the 'total' and
-        'by_case' indicators before adding them to the data set.
+        If the value in the 'case_owner' column of the QuerySet is a Group then attribute that
+        indicator value to each user in the group (adding it to any existing value that the user has).
+
+        :param dataset: dict mapping owner_id to count
+        """
+        if owner_id in self.group_to_user_ids:
+            for user_id in self.group_to_user_ids[owner_id]:
+                if user_id in self.users_needing_data:
+                    dataset[user_id] += cnt
+        else:
+            dataset[owner_id] += cnt
+
+    def _reformat_and_add(self, data_dict, indicator_name, legacy_name=None):
+        rows = [dict(user_id=user, count=cnt) for user, cnt in data_dict.items()]
+        self._add_data(FakeQuerySet(rows), indicator_name)
+        if legacy_name:
+            self._add_data(FakeQuerySet(rows), legacy_name)
+
+    def _add_case_data_total(self, queryset, indicator_prefix, range_name, legacy_prefix=None):
+        """
+        Given a QuerySet containing data for case based indicators generate the 'total'
+        indicators before adding them to the data set.
+
+        QuerySet expected to contain the following columns:
+        *  case_owner:  ID of the case owner - User or Group.
+        *  count:       The value of the indicator for the owner and the case type.
+        """
+        total_data = defaultdict(lambda: 0)
+        for result in queryset:
+            owner = result['case_owner']
+            count = result['count']
+            self._update_dataset(total_data, owner, count)
+
+        self._reformat_and_add(
+            total_data,
+            '{}_{}'.format(indicator_prefix, range_name),
+            legacy_name='{}{}'.format(legacy_prefix, range_name.title()) if legacy_prefix else None
+        )
+
+    def _add_case_data_by_type(self, queryset, indicator_prefix, range_name, add_missing_types):
+        """
+        Given a QuerySet containing data for case based indicators generate the 'by_case'
+        indicators before adding them to the data set.
 
         QuerySet expected to contain the following columns:
         *  case_owner:  ID of the case owner - User or Group.
         *  type:        The case type.
         *  count:       The value of the indicator for the owner and the case type.
 
-        If the value in the 'case_owner' column of the QuerySet is a Group then attribute that
-        indicator value to each user in the group (adding it to any existing value that the user has).
-
         Also include default values for any case_types that are missing from the QuerySet
         """
-        def _update_dataset(dataset, owner_id, cnt):
-            if owner_id in self.group_to_user_ids:
-                for user_id in self.group_to_user_ids[owner_id]:
-                    if user_id in self.users_needing_data:
-                        dataset[user_id] += cnt
-            else:
-                dataset[owner_id] += cnt
-
-        total_data = defaultdict(lambda: 0)
         type_data = defaultdict(lambda: defaultdict(lambda: 0))
         for result in queryset:
             owner = result['case_owner']
             count = result['count']
             case_type = result['type']
-            _update_dataset(total_data, owner, count)
-            _update_dataset(type_data[case_type], owner, count)
-
-        def _reformat_and_add(data_dict, indicator_name, legacy_name=None):
-            rows = [dict(user_id=user, count=cnt) for user, cnt in data_dict.items()]
-            self._add_data(FakeQuerySet(rows), indicator_name)
-            if legacy_name:
-                self._add_data(FakeQuerySet(rows), legacy_name)
-
-        _reformat_and_add(
-            total_data,
-            '{}_{}'.format(indicator_prefix, range_name),
-            legacy_name='{}{}'.format(legacy_prefix, range_name.title()) if legacy_prefix else None
-        )
+            self._update_dataset(type_data[case_type], owner, count)
 
         seen_types = set()
         for case_type, data in type_data.items():
-            _reformat_and_add(data, '{}_{}_{}'.format(indicator_prefix, case_type, range_name))
+            self._reformat_and_add(data, '{}_{}_{}'.format(indicator_prefix, case_type, range_name))
             seen_types.add(case_type)
 
-        # add data for case types with no data
-        unseen_cases = self.case_types - seen_types
-        for case_type in unseen_cases:
-            self._add_data(FakeQuerySet([]), '{}_{}_{}'.format(indicator_prefix, case_type, range_name))
+        if add_missing_types:
+            # add data for case types with no data
+            unseen_cases = self.case_types - seen_types
+            for case_type in unseen_cases:
+                self._add_data(FakeQuerySet([]), '{}_{}_{}'.format(indicator_prefix, case_type, range_name))
 
-    def _case_query_opened_closed(self, opened_or_closed, lower, upper):
+    def _case_query_opened_closed(self, opened_or_closed, values, filters, lower, upper):
+        """
+        Count of cases where lower <= opened_on < upper
+            cases_opened_{period}
+            cases_opened_{case_type}_{period}
+
+        Count of cases where lower <= closed_on < upper
+            cases_closed_{period}
+            cases_closed_{case_type}_{period}
+        """
         return CaseData.objects \
             .extra(select={'case_owner': '{}_by'.format(opened_or_closed)}) \
-            .values('case_owner', 'type') \
-            .exclude(type=self.cc_case_type) \
+            .values(*values) \
+            .filter(filters) \
             .filter(
                 domain=self.domain) \
             .filter(**self._date_filters('{}_on'.format(opened_or_closed), lower, upper)) \
             .filter(**{
                 '{}_by__in'.format(opened_or_closed): self.users_needing_data
             }).annotate(count=Count('case_id'))
+
+    def _case_query_active(self, values, filters, lower, upper):
+        """
+        Count of cases where lower <= case_action.date < upper
+
+        cases_active_{period}
+        cases_active_{case_type}_{period}
+        """
+        return CaseActionData.objects \
+            .extra(select={'type': 'case_type'}) \
+            .values(*values) \
+            .filter(filters) \
+            .filter(
+                domain=self.domain,
+                case_owner__in=self.owners_needing_data,
+                date__gte=lower,
+                date__lt=upper
+            ).annotate(count=Count('case', distinct=True))
+
+    def _cases_total_query(self, values, filters, lower, upper):
+        """
+        Count of cases where opened_on < upper and (closed == False or closed_on >= lower)
+
+        cases_total_{period}
+        cases_total_{case_type}_{period}
+        """
+        return CaseData.objects \
+            .values(*values) \
+            .filter(filters) \
+            .filter(
+                case_owner__in=self.owners_needing_data,
+                domain=self.domain,
+                opened_on__lt=upper) \
+            .filter(Q(closed=False) | Q(closed_on__gte=lower)) \
+            .annotate(count=Count('case_id'))
+
+    def add_case_data(self, query_fn, slug, indicator_config, type_column='type', legacy_prefix=None):
+        include_types = indicator_config.all_types or indicator_config.types
+        include_total = indicator_config.total.active,
+        limit_types = indicator_config.types
+
+        q_type_exclude = {type_column: self.cc_case_type}
+        if include_total and include_types and not limit_types:
+            for range_name in indicator_config.total.date_ranges:
+                lower, upper = self.date_ranges[range_name]
+                results = query_fn(['case_owner', 'type'], ~Q(**q_type_exclude), lower, upper)
+                self._add_case_data_total(results, slug, range_name, legacy_prefix=legacy_prefix)
+                self._add_case_data_by_type(results, slug, range_name, add_missing_types=True)
+        else:
+            for range_name in indicator_config.total.date_ranges:
+                lower, upper = self.date_ranges[range_name]
+                total_results = query_fn(['case_owner'], ~Q(**q_type_exclude), lower, upper)
+                self._add_case_data_total(total_results, slug, range_name, legacy_prefix=legacy_prefix)
+
+            if include_types:
+                for range_name, types in indicator_config.types_by_date_range():
+                    q_type_in = {'{}__in'.format(type_column): types}
+                    lower, upper = self.date_ranges[range_name]
+                    type_results = query_fn(['case_owner', 'type'], Q(**q_type_in), lower, upper)
+                    self._add_case_data_by_type(type_results, slug, range_name, add_missing_types=False)
 
     def add_case_total_legacy(self):
         """
@@ -302,59 +393,6 @@ class CallCenterIndicators(object):
             .annotate(count=Count('case_id'))
 
         self._add_data(results, LEGACY_TOTAL_CASES)
-
-    def add_cases_total_data(self, range_name, lower, upper):
-        """
-        Count of cases where opened_on < upper and (closed == False or closed_on >= lower)
-
-        cases_total_{period}
-        cases_total_{case_type}_{period}
-        """
-        results = CaseData.objects \
-            .values('case_owner', 'type') \
-            .exclude(type=self.cc_case_type) \
-            .filter(
-                case_owner__in=self.owners_needing_data,
-                domain=self.domain,
-                opened_on__lt=upper) \
-            .filter(Q(closed=False) | Q(closed_on__gte=lower)) \
-            .annotate(count=Count('case_id'))
-
-        self._add_case_data(results, CASES_TOTAL, range_name)
-
-    def add_cases_opened_closed_data(self, range_name, lower, upper):
-        """
-        Count of cases where lower <= opened_on < upper
-            cases_opened_{period}
-            cases_opened_{case_type}_{period}
-
-        Count of cases where lower <= closed_on < upper
-            cases_closed_{period}
-            cases_closed_{case_type}_{period}
-        """
-        for state, name in [('opened', CASES_OPENED), ('closed', CASES_CLOSED)]:
-            results = self._case_query_opened_closed(state, lower, upper)
-            self._add_case_data(results, name, range_name)
-
-    def add_cases_active_data(self, range_name, lower, upper):
-        """
-        Count of cases where lower <= case_action.date < upper
-
-        cases_active_{period}
-        cases_active_{case_type}_{period}
-        """
-        results = CaseActionData.objects \
-            .extra(select={'type': 'case_type'}) \
-            .values('case_owner', 'type') \
-            .exclude(case_type=self.cc_case_type) \
-            .filter(
-                domain=self.domain,
-                case_owner__in=self.owners_needing_data,
-                date__gte=lower,
-                date__lt=upper
-            ).annotate(count=Count('case', distinct=True))
-
-        self._add_case_data(results, CASES_ACTIVE, range_name, legacy_prefix=LEGACY_CASES_UPDATED)
 
     def add_custom_form_data(self, indicator_name, range_name, xmlns, indicator_type, lower, upper):
         """
@@ -382,43 +420,66 @@ class CallCenterIndicators(object):
             '{}{}'.format(indicator_name, range_name.title()),
             transformer=transformer)
 
-    def add_form_data(self, range_name, lower, upper):
+    def add_form_data(self, indicator_config):
         """
         Count of forms submitted by each user during the period (upper to lower)
         """
-        results = FormData.objects \
-            .values('user_id') \
-            .filter(**self._date_filters('time_end', lower, upper)) \
-            .filter(
-                domain=self.domain,
-                user_id__in=self.users_needing_data
-            )\
-            .annotate(count=Count('instance_id'))
+        for range_name in indicator_config.date_ranges:
+            lower, upper = self.date_ranges[range_name]
+            results = FormData.objects \
+                .values('user_id') \
+                .filter(**self._date_filters('time_end', lower, upper)) \
+                .filter(
+                    domain=self.domain,
+                    user_id__in=self.users_needing_data
+                )\
+                .annotate(count=Count('instance_id'))
 
-        self._add_data(results, '{}_{}'.format(FORMS_SUBMITTED, range_name))
-        #  maintained for backwards compatibility
-        self._add_data(results, '{}{}'.format(LEGACY_FORMS_SUBMITTED, range_name.title()))
+            self._add_data(results, '{}_{}'.format(FORMS_SUBMITTED, range_name))
 
-        if self.domain in PER_DOMAIN_FORM_INDICATORS:
-            for custom in PER_DOMAIN_FORM_INDICATORS[self.domain]:
-                self.add_custom_form_data(
-                    custom['slug'],
-                    range_name,
-                    custom['xmlns'],
-                    custom['type'],
-                    lower,
-                    upper
-                )
+            if indicator_config.include_legacy:
+                #  maintained for backwards compatibility
+                self._add_data(results, '{}{}'.format(LEGACY_FORMS_SUBMITTED, range_name.title()))
+
+            if self.domain in PER_DOMAIN_FORM_INDICATORS:
+                for custom in PER_DOMAIN_FORM_INDICATORS[self.domain]:
+                    self.add_custom_form_data(
+                        custom['slug'],
+                        range_name,
+                        custom['xmlns'],
+                        custom['type'],
+                        lower,
+                        upper
+                    )
 
     def get_data(self):
         final_data = {}
         if self.users_needing_data:
-            self.add_case_total_legacy()
-            for range_name, lower, upper in self.date_ranges:
-                self.add_form_data(range_name, lower, upper)
-                self.add_cases_total_data(range_name, lower, upper)
-                self.add_cases_opened_closed_data(range_name, lower, upper)
-                self.add_cases_active_data(range_name, lower, upper)
+            if self.config.cases_total.active and self.config.cases_total.include_legacy:
+                self.add_case_total_legacy()
+
+            if self.config.forms_updated.active:
+                self.add_form_data(self.config.forms_updated)
+
+            if self.config.cases_total.active:
+                self.add_case_data(self._cases_total_query, CASES_TOTAL, self.config.cases_total)
+
+            if self.config.cases_opened.active:
+                query_fn = functools.partial(self._case_query_opened_closed, 'opened')
+                self.add_case_data(query_fn, CASES_OPENED, self.config.cases_opened)
+
+            if self.config.cases_closed.active:
+                query_fn = functools.partial(self._case_query_opened_closed, 'closed')
+                self.add_case_data(query_fn, CASES_CLOSED, self.config.cases_closed)
+
+            if self.config.cases_active.active:
+                self.add_case_data(
+                    self._case_query_active,
+                    CASES_ACTIVE,
+                    self.config.cases_total,
+                    type_column='case_type',
+                    legacy_prefix=LEGACY_CASES_UPDATED
+                )
 
             cache_timeout = seconds_till_midnight(self.timezone)
             user_to_case_map = self.user_to_case_map
--- corehq/apps/callcenter/models.py
+++ corehq/apps/callcenter/models.py
@@ -1,8 +1,80 @@
 # Stub models file
-from dimagi.ext.couchdbkit import Document
+from collections import namedtuple
+import itertools
+from corehq.apps.callcenter.const import DATE_RANGES
+from corehq.util.quickcache import skippable_quickcache
+from dimagi.ext.couchdbkit import *
+
+TypeRange = namedtuple('TypeRange', 'type, range_slug')
+
+class BasicIndicator(DocumentSchema):
+    active = BooleanProperty()
+    date_ranges = StringListProperty()
+    include_legacy = BooleanProperty()
 
 
-class _(Document):
-    pass
+class TypedIndicator(BasicIndicator):
+    type = StringProperty()
+
+
+class ByTypeIndicator(BasicIndicator):
+    total = SchemaProperty(BasicIndicator)
+    all_types = BooleanProperty()  # same date ranges as 'total'
+    types = SchemaListProperty(TypedIndicator)
+
+    def types_by_date_range(self):
+        types_list = [
+            TypeRange(type_, date_range) for type_ in self.types
+            for date_range in type_.date_ranges
+        ]
+        return {
+            range_slug: [type_.type for type_ in group]
+            for range_slug, group in itertools.groupby(types_list, lambda x: x.range_slug)
+        }
+
+
+class CallCenterIndicatorConfig(Document):
+    domain = StringProperty()
+    forms_updated = SchemaProperty(BasicIndicator)
+    cases_total = SchemaProperty(ByTypeIndicator)
+    cases_active = SchemaProperty(ByTypeIndicator)
+    cases_opened = SchemaProperty(ByTypeIndicator)
+    cases_closed = SchemaProperty(ByTypeIndicator)
+
+    @classmethod
+    @skippable_quickcache(['domain'], skip_arg='skip_cache')
+    def for_domain(cls, domain, skip_cache=False):
+        res = cls.view(
+            "domain/docs",
+            key=[domain, cls.__name__, None],
+            reduce=False,
+            include_docs=True,
+            wrapper=cls.wrap)
+
+        return res[0] if len(res) else cls.default_config(domain)
+
+    @classmethod
+    def default_config(cls, domain):
+        def default_basic():
+            return BasicIndicator(active=True, date_ranges=DATE_RANGES, include_legacy=True)
+
+        def default_typed():
+            return ByTypeIndicator(
+                active=True,
+                date_ranges=DATE_RANGES,
+                total=default_basic(),
+                all_types=True,
+                include_legacy=True
+            )
+
+        return cls(
+            domain=domain,
+            forms_updated=default_basic(),
+            cases_total=default_typed(),
+            cases_active=default_typed(),
+            cases_opened=default_typed(),
+            cases_closed=default_typed(),
+        )
+
 
 from .signals import *
