643ceb47141ad2a09209a72908c37f5de3d66a64
Daniel Roberts <droberts@dimagi.com>
2015-09-13 17:31:08 -0400

    use caches instead of get_cache in quickcache

--- .travis/localsettings.py
+++ .travis/localsettings.py
@@ -59,7 +59,8 @@ redis_cache = {
 }
 CACHES = {
     'default': redis_cache,
-    'redis': redis_cache
+    'redis': redis_cache,
+    'locmem': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'},
 }
 
 AUDIT_MODEL_SAVE = ['django.contrib.auth.models.User']
