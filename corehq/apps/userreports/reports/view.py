047749bf7d68b48755b0cec543353575a8374749
Nick Pellegrino <npellegrino@dimagi.com>
2015-09-14 23:35:36 -0400

    use CustomConfigurableReport class

--- corehq/apps/userreports/reports/view.py
+++ corehq/apps/userreports/reports/view.py
@@ -356,6 +356,10 @@ class ConfigurableReport(JSONResponseMixin, TemplateView):
         return file
 
 
+class CustomConfigurableReport(ConfigurableReport):
+    slug = 'custom_configurable'
+
+
 class CustomConfigurableReportDispatcher(ReportDispatcher):
     slug = prefix = 'custom_configurable'
     map_name = 'CUSTOM_UCR'
