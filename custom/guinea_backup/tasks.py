47d8139d6a9eae2d508e536c12341e094ae46d6c
Tyler Sheffels <tsheffels@dimagi.com>
2015-09-15 17:00:10 -0400

    Only run guinea backup on production, correctly select last updated date

--- custom/guinea_backup/tasks.py
+++ custom/guinea_backup/tasks.py
@@ -13,30 +13,31 @@ GUINEA_CONTACT_TRACING_DATABASE = 'guineact-backup'
 
 @periodic_task(run_every=crontab(hour="1", day_of_week="0"), queue='background_queue')
 def copy_data_to_backup():
-    # https://<your_username>:<your_password>@commcarehq.cloudant.com/commcarehq
-    prod_couchdb_connection = 'https://{username}:{password}@commcarehq.cloudant.com/{database}'.format(
-        username=settings.COUCH_USERNAME,
-        password=settings.COUCH_PASSWORD,
-        database=settings.COUCH_DATABASE_NAME,
-    )
-    guinea_couchdb_connection = 'https://{username}:{password}@commcarehq.cloudant.com/{database}'.format(
-        username=settings.COUCH_USERNAME,
-        password=settings.COUCH_PASSWORD,
-        database=GUINEA_CONTACT_TRACING_DATABASE,
-    )
-    last_update = BackupRecord.objects.order_by('last_update')[0]
+    if settings.SERVER_ENVIRONMENT == 'production':
+        # https://<your_username>:<your_password>@commcarehq.cloudant.com/commcarehq
+        prod_couchdb_connection = 'https://{username}:{password}@commcarehq.cloudant.com/{database}'.format(
+            username=settings.COUCH_USERNAME,
+            password=settings.COUCH_PASSWORD,
+            database=settings.COUCH_DATABASE_NAME,
+        )
+        guinea_couchdb_connection = 'https://{username}:{password}@commcarehq.cloudant.com/{database}'.format(
+            username=settings.COUCH_USERNAME,
+            password=settings.COUCH_PASSWORD,
+            database=GUINEA_CONTACT_TRACING_DATABASE,
+        )
+        last_update = BackupRecord.objects.order_by('last_update')[0].last_update
 
-    call_command('copy_domain',
-                 prod_couchdb_connection,
-                 GUINEA_CONTACT_TRACING_DOMAIN,
-                 guinea_couchdb_connection,
-                 **{'since': last_update})
+        call_command('copy_domain',
+                     prod_couchdb_connection,
+                     GUINEA_CONTACT_TRACING_DOMAIN,
+                     guinea_couchdb_connection,
+                     **{'since': unicode(last_update)})
 
-    # A dumb soft assert to make sure I see this working
-    _assert = soft_assert(to='{}@{}'.format('tsheffels', 'dimagi.com'),
-                          notify_admins=False,
-                          exponential_backoff=False)
-    _assert(False)
+        # A dumb soft assert to make sure I see this working
+        _assert = soft_assert(to='{}@{}'.format('tsheffels', 'dimagi.com'),
+                              notify_admins=False,
+                              exponential_backoff=False)
+        _assert(False)
 
-    successful_insert = BackupRecord(last_update=date.now())
-    successful_insert.save()
+        successful_insert = BackupRecord(last_update=date.now())
+        successful_insert.save()
