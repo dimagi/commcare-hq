f352193de3e165c47e856c5eb03809a71136d13e
NoahCarnahan <ncarnahan@dimagi.com>
2015-09-17 17:51:22 -0400

    move translations views to separate file

---
+++ corehq/apps/app_manager/views/__init__.py
@@ -0,0 +1,6 @@
+from corehq.apps.app_manager.views.translations import (
+    upload_bulk_ui_translations,
+    download_bulk_ui_translations,
+    download_bulk_app_translations,
+    upload_bulk_app_translations,
+)
