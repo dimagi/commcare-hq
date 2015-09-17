6f00ebdbf8e6b951056a789cf6bae83adfd78a5a
Biyeun Buczyk <biyeun@dimagi.com>
2015-09-17 09:01:51 -0400

    delete old create export form views from earlier re-implementations

--- corehq/apps/export/urls.py
+++ corehq/apps/export/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import *
 from corehq.apps.export.views import (
-    CreateFormExportView,
-    CreateCaseExportView,
     CreateCustomFormExportView,
     CreateCustomCaseExportView,
     EditCustomFormExportView,
@@ -12,12 +10,10 @@ from corehq.apps.export.views import (
 
 urlpatterns = patterns(
     'corehq.apps.export.views',
-    url(r"^create/form/$", CreateFormExportView.as_view(), name=CreateFormExportView.urlname),
-    url(r"^create/case/$", CreateCaseExportView.as_view(), name=CreateCaseExportView.urlname),
-    url(r"^customize/form/$", CreateCustomFormExportView.as_view(), name=CreateCustomFormExportView.urlname),
-    url(r"^customize/case/$", CreateCustomCaseExportView.as_view(), name=CreateCustomCaseExportView.urlname),
     url(r"^custom/list/form/$", FormExportListView.as_view(), name=FormExportListView.urlname),
     url(r"^custom/list/case/$", CaseExportListView.as_view(), name=CaseExportListView.urlname),
+    url(r"^customize/form/$", CreateCustomFormExportView.as_view(), name=CreateCustomFormExportView.urlname),
+    url(r"^customize/case/$", CreateCustomCaseExportView.as_view(), name=CreateCustomCaseExportView.urlname),
     url(r"^custom/form/(?P<export_id>[\w\-]+)/download/$",
         DownloadFormExportView.as_view(), name=DownloadFormExportView.urlname),
     url(r"^custom/form/(?P<export_id>[\w\-]+)/edit/$", EditCustomFormExportView.as_view(),
