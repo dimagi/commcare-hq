2a77e5ee6208a20ff92a3562fe18a36f13238503
Daniel Roberts <droberts@dimagi.com>
2015-09-17 22:19:24 -0400

    reorganize user bulkupload _Creator

--- corehq/apps/users/bulkupload/create.py
+++ corehq/apps/users/bulkupload/create.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 import logging
 
 from couchdbkit import BulkSaveError, ResourceNotFound, MultipleResultsFound
+from dimagi.utils.decorators.memoized import memoized
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext as _
 
@@ -33,172 +34,36 @@ class _Creator(object):
         self.location_specs = location_specs
         self.task = task
         self.total = len(self.user_specs) + len(self.group_specs) + len(self.location_specs)
+        self.group_memoizer = GroupMemoizer(self.domain)
+
+    def create(self):
+        self.group_memoizer.load_all()
+        return self.create_or_update_users_groups_and_locations()
 
     def _set_progress(self, progress):
         if self.task is not None:
             DownloadBase.set_progress(self.task, progress, self.total)
 
+    @property
+    @memoized
+    def can_access_locations(self):
+        return domain_has_privilege(self.domain, privileges.LOCATIONS)
+
+    @property
+    @memoized
+    def location_cache(self):
+        return SiteCodeToLocationCache(self.domain)
+
     def create_or_update_users_groups_and_locations(self):
-        from corehq.apps.users.views.mobile import UserFieldsView
-        custom_data_validator = UserFieldsView.get_validator(self.domain)
         ret = {"errors": [], "rows": []}
 
-        group_memoizer = self.create_or_update_groups(log=ret)
-        current = len(self.group_specs)
+        self.create_or_update_groups(log=ret)
 
-        usernames = set()
-        user_ids = set()
-        allowed_groups = set(group_memoizer.groups)
-        allowed_group_names = [group.name for group in allowed_groups]
-        can_access_locations = domain_has_privilege(self.domain, privileges.LOCATIONS)
-        if can_access_locations:
-            location_cache = SiteCodeToLocationCache(self.domain)
         try:
-            for row in self.user_specs:
-                self._set_progress(current)
-                current += 1
-
-                data = row.get('data')
-                email = row.get('email')
-                group_names = map(unicode, row.get('group') or [])
-                language = row.get('language')
-                name = row.get('name')
-                password = row.get('password')
-                phone_number = row.get('phone-number')
-                uncategorized_data = row.get('uncategorized_data')
-                user_id = row.get('user_id')
-                username = row.get('username')
-                location_code = row.get('location-sms-code', '')
-
-                if password:
-                    password = unicode(password)
-                try:
-                    username = normalize_username(str(username), self.domain)
-                except TypeError:
-                    username = None
-                except ValidationError:
-                    ret['rows'].append({
-                        'username': username,
-                        'row': row,
-                        'flag': _('username cannot contain spaces or symbols'),
-                    })
-                    continue
-                status_row = {
-                    'username': raw_username(username) if username else None,
-                    'row': row,
-                }
-
-                is_active = row.get('is_active')
-                if isinstance(is_active, basestring):
-                    try:
-                        is_active = string_to_boolean(is_active)
-                    except ValueError:
-                        ret['rows'].append({
-                            'username': username,
-                            'row': row,
-                            'flag': _("'is_active' column can only contain 'true' or 'false'"),
-                        })
-                        continue
-
-                if username in usernames or user_id in user_ids:
-                    status_row['flag'] = 'repeat'
-                elif not username and not user_id:
-                    status_row['flag'] = 'missing-data'
-                else:
-                    try:
-                        if username:
-                            usernames.add(username)
-                        if user_id:
-                            user_ids.add(user_id)
-                        if user_id:
-                            user = CommCareUser.get_by_user_id(user_id, self.domain)
-                        else:
-                            user = CommCareUser.get_by_username(username)
-
-                        def is_password(password):
-                            if not password:
-                                return False
-                            for c in password:
-                                if c != "*":
-                                    return True
-                            return False
-
-                        if user:
-                            if user.domain != self.domain:
-                                raise UserUploadError(_(
-                                    'User with username %(username)r is '
-                                    'somehow in domain %(domain)r'
-                                ) % {'username': user.username, 'domain': user.domain})
-                            if username and user.username != username:
-                                user.change_username(username)
-                            if is_password(password):
-                                user.set_password(password)
-                            status_row['flag'] = 'updated'
-                        else:
-                            if len(raw_username(username)) > CommCareAccountForm.max_len_username:
-                                ret['rows'].append({
-                                    'username': username,
-                                    'row': row,
-                                    'flag': _("username cannot contain greater than %d characters" %
-                                              CommCareAccountForm.max_len_username)
-                                })
-                                continue
-                            if not is_password(password):
-                                raise UserUploadError(_("Cannot create a new user with a blank password"))
-                            user = CommCareUser.create(self.domain, username, password, commit=False)
-                            status_row['flag'] = 'created'
-                        if phone_number:
-                            user.add_phone_number(_fmt_phone(phone_number), default=True)
-                        if name:
-                            user.set_full_name(name)
-                        if data:
-                            error = custom_data_validator(data)
-                            if error:
-                                raise UserUploadError(error)
-                            user.user_data.update(data)
-                        if uncategorized_data:
-                            user.user_data.update(uncategorized_data)
-                        if language:
-                            user.language = language
-                        if email:
-                            user.email = email
-                        if is_active is not None:
-                            user.is_active = is_active
-
-                        user.save()
-                        if can_access_locations and location_code:
-                            loc = location_cache.get(location_code)
-                            if user.location_id != loc._id:
-                                # this triggers a second user save so
-                                # we want to avoid doing it if it isn't
-                                # needed
-                                user.set_location(loc)
-                        if is_password(password):
-                            # Without this line, digest auth doesn't work.
-                            # With this line, digest auth works.
-                            # Other than that, I'm not sure what's going on
-                            user.get_django_user().check_password(password)
-
-                        for group_id in Group.by_user(user, wrap=False):
-                            group = group_memoizer.get(group_id)
-                            if group.name not in group_names:
-                                group.remove_user(user, save=False)
-
-                        for group_name in group_names:
-                            if group_name not in allowed_group_names:
-                                raise UserUploadError(_(
-                                    "Can't add to group '%s' "
-                                    "(try adding it to your spreadsheet)"
-                                ) % group_name)
-                            group_memoizer.by_name(group_name).add_user(user, save=False)
-
-                    except (UserUploadError, CouchUser.Inconsistent) as e:
-                        status_row['flag'] = unicode(e)
-
-                ret["rows"].append(status_row)
+            self._do_most_of_create_users(ret)
         finally:
             try:
-                group_memoizer.save_all()
+                self.group_memoizer.save_all()
             except BulkSaveError as e:
                 _error_message = (
                     "Oops! We were not able to save some of your group changes. "
@@ -217,8 +82,6 @@ class _Creator(object):
         return ret
 
     def create_or_update_groups(self, log):
-        group_memoizer = GroupMemoizer(self.domain)
-        group_memoizer.load_all()
         group_names = set()
         for row in self.group_specs:
             group_id = row.get('id')
@@ -230,33 +93,32 @@ class _Creator(object):
             # check that group_names are unique
             if group_name in group_names:
                 log['errors'].append('Your spreadsheet has multiple groups called "%s" and only the first was processed' % group_name)
-                continue
+                return
             else:
                 group_names.add(group_name)
 
             # check that there's a group_id or a group_name
             if not group_id and not group_name:
                 log['errors'].append('Your spreadsheet has a group with no name or id and it has been ignored')
-                continue
+                return
 
             try:
                 if group_id:
-                    group = group_memoizer.get(group_id)
+                    group = self.group_memoizer.get(group_id)
                 else:
-                    group = group_memoizer.by_name(group_name)
+                    group = self.group_memoizer.by_name(group_name)
                     if not group:
-                        group = group_memoizer.create(domain=self.domain, name=group_name)
+                        group = self.group_memoizer.create(domain=self.domain, name=group_name)
             except ResourceNotFound:
                 log["errors"].append('There are no groups on CommCare HQ with id "%s"' % group_id)
             except MultipleResultsFound:
                 log["errors"].append("There are multiple groups on CommCare HQ named: %s" % group_name)
             else:
                 if group_name:
-                    group_memoizer.rename_group(group, group_name)
+                    self.group_memoizer.rename_group(group, group_name)
                 group.case_sharing = case_sharing
                 group.reporting = reporting
                 group.metadata = data
-        return group_memoizer
 
     def create_or_update_locations(self, log):
         """
@@ -301,6 +163,166 @@ class _Creator(object):
                         message=e
                     ))
                 )
+
+    def _do_most_of_create_users(self, log):
+        from corehq.apps.users.views.mobile import UserFieldsView
+        custom_data_validator = UserFieldsView.get_validator(self.domain)
+        usernames = set()
+        user_ids = set()
+        allowed_group_names = [group.name for group in self.group_memoizer.groups]
+
+        starting_progress = len(self.group_specs)
+        for i, user_spec in enumerate(self.user_specs):
+            self._set_progress(starting_progress + i)
+            self._import_single_user(
+                user_spec=user_spec,
+                log=log,
+                custom_data_validator=custom_data_validator,
+                usernames=usernames,
+                user_ids=user_ids,
+                allowed_group_names=allowed_group_names,
+            )
+
+    def _import_single_user(self, user_spec, log, custom_data_validator,
+                            usernames, user_ids, allowed_group_names):
+        data = user_spec.get('data')
+        email = user_spec.get('email')
+        group_names = map(unicode, user_spec.get('group') or [])
+        language = user_spec.get('language')
+        name = user_spec.get('name')
+        password = user_spec.get('password')
+        phone_number = user_spec.get('phone-number')
+        uncategorized_data = user_spec.get('uncategorized_data')
+        user_id = user_spec.get('user_id')
+        username = user_spec.get('username')
+        location_code = user_spec.get('location-sms-code', '')
+
+        if password:
+            password = unicode(password)
+        try:
+            username = normalize_username(str(username), self.domain)
+        except TypeError:
+            username = None
+        except ValidationError:
+            log['rows'].append({
+                'username': username,
+                'row': user_spec,
+                'flag': _('username cannot contain spaces or symbols'),
+            })
+            return
+        status_row = {
+            'username': raw_username(username) if username else None,
+            'row': user_spec,
+        }
+
+        is_active = user_spec.get('is_active')
+        if isinstance(is_active, basestring):
+            try:
+                is_active = string_to_boolean(is_active)
+            except ValueError:
+                log['rows'].append({
+                    'username': username,
+                    'row': user_spec,
+                    'flag': _("'is_active' column can only contain 'true' or 'false'"),
+                })
+                return
+
+        if username in usernames or user_id in user_ids:
+            status_row['flag'] = 'repeat'
+        elif not username and not user_id:
+            status_row['flag'] = 'missing-data'
+        else:
+            try:
+                if username:
+                    usernames.add(username)
+                if user_id:
+                    user_ids.add(user_id)
+                if user_id:
+                    user = CommCareUser.get_by_user_id(user_id, self.domain)
+                else:
+                    user = CommCareUser.get_by_username(username)
+
+                def is_password(password):
+                    if not password:
+                        return False
+                    for c in password:
+                        if c != "*":
+                            return True
+                    return False
+
+                if user:
+                    if user.domain != self.domain:
+                        raise UserUploadError(_(
+                            'User with username %(username)r is '
+                            'somehow in domain %(domain)r'
+                        ) % {'username': user.username, 'domain': user.domain})
+                    if username and user.username != username:
+                        user.change_username(username)
+                    if is_password(password):
+                        user.set_password(password)
+                    status_row['flag'] = 'updated'
+                else:
+                    if len(raw_username(username)) > CommCareAccountForm.max_len_username:
+                        log['rows'].append({
+                            'username': username,
+                            'row': user_spec,
+                            'flag': _("username cannot contain greater than %d characters" %
+                                      CommCareAccountForm.max_len_username)
+                        })
+                        return
+                    if not is_password(password):
+                        raise UserUploadError(_("Cannot create a new user with a blank password"))
+                    user = CommCareUser.create(self.domain, username, password, commit=False)
+                    status_row['flag'] = 'created'
+                if phone_number:
+                    user.add_phone_number(_fmt_phone(phone_number), default=True)
+                if name:
+                    user.set_full_name(name)
+                if data:
+                    error = custom_data_validator(data)
+                    if error:
+                        raise UserUploadError(error)
+                    user.user_data.update(data)
+                if uncategorized_data:
+                    user.user_data.update(uncategorized_data)
+                if language:
+                    user.language = language
+                if email:
+                    user.email = email
+                if is_active is not None:
+                    user.is_active = is_active
+
+                user.save()
+                if self.can_access_locations and location_code:
+                    loc = self.location_cache.get(location_code)
+                    if user.location_id != loc._id:
+                        # this triggers a second user save so
+                        # we want to avoid doing it if it isn't
+                        # needed
+                        user.set_location(loc)
+                if is_password(password):
+                    # Without this line, digest auth doesn't work.
+                    # With this line, digest auth works.
+                    # Other than that, I'm not sure what's going on
+                    user.get_django_user().check_password(password)
+
+                for group_id in Group.by_user(user, wrap=False):
+                    group = self.group_memoizer.get(group_id)
+                    if group.name not in group_names:
+                        group.remove_user(user, save=False)
+
+                for group_name in group_names:
+                    if group_name not in allowed_group_names:
+                        raise UserUploadError(_(
+                            "Can't add to group '%s' "
+                            "(try adding it to your spreadsheet)"
+                        ) % group_name)
+                    self.group_memoizer.by_name(group_name).add_user(user, save=False)
+
+            except (UserUploadError, CouchUser.Inconsistent) as e:
+                status_row['flag'] = unicode(e)
+
+        log["rows"].append(status_row)
 
 
 def _fmt_phone(phone_number):
