bd44a2cbffdd031d7cd30957f5eb2edada49d036
Cory Zue <czue@dimagi.com>
2015-09-17 20:23:20 -0400

    fix email address reference

--- corehq/apps/accounting/emails.py
+++ corehq/apps/accounting/emails.py
@@ -38,7 +38,7 @@ def send_subscription_change_alert(domain, new_subscription, old_subscription, i
 
     send_html_email_async.delay(
         email_subject,
-        sub_change_email_address(internal_change),
+        sub_change_email_address,
         render_to_string('accounting/subscription_change_email.html', email_context),
         text_content=render_to_string('accounting/subscription_change_email.txt', email_context),
     )
