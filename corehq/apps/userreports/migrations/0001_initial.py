8f935329c013b119dfbb1b82ef2aaf4f5b5b4e21
Cory Zue <czue@dimagi.com>
2015-09-20 16:40:21 -0400

    don't run migration when unit testing

--- corehq/apps/userreports/migrations/0001_initial.py
+++ corehq/apps/userreports/migrations/0001_initial.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.conf import settings
 
 from django.db import migrations
 from corehq.apps.userreports.models import DataSourceConfiguration
@@ -9,12 +10,13 @@ from dimagi.utils.couch.database import iter_docs
 
 
 def set_default_engine_ids(apps, schema_editor):
-    ucr_db = DataSourceConfiguration.get_db()
-    with IterDB(ucr_db) as iter_db:
-        for doc in iter_docs(ucr_db, DataSourceConfiguration.all_ids()):
-            if not doc.get('engine_id'):
-                doc['engine_id'] = DEFAULT_ENGINE_ID
-                iter_db.save(doc)
+    if not settings.UNIT_TESTING:
+        ucr_db = DataSourceConfiguration.get_db()
+        with IterDB(ucr_db) as iter_db:
+            for doc in iter_docs(ucr_db, DataSourceConfiguration.all_ids()):
+                if not doc.get('engine_id'):
+                    doc['engine_id'] = DEFAULT_ENGINE_ID
+                    iter_db.save(doc)
 
 
 class Migration(migrations.Migration):
