e4120aad676e059862bd98eafb195b28cd65a043
Jenny Schweers <orange.jenny@gmail.com>
2015-09-21 10:58:17 -0400

    Update name_to_url.py

--- dimagi/utils/name_to_url.py
+++ dimagi/utils/name_to_url.py
@@ -4,6 +4,5 @@ def name_to_url(name, default=""):
     url = re.sub(r'[^0-9a-z]+', '-', name.strip().lower())
     if re.search('^[0-9\-]*$', url) and default:
         url = "{}-{}".format(default, url)
-    url = re.sub(r'^-+', '', url)
-    url = re.sub(r'-+$', '', url)
+    url = url.strip("-")
     return url
