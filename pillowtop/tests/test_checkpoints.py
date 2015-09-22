3c53569571228cfd2b95a71aa72d2513505ba0df
Cory Zue <czue@dimagi.com>
2015-09-22 16:09:44 -0400

    tests for checkpoint manager instance

--- pillowtop/tests/test_checkpoints.py
+++ pillowtop/tests/test_checkpoints.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.test import SimpleTestCase, override_settings
-from pillowtop.checkpoints.manager import PillowCheckpointManager
+from pillowtop.checkpoints.manager import PillowCheckpointManager, PillowCheckpointManagerInstance
 from pillowtop.checkpoints.util import get_machine_id
 from pillowtop.dao.mock import MockDocumentStore
+from pillowtop.exceptions import PillowtopCheckpointReset
 
 
 class PillowCheckpointTest(SimpleTestCase):
@@ -20,3 +21,35 @@ class PillowCheckpointTest(SimpleTestCase):
         checkpoint_manager = PillowCheckpointManager(MockDocumentStore())
         checkpoint = checkpoint_manager.get_or_create_checkpoint('some-id')
         self.assertEqual('0', checkpoint['seq'])
+
+
+class PillowCheckpointManagerInstanceTest(SimpleTestCase):
+
+    def setUp(self):
+        self._checkpoint_id = 'test-checkpoint-id'
+        self._dao = MockDocumentStore()
+        self._manager = PillowCheckpointManagerInstance(self._dao, self._checkpoint_id)
+
+    def test_checkpoint_id(self):
+        self.assertEqual(self._checkpoint_id, self._manager.checkpoint_id)
+
+    def test_create_initial_checkpoint(self):
+        checkpoint = self._manager.get_or_create_checkpoint()
+        self.assertEqual('0', checkpoint['seq'])
+
+    def test_db_changes_returned(self):
+        self._manager.get_or_create_checkpoint()
+        self._dao.save_document(self._checkpoint_id, {'seq': '1'})
+        checkpoint = self._manager.get_or_create_checkpoint()
+        self.assertEqual('1', checkpoint['seq'])
+
+    def test_verify_unchanged_ok(self):
+        self._manager.get_or_create_checkpoint()
+        checkpoint = self._manager.get_or_create_checkpoint(verify_unchanged=True)
+        self.assertEqual('0', checkpoint['seq'])
+
+    def test_verify_unchanged_fail(self):
+        self._manager.get_or_create_checkpoint()
+        self._dao.save_document(self._checkpoint_id, {'seq': '1'})
+        with self.assertRaises(PillowtopCheckpointReset):
+            self._manager.get_or_create_checkpoint(verify_unchanged=True)
