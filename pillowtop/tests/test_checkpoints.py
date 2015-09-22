3cc4edd988fe92cda5534ee2983e8e15cf2e1679
Cory Zue <czue@dimagi.com>
2015-09-22 14:02:41 -0400

    add test

--- pillowtop/tests/test_checkpoints.py
+++ pillowtop/tests/test_checkpoints.py
@@ -6,5 +6,10 @@ from pillowtop.checkpoints.util import get_machine_id
 class PillowCheckpointTest(SimpleTestCase):
 
     @override_settings(PILLOWTOP_MACHINE_ID='test-ptop')
-    def test_get_machine_id(self):
+    def test_get_machine_id_settings(self):
         self.assertEqual('test-ptop', get_machine_id())
+
+    def test_get_machine_id(self):
+        # since this is machine dependent just ensure that this returns something
+        # and doesn't crash
+        self.assertTrue(bool(get_machine_id()))
