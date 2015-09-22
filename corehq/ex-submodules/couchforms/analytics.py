130313f0d655c52e5a3b4be71da584bd2488634b
Daniel Roberts <droberts@dimagi.com>
2015-09-22 15:56:18 -0400

    have domain_has_submission_in_last_30_days use get_last_form_submission_received

--- corehq/ex-submodules/couchforms/analytics.py
+++ corehq/ex-submodules/couchforms/analytics.py
@@ -5,23 +5,11 @@ from couchforms.models import XFormInstance
 
 
 def domain_has_submission_in_last_30_days(domain):
-    from corehq.apps.reports.util import make_form_couch_key
-    key = make_form_couch_key(domain)
-    results = XFormInstance.get_db().view(
-        'reports_forms/all_forms',
-        startkey=key + [{}],
-        endkey=key,
-        descending=True,
-        reduce=False,
-        include_docs=False,
-        limit=1,
-        stale=stale_ok(),
-    ).all()
+    last_submission = get_last_form_submission_received(domain)
     # if there have been any submissions in the past 30 days
-    if len(results) > 0:
-        received_on = iso_string_to_datetime(results[0]['key'][2])
+    if last_submission:
         _30_days = datetime.timedelta(days=30)
-        return datetime.datetime.utcnow() <= received_on + _30_days
+        return datetime.datetime.utcnow() <= last_submission + _30_days
     else:
         return False
 
