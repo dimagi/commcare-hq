0d916de23f18eedc4551755f4a1aa59b7d25b040
Tyler Sheffels <tsheffels@dimagi.com>
2015-09-23 17:34:21 -0400

    Name logger for doc conflict deletion

--- corehq/apps/cleanup/management/commands/delete_doc_conflicts.py
+++ corehq/apps/cleanup/management/commands/delete_doc_conflicts.py
@@ -5,23 +5,24 @@ from corehq.util.couch import categorize_bulk_save_errors
 from dimagi.utils.couch.database import get_db
 from django.core.management.base import BaseCommand, LabelCommand
 
+logger = logging.getLogger(__name__)
 
 def bulk_delete(db, docs):
     if not docs:
         return
 
-    logging.info("Deleting {} doc revisions".format(len(docs)))
+    logger.info("Deleting {} doc revisions".format(len(docs)))
     try:
         db.bulk_delete(docs)
     except BulkSaveError as e:
         errors = categorize_bulk_save_errors(e)
         successes = errors.pop(None, [])
         conflicts = errors.pop('conflict', [])
-        logging.error("BulkSaveError: {} successful, {} conflicts".format(len(successes), len(conflicts)))
+        logger.error("BulkSaveError: {} successful, {} conflicts".format(len(successes), len(conflicts)))
         for error, results in errors.items():
-            logging.error(results)
+            logger.error(results)
     else:
-        logging.info('{} doc revisions deleted'.format(len(docs)))
+        logger.info('{} doc revisions deleted'.format(len(docs)))
 
 
 class Command(BaseCommand):
@@ -43,15 +44,15 @@ class Command(BaseCommand):
             results = db.view('doc_conflicts/conflicts', reduce=False, limit=options['batch'], include_docs=True, conflicts=True)
             total = results.total_rows
             if not total:
-                logging.info('Document conflict deletion complete')
+                logger.info('Document conflict deletion complete')
                 return
-            logging.info('Processing {} of {} docs'.format(len(results), total))
+            logger.info('Processing {} of {} docs'.format(len(results), total))
             to_delete = []
             for row in results:
                 doc = row['doc']
                 conflicts = doc.get('_conflicts', [])
                 doc_id = doc['_id']
-                logging.info('Deleting {} conflicts for doc: {}'.format(len(conflicts), doc_id))
+                logger.info('Deleting {} conflicts for doc: {}'.format(len(conflicts), doc_id))
                 for rev in conflicts:
                     to_delete.append({
                         '_id': doc_id,
