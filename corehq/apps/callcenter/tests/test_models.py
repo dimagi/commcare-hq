9be1524808f8c6b639ebfe4a7d278910076134f6
Simon Kelly <skelly@dimagi.com>
2015-09-23 10:10:40 -0400

    add real example test

--- corehq/apps/callcenter/tests/test_models.py
+++ corehq/apps/callcenter/tests/test_models.py
@@ -1,5 +1,5 @@
 from corehq.apps.callcenter.const import WEEK1, WEEK0, MONTH0
-from corehq.apps.callcenter.models import TypedIndicator, ByTypeIndicator
+from corehq.apps.callcenter.models import TypedIndicator, ByTypeIndicator, CallCenterIndicatorConfig, BasicIndicator
 from django.test import SimpleTestCase
 
 
@@ -17,5 +17,61 @@ class ModelTests(SimpleTestCase):
             WEEK0: {'dog', 'cat'},
             WEEK1: {'dog', 'canary', 'fish'},
             MONTH0: {'fish'},
+        })
+
+    def _get_indicator_slugs(self, config, all_types=None):
+        def basic_slugs(key, indicator_conf):
+            for date_range in indicator_conf.date_ranges:
+                yield '{}_{}'.format(key, date_range)
+
+        def typed_slugs(key, types, indicator_conf):
+            for date_range in indicator_conf.date_ranges:
+                for type_ in types:
+                    yield '{}_{}_{}'.format(key, type_, date_range)
+
+        slugs = []
+        if config.forms_submitted.active:
+            slugs.extend(basic_slugs('forms_submitted', config.forms_submitted))
+
+        for key in ['cases_total', 'cases_active', 'cases_opened', 'cases_closed']:
+            indicator_config = getattr(config, key)
+            if indicator_config.active:
+                if indicator_config.total.active:
+                    slugs.extend(basic_slugs(key, indicator_config.total))
+                if indicator_config.all_types and all_types:
+                    slugs.extend(typed_slugs(key, all_types, indicator_config))
+                else:
+                    for type_ in indicator_config.types:
+                        if type_.active:
+                            slugs.extend(typed_slugs(key, [type_.type], type_))
+
+        return slugs
+
+    def test_real_example(self):
+        config = CallCenterIndicatorConfig(
+            domain='domain',
+            forms_submitted=BasicIndicator(active=True, include_legacy=False, date_ranges=[MONTH0]),
+            cases_total=ByTypeIndicator(
+                total=BasicIndicator(active=False),
+                types=[
+                    TypedIndicator(active=True, date_ranges=[MONTH0], type='caregiver'),
+                    TypedIndicator(active=True, date_ranges=[MONTH0], type='beneficiary'),
+                ]
+            ),
+            cases_active=ByTypeIndicator(
+                total=BasicIndicator(active=False),
+                types=[
+                    TypedIndicator(active=True, date_ranges=[MONTH0], type='caregiver'),
+                    TypedIndicator(active=True, date_ranges=[MONTH0], type='beneficiary'),
+                ]
+            )
+        )
+
+        self.assertEqual(set(self._get_indicator_slugs(config)), {
+            'forms_submitted_month0',
+            'cases_total_caregiver_month0',
+            'cases_active_caregiver_month0',
+            'cases_total_beneficiary_month0',
+            'cases_active_beneficiary_month0'
         })
 
