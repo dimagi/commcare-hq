16fb2d1e853ea993a583f59e14206c77cbe25869
Cory Zue <czue@dimagi.com>
2015-09-24 13:13:05 -0400

    pull out topics into a module

--- corehq/apps/change_feed/pillow.py
+++ corehq/apps/change_feed/pillow.py
@@ -3,6 +3,7 @@ from kafka import KafkaClient, KeyedProducer
 from casexml.apps.case.models import CommCareCase
 from corehq.apps.change_feed import data_sources
 from corehq.apps.change_feed.models import ChangeMeta
+from corehq.apps.change_feed.topics import get_topic
 from couchforms.models import all_known_formlike_doc_types
 from pillowtop.couchdb import CachedCouchDB
 from pillowtop.listener import PythonPillow
@@ -34,7 +35,7 @@ class ChangeFeedPillow(PythonPillow):
                 is_deletion=change.deleted,
             )
             self._producer.send_messages(
-                bytes(_get_topic(document_type)),
+                bytes(get_topic(document_type)),
                 bytes(change_meta.domain),
                 bytes(json.dumps(change_meta.to_json())),
             )
@@ -51,10 +52,3 @@ def _get_document_subtype(document_or_none):
     elif type in all_known_formlike_doc_types():
         return document_or_none.get('xmlns', None)
     return None
-
-
-def _get_topic(document_type):
-    if document_type == 'CommCareCase':
-        return 'case'
-    elif document_type in all_known_formlike_doc_types():
-        return 'form'
