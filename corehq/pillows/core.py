7ad6694659c123c222ab95a03918ca0f1e37f166
Cory Zue <czue@dimagi.com>
2015-09-28 12:17:14 -0400

    delete unused pillow classes

--- corehq/pillows/core.py
+++ corehq/pillows/core.py
@@ -1,8 +1,3 @@
-from auditcare.models import AuditEvent
-from couchforms.models import XFormInstance
-from couchlog.models import ExceptionRecord
-from pillowtop.listener import LogstashMonitoringPillow
-from django.conf import settings
 
 
 DATE_FORMATS_ARR = ["yyyy-MM-dd",
@@ -23,27 +18,3 @@ DATE_FORMATS_ARR = ["yyyy-MM-dd",
 #elasticsearch Illegal pattern component: t
 #no builtin types for || joins
 DATE_FORMATS_STRING = '||'.join(DATE_FORMATS_ARR)
-
-class AuditcarePillow(LogstashMonitoringPillow):
-    endpoint_host = settings.LOGSTASH_HOST
-    endpoint_port = settings.LOGSTASH_AUDITCARE_PORT
-    document_class = AuditEvent
-    couch_filter = 'auditcare/auditdocs'
-
-
-class CouchlogPillow(LogstashMonitoringPillow):
-    endpoint_host = settings.LOGSTASH_HOST
-    endpoint_port = settings.LOGSTASH_COUCHLOG_PORT
-    document_class = ExceptionRecord
-    couch_filter = 'couchlog/couchlogs'
-
-
-class DevicelogPillow(LogstashMonitoringPillow):
-    endpoint_host = settings.LOGSTASH_HOST
-    endpoint_port = settings.LOGSTASH_DEVICELOG_PORT
-    document_class = XFormInstance
-    couch_filter = 'couchforms/devicelogs'
-
-
-
-
