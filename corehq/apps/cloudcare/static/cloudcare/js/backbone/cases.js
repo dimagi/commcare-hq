18d6b443e88f9e78028ae220b4bc54067bd40c4f
Ben Rudolph <brudolph@dimagi.com>
2015-09-30 18:39:30 -0400

    Fix case crumbs

--- corehq/apps/cloudcare/static/cloudcare/js/backbone/cases.js
+++ corehq/apps/cloudcare/static/cloudcare/js/backbone/cases.js
@@ -390,7 +390,7 @@ cloudCare.CaseSelectionModel = Backbone.Model.extend({
 
 cloudCare.CaseSelectionView = Backbone.View.extend({
     el: $("#case-crumbs"),
-    template: _.template($("#cloudcare-breadcrumbs").html()),
+    template: _.template($("#template-crumbs").html()),
 
     initialize: function (){
         var self = this;
