3493e17fdbfe4c7faec49f159eac9efd4389a13c
NoahCarnahan <ncarnahan@dimagi.com>
2015-09-30 13:24:30 -0400

    Fix string escaping issue by changing html attribute delimiter to single quotes

--- corehq/apps/userreports/reports/builder/forms.py
+++ corehq/apps/userreports/reports/builder/forms.py
@@ -102,11 +102,11 @@ class QuestionSelect(Widget):
         final_attrs = self.build_attrs(attrs, name=name)
 
         return format_html(
-            '<input{0} data-bind="'
+            '<input{0} data-bind=\''
             '   questionsSelect: {1},'
-            '   value: \'{2}\','
-            '   optionsCaption: \' \''
-            '"/>',
+            '   value: "{2}",'
+            '   optionsCaption: " "'
+            '\'/>',
             flatatt(final_attrs),
             mark_safe(self.render_options(choices)),
             value
