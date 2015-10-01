3b432cda3567a00d24beb483270f57fb2b410822
Cory Zue <czue@dimagi.com>
2015-10-01 18:10:28 +0530

    add more methods and tests

--- corehq/apps/users/analytics.py
+++ corehq/apps/users/analytics.py
@@ -31,3 +31,26 @@ def _get_count_of_commcare_users_in_domain(active_flag, domain):
     ).one()
     return result['value'] if result else 0
 
+
+def get_active_commcare_users_in_domain(domain, start_at=0, limit=None):
+    return _get_commcare_users_in_domain('active', domain, start_at, limit)
+
+
+def get_inactive_commcare_users_in_domain(domain, start_at=0, limit=None):
+    return _get_commcare_users_in_domain('inactive', domain, start_at, limit)
+
+
+def _get_commcare_users_in_domain(active_flag, domain, start_at, limit):
+    extra_args = {}
+    if start_at != 0:
+        extra_args['skip'] = start_at
+    if limit is not None:
+        extra_args['limit'] = limit
+    return CommCareUser.view("users/by_domain",
+        reduce=False,
+        include_docs=True,
+        startkey=[active_flag, domain, 'CommCareUser'],
+        endkey=[active_flag, domain, 'CommCareUser', {}],
+        stale=stale_ok(),
+        **extra_args
+    ).all()
--- corehq/apps/users/tests/test_analytics.py
+++ corehq/apps/users/tests/test_analytics.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from corehq.apps.users.analytics import update_analytics_indexes, get_count_of_active_commcare_users_in_domain, \
-    get_count_of_inactive_commcare_users_in_domain
+    get_count_of_inactive_commcare_users_in_domain, get_active_commcare_users_in_domain, \
+    get_inactive_commcare_users_in_domain
 from corehq.apps.users.dbaccessors.all_commcare_users import delete_all_users
 from corehq.apps.users.models import CommCareUser, WebUser
 
@@ -46,3 +47,19 @@ class UserAnalyticsTest(TestCase):
 
     def test_get_count_of_inactive_commcare_users_in_domain_no_results(self):
         self.assertEqual(0, get_count_of_inactive_commcare_users_in_domain('missing'))
+
+    def test_get_active_commcare_users_in_domain(self):
+        users = get_active_commcare_users_in_domain('test')
+        self.assertEqual(2, len(users))
+        self.assertEqual(set(['active', 'active2']), set([u.username for u in users]))
+
+    def test_get_inactive_commcare_users_in_domain(self):
+        users = get_inactive_commcare_users_in_domain('test')
+        self.assertEqual(1, len(users))
+        self.assertEqual('inactive', users[0].username)
+
+    def test_get_active_commcare_users_in_domain_no_results(self):
+        self.assertEqual(0, len(get_active_commcare_users_in_domain('missing')))
+
+    def test_get_inactive_commcare_users_in_domain_no_results(self):
+        self.assertEqual(0, len(get_inactive_commcare_users_in_domain('missing')))
