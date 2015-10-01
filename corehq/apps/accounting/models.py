7a9400c91d1d2278f7a81b3e91d2c4f5e9bad0d6
Nick Pellegrino <npellegrino@dimagi.com>
2015-10-01 15:06:09 -0400

    raise error if contact_emails == ''

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -1586,8 +1586,7 @@ class InvoiceBase(models.Model):
     @property
     def contact_emails(self):
         contact_emails = self.account.billingcontactinfo.emails if self.account.billingcontactinfo else None
-        contact_emails = (contact_emails.split(',')
-                          if contact_emails is not None else [])
+        contact_emails = contact_emails.split(',') if contact_emails else []
         if not contact_emails:
             admins = WebUser.get_admins_by_domain(
                 self.get_domain()
