267ab57a1e0a586e9b3b7251e2ce175b6570fca7
Cory Zue <czue@dimagi.com>
2015-10-01 17:08:00 +0530

    tweaks to checkpoint reset
    - don't abort when answering no
    - sort pillows alphabetically

--- corehq/apps/hqadmin/management/commands/fix_checkpoints_from_file.py
+++ corehq/apps/hqadmin/management/commands/fix_checkpoints_from_file.py
@@ -1,6 +1,6 @@
 import json
 from django.core.management import BaseCommand, CommandError
-from corehq.apps.hqadmin.management.commands.fix_checkpoint_after_rewind import confirm, set_checkpoint
+from corehq.apps.hqadmin.management.commands.fix_checkpoint_after_rewind import set_checkpoint
 from pillowtop import get_pillow_by_name
 
 
@@ -12,13 +12,18 @@ class Command(BaseCommand):
         with open(filename) as f:
             checkpoint_map = json.loads(f.read())
 
-        for pillow_name, checkpoint_to_set in checkpoint_map.items():
+        for pillow_name in sorted(checkpoint_map.keys()):
+            checkpoint_to_set = checkpoint_map[pillow_name]
             pillow = get_pillow_by_name(pillow_name)
             if not pillow:
                 raise CommandError("No pillow found with name: {}".format(pillow_name))
 
             old_seq = pillow.get_checkpoint()['seq']
-            confirm("\nReset checkpoint for '{}' pillow from:\n\n{}\n\nto\n\n{}\n\n".format(
-                pillow_name, old_seq, checkpoint_to_set
-            ))
+            msg = "\nReset checkpoint for '{}' pillow from:\n\n{}\n\nto\n\n{}\n\n".format(
+                pillow_name, old_seq, checkpoint_to_set,
+            )
+            input = raw_input("{} Type ['y', 'yes'] to continue.\n".format(msg))
+            if input not in ['y', 'yes']:
+                print 'skipped'
+                continue
             set_checkpoint(pillow, checkpoint_to_set)
