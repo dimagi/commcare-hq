357f7cc074b88bdd9bec953ffff40c0d79add4eb
Ben Rudolph <brudolph@dimagi.com>
2015-10-02 17:14:00 -0400

    jquery-ui and database with bower

--- settings.py
+++ settings.py
@@ -92,7 +92,6 @@ LOCALE_PATHS = (
 # Do not change, there's a weird bug with Django 1.7 that requires this to be bower_components when using
 # collectstatic
 BOWER_COMPONENTS_ROOT = os.path.join(FILEPATH, 'bower_components')
-
 BOWER_PATH = '/usr/local/bin/bower'
 
 STATICFILES_FINDERS = (
@@ -1190,6 +1189,7 @@ BOWER_CORE_APPS = (
     'jquery-form#3.45.0',
     'jquery.cookie#1.4.1',
     'jquery-timeago#1.2.0',
+    'jquery-ui#1.11.4',
     'angular#1.4.4',
     'angular-route#1.4.4',
     'angular-resource#1.4.4',
@@ -1203,6 +1203,7 @@ BOWER_CORE_APPS = (
     'less#1.7.3',
     'backbone#0.9.1',
     'bootstrap-daterangepicker#2.1.13',
+    'datatables#1.10.9',
 )
 
 BOWER_TEST_APPS = (
