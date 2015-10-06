186b18543268627b6862ac4cc8f1ca48209c8289
NoahCarnahan <ncarnahan@dimagi.com>
2015-10-06 07:39:58 -0400

    [WIP] start converting UCR pages to bootstrap 3

--- settings.py
+++ settings.py
@@ -116,6 +116,7 @@ BOWER_INSTALLED_APPS = (
     'bootstrap-daterangepicker#2.1.13',
     'd3#3.0',
     'nvd3#1.1.10-beta',
+    'datatables#1.10.9',
     'dangrossman/bootstrap-daterangepicker#7435612692b33b85c72539d37c59bb7edd420c6f',
     'jquery-ui#1.11.4',
 )
