290a213c0bbccd4f3537897e91a5534f357825a5
Cory Zue <czue@dimagi.com>
2015-10-06 18:47:57 +0530

    add require_elasticsearch decorator

---
+++ testapps/test_pillowtop/tests/utils.py
@@ -0,0 +1,16 @@
+import functools
+from elasticsearch import exceptions as elasticsearch_exceptions
+from requests import exceptions as requests_exceptions
+
+
+def require_elasticsearch(fn):
+    @functools.wraps(fn)
+    def decorated(*args, **kwargs):
+        try:
+            return fn(*args, **kwargs)
+        except (requests_exceptions.ConnectionError, elasticsearch_exceptions.ConnectionError):
+            pass
+    return decorated
+
+
+
