da26478f70dd798ba1e60db194e151142338e266
Norman Hooper <nhooper@dimagi.com>
2015-10-06 19:41:49 +0200

    A few doctests

---
+++ custom/openclinica/tests.py
@@ -0,0 +1,8 @@
+from custom.openclinica.utils import mk_oc_username, get_matching_start, oc_format
+
+
+__test__ = {
+    'test_mk_oc_username': mk_oc_username,
+    'test_get_matching_start': get_matching_start,
+    'test_oc_format': oc_format,
+}
