45fa907ce7696105a01bd986cf54304156f47453
Cory Zue <czue@dimagi.com>
2015-10-06 17:32:53 +0530

    skip test, remove db comparison, add utility function to regenerate file

--- testapps/test_pillowtop/tests/test_settings.py
+++ testapps/test_pillowtop/tests/test_settings.py
@@ -1,4 +1,5 @@
 import os
+from unittest import skip
 from django.conf import settings
 from django.test import TestCase
 import json
@@ -23,17 +24,23 @@ class PillowtopSettingsTest(TestCase, TestFileMixin):
     def tearDownClass(cls):
         settings.PILLOWTOPS = cls._PILLOWTOPS
 
+    @skip('this test depends on elasticsearch because pillows depend on elasticsearch')
     def test_instantiate_all(self):
         all_pillows = get_all_pillow_instances()
         expected_meta = self.get_json('all-pillow-meta')
+
         for pillow in all_pillows:
             self.assertEqual(expected_meta[pillow.__class__.__name__], _pillow_meta_from_instance(pillow))
+
+    def _rewrite_file(self, pillows):
+        # utility that should only be called manually
+        with open(self.get_path('all-pillow-meta', 'json'), 'w') as f:
+            f.write(json.dumps({p.__class__.__name__: _pillow_meta_from_instance(p) for p in pillows}, indent=4))
 
 
 def _pillow_meta_from_instance(pillow_instance):
     props = {
         'class_name': pillow_instance.__class__.__name__,
-        'db_name': pillow_instance.couch_db.dbname,
         'document_class': pillow_instance.document_class.__name__ if pillow_instance.document_class else None,
         'couch_filter': pillow_instance.couch_filter,
         'include_docs': pillow_instance.include_docs,
