bebe60df888bf8594526f9b20bbeeb44bbd202bf
Daniel Roberts <droberts@dimagi.com>
2015-10-07 15:59:11 +0530

    have soft_assert suppress mail_admins on DEBUG

--- corehq/util/soft_assert/api.py
+++ corehq/util/soft_assert/api.py
@@ -68,14 +68,16 @@ def soft_assert(to=None, notify_admins=False,
 
     if to and notify_admins:
         def send(info):
-            _send_message(info, backend=mail_admins)
+            if not settings.DEBUG:
+                _send_message(info, backend=mail_admins)
             _send_message(info, backend=send_to_recipients)
     elif to:
         def send(info):
             _send_message(info, backend=send_to_recipients)
     elif notify_admins:
         def send(info):
-            _send_message(info, backend=mail_admins)
+            if not settings.DEBUG:
+                _send_message(info, backend=mail_admins)
     else:
         raise ValueError('You must call soft assert with either a '
                          'list of recipients or notify_admins=True')
