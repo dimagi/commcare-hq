bd857e500dafc11b3853cc0bbabd99daaf36835a
Cory Zue <czue@dimagi.com>
2015-10-07 13:48:32 +0530

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pillowtop',
-    version='0.1.6',
+    version='0.1.7',
     description='A couchdbkit changes listener for doing backend processing',
     author='Dimagi',
     author_email='dev@dimagi.com',
