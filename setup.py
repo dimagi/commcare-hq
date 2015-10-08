7b87fd776b56d727647e2c43cea44b21248546d1
Cory Zue <czue@dimagi.com>
2015-10-08 15:05:47 +0530

    bump required ptop version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     test_loader='unittest2:TestLoader',
     install_requires=[
         'jsonobject-couchdbkit>=0.6.5.2',
-        'pillowtop>=0.1.4',
+        'pillowtop>=0.1.7',
         'dimagi-utils>=1.2.1',
         'pytz',
         'SQLAlchemy==0.8.2',
