024a7fbe4a77cb2cd8550242d78e3e0c3747f07d
Cory Zue <czue@dimagi.com>
2015-10-08 16:37:07 +0530

    test for dict settings format

--- pillowtop/tests/test_config.py
+++ pillowtop/tests/test_config.py
@@ -10,3 +10,15 @@ class PillowConfigTest(SimpleTestCase):
         self.assertEqual('my-section', config.section)
         self.assertEqual(class_name, config.class_name)
         self.assertEqual(class_name, config.instance_generator)
+
+    def test_from_dict(self):
+        pillow_config = {
+            'class': 'my.cool.Pillow',
+            'instance': 'my.instance.method',
+            'name': 'MyPillow',
+        }
+        config = get_pillow_config_from_setting('my-section', pillow_config)
+        self.assertEqual('my-section', config.section)
+        self.assertEqual(pillow_config['class'], config.class_name)
+        self.assertEqual(pillow_config['instance'], config.instance_generator)
+        self.assertEqual(pillow_config['name'], config.name)
