6f08fcf82af390412f40b846345a3a7d2c8586cb
Simon Kelly <skelly@dimagi.com>
2015-10-12 17:05:57 +0200

    test_dynamic_properties

--- corehq/ex-submodules/casexml/apps/case/tests/test_dynamic_properties.py
+++ corehq/ex-submodules/casexml/apps/case/tests/test_dynamic_properties.py
@@ -27,3 +27,29 @@ class DynamicPropertiesTest(SimpleTestCase):
         props_dict = dict(props)
         self.assertEqual('some property', props_dict['new_property'])
         self.assertEqual('some other property', props_dict['bar'])
+
+
+class GenericDynamicPropertiesTest(SimpleTestCase):
+
+    def test_normal(self):
+        case = CommCareCase(foo='some property', bar='some other property').to_generic()
+        props = case.dynamic_case_properties()
+        self.assertEqual(2, len(props))
+        props_dict = dict(props)
+        self.assertEqual('some property', props_dict['foo'])
+        self.assertEqual('some other property', props_dict['bar'])
+
+    def test_subclass(self):
+        class CaseWithNewProperty(CommCareCase):
+            new_property = StringProperty()
+
+            class Meta:
+                # For some reason this is necessary for travis
+                app_label = "case"
+
+        case = CaseWithNewProperty(new_property='some property', bar='some other property').to_generic()
+        props = case.dynamic_case_properties()
+        self.assertEqual(2, len(props))
+        props_dict = dict(props)
+        self.assertEqual('some property', props_dict['new_property'])
+        self.assertEqual('some other property', props_dict['bar'])
