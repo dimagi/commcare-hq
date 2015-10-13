f50f7f8c12f512bdf155f2f2572a40a360c9da5b
Cory Zue <czue@dimagi.com>
2015-10-13 12:11:29 +0530

    bump fluff's own version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pillowfluff',
-    version='0.0.4',
+    version='0.0.5',
     description='Map over CouchDB changes feed built to run on Pillowtop',
     author='Dimagi',
     author_email='information@dimagi.com',
