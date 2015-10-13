2fad8ae06b5b04a0d00337ee32606e6761d87c55
Cory Zue <czue@dimagi.com>
2015-10-13 12:00:54 +0530

    bump requirements

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dimagi-utils',
-    version='1.2.2',
+    version='1.2.3',
     description='Dimagi Shared Utilities',
     author='Dimagi',
     author_email='dev@dimagi.com',
@@ -12,8 +12,8 @@ setup(
     test_suite='dimagi.test_utils',
     test_loader='unittest2:TestLoader',
     install_requires=[
-        'jsonobject-couchdbkit==0.6.5.7',
-        'django<1.8',
+        'jsonobject-couchdbkit>=0.7.0.1',
+        'django==1.7.10',
         'django-redis<=3.8',
         'mock>=0.8.0',
         'openpyxl',
