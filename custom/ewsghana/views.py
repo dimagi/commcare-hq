bef20bcc0b57d228be5734258846f78dfe5673aa
kkrampa <kkrampa@soldevelo.com>
2015-10-15 19:22:48 +0200

    Added way to resync users with more than one phone number

--- custom/ewsghana/views.py
+++ custom/ewsghana/views.py
@@ -25,7 +25,8 @@ from custom.ewsghana.reports.specific_reports.stock_status_report import Stockou
 from custom.ewsghana.reports.stock_levels_report import InventoryManagementData, StockLevelsReport
 from custom.ewsghana.stock_data import EWSStockDataSynchronization
 from custom.ewsghana.tasks import ews_bootstrap_domain_task, ews_clear_stock_data_task, \
-    delete_last_migrated_stock_data, convert_user_data_fields_task, migrate_email_settings
+    delete_last_migrated_stock_data, convert_user_data_fields_task, migrate_email_settings, \
+    fix_users_with_more_than_one_phone_number
 from custom.ewsghana.utils import make_url, has_input_stock_permissions
 from custom.ilsgateway.views import GlobalStats
 from custom.logistics.tasks import add_products_to_loc, locations_fix, resync_web_users
@@ -244,6 +245,13 @@ def ews_add_products_to_locs(request, domain):
 @require_POST
 def migrate_email_settings_view(request, domain):
     migrate_email_settings.delay(domain)
+    return HttpResponse('OK')
+
+
+@domain_admin_required
+@require_POST
+def fix_sms_users(request, domain):
+    fix_users_with_more_than_one_phone_number.delay(domain)
     return HttpResponse('OK')
 
 
