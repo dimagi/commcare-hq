c3d6123726030507decba4864c723c422628359a
Biyeun Buczyk <biyeun@dimagi.com>
2015-10-19 13:15:11 -0400

    lint

--- corehq/apps/style/static/style/js/daterangepicker.config.js
+++ corehq/apps/style/static/style/js/daterangepicker.config.js
@@ -6,16 +6,16 @@ $(function () {
     $.fn.createBootstrap3DateRangePicker = function(range_labels, separator) {
         var now = moment();
         var ranges = {};
-        ranges[range_labels['last_7_days']] = [
+        ranges[range_labels.last_7_days] = [
             moment().subtract('7', 'days').startOf('days')
         ];
 
-        ranges[range_labels['last_month']] = [
+        ranges[range_labels.last_month] = [
             moment().subtract('1', 'months').startOf('month'),
             moment().subtract('1', 'months').endOf('month')
         ];
 
-        ranges[range_labels['last_30_days']] = [
+        ranges[range_labels.last_30_days] = [
             moment().subtract('30', 'days').startOf('days')
         ];
         $(this).daterangepicker({
@@ -30,9 +30,9 @@ $(function () {
     $.fn.createBootstrap3DefaultDateRangePicker = function () {
         this.createBootstrap3DateRangePicker(
             {
-                'last_7_days': 'Last 7 Days',
-                'last_month': 'Last Month',
-                'last_30_days': 'Last 30 Days'
+                last_7_days: 'Last 7 Days',
+                last_month: 'Last Month',
+                last_30_days: 'Last 30 Days'
             },
             this.getDateRangeSeparator()
         )
