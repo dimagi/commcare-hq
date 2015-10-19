fb432c6e9836b0e65b00172b8570f3d4369ee679
Farid Rener <proteusvacuum@gmail.com>
2015-10-19 18:59:07 -0400

    Add shared mobile and HQ tests
    updates sniffer to reload when json files are saved

--- scent.py
+++ scent.py
@@ -1,5 +1,14 @@
+import os
 from subprocess import call
-from sniffer.api import runnable
+
+from sniffer.api import file_validator, runnable, select_runnable
+
+
+@select_runnable('execute_tests')
+@file_validator
+def py_files(filename):
+    return (filename.endswith('.py') or filename.endswith('.json')
+            and not os.path.basename(filename).startswith('.'))
 
 
 @runnable
