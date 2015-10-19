716835b22cda1346c55279e44ac641ca2ad4aa77
Cal Ellowitz <calellowitz@gmail.com>
2015-10-19 18:16:56 -0400

    fixing lint errors

--- corehq/ex-submodules/soil/views.py
+++ corehq/ex-submodules/soil/views.py
@@ -39,7 +39,7 @@ def heartbeat_status(request):
 def ajax_job_poll(request, download_id, template="soil/partials/dl_status.html"):
     app_download = False
     if 'apps' in request.META['HTTP_REFERER']:
-         app_download = True
+        app_download = True
     try:
         context = get_download_context(download_id, check_state=True, app_download=app_download)
     except TaskFailedError as e:
