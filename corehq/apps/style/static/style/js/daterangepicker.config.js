eed71327e5f2939672c70f8612564fa3c401d61d
Biyeun Buczyk <biyeun@dimagi.com>
2015-10-19 17:07:56 -0400

    lint and initialization fix

--- corehq/apps/style/static/style/js/daterangepicker.config.js
+++ corehq/apps/style/static/style/js/daterangepicker.config.js
@@ -53,9 +53,9 @@ $(function () {
     $.fn.createBootstrap3DefaultDateRangePicker = function () {
         this.createBootstrap3DateRangePicker(
             {
-                'last_7_days': 'Last 7 Days',
-                'last_month': 'Last Month',
-                'last_30_days': 'Last 30 Days'
+                last_7_days: 'Last 7 Days',
+                last_month: 'Last Month',
+                last_30_days: 'Last 30 Days'
             },
             this.getDateRangeSeparator()
         )
