930c495a0bbb3ebe5ab1d89a325cf43f3deea2c9
Cory Zue <czue@dimagi.com>
2015-10-21 17:16:25 +0200

    support the forever argument

--- corehq/apps/change_feed/consumer/feed.py
+++ corehq/apps/change_feed/consumer/feed.py
@@ -1,6 +1,7 @@
 import json
 from django.conf import settings
 from kafka import KafkaConsumer
+from kafka.common import ConsumerTimeout
 from corehq.apps.change_feed.models import ChangeMeta
 from pillowtop.feed.interface import ChangeFeed, Change
 
@@ -14,22 +15,25 @@ class KafkaChangeFeed(ChangeFeed):
         self._topic = topic
 
     def iter_changes(self, since, forever):
-        consumer = _get_consumer(self._topic)
+        # in milliseconds, -1 means wait forever for changes
+        timeout = -1 if forever else 100
+        consumer = KafkaConsumer(
+            self._topic,
+            group_id='test-consumer',  # todo: what belongs here?
+            bootstrap_servers=[settings.KAFKA_URL],
+            consumer_timeout_ms=timeout,
+        )
+
         partition = 0  # todo: does this need to be configurable?
         offset = int(since)  # coerce sequence IDs to ints
-        # this is how you tell the consumer to start from a certion point in the sequence
+        # this is how you tell the consumer to start from a certain point in the sequence
         consumer.set_topic_partitions((self._topic, partition, offset))
         for message in consumer:
-            yield change_from_kafka_message(message)
-
-
-def _get_consumer(topic):
-    return KafkaConsumer(
-        topic,
-        group_id='test-consumer',  # todo: what belongs here?
-        bootstrap_servers=[settings.KAFKA_URL],
-        consumer_timeout_ms=100,  # todo: what belongs here?
-    )
+            try:
+                yield change_from_kafka_message(message)
+            except ConsumerTimeout:
+                assert not forever, 'Kafka pillow should not timeout when waiting forever!'
+                # no need to do anything since this is just telling us we've reached the end of the feed
 
 
 def change_from_kafka_message(message):
