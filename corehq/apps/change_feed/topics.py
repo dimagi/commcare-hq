34bf476141f49f98040a02e250d00d853772dec1
Cory Zue <czue@dimagi.com>
2015-10-21 13:13:35 +0200

    associate all non-case, non-forms with the "meta" topic

--- corehq/apps/change_feed/topics.py
+++ corehq/apps/change_feed/topics.py
@@ -3,6 +3,7 @@ from couchforms.models import all_known_formlike_doc_types
 
 CASE = 'case'
 FORM = 'form'
+META = 'meta'
 
 
 def get_topic(document_type):
@@ -10,3 +11,6 @@ def get_topic(document_type):
         return CASE
     elif document_type in all_known_formlike_doc_types():
         return FORM
+    else:
+        # at some point we may want to make this more granular
+        return META
