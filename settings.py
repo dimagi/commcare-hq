d87a9e2feb2f0d0921fab2a9cedceb5e9ee694c6
Simon Kelly <skelly@dimagi.com>
2015-10-21 14:19:54 +0200

    remove duplicate bootstrap-daterangepicker entry from bower

--- settings.py
+++ settings.py
@@ -119,7 +119,6 @@ BOWER_INSTALLED_APPS = (
     'd3#3.1.5',
     'nvd3#1.1.10-beta',
     'datatables-bootstrap3#0.1',
-    'dangrossman/bootstrap-daterangepicker#7435612692b33b85c72539d37c59bb7edd420c6f',
 )
 
 BOWER_PATH = '/usr/local/bin/bower'
