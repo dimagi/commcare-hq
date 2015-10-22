912c699658e8737e78ea06be4d32b487d283d65a
Cory Zue <czue@dimagi.com>
2015-10-22 16:31:45 +0200

    allow callers to set the group and partitions of pillows

--- corehq/apps/change_feed/consumer/feed.py
+++ corehq/apps/change_feed/consumer/feed.py
@@ -11,23 +11,29 @@ class KafkaChangeFeed(ChangeFeed):
     Kafka-based implementation of a ChangeFeed
     """
 
-    def __init__(self, topic):
+    def __init__(self, topic, group_id, partition=0):
+        """
+        Create a change feed listener for a particular kafka topic, group ID, and partition.
+
+        See http://kafka.apache.org/documentation.html#introduction for a description of what these are.
+        """
         self._topic = topic
+        self._group_id = group_id
+        self._partition = partition
 
     def iter_changes(self, since, forever):
         # in milliseconds, -1 means wait forever for changes
         timeout = -1 if forever else 100
         consumer = KafkaConsumer(
             self._topic,
-            group_id='test-consumer',  # todo: what belongs here?
+            group_id=self._group_id,
             bootstrap_servers=[settings.KAFKA_URL],
             consumer_timeout_ms=timeout,
         )
 
-        partition = 0  # todo: does this need to be configurable?
         offset = int(since)  # coerce sequence IDs to ints
         # this is how you tell the consumer to start from a certain point in the sequence
-        consumer.set_topic_partitions((self._topic, partition, offset))
+        consumer.set_topic_partitions((self._topic, self._partition, offset))
         for message in consumer:
             try:
                 yield change_from_kafka_message(message)
