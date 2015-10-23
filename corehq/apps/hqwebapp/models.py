b024b811d1ff1b7c898f5578903c3f531e3363fa
Jenny Schweers <jschweers@dimagi.com>
2015-10-23 17:52:11 -0400

    Move 'Case Groups' menu item

--- corehq/apps/hqwebapp/models.py
+++ corehq/apps/hqwebapp/models.py
@@ -615,19 +615,6 @@ class ProjectDataTab(UITab):
                 import EditDataInterfaceDispatcher
             edit_section = EditDataInterfaceDispatcher.navigation_sections(context)
 
-            from corehq.apps.data_interfaces.views \
-                import CaseGroupListView, CaseGroupCaseManagementView, ArchiveFormView
-            edit_section[0][1].append({
-                'title': CaseGroupListView.page_title,
-                'url': reverse(CaseGroupListView.urlname, args=[self.domain]),
-                'subpages': [
-                    {
-                        'title': CaseGroupCaseManagementView.page_title,
-                        'urlname': CaseGroupCaseManagementView.urlname,
-                    }
-                ]
-            })
-
             if toggles.BULK_ARCHIVE_FORMS.enabled(self._request.user.username):
                 edit_section[0][1].append({
                     'title': ArchiveFormView.page_title,
@@ -928,6 +915,21 @@ class MessagingTab(UITab):
                 {'title': _('Chat'),
                  'url': reverse('chat_contacts', args=[self.domain])}
             )
+
+        if self.couch_user.can_edit_data():
+            from corehq.apps.data_interfaces.views \
+                import CaseGroupListView, CaseGroupCaseManagementView, ArchiveFormView
+            contacts_urls.append({
+                'title': CaseGroupListView.page_title,
+                'url': reverse(CaseGroupListView.urlname, args=[self.domain]),
+                'subpages': [
+                    {
+                        'title': CaseGroupCaseManagementView.page_title,
+                        'urlname': CaseGroupCaseManagementView.urlname,
+                    }
+                ]
+            })
+
 
         return contacts_urls
 
