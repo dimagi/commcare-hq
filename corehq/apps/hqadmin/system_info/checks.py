56724b6287ab31c509536b0d181fca87d8428285
Simon Kelly <skelly@dimagi.com>
2015-10-26 13:43:27 +0200

    yet another celery check fix

--- corehq/apps/hqadmin/system_info/checks.py
+++ corehq/apps/hqadmin/system_info/checks.py
@@ -56,15 +56,42 @@ def check_rabbitmq():
 
 def check_celery_health():
 
-    def get_stats(celery_monitoring, status_only=False):
+    def get_stats(celery_monitoring, status_only=False, refresh=False):
         cresource = Resource(celery_monitoring, timeout=3)
         endpoint = "api/workers"
-        params = {'status': 'true'} if status_only else {}
+        params = {'refresh': 'true'} if refresh else {}
+        if status_only:
+            params['status'] = 'true'
         try:
             t = cresource.get(endpoint, params_dict=params).body_string()
             return json.loads(t)
         except Exception:
             return {}
+
+
+    def get_task_html(detailed_stats, worker_name):
+        tasks_ok = 'label-success'
+        tasks_full = 'label-warning'
+
+        tasks_html = mark_safe('<span class="label %s">unknown</span>' % tasks_full)
+        try:
+            worker_stats = detailed_stats[worker_name]
+            pool_stats = worker_stats['stats']['pool']
+            running_tasks = pool_stats['writes']['inqueues']['active']
+            concurrency = pool_stats['max-concurrency']
+            completed_tasks = pool_stats['writes']['total']
+
+            tasks_class = tasks_full if running_tasks == concurrency else tasks_ok
+            tasks_html = mark_safe(
+                '<span class="label %s">%d / %d</span> :: %d' % (
+                    tasks_class, running_tasks, concurrency, completed_tasks
+                )
+            )
+        except KeyError:
+            pass
+
+        return tasks_html
+
 
     ret = {}
     celery_monitoring = getattr(settings, 'CELERY_FLOWER_URL', None)
@@ -73,30 +100,12 @@ def check_celery_health():
         worker_ok = '<span class="label label-success">OK</span>'
         worker_bad = '<span class="label label-important">Down</span>'
 
-        tasks_ok = 'label-success'
-        tasks_full = 'label-warning'
-
         worker_info = []
         worker_status = get_stats(celery_monitoring, status_only=True)
-        detailed_stats = get_stats(celery_monitoring)
+        detailed_stats = get_stats(celery_monitoring, refresh=True)
         for worker_name, status in worker_status.items():
             status_html = mark_safe(worker_ok if status else worker_bad)
-
-            tasks_html = mark_safe('<span class="label %s">unknown</span>' % tasks_full)
-            try:
-                pool_stats = detailed_stats[worker_name]['stats']['pool']
-                running_tasks = pool_stats['writes']['inqueues']['active']
-                concurrency = pool_stats['max-concurrency']
-                completed_tasks = pool_stats['writes']['total']
-
-                tasks_class = tasks_full if running_tasks == concurrency else tasks_ok
-                tasks_html = mark_safe(
-                    '<span class="label %s">%d / %d</span> :: %d' % (
-                        tasks_class, running_tasks, concurrency, completed_tasks
-                    )
-                )
-            except KeyError:
-                pass
+            tasks_html = get_task_html(detailed_stats, worker_name)
             worker_info.append(' '.join([worker_name, status_html, tasks_html]))
         worker_status = '<br>'.join(worker_info)
     ret['worker_status'] = mark_safe(worker_status)
