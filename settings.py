db1d239fce8384ef2c6d894e4f0887fe54447813
Ben Rudolph <brudolph@dimagi.com>
2015-10-27 13:18:28 -0400

    Remove test var from settings

--- settings.py
+++ settings.py
@@ -968,8 +968,6 @@ ENVIRONMENT_HOSTS = {
 DATADOG_API_KEY = None
 DATADOG_APP_KEY = None
 
-TEST_SHOULD_USE_SQL_BACKEND = False
-
 # Override with the PEM export of an RSA private key, for use with any
 # encryption or signing workflows.
 HQ_PRIVATE_KEY = None
