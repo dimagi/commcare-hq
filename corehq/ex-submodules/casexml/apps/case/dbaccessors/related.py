1a7825ffe17fec7bdbfe67597c65408b21af58eb
Farid Rener <proteusvacuum@gmail.com>
2015-10-28 15:59:25 -0400

    Get all reverse indices also returns extensions

--- corehq/ex-submodules/casexml/apps/case/dbaccessors/related.py
+++ corehq/ex-submodules/casexml/apps/case/dbaccessors/related.py
@@ -5,7 +5,7 @@ from casexml.apps.case.const import CASE_INDEX_CHILD, CASE_INDEX_EXTENSION
 
 def get_indexed_case_ids(domain, case_ids):
     """
-    Given a base list of case ids, gets all ids of cases they reference (parent cases)
+    Given a base list of case ids, gets all ids of cases they reference (parent and host cases)
     """
     from casexml.apps.case.models import CommCareCase
     keys = [[domain, case_id, 'index'] for case_id in case_ids]
@@ -45,12 +45,16 @@ def get_reverse_indexed_cases(domain, case_ids, relationship=CASE_INDEX_CHILD):
     )
 
 
-IndexInfo = namedtuple('IndexInfo', ['case_id', 'identifier', 'referenced_id', 'referenced_type'])
+IndexInfo = namedtuple('IndexInfo', ['case_id', 'identifier', 'referenced_id', 'referenced_type', 'relationship'])
 
 
-def get_all_reverse_indices_info(domain, case_ids, relationship=CASE_INDEX_CHILD):
+def get_all_reverse_indices_info(domain, case_ids, relationship=None):
     from casexml.apps.case.models import CommCareCase
-    keys = [[domain, case_id, 'reverse_index', relationship] for case_id in case_ids]
+    if relationship is None:
+        keys = [[domain, case_id, 'reverse_index', reln]
+                for case_id in case_ids for reln in [CASE_INDEX_CHILD, CASE_INDEX_EXTENSION]]
+    else:
+        keys = [[domain, case_id, 'reverse_index', relationship] for case_id in case_ids]
 
     def _row_to_index_info(row):
         return IndexInfo(
@@ -58,6 +62,7 @@ def get_all_reverse_indices_info(domain, case_ids, relationship=CASE_INDEX_CHILD
             identifier=row['value']['identifier'],
             referenced_id=row['key'][1],
             referenced_type=row['value']['referenced_type'],
+            relationship=row['value']['relationship']
         )
     return map(_row_to_index_info, CommCareCase.get_db().view(
         'case/related',
