7aa52691a694e4ab436e78f12d4321a548810425
Cory Zue <czue@dimagi.com>
2015-10-28 13:51:23 +0200

    remove toggle entirely

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -487,15 +487,6 @@ ENABLE_LOADTEST_USERS = StaticToggle(
     help_link='https://confluence.dimagi.com/display/ccinternal/Loadtest+Users',
 )
 
-OWNERSHIP_CLEANLINESS_RESTORE = PredictablyRandomToggle(
-    'enable_owner_cleanliness_restore',
-    'Enable restoring with updated owner cleanliness logic.',
-    TAG_PRODUCT_CORE,
-    namespaces=[NAMESPACE_DOMAIN],
-    randomness=.75,
-    help_link='https://docs.google.com/a/dimagi.com/document/d/12WfZLerFL832LZbMwqRAvXt82scdjDL51WZVNa31f28/edit#heading=h.gu9sjekp0u2p',
-)
-
 MOBILE_UCR = StaticToggle(
     'mobile_ucr',
     ('Mobile UCR: Configure viewing user configurable reports on the mobile '
