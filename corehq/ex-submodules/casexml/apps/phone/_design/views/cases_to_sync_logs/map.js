0b32d7e48d6c279b43d29f5991075077f49bcd42
Cory Zue <czue@dimagi.com>
2015-10-28 14:00:08 +0200

    delete no longer used couch views

--- corehq/ex-submodules/casexml/apps/phone/_design/views/cases_to_sync_logs/map.js
+++
@@ -1,9 +0,0 @@
-// Emit the most recent date a case has been modified on by each sync token
-function (doc) {
-    if (doc.doc_type === 'CommCareCase') {
-        var actions = doc.actions;
-        for (var i = 0; i < actions.length; i += 1) {
-            emit(doc._id, actions[i]['sync_log_id']);
-        }
-    }
-}
