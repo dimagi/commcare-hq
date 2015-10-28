13ec46d67e6a7f7a3ce04513d5e4d9727a9d1ef9
Cory Zue <czue@dimagi.com>
2015-10-28 13:26:28 +0200

    remove unused function

--- corehq/ex-submodules/casexml/apps/phone/data_providers/case/providers.py
+++ corehq/ex-submodules/casexml/apps/phone/data_providers/case/providers.py
@@ -1,5 +1,4 @@
 from casexml.apps.phone.data_providers import LongRunningRestoreDataProvider
-from casexml.apps.phone.data_providers.case.batched import get_case_payload_batched
 from casexml.apps.phone.data_providers.case.clean_owners import get_case_payload
 
 
@@ -9,12 +8,3 @@ class CasePayloadProvider(LongRunningRestoreDataProvider):
     """
     def get_response(self, restore_state):
         return get_case_payload(restore_state)
-
-
-def _batched_response(restore_state):
-    case_response, num_batches = get_case_payload_batched(
-        restore_state=restore_state,
-    )
-    # keep track of the number of batches (if any) for comparison in unit tests
-    restore_state.provider_log['num_case_batches'] = num_batches
-    return case_response
