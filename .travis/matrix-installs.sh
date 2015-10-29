8a517e6b961acb53470811920d6b23793241427d
Simon Kelly <skelly@dimagi.com>
2015-10-29 16:54:05 +0200

    move installs and runner to scripts

---
+++ .travis/matrix-installs.sh
@@ -0,0 +1,10 @@
+if [ $MATRIX_TYPE == "python" ]; then
+    pip install coverage unittest2 mock --use-mirrors
+elif [ $MATRIX_TYPE == "javascript" ]; then
+    npm install -g bower
+    npm install -g grunt
+    npm install -g grunt-cli
+    ln -nfs `which bower` /home/travis/bower
+    python manage.py bower install
+    npm install
+fi
