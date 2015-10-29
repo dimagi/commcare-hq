1079ea25a64ea6a767dfde8f17a044d1772c848d
Simon Kelly <skelly@dimagi.com>
2015-10-29 14:17:42 +0200

    check correct url for different saved apps

--- corehq/apps/app_manager/static/app_manager/spec/releases_spec.js
+++ corehq/apps/app_manager/static/app_manager/spec/releases_spec.js
@@ -4,8 +4,8 @@ describe('App Releases', function() {
     describe('SavedApp', function() {
         var releases = null,
             urls = {
-                download_zip: 'download_zip_url',
-                download_multimedia: 'download_multimedia_url',
+                download_zip: 'download_zip_url ___',
+                download_multimedia: 'download_multimedia_url ___',
                 fetch: 'fetch_url',
                 odk_media: 'odk_media'
             },
@@ -50,14 +50,29 @@ describe('App Releases', function() {
             app = releases.savedApps()[0];
             app.download_application_zip();
             assert.equal($.ajax.callCount, 1);
-            assert.equal(ajax_stub.firstCall.args[0].url, urls['download_zip']);
+            assert.equal(ajax_stub.firstCall.args[0].url, releases.url('download_zip', app.id()));
         });
 
         it('should use the correct URL for downloading multimedia', function() {
             app = releases.savedApps()[0];
             app.download_application_zip(true);
             assert.equal($.ajax.callCount, 1);
-            assert.equal(ajax_stub.firstCall.args[0].url, urls['download_multimedia']);
+            assert.equal(ajax_stub.firstCall.args[0].url, releases.url('download_multimedia', app.id()));
+        });
+
+        it('should use the correct URL for different saved apps', function() {
+            _.each(releases.savedApps(), function (app) {
+                ajax_stub.reset();
+                ajax_stub.onFirstCall(0).yieldsTo("success", {
+                    download_id: '123' + app.id(),
+                    download_url: 'pollUrl'
+                });
+                ajax_stub.onSecondCall().yieldsTo("success", 'ready_123' + app.id());
+
+                app.download_application_zip();
+                assert.equal($.ajax.callCount, 2);
+                assert.equal(ajax_stub.firstCall.args[0].url, releases.url('download_zip', app.id()));
+            });
         });
     });
 });
