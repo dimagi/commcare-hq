8ee3cccf3ed18ece2b85722604cb9d01dde02d69
Ben Rudolph <brudolph@dimagi.com>
2015-11-02 18:01:55 -0500

    remove useless calls to abstract

--- corehq/form_processor/abstract_models.py
+++ corehq/form_processor/abstract_models.py
@@ -1,37 +1,28 @@
-from abc import abstractmethod, abstractproperty, ABCMeta
 
 
 class AbstractXFormInstance(object):
 
-    @abstractproperty
     def form_id(self):
         raise NotImplementedError()
 
-    @abstractproperty
     def auth_context(self):
         raise NotImplementedError()
 
-    @abstractproperty
     def form_data(self):
         raise NotImplementedError()
 
-    @abstractmethod
     def get_data(self, xpath):
         raise NotImplementedError()
 
-    @abstractmethod
     def get_attachment(self, attachment_name):
         raise NotImplementedError()
 
-    @abstractmethod
     def archive(self, xform_generic, user=None):
         raise NotImplementedError()
 
-    @abstractmethod
     def unarchive(self, xform_generic, user=None):
         raise NotImplementedError()
 
-    @abstractmethod
     def get_xml_element(self):
         raise NotImplementedError()
 
@@ -39,38 +30,30 @@ class AbstractXFormInstance(object):
     def get(self, xform_id):
         raise NotImplementedError()
 
-    @abstractmethod
     def save(self, *args, **kwargs):
         raise NotImplementedError()
 
-    @abstractmethod
     def set_submission_properties(self, submission_post):
         raise NotImplementedError()
 
-    @abstractmethod
     def to_json(self):
         raise NotImplementedError()
 
 
 class AbstractCommCareCase(object):
 
-    @abstractproperty
     def id(self):
         raise NotImplementedError()
 
-    @abstractmethod
     def hard_delete(self):
         raise NotImplementedError()
 
-    @abstractmethod
     def soft_delete(self):
         raise NotImplementedError()
 
-    @abstractmethod
     def get_attachment(self, attachment_name):
         raise NotImplementedError()
 
-    @abstractproperty
     def is_deleted(self):
         raise NotImplementedError()
 
