8386fcc67aeb877c36a1404f1678c486734ce4ec
Farid Rener <proteusvacuum@gmail.com>
2015-11-03 11:12:30 -0500

    Sniffer only run tests once

--- scent.py
+++ scent.py
@@ -1,14 +1,5 @@
-import os
 from subprocess import call
-
-from sniffer.api import file_validator, runnable, select_runnable
-
-
-@select_runnable('execute_tests')
-@file_validator
-def py_files(filename):
-    return (filename.endswith('.py') or filename.endswith('.json')
-            and not os.path.basename(filename).startswith('.'))
+from sniffer.api import runnable
 
 
 @runnable
