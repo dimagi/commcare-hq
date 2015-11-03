d9beeb3b023dc2693685fdc7532e1587eb455186
Ben Rudolph <brudolph@dimagi.com>
2015-11-03 11:43:39 -0500

    Remove last of generic

--- corehq/form_processor/utils.py
+++ corehq/form_processor/utils.py
@@ -15,38 +15,6 @@ from corehq.toggles import USE_SQL_BACKEND
 from couchforms.exceptions import DuplicateError
 
 
-class ToFromGeneric(object):
-    def to_generic(self):
-        raise NotImplementedError()
-
-    @classmethod
-    def from_generic(cls, obj_dict):
-        raise NotImplementedError()
-
-
-def to_generic(fn):
-    """
-    Helper decorator to convert from a DB type to a generic type by calling 'to_generic'
-    on the db type. e.g. FormData to XFormInstance
-    """
-    def _wrap(obj):
-        if hasattr(obj, 'to_generic'):
-            return obj.to_generic()
-        elif isinstance(obj, (list, tuple)):
-            return [_wrap(ob) for ob in obj]
-        elif isinstance(obj, (types.GeneratorType, collections.Iterable)):
-            return (_wrap(ob) for ob in obj)
-        else:
-            return obj
-
-    @functools.wraps(fn)
-    def _inner(*args, **kwargs):
-        obj = fn(*args, **kwargs)
-        return _wrap(obj)
-
-    return _inner
-
-
 def should_use_sql_backend(domain):
     if settings.UNIT_TESTING:
         override = getattr(settings, 'TESTS_SHOULD_USE_SQL_BACKEND', None)
