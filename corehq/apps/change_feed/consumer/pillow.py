ae16277c4a0e17bbade2a45bbca648eff554dad1
Cory Zue <czue@dimagi.com>
2015-11-05 19:14:01 +0200

    convert kafka pillow to use sql checkpoints

--- corehq/apps/change_feed/consumer/pillow.py
+++ corehq/apps/change_feed/consumer/pillow.py
@@ -2,18 +2,21 @@ from casexml.apps.case.models import CommCareCase
 from corehq.apps.change_feed import topics
 from corehq.apps.change_feed.consumer.feed import KafkaChangeFeed
 from pillowtop.checkpoints.manager import PillowCheckpoint, PillowCheckpointEventHandler
-from pillowtop.dao.couch import CouchDocumentStore
+from pillowtop.dao.django import DjangoDocumentStore
+from pillowtop.models import DjangoPillowCheckpoint
 from pillowtop.pillow.interface import ConstructedPillow
 from pillowtop.processor import NoopProcessor
 
 
 # this number intentionally left high to avoid many redundant saves while this pillow is still
 # in experimental stage
-KAFKA_CHECKPOINT_FREQUENCY = 10000
+KAFKA_CHECKPOINT_FREQUENCY = 1000
 
 
 def get_demo_case_consumer_pillow():
-    document_store = CouchDocumentStore(CommCareCase.get_db())
+    document_store = DjangoDocumentStore(
+        DjangoPillowCheckpoint, DjangoPillowCheckpoint.to_dict, DjangoPillowCheckpoint.from_dict,
+    )
     checkpoint = PillowCheckpoint(
         document_store,
         'kafka-demo-case-pillow-checkpoint',
