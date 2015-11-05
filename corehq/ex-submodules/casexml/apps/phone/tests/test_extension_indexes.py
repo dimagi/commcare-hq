7d1af8a24edc67439aff28c9d6d5510f2b4208ef
Farid Rener <proteusvacuum@gmail.com>
2015-11-05 11:22:48 -0500

    Tests for purge algorithm

---
+++ corehq/ex-submodules/casexml/apps/phone/tests/test_extension_indexes.py
@@ -0,0 +1,126 @@
+import json
+import os.path
+import uuid
+
+import re
+from casexml.apps.case.mock import CaseIndex, CaseStructure
+from casexml.apps.case.tests.util import assert_user_doesnt_have_cases, \
+    assert_user_has_cases
+from casexml.apps.phone.models import get_properly_wrapped_sync_log
+from casexml.apps.phone.tests.test_sync_mode import SyncBaseTest
+
+
+def get_test_file_json(filename):
+    base = os.path.dirname(__file__)
+    file_path = 'data'
+    path = os.path.join(base, file_path, '%s.json' % filename)
+    with open(path) as f:
+        file_contents = f.read()
+
+    return json.loads(file_contents)
+
+
+def test_generator(test_name, skip=False):
+    def test(self):
+        if skip:
+            self.skipTest(skip)
+        self.build_case_structures(test_name)
+        desired_cases = self._get_test(test_name).get('outcome', [])
+        undesired_cases = [case for case in self.ALL_CASES if case not in desired_cases]
+        sync_log = get_properly_wrapped_sync_log(self.sync_log._id)
+        self.assertEqual(sync_log.case_ids_on_phone, set(desired_cases))
+        assert_user_has_cases(self, self.user, desired_cases)
+        assert_user_doesnt_have_cases(self, self.user, undesired_cases)
+    return test
+
+
+class TestSequenceMeta(type):
+    def __new__(mcs, name, bases, dict):
+        tests_to_run = get_test_file_json('case_relationship_tests')
+        run_single_tests = filter(lambda t: t.get('only', False), tests_to_run)
+        if run_single_tests:
+            tests_to_run = run_single_tests
+
+        for test in [(test['name'], test.get('skip', False)) for test in tests_to_run]:
+            # Create a new testcase that the test runner is able to find
+            dict["test_%s" % re.sub("\s", "_", test[0])] = test_generator(test[0], test[1])
+
+        return type.__new__(mcs, name, bases, dict)
+
+
+class IndexTreeTest(SyncBaseTest):
+    """Fetch all testcases from data/case_relationship_tests.json and run them
+
+    Each testcase is structured as follows:
+    {
+        "name": The name of the test,
+        "owned": Cases whose owner id is set to the user. Cases not in this list are owned by someone else,
+        "subcases": [ A list of ordered pairs e.g. ["a","b"] means "a creates a child index pointing to b"],
+        "extensions": [ A list of ordered pairs e.g. ["a","b"] means "a creates an extension index pointing to b"],
+        "closed": A list of the closed cases,
+        "outcome": When syncing all the cases, which cases should be sent to the phone,
+        "only": For debugging purposes, run a single test by adding 'only': true to those tests,
+    }
+    """
+    __metaclass__ = TestSequenceMeta
+
+    ALL_CASES = ['a', 'b', 'c', 'd', 'e']
+
+    @property
+    def all_tests(self):
+        """All the test cases in a dict"""
+        all_tests = {}
+        tests = get_test_file_json('case_relationship_tests')
+        for test in tests:
+            all_tests[test['name']] = test
+        return all_tests
+
+    def _get_test(self, test_name):
+        return self.all_tests[test_name]
+
+    def build_case_structures(self, test_name):
+        test = self._get_test(test_name)
+        case_structures = []
+        indices = {case: [] for case in self.ALL_CASES}
+
+        for i, subcase in enumerate(test.get('subcases', [])):
+            indices[subcase[0]].append(CaseIndex(
+                related_structure=CaseStructure(
+                    attrs={
+                        'create': False,
+                        'owner_id': self.user.user_id if subcase[1] in test.get('owned', []) else uuid.uuid4().hex,
+                        'close': subcase[1] in test.get('closed', []),
+                    },
+                    case_id=subcase[1],
+                ),
+                relationship='child',
+                identifier='child_{}'.format(i),
+            ))
+
+        for i, extension in enumerate(test.get('extensions', [])):
+            indices[extension[0]].append(CaseIndex(
+                related_structure=CaseStructure(
+                    attrs={
+                        'create': False,
+                        'owner_id': (self.user.user_id if extension[1]
+                                     in test.get('owned', []) else uuid.uuid4().hex),
+                        'close': extension[1] in test.get('closed', []),
+                    },
+                    case_id=extension[1],
+                ),
+                relationship='extension',
+                identifier='extension_{}'.format(i),
+            ))
+
+        for case in self.ALL_CASES:
+            case_structures.append(CaseStructure(
+                attrs={
+                    'create': True,
+                    'owner_id': self.user.user_id if case in test.get('owned', []) else uuid.uuid4().hex,
+                    'close': case in test.get('closed', []),
+                },
+                case_id=case,
+                indices=indices[case],
+            ))
+
+        self.factory.create_or_update_cases(case_structures)
--- corehq/ex-submodules/casexml/apps/phone/tests/test_index_tree.py
+++ corehq/ex-submodules/casexml/apps/phone/tests/test_index_tree.py
@@ -23,6 +23,82 @@ class TestExtendedFootprint(SimpleTestCase):
         cases = tree.get_all_cases_that_depend_on_case(grandparent_id)
         self.assertEqual(cases, set(all_cases))
 
+    def test_simple_extension(self):
+        [host_id, extension_id] = all_ids = ['host', 'extension']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        child_tree = IndexTree()
+        extension_dependencies = IndexTree.get_all_dependencies(extension_id, child_tree, extension_tree)
+        self.assertEqual(extension_dependencies, set(all_ids))
+
+    def test_extension_long_chain(self):
+        [host_id, extension_id, extension_id_2, extension_id_3] = all_ids = [
+            'host', 'extension', 'extension_2', 'extension_3']
+
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            extension_id_2: convert_list_to_dict([extension_id]),
+            extension_id_3: convert_list_to_dict([extension_id_2]),
+        })
+        child_tree = IndexTree()
+        extension_dependencies = IndexTree.get_all_dependencies(extension_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), extension_dependencies)
+        host_dependencies = IndexTree.get_all_dependencies(host_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), host_dependencies)
+
+    def test_child_and_extension(self):
+        """
+         +---+       +---+
+         | C +--c--->| H |
+         +-+-+       +-+-+
+           ^           ^
+           |e          |e
+         +-+-+       +-+-+
+         |E2 |       |E1 |
+         +---+       +---+
+        """
+        [host_id, extension_id, child_id, extension_id_2] = all_ids = ['host', 'extension', 'child', 'extension_2']
+        child_tree = IndexTree(indices={
+            child_id: convert_list_to_dict([host_id]),
+        })
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            extension_id_2: convert_list_to_dict([child_id]),
+        })
+
+        extension_dependencies = IndexTree.get_all_dependencies(extension_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), extension_dependencies)
+        host_dependencies = IndexTree.get_all_dependencies(host_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), host_dependencies)
+        child_dependencies = IndexTree.get_all_dependencies(child_id, child_tree, extension_tree)
+        self.assertEqual(set([child_id, extension_id_2]), child_dependencies)
+
+    def test_multiple_indices(self):
+        """
+        +---+       +---+
+        | C +--c--->| H |
+        +---+--e--->+-+-+
+                      ^
+        +---+         |
+        | E +----e----+
+        +---+
+        """
+        [host_id, extension_id, child_id] = all_ids = ['host', 'extension', 'child']
+        child_tree = IndexTree(indices={
+            child_id: convert_list_to_dict([host_id]),
+        })
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            child_id: convert_list_to_dict([host_id]),
+        })
+
+        child_dependencies = IndexTree.get_all_dependencies(child_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), child_dependencies)
+
+        extension_dependencies = IndexTree.get_all_dependencies(extension_id, child_tree, extension_tree)
+        self.assertEqual(set(all_ids), extension_dependencies)
+
 
 class PruningTest(SimpleTestCase):
 
@@ -33,14 +109,14 @@ class PruningTest(SimpleTestCase):
         })
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
         # this has no effect
-        sync_log.prune_case(parent_id)
+        sync_log.purge(parent_id)
         self.assertTrue(child_id in sync_log.case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.case_ids_on_phone)
         self.assertFalse(child_id in sync_log.dependent_case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.dependent_case_ids_on_phone)
 
         # this should prune it entirely
-        sync_log.prune_case(child_id)
+        sync_log.purge(child_id)
         self.assertFalse(child_id in sync_log.case_ids_on_phone)
         self.assertFalse(parent_id in sync_log.case_ids_on_phone)
 
@@ -52,14 +128,14 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # this should prune the child but not the parent
-        sync_log.prune_case(child_id)
+        sync_log.purge(child_id)
         self.assertFalse(child_id in sync_log.case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.case_ids_on_phone)
         self.assertFalse(child_id in sync_log.dependent_case_ids_on_phone)
         self.assertFalse(parent_id in sync_log.dependent_case_ids_on_phone)
 
         # then pruning the parent should prune it
-        sync_log.prune_case(parent_id)
+        sync_log.purge(parent_id)
         self.assertFalse(parent_id in sync_log.case_ids_on_phone)
         self.assertFalse(parent_id in sync_log.dependent_case_ids_on_phone)
 
@@ -72,7 +148,7 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # this has no effect other than to move the grandparent to dependent
-        sync_log.prune_case(grandparent_id)
+        sync_log.purge(grandparent_id)
         for id in all_ids:
             self.assertTrue(id in sync_log.case_ids_on_phone)
 
@@ -81,7 +157,7 @@ class PruningTest(SimpleTestCase):
         self.assertFalse(child_id in sync_log.dependent_case_ids_on_phone)
 
         # likewise, this should have no effect other than to move the parent to dependent
-        sync_log.prune_case(parent_id)
+        sync_log.purge(parent_id)
         for id in all_ids:
             self.assertTrue(id in sync_log.case_ids_on_phone)
 
@@ -90,7 +166,7 @@ class PruningTest(SimpleTestCase):
         self.assertFalse(child_id in sync_log.dependent_case_ids_on_phone)
 
         # this should now prune everything
-        sync_log.prune_case(child_id)
+        sync_log.purge(child_id)
         for id in all_ids:
             self.assertFalse(id in sync_log.case_ids_on_phone)
             self.assertFalse(id in sync_log.dependent_case_ids_on_phone)
@@ -104,18 +180,18 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # just pruning the child should prune just the child
-        sync_log.prune_case(child_id)
+        sync_log.purge(child_id)
         self.assertTrue(grandparent_id in sync_log.case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.case_ids_on_phone)
         self.assertFalse(child_id in sync_log.case_ids_on_phone)
 
         # same for the parent
-        sync_log.prune_case(parent_id)
+        sync_log.purge(parent_id)
         self.assertTrue(grandparent_id in sync_log.case_ids_on_phone)
         self.assertFalse(parent_id in sync_log.case_ids_on_phone)
 
         # same for the grandparentparent
-        sync_log.prune_case(grandparent_id)
+        sync_log.purge(grandparent_id)
         self.assertFalse(grandparent_id in sync_log.case_ids_on_phone)
 
     def test_prune_multiple_children(self):
@@ -128,15 +204,15 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # first prune the parent and grandparent
-        sync_log.prune_case(grandparent_id)
-        sync_log.prune_case(parent_id)
+        sync_log.purge(grandparent_id)
+        sync_log.purge(parent_id)
         self.assertTrue(grandparent_id in sync_log.case_ids_on_phone)
         self.assertTrue(grandparent_id in sync_log.dependent_case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.dependent_case_ids_on_phone)
 
         # just pruning one child should preserve the parent index
-        sync_log.prune_case(child_id_1)
+        sync_log.purge(child_id_1)
         self.assertTrue(grandparent_id in sync_log.case_ids_on_phone)
         self.assertTrue(grandparent_id in sync_log.dependent_case_ids_on_phone)
         self.assertTrue(parent_id in sync_log.case_ids_on_phone)
@@ -144,7 +220,7 @@ class PruningTest(SimpleTestCase):
         self.assertFalse(child_id_1 in sync_log.case_ids_on_phone)
 
         # pruning the other one should wipe it
-        sync_log.prune_case(child_id_2)
+        sync_log.purge(child_id_2)
         for id in all_ids:
             self.assertFalse(id in sync_log.case_ids_on_phone)
             self.assertFalse(id in sync_log.dependent_case_ids_on_phone)
@@ -159,16 +235,16 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # first prune everything but the child
-        sync_log.prune_case(grandparent_id)
-        sync_log.prune_case(mother_id)
-        sync_log.prune_case(father_id)
+        sync_log.purge(grandparent_id)
+        sync_log.purge(mother_id)
+        sync_log.purge(father_id)
 
         # everything should still be relevant because of the child
         for id in all_ids:
             self.assertTrue(id in sync_log.case_ids_on_phone)
 
         # pruning the child should wipe everything else
-        sync_log.prune_case(child_id)
+        sync_log.purge(child_id)
         for id in all_ids:
             self.assertFalse(id in sync_log.case_ids_on_phone)
             self.assertFalse(id in sync_log.dependent_case_ids_on_phone)
@@ -182,12 +258,12 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # pruning one peer should keep everything around
-        sync_log.prune_case(peer_id_1)
+        sync_log.purge(peer_id_1)
         for id in all_ids:
             self.assertTrue(id in sync_log.case_ids_on_phone)
 
         # pruning the second peer should remove everything
-        sync_log.prune_case(peer_id_2)
+        sync_log.purge(peer_id_2)
         for id in all_ids:
             self.assertFalse(id in sync_log.case_ids_on_phone)
 
@@ -201,12 +277,12 @@ class PruningTest(SimpleTestCase):
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set(all_ids))
 
         # pruning the first two, should still keep everything around
-        sync_log.prune_case(peer_id_1)
-        sync_log.prune_case(peer_id_2)
+        sync_log.purge(peer_id_1)
+        sync_log.purge(peer_id_2)
         for id in all_ids:
             self.assertTrue(id in sync_log.case_ids_on_phone)
 
-        sync_log.prune_case(peer_id_3)
+        sync_log.purge(peer_id_3)
         for id in all_ids:
             self.assertFalse(id in sync_log.case_ids_on_phone)
 
@@ -216,9 +292,143 @@ class PruningTest(SimpleTestCase):
             id: convert_list_to_dict([id]),
         })
         sync_log = SimplifiedSyncLog(index_tree=tree, case_ids_on_phone=set([id]))
-        sync_log.prune_case(id)
+        sync_log.purge(id)
         self.assertFalse(id in sync_log.case_ids_on_phone)
         self.assertFalse(id in sync_log.dependent_case_ids_on_phone)
+
+
+class ExtensionCasesPruningTest(SimpleTestCase):
+
+    def test_prune_host(self, ):
+        """Pruning host removes the extension
+        """
+        [host_id, extension_id] = all_ids = ['host', 'extension']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     dependent_case_ids_on_phone=set([extension_id]),
+                                     case_ids_on_phone=set(all_ids))
+
+        sync_log.purge(host_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_extension(self, ):
+        """Pruning extension removes host
+        """
+        [host_id, extension_id] = all_ids = ['host', 'extension']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     dependent_case_ids_on_phone=set([host_id]),
+                                     case_ids_on_phone=set(all_ids))
+
+        sync_log.purge(extension_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_host_extension_has_extension(self):
+        """Pruning host when extension has an extension removes both
+        """
+        [host_id, extension_id, extension_extension_id] = all_ids = ['host', 'extension', 'extension_extension']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            extension_extension_id: convert_list_to_dict([extension_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     dependent_case_ids_on_phone=set([extension_id, extension_extension_id]),
+                                     case_ids_on_phone=set(all_ids))
+        sync_log.purge(host_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(extension_extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_host_has_multiple_extensions(self):
+        """Pruning host with multiple extensions should remove all extensions
+        """
+        [host_id, extension_id, extension_id_2] = all_ids = ['host', 'extension', 'extension_2']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            extension_id_2: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     dependent_case_ids_on_phone=set([extension_id, extension_id_2]),
+                                     case_ids_on_phone=set(all_ids))
+        sync_log.purge(host_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(extension_id_2 in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_extension_host_has_multiple_extensions(self):
+        """Pruning an extension should remove host and its other extensions
+        """
+        [host_id, extension_id, extension_id_2] = all_ids = ['host', 'extension', 'extension_2']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+            extension_id_2: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     dependent_case_ids_on_phone=set([host_id, extension_id_2]),
+                                     case_ids_on_phone=set(all_ids))
+        sync_log.purge(extension_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(extension_id_2 in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_extension_non_dependent_host(self):
+        """Pruning an extension should not remove the host or itself if the host is directly owned
+        """
+        [host_id, extension_id] = all_ids = ['host', 'extension']
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     case_ids_on_phone=set(all_ids))
+        sync_log.purge(extension_id)
+        self.assertTrue(extension_id in sync_log.case_ids_on_phone)
+        self.assertTrue(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_child_of_extension(self):
+        """Pruning child of extension should remove extension and host
+        """
+        [host_id, extension_id, child_id] = all_ids = ['host', 'extension', 'child']
+        child_tree = IndexTree(indices={
+            child_id: convert_list_to_dict([extension_id]),
+        })
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     index_tree=child_tree,
+                                     dependent_case_ids_on_phone=set([host_id, extension_id]),
+                                     case_ids_on_phone=set(all_ids))
+
+        sync_log.purge(child_id)
+        self.assertFalse(extension_id in sync_log.case_ids_on_phone)
+        self.assertFalse(child_id in sync_log.case_ids_on_phone)
+        self.assertFalse(host_id in sync_log.case_ids_on_phone)
+
+    def test_prune_extension_host_is_parent(self):
+        """Pruning an extension should not prune the host or the extension if the host is a depenency for a child
+        """
+        [host_id, extension_id, child_id] = all_ids = ['host', 'extension', 'child']
+        child_tree = IndexTree(indices={
+            child_id: convert_list_to_dict([host_id]),
+        })
+        extension_tree = IndexTree(indices={
+            extension_id: convert_list_to_dict([host_id]),
+        })
+        sync_log = SimplifiedSyncLog(extension_index_tree=extension_tree,
+                                     index_tree=child_tree,
+                                     dependent_case_ids_on_phone=set([host_id]),
+                                     case_ids_on_phone=set(all_ids))
+
+        sync_log.purge(extension_id)
+        self.assertTrue(extension_id in sync_log.case_ids_on_phone)
+        self.assertTrue(child_id in sync_log.case_ids_on_phone)
+        self.assertTrue(host_id in sync_log.case_ids_on_phone)
 
 
 def convert_list_to_dict(a_list):
