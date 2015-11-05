aac4e423c3a42d80a0af9b689449a4f75cd6facd
Cory Zue <czue@dimagi.com>
2015-11-05 11:32:14 +0200

    silence kafka logging
    related to http://manage.dimagi.com/default.asp?186282
    
    depends on https://github.com/dimagi/pillowtop/pull/171

--- corehq/apps/change_feed/consumer/pillow.py
+++ corehq/apps/change_feed/consumer/pillow.py
@@ -1,10 +1,10 @@
 from casexml.apps.case.models import CommCareCase
-from corehq.apps.change_feed import topics, change_feed_logger
+from corehq.apps.change_feed import topics
 from corehq.apps.change_feed.consumer.feed import KafkaChangeFeed
 from pillowtop.checkpoints.manager import PillowCheckpoint, PillowCheckpointEventHandler
 from pillowtop.dao.couch import CouchDocumentStore
 from pillowtop.pillow.interface import ConstructedPillow
-from pillowtop.processor import LoggingProcessor
+from pillowtop.processor import NoopProcessor
 
 
 # this number intentionally left high to avoid many redundant saves while this pillow is still
@@ -23,7 +23,7 @@ def get_demo_case_consumer_pillow():
         document_store=document_store,
         checkpoint=checkpoint,
         change_feed=KafkaChangeFeed(topic=topics.CASE, group_id='demo-case-group'),
-        processor=LoggingProcessor(logger=change_feed_logger),
+        processor=NoopProcessor(),
         change_processed_event_handler=PillowCheckpointEventHandler(
             checkpoint=checkpoint, checkpoint_frequency=KAFKA_CHECKPOINT_FREQUENCY,
         ),
