8e1d0cdbf74594476a0a2c323f4f174b257597b8
Biyeun Buczyk <biyeun@dimagi.com>
2015-11-06 11:54:09 -0500

    add jquery ui as bower app and create decorator for bootstrap3

--- corehq/apps/style/decorators.py
+++ corehq/apps/style/decorators.py
@@ -111,3 +111,21 @@ def use_daterangepicker(view_func):
         request.use_daterangepicker = True
         return view_func(class_based_view, request, *args, **kwargs)
     return _wrapped
+
+
+def use_jquery_ui(view_func):
+    """Use this decorator on the dispatch method of a TemplateView subclass
+    to enable the inclusion of the jquery-ui library at the base template
+    level.
+
+    Example:
+
+    @use_jquery_ui
+    def dispatch(self, request, *args, **kwargs):
+        return super(MyView, self).dispatch(request, *args, **kwargs)
+    """
+    @wraps(view_func)
+    def _wrapped(class_based_view, request, *args, **kwargs):
+        request.use_jquery_ui = True
+        return view_func(class_based_view, request, *args, **kwargs)
+    return _wrapped
