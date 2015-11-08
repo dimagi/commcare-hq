31ab349526a38866fbd1b345010e981401f2e677
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-08 15:42:56 -0500

    remove description from fixture

--- corehq/apps/userreports/fixtures.py
+++ corehq/apps/userreports/fixtures.py
@@ -76,7 +76,6 @@ class ReportFixturesProvider(object):
         report_elem = ElementTree.Element('report', attrib={'id': report_config.uuid})
         report = ReportConfiguration.get(report_config.report_id)
         report_elem.append(self._element('name', localize(report_config.header, user.language)))
-        report_elem.append(self._element('description', localize(report_config.description, user.language)))
         data_source = ReportFactory.from_spec(report)
 
         data_source.set_filter_values({
