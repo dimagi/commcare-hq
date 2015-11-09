7b4fb0bdd611862e41df3efb070cd57d04bcfafd
Simon Kelly <skelly@dimagi.com>
2015-11-09 12:42:46 +0200

    make task errors more accessible

--- corehq/ex-submodules/soil/exceptions.py
+++ corehq/ex-submodules/soil/exceptions.py
@@ -1,4 +1,6 @@
 
 
 class TaskFailedError(Exception):
-    pass
+    def __init__(self, errors, *args, **kwargs):
+        self.errors = errors
+        super(TaskFailedError, self).__init__(*args, **kwargs)
