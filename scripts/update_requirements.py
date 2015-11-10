27e874ff38141fd32ee0cd5ba931ee3f18480bb3
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-09 21:18:14 -0500

    WIP2

--- scripts/update_requirements.py
+++ scripts/update_requirements.py
@@ -1,17 +1,29 @@
+import fileinput
 import subprocess
 
-subprocess.Popen(['git', 'checkout', 'master'])
 
-for line in subprocess.Popen(
-    ['pip', 'list', '--outdated'],
-    stdout=subprocess.PIPE,
-).communicate()[0].splitlines():
-    parts = line.split()
-    package = parts[0]
-    new_version = parts[4]
+def reset_branch():
+    subprocess.Popen(
+        ['git', 'checkout', 'requirements-updater'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
 
-    updated_branch = 'update-%s%s' % (package, new_version)
 
+def new_packages():
+    for line in subprocess.Popen(
+        ['pip', 'list', '--outdated'],
+        stdout=subprocess.PIPE,
+    ).communicate()[0].splitlines():
+        parts = line.split()
+        package = parts[0]
+        new_version = parts[4]
+        yield package, new_version
+
+reset_branch()
+for package, new_version in new_packages():
+    reset_branch()
+    updated_branch = 'update-%s_%s' % (package, new_version)
     subprocess.Popen(
         [
             'git',
@@ -25,8 +37,15 @@ for line in subprocess.Popen(
 
     print '  - %s' % updated_branch
 
-subprocess.Popen(
-    ['git', 'checkout', 'master'],
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-)
+    for line in fileinput.FileInput('requirements/requirements.txt', inplace=1):
+        if line.startswith(package):
+            line = '%s==%s' % (package, new_version)
+        print line,
+
+    subprocess.Popen(
+        ['git', 'commit', '-am', 'update %s to version %s' % (package, new_version)],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+reset_branch()
