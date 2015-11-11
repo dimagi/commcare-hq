501fa785e5bbcb98c476a81c07acb9ee6ea9d386
Cory Zue <czue@dimagi.com>
2015-11-11 15:56:02 +0200

    add filter functionality and move loging to process_change

--- corehq/apps/change_feed/consumer/pillow.py
+++ corehq/apps/change_feed/consumer/pillow.py
@@ -17,17 +17,23 @@ KAFKA_CHECKPOINT_FREQUENCY = 1000
 
 class LoggingPythonPillow(PythonPillow):
 
+    def __init__(self, couch_db, checkpoint, change_feed, python_filter=None):
+        super(LoggingPythonPillow, self).__init__(
+            couch_db=couch_db, checkpoint=checkpoint, change_feed=change_feed, preload_docs=False
+        )
+        self._python_filter = python_filter
+        self._changes_processed = 0
+
+    def python_filter(self, change):
+        if self._python_filter is not None:
+            return self._python_filter(change)
+
     def process_change(self, change, is_retry_attempt=False):
         # do nothing
-        pass
-
-    def fire_change_processed_event(self, change, context):
-        # todo: when PythonPillow moves checkpoints to this function this class will
-        # need to use some other mechanism for this or reimplement checkpointing
-        if context.changes_seen % KAFKA_CHECKPOINT_FREQUENCY == 0:
+        if self._changes_processed % KAFKA_CHECKPOINT_FREQUENCY == 0:
             # only log a small amount to avoid clogging up supervisor
-            pillow_logging.info('Processed change {}: {}'.format(context.changes_seen, change))
-
+            pillow_logging.info('Processed change {}: {}'.format(self._changes_processed, change))
+        self._changes_processed += 1
 
 def get_demo_case_consumer_pillow():
     checkpoint = PillowCheckpoint(
@@ -51,9 +57,15 @@ def get_demo_python_pillow_consumer():
         get_django_checkpoint_store(),
         'kafka-demo-python-pillow-checkpoint',
     )
+
+    def arbitrary_filter(change):
+        # just to prove that filters work - only process data from domains starting with
+        # letters between "b" and "f"
+        return 'b' < change.metadata.domain < 'f'
+
     return LoggingPythonPillow(
         couch_db=CachedCouchDB(CommCareCase.get_db().uri, readonly=False),
         checkpoint=checkpoint,
         change_feed=KafkaChangeFeed(topic=topics.CASE, group_id='demo-python-pillow-group'),
-        preload_docs=False,
+        python_filter=arbitrary_filter,
     )
