001e924f9b1a609c745968d698f30d0169e196c3
Cory Zue <czue@dimagi.com>
2015-11-11 17:34:31 +0200

    bootstrap db

--- pillowtop/tests/test_couch.py
+++ pillowtop/tests/test_couch.py
@@ -1,9 +1,15 @@
+from couchdbkit import Database
 from django.conf import settings
 from django.test import TestCase
 from pillowtop.couchdb import CachedCouchDB
 
 
 class CachedCouchDbTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # this will ensure the database is created
+        Database(settings.COUCH_DATABASE, create=True)
 
     def test_bulk_load_missing(self):
         db = CachedCouchDB(settings.COUCH_DATABASE, readonly=False)
