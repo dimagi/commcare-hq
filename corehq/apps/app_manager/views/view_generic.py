bd70fc79d2ab57b2a31bfae30a451b2e20138510
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-11 19:22:20 -0500

    support different multimedia in different translations

--- corehq/apps/app_manager/views/view_generic.py
+++ corehq/apps/app_manager/views/view_generic.py
@@ -111,7 +111,7 @@ def view_generic(request, domain, app_id=None, module_id=None, form_id=None,
         template = get_module_template(module)
         # make sure all modules have unique ids
         app.ensure_module_unique_ids(should_save=True)
-        module_context = get_module_view_context(app, module)
+        module_context = get_module_view_context(request, app, module)
         context.update(module_context)
     elif app:
         template = "app_manager/app_view.html"
