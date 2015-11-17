82e5fc539f1230c614de6ab81698117c3e150ec3
Cory Zue <czue@dimagi.com>
2015-11-17 12:02:47 +0200

    don't include archived locations in ucr filters

--- corehq/apps/userreports/reports/filters/choice_providers.py
+++ corehq/apps/userreports/reports/filters/choice_providers.py
@@ -47,11 +47,11 @@ def get_location_choices(query_context):
     # todo: does this need fancier permission restrictions and what not?
     # see e.g. locations.views.child_locations_for_select2
     if query_context.query:
-        locations = SQLLocation.objects.filter_path_by_user_input(
+        locations = SQLLocation.active_objects.filter_path_by_user_input(
             domain=query_context.domain, user_input=query_context.query
         )
     else:
-        locations = SQLLocation.objects.filter(domain=query_context.domain)
+        locations = SQLLocation.active_objects.filter(domain=query_context.domain)
     return [
         Choice(loc.location_id, loc.display_name) for loc in
         locations[query_context.offset:query_context.offset + query_context.limit]
