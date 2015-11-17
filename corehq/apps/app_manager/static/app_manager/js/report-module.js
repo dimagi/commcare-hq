de7caf00ab1d4aaa70f1617fd4be4e1057f521cf
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-17 12:15:46 -0500

    enable Save when modifying multimedia

--- corehq/apps/app_manager/static/app_manager/js/report-module.js
+++ corehq/apps/app_manager/static/app_manager/js/report-module.js
@@ -301,11 +301,11 @@ var ReportModule = (function () {
             }
         });
 
-        var changeSaveButton = function () {
+        self.changeSaveButton = function () {
             self.saveButton.fire('change');
         };
 
-        self.currentModuleName.subscribe(changeSaveButton);
+        self.currentModuleName.subscribe(self.changeSaveButton);
 
         function newReport(options) {
             options = options || {};
@@ -320,10 +320,10 @@ var ReportModule = (function () {
                 options.filters,
                 self.reportFilters,
                 self.lang,
-                changeSaveButton
+                self.changeSaveButton
             );
-            report.display.subscribe(changeSaveButton);
-            report.reportId.subscribe(changeSaveButton);
+            report.display.subscribe(self.changeSaveButton);
+            report.reportId.subscribe(self.changeSaveButton);
             report.reportId.subscribe(function (reportId) {
                 report.display(self.defaultReportTitle(reportId));
             });
@@ -338,7 +338,7 @@ var ReportModule = (function () {
         };
         this.removeReport = function (report) {
             self.reports.remove(report);
-            changeSaveButton();
+            self.changeSaveButton();
         };
 
         // add existing reports to UI
