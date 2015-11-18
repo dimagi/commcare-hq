36af8f52e3b7a17930e94528eea73317889dcdc2
Ethan Soergel <esoergel@gmail.com>
2015-11-18 17:17:32 -0500

    Add new by_domain_doc_type_date view

--- corehq/couchapps/__init__.py
+++ corehq/couchapps/__init__.py
@@ -8,4 +8,5 @@ CouchAppsPreindexPlugin.register('couchapps', __file__, {
     'all_docs': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB),
     # TODO should we make an '_all' option or something?
     'by_domain_doc_type': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB),
+    'by_domain_doc_type_date': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB),
 })
