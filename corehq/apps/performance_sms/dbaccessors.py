ec40f33ff9cd7f29941d89d30ff1b37ca39674f2
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-18 15:14:58 -0500

    replace usage of performance_sms/by_domain

--- corehq/apps/performance_sms/dbaccessors.py
+++ corehq/apps/performance_sms/dbaccessors.py
@@ -1,14 +1,11 @@
 from django.conf import settings
+
+from corehq.apps.domain.dbaccessors import get_docs_in_domain_by_class
 from corehq.apps.performance_sms.models import PerformanceConfiguration
 
 
 def by_domain(domain):
-    return list(PerformanceConfiguration.view(
-        'performance_sms/by_domain',
-        key=domain,
-        reduce=False,
-        include_docs=True
-    ))
+    return list(get_docs_in_domain_by_class(domain, PerformanceConfiguration))
 
 
 def by_interval(interval_keys):
