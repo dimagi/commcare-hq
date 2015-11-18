303293a7e309af5065a1bcccd6f588b13b6a82f8
Cal Ellowitz <calellowitz@gmail.com>
2015-11-18 16:42:52 -0500

    fixing migration conflicts

---
+++ corehq/apps/accounting/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounting', '0007_make_subscriber_domain_required'),
+        ('accounting', '0011_auto_20151113_1710'),
+    ]
+
+    operations = [
+    ]
