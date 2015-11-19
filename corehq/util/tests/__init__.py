7f60661c01b909db46180c60264f26d64851f9ea
Daniel Roberts <droberts@dimagi.com>
2015-11-19 11:35:00 -0500

    rearrange util test imports

--- corehq/util/tests/__init__.py
+++ corehq/util/tests/__init__.py
@@ -1,17 +1,17 @@
-from corehq.util.spreadsheets.excel import IteratorJSONReader
-from test_couch import *
-from test_log import *
-from test_toggle import *
-from test_quickcache import *
-from test_timezone_conversions import *
-from test_soft_assert import *
 from test_cache_util import *
-from test_override_db import *
-from test_spreadsheets import *
-from test_jsonobject import *
-from test_xml import *
+from test_couch import *
 from test_couchdb_management import *
+from test_jsonobject import *
+from test_log import *
+from test_override_db import *
+from test_quickcache import *
+from test_soft_assert import *
+from test_spreadsheets import *
+from test_timezone_conversions import *
+from test_toggle import *
+from test_xml import *
 
+from corehq.util.spreadsheets.excel import IteratorJSONReader
 from corehq.util.dates import iso_string_to_datetime, iso_string_to_date
 
 __test__ = {
