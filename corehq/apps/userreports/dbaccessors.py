14eaff694912320296412f2e4ca51072c5dddf49
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-20 14:39:58 -0500

    add unit_testing_only decorator

--- corehq/apps/userreports/dbaccessors.py
+++ corehq/apps/userreports/dbaccessors.py
@@ -1,9 +1,6 @@
-from django.conf import settings
-
-from dimagi.utils.couch.database import iter_docs
-
 from corehq.apps.domain.dbaccessors import get_docs_in_domain_by_class
 from corehq.apps.domain.models import Domain
+from corehq.util.test_utils import unit_testing_only
 
 
 def get_number_of_report_configs_by_data_source(domain, data_source_id):
@@ -18,9 +15,8 @@ def get_number_of_report_configs_by_data_source(domain, data_source_id):
     ).one()['value']
 
 
+@unit_testing_only
 def get_all_report_configs():
-    assert settings.UNIT_TESTING
-
     all_domains = Domain.get_all()
     for domain in all_domains:
         for report_config in get_report_configs_for_domain(domain.name):
