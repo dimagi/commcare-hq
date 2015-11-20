350e2fc864aabfcc47532cd6480e933b2e52d0dd
Sravan Reddy <sravfeyn@gmail.com>
2015-11-20 16:07:14 +0530

    introduce CSRF protection, but not turn on yet

--- corehq/apps/sms/urls.py
+++ corehq/apps/sms/urls.py
@@ -13,7 +13,7 @@ from corehq.apps.smsbillables.dispatcher import SMSAdminInterfaceDispatcher
 urlpatterns = patterns('corehq.apps.sms.views',
     url(r'^$', 'default', name='sms_default'),
     url(r'^post/?$', 'post', name='sms_post'),
-    url(r'^send_to_recipients/$', 'send_to_recipients'),
+    url(r'^send_to_recipients/$', 'send_to_recipients', name='send_to_recipients'),
     url(r'^compose/$', 'compose_message', name='sms_compose_message'),
     url(r'^message_test/(?P<phone_number>\d+)/$', 'message_test', name='message_test'),
     url(r'^api/send_sms/$', 'api_send_sms', name='api_send_sms'),
