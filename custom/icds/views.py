11a9ec5b1228a1bfa2146cea32b8fd738a2d32fd
Tyler Sheffels <tsheffels@dimagi.com>
2015-11-23 12:32:52 +0530

    Break out tableau JS into it's own file and start parametrizing the report that is loaded

--- custom/icds/views.py
+++ custom/icds/views.py
@@ -1,5 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 
 def tableau(request):
-    return render(request, 'tableau.html')
+    context = {
+        'report_view': 'POCReports/MainDashboard'
+    }
+    response = render_to_string('tableau.html', context)
+    return HttpResponse(response)
+    # return render(request, context=context, template='tableau.html')
