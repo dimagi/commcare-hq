b71a726e4ff745d574ac9aa9e5857eb33688d63a
Daniel Roberts <droberts@dimagi.com>
2015-11-24 17:31:06 -0500

    remove data_source.get_data slugs arg
    As far as I can tell this functionality isn't used anywhere,
    and having it around makes the code more confusing.
    Also, many of the subclasses just ignore that arg

--- corehq/apps/reports/commtrack/data_sources.py
+++ corehq/apps/reports/commtrack/data_sources.py
@@ -103,7 +103,7 @@ class SimplifiedInventoryDataSource(ReportDataSource, CommtrackDataSourceMixin):
 
         return datetime(date.year, date.month, date.day, 23, 59, 59)
 
-    def get_data(self, slugs=None):
+    def get_data(self):
         if self.active_location:
             current_location = self.active_location.sql_location
 
@@ -243,7 +243,7 @@ class StockStatusDataSource(ReportDataSource, CommtrackDataSourceMixin):
             )
         )
 
-    def get_data(self, slugs=None):
+    def get_data(self):
         sp_ids = get_relevant_supply_point_ids(self.domain, self.active_location)
 
         stock_states = StockState.objects.filter(
