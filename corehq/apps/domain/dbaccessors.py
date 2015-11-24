1941ae85c9e0874a3b076b71df9f33ae191cac60
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-24 16:14:17 -0500

    add to whitelist

--- corehq/apps/domain/dbaccessors.py
+++ corehq/apps/domain/dbaccessors.py
@@ -32,6 +32,7 @@ def get_docs_in_domain_by_class(domain, doc_class):
     whitelist = [
         'CallCenterIndicatorConfig',
         'CommtrackConfig',
+        'Group',
         'Invitation',
         'PerformanceConfiguration',
     ]
