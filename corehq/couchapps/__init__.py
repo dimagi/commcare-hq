029e701b1902be0db0da51d2549cd20e88f0ec7b
Nick Pellegrino <npellegrino@dimagi.com>
2015-11-25 11:56:51 -0500

    make all_docs index meta db

--- corehq/couchapps/__init__.py
+++ corehq/couchapps/__init__.py
@@ -5,6 +5,6 @@ CouchAppsPreindexPlugin.register('couchapps', __file__, {
     'form_question_schema': 'meta',
     'users_extra': (settings.USERS_GROUPS_DB, settings.NEW_USERS_GROUPS_DB),
     'noneulized_users': (settings.USERS_GROUPS_DB, settings.NEW_USERS_GROUPS_DB),
-    'all_docs': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB),
+    'all_docs': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB, 'meta'),
     'by_domain_doc_type_date': (None, settings.NEW_USERS_GROUPS_DB, settings.NEW_FIXTURES_DB, 'meta'),
 })
