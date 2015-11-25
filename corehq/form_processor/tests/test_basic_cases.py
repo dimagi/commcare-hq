c2aca387d4a787909a1d97e32c3d913b3dde3a2f
Simon Kelly <skelly@dimagi.com>
2015-11-25 17:35:31 +0200

    update tests for uuids

--- corehq/form_processor/tests/test_basic_cases.py
+++ corehq/form_processor/tests/test_basic_cases.py
@@ -6,12 +6,12 @@ from casexml.apps.case.mock import CaseBlock
 from casexml.apps.case.util import post_case_blocks
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.form_processor.interfaces.processor import FormProcessorInterface
-from corehq.form_processor.tests.utils import FormProcessorTestUtils, run_with_all_backends
+from corehq.form_processor.tests.utils import FormProcessorTestUtils, run_with_all_backends, UuidAssertMixin
 
 DOMAIN = 'fundamentals'
 
 
-class FundamentalCaseTests(TestCase):
+class FundamentalCaseTests(TestCase, UuidAssertMixin):
 
     @classmethod
     def setUpClass(cls):
@@ -24,10 +24,12 @@ class FundamentalCaseTests(TestCase):
 
     @run_with_all_backends
     def test_create_case(self):
+        user = uuid.uuid4().hex
+        owner = uuid.uuid4().hex
         case_id = uuid.uuid4().hex
         modified_on = datetime.utcnow()
         _submit_case_block(
-            True, case_id, user_id='user1', owner_id='owner1', case_type='demo',
+            True, case_id, user_id=user, owner_id=owner, case_type='demo',
             case_name='create_case', date_modified=modified_on, update={
                 'dynamic': '123'
             }
@@ -35,14 +37,14 @@ class FundamentalCaseTests(TestCase):
 
         case = self.casedb.get_case(case_id)
         self.assertIsNotNone(case)
-        self.assertEqual(case.case_id, case_id)
-        self.assertEqual(case.owner_id, 'owner1')
+        self.assertUuidEqual(case.case_id, case_id)
+        self.assertUuidEqual(case.owner_id, owner)
         self.assertEqual(case.type, 'demo')
         self.assertEqual(case.name, 'create_case')
         self.assertEqual(case.opened_on, modified_on)
-        self.assertEqual(case.opened_by, 'user1')
+        self.assertUuidEqual(case.opened_by, user)
         self.assertEqual(case.modified_on, modified_on)
-        self.assertEqual(case.modified_by, 'user1')
+        self.assertUuidEqual(case.modified_by, user)
         self.assertTrue(case.server_modified_on > modified_on)
         self.assertFalse(case.closed)
         self.assertIsNone(case.closed_on)
@@ -57,30 +59,34 @@ class FundamentalCaseTests(TestCase):
 
     @run_with_all_backends
     def test_update_case(self):
+        user1 = uuid.uuid4().hex
+        owner1 = uuid.uuid4().hex
         case_id = uuid.uuid4().hex
         opened_on = datetime.utcnow()
         _submit_case_block(
-            True, case_id, user_id='user1', owner_id='owner1', case_type='demo',
+            True, case_id, user_id=user1, owner_id=owner1, case_type='demo',
             case_name='create_case', date_modified=opened_on, update={
                 'dynamic': '123'
             }
         )
 
         modified_on = datetime.utcnow()
+        user2 = uuid.uuid4().hex
+        owner2 = uuid.uuid4().hex
         _submit_case_block(
-            False, case_id, user_id='user2', owner_id='owner2',
+            False, case_id, user_id=user2, owner_id=owner2,
             case_name='update_case', date_modified=modified_on, update={
                 'dynamic': '1234'
             }
         )
 
         case = self.casedb.get_case(case_id)
-        self.assertEqual(case.owner_id, 'owner2')
+        self.assertUuidEqual(case.owner_id, owner2)
         self.assertEqual(case.name, 'update_case')
         self.assertEqual(case.opened_on, opened_on)
-        self.assertEqual(case.opened_by, 'user1')
+        self.assertUuidEqual(case.opened_by, user1)
         self.assertEqual(case.modified_on, modified_on)
-        self.assertEqual(case.modified_by, 'user2')
+        self.assertUuidEqual(case.modified_by, user2)
         self.assertTrue(case.server_modified_on > modified_on)
         self.assertFalse(case.closed)
         self.assertIsNone(case.closed_on)
@@ -91,37 +97,42 @@ class FundamentalCaseTests(TestCase):
         # same as update, closed, closed on, closed by
         case_id = uuid.uuid4().hex
         opened_on = datetime.utcnow()
+        user1 = uuid.uuid4().hex
+        owner1 = uuid.uuid4().hex
         _submit_case_block(
-            True, case_id, user_id='user1', owner_id='owner1', case_type='demo',
+            True, case_id, user_id=user1, owner_id=owner1, case_type='demo',
             case_name='create_case', date_modified=opened_on
         )
 
         modified_on = datetime.utcnow()
+        user2 = uuid.uuid4().hex
         _submit_case_block(
-            False, case_id, user_id='user2', date_modified=modified_on, close=True
+            False, case_id, user_id=user2, date_modified=modified_on, close=True
         )
 
         case = self.casedb.get_case(case_id)
-        self.assertEqual(case.owner_id, 'owner1')
+        self.assertUuidEqual(case.owner_id, owner1)
         self.assertEqual(case.modified_on, modified_on)
-        self.assertEqual(case.modified_by, 'user2')
+        self.assertUuidEqual(case.modified_by, user2)
         self.assertTrue(case.closed)
         self.assertEqual(case.closed_on, modified_on)
-        self.assertEqual(case.closed_by, 'user2')
+        self.assertUuidEqual(case.closed_by, user2)
         self.assertTrue(case.server_modified_on > modified_on)
 
     @run_with_all_backends
     def test_case_with_index(self):
         # same as update, indexes
         mother_case_id = uuid.uuid4().hex
+        user1 = uuid.uuid4().hex
+        owner1 = uuid.uuid4().hex
         _submit_case_block(
-            True, mother_case_id, user_id='user1', owner_id='owner1', case_type='mother',
+            True, mother_case_id, user_id=user1, owner_id=owner1, case_type='mother',
             case_name='mother', date_modified=datetime.utcnow()
         )
 
         child_case_id = uuid.uuid4().hex
         _submit_case_block(
-            True, child_case_id, user_id='user1', owner_id='owner1', case_type='child',
+            True, child_case_id, user_id=user1, owner_id=owner1, case_type='child',
             case_name='child', date_modified=datetime.utcnow(), index={
                 'mom': ('mother', mother_case_id)
             }
@@ -131,21 +142,23 @@ class FundamentalCaseTests(TestCase):
         self.assertEqual(len(case.indices), 1)
         index = case.indices[0]
         self.assertEqual(index.identifier, 'mom')
-        self.assertEqual(index.referenced_id, mother_case_id)
+        self.assertUuidEqual(index.referenced_id, mother_case_id)
         self.assertEqual(index.referenced_type, 'mother')
         self.assertEqual(index.relationship, 'child')
 
     @run_with_all_backends
     def test_update_index(self):
         mother_case_id = uuid.uuid4().hex
+        user1 = uuid.uuid4().hex
+        owner1 = uuid.uuid4().hex
         _submit_case_block(
-            True, mother_case_id, user_id='user1', owner_id='owner1', case_type='mother',
+            True, mother_case_id, user_id=user1, owner_id=owner1, case_type='mother',
             case_name='mother', date_modified=datetime.utcnow()
         )
 
         child_case_id = uuid.uuid4().hex
         _submit_case_block(
-            True, child_case_id, user_id='user1', owner_id='owner1', case_type='child',
+            True, child_case_id, user_id=user1, owner_id=owner1, case_type='child',
             case_name='child', date_modified=datetime.utcnow(), index={
                 'mom': ('mother', mother_case_id)
             }
@@ -155,7 +168,7 @@ class FundamentalCaseTests(TestCase):
         self.assertEqual(case.indices[0].identifier, 'mom')
 
         _submit_case_block(
-            False, child_case_id, user_id='user1', date_modified=datetime.utcnow(), index={
+            False, child_case_id, user_id=user1, date_modified=datetime.utcnow(), index={
                 'mom': ('other_mother', mother_case_id)
             }
         )
@@ -165,14 +178,16 @@ class FundamentalCaseTests(TestCase):
     @run_with_all_backends
     def test_delete_index(self):
         mother_case_id = uuid.uuid4().hex
+        user1 = uuid.uuid4().hex
+        owner1 = uuid.uuid4().hex
         _submit_case_block(
-            True, mother_case_id, user_id='user1', owner_id='owner1', case_type='mother',
+            True, mother_case_id, user_id=user1, owner_id=owner1, case_type='mother',
             case_name='mother', date_modified=datetime.utcnow()
         )
 
         child_case_id = uuid.uuid4().hex
         _submit_case_block(
-            True, child_case_id, user_id='user1', owner_id='owner1', case_type='child',
+            True, child_case_id, user_id=user1, owner_id=owner1, case_type='child',
             case_name='child', date_modified=datetime.utcnow(), index={
                 'mom': ('mother', mother_case_id)
             }
@@ -182,7 +197,7 @@ class FundamentalCaseTests(TestCase):
         self.assertEqual(len(case.indices), 1)
 
         _submit_case_block(
-            False, child_case_id, user_id='user1', date_modified=datetime.utcnow(), index={
+            False, child_case_id, user_id=user1, date_modified=datetime.utcnow(), index={
                 'mom': ('mother', '')
             }
         )
