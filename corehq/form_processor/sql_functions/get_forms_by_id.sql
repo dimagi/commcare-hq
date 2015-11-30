bc2dd5e849ac7c4973b75987ed92a2ed2aed14d0
Simon Kelly <skelly@dimagi.com>
2015-11-30 09:42:09 +0200

    change lang to plpgsql

--- corehq/form_processor/sql_functions/get_forms_by_id.sql
+++ corehq/form_processor/sql_functions/get_forms_by_id.sql
@@ -1,5 +1,7 @@
 DROP FUNCTION IF EXISTS get_forms_by_id(form_id text);
 
 CREATE FUNCTION get_forms_by_id(form_ids text[]) RETURNS SETOF form_processor_xforminstancesql AS $$
+BEGIN
     SELECT * FROM form_processor_xforminstancesql where form_id = ANY(form_ids);
-$$ LANGUAGE SQL;
+END;
+$$ LANGUAGE plpgsql;
