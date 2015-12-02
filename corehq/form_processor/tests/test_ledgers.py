6bdc85a425b5ebb4773de6bd4a99591fc73f4200
Cory Zue <czue@dimagi.com>
2015-12-02 11:58:54 +0200

    add transfer test

--- corehq/form_processor/tests/test_ledgers.py
+++ corehq/form_processor/tests/test_ledgers.py
@@ -35,6 +35,24 @@ class LedgerTests(TestCase):
         </balance>
         """
         self._submit_ledgers(BALANCE_BLOCK)
+        self._assert_ledger_state(100)
+        # make sure the form is part of the case's history
+        self.assertEqual(orignal_form_count + 1, len(self.interface.get_case_forms(self.case.case_id)))
+
+    @run_with_all_backends
+    def test_transfer_submission(self):
+        orignal_form_count = len(self.interface.get_case_forms(self.case.case_id))
+        TRANSFER_BLOCK = """
+        <transfer xmlns="http://commcarehq.org/ledger/v1" dest="{case_id}" date="2000-01-02" section-id="stock">
+            <entry id="{product_id}" quantity="100" />
+        </transfer >
+        """
+        self._submit_ledgers(TRANSFER_BLOCK)
+        self._assert_ledger_state(100)
+        # make sure the form is part of the case's history
+        self.assertEqual(orignal_form_count + 1, len(self.interface.get_case_forms(self.case.case_id)))
+
+    def _assert_ledger_state(self, expected_balance):
         ledgers = self.interface.ledger_processor.get_ledgers_for_case(self.case.case_id)
         self.assertEqual(1, len(ledgers))
         ledger = ledgers[0]
@@ -42,5 +60,3 @@ class LedgerTests(TestCase):
         self.assertEqual(self.product._id, ledger.entry_id)
         self.assertEqual('stock', ledger.section_id)
         self.assertEqual(100, ledger.balance)
-        # make sure the form is part of the case's history
-        self.assertEqual(orignal_form_count + 1, len(self.interface.get_case_forms(self.case.case_id)))
