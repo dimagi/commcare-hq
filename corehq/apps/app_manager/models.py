0c8d0374da202495c8a2c400bb215eca267ea09f
Jenny Schweers <jschweers@dimagi.com>
2015-12-02 11:21:23 -0500

    Remove connectors UI

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -1563,10 +1563,6 @@ class DetailTab(IndexedSchema):
     has_nodeset = BooleanProperty(default=False)
     nodeset = StringProperty()
 
-    # Any instance connectors necessary for the nodeset,
-    # e.g., "reports" => "jr://fixture/reports"
-    connectors = DictProperty()
-
 
 class DetailColumn(IndexedSchema):
     """
