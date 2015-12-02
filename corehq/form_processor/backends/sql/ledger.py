f88c8088e4ff53d7e7129aefeb059ad0c4844b7d
Cory Zue <czue@dimagi.com>
2015-12-02 12:08:32 +0200

    properly retrieve balance in sql models

--- corehq/form_processor/backends/sql/ledger.py
+++ corehq/form_processor/backends/sql/ledger.py
@@ -40,5 +40,7 @@ class LedgerProcessorSQL(LedgerProcessorInterface):
         return LedgerValue.objects.filter(case_id=case_id)
 
     def get_current_ledger_value(self, unique_ledger_reference):
-        # todo
-        return 0
+        try:
+            return LedgerValue.objects.get(**unique_ledger_reference._asdict()).balance
+        except LedgerValue.DoesNotExist:
+            return 0
