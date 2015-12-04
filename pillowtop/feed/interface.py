688a2302dd49c0ecd8009cacae33ec9fa8d38e49
Cory Zue <czue@dimagi.com>
2015-12-04 14:32:55 +0200

    move ChangeMeta in from commcare HQ

--- pillowtop/feed/interface.py
+++ pillowtop/feed/interface.py
@@ -1,4 +1,21 @@
 from abc import ABCMeta, abstractmethod
+from dimagi.ext import jsonobject
+
+
+class ChangeMeta(jsonobject.JsonObject):
+    """
+    Metadata about a change. If available, this will be set on Change.metadata.
+
+    This is currently only used in kafka-based pillows.
+    """
+    document_id = jsonobject.StringProperty(required=True)
+    data_source_type = jsonobject.StringProperty(required=True)
+    data_source_name = jsonobject.StringProperty(required=True)
+    document_type = jsonobject.StringProperty()
+    document_subtype = jsonobject.StringProperty()
+    domain = jsonobject.StringProperty()
+    is_deletion = jsonobject.BooleanProperty()
+    _allow_dynamic_properties = False
 
 
 class Change(object):
