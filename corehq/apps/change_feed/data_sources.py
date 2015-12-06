c628468ac53ba8e96cc7413f4d495aa81908638d
Cory Zue <czue@dimagi.com>
2015-12-06 18:25:28 +0200

    use custom exception

--- corehq/apps/change_feed/data_sources.py
+++ corehq/apps/change_feed/data_sources.py
@@ -1,3 +1,4 @@
+from corehq.apps.change_feed.exceptions import UnknownDocumentStore
 from corehq.util.couchdb_management import couch_config
 from pillowtop.dao.couch import CouchDocumentStore
 
@@ -8,6 +9,6 @@ def get_document_store(data_source_type, data_source_name):
     if data_source_type == COUCH:
         return CouchDocumentStore(couch_config.get_db_for_db_name(data_source_name))
     else:
-        raise NotImplementedError(
+        raise UnknownDocumentStore(
             'getting document stores for backend {} is not supported!'.format(data_source_type)
         )
