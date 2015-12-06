f16005a1d599ceb4685a3da13f66d0ed7a0ee91a
Cory Zue <czue@dimagi.com>
2015-12-06 18:23:17 +0200

    add get_document_store

--- corehq/apps/change_feed/data_sources.py
+++ corehq/apps/change_feed/data_sources.py
@@ -1,3 +1,13 @@
-
+from corehq.util.couchdb_management import couch_config
+from pillowtop.dao.couch import CouchDocumentStore
 
 COUCH = 'couch'
+
+
+def get_document_store(data_source_type, data_source_name):
+    if data_source_type == COUCH:
+        return CouchDocumentStore(couch_config.get_db_for_db_name(data_source_name))
+    else:
+        raise NotImplementedError(
+            'getting document stores for backend {} is not supported!'.format(data_source_type)
+        )
