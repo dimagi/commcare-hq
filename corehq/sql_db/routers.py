d5c1fc415a00065e977924a9f5817f37eaaa5651
Simon Kelly <skelly@dimagi.com>
2015-12-08 15:01:05 +0200

    extract allow_migrate function

--- corehq/sql_db/routers.py
+++ corehq/sql_db/routers.py
@@ -1,25 +1,33 @@
 from .config import PartitionConfig
 
 PROXY_APP = 'sql_proxy_accessors'
+FORM_PROCESSOR_APP = 'form_processor'
 SQL_ACCESSORS_APP = 'sql_accessors'
 
 
 class PartitionRouter(object):
 
-    def __init__(self):
-        self.config = PartitionConfig()
-
-    def allow_migrate(self, db, app_label, model=None, **hints):
-        if app_label == PROXY_APP:
-            return (db == self.config.get_proxy_db or
-                    db in self.config.get_form_processing_dbs)
-        elif app_label == SQL_ACCESSORS_APP:
-            return db in self.config.get_form_processing_dbs()
-        else:
-            return db == self.config.get_main_db()
+    def allow_migrate(self, db, model):
+        app_label = model._meta.app_label
+        return allow_migrate(db, app_label)
 
 
 class MonolithRouter(object):
 
     def allow_migrate(self, db, app_label, model=None, **hints):
         return app_label != PROXY_APP
+
+
+def allow_migrate(db, app_label):
+    partition_config = PartitionConfig()
+    if app_label == PROXY_APP:
+        return db == partition_config.get_proxy_db()
+    elif app_label == FORM_PROCESSOR_APP:
+        return (
+            db == partition_config.get_proxy_db() or
+            db in partition_config.get_form_processing_dbs()
+        )
+    elif app_label == SQL_ACCESSORS_APP:
+        return db in partition_config.get_form_processing_dbs()
+    else:
+        return db == partition_config.get_main_db()
