75d3d8c2b01f7a894623c0328be1a755d46f4f5e
Cory Zue <czue@dimagi.com>
2015-12-09 11:34:23 +0200

    replace function with a different couch view

--- corehq/apps/reports/analytics/couchaccessors.py
+++ corehq/apps/reports/analytics/couchaccessors.py
@@ -1,26 +1,13 @@
-from corehq.util.couch import stale_ok
+from corehq.util.quickcache import quickcache
+from couchforms.analytics import get_last_form_submission_by_xmlns
 from couchforms.models import XFormInstance
-from dimagi.utils.couch.cache import cache_core
 
 
 def update_reports_analytics_indexes():
-    XFormInstance.get_db().view('reports_forms/name_by_xmlns', limit=1).all()
+    XFormInstance.get_db().view('reports_forms/all_forms', limit=1).all()
 
 
+@quickcache(['domain', 'xmlns'], timeout=5 * 60)
 def guess_form_name_from_submissions_using_xmlns(domain, xmlns):
-    key = ["xmlns", domain, xmlns]
-    results = cache_core.cached_view(
-        XFormInstance.get_db(),
-        'reports_forms/name_by_xmlns',
-        reduce=False,
-        startkey=key,
-        endkey=key + [{}],
-        limit=1,
-        stale=stale_ok(),
-        cache_expire=60
-    )
-    try:
-        data = list(results)[0]
-        return data['value']
-    except IndexError:
-        return None
+    last_form = get_last_form_submission_by_xmlns(domain, xmlns)
+    return last_form.form.get('@name') if last_form else None
