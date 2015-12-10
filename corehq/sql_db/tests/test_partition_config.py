05a233dd5c84dc76d58234e7ce580c7f96aa2d68
Simon Kelly <skelly@dimagi.com>
2015-12-10 12:01:57 +0200

    update tests

--- corehq/sql_db/tests/test_partition_config.py
+++ corehq/sql_db/tests/test_partition_config.py
@@ -1,40 +1,49 @@
 from django.test import SimpleTestCase
 from django.test.utils import override_settings
 
+from corehq.sql_db.config import DbShard
 from corehq.sql_db.management.commands.configure_pl_proxy_cluster import get_shard_config_strings
 from ..config import PartitionConfig
-from ..exceptions import PartitionValidationError
+from ..exceptions import PartitionValidationError, NotPowerOf2Error, NotZeroStartError, NonContinuousShardsError, \
+    ShardOverlapError
 
-TEST_PARTITION_CONFIG = {
-    'shards': {
-        'default': [0, 1],
-        'proxy': [2, 3],
-    },
-    'groups': {
-        'main': ['default'],
-        'proxy': ['proxy'],
-        'form_processing': ['db1', 'db2'],
-    }
-}
 
-INVALID_SHARD_RANGE = {
-    'shards': {
-        'default': [0, 2],
-        'proxy': [1, 4],
-    },
-    'groups': {
-        'main': ['default'],
-        'proxy': ['proxy'],
-        'form_processing': ['db1', 'db2'],
+def _get_partition_config(shard_config):
+    return {
+        'shards': shard_config,
+        'groups': {
+            'main': ['default'],
+            'proxy': ['proxy'],
+            'form_processing': ['db1', 'db2'],
+        }
     }
-}
+
+TEST_PARTITION_CONFIG = _get_partition_config({
+    'db1': [0, 1],
+    'db2': [2, 3],
+})
+
+INVALID_SHARD_RANGE_START = _get_partition_config({
+    'db1': [1, 2],
+    'db2': [3, 4],
+})
+
+INVALID_SHARD_RANGE_CONTINUOUS = _get_partition_config({
+    'db1': [0, 4],
+    'db2': [7, 8],
+})
+
+INVALID_SHARD_RANGE_POWER_2 = _get_partition_config({
+    'db1': [0, 4],
+    'db2': [5, 9],
+})
 
 db_dict = {'NAME': 'commcarehq', 'USER': 'commcarehq', 'HOST': 'hqdb0', 'PORT': 5432}
 TEST_DATABASES = {
     'default': db_dict,
     'proxy': db_dict,
-    'db1': db_dict,
-    'db2': db_dict,
+    'db1': {'NAME': 'db1', 'USER': 'commcarehq', 'HOST': 'hqdb0', 'PORT': 5432},
+    'db2': {'NAME': 'db2', 'USER': 'commcarehq', 'HOST': 'hqdb0', 'PORT': 5432},
 }
 
 
@@ -51,28 +60,40 @@ class TestPartitionConfig(SimpleTestCase):
 
     def test_shard_mapping(self):
         config = PartitionConfig()
-        mapping = config.shard_mapping()
-        self.assertEquals(mapping, {
-            0: 'default',
-            1: 'default',
-            2: 'proxy',
-            3: 'proxy',
-        })
+        shards = config.get_shards()
+        self.assertEquals(shards, [
+            DbShard(0, 'db1'),
+            DbShard(1, 'db1'),
+            DbShard(2, 'db2'),
+            DbShard(3, 'db2'),
+        ])
 
-    @override_settings(PARTITION_DATABASE_CONFIG=INVALID_SHARD_RANGE)
-    def test_invalid_shard_range(self):
-        with self.assertRaises(PartitionValidationError):
+    @override_settings(PARTITION_DATABASE_CONFIG=INVALID_SHARD_RANGE_START)
+    def test_invalid_shard_range_start(self):
+        with self.assertRaises(NotZeroStartError):
+            PartitionConfig()
+
+    @override_settings(PARTITION_DATABASE_CONFIG=INVALID_SHARD_RANGE_CONTINUOUS)
+    def test_invalid_shard_range_continuous(self):
+        with self.assertRaises(NonContinuousShardsError):
+            PartitionConfig()
+
+    @override_settings(PARTITION_DATABASE_CONFIG=INVALID_SHARD_RANGE_POWER_2)
+    def test_invalid_shard_range_power_2(self):
+        with self.assertRaises(NotPowerOf2Error):
             PartitionConfig()
 
 
 @override_settings(DATABASES=TEST_DATABASES)
 class PlProxyTests(SimpleTestCase):
     def test_get_shard_config_strings(self):
-        shard_mapping = {
-            1: 'db1',
-            2: 'db1',
-            3: 'db2',
-        }
-        configs = get_shard_config_strings(shard_mapping)
+        shards = [
+            DbShard(0, 'db1'),
+            DbShard(1, 'db1'),
+            DbShard(2, 'db2'),
+        ]
+        configs = get_shard_config_strings(shards)
         self.assertEqual(3, len(configs))
-        self.assertIn("p1 'dbname=test_commcarehq host=hqdb0 port=5432'", configs)
+        self.assertIn("p0 'dbname=test_db1 host=hqdb0 port=5432'", configs)
+        self.assertIn("p1 'dbname=test_db1 host=hqdb0 port=5432'", configs)
+        self.assertIn("p2 'dbname=test_db2 host=hqdb0 port=5432'", configs)
