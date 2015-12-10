163d5af8392fe8800119aa8dc714271854120671
Biyeun Buczyk <biyeun@dimagi.com>
2015-12-10 18:05:57 -0500

    add tour utils

---
+++ corehq/apps/tour/static/tour/js/tour.utils.js
@@ -0,0 +1,15 @@
+(function(window) {
+    'use strict';
+
+    window.TourUtils = {
+        getEndTourAsync: function (endUrl) {
+            return function (tour) {
+                $.post(endUrl);
+            };
+        },
+        getCustomTemplate: function () {
+            return '<div class="popover guided-tour" role="tooltip"> <div class="arrow"></div> <h3 class="popover-title"></h3> <div class="popover-content"></div> <div class="popover-navigation"> <div class="btn-group"> <button class="btn btn-sm btn-default" data-role="prev">&laquo; Prev</button> <button class="btn btn-sm btn-default" data-role="next">Next &raquo;</button> <button class="btn btn-sm btn-default" data-role="pause-resume" data-pause-text="Pause" data-resume-text="Resume">Pause</button> </div> <button class="btn btn-sm btn-default" data-role="end">End tour</button> </div> </div>'
+        }
+    };
+
+})(window);
