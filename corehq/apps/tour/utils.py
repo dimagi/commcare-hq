ae4294239d5246d9a4b3f944f5f02f9ff859b93a
Biyeun Buczyk <biyeun@dimagi.com>
2015-12-10 18:05:02 -0500

    update guided tour models and uitls

--- corehq/apps/tour/utils.py
+++
@@ -1,9 +0,0 @@
-from corehq.apps.tour.models import GuidedTours
-
-
-def activate_tour_for_user(tour_slug, user):
-    GuidedTours.enable_tour_for_user(tour_slug, user.username)
-
-
-def tour_is_enabled_for_user(tour_slug, user):
-    return GuidedTours.is_tour_enabled_for_user(tour_slug, user)
