f9d914aa1a3648d19541d07ebe46de47a9382576
Giovanni Capalbo <gcapalbo@dimagi.com>
2015-12-11 08:33:37 -0500

    Add DomainRateLimiter

--- dimagi/utils/tests/test_rate_limit.py
+++ dimagi/utils/tests/test_rate_limit.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from dimagi.utils.rate_limit import rate_limit
+from dimagi.utils.rate_limit import rate_limit, DomainRateLimiter
 from django.test import SimpleTestCase
 
 
@@ -17,3 +17,22 @@ class RateLimitTestCase(SimpleTestCase):
 
         self.assertEqual(rate_limit_count, 40)
         self.assertGreater(iteration_count, 40)
+
+    def test_domain_rate_limit(self):
+        rate_limiter = DomainRateLimiter('rate-limit-domain-', 10, 3)
+        domains = ('d1', 'd2')
+        domain_counts = {domain: 0 for domain in domains}
+
+        start = datetime.utcnow()
+        iteration_count = 0
+
+        while (datetime.utcnow() - start) < timedelta(seconds=5):
+            # Only allow 10 actions every 3 seconds in an 5 second period of time
+            for domain in domains:
+                if rate_limiter.can_perform_action(domain):
+                    domain_counts[domain] += 1
+            iteration_count += 1
+
+        for domain in domains:
+            self.assertEqual(domain_counts[domain], 20)
+        self.assertGreater(iteration_count, 20)
