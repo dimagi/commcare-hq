5c7811c10d503d5f86783abaac0531d444589d0d
Daniel Roberts <droberts@dimagi.com>
2015-12-11 15:44:39 -0500

    replace (cases|forms)_over_time with all_docs/by_doc_type

--- corehq/apps/hqadmin/dbaccessors.py
+++ corehq/apps/hqadmin/dbaccessors.py
@@ -1,14 +1,12 @@
+from corehq.dbaccessors.couchapps.all_docs import get_doc_count_by_type, \
+    get_all_docs_with_doc_types
 from corehq.util.test_utils import unit_testing_only
 from couchforms.models import XFormInstance
 
 
 @unit_testing_only
 def get_all_forms_in_all_domains():
-    return XFormInstance.view(
-        'hqadmin/forms_over_time',
-        reduce=False,
-        include_docs=True,
-    ).all()
+    return get_all_docs_with_doc_types(XFormInstance.get_db(), 'XFormInstance')
 
 
 def get_number_of_forms_in_all_domains():
@@ -21,4 +19,4 @@ def get_number_of_forms_in_all_domains():
 
     """
 
-    return XFormInstance.view('hqadmin/forms_over_time').one()['value']
+    return get_doc_count_by_type(XFormInstance.get_db(), 'XFormInstance')
