da8e4b214d77fc7fa37614f69830ae379cdf7988
Giovanni Capalbo <gcapalbo@dimagi.com>
2015-12-11 08:35:37 -0500

    Shorten time of tests

--- dimagi/utils/tests/test_rate_limit.py
+++ dimagi/utils/tests/test_rate_limit.py
@@ -9,14 +9,14 @@ class RateLimitTestCase(SimpleTestCase):
         rate_limit_count = 0
         iteration_count = 0
 
-        while (datetime.utcnow() - start) < timedelta(seconds=18):
-            # Only allow 10 actions every 5 seconds in an 18 second period of time
-            if rate_limit('rate-limit-test', actions_allowed=10, how_often=5):
+        while (datetime.utcnow() - start) < timedelta(seconds=5):
+            # Only allow 10 actions every 3 seconds in an 5 second period of time
+            if rate_limit('rate-limit-test', actions_allowed=10, how_often=3):
                 rate_limit_count += 1
             iteration_count += 1
 
-        self.assertEqual(rate_limit_count, 40)
-        self.assertGreater(iteration_count, 40)
+        self.assertEqual(rate_limit_count, 20)
+        self.assertGreater(iteration_count, 20)
 
     def test_domain_rate_limit(self):
         rate_limiter = DomainRateLimiter('rate-limit-domain-', 10, 3)
