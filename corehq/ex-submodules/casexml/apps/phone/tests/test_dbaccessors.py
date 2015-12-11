986e3c688121c73be5199c00d3792fdb08df6ca6
Daniel Roberts <droberts@dimagi.com>
2015-12-11 13:28:10 -0500

    add multi-db check to phone.DBAccessorsTest

--- corehq/ex-submodules/casexml/apps/phone/tests/test_dbaccessors.py
+++ corehq/ex-submodules/casexml/apps/phone/tests/test_dbaccessors.py
@@ -1,4 +1,5 @@
 import datetime
+from dimagi.utils.couch.database import get_db
 from django.test import TestCase
 from casexml.apps.phone.dbaccessors.sync_logs_by_user import get_sync_logs_for_user, \
     get_last_synclog_for_user
@@ -8,6 +9,7 @@ from corehq.util.test_utils import DocTestMixin
 
 class DBAccessorsTest(TestCase, DocTestMixin):
     dependent_apps = []
+    maxDiff = None
 
     @classmethod
     def setUpClass(cls):
@@ -21,15 +23,22 @@ class DBAccessorsTest(TestCase, DocTestMixin):
         cls.docs = cls.sync_logs + sync_logs_other
         for doc in cls.docs:
             doc.save()
+        cls.legacy_sync_logs = [
+            SyncLog(user_id=cls.user_id, date=datetime.datetime(2014, 12, 31, 0, 0))
+        ]
+        for doc in cls.legacy_sync_logs:
+            get_db(None).save_doc(doc)
 
     @classmethod
     def tearDownClass(cls):
         for doc in cls.docs:
             doc.delete()
+        get_db(None).delete_docs(cls.legacy_sync_logs)
 
     def test_get_sync_logs_for_user(self):
         self.assert_doc_lists_equal(
-            get_sync_logs_for_user(self.user_id, 4), self.sync_logs)
+            get_sync_logs_for_user(self.user_id, 4),
+            self.sync_logs + self.legacy_sync_logs)
 
     def test_get_last_synclog_for_user(self):
         self.assert_docs_equal(
