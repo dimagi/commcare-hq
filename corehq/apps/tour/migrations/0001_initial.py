1f7b2f1a2fe964603a61ff8c0fa85618f782059d
Biyeun Buczyk <biyeun@dimagi.com>
2015-12-11 15:15:48 -0500

    new initial migration

---
+++ corehq/apps/tour/migrations/0001_initial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='GuidedTour',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('tour_slug', models.CharField(unique=True, max_length=255, db_index=True)),
+                ('date_completed', models.DateTimeField(auto_now=True)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, unique=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
