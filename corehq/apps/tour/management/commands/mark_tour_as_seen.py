60b184fe921716b6bcf1e6f970dfd15947ff9952
Biyeun Buczyk <biyeun@dimagi.com>
2015-12-11 13:01:33 -0500

    command cleanup

--- corehq/apps/tour/management/commands/mark_tour_as_seen.py
+++ corehq/apps/tour/management/commands/mark_tour_as_seen.py
@@ -3,6 +3,12 @@ from django.core.management.base import LabelCommand
 import sys
 from corehq.apps.tour.models import GuidedTours
 
+CONFIRM_FOR_SINGLE_USER = """MARK Tour {tour_slug} as SEEN for User {username}?
+    Type 'yes' to continue or 'no' to cancel: """
+
+CONFIRM_FORM_ALL_USERS = """MARK Tour {tour_slug} as SEEN for ALL USERS?
+    Type 'yes' to continue or 'no' to cancel: """
+
 
 class Command(LabelCommand):
     help = "marks a tour with tour_slug as seen for all existing users"
@@ -11,15 +17,14 @@ class Command(LabelCommand):
     def handle(self, tour_slug, *args, **kwargs):
         if len(args) > 0:
             username = args[0]
-            confirm = raw_input("""MARK Tour {tour_slug} as SEEN for User {username}?
-    Type 'yes' to continue or 'no' to cancel: """.format(tour_slug=tour_slug, username=username))
+            confirm = raw_input(CONFIRM_FOR_SINGLE_USER.format(
+                tour_slug=tour_slug, username=username))
             if confirm == 'yes':
                 user = User.objects.filter(username=username).first()
                 GuidedTours.mark_as_seen(user, tour_slug)
                 print("Complete")
         else:
-            confirm = raw_input("""MARK Tour {tour_slug} as SEEN for ALL USERS?
-    Type 'yes' to continue or 'no' to cancel: """.format(tour_slug=tour_slug))
+            confirm = raw_input(CONFIRM_FORM_ALL_USERS.format(tour_slug=tour_slug))
             if confirm == 'yes':
                 for user in User.objects.all():
                     sys.stdout.write(".")
