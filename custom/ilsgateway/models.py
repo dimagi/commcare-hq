bb330fb737fd7022dea79dcc43aa24f8bc62a686
Simon Kelly <skelly@dimagi.com>
2015-12-14 16:23:53 +0200

    invert if condition

--- custom/ilsgateway/models.py
+++ custom/ilsgateway/models.py
@@ -522,40 +522,42 @@ def domain_pre_delete_receiver(domain, **kwargs):
 
     def _delete_ilsgateway_data(domain_name):
         locations_ids = SQLLocation.objects.filter(domain=domain_name).values_list('location_id', flat=True)
-        if locations_ids:
-            DeliveryGroupReport.objects.filter(location_id__in=locations_ids).delete()
-            SupplyPointWarehouseRecord.objects.filter(supply_point__in=locations_ids).delete()
+        if not locations_ids:
+            return
 
-            with connection.cursor() as cursor:
-                cursor.execute(
-                    "DELETE FROM ilsgateway_alert WHERE location_id IN "
-                    "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
-                )
-                cursor.execute(
-                    "DELETE FROM ilsgateway_groupsummary WHERE org_summary_id IN "
-                    "(SELECT id FROM ilsgateway_organizationsummary WHERE location_id IN "
-                    "(SELECT location_id FROM locations_sqllocation WHERE domain=%s))", [domain_name]
-                )
+        DeliveryGroupReport.objects.filter(location_id__in=locations_ids).delete()
+        SupplyPointWarehouseRecord.objects.filter(supply_point__in=locations_ids).delete()
 
-                cursor.execute(
-                    "DELETE FROM ilsgateway_organizationsummary WHERE location_id IN "
-                    "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
-                )
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "DELETE FROM ilsgateway_alert WHERE location_id IN "
+                "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+            )
+            cursor.execute(
+                "DELETE FROM ilsgateway_groupsummary WHERE org_summary_id IN "
+                "(SELECT id FROM ilsgateway_organizationsummary WHERE location_id IN "
+                "(SELECT location_id FROM locations_sqllocation WHERE domain=%s))", [domain_name]
+            )
 
-                cursor.execute(
-                    "DELETE FROM ilsgateway_productavailabilitydata WHERE location_id IN "
-                    "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
-                )
+            cursor.execute(
+                "DELETE FROM ilsgateway_organizationsummary WHERE location_id IN "
+                "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+            )
 
-                cursor.execute(
-                    "DELETE FROM ilsgateway_supplypointstatus WHERE location_id IN "
-                    "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
-                )
+            cursor.execute(
+                "DELETE FROM ilsgateway_productavailabilitydata WHERE location_id IN "
+                "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+            )
 
-                cursor.execute(
-                    "DELETE FROM ilsgateway_historicallocationgroup WHERE location_id_id IN "
-                    "(SELECT id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
-                )
+            cursor.execute(
+                "DELETE FROM ilsgateway_supplypointstatus WHERE location_id IN "
+                "(SELECT location_id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+            )
+
+            cursor.execute(
+                "DELETE FROM ilsgateway_historicallocationgroup WHERE location_id_id IN "
+                "(SELECT id FROM locations_sqllocation WHERE domain=%s)", [domain_name]
+            )
 
     return [
         CustomDeletion('ilsgateway', _delete_ilsgateway_data),
