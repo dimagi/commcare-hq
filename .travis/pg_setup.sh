abed36b4887655162ead9ec0f120075e53ba8528
Simon Kelly <skelly@dimagi.com>
2015-12-14 11:12:35 +0200

    try removing postgresql first

--- .travis/pg_setup.sh
+++ .travis/pg_setup.sh
@@ -3,19 +3,35 @@
 
 set -ev
 
-# install plproxy extension (needed until its added to the travis APT whitelist)
-# https://github.com/travis-ci/apt-package-whitelist/issues/2053
-sudo apt-get update
-sudo apt-get install postgresql-9.1-plproxy postgresql-contrib-$PGVERSION postgresql-server-dev-$PGVERSION
+# ------------------------------------------------------------------------------
+# Remove PostgreSQL and all its files
+# ------------------------------------------------------------------------------
+sudo service postgresql stop
+sudo apt-get remove postgresql libpq-dev libpq5 postgresql-client-common postgresql-common -qq --purge -y
+sudo rm -rf /var/lib/postgresql
 
-# install pghashlib
-sudo /etc/init.d/postgresql stop
+sudo apt-get update -qq
 
+# ------------------------------------------------------------------------------
+# Install PostgreSQL (always install from PostgreSQL Apt repository)
+# ------------------------------------------------------------------------------
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install -y -qq postgresql-$PGVERSION postgresql-contrib-$PGVERSION postgresql-server-dev-$PGVERSION
+
+# Install packages
+# ------------------------------------------------------------------------------
+echo "Installing packages ... this may take some time."
+sudo apt-get install -y postgresql-$PGVERSION-plproxy postgresql-contrib-$PGVERSION postgresql-server-dev-$PGVERSION
+
+# Build and compile pghashlib
 wget --quiet https://github.com/markokr/pghashlib/archive/master.zip -O pghashlib.zip
 unzip pghashlib.zip
 cd pghashlib-master
 echo $PGVERSION
 PG_CONFIG=/usr/lib/postgresql/$PGVERSION/bin/pg_config make
 ! sudo PG_CONFIG=/usr/lib/postgresql/$PGVERSION/bin/pg_config make install
-sudo /etc/init.d/postgresql stop
-sudo /etc/init.d/postgresql start $PGVERSION
+sudo ldconfig
+
+# ------------------------------------------------------------------------------
+# Restart once
+# ------------------------------------------------------------------------------
+sudo /etc/init.d/postgresql restart
