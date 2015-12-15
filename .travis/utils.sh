271b26b30309607f40a45d6e3eb4646ab02a1104
Ben Rudolph <brudolph@dimagi.com>
2015-12-15 16:55:32 +0200

    cache download dirs

--- .travis/utils.sh
+++ .travis/utils.sh
@@ -1,21 +1,31 @@
+DOWNLOAD_DIR=${HOME}/downloads
+
 setup_elasticsearch() {
     es_version=0.90.13
+    es_dir=${DOWNLOAD_DIR}/elasticsearch-${es_version}
 
     echo "Installing elasticsearch version ${es_version}"
 
-    wget "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${es_version}.tar.gz" -O elasticsearch.tar.gz
+    if [ ! -d ${es_dir } ]; then
+        wget "https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${es_version}.tar.gz" -O ${DOWNLOAD_DIR}/elasticsearch.tar.gz
 
-    tar xvzf elasticsearch.tar.gz
-    nohup bash -c "cd elasticsearch-${es_version} && bin/elasticsearch &"
+        tar xvzf ${DOWNLOAD_DIR}/elasticsearch.tar.gz
+    fi
+
+    nohup bash -c "cd ${es_dir} && bin/elasticsearch &"
     sleep 5
 }
 
 setup_kafka() {
+    kafka_dir=${DOWNLOAD_DIR}/kafka
     # kafka install, copied from https://github.com/wvanbergen/kafka/blob/master/.travis.yml
-    wget http://www.us.apache.org/dist/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz -O kafka.tgz
-    mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
-    nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
-    nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
+    if [ ! -d ${es_dir } ]; then
+        wget http://www.us.apache.org/dist/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz -O ${DOWNLOAD_DIR}/kafka.tgz
+        mkdir -p ${kafka_dir} && tar xzf ${DOWNLOAD_DIR}/kafka.tgz -C ${kafka_dir} --strip-components 1
+    fi
+
+    nohup bash -c "cd ${kafka_dir} && bin/zookeeper-server-start.sh config/zookeeper.properties &"
+    nohup bash -c "cd ${kafka_dir} && bin/kafka-server-start.sh config/server.properties &"
     sleep 5
-    kafka/bin/kafka-topics.sh --create --partitions 1 --replication-factor 1 --topic case --zookeeper localhost:2181
+    ${kafka_dir}/bin/kafka-topics.sh --create --partitions 1 --replication-factor 1 --topic case --zookeeper localhost:2181
 }
