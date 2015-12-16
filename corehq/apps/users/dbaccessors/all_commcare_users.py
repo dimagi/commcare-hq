236e43c2899ad95e35cde437809666518ab73348
Jenny Schweers <jschweers@dimagi.com>
2015-12-16 12:29:19 -0500

    undo previous code

--- corehq/apps/users/dbaccessors/all_commcare_users.py
+++ corehq/apps/users/dbaccessors/all_commcare_users.py
@@ -20,7 +20,7 @@ def get_all_commcare_users_by_domain(domain):
             ):
                 yield user['id']
 
-    return imap(CommCareUser.wrap, iter_docs(CommCareUser.get_db(), _get_ids_by_domain(domain)))
+    return imap(CommCareUser.wrap, iter_docs(CommCareUser.get_db(), get_ids(domain)))
 
 
 def get_user_docs_by_username(usernames):
