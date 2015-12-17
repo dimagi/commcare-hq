0c183b15c4696b68c93545176984a203c9332fa5
Norman Hooper <nhooper@dimagi.com>
2015-12-17 13:51:51 +0200

    OtaRestoreUrlTests

--- corehq/apps/ota/tests/app_aware_restore.py
+++ corehq/apps/ota/tests/app_aware_restore.py
@@ -11,6 +11,27 @@ from corehq.apps.userreports.tests import get_sample_report_config
 from corehq.apps.users.models import CommCareUser
 
 
+class OtaRestoreUrlTests(TestCase):
+    domain = 'test_domain'
+
+    @classmethod
+    def setUpClass(cls):
+        cls.app = Application.new_app(cls.domain, 'Test App', application_version=APP_V2)
+        cls.app.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.app.delete()
+
+    def test_app_aware_url(self):
+        toggles.APP_AWARE_SYNC.set(self.domain, True, toggles.NAMESPACE_DOMAIN)
+        self.assertIn(self.app._id, self.app.ota_restore_url)
+
+    def test_default_url(self):
+        toggles.APP_AWARE_SYNC.set(self.domain, False, toggles.NAMESPACE_DOMAIN)
+        self.assertNotIn(self.app._id, self.app.ota_restore_url)
+
+
 class AppAwareSyncTests(TestCase):
     domain = 'test_domain'
     rows = [{
