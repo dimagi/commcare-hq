e671454b7e5a009cb4b25b41ad3d913523c9a94c
NoahCarnahan <ncarnahan@dimagi.com>
2015-12-17 10:20:12 -0500

    Fix botched merge

--- corehq/form_processor/migrations/0045_casetransaction_sync_log_id.py
+++ corehq/form_processor/migrations/0045_casetransaction_sync_log_id.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-from corehq.form_processor.utils.migration import migrate_sql_function
+from corehq.sql_db.operations import RawSQLMigration
 
+migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'))
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('form_processor', '0043_rename_to_match'),
+        ('form_processor', '0044_remove_foreign_key_to_case'),
     ]
 
     operations = [
@@ -18,5 +19,5 @@ class Migration(migrations.Migration):
             field=models.CharField(max_length=255, null=True),
             preserve_default=True,
         ),
-        migrate_sql_function('save_case_and_related_models'),
+        migrator.get_migration('save_case_and_related_models.sql'),
     ]
