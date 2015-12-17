7c1b08023338a2ae9bc251b283c92d36f485faaa
Daniel Roberts <droberts@dimagi.com>
2015-12-17 16:19:36 -0500

    update billing link in Create Mobile Worker modal

--- corehq/apps/users/views/mobile/users.py
+++ corehq/apps/users/views/mobile/users.py
@@ -441,9 +441,9 @@ class MobileWorkerListView(JSONResponseMixin, BaseUserSettingsView):
             'custom_field_names': [f.label for f in self.custom_data.fields],
             'can_bulk_edit_users': self.can_bulk_edit_users,
             'can_add_extra_users': self.can_add_extra_users,
-            'pagination_limit_cookie_name': ('hq.pagination.limit'
-                                             '.mobile_workers_list.%s'
-                                             % self.domain)
+            'pagination_limit_cookie_name': (
+                'hq.pagination.limit.mobile_workers_list.%s' % self.domain),
+            'can_edit_billing_info': self.request.couch_user.is_domain_admin(self.domain)
         }
 
     @property
