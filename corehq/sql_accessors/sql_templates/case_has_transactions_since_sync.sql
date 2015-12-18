2c6f08ae5e343c5cdd325b635de7818315e327bb
NoahCarnahan <ncarnahan@dimagi.com>
2015-12-18 13:37:42 -0500

    Make sql function PostgreSQL 9.1 compatible

--- corehq/sql_accessors/sql_templates/case_has_transactions_since_sync.sql
+++ corehq/sql_accessors/sql_templates/case_has_transactions_since_sync.sql
@@ -4,8 +4,8 @@ CREATE FUNCTION case_has_transactions_since_sync(p_case_id text, p_sync_log_id t
   RETURNS boolean AS $$
     SELECT EXISTS(
       SELECT TRUE FROM form_processor_casetransaction AS transaction_table
-        WHERE transaction_table.case_id = p_case_id
-          AND transaction_table.server_date > sync_log_date
-          AND transaction_table.sync_log_id IS DISTINCT FROM p_sync_log_id
+        WHERE transaction_table.case_id = $1
+          AND transaction_table.server_date > $3
+          AND transaction_table.sync_log_id IS DISTINCT FROM $2
     );
 $$ LANGUAGE sql;
