09c9e0b20fbbf919644c1b2c0de3b457a8576b5c
Nick Pellegrino <npellegrino@dimagi.com>
2015-12-26 14:27:51 -0500

    add total row to fixture

--- corehq/apps/app_manager/fixtures/mobile_ucr.py
+++ corehq/apps/app_manager/fixtures/mobile_ucr.py
@@ -14,6 +14,10 @@ from corehq.util.xml_utils import serialize
 from corehq.apps.userreports.exceptions import UserReportsError
 from corehq.apps.userreports.models import ReportConfiguration
 from corehq.apps.userreports.reports.factory import ReportFactory
+from corehq.apps.userreports.reports.util import (
+    get_expanded_columns,
+    get_total_row,
+)
 
 
 class ReportFixturesProvider(object):
@@ -84,14 +88,35 @@ class ReportFixturesProvider(object):
         deferred_fields = {ui_filter.field for ui_filter in defer_filters.values()}
         filter_options_by_field = defaultdict(set)
 
-        for i, row in enumerate(data_source.get_data()):
-            row_elem = ElementTree.Element('row', attrib={'index': str(i)})
+        def _row_to_row_elem(row, index, is_total_row=False):
+            row_elem = ElementTree.Element(
+                'row',
+                attrib={
+                    'index': str(i),
+                    'is_total_row': str(is_total_row),
+                }
+            )
             for k in sorted(row.keys()):
                 value = serialize(row[k])
                 row_elem.append(self._element('column', value, attrib={'id': k}))
                 if k in deferred_fields:
                     filter_options_by_field[k].add(value)
-            rows_elem.append(row_elem)
+            return row_elem
+
+        for i, row in enumerate(data_source.get_data()):
+            rows_elem.append(_row_to_row_elem(row, i))
+
+        if data_source.has_total_row:
+            total_row = get_total_row(
+                data_source.get_data(),
+                data_source.aggregation_columns,
+                data_source.column_configs,
+                get_expanded_columns(data_source.column_configs, data_source.config)
+            )
+            rows_elem.append(_row_to_row_elem(
+                total_row, data_source.get_total_records(),
+                is_total_row=True,
+            ))
 
         filters_elem = self._element('filters')
         for filter_slug, ui_filter in defer_filters.items():
