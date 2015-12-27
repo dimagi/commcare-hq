d4a9f4542a4de5cb48c9bab8df2d5af3b8915d4b
Nick Pellegrino <npellegrino@dimagi.com>
2015-12-27 15:00:36 -0500

    only send new subscription email when it's starting

--- corehq/apps/accounting/tasks.py
+++ corehq/apps/accounting/tasks.py
@@ -72,7 +72,8 @@ def deactivate_subscriptions(based_on_date=None):
             subscription.is_active = False
             subscription.save()
             next_subscription = subscription.next_subscription
-            if next_subscription and next_subscription.date_start == ending_date:
+            activate_next_subscription = next_subscription and next_subscription.date_start == ending_date
+            if activate_next_subscription:
                 new_plan_version = next_subscription.plan_version
                 next_subscription.is_active = True
                 next_subscription.save()
@@ -83,7 +84,7 @@ def deactivate_subscriptions(based_on_date=None):
                 downgraded_privileges=downgraded_privs,
                 upgraded_privileges=upgraded_privs,
                 old_subscription=subscription,
-                new_subscription=next_subscription,
+                new_subscription=next_subscription if activate_next_subscription else None,
             )
 
 
