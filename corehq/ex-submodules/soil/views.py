f7292fc9b675bcdec12523bfbc0dc04df2a8a025
Nick Pellegrino <npellegrino@dimagi.com>
2015-12-29 16:47:33 -0500

    [refactor] organize imports

--- corehq/ex-submodules/soil/views.py
+++ corehq/ex-submodules/soil/views.py
@@ -1,18 +1,19 @@
-import uuid
 from datetime import datetime
+import json
+import logging
+import uuid
+
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404, HttpResponse, HttpResponseServerError
-import logging
 from django.shortcuts import render_to_response, render
 from django.template.context import RequestContext
 from django.utils.translation import ugettext_lazy as _
 
 from soil import DownloadBase
 from soil.exceptions import TaskFailedError
-from soil.tasks import demo_sleep
-import json
 from soil.heartbeat import get_file_heartbeat, get_cache_heartbeat, last_heartbeat
+from soil.tasks import demo_sleep
 from soil.util import get_download_context
 
 
