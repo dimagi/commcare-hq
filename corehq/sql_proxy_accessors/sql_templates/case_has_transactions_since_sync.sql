288830e3f8d2a8b5156655872fd892eeeca52a3b
NoahCarnahan <ncarnahan@dimagi.com>
2015-12-30 13:51:01 -0500

    Fix typos in plproxy functions

--- corehq/sql_proxy_accessors/sql_templates/case_has_transactions_since_sync.sql
+++ corehq/sql_proxy_accessors/sql_templates/case_has_transactions_since_sync.sql
@@ -1,7 +1,7 @@
 DROP FUNCTION IF EXISTS case_has_transactions_since_sync(TEXT, TEXT, TIMESTAMP WITH TIME ZONE);
 
 CREATE FUNCTION case_has_transactions_since_sync(p_case_id TEXT, p_sync_log_id TEXT, sync_log_date TIMESTAMP WITH TIME ZONE )
-  RETURNS BOOLEAN AS $$
+RETURNS BOOLEAN AS $$
     CLUSTER '{{ PL_PROXY_CLUSTER_NAME }}';
     RUN ON hash_string(p_case_id, 'siphash24');
-$$ LANGUAGE sql;
+$$ LANGUAGE plproxy;
