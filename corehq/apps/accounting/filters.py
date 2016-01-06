e20cb2b3c97b406f0966f6690e10c9c08a3c1c55
Biyeun Buczyk <biyeun@dimagi.com>
2016-01-06 17:56:50 -0500

    move daterange filter to bootstrap2 folder

--- corehq/apps/accounting/filters.py
+++ corehq/apps/accounting/filters.py
@@ -177,7 +177,7 @@ class CreatedSubAdjMethodFilter(BaseSingleOptionFilter):
 
 
 class DateRangeFilter(BaseReportFilter):
-    template = 'reports/filters/daterange.html'
+    template = 'reports/filters/bootstrap2/daterange.html'
     default_days = 7
 
     START_DATE = 'startdate'
