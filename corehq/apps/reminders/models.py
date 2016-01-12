32ce1be44015808a7a4f5b598e1bbe678b2c7633
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-01-12 16:36:09 -0500

    Add option to control behavior on blank start dates

--- corehq/apps/reminders/models.py
+++ corehq/apps/reminders/models.py
@@ -536,6 +536,12 @@ class CaseReminderHandler(Document):
     # If {}, then no filter is applied to the recipient list.
     user_data_filter = DictProperty()
 
+    # When sending a case criteria reminder whose start date is defined in
+    # a case property, this option tells what to do if that start date case
+    # property is blank or unparseable.  If set to True, we use today's date.
+    # If False, we don't schedule any reminder at all.
+    use_today_if_start_date_is_blank = BooleanProperty(default=True)
+
     @property
     def uses_parent_case_property(self):
         events_use_parent_case_property = False
@@ -1006,6 +1012,36 @@ class CaseReminderHandler(Document):
         with CriticalSection([key]):
             self._case_changed(case, now, schedule_changed, prev_definition)
 
+    def get_case_criteria_reminder_start_date(self, case, now):
+        """
+        Returns (start, spawn, used_now) where:
+            start - datetime representing the reminder start time
+            spawn - True to continue with the reminder spawn or
+                    False to not spawn the reminder
+            used_now - True if start is just now, False otherwise
+        """
+        if not self.start_date:
+            return (now, True, True)
+
+        start_date = get_case_property(case, self.start_date)
+
+        if isinstance(start_date, datetime):
+            return (start_date, True, False)
+
+        if isinstance(start_date, date):
+            return (datetime.combine(start_date, time(0,0)), True, False)
+
+        if looks_like_timestamp(start_date):
+            try:
+                return (parse(start_date), True, False)
+            except Exception:
+                pass
+
+        if self.use_today_if_start_date_is_blank:
+            return (now, True, True)
+        else:
+            return (None, False, False)
+
     def _case_changed(self, case, now, schedule_changed, prev_definition):
         """
         This method is used to manage updates to CaseReminderHandler's whose start_condition_type == CASE_CRITERIA.
@@ -1038,23 +1074,13 @@ class CaseReminderHandler(Document):
                 reminder.retire()
         else:
             start_condition_reached = case_matches_criteria(case, self.start_match_type, self.start_property, self.start_value)
-            start_date = get_case_property(case, self.start_date)
-            if (not isinstance(start_date, date)) and not (isinstance(start_date, datetime)):
-                try:
-                    start_date = parse(start_date)
-                except Exception:
-                    start_date = None
-            
-            if isinstance(start_date, datetime):
-                start_condition_datetime = start_date
-                start = start_date
-            elif isinstance(start_date, date):
-                start_condition_datetime = datetime(start_date.year, start_date.month, start_date.day, 0, 0, 0)
-                start = start_condition_datetime
-            else:
-                start_condition_datetime = None
-                start = now
-            
+            start, spawn, used_now = self.get_case_criteria_reminder_start_date(case, now)
+            if not spawn:
+                if reminder:
+                    reminder.retire()
+                return
+            start_condition_datetime = None if used_now else start
+
             # Retire the reminder if the start condition is no longer valid
             if reminder is not None:
                 if not start_condition_reached:
