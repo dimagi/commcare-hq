421e2d89cc952d2fac79bc09e1facbcba896af72
Jonathan Emord <jemord@dimagi.com>
2016-01-12 13:42:17 -0500

    Use new base db module

--- settingshelper.py
+++ settingshelper.py
@@ -4,7 +4,7 @@ import sys
 import tempfile
 import uuid
 
-from django.db.backends.creation import TEST_DATABASE_PREFIX
+from django.db.backends.base.creation import TEST_DATABASE_PREFIX
 
 
 def is_testing():
