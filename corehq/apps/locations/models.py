5ba2cc69a757e91c89c1e4898832b5c2b7848950
Cory Zue <czue@dimagi.com>
2016-01-12 16:52:27 -0500

    [ci skip] lint

--- corehq/apps/locations/models.py
+++ corehq/apps/locations/models.py
@@ -381,7 +381,7 @@ class SQLLocation(MPTTModel):
         for n in nodes:
             lft, rght = n.lft, n.rght
             if include_self:
-                lft -=1
+                lft -= 1
                 rght += 1
             filters.append(Q(tree_id=n.tree_id, lft__gt=lft, rght__lt=rght))
         q = reduce(operator.or_, filters)
