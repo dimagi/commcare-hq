41051e76c7d20a3224f7e4116e08c0c20a203459
Nick Pellegrino <npellegrino@dimagi.com>
2016-01-13 15:51:39 +0100

    separate rebuild and resume building indicators

--- corehq/apps/userreports/tasks.py
+++ corehq/apps/userreports/tasks.py
@@ -35,7 +35,13 @@ def _get_redis_key_for_config(config):
     return 'ucr_queue-{}:{}'.format(config._id, rev)
 
 
-def _build_indicators(indicator_config_id, config, couchdb, adapter, redis_client, redis_key, relevant_ids):
+def _build_indicators(indicator_config_id, relevant_ids):
+    config = _get_config_by_id(indicator_config_id)
+    adapter = IndicatorSqlAdapter(config)
+    couchdb = _get_db(config.referenced_doc_type)
+    redis_client = get_redis_client().client.get_client()
+    redis_key = _get_redis_key_for_config(config)
+
     for doc in iter_docs(couchdb, relevant_ids, chunksize=500):
         try:
             # save is a noop if the filter doesn't match
@@ -61,28 +67,38 @@ def _build_indicators(indicator_config_id, config, couchdb, adapter, redis_clien
 def rebuild_indicators(indicator_config_id):
     config = _get_config_by_id(indicator_config_id)
     adapter = IndicatorSqlAdapter(config)
-
     couchdb = _get_db(config.referenced_doc_type)
+    redis_client = get_redis_client().client.get_client()
+    redis_key = _get_redis_key_for_config(config)
+
+    if not _is_static(indicator_config_id):
+        # Save the start time now in case anything goes wrong. This way we'll be
+        # able to see if the rebuild started a long time ago without finishing.
+        config.meta.build.initiated = datetime.datetime.utcnow()
+        config.save()
+        redis_key = _get_redis_key_for_config(config)
+
+    adapter.rebuild_table()
+    relevant_ids = get_doc_ids_in_domain_by_type(
+        config.domain,
+        config.referenced_doc_type,
+        database=couchdb,
+    )
+    for docs in chunked(relevant_ids, 1000):
+        redis_client.sadd(redis_key, *docs)
+
+    _build_indicators(indicator_config_id, relevant_ids)
+
+
+@task(queue='ucr_queue', ignore_result=True, acks_late=True)
+def resume_building_indicators(indicator_config_id):
+    config = _get_config_by_id(indicator_config_id)
     redis_client = get_redis_client().client.get_client()
     redis_key = _get_redis_key_for_config(config)
 
     if len(redis_client.smembers(redis_key)) > 0:
         relevant_ids = redis_client.smembers(redis_key)
-    else:
-        if not _is_static(indicator_config_id):
-            # Save the start time now in case anything goes wrong. This way we'll be
-            # able to see if the rebuild started a long time ago without finishing.
-            config.meta.build.initiated = datetime.datetime.utcnow()
-            config.save()
-            redis_key = _get_redis_key_for_config(config)
-
-        adapter.rebuild_table()
-        relevant_ids = get_doc_ids_in_domain_by_type(config.domain, config.referenced_doc_type,
-                                   database=couchdb)
-        for docs in chunked(relevant_ids, 1000):
-            redis_client.sadd(redis_key, *docs)
-
-    _build_indicators(indicator_config_id, config, couchdb, adapter, redis_client, redis_key, relevant_ids)
+        _build_indicators(indicator_config_id, relevant_ids)
 
 
 def _get_db(doc_type):
