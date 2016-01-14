d8f887eb05b1d403aecb7c7357c40db0738bf106
Ethan Soergel <esoergel@gmail.com>
2016-01-14 16:20:11 -0500

    Remove unused dbaccessor

--- corehq/apps/commtrack/dbaccessors/__init__.py
+++ corehq/apps/commtrack/dbaccessors/__init__.py
@@ -16,21 +16,6 @@ def get_supply_point_ids_in_domain_by_location(domain):
     }
 
 
-def get_supply_points_json_in_domain_by_location(domain):
-    from corehq.apps.commtrack.models import SupplyPointCase
-    results = SupplyPointCase.get_db().view(
-        'supply_point_by_loc/view',
-        startkey=[domain],
-        endkey=[domain, {}],
-        include_docs=True,
-    )
-
-    for result in results:
-        location_id = result['key'][-1]
-        case = result['doc']
-        yield location_id, case
-
-
 def get_supply_point_case_by_location_id(domain, location_id):
     from corehq.apps.commtrack.models import SupplyPointCase
     return SupplyPointCase.view(
