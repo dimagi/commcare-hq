5cf2096a468d916571aeac38201955c8fc5ce323
kkrampa <kkrampa@soldevelo.com>
2016-01-15 11:22:57 +0100

    Fixed ewsghana tests

--- custom/ewsghana/tests/__init__.py
+++ custom/ewsghana/tests/__init__.py
@@ -1,7 +1,5 @@
 from .test_apis import *
 from .test_products_sync import *
-from .test_locations_sync import *
-from .test_webusers_sync import *
 from .test_smsusers_sync import *
 from .handlers.receipt import *
 from .handlers.requisition_status import *
