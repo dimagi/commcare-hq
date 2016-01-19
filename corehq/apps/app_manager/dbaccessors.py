cd281ee9a56dbedae2125dbffb7ee585619a8783
Daniel Roberts <droberts@dimagi.com>
2016-01-19 16:06:26 -0500

    add test for get_exports_by_application

--- corehq/apps/app_manager/dbaccessors.py
+++ corehq/apps/app_manager/dbaccessors.py
@@ -157,6 +157,11 @@ def get_built_app_ids(domain):
     return [app_id for app_id in app_ids if app_id]
 
 
+def update_analytics_indexes():
+    from .models import Application
+    return Application.get_db().view('exports_forms/by_xmlns', limit=1).all()
+
+
 def get_exports_by_application(domain):
     from .models import Application
     return Application.get_db().view(
@@ -165,7 +170,7 @@ def get_exports_by_application(domain):
         endkey=['^Application', domain, {}],
         reduce=False,
         stale=settings.COUCH_STALE_QUERY,
-    )
+    ).all()
 
 
 def get_all_apps(domain):
