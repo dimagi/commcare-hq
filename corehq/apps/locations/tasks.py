a7c355c8af40f493b0b38bdcb8977757733c4cd2
Ethan Soergel <esoergel@gmail.com>
2016-01-18 19:02:06 -0500

    Make generic `locking_task`

--- corehq/apps/locations/tasks.py
+++ corehq/apps/locations/tasks.py
@@ -1,41 +1,25 @@
-from celery.task import task
-from dimagi.utils.couch.cache.cache_core import get_redis_client
+from corehq.util.decorators import locking_task
 from corehq.apps.commtrack.models import StockState
 from corehq.apps.locations.models import SQLLocation
-
 
 
 class CouldNotAqcuireLock(Exception):
     pass
 
 
-@task(bind=True, queue='background_queue', ignore_result=True,
-      default_retry_delay=10, max_retries=3)
-def sync_administrative_status(self, location_type):
-    client = get_redis_client()
-    key = "{location_type.domain}-{location_type.pk}".format(location_type=location_type)
-    lock = client.lock(key, timeout=10)
-    if lock.acquire(blocking=False):
-        try:
-            # Actually call the function
-            for location in SQLLocation.objects.filter(location_type=location_type):
-                # Saving the location should be sufficient for it to pick up the
-                # new supply point.  We'll need to save it anyways to store the new
-                # supply_point_id.
-                location.save()
-            if location_type.administrative:
-                _hide_stock_states(location_type)
-            else:
-                _unhide_stock_states(location_type)
-        except Exception:
-            # Don't leave the lock around if the task fails
-            lock.release()
-            raise
-        lock.release()
+@locking_task("{location_type.domain}-{location_type.pk}",
+              default_retry_delay=30, max_retries=3)
+def sync_administrative_status(location_type):
+    """Updates supply points of locations of this type"""
+    for location in SQLLocation.objects.filter(location_type=location_type):
+        # Saving the location should be sufficient for it to pick up the
+        # new supply point.  We'll need to save it anyways to store the new
+        # supply_point_id.
+        location.save()
+    if location_type.administrative:
+        _hide_stock_states(location_type)
     else:
-        msg = "Could not aquire lock '{}' for task '{}'.".format(
-            key, 'sync_administrative_status')
-        self.retry(exc=CouldNotAqcuireLock(msg))
+        _unhide_stock_states(location_type)
 
 
 def _hide_stock_states(location_type):
