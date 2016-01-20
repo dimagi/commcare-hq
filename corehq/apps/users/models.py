d9654e84f43a5fb74bf81997f316029f4d3e13ac
Daniel Roberts <droberts@dimagi.com>
2016-01-20 17:19:01 -0500

    add deletion date to deleted users, forms, cases and verified numbers

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -1634,24 +1634,26 @@ class CommCareUser(CouchUser, SingleMembershipMixin, CommCareMobileContactMixin)
     def retire(self):
         suffix = DELETED_SUFFIX
         deletion_id = random_hex()
+        deletion_date = datetime.utcnow()
         deleted_cases = set()
         # doc_type remains the same, since the views use base_doc instead
         if not self.base_doc.endswith(suffix):
             self.base_doc += suffix
             self['-deletion_id'] = deletion_id
+            self['-deletion_date'] = deletion_date
 
         for caselist in chunked(self._get_case_docs(), 50):
-            tag_cases_as_deleted_and_remove_indices.delay(self.domain, caselist, deletion_id)
+            tag_cases_as_deleted_and_remove_indices.delay(self.domain, caselist, deletion_id, deletion_date)
             for case in caselist:
                 deleted_cases.add(case['_id'])
 
         for form_id_list in chunked(self.get_forms(wrap=False, include_docs=False), 50):
             tag_forms_as_deleted_rebuild_associated_cases.delay(
-                self.user_id, self.domain, form_id_list, deletion_id, deleted_cases=deleted_cases
+                self.user_id, self.domain, form_id_list, deletion_id, deletion_date, deleted_cases=deleted_cases
             )
 
         for phone_number in self.get_verified_numbers(True).values():
-            phone_number.retire(deletion_id)
+            phone_number.retire(deletion_id=deletion_id, deletion_date=deletion_date)
 
         try:
             django_user = self.get_django_user()
