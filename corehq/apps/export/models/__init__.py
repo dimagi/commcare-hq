53b42cbe35b9842b29a29efbfefb816cfe515b82
Ben Rudolph <brudolph@dimagi.com>
2016-01-20 14:25:48 -0500

    Separate old and new models

---
+++ corehq/apps/export/models/__init__.py
@@ -0,0 +1,12 @@
+from .deprecated import QuestionMeta, FormQuestionSchema
+from .new import (
+    ExportItem,
+    ExportColumn,
+    ExportRow,
+    ExportableItems,
+    TableConfiguration,
+    RepeatGroup,
+    ScalarItem,
+    Option,
+    MultipleChoiceItem,
+)
