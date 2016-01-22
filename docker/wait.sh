163d73b353ceccf941b542ea6c0a6c8b59b66425
Simon Kelly <skelly@dimagi.com>
2016-01-22 13:41:30 +0200

    fix wait

--- docker/wait.sh
+++ docker/wait.sh
@@ -6,8 +6,10 @@ PREFIX=$(env | grep REDIS_NAME | cut -d / -f 2 | cut -d _ -f 1 | awk '{print tou
 SERVICES="REDIS ELASTICSEARCH COUCH POSTGRES"
 
 for service in $SERVICES; do
-    host=$(env | grep $service | grep $PREFIX | grep _TCP_ADDR | cut -d = -f 2 | head -n1)
-    port=$(env | grep $service | grep $PREFIX | grep _TCP_PORT | cut -d = -f 2 | head -n1)
+    while [ -z "$host" ]; do
+        host=$(env | grep $service | grep $PREFIX | grep _TCP_ADDR | cut -d = -f 2 | head -n1)
+        port=$(env | grep $service | grep $PREFIX | grep _TCP_PORT | cut -d = -f 2 | head -n1)
+    done
 
     echo -n "waiting for TCP connection to $service @ $host:$port..."
 
