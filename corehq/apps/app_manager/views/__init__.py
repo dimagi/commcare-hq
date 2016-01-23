b820886d921a4fada1ec7e7d0fe1bfbd280692e4
Nick Pellegrino <npellegrino@dimagi.com>
2016-01-23 13:01:57 -0500

    cleanup imports

--- corehq/apps/app_manager/views/__init__.py
+++ corehq/apps/app_manager/views/__init__.py
@@ -1,27 +1,3 @@
-from corehq.apps.app_manager.views.translations import (
-    upload_bulk_ui_translations,
-    download_bulk_ui_translations,
-    download_bulk_app_translations,
-    upload_bulk_app_translations,
-)
-from corehq.apps.app_manager.views.download import (
-    download_app_strings,
-    download_file,
-    download_index,
-    download_jad,
-    download_jar,
-    download_media_profile,
-    download_media_suite,
-    download_odk_media_profile,
-    download_odk_profile,
-    download_profile,
-    download_raw_jar,
-    download_suite,
-    download_test_jar,
-    download_user_registration,
-    download_xform,
-    DownloadCCZ,
-)
 from corehq.apps.app_manager.views.app_summary import AppSummaryView
 from corehq.apps.app_manager.views.apps import (
     app_from_template,
@@ -68,6 +44,11 @@ from corehq.apps.app_manager.views.download import (
     download_xform,
     DownloadCCZ,
     validate_form_for_build,
+)
+from corehq.apps.app_manager.views.formdesigner import (
+    form_designer,
+    get_data_schema,
+    user_registration_source,
 )
 from corehq.apps.app_manager.views.forms import (
     copy_form,
@@ -131,9 +112,4 @@ from corehq.apps.app_manager.views.translations import (
     download_bulk_ui_translations,
     upload_bulk_app_translations,
     upload_bulk_ui_translations,
-)
-from corehq.apps.app_manager.views.formdesigner import (
-    form_designer,
-    get_data_schema,
-    user_registration_source,
 )
