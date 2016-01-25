ce4f342db2b9c3c1c990d5162d7acd37e4fae755
Ben Rudolph <brudolph@dimagi.com>
2016-01-25 14:28:02 -0500

    Use normal_state in SQL query

--- corehq/sql_accessors/migrations/0003_get_forms_by_user_id_functions.py
+++ corehq/sql_accessors/migrations/0003_get_forms_by_user_id_functions.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 from corehq.sql_db.operations import RawSQLMigration
+from corehq.form_processor.models import XFormInstanceSQL
 
-migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {})
+migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {
+    'normal_state': XFormInstanceSQL.NORMAL
+})
 
 
 class Migration(migrations.Migration):
--- corehq/sql_accessors/sql_templates/get_forms_by_user_id.sql
+++ corehq/sql_accessors/sql_templates/get_forms_by_user_id.sql
@@ -3,6 +3,8 @@ DROP FUNCTION IF EXISTS get_forms_by_user_id(TEXT);
 CREATE FUNCTION get_forms_by_user_id(p_user_id TEXT) RETURNS SETOF form_processor_xforminstancesql AS $$
 BEGIN
     RETURN QUERY
-    SELECT * FROM form_processor_xforminstancesql where user_id = p_user_id;
+    SELECT * FROM form_processor_xforminstancesql WHERE
+        user_id = p_user_id AND
+        state = {{ normal_state }};
 END;
 $$ LANGUAGE plpgsql;
