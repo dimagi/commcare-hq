b32fb0ad2c164bb234d6d08631a446e382474010
Ben Rudolph <brudolph@dimagi.com>
2016-01-27 14:16:09 -0500

    fix tests

--- corehq/apps/export/tests/test_export_item.py
+++ corehq/apps/export/tests/test_export_item.py
@@ -16,14 +16,14 @@ class TestExportItemGeneration(SimpleTestCase):
         )
 
     def test_create_default_from_export_item(self):
-        column = ExportColumn.create_default_from_export_item(None, self.item, 3)
+        column = ExportColumn.create_default_from_export_item([None], self.item, 3)
 
         self.assertEqual(column.show, True)
         self.assertEqual(column.label, 'Question One')
         self.assertEqual(column.selected, True)
 
     def test_create_default_from_export_item_deleted(self):
-        column = ExportColumn.create_default_from_export_item(None, self.item, 4)
+        column = ExportColumn.create_default_from_export_item([None], self.item, 4)
 
         self.assertEqual(column.show, False)
         self.assertEqual(column.label, 'Question One')
