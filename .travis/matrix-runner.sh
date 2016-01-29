2b6f15f5256b0ff7a658f82e956ad916dd7502da
Simon Kelly <skelly@dimagi.com>
2016-01-29 09:59:48 +0200

    add more tests to sharded matrix

--- .travis/matrix-runner.sh
+++ .travis/matrix-runner.sh
@@ -4,11 +4,12 @@ set -ev
 source .travis/utils.sh
 
 if [ "${MATRIX_TYPE}" = "python" ]; then
-    command="coverage run manage.py test --noinput --failfast --traceback --verbosity=2 --testrunner=$TESTRUNNER"
-    travis_runner web_test $command
+    COMMAND="coverage run manage.py test --noinput --failfast --traceback --verbosity=2 --testrunner=$TESTRUNNER"
+    travis_runner web_test $COMMAND
 elif [ "${MATRIX_TYPE}" = "python-sharded" ]; then
-    command="coverage run manage.py test --noinput --failfast --traceback --verbosity=2 form_processor"
-    travis_runner -e USE_PARTITIONED_DATABASE=yes web_test $command
+    SHARDED_TEST_APPS="form_processor sql_db couchforms case phone receiverwrapper"
+    COMMAND="coverage run manage.py test --noinput --failfast --traceback --verbosity=2 $SHARDED_TEST_APPS"
+    travis_runner -e USE_PARTITIONED_DATABASE=yes web_test $COMMAND
 elif [ "${MATRIX_TYPE}" = "javascript" ]; then
     travis_runner web_test python manage.py migrate --noinput
     travis_runner web_test grunt mocha
