ea650d9ce50604a1d723b2fc43d8993e0485f064
Nick Pellegrino <npellegrino@dimagi.com>
2016-01-29 16:52:43 -0500

    print to console as csv

--- corehq/apps/accounting/management/commands/get_minimal_plan_by_domain.py
+++ corehq/apps/accounting/management/commands/get_minimal_plan_by_domain.py
@@ -10,6 +10,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         domain_names = args
+        privileges = sorted(DomainDowngradeStatusHandler.privilege_to_response().keys())
+        print ','.join(['Project Space'] + privileges + ['Lowest Plan'])
         for domain in filter(lambda domain: domain, map(Domain.get_by_name, domain_names)):
             is_privilege_being_used = {
                 priv: _is_domain_using_privilege(domain, priv)
@@ -17,7 +19,7 @@ class Command(BaseCommand):
             }
             using_privileges = [priv for (priv, is_in_use) in is_privilege_being_used.items() if is_in_use]
             minimum_plan = DefaultProductPlan.get_lowest_edition_by_domain(domain.name, using_privileges)
-            print domain.name, is_privilege_being_used, minimum_plan
+            print ','.join([domain.name] + ['X' if is_privilege_being_used[priv] else '' for priv in privileges] + [minimum_plan])
 
 
 def _is_domain_using_privilege(domain, privilege):
