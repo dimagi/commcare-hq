e16cdb8b425ab9dcf1fa86f418ba313a5ac352a7
Ben Rudolph <brudolph@dimagi.com>
2016-01-29 12:04:36 -0500

    update get_latest_export

--- corehq/apps/export/dbaccessors.py
+++ corehq/apps/export/dbaccessors.py
@@ -1,11 +1,28 @@
 
 
-def get_latest_export_schema_id(domain, doc_type, xmlns_or_case_type):
+def get_latest_case_export_schema_id(domain, case_type):
     from .models import ExportDataSchema
+
+    key = [domain, 'CaseExportDataSchema', case_type]
     result = ExportDataSchema.get_db().view(
         'schemas_by_xmlns_or_case_type/view',
-        startkey=[domain, doc_type, xmlns_or_case_type],
-        endkey=[domain, doc_type, xmlns_or_case_type, {}],
+        startkey=key,
+        endkey=key + [{}],
         include_docs=False,
+        limit=1,
+    ).first()
+    return result['id'] if result else None
+
+
+def get_latest_form_export_schema_id(domain, app_id, xmlns):
+    from .models import ExportDataSchema
+
+    key = [domain, 'FormExportDataSchema', app_id, xmlns]
+    result = ExportDataSchema.get_db().view(
+        'schemas_by_xmlns_or_case_type/view',
+        startkey=key,
+        endkey=key + [{}],
+        include_docs=False,
+        limit=1,
     ).first()
     return result['id'] if result else None
