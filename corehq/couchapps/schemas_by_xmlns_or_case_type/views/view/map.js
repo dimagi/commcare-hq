e907f182b5361c10d3ad76220fdbf6985db29bdf
Ben Rudolph <brudolph@dimagi.com>
2016-01-29 11:43:03 -0500

    add app_id to map.js

--- corehq/couchapps/schemas_by_xmlns_or_case_type/views/view/map.js
+++ corehq/couchapps/schemas_by_xmlns_or_case_type/views/view/map.js
@@ -1,6 +1,6 @@
 function(doc) {
     if (doc.doc_type === "FormExportDataSchema") {
-        emit([doc.domain, doc.doc_type, doc.xmlns], null);
+        emit([doc.domain, doc.doc_type, doc.app_id, doc.xmlns], null);
     } else if (doc.doc_type === "CaseExportDataSchema") {
         emit([doc.domain, doc.doc_type, doc.case_type], null);
     }
