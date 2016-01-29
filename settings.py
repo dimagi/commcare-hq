b89996a3a60398c3ee589afb52b0e71e4dff9735
Cory Zue <czue@dimagi.com>
2016-01-29 10:25:11 +0200

    add sql case pillow to settings

--- settings.py
+++ settings.py
@@ -1423,6 +1423,11 @@ PILLOWTOPS = {
             'class': 'pillowtop.pillow.interface.ConstructedPillow',
             'instance': 'corehq.pillows.xform.get_sql_xform_to_elasticsearch_pillow',
         },
+        {
+            'name': 'SqlCaseToElasticsearchPillow',
+            'class': 'pillowtop.pillow.interface.ConstructedPillow',
+            'instance': 'corehq.pillows.case.get_sql_case_to_elasticsearch_pillow',
+        },
     ]
 }
 
