566ef85abab85f30aa89e5b8ac346dc05056c759
Ben Rudolph <brudolph@dimagi.com>
2016-02-01 16:19:01 -0500

    Reroute views to the new exports page

--- corehq/apps/export/views.py
+++ corehq/apps/export/views.py
@@ -14,6 +14,7 @@ from django.utils.safestring import mark_safe
 from djangular.views.mixins import JSONResponseMixin, allow_remote_invocation
 import pytz
 from corehq import privileges
+from corehq import toggles
 from corehq.apps.accounting.utils import domain_has_privilege
 from corehq.apps.app_manager.fields import ApplicationDataRMIHelper
 from corehq.apps.data_interfaces.dispatcher import require_can_edit_data
@@ -191,6 +192,7 @@ class BaseExportView(BaseProjectDataView):
 
 
 class BaseCreateNewCustomExportView(BaseExportView):
+    template_name = 'export/new_customize_export.html'
 
     @property
     def page_context(self):
@@ -297,7 +299,7 @@ class CreateNewCustomFormExportView(BaseCreateNewCustomExportView):
         )
         self.export_instance = self.get_export_instance(schema, app_id)
 
-        return super(BaseCreateCustomExportView, self).get(request, *args, **kwargs)
+        return super(CreateNewCustomFormExportView, self).get(request, *args, **kwargs)
 
 
 class CreateNewCustomCaseExportView(BaseCreateNewCustomExportView):
@@ -314,7 +316,7 @@ class CreateNewCustomCaseExportView(BaseCreateNewCustomExportView):
         )
         self.export_instance = self.get_export_instance(schema)
 
-        return super(BaseCreateCustomExportView, self).get(request, *args, **kwargs)
+        return super(CreateNewCustomCaseExportView, self).get(request, *args, **kwargs)
 
 
 class CreateCustomFormExportView(BaseCreateCustomExportView):
@@ -1168,8 +1170,12 @@ class FormExportListView(BaseExportListView):
 
         app_id = create_form.cleaned_data['application']
         form_xmlns = create_form.cleaned_data['form']
+        if toggles.NEW_EXPORTS.enabled(self.domain):
+            cls = CreateNewCustomFormExportView
+        else:
+            cls = CreateCustomFormExportView
         return reverse(
-            CreateCustomFormExportView.urlname,
+            cls.urlname,
             args=[self.domain],
         ) + ('?export_tag="{export_tag}"{app_id}'.format(
             app_id=('&app_id={}'.format(app_id)
@@ -1253,8 +1259,12 @@ class CaseExportListView(BaseExportListView):
         if not create_form.is_valid():
             raise ExportFormValidationException()
         case_type = create_form.cleaned_data['case_type']
+        if toggles.NEW_EXPORTS.enabled(self.domain):
+            cls = CreateNewCustomCaseExportView
+        else:
+            cls = CreateCustomCaseExportView
         return reverse(
-            CreateCustomCaseExportView.urlname,
+            cls.urlname,
             args=[self.domain],
         ) + ('?export_tag="{export_tag}"'.format(
             export_tag=case_type,
