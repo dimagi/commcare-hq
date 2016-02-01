d251cfdfd6257bb8098b4b0db0f1ad3bf06491f1
Ben Rudolph <brudolph@dimagi.com>
2016-02-01 10:44:33 -0500

    DRY up dbaccessor call

--- corehq/apps/export/dbaccessors.py
+++ corehq/apps/export/dbaccessors.py
@@ -4,23 +4,18 @@ def get_latest_case_export_schema(domain, case_type):
     from .models import CaseExportDataSchema
 
     key = [domain, 'CaseExportDataSchema', case_type]
-    result = CaseExportDataSchema.get_db().view(
-        'schemas_by_xmlns_or_case_type/view',
-        startkey=key + [{}],
-        endkey=key,
-        include_docs=True,
-        limit=1,
-        reduce=False,
-        descending=True,
-    ).first()
-    return CaseExportDataSchema.wrap(result['doc']) if result else None
+    return _get_latest_export_schema(CaseExportDataSchema, key)
 
 
 def get_latest_form_export_schema(domain, app_id, xmlns):
     from .models import FormExportDataSchema
 
     key = [domain, 'FormExportDataSchema', app_id, xmlns]
-    result = FormExportDataSchema.get_db().view(
+    return _get_latest_export_schema(FormExportDataSchema, key)
+
+
+def _get_latest_export_schema(cls, key):
+    result = cls.get_db().view(
         'schemas_by_xmlns_or_case_type/view',
         startkey=key + [{}],
         endkey=key,
@@ -29,4 +24,4 @@ def get_latest_form_export_schema(domain, app_id, xmlns):
         reduce=False,
         descending=True,
     ).first()
-    return FormExportDataSchema.wrap(result['doc']) if result else None
+    return cls.wrap(result['doc']) if result else None
