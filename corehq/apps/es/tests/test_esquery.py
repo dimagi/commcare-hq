1b46f5f2d98beb1cb5752aec251b086e92a5d4ec
Simon Kelly <skelly@dimagi.com>
2016-02-04 15:02:22 +0200

    add test for values_list

--- corehq/apps/es/tests/test_esquery.py
+++ corehq/apps/es/tests/test_esquery.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
+from mock import patch
 
 from corehq.apps.es import filters
 from corehq.apps.es import forms, users
-from corehq.apps.es.es_query import HQESQuery
+from corehq.apps.es.es_query import HQESQuery, ESQuerySet
 from corehq.apps.es.tests import ElasticTestMixin
 from corehq.elastic import SIZE_LIMIT
 
@@ -116,3 +117,52 @@ class TestESQuery(ElasticTestMixin, TestCase):
                  .remove_default_filter('active'))
         filters = query.raw_query['query']['filtered']['filter']['and']
         self.assertTrue(len(filters) > 0)
+
+    def test_values_list(self):
+        example_response = {
+            u'_shards': {u'failed': 0, u'successful': 5, u'total': 5},
+            u'hits': {u'hits': [{
+                u'_id': u'8063dff5-460b-46f2-b4d0-5871abfd97d4',
+                u'_index': u'xforms_1cce1f049a1b4d864c9c25dc42648a45',
+                u'_score': 1.0,
+                u'_type': u'xform',
+                u'fields': {
+                    u'app_id': u'fe8481a39c3738749e6a4766fca99efd',
+                    u'doc_type': u'xforminstance',
+                    u'domain': u'mikesproject',
+                    u'xmlns': u'http://openrosa.org/formdesigner/3a7cc07c-551c-4651-ab1a-d60be3017485'
+                    }
+                },
+                {
+                    u'_id': u'dc1376cd-0869-4c13-a267-365dfc2fa754',
+                    u'_index': u'xforms_1cce1f049a1b4d864c9c25dc42648a45',
+                    u'_score': 1.0,
+                    u'_type': u'xform',
+                    u'fields': {
+                        u'app_id': u'3d622620ca00d7709625220751a7b1f9',
+                        u'doc_type': u'xforminstance',
+                        u'domain': u'jacksproject',
+                        u'xmlns': u'http://openrosa.org/formdesigner/54db1962-b938-4e2b-b00e-08414163ead4'
+                        }
+                    }
+                ],
+                u'max_score': 1.0,
+                u'total': 5247
+                },
+            u'timed_out': False,
+            u'took': 4
+        }
+        fields = [u'app_id', u'doc_type', u'domain']
+        query = forms.FormES()
+        with patch('corehq.apps.es.es_query.run_query', return_value=example_response):
+            response = query.values_list(*fields)
+            self.assertEqual(
+                [
+                    (u'fe8481a39c3738749e6a4766fca99efd', u'xforminstance', u'mikesproject'),
+                    (u'3d622620ca00d7709625220751a7b1f9', u'xforminstance', u'jacksproject')
+                ],
+                response
+            )
+
+            response = query.values_list('domain', flat=True)
+            self.assertEqual([u'mikesproject', u'jacksproject'], response)
