61074603b67f588cfcea50c31e09d6363550b22d
NoahCarnahan <ncarnahan@dimagi.com>
2016-02-08 15:59:32 -0500

    Return extension cases from CommCareCase.reverse_indices

--- corehq/ex-submodules/casexml/apps/case/dbaccessors/related.py
+++ corehq/ex-submodules/casexml/apps/case/dbaccessors/related.py
@@ -68,13 +68,25 @@ def get_all_reverse_indices_info(domain, case_ids, relationship=None):
     ))
 
 
-def get_reverse_indices_json(domain, case_id, relationship=CASE_INDEX_CHILD):
+def get_reverse_indices_json(domain, case_id, relationship=CASE_INDEX_CHILD, any_relationship=False):
     from casexml.apps.case.models import CommCareCase
+
+    if not any_relationship:
+        key_args = {
+            "key": [domain, case_id, "reverse_index", relationship]
+        }
+    else:
+        key_args = {
+            "startkey": [domain, case_id, "reverse_index"],
+            "endkey": [domain, case_id, "reverse_index", {}]
+        }
+
+
     return CommCareCase.get_db().view(
         "case_indices/related",
-        key=[domain, case_id, "reverse_index", relationship],
         reduce=False,
-        wrapper=lambda r: r['value']
+        wrapper=lambda r: r['value'],
+        **key_args
     ).all()
 
 
@@ -84,7 +96,7 @@ def get_reverse_indices(case):
 
 def get_reverse_indices_for_case_id(domain, case_id):
     return [CommCareCaseIndex.wrap(raw)
-            for raw in get_reverse_indices_json(domain, case_id)]
+            for raw in get_reverse_indices_json(domain, case_id, any_relationship=True)]
 
 
 def get_extension_chain(case_ids, domain):
