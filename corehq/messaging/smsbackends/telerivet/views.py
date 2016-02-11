8b6e959530cc1df067c117a6a5c479f75589de36
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-02-11 08:31:50 -0500

    Add outline for angular single-page app

--- corehq/messaging/smsbackends/telerivet/views.py
+++ corehq/messaging/smsbackends/telerivet/views.py
@@ -1,7 +1,13 @@
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
-from django.http import HttpResponse
+from django.utils.translation import ugettext_lazy
+from djangular.views.mixins import JSONResponseMixin, allow_remote_invocation
+from corehq.apps.sms.views import BaseMessagingSectionView
+from corehq.apps.style.decorators import use_bootstrap3, use_angular_js
 from corehq.messaging.smsbackends.telerivet.tasks import process_incoming_message
+
 
 # Tuple of (hq field name, telerivet field name) tuples
 TELERIVET_INBOUND_FIELD_MAP = (
@@ -21,9 +27,36 @@ TELERIVET_INBOUND_FIELD_MAP = (
     ('secret', 'secret'),
 )
 
+
 @require_POST
 @csrf_exempt
 def incoming_message(request):
     kwargs = {a: request.POST.get(b) for (a, b) in TELERIVET_INBOUND_FIELD_MAP}
     process_incoming_message.delay(**kwargs)
     return HttpResponse()
+
+
+class TelerivetSetupView(JSONResponseMixin, BaseMessagingSectionView):
+    template_name = 'telerivet/telerivet_setup.html'
+    urlname = 'telerivet_setup'
+    page_title = ugettext_lazy("Telerivet Setup")
+
+    @property
+    def page_url(self):
+        return reverse(self.urlname, args=[self.domain])
+
+    @property
+    def page_context(self):
+        return {}
+
+    @allow_remote_invocation
+    def test(self):
+        return {}
+
+    @use_bootstrap3
+    @use_angular_js
+    def dispatch(self, *args, **kwargs):
+        return super(TelerivetSetupView, self).dispatch(*args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse()
