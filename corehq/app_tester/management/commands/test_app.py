13595ec7f35af8696fad3c85a840854e318b81dc
Norman Hooper <nhooper@dimagi.com>
2016-02-11 18:54:50 +0200

    Management command and empty views.py

---
+++ corehq/app_tester/management/commands/test_app.py
@@ -0,0 +1,13 @@
+import yaml
+from django.core.management.base import BaseCommand
+from corehq.app_tester.utils import run_tests
+
+
+class Command(BaseCommand):
+    help = 'Run tests found in the given YAML file'
+    args = '<yaml_file>'
+
+    def handle(self, *args, **options):
+        yaml_file = args[0]
+        test_data = yaml.load(yaml_file)
+        run_tests(test_data)
