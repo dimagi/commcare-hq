e76f20acf4a98424d685bd7ef60455bbc382ffbf
Daniel Miller <dmiller@dimagi.com>
2016-02-12 14:50:11 -0500

    Abandon work on running doctests with nose
    There aren't enough of them to justify it and we shouldn't be using them anyway. See http://nedbatchelder.com/blog/200811/things_i_dont_like_about_doctest.html

--- testsettings.py
+++ testsettings.py
@@ -7,9 +7,7 @@ INSTALLED_APPS += (
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
     #'--with-migrations' # adds ~30s to test run; TODO travis should use it
-    #'--with-doctest', # adds 5s to discovery (before tests start); TODO travis should use it
     '--with-fixture-bundling',
-    '--doctest-options=+NORMALIZE_WHITESPACE',
     '--logging-clear-handlers',
 ]
 NOSE_PLUGINS = [
@@ -26,22 +24,16 @@ NOSE_PLUGINS = [
 for key, value in {
     'NOSE_DB_TEST_CONTEXT': 'corehq.tests.nose.HqdbContext',
     'NOSE_NON_DB_TEST_CONTEXT': 'corehq.tests.nose.ErrorOnDbAccessContext',
-    'NOSE_DOCTEST_EXTENSION': '.py',
 
-    'NOSE_IGNORE_FILES': '^(localsettings.*|setup\.py|bootstrap\.py|[0-9].*)$',
+    'NOSE_IGNORE_FILES': '^localsettings',
 
     'NOSE_EXCLUDE_TESTS': ';'.join([
         'corehq.apps.sms.tests.inbound_handlers',
         'corehq.messaging.ivrbackends.kookoo.tests.outbound',
         'corehq.apps.ota.tests.digest_restore.DigestOtaRestoreTest'
-        #'pillow_retry.south_migrations',
 
         # revisit these (seems like they should be passing)
         'corehq.apps.ota.tests.digest_restore.DigestOtaRestoreTest', # not run by django test runner
-
-        # ignore record_deploy_success.py because datadog may not be installed
-        # (only matters when running --with-doctests)
-        'corehq.apps.hqadmin.management.commands.record_deploy_success',
     ]),
 
     'NOSE_EXCLUDE_DIRS': ';'.join([
@@ -51,16 +43,6 @@ for key, value in {
         # strange error:
         # TypeError: Attribute setup of <module 'touchforms.backend' ...> is not a python function.
         'submodules/touchforms-src/touchforms/backend',
-
-        # excludes for --with-doctest
-        # these cause gevent.threading to be imported, which causes this error:
-        # DatabaseError: DatabaseWrapper objects created in a thread can only
-        # be used in that same thread. ...
-        'corehq/apps/hqadmin/management/commands',
-        'corehq/apps/hqcase/management/commands',
-        'corehq/preindex/management/commands',
-        'deployment',
-        'submodules/dimagi-utils-src/dimagi/utils/management/commands',
     ]),
 }.items():
     os.environ.setdefault(key, value)
