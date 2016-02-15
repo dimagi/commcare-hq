5bfad9682e5fdcc57217327b1222a48063820c4c
Simon Kelly <skelly@dimagi.com>
2016-02-15 14:04:46 +0200

    always show child cases in case display

--- corehq/ex-submodules/casexml/apps/case/templatetags/case_tags.py
+++ corehq/ex-submodules/casexml/apps/case/templatetags/case_tags.py
@@ -308,12 +308,11 @@ def render_case_hierarchy(case, options):
         case, get_case_url, type_info=type_info
     )
 
-    case_list = []
+    parent_cases = []
     if case.indices:
         # has parent case(s)
         # todo: handle duplicates in ancestor path (bubbling up of parent-child
         # relationships)
-        parent_cases = []
         for idx in case.indices:
             try:
                 parent_cases.append(idx.referenced_case)
@@ -332,7 +331,7 @@ def render_case_hierarchy(case, options):
             if not getattr(c, 'treetable_parent_node_id', None) and last_parent_id:
                 c.treetable_parent_node_id = last_parent_id
 
-        case_list = parent_cases + descendent_case_list
+    case_list = parent_cases + descendent_case_list
 
     for c in case_list:
         if not c:
