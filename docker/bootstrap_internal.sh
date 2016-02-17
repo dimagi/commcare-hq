90c62aaba8ece1b21270963ebd3da5f42fa0ec9f
philipkaare <philip.kaare@gmail.com>
2016-02-17 16:09:31 +0100

    Just changed the default project and user.

--- docker/bootstrap_internal.sh
+++ docker/bootstrap_internal.sh
@@ -7,4 +7,4 @@ env CCHQ_IS_FRESH_INSTALL=1 ./manage.py migrate --noinput
 ./manage.py compilejsi18n
 bower install --config.interactive=false
 
-./manage.py bootstrap demo admin@example.com password
+./manage.py bootstrap bandim commcare-admin@bandim.org password
