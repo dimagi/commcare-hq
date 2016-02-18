909359e94cd3be9c3fab4a8240f1436ed8219ac5
Farid Rener <proteusvacuum@gmail.com>
2016-02-18 17:55:19 +0530

    refactor test

--- corehq/ex-submodules/casexml/apps/case/tests/test_close_extension_chain.py
+++ corehq/ex-submodules/casexml/apps/case/tests/test_close_extension_chain.py
@@ -21,48 +21,60 @@ class AutoCloseExtensionsTest(SyncBaseTest):
                          date_joined=datetime(2011, 6, 9))
         self.factory = CaseFactory(domain=self.domain)
         self.extension_ids = ['1', '2', '3']
-        self.host = CaseStructure(case_id='host')
-        self.extension = CaseStructure(
+        self.host_id = 'host'
+
+    def _create_extension_chain(self):
+        host = CaseStructure(case_id=self.host_id)
+        extension = CaseStructure(
             case_id=self.extension_ids[0],
             indices=[CaseIndex(
-                related_structure=self.host,
+                related_structure=host,
                 relationship="extension",
             )],
         )
-        self.extension_2 = CaseStructure(
+        extension_2 = CaseStructure(
             case_id=self.extension_ids[1],
             indices=[CaseIndex(
-                related_structure=self.extension,
+                related_structure=extension,
                 relationship="extension",
             )],
         )
-        self.extension_3 = CaseStructure(
+        extension_3 = CaseStructure(
             case_id=self.extension_ids[2],
             indices=[CaseIndex(
-                related_structure=self.extension_2,
+                related_structure=extension_2,
                 relationship="extension",
             )],
         )
+        return self.factory.create_or_update_cases([extension_3])
 
     def test_get_extension_chain_simple(self):
-        self.factory.create_or_update_cases([self.extension])
-        self.assertEqual(set(self.extension_ids[0]), get_extension_chain([self.host.case_id], self.domain))
+        host = CaseStructure(case_id=self.host_id)
+        extension = CaseStructure(
+            case_id=self.extension_ids[0],
+            indices=[CaseIndex(
+                related_structure=host,
+                relationship="extension",
+            )],
+        )
+        self.factory.create_or_update_cases([extension])
+        self.assertEqual(set(self.extension_ids[0]), get_extension_chain([self.host_id], self.domain))
 
     def test_get_extension_chain_multiple(self):
-        self.factory.create_or_update_cases([self.extension_3])
+        created_cases = self._create_extension_chain()
         self.assertEqual(set(self.extension_ids),
-                         get_extension_chain([self.host], self.domain))
+                         get_extension_chain([created_cases[-1]], self.domain))
 
     @flag_enabled('EXTENSION_CASES_SYNC_ENABLED')
     def test_get_extension_to_close(self):
         """should return empty if case is not a host, otherwise should return full chain"""
-        created_cases = self.factory.create_or_update_cases([self.extension_3])
+        created_cases = self._create_extension_chain()
         # host open, should be empty
         no_cases = get_extensions_to_close(created_cases[-1], self.domain)
         self.assertEqual(set(), no_cases)
 
         created_cases[-1] = self.factory.create_or_update_case(CaseStructure(
-            case_id=self.host.case_id,
+            case_id=self.host_id,
             attrs={'close': True}
         ))[0]
 
@@ -75,27 +87,27 @@ class AutoCloseExtensionsTest(SyncBaseTest):
         self.assertEqual(set(), no_cases)
 
     @flag_enabled('EXTENSION_CASES_SYNC_ENABLED')
-    def test_close_cases(self):
+    def test_close_cases_host(self):
         """Closing a host should close all the extensions"""
-        self.factory.create_or_update_cases([self.extension_3])
-        self.assertFalse(CommCareCase.get(self.extension.case_id).closed)
-        self.assertFalse(CommCareCase.get(self.extension_2.case_id).closed)
-        self.assertFalse(CommCareCase.get(self.extension_3.case_id).closed)
+        self._create_extension_chain()
+        self.assertFalse(CommCareCase.get(self.extension_ids[0]).closed)
+        self.assertFalse(CommCareCase.get(self.extension_ids[1]).closed)
+        self.assertFalse(CommCareCase.get(self.extension_ids[2]).closed)
 
         self.factory.create_or_update_case(CaseStructure(
-            case_id=self.extension.case_id,
+            case_id=self.extension_ids[0],
             attrs={'close': True}
         ))
-        self.assertFalse(CommCareCase.get(self.host.case_id).closed)
-        self.assertTrue(CommCareCase.get(self.extension.case_id).closed)
-        self.assertFalse(CommCareCase.get(self.extension_2.case_id).closed)
-        self.assertFalse(CommCareCase.get(self.extension_3.case_id).closed)
+        self.assertFalse(CommCareCase.get(self.host_id).closed)
+        self.assertTrue(CommCareCase.get(self.extension_ids[0]).closed)
+        self.assertFalse(CommCareCase.get(self.extension_ids[1]).closed)
+        self.assertFalse(CommCareCase.get(self.extension_ids[2]).closed)
 
         self.factory.create_or_update_case(CaseStructure(
-            case_id=self.host.case_id,
+            case_id=self.host_id,
             attrs={'close': True}
         ))
-        self.assertTrue(CommCareCase.get(self.host.case_id).closed)
-        self.assertTrue(CommCareCase.get(self.extension.case_id).closed)
-        self.assertTrue(CommCareCase.get(self.extension_2.case_id).closed)
-        self.assertTrue(CommCareCase.get(self.extension_3.case_id).closed)
+        self.assertTrue(CommCareCase.get(self.host_id).closed)
+        self.assertTrue(CommCareCase.get(self.extension_ids[0]).closed)
+        self.assertTrue(CommCareCase.get(self.extension_ids[1]).closed)
+        self.assertTrue(CommCareCase.get(self.extension_ids[2]).closed)
