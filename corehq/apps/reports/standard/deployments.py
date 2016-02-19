af92148865caeace9236e0e0835ed8ced79212f5
Biyeun Buczyk <biyeun@dimagi.com>
2016-02-19 14:33:37 -0500

    cleanup imports

--- corehq/apps/reports/standard/deployments.py
+++ corehq/apps/reports/standard/deployments.py
@@ -25,13 +25,6 @@ from django.utils.translation import ugettext as _
 from dimagi.utils.couch.database import iter_docs
 from dimagi.utils.dates import safe_strftime
 from dimagi.utils.parsing import string_to_utc_datetime
-from corehq.apps.style.decorators import (
-    use_jquery_ui,
-    use_bootstrap3,
-    use_datatables,
-    use_select2,
-    use_daterangepicker,
-)
 
 
 class DeploymentsReport(GenericTabularReport, ProjectReport, ProjectReportParametersMixin):
