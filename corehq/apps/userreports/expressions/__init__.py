431a02b2b5bc94140fac1d014d92b59231699b1b
Sravan Reddy <sravfeyn@gmail.com>
2016-02-19 16:21:57 +0530

    make custom_ucr_expression module level setting

--- corehq/apps/userreports/expressions/__init__.py
+++ corehq/apps/userreports/expressions/__init__.py
@@ -2,6 +2,15 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 from corehq.apps.userreports.expressions.factory import ExpressionFactory
 
+
+def get_custom_ucr_expressions():
+    custom_ucr_expressions = settings.CUSTOM_UCR_EXPRESSIONS
+
+    for expression_list in settings.CUSTOM_UCR_EXPRESSION_LISTS:
+        custom_ucr_expressions += import_string(expression_list)
+
+    return custom_ucr_expressions
+
 # Bootstrap plugin expressions
-for type_name, factory_function_path in settings.CUSTOM_UCR_EXPRESSIONS:
+for type_name, factory_function_path in get_custom_ucr_expressions():
     ExpressionFactory.register(type_name, import_string(factory_function_path))
