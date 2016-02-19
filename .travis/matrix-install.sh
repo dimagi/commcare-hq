ce12ad5362835fe5034194beaabfd29078899620
Simon Kelly <skelly@dimagi.com>
2016-02-19 17:10:33 +0200

    really fix refs

--- .travis/matrix-install.sh
+++ .travis/matrix-install.sh
@@ -12,7 +12,7 @@ echo "Matrix params: MATRIX_TYPE=${MATRIX_TYPE:?Empty value for MATRIX_TYPE}, BO
 if [ "${MATRIX_TYPE}" = "python" ] || [ "${MATRIX_TYPE}" = "python-sharded" ]; then
 
     sleep 10  # kafka is slow to start up
-    setup_kafka
+    create_kafka_topics
     docker_run web_test .travis/misc-setup.sh
 
 elif [ "${MATRIX_TYPE}" = "javascript" ]; then
