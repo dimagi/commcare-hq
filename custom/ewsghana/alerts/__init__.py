e9bbee89f8edc5caefb732edfab07e66649f6a62
Nick Pellegrino <npellegrino@dimagi.com>
2016-02-21 16:28:11 -0500

    remove unreferenced string literals

--- custom/ewsghana/alerts/__init__.py
+++ custom/ewsghana/alerts/__init__.py
@@ -9,13 +9,7 @@ URGENT_NON_REPORTING = _('URGENT NON-REPORTING: More than half of the facilities
                          'have not reported for the past month. Please log in to http://ewsghana.com for details.')
 WEB_REMINDER = _('Dear %s, you have not visited ewsghana.com in a long time. '
                  'Please log in to find up-to-date info about stock availability and bottlenecks in Ghana.')
-REPORT_REMINDER = _('Dear %s, %s has not reported its stock this week.'
-                    'Please make sure that the SMS stock report is submitted. ')
-INCOMPLETE_REPORT = _('Dear Name, %s SMS stock report was INCOMPLETE.'
-                      'Please report for: %s')
 COMPLETE_REPORT = _('Dear %s, thank you for reporting the commodities you have in stock.')
-BELOW_REORDER_LEVELS = _('Dear %s, the following commodities are at or below re-order level at the %s. %s')
-ABOVE_THRESHOLD = _('Dear %s, these items are overstocked: %s. The district admin has been informed.')
 WITHOUT_RECEIPTS = _('Error! You submitted increases in stock levels of %s '
                      'without corresponding receipts. Please contact your DHIO or RHIO for assistance.')
 
