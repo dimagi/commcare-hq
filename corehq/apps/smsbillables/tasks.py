371c3cde31e5817f590cec0766888651e8d1763c
Nick Pellegrino <npellegrino@dimagi.com>
2016-02-21 23:13:51 -0500

    log via helper functions

--- corehq/apps/smsbillables/tasks.py
+++ corehq/apps/smsbillables/tasks.py
@@ -3,18 +3,15 @@ import urllib2
 from celery.schedules import crontab
 from celery.task import periodic_task
 from django.conf import settings
-from celery.utils.log import get_task_logger
 
 from corehq.apps.accounting.models import Currency
-
-
-logger = get_task_logger("accounting")
+from corehq.apps.accounting.utils import log_accounting_error, log_accounting_info
 
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue='background_queue')
 def update_exchange_rates(app_id=settings.OPEN_EXCHANGE_RATES_API_ID):
     try:
-        logger.info("Updating exchange rates...")
+        log_accounting_info("Updating exchange rates...")
         rates = json.load(urllib2.urlopen(
             'https://openexchangerates.org/api/latest.json?app_id=%s' % app_id))['rates']
         default_rate = float(rates[Currency.get_default().code])
@@ -22,9 +19,9 @@ def update_exchange_rates(app_id=settings.OPEN_EXCHANGE_RATES_API_ID):
             currency, _ = Currency.objects.get_or_create(code=code)
             currency.rate_to_default = float(rate) / default_rate
             currency.save()
-            logger.info("Exchange rate for %(code)s updated %(rate)f." % {
+            log_accounting_info("Exchange rate for %(code)s updated %(rate)f." % {
                 'code': currency.code,
                 'rate': currency.rate_to_default,
             })
     except Exception as e:
-        logger.error(e.message)
+        log_accounting_error(e.message)
