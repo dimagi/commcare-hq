008cfc71dd1ad61574cf1c1356163d7bbd759ade
Jennifer Schweers <orange.jenny@gmail.com>
2016-02-23 09:04:41 -0500

    Undo CUSTOMSETTINGS change

--- dockerhq.sh
+++ dockerhq.sh
@@ -80,7 +80,7 @@ case $key in
         web_runner run --rm web python manage.py migrate $@
         ;;
     runserver)
-        web_runner run --service-ports -e CUSTOMSETTINGS="docker.localsettings_docker" web $@
+        web_runner run --service-ports web $@
         ;;
     shell)
         web_runner run --rm web python manage.py shell
