aa676b68018738a5343d72ce0731dc07b2be0886
Biyeun Buczyk <biyeun@dimagi.com>
2016-02-23 17:47:35 -0500

    lint

--- corehq/apps/data_interfaces/interfaces.py
+++ corehq/apps/data_interfaces/interfaces.py
@@ -58,7 +58,10 @@ class CaseReassignmentInterface(CaseListMixin, DataInterface):
     @property
     def headers(self):
         headers = DataTablesHeader(
-            DataTablesColumn(mark_safe('Select  <a href="#" class="select-all btn btn-xs btn-info">all</a> <a href="#" class="select-none btn btn-xs btn-default">none</a>'), sortable=False, span=2),
+            DataTablesColumn(mark_safe(
+                'Select  <a href="#" class="select-all btn btn-xs btn-info">all'
+                '</a> <a href="#" class="select-none btn btn-xs btn-default">'
+                'none</a>'), sortable=False, span=2),
             DataTablesColumn(_("Case Name"), span=3, prop_name="name.exact"),
             DataTablesColumn(_("Case Type"), span=2, prop_name="type.exact"),
             DataTablesColumn(_("Owner"), span=2, prop_name="owner_display", sortable=False),
