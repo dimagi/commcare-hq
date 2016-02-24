68658bfc8827cf5799f0c96b6b94b38bbcdfc30b
Simon Kelly <skelly@dimagi.com>
2016-02-24 15:32:03 +0200

    add operation state vars

--- corehq/sql_accessors/migrations/0010_update_state_type_values.py
+++ corehq/sql_accessors/migrations/0010_update_state_type_values.py
@@ -3,12 +3,14 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 
-from corehq.form_processor.models import CaseTransaction, XFormInstanceSQL, CommCareCaseIndexSQL
+from corehq.form_processor.models import CaseTransaction, XFormInstanceSQL, CommCareCaseIndexSQL, XFormOperationSQL
 from corehq.sql_db.operations import RawSQLMigration, HqRunSQL
 
 migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {
     'FORM_STATE_ARCHIVED': XFormInstanceSQL.ARCHIVED,
     'FORM_STATE_NORMAL': XFormInstanceSQL.NORMAL,
+    'FORM_OPERATION_ARCHIVE': XFormOperationSQL.ARCHIVE,
+    'FORM_OPERATION_UNARCHIVE': XFormOperationSQL.UNARCHIVE,
     'TRANSACTION_TYPE_LEDGER': CaseTransaction.TYPE_LEDGER,
     'TRANSACTION_TYPE_FORM': CaseTransaction.TYPE_FORM,
     'RELATIONSHIP_TYPE_EXTENSION': CommCareCaseIndexSQL.EXTENSION,
