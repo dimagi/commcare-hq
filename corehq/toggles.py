93175afe026346de3b786a46a4cb3aef2643d206
Norman Hooper <nhooper@dimagi.com>
2016-02-24 08:39:22 +0200

    OpenClinica toggle

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -587,6 +587,13 @@ OPENCLINICA = StaticToggle(
     namespaces=[NAMESPACE_DOMAIN],
 )
 
+OPENCLINICA = StaticToggle(
+    'openclinica',
+    'Offer OpenClinica settings and CDISC ODM export',
+    TAG_UNKNOWN,
+    namespaces=[NAMESPACE_DOMAIN],
+)
+
 OPENLMIS = StaticToggle(
     'openlmis',
     'Offer OpenLMIS settings',
