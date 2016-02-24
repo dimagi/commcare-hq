0a1bbb718e06d54e4c8408d9cfa3a42dc47740d9
Sravan Reddy <sravfeyn@gmail.com>
2016-02-24 20:41:22 +0530

    make custom expression actually work; add tests

--- custom/icds/ucr/expressions.py
+++ custom/icds/ucr/expressions.py
@@ -1,7 +1,10 @@
 from corehq.apps.userreports.specs import TypeProperty
+from corehq.apps.userreports.expressions.getters import transform_date
+from corehq.apps.userreports.expressions.factory import ExpressionFactory
 from dimagi.ext.jsonobject import JsonObject
+from jsonobject.base_properties import DefaultProperty
 
-from ..models import ChildHealthCaseRow
+from custom.icds.models import ChildHealthCaseRow
 
 
 CUSTOM_UCR_EXPRESSIONS = [
@@ -11,14 +14,29 @@ CUSTOM_UCR_EXPRESSIONS = [
 
 class ChildHealthIndicatorExpressionSpec(JsonObject):
     type = TypeProperty('child_health_property')
-    indicator_name = TypeProperty('indicator_name')
+    indicator_name = DefaultProperty(required=True)
+    start_date = DefaultProperty(required=True)
+    end_date = DefaultProperty(required=True)
+
+    def configure(self, start_date, end_date):
+        self._start_date = start_date
+        self._end_date = end_date
 
     def __call__(self, item, context=None):
-        case_doc = item['case']
-        child_health_case = ChildHealthCaseRow(case_doc, context["iteration_month"])
+        case_doc = context.root_doc
+
+        start_date = self._start_date(item, context)
+        end_date = self._end_date(item, context)
+
+        child_health_case = ChildHealthCaseRow(case_doc, transform_date(start_date), transform_date(end_date))
+
         return getattr(child_health_case, self.indicator_name, None)
 
 
 def child_health_expression(spec, context):
     wrapped = ChildHealthIndicatorExpressionSpec.wrap(spec)
+    wrapped.configure(
+        start_date=ExpressionFactory.from_spec(wrapped.start_date, context),
+        end_date=ExpressionFactory.from_spec(wrapped.end_date, context)
+    )
     return wrapped
