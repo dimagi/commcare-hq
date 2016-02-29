6e089c8dda3d2936150152dfaf12ec5b2b08e558
Simon Kelly <skelly@dimagi.com>
2016-02-29 10:57:56 +0200

    move get_all_forms_in_all_domains to custom

--- corehq/apps/hqadmin/dbaccessors.py
+++ corehq/apps/hqadmin/dbaccessors.py
@@ -4,14 +4,6 @@ from corehq.util.test_utils import unit_testing_only
 from couchforms.models import XFormInstance
 
 
-@unit_testing_only
-def get_all_forms_in_all_domains():
-    return [
-        XFormInstance.wrap(doc)
-        for doc in get_all_docs_with_doc_types(XFormInstance.get_db(), ['XFormInstance'])
-    ]
-
-
 def get_number_of_forms_in_all_domains():
     """
     Return number of non-error forms (but incl. logs) total across all domains
