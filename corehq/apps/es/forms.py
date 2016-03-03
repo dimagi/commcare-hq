c2d5c15d0df2104a2db56867bc981ade8d54c1de
Ben Rudolph <brudolph@dimagi.com>
2016-03-03 15:15:16 -0500

    fix for unknown users

--- corehq/apps/es/forms.py
+++ corehq/apps/es/forms.py
@@ -21,6 +21,7 @@ class FormES(HQESQuery):
             submitted,
             completed,
             user_id,
+            user_ids_handle_unknown,
         ] + super(FormES, self).builtin_filters
 
     def user_aggregation(self):
@@ -54,3 +55,20 @@ def completed(gt=None, gte=None, lt=None, lte=None):
 
 def user_id(user_ids):
     return filters.term('form.meta.userID', list(user_ids))
+
+
+def user_ids_handle_unknown(user_ids):
+    missing_users = None in user_ids
+
+    user_ids = filter(None, user_ids)
+
+    if not missing_users:
+        user_filter = user_id(user_ids)
+    elif user_ids and missing_users:
+        user_filter = filters.OR(
+            user_id(user_ids),
+            filters.missing('form.meta.userID'),
+        )
+    else:
+        user_filter = filters.missing('form.meta.userID')
+    return user_filter
