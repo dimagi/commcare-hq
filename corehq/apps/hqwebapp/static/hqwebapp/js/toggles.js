f90512c57df34dfbc41aaf42e428a6ee0b31fdcc
Daniel Roberts <droberts@dimagi.com>
2016-03-03 15:54:00 -0500

    appease js linter

--- corehq/apps/hqwebapp/static/hqwebapp/js/toggles.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/toggles.js
@@ -3,17 +3,18 @@ hqDefine('hqwebapp/js/toggles.js', function () {
         var value = allToggles[toggleName];
         if (typeof value === 'undefined') {
             throw new Error(
-                    'Toggle ' + toggleName + 'not recognized. Must be one of: \n\n'
-                        + _.keys(allToggles).join("\n"));
+                'Toggle ' + toggleName + 'not recognized. Must be one of: \n\n' +
+                _.keys(allToggles).join("\n")
+            );
         }
         return value;
     };
     return {
         toggleEnabled: function (toggleName) {
-            return genericToggleEnabled(hqImport('#toggles').toggles, toggleName)
+            return genericToggleEnabled(hqImport('#toggles').toggles, toggleName);
         },
         previewEnabled: function (toggleName) {
-            return genericToggleEnabled(hqImport('#toggles').previews, toggleName)
+            return genericToggleEnabled(hqImport('#toggles').previews, toggleName);
         }
     };
 });
