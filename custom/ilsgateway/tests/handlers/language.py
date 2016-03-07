2b3efb16b7725c36aed0c7130eae1cbf27bebdce
kkrampa <kkrampa@soldevelo.com>
2016-03-07 10:06:33 +0100

    Added missing language tests

--- custom/ilsgateway/tests/handlers/language.py
+++ custom/ilsgateway/tests/handlers/language.py
@@ -1,20 +1,45 @@
-from corehq.apps.users.models import CommCareUser
-from custom.ilsgateway.tanzania.reminders import LANGUAGE_CONFIRM
+from django.utils import translation
+from django.utils.translation import ugettext as _
+
+from custom.ilsgateway.tanzania.reminders import LANGUAGE_CONFIRM, LANGUAGE_UNKNOWN, HELP_REGISTERED
 from custom.ilsgateway.tests.handlers.utils import ILSTestScript
 
 
 class ILSLanguageTest(ILSTestScript):
 
-    def setUp(self):
-        super(ILSLanguageTest, self).setUp()
+    def _verify_language(self, language, phone_number):
+        previous_language = translation.get_language()
+        translation.activate(language)
+        expected = _(HELP_REGISTERED)
+        translation.activate(previous_language)
+        script = """
+          %(phone)s > help
+          %(phone)s < %(help_registered)s
+        """ % {'phone': phone_number, 'help_registered': expected}
+        self.run_script(script)
 
-    def test_arrived_help(self):
-        self.user_fac1.language = 'en'
-        self.user_fac1.save()
-        language_message = """
-            5551234 > language hin
-            5551234 < {0}
-        """.format(unicode(LANGUAGE_CONFIRM) % dict(language='Hindi'))
-        self.run_script(language_message)
-        user = CommCareUser.get_by_username('stella')
-        self.assertEqual(user.language, 'hin')
+    def test_language_english(self):
+        translation.activate('en')
+        script = """
+            5551234 > language en
+            5551234 < %(language_confirm)s
+            """ % {'language_confirm': _(LANGUAGE_CONFIRM) % {"language": "English"}}
+        self.run_script(script)
+        self._verify_language('en', '5551234')
+
+    def test_language_swahili(self):
+        translation.activate('sw')
+        script = """
+            5551234 > lugha sw
+            5551234 < %(language_confirm)s
+            """ % {'language_confirm': _(LANGUAGE_CONFIRM) % {"language": "Swahili"}}
+        self.run_script(script)
+        self._verify_language('sw', '5551234')
+
+    def test_language_unknown(self):
+        translation.activate('sw')
+        script = """
+            5551234 > language de
+            5551234 < %(language_unknown)s
+            """ % {'language_unknown': _(LANGUAGE_UNKNOWN) % {"language": "de"}}
+        self.run_script(script)
