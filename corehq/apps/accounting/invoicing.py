8d1b41b709b109c3eae42c22bf6571516c4922eb
Nick Pellegrino <npellegrino@dimagi.com>
2016-03-07 11:20:16 -0500

    formatting

--- corehq/apps/accounting/invoicing.py
+++ corehq/apps/accounting/invoicing.py
@@ -204,8 +204,10 @@ class DomainInvoiceFactory(object):
         )
         total_balance = sum(invoice.balance for invoice in visible_domain_invoices)
 
-        should_set_date_due = ((total_balance > SMALL_INVOICE_THRESHOLD) or
-                               (invoice.account.auto_pay_enabled and total_balance > Decimal(0)))
+        should_set_date_due = (
+            total_balance > SMALL_INVOICE_THRESHOLD or
+            (invoice.account.auto_pay_enabled and total_balance > Decimal(0))
+        )
         if should_set_date_due:
             days_until_due = DEFAULT_DAYS_UNTIL_DUE
             if subscription.date_delay_invoicing is not None:
