40ea75edd04a3d90d9c796ef37c75d558b7ec11e
NoahCarnahan <ncarnahan@dimagi.com>
2016-03-11 14:12:39 -0500

    Add more transform functions to export consts

--- corehq/apps/export/const.py
+++ corehq/apps/export/const.py
@@ -2,13 +2,18 @@
 Some of these constants correspond to constants set in corehq/apps/export/static/export/js/const.js
 so if changing a value, ensure that both places reflect the change
 """
-from collections import namedtuple
+from corehq.apps.export.transforms import case_id_to_case_name, \
+    user_id_to_username
 
 DEID_TRANSFORM_FUNCTIONS = {
     'deid_id': lambda x: x,  # TODO: map these to actual deid functions
     'deid_date': lambda x: x,
 }
+CASE_NAME_TRANSFORM = "case_name_transform"
+USERNAME_TRANSFORM = "username_transform"
 TRANSFORM_FUNCTIONS = {
+    CASE_NAME_TRANSFORM: case_id_to_case_name,
+    USERNAME_TRANSFORM: user_id_to_username,
 }
 TRANSFORM_FUNCTIONS.update(DEID_TRANSFORM_FUNCTIONS)
 
