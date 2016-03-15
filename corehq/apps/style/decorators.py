ee31d6468ecf2d5f1c0695cf611aad03adf1e7db
Biyeun Buczyk <biyeun@dimagi.com>
2016-03-15 03:46:30 -0400

    new decorator for nvd3 v3

--- corehq/apps/style/decorators.py
+++ corehq/apps/style/decorators.py
@@ -167,6 +167,24 @@ def use_nvd3(view_func):
     return _wrapped
 
 
+def use_nvd3_v3(view_func):
+    """Use this decorator on the dispatch method of a TemplateView subclass
+    to enable the inclusion of the nvd3 library at the base template
+    level. nvd3 Version 3 is a library of charts for d3.
+
+    Example:
+
+    @use_nvd3
+    def dispatch(self, request, *args, **kwargs):
+        return super(MyView, self).dispatch(request, *args, **kwargs)
+    """
+    @wraps(view_func)
+    def _wrapped(class_based_view, request, *args, **kwargs):
+        request.use_nvd3_v3 = True
+        return view_func(class_based_view, request, *args, **kwargs)
+    return _wrapped
+
+
 def use_datatables(view_func):
     """Use this decorator on the dispatch method of a TemplateView subclass
     to enable the inclusion of the datatables library at the base template
