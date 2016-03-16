a9eab77db025caf451e1cb8dd96cb0f6bb3ddc6f
kkrampa <kkrampa@soldevelo.com>
2016-03-16 15:04:12 +0100

    Removed unused views and tasks

--- custom/ewsghana/__init__.py
+++ custom/ewsghana/__init__.py
@@ -1,5 +1,3 @@
-from custom.ewsghana.comparison_report import ProductsCompareReport, LocationsCompareReport,\
-    SMSUsersCompareReport, WebUsersCompareReport, SupplyPointsCompareReport
 from custom.ewsghana.reports.email_reports import CMSRMSReport, StockSummaryReport
 from custom.ewsghana.reports.maps import EWSMapReport
 from custom.ewsghana.reports.stock_levels_report import StockLevelsReport
@@ -22,13 +20,6 @@ CUSTOM_REPORTS = (
         CMSRMSReport,
         StockSummaryReport,
         StockTransactionReport
-    )),
-    ('Compare reports', (
-        ProductsCompareReport,
-        LocationsCompareReport,
-        SupplyPointsCompareReport,
-        WebUsersCompareReport,
-        SMSUsersCompareReport,
     ))
 )
 
