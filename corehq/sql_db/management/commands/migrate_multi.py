57fa37d7f6dc42cbb22b1897e7abc98a16884db6
Simon Kelly <skelly@dimagi.com>
2016-03-16 14:34:28 +0200

    better args support

--- corehq/sql_db/management/commands/migrate_multi.py
+++ corehq/sql_db/management/commands/migrate_multi.py
@@ -10,14 +10,21 @@ class Command(BaseCommand):
     help = "Call 'migrate' for each configured database"
 
     option_list = BaseCommand.option_list + (
-        make_option(
-            '--noinput',
-            action='store_true',
-            dest='noinput',
-            default=False),
-        )
+        make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.'),
+        make_option('--fake', action='store_true', dest='fake', default=False,
+            help='Mark migrations as run without actually running them'),
+        make_option('--list', '-l', action='store_true', dest='list', default=False,
+            help='Show a list of all known migrations and which are applied'),
+    )
 
     def handle(self, *args, **options):
         for db_alias in settings.DATABASES.keys():
             print '\n======================= Migrating DB: {} ======================='.format(db_alias)
-            call_command('migrate', database=db_alias, noinput=options['noinput'])
+            call_command(
+                'migrate',
+                database=db_alias,
+                interactive=options['interactive'],
+                fake=options['fake'],
+                list=options['list'],
+            )
