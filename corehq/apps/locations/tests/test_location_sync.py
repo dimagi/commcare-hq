cd1f6bdca529eb883f07a87bf22d2d5ecf970bf1
Ethan Soergel <esoergel@gmail.com>
2016-03-16 16:39:44 -0400

    Include more edge-case testing

--- corehq/apps/locations/tests/test_location_sync.py
+++ corehq/apps/locations/tests/test_location_sync.py
@@ -1,22 +1,29 @@
 from couchdbkit import ResourceNotFound
 from django.test import TestCase
 from corehq.apps.domain.shortcuts import create_domain
+from corehq.dbaccessors.couchapps.all_docs import get_doc_count_by_type
 from ..models import SQLLocation, Location
 from .util import setup_location_types, delete_all_locations
 
 DOMAIN = "location_sync_test"
 
+OTHER_FIELDS = {
+    "external_id": "1234",
+    "metadata": {"foo": "bar"},
+    "is_archived": False,
+}
+
 
 def couch_loc(name, location_type, parent=None):
-    loc = Location(site_code=name, name=name, domain=DOMAIN,
-                   location_type=location_type, parent=parent)
+    loc = Location(site_code=name, name=name, domain=DOMAIN, parent=parent,
+                   location_type=location_type, **OTHER_FIELDS)
     loc.save()
     return loc
 
 
 def sql_loc(name, location_type, parent=None):
-    loc = SQLLocation(site_code=name, name=name, domain=DOMAIN,
-                      location_type=location_type, parent=parent)
+    loc = SQLLocation(site_code=name, name=name, domain=DOMAIN, parent=parent,
+                      location_type=location_type, **OTHER_FIELDS)
     loc.save()
     return loc
 
@@ -29,6 +36,7 @@ class TestLocationSync(TestCase):
         cls.state = loc_types['state']
         cls.county = loc_types['county']
         cls.city = loc_types['city']
+        cls.db = Location.get_db()
 
     @classmethod
     def tearDownClass(cls):
@@ -49,16 +57,22 @@ class TestLocationSync(TestCase):
             return loc.parent.location_id if loc.parent else None
         self.assertEqual(get_parent(loc1), get_parent(loc2))
 
+    def assertNumLocations(self, number):
+        self.assertEqual(SQLLocation.objects.count(), number)
+        self.assertEqual(get_doc_count_by_type(self.db, 'Location'), number)
+
     def test_sync_couch_to_sql(self):
         mass = couch_loc("Massachusetts", self.state)
         suffolk = couch_loc("Suffolk", self.state, mass)
         boston = couch_loc("Boston", self.state, suffolk)
+        self.assertNumLocations(3)
 
         for loc in [boston, suffolk, mass]:
             self.assertLocationsEqual(loc, loc.sql_location)
             loc.delete()
             with self.assertRaises(SQLLocation.DoesNotExist):
                 SQLLocation.objects.get(location_id=loc.location_id)
+        self.assertNumLocations(0)
 
     def test_save_couch_without_loc_type(self):
         with self.assertRaises(SQLLocation.location_type.RelatedObjectDoesNotExist):
@@ -68,9 +82,31 @@ class TestLocationSync(TestCase):
         mass = sql_loc("Massachusetts", self.state)
         suffolk = sql_loc("Suffolk", self.state, mass)
         boston = sql_loc("Boston", self.state, suffolk)
+        self.assertNumLocations(3)
 
         for loc in [boston, suffolk, mass]:
             self.assertLocationsEqual(loc, loc.couch_location)
             loc.delete()
             with self.assertRaises(ResourceNotFound):
                 Location.get(loc.location_id)
+        self.assertNumLocations(0)
+
+    def test_edit_sql(self):
+        loc_id = sql_loc("Massachusetts", self.state).location_id
+
+        sql_location = SQLLocation.objects.get(location_id=loc_id)
+        sql_location.name = "New Massachusetts"
+        sql_location.save()
+
+        couch_location = Location.get(loc_id)
+        self.assertEqual(couch_location.name, "New Massachusetts")
+
+    def test_edit_couch(self):
+        loc_id = couch_loc("Massachusetts", self.state).location_id
+
+        couch_location = Location.get(loc_id)
+        couch_location.name = "New Massachusetts"
+        couch_location.save()
+
+        sql_location = SQLLocation.objects.get(location_id=loc_id)
+        self.assertEqual(sql_location.name, "New Massachusetts")
