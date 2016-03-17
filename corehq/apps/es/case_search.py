6f559a1c388839315c1680eef7a8a8b5c8c7075d
Farid Rener <proteusvacuum@gmail.com>
2016-03-17 15:05:37 +0100

    Query case properties

--- corehq/apps/es/case_search.py
+++ corehq/apps/es/case_search.py
@@ -9,10 +9,13 @@ from corehq.apps.es import case_search as case_search_es
     q = (case_search_es.CaseSearchES()
          .domain('testproject')
 """
+from . import filters, queries
 from .es_query import HQESQuery
-from . import filters
 
 from corehq.pillows.const import CASE_SEARCH_ALIAS
+
+
+PATH = "case_properties"
 
 
 class CaseSearchES(HQESQuery):
@@ -20,15 +23,44 @@ class CaseSearchES(HQESQuery):
 
     @property
     def builtin_filters(self):
-        return [case_property] + super(CaseSearchES, self).builtin_filters
+        return [case_property_filter] + super(CaseSearchES, self).builtin_filters
+
+    @property
+    def _case_property_queries(self):
+        """
+        Returns all current case_property queries
+        """
+        try:
+            return self.es_query['query']['filtered']['query']['bool']['must']
+        except (KeyError, TypeError):
+            return []
+
+    def case_property_query(self, key, value):
+        """
+        Search for a case property. Will overwrite other queries set with set_query unless they are 'must's
+        """
+        # Filter by case_properties.key and do a text search in case_properties.value
+        new_query = queries.nested(
+            PATH,
+            queries.filtered(
+                queries.search_string_query(value, default_fields=["{}.value".format(PATH)]),
+                filters.term("{}.key".format(PATH), key)
+            )
+        )
+        return self.set_query(
+            queries.BOOL(
+                queries.MUST(
+                    [new_query] + self._case_property_queries
+                )
+            )
+        )
 
 
-def case_property(key, value):
-    path = "case_properties"
+def case_property_filter(key, value):
     return filters.nested(
-        path,
+        PATH,
         filters.AND(
-            filters.term("{}.key".format(path), key),
-            filters.term("{}.value".format(path), value),
+            filters.term("{}.key".format(PATH), key),
+            filters.term("{}.value".format(PATH), value),
         )
     )
