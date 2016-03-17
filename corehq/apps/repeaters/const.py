f6d3f0f4649dcd19fe789c574a94dfcc538f096e
Ben Rudolph <brudolph@dimagi.com>
2016-03-17 18:22:40 -0400

    Revert "short circuit task creation if we know it's a failure"
    This reverts commit cb5af9f0786527ca6b6fbeae97d17eb067319620.
    
    Conflicts:
    	corehq/apps/repeaters/tasks.py

--- corehq/apps/repeaters/const.py
+++ corehq/apps/repeaters/const.py
@@ -7,7 +7,6 @@ CHECK_REPEATERS_INTERVAL = timedelta(minutes=5)
 CHECK_REPEATERS_KEY = 'check-repeaters-key'
 
 POST_TIMEOUT = 45  # seconds
-SIMPLE_POST_CACHED_PREFIX = 'repeater_cached_post:'
 
 RECORD_PENDING_STATE = 'PENDING'
 RECORD_SUCCESS_STATE = 'SUCCESS'
