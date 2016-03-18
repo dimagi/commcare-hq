ddb40ce4304e2149a1a862c7f1886d9da6702dda
Nick Pellegrino <npellegrino@dimagi.com>
2016-03-18 16:49:10 -0400

    add decorator to wrap data source methods that get data

---
+++ corehq/apps/userreports/decorators.py
@@ -0,0 +1,40 @@
+from functools import wraps
+
+from django.conf import settings
+
+from psycopg2._psycopg import ProgrammingError
+
+from sqlagg import (
+    ColumnNotFoundException,
+    TableNotFoundException,
+)
+
+from corehq.apps.userreports.exceptions import (
+    InvalidQueryColumn,
+    TableNotFoundWarning,
+    UserReportsError,
+)
+from corehq.util.soft_assert import soft_assert
+
+_soft_assert = soft_assert(
+    to='{}@{}'.format('npellegrino+ucr-get-data', 'dimagi.com'),
+    exponential_backoff=False,
+)
+
+
+def catch_and_raise_exceptions(func):
+    @wraps(func)
+    def _inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (
+            ColumnNotFoundException,
+            ProgrammingError,
+            InvalidQueryColumn,
+        ) as e:
+            if not settings.UNIT_TESTING:
+                _soft_assert(False, unicode(e))
+            raise UserReportsError(unicode(e))
+        except TableNotFoundException:
+            raise TableNotFoundWarning
+    return _inner
