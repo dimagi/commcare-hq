07d352aa6cd9cf23c7dceb87dffc59c3a724d77d
Daniel Miller <dmiller@dimagi.com>
2016-03-19 17:16:28 +0530

    Make python-sharded run all passed tests

--- .travis/test_runner.sh
+++ .travis/test_runner.sh
@@ -10,13 +10,12 @@
 
 set -e
 
-TESTS="$1"
 COMMAND="coverage run manage.py test --noinput --failfast --traceback --verbosity=2"
 
 /moto-s3/env/bin/moto_server s3 &
 
 if [ -z ${COMMAND_OVERRIDE} ]; then
-    $COMMAND $TESTS
+    $COMMAND "$@"
 else
     $COMMAND_OVERRIDE
 fi
