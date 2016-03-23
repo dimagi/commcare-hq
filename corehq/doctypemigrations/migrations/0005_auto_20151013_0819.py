f40fb2a4022697fc651090f94946b71a20192305
Daniel Roberts <droberts@dimagi.com>
2016-03-23 15:21:44 -0400

    remove unused imports in migration files

--- corehq/doctypemigrations/migrations/0005_auto_20151013_0819.py
+++ corehq/doctypemigrations/migrations/0005_auto_20151013_0819.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 from corehq.doctypemigrations.djangomigrations import assert_initial_complete
 from corehq.doctypemigrations.migrator_instances import fixtures_migration
 from corehq.sql_db.operations import HqRunPython
