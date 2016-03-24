0767a9461e3bb95e64b27641cadf06e681542d84
Cory Zue <czue@dimagi.com>
2016-03-24 16:48:35 +0200

    change arg names and add support for setting instance_id in util

--- corehq/apps/data_analytics/tests/utils.py
+++ corehq/apps/data_analytics/tests/utils.py
@@ -2,21 +2,20 @@ import uuid
 from corehq.apps.sofabed.models import FormData
 
 
-def save_to_analytics_db(domain, received, app, device, user_id, username):
+def save_to_analytics_db(domain, received_on, app_id, device_id, user_id, username=None, instance_id=None):
     unused_args = {
-        'time_start': received,
-        'time_end': received,
+        'time_start': received_on,
+        'time_end': received_on,
         'duration': 1
     }
+    instance_id = instance_id or uuid.uuid4().hex
     FormData.objects.create(
         domain=domain,
-        received_on=received,
-        instance_id=uuid.uuid4().hex,
-        app_id=app,
-        device_id=device,
+        received_on=received_on,
+        instance_id=instance_id,
+        app_id=app_id,
+        device_id=device_id,
         user_id=user_id,
         username=username,
         **unused_args
     )
-
-
