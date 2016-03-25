2fdf77e50ee28209816c320115179455d6512c85
Cory Zue <czue@dimagi.com>
2016-03-25 19:05:13 +0200

    refactor to helper class

--- corehq/apps/data_analytics/analytics.py
+++ corehq/apps/data_analytics/analytics.py
@@ -21,6 +21,35 @@ def get_app_submission_breakdown(domain_name, monthspan):
     )
 
 
+class NestedQueryHelper(object):
+
+    def __init__(self, base_query, terms):
+        self.base_query = base_query
+        self.terms = terms
+
+    def get_data(self):
+        previous_term = None
+        for name, field in reversed(self.terms):
+            term = TermsAggregation(name, field)
+            if previous_term is not None:
+                term = term.aggregation(previous_term)
+            previous_term = term
+        query = self.base_query.aggregation(term)
+
+        def _add_terms(aggregation_bucket, term, remaining_terms, current_counts, current_key=None):
+            for bucket in getattr(aggregation_bucket, term.name).buckets_list:
+                key = (bucket.key,) if current_key is None else current_key + (bucket.key,)
+                if remaining_terms:
+                    _add_terms(bucket, remaining_terms[0], remaining_terms[1:], current_counts, current_key=key)
+                else:
+                    # base case
+                    current_counts[key] += bucket.doc_count
+
+        counts = defaultdict(lambda: 0)
+        _add_terms(query.run().aggregations, self.terms[0], self.terms[1:], current_counts=counts)
+        return counts
+
+
 def get_app_submission_breakdown_es(domain_name, monthspan):
     terms = [
         AggregationTerm('app_id', 'app_id'),
@@ -32,26 +61,7 @@ def get_app_submission_breakdown_es(domain_name, monthspan):
         gte=monthspan.startdate,
         lt=monthspan.computed_enddate,
     )
-    previous_term = None
-    for name, field in reversed(terms):
-        term = TermsAggregation(name, field)
-        if previous_term is not None:
-            term = term.aggregation(previous_term)
-        previous_term = term
-    query = query.aggregation(term)
-
-    def _add_terms(aggregation_bucket, term, remaining_terms, current_counts, current_key=None):
-        for bucket in getattr(aggregation_bucket, term.name).buckets_list:
-            key = (bucket.key,) if current_key is None else current_key + (bucket.key,)
-            if remaining_terms:
-                _add_terms(bucket, remaining_terms[0], remaining_terms[1:], current_counts, current_key=key)
-            else:
-                # base case
-                current_counts[key] += bucket.doc_count
-
-    counts = defaultdict(lambda: 0)
-    _add_terms(query.run().aggregations, terms[0], terms[1:], current_counts=counts)
-    return counts
+    return NestedQueryHelper(base_query=query, terms=terms).get_data()
 
 
 def _get_app_submission_breakdown_es(domain_name, monthspan):
