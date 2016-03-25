f98923e12108c12a372fd0f6bf1a1e6536cec53b
Cory Zue <czue@dimagi.com>
2016-03-25 19:14:12 +0200

    delete sofabed helper

--- corehq/apps/data_analytics/analytics.py
+++ corehq/apps/data_analytics/analytics.py
@@ -1,24 +1,6 @@
 from collections import defaultdict, namedtuple
-from django.db.models import Count
 from corehq.apps.es.aggregations import MultiTermAggregation, AggregationTerm, TermsAggregation
 from corehq.apps.es.forms import FormES
-from corehq.apps.sofabed.models import FormData
-
-
-def get_app_submission_breakdown(domain_name, monthspan):
-    """
-    Returns one row for every app, device, userid, username tuple, along with the number of
-    forms submitted for that tuple.
-    """
-    start_date = monthspan.computed_startdate
-    end_date = monthspan.computed_enddate
-    forms_query = FormData.objects.filter(
-        domain=domain_name,
-        received_on__range=(start_date, end_date)
-    )
-    return forms_query.values('app_id', 'device_id', 'user_id', 'username').annotate(
-        num_of_forms=Count('instance_id')
-    )
 
 
 class NestedQueryHelper(object):
