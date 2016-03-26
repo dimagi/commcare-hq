39a225a5e298e0ebbb1c36b697f0bcdf711f74c3
Cory Zue <czue@dimagi.com>
2016-03-26 11:44:35 +0200

    remove unused utility

--- corehq/apps/data_analytics/tests/utils.py
+++ corehq/apps/data_analytics/tests/utils.py
@@ -1,26 +1,6 @@
-import uuid
-from corehq.apps.sofabed.models import FormData
 from corehq.form_processor.utils import TestFormMetadata
 from corehq.pillows.xform import XFormPillow
 from corehq.util.test_utils import make_es_ready_form
-
-
-def save_to_analytics_db(domain, received_on, app_id, device_id, user_id, username=None):
-    unused_args = {
-        'time_start': received_on,
-        'time_end': received_on,
-        'duration': 1
-    }
-    FormData.objects.create(
-        domain=domain,
-        received_on=received_on,
-        instance_id=uuid.uuid4().hex,
-        app_id=app_id,
-        device_id=device_id,
-        user_id=user_id,
-        username=username,
-        **unused_args
-    )
 
 
 def save_to_es_analytics_db(domain, received_on, app_id, device_id, user_id, username=None):
