96c003f87efc0593d2878d9a928b2597dc5645f7
Daniel Roberts <droberts@dimagi.com>
2016-03-26 13:09:45 -0400

    remove unused function convert_module_to_advanced

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -4075,14 +4075,6 @@ class ApplicationBase(VersionedDoc, SnapshotMixin,
         if self.build_spec.version:
             return LooseVersion(self.build_spec.version)
 
-    def get_preview_build(self):
-        preview = self.get_build()
-
-        for path in getattr(preview, '_attachments', {}):
-            if path.startswith('Generic/WebDemo'):
-                return preview
-        return CommCareBuildConfig.fetch().preview.get_build()
-
     @property
     def commcare_minor_release(self):
         """This is mostly just for views"""
@@ -4966,42 +4958,6 @@ class Application(ApplicationBase, TranslationMixin, HQMediaMixin):
 
         if from_module['case_type'] != to_module['case_type']:
             raise ConflictingCaseTypeError()
-
-    def convert_module_to_advanced(self, module_id):
-        from_module = self.get_module(module_id)
-
-        name = {lang: u'{} (advanced)'.format(name) for lang, name in from_module.name.items()}
-
-        case_details = deepcopy(from_module.case_details.to_json())
-        to_module = AdvancedModule(
-            name=name,
-            forms=[],
-            case_type=from_module.case_type,
-            case_label=from_module.case_label,
-            put_in_root=from_module.put_in_root,
-            case_list=from_module.case_list,
-            case_details=DetailPair.wrap(case_details),
-            product_details=DetailPair(
-                short=Detail(
-                    columns=[
-                        DetailColumn(
-                            format='plain',
-                            header={'en': ugettext("Product")},
-                            field='name',
-                            model='product',
-                        ),
-                    ],
-                ),
-                long=Detail(),
-            ),
-        )
-        to_module.get_or_create_unique_id()
-        to_module = self.add_module(to_module)
-
-        for form in from_module.get_forms():
-            self._copy_form(from_module, form, to_module)
-
-        return to_module
 
     @cached_property
     def has_case_management(self):
