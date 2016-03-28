f8fe5bdb1a316ad33685bc0d5b550b3cd62737e3
NoahCarnahan <ncarnahan@dimagi.com>
2016-03-28 11:05:20 -0400

    foo

--- corehq/pillows/mappings/xform_mapping.py
+++ corehq/pillows/mappings/xform_mapping.py
@@ -1,9 +1,7 @@
 from corehq.pillows.core import DATE_FORMATS_STRING, DATE_FORMATS_ARR
-from corehq.pillows.mappings import NULL_VALUE
 from corehq.util.elastic import es_index
 
-
-XFORM_INDEX = es_index("xforms_2016-03-02")
+XFORM_INDEX = es_index("xforms_20150403-1728")
 
 
 XFORM_MAPPING = {
@@ -15,7 +13,6 @@ XFORM_MAPPING = {
     },
     "properties": {
         'doc_type': {'type': 'string'},
-        'user_type': {'type': 'string', "index": "not_analyzed", "null_value": NULL_VALUE},
         "domain": {
             "type": "multi_field",
             "fields": {
@@ -92,7 +89,7 @@ XFORM_MAPPING = {
                             "type": "date",
                             "format": DATE_FORMATS_STRING
                         },
-                        "userID": {"type": "string", "index": "not_analyzed", "null_value": NULL_VALUE},
+                        "userID": {"type": "string", "index": "not_analyzed"},
                         "deviceID": {"type": "string", "index": "not_analyzed"},
                         "instanceID": {"type": "string", "index": "not_analyzed"},
                         "username": {"type": "string", "index": "not_analyzed"},
