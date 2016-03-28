a482203c463a981918d1b4a73a0e60f4619cd384
Cory Zue <czue@dimagi.com>
2016-03-28 14:38:09 +0200

    add utility to get all current elasticsearch indices + test

---
+++ corehq/apps/es/index_management.py
@@ -0,0 +1,19 @@
+from dimagi.ext import jsonobject
+from pillowtop.es_utils import get_all_elasticsearch_pillows
+
+
+class ElasticsearchIndexInfo(jsonobject.JsonObject):
+    index = jsonobject.StringProperty(required=True)
+    alias = jsonobject.StringProperty()
+
+
+def get_all_expected_es_indices():
+    """
+    Get all expected elasticsearch indices according to the currently running code
+    """
+    seen_indices = set()
+    pillows = get_all_elasticsearch_pillows()
+    for pillow in pillows:
+        assert pillow.es_index not in seen_indices
+        yield ElasticsearchIndexInfo(index=pillow.es_index, alias=pillow.es_alias)
+        seen_indices.add(pillow.es_index)
--- corehq/apps/es/tests/__init__.py
+++ corehq/apps/es/tests/__init__.py
@@ -2,4 +2,5 @@ from .test_aggregations import *
 from .test_esquery import *
 from .test_esqueryset import *
 from .test_filters import *
+from .test_index_management import *
 from .test_queries import *
