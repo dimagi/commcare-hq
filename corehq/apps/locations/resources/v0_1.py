19c0b4715df9bff0d316a0a63e166a4e003192de
kkrampa <kkrampa@soldevelo.com>
2016-03-31 12:04:00 +0200

    Fetch only child locations in child_queryset method

--- corehq/apps/locations/resources/v0_1.py
+++ corehq/apps/locations/resources/v0_1.py
@@ -50,13 +50,8 @@ class LocationResource(HqBaseResource):
         location_id = kwargs['pk']
         return get_object_or_not_exist(Location, location_id, domain)
 
-    def child_queryset(self, domain, include_inactive, parent_id):
-        if not parent_id:
-            locs = SQLLocation.root_locations(domain, include_inactive)
-        else:
-            parent = get_object_or_not_exist(Location, parent_id, domain)
-            locs = parent.sql_location.child_locations(include_inactive)
-        return locs
+    def child_queryset(self, domain, include_inactive, parent):
+        return parent.sql_location.child_locations(include_inactive)
 
     def obj_get_list(self, bundle, **kwargs):
         domain = kwargs['domain']
@@ -65,7 +60,12 @@ class LocationResource(HqBaseResource):
         include_inactive = json.loads(bundle.request.GET.get('include_inactive', 'false'))
         user = bundle.request.couch_user
         viewable = _user_locations_ids(user, project, only_editable=False)
-        locs = self.child_queryset(domain, include_inactive, parent_id)
+
+        if not parent_id:
+            locs = SQLLocation.root_locations(domain, include_inactive)
+        else:
+            parent = get_object_or_not_exist(Location, parent_id, domain)
+            locs = self.child_queryset(domain, include_inactive, parent)
         return [child for child in locs if child.location_id in viewable]
 
     def dehydrate_can_edit(self, bundle):
--- custom/ilsgateway/resources/v0_1.py
+++ custom/ilsgateway/resources/v0_1.py
@@ -1,20 +1,13 @@
-from corehq.apps.api.util import get_object_or_not_exist
-from corehq.apps.locations.models import SQLLocation, Location
 from corehq.apps.locations.resources.v0_1 import LocationResource
 
 
 class ILSLocationResource(LocationResource):
 
-    def child_queryset(self, domain, include_inactive, parent_id):
-        if not parent_id:
-            locs = SQLLocation.root_locations(domain, include_archive_ancestors=False)
-        else:
-            parent = get_object_or_not_exist(Location, parent_id, domain)
-            locs = parent.sql_location.get_children().filter(
-                location_type__administrative=True,
-                is_archived=False
-            )
-        return locs
+    def child_queryset(self, domain, include_inactive, parent):
+        return parent.sql_location.get_children().filter(
+            location_type__administrative=True,
+            is_archived=False
+        )
 
     class Meta(LocationResource.Meta):
         resource_name = 'ils_location'
