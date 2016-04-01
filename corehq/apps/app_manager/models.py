c20e30da7dddbb89631db5f6715eacd20d44a936
Norman Hooper <nhooper@dimagi.com>
2016-04-01 10:51:04 +0200

    case search models

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -1761,6 +1761,14 @@ class CaseList(IndexedSchema, NavMenuItemMediaMixin):
         _rename_key(self.label, old_lang, new_lang)
 
 
+class CaseSearch(DocumentSchema):
+    """
+    Case properties available to search on.
+    """
+    property = StringProperty()
+    label = StringProperty()
+
+
 class ParentSelect(DocumentSchema):
 
     active = BooleanProperty(default=False)
@@ -2142,6 +2150,7 @@ class Module(ModuleBase, ModuleDetailsMixin):
     referral_list = SchemaProperty(CaseList)
     task_list = SchemaProperty(CaseList)
     parent_select = SchemaProperty(ParentSelect)
+    search_config = SchemaListProperty(CaseSearch)
 
     @classmethod
     def wrap(cls, data):
@@ -2639,6 +2648,7 @@ class AdvancedModule(ModuleBase):
     has_schedule = BooleanProperty()
     schedule_phases = SchemaListProperty(SchedulePhase)
     get_schedule_phases = IndexedSchema.Getter('schedule_phases')
+    search_config = SchemaListProperty(CaseSearch)
 
     @classmethod
     def new_module(cls, name, lang):
