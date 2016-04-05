08e01c8e6555293fc88d43c7e6613dd5731b13b8
Cory Zue <czue@dimagi.com>
2016-04-05 11:26:28 +0200

    simplify posgres functions

--- corehq/sql_accessors/sql_templates/get_form_ids_in_domain_by_type.sql
+++ corehq/sql_accessors/sql_templates/get_form_ids_in_domain_by_type.sql
@@ -2,10 +2,11 @@
 DROP FUNCTION IF EXISTS get_form_ids_in_domain_by_type(TEXT, INTEGER);
 
 -- return SETOF so that we get 0 rows when no form matches otherwise we'll get an empty row
-CREATE FUNCTION get_form_ids_in_domain_by_type(domain_name TEXT, state INTEGER DEFAULT 1) RETURNS TABLE (form_id VARCHAR(255)) AS $$
+CREATE FUNCTION get_form_ids_in_domain_by_type(domain_name TEXT, p_state INTEGER DEFAULT 1) RETURNS TABLE (form_id VARCHAR(255)) AS $$
 BEGIN
-    RETURN QUERY  EXECUTE
-    'SELECT form_table.form_id FROM form_processor_xforminstancesql as form_table WHERE form_table.domain = $1 AND form_table.state = $2'
-    USING domain_name, state;
+    RETURN QUERY
+    SELECT form_table.form_id FROM form_processor_xforminstancesql as form_table
+    WHERE form_table.domain = domain_name
+      AND form_table.state = p_state;
 END;
 $$ LANGUAGE plpgsql;
