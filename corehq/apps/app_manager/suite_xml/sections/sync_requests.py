29462bac7b8c2044d5fb9e181042bc750e72435f
Norman Hooper <nhooper@dimagi.com>
2016-04-05 13:47:57 +0200

    Use id_strings for localization

--- corehq/apps/app_manager/suite_xml/sections/sync_requests.py
+++ corehq/apps/app_manager/suite_xml/sections/sync_requests.py
@@ -1,11 +1,17 @@
+from corehq.apps.app_manager import id_strings
 from corehq.apps.app_manager.suite_xml.contributors import SuiteContributorByModule
 from corehq.apps.app_manager.suite_xml.xml_models import (
+    Command,
+    Display,
     QueryData,
     QueryPrompt,
+    SessionDatum,
+    Stack,
     SyncRequest,
-    SyncRequestQuery,
     SyncRequestPost,
+    SyncRequestQuery,
     SyncRequestSession,
+    Text,
 )
 from corehq.util.view_utils import absolute_reverse
 
@@ -13,31 +19,14 @@ from corehq.util.view_utils import absolute_reverse
 class SyncRequestContributor(SuiteContributorByModule):
     """
     Adds a sync-request node, which sets the URL and query details for
-    synchronous search.
+    synchronous searching and case claiming.
 
     Search is available from the module's case list.
 
-    The node looks like this::
+    See "sync-request" in the `CommCare 2.0 Suite Definition`_ for details.
 
-        <sync-request>
-            <post url="">
-                <data key="" ref="some session based xpath expr"/>
-            </post>
-            <instance/>
-            <command id="...">
-                <display/>
-            </command>
-            <session>
-                <query url="some url" storage-instance="some_easy_to_reference_id">
-                    <data key="some_key" ref="session-based xpath ref"/>
-                    <prompt key="some_key">
-                       <display/>
-                    </prompt>
-                </query>
-                <datum/>
-            </session>
-            <stack/>
-        </sync-request>
+
+    .. _CommCare 2.0 Suite Definition: https://github.com/dimagi/commcare/wiki/Suite20
 
     """
     def get_module_contributions(self, module):
@@ -48,6 +37,13 @@ class SyncRequestContributor(SuiteContributorByModule):
 
             sync_request = SyncRequest(
                 # For synchronous searching:
+                command=Command(
+                    # TODO: How does <command> refer to module or case list?
+                    id=id_strings.case_list_locale(module) + '.or_something',
+                    display=Display(
+                        text=Text(locale_id=id_strings.case_search_locale(module)),
+                    ),
+                ),
                 session=SyncRequestSession(
                     queries=[
                         SyncRequestQuery(
@@ -61,23 +57,27 @@ class SyncRequestContributor(SuiteContributorByModule):
                             prompts=[
                                 QueryPrompt(
                                     key=s.property,
-                                    display=s.label  # TODO: Display(text=s.label)? Localization?
+                                    display=Display(
+                                        text=Text(locale_id=id_strings.search_property_locale(module, s.property)),
+                                    ),
                                 ) for s in module.search_config
                             ]
                         )
-                    ]
+                    ],
+                    # data=[SessionDatum()],  # TODO: Necessary? 0 or more, right?
                 ),
+                # stack=[Stack()],  # TODO: Necessary? 0 or more, right?
                 # For case claiming:
                 post=SyncRequestPost(
                     url=absolute_reverse('claim_case', args=[domain]),
                     data=[
                         QueryData(
                             key='case_id',
-                            ref='session/datum/case[@id]',  # TODO: ref to ID of chosen search result
+                            ref='session/datum[0]/case[@id]',  # TODO: ref to ID of *chosen* search result
                         ),
                         QueryData(
                             key='case_type',
-                            ref='session/datum/case/type',  # TODO: ref to case type of chosen search result
+                            ref='session/datum[0]/case/type',  # TODO: ref to case type of *chosen* search result
                         )
                     ]
                 )
