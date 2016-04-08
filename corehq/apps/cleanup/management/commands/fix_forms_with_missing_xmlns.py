c4e6227607e8ac2f08b3a22b907f0a7456ac9c32
NoahCarnahan <ncarnahan@dimagi.com>
2016-04-08 11:33:04 -0400

    more work on the management command

--- corehq/apps/cleanup/management/commands/fix_forms_with_missing_xmlns.py
+++ corehq/apps/cleanup/management/commands/fix_forms_with_missing_xmlns.py
@@ -1,13 +1,22 @@
+import uuid
+
+from couchdbkit import ResourceNotFound
+
 from corehq.apps.app_manager.models import Application
 from corehq.apps.app_manager.xform import XForm, parse_xml
+from corehq.apps.es import FormES
+from corehq.apps.es.filters import NOT, term
+from corehq.apps.es.forms import xmlns
+from couchforms import const
+from couchforms.const import ATTACHMENT_NAME
 from couchforms.models import XFormInstance
 from dimagi.utils.couch.database import iter_docs
 from django.core.management.base import BaseCommand
 from optparse import make_option
 
-
 class Command(BaseCommand):
     help = 'fix all of the things'
+    args = '<log_path>'
 
     option_list = BaseCommand.option_list + (
         make_option(
@@ -17,52 +26,98 @@ class Command(BaseCommand):
             default=False,
             help="Don't do the actual modifications, just print what would be affected"
         ),
-    )
 
     def handle(self, *args, **options):
 
         dry_run = options.get("dry_run", True)
-        new_xmlnss = {}
+        xlmns_map_path = options.get('xmlns_map_path', False)
+        log_path = args[0].strip()
 
-        for xform_instance in get_submissions_without_xmlns():
-            if xform_instance.app_id in new_xmlnss:
-                set_xmlns_on_submission(xform_instance, new_xmlnss[xform_instance.app_id], dry_run)
-            else:
-                app = Application.get(xform_instance.app_id)
-                forms_without_xmlns = get_forms_without_xmlns(app)
-                if len(forms_without_xmlns) == 1:
-                    new_xmlns = generate_random_xmlns()
-                    new_xmlnss[xform_instance.app_id] = new_xmlns
-                    set_xmlns_on_form(forms_without_xmlns[0], new_xmlns, dry_run)
+        new_xmlnss = {}
+        things_we_can_fix = 0
+        total, submissions = get_submissions_without_xmlns()
+
+        with open(log_path, "w") as f:
+            for i, xform_instance in enumerate(submissions):
+                if i % 500 == 0 and i != 0:
+                    print "Progress: {} of {} ({})".format(i, total, round(i/float(total), 2))
+                    print "We can fix {} of {} ({})".format(things_we_can_fix, i, round(things_we_can_fix/float(i), 2))
+                if xform_instance.app_id in new_xmlnss:
+                    things_we_can_fix += 1
+                    # We've already generated a new xmlns for this app
                     set_xmlns_on_submission(xform_instance, new_xmlnss[xform_instance.app_id], dry_run)
+                    f.write("Set new xmlns on submission {}\n".format(xform_instance._id))
                 else:
-                    # This will require more advanced logic
-                    pass
+                    app = Application.get(xform_instance.app_id)
+                    forms_without_xmlns = get_forms_without_xmlns(app)
+                    if len(forms_without_xmlns) == 1:
+                        form = forms_without_xmlns[0]
+                        if not xforms_with_real_xmlns_possibly_exist(app._id, form):
+                            things_we_can_fix += 1
+                            new_xmlns = generate_random_xmlns()
+                            new_xmlnss[xform_instance.app_id] = new_xmlns
+                            f.write("New xmlns for form {form.unique_id} in app {app._id} is {new_xmlns}\n".format(
+                                form=form,
+                                app=app,
+                                new_xmlns=new_xmlns
+                            ))
+                            set_xmlns_on_form(form, new_xmlns, app, dry_run)
+                            set_xmlns_on_submission(xform_instance, new_xmlnss[xform_instance.app_id], dry_run)
+                            f.write("Set new xmlns on submission {}\n".format(xform_instance._id))
 
 
 def get_submissions_without_xmlns():
     # TODO: This view won't get archived forms etc
-    submission_ids = XFormInstance.get_db().view(
+    submissions = XFormInstance.get_db().view(
         'couchforms/by_xmlns',
-        key="undefined",  # TODO: Is this the right key?
-        include_docs=False
+        key="undefined",
+        include_docs=False,
+        reduce=False,
     ).all()
-    return iter_docs(XFormInstance.get_db(), submission_ids)
-    # Myabe all_forms/_view/view is better view to use?
+    total_submissions = len(submissions)
+    submission_id_generator = (s['id'] for s in submissions)
+    doc_generator = (XFormInstance.wrap(i) for i in iter_docs(XFormInstance.get_db(), submission_id_generator))
+    return total_submissions, doc_generator
+
+
+def xforms_with_real_xmlns_possibly_exist(app_id, form):
+    """
+    Return True if there exist xforms submitted to the given app against a form
+    with the same name as the given name that have a real xmlns.
+
+    If this is True, it could indicate that the form had an xlmns, had submissions,
+    then lost the xmlns. If that's the case, we would want to give the submissions
+    this xmlns, not a new random one.
+    """
+    for form_name in form.name.values():
+        query = (FormES()
+             .term('form.@name', form_name)
+             .app(app_id)
+             .filter(NOT(xmlns('undefined')))
+        )
+        if query.count():
+            return True
+    return False
 
 
 def set_xmlns_on_submission(xform_instance, xmlns, dry_run):
     """
     Set the xmlns on an XFormInstance, and the save the document.
     """
+    old_xml = xform_instance.get_xml()
+    assert old_xml.count('xmlns="undefined"') == 1
+    new_xml = old_xml.replace('xmlns="undefined"', 'xmlns="{}"'.format(xmlns))
+    if not dry_run:
+        replace_xml(xform_instance, new_xml)
+
     xform_instance.xmlns = xmlns
     xform_instance.form['@xmlns'] = xmlns
-    # TODO: Modify the form.xml attachment
     if not dry_run:
         xform_instance.save()
 
 
-def set_xmlns_on_form(form, xmlns, dry_run):
+
+def set_xmlns_on_form(form, xmlns, app, dry_run):
     """
     Set the xmlns on an app_manager.models.Form, and save the document.
     """
@@ -70,10 +125,6 @@ def set_xmlns_on_form(form, xmlns, dry_run):
     wrapped_xml = XForm(xml)
 
     data = wrapped_xml.data_node.render()
-    # TODO: Possible values for the first arg:
-    #   "undefined"
-    #   None
-    #   "http://www.w3.org/2002/xforms"
     data = data.replace("undefined", xmlns, 1)
     wrapped_xml.instance_node.remove(wrapped_xml.data_node.xml)
     wrapped_xml.instance_node.append(parse_xml(data))
@@ -81,13 +132,28 @@ def set_xmlns_on_form(form, xmlns, dry_run):
 
     form.source = new_xml
     if not dry_run:
-        form.save()
+        # TODO: Alter the builds too?
+        app.save()
 
 
 def get_forms_without_xmlns(app):
-    # TODO
-    raise NotImplementedError
+    return [form for form in app.get_forms() if form.xmlns == "undefined"]
+
 
 def generate_random_xmlns():
-    # TODO
-    raise NotImplementedError
+    return 'http://openrosa.org/formdesigner/{}'.format(uuid.uuid4())
+
+
+def replace_xml(xform, new_xml):
+    if (
+        xform._attachments and
+        ATTACHMENT_NAME in xform._attachments and
+        'data' in xform._attachments[ATTACHMENT_NAME]
+    ):
+        raise Exception("Unexpected attachment format: _attachments")
+
+    else:
+        try:
+            xform.put_attachment(new_xml, name=ATTACHMENT_NAME, content_type='text/xml')
+        except ResourceNotFound:
+            raise Exception("Unexpected attachment format: old attachment scheme")
