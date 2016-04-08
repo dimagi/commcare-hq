1c605f6f69ec9f87d1dfdf1f2e161d19b9b01218
Cory Zue <czue@dimagi.com>
2016-04-08 13:50:26 +0200

    delete unused functions

--- corehq/util/elastic.py
+++ corehq/util/elastic.py
@@ -13,15 +13,8 @@ def es_index(index):
 
 @unit_testing_only
 def ensure_index_deleted(es_index):
-    ensure_production_index_deleted(es_index)
-
-
-def ensure_production_index_deleted(es_index):
-    """
-    Like ensure_index_deleted but usable outside unit tests
-    """
     try:
-        delete_production_es_index(es_index)
+        delete_es_index(es_index)
     except NotFoundError:
         pass
 
@@ -29,19 +22,11 @@ def ensure_production_index_deleted(es_index):
 @unit_testing_only
 def delete_es_index(es_index):
     if es_index.startswith(TEST_ES_PREFIX):
-        delete_production_es_index(es_index)
-
+        from corehq.elastic import get_es_new
+        es = get_es_new()
+        es.indices.delete(index=es_index)
     else:
         raise DeleteProductionESIndex('You cannot delete a production index in tests!!')
-
-
-def delete_production_es_index(es_index):
-    """
-    Like delete_es_index but usable outside unit tests
-    """
-    from corehq.elastic import get_es_new
-    es = get_es_new()
-    es.indices.delete(index=es_index)
 
 
 class DeleteProductionESIndex(Exception):
