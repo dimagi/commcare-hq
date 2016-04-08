270658b8bf9bfba888b9cc5eccca08d5028d7679
Norman Hooper <nhooper@dimagi.com>
2016-04-08 16:18:54 +0200

    Tests support both DB backends

--- corehq/apps/ota/tests/claim_case.py
+++ corehq/apps/ota/tests/claim_case.py
@@ -4,12 +4,12 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase, Client
 
 from casexml.apps.case.mock import CaseBlock
-from casexml.apps.case.models import CommCareCase
 from casexml.apps.case.util import post_case_blocks
 from corehq.apps.case_search.models import CLAIM_CASE_TYPE
 from corehq.apps.domain.shortcuts import create_domain
-from corehq.apps.hqcase.dbaccessors import get_case_ids_in_domain
 from corehq.apps.users.models import CommCareUser
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+from corehq.form_processor.tests import run_with_all_backends
 
 DOMAIN = 'test_domain'
 USERNAME = 'testy_mctestface'
@@ -31,19 +31,20 @@ class ClaimCaseTests(TestCase):
             owner_id='someone_else',
         ).as_xml()], {'domain': DOMAIN})
 
+    @run_with_all_backends
     def test_claim_case(self):
         """
         A claim case request should create an extension case
         """
-        self.assertEqual(len(get_case_ids_in_domain(DOMAIN, type=CLAIM_CASE_TYPE)), 0)
+        self.assertEqual(len(CaseAccessors(DOMAIN).get_case_ids_in_domain(CLAIM_CASE_TYPE)), 0)
 
         client = Client()
         client.login(username=USERNAME, password=PASSWORD)
         url = reverse('claim_case', kwargs={'domain': DOMAIN})
         client.post(url, {'case_id': self.case_id})
 
-        claim_ids = get_case_ids_in_domain(DOMAIN, type=CLAIM_CASE_TYPE)
+        claim_ids = CaseAccessors(DOMAIN).get_case_ids_in_domain(CLAIM_CASE_TYPE)
         self.assertEqual(len(claim_ids), 1)
-        claim = CommCareCase.get(claim_ids[0])
+        claim = CaseAccessors(DOMAIN).get_case(claim_ids[0])
         self.assertEqual(claim.owner_id, self.user.get_id)
         self.assertEqual(claim.name, CASE_NAME)
