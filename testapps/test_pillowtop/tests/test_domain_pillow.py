cbdb61465886e743a38ba3f05134f34883adec3a
Cory Zue <czue@dimagi.com>
2016-04-11 18:35:09 +0200

    add domain pillow and test

--- testapps/test_pillowtop/tests/test_domain_pillow.py
+++ testapps/test_pillowtop/tests/test_domain_pillow.py
@@ -1,14 +1,21 @@
 from django.test import TestCase
+from corehq.apps.change_feed import data_sources
+from corehq.apps.change_feed import document_types
+from corehq.apps.change_feed.document_types import get_doc_meta_object_from_document
+from corehq.apps.change_feed.producer import producer
+from corehq.apps.domain.models import Domain
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.domain.signals import commcare_domain_post_save
 from corehq.apps.domain.tests.test_utils import delete_all_domains
 from corehq.apps.es import DomainES
-from corehq.pillows.domain import DomainPillow
+from corehq.pillows.domain import DomainPillow, get_domain_kafka_to_elasticsearch_pillow
 from corehq.util.context_managers import drop_connected_signals
 from corehq.util.elastic import ensure_index_deleted
 from corehq.util.test_utils import trap_extra_setup
 from elasticsearch.exceptions import ConnectionError
 from pillowtop.es_utils import get_index_info_from_pillow, initialize_index
+from pillowtop.feed.interface import Change, ChangeMeta
+from testapps.test_pillowtop.utils import get_current_kafka_seq
 
 
 class DomainPillowTest(TestCase):
@@ -45,9 +52,46 @@ class DomainPillowTest(TestCase):
         # verify there
         self._verify_domain_in_es(domain_name)
 
+    def test_kafka_domain_pillow(self):
+        # make a domain
+        domain_name = 'domain-pillowtest-kafka'
+        with drop_connected_signals(commcare_domain_post_save):
+            domain = create_domain(domain_name)
+
+        # send to kafka
+        since = get_current_kafka_seq(document_types.DOMAIN)
+        producer.send_change(document_types.DOMAIN, domain_to_change(domain).metadata)
+
+        # send to elasticsearch
+        pillow = get_domain_kafka_to_elasticsearch_pillow()
+        pillow.process_changes(since={document_types.DOMAIN: since}, forever=False)
+        self.elasticsearch.indices.refresh(self.index_info.index)
+
+        # verify there
+        self._verify_domain_in_es(domain_name)
+
     def _verify_domain_in_es(self, domain_name):
         results = DomainES().run()
         self.assertEqual(1, results.total)
         domain_doc = results.hits[0]
         self.assertEqual(domain_name, domain_doc['name'])
         self.assertEqual('Domain', domain_doc['doc_type'])
+
+
+def domain_to_change(domain):
+    domain_doc = domain.to_json()
+    doc_info = get_doc_meta_object_from_document(domain_doc)
+    return Change(
+        id=domain_doc['_id'],
+        sequence_id='0',
+        document=domain_doc,
+        metadata=ChangeMeta(
+            document_id=domain_doc['_id'],
+            data_source_type=data_sources.COUCH,
+            data_source_name=Domain.get_db().dbname,
+            document_type=doc_info.raw_doc_type,
+            document_subtype=doc_info.subtype,
+            domain=domain_doc['name'],
+            is_deletion=False,
+        )
+    )
