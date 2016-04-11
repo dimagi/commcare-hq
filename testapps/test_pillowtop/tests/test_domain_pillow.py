9d1c14f4cb769156f237a9b9c78e9e2d56ccca8c
Cory Zue <czue@dimagi.com>
2016-04-11 10:38:45 +0200

    split out function, tweak initialization

--- testapps/test_pillowtop/tests/test_domain_pillow.py
+++ testapps/test_pillowtop/tests/test_domain_pillow.py
@@ -5,10 +5,10 @@ from corehq.apps.domain.tests.test_utils import delete_all_domains
 from corehq.apps.es import DomainES
 from corehq.pillows.domain import DomainPillow
 from corehq.util.context_managers import drop_connected_signals
-from corehq.util.elastic import delete_es_index, ensure_index_deleted
+from corehq.util.elastic import ensure_index_deleted
 from corehq.util.test_utils import trap_extra_setup
 from elasticsearch.exceptions import ConnectionError
-from pillowtop.es_utils import get_index_info_from_pillow
+from pillowtop.es_utils import get_index_info_from_pillow, initialize_index
 
 
 class DomainPillowTest(TestCase):
@@ -19,14 +19,14 @@ class DomainPillowTest(TestCase):
         'corehq.apps.tzmigration'
     ]
 
-
     def setUp(self):
         with trap_extra_setup(ConnectionError):
-            self.pillow = DomainPillow()
-        self.index_info = get_index_info_from_pillow(self.pillow)
-        self.elasticsearch = self.pillow.get_es_new()
-        delete_es_index(self.index_info.index)
+            pillow = DomainPillow(online=False)
+        self.index_info = get_index_info_from_pillow(pillow)
+        self.elasticsearch = pillow.get_es_new()
         delete_all_domains()
+        ensure_index_deleted(self.index_info.index)
+        initialize_index(self.elasticsearch, self.index_info)
 
     def tearDown(self):
         ensure_index_deleted(self.index_info.index)
@@ -38,10 +38,14 @@ class DomainPillowTest(TestCase):
             create_domain(domain_name)
 
         # send to elasticsearch
-        self.pillow.process_changes(since=0, forever=False)
+        pillow = DomainPillow()
+        pillow.process_changes(since=0, forever=False)
         self.elasticsearch.indices.refresh(self.index_info.index)
 
         # verify there
+        self._verify_domain_in_es(domain_name)
+
+    def _verify_domain_in_es(self, domain_name):
         results = DomainES().run()
         self.assertEqual(1, results.total)
         domain_doc = results.hits[0]
