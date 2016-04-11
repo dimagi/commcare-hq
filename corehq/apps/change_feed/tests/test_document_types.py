ec1d29a669fcc8cbe7c57b7042f5233677182873
Cory Zue <czue@dimagi.com>
2016-04-11 20:22:06 +0200

    tests for change meta

--- corehq/apps/change_feed/tests/test_document_types.py
+++ corehq/apps/change_feed/tests/test_document_types.py
@@ -1,5 +1,7 @@
 from django.test import SimpleTestCase
-from corehq.apps.change_feed.document_types import CASE, get_doc_meta_object_from_document, FORM, META, DOMAIN
+from corehq.apps.change_feed.document_types import CASE, get_doc_meta_object_from_document, FORM, META, DOMAIN, \
+    change_meta_from_doc
+from corehq.apps.change_feed.exceptions import MissingMetaInformationError
 from corehq.util.test_utils import generate_cases
 
 
@@ -33,9 +35,36 @@ class DocumentTypeTest(SimpleTestCase):
     ({'doc_type': 'CommCareUser-Deleted'}, META, None, None, True),
 ], DocumentTypeTest)
 def test_document_meta(self, raw_doc, expected_primary_type, expected_subtype=None,
-                        expected_domain=None, expected_deletion=False):
+                       expected_domain=None, expected_deletion=False):
     doc_meta = get_doc_meta_object_from_document(raw_doc)
     self.assertEqual(expected_primary_type, doc_meta.primary_type)
     self.assertEqual(expected_subtype, doc_meta.subtype)
     self.assertEqual(expected_domain, doc_meta.domain)
     self.assertEqual(expected_deletion, doc_meta.is_deletion)
+
+
+@generate_cases([
+    # primary type tests
+    (None,),
+    ({},),
+    ({'_id': 'id-without-type'},),
+    ({'doc_type': 'type-without-id'},),
+], DocumentTypeTest)
+def test_change_from_doc_failures(self, doc):
+    with self.assertRaises(MissingMetaInformationError):
+        change_meta_from_doc(doc, 'dummy-data-source', 'dummy-data-source-name')
+
+
+@generate_cases([
+    ({'_id': 'id', 'doc_type': 'CommCareCase', 'domain': 'test-domain'}, 'id'),
+], DocumentTypeTest)
+def test_change_from_doc_success(self, doc, expected_id):
+    change_meta = change_meta_from_doc(doc, 'dummy-data-source', 'dummy-data-source-name')
+    doc_meta = get_doc_meta_object_from_document(doc)
+    self.assertEqual(expected_id, change_meta.document_id)
+    self.assertEqual('dummy-data-source', change_meta.data_source_type)
+    self.assertEqual('dummy-data-source-name', change_meta.data_source_name)
+    self.assertEqual(doc_meta.raw_doc_type, change_meta.document_type)
+    self.assertEqual(doc_meta.subtype, change_meta.document_subtype)
+    self.assertEqual(doc_meta.domain, change_meta.domain)
+    self.assertEqual(doc_meta.is_deletion, change_meta.is_deletion)
