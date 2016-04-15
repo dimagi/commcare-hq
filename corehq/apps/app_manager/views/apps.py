cee552dd26c05507958f8b0f6747ee25b892853b
Cal Ellowitz <calellowitz@gmail.com>
2016-04-15 15:21:05 -0400

    killing build langs

--- corehq/apps/app_manager/views/apps.py
+++ corehq/apps/app_manager/views/apps.py
@@ -467,7 +467,7 @@ def edit_app_langs(request, domain, app_id):
     """
     app = get_app(domain, app_id)
     try:
-        langs, rename, build = validate_langs(request, app.langs)
+        langs, rename = validate_langs(request, app.langs)
     except AssertionError:
         return HttpResponse(status=400)
 
@@ -482,7 +482,6 @@ def edit_app_langs(request, domain, app_id):
                 list1.pop()
             list1.extend(list2)
     replace_all(app.langs, langs)
-    replace_all(app.build_langs, build)
 
     app.save()
     return json_response(langs)
