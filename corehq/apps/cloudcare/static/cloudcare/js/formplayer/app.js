9de5099cc26382360e90579cf11020fc0cf0ae89
Will Pride <wpride@dimagi.com>
2016-04-19 16:55:34 -0400

    working app loading

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
@@ -1,30 +1,36 @@
 var FormplayerFrontend = new Marionette.Application();
 
-FormplayerFrontend.on("before:start", function(){
+FormplayerFrontend.on("before:start", function () {
     var RegionContainer = Marionette.LayoutView.extend({
-        el: "app-container",
+        el: "#app-container",
 
         regions: {
-            main: "main-region"
+            main: "#main-region"
         }
     });
 
     FormplayerFrontend.regions = new RegionContainer();
 });
 
-FormplayerFrontend.navigate = function(route,  options){
-  options || (options = {});
-  Backbone.history.navigate(route, options);
+FormplayerFrontend.navigate = function (route, options) {
+    options || (options = {});
+    Backbone.history.navigate(route, options);
 };
 
-FormplayerFrontend.getCurrentRoute = function(){
-  return Backbone.history.fragment
+FormplayerFrontend.getCurrentRoute = function () {
+    return Backbone.history.fragment
 };
 
-FormplayerFrontend.on("start", function(){
-    console.log("Starting!");
+FormplayerFrontend.on("start", function (apps) {
+    console.log("Storing: " + apps);
 
-    if(this.getCurrentRoute() === ""){
-        FormplayerFrontend.trigger("apps:list");
+    //apps = new Entities.AppSelectCollection(apps);
+
+    FormplayerFrontend.trigger("apps:storeapps", apps);
+
+    //console.log("Apps: " + apps);
+    if (this.getCurrentRoute() === undefined) {
+        console.log("triggering apps:list");
+        FormplayerFrontend.trigger("apps:list", apps);
     }
 })
--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/js/apps/cc_apps.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/js/apps/cc_apps.js
@@ -1,28 +1,42 @@
 FormplayerFrontend.module("AppSelect", function(AppSelect, FormplayerFrontend, Backbone, Marionette, $, _){
-    AppSelect.Router(Marionette.Approuter.extend({
+    AppSelect.Router = Marionette.AppRouter.extend({
         appRoutes: {
-            "apps": "listApps",
-            "apps/:id/select": "selectApp"
+            "apps/:apps": "listApps",
+            "apps/:id/select": "selectApp",
+            "apps/:apps/store": "storeApps"
         }
-    }));
+    });
 
     var API = {
         listApps: function() {
+            console.log("list apps");
             AppSelect.List.Controller.listApps();
         },
         selectApp: function() {
-            AppSelect.Select.Controller.selectApp(id);
+            console.log("select app");
+            //AppSelect.Select.Controller.selectApp(id);
+        },
+        storeApps: function(apps){
+            console.log("CC apps API store apps");
+            FormplayerFrontend.request("appselect:storeapps", apps)
         }
     };
 
-    FormplayerFrontend.on("apps:list", function(){
-        AppSelect.navigate("apps");
-        API.listApps;
+    FormplayerFrontend.on("apps:list", function(apps){
+        console.log("apps:list: " + apps);
+        FormplayerFrontend.navigate("apps");
+        API.listApps();
     });
 
     FormplayerFrontend.on("apps:select", function(id){
-        AppSelect.navigate("apps/" + id + "/select");
-        API.editContact(id);
+        FormplayerFrontend.navigate("apps/" + id + "/select");
+        API.selectApp(id);
     });
+
+    FormplayerFrontend.on("apps:storeapps", function(apps){
+        console.log("apps:storeapps");
+        API.storeApps(apps);
+        FormplayerFrontend.navigate("apps");
+    })
 
 });
