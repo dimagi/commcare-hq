0f39ca99d9afaf2c74616f1fd5dd0faf3df0bcfb
Simon Kelly <skelly@dimagi.com>
2016-04-19 11:31:49 +0200

    typo

--- corehq/sql_proxy_accessors/sql_templates/save_ledger_values.sql
+++ corehq/sql_proxy_accessors/sql_templates/save_ledger_values.sql
@@ -4,7 +4,7 @@ CREATE FUNCTION save_ledger_values(
     case_id TEXT,
     ledger_value form_processor_ledgervalue,
     ledger_transactions form_processor_ledgertransaction[],
-    deprecated_forn_id TEXT DEFAULT NULL) RETURNS VOID AS $$
+    deprecated_form_id TEXT DEFAULT NULL) RETURNS VOID AS $$
     CLUSTER '{{ PL_PROXY_CLUSTER_NAME }}';
     RUN ON hash_string(case_id, 'siphash24');
 $$ LANGUAGE plproxy;
