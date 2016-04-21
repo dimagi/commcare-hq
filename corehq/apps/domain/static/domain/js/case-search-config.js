9e9968969e2f846be267354e9f19f7c2a15d345f
Norman Hooper <nhooper@dimagi.com>
2016-04-21 22:21:33 +0200

    Make property names observables

--- corehq/apps/domain/static/domain/js/case-search-config.js
+++ corehq/apps/domain/static/domain/js/case-search-config.js
@@ -5,14 +5,21 @@ hqDefine('domain/js/case-search-config.js', function () {
 
     var module = {};
 
+    var Property = function (name) {
+        var self = this;
+        self.name = ko.observable(name);
+    };
+
     var CaseTypeProps = function (caseType, properties) {
         var self = this;
-        self.case_type = ko.observable(caseType);
-        self.properties = ko.observableArray(properties);
-        // TODO: ^^^ This observableArray isn't changing the values of the strings inside it.
+
+        self.caseType = ko.observable(caseType);
+        self.properties = ko.observableArray(
+            _.map(properties, function (name) { return new Property(name); })
+        );
 
         self.addProperty = function () {
-            self.properties.push('');
+            self.properties.push(new Property(''));
         };
         self.removeProperty = function (property) {
             self.properties.remove(property);
@@ -51,9 +58,19 @@ hqDefine('domain/js/case-search-config.js', function () {
         };
 
         self.submit = function (form) {
+            var fuzzyProperties = [];
+            for (var i = 0; i < self.fuzzyProperties().length; i++) {
+                fuzzyProperties.push({
+                    case_type: self.fuzzyProperties()[i].caseType(),
+                    properties: _.map(
+                        self.fuzzyProperties()[i].properties(),
+                        function (property) { return property.name(); }
+                    ),
+                });
+            }
             $.post(form.action, {
                 'enable': self.toggleEnabled(),
-                'config': {'fuzzy_properties': _.pick(self.fuzzyProperties(), 'case_type', 'properties')},
+                'config': {'fuzzy_properties': fuzzyProperties},
             }).success(function () {
                 // TODO: Watch changes. On success change Save button from btn-primary to btn-default
             });
