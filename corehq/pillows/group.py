603a38067ddded76dd9e258fc9ca0793711ac34e
Cory Zue <czue@dimagi.com>
2016-04-22 16:07:13 +0200

    add new group reindexer and test

--- corehq/pillows/group.py
+++ corehq/pillows/group.py
@@ -1,7 +1,10 @@
 from corehq.apps.groups.models import Group
+from corehq.elastic import get_es_new
 
-from .mappings.group_mapping import GROUP_INDEX, GROUP_MAPPING
+from .mappings.group_mapping import GROUP_INDEX, GROUP_MAPPING, GROUP_INDEX_INFO
 from .base import HQPillow
+from pillowtop.reindexer.change_providers.couch import CouchViewChangeProvider
+from pillowtop.reindexer.reindexer import ElasticPillowReindexer
 
 
 class GroupPillow(HQPillow):
@@ -19,3 +22,20 @@ class GroupPillow(HQPillow):
     @classmethod
     def get_unique_id(self):
         return GROUP_INDEX
+
+
+def get_group_reindexer():
+    return ElasticPillowReindexer(
+        pillow=GroupPillow(),
+        change_provider=CouchViewChangeProvider(
+            couch_db=Group.get_db(),
+            view_name='all_docs/by_doc_type',
+            view_kwargs={
+                'startkey': ['Group'],
+                'endkey': ['Group', {}],
+                'include_docs': True,
+            }
+        ),
+        elasticsearch=get_es_new(),
+        index_info=GROUP_INDEX_INFO,
+    )
