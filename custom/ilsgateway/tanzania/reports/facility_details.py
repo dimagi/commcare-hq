4bef78a143254897aa32f0d2af780727cace793f
Kamil Krampa <kkrampa@soldevelo.com>
2016-04-26 11:07:54 +0200

    Recent messages tweaks

--- custom/ilsgateway/tanzania/reports/facility_details.py
+++ custom/ilsgateway/tanzania/reports/facility_details.py
@@ -157,10 +157,14 @@ class RecentMessages(ILSData):
     default_rows = 5
     use_datatables = True
 
+    def __init__(self, config=None):
+        super(RecentMessages, self).__init__(config, 'row_chart_all')
+
     @property
     def headers(self):
         return DataTablesHeader(
             DataTablesColumn('Date'),
+            DataTablesColumn('Contact'),
             DataTablesColumn('Direction'),
             DataTablesColumn('Text')
         )
@@ -172,9 +176,11 @@ class RecentMessages(ILSData):
                 .order_by('date'))
         messages = []
         for message in data:
+            recipient = message.recipient
             timestamp = ServerTime(message.date).user_time(self.config['timezone']).done()
             messages.append([
                 _fmt_timestamp(timestamp),
+                '{} ({})'.format(recipient.full_name, message.phone_number),
                 _fmt(message.direction),
                 _fmt(message.text),
             ])
