dedf30a98bbcaf685b2f96f34e6c301ef81a4098
Simon Kelly <skelly@dimagi.com>
2016-05-01 21:53:07 +0200

    refactor consumption restore to use already calculated value

--- corehq/ex-submodules/casexml/apps/phone/data_providers/case/stock.py
+++ corehq/ex-submodules/casexml/apps/phone/data_providers/case/stock.py
@@ -1,7 +1,10 @@
 from collections import defaultdict
-from casexml.apps.stock.consumption import compute_consumption_or_default
+from casexml.apps.stock.consumption import compute_consumption_or_default, compute_default_monthly_consumption, \
+    ConsumptionConfiguration
 from casexml.apps.stock.utils import get_current_ledger_state
+from corehq.form_processor.exceptions import LedgerValueNotFound
 from corehq.form_processor.interfaces.dbaccessors import LedgerAccessors
+from dimagi.utils.decorators.memoized import memoized
 from dimagi.utils.parsing import json_format_datetime
 from datetime import datetime
 from casexml.apps.stock.const import COMMTRACK_REPORT_XMLNS
@@ -23,17 +26,22 @@ def get_stock_payload(project, stock_settings, case_stub_list):
     def state_to_xml(state):
         return entry_xml(state.product_id, state.stock_on_hand)
 
-    def consumption_entry(case_id, product_id, section_id):
-        consumption_value = compute_consumption_or_default(
-            project.name,
+    def consumption_entry(case_id, product_id, state):
+        if state and state.daily_consumption is not None:
+            return entry_xml(state.product_id, state.daily_consumption)
+
+        return default_consumption_entry(case_id, product_id)
+
+    @memoized
+    def default_consumption_entry(case_id, product_id):
+        configuration = stock_settings.consumption_config or ConsumptionConfiguration()
+        default_value = compute_default_monthly_consumption(
             case_id,
             product_id,
-            datetime.utcnow(),
-            section_id,
-            stock_settings.consumption_config
+            configuration,
         )
-        if consumption_value is not None:
-            return entry_xml(product_id, consumption_value)
+        if default_value is not None:
+            return entry_xml(product_id, default_value)
 
     case_ids = [case.case_id for case in case_stub_list]
     all_current_ledgers = LedgerAccessors(project.name).get_current_ledger_state(case_ids)
@@ -41,14 +49,12 @@ def get_stock_payload(project, stock_settings, case_stub_list):
         case_id = commtrack_case_stub.case_id
         current_ledgers = all_current_ledgers[case_id]
 
-        section_product_map = defaultdict(lambda: [])
         section_timestamp_map = defaultdict(lambda: json_format_datetime(datetime.utcnow()))
         for section_id in sorted(current_ledgers.keys()):
             state_map = current_ledgers[section_id]
             sorted_product_ids = sorted(state_map.keys())
             stock_states = [state_map[p] for p in sorted_product_ids]
             as_of = json_format_datetime(max(txn.last_modified_date for txn in stock_states))
-            section_product_map[section_id] = sorted_product_ids
             section_timestamp_map[section_id] = as_of
             yield E.balance(*(state_to_xml(e) for e in stock_states),
                             **{'entity-id': case_id, 'date': as_of, 'section-id': section_id})
@@ -58,14 +64,28 @@ def get_stock_payload(project, stock_settings, case_stub_list):
             if (section_id in current_ledgers or
                     stock_settings.force_consumption_case_filter(commtrack_case_stub)):
 
-                consumption_product_ids = stock_settings.default_product_list \
-                    if stock_settings.default_product_list \
-                    else section_product_map[section_id]
+                consumption_entries = []
+                current_section_sate = current_ledgers.get(section_id, {})
+                if stock_settings.default_product_list:
+                    for product_id in stock_settings.default_product_list:
+                        state = current_section_sate.get(product_id, None)
+                        if not state:
+                            try:
+                                state = LedgerAccessors(project.name).get_ledger_value(
+                                    case_id, section_id, product_id
+                                )
+                            except LedgerValueNotFound:
+                                pass
 
-                consumption_entries = filter(lambda e: e is not None, [
-                    consumption_entry(case_id, p, section_id)
-                    for p in consumption_product_ids
-                ])
+                        consumption_entries.append(
+                            consumption_entry(case_id, product_id, state)
+                        )
+                else:
+                    for product_id in sorted(current_section_sate.keys()):
+                        state = current_section_sate[product_id]
+                        consumption_entries.append(
+                            consumption_entry(case_id, product_id, state)
+                        )
 
                 if consumption_entries:
                     yield E.balance(
