5168824b06e7a206a677b1df6b0455672b79f575
Kamil Krampa <kkrampa@soldevelo.com>
2016-05-04 15:28:06 +0200

    Added error message

--- custom/ilsgateway/tanzania/handlers/la.py
+++ custom/ilsgateway/tanzania/handlers/la.py
@@ -5,18 +5,14 @@ from corehq.apps.commtrack.models import StockState
 from corehq.apps.products.models import SQLProduct
 from custom.ilsgateway.tanzania.handlers.keyword import KeywordHandler
 from custom.ilsgateway.tanzania.handlers.soh import parse_report
-from custom.ilsgateway.tanzania.reminders import LOSS_ADJUST_HELP, LOSS_ADJUST_BAD_FORMAT, LOSS_ADJUST_CONFIRM
+from custom.ilsgateway.tanzania.reminders import LOSS_ADJUST_HELP, LOSS_ADJUST_BAD_FORMAT, LOSS_ADJUST_CONFIRM, \
+    LOSS_ADJUST_NO_SOH
 
 
 class LossAndAdjustment(KeywordHandler):
 
     def _create_stock_transaction(self, report, product_id, quantity):
-        try:
-            current_soh = StockState.objects.get(product_id=product_id, case_id=self.case_id).stock_on_hand
-        except StockState.DoesNotExist:
-            # When stock state does not exist it means that stock on hand has never been reported.
-            # Loss and adjustments report should be sent after SOH report.
-            return
+        current_soh = StockState.objects.get(product_id=product_id, case_id=self.case_id).stock_on_hand
         StockTransaction.objects.create(
             report=report,
             section_id=SECTION_TYPE_STOCK,
@@ -42,16 +38,22 @@ class LossAndAdjustment(KeywordHandler):
             domain=self.domain
         )
         error = False
+        products_without_soh = set()
         for product_code, quantity in parsed_report:
             try:
                 product_id = SQLProduct.objects.get(domain=self.domain, code__iexact=product_code).product_id
                 self._create_stock_transaction(report, product_id, quantity)
             except SQLProduct.DoesNotExist:
                 error = True
-        if not error:
-            self.respond(LOSS_ADJUST_CONFIRM)
-        else:
+            except StockState.DoesNotExist:
+                products_without_soh.add(product_code.lower())
+
+        if error:
             self.respond(LOSS_ADJUST_BAD_FORMAT)
+        elif products_without_soh:
+            self.respond(LOSS_ADJUST_NO_SOH, products_list=', '.join(sorted(products_without_soh)))
+        else:
+            self.respond(LOSS_ADJUST_CONFIRM)
         return True
 
     def help(self):
