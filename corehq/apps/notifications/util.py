3054ff1017c7fa4cc72a35c2defa7347953d16e3
Jonathan Emord <jonathan.emord@gmail.com>
2016-05-04 16:42:46 -0400

    notifications over 5 are always read

--- corehq/apps/notifications/util.py
+++ corehq/apps/notifications/util.py
@@ -4,15 +4,18 @@ def get_notifications_by_user(user, limit=10):
     notes = Notification.objects.filter(is_active=True)[:limit]
     read_notifications = Notification.objects.filter(users_read=user)
 
-    def _fmt_note(note):
+    def _fmt_note(note_idx):
+        index = note_idx[0]
+        note = note_idx[1]
+
         note_dict = {
             'id': note.id,
             'url': note.url,
             'date': note.created.date(),
             'content': note.content,
             'type': note.type,
-            'isRead': note in read_notifications
+            'isRead': (index > 4 or note in read_notifications),
         }
         return note_dict
 
-    return map(_fmt_note, notes)
+    return map(_fmt_note, enumerate(notes))
