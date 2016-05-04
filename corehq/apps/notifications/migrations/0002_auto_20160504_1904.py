b367356e24138796072b18e9b82e44f15bf01d81
Jonathan Emord <jonathan.emord@gmail.com>
2016-05-04 15:24:45 -0400

    Have active/inactive notifications

---
+++ corehq/apps/notifications/migrations/0002_auto_20160504_1904.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('notifications', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='notification',
+            options={'ordering': ['-created']},
+        ),
+        migrations.AddField(
+            model_name='notification',
+            name='is_active',
+            field=models.BooleanField(default=False),
+            preserve_default=True,
+        ),
+    ]
