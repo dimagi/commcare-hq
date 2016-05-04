47701e948b868eda64ed3782d16ad113826a85fb
Simon Kelly <skelly@dimagi.com>
2016-05-04 11:35:48 +0200

    update get_case_form_ids to remove nested query

---
+++ corehq/sql_accessors/migrations/0023_update_get_case_form_ids.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+from corehq.form_processor.models import CaseTransaction
+from corehq.sql_db.operations import RawSQLMigration, HqRunSQL
+
+migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {
+    'TRANSACTION_TYPE_FORM': CaseTransaction.TYPE_FORM
+})
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('sql_accessors', '0021_get_ledger_values_for_cases'),
+    ]
+
+    operations = [
+        migrator.get_migration('get_case_form_ids.sql'),
+    ]
