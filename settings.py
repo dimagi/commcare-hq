e9bb917e3e2c9546cade933630ebdceef15e3544
Simon Kelly <skelly@dimagi.com>
2016-05-04 21:06:21 +0200

    adjustment 2

--- settings.py
+++ settings.py
@@ -1724,8 +1724,7 @@ TRAVIS_TEST_GROUPS = (
         'facilities', 'fixtures', 'fluff_filter', 'formplayer',
         'formtranslate', 'fri', 'grapevine', 'groups', 'gsid', 'hope',
         'hqadmin', 'hqcase', 'hqcouchlog', 'hqmedia',
-        'care_pathways', 'common', 'compressor', 'smsbillables', 'ilsgateway', 'ewsghana',
-        'logistics'
+        'care_pathways', 'common', 'compressor', 'smsbillables', 'ilsgateway'
     ),
 )
 
