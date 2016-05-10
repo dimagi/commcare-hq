36702a6fe8a2b08349e80807f0c045a8977e9e9a
Simon Kelly <skelly@dimagi.com>
2016-05-10 17:06:49 +0200

    refactor tests for SQL

--- corehq/apps/reports/tests/test_data_sources.py
+++ corehq/apps/reports/tests/test_data_sources.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 
 from django.test import TestCase
+from django.test.utils import override_settings
 
 from corehq.apps.commtrack.models import StockState
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.products.models import SQLProduct
-from corehq.apps.reports.commtrack.data_sources import StockStatusDataSource
+from corehq.apps.reports.commtrack.data_sources import StockStatusDataSource, StockStatusDataSourceNew
 from corehq.apps.users.models import WebUser
+from corehq.form_processor.models import LedgerValue
+from corehq.form_processor.utils.general import should_use_sql_backend
 from dimagi.utils.couch.loosechange import map_reduce
 from corehq.apps.commtrack.helpers import make_product
 from corehq.apps.commtrack.tests.util import make_loc
@@ -21,9 +24,10 @@ TEST_DOMAIN = 'commtrack-test1'
 
 
 class DataSourceTest(TestCase):
+    data_source_class = StockStatusDataSource
+
     @classmethod
     def setUpClass(cls):
-
         cls.domain = create_domain(TEST_DOMAIN)
         cls.domain.convert_to_commtrack()
         cls.couch_user = WebUser.create(None, "report_test", "foobar")
@@ -55,6 +59,7 @@ class DataSourceTest(TestCase):
         cls.sites = {}
         cls.regions = {}
         cls.districts = {}
+        ledger_values = []
         for region_name, districts in test_setup.items():
             region = make_loc(region_name, domain=TEST_DOMAIN, type='region')
             cls.regions[region_name] = region
@@ -67,15 +72,30 @@ class DataSourceTest(TestCase):
                     supply_point = site.linked_supply_point()
                     for p_code, stock in products.items():
                         prod = cls.products[p_code]
-                        StockState.objects.create(
-                            section_id='stock',
-                            case_id=supply_point.case_id,
-                            product_id=prod._id,
-                            stock_on_hand=stock,
-                            last_modified_date=datetime.utcnow(),
-                            sql_product=SQLProduct.objects.get(product_id=prod._id),
-                            sql_location=site.sql_location
-                        )
+                        if should_use_sql_backend(TEST_DOMAIN):
+                            ledger_values.append(LedgerValue(
+                                domain=TEST_DOMAIN,
+                                section_id='stock',
+                                case_id=supply_point.case_id,
+                                entry_id=prod._id,
+                                balance=stock,
+                                last_modified=datetime.utcnow(),
+                                location_id=site._id,
+                            ))
+                        else:
+                            StockState.objects.create(
+                                section_id='stock',
+                                case_id=supply_point.case_id,
+                                product_id=prod._id,
+                                stock_on_hand=stock,
+                                last_modified_date=datetime.utcnow(),
+                                sql_product=SQLProduct.objects.get(product_id=prod._id),
+                                sql_location=site.sql_location
+                            )
+
+        if should_use_sql_backend(TEST_DOMAIN):
+            from corehq.form_processor.backends.sql.dbaccessors import LedgerAccessorSQL
+            LedgerAccessorSQL.save_ledger_values(ledger_values)
 
     @classmethod
     def tearDownClass(cls):
@@ -87,7 +107,7 @@ class DataSourceTest(TestCase):
             'domain': TEST_DOMAIN,
             'enddate': datetime.utcnow()
         }
-        data = list(StockStatusDataSource(config).get_data())
+        data = list(self.data_source_class(config).get_data())
         self.assertEqual(len(data), 6)
         by_location = map_reduce(lambda row: [(row[LOCATION_ID],)], data=data, include_docs=True)
 
@@ -106,7 +126,7 @@ class DataSourceTest(TestCase):
             'location_id': location,
             'enddate': datetime.utcnow()
         }
-        data = list(StockStatusDataSource(config).get_data())
+        data = list(self.data_source_class(config).get_data())
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0][LOCATION_ID], self.sites['A-b-1'][0]._id)
         self.assertEqual(data[0][PRODUCT_ID], self.products['pA']._id)
@@ -120,7 +140,7 @@ class DataSourceTest(TestCase):
             'aggregate': True,
             'enddate': datetime.utcnow()
         }
-        data = list(StockStatusDataSource(config).get_data())
+        data = list(self.data_source_class(config).get_data())
         self.assertEqual(len(data), 2)
         by_product = dict((row[PRODUCT_ID], row) for row in data)
         pA_id = self.products['pA']._id
@@ -137,6 +157,20 @@ class DataSourceTest(TestCase):
             'aggregate': True,
             'enddate': datetime.utcnow()
         }
-        data = list(StockStatusDataSource(config).get_data())
+        data = list(self.data_source_class(config).get_data())
         self.assertEqual(len(data), 1)
         self.assertEqual(data[0][CURRENT_STOCK], 2)
+
+
+class DataSourceTestSQL(DataSourceTest):
+    data_source_class = StockStatusDataSourceNew
+
+    @classmethod
+    def setUpClass(cls):
+        with override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True):
+            super(DataSourceTestSQL, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        with override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True):
+            super(DataSourceTestSQL, cls).tearDownClass()
