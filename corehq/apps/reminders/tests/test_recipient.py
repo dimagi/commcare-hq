741717193dbdbf404a75d2d45f580b034ce6fc62
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-05-11 09:14:04 -0400

    Update recipient test

--- corehq/apps/reminders/tests/test_recipient.py
+++ corehq/apps/reminders/tests/test_recipient.py
@@ -16,45 +16,48 @@ class ReminderRecipientTest(TestCase):
         self.domain_obj = Domain(name=self.domain)
         self.domain_obj.save()
 
-    def tearDown(self):
-        self.domain_obj.delete()
-
-    @run_with_all_backends
-    def test_recipient_case_owner_location_parent(self):
-        parent_location_type = LocationType.objects.create(
+        self.parent_location_type = LocationType.objects.create(
             domain=self.domain,
             name='parent type',
             code='parent'
         )
 
-        child_location_type = LocationType.objects.create(
+        self.child_location_type = LocationType.objects.create(
             domain=self.domain,
             name='child type',
             code='child',
-            parent_type=parent_location_type
+            parent_type=self.parent_location_type
         )
 
+        self.user = CommCareUser.create(self.domain, 'test', 'test')
+
+    def tearDown(self):
+        self.parent_location_type.delete()
+        self.child_location_type.delete()
+        self.user.delete()
+        self.domain_obj.delete()
+
+    @run_with_all_backends
+    def test_recipient_case_owner_location_parent(self):
         parent_location = SQLLocation.objects.create(
             domain=self.domain,
             name='parent test',
             site_code='parent',
-            location_type=parent_location_type
+            location_type=self.parent_location_type
         )
 
         child_location = SQLLocation.objects.create(
             domain=self.domain,
             name='child test',
             site_code='child',
-            location_type=child_location_type,
+            location_type=self.child_location_type,
             parent=parent_location
         )
 
-        user = CommCareUser.create(self.domain, 'test', 'test')
-        user.location_id = child_location.location_id
-        user.save()
+        self.user.set_location(child_location.couch_location)
 
-        with create_test_case(self.domain, 'test-case', 'test-name', owner_id=user.get_id) as case:
-            self.assertEqual(case.owner_id, user.get_id)
+        with create_test_case(self.domain, 'test-case', 'test-name', owner_id=self.user.get_id) as case:
+            self.assertEqual(case.owner_id, self.user.get_id)
             handler = CaseReminderHandler(domain=self.domain, recipient=RECIPIENT_CASE_OWNER_LOCATION_PARENT)
             reminder = CaseReminder(domain=self.domain, case_id=case.case_id)
 
@@ -70,9 +73,8 @@ class ReminderRecipientTest(TestCase):
                 self.assertIsNone(reminder.recipient)
 
             # Remove child location
+            self.user.unset_location()
             child_location.delete()
-            user.location_id = None
-            user.save()
             with patch('corehq.apps.reminders.models.CaseReminder.handler', new=handler):
                 self.assertIsNone(reminder.recipient)
 
@@ -80,7 +82,3 @@ class ReminderRecipientTest(TestCase):
             reminder.case_id = None
             with patch('corehq.apps.reminders.models.CaseReminder.handler', new=handler):
                 self.assertIsNone(reminder.recipient)
-
-        parent_location_type.delete()
-        child_location_type.delete()
-        user.delete()
