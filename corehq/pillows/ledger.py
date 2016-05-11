393013ed25f19b0d6e9958cdac49c79aa5f15bfd
Simon Kelly <skelly@dimagi.com>
2016-05-11 21:40:30 +0200

    ledger pillow

---
+++ corehq/pillows/ledger.py
@@ -0,0 +1,37 @@
+from corehq.apps.change_feed import topics
+from corehq.apps.change_feed.consumer.feed import KafkaChangeFeed
+from corehq.elastic import get_es_new
+from corehq.form_processor.change_providers import LedgerChangeProvider
+from corehq.pillows.mappings.ledger_mapping import LEDGER_INDEX_INFO
+from pillowtop.checkpoints.manager import PillowCheckpoint, PillowCheckpointEventHandler
+from pillowtop.pillow.interface import ConstructedPillow
+from pillowtop.processors.elastic import ElasticProcessor
+from pillowtop.reindexer.reindexer import ElasticPillowReindexer
+
+
+def get_ledger_to_elasticsearch_pillow(pillow_id='LedgerToElasticsearchPillow'):
+    checkpoint = PillowCheckpoint(
+        'ledger-to-elasticsearch',
+    )
+    case_processor = ElasticProcessor(
+        elasticsearch=get_es_new(),
+        index_info=LEDGER_INDEX_INFO
+    )
+    return ConstructedPillow(
+        name=pillow_id,
+        checkpoint=checkpoint,
+        change_feed=KafkaChangeFeed(topics=[topics.LEDGER], group_id='ledgers-to-es'),
+        processor=case_processor,
+        change_processed_event_handler=PillowCheckpointEventHandler(
+            checkpoint=checkpoint, checkpoint_frequency=100
+        ),
+    )
+
+
+def get_ledger_reindexer():
+    return ElasticPillowReindexer(
+        pillow=get_ledger_to_elasticsearch_pillow(),
+        change_provider=LedgerChangeProvider(),
+        elasticsearch=get_es_new(),
+        index_info=LEDGER_INDEX_INFO,
+    )
