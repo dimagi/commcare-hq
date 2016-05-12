23cd6fab1b34904c2923e8f1160532583dcaed42
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2016-05-12 10:43:02 +0200

    Added new reports for icds-test domain

--- custom/icds_reports/reports/basic_info_report.py
+++ custom/icds_reports/reports/basic_info_report.py
@@ -1,5 +1,7 @@
 from custom.icds_reports.reports import IcdsBaseReport
-from custom.icds_reports.sqldata import Identification, Operationalization, Sectors, Population, BirthsAndDeaths
+from custom.icds_reports.sqldata import Identification, Operationalization, Sectors, Population, BirthsAndDeaths, \
+    AWCDetails, SupplementaryNutrition, UsingSalt, ProgrammeCoverage, PreschoolEducation, GrowthMonitoring, \
+    ImmunizationCoverage, Vhnd, ReferralServices, Monitoring
 from dimagi.utils.decorators.memoized import memoized
 
 
@@ -15,5 +17,14 @@ class BasicInfoReport(IcdsBaseReport):
             Sectors(config=config),
             Population(config=config),
             BirthsAndDeaths(config=config),
-            AWCDetails(config=config)
+            AWCDetails(config=config),
+            SupplementaryNutrition(config=config),
+            UsingSalt(config=config),
+            ProgrammeCoverage(config=config),
+            PreschoolEducation(config=config),
+            GrowthMonitoring(config=config),
+            ImmunizationCoverage(config=config),
+            Vhnd(config=config),
+            ReferralServices(config=config),
+            Monitoring(config=config)
         ]
