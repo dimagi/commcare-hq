697a26f3e75ccfd4eade9f0cd4a1bf1cefdf44b7
Cal Ellowitz <calellowitz@gmail.com>
2016-05-13 18:35:21 +0000

    fixing attachment issue

--- corehq/apps/app_manager/views/download.py
+++ corehq/apps/app_manager/views/download.py
@@ -407,7 +407,7 @@ def download_index_files(app, build_profile_id=None):
         prefix += build_profile_id + '/'
     if app.copy_of:
         # profile hasnt been built yet
-        if not (prefix + 'profile.ccpr') in app._attachments:
+        if not (prefix + 'profile.ccpr') in app.blobs:
             app.create_build_files(save=True, build_profile_id=build_profile_id)
             app.save()
         files = [(path[len(prefix):], app.fetch_attachment(path))
