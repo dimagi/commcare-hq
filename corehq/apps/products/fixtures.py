4b31dd04fe4d4a0dbfef77456fe111004457a463
Farid Rener <proteusvacuum@gmail.com>
2016-05-13 17:30:22 -0400

    Use Product

--- corehq/apps/products/fixtures.py
+++ corehq/apps/products/fixtures.py
@@ -55,7 +55,7 @@ class ProductFixturesProvider(object):
 
         def get_products():
             return sorted(
-                SQLProduct.by_domain(user.domain),
+                Product.by_domain(user.domain),
                 key=lambda product: product.code
             )
 
