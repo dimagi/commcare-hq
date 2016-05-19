1fd568fb698a5b70e7432375b45986267d916fba
Jonathan Emord <jonathan.emord@gmail.com>
2016-05-19 11:31:56 -0400

    Hide loading properties from migrated apps

--- corehq/apps/app_manager/static/app_manager/js/case-config-ui-2.js
+++ corehq/apps/app_manager/static/app_manager/js/case-config-ui-2.js
@@ -28,6 +28,7 @@ hqDefine('app_manager/js/case-config-ui-2.js', function () {
         self.reserved_words = params.reserved_words;
         self.moduleCaseTypes = params.moduleCaseTypes;
         self.allowUsercase = params.allowUsercase;
+        self.supportCmitfb = params.supportCmitfb;
 
         self.setPropertiesMap = function (propertiesMap) {
             self.propertiesMap = ko.mapping.fromJS(propertiesMap);
@@ -771,6 +772,9 @@ hqDefine('app_manager/js/case-config-ui-2.js', function () {
                     }),
                     case_preload: ko.computed(function () {
                         return caseConfig.caseConfigViewModel.actionType() === 'update';
+                    }),
+                    only_case_save: ko.computed(function () {
+                        return caseConfig.caseConfigViewModel.actionType() === 'update' && caseConfig.supportCmitfb;
                     }),
                     repeats: function () {
                         return false;
