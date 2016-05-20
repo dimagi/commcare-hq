7ccd18a8c457371c16cb2874288d9941ec840097
Jonathan Emord <jonathan.emord@gmail.com>
2016-05-20 14:02:24 -0400

    Add ES to util test

--- corehq/apps/reports/tests/test_util.py
+++ corehq/apps/reports/tests/test_util.py
@@ -1,13 +1,21 @@
+from django.test import TestCase
+from elasticsearch.exceptions import ConnectionError
+from mock import Mock, patch
+
 from corehq.apps.reports.util import create_export_filter
 from corehq.apps.users.models import CommCareUser
-from django.test import TestCase
-from mock import Mock
+from corehq.pillows.mappings.xform_mapping import XFORM_INDEX
+from corehq.pillows.xform import XFormPillow
+from corehq.util.elastic import ensure_index_deleted
+from corehq.util.test_utils import trap_extra_setup
 
 
 DOMAIN = 'test_domain'
 
 
 class ReportUtilTests(TestCase):
+    pillow_class = XFormPillow
+    es_index = XFORM_INDEX
 
     def setUp(self):
         self.user = CommCareUser.create(DOMAIN, 'user1', '***')
@@ -17,8 +25,16 @@ class ReportUtilTests(TestCase):
         self.request.project.commtrack_enabled = False
         self.request.couch_user = self.user.user_id
 
+        with trap_extra_setup(ConnectionError):
+            ensure_index_deleted(self.es_index)
+            self.pillow = self.get_pillow()
+
     def tearDown(self):
+        ensure_index_deleted(self.es_index)
         self.user.delete()
+
+    def get_pillow(self):
+        return self.pillow_class()
 
     def test_create_export_form_filter(self):
         filter_ = create_export_filter(self.request, DOMAIN, export_type='form')
