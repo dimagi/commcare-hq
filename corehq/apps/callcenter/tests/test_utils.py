6013ee5a1f6e81936c2cf87f658db4ad569a9b88
Simon Kelly <skelly@dimagi.com>
2016-05-24 19:47:02 +0200

    fix tests

--- corehq/apps/callcenter/tests/test_utils.py
+++ corehq/apps/callcenter/tests/test_utils.py
@@ -344,9 +344,10 @@ class CallCenterDomainTest(SimpleTestCase):
         ensure_index_deleted(self.index_info.index)
 
     def test_get_call_center_domains(self):
-        _create_domain('cc-dom1', True, True, 'flw')
-        _create_domain('cc-dom2', True, False, 'aww')
-        _create_domain('cc-dom3', False, False, '')
+        _create_domain('cc-dom1', True, True, 'flw', 'user1', False)
+        _create_domain('cc-dom2', True, False, 'aww', None, True)
+        _create_domain('cc-dom3', False, False, '', 'user2', False)  # case type missing
+        _create_domain('cc-dom3', False, False, 'flw', None, False)  # owner missing
         self.elasticsearch.indices.refresh(self.index_info.index)
 
         domains = get_call_center_domains()
@@ -359,7 +360,7 @@ class CallCenterDomainTest(SimpleTestCase):
         self.assertFalse(dom2.use_fixtures)
 
 
-def _create_domain(name, cc_enabled, cc_use_fixtures, cc_case_type):
+def _create_domain(name, cc_enabled, cc_use_fixtures, cc_case_type, cc_case_owner_id, use_location_as_owner):
     with drop_connected_signals(commcare_domain_post_save):
         domain = Domain(
             _id=uuid.uuid4().hex,
@@ -370,6 +371,8 @@ def _create_domain(name, cc_enabled, cc_use_fixtures, cc_case_type):
         domain.call_center_config.enabled = cc_enabled
         domain.call_center_config.use_fixtures = cc_use_fixtures
         domain.call_center_config.case_type = cc_case_type
+        domain.call_center_config.case_owner_id = cc_case_owner_id
+        domain.call_center_config.use_user_location_as_owner = use_location_as_owner
 
         send_to_elasticsearch(
             index='domains',
