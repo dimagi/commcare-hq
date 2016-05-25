267427206000c0bc32b723019e3eaeb55bf2235a
Ben Rudolph <brudolph@dimagi.com>
2016-05-25 16:31:27 -0400

    Add (and cleanup) web user digest tests

--- corehq/apps/ota/tests/test_digest_restore.py
+++ corehq/apps/ota/tests/test_digest_restore.py
@@ -1,11 +1,12 @@
 import time
 from python_digest import build_authorization_request, calculate_nonce
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
-from corehq.apps.domain.models import Domain
+from corehq.apps.domain.tests.test_utils import delete_all_domains
 from corehq.apps.domain.shortcuts import create_domain
-from corehq.apps.users.models import CommCareUser
+from corehq.apps.users.models import CommCareUser, WebUser
 
 
 class DigestOtaRestoreTest(TestCase):
@@ -13,37 +14,68 @@ class DigestOtaRestoreTest(TestCase):
     Integration test for django_digest based ota restore is tested
     """
     domain = "test"
+    wrong_domain = 'test-wrong-domain'
     username = "testota"
-    first_name = "ota"
-    last_name = "restorer"
+    web_username = 'test-webuser'
     password = "123"
 
     def setUp(self):
         create_domain(self.domain)
-        self.couch_user = CommCareUser.create(self.domain, self.username, self.password)
-        self.couch_user.first_name = self.first_name
-        self.couch_user.last_name = self.last_name
-        self.couch_user.save()
+        create_domain(self.wrong_domain)
+        self.commcare_user = CommCareUser.create(self.domain, self.username, self.password)
+        self.web_user = WebUser.create(self.domain, self.web_username, self.password)
 
     def tearDown(self):
-        self.couch_user.delete()
-        domain = Domain.get_by_name(self.domain)
-        domain.delete()
+        self.commcare_user.delete()
+        self.web_user.delete()
+        delete_all_domains()
 
-    def testOtaRestore(self, password=None):
-        uri = '/a/%s/phone/restore/' % self.domain
-        self.client.defaults['HTTP_AUTHORIZATION'] = build_authorization_request(
-            self.couch_user.username,
-            'GET',
+    def test_commcare_user_restore(self):
+        uri = reverse('ota_restore', args=[self.domain])
+        client = Client(HTTP_AUTHORIZATION=_get_http_auth_header(
+            self.commcare_user.username,
+            self.password,
             uri,
-            3,
-            nonce=calculate_nonce(time.time(), settings.SECRET_KEY),
-            realm='DJANGO',
-            opaque='myopaque',
-            password=self.password,
-        )
+        ))
 
-        resp = self.client.get(uri, follow=True)
+        resp = client.get(uri, follow=True)
         self.assertEqual(resp.status_code, 200)
         content = list(resp.streaming_content)[0]
         self.assertTrue("Successfully restored account {}!".format(self.username) in content)
+
+    def test_web_user_restore(self):
+        uri = reverse('ota_restore', args=[self.domain])
+        client = Client(HTTP_AUTHORIZATION=_get_http_auth_header(
+            self.web_user.username,
+            self.password,
+            uri,
+        ))
+
+        resp = client.get(uri, follow=True)
+        self.assertEqual(resp.status_code, 200)
+        content = list(resp.streaming_content)[0]
+        self.assertTrue("Successfully restored account {}!".format(self.web_username) in content)
+
+    def test_wrong_domain_web_user(self):
+        uri = reverse('ota_restore', args=[self.wrong_domain])
+        client = Client(HTTP_AUTHORIZATION=_get_http_auth_header(
+            self.web_user.username,
+            self.password,
+            uri,
+        ))
+
+        resp = client.get(uri, follow=True)
+        self.assertEqual(resp.status_code, 401)
+
+
+def _get_http_auth_header(username, password, uri):
+    return build_authorization_request(
+        username,
+        'GET',
+        uri,
+        3,
+        nonce=calculate_nonce(time.time(), settings.SECRET_KEY),
+        realm='DJANGO',
+        opaque='myopaque',
+        password=password,
+    )
