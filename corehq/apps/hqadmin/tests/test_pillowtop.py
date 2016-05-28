eee9004429615e210b5cdc86dc2cac01124dc683
Cory Zue <czue@dimagi.com>
2016-05-28 12:19:13 +0200

    add tests for exclusion by class and name

--- corehq/apps/hqadmin/tests/test_pillowtop.py
+++ corehq/apps/hqadmin/tests/test_pillowtop.py
@@ -54,6 +54,25 @@ class TestPillowTopFiltering(SimpleTestCase):
         configs_back = apply_pillow_actions_to_pillows([{'exclude_groups': ['phonelog']}], self.pillowtops)
         self.assertEqual(expected_pillows, set([c.name for c in configs_back]))
 
+    def test_with_exclusion_by_name(self):
+        expected_pillows = {'CasePillow',
+                            'XFormPillow',
+                            'PhoneLogPillow',
+                            }
+
+        configs_back = apply_pillow_actions_to_pillows([{'exclude_pillows': ['FakeConstructedPillowName']}], self.pillowtops)
+        self.assertEqual(expected_pillows, set([c.name for c in configs_back]))
+
+    def test_with_exclusion_by_class(self):
+        expected_pillows = {'CasePillow',
+                            'XFormPillow',
+                            'PhoneLogPillow',
+                            }
+
+        configs_back = apply_pillow_actions_to_pillows(
+            [{'exclude_pillows': ['pillowtop.tests.FakeConstructedPillow']}], self.pillowtops)
+        self.assertEqual(expected_pillows, set([c.name for c in configs_back]))
+
     def test_loading_existing_conf_file(self):
         config = {'include_groups': ['mvp_indicators']}
 
