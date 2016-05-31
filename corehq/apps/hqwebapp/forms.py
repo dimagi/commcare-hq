3b57990e17921b911792c9563df1703f8edb9ad6
Biyeun Buczyk <biyeun@dimagi.com>
2016-05-31 14:12:23 -0400

    use b3 version of template by default

--- corehq/apps/hqwebapp/forms.py
+++ corehq/apps/hqwebapp/forms.py
@@ -150,7 +150,7 @@ class FormListForm(object):
     columns = None  # list configuring the columns to display
 
     child_form_data = forms.CharField(widget=forms.HiddenInput)
-    template = "style/bootstrap2/partials/form_list_form.html"
+    template = "style/bootstrap3/partials/form_list_form.html"
 
     def __init__(self, data=None, request=None, *args, **kwargs):
         self.request = request
