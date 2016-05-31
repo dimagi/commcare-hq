454986af149f7c84709e7df8edb0826196aeaac6
Cory Zue <czue@dimagi.com>
2016-05-31 18:41:31 +0200

    delete FormData and tests

--- corehq/apps/sofabed/tests/__init__.py
+++ corehq/apps/sofabed/tests/__init__.py
@@ -1,6 +1,5 @@
 import logging
 try:
-    from .test_formdata import *
     from .test_casedata import *
 except ImportError, e:
     # for some reason the test harness squashes these so log them here for clarity
