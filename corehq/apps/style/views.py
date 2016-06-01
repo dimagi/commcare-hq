e69d7f98cdbfc471d36a0f4524a669c04eaf04d8
Biyeun Buczyk <biyeun@dimagi.com>
2016-06-01 17:35:21 -0400

    remove BaseB3SectionPageView and replace with BaseSectionPageView

--- corehq/apps/style/views.py
+++
@@ -1,7 +0,0 @@
-from corehq.apps.hqwebapp.views import BaseSectionPageView
-
-
-class BaseB3SectionPageView(BaseSectionPageView):
-    """Subclass of BaseSectionPageView to immediately support Bootstrap3.
-    """
-    template_name = "style/base_section.html"
