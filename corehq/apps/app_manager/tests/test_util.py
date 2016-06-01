f7fd72c2ff389836a48b61d6da56d3d105831dec
Simon Kelly <skelly@dimagi.com>
2016-06-01 15:26:49 +0200

    switch to case factory

--- corehq/apps/app_manager/tests/test_util.py
+++ corehq/apps/app_manager/tests/test_util.py
@@ -94,9 +94,11 @@ class GetCasePropertiesTest(SimpleTestCase, TestXmlMixin):
 @patch('corehq.apps.app_manager.models.is_usercase_in_use', MagicMock(return_value=False))
 @patch('corehq.apps.app_manager.util.get_per_type_defaults', MagicMock(return_value={}))
 class SchemaTest(SimpleTestCase):
+    def setUp(self):
+        self.factory = AppFactory()
+
     def test_get_casedb_schema_empty_app(self):
-        app = self.make_app()
-        schema = util.get_casedb_schema(app)
+        schema = util.get_casedb_schema(self.factory.app)
         self.assert_has_kv_pairs(schema, {
             "id": "casedb",
             "uri": "jr://instance/casedb",
@@ -107,9 +109,8 @@ class SchemaTest(SimpleTestCase):
         })
 
     def test_get_casedb_schema_with_form(self):
-        app = self.make_app()
-        self.add_form(app, "village")
-        schema = util.get_casedb_schema(app)
+        self.add_form("village")
+        schema = util.get_casedb_schema(self.factory.app)
         self.assertEqual(len(schema["subsets"]), 1, schema["subsets"])
         self.assert_has_kv_pairs(schema["subsets"][0], {
             'id': 'village',
@@ -119,21 +120,16 @@ class SchemaTest(SimpleTestCase):
         })
 
     def test_get_casedb_schema_with_related_case_types(self):
-        app = self.make_app()
-        self.add_form(app, "family")
-        village = self.add_form(app, "village")
-        village.actions.subcases.append(OpenSubCaseAction(
-            case_type='family',
-            reference_id='parent'
-        ))
-        schema = util.get_casedb_schema(app)
+        self.add_form("family")
+        village = self.add_form("village")
+        self.factory.form_opens_case(village, case_type='family', is_subcase=True)
+        schema = util.get_casedb_schema(self.factory.app)
         subsets = {s["id"]: s for s in schema["subsets"]}
         self.assertEqual(subsets["village"]["related"], None)
         self.assertDictEqual(subsets["family"]["related"], {"parent": "village"})
 
     def test_get_session_schema_for_module_with_no_case_type(self):
-        app = self.make_app()
-        form = self.add_form(app)
+        form = self.add_form()
         schema = util.get_session_schema(form)
         self.assert_has_kv_pairs(schema, {
             "id": "commcaresession",
@@ -144,8 +140,7 @@ class SchemaTest(SimpleTestCase):
         assert "case_id" not in schema["structure"], schema["structure"]
 
     def test_get_session_schema_for_simple_module_with_case(self):
-        app = self.make_app()
-        form = self.add_form(app, "village")
+        form = self.add_form("village")
         schema = util.get_session_schema(form)
         self.assertDictEqual(schema["structure"]["case_id_new_village_0"], {
             "reference": {
@@ -166,19 +161,9 @@ class SchemaTest(SimpleTestCase):
         for key, value in expected_dict.items():
             self.assertEqual(test_dict[key], value)
 
-    def add_form(self, app, case_type=None, module_id=None):
-        if module_id is None:
-            module_id = len(app.modules)
-            m = app.add_module(Module.new_module('Module{}'.format(module_id), lang='en'))
-            if case_type:
-                m.case_type = case_type
-        form = app.new_form(module_id, 'form {}'.format(case_type), lang='en')
+    def add_form(self, case_type=None):
+        module_id = len(self.factory.app.modules)
+        module, form = self.factory.new_basic_module(module_id, case_type)
         if case_type:
-            form.actions.open_case = OpenCaseAction(name_path="/data/question1", external_id=None)
-            form.actions.open_case.condition.type = 'always'
+            self.factory.form_opens_case(form, case_type)
         return form
-
-    def make_app(self):
-        app = Application.new_app('domain', 'New App', APP_V2)
-        app.version = 1
-        return app
