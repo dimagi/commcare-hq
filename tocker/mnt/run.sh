ba470c103ff9b55488b955fefe2eb03d73155158
Daniel Miller <dmiller@dimagi.com>
2016-06-02 13:22:33 -0400

    Incomplete, first pass at untangling docker setup

---
+++ tocker/mnt/run.sh
@@ -0,0 +1,84 @@
+#! /bin/bash
+set -e
+
+if [ -z "$1" ]; then
+    # the main container need not stay running for services
+    exit 0
+fi
+
+function test_setup() {
+    [ -n "$1" ] && TEST="$1"
+
+    if [ "$TEST" = "python" -o "$TEST" = "python-sharded" ]; then
+
+        if [ -f python_env/bin/activate ]; then
+            source python_env/bin/activate
+        else
+            virtualenv ./python_env
+        fi
+
+        scripts/uninstall-requirements.sh
+        pip install \
+            -r requirements/requirements.txt \
+            -r requirements/dev-requirements.txt \
+            coveralls
+        rm -rf /root/.cache/pip  # skip Running setup.py bdist_wheel for ...
+
+        /usr/lib/jvm/jdk1.7.0/bin/keytool -genkey \
+            -keyalg RSA \
+            -keysize 2048 \
+            -validity 10000 \
+            -alias javarosakey \
+            -keypass onetwothreefourfive \
+            -keystore InsecureTestingKeyStore \
+            -storepass onetwothreefourfive \
+            -dname 'CN=Foo, OU=Bar, O=Bizzle, L=Bazzle, ST=Bingle, C=US'
+    fi
+
+    if [ "$TEST" = "javascript" -o "$JS" = "yes" ]; then
+        npm install
+        bower install
+    fi
+
+    /mnt/wait.sh
+}
+
+function run_tests() {
+    TEST="$1"
+    if [ "$TEST" != "javascript" -a "$TEST" != "python" -a "$TEST" != "python-sharded" ]; then
+        echo "Unknown test suite: $TEST"
+        exit 1
+    fi
+    shift
+    test_setup $TEST
+
+    rm localsettings.py  # remove first in case it's a dangling symlink
+    cp .travis/localsettings.py localsettings.py
+    if [ "$TEST" == "python-sharded" ]; then
+        export USE_PARTITIONED_DATABASE=yes
+        TESTS=" \
+            corehq.form_processor \
+            corehq.sql_db \
+            couchforms \
+            casexml.apps.case \
+            casexml.apps.phone \
+            corehq.apps.receiverwrapper"
+    else
+        TESTS=""
+    fi
+
+    ./manage.py test "$@" $TESTS
+}
+
+# commcare-hq source overlay prevents modifications in this container
+# from leaking to the host; allows safe overwrite of localsettings.py
+rm -rf /mnt/lib/overlay  # clear source overlay
+mkdir -p commcare-hq lib/overlay lib/python_env lib/node_modules
+mount -t aufs -o br=lib/overlay:commcare-hq-ro none /mnt/commcare-hq
+
+cd commcare-hq
+ln -sf /mnt/lib/node_modules node_modules
+ln -sf /mnt/lib/python_env python_env
+
+echo "docker command: $@"
+"$@"
