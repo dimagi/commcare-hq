85a18abaeba1271db414fcae6a76d46a4936f697
Daniel Roberts <droberts@dimagi.com>
2016-06-07 21:36:53 -0400

    indent export/js/utils.js

--- corehq/apps/export/static/export/js/utils.js
+++ corehq/apps/export/static/export/js/utils.js
@@ -1,29 +1,27 @@
 hqDefine('export/js/utils.js', function () {
+    var getTagCSSClass = function(tag) {
+        var constants = hqImport('export/js/const.js');
+        var cls = 'label';
+        if (tag === constants.TAG_DELETED) {
+            return cls + ' label-warning';
+        } else {
+            return cls + ' label-default';
+        }
+    };
 
-var getTagCSSClass = function(tag) {
-    var constants = hqImport('export/js/const.js');
-    var cls = 'label';
-    if (tag === constants.TAG_DELETED) {
-        return cls + ' label-warning';
-    } else {
-        return cls + ' label-default';
-    }
-};
+    var redirect = function(url) {
+        window.location.href = url;
+    };
 
-var redirect = function(url) {
-    window.location.href = url;
-};
+    var animateToEl = function(toElementSelector, callback) {
+        $('html, body').animate({
+            scrollTop: $(toElementSelector).offset().top + 'px'
+        }, 'slow', undefined, callback);
+    };
 
-var animateToEl = function(toElementSelector, callback) {
-    $('html, body').animate({
-        scrollTop: $(toElementSelector).offset().top + 'px'
-    }, 'slow', undefined, callback);
-};
-
-return {
-    getTagCSSClass: getTagCSSClass,
-    redirect: redirect,
-    animateToEl: animateToEl,
-};
-
+    return {
+        getTagCSSClass: getTagCSSClass,
+        redirect: redirect,
+        animateToEl: animateToEl,
+    };
 });
