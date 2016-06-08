5177fe2a321604350e3dbba7260557e4884783bf
Daniel Miller <dmiller@dimagi.com>
2016-06-08 17:53:59 -0400

    Improve docker wait.sh

--- docker/wait.sh
+++ docker/wait.sh
@@ -2,34 +2,36 @@
 
 set -e
 
-SERVICES="REDIS COUCH POSTGRES"
 if [ -n "$1" ]; then
     SERVICES="$1"
 else [ -n "DEPENDENT_SERVICES" ]
     SERVICES="$DEPENDENT_SERVICES"
 fi
+if [ -z "$SERVICES" ]; then
+    exit 0
+fi
 
-echo "Services: $SERVICES"
+echo "Waiting for services: $SERVICES"
 
 for service in $SERVICES; do
     while [ -z "$host" ]; do
-        host=$(env | grep $service | grep _TCP_ADDR | cut -d = -f 2 | head -n1)
-        port=$(env | grep $service | grep _TCP_PORT | cut -d = -f 2 | head -n1)
+        svc=$(echo $service | tr '[:lower:]' '[:upper:]')
+        host=$(env | grep -E "${svc}_PORT_[0-9]+_TCP_ADDR" | cut -d = -f 2 | head -n1)
+        port=$(env | grep -E "${svc}_PORT_[0-9]+_TCP_PORT" | cut -d = -f 2 | head -n1)
 
         [ -z "$host" ] && echo "Waiting for link to $service" && sleep 1
     done
 
     echo -n "Waiting for TCP connection to $service @ $host:$port..."
 
-    while ! exec 6<>/dev/tcp/${host}/${port}
+    while ! { exec 6<>/dev/tcp/${host}/${port}; } 2>/dev/null
     do
       echo -n .
       sleep 1
     done
 
     echo "$service ok"
-    #reset host
     host=
 done
 
-echo "ALL SERVICES READY"
+echo "Services ready: $SERVICES"
