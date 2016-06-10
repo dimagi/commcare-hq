570cad02e6d505d741c908f1ecc8a17831a56cc6
Nick Pellegrino <npellegrino@dimagi.com>
2016-06-10 17:03:15 -0400

    also filter out None in get_autopay_card

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -2872,9 +2872,11 @@ class StripePaymentMethod(PaymentMethod):
         return self.customer.cards.retrieve(card_token)
 
     def get_autopay_card(self, billing_account):
-        return next((card for card in self.all_cards
-                     if card.metadata.get(self._auto_pay_card_metadata_key(billing_account)) == 'True'),
-                    None)
+        return next((
+            card for card in self.all_cards
+            if card is not None
+            and card.metadata.get(self._auto_pay_card_metadata_key(billing_account)) == 'True'
+        ), None)
 
     def remove_card(self, card_token):
         card = self.get_card(card_token)
