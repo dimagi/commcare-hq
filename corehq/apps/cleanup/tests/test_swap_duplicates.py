5db9a0e7e95e25862b38040281b3e1633b88286c
NoahCarnahan <ncarnahan@dimagi.com>
2016-06-13 15:31:08 -0400

    cleanup imports

--- corehq/apps/cleanup/tests/test_swap_duplicates.py
+++ corehq/apps/cleanup/tests/test_swap_duplicates.py
@@ -1,26 +1,13 @@
-from mock import MagicMock
 import os
 import uuid
 
 from django.core.management import call_command
 from django.test import TestCase
-from elasticsearch import ConnectionError
 from testil import tempdir
 
-from corehq.apps.app_manager.const import APP_V2
-from corehq.apps.app_manager.models import Application, Module
 from corehq.apps.app_manager.tests import TestXmlMixin
-from corehq.apps.app_manager.util import get_correct_app_class
-from corehq.apps.cleanup.management.commands.fix_forms_with_missing_xmlns import (
-    generate_random_xmlns,
-    set_xmlns_on_form,
-)
 from corehq.apps.receiverwrapper import submit_form_locally
-from corehq.pillows.xform import XFormPillow
-from corehq.util.elastic import ensure_index_deleted
-from corehq.util.test_utils import trap_extra_setup
 from couchforms.models import XFormInstance
-from pillowtop.es_utils import completely_initialize_pillow_index
 
 DOMAIN = "test"
 
@@ -40,8 +27,8 @@ class TestFixFormsWithMissingXmlns(TestCase, TestXmlMixin):
         dup_id = self._submit_form(form_id)._id
         # Test a form with multiple dups
         form_with_multi_dups_id = self._submit_form()._id
-        dup_2_id = self._submit_form(form_with_multi_dups_id)._id
-        dup_3_id = self._submit_form(form_with_multi_dups_id)._id
+        _ = self._submit_form(form_with_multi_dups_id)._id
+        _ = self._submit_form(form_with_multi_dups_id)._id
         # Test a form with no dups
         another_form_id = self._submit_form()._id
 
