384e186d4d40f6279bb2b32e0e5b99b3c5f7e309
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2016-06-15 11:24:44 +0200

    Refactor Block reports

--- settings.py
+++ settings.py
@@ -1742,6 +1742,7 @@ DOMAIN_MODULE_MAP = {
 
     'ipm-senegal': 'custom.intrahealth',
     'icds-test': 'custom.icds_reports',
+    'icds-cas': 'custom.icds_reports',
     'testing-ipm-senegal': 'custom.intrahealth',
     'up-nrhm': 'custom.up_nrhm',
 
