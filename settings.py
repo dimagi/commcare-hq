05f6e3a3365a546e7d7ccadaf38c017f4df52c89
Ben Rudolph <brudolph@dimagi.com>
2016-06-15 14:23:03 -0400

    app form tracker pillow

--- settings.py
+++ settings.py
@@ -1461,6 +1461,11 @@ PILLOWTOPS = {
             'class': 'pillowtop.pillow.interface.ConstructedPillow',
             'instance': 'corehq.pillows.domain.get_domain_kafka_to_elasticsearch_pillow',
         },
+        {
+            'name': 'AppFormSubmissionTrackerPillow',
+            'class': 'pillowtop.pillow.interface.ConstructedPillow',
+            'instance': 'corehq.pillows.xform.get_app_form_submission_tracker_pillow',
+        },
     ],
     'core_ext': [
         'corehq.pillows.reportcase.ReportCasePillow',
