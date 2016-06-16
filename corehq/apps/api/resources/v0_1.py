ac8335d21724b4c7e0acaba19da1f88bd1aa4039
Sravan Reddy <sravfeyn@gmail.com>
2016-06-16 16:48:55 +0530

    comment

--- corehq/apps/api/resources/v0_1.py
+++ corehq/apps/api/resources/v0_1.py
@@ -72,6 +72,10 @@ def api_auth(view_func):
 class LoginAndDomainAuthentication(Authentication):
 
     def __init__(self, allow_internal=False, *args, **kwargs):
+        """
+        allow_internal:
+            set this to True to allow session based access to this resource
+        """
         super(LoginAndDomainAuthentication, self).__init__(*args, **kwargs)
         if allow_internal:
             self.decorator_map = {
