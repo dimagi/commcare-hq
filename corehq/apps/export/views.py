8e02545e091f00108ab04f0bc067851c961ade42
Ben Rudolph <brudolph@dimagi.com>
2016-06-17 12:09:40 -0400

    do not force rebuild on export creation

--- corehq/apps/export/views.py
+++ corehq/apps/export/views.py
@@ -1463,7 +1463,6 @@ class CreateNewCustomFormExportView(BaseModifyNewCustomView):
             self.domain,
             app_id,
             xmlns,
-            force_rebuild=True,
         )
         self.export_instance = self.export_instance_cls.generate_instance_from_schema(schema)
 
@@ -1481,7 +1480,6 @@ class CreateNewCustomCaseExportView(BaseModifyNewCustomView):
         schema = CaseExportDataSchema.generate_schema_from_builds(
             self.domain,
             case_type,
-            force_rebuild=True,
         )
         self.export_instance = self.export_instance_cls.generate_instance_from_schema(schema)
 
