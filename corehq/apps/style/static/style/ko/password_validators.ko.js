00d53e3bb8e281f9f37f2ceaa3ca33feb4e7026a
Biyeun Buczyk <biyeun@dimagi.com>
2016-06-21 01:42:41 -0400

    add password ko validator

---
+++ corehq/apps/style/static/style/ko/password_validators.ko.js
@@ -0,0 +1,15 @@
+/* global ko */
+/* global django */
+/* global zxcvbn */
+
+ko.validation.rules['zxcvbnPassword'] = {
+    validator: function (val, minScore, penalizedWords) {
+        if (val === undefined || val.length == 0) return true;  // do separate validation for required
+        penalizedWords = penalizedWords || ['dimagi', 'commcare', 'hq', 'commcarehq'];
+        var score = zxcvbn(val, penalizedWords).score;
+        return score >= minScore;
+    },
+    message: django.gettext("Your password is too weak! Try adding numbers or symbols.")
+};
+
+ko.validation.registerExtenders();
