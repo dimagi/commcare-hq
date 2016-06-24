648bb8ea7d33023bb4b3d5a30cfd2e91e37913d0
Simon Kelly <skelly@dimagi.com>
2016-06-24 16:05:55 +0200

    allow missing domain objects in tests

--- corehq/form_processor/utils/general.py
+++ corehq/form_processor/utils/general.py
@@ -6,11 +6,7 @@ from corehq.toggles import USE_SQL_BACKEND, NAMESPACE_DOMAIN
 def should_use_sql_backend(domain_name):
     from corehq.apps.domain.models import Domain
     if settings.UNIT_TESTING:
-        override = getattr(settings, 'TESTS_SHOULD_USE_SQL_BACKEND', None)
-        if override is not None:
-            return override
-        elif not getattr(settings, 'DB_ENABLED', True):
-            return False
+        return _should_use_sql_backend_in_tests(domain_name)
 
     toggle_enabled = USE_SQL_BACKEND.enabled(domain_name)
     if toggle_enabled:
@@ -22,6 +18,19 @@ def should_use_sql_backend(domain_name):
         return True
 
     return toggle_enabled or Domain.get_by_name(domain_name).use_sql_backend
+
+
+def _should_use_sql_backend_in_tests(domain_name):
+    assert settings.UNIT_TESTING
+    from corehq.apps.domain.models import Domain
+    override = getattr(settings, 'TESTS_SHOULD_USE_SQL_BACKEND', None)
+    if override is not None:
+        return override
+    elif getattr(settings, 'DB_ENABLED', True):
+        domain = Domain.get_by_name(domain_name)
+        return domain and domain.use_sql_backend
+    else:
+        return False
 
 
 def is_commcarecase(obj):
