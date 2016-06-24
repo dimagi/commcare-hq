dd5107544d10c1663f68ce24be48ad0279fd0f35
Simon Kelly <skelly@dimagi.com>
2016-06-24 12:59:07 +0200

    migrate toggle domains

--- corehq/form_processor/utils/general.py
+++ corehq/form_processor/utils/general.py
@@ -1,16 +1,25 @@
 from django.conf import settings
 
 from corehq.apps.domain.models import Domain
-from corehq.toggles import USE_SQL_BACKEND
+from corehq.toggles import USE_SQL_BACKEND, NAMESPACE_DOMAIN
 
 
-def should_use_sql_backend(domain):
+def should_use_sql_backend(domain_name):
     if settings.UNIT_TESTING:
         override = getattr(settings, 'TESTS_SHOULD_USE_SQL_BACKEND', None)
         if override is not None:
             return override
 
-    return USE_SQL_BACKEND.enabled(domain) or Domain.get_by_name(domain).use_sql_backend
+    toggle_enabled = USE_SQL_BACKEND.enabled(domain_name)
+    if toggle_enabled:
+        domain = Domain.get_by_name(domain_name)
+        if not domain.use_sql_backend:
+            domain.use_sql_backend = True
+            domain.save()
+            USE_SQL_BACKEND.set(domain_name, enabled=False, namespace=NAMESPACE_DOMAIN)
+        return True
+
+    return toggle_enabled or Domain.get_by_name(domain_name).use_sql_backend
 
 
 def is_commcarecase(obj):
