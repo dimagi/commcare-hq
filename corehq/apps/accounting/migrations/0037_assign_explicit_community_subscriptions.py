59209843d68ef415042320932f5a09248845854b
Nick Pellegrino <npellegrino@dimagi.com>
2016-06-27 15:25:21 -0400

    separate migration so only first half can be run before deploy

--- corehq/apps/accounting/migrations/0037_assign_explicit_community_subscriptions.py
+++ corehq/apps/accounting/migrations/0037_assign_explicit_community_subscriptions.py
@@ -7,14 +7,6 @@ from django.db import migrations
 
 from dimagi.utils.couch.database import iter_docs
 
-from corehq.apps.accounting.bootstrap.config.new_user_numbers import (
-    BOOTSTRAP_EDITION_TO_ROLE,
-    BOOTSTRAP_FEATURE_RATES,
-    BOOTSTRAP_PRODUCT_RATES,
-    FEATURE_TYPES,
-    PRODUCT_TYPES,
-)
-from corehq.apps.accounting.bootstrap.utils import ensure_plans
 from corehq.apps.accounting.tasks import ensure_explicit_community_subscription
 from corehq.apps.accounting.utils import log_accounting_error
 from corehq.apps.domain.models import Domain
@@ -38,17 +30,6 @@ def _assign_explicit_community_subscriptions(apps, schema_editor):
             )
 
 
-def _bootstrap_new_community_plan_versions(apps, schema_editor):
-    ensure_plans(
-        edition_to_role=BOOTSTRAP_EDITION_TO_ROLE,
-        edition_to_product_rate=BOOTSTRAP_PRODUCT_RATES,
-        edition_to_feature_rate=BOOTSTRAP_FEATURE_RATES,
-        feature_types=FEATURE_TYPES,
-        product_types=PRODUCT_TYPES,
-        dry_run=False, verbose=True, apps=apps,
-    )
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -57,5 +38,5 @@ class Migration(migrations.Migration):
 
     operations = [
         HqRunPython(_assign_explicit_community_subscriptions),
-        HqRunPython(_bootstrap_new_community_plan_versions),
+
     ]
