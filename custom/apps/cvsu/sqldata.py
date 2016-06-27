c7a4e4146a43e65b96b54d7ae77189c93648361a
Simon Kelly <skelly@dimagi.com>
2016-06-27 16:07:12 +0200

    remove unused function

--- custom/apps/cvsu/sqldata.py
+++ custom/apps/cvsu/sqldata.py
@@ -5,7 +5,7 @@ from sqlagg.filters import BETWEEN
 from corehq.apps.groups.models import Group
 from corehq.apps.reports import util
 from corehq.apps.reports.standard import CommCareUserMemoizer
-from corehq.apps.reports.util import format_datatables_data, make_ctable_table_name
+from corehq.apps.reports.util import format_datatables_data
 from corehq.apps.userreports.sql import get_table_name
 from corehq.sql_db.connections import UCR_ENGINE_ID
 from custom.apps.cvsu.mixins import CVSUSqlDataMixin, FilterMixin, DateColumnMixin, combine_month_year, format_date, \
