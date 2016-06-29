f909119b697503d87769cadf5146b6b0c6f0ca41
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-06-29 18:00:57 -0400

    Add additional fields needed for reporting

--- custom/zipline/models.py
+++ custom/zipline/models.py
@@ -22,7 +22,15 @@ class EmergencyOrder(models.Model):
 
     # A list of {'code': <code>, 'quantity': <quantity>} dictionaries
     # representing the product code and quantity of the products being ordered
-    product_info = jsonfield.JSONField(default=list)
+    products_requested = jsonfield.JSONField(default=list)
+
+    # Same format as products_requested; represents products and quantities delivered
+    # according to the Zipline delivered status update(s)
+    products_delivered = jsonfield.JSONField(default=list)
+
+    # Same format as products_requested; represents products and quantities reported
+    # to have been received by the facility
+    products_confirmed = jsonfield.JSONField(default=list)
 
     # The timestamp in CommCareHQ that the order was created
     timestamp = models.DateTimeField()
@@ -30,23 +38,59 @@ class EmergencyOrder(models.Model):
     # The total number of vehicles that will be used to fulfill the request
     total_vehicles = models.IntegerField(null=True)
 
+    # The total number of attempts made while sending this emergency order
+    # to Zipline
+    zipline_request_attempts = models.IntegerField(default=0)
+
+    # One of the STATUS_* constants from EmergencyOrderStatusUpdate
+    status = models.CharField(max_length=126, default=EmergencyOrderStatusUpdate.STATUS_PENDING)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the received status update
+    received_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the rejected status update
+    rejected_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the approved status update
+    approved_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the canceled status update
+    canceled_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the dispatched status
+    # update for the first vehicle
+    dispatched_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
+    # A pointer to the EmergencyOrderStatusUpdate record representing the delivered status
+    # update for the last vehicle
+    delivered_status = models.ForeignKey('EmergencyOrderStatusUpdate', on_delete=models.PROTECT)
+
 
 class EmergencyOrderStatusUpdate(models.Model):
+    STATUS_PENDING = 'PENDING'
     STATUS_RECEIVED = 'RECEIVED'
     STATUS_REJECTED = 'REJECTED'
     STATUS_APPROVED = 'APPROVED'
     STATUS_CANCELED = 'CANCELED'
     STATUS_DISPATCHED = 'DISPATCHED'
     STATUS_DELIVERED = 'DELIVERED'
+    STATUS_ERROR = 'ERROR'
 
     STATUS_CHOICES = (
+        (STATUS_PENDING, ugettext_lazy('Pending')),
         (STATUS_RECEIVED, ugettext_lazy('Received')),
         (STATUS_REJECTED, ugettext_lazy('Rejected')),
         (STATUS_APPROVED, ugettext_lazy('Approved')),
         (STATUS_CANCELED, ugettext_lazy('Canceled')),
         (STATUS_DISPATCHED, ugettext_lazy('Dispatched')),
         (STATUS_DELIVERED, ugettext_lazy('Delivered')),
+        (STATUS_ERROR, ugettext_lazy('Error')),
     )
+
+    class Meta:
+        index_together = [
+            ['order_id', 'vehicle_number'],
+        ]
 
     order = models.ForeignKey('EmergencyOrder')
 
@@ -58,7 +102,12 @@ class EmergencyOrderStatusUpdate(models.Model):
     # STATUS_RECEIVED and STATUS_REJECTED
     zipline_timestamp = models.DateTimeField(null=True)
 
-    status = models.CharField(max_length=126, choices=STATUS_CHOICES)
+    # One of the STATUS_* constants above
+    status = models.CharField(max_length=126)
+
+    # Optionally, some additional information about the status, such as an
+    # error code or error message
+    additional_text = models.TextField(null=True)
 
     # The vehicle number that this status update applies to, ranging from 1 to
     # order.total_vehicles; this only applies to statuses that are received on
@@ -69,8 +118,14 @@ class EmergencyOrderStatusUpdate(models.Model):
     # vehicle_number also applies
     vehicle_id = models.CharField(max_length=126, null=True)
 
+    # If status == STATUS_DISPATCHED, this should be the products and quantities
+    # this vehicle is carrying for this order; the format of this field matches
+    # the format of EmergencyOrder.products_requested
+    products = jsonfield.JSONField(default=list)
+
     @classmethod
-    def create_for_order(cls, order_id, status, zipline_timestamp=None, vehicle_number=None, vehicle_id=None):
+    def create_for_order(cls, order_id, status, zipline_timestamp=None,
+            vehicle_number=None, vehicle_id=None, additional_text=None):
         """
         Creates an EmergencyOrderStatusUpdate record for the given order.
         :param order_id: the id of the EmergencyOrder
@@ -79,11 +134,12 @@ class EmergencyOrderStatusUpdate(models.Model):
         :param vehicle_number: the value for vehicle_number
         :param vehicle_id: the value for vehicle_id
         """
-        cls.objects.create(
+        return cls.objects.create(
             order_id=order_id,
             timestamp=datetime.utcnow(),
             zipline_timestamp=zipline_timestamp,
             status=status,
             vehicle_number=vehicle_number,
-            vehicle_id=vehicle_id
+            vehicle_id=vehicle_id,
+            additional_text=additional_text
         )
--- custom/zipline/tasks.py
+++ custom/zipline/tasks.py
@@ -5,6 +5,7 @@ from collections import namedtuple
 from custom.zipline.models import EmergencyOrder, EmergencyOrderStatusUpdate
 from datetime import datetime
 from django.conf import settings
+from dimagi.utils.couch import CriticalSection
 from dimagi.utils.logging import notify_exception
 from dimagi.utils.parsing import json_format_datetime
 from requests.auth import HTTPBasicAuth
@@ -50,27 +51,43 @@ def initiate_emergency_order(domain, user, phone_number, location, products):
         requesting_phone_number=phone_number,
         location=location,
         location_code=location.site_code,
-        product_info=[{'code': p.code, 'quantity': p.quantity} for p in products],
+        products_requested=[{'code': p.code, 'quantity': p.quantity} for p in products],
         timestamp=datetime.utcnow()
     )
 
-    send_emergency_order_request(order)
+    send_emergency_order_request(order.pk)
 
 
 @task(ignore_result=True)
-def send_emergency_order_request(order, attempt=1):
+def send_emergency_order_request(order_id, attempt=1):
     try:
-        _send_emergency_order_request(order, attempt)
-    except:
+        with CriticalSection(
+            [get_order_update_critical_section_key(order_id)],
+            timeout=(REQUEST_TIMEOUT + 10)
+        ):
+            order = EmergencyOrder.objects.get(pk=order_id)
+            order.status = _send_emergency_order_request(order, attempt)
+            order.save()
+    except Exception as e:
         notify_exception(
             None,
             message='[ZIPLINE] Error while sending order',
             details={
-                'order_id': order.pk,
+                'order_id': order_id,
                 'attempt': attempt,
             }
         )
+        create_error_record(order, 'Internal error: '.format(str(e)))
         handle_emergency_order_request_retry(order, attempt)
+
+
+def get_order_update_critical_section_key(order_id):
+    """
+    :param order_id: the pk of the order
+    :return: the key to be used with CriticalSection for preventing multiple
+    threads from updating the same order at the same time
+    """
+    return 'zipline-updating-order-id-{}'.format(order_id)
 
 
 def _send_emergency_order_request(order, attempt):
@@ -79,7 +96,10 @@ def _send_emergency_order_request(order, attempt):
     :param order: the EmergencyOrder that should be sent
     :param attempt: the current attempt number; in the event of errors, a total of MAX_ATTEMPTS will
     be made, separated by a wait time of RETRY_INTERVAL minutes
+    :return: the new status to be set on the order
     """
+    order.zipline_request_attempts += 1
+
     json_payload = get_json_payload_from_order(order)
     json_payload = json.dumps(json_payload)
 
@@ -93,7 +113,8 @@ def _send_emergency_order_request(order, attempt):
 
     if response.status_code != 200:
         handle_emergency_order_request_retry(order, attempt)
-        return
+        create_error_record(order, 'Received HTTP Response {} from Zipline'.format(response.status_code))
+        return EmergencyOrderStatusUpdate.STATUS_ERROR
 
     try:
         response_json = response.json()
@@ -106,16 +127,26 @@ def _send_emergency_order_request(order, attempt):
                 'attempt': attempt,
             }
         )
+        create_error_record(order, 'Could not parse JSON response from Zipline')
         handle_emergency_order_request_retry(order, attempt)
-        return
+        return EmergencyOrderStatusUpdate.STATUS_ERROR
 
     status = response_json.get('status')
     if status == ZIPLINE_STATUS_RECEIVED:
         handle_request_received(order)
+        return EmergencyOrderStatusUpdate.STATUS_RECEIVED
     elif status == ZIPLINE_STATUS_REJECTED:
-        handle_request_rejected(order)
+        reason = response_json.get('reason')
+        handle_request_rejected(order, reason)
+        return EmergencyOrderStatusUpdate.STATUS_REJECTED
+    elif status == ZIPLINE_STATUS_ERROR:
+        description = response_json.get('description')
+        create_error_record(order, 'Error received from Zipline: {}'.format(description))
+        return EmergencyOrderStatusUpdate.STATUS_ERROR
     else:
+        create_error_record(order, 'Unrecognized status received from Zipline: {}'.format(status))
         handle_emergency_order_request_retry(order, attempt)
+        return EmergencyOrderStatusUpdate.STATUS_ERROR
 
 
 def get_json_payload_from_order(order):
@@ -132,7 +163,7 @@ def get_json_payload_from_order(order):
         'locationCode': order.location_code,
         'products': [
             {'productCode': p.get('code'), 'quantityOrdered': p.get('quantity')}
-            for p in order.product_info
+            for p in order.products_requested
         ],
     }
 
@@ -142,15 +173,30 @@ def handle_request_received(order):
     Handles a received response from Zipline.
     :param order: the EmergencyOrder for the request
     """
-    EmergencyOrderStatusUpdate.create_for_order(order.pk, EmergencyOrderStatusUpdate.STATUS_RECEIVED)
+    order.received_status = EmergencyOrderStatusUpdate.create_for_order(
+        order.pk,
+        EmergencyOrderStatusUpdate.STATUS_RECEIVED
+    )
 
 
-def handle_request_rejected(order):
+def handle_request_rejected(order, reason):
     """
     Handles a rejected response from Zipline.
     :param order: the EmergencyOrder for the request
     """
-    EmergencyOrderStatusUpdate.create_for_order(order.pk, EmergencyOrderStatusUpdate.STATUS_REJECTED)
+    order.rejected_status = EmergencyOrderStatusUpdate.create_for_order(
+        order.pk,
+        EmergencyOrderStatusUpdate.STATUS_REJECTED,
+        additional_text=reason
+    )
+
+
+def create_error_record(order, error_message):
+    EmergencyOrderStatusUpdate.create_for_order(
+        order.pk,
+        EmergencyOrderStatusUpdate.STATUS_ERROR,
+        additional_text=error_message
+    )
 
 
 def handle_emergency_order_request_retry(order, current_attempt):
@@ -161,7 +207,7 @@ def handle_emergency_order_request_retry(order, current_attempt):
     """
     if current_attempt < MAX_ATTEMPTS:
         send_emergency_order_request.apply_async(
-            args=[order],
+            args=[order.pk],
             kwargs={'attempt': (current_attempt + 1)},
             countdown=(60 * RETRY_INTERVAL)
         )
