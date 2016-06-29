ca3812b757bc563a0c96cb1209531051b42060db
Daniel Miller <dmiller@dimagi.com>
2016-06-29 14:08:23 -0400

    Fix broken ES accessors test

--- corehq/apps/reports/tests/test_esaccessors.py
+++ corehq/apps/reports/tests/test_esaccessors.py
@@ -19,6 +19,7 @@ from elasticsearch.exceptions import ConnectionError
 from corehq.util.elastic import ensure_index_deleted
 from corehq.apps.users.models import CommCareUser
 from corehq.apps.groups.models import Group
+from corehq.blobs.mixin import BlobMeta
 from corehq.form_processor.utils import TestFormMetadata
 from casexml.apps.case.models import CommCareCase, CommCareCaseAction
 from casexml.apps.case.const import CASE_ACTION_CREATE
@@ -92,7 +93,8 @@ class TestFormESAccessors(BaseESAccessorsTest):
 
         form_pair = make_es_ready_form(metadata)
         if attachment_dict:
-            setattr(form_pair.wrapped_form, 'external_blobs', attachment_dict)
+            form_pair.wrapped_form.external_blobs = {name: BlobMeta(**meta)
+                for name, meta in attachment_dict.iteritems()}
             form_pair.json_form['external_blobs'] = attachment_dict
         self._pillow_process_form(form_pair)
         es = get_es_new()
@@ -142,10 +144,7 @@ class TestFormESAccessors(BaseESAccessorsTest):
             received_on=datetime(2013, 7, 2),
             user_id=user_id,
             attachment_dict={
-                'my_image': {
-                    'content_type': 'image/jpg',
-                    'data': 'abc'
-                }
+                'my_image': {'content_type': 'image/jpg'}
             }
         )
 
