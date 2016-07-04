f1f9afa558e88ad2c7e0629446d058eddad8b432
Simon Kelly <skelly@dimagi.com>
2016-07-04 14:20:22 +0200

    app index info

--- corehq/pillows/application.py
+++ corehq/pillows/application.py
@@ -1,6 +1,6 @@
 from corehq.apps.app_manager.models import ApplicationBase
 from corehq.apps.app_manager.util import get_correct_app_class
-from corehq.pillows.mappings.app_mapping import APP_INDEX, APP_MAPPING
+from corehq.pillows.mappings.app_mapping import APP_INDEX_INFO
 from pillowtop.listener import AliasedElasticPillow
 
 
@@ -12,23 +12,11 @@ class AppPillow(AliasedElasticPillow):
     document_class = ApplicationBase
     couch_filter = "app_manager/all_apps"
     es_timeout = 60
-    es_alias = "hqapps"
-    es_type = "app"
-    es_meta = {
-        "settings": {
-            "analysis": {
-                "analyzer": {
-                    "default": {
-                        "type": "custom",
-                        "tokenizer": "whitespace",
-                        "filter": ["lowercase"]
-                    },
-                }
-            }
-        }
-    }
-    es_index = APP_INDEX
-    default_mapping = APP_MAPPING
+    es_alias = APP_INDEX_INFO.alias
+    es_type = APP_INDEX_INFO.type
+    es_meta = APP_INDEX_INFO.meta
+    es_index = APP_INDEX_INFO.index
+    default_mapping = APP_INDEX_INFO.mapping
 
     def change_transform(self, doc_dict):
         # perform any lazy migrations
