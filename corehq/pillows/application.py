a04fb365e5d5f0006334ab0d4550725965893b09
Simon Kelly <skelly@dimagi.com>
2016-07-04 15:14:50 +0200

    app to ES kafka pillow

--- corehq/pillows/application.py
+++ corehq/pillows/application.py
@@ -1,7 +1,13 @@
 from corehq.apps.app_manager.models import ApplicationBase
 from corehq.apps.app_manager.util import get_correct_app_class
+from corehq.apps.change_feed import topics
+from corehq.apps.change_feed.consumer.feed import KafkaChangeFeed
+from corehq.elastic import get_es_new
 from corehq.pillows.mappings.app_mapping import APP_INDEX_INFO
+from pillowtop.checkpoints.manager import PillowCheckpoint, PillowCheckpointEventHandler
 from pillowtop.listener import AliasedElasticPillow
+from pillowtop.pillow.interface import ConstructedPillow
+from pillowtop.processors import ElasticProcessor
 
 
 class AppPillow(AliasedElasticPillow):
@@ -19,6 +25,30 @@ class AppPillow(AliasedElasticPillow):
     default_mapping = APP_INDEX_INFO.mapping
 
     def change_transform(self, doc_dict):
-        # perform any lazy migrations
-        doc = get_correct_app_class(doc_dict).wrap(doc_dict)
-        return doc.to_json()
+        return transform_app_for_es(doc_dict)
+
+
+def transform_app_for_es(doc_dict):
+    # perform any lazy migrations
+    doc = get_correct_app_class(doc_dict).wrap(doc_dict)
+    return doc.to_json()
+
+
+def get_app_to_elasticsearch_pillow(pillow_id='ApplicationToElasticsearchPillow'):
+    checkpoint = PillowCheckpoint(
+        'applications-to-elasticsearch',
+    )
+    app_processor = ElasticProcessor(
+        elasticsearch=get_es_new(),
+        index_info=APP_INDEX_INFO,
+        doc_prep_fn=transform_app_for_es
+    )
+    return ConstructedPillow(
+        name=pillow_id,
+        checkpoint=checkpoint,
+        change_feed=KafkaChangeFeed(topics=[topics.APP], group_id='apps-to-es'),
+        processor=app_processor,
+        change_processed_event_handler=PillowCheckpointEventHandler(
+            checkpoint=checkpoint, checkpoint_frequency=100,
+        ),
+    )
--- settings.py
+++ settings.py
@@ -1454,6 +1454,11 @@ PILLOWTOPS = {
             'instance': 'corehq.apps.change_feed.pillow.get_application_db_kafka_pillow',
         },
         {
+            'name': 'ApplicationToElasticsearchPillow',
+            'class': 'pillowtop.pillow.interface.ConstructedPillow',
+            'instance': 'corehq.pillows.application.get_app_to_elasticsearch_pillow',
+        },
+        {
             'name': 'DefaultChangeFeedPillow',
             'class': 'pillowtop.pillow.interface.ConstructedPillow',
             'instance': 'corehq.apps.change_feed.pillow.get_default_couch_db_change_feed_pillow',
