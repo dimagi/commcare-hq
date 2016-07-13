42070c8c57cd4090941d02b431d04f3ce2f9fa2f
Simon Kelly <skelly@dimagi.com>
2016-07-13 15:11:33 +0200

    update tests

--- corehq/apps/change_feed/tests/test_pillow.py
+++ corehq/apps/change_feed/tests/test_pillow.py
@@ -3,11 +3,13 @@ from django.conf import settings
 from django.test import SimpleTestCase
 from fakecouch import FakeCouchDb
 from kafka import KafkaConsumer
-from kafka.common import ConsumerTimeout, KafkaUnavailableError
+from kafka.common import KafkaUnavailableError
+from kafka.structs import TopicPartition
+
 from corehq.apps.change_feed import topics
 from corehq.apps.change_feed.consumer.feed import change_meta_from_kafka_message
-from corehq.apps.change_feed.pillow import get_change_feed_pillow_for_db
 from corehq.apps.change_feed.data_sources import COUCH
+from corehq.apps.change_feed.pillow import get_change_feed_pillow_for_db
 from corehq.util.test_utils import trap_extra_setup
 from pillowtop.feed.interface import Change
 
@@ -20,11 +22,13 @@ class ChangeFeedPillowTest(SimpleTestCase):
         self._fake_couch.dbname = 'test-couchdb'
         with trap_extra_setup(KafkaUnavailableError):
             self.consumer = KafkaConsumer(
-                topics.CASE,
                 group_id='test-consumer',
                 bootstrap_servers=[settings.KAFKA_URL],
-                consumer_timeout_ms=100,
+                consumer_timeout_ms=200,
             )
+            self.consumer.assign([TopicPartition(topics.CASE, 0)])
+            # hack to get consumer to initialize it's current position
+            self.consumer.position(TopicPartition(topics.CASE, 0))
         self.pillow = get_change_feed_pillow_for_db('fake-changefeed-pillow-id', self._fake_couch)
 
     def test_process_change(self):
@@ -45,7 +49,7 @@ class ChangeFeedPillowTest(SimpleTestCase):
         self.assertEqual(document['domain'], change_meta.domain)
         self.assertEqual(False, change_meta.is_deletion)
 
-        with self.assertRaises(ConsumerTimeout):
+        with self.assertRaises(StopIteration):
             self.consumer.next()
 
     def test_process_change_with_unicode_domain(self):
