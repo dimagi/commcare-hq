24ef37df53b3c6b6b2188a312d485d2b8d348137
Jenny Schweers <orange.jenny@gmail.com>
2016-07-15 14:26:46 -0400

    Update email address

--- corehq/apps/hqadmin/views.py
+++ corehq/apps/hqadmin/views.py
@@ -489,7 +489,7 @@ class VCMMigrationView(BaseAdminSectionView):
                     send_mail_async.delay(
                         _(u'Upcoming migration to easy references.'),
                             'Upcoming migration to easy references. Two weeks from now.',
-                            settings.CCHQ_BUG_REPORT_EMAIL,
+                            settings.SUPPORT_EMAIL,
                             m.admins,
                             fail_silently=False)
                     m.emailed = datetime.now()
