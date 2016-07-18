8d0c60138571d025e27e18782d4d6c8493809da1
Cory Zue <czue@dimagi.com>
2016-07-18 12:08:07 +0200

    only allow command to be run on couch domains

--- corehq/apps/cleanup/management/commands/rebuild_domain_cases.py
+++ corehq/apps/cleanup/management/commands/rebuild_domain_cases.py
@@ -3,11 +3,12 @@ from django.core.management.base import BaseCommand, CommandError
 from casexml.apps.case.cleanup import rebuild_case_from_forms
 from corehq.apps.hqcase.dbaccessors import get_case_ids_in_domain
 from corehq.form_processor.models import RebuildWithReason
+from corehq.form_processor.utils import should_use_sql_backend
 
 
 class Command(BaseCommand):
     args = '<domain> <reason>'
-    help = ('Reprocesses all cases in a domain.')
+    help = ('Reprocesses all cases in a domain. Only works with couch-based domains')
 
     def handle(self, *args, **options):
         if len(args) == 2:
@@ -15,6 +16,9 @@ class Command(BaseCommand):
             reason = args[1]
         else:
             raise CommandError('Usage: %s\n%s' % (self.args, self.help))
+
+        if should_use_sql_backend(domain):
+            raise CommandError('This command only works for couch-based domains.')
 
         ids = get_case_ids_in_domain(domain)
         for count, case_id in enumerate(ids):
