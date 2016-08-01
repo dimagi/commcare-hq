1bf4015ca31026554ae7ff622f79af119f9e71b3
Cory Zue <czue@dimagi.com>
2016-08-01 10:53:55 +0200

    run duplicate form test in both backends

--- corehq/form_processor/tests/test_kafka.py
+++ corehq/form_processor/tests/test_kafka.py
@@ -52,6 +52,7 @@ class KafkaPublishingTest(OverridableSettingsTestMixin, TestCase):
         self.assertEqual(case.case_id, change_meta.document_id)
         self.assertEqual(self.domain, change_meta.domain)
 
+    @run_with_all_backends
     def test_duplicate_form_published(self):
         form_id = uuid.uuid4().hex
         form_xml = get_simple_form_xml(form_id)
@@ -59,20 +60,21 @@ class KafkaPublishingTest(OverridableSettingsTestMixin, TestCase):
         self.assertEqual(form_id, orig_form.form_id)
         self.assertEqual(1, len(self.form_accessors.get_all_form_ids_in_domain()))
 
-        form_consumer = get_test_kafka_consumer(topics.FORM_SQL)
-
-        # post an exact duplicate
-        dupe_form = submit_form_locally(form_xml, domain=self.domain)[1]
-        self.assertTrue(dupe_form.is_duplicate)
-        self.assertNotEqual(form_id, dupe_form.form_id)
-        self.assertEqual(form_id, dupe_form.orig_id)
+        with process_kafka_changes(self.pillow):
+            with process_couch_changes('DefaultChangeFeedPillow'):
+                # post an exact duplicate
+                dupe_form = submit_form_locally(form_xml, domain=self.domain)[1]
+                self.assertTrue(dupe_form.is_duplicate)
+                self.assertNotEqual(form_id, dupe_form.form_id)
+                self.assertEqual(form_id, dupe_form.orig_id)
 
         # make sure changes made it to kafka
         # first the dupe
-        dupe_form_meta = change_meta_from_kafka_message(form_consumer.next().value)
+        self.assertEqual(2, len(self.processor.changes_seen))
+        dupe_form_meta = self.processor.changes_seen[0].metadata
         self.assertEqual(dupe_form.form_id, dupe_form_meta.document_id)
         # then the original form
-        orig_form_meta = change_meta_from_kafka_message(form_consumer.next().value)
+        orig_form_meta = self.processor.changes_seen[1].metadata
         self.assertEqual(orig_form.form_id, orig_form_meta.document_id)
         self.assertEqual(self.domain, orig_form_meta.domain)
 
