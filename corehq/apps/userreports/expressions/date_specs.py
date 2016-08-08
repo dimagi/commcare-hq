21121662cbe1d6a7755a711ca303ebb224f1b1bd
sheelio <sshah@dimagi.com>
2016-08-08 13:24:42 -0400

    Remove Diff Seconds Expression

--- corehq/apps/userreports/expressions/date_specs.py
+++ corehq/apps/userreports/expressions/date_specs.py
@@ -87,20 +87,3 @@ class DiffDaysExpressionSpec(JsonObject):
         if from_date_val is not None and to_date_val is not None:
             return (to_date_val - from_date_val).days
         return None
-
-
-class DiffSecondsExpressionSpec(JsonObject):
-    type = TypeProperty('diff_seconds')
-    from_expression = DefaultProperty(required=True)
-    to_expression = DefaultProperty(required=True)
-
-    def configure(self, from_expression, to_expression):
-        self._from_expression = from_expression
-        self._to_expression = to_expression
-
-    def __call__(self, item, context=None):
-        from_val = transform_datetime(self._from_expression(item, context))
-        to_val = transform_datetime(self._to_expression(item, context))
-        if from_val is not None and to_val is not None:
-            return (to_val - from_val).total_seconds()
-        return None
