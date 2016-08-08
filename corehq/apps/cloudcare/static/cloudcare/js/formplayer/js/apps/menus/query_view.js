79b453afc98d83bf0bd8deee8d85e78412cd9579
Will Pride <wpride@dimagi.com>
2016-08-08 10:50:51 -0400

    use string keys

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/js/apps/menus/query_view.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/js/apps/menus/query_view.js
@@ -5,9 +5,6 @@ FormplayerFrontend.module("SessionNavigate.MenuList", function (MenuList, Formpl
         tagName: "tr",
         className: "formplayer-request",
         template: "#query-view-item-template",
-        events: {
-            "click": "rowClick",
-        },
 
         templateHelpers: function () {
             var imageUri = this.options.model.get('imageUri');
@@ -25,6 +22,11 @@ FormplayerFrontend.module("SessionNavigate.MenuList", function (MenuList, Formpl
         template: "#query-view-list-template",
         childView: MenuList.QueryView,
         childViewContainer: "tbody",
+
+        initialize: function(options) {
+            this.parentModel = options.collection.models;
+        },
+
         templateHelpers: function () {
             return {
                 title: this.options.title,
@@ -42,8 +44,9 @@ FormplayerFrontend.module("SessionNavigate.MenuList", function (MenuList, Formpl
         submitAction: function() {
             var payload = {};
             var fields = $(".query-field");
+            var model = this.parentModel;
             fields.each(function(index) {
-                payload[index] = this.value;
+                payload[model[index].get('id')] = this.value;
             });
             FormplayerFrontend.trigger("menu:query", payload);
         },
