50fdbe0e86a2f0e82b4a42019ea614ebd718985a
Ethan Soergel <esoergel@gmail.com>
2016-08-10 17:23:54 -0400

    Require a location.  Use custom 403 msg

--- corehq/apps/users/location_access_restrictions.py
+++ corehq/apps/users/location_access_restrictions.py
@@ -1,13 +1,21 @@
 from django.core.exceptions import PermissionDenied
 from django.utils.translation import ugettext as _
 
+from corehq.apps.hqwebapp.views import no_permissions
+
+LOCATION_ACCESS_MSG = (
+    "This project has restricted data access rules.  Please contact your "
+    "project administrator to access specific data in the project"
+)
+
 
 class LocationAccessMiddleware(object):
     """
     Many large projects want to restrict data access by location.
     Views which handle that properly are called "location safe". This
     middleware blocks access to any non location safe features by users who
-    have such a restriction.
+    have such a restriction. If these users do not have an assigned location,
+    they cannot access anything.
     """
 
     def process_view(self, request, view_fn, view_args, view_kwargs):
@@ -16,9 +24,12 @@ class LocationAccessMiddleware(object):
         if (
             user and domain
             and not user.has_permission(domain, 'access_all_locations')
-            and not is_location_safe(view_fn)
+            and (
+                not is_location_safe(view_fn)
+                or not user.get_sql_location(domain)
+            )
         ):
-            raise PermissionDenied()
+            return no_permissions(request, message=LOCATION_ACCESS_MSG)
 
 
 def is_location_safe(view_fn):
