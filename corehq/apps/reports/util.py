410330c39419725403263623d3fa5e629fb7f5dd
Manish Kangia <mkangia@dimagi.com>
2016-08-10 19:22:32 +0200

    Use better check for empty container which used to be just a list usually but now can also be a set

--- corehq/apps/reports/util.py
+++ corehq/apps/reports/util.py
@@ -84,7 +84,7 @@ def get_all_users_by_domain(domain=None, group=None, user_ids=None,
             return temp_user
         return None
 
-    user_ids = user_ids if user_ids and user_ids[0] else None
+    user_ids = user_ids if bool(user_ids) else None
     if not CommCareUser:
         from corehq.apps.users.models import CommCareUser
 
