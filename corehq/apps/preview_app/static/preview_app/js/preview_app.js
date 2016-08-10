0c1a91c8460b5b5068b99227f71e63e99ca0a987
Ben Rudolph <brudolph@dimagi.com>
2016-08-10 01:37:29 -0400

    [ci skip] dimagimon

--- corehq/apps/preview_app/static/preview_app/js/preview_app.js
+++ corehq/apps/preview_app/static/preview_app/js/preview_app.js
@@ -3,7 +3,7 @@
 hqDefine('preview_app/js/preview_app.js', function() {
 
     var start = function(options) {
-        FormplayerFrontend.start(options)
+        FormplayerFrontend.start(options);
     };
 
     return {
