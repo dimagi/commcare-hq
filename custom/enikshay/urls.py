173b4f39433aa27970955173e63f99f58cfeecb1
Farid Rener <proteusvacuum@gmail.com>
2016-08-12 15:25:52 -0400

    Remove reference to wrong view

--- custom/enikshay/urls.py
+++ custom/enikshay/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include
 
 urlpatterns = patterns(
-    'custom.enikshay.integrations.ninetyninedots.views',
+    '',
     (r'^99dots/', include("custom.enikshay.integrations.ninetyninedots.urls")),
 )
