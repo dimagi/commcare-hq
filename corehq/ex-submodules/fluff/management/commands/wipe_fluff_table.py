9cd66b2c83956efc0d8326bcf3c07da16b57a89a
Simon Kelly <skelly@dimagi.com>
2016-08-22 14:11:41 +0200

    update wipe_fluff_table command

--- corehq/ex-submodules/fluff/management/commands/wipe_fluff_table.py
+++ corehq/ex-submodules/fluff/management/commands/wipe_fluff_table.py
@@ -1,6 +1,8 @@
 from optparse import make_option
 
 from django.core.management.base import BaseCommand
+
+from fluff.pillow import FluffPillowProcessor
 from pillowtop.utils import get_pillow_by_name
 
 
@@ -13,19 +15,21 @@ class Command(BaseCommand):
                                                          help='Skip important confirmation warnings.'),)
 
     def handle(self, *args, **options):
-        pillow_class = get_pillow_by_name(args[0], instantiate=False)
+        pillow = get_pillow_by_name(args[0])
         if not options['noinput']:
             confirm = raw_input(
                 """
                 You have requested to wipe %s table
 
                 Type 'yes' to continue, or 'no' to cancel:
-                """ % pillow_class.__name__
+                """ % pillow.pillow_id
             )
 
             if confirm != 'yes':
                 print "\tWipe cancelled."
                 return
-        engine = pillow_class.get_sql_engine()
-        table = pillow_class.indicator_class().table
+
+        processor = FluffPillowProcessor(pillow.indicator_class)
+        engine = processor.get_sql_engine()
+        table = pillow.indicator_class().table
         engine.execute(table.delete())
