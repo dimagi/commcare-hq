ff7f15e8d3d347a8a768bbeb66acd2b93accc736
Jennifer Schweers <orange.jenny@gmail.com>
2016-08-23 11:52:59 -0400

    delegate has been removed

--- corehq/apps/app_manager/static/app_manager/js/case-config-ui-1.js
+++ corehq/apps/app_manager/static/app_manager/js/case-config-ui-1.js
@@ -98,7 +98,7 @@ var CaseConfig = (function () {
     CaseConfig.prototype.init = function () {
         var casexml = this;
         if (this.questions.length) {
-            this.home.delegate('input:not(.action-checkbox), select', 'change textchange', function () {
+            this.home.delegate('change textchange', 'input:not(.action-checkbox), select', function () {
                 // recompute casexml_json
                 casexml.refreshActions();
                 casexml.render();
@@ -106,7 +106,7 @@ var CaseConfig = (function () {
                 casexml.render();
                 $("#casexml_json").text(JSON.stringify(casexml.actions));
                 casexml.saveButton.fire('change');
-            }).delegate('input.action-checkbox', 'change', function () {
+            }).delegate('change', 'input.action-checkbox', function () {
                 var container = $(this).parent().next('.well');
                 if ($(this).is(':checked')) {
                     container.slideDown();
