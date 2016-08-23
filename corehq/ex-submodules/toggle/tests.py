db55edc55df308cddcf853d58d6771ca9baa67b9
Simon Kelly <skelly@dimagi.com>
2016-08-23 14:50:54 +0200

    update tests to work with quickcache

--- corehq/ex-submodules/toggle/tests.py
+++ corehq/ex-submodules/toggle/tests.py
@@ -1,45 +1,45 @@
+import uuid
+
 from couchdbkit import ResourceConflict
-from dimagi.ext.couchdbkit import Document
-from django.conf import settings
+from couchdbkit.exceptions import ResourceNotFound
 from django.test import TestCase
+
+from toggle.shortcuts import update_toggle_cache, namespaced_item, clear_toggle_cache
 from .models import generate_toggle_id, Toggle
 from .shortcuts import toggle_enabled, set_toggle
 
 
 class ToggleTestCase(TestCase):
+    def setUp(self):
+        super(ToggleTestCase, self).setUp()
+        self.slug = uuid.uuid4().hex
 
-    @classmethod
-    def setUpClass(cls):
-        cls.realcaches = settings.CACHES
-        settings.CACHES = {
-            'default': {
-                'BACKEND':  'django.core.cache.backends.dummy.DummyCache',
-            }
-        }
-
-    @classmethod
-    def tearDownClass(cls):
-        settings.CACHES = cls.realcaches
+    def tearDown(self):
+        try:
+            toggle = Toggle.get(self.slug)
+        except ResourceNotFound:
+            pass
+        else:
+            toggle.delete()
+        super(ToggleTestCase, self).tearDown()
 
     def test_generate_id(self):
         self.assertEqual('hqFeatureToggle-sluggy', generate_toggle_id('sluggy'))
 
     def test_save_and_get_id(self):
-        slug = 'batcave'
         users = ['bruce', 'alfred']
-        toggle = Toggle(slug=slug, enabled_users=users)
+        toggle = Toggle(slug=self.slug, enabled_users=users)
         toggle.save()
-        self.assertEqual(generate_toggle_id(slug), toggle._id)
-        for id in (toggle._id, slug):
+        self.assertEqual(generate_toggle_id(self.slug), toggle._id)
+        for id in (toggle._id, self.slug):
             fromdb = Toggle.get(id)
-            self.assertEqual(slug, fromdb.slug)
+            self.assertEqual(self.slug, fromdb.slug)
             self.assertEqual(users, fromdb.enabled_users)
 
     def test_no_overwrite(self):
-        slug = 'conflict'
-        somedoc = Document(_id=generate_toggle_id(slug))
-        Toggle.get_db().save_doc(somedoc)
-        conflict = Toggle(slug=slug)
+        existing = Toggle(slug=self.slug)
+        existing.save()
+        conflict = Toggle(slug=self.slug)
         try:
             conflict.save()
             self.fail('saving a toggle on top of an existing document should not be allowed')
@@ -47,17 +47,16 @@ class ToggleTestCase(TestCase):
             pass
 
     def test_toggle_enabled(self):
-        slug = 'mansion'
         users = ['prof', 'logan']
-        toggle = Toggle(slug=slug, enabled_users=users)
+        toggle = Toggle(slug=self.slug, enabled_users=users)
         toggle.save()
-        self.assertTrue(toggle_enabled(slug, 'prof'))
-        self.assertTrue(toggle_enabled(slug, 'logan'))
-        self.assertFalse(toggle_enabled(slug, 'richard'))
+        self.assertTrue(toggle_enabled(self.slug, 'prof'))
+        self.assertTrue(toggle_enabled(self.slug, 'logan'))
+        self.assertFalse(toggle_enabled(self.slug, 'richard'))
         self.assertFalse(toggle_enabled('gotham', 'prof'))
 
     def test_add_remove(self):
-        toggle = Toggle(slug='council', enabled_users=['petyr', 'jon'])
+        toggle = Toggle(slug=self.slug, enabled_users=['petyr', 'jon'])
         toggle.save()
         rev = toggle._rev
         self.assertTrue('jon' in toggle.enabled_users)
@@ -86,17 +85,38 @@ class ToggleTestCase(TestCase):
         self.assertFalse('jon' in toggle.enabled_users)
 
     def test_set_toggle(self):
-        slug = 'wall'
-        toggle = Toggle(slug=slug, enabled_users=['benjen', 'aemon'])
+        toggle = Toggle(slug=self.slug, enabled_users=['benjen', 'aemon'])
         toggle.save()
-        self.assertTrue(toggle_enabled(slug, 'benjen'))
-        self.assertTrue(toggle_enabled(slug, 'aemon'))
 
-        set_toggle(slug, 'benjen', False)
-        self.assertFalse(toggle_enabled(slug, 'benjen'))
-        self.assertTrue(toggle_enabled(slug, 'aemon'))
+        self.assertTrue(toggle_enabled(self.slug, 'benjen'))
+        self.assertTrue(toggle_enabled(self.slug, 'aemon'))
 
-        set_toggle(slug, 'jon', True)
-        self.assertTrue(toggle_enabled(slug, 'jon'))
-        self.assertFalse(toggle_enabled(slug, 'benjen'))
-        self.assertTrue(toggle_enabled(slug, 'aemon'))
+        set_toggle(self.slug, 'benjen', False)
+        self.assertFalse(toggle_enabled(self.slug, 'benjen'))
+        self.assertTrue(toggle_enabled(self.slug, 'aemon'))
+
+        set_toggle(self.slug, 'jon', True)
+        self.assertTrue(toggle_enabled(self.slug, 'jon'))
+        self.assertFalse(toggle_enabled(self.slug, 'benjen'))
+        self.assertTrue(toggle_enabled(self.slug, 'aemon'))
+
+    def test_toggle_cache(self):
+        ns = 'ns'
+        toggle = Toggle(slug=self.slug, enabled_users=['mojer', namespaced_item('fizbod', ns)])
+        toggle.save()
+
+        self.assertTrue(toggle_enabled(self.slug, 'mojer'))
+        self.assertFalse(toggle_enabled(self.slug, 'fizbod'))
+        self.assertTrue(toggle_enabled(self.slug, 'fizbod', namespace=ns))
+
+        update_toggle_cache(self.slug, 'mojer', False)
+        update_toggle_cache(self.slug, 'fizbod', False, namespace=ns)
+
+        self.assertFalse(toggle_enabled(self.slug, 'mojer'))
+        self.assertFalse(toggle_enabled(self.slug, 'fizbod', namespace=ns))
+
+        clear_toggle_cache(self.slug, 'mojer')
+        clear_toggle_cache(self.slug, 'fizbod', namespace=ns)
+
+        self.assertTrue(toggle_enabled(self.slug, 'mojer'))
+        self.assertTrue(toggle_enabled(self.slug, 'fizbod', namespace=ns))
