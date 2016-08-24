9818d2976cf8ae018b8be126fea4c8e238dc2d6e
Cal Ellowitz <calellowitz@gmail.com>
2016-08-24 09:37:13 -0400

    [ci skip] lint

--- corehq/apps/users/management/commands/create_billing_admin_role.py
+++ corehq/apps/users/management/commands/create_billing_admin_role.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from corehq.apps.domain.models import Domain
 from corehq.apps.users.models import UserRole, UserRolePresets
 
+
 class Command(BaseCommand):
     args = ""
     help = ("Adds the billing admin preset role to all existing domains")
