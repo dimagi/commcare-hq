39cf5ca9de7d6a589fefcb351d809357e483166a
Jonathan Emord <jonathan.emord@gmail.com>
2016-08-25 11:10:27 -0400

    use index instead of create

--- corehq/apps/userreports/es/adapter.py
+++ corehq/apps/userreports/es/adapter.py
@@ -43,7 +43,7 @@ class IndicatorESAdapter(IndicatorAdapter):
             for indicator_row in indicator_rows:
                 primary_key_values = [i.value for i in indicator_row if i.column.is_primary_key]
                 all_values = {i.column.database_column_name: i.value for i in indicator_row}
-                es.create(
+                es.index(
                     index=self.table_name, body=all_values,
                     id=normalize_id(primary_key_values), doc_type="indicator"
                 )
