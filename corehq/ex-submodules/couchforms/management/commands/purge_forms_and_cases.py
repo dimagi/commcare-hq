4222cefb6f35134f3e6b41b67300799600cd23d8
Manish Kangia <mkangia@dimagi.com>
2016-08-28 20:27:54 +0200

    Add timestamp to files, X-Form -> XForm, fix printing case details, add help attribute, include xforms count for info

--- corehq/ex-submodules/couchforms/management/commands/purge_forms_and_cases.py
+++ corehq/ex-submodules/couchforms/management/commands/purge_forms_and_cases.py
@@ -5,21 +5,26 @@ from corehq.apps.app_manager.models import Application
 from casexml.apps.case.xform import get_case_ids_from_form
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors, CaseAccessors
 from dimagi.utils.django.management import are_you_sure
+from datetime import datetime
 
 
-X_FORM_HEADER = ['Version', 'Form Id', 'Domain']
+XFORM_HEADER = ['Version', 'Form Id', 'Domain']
 CASE_HEADER = ['CaseID', 'CaseType', 'CaseOpenedOn', 'CaseName', 'CaseUserId', 'CaseXFormIds']
-X_FORM_FILENAME = 'X-Form-Details.csv'
-CASE_FILE_NAME = 'Case-Details.csv'
+CURRENT_TIME = datetime.strftime(datetime.now(), '-%Y-%m-%d-%H:%M:%S')
+XFORM_FILENAME = "XForm-Details%s.csv" % CURRENT_TIME
+CASE_FILE_NAME = 'Case-Details%s.csv' % CURRENT_TIME
 
 
-# Expects 4 arguments in order : domain app_id version_number test_run
-# ex: ./manage.py purge_forms_and_cases testapp c531daeece0633738c9a3676a13e3d4f 88 yes
-# domain is included with app_id to ensure the user knows what app to delete
-# version_number to delete data accumulated by versions BEFORE this : integer
-# test_run should be yes(case-sensitive) for a test_run and anything otherwise
-# though deletion would be re-confirmed so no panic
+
 class Command(BaseCommand):
+    help = """Expects 4 arguments in order : domain app_id version_number test_run
+ex: ./manage.py purge_forms_and_cases testapp c531daeece0633738c9a3676a13e3d4f 88 yes
+domain is included with app_id to ensure the user knows what app to delete
+version_number to delete data accumulated by versions BEFORE this : integer
+test_run should be yes(case-sensitive) for a test_run and anything otherwise
+though deletion would be re-confirmed so dont panic
+"""
+
     def __init__(self):
         super(Command, self).__init__()
         self.case_ids = set()
@@ -29,8 +34,8 @@ class Command(BaseCommand):
         self.domain, self.app_id, self.version_number, self.test_run = None, None, None, None
 
     def setup(self):
-        self.xform_writer = csv.writer(open(X_FORM_FILENAME, 'w+b'))
-        self.xform_writer.writerow(X_FORM_HEADER)
+        self.xform_writer = csv.writer(open(XFORM_FILENAME, 'w+b'))
+        self.xform_writer.writerow(XFORM_HEADER)
         self.case_writer = csv.writer(open(CASE_FILE_NAME, 'w+b'))
         self.case_writer.writerow(CASE_HEADER)
         self.forms_accessor = FormAccessors(self.domain)
@@ -56,12 +61,12 @@ class Command(BaseCommand):
         _print_final_debug_info(self.xform_ids, self.filtered_xform_ids, self.case_ids)
         if self.data_to_delete() and self.delete_permitted():
             self.delete_forms_and_cases()
-            print "Process Completed!! Keep copy of files %s, %s" % (X_FORM_FILENAME, CASE_FILE_NAME)
+            print "Process Completed!! Keep copy of files %s, %s" % (XFORM_FILENAME, CASE_FILE_NAME)
         else:
             print 'Process Finished w/o Changes..'
 
     def iterate_forms_and_collect_case_ids(self):
-        print 'Iterating Through X-Forms and Collecting Case Ids'
+        print "Iterating Through %s XForms and Collecting Case Ids" % len(self.xform_ids)
         for xform in self.forms_accessor.iter_forms(self.xform_ids):
             _print_form_details(xform, self.xform_writer)
             if '@version' in xform.form and int(xform.form['@version']) < self.version_number:
@@ -70,12 +75,14 @@ class Command(BaseCommand):
                 self.case_ids = self.case_ids.union(get_case_ids_from_form(xform))
             else:
                 print 'skipping xform id: %s' % xform.form_id
+        if self.case_ids:
+            self.print_case_details()
 
     def ensure_valid_xform(self, xform):
         if xform.app_id != self.app_id and xform.domain != self.domain:
             _raise_xform_domain_mismatch(xform)
 
-    def print_case_detail(self):
+    def print_case_details(self):
         for case in self.case_accessors.iter_cases(self.case_ids):
             _print_case_details(case, self.case_writer)
 
@@ -92,7 +99,7 @@ class Command(BaseCommand):
 
 
 def _print_form_details(xform, file_writer):
-    print X_FORM_HEADER
+    print XFORM_HEADER
     form_details = _form_details(xform)
     file_writer.writerow(form_details)
     print form_details
