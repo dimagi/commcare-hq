7b3c6129df16eed0354baf777b02d6feddebb4d1
Ben Rudolph <brudolph@dimagi.com>
2016-08-29 11:21:18 -0400

    ability to get latest app build

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -877,3 +877,10 @@ DISABLE_COLUMN_LIMIT_IN_UCR = StaticToggle(
     TAG_ONE_OFF,
     [NAMESPACE_DOMAIN]
 )
+
+CLOUDCARE_LATEST_BUILD = StaticToggle(
+    'use_latest_build_cloudcare',
+    'Uses latest build for cloudcare instead of latest starred',
+    TAG_ONE_OFF,
+    [NAMESPACE_DOMAIN]
+)
