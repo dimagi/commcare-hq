5ffc7ef1f06c640bb53ca230ee7e32f93d0ac903
Simon Kelly <skelly@dimagi.com>
2016-08-30 15:22:00 +0200

    exclude deleted cases from get_case_by_external_id

--- corehq/sql_accessors/migrations/0036_exclude_deleted.py
+++ corehq/sql_accessors/migrations/0036_exclude_deleted.py
@@ -21,4 +21,5 @@ class Migration(migrations.Migration):
         ),
         migrator.get_migration('get_case_ids_in_domain_2.sql'),
         migrator.get_migration('get_case_ids_modified_with_owner_since_1.sql'),
+        migrator.get_migration('get_case_by_external_id_1.sql'),
     ]
