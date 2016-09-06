33013a0aec91bd9e1aca923ee67c28c3f942b6d3
Ben Rudolph <brudolph@dimagi.com>
2016-09-06 17:08:53 -0400

    Revert "add button to clear user data"
    This reverts commit 279676a015e2c64cbe3fe09d1d6f742bf312cc38.

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/navigation/navigation_view.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/navigation/navigation_view.js
@@ -11,9 +11,8 @@ FormplayerFrontend.module("Navigation", function (Navigation, FormplayerFrontend
         className: 'formplayer-phone-navigation',
         template: '#formplayer-phone-navigation-template',
         events: {
-            'click .js-formplayer-back': 'onBack',
-            'click .js-formplayer-reload': 'onReload',
-            'click .js-formplayer-clear-user-data': 'onClearUserData',
+            'click .formplayer-back': 'onBack',
+            'click .formplayer-reload': 'onReload',
         },
         initialize: function(options) {
             this.appId = options.appId;
@@ -21,10 +20,6 @@ FormplayerFrontend.module("Navigation", function (Navigation, FormplayerFrontend
         onBack: function(e) {
             e.preventDefault();
             window.history.back();
-        },
-        onClearUserData: function(e) {
-            e.preventDefault();
-            FormplayerFrontend.trigger('clearUserData', this.appId);
         },
         onReload: function(e) {
             e.preventDefault();
