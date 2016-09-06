1223e3c15c33b07a8998403034f333cfb432ba65
Norman Hooper <nhooper@dimagi.com>
2016-09-06 13:21:50 +0200

    Test form_data returns {} on invalid XML

--- corehq/apps/receiverwrapper/tests/test_submit_errors.py
+++ corehq/apps/receiverwrapper/tests/test_submit_errors.py
@@ -106,6 +106,7 @@ class SubmissionErrorTest(TestCase):
         self.assertIsNotNone(log)
         self.assertIn('Invalid XML', log.problem)
         self.assertEqual("this isn't even close to xml", log.get_xml())
+        self.assertEqual(log.form_data, {})
 
     def test_missing_xmlns(self):
         file, res = self._submit('missing_xmlns.xml')
