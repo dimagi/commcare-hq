e435b340b92ac665ba0dbc48c70d8d85bb5dc779
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-07 17:31:41 -0400

    don't search for module if not in map

--- corehq/apps/reports/dispatcher.py
+++ corehq/apps/reports/dispatcher.py
@@ -88,18 +88,18 @@ class ReportDispatcher(View):
 
         corehq_reports = process(getattr(reports, attr_name, ()))
 
-        module_name = get_domain_module_map().get(domain, domain)
-        try:
+        module_name = get_domain_module_map().get(domain)
+        if module_name is None:
+            custom_reports = ()
+        else:
             module = __import__(module_name, fromlist=['reports'])
             reports = getattr(module, 'reports')
             custom_reports = process(getattr(reports, attr_name, ()))
-        except ImportError:
-            custom_reports = ()
 
-        # soon to be removed
-        if not custom_reports:
-            domain_module = Domain.get_module_by_name(domain)
-            custom_reports = process(getattr(domain_module, attr_name, ()))
+            # soon to be removed
+            if not custom_reports:
+                domain_module = Domain.get_module_by_name(domain)
+                custom_reports = process(getattr(domain_module, attr_name, ()))
 
         return corehq_reports + custom_reports
 
