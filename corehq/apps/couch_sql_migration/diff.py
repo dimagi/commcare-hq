1f5b80dc14bd0c7c0830d7d66e242cc693b81d76
Simon Kelly <skelly@dimagi.com>
2016-09-09 13:49:30 +0200

    cleanup diff filtering and add tests

--- corehq/apps/couch_sql_migration/diff.py
+++ corehq/apps/couch_sql_migration/diff.py
@@ -1,100 +1,97 @@
-from corehq.apps.tzmigration.timezonemigration import is_datetime
+from corehq.apps.tzmigration.timezonemigration import is_datetime, FormJsonDiff
 
-BASE_IGNORED_FORM_PATHS = {
-    '_rev',
-    'migrating_blobs_from_couch',
-    '#export_tag',
-    'computed_',
-    'state',
-    'computed_modified_on_',
-    'deprecated_form_id',
-    'path',
-    'user_id',
-    'external_blobs',
+PARTIAL_DIFFS = {
+    'XFormInstance*': [
+        {'path': ('_rev',)},
+        {'path': ('migrating_blobs_from_couch',)},
+        {'path': ('#export_tag',)},
+        {'path': ('computed_',)},
+        {'path': ('state',)},
+        {'path': ('computed_modified_on_',)},
+        {'path': ('deprecated_form_id',)},
+        {'path': ('path',)},
+        {'path': ('user_id',)},
+        {'path': ('external_blobs',)},
+        {'diff_type': 'type', 'path': ('openrosa_headers', 'HTTP_X_OPENROSA_VERSION')},
+    ],
+    'XFormInstance': [
+        {'path': ('problem',)},
+        {'path': ('orig_id',)},
+        {'path': ('edited_on',)},
+    ],
+    'XFormInstance-Deleted': [
+        {'path': ('problem',)},
+        {'path': ('orig_id',)},
+        {'path': ('edited_on',)},
+    ],
+    'HQSubmission': [
+        {'path': ('problem',)},
+        {'path': ('orig_id',)},
+        {'path': ('edited_on',)},
+    ],
+    'XFormArchived': [
+        {'path': ('edited_on',)},
+    ],
+    'XFormError': [
+        {'path': ('edited_on',)},
+    ],
+    'XFormDuplicate': [
+        {'path': ('edited_on',)},
+    ],
+    'XFormDeprecated': [],
+    'CommCareCase': [
+        {'path': ('_rev',)},
+        {'path': ('initial_processing_complete',)},
+        {'path': ('actions', '[*]')},
+        {'path': ('id',)},
+        {'path': ('#export_tag',)},
+        {'path': ('computed_',)},
+        {'path': ('version',)},
+        {'path': ('case_attachments',)},
+        {'path': ('deleted',)},
+        {'path': ('export_tag',)},
+        {'path': ('computed_modified_on_',)},
+        {'path': ('case_id',)},
+        {'path': ('case_json',)},
+        {'path': ('modified_by',)},
+        {'path': ('indices', '[*]', 'case_id')},
+        {'diff_type': 'diff', 'path': ('owner_id',), 'old_value': ''},
+        {'diff_type': 'diff', 'path': ('owner_id',), 'old_value': None},
+    ],
+    'LedgerValue': [
+        {'path': ('_id',)},
+    ]
 }
 
-FORM_IGNORE_PATHS = {
-    'XFormInstance': BASE_IGNORED_FORM_PATHS | {'problem', 'orig_id', 'edited_on'},
-    'XFormInstance-Deleted': BASE_IGNORED_FORM_PATHS | {'problem', 'orig_id', 'edited_on'},
-    'HQSubmission': BASE_IGNORED_FORM_PATHS | {'problem', 'orig_id', 'edited_on'},
-    'XFormArchived': BASE_IGNORED_FORM_PATHS | {'edited_on'},
-    'XFormError': BASE_IGNORED_FORM_PATHS | {'edited_on'},
-    'XFormDuplicate': BASE_IGNORED_FORM_PATHS | {'edited_on'},
-    'XFormDeprecated': BASE_IGNORED_FORM_PATHS,
-}
 
-FORM_PARTIAL_DIFFS = (
-    {'diff_type': 'type', 'path': ('openrosa_headers', 'HTTP_X_OPENROSA_VERSION')},
+FORM_IGNORED_DIFFS = (
+    FormJsonDiff(
+        diff_type=u'missing', path=(u'history', u'[*]', u'doc_type'),
+        old_value=u'XFormOperation', new_value=Ellipsis
+    ),
+    FormJsonDiff(
+        diff_type=u'diff', path=(u'doc_type',),
+        old_value=u'HQSubmission', new_value=u'XFormInstance'
+    ),
+    FormJsonDiff(diff_type=u'missing', path=(u'deleted_on',), old_value=Ellipsis, new_value=None),
+    FormJsonDiff(diff_type=u'missing', path=(u'location_',), old_value=[], new_value=Ellipsis),
 )
 
-
-def _form_ignored_diffs():
-    from corehq.apps.tzmigration.timezonemigration import FormJsonDiff
-    return (
-        FormJsonDiff(
-            diff_type=u'missing', path=(u'history', u'[*]', u'doc_type'),
-            old_value=u'XFormOperation', new_value=Ellipsis
-        ),
-        FormJsonDiff(
-            diff_type=u'diff', path=(u'doc_type',),
-            old_value=u'HQSubmission', new_value=u'XFormInstance'
-        ),
-        FormJsonDiff(diff_type=u'missing', path=(u'deleted_on',), old_value=Ellipsis, new_value=None),
-        FormJsonDiff(diff_type=u'missing', path=(u'location_',), old_value=[], new_value=Ellipsis),
-    )
-
-FORM_IGNORED_DIFFS = _form_ignored_diffs()
-
-CASE_IGNORED_PATHS = {
-    ('_rev',),
-    ('initial_processing_complete',),
-    ('actions', '[*]'),
-    ('id',),
-    ('#export_tag',),
-    ('computed_',),
-    ('version',),
-    ('case_attachments',),
-    ('deleted',),
-    ('export_tag',),
-    ('computed_modified_on_',),
-    ('case_id',),
-    ('case_json',),
-    ('modified_by',),
-    ('indices', '[*]', 'case_id'),
-}
-
-
-CASE_PARTIAL_DIFFS = (
-    {'diff_type': 'diff', 'path': ('owner_id',), 'old_value': ''},
-    {'diff_type': 'diff', 'path': ('owner_id',), 'old_value': None},
+CASE_IGNORED_DIFFS = (
+    FormJsonDiff(diff_type=u'type', path=(u'name',), old_value=u'', new_value=None),
+    FormJsonDiff(diff_type=u'type', path=(u'closed_by',), old_value=u'', new_value=None),
+    FormJsonDiff(diff_type=u'missing', path=(u'location_id',), old_value=Ellipsis, new_value=None),
+    FormJsonDiff(
+        diff_type=u'missing', path=(u'indices', u'[*]', u'doc_type'),
+        old_value=u'CommCareCaseIndex', new_value=Ellipsis),
+    FormJsonDiff(diff_type=u'missing', path=(u'referrals',), old_value=[], new_value=Ellipsis),
+    FormJsonDiff(diff_type=u'missing', path=(u'location_',), old_value=[], new_value=Ellipsis),
+    FormJsonDiff(diff_type=u'type', path=(u'type',), old_value=None, new_value=u''),
+    # this happens for cases where the creation form has been archived but the case still has other forms
+    FormJsonDiff(diff_type=u'type', path=(u'owner_id',), old_value=None, new_value=u''),
+    FormJsonDiff(diff_type=u'missing', path=(u'closed_by',), old_value=Ellipsis, new_value=None),
+    FormJsonDiff(diff_type=u'type', path=(u'external_id',), old_value=u'', new_value=None),
 )
-
-
-def _case_ignored_diffs():
-    from corehq.apps.tzmigration.timezonemigration import FormJsonDiff
-    return (
-        FormJsonDiff(diff_type=u'type', path=(u'name',), old_value=u'', new_value=None),
-        FormJsonDiff(diff_type=u'type', path=(u'closed_by',), old_value=u'', new_value=None),
-        FormJsonDiff(diff_type=u'missing', path=(u'location_id',), old_value=Ellipsis, new_value=None),
-        FormJsonDiff(
-            diff_type=u'missing', path=(u'indices', u'[*]', u'doc_type'),
-            old_value=u'CommCareCaseIndex', new_value=Ellipsis),
-        FormJsonDiff(diff_type=u'missing', path=(u'referrals',), old_value=[], new_value=Ellipsis),
-        FormJsonDiff(diff_type=u'missing', path=(u'location_',), old_value=[], new_value=Ellipsis),
-        FormJsonDiff(diff_type=u'type', path=(u'type',), old_value=None, new_value=u''),
-        # this happens for cases where the creation form has been archived but the case still has other forms
-        FormJsonDiff(diff_type=u'type', path=(u'owner_id',), old_value=None, new_value=u''),
-        FormJsonDiff(diff_type=u'missing', path=(u'closed_by',), old_value=Ellipsis, new_value=None),
-        FormJsonDiff(diff_type=u'type', path=(u'external_id',), old_value=u'', new_value=None),
-    )
-
-CASE_IGNORED_DIFFS = _case_ignored_diffs()
-
-
-LEDGER_IGNORED_PATHS = {
-    ('_id',),
-}
-
 
 DATE_FIELDS = {
     'modified_on',
@@ -115,25 +112,39 @@ RENAMED_FIELDS = {
 
 
 def filter_form_diffs(doc_type, diffs):
-    paths_to_ignore = FORM_IGNORE_PATHS[doc_type]
-    filtered = [
-        diff for diff in diffs
-        if diff.path[0] not in paths_to_ignore and diff not in FORM_IGNORED_DIFFS
-    ]
+    filtered = _filter_exact_matches(diffs, FORM_IGNORED_DIFFS)
+    partial_diffs = PARTIAL_DIFFS[doc_type] + PARTIAL_DIFFS['XFormInstance*']
+    filtered = _filter_partial_matches(filtered, partial_diffs)
     filtered = _filter_renamed_fields(filtered, doc_type)
-    filtered = _filter_partial_diffs(filtered, FORM_PARTIAL_DIFFS)
     filtered = _filter_date_diffs(filtered)
     return filtered
 
 
-def _filter_partial_diffs(diffs, partial_diffs_to_exclude):
+def filter_case_diffs(doc_type, diffs):
+    filtered_diffs = _filter_exact_matches(diffs, CASE_IGNORED_DIFFS)
+    filtered_diffs = _filter_partial_matches(filtered_diffs, PARTIAL_DIFFS['CommCareCase'])
+    filtered_diffs = _filter_renamed_fields(filtered_diffs, doc_type)
+    filtered_diffs = _filter_date_diffs(filtered_diffs)
+    return filtered_diffs
+
+
+def filter_ledger_diffs(diffs):
+    return _filter_partial_matches(diffs, PARTIAL_DIFFS['LedgerValue'])
+
+
+def _filter_exact_matches(diffs, diffs_to_ignore):
+    return [
+        diff for diff in diffs
+        if diff not in diffs_to_ignore
+    ]
+
+
+def _filter_partial_matches(diffs, partial_diffs_to_exclude):
     """Filter out diffs that match a subset of attributes
     :type partial_diffs_to_exclude: dict([(attr, value)...])
     """
     def _partial_match(diff):
-        print diff
         for partial in partial_diffs_to_exclude:
-            print partial
             if all(getattr(diff, attr) == val for attr, val in partial.items()):
                 return True
         return False
@@ -167,24 +178,6 @@ def _check_renamed_fields(filtered_diffs, couch_field_name, sql_field_name):
                 diff_type='complex', path=(couch_field_name, sql_field_name),
                 old_value=couch_field.old_value, new_value=sql_field.new_value
             ))
-
-
-def filter_case_diffs(doc_type, diffs):
-    filtered_diffs = [
-        diff for diff in diffs
-        if diff.path not in CASE_IGNORED_PATHS and diff not in CASE_IGNORED_DIFFS
-    ]
-    filtered_diffs = _filter_renamed_fields(filtered_diffs, doc_type)
-    filtered_diffs = _filter_partial_diffs(filtered_diffs, CASE_PARTIAL_DIFFS)
-    filtered_diffs = _filter_date_diffs(filtered_diffs)
-    return filtered_diffs
-
-
-def filter_ledger_diffs(diffs):
-    return [
-        diff for diff in diffs
-        if diff.path not in LEDGER_IGNORED_PATHS
-    ]
 
 
 def _filter_date_diffs(diffs):
