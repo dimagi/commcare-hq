21937ebcd722ecd1152a529f3f050e1060967dd9
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-09 12:04:28 -0400

    move pact reports

--- custom/_legacy/pact/reports/__init__.py
+++ custom/_legacy/pact/reports/__init__.py
@@ -47,3 +47,18 @@ class PactDrilldownReportMixin(object):
     @classmethod
     def show_in_navigation(cls, *args, **kwargs):
         return False
+
+
+from pact.reports import patient_list, dot, patient, chw_list, chw, admin_dot_reports, admin_chw_reports
+
+CUSTOM_REPORTS = (
+    ("PACT Reports", (
+        patient_list.PatientListDashboardReport,
+        dot.PactDOTReport,
+        patient.PactPatientInfoReport,
+        chw_list.PactCHWDashboard,
+        chw.PactCHWProfileReport,
+        admin_dot_reports.PactDOTAdminReport,
+        admin_chw_reports.PactCHWAdminReport,
+    )),
+)
