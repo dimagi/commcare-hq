24b5e58742849a5fb8ee8431a3edaf3daf423660
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-09 16:12:42 -0400

    move mc CUSTOM_REPORTS to reports/__init__.py

--- custom/reports/mc/reports/__init__.py
+++ custom/reports/mc/reports/__init__.py
@@ -1,1 +1,10 @@
+from . import sql
 
+CUSTOM_REPORTS = (
+    ('Custom Reports', (
+        sql.DistrictMonthly,
+        sql.HeathFacilityMonthly,
+        sql.DistrictWeekly,
+        sql.HealthFacilityWeekly,
+    )),
+)
