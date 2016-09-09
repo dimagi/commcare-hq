32bb5c398822bbc6c5872b1e492330fc7fcf49af
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-09 13:58:33 -0400

    move ewsghana CUSTOM_REPORTS to reports.py

--- custom/ewsghana/__init__.py
+++ custom/ewsghana/__init__.py
@@ -1,26 +1,5 @@
-from custom.ewsghana.reports.email_reports import CMSRMSReport, StockSummaryReport
-from custom.ewsghana.reports.maps import EWSMapReport
-from custom.ewsghana.reports.stock_levels_report import StockLevelsReport
-from custom.ewsghana.reports.specific_reports.dashboard_report import DashboardReport
-from custom.ewsghana.reports.specific_reports.stock_status_report import StockStatus
-from custom.ewsghana.reports.specific_reports.reporting_rates import ReportingRatesReport
-from custom.ewsghana.reports.stock_transaction import StockTransactionReport
-
 LOCATION_TYPES = ["country", "region", "district", "facility"]
 
 CUSTOM_DASHBOARD_VIEW_NAME = 'dashboard_page'
-
-CUSTOM_REPORTS = (
-    ('Custom reports', (
-        DashboardReport,
-        StockStatus,
-        StockLevelsReport,
-        ReportingRatesReport,
-        EWSMapReport,
-        CMSRMSReport,
-        StockSummaryReport,
-        StockTransactionReport
-    )),
-)
 
 ROOT_SITE_CODE = 'ghana'
--- custom/ewsghana/reports/__init__.py
+++ custom/ewsghana/reports/__init__.py
@@ -477,3 +477,24 @@ class ProductSelectionPane(EWSData):
             'is_rendered_as_email': self.config.get('is_rendered_as_email', False),
             'hide_columns': self.hide_columns
         })
+
+from custom.ewsghana.reports.email_reports import CMSRMSReport, StockSummaryReport
+from custom.ewsghana.reports.maps import EWSMapReport
+from custom.ewsghana.reports.stock_levels_report import StockLevelsReport
+from custom.ewsghana.reports.specific_reports.dashboard_report import DashboardReport
+from custom.ewsghana.reports.specific_reports.stock_status_report import StockStatus
+from custom.ewsghana.reports.specific_reports.reporting_rates import ReportingRatesReport
+from custom.ewsghana.reports.stock_transaction import StockTransactionReport
+
+CUSTOM_REPORTS = (
+    ('Custom reports', (
+        DashboardReport,
+        StockStatus,
+        StockLevelsReport,
+        ReportingRatesReport,
+        EWSMapReport,
+        CMSRMSReport,
+        StockSummaryReport,
+        StockTransactionReport
+    )),
+)
