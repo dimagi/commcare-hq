ba7b063fa8020d7f2f80cde359d9e70828c50295
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-09 12:42:49 -0400

    move custom reports for domain crs-remind

--- custom/apps/crs_reports/reports.py
+++ custom/apps/crs_reports/reports.py
@@ -230,3 +230,100 @@ class HBNCMotherReport(BaseHNBCReport):
 
         return {'and': filters} if filters else {}
 
+CUSTOM_REPORTS = (
+    (_('Custom Reports'), (
+        HBNCMotherReport,
+    )),
+)
+
+QUESTION_TEMPLATES = (
+    (HBNCMotherReport.slug, [
+        { 'questions' :[
+            {'case_property': 'section_a',
+             'question': _('A. Ask Mother.')
+            },
+            {'case_property': 'meals',
+            'question': _('Number of times mother takes full meals in 24 hours?'),
+            },
+            {'case_property': 'bleeding',
+            'question': _('Bleeding. How many Pads are changed in a day?'),
+            },
+            {'case_property': 'warm',
+            'question': _('During the cold season is the baby being kept warm?'),
+            },
+            {'case_property': 'feeding',
+            'question': _('Is the baby being fed properly?'),
+            },
+            {'case_property': 'incessant_cry',
+            'question': _('Is the baby crying incessantly or passing urine less than 6 times?'),
+            },
+            {'case_property': 'section_b',
+            'question': _('B. Examination of mother'),
+            },
+            {'case_property': 'maternal_temp',
+            'question': _('Temperature: Measure and Record?'),
+            },
+            {'case_property': 'discharge',
+            'question': _('Foul Smelling Discharge?'),
+            },
+            {'case_property': 'maternal_fits',
+            'question': _('Is mother speaking normally or having fits'),
+            },
+            {'case_property': 'no_milk',
+            'question': _('Mother has no milk since delivery or less milk'),
+            },
+            {'case_property': 'sore_breast',
+            'question': _('Cracked Nipples/Painful or Engorged Breast/'),
+            }]
+        },
+        { 'questions' :[
+            {'case_property': 'section_c',
+             'question': _('C.  Examination of Baby')
+            },
+            {'case_property': 'baby_eye',
+            'question': _('Eyes Swollen with pus?'),
+            },
+            {'case_property': 'weight',
+            'question': _('Weight (7,14,21,28)?'),
+            },
+            {'case_property': 'baby_temp',
+            'question': _('Temperature: Measure and Record?'),
+            },
+            {'case_property': 'pustules',
+            'question': _('Skin: Pus filled pustules?'),
+            },
+            {'case_property': 'cracks',
+            'question': _('Cracks and Redness on the skin fold?'),
+            },
+            {'case_property': 'jaundice',
+            'question': _('Yellowness in eyes'),
+            }]
+        },
+        { 'questions' :[
+            {'case_property': 'section_d',
+             'question': _('D.  Sepsis Signs Checkup')
+            },
+            {'case_property': 'limbs',
+            'question': _('All limbs up?'),
+            },
+            {'case_property': 'feeding_less',
+            'question': _('Feeding Less/Stopped?'),
+            },
+            {'case_property': 'cry',
+            'question': _('Cry Weak/Stopped?'),
+            },
+            {'case_property': 'abdomen_vomit',
+            'question': _('Distant Abdomen?'),
+            },
+            {'case_property': 'cold',
+            'question': _('Baby Cold to touch?'),
+            },
+            {'case_property': 'chest',
+            'question': _('Chest Indrawing?'),
+            },
+            {'case_property': 'pus',
+            'question': _('Pus on umbilicus?'),
+            }]
+        }
+    ]),
+)
