7d039ed7c1de8a86c71d60a169e5da404d41d831
Simon Kelly <skelly@dimagi.com>
2016-09-13 12:32:34 +0200

    reference form attachments in case - don't copy

--- corehq/form_processor/backends/sql/update_strategy.py
+++ corehq/form_processor/backends/sql/update_strategy.py
@@ -159,12 +159,10 @@ class SqlCaseUpdateStrategy(UpdateStrategy):
                 form_attachment = xform.get_attachment_meta(att.attachment_src)
                 if identifier in current_attachments:
                     existing_attachment = current_attachments[identifier]
-                    existing_attachment.update_from_attachment(form_attachment)
-                    existing_attachment.copy_content(form_attachment)
+                    existing_attachment.from_form_attachment(form_attachment)
                     self.case.track_update(existing_attachment)
                 else:
-                    new_attachment.update_from_attachment(form_attachment)
-                    new_attachment.copy_content(form_attachment)
+                    new_attachment.from_form_attachment(form_attachment)
                     new_attachment.case = self.case
                     self.case.track_create(new_attachment)
             elif identifier in current_attachments:
