50503c6bb7c0a3e6c51b33e107c56e08b1c7274a
Ben Rudolph <brudolph@dimagi.com>
2016-09-12 23:07:49 -0400

    Remove rest of touch forms from subtree merge

--- corehq/ex-submodules/touchforms/localsettings.example.py
+++
@@ -1,9 +0,0 @@
-# you can override settings here
-XFORMS_PLAYER_URL = "http://localhost:4444/"
-
-COUCH_SERVER_ROOT = 'localhost:5984'
-COUCH_USERNAME = ''
-COUCH_PASSWORD = ''
-COUCH_DATABASE_NAME = 'xformplayer'
-
-CACHE_BACKEND = "memcached://127.0.0.1:11211/"
--- corehq/ex-submodules/touchforms/offline/src/main/resources/Lib/__run__.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.insert(0, '__pyclasspath__/Lib') # 'Lib' seems magic somehow; don't use any other directory name
-
-sys.prefix = '' # fix issue with optparse
-
-from touchforms import xformserver
-xformserver.main(stale_window=0.1, offline=True)
-
