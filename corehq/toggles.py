b64e210f71457e321aede670fc60a15487bedce4
Ben Rudolph <rudolphben@gmail.com>
2016-09-12 23:41:45 -0400

    [ci skip] bumpity bump

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -794,7 +794,7 @@ TF_USES_SQLITE_BACKEND = PredictablyRandomToggle(
     'Use a SQLite backend for Touchforms',
     TAG_PRODUCT_PATH,
     [NAMESPACE_DOMAIN],
-    0.1,
+    0.5,
     always_disabled=['hsph-betterbirth'],
 )
 
