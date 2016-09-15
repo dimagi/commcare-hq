5bc843de89db1a6692fc5fa5c4dd8c84653769a5
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-15 17:47:58 +0530

    include indices

--- custom/enikshay/nikshay_datamigration/management/commands/create_enikshay_cases.py
+++ custom/enikshay/nikshay_datamigration/management/commands/create_enikshay_cases.py
@@ -1,56 +1,74 @@
 from django.core.management import BaseCommand
 
-from dimagi.utils import make_uuid
-
-from corehq.util.test_utils import create_and_save_a_case
+from casexml.apps.case.const import CASE_INDEX_EXTENSION
+from casexml.apps.case.mock import CaseFactory, CaseStructure, CaseIndex
 
 from custom.enikshay.nikshay_datamigration.models import PatientDetail, Outcome
 
+ENIKSHAY_DOMAIN = 'enikshay-np'
 
-class CaseFactory(object):
+
+class EnikshayCaseFactory(object):
 
     patient_detail = None
 
     def __init__(self, patient_detail):
         self.patient_detail = patient_detail
+        self.factory = CaseFactory(domain=ENIKSHAY_DOMAIN)
 
     def create_cases(self):
         self.create_person_case()
         self.create_occurrence_cases()
 
     def create_person_case(self):
-        create_and_save_a_case(
-            domain='enikshay-np',
-            case_id=self.patient_detail.PregId.strip(),
-            case_name=self.patient_detail.pname,
-            # if can be blank (or null) should we skip adding the property?
-            case_properties={
-                'name': self.patient_detail.pname,
-                'aadhaar_number': self.patient_detail.paadharno,
-                'phi': self.patient_detail.PHI,
-                'first_name': self.patient_detail.first_name,
-                'middle_name': self.patient_detail.middle_name,
-                'last_name': self.patient_detail.last_name,
-                'age': self.patient_detail.page,
-                'sex': self.patient_detail.sex,
-                'current_address': self.patient_detail.paddress,
-                'mobile_number': self.patient_detail.pmob,
-            },
-            case_type='person',
-        )
+        self.factory.create_or_update_cases([self.person])
 
     def create_occurrence_cases(self):
         for outcome in Outcome.objects.filter(PatientId=self.patient_detail):
-            create_and_save_a_case(
-                domain='enikshay-np',
-                case_id=make_uuid(),
-                case_name=make_uuid(),
-                case_properties={
+            self.factory.create_or_update_cases([self.occurrence(outcome)])
+
+    @property
+    def person(self):
+        return CaseStructure(
+            case_id=self.patient_detail.PregId,
+            attrs={
+                'create': True,
+                'case_type': 'person',
+                'update': {
+                    'name': self.patient_detail.pname,
+                    'aadhaar_number': self.patient_detail.paadharno,
+                    'phi': self.patient_detail.PHI,
+                    'first_name': self.patient_detail.first_name,
+                    'middle_name': self.patient_detail.middle_name,
+                    'last_name': self.patient_detail.last_name,
+                    'age': self.patient_detail.page,
+                    'sex': self.patient_detail.sex,
+                    'current_address': self.patient_detail.paddress,
+                    'mobile_number': self.patient_detail.pmob,
+                },
+            },
+        )
+
+    def occurrence(self, outcome):
+        return CaseStructure(
+            attrs={
+                'create': True,
+                'case_type': 'occurrence',
+                'update': {
                     'nikshay_id': outcome.PatientId.PregId,
                     'hiv_status': outcome.HIVStatus,
                 },
-                case_type='occurrence',
-            )
+            },
+            indices=[CaseIndex(
+                self.person,
+                identifier='host',
+                relationship=CASE_INDEX_EXTENSION,
+                related_type=self.person.attrs['case_type'],
+            )],
+        )
+
+    def create_case(self, case_structure):
+        return self.factory.create_or_update_cases([case_structure])
 
 
 class Command(BaseCommand):
@@ -58,7 +76,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         counter = 0
         for patient_detail in PatientDetail.objects.all():
-            case_factory = CaseFactory(patient_detail)
+            case_factory = EnikshayCaseFactory(patient_detail)
             case_factory.create_cases()
             counter += 1
             print counter
