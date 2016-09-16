9711334a486feb1cb498a66feab4652ce5735ab3
Cal Ellowitz <calellowitz@gmail.com>
2016-09-16 13:49:57 -0400

    adding management command to force user 412

---
+++ corehq/ex-submodules/casexml/apps/phone/management/commands/invalidate_sync_heads.py
@@ -0,0 +1,31 @@
+from django.core.management import BaseCommand, CommandError
+
+from casexml.apps.phone.models import SimplifiedSyncLog
+from casexml.apps.phone.dbaccessors.sync_logs_by_user import synclog_view
+
+
+class Command(BaseCommand):
+    """
+    Forces a 412 for a given user by creating bad state in the all synclogs
+    for the given user after the given date
+    """
+    args = "user_id date"
+
+    def handle(self, *args, **options):
+        if len(args) != 2:
+            raise CommandError("Usage is ./manage.py invalidate_sync_heads %s" % self.args)
+        user_id = args[0]
+        date = args[1]
+        results = synclog_view(
+            "phone/sync_logs_by_user",
+            startkey=[user_id, {}],
+            endkey=[user_id, date],
+            descending=True,
+            reduce=False,
+            include_docs=True,
+        )
+
+        logs = [SimplifiedSyncLog.wrap(res['doc']) for res in results]
+        for log in logs:
+            log.case_ids_on_phone = {'broken to force 412'}
+            log.save()
