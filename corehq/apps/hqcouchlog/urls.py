b632af9ee73b205791246b6a961d6ef8ecd8ae76
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-19 08:59:54 +0530

    WIP urls

--- corehq/apps/hqcouchlog/urls.py
+++ corehq/apps/hqcouchlog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import *
 
+from corehq.apps.hqcouchlog.views import fail
+
 urlpatterns = patterns('corehq.apps.hqcouchlog.views',
-    (r'^fail/$', 'fail'),
+    (r'^fail/$', fail),
 )
