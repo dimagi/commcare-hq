bfb167584d36982926921ca44014b69f798aae38
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-19 08:54:38 +0530

    update dashboard url

--- corehq/apps/dashboard/urls.py
+++ corehq/apps/dashboard/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from corehq.apps.dashboard.views import *
+
+from corehq.apps.dashboard.views import dashboard_default, DomainDashboardView
 
 urlpatterns = patterns('corehq.apps.dashboard.views',
-    url(r'^$', 'dashboard_default', name='dashboard_default'),
+    url(r'^$', dashboard_default, name='dashboard_default'),
     url(r'^project/$', DomainDashboardView.as_view(),
         name=DomainDashboardView.urlname),
 )
