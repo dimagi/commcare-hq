8ab85598f48cb480d7210af2838df1605ad50ced
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-19 08:58:45 +0530

    update groups urls

--- corehq/apps/groups/urls.py
+++ corehq/apps/groups/urls.py
@@ -1,25 +1,30 @@
 from django.conf.urls import *
 
+from corehq.apps.groups.views import (
+    add_group, delete_group, undo_delete_group, restore_group, edit_group,
+    update_group_data, update_group_membership,
+)
+
 urlpatterns = patterns('corehq.apps.groups.views',
     url(r'^add_group/$',
-        'add_group',
+        add_group,
         name='add_group'),
     url(r'^delete_group/(?P<group_id>[\w-]+)/$',
-        'delete_group',
+        delete_group,
         name='delete_group'),
     url(r'^undo_delete_group/(?P<record_id>[\w-]+)/$',
-        'undo_delete_group',
+        undo_delete_group,
         name='undo_delete_group'),
     url(r'^restore_group/(?P<group_id>[\w-]+)/$',
-        'restore_group',
+        restore_group,
         name='restore_group'),
     url(r'^edit_group/(?P<group_id>[\w-]+)/$',
-        'edit_group',
+        edit_group,
         name='edit_group'),
     url(r'^update_group_data/(?P<group_id>[\w-]+)/$',
-        'update_group_data',
+        update_group_data,
         name='update_group_data'),
     url(r'^update_group_membership/(?P<group_id>[\w-]+)/$',
-        'update_group_membership',
+        update_group_membership,
         name='update_group_membership'),
 )
