1758bee459ed2d8fa2465ed27553b72d5fd0de66
Cal Ellowitz <calellowitz@gmail.com>
2016-09-20 13:28:26 -0400

    adding update on save for case auto updates

--- corehq/apps/data_interfaces/tasks.py
+++ corehq/apps/data_interfaces/tasks.py
@@ -14,6 +14,8 @@ from corehq.apps.data_interfaces.utils import add_cases_to_case_group, archive_f
 from .interfaces import FormManagementMode, BulkFormManagementInterface
 from .dispatcher import EditDataInterfaceDispatcher
 from dimagi.utils.django.email import send_HTML_email
+from dimagi.utils.couch import CriticalSection
+
 
 logger = get_task_logger('data_interfaces')
 ONE_HOUR = 60 * 60
@@ -100,7 +102,7 @@ def run_case_update_rules_for_domain(domain, now=None):
 
     for case_type, rules in rules_by_case_type.iteritems():
         boundary_date = AutomaticUpdateRule.get_boundary_date(rules, now)
-        case_ids = AutomaticUpdateRule.get_case_ids(domain, boundary_date, case_type)
+        case_ids = AutomaticUpdateRule.get_case_ids(domain, case_type, boundary_date)
 
         for case in CaseAccessors(domain).iter_cases(case_ids):
             for rule in rules:
@@ -111,3 +113,18 @@ def run_case_update_rules_for_domain(domain, now=None):
         for rule in rules:
             rule.last_run = now
             rule.save()
+
+
+@task(queue='background_queue', acks_late=True, ignore_result=True)
+def run_case_update_rules_on_save(domain, case_type, case_id):
+    rules = AutomaticUpdateRule.by_domain(domain).filter(case_type=case_type)
+    case = CaseAccessors(domain).get_case(case_id)
+    now = datetime.utcnow()
+    key = 'case-update-on-save-case-{case}'.format(case=case_id)
+    with CriticalSection([key]):
+        for rule in rules:
+            stop_processing = rule.apply_rule(case, now)
+            rule.last_run = now
+            rule.save()
+            if stop_processing:
+                break
