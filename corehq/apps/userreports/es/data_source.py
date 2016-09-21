337df03cea6810c4be5eac9078a89a0ca8ec3443
Jonathan Emord <jonathan.emord@gmail.com>
2016-09-21 13:25:53 -0400

    remove unused functions

--- corehq/apps/userreports/es/data_source.py
+++ corehq/apps/userreports/es/data_source.py
@@ -1,17 +1,13 @@
-import numbers
 from collections import OrderedDict
 
 from django.utils.decorators import method_decorator
-from django.utils.translation import ugettext
 
 from dimagi.utils.decorators.memoized import memoized
 
 from corehq.apps.reports.api import ReportDataSource
 from corehq.apps.userreports.decorators import catch_and_raise_exceptions
-from corehq.apps.userreports.exceptions import InvalidQueryColumn
 from corehq.apps.userreports.models import DataSourceConfiguration, get_datasource_config
 from corehq.apps.userreports.reports.sorting import ASCENDING, DESCENDING
-from corehq.apps.userreports.reports.util import get_expanded_columns
 from corehq.apps.userreports.util import get_table_name
 
 from corehq.apps.es.es_query import HQESQuery
@@ -78,18 +74,6 @@ class ConfigurableReportEsDataSource(ReportDataSource):
 
     def set_order_by(self, columns):
         self._order_by = columns
-
-    # @property
-    # def filter_values(self):
-    #     return {k: v for fv in self._filter_values.values() for k, v in fv.to_sql_values().items()}
-    #
-    # @property
-    # def group_by(self):
-    #     # ask each column for its group_by contribution and combine to a single list
-    #     return [
-    #         group_by for col_id in self.aggregation_columns
-    #         for group_by in self._get_db_column_ids(col_id)
-    #     ]
 
     @property
     def order_by(self):
@@ -168,50 +152,6 @@ class ConfigurableReportEsDataSource(ReportDataSource):
         ret = [c.field for c in self.column_configs]
         return ret + [c for c in self.aggregation_columns]
 
-    # @method_decorator(catch_and_raise_exceptions)
-    # def get_total_row(self):
-    #     def _clean_total_row(val, col):
-    #         if isinstance(val, numbers.Number):
-    #             return val
-    #         elif col.calculate_total:
-    #             return 0
-    #         return ''
-    #
-    #     def _get_relevant_column_ids(col, column_id_to_expanded_column_ids):
-    #         return column_id_to_expanded_column_ids.get(col.column_id, [col.column_id])
-    #
-    #     expanded_columns = get_expanded_columns(self.column_configs, self.config)
-    #
-    #     qc = self.query_context()
-    #     for c in self.columns:
-    #         qc.append_column(c.view)
-    #
-    #     session = connection_manager.get_scoped_session(self.engine_id)
-    #     totals = qc.totals(
-    #         session.connection(),
-    #         [
-    #             column_id
-    #             for col in self.column_configs for column_id in _get_relevant_column_ids(col, expanded_columns)
-    #             if col.calculate_total
-    #         ],
-    #         self.filter_values
-    #     )
-    #
-    #     total_row = [
-    #         _clean_total_row(totals.get(column_id), col)
-    #         for col in self.column_configs for column_id in _get_relevant_column_ids(
-    #             col, expanded_columns
-    #         )
-    #     ]
-    #     if total_row and total_row[0] is '':
-    #         total_row[0] = ugettext('Total')
-    #     return total_row
-    #
-    # def _get_db_column_ids(self, column_id):
-    #     # for columns that end up being complex queries (e.g. aggregate dates)
-    #     # there could be more than one column ID and they may specify aliases
-    #     if column_id in self._column_configs:
-    #         return self._column_configs[column_id].get_query_column_ids()
-    #     else:
-    #         # if the column isn't found just treat it as a normal field
-    #         return [column_id]
+    @method_decorator(catch_and_raise_exceptions)
+    def get_total_row(self):
+        return []
