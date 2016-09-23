6abfaf58e2b20936bf84179011e05367cbfb88ea
Biyeun Buczyk <biyeun@dimagi.com>
2016-09-23 13:35:30 -0400

    fix display options entry point

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
@@ -537,10 +537,9 @@ Formplayer.Utils.answersEqual = function(answer1, answer2) {
  * @param {Object} formJSON - The json representation of the form
  * @param {Object} resourceMap - Function for resolving multimedia paths
  * @param {Object} $div - The jquery element that the form will be rendered in.
- * @param {Object} displayOptions - A dictionary detailing various modes or options formplayer might be in.
  */
-Formplayer.Utils.initialRender = function(formJSON, resourceMap, $div, displayOptions) {
-    var form = new Form(formJSON, displayOptions),
+Formplayer.Utils.initialRender = function(formJSON, resourceMap, $div) {
+    var form = new Form(formJSON),
         $debug = $('#cloudcare-debugger'),
         cloudCareDebugger;
     Formplayer.resourceMap = resourceMap;
