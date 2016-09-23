aaa3abba066be1e9186ef5ab7a79f361b1ceabfd
Simon Kelly <skelly@dimagi.com>
2016-09-23 09:48:05 -0400

    remove old function, filter by correct case field

--- corehq/sql_accessors/migrations/0040_fix_case_reindex_query.py
+++ corehq/sql_accessors/migrations/0040_fix_case_reindex_query.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from corehq.sql_db.operations import RawSQLMigration
+from corehq.sql_db.operations import RawSQLMigration, HqRunSQL
 
 migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {})
 
@@ -15,6 +15,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrator.get_migration('get_all_forms_received_since_2.sql'),
-        migrator.get_migration('get_all_ledger_values_modified_since_2.sql'),
+        HqRunSQL(
+            "DROP FUNCTION IF EXISTS get_all_cases_modified_since(timestamp with time zone, integer)",
+            "SELECT 1"
+        ),
+        migrator.get_migration('get_all_cases_modified_since_3.sql'),
     ]
