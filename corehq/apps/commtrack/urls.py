9a183fb6693709f6c1ee22ca713d0e2a63d54274
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-27 13:08:21 -0400

    update commtrack url

--- corehq/apps/commtrack/urls.py
+++ corehq/apps/commtrack/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, url
 from corehq.apps.commtrack.views import (
     DefaultConsumptionView, SMSSettingsView, CommTrackSettingsView,
     StockLevelsView, RebuildStockStateView,
+    default,
 )
 
 # used in settings urls
 settings_urls = patterns('corehq.apps.commtrack.views',
-    url(r'^$', 'default', name="default_commtrack_setup"),
+    url(r'^$', default, name="default_commtrack_setup"),
     url(r'^project_settings/$', CommTrackSettingsView.as_view(), name=CommTrackSettingsView.urlname),
     url(r'^default_consumption/$', DefaultConsumptionView.as_view(), name=DefaultConsumptionView.urlname),
     url(r'^sms/$', SMSSettingsView.as_view(), name=SMSSettingsView.urlname),
