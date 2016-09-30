ab05a9f9755aacfe9c1a5ff132aafb675296ccbd
Nick Pellegrino <npellegrino@dimagi.com>
2016-09-30 19:46:07 -0400

    remove extra space

--- corehq/apps/receiverwrapper/urls.py
+++ corehq/apps/receiverwrapper/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('corehq.apps.receiverwrapper.views',
     url(r'^/secure/$', secure_post, name='receiver_secure_post'),
 
     # odk urls
-    url(r'^/submission/?$',  post, name="receiver_odk_post"),
+    url(r'^/submission/?$', post, name="receiver_odk_post"),
     url(r'^/(?P<app_id>[\w-]+)/$', post, name='receiver_post_with_app_id'),
 )
