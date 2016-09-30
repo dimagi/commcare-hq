36214ba27e145f787099a3d700ec86f355e6901b
Cal Ellowitz <calellowitz@gmail.com>
2016-09-30 15:50:14 -0400

    adding assert to test

--- corehq/apps/data_interfaces/tests/test_auto_case_updates.py
+++ corehq/apps/data_interfaces/tests/test_auto_case_updates.py
@@ -483,7 +483,9 @@ class AutomaticCaseUpdateTest(TestCase):
     @run_with_all_backends
     def test_early_task_exit(self):
         with _with_case(self.domain, 'test-case-type-3', datetime(2016, 1, 1), case_name='signal') as case:
-            hqcase.utils.update_case(case.domain, case.case_id, case_properties={}, xmlns=AUTO_UPDATE_XMLNS)
+            with patch('corehq.apps.data_interfaces.models.AutomaticUpdateRule.apply_actions') as apply:
+                hqcase.utils.update_case(case.domain, case.case_id, case_properties={}, xmlns=AUTO_UPDATE_XMLNS)
+                apply.assert_not_called()
 
 
 @contextmanager
