937e21f002f1612d7035d37ea2e58fb3bd0cb963
Nick Pellegrino <npellegrino@dimagi.com>
2016-10-05 13:23:34 -0400

    update loadtestendpoints urls

--- corehq/apps/loadtestendpoints/urls.py
+++ corehq/apps/loadtestendpoints/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, url
 
+from corehq.apps.loadtestendpoints.views import noop, saving
+
 urlpatterns = patterns(
     'corehq.apps.loadtestendpoints.views',
-    url(r'^noop/$', 'noop'),
-    url(r'^saving/$', 'saving'),
+    url(r'^noop/$', noop, name='noop'),
+    url(r'^saving/$', saving, name='saving'),
 )
