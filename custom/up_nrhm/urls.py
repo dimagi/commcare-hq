100a92a9d3739d0e750c863e32c1a571e82adb0b
Nick Pellegrino <npellegrino@dimagi.com>
2016-10-05 13:28:21 -0400

    update urls in custom

--- custom/up_nrhm/urls.py
+++ custom/up_nrhm/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, url
 
+from custom.up_nrhm.views import asha_af_report
+
 urlpatterns = patterns('custom.up_nrhm.views',
-                       url(r'^asha_af_report/$', 'asha_af_report', name='asha_af_report'),)
+                       url(r'^asha_af_report/$', asha_af_report, name='asha_af_report'),)
