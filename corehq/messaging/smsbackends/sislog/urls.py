dc9925a6cef1209763409c5bb66f8beb09384e01
Nick Pellegrino <npellegrino@dimagi.com>
2016-10-05 13:32:08 -0400

    update sislog urls

--- corehq/messaging/smsbackends/sislog/urls.py
+++ corehq/messaging/smsbackends/sislog/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import *
+from django.conf.urls import patterns, url
+
+from corehq.messaging.smsbackends.sislog.views import sms_in
 
 urlpatterns = patterns('corehq.messaging.smsbackends.sislog.views',
-    url(r'^in/?$', 'sms_in', name='sms_in'),
+    url(r'^in/?$', sms_in, name='sms_in'),
 )
