61e304a4f50b5bcacd5d8ec0d5329b8b08927d7a
Kamil Krampa <kkrampa@soldevelo.com>
2016-10-10 11:47:49 +0200

    Moved reports outside module __init__.py

--- custom/enikshay/__init__.py
+++
@@ -1,9 +0,0 @@
-from custom.enikshay.reports.case_finding import CaseFindingReport
-from custom.enikshay.reports.treatment_outcome import TreatmentOutcomeReport
-
-CUSTOM_REPORTS = (
-    ('Custom Reports', (
-        CaseFindingReport,
-        TreatmentOutcomeReport
-    )),
-)
