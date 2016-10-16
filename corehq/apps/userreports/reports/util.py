d32ac6943768949d3678015acf127d6513101bfd
Farid Rener <proteusvacuum@gmail.com>
2016-10-16 17:29:41 -0400

    Use spec not view

--- corehq/apps/userreports/reports/util.py
+++ corehq/apps/userreports/reports/util.py
@@ -1,3 +1,4 @@
+from corehq.apps.userreports.models import get_report_config
 from corehq.apps.userreports.sql import get_expanded_column_config
 
 
@@ -16,10 +17,9 @@ def get_expanded_columns(column_configs, data_source_config):
 def has_location_filter(view_fn, *args, **kwargs):
     """check that the report has at least one location choice provider filter
     """
-    from corehq.apps.userreports.reports.view import ConfigurableReport
-    report = ConfigurableReport(args=args, kwargs=kwargs)
+    report, _ = get_report_config(config_id=kwargs.get('subreport_slug'), domain=kwargs.get('domain'))
     return any(
         filter_.choice_provider.location_safe
         if hasattr(filter_, 'choice_provider') else False
-        for filter_ in report.filters
+        for filter_ in report.ui_filters
     )
