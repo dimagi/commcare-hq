17e40f2c7a1b7bd2c1892b1b318a25704bb2a075
Ben Rudolph <brudolph@dimagi.com>
2016-10-18 13:10:30 -0400

    Progress bar transition

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/progress_view.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/progress_view.js
@@ -5,8 +5,10 @@ FormplayerFrontend.module("Utils.Views", function (Views, FormplayerFrontend, Ba
     Views.ProgressView = Marionette.ItemView.extend({
         template: "#progress-view-template",
 
-        setProgress: function(progress) {
-            $(this.el).find('.js-progress-bar').width(progress);
+        setProgress: function(progress, duration) {
+            // Due to jQuery bug, can't use .animate() with % until jQuery 3.0
+            $(this.el).find('.js-progress-bar').css('transition', duration + 'ms');
+            $(this.el).find('.js-progress-bar').width(progress * 100 + '%');
         },
     });
 });
