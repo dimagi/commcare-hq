c727a2326e34427c6547c1a449d4451ae76210b5
Biyeun Buczyk <biyeun@dimagi.com>
2016-10-21 23:07:51 -0400

    redirect to vellum on creationg of module or form (re: b817f1114915a9d8e815afeb2f346707a2e01578)

--- corehq/apps/app_manager/views/modules.py
+++ corehq/apps/app_manager/views/modules.py
@@ -829,6 +829,7 @@ def new_module(request, domain, app_id):
         module = app.add_module(Module.new_module(name, lang))
         module_id = module.id
 
+        form_id = None
         if toggles.APP_MANAGER_V2.enabled(domain):
             if module_type == 'case':
                 # registration form
@@ -854,11 +855,13 @@ def new_module(request, domain, app_id):
                     module.case_type = 'case-{}'.format(suffix)
             else:
                 form = app.new_form(module_id, "Survey", lang)
+            form_id = 0
         else:
             app.new_form(module_id, "Untitled Form", lang)
 
         app.save()
-        response = back_to_main(request, domain, app_id=app_id, module_id=module_id)
+        response = back_to_main(request, domain, app_id=app_id,
+                                module_id=module_id, form_id=form_id)
         response.set_cookie('suppress_build_errors', 'yes')
         return response
     elif module_type in MODULE_TYPE_MAP:
