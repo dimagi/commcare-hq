e260b9288dd68c227ab1cbf01741c6a917c10c8d
Jonathan Emord <jonathan.emord@gmail.com>
2016-10-24 15:57:11 -0400

    unused import

--- corehq/apps/accounting/migrations/0040_community_v1.py
+++ corehq/apps/accounting/migrations/0040_community_v1.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models, migrations
+from django.db import migrations
 
-from corehq.apps.accounting.bootstrap.config.community_v1 import (
-    BOOTSTRAP_CONFIG,
-    BOOTSTRAP_CONFIG_TESTING,
-)
+from corehq.apps.accounting.bootstrap.config.community_v1 import BOOTSTRAP_CONFIG
 from corehq.apps.accounting.bootstrap.utils import ensure_plans
 from corehq.apps.hqadmin.management.commands.cchq_prbac_bootstrap import cchq_prbac_bootstrap
 from corehq.sql_db.operations import HqRunPython
