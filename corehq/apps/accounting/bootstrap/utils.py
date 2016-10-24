96f02dec851285793576781fb537d807dd24c58e
Jonathan Emord <jonathan.emord@gmail.com>
2016-10-24 15:19:32 -0400

    use trials defined in config

--- corehq/apps/accounting/bootstrap/utils.py
+++ corehq/apps/accounting/bootstrap/utils.py
@@ -31,30 +31,31 @@ def ensure_plans(config, dry_run, verbose, apps):
     DefaultProductPlan = apps.get_model('accounting', 'DefaultProductPlan')
     SoftwarePlan = apps.get_model('accounting', 'SoftwarePlan')
     SoftwarePlanVersion = apps.get_model('accounting', 'SoftwarePlanVersion')
+    Role = apps.get_model('django_prbac', 'Role')
 
-    edition_to_role = {plan_name[0]: conf['role'] for plan_name, conf in config.iteritems()}
-    edition_to_product_rate = {plan_name[0]: conf['product_rate'] for plan_name, conf in config.iteritems()}
-    edition_to_feature_rate = {plan_name[0]: conf['feature_rates'] for plan_name, conf in config.iteritems()}
-
-    editions = edition_to_role.keys()
-    edition_to_features = _ensure_features(editions, dry_run=dry_run, verbose=verbose, apps=apps)
     for product_type in PRODUCT_TYPES:
-        for edition in editions:
-            role_slug = edition_to_role[edition]
-            role = _ensure_role(role_slug, apps)
-            software_plan_version = SoftwarePlanVersion(role=role)
+        for plan_key, plan_deets in config.iteritems():
+            edition = plan_key[0]
+            is_trial = plan_key[1]
+            features = _ensure_features(edition, dry_run, verbose, apps)
+            try:
+                role = _ensure_role(plan_deets['role'], apps)
+            except Role.DoesNotExist:
+                return
 
             product, product_rate = _ensure_product_and_rate(
-                edition_to_product_rate, product_type, edition,
+                plan_deets['product_rate'], product_type, edition,
                 dry_run=dry_run, verbose=verbose, apps=apps,
             )
             feature_rates = _ensure_feature_rates(
-                edition_to_feature_rate, edition_to_features[edition], edition,
+                plan_deets['feature_rates'], features, edition,
                 dry_run=dry_run, verbose=verbose, apps=apps,
             )
+
             software_plan = SoftwarePlan(
                 name='%s Edition' % product.name, edition=edition, visibility=SoftwarePlanVisibility.PUBLIC
             )
+
             if dry_run:
                 logger.info("[DRY RUN] Creating Software Plan: %s", software_plan.name)
             else:
@@ -69,7 +70,7 @@ def ensure_plans(config, dry_run, verbose, apps):
                     if verbose:
                         logger.info("Creating Software Plan: %s", software_plan.name)
 
-                software_plan_version.plan = software_plan
+                software_plan_version = SoftwarePlanVersion(role=role, plan=software_plan)
 
                 # must save before assigning many-to-many relationship
                 if hasattr(SoftwarePlanVersion, 'product_rates'):
@@ -92,28 +93,25 @@ def ensure_plans(config, dry_run, verbose, apps):
                     software_plan_version.feature_rates.add(feature_rate)
                 software_plan_version.save()
 
-            if edition == SoftwarePlanEdition.ADVANCED:
-                trials = [True, False]
+            default_product_plan = DefaultProductPlan(
+                product_type=product.product_type, edition=edition, is_trial=is_trial
+            )
+            if dry_run:
+                logger.info("[DRY RUN] Setting plan as default for product '%s' and edition '%s'.",
+                            product.product_type, default_product_plan.edition)
             else:
-                trials = [False]
-            for is_trial in trials:
-                default_product_plan = DefaultProductPlan(product_type=product.product_type, edition=edition, is_trial=is_trial)
-                if dry_run:
-                    logger.info("[DRY RUN] Setting plan as default for product '%s' and edition '%s'.",
-                                product.product_type, default_product_plan.edition)
-                else:
-                    try:
-                        default_product_plan = DefaultProductPlan.objects.get(product_type=product.product_type,
-                                                                              edition=edition, is_trial=is_trial)
-                        if verbose:
-                            logger.info("Default for product '%s' and edition '%s' already exists.",
-                                        product.product_type, default_product_plan.edition)
-                    except DefaultProductPlan.DoesNotExist:
-                        default_product_plan.plan = software_plan
-                        default_product_plan.save()
-                        if verbose:
-                            logger.info("Setting plan as default for product '%s' and edition '%s'.",
-                                        product.product_type, default_product_plan.edition)
+                try:
+                    default_product_plan = DefaultProductPlan.objects.get(product_type=product.product_type,
+                                                                          edition=edition, is_trial=is_trial)
+                    if verbose:
+                        logger.info("Default for product '%s' and edition '%s' already exists.",
+                                    product.product_type, default_product_plan.edition)
+                except DefaultProductPlan.DoesNotExist:
+                    default_product_plan.plan = software_plan
+                    default_product_plan.save()
+                    if verbose:
+                        logger.info("Setting plan as default for product '%s' and edition '%s'.",
+                                    product.product_type, default_product_plan.edition)
 
 
 def _ensure_role(role_slug, apps):
@@ -127,7 +125,7 @@ def _ensure_role(role_slug, apps):
     return role
 
 
-def _ensure_product_and_rate(edition_to_product_rate, product_type, edition, dry_run, verbose, apps):
+def _ensure_product_and_rate(product_rate, product_type, edition, dry_run, verbose, apps):
     """
     Ensures that all the necessary SoftwareProducts and SoftwareProductRates are created for the plan.
     """
@@ -141,7 +139,7 @@ def _ensure_product_and_rate(edition_to_product_rate, product_type, edition, dry
     if edition == SoftwarePlanEdition.ENTERPRISE:
         product.name = "Dimagi Only %s" % product.name
 
-    product_rate = SoftwareProductRate(**edition_to_product_rate[edition])
+    product_rate = SoftwareProductRate(**product_rate)
     if dry_run:
         logger.info("[DRY RUN] Creating Product: %s", product)
         logger.info("[DRY RUN] Corresponding product rate of $%d created.", product_rate.monthly_fee)
@@ -160,39 +158,38 @@ def _ensure_product_and_rate(edition_to_product_rate, product_type, edition, dry
     return product, product_rate
 
 
-def _ensure_features(editions, dry_run, verbose, apps):
+def _ensure_features(edition, dry_run, verbose, apps):
     """
     Ensures that all the Features necessary for the plans are created.
     """
     Feature = apps.get_model('accounting', 'Feature')
 
     if verbose:
-        logger.info('Ensuring Features')
+        logger.info('Ensuring Features for plan: %s', edition)
 
-    edition_to_features = defaultdict(list)
-    for edition in editions:
-        for feature_type in FEATURE_TYPES:
-            feature = Feature(name='%s %s' % (feature_type, edition), feature_type=feature_type)
-            if edition == SoftwarePlanEdition.ENTERPRISE:
-                feature.name = "Dimagi Only %s" % feature.name
-            if dry_run:
-                logger.info("[DRY RUN] Creating Feature: %s", feature)
-            else:
-                try:
-                    feature = Feature.objects.get(name=feature.name)
-                    if verbose:
-                        logger.info("Feature '%s' already exists. Using "
+    features = []
+    for feature_type in FEATURE_TYPES:
+        feature = Feature(name='%s %s' % (feature_type, edition), feature_type=feature_type)
+        if edition == SoftwarePlanEdition.ENTERPRISE:
+            feature.name = "Dimagi Only %s" % feature.name
+        if dry_run:
+            logger.info("[DRY RUN] Creating Feature: %s", feature)
+        else:
+            try:
+                feature = Feature.objects.get(name=feature.name)
+                if verbose:
+                    logger.info("Feature '%s' already exists. Using "
                                 "existing feature to add rate.",
                                 feature.name)
-                except Feature.DoesNotExist:
-                    feature.save()
-                    if verbose:
-                        logger.info("Creating Feature: %s", feature)
-            edition_to_features[edition].append(feature)
-    return edition_to_features
+            except Feature.DoesNotExist:
+                feature.save()
+                if verbose:
+                    logger.info("Creating Feature: %s", feature)
+        features.append(feature)
+    return features
 
 
-def _ensure_feature_rates(edition_to_feature_rate, features, edition, dry_run, verbose, apps):
+def _ensure_feature_rates(feature_rates, features, edition, dry_run, verbose, apps):
     """
     Ensures that all the FeatureRates necessary for the plans are created.
     """
@@ -201,13 +198,13 @@ def _ensure_feature_rates(edition_to_feature_rate, features, edition, dry_run, v
     if verbose:
         logger.info('Ensuring Feature Rates')
 
-    feature_rates = []
+    db_feature_rates = []
     for feature in features:
-        feature_rate = FeatureRate(**edition_to_feature_rate[edition][feature.feature_type])
+        feature_rate = FeatureRate(**feature_rates[feature.feature_type])
         feature_rate.feature = feature
         if dry_run:
             logger.info("[DRY RUN] Creating rate for feature '%s': %s", feature.name, feature_rate)
         elif verbose:
             logger.info("Creating rate for feature '%s': %s", feature.name, feature_rate)
-        feature_rates.append(feature_rate)
-    return feature_rates
+        db_feature_rates.append(feature_rate)
+    return db_feature_rates
