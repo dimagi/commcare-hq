819b0b4f50be9e0f2aaad9ebf45d29504b8b1f84
Biyeun Buczyk <biyeun@dimagi.com>
2016-10-24 13:53:34 -0400

    breaks toggles because this is used before the toggles are defined. Just don't use this class in onboarding templates

--- corehq/apps/style/static/style/js/main.js
+++ corehq/apps/style/static/style/js/main.js
@@ -59,15 +59,13 @@ COMMCAREHQ.transformHelpTemplate = function ($template, wrap) {
 COMMCAREHQ.initBlock = function ($elem) {
     'use strict';
 
-    if (! COMMCAREHQ.toggleEnabled('APP_MANAGER_V2')) {
-        $('.submit_on_click', $elem).on("click", function (e) {
-            e.preventDefault();
-            if (!$(this).data('clicked')) {
-                $(this).prev('form').submit();
-                $(this).data('clicked', 'true').children('i').removeClass().addClass("icon-refresh icon-spin fa fa-refresh fa-spin");
-            }
-        });
-    }
+    $('.submit_on_click', $elem).on("click", function (e) {
+        e.preventDefault();
+        if (!$(this).data('clicked')) {
+            $(this).prev('form').submit();
+            $(this).data('clicked', 'true').children('i').removeClass().addClass("icon-refresh icon-spin fa fa-refresh fa-spin");
+        }
+    });
 
     $('.submit').click(function (e) {
         var $form = $(this).closest('.form, form'),
