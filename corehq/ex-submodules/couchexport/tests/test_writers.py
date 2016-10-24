794c4ec73d890cca805c2a16e4348cd5110e021b
NoahCarnahan <ncarnahan@dimagi.com>
2016-10-24 14:21:39 -0400

    Add export writer tests

--- corehq/ex-submodules/couchexport/tests/test_writers.py
+++ corehq/ex-submodules/couchexport/tests/test_writers.py
@@ -1,6 +1,9 @@
 # coding: utf-8
 from codecs import BOM_UTF8
-from couchexport.writers import ZippedExportWriter, CsvFileWriter
+
+import StringIO
+
+from couchexport.writers import ZippedExportWriter, CsvFileWriter, PythonDictWriter
 from django.test import SimpleTestCase
 from mock import patch, Mock
 
@@ -48,3 +51,49 @@ class CsvFileWriterTests(SimpleTestCase):
         writer.finish()
         file_start = writer.get_file().read(6)
         self.assertEqual(file_start, BOM_UTF8 + 'ham')
+
+
+class HeaderNameTest(SimpleTestCase):
+
+    def test_names_matching_case(self):
+        writer = PythonDictWriter()
+        stringio = StringIO.StringIO()
+        table_index_1 = "case_Sensitive"
+        table_index_2 = "case_sensitive"
+        table_headers = [[]]
+        writer.open(
+            [
+                (table_index_1, table_headers),
+                (table_index_2, table_headers)
+            ],
+            stringio
+        )
+        writer.close()
+        preview = writer.get_preview()
+
+        first_sheet_name = preview[0]['table_name']
+        second_sheet_name = preview[1]['table_name']
+        self.assertNotEqual(
+            first_sheet_name.lower(),
+            second_sheet_name.lower(),
+            "Sheet names must not be equal. Comparison is NOT case sensitive. Names were '{}' and '{}'".format(
+                first_sheet_name, second_sheet_name
+            )
+        )
+
+    def test_max_header_length(self):
+        writer = PythonDictWriter()
+        writer.max_table_name_size = 10
+        stringio = StringIO.StringIO()
+        table_index = "my_table_index"
+        table_headers = [("first header", "second header")]
+        writer.open(
+            [(table_index, table_headers)],
+            stringio
+        )
+        writer.close()
+        preview = writer.get_preview()
+        self.assertGreater(len(table_index), writer.max_table_name_size)
+        self.assertLessEqual(len(preview[0]['table_name']), writer.max_table_name_size)
+
+
