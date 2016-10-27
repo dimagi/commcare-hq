e44e943968cfe3f859cc017c554656d9e68043c8
Jonathan Emord <jonathan.emord@gmail.com>
2016-10-27 12:01:57 -0400

    Remove unused header code

--- corehq/apps/userreports/columns.py
+++ corehq/apps/userreports/columns.py
@@ -7,13 +7,9 @@ class ColumnConfig(object):
     Stub object to send column information to the data source
     """
 
-    def __init__(self, columns, headers=None, warnings=None):
+    def __init__(self, columns, warnings=None):
         self.columns = columns
-        # default headers to column headers, but allow subclasses to override
-        if headers is not None:
-            self.headers = [c.header for c in self.columns]
-        else:
-            self.headers = headers
+        self.headers = [c.header for c in self.columns]
         self.warnings = warnings if warnings is not None else []
 
 
