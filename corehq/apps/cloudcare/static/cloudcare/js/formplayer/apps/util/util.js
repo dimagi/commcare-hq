6ee11b5390a21819325d62ffb68fc8bb73fd1b0e
Ben Rudolph <brudolph@dimagi.com>
2016-10-28 14:29:04 -0400

    [ci skip] dimagimon

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/util.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/util.js
@@ -188,13 +188,13 @@ if (!String.prototype.startsWith) {
 }
 
 if (!String.prototype.endsWith) {
-  String.prototype.endsWith = function(searchString, position) {
-      var subjectString = this.toString();
-      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-        position = subjectString.length;
-      }
-      position -= searchString.length;
-      var lastIndex = subjectString.lastIndexOf(searchString, position);
-      return lastIndex !== -1 && lastIndex === position;
-  };
+    String.prototype.endsWith = function(searchString, position) {
+        var subjectString = this.toString();
+        if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+            position = subjectString.length;
+        }
+        position -= searchString.length;
+        var lastIndex = subjectString.lastIndexOf(searchString, position);
+        return lastIndex !== -1 && lastIndex === position;
+    };
 }
