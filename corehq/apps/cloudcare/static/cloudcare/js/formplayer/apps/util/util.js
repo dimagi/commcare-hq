0e1a36a66f0fc8d2243ef9ad14e23076a1225f09
Ben Rudolph <brudolph@dimagi.com>
2016-10-28 15:12:32 -0400

    [ci skip] dimagimon

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/util.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/util/util.js
@@ -200,15 +200,15 @@ if (!String.prototype.endsWith) {
 }
 
 if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') {
-      start = 0;
-    }
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
-  };
+    String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+            start = 0;
+        }
+        if (start + search.length > this.length) {
+            return false;
+        } else {
+            return this.indexOf(search, start) !== -1;
+        }
+    };
 }
