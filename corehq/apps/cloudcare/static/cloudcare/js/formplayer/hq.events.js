003572e5c7daceb12971aaef76d323902343e963
Ben Rudolph <brudolph@dimagi.com>
2016-10-31 15:23:50 -0400

    Remove ability to have callbacks

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/hq.events.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/hq.events.js
@@ -16,14 +16,8 @@ FormplayerFrontend.module("HQ.Events", function(Events, FormplayerFrontend) {
         // For Chrome, the origin property is in the event.originalEvent object
         var origin = event.origin || event.originalEvent.origin,
             data = event.data,
-            returnValue = null,
             success = true;
 
-        _.defaults(data, {
-            success: function() {},
-            error: function() {},
-            complete: function() {},
-        });
         if (!origin.endsWith(this.allowedHost)) {
             throw new Error('Disallowed origin ' + origin);
         }
@@ -35,20 +29,10 @@ FormplayerFrontend.module("HQ.Events", function(Events, FormplayerFrontend) {
             throw new Error('Invalid action ' + data.action);
         }
 
-        try {
-            switch (data.action) {
-            case Events.Actions.BACK:
-                FormplayerFrontend.trigger('navigation:back');
-                break;
-            }
-        } catch (e) {
-            success = false;
-            data.error(event, data, returnValue);
-        } finally {
-            data.complete(event, data, returnValue);
-        }
-        if (success) {
-            data.success(event, data, returnValue);
+        switch (data.action) {
+        case Events.Actions.BACK:
+            FormplayerFrontend.trigger('navigation:back');
+            break;
         }
     };
 
