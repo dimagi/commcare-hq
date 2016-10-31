eb09ff0023fd7379ff248b4878e07e794783d665
Ben Rudolph <rudolphben@gmail.com>
2016-10-31 18:23:46 -0400

    Revert "Revert "Signal to update inferred schema""

--- corehq/apps/export/models/new.py
+++ corehq/apps/export/models/new.py
@@ -39,6 +39,7 @@ from dimagi.ext.couchdbkit import (
     ListProperty,
     StringProperty,
     DateTimeProperty,
+    SetProperty,
 )
 from corehq.apps.export.const import (
     PROPERTY_TAG_UPDATE,
@@ -54,6 +55,7 @@ from corehq.apps.export.const import (
     MISSING_VALUE,
     EMPTY_VALUE,
     KNOWN_CASE_PROPERTIES,
+    UNKNOWN_INFERRED_FROM,
 )
 from corehq.apps.export.exceptions import BadExportConfiguration
 from corehq.apps.export.dbaccessors import (
@@ -119,6 +121,7 @@ class ExportItem(DocumentSchema):
     # True if this item was inferred from different actions in HQ (i.e. case upload)
     # False if the item was found in the application structure
     inferred = BooleanProperty(default=False)
+    inferred_from = SetProperty(default=set)
 
     @classmethod
     def wrap(cls, data):
@@ -156,6 +159,7 @@ class ExportItem(DocumentSchema):
         item = cls(one.to_json())
         item.last_occurrences = _merge_dicts(one.last_occurrences, two.last_occurrences, max)
         item.inferred = one.inferred or two.inferred
+        item.inferred_from |= two.inferred_from
         return item
 
     @property
@@ -939,18 +943,19 @@ class InferredExportGroupSchema(ExportGroupSchema):
     Same as an ExportGroupSchema with a few utility methods
     """
 
-    def put_item(self, path):
+    def put_item(self, path, inferred_from=None):
         assert self.path == path[:len(self.path)], "ExportItem's path doesn't start with the table"
-
         item = self.get_item(path)
 
         if item:
+            item.inferred_from.add(inferred_from or UNKNOWN_INFERRED_FROM)
             return item
 
         item = ExportItem(
             path=path,
             label='.'.join(map(lambda node: node.name, path)),
-            inferred=True
+            inferred=True,
+            inferred_from=set([inferred_from or UNKNOWN_INFERRED_FROM])
         )
         self.items.append(item)
         return item
@@ -1011,7 +1016,7 @@ class ExportDataSchema(Document):
     An object representing the things that can be exported for a particular
     form xmlns or case type. It contains a list of ExportGroupSchema.
     """
-    domain = StringProperty()
+    domain = StringProperty(required=True)
     created_on = DateTimeProperty(default=datetime.utcnow)
     group_schemas = SchemaListProperty(ExportGroupSchema)
     app_id = StringProperty()
@@ -1067,14 +1072,14 @@ class ExportDataSchema(Document):
 
             current_schema.record_update(app.copy_of or app._id, app.version)
 
+        inferred_schema = cls._get_inferred_schema(domain, identifier)
+        if inferred_schema:
+            current_schema = cls._merge_schemas(current_schema, inferred_schema)
+
         current_schema.domain = domain
         current_schema.app_id = app_id
         current_schema.version = DATA_SCHEMA_VERSION
         current_schema._set_identifier(identifier)
-
-        inferred_schema = current_schema._get_inferred_schema()
-        if inferred_schema:
-            current_schema = cls._merge_schemas(current_schema, inferred_schema)
 
         current_schema = cls._save_export_schema(
             current_schema,
@@ -1174,7 +1179,7 @@ class ExportDataSchema(Document):
 
 class FormExportDataSchema(ExportDataSchema):
 
-    xmlns = StringProperty()
+    xmlns = StringProperty(required=True)
     datatype_mapping = defaultdict(lambda: ScalarItem, {
         'MSelect': MultipleChoiceItem,
         'Geopoint': GeopointItem,
@@ -1187,7 +1192,8 @@ class FormExportDataSchema(ExportDataSchema):
     def type(self):
         return FORM_EXPORT
 
-    def _get_inferred_schema(self):
+    @classmethod
+    def _get_inferred_schema(cls, domain, xmlns):
         return None
 
     def _set_identifier(self, form_xmlns):
@@ -1326,7 +1332,7 @@ class FormExportDataSchema(ExportDataSchema):
 
 class CaseExportDataSchema(ExportDataSchema):
 
-    case_type = StringProperty()
+    case_type = StringProperty(required=True)
 
     @property
     def type(self):
@@ -1335,8 +1341,9 @@ class CaseExportDataSchema(ExportDataSchema):
     def _set_identifier(self, case_type):
         self.case_type = case_type
 
-    def _get_inferred_schema(self):
-        return get_inferred_schema(self.domain, self.case_type)
+    @classmethod
+    def _get_inferred_schema(cls, domain, case_type):
+        return get_inferred_schema(domain, case_type)
 
     @classmethod
     def _get_current_app_ids_for_domain(cls, domain):
