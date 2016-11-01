7c783cc5ef1529c3d6a35bef2b04b2d25766e8ee
Ben Rudolph <brudolph@dimagi.com>
2016-11-01 17:11:48 -0400

    Add tests

---
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/util_spec.js
@@ -0,0 +1,27 @@
+/* global Util */
+/* eslint-env mocha */
+
+describe('Util', function() {
+    describe('#displayOptions', function() {
+        beforeEach(function() {
+            sinon.stub(Util, 'getDisplayOptionsKey', function() { return 'mykey'; });
+            window.localStorage.clear();
+        });
+
+        afterEach(function() {
+            Util.getDisplayOptionsKey.restore();
+        });
+
+        it('should retrieve saved display options', function() {
+            var options = { option: 'yes' }
+            Util.saveDisplayOptions(options);
+            assert.deepEqual(Util.getSavedDisplayOptions(), options);
+        });
+
+        it('should not fail on bad json saved', function() {
+            localStorage.setItem(Util.getDisplayOptionsKey(), 'bad json');
+            assert.deepEqual(Util.getSavedDisplayOptions(), {});
+        });
+
+    });
+});
