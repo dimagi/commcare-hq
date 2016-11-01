643e59364a6e8ae5863fe5e580eb186cfd18ec87
Jenny Schweers <orange.jenny@gmail.com>
2016-11-01 14:23:28 -0400

    Remove copy_app_check_domain from views/__init__

--- corehq/apps/app_manager/views/__init__.py
+++ corehq/apps/app_manager/views/__init__.py
@@ -31,7 +31,6 @@ from corehq.apps.app_manager.views.apps import (
     app_from_template,
     app_source,
     copy_app,
-    copy_app_check_domain,
     default_new_app,
     delete_app,
     delete_app_lang,
