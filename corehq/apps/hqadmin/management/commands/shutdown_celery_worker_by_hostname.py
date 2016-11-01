98e5a1fe20e6eefa108ca3e5323da1bf3ad65be9
Ben Rudolph <brudolph@dimagi.com>
2016-11-01 14:31:20 -0400

    Exit 1 based on whether or not it was shutdown correctly

--- corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
+++ corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 from celery import Celery
 
+from corehq.apps.hqadmin.utils import parse_celery_pings
+
 
 class Command(BaseCommand):
     help = "Gracefully shutsdown a celery worker"
@@ -11,3 +13,9 @@ class Command(BaseCommand):
         celery = Celery()
         celery.config_from_object(settings)
         celery.control.broadcast('shutdown', destination=[hostname])
+        worker_responses = celery.control.ping(timeout=10, destination=[hostname])
+        pings = parse_celery_pings(worker_responses)
+        if hostname in pings:
+            print 'Did not shutdown worker'
+            exit(1)
+        print 'Successfully initiated warm shutdown'
