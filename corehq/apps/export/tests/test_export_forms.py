f8ee111817cff396bc17b22c17d5b32c90f411ef
NoahCarnahan <ncarnahan@dimagi.com>
2016-11-01 13:44:27 -0400

    Fix export filter form tests

--- corehq/apps/export/tests/test_export_forms.py
+++ corehq/apps/export/tests/test_export_forms.py
@@ -3,13 +3,17 @@ from collections import namedtuple
 
 import pytz
 from django.test import SimpleTestCase
-from mock import patch
+from mock import patch, MagicMock
 
+from corehq.apps.export.filter_builders import ESFormExportFilterBuilder
 from corehq.apps.export.filters import FormSubmittedByFilter
 from corehq.apps.export.forms import FilterFormESExportDownloadForm
 
 
 @patch('corehq.apps.reports.util.get_first_form_submission_received', lambda x: datetime.datetime(2015, 1, 1))
+@patch('corehq.apps.export.forms.Group', new=MagicMock(
+    get_reporting_groups=lambda x: [MagicMock(_id='some_group_id', name='my group')]
+))
 class TestFilterFormESExportDownloadForm(SimpleTestCase):
 
     def setUp(self):
@@ -17,27 +21,42 @@ class TestFilterFormESExportDownloadForm(SimpleTestCase):
         self.project = DomainObject(False, "foo")
 
     def test_get_datespan_filter(self):
+
         form_data = {'date_range': '2015-06-25 to 2016-02-19'}
         form = FilterFormESExportDownloadForm(self.project, pytz.utc, form_data)
         self.assertTrue(form.is_valid())
-        datespan_filter = form._get_datespan_filter()
+
+        filter_builder = ESFormExportFilterBuilder(
+            self.project.name, pytz.utc, 'group', [], [], form.cleaned_data['date_range']
+        )
+
+        datespan_filter = filter_builder._get_datespan_filter()
         self.assertEqual(datespan_filter.lt, datetime.datetime(2016, 2, 20, tzinfo=pytz.utc))
         self.assertEqual(datespan_filter.gte, datetime.datetime(2015, 6, 25, tzinfo=pytz.utc))
         self.assertEqual(datespan_filter.lte, None)
         self.assertEqual(datespan_filter.gt, None)
 
     def test_get_group_filter(self):
-        """
-        Confirm that FilterFormESExportDownloadForm._get_group_filter() returns
-        a filter with the correct group_id and correct base_filter.
-        """
+        # Confirm that FilterFormESExportDownloadForm._get_group_filter() returns
+        # a filter with the correct group_id and correct base_filter.
         form_data = {
             'type_or_group': 'group',
             'group': 'some_group_id',
             'date_range': '2015-06-25 to 2016-02-19',
         }
         form = FilterFormESExportDownloadForm(self.project, pytz.utc, form_data)
+
         self.assertTrue(form.is_valid(), "Form had the following errors: {}".format(form.errors))
-        group_filter = form._get_group_filter()
+
+        filter_builder = ESFormExportFilterBuilder(
+            self.project.name,
+            pytz.utc,
+            form.cleaned_data['type_or_group'],
+            form.cleaned_data['group'],
+            form.cleaned_data['user_types'],
+            form.cleaned_data['date_range']
+        )
+
+        group_filter = filter_builder._get_group_filter()
         self.assertEqual(group_filter.group_id, 'some_group_id')
         self.assertEqual(group_filter.base_filter, FormSubmittedByFilter)
