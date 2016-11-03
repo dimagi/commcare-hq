198f2ddc851717b4b2a73b4892d6b32bc978f810
NoahCarnahan <ncarnahan@dimagi.com>
2016-11-03 16:24:24 -0400

    Revert "[refactor] Move filter builders to their own file"
    This reverts commit f18d124186d5f2a478cfeb3281b4ae6b2c2a486f.

--- corehq/apps/export/forms.py
+++ corehq/apps/export/forms.py
@@ -5,17 +5,32 @@ from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext as _, ugettext_lazy
 from unidecode import unidecode
 
-from corehq.apps.export.models.new import ExportInstanceFilters, DatePeriod
+from corehq.apps.export.filters import (
+    ReceivedOnRangeFilter,
+    GroupFormSubmittedByFilter,
+    OR, OwnerFilter, LastModifiedByFilter, UserTypeFilter,
+    OwnerTypeFilter, ModifiedOnRangeFilter
+)
 from corehq.apps.groups.models import Group
-
+from corehq.apps.reports.models import HQUserType
 from corehq.apps.reports.util import (
+    group_filter,
+    users_matching_filter,
+    users_filter,
+    datespan_export_filter,
+    app_export_filter,
+    case_group_filter,
+    case_users_filter,
     datespan_from_beginning,
 )
 from corehq.apps.style.crispy import B3MultiField, CrispyTemplate
 from corehq.apps.style.forms.widgets import (
     Select2MultipleChoiceWidget,
     DateRangePickerWidget,
-    Select2)
+    Select2,
+)
+from corehq.pillows import utils
+from couchexport.util import SerializableFunction
 
 from crispy_forms.bootstrap import InlineField
 from crispy_forms.helper import FormHelper
@@ -24,20 +39,61 @@ from crispy_forms import layout as crispy
 from crispy_forms.layout import Layout
 from dimagi.utils.dates import DateSpan
 
-USER_MOBILE = 'mobile'
-USER_DEMO = 'demo_user'
-USER_UNKNOWN = 'unknown'
-USER_SUPPLY = 'supply'
+_USER_MOBILE = 'mobile'
+_USER_DEMO = 'demo_user'
+_USER_UNKNOWN = 'unknown'
+_USER_SUPPLY = 'supply'
+
+
+def _get_filtered_users(domain, user_types):
+    user_types = _user_type_choices_to_es_user_types(user_types)
+    user_filter_toggles = [
+        _USER_MOBILE in user_types,
+        _USER_DEMO in user_types,
+        # The following line results in all users who match the
+        # HQUserType.ADMIN filter to be included if the unknown users
+        # filter is selected.
+        _USER_UNKNOWN in user_types,
+        _USER_UNKNOWN in user_types,
+        _USER_SUPPLY in user_types
+    ]
+    # todo refactor HQUserType
+    user_filters = HQUserType._get_manual_filterset(
+        (True,) * HQUserType.count,
+        user_filter_toggles
+    )
+    return users_matching_filter(domain, user_filters)
+
+
+def _user_type_choices_to_es_user_types(choices):
+    """
+    Return a list of elastic search user types (each item in the return list
+    is in corehq.pillows.utils.USER_TYPES) corresponding to the selected
+    export user types.
+    """
+    es_user_types = []
+    export_to_es_user_types_map = {
+        _USER_MOBILE: [utils.MOBILE_USER_TYPE],
+        _USER_DEMO: [utils.DEMO_USER_TYPE],
+        _USER_UNKNOWN: [
+            utils.UNKNOWN_USER_TYPE, utils.SYSTEM_USER_TYPE, utils.WEB_USER_TYPE
+        ],
+        _USER_SUPPLY: [utils.COMMCARE_SUPPLY_USER_TYPE]
+    }
+    for type_ in choices:
+        es_user_types.extend(export_to_es_user_types_map[type_])
+    return es_user_types
 
 
 class UserTypesField(forms.MultipleChoiceField):
 
     _USER_TYPES_CHOICES = [
-        (USER_MOBILE, ugettext_lazy("All Mobile Workers")),
-        (USER_DEMO, ugettext_lazy("Demo User")),
-        (USER_UNKNOWN, ugettext_lazy("Unknown Users")),
-        (USER_SUPPLY, ugettext_lazy("CommCare Supply")),
+        (_USER_MOBILE, ugettext_lazy("All Mobile Workers")),
+        (_USER_DEMO, ugettext_lazy("Demo User")),
+        (_USER_UNKNOWN, ugettext_lazy("Unknown Users")),
+        (_USER_SUPPLY, ugettext_lazy("CommCare Supply")),
     ]
+
     widget = Select2MultipleChoiceWidget
 
     def __init__(self, *args, **kwargs):
@@ -188,6 +244,139 @@ class CreateExportTagForm(forms.Form):
             # Require case_type if model_type is case
             if not cleaned_data.get('case_type'):
                 raise forms.ValidationError(_("case type is required"))
+
+
+class BaseExportFilterBuilder(object):
+    """
+    A class for building export filters.
+    Instantiate with selected filter options, and get the corresponding export filters with get_filters()
+    """
+    def __init__(self, domain, timezone, type_or_group, group, user_types, date_interval):
+        """
+        :param domain:
+        :param timezone:
+        :param type_or_group:
+        :param group:
+        :param user_types:
+        :param date_interval: A DateSpan or DatePeriod
+        """
+        self.domain = domain
+        self.timezone = timezone
+        self.type_or_group = type_or_group
+        self.group = group
+        self.user_types = user_types
+        self.date_interval = date_interval
+
+    def get_filter(self):
+        raise NotImplementedError
+
+
+class ESFormExportFilterBuilder(BaseExportFilterBuilder):
+
+    def get_filter(self):
+        return filter(None, [
+            self._get_datespan_filter(),
+            self._get_group_filter(),
+            self._get_user_filter()
+        ])
+
+    def _get_datespan_filter(self):
+        if self.date_interval and (not hasattr(self.date_interval, "is_valid") or self.date_interval.is_valid()):
+            try:
+                self.date_interval.set_timezone(self.timezone)
+            except AttributeError:
+                # Some date_intervals (e.g. DatePeriod instances) don't have a set_timezone method.
+                pass
+            return ReceivedOnRangeFilter(gte=self.date_interval.startdate, lt=self.date_interval.enddate + timedelta(days=1))
+
+    def _get_group_filter(self):
+        if self.group and self.type_or_group == "group":
+            return GroupFormSubmittedByFilter(self.group)
+
+    def _get_user_filter(self):
+        if self.user_types and self.type_or_group == "users":
+            return UserTypeFilter(_user_type_choices_to_es_user_types(self.user_types))
+
+
+class CouchFormExportFilterBuilder(BaseExportFilterBuilder):
+
+    def get_filter(self):
+        form_filter = SerializableFunction(app_export_filter, app_id=None)
+        datespan_filter = self._get_datespan_filter()
+        if datespan_filter:
+            form_filter &= datespan_filter
+        form_filter &= self._get_user_or_group_filter()
+        return form_filter
+
+    def _get_user_or_group_filter(self):
+        if self.group:
+            # filter by groups
+            group = Group.get(self.group)
+            return SerializableFunction(group_filter, group=group)
+        # filter by users
+        return SerializableFunction(users_filter, users=_get_filtered_users(self.domain, self.user_types))
+
+    def _get_datespan_filter(self):
+        try:
+            if not self.date_interval.is_valid():
+                return
+            self.date_interval.set_timezone(self.timezone)
+        except AttributeError:
+            pass  # TODO: Explain this
+        return SerializableFunction(datespan_export_filter, datespan=self.date_interval)
+
+
+class ESCaseExportFilterBuilder(BaseExportFilterBuilder):
+
+    def get_filter(self):
+        if self.group:
+            group = Group.get(self.group)
+            user_ids = set(group.get_static_user_ids())
+            case_filter = [OR(
+                OwnerFilter(group._id),
+                OwnerFilter(user_ids),
+                LastModifiedByFilter(user_ids)
+            )]
+        else:
+            case_sharing_groups = [g.get_id for g in Group.get_case_sharing_groups(self.domain)]
+            case_filter = [OR(
+                OwnerTypeFilter(_user_type_choices_to_es_user_types(self.user_types)),
+                OwnerFilter(case_sharing_groups),
+                LastModifiedByFilter(case_sharing_groups)
+            )]
+
+        date_filter = self._get_datespan_filter()
+        if date_filter:
+            case_filter.append(date_filter)
+
+        return case_filter
+
+
+    def _get_datespan_filter(self):
+        try:
+            if not self.date_interval.is_valid():
+                return
+            self.date_interval.set_timezone(self.timezone)
+        except AttributeError:
+            pass  # TODO: Explain this
+        return ModifiedOnRangeFilter(
+            gte=self.date_interval.startdate, lt=self.date_interval.enddate + timedelta(days=1)
+        )
+
+
+class CouchCaseExportFilterBuilder(BaseExportFilterBuilder):
+
+    def get_filter(self):
+        if self.group:
+            group = Group.get(self.group)
+            return SerializableFunction(case_group_filter, group=group)
+        case_sharing_groups = [g.get_id for g in Group.get_case_sharing_groups(self.domain)]
+        return SerializableFunction(
+            case_users_filter,
+            users=_get_filtered_users(self.domain, self.user_types),
+            groups=case_sharing_groups
+        )
+
 
 
 class BaseFilterExportDownloadForm(forms.Form):
@@ -507,7 +696,6 @@ class FilterFormCouchExportDownloadForm(GenericFilterFormExportDownloadForm):
                        args=(self.domain_object.name, export.get_id))
 
     def get_form_filter(self):
-        from corehq.apps.export.filter_builders import CouchFormExportFilterBuilder
         return CouchFormExportFilterBuilder(
             self.domain_object.name,
             self.timezone,
@@ -531,7 +719,6 @@ class FilterFormESExportDownloadForm(GenericFilterFormExportDownloadForm):
                        args=(self.domain_object.name, export._id))
 
     def get_form_filter(self):
-        from corehq.apps.export.filter_builders import ESFormExportFilterBuilder
         return ESFormExportFilterBuilder(
             self.domain_object.name,
             self.timezone,
@@ -562,7 +749,6 @@ class FilterCaseCouchExportDownloadForm(GenericFilterCaseExportDownloadForm):
                        args=(self.domain_object.name, export.get_id))
 
     def get_case_filter(self):
-        from corehq.apps.export.filter_builders import CouchCaseExportFilterBuilder
         return CouchCaseExportFilterBuilder(
             self.domain_object.name,
             self.timezone,
@@ -600,7 +786,6 @@ class FilterCaseESExportDownloadForm(GenericFilterCaseExportDownloadForm):
                        args=(self.domain_object.name, export.get_id))
 
     def get_case_filter(self):
-        from corehq.apps.export.filter_builders import ESCaseExportFilterBuilder
         return ESCaseExportFilterBuilder(
             self.domain_object.name,
             self.timezone,
