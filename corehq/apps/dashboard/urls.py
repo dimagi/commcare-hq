0b19b376a48721b999639430a7ee9425823940d9
Nick Pellegrino <npellegrino@dimagi.com>
2016-11-05 15:51:16 -0400

    remove more use of patterns

--- corehq/apps/dashboard/urls.py
+++ corehq/apps/dashboard/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from corehq.apps.dashboard.views import dashboard_default, DomainDashboardView, NewUserDashboardView
 
-urlpatterns = patterns('corehq.apps.dashboard.views',
+urlpatterns = [
     url(r'^$', dashboard_default, name='dashboard_default'),
     url(r'^project/$', DomainDashboardView.as_view(), name=DomainDashboardView.urlname),
     url(r'^apps/$', NewUserDashboardView.as_view(), name=NewUserDashboardView.urlname),
-)
+]
