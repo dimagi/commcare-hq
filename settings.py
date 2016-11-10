f94dbf672e7c7a3ebf4129014fef271d498fac5b
Kamil Krampa <kkrampa@soldevelo.com>
2016-11-10 13:08:19 +0100

    Added Patient Overview Mobile Report

--- settings.py
+++ settings.py
@@ -1698,6 +1698,7 @@ STATIC_UCR_REPORTS = [
     os.path.join('custom', 'enikshay', 'ucr', 'reports', 'lab_monthly_summary.json'),
     os.path.join('custom', 'enikshay', 'ucr', 'reports', 'tb_lab_register.json'),
     os.path.join('custom', 'enikshay', 'ucr', 'reports', 'new_patient_summary.json'),
+    os.path.join('custom', 'enikshay', 'ucr', 'reports', 'patient_overview_mobile.json')
 ]
 
 
