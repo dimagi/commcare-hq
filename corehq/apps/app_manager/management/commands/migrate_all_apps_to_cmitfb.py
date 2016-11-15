6fa037ae7489e34bc3ec7df9a9c3798a7669f11a
Jennifer Schweers <orange.jenny@gmail.com>
2016-11-15 16:49:20 -0500

    make more concise

--- corehq/apps/app_manager/management/commands/migrate_all_apps_to_cmitfb.py
+++ corehq/apps/app_manager/management/commands/migrate_all_apps_to_cmitfb.py
@@ -1,7 +1,6 @@
 import logging
 from django.core.management import BaseCommand, call_command
 
-from corehq.apps.app_manager.dbaccessors import get_app
 from corehq.apps.es.apps import AppES
 
 
@@ -15,13 +14,8 @@ class Command(BaseCommand):
     '''
 
     def handle(self, *args, **options):
-        # Find all apps not yet using vellum case management
-        app_query = AppES().is_build(False).term('vellum_case_management', False) \
-                           .term('doc_type', 'Application').source(['domain', '_id'])
-
-        hits = app_query.run().hits
-        total = 0
-        for hit in hits:
-            call_command('migrate_app_to_cmitfb', hit['_id'])
-            total = total + 1
-        logger.info('done with migrate_all_apps_to_cmitfb, migrated {} apps'.format(total))
+        ids = AppES().is_build(False).term('vellum_case_management', False) \
+                     .term('doc_type', 'Application').get_ids()
+        for id in ids:
+            call_command('migrate_app_to_cmitfb', id)
+        logger.info('done with migrate_all_apps_to_cmitfb, migrated {} apps'.format(len(ids)))
