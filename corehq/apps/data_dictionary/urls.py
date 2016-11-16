23d65931b52ecefedfa2763e31ed312324f845f0
Jonathan Emord <jonathan.emord@gmail.com>
2016-11-16 14:31:00 -0500

    base template

--- corehq/apps/data_dictionary/urls.py
+++ corehq/apps/data_dictionary/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from corehq.apps.data_dictionary.views import (
     data_dictionary_json,
     generate_data_dictionary,
+    DataDictionaryView,
 )
 
 urlpatterns = [
     url(r"^generate/$", generate_data_dictionary),
     url(r"^json/$", data_dictionary_json),
+    url(r"^$", DataDictionaryView.as_view(), name=DataDictionaryView.urlname)
 ]
--- corehq/apps/data_dictionary/views.py
+++ corehq/apps/data_dictionary/views.py
@@ -1,12 +1,19 @@
 import json
 
-from django.core import serializers
+from django.core.urlresolvers import reverse
 from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _
+
+from dimagi.utils.decorators.memoized import memoized
 
 from corehq import toggles
 from corehq.apps.domain.decorators import login_and_domain_required
+from corehq.apps.domain.views import BaseDomainView
 from corehq.apps.data_dictionary import util
 from corehq.apps.data_dictionary.models import CaseProperty
+from corehq.apps.data_dictionary.models import CaseType
+from corehq.tabs.tabclasses import ApplicationsTab
 
 
 @login_and_domain_required
@@ -31,3 +38,42 @@ def data_dictionary_json(request, domain):
             'type': prop.type,
         })
     return JsonResponse({'properties': props})
+
+
+class DataDictionaryView(BaseDomainView):
+    section_name = _("Data Dictionary")
+    template_name = "data_dictionary/base.html"
+    urlname = 'data_dictionary'
+
+    @method_decorator(login_and_domain_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(DataDictionaryView, self).dispatch(request, *args, **kwargs)
+
+    @property
+    def main_context(self):
+        main_context = super(DataDictionaryView, self).main_context
+        main_context.update({
+            'active_tab': ApplicationsTab(
+                self.request,
+                domain=self.domain,
+                couch_user=self.request.couch_user,
+                project=self.request.project
+            ),
+        })
+        return main_context
+
+    @property
+    def page_context(self):
+        page_context = super(DataDictionaryView, self).page_context
+        case_types = [
+            c.name for c in CaseType.objects.filter(domain=self.request.domain).all()
+        ]
+        page_context.update({
+            'case_types': case_types,
+        })
+        return page_context
+
+    @property
+    @memoized
+    def section_url(self):
+        return reverse(DataDictionaryView.urlname, args=[self.domain])
