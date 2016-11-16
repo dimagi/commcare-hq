7e501a28a5fb500b23188990a5e2df9c38010c2b
Ben Rudolph <brudolph@dimagi.com>
2016-11-16 17:51:43 -0500

    Remove refresh button

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/layout/views/phone_navigation.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/layout/views/phone_navigation.js
@@ -10,19 +10,8 @@ FormplayerFrontend.module("Layout.Views", function (Views, FormplayerFrontend, B
     Views.PhoneNavigation = Marionette.ItemView.extend({
         className: 'formplayer-phone-navigation',
         template: '#formplayer-phone-navigation-template',
-        ui: {
-            reloadButton: '.formplayer-reload',
-        },
-        events: {
-            'click @ui.backButton': 'onBack',
-            'click @ui.reloadButton': 'onReload',
-        },
         initialize: function(options) {
             this.appId = options.appId;
-        },
-        onReload: function(e) {
-            e.preventDefault();
-            FormplayerFrontend.trigger('refreshApplication', this.appId);
         },
     });
 });
