2a5cc23be491fa3f42fe039b421ad436a94d59c2
Giovanni Capalbo <gcapalbo@dimagi.com>
2016-11-23 15:30:23 -0500

    Update SMSGH Backend view to be NewIncomingBackendView

--- corehq/messaging/smsbackends/smsgh/views.py
+++ corehq/messaging/smsbackends/smsgh/views.py
@@ -1,11 +1,15 @@
 from corehq.apps.sms.api import incoming
-from corehq.apps.sms.views import IncomingBackendView
+from corehq.apps.sms.views import NewIncomingBackendView
 from corehq.messaging.smsbackends.smsgh.models import SQLSMSGHBackend
 from django.http import HttpResponse, HttpResponseBadRequest
 
 
-class SMSGHIncomingView(IncomingBackendView):
+class SMSGHIncomingView(NewIncomingBackendView):
     urlname = 'smsgh_sms_in'
+
+    @property
+    def backend_class(self):
+        return SQLSMSGHBackend
 
     def get(self, request, api_key, *args, **kwargs):
         msg = request.GET.get('msg', None)
@@ -17,7 +21,7 @@ class SMSGHIncomingView(IncomingBackendView):
         if not msg or not snr:
             return HttpResponseBadRequest("ERROR: Missing msg or snr")
 
-        incoming(snr, msg, SQLSMSGHBackend.get_api_id())
+        incoming(snr, msg, SQLSMSGHBackend.get_api_id(), domain_scope=self.domain)
         return HttpResponse("")
 
     def post(self, request, api_key, *args, **kwargs):
