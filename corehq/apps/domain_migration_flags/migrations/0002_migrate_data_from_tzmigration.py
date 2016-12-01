dc308677f47ecf367b309533502115896494b4e1
Simon Kelly <skelly@dimagi.com>
2016-12-01 14:43:37 +0200

    add generic migration flag model

---
+++ corehq/apps/domain_migration_flags/migrations/0002_migrate_data_from_tzmigration.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('domain_migration_flags', '0001_initial')
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            """
+            INSERT INTO domain_migration_flags_domainmigrationprogress (domain, migration_slug, migration_status)
+            SELECT domain, 'tzmigration' as migration_slug, migration_status
+            FROM tzmigration_timezonemigrationprogress
+            """,
+            ""
+        )
+    ]
