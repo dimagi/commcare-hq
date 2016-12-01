1c269e2d6ac407443b4c1abc0cdfb0beea0c3e9f
Jonathan Emord <jonathan.emord@gmail.com>
2016-12-01 16:39:01 -0500

    Extract add to db

--- corehq/apps/data_dictionary/util.py
+++ corehq/apps/data_dictionary/util.py
@@ -5,27 +5,8 @@ from corehq.apps.export.models.new import CaseExportDataSchema
 
 
 def generate_data_dictionary(domain):
-    current_case_types, current_properties = _get_current_case_types_and_properties(domain)
-
     properties = _get_all_case_properties(domain)
-    new_case_properties = []
-    for case_type, props in properties.items():
-        if not case_type:
-            continue
-
-        if case_type in current_case_types:
-            case_type_obj = current_case_types[case_type]
-        else:
-            case_type_obj = CaseType.objects.create(domain=domain, name=case_type)
-
-        for prop in props:
-            if (case_type not in current_properties or
-                    prop not in current_properties[case_type]):
-                new_case_properties.append(CaseProperty(
-                    case_type=case_type_obj, name=prop
-                ))
-
-    CaseProperty.objects.bulk_create(new_case_properties)
+    _create_properties_for_case_types(domain, properties)
 
 
 def _get_all_case_properties(domain):
@@ -69,16 +50,30 @@ def _get_current_case_types_and_properties(domain):
 
 
 def add_properties_to_data_dictionary(domain, case_type, properties):
-    case_type_obj, created = CaseType.objects.get_or_create(domain=domain, name=case_type)
-    if not created:
-        old_properties = set(CaseProperty.objects.filter(case_type=case_type_obj).values_list('name', flat=True))
-    else:
-        old_properties = set()
-    new_properties = []
+    if not properties:
+        return
 
-    for case_property in properties:
-        if case_property not in old_properties:
-            old_properties.add(case_property)
-            new_properties.append(CaseProperty(name=case_property, case_type=case_type_obj))
+    new_case_properties = _create_properties_for_case_types(domain, {case_type: properties})
 
-    CaseProperty.objects.bulk_create(new_properties)
+
+def _create_properties_for_case_types(domain, case_type_to_prop):
+    current_case_types, current_properties = _get_current_case_types_and_properties(domain)
+    new_case_properties = []
+
+    for case_type, props in case_type_to_prop.items():
+        if not case_type:
+            continue
+
+        if case_type in current_case_types:
+            case_type_obj = current_case_types[case_type]
+        else:
+            case_type_obj = CaseType.objects.create(domain=domain, name=case_type)
+
+        for prop in props:
+            if (case_type not in current_properties or
+                    prop not in current_properties[case_type]):
+                new_case_properties.append(CaseProperty(
+                    case_type=case_type_obj, name=prop
+                ))
+
+    CaseProperty.objects.bulk_create(new_case_properties)
