8e67fab132ea4458fbcc9de7089775d4ce085440
Daniel Roberts <droberts@dimagi.com>
2016-12-07 16:40:25 -0500

    make case importer filestorage save filename
    and length for persistent

--- corehq/apps/case_importer/tracking/tests/test_filestorage.py
+++ corehq/apps/case_importer/tracking/tests/test_filestorage.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from corehq.apps.case_importer.tracking.filestorage import transient_file_store, persistent_file_store
+from corehq.util.files import file_extention_from_filename
 
 
 class FilestorageTest(TestCase):
@@ -17,18 +18,18 @@ class FilestorageTest(TestCase):
 
     def test_transient(self):
         with open(self.filename, 'r') as f:
-            identifier = transient_file_store.write_file(f)
+            identifier = transient_file_store.write_file(f, 'test_file.txt')
 
-        tmpfile = transient_file_store.get_filename(identifier, 'txt')
-
+        tmpfile = transient_file_store.get_tempfile(identifier)
+        self.assertEqual(file_extention_from_filename(tmpfile), '.txt')
         with open(tmpfile) as f:
             self.assertEqual(f.read(), self.content)
 
     def test_persistent(self):
         with open(self.filename, 'r') as f:
-            identifier = persistent_file_store.write_file(f)
+            identifier = persistent_file_store.write_file(f, 'test_file.txt')
 
-        tmpfile = persistent_file_store.get_filename(identifier, 'txt')
-
+        tmpfile = persistent_file_store.get_tempfile(identifier)
+        self.assertEqual(file_extention_from_filename(tmpfile), '.txt')
         with open(tmpfile) as f:
             self.assertEqual(f.read(), self.content)
