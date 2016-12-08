f648c9fde375af5db99fd75318c46b5f45b6253f
sheelio <sshah@dimagi.com>
2016-12-08 15:33:59 -0500

    Change aggregation columns to nullable

--- custom/icds_reports/migrations/sql_templates/update_tables.sql
+++ custom/icds_reports/migrations/sql_templates/update_tables.sql
@@ -1,2 +1,16 @@
--- Remove not null constraint on age tranche (data is incomplete)
+-- Remove not null constraint on aggregated columns (data is sometimes null)
 ALTER TABLE agg_child_health ALTER COLUMN age_tranche DROP NOT NULL;
+ALTER TABLE agg_child_health ALTER COLUMN disabled DROP NOT NULL;
+ALTER TABLE agg_child_health ALTER COLUMN resident DROP NOT NULL;
+ALTER TABLE agg_child_health ALTER COLUMN caste DROP NOT NULL;
+ALTER TABLE agg_child_health ALTER COLUMN minority DROP NOT NULL;
+ALTER TABLE agg_child_health ALTER COLUMN gender DROP NOT NULL;
+ALTER TABLE agg_ccs_record ALTER COLUMN registration_trimester_at_delivery DROP NOT NULL;
+ALTER TABLE agg_ccs_record ALTER COLUMN disabled DROP NOT NULL;
+ALTER TABLE agg_ccs_record ALTER COLUMN resident DROP NOT NULL;
+ALTER TABLE agg_ccs_record ALTER COLUMN caste DROP NOT NULL;
+ALTER TABLE agg_ccs_record ALTER COLUMN minority DROP NOT NULL;
+ALTER TABLE agg_thr_data ALTER COLUMN disabled DROP NOT NULL;	
+ALTER TABLE agg_thr_data ALTER COLUMN resident DROP NOT NULL;
+ALTER TABLE agg_thr_data ALTER COLUMN caste DROP NOT NULL;
+ALTER TABLE agg_thr_data ALTER COLUMN minority DROP NOT NULL;
