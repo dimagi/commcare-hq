34ca89290c4fac855bcad0e52b5f60366fe0901b
Daniel Roberts <droberts@dimagi.com>
2016-12-09 14:41:13 -0500

    split import history frontend into separate files

---
+++ corehq/apps/case_importer/static/case_importer/js/import_history.js
@@ -0,0 +1,43 @@
+hqDefine('case_importer/js/import_history.js', function () {
+    var urllib = hqImport('hqwebapp/js/urllib.js');
+    function RecentUploads() {
+        var self = {
+            getCaseUploadFileDownloadResource: function (upload_id) {
+                return urllib.reverse('case_importer_upload_file_download', upload_id);
+            },
+        };
+        // this is used both for the state of the ajax request
+        // and for the state of celery task
+        self.states = {
+            NOT_STARTED: 0,
+            STARTED: 1,
+            SUCCESS: 2,
+            FAILED: 3,
+        };
+        self.case_uploads = ko.observableArray(null);
+        self.state = ko.observable(self.states.NOT_STARTED);
+        self.fetchCaseUploads = function () {
+            if (self.state() === self.states.NOT_STARTED) {
+                // only show spinner on first fetch
+                self.state(self.states.STARTED);
+            }
+            $.get(urllib.reverse('case_importer_uploads'), {limit: urllib.getUrlParameter('limit')}).success(function (data) {
+                self.state(self.states.SUCCESS);
+                self.case_uploads(data);
+                var anyInProgress = _.any(self.case_uploads(), function (case_upload) {
+                    return case_upload.task_status.state === self.states.STARTED ||
+                            case_upload.task_status.state === self.states.NOT_STARTED;
+                });
+                if (anyInProgress) {
+                    _.delay(self.fetchCaseUploads, 5000);
+                }
+            }).error(function () {
+                self.state(self.states.FAILED);
+            });
+        };
+        return self;
+    }
+    return {
+        RecentUploads: RecentUploads
+    };
+});
