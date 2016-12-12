8650a581be4c4f497792186286034df8d0b479d1
Daniel Roberts <droberts@dimagi.com>
2016-12-12 09:48:37 -0500

    simplify case importer upload history url

--- corehq/apps/case_importer/urls.py
+++ corehq/apps/case_importer/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     url(r'^excel/fields/$', excel_fields, name='excel_fields'),
     url(r'^excel/commit/$', excel_commit, name='excel_commit'),
     url(r'^history/uploads/$', case_uploads, name='case_importer_uploads'),
-    # allow '---' as a dummy value to be able to reverse url in javascript w/o upload_id
-    url(r'^history/uploads/(?P<upload_id>---|[-0-9a-f]{36})/$', case_upload_file,
+    url(r'^history/uploads/(?P<upload_id>[\w-]+)/$', case_upload_file,
         name='case_importer_upload_file_download'),
 ]
