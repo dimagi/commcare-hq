5836396da83f9d3584bdef0bba71b78487aedab4
Nick Pellegrino <npellegrino@dimagi.com>
2016-12-15 11:10:42 -0500

    add TODO

--- custom/enikshay/nikshay_datamigration/management/commands/cleanup_followup_csv.py
+++ custom/enikshay/nikshay_datamigration/management/commands/cleanup_followup_csv.py
@@ -30,6 +30,7 @@ class Command(BaseCleanupCommand):
                 new_row[1] = new_row[1][3:]
             new_row[1] = new_row[1].strip()
 
+            # TODO - figure out what's wrong with these rows
             if len(new_row) > len(new_rows[0]) and i in [1464815, 1484488]:
                 continue
             if any(id in new_row[1] for id in exclude_ids):
