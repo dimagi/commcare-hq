e531e06896c99c38e6b8e16c157e2e7fbab9cd45
Daniel Roberts <droberts@dimagi.com>
2016-12-15 13:58:45 -0500

    document case importer special fields in code
    and switch to using that, without changing current functionality

---
+++ corehq/apps/case_importer/suggested_fields.py
@@ -0,0 +1,62 @@
+from django.utils.translation import ugettext as _
+from corehq.apps.case_importer.util import get_case_properties_for_case_type, \
+    RESERVED_FIELDS
+from dimagi.ext import jsonobject
+
+
+def get_suggested_case_fields(domain, case_type, exclude=None):
+    exclude_fields = set(RESERVED_FIELDS)
+    if exclude:
+        exclude_fields.update(exclude)
+    dynamic_fields = set(get_case_properties_for_case_type(domain, case_type)) - exclude_fields
+    return sorted(
+        get_special_fields() +
+        [FieldSpec(field=field, show_in_menu=True) for field in dynamic_fields],
+        key=lambda field_spec: field_spec.field
+    )
+
+
+class FieldSpec(jsonobject.StrictJsonObject):
+    field = jsonobject.StringProperty()
+    description = jsonobject.StringProperty()
+    show_in_menu = jsonobject.BooleanProperty(default=False)
+    discoverable = jsonobject.BooleanProperty(default=True)
+
+
+def get_special_fields():
+    return [
+        FieldSpec(
+            field='name',
+            description=_("This field will used to set the case's name.")),
+        FieldSpec(
+            field='owner_name',
+            description=_("This field will assign the case to a new owner given by "
+                          "Username, Group name, or Organization name."),
+        ),
+        FieldSpec(
+            field='owner_id',
+            description=_("This field will assign the case to a new owner given by "
+                          "User ID, Group ID, or Organization ID.")),
+        FieldSpec(
+            field='parent_external_id',
+            description=_("This field will assign the case a new parent given by "
+                          "the parent case's external_id. "
+                          "You must use along with parent_type.")),
+        FieldSpec(
+            field='parent_id',
+            description=_("This field will assign the case a new parent given by "
+                          "the parent's Case ID. "
+                          "You must use along with parent_type.")),
+        FieldSpec(
+            field='parent_type',
+            description=_("Use to specify the parent's case type. "
+                          "Usually used with parent_id or parent_external_id")),
+        FieldSpec(
+            field='parent_ref',
+            description=_("This is a deprecated feature needed for a handful of clients. "
+                          "Please do not use unless you know what you are doing")),
+        FieldSpec(
+            field='close',
+            description=_("This field will be used to close cases. "
+                          "Any case with 'yes' in this column will be closed.")),
+    ]
--- corehq/apps/case_importer/views.py
+++ corehq/apps/case_importer/views.py
@@ -6,9 +6,10 @@ from corehq.apps.case_importer import base
 from corehq.apps.case_importer import util as importer_util
 from corehq.apps.case_importer.exceptions import ImporterError
 from django.views.decorators.http import require_POST
+from corehq.apps.case_importer.suggested_fields import get_suggested_case_fields
 from corehq.apps.case_importer.tracking.case_upload_tracker import CaseUpload
 
-from corehq.apps.case_importer.util import get_case_properties_for_case_type, get_importer_error_message
+from corehq.apps.case_importer.util import get_importer_error_message
 from corehq.apps.users.decorators import require_permission
 from corehq.apps.users.models import Permissions
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
@@ -195,24 +196,15 @@ def excel_fields(request, domain):
         else:
             excel_fields = columns
 
-    case_fields = get_case_properties_for_case_type(domain, case_type)
-
     # hide search column and matching case fields from the update list
-    try:
-        excel_fields.remove(search_column)
-    except:
-        pass
+    if search_field in excel_fields:
+        excel_fields.remove(search_field)
 
-    try:
-        case_fields.remove(search_field)
-    except:
-        pass
+    field_specs = get_suggested_case_fields(
+        domain, case_type, exclude=[search_column, search_field])
 
-    # we can't actually update this so don't show it
-    try:
-        case_fields.remove('type')
-    except:
-        pass
+    case_fields_in_menu = [field_spec.field for field_spec in field_specs
+                           if field_spec.show_in_menu]
 
     return render(
         request,
@@ -226,7 +218,7 @@ def excel_fields(request, domain):
             'value_column': value_column,
             'columns': columns,
             'excel_fields': excel_fields,
-            'case_fields': case_fields,
+            'case_fields': case_fields_in_menu,
             'domain': domain,
             'report': {
                 'name': 'Import: Match columns to fields'
