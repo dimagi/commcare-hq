9bff23633bb4e2fac43e53734bf3a772d99d9423
Daniel Roberts <droberts@dimagi.com>
2016-12-15 08:55:39 -0500

    make datadog_gauge_task warn when not prefixed with 'commcare.'
    and fail hard in DEBUG. Also provide easy escape hatch for when you
    really do want to skip that prefix. The error message looks like this:
    
    AssertionError: Did you mean to call your gauge
    'commcare.case_importer.files.total_bytes'? If you're sure you want to
    forgo the prefix, you can pass enforce_prefix=None

--- corehq/util/datadog/gauges.py
+++ corehq/util/datadog/gauges.py
@@ -1,9 +1,10 @@
 from functools import wraps
 from celery.task import periodic_task
 from corehq.util.datadog import statsd
+from corehq.util.soft_assert import soft_assert
 
 
-def datadog_gauge_task(name, fn, run_every):
+def datadog_gauge_task(name, fn, run_every, enforce_prefix='commcare'):
     """"
     helper for easily registering datadog gauges to run periodically
 
@@ -14,6 +15,11 @@ def datadog_gauge_task(name, fn, run_every):
                                             run_every=crontab(minute=0))
 
     """
+    soft_assert(fail_if_debug=True).call(
+        not enforce_prefix or name.split('.')[0] == enforce_prefix,
+        "Did you mean to call your gauge 'commcare.{}'? "
+        "If you're sure you want to forgo the prefix, you can "
+        "pass enforce_prefix=None".format(name))
 
     datadog_gauge = _DatadogGauge(name, fn, run_every)
     return datadog_gauge.periodic_task()
