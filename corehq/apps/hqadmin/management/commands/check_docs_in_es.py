a12964c01501ba0dfac6ef69135090c3aedd0989
Jonathan Emord <jonathan.emord@gmail.com>
2016-12-16 11:17:46 -0500

    first pass at es reliability script

---
+++ corehq/apps/hqadmin/management/commands/check_docs_in_es.py
@@ -0,0 +1,57 @@
+import csv
+from django.core.management import BaseCommand
+
+from corehq.apps.app_manager.dbaccessors import get_app_ids_in_domain
+from corehq.apps.domain.calculations import cases, forms
+from corehq.apps.domain.models import Domain
+from corehq.apps.es.apps import AppES
+from corehq.apps.es.domains import DomainES
+from corehq.apps.es.groups import GroupES
+from corehq.apps.es.users import UserES
+from corehq.apps.groups.dbaccessors import get_group_ids_by_domain
+from corehq.apps.users.dbaccessors.all_commcare_users import get_all_user_ids_by_domain
+
+
+class Command(BaseCommand):
+    help = "Compares the number of documents in ES to primary DB to detect inconsistency"
+
+    def add_arguments(self, parser):
+        parser.add_argument('--filename', dest='filename', default='es_reliability.csv')
+
+    def handle(self, *args, **options):
+        num_es_domains = DomainES().count()
+        num_couch_domains = Domain.get_db().view("domain/domains").all()[0]['value']
+
+        if num_es_domains != num_couch_domains:
+            print str(num_es_domains) + " != " + str(num_couch_domains)
+
+        with open(options['filename'], 'w') as csvfile:
+            self.check_domains(csv.writer(csvfile))
+
+    def check_domains(self, csvfile):
+        domains = DomainES().fields(["name", "_id", "cp_last_updated"]).scroll()
+        for domain in domains:
+            self.check_domain(domain['name'], csvfile)
+
+    def check_domain(self, domain, csvfile):
+        num_cases_es = cases(domain)
+        num_forms_es = forms(domain)
+        num_apps_es = AppES().domain(domain).count()
+        num_users_es = UserES().domain(domain).count()
+        num_groups_es = GroupES().domain(domain).count()
+        num_cases_primary = cases(domain, primary_db=True)
+        num_forms_primary = forms(domain, primary_db=True)
+        num_apps_primary = len(get_app_ids_in_domain(domain))
+        num_users_primary = len(list(get_all_user_ids_by_domain(domain)))
+        num_groups_primary = len(list(get_group_ids_by_domain(domain)))
+
+        if num_cases_es != num_cases_primary:
+            csvfile.writerow([domain, 'cases', num_cases_es, num_cases_primary])
+        if num_forms_es != num_forms_primary:
+            csvfile.writerow([domain, 'forms', num_forms_es, num_forms_primary])
+        if num_apps_es != num_apps_primary:
+            csvfile.writerow([domain, 'apps', num_apps_es, num_apps_primary])
+        if num_users_es != num_users_primary:
+            csvfile.writerow([domain, 'users', num_users_es, num_users_primary])
+        if num_groups_es != num_groups_primary:
+            csvfile.writerow([domain, 'groups', num_groups_es, num_groups_primary])
