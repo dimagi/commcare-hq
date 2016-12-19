caa49f22975c1230d1ad69de945b5387a35173ce
Daniel Roberts <droberts@dimagi.com>
2016-12-19 15:51:53 -0500

    fix case importer migrations

---
+++ corehq/apps/case_importer/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('case_importer', '0008_caseuploadrecord_task_status_json'),
+        ('case_importer', '0008_merge'),
+    ]
+
+    operations = [
+    ]
