6a819e397b7a2c6ffd6181001876296504dd7358
Kamil Krampa <kkrampa@soldevelo.com>
2016-12-20 15:42:52 +0100

    Added test

--- corehq/apps/zapier/api/v0_5.py
+++ corehq/apps/zapier/api/v0_5.py
@@ -1,20 +1,9 @@
 from corehq.apps.api.resources.v0_4 import XFormInstanceResource
-from corehq.apps.export.system_properties import MAIN_FORM_TABLE_PROPERTIES
+from corehq.apps.zapier.util import remove_advanced_fields
 
 
 class ZapierXFormInstanceResource(XFormInstanceResource):
 
     def dehydrate(self, bundle):
-        for form_property in MAIN_FORM_TABLE_PROPERTIES:
-            if not form_property.is_advanced:
-                continue
-            element = bundle.data
-            for node in form_property.item.path[:-1]:
-                element = element.get(node.name)
-                if not element:
-                    break
-
-            key_to_delete = form_property.item.path[-1].name
-            if element and key_to_delete in element:
-                del element[key_to_delete]
+        remove_advanced_fields(bundle.data)
         return bundle
