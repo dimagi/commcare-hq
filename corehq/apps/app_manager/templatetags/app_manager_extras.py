6a53898ff86348cbb316f579623a710f6682e02e
Cory Zue <czue@dimagi.com>
2016-12-26 14:30:02 +0200

    [ci skip] lint

--- corehq/apps/app_manager/templatetags/app_manager_extras.py
+++ corehq/apps/app_manager/templatetags/app_manager_extras.py
@@ -8,7 +8,7 @@ register = template.Library()
 def get_available_modules_for_case_list_configuration(app, module):
     return [
         m for m in app.get_modules()
-        if m.id != module.id
-           and m.module_type not in ('reports', 'careplan')
-           and m.case_type == module.case_type
+        if (m.id != module.id
+            and m.module_type not in ('reports', 'careplan')
+            and m.case_type == module.case_type)
     ]
