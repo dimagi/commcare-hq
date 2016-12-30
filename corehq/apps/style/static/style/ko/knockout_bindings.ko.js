6b1e84226cc933220b82d953997148bea3aea225
Jenny Schweers <orange.jenny@gmail.com>
2016-12-30 15:21:59 -0500

    might as well lint

--- corehq/apps/style/static/style/ko/knockout_bindings.ko.js
+++ corehq/apps/style/static/style/ko/knockout_bindings.ko.js
@@ -7,7 +7,7 @@ ko.bindingHandlers.hqbSubmitReady = {
             $(element).addClass("btn-primary").removeClass("disabled");
         else
             $(element).addClass("disabled").removeClass("btn-primary");
-    }
+    },
 };
 
 ko.bindingHandlers.fadeVisible = {
@@ -19,7 +19,7 @@ ko.bindingHandlers.fadeVisible = {
     update: function(element, valueAccessor) {
         var value = valueAccessor();
         ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
-    }
+    },
 };
 
 ko.bindingHandlers.fadeVisibleInOnly = {
@@ -31,7 +31,7 @@ ko.bindingHandlers.fadeVisibleInOnly = {
     update: function(element, valueAccessor) {
         var value = valueAccessor();
         ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).hide();
-    }
+    },
 };
 
 ko.bindingHandlers.staticChecked = {
