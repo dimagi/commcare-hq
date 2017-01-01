52e6242814a0778195e2a9088bd989298084f3ad
Manish Kangia <mkangia@dimagi.com>
2017-01-01 17:30:44 +0530

    Avoid empty result for an unrestricted user for blank filter

--- corehq/apps/reports/standard/inspect.py
+++ corehq/apps/reports/standard/inspect.py
@@ -112,8 +112,7 @@ class SubmitHistoryMixin(ElasticProjectInspectionReport,
                  .domain(self.domain)
                  .filter(time_filter(gte=self.datespan.startdate,
                                      lt=self.datespan.enddate_adjusted)))
-        if self.request.can_access_all_locations or (not self.request.can_access_all_locations
-                                                     and filter(None, mobile_user_and_group_slugs)):
+        if filter(None, mobile_user_and_group_slugs):
             query = query.filter(self._get_users_filter(mobile_user_and_group_slugs))
 
         if not self.request.can_access_all_locations:
