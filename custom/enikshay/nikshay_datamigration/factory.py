07a8b290581c4bee5fb2744a6075baed7abce85a
Nick Pellegrino <npellegrino@dimagi.com>
2017-01-04 11:54:58 -0500

    set adherence_schedule_date_start to treatment_initiation_date

--- custom/enikshay/nikshay_datamigration/factory.py
+++ custom/enikshay/nikshay_datamigration/factory.py
@@ -204,6 +204,7 @@ class EnikshayCaseFactory(object):
                 'date_opened': self.patient_detail.pregdate1,
                 'owner_id': '-',
                 'update': {
+                    'adherence_schedule_date_start': self.patient_detail.treatment_initiation_date,
                     'date_of_diagnosis': self.patient_detail.pregdate1,
                     'date_of_mo_signature': self.patient_detail.date_of_mo_signature,
                     'disease_classification': self.patient_detail.disease_classification,
