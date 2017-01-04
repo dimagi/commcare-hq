0d04891f525838c4f4e1c53aa9922d35deb2a907
Manish Kangia <mkangia@dimagi.com>
2017-01-04 16:08:41 +0530

    Avoid inheriting CaseExportScope and rely on CaseListMixin only as the changes in former are now absorbed in latter

--- corehq/apps/data_interfaces/interfaces.py
+++ corehq/apps/data_interfaces/interfaces.py
@@ -16,7 +16,7 @@ from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
 from corehq.apps.reports.generic import GenericReportView
 from corehq.apps.reports.models import HQUserType
 from corehq.apps.reports.standard import ProjectReport
-from corehq.apps.reports.standard.cases.basic import CaseListMixin, CaseExportScope
+from corehq.apps.reports.standard.cases.basic import CaseListMixin
 from corehq.apps.reports.standard.cases.data_sources import CaseDisplay
 from corehq.apps.reports.standard.inspect import SubmitHistoryMixin
 from corehq.apps.reports.filters.base import BaseSingleOptionFilter
@@ -38,7 +38,7 @@ class DataInterface(GenericReportView):
 
 
 @location_safe
-class CaseReassignmentInterface(CaseExportScope, CaseListMixin, DataInterface):
+class CaseReassignmentInterface(CaseListMixin, DataInterface):
     name = ugettext_noop("Reassign Cases")
     slug = "reassign_cases"
     report_template_path = 'data_interfaces/interfaces/case_management.html'
