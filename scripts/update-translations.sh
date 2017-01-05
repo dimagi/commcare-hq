4237cc403c592199a135a308d4a23d37a2ee2ed5
Ethan Soergel <esoergel@gmail.com>
2017-01-05 15:55:23 -0500

    Handle failures automatically

--- scripts/update-translations.sh
+++ scripts/update-translations.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -euo pipefail  # bash strict mode - exit on hard failures
 
 # Based on this confluence page:
 # https://confluence.dimagi.com/display/commcarehq/Internationalization+and+Localization+-+Transifex+Translations
 
 function abort () {
-    echo $1
     echo "Aborting."
     exit 1
 }
@@ -32,38 +32,31 @@ fi
 echo "Pulling translations from transifex"
 tx pull -f
 
-if [[ $? -ne "0" ]]; then
-    abort "Pulling from transifex failed."
-fi
-
 echo "Gathering all translation strings.  Note that this will probably take a while"
 ./manage.py makemessages --all --ignore 'corehq/apps/app_manager/tests/data/v2_diffs*' --ignore 'node_modules'
-
-if [[ $? -ne "0" ]]; then
-    abort "Looks like there's a problem running makemessages, you should probably fix it."
-fi
 
 echo "Gathering javascript translation strings.  This will also probably take a while"
 ./manage.py makemessages -d djangojs --all
 
-if [[ $? -ne "0" ]]; then
-    abort "Looks like there's a problem translating the javascript strings, you should probably fix it."
-fi
-
+set +e   # Temporarily disable strict mode so we can respond any failure
 echo "Compiling translation files."
 ./manage.py compilemessages
-
 if [[ $? -ne "0" ]]; then
-    git checkout -- locale/
-    abort "./manage.py compilemessages failed.  This is probably due to a bad translation, maybe a translator messed up a format string or something.  Make an interrupt ticket or something to figure it out."
+    echo ""
+    echo "'./manage.py compilemessages' failed.  This is probably due to a bad translation, maybe a translator messed up a format string or something."
+    echo "If the deploy is urgent, it might be best to drop the changes to locale/ and deploy without updating translations, then assign an interrupt ticket to fix."
+    echo "The output above from compilemessages should have specifics about the locale/ file and line where the error(s) occurred."
+    echo "1. Open those files, go to the appropriate lines and fix the errors."
+    echo "2. Run './manage.py compilemessages' again to make sure it's fixed."
+    echo "3. Run 'tx push -st' to update them on transifex."
+    echo "4. Now if you do 'git checkout -- locale' and run this script again, it should succeed."
+    echo "5. You should probably also reach out to the translator to make sure they don't mess this up again."
+    abort
 fi
+set -e
 
 echo "Pushing updates to transifex."
 tx push -st
-
-if [[ $? -ne "0" ]]; then
-    abort "Pushing to transifex failed."
-fi
 
 has_local_changes=`git diff-index HEAD` && true
 if [[ ! $has_local_changes ]]
