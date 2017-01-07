603528d3d3363047c987ba80e88f16af10fe5591
Cory Zue <czue@dimagi.com>
2017-01-07 10:21:24 +0200

    more tests

--- corehq/apps/hqwebapp/tests/test_proptable_tags.py
+++ corehq/apps/hqwebapp/tests/test_proptable_tags.py
@@ -4,6 +4,17 @@ from corehq.apps.hqwebapp.templatetags.proptable_tags import get_display_data
 
 class CaseDisplayDataTest(SimpleTestCase):
 
+    def test_get_display_data_name(self):
+        column = {
+            'name': 'favorite color',
+            'expr': 'color'
+        }
+        data = {
+            'color': 'red'
+        }
+        self.assertEqual(get_display_data(data, column),
+                         {'expr': 'color', 'name': 'favorite color', 'value': 'red'})
+
     def test_get_display_data_no_name(self):
         column = {
             'expr': 'color'
@@ -12,3 +23,15 @@ class CaseDisplayDataTest(SimpleTestCase):
             'color': 'red'
         }
         self.assertEqual(get_display_data(data, column), {'expr': 'color', 'name': 'color', 'value': 'red'})
+
+    def test_get_display_data_function(self):
+        get_color = lambda x: x['color']
+        column = {
+            'name': 'favorite color',
+            'expr': get_color,
+        }
+        data = {
+            'color': 'red'
+        }
+        self.assertEqual(get_display_data(data, column),
+                         {'expr': get_color, 'name': 'favorite color', 'value': 'red'})
