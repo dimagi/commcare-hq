cb11d29386d0e95a4ecfa0bf6e78f7c2d74d9682
Sravan Reddy <sravfeyn@gmail.com>
2017-01-10 13:53:15 +0530

    update user buckets;

---
+++ corehq/apps/accounting/bootstrap/config/user_buckets_jan_2017.py
@@ -0,0 +1,49 @@
+from decimal import Decimal
+
+from corehq.apps.accounting.models import (
+    FeatureType,
+    SoftwarePlanEdition,
+)
+
+BOOTSTRAP_CONFIG = {
+    (SoftwarePlanEdition.COMMUNITY, False): {
+        'role': 'community_plan_v0',
+        'product_rate': dict(),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=10, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        }
+    },
+    (SoftwarePlanEdition.STANDARD, False): {
+        'role': 'standard_plan_v0',
+        'product_rate': dict(),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=50, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=100),
+        }
+    },
+    (SoftwarePlanEdition.PRO, False): {
+        'role': 'pro_plan_v0',
+        'product_rate': dict(),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=250, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=500),
+        }
+    },
+    (SoftwarePlanEdition.ADVANCED, False): {
+        'role': 'advanced_plan_v0',
+        'product_rate': dict(),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=500, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=1000),
+        }
+    },
+    (SoftwarePlanEdition.ADVANCED, True): {
+        'role': 'advanced_plan_v0',
+        'product_rate': dict(),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=10, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        }
+    }
+}
