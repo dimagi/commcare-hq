5d0008e4f579b5261f26d78188e4b8e48762983d
Jennifer Schweers <orange.jenny@gmail.com>
2017-01-12 15:15:35 -0500

    add initial_page_data template tag

--- corehq/apps/fixtures/static/fixtures/js/view-table.js
+++ corehq/apps/fixtures/static/fixtures/js/view-table.js
@@ -1,7 +1,24 @@
 (function () {
-    var initialPageData = $.parseJSON($("#initial-page-json").text());
+    var initialPageData = {};
+    _.each($(".initial-page-data"), function(div) {
+        var data = $(div).data();
+        initialPageData[data.name] = data.value;
+    });
     if (initialPageData.renderReportTables) {
-        var reportTables = new HQReportDataTables(initialPageData.reportTablesOptions);
+        var reportTables = new HQReportDataTables(_.pick(initialPageData, [
+            'slug',
+            'defaultRows',
+            'startAtRowNum',
+            'showAllRowsOption',
+            'autoWidth',
+            'aoColumns',
+            'customSort',
+            'ajaxSource',
+            'ajaxParams',
+            'fixColumns',
+            'fixColsNumLeft',
+            'fixColsWidth',
+        ]));
         if (typeof standardHQReport !== 'undefined') {
             standardHQReport.handleTabularReportCookies(reportTables);
         }
