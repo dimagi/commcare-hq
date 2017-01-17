da28675f267e3e3bac1e74d2b05842956cc66d95
Manish Kangia <mkangia@dimagi.com>
2017-01-17 20:00:00 +0530

    Update error message for discontinued feature

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -2131,7 +2131,10 @@ class ModuleBase(IndexedSchema, NavMenuItemMediaMixin, CommentMixin):
                 project = Domain.get_by_name(domain)
                 try:
                     if not should_sync_hierarchical_fixture(project):
-                        raise LocationXpathValidationError(_('Location references not supported in Case List.'))
+                        raise LocationXpathValidationError(
+                            _('That format is no longer supported. To reference the location hierarchy you need to'
+                              ' use the "Custom Calculations in Case List" feature preview. For more information '
+                              'see: https://confluence.dimagi.com/pages/viewpage.action?pageId=38276915'))
                     hierarchy = hierarchy or parent_child(domain)
                     LocationXpath('').validate(column.field_property, hierarchy)
                 except LocationXpathValidationError, e:
