dd5a2637451859f46239141d20708b28618a501c
Manish Kangia <mkangia@dimagi.com>
2017-01-18 00:51:21 +0530

    extract a helper method for field choices

--- custom/rch/forms.py
+++ custom/rch/forms.py
@@ -8,43 +8,11 @@ from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from custom.rch.models import AreaMapping
 
 
-def get_state_names_choices():
-    states = AreaMapping.objects.order_by().values_list('stname', flat=True).distinct()
+def get_choices_for(field_name):
+    field_values = AreaMapping.objects.values_list(field_name, flat=True).distinct()
     options = set()
-    for state in states:
-        options.add((state, state))
-    return tuple(options)
-
-
-def get_district_names_choices():
-    districts = AreaMapping.objects.order_by().values_list('dtname', flat=True).distinct()
-    options = set()
-    for district in districts:
-        options.add((district, district))
-    return tuple(options)
-
-
-def get_awcids_choices():
-    awcids = AreaMapping.objects.order_by().values_list('awcid', flat=True).distinct()
-    options = set()
-    for awcid in awcids:
-        options.add((awcid, awcid))
-    return tuple(options)
-
-
-def get_village_codes_choices():
-    village_codes = AreaMapping.objects.order_by().values_list('villcode', flat=True).distinct()
-    options = set()
-    for village_code in village_codes:
-        options.add((village_code, village_code))
-    return tuple(options)
-
-
-def get_village_names():
-    village_names = AreaMapping.objects.order_by('Village_name').values_list('Village_name', flat=True).distinct()
-    options = set()
-    for village_name in village_names:
-        options.add((village_name, village_name))
+    for field_value in field_values:
+        options.add((field_value, field_value))
     return tuple(options)
 
 
@@ -52,31 +20,31 @@ class BeneficiariesFilterForm(forms.Form):
     state = forms.ChoiceField(
         label=ugettext_lazy("State"),
         required=True,
-        choices=get_state_names_choices()
+        choices=get_choices_for('stname')
     )
 
     district = forms.ChoiceField(
         label=ugettext_lazy("District"),
         required=True,
-        choices=get_district_names_choices()
+        choices=get_choices_for('dtname')
     )
 
     awcid = forms.ChoiceField(
         label=ugettext_lazy("AWC-ID"),
         required=True,
-        choices=get_awcids_choices()
+        choices=get_choices_for('awcid')
     )
 
     village_id = forms.ChoiceField(
         label=ugettext_lazy("Village-ID"),
         required=True,
-        choices=get_village_codes_choices()
+        choices=get_choices_for('villcode')
     )
 
     village_name = forms.ChoiceField(
         label=ugettext_lazy("Village Name"),
         required=True,
-        choices=get_village_names()
+        choices=get_choices_for('Village_name')
     )
 
     def __init__(self, *args, **kwargs):
