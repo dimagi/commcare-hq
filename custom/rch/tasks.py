602487c2a2a8a572e4f0997d7d7c44186fc6476b
Manish Kangia <mkangia@dimagi.com>
2017-01-18 01:13:02 +0530

    fix time for periodic task to be in PM [skip ci]

--- custom/rch/tasks.py
+++ custom/rch/tasks.py
@@ -4,11 +4,11 @@ from celery.schedules import crontab
 from custom.rch.models import RCHMother, RCHChild
 
 
-@periodic_task(run_every=crontab(minute="0", hour="6"), queue='background_queue')
+@periodic_task(run_every=crontab(minute="0", hour="18"), queue='background_queue')
 def fetch_rch_mother_beneficiaries():
     RCHMother.update_beneficiaries()
 
 
-@periodic_task(run_every=crontab(minute="15", hour="6"), queue='background_queue')
+@periodic_task(run_every=crontab(minute="15", hour="18"), queue='background_queue')
 def fetch_rch_child_beneficiaries():
     RCHChild.update_beneficiaries()
