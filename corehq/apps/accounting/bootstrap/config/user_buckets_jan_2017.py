d9cbe3a3a9df0a988f0c511f639dc458b94b27c0
Nick Pellegrino <npellegrino@dimagi.com>
2017-01-18 09:29:15 -0500

    fix product rates

--- corehq/apps/accounting/bootstrap/config/user_buckets_jan_2017.py
+++ corehq/apps/accounting/bootstrap/config/user_buckets_jan_2017.py
@@ -16,7 +16,7 @@ BOOTSTRAP_CONFIG = {
     },
     (SoftwarePlanEdition.STANDARD, False): {
         'role': 'standard_plan_v0',
-        'product_rate': dict(),
+        'product_rate': dict(monthly_fee=Decimal('100.00')),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=50, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=100),
@@ -24,7 +24,7 @@ BOOTSTRAP_CONFIG = {
     },
     (SoftwarePlanEdition.PRO, False): {
         'role': 'pro_plan_v0',
-        'product_rate': dict(),
+        'product_rate': dict(monthly_fee=Decimal('500.00')),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=250, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=500),
@@ -32,7 +32,7 @@ BOOTSTRAP_CONFIG = {
     },
     (SoftwarePlanEdition.ADVANCED, False): {
         'role': 'advanced_plan_v0',
-        'product_rate': dict(),
+        'product_rate': dict(monthly_fee=Decimal('1000.00')),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=500, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=1000),
