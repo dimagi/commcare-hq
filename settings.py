6beb249a5b240788949854f4c1a832d74ed9dcd8
Nick Pellegrino <npellegrino@dimagi.com>
2017-01-18 21:14:22 -0500

    update request context processor import

--- settings.py
+++ settings.py
@@ -909,10 +909,10 @@ TEMPLATES = [
             'context_processors': [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.request',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
+                'django.template.context_processors.request',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
 
