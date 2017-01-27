84afd58d3b9e1d82342d9e4931416f1de8b97ab3
Nick Pellegrino <npellegrino@dimagi.com>
2017-01-27 15:53:30 -0500

    update date_of_mo_signature

--- custom/enikshay/nikshay_datamigration/factory.py
+++ custom/enikshay/nikshay_datamigration/factory.py
@@ -205,7 +205,11 @@ class EnikshayCaseFactory(object):
                     ),
                     'adherence_schedule_id': 'schedule_mwf',
                     'date_of_diagnosis': self.patient_detail.pregdate1,
-                    'date_of_mo_signature': self.patient_detail.date_of_mo_signature,
+                    'date_of_mo_signature': (
+                        self.patient_detail.date_of_mo_signature
+                        if self.patient_detail.date_of_mo_signature
+                        else self.patient_detail.pregdate1
+                    ),
                     'disease_classification': self.patient_detail.disease_classification,
                     'dots_99_enabled': 'false',
                     'episode_pending_registration': 'no',
