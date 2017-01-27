59433810ff948060e4ec45f7668300924a6ce046
Will Pride <wpride@dimagi.com>
2017-01-27 11:09:46 +0200

    add case id

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/controller.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/controller.js
@@ -102,7 +102,7 @@ FormplayerFrontend.module("Menus", function (Menus, FormplayerFrontend, Backbone
             var tabListView = new Menus.Views.DetailTabListView({
                 collection: tabCollection,
                 showDetail: function (detailTabIndex) {
-                    self.showDetail(model, detailTabIndex);
+                    self.showDetail(model, detailTabIndex, caseId);
                 },
             });
 
