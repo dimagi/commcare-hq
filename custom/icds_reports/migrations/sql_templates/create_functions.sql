5fa2f49d11e0b37fae327b4da84ac439db478130
sheelio <sshah@dimagi.com>
2017-01-29 16:57:03 +0530

    Add check constraint to monthly tables

--- custom/icds_reports/migrations/sql_templates/create_functions.sql
+++ custom/icds_reports/migrations/sql_templates/create_functions.sql
@@ -59,7 +59,9 @@ DECLARE
 BEGIN
 	_tablename := $1 || '_' || (date_trunc('MONTH', $2)::DATE);
 	EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(_tablename);
-	EXECUTE 'CREATE TABLE ' || quote_ident(_tablename) || '() INHERITS ('  || quote_ident($1) || ')';
+	EXECUTE 'CREATE TABLE ' || quote_ident(_tablename) || '(' ||
+        'CHECK ( month = DATE ' || quote_literal(date_trunc('MONTH', $2)::DATE) || ' )' ||
+	    ') INHERITS ('  || quote_ident($1) || ')';
 END;
 $BODY$
 LANGUAGE plpgsql;
