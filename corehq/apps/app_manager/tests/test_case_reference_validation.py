75929a46cd498fb84f376339a39398958bf03bae
Cory Zue <czue@dimagi.com>
2017-02-01 19:02:34 +0200

    add a bunch more tests for case reference validation

--- corehq/apps/app_manager/tests/test_case_reference_validation.py
+++ corehq/apps/app_manager/tests/test_case_reference_validation.py
@@ -6,7 +6,34 @@ from corehq.util.test_utils import generate_cases
 @generate_cases([
     ({'load': {}},),
     ({'load': {'data/empty': []}},),
-    ({'load': {'data/multiple': ['p1', 'p2']}},),
+    ({'load': {'data/properties': ['p1', 'p2']}},),
+    ({'load': {'data/multiple1': ['p1', 'p2'], 'data/multiple2': ['p1', 'p2']}},),
+    ({'load': {}, 'save': {}},),
+    ({'load': {}, 'save': {'data/empty': {}}},),
+    ({'load': {}, 'save': {'data/case_type': {'case_type': 'ct'}}},),
+    ({'load': {}, 'save': {'data/props': {'properties': ['p1', 'p2']}}},),
+    ({'load': {}, 'save': {'data/create': {'create': True}}},),
+    ({'load': {}, 'save': {'data/close': {'close': True}}},),
+    ({
+         'load': {
+             'data/load1': ['p1', 'p2'],
+             'data/load2': ['p1', 'p2']
+         },
+         'save': {
+             'data/save1': {
+                 'case_type': 'foo_type',
+                 'properties': ['p1', 'p2'],
+                 'create': True,
+                 'close': True,
+              },
+             'data/save2': {
+                 'case_type': 'foo_type',
+                 'properties': ['p1', 'p2'],
+                 'create': True,
+                 'close': True,
+              }
+         }
+     },),
 ])
 def test_valid_args(self, case_references):
     wrapped_references = {
@@ -18,7 +45,11 @@ def test_valid_args(self, case_references):
 @generate_cases([
     ({},),
     ({'load': {'data/non-strings': [0, 'p2']}},),
-    ({'load': {}, 'extra': {}},),
+    ({'load': {}, 'extra': 'stuff'},),
+    ({'load': {}, 'save': {'data/extra': {'extra': 'stuff'}}},),
+    ({'load': {}, 'save': {'data/non-strings': {'properties': [0, 'p2']}}},),
+    ({'load': {}, 'save': {'data/create-non-bool': {'create': 'abc'}}},),
+    ({'load': {}, 'save': {'data/close-non-bool': {'close': 0}}},),
 ])
 def test_invalid_args(self, structure):
     with self.assertRaises(Exception):
