0c628c0bf88f98899e371fcf7d11a1af6fcf7bd7
Cory Zue <czue@dimagi.com>
2017-02-01 17:04:20 +0200

    use proper setter

--- corehq/apps/app_manager/tests/test_case_meta.py
+++ corehq/apps/app_manager/tests/test_case_meta.py
@@ -116,7 +116,7 @@ class CaseMetaTest(SimpleTestCase, TestXmlMixin):
         m0 = app.add_module(AdvancedModule.new_module('Module3', lang='en'))
         m0.case_type = 'household_advanced'
         m0f1 = m0.new_form('save to case', 'en', attachment=self.get_xml('standard_questions'))
-        m0f1.case_references_data = {
+        m0f1.case_references = {
             'save': {
                 "/data/question1": {
                     "case_type": "household_advanced",
