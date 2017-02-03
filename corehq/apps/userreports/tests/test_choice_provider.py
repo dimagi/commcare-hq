d9d0ec907b701601b9531af72a155877e348d725
Nick Pellegrino <npellegrino@dimagi.com>
2017-02-03 16:07:14 -0500

    add from __future__ import absolute_import

--- corehq/apps/userreports/tests/test_choice_provider.py
+++ corehq/apps/userreports/tests/test_choice_provider.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from abc import ABCMeta, abstractmethod
 from django.test import SimpleTestCase
 import mock
--- corehq/apps/userreports/tests/test_expressions.py
+++ corehq/apps/userreports/tests/test_expressions.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import copy
 import uuid
 from datetime import date, datetime
