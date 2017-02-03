97264184fb35d34c3e5f91757d3f4fe9c377d9ff
Nick Pellegrino <npellegrino@dimagi.com>
2017-02-03 17:48:39 -0500

    from __future__ import absolute_import

--- corehq/apps/builds/models.py
+++ corehq/apps/builds/models.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from datetime import datetime
 from zipfile import ZipFile
 from couchdbkit.exceptions import ResourceNotFound, BadValueError
--- corehq/apps/builds/views.py
+++ corehq/apps/builds/views.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from cStringIO import StringIO
 from couchdbkit import ResourceNotFound, BadValueError
 from django.core.urlresolvers import reverse
