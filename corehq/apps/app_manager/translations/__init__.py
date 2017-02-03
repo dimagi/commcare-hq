225048ef25d6144bf55ae1003aa626c51dd68b30
Daniel Roberts <droberts@dimagi.com>
2017-02-03 11:53:44 -0500

    change translations.py to app_translations.py

--- corehq/apps/app_manager/translations/__init__.py
+++ corehq/apps/app_manager/translations/__init__.py
@@ -1,9 +1,8 @@
-from .translations import (
+from .app_translations import (
     get_unicode_dicts,
     process_bulk_app_translation_upload,
     expected_bulk_app_sheet_headers,
     expected_bulk_app_sheet_rows,
     update_form_translations,
     escape_output_value,
-    _update_case_list_translations,
 )
