89dd42a26fdafe36a65f367315673f2b4f0cb104
Cal Ellowitz <calellowitz@gmail.com>
2017-02-03 15:39:31 -0500

    fixing updates to latest saved version

--- corehq/apps/app_manager/tasks.py
+++ corehq/apps/app_manager/tasks.py
@@ -1,5 +1,6 @@
 from celery.task import task
 from corehq.apps.users.models import CommCareUser
+from corehq.util.decorators import serial_task
 
 
 @task(queue='background_queue', ignore_result=True)
@@ -7,3 +8,16 @@ def create_user_cases(domain_name):
     from corehq.apps.callcenter.utils import sync_usercase
     for user in CommCareUser.by_domain(domain_name):
         sync_usercase(user)
+
+
+@serial_task('{app._id}', max_retries=0)
+def make_async_build(app):
+    latest_build = app.get_latest_app(released_only=False)
+    if latest_build.version == app.version:
+        return
+    errors = app.validate_app()
+    if not errors:
+        copy = app.make_build(
+            previous_version=latest_build
+        )
+        copy.save(increment_version=False)
