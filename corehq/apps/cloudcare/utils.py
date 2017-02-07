c911add48754f584451f4af2e798fdf43cf98c01
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-07 16:32:13 -0500

    Remove is_onboarding_domain check

--- corehq/apps/cloudcare/utils.py
+++ corehq/apps/cloudcare/utils.py
@@ -3,9 +3,4 @@ from corehq.apps.analytics import ab_tests
 
 
 def should_show_preview_app(request, domain_obj, username):
-    if domain_obj.is_onboarding_domain:
-        return True
-    elif domain_obj.is_onboarding_domain and not request.couch_user.is_dimagi:
-        return False
-
     return PREVIEW_APP.enabled(domain_obj.name) or PREVIEW_APP.enabled(username)
