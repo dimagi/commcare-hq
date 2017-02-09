fca1d6538134c2d6ee46c1379e00e260676e9e0d
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-09 11:00:44 -0500

    App manager: stay on same tab when refreshing

--- corehq/apps/accounting/static/accounting/js/stay-on-tab.js
+++ corehq/apps/accounting/static/accounting/js/stay-on-tab.js
@@ -1,19 +1,21 @@
 // hqDefine intentionally not used
 
-// From http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
+// Modified from
+// http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
 $(function() {
-    $('a[data-toggle="tab"]').on('shown', function(e){
-        //save the latest tab using a cookie:
-        $.cookie('last_tab', $(e.target).attr('href'));
+    // Save latest tab when switching tabs
+    $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function(e){
+        var href = $(e.target).attr('href');
+        if (href.startsWith("#")) {
+            // App manager does complex things with tabs.
+            // Ignore those, only deal with simple tabs.
+            $.cookie('last_tab', href);
+        }
     });
-    //activate latest tab, if it exists:
+
+    // Activate latest tab on document ready
     var lastTab = $.cookie('last_tab');
     if (lastTab) {
-        $('a[href=' + lastTab + ']').tab('show');
-    }
-    else
-    {
-        // Set the first tab if cookie do not exist
-        $('a[data-toggle="tab"]:first').tab('show');
+        $('a[href="' + lastTab + '"]').tab('show');
     }
 });
