371b25c097c6fd385af023b1c863990578e40477
NoahCarnahan <ncarnahan@dimagi.com>
2017-02-10 16:49:43 -0500

    Revert "temp"
    This reverts commit 924d7b9bf68605b4cf94121e49e4879c1b195899.

--- corehq/apps/reports/dispatcher.py
+++ corehq/apps/reports/dispatcher.py
@@ -232,7 +232,6 @@ class ReportDispatcher(View):
                         'subpages': report.get_subpages(),
                         'show_in_navigation': show_in_navigation,
                         'show_in_dropdown': show_in_dropdown,
-                        'class_name': report.__name__,
                     })
             if report_contexts:
                 if hasattr(section_name, '__call__'):
