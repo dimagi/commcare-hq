c856d5e2d2c7f5a8a92d045648a5bd597aa1371c
Nick Pellegrino <npellegrino@dimagi.com>
2017-02-10 11:59:46 -0500

    use add_arguments

--- corehq/apps/hqadmin/management/commands/fix_checkpoints_from_file.py
+++ corehq/apps/hqadmin/management/commands/fix_checkpoints_from_file.py
@@ -5,15 +5,17 @@ from pillowtop import get_pillow_by_name
 
 
 class Command(BaseCommand):
-    args = 'filename'
     help = ("Update the pillow sequence IDs based on a passed in file")
-    option_list = (
-        make_option('--noinput',
-                    action='store_true',
-                    dest='noinput',
-                    default=False,
-                    help="Disable interactive mode"),
-    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename')
+        parser.add_argument(
+            '--noinput',
+            action='store_true',
+            dest='noinput',
+            default=False,
+            help="Disable interactive mode",
+        )
 
     def handle(self, filename, **options):
         with open(filename) as f:
