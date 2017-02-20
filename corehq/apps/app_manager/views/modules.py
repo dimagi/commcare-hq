793d5a08291b91054532b77ca51a8709b0cb3678
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-20 14:42:16 -0500

    case_list_form_options needed by both js and template

--- corehq/apps/app_manager/views/modules.py
+++ corehq/apps/app_manager/views/modules.py
@@ -133,9 +133,9 @@ def _get_shared_module_view_context(app, module, case_property_builder, lang=Non
     case_type = module.case_type
     return {
         'details': _get_module_details_context(app, module, case_property_builder, case_type),
+        'case_list_form_options': _case_list_form_options(app, module, case_type, lang),
         'js_options': {
             'fixture_columns_by_type': _get_fixture_columns_by_type(app.domain),
-            'case_list_form_options': _case_list_form_options(app, module, case_type, lang),
             'valid_parent_modules': _get_valid_parent_modules(app, module),
             'is_search_enabled': case_search_enabled_for_domain(app.domain),
             'search_properties': module.search_config.properties if module_offers_search(module) else [],
