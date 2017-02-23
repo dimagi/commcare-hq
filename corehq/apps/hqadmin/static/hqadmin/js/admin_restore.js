03b4233999bbc6fb0ec61406c8ce79c9041cef80
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-23 13:16:09 -0500

    split out admin_restore.js

---
+++ corehq/apps/hqadmin/static/hqadmin/js/admin_restore.js
@@ -0,0 +1,16 @@
+$(function() {
+    var payloadElement = document.getElementById('payload');
+    var myCodeMirror = CodeMirror(function(elt) {
+        payloadElement.parentNode.replaceChild(elt, payloadElement);
+    }, {
+        value: payloadElement.textContent,
+        readOnly: true,
+        lineNumbers: true,
+        mode: {name: "text/xml", json: true},
+        viewportMargin: Infinity,
+        foldGutter: true,
+        gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    });
+
+    $("#timingTable").treetable();
+});
