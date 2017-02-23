c6314c8f1c67caeec28be65153cecb3cfc5e9a03
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-23 14:42:17 -0500

    Revert "(moving code only) move some non-django-dependent js"
    This reverts commit 5c49ce55130992aeec98eddfa73d39fda54d7c5b.

--- corehq/apps/app_manager/static/app_manager/js/module-view.js
+++ corehq/apps/app_manager/static/app_manager/js/module-view.js
@@ -1,20 +1,2 @@
 $(function () {
-    $(function () {
-        var setupValidation = hqImport('app_manager/js/app_manager.js').setupValidation;
-        setupValidation(hqImport('hqwebapp/js/urllib.js').reverse('validate_module_for_build'));
-    });
-    $(function() {
-        // show display style options only when module configured to show module and then forms
-        var $menu_mode = $('#put_in_root');
-        var $display_style_container = $('#display_style_container');
-        var update_display_view = function() {
-            if($menu_mode.val() == 'false') {
-                $display_style_container.show();
-            } else {
-                $display_style_container.hide();
-            }
-        }
-        update_display_view()
-        $menu_mode.on('change', update_display_view)
-    });
 });
