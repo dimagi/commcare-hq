edb1db1d94c0a32aa06a0ac6bc5a45ed887c299f
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-23 11:08:33 -0500

    fix js error in raw_couch

--- corehq/apps/hqadmin/static/hqadmin/js/raw_couch.js
+++ corehq/apps/hqadmin/static/hqadmin/js/raw_couch.js
@@ -2,16 +2,18 @@ $(function() {
     // don't break if offline (Also why I left it as a <pre/>)
     if (window.CodeMirror) {
         var couchDocElement = document.getElementById('couch-document');
-        var myCodeMirror = CodeMirror(function(elt) {
-            couchDocElement.parentNode.replaceChild(elt, couchDocElement);
-        }, {
-            value: couchDocElement.textContent,
-            readOnly: true,
-            lineNumbers: true,
-            mode: {name: "javascript", json: true},
-            viewportMargin: Infinity,
-            foldGutter: true,
-            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
-        });
+        if (couchDocElement) {
+            var myCodeMirror = CodeMirror(function(elt) {
+                couchDocElement.parentNode.replaceChild(elt, couchDocElement);
+            }, {
+                value: couchDocElement.textContent,
+                readOnly: true,
+                lineNumbers: true,
+                mode: {name: "javascript", json: true},
+                viewportMargin: Infinity,
+                foldGutter: true,
+                gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+            });
+        }
     }
 });
