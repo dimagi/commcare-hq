4f855806edaff519f1920f1be3eed891e8389c1a
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-23 10:58:38 -0500

    externalize js in raw_couch.html

---
+++ corehq/apps/hqadmin/static/hqadmin/js/raw_couch.js
@@ -0,0 +1,17 @@
+$(function() {
+    // don't break if offline (Also why I left it as a <pre/>)
+    if (window.CodeMirror) {
+        var couchDocElement = document.getElementById('couch-document');
+        var myCodeMirror = CodeMirror(function(elt) {
+            couchDocElement.parentNode.replaceChild(elt, couchDocElement);
+        }, {
+            value: couchDocElement.textContent,
+            readOnly: true,
+            lineNumbers: true,
+            mode: {name: "javascript", json: true},
+            viewportMargin: Infinity,
+            foldGutter: true,
+            gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+        });
+    }
+});
