64ad9443cfe8fce20aeeadbeb3ee4fee434a14ff
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-23 01:18:48 -0500

    split out info_base.js

---
+++ corehq/apps/domain/static/domain/js/info_base.js
@@ -0,0 +1,16 @@
+$(function() {
+    var fields = hqImport('hqwebapp/js/initial_page_data.js').get('autocomplete_fields');
+    _.each(hqImport('hqwebapp/js/initial_page_data.js').get('autocomplete_fields'), function(field) {
+        $("#id_" + field).focus(function() {
+            if (!$("#id_" + field).data('loaded')) {
+                $("#id_" + field).data('loaded', 'true');
+                $.getJSON(hqImport("hqwebapp/js/urllib.js").reverse("domain_autocomplete_fields", field), function(results) {
+                    $("#id_" + field).typeahead({
+                        source: results,
+                        items: 8,
+                    }).attr("autocomplete", "off");
+                });
+            }
+        });
+    });
+});
