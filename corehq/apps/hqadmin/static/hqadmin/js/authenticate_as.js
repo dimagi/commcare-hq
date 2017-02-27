c9b520ed5b7cccdc71fae945cfca43fa3dcd9233
Jennifer Schweers <orange.jenny@gmail.com>
2017-02-27 18:45:05 -0500

    some more

--- corehq/apps/hqadmin/static/hqadmin/js/authenticate_as.js
+++ corehq/apps/hqadmin/static/hqadmin/js/authenticate_as.js
@@ -1,13 +1,16 @@
-$(function() {
-    $('#id_username, #id_domain').change(function() {
-        var username = $('#id_username').val(),
-            domain = $('#id_domain').val();
-
-        var action = hqImport('hqwebapp/js/initial_page_data.js').get('url') + username + '/';
-        if (domain) {
-            action += domain + '/';
-        }
-
-        $('#auth-as-form').attr('action', action);
+/* globals hqDefine */
+hqDefine('hqadmin/js/authenticate_as.js', function () {
+    $(function() {
+        $('#id_username, #id_domain').change(function() {
+            var username = $('#id_username').val(),
+                domain = $('#id_domain').val();
+    
+            var action = hqImport('hqwebapp/js/initial_page_data.js').get('url') + username + '/';
+            if (domain) {
+                action += domain + '/';
+            }
+    
+            $('#auth-as-form').attr('action', action);
+        });
     });
 });
--- corehq/apps/hqadmin/static/hqadmin/js/domain_faceted_report.js
+++ corehq/apps/hqadmin/static/hqadmin/js/domain_faceted_report.js
@@ -1,4 +1,5 @@
-$(function() {
+/* globals hqDefine */
+hqDefine('hqadmin/js/domain_faceted_report.js', function () {
     var visualizations = {
         forms: {name: "forms", xaxis_label: "# form submissions", viz: null},
         cases: {name: "cases", xaxis_label: "# case creations", viz: null},
@@ -23,32 +24,34 @@ $(function() {
     }
     var url_params = parse_url_params();
     
-    for (var key in visualizations) {
-        if (visualizations.hasOwnProperty(key)) {
-            visualizations[key].viz = new HQVisualizations({
-                chart_name: key,
-                histogram_type: key,
-                xaxis_label: visualizations[key].xaxis_label,
-                ajax_url: hqImport("hqwebapp/js/urllib.js").reverse("admin_stats_data"),
-                data: url_params,
-                interval: hqImport("hqwebapp/js/initial_page_data.js").get("interval"),
-            });
-            visualizations[key].viz.init();
-        }
-}
-
-    $("#all-charts-filter").on("submit", function() {
-        var $this = $(this);
-        var startdate = $this.find('[name="startdate"]').val();
-        var enddate = $this.find('[name="enddate"]').val();
-        var interval = $this.find('[name="interval"]').val();
-
-        $('.startdate-input').val(startdate);
-        $('.enddate-input').val(enddate);
-        $('.interval-input').val(interval);
-
-        $('.reload-graph-form').submit();
-
-        return false;
+    $(function() {
+        for (var key in visualizations) {
+            if (visualizations.hasOwnProperty(key)) {
+                visualizations[key].viz = new HQVisualizations({
+                    chart_name: key,
+                    histogram_type: key,
+                    xaxis_label: visualizations[key].xaxis_label,
+                    ajax_url: hqImport("hqwebapp/js/urllib.js").reverse("admin_stats_data"),
+                    data: url_params,
+                    interval: hqImport("hqwebapp/js/initial_page_data.js").get("interval"),
+                });
+                visualizations[key].viz.init();
+            }
+    }
+    
+        $("#all-charts-filter").on("submit", function() {
+            var $this = $(this);
+            var startdate = $this.find('[name="startdate"]').val();
+            var enddate = $this.find('[name="enddate"]').val();
+            var interval = $this.find('[name="interval"]').val();
+    
+            $('.startdate-input').val(startdate);
+            $('.enddate-input').val(enddate);
+            $('.interval-input').val(interval);
+    
+            $('.reload-graph-form').submit();
+    
+            return false;
+        });
     });
 });
