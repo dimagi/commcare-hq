553886765cc6c51b859374cecee34898a0361538
Jennifer Schweers <orange.jenny@gmail.com>
2017-03-01 11:13:08 -0500

    Set up cookie to snooze modal

--- corehq/apps/hqwebapp/static/hqwebapp/js/rollout_modal.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/rollout_modal.js
@@ -1,11 +1,33 @@
 hqDefine("hqwebapp/js/rollout_modal.js", function() {
     $(function() {
-        $('.rollout-modal').modal({
-            backdrop: 'static',
-            keyboard: false,
-            show: true,
-        })/*.on('hide.bs.modal', function () {
-            window.location = hqImport('hqwebapp/js/urllib.js').reverse('default_app');
-        })*/;
+        var $modal = $(".rollout-modal"),
+            slug = $modal.data("slug"),
+            cookie_name = "snooze_" + slug;
+        if (!$.cookie(cookie_name)) {
+            $modal.modal({
+                backdrop: 'static',
+                keyboard: false,
+                show: true,
+            });
+            $modal.on('click', '.flag-enable', function() {
+                $.get({
+                    url: '',    // TODO
+                    data: {
+                        // TODO
+                    },
+                    success: function() {
+                        window.location.reload(true);
+                    },
+                    error: function() {
+                        alert_user(gettext('We could not turn on the new feature. You will have the opportunity' +
+                                           'to turn it on the next time you visit this page.'), 'warning');
+                    },
+                });
+            });
+            $modal.on('click', '.flag-snooze', function() {
+                $.cookie(cookie_name, true, { expires: 3, path: '/' });
+                $modal.modal('hide');
+            });
+        }
     });
 });
