6bbb1aa9a42dfb88f0bdc1a737f6974d55f48e29
Daniel Roberts <droberts@dimagi.com>
2017-03-06 14:59:12 -0500

    remove cancelled repeat records from next_check queuing

--- corehq/couchapps/receiverwrapper/views/repeat_records_by_next_check/map.js
+++ corehq/couchapps/receiverwrapper/views/repeat_records_by_next_check/map.js
@@ -1,6 +1,6 @@
 function (doc) {
     if (doc.doc_type === 'RepeatRecord') {
-        if (!doc.succeeded) {
+        if (!doc.succeeded && !doc.cancelled) {
             emit([doc.domain, doc.next_check], null);
             emit([null, doc.next_check], null);
         }
