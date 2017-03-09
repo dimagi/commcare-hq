ed62a81620ee94a33e55390df41449bf28061a89
Jennifer Schweers <orange.jenny@gmail.com>
2017-03-09 16:57:51 -0500

    PR 14855

--- corehq/apps/domain/static/domain/js/internal_settings.js
+++ corehq/apps/domain/static/domain/js/internal_settings.js
@@ -1,59 +1,62 @@
-var areas = hqImport('hqwebapp/js/initial_page_data.js').get('areas');
-function update_subareas() {
-    var $subarea = $subarea || $('[name="sub_area"]');
-    var chosen_sub_area = $subarea.val();
-    var area = $('[name="area"]').val();
-    var valid_sub_areas = [];
-    if (area) {
-        valid_sub_areas = areas[area];
-    }
-    $subarea.empty().append($("<option></option>").attr("value", '').text('---'));
-    _.each(valid_sub_areas, function(val) {
-        var $opt = $("<option></option>").attr("value", val).text(val);
-        if (val == chosen_sub_area) {
-            $opt.prop("selected", true);
+/* globals hqDefine */
+hqDefine("domain/js/internal_settings.js", function() {
+    var areas = hqImport('hqwebapp/js/initial_page_data.js').get('areas');
+    function update_subareas() {
+        var $subarea = $subarea || $('[name="sub_area"]');
+        var chosen_sub_area = $subarea.val();
+        var area = $('[name="area"]').val();
+        var valid_sub_areas = [];
+        if (area) {
+            valid_sub_areas = areas[area];
         }
-         $subarea.append($opt);
-    });
-}
-
-function update_workshop_region() {
-    var $wr = $wr || $('#id_workshop_region').parent().parent();
-    var $workshop_initiative = $workshop_initiative || $('[name="initiative"][value="Workshop"]');
-    if ($workshop_initiative.is(":checked")) {
-        $wr.show();
-    } else {
-        $wr.hide();
+        $subarea.empty().append($("<option></option>").attr("value", '').text('---'));
+        _.each(valid_sub_areas, function(val) {
+            var $opt = $("<option></option>").attr("value", val).text(val);
+            if (val == chosen_sub_area) {
+                $opt.prop("selected", true);
+            }
+             $subarea.append($opt);
+        });
     }
-}
-
-$(function() {
-    update_subareas();
-    update_workshop_region();
-    $('[name="area"]').change(function() {
+    
+    function update_workshop_region() {
+        var $wr = $wr || $('#id_workshop_region').parent().parent();
+        var $workshop_initiative = $workshop_initiative || $('[name="initiative"][value="Workshop"]');
+        if ($workshop_initiative.is(":checked")) {
+            $wr.show();
+        } else {
+            $wr.hide();
+        }
+    }
+    
+    $(function() {
         update_subareas();
-    });
-    $('[name="initiative"]').change(function() {
         update_workshop_region();
+        $('[name="area"]').change(function() {
+            update_subareas();
+        });
+        $('[name="initiative"]').change(function() {
+            update_workshop_region();
+        });
+    
     });
-
-});
-
-$(function() {
-    $('#id_deployment_date').datepicker({
-        changeMonth: true,
-        changeYear: true,
-        showButtonPanel: true,
-        dateFormat: 'yy-mm-dd',
-        maxDate: '0',
-        numberOfMonths: 2
+    
+    $(function() {
+        $('#id_deployment_date').datepicker({
+            changeMonth: true,
+            changeYear: true,
+            showButtonPanel: true,
+            dateFormat: 'yy-mm-dd',
+            maxDate: '0',
+            numberOfMonths: 2
+        });
     });
+    
+    var multiselect_utils = hqImport('style/js/multiselect_utils');
+    multiselect_utils.createFullMultiselectWidget(
+        'id_countries',
+        django.gettext("Available Countries"),
+        django.gettext("Active Countries"),
+        django.gettext("Search Countries...")
+    );
 });
-
-var multiselect_utils = hqImport('style/js/multiselect_utils');
-multiselect_utils.createFullMultiselectWidget(
-    'id_countries',
-    django.gettext("Available Countries"),
-    django.gettext("Active Countries"),
-    django.gettext("Search Countries...")
-);
