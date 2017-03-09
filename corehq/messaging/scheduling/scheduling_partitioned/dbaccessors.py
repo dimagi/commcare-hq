b340f7b78d83c14ba787436c9db0036126fdf146
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-03-09 09:33:24 -0500

    Give variable better name

--- corehq/messaging/scheduling/scheduling_partitioned/dbaccessors.py
+++ corehq/messaging/scheduling/scheduling_partitioned/dbaccessors.py
@@ -42,14 +42,14 @@ def delete_schedule_instance(instance):
 def get_active_schedule_instance_ids(start_timestamp, end_timestamp):
     from corehq.messaging.scheduling.scheduling_partitioned.models import ScheduleInstance
 
-    q_expression = Q(
+    active_filter = Q(
         active=True,
         next_event_due__gt=start_timestamp,
         next_event_due__lte=end_timestamp,
     )
     for schedule_instance_id in run_query_across_partitioned_databases(
         ScheduleInstance,
-        q_expression,
+        active_filter,
         values=['schedule_instance_id']
     ):
         yield schedule_instance_id
