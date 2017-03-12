e2def6110bf921bea40b64601a8f8d1e908e1d42
Manish Kangia <mkangia@dimagi.com>
2017-03-12 15:39:24 +0530

    add repeater for HIV Test, also update test_submission to receive person_case to make it generic

--- custom/enikshay/integrations/nikshay/repeaters.py
+++ custom/enikshay/integrations/nikshay/repeaters.py
@@ -10,7 +10,10 @@ from casexml.apps.case.xform import get_case_updates
 from casexml.apps.case.models import CommCareCase
 from casexml.apps.case.signals import case_post_save
 from corehq.apps.repeaters.signals import create_repeat_records
-from custom.enikshay.case_utils import get_person_case_from_episode
+from custom.enikshay.case_utils import (
+    get_person_case_from_episode,
+    get_open_episode_case_from_person,
+)
 from custom.enikshay.exceptions import NikshayLocationNotFound
 
 
@@ -34,17 +37,60 @@ class NikshayRegisterPatientRepeater(CaseRepeater):
         # When case property episode.episode_pending_registration transitions from 'yes' to 'no',
         # and (episode.nikshay_registered != 'true'  or episode.nikshay_id != '')
         allowed_case_types_and_users = self._allowed_case_type(episode_case) and self._allowed_user(episode_case)
-        episode_case_properties = episode_case.dynamic_case_properties()
-        return allowed_case_types_and_users and (
-            not episode_case_properties.get('nikshay_registered', 'false') == 'true' and
-            not episode_case_properties.get('nikshay_id', False) and
-            episode_pending_registration_changed(episode_case) and
-            not test_submission(episode_case)
-        )
+        if allowed_case_types_and_users:
+            episode_case_properties = episode_case.dynamic_case_properties()
+            person_case = get_person_case_from_episode(episode_case.domain, episode_case.get_id)
+
+            return (
+                not episode_case_properties.get('nikshay_registered', 'false') == 'true' and
+                not episode_case_properties.get('nikshay_id', False) and
+                episode_pending_registration_changed(episode_case) and
+                not test_submission(person_case)
+            )
+        else:
+            return False
 
 
-def test_submission(episode_case):
-    person_case = get_person_case_from_episode(episode_case.domain, episode_case.get_id)
+class NikshayHIVTestRepeater(CaseRepeater):
+    class Meta(object):
+        app_label = 'repeaters'
+
+    include_app_id_param = False
+    friendly_name = _("Forward eNikshay Patient's HIV Test to Nikshay")
+
+    @classmethod
+    def available_for_domain(cls, domain):
+        return NIKSHAY_INTEGRATION.enabled(domain)
+
+    @classmethod
+    def get_custom_url(cls, domain):
+        from custom.enikshay.integrations.nikshay.views import NikshayHIVTestRepeaterView
+        return reverse(NikshayHIVTestRepeaterView.urlname, args=[domain])
+
+    def allowed_to_forward(self, person_case):
+        # episode.nikshay_registered is true and nikshay_id present and
+        # person.hiv_status changed OR
+        # CPTDeliverDate changes OR
+        # InitiatedDate/Art Initiated date changes
+        allowed_case_types_and_users = self._allowed_case_type(person_case) and self._allowed_user(person_case)
+        if allowed_case_types_and_users:
+            episode_case = get_open_episode_case_from_person(person_case.domain, person_case.get_id)
+            episode_case_properties = episode_case.dynamic_case_properties()
+
+            return allowed_case_types_and_users and (
+                episode_case_properties.get('nikshay_registered', 'false') == 'true' and
+                episode_case_properties.get('nikshay_id') and
+                not test_submission(person_case) and
+                (
+                    related_dates_changed(person_case) or
+                    person_hiv_status_changed(person_case)
+                )
+            )
+        else:
+            return False
+
+
+def test_submission(person_case):
     try:
         phi_location = SQLLocation.objects.get(location_id=person_case.owner_id)
     except SQLLocation.DoesNotExist:
@@ -70,10 +116,45 @@ def episode_pending_registration_changed(case):
     return value_changed
 
 
+def person_hiv_status_changed(case):
+    last_case_action = case.actions[-1]
+    if last_case_action.is_case_create:
+        return False
+
+    last_update_actions = [update.get_update_action() for update in get_case_updates(last_case_action.form)]
+    value_changed = any(
+        action for action in last_update_actions
+        if isinstance(action, CaseUpdateAction) and 'hiv_status' in action.dynamic_properties
+    )
+    return value_changed
+
+
+def related_dates_changed(case):
+    last_case_action = case.actions[-1]
+    if last_case_action.is_case_create:
+        return False
+
+    last_update_actions = [update.get_update_action() for update in get_case_updates(last_case_action.form)]
+    value_changed = any(
+        action for action in last_update_actions
+        if isinstance(action, CaseUpdateAction) and (
+            'art_initiation_date' in action.dynamic_properties or
+            'cpt_initiation_date' in action.dynamic_properties
+        )
+    )
+    return value_changed
+
+
 def create_case_repeat_records(sender, case, **kwargs):
     create_repeat_records(NikshayRegisterPatientRepeater, case)
 
+
+def create_hiv_test_repeat_records(sender, case, **kwargs):
+    create_repeat_records(NikshayHIVTestRepeater, case)
+
 case_post_save.connect(create_case_repeat_records, CommCareCaseSQL)
+case_post_save.connect(create_hiv_test_repeat_records, CommCareCaseSQL)
 
 # TODO: Remove this when eNikshay gets migrated to SQL
 case_post_save.connect(create_case_repeat_records, CommCareCase)
+case_post_save.connect(create_hiv_test_repeat_records, CommCareCase)
