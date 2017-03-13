a978645f5bc340cb8f704f4716a10d2e62a6bf55
Manish Kangia <mkangia@dimagi.com>
2017-03-13 17:04:11 +0530

    Add Treatment Outcome Repeater, Update patient registration repeater to use case_properties_changes and const

--- custom/enikshay/integrations/nikshay/repeaters.py
+++ custom/enikshay/integrations/nikshay/repeaters.py
@@ -12,6 +12,8 @@ from casexml.apps.case.signals import case_post_save
 from corehq.apps.repeaters.signals import create_repeat_records
 from custom.enikshay.case_utils import get_person_case_from_episode
 from custom.enikshay.exceptions import NikshayLocationNotFound
+from custom.enikshay.const import TREATMENT_OUTCOME, EPISODE_PENDING_REGISTRATION
+from custom.enikshay.integrations.ninetyninedots.repeaters import case_properties_changed
 
 
 class NikshayRegisterPatientRepeater(CaseRepeater):
@@ -19,7 +21,7 @@ class NikshayRegisterPatientRepeater(CaseRepeater):
         app_label = 'repeaters'
 
     include_app_id_param = False
-    friendly_name = _("Forward eNikshay Patients to Nikshay")
+    friendly_name = _("Forward eNikshay Patients to Nikshay (episode case type)")
 
     @classmethod
     def available_for_domain(cls, domain):
@@ -38,8 +40,35 @@ class NikshayRegisterPatientRepeater(CaseRepeater):
         return allowed_case_types_and_users and (
             not episode_case_properties.get('nikshay_registered', 'false') == 'true' and
             not episode_case_properties.get('nikshay_id', False) and
-            episode_pending_registration_changed(episode_case) and
+            case_properties_changed(episode_case, [EPISODE_PENDING_REGISTRATION]) and
+            episode_case_properties.get(EPISODE_PENDING_REGISTRATION, 'yes') == 'no' and
             not test_submission(episode_case)
+        )
+
+
+class NikshayTreatmentOutcomeRepeater(CaseRepeater):
+    class Meta(object):
+        app_label = 'repeaters'
+
+    friendly_name = _("Forward Treatment Outcomes to Nikshay (episode case type)")
+
+    @classmethod
+    def available_for_domain(cls, domain):
+        return NIKSHAY_INTEGRATION.enabled(domain)
+
+    @classmethod
+    def get_custom_url(cls, domain):
+        from custom.enikshay.integrations.nikshay.views import NikshayTreatmentOutcomesView
+        return reverse(NikshayTreatmentOutcomesView.urlname, args=[domain])
+
+    def allowed_to_forward(self, episode_case):
+        allowed_case_types_and_users = self._allowed_case_type(episode_case) and self._allowed_user(episode_case)
+        episode_case_properties = episode_case.dynamic_case_properties()
+        return allowed_case_types_and_users and (
+            not episode_case_properties.get('nikshay_registered', 'false') == 'true' and
+            not episode_case_properties.get('nikshay_id', False) and
+            not episode_case_properties.get('treatment_outcome_nikshay_registered', False) == 'true' and
+            case_properties_changed(episode_case, [TREATMENT_OUTCOME])
         )
 
 
@@ -55,23 +84,9 @@ def test_submission(episode_case):
     return phi_location.metadata.get('is_test', "yes") == "yes"
 
 
-def episode_pending_registration_changed(case):
-    last_case_action = case.actions[-1]
-    if last_case_action.is_case_create:
-        return False
-
-    last_update_actions = [update.get_update_action() for update in get_case_updates(last_case_action.form)]
-    value_changed = any(
-        action for action in last_update_actions
-        if isinstance(action, CaseUpdateAction)
-        and 'episode_pending_registration' in action.dynamic_properties
-        and action.dynamic_properties['episode_pending_registration'] == 'no'
-    )
-    return value_changed
-
-
 def create_case_repeat_records(sender, case, **kwargs):
     create_repeat_records(NikshayRegisterPatientRepeater, case)
+    create_repeat_records(NikshayTreatmentOutcomeRepeater, case)
 
 case_post_save.connect(create_case_repeat_records, CommCareCaseSQL)
 
