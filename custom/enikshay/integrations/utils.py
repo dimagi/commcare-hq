2ba9865fa0f05e9ae244b2513e046c961540fca1
Farid Rener <proteusvacuum@gmail.com>
2017-03-17 15:28:47 -0400

    Fallback is test location

--- custom/enikshay/integrations/utils.py
+++ custom/enikshay/integrations/utils.py
@@ -1,13 +1,14 @@
 from corehq.apps.locations.models import SQLLocation
-from custom.enikshay.exceptions import NikshayLocationNotFound
+from dimagi.utils.logging import notify_exception
 
 
 def is_submission_from_test_location(person_case):
     try:
         phi_location = SQLLocation.objects.get(location_id=person_case.owner_id)
     except SQLLocation.DoesNotExist:
-        raise NikshayLocationNotFound(
-            "Location with id {location_id} not found. This is the owner for person with id: {person_id}"
-            .format(location_id=person_case.owner_id, person_id=person_case.case_id)
-        )
+        message = ("Location with id {location_id} not found. This is the owner for person with id: {person_id}"
+                   .format(location_id=person_case.owner_id, person_id=person_case.case_id))
+        notify_exception(None, message="[ENIKSHAY] {}".format(message))
+        return True
+
     return phi_location.metadata.get('is_test', "yes") == "yes"
