e5bd4884fc7ea4389315d0d2b8ff248bbda9a905
Farid Rener <proteusvacuum@gmail.com>
2017-03-20 15:17:44 -0400

    Revert "Fallback is test location"
    This reverts commit 2ba9865fa0f05e9ae244b2513e046c961540fca1.

--- custom/enikshay/integrations/utils.py
+++ custom/enikshay/integrations/utils.py
@@ -1,14 +1,13 @@
 from corehq.apps.locations.models import SQLLocation
-from dimagi.utils.logging import notify_exception
+from custom.enikshay.exceptions import NikshayLocationNotFound
 
 
 def is_submission_from_test_location(person_case):
     try:
         phi_location = SQLLocation.objects.get(location_id=person_case.owner_id)
     except SQLLocation.DoesNotExist:
-        message = ("Location with id {location_id} not found. This is the owner for person with id: {person_id}"
-                   .format(location_id=person_case.owner_id, person_id=person_case.case_id))
-        notify_exception(None, message="[ENIKSHAY] {}".format(message))
-        return True
-
+        raise NikshayLocationNotFound(
+            "Location with id {location_id} not found. This is the owner for person with id: {person_id}"
+            .format(location_id=person_case.owner_id, person_id=person_case.case_id)
+        )
     return phi_location.metadata.get('is_test', "yes") == "yes"
