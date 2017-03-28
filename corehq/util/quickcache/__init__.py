14c7a44f85d75014bbbfd053355f9f77b5f54cc5
Daniel Roberts <droberts@dimagi.com>
2017-03-28 13:36:25 -0400

    make quickcache a package

---
+++ corehq/util/quickcache/__init__.py
@@ -0,0 +1,16 @@
+from .quickcache import (
+    quickcache,
+    skippable_quickcache,
+    QuickCache,
+    SkippableQuickCache,
+    TieredCache,
+)
+
+
+__all__ = [
+    'quickcache',
+    'skippable_quickcache',
+    'QuickCache',
+    'SkippableQuickCache',
+    'TieredCache',
+]
