b4c4bbbb34f53970bd1765b43318994032ddcfba
Ben Rudolph <brudolph@dimagi.com>
2017-03-28 16:15:17 -0500

    double refresH_interval

--- corehq/ex-submodules/pillowtop/es_utils.py
+++ corehq/ex-submodules/pillowtop/es_utils.py
@@ -7,7 +7,7 @@ from pillowtop.logger import pillow_logging
 
 INDEX_REINDEX_SETTINGS = {
     "index": {
-        "refresh_interval": "900s",
+        "refresh_interval": "1800s",
         "merge.policy.merge_factor": 20,
         "store.throttle.max_bytes_per_sec": "1mb",
         "store.throttle.type": "merge",
