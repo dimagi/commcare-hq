c6207ac3b3b99dfcdf87f4b6417989a65fd0ede7
Jennifer Schweers <orange.jenny@gmail.com>
2017-03-28 13:47:50 -0400

    Remove stray debugger

--- corehq/apps/groups/static/groups/js/all_groups.js
+++ corehq/apps/groups/static/groups/js/all_groups.js
@@ -1,7 +1,6 @@
 hqDefine("groups/js/all_groups.js", function() {
     $(function () {
         var $createGroupForm = $("#create_group_form");
-        debugger;
         $("button:submit", $createGroupForm).click(function(){
             ga_track_event("Groups", "Create Group", {
                'hitCallback': function () {
