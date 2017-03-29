1a61f7a8035dd4227d17a5a75541646769a78408
Manish Kangia <mkangia@dimagi.com>
2017-03-29 17:17:59 +0530

    use available constant for archive case

--- custom/enikshay/integrations/utils.py
+++ custom/enikshay/integrations/utils.py
@@ -1,6 +1,7 @@
 from corehq.apps.locations.models import SQLLocation
 from custom.enikshay.exceptions import NikshayLocationNotFound, ENikshayCaseNotFound
 from custom.enikshay.case_utils import get_person_case_from_episode
+from casexml.apps.case.const import ARCHIVED_CASE_OWNER_ID
 
 
 def _is_submission_from_test_location(case_id, owner_id):
@@ -32,7 +33,7 @@ def is_valid_archived_submission(episode_case):
     except ENikshayCaseNotFound:
         return False
     owner_id = person_case.owner_id
-    if owner_id == "_archive_":
+    if owner_id == ARCHIVED_CASE_OWNER_ID:
         owner_id = person_case.dynamic_case_properties().get('last_owner', None)
 
     return not _is_submission_from_test_location(person_case.case_id, owner_id)
