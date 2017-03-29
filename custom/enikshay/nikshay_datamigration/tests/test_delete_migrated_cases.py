164667b06959db52df385c40c61ef8e828a3538d
Nick Pellegrino <npellegrino@dimagi.com>
2017-03-29 15:25:30 -0400

    test deletion when cases are closed

--- custom/enikshay/nikshay_datamigration/tests/test_delete_migrated_cases.py
+++ custom/enikshay/nikshay_datamigration/tests/test_delete_migrated_cases.py
@@ -7,8 +7,27 @@ from custom.enikshay.nikshay_datamigration.tests.utils import NikshayMigrationMi
 @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
 class TestDeleteMigratedCases(NikshayMigrationMixin, TestCase):
 
-    def test_deletion(self):
+    def test_deletion_open_cases(self):
         self.outcome.HIVStatus = None
+        self.outcome.save()
+        call_command('create_enikshay_cases', self.domain)
+        assert len(self.case_accessor.get_case_ids_in_domain(type='person')) == 1
+        assert len(self.case_accessor.get_case_ids_in_domain(type='occurrence')) == 1
+        episode_case_ids = self.case_accessor.get_case_ids_in_domain(type='episode')
+        assert len(episode_case_ids) == 1
+        assert len(self.case_accessor.get_case_ids_in_domain(type='drtb-hiv-referral')) == 1
+
+        call_command('delete_migrated_cases', self.domain, *episode_case_ids)
+
+        self.assertListEqual(self.case_accessor.get_case_ids_in_domain(type='person'), [])
+        self.assertListEqual(self.case_accessor.get_case_ids_in_domain(type='occurrence'), [])
+        self.assertListEqual(self.case_accessor.get_case_ids_in_domain(type='episode'), [])
+        self.assertListEqual(self.case_accessor.get_case_ids_in_domain(type='drtb-hiv-referral'), [])
+
+    def test_deletion_closed_cases(self):
+        self.outcome.HIVStatus = None
+        self.outcome.Outcome = '1'
+        self.outcome.OutcomeDate = '2/01/2017'
         self.outcome.save()
         call_command('create_enikshay_cases', self.domain)
         assert len(self.case_accessor.get_case_ids_in_domain(type='person')) == 1
