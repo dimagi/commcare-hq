d1452f8da631aa18eb37d7e479106a0793858ff1
Simon Kelly <skelly@dimagi.com>
2017-03-31 20:38:23 +0200

    [ci skip] cleanup imports

--- corehq/apps/cleanup/management/commands/reprocess_error_forms.py
+++ corehq/apps/cleanup/management/commands/reprocess_error_forms.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from collections import defaultdict
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from corehq.form_processor.backends.sql.dbaccessors import FormAccessorSQL
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors
@@ -10,7 +10,6 @@ from corehq.form_processor.utils import should_use_sql_backend
 from corehq.form_processor.utils.xform import reprocess_xform_error
 from corehq.util.log import with_progress_bar
 from couchforms.dbaccessors import get_form_ids_by_type
-from dimagi.utils.parsing import string_to_datetime
 
 
 class Command(BaseCommand):
