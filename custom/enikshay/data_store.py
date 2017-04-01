81b30b634db79fd9332b324ab7656377e9d390d6
Sravan Reddy <sravfeyn@gmail.com>
2017-04-01 16:52:47 +0530

    Add adherence ES UCR

---
+++ custom/enikshay/data_store.py
@@ -0,0 +1,38 @@
+from django.utils.dateparse import parse_datetime
+
+from corehq.apps.es import filters
+from corehq.apps.userreports.models import StaticDataSourceConfiguration
+from corehq.apps.userreports.util import get_indicator_adapter
+
+from custom.enikshay.tasks import DOSE_KNOWN_INDICATORS
+
+
+class AdherenceDatastore(object):
+    # collection of adherence-data lookup queries that can be run on adherence UCR
+    def __init__(self, domain):
+        self.datasource = StaticDataSourceConfiguration.by_id("static-{}-adherence".format(domain))
+        self.adapter = get_indicator_adapter(self.datasource)
+        self.es = self.adapter.get_query_object().es
+
+    def _base_filters(self, episode_id):
+        return filters.AND(
+            filters.term('episode_id', episode_id),
+            filters.term('adherence_value', DOSE_KNOWN_INDICATORS)
+        )
+
+    def latest_adherence_date(self, episode_id):
+        result = self.es.filter(
+            self._base_filters(episode_id)
+        ).sort('adherence_date', desc=True).size(1).run().hits
+        print result, "ao89"
+        if len(result) > 0:
+            return parse_datetime(result[0].get('adherence_date', None))
+        else:
+            return None
+
+    def adherences_between(self, episode_id, start, end):
+        return self.es.filter(
+            self._base_filters(episode_id)
+        ).filter(
+            filters.date_range('adherence_date', gte=start, lte=end)
+        ).run().hits
---
+++ custom/enikshay/ucr/tests/test_adherence_datasource.py
@@ -0,0 +1,107 @@
+from datetime import datetime
+
+from django.test import TestCase
+
+from casexml.apps.case.mock import CaseFactory
+from corehq.apps.users.models import CommCareUser
+from corehq.apps.userreports.tasks import rebuild_indicators
+from corehq.form_processor.tests.utils import FormProcessorTestUtils
+
+
+from custom.enikshay.data_store import AdherenceDatastore
+from custom.enikshay.tasks import DOSE_UNKNOWN, DOSE_TAKEN_INDICATORS
+from custom.enikshay.tests.utils import (
+    get_person_case_structure,
+    get_adherence_case_structure,
+    get_occurrence_case_structure,
+    get_episode_case_structure
+)
+
+
+class TestAdherenceUCRSource(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestAdherenceUCRSource, cls).setUpClass()
+        cls.domain = "enikshay"
+        cls.data_store = AdherenceDatastore(cls.domain)
+        cls.user = CommCareUser.create(
+            cls.domain,
+            "ad-test@user",
+            "123",
+        )
+
+        cls._setup_casedata()
+
+    @classmethod
+    def _setup_casedata(cls):
+        cls.episode_id = "episode"
+        factory = CaseFactory(domain=cls.domain)
+
+        person = get_person_case_structure(
+            "person",
+            cls.user.user_id,
+        )
+
+        occurrence = get_occurrence_case_structure(
+            "occurence",
+            person
+        )
+
+        episode_structure = get_episode_case_structure(
+            cls.episode_id,
+            occurrence
+        )
+
+        cases = {case.case_id: case for case in factory.create_or_update_cases([episode_structure])}
+        cls.episode = cases[cls.episode_id]
+
+        def uid(index):
+            return "adherence{}".format(index)
+
+        adherence_data = [
+            (uid(1), datetime(2016, 1, 21, 1), DOSE_TAKEN_INDICATORS[0]),
+            (uid(2), datetime(2016, 1, 21, 3), DOSE_UNKNOWN),
+            (uid(3), datetime(2016, 1, 22), DOSE_TAKEN_INDICATORS[0]),
+            (uid(4), datetime(2016, 1, 24), DOSE_TAKEN_INDICATORS[0]),
+        ]
+        adherence_cases = factory.create_or_update_cases([
+            get_adherence_case_structure(
+                case_id,
+                cls.episode_id,
+                adherence_date,
+                extra_update={
+                    "name": adherence_date,
+                    "adherence_value": adherence_value,
+                }
+            )
+            for (case_id, adherence_date, adherence_value) in adherence_data
+        ])
+
+        by_case_id = {c.case_id: c for c in adherence_cases}
+        cls.latest_adherence_case = by_case_id[uid(4)]
+        cls.valid_adherence_value_cases = [by_case_id[uid(x)] for x in range(1, 4)]
+        cls.data = [
+            cls.latest_adherence_case
+        ] + cls.valid_adherence_value_cases
+
+        rebuild_indicators(cls.data_store.datasource._id)
+        cls.data_store.adapter.refresh_table()
+
+    @classmethod
+    def tearDownClass(cls):
+        FormProcessorTestUtils.delete_all_cases()
+        cls.user.delete()
+        cls.data_store.adapter.drop_table()
+        super(TestAdherenceUCRSource, cls).tearDownClass()
+
+    def test_max_adherence_date(self):
+        result = self.data_store.latest_adherence_date(self.episode_id)
+        self.assertEqual(result, datetime(2016, 1, 24))
+
+    def test_daterange(self):
+        rows = self.data_store.adherences_between(
+            self.episode_id, datetime(2016, 1, 21), datetime(2016, 1, 23)
+        )
+        # should exclude 1 'DOSE_UNKNOWN' and 1 out of range case
+        self.assertEqual(len(rows), 2)
