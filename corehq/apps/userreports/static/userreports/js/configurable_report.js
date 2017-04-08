0e58b87e0cd0904fffba1da073cbd0a42688baef
Jennifer Schweers <orange.jenny@gmail.com>
2017-04-08 16:14:54 -0400

    Move more js into configurable_report.js

--- corehq/apps/userreports/static/userreports/js/configurable_report.js
+++ corehq/apps/userreports/static/userreports/js/configurable_report.js
@@ -16,8 +16,41 @@ hqDefine("userreports/js/configurable_report.js", function() {
             gaTrackLink($editReportButton, 'Edit UCR', 'Edit UCR');
         }
 
-        _.each(initial_page_data("report_builder_events", function(e) {
+        _.each(initial_page_data("report_builder_events"), function(e) {
             window.analytics.usage.apply(this, e);
+        });
+
+        // Poll the status of the data source
+        if (!initial_page_data("is_static")) {
+            var retrying = false;
+            (function poll() {
+                $.ajax({
+                    url: hqImport("hqwebapp/js/urllib.js").reverse('configurable_data_source_status'),
+                    dataType: 'json',
+                    success: function(data) {
+                        if (data.isBuilt){
+                            $('#built-warning').addClass('hide');
+                            if (retrying){
+                                location.reload();
+                            } else if ($('#report-filters').find('.control-label').length === 0) {
+                                $('#report-filters').submit();
+                            }
+                        } else {
+                            retrying = true;
+                            $('#built-warning').removeClass('hide');
+                            setTimeout(poll, 5000);
+                        }
+                    }
+                });
+            })();
+        }
+
+        if (initial_page_data("created_by_builder")) {
+            window.analytics.usage(
+                    'Report Builder',
+                    initial_page_data("builder_report_type"),
+                    'Load a report that was built in report builder'
+            )
         }
     });
 });
