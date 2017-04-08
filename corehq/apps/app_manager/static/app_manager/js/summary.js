4fd020c25d6f61e035b2655aa5be4ae3c9c6ad06
Jennifer Schweers <orange.jenny@gmail.com>
2017-04-07 21:29:51 -0400

    Split out summary.js

---
+++ corehq/apps/app_manager/static/app_manager/js/summary.js
@@ -0,0 +1,47 @@
+hqDefine("app_manager/js/summary.js", function() {
+    $(function() {
+        'use strict';
+
+        var summaryApp = window.angular.module('summaryApp', ['ngRoute', 'summaryModule']),
+            initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get,
+            url = hqImport('hqwebapp/js/urllib.js').reverse;
+        summaryApp.config(['$httpProvider', function($httpProvider) {
+            $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+            $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+            $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+        }]);
+        summaryApp.config(["djangoRMIProvider", function(djangoRMIProvider) {
+            djangoRMIProvider.configure(initial_page_data('djng_current_rmi'));
+        }]);
+
+        summaryApp.constant('summaryConfig', {
+            staticRoot: initial_page_data('static_root'),
+            vellumTypes: initial_page_data('vellum_types'),
+            formNameMap: initial_page_data('form_name_map'),
+            appLangs: initial_page_data('app_langs'),
+            caseDownloadURL: url("download_case_summary"),
+            formDownloadURL: url("download_form_summary"),
+            appDownloadURL: url("download_app_summary"),
+            appSettingsURL: url("release_manager"),
+            appHomeURL: url("view_app"),
+            appName: initial_page_data("app_name"),
+        });
+
+        summaryApp.config(['$routeProvider', function($routeProvider) {
+            $routeProvider.
+                when('/forms', {
+                    templateUrl: url("ng_template", "form_summary_view"),
+                    controller: 'FormController',
+                }).
+                when('/cases', {
+                    templateUrl: url("ng_template", "case_summary_view"),
+                    controller: 'CaseController',
+                }).
+                otherwise({
+                    redirectTo: '/forms',
+                });
+        }]);
+
+        angular.bootstrap(document.getElementById("summary-app"), ["summaryApp"]);
+    });
+});
