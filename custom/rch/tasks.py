95ea013d72fa84001bbeced149567448c760abd3
Manish Kangia <mkangia@dimagi.com>
2017-04-16 04:59:18 +0530

    update celery tasks to pull data according to new model structure

--- custom/rch/tasks.py
+++ custom/rch/tasks.py
@@ -1,14 +1,14 @@
 from celery.task import periodic_task
 from celery.schedules import crontab
 
-from custom.rch.models import RCHMother, RCHChild
+from custom.rch.models import RCHRecord, MOTHER_DATA_TYPE, CHILD_DATA_TYPE
 
 
 @periodic_task(run_every=crontab(minute="0", hour="18"), queue='background_queue')
 def fetch_rch_mother_beneficiaries():
-    RCHMother.update_beneficiaries()
+    RCHRecord.update_beneficiaries(MOTHER_DATA_TYPE)
 
 
 @periodic_task(run_every=crontab(minute="15", hour="18"), queue='background_queue')
 def fetch_rch_child_beneficiaries():
-    RCHChild.update_beneficiaries()
+    RCHRecord.update_beneficiaries(CHILD_DATA_TYPE)
