9a794b0b8b420f4268d56de2566b391237d8b4a5
Jennifer Schweers <orange.jenny@gmail.com>
2017-04-24 13:29:53 -0400

    Move CommCare Translations UI

--- corehq/apps/app_manager/static/app_manager/js/app_view.js
+++ corehq/apps/app_manager/static/app_manager/js/app_view.js
@@ -1,3 +1,20 @@
 hqDefine("app_manager/js/app_view.js", function() {
+    $(function () {
+        var CommcareSettings = hqImport('app_manager/js/commcaresettings.js').CommcareSettings;
+        COMMCAREHQ.appView.settings = new CommcareSettings(COMMCAREHQ.appView.settings);
+        $('#commcare-settings').koApplyBindings(COMMCAREHQ.appView.settings);
 
+        // Set up typeahead for domain names
+        $("#id_domain").koApplyBindings({
+            domain_names: hqImport("hqwebapp/js/initial_page_data.js").get("domain_names"),
+        });
+
+        // Multimedia analytics
+        $(document).on("click", '#download_zip', function() {
+            ga_track_event('App Builder', 'Download Multimedia');
+        });
+        $(document).on("click", '#open_checker', function() {
+            ga_track_event('App Builder', 'Manage Multimedia');
+        });
+    });
 });
--- corehq/apps/app_manager/static/app_manager/js/app_view_application.js
+++ corehq/apps/app_manager/static/app_manager/js/app_view_application.js
@@ -1,3 +1,16 @@
 hqDefine("app_manager/js/app_manager_application.js", function() {
+    $(function() {
+        var initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get,
+            reverse = hqImport("hqwebapp/js/urllib.js").reverse;
 
+        var $translation_ui = $("#translations_ui");
+        mk_translation_ui({
+            translations: initial_page_data("translations"),
+            url: reverse("edit_app_ui_translations"),
+            suggestion_url: reverse("get_app_ui_translations"),
+            lang: initial_page_data("lang"),
+            allow_autofill: initial_page_data("lang") !== 'en',
+            $home: $translation_ui
+        });
+    });
 });
