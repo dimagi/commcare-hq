3714349bf06153aa354184d86f7df2de718c0e29
Jennifer Schweers <orange.jenny@gmail.com>
2017-04-24 13:47:21 -0400

    Move logic to copy app with feature flags

--- corehq/apps/app_manager/static/app_manager/js/app_view_application.js
+++ corehq/apps/app_manager/static/app_manager/js/app_view_application.js
@@ -3,6 +3,7 @@ hqDefine("app_manager/js/app_manager_application.js", function() {
         var initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get,
             reverse = hqImport("hqwebapp/js/urllib.js").reverse;
 
+        // Languages: CommCare Translations
         var $translation_ui = $("#translations_ui");
         mk_translation_ui({
             translations: initial_page_data("translations"),
@@ -11,6 +12,51 @@ hqDefine("app_manager/js/app_manager_application.js", function() {
             lang: initial_page_data("lang"),
             allow_autofill: initial_page_data("lang") !== 'en',
             $home: $translation_ui
+        });
+
+        // Copy app with feature flags
+        $("#copy-app-form form button").click(function() {
+          var $submit = $(this),
+            $form = $submit.closest("form"),
+            domain = $form.find("#id_domain").val(),
+            $modal = $("#copy-toggles");
+          if (initial_page_data("is_superuser")) {
+            $submit.disableButton();
+            $.ajax({
+              method: "GET",
+              url: reverse("toggle_diff"),
+              data: {
+                domain: domain,
+              },
+              success: function(toggles) {
+                if (toggles.length) {
+                  var template = _.template($modal.find("script").html()),
+                    $ul = $modal.find("ul").html("");
+                  _.each(toggles, function(toggle) {
+                    $ul.append(template(toggle));
+                  });
+                  $modal.modal().one("click", ".btn-primary", function() {
+                    $(this).disableButton();
+                    var slugs = _.map($modal.find(":checked"), function(c) {
+                        return $(c).data("slug");
+                    });
+                    $form.find("input[name='toggles']").val(slugs.join(","));
+                    $form.submit();
+                  }).one("hide.bs.modal", function() {
+                    $submit.enableButton();
+                  });
+                } else {
+                  $form.submit();
+                }
+              },
+              error: function() {
+                // If anything goes wrong, just submit the form
+                $form.submit();
+              },
+            });
+          } else {
+            $form.submit();
+          }
         });
     });
 });
