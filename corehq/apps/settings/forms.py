2deb9222aa7e646bc0f8ecd24c97651c56c9828a
Manish Kangia <mkangia@dimagi.com>
2017-04-27 03:15:43 +0530

    raise validation and add message in case old password is not decoded properly

--- corehq/apps/settings/forms.py
+++ corehq/apps/settings/forms.py
@@ -89,6 +89,10 @@ class HQPasswordChangeForm(EncodedPasswordChangeForm, PasswordChangeForm):
     def clean_old_password(self):
         from corehq.apps.hqwebapp.utils import decode_password
         old_password = decode_password(self.cleaned_data['old_password'])
+        if old_password == '':
+            raise ValidationError(
+                _("Password cannot be empty"), code='password_empty',
+            )
         if not self.user.check_password(old_password):
             raise forms.ValidationError(
                 self.error_messages['password_incorrect'],
