2a79bb36f17fd916d024d4dd74344d3d8bb926ac
Simon Kelly <skelly@dimagi.com>
2017-05-01 21:47:31 +0200

    UI for setting default sync interval

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1128,3 +1128,11 @@ MARK_LATEST_SUBMISSION_ON_USER = StaticToggle(
     [NAMESPACE_DOMAIN],
     always_enabled={'icds-cas'}
 )
+
+MOBIE_UCR_SYNC_DELAY_CONFIG = StaticToggle(
+    'mobile_ucr_sync_delay',
+    "Show settings for configuring mobile UCR sync delay",
+    TAG_EXPERIMENTAL,
+    [NAMESPACE_DOMAIN],
+    always_enabled=['icds-cas']
+)
