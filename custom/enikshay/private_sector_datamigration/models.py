32f446fa13aa448cc2810f615c505c399bec16e9
Nick Pellegrino <npellegrino@dimagi.com>
2017-05-01 18:20:35 -0400

    remove comment

--- custom/enikshay/private_sector_datamigration/models.py
+++ custom/enikshay/private_sector_datamigration/models.py
@@ -208,7 +208,7 @@ class Episode(models.Model):
     tsProviderType = models.CharField(max_length=255, null=True)
     unknownAdherencePct = models.DecimalField(decimal_places=10, max_digits=14)
     unresolvedMissedDosesPct = models.DecimalField(decimal_places=10, max_digits=14)
-    treatingHospital = models.CharField(max_length=10, null=True)  # this might be the agency that owns the patient
+    treatingHospital = models.CharField(max_length=10, null=True)
     treatmentCompletionInsentiveFlag = models.CharField(max_length=1, null=True)
     mermIMIEno = models.CharField(max_length=255, null=True)
     adherenceSupportAssigned = models.CharField(max_length=255, null=True)
