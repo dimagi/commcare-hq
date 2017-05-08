384a338055a52ccb056d2362d80e185d9e301935
Cal Ellowitz <calellowitz@gmail.com>
2017-05-08 18:11:32 +0530

    actual fixes

--- corehq/apps/analytics/views.py
+++ corehq/apps/analytics/views.py
@@ -31,7 +31,12 @@ class GreenhouseCandidateView(View):
         digester = hmac.new(settings.GREENHOUSE_API_KEY, request.body, hashlib.sha256)
         calculated_signature = digester.hexdigest()
 
-        if str(calculated_signature) == str(request.META.get('HTTP_SIGNATURE'.split()[1], '')):
+        signature_header = request.META.get('HTTP_SIGNATURE', '').split()
+        if len(signature_header) == 2:
+            signature_from_request = signature_header[1]
+        else:
+            return HttpResponse()
+        if str(calculated_signature) == str(signature_from_request):
             body_unicode = request.body.decode('utf-8')
             data = json.loads(body_unicode)
             try:
