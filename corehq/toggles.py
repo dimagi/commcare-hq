b858b7b10df00631dd1551c781e290bdaf14e5da
Ben Rudolph <rudolphben@gmail.com>
2017-05-09 08:57:09 +0200

    Update toggles.py

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1134,7 +1134,7 @@ BLOBDB_RESTORE = PredictablyRandomToggle(
     "Blobdb restore",
     TAG_PRODUCT_PATH,
     [NAMESPACE_DOMAIN],
-    randomness=0,
+    randomness=0.3,
 )
 
 SHOW_DEV_TOGGLE_INFO = StaticToggle(
