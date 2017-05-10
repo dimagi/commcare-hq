9afac31251e193cafe8f2c81eb453f29e6077ec0
Łukasz <lwyszomirski@soldevelo.com>
2017-05-10 16:14:32 +0200

    WIP: dashboard layout

---
+++ custom/icds_reports/static/js/system_usage.js
@@ -0,0 +1,5 @@
+angular.module('icdsApp').controller('SystemUsageController', function() {
+    this.data = {};
+    this.label = "Program Summary" // TODO add transtaltion support
+    this.step = void(0)
+});
