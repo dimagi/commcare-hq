fc7768d8ded9a57caa6cd2939c30a2bf76d3f448
Łukasz <lwyszomirski@soldevelo.com>
2017-05-11 12:40:12 +0200

    WIP: Program Summary

--- custom/icds_reports/static/js/system_usage.js
+++ custom/icds_reports/static/js/system_usage.js
@@ -1,5 +1,23 @@
 angular.module('icdsApp').controller('SystemUsageController', function() {
-    this.data = {};
-    this.label = "Program Summary" // TODO add transtaltion support
-    this.step = void(0)
+    var vm = this;
+    vm.data = {};
+    vm.label = "Program Summary";
+    vm.tooltipPlacement = "right";
+    vm.steps =[
+        { "slug": "system_usage", "label": "System Usage"},
+        { "slug": "maternal_child", "label": "Maternal & Child Health"},
+        { "slug": "icds_cas_reach", "label": "ICDS-CAS Reach"},
+        { "slug": "demographics", "label": "Demographics"},
+        { "slug": "awc_infrastructure", "label": "AWC Infrastructure"}
+    ];
+
+    vm.step = vm.steps[0];
+
+    this.goToStep = function(s) {
+        angular.forEach(this.steps, function(value, key) {
+            if (value.slug === s.slug) {
+                vm.step = value;
+            }
+        });
+    }
 });
