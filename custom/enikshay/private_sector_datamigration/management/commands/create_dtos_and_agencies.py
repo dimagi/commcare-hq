4f26af0040c41d6579d6781144c3fffc72652582
Nick Pellegrino <npellegrino@dimagi.com>
2017-05-12 10:02:22 -0400

    add agency migration

---
+++ custom/enikshay/private_sector_datamigration/management/commands/create_dtos_and_agencies.py
@@ -0,0 +1,78 @@
+from django.core.management import BaseCommand
+
+from corehq.apps.locations.models import SQLLocation, LocationType
+from custom.enikshay.private_sector_datamigration.models import Agency, UserDetail
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('domain')
+        parser.add_argument('state_code')
+        parser.add_argument('district_code')
+        parser.add_argument('parent_loc_id')
+        parser.add_argument('org_ids', metavar='org_id', nargs='*', type=int)
+
+    def handle(self, domain, state_code, district_code, parent_loc_id, org_ids, **options):
+        dto_parent = SQLLocation.active_objects.get(location_id=parent_loc_id)
+        for org_id in org_ids:
+            dto = self.create_dto(domain, state_code, district_code, dto_parent, org_id)
+            for agency in self.get_agencies_by_state_district_org(state_code, district_code, org_id):
+                self.create_agency(domain, agency, dto, org_id)
+
+    def create_dto(self, domain, state_code, district_code, dto_parent, org_id):
+        return SQLLocation.objects.create(
+            domain=domain,
+            name=self._get_org_name_by_id(org_id),
+            site_code='%s_%s_%d' % (state_code, district_code, org_id),
+            location_type=LocationType.objects.get(
+                domain=domain,
+                code='dto',
+            ),
+            parent=dto_parent,
+            metadata={
+                'enikshay_enabled': 'yes',
+                'is_test': 'no',
+                'private_sector_org_id': org_id,
+            },
+        )
+
+    @staticmethod
+    def get_agencies_by_state_district_org(state_code, district_code, org_id):
+        agency_ids = UserDetail.objects.filter(
+            isPrimary=True,
+        ).filter(
+            districtId=district_code,
+            organisationId=org_id,
+            stateId=state_code,
+        ).values('agencyId').distinct()
+        return Agency.objects.filter(agencyId__in=agency_ids)
+
+    @staticmethod
+    def create_agency(domain, agency, dto, org_id):
+        SQLLocation.objects.create(
+            domain=domain,
+            name=agency.agencyName,
+            site_code=str(agency.agencyId),
+            location_type=LocationType.objects.get(
+                domain=domain,
+                code=agency.location_type,
+            ),
+            parent=dto,
+            metadata={
+                'enikshay_enabled': 'yes',
+                'is_test': 'no',
+                'nikshay_code': agency.nikshayId,
+                'private_sector_agency_id': agency.agencyId,
+                'private_sector_org_id': org_id,
+
+            },
+        )
+
+    @staticmethod
+    def _get_org_name_by_id(org_id):
+        return {
+            1: 'PATH',
+            4: 'WHP',
+            5: 'DTO-Mehsana',
+        }[org_id]
