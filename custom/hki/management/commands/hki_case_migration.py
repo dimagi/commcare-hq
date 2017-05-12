7b90a91d73364de84041f77e1d19e4488c7f2698
Cal Ellowitz <calellowitz@gmail.com>
2017-05-12 12:06:37 +0530

    adding hki migration

---
+++ custom/hki/management/commands/hki_case_migration.py
@@ -0,0 +1,38 @@
+from django.core.management import BaseCommand
+
+from corehq.apps.hqcase.utils import update_case
+from corehq.apps.locations.models import SQLLocation
+from corehq.form_processor.models import CommCareCaseSQL
+
+
+class Command(BaseCommand):
+
+    def handle(self, **options):
+        domain = 'hki-nepal-suaahara-2'
+        loc_mapping = {}
+        locs = SQLLocation.objects.filter(domain=domain, level=4)
+        for loc in locs:
+            loc_mapping[loc.site_code] = loc.location_id
+
+        failed_updates = []
+        from corehq.sql_db.util import get_db_aliases_for_partitioned_query
+        dbs = get_db_aliases_for_partitioned_query()
+        for db in dbs:
+            for case_type in ('household', 'household_member'):
+                cases = CommCareCaseSQL.objects.using(db).filter(domain=domain, type=case_type)
+                for case in cases:
+                    try:
+                        if not case.get_case_property('location_migration_complete') == 'yes':
+                            level2_code = case.get_case_property('household_level2_code')
+                            ward_number = case.get_case_property('household_ward_number')
+                            new_site_code = '{}{}'.format(level2_code, ward_number.zfill(2))
+                            new_owner_id = loc_mapping.get(new_site_code, 'c9c86e46f57b4d2f81045e5250e03889')
+                            case_properties = {
+                                'owner_id': new_owner_id,
+                                'location_migration_complete': 'yes'
+                            }
+                            update_case(domain, case.case_id, case_properties)
+                    except:
+                        failed_updates.append(case.case_id)
+        for case in failed_updates:
+            print case
