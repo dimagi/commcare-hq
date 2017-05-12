ef16eaaf2d8b33c5160134b5450a0b6fc557676e
Ben Rudolph <brudolph@dimagi.com>
2017-05-12 13:22:14 +0200

    add docs

--- corehq/apps/cloudcare/esaccessors.py
+++ corehq/apps/cloudcare/esaccessors.py
@@ -10,6 +10,22 @@ def login_as_user_query(
         offset,
         can_access_all_locations=False,
         user_data_fields=None):
+    '''
+    Takes in various parameters to determine which users to populate the login as screen.
+
+    :param domain: String domain
+    :param couch_user: The CouchUser that is using the Login As feature
+    :param search_string: The query that filters the users returned. Filters based on the
+        `search_fields` as well as any fields defined in `user_data_fields`.
+    :param limit: The max amount of users returned.
+    :param offset: From where to start the query.
+    :param can_access_all_locations: When set to False, will limit the users returned by the
+        locations the user has visibility into.
+    :param user_data_fields: A list of custom user data fields that should also be searched
+        by the `search_string`
+
+    :returns: An EsQuery instance.
+    '''
     search_fields = ["base_username", "last_name", "first_name", "phone_numbers"]
 
     should_criteria_query = [
@@ -43,6 +59,10 @@ def login_as_user_query(
             queries.BOOL_CLAUSE(
                 queries.SHOULD_CLAUSE(
                     should_criteria_query,
+                    # It should either match on the search fields like username or it
+                    # should match on the custom user data fields. If this were 2, then
+                    # it would require the search string to match both on the search fields and
+                    # the custom user data fields.
                     minimum_should_match=1,
                 ),
             )
