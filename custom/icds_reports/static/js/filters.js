e600eaf8d1b7cabfb611ba00700158a9e8722834
Kamil Krampa <kkrampa@soldevelo.com>
2017-05-12 13:17:18 +0200

    Fix eslint errors, use compressor

--- custom/icds_reports/static/js/filters.js
+++ custom/icds_reports/static/js/filters.js
@@ -1,15 +1,17 @@
+var angular = window.angular;
+
 angular.module('icdsApp').directive("filters", function() {
     var url = hqImport('hqwebapp/js/urllib.js').reverse;
     return {
         restrict:'E',
         scope: {
-            data: '='
+            data: '=',
         },
         bindToController: true,
         templateUrl: url('icds-ng-template', 'filters'),
         controller: function() {},
-        controllerAs: "$ctrl"
-    }
+        controllerAs: "$ctrl",
+    };
 });
 
 angular.module('icdsApp').directive("locationFilter", function() {
@@ -17,22 +19,22 @@ angular.module('icdsApp').directive("locationFilter", function() {
     return {
         restrict:'E',
         scope: {
-            location: '='
+            location: '=',
         },
         bindToController: true,
         require: 'ngModel',
         templateUrl: url('icds-ng-template', 'location_filter'),
         controller: function LocationFilterControler() {
-          this.locations = [
-              {name: 'Location 1'},
-              {name: 'Location 2'},
-              {name: 'Location 3'},
-              {name: 'Location 4'},
-              {name: 'Location 5'}
-          ];
+            this.locations = [
+                {name: 'Location 1'},
+                {name: 'Location 2'},
+                {name: 'Location 3'},
+                {name: 'Location 4'},
+                {name: 'Location 5'},
+            ];
         },
-        controllerAs: "$locationCtrl"
-    }
+        controllerAs: "$locationCtrl",
+    };
 });
 
 angular.module('icdsApp').filter('propsFilter', function() {
@@ -40,29 +42,29 @@ angular.module('icdsApp').filter('propsFilter', function() {
         var out = [];
 
         if (angular.isArray(items)) {
-          var keys = Object.keys(props);
+            var keys = Object.keys(props);
 
-          items.forEach(function(item) {
-            var itemMatches = false;
+            items.forEach(function(item) {
+                var itemMatches = false;
 
-            for (var i = 0; i < keys.length; i++) {
-              var prop = keys[i];
-              var text = props[prop].toLowerCase();
-              if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
-                itemMatches = true;
-                break;
-              }
-            }
+                for (var i = 0; i < keys.length; i++) {
+                    var prop = keys[i];
+                    var text = props[prop].toLowerCase();
+                    if (item[prop].toString().toLowerCase().indexOf(text) !== -1) {
+                        itemMatches = true;
+                        break;
+                    }
+                }
 
-            if (itemMatches) {
-              out.push(item);
-            }
-          });
+                if (itemMatches) {
+                    out.push(item);
+                }
+            });
         } else {
-          // Let the output be the input untouched
-          out = items;
+            // Let the output be the input untouched
+            out = items;
         }
 
         return out;
-      };
+    };
 });
