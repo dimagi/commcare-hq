cc1938fb82ba5721b6470ab3fc827934df38dc98
Cal Ellowitz <calellowitz@gmail.com>
2017-05-12 15:26:43 +0530

    adding rough progress bar

--- custom/hki/management/commands/hki_case_migration.py
+++ custom/hki/management/commands/hki_case_migration.py
@@ -1,8 +1,10 @@
 from django.core.management import BaseCommand
 
+from corehq.apps.es import CaseES
 from corehq.apps.hqcase.utils import update_case
 from corehq.apps.locations.models import SQLLocation
 from corehq.form_processor.models import CommCareCaseSQL
+from corehq.util.log import with_progress_bar
 
 
 class Command(BaseCommand):
@@ -15,21 +17,22 @@ class Command(BaseCommand):
             loc_mapping[loc.site_code] = loc.location_id
 
         failed_updates = []
+        total_cases = CaseES().domain(domain).case_type('household').count()
         from corehq.sql_db.util import get_db_aliases_for_partitioned_query
         dbs = get_db_aliases_for_partitioned_query()
         for db in dbs:
             for case_type in ('household', 'household_member'):
                 cases = CommCareCaseSQL.objects.using(db).filter(domain=domain, type=case_type)
-                for case in cases:
+                for case in with_progress_bar(cases, total_cases/10/2):
                     try:
-                        if not case.get_case_property('location_migration_complete') == 'yes':
+                        if not case.get_case_property('location_migration_complete') is None:
                             level2_code = case.get_case_property('household_level2_code')
                             ward_number = case.get_case_property('household_ward_number')
                             new_site_code = '{}{}'.format(level2_code, ward_number.zfill(2))
                             new_owner_id = loc_mapping.get(new_site_code, 'c9c86e46f57b4d2f81045e5250e03889')
                             case_properties = {
                                 'owner_id': new_owner_id,
-                                'location_migration_complete': 'yes'
+                                'location_migration_complete': 'no' if 'c9c86e46f57b4d2f81045e5250e03889' else 'yes'
                             }
                             update_case(domain, case.case_id, case_properties)
                     except:
