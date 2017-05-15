f8d412d628790e3f72c23e47f5a04ca0115d4e29
Nick Pellegrino <npellegrino@dimagi.com>
2017-05-15 13:53:18 -0400

    replace treatment_initiated=yes_private with yes_pcp

--- custom/enikshay/private_sector_datamigration/factory.py
+++ custom/enikshay/private_sector_datamigration/factory.py
@@ -211,7 +211,7 @@ class BeneficiaryCaseFactory(object):
             kwargs['attrs']['update']['site'] = self._episode.site_property
             kwargs['attrs']['update']['site_choice'] = self._episode.site_choice
             kwargs['attrs']['update']['treatment_card_completed_date'] = self._episode.creationDate.date()
-            kwargs['attrs']['update']['treatment_initiated'] = 'yes_private'
+            kwargs['attrs']['update']['treatment_initiated'] = 'yes_pcp'
             kwargs['attrs']['update']['treatment_initiation_date'] = rx_start_datetime.date()
             kwargs['attrs']['update']['treatment_phase'] = self._episode.treatment_phase
             kwargs['attrs']['update']['weight'] = int(self._episode.patientWeight)
