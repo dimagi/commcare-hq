b6aea9c2509f3426416fa7bac16db11baedc4d98
Kamil Krampa <kkrampa@soldevelo.com>
2017-05-15 10:07:59 +0200

    Add mock data to the map

--- custom/icds_reports/static/js/directives/underweight-children-report/underweight-children-report.directive.js
+++ custom/icds_reports/static/js/directives/underweight-children-report/underweight-children-report.directive.js
@@ -1,16 +1,22 @@
 var url = hqImport('hqwebapp/js/urllib.js').reverse;
 
 
-function UnderweightChildrenReportController($routeParams) {
+function UnderweightChildrenReportController($routeParams, maternalChildService) {
     var vm = this;
     vm.step = $routeParams.step;
     vm.steps = [
         {route: '/underweight_children/1', label: 'MapView'},
         {route: '/underweight_children/2', label: 'ChartView'},
     ];
+
+    vm.mapData = {};
+
+    maternalChildService.getUnderweightChildrenData().then(function(response) {
+        vm.mapData = response.data;
+    });
 }
 
-UnderweightChildrenReportController.$inject = ['$routeParams'];
+UnderweightChildrenReportController.$inject = ['$routeParams', 'maternalChildService'];
 
 window.angular.module('icdsApp').directive('underweightChildrenReport', function() {
     return {
