3fe5bac8f198d0ce4f57ed88c481d632884160cc
Ethan Soergel <esoergel@gmail.com>
2017-05-15 15:41:59 -0400

    Bonus test

--- custom/enikshay/tests/test_voucher_counts.py
+++ custom/enikshay/tests/test_voucher_counts.py
@@ -34,3 +34,21 @@ class TestVoucherCounts(ENikshayCaseStructureMixin, TestCase):
                 "prescription_total_days_threshold_30": '2017-01-03T00:00:00.000000Z',
             }
         )
+
+    def test_thresholds(self):
+        prescription = self.create_prescription_case()
+        self.make_voucher(prescription, 29, datetime(2017, 1, 2))
+        self.make_voucher(prescription, 1, datetime(2017, 1, 1))
+        # It should hit 30 now, and should flp the order of the above two
+        self.make_voucher(prescription, 7, datetime(2017, 1, 3))
+        # This one should trigger both the 60 and 90 thresholds
+        self.make_voucher(prescription, 70, datetime(2017, 1, 4))
+        self.assertEqual(
+            EpisodeVoucherUpdate(self.domain, self.episode).update_json(),
+            {
+                "prescription_total_days": 29 + 1 + 7 + 70,
+                "prescription_total_days_threshold_30": '2017-01-02T00:00:00.000000Z',
+                "prescription_total_days_threshold_60": '2017-01-04T00:00:00.000000Z',
+                "prescription_total_days_threshold_90": '2017-01-04T00:00:00.000000Z',
+            }
+        )
