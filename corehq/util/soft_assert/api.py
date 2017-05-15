f9b82d0f29ca10c6264b57bb5c9f5319616e93ec
Simon Kelly <skelly@dimagi.com>
2017-05-15 14:30:28 +0530

    optionally include breadcrumbs in soft assert emails

--- corehq/util/soft_assert/api.py
+++ corehq/util/soft_assert/api.py
@@ -16,14 +16,15 @@ def _send_message(info, backend):
                  'Value: {info.obj!r}\n'
                  'Traceback:\n{info.traceback}\n'
                  'Request:\n{request}\n'
-                 'Occurrences to date: {info.count}\n').format(
+                 'Occurrences to date: {info.count}\n'
+                 'Breadcrumbs: {info.breadcrumbs}\n').format(
                 info=info, request=request_repr)
     )
 
 
 def soft_assert(to=None, notify_admins=False,
                 fail_if_debug=False, exponential_backoff=True, skip_frames=0,
-                send_to_ops=True,):
+                send_to_ops=True, include_breadcrumbs=False):
     """
     send an email with stack trace if assertion is not True
 
@@ -109,4 +110,5 @@ def soft_assert(to=None, notify_admins=False,
         send=send,
         use_exponential_backoff=exponential_backoff,
         skip_frames=skip_frames,
+        include_breadcrumbs=include_breadcrumbs
     )
