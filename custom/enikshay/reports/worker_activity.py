7d758ab2099ce39c7fe31cd2030839da2a8925cb
Kamil Krampa <kkrampa@soldevelo.com>
2017-05-17 08:38:29 +0200

    Switch to ajax pagination

--- custom/enikshay/reports/worker_activity.py
+++ custom/enikshay/reports/worker_activity.py
@@ -1,19 +1,23 @@
 from corehq.apps.es import UserES
 from corehq.apps.es.users import mobile_users
 from corehq.apps.reports.analytics.esaccessors import get_submission_counts_by_user
-from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn, DTSortType
+from corehq.apps.reports.datatables import DataTablesHeader, DataTablesColumn
 from corehq.apps.reports.filters.dates import DatespanFilter
 from corehq.apps.reports.standard import CustomProjectReport
 from corehq.apps.reports.standard.monitoring import WorkerActivityReport
-from corehq.apps.reports.util import get_simplified_users, numcell
+from corehq.apps.reports.util import get_simplified_users
 from custom.enikshay.reports.filters import EnikshayLocationFilter
 
 from django.utils.translation import ugettext as _
+
+from dimagi.utils.decorators.memoized import memoized
 
 
 class EnikshayWorkerActivityReport(WorkerActivityReport, CustomProjectReport):
     slug = 'enikshay_worker_activity_report'
     num_avg_intervals = 4
+    ajax_pagination = True
+    is_cacheable = False
 
     @property
     def fields(self):
@@ -23,33 +27,61 @@ class EnikshayWorkerActivityReport(WorkerActivityReport, CustomProjectReport):
     def headers(self):
         return DataTablesHeader(
             DataTablesColumn(_("User")),
-            DataTablesColumn(_("# Forms Submitted"), sort_type=DTSortType.NUMERIC),
-            DataTablesColumn(_("Avg # Forms Submitted"), sort_type=DTSortType.NUMERIC),
-            DataTablesColumn(_("Last Form Submission"))
+            DataTablesColumn(_("# Forms Submitted"), sortable=False),
+            DataTablesColumn(_("Avg # Forms Submitted"), sortable=False),
+            DataTablesColumn(_("Last Form Submission"), sortable=False)
         )
 
     @property
-    def users(self):
-        location_ids = EnikshayLocationFilter.get_value(self.request, self.domain)
+    def locations_id(self):
+        return EnikshayLocationFilter.get_value(self.request, self.domain)
+
+    @property
+    def user_query(self):
         user_query = UserES().domain(self.domain).filter(mobile_users())
+        location_ids = self.locations_id
         if location_ids:
             user_query = user_query.location(location_ids)
-        return get_simplified_users(user_query)
+        return user_query
+
+    @property
+    @memoized
+    def users(self):
+        return get_simplified_users(
+            self.user_query
+                .start(self.pagination.start)
+                .size(self.pagination.count)
+                .sort('username', desc=self.pagination.desc)
+        )
+
+    @property
+    def user_ids(self):
+        return map(lambda u: u['user_id'], self.users)
 
     @property
     def rows(self):
         rows = []
-        submissions_by_user = get_submission_counts_by_user(self.domain, self.datespan)
-        avg_submissions_by_user = get_submission_counts_by_user(self.domain, self.avg_datespan)
+        submissions_by_user = get_submission_counts_by_user(self.domain, self.datespan, self.user_ids)
+        avg_submissions_by_user = get_submission_counts_by_user(self.domain, self.avg_datespan, self.user_ids)
         last_form_by_user = self.es_last_submissions()
 
         for user in self.users:
             rows.append([
                 user['username_in_report'],
                 submissions_by_user.get(user['user_id'], 0),
-                numcell(
-                    int(avg_submissions_by_user.get(user["user_id"], 0)) / float(self.num_avg_intervals)
-                ),
+                int(avg_submissions_by_user.get(user["user_id"], 0)) / float(self.num_avg_intervals),
                 last_form_by_user.get(user["user_id"]) or _(self.NO_FORMS_TEXT),
             ])
         return rows
+
+    @property
+    def shared_pagination_GET_params(self):
+        return [
+            dict(name='startdate', value=self.datespan.startdate_display),
+            dict(name='enddate', value=self.datespan.enddate_display),
+            dict(name='locations_id', value=self.request.GET.getlist('locations_id')),
+        ]
+
+    @property
+    def total_records(self):
+        return self.user_query.count()
