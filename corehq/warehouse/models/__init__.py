2180950f2223350faa6b844558e3abcc10a75d6f
Ben Rudolph <brudolph@dimagi.com>
2017-05-18 21:45:13 +0200

    Add domain staging table

--- corehq/warehouse/models/__init__.py
+++ corehq/warehouse/models/__init__.py
@@ -13,4 +13,5 @@ from corehq.warehouse.models.meta import (
 
 from corehq.warehouse.models.staging import (
     GroupStagingTable,
+    DomainStagingTable,
 )
--- corehq/warehouse/models/staging.py
+++ corehq/warehouse/models/staging.py
@@ -5,8 +5,12 @@ from django.db import models, transaction, connections
 from dimagi.utils.couch.database import iter_docs
 
 from corehq.sql_db.routers import db_for_read_write
-from corehq.apps.groups.dbaccessors import get_group_ids_by_last_modified
 from corehq.apps.groups.models import Group
+from corehq.apps.domain.models import Domain
+from corehq.warehouse.dbaccessors import (
+    get_group_ids_by_last_modified,
+    get_domain_ids_by_last_modified,
+)
 from corehq.warehouse.utils import django_batch_records
 
 
@@ -60,3 +64,49 @@ class GroupStagingTable(StagingTable):
         group_ids = get_group_ids_by_last_modified(start_datetime, end_datetime)
 
         return iter_docs(Group.get_db(), group_ids)
+
+
+class DomainStagingTable(StagingTable):
+    slug = 'domain_staging'
+
+    domain_id = models.CharField(max_length=255)
+    default_timezone = models.CharField(max_length=255)
+    hr_name = models.CharField(max_length=255, null=True)
+    creating_user_id = models.CharField(max_length=255, null=True)
+    project_type = models.CharField(max_length=255, null=True)
+
+    is_active = models.BooleanField()
+    case_sharing = models.BooleanField()
+    commtrack_enabled = models.BooleanField()
+    is_test = models.CharField(max_length=255)
+    location_restriction_for_users = models.BooleanField()
+    use_sql_backend = models.NullBooleanField()
+    first_domain_for_user = models.NullBooleanField()
+
+    domain_last_modified = models.DateTimeField()
+    domain_created_on = models.DateTimeField()
+
+    FIELD_MAPPING = [
+        ('_id', 'domain_id'),
+        ('default_timezone', 'default_timezone'),
+        ('hr_name', 'hr_name'),
+        ('creating_user_id', 'creating_user_id'),
+        ('project_type', 'project_type'),
+
+        ('is_active', 'is_active'),
+        ('case_sharing', 'case_sharing'),
+        ('commtrack_enabled', 'commtrack_enabled'),
+        ('is_test', 'is_test'),
+        ('location_restriction_for_users', 'location_restriction_for_users'),
+        ('use_sql_backend', 'use_sql_backend'),
+        ('first_domain_for_user', 'first_domain_for_user'),
+
+        ('last_modified', 'domain_last_modified'),
+        ('date_created', 'domain_created_on'),
+    ]
+
+    @classmethod
+    def raw_record_iter(cls, start_datetime, end_datetime):
+        domain_ids = get_domain_ids_by_last_modified(start_datetime, end_datetime)
+
+        return iter_docs(Domain.get_db(), domain_ids)
