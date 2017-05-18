e7b301c48954d8d3ff6a93d48935eb53eb8b6d41
Ben Rudolph <brudolph@dimagi.com>
2017-05-18 22:02:10 +0200

    bulk insert 1000 at a time

--- corehq/warehouse/utils.py
+++ corehq/warehouse/utils.py
@@ -1,10 +1,17 @@
+from corehq.warehouse.const import DJANGO_MAX_BATCH_SIZE
+
 
 def django_batch_records(cls, record_iter, field_mapping):
     records = []
-    for raw_record in record_iter:
+    for index, raw_record in enumerate(record_iter):
         record = {}
         for source_key, destination_key in field_mapping:
             record[destination_key] = raw_record.get(source_key)
 
         records.append(cls(**record))
-    cls.objects.bulk_create(records)
+        if index % DJANGO_MAX_BATCH_SIZE == 0 and index > 0:
+            cls.objects.bulk_create(records)
+            records = []
+
+    if records:
+        cls.objects.bulk_create(records)
