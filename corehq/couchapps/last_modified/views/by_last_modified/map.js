70cd254e137a3c600967d904ec76336c1c081409
Ben Rudolph <brudolph@dimagi.com>
2017-05-23 08:21:44 +0200

    doc_type -> base_doc

--- corehq/couchapps/last_modified/views/by_last_modified/map.js
+++ corehq/couchapps/last_modified/views/by_last_modified/map.js
@@ -3,7 +3,7 @@ function(doc){
         emit([doc.doc_type, doc.last_modified], null);
     } else if (doc.doc_type === "Domain" || doc.doc_type === "Domain-Deleted") {
         emit([doc.doc_type, doc.last_modified], null);
-    } else if (doc.base_doc && (doc.base_doc === "CouchUser" || doc.doc_type === "CouchUser-Deleted")) {
+    } else if (doc.base_doc && (doc.base_doc === "CouchUser" || doc.base_doc === "CouchUser-Deleted")) {
         emit([doc.base_doc, doc.last_modified], null);
     }
 }
