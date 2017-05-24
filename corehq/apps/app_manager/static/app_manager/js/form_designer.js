19a3826d94ebcee18ec1dc5d17a5004eb2df5102
Biyeun Buczyk <biyeun@dimagi.com>
2017-05-23 21:53:39 -0400

    update form name in form navigation upon change of form name within form builder

--- corehq/apps/app_manager/static/app_manager/js/form_designer.js
+++ corehq/apps/app_manager/static/app_manager/js/form_designer.js
@@ -152,6 +152,7 @@ hqDefine("app_manager/js/form_designer.js", function() {
             );
             editDetails.setUpdateCallbackFn(function (name) {
                 $('#formdesigner .fd-content-left .fd-head-text').text(name);
+                $('.variable-form_name').text(name);
                 $('#edit-form-name-modal').modal('hide');
             });
             $('#edit-form-name-modal').koApplyBindings(editDetails);
