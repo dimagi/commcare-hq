20c555fe75b143f43514ff19c525997017430cf3
Ben Rudolph <brudolph@dimagi.com>
2017-05-24 10:50:12 +0200

    fix call to get_form_by_xmlns

--- corehq/apps/reports/models.py
+++ corehq/apps/reports/models.py
@@ -964,9 +964,9 @@ class FormExportSchema(HQExportSchema):
     def uses_cases(self):
         if not self.app or isinstance(self.app, RemoteApp):
             return False
-        form = self.app.get_form_by_xmlns(self.xmlns)
-        if form and isinstance(form, Form):
-            return bool(form.active_actions())
+        forms = self.app.get_forms_by_xmlns(self.xmlns)
+        if forms and isinstance(forms[0], Form):
+            return bool(forms[0].active_actions())
         return False
 
 
