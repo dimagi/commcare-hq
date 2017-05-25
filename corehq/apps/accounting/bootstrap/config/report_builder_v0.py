4a2ff674aeb8f0ea6841aeb42e9bb23e33b940ff
Nick Pellegrino <npellegrino@dimagi.com>
2017-05-24 19:34:26 -0700

    require specifying monthly_fee for product rate in bootstrap configs

--- corehq/apps/accounting/bootstrap/config/report_builder_v0.py
+++ corehq/apps/accounting/bootstrap/config/report_builder_v0.py
@@ -8,7 +8,7 @@ from corehq.apps.accounting.models import (
 BOOTSTRAP_CONFIG = {
     (SoftwarePlanEdition.STANDARD, False, True): {
         'role': 'standard_plan_report_builder_v0',
-        'product_rate': dict(monthly_fee=Decimal('100.00')),
+        'product_rate_monthly_fee': Decimal('100.00'),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=100, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=100),
@@ -16,7 +16,7 @@ BOOTSTRAP_CONFIG = {
     },
     (SoftwarePlanEdition.PRO, False, True): {
         'role': 'pro_plan_report_builder_v0',
-        'product_rate': dict(monthly_fee=Decimal('500.00')),
+        'product_rate_monthly_fee': Decimal('500.00'),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=500, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=500),
@@ -24,7 +24,7 @@ BOOTSTRAP_CONFIG = {
     },
     (SoftwarePlanEdition.ADVANCED, False, True): {
         'role': 'advanced_plan_report_builder_v0',
-        'product_rate': dict(monthly_fee=Decimal('1000.00')),
+        'product_rate_monthly_fee': Decimal('1000.00'),
         'feature_rates': {
             FeatureType.USER: dict(monthly_limit=1000, per_excess_fee=Decimal('2.00')),
             FeatureType.SMS: dict(monthly_limit=1000),
