e2729a9cae5f6f9512b47f95c9ba9c1d61926c5a
Manish Kangia <mkangia@dimagi.com>
2017-05-29 15:03:25 +0530

    https seems to be working now, update link

--- corehq/messaging/smsbackends/vertex/models.py
+++ corehq/messaging/smsbackends/vertex/models.py
@@ -6,7 +6,7 @@ from corehq.messaging.smsbackends.http.models import SQLSMSBackend
 from corehq.messaging.smsbackends.vertex.forms import VertexBackendForm
 from corehq.apps.sms.util import strip_plus
 
-VERTEX_URL = "http://www.smsjust.com/sms/user/urlsms.php"
+VERTEX_URL = "https://www.smsjust.com/sms/user/urlsms.php"
 SUCCESS_RESPONSE_REGEX = r'^(\d+)-(20\d{2})_(\d{2})_(\d{2})$'  # 570737298-2017_05_27
 SUCCESS_RESPONSE_REGEX_MATCHER = re.compile(SUCCESS_RESPONSE_REGEX)
 
