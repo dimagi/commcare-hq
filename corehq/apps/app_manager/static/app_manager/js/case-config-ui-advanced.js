4524261bcd731f4a483613eb86f940f6c0649593
Jennifer Schweers <orange.jenny@gmail.com>
2017-05-30 17:03:12 -0400

    Remove inline js from form_view_advanced.html

--- corehq/apps/app_manager/static/app_manager/js/case-config-ui-advanced.js
+++ corehq/apps/app_manager/static/app_manager/js/case-config-ui-advanced.js
@@ -1257,7 +1257,22 @@ hqDefine('app_manager/js/case-config-ui-advanced.js', function () {
         }
     };
 
-    return {
-        CaseConfig: CaseConfig
-    };
+    $(function() {
+        var initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get;
+        if (initial_page_data('has_form_source')) {
+            var caseConfig = new CaseConfig(_.extend({}, initial_page_data("case_config_options"), {
+                home: $('#case-config-ko'),
+                requires: ko.observable(initial_page_data("form_requires")),
+            }));
+            caseConfig.init();
+
+            if (initial_page_data("schedule_options")) {
+                var VisitScheduler = hqImport('app_manager/js/visit-scheduler.js');
+                var visitScheduler = new VisitScheduler.Scheduler(_.extend({}, initial_page_data("schedule_options"), {
+                    home: $('#visit-scheduler'),
+                }));
+                visitScheduler.init();
+            }
+        }
+    });
 });
