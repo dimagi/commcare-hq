b7b33d7b6f4563c500cb8a30c46646ab5ee2a339
Jennifer Schweers <orange.jenny@gmail.com>
2017-05-31 16:10:32 -0400

    Get rid of form_view_careplan.html

--- corehq/apps/app_manager/views/forms.py
+++ corehq/apps/app_manager/views/forms.py
@@ -654,8 +654,8 @@ def get_form_view_context_and_template(request, domain, form, langs, messages=me
         })
         template = get_app_manager_template(
             request.user,
-            "app_manager/v1/form_view_careplan.html",
-            "app_manager/v2/form_view_careplan.html",
+            "app_manager/v1/form_view_base.html",
+            "app_manager/v2/form_view_base.html",
         )
     elif isinstance(form, AdvancedForm):
         def commtrack_programs():
