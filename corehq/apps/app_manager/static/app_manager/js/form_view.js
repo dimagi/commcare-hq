59162cea941250a4fd51cd35cbfaa7d290b8f774
Jennifer Schweers <orange.jenny@gmail.com>
2017-06-06 09:49:59 -0400

    move xform event handling and fix some syntax errors

--- corehq/apps/app_manager/static/app_manager/js/form_view.js
+++ corehq/apps/app_manager/static/app_manager/js/form_view.js
@@ -1,9 +1,9 @@
 hqDefine("app_manager/js/form_view.js", function() {
     var initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get;
     $(function (){
-        if (COMMCAREHQ.toggleEnabled(CUSTOM_INSTANCES)) {
+        if (COMMCAREHQ.toggleEnabled('CUSTOM_INSTANCES')) {
             var customInstances = hqImport('app_manager/js/custom_intances.js').wrap({
-                customInstances: initial_page_data('custom_instances');
+                customInstances: initial_page_data('custom_instances'),
             });
             $('#custom-instances').koApplyBindings(customInstances);
         }
@@ -13,5 +13,41 @@ hqDefine("app_manager/js/form_view.js", function() {
 
         // Data dictionary descriptions for case properties
         $('.property-description').popover();
+
+        // Advanced > XForm > Upload
+        (function(){
+            $("#xform_file_input").change(function(){
+                if ($(this).val()) {
+                    $("#xform_file_submit").show();
+                } else {
+                    $("#xform_file_submit").hide();
+                }
+            }).trigger('change');
+        }());
+
+        // Advanced > XForm > View
+        $("#xform-source-opener").click(function(evt){
+            if (evt.shiftKey) {
+                // Shift+click: edit form source
+                $(".source-readonly").hide();
+                $(".source-edit").show();
+                $.get($(this).data('href'), function (data) {
+                    $("#xform-source-edit").text(data).blur();
+                }, 'json');
+            } else {
+                // Plain click: view form source
+                $(".source-edit").hide();
+                $(".source-readonly").show();
+                $("#xform-source").text("Loading...");
+                $.get($(this).data('href'), function (data) {
+                    var brush = new SyntaxHighlighter.brushes.Xml();
+                    brush.init({ toolbar: false });
+                    // brush.getDiv seems to escape inconsistently, so I'm helping it out
+                    data = data.replace(/&/g, '&amp;');
+                    $("#xform-source").html(brush.getDiv(data));
+                }, 'json');
+            }
+            $(".xml-source").modal();
+        });
     });
 });
