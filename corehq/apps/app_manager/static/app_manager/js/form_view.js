b5920e81f9fffb079ecedd6e44dad18cb826a128
Jennifer Schweers <orange.jenny@gmail.com>
2017-06-06 10:36:21 -0400

    move form filter logic into form_view.js

--- corehq/apps/app_manager/static/app_manager/js/form_view.js
+++ corehq/apps/app_manager/static/app_manager/js/form_view.js
@@ -1,6 +1,72 @@
 hqDefine("app_manager/js/form_view.js", function() {
+    function formFilterMatches(filter, pattern_matches, substring_matches) {
+        if (typeof(filter) != 'string') {
+            return false;
+        }
+
+        var result = false;
+        $.each(pattern_matches, function(index, pattern) {
+            result = result || filter.match(pattern);
+        });
+
+        $.each(substring_matches, function(index, sub) {
+            result = result || filter.indexOf(sub) != -1;
+        });
+
+        return result;
+    }
+
+    function FormFilter() {
+        var self = this;
+        var patterns = initial_page_data('form_filter_patterns');
+
+        self.formFilter = ko.observable(initial_page_data('form_filter'));
+
+        self.caseReferenceNotAllowed = ko.computed(function() {
+            var moduleUsesCase = initial_page_data('all_other_forms_require_a_case') && initial_page_data('form_requires') === 'case';
+            if (!moduleUsesCase || initial_page_data('put_in_root')) {
+                // We want to determine here if the filter expression references
+                // any case but the user case.
+                var filter = self.formFilter();
+                if (typeof(filter) !== 'string') {
+                    return true;
+                }
+
+                $.each(patterns.usercase_substring, function(index, sub) {
+                    filter = filter.replace(sub, '');
+                });
+
+                if (formFilterMatches(filter, patterns.case, patterns.case_substring)) {
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        self.userCaseReferenceNotAllowed = ko.computed(function() {
+            return !initial_page_data('is_usercase_in_use') && formFilterMatches(
+                self.formFilter(), patterns.usercase, patterns.usercase_substring
+            );
+        });
+
+        self.allowed = ko.computed(function () {
+            return !self.formFilter() || !self.caseReferenceNotAllowed() && !self.userCaseReferenceNotAllowed();
+        });
+    }
+
     var initial_page_data = hqImport("hqwebapp/js/initial_page_data.js").get;
     $(function (){
+        // Validation for build
+        var setupValidation = hqImport('app_manager/js/app_manager.js').setupValidation;
+        setupValidation(hqImport("hqwebapp/js/urllib.js").reverse("validate_form_for_build"));
+
+        // Settings > Display Condition
+        var $formFilter = $('#form-filter');
+        if ($formFilter.length && initial_page_data('allow_form_filtering')) {
+            $('#form-filter').koApplyBindings(new FormFilter());
+        }
+
+        // Settings > Custom Instances
         if (COMMCAREHQ.toggleEnabled('CUSTOM_INSTANCES')) {
             var customInstances = hqImport('app_manager/js/custom_intances.js').wrap({
                 customInstances: initial_page_data('custom_instances'),
@@ -8,10 +74,7 @@ hqDefine("app_manager/js/form_view.js", function() {
             $('#custom-instances').koApplyBindings(customInstances);
         }
 
-        var setupValidation = hqImport('app_manager/js/app_manager.js').setupValidation;
-        setupValidation(hqImport("hqwebapp/js/urllib.js").reverse("validate_form_for_build"));
-
-        // Data dictionary descriptions for case properties
+        // Case Management > Data dictionary descriptions for case properties
         $('.property-description').popover();
 
         // Advanced > XForm > Upload
