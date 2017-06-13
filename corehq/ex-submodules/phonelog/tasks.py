c92a9b7b805da381ab217901eb67f4408fd0a995
Simon Kelly <skelly@dimagi.com>
2017-06-13 15:55:39 +0200

    add date to UserEntry records and delete them after 60 days

--- corehq/ex-submodules/phonelog/tasks.py
+++ corehq/ex-submodules/phonelog/tasks.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from celery.schedules import crontab
 from celery.task import periodic_task
 from django.conf import settings
-from phonelog.models import DeviceReportEntry, UserErrorEntry, ForceCloseEntry
+from phonelog.models import DeviceReportEntry, UserErrorEntry, ForceCloseEntry, UserEntry
 
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
@@ -11,3 +11,4 @@ def purge_old_device_report_entries():
     DeviceReportEntry.objects.filter(server_date__lt=max_age).delete()
     UserErrorEntry.objects.filter(server_date__lt=max_age).delete()
     ForceCloseEntry.objects.filter(server_date__lt=max_age).delete()
+    UserEntry.objects.filter(server_date__lt=max_age).delete()
