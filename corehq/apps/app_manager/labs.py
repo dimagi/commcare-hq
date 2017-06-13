87586d059506b9de6b166346350b0e456fde88ed
Jennifer Schweers <orange.jenny@gmail.com>
2017-06-13 18:18:46 -0400

    translate names and descriptions

--- corehq/apps/app_manager/labs.py
+++ corehq/apps/app_manager/labs.py
@@ -1,4 +1,6 @@
 from collections import namedtuple
+from django.utils.translation import ugettext_lazy as _
+
 from dimagi.utils.decorators.memoized import memoized
 
 from corehq import feature_previews
@@ -26,12 +28,12 @@ _LABS = {
         #used_in_module=TODO
     ),
     "case_detail_overwrite": Lab(
-        name="Case Detail Overwrite",
-        description="Ability to overwrite one case list or detail's settings with another's",
+        name=_("Case Detail Overwrite"),
+        description=_("Ability to overwrite one case list or detail's settings with another's"),
     ),
     "case_list_menu_item": Lab(
-        name="Case List Menu Item",
-        description="TODO",
+        name=_("Case List Menu Item"),
+        description=_("TODO"),
         used_in_module=lambda m: isinstance(m, Module) and (m.case_list.show or m.task_list.show),  # TODO: will this break anything?
     ),
     "conditional_enum": Lab(
@@ -40,19 +42,19 @@ _LABS = {
         #used_in_module=TODO
     ),
     "conditional_form_actions": Lab(
-        name='Allow opening or closing bases based on a condition ("Only if the answer to...")',
-        description="Allow changing form actions, deleting registration forms (TODO: rephrase?)",
+        name=_('Allow opening or closing bases based on a condition ("Only if the answer to...")'),
+        description=_("Allow changing form actions, deleting registration forms (TODO: rephrase?)"),
         used_in_form=lambda f: f.actions.open_case.condition.type == 'if' or f.actions.close_case.condition.type, # TODO: will this break advanced forms?
     ),
     "display_conditions": Lab(
-        name="Form and Menu Display Conditions",
-        description="TODO",
+        name=_("Form and Menu Display Conditions"),
+        description=_("TODO"),
         used_in_form=lambda f: bool(f.form_filter),
         used_in_module=lambda m: bool(m.module_filter),
     ),
     "edit_form_actions": Lab(
-        name="Editing Form Actions",
-        description="Allow changing form actions and deleting registration forms",
+        name=_("Editing Form Actions"),
+        description=_("Allow changing form actions and deleting registration forms"),
     ),
     "enum_image": Lab(
         name=feature_previews.ENUM_IMAGE.label,
@@ -60,23 +62,23 @@ _LABS = {
         #used_in_module=TODO
     ),
     "menu_mode": Lab(
-        name="Menu Mode",
-        description="TODO",
+        name=_("Menu Mode"),
+        description=_("TODO"),
         used_in_module=lambda m: m.put_in_root,
     ),
     "register_from_case_list": Lab(
-        name="Register from case list",
-        description="TODO",
+        name=_("Register from case list"),
+        description=_("TODO"),
         used_in_module=lambda m: m.case_list_form.form_id, # TODO: break anything?
     ),
     "subcases": Lab(
-        name="Child Cases",
-        description="TODO",
+        name=_("Child Cases"),
+        description=_("TODO"),
         used_in_form=lambda f: bool(f.actions.subcases),    # TODO: will this break anything?
     ),
     "unstructured_case_lists": Lab(
-        name="Customize Case List Registration",
-        description="Create new case lists without a registration form, and allow deletion of registration forms.",
+        name=_("Customize Case List Registration"),
+        description=_("Create new case lists without a registration form, and allow deletion of registration forms."),
     ),
 }
 
