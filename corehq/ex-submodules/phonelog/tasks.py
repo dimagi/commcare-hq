275c9b41423db8ac3168ab6ea9cb3ca7522b86bd
Simon Kelly <skelly@dimagi.com>
2017-06-13 15:45:37 +0200

    delete all old device log models

--- corehq/ex-submodules/phonelog/tasks.py
+++ corehq/ex-submodules/phonelog/tasks.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 from celery.schedules import crontab
 from celery.task import periodic_task
 from django.conf import settings
-from phonelog.models import DeviceReportEntry
+from phonelog.models import DeviceReportEntry, UserErrorEntry, ForceCloseEntry
 
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def purge_old_device_report_entries():
     max_age = datetime.utcnow() - timedelta(days=60)
     DeviceReportEntry.objects.filter(server_date__lt=max_age).delete()
+    UserErrorEntry.objects.filter(server_date__lt=max_age).delete()
+    ForceCloseEntry.objects.filter(server_date__lt=max_age).delete()
