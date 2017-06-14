89f91263ca6af73e7316bc7fae23a6d0118648b7
Ethan Soergel <esoergel@gmail.com>
2017-06-14 17:09:59 +0200

    Save only once after signal

--- custom/enikshay/user_setup.py
+++ custom/enikshay/user_setup.py
@@ -160,10 +160,13 @@ def get_site_code(name, nikshay_code, type_code, parent):
 def save_user_callback(sender, couch_user, **kwargs):
     commcare_user = couch_user  # django signals enforce param names
     if toggles.ENIKSHAY.enabled(commcare_user.domain):
+        changed = False
         if kwargs.get('is_new_user', False):
-            set_default_role(commcare_user.domain, commcare_user)
-        set_issuer_id(commcare_user.domain, commcare_user)
-        add_drtb_hiv_to_dto(commcare_user.domain, commcare_user)
+            changed = set_default_role(commcare_user.domain, commcare_user) or changed
+        changed = set_issuer_id(commcare_user.domain, commcare_user) or changed
+        changed = add_drtb_hiv_to_dto(commcare_user.domain, commcare_user) or changed
+        if changed:
+            commcare_user.save()
 
 
 def set_default_role(domain, commcare_user):
@@ -257,10 +260,7 @@ def set_issuer_id(domain, user):
         user.user_data['id_device_body'] = compress_nikshay_id(device_number, 0)
         changed = True
 
-    if changed:
-        # note that this is saving the user a second time 'cause it needs a
-        # user id first, but if refactoring, be wary of a loop!
-        user.save()
+    return changed
 
 
 def add_drtb_hiv_to_dto(domain, user):
@@ -269,7 +269,8 @@ def add_drtb_hiv_to_dto(domain, user):
         # also assign user to the parent DTO
         loc_ids = user.get_location_ids(domain)
         if location.parent.location_id not in loc_ids:
-            user.add_to_assigned_locations(location.parent)
+            user.add_to_assigned_locations(location.parent, commit=False)
+            return True
 
 
 def connect_signals():
