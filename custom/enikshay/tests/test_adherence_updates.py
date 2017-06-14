58a3a5099ad3f310db2ec1c5d1754c02f5e16200
Farid Rener <proteusvacuum@gmail.com>
2017-06-14 17:38:00 -0400

    Small refactor to test

--- custom/enikshay/tests/test_adherence_updates.py
+++ custom/enikshay/tests/test_adherence_updates.py
@@ -120,7 +120,7 @@ class TestAdherenceUpdater(TestCase):
         self.data_store.adapter.clear_table()
         FormProcessorTestUtils.delete_all_cases()
 
-    def _create_episode_case(self, adherence_schedule_date_start, adherence_schedule_id):
+    def _create_episode_case(self, adherence_schedule_date_start=None, adherence_schedule_id=None):
         person = get_person_case_structure(
             self.person_id,
             self.user.user_id,
@@ -142,20 +142,15 @@ class TestAdherenceUpdater(TestCase):
         cases = {case.case_id: case for case in self.factory.create_or_update_cases([episode_structure])}
         return cases[self.episode_id]
 
-    def _create_adherence_cases(self, adherence_cases):
+    def _create_adherence_cases(self, case_dicts):
         self.factory.create_or_update_cases([
             get_adherence_case_structure(
                 "adherence{}".format(i),
                 self.episode_id,
-                adherence_date,
-                extra_update={
-                    "name": adherence_date,
-                    "adherence_value": adherence_value,
-                    "adherence_source": 'enikshay',
-                    "adherence_report_source": 'treatment_supervisor',
-                }
+                case['name'],
+                extra_update=case
             )
-            for (i, (adherence_date, adherence_value)) in enumerate(adherence_cases)
+            for i, case in enumerate(case_dicts)
         ])
 
     def assert_update(self, input, output):
@@ -174,17 +169,26 @@ class TestAdherenceUpdater(TestCase):
         self.case_updater.purge_date = input[0]
         # setup episode and adherence cases
         adherence_schedule_date_start, adherence_schedule_id = input[1]
-        adherence_cases = input[2]
+        adherence_cases = [
+            {
+                "name": adherence_case[0],
+                "adherence_value": adherence_case[1],
+                "adherence_source": "enikshay",
+                "adherence_source": "treatment_supervisor"
+            }
+            for adherence_case in input[2]
+        ]
         episode = self._create_episode_case(adherence_schedule_date_start, adherence_schedule_id)
         self._create_adherence_cases(adherence_cases)
-
-        # rebuild so that adherence UCR data gets updated
-        rebuild_indicators(self.data_store.datasource._id)
-        self.data_store.adapter.refresh_table()
-
+        self._rebuild_indicators()
         # _get_open_episode_cases doesn't work in tests due to caseaccessors returning empty list, so override it
         self.case_updater._get_open_episode_cases = mock.MagicMock(return_value=[episode])
         return episode
+
+    def _rebuild_indicators(self):
+        # rebuild so that adherence UCR data gets updated
+        rebuild_indicators(self.data_store.datasource._id)
+        self.data_store.adapter.refresh_table()
 
     def test_adherence_schedule_date_start_late(self):
         #   Sample test case
