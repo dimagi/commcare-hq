90791ba07e032e0ee6ab7f2a23db8d1db691bd6f
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-06-16 06:40:56 -0400

    Run preindex every 5 minutes

--- corehq/preindex/tasks.py
+++ corehq/preindex/tasks.py
@@ -4,7 +4,7 @@ from celery.task.base import periodic_task
 from corehq.preindex.accessors import index_design_doc, get_preindex_designs
 
 
-@periodic_task(run_every=crontab(minute=0), queue='background_queue')
+@periodic_task(run_every=crontab(minute='*/5'), queue='background_queue')
 def preindex_couch_views():
     for design in get_preindex_designs():
         index_design_doc(design)
