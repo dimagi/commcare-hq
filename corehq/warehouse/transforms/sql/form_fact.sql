04e42f1c04ac084286062dca87e03712cb605d53
Ben Rudolph <brudolph@dimagi.com>
2017-06-19 11:10:45 -0400

    Use GREATEST

--- corehq/warehouse/transforms/sql/form_fact.sql
+++ corehq/warehouse/transforms/sql/form_fact.sql
@@ -26,17 +26,7 @@ SELECT
     edited_on,
     build_id,
     state,
-    CASE
-        WHEN
-            (received_on > deleted_on OR deleted_on IS NULL) AND
-            (received_on > edited_on OR edited_on IS NULL)
-        THEN received_on::timestamp AT TIME ZONE default_timezone
-        WHEN
-            (deleted_on > received_on OR received_on IS NULL) AND
-            (deleted_on > edited_on OR edited_on IS NULL)
-        THEN deleted_on::timestamp AT TIME ZONE default_timezone
-        ELSE edited_on::timestamp AT TIME ZONE default_timezone
-    END
+    GREATEST(received_on, deleted_on, edited_on) AT TIME ZONE default_timezone
 FROM
     {{ form_staging }} AS form_table
 JOIN {{ domain_dim }} AS domain_table ON form_table.domain = domain_table.domain
