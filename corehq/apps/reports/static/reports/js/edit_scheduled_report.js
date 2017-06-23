7a370f13f6c14cde98a35c889bb4b1fc85287db8
Jennifer Schweers <orange.jenny@gmail.com>
2017-06-23 09:26:45 -0400

    lint new code

--- corehq/apps/reports/static/reports/js/edit_scheduled_report.js
+++ corehq/apps/reports/static/reports/js/edit_scheduled_report.js
@@ -9,13 +9,13 @@ hqDefine("reports/js/edit_scheduled_report.js", function() {
             $select.append($opt);
         }
         return $select;
-    }
+    };
 
     var update_day_input = function(weekly_options, monthly_options, day_value) {
         var $interval = $interval || $('[name="interval"]');
         $('#div_id_day').remove();
         if ($interval.val() !== 'daily') {
-            var opts = $interval.val() === 'weekly' ? weekly_options : monthly_options
+            var opts = $interval.val() === 'weekly' ? weekly_options : monthly_options;
             var $day_select = $('<select id="id_day" class="select form-control" name="day" />');
             $day_select = add_options_to_select($day_select, opts, day_value);
             var $day_control_group = $('<div id="div_id_day" class="form-group" />')
@@ -36,7 +36,7 @@ hqDefine("reports/js/edit_scheduled_report.js", function() {
                 update_day_input(self.weekly_options, self.monthly_options, self.day_value);
                 $('[name="interval"]').change(function() {
                     update_day_input(self.weekly_options, self.monthly_options);
-                })
+                });
             });
         };
     };
