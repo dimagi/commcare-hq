8ed096838475c41acbbfdb57bc8dbc074744fd22
Jonathan Emord <jonathan.emord@gmail.com>
2017-06-23 11:25:40 -0400

    Use decimal for soft rollout

--- corehq/apps/app_manager/fixtures/mobile_ucr.py
+++ corehq/apps/app_manager/fixtures/mobile_ucr.py
@@ -135,8 +135,8 @@ class ReportFixturesProvider(FixtureProvider):
     def _get_report_and_data_source(report_id, domain):
         report = get_report_config(report_id, domain)[0]
         data_source = ReportFactory.from_spec(report, include_prefilters=True)
-        if report.soft_rollout != 0 and data_source.config.backend_id == UCR_LABORATORY_BACKEND:
-            if random.randint(0, report.soft_rollout) == report.soft_rollout:
+        if report.soft_rollout > 0 and data_source.config.backend_id == UCR_LABORATORY_BACKEND:
+            if random.random() < report.soft_rollout:
                 data_source.override_backend_id(UCR_ES_BACKEND)
         return report, data_source
 
