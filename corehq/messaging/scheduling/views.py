e950b93308146ce86d0b4a98795c90faefbbd6eb
Jonathan Emord <jonathan.emord@gmail.com>
2017-06-26 15:51:08 -0400

    Added basic view for scheduled and immediate messages.

--- corehq/messaging/scheduling/views.py
+++ corehq/messaging/scheduling/views.py
@@ -10,6 +10,12 @@ from corehq.apps.domain.models import Domain
 from corehq.apps.sms.views import BaseMessagingSectionView
 from corehq.apps.style.decorators import use_datatables
 from corehq.apps.hqwebapp.views import DataTablesAJAXPaginationMixin
+from corehq.messaging.scheduling.models import (
+    ImmediateBroadcast,
+    ScheduledBroadcast
+)
+from corehq.const import SERVER_DATETIME_FORMAT
+from corehq.util.timezones.conversions import ServerTime
 from corehq.util.timezones.utils import get_timezone_for_user
 
 from dimagi.utils.decorators.memoized import memoized
@@ -29,10 +35,12 @@ def _requires_new_reminder_framework():
 
 
 class BroadcastListView(BaseMessagingSectionView, DataTablesAJAXPaginationMixin):
-    # TODO: should use template in its own folder
-    template_name = 'reminders/broadcasts_list.html'
+    template_name = 'scheduling/broadcasts_list.html'
     urlname = 'new_list_broadcasts'
     page_title = ugettext_lazy('Schedule a Message')
+
+    LIST_SCHEDULED = 'list_scheduled'
+    LIST_IMMEDIATE = 'list_immediate'
 
     @method_decorator(_requires_new_reminder_framework())
     @method_decorator(requires_privilege_with_fallback(privileges.OUTBOUND_SMS))
@@ -44,3 +52,54 @@ class BroadcastListView(BaseMessagingSectionView, DataTablesAJAXPaginationMixin)
     @memoized
     def project_timezone(self):
         return get_timezone_for_user(None, self.domain)
+
+    def _format_time(self, time):
+        user_time = ServerTime(time).user_time(self.project_timezone)
+        return user_time.ui_string(SERVER_DATETIME_FORMAT)
+
+    def get_scheduled_ajax_response(self):
+        query = (
+            ScheduledBroadcast.objects
+            .filter(domain=self.domain)
+            .order_by('-last_sent_timestamp')
+        )
+        total_records = query.count()
+        query = query.select_related('schedule')
+        broadcasts = query[self.display_start:self.display_start + self.display_length]
+
+        data = []
+        for broadcast in broadcasts:
+            data.append([
+                broadcast.id,
+                broadcast.name,
+                self._format_time(broadcast.last_sent_timestamp),
+                broadcast.schedule.active,
+            ])
+        return self.datatables_ajax_response(data, total_records)
+
+    def get_immediate_ajax_response(self):
+        query = (
+            ImmediateBroadcast.objects
+            .filter(domain=self.domain)
+            .order_by('-last_sent_timestamp')
+        )
+        total_records = query.count()
+        broadcasts = query[self.display_start:self.display_start + self.display_length]
+
+        data = []
+        for broadcast in broadcasts:
+            data.append([
+                broadcast.name,
+                self._format_time(broadcast.last_sent_timestamp),
+                broadcast.id,
+            ])
+        return self.datatables_ajax_response(data, total_records)
+
+    def get(self, request, *args, **kwargs):
+        action = request.GET.get('action')
+        if action == self.LIST_SCHEDULED:
+            return self.get_scheduled_ajax_response()
+        elif action == self.LIST_IMMEDIATE:
+            return self.get_immediate_ajax_response()
+
+        return super(BroadcastListView, self).get(*args, **kwargs)
