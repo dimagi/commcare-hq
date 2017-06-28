1a3841b494f1fff9d356d8ab1fc123c120d4800b
Cal Ellowitz <calellowitz@gmail.com>
2017-06-28 15:14:50 -0400

    adding tests

--- corehq/apps/app_manager/tests/test_util.py
+++ corehq/apps/app_manager/tests/test_util.py
@@ -1,11 +1,13 @@
-from django.test.testcases import SimpleTestCase
+from django.test.testcases import SimpleTestCase, TestCase
 
 import corehq.apps.app_manager.util as util
+from corehq.apps.app_manager.exceptions import AppEditingError
 from corehq.apps.app_manager.models import (
     AdvancedModule,
     Application,
     LoadUpdateAction,
-)
+    ReportModule, ReportAppConfig)
+from corehq.apps.app_manager.views.utils import overwrite_app
 
 
 class TestGetFormData(SimpleTestCase):
@@ -23,3 +25,32 @@ class TestGetFormData(SimpleTestCase):
 
         modules, errors = util.get_form_data('domain', app)
         self.assertEqual(modules[0]['forms'][0]['action_type'], 'load (load_0)')
+
+
+class TestOverwriteApp(TestCase):
+
+
+    def test_missing_ucrs(self):
+        master_app = Application.new_app('domain', "Master Application")
+        linked_app = Application.new_app('domain-2', "Linked Application")
+        target_json = linked_app.to_json()
+        module = master_app.add_module(ReportModule.new_module('Reports', None))
+        module.report_configs = [
+            ReportAppConfig(report_id='id', header={'en': 'CommBugz'}),
+        ]
+        with self.assertRaises(AppEditingError):
+            overwrite_app(target_json, master_app, {})
+
+    def test_static_ucrs(self):
+        master_app = Application.new_app('domain', "Master Application")
+        linked_app = Application.new_app('domain-2', "Linked Application")
+        linked_app.save()
+        target_json = linked_app.to_json()
+        module = master_app.add_module(ReportModule.new_module('Reports', None))
+        module.report_configs = [
+            ReportAppConfig(report_id='id', header={'en': 'CommBugz'}),
+        ]
+        report_map = {'id': 'mapped_id'}
+        overwrite_app(target_json, master_app, report_map)
+        linked_app = Application.get(linked_app._id)
+        self.assertEqual(linked_app.modules[0].report_configs[0].report_id, 'mapped_id')
