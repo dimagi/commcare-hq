dfa3ec808a1ccde1189d5cf17a5fd0547e1db314
Cal Ellowitz <calellowitz@gmail.com>
2017-06-29 14:12:37 -0400

    fixing orphan analysis command for new requirements

--- custom/icds/management/commands/export_orphaned_cases.py
+++ custom/icds/management/commands/export_orphaned_cases.py
@@ -14,22 +14,18 @@ CHILD_PROPERTIES = ['case_id', 'owner_id', 'opened_on', 'server_modified_on',
 
 SOURCE_FIELDS = CHILD_PROPERTIES + ['indices']
 
-CSV_HEADERS = CHILD_PROPERTIES + ['owner_name']
+CSV_HEADERS = CHILD_PROPERTIES + ['owner_name', 'hh_id', 'hh_name', 'hh_closed_on']
 
 
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'hh_file',
-            help="File path for household case output file",
-        )
-        parser.add_argument(
             'child_file',
             help="File path child case output file",
         )
 
-    def handle(self, hh_file, child_file, **options):
+    def handle(self, child_file, **options):
         relevant_districts = SQLLocation.objects.filter(domain='icds-cas',
                                                         location_id__in=['fd0e25031bab6d778d7391c1d1ba715a',
                                                                          'a2fcb186e9be8464e167bb1c56ce8260',
@@ -39,23 +35,29 @@ class Command(BaseCommand):
         owners = SQLLocation.objects.get_queryset_descendants(relevant_districts, include_self=True)
         owner_name_mapping = {loc.location_id: loc.name for loc in owners}
         hh_cases = self._get_closed_hh_cases(owner_name_mapping.keys())
-        with open(hh_file, 'w') as hh_csv, open(child_file, 'w') as child_csv:
-            hh_writer = csv.writer(hh_csv)
+        with open(child_file, 'w') as child_csv:
             child_writer = csv.writer(child_csv)
-            hh_writer.writerow(['case_id', 'closed_on'])
             child_writer.writerow(CSV_HEADERS)
             for cases in chunked(with_progress_bar(hh_cases, hh_cases.count), 500):
                 household_ids = []
+                hh_map = {}
                 for hh in cases:
-                    hh_writer.writerow([hh['case_id'], hh.get('closed_on', '')])
+                    hh_map[hh['case_id']] = (hh['name'].encode('utf-8'), hh.get('closed_on', '').encode('utf-8'))
                     household_ids.append(hh['case_id'])
                 child_cases = self._get_child_cases(household_ids)
                 ids = set(household_ids)
                 for child in child_cases.hits:
-                    if any(True for index in child['indices'] if index['referenced_id'] in ids and index['identifier'] == 'parent'):
+                    parent_index = filter(
+                        lambda index: index['referenced_id'] in ids and index['identifier'] == 'parent',
+                        child['indices']
+                    )[0]
+                    if parent_index:
+                        hh_id = parent_index['referenced_id']
                         row = [child.get(prop, '').encode('utf-8') for prop in CHILD_PROPERTIES]
                         row.append(owner_name_mapping.get(child.get('owner_id', ''), '').encode('utf-8'))
-            child_writer.writerow(row)
+                        hh_info = (hh_id, hh_map[hh_id][0], hh_map[hh_id][1])
+                        row.extend(hh_info)
+                child_writer.writerow(hh['case_id'], hh.get('closed_on', ''))
 
     def _get_closed_hh_cases(self, owners):
         query = (CaseES(es_instance_alias='export')
@@ -63,7 +65,7 @@ class Command(BaseCommand):
                  .domain('icds-cas')
                  .case_type('household')
                  .owner(owners)
-		         .source(['case_id', 'closed_on'])
+		         .source(['case_id', 'closed_on', 'name'])
                  .size(100)
                  )
         return query.scroll()
@@ -72,6 +74,7 @@ class Command(BaseCommand):
         query = (CaseES(es_instance_alias='export')
                  .domain('icds-cas')
                  .case_type('person')
+                 .is_closed(False)
                  .source(SOURCE_FIELDS)
                  .filter(filters.term("indices.referenced_id", household_ids))
                 )
