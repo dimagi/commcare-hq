18004129541825d736d5fbacc6b15b0014ca252f
Biyeun Buczyk <biyeun@dimagi.com>
2017-07-04 00:25:14 -0400

    create structure for static ui notification

---
+++ corehq/apps/notifications/ui_notify.py
@@ -0,0 +1,18 @@
+class StaticUINotify(object):
+    """
+    Useful for handling more complex & dismissable UI notifications
+    that might appear inline in the UI. This is still in it's early stages
+    and used in App Builder. Will build upon this as we go along.
+    """
+
+    def __init__(self, slug, label):
+        self.slug = slug
+        self.label = label
+
+    def enabled(self, request):
+        if hasattr(request, 'user'):
+            from corehq.apps.notifications.models import DismissedUINotify
+            return not DismissedUINotify.is_notification_dismissed(
+                request.user, self.slug
+            )
+        return False
