9d2c5059769766d613487ba4307be9c7da2fc94a
Nick Pellegrino <npellegrino@dimagi.com>
2017-07-07 17:58:01 -0400

    update case properties

--- custom/enikshay/private_sector_datamigration/factory.py
+++ custom/enikshay/private_sector_datamigration/factory.py
@@ -161,7 +161,7 @@ class BeneficiaryCaseFactory(object):
             kwargs['attrs']['update']['created_by_user_type'] = self.beneficiary.creating_agency.usertype
             creating_loc = self._location_by_agency(self.beneficiary.creating_agency)
             if creating_loc:
-                kwargs['attrs']['update']['created_by_user_location_id'] = creating_loc.location_id
+                kwargs['attrs']['update']['registered_by'] = creating_loc.location_id
 
         return CaseStructure(**kwargs)
 
@@ -295,8 +295,7 @@ class BeneficiaryCaseFactory(object):
             kwargs['attrs']['update']['created_by_user_type'] = self.beneficiary.creating_agency.usertype
             creating_loc = self._location_by_agency(self.beneficiary.creating_agency)
             if creating_loc:
-                kwargs['attrs']['update']['created_by_user_id'] = creating_loc.user_id
-                kwargs['attrs']['update']['created_by_user_location_id'] = creating_loc.location_id
+                kwargs['attrs']['update']['registered_by'] = creating_loc.location_id
 
         return CaseStructure(**kwargs)
 
