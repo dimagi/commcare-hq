9f867ecd52d1c69b82424120113076fd53036c17
Nick Pellegrino <npellegrino@dimagi.com>
2017-07-07 14:12:52 -0400

    fix beneficiary number

--- custom/enikshay/private_sector_datamigration/management/commands/create_cases_by_beneficiary.py
+++ custom/enikshay/private_sector_datamigration/management/commands/create_cases_by_beneficiary.py
@@ -241,7 +241,7 @@ def migrate_to_enikshay(domain, migration_comment, beneficiaries, skip_adherence
             if num_succeeded % chunk_size == 0:
                 logger.info('%d cases to save.' % len(case_structures))
                 logger.info('committing beneficiaries {}-{}...'.format(
-                    num_succeeded - chunk_size, num_succeeded
+                    num_succeeded - chunk_size + 1, num_succeeded
                 ))
                 try:
                     factory.create_or_update_cases(case_structures)
