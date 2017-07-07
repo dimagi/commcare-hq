ce8adcbc77f1322c4a46ed9dbc7d74b99bc9496a
Daniel Miller <dmiller@dimagi.com>
2017-07-07 13:32:17 -0400

    lint [ci skip]

--- corehq/form_processor/backends/sql/dbaccessors.py
+++ corehq/form_processor/backends/sql/dbaccessors.py
@@ -1193,13 +1193,14 @@ def _sort_with_id_list(object_list, id_list, id_property):
 
     SQL does not necessarily return the rows in any particular order so
     we need to order them ourselves.
-    
+
     NOTE: this does not return the sorted list. It sorts `object_list`
     in place using Python's built-in `list.sort`.
     """
-    index_map = {id_: index for index, id_ in enumerate(id_list)}
     def key(obj):
         return index_map[getattr(obj, id_property)]
+
+    index_map = {id_: index for index, id_ in enumerate(id_list)}
     object_list.sort(key=key)
 
 
