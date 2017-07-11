62cc4e0fcaffa775a067b3607dae18a36f475b8a
Farid Rener <proteusvacuum@gmail.com>
2017-07-11 09:24:00 -0400

    Revert "Fetch global types from blobdb"
    This reverts commit 32ac28a02f0217e72250f8bf2179ef8d4141e4e6.

--- corehq/apps/fixtures/fixturegenerators.py
+++ corehq/apps/fixtures/fixturegenerators.py
@@ -1,12 +1,8 @@
 from collections import defaultdict
 from xml.etree import ElementTree
-from StringIO import StringIO
-
-from corehq.blobs import get_blob_db
-from corehq.blobs.exceptions import NotFound
 
 from casexml.apps.phone.fixtures import FixtureProvider
-from corehq.apps.fixtures.models import FixtureDataItem, FixtureDataType, FIXTURE_BUCKET
+from corehq.apps.fixtures.models import FixtureDataItem, FixtureDataType
 from corehq.apps.products.fixtures import product_fixture_generator_json
 from corehq.apps.programs.fixtures import program_fixture_generator_json
 
@@ -47,59 +43,37 @@ class ItemListsProvider(FixtureProvider):
 
     def __call__(self, restore_state):
         restore_user = restore_state.restore_user
-        domain = restore_user.domain
-        db = get_blob_db()
 
-        all_types = {t._id: t for t in FixtureDataType.by_domain(domain)}
+        all_types = {t._id: t for t in FixtureDataType.by_domain(restore_user.domain)}
         global_types = {id: t for id, t in all_types.items() if t.is_global}
 
-        if restore_state.overwrite_cache:
-            db.delete(domain, FIXTURE_BUCKET)
-
-        # get global types from the db
-        try:
-            global_items = [db.get(domain, FIXTURE_BUCKET).read()]
-        except NotFound:
-            global_items = self.get_global_items(restore_user, global_types)
-            cached_element = [ElementTree.tostring(element, encoding='utf-8') for element in global_items]
-            db.put(StringIO("".join(cached_element)), domain, FIXTURE_BUCKET)
-
-        user_items = self.get_user_items(restore_user, all_types, global_types)
-        return global_items + user_items
-
-    def _set_cached_type(self, item, data_type):
-        # set the cached version used by the object so that it doesn't
-        # have to do another db trip later
-        item._data_type = data_type
-
-    def get_global_items(self, restore_user, global_types):
-        types_sorted_by_tag = sorted(global_types.iteritems(), key=lambda (id_, type_): type_.tag)
         items_by_type = defaultdict(list)
+
+        def _set_cached_type(item, data_type):
+            # set the cached version used by the object so that it doesn't
+            # have to do another db trip later
+            item._data_type = data_type
+
         items = FixtureDataItem.by_data_types(restore_user.domain, global_types)
         for item in items:
-            self._set_cached_type(item, global_types[item.data_type_id])
+            _set_cached_type(item, global_types[item.data_type_id])
             items_by_type[item.data_type_id].append(item)
-        return self._generate_fixture_from_type(restore_user, items_by_type, types_sorted_by_tag)
 
-    def get_user_items(self, restore_user, all_types, global_types):
         if set(all_types) - set(global_types):
             # only query ownership models if there are non-global types
-            types_sorted_by_tag = sorted(all_types.iteritems(), key=lambda (id_, type_): type_.tag)
+            other_items = restore_user.get_fixture_data_items()
 
-            items_by_type = defaultdict(list)
-            for item in restore_user.get_fixture_data_items():
+            for item in other_items:
                 if item.data_type_id in global_types:
                     continue  # was part of the global type so no need to add here
                 try:
-                    self._set_cached_type(item, all_types[item.data_type_id])
+                    _set_cached_type(item, all_types[item.data_type_id])
                 except (AttributeError, KeyError):
                     continue
                 items_by_type[item.data_type_id].append(item)
-            return self._generate_fixture_from_type(restore_user, items_by_type, types_sorted_by_tag)
-        return []
 
-    def _generate_fixture_from_type(self, restore_user, items_by_type, types_sorted_by_tag):
         fixtures = []
+        types_sorted_by_tag = sorted(all_types.iteritems(), key=lambda (id_, type_): type_.tag)
         for data_type_id, data_type in types_sorted_by_tag:
             if data_type.is_indexed:
                 fixtures.append(self._get_schema_element(data_type))
@@ -119,7 +93,7 @@ class ItemListsProvider(FixtureProvider):
         fixture_element.append(item_list_element)
         for item in items:
             item_list_element.append(item.to_xml())
-        return ElementTree.tostring(fixture_element, encoding="utf-8")
+        return fixture_element
 
     def _get_schema_element(self, data_type):
         schema_element = ElementTree.Element(
