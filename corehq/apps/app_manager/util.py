0b9162e7e0730cd5f1313344b3be68c000faeea2
Sravan Reddy <sravfeyn@gmail.com>
2017-07-12 20:44:59 +0530

    quote and is_active issue

--- corehq/apps/app_manager/util.py
+++ corehq/apps/app_manager/util.py
@@ -599,4 +599,9 @@ def get_and_assert_practice_user_in_domain(practice_user_id, domain):
             _("User {username} has been deleted, you can't use that user as practice user").format(
                 username=user.username)
         )
+    if not user.is_active:
+        raise PracticeUserException(
+            _("User {username} has been deactivated, you can't use that user as practice user").format(
+                username=user.username)
+        )
     return user
