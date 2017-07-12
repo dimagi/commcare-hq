934042a0952f303a567dc0f33c9fecb351b0d0e0
Ethan Soergel <esoergel@gmail.com>
2017-07-12 18:37:52 -0400

    This should actually be above, otherwise it only does exact matches.

--- custom/enikshay/reports/filters.py
+++ custom/enikshay/reports/filters.py
@@ -36,7 +36,7 @@ class EnikshayLocationFilter(BaseMultipleOptionFilter):
         if len(location_ids) == 0 \
                 and not self.request.couch_user.has_permission(self.request.domain, 'access_all_locations'):
             # Display the user's location in the filter if none is selected
-            location_ids = list(self.request.couch_user.get_location_ids(self.request.domain))
+            location_ids = self.request.couch_user.get_location_ids(self.request.domain)
 
         choice_provider = LocationChoiceProvider(StubReport(domain=self.domain), None)
         # We don't include descendants here because they will show up in select box
@@ -53,16 +53,16 @@ class EnikshayLocationFilter(BaseMultipleOptionFilter):
     @classmethod
     def get_value(cls, request, domain):
         selected = super(EnikshayLocationFilter, cls).get_value(request, domain)
+        if len(filter(None, selected)) == 0 and not request.couch_user.has_permission(domain, 'access_all_locations'):
+            # Force the user to select their assigned locations, otherwise selecting no locations will result in
+            # all results being returned.
+            selected = request.couch_user.get_location_ids(domain)
         choice_provider = LocationChoiceProvider(StubReport(domain=domain), None)
         choice_provider.configure({'include_descendants': True})
         selected_locations = [
             choice.value
             for choice in choice_provider.get_choices_for_known_values(selected, request.couch_user)
         ]
-        if len(selected_locations) == 0 and not request.couch_user.has_permission(domain, 'access_all_locations'):
-            # Force the user to select their assigned locations, otherwise selecting no locations will result in
-            # all results being returned.
-            return list(request.couch_user.get_location_ids(domain))
         return selected_locations
 
 
