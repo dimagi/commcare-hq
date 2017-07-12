5517e0cf37d56e36e74d5aa748d15305dc6948a8
Biyeun Buczyk <biyeun@dimagi.com>
2017-07-12 16:58:31 -0400

    remove test ui notification and toggle

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1273,10 +1273,3 @@ SKIP_REMOVE_INDICES = StaticToggle(
     TAG_ONE_OFF,
     [NAMESPACE_DOMAIN]
 )
-
-SHOW_UI_NOTIFICATIONS = StaticToggle(
-    'show_ui_notify',
-    'Show Tests of UI Notifications before the real deal',
-    TAG_ONE_OFF,
-    [NAMESPACE_USER]
-)
