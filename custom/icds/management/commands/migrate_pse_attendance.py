3186ca38f298b141837ba8819490c3356d70917d
Cal Ellowitz <calellowitz@gmail.com>
2017-07-15 00:23:17 -0400

    switching to update query

--- custom/icds/management/commands/migrate_pse_attendance.py
+++ custom/icds/management/commands/migrate_pse_attendance.py
@@ -9,21 +9,25 @@ new_table = 'config_report_icds-cas_static-child_cases_monthly_v2_198ccc06'
 old_table = 'config_report_icds-cas_static-child_cases_monthly_tabl_551fd064'
 
 male_sql_query = """
-INSERT INTO {new_table} (pse_daily_attendance, pse_daily_attendance_male)
-SELECT pse_days_attended, pse_days_attended
+UPDATE {new_table}
+SET
+pse_daily_attendance = A.pse_days_attended
+pse_daily_attendance_male = A.pse_days_attended
+FROM {old_table} A
+LEFT JOIN {new_table} B
+ON A.doc_id = B.doc_id and A.month_start = B.month_start
+WHERE B.sex = 'M'
+""".format(new_table=new_table, old_table=old_table)
+
+female_sql_query = """
+UPDATE {new_table}
+SET
+pse_daily_attendance = A.pse_days_attended
+pse_daily_attendance_female = A.pse_days_attended
 FROM {old_table} A
 LEFT JOIN {new_table} B
 ON A.doc_id = B.doc_id and A.month_start = B.month_start
 WHERE B.sex = 'F'
-""".format(new_table=new_table, old_table=old_table)
-
-female_sql_query = """
-INSERT INTO {new_table} (pse_daily_attendance, pse_daily_attendance_female)
-SELECT pse_days_attended, pse_days_attended
-FROM {old_table} A
-LEFT JOIN {new_table} B
-ON A.doc_id = B.doc_id and A.month = B.month_start
-WHERE B.sex = 'M'
 """.format(new_table=new_table, old_table=old_table)
 
 class Command(BaseCommand):
