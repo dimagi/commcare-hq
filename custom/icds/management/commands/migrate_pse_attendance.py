55dacf127e29c923556bdec9c9737f452ed413e0
Cal Ellowitz <calellowitz@gmail.com>
2017-07-17 16:48:18 -0400

    adding quotes

--- custom/icds/management/commands/migrate_pse_attendance.py
+++ custom/icds/management/commands/migrate_pse_attendance.py
@@ -9,12 +9,12 @@ new_table = 'config_report_icds-cas_static-child_cases_monthly_v2_198ccc06'
 old_table = 'config_report_icds-cas_static-child_cases_monthly_tabl_551fd064'
 
 migration_query = """
-UPDATE {new_table} B
+UPDATE "{new_table}" B
 SET
 B.pse_daily_attendance = A.pse_days_attended,
 B.pse_daily_attendance_male = CASE WHEN sex = 'M' THEN A.pse_days_attended ELSE NULL END,
 B.pse_daily_attendance_female = CASE WHEN sex = 'F' THEN A.pse_days_attended ELSE NULL END
-FROM {old_table} A
+FROM "{old_table}" A
 WHERE A.doc_id = B.doc_id and A.month = immutable_date_cast(B.month_start)
 """.format(new_table=new_table, old_table=old_table)
 
