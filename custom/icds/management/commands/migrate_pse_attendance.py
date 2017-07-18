2c7aa5b075cc347023cb8a51fcc8a9f091456e08
Cal Ellowitz <calellowitz@gmail.com>
2017-07-17 21:13:18 -0400

    removing unneeded table ref and adding check mode

--- custom/icds/management/commands/migrate_pse_attendance.py
+++ custom/icds/management/commands/migrate_pse_attendance.py
@@ -11,9 +11,9 @@ old_table = 'config_report_icds-cas_static-child_cases_monthly_tabl_551fd064'
 migration_query = """
 UPDATE "{new_table}" B
 SET
-B.pse_daily_attendance = A.pse_days_attended,
-B.pse_daily_attendance_male = CASE WHEN sex = 'M' THEN A.pse_days_attended ELSE NULL END,
-B.pse_daily_attendance_female = CASE WHEN sex = 'F' THEN A.pse_days_attended ELSE NULL END
+pse_daily_attendance = A.pse_days_attended,
+pse_daily_attendance_male = CASE WHEN sex = 'M' THEN A.pse_days_attended ELSE NULL END,
+pse_daily_attendance_female = CASE WHEN sex = 'F' THEN A.pse_days_attended ELSE NULL END
 FROM "{old_table}" A
 WHERE A.doc_id = B.doc_id and A.month = immutable_date_cast(B.month_start)
 """.format(new_table=new_table, old_table=old_table)
@@ -21,9 +21,24 @@ WHERE A.doc_id = B.doc_id and A.month = immutable_date_cast(B.month_start)
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **options):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--check',
+            action='store_true',
+            dest='check',
+            default=False,
+            help="Print query and output from EXPLAIN"
+        )
+    def handle(self, check, **options):
         with connections['icds-ucr'].cursor() as cursor:
-            print('Migrating pse data')
-            cursor.execute(migration_query)
-            print('Vacuuming table')
-            cursor.execute("VACUUM ANALYZE {}".format(new_table))
+            if check:
+                print('Query is')
+                print(migration_query)
+                print('-----------------------------')
+                explain_result = cursor.execute("EXPLAIN {}".format(migration_query))
+                print(explain_result)
+            else:
+                print('Migrating pse data')
+                cursor.execute(migration_query)
+                print('Vacuuming table')
+                cursor.execute("VACUUM ANALYZE {}".format(new_table))
