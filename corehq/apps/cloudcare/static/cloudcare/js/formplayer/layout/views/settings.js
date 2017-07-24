55e5b9fc954c2a88df96a228d6849bdb7cc4e25f
Ben Rudolph <brudolph@dimagi.com>
2017-07-24 11:07:33 -0400

    Refactor each setting to be its own view

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/layout/views/settings.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/layout/views/settings.js
@@ -1,28 +1,50 @@
 /*global FormplayerFrontend, Util */
 
 FormplayerFrontend.module("Layout.Views", function (Views, FormplayerFrontend, Backbone, Marionette) {
-    Views.SettingsView = Marionette.ItemView.extend({
-        ui: {
-            oneQuestionPerScreen: '.js-one-question-per-screen',
-            language: '.js-lang',
-            done: '.js-done',
-        },
-        events: {
-            'switchChange.bootstrapSwitch @ui.oneQuestionPerScreen': 'onChangeOneQuestionPerScreen',
-            'change @ui.language': 'onLanguageChange',
-            'click @ui.done': 'onClickDone',
-        },
-        template: '#settings-template',
+    /**
+     * Sets the application language. Should only be used for App Preview.
+     */
+    var LangSettingView = Marionette.ItemView.extend({
+        template: '#lang-setting-template',
+        tagName: 'tr',
         initialize: function() {
             this.currentUser = FormplayerFrontend.request('currentUser');
+        },
+        ui: {
+            language: '.js-lang',
+        },
+        events: {
+            'change @ui.language': 'onLanguageChange',
+        },
+        onLanguageChange: function(e) {
+            this.currentUser.displayOptions.language = $(e.currentTarget).val();
+            Util.saveDisplayOptions(this.currentUser.displayOptions);
         },
         templateHelpers: function() {
             var appId = FormplayerFrontend.request('getCurrentAppId');
             var currentApp = FormplayerFrontend.request("appselect:getApp", appId);
             return {
                 langs: currentApp.get('langs'),
-                displayOptions: this.currentUser.displayOptions,
+                currentLang: this.currentUser.displayOptions.language,
             };
+        },
+    });
+
+    /**
+     * Sets whether or not the application should use One Question Per Screen or not.
+     * Should only be used for App Preview.
+     */
+    var DisplaySettingView = Marionette.ItemView.extend({
+        template: '#display-setting-template',
+        tagName: 'tr',
+        initialize: function() {
+            this.currentUser = FormplayerFrontend.request('currentUser');
+        },
+        ui: {
+            oneQuestionPerScreen: '.js-one-question-per-screen',
+        },
+        events: {
+            'switchChange.bootstrapSwitch @ui.oneQuestionPerScreen': 'onChangeOneQuestionPerScreen',
         },
         onRender: function() {
             this.ui.oneQuestionPerScreen.bootstrapSwitch(
@@ -34,12 +56,33 @@ FormplayerFrontend.module("Layout.Views", function (Views, FormplayerFrontend, B
             this.currentUser.displayOptions.oneQuestionPerScreen = switchValue;
             Util.saveDisplayOptions(this.currentUser.displayOptions);
         },
-        onLanguageChange: function(e) {
-            this.currentUser.displayOptions.language = $(e.currentTarget).val();
-            Util.saveDisplayOptions(this.currentUser.displayOptions);
+    });
+
+    Views.SettingsView = Marionette.CompositeView.extend({
+        ui: {
+            done: '.js-done',
+        },
+        childViewContainer: 'tbody',
+        getChildView: function(item) {
+            if (item.get('slug') === 'lang') {
+                return LangSettingView;
+            } else if (item.get('slug') === 'display') {
+                return DisplaySettingView;
+            }
+        },
+        events: {
+            'click @ui.done': 'onClickDone',
+        },
+        template: '#settings-template',
+        initialize: function() {
+            this.collection = new Backbone.Collection([
+                new Backbone.Model({ slug: 'lang' }),
+                new Backbone.Model({ slug: 'display' }),
+            ]);
         },
         onClickDone: function() {
             FormplayerFrontend.trigger('navigateHome');
         },
     });
+
 });
