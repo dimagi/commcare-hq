9c5ae9eeba313f2701456d714f5c78f6b54977a5
Simon Kelly <skelly@dimagi.com>
2017-07-24 17:25:33 +0200

    initial test

--- corehq/form_processor/tests/test_reprocess_errors.py
+++ corehq/form_processor/tests/test_reprocess_errors.py
@@ -2,15 +2,22 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import uuid
 
+from django.db.utils import InternalError
 from django.test import TestCase
 from django.test import override_settings
+from mock import patch
 
 from casexml.apps.case.exceptions import InvalidCaseIndex
 from casexml.apps.case.mock import CaseBlock
+from casexml.apps.case.mock import CaseFactory
+from casexml.apps.case.mock import CaseStructure
 from casexml.apps.case.util import post_case_blocks
+from corehq.form_processor.backends.sql.dbaccessors import CaseAccessorSQL, FormAccessorSQL
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors, CaseAccessors
+from corehq.form_processor.models import XFormInstanceSQL
+from corehq.form_processor.reprocess import reprocess_xform_error, _reprocess_form
 from corehq.form_processor.tests.utils import FormProcessorTestUtils
-from corehq.form_processor.reprocess import reprocess_xform_error
+from couchforms.models import UnfinishedSubmissionStub
 
 
 class ReprocessXFormErrorsTest(TestCase):
@@ -85,3 +92,51 @@ class ReprocessXFormErrorsTestSQL(ReprocessXFormErrorsTest):
         self.assertTrue(case.transactions[0].is_case_create)
         self.assertTrue(case.transactions[0].is_case_index)
         self.assertFalse(case.transactions[0].revoked)
+
+
+@override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
+class ReprocessSubmissionStubTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(ReprocessSubmissionStubTests, cls).setUpClass()
+        cls.domain = uuid.uuid4().hex
+        cls.factory = CaseFactory(domain=cls.domain)
+
+    def test_reprocess_unfinished_submission_case_create(self):
+        case_id = uuid.uuid4().hex
+        transaction_patch = patch('corehq.form_processor.backends.sql.processor.transaction')
+        case_save_patch = patch('corehq.form_processor.backends.sql.dbaccessors.CaseAccessorSQL.save_case', side_effect=InternalError)
+        with transaction_patch, case_save_patch, self.assertRaises(InternalError):
+            self.factory.create_or_update_cases([
+                CaseStructure(case_id=case_id, attrs={'case_type': 'parent', 'create': False})
+            ])
+
+        stubs = UnfinishedSubmissionStub.objects.filter(domain=self.domain, saved=False).all()
+        self.assertEqual(1, len(stubs))
+
+        # form that was saved before case error raised
+        normal_form_ids = FormAccessorSQL.get_form_ids_in_domain_by_state(self.domain, XFormInstanceSQL.NORMAL)
+        self.assertEqual(1, len(normal_form_ids))
+
+        # shows error form (duplicate of form that was saved before case error)
+        # this is saved becuase the saving was assumed to be atomic so if there was any error it's assumed
+        # the form didn't get saved
+        error_forms = FormAccessorSQL.get_forms_by_type(self.domain, 'XFormError', 10)
+        self.assertEqual(1, len(error_forms))  # we don't really care about this form in this test
+        self.assertEqual(error_forms[0].orig_id, normal_form_ids[0])
+
+        self.assertEqual(0, len(CaseAccessorSQL.get_case_ids_in_domain(self.domain)))
+
+        _reprocess_form(FormAccessorSQL.get_form(normal_form_ids[0]))
+        case_ids = CaseAccessorSQL.get_case_ids_in_domain(self.domain)
+        self.assertEqual(1, len(case_ids))
+        self.assertEqual(case_id, case_ids[0])
+
+    def test_reprocess_unfinished_submission_case_update(self):
+        pass
+
+    def test_reprocess_unfinished_submission_ledger_create(self):
+        pass
+
+    def test_reprocess_unfinished_submission_ledger_update(self):
+        pass
