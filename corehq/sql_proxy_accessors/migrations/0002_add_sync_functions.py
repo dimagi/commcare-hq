e583ba72e0e0fa5bf62c087b3bfa7cf468e4b5d4
Jonathan Emord <emord@users.noreply.github.com>
2017-07-24 08:57:07 -0400

    Revert "remove unused sql function "

--- corehq/sql_proxy_accessors/migrations/0002_add_sync_functions.py
+++ corehq/sql_proxy_accessors/migrations/0002_add_sync_functions.py
@@ -19,6 +19,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrator.get_migration('get_case_last_modified_dates.sql'),
+        migrator.get_migration('get_extension_case_ids.sql'),
         migrator.get_migration('case_has_transactions_since_sync.sql'),
         migrator.get_migration('get_all_reverse_indices.sql'),
         migrator.get_migration('get_case_ids_modified_with_owner_since.sql'),
