ec3bd96ca1ddb0b4becd1c75dee6a3e2cd44ef31
Daniel Roberts <droberts@dimagi.com>
2017-07-26 15:55:32 -0400

    make app preview work again

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/api.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/apps/api.js
@@ -7,6 +7,7 @@
 FormplayerFrontend.module("Apps", function (Apps, FormplayerFrontend, Backbone) {
 
     var appsPromiseByRestoreAs = {};
+    var appsByRestoreAs = {};
 
     Apps.API = {
         primeApps: function (restoreAs, apps) {
@@ -19,12 +20,20 @@ FormplayerFrontend.module("Apps", function (Apps, FormplayerFrontend, Backbone) 
                 appsPromise = appsPromiseByRestoreAs[restoreAs] = $.getJSON('?option=apps');
             }
             return appsPromise.pipe(function (apps) {
+                appsByRestoreAs[restoreAs] = apps;
                 return new FormplayerFrontend.Apps.Collections.App(apps);
             });
         },
         getAppEntity: function (app_id) {
-            var apps = Apps.API.getAppEntities();
-            return apps.get(app_id);
+            var restoreAs = FormplayerFrontend.request('currentUser').restoreAs;
+            var apps = appsByRestoreAs[restoreAs];
+            if (!apps) {
+                console.warn("getAppEntity is returning null. If the app_id is correct, " +
+                             "it may have been called before getAppEntities populated it asynchronously.");
+                return null;
+            }
+            var appCollection = new FormplayerFrontend.Apps.Collections.App(apps);
+            return appCollection.get(app_id);
         },
     };
 
