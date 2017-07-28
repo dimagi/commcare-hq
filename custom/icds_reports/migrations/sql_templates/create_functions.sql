f121cadf2ef8228ab568305f033941a23b6dbb30
sheelio <sshah@dimagi.com>
2017-07-28 18:52:55 -0400

    Add AWC ID to supervision table

--- custom/icds_reports/migrations/sql_templates/create_functions.sql
+++ custom/icds_reports/migrations/sql_templates/create_functions.sql
@@ -1317,7 +1317,7 @@ BEGIN
 		'ls_awc_not_open_unknown = ut.ls_awc_not_open_unknown, ' ||
 		'ls_awc_not_open_other = ut.ls_awc_not_open_other ' ||
 	'FROM (SELECT ' ||
-		'location_id AS awc_id, ' ||
+		'awc_id AS awc_id, ' ||
 		'month, ' ||
 		'sum(count) AS ls_supervision_visit, ' ||
 		'CASE WHEN sum(count) > 0 THEN 1 ELSE 0 END AS ls_num_supervised, ' ||
@@ -1331,7 +1331,7 @@ BEGIN
 		'sum(awc_not_open_unknown) AS ls_awc_not_open_unknown, ' ||
 		'sum(awc_not_open_other) AS ls_awc_not_open_other '
 		'FROM ' || quote_ident(_ls_tablename) || ' ' ||
-		'WHERE month = ' || quote_literal(_start_date) || ' GROUP BY location_id, month) ut ' ||
+		'WHERE month = ' || quote_literal(_start_date) || ' GROUP BY awc_id, month) ut ' ||
 	'WHERE ut.month = agg_awc.month AND ut.awc_id = agg_awc.awc_id';
 
 
