fd8f676166379dcbeec06689ff6f8eb26a8d96e1
NoahCarnahan <ncarnahan@dimagi.com>
2017-07-31 16:52:25 -0400

    Add management command to fix cases migrated with bug

---
+++ custom/enikshay/two_b_release_1/management/commands/enikshay_2b_reason_for_test_fix.py
@@ -0,0 +1,66 @@
+import datetime
+import logging
+
+from corehq.apps.es import CaseSearchES
+from django.core.management import BaseCommand
+from casexml.apps.case.mock import CaseStructure, CaseFactory
+
+logger = logging.getLogger('enikshay_2b_reason_for_test_fix')
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'domain',
+            help="The domain to migrate."
+        )
+        parser.add_argument(
+            "log_path",
+            help="Path to write the log to"
+        )
+        parser.add_argument(
+            '--commit',
+            action='store_true',
+            help="actually modifies the cases. Without this flag, it's a dry run."
+        )
+
+    def handle(self, domain, log_path, **options):
+        commit = options['commit']
+        factory = CaseFactory(domain)
+
+        logger.info("Starting {} migration on {} at {}".format(
+            "real" if commit else "fake", domain, datetime.datetime.utcnow()
+        ))
+
+        cases = (CaseSearchES()
+                    .domain(domain)
+                    .case_type("test")
+                    .case_property_query("updated_by_migration", "enikshay_2b_case_properties", "must"))
+
+        with open(log_path, "w") as f:
+            for case in cases:
+                if (
+                    case['properties']['purpose_of_testing'] == "follow_up_dstb" and
+                    case['properties'].get("rft_general", "") in ("", None)
+                ):
+                    case_id = case['case_id']
+                    f.write(case_id + "\n")
+                    logger.info(case_id)
+
+                    case_structure = CaseStructure(
+                        case_id=case_id,
+                        walk_related=False,
+                        attrs={
+                            "create": False,
+                            "update": {
+                                "rft_general": "follow_up_dstb",
+                                "updated_by_migration": "enikshay_2b_reason_for_test_fix",
+                            },
+                        },
+                    )
+
+                    if commit:
+                        factory.create_or_update_case(case_structure)
+
+        logger.info("Migration finished at {}".format(datetime.datetime.utcnow()))
