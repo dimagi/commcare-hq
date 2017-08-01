d321727df0240fcba394788034faac526f4df84d
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-08-01 15:43:44 -0400

    Allow sending to case owner from new scheduling framework

--- corehq/messaging/scheduling/scheduling_partitioned/models.py
+++ corehq/messaging/scheduling/scheduling_partitioned/models.py
@@ -4,7 +4,9 @@ from corehq.apps.casegroups.models import CommCareCaseGroup
 from corehq.apps.groups.models import Group
 from corehq.apps.locations.dbaccessors import get_all_users_by_location
 from corehq.apps.locations.models import SQLLocation
+from corehq.apps.users.cases import get_owner_id, get_wrapped_owner
 from corehq.apps.users.models import CommCareUser, WebUser
+from corehq.form_processor.exceptions import CaseNotFound
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.form_processor.utils import is_commcarecase
 from corehq.messaging.scheduling import util
@@ -13,6 +15,7 @@ from corehq.messaging.scheduling.models import AlertSchedule, TimedSchedule
 from corehq.sql_db.models import PartitionedModel
 from corehq.util.timezones.conversions import ServerTime
 from corehq.util.timezones.utils import get_timezone_for_domain, coerce_timezone_value
+from couchdbkit.exceptions import ResourceNotFound
 from datetime import tzinfo
 from dimagi.utils.decorators.memoized import memoized
 from dimagi.utils.modules import to_function
@@ -246,10 +249,21 @@ class CaseScheduleInstanceMixin(object):
 
     @property
     @memoized
+    def case(self):
+        try:
+            return CaseAccessors(self.domain).get_case(self.case_id)
+        except (CaseNotFound, ResourceNotFound):
+            return None
+
+    @property
+    @memoized
     def recipient(self):
         if self.recipient_type == 'Self':
-            return CaseAccessors(self.domain).get_case(self.case_id)
+            return self.case
         elif self.recipient_type == 'Owner':
+            if self.case:
+                return get_wrapped_owner(get_owner_id(self.case))
+
             return None
         if self.recipient_type == 'LastSubmittingUser':
             return None
---
+++ corehq/messaging/scheduling/tests/test_recipients.py
@@ -0,0 +1,138 @@
+from django.test import TestCase
+from corehq.apps.data_interfaces.tests.util import create_case
+from corehq.apps.domain.shortcuts import create_domain
+from corehq.apps.groups.models import Group
+from corehq.apps.locations.models import SQLLocation
+from corehq.apps.locations.tests.util import make_loc, setup_location_types
+from corehq.apps.users.models import CommCareUser, WebUser
+from corehq.form_processor.tests.utils import run_with_all_backends
+from corehq.form_processor.utils import is_commcarecase
+from corehq.messaging.scheduling.models import TimedSchedule, SMSContent
+from corehq.messaging.scheduling.scheduling_partitioned.models import CaseTimedScheduleInstance
+from corehq.messaging.scheduling.tests.util import delete_timed_schedules
+from datetime import time
+from mock import patch
+
+
+class SchedulingRecipientTest(TestCase):
+    domain = 'scheduling-recipient-test'
+
+    @classmethod
+    def setUpClass(cls):
+        super(SchedulingRecipientTest, cls).setUpClass()
+
+        cls.domain_obj = create_domain(cls.domain)
+
+        cls.location_types = setup_location_types(cls.domain, ['country', 'state',  'city'])
+        cls.state_location = make_loc('ma', domain=cls.domain, type='state')
+        cls.city_location = make_loc('boston', domain=cls.domain, type='city', parent=cls.state_location)
+
+        cls.mobile_user = CommCareUser.create(cls.domain, 'mobile', 'abc')
+        cls.mobile_user.set_location(cls.city_location)
+
+        cls.web_user = WebUser.create(cls.domain, 'web', 'abc')
+
+        cls.group = Group(domain=cls.domain, users=[cls.mobile_user.get_id])
+        cls.group.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.domain_obj.delete()
+        super(SchedulingRecipientTest, cls).tearDownClass()
+
+    def tearDown(self):
+        delete_timed_schedules(self.domain)
+
+    def user_ids(self, users):
+        return [user.get_id for user in users]
+
+    @run_with_all_backends
+    def test_case_recipient(self):
+        with create_case(self.domain, 'person') as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Self')
+            self.assertTrue(is_commcarecase(instance.recipient))
+            self.assertEqual(instance.recipient.case_id, case.case_id)
+
+    @run_with_all_backends
+    def test_owner_recipient(self):
+        with create_case(self.domain, 'person', owner_id=self.city_location.location_id) as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Owner')
+            self.assertTrue(isinstance(instance.recipient, SQLLocation))
+            self.assertEqual(instance.recipient.location_id, self.city_location.location_id)
+
+        with create_case(self.domain, 'person', owner_id=self.group.get_id) as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Owner')
+            self.assertTrue(isinstance(instance.recipient, Group))
+            self.assertEqual(instance.recipient.get_id, self.group.get_id)
+
+        with create_case(self.domain, 'person', owner_id=self.mobile_user.get_id) as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Owner')
+            self.assertTrue(isinstance(instance.recipient, CommCareUser))
+            self.assertEqual(instance.recipient.get_id, self.mobile_user.get_id)
+
+        with create_case(self.domain, 'person', owner_id=self.web_user.get_id) as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Owner')
+            self.assertTrue(isinstance(instance.recipient, WebUser))
+            self.assertEqual(instance.recipient.get_id, self.web_user.get_id)
+
+        with create_case(self.domain, 'person') as case:
+            instance = CaseTimedScheduleInstance(domain=self.domain, case_id=case.case_id, recipient_type='Owner')
+            self.assertIsNone(instance.recipient)
+
+    def test_expand_location_recipients(self):
+        schedule_without_descendants = TimedSchedule.create_simple_daily_schedule(
+            self.domain,
+            time(9, 0),
+            SMSContent(message={'en': 'Hello'})
+        )
+        schedule_without_descendants.include_descendant_locations = False
+        schedule_without_descendants.save()
+
+        schedule_with_descendants = TimedSchedule.create_simple_daily_schedule(
+            self.domain,
+            time(9, 0),
+            SMSContent(message={'en': 'Hello'})
+        )
+        schedule_with_descendants.include_descendant_locations = True
+        schedule_with_descendants.save()
+
+        instance = CaseTimedScheduleInstance(
+            domain=self.domain,
+            timed_schedule_id=schedule_without_descendants.schedule_id,
+            recipient_type='Location',
+            recipient_id=self.city_location.location_id
+        )
+        self.assertEqual(
+            self.user_ids(instance.expand_recipients()),
+            [self.mobile_user.get_id]
+        )
+
+        instance = CaseTimedScheduleInstance(
+            domain=self.domain,
+            timed_schedule_id=schedule_without_descendants.schedule_id,
+            recipient_type='Location',
+            recipient_id=self.state_location.location_id
+        )
+        self.assertEqual(
+            list(instance.expand_recipients()),
+            []
+        )
+
+        instance = CaseTimedScheduleInstance(
+            domain=self.domain,
+            timed_schedule_id=schedule_with_descendants.schedule_id,
+            recipient_type='Location',
+            recipient_id=self.state_location.location_id
+        )
+        self.assertEqual(
+            self.user_ids(instance.expand_recipients()),
+            [self.mobile_user.get_id]
+        )
+
+    def test_expand_group_recipients(self):
+        instance = CaseTimedScheduleInstance(domain=self.domain, recipient_type='Group',
+            recipient_id=self.group.get_id)
+        self.assertEqual(
+            self.user_ids(instance.expand_recipients()),
+            [self.mobile_user.get_id]
+        )
