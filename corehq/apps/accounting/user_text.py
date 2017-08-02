9bd87932e79294755487df8fb431d7bac7495385
Nick Pellegrino <npellegrino@dimagi.com>
2017-08-02 14:45:06 -0400

    remove unused imports

--- corehq/apps/accounting/user_text.py
+++ corehq/apps/accounting/user_text.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
-from django.urls import reverse
-from django.utils.safestring import mark_safe
-from django.utils.translation import ugettext_noop, ugettext_lazy as _, ugettext
+from django.utils.translation import ugettext_lazy as _
 from corehq.apps.accounting.models import (
     FeatureType,
     SoftwarePlanEdition as Edition,
