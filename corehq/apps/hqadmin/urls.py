2caa181dcf50e7990314e1626f79e6fd8ffe308d
Simon Kelly <skelly@dimagi.com>
2017-08-04 16:45:57 +0200

    urlpatterns must be list

--- corehq/apps/hqadmin/urls.py
+++ corehq/apps/hqadmin/urls.py
@@ -34,7 +34,7 @@ from corehq.apps.reports.dispatcher import AdminReportDispatcher
 
 from corehq.apps.api.urls import admin_urlpatterns as admin_api_urlpatterns
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$', default, name="default_admin_report"),
     url(r'^system/$', SystemInfoView.as_view(), name=SystemInfoView.urlname),
     url(r'^system/recent_changes/$', RecentCouchChangesView.as_view(),
@@ -75,4 +75,4 @@ urlpatterns = {
     url(r'^web_user_data', WebUserDataView.as_view(), name=WebUserDataView.urlname),
     url(r'^session_details/', SessionDetialsView.as_view(), name=SessionDetialsView.urlname),
     AdminReportDispatcher.url_pattern(),
-}
+]
