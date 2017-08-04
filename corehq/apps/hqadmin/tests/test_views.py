f49fbef1b760ffd89d9e0bc12010e11706922730
Simon Kelly <skelly@dimagi.com>
2017-08-04 17:32:13 +0200

    add tests

--- corehq/apps/hqadmin/tests/test_views.py
+++ corehq/apps/hqadmin/tests/test_views.py
@@ -1,7 +1,20 @@
+import base64
+import hashlib
+import hmac
+import json
+
+from django.conf import settings
 from django.http import HttpResponse
 from django.test import SimpleTestCase
+from django.test import TestCase
+from django.test.client import Client
+from django.test.utils import override_settings
+from django.urls.base import reverse
 from mock import patch, Mock
+
+from corehq.apps.domain.shortcuts import create_user
 from corehq.apps.hqadmin.views import AdminRestoreView
+from corehq.util.test_utils import softer_assert
 
 
 class AdminRestoreViewTests(SimpleTestCase):
@@ -32,3 +45,50 @@ class AdminRestoreViewTests(SimpleTestCase):
                 'num_cases': 0,
                 'num_locations': 0,
             })
+
+
+class SessionDetailsView(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(SessionDetailsView, cls).setUpClass()
+        cls.user = create_user('bunkey', '123')
+
+        client = Client()
+        client.login(username='bunkey', password='123')
+
+        session = client.session
+        session.save()
+        cls.session_key = session.session_key
+
+        cls.url = reverse('session_details')
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.user.delete()
+        super(SessionDetailsView, cls).tearDownClass()
+
+    @override_settings(DEBUG=True)
+    @softer_assert()
+    def test_session_details_view(self):
+        data = json.dumps({'session_id': self.session_key})
+        response = Client().post(self.url, data, content_type="application/json")
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(json.dumps({'user_id': self.user.id}), response.content)
+
+    @override_settings(FORMPLAYER_INTERNAL_AUTH_KEY='123abc', DEBUG=False)
+    def test_with_hmac_signing(self):
+        assert not settings.DEBUG
+        data = json.dumps({'session_id': self.session_key})
+        header_value = base64.b64encode(hmac.new('123abc', data, hashlib.sha256).digest())
+        response = Client().post(self.url, data, content_type="application/json", HTTP_X_MAC_DIGEST=header_value)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(json.dumps({'user_id': self.user.id}), response.content)
+
+    @override_settings(FORMPLAYER_INTERNAL_AUTH_KEY='123abc', DEBUG=False)
+    def test_with_hmac_signing_fail(self):
+        assert not settings.DEBUG
+        data = json.dumps({'session_id': self.session_key})
+
+        response = Client().post(self.url, data, content_type="application/json", HTTP_X_MAC_DIGEST='bad signature')
+        self.assertEqual(401, response.status_code)
+
