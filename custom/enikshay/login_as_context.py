35173136fe84958df4ee2e813adcb3ff8fafa8cb
Daniel Roberts <droberts@dimagi.com>
2017-08-04 16:53:01 -0400

    add usertype_display for enikshay login as

--- custom/enikshay/login_as_context.py
+++ custom/enikshay/login_as_context.py
@@ -1,5 +1,15 @@
 def get_enikshay_login_as_context(user):
     return {
         'district': None,
-        'usertype_display': None,
+        'usertype_display': get_usertype_display(user.user_data.get('usertype')),
     }
+
+
+def get_usertype_display(usertype):
+    return {
+        'pac': 'AYUSH / Other Provider',
+        'pcp': 'MBBS Provider',
+        'pcc-chemist': 'Chemist',
+        'plc': 'Lab',
+        'ps-fieldstaff': 'Field Staff',
+    }.get(usertype, usertype)
