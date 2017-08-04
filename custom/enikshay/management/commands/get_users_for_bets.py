8a3d5a2de31400000d90fab347c9b609db64db9f
Ethan Soergel <esoergel@gmail.com>
2017-08-04 18:32:50 -0500

    Add the rest of the fields

--- custom/enikshay/management/commands/get_users_for_bets.py
+++ custom/enikshay/management/commands/get_users_for_bets.py
@@ -2,6 +2,9 @@ import csv
 from django.core.management.base import BaseCommand
 from corehq.apps.locations.models import SQLLocation
 from corehq.apps.users.models import CommCareUser
+from corehq.apps.custom_data_fields.models import CustomDataFieldsDefinition
+from corehq.apps.locations.views import LocationFieldsView
+from corehq.apps.users.views.mobile.custom_data_fields import UserFieldsView
 
 
 class Command(BaseCommand):
@@ -12,12 +15,20 @@ class Command(BaseCommand):
     def confirm(self):
         return raw_input("Continue?\n(y/n)") == 'y'
 
+    def get_data_fields(self):
+        data_model = CustomDataFieldsDefinition.get_or_create(
+            self.domain,
+            UserFieldsView.field_type,
+        )
+        return sorted(data_model.fields)
+
     def handle(self, domain, **options):
         self.domain = domain
         self.locations_by_id = {
             loc.location_id: loc for loc in SQLLocation.objects.filter(domain=domain)
         }
         filename = 'agency_users.csv'
+        self.data_fields = self.get_data_fields()
         with open(filename, 'w') as f:
             writer = csv.writer(f)
             writer.writerow([
@@ -29,6 +40,11 @@ class Command(BaseCommand):
                 'assigned_location_id',
                 'location_id',
                 'location_name',
+                'phone_number',
+                'email',
+                'language',
+            ] + [
+                'data: {}'.format(field.slug) for field in self.data_fields
             ])
             for user in CommCareUser.by_domain(domain):
                 self.add_user(user, writer)
@@ -39,10 +55,10 @@ class Command(BaseCommand):
                 or user.user_data.get('user_level', None) != 'real'):
             return
 
-        virtual_location_id = user.user_location_id,
+        virtual_location_id = user.user_location_id
         assigned_location_id = user.location_id
 
-        location_id = virtual_location_id or assigned_location
+        location_id = virtual_location_id or assigned_location_id
         location = self.locations_by_id.get(location_id, None) if location_id else None
         if not location:
             print "user {} {} has no location".format(user.username, user._id)
@@ -50,7 +66,7 @@ class Command(BaseCommand):
 
         writer.writerow([
             user._id,
-            user.username,
+            user.raw_username,
             user.first_name,
             user.last_name,
             # We expect this to ALWAYS be set
@@ -58,4 +74,9 @@ class Command(BaseCommand):
             assigned_location_id,
             location_id,
             location.name,
+            user.phone_number,
+            user.email,
+            user.language,
+        ] + [
+            user.user_data.get(field, '') for field in self.data_fields
         ])
