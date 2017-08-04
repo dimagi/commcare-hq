1e969f9c404319e669e4029a659b17ccd9c29ff8
NoahCarnahan <ncarnahan@dimagi.com>
2017-08-04 09:54:09 -0400

    Bring back v1 of property_list_configuration.html

--- corehq/apps/userreports/reports/builder/v1/forms.py
+++ corehq/apps/userreports/reports/builder/v1/forms.py
@@ -707,7 +707,7 @@ class ConfigureNewReportBase(forms.Form):
 
     @property
     def column_config_template(self):
-        return render_to_string('userreports/partials/property_list_configuration.html')
+        return render_to_string('userreports/partials/v1/property_list_configuration.html')
 
     @property
     def container_fieldset(self):
