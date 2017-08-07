9e8314e1cb5c101b686765ee9cd7227724e7e0b1
Farid Rener <proteusvacuum@gmail.com>
2017-08-07 18:18:35 -0400

    Catch duplicate incentives

--- custom/enikshay/management/commands/get_incentives_for_bets.py
+++ custom/enikshay/management/commands/get_incentives_for_bets.py
@@ -9,7 +9,9 @@ from corehq.motech.repeaters.dbaccessors import iter_repeat_records_by_domain, g
 
 class Command(BaseCommand):
     help = """
-    Output a CSV file of incentive payloads that were sent to BETS in repeater <repeater_id>.
+    Output 3 CSV files of incentive payloads that were sent to BETS in repeater <repeater_id>.
+    The second file contains a list of duplicate episode ids which should be handled manually.
+    The third file contains a list of errored cases.
 
     This should be run once for each incentive type:
     - BETS180TreatmentRepeater
@@ -41,10 +43,11 @@ class Command(BaseCommand):
         ]
 
         errors = []
+        seen_incentive_ids = set()
+        duplicate_incentive_ids = set()
         with open(filename, 'w') as f:
             writer = csv.writer(f)
             writer.writerow(row_names)
-
             for record in with_progress_bar(records, length=record_count):
                 try:
                     payload = json.loads(record.get_payload())['incentive_details'][0]
@@ -52,15 +55,27 @@ class Command(BaseCommand):
                     errors.append([record.payload_id, unicode(e)])
                     continue
                 payload['succeeded'] = record.succeeded
+                incentive_episode_pair = (payload.get('EpisodeID'), payload.get('EventID'),)
+                if incentive_episode_pair in seen_incentive_ids:
+                    duplicate_incentive_ids.add(incentive_episode_pair)
+                else:
+                    seen_incentive_ids.add(incentive_episode_pair)
                 row = [payload.get(name) for name in row_names]
+
                 writer.writerow(row)
 
-        print "{} errors".format(len(errors))
-        if len(errors) == 0:
-            return
+        print "{} duplicates found".format(len(duplicate_incentive_ids))
+        if duplicate_incentive_ids:
+            with open('duplicates_{}'.format(filename), 'w') as f:
+                writer = csv.writer(f)
+                writer.write_row(['episode_id', 'event_id'])
+                for duplicate_id in duplicate_incentive_ids:
+                    writer.write_row(duplicate_id)
 
-        with open('errors_{}'.format(filename), 'w') as f:
-            writer = csv.writer(f)
-            writer.write_row(['episode_id', 'error'])
-            for error in errors:
-                writer.write_row(errors)
+        print "{} errors".format(len(errors))
+        if errors:
+            with open('errors_{}'.format(filename), 'w') as f:
+                writer = csv.writer(f)
+                writer.write_row(['episode_id', 'error'])
+                for error in errors:
+                    writer.write_row(errors)
