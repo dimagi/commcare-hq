1f8a0e75df7c3c6136cca1a9a3455f4dab9e3c65
Farid Rener <proteusvacuum@gmail.com>
2017-08-08 16:26:42 -0400

    Update voucher output to match desired file

--- custom/enikshay/management/commands/get_vouchers_for_bets.py
+++ custom/enikshay/management/commands/get_vouchers_for_bets.py
@@ -3,8 +3,11 @@ import json
 
 from django.core.management.base import BaseCommand
 
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.util.log import with_progress_bar
 from corehq.motech.repeaters.dbaccessors import iter_repeat_records_by_domain, get_repeat_record_count
+from custom.enikshay.case_utils import get_person_case_from_voucher
+from corehq.apps.users.models import CommCareUser
 
 
 class Command(BaseCommand):
@@ -22,19 +25,26 @@ class Command(BaseCommand):
         parser.add_argument('filename')
 
     def handle(self, domain, repeater_id, filename, **options):
+        accessor = CaseAccessors(domain)
         records = iter_repeat_records_by_domain(domain, repeater_id=repeater_id)
         record_count = get_repeat_record_count(domain, repeater_id=repeater_id)
 
         row_names = [
-            'EventID',
+            'VoucherID',
             'EventOccurDate',
+            'EventID',
             'BeneficiaryUUID',
             'BeneficiaryType',
             'Location',
-            'DTOLocation',
-            'VoucherID',
             'Amount',
-            'succeeded',    # Some records did succeed when we sent them.
+            'DTOLocation',
+            'InvestigationType',
+            'PersonId',
+            'AgencyId',
+            'EnikshayApprover',
+            'EnikshayRole',
+            'EnikshayApprovalDate',
+            'Succeeded',    # Some records did succeed when we sent them.
                             # Include this so they don't re-pay people.
         ]
 
@@ -48,8 +58,21 @@ class Command(BaseCommand):
             for record in with_progress_bar(records, length=record_count):
                 try:
                     payload = json.loads(record.get_payload())['voucher_details'][0]
-                    payload['succeeded'] = record.succeeded
-                    voucher_id = payload['VoucherID']
+
+                    voucher_id = record.payload_id
+                    person_case = get_person_case_from_voucher(domain, voucher_id)
+                    voucher_case = accessor.get_case(voucher_id)
+                    payload[u'PersonId'] = person_case.case_id
+                    payload[u'AgencyId'] = voucher_case.get_case_property('voucher_fulfilled_by_id')
+
+                    approver_id = voucher_case.get_case_property('voucher_approved_by_id')
+                    approver = CommCareUser.get_by_user_id(approver_id)
+                    payload[u'EnikshayApprover'] = approver.name
+                    payload[u'EnikshayRole'] = approver.user_data.get('usertype')
+                    payload[u'EnikshayApprovalDate'] = voucher_case.get_case_property('date_approved')
+
+                    payload['Succeeded'] = record.succeeded
+
                 except Exception as e:
                     errors.append([record.payload_id, unicode(e)])
                     continue
@@ -57,7 +80,10 @@ class Command(BaseCommand):
                     duplicate_voucher_ids.add(voucher_id)
                 else:
                     seen_voucher_ids.add(voucher_id)
-                row = [payload.get(name) for name in row_names]
+                row = [
+                    payload.get(name) if payload.get(name) is not None else ""
+                    for name in row_names
+                ]
                 writer.writerow(row)
 
         print "{} duplicates found".format(len(duplicate_voucher_ids))
