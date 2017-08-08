bfbec7ad9f331d34b64db14cf3a20005b207a157
Simon Kelly <skelly@dimagi.com>
2017-08-08 14:21:13 +0200

    don't fail if missing

--- corehq/form_processor/tasks.py
+++ corehq/form_processor/tasks.py
@@ -9,6 +9,10 @@ SUBMISSION_REPROCESS_CELERY_QUEUE = 'submission_reprocessing_queue'
 
 @no_result_task(queue=SUBMISSION_REPROCESS_CELERY_QUEUE, acks_late=True)
 def reprocess_submission(submssion_stub_id):
-    stub = UnfinishedSubmissionStub.objects.get(id=submssion_stub_id)
+    try:
+        stub = UnfinishedSubmissionStub.objects.get(id=submssion_stub_id)
+    except UnfinishedSubmissionStub.DoesNotExist:
+        return
+
     reprocess_unfinished_stub(stub)
     datadog_counter('commcare.submission_reprocessing.count')
