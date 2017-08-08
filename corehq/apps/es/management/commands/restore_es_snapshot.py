095631d81f601fc6e05bdca2f6fcaf9479527c34
Nick Pellegrino <npellegrino@dimagi.com>
2017-08-08 19:44:39 -0400

    check pillow.checkpoint.sequence_format

--- corehq/apps/es/management/commands/restore_es_snapshot.py
+++ corehq/apps/es/management/commands/restore_es_snapshot.py
@@ -94,7 +94,10 @@ class Command(BaseCommand):
             try:
                 checkpoint = HistoricalPillowCheckpoint.objects.get(checkpoint_id=checkpoint.checkpoint_id,
                                                                     date_updated=date)
-                seq = str_to_kafka_seq(checkpoint.seq)
+                if pillow.checkpoint.sequence_format == 'json':
+                    seq = str_to_kafka_seq(checkpoint.seq)
+                else:
+                    seq = checkpoint.seq
             except HistoricalPillowCheckpoint.DoesNotExist:
                 seq = DEFAULT_EMPTY_CHECKPOINT_SEQUENCE_FOR_RESTORE[pillow.checkpoint.sequence_format]
 
