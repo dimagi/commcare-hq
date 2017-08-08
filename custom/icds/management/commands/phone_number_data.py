cefeb96c3fbd88cf00cb41282975f96f0e518593
Cal Ellowitz <calellowitz@gmail.com>
2017-08-08 17:44:13 -0500

    better handling for missing hh

--- custom/icds/management/commands/phone_number_data.py
+++ custom/icds/management/commands/phone_number_data.py
@@ -27,10 +27,14 @@ class Command(BaseCommand):
                 hh_id = row[4]
                 if hh_id:
                     person, hh = self.case_accessor.get_cases([case_id, hh_id], ordered=True)
-                    row[15] = SQLLocation.objects.get(location_id=owner).name
+                else:
+                    person = self.case_accessor.get_case(case_id)
+                    hh = None
+                row[15] = SQLLocation.objects.get(location_id=owner).name
+                if hh:
                     row[16] = hh.get_case_property('name')
                     row[17] = hh.get_case_property('hh_num')
-                    row[18] = person.get_case_property('name')
-                    row[19] = person.get_case_property('dob')
-                    row[20] = person.get_case_property('sex')
+                row[18] = person.get_case_property('name')
+                row[19] = person.get_case_property('dob')
+                row[20] = person.get_case_property('sex')
                 writer.writerow(row)
