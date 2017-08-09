58ccfbbf08a79aac7b5ba12ebca226b2f6c69f32
Ethan Soergel <esoergel@gmail.com>
2017-08-09 10:19:53 -0500

    Use raw_username, as discussed on 2017-08-09 call
    They don't want the @enikshay.commcarehq.org business

--- custom/enikshay/integrations/bets/repeater_generators.py
+++ custom/enikshay/integrations/bets/repeater_generators.py
@@ -441,7 +441,7 @@ class BETSUserPayloadGenerator(UserPayloadGenerator):
     def serialize(domain, user):
         location = user.get_sql_location(domain)
         user_json = {
-            "username": user.username,
+            "username": user.raw_username,
             "first_name": user.first_name,
             "last_name": user.last_name,
             "default_phone_number": user.default_phone_number,
