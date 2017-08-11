8a3cf88ad55d38b30177cfc9b735c9ec4dbbde13
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-08-11 11:01:14 -0400

    Add custom content handlers for LS 4b and AWW 6

--- settings.py
+++ settings.py
@@ -1558,6 +1558,10 @@ AVAILABLE_CUSTOM_SCHEDULING_CONTENT = {
         "custom.icds.messaging.custom_content.missed_cf_visit_to_ls",
     "ICDS_MISSED_PNC_VISIT_TO_LS":
         "custom.icds.messaging.custom_content.missed_pnc_visit_to_ls",
+    "ICDS_CHILD_ILLNESS_REPORTED":
+        "custom.icds.messaging.custom_content.child_illness_reported",
+    "ICDS_CF_VISITS_COMPLETE":
+        "custom.icds.messaging.custom_content.cf_visits_complete",
 }
 
 MAX_RULE_UPDATES_IN_ONE_RUN = 10000
