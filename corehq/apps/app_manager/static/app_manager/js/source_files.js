d037f04d2eccdcaf898776a12363b03029bee476
Jennifer Schweers <orange.jenny@gmail.com>
2017-08-11 11:04:30 -0400

    lint

--- corehq/apps/app_manager/static/app_manager/js/source_files.js
+++ corehq/apps/app_manager/static/app_manager/js/source_files.js
@@ -14,7 +14,7 @@ hqDefine('app_manager/js/source_files.js', function() {
         built_versions = _.sortBy(_.filter(built_versions, function(v) {
             return v.version != current_version;
         }), function(v) { return parseInt(v.version); }).reverse();
-        version_map = _.indexBy(built_versions, 'version');
+        var version_map = _.indexBy(built_versions, 'version');
     
         $input.select2({
             data: _.map(built_versions, function(v) {
