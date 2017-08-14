5570546c0547b7c6a7504291a9eb2ff5482f9f16
Nick Pellegrino <npellegrino@dimagi.com>
2017-08-14 11:27:12 -0400

    update internal subscription ending email

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -1522,6 +1522,7 @@ class Subscription(models.Model):
         context = {
             'domain': domain,
             'end_date': end_date,
+            'client_reminder_email_date': end_date - datetime.timedelta(days=30),
             'contacts': ', '.join(self._reminder_email_contacts(domain)),
             'dimagi_contact': email,
         }
