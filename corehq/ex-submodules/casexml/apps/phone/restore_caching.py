0a1e459fa2cbcf756b0e372077dfc977bb1fd3da
Daniel Roberts <droberts@dimagi.com>
2017-08-21 18:49:01 -0400

    add debugging to restore cache accessors

--- corehq/ex-submodules/casexml/apps/phone/restore_caching.py
+++ corehq/ex-submodules/casexml/apps/phone/restore_caching.py
@@ -1,7 +1,10 @@
 import hashlib
+import logging
 from casexml.apps.phone.const import RESTORE_CACHE_KEY_PREFIX, ASYNC_RESTORE_CACHE_KEY_PREFIX
 from casexml.apps.phone.utils import get_restore_response_class
 from dimagi.utils.couch.cache.cache_core import get_redis_default_cache
+
+logger = logging.getLogger(__name__)
 
 
 def _restore_cache_key(domain, prefix, user_id, version, sync_log_id, device_id):
@@ -42,22 +45,27 @@ def _async_restore_task_id_cache_key(domain, user_id, sync_log_id, device_id):
 class CacheAccessor(object):
     cache_key = None
     timeout = None
+    debug_info = None
 
     def get_value(self):
+        logger.debug('getting {}'.format(self.debug_info))
         return get_redis_default_cache().get(self.cache_key)
 
     def set_value(self, value, timeout=None):
+        logger.debug('setting {}'.format(self.debug_info))
         if timeout is None:
             timeout = self.timeout
         get_redis_default_cache().set(self.cache_key, value, timeout=timeout)
 
     def invalidate(self):
+        logger.debug('invalidating {}'.format(self.debug_info))
         get_redis_default_cache().delete(self.cache_key)
 
 
 class RestorePayloadPathCache(CacheAccessor):
     def __init__(self, domain, user_id, sync_log_id, device_id):
         self.cache_key = _restore_payload_path_cache_key(domain, user_id, sync_log_id, device_id)
+        self.debug_info = ('RestorePayloadPathCache', domain, user_id, sync_log_id, device_id)
 
 
 class AsyncRestoreTaskIdCache(CacheAccessor):
@@ -65,3 +73,4 @@ class AsyncRestoreTaskIdCache(CacheAccessor):
 
     def __init__(self, domain, user_id, sync_log_id, device_id):
         self.cache_key = _async_restore_task_id_cache_key(domain, user_id, sync_log_id, device_id)
+        self.debug_info = ('AsyncRestoreTaskIdCache', domain, user_id, sync_log_id, device_id)
