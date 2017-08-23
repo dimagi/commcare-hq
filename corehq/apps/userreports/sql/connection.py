d6db3c696ac36989465d8bef2245ab42e6ea7862
Simon Kelly <skelly@dimagi.com>
2017-08-23 15:49:08 +0200

    only allow replica usage for report datasources

--- corehq/apps/userreports/sql/connection.py
+++ corehq/apps/userreports/sql/connection.py
@@ -5,13 +5,13 @@ from corehq.sql_db.connections import connection_manager
 READ_REPLICA_ROLLOUT_FACTOR = 0.01  # 1%
 
 
-def get_engine_id(an_object):
+def get_engine_id(an_object, allow_read_replicas=False):
     """
     Given an object, get the engine id for it.
     """
     # for now this only deals with data sources.
     from corehq.apps.userreports.models import DataSourceConfiguration
     assert isinstance(an_object, DataSourceConfiguration)
-    if random.random() < READ_REPLICA_ROLLOUT_FACTOR:
+    if allow_read_replicas and random.random() < READ_REPLICA_ROLLOUT_FACTOR:
         return connection_manager.get_read_replica_engine_id(an_object.engine_id)
     return an_object.engine_id
