ea395aec1362dd7d041f6c5de9f0738de1214e74
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-08-24 12:49:49 -0400

    Remove enikshay-only setting

--- corehq/apps/data_interfaces/tasks.py
+++ corehq/apps/data_interfaces/tasks.py
@@ -138,10 +138,9 @@ def run_case_update_rules_for_domain(domain, now=None):
                     last_migration_check_time)
 
                 time_elapsed = datetime.utcnow() - start_run
-                max_updates = 50000 if domain == 'enikshay' else settings.MAX_RULE_UPDATES_IN_ONE_RUN
                 if (
                     time_elapsed.seconds > HALT_AFTER or
-                    case_update_result.total_updates >= max_updates or
+                    case_update_result.total_updates >= settings.MAX_RULE_UPDATES_IN_ONE_RUN or
                     migration_in_progress
                 ):
                     run_record.done(DomainCaseRuleRun.STATUS_HALTED, cases_checked, case_update_result)
