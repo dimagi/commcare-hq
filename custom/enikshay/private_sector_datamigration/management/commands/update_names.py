7ed918179d67271c7eb9e2addb6925d8f9100240
Nick Pellegrino <npellegrino@dimagi.com>
2017-08-29 08:16:52 -0400

    agency_id

--- custom/enikshay/private_sector_datamigration/management/commands/update_names.py
+++ custom/enikshay/private_sector_datamigration/management/commands/update_names.py
@@ -3,6 +3,7 @@ from django.core.management import BaseCommand
 from corehq.apps.locations.models import SQLLocation
 from corehq.apps.users.models import CommCareUser
 from corehq.util.log import with_progress_bar
+from custom.enikshay.models import AgencyIdCounter
 from custom.enikshay.private_sector_datamigration.models import UserDetail, Agency
 
 
@@ -10,8 +11,14 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('domain')
+        parser.add_argument(
+            '--dry_run',
+            action='store_true',
+            default=False,
+            dest='dry_run',
+        )
 
-    def handle(self, domain, **options):
+    def handle(self, domain, dry_run=True, **options):
         locs_matching_type = SQLLocation.active_objects.filter(
             domain=domain,
             location_type__code__in=[
@@ -23,35 +30,26 @@ class Command(BaseCommand):
             ],
         )
 
-        user_ids_updated = []
+        location_ids_updated = []
 
         for i, loc in enumerate(with_progress_bar(locs_matching_type)):
             private_sector_agency_id = loc.metadata.get('private_sector_agency_id', '')
             if not private_sector_agency_id.isdigit():
-                continue
-            agency_id = int(private_sector_agency_id)
-
-            try:
-                agency = Agency.objects.get(agencyId=agency_id)
-            except Agency.DoesNotExist:
-                continue
-
-            try:
-                user_detail = UserDetail.objects.get(agencyId=agency_id, isPrimary=True)
-            except UserDetail.DoesNotExist:
-                user_detail = None
-            location_user = CommCareUser.get_by_user_id(loc.user_id)
-
-            if user_detail is None or user_detail.firstName.lower() in ['dummy', 'not available', 'na']:
-                agency_name_split = agency.agencyName.split()
-                if len(agency_name_split) == 1:
-                    location_user.first_name = agency_name_split[0]
-                    location_user.last_name = ''
+                if dry_run:
+                    print 'assign private_sector_agency_id to %s' % loc.name
+                    continue
                 else:
-                    location_user.first_name = ' '.join(agency_name_split[:-1])
-                    location_user.last_name = agency_name_split[-1]
-                location_user.save()
-                user_ids_updated.append(location_user._id)
+                    loc.metadata['private_sector_agency_id'] = private_sector_agency_id = str(AgencyIdCounter.get_new_agency_id())
+                    loc.save()
+            agency_name_split = loc.name.split('-')
+            if agency_name_split[-1].strip() != private_sector_agency_id:
+                print 'old name: %s' % loc.name
+                loc.name = '%s - %s' % (loc.name, private_sector_agency_id)
+                if not dry_run:
+                    loc.save()
+                print 'new name: %s' % loc.name
+                print 'updated loc %s' % loc.location_id
+                print '-----'
+                location_ids_updated.append(loc.location_id)
 
-        print user_ids_updated
-        print "Updated {} users".format(len(user_ids_updated))
+        print "Updated {} users".format(len(location_ids_updated))
