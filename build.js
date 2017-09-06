5cadfd66e21f17c8711d928ac8024961264bbebc
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-06 09:28:24 -0400

    fix r.js not finding paths correctly

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 ({
-    mainConfigFile: "hq.js",
+    mainConfigFile: "corehq/apps/fixtures/static/hq.js",
+    baseUrl: 'staticfiles',
     name: "fixtures/js/lookup-manage",
     out: "corehq/apps/fixtures/static/fixtures/js/built.js",
     optimize: "none",
