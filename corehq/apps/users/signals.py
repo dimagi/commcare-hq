a1486adb1594da16c888a4197033019cab7ea994
Jonathan Emord <jonathan.emord@gmail.com>
2017-09-11 14:14:01 +0530

    Remove user signal

--- corehq/apps/users/signals.py
+++ corehq/apps/users/signals.py
@@ -31,15 +31,6 @@ def django_user_post_save_signal(sender, instance, created, raw=False, **kwargs)
     return CouchUser.django_user_post_save_signal(sender, instance, created)
 
 
-def update_user_in_es(sender, couch_user, **kwargs):
-    """
-    Automatically sync the user to elastic directly on save or delete
-    """
-    from corehq.pillows.user import transform_user_for_elasticsearch
-    send_to_elasticsearch("users", transform_user_for_elasticsearch(couch_user.to_json()),
-                          delete=couch_user.to_be_deleted())
-
-
 def sync_user_phone_numbers(sender, couch_user, **kwargs):
     from corehq.apps.sms.tasks import sync_user_phone_numbers as sms_sync_user_phone_numbers
     sms_sync_user_phone_numbers.delay(couch_user.get_id)
@@ -50,5 +41,4 @@ def connect_user_signals():
     from django.contrib.auth.models import User
     post_save.connect(django_user_post_save_signal, User,
                       dispatch_uid="django_user_post_save_signal")
-    couch_user_post_save.connect(update_user_in_es, dispatch_uid="update_user_in_es")
     couch_user_post_save.connect(sync_user_phone_numbers, dispatch_uid="sync_user_phone_numbers")
