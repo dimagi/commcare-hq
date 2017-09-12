1b8d54d71c94476ea0a79d63ef82c670c4f525aa
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-12 18:12:45 -0400

    fix path to ko.mapping

--- corehq/apps/hqwebapp/static/hqwebapp/js/requirejs_config.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/requirejs_config.js
@@ -7,7 +7,7 @@ requirejs.config({
         "underscore": "underscore/underscore",
         "bootstrap": "bootstrap/dist/js/bootstrap.min",
         "knockout": "knockout/dist/knockout.debug",
-        "ko.mapping": "style/lib/knockout_plugins/knockout_mapping.ko.min",
+        "ko.mapping": "hqwebapp/js/lib/knockout_plugins/knockout_mapping.ko.min",
         "datatables": "datatables/media/js/jquery.dataTables.min",
         "datatables.fixedColumns": "datatables-fixedcolumns/js/dataTables.fixedColumns",
         "datatables.bootstrap": "datatables-bootstrap3/BS3/assets/js/datatables",
