332a98ba04384bb120a5ce6d92c72492d1b588a7
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-13 13:55:53 -0400

    Introduce buld config, bundle for jquery/ko/_, and bundle for hqwebapp

---
+++ corehq/apps/hqwebapp/static/hqwebapp/js/built.js
@@ -0,0 +1,17 @@
+/* globals define */
+define("hqwebapp/js/built", [
+    "hqwebapp/js/alert_user",
+    "hqwebapp/js/initial_page_data",
+    "hqwebapp/js/main",
+    "hqwebapp/js/hq.helpers",
+], function(
+    alertUser,
+    initialPageData,
+    main
+){
+    return {
+        alertUser: alertUser,
+        initialPageData: initialPageData,
+        main: main
+    };
+});
