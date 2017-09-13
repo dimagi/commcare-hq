e472ab3cf056b917872b2ba7f163e293fd0738d6
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-13 13:09:13 -0400

    Basic configuration for requirejs

---
+++ corehq/apps/hqwebapp/static/hqwebapp/js/requirejs_config.js
@@ -0,0 +1,26 @@
+/* globals requirejs */
+requirejs.config({
+    baseUrl: '/static/',
+    paths: {
+        "jquery": "jquery/dist/jquery.min",
+        "jquery.form": "jquery-form/jquery.form",
+        "underscore": "underscore/underscore",
+        "bootstrap": "bootstrap/dist/js/bootstrap.min",
+        "knockout": "knockout/dist/knockout.debug",
+        "ko.mapping": "hqwebapp/js/lib/knockout_plugins/knockout_mapping.ko.min",
+        "datatables": "datatables/media/js/jquery.dataTables.min",
+        "datatables.fixedColumns": "datatables-fixedcolumns/js/dataTables.fixedColumns",
+        "datatables.bootstrap": "datatables-bootstrap3/BS3/assets/js/datatables",
+    },
+    shim: {
+        "bootstrap": { deps: ['jquery'] },
+        "ko.mapping": { deps: ['knockout'] },
+        "hqwebapp/js/hq.helpers": { deps: ['jquery', 'bootstrap', 'knockout', 'underscore'] },
+        "datatables.bootstrap": { deps: ['datatables'] },
+    },
+    map: {
+        "datatables.fixedColumns": {
+            "datatables.net": "datatables",
+        },
+    },
+});
