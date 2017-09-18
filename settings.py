18eed3541a100c5e0d7b99c72a99835fd1a57b90
Jonathan Emord <jonathan.emord@gmail.com>
2017-09-18 09:56:28 +0530

    Added configurable crontab for async UCRs.

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@
 from __future__ import absolute_import
 import importlib
 from collections import defaultdict
-
 import os
-from six.moves.urllib.parse import urlencode
+
+from celery.schedules import crontab
 from django.contrib import messages
 import settingshelper as helper
 
@@ -916,6 +916,7 @@ ENIKSHAY_PRIVATE_API_PASSWORD = None
 # number of docs for UCR to queue asynchronously at once
 # ideally # of documents it takes to process in ~30 min
 ASYNC_INDICATORS_TO_QUEUE = 10000
+ASYNC_INDICATOR_QUEUE_CRONTAB = crontab(minute="*/5")
 DAYS_TO_KEEP_DEVICE_LOGS = 60
 
 MAX_RULE_UPDATES_IN_ONE_RUN = 10000
