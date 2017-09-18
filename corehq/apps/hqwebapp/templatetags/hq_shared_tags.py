9b44f48610a30b41eac6b49009890335e7aa9f5a
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-18 17:35:46 -0400

    comment for set_requirejs_main

--- corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
+++ corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
@@ -692,6 +692,8 @@ def analytics_ab_test(parser, token):
 @register.simple_tag()
 def set_requirejs_main(request, path):
     """
+    Indicate that a page should be using RequireJS, by naming the
+    JavaScript module to be used as the page's main entry point.
     """
     request.requirejs_main = path
     return ''
