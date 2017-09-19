8867abdac2ec0d9f404bc3041dee2c6379c22134
Jonathan Emord <jonathan.emord@gmail.com>
2017-09-19 12:23:26 +0530

    Added v2 mobile UCR support.
    Added indexed attribute for mobile UCR fixtures.

--- corehq/apps/app_manager/fixtures/mobile_ucr.py
+++ corehq/apps/app_manager/fixtures/mobile_ucr.py
@@ -49,12 +49,13 @@ def _should_sync(restore_state):
     return (
         not last_sync_log or
         not sync_interval or
-        (datetime.utcnow() - last_sync_log.date).total_seconds() > sync_interval
+        (_utcnow() - last_sync_log.date).total_seconds() > sync_interval
     )
 
 
 class ReportFixturesProvider(FixtureProvider):
     v1_id = 'commcare:reports'
+    v2_id_prefix = 'commcare-reports:'
 
     def __call__(self, restore_state):
         """
@@ -100,7 +101,7 @@ class ReportFixturesProvider(FixtureProvider):
 
     def _v1_fixture(self, restore_user, report_configs):
         root = E.fixture(id=self.v1_id, user_id=restore_user.user_id)
-        reports_elem = E.reports(last_sync=datetime.utcnow().isoformat())
+        reports_elem = E.reports(last_sync=_utcnow().isoformat())
         for report_config in report_configs:
             try:
                 reports_elem.append(self.report_config_to_v1_fixture(report_config, restore_user))
@@ -116,6 +117,23 @@ class ReportFixturesProvider(FixtureProvider):
                     raise
         root.append(reports_elem)
         return [root]
+
+    def _v2_fixtures(self, restore_user, report_configs):
+        fixtures = []
+        for report_config in report_configs:
+            try:
+                fixtures.extend(self.report_config_to_v2_fixture(report_config, restore_user))
+            except ReportConfigurationNotFoundError as err:
+                logging.exception('Error generating report fixture: {}'.format(err))
+                continue
+            except UserReportsError:
+                if settings.UNIT_TESTING or settings.DEBUG:
+                    raise
+            except Exception as err:
+                logging.exception('Error generating report fixture: {}'.format(err))
+                if settings.UNIT_TESTING or settings.DEBUG:
+                    raise
+        return fixtures
 
     @staticmethod
     def report_config_to_v1_fixture(report_config, restore_user):
@@ -153,7 +171,7 @@ class ReportFixturesProvider(FixtureProvider):
             {ui_filter.field for ui_filter in defer_filters.values()},
             filter_options_by_field
         )
-        filters_elem = ReportFixturesProvider._get_v1_filters_elem(
+        filters_elem = ReportFixturesProvider._get_filters_elem(
             defer_filters, filter_options_by_field, restore_user._couch_user)
 
         if (data_source.config.backend_id in UCR_SUPPORT_BOTH_BACKENDS and
@@ -166,6 +184,59 @@ class ReportFixturesProvider(FixtureProvider):
         return report_elem
 
     @staticmethod
+    def report_config_to_v2_fixture(report_config, restore_user):
+        domain = restore_user.domain
+        report, data_source = ReportFixturesProvider._get_report_and_data_source(
+            report_config.report_id, domain
+        )
+        report_fixture_id = 'commcare-reports:' + report_config.report_id
+
+        # TODO: Convert to be compatible with restore_user
+        # apply filters specified in report module
+        all_filter_values = {
+            filter_slug: restore_user.get_ucr_filter_value(filter, report.get_ui_filter(filter_slug))
+            for filter_slug, filter in report_config.filters.items()
+        }
+        # apply all prefilters
+        prefilters = [ReportFilterFactory.from_spec(p, report) for p in report.prefilters]
+        prefilter_values = {prefilter.name: prefilter.value() for prefilter in prefilters}
+        all_filter_values.update(prefilter_values)
+        # filter out nulls
+        filter_values = {
+            filter_slug: filter_value for filter_slug, filter_value in all_filter_values.items()
+            if filter_value is not None
+        }
+        defer_filters = {
+            filter_slug: report.get_ui_filter(filter_slug)
+            for filter_slug, filter_value in all_filter_values.items()
+            if filter_value is None and is_valid_mobile_select_filter_type(report.get_ui_filter(filter_slug))
+        }
+        data_source.set_filter_values(filter_values)
+        data_source.defer_filters(defer_filters)
+        filter_options_by_field = defaultdict(set)
+
+        rows_elem = ReportFixturesProvider._get_v2_report_elem(
+            data_source,
+            {ui_filter.field for ui_filter in defer_filters.values()},
+            filter_options_by_field
+        )
+        filters_elem = ReportFixturesProvider._get_filters_elem(
+            defer_filters, filter_options_by_field, restore_user._couch_user)
+        filters_elem.attrib['id'] = report_fixture_id
+
+        if (data_source.config.backend_id in UCR_SUPPORT_BOTH_BACKENDS and
+                random.randint(0, MOBILE_UCR_RANDOM_THRESHOLD) == MOBILE_UCR_RANDOM_THRESHOLD):
+            compare_ucr_dbs.delay(domain, report_config.report_id, filter_values)
+
+        report_elem = E.fixture(
+            id=report_fixture_id, user_id=restore_user.user_id,
+            report_id=report_config.report_id, last_sync=_utcnow().isoformat(),
+            indexed='true'
+        )
+        report_elem.append(rows_elem)
+        return [filters_elem, report_elem]
+
+    @staticmethod
     def _get_report_and_data_source(report_id, domain):
         report = get_report_config(report_id, domain)[0]
         data_source = ReportFactory.from_spec(report, include_prefilters=True)
@@ -175,7 +246,7 @@ class ReportFixturesProvider(FixtureProvider):
         return report, data_source
 
     @staticmethod
-    def _get_v1_filters_elem(defer_filters, filter_options_by_field, couch_user):
+    def _get_filters_elem(defer_filters, filter_options_by_field, couch_user):
         filters_elem = E.filters()
         for filter_slug, ui_filter in defer_filters.items():
             # @field is maybe a bad name for this attribute,
@@ -217,6 +288,39 @@ class ReportFixturesProvider(FixtureProvider):
                 is_total_row=True,
             ))
         return rows_elem
+
+    @staticmethod
+    def _get_v2_report_elem(data_source, deferred_fields, filter_options_by_field):
+        def _row_to_row_elem(row, index, is_total_row=False):
+            row_elem = E.row(index=str(index), is_total_row=str(is_total_row))
+            for k in sorted(row.keys()):
+                value = serialize(row[k])
+                # todo escape k to be valid xml tag
+                row_elem.append(E(k, value))
+                if not is_total_row and k in deferred_fields:
+                    filter_options_by_field[k].add(value)
+            return row_elem
+
+        rows_elem = E.rows()
+        for i, row in enumerate(data_source.get_data()):
+            rows_elem.append(_row_to_row_elem(row, i))
+        if data_source.has_total_row:
+            total_row = data_source.get_total_row()
+            rows_elem.append(_row_to_row_elem(
+                dict(
+                    zip(
+                        map(lambda column_config: column_config.column_id, data_source.top_level_columns),
+                        map(str, total_row)
+                    )
+                ),
+                data_source.get_total_records(),
+                is_total_row=True,
+            ))
+        return rows_elem
+
+
+def _utcnow():
+    return datetime.utcnow()
 
 
 report_fixture_generator = ReportFixturesProvider()
