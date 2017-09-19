794f7b47f09ab1cd034a384c536ee7a1019321ca
Cory Zue <czue@dimagi.com>
2017-09-19 17:58:58 +0200

    drop no-longer used pl_proxy files

--- corehq/sql_accessors/sql_templates/get_cases_by_id.sql
+++
@@ -1,8 +0,0 @@
-DROP FUNCTION IF EXISTS get_cases_by_id(TEXT[]);
-
-CREATE FUNCTION get_cases_by_id(case_ids TEXT[]) RETURNS SETOF form_processor_commcarecasesql AS $$
-BEGIN
-    RETURN QUERY
-    SELECT * FROM form_processor_commcarecasesql where case_id = ANY(case_ids);
-END;
-$$ LANGUAGE plpgsql;
