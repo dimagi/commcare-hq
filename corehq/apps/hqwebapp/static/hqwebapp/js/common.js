c6367bd051bd89761836bb7f6e21b46aedc252fe
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-19 10:20:33 -0400

    left underscore out of common.js

--- corehq/apps/hqwebapp/static/hqwebapp/js/common.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/common.js
@@ -3,6 +3,7 @@ define([
     "knockout",
     "ko.mapping",
     "hqwebapp/js/knockout_bindings.ko",
+    "underscore",
     "hqwebapp/js/django",
 ], function() {
     /* in non-development environments, this will be overwritten by r.js */
