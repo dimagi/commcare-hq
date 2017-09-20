9c864ff4d27ff6b16808344e8469d39d9804bda3
Daniel Miller <dmiller@dimagi.com>
2017-09-20 18:21:37 -0400

    Remove unnecessary block in test

--- corehq/apps/hqwebapp/templatetags/tests/test_tag.py
+++ corehq/apps/hqwebapp/templatetags/tests/test_tag.py
@@ -80,7 +80,7 @@ class TagTest(SimpleTestCase):
                 {% requirejs_main "requirejs/main" %}
                 {% block content %}{{requirejs_main}}{% endblock %}
             """).strip(),
-            "expected requirejs/main after block\n\n\nrequirejs/main",
+            "expected requirejs/main after tag\n\n\nrequirejs/main",
         )
 
     def test_requirejs_main_no_arg(self):
