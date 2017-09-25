a04c160bfafbfb0cc5be4ec1de9061a5c2a91f99
Jennifer Schweers <orange.jenny@gmail.com>
2017-09-25 15:17:07 -0400

    clean up debug code

--- corehq/apps/app_manager/static/app_manager/js/forms/app_notifications.js
+++ corehq/apps/app_manager/static/app_manager/js/forms/app_notifications.js
@@ -3,34 +3,22 @@ hqDefine('app_manager/js/forms/app_notifications', function () {
     var getMessage = function(redisMessage, userId) {
         var msgObj = JSON.parse(redisMessage);
         // only show notifications from other users
-console.log("in getMessage, ids = " + msgObj.user_id + ", " + userId);
-debugger;
-        //if (msgObj.user_id !== userId) {
+        if (msgObj.user_id !== userId) {
             return moment(msgObj.timestamp).format('h:mm:ss a') + ': ' + msgObj.text;
-        //}
+        }
         return "";
     };
 
     var alertUser = function(userId, callback, context) {
-console.log("calling alertUser, callback=" + callback);
-debugger;
         if (!callback) {
             callback = hqImport("hqwebapp/js/alert_user").alert_user;
         }
-//        return function(userId) {
-//console.log("calling inner function, userId=" + userId);
-//debugger;
-            return function(redisMessage) {
-console.log("calling innermost function, redisMessage=" + redisMessage);
-debugger;
-                var message = getMessage(redisMessage, userId);
-console.log("message=" + message);
-debugger;
-                if (message) {
-                    callback.apply(context, [message, 'info', true]);
-                }
-            };
-//        };
+        return function(redisMessage) {
+            var message = getMessage(redisMessage, userId);
+            if (message) {
+                callback.apply(context, [message, 'info', true]);
+            }
+        };
     };
 
     return {
