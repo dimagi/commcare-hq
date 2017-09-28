c73d378a640ae8cb581a73c2f1805d51b894cdbb
sheelio <sshah@dimagi.com>
2017-09-28 11:20:16 -0400

    Clean up

--- custom/enikshay/management/commands/run_interim_outcome.py
+++ custom/enikshay/management/commands/run_interim_outcome.py
@@ -66,123 +66,128 @@ class Command(BaseCommand):
             writer.writerow(headers)
             accessor = CaseAccessors(domain=domain)
             for episode in accessor.iter_cases(case_ids):
-                if episode.get_case_property('datamigration_interim_outcome') != 'yes':
-                    # Filter and skip private cases
-                    try:
-                        person = get_person_case_from_episode(domain, episode.case_id)
-                    except ENikshayCaseNotFound:
-                        continue
-                    if person.get_case_property(ENROLLED_IN_PRIVATE) != 'true':
-                        # Get all follow up tests with a result
-                        occurrence = get_occurrence_case_from_episode(domain, episode.case_id)
-                        tests = [case for case in accessor.get_reverse_indexed_cases([occurrence.get_id])
-                                 if case.type == CASE_TYPE_TEST
-                                 and case.get_case_property('rft_general') == 'follow_up_drtb'
-                                 and case.get_case_property('result_recorded') == 'yes'
-                                ]
-                        tests = sorted(tests, key=lambda test: test.get_case_property('date_tested'))
+                if self.is_valid_case(domain, episode):
+                    # Get all follow up tests with a result
+                    occurrence = get_occurrence_case_from_episode(domain, episode.case_id)
+                    tests = [case for case in accessor.get_reverse_indexed_cases([occurrence.get_id])
+                             if case.type == CASE_TYPE_TEST
+                             and case.get_case_property('rft_general') == 'follow_up_drtb'
+                             and case.get_case_property('result_recorded') == 'yes'
+                            ]
+                    tests = sorted(tests, key=lambda test: test.get_case_property('date_tested'))
 
-                        interim_outcome = ''
-                        interim_outcome_history = ''
-                        for i in range(1, len(tests)):
-                            test_interim_outcome = None
-                            test_interim_outcome_text = None
-                            current_test = tests[i]
-                            current_test_date = datetime.datetime.strptime(
-                                current_test.get_case_property('date_tested'),
-                                "%Y-%m-%d").date()
-                            prev_test = tests[i - 1]
-                            prev_test_date = datetime.datetime.strptime(
-                                prev_test.get_case_property('date_tested'),
-                                "%Y-%m-%d").date()
-                            if (current_test_date - prev_test_date).days >= 30:
-                                if (
-                                    prev_test.get_case_property('result') == TEST_RESULT_TB_NOT_DETECTED
-                                    and current_test.get_case_property('result') == TEST_RESULT_TB_NOT_DETECTED
-                                    and interim_outcome != 'culture_conversion'
-                                ):
-                                    test_interim_outcome = 'culture_conversion'
-                                    test_interim_outcome_text = "Culture Conversion"
-                                elif (
-                                    prev_test.get_case_property('result') == TEST_RESULT_TB_DETECTED
-                                    and current_test.get_case_property('result') == TEST_RESULT_TB_DETECTED
-                                    and interim_outcome == 'culture_conversion'
-                                ):
-                                    test_interim_outcome = 'culture_reversion'
-                                    test_interim_outcome_text = "Culture Reversion"
+                    interim_outcome = ''
+                    interim_outcome_history = ''
+                    for i in range(1, len(tests)):
+                        test_interim_outcome = None
+                        test_interim_outcome_text = None
+                        current_test = tests[i]
+                        current_test_date = datetime.datetime.strptime(
+                            current_test.get_case_property('date_tested'),
+                            "%Y-%m-%d").date()
+                        prev_test = tests[i - 1]
+                        prev_test_date = datetime.datetime.strptime(
+                            prev_test.get_case_property('date_tested'),
+                            "%Y-%m-%d").date()
+                        if (current_test_date - prev_test_date).days >= 30:
+                            if (
+                                prev_test.get_case_property('result') == TEST_RESULT_TB_NOT_DETECTED
+                                and current_test.get_case_property('result') == TEST_RESULT_TB_NOT_DETECTED
+                                and interim_outcome != 'culture_conversion'
+                            ):
+                                test_interim_outcome = 'culture_conversion'
+                                test_interim_outcome_text = "Culture Conversion"
+                            elif (
+                                prev_test.get_case_property('result') == TEST_RESULT_TB_DETECTED
+                                and current_test.get_case_property('result') == TEST_RESULT_TB_DETECTED
+                                and interim_outcome == 'culture_conversion'
+                            ):
+                                test_interim_outcome = 'culture_reversion'
+                                test_interim_outcome_text = "Culture Reversion"
 
-                                if test_interim_outcome:
-                                    interim_outcome = test_interim_outcome
-                                    interim_outcome_history = prev_test_date.strftime("%d/%m/%y") \
-                                                              + ": " \
-                                                              + test_interim_outcome_text \
-                                                              + '\n' \
-                                                              + interim_outcome_history
+                            if test_interim_outcome:
+                                interim_outcome = test_interim_outcome
+                                interim_outcome_history = prev_test_date.strftime("%d/%m/%y") \
+                                                          + ": " \
+                                                          + test_interim_outcome_text \
+                                                          + '\n' \
+                                                          + interim_outcome_history
 
-                                    writer.writerow([current_test.case_id,
-                                                     "test",
-                                                     current_test.get_case_property('date_tested'),
-                                                     current_test.get_case_property('result'),
-                                                     test_interim_outcome,
-                                                     None,
-                                                     None,
-                                                     "yes"])
-                                    print('Updating {}...'.format(current_test.case_id))
-                                    case_structure = CaseStructure(
-                                        case_id=current_test.case_id,
-                                        walk_related=False,
-                                        attrs={
-                                            "create": False,
-                                            "update": {
-                                                "interim_outcome": test_interim_outcome,
-                                                "datamigration_interim_outcome": "yes",
-                                            },
+                                writer.writerow([current_test.case_id,
+                                                 "test",
+                                                 current_test.get_case_property('date_tested'),
+                                                 current_test.get_case_property('result'),
+                                                 test_interim_outcome,
+                                                 None,
+                                                 None,
+                                                 "yes"])
+                                print('Updating {}...'.format(current_test.case_id))
+                                case_structure = CaseStructure(
+                                    case_id=current_test.case_id,
+                                    walk_related=False,
+                                    attrs={
+                                        "create": False,
+                                        "update": {
+                                            "interim_outcome": test_interim_outcome,
+                                            "datamigration_interim_outcome": "yes",
                                         },
-                                    )
-                                    if commit:
-                                        factory.create_or_update_case(case_structure)
-                                else:
-                                    writer.writerow([current_test.case_id,
-                                                     "test",
-                                                     current_test.get_case_property('date_tested'),
-                                                     current_test.get_case_property('result'),
-                                                     None,
-                                                     None,
-                                                     None,
-                                                     "no"])
-
-                        # update episode if needed
-                        if interim_outcome:
-                            writer.writerow([episode.case_id,
-                                             "episode",
-                                             None,
-                                             None,
-                                             None,
-                                             interim_outcome,
-                                             interim_outcome_history,
-                                             "yes"])
-                            print('Updating {}...'.format(episode.case_id))
-                            case_structure = CaseStructure(
-                                case_id=episode.case_id,
-                                walk_related=False,
-                                attrs={
-                                    "create": False,
-                                    "update": {
-                                        "interim_outcome": interim_outcome,
-                                        "datamigration_interim_outcome": "yes",
                                     },
+                                )
+                                if commit:
+                                    factory.create_or_update_case(case_structure)
+                            else:
+                                writer.writerow([current_test.case_id,
+                                                 "test",
+                                                 current_test.get_case_property('date_tested'),
+                                                 current_test.get_case_property('result'),
+                                                 None,
+                                                 None,
+                                                 None,
+                                                 "no"])
+
+                    # update episode if needed
+                    if interim_outcome:
+                        writer.writerow([episode.case_id,
+                                         "episode",
+                                         None,
+                                         None,
+                                         None,
+                                         interim_outcome,
+                                         interim_outcome_history,
+                                         "yes"])
+                        print('Updating {}...'.format(episode.case_id))
+                        case_structure = CaseStructure(
+                            case_id=episode.case_id,
+                            walk_related=False,
+                            attrs={
+                                "create": False,
+                                "update": {
+                                    "interim_outcome": interim_outcome,
+                                    "interim_outcome_history": interim_outcome_history,
+                                    "datamigration_interim_outcome": "yes",
                                 },
-                            )
-                            if commit:
-                                factory.create_or_update_case(case_structure)
-                        else:
-                            writer.writerow([episode.case_id,
-                                             "episode",
-                                             None,
-                                             None,
-                                             None,
-                                             None,
-                                             None,
-                                             "no"])
+                            },
+                        )
+                        if commit:
+                            factory.create_or_update_case(case_structure)
+                    else:
+                        writer.writerow([episode.case_id,
+                                         "episode",
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         None,
+                                         "no"])
 
         print("Migration finished at {}".format(datetime.datetime.utcnow()))
+
+    def is_valid_case(self, domain, episode):
+        if episode.get_case_property('datamigration_interim_outcome') != 'yes':
+            # Filter and skip private cases
+            try:
+                person = get_person_case_from_episode(domain, episode.case_id)
+            except ENikshayCaseNotFound:
+                return False
+            return person.get_case_property(ENROLLED_IN_PRIVATE) != 'true'
+        return False
