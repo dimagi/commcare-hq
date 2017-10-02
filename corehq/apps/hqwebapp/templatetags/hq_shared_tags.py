f43b9cef8f6c5a2ed0f0e93962dfa85038352706
Jennifer Schweers <orange.jenny@gmail.com>
2017-10-02 16:18:08 -0400

    Support static tag ignoring CDN

--- corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
+++ corehq/apps/hqwebapp/templatetags/hq_shared_tags.py
@@ -104,8 +104,10 @@ except (ImportError, SyntaxError):
 
 @register.filter
 @register.simple_tag
-def static(url):
+def static(url, skip_cdn=False):
     resource_url = url
+    if skip_cdn:
+        return settings.STATIC_URL + url
     version = resource_versions.get(resource_url)
     url = settings.STATIC_CDN + settings.STATIC_URL + url
     is_less = url.endswith('.less')
