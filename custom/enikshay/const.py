e9c046ee86abca06a700d3f6db4d513619b8e692
Farid Rener <proteusvacuum@gmail.com>
2017-10-03 11:22:13 -0400

    Update merm alarm enabled property

--- custom/enikshay/const.py
+++ custom/enikshay/const.py
@@ -15,7 +15,7 @@ NINETYNINEDOTS_NUMBERS = [
 ]
 
 MERM_ID = 'merm_id'
-MERM_DAILY_REMINDER_STATUS = 'merm_daily_reminder_status'
+MERM_DAILY_REMINDER_STATUS = 'merm_alarm_enabled'
 MERM_DAILY_REMINDER_TIME = "merm_daily_reminder_time"
 MERM_REFILL_REMINDER_STATUS = "merm_refill_reminder_status"
 MERM_REFILL_REMINDER_DATE = "merm_refill_reminder_date"
