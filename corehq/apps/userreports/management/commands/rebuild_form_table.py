fda151844217030271af8d3fefa4cbc4f431476f
sheelio <sshah@dimagi.com>
2017-10-05 17:29:24 -0400

    Fix bugs

--- corehq/apps/userreports/management/commands/rebuild_form_table.py
+++ corehq/apps/userreports/management/commands/rebuild_form_table.py
@@ -29,5 +29,22 @@ class Command(BaseCommand):
         if xmlns_list:
             query = query.OR(*[form_es.xmlns(x) for x in xmlns_list])
 
-        form_ids = query.get_ids()
-        rebuild_indicators_in_place(indicator_config_id, None, form_ids)
+        form_ids = IterableESID(query)
+        rebuild_indicators_in_place.delay(indicator_config_id, None, form_ids)
+
+
+class IterableESID():
+    def __init__(self, query):
+        self.query = query
+        self.query_result = None
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if not self.query_result:
+            self.query_result = self.query.get_ids()
+        id = self.query_result[self.index]
+        self.index += 1
+        return id
