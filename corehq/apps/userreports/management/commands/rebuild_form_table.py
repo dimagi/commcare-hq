c834226da583aa23532a555917905bfd2e179bcc
sheelio <sshah@dimagi.com>
2017-10-05 16:26:37 -0400

    Option to filter on xmlns

--- corehq/apps/userreports/management/commands/rebuild_form_table.py
+++ corehq/apps/userreports/management/commands/rebuild_form_table.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 from datetime import datetime
+import argparse
 
 from django.core.management.base import BaseCommand
-
 
 from corehq.apps.es import forms as form_es
 from corehq.apps.userreports.tasks import rebuild_indicators_in_place
@@ -12,17 +12,21 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('domain')
+        parser.add_argument('indicator_config_id')
         parser.add_argument('start_date', help="YYYY-MM-DD")
         parser.add_argument('end_date', help="YYYY-MM-DD")
-        parser.add_argument('indicator_config_id')
+        parser.add_argument('xmlns_list', nargs=argparse.REMAINDER)
 
-    def handle(self, domain, start_date, end_date, indicator_config_id, **options):
+    def handle(self, domain, indicator_config_id, start_date, end_date, xmlns_list, **options):
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
         end_date = datetime.strptime(end_date, '%Y-%m-%d')
 
         time_filter = form_es.submitted
-        form_ids = (form_es.FormES()
-                    .domain(domain)
-                    .filter(time_filter(gte=start_date, lte=end_date))
-                    .get_ids())
+        query = (form_es.FormES()
+                 .domain(domain)
+                 .filter(time_filter(gte=start_date, lte=end_date)))
+        if xmlns_list:
+            query = query.OR(*[form_es.xmlns(x) for x in xmlns_list])
+
+        form_ids = query.get_ids()
         rebuild_indicators_in_place(indicator_config_id, None, form_ids)
