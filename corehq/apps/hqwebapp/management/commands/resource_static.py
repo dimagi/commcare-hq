fcb15fb818454a2a1b9d51aa33d040a5a6ec549e
Jennifer Schweers <orange.jenny@gmail.com>
2017-10-05 22:08:25 -0400

    Write out resource_versions at the end of builde_requirejs

--- corehq/apps/hqwebapp/management/commands/resource_static.py
+++ corehq/apps/hqwebapp/management/commands/resource_static.py
@@ -35,6 +35,12 @@ class Command(BaseCommand):
         with open(os.path.join(self.root_dir, 'resource_versions.py'), 'w') as fout:
             fout.write("resource_versions = %s" % json.dumps(resources, indent=2))
 
+    def overwrite_resources(self, resources, sha=None):
+        if not sha:
+            sha = self.current_sha()
+        rcache.set(RESOURCE_PREFIX % sha, resources, 86400)
+        self.output_resources(resources)
+
     def handle(self, **options):
         prefix = os.getcwd()
 
@@ -60,8 +66,7 @@ class Command(BaseCommand):
                     url = os.path.join(storage.prefix, path)
                 filename = os.path.join(storage.location, path)
                 resources[url] = self.get_hash(filename)
-        rcache.set(RESOURCE_PREFIX % current_sha, resources, 86400)
-        self.output_resources(resources)
+        self.overwrite_resources(resources, sha=current_sha)
 
     def get_hash(self, filename):
         with open(filename) as f:
