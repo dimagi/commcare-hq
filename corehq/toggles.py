079c551b42bc350807559ffa125d5bc2524b7082
Jennifer Schweers <orange.jenny@gmail.com>
2017-10-09 10:40:10 -0400

    Fix toggle error

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -323,7 +323,7 @@ def toggle_values_by_name(username=None, domain=None):
 
     all toggles (including those not enabled) are included
     """
-    return {toggle_name: (toggle.enabled(username) or toggle.enabled(domain))
+    return {toggle_name: (toggle.enabled(username, NAMESPACE_USER) or toggle.enabled(domain, NAMESPACE_DOMAIN))
             for toggle_name, toggle in all_toggles_by_name().items()}
 
 
