293d661ae98a4cc492c855dae51e7a062f7b6bec
Manish Kangia <mkangia@dimagi.com>
2017-10-17 01:17:04 +0530

    make further progress on investigation reconciliation [skip ci]

--- custom/enikshay/management/commands/investigations_reconciliation.py
+++ custom/enikshay/management/commands/investigations_reconciliation.py
@@ -15,7 +15,7 @@ from corehq.apps.hqcase.utils import bulk_update_cases
 from custom.enikshay.const import ENROLLED_IN_PRIVATE
 
 DOMAIN = "enikshay"
-EPISODE_TYPE = "confirmed_drtb"
+CONFIRMED_DRTB_EPISODE_TYPE = "confirmed_drtb"
 DATE_MODIFIED_FIELD = "modified_on"
 PROPERTIES_TO_BE_COALESCED = [
     "lft_results",
@@ -45,31 +45,36 @@ class Command(BaseCommand):
         self.result_file = self.setup_result_file()
         self.case_accessor = CaseAccessors(DOMAIN)
         self.investigation_interval_values = []
-        # iterate all person cases
-        for person_case_id in self._get_open_person_case_ids_to_process():
-            person_case = self.case_accessor.get_case(person_case_id)
-            if self.public_app_case(person_case):
-                open_confirmed_drtb_episode_cases = get_open_confirmed_drtb_episode_cases(person_case_id)
-                for episode_case in open_confirmed_drtb_episode_cases:
-                    investigations_to_be_reconciled = self.episode_case_needs_reconciliation(episode_case)
-                    if investigations_to_be_reconciled:
-                        pass
+        with open(self.result_file, 'a') as csvfile:
+            self.writer = csv.DictWriter(csvfile, fieldnames=self.get_result_file_headers())
+            # iterate all person cases
+            for person_case_id in self._get_open_person_case_ids_to_process():
+                person_case = self.case_accessor.get_case(person_case_id)
+                # check if person is a public app case
+                if self.public_app_case(person_case):
+                    # get all confirmed drtb cases to check for cases under them
+                    open_confirmed_drtb_episode_cases = get_open_confirmed_drtb_episode_cases(person_case_id)
+                    for episode_case in open_confirmed_drtb_episode_cases:
+                        episode_case_id = episode_case.case_id
+                        # get any investigations that need to be reconciled under this episode
+                        investigations_to_be_reconciled = self.episode_case_needs_reconciliation(episode_case)
+                        if investigations_to_be_reconciled:
+                            for interval_type, investigation_cases in investigations_to_be_reconciled.items():
+                                self.reconcile_investigation_cases(episode_case_id, investigation_cases, csvfile)
 
     @staticmethod
     def get_result_file_headers():
         headers = [
-            "episode_case_id",
-            "investigation_interval",
+            "episode_id",
             "investigation_case_id",
             "modified_on",
-            "updates",
-            "update/close"
+            "update_or_close"
         ]
         headers += PROPERTIES_TO_BE_COALESCED
         return headers
 
     def setup_result_file(self):
-        file_name = "duplicate_occurrence_and_episode_reconciliation_report_{timestamp}.csv".format(
+        file_name = "duplicate_investigations_reconciliation_report_{timestamp}.csv".format(
             timestamp=datetime.now().strftime("%Y-%m-%d-%H-%M-%S"),
         )
         with open(file_name, 'w') as csvfile:
@@ -78,49 +83,90 @@ class Command(BaseCommand):
         return file_name
 
     def writerow(self, row):
-        with open(self.result_file, 'a') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=self.get_result_file_headers())
-            writer.writerow(row)
+        self.writer.writerow(row)
 
-    def reconcile_investigation_cases(self, investigation_cases):
+    def reconcile_investigation_cases(self, episode_case_id, investigation_cases, csvfile):
+        # fetch latest investigation case which would retain final values
         latest_investigation_case = sorted(investigation_cases, key=lambda x: x.modified_on)[0]
-        for case_property in PROPERTIES_TO_BE_COALESCED:
-            all_property_values = []
-            for investigation_case in investigation_cases:
-                all_property_values.append(investigation_case.get_case_property(case_property))
-            coalesced_value = self.get_coalesced_value_for_case_property(
-                case_property, all_property_values
-            )
+        retain_case_id = latest_investigation_case.case_id
 
-    def close_or_update_investigation_cases(self, all_cases, retain_case_id, episode_case_id,
-                                            investigation_interval, updates):
-        all_case_ids = [investigation_case.case_id for investigation_case in all_cases]
-        # ToDo: refetch investigation_cases in case the len of set is different from list
-        # remove duplicates in case ids to remove so that we don't retain and close
-        # the same case by mistake
-        all_case_ids = set(all_case_ids)
-        case_ids_to_close = all_case_ids.copy()
-        case_ids_to_close.remove(retain_case_id)
-        for investigation_case in all_cases:
-            self.writerow({
-                "episode_case_id": episode_case_id,
-                "investigation_interval": investigation_interval,
-                "investigation_case_id": investigation_case.case_id,
-                "modified_on": investigation_case.get_case_property(DATE_MODIFIED_FIELD),
-                "updates": case_updates,
-                "update/close": ('update' if investigation_case.case_id == retain_case_id
-                                 else 'closed')
-            })
-        if not self.dry_run:
-            updates = [(case_id, {'close_reason': "duplicate_reconciliation"}, True)
-                       for case_id in case_ids_to_close]
-            bulk_update_cases(DOMAIN, updates, self.__module__)
+        properties_by_investigation = defaultdict(dict)
+        values_for_property = defaultdict(list)
+        investigation_case_modified_on = {}
+        coalesced_values = {}
+
+        # iterate over cases to prepare
+        # 1. modified on
+        # 2. values for all case properties
+        # 3. values for all case properties for each investigation case
+        for investigation_case in investigation_cases:
+            investigation_case_modified_on[investigation_case.case_id] = investigation_case.modified_on
+            for case_property in PROPERTIES_TO_BE_COALESCED:
+                prop_value = investigation_case.get_case_property(case_property)
+                # add value for this investigation under its mapping for writing later
+                properties_by_investigation[investigation_case.case_id][case_property] = prop_value
+                # add value for this investigation in the collection for values for investigations
+                values_for_property[case_property].append(prop_value)
+
+        # iterate over all values for each case property to prepare coalesced values
+        for case_property, values in values_for_property.items():
+            coalesced_value = self.get_coalesced_value_for_case_property(
+                case_property, values
+            )
+            coalesced_values[case_property] = coalesced_value
+
+        # write values for each investigation
+        for investigation_case_id, investigation_case_values in properties_by_investigation.items():
+            other_investigation_case_values = investigation_case_values.copy()
+            other_investigation_case_values['investigation_case_id'] = investigation_case_id
+            other_investigation_case_values['modified_on'] = investigation_case_modified_on[investigation_case_id]
+            other_investigation_case_values['episode_id'] = episode_case_id
+            other_investigation_case_values['update_or_close'] = (
+                'to_be_updated'
+                if investigation_case_id == retain_case_id
+                else 'to_be_closed'
+            )
+            self.writerow(other_investigation_case_values)
+
+        # write final coalesced values
+        coalesced_investigation_case = coalesced_values.copy()
+        coalesced_investigation_case['investigation_case_id'] = retain_case_id
+        coalesced_investigation_case['modified_on'] = latest_investigation_case.modified_on
+        coalesced_investigation_case['episode_id'] = episode_case_id
+        coalesced_investigation_case['update_or_close'] = 'update'
+        self.writerow(coalesced_investigation_case)
+
+    # def close_or_update_investigation_cases(self, all_cases, retain_case_id, episode_case_id,
+    #                                         investigation_interval, updates):
+    #     all_case_ids = [investigation_case.case_id for investigation_case in all_cases]
+    #     # ToDo: refetch investigation_cases in case the len of set is different from list
+    #     # remove duplicates in case ids to remove so that we don't retain and close
+    #     # the same case by mistake
+    #     all_case_ids = set(all_case_ids)
+    #     case_ids_to_close = all_case_ids.copy()
+    #     case_ids_to_close.remove(retain_case_id)
+    #     for investigation_case in all_cases:
+    #         self.writerow({
+    #             "episode_case_id": episode_case_id,
+    #             "investigation_interval": investigation_interval,
+    #             "investigation_case_id": investigation_case.case_id,
+    #             "modified_on": investigation_case.get_case_property(DATE_MODIFIED_FIELD),
+    #             "updates": updates,
+    #             "update/close": ('update' if investigation_case.case_id == retain_case_id
+    #                              else 'closed')
+    #         })
+    #     if not self.dry_run:
+    #         updates = [(case_id, {'close_reason': "duplicate_reconciliation"}, True)
+    #                    for case_id in case_ids_to_close]
+    #         bulk_update_cases(DOMAIN, updates, self.__module__)
 
     def get_coalesced_value_for_case_property(self, case_property, all_values):
         #ToDo: write logic for this
         return all_values[0]
 
     def episode_case_needs_reconciliation(self, episode_case):
+        # iterate investigation cases to check if any investigation type has more than 1
+        # investigation case added for it
         investigation_cases = get_investigation_cases_from_episode(episode_case)
         investigation_cases_by_interval = defaultdict(list)
         for investigation_case in investigation_cases:
@@ -130,10 +176,12 @@ class Command(BaseCommand):
             investigation_cases_by_interval[investigation_case_interval].append(
                 investigation_case
             )
+        investigation_cases_to_reconcile = {}
         for interval_type, investigation_cases in investigation_cases_by_interval.items():
             if len(investigation_cases) > 1:
-                return investigation_cases_by_interval
-        return False
+                investigation_cases_to_reconcile[interval_type] = investigation_cases
+
+        return investigation_cases_to_reconcile
 
     @staticmethod
     def public_app_case(person_case):
@@ -178,7 +226,7 @@ def get_open_confirmed_drtb_episode_cases(person_case_id):
             case for case in all_cases
             if not case.closed
             and case.type == CASE_TYPE_EPISODE
-            and case.get_case_property("episode_type") == EPISODE_TYPE
+            and case.get_case_property("episode_type") == CONFIRMED_DRTB_EPISODE_TYPE
         ]
     return open_confirmed_drtb_episode_cases
 
