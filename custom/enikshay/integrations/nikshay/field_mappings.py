09e34d4c0a5c2546f721c60ab06f494f9bfe19cb
Manish Kangia <mkangia@dimagi.com>
2017-10-18 14:57:44 +0530

    Add mapping for patient_family_member and desc for all

--- custom/enikshay/integrations/nikshay/field_mappings.py
+++ custom/enikshay/integrations/nikshay/field_mappings.py
@@ -21,14 +21,15 @@ patient_type_choice = {
 }
 
 treatment_support_designation = {
-    'health_worker': '1',
-    'tbhv': '2',
-    'asha': '3',
-    'asha_or_other_phi_hw': '3',
-    'aww': '4',
-    'ngo_volunteer': '5',
-    'private_medical_pracitioner': '6',
-    'other_community_volunteer': '7',
+    'health_worker': '1',  # Health Worker
+    'tbhv': '2',  # TBHV
+    'asha': '3',  # ASHA
+    'asha_or_other_phi_hw': '3',  # ASHA
+    'aww': '4',  # AWW
+    'ngo_volunteer': '5',  # NGO VOLUNTEER
+    'private_medical_pracitioner': '6',  # PRIVATE PRACTICENER
+    'other_community_volunteer': '7',  # OTHER COMMUNITY VOLUNTEERS
+    'patient_family_member': '7',  # OTHER COMMUNITY VOLUNTEERS
 }
 
 occupation = {
