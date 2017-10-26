815a163f63d113004914b673aa5bb5f463ff7487
Daniel Miller <dmiller@dimagi.com>
2017-10-26 08:10:52 -0400

    Use regex

--- corehq/ex-submodules/casexml/apps/phone/utils.py
+++ corehq/ex-submodules/casexml/apps/phone/utils.py
@@ -1,3 +1,5 @@
+import re
+
 from casexml.apps.phone.exceptions import CouldNotPruneSyncLogs
 from couchdbkit.exceptions import BulkSaveError
 
@@ -18,6 +20,7 @@ def delete_sync_logs(before_date, limit=1000, num_tries=10):
 
 
 ITEMS_COMMENT_PREFIX = b'<!--items='
+ITESM_COMMENT_REGEX = re.compile(br'(<!--items=(\d+)-->)')
 
 
 def get_cached_items_with_count(cached_bytes):
@@ -34,16 +37,10 @@ def get_cached_items_with_count(cached_bytes):
 
     :returns: Two-tuple: (xml_elements_bytes, num_items)
     """
-    if cached_bytes.startswith(ITEMS_COMMENT_PREFIX):
-        DIGITS = b'0123456789'
-        digits = []
-        i = len(ITEMS_COMMENT_PREFIX)
-        total_bytes = len(cached_bytes)
-        while i < total_bytes and cached_bytes[i] in DIGITS:
-            digits.append(cached_bytes[i])
-            i += 1
-        if digits and cached_bytes[i:i + 3] == b'-->':
-            num_items = int(b"".join(digits))
-            return cached_bytes[i + 3:], num_items
+    match = ITESM_COMMENT_REGEX.match(cached_bytes)
+    if match:
+        offset = len(match.group(1))
+        num_items = int(match.group(2))
+        return cached_bytes[offset:], num_items
     # TODO parse and count elements in cached bytes?
     return cached_bytes, 1
