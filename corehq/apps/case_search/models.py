160efc0e637b702648322c492a7db85c4757a586
Simon Kelly <skelly@dimagi.com>
2017-10-26 11:24:00 +0200

    escape slashes for json encoding

--- corehq/apps/case_search/models.py
+++ corehq/apps/case_search/models.py
@@ -164,6 +164,8 @@ def replace_custom_query_variables(query_addition, criteria, ignore_patterns):
         to_add = re.escape(value)
         query_addition = re.sub(key, to_add, query_addition)
 
+    query_addition = re.sub(re.escape('\\'), re.escape('\\\\'), query_addition)
+
     return json.loads(query_addition)
 
 
