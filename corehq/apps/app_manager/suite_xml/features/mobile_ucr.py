dimagi/commcare-hq

commit 4f2281dec0c71798b33e07675e7446b600fae65f

DRYer

corehq/apps/app_manager/suite_xml/features/mobile_ucr.py
@@ -430,17 +430,15 @@ class MobileSelectFilterHelpers(object):
     @staticmethod
     def get_data_filter_xpath(config, domain, new_mobile_ucr_restore):
         if new_mobile_ucr_restore:
-            return ''.join([
-                "[{column_id}=instance('commcaresession')/session/data/{datum_id}]".format(
-                    column_id=config.report(domain).get_ui_filter(slug).field,
-                    datum_id=MobileSelectFilterHelpers.get_datum_id(config, slug))
-                for slug, f in MobileSelectFilterHelpers.get_filters(config, domain)])
+            base_xpath = "[{column_id}=instance('commcaresession')/session/data/{datum_id}]"
         else:
-            return ''.join([
-                "[column[@id='{column_id}']=instance('commcaresession')/session/data/{datum_id}]".format(
-                    column_id=config.report(domain).get_ui_filter(slug).field,
-                    datum_id=MobileSelectFilterHelpers.get_datum_id(config, slug))
-                for slug, f in MobileSelectFilterHelpers.get_filters(config, domain)])
+            base_xpath = "[column[@id='{column_id}'=instance('commcaresession')/session/data/{datum_id}]"
+
+        return ''.join([
+            base_xpath.format(
+                column_id=config.report(domain).get_ui_filter(slug).field,
+                datum_id=MobileSelectFilterHelpers.get_datum_id(config, slug))
+            for slug, f in MobileSelectFilterHelpers.get_filters(config, domain)])
 
 
 def is_valid_mobile_select_filter_type(ui_filter):

commit 2edaca593858a988fc81400499df18fbea16499c

Fixed issue introduced in 4f2281dec.

corehq/apps/app_manager/suite_xml/features/mobile_ucr.py
@@ -432,7 +432,7 @@ class MobileSelectFilterHelpers(object):
         if new_mobile_ucr_restore:
             base_xpath = "[{column_id}=instance('commcaresession')/session/data/{datum_id}]"
         else:
-            base_xpath = "[column[@id='{column_id}'=instance('commcaresession')/session/data/{datum_id}]"
+            base_xpath = "[column[@id='{column_id}']=instance('commcaresession')/session/data/{datum_id}]"
 
         return ''.join([
             base_xpath.format(