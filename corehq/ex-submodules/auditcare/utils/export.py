6095ada038d8edba0aaff50d46b55a2308176d2d
Simon Kelly <skelly@dimagi.com>
2017-10-27 11:54:49 +0200

    preserve order

--- corehq/ex-submodules/auditcare/utils/export.py
+++ corehq/ex-submodules/auditcare/utils/export.py
@@ -1,3 +1,5 @@
+from django.utils.datastructures import OrderedSet
+
 from auditcare.models import NavigationEventAudit
 from dimagi.utils.couch.database import iter_docs
 
@@ -5,20 +7,24 @@ from dimagi.utils.couch.database import iter_docs
 def navigation_event_ids_by_user(user):
     database = NavigationEventAudit.get_db()
 
-    return {row['id'] for row in database.view(
+    ids = OrderedSet()
+    results = database.view(
         'auditcare/urlpath_by_user_date',
         startkey=[user],
         endkey=[user, {}],
         reduce=False,
         include_docs=False,
-    )}
+    )
+    for row in results:
+        ids.add(row['id'])
+    return ids
 
 
 def write_log_events(writer, user, domain=None, override_user=None):
     for event in iter_docs(NavigationEventAudit.get_db(), navigation_event_ids_by_user(user)):
         doc = NavigationEventAudit.wrap(event)
         if not domain or domain == doc.domain:
-            write_log_events(writer, doc, override_user)
+            write_log_event(writer, doc, override_user)
 
 
 def write_log_event(writer, event, override_user=None):
