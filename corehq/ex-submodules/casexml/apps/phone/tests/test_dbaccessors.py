6cef0f545772c6e2d4b16c37c96fed152665f08f
Simon Kelly <skelly@dimagi.com>
2017-11-01 12:26:29 +0200

    remove outdated test

--- corehq/ex-submodules/casexml/apps/phone/tests/test_dbaccessors.py
+++ corehq/ex-submodules/casexml/apps/phone/tests/test_dbaccessors.py
@@ -51,25 +51,6 @@ class DBAccessorsTest(TestCase, DocTestMixin):
         self.assert_docs_equal(
             get_last_synclog_for_user(self.user_id), self.sync_logs[0])
 
-    def test_get_and_save_legacy_synclog_with_attachm(self):
-        legacy_sync_log = self.legacy_sync_logs[0]
-        attachment_name = 'test_attach'
-        get_db(None).put_attachment(legacy_sync_log._doc, 'test', attachment_name, 'text/plain')
-
-        sync_log = get_properly_wrapped_sync_log(legacy_sync_log._id)
-        self.assertIn(attachment_name, sync_log._attachments)
-
-        # this used to fail for docs with attachments
-        sync_log.save()
-
-        # cleanup
-        def del_attachment():
-            get_db(None).delete_attachment(legacy_sync_log._doc, attachment_name)
-            del legacy_sync_log._attachments
-
-        self.addCleanup(del_attachment)
-        self.addCleanup(sync_log.delete)
-
     def test_get_synclog_ids_before_date(self):
         doc_ids = get_synclog_ids_before_date(SyncLog.get_db(), datetime.datetime.utcnow())
         self.assertEqual(doc_ids, [
