674307b7fccb173b021a372965fbc3947671bfab
Biyeun Buczyk <biyeun@dimagi.com>
2017-11-02 15:41:42 -0400

    py3 fix

--- corehq/apps/userreports/reports/builder/forms.py
+++ corehq/apps/userreports/reports/builder/forms.py
@@ -1337,8 +1337,8 @@ class ConfigureListReportForm(ConfigureNewReportBase):
             data_source_field=prop.to_report_column_option().get_indicator(COUNT_PER_CHOICE)['column_id'],
             calculation=COUNT_PER_CHOICE
         ))
-        questions = filter(lambda x: x.get_type() == PROPERTY_TYPE_QUESTION,
-            self.data_source_properties.values())
+        questions = [prop for prop in self.data_source_properties.values()
+                     if prop.get_type() == PROPERTY_TYPE_QUESTION]
         if len(questions) > 9:
             questions = questions[:9]
         for q in questions:
