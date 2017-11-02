be10691d6d33788417e1f78da942543826225435
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-02 14:07:00 -0400

    lmap

--- corehq/apps/userreports/indicators/__init__.py
+++ corehq/apps/userreports/indicators/__init__.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 from collections import defaultdict
+
+from future.utils import lmap
+
 from corehq.apps.userreports.util import truncate_value
 from corehq.form_processor.interfaces.dbaccessors import LedgerAccessors
 from fluff import TYPE_INTEGER
@@ -129,7 +132,7 @@ class LedgerBalancesIndicator(ConfigurableIndicator):
         return ret
 
     def get_columns(self):
-        return map(self._make_column, self.product_codes)
+        return lmap(self._make_column, self.product_codes)
 
     def get_values(self, item, context=None):
         case_id = self.case_id_expression(item)
