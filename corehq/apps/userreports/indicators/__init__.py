3c386d3761418996d06a0940c4cd247d31aa78a6
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-03 10:50:31 -0400

    replace lmap with list comprehension

--- corehq/apps/userreports/indicators/__init__.py
+++ corehq/apps/userreports/indicators/__init__.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 from collections import defaultdict
 
-from future.utils import lmap
-
 from corehq.apps.userreports.util import truncate_value
 from corehq.form_processor.interfaces.dbaccessors import LedgerAccessors
 from fluff import TYPE_INTEGER
@@ -132,7 +130,7 @@ class LedgerBalancesIndicator(ConfigurableIndicator):
         return ret
 
     def get_columns(self):
-        return lmap(self._make_column, self.product_codes)
+        return [self._make_column(product_code) for product_code in self.product_codes]
 
     def get_values(self, item, context=None):
         case_id = self.case_id_expression(item)
