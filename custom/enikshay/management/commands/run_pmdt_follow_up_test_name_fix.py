df45cb269f4d8dc9d3fb3bfdd1d35ac3cffa8668
sheelio <sshah@dimagi.com>
2017-11-06 11:57:14 -0500

    Minor tweaks

--- custom/enikshay/management/commands/run_pmdt_follow_up_test_name_fix.py
+++ custom/enikshay/management/commands/run_pmdt_follow_up_test_name_fix.py
@@ -5,14 +5,19 @@ from custom.enikshay.two_b_datamigration.management.commands.import_drtb_cases i
 
 TESTING_FACILITY_ID = 'testing_facility_id'
 TESTING_FACILITY_NAME = 'testing_facility_name'
+RFT_GENERAL = 'rft_general'
+MIGRATION_TYPE = 'migration_type'
 DATAMIGRATION_CASE_PROPERTY = 'datamigration_pmdt_follow_up_test_name'
 
 
 class Command(BaseEnikshayCaseMigration):
     case_type = 'test'
+    # Not all these case properties are updated but want their output for debugging
     case_properties_to_update = [
         TESTING_FACILITY_ID,
         TESTING_FACILITY_NAME,
+        RFT_GENERAL,
+        MIGRATION_TYPE,
     ]
     datamigration_case_property = DATAMIGRATION_CASE_PROPERTY
     include_public_cases = True
@@ -22,8 +27,8 @@ class Command(BaseEnikshayCaseMigration):
     def get_case_property_updates(test, domain):
         if (
             test.get_case_property(DATAMIGRATION_CASE_PROPERTY) != 'yes'
-            and test.get_case_property('migration_type') == 'pmdt_excel'
-            and test.get_case_property('rft_general') == 'follow_up_drtb'
+            and test.get_case_property(MIGRATION_TYPE) == 'pmdt_excel'
+            and test.get_case_property(RFT_GENERAL) == 'follow_up_drtb'
         ):
             try:
                 name, id = match_facility(domain, test.get_case_property(TESTING_FACILITY_NAME))
