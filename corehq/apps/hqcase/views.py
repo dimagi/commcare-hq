5c600f8f723d5a70018d30a0907ea23684261f36
Farid Rener <proteusvacuum@gmail.com>
2017-11-07 20:10:47 -0500

    explode_cases_view as CBV

--- corehq/apps/hqcase/views.py
+++ corehq/apps/hqcase/views.py
@@ -1,16 +1,35 @@
 from __future__ import absolute_import
-from corehq.apps.users.models import CommCareUser
+
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 
 from corehq.apps.domain.decorators import require_superuser_or_developer
+from corehq.apps.domain.views import BaseProjectSettingsView
 from corehq.apps.hqcase.tasks import explode_case_task
+from corehq.apps.users.models import CommCareUser
 from soil import DownloadBase
 
 
-@require_superuser_or_developer
-def explode_cases(request, domain, template="hqcase/explode_cases.html"):
-    if request.method == 'POST':
+class ExplodeCasesView(BaseProjectSettingsView, TemplateView):
+    url_name = "explode_cases"
+    template_name = "hqcase/explode_cases.html"
+    page_title = "Explode Cases"
+
+    @method_decorator(require_superuser_or_developer)
+    def dispatch(self, *args, **kwargs):
+        return super(ExplodeCasesView, self).dispatch(*args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(ExplodeCasesView, self).get_context_data(**kwargs)
+        context.update({
+            'domain': self.domain,
+            'users': CommCareUser.by_domain(self.domain)
+        })
+        return context
+
+    def post(self, request, domain):
         user_id = request.POST['user_id']
         factor = request.POST.get('factor', '2')
         try:
@@ -19,12 +38,7 @@ def explode_cases(request, domain, template="hqcase/explode_cases.html"):
             messages.error(request, 'factor must be an int; was: %s' % factor)
         else:
             download = DownloadBase()
-            res = explode_case_task.delay(user_id, domain, factor)
+            res = explode_case_task.delay(user_id, self.domain, factor)
             download.set_task(res)
 
-            return redirect('hq_soil_download', domain, download.download_id)
-
-    return render(request, template, {
-        'domain': domain,
-        'users': CommCareUser.by_domain(domain),
-    })
+            return redirect('hq_soil_download', self.domain, download.download_id)
