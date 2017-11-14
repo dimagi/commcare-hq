2fcfd940556b1376af751d78c68ff22eb6f8c77b
Nick Pellegrino <npellegrino@dimagi.com>
2017-11-13 20:56:37 -0600

    python-modernize . -w -f lib2to3.fixes.fix_funcattrs

--- corehq/ex-submodules/auditcare/utils/resolver.py
+++ corehq/ex-submodules/auditcare/utils/resolver.py
@@ -13,7 +13,7 @@ def _pattern_resolve_to_name(self, path):
         elif hasattr(self, '_callback_str'):
             name = self._callback_str
         else:
-            name = "%s.%s" % (self.callback.__module__, self.callback.func_name)
+            name = "%s.%s" % (self.callback.__module__, self.callback.__name__)
         return name
 
 def _resolver_resolve_to_name(self, path):
