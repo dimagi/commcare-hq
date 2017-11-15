d1a522c137bb1029ea4eb904b88da2ea49c5c455
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-11-15 14:05:18 -0500

    Lint

--- corehq/messaging/smsbackends/ivory_coast_mtn/tests/test_response.py
+++ corehq/messaging/smsbackends/ivory_coast_mtn/tests/test_response.py
@@ -1,18 +1,21 @@
 from __future__ import absolute_import
-import uuid
 from corehq.apps.sms.models import QueuedSMS
 from corehq.messaging.smsbackends.ivory_coast_mtn.exceptions import IvoryCoastMTNError
 from corehq.messaging.smsbackends.ivory_coast_mtn.models import IvoryCoastMTNBackend
 from django.test import TestCase
 
 SUCCESS_RESPONSE = u"""<?xml version="1.0" encoding="utf-8"?>
-<SendResult xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://pmmsoapmessenger.com/">
+<SendResult xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://pmmsoapmessenger.com/">
   <Result>OK</Result>
   <TransactionID>164b210b-748c-41d2-97f4-88d166415423</TransactionID>
 </SendResult>"""
 
 ERROR_RESPONSE = u"""<?xml version="1.0" encoding="utf-8"?>
-<SendResult xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="http://pmmsoapmessenger.com/">
+<SendResult xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xmlns="http://pmmsoapmessenger.com/">
   <Result>FAILED</Result>
   <TransactionID>1756d6f2-e15f-4397-9670-47b98e92ba62</TransactionID>
 </SendResult>"""
