de52cf41011d3f26af5f3d9b98137151a23430fb
Łukasz Wyszomirski <lwyszomirski@gmail.com>
2017-11-16 10:54:14 +0100

    extend condition

--- custom/icds_reports/static/js/directives/awc-reports/awc-reports.directive.js
+++ custom/icds_reports/static/js/directives/awc-reports/awc-reports.directive.js
@@ -2378,7 +2378,7 @@ function AwcReportsController($scope, $http, $location, $routeParams, $log, DTOp
                 var hasClass = [].some.call(mutation.addedNodes, function(el) {
                     return el.classList.contains('fixedHeader-floating');
                 });
-                if (hasClass) {
+                if (hasClass && !vm.showBeneficiary) {
                     var width = "width: " + mutation.addedNodes[0].style.width + ' !important';
                     mutation.addedNodes[0].style.cssText = (mutation.addedNodes[0].style.cssText + width);
                 }
