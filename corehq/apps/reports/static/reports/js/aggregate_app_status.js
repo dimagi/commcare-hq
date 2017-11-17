df7c2f52b85ae07c5045adf3d875b6b419ea2adb
Cory Zue <czue@dimagi.com>
2017-11-17 16:07:19 +0200

    use button selectors instead of multiple charts

--- corehq/apps/reports/static/reports/js/aggregate_app_status.js
+++ corehq/apps/reports/static/reports/js/aggregate_app_status.js
@@ -3,7 +3,7 @@ hqDefine("reports/js/aggregate_app_status", function() {
     function setupCharts(data, div) {
         nv.addGraph(function() {
         var chart = nv.models.multiBarChart()
-          .transitionDuration(350)
+          .transitionDuration(100)
           .showControls(false)
           .reduceXTicks(true)
           .rotateLabels(0)
@@ -25,9 +25,12 @@ hqDefine("reports/js/aggregate_app_status", function() {
     $(document).ajaxSuccess(function(event, xhr, settings) {
         if (settings.url.match(/reports\/async\/aggregate_user_status/)) {
             setupCharts($("#submission-data").data("value"), 'submission_dates');
-            setupCharts($("#submission-totals").data("value"), 'submission_totals');
             setupCharts($("#sync-data").data("value"), 'sync_dates');
-            setupCharts($("#sync-totals").data("value"), 'sync_totals');
+            $('.chart-toggle').click(function () {
+                $(this).parent().children().not(this).removeClass('btn-primary');
+                $(this).addClass('btn-primary');
+                setupCharts($("#" + $(this).data('chart-data')).data("value"), $(this).data('chart-div'));
+            })
         }
     });
 });
