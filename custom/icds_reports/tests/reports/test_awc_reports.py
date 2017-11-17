c470ae362b0b70c5e6d8c7a268cb97d02085022e
Łukasz Wyszomirski <lwyszomirski@gmail.com>
2017-11-17 16:04:34 +0100

    fix tests

--- custom/icds_reports/tests/reports/test_awc_reports.py
+++ custom/icds_reports/tests/reports/test_awc_reports.py
@@ -22,7 +22,7 @@ class TestAWCReport(TestCase):
 
     def test_beneficiary_details_recorded_weight_is_not_none(self):
         data = get_beneficiary_details(case_id='8e226cc6-740f-4146-b017-69d9f6e9651b')
-        self.assertEqual(data['age_in_months'], 53)
+        self.assertEqual(data['age_in_months'], 54)
         self.assertEqual(data['sex'], 'M')
         self.assertEqual(data['person_name'], 'Name 3141')
         self.assertEqual(data['mother_name'], 'शियामु बाई')
@@ -32,7 +32,7 @@ class TestAWCReport(TestCase):
 
     def test_beneficiary_details_have_age_in_month_not_have_recorded_height(self):
         data = get_beneficiary_details(case_id='411c4234-8475-415a-9c28-911b85868aa5')
-        self.assertEqual(data['age_in_months'], 36)
+        self.assertEqual(data['age_in_months'], 37)
         self.assertEqual(data['sex'], 'F')
         self.assertEqual(data['person_name'], 'Name 3483')
         self.assertEqual(data['mother_name'], u'रींकीकुँवर')
