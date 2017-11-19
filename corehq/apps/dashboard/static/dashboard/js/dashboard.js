c0d9105300d00d579f353c54212b71b179c528c5
Jennifer Schweers <orange.jenny@gmail.com>
2017-11-19 13:57:04 -0500

    Handle popovers in knockout

--- corehq/apps/dashboard/static/dashboard/js/dashboard.js
+++ corehq/apps/dashboard/static/dashboard/js/dashboard.js
@@ -104,18 +104,5 @@ hqDefine("dashboard/js/dashboard", function() {
         $("#dashboard-tiles").koApplyBindings(new DashboardModel({
             tiles: hqImport("hqwebapp/js/initial_page_data").get("dashboard_tiles"),
         }));
-
-        // Set up popovers
-        // TODO: need to initialize these every time there's a new page (report titles have popovers)
-        $(".panel-dashboard [data-popover]").each(function() {
-            var $target = $(this),
-                data = $target.data();
-            $target.popover({
-                title: data.popoverTitle,
-                content: data.popover,
-                placement: data.popoverPlacement || "top",
-                trigger: 'hover',
-            });
-        });
     });
 });
