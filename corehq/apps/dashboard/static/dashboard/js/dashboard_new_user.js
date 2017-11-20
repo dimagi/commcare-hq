7d42326142c41380ef1f29257179a5310e1be5e8
Biyeun Buczyk <biyeun@dimagi.com>
2017-11-20 11:20:32 -0500

    retire gaTrackClick

--- corehq/apps/dashboard/static/dashboard/js/dashboard_new_user.js
+++ corehq/apps/dashboard/static/dashboard/js/dashboard_new_user.js
@@ -6,7 +6,7 @@ hqDefine("static/dashboard/js/dashboard_new_user", function() {
         var templates = hqImport('hqwebapp/js/initial_page_data').get('templates');
         _.each(templates, function(template, index) {
             $links.push($('.dashboard-link[data-index="' + index + '"]'));
-            gaTrackLink($links[index], 'Dashboard', 'Welcome Tile', template.action);
+            hqImport('analytics/js/google').track.click($links[index], 'Dashboard', 'Welcome Tile', template.action);
             analytics.trackWorkflowLink($links[index], template.description);
             analytics.trackWorkflowLink($links[index], 'Clicked App Template Tile');
         });
