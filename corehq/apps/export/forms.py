e4b5a25549630b83a4ccd31d0213cac74dd498ac
Manish Kangia <mkangia@dimagi.com>
2017-11-28 18:48:24 +0530

    Avoid adding None returned in case of owner not found

--- corehq/apps/export/forms.py
+++ corehq/apps/export/forms.py
@@ -646,7 +646,11 @@ class DashboardFeedFilterForm(forms.Form):
                 )
 
             emwf_utils_class = CaseListFilterUtils if export_type is CaseExportInstance else EmwfUtils
-            emwf_data = [emwf_utils_class(domain).id_to_choice_tuple(str(x)) for x in selected_items]
+            emwf_data = []
+            for x in selected_items:
+                choice_tuple = emwf_utils_class(domain).id_to_choice_tuple(str(x))
+                if choice_tuple:
+                    emwf_data.append(choice_tuple)
             emwf_data = [{"id": x[0], "text": x[1]} for x in emwf_data]
 
             return {
