0c9b5314aac716f63daa5dd36516531b75595de3
Ethan Soergel <esoergel@gmail.com>
2017-11-28 23:33:12 -0500

    Log updates performed

--- custom/enikshay/management/commands/flag_duplicate_ids.py
+++ custom/enikshay/management/commands/flag_duplicate_ids.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
+import csv
+import datetime
 from django.core.management.base import BaseCommand
 
 from dimagi.utils.chunked import chunked
@@ -32,11 +34,17 @@ class Command(BaseCommand):
         self.case_type = case_type
         commit = options['commit']
 
-        for chunk in chunked(self.get_updates(), 100):
-            if commit:
-                bulk_update_cases(self.domain, chunk, self.__module__)
+        filename = '{}-{}.csv'.format(self.__module__,
+                                      datetime.datetime.now().strftime('%Y-%m-%d_%H.%M.%S'))
+        print("Logging actions to {}".format(filename))
+        with open(filename, 'w') as f:
+            logfile = csv.writer(f)
+            logfile.writerow(['case_id', 'marked_as_duplicate'])
+            for chunk in chunked(self.get_updates(logfile), 100):
+                if commit:
+                    bulk_update_cases(self.domain, chunk, self.__module__)
 
-    def get_updates(self):
+    def get_updates(self, logfile):
         accessor = CaseAccessors(self.domain)
         case_ids = accessor.get_case_ids_in_domain(self.case_type)
         bad_cases = get_cases_with_duplicate_ids(self.domain, self.case_type, case_ids)
@@ -45,6 +53,8 @@ class Command(BaseCommand):
         for case in with_progress_bar(bad_cases):
             if case['case_id'] in self.already_seen:
                 yield (case['case_id'], {'has_duplicate_id': 'yes'}, False)
+                logfile.writerow([case['case_id'], True])
             else:
                 # Don't mark this one
                 self.already_seen.add(case['case_id'])
+                logfile.writerow([case['case_id'], False])
