9b0838806e885420b453f9cb95845c019bce16bc
Ethan Soergel <esoergel@gmail.com>
2017-11-28 21:23:56 -0500

    Add rudimentary command to flag duplicate cases

---
+++ custom/enikshay/management/commands/flag_duplicate_ids.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from django.core.management.base import BaseCommand
+
+from dimagi.utils.chunked import chunked
+
+from corehq.apps.hqcase.utils import bulk_update_cases
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+from corehq.util.log import with_progress_bar
+
+from custom.enikshay.duplicate_ids import get_cases_with_duplicate_ids
+
+
+class Command(BaseCommand):
+    help = """
+    Finds cases with duplicate IDs and marks all but one of each ID as a duplicate
+    """
+    already_seen = set()
+
+    def add_arguments(self, parser):
+        parser.add_argument('domain')
+        parser.add_argument('case_type')
+        parser.add_argument(
+            '--commit',
+            action='store_true',
+            dest='commit',
+            default=False,
+        )
+
+    def handle(self, domain, case_type, **options):
+        self.domain = domain
+        self.case_type = case_type
+        commit = options['commit']
+
+        for chunk in chunked(self.get_updates(), 100):
+            if commit:
+                bulk_update_cases(self.domain, chunk, self.__module__)
+
+    def get_updates(self):
+        accessor = CaseAccessors(self.domain)
+        case_ids = accessor.get_case_ids_in_domain(self.case_type)
+        bad_cases = get_cases_with_duplicate_ids(self.domain, self.case_type, case_ids)
+        # bad_case_ids = [case['case_id'] for case in bad_cases]
+        # for case in with_progress_bar(accessor.iter_cases(bad_case_ids), len(bad_case_ids)):
+        for case in with_progress_bar(bad_cases):
+            if case['case_id'] in self.already_seen:
+                yield (case['case_id'], {'has_duplicate_id': 'yes'}, False)
+            else:
+                # Don't mark this one
+                self.already_seen.add(case['case_id'])
