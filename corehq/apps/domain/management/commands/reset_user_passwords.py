2cb1cd31bc385deee7dd3061bb28430b1fd819cb
Sravan Reddy <sravfeyn@gmail.com>
2017-11-30 17:48:49 +0530

    deid other columns

--- corehq/apps/domain/management/commands/reset_user_passwords.py
+++ corehq/apps/domain/management/commands/reset_user_passwords.py
@@ -12,7 +12,9 @@ from six.moves import input
 
 
 class Command(BaseCommand):
-    help = "Reset all mobile user passwords to a given password"
+    help = """Reset all mobile user passwords to a given password and delete
+    personally identifiable data (name, phone_numbers)
+    """
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -45,6 +47,9 @@ class Command(BaseCommand):
         num_users = len(all_users)
         for i, users in enumerate(chunked(all_users, 1000)):
             for user in users:
+                user.phone_numbers = []
+                user.first_name = ""
+                user.last_name = ""
                 user.set_password(password)
             CommCareUser.bulk_save(users)
             print("Updated {count}/{total}".format(count=(i+1)*1000, total=num_users))
@@ -52,5 +57,7 @@ class Command(BaseCommand):
         for i, users in enumerate(chunked(django_users, 1000)):
             for user in users:
                 user.set_password(password)
+                user.first_name = ""
+                user.last_name = ""
             bulk_update_helper(users)
             print("Updated {count}/{total}".format(count=(i+1)*1000, total=num_users))
