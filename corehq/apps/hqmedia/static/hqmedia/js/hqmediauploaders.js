c354c0931e15962ef5ce3e08915982390c536d8e
Sarah Dawson <knittingarch@gmail.com>
2017-11-30 19:23:59 -0500

    Stickler-ci Updates

--- corehq/apps/hqmedia/static/hqmedia/js/hqmediauploaders.js
+++ corehq/apps/hqmedia/static/hqmedia/js/hqmediauploaders.js
@@ -3,7 +3,7 @@ hqDefine("hqmedia/js/hqmediauploaders", function() {
     var HQMediaUploaders = {};  // This will be referenced by the media references
     var initial_page_data = hqImport("hqwebapp/js/initial_page_data").get;
     _.each(initial_page_data("uploaders"), function(uploader) {
-        print(uploader)
+        print(uploader);
         HQMediaUploaders[uploader.slug] = new HQMediaUploaderTypes[uploader.uploader_type] (
             uploader.slug,
             uploader.media_type,
