2419e336184975dadc85c91eecc35039af4585ba
Nick Pellegrino <npellegrino@dimagi.com>
2017-12-11 10:03:18 -0500

    write to file

--- custom/enikshay/management/commands/fix_field_officers.py
+++ custom/enikshay/management/commands/fix_field_officers.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from datetime import date
 from django.core.management import BaseCommand
 
@@ -20,30 +18,33 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('domain')
+        parser.add_argument('log_filename')
 
-    def handle(self, domain, **options):
-        field_officer_location_type = LocationType.objects.get(domain=domain, code='fo')
-        for field_officer_location in SQLLocation.active_objects.filter(
-            domain=domain,
-            location_type=field_officer_location_type,
-        ):
-            user_query = UserES().domain(domain).mobile_users().location(
-                field_officer_location.location_id
-            ).fields(['_id'])
-            user_ids = [u['_id'] for u in user_query.run().hits]
-            # print('user_ids with %s: %s' % (field_officer_location.location_id, user_ids))
-            users = map(CommCareUser.get, user_ids)
-            archiveable_users = [u for u in users if looks_archiveable(u)]
-            nonarchiveable_users = [u for u in users if not looks_archiveable(u)]
-            if len(nonarchiveable_users) == 1:
-                continue
-                for archiveable_user in archiveable_users:
-                    archiveable_user.is_active = False
-                    # archiveable_user.save()
-                    print('archived %s' % archiveable_user._id)
-                field_officer_location.user_id = nonarchiveable_users[0]._id
-                # field_officer_location.save()
-                print('set location user to: %s' % nonarchiveable_users[0]._id)
-            elif len(nonarchiveable_users) > 1:
-                print('multiple nonarchiveable users for %s: %s' % (field_officer_location.location_id, user_ids))
-
+    def handle(self, domain, log_filename, **options):
+        with open(log_filename, 'w') as f:
+            field_officer_location_type = LocationType.objects.get(domain=domain, code='fo')
+            for field_officer_location in SQLLocation.active_objects.filter(
+                domain=domain,
+                location_type=field_officer_location_type,
+            ):
+                user_query = UserES().domain(domain).mobile_users().location(
+                    field_officer_location.location_id
+                ).fields(['_id'])
+                user_ids = [u['_id'] for u in user_query.run().hits]
+                f.write('user_ids with %s: %s' % (field_officer_location.location_id, user_ids))
+                users = map(CommCareUser.get, user_ids)
+                archiveable_users = [u for u in users if looks_archiveable(u)]
+                nonarchiveable_users = [u for u in users if not looks_archiveable(u)]
+                if len(nonarchiveable_users) == 1:
+                    for archiveable_user in archiveable_users:
+                        archiveable_user.is_active = False
+                        # archiveable_user.save()
+                        f.write('archived %s' % archiveable_user._id)
+                    field_officer_location.user_id = nonarchiveable_users[0]._id
+                    # field_officer_location.save()
+                    f.write('set location user to: %s' % nonarchiveable_users[0]._id)
+                elif len(nonarchiveable_users) > 1:
+                    f.write(
+                        'multiple nonarchiveable users for %s: %s' %
+                        (field_officer_location.location_id, user_ids)
+                    )
