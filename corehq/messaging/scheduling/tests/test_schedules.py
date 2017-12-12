21f445e7490785dff4a4071e53e8e5c3e2ff6c82
Giovanni Capalbo <gcapalbo@dimagi.com>
2017-12-12 15:46:29 -0500

    Add tests for random scheduling

--- corehq/messaging/scheduling/tests/test_schedules.py
+++ corehq/messaging/scheduling/tests/test_schedules.py
@@ -14,6 +14,7 @@ from corehq.messaging.scheduling.models import (
     AlertSchedule,
     TimedSchedule,
     TimedEvent,
+    RandomTimedEvent,
     SMSContent,
 )
 from corehq.messaging.scheduling.tasks import (
@@ -54,6 +55,19 @@ class BaseScheduleTest(TestCase):
         self.assertEqual(instance.current_event_num, current_event_num)
         self.assertEqual(instance.schedule_iteration_num, schedule_iteration_num)
         self.assertEqual(instance.next_event_due, next_event_due)
+        self.assertEqual(instance.active, active)
+        self.assertEqual(instance.start_date, start_date)
+
+    def assertRandomTimedScheduleInstance(self, instance, current_event_num, schedule_iteration_num,
+            next_event_due_min, next_event_due_max, active, start_date, recipient):
+        self.assertEqual(instance.domain, self.domain)
+        self.assertEqual(instance.recipient_type, recipient.doc_type)
+        self.assertEqual(instance.recipient_id, recipient.get_id)
+        self.assertEqual(instance.timed_schedule_id, self.schedule.schedule_id)
+        self.assertEqual(instance.current_event_num, current_event_num)
+        self.assertEqual(instance.schedule_iteration_num, schedule_iteration_num)
+        self.assertGreaterEqual(instance.next_event_due, next_event_due_min)
+        self.assertLessEqual(instance.next_event_due, next_event_due_max)
         self.assertEqual(instance.active, active)
         self.assertEqual(instance.start_date, start_date)
 
@@ -185,6 +199,93 @@ class DailyScheduleTest(BaseScheduleTest):
         self.assertTimedScheduleInstance(instance, 0, 1, datetime(2017, 3, 16, 16, 0), True, date(2017, 3, 16),
             self.user2)
 
+        self.assertEqual(send_patch.call_count, 0)
+
+
+@partitioned
+@patch('corehq.messaging.scheduling.models.content.SMSContent.send')
+@patch('corehq.messaging.scheduling.util.utcnow')
+class RandomTimedEventTest(BaseScheduleTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(RandomTimedEventTest, cls).setUpClass()
+        cls.schedule = TimedSchedule.create_simple_daily_schedule(
+            cls.domain,
+            RandomTimedEvent(time=time(12, 0), window_length=120),
+            SMSContent(),
+            total_iterations=2,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.schedule.delete()
+        super(RandomTimedEventTest, cls).tearDownClass()
+
+    def test_schedule_start_to_finish(self, utcnow_patch, send_patch):
+        self.assertNumInstancesForSchedule(0)
+
+        # Schedule the instance
+        utcnow_patch.return_value = datetime(2017, 3, 16, 6, 0)
+        refresh_timed_schedule_instances(self.schedule.schedule_id, (('CommCareUser', self.user1.get_id),),
+            date(2017, 3, 16))
+        self.assertNumInstancesForSchedule(1)
+        [instance] = get_timed_schedule_instances_for_schedule(self.schedule)
+        self.assertRandomTimedScheduleInstance(instance, 0, 1, datetime(2017, 3, 16, 16, 0),
+            datetime(2017, 3, 16, 17, 59), True, date(2017, 3, 16), self.user1)
+        self.assertEqual(send_patch.call_count, 0)
+
+        # Send first event
+        utcnow_patch.return_value = datetime(2017, 3, 16, 18, 0)
+        instance.handle_current_event()
+        save_timed_schedule_instance(instance)
+        self.assertNumInstancesForSchedule(1)
+        self.assertRandomTimedScheduleInstance(instance, 0, 2, datetime(2017, 3, 17, 16, 0),
+            datetime(2017, 3, 17, 17, 59), True, date(2017, 3, 16), self.user1)
+        self.assertEqual(send_patch.call_count, 1)
+
+        # Send second (and final) event
+        utcnow_patch.return_value = datetime(2017, 3, 17, 18, 0)
+        instance.handle_current_event()
+        save_timed_schedule_instance(instance)
+        self.assertNumInstancesForSchedule(1)
+        self.assertRandomTimedScheduleInstance(instance, 0, 3, datetime(2017, 3, 18, 16, 0),
+            datetime(2017, 3, 18, 17, 59), False, date(2017, 3, 16), self.user1)
+        self.assertEqual(send_patch.call_count, 2)
+
+
+
+@partitioned
+@patch('corehq.messaging.scheduling.models.content.SMSContent.send')
+@patch('corehq.messaging.scheduling.util.utcnow')
+class RandomTimedEventSpanningTwoDaysTest(BaseScheduleTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(RandomTimedEventSpanningTwoDaysTest, cls).setUpClass()
+        cls.schedule = TimedSchedule.create_simple_daily_schedule(
+            cls.domain,
+            RandomTimedEvent(time=time(23, 0), window_length=120),
+            SMSContent(),
+            total_iterations=1,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.schedule.delete()
+        super(RandomTimedEventSpanningTwoDaysTest, cls).tearDownClass()
+
+    def test_schedule(self, utcnow_patch, send_patch):
+        self.assertNumInstancesForSchedule(0)
+
+        # Schedule the instance
+        utcnow_patch.return_value = datetime(2017, 3, 16, 6, 0)
+        refresh_timed_schedule_instances(self.schedule.schedule_id, (('CommCareUser', self.user1.get_id),),
+            date(2017, 3, 16))
+        self.assertNumInstancesForSchedule(1)
+        [instance] = get_timed_schedule_instances_for_schedule(self.schedule)
+        self.assertRandomTimedScheduleInstance(instance, 0, 1, datetime(2017, 3, 17, 3, 0),
+            datetime(2017, 3, 17, 14, 59), True, date(2017, 3, 16), self.user1)
         self.assertEqual(send_patch.call_count, 0)
 
 
