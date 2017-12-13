dimagi/commcare-hq

commit 08214f3e950bded59d78370837b3a2d7f210f2b0

pr feedback

corehq/apps/analytics/static/analytix/js/logging.js
@@ -126,8 +126,8 @@ hqDefine('analytix/js/logging', [
 
     var levels = ['warning', 'debug', 'verbose'];
     var Level = function (_levelSlug, _levelPrefix, _logger) {
-        var logLevel = initialAnalytics.getFn('global')('logLevel'),
-            isVisible = levels.indexOf(_levelSlug) <= levels.indexOf(logLevel),
+        var globalLevel = initialAnalytics.getFn('global')('globalLevel'),
+            isVisible = levels.indexOf(_levelSlug) <= levels.indexOf(globalLevel),
             _levelData = {
                 slug: _levelSlug,
                 prefix: _levelPrefix,

commit 0dcd842035326c6a3aa066baa93f1192d5b21cec

fix bug introduced in 08214f3e950bded59d78370837b3a2d7f210f2b0

corehq/apps/analytics/static/analytix/js/logging.js
@@ -126,7 +126,7 @@ hqDefine('analytix/js/logging', [
 
     var levels = ['warning', 'debug', 'verbose'];
     var Level = function (_levelSlug, _levelPrefix, _logger) {
-        var globalLevel = initialAnalytics.getFn('global')('globalLevel'),
+        var globalLevel = initialAnalytics.getFn('global')('logLevel'),
             isVisible = levels.indexOf(_levelSlug) <= levels.indexOf(globalLevel),
             _levelData = {
                 slug: _levelSlug,