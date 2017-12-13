54825fb9969a295dac76e38eec848107696b818f
Jonathan Emord <jonathan.emord@gmail.com>
2017-12-13 16:13:32 -0500

    Delay all aggregation by 4 days

--- custom/icds_reports/migrations/sql_templates/create_functions.sql
+++ custom/icds_reports/migrations/sql_templates/create_functions.sql
@@ -319,7 +319,7 @@ DECLARE
 	_null_value text;
 	_blank_value text;
 	_no_text text;
-	_one_week_ago date;
+	_aggregation_delay date;
 	_rollup_text text;
 	_rollup_text2 text;
 BEGIN
@@ -334,7 +334,7 @@ BEGIN
 	_null_value = NULL;
 	_blank_value = '';
 	_no_text = 'no';
-	_one_week_ago = ($1 + INTERVAL ' - 7 DAYS')::DATE;
+	_aggregation_delay = ($1 + INTERVAL ' - 4 DAYS')::DATE;
 
 	EXECUTE 'INSERT INTO ' || quote_ident(_tablename5) || '(SELECT ' ||
 		'state_id, ' ||
@@ -396,7 +396,7 @@ BEGIN
 		'sum(valid_all_registered_in_month), ' ||
 		'sum(ebf_no_info_recorded) ' ||
 		'FROM ' || quote_ident(_ucr_child_monthly_table) || ' ' ||
-    'WHERE state_id != ' || quote_literal(_blank_value) ||  ' AND month = ' || quote_literal(_start_date) || ' AND dob <= ' || quote_literal(_one_week_ago) || ' '
+    'WHERE state_id != ' || quote_literal(_blank_value) ||  ' AND month = ' || quote_literal(_start_date) || ' AND dob <= ' || quote_literal(_aggregation_delay) || ' '
 		'GROUP BY state_id, district_id, block_id, supervisor_id, awc_id, month, sex, age_tranche, caste, disabled, minority, resident)';
 
 	EXECUTE 'CREATE INDEX ' || quote_ident(_tablename5 || '_indx1') || ' ON ' || quote_ident(_tablename5) || '(state_id, district_id, block_id, supervisor_id, awc_id)';
@@ -568,7 +568,7 @@ DECLARE
 	_null_value text;
 	_blank_value text;
 	_no_text text;
-	_one_week_ago date;
+	_aggregation_delay date;
 	_rollup_text text;
 	_rollup_text2 text;
 BEGIN
@@ -583,7 +583,7 @@ BEGIN
 	_blank_value = '';
 	_no_text = 'no';
 	EXECUTE 'SELECT table_name FROM ucr_table_name_mapping WHERE table_type = ' || quote_literal('ccs_record_monthly') INTO _ucr_ccs_record_table;
-	_one_week_ago = ($1 + INTERVAL ' - 7 DAYS')::DATE;
+	_aggregation_delay = ($1 + INTERVAL ' - 4 DAYS')::DATE;
 
 	EXECUTE 'INSERT INTO ' || quote_ident(_tablename5) || '(SELECT ' ||
 		'state_id, ' ||
@@ -638,7 +638,7 @@ BEGIN
 		'sum(lactating_all), ' ||
 		'sum(pregnant_all) ' ||
 		'FROM ' || quote_ident(_ucr_ccs_record_table) || ' ' ||
-    'WHERE state_id != ' || quote_literal(_blank_value) ||  ' AND month = ' || quote_literal(_start_date) || ' AND (add IS NULL OR add <= ' || quote_literal(_one_week_ago) || ') ' ||
+    'WHERE state_id != ' || quote_literal(_blank_value) ||  ' AND month = ' || quote_literal(_start_date) || ' AND (add IS NULL OR add <= ' || quote_literal(_aggregation_delay) || ') ' ||
 		'GROUP BY state_id, district_id, block_id, supervisor_id, awc_id, month, ccs_status, trimester, caste, disabled, minority, resident)';
 
 	EXECUTE 'CREATE INDEX ' || quote_ident(_tablename5 || '_indx1') || ' ON ' || quote_ident(_tablename5) || '(state_id, district_id, block_id, supervisor_id, awc_id)';
