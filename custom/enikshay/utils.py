0c3748fe81849e163638c7c0b0e903268326afe4
Manish Kangia <mkangia@dimagi.com>
2017-12-15 15:08:14 +0530

    submit forms to update ledgers to maintain history, optimise getting fixture items

--- custom/enikshay/utils.py
+++ custom/enikshay/utils.py
@@ -1,13 +1,18 @@
 from __future__ import absolute_import
 
 from datetime import datetime
+from collections import defaultdict
 
 from corehq.apps.fixtures.models import FixtureDataItem, FixtureDataType
-from corehq.form_processor.backends.sql.dbaccessors import LedgerAccessorSQL
 from corehq.form_processor.interfaces.dbaccessors import LedgerAccessors
-from corehq.form_processor.models import LedgerValue
 from corehq.util.quickcache import quickcache
 from custom.enikshay.case_utils import get_episode_case_from_adherence
+from casexml.apps.stock.mock import (
+    Balance,
+    Entry,
+)
+from corehq.apps.hqcase.utils import submit_case_blocks
+from corehq.apps.users.util import SYSTEM_USER_ID
 
 
 def get_episode_adherence_ledger(domain, episode_case_id, entry_id):
@@ -20,20 +25,44 @@ def get_episode_adherence_ledger(domain, episode_case_id, entry_id):
     return ledger_accessor.get_ledger_value(episode_case_id, "adherence", entry_id)
 
 
-@quickcache(['domain', 'adherence_source'], memoize_timeout=12 * 60 * 60, timeout=12 * 60 * 60)
-def get_fixture_items_for_adherence_source(domain, adherence_source):
-    """
-    currently this would be few cached FixtureDataItem objects
-    Can also consider turning the fixture in a key => value pair with key being the
-    adherence source and adherence value mapped to the ledger value
-    and combine with the next set of calculation to get fixture_item_for_value
-    """
+@quickcache(['domain'], memoize_timeout=7 * 24 * 60 * 60, timeout=7 * 24 * 60 * 60)
+def get_id_of_fixture_tagged_adherence_ledger_values(domain):
     fixture = FixtureDataType.by_domain_tag(domain, "adherence_ledger_values").first()
     if fixture:
-        return FixtureDataItem.by_field_value(domain, fixture,
-                                              "adherence_source", adherence_source).all()
+        return fixture.get_id
+
+
+@quickcache(['domain', 'fixture_id'], memoize_timeout=12 * 60 * 60, timeout=12 * 60 * 60)
+def get_all_fixture_items(domain, fixture_id):
+    """
+    :return: returns a dict mapped like
+    defaultdict(dict,
+            {u'99DOTS': {u'directly_observed_dose': u'13',
+              u'manual': u'18',
+              u'missed_dose': u'15',
+              u'missing_data': u'16',
+              u'self_administered_dose': u'17',
+              u'unobserved_dose': u'14'},
+             u'enikshay': {u'directly_observed_dose': u'1',
+              u'manual': u'6',
+              u'missed_dose': u'3',
+              u'missing_data': u'4',
+              u'self_administered_dose': u'5',
+              u'unobserved_dose': u'2'},
+            ...
+            so one can use result[u'99DOTS'][u'missed_dose'] => 15
+    """
+    if fixture_id:
+        all_items = FixtureDataItem.by_data_type(domain, fixture_id)
+        result = defaultdict(dict)
+        for item in all_items:
+            source = item.fields['adherence_source'].field_list[0].field_value
+            value = item.fields['adherence_value'].field_list[0].field_value
+            ledger_value = item.fields['ledger_value'].field_list[0].field_value
+            result[source][value] = ledger_value
+        return result
     else:
-        return None
+        return {}
 
 
 def update_ledger_with_episode(episode_case, adherence_case, entry_id):
@@ -43,33 +72,26 @@ def update_ledger_with_episode(episode_case, adherence_case, entry_id):
     """
     adherence_source = adherence_case.get_case_property('adherence_source')
     adherence_value = adherence_case.get_case_property('adherence_value')
+    domain = episode_case.domain
     if adherence_source and adherence_value:
-        fixture_items_for_source = get_fixture_items_for_adherence_source(
-            adherence_case.domain, adherence_source
-        )
-        if fixture_items_for_source:
-            fixture_items_for_value = [
-                fixture_item for fixture_item in fixture_items_for_source
-                if fixture_item.fields['adherence_value'].field_list[0].field_value == adherence_value
-            ]
-            if fixture_items_for_value:
-                fixture_item = fixture_items_for_source[0]
-                ledger_value = fixture_item.fields['ledger_value'].field_list[0].field_value
-                ledger = get_episode_adherence_ledger(episode_case.domain, episode_case.case_id, entry_id)
-                if ledger:
-                    ledger.balance = ledger_value
-                    ledger.save()
-                else:
-                    ledger = LedgerValue(
-                        domain=adherence_case.domain,
-                        case_id=episode_case.case_id,
-                        section_id="adherence",
-                        entry_id=entry_id,
-                        balance=ledger_value,
-                        last_modified=datetime.utcnow()
-                    )
-                LedgerAccessorSQL.save_ledger_values([ledger])
-                return ledger
+        fixture_id = get_id_of_fixture_tagged_adherence_ledger_values(domain)
+        if fixture_id:
+            ledger_value = get_all_fixture_items(
+                domain,
+                fixture_id
+            ).get(adherence_source, {}).get(adherence_value)
+            if ledger_value:
+                balance = Balance()
+                balance.entity_id = episode_case.case_id
+                balance.date = datetime.utcnow()
+                balance.section_id = "adherence"
+                entry = Entry()
+                entry.id = entry_id
+                entry.quantity = ledger_value
+                balance.entry = entry
+                return submit_case_blocks([balance.as_string()],
+                                          episode_case.domain,
+                                          SYSTEM_USER_ID)
 
 
 def update_ledger_for_adherence(adherence_case, episode_case=None):
