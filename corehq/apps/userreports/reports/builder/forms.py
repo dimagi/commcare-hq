fcc86687b9678e4638c07c2ae223792254f74011
sheelio <sshah@dimagi.com>
2017-12-22 12:38:49 -0500

    Revert "Remove unneeded function"
    This reverts commit b929a57f373f70eb1576e4d68f823c1f5bce176a.

--- corehq/apps/userreports/reports/builder/forms.py
+++ corehq/apps/userreports/reports/builder/forms.py
@@ -804,19 +804,20 @@ class ConfigureNewReportBase(forms.Form):
             }]
         return configured_columns
 
-    def _build_data_source(self):
+    def _get_data_source_configuration_kwargs(self):
         filters = self.cleaned_data['user_filters'] + self.cleaned_data['default_filters']
         ms_field = self._report_aggregation_cols[0] if self._is_multiselect_chart_report else None
-        config = self.ds_builder.get_ds_config_kwargs(self._configured_columns,
-                                                      filters,
-                                                      self._is_multiselect_chart_report,
-                                                      ms_field)
+        return self.ds_builder.get_ds_config_kwargs(self._configured_columns,
+                                                    filters,
+                                                    self._is_multiselect_chart_report,
+                                                    ms_field)
 
+    def _build_data_source(self):
         data_source_config = DataSourceConfiguration(
             domain=self.domain,
             # The uuid gets truncated, so it's not really universally unique.
             table_id=_clean_table_name(self.domain, str(uuid.uuid4().hex)),
-            **config
+            **self._get_data_source_configuration_kwargs()
         )
         data_source_config.validate()
         data_source_config.save()
