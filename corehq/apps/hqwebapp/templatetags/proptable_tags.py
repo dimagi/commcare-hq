c9d03a8a91743cca5c760fd4bf4f598fd07589e4
Jennifer Schweers <orange.jenny@gmail.com>
2018-01-04 10:33:36 -0500

    Remove unused adjust_heights option

--- corehq/apps/hqwebapp/templatetags/proptable_tags.py
+++ corehq/apps/hqwebapp/templatetags/proptable_tags.py
@@ -253,13 +253,11 @@ def render_tables(tables, options=None):
         })
 
     else:
-        adjust_heights = options.get('adjust_heights', True)
         put_loners_in_wells = options.get('put_loners_in_wells', True)
 
         return render_to_string("hqwebapp/proptable/dl_property_table.html", {
             "tables": tables,
             "id": id,
-            "adjust_heights": adjust_heights,
             "put_loners_in_wells": put_loners_in_wells
         })
 
