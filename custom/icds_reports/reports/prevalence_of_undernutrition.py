005569712d05428947b100b1dc5e43ce2a61243c
Jonathan Emord <jonathan.emord@gmail.com>
2018-01-17 18:09:14 -0500

    Fixed prevalence of undernutrition bugs and test.

--- custom/icds_reports/reports/prevalence_of_undernutrition.py
+++ custom/icds_reports/reports/prevalence_of_undernutrition.py
@@ -30,6 +30,7 @@ def get_prevalence_of_undernutrition_data_map(domain, config, loc_level, show_te
             severely_underweight=Sum('nutrition_status_severely_underweight'),
             normal=Sum('nutrition_status_normal'),
             valid=Sum('nutrition_status_weighed'),
+            wer_eligible=Sum('wer_eligible'),
         ).order_by('%s_name' % loc_level, '%s_map_location_name' % loc_level)
         if not show_test:
             queryset = apply_exclude(domain, queryset)
@@ -49,9 +50,11 @@ def get_prevalence_of_undernutrition_data_map(domain, config, loc_level, show_te
     severely_underweight_total = 0
     normal_total = 0
     valid_total = 0
+    eligible_total = 0
 
     for row in get_data_for(config):
         valid = row['valid'] or 0
+        eligible = row['wer_eligible'] or 0
         name = row['%s_name' % loc_level]
         on_map_name = row['%s_map_location_name' % loc_level] or name
         severely_underweight = row['severely_underweight'] or 0
@@ -62,6 +65,7 @@ def get_prevalence_of_undernutrition_data_map(domain, config, loc_level, show_te
         severely_underweight_total += severely_underweight
         normal_total += normal
         valid_total += valid
+        eligible_total += eligible
 
         data_for_map[on_map_name]['severely_underweight'] += severely_underweight
         data_for_map[on_map_name]['moderately_underweight'] += moderately_underweight
@@ -89,8 +93,7 @@ def get_prevalence_of_undernutrition_data_map(domain, config, loc_level, show_te
 
     average = ((sum(values)) / float(len(values) or 1))
 
-    sum_of_indicators = moderately_underweight_total + severely_underweight_total + normal_total
-    percent_unweighed = (valid_total - sum_of_indicators) * 100 / float(valid_total or 1)
+    percent_unweighed = (eligible_total - valid_total) * 100 / float(eligible_total or 1)
 
     gender_label, age_label, chosen_filters = chosen_filters_to_labels(config, default_interval='0 - 5 years')
 
--- custom/icds_reports/tests/reports/test_prevalence_of_undernutrition.py
+++ custom/icds_reports/tests/reports/test_prevalence_of_undernutrition.py
@@ -28,13 +28,13 @@ class TestPrevalenceOfUndernutrition(TestCase):
                             u' of the WHO Child Growth Standards median.'
                             u' <br/><br/>Children who are moderately or severely underweight'
                             u' have a higher risk of mortality',
-                    'average': 15.166496006553349,
+                    'average': 21.64670434399008,
                     'extended_info': [
-                        {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '4,945'},
+                        {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '3,480'},
                         {'indicator': '% Unweighed (0 - 5 years):', 'value': '29.63%'},
-                        {'indicator': '% Severely Underweight (0 - 5 years):', 'value': '2.02%'},
-                        {'indicator': '% Moderately Underweight (0 - 5 years):', 'value': '13.14%'},
-                        {'indicator': '% Normal (0 - 5 years):', 'value': '55.21%'}]
+                        {'indicator': '% Severely Underweight (0 - 5 years):', 'value': '2.87%'},
+                        {'indicator': '% Moderately Underweight (0 - 5 years):', 'value': '18.68%'},
+                        {'indicator': '% Normal (0 - 5 years):', 'value': '78.45%'}]
                 },
                 'fills': OrderedDict(
                     [
@@ -46,19 +46,19 @@ class TestPrevalenceOfUndernutrition(TestCase):
                 ),
                 'data': {
                     'st1': {
-                        'total': 2375,
+                        'total': 1585,
                         'severely_underweight': 40,
                         'moderately_underweight': 320,
-                        'fillKey': '0%-20%',
+                        'fillKey': '20%-35%',
                         'original_name': ["st1"],
                         'normal': 1225
                     },
                     'st2': {
-                        'total': 2570,
+                        'total': 1895,
                         'severely_underweight': 60,
                         'moderately_underweight': 330,
                         'original_name': ["st2"],
-                        'fillKey': '0%-20%',
+                        'fillKey': '20%-35%',
                         'normal': 1505
                     }
                 },
@@ -86,13 +86,13 @@ class TestPrevalenceOfUndernutrition(TestCase):
                             u' of the WHO Child Growth Standards median.'
                             u' <br/><br/>Children who are moderately or severely underweight'
                             u' have a higher risk of mortality',
-                    'average': 15.157894736842104,
+                    'average': 22.71293375394322,
                     'extended_info': [
-                        {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '475'},
+                        {'indicator': 'Total Children (0 - 5 years) weighed in given month:', 'value': '317'},
                         {'indicator': '% Unweighed (0 - 5 years):', 'value': '33.26%'},
-                        {'indicator': '% Severely Underweight (0 - 5 years):', 'value': '1.68%'},
-                        {'indicator': '% Moderately Underweight (0 - 5 years):', 'value': '13.47%'},
-                        {'indicator': '% Normal (0 - 5 years):', 'value': '51.58%'}
+                        {'indicator': '% Severely Underweight (0 - 5 years):', 'value': '2.52%'},
+                        {'indicator': '% Moderately Underweight (0 - 5 years):', 'value': '20.19%'},
+                        {'indicator': '% Normal (0 - 5 years):', 'value': '77.29%'}
                     ]
                 },
                 'fills': OrderedDict(
@@ -108,9 +108,9 @@ class TestPrevalenceOfUndernutrition(TestCase):
                         'severely_underweight': 8,
                         'moderately_underweight': 64,
                         'normal': 245,
-                        'total': 475,
+                        'total': 317,
                         'original_name': ['b1', 'b2'],
-                        'fillKey': '0%-20%'
+                        'fillKey': '20%-35%'
                     }
                 },
                 'slug': 'moderately_underweight',
@@ -268,19 +268,19 @@ class TestPrevalenceOfUndernutrition(TestCase):
                         "or severely underweight have a higher risk of mortality",
                 "tooltips_data": {
                     u"s2": {
-                        "total": 326,
+                        "total": 182,
                         "severely_underweight": 4,
                         "moderately_underweight": 54,
                         "normal": 124
                     },
                     u"s1": {
-                        "total": 144,
+                        "total": 134,
                         "severely_underweight": 8,
                         "moderately_underweight": 36,
                         "normal": 90
                     },
                     None: {
-                        "total": 235,
+                        "total": 158,
                         "severely_underweight": 6,
                         "moderately_underweight": 45,
                         "normal": 107
@@ -294,15 +294,15 @@ class TestPrevalenceOfUndernutrition(TestCase):
                         "values": [
                             [
                                 None,
-                                0.2170212765957447
+                                0.3227848101265823
                             ],
                             [
                                 "s1",
-                                0.3055555555555556
+                                0.3283582089552239
                             ],
                             [
                                 "s2",
-                                0.17791411042944785
+                                0.31868131868131866
                             ]
                         ],
                         "key": ""
