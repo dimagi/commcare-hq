39b2c3f0c08071a5b41c99e8ac83b443ad083d2d
Giovanni Capalbo <gcapalbo@dimagi.com>
2018-01-18 07:49:10 -0500

    Rename argument

--- corehq/apps/smsforms/tasks.py
+++ corehq/apps/smsforms/tasks.py
@@ -3,6 +3,6 @@ from corehq.util.celery_utils import no_result_task
 
 
 @no_result_task(queue='reminder_queue')
-def handle_due_survey_action(domain, connection_id, session_id):
-    with critical_section_for_smsforms_sessions(connection_id):
+def handle_due_survey_action(domain, contact_id, session_id):
+    with critical_section_for_smsforms_sessions(contact_id):
         pass
