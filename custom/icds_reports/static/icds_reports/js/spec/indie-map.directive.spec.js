0c23eb4ffcd7a0cc50a00289c8be75b5cb424fa6
przemyslawStudzinskiSolDevelo <pstudzinski@soldevelo.com>
2018-01-18 13:56:33 +0100

    Added close popup option when user click 'x' in ICDS indie-map
    Changed definition beneath chart and tooltip on hover in:
    ICDS-CAS Reach - AWCs Covered - Chart View

--- custom/icds_reports/static/icds_reports/js/spec/indie-map.directive.spec.js
+++ custom/icds_reports/static/icds_reports/js/spec/indie-map.directive.spec.js
@@ -140,9 +140,10 @@ describe('Indie Map Directive', function () {
 
     it('tests html content of update map', function () {
         controller.data = mockData;
-        var expected = "<button class=\"btn btn-xs btn-default\" ng-click=\"$ctrl.updateMap('Uttar Pradesh')\">Uttar Pradesh</button>";
-
-        var result = controller.getContent(mockGeography);
+        var expected = "<button type=\"button\" class=\"close\" ng-click=\"$ctrl.closePopup()\" aria-label=\"Close\">" +
+            "<span aria-hidden=\"true\">&times;</span> </button><button class=\"btn btn-xs btn-default btn-block\"" +
+            " ng-click=\"$ctrl.updateMap(\'Uttar Pradesh\')\">Uttar Pradesh</button>";
+        var result = controller.getHtmlContent(mockGeography);
         assert.equal(expected, result);
     });
 
