96ce9cf8fcd9804b8a99037c0170353c9c4d0d18
Farid Rener <proteusvacuum@gmail.com>
2018-01-19 12:11:33 -0500

    Revert index name

--- corehq/pillows/mappings/case_search_mapping.py
+++ corehq/pillows/mappings/case_search_mapping.py
@@ -5,7 +5,7 @@ from corehq.util.elastic import es_index
 from pillowtop.es_utils import ElasticsearchIndexInfo
 
 
-CASE_SEARCH_INDEX = es_index("case_search_2018-01-16")
+CASE_SEARCH_INDEX = es_index("case_search_2016-03-15")
 CASE_SEARCH_ALIAS = "case_search"
 CASE_SEARCH_MAX_RESULTS = 100
 CASE_SEARCH_MAPPING = mapping_from_json('case_search_mapping.json')
