bed95bed6c1e4e5030c0cc6a3ab2aa83e7fa0628
Ethan Soergel <esoergel@gmail.com>
2018-01-22 11:56:08 -0500

    Remove old version of get_reverse_indexed_cases

--- corehq/sql_accessors/migrations/0050_faster_get_reverse_indexed_cases.py
+++ corehq/sql_accessors/migrations/0050_faster_get_reverse_indexed_cases.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from __future__ import absolute_import
 from django.db import migrations
 
-from corehq.form_processor.models import CommCareCaseIndexSQL
-from corehq.sql_db.operations import RawSQLMigration
-
-migrator = RawSQLMigration(('corehq', 'sql_accessors', 'sql_templates'), {})
+from corehq.sql_db.operations import noop_migration
 
 
 class Migration(migrations.Migration):
@@ -17,5 +14,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrator.get_migration('get_reverse_indexed_cases_2.sql'),
+        noop_migration()
     ]
