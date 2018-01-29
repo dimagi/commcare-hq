7c5ca660655438de03dcc4e535c15b65df6a9320
Sravan Reddy <sravfeyn@gmail.com>
2018-01-29 18:35:06 +0530

    remove domain from userdim

--- corehq/warehouse/migrations/0017_domain_conditional_null.py
+++ corehq/warehouse/migrations/0017_domain_conditional_null.py
@@ -14,21 +14,9 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.RemoveField(
             model_name='userdim',
             name='domain',
-            field=models.CharField(max_length=255, null=True, blank=True),
-        ),
-        HqRunSQL(
-            """
-            ALTER TABLE warehouse_userdim
-            ADD CONSTRAINT domain_conditional_null CHECK
-            ((doc_type = 'WebUser' and domain IS NULL) OR (doc_type = 'CommCareUser' and domain is NOT NULL))
-            """,
-            reverse_sql="""
-            ALTER TABLE warehouse_userdim
-            DROP CONSTRAINT IF EXISTS domain_conditional_null;
-            """
         ),
         migrations.AlterField(
             model_name='userstagingtable',
