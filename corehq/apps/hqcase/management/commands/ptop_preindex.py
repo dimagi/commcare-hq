f8a41f3a4bcb4ab87c83ee0b5463c16f00b717eb
Nick Pellegrino <npellegrino@dimagi.com>
2018-01-30 08:47:49 -0500

    organize imports

--- corehq/apps/hqcase/management/commands/ptop_preindex.py
+++ corehq/apps/hqcase/management/commands/ptop_preindex.py
@@ -1,26 +1,24 @@
 from __future__ import print_function
-
 from __future__ import absolute_import
 # http://www.gevent.org/gevent.monkey.html#module-gevent.monkey
 from gevent import monkey
-import six
-from six.moves import map
 monkey.patch_all()
 
-from corehq.apps.hqcase.management.commands.ptop_reindexer_v2 import FACTORIES_BY_SLUG
-
-from corehq.pillows.utils import get_all_expected_es_indices
-
-from corehq.elastic import get_es_new
-
-from corehq.util.log import get_traceback_string
+import gevent
+import six
 
 from datetime import datetime
+from six.moves import map
+
 from django.core.mail import mail_admins
-from corehq.pillows.user import add_demo_user_to_user_index
-import gevent
 from django.core.management.base import BaseCommand
 from django.conf import settings
+
+from corehq.apps.hqcase.management.commands.ptop_reindexer_v2 import FACTORIES_BY_SLUG
+from corehq.elastic import get_es_new
+from corehq.pillows.user import add_demo_user_to_user_index
+from corehq.pillows.utils import get_all_expected_es_indices
+from corehq.util.log import get_traceback_string
 
 
 def get_reindex_commands(alias_name):
