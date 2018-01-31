ff1b097fe5549c900ef99be538ca49f2c31cd62a
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-01-31 14:40:18 -0500

    added comment

--- corehq/apps/userreports/transforms/custom/date.py
+++ corehq/apps/userreports/transforms/custom/date.py
@@ -69,6 +69,7 @@ def get_gregorian_to_ethiopian(date_string):
         return ''
 
     try:
+        print("About to enter conversion script.")
         ethiopian_year, ethiopian_month, ethiopian_day = EthiopianDateConverter.to_ethiopian(year, month, day)
         return '{}-{:02d}-{:02d}'.format(ethiopian_year, ethiopian_month, ethiopian_day)
     except Exception:
