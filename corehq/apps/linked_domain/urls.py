e880cc89df14e62eb9cfb1cd0f34406c76b672f8
Simon Kelly <skelly@dimagi.com>
2018-02-01 12:40:27 +0200

    add basic views

---
+++ corehq/apps/linked_domain/urls.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+from django.conf.urls import url
+
+from corehq.apps.linked_domain.views import toggles_and_previews, custom_data_models
+
+urlpatterns = [
+    url(r'^toggles/$', toggles_and_previews),
+    url(r'^custom_data_models/$', custom_data_models),
+]
---
+++ corehq/apps/linked_domain/views.py
@@ -0,0 +1,29 @@
+from django.http import JsonResponse
+
+from corehq import toggles, feature_previews
+from corehq.apps.custom_data_fields.dbaccessors import get_by_domain_and_type
+from corehq.apps.domain.decorators import api_key_auth
+from corehq.apps.linked_domain.decorators import require_linked_domain
+from corehq.apps.locations.views import LocationFieldsView
+from corehq.apps.products.views import ProductFieldsView
+
+
+@api_key_auth
+@require_linked_domain
+def toggles_and_previews(request, domain):
+    return JsonResponse({
+        'toggles': list(toggles.toggles_dict(domain=domain)),
+        'previews': list(feature_previews.previews_dict(domain=domain))
+    })
+
+
+@api_key_auth
+@require_linked_domain
+def custom_data_models(request, domain):
+    custom_data_models = {}
+    for field_view in (LocationFieldsView, ProductFieldsView, LocationFieldsView):
+        model = get_by_domain_and_type(domain, field_view.field_type)
+        if model:
+            custom_data_models[field_view.field_type] = model.to_json()['fields']
+
+    return JsonResponse(custom_data_models)
