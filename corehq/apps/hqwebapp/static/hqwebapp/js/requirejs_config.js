4c8cb0d6b3fb2fd9fa7eef4898b6a9fc38e94cbd
Jennifer Schweers <orange.jenny@gmail.com>
2018-02-01 17:03:58 -0500

    Move knockout mapping plugin to bower

--- corehq/apps/hqwebapp/static/hqwebapp/js/requirejs_config.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/requirejs_config.js
@@ -6,7 +6,7 @@ requirejs.config({
         "underscore": "underscore/underscore",
         "bootstrap": "bootstrap/dist/js/bootstrap.min",
         "knockout": "knockout/dist/knockout.debug",
-        "ko.mapping": "hqwebapp/js/lib/knockout_plugins/knockout_mapping.ko.min",
+        "ko.mapping": "knockout-mapping/knockout.mapping",
         "datatables": "datatables/media/js/jquery.dataTables.min",
         "datatables.fixedColumns": "datatables-fixedcolumns/js/dataTables.fixedColumns",
         "datatables.bootstrap": "datatables-bootstrap3/BS3/assets/js/datatables",
