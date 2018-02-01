ecefd8a3c64a2c74e5933ad56c7f30a32bd7105b
Norman Hooper <nhooper@dimagi.com>
2018-02-01 19:55:02 +0200

    Log the function / request type

--- corehq/motech/dhis2/api.py
+++ corehq/motech/dhis2/api.py
@@ -35,7 +35,8 @@ def log_request(func):
             return response
         finally:
             if log_level >= domain_log_level:
-                JsonApiLog.log(log_level, self, request_error, response_status, response_body, *args, **kwargs)
+                JsonApiLog.log(log_level, self, request_error, response_status, response_body, func,
+                               *args, **kwargs)
 
     return request_wrapper
 
