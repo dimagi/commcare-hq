1bac2084d7e1b4bcbf2646e92692c58d95064604
Jordan Roth <jmtroth0@gmail.com>
2018-02-05 16:02:16 -0500

    moving billing statements code into new module

---
+++ corehq/apps/domain/static/domain/js/billing_statements.js
@@ -0,0 +1,126 @@
+/* global Stripe */
+hqDefine("domain/js/billing_statements", function() {
+
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        paymentMethodHandlers = hqImport("accounting/js/payment_method_handler");
+
+    $(function () {
+        Stripe.setPublishableKey(initialPageData.get("stripe_options").stripe_public_key);
+        var WireInvoiceHandler = paymentMethodHandlers.WireInvoiceHandler;
+        var PaymentMethodHandler = paymentMethodHandlers.PaymentMethodHandler;
+        var Invoice = paymentMethodHandlers.Invoice;
+        var TotalCostItem = paymentMethodHandlers.TotalCostItem;
+        var pagination = initialPageData.get("pagination");
+        var paginatedListModel = new window.CRUDPaginatedListModel(
+            pagination.total,
+            pagination.limit,
+            pagination.page,
+            {
+                statusCodeText: pagination.status_codes,
+                createItemForm: pagination.create_item_form,
+            }
+        );
+        $(function () {
+            ko.applyBindings(paginatedListModel, $('#editable-paginated-list').get(0));
+        });
+
+        var bulkPaymentHandler = new PaymentMethodHandler(
+            "bulk-payment-form",
+            {
+                submitBtnText: gettext("Submit Payment"),
+                errorMessages: initialPageData.get("payment_error_messages"),
+                submitURL: initialPageData.get("payment_urls").process_bulk_payment_url,
+            }
+        );
+
+        var bulkWirePaymentHandler = new WireInvoiceHandler(
+            "bulk-wire-payment-form",
+            {
+                submitBtnText: gettext("Submit Invoice Request"),
+                isWire: true,
+                errorMessages: initialPageData.get("payment_error_messages"),
+                submitURL: initialPageData.get("payment_urls").process_wire_invoice_url,
+            }
+        );
+
+        var paymentHandler = new PaymentMethodHandler(
+            "payment-form",
+            {
+                submitBtnText: gettext("Submit Payment"),
+                errorMessages: initialPageData.get("payment_error_messages"),
+                submitURL: initialPageData.get("payment_urls").process_invoice_payment_url,
+            }
+        );
+
+        // A sign that the data model isn't exactly right - credit cards are shared data.
+        // Consider refactoring at some point.
+        var handlers = [bulkPaymentHandler, paymentHandler];
+        for (var i = 0; i < handlers.length; i++) {
+            handlers[i].handlers = handlers;
+        }
+        var stripe_cards = initialPageData.get("stripe_options").stripe_cards;
+        if (stripe_cards) {
+            bulkPaymentHandler.loadCards(stripe_cards);
+            paymentHandler.loadCards(stripe_cards);
+        }
+
+        $(function () {
+            $('#bulkPaymentModal').koApplyBindings(bulkPaymentHandler);
+            $('#bulkWirePaymentModal').koApplyBindings(bulkWirePaymentHandler);
+            $('#paymentModal').koApplyBindings(paymentHandler);
+        });
+
+        $('#bulkPaymentBtn').click(function () {
+            bulkPaymentHandler.costItem(new TotalCostItem({
+                totalBalance: paginatedListModel.totalDue(),
+                paginatedListModel: paginatedListModel,
+            }));
+            bulkPaymentHandler.reset();
+        });
+        $('#bulkWirePaymentBtn').click(function () {
+            bulkWirePaymentHandler.costItem(new TotalCostItem({
+                totalBalance: paginatedListModel.totalDue(),
+                paginatedListModel: paginatedListModel,
+            }));
+        });
+
+        paginatedListModel.totalDue = ko.observable(parseFloat("{{total_balance}}"));
+
+        paginatedListModel.displayTotalDue = ko.computed(function () {
+            return "$" + paginatedListModel.totalDue().toFixed(2);
+        });
+        paginatedListModel.showUnpaidBills = ko.observable(true);
+        paginatedListModel.showAllBills = ko.computed(function () {
+            return !paginatedListModel.showUnpaidBills();
+        });
+        paginatedListModel.toggleUnpaidBills = function () {
+            paginatedListModel.showUnpaidBills(!paginatedListModel.showUnpaidBills());
+            paginatedListModel.currentPage(1);
+            paginatedListModel.refreshList();
+        };
+
+        paginatedListModel.getAdditionalData = function () {
+            var additional_data = {
+                'show_unpaid': paginatedListModel.showUnpaidBills()
+            };
+            if (window.location.href.split('?').length > 2) {
+                additional_data['show_hidden'] = _(window.location.href.split('?')[1].split('&')).contains('show_hidden=true');
+            }
+            return additional_data;
+        };
+        paginatedListModel.initRow = function (rowElems, paginatedItem) {
+            var paymentButton = $(rowElems).find('.payment-button');
+            if (paymentButton) {
+                paymentButton.click(function (e) {
+                    paymentHandler.costItem(new Invoice({
+                        paginatedItem: paginatedItem,
+                        paginatedList: paginatedListModel,
+                    }));
+                    paymentHandler.reset();
+                    e.preventDefault();
+                });
+            }
+        };
+        paginatedListModel.init();
+    });
+});
