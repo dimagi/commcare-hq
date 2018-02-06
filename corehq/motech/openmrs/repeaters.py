acdb264f8bcc7354da3b3674c23b3a960e427b4c
Norman Hooper <nhooper@dimagi.com>
2018-02-06 17:18:24 +0200

    Use form_class_name to determine edit form

--- corehq/motech/openmrs/repeaters.py
+++ corehq/motech/openmrs/repeaters.py
@@ -37,6 +37,13 @@ class OpenmrsRepeater(CaseRepeater):
     def payload_doc(self, repeat_record):
         return FormAccessors(repeat_record.domain).get_form(repeat_record.payload_id)
 
+    @property
+    def form_class_name(self):
+        """
+        The class name used to determine which edit form to use
+        """
+        return self.__class__.__name__
+
     @classmethod
     def available_for_domain(cls, domain):
         return OPENMRS_INTEGRATION.enabled(domain)
