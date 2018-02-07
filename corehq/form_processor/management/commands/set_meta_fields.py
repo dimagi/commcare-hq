0fcf711e9bddb890fcc837440a8ac4be0bb63fe9
Sravan Reddy <sravfeyn@gmail.com>
2018-02-07 16:07:38 +0530

    split by partition

--- corehq/form_processor/management/commands/set_meta_fields.py
+++ corehq/form_processor/management/commands/set_meta_fields.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors
 from corehq.form_processor.models import XFormInstanceSQL
+from corehq.sql_db.util import split_list_by_db_partition
 from dimagi.utils.chunked import chunked
 
 
@@ -16,35 +17,42 @@ logger.setLevel('DEBUG')
 META_FIELDS = ("time_end", "time_start", "commcare_version", "build_version")
 
 
-def iter_form_ids_by_domain_received_on(domain, from_date, to_date):
+def form_ids_by_domain_received_on(domain, from_date, to_date):
     from corehq.sql_db.util import run_query_across_partitioned_databases
     q_expr = Q(domain=domain, received_on__gt=from_date, received_on__lt=to_date)
-    for form_id in run_query_across_partitioned_databases(
-            XFormInstanceSQL, q_expr, values=['form_id']):
-        yield form_id
+    return list(run_query_across_partitioned_databases(
+        XFormInstanceSQL, q_expr, values=['form_id']
+    ))
 
 
 def set_meta_fields(domain, from_date, to_date, failfast):
-    all_form_ids = iter_form_ids_by_domain_received_on(domain, from_date, to_date)
-    for form_ids in chunked(all_form_ids, 100):
-        forms_to_update = []
-        for form in FormAccessors(domain).iter_forms(form_ids):
-            already_set = any([getattr(form, field) for field in META_FIELDS])
-            if not already_set:
-                try:
-                    form.set_meta_properties()
-                except Exception as e:
-                    logger.exception("Error setting meta properties for form {id}".format(id=form.form_id))
-                    if failfast:
-                        raise e
-                if any([getattr(form, field) for field in META_FIELDS]):
-                    forms_to_update.append(form)
-        if forms_to_update:
-            print('Updating form_ids: {}'.format(
-                ','.join([form.form_id for form in forms_to_update]))
-            )
-        bulk_update_helper(forms_to_update)
-
+    all_form_ids = form_ids_by_domain_received_on(domain, from_date, to_date)
+    total_count = len(all_form_ids)
+    updated_count = 0
+    # split by partition for bulk updating
+    for dbname, form_ids_by_db in split_list_by_db_partition(all_form_ids):
+        for form_ids in chunked(form_ids_by_db, 100):
+            forms_to_update = []
+            for form in FormAccessors(domain).iter_forms(form_ids):
+                already_set = any([getattr(form, field) for field in META_FIELDS])
+                if not already_set:
+                    try:
+                        form.set_meta_properties()
+                    except Exception as e:
+                        logger.exception("Error setting meta properties for form {id}".format(id=form.form_id))
+                        if failfast:
+                            raise e
+                    if any([getattr(form, field) for field in META_FIELDS]):
+                        forms_to_update.append(form)
+            if forms_to_update:
+                print('Updating form_ids: {}'.format(
+                    ','.join([form.form_id for form in forms_to_update]))
+                )
+            bulk_update_helper(forms_to_update)
+            updated_count += len(forms_to_update)
+    print("Updated {updated} forms out of {total} forms".format(
+        updated=updated_count, total=total_count
+    ))
 
 class Command(BaseCommand):
     help = "Set xform meta fields for all XFormInstanceSQL docs"
