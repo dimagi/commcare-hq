8567dcb509a528a79396a3ca4e491b2bd2db5c51
Jordan Roth <jmtroth0@gmail.com>
2018-02-07 17:14:37 -0500

    adding code in new module

---
+++ corehq/apps/domain/static/domain/js/confirm_billing_info.js
@@ -0,0 +1,23 @@
+/* global Stripe */
+hqDefine("domain/js/confirm_billing_info", function() {
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        stripeCardManager = hqImport("accounting/js/stripe_card_manager");
+
+    $('a.breadcrumb-2').click(function (e) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        var $navigateForm = $('<form method="post" style="display: none;" />').attr('action', url + 'confirm/');
+        $(this).after($navigateForm);
+        $navigateForm.append($('<input type="hidden" name="plan_edition" />').val(initialPageData.get("plan").edition));
+        $navigateForm.submit();
+    });
+
+    Stripe.setPublishableKey(initialPageData.get("stripe_public_key"));
+    var cardManager = new stripeCardManager.StripeCardManager({
+        cards: initialPageData.get("cards"),
+        url: initialPageData.get("cards_view_url"),
+    });
+    $(function() {
+        $("#card-manager").koApplyBindings(cardManager);
+    });
+});
