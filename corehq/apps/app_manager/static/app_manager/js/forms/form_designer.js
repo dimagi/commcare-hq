d65bba0752b9ee42f37ec0cf2d8b0ed12d22a86a
Jordan Roth <jmtroth0@gmail.com>
2018-02-09 15:35:03 -0500

    match up with definition

--- corehq/apps/app_manager/static/app_manager/js/forms/form_designer.js
+++ corehq/apps/app_manager/static/app_manager/js/forms/form_designer.js
@@ -90,15 +90,15 @@ hqDefine("app_manager/js/forms/form_designer", function() {
                 usage("Form Builder", group, message);
             }
 
-            function interaction(label, attributes) {
-                hqImport('analytix/js/appcues').track(label, attributes);
+            function event(label, attributes) {
+                hqImport('analytix/js/appcues').trackEvent(label, attributes);
             }
 
             return {
                 fbUsage: fbUsage,
                 usage: usage,
                 workflow: workflow,
-                interaction: interaction,
+                event: event,
             };
         });
 
