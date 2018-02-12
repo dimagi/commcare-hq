b4e6f0a58a9b50784ea001035ca780cc6e0fc543
Sravan Reddy <sravfeyn@gmail.com>
2018-02-12 14:18:42 +0530

    move migration

--- corehq/warehouse/migrations/0023_add_build_info.py
+++ corehq/warehouse/migrations/0023_add_build_info.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('warehouse', '0020_synclogfact'),
+        ('warehouse', '0022_userstagingtable_assigned_location_ids'),
     ]
 
     operations = [
