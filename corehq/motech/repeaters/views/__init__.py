67dd6a826fdfbea50da3cf0cb045bc38433e50f7
Ethan Soergel <esoergel@gmail.com>
2018-02-13 17:29:56 -0500

    Reorganize repeater views
    A bunch of them were lost in domain.views, so I pulled those in and
    split the views across two files - one for repeaters, one for repeat
    records

---
+++ corehq/motech/repeaters/views/__init__.py
@@ -0,0 +1,21 @@
+from .repeaters import (
+    AddCaseRepeaterView,
+    AddCustomSOAPCaseRepeaterView,
+    AddCustomSOAPLocationRepaterView,
+    AddFormRepeaterView,
+    AddRepeaterView,
+    DomainForwardingOptionsView,
+    EditCaseRepeaterView,
+    EditFormRepeaterView,
+    EditRepeaterView,
+    drop_repeater,
+    pause_repeater,
+    resume_repeater,
+    test_repeater,
+)
+from .repeat_records import (
+    DomainForwardingRepeatRecords,
+    RepeatRecordView,
+    cancel_repeat_record,
+    requeue_repeat_record,
+)
