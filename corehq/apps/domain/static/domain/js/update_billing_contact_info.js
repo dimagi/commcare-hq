8c1cee3756d8ed3bb8e6e8ec1feeba3a0527b50b
Jordan Roth <jmtroth0@gmail.com>
2018-02-14 12:19:23 -0500

    doc ready handler

--- corehq/apps/domain/static/domain/js/update_billing_contact_info.js
+++ corehq/apps/domain/static/domain/js/update_billing_contact_info.js
@@ -3,15 +3,17 @@ hqDefine('domain/js/update_billing_contact_info', function () {
     var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
         StripeCardManager = hqImport("accounting/js/stripe_card_manager").StripeCardManager;
 
-    Stripe.setPublishableKey(initialPageData.get("stripe_public_key"));
-    var cardManager = new StripeCardManager({
-        cards: initialPageData.get("cards"),
-        url: initialPageData.reverse("cards_view"),
-    });
-    $("#card-manager").koApplyBindings(cardManager);
+    $(function() {
+        Stripe.setPublishableKey(initialPageData.get("stripe_public_key"));
+        var cardManager = new StripeCardManager({
+            cards: initialPageData.get("cards"),
+            url: initialPageData.reverse("cards_view"),
+        });
+        $("#card-manager").koApplyBindings(cardManager);
 
-    $("#show_emails").click(function() {
-        $('#emails-text').show();
-        $(this).parent().hide();
+        $("#show_emails").click(function() {
+            $('#emails-text').show();
+            $(this).parent().hide();
+        });
     });
 });
