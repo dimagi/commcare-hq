c721111705e8198d37dee54d9817ba9a3c13a8f9
Jordan Roth <jmtroth0@gmail.com>
2018-02-15 11:37:51 -0500

    indentation and on page ready

--- corehq/apps/sms/static/sms/js/add_gateway.js
+++ corehq/apps/sms/static/sms/js/add_gateway.js
@@ -1,11 +1,14 @@
 hqDefine('sms/js/add_gateway', function() {
     var initialPageData = hqImport('hqwebapp/js/initial_page_data');
-        var gatewayFormHandler = new AddGatewayFormHandler({
-            share_backend: initialPageData.get('form').give_other_domains_access.value,
-            use_load_balancing: initialPageData.get('use_load_balancing'),
-            phone_numbers: initialPageData.get('form').phone_numbers.value || '[]',
-            phone_number_required_text: gettext('You must have at least one phone number.'),
-        });
+    var gatewayFormHandler = new AddGatewayFormHandler({
+        share_backend: initialPageData.get('form').give_other_domains_access.value,
+        use_load_balancing: initialPageData.get('use_load_balancing'),
+        phone_numbers: initialPageData.get('form').phone_numbers.value || '[]',
+        phone_number_required_text: gettext('You must have at least one phone number.'),
+    });
+
+    $(function () {
         $('#add-gateway-form').koApplyBindings(gatewayFormHandler);
         gatewayFormHandler.init();
+    });
 });
