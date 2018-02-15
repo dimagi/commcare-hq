af369f53f8346dff68ce1bc8c4b8f546b889fbca
Jordan Roth <jmtroth0@gmail.com>
2018-02-15 11:59:37 -0500

    also migrating the add gateway form handler

--- corehq/apps/sms/static/sms/js/add_gateway.js
+++ corehq/apps/sms/static/sms/js/add_gateway.js
@@ -1,15 +1,14 @@
 hqDefine('sms/js/add_gateway', function() {
     var initialPageData = hqImport('hqwebapp/js/initial_page_data'),
-        AddGatewayFormHandler = hqImport('add_gateway_form_handler').AddGatewayFormHandler;
-
-    var gatewayFormHandler = new AddGatewayFormHandler({
-        share_backend: initialPageData.get('form').give_other_domains_access.value,
-        use_load_balancing: initialPageData.get('use_load_balancing'),
-        phone_numbers: initialPageData.get('form').phone_numbers.value || '[]',
-        phone_number_required_text: gettext('You must have at least one phone number.'),
-    });
+        AddGatewayFormHandler = hqImport('sms/js/add_gateway_form_handler').AddGatewayFormHandler;
 
     $(function () {
+        var gatewayFormHandler = new AddGatewayFormHandler({
+            share_backend: initialPageData.get('form').give_other_domains_access.value,
+            use_load_balancing: initialPageData.get('use_load_balancing'),
+            phone_numbers: initialPageData.get('form').phone_numbers.value || '[]',
+            phone_number_required_text: gettext('You must have at least one phone number.'),
+        });
         $('#add-gateway-form').koApplyBindings(gatewayFormHandler);
         gatewayFormHandler.init();
     });
--- corehq/apps/sms/static/sms/js/add_gateway_form_handler.ko.js
+++ corehq/apps/sms/static/sms/js/add_gateway_form_handler.ko.js
@@ -1,48 +1,52 @@
-var AddGatewayFormHandler = function (initial) {
-    'use strict';
-    var self = this;
+hqDefine('sms/js/add_gateway_form_handler', function () {
+    var AddGatewayFormHandler = function (initial) {
+        'use strict';
+        var self = this;
 
-    self.share_backend = ko.observable(initial.share_backend);
-    self.showAuthorizedDomains = ko.computed(function () {
-        return self.share_backend();
-    });
+        self.share_backend = ko.observable(initial.share_backend);
+        self.showAuthorizedDomains = ko.computed(function () {
+            return self.share_backend();
+        });
 
-    self.phone_numbers = ko.observableArray();
-    self.phone_numbers_json = ko.computed(function() {
-        return ko.toJSON(self.phone_numbers());
-    });
+        self.phone_numbers = ko.observableArray();
+        self.phone_numbers_json = ko.computed(function() {
+            return ko.toJSON(self.phone_numbers());
+        });
 
-    self.use_load_balancing = ko.observable(initial.use_load_balancing);
+        self.use_load_balancing = ko.observable(initial.use_load_balancing);
 
-    self.init = function () {
-        if(self.use_load_balancing()) {
-            var arr = initial.phone_numbers;
-            for(var i = 0; i < arr.length; i++) {
-                self.phone_numbers.push(new PhoneNumber(arr[i].phone_number));
+        self.init = function () {
+            if(self.use_load_balancing()) {
+                var arr = initial.phone_numbers;
+                for(var i = 0; i < arr.length; i++) {
+                    self.phone_numbers.push(new PhoneNumber(arr[i].phone_number));
+                }
+                if(self.phone_numbers().length == 0) {
+                    self.addPhoneNumber();
+                }
             }
-            if(self.phone_numbers().length == 0) {
-                self.addPhoneNumber();
+        };
+
+        self.addPhoneNumber = function() {
+            self.phone_numbers.push(new PhoneNumber(""));
+        };
+
+        self.removePhoneNumber = function() {
+            if(self.phone_numbers().length == 1) {
+                alert(initial.phone_number_required_text);
+            } else {
+                self.phone_numbers.remove(this);
             }
-        }
+        };
     };
 
-    self.addPhoneNumber = function() {
-        self.phone_numbers.push(new PhoneNumber(""));
+    var PhoneNumber = function (phone_number) {
+        'use strict';
+        var self = this;
+        self.phone_number = ko.observable(phone_number);
     };
 
-    self.removePhoneNumber = function() {
-        if(self.phone_numbers().length == 1) {
-            alert(initial.phone_number_required_text);
-        } else {
-            self.phone_numbers.remove(this);
-        }
+    return {
+        AddGatewayFormHandler: AddGatewayFormHandler,
     };
-};
-
-var PhoneNumber = function (phone_number) {
-    'use strict';
-    var self = this;
-    self.phone_number = ko.observable(phone_number);
-};
-
-
+});
