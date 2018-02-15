f844461780bc0511eb921e787ba2cac1e4ae42d2
Jordan Roth <jmtroth0@gmail.com>
2018-02-15 11:37:04 -0500

    initial page data

--- corehq/apps/sms/static/sms/js/add_gateway.js
+++ corehq/apps/sms/static/sms/js/add_gateway.js
@@ -1,9 +1,10 @@
 hqDefine('sms/js/add_gateway', function() {
+    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
         var gatewayFormHandler = new AddGatewayFormHandler({
-            share_backend: {{ form.give_other_domains_access.value|BOOL }},
-            use_load_balancing: {{ use_load_balancing|BOOL }},
-            phone_numbers: {{ form.phone_numbers.value|default:'[]'|safe }},
-            phone_number_required_text: "{% trans 'You must have at least one phone number.' %}"
+            share_backend: initialPageData.get('form').give_other_domains_access.value,
+            use_load_balancing: initialPageData.get('use_load_balancing'),
+            phone_numbers: initialPageData.get('form').phone_numbers.value || '[]',
+            phone_number_required_text: gettext('You must have at least one phone number.'),
         });
         $('#add-gateway-form').koApplyBindings(gatewayFormHandler);
         gatewayFormHandler.init();
