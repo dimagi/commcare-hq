12d00afd9076a185bad03f1ee1ca67c29d4434ac
Jordan Roth <jmtroth0@gmail.com>
2018-02-16 13:36:27 -0500

    first pass externalizing javascript in manage_registration_invitations

---
+++ corehq/apps/sms/static/sms/js/manage_registration_invitations.js
@@ -0,0 +1,13 @@
+hqDefine('sms/js/manage_registration_invitations', function() {
+$(function () {
+    var RegistrationModel = function() {
+        var self = this;
+        self.registration_message_type = ko.observable();
+        self.showCustomRegistrationMessage = ko.computed(function() {
+            return self.registration_message_type() == 'CUSTOM';
+        });
+    };
+    var registrationModel = new RegistrationModel();
+    ko.applyBindings(registrationModel, $('#registration-modal').get(0));
+});
+});
