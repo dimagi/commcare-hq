c484cfbf00d428caa4cbfba48f29c386e2ca18eb
Jordan Roth <jmtroth0@gmail.com>
2018-02-16 11:37:49 -0500

    lint

--- corehq/apps/sms/static/sms/js/chat_contacts.js
+++ corehq/apps/sms/static/sms/js/chat_contacts.js
@@ -1,5 +1,5 @@
 hqDefine('sms/js/chat_contacts', function() {
-var initialPageData = hqImport('hqwebapp/js/initial_page_data');
+    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
     var contactListTable = null;
 
     function FilterViewModel() {
@@ -23,13 +23,13 @@ var initialPageData = hqImport('hqwebapp/js/initial_page_data');
             "aoColumnDefs": [
                 {
                     "aTargets": [0],
-                    "render": function(data, type, row, meta) {
-                        return '<a target="_blank" href="'+row[4]+'">'+row[0]+'</a>'+
-                               '<span class="btn btn-primary pull-right" '+
-                               'onClick="window.open(\''+row[5]+'\', \'_blank\', \'location=no,menubar=no,scrollbars=no,status=no,toolbar=no,height=400,width=400\');">' +
+                    "render": function(data, type, row) {
+                        return '<a target="_blank" href="' + row[4] + '">' + row[0] + '</a>' +
+                               '<span class="btn btn-primary pull-right" ' +
+                               'onClick="window.open(\'' + row[5] + '\', \'_blank\', \'location=no,menubar=no,scrollbars=no,status=no,toolbar=no,height=400,width=400\');">' +
                                gettext("Chat") + '<i class="fa fa-share"></i></span>';
-                    }
-                }
+                    },
+                },
             ],
             "bProcessing": true,
             "bServerSide": true,
@@ -37,7 +37,9 @@ var initialPageData = hqImport('hqwebapp/js/initial_page_data');
             "bFilter": true,
             "oLanguage": {
                 "sLengthMenu": gettext("Show ") + "_MENU_" + gettext(" contacts per page"),
-                "sProcessing": '<img src="' + initialPageData.get("ajax_loader") + '" alt="loading indicator" />' + gettext("Loading Contacts..."),
+                "sProcessing": '<img src="' + initialPageData.get("ajax_loader") +
+                    '" alt="loading indicator" />' +
+                    gettext("Loading Contacts..."),
                 "sInfo": gettext("Showing _START_ to _END_ of _TOTAL_ contacts"),
                 "sInfoFiltered": gettext("(filtered from _MAX_ total contacts)"),
             },
