4af538049cb6984348f5fb671cf02ca0cb3c2246
Jordan Roth <jmtroth0@gmail.com>
2018-02-16 12:12:40 -0500

    indentation and lint

--- corehq/apps/sms/static/sms/js/compose.js
+++ corehq/apps/sms/static/sms/js/compose.js
@@ -1,30 +1,34 @@
 hqDefine('sms/js/compose', function() {
     var initialPageData = hqImport('hqwebapp/js/initial_page_data');
 
-        $(function(){
-            $("#hint_id_recipients").addClass("alert alert-info");
-            $("#hint_id_message").addClass("alert alert-info");
-            $("#id_message").on('keyup', function () {
-                var max_char = 160,
-                        current_count = $(this).val().length,
-                        cc_message = "";
-                var chars_remaining = max_char - current_count;
-                var $char_count = $("#hint_id_message");
-                if(chars_remaining < 0)
-                    $char_count.addClass("alert-danger").removeClass('alert-info');
-                else
-                    $char_count.removeClass("alert-danger").addClass('alert-info');
-                cc_message = current_count + " character";
-                if(current_count != 1)
-                    cc_message = cc_message + "s";
-                cc_message = cc_message + " (" + max_char + " max)";
-                $char_count.text(cc_message);
-            });
+    $(function(){
+        $("#hint_id_recipients").addClass("alert alert-info");
+        $("#hint_id_message").addClass("alert alert-info");
+        $("#id_message").on('keyup', function () {
+            var max_char = 160,
+                current_count = $(this).val().length,
+                cc_message = "",
+                chars_remaining = max_char - current_count,
+                $char_count = $("#hint_id_message");
 
+            if (chars_remaining < 0) {
+                $char_count.addClass("alert-danger").removeClass('alert-info');
+            } else {
+                $char_count.removeClass("alert-danger").addClass('alert-info');
+            }
 
-            $('.sms-typeahead').multiTypeahead({
-                source: initialPageData.get('sms_contacts')
-            }).focus();
+            cc_message = current_count + " character";
+            if (current_count !== 1) {
+                cc_message = cc_message + "s";
+            }
 
+            cc_message = cc_message + " (" + max_char + " max)";
+            $char_count.text(cc_message);
         });
+
+
+        $('.sms-typeahead').multiTypeahead({
+            source: initialPageData.get('sms_contacts')
+        }).focus();
+    });
 });
