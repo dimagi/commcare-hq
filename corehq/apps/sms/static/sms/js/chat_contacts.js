afb1187a374a09ea646d3ed60f9a8616ace142c6
Jordan Roth <jmtroth0@gmail.com>
2018-02-16 11:26:15 -0500

    initial page data

--- corehq/apps/sms/static/sms/js/chat_contacts.js
+++ corehq/apps/sms/static/sms/js/chat_contacts.js
@@ -1,4 +1,5 @@
 hqDefine('sms/js/chat_contacts', function() {
+var initialPageData = hqImport('hqwebapp/js/initial_page_data');
     var contactListTable = null;
 
     function FilterViewModel() {
@@ -25,8 +26,8 @@ hqDefine('sms/js/chat_contacts', function() {
                     "render": function(data, type, row, meta) {
                         return '<a target="_blank" href="'+row[4]+'">'+row[0]+'</a>'+
                                '<span class="btn btn-primary pull-right" '+
-                               'onClick="window.open(\''+row[5]+'\', \'_blank\', \'location=no,menubar=no,scrollbars=no,status=no,toolbar=no,height=400,width=400\');">'+
-                               '{% trans "Chat" %} <i class="fa fa-share"></i></span>';
+                               'onClick="window.open(\''+row[5]+'\', \'_blank\', \'location=no,menubar=no,scrollbars=no,status=no,toolbar=no,height=400,width=400\');">' +
+                               gettext("Chat") + '<i class="fa fa-share"></i></span>';
                     }
                 }
             ],
@@ -35,12 +36,12 @@ hqDefine('sms/js/chat_contacts', function() {
             "bSort": false,
             "bFilter": true,
             "oLanguage": {
-                "sLengthMenu": "{% trans "Show" %} _MENU_ {% trans "contacts per page" %}",
-                "sProcessing": '<img src="{% static "hqwebapp/images/ajax-loader.gif" %}" alt="loading indicator" /> {% trans "Loading Contacts..." %}',
-                "sInfo": "{% trans "Showing _START_ to _END_ of _TOTAL_ contacts" %}",
-                "sInfoFiltered": "{% trans "(filtered from _MAX_ total contacts)" %}",
+                "sLengthMenu": gettext("Show ") + "_MENU_" + gettext(" contacts per page"),
+                "sProcessing": '<img src="' + initialPageData.get("ajax_loader") + '" alt="loading indicator" />' + gettext("Loading Contacts..."),
+                "sInfo": gettext("Showing _START_ to _END_ of _TOTAL_ contacts"),
+                "sInfoFiltered": gettext("(filtered from _MAX_ total contacts)"),
             },
-            "sAjaxSource": "{% url "chat_contact_list" domain %}",
+            "sAjaxSource": initialPageData.reverse("chat_contact_list"),
             "sDom": "lrtip",
         });
         var filterViewModel = new FilterViewModel();
