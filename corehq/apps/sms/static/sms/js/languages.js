0f57647efbbdd525b231d836f9df7d31d7f1357e
jmtroth0 <jroth@dimagi.com>
2018-02-16 17:23:32 -0500

    ejs/sms languages (#19498)
    * first pass externalizing javascript in languages
    
    * initial page data
    
    * lint

---
+++ corehq/apps/sms/static/sms/js/languages.js
@@ -0,0 +1,17 @@
+hqDefine('sms/js/languages', function() {
+    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
+    $(function () {
+        var langs = initialPageData.get('sms_langs');
+        var saveURL = initialPageData.reverse("edit_sms_languages");
+        var edit = true;
+        var validate = true;
+        var SupportedLanguages = hqImport('app_manager/js/supported_languages').SupportedLanguages;
+        var sl = new SupportedLanguages({
+            langs: langs,
+            saveURL: saveURL,
+            edit: edit,
+            validate: validate,
+        });
+        $("#supported-languages").koApplyBindings(sl);
+    });
+});
