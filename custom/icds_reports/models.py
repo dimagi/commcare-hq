d5827381480fafc1f5d60f93c328d826e0eae6d3
Jonathan Emord <jonathan.emord@gmail.com>
2018-02-16 19:06:09 -0500

    Added an aggregation helper.
    Fixed a couple issues in comparison query.
    
    Removed unneeded imports.
    
    Added documentation of the aggregation helpers.

--- custom/icds_reports/models.py
+++ custom/icds_reports/models.py
@@ -1,13 +1,10 @@
 from __future__ import absolute_import
-from datetime import date
-import hashlib
 
 from dateutil.relativedelta import relativedelta
-from django.db import models
-from toggle.models import Toggle
+from django.db import connections, models
 
-from corehq.apps.userreports.models import StaticDataSourceConfiguration, get_datasource_config
-from corehq.apps.userreports.util import get_table_name
+from corehq.sql_db.routers import db_for_read_write
+from custom.icds_reports.utils.aggregation import ComplementaryFormsAggregationHelper
 
 
 class AwcLocation(models.Model):
@@ -566,10 +563,6 @@ class CcsRecordMonthly(models.Model):
         db_table = 'ccs_record_monthly'
 
 
-from django.db import connections, InternalError, transaction
-from corehq.sql_db.routers import db_for_read_write
-
-
 def get_cursor(model):
     db = db_for_read_write(model)
     return connections[db].cursor()
@@ -589,7 +582,7 @@ class AggregateComplementaryFeedingForms(models.Model):
     state_id = models.CharField(max_length=40)
     month = models.DateField(help_text="Will always be YYYY-MM-01")
 
-    # primary key as it's unqiue for every partition
+    # primary key as it's unique for every partition
     case_id = models.CharField(max_length=40, primary_key=True)
 
     latest_time_end_processed = models.DateTimeField(
@@ -634,155 +627,22 @@ class AggregateComplementaryFeedingForms(models.Model):
     class Meta:
         db_table = 'icds_dashboard_comp_feed_form'
 
-    @staticmethod
-    def child_tablename(state_id, month):
-        month_string = month.strftime('%Y-%m')
-        hash_for_table = hashlib.md5(state_id + month_string).hexdigest()[8:]
-        return 'icds_db_child_cf_form_' + hash_for_table
+    @classmethod
+    def aggregate(cls, state_id, month):
+        helper = ComplementaryFormsAggregationHelper(state_id, month)
+        with get_cursor(cls) as cursor:
+            cursor.execute(helper.create_table_query(month - relativedelta(months=1)))
+            cursor.execute(helper.drop_table_query())
+            cursor.execute(helper.create_table_query())
+            cursor.execute(helper.aggregation_query())
 
-    @staticmethod
-    def bootstrap_prev_month(state_id, month):
-        month = month - relativedelta(months=1)
-        parent_tablename = 'icds_dashboard_comp_feed_form'
-        month_string = month.strftime('%Y-%m-%d')
-        tablename = AggregateComplementaryFeedingForms.child_tablename(state_id, month)
-
-        with get_cursor(AggregateComplementaryFeedingForms) as cursor:
-            cursor.execute(
-                "CREATE TABLE IF NOT EXISTS \"" + tablename + "\" (CHECK ( "
-                "  month = '" + month_string + "' AND "
-                "  state_id = '" + state_id + "'"
-                " ), "
-                "LIKE \"" + parent_tablename + "\" INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES"
-                ") INHERITS (\"" + parent_tablename + "\")"
-            )
-
-        return tablename
-
-    @staticmethod
-    def create_child_table(state_id, month):
-        parent_tablename = 'icds_dashboard_comp_feed_form'
-        month_string = month.strftime('%Y-%m-%d')
-        tablename = AggregateComplementaryFeedingForms.child_tablename(state_id, month)
-
-        with get_cursor(AggregateComplementaryFeedingForms) as cursor:
-            cursor.execute("DROP TABLE IF EXISTS \"" + tablename + "\"")
-            cursor.execute(
-                "CREATE TABLE \"" + tablename + "\" (CHECK ( " +
-                "  month = '" + month_string + "' AND " +
-                "  state_id = '" + state_id + "'" +
-                "), " +
-                "LIKE \"" + parent_tablename + "\" INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES" +
-                ") INHERITS (\"" + parent_tablename + "\")"
-            )
-
-        return tablename
-
-    @staticmethod
-    def data_from_ucr(state_id, month):
-        current_month_start = month
-        next_month_start = current_month_start + relativedelta(months=1)
-
-        return """
-        SELECT child_health_case_id AS case_id,
-        LAST_VALUE(timeend) OVER w AS latest_time_end,
-        MAX(play_comp_feeding_vid) OVER w AS play_comp_feeding_vid,
-        MAX(comp_feeding) OVER w AS comp_feeding_ever,
-        MAX(demo_comp_feeding) OVER w AS demo_comp_feeding,
-        MAX(counselled_pediatric_ifa) OVER w AS counselled_pediatric_ifa,
-        LAST_VALUE(comp_feeding) OVER w AS comp_feeding_latest,
-        LAST_VALUE(diet_diversity) OVER w AS diet_diversity,
-        LAST_VALUE(diet_quantity) OVER w AS diet_quantity,
-        LAST_VALUE(hand_wash) OVER w AS hand_wash
-        FROM {ucr_tablename}
-        WHERE timeend >= {current_month_start} AND timeend < {next_month_start} AND state_id = {state_id}
-        WINDOW w AS (
-            PARTITION BY child_health_case_id
-            ORDER BY timeend RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-        )
-        """.format(
-            ucr_tablename='"{}"'.format(ucr_data_source_tablename()),
-            current_month_start="'{}'".format(current_month_start),
-            next_month_start="'{}'".format(next_month_start),
-            state_id="'{}'".format(state_id)
-        )
-
-    @staticmethod
-    def query(state_id, month):
-        month = month.replace(day=1)
-
-        tablename = AggregateComplementaryFeedingForms.create_child_table(state_id, month)
-        previous_month_tablename = AggregateComplementaryFeedingForms.bootstrap_prev_month(state_id, month)
-
-        # data from the UCR will already be filtered for data from this month so
-        # all data from the UCR is from a later time than the previous month and
-        # we can safely use COALESCE instead of a larger CASE WHEN
-        return ("""
-            INSERT INTO {tablename} (
-              state_id, month, case_id, latest_time_end_processed, comp_feeding_ever,
-              demo_comp_feeding, counselled_pediatric_ifa, play_comp_feeding_vid,
-              comp_feeding_latest, diet_diversity, diet_quantity, hand_wash
-            ) (
-              SELECT
-                '{state_id}' AS state_id,
-                '{month}' AS month,
-                COALESCE(ucr.case_id, prev_month.case_id) AS case_id,
-                GREATEST(ucr.latest_time_end, prev_month.latest_time_end_processed) AS latest_time_end_processed,
-                GREATEST(ucr.comp_feeding_ever, prev_month.comp_feeding_ever) AS comp_feeding_ever,
-                GREATEST(ucr.demo_comp_feeding, prev_month.demo_comp_feeding) AS demo_comp_feeding,
-                GREATEST(ucr.counselled_pediatric_ifa, prev_month.counselled_pediatric_ifa) AS counselled_pediatric_ifa,
-                GREATEST(ucr.play_comp_feeding_vid, prev_month.play_comp_feeding_vid) AS play_comp_feeding_vid,
-                COALESCE(ucr.comp_feeding_latest, prev_month.comp_feeding_latest) AS comp_feeding_latest,
-                COALESCE(ucr.diet_diversity, prev_month.diet_diversity) AS diet_diversity,
-                COALESCE(ucr.diet_quantity, prev_month.diet_quantity) AS diet_quantity,
-                COALESCE(ucr.hand_wash, prev_month.hand_wash) AS hand_wash
-              FROM ({ucr_table_query}) ucr
-              FULL OUTER JOIN "{previous_month_tablename}" prev_month
-              ON ucr.case_id = prev_month.case_id
-            )
-            """.format(
-                previous_month_tablename=previous_month_tablename,
-                tablename=tablename,
-                ucr_table_query=AggregateComplementaryFeedingForms.data_from_ucr(state_id, month),
-                month=month.strftime('%Y-%m-%d'),
-                state_id=state_id))
-
-    @staticmethod
-    def aggregate(state_id, month):
-        with get_cursor(AggregateComplementaryFeedingForms) as cursor:
-            cursor.execute(AggregateComplementaryFeedingForms.query(state_id, month))
-
-    @staticmethod
-    def compare_with_old_data(state_id, month):
+    @classmethod
+    def compare_with_old_data(cls, state_id, month):
         from corehq.form_processor.utils.sql import fetchall_as_namedtuple
+        helper = ComplementaryFormsAggregationHelper(state_id, month)
+        query = helper.compare_with_old_data_query()
 
         with get_cursor(AggregateComplementaryFeedingForms) as cursor:
-            cursor.execute("""
-            SELECT agg.case_id
-            FROM child_health_monthly_ucr chm_ucr
-            FULL OUTER JOIN cf_form_agg agg
-            ON chm_ucr.doc_id = agg.case_id AND chm_ucr.month = agg.month
-            WHERE chm_ucr = month and agg.state_id = state_id AND (
-                  (chm_ucr.cf_eligible = 1 AND (
-                      chm_ucr.cf_in_month != agg.comp_feeding_latest OR
-                      chm_ucr.cf_diet_diversity != agg.diet_diversity OR
-                      chm_ucr.cf_diet_quantity != agg.diet_quantity OR
-                      chm_ucr.cf_handwashing != agg.hand_wash OR
-                      chm_ucr.cf_demo != agg.demo_comp_feeding OR
-                      chm_ucr.counsel_pediatric_ifa != agg.counselled_pediatric_ifa OR
-                      chm_ucr.counsel_comp_feeding_vid != agg.play_comp_feeding_vid OR
-                  )) OR (chm_ucr.cf_initiation_eligible = 1 AND
-                      chm_ucr.cf_initiated != agg.comp_feeding_ever OR
-                  )
-            )
-            """)
-
+            cursor.execute(query)
             rows = fetchall_as_namedtuple(cursor)
             return [row.child_health_case_id for row in rows]
-
-
-def ucr_data_source_tablename():
-    domain = Toggle.get('dashboard_icds_reports').enabled_users[0][len('domain:'):]
-    doc_id = StaticDataSourceConfiguration.get_doc_id(domain, 'static-complementary_feeding_forms')
-    config, _ = get_datasource_config(doc_id, domain)
-    return get_table_name('icds-cas', config.table_id)
---
+++ custom/icds_reports/utils/aggregation.py
@@ -0,0 +1,193 @@
+import hashlib
+
+from dateutil.relativedelta import relativedelta
+from toggle.models import Toggle
+
+from corehq.apps.userreports.models import StaticDataSourceConfiguration, get_datasource_config
+from corehq.apps.userreports.util import get_table_name
+
+
+class BaseICDSAggregationHelper(object):
+    """Defines an interface for aggregating data from UCRs to specific tables
+    for the dashboard.
+
+    All aggregate tables are partitioned by state and month
+
+    Attributes:
+        ucr_data_source_id - The UCR data source that contains the raw data to aggregate
+        aggregate_parent_table - The parent table defined in models.py that will contain aggregate data
+        aggregate_child_table_prefix - The prefix for tables that inherit from the parent table
+    """
+    ucr_data_source_id = None
+    aggregate_parent_table = None
+    aggregate_child_table_prefix = None
+
+    def __init__(self, state_id, month):
+        self.state_id = state_id
+        self.month = month
+
+    @property
+    def domain(self):
+        # Currently its only possible for one domain to have access to the ICDS dashboard per env
+        return Toggle.get('dashboard_icds_reports').enabled_users[0][len('domain:'):]
+
+    @property
+    def ucr_tablename(self):
+        doc_id = StaticDataSourceConfiguration.get_doc_id(self.domain, self.ucr_data_source_id)
+        config, _ = get_datasource_config(doc_id, self.domain)
+        return get_table_name(self.domain, config.table_id)
+
+    def generate_child_tablename(self, month=None):
+        month = month or self.month
+        month_string = month.strftime('%Y-%m')
+        hash_for_table = hashlib.md5(self.state_id + month_string).hexdigest()[8:]
+        return self.aggregate_child_table_prefix + hash_for_table
+
+    def create_table_query(self, month=None):
+        month = month or self.month
+        month_string = month.strftime('%Y-%m-%d')
+        tablename = self.generate_child_tablename(month)
+
+        return """
+        CREATE TABLE IF NOT EXISTS \"{child_tablename}\" (
+            CHECK (month = '{month_string}' AND state_id = '{state_id}'),
+            LIKE \"{parent_tablename}\" INCLUDING DEFAULTS INCLUDING CONSTRAINTS INCLUDING INDEXES
+        ) INHERITS (\"{parent_tablename}\")
+        """.format(
+            parent_tablename=self.aggregate_parent_table,
+            child_tablename=tablename,
+            month_string=month_string,
+            state_id=self.state_id
+        )
+
+    def drop_table_query(self):
+        tablename = self.generate_child_tablename(self.month)
+        return "DROP TABLE IF EXISTS \"{tablename}\"".format(tablename=tablename)
+
+    def data_from_ucr_query(self):
+        """Returns a SQL query from the UCR data table that puts data in the
+        form expected by the aggregate table
+        """
+        raise NotImplementedError
+
+    def aggregate_query(self):
+        """Returns the SQL query that will aggregate from a UCR source to an
+        aggregate table.
+        """
+        raise NotImplementedError
+
+    def compare_with_old_data_query(self):
+        """Used for backend migrations from one data source to another. Returns
+        a SQL query that will return any rows that are inconsistent from the
+        old data to the new.
+        """
+        raise NotImplementedError
+
+
+class ComplementaryFormsAggregationHelper(BaseICDSAggregationHelper):
+    ucr_data_source_id = 'static-complementary_feeding_forms'
+    aggregate_parent_table = 'icds_dashboard_comp_feed_form'
+    aggregate_child_table_prefix = 'icds_db_child_cf_form_'
+    child_health_monthly_ucr_id = 'static-child_cases_monthly_tableau_v2'
+
+    @property
+    def _old_ucr_tablename(self):
+        doc_id = StaticDataSourceConfiguration.get_doc_id(self.domain, self.child_health_monthly_ucr_id)
+        config, _ = get_datasource_config(doc_id, self.domain)
+        return get_table_name(self.domain, config.table_id)
+
+    def data_from_ucr_query(self):
+        current_month_start = self.month.replace(day=1)
+        next_month_start = current_month_start + relativedelta(months=1)
+
+        return """
+        SELECT child_health_case_id AS case_id,
+        LAST_VALUE(timeend) OVER w AS latest_time_end,
+        MAX(play_comp_feeding_vid) OVER w AS play_comp_feeding_vid,
+        MAX(comp_feeding) OVER w AS comp_feeding_ever,
+        MAX(demo_comp_feeding) OVER w AS demo_comp_feeding,
+        MAX(counselled_pediatric_ifa) OVER w AS counselled_pediatric_ifa,
+        LAST_VALUE(comp_feeding) OVER w AS comp_feeding_latest,
+        LAST_VALUE(diet_diversity) OVER w AS diet_diversity,
+        LAST_VALUE(diet_quantity) OVER w AS diet_quantity,
+        LAST_VALUE(hand_wash) OVER w AS hand_wash
+        FROM {ucr_tablename}
+        WHERE timeend >= {current_month_start} AND timeend < {next_month_start} AND state_id = {state_id}
+        WINDOW w AS (
+            PARTITION BY child_health_case_id
+            ORDER BY timeend RANGE BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        )
+        """.format(
+            ucr_tablename='"{}"'.format(self.ucr_tablename),
+            current_month_start="'{}'".format(current_month_start),
+            next_month_start="'{}'".format(next_month_start),
+            state_id="'{}'".format(self.state_id)
+        )
+
+    def aggregation_query(self):
+        month = self.month.replace(day=1)
+        tablename = self.generate_child_tablename(month)
+        previous_month_tablename = self.generate_child_tablename(month - relativedelta(months=1))
+
+        # data from the UCR will already be filtered for data from this month so
+        # all data from the UCR is from a later time than the previous month and
+        # we can safely use COALESCE instead of a larger CASE WHEN
+        return """
+        INSERT INTO {tablename} (
+          state_id, month, case_id, latest_time_end_processed, comp_feeding_ever,
+          demo_comp_feeding, counselled_pediatric_ifa, play_comp_feeding_vid,
+          comp_feeding_latest, diet_diversity, diet_quantity, hand_wash
+        ) (
+          SELECT
+            '{state_id}' AS state_id,
+            '{month}' AS month,
+            COALESCE(ucr.case_id, prev_month.case_id) AS case_id,
+            GREATEST(ucr.latest_time_end, prev_month.latest_time_end_processed) AS latest_time_end_processed,
+            GREATEST(ucr.comp_feeding_ever, prev_month.comp_feeding_ever) AS comp_feeding_ever,
+            GREATEST(ucr.demo_comp_feeding, prev_month.demo_comp_feeding) AS demo_comp_feeding,
+            GREATEST(ucr.counselled_pediatric_ifa, prev_month.counselled_pediatric_ifa) AS counselled_pediatric_ifa,
+            GREATEST(ucr.play_comp_feeding_vid, prev_month.play_comp_feeding_vid) AS play_comp_feeding_vid,
+            COALESCE(ucr.comp_feeding_latest, prev_month.comp_feeding_latest) AS comp_feeding_latest,
+            COALESCE(ucr.diet_diversity, prev_month.diet_diversity) AS diet_diversity,
+            COALESCE(ucr.diet_quantity, prev_month.diet_quantity) AS diet_quantity,
+            COALESCE(ucr.hand_wash, prev_month.hand_wash) AS hand_wash
+          FROM ({ucr_table_query}) ucr
+          FULL OUTER JOIN "{previous_month_tablename}" prev_month
+          ON ucr.case_id = prev_month.case_id
+        )
+        """.format(
+            previous_month_tablename=previous_month_tablename,
+            tablename=tablename,
+            ucr_table_query=self.data_from_ucr_query(),
+            month=month.strftime('%Y-%m-%d'),
+            state_id=self.state_id
+        )
+
+    def compare_with_old_data_query(self):
+        """Compares data from the complementary feeding forms aggregate table
+        to the the old child health monthly UCR table that current aggregate
+        script uses
+        """
+        month = self.month.replace(day=1)
+        return """
+        SELECT agg.case_id
+        FROM "{child_health_monthly_ucr}" chm_ucr
+        FULL OUTER JOIN "{new_agg_table}" agg
+        ON chm_ucr.doc_id = agg.case_id AND chm_ucr.month = agg.month AND agg.state_id = chm_ucr.state_id
+        WHERE chm_ucr.month = '{month}' and agg.state_id = '{state_id}' AND (
+              (chm_ucr.cf_eligible = 1 AND (
+                  chm_ucr.cf_in_month != agg.comp_feeding_latest OR
+                  chm_ucr.cf_diet_diversity != agg.diet_diversity OR
+                  chm_ucr.cf_diet_quantity != agg.diet_quantity OR
+                  chm_ucr.cf_handwashing != agg.hand_wash OR
+                  chm_ucr.cf_demo != agg.demo_comp_feeding OR
+                  chm_ucr.counsel_pediatric_ifa != agg.counselled_pediatric_ifa OR
+                  chm_ucr.counsel_comp_feeding_vid != agg.play_comp_feeding_vid
+              )) OR (chm_ucr.cf_initiation_eligible = 1 AND chm_ucr.cf_initiated != agg.comp_feeding_ever)
+        )
+        """.format(
+            child_health_monthly_ucr=self._old_ucr_tablename,
+            new_agg_table=self.aggregate_parent_table,
+            month=month.strftime('%Y-%m-%d'),
+            state_id=self.state_id
+        )
