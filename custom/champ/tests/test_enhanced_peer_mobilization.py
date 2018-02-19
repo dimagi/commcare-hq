b811449f30940acbdf2bb7beb6e253b79fbbb0dc
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2018-02-19 15:20:27 +0100

    added __future__ import

--- custom/champ/tests/test_enhanced_peer_mobilization.py
+++ custom/champ/tests/test_enhanced_peer_mobilization.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from datetime import date
 from corehq.apps.userreports.specs import EvaluationContext
 from custom.champ.tests.utils import TestDataSourceExpressions
--- custom/champ/tests/test_service_uptake.py
+++ custom/champ/tests/test_service_uptake.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from custom.champ.tests import ChampTestCase
 from custom.champ.views import ServiceUptakeView
 
