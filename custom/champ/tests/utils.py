cb08d3f677028d9a01558e060bcebc11306d5036
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2018-02-20 16:38:50 +0100

    fix tests

--- custom/champ/tests/utils.py
+++ custom/champ/tests/utils.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 
 import json
-import os
-
 from django.test.testcases import TestCase
 from django.test.client import RequestFactory
 from django.test.testcases import SimpleTestCase
@@ -17,9 +15,43 @@ from corehq.apps.userreports.models import DataSourceConfiguration
 from corehq.apps.userreports.specs import FactoryContext
 from corehq.apps.users.models import CommCareUser
 from couchforms.models import XFormInstance
+import postgres_copy
+import sqlalchemy
+import os
+import mock
+
+from django.test.utils import override_settings
+from corehq.apps.userreports.models import StaticDataSourceConfiguration
+from corehq.apps.userreports.util import get_indicator_adapter, get_table_name
+from corehq.sql_db.connections import connection_manager, UCR_ENGINE_ID
 
 
 class ChampTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ChampTestCase, cls).setUpClass()
+        _call_center_domain_mock = mock.patch(
+            'corehq.apps.callcenter.data_source.call_center_data_source_configuration_provider'
+        )
+        _call_center_domain_mock.start()
+        with override_settings(SERVER_ENVIRONMENT='production'):
+            configs = StaticDataSourceConfiguration.by_domain('champ-cameroon')
+            adapters = [get_indicator_adapter(config) for config in configs]
+
+            for adapter in adapters:
+                adapter.build_table()
+
+            engine = connection_manager.get_engine(UCR_ENGINE_ID)
+            metadata = sqlalchemy.MetaData(bind=engine)
+            metadata.reflect(bind=engine, extend_existing=True)
+            path = os.path.join(os.path.dirname(__file__), 'fixtures')
+            for file_name in os.listdir(path):
+                with open(os.path.join(path, file_name)) as f:
+                    table_name = get_table_name('champ-cameroon', file_name[:-4])
+                    table = metadata.tables[table_name]
+                    postgres_copy.copy_from(f, table, engine, format='csv', null='', header=True)
+        _call_center_domain_mock.stop()
 
     def setUp(self):
         self.factory = RequestFactory()
@@ -35,6 +67,19 @@ class ChampTestCase(TestCase):
         user.is_authenticated = True
         user.is_active = True
         self.user = user
+
+    @classmethod
+    def tearDownClass(cls):
+        _call_center_domain_mock = mock.patch(
+            'corehq.apps.callcenter.data_source.call_center_data_source_configuration_provider'
+        )
+        _call_center_domain_mock.start()
+        with override_settings(SERVER_ENVIRONMENT='production'):
+            configs = StaticDataSourceConfiguration.by_domain('champ-cameroon')
+            adapters = [get_indicator_adapter(config) for config in configs]
+            for adapter in adapters:
+                adapter.drop_table()
+        _call_center_domain_mock.stop()
 
 
 class TestDataSourceExpressions(SimpleTestCase):
