a4c146595f9b472a7a3555504437fd7d1b85011c
Nick Pellegrino <npellegrino@dimagi.com>
2018-02-21 18:02:38 -0500

    continue to use case_name for cases

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -2090,7 +2090,13 @@ class DetailColumn(IndexedSchema):
 
         # Lazy migration: xpath expressions from format to first-class property
         if data.get('format') == 'calculate':
-            data['field'] = dot_interpolate(data.get('calc_xpath', '.'), data['field'])
+            if data.get('model') == 'case':
+                property_xpath = PropertyXpathGenerator(
+                    None, None, None, super(DetailColumn, cls).wrap(data)
+                ).xpath
+                data['field'] = dot_interpolate(data.get('calc_xpath', '.'), property_xpath)
+            else:
+                data['field'] = dot_interpolate(data.get('calc_xpath', '.'), data['field'])
             data['useXpathExpression'] = True
             data['hasAutocomplete'] = False
             data['format'] = 'plain'
