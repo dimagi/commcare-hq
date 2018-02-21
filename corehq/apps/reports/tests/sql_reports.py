3b37c265bd5ec19a9255078718d577c367cce636
Nick Pellegrino <npellegrino@dimagi.com>
2018-02-20 22:34:11 -0500

    use float division in report test

--- corehq/apps/reports/tests/sql_reports.py
+++ corehq/apps/reports/tests/sql_reports.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import
 from __future__ import unicode_literals
+from __future__ import division
 from numbers import Number
 from corehq.apps.reports.filters.dates import DatespanFilter
 from corehq.apps.reports.standard import DatespanMixin, CustomProjectReport
