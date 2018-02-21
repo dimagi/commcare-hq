8e62eaca2ef09835b79b3ea2859258476d3a75cc
Nick Pellegrino <npellegrino@dimagi.com>
2018-02-21 11:32:58 -0500

    python-modernize custom/ewsghana/ -w --future-unicode

--- custom/ewsghana/alerts/ongoing_stockouts.py
+++ custom/ewsghana/alerts/ongoing_stockouts.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from datetime import datetime, timedelta
 from django.db.models.aggregates import Max
 from casexml.apps.stock.models import StockTransaction
