68c09bb93e0b1c2c44a9cc19308777bf3534cdd4
Jonathan Emord <jonathan.emord@gmail.com>
2018-02-26 16:50:29 -0500

    Refactored async rebuild table.

--- corehq/apps/userreports/management/commands/async_rebuild_table.py
+++ corehq/apps/userreports/management/commands/async_rebuild_table.py
@@ -44,10 +44,7 @@ class Command(BaseCommand):
             adapter.after_table_build()
 
         self.domain = domain
-        if self.referenced_type == CASE_DOC_TYPE:
-            self.case_type = case_type_or_xmlns
-        else:
-            self.xmlns = case_type_or_xmlns
+        self.case_type_or_xmlns = case_type_or_xmlns
 
         config_ids = [config._id for config in configs]
         for id_ in self._get_ids_to_process():
@@ -60,40 +57,29 @@ class Command(BaseCommand):
                 config.save()
 
     def _get_ids_to_process(self):
-        if self.referenced_type == CASE_DOC_TYPE:
-            return self._get_case_ids_to_process()
-        return self._get_form_ids_to_process()
-
-    def _get_form_ids_to_process(self):
         from corehq.sql_db.util import get_db_aliases_for_partitioned_query
         dbs = get_db_aliases_for_partitioned_query()
         for db in dbs:
-            form_ids = (
-                XFormInstanceSQL.objects
-                .using(db)
-                .filter(domain=self.domain, xmlns=self.xmlns)
-                .values_list('form_id', flat=True)
-            )
-            num_ids = len(form_ids)
+            ids_ = self._get_ids(db)
+            num_ids = len(ids_)
             print("processing %d docs from db %s" % (num_ids, db))
-            for i, id_ in enumerate(num_ids):
+            for i, id_ in enumerate(ids_):
                 yield id_
                 if i % 1000 == 0:
                     print("processed %d / %d docs from db %s" % (i, num_ids, db))
 
-    def _get_case_ids_to_process(self):
-        from corehq.sql_db.util import get_db_aliases_for_partitioned_query
-        dbs = get_db_aliases_for_partitioned_query()
-        for db in dbs:
-            case_ids = (
+    def _get_ids(self, db):
+        if self.referenced_type == CASE_DOC_TYPE:
+            return (
                 CommCareCaseSQL.objects
                 .using(db)
-                .filter(domain=self.domain, type=self.case_type)
+                .filter(domain=self.domain, type=self.case_type_or_xmlns)
                 .values_list('case_id', flat=True)
             )
-            num_case_ids = len(case_ids)
-            print("processing %d docs from db %s" % (num_case_ids, db))
-            for i, case_id in enumerate(case_ids):
-                yield case_id
-                if i % 1000 == 0:
-                    print("processed %d / %d docs from db %s" % (i, num_case_ids, db))
+        elif self.referenced_type == XFORM_DOC_TYPE:
+            return(
+                XFormInstanceSQL.objects
+                .using(db)
+                .filter(domain=self.domain, xmlns=self.case_type_or_xmlns)
+                .values_list('form_id', flat=True)
+            )
