d11cb131f967d6e21747c1cb3fa003c5114d9590
Nick Pellegrino <npellegrino@dimagi.com>
2018-02-26 09:49:38 -0500

    python-modernize custom/bihar/ -w --future-unicode

--- custom/bihar/reports/__init__.py
+++ custom/bihar/reports/__init__.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.utils.translation import ugettext_noop as _
 
 from custom.bihar.reports import supervisor, due_list, mch_reports
