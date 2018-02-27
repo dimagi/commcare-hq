04ab95ecd2c333aca577686cffb0544dcf74d22b
Ethan Soergel <esoergel@gmail.com>
2018-02-27 12:03:18 -0700

    Fall back to amount_fulfilled

--- custom/enikshay/const.py
+++ custom/enikshay/const.py
@@ -66,6 +66,7 @@ VOUCHER_ID = "voucher_id"
 FULFILLED_BY_ID = "voucher_fulfilled_by_id"
 FULFILLED_BY_LOCATION_ID = "voucher_fulfilled_by_location_id"
 AMOUNT_APPROVED = "amount_approved"
+AMOUNT_FULFILLED = "amount_fulfilled"
 INVESTIGATION_TYPE = "investigation_type"
 
 ENIKSHAY_TIMEZONE = 'Asia/Kolkata'
