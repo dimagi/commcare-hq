7ffa0c6793bf6092d34d5f47563604506f0ddae1
Gabriella Bova <bova3.0@gmail.com>
2018-03-05 10:24:04 -0500

    Get all user IDs when deleting a location

--- corehq/apps/locations/models.py
+++ corehq/apps/locations/models.py
@@ -767,8 +767,13 @@ def _unassign_users_from_location(domain, location_id):
     """
     Unset location for all users assigned to that location.
     """
-    from corehq.apps.locations.dbaccessors import get_all_users_by_location
-    for user in get_all_users_by_location(domain, location_id):
+    from corehq.apps.locations.dbaccessors import user_ids_at_locations
+    from corehq.apps.users.models import CommCareUser
+    from dimagi.utils.couch.database import iter_docs
+
+    user_ids = user_ids_at_locations([location_id])
+    for doc in iter_docs(CommCareUser.get_db(), user_ids):
+        user = CommCareUser.wrap(doc)
         if user.is_web_user():
             user.unset_location_by_id(domain, location_id, fall_back_to_next=True)
         elif user.is_commcare_user():
