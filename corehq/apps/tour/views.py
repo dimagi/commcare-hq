ee0458d022c5c159540877f2ad24588e1b22ad80
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-07 16:57:03 -0500

    python-modernize corehq/apps/tour/ -w --future-unicode

--- corehq/apps/tour/views.py
+++ corehq/apps/tour/views.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.generic import View
