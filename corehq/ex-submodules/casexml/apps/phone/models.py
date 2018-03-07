0aa3d722feebf161a836ecf4822543a880d2a152
Sravan Reddy <sravfeyn@gmail.com>
2018-03-07 09:00:03 +0530

    import

--- corehq/ex-submodules/casexml/apps/phone/models.py
+++ corehq/ex-submodules/casexml/apps/phone/models.py
@@ -19,6 +19,7 @@ from django.core.exceptions import ValidationError
 from memoized import memoized
 from dimagi.utils.mixins import UnicodeMixIn
 from dimagi.utils.couch import LooselyEqualDocumentSchema
+from dimagi.utils.logging import notify_exception
 from casexml.apps.case import const
 from casexml.apps.case.sharedmodels import CommCareCaseIndex, IndexHoldingMixIn
 from casexml.apps.phone.checksum import Checksum, CaseStateHash
