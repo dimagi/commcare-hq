879c3eaf0c1db5be16d115e944ca8bfc6f9ec0cc
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-07 20:30:54 -0500

    use py3 compatible syntax

--- corehq/util/tests/test_view_utils.py
+++ corehq/util/tests/test_view_utils.py
@@ -14,13 +14,13 @@ def my_view(request, err):
 class NotifyExceptionTest(SimpleTestCase):
 
     def test_notify_exception_utf8(self):
-        err = ValueError(b'βªđ ṿåƚŭę')
+        err = ValueError('βªđ ṿåƚŭę'.encode('utf-8'))
         with patch('corehq.util.view_utils.notify_exception') as notify_exception_patch:
             my_view('foo', err)
-            notify_exception_patch.assert_called_with('foo', b'JSON exception response: βªđ ṿåƚŭę')
+            notify_exception_patch.assert_called_with('foo', 'JSON exception response: βªđ ṿåƚŭę'.encode('utf-8'))
 
     def test_notify_exception_unicode(self):
         err = ValueError('βªđ ṿåƚŭę')
         with patch('corehq.util.view_utils.notify_exception') as notify_exception_patch:
             my_view('foo', err)
-            notify_exception_patch.assert_called_with('foo', b'JSON exception response: βªđ ṿåƚŭę')
+            notify_exception_patch.assert_called_with('foo', 'JSON exception response: βªđ ṿåƚŭę'.encode('utf-8'))
