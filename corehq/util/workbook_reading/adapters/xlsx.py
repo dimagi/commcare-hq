4af4f74e6da581f04be1d6689ad2681b2b7ff39d
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-07 19:56:28 -0500

    fix another test

--- corehq/util/workbook_reading/adapters/xlsx.py
+++ corehq/util/workbook_reading/adapters/xlsx.py
@@ -15,7 +15,7 @@ from corehq.util.workbook_reading import Worksheet, Cell, Workbook, \
 #     a container format used for document by older versions of Microsoft Office.
 #     It is however an open format used by other programs as well.
 # Also checked that it's not used non-encrypted xlsx files, which are just .zip files
-XLSX_ENCRYPTED_MARKER = '\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1'
+XLSX_ENCRYPTED_MARKER = b'\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1'
 
 
 @contextmanager
