3a900068a92a21b0445bfe43f966d39c94701723
Norman Hooper <nhooper@dimagi.com>
2018-03-12 13:28:32 +0200

    Update utils tests

--- corehq/motech/tests/test_utils.py
+++ corehq/motech/tests/test_utils.py
@@ -1,7 +1,28 @@
+# coding=utf-8
 from __future__ import absolute_import
+from __future__ import unicode_literals
 import doctest
 from django.test import SimpleTestCase
 import corehq.motech.utils
+from corehq.motech.utils import pad
+
+
+class PadTests(SimpleTestCase):
+
+    def test_assertion(self):
+        with self.assertRaises(AssertionError):
+            pad(u'xyzzy', 8, b'*')
+
+    def test_ascii_bytestring_default_char(self):
+        padded = pad(b'xyzzy', 8)
+        self.assertEqual(padded, b'xyzzy   ')
+
+    def test_nonascii(self):
+        """
+        pad should pad a string according to its size in bytes, not its length in letters.
+        """
+        padded = pad(b'xyźźy', 8, b'*')
+        self.assertEqual(padded, b'xyźźy*')
 
 
 class DocTests(SimpleTestCase):
