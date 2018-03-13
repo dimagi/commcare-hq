910f65972eb563c455e1f43f3b1c856bbd0043dd
Simon Kelly <skelly@dimagi.com>
2018-03-13 11:23:42 +0200

    add system check to ensure DB tables queryable

--- corehq/sql_db/__init__.py
+++ corehq/sql_db/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from django.apps import AppConfig
 from django.conf import settings
-from django.core.checks import Error, register
+from django.core.checks import Error, register, Tags
 
 
 class SQLDBAppConfig(AppConfig):
@@ -32,4 +32,33 @@ def custom_db_checks(app_configs, **kwargs):
                       'settings.USE_PARTITIONED_DATABASE must be True if '
                       'settings.{} is set to a custom database.'.format(setting))
             )
+    return errors
+
+
+@register(Tags.database, deploy=True)
+def check_db_tables(app_configs, **kwargs):
+    errors = []
+    from django.apps import apps
+    from corehq.sql_db.models import PartitionedModel
+    from corehq.sql_db.util import get_db_aliases_for_partitioned_query
+
+    def _check_model(model_class, using=None):
+        try:
+            model_class._default_manager.using(using).all().exists()
+        except Exception as e:
+            return Error('Error querying model on database "{}": "{}.{}": {}.{}({})'.format(
+                using or 'default',
+                model_class._meta.app_label, model_class.__name__,
+                e.__class__.__module__, e.__class__.__name__,
+                e
+            ))
+
+    for model in apps.get_models():
+        if issubclass(model, PartitionedModel):
+            for db in get_db_aliases_for_partitioned_query():
+                error = _check_model(model, using=db)
+                error and errors.append(error)
+        else:
+            error = _check_model(model)
+            error and errors.append(error)
     return errors
