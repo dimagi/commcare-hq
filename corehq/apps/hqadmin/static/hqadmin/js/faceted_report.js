23ce0f26e1532ee747bd1485dc6239ae9aa3ebbb
Jordan Roth <jmtroth0@gmail.com>
2018-03-13 16:53:54 -0400

    first pass externalizing javascript in faceted_report

---
+++ corehq/apps/hqadmin/static/hqadmin/js/faceted_report.js
@@ -0,0 +1,4 @@
+hqDefine('hqadmin/js/faceted_report', function() {
+    var standardHQReport = hqImport("reports/js/standard_hq_report").getStandardHQReport(),
+        asyncHQReport = hqImport("reports/js/standard_hq_report").getAsyncHQReport();
+});
