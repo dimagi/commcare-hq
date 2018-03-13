2f61113c45b4a6c0a96e132cbdf1985ab4ca278d
Jordan Roth <jmtroth0@gmail.com>
2018-03-13 16:58:25 -0400

    document ready handler

--- corehq/apps/hqadmin/static/hqadmin/js/faceted_report.js
+++ corehq/apps/hqadmin/static/hqadmin/js/faceted_report.js
@@ -1,4 +1,13 @@
 hqDefine('hqadmin/js/faceted_report', function() {
-    var standardHQReport = hqImport("reports/js/standard_hq_report").getStandardHQReport(),
+    var standardHQReport, asyncHQReport;
+
+    $(function() {
+        standardHQReport = hqImport("reports/js/standard_hq_report").getStandardHQReport();
         asyncHQReport = hqImport("reports/js/standard_hq_report").getAsyncHQReport();
+    });
+
+    return {
+        standardHQReport: standardHQReport,
+        asyncHQReport: asyncHQReport,
+    };
 });
