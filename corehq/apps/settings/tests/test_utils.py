f42ef81e7a5ded02b3fee5c183fd6f4d6df95f35
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-13 17:03:41 -0400

    python-modernize corehq/apps/settings/ -w --future-unicode

--- corehq/apps/settings/tests/test_utils.py
+++ corehq/apps/settings/tests/test_utils.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 import os
 from django.test import SimpleTestCase
 from corehq.apps.settings.utils import get_temp_file
