e4e37fb8550412064805ca68902e0b8375f70e66
Jordan Roth <jmtroth0@gmail.com>
2018-03-14 14:45:30 -0400

    initial page data

--- corehq/apps/domain/static/domain/js/case_search.js
+++ corehq/apps/domain/static/domain/js/case_search.js
@@ -1,9 +1,10 @@
 hqDefine('domain/js/case_search', function() {
+var initialPageData = hqImport('hqwebapp/js/initial_page_data');
      (function () {
          var CaseSearchConfig = hqImport('domain/js/case-search-config').CaseSearchConfig;
          var viewModel = new CaseSearchConfig({
-             values: {{ values|JSON }},
-             caseTypes: {{ case_types|JSON }},
+             values: initialPageData.get('values|JSON'),
+             caseTypes: initialPageData.get('case_types|JSON'),
          });
          $('#case-search-config').koApplyBindings(viewModel);
          $('#case-search-config').on('change', viewModel.change).on('click', ':button', viewModel.change);
