75ee55944f4691643184bc1d6393f2d53fe4c742
Jordan Roth <jmtroth0@gmail.com>
2018-03-14 10:56:23 -0400

    cleanup

--- corehq/apps/notifications/static/notifications/js/manage_notifications.js
+++ corehq/apps/notifications/static/notifications/js/manage_notifications.js
@@ -1,8 +1,8 @@
 hqDefine('notifications/js/manage_notifications', function() {
-var initialPageData = hqImport('hqwebapp/js/initial_page_data');
-$(function () {
-    $('#ko-alert-container').koApplyBindings({
-        alerts: initialPageData.get('alerts|JSON')
+    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
+    $(function () {
+        $('#ko-alert-container').koApplyBindings({
+            alerts: initialPageData.get('alerts'),
+        });
     });
-});
 });
