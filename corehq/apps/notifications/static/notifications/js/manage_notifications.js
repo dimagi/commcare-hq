360d3eac6a9c54fbfb8c170539268e9c24eba46c
Jordan Roth <jmtroth0@gmail.com>
2018-03-14 10:32:52 -0400

    initial page data

--- corehq/apps/notifications/static/notifications/js/manage_notifications.js
+++ corehq/apps/notifications/static/notifications/js/manage_notifications.js
@@ -1,7 +1,8 @@
 hqDefine('notifications/js/manage_notifications', function() {
+var initialPageData = hqImport('hqwebapp/js/initial_page_data');
 $(function () {
     $('#ko-alert-container').koApplyBindings({
-        alerts: {{ alerts|JSON }}
+        alerts: initialPageData.get('alerts|JSON')
     });
 });
 });
