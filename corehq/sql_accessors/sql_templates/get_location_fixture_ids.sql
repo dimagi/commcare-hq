857f8adbc753db745aa2d65d403952475b5bb57d
Daniel Miller <dmiller@dimagi.com>
2018-03-14 10:43:43 -0400

    JOIN instead of IN subquery for efficiency
    Also use shorter table aliases for readability.

--- corehq/sql_accessors/sql_templates/get_location_fixture_ids.sql
+++ corehq/sql_accessors/sql_templates/get_location_fixture_ids.sql
@@ -15,43 +15,41 @@ BEGIN
     WITH RECURSIVE expand_to AS (
         WITH RECURSIVE cte AS (
             SELECT
-                "locations_locationtype"."parent_type_id",
+                expand_to_type."parent_type_id",
                 0 AS "depth",
-                "locations_locationtype"."id" AS "expand_to_type"
-            FROM "locations_locationtype"
-            WHERE "locations_locationtype"."id" IN (
-                SELECT U1."expand_to_id"
-                FROM "locations_sqllocation" U0
-                INNER JOIN "locations_locationtype" U1 ON U0."location_type_id" = U1."id"
-                WHERE
-                    U0."id" = ANY(user_location_ids_array)
-                    AND U1."expand_to_id" IS NOT NULL
-            )
+                expand_to_type."id" AS "expand_to_type"
+            FROM "locations_sqllocation" loc
+            INNER JOIN "locations_locationtype" loc_type
+                ON loc."location_type_id" = loc_type."id"
+            INNER JOIN "locations_locationtype" expand_to_type
+                ON expand_to_type."id" = loc_type."expand_to_id"
+            WHERE
+                loc."id" = ANY(user_location_ids_array)
+                AND loc_type."expand_to_id" IS NOT NULL
 
             UNION ALL
 
             SELECT
-                "locations_locationtype"."parent_type_id",
+                iwe_type."parent_type_id",
                 0 AS "depth",
                 -1 AS "expand_to_type"
-            FROM "locations_locationtype"
-            WHERE "locations_locationtype"."id" IN (
-                SELECT U1."include_without_expanding_id"
-                FROM "locations_sqllocation" U0
-                INNER JOIN "locations_locationtype" U1 ON U0."location_type_id" = U1."id"
-                WHERE
-                    U0."id" = ANY(user_location_ids_array)
-                    AND U1."include_without_expanding_id" IS NOT NULL
-            )
+            FROM "locations_sqllocation" loc
+            INNER JOIN "locations_locationtype" loc_type
+                ON loc."location_type_id" = loc_type."id"
+            INNER JOIN "locations_locationtype" iwe_type
+                ON loc_type."include_without_expanding_id" = iwe_type."id"
+            WHERE
+                loc."id" = ANY(user_location_ids_array)
+                AND loc_type."include_without_expanding_id" IS NOT NULL
 
             UNION ALL
 
             SELECT
-                "locations_locationtype"."parent_type_id",
+                loc_type."parent_type_id",
                 "cte"."depth" + 1 AS "depth",
                 "cte"."expand_to_type" AS "expand_to_type"
-            FROM "locations_locationtype", "cte"
-            WHERE "locations_locationtype"."id" = "cte"."parent_type_id"
+            FROM "locations_locationtype" loc_type
+            INNER JOIN "cte" ON loc_type."id" = "cte"."parent_type_id"
         )
 
         SELECT
@@ -74,70 +72,67 @@ BEGIN
             UNION ALL
 
             SELECT
-                "locations_sqllocation"."parent_id",
+                loc."parent_id",
                 CASE
                     WHEN (
-                        "locations_locationtype"."expand_from_root" = FALSE
-                        AND "locations_locationtype"."expand_from" IS NOT NULL
+                        loc_type."expand_from_root" = FALSE
+                        AND loc_type."expand_from" IS NOT NULL
                         AND NOT (
-                            "locations_locationtype"."expand_from" = "locations_sqllocation"."location_type_id"
-                            AND "locations_locationtype"."expand_from" IS NOT NULL
+                            loc_type."expand_from" = loc."location_type_id"
+                            AND loc_type."expand_from" IS NOT NULL
                         )
                         AND NOT EXISTS (
                             SELECT 1
                             FROM "locations_locationtype" U0
                             INNER JOIN "locations_locationtype_include_only" U1 ON U0."id" = U1."to_locationtype_id"
-                            WHERE U1."from_locationtype_id" = "locations_sqllocation"."location_type_id"
+                            WHERE U1."from_locationtype_id" = loc."location_type_id"
                         )
                     )
-                    THEN "locations_locationtype"."expand_from"
+                    THEN loc_type."expand_from"
                     ELSE NULL
                 END AS "expand_from_type",
                 CASE
-                    WHEN "locations_locationtype"."expand_from_root" = TRUE THEN NULL
-                    ELSE "locations_sqllocation"."id"
+                    WHEN loc_type."expand_from_root" = TRUE THEN NULL
+                    ELSE loc."id"
                 END AS "loc_id",
                 CASE
-                    WHEN "locations_locationtype"."expand_to_id" IS NOT NULL THEN (
+                    WHEN loc_type."expand_to_id" IS NOT NULL THEN (
                         SELECT U0."expand_to_depth"
                         FROM "expand_to" U0
-                        WHERE U0."expand_to_type" = "locations_locationtype"."expand_to_id"
+                        WHERE U0."expand_to_type" = loc_type."expand_to_id"
                     )
                     WHEN EXISTS (
                         SELECT 1
                         FROM "locations_locationtype" U0
-                        INNER JOIN "locations_locationtype_include_only" U1 ON U0."id" = U1."to_locationtype_id"
-                        WHERE U1."from_locationtype_id" = "locations_sqllocation"."location_type_id"
+                        INNER JOIN "locations_locationtype_include_only" U1
+                            ON U0."id" = U1."to_locationtype_id"
+                        WHERE U1."from_locationtype_id" = loc."location_type_id"
                     )
                     THEN -3
                     ELSE -2
                 END AS "depth"
-            FROM "locations_sqllocation"
-            INNER JOIN "locations_locationtype" ON "locations_sqllocation"."location_type_id" = "locations_locationtype"."id"
+            FROM "locations_sqllocation" loc
+            INNER JOIN "locations_locationtype" loc_type ON loc."location_type_id" = loc_type."id"
             WHERE
-                "locations_sqllocation"."is_archived" = FALSE
-                AND "locations_sqllocation"."domain" = domain_name
-                AND "locations_sqllocation"."id" IN (
-                    SELECT U0."id"
-                    FROM "locations_sqllocation" U0
-                    WHERE U0."id" = ANY(user_location_ids_array)
-                )
+                loc."is_archived" = FALSE
+                AND loc."domain" = domain_name
+                AND loc."id" = ANY(user_location_ids_array)
 
             UNION ALL
 
             SELECT
-                "locations_sqllocation"."parent_id",
+                loc."parent_id",
                 CASE
                     WHEN (
                         "cte"."expand_from_type" IS NOT NULL
-                        AND "cte"."expand_from_type" <> "locations_sqllocation"."location_type_id"
+                        AND "cte"."expand_from_type" <> loc."location_type_id"
                     )
                     THEN "cte"."expand_from_type"
                     ELSE NULL
                 END AS "expand_from_type",
                 CASE
                     WHEN "cte"."loc_id" IS NULL THEN NULL
-                    ELSE "locations_sqllocation"."id"
+                    ELSE loc."id"
                 END AS "loc_id",
                 CASE
                     WHEN (
@@ -147,25 +142,24 @@ BEGIN
                     THEN -1
                     ELSE "cte"."depth"
                 END AS "depth"
-            FROM "locations_sqllocation", "cte"
-            WHERE
-                "locations_sqllocation"."is_archived" = FALSE
-                AND "locations_sqllocation"."id" = "cte"."parent_id"
+            FROM "locations_sqllocation" loc
+            INNER JOIN "cte" ON loc."id" = "cte"."parent_id"
+            WHERE loc."is_archived" = FALSE
         )
 
         SELECT DISTINCT "cte"."loc_id", "cte"."depth" FROM "cte"
 
     ), fixture_ids AS (
         SELECT
-            "locations_sqllocation"."id",
-            "locations_sqllocation"."parent_id",
-            Array ["locations_sqllocation"."id"] AS "path",
+            loc."id",
+            loc."parent_id",
+            ARRAY[loc."id"] AS "path",
             0 AS "depth"
-        FROM "locations_sqllocation"
+        FROM "locations_sqllocation" loc
         WHERE
-            "locations_sqllocation"."is_archived" = FALSE
-            AND "locations_sqllocation"."domain" = domain_name
-            AND "locations_sqllocation"."parent_id" IS NULL
+            loc."is_archived" = FALSE
+            AND loc."domain" = domain_name
+            AND loc."parent_id" IS NULL
             AND EXISTS (
                 SELECT 1
                 FROM "expand_from" U0
@@ -176,29 +170,27 @@ BEGIN
                             OR U0."depth" = -2
                             OR U0."depth" >= 0
                         )
-                        AND U0."loc_id" = "locations_sqllocation"."id"
+                        AND U0."loc_id" = loc."id"
                     ) OR (
                         (
                             U0."depth" = -2
                             OR U0."depth" >= 0
                         ) AND (
                             U0."loc_id" IS NULL
-                            OR U0."loc_id" = "locations_sqllocation"."id"
+                            OR U0."loc_id" = loc."id"
                         )
                     ) OR (
                         (
-                            U0."loc_id" = "locations_sqllocation"."id"
-                            OR U0."loc_id" = "locations_sqllocation"."id"
+                            U0."loc_id" = loc."id"
+                            OR U0."loc_id" = loc."id"
                         )
                         AND U0."depth" = -3
-                        AND "locations_sqllocation"."location_type_id" IN (
+                        AND loc."location_type_id" IN (
                             SELECT to_locationtype_id
                             FROM locations_locationtype_include_only
-                            WHERE from_locationtype_id IN (
-                                SELECT "locations_sqllocation"."location_type_id"
-                                FROM "locations_sqllocation"
-                                WHERE "locations_sqllocation"."id" = ANY(user_location_ids_array)
-                            )
+                            INNER JOIN "locations_sqllocation" x
+                                ON x."location_type_id" = from_locationtype_id
+                            WHERE x."id" = ANY(user_location_ids_array)
                         )
                     )
             )
@@ -206,15 +198,15 @@ BEGIN
         UNION ALL
 
         SELECT
-            "locations_sqllocation"."id",
-            "locations_sqllocation"."parent_id",
-            array_append("fixture_ids"."path", "locations_sqllocation"."id") AS "path",
+            loc."id",
+            loc."parent_id",
+            array_append("fixture_ids"."path", loc."id") AS "path",
             "fixture_ids"."depth" + 1 AS "depth"
-        FROM "locations_sqllocation", "fixture_ids"
+        FROM "locations_sqllocation" loc
+        INNER JOIN "fixture_ids" ON loc."parent_id" = "fixture_ids"."id"
         WHERE
-            "locations_sqllocation"."is_archived" = FALSE
-            AND "locations_sqllocation"."domain" = domain_name
-            AND "locations_sqllocation"."parent_id" = "fixture_ids"."id"
+            loc."is_archived" = FALSE
+            AND loc."domain" = domain_name
             AND EXISTS (
                 SELECT 1
                 FROM "expand_from" U0
@@ -225,7 +217,7 @@ BEGIN
                             OR U0."depth" = -2
                             OR U0."depth" >= "fixture_ids"."depth" + 1
                         )
-                        AND U0."loc_id" = "locations_sqllocation"."id"
+                        AND U0."loc_id" = loc."id"
                     ) OR (
                         (
                             U0."depth" = -2
@@ -237,17 +229,15 @@ BEGIN
                     ) OR (
                         U0."depth" = -3 AND
                         (
-                            U0."loc_id" = "locations_sqllocation"."id"
+                            U0."loc_id" = loc."id"
                             OR U0."loc_id" = ANY("fixture_ids"."path")
                         )
-                        AND "locations_sqllocation"."location_type_id" IN (
+                        AND loc."location_type_id" IN (
                             SELECT to_locationtype_id
                             FROM locations_locationtype_include_only
-                            WHERE from_locationtype_id IN (
-                                SELECT "locations_sqllocation"."location_type_id"
-                                FROM "locations_sqllocation"
-                                WHERE "locations_sqllocation"."id" = ANY(user_location_ids_array)
-                            )
+                            INNER JOIN "locations_sqllocation" x
+                                ON x."location_type_id" = from_locationtype_id
+                            WHERE x."id" = ANY(user_location_ids_array)
                         )
                     )
             )
