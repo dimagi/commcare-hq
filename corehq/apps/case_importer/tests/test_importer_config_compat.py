d68ba9da7a0d22f0bfc37b72fd9d29ac8238c44b
Nick Pellegrino <npellegrino@dimagi.com>
2018-03-14 18:28:48 -0400

    python-modernize corehq/apps/case_importer/ -w --future-unicode

--- corehq/apps/case_importer/tests/test_importer_config_compat.py
+++ corehq/apps/case_importer/tests/test_importer_config_compat.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.test import SimpleTestCase
 import pickle
 from corehq.apps.case_importer.util import ImporterConfig
--- corehq/apps/case_importer/tracking/models.py
+++ corehq/apps/case_importer/tracking/models.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.db import models
 from jsonfield import JSONField
 from corehq.apps.case_importer.tracking.task_status import TaskStatus, \
