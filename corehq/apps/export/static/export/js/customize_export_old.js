83e1ee8d38b9fd36cfd33f089d73821acc50f368
Jordan Roth <jmtroth0@gmail.com>
2018-03-14 11:10:02 -0400

    initial page data

--- corehq/apps/export/static/export/js/customize_export_old.js
+++ corehq/apps/export/static/export/js/customize_export_old.js
@@ -1,28 +1,29 @@
 hqDefine('export/js/customize_export_old', function() {
+var initialPageData = hqImport('hqwebapp/js/initial_page_data');
         $(function () {
             var translations = {
-                forms: "{% trans 'Forms'|escapejs %}",
-                repeat: "{% trans 'Repeat: '|escapejs %}",
-                cases: "{% trans 'Cases'|escapejs %}",
-                case_history: "{% trans 'Case History'|escapejs %}",
-                history_to_parents: "{% trans 'Case History > Parent Cases'|escapejs %}",
-                parent_cases: "{% trans 'Parent Cases'|escapejs %}"
+                forms: gettext('Forms'|escapejs),
+                repeat: gettext('Repeat: '|escapejs),
+                cases: gettext('Cases'|escapejs),
+                case_history: gettext('Case History'|escapejs),
+                history_to_parents: gettext('Case History > Parent Cases'|escapejs),
+                parent_cases: gettext('Parent Cases'|escapejs)
             };
             var customExportView = CustomExportView.wrap({
-                export_type: {{ custom_export.type|JSON }},
-                custom_export: {{ custom_export|JSON }},
-                table_configuration: {{ table_configuration|JSON }},
-                presave: {{ presave|JSON }},
-                export_stock: {{ export_stock|JSON }},
-                deid_options: {{ deid_options|JSON }},
-                column_type_options: {{ column_type_options|JSON }},
+                export_type: initialPageData.get('custom_export.type|JSON'),
+                custom_export: initialPageData.get('custom_export|JSON'),
+                table_configuration: initialPageData.get('table_configuration|JSON'),
+                presave: initialPageData.get('presave|JSON'),
+                export_stock: initialPageData.get('export_stock|JSON'),
+                deid_options: initialPageData.get('deid_options|JSON'),
+                column_type_options: initialPageData.get('column_type_options|JSON'),
                 urls: {
-                    save: {{ request.get_full_path|safe|JSON }}
+                    save: initialPageData.get('request.get_full_path|safe|JSON')
                 },
-                allow_repeats: {{ helper.allow_repeats|JSON }},
-                default_order: {{ default_order|JSON }},
-                minimal: {{ minimal|JSON }}
-            }, translations, {{ has_excel_dashboard_access|JSON }});
+                allow_repeats: initialPageData.get('helper.allow_repeats|JSON'),
+                default_order: initialPageData.get('default_order|JSON'),
+                minimal: initialPageData.get('minimal|JSON')
+            }, translations, initialPageData.get('has_excel_dashboard_access|JSON'));
 
             var $export = $('#customize-export');
             $export.koApplyBindings(customExportView);
