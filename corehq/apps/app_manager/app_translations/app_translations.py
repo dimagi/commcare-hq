8b3fbbac1604af11f41098583b5578e097436509
Jennifer Schweers <orange.jenny@gmail.com>
2018-03-19 12:35:38 -0400

    Add conditional ID mapping columns to bulk translation download

--- corehq/apps/app_manager/app_translations/app_translations.py
+++ corehq/apps/app_manager/app_translations/app_translations.py
@@ -295,7 +295,7 @@ def expected_bulk_app_sheet_rows(app):
                 for detail in case_properties:
 
                     field_name = detail.field
-                    if detail.format == "enum":
+                    if detail.format in ["enum", "conditional-enum"]:
                         field_name += " (ID Mapping Text)"
                     elif detail.format == "graph":
                         field_name += " (graph)"
@@ -307,7 +307,7 @@ def expected_bulk_app_sheet_rows(app):
                     )
 
                     # Add a row for any mapping pairs
-                    if detail.format == "enum":
+                    if detail.format in ["enum", "conditional-enum"]:
                         for mapping in detail.enum:
                             rows[module_string].append(
                                 (
