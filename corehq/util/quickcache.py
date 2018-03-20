0789566b578fecf83ed72c227af4d990d3b91450
Daniel Roberts <droberts@dimagi.com>
2018-03-20 10:39:26 -0700

    use quickcache session_function
    to keep local cache from being shared between requests/tasks

--- corehq/util/quickcache.py
+++ corehq/util/quickcache.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 import warnings
 from quickcache.django_quickcache import get_django_quickcache
-
+from celery._state import get_current_task
+from corehq.util.global_request import get_request
 
 from corehq.util.soft_assert import soft_assert
 
@@ -12,8 +13,22 @@ quickcache_soft_assert = soft_assert(
     skip_frames=5,
 )
 
+
+def get_session_key():
+    """
+    returns a 7 character string that varies with the current "session" (task or request)
+    """
+    session = get_current_task() or get_request()
+    if session:
+        return hex(id(session))[2:9]
+    else:
+        # arbitrary 7-letter that is clearly not from a session
+        return 'xxxxxxx'
+
+
 quickcache = get_django_quickcache(timeout=5 * 60, memoize_timeout=10,
-                                   assert_function=quickcache_soft_assert)
+                                   assert_function=quickcache_soft_assert,
+                                   session_function=get_session_key)
 
 
 def skippable_quickcache(*args, **kwargs):
