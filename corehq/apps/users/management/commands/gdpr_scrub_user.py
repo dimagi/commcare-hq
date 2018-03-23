1806e29ba5827615de4ac845a8e9a2b775b97187
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-03-23 12:28:34 -0400

    Refactor code to separate out both replacements

--- corehq/apps/users/management/commands/gdpr_scrub_user.py
+++ corehq/apps/users/management/commands/gdpr_scrub_user.py
@@ -17,23 +17,31 @@ class Command(BaseCommand):
         DOMAIN = 'test-proj-2'
 
         # Replace the username in the XML
+        user_id = "0a286c0eb864a382a85974336f9dad09"
         this_form_accessor = FormAccessors(domain=DOMAIN)
         form_ids = this_form_accessor.get_form_ids_for_user(user_id)
+        new_username = "Deleted username success"
         for form_data in this_form_accessor.iter_forms(form_ids):
-
-            form_attachment_xml = form_data.get_attachment("form.xml")
-            form_attachment_dict = xmltodict.parse(form_attachment_xml)
-
-            # replace the old username with the new username
-            form_attachment_dict["data"]["n0:meta"]["n0:username"] = "NEW USERNAME"
-
-            # convert the dict back to xml
-            form_attachment_xml_new = xmltodict.unparse(form_attachment_dict)
-
-            # TODO: Save the new xml to the database
+            self.replace_username_in_xml(form_data, new_username)
+            self.replace_username_in_metadata()
 
 
-        # Replace the username in the form metadata
+
+
+    def replace_username_in_xml(self, form_data, new_username):
+        form_attachment_xml = form_data.get_attachment("form.xml")
+        form_attachment_dict = xmltodict.parse(form_attachment_xml)
+
+        # replace the old username with the new username
+        form_attachment_dict["data"]["n0:meta"]["n0:username"] = new_username
+
+        # convert the dict back to xml
+        form_attachment_xml_new = xmltodict.unparse(form_attachment_dict)
+
+        # TODO: Save the new xml to the database
+
+    def replace_username_in_metadata(self):
+        pass
 
 
 if __name__=="__main__":
