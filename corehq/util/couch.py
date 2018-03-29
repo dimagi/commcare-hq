c83076b75cd2f65a369b53e11d1ab4399e2ffb61
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-03-29 17:07:40 -0400

    Fix _iter_update to handle doc or dict inputs

--- corehq/util/couch.py
+++ corehq/util/couch.py
@@ -311,6 +311,13 @@ def iter_update(db, fn, ids, max_retries=3, verbose=False):
     IterResults = namedtuple('IterResults', fields)
     results = IterResults(set(), set(), set(), set(), set())
 
+    def _get_updated_doc_id(doc_update):
+        if isinstance(doc_update.doc, dict):
+            updated_doc_id = doc_update.doc.get('_id')
+        else:
+            updated_doc_id = doc_update.doc._id
+        return updated_doc_id
+
     def _iter_update(doc_ids, try_num):
         with IterDB(db, chunksize=100) as iter_db:
             for chunk in chunked(set(doc_ids), 100):
@@ -322,14 +329,10 @@ def iter_update(db, fn, ids, max_retries=3, verbose=False):
                     else:
                         # copy the dictionary so we can tell if it changed
                         doc_update = fn(deepcopy(raw_doc))
-                        if isinstance(doc_update.doc, dict):
-                            updated_doc_id = doc_update.doc.get('_id')
-                        else:
-                            updated_doc_id = doc_update.doc._id
                         if doc_update is None:
                             results.ignored_ids.add(doc_id)
                         elif (not isinstance(doc_update, DocUpdate)
-                              or updated_doc_id != doc_id):
+                              or _get_updated_doc_id(doc_update) != doc_id):
                             results.error_ids.add(doc_id)
                         elif doc_update.delete:
                             iter_db.delete(raw_doc)
