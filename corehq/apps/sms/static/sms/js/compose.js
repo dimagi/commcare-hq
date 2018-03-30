699b1278a971ee70c269ef794f3d33ef7927d724
Jennifer Schweers <orange.jenny@gmail.com>
2018-03-30 10:13:34 -0400

    lint

--- corehq/apps/sms/static/sms/js/compose.js
+++ corehq/apps/sms/static/sms/js/compose.js
@@ -3,20 +3,22 @@ hqDefine("sms/js/compose", function() {
         $("#hint_id_recipients").addClass("alert alert-info");
         $("#hint_id_message").addClass("alert alert-info");
         $("#id_message").on('keyup', function () {
-            var max_char = 160,
-                    current_count = $(this).val().length,
-                    cc_message = "";
-            var chars_remaining = max_char - current_count;
-            var $char_count = $("#hint_id_message");
-            if(chars_remaining < 0)
-                $char_count.addClass("alert-danger").removeClass('alert-info');
-            else
-                $char_count.removeClass("alert-danger").addClass('alert-info');
-            cc_message = current_count + " character";
-            if(current_count != 1)
-                cc_message = cc_message + "s";
-            cc_message = cc_message + " (" + max_char + " max)";
-            $char_count.text(cc_message);
+            var maxChar = 160,
+                currentCount = $(this).val().length,
+                message = "";
+            var charsRemaining = maxChar - currentCount;
+            var $charCount = $("#hint_id_message");
+            if (charsRemaining < 0) {
+                $charCount.addClass("alert-danger").removeClass('alert-info');
+            } else {
+                $charCount.removeClass("alert-danger").addClass('alert-info');
+            }
+            message = currentCount + " character";
+            if (currentCount !== 1) {
+                message = message + "s";
+            }
+            message = message + " (" + maxChar + " max)";
+            $charCount.text(message);
         });
 
         $('.sms-typeahead').multiTypeahead({
