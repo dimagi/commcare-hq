773e18e038c3712a8c44d01a62260251e02b6d42
Manish Kangia <mkangia@dimagi.com>
2018-04-01 01:07:14 +0530

    drug voucher details fixes [skip ci]

--- custom/enikshay/management/commands/data_dumps_private_drug_voucher_details.py
+++ custom/enikshay/management/commands/data_dumps_private_drug_voucher_details.py
@@ -4,6 +4,7 @@ from __future__ import (
     unicode_literals,
 )
 
+from corehq.apps.locations.models import SQLLocation
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 
 from custom.enikshay.case_utils import (
@@ -16,7 +17,7 @@ from custom.enikshay.case_utils import (
 )
 
 from custom.enikshay.const import ENROLLED_IN_PRIVATE
-from custom.enikshay.management.commands.base_data_dump import BaseDataDump
+from custom.enikshay.management.commands.base_data_dump import BaseDataDump, PRIVATE_SECTOR_ID_MAPPING
 
 DOMAIN = "enikshay"
 
@@ -41,6 +42,27 @@ class Command(BaseDataDump):
         elif column_name == "eNikshay episode UUID":
             episode_case = self.get_episode(case)
             return episode_case.case_id
+        elif column_name == "Organisation":
+            owner_id = self.get_person(case).owner_id
+            location = SQLLocation.active_objects.get_or_None(location_id=owner_id)
+            if location:
+                private_sector_org_id = location.metadata.get('private_sector_org_id')
+                if private_sector_org_id:
+                    return PRIVATE_SECTOR_ID_MAPPING.get(private_sector_org_id, private_sector_org_id)
+                else:
+                    raise Exception("Private Sector Organization ID not set for location %s" % owner_id)
+            else:
+                raise Exception("Location not found for id %s" % owner_id)
+        elif column_name == "Voucher?":
+            voucher_case = self.get_voucher(case, raise_if_none=False)
+            if voucher_case:
+                return 'Yes'
+            else:
+                return 'No'
+        elif column_name == "Voucher UUID":
+            return self.get_voucher(case).case_id
+        elif column_name == "Prescription UUID":
+            return case.case_id
         raise Exception("unknown custom column %s" % column_name)
 
     def get_case_ids_query(self, case_type):
@@ -62,7 +84,7 @@ class Command(BaseDataDump):
             self.person_belongs_to_real_location(person)
         )
 
-    def get_voucher(self, prescription_case):
+    def get_voucher(self, prescription_case, raise_if_none=True):
         if 'voucher' not in self.context:
             voucher_cases = self.case_accessor.get_reverse_indexed_cases(
                 [prescription_case.case_id], case_types=[CASE_TYPE_VOUCHER])
@@ -72,8 +94,9 @@ class Command(BaseDataDump):
                 self.context['voucher'] = voucher_cases[0]
             else:
                 self.context['voucher'] = None
-        if not self.context['voucher']:
-            raise Exception("No voucher cases found for prescription %s" % prescription_case.case_id)
+        if raise_if_none:
+            if not self.context['voucher']:
+                raise Exception("No voucher cases found for prescription %s" % prescription_case.case_id)
         return self.context['voucher']
 
     def get_person(self, prescription_case):
