1c9fbb742b11ea730012f6b744a29a1710967579
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-03 13:55:21 -0400

    Split out edit_submission.js

--- corehq/apps/reports/static/reports/js/edit_submission.js
+++ corehq/apps/reports/static/reports/js/edit_submission.js
@@ -1,2 +1,26 @@
 hqDefine("reports/js/edit_submission", function() {
+    $(function () {
+        var initialPageData = hqImport("hqwebapp/js/initial_page_data").get;
+        GMAPS_API_KEY = initialPageData('maps_api_key'); // maps api is a global variable depended on by touchforms
+        var edit_context = initialPageData("edit_context");
+        var uses_sql_backend = initialPageData("use_sqlite_backend");
+        var username = initialPageData("username");
+        var edit_formplayer = initialPageData("edit_formplayer");
+        var restoreAs = edit_context.sessionData.username;
+        $('#edit-container').inlineFormplayer({
+            formUrl: edit_context.formUrl,
+            submitUrl: edit_context.submitUrl,
+            sessionData: edit_context.sessionData,
+            uses_sql_backend: uses_sql_backend,
+            formplayerEnabled: edit_formplayer,
+            domain: edit_context.domain,
+            username: username,
+            restoreAs: restoreAs,
+            onsubmit: function () {
+                window.location.href = edit_context.returnUrl;
+            },
+            onload: function () {
+            }
+        });
+    });
 });
