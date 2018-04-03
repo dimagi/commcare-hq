ff877655e84c30f0b5bb332243c8197efc939a57
Jenny Schweers <orange.jenny@gmail.com>
2018-04-03 08:28:41 -0400

    Fix spacing on chat contacts button

--- corehq/apps/sms/static/sms/js/chat_contacts.js
+++ corehq/apps/sms/static/sms/js/chat_contacts.js
@@ -28,7 +28,7 @@ hqDefine('sms/js/chat_contacts', function() {
                             '<a target="_blank" href="<%= href %>"><%= content %></a>' +
                             '<span class="btn btn-primary pull-right" ' +
                                   'onClick="window.open(\'<%= url %>\', \'_blank\', \'location=no,menubar=no,scrollbars=no,status=no,toolbar=no,height=400,width=400\');">' +
-                            '<%= chat %><i class="fa fa-share"></i></span>'
+                            '<%= chat %> <i class="fa fa-share"></i></span>'
                         )({
                             href: row[4],
                             content: row[0],
