8ebae5956a3ac6ad977cb8eeadcb78e5b36e90fc
Jordan Roth <jmtroth0@gmail.com>
2018-04-04 17:23:06 -0400

    accepting multiple domains

--- corehq/form_processor/management/commands/check_forms_have_xml.py
+++ corehq/form_processor/management/commands/check_forms_have_xml.py
@@ -14,32 +14,33 @@ class Command(BaseCommand):
     help = "Check if there is form.xml in all forms for a domain."
 
     def add_arguments(self, parser):
-        parser.add_argument('domain', help='Domain to check.')
+        parser.add_argument('domains', nargs="*", help='Domains to check.')
         parser.add_argument('file_name', help='Location to put the output.')
 
-    def handle(self, domain, file_name, **options):
+    def handle(self, domains, file_name, **options):
         blob_db = get_blob_db()
-        form_db = FormAccessors(domain)
-        form_ids = form_db.get_all_form_ids_in_domain()
-        form_ids.append(form_db.get_all_form_ids_in_domain('XFormArchived'))
 
         with open(file_name, 'w') as csv_file:
             field_names = ['domain', 'archived', 'form_id', 'received_on']
             csv_writer = csv.DictWriter(csv_file, field_names)
             csv_writer.writeheader()
-
-            for form in with_progress_bar(form_db.iter_forms(form_ids), len(form_ids)):
-                if isinstance(form, CouchForm):
-                    meta = form.blobs.get(ATTACHMENT_NAME)
-                    if not meta or not blob_db.exists(
-                            meta.id, form._blobdb_bucket()):  # pylint: disable=protected-access
-                        self.write_row(csv_writer, domain, form.is_archived, form.received_on, form_id)
-                elif isinstance(form, XFormInstanceSQL):
-                    meta = form.get_attachment_meta(ATTACHMENT_NAME)
-                    if not meta or not blob_db.exists(meta.blob_id, meta.blobdb_bucket()):
-                        self.write_row(csv_writer, domain, form.is_archived, form.received_on, form_id)
-                else:
-                    raise Exception("not sure how we got here")
+            for domain in domains:
+                print "Handling domain %s" % domain
+                form_db = FormAccessors(domain)
+                form_ids = form_db.get_all_form_ids_in_domain()
+                form_ids.append(form_db.get_all_form_ids_in_domain('XFormArchived'))
+                for form in with_progress_bar(form_db.iter_forms(form_ids), len(form_ids)):
+                    if isinstance(form, CouchForm):
+                        meta = form.blobs.get(ATTACHMENT_NAME)
+                        if not meta or not blob_db.exists(
+                                meta.id, form._blobdb_bucket()):  # pylint: disable=protected-access
+                            self.write_row(csv_writer, domain, form.is_archived, form.received_on, form_id)
+                    elif isinstance(form, XFormInstanceSQL):
+                        meta = form.get_attachment_meta(ATTACHMENT_NAME)
+                        if not meta or not blob_db.exists(meta.blob_id, meta.blobdb_bucket()):
+                            self.write_row(csv_writer, domain, form.is_archived, form.received_on, form_id)
+                    else:
+                        raise Exception("not sure how we got here")
 
     @staticmethod
     def write_row(writer, domain, archived, received_on, form_id):
