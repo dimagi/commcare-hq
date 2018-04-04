ff2446a5a7416c6c628f35f4f18d98b68de78da0
Jordan Roth <jmtroth0@gmail.com>
2018-04-04 16:46:45 -0400

    write as csv

--- corehq/form_processor/management/commands/check_forms_have_xml.py
+++ corehq/form_processor/management/commands/check_forms_have_xml.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import csv
 from django.core.management.base import BaseCommand
 from corehq.blobs import get_blob_db
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors
@@ -22,16 +23,29 @@ class Command(BaseCommand):
         form_ids = form_db.get_all_form_ids_in_domain()
         form_ids.append(form_db.get_all_form_ids_in_domain('XFormArchived'))
 
-        with open(file_name, 'w') as open_file:
+        with open(file_name, 'w') as csv_file:
+            field_names = ['domain', 'archived', 'form_id']
+            csv_writer = csv.DictWriter(csv_file, field_names)
+            csv_writer.writeheader()
+
             for form in with_progress_bar(form_db.iter_forms(form_ids), len(form_ids)):
                 if isinstance(form, CouchForm):
                     meta = form.blobs.get(ATTACHMENT_NAME)
                     if not meta or not blob_db.exists(
                             meta.id, form._blobdb_bucket()):  # pylint: disable=protected-access
-                        open_file.write(form.form_id + "\n")
+                        self.write_row(csv_writer, domain, form.is_archived, form_id)
                 elif isinstance(form, XFormInstanceSQL):
                     meta = form.get_attachment_meta(ATTACHMENT_NAME)
                     if not meta or not blob_db.exists(meta.blob_id, meta.blobdb_bucket()):
-                        open_file.write(form.form_id + "\n")
+                        self.write_row(csv_writer, domain, form.is_archived, form_id)
                 else:
                     raise Exception("not sure how we got here")
+
+    @staticmethod
+    def write_row(writer, domain, archived, form_id):
+        properties = {
+            'domain': domain,
+            'archived': archived,
+            'form_id': form_id,
+        }
+        csv_writer.writerow(properties)
