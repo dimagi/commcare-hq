9782b6ada66f767615c35af61d31483a9a562b70
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-04-05 10:20:06 -0400

    Make NEW_USERNAME constant

--- corehq/apps/users/management/commands/gdpr_scrub_user_from_forms.py
+++ corehq/apps/users/management/commands/gdpr_scrub_user_from_forms.py
@@ -16,7 +16,8 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = "Scrubs the username from all forms associated with the given user"
-
+    NEW_USERNAME = "Redacted User (GDPR)"
+    
     def add_arguments(self, parser):
         parser.add_argument('username')
         parser.add_argument('domain')
@@ -29,12 +30,11 @@ class Command(BaseCommand):
             sys.exit(1)
         user_id = user._id
         form_ids = this_form_accessor.get_form_ids_for_user(user_id)
-        new_username = "Redacted User (GDPR)"
         input_response = six.moves.input(
             "Update {} form(s) for user {} in domain {}? (y/n): ".format(len(form_ids), username, domain))
         if input_response == "y":
             for form_data in this_form_accessor.iter_forms(form_ids):
-                form_attachment_xml_new = self.update_form_data(form_data, new_username)
+                form_attachment_xml_new = self.update_form_data(form_data, NEW_USERNAME)
                 this_form_accessor.modify_attachment_xml_and_metadata(form_data, form_attachment_xml_new)
             logging.info("Updated {} form(s) for user {} in domain {}".format(len(form_ids), username, domain))
         elif input_response == "n":
