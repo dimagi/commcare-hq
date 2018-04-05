dd2633cd27647c2918a96acd9b6bff523c6860ba
Sravan Reddy <sravfeyn@gmail.com>
2018-04-05 17:09:39 +0530

    omit couch specific properties

--- corehq/ex-submodules/casexml/apps/phone/restore.py
+++ corehq/ex-submodules/casexml/apps/phone/restore.py
@@ -436,18 +436,13 @@ class RestoreState(object):
 
     def _new_sync_log(self):
         previous_log_id = None if self.is_initial else self.last_sync_log._id
-        previous_log_rev = None if self.is_initial else self.last_sync_log._rev
-        last_seq = str(get_db().info()["update_seq"])
         new_synclog = SimplifiedSyncLog(
-            _id=SyncLog.get_db().server.next_uuid(),
             domain=self.restore_user.domain,
             build_id=self.params.app_id,
             user_id=self.restore_user.user_id,
-            last_seq=last_seq,
             owner_ids_on_phone=set(self.owner_ids),
             date=datetime.utcnow(),
             previous_log_id=previous_log_id,
-            previous_log_rev=previous_log_rev,
             extensions_checked=True,
         )
         if self.is_livequery:
