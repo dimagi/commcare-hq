eff4da9e20dd7c5acc577f4f28224330bf0b40ab
Nick Pellegrino <npellegrino@dimagi.com>
2018-04-04 21:11:17 -0400

    python-modernize corehq/apps/ota/ -w --future-unicode

--- corehq/apps/ota/urls.py
+++ corehq/apps/ota/urls.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.conf.urls import url
 from corehq.apps.ota.views import (
     restore, search, claim, heartbeat, get_next_id,
