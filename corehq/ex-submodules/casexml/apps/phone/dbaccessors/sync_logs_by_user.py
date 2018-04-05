dfc6bfa59f37b4ed8d55bdef7291eae08ce4e99c
Sravan Reddy <sravfeyn@gmail.com>
2018-04-05 23:28:14 +0530

    simpler

--- corehq/ex-submodules/casexml/apps/phone/dbaccessors/sync_logs_by_user.py
+++ corehq/ex-submodules/casexml/apps/phone/dbaccessors/sync_logs_by_user.py
@@ -15,10 +15,7 @@ def get_last_synclog_for_user(user_id):
 
 def get_synclogs_for_user(user_id, limit=10, wrap=True):
     synclogs = SyncLogSQL.objects.filter(user_id=user_id).order_by('date')[:limit]
-    if synclogs:
-        docs = [synclog.doc for synclog in synclogs]
-    else:
-        docs = []
+    docs = [synclog.doc for synclog in synclogs]
 
     if wrap:
         return [properly_wrap_sync_log(doc) for doc in docs]
