1b9c2f81106b9151b714911af44cd2f593971291
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-06 14:49:56 -0400

    split out button_group.js

--- corehq/apps/reports/static/reports/js/filters/main.js
+++ corehq/apps/reports/static/reports/js/filters/main.js
@@ -10,36 +10,6 @@ hqDefine("reports/js/filters/main", [
     hqMain,
     standardHQReportModule
 ) {
-    var linkButtonGroup = function (groupIdOrEl, can_be_empty) {
-        // this is used to initialize the buttongroup filters
-        // see the user filter for sample usage.
-        var $el = typeof groupIdOrEl === "string" ? $("#" + groupIdOrEl) : $(groupIdOrEl);
-        $el.find("button").click(function(e) {
-            e.preventDefault();
-            var $activeCheckbox = $('#'+$(this).data("checkfilter"));
-
-            if($(this).hasClass('active')) {
-                $(this).addClass('btn-success');
-                $activeCheckbox.prop("checked", true);
-            } else {
-                $(this).removeClass('btn-success');
-                $activeCheckbox.prop("checked", false);
-            }
-            $activeCheckbox.trigger('change');
-
-            if((!$el.children().hasClass('btn-success')) && !can_be_empty) {
-                var $firstChild = $el.children().first();
-                $firstChild.addClass('btn-success');
-                $('#'+$firstChild.data("checkfilter")).prop("checked", true);
-                if ($(this).data("checkfilter") !== $firstChild.data("checkfilter")) {
-                    $firstChild.removeClass('active');
-                } else {
-                    return false;
-                }
-            }
-        });
-    };
-
     var init = function() {
         // Datespans
         $('.report-filter-datespan').each(function() {
@@ -144,7 +114,7 @@ hqDefine("reports/js/filters/main", [
 
         // Submission type (Raw Forms, Errors, & Duplicates)
         $('.report-filter-button-group').each(function() {
-            linkButtonGroup(this);
+            hqImport("reports/js/filters/button_group").link(this);
         });
 
         // Initialize any help bubbles
