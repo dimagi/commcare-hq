bc69989e17abba9bac054f0db044f2ebd58b1169
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-06 14:45:02 -0400

    switch to functional inheritance

--- corehq/apps/reports/static/reports/js/filters/phone_number.js
+++ corehq/apps/reports/static/reports/js/filters/phone_number.js
@@ -5,42 +5,44 @@ hqDefine("reports/js/filters/phone_number", function() {
                 { id: "has_phone_number", text: gettext("That have phone numbers") },
                 { id: "no_phone_number", text: gettext("That do not have phone numbers") },
             ],
-            model = this;
+            model = {};
 
-        this.filter_type = ko.observable(initial_value.filter_type || 'phone_number');
-        this.phone_number_filter = ko.observable(initial_value.phone_number_filter);
-        this.has_phone_number = ko.observable(initial_value.has_phone_number);
-        this.contact_type = ko.observable(initial_value.contact_type);
-        this.groups = ko.observableArray(groups);
-        this.selected_group = ko.observable(initial_value.selected_group);
-        this.verification_status = ko.observable(initial_value.verification_status);
+        model.filter_type = ko.observable(initial_value.filter_type || 'phone_number');
+        model.phone_number_filter = ko.observable(initial_value.phone_number_filter);
+        model.has_phone_number = ko.observable(initial_value.has_phone_number);
+        model.contact_type = ko.observable(initial_value.contact_type);
+        model.groups = ko.observableArray(groups);
+        model.selected_group = ko.observable(initial_value.selected_group);
+        model.verification_status = ko.observable(initial_value.verification_status);
 
-        this.phone_number_options = ko.pureComputed(function () {
+        model.phone_number_options = ko.pureComputed(function () {
             if (model.contact_type() === 'cases') {
                 return [PHONE_NUMBER_SELECT_OPTIONS[0]];
             }
             return PHONE_NUMBER_SELECT_OPTIONS;
         });
 
-        this.show_phone_filter = ko.pureComputed(function () {
+        model.show_phone_filter = ko.pureComputed(function () {
             return model.filter_type() === 'phone_number';
         });
 
-        this.show_contact_filter = ko.pureComputed(function () {
+        model.show_contact_filter = ko.pureComputed(function () {
             return model.filter_type() === 'contact';
         });
 
-        this.show_group_filter = ko.pureComputed(function () {
+        model.show_group_filter = ko.pureComputed(function () {
             return model.show_contact_filter() && model.contact_type() === 'users';
         });
 
-        this.can_edit_has_phone_number = ko.pureComputed(function () {
+        model.can_edit_has_phone_number = ko.pureComputed(function () {
             return model.show_contact_filter() && model.contact_type() === 'cases';
         });
 
-        this.show_verification_filter = ko.pureComputed(function () {
+        model.show_verification_filter = ko.pureComputed(function () {
             return model.show_contact_filter() && model.has_phone_number() === 'has_phone_number';
         });
+
+        return model;
     };
 
     return {
