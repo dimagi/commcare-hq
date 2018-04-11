5f519fa377f02c6194fcfbb81cfe44f01722761b
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-11 19:08:22 -0400

    Make hqImport fail in RequireJS world

--- corehq/apps/hqwebapp/static/hqwebapp/js/hqModules.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/hqModules.js
@@ -87,6 +87,9 @@ if (typeof define === 'undefined') {
 }
 
 function hqImport(path) {
+    if (window.USE_REQUIREJS) {
+        throw new Error("Tried to hqImport " + path + " but hqImport is incompatible with RequireJS!");
+    }
     if (COMMCAREHQ_MODULES[path] === undefined) {
         throw new Error("The module '" + path + "' has not yet been defined.\n\n" +
             'Did you include <script src="' + path + '.js"></script> on your html page?');
