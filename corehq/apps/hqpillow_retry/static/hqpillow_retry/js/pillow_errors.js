9d9832e775bc6abf1055c965d73d97cc80d0e935
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-12 15:26:00 -0400

    more readable

--- corehq/apps/hqpillow_retry/static/hqpillow_retry/js/pillow_errors.js
+++ corehq/apps/hqpillow_retry/static/hqpillow_retry/js/pillow_errors.js
@@ -1,7 +1,8 @@
 hqDefine("hqpillow_retry/js/pillow_errors", function() {
     $(function() {
         $(document).on('click', '#check_all', function() {
-            var oTable = $('#report_table_' + hqImport('hqwebapp/js/initial_page_data').get('slug')).dataTable();
+            var slug = hqImport('hqwebapp/js/initial_page_data').get('slug'),
+                oTable = $('#report_table_' + slug).dataTable();
             $('input', oTable.fnGetNodes()).prop('checked', this.checked);
         });
     });
