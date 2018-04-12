299f2c41306a3dae270ed57397f03c6d97325d74
Jordan Roth <jmtroth0@gmail.com>
2018-04-12 11:54:43 -0400

    move pie chart js to defined module

---
+++ corehq/apps/reports/static/reports/js/charts/pie_chart.js
@@ -0,0 +1,35 @@
+/* globals nv, d3 */
+hqDefine("reports/js/charts/pie_chart", function() {
+    var init = function(el) {
+        var $el = $(el), data = $el.data();
+        nv.addGraph(function () {
+            var chartConfig = data.configDict,
+                chartData = data.chartData,
+                chartId = '#' + data.chartId;
+
+            $(chartId).show();
+
+            var chart = nv.models.pieChart()
+                .x(function (d) { return d.label; })
+                .y(function (d) { return d.value; });
+
+            chart.showLabels(chartConfig.showLabels);
+            chart.donut(chartConfig.donut);
+            chart.tooltips(chartConfig.tooltips);
+            chart.margin(chartConfig.margin);
+            if (chartConfig.color.length !== 0) {
+                chart.color(chartConfig.color);
+            }
+
+            d3.select(chartId + ' svg')
+                .datum(chartData)
+                .transition().duration(500).call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
+        });
+    };
+
+    return { init: init };
+});
