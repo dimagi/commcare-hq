4c20650c0208781cec06bfb62fc829f8214322c5
Simon Kelly <skelly@dimagi.com>
2018-04-12 16:56:40 +0200

    fix import cycle

--- corehq/ex-submodules/casexml/apps/phone/change_publishers.py
+++ corehq/ex-submodules/casexml/apps/phone/change_publishers.py
@@ -1,16 +1,17 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from corehq.apps.change_feed import topics
 from corehq.apps.change_feed.producer import producer
 from pillowtop.feed.interface import ChangeMeta
 
 
 def publish_synclog_saved(synclog):
+    from corehq.apps.change_feed import topics
     producer.send_change(topics.SYNCLOG_SQL, change_meta_from_synclog(synclog))
 
 
 def change_meta_from_synclog(synclog):
+    from corehq.apps.change_feed import topics
     return ChangeMeta(
         document_id=synclog.synclog_id.hex,
         document_type=topics.SYNCLOG_SQL,
