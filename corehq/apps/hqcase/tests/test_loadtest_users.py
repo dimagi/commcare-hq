95bd01f615b29bfc0df298165b975f61303fdcf2
Nick Pellegrino <npellegrino@dimagi.com>
2018-04-12 10:54:09 -0400

    python-modernize corehq/apps/hqcase/ -w --future-unicode

--- corehq/apps/hqcase/tests/test_loadtest_users.py
+++ corehq/apps/hqcase/tests/test_loadtest_users.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 from django.test import TestCase
 from casexml.apps.case.mock import CaseFactory, CaseStructure, CaseIndex
 from casexml.apps.case.tests.util import delete_all_cases
