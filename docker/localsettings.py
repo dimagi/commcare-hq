1cfd9fab48d16c2f74f788865069920ce0e83d44
Nick Pellegrino <npellegrino@dimagi.com>
2018-04-13 13:43:36 -0400

    use bytestrings for couch settings

--- docker/localsettings.py
+++ docker/localsettings.py
@@ -97,13 +97,13 @@ if USE_PARTITIONED_DATABASE:
 
 ####### Couch Config ######
 COUCH_DATABASES = {
-    'default': {
+    b'default': {
         # for production this ought to be set to true on your configured couch instance
-        'COUCH_HTTPS': False,
-        'COUCH_SERVER_ROOT': 'couch:5984',  # 6984 for https couch
-        'COUCH_USERNAME': '',
-        'COUCH_PASSWORD': '',
-        'COUCH_DATABASE_NAME': 'commcarehq'
+        b'COUCH_HTTPS': False,
+        b'COUCH_SERVER_ROOT': 'couch:5984',  # 6984 for https couch
+        b'COUCH_USERNAME': '',
+        b'COUCH_PASSWORD': '',
+        b'COUCH_DATABASE_NAME': 'commcarehq'
     }
 }
 
