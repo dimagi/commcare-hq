323f47de97b3e0317aebd4a06f96698df873b0ce
Daniel Roberts <droberts@dimagi.com>
2018-04-17 18:36:26 -0400

    use namedtuples where possible

--- corehq/apps/app_manager/app_schemas/case_properties.py
+++ corehq/apps/app_manager/app_schemas/case_properties.py
@@ -38,6 +38,8 @@ def _zip_update(properties_by_case_type, additional_properties_by_case_type):
 
 
 _CaseTypeReference = namedtuple('_CaseTypeReference', ['case_type', 'relationship_path'])
+_PropertyReference = namedtuple('_PropertyReference', ['case_type_ref', 'case_property'])
+_CaseTypeEquivalence = namedtuple('_CaseTypeEquivalence', ['case_type', 'expansion'])
 
 
 class _CaseRelationshipManager(object):
@@ -49,105 +51,111 @@ class _CaseRelationshipManager(object):
 
     @property
     @memoized
-    def _all_possible_references(self):
+    def _all_possible_equivalences(self):
         """
         If referral is a child case of patient, which is the child case of a household,
         then this will return
 
         {
             # a referral is a referral
-            ('referral', _CaseTypeReference('referral', ())),
+            _CaseTypeEquivalence('referral', _CaseTypeReference('referral', ())),
             # a patient is a patient
-            ('patient', _CaseTypeReference('patient', ())),
+            _CaseTypeEquivalence('patient', _CaseTypeReference('patient', ())),
             # a patient is a referal's parent
-            ('patient', _CaseTypeReference('referral', ('parent',)))
+            _CaseTypeEquivalence('patient', _CaseTypeReference('referral', ('parent',)))
             # a household is a household
-            ('household', _CaseTypeReference('household', ())),
+            _CaseTypeEquivalence('household', _CaseTypeReference('household', ())),
             # a household is a patient's parent
-            ('household', _CaseTypeReference('patient', ('parent'))),
+            _CaseTypeEquivalence('household', _CaseTypeReference('patient', ('parent'))),
             # a houseold is a referral's parent's parent
-            ('household', _CaseTypeReference('referral', ('parent', 'parent')))
+            _CaseTypeEquivalence('household', _CaseTypeReference('referral', ('parent', 'parent')))
         }
         """
-        all_possible_references = set()
-        references_queue = deque((case_type, _CaseTypeReference(case_type, ()))
+        all_possible_equivalences = set()
+        references_queue = deque(_CaseTypeEquivalence(case_type, _CaseTypeReference(case_type, ()))
                                  for case_type in self.parent_type_map)
         while True:
             try:
                 current_reference = references_queue.popleft()
             except IndexError:
                 break
-            all_possible_references.add(current_reference)
+            all_possible_equivalences.add(current_reference)
             current_parent, (child, relationship_path) = current_reference
             for relationship, grandparents in self.parent_type_map[current_parent].items():
                 for grandparent in grandparents:
-                    new_reference = (grandparent, _CaseTypeReference(child, relationship_path + (relationship,)))
+                    new_reference = _CaseTypeEquivalence(
+                        case_type=grandparent,
+                        expansion=_CaseTypeReference(child, relationship_path + (relationship,))
+                    )
                     cycle_found = (grandparent == child)
-                    already_visited = (new_reference in all_possible_references)
+                    already_visited = (new_reference in all_possible_equivalences)
                     if not cycle_found and not already_visited:
                         references_queue.append(new_reference)
-        return all_possible_references
+        return all_possible_equivalences
 
     @property
     @memoized
-    def _case_type_to_references(self):
-        case_type_to_references = defaultdict(set)
-        for case_type, reference in self._all_possible_references:
-            case_type_to_references[case_type].add(reference)
-        return case_type_to_references
+    def _case_type_to_expansions(self):
+        case_type_to_expansions = defaultdict(set)
+        for equivalence in self._all_possible_equivalences:
+            case_type_to_expansions[equivalence.case_type].add(equivalence.expansion)
+        return case_type_to_expansions
 
     @property
     @memoized
-    def _reference_to_case_types(self):
-        reference_to_case_types = defaultdict(set)
-        for case_type, reference in self._all_possible_references:
-            reference_to_case_types[reference].add(case_type)
-        return reference_to_case_types
+    def _expansion_to_case_types(self):
+        expansion_to_case_types = defaultdict(set)
+        for case_type, reference in self._all_possible_equivalences:
+            expansion_to_case_types[reference].add(case_type)
+        return expansion_to_case_types
 
     def expand_case_type(self, case_type):
-        return self._case_type_to_references[case_type]
+        return self._case_type_to_expansions[case_type]
 
-    def resolve_reference(self, case_type, relationship_path):
-        return self._reference_to_case_types[_CaseTypeReference(case_type, relationship_path)]
+    def resolve_expansion(self, case_type_ref):
+        return self._expansion_to_case_types[case_type_ref]
+
+
+def _flatten_case_properties(case_properties_by_case_type):
+    result = set()
+    for case_type, properties in case_properties_by_case_type.items():
+        for case_property in properties:
+            case_property_parts = tuple(case_property.split('/'))
+            result.add(_PropertyReference(
+                case_type_ref=_CaseTypeReference(case_type, relationship_path=case_property_parts[:-1]),
+                case_property=case_property_parts[-1]
+            ))
+    return result
 
 
 def _normalize_case_properties(case_properties_by_case_type, parent_type_map,
                                include_parent_properties):
     case_relationship_manager = _CaseRelationshipManager(
         parent_type_map, case_types=case_properties_by_case_type.keys())
-    flattened_case_properties = {
-        (_CaseTypeReference(case_type, relationship_path=case_property_parts[:-1]),
-         case_property_parts[-1])
-        for case_type, case_property_parts in (
-            (case_type, tuple(case_property.split('/')))
-            for case_type, properties in case_properties_by_case_type.items()
-            for case_property in properties
-        )
-    }
+    flattened_case_properties = _flatten_case_properties(case_properties_by_case_type)
     normalized_case_properties = set()
-    for reference, case_property in flattened_case_properties:
-        resolved_case_types = case_relationship_manager.resolve_reference(
-            reference.case_type, reference.relationship_path)
+    for property_ref in flattened_case_properties:
+        resolved_case_types = case_relationship_manager.resolve_expansion(property_ref.case_type_ref)
         if resolved_case_types:
             if include_parent_properties:
                 normalized_case_properties.update({
-                    (expanded_reference, case_property)
+                    _PropertyReference(expanded_reference, property_ref.case_property)
                     for case_type in resolved_case_types
                     for expanded_reference in case_relationship_manager.expand_case_type(case_type)
                 })
             else:
                 normalized_case_properties.update({
-                    (_CaseTypeReference(case_type, ()), case_property)
+                    _PropertyReference(_CaseTypeReference(case_type, ()), property_ref.case_property)
                     for case_type in resolved_case_types
                 })
         else:
             # if e.g. #case/parent doesn't match any known case type, leave the reference as is
-            normalized_case_properties.add((reference, case_property))
+            normalized_case_properties.add(property_ref)
 
     normalized_case_properties_by_case_type = defaultdict(set)
-    for reference, case_property in normalized_case_properties:
-        normalized_case_properties_by_case_type[reference.case_type].add(
-            '/'.join(reference.relationship_path + (case_property,)))
+    for property_ref in normalized_case_properties:
+        normalized_case_properties_by_case_type[property_ref.case_type_ref.case_type].add(
+            '/'.join(property_ref.case_type_ref.relationship_path + (property_ref.case_property,)))
     return normalized_case_properties_by_case_type
 
 
