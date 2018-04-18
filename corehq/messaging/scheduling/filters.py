88278dab068ccf052cfb2575b7cca4ce531fed4c
Ethan Soergel <esoergel@gmail.com>
2018-04-18 17:28:59 -0400

    Gettext doesn't let you flag empty strings for translation
    [ci skip]

--- corehq/messaging/scheduling/filters.py
+++ corehq/messaging/scheduling/filters.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy
 
 
 class ScheduleInstanceFilter(BaseReportFilter):
-    label = ugettext_lazy("")
+    label = ""
     slug = 'schedule_instance_filter'
     template = 'scheduling/schedule_instance_filter.html'
 
