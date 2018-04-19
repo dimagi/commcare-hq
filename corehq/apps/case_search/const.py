884760de6878fd8beb069d2cd9e08c71d0a762b8
Farid Rener <proteusvacuum@gmail.com>
2018-04-19 15:58:08 -0400

    Add get_child_cases to search index

--- corehq/apps/case_search/const.py
+++ corehq/apps/case_search/const.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 
-PATH = 'case_properties'
+CASE_PROPERTIES_PATH = 'case_properties'
+INDICES_PATH = 'indices'
+REFERENCED_ID = 'referenced_id'
 VALUE_TEXT = 'value'
 VALUE_DATE = 'value_date'
 VALUE_NUMERIC = 'value_numeric'
