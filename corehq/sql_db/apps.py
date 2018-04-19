b543ca6b13a7b0245a99edc25ddf1dab86a002c0
Sravan Reddy <sravfeyn@gmail.com>
2018-04-19 15:05:50 +0530

    lint

--- corehq/sql_db/apps.py
+++ corehq/sql_db/apps.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from django.apps import AppConfig
 from django.conf import settings
 from django.db import connections
@@ -7,18 +9,19 @@ class SqlDbAppConfig(AppConfig):
     name = 'corehq.sql_db'
 
     def ready(self):
-    	"""This gets run when Django starts"""
+        """This gets run when Django starts"""
         identify_standby_databases()
 
 
 # gets written when `identify_standby_databases` gets called or
-# 	after Django starts.
+#   after Django starts.
 STANDBY_DATABASE_ALIASES = []
 
+
 def identify_standby_databases():
-	for db_alias in settings.DATABASES:
-		with connections[db_alias].cursor() as cursor:
-			cursor.execute("SELECT pg_is_in_recovery()")
-			[(is_standby, )] = cursor.fetchall()
-			if is_standby:
-				STANDBY_DATABASE_ALIASES.append(db_alias)
+    for db_alias in settings.DATABASES:
+        with connections[db_alias].cursor() as cursor:
+            cursor.execute("SELECT pg_is_in_recovery()")
+            [(is_standby, )] = cursor.fetchall()
+            if is_standby:
+                STANDBY_DATABASE_ALIASES.append(db_alias)
