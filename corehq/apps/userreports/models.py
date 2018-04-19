788ea1699a55d09985816e3cbb95c1bc45c8404f
Jonathan Emord <jonathan.emord@gmail.com>
2018-04-19 17:52:17 -0400

    Remove confusing comment

--- corehq/apps/userreports/models.py
+++ corehq/apps/userreports/models.py
@@ -643,18 +643,10 @@ class StaticDataSourceConfiguration(JsonObject):
 
     @classmethod
     def by_domain(cls, domain):
-        """
-        Returns a list of DataSourceConfiguration objects,
-        NOT a list of StaticDataSourceConfiguration objects.
-        """
         return [ds for ds in cls.all() if ds.domain == domain]
 
     @classmethod
     def by_id(cls, config_id):
-        """
-        Returns a DataSourceConfiguration object,
-        NOT a StaticDataSourceConfiguration object.
-        """
         mapping = cls.by_id_mapping()
         if config_id not in mapping:
             mapping = cls.by_id_mapping(rebuild=True)
