d44b92bdf3239c083d58e4488d4b9729fb66ac0c
Cal Ellowitz <calellowitz@gmail.com>
2018-04-19 09:41:37 +0530

    fixing comment

--- corehq/sql_db/connections.py
+++ corehq/sql_db/connections.py
@@ -165,7 +165,7 @@ class ConnectionManager(object):
                             self._add_django_db(read_db, read_db)
 
         for app, weights in settings.LOAD_BALANCED_APPS.items():
-            # this tests that the config is real and raises an exception if not
+            # this tests that the app_label is real and raises an exception if not
             apps.get_app_config(app)
             self.read_database_mapping[app] = []
             for db_alias, weighting in weights:
