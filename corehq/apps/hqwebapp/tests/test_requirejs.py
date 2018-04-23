d25331c15a6cf5aae2b3d68f96cec33c5e394a7f
Jennifer Schweers <orange.jenny@gmail.com>
2018-04-23 15:22:03 -0400

    Add logger

--- corehq/apps/hqwebapp/tests/test_requirejs.py
+++ corehq/apps/hqwebapp/tests/test_requirejs.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import logging
 import os
 import re
 import subprocess
 
 from django.test import SimpleTestCase
+
+logger = logging.getLogger(__name__)
 
 
 class TestRequireJS(SimpleTestCase):
