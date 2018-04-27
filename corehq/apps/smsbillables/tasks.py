9b23d57fb244a8a368ad215a47476afd58f8e344
Nick Pellegrino <npellegrino@dimagi.com>
2018-04-27 14:39:01 -0400

    use values_list

--- corehq/apps/smsbillables/tasks.py
+++ corehq/apps/smsbillables/tasks.py
@@ -18,7 +18,7 @@ from dimagi.utils.dates import add_months_to_date
 
 @periodic_task(run_every=crontab(day_of_month='1', hour=5, minute=0), queue='background_queue', acks_late=True)
 def send_gateway_fee_report_out():
-    backend_api_ids = SmsGatewayFeeCriteria.objects.values('backend_api_id').distinct()
+    backend_api_ids = SmsGatewayFeeCriteria.objects.values_list('backend_api_id', flat=True).distinct()
     first_day_previous_month = add_months_to_date(date.today(), -1)
     billables_in_month = SmsBillable.objects.filter(
         date_sent__year=first_day_previous_month.year,
@@ -28,7 +28,7 @@ def send_gateway_fee_report_out():
     cost_by_backend = {
         backend_api_id: sum(billable.gateway_charge for billable in
             billables_in_month.filter(gateway_fee__criteria__backend_api_id=backend_api_id))
-        for backend_api_id in map(lambda _dict: _dict['backend_api_id'], backend_api_ids)
+        for backend_api_id in backend_api_ids
     }
 
     subject = "[{}] Cost per SMS Gateway Monthly Summary".format(settings.SERVER_ENVIRONMENT)
