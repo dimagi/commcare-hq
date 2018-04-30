0dfe0084ce6f3c000abcb1143702fa915becb1a8
Jordan Roth <jmtroth0@gmail.com>
2018-04-30 17:53:40 -0400

    add docstring

--- corehq/util/signals.py
+++ corehq/util/signals.py
@@ -11,6 +11,13 @@ def log_celery_task_exception(task_id, exception, traceback, einfo, *args, **kwa
 
 
 class SignalHandlerContext(object):
+    """ Used to clean up long running processes gracefully on unexpected exits
+
+    :param <int|list<ints>> signals: the signals to handle
+    :param <lambda> handler: the handler to field the signals
+    :param <int> default_handler: the default handler. don't touch if you need more explanation
+    """
+
     def __init__(self, signals, handler, default_handler=signal.SIG_DFL):
         if type(signals) is not list:
             signals = [signals]
