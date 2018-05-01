720f2bd4c0c884d6af5cfdb84ad237417fe1f56a
Jennifer Schweers <orange.jenny@gmail.com>
2018-05-01 12:19:03 -0400

    Use gevent

--- corehq/apps/hqwebapp/tests/test_requirejs.py
+++ corehq/apps/hqwebapp/tests/test_requirejs.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import gevent
 import os
 import re
 import subprocess
@@ -8,6 +9,13 @@ from django.test import SimpleTestCase
 
 
 class TestRequireJS(SimpleTestCase):
+
+    @classmethod
+    def _run_jobs(cls, files, processor):
+        jobs = []
+        for filename in files:
+            jobs.append(gevent.spawn(processor, filename))
+        gevent.joinall(jobs)
 
     @classmethod
     def setUpClass(cls):
@@ -23,7 +31,8 @@ class TestRequireJS(SimpleTestCase):
 
         cls.hqdefine_files = []
         cls.requirejs_files = []
-        for filename in cls.js_files:
+
+        def _categorize_file(filename):
             proc = subprocess.Popen(["grep", "^\s*hqDefine", filename], stdout=subprocess.PIPE)
             (out, err) = proc.communicate()
             if out:
@@ -33,10 +42,12 @@ class TestRequireJS(SimpleTestCase):
                 if out:
                     cls.requirejs_files.append(filename)
 
+        cls._run_jobs(cls.js_files, _categorize_file)
 
     def test_files_match_modules(self):
         errors = []
-        for filename in self.hqdefine_files:
+
+        def _test_file(filename):
             proc = subprocess.Popen(["grep", "hqDefine", filename], stdout=subprocess.PIPE)
             (out, err) = proc.communicate()
             for line in out.split("\n"):
@@ -46,19 +57,22 @@ class TestRequireJS(SimpleTestCase):
                     if not filename.endswith(module + ".js"):
                         errors.append("Module {} defined in file {}".format(module, filename))
 
+        self._run_jobs(self.hqdefine_files, _test_file)
+
         if errors:
             self.fail("Mismatched JS file/modules: \n{}".format("\n".join(errors)))
 
     def test_requirejs_disallows_hqimport(self):
         errors = []
-        for filename in self.requirejs_files:
-            # Special case 1: ignore standard_hq_report.js until we migrate UCRs and reports
-            if filename.endswith("reports/js/standard_hq_report.js"):
-                continue
-            # Special case 2: knockout_bindings should be broken up, in the meantime, ignore
-            if filename.endswith("hqwebapp/js/knockout_bindings.ko.js"):
-                continue
 
+        # Special cases:
+        #   Ignore standard_hq_report.js until we migrate UCRs and reports
+        #   knockout_bindings should be broken up, in the meantime, ignore
+        test_files = [f for f in self.requirejs_files
+                      if not f.endswith("reports/js/standard_hq_report.js")
+                      and not f.endswith("hqwebapp/js/knockout_bindings.ko.js")]
+
+        def _test_file(filename):
             proc = subprocess.Popen(["grep", "hqImport", filename], stdout=subprocess.PIPE)
             (out, err) = proc.communicate()
             for line in out.split("\n"):
@@ -68,6 +82,8 @@ class TestRequireJS(SimpleTestCase):
                         errors.append("{} imported in {}".format(match.group(1), filename))
                     else:
                         errors.append("hqImport found in {}: {}".format(filename, line.strip()))
+
+        self._run_jobs(test_files, _test_file)
 
         if errors:
             self.fail("hqImport used in RequireJS modules: \n{}".format("\n".join(errors)))
