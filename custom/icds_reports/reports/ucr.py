75efb29648137dd7436e7e3ad89d6838a56ea2af
Jonathan Emord <jonathan.emord@gmail.com>
2018-05-03 16:06:31 -0400

    Finish first test of custom SQL

--- custom/icds_reports/reports/ucr.py
+++ custom/icds_reports/reports/ucr.py
@@ -1,3 +1,4 @@
+from memoized import memoized
 from sqlalchemy.sql import func
 
 
@@ -10,21 +11,67 @@ class MPR2APersonCases(object):
     def table(self):
         return self.helper.get_table()
 
+    @property
+    @memoized
+    def _age_in_days_at_death(self):
+        return (self.table.c.date_death - self.table.c.dob)
+
+    def _count_filter_aggregate_expr(self, column, label):
+        return (func.count(self.table.c.doc_id).filter(column).label(label))
+
+    def _column_helper(self, x, y, z=None):
+        columns = self.table.c
+        column = (columns.closed_on != None) & (columns.date_death != None)
+        column &= {
+            "F": columns.sex == "F",
+            "M": columns.sex == "M",
+            "preg": (columns.female_death_type == "pregnant"),
+            "del": (columns.female_death_type == "delivery"),
+            "pnc": (columns.female_death_type == "pnc"),
+        }[x]
+        column &= {
+            "resident": columns.resident == "yes",
+            "migrant": columns.resident != "yes",
+        }[y]
+
+        if z is None:
+            return self._count_filter_aggregate_expr(column, "dead_{}_{}_count".format(x, y))
+
+        column &= {
+            "neo": self._age_in_days_at_death <= 28,
+            "postneo": self._age_in_days_at_death.between(29, 364),
+            "child": self._age_in_days_at_death.between(365, 1826),
+            "adult": columns.age_at_death_yrs >= 11,
+        }[z]
+
+        return self._count_filter_aggregate_expr(column, "dead_{}_{}_{}_count".format(x, y, z))
+
     def _columns(self, total_row=False):
-        table = self.table
         columns = (
-            func.count(table.c.doc_id).filter(
-                (table.c.closed_on != None) &
-                (table.c.sex == "F") &
-                (table.c.resident == "yes") &
-                (table.c.date_death != None) &
-                (table.c.dob != None) &
-                ((table.c.date_death - table.c.dob).between(0, 28))
-            ).label("dead_F_resident_neo_count"),
+            self._column_helper("F", "resident", "neo"),
+            self._column_helper("M", "resident", "neo"),
+            self._column_helper("F", "migrant", "neo"),
+            self._column_helper("M", "migrant", "neo"),
+            self._column_helper("F", "resident", "postneo"),
+            self._column_helper("M", "resident", "postneo"),
+            self._column_helper("F", "migrant", "postneo"),
+            self._column_helper("M", "migrant", "postneo"),
+            self._column_helper("F", "resident", "child"),
+            self._column_helper("M", "resident", "child"),
+            self._column_helper("F", "migrant", "child"),
+            self._column_helper("M", "migrant", "child"),
+            self._column_helper("F", "migrant", "adult"),
+            self._column_helper("F", "resident", "adult"),
+            self._column_helper("preg", "resident"),
+            self._column_helper("preg", "migrant"),
+            self._column_helper("del", "resident"),
+            self._column_helper("del", "migrant"),
+            self._column_helper("pnc", "resident"),
+            self._column_helper("pnc", "migrant"),
         )
 
         if not total_row:
-            columns = (table.c.owner_id.label("owner_id"),) + columns
+            columns = (self.table.c.owner_id.label("owner_id"),) + columns
 
         return columns
 
