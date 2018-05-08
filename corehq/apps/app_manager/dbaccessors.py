610601a841899e0afdd04fb7cd8d677bde949270
Manish Kangia <mkangia@dimagi.com>
2018-05-09 00:28:05 +0530

    fix view for getting saved versions of the app

--- corehq/apps/app_manager/dbaccessors.py
+++ corehq/apps/app_manager/dbaccessors.py
@@ -431,11 +431,10 @@ def get_all_built_app_results(domain, app_id=None):
 
 def get_available_versions_for_app(domain, app_id):
     from .models import Application
-    result = Application.get_db().view(
-        'app_manager/applications_brief',
-        startkey=[domain, app_id],
-        endkey=[domain, app_id, {}]
-    )
+    result = Application.get_db().view('app_manager/saved_app',
+                                       startkey=[domain, app_id, {}],
+                                       endkey=[domain, app_id],
+                                       descending=True)
     return [doc['value']['version'] for doc in result]
 
 
