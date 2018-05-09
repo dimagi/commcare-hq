602265886dbd4e2a441a8b6eb86c19bc13475ad4
Gabriella Bova <bova3.0@gmail.com>
2018-05-09 14:41:53 -0400

    Fix failing test

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -2077,7 +2077,7 @@ class BillingRecordBase(models.Model):
             file_attachments=[pdf_attachment],
             cc=cc_emails
         )
-        self.recipients = contact_email
+        self.recipients = [contact_email]
         self.save()
         log_accounting_info(
             "Sent billing statements for domain %(domain)s to %(emails)s." % {
