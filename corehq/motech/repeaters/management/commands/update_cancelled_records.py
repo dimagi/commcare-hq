332c0fe9eee9f61b4b403b3683ef7405d2fbcfeb
Nick Pellegrino <npellegrino@dimagi.com>
2018-05-16 09:03:48 -0400

    use io.open in corehq/motech/

--- corehq/motech/repeaters/management/commands/update_cancelled_records.py
+++ corehq/motech/repeaters/management/commands/update_cancelled_records.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
-import csv
+import csv342 as csv
 import datetime
 import re
 import time
@@ -14,6 +14,7 @@ from corehq.motech.repeaters.dbaccessors import iter_repeat_records_by_domain
 from corehq.motech.repeaters.models import Repeater, RepeatRecordAttempt
 from six.moves import input
 from six.moves import filter
+from io import open
 
 
 class Command(BaseCommand):
