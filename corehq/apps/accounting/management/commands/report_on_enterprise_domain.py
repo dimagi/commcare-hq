533308dddef12e0822230c610cef8a417219d6c9
Jennifer Schweers <orange.jenny@gmail.com>
2018-05-17 18:27:15 -0400

    Write last report to actual file

--- corehq/apps/accounting/management/commands/report_on_enterprise_domain.py
+++ corehq/apps/accounting/management/commands/report_on_enterprise_domain.py
@@ -46,15 +46,20 @@ class Command(BaseCommand):
     def _domain_url(self, domain):
         return "https://www.commcarehq.org" + reverse('dashboard_domain', kwargs={'domain': domain})
 
-    def _write_file(self, slug, headers, process_domain=None):
+    def _write_file(self, slug, headers, process_domain, multiple=False):
         timestamp = datetime.utcnow().strftime('%Y%m%d_%H%M%S')
         filename = 'enterprise_{}_{}.csv'.format(slug, timestamp)
         with open(filename, 'wb') as csvfile:
             csvwriter = csv.writer(csvfile)
             csvwriter.writerow(headers)
+            row_count = 0
             for domain in [domain.name for domain in map(Domain.get_by_name, self.domain_names) if domain]:
-                csvwriter.writerow(process_domain(domain))
-            print('Wrote {}'.format(filename))
+                result = process_domain(domain)
+                rows = result if multiple else [result]
+                row_count = row_count + len(rows)
+                for row in rows:
+                    csvwriter.writerow(row)
+            print('Wrote {} rows of data to {}'.format(row_count, filename))
 
     def _domain_row(self, domain):
         subscription = Subscription.get_active_subscription_by_domain(domain)
@@ -78,11 +83,49 @@ class Command(BaseCommand):
                 self._domain_url(domain),
             ]
 
+    def _form_submissions_row(self, domain):
+        time_filter = form_es.submitted
+        datespan = DateSpan(datetime.now() - timedelta(days=700), datetime.utcnow())
+        apps = get_brief_apps_in_domain(domain)
+        apps = {a.id: a.name for a in apps}
+        users = get_all_user_rows(domain, include_web_users=False, include_mobile_users=True,
+                                  include_inactive=False, include_docs=True)
+        names = {}
+        for user in users:
+            user = user['doc']
+            username = user['username']
+            username = re.sub(r'@.*', '', username)
+            names[username] = (user['first_name'], user['last_name'])
+        users_filter = form_es.user_id(EMWF.user_es_query(domain,
+                                       ['t__0'],  # All mobile workers
+                                       self.couch_user)
+                        .values_list('_id', flat=True))
+        query = (form_es.FormES()
+                 .domain(domain)
+                 .filter(time_filter(gte=datespan.startdate,
+                                     lt=datespan.enddate_adjusted))
+                 .filter(users_filter))
+        rows = []
+        for hit in query.run().hits:
+            username = hit['form']['meta']['username']
+            submitted = datetime.strptime(hit['received_on'][:19], '%Y-%m-%dT%H:%M:%S').strftime(self.date_fmt)
+            rows.append([
+                hit['form']['@name'],
+                submitted,
+                apps[hit['app_id']] if hit['app_id'] in apps else 'App not found',
+                domain,
+                self._domain_url(domain),
+                username,
+                names[username][0] if username in names else 'User not found',
+                names[username][1] if username in names else 'User not found',
+            ])
+        return rows
+
     def handle(self, domain_names, **kwargs):
         self.domain_names = domain_names
         self.date_fmt = '%Y/%m/%d %H:%M:%S'
-        couch_user = CouchUser.get_by_username(kwargs.get('username'))
-        if not couch_user:
+        self.couch_user = CouchUser.get_by_username(kwargs.get('username'))
+        if not self.couch_user:
             raise CommandError("Option: '--username' must be specified")
 
         print('Processing {} domains'.format(len(self.domain_names)))
@@ -91,44 +134,8 @@ class Command(BaseCommand):
         self._write_file('domains', headers, self._domain_row)
 
         headers = ['Name', 'Email Address', 'Role', 'Last Login', 'Project Space Name', 'Project Space URL']
-        self._write_file('domains', headers, self._web_users_row)
+        self._write_file('web_users', headers, self._web_users_row)
 
-        # Report 3: Form Submissions
         headers = ['Form Name', 'Submitted', 'App Name', 'Project Space Name', 'Project Space URL',
                    'Mobile User', 'First Name', 'Last Name']
-        print(','.join(headers))
-        time_filter = form_es.submitted
-        datespan = DateSpan(datetime.now() - timedelta(days=7), datetime.utcnow())
-        for domain in [domain for domain in map(Domain.get_by_name, domain_names) if domain]:
-            apps = get_brief_apps_in_domain(domain.name)
-            apps = {a.id: a.name for a in apps}
-            users = get_all_user_rows(domain.name, include_web_users=False, include_mobile_users=True,
-                                      include_inactive=False, include_docs=True)
-            names = {}
-            for user in users:
-                user = user['doc']
-                username = user['username']
-                username = re.sub(r'@.*', '', username)
-                names[username] = (user['first_name'], user['last_name'])
-            users_filter = form_es.user_id(EMWF.user_es_query(domain.name,
-                                           ['t__0'],  # All mobile workers
-                                           couch_user)
-                            .values_list('_id', flat=True))
-            query = (form_es.FormES()
-                     .domain(domain.name)
-                     .filter(time_filter(gte=datespan.startdate,
-                                         lt=datespan.enddate_adjusted))
-                     .filter(users_filter))
-            for hit in query.run().hits:
-                username = hit['form']['meta']['username']
-                submitted = datetime.strptime(hit['received_on'][:19], '%Y-%m-%dT%H:%M:%S').strftime(self.date_fmt)
-                print(','.join([
-                    hit['form']['@name'],
-                    submitted,
-                    apps[hit['app_id']] if hit['app_id'] in apps else 'App not found',
-                    domain.name,
-                    reverse('domain_login', kwargs={'domain': domain.name}),     # TODO: make full URL
-                    username,
-                    names[username][0] if username in names else 'User not found',
-                    names[username][1] if username in names else 'User not found',
-                ]))
+        self._write_file('form_submissions', headers, self._form_submissions_row, multiple=True)
