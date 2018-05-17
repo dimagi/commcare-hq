6f62b04ea6090f7633762dafb5b79276f5eb0612
Jennifer Schweers <orange.jenny@gmail.com>
2018-05-17 13:53:00 -0400

    Start third report: fetch past week's form submissions

--- corehq/apps/accounting/management/commands/report_on_enterprise_domain.py
+++ corehq/apps/accounting/management/commands/report_on_enterprise_domain.py
@@ -1,13 +1,19 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+from datetime import datetime, timedelta
 from django.core.management import BaseCommand
 from django.urls import reverse
 
+from dimagi.utils.dates import DateSpan
+
 from corehq.apps.accounting.models import DefaultProductPlan, Subscription
 from corehq.apps.domain.models import Domain
+from corehq.apps.es import forms as form_es
+from corehq.apps.reports.filters.users import ExpandedMobileWorkerFilter as EMWF
 from corehq.apps.users.dbaccessors.all_commcare_users import get_all_user_rows, get_mobile_user_count
-from corehq.apps.users.models import WebUser
+from corehq.apps.users.models import CouchUser, WebUser
 from six.moves import map
 
 
@@ -49,4 +55,33 @@ class Command(BaseCommand):
                     user.last_login.strftime('%Y/%m/%d %H:%M:%S'),
                     domain.name,
                     reverse('domain_login', kwargs={'domain': domain.name}),     # TODO: make full URL
+                ]))
+
+        # Report 3: Form Submissions
+        headers = ['Form Name', 'Submitted', 'App Name', 'Project Space Name', 'Project Space URL',
+                   'Mobile User', 'Mobile User First Name', 'Mobile User Last Name']
+        print(','.join(headers))
+        time_filter = form_es.submitted
+        datespan = DateSpan(datetime.now() - timedelta(days=7), datetime.utcnow())
+        for domain in [domain for domain in map(Domain.get_by_name, domain_names) if domain]:
+            users_filter = form_es.user_id(EMWF.user_es_query(domain.name,
+                                           ['t__0'],  # All mobile workers
+                                           CouchUser.get_by_username('jschweers@dimagi.com')) # TODO: take as param
+                            .values_list('_id', flat=True))
+            query = (form_es.FormES()
+                     .domain(domain.name)
+                     .filter(time_filter(gte=datespan.startdate,
+                                         lt=datespan.enddate_adjusted))
+                     .filter(users_filter))
+            hits = query.run().hits
+            for hit in hits:
+                print(','.join([
+                    'TODO',
+                    'TODO',
+                    'TODO',
+                    domain.name,
+                    reverse('domain_login', kwargs={'domain': domain.name}),     # TODO: make full URL
+                    'TODO',
+                    'TODO',
+                    'TODO',
                 ]))
