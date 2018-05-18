b8c1a153c57a6df4943ad609872aed90e6c7eb93
Cory Zue <czue@dimagi.com>
2018-05-18 14:46:43 +0200

    add get_config_spec to base test and convert to classmethods

--- corehq/apps/aggregate_ucrs/tests/base.py
+++ corehq/apps/aggregate_ucrs/tests/base.py
@@ -11,9 +11,11 @@ class AggregationBaseTestMixin(TestFileMixin):
     file_path = ('data', 'table_definitions')
     root = os.path.dirname(__file__)
 
-    def get_config_json(self):
-        config_yml = self.get_file('aggregate_sample_definition', 'yml')
+    @classmethod
+    def get_config_json(cls):
+        config_yml = cls.get_file('aggregate_sample_definition', 'yml')
         return yaml.load(config_yml)
 
-    def get_config_spec(self):
-        return AggregationSpec.wrap(self.get_config_json())
+    @classmethod
+    def get_config_spec(cls):
+        return AggregationSpec.wrap(cls.get_config_json())
