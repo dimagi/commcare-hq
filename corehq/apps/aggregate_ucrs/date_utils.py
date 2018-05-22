0c3eefb0cf760a1e6967edf517d4aa182e4bcdaf
Cory Zue <czue@dimagi.com>
2018-05-22 16:35:03 +0200

    add Month utility class + test

---
+++ corehq/apps/aggregate_ucrs/date_utils.py
@@ -0,0 +1,32 @@
+from collections import namedtuple
+
+from datetime import datetime, timedelta
+
+
+class Month(namedtuple('Month', 'year month')):
+
+    @property
+    def start(self):
+        """The very beginning of the month"""
+        return datetime(self.year, self.month, 1)
+
+    @property
+    def end(self):
+        """The end of the month, non-inclusive (aka the beginning of the next month)"""
+        day = self.start + timedelta(days=32)
+        return datetime(day.year, day.month, 1)
+
+    def get_previous_month(self):
+        day = self.start - timedelta(days=1)
+        return Month.datetime_to_month(day)
+
+    def get_next_month(self):
+        return Month.datetime_to_month(self.end)
+
+    @classmethod
+    def datetime_to_month(cls, dt):
+        return cls(dt.year, dt.month)
+
+    @classmethod
+    def current_month(cls):
+        return cls.datetime_to_month(datetime.now())
