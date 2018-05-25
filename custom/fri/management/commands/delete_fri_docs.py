7387205ba9ab0edf58246b8a9d28a07a3e9831b1
Ethan Soergel <esoergel@gmail.com>
2018-05-25 14:52:04 -0400

    lint

--- custom/fri/management/commands/delete_fri_docs.py
+++ custom/fri/management/commands/delete_fri_docs.py
@@ -7,6 +7,8 @@ from corehq.dbaccessors.couchapps.all_docs import get_doc_ids_by_class
 from corehq.util.couch import iter_update, DocUpdate
 from custom.fri.models import FRIMessageBankMessage, FRIRandomizedMessage, FRIExtraMessage
 
+from six.moves import input
+
 
 def delete_fn(doc):
     return DocUpdate(doc, delete=True)
@@ -19,5 +21,5 @@ class Command(BaseCommand):
             ids = get_doc_ids_by_class(doc_class)
 
             msg = "Delete {} {} docs?\n(y/N)".format(len(ids), doc_class.__name__)
-            if raw_input(msg) == 'y':
+            if input(msg) == 'y':
                 iter_update(doc_class.get_db(), delete_fn, ids, verbose=True)
