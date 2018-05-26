b9d0078cdc2544b73fb9308862b2fad15ee284cc
Nick Pellegrino <npellegrino@dimagi.com>
2018-05-25 23:05:26 -0400

    try punting emails to celery queue with no workers

--- corehq/ex-submodules/dimagi/utils/django/email.py
+++ corehq/ex-submodules/dimagi/utils/django/email.py
@@ -6,6 +6,7 @@ import requests
 from smtplib import SMTPSenderRefused
 import uuid
 
+from celery.task import task
 from django.conf import settings
 from django.core.mail import get_connection
 from django.core.mail.message import EmailMultiAlternatives
@@ -26,7 +27,16 @@ in HTML, or use an email client that supports HTML emails.
 def send_HTML_email(subject, recipient, html_content, text_content=None,
                     cc=None, email_from=settings.DEFAULT_FROM_EMAIL,
                     file_attachments=None, bcc=None):
+    send_HTML_email_punt.delay(subject, recipient, html_content, text_content=text_content,
+        cc=cc, email_from=email_from,
+        file_attachments=file_attachments, bcc=bcc)
 
+
+@task(queue="email_queue_punt",
+      bind=True, default_retry_delay=15 * 60, max_retries=10, acks_late=True)
+def send_HTML_email_punt(subject, recipient, html_content, text_content=None,
+                         cc=None, email_from=settings.DEFAULT_FROM_EMAIL,
+                         file_attachments=None, bcc=None):
     recipient = list(recipient) if not isinstance(recipient, six.string_types) else [recipient]
 
     if not isinstance(html_content, six.text_type):
