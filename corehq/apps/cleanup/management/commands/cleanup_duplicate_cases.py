199d984e294692d265f3e5e71e9afe4dddaa8b58
Nick Pellegrino <npellegrino@dimagi.com>
2018-05-31 17:04:18 -0400

    first draft

---
+++ corehq/apps/cleanup/management/commands/cleanup_duplicate_cases.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.core.management import BaseCommand
+
+from casexml.apps.case.mock import CaseFactory, CaseStructure
+from corehq.apps.hqcase.utils import SYSTEM_FORM_XMLNS
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors, FormAccessors
+from corehq.util.log import with_progress_bar
+
+DOMAIN = 'mukte-ii'
+
+def log(message):
+    print(message)
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'case_ids',
+            nargs='*',
+        )
+        parser.add_argument(
+            '--close-cases',
+            action='store_true',
+            default=False,
+        )
+
+    def handle(self, case_ids, **options):
+        close_cases = options['close_cases']
+        case_ids = case_ids or self._get_case_ids()
+        cases = self.accessor.get_cases(case_ids)
+        for case in with_progress_bar(cases):
+            if self.is_duplicate(case):
+                log('Flagging %s' % case.case_id)
+                self._handle_duplicate(case.case_id, close_cases)
+            else:
+                log('Skipping %s' % case.case_id)
+
+    def _get_case_ids(self):
+        raise NotImplementedError
+
+    def is_duplicate(self, case):
+        if len(case.xform_ids) != 3:
+            return False
+        last_xform_id = case.xform_ids[-1]
+        last_xform = FormAccessors(domain=DOMAIN).get_form(last_xform_id)
+        return last_xform.xmlns == SYSTEM_FORM_XMLNS
+
+    def _handle_duplicate(self, case_id, close_cases):
+        if close_cases:
+            CaseFactory(DOMAIN).create_or_update_case(
+                CaseStructure(
+                    case_id=case_id,
+                    walk_related=False,
+                    attrs={
+                        'close': True,
+                    },
+                )
+            )
+            log('Closed %s' % case_id)
+
+    @property
+    def accessor(self):
+        return CaseAccessors(domain=DOMAIN)
