a166ba305b79bd2d13464f17674f7c97aa44a3e8
Gabriella Bova <bova3.0@gmail.com>
2018-06-04 15:58:02 -0400

    Use camelCase

--- corehq/apps/accounting/static/accounting/js/billing_account_form.js
+++ corehq/apps/accounting/static/accounting/js/billing_account_form.js
@@ -10,12 +10,12 @@ hqDefine('accounting/js/billing_account_form', [
     ko,
     initialPageData
 ) {
-    var billingAccountFormModel = function (is_active, is_customer_billing_account) {
+    var billingAccountFormModel = function (isActive, isCustomerBillingAccount) {
         'use strict';
         var self = {};
 
-        self.is_active = ko.observable(is_active);
-        self.is_customer_billing_account = ko.observable(is_customer_billing_account);
+        self.is_active = ko.observable(isActive);
+        self.is_customer_billing_account = ko.observable(isCustomerBillingAccount);
         self.showActiveAccounts = ko.computed(function () {
             return !self.is_active();
         });
