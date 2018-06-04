dab8d14505023e257ee8facc0de0e3e0fb1129d6
Ethan Soergel <esoergel@gmail.com>
2018-06-04 17:02:30 -0400

    Just mock feature flag

--- corehq/apps/app_manager/tests/test_profile.py
+++ corehq/apps/app_manager/tests/test_profile.py
@@ -1,18 +1,20 @@
 # coding: utf-8
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
 import mock
+import xml.etree.cElementTree as ET
 from django.test import SimpleTestCase
+
+from corehq.apps.builds.models import BuildSpec
+from corehq.util.test_utils import flag_enabled
+
 from corehq.apps.app_manager.commcare_settings import get_commcare_settings_lookup, get_custom_commcare_settings
 from corehq.apps.app_manager.models import Application
 from corehq.apps.app_manager.tests.util import TestXmlMixin
-import xml.etree.cElementTree as ET
-
-from corehq.apps.builds.models import BuildSpec
-from corehq import toggles
-from toggle.shortcuts import update_toggle_cache, clear_toggle_cache
 
 
+@flag_enabled('CUSTOM_PROPERTIES')
 class ProfileTest(SimpleTestCase, TestXmlMixin):
     file_path = ('data',)
 
@@ -24,11 +26,6 @@ class ProfileTest(SimpleTestCase, TestXmlMixin):
             domain="potter",
             langs=['en']
         )
-
-        update_toggle_cache(toggles.CUSTOM_PROPERTIES.slug, 'potter', True, toggles.NAMESPACE_DOMAIN)
-
-    def tearDown(self):
-        clear_toggle_cache(toggles.CUSTOM_PROPERTIES.slug, 'potter', toggles.NAMESPACE_DOMAIN)
 
     def _test_profile(self, app):
         profile = app.create_profile()
