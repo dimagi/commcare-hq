671a9274797794d3c55eb64d6189b511ab6f0a6c
Jennifer Schweers <orange.jenny@gmail.com>
2018-06-07 18:11:49 -0400

    Finish migrating locations import page

--- corehq/apps/locations/static/locations/js/import.js
+++ corehq/apps/locations/static/locations/js/import.js
@@ -1,9 +1,11 @@
 hqDefine('locations/js/import', [
     'jquery',
+    'knockout',
     'analytix/js/google',
     'commtrack/js/location_bulk_upload_file',
 ], function(
     $,
+    ko,
     googleAnalytics
 ) {
     $(function() {
@@ -11,5 +13,29 @@ hqDefine('locations/js/import', [
         $("button[type='submit']").click(function() {
             googleAnalytics.track.event('Organization Structure', 'Bulk Import', 'Upload');
         });
+
+        if ($("#bulk_upload_form").get(0)) {
+            $("#bulk_upload_form").koApplyBindings({
+                file: ko.observable(null),
+            });
+        }
+
+        // modify download url to pass extra options
+        function consumptionOptionsViewModel(base_url) {
+            var self = {};
+            self.base_url = base_url;
+            self.include_consumption = ko.observable(false);
+            self.url = ko.computed(function() {
+                return (
+                    self.base_url + "?"
+                    + (self.include_consumption() ? "include_consumption=true" : "")
+                );
+            });
+            return self;
+        }
+
+        $("#download_block").koApplyBindings(
+            consumptionOptionsViewModel($("#download_link").get(0).href)
+        );
     });
 });
