60543ccb97037b42d7a898345af1da88b4799253
Cory Zue <czue@dimagi.com>
2018-06-12 09:30:31 +0200

    update test with updated data

--- corehq/apps/aggregate_ucrs/tests/test_parse_config.py
+++ corehq/apps/aggregate_ucrs/tests/test_parse_config.py
@@ -9,7 +9,7 @@ class ConfigParseTest(SimpleTestCase, AggregationBaseTestMixin):
 
     def test_parse_basic_definition(self):
         spec = self.get_config_spec()
-        self.assertEqual('pregnancy_cases', spec.primary_table.data_source_id)
+        self.assertEqual('550c3cd432d931387e75e8506b5caf9e', spec.primary_table.data_source_id)
         self.assertEqual('doc_id', spec.primary_table.key_column)
         self.assertEqual(4, len(spec.primary_table.columns))
         self.assertEqual('month', spec.aggregation_config.unit)
@@ -17,7 +17,7 @@ class ConfigParseTest(SimpleTestCase, AggregationBaseTestMixin):
         self.assertEqual('date_closed', spec.aggregation_config.end_column)
         self.assertEqual(1, len(spec.secondary_tables))
         secondary_table = spec.secondary_tables[0]
-        self.assertEqual('followup_forms', secondary_table.data_source_id)
-        self.assertEqual('case_id', secondary_table.key_column)
+        self.assertEqual('d824a4864ecb421fb3be8bf8173a05d7', secondary_table.data_source_id)
+        self.assertEqual('form.case.@case_id', secondary_table.key_column)
         self.assertEqual('received_on', secondary_table.aggregation_column)
         self.assertEqual(1, len(secondary_table.columns))
