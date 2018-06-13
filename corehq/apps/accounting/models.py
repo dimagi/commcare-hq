77d6b97bfd886f7dcc3ee757598db1b4473506c7
Gabriella Bova <bova3.0@gmail.com>
2018-06-13 13:18:15 -0400

    Error from billing account

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -1604,6 +1604,19 @@ class Subscription(models.Model):
                 }
             )
 
+        if (not plan_version.plan.is_customer_software_plan and account.is_customer_billing_account) or \
+                (plan_version.plan.is_customer_software_plan and not account.is_customer_billing_account):
+            if plan_version.plan.is_customer_software_plan:
+                raise NewSubscriptionError(
+                    'You are trying to add a Customer Software Plan to a regular Billing Account. '
+                    'Both or neither must be customer-level.'
+                )
+            else:
+                raise NewSubscriptionError(
+                    'You are trying to add a regular Software Plan to a Customer Billing Account. '
+                    'Both or neither must be customer-level.'
+                )
+
         subscriber = Subscriber.objects.get_or_create(domain=domain)[0]
         today = datetime.date.today()
         date_start = date_start or today
