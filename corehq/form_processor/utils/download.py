6cb2aeca25eb7ef8cdbdc561e7c9a79eb904ef58
Simon Kelly <skelly@dimagi.com>
2018-06-14 13:50:39 +0200

    add compatability imports

--- corehq/form_processor/utils/download.py
+++ corehq/form_processor/utils/download.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from wsgiref.util import FileWrapper
 
 from django.http import StreamingHttpResponse, HttpResponse
