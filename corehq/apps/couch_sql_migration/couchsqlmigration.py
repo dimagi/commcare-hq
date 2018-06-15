2ae1b14398594f9170b15ed62dae3b3a9ea22c0a
Jennifer Schweers <orange.jenny@gmail.com>
2018-06-15 17:30:25 -0400

    unneeded import

--- corehq/apps/couch_sql_migration/couchsqlmigration.py
+++ corehq/apps/couch_sql_migration/couchsqlmigration.py
@@ -28,7 +28,7 @@ from corehq.form_processor.backends.sql.processor import FormProcessorSQL
 from corehq.form_processor.interfaces.processor import FormProcessorInterface, ProcessedForms
 from corehq.form_processor.models import (
     XFormInstanceSQL, XFormOperationSQL, XFormAttachmentSQL, CommCareCaseSQL,
-    CaseTransaction, RebuildWithReason, CommCareCaseIndexSQL, CaseAttachmentSQL
+    CaseTransaction, RebuildWithReason, CommCareCaseIndexSQL,
 )
 from corehq.form_processor.submission_post import CaseStockProcessingResult
 from corehq.form_processor.utils import adjust_datetimes
