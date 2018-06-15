886a51a53c2ad7307ce957db0c6c617e74e5ba0f
Nick Pellegrino <npellegrino@dimagi.com>
2018-06-15 14:20:41 -0400

    filter out other users' private exports

--- corehq/apps/export/views.py
+++ corehq/apps/export/views.py
@@ -1046,6 +1046,11 @@ class BaseExportListView(ExportsPermissionsMixin, HQJSONResponseMixin, BaseProje
         """
         try:
             saved_exports = self.get_saved_exports()
+            saved_exports = [
+                export for export in saved_exports
+                if export.sharing != Sharing.PRIVATE
+                or not export.owner_id or export.owner_id == self.request.couch_user.user_id
+            ]
             if self.is_deid:
                 saved_exports = [x for x in saved_exports if x.is_safe]
             saved_exports = list(map(self.fmt_export_data, saved_exports))
