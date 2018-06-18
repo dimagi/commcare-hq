2f903cfe0915c793c2b886f5aec32302c54aef0b
Nick Pellegrino <npellegrino@dimagi.com>
2018-06-18 17:24:30 -0400

    owner_id is None means all users can access

--- corehq/apps/export/models/new.py
+++ corehq/apps/export/models/new.py
@@ -773,10 +773,10 @@ class ExportInstance(BlobMixin, Document):
         return instance
 
     def can_view(self, user_id):
-        return self.sharing != Sharing.PRIVATE or not self.owner_id or self.owner_id == user_id
+        return self.owner_id is None or self.sharing != Sharing.PRIVATE or self.owner_id == user_id
 
     def can_edit(self, user_id):
-        return self.sharing == Sharing.EDIT_AND_EXPORT or self.owner_id == user_id
+        return self.owner_id is None or self.sharing == Sharing.EDIT_AND_EXPORT or self.owner_id == user_id
 
     @classmethod
     def _move_selected_columns_to_top(cls, columns):
