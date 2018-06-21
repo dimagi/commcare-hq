ce21cd7aba6a94be4808db47c61b1ae0fecfffc0
Giovanni Capalbo <gcapalbo@dimagi.com>
2018-06-21 07:10:38 -0400

    Copy over necessary files from touchforms repo untouched

---
+++ corehq/apps/formplayer_api/smsforms/signals.py
@@ -0,0 +1,11 @@
+from django.dispatch import Signal
+
+"""
+When an xform is received, either from posting or when finished playing.
+"""
+xform_received = Signal(providing_args=["form"])
+
+"""
+When a form is finished playing (via the SMS apis)
+"""
+sms_form_complete = Signal(providing_args=["session_id", "form"])
