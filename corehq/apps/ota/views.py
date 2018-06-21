bc9018c13b3032047cb6313c8eeaf6eaa180aa72
Simon Kelly <skelly@dimagi.com>
2018-06-21 16:47:02 +0200

    csrf_exempt

--- corehq/apps/ota/views.py
+++ corehq/apps/ota/views.py
@@ -55,6 +55,7 @@ PROFILE_LIMIT = os.getenv(b'COMMCARE_PROFILE_RESTORE_LIMIT')
 PROFILE_LIMIT = int(PROFILE_LIMIT) if PROFILE_LIMIT is not None else 1
 
 
+@csrf_exempt
 @location_safe
 @handle_401_response
 @mobile_auth_or_formplayer
