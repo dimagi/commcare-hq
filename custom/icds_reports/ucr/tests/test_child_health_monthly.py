be5ec7454470c7813ac006111231a8513eaceb77
Jonathan Emord <jonathan.emord@gmail.com>
2018-06-21 13:57:28 -0400

    Don't compute pse/daily feeding form in child health monthly

--- custom/icds_reports/ucr/tests/test_child_health_monthly.py
+++ custom/icds_reports/ucr/tests/test_child_health_monthly.py
@@ -12,15 +12,8 @@ from casexml.apps.case.const import CASE_INDEX_CHILD
 from casexml.apps.case.mock import CaseStructure, CaseIndex
 from custom.icds_reports.ucr.tests.base_test import BaseICDSDatasourceTest, add_element, mget_query_fake
 
-XMNLS_BP_FORM = 'http://openrosa.org/formdesigner/2864010F-B1B1-4711-8C59-D5B2B81D65DB'
-XMLNS_THR_FORM = 'http://openrosa.org/formdesigner/F1B73934-8B70-4CEE-B462-3E4C81F80E4A'
 XMLNS_DELIVERY_FORM = 'http://openrosa.org/formdesigner/376FA2E1-6FD1-4C9E-ACB4-E046038CD5E2'
-XMLNS_PNC_FORM = 'http://openrosa.org/formdesigner/D4A7ABD2-A7B8-431B-A88B-38245173B0AE'
-XMLNS_EBF_FORM = 'http://openrosa.org/formdesigner/89097FB1-6C08-48BA-95B2-67BCF0C5091D'
 XMLNS_GMP_FORM = 'http://openrosa.org/formdesigner/b183124a25f2a0ceab266e4564d3526199ac4d75'
-XMLNS_DAILYFEEDING_FORM = 'http://openrosa.org/formdesigner/66d52f84d606567ea29d5fae88f569d2763b8b62'
-XMLNS_HH_REG_FORM = 'http://openrosa.org/formdesigner/1D568275-1D19-46DB-8C54-2C9765DF6335'
-XMLNS_ADD_MEMBER_FORM = 'http://openrosa.org/formdesigner/756ec44475658f3f463f8012632def2bc9fbe731'
 
 NUTRITION_STATUS_NORMAL = "green"
 NUTRITION_STATUS_MODERATE = "yellow"
@@ -173,25 +166,6 @@ class TestChildHealthDataSource(BaseICDSDatasourceTest):
             case.append(case_update)
         form.append(case)
         add_element(form, 'zscore_grading_wfa', nutrition_status)
-
-        self._submit_form(form)
-
-    def _submit_dailyfeeding_form(
-            self, form_date, case_id):
-
-        form = ElementTree.Element('data')
-        form.attrib['xmlns'] = XMLNS_DAILYFEEDING_FORM
-        form.attrib['xmlns:jrm'] = 'http://openrosa.org/jr/xforms'
-
-        meta = ElementTree.Element('meta')
-        add_element(meta, 'timeEnd', form_date.isoformat())
-        form.append(meta)
-
-        case = ElementTree.Element('case')
-        case.attrib['date_modified'] = form_date.isoformat()
-        case.attrib['case_id'] = case_id
-        case.attrib['xmlns'] = 'http://commcarehq.org/case/transaction/v2'
-        form.append(case)
 
         self._submit_form(form)
 
@@ -731,40 +705,6 @@ class TestChildHealthDataSource(BaseICDSDatasourceTest):
                  ('nutrition_status_weighed', 0),
                  ('nutrition_status_unweighed', 1)],
              ),
-        ]
-        self._run_iterative_monthly_test(case_id=case_id, cases=cases)
-
-    def test_pse(self):
-        case_id = uuid.uuid4().hex
-        self._create_case(
-            case_id=case_id,
-            dob=date(2012, 1, 12),
-            date_opened=datetime(2014, 2, 1),
-            date_modified=datetime(2016, 3, 12),
-        )
-
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 2), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 3), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 4), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 5), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 6), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 7), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 8), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 9), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 10), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 11), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 12), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 13), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 14), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 15), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 16), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 2, 17), case_id=case_id)
-        self._submit_dailyfeeding_form(form_date=datetime(2016, 3, 3), case_id=case_id)
-
-        cases = [
-            (0, [('pse_days_attended', 16), ('pse_attended_16_days', 1)]),
-            (1, [('pse_days_attended', 1), ('pse_attended_16_days', 0)]),
-            (2, [('pse_days_attended', 0), ('pse_attended_16_days', 0)]),
         ]
         self._run_iterative_monthly_test(case_id=case_id, cases=cases)
 
