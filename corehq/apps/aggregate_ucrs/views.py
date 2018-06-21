026cf108e0cebd661eb77d68eef95804bc3990c3
Cory Zue <czue@dimagi.com>
2018-06-21 14:49:22 +0200

    preemptive stickler attacks

--- corehq/apps/aggregate_ucrs/views.py
+++ corehq/apps/aggregate_ucrs/views.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, unicode_literals
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
