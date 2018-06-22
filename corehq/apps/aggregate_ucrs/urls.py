a422ab70ddae8568eacf50e9d9dc9403b45fc6ed
Cory Zue <czue@dimagi.com>
2018-06-22 09:27:18 +0200

    add export view for aggregate ucrs

--- corehq/apps/aggregate_ucrs/urls.py
+++ corehq/apps/aggregate_ucrs/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
         name=views.AggregateUCRView.urlname),
     url(r'^preview/(?P<table_id>[\w-]+)/$', views.PreviewAggregateUCRView.as_view(),
         name=views.PreviewAggregateUCRView.urlname),
+    url(r'^export/(?P<table_id>[\w-]+)/$', views.export_aggregate_ucr,
+        name='export_aggregate_ucr'),
     url(r'^rebuild/(?P<table_id>[\w-]+)/$', views.rebuild_aggregate_ucr,
         name='rebuild_aggregate_ucr'),
+
 ]
