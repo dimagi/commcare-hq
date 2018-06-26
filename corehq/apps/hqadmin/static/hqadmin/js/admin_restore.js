8966458af230ab7f83683d7701b8e5342345c45d
Farid Rener <proteusvacuum@gmail.com>
2018-06-26 14:06:03 -0400

    Use ace editor for admin restore page

--- corehq/apps/hqadmin/static/hqadmin/js/admin_restore.js
+++ corehq/apps/hqadmin/static/hqadmin/js/admin_restore.js
@@ -2,5 +2,20 @@
 hqDefine('hqadmin/js/admin_restore', function () {
     $(function() {
         $("#timingTable").treetable();
+        var element = document.getElementById("payload");
+        var editor = ace.edit(
+                element,
+                {
+                    showPrintMargin: false,
+                    maxLines: 40,
+                    minLines: 3,
+                    fontSize: 14,
+                    wrap: true,
+                    useWorker: false, // enable the worker to show syntax errors
+                }
+            );
+        editor.session.setMode('ace/mode/xml');
+        editor.setReadOnly(true);
+        editor.session.setValue($("#payload").data('payload'));
     });
 });
