61efcf59cddf165598da71d5c664b3447368c441
Jennifer Schweers <orange.jenny@gmail.com>
2018-06-26 16:08:50 -0400

    lint

--- corehq/apps/accounting/static/accounting/js/enterprise_dashboard.js
+++ corehq/apps/accounting/static/accounting/js/enterprise_dashboard.js
@@ -30,7 +30,7 @@ hqDefine("accounting/js/enterprise_dashboard", [
                         alertUser.alert_user(data.message, "success");
                         $button.enableButton();
                     },
-                    error: function(data) {
+                    error: function() {
                         alertUser.alert_user(gettext("Error sending email, please try again or report an issue if this persists."), "danger");
                         $button.enableButton();
                     },
