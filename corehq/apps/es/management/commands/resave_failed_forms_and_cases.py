1591adaa2a1a60b670511c0beba88eec8471b444
Jonathan Emord <jonathan.emord@gmail.com>
2018-06-27 13:34:09 -0400

    Coerce to correct data structure

--- corehq/apps/es/management/commands/resave_failed_forms_and_cases.py
+++ corehq/apps/es/management/commands/resave_failed_forms_and_cases.py
@@ -68,14 +68,14 @@ def perform_resave_on_xforms(domain, start_date, end_date):
         return
     form_accessor = FormAccessors(domain)
     for xform_ids in chunked(with_progress_bar(xform_ids_missing_in_es), 100):
-        xforms = form_accessor.get_forms(xform_ids)
+        xforms = form_accessor.get_forms(list(xform_ids))
         found_xform_ids = set()
 
         for xform in xforms:
             resave_form(domain, xform)
             found_xform_ids.add(xform.form_id)
 
-        for xform_id in xform_ids - found_xform_ids:
+        for xform_id in set(xform_ids) - found_xform_ids:
             print("form not found %s" % xform_id)
 
 
@@ -90,12 +90,12 @@ def perform_resave_on_cases(domain, start_date, end_date):
         return
     case_accessor = CaseAccessors(domain)
     for case_ids in chunked(with_progress_bar(case_ids_missing_in_es), 100):
-        cases = case_accessor.get_cases(case_ids)
+        cases = case_accessor.get_cases(list(case_ids))
         found_case_ids = set()
 
         for case in cases:
             resave_case(domain, case, send_post_save_signal=False)
             found_case_ids.add(case.case_id)
 
-        for case_id in case_ids - found_case_ids:
+        for case_id in set(case_ids) - found_case_ids:
             print("case not found %s" % case_id)
