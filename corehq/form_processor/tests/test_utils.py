9a829418eede139456ba136f41c859e9e1497e79
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-03 14:46:36 -0400

    Add FormSubmissionBuilder tests

--- corehq/form_processor/tests/test_utils.py
+++ corehq/form_processor/tests/test_utils.py
@@ -31,3 +31,32 @@ class FormSubmissionBuilderTest(SimpleTestCase):
     def test_invalid_tag(self):
         with self.assertRaises(ValueError):
             build_form_xml_from_property_dict({'no spaces': 'are allowed'})
+
+    def test_property_dict_to_xml_group(self):
+        body = OrderedDict()
+        organs = OrderedDict()
+        organs['stomach'] = 'one'
+        organs['kidneys'] = 'two'
+        body['head'] = 'one'
+        body['organs'] = organs
+        body['feet'] = 'two'
+        self.assertEqual(
+            '<head>one</head><organs><stomach>one</stomach><kidneys>two</kidneys></organs><feet>two</feet>',
+            build_form_xml_from_property_dict(body)
+        )
+
+    def test_property_dict_to_xml_repeat_group(self):
+        breakfast = OrderedDict()
+        breakfast['cup_of_tea'] = [
+            OrderedDict([('flavor', 'hibiscus')]),
+            OrderedDict([('flavor', 'assam')]),
+        ]
+        breakfast['toast'] = [
+            'wheat',
+            'white',
+        ]
+        breakfast['muffin'] = 'banana'
+        self.assertEqual(
+            '<cup_of_tea><flavor>hibiscus</flavor></cup_of_tea><cup_of_tea><flavor>assam</flavor></cup_of_tea><toast>wheat</toast><toast>white</toast><muffin>banana</muffin>',
+            build_form_xml_from_property_dict(breakfast)
+        )
