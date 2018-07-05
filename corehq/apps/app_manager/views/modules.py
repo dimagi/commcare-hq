4d9a1ab0a4b619f0be5ec4216742dd6ec6539b94
Ethan Soergel <esoergel@gmail.com>
2018-07-05 16:33:03 -0400

    Don't try to do version comparison in JS

--- corehq/apps/app_manager/views/modules.py
+++ corehq/apps/app_manager/views/modules.py
@@ -257,7 +257,7 @@ def _get_report_module_context(app, module):
             'columnXpathTemplate': get_column_xpath_client_template(app.mobile_ucr_restore_version),
             'dataPathPlaceholders': data_path_placeholders,
             'languages': app.langs,
-            'mobileUcrVersion': app.mobile_ucr_restore_version,
+            'mobileUcrV1': app.mobile_ucr_restore_version == MOBILE_UCR_VERSION_1,
             'globalSyncDelay': Domain.get_by_name(app.domain).default_mobile_ucr_sync_interval,
         },
         'static_data_options': {
