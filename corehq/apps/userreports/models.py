5fd78b73bed0494002b2a8e156465e5f9018c8eb
Simon Kelly <skelly@dimagi.com>
2018-07-05 11:56:08 +0200

    iterate over values

--- corehq/apps/userreports/models.py
+++ corehq/apps/userreports/models.py
@@ -704,7 +704,7 @@ class StaticDataSourceConfiguration(JsonObject):
     def by_domain(cls, domain):
         return [
             cls._get_datasource_config(wrapped, dom)
-            for dom, wrapped in cls.by_id_mapping().items()
+            for dom, wrapped in cls.by_id_mapping().values()
             if domain == dom
         ]
 
@@ -789,7 +789,7 @@ class StaticReportConfiguration(JsonObject):
         """
         return [
             cls._get_report_config(wrapped, dom)
-            for dom, wrapped in cls.by_id_mapping().items()
+            for dom, wrapped in cls.by_id_mapping().values()
             if domain == dom
         ]
 
