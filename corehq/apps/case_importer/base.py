2e1b6ad7157b67d567db9887a4cf30f485ab9fb9
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-17 10:46:05 -0400

    Inherit from non-async page

--- corehq/apps/case_importer/base.py
+++ corehq/apps/case_importer/base.py
@@ -7,7 +7,6 @@ from django.utils.translation import ugettext_lazy
 class ImportCases(DataInterface):
     name = ugettext_lazy("Import Cases from Excel")
     slug = "import_cases"
-    description = ugettext_lazy("Import case data from an external Excel file")
     report_template_path = "case_importer/import_cases.html"
-    gide_filters = True
+    hide_filters = True
     asynchronous = False
