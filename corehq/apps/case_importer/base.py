435875d1ad9970d149534369444f4c093c0fc39a
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-17 11:44:15 -0400

    Use base_section.html markup instead of custom markup in import_cases.html

--- corehq/apps/case_importer/base.py
+++ corehq/apps/case_importer/base.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from corehq.apps.data_interfaces.interfaces import DataInterface
+from corehq.apps.data_interfaces.views import DataInterfaceSection
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy
 
 
@@ -10,3 +12,16 @@ class ImportCases(DataInterface):
     report_template_path = "case_importer/import_cases.html"
     hide_filters = True
     asynchronous = False
+
+    @property
+    def template_context(self):
+        return {
+            'current_page': {
+                'title': self.name,
+                'page_name': self.name,
+            },
+            'section': {
+                'page_name': DataInterfaceSection.section_name,
+                'url': reverse(DataInterfaceSection.urlname, args=[self.domain]),
+            },
+        }
