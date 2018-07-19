42e27a6a274958c43618799de24d5e3ef2dd55d5
Nick Pellegrino <npellegrino@dimagi.com>
2018-07-18 20:14:47 -0400

    delete CaseType

--- corehq/apps/domain/deletion.py
+++ corehq/apps/domain/deletion.py
@@ -174,6 +174,7 @@ DOMAIN_DELETE_OPERATIONS = [
     ModelDeletion('case_search', 'CaseSearchQueryAddition', 'domain'),
     ModelDeletion('case_search', 'FuzzyProperties', 'domain'),
     ModelDeletion('case_search', 'IgnorePatterns', 'domain'),
+    ModelDeletion('data_dictionary', 'CaseType', 'domain'),
 ]
 
 
