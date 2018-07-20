fcc055053905e063f0ae532233d2e1faa2073733
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-20 09:43:16 -0400

    Project Settings > Basic

--- corehq/apps/domain/static/domain/js/info_base.js
+++ corehq/apps/domain/static/domain/js/info_base.js
@@ -1,10 +1,20 @@
-hqDefine("domain/js/info_base", function() {
+hqDefine("domain/js/info_base", [
+    'jquery',
+    'underscore',
+    'hqwebapp/js/initial_page_data',
+    'bootstrap3-typeahead/bootstrap3-typeahead.min',
+    'hqwebapp/js/bootstrap-multi-typeahead',
+], function(
+    $,
+    _,
+    initialPageData
+) {
     $(function() {
-        _.each(hqImport('hqwebapp/js/initial_page_data').get('autocomplete_fields'), function(field) {
+        _.each(initialPageData.get('autocomplete_fields'), function(field) {
             $("#id_" + field).focus(function() {
                 if (!$("#id_" + field).data('loaded')) {
                     $("#id_" + field).data('loaded', 'true');
-                    $.getJSON(hqImport("hqwebapp/js/initial_page_data").reverse("domain_autocomplete_fields", field), function(results) {
+                    $.getJSON(initialPageData.reverse("domain_autocomplete_fields", field), function(results) {
                         $("#id_" + field).typeahead({
                             source: results,
                             items: 8,
