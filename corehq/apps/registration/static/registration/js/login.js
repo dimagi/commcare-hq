2f25225dee66fe6d0b71b806c349deaa4e47c206
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-22 17:25:45 -0400

    Login page

--- corehq/apps/registration/static/registration/js/login.js
+++ corehq/apps/registration/static/registration/js/login.js
@@ -1,9 +1,17 @@
-/* globals Blazy */
-$(function () {
-    // Blazy for loading images asynchronously
-    // Usage: specify the b-lazy class on an element and adding the path
-    // to the image in data-src="{% static 'path/to/image.jpg' %}"
-    new Blazy({
-        container: 'body',
+hqDefine('registration/js/login', [
+    'jquery',
+    'blazy/blazy',
+    'nic_compliance/js/encoder',
+], function(
+    $,
+    Blazy
+) {
+    $(function () {
+        // Blazy for loading images asynchronously
+        // Usage: specify the b-lazy class on an element and adding the path
+        // to the image in data-src="{% static 'path/to/image.jpg' %}"
+        new Blazy({
+            container: 'body',
+        });
     });
 });
