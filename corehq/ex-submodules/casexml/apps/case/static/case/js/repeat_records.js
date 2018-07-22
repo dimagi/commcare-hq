e1adb051ede4c95dc40aa09d0207c68fae779802
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-22 17:03:28 -0400

    Repeat Records tab on cases data page

--- corehq/ex-submodules/casexml/apps/case/static/case/js/repeat_records.js
+++ corehq/ex-submodules/casexml/apps/case/static/case/js/repeat_records.js
@@ -1,22 +1,29 @@
-$(function(){
-    $('.requeue-record-payload').click(function() {
-        var $btn = $(this),
-            recordId = $btn.data().recordId,
-            reverse = hqImport('hqwebapp/js/initial_page_data').reverse;
-        $btn.disableButton();
-        $.post({
-            url: reverse('requeue_repeat_record'), // this url is added to the page in case_details.html
-            data: { record_id: recordId },
-            success: function() {
-                $btn.removeSpinnerFromButton();
-                $btn.text(gettext('Requeued'));
-                $btn.addClass('btn-success');
-            },
-            error: function() {
-                $btn.removeSpinnerFromButton();
-                $btn.text(gettext('Failed'));
-                $btn.addClass('btn-danger');
-            },
+hqDefine("case/js/repeat_records", [
+    'jquery',
+    'hqwebapp/js/initial_page_data',
+], function(
+    $,
+    initialPageData
+) {
+    $(function(){
+        $('.requeue-record-payload').click(function() {
+            var $btn = $(this),
+                recordId = $btn.data().recordId;
+            $btn.disableButton();
+            $.post({
+                url: initialPageData.reverse('requeue_repeat_record'), // this url is added to the page in case_details.html
+                data: { record_id: recordId },
+                success: function() {
+                    $btn.removeSpinnerFromButton();
+                    $btn.text(gettext('Requeued'));
+                    $btn.addClass('btn-success');
+                },
+                error: function() {
+                    $btn.removeSpinnerFromButton();
+                    $btn.text(gettext('Failed'));
+                    $btn.addClass('btn-danger');
+                },
+            });
         });
     });
 });
