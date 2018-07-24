3835e3b5ebf848b1f7a042865dfedc0c08548275
Nick Pellegrino <npellegrino@dimagi.com>
2018-07-24 17:46:14 -0400

    keep one test around in a block comment

--- corehq/blobs/tests/test_migrate.py
+++ corehq/blobs/tests/test_migrate.py
@@ -325,3 +325,61 @@ class TestMigrateBackend(TestCase):
                 data = content.read()
                 self.assertEqual(data, b'binary data not valid utf-8 \xe4\x94')
                 self.assertEqual(len(data), meta.content_length)
+
+
+"""
+Tests below are commented out because they are slow and no longer relevant
+to any current envs (all envs have been migrated). Kept for reference when
+writing new migrations.
+class TestApplicationMigrations(BaseMigrationTest):
+
+    slug = "applications"
+    doc_type_map = {
+        "Application": Application,
+        "RemoteApp": RemoteApp,
+        "Application-Deleted": Application,
+        "RemoteApp-Deleted": RemoteApp,
+    }
+    def test_migrate_happy_path(self):
+        apps = {}
+        form = '<fake xform source>\u2713</fake>'
+        for doc_type, model_class in self.doc_type_map.items():
+            app = model_class(domain="test")
+            app.save()
+            super(BlobMixin, app).put_attachment(form, "form.xml")
+            app.doc_type = doc_type
+            app.save()
+            apps[doc_type] = app
+        # add legacy attribute to make sure the migration uses doc_type.wrap()
+        app = apps["Application"]
+        db = app.get_db()
+        doc = db.get(app._id, wrapper=None)
+        doc["commtrack_enabled"] = True
+        db.save_doc(doc)
+        apps["Application"] = Application.get(app._id)  # update _rev
+        self.do_migration(list(apps.values()))
+        for app in apps.values():
+            exp = type(app).get(app._id)
+            self.assertEqual(exp.fetch_attachment("form.xml"), form)
+    def test_migrate_with_concurrent_modification(self):
+        apps = {}
+        new_form = 'something new'
+        old_form = 'something old'
+        for doc_type, model_class in self.doc_type_map.items():
+            app = model_class(domain="test")
+            app.save()
+            super(BlobMixin, app).put_attachment(old_form, "form.xml")
+            super(BlobMixin, app).put_attachment(old_form, "other.xml")
+            app.doc_type = doc_type
+            app.save()
+            self.assertEqual(len(app._attachments), 2)
+            apps[app] = (1, 1)
+        def modify(app):
+            # put_attachment() calls .save()
+            type(app).get(app._id).put_attachment(new_form, "form.xml")
+        self.do_failed_migration(apps, modify)
+        for app in apps:
+            exp = type(app).get(app._id)
+            self.assertEqual(exp.fetch_attachment("form.xml"), new_form)
+            self.assertEqual(exp.fetch_attachment("other.xml"), old_form)
+"""
