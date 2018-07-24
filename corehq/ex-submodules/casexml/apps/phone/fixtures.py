02c28ce7220e5748f26d492dea2ce4de26532b8e
Jonathan Emord <jonathan.emord@gmail.com>
2018-07-24 12:07:02 -0400

    Remove unused fixture by id funtionality

--- corehq/ex-submodules/casexml/apps/phone/fixtures.py
+++ corehq/ex-submodules/casexml/apps/phone/fixtures.py
@@ -62,45 +62,7 @@ class FixtureGenerator(object):
         if not isinstance(user, OTARestoreUser):
             return []
 
-        if fixture_id:
-            full_id = fixture_id
-            prefix = fixture_id.split(':', 1)[0]
-
-            def provider_matches(provider):
-                # some providers generate fixtures with dynamic ID's e.g. item-list:my-item-list
-                # in which case provider.id is just the prefix.
-                return provider.id == full_id or provider.id == prefix
-
-            providers = [provider for provider in self._generator_providers if provider_matches(provider)]
-        else:
-            providers = self._generator_providers
-
-        return providers
-
-    def _get_fixtures(self, restore_user, fixture_id=None):
-        providers = self.get_providers(restore_user, fixture_id=fixture_id)
-        restore_state = _get_restore_state(restore_user)
-        return itertools.chain(*[
-            provider(restore_state)
-            for provider in providers
-        ])
-
-    def get_fixture_by_id(self, fixture_id, restore_user):
-        """
-        Only get fixtures with the specified ID.
-        """
-        fixtures = self._get_fixtures(restore_user, fixture_id)
-        for fixture in fixtures:
-            if isinstance(fixture, six.binary_type):
-                # could be bytes if it's coming from cache
-                cached_fixtures = ElementTree.fromstring(
-                    b"<cached-fixture>%s</cached-fixture>" % fixture
-                )
-                for fixture in cached_fixtures:
-                    if fixture.attrib.get("id") == fixture_id:
-                        return fixture
-            elif fixture.attrib.get("id") == fixture_id:
-                return fixture
+        return self._generator_providers
 
 
 generator = FixtureGenerator()
