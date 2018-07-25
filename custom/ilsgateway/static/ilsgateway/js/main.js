293d9d4a2fcf848bab6ecc56bfeea91ca2299f63
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-24 20:26:25 -0400

    Move js from supervision_docs.html

--- custom/ilsgateway/static/ilsgateway/js/main.js
+++ custom/ilsgateway/static/ilsgateway/js/main.js
@@ -2,6 +2,11 @@
  * Main module for non-report ilsgateway pages
  */
 hqDefine("ilsgateway/js/main", function() {
+    // Supervision Docs page
+    $('.delete').click(function() {
+        $(this).parent().find('.modal').modal();
+    });
+
     // Pending Recalculations page
     var $recalculations = $('#recalculations');
     if ($recalculations.length) {
