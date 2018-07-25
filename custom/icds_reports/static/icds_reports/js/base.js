4670b85431b887b2b4bb1092f83aa242289d1a80
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-24 20:13:15 -0400

    move js

--- custom/icds_reports/static/icds_reports/js/base.js
+++ custom/icds_reports/static/icds_reports/js/base.js
@@ -1,2 +1,14 @@
 hqDefine("icds_reports/js/base", function() {
+    $(function() {
+        $('#fieldset_location_async').on('change', 'select', function(e) {
+            e.stopPropagation();
+            var state = $('select.form-control')[0].selectedIndex;
+            var applyBtn = $('#apply-filters');
+            if (state == 0) {
+                applyBtn.disableButtonNoSpinner();
+            } else {
+                applyBtn.enableButton();
+            }
+        })
+    })
 });
