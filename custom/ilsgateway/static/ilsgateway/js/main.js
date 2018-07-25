ad507542d94828248b7ed44b64d70fdc1367ac93
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-24 20:25:05 -0400

    Move js from pending_recalculations.html

--- custom/ilsgateway/static/ilsgateway/js/main.js
+++ custom/ilsgateway/static/ilsgateway/js/main.js
@@ -2,4 +2,9 @@
  * Main module for non-report ilsgateway pages
  */
 hqDefine("ilsgateway/js/main", function() {
+    // Pending Recalculations page
+    var $recalculations = $('#recalculations');
+    if ($recalculations.length) {
+        $recalculations.dataTable();
+    }
 });
