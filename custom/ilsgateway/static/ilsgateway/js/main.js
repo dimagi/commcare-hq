e2883f536ac4bf805bd9f05a4bc7ad1bcafefc1b
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-24 20:30:33 -0400

    Move js from edit_location.html

--- custom/ilsgateway/static/ilsgateway/js/main.js
+++ custom/ilsgateway/static/ilsgateway/js/main.js
@@ -13,5 +13,22 @@ hqDefine("ilsgateway/js/main", function() {
         if ($recalculations.length) {
             $recalculations.dataTable();
         }
+
+        // Edit Location page
+        var $editLocations = $('#edit_locations');
+        if ($editLocations.length) {
+            var multiselect_utils = hqImport('hqwebapp/js/multiselect_utils');
+            multiselect_utils.createFullMultiselectWidget(
+                'id_selected_ids',
+                gettext("Available Locations"),
+                gettext("Locations in Program"),
+                gettext("Search Locations...")
+            );
+
+            var LocationModelView = function() {
+                this.isPilot = ko.observable($('#id_is_pilot').get(0).checked);
+            };
+            ko.applyBindings(new LocationModelView(), $editLocations.get(0));
+        }
     });
 });
