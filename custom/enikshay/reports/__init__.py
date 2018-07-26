9b3a7bd76d1f6b22f48ea510f654de2540d4e882
Jennifer Schweers <orange.jenny@gmail.com>
2018-07-26 11:18:02 -0400

    HistoricalAdherenceReport

--- custom/enikshay/reports/__init__.py
+++ custom/enikshay/reports/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from custom.enikshay.reports.adherence import AdherenceReport
 from custom.enikshay.reports.case_finding import CaseFindingReport
-from custom.enikshay.reports.historical_adherence import HistoricalAdherenceReport
 from custom.enikshay.reports.treatment_outcome import TreatmentOutcomeReport
 from custom.enikshay.reports.repeaters import ENikshayForwarderReport, ENikshayVoucherReport
 from custom.enikshay.reports.worker_activity import EnikshayWorkerActivityReport
@@ -12,7 +11,6 @@ CUSTOM_REPORTS = (
         CaseFindingReport,
         TreatmentOutcomeReport,
         AdherenceReport,
-        HistoricalAdherenceReport,
         ENikshayForwarderReport,
         EnikshayWorkerActivityReport,
         ENikshayVoucherReport,
