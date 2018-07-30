69e2f1212e744b092a91d6175d5d1376130abdb7
Norman Hooper <nhooper@dimagi.com>
2018-07-30 21:18:06 +0200

    One toggle for DHIS2

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -502,13 +502,6 @@ DHIS2_INTEGRATION = StaticToggle(
     [NAMESPACE_DOMAIN]
 )
 
-DHIS2_REPEATER_INTEGRATION = StaticToggle(
-    'dhis2_repeater_integration',
-    'DHIS2 Repeater Integration (Needed DHIS2 Integration enabled)',
-    TAG_SOLUTIONS,
-    [NAMESPACE_DOMAIN]
-)
-
 GRAPH_CREATION = StaticToggle(
     'graph-creation',
     'Case list/detail graph creation',
