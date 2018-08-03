9c8fde3c17d77726e8faa49764ff8f33834f7c43
Gabriella Bova <bova3.0@gmail.com>
2018-08-03 16:21:12 -0400

    WIP: Broken in a different way, load ConfirmBilling info and create scheduled subscription

--- corehq/apps/accounting/static/accounting/js/confirm_plan.js
+++ corehq/apps/accounting/static/accounting/js/confirm_plan.js
@@ -1,41 +1,59 @@
 hqDefine('accounting/js/confirm_plan', function () {
-    var confirmPlanModel = function (isUpgrade, nextInvoiceDate, currentPlan, isDowngradeBeforeMinimum,
+    var confirmPlanModel = function (isUpgrade, nextInvoiceDate, currentPlan, newPlan, isDowngradeBeforeMinimum,
                                      currentSubscriptionEndDate) {
-        debugger;
         'use strict';
         var self = {};
         self.isUpgrade = isUpgrade;
-        self.nextInvoiceDate = nextInvoiceDate;
+        self.nextInvoiceDate = nextInvoiceDate;                         // TODO: Delete if unnecessary
         self.currentPlan = currentPlan;
-        self.isDowngradeBeforeMinimum = isDowngradeBeforeMinimum;
-        self.currentSubscriptionEndDate = currentSubscriptionEndDate;
+        self.newPlan = newPlan;
+        self.isDowngradeBeforeMinimum = isDowngradeBeforeMinimum;       // TODO: Delete if unnecessary
+        self.currentSubscriptionEndDate = currentSubscriptionEndDate;   // TODO: Delete if unnecessary
 
-        self.openDowngradeModal = function() {
-            debugger;
-            // var editionSlugs = _.map(self.editions(), function(e) { return e.slug(); });
-            // self.form = $(e.currentTarget).closest("form");
-            // if (editionSlugs.indexOf(self.selected_edition()) < editionSlugs.indexOf(self.currentEdition)) {
-            //     var $modal = $("#modal-downgrade");
-            //     $modal.modal('show');
-            // } else {
-            //     self.form.submit();
-            // }
+        self.openDowngradeModal = function(confirmPlanModel) {
+            if (confirmPlanModel.isUpgrade) {
+                self.submitPlanChange();
+            } else {
+                var $modal = $("#modal-downgrade");
+                $modal.modal('show');
+            }
         };
-        self.submitDowngrade = function() {
-            debugger;
-            // var $button = $(e.currentTarget);
-            // $button.disableButton();
-            // $.ajax({
-            //     method: "POST",
-            //     url: hqImport('hqwebapp/js/initial_page_data').reverse('email_on_downgrade'),
-            //     data: {
-            //         old_plan: self.currentEdition,
-            //         new_plan: self.selected_edition(),
-            //         note: $button.closest(".modal").find("textarea").val(),
-            //     },
-            //     success: finish,
-            //     error: finish,
-            // });
+        self.submitDowngrade = function(pricingTable, e) {
+            var finish = function() {
+                self.submitPlanChange();
+            };
+
+            var $button = $(e.currentTarget);
+            $button.disableButton();
+            $.ajax({
+                method: "POST",
+                url: hqImport('hqwebapp/js/initial_page_data').reverse('email_on_downgrade'),
+                data: {
+                    old_plan: self.currentPlan.name,
+                    new_plan: self.newPlan.name,
+                    note: $button.closest(".modal").find("textarea").val(),
+                },
+                success: finish,
+                error: finish
+            });
+        };
+        self.submitPlanChange = function () {
+            var success = function () {
+                console.log("Success in confirm_billing_account_info");
+            };
+            var error = function () {
+                console.log("Error in confirm_billing_account_info");
+            };
+
+            $.ajax({
+                method: "POST",
+                class: "form",
+                // data: {plan_edition: self.newPlan.edition},
+                data: new FormData($(this)),
+                url: hqImport('hqwebapp/js/initial_page_data').reverse('confirm_billing_account_info'),
+                success: success,
+                error: error
+            });
         };
 
         return self;
@@ -48,9 +66,11 @@ hqDefine('accounting/js/confirm_plan', function () {
             initial_page_data('is_upgrade'),
             initial_page_data('next_invoice_date'),
             initial_page_data('current_plan'),
+            initial_page_data('new_plan'),
             initial_page_data('is_downgrade_before_minimum'),
             initial_page_data('current_subscription_end_date')
         );
+        $('#confirm-plan').koApplyBindings(confirmPlanModel);
         $('#modal-downgrade').koApplyBindings(confirmPlanModel);
     }());
 });
