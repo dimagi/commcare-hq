c1b49703a60c9cc2e86fe5873bbeb2f68ae1713f
Farid Rener <proteusvacuum@gmail.com>
2018-08-03 14:00:02 -0400

    Remove ENIKSHAY_APPS from settings

--- settings.py
+++ settings.py
@@ -370,12 +370,6 @@ HQ_APPS = (
     'custom.champ',
 )
 
-
-ENIKSHAY_APPS = (
-    'custom.enikshay',
-    'custom.enikshay.two_b_datamigration',
-)
-
 # also excludes any app starting with 'django.'
 APPS_TO_EXCLUDE_FROM_TESTS = (
     'captcha',
@@ -406,7 +400,7 @@ APPS_TO_EXCLUDE_FROM_TESTS = (
 )
 
 # any built-in management commands we want to override should go in hqscripts
-INSTALLED_APPS = ('hqscripts',) + DEFAULT_APPS + HQ_APPS + ENIKSHAY_APPS
+INSTALLED_APPS = ('hqscripts',) + DEFAULT_APPS + HQ_APPS
 
 # after login, django redirects to this URL
 # rather than the default 'accounts/profile'
