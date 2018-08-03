cae6d03c1b3076ba032071f8c8106da26401b60d
Farid Rener <proteusvacuum@gmail.com>
2018-08-03 15:38:35 -0400

    Remove enikshay specific urls

--- urls.py
+++ urls.py
@@ -75,7 +75,6 @@ domain_specific = [
     url(r'^dashboard/', include('corehq.apps.dashboard.urls')),
     url(r'^configurable_reports/', include('corehq.apps.userreports.urls')),
     url(r'^', include('custom.icds_reports.urls')),
-    url(r'^', include('custom.enikshay.urls')),
     url(r'^champ_cameroon/', include('custom.champ.urls')),
     url(r'^motech/', include('corehq.motech.urls')),
     url(r'^dhis2/', include('corehq.motech.dhis2.urls')),
