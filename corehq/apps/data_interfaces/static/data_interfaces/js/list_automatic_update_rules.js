349a0433d48a9288dae2fbf8536b1cd04d005caf
Jennifer Schweers <orange.jenny@gmail.com>
2018-08-06 14:25:20 -0400

    Setup for row-specific javascript interactions

--- corehq/apps/data_interfaces/static/data_interfaces/js/list_automatic_update_rules.js
+++ corehq/apps/data_interfaces/static/data_interfaces/js/list_automatic_update_rules.js
@@ -1,5 +1,44 @@
-hqDefine("data_interfaces/js/list_automatic_update_rules", function() {
-    'use strict';
+hqDefine("data_interfaces/js/list_automatic_update_rules", [
+    'jquery',
+    'knockout',
+    'underscore',
+    'hqwebapp/js/initial_page_data',
+    'hqwebapp/js/crud_paginated_list',
+], function(
+    $,
+    ko,
+    _,
+    initialPageData,
+    CRUDPaginatedList
+) {
+    var ruleModel = function(itemSpec, initRow) {
+        return CRUDPaginatedList.PaginatedItem(itemSpec, function(rowElems, paginatedItem) {
+            initRow(rowElems, paginatedItem);
+
+            $(rowElems).find(".activate").click(function() {
+                console.log("activate me");
+            });
+
+            $(rowElems).find(".deactivate").click(function() {
+                console.log("deactivate me");
+            });
+        });
+    };
+
+    $(function () {
+        var paginatedListModel = CRUDPaginatedList.CRUDPaginatedListModel(
+            initialPageData.get('total'),
+            initialPageData.get('limit'),
+            initialPageData.get('page'), {
+                statusCodeText: initialPageData.get('status_codes'),
+                allowItemCreation: initialPageData.get('allow_item_creation'),
+                createItemForm: initialPageData.get('create_item_form'),
+            }, ruleModel);
+
+        ko.applyBindings(paginatedListModel, $('#editable-paginated-list').get(0));
+        paginatedListModel.init();
+    });
+    /*'use strict';
     var autoUpdateRuleApp = window.angular.module('autoUpdateRuleApp', ['hq.pagination']);
     autoUpdateRuleApp.config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -68,5 +107,5 @@ hqDefine("data_interfaces/js/list_automatic_update_rules", function() {
         trackNewRule: function () {
             hqImport('analytix/js/google').track.event('Automatic Case Closure', 'Rules', 'Set Rule');
         },
-    });
+    });*/
 });
