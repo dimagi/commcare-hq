910c5f4e2f6b1c4b2a218e0b420c1521b1898e61
Gabriella Bova <bova3.0@gmail.com>
2018-08-06 12:41:44 -0400

    Load ConfirmBilling info

--- corehq/apps/accounting/static/accounting/js/confirm_plan.js
+++ corehq/apps/accounting/static/accounting/js/confirm_plan.js
@@ -1,18 +1,16 @@
 hqDefine('accounting/js/confirm_plan', function () {
-    var confirmPlanModel = function (isUpgrade, nextInvoiceDate, currentPlan, newPlan, isDowngradeBeforeMinimum,
-                                     currentSubscriptionEndDate) {
+    var confirmPlanModel = function (isUpgrade, currentPlan, newPlan) {
         'use strict';
         var self = {};
         self.isUpgrade = isUpgrade;
-        self.nextInvoiceDate = nextInvoiceDate;                         // TODO: Delete if unnecessary
         self.currentPlan = currentPlan;
         self.newPlan = newPlan;
-        self.isDowngradeBeforeMinimum = isDowngradeBeforeMinimum;       // TODO: Delete if unnecessary
-        self.currentSubscriptionEndDate = currentSubscriptionEndDate;   // TODO: Delete if unnecessary
 
-        self.openDowngradeModal = function(confirmPlanModel) {
+        self.form = undefined;
+        self.openDowngradeModal = function(confirmPlanModel, e) {
+            self.form = $(e.currentTarget).closest("form");
             if (confirmPlanModel.isUpgrade) {
-                self.submitPlanChange();
+                self.form.submit();
             } else {
                 var $modal = $("#modal-downgrade");
                 $modal.modal('show');
@@ -20,7 +18,9 @@ hqDefine('accounting/js/confirm_plan', function () {
         };
         self.submitDowngrade = function(pricingTable, e) {
             var finish = function() {
-                self.submitPlanChange();
+                if (self.form) {
+                    self.form.submit();
+                }
             };
 
             var $button = $(e.currentTarget);
@@ -37,24 +37,6 @@ hqDefine('accounting/js/confirm_plan', function () {
                 error: finish
             });
         };
-        self.submitPlanChange = function () {
-            var success = function () {
-                console.log("Success in confirm_billing_account_info");
-            };
-            var error = function () {
-                console.log("Error in confirm_billing_account_info");
-            };
-
-            $.ajax({
-                method: "POST",
-                class: "form",
-                // data: {plan_edition: self.newPlan.edition},
-                data: new FormData($(this)),
-                url: hqImport('hqwebapp/js/initial_page_data').reverse('confirm_billing_account_info'),
-                success: success,
-                error: error
-            });
-        };
 
         return self;
     };
@@ -64,11 +46,8 @@ hqDefine('accounting/js/confirm_plan', function () {
         var initial_page_data = hqImport('hqwebapp/js/initial_page_data').get;
         confirmPlanModel = confirmPlanModel(
             initial_page_data('is_upgrade'),
-            initial_page_data('next_invoice_date'),
             initial_page_data('current_plan'),
-            initial_page_data('new_plan'),
-            initial_page_data('is_downgrade_before_minimum'),
-            initial_page_data('current_subscription_end_date')
+            initial_page_data('new_plan')
         );
         $('#confirm-plan').koApplyBindings(confirmPlanModel);
         $('#modal-downgrade').koApplyBindings(confirmPlanModel);
