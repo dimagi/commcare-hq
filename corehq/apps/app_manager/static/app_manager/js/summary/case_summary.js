eda86273da3982fe8148745b4b92222d19e1d487
Jennifer Schweers <orange.jenny@gmail.com>
2018-08-09 22:09:27 -0400

    Case summary: add case type content skeleton, hooked up to menu

--- corehq/apps/app_manager/static/app_manager/js/summary/case_summary.js
+++ corehq/apps/app_manager/static/app_manager/js/summary/case_summary.js
@@ -1,13 +1,42 @@
 hqDefine('app_manager/js/summary/case_summary', function() {
+    var assertProperties = hqImport("hqwebapp/js/assert_properties").assert,
+        initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        menu = hqImport("app_manager/js/summary/menu");
+
+    var caseTypeModel = function(caseType) {
+        var self = _.extend({}, caseType);
+
+        self.isSelected = ko.observable(true);
+
+        return self;
+    };
+
+    var contentModel = function(options) {
+        assertProperties(options, ['case_types'], []);
+
+        var self = {};
+        self.caseTypes = _.map(options.case_types, function(caseType) {
+            return caseTypeModel(caseType);
+        });
+
+        self.selectedItemId = ko.observable('');      // blank indicates "View All"
+        self.selectedItemId.subscribe(function(selectedId) {
+            _.each(self.caseTypes, function(caseType) {
+                caseType.isSelected(!selectedId || selectedId === caseType.name);
+            });
+        });
+
+        return self;
+    };
+
     $(function() {
-        var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
-            menu = hqImport("app_manager/js/summary/menu");
+        var caseTypes = initialPageData.get("case_metadata").case_types;
 
         var caseSummaryMenu = menu.menuModel({
-            items: _.map(initialPageData.get("case_metadata").case_types, function(case_type) {
+            items: _.map(caseTypes, function(caseType) {
                 return menu.menuItemModel({
-                    id: case_type.name,
-                    name: case_type.name,
+                    id: caseType.name,
+                    name: caseType.name,
                     icon: "fcc fcc-fd-external-case appnav-primary-icon",
                     subitems: [],
                 });
@@ -15,6 +44,15 @@ hqDefine('app_manager/js/summary/case_summary', function() {
             viewAllItems: gettext("View All Cases"),
         });
 
+        var caseSummaryContent = contentModel({
+            case_types: caseTypes,
+        });
+
         $("#hq-sidebar > nav").koApplyBindings(caseSummaryMenu);
+        $("#js-appmanager-body").koApplyBindings(caseSummaryContent);
+
+        caseSummaryMenu.selectedItemId.subscribe(function(newValue) {
+            caseSummaryContent.selectedItemId(newValue);
+        });
     });
 });
