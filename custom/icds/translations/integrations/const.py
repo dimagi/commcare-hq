e10940c0ab346757f771331164e15bb3b479ebaf
mkangia <mkangia@dimagi.com>
2018-08-13 17:11:20 +0530

    add more languages

--- custom/icds/translations/integrations/const.py
+++ custom/icds/translations/integrations/const.py
@@ -1,7 +1,13 @@
+from __future__ import absolute_import
 from __future__ import unicode_literals
 API_USER = "api"
 SOURCE_LANGUAGE_MAPPING = {
     # 'hq_code' : 'transifex_code'
     'hin': 'hi',
     'ori': 'or',
+    'tam': 'ta',
+    'kha': 'km',  # khasi
+    'grt': 'lg',  # garo
+    'lus': 'mi',  # mizo
+    'pan': 'pa',  # punjabi
 }
