80bbe7f371aa9b44fbe1b772618d4d6de358151f
Jonathan Emord <jonathan.emord@gmail.com>
2018-08-14 15:14:13 -0400

    Remove old analytics task

--- corehq/util/decorators.py
+++ corehq/util/decorators.py
@@ -146,10 +146,6 @@ def serial_task(unique_key, default_retry_delay=30, timeout=5*60, max_retries=3,
     return decorator
 
 
-def old_analytics_task(default_retry_delay=10, max_retries=3, queue='background_queue'):
-    return analytics_task(default_retry_delay, max_retries, queue)
-
-
 def analytics_task(default_retry_delay=10, max_retries=3, queue='analytics_queue'):
     '''
         defines a task that posts data to one of our analytics endpoints. It retries the task
