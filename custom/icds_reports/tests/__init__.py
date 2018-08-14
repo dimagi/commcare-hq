77dc1d73f398d7111ec5a38607c353292c338437
Cal Ellowitz <calellowitz@gmail.com>
2018-08-14 23:57:20 +0530

    readding view creation

--- custom/icds_reports/tests/__init__.py
+++ custom/icds_reports/tests/__init__.py
@@ -135,6 +135,9 @@ def setUpModule():
         finally:
             _call_center_domain_mock.stop()
 
+        with connections['icds-ucr'].cursor() as cursor:	
+            create_views(cursor)
+
 
 def tearDownModule():
     if settings.USE_PARTITIONED_DATABASE:
