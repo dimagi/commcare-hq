16ec19a242ec2e96a318eeae4d1b26b6cf967b32
Nick Pellegrino <npellegrino@dimagi.com>
2018-08-16 15:19:23 -0700

    Revert "Case Search"
    This reverts commit ea5fba6daf79d47fbc7a622a407673edc65dcf1f.

--- corehq/apps/domain/static/domain/js/case_search_main.js
+++ corehq/apps/domain/static/domain/js/case_search_main.js
@@ -1,14 +1,8 @@
-hqDefine('domain/js/case_search_main', [
-    'jquery',
-    'hqwebapp/js/initial_page_data',
-    'domain/js/case_search',
-], function(
-    $,
-    initialPageData,
-    caseSearch
-) {
+hqDefine('domain/js/case_search_main', function() {
+    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
     $(function () {
-        var viewModel = caseSearch.CaseSearchConfig({
+        var CaseSearchConfig = hqImport('domain/js/case_search').CaseSearchConfig;
+        var viewModel = new CaseSearchConfig({
             values: initialPageData.get('values'),
             caseTypes: initialPageData.get('case_types'),
         });
