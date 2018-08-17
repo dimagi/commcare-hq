8fc5d80d17f0177af6c044d04b55f2e5fe2cdbbd
Gabriella Bova <bova3.0@gmail.com>
2018-08-17 16:30:04 -0400

    KO observable to switch between monthly and yearly

--- corehq/apps/accounting/static/accounting/js/pricing_table.js
+++ corehq/apps/accounting/static/accounting/js/pricing_table.js
@@ -175,6 +175,9 @@ hqDefine('accounting/js/pricing_table', [
         // but the only HTML ancestor the modal shares with the pricing table is <body>.
         $('#plans').koApplyBindings(pricingTable);
         $('#modal-downgrade').koApplyBindings(pricingTable);
+        $('#toggle-annual-pricing').koApplyBindings(pricingTable);
+        $('#annual-pricing-table').koApplyBindings(pricingTable);
+        $('#modal-contact-sales').koApplyBindings(pricingTable);
 
         pricingTable.init();
     });
