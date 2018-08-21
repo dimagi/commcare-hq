2ac8bf621551f4b526982d16263679886e952631
Jennifer Schweers <orange.jenny@gmail.com>
2018-08-21 13:31:11 -0400

    lint

--- corehq/apps/users/static/users/js/web_users.js
+++ corehq/apps/users/static/users/js/web_users.js
@@ -85,9 +85,8 @@ hqDefine("users/js/web_users", function() {
                 .text(gettext("Save"));
         });
         $('#save_restrict_option').click(function(e) {
-            var post_url = url("location_restriction_for_users");
             $(this).text(gettext('Saving ...'));
-            $.post(post_url, {
+            $.post(url("location_restriction_for_users"), {
                 restrict_users: $('#restrict_users')[0].checked,
             },
             function() {
@@ -102,11 +101,10 @@ hqDefine("users/js/web_users", function() {
 
         $('.resend-invite').click(function(e) {
             $(this).addClass('disabled').prop('disabled', true);
-            var post_url = url("reinvite_web_user");
-            var doc_id = this.getAttribute('data-invite');
+            var docId = this.getAttribute('data-invite');
             var self = this;
-            $.post(post_url, {
-                invite: doc_id,
+            $.post(url("reinvite_web_user"), {
+                invite: docId,
             },
             function(data) {
                 $(self).parent().text(data.response);
@@ -116,27 +114,27 @@ hqDefine("users/js/web_users", function() {
         });
 
         var $userRolesTable = $('#user-roles-table'),
-            initial_page_data = hqImport("hqwebapp/js/initial_page_data").get;
+            initialPageData = hqImport("hqwebapp/js/initial_page_data");
         hqImport('users/js/roles').initUserRoles($userRolesTable, {
-            userRoles: initial_page_data("user_roles"),
-            defaultRole: initial_page_data("default_role"),
+            userRoles: initialPageData.get("user_roles"),
+            defaultRole: initialPageData.get("default_role"),
             saveUrl: url("post_user_role"),
             deleteUrl: url("delete_user_role"),
-            reportOptions: initial_page_data("report_list"),
-            webAppsList: initial_page_data("web_apps_list"),
-            allowEdit: initial_page_data("can_edit_roles"),
-            canRestrictAccessByLocation: initial_page_data("can_restrict_access_by_location"),
-            landingPageChoices: initial_page_data("landing_page_choices"),
+            reportOptions: initialPageData.get("report_list"),
+            webAppsList: initialPageData.get("web_apps_list"),
+            allowEdit: initialPageData.get("can_edit_roles"),
+            canRestrictAccessByLocation: initialPageData.get("can_restrict_access_by_location"),
+            landingPageChoices: initialPageData.get("landing_page_choices"),
         });
         $userRolesTable.show();
 
-        function handleDeletion($el, title, body, post_url) {
+        function handleDeletion($el, title, body, postUrl) {
             var id = $el.data('id');
             $('#confirm-delete').off('click');
             $('#confirm-delete').on('click', function() {
                 var $button = $(this);
                 $button.addClass('disabled').prop('disabled', true);
-                $.post(post_url, {
+                $.post(postUrl, {
                     id: id,
                 },
                 function() {
