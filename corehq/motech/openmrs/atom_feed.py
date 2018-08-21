cc4da3aa596541a3d0a73a3b53199410c885182d
Norman Hooper <nhooper@dimagi.com>
2018-08-21 12:58:52 +0200

    Use ValueSource.deserialize() to get case values

--- corehq/motech/openmrs/atom_feed.py
+++ corehq/motech/openmrs/atom_feed.py
@@ -149,11 +149,11 @@ def get_addpatient_caseblock(case_type, owner, patient, repeater):
     property_map = get_property_map(repeater.openmrs_config.case_config)
 
     fields_to_update = {}
-    for prop, (jsonpath, value_map) in property_map.items():
+    for prop, (jsonpath, value_source) in property_map.items():
         matches = jsonpath.find(patient)
         if matches:
             patient_value = matches[0].value
-            new_value = value_map.get(patient_value, patient_value)
+            new_value = value_source.deserialize(patient_value)
             fields_to_update[prop] = new_value
 
     if fields_to_update:
@@ -175,12 +175,12 @@ def get_updatepatient_caseblock(case, patient, repeater):
     property_map = get_property_map(repeater.openmrs_config.case_config)
 
     fields_to_update = {}
-    for prop, (jsonpath, value_map) in property_map.items():
+    for prop, (jsonpath, value_source) in property_map.items():
         matches = jsonpath.find(patient)
         if matches:
             patient_value = matches[0].value
             case_value = case.get_case_property(prop)
-            new_value = value_map.get(patient_value, patient_value)
+            new_value = value_source.deserialize(patient_value)
             if case_value != new_value:
                 fields_to_update[prop] = new_value
 
--- corehq/motech/openmrs/openmrs_config.py
+++ corehq/motech/openmrs/openmrs_config.py
@@ -188,10 +188,10 @@ class OpenmrsConfig(DocumentSchema):
 # The `jsonpath` attribute is used for retrieving values from an
 # OpenMRS patient and the `value_map` attribute is for converting
 # OpenMRS concept UUIDs to CommCare property values, if necessary.
-JsonpathValuemap = namedtuple('JsonpathValuemap', ['jsonpath', 'value_map'])
+JsonpathValuesource = namedtuple('JsonpathValuesource', ['jsonpath', 'value_source'])
 
 
-def get_caseproperty_jsonpathvaluemap(jsonpath, value_source):
+def get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config):
     """
     Used for updating _property_map to map case properties to OpenMRS
     patient property-, attribute- and concept values.
@@ -200,37 +200,36 @@ def get_caseproperty_jsonpathvaluemap(jsonpath, value_source):
     do we find the OpenMRS value?"
 
     :param jsonpath: The path to a value in an OpenMRS patient JSON object
-    :param value_source: A case_config ValueSource instance
+    :param value_source_config: A case_config ValueSource instance
     :return: A single-item dictionary with the name of the case
-             property as key, and a JsonpathValuemap as value. If
-             value_source is a constant, then there is no corresponding
-             case property, so the function returns an empty dictionary
+             property as key, and a JsonpathValuesource as value. If
+             value_source_config is a ConstantString, then there is no
+             corresponding case property, so the function returns an
+             empty dictionary.
     """
-    if value_source['doc_type'] == 'ConstantString':
+    if value_source_config['doc_type'] == 'ConstantString':
         return {}
-    if value_source['doc_type'] == 'CaseProperty':
-        return {value_source['case_property']: JsonpathValuemap(jsonpath, {})}
-    if value_source['doc_type'] == 'CasePropertyMap':
-        value_map = {v: k for k, v in value_source['value_map'].items()}
-        return {value_source['case_property']: JsonpathValuemap(jsonpath, value_map)}
+    if value_source_config['doc_type'] in ('CaseProperty', 'CasePropertyMap'):
+        value_source = ValueSource.wrap(value_source_config)
+        return {value_source_config['case_property']: JsonpathValuesource(jsonpath, value_source)}
     raise ValueError(
         '"{}" is not a recognised ValueSource for setting OpenMRS patient values from CommCare case properties. '
-        'Please check your OpenMRS case config.'.format(value_source['doc_type'])
+        'Please check your OpenMRS case config.'.format(value_source_config['doc_type'])
     )
 
 
 def get_property_map(case_config):
     """
-    Returns a map of OpenMRS patient properties and attributes to case
-    properties.
+    Returns a map of case properties to OpenMRS patient properties and
+    attributes, and a ValueSource instance to deserialize them.
     """
     property_map = {}
 
-    for person_prop, value_source in case_config['person_properties'].items():
+    for person_prop, value_source_config in case_config['person_properties'].items():
         jsonpath = parse_jsonpath('person.' + person_prop)
-        property_map.update(get_caseproperty_jsonpathvaluemap(jsonpath, value_source))
+        property_map.update(get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config))
 
-    for attr_uuid, value_source in case_config['person_attributes'].items():
+    for attr_uuid, value_source_config in case_config['person_attributes'].items():
         # jsonpath_rw offers programmatic JSONPath expressions. For details on how to create JSONPath
         # expressions programmatically see the
         # `jsonpath_rw documentation <https://github.com/kennknowles/python-jsonpath-rw#programmatic-jsonpath>`__
@@ -250,17 +249,17 @@ def get_property_map(case_config):
             ),
             Fields('value')
         )
-        property_map.update(get_caseproperty_jsonpathvaluemap(jsonpath, value_source))
+        property_map.update(get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config))
 
-    for name_prop, value_source in case_config['person_preferred_name'].items():
+    for name_prop, value_source_config in case_config['person_preferred_name'].items():
         jsonpath = parse_jsonpath('person.preferredName.' + name_prop)
-        property_map.update(get_caseproperty_jsonpathvaluemap(jsonpath, value_source))
+        property_map.update(get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config))
 
-    for addr_prop, value_source in case_config['person_preferred_address'].items():
+    for addr_prop, value_source_config in case_config['person_preferred_address'].items():
         jsonpath = parse_jsonpath('person.preferredAddress.' + addr_prop)
-        property_map.update(get_caseproperty_jsonpathvaluemap(jsonpath, value_source))
+        property_map.update(get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config))
 
-    for id_type_uuid, value_source in case_config['patient_identifiers'].items():
+    for id_type_uuid, value_source_config in case_config['patient_identifiers'].items():
         if id_type_uuid == 'uuid':
             jsonpath = parse_jsonpath('uuid')
         else:
@@ -277,6 +276,6 @@ def get_property_map(case_config):
                 ),
                 Fields('identifier')
             )
-        property_map.update(get_caseproperty_jsonpathvaluemap(jsonpath, value_source))
+        property_map.update(get_caseproperty_jsonpathvaluesource(jsonpath, value_source_config))
 
     return property_map
