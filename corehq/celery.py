03283bed07438616fe1bebd65c5067e8b85cc7ba
Nick Pellegrino <npellegrino@dimagi.com>
2018-08-21 01:06:21 -0400

    cleanup

--- corehq/celery.py
+++ corehq/celery.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
-from django.conf import settings
 
 app = Celery('commcare-hq')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
