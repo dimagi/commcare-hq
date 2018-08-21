103d1bd3d0a25eaaa7c1ea0d9eebf078a5da31b5
Nick Pellegrino <npellegrino@dimagi.com>
2018-08-21 01:34:49 -0400

    pass INSTALLED_APPS

--- corehq/celery.py
+++ corehq/celery.py
@@ -1,6 +1,9 @@
 from __future__ import absolute_import, unicode_literals
+
+from django.conf import settings
+
 from celery import Celery
 
 app = Celery('commcare-hq')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
