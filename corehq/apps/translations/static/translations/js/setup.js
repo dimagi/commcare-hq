99d4da768b9b2767493c716b1a992eefb88d9044
mkangia <mkangia@dimagi.com>
2018-08-24 02:48:47 +0530

    lint [skip ci]

--- corehq/apps/translations/static/translations/js/setup.js
+++ corehq/apps/translations/static/translations/js/setup.js
@@ -1,9 +1,7 @@
 hqDefine('translations/js/setup', [
     'jquery',
     'select2-3.5.2-legacy/select2'
-], function(
-    $,
-) {
+], function($){
     $(function() {
         $("#id_source_lang").select2();
         $("#id_target_lang").select2();
