35baff5a24bf363fe7bf1170b4f3a495845a570a
mkangia <mkangia@dimagi.com>
2018-08-24 02:46:39 +0530

    add other languages which differ on transifex from HQ [skip ci]

--- custom/icds/translations/integrations/const.py
+++ custom/icds/translations/integrations/const.py
@@ -6,8 +6,12 @@ SOURCE_LANGUAGE_MAPPING = {
     'hin': 'hi',  # hindi
     'ori': 'or',  # oriya
     'tam': 'ta',  # tamil
-    'kha': 'km',  # khasi
-    'grt': 'lg',  # garo
-    'lus': 'mi',  # mizo
+    'kha': 'km',  # khasi. Can be removed once we move ICDS CAS translation to new project on transifex
+    'grt': 'lg',  # garo. Can be removed once we move ICDS CAS translation to new project on transifex
+    'lus': 'mi',  # mizo. Can be removed once we move ICDS CAS translation to new project on transifex
     'pan': 'pa',  # punjabi
+    'asm': 'as',  # assamese
+    'ben': 'bn',  # bengali
+    'guj': 'gu',  # gujarati
+    'mal': 'ml',  # malayalam
 }
