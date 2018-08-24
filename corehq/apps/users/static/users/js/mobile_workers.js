d6a477cfb098f3b3726b67cea87f4bb6cf3158e6
Jennifer Schweers <orange.jenny@gmail.com>
2018-08-24 11:25:44 -0400

    Set up spinner, error, and no users messages

--- corehq/apps/users/static/users/js/mobile_workers.js
+++ corehq/apps/users/static/users/js/mobile_workers.js
@@ -3,25 +3,56 @@ hqDefine("users/js/mobile_workers", function() {
         var self = {};
         self.users = ko.observableArray([]);
 
+        self.query = ko.observable(''); // TODO
+
+        // Visibility of spinner, messages, and user table
+        self.hasError = ko.observable(false);
+        self.notLoaded = ko.observable(true);
+        self.projectHasUsers = ko.observable(true);
+
+        self.showProjectHasNoUsers = ko.computed(function() {
+            return !self.notLoaded() && !self.hasError() && !self.projectHasUsers();
+        });
+
+        self.showNoUsers = ko.computed(function() { // TODO: test
+            return !self.notLoaded() && !self.hasError() && !self.users().length && !self.showProjectHasNoUsers();
+        });
+
+        self.showSpinner = ko.computed(function() {
+            return self.notLoaded() && !self.hasError();
+        });
+
+        self.showTable = ko.computed(function() {
+            return !self.notLoaded() && !self.hasError() && !self.showNoUsers() && !self.showProjectHasNoUsers();
+        });
+
         self.goToPage = function(page) {
             self.users.removeAll();
 
+            self.hasError(false);
+            self.notLoaded(true);
             $.ajax({
                 method: 'GET',
                 url: hqImport("hqwebapp/js/initial_page_data").reverse('paginate_mobile_workers'),
                 data: {
                     page: page || 1,
-                    query: '',  // TODO
+                    query: self.query(),
                     limit: 10,  // TODO
                 },
                 success: function(data) {
+                    self.notLoaded(false);
+                    self.hasError(false);
+                    if (!self.query()) {
+                        self.projectHasUsers(!!data.users.length);
+                    }
                     self.users.removeAll();     // just in case there are multiple goToPage calls simultaneously
                     _.each(data.users, function(user) {
                         self.users.push(user);
                     });
                 },
                 error: function() {
-                    // TODO: something generic
+                    self.notLoaded(false);
+                    self.hasError(true);
                },
             });
         };
