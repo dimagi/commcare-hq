296cbbf86b2750bb1c556d662264d42aa2ad7dd9
Nick Pellegrino <npellegrino@dimagi.com>
2018-08-26 12:30:36 -0400

    fix TestDjangoCompressOffline

--- corehq/apps/hqwebapp/tests/test_compress_command.py
+++ corehq/apps/hqwebapp/tests/test_compress_command.py
@@ -11,9 +11,9 @@ from io import open
 
 B3_BASE = 'hqwebapp/base.html'
 
-COMPRESS_JS = b' compress js '
-COMPRESS_CSS = b' compress css '
-ENDCOMPRESS = b' endcompress '
+COMPRESS_JS = ' compress js '
+COMPRESS_CSS = ' compress css '
+ENDCOMPRESS = ' endcompress '
 
 DISALLOWED_TAGS = [
     ('{% if', 'You cannot use "if" tags in a compress block'),
@@ -36,8 +36,6 @@ class TestDjangoCompressOffline(SimpleTestCase):
 
     @attr("slow")
     def test_compress_offline(self):
-        in_compress_block = False
-
         template_dir_list = []
         for template_dir in get_app_template_dirs('templates'):
             if settings.BASE_DIR in template_dir:
