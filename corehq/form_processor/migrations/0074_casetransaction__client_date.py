27c57916e6fad6ea9b8748e477b72db7e28d305d
Jordan Roth <jmtroth0@gmail.com>
2018-08-28 13:23:22 +0200

    migration conflict

--- corehq/form_processor/migrations/0074_casetransaction__client_date.py
+++ corehq/form_processor/migrations/0074_casetransaction__client_date.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('form_processor', '0072_case_attachment_drops'),
+        ('form_processor', '0073_drop_case_uuid_like_index'),
     ]
 
     operations = [
