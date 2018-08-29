0a318c6f665585a9212089e9caa078716bc0a2c1
Gabriella Bova <bova3.0@gmail.com>
2018-08-29 16:29:08 -0400

    Re-run migrations

--- corehq/apps/accounting/bootstrap/config/user_buckets_august_2018.py
+++ corehq/apps/accounting/bootstrap/config/user_buckets_august_2018.py
@@ -5,6 +5,7 @@ from decimal import Decimal
 from corehq.apps.accounting.models import (
     FeatureType,
     SoftwarePlanEdition,
+    UNLIMITED_FEATURE_USAGE
 )
 
 BOOTSTRAP_CONFIG = {
@@ -44,8 +45,8 @@ BOOTSTRAP_CONFIG = {
         'role': 'advanced_plan_v0',
         'product_rate_monthly_fee': Decimal('0.00'),
         'feature_rates': {
-            FeatureType.USER: dict(monthly_limit=10, per_excess_fee=Decimal('2.00')),
-            FeatureType.SMS: dict(monthly_limit=0),
+            FeatureType.USER: dict(monthly_limit=UNLIMITED_FEATURE_USAGE, per_excess_fee=Decimal('0.00')),
+            FeatureType.SMS: dict(monthly_limit=UNLIMITED_FEATURE_USAGE),
         }
     }
 }
