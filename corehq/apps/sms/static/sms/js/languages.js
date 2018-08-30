9749cd2c36c02aaa8a9fdfba2db702b1d68fbedd
Rohit Negi <rohit25.negi@gmail.com>
2018-08-30 11:51:14 +0530

    migrating sms language

--- corehq/apps/sms/static/sms/js/languages.js
+++ corehq/apps/sms/static/sms/js/languages.js
@@ -1,11 +1,14 @@
-hqDefine('sms/js/languages', function () {
-    var initialPageData = hqImport('hqwebapp/js/initial_page_data');
+hqDefine('sms/js/languages',[
+    "jquery",
+    "hqwebapp/js/initial_page_data",
+    "app_manager/js/supported_languages",
+], function ($, initialPageData, supportedLanguages) {
     $(function () {
         var langs = initialPageData.get('sms_langs');
         var saveURL = initialPageData.reverse("edit_sms_languages");
         var edit = true;
         var validate = true;
-        var SupportedLanguages = hqImport('app_manager/js/supported_languages').SupportedLanguages;
+        var SupportedLanguages = supportedLanguages.SupportedLanguages;
         var sl = new SupportedLanguages({
             langs: langs,
             saveURL: saveURL,
