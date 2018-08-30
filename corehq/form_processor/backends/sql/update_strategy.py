ef51506ed1ef6d524d7b2c23c84dae54e32b2d43
Jordan Roth <jmtroth0@gmail.com>
2018-08-30 12:00:53 +0200

    adding datadog counter

--- corehq/form_processor/backends/sql/update_strategy.py
+++ corehq/form_processor/backends/sql/update_strategy.py
@@ -33,6 +33,7 @@ from corehq.form_processor.backends.sql.dbaccessors import (
 from django.utils.translation import ugettext as _
 
 from corehq.util.soft_assert import soft_assert
+from corehq.util.datadog.gauges import datadog_counter
 reconciliation_soft_assert = soft_assert('jroth@dimagi.com', include_breadcrumbs=True)
 
 
@@ -307,6 +308,7 @@ class SqlCaseUpdateStrategy(UpdateStrategy):
     def reconcile_transactions_if_necessary(self, xform):
         if self.case.check_transaction_order():
             return
+        datadog_counter("form_processor.sql.reconciling_transactions")
         try:
             self.reconcile_transactions()
         except ReconciliationError as e:
