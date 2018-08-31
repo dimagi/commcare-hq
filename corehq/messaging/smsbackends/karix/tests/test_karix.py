029211e0fc819a12c035a39ae2c47e8562319f84
Giovanni Capalbo <gcapalbo@dimagi.com>
2018-08-31 07:10:26 -0400

    Add tests for get_json_payload

--- corehq/messaging/smsbackends/karix/tests/test_karix.py
+++ corehq/messaging/smsbackends/karix/tests/test_karix.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import json
 from corehq.apps.sms.models import QueuedSMS
 from corehq.messaging.smsbackends.karix.exceptions import KarixError
 from corehq.messaging.smsbackends.karix.models import KarixBackend
@@ -49,3 +50,55 @@ class KarixBackendTest(TestCase):
         msg_obj = QueuedSMS()
         KarixBackend.handle_response(msg_obj, MockResponse(200, {'ackid': '123', 'status': {'code': '200'}}))
         self.assertEqual(msg_obj.backend_message_id, '123')
+
+    def test_get_json_payload_unicode(self):
+        backend = KarixBackend()
+        backend.set_extra_fields(
+            username='abc',
+            password='123',
+            sender_id='SENDER',
+        )
+
+        msg_obj = QueuedSMS(text='\u0928\u092e\u0938\u094d\u0924\u0947', phone_number='+919999999999')
+        payload = backend.get_json_payload(msg_obj)
+        self.assertEqual(
+            json.loads(json.dumps(payload)),
+            {
+                'ver': '1.0',
+                'key': 'YWJjOjEyMw==',
+                'messages': [
+                    {
+                        'dest': ['919999999999'],
+                        'send': 'SENDER',
+                        'text': '\u0928\u092e\u0938\u094d\u0924\u0947',
+                        'type': 'UC',
+                    },
+                ],
+            }
+        )
+
+    def test_get_json_payload_ascii(self):
+        backend = KarixBackend()
+        backend.set_extra_fields(
+            username='abc',
+            password='123',
+            sender_id='SENDER',
+        )
+
+        msg_obj = QueuedSMS(text='test', phone_number='+919999999999')
+        payload = backend.get_json_payload(msg_obj)
+        self.assertEqual(
+            json.loads(json.dumps(payload)),
+            {
+                'ver': '1.0',
+                'key': 'YWJjOjEyMw==',
+                'messages': [
+                    {
+                        'dest': ['919999999999'],
+                        'send': 'SENDER',
+                        'text': 'test',
+                        'type': 'PM',
+                    },
+                ],
+            }
+        )
