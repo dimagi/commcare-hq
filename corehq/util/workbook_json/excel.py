7280d9245606d3432230755ec7f58559ce6e3808
Nick Pellegrino <npellegrino@dimagi.com>
2018-09-04 16:08:29 -0400

    cleanup

--- corehq/util/workbook_json/excel.py
+++ corehq/util/workbook_json/excel.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import io
-from tempfile import NamedTemporaryFile
 from zipfile import BadZipfile
 import openpyxl
 from openpyxl.utils.exceptions import InvalidFileException
