a0a815d8316fce3822caa24b40f0118858ba4e3d
Jennifer Schweers <orange.jenny@gmail.com>
2018-09-06 21:30:17 -0400

    Split hqwebapp/js/widgets in two

--- corehq/apps/hqwebapp/static/hqwebapp/js/widgets.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/widgets.js
@@ -1,8 +1,12 @@
+/**
+ * Temporary for select2 migration.
+ * This should only be referenced by hqwebapps/js/select2_v3 and hqwebapps/js/select2_v4
+ * All client code should depend on one of those two modules.
+ */
 hqDefine("hqwebapp/js/widgets", [
     'jquery',
-    'select2-3.5.2-legacy/select2',
 ], function ($) {
-    $(function () {
+    var init = function () {
         _.each($(".hqwebapp-autocomplete"), function (input) {
             var $input = $(input);
             $input.select2({
@@ -14,5 +18,9 @@ hqDefine("hqwebapp/js/widgets", [
         _.each($(".ko-select2"), function (element) {
             $(element).select2();
         });
-    });
+    };
+
+    return {
+        init: init,
+    };
 });
