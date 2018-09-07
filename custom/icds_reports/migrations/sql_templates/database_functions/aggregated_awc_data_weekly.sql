e7b73a28ea30200ae73a857127339f3133e54d64
Rohit Negi <rohit25.negi@gmail.com>
2018-09-07 15:24:06 +0530

    removed the unnecessary columns from updation

--- custom/icds_reports/migrations/sql_templates/database_functions/aggregated_awc_data_weekly.sql
+++ custom/icds_reports/migrations/sql_templates/database_functions/aggregated_awc_data_weekly.sql
@@ -4,22 +4,18 @@ $BODY$
 DECLARE
   _start_date date;
   _usage_tablename text;
-  _all_text text;
-  _null_value text;
   _yes_text text;
   _no_text text;
   _month_start_6m date;
 BEGIN
   _start_date = date_trunc('MONTH', $1)::DATE;
   _month_start_6m = (_start_date + INTERVAL ' - 6 MONTHS')::DATE;
-  _all_text = 'All';
-  _null_value = NULL;
   _yes_text = 'yes';
   _no_text = 'no';
 
   EXECUTE 'SELECT table_name FROM ucr_table_name_mapping WHERE table_type = ' || quote_literal('usage') INTO _usage_tablename;
 
-  -- Update data from usage table from last 6 months
+  -- Updates agg level=5 data from usage table from last 6 months
   -- this will update data in agg_awc child tables https://www.postgresql.org/docs/9.2/static/ddl-inherit.html
   EXECUTE 'UPDATE  agg_awc SET ' ||
     'usage_num_hh_reg = ut.usage_num_hh_reg, ' ||
@@ -29,7 +25,7 @@ BEGIN
     'num_launched_blocks = ut.num_launched_awcs, ' ||
     'num_launched_supervisors = ut.num_launched_awcs, ' ||
     'num_launched_awcs = ut.num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active, ' ||
+    'usage_awc_num_active = ut.usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'awc_id, ' ||
     'month, ' ||
@@ -45,18 +41,17 @@ BEGIN
 
   -- Rolling up the aggregation to supervisor level
   EXECUTE 'UPDATE  agg_awc SET ' ||
-    'usage_num_hh_reg = ut.usage_num_hh_reg, ' ||
+    'usage_num_hh_reg = ut.sum_usage_num_hh_reg, ' ||
     'num_launched_states = ut.num_launched_supervisors, ' ||
     'num_launched_districts = ut.num_launched_supervisors, ' ||
     'num_launched_blocks = ut.num_launched_supervisors, ' ||
     'num_launched_supervisors = ut.num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active, ' ||
+    'usage_awc_num_active = ut.usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'supervisor_id'
-    'awc_id, ' ||
     'month, ' ||
-    'usage_num_hh_reg, ' ||
+    'sum(usage_num_hh_reg) as sum_usage_num_hh_reg, ' ||
     'CASE WHEN (sum(num_launched_awcs) > 0) THEN 1 ELSE 0 END as num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
     'usage_awc_num_active, ' ||
@@ -68,18 +63,17 @@ BEGIN
 
   -- Rolling up the aggregation to block level
   EXECUTE 'UPDATE  agg_awc SET ' ||
-    'usage_num_hh_reg = ut.usage_num_hh_reg, ' ||
+    'usage_num_hh_reg = ut.sum_usage_num_hh_reg, ' ||
     'num_launched_states = ut.num_launched_blocks, ' ||
     'num_launched_districts = ut.num_launched_blocks, ' ||
     'num_launched_blocks = ut.num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active, ' ||
+    'usage_awc_num_active = ut.usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'block_id' ||
     'month, ' ||
-    'usage_num_hh_reg, ' ||
-    'is_launched, ' ||
+    'sum(usage_num_hh_reg) as sum_usage_num_hh_reg, ' ||
     'CASE WHEN (sum(num_launched_supervisors) > 0) THEN 1 ELSE 0 END as num_launched_blocks, '||
     'sum(num_launched_supervisors) as sum_num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
@@ -92,18 +86,17 @@ BEGIN
 
   -- Rolling up the aggregation to district level
   EXECUTE 'UPDATE  agg_awc SET ' ||
-    'usage_num_hh_reg = ut.usage_num_hh_reg, ' ||
+    'usage_num_hh_reg = ut.sum_usage_num_hh_reg, ' ||
     'num_launched_states = ut.num_launched_districts, ' ||
     'num_launched_districts = ut.num_launched_districts, ' ||
     'num_launched_blocks = ut.sum_num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active, ' ||
+    'usage_awc_num_active = ut.usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'district_id' ||
     'month, ' ||
-    'usage_num_hh_reg, ' ||
-    'is_launched, ' ||
+    'sum(usage_num_hh_reg) as sum_usage_num_hh_reg, ' ||
     'CASE WHEN (sum(num_launched_blocks) > 0) THEN 1 ELSE 0 END as num_launched_districts, ' ||
     'sum(num_launched_blocks) as sum_num_launched_blocks, ' ||
     'sum(num_launched_supervisors) as sum_num_launched_supervisors, '||
@@ -117,18 +110,17 @@ BEGIN
 
   -- Rolling up the aggregation to state level
   EXECUTE 'UPDATE  agg_awc SET ' ||
-    'usage_num_hh_reg = ut.usage_num_hh_reg, ' ||
+    'usage_num_hh_reg = ut.sum_usage_num_hh_reg, ' ||
     'num_launched_states = ut.num_launched_state, ' ||
     'num_launched_districts = ut.sum_num_launched_districts, ' ||
     'num_launched_blocks = ut.sum_num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active, ' ||
+    'usage_awc_num_active = ut.usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'state_id' ||
     'month, ' ||
-    'usage_num_hh_reg, ' ||
-    'is_launched, ' ||
+    'sum(usage_num_hh_reg) as sum_usage_num_hh_reg, ' ||
     'CASE WHEN (sum(num_launched_districts) > 0) THEN 1 ELSE 0 END as num_launched_state, ' ||
     'sum(num_launched_districts) as sum_num_launched_districts, ' ||
     'sum(num_launched_blocks) as sum_num_launched_blocks, ' ||
