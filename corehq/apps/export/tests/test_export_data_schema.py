be9feb780497ee097bcd89f764226b23c881caaa
Nick Pellegrino <npellegrino@dimagi.com>
2018-09-10 15:39:04 -0400

    modernize

--- corehq/apps/export/tests/test_export_data_schema.py
+++ corehq/apps/export/tests/test_export_data_schema.py
@@ -1213,7 +1213,7 @@ class TestOrderingOfSchemas(SimpleTestCase):
         )
 
     def _assert_item_order(self, schema, path, items):
-        group_schema = list(filter(lambda gs: gs.path == path, schema.group_schemas))[0]
+        group_schema = list([gs for gs in schema.group_schemas if gs.path == path])[0]
 
         for item in group_schema.items:
             if not items:
