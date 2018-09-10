e464e28ccd2f85da06b51af99407dbc975b5a81b
Jonathan Emord <jonathan.emord@gmail.com>
2018-09-10 15:58:48 -0400

    create topics with new kafka client

--- corehq/apps/change_feed/management/commands/create_kafka_topics.py
+++ corehq/apps/change_feed/management/commands/create_kafka_topics.py
@@ -3,21 +3,18 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from django.core.management import BaseCommand
 from corehq.apps.change_feed import topics
-from corehq.apps.change_feed.connection import get_simple_kafka_client
+from corehq.apps.change_feed.connection import get_kafka_client
 
 
 class Command(BaseCommand):
 
     def handle(self, **options):
-        create_kafka_topics()
-
-
-def create_kafka_topics():
-    client = get_simple_kafka_client()
-    for topic in topics.ALL:
-        if client.has_metadata_for_topic(topic):
-            status = "already exists"
-        else:
-            client.ensure_topic_exists(topic, timeout=10)
-            status = "created"
-        print("topic {}: {}".format(status, topic))
+        client = get_kafka_client()
+        current_topics = client.cluster.topics()
+        for topic in topics.ALL:
+            if topic in current_topics:
+                status = "already exists"
+            else:
+                client.add_topic(topic)
+                status = "created"
+            print("topic {}: {}".format(status, topic))
