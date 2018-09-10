653907e18021f12075b63d17b50384783a3a44d6
Jonathan Emord <jonathan.emord@gmail.com>
2018-09-10 12:07:28 -0400

    Explicitly use SimpleClient

--- corehq/apps/change_feed/connection.py
+++ corehq/apps/change_feed/connection.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import logging
 
 from django.conf import settings
-from kafka import KafkaClient
+from kafka.client import SimpleClient
 from kafka.common import KafkaUnavailableError
 
 
@@ -12,12 +12,11 @@ GENERIC_KAFKA_CLIENT_ID = 'cchq-kafka-client'
 
 
 def get_kafka_client(client_id=GENERIC_KAFKA_CLIENT_ID):
-    # configure connections_max_idle_ms?
-    return KafkaClient(
-        bootstrap_servers=settings.KAFKA_BROKERS,
-        config_id=client_id,
-        request_timeout_ms=100,
-        api_version=settings.KAFKA_API_VERSION,
+    # this uses the old SimpleClient because we are using the old SimpleProducer interface
+    return SimpleClient(
+        hosts=settings.KAFKA_BROKERS,
+        client_id=client_id,
+        timeout=30,  # seconds
     )
 
 
