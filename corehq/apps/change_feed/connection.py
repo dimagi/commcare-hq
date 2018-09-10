739eba6a284ac3a5b4c38163dd564c19ca5cab1f
Jonathan Emord <jonathan.emord@gmail.com>
2018-09-10 14:19:39 -0400

    Add kafka client interface

--- corehq/apps/change_feed/connection.py
+++ corehq/apps/change_feed/connection.py
@@ -5,6 +5,7 @@ import logging
 
 from django.conf import settings
 from kafka.client import SimpleClient
+from kafka.client import KafkaClient
 from kafka.common import KafkaUnavailableError
 
 
@@ -26,3 +27,10 @@ def get_simple_kafka_client_or_none(client_id=GENERIC_KAFKA_CLIENT_ID):
     except KafkaUnavailableError:
         logging.warning('Ignoring missing kafka client during unit testing')
         return None
+
+
+def get_kafka_client(client_id=GENERIC_KAFKA_CLIENT_ID):
+    return KafkaClient(
+        bootstrap_servers=settings.KAFKA_BROKERS,
+        client_id=client_id
+    )
