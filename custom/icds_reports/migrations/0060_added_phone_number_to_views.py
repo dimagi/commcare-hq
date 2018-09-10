b37988c7d6b260793cc8e88e0057f1a59d2fcc0b
user <lwyszomirski@soldevelo.com>
2018-09-10 14:49:28 +0200

    fix migration file

--- custom/icds_reports/migrations/0060_added_phone_number_to_views.py
+++ custom/icds_reports/migrations/0060_added_phone_number_to_views.py
@@ -12,7 +12,7 @@ migrator = RawSQLMigration(('custom', 'icds_reports', 'migrations', 'sql_templat
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0057_aggregateccsrecordpostnatalcareforms_is_ebf'),
+        ('icds_reports', '0059_update_blob_paths'),
     ]
 
     operations = [
