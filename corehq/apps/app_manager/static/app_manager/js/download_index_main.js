9811ceb6d36cb9d2382cfb3700b8a2661067676c
Rohit Negi <rohit25.negi@gmail.com>
2018-09-12 20:28:26 +0530

    update the module path

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -6,36 +6,39 @@ hqDefine('app_manager/js/download_index_main',[
     'app_manager/js/download_async_modal',
     'app_manager/js/source_files',
     'd3/d3.min'
-],function () {
+],function ($, _, ace) {
 
-    hqRequire(['ace-builds/src-min-noconflict/mode-json'],function () {
-        alert("heheheheeheh");
-    }) ;
-    // ace.config.set('basePath', '/ace-builds/src-noconflict');
-    // $(function () {
-    //     var elements = $('.prettyprint');
-    //
-    //     _.each(elements, function (elem) {
-    //         var editor = ace.edit(
-    //             elem,
-    //             {
-    //                 showPrintMargin: false,
-    //                 maxLines: 40,
-    //                 minLines: 3,
-    //                 fontSize: 14,
-    //                 wrap: true,
-    //                 useWorker: false, // enable the worker to show syntax errors
-    //             }
-    //         );
-    //         var fileName = $(elem).data('filename');
-    //
-    //         if (fileName.endsWith('json')) {
-    //             editor.session.setMode('ace/mode/json');
-    //         } else {
-    //             editor.session.setMode('ace/mode/xml');
-    //         }
-    //         editor.setReadOnly(true);
-    //     });
-    //
-    // });
+    var requirejsConfig = requirejs.s.contexts._.config;
+
+    if(requirejsConfig.paths["ace-builds/src-min-noconflict/mode-json"])
+        ace.config.setModuleUrl('ace/mode/json',requirejsConfig.paths["ace-builds/src-min-noconflict/mode-json"]);
+    if(requirejsConfig.paths["ace-builds/src-min-noconflict/mode-xml"])
+        ace.config.setModuleUrl('ace/mode/xml',requirejsConfig.paths["ace-builds/src-min-noconflict/mode-xml"]);
+
+    $(function () {
+        var elements = $('.prettyprint');
+
+        _.each(elements, function (elem) {
+            var editor = ace.edit(
+                elem,
+                {
+                    showPrintMargin: false,
+                    maxLines: 40,
+                    minLines: 3,
+                    fontSize: 14,
+                    wrap: true,
+                    useWorker: false, // enable the worker to show syntax errors
+                }
+            );
+            var fileName = $(elem).data('filename');
+
+            if (fileName.endsWith('json')) {
+                editor.session.setMode('ace/mode/json');
+            } else {
+                editor.session.setMode('ace/mode/xml');
+            }
+            editor.setReadOnly(true);
+        });
+    });
+
 });
