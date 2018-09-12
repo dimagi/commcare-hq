3830a61aad8662266aaafdb5cb4e537a82bfffe9
Rohit Negi <rohit25.negi@gmail.com>
2018-09-12 17:27:03 +0530

    testing module loading

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -5,32 +5,37 @@ hqDefine('app_manager/js/download_index_main',[
     'ace-builds/src-min-noconflict/ace',
     'app_manager/js/download_async_modal',
     'app_manager/js/source_files',
-],function ($, _, ace) {
-    ace.config.set('basePath', '/ace-builds/src-noconflict');
-    $(function () {
-        var elements = $('.prettyprint');
+    'd3/d3.min'
+],function () {
 
-        _.each(elements, function (elem) {
-            var editor = ace.edit(
-                elem,
-                {
-                    showPrintMargin: false,
-                    maxLines: 40,
-                    minLines: 3,
-                    fontSize: 14,
-                    wrap: true,
-                    useWorker: false, // enable the worker to show syntax errors
-                }
-            );
-            var fileName = $(elem).data('filename');
-
-            if (fileName.endsWith('json')) {
-                editor.session.setMode('ace/mode/json');
-            } else {
-                editor.session.setMode('ace/mode/xml');
-            }
-            editor.setReadOnly(true);
-        });
-
-    });
+    hqRequire(["nvd3/nv.d3.min"],function () {
+        alert("heheheheeheh");
+    }) ;
+    // ace.config.set('basePath', '/ace-builds/src-noconflict');
+    // $(function () {
+    //     var elements = $('.prettyprint');
+    //
+    //     _.each(elements, function (elem) {
+    //         var editor = ace.edit(
+    //             elem,
+    //             {
+    //                 showPrintMargin: false,
+    //                 maxLines: 40,
+    //                 minLines: 3,
+    //                 fontSize: 14,
+    //                 wrap: true,
+    //                 useWorker: false, // enable the worker to show syntax errors
+    //             }
+    //         );
+    //         var fileName = $(elem).data('filename');
+    //
+    //         if (fileName.endsWith('json')) {
+    //             editor.session.setMode('ace/mode/json');
+    //         } else {
+    //             editor.session.setMode('ace/mode/xml');
+    //         }
+    //         editor.setReadOnly(true);
+    //     });
+    //
+    // });
 });
