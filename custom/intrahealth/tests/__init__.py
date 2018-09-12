60f7c686a1b593a3dd80b7f7bf5f31474f487ae3
Nick Pellegrino <npellegrino@dimagi.com>
2018-09-11 21:41:45 -0400

    bytes -> unicode

--- custom/intrahealth/tests/__init__.py
+++ custom/intrahealth/tests/__init__.py
@@ -207,7 +207,7 @@ def setUpModule():
             with open(os.path.join(path, file_name), encoding='utf-8') as f:
                 table_name = get_table_name(domain.name, file_name[:-4]).decode('utf-8')
                 table = metadata.tables[table_name]
-                postgres_copy.copy_from(f, table, engine, format=b'csv', null=b'', header=True)
+                postgres_copy.copy_from(f, table, engine, format='csv', null='', header=True)
     _call_center_domain_mock.stop()
 
 
