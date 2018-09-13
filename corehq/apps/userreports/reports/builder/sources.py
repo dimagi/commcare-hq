ffe276ca4ac8d0b0f6828c66196c77b836bfd496
Cory Zue <czue@dimagi.com>
2018-09-13 10:40:20 +0200

    use constants

--- corehq/apps/userreports/reports/builder/sources.py
+++ corehq/apps/userreports/reports/builder/sources.py
@@ -1,3 +1,4 @@
+from corehq.apps.userreports.app_manager.data_source_meta import DATA_SOURCE_TYPE_CASE, DATA_SOURCE_TYPE_FORM
 
 
 def get_source_type_from_report_config(report_config):
@@ -7,6 +8,6 @@ def get_source_type_from_report_config(report_config):
     :return:
     """
     return report_config.report_meta.builder_source_type or {
-        "CommCareCase": "case",
-        "XFormInstance": "form"
+        "CommCareCase": DATA_SOURCE_TYPE_CASE,
+        "XFormInstance": DATA_SOURCE_TYPE_FORM,
     }[report_config.config.referenced_doc_type]
