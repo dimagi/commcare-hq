bbbf4d93600cd7b29405f1acb12fee5bcac6b7c0
Jordan Roth <jmtroth0@gmail.com>
2018-09-17 15:24:13 -0400

    use correctly named logger

--- settings.py
+++ settings.py
@@ -1270,10 +1270,10 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': False,
         },
-        'kafka': {
+        'kafka.conn': {
             'handlers': ['file'],
             'level': 'ERROR',
-            'propogate': False,
+            'propagate': False,
         },
     }
 }
