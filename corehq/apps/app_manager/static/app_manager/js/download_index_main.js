20627ea6480d165630b60bc069f275311f026285
Rohit Negi <rohit25.negi@gmail.com>
2018-09-18 10:58:18 +0530

    Use ace with hqRequire

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -1,34 +1,34 @@
 hqDefine('app_manager/js/download_index_main',[
     'jquery',
     'underscore',
-    'ace-builds/src-min-noconflict/ace',
     'app_manager/js/download_async_modal',
     'app_manager/js/source_files',
-],function ($, _, ace) {
-    // work around with ace issue https://github.com/ajaxorg/ace/issues/732 also see the linked open issue.
-    ace.require("ace/edit_session").EditSession.prototype.$startWorker = function () {};
-    $(function () {
-        var elements = $('.prettyprint');
-        _.each(elements, function (elem) {
-            var editor = ace.edit(
-                elem,
-                {
-                    showPrintMargin: false,
-                    maxLines: 40,
-                    minLines: 3,
-                    fontSize: 14,
-                    wrap: true,
-                    useWorker: false, // enable the worker to show syntax errors
-                }
-            );
-            var fileName = $(elem).data('filename');
+],function ($, _) {
 
-            if (fileName.endsWith('json')) {
-                editor.session.setMode('ace/mode/json');
-            } else {
-                editor.session.setMode('ace/mode/xml');
-            }
-            editor.setReadOnly(true);
+    $(function () {
+        hqRequire(['ace-builds/src-min-noconflict/ace'],function (ace) {
+            var elements = $('.prettyprint');
+            _.each(elements, function (elem) {
+                var editor = ace.edit(
+                    elem,
+                    {
+                        showPrintMargin: false,
+                        maxLines: 40,
+                        minLines: 3,
+                        fontSize: 14,
+                        wrap: true,
+                        useWorker: false, // enable the worker to show syntax errors
+                    }
+                );
+                var fileName = $(elem).data('filename');
+
+                if (fileName.endsWith('json')) {
+                    editor.session.setMode('ace/mode/json');
+                } else {
+                    editor.session.setMode('ace/mode/xml');
+                }
+                editor.setReadOnly(true);
+            });
         });
 
     });
