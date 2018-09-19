f9be2fff5a3a2c418b713feb08f98dc3eb8ccfe0
Rohit Negi <rohit25.negi@gmail.com>
2018-09-20 00:59:45 +0530

    taking require outof loop

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -5,36 +5,37 @@ hqDefine('app_manager/js/download_index_main',[
     'app_manager/js/download_async_modal',
     'app_manager/js/source_files',
 ],function ($, _, ace) {
-
+    
     $(function () {
-        var elements = $('.prettyprint');
-        _.each(elements, function (elem) {
-            var editor = ace.edit(
-                elem,
-                {
-                    showPrintMargin: false,
-                    maxLines: 40,
-                    minLines: 3,
-                    fontSize: 14,
-                    wrap: true,
-                    useWorker: false, // enable the worker to show syntax errors
-                }
-            );
+        hqRequire([
+            'ace-builds/src-min-noconflict/mode-json',
+            'ace-builds/src-min-noconflict/mode-xml',
+        ],function () {
+            var elements = $('.prettyprint');
+            _.each(elements, function (elem) {
 
-            var fileName = $(elem).data('filename');
-            hqRequire([
-                'ace-builds/src-min-noconflict/mode-json',
-                'ace-builds/src-min-noconflict/mode-xml',
-            ],function () {
+                var editor = ace.edit(
+                    elem,
+                    {
+                        showPrintMargin: false,
+                        maxLines: 40,
+                        minLines: 3,
+                        fontSize: 14,
+                        wrap: true,
+                        useWorker: false, // enable the worker to show syntax errors
+                    }
+                );
+
+                var fileName = $(elem).data('filename');
                 if (fileName.endsWith('json')) {
                     editor.session.setMode('ace/mode/json');
                 } else {
                     editor.session.setMode('ace/mode/xml');
                 }
-
+                editor.setReadOnly(true);
             });
-            editor.setReadOnly(true);
         });
+
     }
     );
 
