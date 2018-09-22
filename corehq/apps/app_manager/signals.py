05aa15de0a4b0b73b8e68dd2f27cc5948bcdf77c
Simon Kelly <skelly@dimagi.com>
2018-09-22 08:54:22 -0400

    catch exceptions for safety
    Since this isn't a critical piece of functionality we don't want
    to break app saves if this fails.

--- corehq/apps/app_manager/signals.py
+++ corehq/apps/app_manager/signals.py
@@ -4,6 +4,7 @@ from django.dispatch.dispatcher import Signal
 
 from corehq.apps.callcenter.app_parser import get_call_center_config_from_app
 from corehq.apps.domain.models import Domain
+from dimagi.utils.logging import notify_exception
 
 
 def create_app_structure_repeat_records(sender, application, **kwargs):
@@ -19,15 +20,18 @@ def update_callcenter_config(sender, application, **kwargs):
     if not application.copy_of:
         return
 
-    domain = Domain.get_by_name(application.domain)
-    cc_config = domain.call_center_config
-    if not cc_config or not (cc_config.fixtures_are_active() and cc_config.config_is_valid()):
-        return
+    try:
+        domain = Domain.get_by_name(application.domain)
+        cc_config = domain.call_center_config
+        if not cc_config or not (cc_config.fixtures_are_active() and cc_config.config_is_valid()):
+            return
 
-    app_config = get_call_center_config_from_app(application)
-    save = cc_config.update_from_app_config(app_config)
-    if save:
-        cc_config.save()
+        app_config = get_call_center_config_from_app(application)
+        save = cc_config.update_from_app_config(app_config)
+        if save:
+            cc_config.save()
+    except Exception:
+        notify_exception(None, "Error updating CallCenter config for app build")
 
 
 app_post_save = Signal(providing_args=['application'])
