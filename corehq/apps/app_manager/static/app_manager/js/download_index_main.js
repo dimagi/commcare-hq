7448861660925376c74f53cceb0c768e2c59a87d
Rohit Negi <rohit25.negi@gmail.com>
2018-09-24 16:33:50 +0530

    Using shim to load ace modes

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -2,40 +2,38 @@ hqDefine('app_manager/js/download_index_main',[
     'jquery',
     'underscore',
     'ace-builds/src-min-noconflict/ace',
+    'ace-builds/src-min-noconflict/mode-json', //to the ace mode from CDN
+    'ace-builds/src-min-noconflict/mode-xml',
     'app_manager/js/download_async_modal',
     'app_manager/js/source_files',
 ],function ($, _, ace) {
 
     $(function () {
-        //Use hqRequire to load the ace modes from CDN
-        hqRequire([
-            'ace-builds/src-min-noconflict/mode-json',
-            'ace-builds/src-min-noconflict/mode-xml',
-        ],function () {
-            var elements = $('.prettyprint');
-            _.each(elements, function (elem) {
 
-                var editor = ace.edit(
-                    elem,
-                    {
-                        showPrintMargin: false,
-                        maxLines: 40,
-                        minLines: 3,
-                        fontSize: 14,
-                        wrap: true,
-                        useWorker: false,
-                    }
-                );
+        var elements = $('.prettyprint');
+        _.each(elements, function (elem) {
 
-                var fileName = $(elem).data('filename');
-                if (fileName.endsWith('json')) {
-                    editor.session.setMode('ace/mode/json');
-                } else {
-                    editor.session.setMode('ace/mode/xml');
+            var editor = ace.edit(
+                elem,
+                {
+                    showPrintMargin: false,
+                    maxLines: 40,
+                    minLines: 3,
+                    fontSize: 14,
+                    wrap: true,
+                    useWorker: false,
                 }
-                editor.setReadOnly(true);
-            });
+            );
+
+            var fileName = $(elem).data('filename');
+            if (fileName.endsWith('json')) {
+                editor.session.setMode('ace/mode/json');
+            } else {
+                editor.session.setMode('ace/mode/xml');
+            }
+            editor.setReadOnly(true);
         });
+
 
     });
 
