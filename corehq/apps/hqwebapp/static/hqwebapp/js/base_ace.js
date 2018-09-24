16ee0688d7ee48d68c5b9a669e79bee057e09afd
Rohit Negi <rohit25.negi@gmail.com>
2018-09-24 17:49:46 +0530

    set base Path

--- corehq/apps/hqwebapp/static/hqwebapp/js/base_ace.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/base_ace.js
@@ -1,13 +1,15 @@
 hqDefine('hqwebapp/js/base_ace', [
     'jquery',
     'ace-builds/src-min-noconflict/ace',
-    'ace-builds/src-min-noconflict/mode-json',
-    'ace-builds/src-min-noconflict/mode-xml',
 ], function (
     $,
     ace
 ) {
 
+    if (!ace.config.get('basePath')) {
+        var basePath = requirejs.s.contexts._.config.paths["ace-builds/src-min-noconflict/ace"];
+        ace.config.set("basePath",basePath.substring(0,basePath.lastIndexOf("/")));
+    }
     var initAceEditor = function (element, mode, options, value) {
         var defaultOptions = {
             showPrintMargin: false,
