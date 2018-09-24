c6bfe291e9b00d0150f51d70ae81301ef0e37e53
Rohit Negi <rohit25.negi@gmail.com>
2018-09-24 16:33:50 +0530

    Set Ace basePath to be used by ace worker/modes

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -6,36 +6,37 @@ hqDefine('app_manager/js/download_index_main',[
     'app_manager/js/source_files',
 ],function ($, _, ace) {
 
+    if (!ace.config.get('basePath')) { //TODO:Rohit move to base ace module(create if needed)
+        //fetch ace path from requirejs configuration
+        var acePath = requirejs.s.contexts._.config.paths["ace-builds/src-min-noconflict/ace"];
+        ace.config.set("basePath",acePath.substring(0,acePath.lastIndexOf("/")));
+    }
+
     $(function () {
-        //Use hqRequire to load the ace modes from CDN
-        hqRequire([
-            'ace-builds/src-min-noconflict/mode-json',
-            'ace-builds/src-min-noconflict/mode-xml',
-        ],function () {
-            var elements = $('.prettyprint');
-            _.each(elements, function (elem) {
+        var elements = $('.prettyprint');
+        _.each(elements, function (elem) {
 
-                var editor = ace.edit(
-                    elem,
-                    {
-                        showPrintMargin: false,
-                        maxLines: 40,
-                        minLines: 3,
-                        fontSize: 14,
-                        wrap: true,
-                        useWorker: false,
-                    }
-                );
-
-                var fileName = $(elem).data('filename');
-                if (fileName.endsWith('json')) {
-                    editor.session.setMode('ace/mode/json');
-                } else {
-                    editor.session.setMode('ace/mode/xml');
+            var editor = ace.edit(
+                elem,
+                {
+                    showPrintMargin: false,
+                    maxLines: 40,
+                    minLines: 3,
+                    fontSize: 14,
+                    wrap: true,
+                    useWorker: false,
                 }
-                editor.setReadOnly(true);
-            });
+            );
+
+            var fileName = $(elem).data('filename');
+            if (fileName.endsWith('json')) {
+                editor.session.setMode('ace/mode/json');
+            } else {
+                editor.session.setMode('ace/mode/xml');
+            }
+            editor.setReadOnly(true);
         });
+
 
     });
 
