d425df6caa6bdfed44b642ca7bdf1da23478dea8
Rohit Negi <rohit25.negi@gmail.com>
2018-09-25 12:42:22 +0530

    Using default ace options

--- corehq/apps/app_manager/static/app_manager/js/download_index_main.js
+++ corehq/apps/app_manager/static/app_manager/js/download_index_main.js
@@ -10,9 +10,9 @@ hqDefine('app_manager/js/download_index_main',[
         var elements = $('.prettyprint');
         _.each(elements, function (elem) {
             var fileName = $(elem).data('filename'),
-                mode = fileName.endsWith('json') ? 'ace/mode/json': 'ace/mode/xml';
+                mode = fileName.endsWith('json') ? 'ace/mode/json' : 'ace/mode/xml';
 
-            baseAce.initAceEditor(elem, mode, {useWorker: true,readOnly: false});
+            baseAce.initAceEditor(elem, mode, {});
 
         });
 
