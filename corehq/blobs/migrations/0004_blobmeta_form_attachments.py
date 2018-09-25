6c185e14402ef5a3eeb97bb5036fb54f0bf1da7c
Daniel Miller <dmiller@dimagi.com>
2018-09-25 16:52:55 +0000

    Restore accidentally dropped constraint

--- corehq/blobs/migrations/0004_blobmeta_form_attachments.py
+++ corehq/blobs/migrations/0004_blobmeta_form_attachments.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
-from corehq.sql_db.operations import RawSQLMigration
+from corehq.sql_db.operations import RawSQLMigration, HqRunSQL
 from corehq.sql_db.migrations import partitioned
 
 migrator = RawSQLMigration(('corehq', 'blobs', 'sql_templates'), {})
@@ -19,6 +19,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        # this was accidentally removed by 0003_big_content
+        # drop first in case it already exists
+        HqRunSQL(
+            """
+            ALTER TABLE blobs_blobmeta DROP CONSTRAINT IF EXISTS
+                blobs_blobmeta_content_length_check;
+            ALTER TABLE blobs_blobmeta ADD CONSTRAINT
+                blobs_blobmeta_content_length_check CHECK (content_length >= 0);
+            """,
+            "SELECT 1"
+        ),
+
         migrator.get_migration('get_blobmetas.sql'),
         migrator.get_migration('setup_blobmeta_view.sql', 'drop_blobmeta_view.sql'),
         migrator.get_migration('restrict_legacy_attachment_metadata_insert.sql', testing_only=True),
