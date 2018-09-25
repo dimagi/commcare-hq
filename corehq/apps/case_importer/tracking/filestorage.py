57b44fbfd42e413b1e48fd7e7b97f28071215a12
Nick Pellegrino <npellegrino@dimagi.com>
2018-09-24 22:23:48 -0400

    remove print

--- corehq/apps/case_importer/tracking/filestorage.py
+++ corehq/apps/case_importer/tracking/filestorage.py
@@ -33,7 +33,6 @@ class PersistentFileStore(object):
 
     def write_file(self, f, filename, domain):
         identifier = random_url_id(16)
-        print(type(f))
         meta = self._db.put(
             f,
             domain=domain,
