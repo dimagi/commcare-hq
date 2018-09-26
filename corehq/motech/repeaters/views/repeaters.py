ccc27bcf8f41d39188f2552183c8c9028593ab89
Norman Hooper <nhooper@dimagi.com>
2018-09-26 17:58:42 +0200

    Stickler

--- corehq/motech/repeaters/views/repeaters.py
+++ corehq/motech/repeaters/views/repeaters.py
@@ -63,9 +63,11 @@ class DomainForwardingOptionsView(BaseAdminProjectSettingsView):
         return {
             'repeaters': self.repeaters,
             'pending_record_count': RepeatRecord.count(self.domain),
-            'gefingerpoken': self.request.couch_user.is_superuser or
-                             self.request.couch_user.has_permission(self.domain, 'edit_motech') or
-                             toggles.IS_CONTRACTOR.enabled(self.request.couch_user.username)
+            'gefingerpoken': (
+                self.request.couch_user.is_superuser or
+                self.request.couch_user.has_permission(self.domain, 'edit_motech') or
+                toggles.IS_CONTRACTOR.enabled(self.request.couch_user.username)
+            )
         }
 
 
