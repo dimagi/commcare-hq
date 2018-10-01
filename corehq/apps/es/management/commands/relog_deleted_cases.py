a635d80d39d52716a1b5d3dbaff6c57bbeeeda80
Jordan Roth <jmtroth0@gmail.com>
2018-10-01 16:08:47 -0400

    management command for relogging all saved deleted cases

---
+++ corehq/apps/es/management/commands/relog_deleted_cases.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from corehq.apps.es import CaseES
+from corehq.apps.hqcase.utils import resave_case
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+
+from corehq.util.log import with_progress_bar
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        case_ids_and_domains = self._get_saved_deleted_case_ids()
+
+        for case_ids_and_domains in with_progress_bar(case_ids_and_domains):
+            for case_id, domain in case_ids_and_domains:
+                case = CaseAccessors(domain).get_case(case_id)
+                resave_case(domain, case)
+
+    @staticmethod
+    def _get_saved_deleted_case_ids():
+        return set(
+            CaseES().set_query(
+                {
+                    "term": {
+                        "doc_type": "CommCareCase-Deleted"
+                    }
+                }
+            ).values_list("_id", "domain"))
