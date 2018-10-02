d7f50ef4b67476dfce96bf9d093a409f827160c1
Nick Pellegrino <npellegrino@dimagi.com>
2018-10-02 10:37:25 -0400

    only send one email if there are no issues

--- corehq/apps/cleanup/tasks.py
+++ corehq/apps/cleanup/tasks.py
@@ -192,6 +192,7 @@ def check_for_sql_forms_without_existing_domain():
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def check_for_elasticsearch_data_without_existing_domain():
+    issue_found = False
     existing_domain_names = set(Domain.get_all_names())
     for hqESQuery in [AppES, CaseSearchES, FormES, GroupES, LedgerES, UserES]:
         query = hqESQuery()
@@ -199,10 +200,11 @@ def check_for_elasticsearch_data_without_existing_domain():
             query = query.NOT(filters.domain(domain_name))
         count = query.count()
         if count != 0:
+            issue_found = True
             mail_admins_async.delay(
                 'ES index "%s" contains %s items belonging to a missing domain' % (query.index, count), ''
             )
-        elif datetime.utcnow().isoweekday() == 1:
-            mail_admins_async.delay(
-                'All data in ES index "%s" belong to valid domains' % query.index, ''
-            )
+    if not issue_found and datetime.utcnow().isoweekday() == 1:
+        mail_admins_async.delay(
+            'All data in ES belongs to valid domains', ''
+        )
