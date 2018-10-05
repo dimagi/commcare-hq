f0802a0b74982de34054b59f275ebee9145c496f
Nick Pellegrino <npellegrino@dimagi.com>
2018-10-04 22:24:01 -0400

    fix corehq.apps.hqcase.tests.test_explode_cases:ExplodeCasesDbTest.test_parent_child

--- corehq/apps/hqcase/tasks.py
+++ corehq/apps/hqcase/tasks.py
@@ -78,13 +78,13 @@ def explode_cases(domain, user_id, factor, task=None):
                     i.case_type, new_case_ids[i.case_id][explosion], i.relationship
                 ) for key, i in six.iteritems(cases[old_case_id].index)
             }
-            progress += queue_case(new_case.as_string(), queue, progress)
+            progress += queue_case(new_case.as_string().decode('utf-8'), queue, progress)
 
             for ledger in sync_result.ledgers.get(old_case_id, []):
                 new_ledger = copy(ledger)
                 new_ledger.entity_id = new_case_id
                 total_ledgers += 1
-                queue_case(new_ledger.as_string(), queue, progress)
+                queue_case(new_ledger.as_string().decode('utf-8'), queue, progress)
 
     if len(queue):
         submit_case_blocks(queue, domain, user_id=user_id, device_id="explode_cases")
--- corehq/apps/hqcase/tests/test_explode_cases.py
+++ corehq/apps/hqcase/tests/test_explode_cases.py
@@ -95,7 +95,7 @@ class ExplodeCasesDbTest(TestCase):
             user_id=self.user_id,
             owner_id=self.user_id,
             case_type=parent_type,
-        ).as_string()
+        ).as_string().decode('utf-8')
 
         child_id = uuid.uuid4().hex
         child_block = CaseBlock(
@@ -105,7 +105,7 @@ class ExplodeCasesDbTest(TestCase):
             owner_id=self.user_id,
             case_type='exploder-child-type',
             index={'parent': (parent_type, parent_id)},
-        ).as_string()
+        ).as_string().decode('utf-8')
 
         submit_case_blocks([parent_block, child_block], self.domain.name)
         self.assertEqual(2, len(self.accessor.get_case_ids_in_domain()))
