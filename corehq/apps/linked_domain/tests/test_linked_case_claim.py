69731070bb588a1802b0b92ce4136d4981387bde
Farid Rener <proteusvacuum@gmail.com>
2018-10-11 14:33:13 -0400

    Add test for remote config

--- corehq/apps/linked_domain/tests/test_linked_case_claim.py
+++ corehq/apps/linked_domain/tests/test_linked_case_claim.py
@@ -1,13 +1,23 @@
 from __future__ import absolute_import, unicode_literals
 
+import json
+
+from mock import patch
+from tastypie.models import ApiKey
+
 from corehq.apps.case_search.models import (
     CaseSearchConfig,
     CaseSearchQueryAddition,
     FuzzyProperties,
     IgnorePatterns,
 )
-from corehq.apps.linked_domain.updates import update_case_search_config
+from corehq.apps.domain.shortcuts import create_domain
+from corehq.apps.linked_domain.decorators import REMOTE_REQUESTER_HEADER
 from corehq.apps.linked_domain.tests.test_linked_apps import BaseLinkedAppsTest
+from corehq.apps.linked_domain.updates import update_case_search_config
+from corehq.apps.users.models import WebUser
+from corehq.util import reverse
+from corehq.util.view_utils import absolute_reverse
 
 
 class TestLinkedCaseClaim(BaseLinkedAppsTest):
@@ -59,3 +69,43 @@ class TestLinkedCaseClaim(BaseLinkedAppsTest):
         self.assertEqual(name, query_addition.name)
         self.assertEqual(addition, query_addition.query_addition)
         self.assertNotEqual(query_addition.pk, self.query_addition.pk)
+
+
+class TestRemoteLinkedCaseClaim(TestLinkedCaseClaim):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestRemoteLinkedCaseClaim, cls).setUpClass()
+        cls.domain_obj = create_domain(cls.domain)
+        cls.couch_user = WebUser.create(cls.domain, "test", "foobar")
+        cls.django_user = cls.couch_user.get_django_user()
+        cls.api_key, _ = ApiKey.objects.get_or_create(user=cls.django_user)
+        cls.auth_headers = {'HTTP_AUTHORIZATION': 'apikey test:%s' % cls.api_key.key}
+        cls.linked_domain_requester = absolute_reverse('domain_homepage', args=[cls.domain_link.linked_domain])
+        cls.domain_link.linked_domain = cls.linked_domain_requester
+        cls.domain_link.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.couch_user.delete()
+        cls.api_key.delete()
+        super(TestRemoteLinkedCaseClaim, cls).tearDownClass()
+        cls.domain_obj.delete()  # deleting the domain before other things causes failures
+
+    @patch('corehq.apps.linked_domain.updates.remote_get_case_search_config')
+    def test_remote_linked_app(self, fake_case_search_config_getter):
+        url = reverse('linked_domain:case_search_config', args=[self.domain])
+        headers = self.auth_headers.copy()
+        headers[REMOTE_REQUESTER_HEADER] = self.linked_domain_requester
+        resp = self.client.get(url, **headers)
+
+        fake_case_search_config_getter.return_value = json.loads(resp.content)
+
+        update_case_search_config(self.domain_link)
+        new_search_config = (CaseSearchConfig.objects
+                         .prefetch_related('fuzzy_properties')
+                         .get(domain=self.domain_link.linked_domain))
+
+        self.assertTrue(new_search_config.enabled)
+        self.assertItemsEqual(new_search_config.fuzzy_properties.all()[0].properties,
+                              self.search_config.fuzzy_properties.all()[0].properties)
