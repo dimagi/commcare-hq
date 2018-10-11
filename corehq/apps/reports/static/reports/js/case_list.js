1badf200b5fd26f230a49ea104a38b35f4a62993
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-10-11 15:49:09 -0400

    Stickler

--- corehq/apps/reports/static/reports/js/case_list.js
+++ corehq/apps/reports/static/reports/js/case_list.js
@@ -1,21 +1,21 @@
 hqDefine("reports/js/case_list", ['jquery', 'analytix/js/kissmetrix', 'hqwebapp/js/initial_page_data'],
     function ($, kissAnalytics, initialPageData) {
-    $(function () {
-        if (document.location.href.match(/reports\/case_list/)) {
-            $(document).on('click', 'td.case-name-link', function () {
-                kissAnalytics.track.event("Clicked Case Name in Case List Report");
-            });
-            var userTypes = initialPageData.get('user_types');
-            $(document).on('click', '#apply-filters', function () {
-                kissAnalytics.track.event("Clicked Apply",
-                    {"filters": _.map($("#paramSelectorForm input[name='case_list_filter']").serializeArray()[0].value.split(','),
+        $(function () {
+            if (document.location.href.match(/reports\/case_list/)) {
+                $(document).on('click', 'td.case-name-link', function () {
+                    kissAnalytics.track.event("Clicked Case Name in Case List Report");
+                });
+                var userTypes = initialPageData.get('user_types');
+                $(document).on('click', '#apply-filters', function () {
+                    kissAnalytics.track.event("Clicked Apply",
+                        {"filters": _.map($("#paramSelectorForm input[name='case_list_filter']").serializeArray()[0].value.split(','),
                             function (item) {
                                 if (item[0] === "t") { return userTypes[item.substring(3)]; }
                                 else { return item; }
                             }
-                    )}
-                );
-            });
-        }
+                        )}
+                    );
+                });
+            }
+        });
     });
-});
--- corehq/apps/reports/static/reports/js/submit_history.js
+++ corehq/apps/reports/static/reports/js/submit_history.js
@@ -1,21 +1,21 @@
 hqDefine("reports/js/submit_history", ['jquery', 'analytix/js/kissmetrix', 'hqwebapp/js/initial_page_data'],
     function ($, kissAnalytics, initialPageData) {
-    $(function () {
-        if (document.location.href.match(/reports\/submit_history/)) {
-            $(document).on('click', 'td.view-form-link', function () {
-                kissAnalytics.track.event("Clicked View Form in Submit History Report");
-            });
-            var userTypes = initialPageData.get('user_types');
-            $(document).on('click', '#apply-filters', function () {
-                kissAnalytics.track.event("Clicked Apply",
-                    {"filters": _.map($("#paramSelectorForm input[name='emw']").serializeArray()[0].value.split(','),
+        $(function () {
+            if (document.location.href.match(/reports\/submit_history/)) {
+                $(document).on('click', 'td.view-form-link', function () {
+                    kissAnalytics.track.event("Clicked View Form in Submit History Report");
+                });
+                var userTypes = initialPageData.get('user_types');
+                $(document).on('click', '#apply-filters', function () {
+                    kissAnalytics.track.event("Clicked Apply",
+                        {"filters": _.map($("#paramSelectorForm input[name='emw']").serializeArray()[0].value.split(','),
                             function (item) {
                                 if (item[0] === "t") { return userTypes[item.substring(3)]; }
                                 else { return item; }
                             }
-                    )}
-                );
-            });
-        }
+                        )}
+                    );
+                });
+            }
+        });
     });
-});
