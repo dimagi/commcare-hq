f122587fd0bafdb4cc58162bd795c0aa25efd7b3
Farid Rener <proteusvacuum@gmail.com>
2018-10-12 11:20:03 -0400

    Dont use confusing remote parameter

--- corehq/apps/linked_domain/tests/test_linked_case_claim.py
+++ corehq/apps/linked_domain/tests/test_linked_case_claim.py
@@ -17,7 +17,6 @@ from corehq.apps.linked_domain.tests.test_linked_apps import BaseLinkedAppsTest
 from corehq.apps.linked_domain.updates import update_case_search_config
 from corehq.apps.users.models import WebUser
 from corehq.util import reverse
-from corehq.util.view_utils import absolute_reverse
 
 
 class BaseLinkedCaseClaimTest(BaseLinkedAppsTest):
@@ -87,8 +86,6 @@ class TestRemoteLinkedCaseClaim(BaseLinkedCaseClaimTest):
         cls.django_user = cls.couch_user.get_django_user()
         cls.api_key, _ = ApiKey.objects.get_or_create(user=cls.django_user)
         cls.auth_headers = {'HTTP_AUTHORIZATION': 'apikey test:%s' % cls.api_key.key}
-        cls.linked_domain_requester = absolute_reverse('domain_homepage', args=[cls.domain_link.linked_domain])
-        cls.domain_link.linked_domain = cls.linked_domain_requester
         cls.domain_link.save()
 
     @classmethod
@@ -102,7 +99,7 @@ class TestRemoteLinkedCaseClaim(BaseLinkedCaseClaimTest):
     def test_remote_linked_app(self, fake_case_search_config_getter):
         url = reverse('linked_domain:case_search_config', args=[self.domain])
         headers = self.auth_headers.copy()
-        headers[REMOTE_REQUESTER_HEADER] = self.linked_domain_requester
+        headers[REMOTE_REQUESTER_HEADER] = self.domain_link.linked_domain
         resp = self.client.get(url, **headers)
 
         fake_case_search_config_getter.return_value = json.loads(resp.content)
