87e722ad4a6e065548b8d422541a432e0c3d9204
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-16 13:47:01 -0400

    Added data bindings for everything in the main .modal-body except the django form

--- corehq/apps/export/static/export/js/export_list.js
+++ corehq/apps/export/static/export/js/export_list.js
@@ -39,10 +39,83 @@ hqDefine("export/js/export_list", function () {
         self.isLoaded = ko.observable(false);
         self.isSubmittingForm = ko.observable(false);
         self.showNoAppsError = ko.observable(false);
+        self.hasNoCaseTypes = ko.observable(false);
+        self.hasSpecialAppTypes = ko.observable(false); // TODO: test
         self.formLoadError = ko.observable('');
+        self.formSubmissionError = ko.observable('');   // TODO: test
+
+        // Form Data
+        self.modelType = ko.observable();
+        self.appType = ko.observable();
+        self.application = ko.observable();
+        self.form = ko.observable();
+        self.selectedAppData = ko.observable({});
+        self.selectedFormData = ko.observable({});
+        self.selectedFormName = ko.observable('');
+
+        self.showMislabeled = ko.observable(false);
+        self.showSuggestions = ko.observable(false);
+        self.showAppDoesNotExist = ko.observable(false);
+        self.showDuplicate = ko.observable(false);
+        self.duplicatePossibilities = ko.observableArray();
+        self.restoreAppText = ko.observable('');
+        self.restoreAppLink = ko.observable('');
 
         self.defaultFormLoadError = ko.computed(function () {
             return self.formLoadError() === 'default';
+        });
+        self.defaultFormSubmissionError = ko.computed(function () {
+            return self.formSubmissionError() === 'default';
+        });
+
+        self.showHasNoCaseTypes = ko.computed(function () {
+            return self.hasNoCaseTypes() && self.application();
+        });
+
+        self.showSubmissionCount = ko.computed(function () {
+            return self.modelType() === 'form' && !_.isEmpty(self.selectedFormData());
+        });
+        self.submissionCountText = ko.observable('');
+
+        // TODO: test
+        self.selectedAppData.subscribe(function (newValue) {
+            if (self.appType() === 'deleted' && self.application()) {
+                self.restoreAppText(_.template(gettext("<%= name %> has been deleted."))({ name: newValue.name }));
+                self.restoreAppLink(newValue.restoreUrl);
+            } else {
+                self.restoreAppText('');
+                self.restoreAppLink('');
+            }
+        });
+
+        self.selectedFormData.subscribe(function (newValue) {
+            // Update form submission count message
+            var text = '';
+            if (self.modelType() === 'form') {
+                if (newValue.submissions === 1) {
+                    text = gettext('<%= count %> form submission available.');
+                } else {
+                    text = gettext('<%= count %> form submissions available.');
+                }
+                text = _.template(text)({ count: newValue.submissions, });
+            }
+            self.submissionCountText(text);
+
+            // Display "Mislabeled" message
+            self.showMislabeled(self.appType() === '_unknown' && !self.form() && !newValue.app_copy);
+
+            // Display suggestions
+            self.showSuggestions(self.appType() === '_unknown' && !self.form() && !newValue.no_suggestions);
+
+            self.showAppDoesNotExist(newValue.app_does_not_exist);
+
+            self.showDuplicate(newValue.duplicate);
+            self.duplicatePossibilities(newValue.possibilities);
+            self.selectedFormName(newValue.is_user_registration ? gettext('User Registration') : newValue.name);
+        });
+
+        self.showDrilldown = ko.computed(function () {
+            return !self.showNoAppsError() && !self.formLoadError() && self.isLoaded() && !self.isSubmittingForm();
         });
 
         self.isValid = ko.computed(function () {
