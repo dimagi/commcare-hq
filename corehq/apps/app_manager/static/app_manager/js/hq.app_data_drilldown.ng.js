ac6f4e24225e2131f6c6fc7ede84c532b6bf109a
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-16 20:09:44 -0400

    Added drilldown interaction

--- corehq/apps/app_manager/static/app_manager/js/hq.app_data_drilldown.ng.js
+++ corehq/apps/app_manager/static/app_manager/js/hq.app_data_drilldown.ng.js
@@ -16,22 +16,6 @@
     ]);
 
     // defaults
-    app_drilldown.constant('formFieldSlugs', {
-        model_type: 'model_type',
-        app_type: 'app_type',
-        application: 'application',
-        module: 'module',
-        form: 'form',
-        case_type: 'case_type',
-    });
-    app_drilldown.constant('formDefaults', {
-        model_type: '',
-        app_type: 'all',
-        application: null,
-        module: null,
-        form: null,
-        case_type: null,
-    });
     app_drilldown.constant('djangoRMICallbackName', 'submit_app_data_drilldown_form');
     app_drilldown.constant('processApplicationDataFormSuccessCallback', function (data) {
         console.log('success');
@@ -62,71 +46,6 @@
             $scope.formData['model_type'] = modelType;
         }
 
-        self._labels = {};
-        self._placeholders = {};
-        self._app_types = [];
-        self._apps_by_type = {};
-        self._modules_by_app = {};
-        self._forms_by_app_by_module = {};
-        self._case_types_by_app = {};
-
-        self._select2Test = {};
-
-        var _formElemContainer = function (fieldSlug) {
-            return $('#div_id_' + fieldSlug);
-        };
-
-        var _formElemGetter = function (fieldSlug) {
-            return $('#id_' + fieldSlug);
-        };
-
-        var _formSelect2Setter = function (fieldSlug) {
-            return function (field_data) {
-                if (fieldSlug) {
-                    $scope.formData[fieldSlug] = '';
-                    _formElemContainer(fieldSlug).find("label").text(self._labels[fieldSlug]);
-                    var $formElem = _formElemGetter(fieldSlug);
-                    if ($formElem.length > 0) {
-                        $formElem.select2({
-                            data: field_data || [],
-                            triggerChange: true,
-                        }).select2('val', formDefaults[fieldSlug]).trigger('change');
-                        $('#s2id_id_' + fieldSlug)
-                            .find('.select2-choice').addClass('select2-default')
-                            .find('.select2-chosen').text(self._placeholders[fieldSlug]);
-                    }
-                    self._select2Test[fieldSlug] = {
-                        data: field_data,
-                        placeholder: self._placeholders[fieldSlug],
-                        defaults: formDefaults[fieldSlug],
-                    };
-                }
-            };
-        };
-
-        var util = {
-            setAppTypes: function () {
-                $scope.formData.app_type = formDefaults.app_type;
-
-                var $formElem = _formElemGetter('app_type');
-                if ($formElem.length > 0) {
-                    $formElem.select2({
-                        data: self._app_types || [],
-                        triggerChange: true,
-                    }).select2('val', formDefaults.app_type).trigger('change');
-                }
-                self._select2Test.app_type = {
-                    data: self._app_types || [],
-                    placeholder: null,
-                    defaults: formDefaults.app_type,
-                };
-            },
-            setApps: _formSelect2Setter(formFieldSlugs.application),
-            setModules: _formSelect2Setter(formFieldSlugs.module),
-            setForms: _formSelect2Setter(formFieldSlugs.form),
-            setCaseTypes: _formSelect2Setter(formFieldSlugs.case_type),
-        };
-
         $scope.resetForm = function () {
             util.setAppTypes();
             util.setApps(self._apps_by_type.all || []);
@@ -147,36 +66,6 @@
             });
         }
 
-        $scope.updateAppChoices = function () {
-            var app_choices = self._apps_by_type[$scope.formData.app_type];
-            util.setApps(app_choices);
-            $scope.selectedAppData = {};
-            $scope.selectedFormData = {};
-            $scope.hasNoCaseTypes = false;
-            util.setModules();
-            util.setForms();
-            util.setCaseTypes();
-        };
-
-        $scope.updateModuleChoices = function () {
-            var module_choices = self._modules_by_app[$scope.formData.application];
-            util.setModules(module_choices);
-            $scope.selectedFormData = {};
-            util.setForms();
-        };
-
-        $scope.updateFormChoices = function () {
-            if ($scope.formData.application && $scope.formData.module) {
-                var form_choices = self._forms_by_app_by_module[$scope.formData.application][$scope.formData.module];
-                util.setForms(form_choices);
-            }
-        };
-
-        $scope.updateCaseTypeChoices = function () {
-            var case_type_choices = self._case_types_by_app[$scope.formData.application];
-            util.setCaseTypes(case_type_choices);
-            $scope.hasNoCaseTypes = _.isEmpty(case_type_choices);
-        };
 
         $scope.handleSubmitForm = function () {
             $scope.isSubmittingForm = true;
@@ -204,32 +93,10 @@
 
         $scope.selectedAppData = {};
         $scope.$watch('formData.application', function (curAppId) {
-            var currentApps = _.filter(self._apps_by_type[$scope.formData.app_type], function (app) {
-                return app.id === curAppId;
-            });
-            if (currentApps.length === 0) {
-                $scope.selectedAppData = {};
-            } else {
-                var app_data = currentApps[0].data;
-                app_data.name = currentApps[0].text;
-                $scope.selectedAppData = app_data;
-            }
         });
 
         $scope.selectedFormData = {};
         $scope.$watch('formData.form', function (curFormId) {
-            if ($scope.formData.application && $scope.formData.module) {
-                var curForms = _.filter(self._forms_by_app_by_module[$scope.formData.application][$scope.formData.module], function (form) {
-                    return form.id === curFormId;
-                });
-                if (curForms.length === 0) {
-                    $scope.selectedFormData = {};
-                } else {
-                    var form_data = curForms[0].data;
-                    form_data.name = curForms[0].text;
-                    $scope.selectedFormData = form_data;
-                }
-            }
         });
     };
     app_drilldown.controller(selectControllers);
