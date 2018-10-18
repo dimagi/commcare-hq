6b3c9d5671cdb9611acd1b7916ed825ab458e9a2
Dawid Mydlo <dmydlo@soldevelo.com>
2018-10-18 10:30:05 +0200

    Fix lint

--- custom/icds_reports/migrations/0070_aww_name_in_agg_ccs_view.py
+++ custom/icds_reports/migrations/0070_aww_name_in_agg_ccs_view.py
@@ -5,8 +5,6 @@ from __future__ import absolute_import
 
 from django.db import migrations
 
-from custom.icds_reports.utils.migrations import get_view_migrations
-
 
 class Migration(migrations.Migration):
 
