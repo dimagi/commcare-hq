4b6101d6bc44ce04525ef545dddaa3381030816e
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-19 15:08:25 -0400

    Added documentation, renamed variables, and moved code blocks.
    The diff on this one is messy. These are fairly mechanical changes: renaming variables
    (mostly replacing 'form' with 'drilldown' since there are several meanings of 'form' in
    this code) and then reordering code blocks to group related functionality together.

--- corehq/apps/export/static/export/js/create_export.js
+++ corehq/apps/export/static/export/js/create_export.js
@@ -1,33 +1,41 @@
+/**
+ * Controls the modal to create a new export.
+ * Used on the basic Export Form/Case Data pages (not SMS), Daily Saved Exports, and Excel Dashboard Integration (feeds).
+ *
+ * Oriented around a drilldown form.
+ *  For form-based exports: app type > app > module > form
+ *  For case-based exports: app type > app > case type
+ *
+ * The daily saved and feeds pages aren't form/case specific,
+ * so for those the drilldown starts with model type (form/case).
+ */
 hqDefine("export/js/create_export", function () {
-    var formDefaults = {
-        model_type: '',
-        app_type: 'all',
-        application: null,
-        module: null,
-        form: null,
-        case_type: null,
-    };
-
     var createExportModel = function (options) {
         var assertProperties = hqImport("hqwebapp/js/assert_properties");
         assertProperties.assert(options, ['drilldown_fetch_url', 'drilldown_submit_url', 'page'], ['model_type']);
-        assertProperties.assert(options.page, ['is_daily_saved_export', 'is_feed', 'is_deid', 'model_type']);
 
         var self = {};
 
+        // This contains flags that distinguish the various pages that use this modal.
+        // Note that there is both a page-level model type and an observable model type below, since model type
+        // is static on the basic form/case export pages but is a user option on the daily saved & feed pages.
+        assertProperties.assert(options.page, ['is_daily_saved_export', 'is_feed', 'is_deid', 'model_type']);
         self.pageOptions = options.page;
 
+        // Flags for the drilldown form, which is fetched via ajax on page initialization.
         self.isLoaded = ko.observable(false);
-        self.isSubmittingForm = ko.observable(false);
+        self.isSubmitting = ko.observable(false);
+        self.drilldownLoadError = ko.observable('');
+        self.drilldownSubmissionError = ko.observable('');
         self.showNoAppsError = ko.observable(false);
-        self.hasNoCaseTypes = ko.observable(false);
-        self.hasSpecialAppTypes = ko.observable(false);
-        self.formLoadError = ko.observable('');
-        self.formSubmissionError = ko.observable('');
 
-        // Form Data
-        self.modelType = ko.observable(options.model_type);
+        self.showDrilldown = ko.computed(function () {
+            return !self.showNoAppsError() && !self.drilldownLoadError() && self.isLoaded() && !self.isSubmitting();
+        });
+
+        // Drilldown data
         self.staticModelType = !!options.model_type;
+        self.modelType = ko.observable(options.model_type);
         self.appType = ko.observable();
         self.application = ko.observable();
         self.caseType = ko.observable();
@@ -43,19 +51,24 @@ hqDefine("export/js/create_export", function () {
         self.isFormModel = ko.computed(function () {
             return self.modelType() === 'form';
         });
+
+        // Behavior related to selecting an app type and an app
+        self.hasSpecialAppTypes = ko.observable(false);
+        self.hasNoCaseTypes = ko.observable(false);
         self.showAppType = ko.computed(function () {
             return self.hasSpecialAppTypes() || self.isFormModel();
         });
 
+        self.showHasNoCaseTypes = ko.computed(function () {
+            return self.hasNoCaseTypes() && self.application();
+        });
+
+        // Behavior related to selecting a form
         self.showMislabeled = ko.observable(false);
         self.showSuggestions = ko.observable(false);
         self.showAppDoesNotExist = ko.observable(false);
         self.showDuplicate = ko.observable(false);
         self.duplicatePossibilities = ko.observableArray();
-
-        self.showHasNoCaseTypes = ko.computed(function () {
-            return self.hasNoCaseTypes() && self.application();
-        });
 
         self.showSubmissionCount = ko.computed(function () {
             return self.modelType() === 'form' && !_.isEmpty(self.selectedFormData());
@@ -63,7 +76,6 @@ hqDefine("export/js/create_export", function () {
         self.submissionCountText = ko.observable('');
 
         self.selectedFormData.subscribe(function (newValue) {
-            // Update form submission count message
             var text = '';
             if (self.modelType() === 'form') {
                 if (newValue.submissions === 1) {
@@ -75,75 +87,36 @@ hqDefine("export/js/create_export", function () {
             }
             self.submissionCountText(text);
 
-            // Display "Mislabeled" message
             self.showMislabeled(self.appType() === '_unknown' && !self.form() && !newValue.app_copy);
-
-            // Display suggestions
             self.showSuggestions(self.appType() === '_unknown' && !self.form() && !newValue.no_suggestions);
-
             self.showAppDoesNotExist(newValue.app_does_not_exist);
-
             self.showDuplicate(newValue.duplicate);
             self.duplicatePossibilities(newValue.possibilities);
             self.selectedFormName(newValue.is_user_registration ? gettext('User Registration') : newValue.name);
         });
 
-        self.showDrilldown = ko.computed(function () {
-            return !self.showNoAppsError() && !self.formLoadError() && self.isLoaded() && !self.isSubmittingForm();
-        });
-
-        self.isValid = ko.computed(function () {
-            return self.modelType()
-                && self.appType()
-                && self.application()
-                && (self.module() || self.isCaseModel())
-                && (self.form() || self.isCaseModel())
-                && (self.caseType() || self.isFormModel());
-        });
-
-        self.showSubmit = ko.computed(function () {
-            return !self.showNoAppsError() && !self.formLoadError();
-        });
-        self.disableSubmit = ko.computed(function () {
-            return !self.isValid() || self.isSubmittingForm();
-        });
-
-        self.handleSubmitForm = function () {
-            self.isSubmittingForm(true);
-            $.ajax({
-                method: 'POST',
-                url: options.submit_app_data_drilldown_form,
-                data: {
-                    is_daily_saved_export: self.pageOptions.is_daily_saved_export,
-                    is_feed: self.pageOptions.is_feed,
-                    is_deid: self.pageOptions.is_deid,
-                    model_type: self.pageOptions.model_type,
-                    form_data: JSON.stringify({
-                        model_type: self.modelType(),
-                        app_type: self.appType(),
-                        application: self.application(),
-                        module: self.module(),
-                        form: self.form(),
-                        case_type: self.caseType(),
-                    }),
-                },
-                success: function (data) {
-                    if (data.success) {
-                        window.location = data.url;
-                    } else {
-                        self.isSubmittingForm(false);
-                        self.formSubmissionError(data.error);
-                    }
-                },
-                error: function () {
-                    self.isSubmittingForm(false);
-                    self.formSubmissionError(gettext("There was an issue fetching data. Please check your internet connection."));
-                },
-            });
+        // Behavior related to initializing the drilldown form
+        var drilldownDefaults = {
+            model_type: '',
+            app_type: 'all',
+            application: null,
+            module: null,
+            form: null,
+            case_type: null,
         };
 
-        // Helper functions for initializing form
-        self._formSelect2Setter = function (observable, fieldSlug) {
+        self.setAppTypes = function () {
+            self.appType(drilldownDefaults.app_type);
+
+            var $formElem = $('#id_app_type');
+            if ($formElem.length > 0) {
+                $formElem.select2({
+                    data: self._app_types || [],
+                    triggerChange: true,
+                }).select2('val', drilldownDefaults.app_type).trigger('change');
+            }
+        },
+        self._initSelect2 = function (observable, fieldSlug) {
             return function (field_data) {
                 if (fieldSlug) {
                     observable('');
@@ -153,7 +126,7 @@ hqDefine("export/js/create_export", function () {
                         $formElem.select2({
                             data: field_data || [],
                             triggerChange: true,
-                        }).select2('val', formDefaults[fieldSlug]).trigger('change');
+                        }).select2('val', drilldownDefaults[fieldSlug]).trigger('change');
                         $('#s2id_id_' + fieldSlug)
                             .find('.select2-choice').addClass('select2-default')
                             .find('.select2-chosen').text(self._placeholders[fieldSlug]);
@@ -161,37 +134,12 @@ hqDefine("export/js/create_export", function () {
                 }
             };
         };
-        self.setAppTypes = function () {
-            self.appType(formDefaults.app_type);
+        self.setApps = self._initSelect2(self.application, 'application');
+        self.setModules = self._initSelect2(self.module, 'module');
+        self.setForms = self._initSelect2(self.form, 'form');
+        self.setCaseTypes = self._initSelect2(self.caseType, 'case_type');
 
-            var $formElem = $('#id_app_type');
-            if ($formElem.length > 0) {
-                $formElem.select2({
-                    data: self._app_types || [],
-                    triggerChange: true,
-                }).select2('val', formDefaults.app_type).trigger('change');
-            }
-        },
-        self.setApps = self._formSelect2Setter(self.application, 'application');
-        self.setModules = self._formSelect2Setter(self.module, 'module');
-        self.setForms = self._formSelect2Setter(self.form, 'form');
-        self.setCaseTypes = self._formSelect2Setter(self.caseType, 'case_type');
-        self.resetForm = function () {
-            if (!self.isLoaded()) {
-                return;
-            }
-
-            self.setAppTypes();
-            self.setApps(self._apps_by_type.all || []);
-            self.setModules();
-            self.setForms();
-            self.setCaseTypes();
-            self.selectedAppData({});
-            self.selectedFormData({});
-            self.hasNoCaseTypes(false);
-        };
-
-        // Helper functions for handling drilldown
+        // Behavior of drilldown itself (interactions between the dropdowns)
         self.updateAppChoices = function () {
             var app_choices = self._apps_by_type[self.appType()];
             self.setApps(app_choices);
@@ -202,7 +150,6 @@ hqDefine("export/js/create_export", function () {
             self.setForms();
             self.setCaseTypes();
         };
-        self.modelType.subscribe(self.resetForm);
         self.application.subscribe(function (newValue) {
             if (newValue) {
                 if (self.modelType() === 'form') {
@@ -254,7 +201,74 @@ hqDefine("export/js/create_export", function () {
             }
         });
 
-        // Fetch form values on page load
+        // Behavior related to validating and resetting the drilldown form
+        self.isValid = ko.computed(function () {
+            return self.modelType()
+                && self.appType()
+                && self.application()
+                && (self.module() || self.isCaseModel())
+                && (self.form() || self.isCaseModel())
+                && (self.caseType() || self.isFormModel());
+        });
+        self.resetForm = function () {
+            if (!self.isLoaded()) {
+                return;
+            }
+
+            self.setAppTypes();
+            self.setApps(self._apps_by_type.all || []);
+            self.setModules();
+            self.setForms();
+            self.setCaseTypes();
+            self.selectedAppData({});
+            self.selectedFormData({});
+            self.hasNoCaseTypes(false);
+        };
+        self.modelType.subscribe(self.resetForm);
+
+        // Behavior related to submitting drilldown form
+        self.showSubmit = ko.computed(function () {
+            return !self.showNoAppsError() && !self.drilldownLoadError();
+        });
+        self.disableSubmit = ko.computed(function () {
+            return !self.isValid() || self.isSubmitting();
+        });
+
+        self.handleSubmitForm = function () {
+            self.isSubmitting(true);
+            $.ajax({
+                method: 'POST',
+                url: options.drilldown_submit_url,
+                data: {
+                    is_daily_saved_export: self.pageOptions.is_daily_saved_export,
+                    is_feed: self.pageOptions.is_feed,
+                    is_deid: self.pageOptions.is_deid,
+                    model_type: self.pageOptions.model_type,
+                    form_data: JSON.stringify({
+                        model_type: self.modelType(),
+                        app_type: self.appType(),
+                        application: self.application(),
+                        module: self.module(),
+                        form: self.form(),
+                        case_type: self.caseType(),
+                    }),
+                },
+                success: function (data) {
+                    if (data.success) {
+                        window.location = data.url;
+                    } else {
+                        self.isSubmitting(false);
+                        self.drilldownSubmissionError(data.error);
+                    }
+                },
+                error: function () {
+                    self.isSubmitting(false);
+                    self.drilldownSubmissionError(gettext("There was an issue fetching data. Please check your internet connection."));
+                },
+            });
+        };
+
+        // Initialize drilldown on page load
         $.ajax({
             method: 'GET',
             url: options.drilldown_fetch_url,
@@ -284,11 +298,10 @@ hqDefine("export/js/create_export", function () {
                 self.isLoaded(true);
             },
             error: function () {
-                self.formLoadError(gettext('There is an issue communicating with the server at this time.'));
+                self.drilldownLoadError(gettext('There is an issue communicating with the server at this time.'));
                 self.isLoaded(true);
             },
         });
-
         return self;
     };
 
