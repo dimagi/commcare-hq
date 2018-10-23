588694760a7741139163a38b2e2a181c1d98c81d
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2018-10-23 12:54:51 +0200

    Moved Daily Attendance sql to python

--- custom/icds_reports/const.py
+++ custom/icds_reports/const.py
@@ -94,5 +94,6 @@ USAGE_TABLE_ID = 'static-usage_forms'
 PERSON_TABLE_ID = 'static-person_cases_v2'
 HOUSEHOLD_TABLE_ID = 'static-household_cases'
 AWW_USER_TABLE_ID = 'static-commcare_user_cases'
+DAILY_FEEDING_TABLE_ID = 'static-daily_feeding_forms'
 
 DASHBOARD_DOMAIN = 'icds-dashboard-qa' if settings.SERVER_ENVIRONMENT == 'softlayer' else 'icds-cas'
