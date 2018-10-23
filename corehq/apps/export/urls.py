f61b3698ec16ce0ecd6308231df6f51c79414533
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-23 14:12:37 -0400

    Migrated poll_custom_export_download

--- corehq/apps/export/urls.py
+++ corehq/apps/export/urls.py
@@ -51,7 +51,6 @@ from corehq.apps.export.views.utils import (
     DailySavedExportPaywall,
     DataFileDownloadList,
     DataFileDownloadDetail,
-    GenerateSchemaFromAllBuildsView,
 )
 
 urlpatterns = [
