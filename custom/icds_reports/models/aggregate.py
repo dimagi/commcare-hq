7956a50e39d99975d3407f7b52a3cd3c1c89eddc
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2018-10-24 08:35:46 +0200

    Moved BirthPreparednessFormsAggregationHelper to own file

--- custom/icds_reports/models/aggregate.py
+++ custom/icds_reports/models/aggregate.py
@@ -13,7 +13,6 @@ from custom.icds_reports.const import (AGG_CCS_RECORD_BP_TABLE,
 from custom.icds_reports.utils.aggregation import (
     AggCcsRecordAggregationHelper, AggChildHealthAggregationHelper,
     AwcInfrastructureAggregationHelper,
-    BirthPreparednessFormsAggregationHelper,
     CcsRecordMonthlyAggregationHelper, ChildHealthMonthlyAggregationHelper,
     DailyFeedingFormsChildHealthAggregationHelper,
     DeliveryFormsAggregationHelper,
@@ -24,6 +23,8 @@ from dateutil.relativedelta import relativedelta
 from django.db import connections, models, transaction
 from six.moves import range
 
+from custom.icds_reports.utils.aggregation_helpers.birth_preparedness_forms import \
+    BirthPreparednessFormsAggregationHelper
 from custom.icds_reports.utils.aggregation_helpers.complementary_forms import ComplementaryFormsAggregationHelper
 from custom.icds_reports.utils.aggregation_helpers.complementary_forms_ccs_record import \
     ComplementaryFormsCcsRecordAggregationHelper
