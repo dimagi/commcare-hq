20de3ce02ea7d10de78a74cc75f963df3f4b5cbf
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-25 10:47:55 -0400

    Added support for showing download progress
    This replaces angular's DownloadProgressController.

--- corehq/apps/export/static/export/js/download_export.js
+++ corehq/apps/export/static/export/js/download_export.js
@@ -40,6 +40,8 @@ hqDefine('export/js/download_export', function () {
         self.dateRange = ko.observable();
         self.emw = ko.observable();
 
+        self.progressModel = options.progressModel;
+
         // UI flags
         self.preparingExport = ko.observable(false);
         self.prepareExportError = ko.observable('');
@@ -113,8 +115,7 @@ hqDefine('export/js/download_export', function () {
                         self.sendAnalytics();
                         self.preparingExport(false);
                         self.downloadInProgress(true);
-                        // TODO
-                        //exportDownloadService.startDownload(data.download_id, self.exportType);
+                        self.progressModel.startDownload(data.download_id);
                     } else {
                         self._handlePrepareError(data);
                     }
@@ -137,6 +138,156 @@ hqDefine('export/js/download_export', function () {
         return self;
     };
 
+    // Model for showing progress, etc once the download has been generated
+    var downloadProgressModel = function (options) {
+        hqImport("hqwebapp/js/assert_properties").assert(options, ['exportType', 'formOrCase', 'emailUrl', 'pollUrl']);
+
+        var self = {};
+
+        self.exportType = options.exportType;
+        self.formOrCase = options.formOrCase;
+
+        self.emailUrl = options.emailUrl;
+        self.pollUrl = options.pollUrl;
+
+        self.downloadId = ko.observable();
+        self.showDownloadStatus = ko.observable(false);
+        self.showError = ko.observable(false);
+        self.hasCeleryError = ko.observable(false);
+        self.isDownloaded = ko.observable(false);
+        self.isDownloadReady = ko.observable(false);
+        self.hasDownloadError = ko.observable(false);
+        self.isMultimediaDownload = ko.observable(false);
+        self.progressError = ko.observable('');
+        self.progress = ko.observable(0);
+        self.sendEmailFlag = ko.observable(false);
+
+        self.dropboxUrl = ko.observable('');
+        self.downloadUrl = ko.observable('');
+
+        self.resetDownload = function () {
+            self.downloadId(null);
+            self._numErrors = 0;
+            self._numCeleryRetries = 0;
+            self._lastProgress = 0;
+            self.showDownloadStatus(false);
+            self.hasCeleryError(false);
+            self.hasDownloadError(false);
+            self.isMultimediaDownload(false);
+        };
+        self.resetDownload();
+
+        self.startDownload = function (downloadId) {
+            self.showDownloadStatus(true);
+            self.downloadId(downloadId);
+            self.interval = setInterval(self._checkDownloadProgress, 2000);
+        };
+
+        self.clickDownload = function () {
+            self.isDownloaded(true);
+            self.sendAnalytics();
+            return true;    // allow default click action
+        };
+
+        self.sendAnalytics = function () {
+            hqImport('analytix/js/google').track.event("Download Export", hqImport('export/js/utils').capitalize(self.exportType), "Saved");
+            hqImport('analytix/js/kissmetrix').track.event("Clicked Download button");
+        };
+
+        self._checkDownloadProgress = function () {
+            $.ajax({
+                method: 'GET',
+                url: self.pollUrl,
+                data: {
+                    form_or_case: self.formOrCase,
+                    download_id: self.downloadId,
+                },
+                success: function (data) {
+                    if (data.is_poll_successful) {
+                        self._updateProgressBar(data);
+                        self.downloadId(data.download_id);
+                        if (data.has_file && data.is_ready) {
+                            clearInterval(self.interval);
+                            return;
+                        }
+                        if (data.progress && data.progress.error) {
+                            clearInterval(self.interval);
+                            self.downloadError(data.progress.error);
+                            return;
+                        }
+                        if (data.progress.current > self._lastProgress) {
+                            self._lastProgress = data.progress.current;
+                            // processing is still going, keep moving.
+                            // this avoids failing hard prematurely at celery errors if
+                            // the polling is still reporting forward progress.
+                            self._numCeleryRetries = 0;
+                            return;
+                        }
+                    }
+                    if (data.error) {
+                        self._dealWithErrors(data);
+                    }
+                    if (_.isNull(data.is_alive)) {
+                        self._dealWithCeleryErrors();
+                    }
+                },
+                error: self._dealWithErrors,
+            });
+        };
+
+        self._dealWithCeleryErrors = function () {
+            // Sometimes the task handler for celery is a little slow to get
+            // started, so we have to try a few times.
+            if (self._numCeleryRetries > 10) {
+                clearInterval(self.interval);
+                self.celeryError = true;
+            }
+            self._numCeleryRetries ++;
+        };
+
+        self._dealWithErrors = function (data) {
+            if (self._numErrors > 3) {
+                if (data && data.error) {
+                    self.downloadError = data.error;
+                } else {
+                    self.downloadError = "default";
+                }
+                clearInterval(self.interval);
+            }
+            self._numErrors ++;
+        };
+
+        self._updateProgressBar = function (data) {
+            var progressPercent = 0;
+            if (data.is_ready && data.has_file) {
+                progressPercent = 100;
+                self.isDownloadReady(true);
+                self.dropboxUrl(data.dropbox_url);
+                self.downloadUrl(data.download_url);
+            } else if (_.isNumber(data.progress.percent)) {
+                progressPercent = data.progress.percent;
+            }
+            self.progress(progressPercent);
+        };
+
+        self.sendEmailUponCompletion = function () {
+            setTimeout(function () {  // function must wait until download_id is available in the scope // <= TODO: still true?
+                if (self.downloadId) {
+                    $.ajax({
+                        method: 'POST',
+                        dataType: 'json',
+                        url: self.emailUrl,
+                        data: { download_id: self.downloadId },
+                    });
+                } else {
+                    self.sendEmailUponCompletion();
+                }
+            });
+        };
+
+        return self;
+    };
+
     $(function () {
         hqImport("reports/js/filters/main").init();
 
@@ -151,14 +302,23 @@ hqDefine('export/js/download_export', function () {
             pollUrl: initialPageData.reverse('poll_custom_export_download'),
         });
 
+        var progressModel = downloadProgressModel({
+            exportType: exportType,
+            emailUrl: initialPageData.reverse('add_export_email_request'),
+            formOrCase: initialPageData.get('form_or_case'),
+            pollUrl: initialPageData.reverse('poll_custom_export_download'),
+        });
+
         $("#download-export-form").koApplyBindings(downloadFormModel({
             exportList: exportList,
             exportType: exportType,
             formOrCase: initialPageData.get('form_or_case'),
             userTypes: initialPageData.get('user_types'),
-            download: download,
+            progressModel: progressModel,
             prepareUrl: initialPageData.reverse('prepare_custom_export'),
             smsExport: initialPageData.get('sms_export'),
         }));
+
+        $("#download-progress").koApplyBindings(progressModel);
     });
 });
