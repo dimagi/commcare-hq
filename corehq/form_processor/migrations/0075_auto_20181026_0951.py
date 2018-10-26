406c82f441eaf7cbc11f005bb7bc421042e470d6
mkangia <mkangia@dimagi.com>
2018-10-26 22:30:24 +0530

    made changes to enable roll out via django

--- corehq/form_processor/migrations/0075_auto_20181026_0951.py
+++ corehq/form_processor/migrations/0075_auto_20181026_0951.py
@@ -4,22 +4,39 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from django.db import migrations
-from corehq.util.django_migrations import AlterIndexIfNotExists
+
+from corehq.sql_db.operations import HqRunSQL
+
+TABLE_NAME = 'form_processor_commcarecasesql'
+INDEX_NAME = 'form_processor_commcarecasesql_domain_type_05e599e0_idx'
+COLUMNS = ['domain', 'type']
+
+CREATE_INDEX_SQL = "CREATE INDEX CONCURRENTLY {} ON {} ({})".format(
+    INDEX_NAME, TABLE_NAME, ','.join(COLUMNS))
+DROP_INDEX_SQL = "DROP INDEX CONCURRENTLY {}".format(INDEX_NAME)
 
 
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         ('form_processor', '0074_casetransaction__client_date'),
     ]
 
     operations = [
-        AlterIndexIfNotExists(
-            name='commcarecasesql',
-            index_together=set([
-                ('owner_id', 'server_modified_on'),
-                ('domain', 'owner_id', 'closed'),
-                ('domain', 'external_id', 'type'),
-                ('domain', 'type')]),
+        HqRunSQL(
+            sql=CREATE_INDEX_SQL,
+            reverse_sql=DROP_INDEX_SQL,
+            state_operations=[
+                migrations.AlterIndexTogether(
+                    name='commcarecasesql',
+                    index_together=set([
+                        ('owner_id', 'server_modified_on'),
+                        ('domain', 'owner_id', 'closed'),
+                        ('domain', 'external_id', 'type'),
+                        ('domain', 'type')
+                    ]),
+                ),
+            ]
         ),
     ]
