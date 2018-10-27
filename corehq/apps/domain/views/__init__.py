06e9c8665804a05309864b2c230024cbea6c9c5e
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-27 17:25:26 -0400

    fixup! Added views directory and all imports to each file

--- corehq/apps/domain/views/__init__.py
+++ corehq/apps/domain/views/__init__.py
@@ -1,88 +1,88 @@
 from __future__ import absolute_import
 
 '''
-from corehq.apps.domain.base import(
-    def select(request, domain_select_template='domain/select.html', do_not_redirect=False):
-    class DomainViewMixin(object):
-    class LoginAndDomainMixin(object):
-    class BaseDomainView(LoginAndDomainMixin, BaseSectionPageView, DomainViewMixin):
+from corehq.apps.domain.views.base import (
+    select,
+    DomainViewMixin,
+    LoginAndDomainMixin,
+    BaseDomainView,
 )
-from corehq.apps.domain.accounting import(
-    class EditPrivacySecurityView(BaseAdminProjectSettingsView):
-    class SelectedEnterprisePlanView(SelectPlanView):
-    class SelectedAnnualPlanView(SelectPlanView):
-    class ConfirmSelectedPlanView(SelectPlanView):
-    class ConfirmBillingAccountInfoView(ConfirmSelectedPlanView, AsyncHandlerMixin):
-    class SubscriptionMixin(object):
-    class SubscriptionRenewalView(SelectPlanView, SubscriptionMixin):
-    class ConfirmSubscriptionRenewalView(DomainAccountingSettings, AsyncHandlerMixin, SubscriptionMixin):
-    class EmailOnDowngradeView(View):
-    class SubscriptionUpgradeRequiredView(LoginAndDomainMixin, BasePageView,
-    class DomainAccountingSettings(BaseProjectSettingsView):
-    class DomainSubscriptionView(DomainAccountingSettings):
-    class EditExistingBillingAccountView(DomainAccountingSettings, AsyncHandlerMixin):
-    class DomainBillingStatementsView(DomainAccountingSettings, CRUDPaginatedViewMixin):
-    class BaseStripePaymentView(DomainAccountingSettings):
-    class CreditsStripePaymentView(BaseStripePaymentView):
-    class CreditsWireInvoiceView(DomainAccountingSettings):
-    class InvoiceStripePaymentView(BaseStripePaymentView):
-    class BulkStripePaymentView(BaseStripePaymentView):
-    class WireInvoiceView(View):
-    class BillingStatementPdfView(View):
-    class InternalSubscriptionManagementView(BaseAdminProjectSettingsView):
-    class SelectPlanView(DomainAccountingSettings):
-    class BaseCardView(DomainAccountingSettings):
-    class CardView(BaseCardView):
-    class CardsView(BaseCardView):
+from corehq.apps.domain.views.accounting import (
+    EditPrivacySecurityView,
+    SelectedEnterprisePlanView,
+    SelectedAnnualPlanView,
+    ConfirmSelectedPlanView,
+    ConfirmBillingAccountInfoView,
+    SubscriptionMixin,
+    SubscriptionRenewalView,
+    ConfirmSubscriptionRenewalView,
+    EmailOnDowngradeView,
+    SubscriptionUpgradeRequiredView,
+    DomainAccountingSettings,
+    DomainSubscriptionView,
+    EditExistingBillingAccountView,
+    DomainBillingStatementsView,
+    BaseStripePaymentView,
+    CreditsStripePaymentView,
+    CreditsWireInvoiceView,
+    InvoiceStripePaymentView,
+    BulkStripePaymentView,
+    WireInvoiceView,
+    BillingStatementPdfView,
+    InternalSubscriptionManagementView,
+    SelectPlanView,
+    BaseCardView,
+    CardView,
+    CardsView,
 )
-from corehq.apps.domain.exchange import(
-    class ExchangeSnapshotsView(BaseAdminProjectSettingsView):
-    class CreateNewExchangeSnapshotView(BaseAdminProjectSettingsView):
-    def _publish_snapshot(request, domain, published_snapshot=None):
-    def _notification_email_on_publish(domain, snapshot, published_by):
-    def set_published_snapshot(request, domain, snapshot_name=''):
+from corehq.apps.domain.views.exchange import (
+    ExchangeSnapshotsView,
+    CreateNewExchangeSnapshotView,
+    _publish_snapshot,
+    _notification_email_on_publish,
+    set_published_snapshot,
 )
-from corehq.apps.domain.fixtures import(
-    class CalendarFixtureConfigView(BaseAdminProjectSettingsView):
-    class LocationFixtureConfigView(BaseAdminProjectSettingsView):
+from corehq.apps.domain.views.fixtures import (
+    CalendarFixtureConfigView,
+    LocationFixtureConfigView,
 )
-from corehq.apps.domain.internal import(
-    def calculated_properties(request, domain):
-    class FlagsAndPrivilegesView(BaseAdminProjectSettingsView):
-    class TransferDomainView(BaseAdminProjectSettingsView):
-    class ActivateTransferDomainView(BasePageView):
-    class DeactivateTransferDomainView(View):
-    class BaseInternalDomainSettingsView(BaseProjectSettingsView):
-    class EditInternalDomainInfoView(BaseInternalDomainSettingsView):
-    class EditInternalCalculationsView(BaseInternalDomainSettingsView):
+from corehq.apps.domain.views.internal import (
+    calculated_properties,
+    FlagsAndPrivilegesView,
+    TransferDomainView,
+    ActivateTransferDomainView,
+    DeactivateTransferDomainView,
+    BaseInternalDomainSettingsView,
+    EditInternalDomainInfoView,
+    EditInternalCalculationsView,
 )
-from corehq.apps.domain.pro_bono import(
-    class ProBonoMixin(object):
-    class ProBonoStaticView(ProBonoMixin, BasePageView):
-    class ProBonoView(ProBonoMixin, DomainAccountingSettings):
+from corehq.apps.domain.views.pro_bono import (
+    ProBonoMixin,
+    ProBonoStaticView,
+    ProBonoView,
 )
-from corehq.apps.domain.repeaters import(
-    def generate_repeater_payloads(request, domain):
+from corehq.apps.domain.views.repeaters import (
+    generate_repeater_payloads,
 )
-from corehq.apps.domain.settings import(
-    class BaseProjectSettingsView(BaseDomainView):
-    class DefaultProjectSettingsView(BaseDomainView):
-    class BaseAdminProjectSettingsView(BaseProjectSettingsView):
-    class BaseEditProjectInfoView(BaseAdminProjectSettingsView):
-    class EditBasicProjectInfoView(BaseEditProjectInfoView):
-    class EditMyProjectSettingsView(BaseProjectSettingsView):
-    class EditOpenClinicaSettingsView(BaseProjectSettingsView):
-    class ManageProjectMediaView(BaseAdminProjectSettingsView):
-    def logo(request, domain):
-    class CaseSearchConfigView(BaseAdminProjectSettingsView):
-    class RecoveryMeasuresHistory(BaseAdminProjectSettingsView):
+from corehq.apps.domain.views.settings import (
+    BaseProjectSettingsView,
+    DefaultProjectSettingsView,
+    BaseAdminProjectSettingsView,
+    BaseEditProjectInfoView,
+    EditBasicProjectInfoView,
+    EditMyProjectSettingsView,
+    EditOpenClinicaSettingsView,
+    ManageProjectMediaView,
+    logo,
+    CaseSearchConfigView,
+    RecoveryMeasuresHistory,
 )
-from corehq.apps.domain.sms import(
-    class PublicSMSRatesView(BasePageView, AsyncHandlerMixin):
-    class SMSRatesView(BaseAdminProjectSettingsView, AsyncHandlerMixin):
+from corehq.apps.domain.views.sms import (
+    PublicSMSRatesView,
+    SMSRatesView,
 )
-from corehq.apps.domain.toggles import(
-    def toggle_diff(request, domain):
-    class FeaturePreviewsView(BaseAdminProjectSettingsView):
+from corehq.apps.domain.views.toggles import (
+    toggle_diff,
+    FeaturePreviewsView,
 )
 '''
