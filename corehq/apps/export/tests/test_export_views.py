550f78ab38572179f66f2c1fb5a41f3839dbf4c4
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-27 18:45:17 -0400

    Fixed another test failure

--- corehq/apps/export/tests/test_export_views.py
+++ corehq/apps/export/tests/test_export_views.py
@@ -32,7 +32,7 @@ from corehq.apps.export.views.new import (
     CreateNewCustomFormExportView,
     CreateNewDailySavedCaseExport,
 )
-from corehq.apps.export.util import DataFileDownloadDetail
+from corehq.apps.export.views.utils import DataFileDownloadDetail
 from corehq.util.test_utils import flag_enabled, generate_cases
 from io import open
 
