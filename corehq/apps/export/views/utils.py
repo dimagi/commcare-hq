d05c9bd1bb6889a4cabf8678c9dc2cfc4cd14388
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-27 15:54:30 -0400

    Removed unused imports

--- corehq/apps/export/views/utils.py
+++ corehq/apps/export/views/utils.py
@@ -2,53 +2,28 @@ from __future__ import absolute_import
 
 from __future__ import division
 from __future__ import unicode_literals
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
 
-from couchdbkit import ResourceNotFound
-from django.conf import settings
 from django.contrib import messages
-from django.contrib.humanize.templatetags.humanize import naturaltime
-from django.core.exceptions import SuspiciousOperation
 from django.urls import reverse
 from django.http import HttpResponseRedirect, HttpResponseBadRequest, Http404, HttpResponse, \
     HttpResponseServerError
-from django.template.defaultfilters import filesizeformat
-from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_GET, require_POST
 
-from corehq.apps.analytics.tasks import send_hubspot_form, HUBSPOT_DOWNLOADED_EXPORT_FORM_ID
 from corehq.blobs.exceptions import NotFound
 from corehq.util.download import get_download_response
 from corehq.util.timezones.utils import get_timezone_for_user
-from corehq.toggles import MESSAGE_LOG_METADATA, PAGINATED_EXPORTS
-from corehq.apps.export.export import get_export_download, get_export_size
-from corehq.apps.export.models.new import DatePeriod, DataFile, EmailExportWhenDoneRequest
-from corehq.apps.hqwebapp.views import HQJSONResponseMixin
-from corehq.apps.hqwebapp.utils import format_angular_error, format_angular_success
+from corehq.apps.export.models.new import DataFile, DatePeriod
 from corehq.apps.locations.models import SQLLocation
-from corehq.apps.locations.permissions import location_safe, location_restricted_response
-from corehq.apps.reports.filters.case_list import CaseListFilter
-from corehq.apps.reports.filters.users import ExpandedMobileWorkerFilter, SubmitHistoryFilter
-from corehq.apps.reports.views import should_update_export
-from corehq.apps.reports.models import HQUserType
+from corehq.apps.locations.permissions import location_safe
 from corehq.privileges import EXCEL_DASHBOARD, DAILY_SAVED_EXPORT
-from django_prbac.utils import has_privilege
 from django.utils.decorators import method_decorator
-import json
-import re
-from django.utils.safestring import mark_safe
 from django.views.generic import View
 
-from couchexport.writers import XlsLengthException
 
-from djangular.views.mixins import allow_remote_invocation
 import pytz
-from corehq import privileges, toggles
+from corehq import privileges
 from corehq.apps.accounting.utils import domain_has_privilege
-from corehq.apps.app_manager.fields import ApplicationDataRMIHelper
-from corehq.couchapps.dbaccessors import forms_have_multimedia
-from corehq.apps.data_interfaces.dispatcher import require_can_edit_data
-from corehq.apps.domain.decorators import login_and_domain_required, api_auth
+from corehq.apps.domain.decorators import api_auth
 from corehq.apps.export.tasks import (
     generate_schema_for_all_builds,
     get_saved_export_task_status,
@@ -91,7 +66,6 @@ from corehq.apps.export.dbaccessors import (
     get_case_exports_by_domain,
     get_form_exports_by_domain,
 )
-from corehq.apps.reports.models import HQGroupExportConfiguration
 from corehq.apps.reports.util import datespan_from_beginning
 from corehq.apps.settings.views import BaseProjectDataView
 from corehq.apps.hqwebapp.decorators import (
@@ -100,9 +74,8 @@ from corehq.apps.hqwebapp.decorators import (
     use_jquery_ui,
     use_ko_validation,
     use_angular_js)
-from corehq.apps.hqwebapp.widgets import DateRangePickerWidget
 from corehq.apps.users.decorators import get_permission_name
-from corehq.apps.users.models import Permissions, CouchUser, WebUser
+from corehq.apps.users.models import Permissions
 from corehq.apps.users.permissions import (
     can_download_data_files,
     CASE_EXPORT_PERMISSION,
@@ -110,19 +83,12 @@ from corehq.apps.users.permissions import (
     FORM_EXPORT_PERMISSION,
     has_permission_to_view_report,
 )
-from corehq.apps.analytics.tasks import track_workflow
 from corehq.util.timezones.utils import get_timezone_for_user
 from couchexport.models import Format
-from memoized import memoized
-from django.utils.translation import ugettext as _, ugettext_noop, ugettext_lazy
-from dimagi.utils.logging import notify_exception
+from django.utils.translation import ugettext as _, ugettext_lazy
 from dimagi.utils.web import json_response, get_url_base
-from dimagi.utils.couch import CriticalSection
 from soil import DownloadBase
-from soil.exceptions import TaskFailedError
-from soil.util import get_download_context, process_email_request
 from soil.progress import get_task_status
-from six.moves import map
 
 
 def get_timezone(domain, couch_user):
