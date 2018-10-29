53fb145612d8947b09bbb502a427f94be8f196b6
Rohit Negi <rohit25.negi@gmail.com>
2018-10-29 10:48:44 +0530

    Summing active awc 1. correcting sql syntax error

--- custom/icds_reports/migrations/sql_templates/database_functions/aggregated_awc_data_weekly.sql
+++ custom/icds_reports/migrations/sql_templates/database_functions/aggregated_awc_data_weekly.sql
@@ -32,7 +32,7 @@ BEGIN
     'sum(add_household) AS usage_num_hh_reg, ' ||
     'CASE WHEN sum(add_household) > 0 THEN ' || quote_literal(_yes_text) || ' ELSE ' || quote_literal(_no_text) || ' END as is_launched, '
     'CASE WHEN sum(add_household) > 0 THEN 1 ELSE 0 END as num_launched_awcs, '
-    'CASE WHEN (sum(due_list_ccs) + sum(due_list_child) + sum(pse) + sum(gmp) + sum(thr) + sum(home_visit) + sum(add_pregnancy) + sum(add_household)) >= 15 THEN 1 ELSE 0 END AS usage_awc_num_active, ' ||
+    'CASE WHEN (sum(due_list_ccs) + sum(due_list_child) + sum(pse) + sum(gmp) + sum(thr) + sum(home_visit) + sum(add_pregnancy) + sum(add_household)) >= 15 THEN 1 ELSE 0 END AS usage_awc_num_active ' ||
     'FROM ' || quote_ident(_usage_tablename) || ' ' ||
     'WHERE month >= ' || quote_literal(_month_start_6m) || ' GROUP BY awc_id, month) ut ' ||
   'WHERE ut.month <= agg_awc.month AND ut.awc_id = agg_awc.awc_id AND aggregation_level=5 '||
@@ -47,14 +47,14 @@ BEGIN
     'num_launched_blocks = ut.num_launched_supervisors, ' ||
     'num_launched_supervisors = ut.num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active ' ||
+    'usage_awc_num_active = ut.sum_usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'supervisor_id'
     'month, ' ||
     'sum(usage_num_hh_reg) as sum_usage_num_hh_reg, ' ||
     'CASE WHEN (sum(num_launched_awcs) > 0) THEN 1 ELSE 0 END as num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
-    'usage_awc_num_active, ' ||
+    'sum(usage_awc_num_active) as sum_usage_awc_num_active ' ||
     'FROM agg_awc  ' ||
     'WHERE aggregation_level=5 AND month>=' || quote_literal(_month_start_6m) || ' ' ||
     'GROUP BY state_id, district_id, block_id, supervisor_id, month) ut ' ||
@@ -69,7 +69,7 @@ BEGIN
     'num_launched_blocks = ut.num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active ' ||
+    'usage_awc_num_active = ut.sum_usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'block_id' ||
     'month, ' ||
@@ -77,7 +77,7 @@ BEGIN
     'CASE WHEN (sum(num_launched_supervisors) > 0) THEN 1 ELSE 0 END as num_launched_blocks, '||
     'sum(num_launched_supervisors) as sum_num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
-    'usage_awc_num_active, ' ||
+    'sum(usage_awc_num_active) as sum_usage_awc_num_active ' ||
     'FROM agg_awc  ' ||
     'WHERE aggregation_level=4 AND month>='|| quote_literal(_month_start_6m) || ' ' ||
     'GROUP BY state_id, district_id, block_id, month) ut ' ||
@@ -92,7 +92,7 @@ BEGIN
     'num_launched_blocks = ut.sum_num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active ' ||
+    'usage_awc_num_active = ut.sum_usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'district_id' ||
     'month, ' ||
@@ -101,7 +101,7 @@ BEGIN
     'sum(num_launched_blocks) as sum_num_launched_blocks, ' ||
     'sum(num_launched_supervisors) as sum_num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
-    'usage_awc_num_active, ' ||
+    'sum(usage_awc_num_active) as  sum_usage_awc_num_active' ||
     'FROM agg_awc  ' ||
     'WHERE aggregation_level=3 AND month>='|| quote_literal(_month_start_6m) || ' ' ||
     'GROUP BY state_id, district_id, month) ut ' ||
@@ -116,7 +116,7 @@ BEGIN
     'num_launched_blocks = ut.sum_num_launched_blocks, ' ||
     'num_launched_supervisors = ut.sum_num_launched_supervisors, ' ||
     'num_launched_awcs = ut.sum_num_launched_awcs, ' ||
-    'usage_awc_num_active = ut.usage_awc_num_active ' ||
+    'usage_awc_num_active = ut.sum_usage_awc_num_active ' ||
   'FROM (SELECT ' ||
     'state_id' ||
     'month, ' ||
@@ -126,7 +126,7 @@ BEGIN
     'sum(num_launched_blocks) as sum_num_launched_blocks, ' ||
     'sum(num_launched_supervisors) as sum_num_launched_supervisors, '||
     'sum(num_launched_awcs) as sum_num_launched_awcs, '||
-    'usage_awc_num_active, ' ||
+    'sum(usage_awc_num_active) as sum_usage_awc_num_active' ||
     'FROM agg_awc  ' ||
     'WHERE aggregation_level=2 AND month>='|| quote_literal(_month_start_6m) || ' ' ||
     'GROUP BY state_id,month) ut ' ||
