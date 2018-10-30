6ce320dfff33c28c482d7141b72e69bca58677f2
Jennifer Schweers <orange.jenny@gmail.com>
2018-10-30 19:12:17 -0400

    Added another patch

--- corehq/apps/export/tests/test_export_views.py
+++ corehq/apps/export/tests/test_export_views.py
@@ -247,6 +247,7 @@ class ExportViewTest(ViewTestCase):
         self.assertEqual(resp.status_code, 200)
 
     @patch('corehq.apps.export.views.list.domain_has_privilege', lambda x, y: True)
+    @patch('corehq.apps.export.views.new.domain_has_privilege', lambda x, y: True)
     @patch("corehq.apps.export.tasks.rebuild_export")
     def test_edit_daily_saved_export_filters(self, _):
         # Create an export
