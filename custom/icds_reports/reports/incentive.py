84b5d555a2ee69a4bf30d6b07a2e263b5fd7ac20
Cal Ellowitz <calellowitz@gmail.com>
2018-10-30 13:36:44 -0400

    use proper language

--- custom/icds_reports/reports/incentive.py
+++ custom/icds_reports/reports/incentive.py
@@ -22,21 +22,25 @@ class IncentiveReport(object):
             weighing_efficiency = row['wer_weighed'] / row['wer_eligible'] if row['wer_eligible'] != 0 else 1
             if home_visit_percent > 1:
                 home_visit_percent = 1
+            if row['awc_num_open'] is None:
+                num_open = 'Data not entered'
+            else:
+                num_open = row['awc_num_open']
 
             excel_rows.append([row['state_name'],
-                              row['district_name'],
-                              row['block_name'],
-                              row['supervisor_name'],
-                              row['awc_name'], row['aww_name'],
-                              row['contact_phone_number'],
-                              '{:.2%}'.format(home_visit_percent),
-                              row['awc_num_open'],
-                              '{:.2%}'.format(weighing_efficiency),
+                               row['district_name'],
+                               row['block_name'],
+                               row['supervisor_name'],
+                               row['awc_name'], row['aww_name'],
+                               row['contact_phone_number'],
+                               '{:.2%}'.format(home_visit_percent),
+                               num_open,
+                               '{:.2%}'.format(weighing_efficiency),
                                'Yes' if weighing_efficiency > 0.6 and home_visit_percent > 0.6 else 'No'])
 
         return [
             [
-                'Incentive Report',
+                'AWW Performance Report',
                 excel_rows
             ],
             [
