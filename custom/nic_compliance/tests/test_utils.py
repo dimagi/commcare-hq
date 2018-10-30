10195143fcd23acd3f0518e0be196a3ea17c0a20
mkangia <mkangia@dimagi.com>
2018-10-30 18:56:37 +0530

    differentiate test for web and mobile

--- custom/nic_compliance/tests/test_utils.py
+++ custom/nic_compliance/tests/test_utils.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import base64
+
 from django.test import TestCase, override_settings, Client
 from django.urls import reverse
 from dimagi.utils.couch.cache.cache_core import get_redis_client
 
 from mock import patch
 
-from custom.nic_compliance.utils import extract_password, verify_password, get_obfuscated_passwords, \
-    obfuscated_password_redis_key_for_user
+from custom.nic_compliance.utils import (
+    extract_password,
+    obfuscated_password_redis_key_for_user,
+)
 from corehq.apps.domain.models import Domain
 from corehq.apps.users.models import WebUser
 
@@ -43,7 +47,30 @@ class TestDecodePassword(TestCase):
         super(TestDecodePassword, cls).tearDownClass()
 
     @patch("custom.nic_compliance.utils.USERS_TO_TRACK_FOR_REPLAY_ATTACK", [username])
-    def test_login_attempt(self):
+    def test_mobile_heartbeat_attempt(self):
+        get_redis_client().clear()
+        redis_client = get_redis_client()
+        with override_settings(OBFUSCATE_PASSWORD_FOR_NIC_COMPLIANCE=True):
+            obfuscated_password = "sha256$1e2d5bc2hhMjU2JDFlMmQ1Yk1USXpORFUyZjc5MTI3PQ==f79127="
+            client = Client(enforce_csrf_checks=False)
+            auth_headers = {
+                'HTTP_AUTHORIZATION': 'Basic ' + base64.b64encode('%s:%s' % (
+                    self.username, obfuscated_password
+                )),
+            }
+            # ensure that login attempt gets stored
+            key_name = obfuscated_password_redis_key_for_user(self.username, obfuscated_password)
+            self.assertFalse(redis_client.get(key_name))
+            response = client.get(reverse('phone_heartbeat'), **auth_headers)
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(redis_client.get(key_name))
+
+            # test replay attack
+            response = client.get(reverse('phone_heartbeat'), **auth_headers)
+            self.assertEqual(response.status_code, 401)
+            redis_client.expire(key_name, 0)
+
+    def test_web_login_attempt(self):
         get_redis_client().clear()
         redis_client = get_redis_client()
         with override_settings(OBFUSCATE_PASSWORD_FOR_NIC_COMPLIANCE=True):
