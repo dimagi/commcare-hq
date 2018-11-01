a603f12826d6cc2a5c01c99ac88eda7aa8e1adb6
mkangia <mkangia@dimagi.com>
2018-11-01 16:28:19 +0530

    Add couch views for active reports [skip ci]

---
+++ corehq/apps/userreports/_design/views/active_data_sources/map.js
@@ -0,0 +1,9 @@
+function (doc) {
+    if(doc.doc_type === "DataSourceConfiguration" && doc.is_deactivated == false) {
+        emit([
+            doc.domain,
+            doc._id,
+            doc.table_id,
+        ], doc);
+    }
+}
