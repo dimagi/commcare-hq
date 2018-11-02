5a1a6158bbe076b29ef0100103dc075c8f34d5b8
Manish Kangia <mkangia@dimagi.com>
2018-11-03 01:43:22 +0530

    enable search by table_id

--- corehq/apps/userreports/_design/views/active_data_sources/map.js
+++ corehq/apps/userreports/_design/views/active_data_sources/map.js
@@ -2,8 +2,8 @@ function (doc) {
     if(doc.doc_type === "DataSourceConfiguration" && doc.is_deactivated == false) {
         emit([
             doc.domain,
-            doc._id,
             doc.table_id,
+            doc._id,
         ], null);
     }
 }
