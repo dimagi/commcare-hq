0541b3d6cf3db460944352729348a28e835ee9e2
Dawid Mydlo <dmydlo@soldevelo.com>
2018-11-02 10:43:46 +0100

    fix lint

--- custom/icds_reports/migrations/0073_ccsrecordmonthly_closed.py
+++ custom/icds_reports/migrations/0073_ccsrecordmonthly_closed.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 from __future__ import absolute_import
 
-from django.db import migrations, models
+from django.db import migrations
 from corehq.sql_db.operations import RawSQLMigration
 from custom.icds_reports.utils.migrations import get_view_migrations
 
