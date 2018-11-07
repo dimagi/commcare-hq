0e36c70aaccb3f50541b2682e50512298e69ff1c
Cal Ellowitz <calellowitz@gmail.com>
2018-11-07 11:11:24 -0500

    only count cases open in that month

--- custom/icds_reports/utils/aggregation_helpers/aww_incentive.py
+++ custom/icds_reports/utils/aggregation_helpers/aww_incentive.py
@@ -63,9 +63,9 @@ class AwwIncentiveAggregationHelper(BaseICDSAggregationHelper):
         UPDATE "{tablename}" perf
         SET expected_visits = expected_visits + ucr.expected
         FROM (
-             SELECT SUM(0.39) AS expected, awc_id
+             SELECT FLOOR(SUM(0.39)) AS expected, awc_id
              FROM "{ccs_record_case_ucr}"
-             WHERE %(month)s - add > 183 AND (closed_on IS NULL OR date_trunc('month', closed_on)::DATE > %(month)s)
+             WHERE %(month)s - add > 183 AND (closed_on IS NULL OR date_trunc('month', closed_on)::DATE > %(month)s) AND date_trunc('month', opened_on) <= %(month)s
              GROUP BY awc_id
              ) ucr
         WHERE ucr.awc_id = perf.awc_id
