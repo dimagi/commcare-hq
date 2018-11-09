ac123cf9bc5f6209b05616469fd4a0dcc6464197
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-11-09 14:39:06 -0500

    Create the unfinished archive stub class and sql mgiration

--- corehq/ex-submodules/couchforms/models.py
+++ corehq/ex-submodules/couchforms/models.py
@@ -517,3 +517,24 @@ class UnfinishedSubmissionStub(models.Model):
 
     class Meta(object):
         app_label = 'couchforms'
+
+
+class UnfinishedArchiveStub(models.Model):
+    xform_id = models.CharField(max_length=200)
+    timestamp = models.DateTimeField(db_index=True)
+    saved = models.BooleanField(default=False)
+    domain = models.CharField(max_length=256)
+    date_queued = models.DateTimeField(null=True, db_index=True)
+    attempts = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return six.text_type(
+            "UnfinishedArchiveStub("
+            "xform_id={s.xform_id},"
+            "timestamp={s.timestamp},"
+            "saved={s.saved},"
+            "domain={s.domain})"
+        ).format(s=self)
+
+    class Meta(object):
+        app_label = 'couchforms'
