f7c9b1f90341c7c20f0a0e68ca179917f179cb04
Sravan Reddy <sravfeyn@gmail.com>
2018-11-12 18:29:27 +0530

    test reindexing

--- testapps/test_pillowtop/tests/test_reindexer.py
+++ testapps/test_pillowtop/tests/test_reindexer.py
@@ -174,6 +174,12 @@ def test_checkpoint_creation(self, reindex_id, pillow_name):
     ('sql-form', 'kafka-xform-ucr-es'),
     ('ledger-v2', 'LedgerToElasticsearchPillow'),
     ('groups-to-user', 'UserPillow'),
+    ('case', 'kafka-case-ucr-es'),
+    ('form', 'kafka-xform-ucr-es'),
+    ('report-case', 'kafka-case-ucr-es'),
+    ('report-xform', 'kafka-xform-ucr-es'),
+    ('user', 'user-to-es-ucr-pillow'),
+    ('group', 'GroupESPillow'),
 ], CheckpointCreationTest)
 def test_no_checkpoint_creation(self, reindex_id, pillow_name):
     # these pillows should not touch checkpoints since they are run with other
