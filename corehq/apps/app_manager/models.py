70ebfe06bc882be32dbf56880720713abb1533bd
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-11-12 14:02:13 -0500

    Add the app to the function input

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -2592,7 +2592,7 @@ class ModuleBase(IndexedSchema, NavMenuItemMediaMixin, CommentMixin):
                 domain = self.get_app().domain
                 project = Domain.get_by_name(domain)
                 try:
-                    if not should_sync_hierarchical_fixture(project):
+                    if not should_sync_hierarchical_fixture(project, self.get_app()):
                         # discontinued feature on moving to flat fixture format
                         raise LocationXpathValidationError(
                             _('That format is no longer supported. To reference the location hierarchy you need to'
