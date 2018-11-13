fd770710d49c0e99f1db66bce8e8ad028483560c
Rohit Negi <rohit25.negi@gmail.com>
2018-11-13 13:07:31 +0530

    Satisfy lynter

--- custom/icds_reports/ucr/tests/test_beneficiary_form.py
+++ custom/icds_reports/ucr/tests/test_beneficiary_form.py
@@ -5,10 +5,12 @@ from __future__ import unicode_literals
 from mock import patch
 from custom.icds_reports.ucr.tests.test_base_form_ucr import BaseFormsTest
 
+
 class TestAWCMgtForms(BaseFormsTest):
     ucr_name = "static-icds-cas-static-ls_home_visit_forms_filled"
 
-    @patch('custom.icds_reports.ucr.expressions._get_user_location_id', lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
+    @patch('custom.icds_reports.ucr.expressions._get_user_location_id',
+           lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
     def test_awc_visit_form_with_location_entered(self):
         self._test_data_source_results(
             'beneficiary_form_with_type_of_visit',
@@ -19,8 +21,8 @@ class TestAWCMgtForms(BaseFormsTest):
               'home_visit_count': 1,
               'submitted_on': None}])
 
-
-    @patch('custom.icds_reports.ucr.expressions._get_user_location_id', lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
+    @patch('custom.icds_reports.ucr.expressions._get_user_location_id',
+           lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
     def test_awc_visit_form_without_location_entered(self):
         self._test_data_source_results(
             'beneficiary_form_without_type_of_visit',
--- custom/icds_reports/ucr/tests/test_ls_vhnd_form_ucr.py
+++ custom/icds_reports/ucr/tests/test_ls_vhnd_form_ucr.py
@@ -10,14 +10,14 @@ from custom.icds_reports.ucr.tests.test_base_form_ucr import BaseFormsTest
 class TestLsVhndForms(BaseFormsTest):
     ucr_name = "static-icds-cas-static-ls_vhnd_form"
 
-    @patch('custom.icds_reports.ucr.expressions._get_user_location_id', lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
+    @patch('custom.icds_reports.ucr.expressions._get_user_location_id',
+           lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
     def test_vhnd_form_with_vhnd_date(self):
         self._test_data_source_results(
-            'ls_vhnd_observation_form',
-          [
-            {'submitted_on': None,
-             'doc_id': None,
-             'location_id': 'qwe56poiuytr4xcvbnmkjfghwerffdaa',
-             'vhnd_date': datetime.date(2018, 8, 24),
-             'month': None}
-          ])
+            'ls_vhnd_observation_form', [{
+                'submitted_on': None,
+                'doc_id': None,
+                'location_id': 'qwe56poiuytr4xcvbnmkjfghwerffdaa',
+                'vhnd_date': datetime.date(2018, 8, 24),
+                'month': None}
+            ])
