ad1c7582c03fcf77736074dcefbe4ac65f12f4a8
mkangia <mkangia@dimagi.com>
2018-11-13 18:23:33 +0530

    remove domain from view as well

--- corehq/couchapps/export_instances_by_is_daily_saved/views/view/map.js
+++ corehq/couchapps/export_instances_by_is_daily_saved/views/view/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.is_daily_saved_export || doc.auto_rebuild_enabled) {
-        emit([doc.last_accessed, doc.domain], null);
+        emit([doc.last_accessed], null);
     }
 }
