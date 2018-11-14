4a5daaea6b0d61899690654b0750b23f6282d2ce
mkangia <mkangia@dimagi.com>
2018-11-14 23:57:32 +0530

    add test to filter by last accessed

--- corehq/apps/export/tests/test_dbaccessors.py
+++ corehq/apps/export/tests/test_dbaccessors.py
@@ -130,7 +130,8 @@ class TestExportInstanceDBAccessors(TestCase):
         cls.form_instance_daily_saved = FormExportInstance(
             domain='wrong-domain',
             is_daily_saved_export=True,
-            auto_rebuild_enabled=True
+            auto_rebuild_enabled=True,
+            last_accessed=datetime.utcnow()
         )
         cls.case_instance_deid = CaseExportInstance(
             domain=cls.domain,
@@ -145,7 +146,8 @@ class TestExportInstanceDBAccessors(TestCase):
         cls.case_instance_daily_saved = CaseExportInstance(
             domain='wrong-domain',
             is_daily_saved_export=True,
-            auto_rebuild_enabled=True
+            auto_rebuild_enabled=True,
+            last_accessed=(datetime.utcnow() - timedelta(days=4))
         )
 
         cls.instances = [
@@ -195,6 +197,12 @@ class TestExportInstanceDBAccessors(TestCase):
         self.assertEqual(
             set(instance_ids),
             {self.form_instance_daily_saved._id, self.case_instance_daily_saved._id}
+        )
+        recently_accessed_instance_ids = get_daily_saved_export_ids_for_auto_rebuild(
+            datetime.utcnow() - timedelta(days=2))
+        self.assertEqual(
+            set(recently_accessed_instance_ids),
+            {self.form_instance_daily_saved._id}
         )
 
     def test_get_properly_wrapped_export_instance(self):
