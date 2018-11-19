f4303a7fda150b22fcd01a6fd5a7d5ff80e52c75
Nick Pellegrino <npellegrino@dimagi.com>
2018-11-19 16:56:05 -0500

    use hqDefine

--- corehq/apps/hqadmin/static/hqadmin/js/webconnector.js
+++ corehq/apps/hqadmin/static/hqadmin/js/webconnector.js
@@ -1,61 +1,67 @@
-(function() {
-    // Create the connector object
-    var myConnector = tableau.makeConnector();
+hqDefine('hqadmin/js/webconnector', [
+], function (
+) {
 
-    // Define the schema
-    myConnector.getSchema = function(schemaCallback) {
-        var cols = [{
-            id: "id",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "name",
-            dataType: tableau.dataTypeEnum.string
-        }, {
-            id: "abc",
-            dataType: tableau.dataTypeEnum.string
-        }];
+    (function () {
+        // Create the connector object
+        var myConnector = tableau.makeConnector();
 
-        var tableSchema = {
-            id: "cases",
-            alias: "Case List",
-            columns: cols
+        // Define the schema
+        myConnector.getSchema = function (schemaCallback) {
+            var cols = [{
+                id: "id",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "name",
+                dataType: tableau.dataTypeEnum.string
+            }, {
+                id: "abc",
+                dataType: tableau.dataTypeEnum.string
+            }];
+
+            var tableSchema = {
+                id: "cases",
+                alias: "Case List",
+                columns: cols
+            };
+
+            schemaCallback([tableSchema]);
         };
 
-        schemaCallback([tableSchema]);
-    };
+        myConnector.init = function (initCallback) {
+            tableau.authType = tableau.authTypeEnum.none;
+            initCallback();
+        }
 
-    myConnector.init = function(initCallback) {
-        tableau.authType = tableau.authTypeEnum.none;
-        initCallback();
-    }
+        // Download the data
+        myConnector.getData = function (table, doneCallback) {
+            $.getJSON("http://localhost:8000/hq/admin/4.5_week.geojson", function (resp) {
+                var cases = resp.cases,
+                    tableData = [];
 
-    // Download the data
-    myConnector.getData = function(table, doneCallback) {
-        $.getJSON("http://localhost:8000/hq/admin/4.5_week.geojson", function(resp) {
-            var cases = resp.cases,
-                tableData = [];
+                // Iterate over the JSON object
+                for (var i = 0, len = cases.length; i < len; i++) {
+                    tableData.push({
+                        "id": cases[i].id,
+                        "name": cases[i].name,
+                        "abc": cases[i].properties['abc']
+                    });
+                }
 
-            // Iterate over the JSON object
-            for (var i = 0, len = cases.length; i < len; i++) {
-                tableData.push({
-                    "id": cases[i].id,
-                    "name": cases[i].name,
-                    "abc": cases[i].properties['abc']
-                });
-            }
+                table.appendRows(tableData);
+                doneCallback();
+            });
+        };
 
-            table.appendRows(tableData);
-            doneCallback();
+        tableau.registerConnector(myConnector);
+
+        // Create event listeners for when the user submits the form
+        $(document).ready(function () {
+            $("#submitButton").click(function () {
+                tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+                tableau.submit(); // This sends the connector object to Tableau
+            });
         });
-    };
+    })();
 
-    tableau.registerConnector(myConnector);
-
-    // Create event listeners for when the user submits the form
-    $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-        });
-    });
-})();
+}
