4a3a5301a989473cfafec5b677bef665357851c3
Nick Pellegrino <npellegrino@dimagi.com>
2018-11-19 11:18:21 -0500

    use html template

--- corehq/apps/hqadmin/views/data.py
+++ corehq/apps/hqadmin/views/data.py
@@ -186,38 +186,12 @@ class Webconnector(View):
     urlname = 'webconnector'
 
     def get(self, request):
-
-        js_url = reverse(WebconnectorJS.urlname)
+        context = {
+            'web_connector_js': reverse(WebconnectorJS.urlname),
+        }
 
         return HttpResponse(
-            '''
-            <html>
-            
-            <head>
-                <title>USGS Earthquake Feed</title>
-                <meta http-equiv="Cache-Control" content="no-store" />
-            
-                <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js" type="text/javascript"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
-            
-                <script src="https://connectors.tableau.com/libs/tableauwdc-2.3.latest.js" type="text/javascript"></script>
-                <script src="%s" type="text/javascript"></script>
-            </head>
-            
-            <body>
-                <div class="container container-table">
-                    <div class="row vertical-center-row">
-                        <div class="text-center col-md-4 col-md-offset-4">
-                            <button type="button" id="submitButton" class="btn btn-success" style="margin: 10px;">Get Earthquake Data!</button>
-                        </div>
-                    </div>
-                </div>
-            </body>
-            
-            </html>
-            '''
-            % js_url
+            render(request, 'hqadmin/webconnector.html', context)
         )
 
 
