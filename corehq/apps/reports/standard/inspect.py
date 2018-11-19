ba33d70f28b54e882c257b443e1e6f60b8e5a5c6
jmtroth0 <jroth@dimagi.com>
2018-11-19 17:19:36 -0500

    Revert "Added case data and form data pages to left hand nav"

--- corehq/apps/reports/standard/inspect.py
+++ corehq/apps/reports/standard/inspect.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
-from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_noop, get_language
 
@@ -143,24 +142,6 @@ class SubmitHistory(SubmitHistoryMixin, ProjectReport):
             return False
         else:
             return True
-
-    @classmethod
-    def get_subpages(cls):
-        def _form_name(request=None, **context):
-            if 'instance' in context:
-                try:
-                    return mark_safe(context['instance'].form_data['@name'])
-                except KeyError:
-                    pass
-            return _('View Form')
-
-        from corehq.apps.reports.views import FormDataView
-        return [
-            {
-                'title': _form_name,
-                'urlname': FormDataView.urlname,
-            },
-        ]
 
     @property
     def headers(self):
