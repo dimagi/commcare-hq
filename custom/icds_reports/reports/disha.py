38198add216fe0e10bfaaf92595aa390f957ad7d
Sravan Reddy <sravfeyn@gmail.com>
2018-11-23 16:49:21 +0530

    handle unicode/blobdb issue

--- custom/icds_reports/reports/disha.py
+++ custom/icds_reports/reports/disha.py
@@ -29,7 +29,7 @@ class DishaDump(object):
         self.month = month
 
     def _blob_id(self):
-        return 'disha_dump-{}-{}.json'.format(self.state_name.replace(" ", ""), self.month.strftime('%Y-%m-%d'))
+        return 'disha_dump-{}-{}.json'.format(self.state_name.encode('ascii', 'ignore').replace(" ", ""), self.month.strftime('%Y-%m-%d'))
 
     @memoized
     def _get_file_ref(self):
@@ -69,7 +69,7 @@ class DishaDump(object):
             # batch_qs requires ordered queryset
         ).order_by('pk').values_list(*self._get_columns())
 
-    def _write_data_in_chunks(self, temp_path):
+    def _write_data_in_chunks(self, file_obj):
         # Writes indicators in json format to the file at temp_path
         #   in chunks so as to avoid memory errors while doing json.dumps.
         #   The structure of the json is as below
@@ -81,32 +81,32 @@ class DishaDump(object):
         #   }
         columns = self._get_columns()
         indicators = self._get_rows()
-        metadata_line = '{{'\
+        metadata_line = u'{{'\
             '"month":"{month}", '\
             '"state_name": "{state_name}", '\
             '"column_names": {columns}, '\
             '"rows": ['.format(
                 month=self.month,
                 state_name=self.state_name,
-                columns=json.dumps(columns, ensure_ascii=False))
-        with open(temp_path, 'w', encoding='utf-8') as f:
-            f.write(metadata_line)
-            written_count = 0
-            num_batches = 10
-            for count, (_, end, total, chunk) in enumerate(batch_qs(indicators, num_batches=num_batches)):
-                chunk_string = json.dumps(list(chunk), ensure_ascii=False)
-                # chunk is list of lists, so skip enclosing brackets
-                f.write(chunk_string[1:-1])
-                written_count += len(chunk)
-                if written_count != total:
-                    f.write(",")
-                logger.info("Processed {count}/{total} batches".format(count=count, total=num_batches))
-            f.write("]}")
+                columns=json.dumps(columns, ensure_ascii=False)).encode('utf8')
+        file_obj.write(metadata_line)
+        written_count = 0
+        num_batches = 10
+        for count, (_, end, total, chunk) in enumerate(batch_qs(indicators, num_batches=num_batches)):
+            chunk_string = json.dumps(list(chunk), ensure_ascii=False).encode('utf8')
+            # chunk is list of lists, so skip enclosing brackets
+            file_obj.write(chunk_string[1:-1])
+            written_count += len(chunk)
+            if written_count != total:
+                file_obj.write(",")
+            logger.info("Processed {count}/{total} batches".format(count=count, total=num_batches))
+        file_obj.write("]}")
 
     def build_export_json(self):
         with TransientTempfile() as temp_path:
-            self._write_data_in_chunks(temp_path)
-            with open(temp_path, 'r', encoding='utf-8') as f:
+            with open(temp_path, 'w+b') as f:
+                self._write_data_in_chunks(f)
+                f.seek(0)
                 blob_ref, _ = IcdsFile.objects.get_or_create(blob_id=self._blob_id(), data_type='disha_dumps')
                 blob_ref.store_file_in_blobdb(f, expired=1)
                 blob_ref.save()
