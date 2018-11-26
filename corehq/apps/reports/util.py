bbb7c007bea1d331bd932d4774d7959a15367d65
Sravan Reddy <sravfeyn@gmail.com>
2018-11-26 11:28:38 +0530

    don't chunk for small querysets

--- corehq/apps/reports/util.py
+++ corehq/apps/reports/util.py
@@ -411,8 +411,8 @@ def friendly_timedelta(td):
     return ", ".join(text)
 
 
-# Copied from http://djangosnippets.org/snippets/1170/
-def batch_qs(qs, num_batches=10):
+# Copied/extended from http://djangosnippets.org/snippets/1170/
+def batch_qs(qs, num_batches=10, min_batch_size=100000):
     """
     Returns a (start, end, total, queryset) tuple for each batch in the given
     queryset.
@@ -426,7 +426,10 @@ def batch_qs(qs, num_batches=10):
                 print article.body
     """
     total = qs.count()
-    batch_size = int(total / num_batches) or total
+    if total < min_batch_size:
+        batch_size = total
+    else:
+        batch_size = int(total / num_batches) or total
     for start in range(0, total, batch_size):
         end = min(start + batch_size, total)
         yield (start, end, total, qs[start:end])
