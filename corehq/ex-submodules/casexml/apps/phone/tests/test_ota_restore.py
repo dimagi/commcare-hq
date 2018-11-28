1f0482e6bfe3e2d30e536cf74b7d9c4147f31439
Nick Pellegrino <npellegrino@dimagi.com>
2018-11-28 17:23:19 -0500

    fix errors in casexml.apps.phone.tests.test_ota_restore

--- corehq/ex-submodules/casexml/apps/phone/tests/test_ota_restore.py
+++ corehq/ex-submodules/casexml/apps/phone/tests/test_ota_restore.py
@@ -157,7 +157,7 @@ class OtaRestoreTest(BaseOtaRestoreTest):
         self.assertNotIsInstance(restore_config_other_device.get_payload(), CachedResponse)
 
     def testUserRestoreWithCase(self):
-        xml_data = self.get_xml('create_short')
+        xml_data = self.get_xml('create_short').decode('utf-8')
         xml_data = xml_data.format(user_id=self.restore_user.user_id)
 
         # implicit length assertion
@@ -234,7 +234,7 @@ class OtaRestoreTest(BaseOtaRestoreTest):
         def get_all_syncslogs():
             return [properly_wrap_sync_log(log.doc) for log in SyncLogSQL.objects.all()]
 
-        xml_data = self.get_xml('create_short')
+        xml_data = self.get_xml('create_short').decode('utf-8')
         xml_data = xml_data.format(user_id=self.restore_user.user_id)
         submit_form_locally(xml_data, domain=self.project.name)
 
@@ -299,7 +299,7 @@ class OtaRestoreTest(BaseOtaRestoreTest):
                                sync_restore_payload)
 
     def testRestoreAttributes(self):
-        xml_data = self.get_xml('attributes')
+        xml_data = self.get_xml('attributes').decode('utf-8')
         xml_data = xml_data.format(user_id=self.restore_user.user_id)
         newcase = submit_form_locally(xml_data, domain=self.project.name).case
 
