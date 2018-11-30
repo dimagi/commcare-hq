066317661017d9ce4395615cab201010e3611d0c
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2018-11-30 18:33:20 -0500

    Implement HQUserType.HqUser_friendly_name_helper

--- corehq/apps/reports/util.py
+++ corehq/apps/reports/util.py
@@ -158,7 +158,7 @@ def get_username_from_forms(domain, user_id):
         if possible_username:
             return possible_username
     else:
-        return HQUserType.human_readable[HQUserType.ADMIN]
+        return HQUserType.HqUser_friendly_name_helper(HQUserType.ADMIN)
 
 
 def namedtupledict(name, fields):
