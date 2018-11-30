9ace38686247ee9e965f994285c4f0693ee2b140
mkangia <mkangia@dimagi.com>
2018-12-01 00:57:21 +0530

    get role if needed

--- corehq/apps/users/permissions.py
+++ corehq/apps/users/permissions.py
@@ -73,10 +73,10 @@ def has_permission_to_view_report(couch_user, domain, report_to_check):
 def can_manage_releases(couch_user, domain, app_id):
     from corehq.apps.users.decorators import get_permission_name
     from corehq.apps.users.models import Permissions
-    role = couch_user.get_role(domain)
     restricted_app_release = toggles.RESTRICT_APP_RELEASE.enabled(domain)
     if not restricted_app_release:
         return True
+    role = couch_user.get_role(domain)
     return (
         couch_user.has_permission(
             domain, get_permission_name(Permissions.manage_releases),
