e3efdbd258d05f6b7fc9312339db1fed2a502aa1
Nick Pellegrino <npellegrino@dimagi.com>
2018-12-03 11:04:13 -0500

    update test_form_xmlns

--- corehq/apps/app_manager/tests/test_form_xmlns.py
+++ corehq/apps/app_manager/tests/test_form_xmlns.py
@@ -33,26 +33,26 @@ class FormXmlnsTest(SimpleTestCase, TestXmlMixin):
             if xmlns != default_xmlns:
                 source = source.replace(default_xmlns, xmlns)
             assert xmlns in source, source
-        return source.encode('utf-8')
+        return source
 
     def test_save_xform_changes_empty_xmlns(self):
         source = self.get_source(xmlns=None)
-        self.assertNotIn(self.form.xmlns, source.decode('utf-8'))
-        save_xform(self.app, self.form, source)
-        self.assertIn(self.form.xmlns, self.form.source.decode('utf-8'))
+        self.assertNotIn(self.form.xmlns, source)
+        save_xform(self.app, self.form, source.encode('utf-8'))
+        self.assertIn(self.form.xmlns, self.form.source)
         self.assertNotEqual(self.form.xmlns, GENERIC_XMLNS)
 
     def test_save_xform_changes_generic_xmlns(self):
         source = self.get_source(xmlns=GENERIC_XMLNS)
-        self.assertNotIn(self.form.xmlns, source.decode('utf-8'))
-        save_xform(self.app, self.form, source)
-        self.assertIn(self.form.xmlns, self.form.source.decode('utf-8'))
+        self.assertNotIn(self.form.xmlns, source)
+        save_xform(self.app, self.form, source.encode('utf-8'))
+        self.assertIn(self.form.xmlns, self.form.source)
         self.assertNotEqual(self.form.xmlns, GENERIC_XMLNS)
 
     def test_save_xform_does_not_change_xmlns_if_already_unique(self):
         source = self.get_source()
         form = self.form
-        save_xform(self.app, form, source)
+        save_xform(self.app, form, source.encode('utf-8'))
         self.assertEqual(form.source, source)
         self.assertEqual(form.xmlns, DEFAULT_XMLNS)
         # clear cach because form.xmlns changed in save_xform
@@ -61,30 +61,30 @@ class FormXmlnsTest(SimpleTestCase, TestXmlMixin):
 
     def test_save_xform_with_conflicting_xmlns_sets_new_xmlns(self):
         source = self.get_source()
-        save_xform(self.app, self.form, source)
+        save_xform(self.app, self.form, source.encode('utf-8'))
         self.assertEqual(self.form.source, source)
 
         # clear cach because form.xmlns changed in save_xform
         Application.get_xmlns_map.get_cache(self.app).clear()
         form2 = self.app.new_form(self.module.id, "Form 2", None)
         form2_xmlns = form2.xmlns
-        self.assertNotIn(form2_xmlns, source.decode('utf-8'))
+        self.assertNotIn(form2_xmlns, source)
         save_xform(self.app, form2, source)
-        self.assertIn(form2_xmlns, form2.source.decode('utf-8'))
-        self.assertNotIn(DEFAULT_XMLNS, form2.source.decode('utf-8'))
+        self.assertIn(form2_xmlns, form2.source)
+        self.assertNotIn(DEFAULT_XMLNS, form2.source)
 
     def test_save_xform_with_shadow_form_does_not_change_xmlns(self):
         source = self.get_source()
         module = self.app.add_module(AdvancedModule.new_module('a module', None))
         form = self.app.new_form(self.module.id, "a form", None)
-        save_xform(self.app, form, source)
+        save_xform(self.app, form, source.encode('utf-8'))
         Application.get_xmlns_map.get_cache(self.app).clear()
 
         shadow = module.new_shadow_form("shadow form", "en")
         shadow.shadow_parent_form_id = form.unique_id
         self.assertEqual(shadow.shadow_parent_form, form)
 
-        save_xform(self.app, form, source)
+        save_xform(self.app, form, source.encode('utf-8'))
         self.assertEqual(form.source, source)
         self.assertEqual(form.xmlns, DEFAULT_XMLNS)
 
@@ -95,7 +95,7 @@ class FormXmlnsTest(SimpleTestCase, TestXmlMixin):
         new_form = self.app.copy_form(self.module, old_form, self.module)
 
         self.assertEqual(new_form.xmlns, XMLNS_PREFIX + new_form.unique_id)
-        self.assertIn(new_form.xmlns, new_form.source.decode('utf-8'))
+        self.assertIn(new_form.xmlns, new_form.source)
         self.assertNotEqual(new_form.xmlns, old_form.xmlns)
-        self.assertNotIn(old_form.xmlns, new_form.source.decode('utf-8'))
-        self.assertNotIn(new_form.xmlns, old_form.source.decode('utf-8'))
+        self.assertNotIn(old_form.xmlns, new_form.source)
+        self.assertNotIn(new_form.xmlns, old_form.source)
