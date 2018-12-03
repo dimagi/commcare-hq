782e6daea1aa4d145c06b71f7cb50bf361b86e93
Dawid Mydlo <dmydlo@soldevelo.com>
2018-12-03 14:51:09 +0100

    Fix migrations

--- custom/icds_reports/migrations/0080_merge_20181130_1818.py
+++ custom/icds_reports/migrations/0080_merge_20181130_1818.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 
 from django.db import migrations
+from custom.icds_reports.utils.migrations import get_view_migrations
 
 
 class Migration(migrations.Migration):
@@ -15,3 +16,4 @@ class Migration(migrations.Migration):
 
     operations = [
     ]
+    operations.extend(get_view_migrations())
