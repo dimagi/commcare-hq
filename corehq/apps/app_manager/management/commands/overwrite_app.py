fca79b1ddd2b6bb043c708270e99b065a798cca2
Simon Kelly <skelly@dimagi.com>
2018-12-05 14:15:54 +0200

    create new app

--- corehq/apps/app_manager/management/commands/overwrite_app.py
+++ corehq/apps/app_manager/management/commands/overwrite_app.py
@@ -6,6 +6,7 @@ from django.core.management.base import BaseCommand
 
 from corehq.apps.app_manager.dbaccessors import get_current_app, get_build_doc_by_version, \
     get_latest_released_app_doc, wrap_app
+from corehq.apps.app_manager.models import Application
 from corehq.apps.app_manager.views.utils import overwrite_app
 from corehq.apps.userreports.util import get_static_report_mapping
 
@@ -15,7 +16,7 @@ class Command(BaseCommand):
     Overwrites target application with other application
     """
 
-    args = "<from_domain> <from_app_id> <to_domain> <to_app_id>"
+    args = "<from_domain> <from_app_id> <to_domain>"
 
     _report_map = None
 
@@ -23,18 +24,27 @@ class Command(BaseCommand):
         parser.add_argument('from_domain')
         parser.add_argument('from_app_id')
         parser.add_argument('to_domain')
-        parser.add_argument('to_app_id')
+        parser.add_argument(
+            '--to-app-id',
+            help="Overwrite this app. A new app will be created if not specified."
+        )
         parser.add_argument(
             '--version',
             help="Specify version of 'from app' to use. If not specified the latest released version will be used."
         )
 
-    def handle(self, from_domain, from_app_id, to_domain, to_app_id, *args, **options):
+    def handle(self, from_domain, from_app_id, to_domain, *args, **options):
         self.from_domain = from_domain
         self.to_domain = to_domain
-        app = get_current_app(self.to_domain, to_app_id)
-        if options.get('version'):
-            from_app_doc = get_build_doc_by_version(self.from_domain, from_app_id, options.get('version'))
+        to_app_id = options.get('to-app-id')
+        version = options.get('version')
+        if to_app_id:
+            app = get_current_app(self.to_domain, to_app_id)
+        else:
+            app = Application()
+
+        if version:
+            from_app_doc = get_build_doc_by_version(self.from_domain, from_app_id, version)
         else:
             from_app_doc = get_latest_released_app_doc(self.from_domain, from_app_id)
 
