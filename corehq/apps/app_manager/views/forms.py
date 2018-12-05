90cc9eb189bac2e08dd8814288a36cc65b950701
Nick Pellegrino <npellegrino@dimagi.com>
2018-12-05 09:03:23 -0500

    encode xform

--- corehq/apps/app_manager/views/forms.py
+++ corehq/apps/app_manager/views/forms.py
@@ -287,6 +287,8 @@ def _edit_form_attr(request, domain, app_id, form_unique_id, attr):
                 except Exception:
                     pass
             if xform:
+                if isinstance(xform, six.text_type):
+                    xform = xform.encode('utf-8')
                 save_xform(app, form, xform)
             else:
                 raise Exception("You didn't select a form to upload")
