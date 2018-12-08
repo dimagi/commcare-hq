330e47fa4988b4e8fd7379e53bf12fbae7f461bd
mkangia <mkangia@dimagi.com>
2018-12-08 19:00:38 +0530

    better form spacing and remove redundant comment
    resstricting to project target languages is anyway not useful since it restricts user to not be able to create new languages directly from HQ. Transifex adds any new languages pushed by HQ.

--- corehq/apps/translations/forms.py
+++ corehq/apps/translations/forms.py
@@ -105,8 +105,6 @@ class AppTranslationsForm(forms.Form):
     )
     transifex_project_slug = forms.ChoiceField(label=ugettext_lazy("Trasifex project"), choices=(),
                                                required=True)
-    # Unfortunately transifex api does not provide a way to pull all possible target languages and
-    # allow us to just add a checkbox instead of selecting a single/multiple target languages at once
     target_lang = forms.ChoiceField(label=ugettext_lazy("Translated Language"),
                                     choices=([(None, ugettext_lazy('Select Translated Language'))] +
                                              langcodes.get_all_langs_for_select()),
@@ -124,8 +122,8 @@ class AppTranslationsForm(forms.Form):
         self.domain = domain
         self.helper = FormHelper()
         self.helper.form_tag = False
-        self.helper.label_class = 'col-sm-3 col-md-4 col-lg-2'
-        self.helper.field_class = 'col-sm-4 col-md-5 col-lg-3'
+        self.helper.label_class = 'col-sm-4 col-md-4 col-lg-3'
+        self.helper.field_class = 'col-sm-6 col-md-6 col-lg-5'
 
         self.fields['app_id'].choices = tuple((app.id, app.name) for app in get_brief_apps_in_domain(domain))
         if settings.TRANSIFEX_DETAILS:
