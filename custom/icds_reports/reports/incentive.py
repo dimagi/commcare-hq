e4f9eb2474bbe2e6f9c25335861eb17690331a26
Dawid Mydlo <dmydlo@soldevelo.com>
2018-12-12 17:45:47 +0100

    AWW performance report excel format

--- custom/icds_reports/reports/incentive.py
+++ custom/icds_reports/reports/incentive.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals, absolute_import, division
 
 
 from custom.icds_reports.models.aggregate import AWWIncentiveReport
-from custom.icds_reports.utils import india_now
+
 
 class IncentiveReport(object):
 
@@ -15,9 +15,16 @@ class IncentiveReport(object):
         def _format_infrastructure_data(data):
             return data if data else 'Data not entered'
 
-        data = AWWIncentiveReport.objects.filter(month=self.month, block_id=self.block).values('state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name', 'aww_name', 'contact_phone_number', 'wer_weighed', 'wer_eligible', 'awc_num_open', 'valid_visits', 'expected_visits')
+        data = AWWIncentiveReport.objects.filter(
+            month=self.month, block_id=self.block
+        ).order_by(
+            '-supervisor_name'
+        ).values(
+            'supervisor_name', 'awc_name', 'aww_name', 'contact_phone_number', 'wer_weighed', 'wer_eligible',
+            'awc_num_open', 'valid_visits', 'expected_visits'
+        )
 
-        headers = ['State', 'District', 'Block', 'Supervisor', 'AWC', 'AWW Name', 'AWW Contact Number', 'Home Visits Conducted', 'Number of Days AWC was Open', 'Weighing Efficiency', 'Eligible for Incentive']
+        headers = ['District', 'Block', 'Supervisor', 'AWC', 'AWW Name', 'AWW Contact Number', 'Home Visits Conducted', 'Number of Days AWC was Open', 'Weighing Efficiency', 'Eligible for Incentive']
 
         excel_rows = []
         excel_rows.append(headers)
@@ -31,10 +38,7 @@ class IncentiveReport(object):
             else:
                 num_open = row['awc_num_open']
 
-            excel_rows.append([row['state_name'],
-                               row['district_name'],
-                               row['block_name'],
-                               row['supervisor_name'],
+            excel_rows.append([row['supervisor_name'],
                                row['awc_name'],
                                _format_infrastructure_data(row['aww_name']),
                                _format_infrastructure_data(row['contact_phone_number']),
@@ -43,20 +47,4 @@ class IncentiveReport(object):
                                '{:.2%}'.format(weighing_efficiency),
                                'Yes' if weighing_efficiency >= 0.6 and home_visit_percent >= 0.6 else 'No'])
 
-        return [
-            [
-                'AWW Performance Report',
-                excel_rows
-            ],
-            [
-                'Export Info',
-                [
-                    ['Generated at', india_now()],
-                    ['Grouped By', 'AWC'],
-                    ['Month', self.month.month],
-                    ['Year', self.month.year],
-                    ['Disclaimer', "The information in the report is based on the self-reported data entered by the Anganwadi Worker in ICDS-CAS mobile application and is subject to timely data syncs."]
-
-                ]
-            ]
-        ]
+        return excel_rows
