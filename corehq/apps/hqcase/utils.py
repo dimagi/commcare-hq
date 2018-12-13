8438134e559b2d43e8753e8191ea8becfee37e29
Nick Pellegrino <npellegrino@dimagi.com>
2018-12-12 20:35:26 -0500

    fix OptTestCase

--- corehq/apps/hqcase/utils.py
+++ corehq/apps/hqcase/utils.py
@@ -201,7 +201,7 @@ def submit_case_block_from_template(domain, template, context, xmlns=None,
     case_block = render_to_string(template, context)
     # Ensure the XML is formatted properly
     # An exception is raised if not
-    case_block = ElementTree.tostring(ElementTree.XML(case_block))
+    case_block = ElementTree.tostring(ElementTree.XML(case_block)).decode('utf-8')
 
     return submit_case_blocks(
         case_block,
