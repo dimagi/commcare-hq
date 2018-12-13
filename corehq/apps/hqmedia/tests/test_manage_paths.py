2b09aa186c70c15d1f75c85a647fa81857f8e9ad
Jennifer Schweers <orange.jenny@gmail.com>
2018-12-13 15:05:19 -0500

    Removed unused imports

--- corehq/apps/hqmedia/tests/test_manage_paths.py
+++ corehq/apps/hqmedia/tests/test_manage_paths.py
@@ -1,10 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
-import json
 from django.test import SimpleTestCase
-import os
-
-from mock import patch
 
 from corehq.apps.app_manager.tests.app_factory import AppFactory
 from corehq.apps.app_manager.tests.util import TestXmlMixin
