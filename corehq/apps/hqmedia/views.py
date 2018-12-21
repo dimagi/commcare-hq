4c73ce7d8f3d10cf5f739d84e982c23656407092
Jennifer Schweers <orange.jenny@gmail.com>
2018-12-21 16:52:51 -0500

    Extracted update_multimedia_paths into view_helpers

--- corehq/apps/hqmedia/views.py
+++ corehq/apps/hqmedia/views.py
@@ -18,7 +18,6 @@ from django.views.decorators.http import require_GET, require_POST
 from django.views.generic import View, TemplateView
 
 from couchdbkit.exceptions import ResourceNotFound, ResourceConflict
-from lxml import etree
 
 from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseBadRequest
 
@@ -273,7 +272,7 @@ def update_multimedia_paths(request, domain, app_id):
         })
 
     app = get_app(domain, app_id)
-    from corehq.apps.hqmedia.view_helpers import validate_multimedia_paths_rows
+    from corehq.apps.hqmedia.view_helpers import validate_multimedia_paths_rows, update_multimedia_paths
     with get_spreadsheet(f) as spreadsheet:
         rows = list(spreadsheet.iter_rows())
         (errors, warnings) = validate_multimedia_paths_rows(app, rows)
@@ -283,71 +282,9 @@ def update_multimedia_paths(request, domain, app_id):
                 'errors': errors,
             })
 
-        # Master list of old_path => new_path
-        paths = {row[0]: interpolate_media_path(row[1]) for row in rows}
-
-        # Update module and form references
-        success_counts = defaultdict(lambda: 0)
-        xforms_by_id = defaultdict(lambda: None)    # cache forms rather than parsing repeatedly
-        dirty_xform_ids = set()
-        for old_path, new_path in six.iteritems(paths):
-            for module in app.modules:
-                success_counts[module.unique_id] += module.rename_media(old_path, new_path)
-                if module.case_list_form.form_id:
-                    success_counts[module.unique_id] += module.case_list_form.rename_media(old_path, new_path)
-                if hasattr(module, 'case_list') and module.case_list.show:
-                    success_counts[module.unique_id] += module.case_list.rename_media(old_path, new_path)
-                for name, details, display in module.get_details():
-                    # Case list lookup
-                    if display and details.display == 'short' and details.lookup_enabled and details.lookup_image:
-                        if details.lookup_image == old_path:
-                            details.lookup_image = new_path
-                            success_counts[module.unique_id] += 1
-
-                    # Icons in case details
-                    for column in details.get_columns():
-                        if column.format == 'enum-image':
-                            for map_item in column.enum:
-                                for lang, icon in six.iteritems(map_item.value):
-                                    if icon == old_path:
-                                        map_item.value[lang] = new_path
-                                        success_counts[module.unique_id] += 1
-                for form in module.forms:
-                    success_counts[form.unique_id] += form.rename_media(old_path, new_path)
-                    if not xforms_by_id[form.unique_id]:
-                        xforms_by_id[form.unique_id] = form.wrapped_xform()
-                    update_count = xforms_by_id[form.unique_id].rename_media(old_path, new_path)
-                    success_counts[form.unique_id] += update_count
-                    if update_count:
-                        dirty_xform_ids.add(form.unique_id)
-
-        # Update any form xml that changed
-        for module in app.modules:
-            for form in module.forms:
-                if form.unique_id in dirty_xform_ids:
-                    form.source = etree.tostring(xforms_by_id[form.unique_id].xml).decode('utf-8')
-
-        # Update app's master map of multimedia
-        for old_path, new_path in six.iteritems(paths):
-            app.multimedia_map.update({
-                new_path: app.multimedia_map[old_path],
-            })
-
-        # Put together success messages
-        successes = []
-        for module in app.modules:
-            if success_counts[module.unique_id]:
-                successes.append(_("{} item(s) updated in <a href='{}' target='_blank'>{}</a>").format(
-                                 success_counts[module.unique_id],
-                                 reverse("view_module", args=[domain, app.id, module.unique_id]),
-                                 module.default_name()))
-            for form in module.forms:
-                if success_counts[form.unique_id]:
-                    successes.append(_("{} item(s) updated in <a href='{}' target='_blank'>{}</a>").format(
-                                     success_counts[form.unique_id],
-                                     reverse("view_form", args=[domain, app.id, form.unique_id]),
-                                     "{} > {}".format(module.default_name(), form.default_name())))
-
+        update_multimedia_paths(app, {
+            row[0]: interpolate_media_path(row[1]) for row in rows
+        })
         app.save()
 
         # Force all_media to reset
