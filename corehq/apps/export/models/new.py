add3426db53da8629bd91be86ee5ceb2bfed0814
Nick Pellegrino <npellegrino@dimagi.com>
2018-12-24 16:55:31 -0500

    get right hyperlink indices regardless of other column types

--- corehq/apps/export/models/new.py
+++ corehq/apps/export/models/new.py
@@ -550,10 +550,7 @@ class TableConfiguration(DocumentSchema):
         for selected_column in self.selected_columns:
             if selected_column.item.transform in [CASE_ID_TO_LINK, FORM_ID_TO_LINK]:
                 hyperlink_column_indices.append(export_column_index)
-            if isinstance(selected_column, SplitExportColumn) and split_columns:
-                export_column_index += len(selected_column.item.options)
-            else:
-                export_column_index += 1
+            export_column_index += len(selected_column.get_headers(split_column=split_columns))
         return hyperlink_column_indices
 
     def _get_sub_documents(self, document, row_number, document_id=None):
