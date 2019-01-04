5b47311caca04192bedbc22330608d5be6f2dbaf
Daniel Miller <dmiller@dimagi.com>
2019-01-04 11:48:00 -0500

    Drop blobs_blobmeta view

--- corehq/blobs/sql_templates/drop_blobmeta_view.sql
+++ corehq/blobs/sql_templates/drop_blobmeta_view.sql
@@ -1,3 +1,4 @@
-DROP VIEW blobs_blobmeta;
+DROP TRIGGER blobs_blobmeta_trigger ON blobs_blobmeta;
 DROP FUNCTION mutate_blobs_blobmeta();
+DROP VIEW blobs_blobmeta;
 ALTER TABLE blobs_blobmeta_tbl RENAME TO blobs_blobmeta;
