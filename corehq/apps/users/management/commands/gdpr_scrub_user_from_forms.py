f21180292db82abfc69272c5c1b9e50c68645eca
Nick Pellegrino <npellegrino@dimagi.com>
2019-01-15 18:35:50 -0500

    fix gdpr form scrub tests in python 3

--- corehq/apps/users/management/commands/gdpr_scrub_user_from_forms.py
+++ corehq/apps/users/management/commands/gdpr_scrub_user_from_forms.py
@@ -46,8 +46,8 @@ class Command(BaseCommand):
 
     @staticmethod
     def update_form_data(form_data, new_username):
-        form_attachment_xml = form_data.get_attachment("form.xml")
-        xml_elem = etree.parse(StringIO(six.text_type(form_attachment_xml)))
+        form_attachment_xml = form_data.get_attachment("form.xml").decode('utf-8')
+        xml_elem = etree.parse(StringIO(form_attachment_xml))
         id_elem = xml_elem.find("{http://openrosa.org/jr/xforms}meta").find(
             "{http://openrosa.org/jr/xforms}username")
         id_elem.text = new_username
