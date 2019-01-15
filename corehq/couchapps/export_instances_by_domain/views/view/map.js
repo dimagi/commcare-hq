1c0139b770d0677b1b27fee353c1262058957708
Jenny Schweers <orange.jenny@gmail.com>
2019-01-15 15:04:08 -0500

    Added name to export_instances_by_domain view

--- corehq/couchapps/export_instances_by_domain/views/view/map.js
+++ corehq/couchapps/export_instances_by_domain/views/view/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.doc_type === "FormExportInstance" || doc.doc_type === "CaseExportInstance") {
-        emit([doc.domain, doc.doc_type, doc.is_deidentified], null);
+        emit([doc.domain, doc.doc_type, doc.is_deidentified, doc.name], null);
     }
 }
