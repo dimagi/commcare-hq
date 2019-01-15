ad5abd885026dbbb6bd0e992c53cbca70f6edf31
Jenny Schweers <orange.jenny@gmail.com>
2019-01-15 15:49:42 -0500

    Added is_daily_saved_export and export_format to key

--- corehq/couchapps/export_instances_by_domain/views/view/map.js
+++ corehq/couchapps/export_instances_by_domain/views/view/map.js
@@ -1,5 +1,5 @@
 function(doc) {
     if (doc.doc_type === "FormExportInstance" || doc.doc_type === "CaseExportInstance") {
-        emit([doc.domain, doc.doc_type, doc.is_deidentified, doc.name], null);
+        emit([doc.domain, doc.doc_type, doc.is_deidentified, doc.is_daily_saved_export, doc.export_format, doc.name], null);
     }
 }
