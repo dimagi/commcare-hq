9c00306381c3da340f91c4b1959ffeb13530efb8
Rohit Negi <rohit25.negi@gmail.com>
2019-01-23 05:26:33 +0530

    Run query for selected months

--- custom/icds_reports/management/commands/rebuild_ccs_record_ucr.py
+++ custom/icds_reports/management/commands/rebuild_ccs_record_ucr.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function
 from __future__ import unicode_literals
 
+import datetime
 import time
 
 from django.core.management.base import BaseCommand
@@ -17,15 +18,27 @@ class Command(BaseCommand):
         indicator_config_id = 'static-ccs_record_cases'
         case_type_of_xmlns = 'ccs_record'
         config = _get_config_by_id(indicator_config_id)
+
         document_store = get_document_store_for_doc_type(
             config.domain, config.referenced_doc_type, case_type_or_xmlns=case_type_of_xmlns
         )
 
-        doc_ids = CcsRecordMonthly.objects.filter(pnc_complete=1).values('case_id')
+        current_month_start = datetime.date.today().replace(day=1)
+        last_month_start = (current_month_start - datetime.timedelta(days=1)).replace(day=1)
+
+        current_month_start = current_month_start.strftime('%Y-%m-%d')
+        last_month_start = last_month_start.strftime('%Y-%m-%d')
+        current_month_doc_ids = CcsRecordMonthly.objects.filter(pnc_complete=1,
+                                                                month=current_month_start).values('case_id')
+        docs_last_month = CcsRecordMonthly.objects.filter(pnc_complete=1,
+                                                          month=last_month_start).values('case_id')
+
+        doc_ids = current_month_doc_ids + docs_last_month
+        doc_ids = {doc_id['case_id'] for doc_id in doc_ids}
         relevant_ids = list()
         next_event = time.time() + 10
         for doc_id in doc_ids:
-            relevant_ids.append(doc_id['case_id'])
+            relevant_ids.append(doc_id)
             if len(relevant_ids) >= ID_CHUNK_SIZE:
                 _build_indicators(config, document_store, relevant_ids)
                 relevant_ids = []
