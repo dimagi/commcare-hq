10f890c4e427fdf06c915e2c78e9bf729bcc02ea
Nick Pellegrino <npellegrino@dimagi.com>
2019-01-24 15:32:29 -0500

    remove date_closed

--- corehq/apps/api/odata/serializers.py
+++ corehq/apps/api/odata/serializers.py
@@ -51,4 +51,7 @@ class ODataCommCareCaseSerializer(Serializer):
         data.pop('meta')
         data.pop('resource_name')
 
+        for value in data['values']:
+            value.pop('date_closed')
+
         return json.dumps(data, cls=DjangoJSONEncoder, sort_keys=True)
