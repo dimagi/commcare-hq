60efcd6386eee69b0e2d036fc41381198c118eb1
Nick Pellegrino <npellegrino@dimagi.com>
2019-01-25 15:14:12 -0500

    undo bug

--- corehq/apps/api/odata/views.py
+++ corehq/apps/api/odata/views.py
@@ -36,7 +36,7 @@ class ODataMetadataView(View):
     @method_decorator(toggles.ODATA.required_decorator())
     def get(self, request, domain):
         # todo: should generate this dynamically based on the domain / case schema / data dictionary
-        data_file = os.path.join(os.path.dirname(__file__), 'templates/odata/metadata.xml')
+        data_file = os.path.join(os.path.dirname(__file__), 'metadata.xml')
         case_types = get_case_types_for_domain_es(domain)
         with open(data_file, 'r') as f:
             metadata_template = Template(f.read())
