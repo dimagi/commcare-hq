8e2eff284fbb7a74c0991713db52c94cb311b6cd
Łukasz Wyszomirski <lwyszomirski@soldevelo.com>
2019-01-30 11:02:36 +0100

    change place of title add translations support

--- custom/reach/const.py
+++ custom/reach/const.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
+from django.utils.translation import ugettext_lazy as _
+
 INDICATOR_LIST = {
-    'registered_eligible_couples': 'Registered Eligible Couples',
-    'registered_pregnancies': 'Registered Pregnancies',
-    'registered_children': 'Registered Children',
-    'couples_family_planning': 'Couples using Family Planning Method',
-    'high_risk_pregnancies': 'High Risk Pregnancies',
-    'institutional_deliveries': 'Institutional Deliveries',
+    'registered_eligible_couples': _('Registered Eligible Couples'),
+    'registered_pregnancies': _('Registered Pregnancies'),
+    'registered_children': _('Registered Children'),
+    'couples_family_planning': _('Couples using Family Planning Method'),
+    'high_risk_pregnancies': _('High Risk Pregnancies'),
+    'institutional_deliveries': _('Institutional Deliveries'),
 }
 
 NUMERIC = 'numeric'
