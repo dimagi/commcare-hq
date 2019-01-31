e59d81d4e566274242bd18c7ddd63002864aa349
Rohit Negi <rohit25.negi@gmail.com>
2019-01-31 16:11:10 +0530

    Add import

--- custom/icds_reports/migrations/0089_auto_20190131_0617.py
+++ custom/icds_reports/migrations/0089_auto_20190131_0617.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 
 from django.db import migrations, models
+from corehq.sql_db.operations import RawSQLMigration
 
 
 migrator = RawSQLMigration(('custom', 'icds_reports', 'migrations', 'sql_templates'))
