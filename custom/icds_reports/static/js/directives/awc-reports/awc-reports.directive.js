ca4cddaba9195ed446c3d45534ebd77dcb1d56fb
Dawid Mydlo <dmydlo@soldevelo.com>
2019-01-31 15:03:43 +0100

    Show last value in tooltip when there are multiple values on x axis in chart in AWC beneficiary details page report

--- custom/icds_reports/static/js/directives/awc-reports/awc-reports.directive.js
+++ custom/icds_reports/static/js/directives/awc-reports/awc-reports.directive.js
@@ -2201,9 +2201,12 @@ function AwcReportsController($scope, $http, $location, $routeParams, $log, DTOp
                 var tooltip = chart.interactiveLayer.tooltip;
                 tooltip.contentGenerator(function (d) {
                     var html = "";
-                    var tooltip_data = _.find(vm.lineChartTwoData, function (x) {
-                        return x.x === d.value;
-                    });
+                    var tooltip_data = void(0);
+                    for (var i = 0; i < vm.lineChartTwoData.length; i++) {
+                        if (vm.lineChartTwoData[i].x === d.value) {
+                            tooltip_data = vm.lineChartTwoData[i];
+                        }
+                    }
 
                     if (tooltip_data) {
                         html = "<p>Height: <strong>" + tooltip_data.y + "</strong> cm</p>";
@@ -2261,9 +2264,12 @@ function AwcReportsController($scope, $http, $location, $routeParams, $log, DTOp
                 var tooltip = chart.interactiveLayer.tooltip;
                 tooltip.contentGenerator(function (d) {
                     var html = "";
-                    var tooltip_data = _.find(vm.lineChartOneData, function (x) {
-                        return x.x === d.value;
-                    });
+                    var tooltip_data = void(0);
+                    for (var i = 0; i < vm.lineChartOneData.length; i++) {
+                        if (vm.lineChartOneData[i].x === d.value) {
+                            tooltip_data = vm.lineChartOneData[i];
+                        }
+                    }
 
                     if (tooltip_data) {
                         html = "<p>Weight: <strong>" + tooltip_data.y + "</strong> kg</p>";
@@ -2321,9 +2327,12 @@ function AwcReportsController($scope, $http, $location, $routeParams, $log, DTOp
                 var tooltip = chart.interactiveLayer.tooltip;
                 tooltip.contentGenerator(function (d) {
                     var html = "";
-                    var tooltip_data = _.find(vm.lineChartThreeData, function (x) {
-                        return x.x === d.value;
-                    });
+                    var tooltip_data = void(0);
+                    for (var i = 0; i < vm.lineChartThreeData.length; i++) {
+                        if (vm.lineChartThreeData[i].x === d.value) {
+                            tooltip_data = vm.lineChartThreeData[i];
+                        }
+                    }
 
                     if (tooltip_data) {
                         html = "<p>Weight: <strong>" + tooltip_data.y + "</strong> kg</p>";
