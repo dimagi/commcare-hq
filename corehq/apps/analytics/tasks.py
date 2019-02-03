fa2e98dc8fd9d34e0d545ea4d51e88c227ef7646
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-03 08:39:42 -0500

    remove obsolete check for string

--- corehq/apps/analytics/tasks.py
+++ corehq/apps/analytics/tasks.py
@@ -333,8 +333,7 @@ def send_hubspot_form(form_id, request, user=None, extra_fields=None):
 @analytics_task()
 def send_hubspot_form_task_v2(form_id, web_user_id, hubspot_cookie, meta,
                               extra_fields=None):
-    # TODO - else avoids transient celery errors.  Can remove after deploying to all environments.
-    web_user = WebUser.get_by_user_id(web_user_id) if isinstance(web_user_id, six.string_types) else web_user_id
+    web_user = WebUser.get_by_user_id(web_user_id)
     _send_form_to_hubspot(form_id, web_user, hubspot_cookie, meta,
                           extra_fields=extra_fields)
 
