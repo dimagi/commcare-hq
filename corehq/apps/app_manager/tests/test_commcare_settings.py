69c73c348218a74fdc8aa10cd173eaa7dec4106f
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-03 08:38:49 -0500

    replace six.string_types with six.text_type in tests

--- corehq/apps/app_manager/tests/test_commcare_settings.py
+++ corehq/apps/app_manager/tests/test_commcare_settings.py
@@ -121,7 +121,7 @@ class CommCareSettingsTest(SimpleTestCase):
                         value = setting.get(key)
                         if not value:
                             continue
-                        if not isinstance(value, six.string_types):
+                        if not isinstance(value, six.text_type):
                             for v in value:
                                 self.assertIn(
                                     v,
