98628b69c0dd35ddb5cadca7f582e543c756c039
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-04 08:37:01 -0600

    convert to list of keys

--- corehq/apps/userreports/tests/test_report_builder.py
+++ corehq/apps/userreports/tests/test_report_builder.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-import six
 from django.test import TestCase
 from mock import patch
 
@@ -137,7 +136,7 @@ class DataSourceReferenceTest(ReportBuilderDBTest):
             'form.case.update.last_name', "count",
         ]
 
-        self.assertItemsEqual(expected_property_names, six.iterkeys(reference.data_source_properties))
+        self.assertItemsEqual(expected_property_names, list(reference.data_source_properties))
         user_id_prop = reference.data_source_properties['userID']
         self.assertEqual('userID', user_id_prop.get_id())
         self.assertEqual('userID', user_id_prop.get_text())
