7ebaf8cdc05725cb2117fec302b85b633e240707
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-08 15:25:08 -0600

    generate metadata response dynamically

--- corehq/apps/api/odata/views.py
+++ corehq/apps/api/odata/views.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from django.http import HttpResponse, JsonResponse
+from django.template import Context, Template
 from django.utils.decorators import method_decorator
 from django.views import View
 
 from corehq import toggles
+from corehq.apps.api.odata.utils import get_case_type_to_properties
+from corehq.apps.app_manager.app_schemas.case_properties import ParentCasePropertyBuilder
+from corehq.apps.app_manager.dbaccessors import get_apps_in_domain
 from corehq.apps.domain.decorators import api_auth
 from corehq.apps.reports.analytics.esaccessors import get_case_types_for_domain_es
 from corehq.util.view_utils import absolute_reverse
@@ -35,10 +39,13 @@ class ODataMetadataView(View):
     @method_decorator(api_auth)
     @method_decorator(toggles.ODATA.required_decorator())
     def get(self, request, domain):
-        # todo: should generate this dynamically based on the domain / case schema / data dictionary
         data_file = os.path.join(os.path.dirname(__file__), 'metadata.xml')
         with open(data_file, 'r') as f:
-            return add_odata_headers(HttpResponse(f.read(), content_type='application/xml'))
+            metadata_template = Template(f.read())
+        metadata = metadata_template.render(Context({
+            'case_type_to_properties': get_case_type_to_properties(domain)
+        }))
+        return add_odata_headers(HttpResponse(metadata, content_type='application/xml'))
 
 
 def add_odata_headers(response):
