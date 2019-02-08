9c18b1e451ca7782ff13c8e7c1d6d51ff3f0fa17
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2019-02-08 11:14:30 -0500

    Only return the values of the dict, not the full html dict

--- custom/intrahealth/utils.py
+++ custom/intrahealth/utils.py
@@ -360,8 +360,14 @@ class MultiReport(CustomProjectReport, YeksiNaaMixin, ProjectReportParametersMix
 
     @property
     def export_table(self):
-        reports = [r['report_table'] for r in self.report_context['reports']]
-        return [self._export_table(r['title'], r['headers'], r['rows'], total_row=r['total_row']) for r in reports]
+        export_tables = []
+        for individual_report in self.report_context['reports']:
+            report_table = individual_report['report_table']
+            # The keys contain html info, so only the values get exported to excel
+            total_row = [row_value.items()[0][1] for row_value in report_table['total_row']]
+            export_tables.append(self._export_table(report_table['title'], report_table['headers'],
+                                                    report_table['rows'], total_row))
+        return export_tables
 
     def _export_table(self, export_sheet_name, headers, formatted_rows, total_row=None):
         def _unformat_row(row):
