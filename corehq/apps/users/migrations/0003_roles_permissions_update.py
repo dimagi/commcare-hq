cb4aac9c37d45e62d093a3e03febee2ca2d3ed04
Biyeun Buczyk <biyeun@dimagi.com>
2019-02-08 15:12:47 -0500

    add migration to run roles and permissions management command

---
+++ corehq/apps/users/migrations/0003_roles_permissions_update.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import print_function
+
+from django.core.management import call_command
+from django.db import migrations
+
+from corehq.sql_db.operations import HqRunPython
+
+
+def migrate_roles_permissions_feb2019(*args, **kwargs):
+    def _migrate_roles_permissions_feb2019():
+        return call_command('migrate_roles_permissions_feb2019', noinput=True)
+    return _migrate_roles_permissions_feb2019
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0002_domainrequest'),
+    ]
+
+    operations = [
+        HqRunPython(migrate_roles_permissions_feb2019)
+    ]
