dc34881586d394c9482931cc817332821eb9dca3
Farid Rener <proteusvacuum@gmail.com>
2019-02-07 22:38:33 -0500

    Translate data dictionary download

--- corehq/apps/data_dictionary/views.py
+++ corehq/apps/data_dictionary/views.py
@@ -115,14 +115,14 @@ def _export_data_dictionary(domain):
     )
     export_data = {}
     for case_type in queryset:
-        export_data[case_type.name or "No Name"] = [{
-            'Case Property': prop.name,
-            'Group': prop.group,
-            'Data Type': prop.data_type,
-            'Description': prop.description,
-            'Deprecated': prop.deprecated
+        export_data[case_type.name or _("No Name")] = [{
+            _('Case Property'): prop.name,
+            _('Group'): prop.group,
+            _('Data Type'): prop.data_type,
+            _('Description'): prop.description,
+            _('Deprecated'): prop.deprecated
         } for prop in case_type.properties.all()]
-    headers = ('Case Property', 'Group', 'Data Type', 'Description', 'Deprecated')
+    headers = (_('Case Property'), _('Group'), _('Data Type'), _('Description'), _('Deprecated'))
     outfile = io.BytesIO()
     writer = Excel2007ExportWriter()
     header_table = [(tab_name, [headers]) for tab_name in export_data]
