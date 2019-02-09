8120cc8daf3157294a1c40120bec23d8c1d7995e
Biyeun Buczyk <biyeun@dimagi.com>
2019-02-09 11:23:13 -0500

    add view only permission option for require_can_edit_commcare_users decorator

--- corehq/apps/users/decorators.py
+++ corehq/apps/users/decorators.py
@@ -73,7 +73,9 @@ def require_permission(permission,
 
 
 require_can_edit_web_users = require_permission('edit_web_users')
-require_can_edit_commcare_users = require_permission('edit_commcare_users')
+require_can_edit_commcare_users = require_permission(
+    'edit_commcare_users', view_only_permission='view_commcare_users'
+)
 
 
 def require_permission_to_edit_user(view_func):
