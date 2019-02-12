ca555cd8ae7e241dfb68f92c248698c3fef39461
Dawid Mydlo <dmydlo@soldevelo.com>
2019-02-12 14:00:18 +0100

    Remove not used link_callback function

--- custom/icds_reports/utils/__init__.py
+++ custom/icds_reports/utils/__init__.py
@@ -606,34 +606,6 @@ def create_excel_file(excel_data, data_type, file_format):
     return file_hash
 
 
-def link_callback(uri, rel):
-    """
-    Convert HTML URIs to absolute system paths so xhtml2pdf can access those
-    resources
-    https://xhtml2pdf.readthedocs.io/en/latest/usage.html
-    """
-    # use short variable names
-    sUrl = settings.STATIC_URL      # Typically /static/
-    sRoot = settings.STATIC_ROOT    # Typically /home/userX/project_static/
-    mUrl = settings.MEDIA_URL       # Typically /static/media/
-    mRoot = settings.MEDIA_ROOT     # Typically /home/userX/project_static/media/
-
-    # convert URIs to absolute system paths
-    if uri.startswith(mUrl):
-        path = os.path.join(mRoot, uri.replace(mUrl, ""))
-    elif uri.startswith(sUrl):
-        path = os.path.join(sRoot, uri.replace(sUrl, ""))
-    else:
-        return uri  # handle absolute uri (ie: http://some.tld/foo.png)
-
-    # make sure that file exists
-    if not os.path.isfile(path):
-            raise Exception(
-                'media URI must start with %s or %s' % (sUrl, mUrl)
-            )
-    return path
-
-
 def create_pdf_file(pdf_context):
     pdf_hash = uuid.uuid4().hex
     template = get_template("icds_reports/icds_app/pdf/issnip_monthly_register.html")
