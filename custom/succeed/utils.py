35610f43be4346418ecf4c6d7ed09a91d5caf833
Gabriella Bova <bova3.0@gmail.com>
2019-02-13 14:13:21 -0500

    Remove unused import

--- custom/succeed/utils.py
+++ custom/succeed/utils.py
@@ -2,11 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 from django.utils.translation import ugettext_noop
 import dateutil
-from corehq.apps.app_manager.dbaccessors import (
-    get_latest_build_id,
-    get_latest_released_app,
-    get_latest_released_build_id
-)
+from corehq.apps.app_manager.dbaccessors import get_latest_build_id, get_latest_released_build_id
 from corehq.apps.domain.models import Domain
 from datetime import timedelta
 from pytz import timezone
