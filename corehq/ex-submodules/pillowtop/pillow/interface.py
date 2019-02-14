4538c55cd5c937e0f03ce411117f9671c48f134c
Sravan Reddy <sravfeyn@gmail.com>
2019-02-14 10:33:54 +0530

    dont report time for offsets

--- corehq/ex-submodules/pillowtop/pillow/interface.py
+++ corehq/ex-submodules/pillowtop/pillow/interface.py
@@ -297,11 +297,10 @@ class PillowBase(six.with_metaclass(ABCMeta, object)):
         datadog_gauge('commcare.change_feed.chunked.min_change_lag', min_change_lag, tags=tags)
         datadog_gauge('commcare.change_feed.chunked.max_change_lag', max_change_lag, tags=tags)
 
-        datadog_histogram('commcare.change_feed.chunked.processing_time_total', processing_time,
-            tags=tags + ["chunk_size:{}".format(str(len(changes_chunk)))])
-
         if len(changes_chunk) == self.processor_chunk_size:
             # don't report offset chunks to ease up datadog calculations
+            datadog_histogram('commcare.change_feed.chunked.processing_time_total', processing_time,
+                tags=tags + ["chunk_size:{}".format(str(len(changes_chunk)))])
             datadog_histogram(
                 'commcare.change_feed.processing_time',
                 processing_time / len(changes_chunk),
