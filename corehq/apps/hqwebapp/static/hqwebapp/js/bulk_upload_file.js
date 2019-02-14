b0aeb60738e05a64ed1b2f8c54f6e88b95e8a349
Jennifer Schweers <orange.jenny@gmail.com>
2019-02-14 16:16:44 -0500

    Added UI: new panel in app settings > languages

--- corehq/apps/hqwebapp/static/hqwebapp/js/bulk_upload_file.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bulk_upload_file.js
@@ -7,17 +7,11 @@ hqDefine("hqwebapp/js/bulk_upload_file", [
 ) {
     $(function () {
         "use strict";
-        var ids = [
-            "#bulk_upload_form",
-            "#bulk_ui_translation_upload_form",
-            "#bulk_app_translation_upload_form",
-        ];
-        for (var i = 0; i < ids.length; i++) {
-            if ($(ids[i]).get(0)) {
-                $(ids[i]).koApplyBindings({
-                    file: ko.observable(null),
-                });
-            }
+        var $form = $("#bulk_upload_form");
+        if ($form.length) {
+            $form.koApplyBindings({
+                file: ko.observable(),
+            });
         }
     });
 
