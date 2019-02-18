12dc09bc5dd5aace1cb777bac9b57e59cd74aa4a
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-18 14:27:58 -0500

    include django.setup at end of celery config

--- corehq/celery.py
+++ corehq/celery.py
@@ -1,6 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 import os
 
+import django
+from django.core.checks import run_checks
+from django.core.exceptions import AppRegistryNotReady
+
 from celery import Celery
 
 from manage import init_hq_python_path
@@ -11,3 +15,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 app = Celery()
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+django.setup()
+try:
+    run_checks()
+except AppRegistryNotReady:
+    pass
