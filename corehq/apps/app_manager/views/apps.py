e463c7ee3e4d3e458a5e8252842170b32c3bfd90
Jonathan Emord <jonathan.emord@gmail.com>
2019-02-18 23:36:43 -0500

    Only show custom properties for Application doc_type

--- corehq/apps/app_manager/views/apps.py
+++ corehq/apps/app_manager/views/apps.py
@@ -192,7 +192,9 @@ def get_app_view_context(request, app):
         'values': get_settings_values(app),
         'warning': _("This is not an allowed value for this field"),
     }
-    if toggles.CUSTOM_PROPERTIES.enabled(request.domain) and 'custom_properties' in getattr(app, 'profile', {}):
+    if (app.get_doc_type() == 'Application'
+            and toggles.CUSTOM_PROPERTIES.enabled(request.domain)
+            and 'custom_properties' in getattr(app, 'profile', {})):
         custom_properties_array = [{'key': p[0], 'value': p[1]} for p in app.profile.get('custom_properties').items()]
         app_view_options.update({'customProperties': custom_properties_array})
     context.update({
