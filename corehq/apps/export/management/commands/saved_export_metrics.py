bb5826f45e3ccbb5f21ac3a76a9eacc4cf02ff63
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-20 16:06:35 -0500

    use real code

--- corehq/apps/export/management/commands/saved_export_metrics.py
+++ corehq/apps/export/management/commands/saved_export_metrics.py
@@ -33,28 +33,20 @@ class Command(BaseCommand):
         for domain_name in with_progress_bar(domain_names):
             exports = get_form_export_instances(domain_name)
             for export in exports:
-                if export.blobs:
-                    try:
-                        attachment = export.fetch_attachment('payload')
-                    except ResourceNotFound:
-                        attachment_length = ''
-                        export_format = ''
+                if export.has_file():
+                    attachment = export.get_payload()
+                    if isinstance(attachment, six.text_type):
+                        attachment = attachment.encode('utf-8')
+                    attachment_io = BytesIO(attachment)
+                    export_format = export.export_format
+                    if export_format == 'xlsx':
+                        workbook = openpyxl.load_workbook(attachment_io, read_only=True)
+                        first_worksheet = workbook.worksheets[0]
+                        row_count = first_worksheet.max_row
+                        column_count = first_worksheet.max_column
+                    else:
                         row_count = ''
                         column_count = ''
-                    else:
-                        if isinstance(attachment, six.text_type):
-                            attachment = attachment.encode('utf-8')
-                        attachment_length = len(attachment)
-                        attachment_io = BytesIO(attachment)
-                        export_format = export.export_format
-                        if export_format == 'xlsx':
-                            workbook = openpyxl.load_workbook(attachment_io, read_only=True)
-                            first_worksheet = workbook.worksheets[0]
-                            row_count = first_worksheet.max_row
-                            column_count = first_worksheet.max_column
-                        else:
-                            row_count = ''
-                            column_count = ''
                     yield [
-                        export.get_id, domain_name, attachment_length, export_format, row_count, column_count
+                        export.get_id, domain_name, export.file_size, export_format, row_count, column_count
                     ]
