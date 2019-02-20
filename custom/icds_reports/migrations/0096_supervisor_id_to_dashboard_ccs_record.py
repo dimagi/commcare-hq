db3b91ad13fdfbe7a689653b48bbcb0e2d0e4ab4
Sravan Reddy <sravfeyn@gmail.com>
2019-02-20 15:02:12 +0530

    consistent

--- custom/icds_reports/migrations/0096_supervisor_id_to_dashboard_ccs_record.py
+++ custom/icds_reports/migrations/0096_supervisor_id_to_dashboard_ccs_record.py
@@ -35,7 +35,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='aggregatecomplementaryfeedingforms',
             name='supervisor_id',
-            field=models.TextField(null=True),
+            field=models.CharField(max_length=40, null=True),
         ),
         migrations.AddField(
             model_name='awwincentivereport',
