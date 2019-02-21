22a471ab856ed69f4086f9c88cbab480823eab08
Jonathan Emord <jonathan.emord@gmail.com>
2019-02-21 17:59:02 -0500

    Comment out test class

--- custom/aaa/tests/test_aggregations.py
+++ custom/aaa/tests/test_aggregations.py
@@ -47,71 +47,71 @@ TEST_DOMAIN = 'reach-test'
 TEST_ENVIRONMENT = 'icds'
 
 
-@override_settings(SERVER_ENVIRONMENT='icds')
-class AggregationScriptTest(CSVTestCase):
-    always_include_columns = {'village_id'}
-
-    @classmethod
-    def setUpClass(cls):
-        super(AggregationScriptTest, cls).setUpClass()
-        _setup_ucr_tables()
-        update_child_table(TEST_DOMAIN)
-        update_woman_table(TEST_DOMAIN)
-        update_ccs_record_table(TEST_DOMAIN)
-
-        for month in range(1, 3):
-            update_agg_awc_table(TEST_DOMAIN, date(2019, month, 1))
-            update_agg_village_table(TEST_DOMAIN, date(2019, month, 1))
-
-    @classmethod
-    def tearDownClass(cls):
-        _teardown_ucr_tables()
-        super(AggregationScriptTest, cls).tearDownClass()
-
-    def _convert_decimal_to_string(self, value):
-        """
-            Args:
-                value (decimal.Decimal)
-            Returns:
-                str
-            Converts scientific notation to decimal form if needed.
-            it's needed because in csv file all numbers are written in decimal form.
-            Here is an example why we can't simply apply str to decimal number
-                >>> str(Decimal('0.0000000'))
-                '0E-7'
-                >>> self._convert_decimal_to_string(Decimal('0.0000000'))
-                '0.0000000'
-        """
-        value_str = str(value)
-        p = re.compile('0E-(?P<zeros>[0-9]+)')
-        match = p.match(value_str)
-        if match:
-            return '0.{}'.format(int(match.group('zeros')) * '0')
-        else:
-            return value_str
-
-    def _load_data_from_db(self, table_cls, sort_key):
-        for row in table_cls.objects.order_by(*sort_key).values().all():
-            for key, value in list(row.items()):
-                if isinstance(value, date):
-                    row[key] = value.strftime('%Y-%m-%d')
-                elif isinstance(value, time):
-                    row[key] = value.strftime("%H:%M:%S.%f").rstrip('0').rstrip('.')
-                elif isinstance(value, six.integer_types):
-                    row[key] = str(value)
-                elif isinstance(value, (float, Decimal)):
-                    row[key] = self._convert_decimal_to_string(row[key])
-                elif isinstance(value, six.string_types):
-                    row[key] = value.encode('utf-8')
-                elif value is None:
-                    row[key] = ''
-            yield row
-
-    def _load_and_compare_data(self, table_name, path, sort_key=None):
-        self._fasterAssertListEqual(
-            list(self._load_data_from_db(table_name, sort_key)),
-            self._load_csv(path)
-        )
+# @override_settings(SERVER_ENVIRONMENT='icds')
+# class AggregationScriptTest(CSVTestCase):
+#     always_include_columns = {'village_id'}
+#
+#     @classmethod
+#     def setUpClass(cls):
+#         super(AggregationScriptTest, cls).setUpClass()
+#         _setup_ucr_tables()
+#         update_child_table(TEST_DOMAIN)
+#         update_woman_table(TEST_DOMAIN)
+#         update_ccs_record_table(TEST_DOMAIN)
+#
+#         for month in range(1, 3):
+#             update_agg_awc_table(TEST_DOMAIN, date(2019, month, 1))
+#             update_agg_village_table(TEST_DOMAIN, date(2019, month, 1))
+#
+#     @classmethod
+#     def tearDownClass(cls):
+#         _teardown_ucr_tables()
+#         super(AggregationScriptTest, cls).tearDownClass()
+#
+#     def _convert_decimal_to_string(self, value):
+#         """
+#             Args:
+#                 value (decimal.Decimal)
+#             Returns:
+#                 str
+#             Converts scientific notation to decimal form if needed.
+#             it's needed because in csv file all numbers are written in decimal form.
+#             Here is an example why we can't simply apply str to decimal number
+#                 >>> str(Decimal('0.0000000'))
+#                 '0E-7'
+#                 >>> self._convert_decimal_to_string(Decimal('0.0000000'))
+#                 '0.0000000'
+#         """
+#         value_str = str(value)
+#         p = re.compile('0E-(?P<zeros>[0-9]+)')
+#         match = p.match(value_str)
+#         if match:
+#             return '0.{}'.format(int(match.group('zeros')) * '0')
+#         else:
+#             return value_str
+#
+#     def _load_data_from_db(self, table_cls, sort_key):
+#         for row in table_cls.objects.order_by(*sort_key).values().all():
+#             for key, value in list(row.items()):
+#                 if isinstance(value, date):
+#                     row[key] = value.strftime('%Y-%m-%d')
+#                 elif isinstance(value, time):
+#                     row[key] = value.strftime("%H:%M:%S.%f").rstrip('0').rstrip('.')
+#                 elif isinstance(value, six.integer_types):
+#                     row[key] = str(value)
+#                 elif isinstance(value, (float, Decimal)):
+#                     row[key] = self._convert_decimal_to_string(row[key])
+#                 elif isinstance(value, six.string_types):
+#                     row[key] = value.encode('utf-8')
+#                 elif value is None:
+#                     row[key] = ''
+#             yield row
+#
+#     def _load_and_compare_data(self, table_name, path, sort_key=None):
+#         self._fasterAssertListEqual(
+#             list(self._load_data_from_db(table_name, sort_key)),
+#             self._load_csv(path)
+#         )
 
     # def test_agg_woman_table(self):
     #     self._load_and_compare_data(
