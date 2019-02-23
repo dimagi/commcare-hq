0f28b61d271e39babac4ddace3c0ff6e95397afa
Jennifer Schweers <orange.jenny@gmail.com>
2019-02-23 18:33:52 -0500

    Added tests
    Changes to app_translations.py are just renames removing leading underscores.

--- corehq/apps/translations/tests/test_bulk_app_translation.py
+++ corehq/apps/translations/tests/test_bulk_app_translation.py
@@ -30,8 +30,8 @@ from corehq.apps.translations.app_translations import (
     get_modules_and_forms_row,
     get_unicode_dicts,
     process_bulk_app_translation_upload,
+    _remove_description_from_case_property,
     update_form_translations,
-    _remove_description_from_case_property
 )
 from corehq.apps.translations.const import MODULES_AND_FORMS_SHEET_NAME
 from corehq.util.test_utils import flag_enabled
