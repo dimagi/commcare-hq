9b8ab012284367001f101cc758870c6e4dd0c718
Biyeun Buczyk <biyeun@dimagi.com>
2019-02-25 13:57:54 -0600

    add corresponding ko model for updating simprints integration

---
+++ corehq/apps/integration/static/integration/js/biometric.js
@@ -0,0 +1,14 @@
+hqDefine('integration/js/biometric', [
+    'jquery',
+    'hqwebapp/js/initial_page_data',
+    'integration/js/simprints',
+], function (
+    $,
+    initialPageData,
+    simprints
+) {
+    $(function () {
+        var simprintsModel = simprints.simprintsFormModel(initialPageData.get('simprintsFormData'));
+        $('#simprints-form').koApplyBindings(simprintsModel);
+    });
+});
