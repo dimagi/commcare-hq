f224ea3f55cbe565f1723062ad5df285c2e95a4d
Sravan Reddy <sravfeyn@gmail.com>
2019-02-25 10:34:12 +0530

    update query, add comments from softlayer test results

--- custom/icds_reports/management/commands/backfill_supervisor_id.py
+++ custom/icds_reports/management/commands/backfill_supervisor_id.py
@@ -1,10 +1,15 @@
+# check all tables have non-empty awc_id
 ucrs_with_awc_id = [
+    # some awc_id of this UCR are null
     'config_report_icds-cas_static-pregnant-tasks_cases_6c2a698f',
+    # some forms are from deleted locations, so loc table doesn't have data on some rows
     'config_report_icds-cas_static-usage_forms_92fbe2aa',
+    # some awc_id of this UCR are null
     'config_report_icds-cas_static-awc_mgt_forms_ad1b11f0'
 ]
 
 child_health_ucrs = [
+    # child_health_cases_a46c129f loc table has some empty supervisor_id
     'config_report_icds-cas_dashboard_child_health_daily_fe_f83b12b7',
     'config_report_icds-cas_static-dashboard_thr_forms_b8bca6ea',
     'config_report_icds-cas_static-dashboard_growth_monitor_8f61534c',
@@ -12,19 +17,18 @@ child_health_ucrs = [
     'config_report_icds-cas_static-complementary_feeding_fo_4676987e'
 ]
 
+# some supervisor_id on loc table are null
 ccs_record_ucrs = [
     'config_report_icds-cas_static-dashboard_birth_prepared_fd07c11f',
 ]
 
-ucrs_with_location_id = ['config_report_icds-cas_static-it_report_follow_issue_5be3538d']
-
 # has column 'case_load_ccs_record0'
+# some supervisor_id on loc table are null
 unclear_ccs_record = ['config_report_icds-cas_static-dashboard_delivery_forms_946d56bd']
 
 ucr_to_sql = [
     # ([ucr_table_list, (join_table_name, ucr_column, join_table_column)])
-    (ucrs_with_awc_id, ('awc_location', 'awc_id', 'doc_id')),
-    (ucrs_with_location_id, ('awc_location', 'user_location_id', 'doc_id')),
+    (ucrs_with_awc_id, ('config_report_icds-cas_static-awc_location_88b3f9c3', 'awc_id', 'doc_id')),
     (child_health_ucrs, (
         'config_report_icds-cas_static-child_health_cases_a46c129f',
         'child_health_case_id',
@@ -42,11 +46,10 @@ ucr_to_sql = [
 
 def generate_sql(ucr_table, join_table, ucr_column, join_table_column):
     template = """
-    UPDATE "{ucr_table}"
+    UPDATE "{ucr_table}" ucr
     SET supervisor_id = loc.supervisor_id
-    FROM "{ucr_table}" ucr
-    INNER JOIN "{join_table}" loc
-        ON ucr.{ucr_column} = loc.{join_table_column}
+    FROM "{join_table}" loc
+    WHERE ucr.{ucr_column} = loc.{join_table_column} and ucr.{ucr_column} is NOT NULL
     """
     sql = template.format(
         ucr_table=ucr_table, join_table=join_table,
