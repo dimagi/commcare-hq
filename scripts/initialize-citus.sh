23704862c657184f2d2115c96c10e0f35fb7f5f7
Simon Kelly <skelly@dimagi.com>
2019-02-27 13:17:09 +0200

    add citus_master to hqtest network

--- scripts/initialize-citus.sh
+++ scripts/initialize-citus.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+NETWORK=${1:-}
+
 DATABASE=postgres
 
 function exec_sql() {
@@ -13,3 +15,11 @@ function exec_sql() {
 COMPOSE_PROJECT_NAME=citus docker-compose -f docker/citus-compose.yml up -d
 
 exec_sql citus_master $DATABASE "select * from master_get_active_worker_nodes();"
+
+if [ !  -z  $NETWORK  ]; then
+    # Connecting the coordinator to the HQ docker network allows the containers
+    # in that network to reach the coordinator and to resolve 'citus_master' to
+    # the correct IP
+    echo "Connecting citus_master to '$NETWORK' network"
+    docker network connect $NETWORK citus_master
+fi
