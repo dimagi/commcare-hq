65b0b97f107c0dd14ccd993e8517f3fa5168f504
Norman Hooper <nhooper@dimagi.com>
2019-02-27 18:23:51 +0200

    Pull out prefix

--- corehq/apps/api/domain_metadata.py
+++ corehq/apps/api/domain_metadata.py
@@ -59,6 +59,7 @@ class DomainMetadataResource(CouchResourceMixin, HqBaseResource):
         }
 
     def dehydrate_calculated_properties(self, bundle):
+        calc_prop_prefix = 'cp_'
         domain_obj = _get_domain(bundle)
         try:
             es_data = (DomainES()
@@ -68,7 +69,8 @@ class DomainMetadataResource(CouchResourceMixin, HqBaseResource):
                        .hits[0])
             base_properties = {
                 prop_name: es_data[prop_name]
-                for prop_name in es_data if prop_name[:3] == 'cp_'
+                for prop_name in es_data
+                if prop_name.startswith(calc_prop_prefix)
             }
             try:
                 audit_record = DomainAuditRecordEntry.objects.get(domain=domain_obj.name)
@@ -77,7 +79,7 @@ class DomainMetadataResource(CouchResourceMixin, HqBaseResource):
             extra_properties = {
                 field.name: getattr(audit_record, field.name, 0)
                 for field in DomainAuditRecordEntry._meta.fields
-                if field.name[:3] == 'cp_'
+                if field.name.startswith(calc_prop_prefix)
             }
             base_properties.update(extra_properties)
             return base_properties
