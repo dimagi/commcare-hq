5347c73081a95c06ad59550330ca5b2c80988e23
Nick Pellegrino <npellegrino@dimagi.com>
2019-02-28 17:43:02 -0500

    use key in DATABASES

--- testsettings.py
+++ testsettings.py
@@ -118,9 +118,9 @@ if 'aaa-data' not in DATABASES:
 REPORTING_DATABASES = {
     'default': 'default',
     'ucr': 'default',
-    'icds-ucr': 'icds-ucr_commcarehq',
-    'icds-ucr-non-dashboard': 'icds-ucr_commcarehq',
-    'icds-test-ucr': 'icds-ucr_commcarehq',
+    'icds-ucr': 'icds-ucr',
+    'icds-ucr-non-dashboard': 'icds-ucr',
+    'icds-test-ucr': 'icds-ucr',
     'aaa-data': 'aaa-data',
 }
 
