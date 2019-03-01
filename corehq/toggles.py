5db1329bfc5dd3c5cd02279ca9f84fffccde3d81
Jonathan Emord <jonathan.emord@gmail.com>
2019-03-01 16:28:54 -0500

    Missed one toggle

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1650,7 +1650,7 @@ ICDS_DISHA_API = StaticToggle(
     'ICDS: Access DISHA API',
     TAG_CUSTOM,
     namespaces=[NAMESPACE_USER],
-    relevant_environments={'icds', 'icds-new', 'softlayer'},
+    relevant_environments={'icds', 'icds-new', 'india'},
 )
 
 ALLOW_BLANK_CASE_TAGS = StaticToggle(
