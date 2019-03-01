f91e202051ec40a1ef932e2486f76283384ae7cb
Jonathan Emord <jonathan.emord@gmail.com>
2019-03-01 14:58:12 -0500

    Add restrictions to CaseUploadRecord admin model

--- corehq/apps/case_importer/admin.py
+++ corehq/apps/case_importer/admin.py
@@ -7,7 +7,16 @@ from corehq.apps.case_importer.tracking.models import CaseUploadRecord
 
 
 class CaseUploadRecordAdmin(admin.ModelAdmin):
+    list_display = ['domain', 'task_id', 'upload_id']
     search_fields = ['task_id__exact', 'upload_id__exact']
+    readonly_fields = ['upload_file_meta']
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 
 admin.site.register(CaseUploadRecord, CaseUploadRecordAdmin)
