dbc2711b6ef9844d71afc55462b20aed1d4f58a4
Norman Hooper <nhooper@dimagi.com>
2019-03-01 22:18:08 +0200

    Break up SQL for updated_balance column

--- corehq/form_processor/migrations/sql_templates/migrate_updated_balance_to_bigint.sql
+++
@@ -1,6 +0,0 @@
--- Create bigint_updated_balance column, copy data from updated_balance column, and then rename
-ALTER TABLE form_processor_ledgertransaction ADD COLUMN bigint_updated_balance BIGINT;
-UPDATE form_processor_ledgertransaction SET bigint_updated_balance = updated_balance;
-ALTER TABLE form_processor_ledgertransaction ALTER COLUMN bigint_updated_balance SET NOT NULL;
-ALTER TABLE form_processor_ledgertransaction DROP COLUMN updated_balance;
-ALTER TABLE form_processor_ledgertransaction RENAME COLUMN bigint_updated_balance TO updated_balance;
