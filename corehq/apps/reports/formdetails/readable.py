17545b7cc9afd09366b0fa94622ec6bc8bf0956f
Farid Rener <proteusvacuum@gmail.com>
2019-03-01 14:45:29 -0500

    Handle XForm Exceptions in form summary

--- corehq/apps/reports/formdetails/readable.py
+++ corehq/apps/reports/formdetails/readable.py
@@ -721,7 +721,10 @@ class _AppSummaryFormDataGenerator(object):
         self.errors = []
 
         self._seen_save_to_case = defaultdict(list)
-        self._case_meta = self.app.get_case_metadata()
+        try:
+            self._case_meta = self.app.get_case_metadata()
+        except XFormException:
+            self._case_meta = AppCaseMetadata()
 
     def generate(self):
         return [self._compile_module(module) for module in self.app.get_modules()], self.errors
@@ -761,8 +764,7 @@ class _AppSummaryFormDataGenerator(object):
         except XFormException as exception:
             form_meta['questions'] = []
             form_meta['error'] = {
-                'details': six.text_type(exception),
-                'edit_url': reverse('form_source', args=[self.domain, self.app._id, form.unique_id]),
+                'details': six.text_type(exception[0]),
             }
             self.errors.append(form_meta)
         return form_meta
