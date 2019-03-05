c81d13b7347012926fc6f9eb445c48ea2cf0798b
Jennifer Schweers <orange.jenny@gmail.com>
2019-03-05 14:55:40 +0530

    Fixed cache clearing for get_latest_enabled_versions_per_profile

--- corehq/apps/app_manager/signals.py
+++ corehq/apps/app_manager/signals.py
@@ -41,7 +41,7 @@ def expire_latest_enabled_build_profiles(sender, application, **kwargs):
     if application.copy_of and toggles.RELEASE_BUILDS_PER_PROFILE.enabled(application.domain):
         for build_profile_id in application.build_profiles:
             get_latest_enabled_build_for_profile.clear(application.domain, build_profile_id)
-        get_latest_enabled_versions_per_profile.clear(application.get_id, application.version)
+        get_latest_enabled_versions_per_profile.clear(application.get_id)
 
 
 app_post_save = Signal(providing_args=['application'])
