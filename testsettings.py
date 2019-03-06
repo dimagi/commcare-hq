1e75637104831ecf870ccce87c6ebc80ae3fcbd3
Nick Pellegrino <npellegrino@dimagi.com>
2019-03-06 16:47:58 -0500

    follow aaa example

--- testsettings.py
+++ testsettings.py
@@ -103,6 +103,22 @@ LOGGING = {
 # This is necessary because REPORTING_DATABASES references aaa-data.
 # We must have aaa-data in a separate database
 # https://github.com/dimagi/commcare-hq/pull/23351#issuecomment-467500691
+if 'icds-ucr' not in DATABASES:
+    DATABASES['icds-ucr'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'icds_commcarehq',
+        'USER': 'commcarehq',
+        'PASSWORD': 'commcarehq',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'TEST': {
+            'SERIALIZE': False,
+        }
+    }
+# Define an aaa-data database if its not already defined
+# This is necessary because REPORTING_DATABASES references aaa-data.
+# We must have aaa-data in a separate database
+# https://github.com/dimagi/commcare-hq/pull/23351#issuecomment-467500691
 if 'aaa-data' not in DATABASES:
     DATABASES['aaa-data'] = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
