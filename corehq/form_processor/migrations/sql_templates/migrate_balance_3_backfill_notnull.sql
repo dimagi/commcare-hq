0bb80bc8973f571928e9e676a18b7ec1159e8046
Norman Hooper <nhooper@dimagi.com>
2019-03-08 00:31:14 +0200

    Use filter on backfill.

--- corehq/form_processor/migrations/sql_templates/migrate_balance_3_backfill_notnull.sql
+++ corehq/form_processor/migrations/sql_templates/migrate_balance_3_backfill_notnull.sql
@@ -1,2 +1,2 @@
-UPDATE form_processor_ledgertransaction SET new_updated_balance = updated_balance;
+UPDATE form_processor_ledgertransaction SET new_updated_balance = updated_balance WHERE new_updated_balance IS NULL;
 ALTER TABLE form_processor_ledgertransaction ALTER COLUMN new_updated_balance SET NOT NULL;
