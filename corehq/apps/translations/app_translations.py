9ca0fbdf0e3fe5f8956b04991b324dca4bace4bc
Preethi Vaidyanathan <pvaidyanathan@dimagi.com>
2019-03-08 11:26:39 -0500

    Add tests

--- corehq/apps/translations/app_translations.py
+++ corehq/apps/translations/app_translations.py
@@ -793,6 +793,10 @@ def escape_output_value(value):
         return element
 
 
+def _remove_description_from_case_property(row):
+    return re.match('.*(?= \()', row['case_property']).group()
+
+
 def _update_case_list_translations(sheet, rows, app):
     """
     Modify the translations of a module case list and detail display properties
@@ -827,37 +831,34 @@ def _update_case_list_translations(sheet, rows, app):
     index_of_last_enum_in_condensed = -1
     index_of_last_graph_in_condensed = -1
 
-    def remove_description_from_case_property(row):
-        return re.match('.*(?= \()', row['case_property']).group()
-
     for i, row in enumerate(rows):
         # If it's an enum case property, set index_of_last_enum_in_condensed
         if row['case_property'].endswith(" (ID Mapping Text)"):
-            row['id'] = remove_description_from_case_property(row)
+            row['id'] = _remove_description_from_case_property(row)
             condensed_rows.append(row)
             index_of_last_enum_in_condensed = len(condensed_rows) - 1
 
         # If it's an enum value, add it to it's parent enum property
         elif row['case_property'].endswith(" (ID Mapping Value)"):
-            row['id'] = remove_description_from_case_property(row)
+            row['id'] = _remove_description_from_case_property(row)
             parent = condensed_rows[index_of_last_enum_in_condensed]
             parent['mappings'] = parent.get('mappings', []) + [row]
 
         # If it's a graph case property, set index_of_last_graph_in_condensed
         elif row['case_property'].endswith(" (graph)"):
-            row['id'] = remove_description_from_case_property(row)
+            row['id'] = _remove_description_from_case_property(row)
             condensed_rows.append(row)
             index_of_last_graph_in_condensed = len(condensed_rows) - 1
 
         # If it's a graph configuration item, add it to its parent
         elif row['case_property'].endswith(" (graph config)"):
-            row['id'] = remove_description_from_case_property(row)
+            row['id'] = _remove_description_from_case_property(row)
             parent = condensed_rows[index_of_last_graph_in_condensed]
             parent['configs'] = parent.get('configs', []) + [row]
 
         # If it's a graph series configuration item, add it to its parent
         elif row['case_property'].endswith(" (graph series config)"):
-            row['id'] = remove_description_from_case_property(row)
+            row['id'] = _remove_description_from_case_property(row)
             row['series_index'] = row['case_property'].split(" ")[1]
             parent = condensed_rows[index_of_last_graph_in_condensed]
             parent['series_configs'] = parent.get('series_configs', []) + [row]
