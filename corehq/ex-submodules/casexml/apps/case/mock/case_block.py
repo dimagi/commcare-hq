29984aa948330d6b29571d40d298673c64758b7b
Nick Pellegrino <npellegrino@dimagi.com>
2019-03-10 09:50:22 -0400

    remove unused import

--- corehq/ex-submodules/casexml/apps/case/mock/case_block.py
+++ corehq/ex-submodules/casexml/apps/case/mock/case_block.py
@@ -6,7 +6,6 @@ import warnings
 from datetime import datetime, date
 from xml.etree import cElementTree as ElementTree
 from casexml.apps.case.xml import V2_NAMESPACE
-from corehq.util.python_compatibility import soft_assert_type_text
 from dimagi.utils.parsing import json_format_datetime, string_to_datetime
 from collections import namedtuple
 from functools import partial
