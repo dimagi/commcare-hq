579c70553d08c39147936aa3fd259c82ef7235d4
Sravan Reddy <sravfeyn@gmail.com>
2019-03-12 11:48:53 +0530

    add state_operations

--- custom/icds_reports/migrations/0104_citus_composite_key.py
+++ custom/icds_reports/migrations/0104_citus_composite_key.py
@@ -2,12 +2,51 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.apps import apps
+from django.db import connections, migrations
 from django.conf import settings
-from corehq.sql_db.operations import HqRunPython
+
+from corehq.sql_db.connections import get_icds_ucr_db_alias
 
 
-def _citus_composite_key_migration(apps, schema_editor):
+def _citus_composite_key_sql(model_cls):
+    connection = connections[get_icds_ucr_db_alias()]
+    with connection.schema_editor() as schema_editor:
+        pkey_name = '{}_pkey'.format(model_cls._meta.db_table)
+        fields = list(model_cls._meta.unique_together)[0]
+        columns = [model_cls._meta.get_field(field).column for field in fields]
+        index = schema_editor._create_index_name(model_cls, columns, suffix="_uniq")
+        sql = """
+            CREATE UNIQUE INDEX {index} on "{table}" ({cols});
+            ALTER TABLE "{table}" DROP CONSTRAINT {pkey_name},
+            ADD CONSTRAINT {pkey_name} PRIMARY KEY USING INDEX {index};
+        """.format(
+            table=model_cls._meta.db_table,
+            pkey_name=pkey_name,
+            index=index,
+            cols=','.join(columns)
+        )
+        reverse_sql = """
+            ALTER TABLE "{table}" DROP CONSTRAINT IF EXISTS {index},
+            DROP CONSTRAINT IF EXISTS {pkey_name},
+            ADD CONSTRAINT {pkey_name} PRIMARY KEY ({pkey});
+            DROP INDEX IF EXISTS {index};
+        """.format(
+            table=model_cls._meta.db_table,
+            pkey_name=pkey_name,
+            pkey=model_cls._meta.pk.name,
+            index=index,
+        )
+    if not getattr(settings, 'IS_ON_CITUSDB_BACKEND', False):
+        print "Not CITUS"
+        return migrations.RunSQL.noop, reverse_sql
+    else:
+        print "Yes CITUS"
+        print sql
+        return sql, reverse_sql
+
+
+def get_sql_operations():
     models_to_update = [
         'aggregatebirthpreparednesforms',
         'aggregateccsrecorddeliveryforms',
@@ -20,34 +59,26 @@ def _citus_composite_key_migration(apps, schema_editor):
         'aggregategrowthmonitoringforms',
         'dailyattendance',
     ]
-    if not getattr(settings, 'IS_ON_CITUSDB_BACKEND', False):
-        return
-    else:
-        with schema_editor.connection.cursor() as cursor:
-            for model in models_to_update:
-                model_cls = apps.get_model('icds_reports', model)
-                pkey = '{}_pkey'.format(model_cls._meta.db_table)
-                fields = list(model_cls._meta.unique_together)[0]
-                columns = [model_cls._meta.get_field(field).column for field in fields]
-                index = schema_editor._create_index_name(model_cls, columns, suffix="_uniq")
-                cursor.execute("""
-                    CREATE UNIQUE INDEX {index} on "{table}" ({cols});
-                    ALTER TABLE "{table}" DROP CONSTRAINT {pkey},
-                    ADD CONSTRAINT {pkey} PRIMARY KEY USING INDEX {index};
-                """.format(
-                    table=model_cls._meta.db_table,
-                    pkey=pkey,
-                    index=index,
-                    cols=','.join(columns)
-                ))
+
+    operations = []
+    for model_name in models_to_update:
+        model = apps.get_model('icds_reports', model_name)
+        sql, reverse_sql = _citus_composite_key_sql(model)
+        operations.append(migrations.RunSQL(
+            sql,
+            reverse_sql=reverse_sql,
+            state_operations=[migrations.AlterUniqueTogether(
+                name=model_name,
+                unique_together=set(model._meta.unique_together),
+            )]
+        ))
+    return operations
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounting', '0103_aggregateccsrecordcomplementaryfeedingforms_supervisor_id'),
+        ('icds_reports', '0103_aggregateccsrecordcomplementaryfeedingforms_supervisor_id'),
     ]
 
-    operations = [
-        HqRunPython(_citus_composite_key_migration),
-    ]
+    operations = get_sql_operations()
