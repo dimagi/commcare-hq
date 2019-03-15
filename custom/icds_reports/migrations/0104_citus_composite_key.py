2ef6757bd88ea975f9f74096008558c7720cb418
Cal Ellowitz <calellowitz@gmail.com>
2019-03-14 22:47:08 -0400

    fix pk migrations

--- custom/icds_reports/migrations/0104_citus_composite_key.py
+++ custom/icds_reports/migrations/0104_citus_composite_key.py
@@ -7,36 +7,37 @@ from django.db import connections, migrations
 from django.conf import settings
 
 from corehq.sql_db.routers import db_for_read_write
+from corehq.sql_db.operations import RawSQLMigration, SQL
 
+
+migrator = RawSQLMigration(('custom', 'icds_reports'))
 
 def _citus_composite_key_sql(model_cls):
-    connection = connections[db_for_read_write(model_cls)]
-    with connection.schema_editor() as schema_editor:
-        pkey_name = '{}_pkey'.format(model_cls._meta.db_table)
-        fields = list(model_cls._meta.unique_together)[0]
-        columns = [model_cls._meta.get_field(field).column for field in fields]
-        index = schema_editor._create_index_name(model_cls, columns, suffix="_uniq")
-        sql = """
-            CREATE UNIQUE INDEX {index} on "{table}" ({cols});
-            ALTER TABLE "{table}" DROP CONSTRAINT {pkey_name},
-            ADD CONSTRAINT {pkey_name} PRIMARY KEY USING INDEX {index};
-        """.format(
-            table=model_cls._meta.db_table,
-            pkey_name=pkey_name,
-            index=index,
-            cols=','.join(columns)
-        )
-        reverse_sql = """
-            ALTER TABLE "{table}" DROP CONSTRAINT IF EXISTS {index},
-            DROP CONSTRAINT IF EXISTS {pkey_name},
-            ADD CONSTRAINT {pkey_name} PRIMARY KEY ({pkey});
-            DROP INDEX IF EXISTS {index};
-        """.format(
-            table=model_cls._meta.db_table,
-            pkey_name=pkey_name,
-            pkey=model_cls._meta.pk.name,
-            index=index,
-        )
+    pkey_name = '{}_pkey'.format(model_cls._meta.db_table)
+    fields = list(model_cls._meta.unique_together)[0]
+    columns = [model_cls._meta.get_field(field).column for field in fields]
+    index = '{}_{}_uniq'.format(model_cls._meta.db_table, '_'.join(columns))
+    sql = SQL("""
+        CREATE UNIQUE INDEX {index} on "{table}" ({cols});
+        ALTER TABLE "{table}" DROP CONSTRAINT {pkey_name},
+        ADD CONSTRAINT {pkey_name} PRIMARY KEY USING INDEX {index};
+    """.format(
+        table=model_cls._meta.db_table,
+        pkey_name=pkey_name,
+        index=index,
+        cols=','.join(columns)
+    ))
+    reverse_sql = SQL("""
+        ALTER TABLE "{table}" DROP CONSTRAINT IF EXISTS {index},
+        DROP CONSTRAINT IF EXISTS {pkey_name},
+        ADD CONSTRAINT {pkey_name} PRIMARY KEY ({pkey});
+        DROP INDEX IF EXISTS {index};
+    """.format(
+        table=model_cls._meta.db_table,
+        pkey_name=pkey_name,
+        pkey=model_cls._meta.pk.name,
+        index=index,
+    ))
     if getattr(settings, 'UNIT_TESTING', False):
         return sql, reverse_sql
     else:
@@ -62,13 +63,9 @@ def get_sql_operations():
     for model_name in models_to_update:
         model = apps.get_model('icds_reports', model_name)
         sql, reverse_sql = _citus_composite_key_sql(model)
-        operations.append(migrations.RunSQL(
+        operations.append(migrator.get_migration(
             sql,
-            reverse_sql=reverse_sql,
-            state_operations=[migrations.AlterUniqueTogether(
-                name=model_name,
-                unique_together=set(model._meta.unique_together),
-            )]
+            reverse_sql,
         ))
     return operations
 
