33663e0d6f270b9b80c15c3fc1e1d5c361578864
Norman Hooper <nhooper@dimagi.com>
2019-03-19 15:45:43 +0200

    Simplify

--- corehq/apps/domain/views/accounting.py
+++ corehq/apps/domain/views/accounting.py
@@ -178,6 +178,7 @@ class DomainSubscriptionView(DomainAccountingSettings):
             'price': None,
         }
         cards = None
+        trial_length = None
         if subscription:
             cards = get_customer_cards(self.request.user.username, self.domain)
             date_end = (subscription.date_end.strftime(USER_DATE_FORMAT)
@@ -228,7 +229,7 @@ class DomainSubscriptionView(DomainAccountingSettings):
             'css_class': "label-plan label-plan-%s" % plan_version.plan.edition.lower(),
             'do_not_invoice': subscription.do_not_invoice if subscription is not None else False,
             'is_trial': subscription.is_trial if subscription is not None else False,
-            'trial_length': trial_length if subscription is not None and subscription.is_trial else None,
+            'trial_length': trial_length,
             'date_start': (subscription.date_start.strftime(USER_DATE_FORMAT)
                            if subscription is not None else None),
             'date_end': date_end,
