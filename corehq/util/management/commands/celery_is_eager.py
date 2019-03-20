a4e0480f813ecc99a32c64914ba4cc67a05980c9
Norman Hooper <nhooper@dimagi.com>
2019-03-20 11:02:11 +0200

    Management command for scripting to start celery

---
+++ corehq/util/management/commands/celery_is_eager.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Prints "true" if settings.CELERY_TASK_ALWAYS_EAGER is truthy, else "false".'
+
+    def handle(self, **options):
+        always_eager = getattr(settings, 'CELERY_TASK_ALWAYS_EAGER', False)
+        print('true' if always_eager else 'false')
