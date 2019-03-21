e6a6d67a89aefc2bd2225f89afd6b64a1d6948bf
Daniel Roberts <droberts@dimagi.com>
2019-03-21 11:37:37 -0500

    Use testil.eq instead of assert == in test_buckets.py

--- corehq/util/datadog/tests/test_buckets.py
+++ corehq/util/datadog/tests/test_buckets.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import
 from __future__ import print_function
 from datetime import timedelta
 
+from testil import eq
+
 from corehq.util.datadog.utils import make_bucket_from_timedeltas, DAY_SCALE_TIME_BUCKETS
 
 
 def test_make_bucket_from_timedeltas():
     buckets = [1, 10, 60, 10 * 60, 60 * 60, 12 * 60 * 60, 24 * 60 * 60]
-    assert make_bucket_from_timedeltas(
+    eq(make_bucket_from_timedeltas(
         timedelta(seconds=1),
         timedelta(seconds=10),
         timedelta(minutes=1),
@@ -15,5 +17,5 @@ def test_make_bucket_from_timedeltas():
         timedelta(hours=1),
         timedelta(hours=12),
         timedelta(hours=24),
-    ) == buckets
-    assert DAY_SCALE_TIME_BUCKETS == buckets
+    ), buckets)
+    eq(DAY_SCALE_TIME_BUCKETS, buckets)
