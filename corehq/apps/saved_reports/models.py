f1a7c84caa9903c877ac2e16678c1e39d2602717
Daniel Roberts <droberts@dimagi.com>
2019-03-22 12:43:57 -0500

    Address stickler warning (line legnth)

--- corehq/apps/saved_reports/models.py
+++ corehq/apps/saved_reports/models.py
@@ -364,7 +364,8 @@ class ReportConfig(CachedCouchDocumentMixin, Document):
         LocationAccessMiddleware.apply_location_access(mock_request)
 
         try:
-            dispatch_func = functools.partial(self._dispatcher.__class__.as_view(), mock_request, **self.view_kwargs)
+            dispatch_func = functools.partial(self._dispatcher.__class__.as_view(),
+                                              mock_request, **self.view_kwargs)
             email_response = dispatch_func(render_as='email')
             if email_response.status_code == 302:
                 return ReportContent(
