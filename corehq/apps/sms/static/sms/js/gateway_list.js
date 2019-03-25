5bc3721a59402fe2411c70b7f3f3f6e372f90055
Jenny Schweers <orange.jenny@gmail.com>
2019-03-25 10:31:50 -0500

    Fixed spelling error

--- corehq/apps/sms/static/sms/js/gateway_list.js
+++ corehq/apps/sms/static/sms/js/gateway_list.js
@@ -2,5 +2,5 @@ hqDefine("sms/js/gateway_list", [
     "hqwebapp/js/crud_paginated_list_init",
     "hqwebapp/js/widgets_v4",
 ], function () {
-    // No page-specific logic, just need to cllect the dependencies above
+    // No page-specific logic, just need to collect the dependencies above
 });
