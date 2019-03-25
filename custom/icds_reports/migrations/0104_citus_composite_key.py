d19ff99574abd2457478a864c429eba541482855
Simon Kelly <skelly@dimagi.com>
2019-03-25 14:01:06 +0200

    add missing model

--- custom/icds_reports/migrations/0104_citus_composite_key.py
+++ custom/icds_reports/migrations/0104_citus_composite_key.py
@@ -56,6 +56,7 @@ def get_sql_operations():
         'aggregatechildhealththrforms',
         'aggregatecomplementaryfeedingforms',
         'aggregategrowthmonitoringforms',
+        'aggregateawcinfrastructureforms',
         'dailyattendance',
     ]
 
