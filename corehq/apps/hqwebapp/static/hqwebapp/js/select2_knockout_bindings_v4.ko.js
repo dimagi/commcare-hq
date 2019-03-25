b4630aeb2fb5238c162c34beac1577d22b02ee65
Jennifer Schweers <orange.jenny@gmail.com>
2019-03-24 20:04:54 -0500

    Fixed module name for select2_knockout_bindings_v4.ko.js

--- corehq/apps/hqwebapp/static/hqwebapp/js/select2_knockout_bindings_v4.ko.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/select2_knockout_bindings_v4.ko.js
@@ -1,4 +1,4 @@
-hqDefine("hqwebapp/js/select2_knockout_bindings_v3.ko", [
+hqDefine("hqwebapp/js/select2_knockout_bindings_v4.ko", [
     'jquery',
     'knockout',
     'DOMPurify/dist/purify.min',
