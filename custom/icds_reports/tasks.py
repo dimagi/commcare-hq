141acad77254b4191928bcfd3c5f2172fec9db22
Jonathan Emord <emord@users.noreply.github.com>
2019-03-25 22:21:15 -0400

    Update tasks.py

--- custom/icds_reports/tasks.py
+++ custom/icds_reports/tasks.py
@@ -1020,7 +1020,7 @@ def push_missing_docs_to_es():
         )
 
 
-@periodic_task(run_every=crontab(hour=18, minute=0, day_of_month='12'), acks_late=True, queue='icds_aggregation_queue')
+@periodic_task(run_every=crontab(hour=17, minute=0, day_of_month='12'), acks_late=True, queue='icds_aggregation_queue')
 def build_incentive_report(agg_date=None):
     state_ids = (SQLLocation.objects
                  .filter(domain=DASHBOARD_DOMAIN, location_type__name='state')
