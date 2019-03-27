07f078c4a72238a8d084f273a555a0ee174e957a
Simon Kelly <skelly@dimagi.com>
2019-03-27 10:19:39 +0200

    resolve migration conflict

--- custom/icds_reports/migrations/0107_citus_composite_key.py
+++ custom/icds_reports/migrations/0107_citus_composite_key.py
@@ -75,7 +75,7 @@ def get_sql_operations():
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0104_agg_ls_monthly_ls_name'),
+        ('icds_reports', '0106_left_join_service_delivery'),
     ]
 
     operations = get_sql_operations()
