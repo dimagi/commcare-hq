56441599b9ec3ea041d6511991db4817f4ebf2f3
Daniel Roberts <droberts@dimagi.com>
2019-03-29 17:22:41 -0500

    Drip tasks into each queue to measure blockage
    this basically expands the heartbeat concept to all queues

---
+++ corehq/celery_monitoring/heartbeat.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import datetime
+
+from celery.task import periodic_task
+
+from corehq.util.datadog.gauges import datadog_gauge
+from corehq.util.quickcache import quickcache
+
+
+HEARTBEAT_FREQUENCY = datetime.timedelta(seconds=10)
+HEARTBEAT_CACHE_TIMEOUT = datetime.timedelta(days=2)
+
+
+class HeartbeatNeverRecorded(Exception):
+    pass
+
+
+class Heartbeat(object):
+    def __init__(self, queue):
+        self.queue = queue
+
+    @quickcache(['self.queue'], timeout=HEARTBEAT_CACHE_TIMEOUT.total_seconds())
+    def get_last_seen(self):
+        # This function relies on the cache getting set manually in mark_seen
+        raise HeartbeatNeverRecorded()
+
+    def mark_seen(self):
+        self.get_last_seen.set_cached_value(self).to(datetime.datetime.utcnow())
+
+    def get_blockage_duration(self):
+        """
+        Estimate the amount of time the queue has been blocked
+
+        :returns a timedelta or raises HeartbeatNeverRecorded
+        """
+        # Subtract off the time between heartbeats
+        # since we don't know how long it's been since the last heartbeat
+        return max(datetime.datetime.utcnow() - self.get_last_seen() - HEARTBEAT_FREQUENCY,
+                   datetime.timedelta(seconds=0))
+
+    @property
+    def periodic_task_name(self):
+        return 'heartbeat__{}'.format(self.queue)
+
+    @property
+    def periodic_task(self):
+        queue = self.queue
+
+        def heartbeat():
+            hb = Heartbeat(queue)
+            try:
+                datadog_gauge(
+                    'commcare.celery.heartbeat.blockage_duration',
+                    hb.get_blockage_duration(),
+                    tags=['celery_queue:{}'.format(queue)]
+                )
+            except HeartbeatNeverRecorded:
+                pass
+            hb.mark_seen()
+
+        heartbeat.func_name = self.periodic_task_name
+        heartbeat.__name__ = self.periodic_task_name
+
+        heartbeat = periodic_task(run_every=HEARTBEAT_FREQUENCY, queue=queue)(heartbeat)
+        return heartbeat
---
+++ corehq/celery_monitoring/tests.py
@@ -0,0 +1,29 @@
+import datetime
+
+from freezegun import freeze_time
+
+from corehq.celery_monitoring.heartbeat import Heartbeat, HeartbeatNeverRecorded, \
+    HEARTBEAT_FREQUENCY
+from testil import assert_raises, eq
+
+
+def test_heartbeat():
+    hb = Heartbeat('celery_periodic')
+    hb.get_last_seen.clear(hb)
+
+    with assert_raises(HeartbeatNeverRecorded):
+        hb.get_last_seen()
+
+    with assert_raises(HeartbeatNeverRecorded):
+        hb.get_blockage_duration()
+
+    seen_time = datetime.datetime.utcnow()
+
+    with freeze_time(seen_time):
+        hb.mark_seen()
+        eq(hb.get_last_seen(), seen_time)
+        eq(hb.get_blockage_duration(), datetime.timedelta(seconds=0))
+
+    with freeze_time(seen_time + datetime.timedelta(minutes=10)):
+        eq(hb.get_last_seen(), seen_time)
+        eq(hb.get_blockage_duration(), datetime.timedelta(minutes=10) - HEARTBEAT_FREQUENCY)
