46c9aa915b14deab899b207877d25d107902246f
Daniel Roberts <droberts@dimagi.com>
2019-03-29 13:16:41 -0500

    Fix the way datadog tags arg is formatted

--- corehq/celery.py
+++ corehq/celery.py
@@ -40,16 +40,12 @@ def celery_add_time_sent(headers=None, body=None, **kwargs):
 def celery_record_time_to_start(task_id=None, task=None, **kwargs):
     from corehq.util.datadog.gauges import datadog_gauge, datadog_counter
     time_sent = cache.get('task.{}.time_sent'.format(task_id))
-
+    tags = [
+        'celery_task_name:{}'.format(task.name),
+        'celery_queue:{}'.format(task.queue),
+    ]
     if time_sent:
         time_to_start = (datetime.datetime.utcnow() - time_sent).total_seconds()
-
-        datadog_gauge('commcare.celery.task.time_to_start', time_to_start, tags={
-            'celery_task_name': task.name,
-            'celery_queue': task.queue,
-        })
+        datadog_gauge('commcare.celery.task.time_to_start', time_to_start, tags=tags)
     else:
-        datadog_counter('commcare.celery.task.time_to_start_unavailable', tags={
-            'celery_task_name': task.name,
-            'celery_queue': task.queue,
-        })
+        datadog_counter('commcare.celery.task.time_to_start_unavailable', tags=tags)
