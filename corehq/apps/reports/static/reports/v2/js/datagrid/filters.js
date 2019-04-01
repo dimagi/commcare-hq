89fd20a44def81fa5852e7a1c2443f07ada7503b
Biyeun Buczyk <biyeun@dimagi.com>
2019-04-01 15:17:33 -0500

    add front end column filter models

---
+++ corehq/apps/reports/static/reports/v2/js/datagrid/filters.js
@@ -0,0 +1,39 @@
+/**
+ * todo add docstring
+ */
+
+hqDefine('reports/v2/js/datagrid/filters', [
+    'jquery',
+    'knockout',
+    'underscore',
+    'hqwebapp/js/assert_properties',
+], function (
+    $,
+    ko
+) {
+    'use strict';
+
+    var columnFilterModel = function (data) {
+        var self = {};
+
+        self.filterType = ko.observable(data.filterType);
+        self.name = ko.observable(data.name);
+        self.title = ko.observable(data.title);
+
+        return self;
+    };
+
+    var appliedColumnFilterModel = function (data) {
+        var self = {};
+
+        self.filter = ko.observable(columnFilterModel(data.filter || {}));
+        self.value = ko.observable(data.value);
+
+        return self;
+    };
+
+    return {
+        columnFilterModel: columnFilterModel,
+        appliedColumnFilterModel: appliedColumnFilterModel,
+    };
+});
