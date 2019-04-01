b3e9b0aa20ba58f03dcd5dbfd7c7128d1b820535
Simon Kelly <skelly@dimagi.com>
2019-04-01 14:37:59 +0200

    don't call rebuild_table twice

--- corehq/apps/userreports/pillow.py
+++ corehq/apps/userreports/pillow.py
@@ -181,7 +181,7 @@ class ConfigurableReportTableManagerMixin(object):
             latest_rev = config.get_db().get_rev(config._id)
             if config._rev != latest_rev:
                 raise StaleRebuildError('Tried to rebuild a stale table ({})! Ignoring...'.format(config))
-        adapter.rebuild_table()
+            adapter.rebuild_table()
         if config.is_static:
             rebuild_indicators.delay(adapter.config.get_id)
 
