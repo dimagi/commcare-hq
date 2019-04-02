76b93ca80b5d2878b767eff45450e9bcc8036cbe
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-02 15:04:53 -0400

    Add test for multiple validation errors

--- corehq/apps/userreports/tests/test_data_source_validaton.py
+++ corehq/apps/userreports/tests/test_data_source_validaton.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.test import SimpleTestCase
 
+from corehq.apps.userreports.exceptions import ValidationError
 from corehq.apps.userreports.models import Validation
 from corehq.apps.userreports.tests.utils import get_sample_data_source, get_sample_doc_and_indicators
 
@@ -33,5 +34,34 @@ class DataSourceConfigurationTest(SimpleTestCase):
 
         sample_doc['is_starred'] = 'what is a star?'
 
-        with self.assertRaisesRegexp(Exception, "is_starred has unexpected value"):
+        with self.assertRaisesRegexp(ValidationError, "is_starred has unexpected value"):
             self.config.validate_document(sample_doc)
+
+    def test_multiple_validations(self):
+        self.config.validations = self.config.validations + [
+            Validation.wrap({
+                "name": "a_second_validation",
+                "error_message": "another starred validation",
+                "expression": {
+                    "type": "boolean_expression",
+                    "expression": {
+                        "type": "property_name",
+                        "property_name": "is_starred"
+                    },
+                    "operator": "in",
+                    "property_value": ["yes", "no"]
+                }
+            })
+        ]
+        sample_doc, expected_indicators = get_sample_doc_and_indicators()
+
+        self.assertIsNone(self.config.validate_document(sample_doc))
+
+        sample_doc['is_starred'] = 'what is a star?'
+
+        try:
+            self.config.validate_document(sample_doc)
+        except ValidationError as e:
+            self.assertEquals(len(e.errors), 2)
+        else:
+            self.fail("There were no validation errors returned")
