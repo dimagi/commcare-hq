dimagi/commcare-hq

commit 8f46912a632663a25bc0e3ee3c4b76f2fa4bf5d4

Improve diff output
Indicate ids difference with ≠ character

corehq/apps/couch_sql_migration/management/commands/migrate_domain_from_couch_to_sql.py
@@ -209,7 +209,7 @@ class Command(BaseCommand):
     def _print_status(self, name, ids_in_couch, ids_in_sql, diff_count, num_docs_with_diffs, short, diffs_only):
         n_couch = len(ids_in_couch)
         n_sql = len(ids_in_sql)
-        has_diff = n_couch != n_sql or diff_count
+        has_diff = ids_in_couch != ids_in_sql or diff_count
 
         if diffs_only and not has_diff:
             return False
@@ -217,12 +217,13 @@ class Command(BaseCommand):
         def _highlight(text):
             return shell_red(text) if has_diff else text
 
-        row = "{:^40} | {:^40}"
-        doc_count_row = row.format(n_couch, n_sql)
+        row = "{:^40} {} {:^40}"
+        sep = "|" if ids_in_couch == ids_in_sql else "≠"
+        doc_count_row = row.format(n_couch, sep, n_sql)
         header = ((82 - len(name)) // 2) * '_'
 
         print('\n{} {} {}'.format(header, name, header))
-        print(row.format('Couch', 'SQL'))
+        print(row.format('Couch', '|', 'SQL'))
         print(_highlight(doc_count_row))
         if diff_count:
             print(_highlight("{:^83}".format('{} diffs ({} docs)'.format(diff_count, num_docs_with_diffs))))
@@ -232,7 +233,7 @@ class Command(BaseCommand):
                 couch_only = list(ids_in_couch - ids_in_sql)
                 sql_only = list(ids_in_sql - ids_in_couch)
                 for couch, sql in zip_longest(couch_only, sql_only):
-                    print(row.format(couch or '', sql or ''))
+                    print(row.format(couch or '', '|', sql or ''))
 
         return True
 

commit bba7c3aa586426a4698100d60f6a5e3e8ce531ce

Set file encoding
Issue introduced in 8f46912a632663a25bc0e3ee3c4b76f2fa4bf5d4

corehq/apps/couch_sql_migration/management/commands/migrate_domain_from_couch_to_sql.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function