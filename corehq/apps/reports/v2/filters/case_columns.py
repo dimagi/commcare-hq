e044c6f825f7e844194a403c59a5ae7fee7dc92b
Biyeun Buczyk <biyeun@dimagi.com>
2019-04-03 11:47:34 -0500

    implement TextCaseColumnFilter

--- corehq/apps/reports/v2/filters/case_columns.py
+++ corehq/apps/reports/v2/filters/case_columns.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.utils.translation import ugettext_lazy
 
+from corehq.apps.es import queries
 from corehq.apps.reports.v2.models import BaseColumnFilter, FilterChoiceMeta
 
 
@@ -16,6 +17,19 @@ class TextCaseColumnFilter(BaseColumnFilter):
         FilterChoiceMeta('not_equals', ugettext_lazy('Does Not Equal')),
     ]
 
-    def get_filtered_query(self, query, config):
-        # todo
+    @classmethod
+    def get_filtered_query(cls, query, config):
+        property_name = config['propertyName']
+        filter_name = config['filterName']
+        value = config['filterValue']
+
+        clause = queries.MUST_NOT if filter_name.startswith('not_') else queries.MUST
+
+        if filter_name.endswith('contains'):
+            query = query.regexp_case_property_query(
+                property_name, ".*{}.*".format(value), clause
+            )
+        elif filter_name.endswith('equals'):
+            query = query.case_property_query(property_name, value, clause)
+
         return query
