d04c75fc18062c0b1bc815ee7b445f7e62215942
Biyeun Buczyk <biyeun@dimagi.com>
2019-04-03 11:46:25 -0500

    update filter options and title in TextCaseColumnFilter

--- corehq/apps/reports/v2/filters/case_columns.py
+++ corehq/apps/reports/v2/filters/case_columns.py
@@ -1,16 +1,19 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from django.utils.translation import ugettext_noop
+from django.utils.translation import ugettext_lazy
 
 from corehq.apps.reports.v2.models import BaseColumnFilter, FilterChoiceMeta
 
 
 class TextCaseColumnFilter(BaseColumnFilter):
     filter_type = 'column_text'
+    title = ugettext_lazy("Text")
     choices = [
-        FilterChoiceMeta('contains', ugettext_noop('Contains')),
-        FilterChoiceMeta('is_exactly', ugettext_noop('Is Exactly')),
+        FilterChoiceMeta('contains', ugettext_lazy('Contains')),
+        FilterChoiceMeta('equals', ugettext_lazy('Equals')),
+        FilterChoiceMeta('not_contains', ugettext_lazy('Does Not Contain')),
+        FilterChoiceMeta('not_equals', ugettext_lazy('Does Not Equal')),
     ]
 
     def get_filtered_query(self, query, config):
