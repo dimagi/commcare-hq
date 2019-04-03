6fe437e5fb63681c0b2a5a6bdc9f2ca639c08901
Simon Kelly <skelly@dimagi.com>
2019-04-03 11:05:01 +0200

    test for awc-location table to verify no rebuilds

--- corehq/apps/userreports/tests/test_data_source_rename.py
+++ corehq/apps/userreports/tests/test_data_source_rename.py
@@ -10,12 +10,17 @@ from django.test import TestCase
 from mock import MagicMock, patch
 from sqlalchemy.engine import reflection
 
+from corehq.apps.domain.shortcuts import create_domain
+from corehq.apps.locations.document_store import LOCATION_DOC_TYPE
+from corehq.apps.locations.models import LocationType, SQLLocation
 from corehq.apps.userreports.management.commands.rename_ucr_tables import (
     create_ucr_views,
     _rename_tables,
     _get_old_new_tablenames
 )
-from corehq.apps.userreports.models import DataSourceConfiguration, SQLPartition
+from corehq.apps.userreports.models import DataSourceConfiguration, SQLPartition, StaticDataSourceConfiguration
+from corehq.apps.userreports.pillow import get_location_pillow
+from corehq.apps.userreports.rebuild import _get_columns_to_add, _get_indexes_to_change
 from corehq.apps.userreports.sql.adapter import build_table, get_indicator_table, IndicatorSqlAdapter
 from corehq.apps.userreports.sql.util import table_exists, view_exists
 from corehq.apps.userreports.tests.utils import (
@@ -25,6 +30,7 @@ from corehq.apps.userreports.tests.utils import (
 )
 from corehq.apps.userreports.util import get_indicator_adapter, get_legacy_table_name
 from corehq.pillows.case import get_case_pillow
+from pillowtop.feed.interface import ChangeMeta, Change
 
 
 class DataSourceRenameTest(TestCase):
@@ -178,3 +184,142 @@ class DataSourceRenamePartitionedTest(TestCase):
             'SELECT COUNT(*) FROM "{}y2018d002";'.format('tbl_8e3a5ee0a4309ee52345c2cdcbb1215a_'))
         result = result.fetchone()[0]
         self.assertEqual(1, result)
+
+
+class DataSourceRenameICDSTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(DataSourceRenameICDSTest, cls).setUpClass()
+        cls.data_source = StaticDataSourceConfiguration.by_id('static-icds-cas-static-awc_location')
+        cls.old_table_name = get_legacy_table_name(
+            cls.data_source.domain, cls.data_source.table_id,
+        )
+        cls.legacy_adapter = IndicatorSqlAdapter(cls.data_source, cls.old_table_name)
+        cls.adapter = IndicatorSqlAdapter(cls.data_source)
+        cls.legacy_adapter.build_table()
+
+        # this is expensive so avoid doing it twice
+        cls.tables_by_engine = _get_old_new_tablenames()
+        create_ucr_views(tables_by_engine=cls.tables_by_engine)
+
+        domain = create_domain('icds-cas')
+        state_location_type = LocationType.objects.create(
+            domain=domain.name,
+            name='state',
+        )
+        st1 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='st1',
+            location_id='st1',
+            location_type=state_location_type
+        )
+
+        district_location_type = LocationType.objects.create(
+            domain=domain.name,
+            name='district',
+            parent_type=state_location_type
+        )
+        d1 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='d1',
+            location_id='d1',
+            location_type=district_location_type,
+            parent=st1
+        )
+
+        block_location_type = LocationType.objects.create(
+            domain=domain.name,
+            name='block',
+            parent_type=district_location_type
+        )
+        b1 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='b1',
+            location_id='b1',
+            location_type=block_location_type,
+            parent=d1
+        )
+
+        supervisor_location_type = LocationType.objects.create(
+            domain=domain.name,
+            name='supervisor',
+            parent_type=block_location_type
+        )
+        s1 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='s1',
+            location_id='s1',
+            location_type=supervisor_location_type,
+            parent=b1
+        )
+
+        awc_location_type = LocationType.objects.create(
+            domain=domain.name,
+            name='awc',
+            parent_type=supervisor_location_type
+        )
+        cls.location1 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='a7',
+            location_id='a7',
+            location_type=awc_location_type,
+            parent=s1
+        )
+        cls.location2 = SQLLocation.objects.create(
+            domain=domain.name,
+            name='a8',
+            location_id='a8',
+            location_type=awc_location_type,
+            parent=s1
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.adapter.drop_table()
+        cls.adapter.engine.dispose()
+        cls.legacy_adapter.engine.dispose()
+        super(DataSourceRenameICDSTest, cls).tearDownClass()
+
+    def loc_to_change(self, loc):
+        from corehq.apps.change_feed import data_sources
+
+        return Change(
+            id=loc.location_id,
+            sequence_id='0',
+            document=loc.to_json(),
+            metadata=ChangeMeta(
+                document_id=loc.location_id,
+                data_source_type=data_sources.SOURCE_SQL,
+                data_source_name=data_sources.LOCATION,
+                document_type=LOCATION_DOC_TYPE,
+                domain=loc.domain,
+                is_deletion=False,
+            )
+        )
+
+    def _process_change(self, location):
+        with mock.patch('corehq.apps.userreports.pillow.ConfigurableReportPillowProcessor.rebuild_table'), \
+             mock.patch('corehq.apps.userreports.pillow.migrate_tables') as migrate:
+            pillow = get_location_pillow(ucr_configs=[self.data_source])
+
+            # check that no rebuilds or migrations happen
+            self.assertFalse(pillow.processors[0].rebuild_table.called)
+            for call in migrate.call_args_list:
+                (engine, raw_diffs, tables_to_migrate), kwargs = call
+                self.assertFalse(_get_columns_to_add(raw_diffs, tables_to_migrate))
+                self.assertFalse(_get_indexes_to_change(raw_diffs, tables_to_migrate))
+
+            pillow.process_change(self.loc_to_change(self.location1))
+
+    def test_icds_awc_location_table(self):
+        self._process_change(self.location1)
+
+        with self.legacy_adapter.session_context():
+            self.assertEqual(1, self.legacy_adapter.get_query_object().count())
+
+        _rename_tables(tables_by_engine=self.tables_by_engine)
+
+        self._process_change(self.location2)
+
+        with self.legacy_adapter.session_context():
+            self.assertEqual(2, self.adapter.get_query_object().count())
