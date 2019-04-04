1c7928a5aeff55518bfda2b9a9ef1ec2a2ef76e4
Daniel Roberts <droberts@dimagi.com>
2019-04-04 17:19:40 -0500

    Add simple test for celery time to start timer

--- corehq/celery_monitoring/tests.py
+++ corehq/celery_monitoring/tests.py
@@ -9,6 +9,8 @@ from corehq.celery_monitoring.heartbeat import Heartbeat, HeartbeatNeverRecorded
     HEARTBEAT_FREQUENCY
 from testil import assert_raises, eq
 
+from corehq.celery_monitoring.signals import TimeToStartTimer
+
 
 def test_heartbeat():
     hb = Heartbeat('celery_periodic')
@@ -30,3 +32,23 @@ def test_heartbeat():
     with freeze_time(seen_time + datetime.timedelta(minutes=10)):
         eq(hb.get_last_seen(), seen_time)
         eq(hb.get_blockage_duration(), datetime.timedelta(minutes=10) - HEARTBEAT_FREQUENCY)
+
+
+def test_time_to_start_timer():
+    task_id = 'abc123'
+    delay = datetime.timedelta(seconds=6)
+
+    start_time = datetime.datetime.utcnow()
+
+    # starts empty
+    eq(TimeToStartTimer(task_id).stop_and_pop_timing(), None)
+
+    with freeze_time(start_time):
+        TimeToStartTimer(task_id).start_timing()
+
+    with freeze_time(start_time + delay):
+        time_to_start = TimeToStartTimer(task_id).stop_and_pop_timing()
+
+    eq(time_to_start, delay)
+    # can only pop once, second time empty
+    eq(TimeToStartTimer(task_id).stop_and_pop_timing(), None)
