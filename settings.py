8c16befd39b6d87814d273fcfcd9bee71e2fdcb4
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-04 16:54:48 -0400

    Move mpr_10ab to its own folder

--- settings.py
+++ settings.py
@@ -1811,11 +1811,11 @@ STATIC_UCR_REPORTS = [
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'asr', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'asr', 'ucr_v2', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'mpr', '*.json'),
+    os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'mpr', 'dashboard', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'mpr', 'testing', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'mpr', 'ucr_v2', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'ls', '*.json'),
     os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'other', '*.json'),
-    os.path.join('custom', 'icds_reports', 'ucr', 'reports', 'mpr_10ab_person_cases.json'),
     os.path.join('custom', 'echis_reports', 'ucr', 'reports', '*.json'),
 ]
 
