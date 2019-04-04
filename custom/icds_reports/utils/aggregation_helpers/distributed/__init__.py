76784d4974c91713f7cb0c52a6aa1657a49fb2b5
Simon Kelly <skelly@dimagi.com>
2019-04-04 13:23:52 +0200

    remove unused functions

--- custom/icds_reports/utils/aggregation_helpers/distributed/__init__.py
+++ custom/icds_reports/utils/aggregation_helpers/distributed/__init__.py
@@ -1,34 +1,28 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from datetime import date
-
-from six.moves import range
-
-from corehq.apps.locations.models import SQLLocation
-
+from .agg_awc import AggAwcDistributedHelper
+from .agg_awc_daily import AggAwcDailyAggregationDistributedHelper
 from .agg_ccs_record import AggCcsRecordAggregationDistributedHelper
 from .agg_child_health import AggChildHealthAggregationDistributedHelper
-from .ls_awc_visit_form import LSAwcMgtFormAggDistributedHelper
-from .ls_beneficiary_form import LSBeneficiaryFormAggDistributedHelper
-from .ls_vhnd_form import LSVhndFormAggDistributedHelper
 from .birth_preparedness_forms import BirthPreparednessFormsAggregationDistributedHelper
 from .ccs_record_monthly import CcsRecordMonthlyAggregationDistributedHelper
 from .child_health_monthly import ChildHealthMonthlyAggregationDistributedHelper
 from .complementary_forms import ComplementaryFormsAggregationDistributedHelper
 from .complementary_forms_ccs_record import ComplementaryFormsCcsRecordAggregationDistributedHelper
+from .daily_attendance import DailyAttendanceAggregationDistributedHelper
 from .daily_feeding_forms_child_health import DailyFeedingFormsChildHealthAggregationDistributedHelper
 from .delivery_forms import DeliveryFormsAggregationDistributedHelper
 from .growth_monitoring_forms import GrowthMonitoringFormsAggregationDistributedHelper
 from .inactive_awws import InactiveAwwsAggregationDistributedHelper
+from .ls_awc_visit_form import LSAwcMgtFormAggDistributedHelper
+from .ls_beneficiary_form import LSBeneficiaryFormAggDistributedHelper
+from .ls_vhnd_form import LSVhndFormAggDistributedHelper
+from .mbt import CcsMbtDistributedHelper, ChildHealthMbtDistributedHelper, AwcMbtDistributedHelper
 from .postnatal_care_forms_ccs_record import PostnatalCareFormsCcsRecordAggregationDistributedHelper
 from .postnatal_care_forms_child_health import PostnatalCareFormsChildHealthAggregationDistributedHelper
 from .thr_forms_child_health import THRFormsChildHealthAggregationDistributedHelper
 from .thr_froms_ccs_record import THRFormsCcsRecordAggregationDistributedHelper
-from .agg_awc import AggAwcDistributedHelper
-from .agg_awc_daily import AggAwcDailyAggregationDistributedHelper
-from .daily_attendance import DailyAttendanceAggregationDistributedHelper
-from .mbt import CcsMbtDistributedHelper, ChildHealthMbtDistributedHelper, AwcMbtDistributedHelper
 
 __all__ = (
     'AggCcsRecordAggregationDistributedHelper',
@@ -56,22 +50,3 @@ __all__ = (
     'ChildHealthMbtDistributedHelper',
     'AwcMbtDistributedHelper',
 )
-
-def recalculate_aggregate_table(model_class):
-    """Expects a class (not instance) of models.Model
-
-    Not expected to last past 2018 (ideally past May) so this shouldn't break in 2019
-    """
-    state_ids = (
-        SQLLocation.objects
-        .filter(domain='icds-cas', location_type__name='state')
-        .values_list('location_id', flat=True)
-    )
-
-    for state_id in state_ids:
-        for year in (2015, 2016, 2017):
-            for month in range(1, 13):
-                model_class.aggregate(state_id, date(year, month, 1))
-
-        for month in range(1, date.today().month + 1):
-            model_class.aggregate(state_id, date(2018, month, 1))
--- custom/icds_reports/utils/aggregation_helpers/monolith/__init__.py
+++ custom/icds_reports/utils/aggregation_helpers/monolith/__init__.py
@@ -1,38 +1,32 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from datetime import date
-
-from six.moves import range
-
-from corehq.apps.locations.models import SQLLocation
-
+from .agg_awc import AggAwcHelper
+from .agg_awc_daily import AggAwcDailyAggregationHelper
 from .agg_ccs_record import AggCcsRecordAggregationHelper
 from .agg_child_health import AggChildHealthAggregationHelper
-from .awc_infrastructure import AwcInfrastructureAggregationHelper
-from .aww_incentive import AwwIncentiveAggregationHelper
-from .ls_awc_visit_form import LSAwcMgtFormAggHelper
-from .ls_beneficiary_form import LSBeneficiaryFormAggHelper
-from .ls_vhnd_form import LSVhndFormAggHelper
 from .agg_ls_data import AggLsHelper
+from .awc_infrastructure import AwcInfrastructureAggregationHelper
+from .awc_location import LocationAggregationHelper
+from .aww_incentive import AwwIncentiveAggregationHelper
 from .birth_preparedness_forms import BirthPreparednessFormsAggregationHelper
 from .ccs_record_monthly import CcsRecordMonthlyAggregationHelper
 from .child_health_monthly import ChildHealthMonthlyAggregationHelper
 from .complementary_forms import ComplementaryFormsAggregationHelper
 from .complementary_forms_ccs_record import ComplementaryFormsCcsRecordAggregationHelper
+from .daily_attendance import DailyAttendanceAggregationHelper
 from .daily_feeding_forms_child_health import DailyFeedingFormsChildHealthAggregationHelper
 from .delivery_forms import DeliveryFormsAggregationHelper
 from .growth_monitoring_forms import GrowthMonitoringFormsAggregationHelper
 from .inactive_awws import InactiveAwwsAggregationHelper
+from .ls_awc_visit_form import LSAwcMgtFormAggHelper
+from .ls_beneficiary_form import LSBeneficiaryFormAggHelper
+from .ls_vhnd_form import LSVhndFormAggHelper
+from .mbt import CcsMbtHelper, ChildHealthMbtHelper, AwcMbtHelper
 from .postnatal_care_forms_ccs_record import PostnatalCareFormsCcsRecordAggregationHelper
 from .postnatal_care_forms_child_health import PostnatalCareFormsChildHealthAggregationHelper
 from .thr_forms_child_health import THRFormsChildHealthAggregationHelper
 from .thr_froms_ccs_record import THRFormsCcsRecordAggregationHelper
-from .agg_awc import AggAwcHelper
-from .agg_awc_daily import AggAwcDailyAggregationHelper
-from .awc_location import LocationAggregationHelper
-from .daily_attendance import DailyAttendanceAggregationHelper
-from .mbt import CcsMbtHelper, ChildHealthMbtHelper, AwcMbtHelper
 
 __all__ = (
     'AggCcsRecordAggregationHelper',
@@ -64,22 +58,3 @@ __all__ = (
     'ChildHealthMbtHelper',
     'AwcMbtHelper',
 )
-
-def recalculate_aggregate_table(model_class):
-    """Expects a class (not instance) of models.Model
-
-    Not expected to last past 2018 (ideally past May) so this shouldn't break in 2019
-    """
-    state_ids = (
-        SQLLocation.objects
-        .filter(domain='icds-cas', location_type__name='state')
-        .values_list('location_id', flat=True)
-    )
-
-    for state_id in state_ids:
-        for year in (2015, 2016, 2017):
-            for month in range(1, 13):
-                model_class.aggregate(state_id, date(year, month, 1))
-
-        for month in range(1, date.today().month + 1):
-            model_class.aggregate(state_id, date(2018, month, 1))
