b0f51f4149d8696393c9135be7c9573d2d9caa76
Simon Kelly <skelly@dimagi.com>
2019-04-05 11:05:41 +0200

    remove settings override

--- custom/champ/tests/__init__.py
+++ custom/champ/tests/__init__.py
@@ -28,29 +28,29 @@ def setUpModule():
     _call_center_domain_mock.start()
 
     domain = create_domain('champ-cameroon')
-    with override_settings(SERVER_ENVIRONMENT='production'):
-        try:
-            configs = StaticDataSourceConfiguration.by_domain(domain.name)
-            adapters = [get_indicator_adapter(config) for config in configs]
 
-            for adapter in adapters:
-                adapter.build_table()
+    try:
+        configs = StaticDataSourceConfiguration.by_domain(domain.name)
+        adapters = [get_indicator_adapter(config) for config in configs]
 
-            engine = connection_manager.get_engine(UCR_ENGINE_ID)
-            metadata = sqlalchemy.MetaData(bind=engine)
-            metadata.reflect(bind=engine, extend_existing=True)
-            path = os.path.join(os.path.dirname(__file__), 'fixtures')
-            for file_name in os.listdir(path):
-                with open(os.path.join(path, file_name), encoding='utf-8') as f:
-                    table_name = get_table_name(domain.name, file_name[:-4])
-                    table = metadata.tables[table_name]
-                    postgres_copy.copy_from(
-                        f, table, engine, format='csv' if six.PY3 else b'csv',
-                        null='' if six.PY3 else b'', header=True
-                    )
-        except Exception:
-            tearDownModule()
-            raise
+        for adapter in adapters:
+            adapter.build_table()
+
+        engine = connection_manager.get_engine(UCR_ENGINE_ID)
+        metadata = sqlalchemy.MetaData(bind=engine)
+        metadata.reflect(bind=engine, extend_existing=True)
+        path = os.path.join(os.path.dirname(__file__), 'fixtures')
+        for file_name in os.listdir(path):
+            with open(os.path.join(path, file_name), encoding='utf-8') as f:
+                table_name = get_table_name(domain.name, file_name[:-4])
+                table = metadata.tables[table_name]
+                postgres_copy.copy_from(
+                    f, table, engine, format='csv' if six.PY3 else b'csv',
+                    null='' if six.PY3 else b'', header=True
+                )
+    except Exception:
+        tearDownModule()
+        raise
 
     _call_center_domain_mock.stop()
 
@@ -64,13 +64,12 @@ def tearDownModule():
         'corehq.apps.callcenter.data_source.call_center_data_source_configuration_provider'
     )
     _call_center_domain_mock.start()
-    with override_settings(SERVER_ENVIRONMENT='production'):
 
-        configs = StaticDataSourceConfiguration.by_domain('champ-cameroon')
-        adapters = [get_indicator_adapter(config) for config in configs]
+    configs = StaticDataSourceConfiguration.by_domain('champ-cameroon')
+    adapters = [get_indicator_adapter(config) for config in configs]
 
-        for adapter in adapters:
-            adapter.drop_table()
+    for adapter in adapters:
+        adapter.drop_table()
 
     Domain.get_by_name('champ-cameroon').delete()
     _call_center_domain_mock.stop()
