d71d8b11896dd46da357b3907eca3110ad9f04e2
Cal Ellowitz <calellowitz@gmail.com>
2019-04-05 18:10:06 -0500

    create table if not exists

--- custom/icds_reports/migrations/0111_awc_location_local.py
+++ custom/icds_reports/migrations/0111_awc_location_local.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL('CREATE TABLE "icds_months_local" (LIKE "icds_months" INCLUDING ALL)'),
-        migrations.RunSQL('CREATE TABLE "awc_location_local" (LIKE "awc_location" INCLUDING ALL)')
+        migrations.RunSQL('CREATE TABLE IF NOT EXISTS "icds_months_local" (LIKE "icds_months" INCLUDING ALL)'),
+        migrations.RunSQL('CREATE TABLE IF NOT EXISTS "awc_location_local" (LIKE "awc_location" INCLUDING ALL)')
     ]
     operations.extend(get_view_migrations())
