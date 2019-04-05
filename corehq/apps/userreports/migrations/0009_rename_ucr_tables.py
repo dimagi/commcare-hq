fb101d6a5e6f5e512f1a769284e1f295e4e58f36
Simon Kelly <skelly@dimagi.com>
2019-04-05 16:35:18 +0200

    remove migration command and make migrations noops

--- corehq/apps/userreports/migrations/0009_rename_ucr_tables.py
+++ corehq/apps/userreports/migrations/0009_rename_ucr_tables.py
@@ -2,15 +2,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from django.conf import settings
 from django.db import migrations
-
-from corehq.apps.userreports.management.commands.rename_ucr_tables import _rename_tables
-
-
-def _rename_ucr_tables_migration(apps, schema_editor):
-    if not settings.UNIT_TESTING:
-        _rename_tables()
 
 
 class Migration(migrations.Migration):
@@ -20,5 +12,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(_rename_ucr_tables_migration, migrations.RunPython.noop, elidable=True),
+        migrations.RunPython(migrations.RunPython.noop, migrations.RunPython.noop, elidable=True),
     ]
