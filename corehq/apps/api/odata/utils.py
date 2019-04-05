7fc8f187ca4b1a4945691158aee21963c73a7db6
Nick Pellegrino <npellegrino@dimagi.com>
2019-04-05 11:56:45 -0500

    speedup get_case_type_to_properties

--- corehq/apps/api/odata/utils.py
+++ corehq/apps/api/odata/utils.py
@@ -10,7 +10,9 @@ def get_case_type_to_properties(domain):
     case_types = get_case_types_for_domain_es(domain)
     for case_type in case_types:
         case_type_to_properties[case_type] = []
-        case_export_schema = CaseExportDataSchema.generate_schema_from_builds(domain, None, case_type)
+        case_export_schema = CaseExportDataSchema.generate_schema_from_builds(
+            domain, None, case_type, only_process_current_builds=True
+        )
         for export_group_schema in case_export_schema.group_schemas[0].items:
             cleaned_case_property = export_group_schema.label.replace('_', '')
             case_type_to_properties[case_type].append(cleaned_case_property)
