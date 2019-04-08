041e6255c511a82e2f1c3929633ec996edd47737
Manish Kangia <mkangia@dimagi.com>
2019-04-08 14:22:28 +0530

    specify unit [skip ci]

--- settings.py
+++ settings.py
@@ -537,6 +537,7 @@ CELERY_EVENT_QUEUE_TTL = 2 * 60 * 60
 CELERY_TASK_SERIALIZER = 'json'  # Default value in celery 4.x
 CELERY_ACCEPT_CONTENT = ['json', 'pickle']  # Defaults to ['json'] in celery 4.x.  Remove once pickle is not used.
 
+# in seconds
 CELERY_HEARTBEAT_THRESHOLDS = {
     "analytics_queue": 30 * 60,
     "async_restore_queue": 60,
