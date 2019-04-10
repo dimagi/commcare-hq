dd4e588b65865eeadee57c7581a9f8155df6d6d6
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-10 14:53:46 -0400

    Add mocks for UCR data source tests

--- corehq/apps/locations/ucr_expressions.py
+++ corehq/apps/locations/ucr_expressions.py
@@ -21,6 +21,14 @@ def _get_location(location_id, context):
         return None
 
 
+def _get_location_type_name(location_id, context):
+    location = _get_location(location_id, context)
+    if not location:
+        return None
+
+    return location.location_type.name
+
+
 class LocationTypeSpec(JsonObject):
     type = TypeProperty('location_type_name')
     location_id_expression = DictProperty(required=True)
@@ -34,9 +42,7 @@ class LocationTypeSpec(JsonObject):
             return None
 
         assert context.root_doc['domain']
-        location = _get_location(doc_id, context)
-        if location:
-            return location.location_type.name
+        return _get_location_type_name(doc_id, context)
 
 
 class LocationParentIdSpec(JsonObject):
