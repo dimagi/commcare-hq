3a430de54a4542bac38438d383c0acc94b14285a
Nick Pellegrino <npellegrino@dimagi.com>
2019-04-10 17:47:15 -0500

    move error handling to import_app

--- corehq/apps/app_manager/views/apps.py
+++ corehq/apps/app_manager/views/apps.py
@@ -457,12 +457,7 @@ def _create_linked_app(request, master_app, link_domain, link_app_name):
 
 def _copy_app_helper(request, master_domain, master_app_id_or_source, copy_to_domain, copy_to_app_name, app_id):
     extra_properties = {'name': copy_to_app_name}
-    try:
-        app_copy = import_app_util(master_app_id_or_source, copy_to_domain, extra_properties)
-    except ReportConfigurationNotFoundError:
-        messages.warning(request, _("Copying the application succeeded, but the application will have errors "
-                                    "because your application contains a Mobile Report Module that references "
-                                    "a UCR configured in this project space. Multimedia may be absent."))
+    app_copy = import_app_util(master_app_id_or_source, copy_to_domain, extra_properties, request)
     return back_to_main(request, app_copy.domain, app_id=app_copy._id)
 
 
