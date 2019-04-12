8347613c47e5bf5fb8de80fede685fd38cfb6974
Daniel Roberts <droberts@dimagi.com>
2019-04-12 17:18:01 -0400

    Add ucr tables to domain delete operations
    I think this is a safe way to do this. First, pull all tables in the ucr db that
    have the domain name in them, just as a way to filter down to a very small set
    to pull into memory. Then, for each one, pull out the table prefix and table id,
    reconstruct the expected table name from those, and only keep the ones
    where the original table name matches the expected table name.
    
    I think I can assert that this will _never_ match a table in the wrong domain.
    For a very long domain name, however, this may _miss_ some tables in the domain,
    specficially if the table name was trucated so that the full table id is not present
    in the table name. Then I think short of some sort of brute force search,
    there is no simple way to show that a table belongs to a domain.

--- corehq/apps/userreports/dbaccessors.py
+++ corehq/apps/userreports/dbaccessors.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+import re
+
 from corehq.apps.domain.dbaccessors import get_docs_in_domain_by_class
 from corehq.apps.domain.models import Domain
+from corehq.apps.userreports.util import get_legacy_table_name, get_table_name
 from corehq.dbaccessors.couchapps.all_docs import delete_all_docs_by_doc_type
+from corehq.sql_db.connections import connection_manager, UCR_ENGINE_ID
 from corehq.util.test_utils import unit_testing_only
 
 
@@ -65,3 +70,30 @@ def get_all_report_configs():
 def delete_all_report_configs():
     from corehq.apps.userreports.models import ReportConfiguration
     delete_all_docs_by_doc_type(ReportConfiguration.get_db(), ('ReportConfiguration',))
+
+
+def get_all_table_names_for_domain(domain):
+    engine = connection_manager.get_engine(UCR_ENGINE_ID)
+    with engine.begin() as connection:
+        possible_matches = connection.execute("select tablename from pg_tables where tablename ~ %s", domain)
+    # DO NOT assume just because the domain is in the table name
+    # that the table is in the domain
+    all_table_names_for_domain = []
+    for table_name, in possible_matches:
+        match = re.match(r'^(config_report|ucr)_(.*)_([0-9a-f]{0,29})_([0-9a-f]{8})$', table_name)
+        if match:
+            prefix, table_domain, table_big_hash, table_little_hash = match.groups()
+            if prefix == 'ucr':
+                table_name_check = get_table_name(domain, table_big_hash)
+            else:
+                table_name_check = get_legacy_table_name(domain, table_big_hash)
+            if table_name == table_name_check:
+                all_table_names_for_domain.append(table_name)
+    return all_table_names_for_domain
+
+
+def delete_all_ucr_tables_for_domain(domain):
+    engine = connection_manager.get_engine(UCR_ENGINE_ID)
+    with engine.begin() as connection:
+        for table_name in get_all_table_names_for_domain(domain):
+            connection.execute('DROP TABLE "{}"'.format(table_name))
