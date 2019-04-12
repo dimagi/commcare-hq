f189b7f19055a5ac16e9749393e0b7b737d0e5bd
Nick Pellegrino <npellegrino@dimagi.com>
2019-04-12 08:22:47 -0500

    use get_latest_case_export_schema to speedup get_case_type_to_properties

--- corehq/apps/api/odata/utils.py
+++ corehq/apps/api/odata/utils.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from collections import defaultdict
 
-from corehq.apps.export.models import CaseExportDataSchema
+from corehq.apps.export.dbaccessors import get_latest_case_export_schema
 from corehq.apps.reports.analytics.esaccessors import get_case_types_for_domain_es
 
 
@@ -14,7 +14,7 @@ def get_case_type_to_properties(domain):
         if not case_type:
             # TODO - understand why a case can have a blank case type and handle appropriately
             continue
-        case_export_schema = CaseExportDataSchema.generate_schema_from_builds(domain, None, case_type)
+        case_export_schema = get_latest_case_export_schema(domain, case_type)
         for export_group_schema in case_export_schema.group_schemas[0].items:
             cleaned_case_property = export_group_schema.label.replace('_', '')
             case_type_to_properties[case_type].append(cleaned_case_property)
