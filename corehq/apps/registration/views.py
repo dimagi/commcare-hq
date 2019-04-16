149f5779247dfbcd2110a57827385cfab32533b8
Biyeun Buczyk <biyeun@dimagi.com>
2019-04-16 10:53:01 -0400

    make sure this soft assert doesn't cause tests to fail

--- corehq/apps/registration/views.py
+++ corehq/apps/registration/views.py
@@ -59,7 +59,7 @@ from corehq.util.context_processors import get_per_domain_context
 _domainless_new_user_soft_assert = soft_assert(to=[
     '{}@{}'.format('biyeun', 'dimagi.com'),
     '{}@{}'.format('jschweers', 'dimagi.com')
-], send_to_ops=False)
+], send_to_ops=False, fail_if_debug=False)
 
 
 def get_domain_context():
