a22c10e4d97083d981b46919497bc5453db42ee6
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-17 14:04:11 -0400

    Remove instances of has_user in location form

--- corehq/apps/locations/forms.py
+++ corehq/apps/locations/forms.py
@@ -375,8 +375,6 @@ class LocationFormSet(object):
     """Ties together the forms for location, location data, user, and user data."""
     _location_form_class = LocationForm
     _location_data_editor = CustomDataEditor
-    _user_form_class = LocationUserForm
-    _user_data_editor = CustomDataEditor
 
     def __init__(self, location, request_user, is_new, bound_data=None, *args, **kwargs):
         self.location = location
@@ -385,31 +383,7 @@ class LocationFormSet(object):
         self.request_user = request_user
         self.location_form = self._location_form_class(location, bound_data, is_new=is_new)
         self.custom_location_data = self._get_custom_location_data(bound_data, is_new)
-
-        if self.include_user_forms:
-            self.user_form = self._get_user_form(bound_data)
-            self.custom_user_data = self._get_custom_user_data(bound_data)
-            self.forms = [self.location_form, self.custom_location_data,
-                          self.user_form, self.custom_user_data]
-        else:
-            self.forms = [self.location_form, self.custom_location_data]
-
-    @property
-    @memoized
-    def include_user_forms(self):
-        if not self.is_new:
-            return False
-
-        possible_types = LocationForm.get_allowed_types(self.domain, self.location.parent)
-        if any(lt.has_user for lt in possible_types):
-            if not self.location_form.is_bound:
-                # The form hasn't yet been submitted, so we don't know which type
-                return True
-            else:
-                self.location_form.is_valid()
-                if 'location_type_object' in self.location_form.cleaned_data:
-                    return self.location_form.cleaned_data['location_type_object'].has_user
-        return False
+        self.forms = [self.location_form, self.custom_location_data]
 
     @memoized
     def is_valid(self):
@@ -427,16 +401,8 @@ class LocationFormSet(object):
         if not self.is_valid():
             raise ValueError('Form is not valid')
 
-        if self.include_user_forms:
-            self.user.save()
-            self.location_form.location.user_id = self.user._id
-            location_data = self.custom_location_data.get_data_to_save()
-            location = self.location_form.save(metadata=location_data)
-            self.user.user_location_id = location.location_id
-            self.user.set_location(location)
-        else:
-            location_data = self.custom_location_data.get_data_to_save()
-            location = self.location_form.save(metadata=location_data)
+        location_data = self.custom_location_data.get_data_to_save()
+        self.location_form.save(metadata=location_data)
 
     @property
     @memoized
@@ -479,51 +445,6 @@ class LocationFormSet(object):
         custom_data.form.helper.label_class = 'col-sm-3 col-md-4 col-lg-2'
         custom_data.form.helper.field_class = 'col-sm-4 col-md-5 col-lg-3'
         return custom_data
-
-    def _get_user_form(self, bound_data):
-        domain_obj = Domain.get_by_name(self.domain)
-        form = self._user_form_class(
-            project=domain_obj,
-            data=bound_data,
-            request_user=self.request_user,
-            prefix='location_user',
-        )
-
-        if domain_obj.strong_mobile_passwords:
-            initial_password = generate_strong_password()
-            pw_field = crispy.Field(
-                'new_password',
-                value=initial_password,
-            )
-        else:
-            pw_field = 'new_password'
-
-        form.fields['username'].help_text = None
-        form.fields['location_id'].required = False  # This field isn't displayed
-        form.helper.label_class = 'col-sm-3 col-md-4 col-lg-2'
-        form.helper.field_class = 'col-sm-4 col-md-5 col-lg-3'
-        form.helper.layout = crispy.Layout(
-            crispy.Fieldset(
-                _("Location User"),
-                'username',
-                'first_name',
-                'last_name',
-                pw_field,
-            )
-        )
-        return form
-
-    def _get_custom_user_data(self, bound_data):
-        from corehq.apps.users.views.mobile.custom_data_fields import UserFieldsView
-        user_data = self._user_data_editor(
-            field_view=UserFieldsView,
-            domain=self.domain,
-            post_dict=bound_data,
-            required_only=True,
-            # Set a different prefix so it's not confused with custom location data
-            prefix='user_data',
-        )
-        return user_data
 
 
 class UsersAtLocationForm(forms.Form):
