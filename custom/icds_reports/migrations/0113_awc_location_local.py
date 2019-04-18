c1efac4e19610bbe70ca33156fb7e5fb4915a967
Simon Kelly <skelly@dimagi.com>
2019-04-18 17:35:24 +0200

    fix migrations

--- custom/icds_reports/migrations/0113_awc_location_local.py
+++ custom/icds_reports/migrations/0113_awc_location_local.py
@@ -4,7 +4,10 @@ from __future__ import unicode_literals
 
 from django.db import migrations
 
+from corehq.sql_db.operations import RawSQLMigration
 from custom.icds_reports.utils.migrations import get_view_migrations
+
+migrator = RawSQLMigration(('custom', 'icds_reports', 'migrations', 'sql_templates', 'database_views'))
 
 
 class Migration(migrations.Migration):
@@ -18,3 +21,4 @@ class Migration(migrations.Migration):
         migrations.RunSQL('CREATE TABLE IF NOT EXISTS "awc_location_local" (LIKE "awc_location" INCLUDING ALL)')
     ]
     operations.extend(get_view_migrations())
+    operations.append(migrator.get_migration('service_delivery_monthly.sql'),)
