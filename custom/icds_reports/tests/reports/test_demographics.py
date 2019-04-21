3f82fc79a471431cd484a79a3e308d5684260f23
Nick Pellegrino <npellegrino@dimagi.com>
2019-04-21 08:17:32 -0400

    run TestDemographics

--- custom/icds_reports/tests/reports/test_demographics.py
+++ custom/icds_reports/tests/reports/test_demographics.py
@@ -1,9 +1,6 @@
 from __future__ import absolute_import
 
 from __future__ import unicode_literals
-
-from unittest import skip
-
 from django.test import TestCase
 
 from custom.icds_reports.messages import percent_children_enrolled_help_text, \
@@ -12,7 +9,6 @@ from custom.icds_reports.messages import percent_children_enrolled_help_text, \
 from custom.icds_reports.reports.demographics_data import get_demographics_data
 
 
-@skip
 class TestDemographics(TestCase):
 
     def test_data_monthly_format(self):
