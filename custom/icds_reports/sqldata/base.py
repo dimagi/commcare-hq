5b8ae0fc4873eac3b385e8dd8421418af9a79744
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-22 14:38:29 -0400

    Run experiment on all export queries

--- custom/icds_reports/sqldata/base.py
+++ custom/icds_reports/sqldata/base.py
@@ -6,6 +6,13 @@ from corehq.apps.reports.sqlreport import DatabaseColumn, SqlData
 class IcdsSqlData(SqlData):
     engine_id = 'icds-ucr'
 
+    def get_data(self, start=None, limit=None):
+        from custom.icds_reports.tasks import run_citus_experiment_raw_sql
+
+        for query in self.get_sql_queries():
+            run_citus_experiment_raw_sql.delay(query)
+        return super(IcdsSqlData, self).get_data(start, limit)
+
 
 class ICDSDatabaseColumn(DatabaseColumn):
     def get_raw_value(self, row):
