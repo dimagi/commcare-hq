0fc7a7962579cef554b30028e56923e2d05903c1
Jonathan Emord <jonathan.emord@gmail.com>
2019-04-23 17:11:36 -0400

    Use django json encoder for models manger params

--- custom/icds_reports/models/manager.py
+++ custom/icds_reports/models/manager.py
@@ -1,7 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
+import json
 import uuid
 
+from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models.manager import BaseManager
 from django.db.models.query import QuerySet
 
@@ -13,6 +15,7 @@ class CitusComparisonQuerySet(QuerySet):
         from custom.icds_reports.tasks import run_citus_experiment_raw_sql
         if ICDS_COMPARE_QUERIES_AGAINST_CITUS.enabled(uuid.uuid4().hex, NAMESPACE_OTHER):
             query, params = self.query.sql_with_params()
+            params = json.loads(json.dumps(params, cls=DjangoJSONEncoder))
             run_citus_experiment_raw_sql.delay(query, params, data_source=self.model.__name__)
         super(CitusComparisonQuerySet, self)._fetch_all()
 
