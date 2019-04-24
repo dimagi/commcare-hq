d2cd2105b74b07823dcff93322ee9a380f13c682
Daniel Roberts <droberts@dimagi.com>
2019-04-24 17:53:38 -0500

    Refactor env_settings.py

--- env_settings.py
+++ env_settings.py
@@ -1,129 +1,79 @@
 from __future__ import unicode_literals
+
+
+def _get_analysis(*names):
+    return {
+        "analyzer": {name: ANALYZERS[name] for name in names}
+    }
+
+
+ANALYZERS = {
+    "default": {
+        "type": "custom",
+        "tokenizer": "whitespace",
+        "filter": ["lowercase"]
+    },
+    "comma": {
+        "type": "pattern",
+        "pattern": r"\s*,\s*"
+    },
+    "sortable_exact": {
+        "type": "custom",
+        "tokenizer": "keyword",
+        "filter": ["lowercase"]
+    },
+}
+
 ES_META = {
     # Default settings for all indexes on ElasticSearch
     'default': {
         "settings": {
             "number_of_replicas": 0,
-            "analysis": {
-                "analyzer": {
-                    "default": {
-                        "type": "custom",
-                        "tokenizer": "whitespace",
-                        "filter": ["lowercase"]
-                    },
-                    "sortable_exact": {
-                        "type": "custom",
-                        "tokenizer": "keyword",
-                        "filter": ["lowercase"]
-                    }
-                }
-            }
-        }
+            "analysis": _get_analysis('default', 'sortable_exact'),
+        },
     },
     # Default settings for aliases on all environments (overrides default settings)
     'hqdomains': {
         "settings": {
             "number_of_replicas": 0,
-            "analysis": {
-                "analyzer": {
-                    "default": {
-                        "type": "custom",
-                        "tokenizer": "whitespace",
-                        "filter": ["lowercase"]
-                    },
-                    "comma": {
-                        "type": "pattern",
-                        "pattern": r"\s*,\s*"
-                    },
-                }
-            }
-        }
+            "analysis": _get_analysis('default', 'comma'),
+        },
     },
 
     'hqapps': {
         "settings": {
             "number_of_replicas": 0,
-            "analysis": {
-                "analyzer": {
-                    "default": {
-                        "type": "custom",
-                        "tokenizer": "whitespace",
-                        "filter": ["lowercase"]
-                    },
-                }
-            }
-        }
+            "analysis": _get_analysis('default'),
+        },
     },
 
     'hqusers': {
         "settings": {
             "number_of_shards": 2,
             "number_of_replicas": 0,
-            "analysis": {
-                "analyzer": {
-                    "default": {
-                        "type": "custom",
-                        "tokenizer": "whitespace",
-                        "filter": ["lowercase"]
-                    },
-                }
-            }
-        }
+            "analysis": _get_analysis('default'),
+        },
     },
 
     # Default settings for aliases per environment (overrides default settings for alias)
     'production': {
         'xforms': {
             'settings': {
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                        "sortable_exact": {
-                            "type": "custom",
-                            "tokenizer": "keyword",
-                            "filter": ["lowercase"]
-                        }
-                    },
-                },
+                "analysis": _get_analysis('default', 'sortable_exact'),
             },
         },
         'hqcases': {
             'settings': {
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                        "sortable_exact": {
-                            "type": "custom",
-                            "tokenizer": "keyword",
-                            "filter": ["lowercase"]
-                        },
-                    },
-                },
+                "analysis": _get_analysis('default', 'sortable_exact'),
             },
         },
         'hqusers': {
             "settings": {
                 "number_of_shards": 2,
                 "number_of_replicas": 1,
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                    }
-                }
-            }
-        }
+                "analysis": _get_analysis('default'),
+            },
+        },
     },
 
     'swiss': {
@@ -131,16 +81,8 @@ ES_META = {
             "settings": {
                 "number_of_shards": 2,
                 "number_of_replicas": 0,
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                    }
-                }
-            }
+                "analysis": _get_analysis('default'),
+            },
         },
     },
 
@@ -149,16 +91,8 @@ ES_META = {
             "settings": {
                 "number_of_shards": 2,
                 "number_of_replicas": 0,
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                    }
-                }
-            }
+                "analysis": _get_analysis('default'),
+            },
         },
     },
 
@@ -167,16 +101,8 @@ ES_META = {
             "settings": {
                 "number_of_shards": 2,
                 "number_of_replicas": 0,
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                    }
-                }
-            }
+                "analysis": _get_analysis('default'),
+            },
         },
     },
     'icds': {
@@ -184,16 +110,8 @@ ES_META = {
             "settings": {
                 "number_of_shards": 2,
                 "number_of_replicas": 1,
-                "analysis": {
-                    "analyzer": {
-                        "default": {
-                            "type": "custom",
-                            "tokenizer": "whitespace",
-                            "filter": ["lowercase"]
-                        },
-                    }
-                }
-            }
+                "analysis": _get_analysis('default'),
+            },
         },
     },
 }
