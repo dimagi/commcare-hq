78865753967e45dd71febfee00dae8eb16ac0b87
mkangia <mkangia@dimagi.com>
2019-04-25 16:09:46 +0530

    add link under application tab, CCZHostingtab and urls

---
+++ custom/icds/urls.py
@@ -0,0 +1,23 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+from custom.icds.views.ccz_hosting import (
+    ManageCCZHosting,
+    ManageCCZHostingLink,
+    EditCCZHostingLink,
+    CCZHostingView,
+    remove_ccz_hosting,
+)
+
+urlpatterns = [
+    url(r'^ccz/hostings/manage', ManageCCZHosting.as_view(), name=ManageCCZHosting.urlname),
+    url(r'^ccz/hostings/links', ManageCCZHostingLink.as_view(), name=ManageCCZHostingLink.urlname),
+    url(r'^ccz/hostings/link/(?P<link_id>[\d-]+)/edit/', EditCCZHostingLink.as_view(),
+        name=EditCCZHostingLink.urlname),
+    url(r'^ccz/hostings/link/(?P<link_id>[\d-]+)/delete/', remove_ccz_hosting,
+        name="remove_ccz_hosting"),
+    url(r'^ccz/hostings/link/(?P<link_id>[\d-]+)/', ManageCCZHostingLink.as_view(),
+        name=ManageCCZHostingLink.urlname),
+    url(r'^ccz/hostings/(?P<identifier>[\w-]+)/', CCZHostingView.as_view(), name=CCZHostingView.urlname),
+]
