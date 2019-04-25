5fbb2db7d2c54a16f139df5fe614b1bfc7e9434a
mkangia <mkangia@dimagi.com>
2019-04-25 14:50:38 +0530

    add model validation

--- custom/icds/models.py
+++ custom/icds/models.py
@@ -3,7 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.functional import cached_property
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
+from corehq.apps.app_manager.dbaccessors import get_build_by_version
 from corehq.motech.utils import b64_aes_decrypt
 from custom.icds.validators import (
     LowercaseAlphanumbericValidator,
@@ -30,3 +33,16 @@ class CCZHosting(models.Model):
     link = models.ForeignKey(CCZHostingLink, on_delete=models.CASCADE)
     app_id = models.CharField(max_length=255, null=False)
     version = models.IntegerField(null=False)
+
+    def build_doc(self):
+        return get_build_by_version(self.link.domain, self.app_id, self.version)
+
+    def clean(self):
+        if not self.build_doc['is_released']:
+            raise ValidationError({
+                'version': _("Version not released. Please mark it as released.")})
+        super(CCZHosting, self).clean()
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super(CCZHosting, self).save(*args, **kwargs)
