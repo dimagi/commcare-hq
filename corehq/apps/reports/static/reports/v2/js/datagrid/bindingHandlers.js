040a2b806497e2b75b30fdd4c7a67f7fe66898b6
Biyeun Buczyk <biyeun@dimagi.com>
2019-05-02 16:09:01 +0200

    add select2 support for datagrid

--- corehq/apps/reports/static/reports/v2/js/datagrid/bindingHandlers.js
+++ corehq/apps/reports/static/reports/v2/js/datagrid/bindingHandlers.js
@@ -5,16 +5,77 @@
 hqDefine('reports/v2/js/datagrid/bindingHandlers', [
     'jquery',
     'knockout',
+    'underscore',
     'hqwebapp/js/atwho',
     'atjs',
     'caretjs',
     'bootstrap-daterangepicker/daterangepicker',
+    "select2/dist/js/select2.full.min",
 ], function (
     $,
     ko,
+    _,
     atwho
 ) {
     'use strict';
+
+    ko.bindingHandlers.select2 = {
+        init: function (element, valueAccessor, allBindings) {
+            var options = ko.utils.unwrapObservable(valueAccessor()),
+                ajax = {};
+
+            if (options.url) {
+                ajax = {
+                    delay: 150,
+                    url: options.url,
+                    dataType: 'json',
+                    type: 'post',
+                    data: function (params) {
+                        var data = {
+                            search: params.term,
+                            page: params.page || 1
+                        };
+                        if (_.isFunction(options.getData)) {
+                            data = options.getData(data);
+                        }
+                        return data;
+                    },
+                    processResults: function (data) {
+                        if (_.isFunction(options.processResults)) {
+                            data = options.processResults(data);
+                        }
+                        return data;
+                    },
+                    error: function () {
+                        if (_.isFunction(options.handleError)) {
+                            options.handleError();
+                        }
+                    },
+                };
+            }
+
+            $(element).select2({
+                minimumInputLength: 0,
+                allowClear: true,
+                multiple: !!options.multiple,
+                placeholder: options.placeholder || gettext("Search..."), // some placeholder required for allowClear
+                width: options.width || '100%',
+                ajax: ajax,
+                templateResult: function (result) {
+                    if (_.isFunction(options.templateResult)) {
+                        return options.templateResult(result);
+                    }
+                    return result.text;
+                },
+                templateSelection: function (selection) {
+                    if (_.isFunction(options.templateSelection)) {
+                        return options.templateSelection(selection);
+                    }
+                    return selection.text;
+                },
+            });
+        },
+    };
 
     ko.bindingHandlers.datagridAutocomplete = {
         init: function (element) {
