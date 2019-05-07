1898263228586da8da17a074f0e8633674628fa2
Gabriella Bova <bova3.0@gmail.com>
2019-05-07 12:38:10 -0400

    Remove unused imports

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
-import inspect
-from datetime import datetime, timedelta
+from datetime import datetime
 from dateutil.relativedelta import relativedelta
 import logging
 import re
@@ -22,7 +21,6 @@ from corehq.form_processor.interfaces.supply import SupplyInterface
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors
 from corehq.sql_db.routers import db_for_read_write
 from corehq.util.python_compatibility import soft_assert_type_text
-from corehq.util.soft_assert import soft_assert
 from dimagi.ext.couchdbkit import (
     StringProperty,
     IntegerProperty,
@@ -61,7 +59,6 @@ from corehq.apps.users.util import (
     user_display_string,
     user_location_data,
     username_to_user_id,
-    format_username,
     filter_by_app
 )
 from corehq.apps.users.tasks import tag_forms_as_deleted_rebuild_associated_cases, \
@@ -73,7 +70,7 @@ from corehq.apps.hqwebapp.tasks import send_html_email_async
 from dimagi.utils.dates import force_to_datetime
 from xml.etree import cElementTree as ElementTree
 
-from couchdbkit.exceptions import ResourceConflict, NoResultFound, BadValueError
+from couchdbkit.exceptions import ResourceConflict, BadValueError
 
 from dimagi.utils.web import get_site_domain
 import six
