bb32f2327d2e3aa386fffd2fd320a7af7b03ce95
Gabriella Bova <bova3.0@gmail.com>
2019-05-09 11:14:24 -0400

    Include superusers in web user domaing access record

--- corehq/apps/domain/project_access/middleware.py
+++ corehq/apps/domain/project_access/middleware.py
@@ -12,13 +12,13 @@ class ProjectAccessMiddleware(MiddlewareMixin):
     def process_view(self, request, view_func, view_args, view_kwargs):
         if getattr(request, 'couch_user', None) and request.couch_user.is_superuser \
                 and hasattr(request, 'domain'):
-            return self.record_entry(request.domain, request.couch_user.username)
+            self.record_superuser_entry(request.domain, request.couch_user.username)
         if getattr(request, 'couch_user', None) and request.couch_user.is_web_user() \
                 and hasattr(request, 'domain'):
             self.record_web_user_entry(request.couch_user, request.domain)
 
     @quickcache(['domain', 'username'], timeout=ENTRY_RECORD_FREQUENCY.seconds)
-    def record_entry(self, domain, username):
+    def record_superuser_entry(self, domain, username):
         if not SuperuserProjectEntryRecord.entry_recently_recorded(username, domain):
             SuperuserProjectEntryRecord.record_entry(username, domain)
         return None
