3d2bf39b796a4ebe517204b16eb00198f9dbd9fa
Biyeun Buczyk <biyeun@dimagi.com>
2019-05-09 17:12:15 +0200

    add pagination functionality to datagrid report

--- corehq/apps/reports/static/reports/v2/js/datagrid/data_models.js
+++ corehq/apps/reports/static/reports/v2/js/datagrid/data_models.js
@@ -13,6 +13,17 @@ hqDefine('reports/v2/js/datagrid/data_models', [
 ) {
     'use strict';
 
+    var _numberOption = function (number, current) {
+        var self = {};
+
+        self.number = ko.observable(number);
+        self.isCurrent = ko.computed(function () {
+            return self.number() === current;
+        });
+
+        return self;
+    };
+
     var scrollingDataModel = function (endpoint) {
         var self = {};
 
@@ -22,7 +33,68 @@ hqDefine('reports/v2/js/datagrid/data_models', [
         self.isLoadingError = ko.observable(false);
 
         self.rows = ko.observableArray([]);
-        self.length = 50; // todo change number dynamically?
+        self.limit = ko.observable(10);
+        self.currentPage = ko.observable(1);
+        self.totalPages = ko.observable(1);
+        self.totalRecords = ko.observable(1);
+
+        self.pages = ko.computed(function () {
+            return _.map(_.range(1, self.totalPages() + 1), function (pageNum) {
+               return _numberOption(pageNum, self.currentPage());
+            });
+        });
+
+        self.limits = ko.computed(function() {
+            return _.map([10, 25, 50, 100], function (limitNum) {
+                return _numberOption(limitNum, self.limit());
+            });
+        });
+
+        self.changePage = function (page) {
+            self.currentPage(page.number());
+            self.loadRecords();
+        };
+
+        self.changeLimit = function (limit) {
+            self.limit(limit.number());
+            self.currentPage(1);
+            self.loadRecords();
+        };
+
+        self._pushState = function () {
+            var stateInfo = {
+                    page: self.currentPage(),
+                    limit: self.limit(),
+                },
+                url = '?p=' + self.currentPage() + "&l=" + self.limit();
+            window.history.pushState(stateInfo, self.pageTitle, url);
+        };
+
+        self._loadState = function () {
+            var history = window.history.state;
+            if (_.isObject(history)) {
+                self.currentPage(history.page || 1);
+                self.limit(history.limit || 10);
+            }
+        };
+
+        self.showNext = ko.computed(function () {
+           return self.currentPage() < self.totalPages();
+        });
+
+        self.showPrevious = ko.computed(function () {
+            return self.currentPage() > 1;
+        });
+
+        self.nextPage = function () {
+           self.currentPage(Math.min(self.totalPages(), self.currentPage() + 1));
+           self.loadRecords();
+        };
+
+        self.previousPage = function () {
+            self.currentPage(Math.max(self.currentPage() - 1, 1));
+            self.loadRecords();
+        };
 
         self.loadRecords = function () {
             if (!self.reportContext) {
@@ -37,13 +109,16 @@ hqDefine('reports/v2/js/datagrid/data_models', [
                 method: 'post',
                 dataType: 'json',
                 data: {
-                    length: self.length,
-                    start: 0, // fix with pagination
+                    limit: self.limit(),
+                    page: self.currentPage(),
                     reportContext: JSON.stringify(self.reportContext()),
                 },
             })
                 .done(function (data) {
                     self.rows(data.rows);
+                    self.totalPages(data.totalPages);
+                    self.totalRecords(data.totalRecords);
+                    self._pushState();
                 })
                 .fail(function () {
                     self.isLoadingError(true);
@@ -55,6 +130,8 @@ hqDefine('reports/v2/js/datagrid/data_models', [
 
         self.init = function (reportContextObservable) {
             self.reportContext = reportContextObservable;
+            self.pageTitle = $(document).find("title").text();
+            self._loadState();
             self.loadRecords();
         };
 
