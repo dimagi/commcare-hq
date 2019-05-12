84872c014a17faee7b3dd5b95c1a90d08cc8f67f
mkangia <mkangia@dimagi.com>
2019-05-13 03:33:55 +0530

    revert changes made to IcdsFile

--- custom/icds_reports/models/helper.py
+++ custom/icds_reports/models/helper.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from django.utils.functional import cached_property
 
 from corehq.blobs import CODES, get_blob_db
 from corehq.blobs.models import BlobMeta
@@ -26,7 +25,7 @@ class IcdsFile(models.Model):
     data_type = models.CharField(max_length=255)
     file_added = models.DateField(auto_now=True)
 
-    def store_file_in_blobdb(self, file, expired=EXPIRED, name=None):
+    def store_file_in_blobdb(self, file, expired=EXPIRED):
         db = get_blob_db()
         try:
             kw = {"meta": db.metadb.get(
@@ -39,10 +38,8 @@ class IcdsFile(models.Model):
                 "parent_id": 'IcdsFile',
                 "type_code": CODES.tempfile,
                 "key": self.blob_id,
-                "timeout": expired,
+                "timeout": expired
             }
-            if name:
-                kw["name"] = name
         db.put(file, **kw)
 
     def get_file_from_blobdb(self):
@@ -50,18 +47,6 @@ class IcdsFile(models.Model):
 
     def get_file_size(self):
         return get_blob_db().size(key=self.blob_id)
-
-    @cached_property
-    def get_file_meta(self):
-        db = get_blob_db()
-        return db.metadb.get(key=self.blob_id, parent_id='IcdsFile')
-
-    def remove_file_from_blobdb(self):
-        get_blob_db().delete(key=self.blob_id)
-
-    def delete(self, *args, **kwargs):
-        self.remove_file_from_blobdb()
-        super(IcdsFile, self).delete(*args, **kwargs)
 
     class Meta:
         app_label = 'icds_reports'
