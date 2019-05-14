23ebd864e5f9b01a931a5cabf9a9884778eb7ec9
Farid Rener <proteusvacuum@gmail.com>
2019-05-14 13:59:06 -0400

    Save and retreive template xml from blobdb

--- corehq/apps/case_templates/models.py
+++ corehq/apps/case_templates/models.py
@@ -2,8 +2,19 @@
 from __future__ import absolute_import, unicode_literals
 
 import uuid
+from xml.etree import cElementTree as ElementTree
 
 from django.db import models
+from django.utils.functional import cached_property
+
+from six import StringIO
+
+from casexml.apps.case.mock import CaseBlock
+from casexml.apps.case.xml import V2, V2_NAMESPACE
+from casexml.apps.phone.xml import get_case_xml
+
+from corehq.blobs import CODES, get_blob_db
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 
 
 class CaseTemplate(models.Model):
@@ -12,17 +23,77 @@ class CaseTemplate(models.Model):
     name = models.CharField(max_length=256, null=False)
     comment = models.TextField(null=True)
 
-    def get_template_xml(self):
+    def __repr__(self):
+        return (
+            "CaseTemplate("
+            "template_id='{self.template_id}', "
+            "domain='{self.domain}', "
+            "name='{self.name}', "
+            "comment='{self.comment}'"
+            ")"
+        ).format(self=self)
+
+    @classmethod
+    def create(cls, domain, root_case_id, name, comment=None):
+        template = cls.objects.create(domain=domain, name=name, comment=comment)
+        template.save_template_xml(cls._generate_case_xml(domain, root_case_id))
+        return template
+
+    @staticmethod
+    def _generate_case_xml(domain, root_case_id):
+        """Returns a blob of XML containing all the cases in the case hierarchy starting with `root_case_id`
+        """
+        root_case = CaseAccessors(domain).get_case(root_case_id)
+        case_xml_blocks = (
+            get_case_xml(case, ('create', 'update'), V2) for case in get_case_hierarchy(root_case)
+        )
+        return "<template-cases>{}</template-cases>".format("".join(case_xml_blocks))
+
+    @property
+    def _blob_key(self):
+        return "case-template-{}-{}".format(self.domain, self.template_id)
+
+    def save_template_xml(self, case_xml):
+        """saves the case_xml to the blobdb
+        """
+        blob_meta_args = {
+            "name": "{}.xml".format(self._blob_key),
+            "parent_id": self.template_id,
+            "domain": self.domain,
+            "type_code": CODES.case_template,
+            "key": self._blob_key,
+        }
+        return get_blob_db().put(StringIO(case_xml), **blob_meta_args)
+
+    @cached_property
+    def prototype_cases(self):
+        """Dict of cases, keyed by case ID contained in the template
+        """
+        return {case.case_id: case for case in (CaseBlock.from_xml(node)
+                for node in self._get_template_xml().findall("{%s}case" % V2_NAMESPACE))}
+
+    def _get_template_xml(self):
         """get case xml from the blobdb
         """
-        pass
-
-    def save_template_xml(self, cases):
-        """saves to the blobdb
-        """
-        pass
+        with get_blob_db().get(self._blob_key) as file_obj:
+            template_xml = ElementTree.fromstring(file_obj.read())
+        return template_xml
 
 
 class CaseTemplateInstanceCase(models.Model):
-    template = models.ForeignKey(CaseTemplate, on_delete=models.CASCADE, related_name='cases')
+    template = models.ForeignKey(CaseTemplate, on_delete=models.CASCADE, related_name='instance_cases')
     case_id = models.CharField(max_length=255, unique=True, db_index=True)
+
+
+def get_case_hierarchy(case):
+    new_subcases = list(case.get_subcases())
+    all_subcases = []
+    while new_subcases:
+        all_subcases.extend(new_subcases)
+        new_subcases = [
+            subcase
+            for new_case in new_subcases
+            for subcase in list(new_case.get_subcases())
+            if list(new_case.get_subcases())
+        ]
+    return [case] + list(all_subcases)
---
+++ corehq/apps/case_templates/tests.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+import uuid
+
+from django.test import TestCase
+
+import six
+
+from casexml.apps.case.mock import CaseFactory, CaseIndex, CaseStructure
+
+from corehq.apps.case_templates.models import CaseTemplate
+from corehq.form_processor.tests.utils import (
+    FormProcessorTestUtils,
+    run_with_all_backends,
+)
+
+
+class CaseTemplateTests(TestCase):
+    def setUp(self):
+        super(CaseTemplateTests, self).setUp()
+        FormProcessorTestUtils.delete_all_cases()
+        self.domain = 'template-domain'
+        self.factory = CaseFactory(self.domain)
+        self.cases = self._create_case_structure()
+
+    def tearDown(self):
+        FormProcessorTestUtils.delete_all_cases()
+        super(CaseTemplateTests, self).tearDown()
+
+    def _create_case_structure(self):
+        self.parent_id = six.text_type(uuid.uuid4())
+        self.child_id = six.text_type(uuid.uuid4())
+        self.grandchild_id = six.text_type(uuid.uuid4())
+        parent = CaseStructure(
+            case_id=self.parent_id,
+            attrs={
+                'case_type': 'parent',
+                'update': {'name': 'mother', 'age': '61'},
+            },
+        )
+        child = CaseStructure(
+            case_id=self.child_id,
+            attrs={
+                'case_type': 'child',
+                'update': {'name': 'firstborn', 'age': '30'},
+            },
+            indices=[
+                CaseIndex(parent, identifier='parent'),
+            ],
+        )
+        grandchild = CaseStructure(
+            case_id=self.grandchild_id,
+            attrs={
+                'case_type': 'grandchild',
+                'update': {'name': 'baby', 'age': '3'},
+            },
+            indices=[
+                CaseIndex(child, identifier='parent'),
+            ],
+        )
+        return self.factory.create_or_update_cases([grandchild])
+
+    @run_with_all_backends
+    def test_create_template(self):
+        """Creating a template successfully stores the whole tree of cases given a specific root
+        """
+        template = CaseTemplate.create(self.domain, self.parent_id, 'template')
+        template_cases = template.prototype_cases
+
+        self.assertItemsEqual([self.parent_id, self.child_id, self.grandchild_id], template_cases.keys())
+
+        self.assertEqual(template_cases[self.child_id].case_name, 'firstborn')
+        self.assertEqual(template_cases[self.child_id].update['age'], '30')
+        self.assertEqual(template_cases[self.child_id].index['parent'].case_id, self.parent_id)
+
+        self.assertEqual(template_cases[self.grandchild_id].case_name, 'baby')
+        self.assertEqual(template_cases[self.grandchild_id].update['age'], '3')
+        self.assertEqual(template_cases[self.grandchild_id].index['parent'].case_id, self.child_id)
+
+    @run_with_all_backends
+    def test_create_template_children_only(self):
+        """Creating a template successfully stores only the children of the given root
+        """
+        template = CaseTemplate.create(self.domain, self.child_id, 'template')
+        self.assertItemsEqual([self.child_id, self.grandchild_id], template.prototype_cases.keys())
+
+    def test_get_instances(self):
+        # Get all the instances created by a template
+        pass
+
+    def test_delete_template(self):
+        # Deleting a template deletes all instances and forms against them
+        pass
+
+
+class CaseTemplateInstanceTests(TestCase):
+
+    def test_create_instance_from_template(self):
+        # With some arbitrary case XML, create an instance
+        # Ensure ownership is the same
+        # Ensure all case relationships are the same
+        pass
+
+    def test_name_increments(self):
+        # With multiple instances, name increments correctly
+        pass
