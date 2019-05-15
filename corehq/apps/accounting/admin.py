0a9c5f50db0f51074159430e31d8030c1906b522
Sravan Reddy <sravfeyn@gmail.com>
2019-05-15 11:07:50 +0530

    use decorator

--- corehq/apps/accounting/admin.py
+++ corehq/apps/accounting/admin.py
@@ -6,10 +6,8 @@ from django.contrib import admin
 from .models import DomainUserHistory
 
 
+@admin.register(DomainUserHistory)
 class DomainUserHistoryAdmin(admin.ModelAdmin):
     model = DomainUserHistory
     list_display = ('domain', 'record_date', 'num_users')
     list_filter = ('domain', 'record_date')
-
-
-admin.site.register(DomainUserHistory, DomainUserHistoryAdmin)
