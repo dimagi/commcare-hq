bb5ba7e3b701609281da935f549da19842cf8ab5
Simon Kelly <skelly@dimagi.com>
2019-05-16 17:05:43 +0200

    split command and change approach

---
+++ custom/icds_reports/management/commands/generate_migration_tables.py
@@ -0,0 +1,119 @@
+from __future__ import absolute_import, print_function
+from __future__ import unicode_literals
+
+import logging
+import re
+
+from django.core.management.base import BaseCommand
+from sqlalchemy import inspect as sqlinspect
+
+from corehq.apps.userreports.models import StaticDataSourceConfiguration
+from corehq.apps.userreports.util import get_indicator_adapter, UCR_TABLE_PREFIX
+from corehq.sql_db.connections import connection_manager
+from custom.icds_reports.management.commands.create_citus_child_tables import keep_child_tables, plain_tables, \
+    drop_child_tables, get_parent_child_mapping
+from custom.icds_reports.models import AggregateSQLProfile
+
+logger = logging.getLogger(__name__)
+
+
+IGNORE_TABLES = {
+    'django_migrations',
+    AggregateSQLProfile._meta.db_table,
+    'ucr_table_name_mapping',
+}
+
+
+def get_all_tables(connection):
+    res = connection.execute("select tablename from pg_tables where schemaname = 'public'")
+    return {row.tablename for row in res}
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--source-db-alias', default='icds-ucr',
+            help='Django alias for source database'
+        )
+
+    def handle(self, source_db_alias, **options):
+        with connection_manager.get_engine(source_db_alias).begin() as conn:
+            self.parent_child_mapping = get_parent_child_mapping(conn)
+            self.child_parent_mapping = {
+                child: parent
+                for parent, children in self.parent_child_mapping.items()
+                for child in children
+            }
+
+        self.generate_dump_script(source_db_alias)
+
+    def generate_dump_script(self, source_db_alias):
+        self.seen_tables = set()
+
+        source_engine = connection_manager.get_engine(source_db_alias)
+        # direct dump and load from parent + child tables
+        with source_engine.begin() as source_conn:
+            insp = sqlinspect(source_conn)
+            for table in keep_child_tables + plain_tables:
+                for line in self.get_table_date_target(insp, table):
+                    self.stdout.write(','.join(line))
+
+            # direct dump and load from parent
+            # dump from all child tables into parent table
+            for table in drop_child_tables:
+                for line in self.get_table_date_target(insp, table, all_in_parent=True):
+                    self.stdout.write(','.join(line))
+
+            for datasource in StaticDataSourceConfiguration.by_domain('icds-cas'):
+                adapter = get_indicator_adapter(datasource)
+                table_name = adapter.get_table().name
+
+                # direct dump and load from parent
+                # dump from all child tables into parent table
+                #  - if table is distrubuted, citus will distribute the data
+                #  - if table is partitioned the triggers on the parent will distribute the data
+                for line in self.get_table_date_target(insp, table_name, all_in_parent=True):
+                    self.stdout.write(','.join(line))
+
+            all_tables = get_all_tables(source_conn)
+            remaining_tables = all_tables - self.seen_tables - IGNORE_TABLES
+            icds_ucr_prefix = '{}{}_'.format(UCR_TABLE_PREFIX, 'icds-cas')
+
+            def keep_table(table):
+                root_table = self.child_parent_mapping.get(table, table)
+                return not root_table.startswith(UCR_TABLE_PREFIX) or root_table.startswith(icds_ucr_prefix)
+
+            remaining_tables = filter(keep_table, remaining_tables)
+
+            if remaining_tables:
+                self.stderr.write("Some tables not seen:")
+                for t in remaining_tables:
+                    parent = self.child_parent_mapping.get(t)
+                    if parent:
+                        self.stderr.write("\t{} (parent: {})".format(t, parent))
+                    else:
+                        self.stderr.write("\t{}".format(t))
+
+    def get_table_date_target(self, sql_insepctor, table, all_in_parent=False):
+        yield table, '', ''
+        self.seen_tables.add(table)
+        for child in self.parent_child_mapping[table]:
+            self.seen_tables.add(child)
+            yield child, get_table_date(sql_insepctor, child), table if all_in_parent else ''
+
+
+def get_table_date(sql_insepctor, table):
+    def _get_date(string):
+        match = re.match(r'.*(\d{4}-\d{2}-\d{2}).*', string)
+        if match:
+            return match.groups()[0]
+
+    date = _get_date(table)
+    if not date:
+        constraints = [
+            constraint for constraint in sql_insepctor.get_check_constraints(table)
+            if constraint['name'].startswith(table)
+        ]
+        if constraints:
+            date = _get_date(constraints[0]['sqltext'])
+    return date or ''
---
+++ custom/icds_reports/management/commands/migrate_tables.py
@@ -0,0 +1,148 @@
+from __future__ import absolute_import, print_function
+from __future__ import unicode_literals
+
+import argparse
+import inspect
+import logging
+import subprocess
+import sys
+from datetime import datetime, date
+
+import csv342 as csv
+from six.moves import input
+
+logger = logging.getLogger(__name__)
+
+
+def parse_date(s, default=None):
+    if not s:
+        return default
+
+    try:
+        return datetime.strptime(s, "%Y-%m-%d").date().replace(day=1)
+    except ValueError:
+        msg = "Not a valid date: '{0}'.".format(s)
+        raise argparse.ArgumentTypeError(msg)
+
+
+def run_migration(
+    table_path, source_db,
+    target_db, target_host, target_user,
+    start_date, end_date,
+    confirm, dry_run
+):
+    with open(table_path, 'r') as file:
+        tables = [
+            (source_table, parse_date(date_str, date.max), target_table)
+            for source_table, date_str, target_table in csv.reader(file)
+        ]
+
+    filtered_tables = filter_tables_by_date(tables, start_date, end_date)
+
+    if dry_run or _confirm('Preparing to migrate {} tables.'.format(len(filtered_tables))):
+        migrate_tables(
+            filtered_tables, source_db, target_db,
+            target_host, target_user, confirm, dry_run
+        )
+
+
+def migrate_tables(tables, source_db, target_db, target_host, target_user, confirm, dry_run):
+    commands = get_dump_load_commands(tables, source_db, target_db, target_host, target_user)
+    for source_table, target_table, cmd_parts in commands:
+        print(' '.join(cmd_parts))
+        if not dry_run and (not confirm or _confirm('Migrate {} to {}'.format(source_table, target_table))):
+            code = subprocess.call(cmd_parts, shell=True)
+            if code != 0:
+                sys.exit(code)
+
+
+def filter_tables_by_date(tables, start_date, end_date):
+    return [
+        (source_table, target_table) for source_table, table_date, target_table in tables
+        if (not start_date or table_date >= start_date) and (not end_date or table_date < end_date)
+    ]
+
+
+def get_dump_load_commands(tables, source_db, target_db, target_host, target_user):
+    dump_opts = ['--data-only', '--no-acl']
+    load_opts = ['-h', target_host, '-U', target_user, target_db]
+    for source_table, target_table in tables:
+        cmd_parts = ['pg_dump', '-t', source_table, source_db] + dump_opts
+        if target_table:
+            cmd_parts += ['|', 'sed', '"s/{}/{}/g"'.format(source_table, target_table)]
+        cmd_parts += ['|', 'psql'] + load_opts
+        yield source_table, target_table, cmd_parts
+
+
+def _confirm(msg):
+    confirm_update = input(msg + ' [yes / no] ')
+    return confirm_update == 'yes'
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Migrate DB tables from one DB to another using pg_dump")
+    parser.add_argument(
+        'table_path',
+        help=inspect.cleandoc("""
+        Path to list file containing list of tables formatted as CSV.
+        File should have 3 columns in this order: source_table_name,table_date,target_table_name
+            source_table_name: name of table in source DB
+            table_date: For tables partitioned by month this should be the month of the data
+                        in the table e.g. 2018-03-01
+            target_table_name: name of the table to load the data into in the target database
+        """)
+    )
+    parser.add_argument(
+        '-d', '--source-db',
+        required=True,
+        help='Name for source database'
+    )
+    parser.add_argument(
+        '-t', '--target-db',
+        required=True,
+        help='Name for target database'
+    )
+    parser.add_argument(
+        '-o', '--target-host',
+        required=True,
+        help='Name for target database'
+    )
+    parser.add_argument(
+        '-u', '--target-user',
+        required=True,
+        help='PG user to connect to target DB as. This user should be able to connect to the target'
+             'DB without a password.',
+    )
+    parser.add_argument(
+        '--start-date',
+        type=parse_date,
+        help='Only migrate tables with date on or after this date. Format YYYY-MM-DD',
+    )
+    parser.add_argument(
+        '--end-date',
+        type=parse_date,
+        help='Only migrate tables with date before this date. Format YYYY-MM-DD',
+    )
+    parser.add_argument(
+        '--confirm',
+        action='store_true',
+        help='Confirm before each table.',
+    )
+    parser.add_argument(
+        '--dry-run',
+        action='store_true',
+        help='Only output the commands.',
+    )
+
+    args = parser.parse_args()
+
+    run_migration(
+        args.table_path, args.source_db,
+        args.target_db, args.target_host, args.target_user,
+        args.start_date, args.end_date,
+        args.confirm, args.dry_run
+    )
+
+
+if __name__ == "__main__":
+    main()
