3b9e05e11b7135ad731179935ec85325fb80739c
Simon Kelly <skelly@dimagi.com>
2019-05-21 17:39:10 +0200

    add progress and args for source host + user

--- custom/icds_reports/scripts/migrate_tables.py
+++ custom/icds_reports/scripts/migrate_tables.py
@@ -25,7 +25,7 @@ def parse_date(s, default=None):
 
 
 def run_migration(
-    table_path, source_db,
+    table_path, source_db, source_host, source_user,
     target_db, target_host, target_user,
     start_date, end_date,
     start_table, only_table, confirm, dry_run
@@ -42,7 +42,7 @@ def run_migration(
         if not start_index:
             raise Exception('start table not found')
         else:
-            start_index = start_index[0]
+            start_index = start_index[0] + 1
             filtered_tables = filtered_tables[start_index:]
 
     if only_table:
@@ -54,19 +54,49 @@ def run_migration(
 
     if dry_run or _confirm('Preparing to migrate {} tables.'.format(len(filtered_tables))):
         migrate_tables(
-            filtered_tables, source_db, target_db,
-            target_host, target_user, confirm, dry_run
+            filtered_tables, source_db, source_host, source_user,
+            target_db, target_host, target_user,
+            confirm, dry_run
         )
 
 
-def migrate_tables(tables, source_db, target_db, target_host, target_user, confirm, dry_run):
-    commands = get_dump_load_commands(tables, source_db, target_db, target_host, target_user)
+def get_table_sizes(source_db, source_host, source_user):
+    try:
+        from sqlalchemy import create_engine
+    except ImportError:
+        print('sqlalchemy not installed. Progress not supported.')
+        return
+
+    engine = create_engine("postgresql://{}:@{}/{}".format(source_user, source_host, source_db))
+    with engine.begin() as conn:
+        res = conn.execute("select relname, n_live_tup from pg_stat_user_tables")
+        return {
+            row.relname: row.n_live_tup
+            for row in res
+        }
+
+
+def migrate_tables(tables, source_db, source_host, source_user, target_db, target_host, target_user, confirm, dry_run):
+    table_sizes = get_table_sizes(source_db, source_host, source_user)
+
+    total_size = None
+    progress = float(0)
+    if table_sizes is not None:
+        source_tables = {source_table for source_table, target_table in tables}
+        total_size = sum([size for table, size in table_sizes.items() if table in source_tables])
+
+    commands = get_dump_load_commands(tables, source_db, source_host, source_user, target_db, target_host,
+                                      target_user)
+
     for source_table, target_table, cmd in commands:
         print(cmd)
         if not dry_run and (not confirm or _confirm('Migrate {} to {}'.format(source_table, target_table))):
             code = subprocess.call(cmd, shell=True)
             if code != 0:
                 sys.exit(code)
+        if table_sizes:
+            progress += table_sizes[source_table]
+            print('Progress: {:.1f} ({} of {})'.format(100 * progress / total_size, progress, total_size))
 
 
 def filter_tables_by_date(tables, start_date, end_date):
@@ -76,9 +106,11 @@ def filter_tables_by_date(tables, start_date, end_date):
     ]
 
 
-def get_dump_load_commands(tables, source_db, target_db, target_host, target_user):
+def get_dump_load_commands(tables, source_db, source_host, source_user, target_db, target_host, target_user):
     for source_table, target_table in tables:
-        cmd = 'pg_dump -t {} {} --data-only --no-acl'.format(source_table, source_db)
+        cmd = 'pg_dump -h {} -U {} -t {} {} --data-only --no-acl'.format(
+            source_host, source_user, source_table, source_db
+        )
         if target_table:
             cmd += ' | sed "s/{}/{}/g"'.format(source_table, target_table)
         cmd += ' | psql -h {} -U {} -v ON_ERROR_STOP=1 {} --single-transaction'.format(
@@ -107,12 +139,22 @@ def main():
         """)
     )
     parser.add_argument(
-        '-d', '--source-db',
+        '-D', '--source-db',
         required=True,
         help='Name for source database'
     )
     parser.add_argument(
-        '-t', '--target-db',
+        '-O', '--source-host',
+        required=True,
+        help='Name for source database'
+    )
+    parser.add_argument(
+        '-U', '--source-user',
+        required=True,
+        help='Name for source database'
+    )
+    parser.add_argument(
+        '-d', '--target-db',
         required=True,
         help='Name for target database'
     )
@@ -138,8 +180,8 @@ def main():
         help='Only migrate tables with date before this date. Format YYYY-MM-DD',
     )
     parser.add_argument(
-        '--start-table',
-        help='Skip all tables up to this one',
+        '--start-after-table',
+        help='Skip all tables up to and including this one',
     )
     parser.add_argument(
         '--table',
@@ -159,10 +201,10 @@ def main():
     args = parser.parse_args()
 
     run_migration(
-        args.table_path, args.source_db,
+        args.table_path, args.source_db, args.source_host, args.source_user,
         args.target_db, args.target_host, args.target_user,
         args.start_date, args.end_date,
-        args.start_table, args.table, args.confirm, args.dry_run
+        args.start_after_table, args.table, args.confirm, args.dry_run
     )
 
 
