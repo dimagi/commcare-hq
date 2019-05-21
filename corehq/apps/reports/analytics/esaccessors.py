e0bd9f2081d7424ef9ab7d75e8e64e93f3004cf6
Biyeun Buczyk <biyeun@dimagi.com>
2019-05-21 12:07:53 +0200

    add ability to use the new case search index in get_case_types_for_domain_es

--- corehq/apps/reports/analytics/esaccessors.py
+++ corehq/apps/reports/analytics/esaccessors.py
@@ -4,7 +4,16 @@ from __future__ import unicode_literals
 from collections import defaultdict, namedtuple
 from datetime import datetime
 
-from corehq.apps.es import FormES, UserES, GroupES, CaseES, filters, aggregations, LedgerES
+from corehq.apps.es import (
+    FormES,
+    UserES,
+    GroupES,
+    CaseES,
+    filters,
+    aggregations,
+    LedgerES,
+    CaseSearchES,
+)
 from corehq.apps.es.aggregations import (
     TermsAggregation,
     ExtendedStatsAggregation,
@@ -653,9 +662,10 @@ def _get_attachment_dicts_from_form(form):
 
 
 @quickcache(['domain'], timeout=24 * 3600)
-def get_case_types_for_domain_es(domain):
+def get_case_types_for_domain_es(domain, use_case_search=False):
+    index_class = CaseSearchES if use_case_search else CaseES
     query = (
-        CaseES().domain(domain).size(0)
+        index_class().domain(domain).size(0)
         .terms_aggregation("type.exact", "case_types")
     )
     return set(query.run().aggregations.case_types.keys)
