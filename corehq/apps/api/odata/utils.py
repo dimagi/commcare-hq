45a3cd394a654d0cb5532c370922391f328c42bc
Nick Pellegrino <npellegrino@dimagi.com>
2019-05-21 10:32:58 -0400

    WIP

--- corehq/apps/api/odata/utils.py
+++ corehq/apps/api/odata/utils.py
@@ -3,8 +3,9 @@ from __future__ import unicode_literals
 
 from collections import defaultdict
 
-from corehq.apps.export.dbaccessors import get_latest_case_export_schema
-from corehq.apps.export.models import CaseExportDataSchema
+from corehq.apps.app_manager.dbaccessors import get_current_app
+from corehq.apps.export.dbaccessors import get_latest_case_export_schema, get_latest_form_export_schema
+from corehq.apps.export.models import CaseExportDataSchema, ExportItem, FormExportDataSchema
 from corehq.apps.reports.analytics.esaccessors import get_case_types_for_domain_es
 
 
@@ -23,3 +24,22 @@ def get_case_type_to_properties(domain):
             cleaned_case_property = export_group_schema.label.replace('_', '')
             case_type_to_properties[case_type].append(cleaned_case_property)
     return dict(case_type_to_properties)
+
+
+def get_xmlns_by_app(domain, app_id):
+    app = get_current_app(domain, app_id)
+    xmlnss = [form.xmlns.split('/')[-1] for form in app.get_forms()]
+    print(xmlnss)
+    return xmlnss
+
+
+def get_properties_by_xmlns(domain, app_id, xmlns):
+    complete_xmlns = 'http://openrosa.org/formdesigner/' + xmlns
+    form_export_schema = get_latest_form_export_schema(
+        domain, app_id, complete_xmlns
+    ) or FormExportDataSchema.generate_schema_from_builds(domain, app_id, complete_xmlns)
+    export_items = [
+        item for item in form_export_schema.group_schemas[0].items
+        if isinstance(item, ExportItem)
+    ]
+    return [item.label.split('.')[-1] for item in export_items]
