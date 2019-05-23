65abccfaf90547970487b7dd65530f3109747eac
Simon Kelly <skelly@dimagi.com>
2019-05-23 16:18:59 +0200

    handle skips when get_session_key raises exception

--- custom/icds_reports/cache.py
+++ custom/icds_reports/cache.py
@@ -1,23 +1,33 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-from quickcache import get_quickcache
+from quickcache import get_quickcache, ForceSkipCache
 from quickcache.django_quickcache import tiered_django_cache
 
 from corehq.sql_db.routers import forced_citus
 from corehq.util.quickcache import quickcache_soft_assert, get_session_key
 
 
-def get_cache_prefix():
-    prefix = get_session_key()
-    if forced_citus():
-        prefix += 'c'
-    return prefix
+def get_cache_prefix_func(catch_force_skip=False):
+    def prefix_func():
+        try:
+            prefix = get_session_key()
+        except ForceSkipCache:
+            if not catch_force_skip:
+                raise
+            else:
+                prefix = ''
+
+        if forced_citus():
+            prefix += 'c'
+        return prefix
+
+    return prefix_func
 
 
 cache = tiered_django_cache([
-    ('locmem', 10, get_cache_prefix),
-    ('default', 5 * 60, get_cache_prefix)
+    ('locmem', 10, get_cache_prefix_func()),
+    ('default', 5 * 60, get_cache_prefix_func(catch_force_skip=True))
 ])
 
 
