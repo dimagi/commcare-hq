9fa5a7fca9fc4126ead61946b9c25c0e9760dc32
mkangia <mkangia@dimagi.com>
2019-05-24 22:31:26 +0530

    update validator to allow - and _

--- custom/icds/validators.py
+++ custom/icds/validators.py
@@ -5,8 +5,8 @@ from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 
 
-LowercaseAlphanumericValidator = RegexValidator(
-    r'^[a-z0-9]*$',
-    message=_('must be lowercase alphanumeric'),
-    code='invalid_lowercase_alphanumeric'
+HostedCCZLinkIdentifierValidator = RegexValidator(
+    r'^[a-z0-9_-]*$',
+    message=_('must contain lowercase alphanumeric or - or _'),
+    code='invalid_hosted_ccz_link_identifier'
 )
