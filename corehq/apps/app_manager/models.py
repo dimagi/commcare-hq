315e53b2e597a7d5d37a34cc111f8d622a2ba2b5
Jennifer Schweers <orange.jenny@gmail.com>
2019-05-24 08:53:33 -0400

    Removed logic to update multimedia versions

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -4799,6 +4799,7 @@ class Application(ApplicationBase, TranslationMixin, ApplicationMediaMixin,
         """
 
         # access to .multimedia_map is slow
+        '''
         previous_version = self.get_previous_version()
         prev_multimedia_map = previous_version.multimedia_map if previous_version else {}
 
@@ -4812,6 +4813,7 @@ class Application(ApplicationBase, TranslationMixin, ApplicationMediaMixin,
                 map_item.version = prev_map_item.version
             else:
                 map_item.version = self.version
+        '''
 
     def ensure_module_unique_ids(self, should_save=False):
         """
