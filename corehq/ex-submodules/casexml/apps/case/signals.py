469ccefd73adfe7d591b36bf7885359d4ff7dfcf
mkangia <mkangia@dimagi.com>
2019-05-31 23:06:04 +0530

    fix/use better condition

--- corehq/ex-submodules/casexml/apps/case/signals.py
+++ corehq/ex-submodules/casexml/apps/case/signals.py
@@ -6,7 +6,7 @@ from couchforms.signals import xform_archived, xform_unarchived
 
 
 def rebuild_form_cases(sender, xform, *args, **kwargs):
-    if not kwargs.get('rebuild_cases'):
+    if kwargs.get('rebuild_cases') is False:
         return
     from casexml.apps.case.xform import get_case_ids_from_form
     from casexml.apps.case.cleanup import rebuild_case_from_forms
