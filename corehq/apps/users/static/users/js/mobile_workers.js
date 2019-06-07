c2c1f6f986b9a7fcf7afad001f8bc5d2262ed562
Jennifer Schweers <orange.jenny@gmail.com>
2019-06-07 17:10:55 -0400

    Added support for multiple new mobile workers

--- corehq/apps/users/static/users/js/mobile_workers.js
+++ corehq/apps/users/static/users/js/mobile_workers.js
@@ -1,9 +1,29 @@
 /* global RMI */
 hqDefine("users/js/mobile_workers", function () {
+    // TODO: rename to something more specific
+    var STATUS = {
+        NEW: 'new',
+        PENDING: 'pending',
+        WARNING: 'warning',
+        SUCCESS: 'success',
+        RETRIED: 'retried',
+    };
+
     var rmi = function () {};
 
     // TODO: rename to mobileWorkerModel for consistency
     var mobileWorker = function (options) {
+        options = options || {};
+        options = _.defaults(options, {
+            creationStatus: STATUS.NEW,     // only relevant for new mobile workers
+            username: '',
+            first_name: '',
+            last_name: '',
+            location_id: '',
+            password: '',
+            user_id: '',
+            is_active: true,
+        });
         var self = ko.mapping.fromJS(options);
 
         self.action_error = ko.observable('');
@@ -108,32 +128,13 @@ hqDefine("users/js/mobile_workers", function () {
         return self;
     };
 
-    // TODO: rename to something more specific
-    var STATUS = {
-        NEW: 'new',
-        PENDING: 'pending',
-        WARNING: 'warning',
-        SUCCESS: 'success',
-        RETRIED: 'retried',
-    };
-
     // TODO: rename worker / mobile worker to "user" everywhere?
     var mobileWorkerCreationModel = function (options) {
         hqImport("hqwebapp/js/assert_properties").assertRequired(options, ['location_url', 'require_location_id']);
 
         var self = {};
 
-        self.newMobileWorker = mobileWorker({
-            user_id: '',
-            first_name: 'my first name',
-            last_name: 'my last name',
-            username: 'me',
-            password: '',
-            is_active: true,
-            location_id: '',
-            creationStatus: STATUS.NEW,
-        });
-
+        self.newMobileWorker = ko.observable(mobileWorker());
         self.newMobileWorkers = ko.observableArray();
 
         self.initializeMobileWorker = function () {
@@ -167,9 +168,9 @@ hqDefine("users/js/mobile_workers", function () {
         };
 
         self.allowSubmit = ko.computed(function () {
-            var isValid = self.newMobileWorker.username() && self.newMobileWorker.password();
+            var isValid = self.newMobileWorker().username() && self.newMobileWorker().password();
             if (options.require_location_id) {
-                isValid = isValid && self.newMobileWorker.location_id();
+                isValid = isValid && self.newMobileWorker().location_id();
             }
             // TODO: also require that usernameAvailabilityStatus === 'available'
             return isValid;
@@ -177,27 +178,27 @@ hqDefine("users/js/mobile_workers", function () {
 
         self.submitNewMobileWorker = function () {
             $("#newMobileWorkerModal").modal('hide');
-            self.newMobileWorkers.push(self.newMobileWorker);
-            self.newMobileWorker.creationStatus(STATUS.PENDING);
+            var submittedMobileWorker = mobileWorker(ko.mapping.toJS(self.newMobileWorker));
+            self.newMobileWorker(mobileWorker());
+            self.newMobileWorkers.push(submittedMobileWorker);
+            submittedMobileWorker.creationStatus(STATUS.PENDING);
             if (hqImport("hqwebapp/js/initial_page_data").get("implement_password_obfuscation")) {
                 // TODO: draconian password requirements
                 //newWorker.password = (hqImport("nic_compliance/js/encoder")()).encode(newWorker.password);
             }
             rmi('create_mobile_worker', {
-                mobileWorker: ko.mapping.toJS(self.newMobileWorker),
+                mobileWorker: ko.mapping.toJS(submittedMobileWorker),
             })
             .done(function (data) {
                 if (data.success) {
-                    ko.mapping.fromJS({
-                        user_id: data.user_id,
-                    }, self.newMobileWorker);
-                    self.newMobileWorker.creationStatus(STATUS.SUCCESS);
+                    submittedMobileWorker.user_id(data.user_id);
+                    submittedMobileWorker.creationStatus(STATUS.SUCCESS);
                 } else {
-                    self.newMobileWorker.creationStatus(STATUS.WARNING);
+                    submittedMobileWorker.creationStatus(STATUS.WARNING);
                 }
             })
             .fail(function () {
-                self.newMobileWorker.creationStatus(STATUS.WARNING);
+                submittedMobileWorker.creationStatus(STATUS.WARNING);
             });
         };
 
