2398e1db8a61030e9eeb9fb592bf56bb7ba1568c
Daniel Miller <dmiller@dimagi.com>
2019-06-10 11:53:43 -0400

    Clean up

--- scripts/test-make-requirements.sh
+++ scripts/test-make-requirements.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# This is a dirty test. It can leave some files in a different state
-# than they were when it started.
 make requirements
 git diff
 git update-index -q --refresh
@@ -12,5 +10,6 @@ if git diff-index --quiet HEAD --; then
 else
     # Changes
     echo -e "\033[0;31mRequirements are inconsistent.  Did you run 'make requirements'?\033[0m"
+    git checkout requirements/ requirements-py3/  # clean up
     exit 1
 fi
