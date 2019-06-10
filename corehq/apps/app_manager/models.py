a381284192b91a4d296fbe34803d273b287be775
Jennifer Schweers <orange.jenny@gmail.com>
2019-06-10 18:38:32 -0400

    Added handling of upstream_app_id
    - Added logic to fetch all possible masters for a given linked app
    - Added dropdown of all possible masters to app settings page to select
      which master to pull. Only show if the linked app has multiple master
      apps. The pulling itself is not yet implemented.
    - Added link to master app to each build on releases page
    - Stop using the old 'master' property in tests

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -5561,7 +5561,6 @@ class LinkedApplication(Application):
     """
     An app that can pull changes from an app in a different domain.
     """
-    master = StringProperty()  # Legacy, should be removed once all linked apps support multiple masters
     upstream_app_id = StringProperty()  # ID of the app that was most recently pulled
     upstream_version = IntegerProperty()  # Version of the app that was most recently pulled
 
@@ -5583,6 +5582,12 @@ class LinkedApplication(Application):
     def domain_link(self):
         from corehq.apps.linked_domain.dbaccessors import get_domain_master_link
         return get_domain_master_link(self.domain)
+
+    @memoized
+    def get_master_app_briefs(self):
+        if self.domain_link:
+            return get_master_app_briefs(self.domain_link)
+        return []
 
     def get_master_version(self):
         if self.domain_link:
