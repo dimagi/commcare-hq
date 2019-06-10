966354af720e49993a3481971da63d92f0d83eda
Nick Pellegrino <npellegrino@dimagi.com>
2019-06-10 12:05:20 -0400

    add unit tests for get_odata_property_from_export_item

---
+++ corehq/apps/api/odata/tests/test_utils.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase
+
+from corehq.apps.api.odata.utils import get_odata_property_from_export_item
+from corehq.apps.export.models import ExportItem, PathNode
+
+
+class TestOdataFromExportItem(SimpleTestCase):
+
+    def test_get_odata_property_from_export_item(self):
+        self.assertEqual(
+            get_odata_property_from_export_item(ExportItem(label='x.y.name')),
+            'name'
+        )
+
+    def test_get_odata_property_from_export_item_with_hashtag(self):
+        self.assertEqual(
+            get_odata_property_from_export_item(ExportItem(label='x.y.#name')),
+            'name'
+        )
+
+    def test_get_odata_property_from_export_item_at_sign(self):
+        self.assertEqual(
+            get_odata_property_from_export_item(ExportItem(label='x.y.@name')),
+            'name'
+        )
