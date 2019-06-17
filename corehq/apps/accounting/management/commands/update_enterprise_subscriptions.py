b317e0fb5bfbf9fd160e3da3b7d2fc906fa528aa
Nick Pellegrino <npellegrino@dimagi.com>
2019-06-17 14:38:21 -0400

    just update

--- corehq/apps/accounting/management/commands/update_enterprise_subscriptions.py
+++ corehq/apps/accounting/management/commands/update_enterprise_subscriptions.py
@@ -48,24 +48,14 @@ class Command(BaseCommand):
         print('Proceeding...')
 
         with transaction.atomic():
-            april_subscriptions = self.get_april_subscriptions_queryset(account, plan, april_and_may_plan_version)
-            for april_subscription in with_progress_bar(april_subscriptions):
-                original_end_date = april_subscription.date_end
-                new_end_date = max(APRIL_1, april_subscription.date_start)
-                april_subscription.change_plan(
-                    april_and_may_plan_version, new_end_date, date_end=original_end_date,
-                    note='CRS Enterprise after April 1 2019'
-                )
+            self.get_april_subscriptions_queryset(account, plan, april_and_may_plan_version).update(
+                plan_version=april_and_may_plan_version
+            )
 
         with transaction.atomic():
-            may_subscriptions = self.get_may_subscriptions_queryset(account, plan, april_and_may_plan_version)
-            for may_subscription in with_progress_bar(may_subscriptions):
-                original_end_date = may_subscription.date_end
-                new_end_date = max(MAY_1, may_subscription.date_start)
-                may_subscription.change_plan(
-                    april_and_may_plan_version, new_end_date, date_end=original_end_date,
-                    note='CRS Enterprise after May 1 2019'
-                )
+            self.get_may_subscriptions_queryset(account, plan, april_and_may_plan_version).update(
+                plan_version=april_and_may_plan_version
+            )
 
     @staticmethod
     def get_april_subscriptions_queryset(account, plan, april_plan_version):
