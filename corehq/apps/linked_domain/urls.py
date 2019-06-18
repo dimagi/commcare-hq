c93fb65e08963a35bdaef97fe386b09d9cd01de9
Jennifer Schweers <orange.jenny@gmail.com>
2019-06-18 17:08:29 -0400

    Updated masters dropdown to display most recently released version

--- corehq/apps/linked_domain/urls.py
+++ corehq/apps/linked_domain/urls.py
@@ -9,6 +9,7 @@ from corehq.apps.linked_domain.views import (
     custom_data_models,
     DomainLinkRMIView,
     get_latest_released_app_source,
+    released_app_versions,
     toggles_and_previews,
     user_roles,
 )
@@ -21,6 +22,7 @@ urlpatterns = [
     url(r'^case_search_config/$', case_search_config, name='case_search_config'),
     url(r'^custom_data_models/$', custom_data_models, name='custom_data_models'),
     url(r'^toggles/$', toggles_and_previews, name='toggles'),
+    url(r'^released_app_versions/$', released_app_versions, name='released_app_versions'),
     url(r'^release_source/(?P<app_id>[\w-]+)/$', get_latest_released_app_source,
         name='latest_released_app_source'),
     url(r'^service/$', DomainLinkRMIView.as_view(), name=DomainLinkRMIView.urlname),
