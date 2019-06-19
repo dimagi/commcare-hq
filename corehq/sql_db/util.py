850791c04c5f8efad4f652d75ca373053f71cb62
Jonathan Emord <jonathan.emord@gmail.com>
2019-06-19 11:47:17 -0400

    Fix pk access

--- corehq/sql_db/util.py
+++ corehq/sql_db/util.py
@@ -113,7 +113,10 @@ def paginate_query_across_partitioned_databases(model_class, q_expression, annot
             while value < last_value:
                 filter_expression = {'{}__gt'.format(sort_col): value}
                 for row in qs.filter(**filter_expression)[:query_size]:
-                    value = row.pk
+                    if return_values:
+                        value = row[0]
+                    else:
+                        value = row.pk
                     yield row
 
 
