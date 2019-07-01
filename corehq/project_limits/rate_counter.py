c8629dee05a49fe7c6153de850b8bca89bf4f3f4
Daniel Roberts <droberts@dimagi.com>
2019-07-01 18:23:11 -0500

    Add and test efficient sliding window rate counter
    It works by breaking the window into "grains" with fixed period
    boundaries, and then treating the traffic from the earliest grain that's
    sliding out of the window as if it were even over the period. This
    trades off the accuracy of keeping the date of every event and the
    efficiecy of counting within fixed time buckets, and with enough grains
    it approaches the former.

---
+++ corehq/project_limits/rate_counter.py
@@ -0,0 +1,194 @@
+import abc
+import hashlib
+import time
+from datetime import timedelta
+
+import six
+from django.core.cache import caches
+
+REDIS = caches['default']
+LOCMEM = caches['locmem']
+
+
+class CounterCache(object):
+    def __init__(self, memoized_timeout, timeout, local_cache=LOCMEM, shared_cache=REDIS):
+        self.memoized_timeout = memoized_timeout
+        self.timeout = timeout
+        self.local_cache = local_cache
+        self.shared_cache = shared_cache
+
+    def incr(self, key, delta=1):
+        value = self.shared_cache.incr(key, delta)
+        if value == 1:
+            self.shared_cache.expire(key, timeout=self.timeout)
+        self.local_cache.set(key, value, timeout=self.memoized_timeout)
+        return value
+
+    def get(self, key, key_is_active=True):
+        """
+        :param key: Cache key
+        :param key_is_active: Whether you believe the key is being actively updated
+            If not, then use the longer timeout for local memory cache as well.
+        """
+        local_timeout = self.memoized_timeout if key_is_active else self.timeout
+        value = self.local_cache.get(key, default=None)
+        if value is None:
+            value = self.shared_cache.get(key, default=0)
+            self.local_cache.set(key, value, timeout=local_timeout)
+        assert value is not None
+        return value
+
+
+class AbstractRateCounter(six.with_metaclass(abc.ABCMeta)):
+    @abc.abstractmethod
+    def get(self, scope):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def increment(self, scope):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def increment_and_get(self, scope):
+        raise NotImplementedError()
+
+
+class FixedWindowRateCounter(AbstractRateCounter):
+    def __init__(self, key, window_duration, window_offset=0, keep_windows=1,
+                 memoize_timeout=15.0, _CounterCache=CounterCache):
+        """
+        :param key: short description of the window e.g. "week"
+        :param window_duration: length in seconds of the window
+        :param window_offset: offset from epoch of window boundary
+        :param keep_windows: number of windows to retain (including current one)
+        :param memoize_timeout: how long to memoize the information in memory
+            This is the upper limit on how long a `get` could return a stale value.
+        """
+        assert keep_windows >= 1
+        self.key = key
+        self.window_duration = window_duration
+        self.window_offset = window_offset
+        self.counter = _CounterCache(memoize_timeout, timeout=keep_windows * window_duration)
+
+    @staticmethod
+    def _digest(string):
+        return hashlib.sha1(string).hexdigest()
+
+    def _cache_key(self, scope, timestamp=None):
+        if timestamp is None:
+            timestamp = time.time()
+        return self._digest('fwrc-{}-{}-{}'.format(
+            self.key,
+            self._digest(scope),
+            int((timestamp - self.window_offset) / self.window_duration)
+        ))
+
+    def get(self, scope, timestamp=None, key_is_active=True):
+        return self.counter.get(self._cache_key(scope, timestamp), key_is_active=key_is_active)
+
+    def increment_and_get(self, scope, timestamp=None):
+        return self.counter.incr(self._cache_key(scope, timestamp))
+
+    def increment(self, scope, timestamp=None):
+        self.increment_and_get(scope, timestamp)
+
+
+class SlidingWindowOverFixedGrainsRateCounter(AbstractRateCounter):
+    def __init__(self, key, window_duration, window_offset=0, grains_per_window=1,
+                 memoize_timeout=15.0, _FixedWindowRateCounter=FixedWindowRateCounter):
+        """
+        A "Sliding Window Over Fixed Grains" approach that approximates perfect sliding window
+
+        The "Sliding Window Over Fixed Grains" approach approximates
+        the perfect sliding window approach (where you keep the timestamp of every event
+        and count the number that fall between now and now - window_duration)
+        by dividing time up into fixed "grains", i.e. sub-windows,
+        over which we slide the larger window.
+        For the earliest grain which is sliding out of the window,
+        we assume linear distribution of events over time, and thus compute its contribution
+        to the total as (% overlap of grain with our window) * (events in grain).
+
+        See a description of this approach (with grains_per_window=1) here:
+        https://konghq.com/blog/how-to-design-a-scalable-rate-limiting-algorithm/
+
+        :param key: short description of the window e.g. "week"
+        :param window_duration: length in seconds of the window
+        :param window_offset: offset from epoch of window boundary
+        :param grains_per_window: How many grains the window should be divided into
+            (higher number = more accuracy)
+        :param memoize_timeout: how long to memoize the information in memory
+            This is the upper limit on how long a `get` could return a stale value.
+        """
+        self.key = key
+        self.window_duration = window_duration
+        self.window_offset = window_offset
+        self.grains_per_window = grains_per_window
+        self.memoize_timeout = memoize_timeout
+        self.grain_duration = window_duration / grains_per_window
+        self.grain_counter = _FixedWindowRateCounter(
+            key=key,
+            window_duration=self.grain_duration,
+            window_offset=window_offset,
+            keep_windows=grains_per_window + 1,
+            memoize_timeout=memoize_timeout
+        )
+
+    def get(self, scope, timestamp=None):
+        if timestamp is None:
+            timestamp = time.time()
+        counts = [
+            self.grain_counter.get(scope, timestamp - i * self.grain_duration,
+                                   key_is_active=(i is 0))
+            for i in range(self.grains_per_window + 1)
+        ]
+        earliest_grain_count = counts.pop()
+        # This is the percentage of the way through the current grain we are
+        progress_in_current_grain = (timestamp % self.grain_duration) / self.grain_duration
+        # This is the count from the percentage of the earliest grain that should count
+        contribution_from_earliest = earliest_grain_count * (1 - progress_in_current_grain)
+        return sum(counts) + contribution_from_earliest
+
+    def increment(self, scope, timestamp=None):
+        # this intentionally doesn't return because this is the active grain count,
+        # not the total that would be returned by get
+        self.grain_counter.increment(scope, timestamp)
+
+    def increment_and_get(self, scope, timestamp=None):
+        self.increment(scope, timestamp)
+        return self.get(scope, timestamp)
+
+
+week_rate_counter = SlidingWindowOverFixedGrainsRateCounter(
+    'week',
+    timedelta(days=7).total_seconds(),
+    grains_per_window=7,
+    memoize_timeout=timedelta(hours=6).total_seconds()
+)
+
+day_rate_counter = SlidingWindowOverFixedGrainsRateCounter(
+    'day',
+    timedelta(days=1).total_seconds(),
+    grains_per_window=4,
+    memoize_timeout=timedelta(hours=6).total_seconds()
+)
+
+hour_rate_counter = SlidingWindowOverFixedGrainsRateCounter(
+    'hour',
+    timedelta(hours=1).total_seconds(),
+    grains_per_window=3,
+    memoize_timeout=timedelta(hours=1).total_seconds()
+)
+
+minute_rate_counter = SlidingWindowOverFixedGrainsRateCounter(
+    'hour',
+    timedelta(minutes=1).total_seconds(),
+    grains_per_window=2,
+    memoize_timeout=timedelta(minutes=1).total_seconds()
+)
+
+second_rate_counter = SlidingWindowOverFixedGrainsRateCounter(
+    'hour',
+    timedelta(seconds=1).total_seconds(),
+    grains_per_window=1,
+    memoize_timeout=timedelta(seconds=1).total_seconds()
+)
---
+++ corehq/project_limits/tests/test_rate_counter.py
@@ -0,0 +1,91 @@
+import functools
+from datetime import timedelta
+import testil
+from django.core.cache.backends.locmem import LocMemCache
+
+from corehq.project_limits.rate_counter import CounterCache, FixedWindowRateCounter, \
+    SlidingWindowOverFixedGrainsRateCounter
+
+
+class CacheForTesting(LocMemCache):
+    def incr(self, key, delta=1):
+        try:
+            return super(CacheForTesting, self).incr(key, delta)
+        except ValueError as e:
+            if str(e) == "Key '{}' not found".format(key):
+                self.set(key, 1)
+                return 1
+            raise
+
+    def expire(self, key, timeout):
+        pass
+
+
+_CounterCache = functools.partial(
+    CounterCache,
+    local_cache=CacheForTesting('local-test', {}),
+    shared_cache=CacheForTesting('shared-test', {}),
+)
+_FixedWindowRateCounter = functools.partial(
+    FixedWindowRateCounter,
+    _CounterCache=_CounterCache
+)
+_SlidingWindowOverFixedGrainsRateCounter = functools.partial(
+    SlidingWindowOverFixedGrainsRateCounter,
+    _FixedWindowRateCounter=_FixedWindowRateCounter
+)
+
+
+DAYS = timedelta(days=1).total_seconds()
+
+
+def test_fixed_window_rate_counter():
+    # this timestamp is chosen to be 6 days into a week window
+    timestamp = (1000 * 7 * DAYS + 6 * DAYS)
+    counter = _FixedWindowRateCounter('test-week', 7 * DAYS)
+    testil.eq(counter.increment_and_get('alice', timestamp), 1)
+    testil.eq(counter.increment_and_get('alice', timestamp), 2)
+    testil.eq(counter.increment_and_get('alice', timestamp), 3)
+
+    testil.eq(counter.increment_and_get('bob', timestamp), 1)
+
+    testil.eq(counter.get('alice', timestamp), 3)
+    testil.eq(counter.get('alice', timestamp - 6 * DAYS), 3)
+    testil.eq(counter.get('alice', timestamp - 7 * DAYS), 0)
+    testil.eq(counter.get('alice', timestamp + 1 * DAYS), 0)
+
+
+def float_eq(f1, f2, text=None):
+    f1 = int(f1 * 1000000) / 1000000.
+    f2 = int(f2 * 1000000) / 1000000.
+    testil.eq(f1, f2, text)
+
+
+def test_sliding_window_with_grains_rate_counter():
+    # this timestamp is chosen to be 6 days into a week window
+
+    timestamp = (1000 * 7 * DAYS + 6 * DAYS)
+    counter = _SlidingWindowOverFixedGrainsRateCounter('test-sliding-week', 7 * DAYS)
+    testil.eq(counter.increment_and_get('alice', timestamp), 1)
+    testil.eq(counter.increment_and_get('alice', timestamp), 2)
+    testil.eq(counter.increment_and_get('alice', timestamp), 3)
+
+    testil.eq(counter.increment_and_get('bob', timestamp), 1)
+
+    testil.eq(counter.get('alice', timestamp), 3)
+    testil.eq(counter.get('alice', timestamp - 6 * DAYS), 3)
+    testil.eq(counter.get('alice', timestamp - 7 * DAYS), 0)
+
+    # As time moves forward beyond the period boundary
+    # the value fades linearly to 0 by the end of the following period
+    float_eq(counter.get('alice', timestamp + 1 * DAYS), 3)
+    float_eq(counter.get('alice', timestamp + 2 * DAYS), 3 * 6./7)
+    float_eq(counter.get('alice', timestamp + 3 * DAYS), 3 * 5./7)
+    float_eq(counter.get('alice', timestamp + 4 * DAYS), 3 * 4./7)
+    float_eq(counter.get('alice', timestamp + 5 * DAYS), 3 * 3./7)
+    float_eq(counter.get('alice', timestamp + 6 * DAYS), 3 * 2./7)
+    float_eq(counter.get('alice', timestamp + 7 * DAYS), 3 * 1./7)
+    float_eq(counter.get('alice', timestamp + 8 * DAYS), 0)
+
+    float_eq(counter.increment_and_get('alice', timestamp + 1 * DAYS), 4)
+    float_eq(counter.get('alice', timestamp + 2 * DAYS), 3 * 6./7 + 1)
