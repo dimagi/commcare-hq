152264e5ff42d32161672e8ae2acf29fe44e93b7
Biyeun Buczyk <biyeun@dimagi.com>
2019-07-03 18:13:48 +0200

    elaborate on error message

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
@@ -642,7 +642,8 @@ Formplayer.Errors = {
     LOCK_TIMEOUT_ERROR: gettext('Another process prevented us from servicing your request. ' +
         'Please try again later.'),
     NO_INTERNET_ERROR: gettext("We have detected an issue with your network. " +
-        "Please check your Internet connection"),
+        "Please check your Internet connection and retry when connectivity " +
+        "improves."),
 };
 
 Formplayer.Utils.touchformsError = function (message) {
