6259423ef7eec5655d366316a53aaf24999796d7
Ethan Soergel <esoergel@gmail.com>
2019-07-04 14:29:40 -0400

    Remove old repeater migration and associated code

--- corehq/motech/repeaters/migrations/0001_adjust_auth_field_format.py
+++ corehq/motech/repeaters/migrations/0001_adjust_auth_field_format.py
@@ -1,25 +1,8 @@
-from __future__ import absolute_import
-from __future__ import unicode_literals
+from __future__ import absolute_import, unicode_literals
+
 from django.db import migrations
 
-from corehq.motech.repeaters.models import Repeater
-from corehq.motech.repeaters.utils import migrate_repeater
-
-from corehq.util.couch import iter_update, DocUpdate
-
-
-def migrate_auth_field(apps, schema_editor):
-    repeater_ids = [row['id'] for row in Repeater.view(
-        'repeaters/repeaters',
-        include_docs=False,
-        reduce=False,
-        wrap_doc=False
-    )]
-    iter_update(
-        db=Repeater.get_db(),
-        fn=migrate_repeater,
-        ids=repeater_ids,
-    )
+from corehq.util.django_migrations import noop_migration
 
 
 class Migration(migrations.Migration):
@@ -27,5 +10,5 @@ class Migration(migrations.Migration):
     dependencies = []
 
     operations = [
-        migrations.RunPython(migrate_auth_field),
+        noop_migration(),
     ]
