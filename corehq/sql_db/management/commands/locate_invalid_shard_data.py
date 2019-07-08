295f5c90d3401cf789c6242c6620a81388c15f0f
Jonathan Emord <jonathan.emord@gmail.com>
2019-07-08 15:51:51 -0400

    Add option to delete invalid shard data

--- corehq/sql_db/management/commands/locate_invalid_shard_data.py
+++ corehq/sql_db/management/commands/locate_invalid_shard_data.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, print_function, unicode_literals
 from django.core.management.base import BaseCommand
 
 from corehq.sql_db.shard_data_management import (
+    delete_unmatched_shard_data,
     get_count_of_unmatched_models_by_shard,
 )
 from corehq.sql_db.util import (
@@ -14,13 +15,23 @@ from corehq.sql_db.util import (
 class Command(BaseCommand):
     help = "Print out all shard data that exists in databases that don't contain the associated shards."
 
+    def add_arguments(self, parser):
+        parser.add_argument('--delete', action='store_true')
+
     def handle(self, **options):
         sharded_models = list(get_all_sharded_models())
         for database in get_db_aliases_for_partitioned_query():
             for model in sharded_models:
-                invalid_data = get_count_of_unmatched_models_by_shard(database, model)
-                if invalid_data:
-                    for shard_id, count in invalid_data:
-                        print('found {} unexpected {}s in {} (shard {}).'.format(
-                            count, model.__name__, database, shard_id)
+                if options['delete']:
+                    count = delete_unmatched_shard_data(database, model)
+                    if count:
+                        print('deleted {} {} from {}'.format(
+                            count, model.__name__, database)
                         )
+                else:
+                    invalid_data = get_count_of_unmatched_models_by_shard(database, model)
+                    if invalid_data:
+                        for shard_id, count in invalid_data:
+                            print('found {} unexpected {}s in {} (shard {}).'.format(
+                                count, model.__name__, database, shard_id)
+                            )
--- corehq/sql_db/shard_data_management.py
+++ corehq/sql_db/shard_data_management.py
@@ -99,6 +99,43 @@ def get_count_of_unmatched_models_by_shard(database, model):
     return results
 
 
+def delete_unmatched_shard_data(database, model):
+    """
+    Deletes any `model` data residing on shards that aren't expected to live in `database`.
+
+    Returns number of records deleted
+    """
+    field_type = model._meta.get_field(model.partition_attr)
+    if isinstance(field_type, UUIDField):
+        # magic: https://gist.github.com/cdmckay/a82261e48a42a3bbd78a
+        shard_id_function = (
+            "hash_string(decode(replace({id_field}::text, '-', ''), 'hex'), 'siphash24')"
+            " & {total_shard_count}"
+        ).format(
+            total_shard_count=partition_config.num_shards - 1,
+            id_field=model.partition_attr,
+        )
+    elif _is_a_string_field(field_type):
+        # todo: are there any other types we need to worry about?
+        shard_id_function = "hash_string({id_field}, 'siphash24') & {total_shard_count}".format(
+            total_shard_count=partition_config.num_shards - 1,
+            id_field=model.partition_attr,
+        )
+    else:
+        raise Exception('Tried to shard based on an unexpected field type: {}'.format(type(field_type)))
+    query = """
+    DELETE FROM {table_name}
+    WHERE NOT ({shard_id_function}) = ANY(%s);
+    """.format(
+        shard_id_function=shard_id_function,
+        table_name=model._meta.db_table,
+    )
+    valid_shards = partition_config.get_shards_on_db(database)
+    with connections[database].cursor() as cursor:
+        cursor.execute(query, [valid_shards])
+        return cursor.rowcount
+
+
 def get_count_of_models_by_shard_for_testing(database, model):
     cursor = connections[database].cursor()
     query = _get_counts_by_shard_query_for_testing(model)
