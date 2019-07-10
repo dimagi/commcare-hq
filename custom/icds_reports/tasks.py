2fd77150c4c03f1ad9f425b255d4ef8c182b90f5
Sravan Reddy <sravfeyn@gmail.com>
2019-07-10 12:35:02 +0530

    unused import

--- custom/icds_reports/tasks.py
+++ custom/icds_reports/tasks.py
@@ -12,7 +12,6 @@ from datetime import date, datetime, timedelta
 from io import BytesIO, open
 
 from django.conf import settings
-from django.core.cache import cache
 from django.db import Error, IntegrityError, connections, transaction
 from django.db.models import F
 
