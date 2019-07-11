d9b4121743f253b6d1f2b4032517f8f53fb70ed2
Nick Pellegrino <npellegrino@dimagi.com>
2019-07-11 14:22:47 -0400

    remove '\r' from xpath before validation

--- corehq/apps/app_manager/xpath_validator/wrapper.py
+++ corehq/apps/app_manager/xpath_validator/wrapper.py
@@ -17,7 +17,7 @@ def validate_xpath(xpath, allow_case_hashtags=False):
             cmd = ['node', path]
         p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
-        stdout, stderr = p.communicate(xpath.encode('utf-8'))
+        stdout, stderr = p.communicate(xpath.replace('\r', ' ').encode('utf-8'))
         exit_code = p.wait()
         if exit_code == 0:
             return XpathValidationResponse(is_valid=True, message=None)
