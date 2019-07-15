18779e5b9e1670fa3505390a9ad9c427a33e7fa5
Nick Pellegrino <npellegrino@dimagi.com>
2019-07-15 19:45:09 -0400

    clean OData columns in edited feed

--- corehq/apps/export/views/edit.py
+++ corehq/apps/export/views/edit.py
@@ -17,6 +17,7 @@ from corehq.apps.export.views.utils import (
     DailySavedExportMixin,
     DashboardFeedMixin,
     ODataFeedMixin,
+    clean_odata_columns,
     remove_row_number_from_export_columns,
 )
 
@@ -123,6 +124,7 @@ class EditODataCaseFeedView(ODataFeedMixin, EditNewCustomCaseExportView):
     def get_export_instance(self, schema, original_export_instance):
         export_instance = super(EditODataCaseFeedView, self).get_export_instance(schema, original_export_instance)
         remove_row_number_from_export_columns(export_instance)
+        clean_odata_columns(export_instance)
         return export_instance
 
 
@@ -133,6 +135,7 @@ class EditODataFormFeedView(ODataFeedMixin, EditNewCustomFormExportView):
     def get_export_instance(self, schema, original_export_instance):
         export_instance = super(EditODataFormFeedView, self).get_export_instance(schema, original_export_instance)
         remove_row_number_from_export_columns(export_instance)
+        clean_odata_columns(export_instance)
         return export_instance
 
 
