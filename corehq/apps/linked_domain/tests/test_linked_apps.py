ba0e1f3710d99b08efeb8955574f7ddd5271da16
Jennifer Schweers <orange.jenny@gmail.com>
2019-07-15 13:15:37 -0400

    Fixed lint errors

--- corehq/apps/linked_domain/tests/test_linked_apps.py
+++ corehq/apps/linked_domain/tests/test_linked_apps.py
@@ -115,7 +115,7 @@ class TestLinkedApps(BaseLinkedAppsTest):
 
         self.assertIsNone(self.linked_app.get_latest_master_release(master_id))
 
-        master1_copy1 = self.master1.make_build()
+        self.master1.make_build()
 
         self.assertIsNone(self.linked_app.get_latest_master_release(master_id))
 
@@ -225,7 +225,7 @@ class TestLinkedApps(BaseLinkedAppsTest):
     def test_override_translations(self):
         translations = {'en': {'updates.check.begin': 'update?'}}
 
-        copy = self.master1.make_build()
+        self.master1.make_build()
         copy1 = self.master1.make_build()
         copy1.is_released = True
         copy1.save()
