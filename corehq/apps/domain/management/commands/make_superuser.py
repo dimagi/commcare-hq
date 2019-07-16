1a5e3517a485cc8cea053e16fddbe6408e729912
Ethan Soergel <esoergel@gmail.com>
2019-07-16 16:41:05 -0400

    Use named logger

--- corehq/apps/domain/management/commands/make_superuser.py
+++ corehq/apps/domain/management/commands/make_superuser.py
@@ -8,6 +8,8 @@ from django.core.management.base import BaseCommand, CommandError
 
 from email_validator import EmailSyntaxError, validate_email
 
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     help = "Make a new superuser or make an existing user a superuser."
@@ -34,11 +36,11 @@ class Command(BaseCommand):
         if couch_user:
             if not isinstance(couch_user, WebUser):
                 raise CommandError('Username already in use by a non-web user')
-            logging.info("✓ User {} exists".format(couch_user.username))
+            logger.info("✓ User {} exists".format(couch_user.username))
         else:
             password = self.get_password_from_user()
             couch_user = WebUser.create(None, username, password)
-            logging.info("→ User {} created".format(couch_user.username))
+            logger.info("→ User {} created".format(couch_user.username))
 
         is_superuser_changed = not couch_user.is_superuser
         is_staff_changed = not couch_user.is_staff
@@ -49,11 +51,11 @@ class Command(BaseCommand):
             couch_user.save()
 
         if is_superuser_changed:
-            logging.info("→ User {} is now a superuser".format(couch_user.username))
+            logger.info("→ User {} is now a superuser".format(couch_user.username))
         else:
-            logging.info("✓ User {} is a superuser".format(couch_user.username))
+            logger.info("✓ User {} is a superuser".format(couch_user.username))
 
         if is_staff_changed:
-            logging.info("→ User {} can now access django admin".format(couch_user.username))
+            logger.info("→ User {} can now access django admin".format(couch_user.username))
         else:
-            logging.info("✓ User {} can access django admin".format(couch_user.username))
+            logger.info("✓ User {} can access django admin".format(couch_user.username))
