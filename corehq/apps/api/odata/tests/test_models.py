8c1a5af54b7076f16a030567317b110c6fb2e8a8
Nick Pellegrino <npellegrino@dimagi.com>
2019-07-18 08:39:29 -0400

    don't reset labels of case and form links

--- corehq/apps/api/odata/tests/test_models.py
+++ corehq/apps/api/odata/tests/test_models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from corehq.apps.export.const import CASE_ID_TO_LINK, FORM_ID_TO_LINK
 from corehq.apps.export.models import (
     CaseExportInstance,
     FormExportInstance,
@@ -146,3 +147,55 @@ class TestODataWrap(TestCase):
         self.addCleanup(export_with_modified_formid_column.delete)
         cleaned_export = FormExportInstance.get(export_with_modified_formid_column.get_id)
         self.assertEqual(cleaned_export.tables[0].columns[0].label, 'formid')
+
+    def test_ignore_case_link_label(self):
+        export_with_case_link = CaseExportInstance(
+            is_odata_config=True,
+            tables=[
+                TableConfiguration(
+                    columns=[
+                        ExportColumn(
+                            label='my_case_link',
+                            item=ExportItem(
+                                path=[
+                                    PathNode(name='_id')
+                                ],
+                                transform=CASE_ID_TO_LINK,
+                            ),
+                            selected=True,
+                        )
+                    ]
+                )
+            ]
+        )
+        export_with_case_link.save()
+        self.addCleanup(export_with_case_link.delete)
+        cleaned_export = CaseExportInstance.get(export_with_case_link.get_id)
+        self.assertEqual(cleaned_export.tables[0].columns[0].label, 'my_case_link')
+
+    def test_ignore_form_link_label(self):
+        export_with_form_link = FormExportInstance(
+            is_odata_config=True,
+            tables=[
+                TableConfiguration(
+                    columns=[
+                        ExportColumn(
+                            label='my_form_link',
+                            item=ExportItem(
+                                path=[
+                                    PathNode(name='form'),
+                                    PathNode(name='meta'),
+                                    PathNode(name='instanceID')
+                                ],
+                                transform=FORM_ID_TO_LINK,
+                            ),
+                            selected=True,
+                        )
+                    ]
+                )
+            ]
+        )
+        export_with_form_link.save()
+        self.addCleanup(export_with_form_link.delete)
+        cleaned_export = FormExportInstance.get(export_with_form_link.get_id)
+        self.assertEqual(cleaned_export.tables[0].columns[0].label, 'my_form_link')
