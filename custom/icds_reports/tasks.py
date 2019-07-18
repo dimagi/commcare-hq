a99ccf9144964d007dfa93472a32576e714a49cc
Daniel Roberts <droberts@dimagi.com>
2019-07-18 10:42:45 +0900

    Run recalculate_stagnant_cases only on ICDS_ENVS
    (and not india)

--- custom/icds_reports/tasks.py
+++ custom/icds_reports/tasks.py
@@ -697,7 +697,7 @@ def email_dashboad_team(aggregation_date, aggregation_start_time, force_citus=Fa
 
 
 @periodic_task_on_envs(
-    settings.ICDS_TESTING_ENVS,
+    settings.ICDS_ENVS,
     queue='background_queue',
     run_every=crontab(day_of_week='tuesday,thursday,saturday', minute=0, hour=16),
     acks_late=True
