1b9563695c6121740a1fff09a4ef73b7af11b26c
Jennifer Schweers <orange.jenny@gmail.com>
2019-07-19 14:28:30 -0400

    WIP

---
+++ get_resource_versions.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import yaml
+from django.conf import settings
+from io import open
+import os
+
+
+def get_resource_versions():
+    resource_versions = {}
+
+    path = os.path.join(settings.FILEPATH, 'resource_versions.yaml')
+    if not os.path.exists(path):
+        return resource_versions
+
+    with open(path, 'r', encoding='utf-8') as f:    # TODO: maybe not relative path?
+        data = yaml.safe_load(f)
+        if data:
+            for resource in data:
+                resource_versions[resource['name']] = resource['version']
+
+    return resource_versions
+
+# TODO: purge_resource_versions
