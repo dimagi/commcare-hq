89af2982b71cf448572c604b43da6d0852de4a46
Cal Ellowitz <calellowitz@gmail.com>
2019-07-22 15:04:27 -0400

    management command to prime restore cache

---
+++ corehq/apps/ota/management/commands/prime_restore_cache.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.core.management.base import BaseCommand
+
+from corehq.apps.ota.views import get_restore_response
+from corehq.apps.users.models import CommCareUser
+from corehq.const import ONE_DAY
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument('file_path')
+
+    def handle(self, file_path):
+        with open(file_path, 'r') as f:
+            for line in f:
+                username, app_id = line.split(',')
+                couch_user = CommCareUser.get_by_username(username)
+                device_id = couch_user.last_device.device_id
+                get_restore_response(
+                    'icds-cas',
+                    couch_user,
+                    app_id,
+                    device_id=device_id,
+                    version='2.0',
+                    force_cache=True,
+                    cache_timeout=ONE_DAY
+                )
