dfe15cf3e8cb86ac6223d799d42e5674d9da39e1
Jennifer Schweers <orange.jenny@gmail.com>
2019-07-22 11:55:05 -0400

    Removed re.sub

--- corehq/apps/app_manager/xpath_validator/wrapper.py
+++ corehq/apps/app_manager/xpath_validator/wrapper.py
@@ -5,7 +5,6 @@ from corehq.apps.app_manager.xpath_validator.config import get_xpath_validator_p
 from corehq.apps.app_manager.xpath_validator.exceptions import XpathValidationError
 from dimagi.utils.subprocess_manager import subprocess_context
 import six
-import re
 
 XpathValidationResponse = namedtuple('XpathValidationResponse', ['is_valid', 'message'])
 
@@ -23,7 +22,7 @@ def validate_xpath(xpath, allow_case_hashtags=False):
             stdout, stderr = p.communicate(xpath)
         else:
             p = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = p.communicate(re.sub(r'\s+', ' ', xpath).encode('utf-8'))
+            stdout, stderr = p.communicate(xpath.encode('utf-8'))
         exit_code = p.wait()
     if exit_code == 0:
         return XpathValidationResponse(is_valid=True, message=None)
