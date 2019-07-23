588bc396045a29a5989b67d1cbd25c0c1bd9aa75
Cal Ellowitz <calellowitz@gmail.com>
2019-07-23 11:15:43 -0400

    print exceptions and use correct properties

--- corehq/apps/ota/management/commands/prime_restore_cache.py
+++ corehq/apps/ota/management/commands/prime_restore_cache.py
@@ -12,13 +12,15 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('file_path')
 
-    def handle(self, file_path):
+    def handle(self, **options):
+        file_path = options['file_path']
         with open(file_path, 'r') as f:
-            for username in f:
-                couch_user = CommCareUser.get_by_username(username)
-                device_id = couch_user.last_device.device_id
-                app_id = couch_user.last_device.app_meta.build_id
+            for u in f:
                 try:
+                    username = u.rstrip()
+                    couch_user = CommCareUser.get_by_username(username)
+                    device_id = couch_user.last_device.device_id
+                    app_id = couch_user.last_device.app_meta[0].build_id
                     get_restore_response(
                         'icds-cas',
                         couch_user,
@@ -28,7 +30,7 @@ class Command(BaseCommand):
                         force_cache=True,
                         cache_timeout=ONE_DAY
                     )
-                except:
-                    pass
+                except Exception as e:
+                    print(e)
                 else:
                     print(username)
