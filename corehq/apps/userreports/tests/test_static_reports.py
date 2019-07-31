a2fd85c038af8e7d385182b091787cc9646cec14
Jennifer Schweers <orange.jenny@gmail.com>
2019-07-31 17:06:52 -0400

    Combined usage of StaticDataSourceConfiguration.all() in TestStaticReportConfig

--- corehq/apps/userreports/tests/test_static_reports.py
+++ corehq/apps/userreports/tests/test_static_reports.py
@@ -17,6 +17,11 @@ class TestStaticReportConfig(SimpleTestCase, TestFileMixin):
     file_path = ('data', 'static_reports')
     root = os.path.dirname(__file__)
 
+    @classmethod
+    def setUpClass(cls):
+        super(TestStaticReportConfig, cls).setUpClass()
+        cls.configs = list(StaticDataSourceConfiguration.all())
+
     def setUp(self):
         super(TestStaticReportConfig, self).setUp()
         StaticReportConfiguration.by_id_mapping.reset_cache(StaticReportConfiguration.__class__)
@@ -27,31 +32,28 @@ class TestStaticReportConfig(SimpleTestCase, TestFileMixin):
 
     def test_get_all_json(self):
         with override_settings(STATIC_UCR_REPORTS=[self.get_path('static_report_config', 'json')]):
-            all = list(StaticReportConfiguration.all())
-            self.assertEqual(2, len(all))
-            example, dimagi = all
+            self.assertEqual(2, len(self.configs))
+            example, dimagi = self.configs
             self.assertEqual('example', example.domain)
             self.assertEqual('dimagi', dimagi.domain)
-            for config in all:
+            for config in self.configs:
                 self.assertEqual('Custom Title', config.title)
 
     def test_get_all_yaml(self):
         with override_settings(STATIC_UCR_REPORTS=[self.get_path('static_report_config', 'yaml')]):
-            all = list(StaticReportConfiguration.all())
-            self.assertEqual(2, len(all))
-            example, dimagi = all
+            self.assertEqual(2, len(self.configs))
+            example, dimagi = self.configs
             self.assertEqual('example', example.domain)
             self.assertEqual('dimagi', dimagi.domain)
-            for config in all:
+            for config in self.configs:
                 self.assertEqual('Custom Title', config.title)
 
     def test_production_config(self):
-        for report_config in StaticReportConfiguration.all():
+        for report_config in self.configs:
             report_config.validate()
 
     def test_for_report_id_conflicts(self):
-        counts = Counter(rc.get_id for rc in
-                         StaticReportConfiguration.all())
+        counts = Counter(rc.get_id for rc in self.configs)
         duplicates = [k for k, v in counts.items() if v > 1]
         msg = "The following report configs have duplicate generated report_ids:\n{}".format(
             "\n".join("report_id: {}".format(report_id) for report_id in duplicates)
@@ -70,8 +72,7 @@ class TestStaticReportConfig(SimpleTestCase, TestFileMixin):
             available_data_sources = data_sources_on_domain[report_config.domain]
             return report_config.config_id not in available_data_sources
 
-        all_configs = StaticReportConfiguration.all()
-        configs_missing_data_source = list(filter(has_no_data_source, all_configs))
+        configs_missing_data_source = list(filter(has_no_data_source, self.configs))
 
         msg = ("There are {} report configs which reference data sources that "
                "don't exist (or which don't exist on that domain):\n{}".format(
