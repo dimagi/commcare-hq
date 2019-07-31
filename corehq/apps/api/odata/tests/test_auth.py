9410b65a4dfc687df783d84627e59a880d3fde30
Ethan Soergel <esoergel@gmail.com>
2019-07-31 18:06:50 -0400

    Pull out repeated use of flag

--- corehq/apps/api/odata/tests/test_auth.py
+++ corehq/apps/api/odata/tests/test_auth.py
@@ -7,7 +7,7 @@ import mock
 
 from corehq.apps.domain.models import Domain
 from corehq.apps.export.models import CaseExportInstance, TableConfiguration
-from corehq.util.test_utils import flag_enabled
+from corehq.util.test_utils import flag_disabled, flag_enabled
 
 from ..views import ODataCaseMetadataView
 from .utils import (
@@ -19,6 +19,7 @@ from .utils import (
 
 
 @mock.patch('corehq.apps.api.odata.views.get_document_or_404', new=mock.MagicMock)
+@flag_enabled('ODATA')
 class TestOdataAuth(TestCase, CaseOdataTestMixin):
 
     view_urlname = ODataCaseMetadataView.urlname  # Testing auth only, any view will do
@@ -38,19 +39,16 @@ class TestOdataAuth(TestCase, CaseOdataTestMixin):
         super(TestOdataAuth, cls).tearDownClass()
 
     def test_sucess(self):
-        with flag_enabled('ODATA'):
-            response = self._execute_query(self._get_correct_credentials())
+        response = self._execute_query(self._get_correct_credentials())
         self.assertEqual(response.status_code, 200)
 
     def test_no_credentials(self):
-        with flag_enabled('ODATA'):
-            response = self.client.get(self.view_url)
+        response = self.client.get(self.view_url)
         self.assertEqual(response.status_code, 401)
 
     def test_wrong_password(self):
         wrong_credentials = self._get_basic_credentials(self.web_user.username, 'wrong_password')
-        with flag_enabled('ODATA'):
-            response = self._execute_query(wrong_credentials)
+        response = self._execute_query(wrong_credentials)
         self.assertEqual(response.status_code, 401)
 
     def test_wrong_domain(self):
@@ -59,11 +57,10 @@ class TestOdataAuth(TestCase, CaseOdataTestMixin):
         self.addCleanup(other_domain.delete)
 
         correct_credentials = self._get_correct_credentials()
-        with flag_enabled('ODATA'):
-            response = self.client.get(
-                reverse(self.view_urlname, kwargs={'domain': other_domain.name, 'config_id': 'my_config_id'}),
-                HTTP_AUTHORIZATION='Basic ' + correct_credentials,
-            )
+        response = self.client.get(
+            reverse(self.view_urlname, kwargs={'domain': other_domain.name, 'config_id': 'my_config_id'}),
+            HTTP_AUTHORIZATION='Basic ' + correct_credentials,
+        )
         self.assertEqual(response.status_code, 403)
 
     def test_user_permissions(self):
@@ -81,20 +78,19 @@ class TestOdataAuth(TestCase, CaseOdataTestMixin):
         self.addCleanup(export_config.delete)
 
         correct_credentials = self._get_correct_credentials()
-        with flag_enabled('ODATA'):
-            response = self._execute_query(correct_credentials)
+        response = self._execute_query(correct_credentials)
         self.assertEqual(response.status_code, 403)
 
     def test_missing_feature_flag(self):
         correct_credentials = self._get_correct_credentials()
-        response = self._execute_query(correct_credentials)
+        with flag_disabled('ODATA'):
+            response = self._execute_query(correct_credentials)
         self.assertEqual(response.status_code, 404)
 
     def test_success_with_api_key(self):
         self.api_key = generate_api_key_from_web_user(self.web_user)
         credentials = self._get_basic_credentials(self.web_user.username, self.api_key.key)
-        with flag_enabled('ODATA'):
-            response = self._execute_query(credentials)
+        response = self._execute_query(credentials)
         self.assertEqual(response.status_code, 200)
 
     def test_success_with_two_factor_api_key(self):
