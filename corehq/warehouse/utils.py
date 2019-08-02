b7071d00cd253249f1b986c42408e16202b9c097
Sravan Reddy <sravfeyn@gmail.com>
2019-08-02 17:49:53 +0530

    Turn off weekly inactive SMS messages if warehouse lag is more than 12 hours

--- corehq/warehouse/utils.py
+++ corehq/warehouse/utils.py
@@ -5,8 +5,10 @@ from django.conf import settings
 
 from dimagi.utils.chunked import chunked
 
+from corehq.warehouse.models.meta import Batch
 from corehq.warehouse.const import DJANGO_MAX_BATCH_SIZE
 from corehq.sql_db.routers import db_for_read_write
+from corehq.util.quickcache import quickcache
 
 
 def django_batch_records(cls, record_iter, field_mapping, batch_id):
@@ -35,3 +37,15 @@ def truncate_records_for_cls(cls, cascade=False):
     database = db_for_read_write(cls)
     with connections[database].cursor() as cursor:
         cursor.execute("TRUNCATE {} {}".format(cls._meta.db_table, 'CASCADE' if cascade else ''))
+
+
+@quickcache([], timeout=60 * 60)
+def get_warehouse_latest_modified_date():
+    """
+    Return in minutes how fresh is the data of app_status warehouse model.
+    """
+    last_completed_app_status_batch = Batch.objects.filter(
+        dag_slug='app_status_batch', completed_on__isnull=False
+    ).order_by('completed_on').last()
+    # The end_datetime of a batch is used to filter on forms by last_modified (received_on, edited_on, deleted_on)
+    return last_completed_app_status_batch.end_datetime
