9cd0907323b19e3c00a9a3af88dbd6b19d686ff3
Jennifer Schweers <orange.jenny@gmail.com>
2019-08-05 14:47:15 -0400

    Fixed test_static_data_sources
    Let the tests that use test configurations use those configurations.

--- corehq/apps/userreports/tests/test_static_data_sources.py
+++ corehq/apps/userreports/tests/test_static_data_sources.py
@@ -12,7 +12,6 @@ from corehq.util.test_utils import TestFileMixin
 from corehq.apps.userreports.models import StaticDataSourceConfiguration, DataSourceConfiguration
 
 
-@patch('corehq.apps.callcenter.data_source.get_call_center_domains', MagicMock(return_value=[domain_lite('cc1')]))
 class TestStaticDataSource(SimpleTestCase, TestFileMixin):
 
     file_path = ('data', 'static_data_sources')
@@ -21,7 +20,9 @@ class TestStaticDataSource(SimpleTestCase, TestFileMixin):
     @classmethod
     def setUpClass(cls):
         super(TestStaticDataSource, cls).setUpClass()
-        cls.configs = list(StaticDataSourceConfiguration.all())
+        with patch('corehq.apps.callcenter.utils.get_call_center_domains',
+                   MagicMock(return_value=[domain_lite('cc1')])):
+            cls.configs = list(StaticDataSourceConfiguration.all())
 
     def test_wrap(self):
         wrapped = StaticDataSourceConfiguration.wrap(self.get_json('sample_static_data_source'))
@@ -29,24 +30,25 @@ class TestStaticDataSource(SimpleTestCase, TestFileMixin):
 
     def test_get_all(self):
         with override_settings(STATIC_DATA_SOURCES=[self.get_path('sample_static_data_source', 'json')]):
-            self.assertEqual(2 + 3, len(self.configs))
-            example, dimagi = self.configs[:2]
+            all = list(StaticDataSourceConfiguration.all())
+            self.assertEqual(2 + 3, len(all))
+            example, dimagi = all[:2]
             self.assertEqual('example', example.domain)
             self.assertEqual('dimagi', dimagi.domain)
-            for config in self.configs[:2]:
+            for config in all[:2]:
                 self.assertEqual('all_candidates', config.table_id)
 
-            for config in self.configs[2:]:
+            for config in all[2:]:
                 self.assertEqual('cc1', config.domain)
 
     def test_is_static_positive_json(self):
         with override_settings(STATIC_DATA_SOURCES=[self.get_path('sample_static_data_source', 'json')]):
-            example = self.configs[0]
+            example = list(StaticDataSourceConfiguration.all())[0]
             self.assertTrue(example.is_static)
 
     def test_is_static_positive_yaml(self):
         with override_settings(STATIC_DATA_SOURCES=[self.get_path('sample_static_data_source', 'yaml')]):
-            example = self.configs[0]
+            example = list(StaticDataSourceConfiguration.all())[0]
             self.assertTrue(example.is_static)
 
     def test_is_static_negative(self):
@@ -55,7 +57,7 @@ class TestStaticDataSource(SimpleTestCase, TestFileMixin):
 
     def test_deactivate_noop(self):
         with override_settings(STATIC_DATA_SOURCES=[self.get_path('sample_static_data_source', 'json')]):
-            example = self.configs[0]
+            example = list(StaticDataSourceConfiguration.all())[0]
             # since this is a SimpleTest, this should fail if the call actually hits the DB
             example.deactivate()
 
