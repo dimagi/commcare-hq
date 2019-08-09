16b07dd961cbe55ee452ed6057048ec452ffbd72
Ethan Soergel <esoergel@gmail.com>
2019-08-09 19:34:42 -0400

    Replace old config IDs with the new ones

--- custom/icds/management/commands/copy_icds_app.py
+++ custom/icds/management/commands/copy_icds_app.py
@@ -23,3 +23,18 @@ class Command(BaseCommand):
         old_app = wrap_app(old_app)
         old_app.convert_build_to_app()
         new_app = import_app(old_app.to_json(), domain, source_properties={'name': new_name})
+
+        old_to_new = get_old_to_new_config_ids(old_app, new_app)
+        for form in new_app.get_forms():
+            for old_id, new_id in old_to_new:
+                form.source = form.source.replace(old_id, new_id)
+
+        new_app.save()
+
+
+def get_old_to_new_config_ids(old_app, new_app):
+    return [
+        (old_config.uuid, new_config.uuid)
+        for old_module, new_module in zip(old_app.get_report_modules(), new_app.get_report_modules())
+        for old_config, new_config in zip(old_module.report_configs, new_module.report_configs)
+    ]
