151a1da5707be83d1396bd4f1e533b9384a1365f
Jonathan Emord <jonathan.emord@gmail.com>
2019-08-13 16:13:28 -0400

    Remove unused parameter

--- corehq/apps/reports/analytics/esaccessors.py
+++ corehq/apps/reports/analytics/esaccessors.py
@@ -574,15 +574,13 @@ def get_username_in_last_form_user_id_submitted(domain, user_id):
         return user_submissions[0]['form']['meta'].get('username', None)
 
 
-def get_wrapped_ledger_values(domain, case_ids, section_id, entry_ids=None, pagination=None):
+def get_wrapped_ledger_values(domain, case_ids, section_id, entry_ids=None):
     # todo: figure out why this causes circular import
     from corehq.apps.reports.commtrack.util import StockLedgerValueWrapper
     query = (LedgerES()
              .domain(domain)
              .section(section_id)
              .case(case_ids))
-    if pagination:
-        query = query.size(pagination.count).start(pagination.start)
     if entry_ids:
         query = query.entry(entry_ids)
 
