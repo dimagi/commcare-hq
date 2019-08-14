e35a1d0d9365cf933f13173ccc15df51787ef4fd
Daniel Roberts <droberts@dimagi.com>
2019-08-14 13:07:56 -0500

    Django 2: remove defunct egg template loader
    added 8 years ago as a dependency for the now-removed djtables:
    https://github.com/dimagi/commcare-hq/commit/64c751ed2afa298d7e0565569348d30133068159

--- settings.py
+++ settings.py
@@ -982,7 +982,6 @@ TEMPLATES = [
             'loaders': [
                 'django.template.loaders.filesystem.Loader',
                 'django.template.loaders.app_directories.Loader',
-                'django.template.loaders.eggs.Loader',
             ],
         },
     },
