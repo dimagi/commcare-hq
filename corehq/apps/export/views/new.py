426ec86d2be49d72a09ed913f07c7a6197f15a39
Biyeun Buczyk <biyeun@dimagi.com>
2019-08-16 16:36:09 -0500

    since no other areas in exports need de-id limiting outside of permissions, use standard behavior with just permissions

--- corehq/apps/export/views/new.py
+++ corehq/apps/export/views/new.py
@@ -50,7 +50,6 @@ class BaseExportView(BaseProjectDataView):
     template_name = 'export/customize_export_new.html'
     export_type = None
     is_async = True
-    allow_deid = True
 
     @method_decorator(require_can_edit_data)
     def dispatch(self, request, *args, **kwargs):
@@ -110,7 +109,7 @@ class BaseExportView(BaseProjectDataView):
         return {
             'export_instance': self.export_instance,
             'export_home_url': self.export_home_url,
-            'allow_deid': self.allow_deid and has_privilege(self.request, privileges.DEIDENTIFIED_DATA),
+            'allow_deid': has_privilege(self.request, privileges.DEIDENTIFIED_DATA),
             'has_excel_dashboard_access': domain_has_privilege(self.domain, EXCEL_DASHBOARD),
             'has_daily_saved_export_access': domain_has_privilege(self.domain, DAILY_SAVED_EXPORT),
             'can_edit': self.export_instance.can_edit(self.request.couch_user),
