f5d327b9b12fed2e8d3883984c5a2d2ff647ba33
Simon Kelly <skelly@dimagi.com>
2019-08-19 14:49:46 +0200

    skip locking in pillow retry queue

--- corehq/apps/hqpillow_retry/management/commands/run_pillow_retry_queue.py
+++ corehq/apps/hqpillow_retry/management/commands/run_pillow_retry_queue.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+from datetime import datetime
+
 from django.conf import settings
 from psycopg2._psycopg import InterfaceError
 import pytz
+
+from corehq.sql_db.util import handle_connection_failure
 from hqscripts.generic_queue import GenericEnqueuingOperation, QueueItem
 from pillow_retry.const import PILLOW_RETRY_QUEUE_ENQUEUING_TIMEOUT
 from pillow_retry.models import PillowError
@@ -23,11 +28,12 @@ class PillowRetryEnqueuingOperation(GenericEnqueuingOperation):
     def get_enqueuing_timeout(self):
         return PILLOW_RETRY_QUEUE_ENQUEUING_TIMEOUT
 
-    def _get_items(self, utcnow):
-        errors = PillowError.get_errors_to_process(utcnow=utcnow, limit=10000)
-        items = [QueueItem(id=e.id, key=e.date_next_attempt, object=e) for e in errors]
-        self._errors_in_queue = bool(items)
-        return items
+    @handle_connection_failure()
+    def populate_queue(self):
+        utcnow = datetime.utcnow()
+        items = self.get_items_to_be_processed(utcnow)
+        for item in items:
+            process_pillow_retry(item.object)
 
     def get_items_to_be_processed(self, utcnow):
         # We're just querying for ids here, so no need to limit
@@ -38,11 +44,14 @@ class PillowRetryEnqueuingOperation(GenericEnqueuingOperation):
             db.connection.close()
             return self._get_items(utcnow)
 
+    def _get_items(self, utcnow):
+        errors = PillowError.get_errors_to_process(utcnow=utcnow, limit=10000)
+        items = [QueueItem(id=e.id, key=e.date_next_attempt, object=e) for e in errors]
+        self._errors_in_queue = bool(items)
+        return items
+
     def use_queue(self):
         return settings.PILLOW_RETRY_QUEUE_ENABLED
-
-    def enqueue_item(self, item):
-        process_pillow_retry(item.object)
 
 
 class Command(PillowRetryEnqueuingOperation):
