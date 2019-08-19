c8d344726fbfd860c49c3e1c4f9bc367d8511c93
Simon Kelly <skelly@dimagi.com>
2019-08-19 14:47:51 +0200

    use gevent

--- corehq/ex-submodules/pillow_retry/management/commands/send_pillow_retry_queue_through_pillows.py
+++ corehq/ex-submodules/pillow_retry/management/commands/send_pillow_retry_queue_through_pillows.py
@@ -1,13 +1,15 @@
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import time
 from datetime import datetime
 
 from django.core.management.base import BaseCommand
-
-from pillow_retry.models import PillowError
+from gevent.pool import Pool
 
 from corehq.apps.change_feed.producer import producer
+from pillow_retry.models import PillowError
 
 
 class Command(BaseCommand):
@@ -15,18 +17,16 @@ class Command(BaseCommand):
         parser.add_argument('pillow')
 
     def handle(self, pillow, **options):
+        self.pool = Pool(10)
         self.pillow = pillow
+        self.count = 0
+        self.start = time.time()
+
         for errors in self.get_next_errors():
-            for error in errors:
-                if error.change_object.metadata:
-                    producer.send_change(
-                        error.change_object.metadata.data_source_type,
-                        error.change_object.metadata
-                    )
+            self.pool.spawn(self._process_errors, errors)
 
     def get_next_errors(self):
         num_retrieved = 1
-        count = 0
 
         while num_retrieved > 0:
             pillow_errors = (
@@ -38,9 +38,28 @@ class Command(BaseCommand):
             num_retrieved = len(pillow_errors)
             yield pillow_errors
 
-            print("deleting")
-            doc_ids = [error.doc_id for error in pillow_errors]
-            PillowError.objects.filter(doc_id__in=doc_ids).delete()
-            count += num_retrieved
-            print(count)
-            print(datetime.utcnow())
+            while not self.pool.wait_available(timeout=10):
+                time.sleep(1)
+
+        while not self.pool.join(timeout=10):
+            print('Waiting for tasks to complete')
+
+    def _process_errors(self, errors):
+        for error in errors:
+            if error.change_object.metadata:
+                producer.send_change(
+                    error.change_object.metadata.data_source_name,
+                    error.change_object.metadata
+                )
+
+        self._delete_errors(errors)
+        self.count += 1000
+        duration = time.time() - self.start
+        print('Processed {} in {}s: {} per s'.format(
+            self.count, duration, self.count / duration if duration else self.count)
+        )
+        print(datetime.utcnow())
+
+    def _delete_errors(self, errors):
+        doc_ids = [error.doc_id for error in errors]
+        PillowError.objects.filter(doc_id__in=doc_ids).delete()
