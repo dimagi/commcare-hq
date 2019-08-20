dd60189830200bda86581846fe907502c78ba66b
mkangia <mkangia@dimagi.com>
2019-08-20 18:51:51 +0530

    make status computed so that it updates with active field in UI

--- corehq/apps/app_manager/static/app_manager/js/manage_releases_by_app_profile.js
+++ corehq/apps/app_manager/static/app_manager/js/manage_releases_by_app_profile.js
@@ -20,7 +20,9 @@ hqDefine('app_manager/js/manage_releases_by_app_profile', [
                 'version']);
             self.id = details.id;
             self.active = ko.observable(details.active);
-            self.status = details.active ? gettext('Active') : gettext('Inactive');
+            self.status = ko.computed(function() {
+                return (self.active() ? gettext("Active") : gettext("Inactive"));
+            })
             self.appName = details.app_name;
             self.version = details.version;
             self.profileName = details.profile_name;
