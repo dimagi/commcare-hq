329ad23ab0d6f99785fa45df3382c96c4635e8ee
Daniel Miller <dmiller@dimagi.com>
2019-08-23 14:49:37 -0400

    Rearrange to follow newspaper metaphor

--- manage.py
+++ manage.py
@@ -7,11 +7,67 @@ from gevent import monkey
 from psycogreen.gevent import patch_psycopg
 
 
+def main():
+    # important to apply gevent monkey patches before running any other code
+    # applying this later can lead to inconsistencies and threading issues
+    # but compressor doesn't like it
+    # ('module' object has no attribute 'poll' which has to do with
+    # gevent-patching subprocess)
+    GEVENT_COMMANDS = (
+        GeventCommand('run_gunicorn'),
+        GeventCommand('run_sql'),
+        GeventCommand('run_blob_migration'),
+        GeventCommand('check_blob_logs'),
+        GeventCommand('preindex_everything'),
+        GeventCommand('migrate_multi'),
+        GeventCommand('prime_views'),
+        GeventCommand('ptop_preindex'),
+        GeventCommand('sync_prepare_couchdb_multi'),
+        GeventCommand('sync_couch_views'),
+        GeventCommand('populate_form_date_modified'),
+        GeventCommand('migrate_domain_from_couch_to_sql', http_adapter_pool_size=32),
+        GeventCommand('migrate_multiple_domains_from_couch_to_sql', http_adapter_pool_size=32),
+        GeventCommand('run_aggregation_query'),
+        GeventCommand('send_pillow_retry_queue_through_pillows'),
+    )
+    if len(sys.argv) > 1 and _should_patch_gevent(sys.argv, GEVENT_COMMANDS):
+        monkey.patch_all(subprocess=True)
+        patch_psycopg()
+
+    init_hq_python_path()
+    run_patches()
+
+    set_default_settings_path(sys.argv)
+    set_nosetests_verbosity(sys.argv)
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
+
+
 @attr.s
 class GeventCommand(object):
     command = attr.ib()
     contains = attr.ib(default=None)
     http_adapter_pool_size = attr.ib(default=None)
+
+
+def _should_patch_gevent(args, gevent_commands):
+    should_patch = False
+    for gevent_command in gevent_commands:
+        should_patch = args[1] == gevent_command.command
+        if gevent_command.contains:
+            should_patch = should_patch and gevent_command.contains in ' '.join(args)
+        if should_patch:
+            if gevent_command.http_adapter_pool_size:
+                import requests
+                requests.adapters.DEFAULT_POOLSIZE = gevent_command.http_adapter_pool_size
+            break
+    return should_patch
+
+
+def init_hq_python_path():
+    _set_source_root_parent('submodules')
+    _set_source_root(os.path.join('corehq', 'ex-submodules'))
+    _set_source_root(os.path.join('custom', '_legacy'))
 
 
 def _set_source_root_parent(source_root_parent):
@@ -46,31 +102,51 @@ def _set_source_root(source_root):
     sys.path.insert(1, os.path.join(filedir, source_root))
 
 
+def run_patches():
+    # workaround for https://github.com/smore-inc/tinys3/issues/33
+    import mimetypes
+    mimetypes.init()
+
+    patch_jsonfield()
+
+    patch_assertItemsEqual()
+
+    import django
+    _setup_once.setup = django.setup
+    django.setup = _setup_once
+
+
+def patch_jsonfield():
+    """Patch the ``to_python`` method of JSONField
+    See https://github.com/bradjasper/django-jsonfield/pull/173 for more details
+    """
+    import six
+    import json
+    from django.core.exceptions import ValidationError
+    from django.utils.translation import ugettext_lazy as _
+    from jsonfield import JSONField
+
+    def to_python(self, value):
+        if isinstance(value, six.string_types):
+            try:
+                return json.loads(value, **self.load_kwargs)
+            except ValueError:
+                raise ValidationError(_("Enter valid JSON"))
+        return value
+
+    JSONField.to_python = to_python
+
+
+def patch_assertItemsEqual():
+    import unittest
+    unittest.TestCase.assertItemsEqual = unittest.TestCase.assertCountEqual
+
+
 # HACK monkey-patch django setup to prevent second setup by django_nose
 def _setup_once(*args, **kw):
     if not hasattr(_setup_once, "done"):
         _setup_once.done = True
         _setup_once.setup(*args, **kw)
-
-
-def init_hq_python_path():
-    _set_source_root_parent('submodules')
-    _set_source_root(os.path.join('corehq', 'ex-submodules'))
-    _set_source_root(os.path.join('custom', '_legacy'))
-
-
-def _should_patch_gevent(args, gevent_commands):
-    should_patch = False
-    for gevent_command in gevent_commands:
-        should_patch = args[1] == gevent_command.command
-        if gevent_command.contains:
-            should_patch = should_patch and gevent_command.contains in ' '.join(args)
-        if should_patch:
-            if gevent_command.http_adapter_pool_size:
-                import requests
-                requests.adapters.DEFAULT_POOLSIZE = gevent_command.http_adapter_pool_size
-            break
-    return should_patch
 
 
 def set_default_settings_path(argv):
@@ -116,77 +192,5 @@ def set_nosetests_verbosity(argv):
                 break
 
 
-def patch_jsonfield():
-    """Patch the ``to_python`` method of JSONField
-    See https://github.com/bradjasper/django-jsonfield/pull/173 for more details
-    """
-    import six
-    import json
-    from django.core.exceptions import ValidationError
-    from django.utils.translation import ugettext_lazy as _
-    from jsonfield import JSONField
-
-    def to_python(self, value):
-        if isinstance(value, six.string_types):
-            try:
-                return json.loads(value, **self.load_kwargs)
-            except ValueError:
-                raise ValidationError(_("Enter valid JSON"))
-        return value
-
-    JSONField.to_python = to_python
-
-
-def patch_assertItemsEqual():
-    import unittest
-    unittest.TestCase.assertItemsEqual = unittest.TestCase.assertCountEqual
-
-
-def run_patches():
-    # workaround for https://github.com/smore-inc/tinys3/issues/33
-    import mimetypes
-    mimetypes.init()
-
-    patch_jsonfield()
-
-    patch_assertItemsEqual()
-
-    import django
-    _setup_once.setup = django.setup
-    django.setup = _setup_once
-
-
 if __name__ == "__main__":
-    # important to apply gevent monkey patches before running any other code
-    # applying this later can lead to inconsistencies and threading issues
-    # but compressor doesn't like it
-    # ('module' object has no attribute 'poll' which has to do with
-    # gevent-patching subprocess)
-    GEVENT_COMMANDS = (
-        GeventCommand('run_gunicorn'),
-        GeventCommand('run_sql'),
-        GeventCommand('run_blob_migration'),
-        GeventCommand('check_blob_logs'),
-        GeventCommand('preindex_everything'),
-        GeventCommand('migrate_multi'),
-        GeventCommand('prime_views'),
-        GeventCommand('ptop_preindex'),
-        GeventCommand('sync_prepare_couchdb_multi'),
-        GeventCommand('sync_couch_views'),
-        GeventCommand('populate_form_date_modified'),
-        GeventCommand('migrate_domain_from_couch_to_sql', http_adapter_pool_size=32),
-        GeventCommand('migrate_multiple_domains_from_couch_to_sql', http_adapter_pool_size=32),
-        GeventCommand('run_aggregation_query'),
-        GeventCommand('send_pillow_retry_queue_through_pillows'),
-    )
-    if len(sys.argv) > 1 and _should_patch_gevent(sys.argv, GEVENT_COMMANDS):
-        monkey.patch_all(subprocess=True)
-        patch_psycopg()
-
-    init_hq_python_path()
-    run_patches()
-
-    set_default_settings_path(sys.argv)
-    set_nosetests_verbosity(sys.argv)
-    from django.core.management import execute_from_command_line
-    execute_from_command_line(sys.argv)
+    main()
