8381cf3b011265e7dd091ce83f279fc5f6af00e3
Norman Hooper <nhooper@dimagi.com>
2019-08-27 18:19:42 +0200

    validate_phone_datetime supports datetime.date

---
+++ corehq/ex-submodules/casexml/apps/case/tests/test_util.py
@@ -0,0 +1,63 @@
+import datetime
+
+from django.test import SimpleTestCase
+
+from iso8601 import iso8601
+
+from casexml.apps.case.exceptions import PhoneDateValueError
+from casexml.apps.case.util import validate_phone_datetime
+
+
+class ValidatePhoneDatetimeTests(SimpleTestCase):
+
+    def test_datetime_string(self):
+        datetime_string = '2019-08-27T17:50:00.000'
+        result = validate_phone_datetime(datetime_string)
+
+        datetime_ = iso8601.parse_date(datetime_string)
+        assert datetime_ == datetime.datetime(
+            2019, 8, 27,
+            17, 50, 0,
+            tzinfo=datetime.timezone.utc
+        )
+        self.assertEqual(result, datetime_)
+
+    def test_date_string(self):
+        date_string = '2019-08-27'
+        result = validate_phone_datetime(date_string)
+
+        datetime_ = iso8601.parse_date(date_string)
+        assert datetime_ == datetime.datetime(
+            2019, 8, 27,
+            0, 0, 0,
+            tzinfo=datetime.timezone.utc
+        )
+        self.assertEqual(result, datetime_)
+
+    def test_datetime(self):
+        datetime_ = datetime.datetime(
+            2019, 8, 27,
+            17, 50, 0
+        )
+        result = validate_phone_datetime(datetime_)
+        self.assertEqual(result, datetime_)
+
+    def test_date(self):
+        date = datetime.date(2019, 8, 27)
+        result = validate_phone_datetime(date)
+        self.assertEqual(result, datetime.datetime(
+            2019, 8, 27,
+            0, 0, 0
+        ))
+
+    def test_int(self):
+        with self.assertRaises(PhoneDateValueError):
+            validate_phone_datetime(1)
+
+    def test_day_string(self):
+        with self.assertRaises(PhoneDateValueError):
+            validate_phone_datetime('Monday')
+
+    def test_int_string(self):
+        with self.assertRaises(PhoneDateValueError):
+            validate_phone_datetime('1')
