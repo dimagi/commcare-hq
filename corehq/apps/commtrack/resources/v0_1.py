e81e0b07b22a0dca8659b745d4f53a4aa148710e
Jonathan Emord <jonathan.emord@gmail.com>
2019-08-30 14:02:22 -0400

    API uses SQLProduct

--- corehq/apps/commtrack/resources/v0_1.py
+++ corehq/apps/commtrack/resources/v0_1.py
@@ -3,8 +3,8 @@ from tastypie import fields
 from corehq.apps.api.resources import HqBaseResource
 from corehq.apps.api.resources.auth import DomainAdminAuthentication
 from corehq.apps.api.resources.meta import CustomResourceMeta
-from corehq.apps.api.util import get_object_or_not_exist
-from corehq.apps.products.models import Product
+from corehq.apps.api.util import object_does_not_exist
+from corehq.apps.products.models import SQLProduct
 
 
 """
@@ -17,21 +17,22 @@ https://confluence.dimagi.com/display/lmis/API
 class ProductResource(HqBaseResource):
 
     type = "product"
-    id = fields.CharField(attribute='_id', readonly=True, unique=True)
+    id = fields.CharField(attribute='product_id', readonly=True, unique=True)
     code = fields.CharField(attribute='code', readonly=True, unique=True)
     name = fields.CharField(attribute='name', readonly=True)
-    unit = fields.CharField(attribute='unit', readonly=True, null=True)
+    unit = fields.CharField(attribute='units', readonly=True, null=True)
     description = fields.CharField(attribute='description', readonly=True, null=True)
     category = fields.CharField(attribute='category', readonly=True, null=True)
     last_modified = fields.DateTimeField(attribute='last_modified', readonly=True, null=True)
-    # TODO:
-    # price?
 
     def obj_get(self, request, **kwargs):
-        return get_object_or_not_exist(Product, kwargs['pk'], kwargs['domain'])
+        try:
+            SQLProduct.objects.get(product_id=kwargs['pk'], domain=kwargs['domain'])
+        except SQLProduct.DoesNotExist:
+            raise object_does_not_exist("Product", kwargs['pk'])
 
     def obj_get_list(self, request, **kwargs):
-        return Product.by_domain(kwargs['domain'])
+        return SQLProduct.active_objects.filter(domain=kwargs['domain'])
 
     class Meta(CustomResourceMeta):
         authentication = DomainAdminAuthentication()
