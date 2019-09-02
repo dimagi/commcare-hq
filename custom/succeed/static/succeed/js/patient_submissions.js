774abee4385e63a7a6af84d83884dbe4b02d177b
Jennifer Schweers <orange.jenny@gmail.com>
2019-09-02 13:32:19 -0400

    Extracted patient_submissions.js

---
+++ custom/succeed/static/succeed/js/patient_submissions.js
@@ -0,0 +1,17 @@
+hqDefine("succeed/js/patient_submissions", function () {
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data");
+    $(function() {
+        var $paramSelectorForm = $('#paramSelectorForm');
+        if ($paramSelectorForm.length && ! $paramSelectorForm.find('#patient_id_field').length) {
+            $('<input>').attr({id: 'patient_id_field', type: 'hidden', name: 'patient_id', value: initialPageData.get('patient_id') }).appendTo('#paramSelectorForm');
+        }
+
+        var tableOptions = initialPageData.get('report_table_js_options');
+        if (tableOptions && tableOptions.datatables) {
+            var id = '#report_table_' + initialPageData.get('slug');
+            var table = $(id).dataTable();
+            var oSettings = table.fnSettings().aaSorting = [[2, 'desc']];
+            table.fnDraw();
+        }
+    });
+});
