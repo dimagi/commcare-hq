308b99e0cbb681eec752cf0dbcb4620b6283a3a9
Jennifer Schweers <orange.jenny@gmail.com>
2019-09-02 13:20:55 -0400

    Cleaned up ucla_table logic

--- custom/succeed/static/succeed/js/main.js
+++ custom/succeed/static/succeed/js/main.js
@@ -1,47 +1,53 @@
 hqDefine("succeed/js/main", function () {
-        $(function() {
-        {% if report_table and report_table.datatables %}
-            var reportTables = hqImport("reports/js/config.dataTables.bootstrap").HQReportDataTables({
-                dataTableElem: '#report_table_{{ report.slug }}',
-                defaultRows: {{ report_table.default_rows|default:10 }},
-                startAtRowNum: {{ report_table.start_at_row|default:0 }},
-                showAllRowsOption: {{ report_table.show_all_rows|JSON }},
-
-                {% if report_table.headers.render_aoColumns %}aoColumns: {{ report_table.headers.render_aoColumns|JSON }},{% endif %}
-                autoWidth: {{ report_table.headers.auto_width|JSON }},
-                {% if report_table.headers.custom_sort %}customSort: {{ report_table.headers.custom_sort|JSON }},{% endif %}
-
-                {% if report_table.pagination.hide %}
-                show_pagination: false,
-                {% endif %}
-
-                {% if report_table.pagination.is_on %}
-                    ajaxSource: '{{ report_table.pagination.source }}',
-                    ajaxParams: {{ report_table.pagination.params|JSON }},
-                {% endif %}
-
-                {% if report_table.bad_request_error_text %}
-                    badRequestErrorText: "<span class='label label-danger'>Sorry!</span> {{ report_table.bad_request_error_text }}",
-                {% endif %}
-
-                {% if report_table.left_col.is_fixed %}
-                    fixColumns: true,
-                    fixColsNumLeft: {{ report_table.left_col.fixed.num }},
-                    fixColsWidth: {{ report_table.left_col.fixed.width }},
-                {% endif %}
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data");
+    $(function() {
+        var tableOptions = initialPageData.get('report_table_js_options');
+        if (tableOptions && tableOptions.datatables) {
+            var reportTablesOptions = {
+                dataTableElem: '#report_table_' + initialPageData.get('slug'),
+                defaultRows: tableOptions.default_rows || 10,
+                startAtRowNum: tableOptions.start_at_row || 0,
+                showAllRowsOption: tableOptions.show_all_rows,
+                autoWidth: tableOptions.headers.auto_width,
                 includeFilter: true,
                 aaSorting: [[5, 'asc']],
-            });
+            };
+            if (reportTablesOptions.headers.render_aoColumns) {
+                reportTablesOptions.aoColumns = tableOptions.headers.render_aoColumns;
+            }
+            if (tableOptions.headers.custom_sort) {
+                reportTablesOptions.customSort = report_table.headers.custom_sort;
+            }
+            if (tableOptions.pagination.hide) {
+                reportTablesOptions.show_pagination = false;
+            }
+            if (tableOptions.pagination.is_on) {
+                reportTablesOptions = _.extend(reportTablesOptions, {
+                    ajaxSource: tableOptions.pagination.source,
+                    ajaxParams: tableOptions.pagination.params,
+                };
+            }
+            if (tableOptions.bad_request_error_text) {
+                reportTablesOptions.badRequestErrorText = "<span class='label label-danger'>Sorry!</span> " + tableOptions.bad_request_error_text;
+            }
+            if (tableOptions.left_col.is_fixed) {
+                reportTablesOptions = _.extend(reportTablesOptions, {
+                    fixColumns: true,
+                    fixColsNumLeft: tableOptions.left_col.fixed.num,
+                    fixColsWidth: tableOptions.left_col.fixed.width,
+                });
+            }
+            var reportTables = hqImport("reports/js/config.dataTables.bootstrap").HQReportDataTables(reportTablesOptions);
             var standardHQReport = hqImport("reports/js/standard_hq_report").getStandardHQReport();
             if (typeof standardHQReport !== 'undefined') {
                 standardHQReport.handleTabularReportCookies(reportTables);
             }
             reportTables.render();
-        {% endif %}
+        }
 
-            $('.header-popover').popout({
-                trigger: 'hover',
-                placement: 'bottom'
-            });
+        $('.header-popover').popout({
+            trigger: 'hover',
+            placement: 'bottom'
         });
+    });
 });
