6b133ec5f6f40701cc5c184d21ef9fd2a96b991f
Jennifer Schweers <orange.jenny@gmail.com>
2019-09-02 11:53:44 -0400

    Moved edit page script into main.js

--- custom/_legacy/pact/static/pact/js/main.js
+++ custom/_legacy/pact/static/pact/js/main.js
@@ -1,7 +1,30 @@
 hqDefine("pact/js/main", function () {
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data");
+
     $(function () {
+        // Widget initialization
         $("#tbl_issues").tablesorter();
         $("abbr.timeago").timeago();
         $(".timeago").timeago();
-    });
+
+        // Edit page
+        $("#submit_button").click(function() {
+            var form_data = $("#pt_edit_form").serialize();
+            var api_url = initialPageData.reverse('pactdata_1') + "?case_id=" + initialPageData.get('patient_id') + "&method=patient_edit";
+            console.log(form_data);
+            var send_xhr = $.ajax({
+                "type": "POST",
+                "url":  api_url,
+                "data": form_data,
+                "success": function(data) {
+                    window.location.href = initialPageData.get('pt_root_url') + "&view=info";
+                },
+                "error": function(data) {
+                    if(data.responseText !== undefined) {
+                        $("#form_errors").html(data.responseText);
+                    }
+                }
+            });
+          });
+      });
 });
