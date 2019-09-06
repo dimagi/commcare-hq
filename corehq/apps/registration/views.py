d3e5af55075e2b1b479a839c7865951be7f24110
Biyeun Buczyk <biyeun@dimagi.com>
2019-09-06 10:42:59 -0500

    don't add new account user in the new trial experience on the web apps page, as by default they only have community

--- corehq/apps/registration/views.py
+++ corehq/apps/registration/views.py
@@ -476,7 +476,7 @@ def confirm_domain(request, guid=''):
         track_confirmed_account_on_hubspot.delay(requesting_user)
         request.session['CONFIRM'] = True
 
-        if settings.IS_SAAS_ENVIRONMENT:
+        if settings.IS_SAAS_ENVIRONMENT and not settings.ENABLE_NEW_TRIAL_EXPERIENCE:
             # For AppCues v3, land new user in Web Apps
             view_name = get_cloudcare_urlname(requested_domain.name)
         return HttpResponseRedirect(reverse(view_name, args=view_args))
