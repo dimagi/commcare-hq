675896d8a2623773ad50f4b97615b79f51e915cd
Daniel Roberts <droberts@dimagi.com>
2019-09-10 14:45:19 -0500

    Give uninstall-requirements.sh nicer input and output

--- scripts/uninstall-requirements.sh
+++ scripts/uninstall-requirements.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
-set -ev
-
-uninstall=requirements/uninstall-requirements.txt
+set -e
+uninstall=$1
+if [[ -z "${uninstall}" ]]
+then
+    uninstall=requirements/uninstall-requirements.txt
+fi
 tmp=requirements/uninstall-tmp.txt
 
-join <(grep -v '#' $uninstall | sort -n) <(pip freeze | grep -v '^-' | cut -d'=' -f1 | sort -n) > $tmp
-join <(grep -v '#' $uninstall | sort -n) <(pip freeze | grep -v '^-' | sort -n) >> $tmp
+join <(grep -v '#' ${uninstall} | sort -n) <(pip freeze | grep -v '^-' | cut -d'=' -f1 | sort -n) > ${tmp}
+join <(grep -v '#' ${uninstall} | sort -n) <(pip freeze | grep -v '^-' | sort -n) >> ${tmp}
 
-# if $tmp isn't just a single newline character
-if [ -s $tmp ]
+# if ${tmp} isn't just a single newline character
+if [ -s ${tmp} ]
 then
-    pip uninstall -r $tmp --yes
+    echo "Uninstalling from ${uninstall}"
+    pip uninstall -r ${tmp} --yes
+else
+    echo "Nothing to uninstall from ${uninstall}"
 fi
-rm $tmp
+rm ${tmp}
