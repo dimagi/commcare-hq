559fd651374de1a1dab968661c170c3c3569d0b2
Daniel Roberts <droberts@dimagi.com>
2019-09-10 17:06:36 -0500

    Revert changes to scripts/uninstall-requirements.sh
    decided we will just stop using it and delete it
    once everyone has had time to update commcare-cloud

--- scripts/uninstall-requirements.sh
+++ scripts/uninstall-requirements.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
-set -e
-uninstall=$1
-if [[ -z "${uninstall}" ]]
-then
-    uninstall=requirements/uninstall-requirements.txt
-fi
+set -ev
+
+uninstall=requirements/uninstall-requirements.txt
 tmp=requirements/uninstall-tmp.txt
 
-join <(grep -v '#' ${uninstall} | sort -n) <(pip freeze | grep -v '^-' | cut -d'=' -f1 | sort -n) > ${tmp}
-join <(grep -v '#' ${uninstall} | sort -n) <(pip freeze | grep -v '^-' | sort -n) >> ${tmp}
+join <(grep -v '#' $uninstall | sort -n) <(pip freeze | grep -v '^-' | cut -d'=' -f1 | sort -n) > $tmp
+join <(grep -v '#' $uninstall | sort -n) <(pip freeze | grep -v '^-' | sort -n) >> $tmp
 
-# if ${tmp} isn't just a single newline character
-if [ -s ${tmp} ]
+# if $tmp isn't just a single newline character
+if [ -s $tmp ]
 then
-    echo "Uninstalling from ${uninstall}"
-    pip uninstall -r ${tmp} --yes
-else
-    echo "Nothing to uninstall from ${uninstall}"
+    pip uninstall -r $tmp --yes
 fi
-rm ${tmp}
+rm $tmp
