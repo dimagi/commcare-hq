d52b83870ec389ce12e9c4e3791bafd400e1d06e
Sravan Reddy <sravfeyn@gmail.com>
2019-09-10 18:08:52 +0530

    pass correct vars

--- corehq/util/context_processors.py
+++ corehq/util/context_processors.py
@@ -44,22 +44,22 @@ def get_per_domain_context(project, request=None):
             domain_has_privilege(project.name, privileges.CUSTOM_BRANDING)):
         custom_logo_url = reverse('logo', args=[project.name])
 
-    def allow_report_issue(user):
-        if toggles.ICDS.enabled(user.domain) and user.is_commcare_user():
-            role = user.get_domain_membership(user.domain).role
+    def allow_report_issue(user, domain):
+        if toggles.ICDS.enabled(domain) and user.is_commcare_user():
+            role = user.get_domain_membership(domain).role
             if not role:
                 return False
-        return user.has_permission(project.name, 'report_an_issue')
+        return user.has_permission(domain, 'report_an_issue')
 
     if getattr(request, 'couch_user', None) and project:
-        allow_report_an_issue = allow_report_issue(request.couch_user)
+        allow_report_an_issue = allow_report_issue(request.couch_user, project.name)
     elif settings.ENTERPRISE_MODE:
         if not getattr(request, 'couch_user', None):
             allow_report_an_issue = False
         elif request.couch_user.is_web_user():
             allow_report_an_issue = True
         else:
-            allow_report_issue(request.couch_user)
+            allow_report_issue(request.couch_user, request.couch_user.domain)
     else:
         allow_report_an_issue = True
 
