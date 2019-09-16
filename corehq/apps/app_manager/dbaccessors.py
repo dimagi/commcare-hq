089cd8a5cfaf9b9cc2965be7a54da456d8a8e4c5
Jennifer Schweers <orange.jenny@gmail.com>
2019-09-16 10:16:37 -0400

    Added comment

--- corehq/apps/app_manager/dbaccessors.py
+++ corehq/apps/app_manager/dbaccessors.py
@@ -65,6 +65,7 @@ def get_latest_released_app_versions_by_app_id(domain):
         endkey=['^ReleasedApplications', domain, {}],
         include_docs=False,
     ).all()
+    # key[3] will be latest released version since view is ordered by app_id, version asc
     return {r['key'][2]: r['key'][3] for r in results}
 
 
