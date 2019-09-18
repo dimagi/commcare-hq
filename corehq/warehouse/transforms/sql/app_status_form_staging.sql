8646327ce7c937276d8939ed9617057a694a963c
Simon Kelly <skelly@dimagi.com>
2019-09-18 16:34:57 +0200

    reformat

--- corehq/warehouse/transforms/sql/app_status_form_staging.sql
+++ corehq/warehouse/transforms/sql/app_status_form_staging.sql
@@ -7,7 +7,6 @@ INSERT INTO {{ app_status_form_staging }} (
        domain,
        batch_id
 )
-
 SELECT
     last_submission,
     submission_build_version,
@@ -18,38 +17,39 @@ SELECT
     '{{ batch_id }}'
 FROM
 (
-SELECT
-	row_number() over wnd,
-	CASE
-	    WHEN received_on < app_status.last_form_submission_date
-	    THEN app_status.last_form_submission_date
-	    ELSE  received_on
-	END AS last_submission,
-	CASE
-	    WHEN received_on < app_status.last_form_submission_date
-	    THEN app_status.last_form_app_build_version
-            ELSE build.version::VARCHAR
-	END AS submission_build_version,
-	CASE
-	    WHEN received_on < app_status.last_form_submission_date
-	    THEN app_status.last_form_app_commcare_version
-	    ELSE commcare_version
-	END AS commcare_version,
-	    form_fact.user_dim_id as user_dim_id,
-	    app_dim.id as app_dim_id,
-	    form_fact.domain as domain
-	FROM {{ form_fact }} as form_fact
-	LEFT JOIN {{ user_dim }} as user_dim
-	ON user_dim.id = form_fact.user_dim_id
-	LEFT JOIN {{ application_dim }} as app_dim
-	ON form_fact.app_id = app_dim.application_id
-	LEFT JOIN {{ application_dim }} as build
-	ON form_fact.build_id = build.application_id
-    LEFT JOIN {{ app_status_fact }} as app_status
-	ON app_dim.id = app_status.app_dim_id and form_fact.user_dim_id = app_status.user_dim_id
-	WHERE form_fact.user_id <> '' and user_dim.doc_type='CommCareUser'
-        WINDOW wnd AS (
-            PARTITION BY user_dim.id, COALESCE(app_dim.id, -1) ORDER BY received_on AT TIME ZONE 'UTC' DESC
-            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
-	)
+    SELECT
+        row_number() over wnd,
+        CASE
+            WHEN received_on < app_status.last_form_submission_date
+            THEN app_status.last_form_submission_date
+            ELSE  received_on
+        END AS last_submission,
+        CASE
+            WHEN received_on < app_status.last_form_submission_date
+            THEN app_status.last_form_app_build_version
+                ELSE build.version::VARCHAR
+        END AS submission_build_version,
+        CASE
+            WHEN received_on < app_status.last_form_submission_date
+            THEN app_status.last_form_app_commcare_version
+            ELSE commcare_version
+        END AS commcare_version,
+        form_fact.user_dim_id as user_dim_id,
+        app_dim.id as app_dim_id,
+        form_fact.domain as domain
+    FROM {{ form_fact }} as form_fact
+        LEFT JOIN {{ user_dim }} as user_dim
+        ON user_dim.id = form_fact.user_dim_id
+        LEFT JOIN {{ application_dim }} as app_dim
+        ON form_fact.app_id = app_dim.application_id
+        LEFT JOIN {{ application_dim }} as build
+        ON form_fact.build_id = build.application_id
+        LEFT JOIN {{ app_status_fact }} as app_status
+        ON app_dim.id = app_status.app_dim_id and form_fact.user_dim_id = app_status.user_dim_id
+    WHERE form_fact.user_id <> '' and user_dim.doc_type='CommCareUser'
+    WINDOW wnd AS (
+        PARTITION BY user_dim.id, COALESCE(app_dim.id, -1)
+        ORDER BY received_on AT TIME ZONE 'UTC' DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    )
 ) as form_data where row_number=1;
--- corehq/warehouse/transforms/sql/app_status_synclog_staging.sql
+++ corehq/warehouse/transforms/sql/app_status_synclog_staging.sql
@@ -11,20 +11,21 @@ SELECT
     '{{ batch_id }}'
 FROM
 (
-SELECT
-	row_number() over wnd,
-	CASE
-	    WHEN sync_date < app_status.last_sync_log_date
-	    THEN app_status.last_sync_log_date
-	    ELSE sync_date
-	END AS last_sync,
-	sync_fact.user_dim_id as user_dim_id,
-	sync_fact.domain as domain
-        FROM {{ synclog_fact }} as sync_fact
-	LEFT JOIN {{ app_status_fact }} as app_status
-	ON sync_fact.user_dim_id = app_status.user_dim_id
-        WINDOW wnd AS (
-            PARTITION BY sync_fact.user_dim_id ORDER BY sync_date AT TIME ZONE 'UTC' DESC, app_status.last_sync_log_date AT TIME ZONE 'UTC' DESC
-            ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+    SELECT
+        row_number() over wnd,
+        CASE
+            WHEN sync_date < app_status.last_sync_log_date
+            THEN app_status.last_sync_log_date
+            ELSE sync_date
+        END AS last_sync,
+        sync_fact.user_dim_id as user_dim_id,
+        sync_fact.domain as domain
+    FROM {{ synclog_fact }} as sync_fact
+	    LEFT JOIN {{ app_status_fact }} as app_status
+	    ON sync_fact.user_dim_id = app_status.user_dim_id
+    WINDOW wnd AS (
+        PARTITION BY sync_fact.user_dim_id
+        ORDER BY sync_date AT TIME ZONE 'UTC' DESC, app_status.last_sync_log_date AT TIME ZONE 'UTC' DESC
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
 	)
 ) as sync_data where row_number=1;
