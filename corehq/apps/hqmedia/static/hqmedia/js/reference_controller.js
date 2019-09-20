735be4a001ccad18d4323f68bdbe7d6aa2b120e7
Jennifer Schweers <orange.jenny@gmail.com>
2019-09-20 13:51:10 -0400

    Moved MultimediaReferenceController model into references_main.js
    The other models are used on other pages.

--- corehq/apps/hqmedia/static/hqmedia/js/reference_controller.js
+++ corehq/apps/hqmedia/static/hqmedia/js/reference_controller.js
@@ -13,88 +13,6 @@ hqDefine("hqmedia/js/reference_controller",[
 ) {
     var HQMediaUploaders = mediaUploaders.get();
 
-    function MultimediaReferenceController() {
-        var self = {};
-        self.references = ko.observableArray();
-        self.showMissingReferences = ko.observable(false);
-        self.totals = ko.observableArray();
-
-        self.isInitialLoad = ko.observable(true);
-        self.showPaginationSpinner = ko.observable(false);
-        self.itemsPerPage = ko.observable();
-        self.totalItems = ko.observable();
-
-        self.toggleRefsText = ko.computed(function () {
-            return (self.showMissingReferences()) ? gettext("Show All References") : gettext("Show Only Missing References");
-        }, self);
-
-        self.goToPage = function (page) {
-            self.showPaginationSpinner(true);
-            $.ajax({
-                url: hqImport("hqwebapp/js/initial_page_data").reverse('hqmedia_references'),
-                data: {
-                    json: 1,
-                    page: page,
-                    limit: self.itemsPerPage(),
-                    only_missing: self.showMissingReferences() ? 1 : 0,
-                },
-                success: function (data) {
-                    self.isInitialLoad(false);
-                    self.showPaginationSpinner(false);
-                    self.references(_.compact(_.map(data.references, function (ref) {
-                        var objRef = data.object_map[ref.path];
-                        if (ref.media_class === "CommCareImage") {
-                            var imageRef = ImageReference(ref);
-                            imageRef.setObjReference(objRef);
-                            return imageRef;
-                        } else if (ref.media_class === "CommCareAudio") {
-                            var audioRef = AudioReference(ref);
-                            audioRef.setObjReference(objRef);
-                            return audioRef;
-                        } else if (ref.media_class === "CommCareVideo") {
-                            var videoRef = VideoReference(ref);
-                            videoRef.setObjReference(objRef);
-                            return videoRef;
-                        } else {
-                            throw new Error("Found unexpected media class: " + ref.media_class);
-                        }
-                        // Other multimedia, like HTML print templates, is ignored by the reference checker
-                        return null;
-                    })));
-                    self.totalItems(data.total_rows);
-                    self.totals(data.totals);
-                    $('.preview-media').tooltip();
-                },
-                error: function () {
-                    self.showPaginationSpinner(false);
-                    hqImport('hqwebapp/js/alert_user').alert_user(gettext("Error fetching multimedia, " +
-                        "please try again or report an issue if the problem persists."), 'danger');
-                },
-            });
-        };
-
-        self.toggleMissingRefs = function (sender, event) {
-            self.showMissingReferences(!self.showMissingReferences());
-            self.goToPage(1);
-        };
-
-        self.incrementTotals = function (trigger, event, data) {
-            var newTotals = _.map(self.totals(), function (media) {
-                if (media.media_type === data.media_type && media.paths.indexOf(data.path) < 0) {
-                    media = _.clone(media);
-                    media.paths.push(data.path);
-                    media.matched = media.paths.length;
-                }
-                return media;
-            });
-            self.totals(newTotals);
-        };
-
-        self.goToPage(1);
-
-        return self;
-    }
-
     function BaseMediaReference(ref) {
         'use strict';
         var self = {};
@@ -205,7 +123,6 @@ hqDefine("hqmedia/js/reference_controller",[
     ImageReference.prototype = Object.create(BaseMediaReference.prototype);
     ImageReference.prototype.constructor = ImageReference;
 
-
     function AudioReference(ref) {
         'use strict';
         var self = {};
@@ -217,7 +134,6 @@ hqDefine("hqmedia/js/reference_controller",[
 
     AudioReference.prototype = Object.create(BaseMediaReference.prototype);
     AudioReference.prototype.constructor = AudioReference;
-
 
     function VideoReference(ref) {
         'use strict';
@@ -233,9 +149,9 @@ hqDefine("hqmedia/js/reference_controller",[
 
 
     return {
-        'MultimediaReferenceController': MultimediaReferenceController,
         'BaseMediaReference': BaseMediaReference,
         'ImageReference': ImageReference,
+        'AudioReference': AudioReference,
+        'VideoReference': VideoReference,
     };
-
 });
