b79ea21de0e4a82857d2970a8a34e2c44ff22fb1
Ethan Soergel <esoergel@users.noreply.github.com>
2019-09-25 18:23:06 -0400

    Revert "Revert "Revert "Multiple master apps for linked apps"""

--- corehq/apps/linked_domain/tests/test_update_roles.py
+++ corehq/apps/linked_domain/tests/test_update_roles.py
@@ -10,6 +10,7 @@ class TestUpdateRoles(BaseLinkedAppsTest):
     @classmethod
     def setUpClass(cls):
         super(TestUpdateRoles, cls).setUpClass()
+        cls.linked_app.master = cls.master1.get_id
         cls.linked_app.save()
 
         cls.role = UserRole(
@@ -17,6 +18,9 @@ class TestUpdateRoles(BaseLinkedAppsTest):
             name='test',
             permissions=Permissions(
                 edit_data=True,
+                view_web_apps_list=[
+                    cls.master1.get_id
+                ],
                 view_report_list=[
                     'corehq.reports.DynamicReportmaster_report_id'
                 ]
@@ -34,7 +38,7 @@ class TestUpdateRoles(BaseLinkedAppsTest):
             role.delete()
         super(TestUpdateRoles, self).tearDown()
 
-    def test_update_report_list(self):
+    def test_update_web_apps_list(self):
         self.assertEqual([], UserRole.by_domain(self.linked_domain))
 
         report_mapping = {'master_report_id': 'linked_report_id'}
@@ -43,4 +47,5 @@ class TestUpdateRoles(BaseLinkedAppsTest):
 
         roles = UserRole.by_domain(self.linked_domain)
         self.assertEqual(1, len(roles))
+        self.assertEqual(roles[0].permissions.view_web_apps_list, [self.linked_app._id])
         self.assertEqual(roles[0].permissions.view_report_list, [get_ucr_class_name('linked_report_id')])
