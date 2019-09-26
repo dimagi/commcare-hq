3662c272e957386b551a8828ad78cccbc00602ed
Simon Kelly <skelly@dimagi.com>
2019-09-26 12:40:19 +0200

    move icds middleware to localsettings

--- settings.py
+++ settings.py
@@ -1371,8 +1371,6 @@ seen = set()
 INSTALLED_APPS = [x for x in INSTALLED_APPS if x not in seen and not seen.add(x)]
 
 MIDDLEWARE += LOCAL_MIDDLEWARE
-if 'icds-ucr' in DATABASES:
-    MIDDLEWARE.append('custom.icds_reports.middleware.ICDSAuditMiddleware')
 
 ### Shared drive settings ###
 SHARED_DRIVE_CONF = helper.SharedDriveConfiguration(
