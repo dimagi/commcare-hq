f00ee4e7896fdbb6ddf050e93799c5ff4e37318d
Cory Zue <czue@dimagi.com>
2019-09-27 13:25:32 +0200

    add stub functionality for supporting SQL domains

--- corehq/apps/hqadmin/management/commands/republish_case_changes.py
+++ corehq/apps/hqadmin/management/commands/republish_case_changes.py
@@ -1,5 +1,6 @@
 from django.core.management import BaseCommand
 
+from corehq.form_processor.utils import should_use_sql_backend
 from dimagi.utils.chunked import chunked
 
 from casexml.apps.case.models import CommCareCase
@@ -22,17 +23,30 @@ class Command(BaseCommand):
         self.publish_cases(domain, case_ids)
 
     def publish_cases(self, domain, case_ids):
-        from corehq.apps.hqcase.management.commands.backfill_couch_forms_and_cases import (
-            publish_change, create_case_change_meta)
-        for ids in chunked(case_ids, 500):
-            doc_id_rev_list = bulk_get_revs(CommCareCase.get_db(), ids)
-            for doc_id, doc_rev in doc_id_rev_list:
-                publish_change(
-                    create_case_change_meta(domain, doc_id, doc_rev)
-                )
+        if should_use_sql_backend(domain):
+            _publish_cases_for_couch(domain, case_ids)
+        else:
+            _publish_cases_for_sql(domain, case_ids)
 
 
 def _get_case_ids(case_ids_file):
     with open(case_ids_file, 'r') as f:
         lines = f.readlines()
         return [l.split(',')[0].strip() for l in lines]
+
+
+def _publish_cases_for_couch(domain, case_ids):
+    from corehq.apps.hqcase.management.commands.backfill_couch_forms_and_cases import (
+        publish_change, create_case_change_meta)
+    for ids in chunked(case_ids, 500):
+        doc_id_rev_list = bulk_get_revs(CommCareCase.get_db(), ids)
+        for doc_id, doc_rev in doc_id_rev_list:
+            publish_change(
+                create_case_change_meta(domain, doc_id, doc_rev)
+            )
+
+
+def _publish_cases_for_sql(domain, case_ids):
+    print('sql domains not supported yet')
+
+
