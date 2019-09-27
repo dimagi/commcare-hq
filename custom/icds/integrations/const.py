31fcf6618da50d6e82fd456cb367a401b23b685a
mkangia <mkangia@dimagi.com>
2019-09-27 16:06:12 +0530

    add client

---
+++ custom/icds/integrations/const.py
@@ -0,0 +1,5 @@
+PHI_URLS = {
+    'search': 'https://api.ors.gov.in/openAPI/phid/search_phid_by_params',
+    'get': 'https://api.ors.gov.in/openAPI/phid/search_by_phid',
+    'validate': 'https://api.ors.gov.in/openAPI/phid/phid_validation',
+}
