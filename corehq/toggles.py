3350a93928561c3e6dc6d32e54ddde44863b9e63
Jennifer Schweers <orange.jenny@gmail.com>
2019-10-01 16:33:34 -0400

    WIP

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1,19 +1,21 @@
+import hashlib
+import math
 import inspect
 from collections import namedtuple
 from functools import wraps
-import hashlib
-import math
 
-from django.contrib import messages
 from django.conf import settings
+from django.contrib import messages
 from django.http import Http404
 from django.urls import reverse
 from django.utils.safestring import mark_safe
 
 from couchdbkit import ResourceNotFound
-from corehq.util.quickcache import quickcache
+
 from toggle.models import Toggle
-from toggle.shortcuts import toggle_enabled, set_toggle
+from toggle.shortcuts import set_toggle, toggle_enabled
+
+from corehq.util.quickcache import quickcache
 
 Tag = namedtuple('Tag', 'name css_class description')
 TAG_CUSTOM = Tag(
