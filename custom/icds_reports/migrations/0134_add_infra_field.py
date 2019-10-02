57b97b55cfdaeec26e9a8a08b9dfdd4561c28c02
Rohit Negi <rohit25.negi@gmail.com>
2019-10-02 12:51:54 +0530

    fix migration file confict fix lynter

--- custom/icds_reports/migrations/0134_add_infra_field.py
+++ custom/icds_reports/migrations/0134_add_infra_field.py
@@ -3,14 +3,16 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+
 from corehq.sql_db.operations import RawSQLMigration
 
 migrator = RawSQLMigration(('custom', 'icds_reports', 'migrations', 'sql_templates'))
 
+
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0132_update_nic_view'),
+        ('icds_reports', '0133_auto_20191001_0835'),
     ]
 
     operations = [
