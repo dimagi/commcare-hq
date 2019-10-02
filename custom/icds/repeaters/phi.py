59e58df766a2b43f0c709dc36dfe57c00662048d
mkangia <mkangia@dimagi.com>
2019-10-03 02:04:06 +0530

    add repeaters

---
+++ custom/icds/repeaters/phi.py
@@ -0,0 +1,43 @@
+from django.utils.translation import ugettext_lazy as _
+
+from corehq import toggles
+from corehq.motech.repeaters.models import CaseRepeater
+from custom.icds.integrations.phi import send_request
+
+
+class BasePHIRepeater(CaseRepeater):
+    @classmethod
+    def available_for_domain(cls, domain):
+        return toggles.PHI_CAS_INTEGRATION.enabled(domain)
+
+
+class SearchByParamsRepeater(BasePHIRepeater):
+    payload_generator_classes = ()
+    friendly_name = _("Search for Beneficiary via params to get PHI ID")
+
+    def allowed_to_forward(self, payload):
+        return (
+            super(SearchByParamsRepeater, self).allowed_to_forward(payload) and
+            not payload.get_case_property('phid_validated')
+        )
+
+    def send_request(self, repeat_record, payload):
+        return send_request('search', payload)
+
+
+class ValidatePHIDRepeater(CaseRepeater):
+    payload_generator_classes = ()
+    friendly_name = _("Validate PHI ID")
+
+    @classmethod
+    def available_for_domain(cls, domain):
+        return toggles.PHI_CAS_INTEGRATION.enabled(domain)
+
+    def allowed_to_forward(self, payload):
+        return (
+            super(ValidatePHIDRepeater, self).allowed_to_forward(payload) and
+            payload.get_case_property('phid_for_beneficiary')
+        )
+
+    def send_request(self, repeat_record, payload):
+        return send_request('validate', payload)
