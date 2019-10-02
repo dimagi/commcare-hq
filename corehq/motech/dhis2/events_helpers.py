18cc06f7e2f294b61294f7da34b7bef73693ca7f
Norman Hooper <nhooper@dimagi.com>
2019-10-02 09:30:48 +0200

    Support programs with stages

--- corehq/motech/dhis2/events_helpers.py
+++ corehq/motech/dhis2/events_helpers.py
@@ -20,6 +20,7 @@ def get_event(domain, config, form_json=None, info=None):
     event = {}
     event_property_functions = [
         _get_program,
+        _get_program_stage,
         _get_org_unit,
         _get_event_date,
         _get_event_status,
@@ -33,6 +34,15 @@ def get_event(domain, config, form_json=None, info=None):
 
 def _get_program(config, case_trigger_info):
     return {'program': config.program_id}
+
+
+def _get_program_stage(config, case_trigger_info):
+    program_stage_id = None
+    if config.program_stage_id:
+        program_stage_id = config.program_stage_id.get_value(case_trigger_info)
+    if program_stage_id:
+        return {'programStage': program_stage_id}
+    return {}
 
 
 def _get_org_unit(config, case_trigger_info):
