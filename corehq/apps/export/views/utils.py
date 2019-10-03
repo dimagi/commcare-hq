f089abfa46e842bbe1b28c9fba7a9aa2a3aefb6c
Biyeun Buczyk <biyeun@dimagi.com>
2019-10-03 21:27:05 +0200

    truncate labels at 511 characters

--- corehq/apps/export/views/utils.py
+++ corehq/apps/export/views/utils.py
@@ -387,3 +387,6 @@ def clean_odata_columns(export_instance):
         for column in table.columns:
             column.label = column.label.replace('@', '').replace(
                 '.', ' ').replace('\n', '').replace('\t', ' ').replace('#', '')
+            # truncate labels for PowerBI limits
+            if len(column.label) >= 511:
+                column.label = column.label[:511]
