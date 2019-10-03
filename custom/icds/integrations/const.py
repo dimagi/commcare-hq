d7f9457340a63dae911fb478571e4afd30f06c19
mkangia <mkangia@dimagi.com>
2019-10-03 14:24:28 +0530

    fix urls [skip ci]

--- custom/icds/integrations/const.py
+++ custom/icds/integrations/const.py
@@ -1,5 +1,5 @@
 PHI_URLS = {
-    'search': 'https://api.ors.gov.in/openAPI/phid/search_phid_by_params',
+    'search': 'https://api.ors.gov.in/openAPI/phi/beneficiarysearchrequest',
     'get': 'https://api.ors.gov.in/openAPI/phi/getDetailsByPhi',
-    'validate': 'https://api.ors.gov.in/openAPI/phid/phid_validation',
+    'validate': 'https://api.ors.gov.in/openAPI/phi/phid_validation',
 }
