57329a4a30bb9224e3f61481a8410d21d3a39b18
Jennifer Schweers <orange.jenny@gmail.com>
2019-10-04 16:47:48 -0400

    Removed announcements code
    Guessing this is an old feature. There are no usages of the announcement-control
    class and no url with 'annoucements' in it.

--- corehq/apps/hqwebapp/static/hqwebapp/js/hq.helpers.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/hq.helpers.js
@@ -9,19 +9,6 @@ hqDefine("hqwebapp/js/hq.helpers", [
     _,
     googleAnalytics
 ) {
-    var clearAnnouncement = function (announcementID) {
-        $.ajax({
-            url: '/announcements/clear/' + announcementID,
-        });
-    };
-
-    $('.page-level-alert').on('closed', function () {
-        var announcement_id = $('.page-level-alert').find('.announcement-control').data('announcementid');
-        if (announcement_id) {
-            clearAnnouncement(announcement_id);
-        }
-    });
-
     // disable-on-submit is a class for form submit buttons so they're automatically disabled when the form is submitted
     $(document).on('submit', 'form', function (ev) {
         var form = $(ev.target);
