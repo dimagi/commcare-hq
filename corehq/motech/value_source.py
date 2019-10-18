02417de6ca3f919a0d6797dfc5bfb448bebe5f2a
Norman Hooper <nhooper@dimagi.com>
2019-10-18 09:36:47 +0200

    Stricter validation

--- corehq/motech/value_source.py
+++ corehq/motech/value_source.py
@@ -199,7 +199,7 @@ class FormQuestion(ValueSource):
     """
     A reference to a form question
     """
-    form_question = StringProperty()  # e.g. "/data/foo/bar"
+    form_question = StringProperty(required=True, validators=not_blank)  # e.g. "/data/foo/bar"
 
     def _get_commcare_value(self, case_trigger_info):
         return case_trigger_info.form_question_values.get(self.form_question)
@@ -221,7 +221,7 @@ class ConstantString(ValueSource):
     #       }
     #     }
     #
-    value = StringProperty()
+    value = StringProperty(required=True)
 
     def __eq__(self, other):
         return (
@@ -316,7 +316,7 @@ class CasePropertyMap(CaseProperty):
     #       }
     #     }
     #
-    value_map = DictProperty()
+    value_map = DictProperty(required=True)
 
     def serialize(self, value):
         # Don't bother serializing. self.value_map does that already.
@@ -336,7 +336,7 @@ class FormQuestionMap(FormQuestion):
     """
     Maps form question values to OpenMRS values or concept UUIDs
     """
-    value_map = DictProperty()
+    value_map = DictProperty(required=True)
 
     def serialize(self, value):
         return self.value_map.get(value)
@@ -352,7 +352,7 @@ class CaseOwnerAncestorLocationField(ValueSource):
     case owner, the case owner's location, or the first ancestor
     location of the case owner where the metadata value is set.
     """
-    location_field = StringProperty()
+    location_field = StringProperty(required=True, validators=not_blank)
 
     def _get_commcare_value(self, case_trigger_info):
         location = get_case_location(case_trigger_info)
@@ -366,7 +366,7 @@ class FormUserAncestorLocationField(ValueSource):
     user's location, or the first ancestor location of the case owner
     where the metadata value is set.
     """
-    location_field = StringProperty()
+    location_field = StringProperty(required=True, validators=not_blank)
 
     def _get_commcare_value(self, case_trigger_info):
         user_id = case_trigger_info.form_question_values.get('/metadata/userID')
