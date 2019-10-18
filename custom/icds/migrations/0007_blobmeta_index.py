b237f3026b7cb2c68a3995173641671ea2af3624
Simon Kelly <skelly@dimagi.com>
2019-10-18 09:56:33 +0200

    use correct column name

--- custom/icds/migrations/0007_blobmeta_index.py
+++ custom/icds/migrations/0007_blobmeta_index.py
@@ -2,7 +2,7 @@ from django.db import migrations
 
 TABLE_NAME = 'blobs_blobmeta'
 INDEX_NAME = 'blobs_blobmeta_type_created_on_idx'
-COLUMNS = ['type', 'created_on']
+COLUMNS = ['type_code', 'created_on']
 
 CREATE_INDEX_SQL = "CREATE INDEX CONCURRENTLY IF NOT EXISTS {} ON {} ({})".format(
     INDEX_NAME, TABLE_NAME, ','.join(COLUMNS))
