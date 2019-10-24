da801b25fd486735195b055645764d137d9e26ae
challa <challarao@beehyv.com>
2019-10-25 00:17:02 +0530

    lint changes

--- custom/icds_reports/static/icds_reports/js/spec/download.directive.spec.js
+++ custom/icds_reports/static/icds_reports/js/spec/download.directive.spec.js
@@ -398,7 +398,7 @@ describe('Download Directive', function () {
         });
     });
 
-    describe('Download Directive have access to features', function() {
+    describe('Download Directive have access to features', function () {
         var $scope, $httpBackend, controller;
 
         pageData.registerUrl('icds-ng-template', 'template');
