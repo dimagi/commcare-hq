560c1440351e9ee608ce5de9ec9f2431bd35665e
Simon Kelly <skelly@dimagi.com>
2019-10-25 17:13:11 +0200

    convert UUID to string

--- corehq/ex-submodules/casexml/apps/phone/document_store.py
+++ corehq/ex-submodules/casexml/apps/phone/document_store.py
@@ -9,3 +9,7 @@ class SyncLogDocumentStore(DjangoDocumentStore):
             return model.doc
 
         super().__init__(SyncLogSQL, doc_generator_fn=_to_doc)
+
+    def iter_document_ids(self):
+        for doc_id in super().iter_document_ids():
+            yield doc_id.hex
