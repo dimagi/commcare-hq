6fe1f89b6727156d7941ce9ad4f3c0aa1d41b2e5
Simon Kelly <skelly@dimagi.com>
2019-10-27 14:20:46 +0200

    create index concurrently

--- corehq/blobs/migrations/0010_auto_20191023_0938.py
+++ corehq/blobs/migrations/0010_auto_20191023_0938.py
@@ -2,7 +2,17 @@ from django.db import migrations
 import partial_index
 
 
+
+CREATE_INDEX_SQL = """
+    CREATE INDEX CONCURRENTLY IF NOT EXISTS "blobs_blobm_type_co_23e226_partial"
+    ON "blobs_blobmeta" ("type_code", "created_on")
+    WHERE "blobs_blobmeta"."domain" = 'icds-cas'
+"""
+DROP_INDEX_SQL = "DROP INDEX CONCURRENTLY IF EXISTS blobs_blobm_type_co_23e226_partial"
+
+
 class Migration(migrations.Migration):
+    atomic = False
 
     dependencies = [
         ('blobs', '0009_delete_blobexpiration'),
@@ -19,8 +29,16 @@ class Migration(migrations.Migration):
                 index=partial_index.PartialIndex(fields=['expires_on'], name='blobs_blobm_expires_ed7e3d_partial', unique=False, where=partial_index.PQ(expires_on__isnull=False)),
             )
         ]),
-        migrations.AddIndex(
-            model_name='blobmeta',
-            index=partial_index.PartialIndex(fields=['type_code', 'created_on'], name='blobs_blobm_type_co_23e226_partial', unique=False, where=partial_index.PQ(domain='icds-cas')),
+        migrations.RunSQL(
+            sql=CREATE_INDEX_SQL,
+            reverse_sql=DROP_INDEX_SQL,
+            state_operations=[
+                migrations.AddIndex(
+                    model_name='blobmeta',
+                    index=partial_index.PartialIndex(fields=['type_code', 'created_on'],
+                                                     name='blobs_blobm_type_co_23e226_partial', unique=False,
+                                                     where=partial_index.PQ(domain='icds-cas')),
+                ),
+            ]
         ),
     ]
