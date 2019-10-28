4f417176b89e5576eb967d0b56c8f0752f57fb7b
Jennifer Schweers <orange.jenny@gmail.com>
2019-10-28 13:47:00 -0400

    Removed feature flag to copy conditional alerts

--- corehq/toggles.py
+++ corehq/toggles.py
@@ -1115,13 +1115,6 @@ MESSAGE_LOG_METADATA = StaticToggle(
     [NAMESPACE_USER],
 )
 
-COPY_CONDITIONAL_ALERTS = StaticToggle(
-    'copy_conditional_alerts',
-    'Allow copying conditional alerts to another project (or within the same project).',
-    TAG_SOLUTIONS_LIMITED,
-    [NAMESPACE_USER],
-)
-
 RUN_AUTO_CASE_UPDATES_ON_SAVE = StaticToggle(
     'run_auto_case_updates_on_save',
     'Run Auto Case Update rules on each case save.',
