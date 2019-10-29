978b7fc873df9e88eaa0b264ea81a329cddf0941
Rohit Negi <rohit25.negi@gmail.com>
2019-10-29 13:06:43 +0530

    fixed naming

--- custom/icds_reports/migrations/0140_gm_column_agg_awc.py
+++ custom/icds_reports/migrations/0140_gm_column_agg_awc.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0138_update_thr_view'),
+        ('icds_reports', '0139_update_thr_view'),
     ]
 
     operations = [
