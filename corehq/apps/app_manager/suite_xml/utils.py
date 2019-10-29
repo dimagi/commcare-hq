bd39a71e9ebdebfa8c704463f9287d54f77a2811
Ethan Soergel <esoergel@gmail.com>
2019-10-29 16:13:29 -0400

    lint

--- corehq/apps/app_manager/suite_xml/utils.py
+++ corehq/apps/app_manager/suite_xml/utils.py
@@ -9,18 +9,18 @@ from corehq.apps.app_manager.suite_xml.xml_models import Suite, Text, XpathEnum
 
 
 def get_select_chain(app, module, include_self=True):
-        select_chain = [module] if include_self else []
-        current_module = module
-        while hasattr(current_module, 'parent_select') and current_module.parent_select.active:
-            current_module = app.get_module_by_unique_id(
-                current_module.parent_select.module_id,
-                error=_("Case list used by parent child selection in '{}' not found").format(
-                      current_module.default_name()),
-            )
-            if current_module in select_chain:
-                raise SuiteValidationError("Circular reference in case hierarchy")
-            select_chain.append(current_module)
-        return select_chain
+    select_chain = [module] if include_self else []
+    current_module = module
+    while hasattr(current_module, 'parent_select') and current_module.parent_select.active:
+        current_module = app.get_module_by_unique_id(
+            current_module.parent_select.module_id,
+            error=_("Case list used by parent child selection in '{}' not found").format(
+                  current_module.default_name()),
+        )
+        if current_module in select_chain:
+            raise SuiteValidationError("Circular reference in case hierarchy")
+        select_chain.append(current_module)
+    return select_chain
 
 
 def get_select_chain_meta(app, module):
