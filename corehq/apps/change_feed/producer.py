8c4f39b7a8dc94a430a3f83abc1619b13688a2ac
Simon Kelly <skelly@dimagi.com>
2019-10-31 17:09:52 +0200

    add audit logging

--- corehq/apps/change_feed/producer.py
+++ corehq/apps/change_feed/producer.py
@@ -1,10 +1,19 @@
 import json
+import logging
+from functools import partial
 
 from django.conf import settings
 
 from kafka import KafkaProducer
 
-from corehq.util.soft_assert import soft_assert
+from dimagi.utils.logging import notify_exception
+
+CHANGE_PRE_SEND = 'PRE-SEND'
+CHANGE_ERROR = 'ERROR'
+CHANGE_SENT = 'SENT'
+KAFKA_AUDIT_LOGGER = 'kafka_producer_audit'
+
+logger = logging.getLogger(KAFKA_AUDIT_LOGGER)
 
 
 class ChangeProducer(object):
@@ -32,15 +41,36 @@ class ChangeProducer(object):
         message = change_meta.to_json()
         message_json_dump = json.dumps(message).encode('utf-8')
         try:
-            self.producer.send(topic, message_json_dump, key=change_meta.document_id)
+            _audit_log(CHANGE_PRE_SEND, change_meta)
+            future = self.producer.send(topic, message_json_dump, key=change_meta.document_id)
             if self.auto_flush:
-                self.producer.flush()
+                future.get()
+                _audit_log(CHANGE_SENT, change_meta)
         except Exception:
+            _audit_log('ERROR', change_meta)
             notify_exception(None, 'Problem sending change to Kafka', details=message)
             raise
 
+        if not self.auto_flush:
+            on_success = partial(_on_success, change_meta)
+            on_error = partial(_on_error, change_meta)
+            future.add_callback(on_success).add_errback(on_error)
+
     def flush(self, timeout=None):
         self.producer.flush(timeout=timeout)
+
+
+def _on_success(change_meta, record_metadata):
+    _audit_log(CHANGE_SENT, change_meta)
+
+
+def _on_error(change_meta, exc_info):
+    _audit_log(CHANGE_ERROR, change_meta)
+    notify_exception(None, 'Problem sending change to Kafka', details=change_meta.to_json(), exec_info=exc_info)
+
+
+def _audit_log(stage, change_meta):
+    logger.debug('%s change to kafka: %s,%s', stage, change_meta.document_type, change_meta.document_id)
 
 
 producer = ChangeProducer()
