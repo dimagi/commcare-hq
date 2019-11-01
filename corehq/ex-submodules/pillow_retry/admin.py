130d73d64e6f4abe4946240a8e876891cb02182c
Jonathan Emord <jonathan.emord@gmail.com>
2019-11-01 16:53:07 -0400

    Add reset attempts to PillowError actions

--- corehq/ex-submodules/pillow_retry/admin.py
+++ corehq/ex-submodules/pillow_retry/admin.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib import admin
 
 from pillow_retry.models import PillowError
@@ -5,7 +7,6 @@ from pillow_retry.models import PillowError
 
 @admin.register(PillowError)
 class PillowErrorAdmin(admin.ModelAdmin):
-
     model = PillowError
     list_display = [
         'pillow',
@@ -17,5 +18,11 @@ class PillowErrorAdmin(admin.ModelAdmin):
     ]
     list_filter = ('pillow', 'error_type')
     actions = [
-        'delete_selected'
+        'delete_selected',
+        'reset_attempts',
     ]
+
+    def reset_attempts(self, request, queryset):
+        queryset.update(current_attempt=0, date_next_attempt=datetime.utcnow())
+
+    reset_attempts.short_description = "Reset Attempts"
