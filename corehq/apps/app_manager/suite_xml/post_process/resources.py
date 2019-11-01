d2442a9b78fa118e0664beaaf82be274995157e2
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-01 18:26:52 -0400

    Added body to ResourceOverrideHelper.update_suite

--- corehq/apps/app_manager/suite_xml/post_process/resources.py
+++ corehq/apps/app_manager/suite_xml/post_process/resources.py
@@ -1,6 +1,9 @@
+from collections import defaultdict
 from django.db import models
 
 from corehq.apps.app_manager.suite_xml.contributors import PostProcessor
+from corehq.apps.app_manager.suite_xml.sections.resources import FormResourceContributor
+from corehq.apps.app_manager.suite_xml.xml_models import XFormResource
 
 
 class ResourceOverride(models.Model):
@@ -14,4 +17,23 @@ class ResourceOverride(models.Model):
 class ResourceOverrideHelper(PostProcessor):
 
     def update_suite(self):
-        pass
+        """
+        Applies manual overrides of resource ids.
+        """
+        overrides_by_pre_id = {
+            override.pre_id: override
+            for override in ResourceOverride.objects.filter(
+                domain=self.app.domain,
+                app_id=self.app.master_id,
+                root_name=XFormResource.ROOT_NAME,
+            )
+        }
+        id_counts = defaultdict(int)
+        for resource in getattr(self.suite, FormResourceContributor.section_name):
+            if resource.id in overrides_by_pre_id:
+                resource.id = overrides_by_pre_id[resource.id].post_id
+            id_counts[resource.id] += 1
+
+        duplicates = [id for id, count in id_counts.items() if count > 1]
+        if duplicates:
+            raise ResourceOverrideError("Duplicate resource ids found: {}".format(", ".join(duplicates)))
