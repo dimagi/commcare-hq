c24a4953581ed236fe05b57462291fdb09284f75
Norman Hooper <nhooper@dimagi.com>
2019-11-01 13:31:38 +0200

    First just deal with classification forms

--- custom/rec/management/commands/export_visit_child.py
+++ custom/rec/management/commands/export_visit_child.py
@@ -9,7 +9,6 @@ import sys
 
 from django.core.management import BaseCommand
 
-from casexml.apps.case.models import CommCareCase
 from couchforms.dbaccessors import get_forms_by_id, iter_form_ids_by_xmlns
 from custom.rec.collections import dicts_or
 from dimagi.utils.chunked import chunked
@@ -50,57 +49,36 @@ def main():
         form_ids = iter_form_ids_by_xmlns(DOMAIN, xmlns)
         for form_ids_chunk in chunked(form_ids, 500):
             couch_forms = get_forms_by_id(form_ids_chunk)
-            visit_id_to_child_id, visit_ids_missing_child_ids = map_visit_to_child_from_forms(couch_forms)
-            visit_id_to_child_id.update(map_visit_to_child_from_visit_cases(visit_ids_missing_child_ids))
-
             for couch_form in couch_forms:
-                imci_visit_id = get_imci_visit_id(couch_form)
                 row = (
                     get_form_id(couch_form),
                     xmlns,
-                    imci_visit_id,
-                    visit_id_to_child_id[imci_visit_id],
+                    get_imci_visit_id(couch_form, xmlns),
+                    get_rec_child_id_from_form(couch_form, xmlns),
                     get_created_at(couch_form),
                 )
                 print(",".join(row))
 
 
 def get_form_id(couch_form):
-    return couch_form.form_json["form_id"]
+    return couch_form["meta"]["instanceID"]
 
 
-def get_imci_visit_id(couch_form):
-    return couch_form.form_json["form"]["case_case_visit"]["case"]["@case_id"]
+def get_imci_visit_id(couch_form, xmlns):
+    if xmlns in CLASSIFICATION_FORMS:
+        assert "subcase_0" in couch_form, "Form missing visit subcase"
+        if couch_form["subcase_0"]["case"].get("create"):
+            case_type = couch_form["subcase_0"]["case"]["create"]["case_type"]
+            assert case_type == "imci_visit", "Bad visit case type"
+        return couch_form["subcase_0"]["case"]["@case_id"]
+    raise NotImplementedError
 
 
-def map_visit_to_child_from_forms(couch_forms):
-    visit_id_to_child_id = {}
-    visit_ids_without_child_ids = set()
-    for couch_form in couch_forms:
-        visit_id = get_imci_visit_id(couch_form)
-        child_id = get_rec_child_id_from_form(couch_form)
-        if child_id:
-            visit_id_to_child_id[visit_id] = child_id
-        else:
-            visit_ids_without_child_ids.add(visit_id)
-    return visit_id_to_child_id, list(visit_ids_without_child_ids)
-
-
-def get_rec_child_id_from_form(couch_form):
-    if "case_case_child" in couch_form.form_json["form"]:
-        return couch_form.form_json["form"]["case_case_child"]["case"]["@case_id"]
-
-
-def map_visit_to_child_from_visit_cases(visit_ids):
-    visits = CommCareCase.view('_all_docs', keys=visit_ids, include_docs=True)
-    return {v.case_id: get_rec_child_id_from_visit(v) for v in visits}
-
-
-def get_rec_child_id_from_visit(visit):
-    for index in visit.indices:
-        if index.identifier == "parent" and index.referenced_type == "rec_child":
-            return index.referenced_id
+def get_rec_child_id_from_form(couch_form, xmlns):
+    if xmlns in CLASSIFICATION_FORMS:
+        return couch_form["case"]["@case_id"]
+    raise NotImplementedError
 
 
 def get_created_at(couch_form):
-    return couch_form.form_json["received_on"]
+    return couch_form["meta"]["timeEnd"]  # couch_form does not have submission time
