66bf05238eaaa54866aa4db58c689df1713667b2
Simon Kelly <skelly@dimagi.com>
2019-11-04 16:50:34 +0200

    change default to false

--- settings.py
+++ settings.py
@@ -113,7 +113,7 @@ SOFT_ASSERTS_LOG_FILE = "%s/%s" % (FILEPATH, "soft_asserts.log")
 MAIN_COUCH_SQL_DATAMIGRATION = "%s/%s" % (FILEPATH, "main_couch_sql_datamigration.log")
 SESSION_ACCESS_LOG_FILE = "%s/%s" % (FILEPATH, "session_access_log.log")
 KAFKA_PRODUCER_AUDIT_FILE = "%s/%s" % (FILEPATH, "commcarehq.kafka_audit.log")
-KAFKA_PRODUCER_AUDIT_LOG_ENABLED = True
+KAFKA_PRODUCER_AUDIT_LOG_ENABLED = False
 
 LOCAL_LOGGING_HANDLERS = {}
 LOCAL_LOGGING_LOGGERS = {}
