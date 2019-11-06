e41d49affeb5d735e60f45ee427208320651daeb
Norman Hooper <nhooper@dimagi.com>
2019-11-06 13:41:29 +0200

    Drop comment

--- corehq/motech/requests.py
+++ corehq/motech/requests.py
@@ -11,8 +11,6 @@ from corehq.motech.const import REQUEST_TIMEOUT
 from corehq.motech.models import RequestLog
 from corehq.motech.utils import pformat_json
 
-logger = logging.getLogger('motech')
-
 
 def log_request(func):
 
@@ -100,10 +98,6 @@ class Requests(object):
             if raise_for_status:
                 response.raise_for_status()
         except requests.RequestException:
-            # commented out since these are spamming Sentry
-            # err_request, err_response = parse_request_exception(err)
-            # logger.error('Request: %s', err_request)
-            # logger.error('Response: %s', err_response)
             raise
         return response
 
