fb3fb6eb02a81894c5509244e7a9b6ad6e630399
Rohit Negi <rohit25.negi@gmail.com>
2019-11-08 02:15:39 +0530

    Added test cases

--- custom/icds_reports/tests/agg_tests/test_nic_indicators_api.py
+++ custom/icds_reports/tests/agg_tests/test_nic_indicators_api.py
@@ -3,6 +3,7 @@ from freezegun import freeze_time
 
 from django.test import TestCase
 from custom.icds_reports.utils.data_accessor import get_inc_indicator_api_data
+from custom.icds_reports.reports.mwcd_indicators import get_mwcd_indicator_api_data
 
 
 @freeze_time("2017-05-02")
@@ -44,3 +45,71 @@ class NICIndicatorTest(TestCase):
              ]},
             data
         )
+
+    def test_mwcd_indicators_data(self):
+        data = get_mwcd_indicator_api_data(date(2017, 4, 1))
+        print(data)
+        self.assertCountEqual({"scheme_code": "C002",
+                               "implementation_status": {
+                                   "national_total": {
+                                       "nation_code": 0,
+                                       "num_launched_awcs": 0,
+                                       "num_launched_districts": 0,
+                                       "num_launched_states": 0,
+                                       "num_ls_launched": 0,
+                                       "awc_with_gm_devices": 0,
+                                       "cases_household": 0,
+                                       "total_mothers": 0,
+                                       "cases_child_health": 0
+                                   },
+                                   "dataarray": [
+                                       {
+                                           "state_name": "st1",
+                                           "state_id": "st1",
+                                           "num_launched_awcs": 0,
+                                           "num_launched_districts": 0,
+                                           "num_launched_states": 0,
+                                           "num_ls_launched": 0,
+                                           "awc_with_gm_devices": 0,
+                                           "cases_household": 0,
+                                           "total_mothers": 0,
+                                           "cases_child_health": 0
+                                       }
+                                   ]
+                               },
+                               "monthly_trend": {
+                                   "national_total": [
+                                       {
+                                           "month": 10,
+                                           "total_awc_launched": 0,
+                                           "total_mothers": 0,
+                                           "total_children": 0,
+                                           "year": 2019},
+                                       {
+                                           "month": 11,
+                                           "total_awc_launched": 0,
+                                           "total_mothers": 0,
+                                           "total_children": 0,
+                                           "year": 2019
+                                       }
+                                   ],
+                                   "dataarray": [
+                                       {
+                                           "state_name": "st1",
+                                           "state_id": "st1",
+                                           "month": 10,
+                                           "total_mothers": 0,
+                                           "num_launched_awcs": 0,
+                                           "cases_child_health": 0,
+                                           "year": 2019},
+                                       {
+                                           "state_name": "st1",
+                                           "state_id": "st1",
+                                           "month": 11,
+                                           "total_mothers": 0,
+                                           "num_launched_awcs": 0,
+                                           "cases_child_health": 0,
+                                           "year": 2019
+                                       }
+                                   ]
+                               }}, data)
