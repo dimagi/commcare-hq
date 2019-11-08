2ad657eb0a6a760cdea8a811f92f2024b4749dad
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-08 14:03:25 -0500

    Updated migrations to use app manager util (new)

--- corehq/apps/reminders/migrations/0002_app_id.py
+++ corehq/apps/reminders/migrations/0002_app_id.py
@@ -1,6 +1,6 @@
 from django.db import migrations
 
-from corehq.apps.app_manager.dbaccessors import get_apps_in_domain
+from corehq.apps.app_manager.util import get_app_id_from_form_unique_id
 from corehq.apps.reminders.models import SurveyKeyword
 from corehq.dbaccessors.couchapps.all_docs import (
     get_deleted_doc_ids_by_class,
@@ -14,26 +14,12 @@ from corehq.util.log import with_progress_bar
 @skip_on_fresh_install
 def _populate_app_id(apps, schema_editor):
     doc_ids = get_doc_ids_by_class(SurveyKeyword) + get_deleted_doc_ids_by_class(SurveyKeyword)
-    domain_forms = {}
-
-    def add_field(doc):
-        return _add_field(doc, domain_forms)
-
-    iter_update(SurveyKeyword.get_db(), add_field, with_progress_bar(doc_ids))
+    iter_update(SurveyKeyword.get_db(), _add_field, with_progress_bar(doc_ids))
 
 
-def _add_field(doc, domain_forms):
-    if doc.get('form_unique_id', None):
-        domain = doc['domain']
-        form_unique_id = doc['form_unique_id']
-        if domain not in domain_forms:
-            domain_forms[domain] = {}
-            apps = get_apps_in_domain(domain)
-            for app in apps:
-                for module in app.modules:
-                    for form in module.get_forms():
-                        domain_forms[domain][form.unique_id] = app.get_id
-        doc['app_id'] = domain_forms[domain].get(form_unique_id, None)
+def _add_field(doc):
+    if doc.get('form_unique_id', None) and not doc.get('app_id', None):
+        doc['app_id'] = get_app_id_from_form_unique_id(doc['domain'], doc['form_unique_id'])
         if doc['app_id']:
             return DocUpdate(doc)
 
