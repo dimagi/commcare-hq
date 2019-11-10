efda0ea7d084e20b2266200f43489159542e786c
mkangia <mkangia@dimagi.com>
2019-11-11 01:56:40 +0530

    find size of mm added

--- corehq/apps/app_manager/views/utils.py
+++ corehq/apps/app_manager/views/utils.py
@@ -415,6 +415,37 @@ def form_has_submissions(domain, app_id, xmlns):
     return FormES().domain(domain).app([app_id]).xmlns([xmlns]).count() != 0
 
 
+def get_new_multimedia_between_builds(domain, source_build_id, target_build_id, build_profile_id=None):
+    source_build = get_app_cached(domain, source_build_id)
+    target_build = get_app_cached(domain, target_build_id)
+    assert source_build.copy_of, _("Size calculation available only for builds")
+    assert target_build.copy_of, _("Size calculation available only for builds")
+    build_profile = source_build.build_profiles[build_profile_id] if build_profile_id else None
+    multimedia_map_for_source_build_by_id = {
+        media_item['multimedia_id']: media_item
+        for path, media_item in
+        source_build.multimedia_map_for_build(build_profile=build_profile)
+    }
+    multimedia_map_for_target_build = target_build.multimedia_map_for_build(build_profile=build_profile)
+    multimedia_map_for_target_build_by_id = {
+        media_item['multimedia_id']: media_item
+        for path, media_item in
+        multimedia_map_for_target_build.items()
+    }
+    added = set(multimedia_map_for_target_build_by_id.keys()).difference(
+        set(multimedia_map_for_source_build_by_id.keys()))
+    media_objects = {
+        media_map_item['multimedia_id']: media_map_item
+        for path, media_map_item in
+        target_build.get_media_objects(multimedia_map=multimedia_map_for_target_build)
+    }
+    total_size = defaultdict(lambda: 0)
+    for multimedia_id in added:
+        media_object = media_objects[multimedia_id]
+        total_size[media_object.doc_type] += media_object.content_length
+    return total_size
+
+
 def get_multimedia_sizes_for_build(domain, build_id, build_profile_id=None):
     build = get_app_cached(domain, build_id)
     assert build.copy_of, _("Size calculation available only for builds")
