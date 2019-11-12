992aa52c7419c20fe6bea8e38c63978acb222594
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-12 17:36:01 -0500

    Removed code references

--- custom/ewsghana/tests/handlers/stock_on_hand.py
+++ custom/ewsghana/tests/handlers/stock_on_hand.py
@@ -1,5 +1,5 @@
 from casexml.apps.stock.models import StockTransaction
-from corehq.toggles import EWS_INVALID_REPORT_RESPONSE, NAMESPACE_DOMAIN
+from corehq.toggles import NAMESPACE_DOMAIN
 from custom.ewsghana.handlers import INVALID_MESSAGE, MS_STOCKOUT
 from custom.ewsghana.tests.handlers.utils import EWSScriptTest, restore_location_products, \
     assign_products_to_location, TEST_DOMAIN
@@ -221,19 +221,6 @@ class StockOnHandTest(EWSScriptTest):
             5551234 < Dear {}, thank you for reporting the commodities you have. You received jd 25.
         """.format(self.user1.full_name)
         self.run_script(a)
-
-    def test_invalid_report_when_toggle_is_on(self):
-        EWS_INVALID_REPORT_RESPONSE.set(TEST_DOMAIN, True, NAMESPACE_DOMAIN)
-        msg = 'Error! You submitted increases in stock levels of mg without corresponding receipts. ' \
-              'Please contact your DHIO or RHIO for assistance.'
-        StockTransaction.objects.all().delete()
-        a = """
-            5551234 > soh mg 25.0 jd 25.25
-            5551234 < Dear {}, thank you for reporting the commodities you have in stock.
-            5551234 < {}
-        """.format(self.user1.full_name, msg)
-        self.run_script(a)
-        EWS_INVALID_REPORT_RESPONSE.set(TEST_DOMAIN, False, NAMESPACE_DOMAIN)
 
     def test_rms_stockout(self):
         a = """
