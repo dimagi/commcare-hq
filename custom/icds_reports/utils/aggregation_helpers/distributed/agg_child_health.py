2226bc50c9393aa3189f51c41306cff0834ba725
Cal Ellowitz <calellowitz@gmail.com>
2019-11-12 20:19:14 +0530

    stop using named indices

--- custom/icds_reports/utils/aggregation_helpers/distributed/agg_child_health.py
+++ custom/icds_reports/utils/aggregation_helpers/distributed/agg_child_health.py
@@ -362,13 +362,12 @@ class AggChildHealthAggregationDistributedHelper(AggregationPartitionedHelper):
         """
 
     def indexes(self):
-        final_tablename = self.monthly_tablename
         staging_tablename = self.staging_tablename
         return [
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_1" ON "{staging_tablename}" (aggregation_level, state_id)',
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_2" ON "{staging_tablename}" (aggregation_level, gender)',
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_3" ON "{staging_tablename}" (aggregation_level, age_tranche)',
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_4" ON "{staging_tablename}" (aggregation_level, district_id) WHERE aggregation_level > 1',
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_5" ON "{staging_tablename}" (aggregation_level, block_id) WHERE aggregation_level > 2',
-            f'CREATE INDEX IF NOT EXISTS "{final_tablename}_idx_6" ON "{staging_tablename}" (aggregation_level, supervisor_id) WHERE aggregation_level > 3',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, state_id)',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, gender)',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, age_tranche)',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, district_id) WHERE aggregation_level > 1',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, block_id) WHERE aggregation_level > 2',
+            f'CREATE INDEX ON "{staging_tablename}" (aggregation_level, supervisor_id) WHERE aggregation_level > 3',
         ]
