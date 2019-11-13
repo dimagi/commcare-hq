9d4e97b57da78e861ab0ee664f8b6f1c9931847f
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-13 10:47:43 -0500

    Removed CommCareExchangeHomeView

--- corehq/apps/appstore/urls.py
+++ corehq/apps/appstore/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import url
 
 from corehq.apps.appstore.views import (
-    CommCareExchangeHomeView,
     approve_app,
     project_documentation_file,
     project_image,
 )
 
 urlpatterns = [
-    url(r'^$', CommCareExchangeHomeView.as_view(), name=CommCareExchangeHomeView.urlname),
-
     url(r'^(?P<snapshot>[\w\.-]+)/approve/$', approve_app, name='approve_appstore_app'),
     url(r'^(?P<snapshot>[\w\.-]+)/image/$', project_image, name='appstore_project_image'),
     url(r'^(?P<snapshot>[\w\.-]+)/documentation_file/$', project_documentation_file,
