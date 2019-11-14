c51c71601102e6890722d3a8324b3942e9f18d58
Norman Hooper <nhooper@dimagi.com>
2019-11-14 10:29:10 +0200

    :black_heart: short dict func
    This reverses `dicts` even though we don't really need to for our
    usecase, because otherwise it wouldn't be an "or". It would be like an
    "update, reduced", which is 6 characters shorter, but just doesn't
    sound as cool.

--- custom/rec/collections.py
+++ custom/rec/collections.py
@@ -5,13 +5,8 @@ def dicts_or(*dicts):
     >>> foo = {1: 'one', 2: 'two'}
     >>> bar = {2: 'deux', 3: 'trois'}
     >>> baz = {3: 'drie', 4: 'vier'}
-    >>> dicts_or(foo, bar, baz)
-    {1: 'one', 2: 'two', 3: 'trois', 4: 'vier'}
+    >>> dicts_or(foo, bar, baz) == {1: 'one', 2: 'two', 3: 'trois', 4: 'vier'}
+    True
 
     """
-    assert len(dicts), "No operands"
-    if len(dicts) == 1:
-        return dicts[0]
-    d1 = dicts[0]
-    dn = dicts_or(*dicts[1:])
-    return {k: d1[k] if k in d1 else dn[k] for k in d1.keys() | dn.keys()}
+    return {k: v for d in dicts[::-1] for k, v in d.items()}
