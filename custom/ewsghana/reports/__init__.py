88772ed68d50c6963080786b57dd5f8e5af4b239
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-16 11:18:45 -0500

    Removed ./custom/ewsghana/reports/__init__.py

--- custom/ewsghana/reports/__init__.py
+++ custom/ewsghana/reports/__init__.py
@@ -481,7 +481,6 @@ class ProductSelectionPane(EWSData):
 
 from custom.ewsghana.reports.email_reports import CMSRMSReport, StockSummaryReport
 from custom.ewsghana.reports.maps import EWSMapReport
-from custom.ewsghana.reports.stock_levels_report import StockLevelsReport
 from custom.ewsghana.reports.specific_reports.dashboard_report import DashboardReport
 from custom.ewsghana.reports.specific_reports.stock_status_report import StockStatus
 from custom.ewsghana.reports.specific_reports.reporting_rates import ReportingRatesReport
@@ -490,7 +489,6 @@ CUSTOM_REPORTS = (
     ('Custom reports', (
         DashboardReport,
         StockStatus,
-        StockLevelsReport,
         ReportingRatesReport,
         EWSMapReport,
         CMSRMSReport,
