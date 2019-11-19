ca9fe681d54ebc67fd149cb3b65752ff2dbfe170
Jenny Schweers <orange.jenny@gmail.com>
2019-11-19 12:38:52 -0500

    Revert "DHIS2 API version"

--- corehq/motech/dhis2/repeaters.py
+++ corehq/motech/dhis2/repeaters.py
@@ -2,17 +2,14 @@ import json
 
 from django.utils.translation import ugettext_lazy as _
 
-from couchdbkit import BadValueError
 from memoized import memoized
-from packaging.version import InvalidVersion, Version
 from requests import RequestException
 from urllib3.exceptions import HTTPError
 
 from couchforms.signals import successful_form_received
-from dimagi.ext.couchdbkit import SchemaProperty, StringProperty
+from dimagi.ext.couchdbkit import SchemaProperty
 
 from corehq.form_processor.interfaces.dbaccessors import FormAccessors
-from corehq.motech.dhis2.const import DHIS2_MAX_VERSION
 from corehq.motech.dhis2.dhis2_config import Dhis2Config
 from corehq.motech.dhis2.events_helpers import send_dhis2_event
 from corehq.motech.repeaters.models import FormRepeater, Repeater
@@ -24,30 +21,6 @@ from corehq.motech.requests import Requests
 from corehq.toggles import DHIS2_INTEGRATION
 
 
-def is_dhis2_version(value):
-    try:
-        version = Version(value)
-        major, minor, *the_rest = version.release
-        if version > Version(DHIS2_MAX_VERSION):
-            raise BadValueError(_(
-                f"Versions of DHIS2 higher than {DHIS2_MAX_VERSION} are not "
-                "yet supported."
-            ))
-        if major == 2:
-            return True
-    except (InvalidVersion, TypeError, ValueError):
-        pass
-    raise BadValueError(_(
-        'DHIS2 version must be in the format "2.xy" or "2.xy.z".'
-    ))
-
-
-def is_dhis2_version_or_blank(value):
-    if value is None or value == "":
-        return True
-    return is_dhis2_version(value)
-
-
 class Dhis2Repeater(FormRepeater):
     class Meta(object):
         app_label = 'repeaters'
@@ -56,7 +29,6 @@ class Dhis2Repeater(FormRepeater):
     friendly_name = _("Forward Forms to DHIS2 as Anonymous Events")
     payload_generator_classes = (FormRepeaterJsonPayloadGenerator,)
 
-    dhis2_version = StringProperty(validators=[is_dhis2_version_or_blank])
     dhis2_config = SchemaProperty(Dhis2Config)
 
     _has_config = True
@@ -91,18 +63,6 @@ class Dhis2Repeater(FormRepeater):
     def get_payload(self, repeat_record):
         payload = super(Dhis2Repeater, self).get_payload(repeat_record)
         return json.loads(payload)
-
-    @property
-    def api_version(self) -> int:
-        """
-        Check API version to determine what calls/schema are supported
-        by the remote system.
-
-        e.g. Not all CRUD operations are supported before version 15.
-        """
-        if self.dhis2_version:
-            major, minor, *the_rest = Version(self.dhis2_version).release
-            return minor
 
     def send_request(self, repeat_record, payload):
         """
