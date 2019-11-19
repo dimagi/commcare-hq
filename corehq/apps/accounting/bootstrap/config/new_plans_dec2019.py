daed351d3bcf1a4f3f23ad0d54fd12ef04c731e2
Biyeun Buczyk <biyeun@dimagi.com>
2019-11-19 10:40:37 -0600

    consolidate all these separate configs (only present in saas-q4 branch rn) into one config, as we'll be adding a couple more plans

---
+++ corehq/apps/accounting/bootstrap/config/new_plans_dec2019.py
@@ -0,0 +1,30 @@
+from decimal import Decimal
+
+from corehq.apps.accounting.models import FeatureType, SoftwarePlanEdition
+
+BOOTSTRAP_CONFIG = {
+    (SoftwarePlanEdition.PAUSED, False, False): {
+        'role': 'paused_plan_v0',
+        'product_rate_monthly_fee': Decimal('0.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=0, per_excess_fee=Decimal('0.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        }
+    },
+    (SoftwarePlanEdition.COMMUNITY, False, False): {
+        'role': 'community_plan_v2',
+        'product_rate_monthly_fee': Decimal('0.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=5, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        }
+    },
+    (SoftwarePlanEdition.PRO, False, False): {
+        'role': 'pro_plan_v1',
+        'product_rate_monthly_fee': Decimal('600.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=250, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=50),
+        },
+    },
+}
--- corehq/apps/accounting/migrations/0045_new_plans.py
+++ corehq/apps/accounting/migrations/0045_new_plans.py
@@ -8,12 +8,8 @@ from django.core.management import call_command
 from corehq.util.django_migrations import skip_on_fresh_install
 from corehq.privileges import PROJECT_ACCESS
 from corehq.apps.accounting.bootstrap.utils import ensure_plans
-from corehq.apps.accounting.bootstrap.config.paused_software_plan import \
-    BOOTSTRAP_CONFIG as paused_config
-from corehq.apps.accounting.bootstrap.config.community_v2_software_plan import \
-    BOOTSTRAP_CONFIG as new_community_config
-from corehq.apps.accounting.bootstrap.config.pro_v1_software_plan import \
-    BOOTSTRAP_CONFIG as new_pro_config
+from corehq.apps.accounting.bootstrap.config.new_plans_dec2019 import \
+    BOOTSTRAP_CONFIG as new_plans_config
 
 
 @skip_on_fresh_install
@@ -28,9 +24,7 @@ def _grandfather_basic_privs(apps, schema_editor):
 
 
 def _ensure_new_software_plans(apps, schema_editor):
-    ensure_plans(paused_config, verbose=True, apps=apps)
-    ensure_plans(new_community_config, verbose=True, apps=apps)
-    ensure_plans(new_pro_config, verbose=True, apps=apps)
+    ensure_plans(new_plans_config, verbose=True, apps=apps)
 
 
 class Migration(migrations.Migration):
