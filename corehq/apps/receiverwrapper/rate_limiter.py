da4d5864354687b1679cda017f54f4e4b79fe8e7
Daniel Roberts <droberts@dimagi.com>
2019-11-20 13:24:09 -0600

    Simplify definition of submission and restore rate limits

--- corehq/apps/receiverwrapper/rate_limiter.py
+++ corehq/apps/receiverwrapper/rate_limiter.py
@@ -5,6 +5,7 @@ from corehq.project_limits.rate_limiter import (
     RateDefinition,
     RateLimiter,
 )
+from corehq.project_limits.shortcuts import get_standard_ratio_rate_definition
 from corehq.toggles import RATE_LIMIT_SUBMISSIONS, NAMESPACE_DOMAIN
 from corehq.util.datadog.gauges import datadog_counter
 from corehq.util.datadog.utils import bucket_value
@@ -12,32 +13,29 @@ from corehq.util.decorators import run_only_when, silence_and_report_error
 from corehq.util.timer import TimingContext
 
 # Danny promised in an Aug 2019 email not to enforce limits that were lower than this.
-# If we as a team end up regretting this decision, we'll have to reset expectations
-# with the Dimagi NDoH team.
-rates_promised_not_to_go_lower_than = RateDefinition(
-    per_week=115,
-    per_day=23,
-    per_hour=3,
-    per_minute=0.07,
-    per_second=0.005,
-)
+#   RateDefinition(
+#       per_week=115,
+#       per_day=23,
+#       per_hour=3,
+#       per_minute=0.07,
+#       per_second=0.005,
+#   ) == get_standard_ratio_rate_definition(events_per_day=23)
 
-floor_for_small_domain = RateDefinition(
-    per_week=100,
-    per_day=50,
-    per_hour=30,
-    per_minute=10,
-    per_second=1,
-)
-
-test_rates = PerUserRateDefinition(
-    per_user_rate_definition=rates_promised_not_to_go_lower_than.times(2.0),
-    constant_rate_definition=floor_for_small_domain,
-)
+SUBMISSIONS_PER_DAY = 46
 
 submission_rate_limiter = RateLimiter(
     feature_key='submissions',
-    get_rate_limits=test_rates.get_rate_limits
+    get_rate_limits=PerUserRateDefinition(
+        per_user_rate_definition=get_standard_ratio_rate_definition(
+            events_per_day=SUBMISSIONS_PER_DAY),
+        constant_rate_definition=RateDefinition(
+            per_week=100,
+            per_day=50,
+            per_hour=30,
+            per_minute=10,
+            per_second=1,
+        ),
+    ).get_rate_limits
 )
 
 
