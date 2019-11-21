ce49b0872486d087a113a503309f23a3593c0ab7
Daniel Roberts <droberts@dimagi.com>
2019-11-21 17:35:31 -0600

    Ignore resource conflicts in update_domain_date

--- corehq/apps/users/tasks.py
+++ corehq/apps/users/tasks.py
@@ -295,7 +295,10 @@ def update_domain_date(user_id, domain):
     if domain_membership and (
             not domain_membership.last_accessed or domain_membership.last_accessed < today):
         domain_membership.last_accessed = today
-        user.save()
+        try:
+            user.save()
+        except ResourceConflict:
+            pass
 
 
 @periodic_task(
