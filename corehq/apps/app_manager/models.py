6515e8be47f402a5e90ec0c0f52e39c228f48966
Jennifer Schweers <orange.jenny@gmail.com>
2019-11-22 16:06:16 -0500

    Added verification of migration, added back skeletal GlobalAppConfig

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -5741,6 +5741,10 @@ class SQLGlobalAppConfig(models.Model):
         return model
 
 
+class GlobalAppConfig(Document):
+    pass
+
+
 class AppReleaseByLocation(models.Model):
     domain = models.CharField(max_length=255, null=False)
     app_id = models.CharField(max_length=255, null=False)
