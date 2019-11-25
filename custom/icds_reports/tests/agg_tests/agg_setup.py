af004b843ad578438589f90b8add96c2e5527b3c
Rohit Negi <rohit25.negi@gmail.com>
2019-11-25 10:21:27 +0530

    Removed create index property from state_id Removed base expression updated test cases

--- custom/icds_reports/tests/agg_tests/agg_setup.py
+++ custom/icds_reports/tests/agg_tests/agg_setup.py
@@ -47,7 +47,7 @@ FILE_NAME_TO_TABLE_MAPPING = {
     'awc_location': 'awc_location',
     'awc_location_local': 'awc_location_local',
     'agg_awc': 'agg_awc',
-    'private_school': get_table_name('static-dashboard_primary_private_school')
+    'private_school': get_table_name('icds-cas', 'static-dashboard_primary_private_school')
 }
 
 
