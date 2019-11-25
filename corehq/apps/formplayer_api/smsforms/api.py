d2fbdf5ca3c6923f21e741ec15aac6debac1e1bc
Daniel Roberts <droberts@dimagi.com>
2019-11-25 15:10:42 -0600

    Clean up imports and formatting

--- corehq/apps/formplayer_api/smsforms/api.py
+++ corehq/apps/formplayer_api/smsforms/api.py
@@ -1,4 +1,9 @@
-import copy
+"""
+A set of wrappers that return the JSON bodies you use to interact with the formplayer
+backend for various sets of tasks.
+
+This API is currently highly beta and could use some hardening.
+"""
 import json
 import socket
 
@@ -6,27 +11,10 @@ from django.conf import settings
 from django.http import Http404
 
 import requests
-import six.moves.http_client
 from requests import HTTPError
-from six.moves.urllib.parse import urlparse
-
-from dimagi.utils.couch.cache.cache_core import get_redis_client
-
 from corehq.apps.formplayer_api.smsforms.exceptions import BadDataError
 from corehq.apps.formplayer_api.utils import get_formplayer_url
-from corehq.form_processor.utils.general import use_sqlite_backend
-from corehq.util.hmac_request import (
-    convert_to_bytestring_if_unicode,
-    get_hmac_digest,
-)
-
-
-"""
-A set of wrappers that return the JSON bodies you use to interact with the formplayer
-backend for various sets of tasks.
-
-This API is currently highly beta and could use some hardening. 
-"""
+from corehq.util.hmac_request import get_hmac_digest
 
 
 class TouchformsError(ValueError):
@@ -45,32 +33,31 @@ class XFormsConfigException(ValueError):
 
 
 class XFormsConfig(object):
-    
-    def __init__(self, form_path=None, form_content=None, language="", 
+
+    def __init__(self, form_path=None, form_content=None, language="",
                  session_data=None, preloader_data={}, instance_content=None,
                  domain=None, restore_as=None, restore_as_case_id=None):
-        
+
         if bool(form_path) == bool(form_content):
-            raise XFormsConfigException\
-                ("Can specify file path or content but not both!\n" 
-                 "File Path: %s, Form Content: %s" % (form_path, form_content))
-        
+            raise XFormsConfigException(
+                "Can specify file path or content but not both!\n"
+                "File Path: %s, Form Content: %s" % (form_path, form_content))
+
         self.form_path = form_path
         self.form_content = form_content
         self.language = language
         self.session_data = session_data
-        self.preloader_data = preloader_data        
+        self.preloader_data = preloader_data
         self.instance_content = instance_content
         self.restore_as = restore_as
         self.restore_as_case_id = restore_as_case_id
         self.domain = domain
-        
+
     def get_touchforms_dict(self):
         """
-        Translates this config into something touchforms wants to 
-        work with
+        Translates this config into something touchforms wants to work with
         """
-        
+
         vals = (("action", "new-form"),
                 ("form-name", self.form_path),
                 ("form-content", self.form_content),
@@ -79,7 +66,7 @@ class XFormsConfig(object):
                 ("session-data", self.session_data),
                 ("lang", self.language),
                 ("form-url", self.form_path))
-        
+
         # only include anything with a value, or touchforms gets mad
         ret = dict([x for x in vals if x[1]])
         self.add_key_helper('username', ret)
@@ -101,20 +88,20 @@ class XFormsConfig(object):
     def add_key_helper(self, key, ret):
         if key in self.session_data:
             ret[key] = self.session_data[key]
-        
+
     def start_session(self):
         """
         Start a new session based on this configuration
         """
 
         return get_response(json.dumps(self.get_touchforms_dict()))
-    
+
 
 class XformsEvent(object):
     """
-    A wrapper for the json event object that comes back from touchforms, which 
+    A wrapper for the json event object that comes back from touchforms, which
     looks approximately like this:n
-    
+
     { "datatype":"select",
       "style":{},
       "choices":["red","green","blue"],
@@ -133,20 +120,20 @@ class XformsEvent(object):
         self.datatype = datadict.get("datatype", "")
         self.output = datadict.get("output", "")
         self.choices = datadict.get("choices", None)
-            
+
     @property
     def text_prompt(self):
         """
         A text-only prompt for this. Used in pure text (or sms) mode.
-        
+
         Kept for backwards compatibility. Should use get_text_prompt, below.
         """
         return self.get_text_prompt()
-    
+
     def get_text_prompt(self):
         """
         Get a text-only prompt for this. Used in pure text (or sms) mode.
-        
+
         """
         if self.datatype == "select" or self.datatype == "multiselect":
             return select_to_text_compact(self.caption, self._dict["choices"])
@@ -158,18 +145,18 @@ def select_to_text_compact(caption, choices):
     """
     A function to convert a select item to text in a compact format.
     Format is:
-    
+
     [question] 1:[choice1], 2:[choice2]...
     """
-    return "%s %s." % (caption,
-                      ", ".join(["%s:%s" % (i+1, val) for i, val in \
-                                 enumerate(choices)])) 
+    return "{} {}.".format(
+        caption, ", ".join(["%s:%s" % (i+1, val) for i, val in enumerate(choices)]))
 
 
 class XformsResponse(object):
     """
-    A wrapper for the json that comes back from touchforms, which 
-    looks approximately like this:
+    A wrapper for the json that comes back from touchforms,
+    which looks approximately like this:
+
     {"event":
         { "datatype":"select",
           "style":{},
@@ -178,15 +165,15 @@ class XformsResponse(object):
      "session_id": 'd0addaa40dbcefefc6a687472a4e65d2',
      "status":"accepted",
      "seq_id":1}
-     
+
     Although errors come back more like this:
-    {'status': 'validation-error', 
-     'seq_id': 2, 
-     'reason': 'some message about constraint', 
+    {'status': 'validation-error',
+     'seq_id': 2,
+     'reason': 'some message about constraint',
      'type': 'constraint'}
-    
+
     """
-    
+
     def __init__(self, datadict):
         self._dict = datadict
         self.is_error = False
@@ -196,17 +183,17 @@ class XformsResponse(object):
             self.text_prompt = self.event.text_prompt
         else:
             self.event = None
-        
-        self.seq_id = datadict.get("seq_id","")
+
+        self.seq_id = datadict.get("seq_id", "")
         self.session_id = datadict.get("session_id", "")
         self.status = datadict.get("status", "")
-        
+
         # custom logic to handle errors
         if self.status == "validation-error":
             assert self.event is None, "There should be no touchforms event for errors"
             self.is_error = True
             self.text_prompt = datadict.get("reason", "that is not a legal answer")
-            
+
         # custom logic to handle http related errors
         elif self.status == "http-error":
             assert self.event is None, "There should be no touchforms event for errors"
@@ -296,12 +283,12 @@ class FormplayerInterface:
 
     def get_raw_instance(self):
         """
-        Gets the raw xml instance of the current session regardless of the state that we're in (used for logging partially complete
-        forms to couch when errors happen).
+        Gets the raw xml instance of the current session regardless of the state that we're in
+        (used for logging partially complete forms to couch when errors happen).
         """
 
         data = {
-            "action":"get-instance",
+            "action": "get-instance",
             "session-id": self.session_id,
             "domain": self.domain
         }
