37606a1716f43d0f15aa9b7db6c800ebbed64b84
Norman Hooper <nhooper@dimagi.com>
2019-11-26 19:11:05 +0200

    Fix import

--- corehq/motech/dhis2/tests/test_repeaters.py
+++ corehq/motech/dhis2/tests/test_repeaters.py
@@ -8,7 +8,6 @@ from django.test import SimpleTestCase, TestCase
 from jsonschema import validate
 from mock import Mock, patch
 from nose.tools import assert_equal, assert_true
-from semantic_version import Version
 
 from corehq.motech.dhis2.const import DHIS2_MAX_VERSION
 from corehq.motech.dhis2.repeaters import Dhis2Repeater
