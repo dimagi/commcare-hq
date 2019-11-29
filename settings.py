ee4addeedbb5ed97a3daa5a13f622c835292cb71
Cory Zue <czue@dimagi.com>
2019-11-29 10:05:51 +0200

    add XFrameOptionsMiddleware
    this will allow django to control the setting of this header at the
    view-level, and prevent the need for it being handled in the nginx / proxy
    server config

--- settings.py
+++ settings.py
@@ -128,6 +128,7 @@ MIDDLEWARE = [
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.common.BrokenLinkEmailsMiddleware',
