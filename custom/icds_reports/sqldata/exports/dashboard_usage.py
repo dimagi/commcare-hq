9a4eb926f7104d83221763ccf72ac36329011b61
challa <challarao@beehyv.com>
2019-12-10 11:42:25 +0530

    changed the logic to remove dependency on userES and fetching usernams from location ids

--- custom/icds_reports/sqldata/exports/dashboard_usage.py
+++ custom/icds_reports/sqldata/exports/dashboard_usage.py
@@ -45,7 +45,7 @@ class DashBoardUsage:
         self.user = couch_user
         self.domain = domain
         self.location_id_name_dict = defaultdict(str)
-        self.national_user = False
+        self.national_user = self.is_national_user()
 
     def get_role_from_username(self, username):
         for key, value in self.roles.items():
@@ -55,21 +55,6 @@ class DashBoardUsage:
 
     def convert_boolean_to_string(self, value):
         return 'yes' if value else 'no'
-
-    def get_users_by_location(self, user_supported_locations):
-        if not self.national_user:
-            user_query = UserES().mobile_users().domain(self.domain).location(
-                user_supported_locations
-            ).fields(['username', 'assigned_location_ids', 'last_login'])
-        else:
-            user_query = UserES().mobile_users().domain(self.domain
-                                                        ).fields(['username',
-                                                                  'assigned_location_ids',
-                                                                  'last_login'])
-        return [u for u in user_query.run().hits]
-
-    def get_names_from_ids(self, location_id):
-        return location_id.split('_')[0] + '_name'
 
     def populate_location_id_vs_name_mapping(self, results_queryset, main_location_type=None):
         """
@@ -89,23 +74,50 @@ class DashBoardUsage:
         return location_ids
 
     def get_location_id_string_from_location_type(self, location_type):
+        """
+        Appends location _id the location
+        :param location_type:
+        :return:
+        """
         return location_type + '_id'
 
     def get_location_type_string_from_location_id(self, location_id):
         return location_id.replace('_id', '')
 
     def check_if_date_in_last_week(self, date):
+        """
+        :param date: user last activity date
+        :return: Returns if the user has an activity in dashboard in last 7 days
+        """
         if date is None:
             return 'N/A', False
         date_formatted = datetime.datetime.strftime(date, "%d/%m/%Y, %I:%M %p")
         now = datetime.datetime.now()
-        return date_formatted, (now - date).days < 7
+        return date_formatted, 0 < (now - date).days <= 7
 
     def get_location_name_from_id(self, location_id):
+        """
+        :param location_id: location_id
+        :return: Returns the name of the location
+        """
         return self.location_id_name_dict[location_id]
 
-    def get_data_for_usgae_report(self, date, filters):
+    def get_data_for_usage_report(self, date, filters):
+        """
+        :param date: Date for which the report is needed
+        :param filters: Filter for the DashboardUserActivityReport
+        :return:Returns a list of activity report records based on the filters
+        """
         return list(DashboardUserActivityReport.objects.filter(date=date, **filters))
+
+    def is_national_user(self):
+        """
+        :return: Returns if the users has access to all locations or not
+        """
+        logged_in_user_locations = list(self.user.get_sql_locations(self.domain))
+        if not logged_in_user_locations or self.user.has_permission(self.domain, 'access_all_locations'):
+            return True
+        return False
 
     def get_excel_data(self):
         excel_rows = []
@@ -114,62 +126,63 @@ class DashBoardUsage:
         headers = ['Sr.No', 'State/UT Name', 'District Name', 'Block Name', 'Username', 'Level', 'Role',
                    'Launched?', 'Last Login  Activity ', 'Activity in the last 7 days?']
         serial_count = 0
-        logged_in_user_locations = list(self.user.get_sql_locations(self.domain))
-        if not logged_in_user_locations:
-            self.national_user = True
 
-        loop_counter = 0
-        # Need to fetch all users for a national user
-        while (self.national_user and loop_counter < 1) or len(logged_in_user_locations) > loop_counter:
-            location_type_filter = {
-                'aggregation_level': 5
-            }
-            if not self.national_user:
-                user_location = logged_in_user_locations[loop_counter]
-                user_location_type_name = \
-                    self.get_location_id_string_from_location_type(user_location.location_type_name)
-                location_type_filter[user_location_type_name] = user_location.get_id
+        location_type_filter = {
+            'aggregation_level': 5
+        }
+        if not self.national_user:
+            user_location = self.user.get_sql_location(self.domain)
+            user_location_type_name = \
+                self.get_location_id_string_from_location_type(user_location.location_type_name)
+            location_type_filter[user_location_type_name] = user_location.get_id
+        else:
+            user_location_type_name = None
+        for test_location in self.location_test_fields:
+            location_type_filter[test_location] = 0
+
+        all_awc_locations = AwcLocation.objects.filter(**location_type_filter).values(*self.required_fields)
+
+        self.populate_location_id_vs_name_mapping(all_awc_locations, user_location_type_name)
+
+        date = (datetime.datetime.now() - relativedelta(days=1)).date()
+        usage_data = None
+
+        dashboard_filters = {}
+
+        if not self.national_user:
+            # retrieving the user_level from logged in user location type
+            user_level = self.user_levels.index(user_location_type_name.replace('_id', '')) + 1
+            dashboard_filters['user_level__gt'] = user_level
+
+            dashboard_filters[user_location_type_name] = user_location.get_id
+
+        # keep the record in searched - current - month
+        while usage_data is None:
+            usage_data = self.get_data_for_usage_report(date, dashboard_filters)
+            date -= relativedelta(days=1)
+
+        for record in usage_data:
+            last_activity, activity_in_last_week = self.check_if_date_in_last_week(record.last_activity)
+
+            if record.user_level == 1:
+                district_name = ''
+                block_name = ''
             else:
-                user_location_type_name = None
-            for test_location in self.location_test_fields:
-                location_type_filter[test_location] = 0
-
-            all_awc_locations = AwcLocation.objects.filter(**location_type_filter).values(*self.required_fields)
-
-            location_ids = self.populate_location_id_vs_name_mapping(all_awc_locations, user_location_type_name)
-
-            users = self.get_users_by_location(location_ids)
-
-            dashboard_uname_rx = re.compile(r'^\d*\.[a-zA-Z]*@.*')
-
-            usernames = [user['username'] for user in users if dashboard_uname_rx.match(user['username'])]
-
-            date = datetime.datetime.now().date()
-            usage_data = None
-            dashboard_filters = {'username__in': usernames}
-            # keep the record in searched - current - month
-            while usage_data is None:
-                usage_data = self.get_data_for_usgae_report(date, dashboard_filters)
-                date -= relativedelta(days=1)
-
-            for record in usage_data:
-                last_activity, activity_in_last_week = self.check_if_date_in_last_week(record.last_activity)
                 district_name = self.get_location_name_from_id(record.district_id)
-                block_name = self.get_location_name_from_id(record.block_id)
-                if record.user_level == 1:
-                    district_name = ''
+                if record.user_level == 2:
                     block_name = ''
-                elif record.user_level == 2:
-                    block_name = ''
-                excel = [serial_count, self.get_location_name_from_id(record.state_id),
-                         district_name, block_name, record.username.split('@')[0],
-                         self.user_levels[record.user_level - 1],
-                         self.get_role_from_username(record.username),
-                         self.convert_boolean_to_string(record.location_launched),
-                         last_activity,
-                         self.convert_boolean_to_string(activity_in_last_week)]
-                excel_rows.append(excel)
-            loop_counter += 1
+                else:
+                    block_name = self.get_location_name_from_id(record.block_id)
+
+            excel = [serial_count, self.get_location_name_from_id(record.state_id),
+                     district_name, block_name, record.username.split('@')[0],
+                     self.user_levels[record.user_level - 1],
+                     self.get_role_from_username(record.username),
+                     self.convert_boolean_to_string(record.location_launched),
+                     last_activity,
+                     self.convert_boolean_to_string(activity_in_last_week)]
+            excel_rows.append(excel)
+
         excel_rows = sorted(excel_rows, key=lambda x: (x[1], x[2], x[3]))
         # appending serial numbers
         for i in range(len(excel_rows)):
