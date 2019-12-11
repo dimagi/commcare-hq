ac665a93c69bf9082560a175b1186114b55f1c6a
Rohit Negi <rohit25.negi@gmail.com>
2019-12-11 22:24:56 +0530

    optimized query

--- custom/icds_reports/utils/data_pull/governance_dashboard.sql
+++ custom/icds_reports/utils/data_pull/governance_dashboard.sql
@@ -41,112 +41,59 @@ where agg_awc_monthly.num_launched_awcs=1 and agg_awc_monthly.month='2019-11-01'
 
 
 
-select
-    state_name,
-    district_name,
-    block_name,
-    supervisor_name,
-    awc_name,
+create unlogged table temp_child_data_pull as select
+awc_id,
     SUM(CASE WHEN age_tranche::INTEGER<=36 THEN valid_in_month else 0 END) as open_valid_till_month_0_3,
     SUM(CASE WHEN age_tranche::INTEGER between 37 and 72 THEN valid_in_month else 0 END) as open_valid_till_month_3_6,
     SUM(CASE WHEN age_tranche::INTEGER<=36 THEN valid_all_registered_in_month else 0 END) as open_register_till_month_0_3,
     SUM(CASE WHEN age_tranche::INTEGER between 37 and 72 THEN valid_all_registered_in_month else 0 END) as open_register_till_month_3_6
-FROM agg_awc_monthly
-    left join agg_child_health  on
-    agg_awc_monthly.awc_id= agg_child_health.awc_id and
-    agg_awc_monthly.month=agg_child_health.month and
-    agg_awc_monthly.aggregation_level=agg_child_health.aggregation_level
-where agg_awc_monthly.num_launched_awcs=1 and agg_awc_monthly.month='2019-11-01' and agg_awc_monthly.aggregation_level=5
-group by state_name,
-    district_name,
-    block_name,
-    supervisor_name,
-    awc_name
-
+from "child_health_monthly" child_health where month='2019-10-01'
+group by awc_id;
 /*
- GroupAggregate  (cost=1514843.35..1514845.27 rows=24 width=108)
-   Group Key: awc_location.state_name, awc_location.district_name, awc_location.block_name, awc_location.supervisor_name, awc_location.awc_name
-   ->  Sort  (cost=1514843.35..1514843.41 rows=24 width=86)
-         Sort Key: awc_location.state_name, awc_location.district_name, awc_location.block_name, awc_location.supervisor_name, awc_location.awc_name
-         ->  Nested Loop Left Join  (cost=80296.62..1514842.80 rows=24 width=86)
-               Join Filter: ((months.start_date = agg_child_health.month) AND (months.start_date = agg_child_health.month))
-               ->  Nested Loop  (cost=80296.62..208072.31 rows=1 width=115)
-                     ->  Gather  (cost=80296.62..208047.25 rows=1 width=115)
-                           Workers Planned: 4
-                           ->  Parallel Hash Join  (cost=79296.62..207047.15 rows=1 width=115)
-                                 Hash Cond: ((agg_awc_1.state_id = awc_location.state_id) AND (agg_awc_1.district_id = awc_location.district_id) AND (agg_awc_1.block_id = awc_location.block_id) AND (agg_awc_1.supervisor_id = awc_location.supervisor_id) AND (agg_awc_1.awc_id = awc_location.doc_id))
-                                 ->  Parallel Append  (cost=0.00..113228.11 rows=141670 width=173)
-                                       ->  Parallel Seq Scan on "agg_awc_2019-11-01_5" agg_awc_1  (cost=0.00..112519.76 rows=141670 width=173)
-                                             Filter: ((month = '2019-11-01'::date) AND (aggregation_level = 5) AND (num_launched_awcs = 1))
-                                       ->  Parallel Seq Scan on agg_awc  (cost=0.00..0.00 rows=1 width=168)
-                                             Filter: ((month = '2019-11-01'::date) AND (aggregation_level = 5) AND (num_launched_awcs = 1))
-                                 ->  Parallel Hash  (cost=69540.88..69540.88 rows=178255 width=240)
-                                       ->  Parallel Seq Scan on awc_location_local awc_location  (cost=0.00..69540.88 rows=178255 width=240)
-                                             Filter: (aggregation_level = 5)
-                     ->  Seq Scan on icds_months_local months  (cost=0.00..25.00 rows=6 width=4)
-                           Filter: (start_date = '2019-11-01'::date)
-               ->  Append  (cost=0.00..1306770.38 rows=8 width=50)
-                     ->  Seq Scan on agg_child_health  (cost=0.00..0.00 rows=1 width=80)
-                           Filter: ((month = '2019-11-01'::date) AND (aggregation_level = 5) AND (awc_location.aggregation_level = aggregation_level) AND (awc_location.doc_id = awc_id))
-                     ->  Index Scan using staging_agg_child_health_aggregation_level_gender_idx1 on "agg_child_health_2019-11-01"  (cost=0.56..1306770.34 rows=7 width=50)
-                           Index Cond: ((awc_location.aggregation_level = aggregation_level) AND (aggregation_level = 5))
-                           Filter: ((month = '2019-11-01'::date) AND (awc_location.doc_id = awc_id))
-(27 rows)
+ HashAggregate  (cost=0.00..0.00 rows=0 width=0)
+   Group Key: remote_scan.awc_id
+   ->  Custom Scan (Citus Real-Time)  (cost=0.00..0.00 rows=0 width=0)
+         Task Count: 64
+         Tasks Shown: One of 64
+         ->  Task
+               Node: host=100.71.184.232 port=6432 dbname=icds_ucr
+               ->  Finalize GroupAggregate  (cost=463779.67..469225.78 rows=8043 width=65)
+                     Group Key: awc_id
+                     ->  Gather Merge  (cost=463779.67..468642.66 rows=40215 width=65)
+                           Workers Planned: 5
+                           ->  Sort  (cost=462779.59..462799.70 rows=8043 width=65)
+                                 Sort Key: awc_id
+                                 ->  Partial HashAggregate  (cost=462177.43..462257.86 rows=8043 width=65)
+                                       Group Key: awc_id
+                                       ->  Parallel Index Scan using chm_month_supervisor_id_102648 on child_health_monthly_102648 child_health  (cost=0.56..454458.92 rows=134235 width=43)
+                                             Index Cond: (month = '2019-10-01'::date)
+(17 rows)
+ */
 
-*/
 
-select
-    state_name,
-    district_name,
-    block_name,
-    supervisor_name,
-    awc_name,
-    SUM(CASE WHEN age_tranche::INTEGER<=36 THEN valid_in_month else 0 END) as open_valid_till_month_0_3,
-    SUM(CASE WHEN age_tranche::INTEGER between 37 and 72 THEN valid_in_month else 0 END) as open_valid_till_month_3_6,
-    SUM(CASE WHEN age_tranche::INTEGER<=36 THEN valid_all_registered_in_month else 0 END) as open_register_till_month_0_3,
-    SUM(CASE WHEN age_tranche::INTEGER between 37 and 72 THEN valid_all_registered_in_month else 0 END) as open_register_till_month_3_6
-FROM agg_awc_monthly
-    left join agg_child_health  on
-    agg_awc_monthly.awc_id= agg_child_health.awc_id and
-    agg_awc_monthly.month=agg_child_health.month and agg_awc_monthly.month=agg_child_health.month and
-    agg_awc_monthly.aggregation_level=agg_child_health.aggregation_level
-where agg_awc_monthly.num_launched_awcs=1 and agg_awc_monthly.month='2019-10-01' and agg_awc_monthly.aggregation_level=5
+COPY(select
+state_name,
+district_name,
+block_name,
+supervisor_name,
+awc_name
+sum(open_valid_till_month_0_3) as "open_valid_till_month_0_3",
+sum(open_valid_till_month_3_6) as "open_valid_till_month_3_6",
+sum(open_register_till_month_0_3) as "open_register_till_month_0_3",
+sum(open_register_till_month_3_6)as "open_register_till_month_3_6",
+from temp_child_data_pull t join awc_location_local a on a.doc_id=t.awc_id where aggregation_level=5 and state_is_test=0
 group by state_name,
-    district_name,
-    block_name,
-    supervisor_name,
-    awc_name
+district_name,
+block_name,
+supervisor_name,
+awc_name
+order by state_name asc) to 'child_Oct.csv' DELIMITER ',' CSV HEADER ENCODING 'UTF-8';
 
-/*
- GroupAggregate  (cost=1385741.00..1385742.76 rows=22 width=108)
-   Group Key: awc_location.state_name, awc_location.district_name, awc_location.block_name, awc_location.supervisor_name, awc_location.awc_name
-   ->  Sort  (cost=1385741.00..1385741.05 rows=22 width=86)
-         Sort Key: awc_location.state_name, awc_location.district_name, awc_location.block_name, awc_location.supervisor_name, awc_location.awc_name
-         ->  Nested Loop Left Join  (cost=80296.62..1385740.51 rows=22 width=86)
-               Join Filter: ((months.start_date = agg_child_health.month) AND (months.start_date = agg_child_health.month))
-               ->  Nested Loop  (cost=80296.62..201384.21 rows=1 width=115)
-                     ->  Gather  (cost=80296.62..201359.15 rows=1 width=115)
-                           Workers Planned: 4
-                           ->  Parallel Hash Join  (cost=79296.62..200359.05 rows=1 width=115)
-                                 Hash Cond: ((agg_awc_1.state_id = awc_location.state_id) AND (agg_awc_1.district_id = awc_location.district_id) AND (agg_awc_1.block_id = awc_location.block_id) AND (agg_awc_1.supervisor_id = awc_location.supervisor_id) AND (agg_awc_1.awc_id = awc_location.doc_id))
-                                 ->  Parallel Append  (cost=0.00..107009.76 rows=134070 width=173)
-                                       ->  Parallel Seq Scan on "agg_awc_2019-10-01_5" agg_awc_1  (cost=0.00..106339.41 rows=134070 width=173)
-                                             Filter: ((month = '2019-10-01'::date) AND (aggregation_level = 5) AND (num_launched_awcs = 1))
-                                       ->  Parallel Seq Scan on agg_awc  (cost=0.00..0.00 rows=1 width=168)
-                                             Filter: ((month = '2019-10-01'::date) AND (aggregation_level = 5) AND (num_launched_awcs = 1))
-                                 ->  Parallel Hash  (cost=69540.88..69540.88 rows=178255 width=240)
-                                       ->  Parallel Seq Scan on awc_location_local awc_location  (cost=0.00..69540.88 rows=178255 width=240)
-                                             Filter: (aggregation_level = 5)
-                     ->  Seq Scan on icds_months_local months  (cost=0.00..25.00 rows=6 width=4)
-                           Filter: (start_date = '2019-10-01'::date)
-               ->  Append  (cost=0.00..1184356.18 rows=8 width=50)
-                     ->  Seq Scan on agg_child_health  (cost=0.00..0.00 rows=1 width=80)
-                           Filter: ((month = '2019-10-01'::date) AND (aggregation_level = 5) AND (awc_location.aggregation_level = aggregation_level) AND (awc_location.doc_id = awc_id))
-                     ->  Index Scan using staging_agg_child_health_aggregation_level_gender_idx2 on "agg_child_health_2019-10-01"  (cost=0.43..1184356.14 rows=7 width=50)
-                           Index Cond: ((awc_location.aggregation_level = aggregation_level) AND (aggregation_level = 5))
-                           Filter: ((month = '2019-10-01'::date) AND (awc_location.doc_id = awc_id))
-(27 rows)
-*/
+
+
+
+
+
 
 
 
@@ -212,3 +159,13 @@ where agg_awc_monthly.num_launched_awcs=1 and agg_awc_monthly.month='2019-10-01'
          Filter: (start_date = '2019-11-01'::date)
 (14 rows)
  */
+
+
+
+
+
+
+
+
+
+​
