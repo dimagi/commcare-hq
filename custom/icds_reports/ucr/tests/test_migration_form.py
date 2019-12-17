e5546b58bc79fd24f842a7c3d800cd88642905a5
Shubham Bansal <shubhambansal17@hotmail.com>
2019-12-17 16:30:31 +0530

    [fix]: added the abrv static-icds-cas-
    - Github Issue:
    
    Authored-by: Shubham Bansal <shubhambansal17@hotmail.com>
    Signed-off-by: Shubham Bansal <shubhambansal17@hotmail.com>

--- custom/icds_reports/ucr/tests/test_migration_form.py
+++ custom/icds_reports/ucr/tests/test_migration_form.py
@@ -6,7 +6,7 @@ from custom.icds_reports.ucr.tests.test_base_form_ucr import BaseFormsTest
 @patch('custom.icds_reports.ucr.expressions._get_user_location_id', lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
 @patch('corehq.apps.locations.ucr_expressions._get_location_type_name', lambda loc_id, context: 'awc')
 class TestMigrationForms(BaseFormsTest):
-    ucr_name = "static-migration_form"
+    ucr_name = "static-icds-cas-static-migration_form"
 
     def test_migration_form(self):
         self._test_data_source_results(
