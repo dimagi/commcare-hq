c5339315d3fcc655d1858f6239396e0b827db6f3
Shubham Bansal <shubhambansal17@hotmail.com>
2019-12-17 15:11:46 +0530

    [build]: added the Migration UCR Form
    - Github Issue:
    
    Authored-by: Shubham Bansal <shubhambansal17@hotmail.com>
    Signed-off-by: Shubham Bansal <shubhambansal17@hotmail.com>

---
+++ custom/icds_reports/ucr/tests/test_migration_form.py
@@ -0,0 +1,20 @@
+from mock import patch
+
+from custom.icds_reports.ucr.tests.test_base_form_ucr import BaseFormsTest
+
+
+@patch('custom.icds_reports.ucr.expressions._get_user_location_id', lambda user_id: 'qwe56poiuytr4xcvbnmkjfghwerffdaa')
+@patch('corehq.apps.locations.ucr_expressions._get_location_type_name', lambda loc_id, context: 'awc')
+class TestMigrationForms(BaseFormsTest):
+    ucr_name = "static-migration_form"
+
+    def test_migration_form(self):
+        pass
+        self._test_data_source_results(
+            'migration_form',
+            [{
+                "timeend": None,
+                "migration_status": "migrated",
+                "awc_id": None,
+                "person_case_id": "b08669b9-f8d5-4dfb-891f-8727a4486682"
+            }])
