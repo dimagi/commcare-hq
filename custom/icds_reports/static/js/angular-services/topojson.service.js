23b55c6751114cc78e353a955b173d7ef3386432
Cory Zue <czue@dimagi.com>
2019-12-18 10:03:38 +0200

    add caching to block-level topojsons

--- custom/icds_reports/static/js/angular-services/topojson.service.js
+++ custom/icds_reports/static/js/angular-services/topojson.service.js
@@ -1,17 +1,27 @@
 window.angular.module('icdsApp').factory('topojsonService', ['$http', function ($http) {
     var url = hqImport('hqwebapp/js/initial_page_data').reverse;
+    var CACHE = {
+        blocks: {}
+    };
+
     return {
         getTopoJsonForDistrict: function (district) {
-            return $http.get(url('topojson'), {
-                params: {district: district},
-            }).then(
-                function (response) {
-                    return response.data;
-                },
-                function () {
-                    console.error(error);
-                }
-            );
+            if (district in CACHE["blocks"]) {
+                // https://javascript.info/promise-api#promise-resolve-reject
+                return Promise.resolve(CACHE["blocks"][district]);
+            } else {
+                return $http.get(url('topojson'), {
+                    params: {district: district},
+                }).then(
+                    function (response) {
+                        CACHE['blocks'][district] = response.data;
+                        return response.data;
+                    },
+                    function () {
+                        console.error(error);
+                    }
+                );
+            }
         },
     };
 }]);
