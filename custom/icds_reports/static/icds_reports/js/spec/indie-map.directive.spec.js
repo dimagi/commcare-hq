41d28d433d4b432a9036f965ad0cd5cc2f5067bd
Cory Zue <czue@dimagi.com>
2019-12-20 13:26:46 +0200

    fix tests

--- custom/icds_reports/static/icds_reports/js/spec/indie-map.directive.spec.js
+++ custom/icds_reports/static/icds_reports/js/spec/indie-map.directive.spec.js
@@ -144,10 +144,12 @@ describe('Indie Map Directive', function () {
 
     it('tests html content of update map', function () {
         controller.data = mockData;
-        var expected = '<div class="modal-header"><button type="button" class="close" ' +
+        var expected = '<div class="secondary-location-selector">' +
+            '<div class="modal-header"><button type="button" class="close" ' +
             'ng-click="$ctrl.closePopup()" aria-label="Close"><span aria-hidden="true">&times;</span>' +
             '</button></div><div class="modal-body"><button class="btn btn-xs btn-default" ' +
-            'ng-click="$ctrl.attemptToDrillToLocation(\'Uttar Pradesh\')">Uttar Pradesh</button></div>';
+            'ng-click="$ctrl.attemptToDrillToLocation(\'Uttar Pradesh\')">Uttar Pradesh</button>' +
+            '</div></div>';
 
         var result = controller.getSecondaryLocationSelectionHtml(mockGeography);
         assert.equal(expected, result);
