7dabcc36c122d4c0784ea541021dc5dd22e66db4
challa <challarao@beehyv.com>
2019-12-30 20:26:58 +0530

    fixed the timestamp issue

--- custom/icds_reports/reports/governance_apis.py
+++ custom/icds_reports/reports/governance_apis.py
@@ -2,7 +2,7 @@ from datetime import date
 
 from custom.icds_reports.cache import icds_quickcache
 from custom.icds_reports.models import AggAwcMonthly
-from custom.icds_reports.utils import DATA_NOT_ENTERED, india_now
+from custom.icds_reports.utils import DATA_NOT_ENTERED
 
 
 @icds_quickcache(['start', 'length', 'year', 'month', 'order', 'query_filters'], timeout=30 * 60)
@@ -14,16 +14,6 @@ def get_home_visit_data(start, length, year, month, order, query_filters):
         'state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name', 'month', 'valid_visits',
         'expected_visits'
     )
-    config = {
-        'data': [],
-        'filter_params': {
-            'start': start,
-            'month': month,
-            'year': year,
-            'count': data.count(),
-            'timestamp': india_now()
-        }
-    }
 
     def get_value_or_data_not_entered(source, field):
         value = source.get(field)
@@ -43,8 +33,9 @@ def get_home_visit_data(start, length, year, month, order, query_filters):
             expected_visits=get_value_or_data_not_entered(row_data, 'expected_visits'),
         )
 
+    data_rows = []
     for row in data:
-        config['data'].append(base_data(row))
-    config['data'] = config['data'][int(start):(int(start) + length)]
+        data_rows.append(base_data(row))
+    data_rows = data_rows[int(start):(int(start) + length)]
 
-    return config
+    return data_rows, data.count()
--- custom/icds_reports/tests/agg_tests/test_governance_apis.py
+++ custom/icds_reports/tests/agg_tests/test_governance_apis.py
@@ -16,10 +16,10 @@ class GovernanceApiTest(TestCase):
         limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
         query_filters = {'aggregation_level': 5}
         order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(0, limit,
-                                   2017, 5, order, query_filters)
+        data, count = get_home_visit_data(0, limit,
+                                          2017, 5, order, query_filters)
         expected_count = 55
-        self.assertEqual(data['filter_params']['count'], expected_count)
+        self.assertEqual(count, expected_count)
 
     def test_data_fetching_without_start_for_home_visit_api(self):
         """
@@ -28,13 +28,13 @@ class GovernanceApiTest(TestCase):
         limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
         query_filters = {'aggregation_level': 5}
         order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(0, limit,
-                                   2017, 5, order, query_filters)
+        data, count = get_home_visit_data(0, limit,
+                                          2017, 5, order, query_filters)
         expected_first_row = {
             'state': 'st1', 'district': 'd1', 'block': 'b1', 'sector': 's1', 'awc': 'a1',
             'month': datetime.date(2017, 5, 1), 'valid_visits': 0, 'expected_visits': 4,
         }
-        self.assertEqual(data['data'][0], expected_first_row)
+        self.assertEqual(data[0], expected_first_row)
 
     def test_data_fetching_with_start_for_home_visit_api(self):
         """
@@ -43,31 +43,13 @@ class GovernanceApiTest(TestCase):
         limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
         query_filters = {'aggregation_level': 5}
         order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(1, limit,
-                                   2017, 5, order, query_filters)
+        data, count = get_home_visit_data(1, limit,
+                                          2017, 5, order, query_filters)
         expected_first_row = {
             'state': 'st1', 'district': 'd1', 'block': 'b1', 'sector': 's1', 'awc': 'a17',
             'month': datetime.date(2017, 5, 1), 'valid_visits': 0, 'expected_visits': 3
         }
-        self.assertEqual(data['data'][0], expected_first_row)
-
-    def test_filter_param_in_response_for_home_visit_api(self):
-        """
-        test to check the filter params are returned from the home visit api with start parameter
-        """
-        limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
-        query_filters = {'aggregation_level': 5}
-        order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(1, limit,
-                                   2017, 5, order, query_filters)
-        expected = {
-            'start': 1,
-            'month': 5,
-            'year': 2017,
-            'count': 55,
-            'timestamp': india_now()
-        }
-        self.assertEqual(data['filter_params'], expected)
+        self.assertEqual(data[0], expected_first_row)
 
     def test_data_fetching_total_record_count_for_no_records_for_home_visit_api(self):
         """
@@ -76,11 +58,11 @@ class GovernanceApiTest(TestCase):
         limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
         query_filters = {'aggregation_level': 5}
         order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(0, limit,
-                                   2017, 6, order, query_filters)
+        data, count = get_home_visit_data(0, limit,
+                                          2017, 6, order, query_filters)
         expected_count = 0
-        self.assertEqual(data['filter_params']['count'], expected_count)
-        self.assertEqual(data['data'], [])
+        self.assertEqual(count, expected_count)
+        self.assertEqual(data, [])
 
     def test_data_fetching_total_record_count_with_state_id_for_home_visit_api(self):
         """
@@ -89,7 +71,7 @@ class GovernanceApiTest(TestCase):
         limit = GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION
         query_filters = {'aggregation_level': 5, 'state_id': 'st1'}
         order = ['state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name']
-        data = get_home_visit_data(0, limit,
-                                   2017, 5, order, query_filters)
+        data, count = get_home_visit_data(0, limit,
+                                          2017, 5, order, query_filters)
         expected_count = 26
-        self.assertEqual(data['filter_params']['count'], expected_count)
+        self.assertEqual(count, expected_count)
