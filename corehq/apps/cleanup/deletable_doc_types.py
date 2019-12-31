3e79c4628c2ef97b825d702867b1ea16399e4011
Jennifer Schweers <orange.jenny@gmail.com>
2019-12-30 21:43:24 -0500

    Added ApplicationAccess to DELETABLE_COUCH_DOC_TYPES

--- corehq/apps/cleanup/deletable_doc_types.py
+++ corehq/apps/cleanup/deletable_doc_types.py
@@ -12,6 +12,7 @@ META_DB = None
 # Doc types for classes we've removed from our code
 # but may still have docs lying around from
 DELETABLE_COUCH_DOC_TYPES = {
+    'ApplicationAccess': (MAIN_DB,),
     'SurveyKeyword': (MAIN_DB,),
     'SurveyKeywordAction': (MAIN_DB,),
     'CaseReminder': (MAIN_DB,),
