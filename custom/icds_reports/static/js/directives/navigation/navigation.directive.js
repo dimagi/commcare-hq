70e97212733a9ae323e896bde38eb0c227ee76a0
Rohit Negi <rohit25.negi@gmail.com>
2020-01-08 18:05:49 +0530

    do calculation with reference to April

--- custom/icds_reports/static/js/directives/navigation/navigation.directive.js
+++ custom/icds_reports/static/js/directives/navigation/navigation.directive.js
@@ -38,8 +38,8 @@ function NavigationController($window, $rootScope, $scope, $route, $routeParams,
         var selectedMonth = parseInt($location.search()['month']) || new Date().getMonth() + 1;
         var selectedYear =  parseInt($location.search()['year']) || new Date().getFullYear();
 
-        var selectedDate = new Date(selectedYear, selectedMonth);
-        return haveAccessToFeatures && selectedDate >= new Date(2019, 4);
+        var selectedDate = new Date(selectedYear, selectedMonth - 1);
+        return haveAccessToFeatures && selectedDate >= new Date(2019, 3);
     };
 
     // used by mobile only
