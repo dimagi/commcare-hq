cd70cc62f1e35c46803ce0f6a4ca5f071f249fec
Rohit Negi <rohit25.negi@gmail.com>
2020-01-08 14:52:32 +0530

    aesthetic change

--- custom/icds_reports/const.py
+++ custom/icds_reports/const.py
@@ -155,7 +155,7 @@ CHILDREN_ENROLLED_FOR_ANGANWADI_SERVICES = 'Children enrolled for Anganwadi Serv
 PREGNANT_WOMEN_ENROLLED_FOR_ANGANWADI_SERVICES = 'Pregnant women enrolled for Anganwadi Services'
 LACTATING_WOMEN_ENROLLED_FOR_ANGANWADI_SERVICES = 'Lactating women enrolled for Anganwadi Services'
 ADOLESCENT_GIRLS_ENROLLED_FOR_ANGANWADI_SERVICES = 'Adolescent girls enrolled for Anganwadi Services'
-OUT_OF_SCHOOL_ADOLESCENT_GIRLS_11_14_YEARS = 'Out of school Adolescent girls(11-14 years)'
+OUT_OF_SCHOOL_ADOLESCENT_GIRLS_11_14_YEARS = 'Out of school Adolescent girls (11-14 years)'
 ADOLESCENT_GIRLS_DATA_THRESHOLD = date(2019, 4, 1)
 
 GOVERNANCE_API_HOME_VISIT_RECORDS_PAGINATION = 10000
