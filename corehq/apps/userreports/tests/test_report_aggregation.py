a8fe8c9adb18b2b8336013ca86e31a58c951aaa3
Jennifer Schweers <orange.jenny@gmail.com>
2020-01-10 15:12:12 -0500

    Revert "A Y2K20 bug?"
    This reverts commit de6b5a62a3a971e31116304c75495931dda07e55.

--- corehq/apps/userreports/tests/test_report_aggregation.py
+++ corehq/apps/userreports/tests/test_report_aggregation.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest
 from django.test import TestCase
+from datetime import datetime
 
 from corehq.apps.userreports.exceptions import BadSpecError, UserReportsError
 from corehq.apps.userreports.models import (
@@ -562,12 +563,12 @@ class TestReportAggregationSQL(ConfigurableReportTestMixin, TestCase):
 class TestReportMultipleAggregationsSQL(ConfigurableReportTestMixin, TestCase):
     @classmethod
     def _relative_date(cls, month, day, year_offset=0):
-        year = 2019 + year_offset
+        year = datetime.utcnow().year + year_offset
         return f"{year}-{month:02}-{day:02}"
 
     @classmethod
     def _relative_month(cls, month, year_offset=0):
-        year = 2019 + year_offset
+        year = datetime.utcnow().year + year_offset
         return f"{year}-{month:02}"
 
     @classmethod
