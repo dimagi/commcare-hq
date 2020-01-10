61aaf24149b990e0cb716a8481cb0fc9b136ca85
Ethan Soergel <esoergel@gmail.com>
2020-01-10 18:57:50 -0500

    Clear cache when fiddling with plans

--- corehq/apps/accounting/tests/test_enterprise_mode.py
+++ corehq/apps/accounting/tests/test_enterprise_mode.py
@@ -4,7 +4,10 @@ from corehq import privileges
 from corehq.apps.accounting.models import SoftwarePlanEdition
 from corehq.apps.accounting.tests.base_tests import BaseAccountingTest
 from corehq.apps.accounting.tests.utils import DomainSubscriptionMixin
-from corehq.apps.accounting.utils import domain_has_privilege
+from corehq.apps.accounting.utils import (
+    clear_plan_version_cache,
+    domain_has_privilege,
+)
 from corehq.apps.domain.shortcuts import create_domain
 
 ADVANCED_PRIVILEGE = privileges.CUSTOM_BRANDING  # chosen arbitrarily, feel free to change
@@ -22,6 +25,7 @@ class TestEnterpriseMode(DomainSubscriptionMixin, BaseAccountingTest):
     def test_standard_cant_access_advanced(self):
         self.setup_subscription(self.domain_obj.name, SoftwarePlanEdition.STANDARD)
         self.addCleanup(self.teardown_subscriptions)
+        self.addCleanup(clear_plan_version_cache)
         self.assertFalse(self.domain_obj.has_privilege(ADVANCED_PRIVILEGE))
 
     def test_no_plan_cant_access_anything(self):
