6636a78a9895c79dafaff80742391f72f0d29793
Cal Ellowitz <calellowitz@gmail.com>
2020-01-13 13:52:11 -0500

    removing concurrently

--- custom/icds_reports/migrations/0160_drop_like_indices.py
+++ custom/icds_reports/migrations/0160_drop_like_indices.py
@@ -12,11 +12,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_growth_monitoring_forms_case_id_ddb22a97_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_daily_feeding_forms_case_id_114445ac_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_ccs_record_cf_forms_case_id_c4e821f9_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_ccs_record_bp_forms_case_id_766b782e_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_ccs_record_postnatal_forms_case_id_d83b4f04_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_child_hea_case_id_e6c43f67_like"'),
-        migrations.RunSQL('DROP INDEX CONCURRENTLY IF EXISTS "icds_dashboard_comp_feed_form_case_id_3c67578e_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_growth_monitoring_forms_case_id_ddb22a97_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_daily_feeding_forms_case_id_114445ac_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_ccs_record_cf_forms_case_id_c4e821f9_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_ccs_record_bp_forms_case_id_766b782e_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_ccs_record_postnatal_forms_case_id_d83b4f04_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_child_hea_case_id_e6c43f67_like"'),
+        migrations.RunSQL('DROP INDEX IF EXISTS "icds_dashboard_comp_feed_form_case_id_3c67578e_like"'),
     ]
