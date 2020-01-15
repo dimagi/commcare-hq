32f0076dfe64473bd03a65fd5ff3a28fd0cd0080
Rohit Negi <rohit25.negi@gmail.com>
2020-01-15 20:02:18 +0530

    Removed Unwanted Columns

--- custom/icds_reports/migrations/sql_templates/database_views/agg_awc_monthly.sql
+++ custom/icds_reports/migrations/sql_templates/database_views/agg_awc_monthly.sql
@@ -89,11 +89,7 @@ CREATE VIEW agg_awc_monthly AS
         "agg_awc"."infantometer" AS "infantometer",
         "agg_awc"."stadiometer" AS "stadiometer",
         "agg_awc"."valid_visits" AS "valid_visits",
-        "agg_awc"."expected_visits" AS "expected_visits",
-        COALESCE("agg_awc"."cases_ccs_pregnant_reg_in_month", 0) AS "cases_ccs_pregnant_reg_in_month",
-        COALESCE("agg_awc"."cases_ccs_lactating_reg_in_month", 0) AS "cases_ccs_lactating_reg_in_month",
-        COALESCE("agg_awc"."cases_ccs_pregnant_all_reg_in_month", 0) AS "cases_ccs_pregnant_all_reg_in_month",
-        COALESCE("agg_awc"."cases_ccs_lactating_all_reg_in_month", 0) AS "cases_ccs_lactating_all_reg_in_month"
+        "agg_awc"."expected_visits" AS "expected_visits"
 
     FROM "public"."awc_location_months_local" "awc_location_months"
     LEFT JOIN "public"."agg_awc" "agg_awc" ON (
