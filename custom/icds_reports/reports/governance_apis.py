5709dd211dad3840d4a1145833ab84b031c8d302
challa <challarao@beehyv.com>
2020-01-17 17:17:39 +0530

    test case fix

--- custom/icds_reports/reports/governance_apis.py
+++ custom/icds_reports/reports/governance_apis.py
@@ -1,5 +1,7 @@
 from datetime import date
 
+from django.db.models import F
+
 from custom.icds_reports.cache import icds_quickcache
 from custom.icds_reports.const import AggregationLevels
 from custom.icds_reports.models import AggAwcMonthly, AwcLocation
@@ -11,29 +13,20 @@ def get_home_visit_data(length, year, month, order, query_filters):
     data = AggAwcMonthly.objects.filter(
         month=date(year, month, 1),
         **query_filters
-    ).order_by(*order).values(
-        'awc_id', 'awc_site_code', 'month', 'valid_visits', 'expected_visits'
+    ).order_by(*order).annotate(awc_code=F('awc_site_code')).values(
+        'awc_id', 'awc_code', 'valid_visits', 'expected_visits'
     )
     paginated_data = data[:length]
 
-    def get_value_or_data_not_entered(source, field):
-        value = source.get(field)
+    def get_value_or_data_not_entered(value):
         if value is None:
             return DATA_NOT_ENTERED
         return value
 
     def base_data(row_data):
-        return dict(
-            awc_id=get_value_or_data_not_entered(row_data, 'awc_id'),
-            awc_code=get_value_or_data_not_entered(row_data, 'awc_site_code'),
-            valid_visits=get_value_or_data_not_entered(row_data, 'valid_visits'),
-            expected_visits=get_value_or_data_not_entered(row_data, 'expected_visits'),
-        )
+        return {key: get_value_or_data_not_entered(value) for key, value in row_data.items()}
 
-    data_rows = []
-    for row in paginated_data:
-        data_rows.append(base_data(row))
-    return data_rows, data.count()
+    return [base_data(row) for row in paginated_data], data.count()
 
 
 @icds_quickcache([], timeout=30 * 60)
