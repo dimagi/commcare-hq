ca05e65ba1865b1d2465cf9d85d6d8fb42d6af13
Jennifer Schweers <orange.jenny@gmail.com>
2020-01-20 17:14:34 -0500

    Moved reads to SQL
    This removes the list_deploys command, which depends on code_snapshot,
    which hasn't been populated since 2016 (see earlier commit message).

--- corehq/apps/hqadmin/models.py
+++ corehq/apps/hqadmin/models.py
@@ -22,6 +22,13 @@ class SQLHqDeploy(models.Model):
     class Meta:
         db_table = "hqadmin_hqdeploy"
 
+    @classmethod
+    def get_latest(cls, environment, limit=1):
+        query = SQLHqDeploy.objects.filter(environment=environment).order_by("-date")
+        if limit:
+            return query[:limit]
+        return query
+
 
 class HqDeploy(Document):
     date = DateTimeProperty()
@@ -30,29 +37,6 @@ class HqDeploy(Document):
     code_snapshot = DictProperty()
     diff_url = StringProperty()
 
-    @classmethod
-    def get_latest(cls, environment, limit=1):
-        result = HqDeploy.view(
-            'hqadmin/deploy_history',
-            startkey=[environment, {}],
-            endkey=[environment],
-            reduce=False,
-            limit=limit,
-            descending=True,
-            include_docs=True
-        )
-        return result.all()
-
-    @classmethod
-    def get_list(cls, environment, startdate, enddate, limit=50):
-        return HqDeploy.view(
-            'hqadmin/deploy_history',
-            startkey=[environment, json_format_datetime(startdate)],
-            endkey=[environment, json_format_datetime(enddate)],
-            reduce=False,
-            limit=limit,
-            include_docs=False
-        ).all()
 
     def save(self, *args, **kwargs):
         # Save to SQL
