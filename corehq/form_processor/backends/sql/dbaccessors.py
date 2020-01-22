975fcca431cc4cc5ba1f2be7f9aeef393c9fa6d2
Simon Kelly <skelly@dimagi.com>
2020-01-22 16:58:46 +0200

    run queries directly on shard DB's

--- corehq/form_processor/backends/sql/dbaccessors.py
+++ corehq/form_processor/backends/sql/dbaccessors.py
@@ -470,7 +470,7 @@ class FormAccessorSQL(AbstractFormAccessor):
 
     @staticmethod
     def get_form_operations(form_id):
-        return list(XFormOperationSQL.objects.plproxy_raw('SELECT * from get_form_operations(%s)', [form_id]))
+        return list(XFormOperationSQL.objects.partitioned_query(form_id).filter(form_id=form_id))
 
     @staticmethod
     def get_forms_with_attachments_meta(form_ids, ordered=False):
@@ -506,10 +506,11 @@ class FormAccessorSQL(AbstractFormAccessor):
 
     @staticmethod
     def form_exists(form_id, domain=None):
-        with XFormInstanceSQL.get_plproxy_cursor() as cursor:
-            cursor.execute('SELECT * FROM check_form_exists(%s, %s)', [form_id, domain])
-            result = fetchone_as_namedtuple(cursor)
-            return result.form_exists
+        query = XFormInstanceSQL.objects.partitioned_query(form_id).filter(form_id=form_id)
+        if domain:
+            query = query.filter(domain=domain)
+
+        return query.exists()
 
     @staticmethod
     def hard_delete_forms(domain, form_ids, delete_attachments=True):
@@ -808,9 +809,8 @@ class CaseAccessorSQL(AbstractCaseAccessor):
 
     @staticmethod
     def get_indices(domain, case_id):
-        return list(CommCareCaseIndexSQL.objects.plproxy_raw(
-            'SELECT * FROM get_case_indices(%s, %s)', [domain, case_id]
-        ))
+        query = CommCareCaseIndexSQL.objects.partitioned_query(case_id)
+        return list(query.filter(case_id=case_id, domain=domain))
 
     @staticmethod
     def get_reverse_indices(domain, case_id):
@@ -922,11 +922,11 @@ class CaseAccessorSQL(AbstractCaseAccessor):
 
     @staticmethod
     def get_attachments(case_id):
-        return list(CaseAttachmentSQL.objects.plproxy_raw('SELECT * from get_case_attachments(%s)', [case_id]))
+        return list(CaseAttachmentSQL.objects.partitioned_query(case_id).filter(case_id=case_id))
 
     @staticmethod
     def get_transactions(case_id):
-        return list(CaseTransaction.objects.plproxy_raw('SELECT * from get_case_transactions(%s)', [case_id]))
+        return list(CaseTransaction.objects.partitioned_query(case_id).filter(case_id=case_id))
 
     @staticmethod
     def get_transaction_by_form_id(case_id, form_id):
