5cf4c58c82ffcbea8442c9698f200c12148e1b6d
Jennifer Schweers <orange.jenny@gmail.com>
2020-01-24 14:09:08 -0500

    Renamed SQLDhis2Connection to Dhis2Connection

--- corehq/apps/cleanup/management/commands/populate_sql_model_from_couch_model.py
+++ corehq/apps/cleanup/management/commands/populate_sql_model_from_couch_model.py
@@ -14,6 +14,8 @@ logger = logging.getLogger(__name__)
 class PopulateSQLCommand(BaseCommand):
     """
         Base class for migrating couch docs to sql models.
+        Adds a SQL object for any couch doc that doesn't yet have one.
+        Override all methods that raise NotImplementedError and, optionoally, couch_db_slug.
     """
 
     def add_arguments(self, parser):
