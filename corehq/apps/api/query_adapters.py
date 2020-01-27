df20f72c63e2d477d9f12c45f0bc183c04701bec
Daniel Roberts <droberts@dimagi.com>
2020-01-27 13:19:16 -0600

    Remove temporary USER_API_USE_COUCH_BACKEND flag

--- corehq/apps/api/query_adapters.py
+++ corehq/apps/api/query_adapters.py
@@ -1,38 +1,7 @@
-from corehq.apps.domain.dbaccessors import get_doc_count_in_domain_by_class, \
-    get_docs_in_domain_by_class
 from corehq.apps.es import GroupES, UserES
 from corehq.apps.groups.models import Group
-from corehq.apps.users.analytics import (
-    get_active_commcare_users_in_domain,
-    get_count_of_active_commcare_users_in_domain,
-    get_count_of_inactive_commcare_users_in_domain,
-    get_inactive_commcare_users_in_domain,
-)
 from corehq.apps.users.models import CommCareUser
 from dimagi.utils.chunked import chunked
-
-
-class UserQuerySetAdapterCouch(object):
-
-    def __init__(self, domain, show_archived):
-        self.domain = domain
-        self.show_archived = show_archived
-
-    def count(self):
-        if self.show_archived:
-            return get_count_of_inactive_commcare_users_in_domain(self.domain)
-        else:
-            return get_count_of_active_commcare_users_in_domain(self.domain)
-
-    def __getitem__(self, item):
-        if isinstance(item, slice):
-            limit = item.stop - item.start
-            if self.show_archived:
-                return get_inactive_commcare_users_in_domain(self.domain, start_at=item.start, limit=limit)
-            else:
-                return get_active_commcare_users_in_domain(self.domain, start_at=item.start, limit=limit)
-        raise ValueError(
-            'Invalid type of argument. Item should be an instance of slice class.')
 
 
 class UserQuerySetAdapterES(object):
