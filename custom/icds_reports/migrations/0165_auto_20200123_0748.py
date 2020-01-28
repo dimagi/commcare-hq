9a2c44dcbb4007acca4cc9e24dccb829a86ba1a0
Rohit Negi <rohit25.negi@gmail.com>
2020-01-28 10:51:52 +0530

    Resolved migration conflict

--- custom/icds_reports/migrations/0165_auto_20200123_0748.py
+++ custom/icds_reports/migrations/0165_auto_20200123_0748.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0163_update_agg_awc_monthly_view'),
+        ('icds_reports', '0164_auto_20200123_0702'),
     ]
 
     operations = [
