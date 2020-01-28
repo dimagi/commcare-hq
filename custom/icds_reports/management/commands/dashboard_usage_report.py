e2753c5a67046d744e16c65025d6776338ff3d40
challarao <challabphc@gmail.com>
2020-01-29 00:05:28 +0530

    used dashboard usage directly

--- custom/icds_reports/management/commands/dashboard_usage_report.py
+++ custom/icds_reports/management/commands/dashboard_usage_report.py
@@ -12,11 +12,12 @@ from django.db.models import Count, IntegerField
 from dimagi.utils.chunked import chunked
 from django.db.models.functions import Cast
 
+from corehq.apps.users.models import CouchUser
 from corehq.util.argparse_types import date_type
 from corehq.util.log import with_progress_bar
 from custom.icds_reports.const import THR_REPORT_EXPORT
 from custom.icds_reports.models import ICDSAuditEntryRecord, AwcLocation
-from custom.icds_reports.models.aggregate import DashboardUserActivityReport
+from custom.icds_reports.sqldata.exports.dashboard_usage import DashBoardUsage
 
 prefix = 'dashboard_usage_data_'
 REQUEST_DATA_CACHE = f'{prefix}tabular_data.csv'
@@ -95,165 +96,84 @@ class Command(BaseCommand):
             help='The end date (exclusive). format YYYY-MM-DD'
         )
         parser.add_argument('domain')
+        parser.add_argument('email', help='The username of the logged in user')
 
-    def get_user_vs_state_mapping_and_usernames(self, user_data):
-        """
-        :param user_data: Dashboard activity records
-        :return: username vs state map and username vs location ids map
-        """
-        user_state_map = defaultdict(str)
-        user_location_dict = defaultdict(list)
 
-        for record in user_data:
-            if record['state_id'] not in user_state_map:
-                user_state_map[record['username']] = record['state_id']
-            if record['username'] not in user_location_dict:
-                user_location_dict[record['username']] = [record['state_id'], record['district_id'],
-                                                          record['block_id'], record['user_level']]
-        return user_state_map, user_location_dict
-
-    def handle(self, start_date, end_date, domain, **options):
+    def handle(self, start_date, end_date, domain, username, **options):
         print(f'fetching users data')
 
-        user_data = list()
-        date = datetime.now()
-        # fetching the last available dashboard activity report
-        while not user_data:
-            user_data = self.get_activity_report_data(date)
-            date -= relativedelta(days=1)
-        # fetching username vs state_id map for cas export data and username vs location ids map
-        user_state_map, usernames_location_map = self.get_user_vs_state_mapping_and_usernames(user_data)
+        user = CouchUser.get_by_username(username)
+        usage_data = DashBoardUsage(couch_user=user, domain=domain).get_excel_data()[0][1]
+
 
         # fetching dashboard usage counts
-        tab_usage_counts, tab_indicators_count, cas_counts =\
-            self.get_dashboard_usage_counts(start_date, end_date, domain, usernames_location_map.keys(),
-                                            user_state_map)
-        location_type_filter = {
-            'aggregation_level': 5
-        }
-        for test_location in self.location_test_fields:
-            location_type_filter[test_location] = 0
-        all_awc_locations = AwcLocation.objects.filter(**location_type_filter).values(*self.required_fields)
+        tab_usage_counts, tab_indicators_count, cas_counts = \
+            self.get_dashboard_usage_counts(start_date, end_date, domain)
 
-        # fetching location_is vs name mapping since dashboard activity table stores only location ids
-        location_mapping = self.get_location_id_vs_name_mapping_from_queryset(all_awc_locations)
-
-        self.get_tabular_data(usernames_location_map.keys(), tab_usage_counts, tab_indicators_count,
-                              location_mapping, usernames_location_map)
+        username_state_mapping = self.get_tabular_data(usage_data, tab_usage_counts, tab_indicators_count)
         print(f'Request data written to file {REQUEST_DATA_CACHE}')
-        self.get_cas_data(cas_counts)
+        self.get_cas_data(cas_counts, username_state_mapping)
         print(f'Request data written to file {CAS_DATA_CACHE}')
 
-    def get_location_id_vs_name_mapping_from_queryset(self, results_queryset):
-        """
-        :param results_queryset: AwcLocation values queryset
-        :return: location id to name mapping dict
-        """
-        location_id_name_mapping = defaultdict(str)
-
-        for result in results_queryset:
-            for location_type in self.location_types:
-                if result[location_type] not in location_id_name_mapping.keys():
-                    location_id_name_mapping[result[location_type]] =\
-                        result[self.get_location_name_string_from_location_id(location_type)]
-        return location_id_name_mapping
-
-    def get_dashboard_usage_counts(self, start_date, end_date, domain, usernames, user_state_map):
+    def get_dashboard_usage_counts(self, start_date, end_date, domain):
         """
         :param start_date: start date of the filter
         :param end_date: end date of the filter
         :param domain
-        :param usernames: list of usernames
-        :param user_state_map: username vs state_id map
         :return: returns the counts of no of downloads of each and total reports for  all usernames
         """
-        print(f'Compiling usage counts for {len(usernames)} users')
+        print(f'Compiling usage counts for users')
         tabular_user_counts = defaultdict(int)
         cas_user_counts = defaultdict(int)
         tabular_user_indicators = defaultdict(lambda: [0] * 10)
 
         urls = ['/a/' + domain + '/cas_export', '/a/' + domain + '/icds_export_indicator']
-        for chunk in chunked(with_progress_bar(usernames), 500):
-            query = (
-                    ICDSAuditEntryRecord.objects.filter(url__in=urls,
-                                                        username__in=chunk, time_of_use__gte=start_date,
-                                                        time_of_use__lte=end_date)
-                    .annotate(indicator=Cast(KeyTextTransform('indicator', 'post_data'), IntegerField()))
-                        .filter(indicator__lte=THR_REPORT_EXPORT).values('indicator', 'username', 'url')
-                    .annotate(count=Count('indicator')).order_by('username', 'indicator'))
-            for record in query:
-                if record['url'] == urls[0]:
-                    cas_user_counts[user_state_map[record['username']]] += record['count']
-                else:
-                    tabular_user_counts[record['username']] += record['count']
-                    tabular_user_indicators[record['username']][int(record['indicator']) - 1] = record['count']
+        query = (
+                ICDSAuditEntryRecord.objects.filter(url__in=urls, time_of_use__gte=start_date,
+                                                    time_of_use__lte=end_date)
+                .annotate(indicator=Cast(KeyTextTransform('indicator', 'post_data'), IntegerField()))
+                    .filter(indicator__lte=THR_REPORT_EXPORT).values('indicator', 'username', 'url')
+                .annotate(count=Count('indicator')).order_by('username', 'indicator'))
+        for record in query:
+            if record['url'] == urls[0]:
+                cas_user_counts[record['username']] += record['count']
+            else:
+                tabular_user_counts[record['username']] += record['count']
+                tabular_user_indicators[record['username']][int(record['indicator']) - 1] = record['count']
 
         return tabular_user_counts, tabular_user_indicators, cas_user_counts
 
-    def get_role_from_username(self, username):
-        """
-        :param username:
-        :return: Role of the user bases on his username
-        """
-        for key, value in self.roles.items():
-            if key.lower() in username:
-                return value
-        return 'N/A'
 
-    def get_activity_report_data(self, date):
-        """
-        :param date: Date for which the report is needed
-        :return:Returns a list of activity report records based on the filters
-        """
-        return list(DashboardUserActivityReport.objects.filter(date=date).values('username', 'state_id',
-                                                                                 'district_id', 'block_id',
-                                                                                 'user_level'))
-
-    def get_location_name_string_from_location_id(self, location_id):
-        return location_id.replace('_id', '_name')
-
-    def get_tabular_data(self, usernames, tab_total_counts, tab_indicators_count, location_mapping,
-                         user_location_mapping):
-        sheet_headers = ['Sr.No', 'State/UT Name', 'District Name', 'Block Name', 'Username', 'Level',
-                         'Role', 'Total', 'Child', 'Pregnant Women', 'Demographics', 'System Usage',
-                         'AWC infrastructure', 'Child Growth Monitoring List', 'ICDS - CAS Monthly Register',
-                         'AWW Performance Report', 'LS Performance Report', 'Take Home Ration']
+    def get_tabular_data(self, usage_data, tab_total_counts, tab_indicators_count):
         tab_data = list()
-
-        print(f'Compiling request data for {len(usernames)} users')
-        for chunk in chunked(with_progress_bar(usernames), 500):
-            for username in chunk:
+        username_vs_state_name = defaultdict()
+        print(f'Compiling request data for {len(usage_data)} users')
+        for chunk in chunked(with_progress_bar(usage_data), 500):
+            for data in chunk:
+                username = data[4]
                 indicator_count = tab_indicators_count[username]
-
-                location_details = user_location_mapping[username]
-
-                for index, value in enumerate(location_details):
-                    if value != 'All' and index < 3:
-                        location_details[index] = location_mapping[value]
-
-                csv_row = [0, location_details[0], location_details[1],
-                           location_details[2], username.split('@')[0],
-                           self.user_level_list[location_details[3] - 1],
-                           self.get_role_from_username(username), tab_total_counts[username]]
-
+                csv_row = data[:7]
+                csv_row.append(tab_total_counts[username])
                 csv_row.extend(indicator_count)
                 tab_data.append(csv_row)
-
-        tab_data = sorted(tab_data, key=lambda x: (x[1], x[2], x[3]))
-        for index, data in enumerate(tab_data):
-            data[0] += (index + 1)
-        tab_data.insert(0, sheet_headers)
+                if username not in username_vs_state_name:
+                    username_vs_state_name[username] = data[1]
         _write_to_file(REQUEST_DATA_CACHE, tab_data)
+        return username_vs_state_name
 
-    def get_cas_data(self, cas_total_counts):
+    def get_cas_data(self, cas_total_counts, username_state_mapping):
         sheet_headers = ['Sr.No', 'State/UT Name',
                          'No. of times CAS data export downloaded (December 2019)']
         cas_data = list()
-        # constructing the cas export data
-        serial = 0
+        cas_data_dict = defaultdict(int)
+        # converting usernames to state names
         for key, value in cas_total_counts.items():
+            if username_state_mapping[key] not in cas_data_dict:
+                cas_data_dict[username_state_mapping[key]] += value
+        # creating cas data
+        serial = 0
+        for key, value in cas_data_dict.items():
             serial += 1
             cas_data.append([serial, key, value])
-
         cas_data.insert(0, sheet_headers)
         _write_to_file(CAS_DATA_CACHE, cas_data)
