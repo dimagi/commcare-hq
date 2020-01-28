a6531e71cfd0894af72fc4a016dcceb2ad40454d
Rohit Negi <rohit25.negi@gmail.com>
2020-01-28 11:21:31 +0530

    Removed duplicate imports

--- custom/icds_reports/utils/aggregation_helpers/distributed/agg_gov_dashboard.py
+++ custom/icds_reports/utils/aggregation_helpers/distributed/agg_gov_dashboard.py
@@ -1,6 +1,3 @@
-from dateutil.relativedelta import relativedelta
-
-from corehq.apps.userreports.util import get_table_name
 from custom.icds_reports.const import AGG_GOV_DASHBOARD_TABLE
 from custom.icds_reports.utils.aggregation_helpers import  month_formatter
 from custom.icds_reports.utils.aggregation_helpers.distributed.base import AggregationPartitionedHelper
