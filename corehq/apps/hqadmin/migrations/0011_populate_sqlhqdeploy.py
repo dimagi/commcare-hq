07d45afd451abeb4c9145ef7561b01a760d72e84
Jennifer Schweers <orange.jenny@gmail.com>
2020-01-31 15:09:42 -0500

    Updated 0011_populate_sqlhqdeploy to reduce boilerplate
    See https://github.com/dimagi/commcare-hq/pull/26492/

--- corehq/apps/hqadmin/migrations/0011_populate_sqlhqdeploy.py
+++ corehq/apps/hqadmin/migrations/0011_populate_sqlhqdeploy.py
@@ -1,54 +1,6 @@
-import sys
-import traceback
-
-from django.core.management import call_command
 from django.db import migrations
 
-from corehq.apps.hqadmin.models import SQLHqDeploy
-from corehq.dbaccessors.couchapps.all_docs import get_doc_ids_by_class
-from corehq.util.django_migrations import skip_on_fresh_install
-
-
-AUTO_MIGRATE_ITEMS_LIMIT = 1000
-AUTO_MIGRATE_FAILED_MESSAGE = """
-    A migration must be performed before this environment can be upgraded to the latest version of CommCareHQ.
-    This migration is run using the management command populate_sql_hq_deploy.
-"""
-
-
-def count_items_to_be_migrated():
-    try:
-        from corehq.apps.hqadmin.models import HqDeploy
-    except ImportError:
-        return 0
-    couch_ids = set(get_doc_ids_by_class(HqDeploy))
-    sql_couch_ids = set([o.couch_id for o in SQLHqDeploy.objects.filter(couch_id__isnull=False)])
-    return len(couch_ids.difference(sql_couch_ids))
-
-
-@skip_on_fresh_install
-def _verify_sql_hq_deploy(apps, schema_editor):
-    to_migrate = count_items_to_be_migrated()
-    migrated = to_migrate == 0
-    if migrated:
-        return
-
-    if to_migrate < AUTO_MIGRATE_ITEMS_LIMIT:
-        try:
-            call_command('populate_sql_hq_deploy')
-            migrated = count_items_to_be_migrated() == 0
-            if not migrated:
-                print("Automatic migration failed")
-        except Exception:
-            traceback.print_exc()
-    else:
-        print("Found {} items that need to be migrated.".format(to_migrate))
-        print("Too many to migrate automatically.")
-
-    if not migrated:
-        print("")
-        print(AUTO_MIGRATE_FAILED_MESSAGE)
-        sys.exit(1)
+from corehq.apps.hqadmin.management.commands.populate_sql_hq_deploy import Command
 
 
 class Migration(migrations.Migration):
@@ -58,7 +10,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(_verify_sql_hq_deploy,
+        migrations.RunPython(Command.migrate_from_migration,
                              reverse_code=migrations.RunPython.noop,
                              elidable=True),
     ]
