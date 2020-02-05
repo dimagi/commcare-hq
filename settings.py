1c0800bc508efe9d00b7a5e1087d57978dc44cfc
Sravan Reddy <sravfeyn@gmail.com>
2020-02-05 19:06:40 +0530

    configurable es timeout

--- settings.py
+++ settings.py
@@ -752,6 +752,8 @@ SUMOLOGIC_URL = None
 ELASTICSEARCH_HOST = 'localhost'
 ELASTICSEARCH_PORT = 9200
 ELASTICSEARCH_MAJOR_VERSION = 1
+# If elasticsearch queries take more than this, they result in timeout errors
+ES_SEARCH_TIMEOUT = 30
 
 BITLY_LOGIN = ''
 BITLY_APIKEY = ''
