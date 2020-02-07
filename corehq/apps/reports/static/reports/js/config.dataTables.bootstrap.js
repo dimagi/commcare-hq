5f8121bf92a76d125b18401c5e58eb24f9666dc4
Biyeun Buczyk <biyeun@dimagi.com>
2020-02-07 14:32:19 -0600

    pr feedback

--- corehq/apps/reports/static/reports/js/config.dataTables.bootstrap.js
+++ corehq/apps/reports/static/reports/js/config.dataTables.bootstrap.js
@@ -137,7 +137,7 @@ hqDefine("reports/js/config.dataTables.bootstrap", [
                     params.fnServerData = function (sSource, aoData, fnCallback, oSettings) {
                         var custom_callback = function (data) {
                             if (data.warning) {
-                                throw Error(data.warning);
+                                throw new Error(data.warning);
                             }
                             var result = fnCallback(data); // this must be called first because datatables clears the tfoot of the table
                             var i;
