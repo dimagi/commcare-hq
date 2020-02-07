acd75bdc1318cc6ed03db5d9099748c92197cd42
Shubham Bansal <shubhambansal17@hotmail.com>
2020-02-07 17:14:42 +0530

    [fix]: added extra params
    - Github Issue:

--- custom/icds_reports/utils/state_level.sql
+++ custom/icds_reports/utils/state_level.sql
@@ -1,5 +1,7 @@
 SELECT
      state_name,
+     SUM(num_launched_districts),
+     SUM(num_launched_blocks),
      SUM(num_launched_awcs),
      SUM(num_launched_supervisors)
  FROM 'agg_awc_daily_view' GROUP BY state_id WHERE date='2020-02-03' AND aggregation_level=1;
