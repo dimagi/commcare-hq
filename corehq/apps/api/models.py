403228131de6b4e2a9cdbaa212d260c2c463ed44
Jennifer Schweers <orange.jenny@gmail.com>
2020-02-07 22:18:14 -0500

    Removed ApiUser

--- corehq/apps/api/models.py
+++ corehq/apps/api/models.py
@@ -88,62 +88,6 @@ class SQLApiUser(models.Model):
             return False
 
 
-class ApiUser(Document):
-    password = StringProperty()
-    permissions = ListProperty(StringProperty)
-
-    @property
-    def username(self):
-        if self['_id'].startswith("ApiUser-"):
-            return self['_id'][len("ApiUser-"):]
-        else:
-            raise Exception("ApiUser _id has to be 'ApiUser-' + username")
-
-    def set_password(self, raw_password):
-        salt = os.urandom(5).encode('hex')
-        self.password = make_password(raw_password, salt=salt)
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
-    def has_permission(self, permission):
-        return permission in self.permissions
-
-    @classmethod
-    def create(cls, username, password, permissions=None):
-        """
-        To create a new ApiUser on the server:
-        ./manage.py shell
-
-        $ from corehq.apps.api.models import *
-        $ ApiUser.create('buildserver', 'RANDOM').save()
-
-        """
-        self = cls()
-        self['_id'] = "ApiUser-%s" % username
-        self.set_password(password)
-        self.permissions = permissions or []
-        return self
-
-    @classmethod
-    def get_user(cls, username):
-        return cls.get("ApiUser-%s" % username)
-
-    @classmethod
-    def auth(cls, username, password, permission=None):
-        try:
-            user = cls.get_user(username)
-            if user.check_password(password):
-                if permission is not None:
-                    return user.has_permission(permission)
-                else:
-                    return True
-            else:
-                return False
-        except ResourceNotFound:
-            return False
-
-
 def _require_api_user(permission=None):
     def _outer2(fn):
         from django.views.decorators.http import require_POST
