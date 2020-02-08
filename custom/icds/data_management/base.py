0b3aa958c4ac796a324b5d644dba8427e4286435
Manish Kangia <mkangia@dimagi.com>
2020-02-09 00:30:14 +0530

    dedup

--- custom/icds/data_management/base.py
+++ custom/icds/data_management/base.py
@@ -1,4 +1,3 @@
-from corehq.apps.es import CaseES
 from corehq.form_processor.backends.sql.dbaccessors import CaseReindexAccessor
 from corehq.util.doc_processor.interface import BulkDocProcessor
 from corehq.util.doc_processor.sql import SqlDocumentProvider
@@ -8,6 +7,7 @@ class DataManagement(object):
     slug = ""
     name = ""
     case_type = None
+    doc_processor = None
 
     def __init__(self, domain, *args, **kwargs):
         self.domain = domain
@@ -15,10 +15,19 @@ class DataManagement(object):
     def run(self, iteration_key):
         raise NotImplementedError
 
+    def case_accessor(self):
+        raise NotImplementedError
+
+    def run(self, iteration_key):
+        """
+        iterate sql records and update them as and when needed
+        """
+        record_provider = SqlDocumentProvider(iteration_key, self.case_accessor())
+        processor = BulkDocProcessor(record_provider, self.doc_processor(self.domain))
+        processor.run()
+
 
 class SQLBasedDataManagement(DataManagement):
-    doc_processor = None
-
     def __init__(self, domain, db_alias, from_date=None, till_date=None):
         super().__init__(domain)
         self.db_aliases = [db_alias] if db_alias else None
@@ -34,18 +43,8 @@ class SQLBasedDataManagement(DataManagement):
             end_date=self.till_date
         )
 
-    def run(self, iteration_key):
-        """
-        iterate sql records and update them as and when needed
-        """
-        record_provider = SqlDocumentProvider(iteration_key, self.case_accessor())
-        processor = BulkDocProcessor(record_provider, self.doc_processor(self.domain))
-        processor.run()
-
 
 class ESBasedDataManagement(DataManagement):
-    doc_processor = None
-
     def _get_case_ids(self):
         raise NotImplementedError
 
@@ -55,11 +54,3 @@ class ESBasedDataManagement(DataManagement):
             case_type=self.case_type,
             case_ids=self._get_case_ids()
         )
-
-    def run(self, iteration_key):
-        """
-        find records to be updated via ES and then update them
-        """
-        record_provider = SqlDocumentProvider(iteration_key, self.case_accessor())
-        processor = BulkDocProcessor(record_provider, self.doc_processor(self.domain))
-        processor.run()
--- custom/icds/data_management/doc_processors.py
+++ custom/icds/data_management/doc_processors.py
@@ -20,24 +20,33 @@ HAS_MOBILE_PROPERTY = "has_mobile"
 HAS_MOBILE_PROPERTY_NO_VALUE = "no"
 
 
-class PopulateMissingMotherNameDocProcessor(BaseDocProcessor):
+class DataManagementDocProcessor(BaseDocProcessor):
     def __init__(self, domain):
         self.domain = domain
-        date_today = date.today()
-        self.cut_off_dob = str(date_today.replace(year=date_today.year - CUT_OFF_AGE_IN_YEARS))
-        self.test_location_ids = find_test_awc_location_ids(self.domain)
-        self.case_accessor = CaseAccessors(self.domain)
 
     @staticmethod
-    def _create_case_blocks(updates):
+    def _create_case_blocks(updates, case_property):
         case_blocks = []
-        for case_id, mother_name in updates.items():
+        for case_id, new_value in updates.items():
             case_block = CaseBlock(case_id,
-                                   update={MOTHER_NAME_PROPERTY: mother_name},
+                                   update={case_property: new_value},
                                    user_id=SYSTEM_USER_ID)
             case_block = ElementTree.tostring(case_block.as_xml()).decode('utf-8')
             case_blocks.append(case_block)
         return case_blocks
+
+    def handle_skip(self, doc):
+        print('Unable to process case {}'.format(doc['_id']))
+        return True
+
+
+class PopulateMissingMotherNameDocProcessor(DataManagementDocProcessor):
+    def __init__(self, domain):
+        super().__init__(domain)
+        date_today = date.today()
+        self.cut_off_dob = str(date_today.replace(year=date_today.year - CUT_OFF_AGE_IN_YEARS))
+        self.test_location_ids = find_test_awc_location_ids(self.domain)
+        self.case_accessor = CaseAccessors(self.domain)
 
     def process_bulk_docs(self, docs):
         updates = {}
@@ -53,11 +62,8 @@ class PopulateMissingMotherNameDocProcessor(BaseDocProcessor):
                 else:
                     updates[case_id] = mother_case.name
         if updates:
-            submit_case_blocks(self._create_case_blocks(updates), self.domain, user_id=SYSTEM_USER_ID)
-        return True
-
-    def handle_skip(self, doc):
-        print('Unable to process case {}'.format(doc['_id']))
+            submit_case_blocks(self._create_case_blocks(updates, MOTHER_NAME_PROPERTY),
+                               self.domain, user_id=SYSTEM_USER_ID)
         return True
 
     def should_process(self, doc):
@@ -70,30 +76,16 @@ class PopulateMissingMotherNameDocProcessor(BaseDocProcessor):
         return False
 
 
-class SanitizePhoneNumberDocProcessor(BaseDocProcessor):
+class SanitizePhoneNumberDocProcessor(DataManagementDocProcessor):
     def __init__(self, domain):
-        self.domain = domain
+        super().__init__(domain)
         self.test_location_ids = find_test_awc_location_ids(self.domain)
-
-    @staticmethod
-    def _create_case_blocks(docs):
-        case_blocks = []
-        for doc in docs:
-            case_id = doc['_id']
-            case_block = CaseBlock(case_id,
-                                   update={PHONE_NUMBER_PROPERTY: ''},
-                                   user_id=SYSTEM_USER_ID)
-            case_block = ElementTree.tostring(case_block.as_xml()).decode('utf-8')
-            case_blocks.append(case_block)
-        return case_blocks
 
     def process_bulk_docs(self, docs):
         if docs:
-            submit_case_blocks(self._create_case_blocks(docs), self.domain, user_id=SYSTEM_USER_ID)
-        return True
-
-    def handle_skip(self, doc):
-        print('Unable to process case {}'.format(doc['_id']))
+            updates = {doc['_id']: '' for doc in docs}
+            submit_case_blocks(self._create_case_blocks(updates, PHONE_NUMBER_PROPERTY),
+                               self.domain, user_id=SYSTEM_USER_ID)
         return True
 
     def should_process(self, doc):
@@ -105,21 +97,10 @@ class SanitizePhoneNumberDocProcessor(BaseDocProcessor):
         return False
 
 
-class ResetMissingCaseNameDocProcessor(BaseDocProcessor):
+class ResetMissingCaseNameDocProcessor(DataManagementDocProcessor):
     def __init__(self, domain):
-        self.domain = domain
+        super().__init__(domain)
         self.case_accessor = CaseAccessors(self.domain)
-
-    @staticmethod
-    def _create_case_blocks(updates):
-        case_blocks = []
-        for case_id, name in updates.items():
-            case_block = CaseBlock(case_id,
-                                   update={'name': name},
-                                   user_id=SYSTEM_USER_ID)
-            case_block = ElementTree.tostring(case_block.as_xml()).decode('utf-8')
-            case_blocks.append(case_block)
-        return case_blocks
 
     def process_bulk_docs(self, docs):
         updates = {}
@@ -130,11 +111,7 @@ class ResetMissingCaseNameDocProcessor(BaseDocProcessor):
                 update_to_name = get_last_non_blank_value(case, 'name')
                 updates[case_id] = update_to_name
         if updates:
-            submit_case_blocks(self._create_case_blocks(updates), self.domain, user_id=SYSTEM_USER_ID)
-        return True
-
-    def handle_skip(self, doc):
-        print('Unable to process case {}'.format(doc['_id']))
+            submit_case_blocks(self._create_case_blocks(updates, 'name'), self.domain, user_id=SYSTEM_USER_ID)
         return True
 
     def should_process(self, doc):
