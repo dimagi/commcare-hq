e6ce38e74cc7d856b3a2caa7005898b114d20433
Manish Kangia <mkangia@dimagi.com>
2020-02-10 02:55:59 +0530

    fix arg passing

--- custom/icds/data_management/tasks.py
+++ custom/icds/data_management/tasks.py
@@ -6,5 +6,6 @@ from custom.icds.data_management.models import DataManagementRequest
 @task(queue='background_queue')
 def execute_data_management(slug, domain, db_alias, initiated_by, from_date, till_date):
     DataManagementRequest(
-        slug, domain, db_alias, initiated_by, from_date, till_date
+        slug=slug, domain=domain, db_alias=db_alias, initiated_by=initiated_by,
+        from_date=from_date, till_date=till_date
     ).execute()
