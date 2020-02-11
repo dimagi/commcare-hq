3167b753eda3c38827bad680b2f3a7b7f7128253
Shubham Bansal <shubhambansal17@hotmail.com>
2020-02-11 22:45:35 +0530

    [sql]: added the query for fixing the past data which changed due to height eligibility
    - Github Issue:

---
+++ custom/icds_reports/utils/fix_past_data_due_to_change_in_height_eligibilty.sql
@@ -0,0 +1,99 @@
+-- child health table fix
+UPDATE child_health_monthy
+   SET
+      height_measured_in_month = tmp.height_measured_in_month, 
+      current_month_stunting = tmp.current_month_stunting,
+      stunting_last_recorded = tmp.stunting_last_recorded,
+      wasting_last_recorded = tmp.wasting_last_recorded
+      current_month_wasting = tmp.current_month_wasting
+   FROM (
+      SELECT
+         chm.case_id as case_id,
+         chm.valid_in_month as valid_in_month,
+         chm.age_tranche as age_tranche,
+         CASE
+            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+         ELSE
+            chm.height_measured_in_month
+         END as height_measured_in_month,
+         CASE
+            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+         ELSE
+            chm.current_month_stunting
+         END as current_month_stunting,
+         CASE
+            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+         ELSE
+            chm.stunting_last_recorded
+         END as stunting_last_recorded,
+         CASE
+            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+         ELSE
+            chm.wasting_last_recorded
+         END as wasting_last_recorded,
+         CASE
+            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+         ELSE
+            chm.current_month_wasting
+         END as current_month_wasting,
+         CASE
+            WHEN WHEN date_trunc('MONTH', gm.height_child_last_recorded) = %(start_date)s AND (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN 1
+         ELSE
+            0
+         END as height_measured_in_month
+         FROM child_health_monthly chm
+         LEFT OUTER JOIN "icds_dashboard_growth_monitoring_forms" gm ON child_health.doc_id = gm.case_id
+                     AND gm.month = %(start_date)s
+              AND child_health.state_id = gm.state_id
+              AND child_health.supervisor_id = gm.supervisor_id
+         ORDER BY child_health.supervisor_id, child_health.awc_id
+) as tmp
+WHERE case_id = tmp.case_id
+
+-- aggegrate child health table fix
+UPDATE agg_child_health
+ SET weighed_and_height_measured_in_month = tmp.weighed_and_height_measured_in_month,
+       height_measured_in_month = tmp.height_measured_in_month,
+       stunting_normal = tmp.stunting_normal,
+       stunting_severe = tmp.stunting_severe,
+       stunting_moderate = tmp.stunting_moderate
+       wasting_moderate = tmp.wasting_moderate,
+       wasting_severe = tmp.wasting_severe,
+       wasting_normal = tmp.wasting_normal
+   FROM (
+      SELECT
+         awc_loc.state_id as state_id,
+         awc_loc.district_id as district_id,
+         awc_loc.block_id as block_id,
+         chm.supervisor_id as supervisor_id,
+         chm.awc_id as awc_id,
+         chm.month as month,
+         SUM(CASE WHEN chm.current_month_wasting = 'moderate' THEN 1 ELSE 0 END) as wasting_moderate,
+         SUM(CASE WHEN chm.current_month_wasting = 'severe' THEN 1 ELSE 0 END) as wasting_severe,
+         SUM(CASE WHEN chm.current_month_wasting = 'normal' THEN 1 ELSE 0 END) as wasting_normal,
+         SUM(CASE WHEN chm.current_month_stunting = 'moderate' THEN 1 ELSE 0 END) as stunting_moderate,
+         SUM(CASE WHEN chm.current_month_stunting = 'severe' THEN 1 ELSE 0 END) as stunting_severe,
+         SUM(CASE WHEN chm.current_month_stunting = 'normal' THEN 1 ELSE 0 END) as stunting_normal,
+         SUM(chm.height_measured_in_month) as height_measured_in_month,
+         SUM(CASE WHEN chm.nutrition_status_weighed = 1 AND chm.height_measured_in_month = 1 THEN 1 ELSE 0 END) as weighed_and_height_measured_in_month
+
+      FROM "child_health_monthly" chm
+      LEFT OUTER JOIN "awc_location" awc_loc ON (
+        awc_loc.supervisor_id = chm.supervisor_id AND awc_loc.doc_id = chm.awc_id
+      )
+      WHERE chm.month = %(start_date)s
+            AND awc_loc.state_id != ''
+            AND awc_loc.state_id IS NOT NULL
+            AND chm.supervisor_id >= '0'
+            AND chm.supervisor_id < '1'
+      GROUP BY awc_loc.state_id, awc_loc.district_id, awc_loc.block_id, chm.supervisor_id, chm.awc_id,
+               chm.month, chm.sex, chm.age_tranche, chm.caste,
+               coalesce_disabled, coalesce_minority, coalesce_resident; 
+   ) tmp
+   WHERE state_id = tmp.state_id
+      AND district_id = tmp.district_id
+      AND block_id = tmp.block_id
+      AND supervisor_id = tmp.supervisor_id
+      AND awc_id = tmp.awc_id
+      AND month = tmp.month
+
