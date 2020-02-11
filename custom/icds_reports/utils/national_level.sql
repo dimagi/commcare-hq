7272adbdda48a6f0f0f2d27bd5ed616321714c28
Shubham <shubhambansal17@hotmail.com>
2020-02-11 23:13:27 +0530

    Delete national_level.sql

--- custom/icds_reports/utils/national_level.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    num_launched_states,
-    num_launched_districts,
-    num_launched_blocks,
-    num_launched_awcs,
-    num_launched_supervisors
-FROM 'agg_awc_daily_view' WHERE date='2020-02-03' AND aggregation_level=1;
