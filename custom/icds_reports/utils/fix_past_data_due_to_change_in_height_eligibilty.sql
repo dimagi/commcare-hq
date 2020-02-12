218d34b9c9196cc3f813a12545520b8835aade61
Shubham Bansal <shubhambansal17@hotmail.com>
2020-02-12 11:30:05 +0530

    [fix]: minor typo fixes
    - Github Issue:

--- custom/icds_reports/utils/fix_past_data_due_to_change_in_height_eligibilty.sql
+++ custom/icds_reports/utils/fix_past_data_due_to_change_in_height_eligibilty.sql
@@ -1,10 +1,10 @@
 -- child health table fix
-UPDATE child_health_monthy
+UPDATE child_health_monthly
    SET
       height_measured_in_month = tmp.height_measured_in_month, 
       current_month_stunting = tmp.current_month_stunting,
       stunting_last_recorded = tmp.stunting_last_recorded,
-      wasting_last_recorded = tmp.wasting_last_recorded
+      wasting_last_recorded = tmp.wasting_last_recorded,
       current_month_wasting = tmp.current_month_wasting
    FROM (
       SELECT
@@ -12,41 +12,36 @@ UPDATE child_health_monthy
          chm.valid_in_month as valid_in_month,
          chm.age_tranche as age_tranche,
          CASE
-            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
-         ELSE
-            chm.height_measured_in_month
-         END as height_measured_in_month,
-         CASE
-            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+            WHEN NOT (chm.valid_in_month::boolean AND chm.age_tranche::Integer <= 60) THEN NULL
          ELSE
             chm.current_month_stunting
          END as current_month_stunting,
          CASE
-            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+            WHEN NOT (chm.valid_in_month::boolean AND chm.age_tranche::Integer <= 60) THEN NULL
          ELSE
             chm.stunting_last_recorded
          END as stunting_last_recorded,
          CASE
-            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+            WHEN NOT (chm.valid_in_month::boolean AND chm.age_tranche::Integer <= 60) THEN NULL
          ELSE
             chm.wasting_last_recorded
          END as wasting_last_recorded,
          CASE
-            WHEN NOT (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN NULL
+            WHEN NOT (chm.valid_in_month::boolean AND chm.age_tranche::Integer <= 60) THEN NULL
          ELSE
             chm.current_month_wasting
          END as current_month_wasting,
          CASE
-            WHEN WHEN date_trunc('MONTH', gm.height_child_last_recorded) = %(start_date)s AND (chm.valid_in_month AND chm.age_tranche::Integer <= 60) THEN 1
+            WHEN date_trunc('MONTH', gm.height_child_last_recorded) = '2018-06-01' AND (chm.valid_in_month::boolean AND chm.age_tranche::Integer <= 60) THEN 1
          ELSE
             0
          END as height_measured_in_month
          FROM child_health_monthly chm
-         LEFT OUTER JOIN "icds_dashboard_growth_monitoring_forms" gm ON child_health.doc_id = gm.case_id
-                     AND gm.month = %(start_date)s
-              AND child_health.state_id = gm.state_id
-              AND child_health.supervisor_id = gm.supervisor_id
-         ORDER BY child_health.supervisor_id, child_health.awc_id
+         LEFT OUTER JOIN icds_dashboard_growth_monitoring_forms gm ON chm.case_id = gm.case_id
+                     AND gm.month = '2018-06-01'
+              AND chm.state_id = gm.state_id
+              AND chm.supervisor_id = gm.supervisor_id
+         ORDER BY chm.supervisor_id, chm.awc_id
 ) as tmp
 WHERE case_id = tmp.case_id
 
@@ -56,7 +51,7 @@ UPDATE agg_child_health
        height_measured_in_month = tmp.height_measured_in_month,
        stunting_normal = tmp.stunting_normal,
        stunting_severe = tmp.stunting_severe,
-       stunting_moderate = tmp.stunting_moderate
+       stunting_moderate = tmp.stunting_moderate,
        wasting_moderate = tmp.wasting_moderate,
        wasting_severe = tmp.wasting_severe,
        wasting_normal = tmp.wasting_normal
@@ -81,15 +76,14 @@ UPDATE agg_child_health
       LEFT OUTER JOIN "awc_location" awc_loc ON (
         awc_loc.supervisor_id = chm.supervisor_id AND awc_loc.doc_id = chm.awc_id
       )
-      WHERE chm.month = %(start_date)s
+      WHERE chm.month = '2018-06-01'
             AND awc_loc.state_id != ''
             AND awc_loc.state_id IS NOT NULL
             AND chm.supervisor_id >= '0'
             AND chm.supervisor_id < '1'
       GROUP BY awc_loc.state_id, awc_loc.district_id, awc_loc.block_id, chm.supervisor_id, chm.awc_id,
-               chm.month, chm.sex, chm.age_tranche, chm.caste,
-               coalesce_disabled, coalesce_minority, coalesce_resident; 
-   ) tmp
+               chm.month, chm.sex, chm.age_tranche, chm.caste
+   ) as tmp
    WHERE state_id = tmp.state_id
       AND district_id = tmp.district_id
       AND block_id = tmp.block_id
