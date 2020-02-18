75d2fb5530a4685019ebf108f62013ae9c4b7294
Shubham Bansal <shubhambansal17@hotmail.com>
2020-02-18 14:01:40 +0530

    [fix]: minor test fix in the test_nic_indicators
    - Github Issue:

--- custom/icds_reports/tests/agg_tests/test_nic_indicators_api.py
+++ custom/icds_reports/tests/agg_tests/test_nic_indicators_api.py
@@ -47,35 +47,39 @@ class NICIndicatorTest(TestCase):
         )
 
     def test_mwcd_indicators_data(self):
-        self.maxDiff = None
         data = get_mwcd_indicator_api_data(date(2017, 4, 1))
-        print(data)
 
         self.assertCountEqual([
-            {'state_name': 'ALL INDIA', 'state_id': '0', 'num_launched_awcs': 21,
-             'num_launched_districts': 3, 'num_launched_states': 2, 'num_ls_launched': 6,
-             'awc_with_gm_devices': 25, 'cases_household': 2798, 'total_mothers': 321,
-             'cases_child_health': 1286},
-            {'state_name': 'st2', 'state_id': 'st2', 'num_launched_awcs': 11,
-             'num_launched_districts': 2, 'num_launched_states': 1, 'num_ls_launched': 2,
-             'awc_with_gm_devices': 11, 'cases_household': 1476, 'total_mothers': 164, 'cases_child_health': 668},
-            {'state_name': 'st1', 'state_id': 'st1', 'num_launched_awcs': 10,
-             'num_launched_districts': 1, 'num_launched_states': 1, 'num_ls_launched': 4,
-             'awc_with_gm_devices': 14, 'cases_household': 1322, 'total_mothers': 157, 'cases_child_health': 618}],
+            {
+                'state_name': 'ALL INDIA', 'state_id': '0', 'num_launched_awcs': 21, 'num_launched_districts': 3,
+                'num_launched_states': 2, 'num_ls_launched': 6, 'awc_with_gm_devices': 25, 'cases_household': 2798,
+                'total_mothers': 321, 'cases_child_health': 1287},
+            {
+                'state_name': 'st2', 'state_id': 'st2', 'num_launched_awcs': 11, 'num_launched_districts': 2,
+                'num_launched_states': 1, 'num_ls_launched': 2, 'awc_with_gm_devices': 11, 'cases_household': 1476,
+                'total_mothers': 164, 'cases_child_health': 669},
+            {
+                'state_name': 'st1', 'state_id': 'st1', 'num_launched_awcs': 10, 'num_launched_districts': 1,
+                'num_launched_states': 1, 'num_ls_launched': 4, 'awc_with_gm_devices': 14, 'cases_household': 1322,
+                'total_mothers': 157, 'cases_child_health': 618}],
             data['implementation_status']['dataarray'])
 
-        self.assertCountEqual([{'state_name': 'st1', 'state_id': 'st1', 'month': 4, 'total_mothers': 134,
-                                'num_launched_awcs': 10, 'cases_child_health': 609, 'year': 2017},
-                               {'state_name': 'st2', 'state_id': 'st2', 'month': 4, 'total_mothers': 129,
-                                'num_launched_awcs': 11, 'cases_child_health': 652, 'year': 2017},
-                               {'state_name': 'st1', 'state_id': 'st1', 'month': 5, 'total_mothers': 157,
-                                'num_launched_awcs': 10, 'cases_child_health': 618, 'year': 2017},
-                               {'state_name': 'st2', 'state_id': 'st2', 'month': 5, 'total_mothers': 164,
-                                'num_launched_awcs': 11, 'cases_child_health': 668, 'year': 2017},
-                               {'month': 4, 'num_launched_awcs': 21, 'total_mothers': 263,
-                                'cases_child_health': 1261, 'state_name': 'ALL INDIA',
-                                'state_id': '0', 'year': 2017},
-                               {'month': 5, 'num_launched_awcs': 21, 'total_mothers': 321,
-                                'cases_child_health': 1286, 'state_name': 'ALL INDIA',
-                                'state_id': '0', 'year': 2017}],
-                              data['monthly_trend']['dataarray'])
+        self.assertCountEqual([
+            {
+                'state_name': 'st2', 'state_id': 'st2', 'month': 5, 'total_mothers': 164, 'num_launched_awcs': 11,
+                'cases_child_health': 669, 'year': 2017},
+            {
+                'state_name': 'st2', 'state_id': 'st2', 'month': 4, 'total_mothers': 129, 'num_launched_awcs': 11,
+                'cases_child_health': 653, 'year': 2017},
+            {
+                'state_name': 'st1', 'state_id': 'st1', 'month': 5,
+                'total_mothers': 157, 'num_launched_awcs': 10, 'cases_child_health': 618, 'year': 2017},
+            {
+                'state_name': 'st1', 'state_id': 'st1', 'month': 4, 'total_mothers': 134, 'num_launched_awcs': 10,
+                'cases_child_health': 609, 'year': 2017},
+            {
+                'month': 4, 'num_launched_awcs': 21, 'total_mothers': 263,
+                'cases_child_health': 1262, 'state_name': 'ALL INDIA', 'state_id': '0', 'year': 2017},
+            {
+                'month': 5, 'num_launched_awcs': 21, 'total_mothers': 321, 'cases_child_health': 1287,
+                'state_name': 'ALL INDIA', 'state_id': '0', 'year': 2017}], data['monthly_trend']['dataarray'])
