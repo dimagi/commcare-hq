11b46f4a1a05baa4b5f6b26f566be9d2d35d7693
Sravan Reddy <sravfeyn@gmail.com>
2020-02-20 12:26:01 +0530

    more options

--- corehq/apps/es/management/commands/initialize_xforms_index_without_all.py
+++ corehq/apps/es/management/commands/initialize_xforms_index_without_all.py
@@ -15,47 +15,67 @@ class Command(BaseCommand):
         parser.add_argument('index_name')
         parser.add_argument('number_of_shards', type=int)
         parser.add_argument('number_of_replicas', type=int)
+        parser.add_argument(
+            '-c',
+            '--create-index',
+            action='store_true',
+            default=False,
+            help="Create and initialize index but don't reindex",
+        )
+        parser.add_argument(
+            '-r',
+            '--do-reindex',
+            action='store_true',
+            default=False,
+            help="Run reindex"
+        )
 
     def handle(self, index_name, number_of_shards, number_of_replicas, **options):
-        mapping = copy.deepcopy(XFORM_MAPPING)
-        mapping["_all"] = {"enabled": False}
+        create_index = options.get('create_index')
+        do_reindex = options.get('do_reindex')
 
-        shard_settings = {
-            'number_of_shards': number_of_shards, 'number_of_replicas': number_of_replicas
-        }
-        if settings.ES_SETTINGS and 'xforms' in settings.ES_SETTINGS:
-            settings.ES_SETTINGS['xforms'].update(shard_settings)
-        elif settings.ES_SETTINGS:
-            settings.ES_SETTINGS['xforms'] = shard_settings
-        else:
-            settings.ES_SETTINGS = {'xforms': shard_settings}
+        print(create_index, do_reindex)
+        if create_index:
+            mapping = copy.deepcopy(XFORM_MAPPING)
+            mapping["_all"] = {"enabled": False}
 
-        es = get_es_new()
-        index_info = ElasticsearchIndexInfo(
-            index=index_name,
-            type=XFORM_ES_TYPE,
-            mapping=mapping,
-        )
-        initialize_index_and_mapping(es, index_info)
+            shard_settings = {
+                'number_of_shards': number_of_shards, 'number_of_replicas': number_of_replicas
+            }
+            if settings.ES_SETTINGS and 'xforms' in settings.ES_SETTINGS:
+                settings.ES_SETTINGS['xforms'].update(shard_settings)
+            elif settings.ES_SETTINGS:
+                settings.ES_SETTINGS['xforms'] = shard_settings
+            else:
+                settings.ES_SETTINGS = {'xforms': shard_settings}
 
-        # tune settings for indexing speed
-        es.indices.put_settings(
-            index=index_name,
-            body='''{
-                "index": {
-                    "refresh_interval": -1,
-                    "number_of_replicas": 0,
-                    "translog.durability": "async"
-                }
-            }'''
-        )
+            es = get_es_new()
+            index_info = ElasticsearchIndexInfo(
+                index=index_name,
+                type=XFORM_ES_TYPE,
+                mapping=mapping,
+            )
+            initialize_index_and_mapping(es, index_info)
 
-        result = es.reindex({
-            "source": {
-                "index": XFORM_INDEX,
-                "_source": {"exclude": ["_id"]}
-            },
-            "dest": {"index": index_name}
-        }, wait_for_completion=False, request_timeout=300)
-        print(result)
-        print("""Check for status 'curl -X GET "http://host:port/_tasks/?pretty&detailed=true&actions=*reindex"'""")
+        if do_reindex:
+            # tune settings for indexing speed
+            es.indices.put_settings(
+                index=index_name,
+                body='''{
+                    "index": {
+                        "refresh_interval": -1,
+                        "number_of_replicas": 0,
+                        "translog.durability": "async"
+                    }
+                }'''
+            )
+
+            result = es.reindex({
+                "source": {
+                    "index": XFORM_INDEX,
+                    "_source": {"exclude": ["_id"]}
+                },
+                "dest": {"index": index_name}
+            }, wait_for_completion=False, request_timeout=300)
+            print(result)
+            print("""Check for status 'curl -X GET "http://host:port/_tasks/?pretty&detailed=true&actions=*reindex"'""")
