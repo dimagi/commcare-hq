d591208c9abe5e9be8a990d60d70a2fb938ed799
Jennifer Schweers <orange.jenny@gmail.com>
2020-02-21 14:24:17 -0500

    Updated Content to read from sql (PR2)

--- corehq/apps/translations/models.py
+++ corehq/apps/translations/models.py
@@ -40,6 +40,13 @@ class SMSTranslations(SyncSQLToCouchMixin, models.Model):
         else:
             return None
 
+    @property
+    def default_lang(self):
+        if len(self.langs) > 0:
+            return self.langs[0]
+        else:
+            return None
+
     @classmethod
     def _migration_get_fields(cls):
         return [
