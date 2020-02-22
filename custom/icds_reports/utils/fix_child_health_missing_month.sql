77378590a5a1226af94aabd54acd682db0b1256a
Shubham Bansal <shubhambansal17@hotmail.com>
2020-02-22 17:33:53 +0530

    [refactor]: added the roll up queries as well
    - Github Issue:

--- custom/icds_reports/utils/fix_child_health_missing_month.sql
+++ custom/icds_reports/utils/fix_child_health_missing_month.sql
@@ -55,44 +55,195 @@ UPDATE child_health_monthly child_health
 --                            Index Cond: (month = '2018-05-01'::date)
 --                      ->  Index Scan using child_health_monthly_default_102648_pkey on child_health_monthly_default_102648 child_health_1  (cost=0.56..2.78 rows=1 width=525)
 --                            Index Cond: ((supervisor_id = gm.supervisor_id) AND (case_id = (gm.case_id)::text) AND (month = '2018-05-01'::date))
--- Second query to update agg_child_table
 
-UPDATE agg_child_health 
- SET wasting_moderate = tmp.wasting_moderate,
- 		 wasting_severe = tmp.wasting_severe,
- 		 wasting_normal = tmp.wasting_normal,
- 		 wasting_normal_v2 = tmp.wasting_normal_v2,
- 		 wasting_moderate_v2 = tmp.wasting_moderate_v2,
- 		 wasting_severe_v2 = tmp.wasting_severe_v2,
- 		 stunting_moderate = tmp.stunting_moderate,
- 		 stunting_severe = tmp.stunting_severe,
- 		 stunting_normal = tmp.stunting_normal
- 	FROM (
- 		SELECT
- 			chm.supervisor_id as supervisor_id,
- 			chm.awc_id as awc_id,
- 			chm.month as month,
- 			SUM(CASE WHEN chm.current_month_wasting = 'moderate' THEN 1 ELSE 0 END) as wasting_moderate,
- 			SUM(CASE WHEN chm.current_month_wasting = 'severe' THEN 1 ELSE 0 END) as wasting_severe,
- 			SUM(CASE WHEN chm.current_month_stunting = 'moderate' THEN 1 ELSE 0 END) as stunting_moderate,
- 			SUM(CASE WHEN chm.current_month_stunting = 'severe' THEN 1 ELSE 0 END) as stunting_severe,
- 			SUM(CASE WHEN chm.current_month_wasting = 'normal' THEN 1 ELSE 0 END) as wasting_normal,
- 			SUM(CASE WHEN chm.current_month_stunting = 'normal' THEN 1 ELSE 0 END) as stunting_normal,
- 			SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 3 THEN 1 ELSE 0 END) as wasting_normal_v2,
- 			SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 2 THEN 1 ELSE 0 END) as wasting_moderate_v2,
- 			SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 1 THEN 1 ELSE 0 END) as wasting_severe_v2
- 		FROM child_health_monthly chm
-      WHERE chm.month = '2018-05-01'
-            AND chm.supervisor_id >= '0'
-            AND chm.supervisor_id < '1'
-      GROUP BY chm.supervisor_id, chm.awc_id,
-               chm.month, chm.sex, chm.age_tranche, chm.caste 
- 	) as tmp
- 	WHERE supervisor_id = tmp.supervisor_id
- 		AND awc_id = tmp.awc_id
- 		AND month = tmp.month
- 		AND aggregation_level = 5
+DROP TABLE IF EXISTS temp_agg_child_my;
+CREATE TABLE temp_agg_child_my AS
+SELECT
+	awc_id,
+	chm.month,
+	sex,
+	age_tranche,
+	caste,
+	SUM(CASE WHEN chm.current_month_wasting = 'moderate' THEN 1 ELSE 0 END) as wasting_moderate,
+	SUM(CASE WHEN chm.current_month_wasting = 'severe' THEN 1 ELSE 0 END) as wasting_severe,
+	SUM(CASE WHEN chm.current_month_stunting = 'moderate' THEN 1 ELSE 0 END) as stunting_moderate,
+	SUM(CASE WHEN chm.current_month_stunting = 'severe' THEN 1 ELSE 0 END) as stunting_severe,
+	SUM(CASE WHEN chm.current_month_wasting = 'normal' THEN 1 ELSE 0 END) as wasting_normal,
+	SUM(CASE WHEN chm.current_month_stunting = 'normal' THEN 1 ELSE 0 END) as stunting_normal,
+	SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 3 THEN 1 ELSE 0 END) as wasting_normal_v2,
+	SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 2 THEN 1 ELSE 0 END) as wasting_moderate_v2,
+	SUM(CASE WHEN chm.zscore_grading_wfh_recorded_in_month = 1 AND chm.zscore_grading_wfh = 1 THEN 1 ELSE 0 END) as wasting_severe_v2,
+	COALESCE(chm.disabled, 'no') as coalesce_disabled,
+	COALESCE(chm.minority, 'no') as coalesce_minority,
+	COALESCE(chm.resident, 'no') as coalesce_resident
 
+	FROM
+	"child_health_monthly" chm
+	WHERE chm.month='2018-05-01'
+	GROUP BY chm.awc_id,
+					 chm.month, chm.sex, chm.age_tranche, chm.caste,
+					 coalesce_disabled, coalesce_minority, coalesce_resident
+	ORDER BY chm.awc_id;
+
+
+
+UPDATE "agg_child_health_2018-05-01_5" agg_child_health
+    SET wasting_moderate = ut.wasting_moderate,
+        wasting_severe = ut.wasting_severe,
+        stunting_moderate = ut.stunting_severe,
+        stunting_severe = ut.stunting_severe,
+        wasting_normal = ut.wasting_normal,
+        stunting_normal = ut.stunting_normal,
+        wasting_normal_v2 = ut.wasting_normal_v2,
+        wasting_moderate_v2 = ut.wasting_moderate_v2,
+        wasting_severe_v2 = ut.wasting_severe_v2
+    from (
+        SELECT * from temp_agg_Child_my
+    ) ut 
+    where (
+        agg_child_health.awc_id=ut.awc_id and 
+        agg_child_health.month=ut.month and
+        agg_child_health.gender=ut.sex and
+        agg_child_health.age_tranche=ut.age_tranche and
+        agg_child_health.caste=ut.caste and
+        agg_child_health.disabled=ut.coalesce_disabled and
+        agg_child_health.minority = ut.coalesce_minority and
+        agg_child_health.resident = ut.coalesce_resident
+            );
+
+Roll ups:
+
+UPDATE "agg_child_health_2018-05-01_4" agg_child_health
+    SET wasting_moderate = ut.wasting_moderate,
+        wasting_severe = ut.wasting_severe,
+        stunting_moderate = ut.stunting_severe,
+        stunting_severe = ut.stunting_severe,
+        wasting_normal = ut.wasting_normal,
+        stunting_normal = ut.stunting_normal,
+        wasting_normal_v2 = ut.wasting_normal_v2,
+        wasting_moderate_v2 = ut.wasting_moderate_v2,
+        wasting_severe_v2 = ut.wasting_severe_v2
+    from (
+        SELECT
+        supervisor_id,
+        gender,
+        age_tranche,
+        SUM(wasting_moderate) as wasting_moderate,
+        SUM(wasting_severe) as wasting_severe,
+        SUM(stunting_moderate) as stunting_moderate,
+        SUM(stunting_severe) as stunting_severe,
+        SUM(wasting_normal) as wasting_normal,
+        SUM(stunting_normal) as stunting_normal,
+        SUM(wasting_normal_v2) as wasting_normal_v2,
+        SUM(wasting_moderate_v2) as wasting_moderate_v2,
+        SUM(wasting_severe_v2) as wasting_severe_v2
+
+        FROM "agg_child_health_2018-05-01_5" agg_child INNER JOIN (SELECT DISTINCT ucr.doc_id FROM "ucr_icds-cas_static-awc_location_88b3f9c3" ucr WHERE ucr.awc_is_test=0) tt ON tt.doc_id = agg_child.awc_id
+        GROUP BY state_id, district_id,block_id,supervisor_id, gender, age_tranche
+    ) ut 
+    WHERE agg_child_health.supervisor_id = ut.supervisor_id and 
+      agg_child_health.gender = ut.gender AND
+      agg_child_health.age_tranche = ut.age_tranche;
+
+
+UPDATE "agg_child_health_2018-05-01_3" agg_child_health
+    SET wasting_moderate = ut.wasting_moderate,
+        wasting_severe = ut.wasting_severe,
+        stunting_moderate = ut.stunting_severe,
+        stunting_severe = ut.stunting_severe,
+        wasting_normal = ut.wasting_normal,
+        stunting_normal = ut.stunting_normal,
+        wasting_normal_v2 = ut.wasting_normal_v2,
+        wasting_moderate_v2 = ut.wasting_moderate_v2,
+        wasting_severe_v2 = ut.wasting_severe_v2
+    from (
+        SELECT
+        block_id,
+        gender,
+        age_tranche,
+        SUM(wasting_moderate) as wasting_moderate,
+        SUM(wasting_severe) as wasting_severe,
+        SUM(stunting_moderate) as stunting_moderate,
+        SUM(stunting_severe) as stunting_severe,
+        SUM(wasting_normal) as wasting_normal,
+        SUM(stunting_normal) as stunting_normal,
+        SUM(wasting_normal_v2) as wasting_normal_v2,
+        SUM(wasting_moderate_v2) as wasting_moderate_v2,
+        SUM(wasting_severe_v2) as wasting_severe_v2
+
+        FROM "agg_child_health_2018-05-01_4" agg_child INNER JOIN (SELECT DISTINCT ucr.supervisor_id FROM "ucr_icds-cas_static-awc_location_88b3f9c3" ucr WHERE ucr.supervisor_is_test=0) tt ON tt.supervisor_id = agg_child.supervisor_id
+        GROUP BY state_id, district_id,block_id, gender, age_tranche
+    ) ut 
+    WHERE agg_child_health.block_id = ut.block_id and 
+      agg_child_health.gender = ut.gender AND
+      agg_child_health.age_tranche = ut.age_tranche;
+
+
+
+UPDATE "agg_child_health_2018-05-01_2" agg_child_health
+    SET wasting_moderate = ut.wasting_moderate,
+        wasting_severe = ut.wasting_severe,
+        stunting_moderate = ut.stunting_severe,
+        stunting_severe = ut.stunting_severe,
+        wasting_normal = ut.wasting_normal,
+        stunting_normal = ut.stunting_normal,
+        wasting_normal_v2 = ut.wasting_normal_v2,
+        wasting_moderate_v2 = ut.wasting_moderate_v2,
+        wasting_severe_v2 = ut.wasting_severe_v2
+    from (
+        SELECT
+        district_id,
+        gender,
+        age_tranche,
+        SUM(wasting_moderate) as wasting_moderate,
+        SUM(wasting_severe) as wasting_severe,
+        SUM(stunting_moderate) as stunting_moderate,
+        SUM(stunting_severe) as stunting_severe,
+        SUM(wasting_normal) as wasting_normal,
+        SUM(stunting_normal) as stunting_normal,
+        SUM(wasting_normal_v2) as wasting_normal_v2,
+        SUM(wasting_moderate_v2) as wasting_moderate_v2,
+        SUM(wasting_severe_v2) as wasting_severe_v2
+
+        FROM "agg_child_health_2018-05-01_3" agg_child INNER JOIN (SELECT DISTINCT ucr.block_id FROM "ucr_icds-cas_static-awc_location_88b3f9c3" ucr WHERE ucr.block_is_test=0) tt ON tt.block_id = agg_child.block_id
+        GROUP BY state_id, district_id,gender, age_tranche
+    ) ut 
+    WHERE agg_child_health.district_id = ut.district_id and 
+      agg_child_health.gender = ut.gender AND
+      agg_child_health.age_tranche = ut.age_tranche;
+
+
+UPDATE "agg_child_health_2018-05-01_1" agg_child_health
+    SET wasting_moderate = ut.wasting_moderate,
+        wasting_severe = ut.wasting_severe,
+        stunting_moderate = ut.stunting_severe,
+        stunting_severe = ut.stunting_severe,
+        wasting_normal = ut.wasting_normal,
+        stunting_normal = ut.stunting_normal,
+        wasting_normal_v2 = ut.wasting_normal_v2,
+        wasting_moderate_v2 = ut.wasting_moderate_v2,
+        wasting_severe_v2 = ut.wasting_severe_v2
+    from (
+        SELECT
+        state_id,
+        gender,
+        age_tranche,
+        SUM(wasting_moderate) as wasting_moderate,
+        SUM(wasting_severe) as wasting_severe,
+        SUM(stunting_moderate) as stunting_moderate,
+        SUM(stunting_severe) as stunting_severe,
+        SUM(wasting_normal) as wasting_normal,
+        SUM(stunting_normal) as stunting_normal,
+        SUM(wasting_normal_v2) as wasting_normal_v2,
+        SUM(wasting_moderate_v2) as wasting_moderate_v2,
+        SUM(wasting_severe_v2) as wasting_severe_v2
+
+        FROM "agg_child_health_2018-05-01_2" agg_child INNER JOIN (SELECT DISTINCT ucr.district_id FROM "ucr_icds-cas_static-awc_location_88b3f9c3" ucr WHERE ucr.block_is_test=0) tt ON tt.district_id = agg_child.district_id
+        GROUP BY state_id, district_id,gender, age_tranche
+    ) ut 
+    WHERE agg_child_health.state_id = ut.state_id and 
+      agg_child_health.gender = ut.gender AND
+      agg_child_health.age_tranche = ut.age_tranche;
 
 
 
