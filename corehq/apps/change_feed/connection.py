33c25ec9cc34261c9dfe4c8ecf41f1483d8fa1d9
Simon Kelly <skelly@dimagi.com>
2020-02-27 19:46:49 +0200

    get topics from consumer

--- corehq/apps/change_feed/connection.py
+++ corehq/apps/change_feed/connection.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 
-from kafka.client import KafkaClient, SimpleClient
-
 from corehq.util.io import ClosingContextProxy
+from kafka import KafkaConsumer
+from kafka.client import KafkaClient, SimpleClient
 
 GENERIC_KAFKA_CLIENT_ID = 'cchq-kafka-client'
 
@@ -22,3 +22,11 @@ def get_kafka_client(client_id=GENERIC_KAFKA_CLIENT_ID):
         client_id=client_id,
         api_version=settings.KAFKA_API_VERSION
     )
+
+
+def get_kafka_consumer():
+    return ClosingContextProxy(KafkaConsumer(
+        client_id='pillowtop_utils',
+        bootstrap_servers=settings.KAFKA_BROKERS,
+        request_timeout_ms=1000
+    ))
