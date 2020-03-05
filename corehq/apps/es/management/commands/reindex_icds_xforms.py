51589b25a21b688902f323fec16d392e218f46b1
Sravan Reddy <sravfeyn@gmail.com>
2020-03-05 10:33:56 +0530

    remove resume logic since scroll doesn't respect sort

--- corehq/apps/es/management/commands/reindex_icds_xforms.py
+++ corehq/apps/es/management/commands/reindex_icds_xforms.py
@@ -40,7 +40,6 @@ class Command(BaseCommand):
 
         print("Total number of docs in this date range in old index",
             old_docs_count)
-        start_date = self._get_last_start_date(es, index_name, start_date, end_date)
         query = self._base_query(start_date, end_date)
         query.update({
             "_source": {"exclude": ["_id"]}
@@ -54,12 +53,11 @@ class Command(BaseCommand):
 
     def _base_query(self, start_date, end_date):
         return copy.deepcopy({
-            "sort": {"received_on": {"order": "asc"}},
             "query": {
                 "range": {
                     "received_on": {
                         "gte": start_date,
-                        "lte": end_date,
+                        "lt": end_date,
                         "format": "yyyy-MM-dd"
                     }
                 }
@@ -70,30 +68,3 @@ class Command(BaseCommand):
         query = self._base_query(start_date, end_date)
         ret = es.count(index, body=query)
         return ret['count']
-
-    def _get_last_start_date(self, es, index_name, start_date, end_date):
-        query = self._base_query(start_date, end_date)
-        query.update({
-            "sort": {"received_on": {"order": "desc"}},
-            "_source": ["received_on"],
-            "from": 0,
-            "size": 1
-        })
-        result = es.search(index_name, body=query)
-        hits = result['hits']['hits']
-        if not hits:
-            print("Starting fresh! No docs in new index in this date range")
-            return start_date
-        else:
-            new_start_date = str(iso_string_to_datetime(hits[0]['_source']['received_on']).date() - timedelta(1))
-            print("Trying to resume from ", new_start_date)
-            already_index_count = self._get_doc_count(es, index_name, start_date, new_start_date)
-            expected_count = self._get_doc_count(es, 'xforms', start_date, new_start_date)
-            print(already_index_count, " docs already reindexed. ", "Expected count ", expected_count)
-            if already_index_count == expected_count:
-                print(already_index_count, " docs are already indexed ")
-                print("Resuming from date ", new_start_date)
-                return new_start_date
-            else:
-                print("Doc counts don't match, starting for all range")
-                return start_date
