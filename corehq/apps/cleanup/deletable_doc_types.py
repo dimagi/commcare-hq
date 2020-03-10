9385d1be378c33460ce60b1803b9295ae2cfad76
Jennifer Schweers <orange.jenny@gmail.com>
2020-03-10 17:58:08 -0400

    Added InvoicePdf to DELETABLE_COUCH_DOC_TYPES (PR3)

--- corehq/apps/cleanup/deletable_doc_types.py
+++ corehq/apps/cleanup/deletable_doc_types.py
@@ -28,6 +28,7 @@ DELETABLE_COUCH_DOC_TYPES = {
     'HqDeploy': (MAIN_DB,),
     'ILSGatewayConfig': (MAIN_DB,),
     'Invitation': (settings.USERS_GROUPS_DB,),
+    'InvoicePdf': (MAIN_DB,),
     'RegistrationRequest': (MAIN_DB,),
     'StandaloneTranslationDoc': (MAIN_DB,),
     'WisePillDeviceEvent': (MAIN_DB,),
