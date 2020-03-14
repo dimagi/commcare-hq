3cc01cbd169541637617ade7f6b91b843e7b17bb
Daniel Roberts <droberts@dimagi.com>
2020-03-14 18:31:34 -0500

    Fix usage of Password*View class-based views

--- corehq/apps/domain/urls.py
+++ corehq/apps/domain/urls.py
@@ -82,7 +82,7 @@ from corehq.motech.repeaters.views import (
 
 PASSWORD_RESET_KWARGS = {
     'template_name': 'login_and_password/password_reset_form.html',
-    'password_reset_form': ConfidentialPasswordResetForm,
+    'form_class': ConfidentialPasswordResetForm,
     'from_email': settings.DEFAULT_FROM_EMAIL,
     'extra_context': {'current_page': {'page_name': _('Password Reset')}}
 }
@@ -100,27 +100,29 @@ urlpatterns = [
         ActivateTransferDomainView.as_view(), name='activate_transfer_domain'),
     url(r'^domain/transfer/(?P<guid>\w+)/deactivate$',
         DeactivateTransferDomainView.as_view(), name='deactivate_transfer_domain'),
-    url(r'^accounts/password_change/$', PasswordChangeView.as_view(),
-        {'template_name': 'login_and_password/password_change_form.html'},
+    url(r'^accounts/password_change/$',
+        PasswordChangeView.as_view(
+            template_name='login_and_password/password_change_form.html'),
         name='password_change'),
-    url(r'^accounts/password_change_done/$', PasswordChangeDoneView.as_view(),
-        {'template_name': 'login_and_password/password_change_done.html',
-         'extra_context': {'current_page': {'page_name': _('Password Change Complete')}}},
+    url(r'^accounts/password_change_done/$',
+        PasswordChangeDoneView.as_view(
+            template_name='login_and_password/password_change_done.html',
+            extra_context={'current_page': {'page_name': _('Password Change Complete')}}),
         name='password_change_done'),
-
-    url(r'^accounts/password_reset_email/$', PasswordResetView.as_view(),
-        PASSWORD_RESET_KWARGS, name='password_reset_email'),
-    url(r'^accounts/password_reset_email/done/$', PasswordResetDoneView.as_view(),
-        PASSWORD_RESET_DONE_KWARGS, name='password_reset_done'),
+    url(r'^accounts/password_reset_email/$',
+        PasswordResetView.as_view(**PASSWORD_RESET_KWARGS), name='password_reset_email'),
+    url(r'^accounts/password_reset_email/done/$',
+        PasswordResetDoneView.as_view(**PASSWORD_RESET_DONE_KWARGS),
+        name='password_reset_done'),
     url(r'^accounts/password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
         CustomPasswordResetView.as_view(),
         {'template_name': 'login_and_password/password_reset_confirm.html', 'set_password_form': HQSetPasswordForm,
          'extra_context': {'current_page': {'page_name': _('Password Reset Confirmation')}}},
         name=CustomPasswordResetView.urlname),
-    url(r'^accounts/password_reset_confirm/done/$', PasswordResetCompleteView.as_view(),
-        {'template_name': 'login_and_password/password_reset_complete.html',
-         'extra_context': {'current_page': {'page_name': _('Password Reset Complete')}}},
-        name='password_reset_complete')
+    url(r'^accounts/password_reset_confirm/done/$', PasswordResetCompleteView.as_view(
+        template_name='login_and_password/password_reset_complete.html',
+        extra_context={'current_page': {'page_name': _('Password Reset Complete')}}),
+        name='password_reset_complete'),
 ]
 
 domain_settings = [
