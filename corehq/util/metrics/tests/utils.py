e00b7e176bb3c296f003d49d7e39354e5f3b1390
Simon Kelly <skelly@dimagi.com>
2020-03-16 13:40:39 +0200

    remove unnecessary list

--- corehq/util/metrics/tests/utils.py
+++ corehq/util/metrics/tests/utils.py
@@ -9,7 +9,7 @@ def patch_datadog():
     def record(fn, name, value, tags=None):
         key = tuple([
             name,
-            tuple(sorted([tuple(t.split(':')) for t in (tags or [])])),
+            tuple(sorted(tuple(t.split(':')) for t in (tags or []))),
         ])
         stats[key].append(value)
 
