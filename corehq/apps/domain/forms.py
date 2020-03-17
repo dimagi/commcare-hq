6ccbde00933b8b3a9a550dcc99cc2af36e71758a
Amit Phulera <amitphulera999@gmail.com>
2020-03-17 18:02:26 +0530

    whitespace removed

--- corehq/apps/domain/forms.py
+++ corehq/apps/domain/forms.py
@@ -289,7 +289,7 @@ class CallCenterOwnerWidget(Select2Ajax):
 
     def render(self, name, value, attrs=None, renderer=None):
         value_to_render = CallCenterOptionsController.convert_owner_id_to_select_choice(value, self.domain)
-        return super(CallCenterOwnerWidget, self).render(name, value_to_render, attrs=attrs, renderer=renderer )
+        return super(CallCenterOwnerWidget, self).render(name, value_to_render, attrs=attrs, renderer=renderer)
 
 
 class DomainGlobalSettingsForm(forms.Form):
