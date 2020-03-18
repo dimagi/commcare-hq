eba6735704b2c17f42e9aa36f6d357a9d0a3df8c
Manish Kangia <mkangia@dimagi.com>
2020-03-18 15:13:44 +0530

    correct usage

--- custom/icds/location_rationalization/views.py
+++ custom/icds/location_rationalization/views.py
@@ -70,7 +70,7 @@ class ValidateView(BaseLocationRationalizationView):
 
     @cached_property
     def _location_types(self):
-        location_types = LocationType.objects.by_domain(self.domain)
+        location_types = [lt.code for lt in LocationType.objects.by_domain(self.domain)]
         location_types.reverse()
         return location_types
 
