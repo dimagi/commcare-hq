d1f21cadf274fdf6c4602d81135317859ac4a769
Jennifer Schweers <orange.jenny@gmail.com>
2020-03-17 20:35:02 -0400

    Moved old upload code into new upload view

--- custom/icds/location_rationalization/forms.py
+++ custom/icds/location_rationalization/forms.py
@@ -10,42 +10,6 @@ from corehq.apps.locations.models import LocationType
 from corehq.util.workbook_json.excel import get_workbook
 
 
-class LocationRationalizationValidateForm(forms.Form):
-    file = forms.FileField(label="", required=True,
-                           help_text=ugettext_lazy("Upload xlsx file"))
-
-    def __init__(self, *args, **kwargs):
-        self.location_types = kwargs.pop('location_types', None)
-        super(LocationRationalizationValidateForm, self).__init__(*args, **kwargs)
-        self.helper = HQFormHelper()
-        self.helper.form_method = 'post'
-        self.helper.layout = crispy.Layout(
-            crispy.Field(ugettext_lazy('file'), data_bind="value: file"),
-            StrictButton(ugettext_lazy('Upload'), css_class='btn-primary', type='submit'),
-        )
-
-    def clean_file(self):
-        uploaded_file = self.cleaned_data.get('file')
-        if not uploaded_file.name.endswith('.xlsx'):
-            raise forms.ValidationError(_('Unexpected file passed. Please upload xlsx file only.'))
-        self._validate_file(uploaded_file)
-        return uploaded_file
-
-    def _validate_file(self, uploaded_file):
-        # ensure mandatory columns in the excel sheet
-        workbook = get_workbook(uploaded_file)
-        worksheet = workbook.worksheets[0]
-        headers = worksheet.fieldnames
-        expected_headers = []
-        for location_type in self.location_types:
-            expected_headers.extend([f'old_{location_type}', f'new_{location_type}'])
-        missing_headers = set(expected_headers) - set(headers)
-        if missing_headers:
-            raise forms.ValidationError(_("Missing following columns in sheet: {columns}").format(
-                columns=", ".join(missing_headers)
-            ))
-
-
 class LocationRationalizationTemplateForm(forms.Form):
     location_id = forms.CharField(label=ugettext_lazy("Location"), widget=forms.widgets.Select(choices=[]),
                                   required=True)
--- custom/icds/location_rationalization/views.py
+++ custom/icds/location_rationalization/views.py
@@ -16,13 +16,12 @@ from corehq.apps.locations.models import LocationType
 from corehq.apps.locations.permissions import require_can_edit_locations
 from corehq.apps.locations.views import LocationsListView
 from corehq.util.files import safe_filename_header
-from corehq.util.workbook_json.excel import get_workbook
+from corehq.util.workbook_json.excel import get_workbook, WorkbookJSONError
 from custom.icds.location_rationalization.download import (
     RequestTemplateDownload,
 )
 from custom.icds.location_rationalization.dumper import Dumper
 from custom.icds.location_rationalization.forms import (
-    LocationRationalizationValidateForm,
     LocationRationalizationTemplateForm,
 )
 from custom.icds.location_rationalization.parser import Parser
@@ -88,17 +87,32 @@ class ValidateView(BaseLocationRationalizationView):
         return LocationRationalizationValidateForm()
 
     def post(self, request, *args, **kwargs):
-        if self.form.is_valid():
-            transitions, errors = self._parse_upload()
-            [messages.error(request, error) for error in errors]
-            if not errors:
-                return self._generate_response(transitions)
+        try:
+            workbook = get_workbook(request.FILES['bulk_upload_file'])
+        except WorkbookJSONError as e:
+            messages.error(request, str(e))
+        else:
+            if self._workbook_is_valid(workbook):
+                transitions, errors = Parser(workbook.worksheets[0], self._location_types).parse()
+                [messages.error(request, error) for error in errors]
+                if not errors:
+                    return self._generate_response(transitions)
         return self.get(request, *args, **kwargs)
 
-    def _parse_upload(self):
-        uploaded_file = self.form.cleaned_data.get('file')
-        worksheet = get_workbook(uploaded_file).worksheets[0]
-        return Parser(worksheet, self._location_types).parse()
+    def _workbook_is_valid(self, workbook):
+        # ensure mandatory columns in the excel sheet
+        worksheet = workbook.worksheets[0]
+        headers = worksheet.fieldnames
+        expected_headers = []
+        for location_type in self._location_types:
+            expected_headers.extend([f'old_{location_type}', f'new_{location_type}'])
+        missing_headers = set(expected_headers) - set(headers)
+        if missing_headers:
+            messages.error(request, _("Missing following columns in sheet: {columns}").format(
+                columns=", ".join(missing_headers)
+            ))
+            return False
+        return True
 
     @cached_property
     def _location_types(self):
@@ -110,11 +124,19 @@ class ValidateView(BaseLocationRationalizationView):
         response_file = Dumper(self._location_types).dump(transitions)
         response_file.seek(0)
         response = HttpResponse(response_file, content_type="text/html; charset=utf-8")
-        filename = self.form.cleaned_data['file'].name.split('.xlsx')[0] + '-Processed'
+        filename = '%s Location Rationalization - Processed' % self.domain
         response['Content-Disposition'] = safe_filename_header(filename, 'xlsx')
         return response
 
 
+@toggles.LOCATION_RATIONALIZATION.required_decorator()
+@require_can_edit_locations
+@require_GET
+def download_location_rationalization(request, domain):
+    pass
+
+
+'''
 class DownloadTemplateView(BaseLocationRationalizationView):
     page_title = _('Download Template')
     urlname = 'download_location_rationalization_template'
