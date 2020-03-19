3039e9702dbe27dec2673f5b7f8ecdf557a600d8
Cory Zue <czue@dimagi.com>
2020-03-19 14:32:20 +0200

    delete inappropriate usage of a logging statement

--- corehq/apps/users/views/__init__.py
+++ corehq/apps/users/views/__init__.py
@@ -712,7 +712,6 @@ class UserInvitationView(object):
     template = "users/accept_invite.html"
 
     def __call__(self, request, invitation_id, **kwargs):
-        logging.info("Don't use this view in more apps until it gets cleaned up.")
         # add the correct parameters to this instance
         self.request = request
         self.inv_id = invitation_id
