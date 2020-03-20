fc3583e2007fb5a26372c5887bf7349cd5f147f2
Ethan Soergel <esoergel@gmail.com>
2020-03-20 16:33:38 -0400

    Split up tests by logical component

--- custom/inddex/tests/test_master_report.py
+++ custom/inddex/tests/test_master_report.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 from mock import patch
 
+from corehq.apps.domain.models import Domain
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.fixtures.dbaccessors import (
     count_fixture_items,
@@ -21,25 +22,50 @@ from ..food import FoodData, FoodRow
 from ..ucr.adapters import FoodCaseData
 from ..ucr.data_providers.master_data_file_data import MasterDataFileData
 
+DOMAIN = 'inddex-reports-test'
 
-class TestMasterReport(TestCase):
-    maxDiff = None
-    domain = 'inddex-reports-test'
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.domain_obj = create_domain(name=cls.domain)
+def setUpModule():
+    create_domain(name=DOMAIN)
+    try:
         with patch('corehq.apps.callcenter.data_source.get_call_center_domains', lambda: []):
-            populate_inddex_domain(cls.domain)
+            populate_inddex_domain(DOMAIN)
+    except Exception:
+        tearDownModule()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.domain_obj.delete()
-        super().tearDownClass()
 
+def tearDownModule():
+    Domain.get_by_name(DOMAIN).delete()
+
+
+def sort_rows(rows):
+    return sorted(rows, key=lambda row: (row['food_name'], row['measurement_amount']))
+
+
+def assert_same_foods_present(expected, actual):
+    assert [r['food_name'] for r in expected] == [r['food_name'] for r in actual]
+
+
+def assert_same_column_vals(expected_row, actual_row, columns):
+    def get_differing_columns(expected_row, actual_row):
+        for header in columns:
+            expected = expected_row[header]
+            actual = actual_row.get(header, 'MISSING')
+            if expected != actual:
+                yield (header, expected, actual)
+
+    differing_cols = list(get_differing_columns(expected_row, actual_row))
+    if differing_cols:
+        food_name = expected_row['food_name']
+        msg = f"Incorrect columns in row for {food_name}:"
+        for header, expected, actual in differing_cols:
+            msg += f"\n{header}: expected '{expected}' got '{actual}'"
+        raise AssertionError(msg)
+
+
+class TestSetupUtils(TestCase):
     def test_cases_created(self):
-        accessor = CaseAccessors(self.domain)
+        accessor = CaseAccessors(DOMAIN)
         case_ids = accessor.get_case_ids_in_domain()
         cases = list(accessor.get_cases(case_ids))
 
@@ -51,16 +77,20 @@ class TestMasterReport(TestCase):
 
     def test_fixtures_created(self):
         # Note, this is actually quite slow - might want to drop
-        data_types = get_fixture_data_types(self.domain)
+        data_types = get_fixture_data_types(DOMAIN)
         self.assertEqual(len(data_types), 4)
         self.assertItemsEqual(
-            [(dt.tag, count_fixture_items(self.domain, dt._id)) for dt in data_types],
+            [(dt.tag, count_fixture_items(DOMAIN, dt._id)) for dt in data_types],
             [('recipes', 384),
              ('food_list', 1130),
              ('food_composition_table', 1042),
              ('conv_factors', 2995)]
         )
 
+
+# I plan to remove this eventually, but it's useful to be able to run while I
+# extract components
+class TestOldReport(TestCase):
     def test_old_report(self):
         expected = get_expected_report()
         # exclude rows not pulled from cases for now
@@ -70,21 +100,17 @@ class TestMasterReport(TestCase):
         self.assertItemsEqual(expected[0].keys(), actual[0].keys())  # compare headers
 
         # sort uniformly for comparison
-        expected = self.sort_rows(expected)
-        actual = self.sort_rows(actual)
+        expected = sort_rows(expected)
+        actual = sort_rows(actual)
 
-        self.assert_same_foods_present(expected, actual)
+        assert_same_foods_present(expected, actual)
         for expected_row, actual_row in zip(expected, actual):
             pass
-            # self.assert_same_column_vals(expected_row, actual_row, expected_row.keys())
-
-    @staticmethod
-    def sort_rows(rows):
-        return sorted(rows, key=lambda row: (row['food_name'], row['measurement_amount']))
+            # assert_same_column_vals(expected_row, actual_row, expected_row.keys())
 
     def run_report(self):
         report_data = MasterDataFileData({
-            'domain': self.domain,
+            'domain': DOMAIN,
             'startdate': date(2020, 1, 1).isoformat(),
             'enddate': date(2020, 4, 1).isoformat(),
             'case_owners': '',
@@ -94,47 +120,30 @@ class TestMasterReport(TestCase):
         headers = [h.html for h in report_data.headers]
         return [dict(zip(headers, row)) for row in report_data.rows]
 
-    def assert_same_foods_present(self, expected, actual):
-        self.assertEqual(
-            [r['food_name'] for r in expected],
-            [r['food_name'] for r in actual],
-        )
 
-    def assert_same_column_vals(self, expected_row, actual_row, columns):
-        def get_differing_columns(expected_row, actual_row):
-            for header in columns:
-                expected = expected_row[header]
-                actual = actual_row.get(header, 'MISSING')
-                if expected != actual:
-                    yield (header, expected, actual)
-
-        differing_cols = list(get_differing_columns(expected_row, actual_row))
-        if differing_cols:
-            food_name = expected_row['food_name']
-            msg = f"Incorrect columns in row for {food_name}:"
-            for header, expected, actual in differing_cols:
-                msg += f"\n{header}: expected '{expected}' got '{actual}'"
-            self.assertTrue(False, msg)
-
+class TestUcrAdapter(TestCase):
     def test_data_source(self):
         # Only the rows with case IDs will appear in the UCR
-        expected = self.sort_rows(r for r in get_expected_report() if r['caseid'])
-        ucr_data = self.sort_rows(self.get_ucr_data())
-        self.assert_same_foods_present(expected, ucr_data)
+        expected = sort_rows(r for r in get_expected_report() if r['caseid'])
+        ucr_data = sort_rows(get_ucr_data())
+        assert_same_foods_present(expected, ucr_data)
 
-    def get_ucr_data(self):
-        return FoodCaseData({
-            'domain': self.domain,
-            'startdate': date(2020, 1, 1).isoformat(),
-            'enddate': date(2020, 4, 1).isoformat(),
-            'case_owners': '',
-            'recall_status': '',
-        }).get_data()
 
+def get_ucr_data():
+    return FoodCaseData({
+        'domain': DOMAIN,
+        'startdate': date(2020, 1, 1).isoformat(),
+        'enddate': date(2020, 4, 1).isoformat(),
+        'case_owners': '',
+        'recall_status': '',
+    }).get_data()
+
+
+class TestNewReport(TestCase):
     def test_new_report(self):
-        expected = self.sort_rows(self.get_expected_rows())
-        actual = self.sort_rows(self.run_new_report())
-        self.assert_same_foods_present(expected, actual)
+        expected = sort_rows(self.get_expected_rows())
+        actual = sort_rows(self.run_new_report())
+        assert_same_foods_present(expected, actual)
         columns_known_to_fail = {  # TODO address these columns
             'unique_respondent_id',
             'respondent_id',
@@ -152,7 +161,7 @@ class TestMasterReport(TestCase):
         columns = [c for c in FoodRow._indicators_in_ucr  # for now, only ucr columns are correct
                    if c not in columns_known_to_fail]
         for expected_row, actual_row in zip(expected, actual):
-            self.assert_same_column_vals(expected_row, actual_row, columns)
+            assert_same_column_vals(expected_row, actual_row, columns)
 
     def get_expected_rows(self):
         # Only the rows with case IDs currently appear
@@ -160,7 +169,7 @@ class TestMasterReport(TestCase):
         expected = [r for r in get_expected_report() if r['caseid']]
 
         # Swap out the external IDs in the test fixture for the real IDs
-        accessor = CaseAccessors(self.domain)
+        accessor = CaseAccessors(DOMAIN)
         case_ids = accessor.get_case_ids_in_domain()
         case_ids_by_external_id = {c.external_id: c.case_id
                                    for c in accessor.get_cases(case_ids)}
@@ -173,6 +182,6 @@ class TestMasterReport(TestCase):
         return map(substitute_real_ids, expected)
 
     def run_new_report(self):
-        ucr_data = self.get_ucr_data()
+        ucr_data = get_ucr_data()
         report = FoodData(ucr_data)
         return [dict(zip(report.headers, row)) for row in report.rows]
