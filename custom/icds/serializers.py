ce58edffde1acebf6bb105c965e6d8fb4d1ef868
Jennifer Schweers <orange.jenny@gmail.com>
2020-03-23 18:59:24 -0400

    Fixed tests

--- custom/icds/serializers.py
+++ custom/icds/serializers.py
@@ -20,8 +20,8 @@ class HostedCCZSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         ret = super(HostedCCZSerializer, self).to_representation(instance)
         if instance.build_doc:
-            ret['app_name'] = instance.build_doc['name']
-            custom_properties = instance.build_doc['profile'].get('custom_properties', {})
+            ret['app_name'] = instance.build_doc.get('name')
+            custom_properties = instance.build_doc.get('profile', {}).get('custom_properties', {})
             ret['app_version_tag'] = custom_properties.get('cc-app-version-tag')
         ret['link_name'] = self.instance.link.identifier
         ret['profile_name'] = self.instance.build_profile['name'] if self.instance.profile_id else ''
