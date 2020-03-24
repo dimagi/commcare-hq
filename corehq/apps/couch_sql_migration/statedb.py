8b2f6b299638b26f2dca38e82d4e14ae2b5a1632
Daniel Miller <dmiller@dimagi.com>
2020-03-24 10:14:42 -0400

    Add options to select all diffs of multiple kinds

--- corehq/apps/couch_sql_migration/statedb.py
+++ corehq/apps/couch_sql_migration/statedb.py
@@ -426,8 +426,11 @@ class StateDB(DiffDB):
             assert kind is None, kind
             assert doc_ids is None, doc_ids
             for kind, doc_ids in by_kind.items():
-                for chunk in chunked(doc_ids, 500, list):
-                    yield from self.iter_doc_diffs(kind, chunk, _model=_model)
+                if not doc_ids:
+                    yield from self.iter_doc_diffs(kind, _model=_model)
+                else:
+                    for chunk in chunked(doc_ids, 500, list):
+                        yield from self.iter_doc_diffs(kind, chunk, _model=_model)
             return
         with self.session() as session:
             query = session.query(_model)
