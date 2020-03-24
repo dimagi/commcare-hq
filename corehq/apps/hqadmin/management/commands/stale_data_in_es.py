3d37b8490179e579b6014d1d9ec65bec6d529a4a
Daniel Roberts <droberts@dimagi.com>
2020-03-24 13:26:22 -0500

    Group stale_data_in_es helper functions into dummy classes

--- corehq/apps/hqadmin/management/commands/stale_data_in_es.py
+++ corehq/apps/hqadmin/management/commands/stale_data_in_es.py
@@ -69,63 +69,86 @@ class Command(BaseCommand):
         run_config = RunConfig(domain, start, end, case_type)
 
         for data_model in data_models:
-            if data_model.lower() == 'case':
-                for case_id, case_type, es_date, primary_date in get_server_modified_on_for_domain(run_config):
-                    print(f"{case_id},CommCareCase,{case_type},{es_date},{primary_date}")
-            elif data_model.lower() == 'form':
-                for form_id, doc_type, xmlns, es_date, primary_date in get_stale_form_data(run_config):
-                    print(f"{form_id},{doc_type},{xmlns},{es_date},{primary_date}")
-            else:
-                raise CommandError('Only valid options for data model are "case" and "form"')
+            try:
+                process_data_model_fn = DATA_MODEL_BACKENDS[data_model.lower()]()
+            except KeyError:
+                raise CommandError('Only valid options for data model are "{}"'.format(
+                    '", "'.join(DATA_MODEL_BACKENDS.keys())
+                ))
+            process_data_model_fn(run_config)
 
 
-def get_server_modified_on_for_domain(run_config):
-    if should_use_sql_backend(run_config.domain):
-        return _get_data_for_sql_backend(run_config)
-    else:
-        return _get_data_for_couch_backend(run_config)
+DATA_MODEL_BACKENDS = {
+    'case': lambda: CaseBackend.run,
+    'form': lambda: FormBackend.run,
+}
 
 
-def _get_data_for_couch_backend(run_config):
-    if run_config.case_type:
-        raise CommandError('Case type argument is not supported for couch domains!')
-    domain = run_config.domain
-    start_time = datetime.utcnow()
-    chunk_size = 1000
-    chunked_iterator = chunked(paginate_view(
-        CommCareCase.get_db(),
-        'cases_by_server_date/by_server_modified_on',
-        chunk_size=chunk_size,
-        startkey=[domain],
-        endkey=[domain, {}],
-        include_docs=False,
-        reduce=False
-    ), chunk_size)
-    for chunk in chunked_iterator:
-        case_ids = [row['id'] for row in chunk]
-        es_modified_on_by_ids = _get_es_modified_dates(domain, case_ids)
-        for row in chunk:
-            case_id, couch_modified_on = row['id'], row['value']
-            if iso_string_to_datetime(couch_modified_on) > start_time:
-                # skip cases modified after the script started
-                continue
-            es_modified_on = es_modified_on_by_ids.get(case_id)
-            if not es_modified_on or (es_modified_on != couch_modified_on):
-                yield (case_id, 'COUCH_TYPE_NOT_SUPPORTED', es_modified_on, couch_modified_on)
+class CaseBackend:
+    @staticmethod
+    def run(run_config):
+        rows = CaseBackend._get_server_modified_on_for_domain(run_config)
+        for case_id, case_type, es_date, primary_date in rows:
+            print(f"{case_id},CommCareCase,{case_type},{es_date},{primary_date}")
 
+    @staticmethod
+    def _get_server_modified_on_for_domain(run_config):
+        if should_use_sql_backend(run_config.domain):
+            return CaseBackend._get_data_for_sql_backend(run_config)
+        else:
+            return CaseBackend._get_data_for_couch_backend(run_config)
 
-def _get_data_for_sql_backend(run_config):
-    for db in get_db_aliases_for_partitioned_query():
-        matching_records_for_db = get_sql_case_data_for_db(db, run_config)
+    @staticmethod
+    def _get_data_for_sql_backend(run_config):
+        for db in get_db_aliases_for_partitioned_query():
+            matching_records_for_db = get_sql_case_data_for_db(db, run_config)
+            chunk_size = 1000
+            for chunk in chunked(matching_records_for_db, chunk_size):
+                case_ids = [val[0] for val in chunk]
+                es_modified_on_by_ids = CaseBackend._get_es_modified_dates(run_config.domain, case_ids)
+                for case_id, case_type, sql_modified_on in chunk:
+                    sql_modified_on_str = f'{sql_modified_on.isoformat()}Z'
+                    es_modified_on = es_modified_on_by_ids.get(case_id)
+                    if not es_modified_on or (es_modified_on < sql_modified_on_str):
+                        yield case_id, case_type, es_modified_on, sql_modified_on_str
+
+    @staticmethod
+    def _get_data_for_couch_backend(run_config):
+        if run_config.case_type:
+            raise CommandError('Case type argument is not supported for couch domains!')
+        domain = run_config.domain
+        start_time = datetime.utcnow()
         chunk_size = 1000
-        for chunk in chunked(matching_records_for_db, chunk_size):
-            case_ids = [val[0] for val in chunk]
-            es_modified_on_by_ids = _get_es_modified_dates(run_config.domain, case_ids)
-            for case_id, case_type, sql_modified_on in chunk:
-                sql_modified_on_str = f'{sql_modified_on.isoformat()}Z'
+        chunked_iterator = chunked(paginate_view(
+            CommCareCase.get_db(),
+            'cases_by_server_date/by_server_modified_on',
+            chunk_size=chunk_size,
+            startkey=[domain],
+            endkey=[domain, {}],
+            include_docs=False,
+            reduce=False
+        ), chunk_size)
+        for chunk in chunked_iterator:
+            case_ids = [row['id'] for row in chunk]
+            es_modified_on_by_ids = CaseBackend._get_es_modified_dates(domain, case_ids)
+            for row in chunk:
+                case_id, couch_modified_on = row['id'], row['value']
+                if iso_string_to_datetime(couch_modified_on) > start_time:
+                    # skip cases modified after the script started
+                    continue
                 es_modified_on = es_modified_on_by_ids.get(case_id)
-                if not es_modified_on or (es_modified_on < sql_modified_on_str):
-                    yield (case_id, case_type, es_modified_on, sql_modified_on_str)
+                if not es_modified_on or (es_modified_on != couch_modified_on):
+                    yield case_id, 'COUCH_TYPE_NOT_SUPPORTED', es_modified_on, couch_modified_on
+
+    @staticmethod
+    def _get_es_modified_dates(domain, case_ids):
+        results = (
+            CaseES(es_instance_alias=ES_EXPORT_INSTANCE)
+            .domain(domain)
+            .case_ids(case_ids)
+            .values_list('_id', 'server_modified_on')
+        )
+        return dict(results)
 
 
 def get_sql_case_data_for_db(db, run_config):
@@ -141,53 +164,52 @@ def get_sql_case_data_for_db(db, run_config):
     return matching_cases.values_list('case_id', 'type', 'server_modified_on')
 
 
-def _get_es_modified_dates(domain, case_ids):
-    results = (CaseES(es_instance_alias=ES_EXPORT_INSTANCE)
+class FormBackend:
+    @staticmethod
+    def run(run_config):
+        for form_id, doc_type, xmlns, es_date, primary_date in FormBackend._get_stale_form_data(run_config):
+            print(f"{form_id},{doc_type},{xmlns},{es_date},{primary_date}")
+
+    @staticmethod
+    def _get_stale_form_data(run_config):
+        if should_use_sql_backend(run_config.domain):
+            return FormBackend._get_stale_form_data_for_sql_backend(run_config)
+        else:
+            raise CommandError('Form data for couch domains is not supported!')
+
+    @staticmethod
+    def _get_stale_form_data_for_sql_backend(run_config):
+        for db in get_db_aliases_for_partitioned_query():
+            matching_records_for_db = FormBackend._get_sql_form_data_for_db(db, run_config)
+            chunk_size = 1000
+            length = len(matching_records_for_db) // chunk_size
+            chunk_iter = chunked(matching_records_for_db, chunk_size)
+            for chunk in with_progress_bar(chunk_iter, prefix=f'Processing DB {db}',
+                                           length=length, stream=sys.stderr):
+                form_ids = [val[0] for val in chunk]
+                es_modified_on_by_ids = FormBackend._get_es_modified_dates_for_forms(run_config.domain, form_ids)
+                for form_id, state, xmlns, sql_modified_on in chunk:
+                    doc_type = state_to_doc_type.get(state, 'XFormInstance')
+                    sql_modified_on_str = f'{sql_modified_on.isoformat()}Z'
+                    es_modified_on = es_modified_on_by_ids.get(form_id)
+                    if not es_modified_on or (es_modified_on < sql_modified_on_str):
+                        yield form_id, doc_type, xmlns, es_modified_on, sql_modified_on_str
+
+    @staticmethod
+    def _get_sql_form_data_for_db(db, run_config):
+        return XFormInstanceSQL.objects.using(db).filter(
+            domain=run_config.domain,
+            received_on__gte=run_config.start_date,
+            received_on__lte=run_config.end_date,
+        ).values_list('form_id', 'state', 'xmlns', 'received_on')
+
+    @staticmethod
+    def _get_es_modified_dates_for_forms(domain, form_ids):
+        results = (
+            FormES(es_instance_alias=ES_EXPORT_INSTANCE)
+            .remove_default_filters()
             .domain(domain)
-            .case_ids(case_ids)
-            .values_list('_id', 'server_modified_on'))
-    return dict(results)
-
-
-def get_stale_form_data(run_config):
-    if should_use_sql_backend(run_config.domain):
-        return _get_stale_form_data_for_sql_backend(run_config)
-    else:
-        raise CommandError('Form data for couch domains is not supported!')
-
-
-def _get_stale_form_data_for_sql_backend(run_config):
-    for db in get_db_aliases_for_partitioned_query():
-        matching_records_for_db = _get_sql_form_data_for_db(db, run_config)
-        chunk_size = 1000
-        length = len(matching_records_for_db) // chunk_size
-        chunk_iter = chunked(matching_records_for_db, chunk_size)
-        for chunk in with_progress_bar(chunk_iter, prefix=f'Processing DB {db}',
-                                       length=length, stream=sys.stderr):
-            form_ids = [val[0] for val in chunk]
-            es_modified_on_by_ids = _get_es_modified_dates_for_forms(run_config.domain, form_ids)
-            for form_id, state, xmlns, sql_modified_on in chunk:
-                doc_type = state_to_doc_type.get(state, 'XFormInstance')
-                sql_modified_on_str = f'{sql_modified_on.isoformat()}Z'
-                es_modified_on = es_modified_on_by_ids.get(form_id)
-                if not es_modified_on or (es_modified_on < sql_modified_on_str):
-                    yield (form_id, doc_type, xmlns, es_modified_on, sql_modified_on_str)
-
-
-def _get_sql_form_data_for_db(db, run_config):
-    return XFormInstanceSQL.objects.using(db).filter(
-        domain=run_config.domain,
-        received_on__gte=run_config.start_date,
-        received_on__lte=run_config.end_date,
-    ).values_list('form_id', 'state', 'xmlns', 'received_on')
-
-
-def _get_es_modified_dates_for_forms(domain, form_ids):
-    results = (
-        FormES(es_instance_alias=ES_EXPORT_INSTANCE)
-        .remove_default_filters()
-        .domain(domain)
-        .form_ids(form_ids)
-        .values_list('_id', 'received_on')
-    )
-    return dict(results)
+            .form_ids(form_ids)
+            .values_list('_id', 'received_on')
+        )
+        return dict(results)
