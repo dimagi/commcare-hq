3633e9a2406de4f9f44cded739578e1bdee0c66c
Rohit Negi <rohit25.negi@gmail.com>
2020-03-24 14:00:19 +0530

    fix lynter error

--- custom/icds_reports/management/commands/queue_person_cases.py
+++ custom/icds_reports/management/commands/queue_person_cases.py
@@ -9,12 +9,18 @@ from dimagi.utils.chunked import chunked
 
 
 class Command(BaseCommand):
-    help = "Rebuild Bihar person cases "
+    help = "Rebuild Bihar person cases"
 
-    def handle(self, *args,**kwargs):
+    def handle(self, *args, **kwargs):
         person_config = _get_config_by_id('static-icds-cas-static-person_cases_v3')
         table_name = get_table_name('icds-cas', 'static-person_cases_v3')
-        query = """select supervisor, doc_id from "{}" where state_id='f9b47ea2ee2d8a02acddeeb491d3e175' order by supervisor, doc_id""".format(table_name)
+        # sort by supervisor_id and doc_id to improve the performance, sorting is needed to resume the queueing
+        # if it fails in between.
+        query = """
+            select supervisor, doc_id from "{}"
+            where state_id='f9b47ea2ee2d8a02acddeeb491d3e175'
+            order by supervisor, doc_id
+        """.format(table_name)
 
         with connections['icds-ucr-citus'].cursor() as cursor:
             cursor.execute(query)
@@ -22,9 +28,10 @@ class Command(BaseCommand):
 
         total_doc_ids = len(doc_ids)
         count = 0
-        for ids_chunk in chunked(doc_ids, 10000):
+        chunk_size = 10000
+        for ids_chunk in chunked(doc_ids, chunk_size):
             ids_list = [item for item in ids_chunk]
             AsyncIndicator.bulk_creation([elem[1] for elem in ids_list], 'case', 'icds-cas', [person_config._id])
-            count += 10000
+            count += chunk_size
             print("Success till doc_id: {}".format(ids_list[-1]))
-            print("progress: {}/{}".format(count, total_doc_ids ))
+            print("progress: {}/{}".format(count, total_doc_ids))
