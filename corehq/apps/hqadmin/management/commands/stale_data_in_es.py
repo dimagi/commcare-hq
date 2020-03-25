16d400e207859371ebd4fdbc88d8e8a06ab21cb2
Daniel Roberts <droberts@dimagi.com>
2020-03-25 08:47:23 -0500

    Check for "normal and "archived" forms in stale_data_in_es

--- corehq/apps/hqadmin/management/commands/stale_data_in_es.py
+++ corehq/apps/hqadmin/management/commands/stale_data_in_es.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 
 import dateutil
+from django.db.models import F
 
 from casexml.apps.case.models import CommCareCase
 from dimagi.utils.chunked import chunked
@@ -227,8 +228,8 @@ class FormBackend:
         matching_forms = XFormInstanceSQL.objects.using(db).filter(
             received_on__gte=run_config.start_date,
             received_on__lte=run_config.end_date,
-            # Only check for "normal" forms
-            state=XFormInstanceSQL.NORMAL
+            # Only check for "normal" and "archived" forms
+            state=F('state').bitand(XFormInstanceSQL.NORMAL) + F('state').bitand(XFormInstanceSQL.ARCHIVED)
         )
         if run_config.domain is not ALL_SQL_DOMAINS:
             matching_forms = matching_forms.filter(
