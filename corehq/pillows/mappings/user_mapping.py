9c4b7e31f5b33dead5dfe509d247c860c31853d9
Jennifer Schweers <orange.jenny@gmail.com>
2020-03-25 19:59:10 -0400

    Updated ES user index name

--- corehq/pillows/mappings/user_mapping.py
+++ corehq/pillows/mappings/user_mapping.py
@@ -4,7 +4,7 @@ from corehq.pillows.core import DATE_FORMATS_ARR, DATE_FORMATS_STRING
 from pillowtop.es_utils import ElasticsearchIndexInfo
 
 
-USER_INDEX = es_index("hqusers_2017-09-07")
+USER_INDEX = es_index("hqusers_2020-03-25")
 USER_MAPPING = {'_all': {'analyzer': 'standard'},
  '_meta': {'created': None},
  'date_detection': False,
