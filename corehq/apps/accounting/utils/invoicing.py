619e6f11ef457ea7f881446699c8f74355b8aab1
Biyeun Buczyk <biyeun@dimagi.com>
2020-03-26 17:18:20 -0500

    make these methods private again

--- corehq/apps/accounting/utils/invoicing.py
+++ corehq/apps/accounting/utils/invoicing.py
@@ -11,7 +11,7 @@ from corehq.apps.accounting.models import (
 UNPAID_INVOICE_THRESHOLD = 100
 
 
-def get_all_unpaid_saas_invoices():
+def _get_all_unpaid_saas_invoices():
     return Invoice.objects.filter(
         is_hidden=False,
         subscription__service_type=SubscriptionType.PRODUCT,
@@ -19,17 +19,17 @@ def get_all_unpaid_saas_invoices():
     )
 
 
-def get_unpaid_saas_invoices_in_downgrade_daterange(today):
-    return get_all_unpaid_saas_invoices().filter(
+def _get_unpaid_saas_invoices_in_downgrade_daterange(today):
+    return _get_all_unpaid_saas_invoices().filter(
         date_due__lte=today - datetime.timedelta(days=1)
     ).order_by('date_due').select_related('subscription__subscriber')
 
 
 def get_unpaid_invoices_over_threshold_by_domain(today, domain):
-    for overdue_invoice in get_unpaid_saas_invoices_in_downgrade_daterange(today).filter(
+    for overdue_invoice in _get_unpaid_saas_invoices_in_downgrade_daterange(today).filter(
         subscription__subscriber__domain=domain
     ):
-        total_overdue_by_domain_and_invoice_date = get_all_unpaid_saas_invoices().filter(
+        total_overdue_by_domain_and_invoice_date = _get_all_unpaid_saas_invoices().filter(
             Q(date_due__lte=overdue_invoice.date_due)
             | (Q(date_due__isnull=True) & Q(date_end__lte=overdue_invoice.date_end)),
             subscription__subscriber__domain=domain,
@@ -40,7 +40,7 @@ def get_unpaid_invoices_over_threshold_by_domain(today, domain):
 
 
 def get_domains_with_subscription_invoices_over_threshold(today):
-    for domain in set(get_unpaid_saas_invoices_in_downgrade_daterange(today).values_list(
+    for domain in set(_get_unpaid_saas_invoices_in_downgrade_daterange(today).values_list(
         'subscription__subscriber__domain', flat=True
     )):
         overdue_invoice, total_overdue_to_date = get_unpaid_invoices_over_threshold_by_domain(today, domain)
