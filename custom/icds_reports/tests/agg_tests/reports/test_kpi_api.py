baf711d0fb648eb4e4358e55afcb34e907304c38
challa <challarao@beehyv.com>
2020-03-27 20:17:44 +0530

    test case fix

--- custom/icds_reports/tests/agg_tests/reports/test_kpi_api.py
+++ custom/icds_reports/tests/agg_tests/reports/test_kpi_api.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.test import TestCase
 
 from custom.icds_reports.models.views import KPIAPIView
@@ -31,14 +33,15 @@ class KPIAPITest(TestCase):
             "cf_initiation_in_month"
         )
         first_result = data[0]
+        print(first_result)
         self.assertDictEqual(
             {
                 "state_id": "st1", "state_name": "st1", "state_site_code": "st1",
                 "district_id": "d1", "district_name": "d1", "district_site_code": "d1",
                 "aggregation_level": 2, "district_map_location_name": "", "state_map_location_name": "",
-                "month": date(2017, 5, 1), "cbe_conducted": 1, "vhnd_conducted": 3, "num_launched_awcs": 10,
-                "wer_weighed": 475, "wer_eligible": 317, "bf_at_birth": 1, "born_in_month": 2,
-                "cf_initiation_eligible": 14, "cf_initiation_in_month": 17
+                "month": datetime.date(2017, 5, 1), "cbe_conducted": 1, "vhnd_conducted": 3, "num_launched_awcs": 10,
+                "wer_weighed": 317, "wer_eligible": 475, "bf_at_birth": 1, "born_in_month": 2,
+                "cf_initiation_eligible": 17, "cf_initiation_in_month": 14
              },
             first_result
         )
