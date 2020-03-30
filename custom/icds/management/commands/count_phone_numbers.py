88643d26016ab1ce92845cbe58874efc57a76347
Jennifer Schweers <orange.jenny@gmail.com>
2020-03-30 18:51:32 -0400

    Added totals

--- custom/icds/management/commands/count_phone_numbers.py
+++ custom/icds/management/commands/count_phone_numbers.py
@@ -41,6 +41,9 @@ class Command(BaseCommand):
             writer = csv.writer(output)
             writer.writerow(['partition', 'total cases', 'phone number present', 'phone number valid'])
 
+            total_persons = 0
+            total_persons_with_phone_number = 0
+            total_persons_with_valid_phone_number = 0
             for partition in partitions:
                 self.stdout.write("querying partition: %s" % partition)
 
@@ -49,14 +52,20 @@ class Command(BaseCommand):
                 base_query = base_query.exclude(owner_id__in=test_locations)
 
                 persons = base_query.count()
-                self.stdout.write("total persons: %s" % persons)
+                self.stdout.write("person cases: %s" % persons)
+                total_persons += persons
 
                 persons_with_phone_number = base_query.filter(case_json__regex="\"contact_phone_number\": \".+\"").count()
                 self.stdout.write("phone number present: %s" % persons_with_phone_number)
+                total_persons_with_phone_number += persons_with_phone_number
 
                 persons_with_valid_phone_number = base_query.filter(case_json__regex="\"contact_phone_number\": \"91[6789][0-9]{9}\"").count()
                 self.stdout.write("phone number valid: %s" % persons_with_valid_phone_number)
+                total_persons_with_valid_phone_number += persons_with_valid_phone_number
 
                 writer.writerow([partition, persons, persons_with_phone_number, persons_with_valid_phone_number])
 
-        self.stdout.write("Results written to : %s" % filename)
+        self.stdout.write("Results written to %s" % filename)
+        self.stdout.write("Total person cases: %s" % total_persons)
+        self.stdout.write("Total phone number present: %s" % total_persons_with_phone_number)
+        self.stdout.write("Total phone number valid: %s" % total_persons_with_valid_phone_number)
