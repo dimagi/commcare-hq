341f14ae387fdac8997f67901b81be21e81619a4
Ethan Soergel <esoergel@gmail.com>
2020-03-31 18:58:53 -0400

    Update test of fixtures created

--- custom/inddex/tests/test_master_report.py
+++ custom/inddex/tests/test_master_report.py
@@ -67,13 +67,14 @@ class TestSetupUtils(TestCase):
     def test_fixtures_created(self):
         # Note, this is actually quite slow - might want to drop
         data_types = get_fixture_data_types(DOMAIN)
-        self.assertEqual(len(data_types), 4)
+        self.assertEqual(len(data_types), 5)
         self.assertItemsEqual(
             [(dt.tag, count_fixture_items(DOMAIN, dt._id)) for dt in data_types],
             [('recipes', 384),
              ('food_list', 1130),
              ('food_composition_table', 1042),
-             ('conv_factors', 2995)]
+             ('conv_factors', 2995),
+             ('nutrients_lookup', 152)]
         )
 
 
