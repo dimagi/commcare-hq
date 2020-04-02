ceab9d537d286eeb61f4f2a73da1459ea7122403
Norman Hooper <nhooper@dimagi.com>
2020-04-02 17:14:32 +0200

    Make event status and program status value sources

--- corehq/motech/dhis2/dhis2_config.py
+++ corehq/motech/dhis2/dhis2_config.py
@@ -31,10 +31,9 @@ class Dhis2FormConfig(DocumentSchema):
     enrollment_date = DictProperty(required=False)
     incident_date = DictProperty(required=False)
     program_stage_id = DictProperty(required=False)
-    program_status = StringProperty(
-        choices=DHIS2_PROGRAM_STATUSES,
-        default=DHIS2_PROGRAM_STATUS_ACTIVE,
-    )
+    program_status = DictProperty(required=False, default={
+        "value": DHIS2_PROGRAM_STATUS_ACTIVE
+    })
     org_unit_id = DictProperty(required=False, default={
         "form_user_ancestor_location_field": LOCATION_DHIS_ID
     })
@@ -42,10 +41,9 @@ class Dhis2FormConfig(DocumentSchema):
         "form_question": "/metadata/received_on",
         "external_data_type": DHIS2_DATA_TYPE_DATE,
     })
-    event_status = StringProperty(
-        choices=DHIS2_EVENT_STATUSES,
-        default=DHIS2_EVENT_STATUS_COMPLETED,
-    )
+    event_status = DictProperty(required=False, default={
+        "value": DHIS2_EVENT_STATUS_COMPLETED
+    })
     completed_date = DictProperty(required=False)
     datavalue_maps = SchemaListProperty(FormDataValueMap)
 
@@ -54,6 +52,10 @@ class Dhis2FormConfig(DocumentSchema):
         if isinstance(data.get('org_unit_id'), str):
             # Convert org_unit_id from a string to a ConstantValue
             data['org_unit_id'] = {'value': data['org_unit_id']}
+        if isinstance(data.get('event_status'), str):
+            data['event_status'] = {'value': data['event_status']}
+        if isinstance(data.get('program_status'), str):
+            data['program_status'] = {'value': data['program_status']}
         return super(Dhis2FormConfig, cls).wrap(data)
 
 
