1a5e6822bbdf38d9bd927d4cffabce758e69e51c
Jennifer Schweers <orange.jenny@gmail.com>
2020-04-03 12:38:48 -0400

    Added tracking for apps imported from COVID library

--- corehq/apps/app_manager/static/app_manager/js/app_exchange.js
+++ corehq/apps/app_manager/static/app_manager/js/app_exchange.js
@@ -1,10 +1,12 @@
 hqDefine("app_manager/js/app_exchange", [
     "jquery",
     "knockout",
+    'analytix/js/kissmetrix',
     "hqwebapp/js/widgets",  // hqwebapp-select2 for versions
 ], function (
     $,
-    ko
+    ko,
+    kissmetrics
 ) {
     var AppExchangeModel = function () {
         var self = {};
@@ -27,5 +29,9 @@ hqDefine("app_manager/js/app_exchange", [
 
     $(function () {
         $("#hq-content").koApplyBindings(AppExchangeModel());
+
+        $('.import-button').on('click', function () {
+            kissmetrics.track.event("COVID App Library: Imported application");
+        });
     });
 });
