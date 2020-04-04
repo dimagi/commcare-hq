cb7f88104a36fa6b97c0a81c55a62c5bf7cdef7f
Daniel Roberts <droberts@dimagi.com>
2020-04-03 19:04:09 -0500

    Add support for Twilio Messaging Services
    They take care of load balancing onto multiple numbers and sticky routing
    that survives adding numbers

--- corehq/messaging/smsbackends/twilio/forms.py
+++ corehq/messaging/smsbackends/twilio/forms.py
@@ -1,4 +1,5 @@
 from corehq.apps.sms.forms import BackendForm, LoadBalancingBackendFormMixin
+from corehq.messaging.smsbackends.twilio.models import SQLTwilioBackend
 from dimagi.utils.django.fields import TrimmedCharField
 from crispy_forms import layout as crispy
 from django.utils.translation import ugettext_lazy as _
@@ -19,3 +20,7 @@ class TwilioBackendForm(BackendForm, LoadBalancingBackendFormMixin):
             'account_sid',
             'auth_token',
         )
+
+    def validate_phone_number(self, phone_number: str) -> None:
+        if not SQLTwilioBackend.phone_number_is_messaging_service_sid(phone_number):
+            super().validate_phone_number(phone_number)
