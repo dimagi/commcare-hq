dimagi/commcare-hq

commit 2adfd8e2b3b195a348cc24c404b5aeaab1256603

Removed CUSTOM_SMS_HANDLERS and CUSTOM_PROJECT_SMS_QUEUES

corehq/apps/sms/api.py
@@ -660,16 +660,8 @@ def process_incoming(msg):
         is_two_way = v is not None and v.is_two_way
 
         if msg.domain and domain_has_privilege(msg.domain, privileges.INBOUND_SMS):
-            handled = load_and_call(settings.CUSTOM_SMS_HANDLERS, v, msg.text, msg)
-
-            if not handled and v and v.pending_verification:
-                from . import verify
-                handled = verify.process_verification(v, msg,
-                    create_subevent_for_inbound=not has_domain_two_way_scope)
-
             if (
-                not handled
-                and (is_two_way or has_domain_two_way_scope)
+                (is_two_way or has_domain_two_way_scope)
                 and is_contact_active(v.domain, v.owner_doc_type, v.owner_id)
             ):
                 handled = load_and_call(settings.SMS_HANDLERS, v, msg.text, msg)

corehq/apps/sms/tasks.py
@@ -427,11 +427,7 @@ def process_sms(queued_sms_pk):
 
 
 def send_to_sms_queue(queued_sms):
-    options = {}
-    if queued_sms.direction == OUTGOING and queued_sms.domain in settings.CUSTOM_PROJECT_SMS_QUEUES:
-        options['queue'] = settings.CUSTOM_PROJECT_SMS_QUEUES[queued_sms.domain]
-
-    process_sms.apply_async([queued_sms.pk], **options)
+    process_sms.apply_async([queued_sms.pk])
 
 
 @no_result_task(serializer='pickle', queue='background_queue', default_retry_delay=10 * 60,

settings.py
@@ -603,12 +603,6 @@ FORMPLAYER_URL = 'http://localhost:8080'
 
 ####### SMS Queue Settings #######
 
-CUSTOM_PROJECT_SMS_QUEUES = {
-    'ils-gateway': 'ils_gateway_sms_queue',
-    'ils-gateway-train': 'ils_gateway_sms_queue',
-    'ils-gateway-training': 'ils_gateway_sms_queue',
-}
-
 # Setting this to False will make the system process outgoing and incoming SMS
 # immediately rather than use the queue.
 # This should always be set to True in production environments, and the sms_queue
@@ -1488,10 +1482,6 @@ WEB_USER_TERM = "Web User"
 
 DEFAULT_CURRENCY = "USD"
 DEFAULT_CURRENCY_SYMBOL = "$"
-
-CUSTOM_SMS_HANDLERS = [
-    'custom.ilsgateway.tanzania.handler.handle',
-]
 
 SMS_HANDLERS = [
     'corehq.apps.commtrack.sms.handle',

commit e0e830d9898d33e06ba766cde9f91836a7e42aa6

Fix verify phone number workflow
possibly broken since
https://github.com/dimagi/commcare-hq/commit/2adfd8e2b3b195a348cc24c404b5aeaab1256603

corehq/apps/sms/api.py
@@ -660,6 +660,10 @@ def process_incoming(msg):
         is_two_way = v is not None and v.is_two_way
 
         if msg.domain and domain_has_privilege(msg.domain, privileges.INBOUND_SMS):
+            if v and v.pending_verification:
+                from . import verify
+                handled = verify.process_verification(v, msg, create_subevent_for_inbound=not has_domain_two_way_scope)
+
             if (
                 (is_two_way or has_domain_two_way_scope)
                 and is_contact_active(v.domain, v.owner_doc_type, v.owner_id)