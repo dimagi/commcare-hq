585101bf7cfaf17b4bcbb9074f739f4b5f2f87e0
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-06 23:03:18 +0530

    [build]: ap data pull
    - Github Issue:

---
+++ custom/icds_reports/management/commands/sql_scripts/ap_data_pull_2.sql
@@ -0,0 +1,18 @@
+
+COPY(SELECT
+    awc.state_name,
+    awc.district_name,
+    awc.block_name,
+    awc.supervisor_name,
+    awc.awc_name,
+    awc.awc_site_code,
+    CASE WHEN (agg.vhnd_conducted IS NOT NULL AND agg.vhnd_conducted>0) THEN 'Y'
+        ELSE 'N'
+    END as vhnd_conducted_month_executes
+FROM
+    "awc_location_local" awc LEFT JOIN "agg_awc" agg ON (
+        agg.supervisor_id=agg.supervisor_id and
+        agg.awc_id = agg.awc_id
+    )
+    where agg.month = '%(month_execute)s'
+    and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5) TO '/tmp/ap_%(month_execute)s_vhnd.csv' DELIMITER ',' CSV HEADER
