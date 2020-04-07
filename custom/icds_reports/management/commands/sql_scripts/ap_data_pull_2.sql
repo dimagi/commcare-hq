a8ff5703c25f9bfeb7e5e9ab0889e4a7ba484695
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-07 15:13:25 +0530

    [refactor]: - Shifted the montly queries to quaters - Added logging - Added the thr from the ccs record - Added the launched and not launched status month wise - Storing files for each quater in the month
    - Github Issue:

--- custom/icds_reports/management/commands/sql_scripts/ap_data_pull_2.sql
+++ custom/icds_reports/management/commands/sql_scripts/ap_data_pull_2.sql
@@ -1,4 +1,3 @@
-
 COPY(SELECT
     awc.state_name,
     awc.district_name,
@@ -6,17 +5,79 @@ COPY(SELECT
     awc.supervisor_name,
     awc.awc_name,
     awc.awc_site_code,
-    CASE WHEN (agg.num_awcs_conducted_vhnd IS NOT NULL AND agg.num_awcs_conducted_vhnd>0) THEN 'Y'
+    CASE WHEN (agg.num_awcs_conducted_vhnd IS NOT NULL AND agg.num_awcs_conducted_vhnd>0 AND agg.month='%(month_1)s') THEN 'Y'
         ELSE 'N'
-    END as vhnd_conducted_month_executes,
-    CASE WHEN num_launched_awcs=1 THEN 'Launched'
+    END as vhnd_conducted_%(column_1)s,
+    CASE WHEN agg.num_launched_awcs=1 AND agg.month='%(month_1)s' THEN 'Launched'
         ELSE 'Not Launched'
-    END as launched_status
+    END as launched_status_%(column_1)s,
+
+    CASE WHEN (agg.num_awcs_conducted_vhnd IS NOT NULL AND agg.num_awcs_conducted_vhnd>0 AND agg.month='%(month_2)s') THEN 'Y'
+        ELSE 'N'
+    END as vhnd_conducted_%(column_2)s,
+    CASE WHEN agg.num_launched_awcs=1 AND agg.month='%(month_2)s' THEN 'Launched'
+        ELSE 'Not Launched'
+    END as launched_status_%(column_2)s,
+
+    CASE WHEN (agg.num_awcs_conducted_vhnd IS NOT NULL AND agg.num_awcs_conducted_vhnd>0 AND agg.month='%(month_3)s') THEN 'Y'
+        ELSE 'N'
+    END as vhnd_conducted_%(column_3)s,
+    CASE WHEN agg.num_launched_awcs=1 AND agg.month='%(month_3)s' THEN 'Launched'
+        ELSE 'Not Launched'
+    END as launched_status_%(column_3)s
 FROM
     "awc_location_local" awc LEFT JOIN "agg_awc" agg ON (
         awc.supervisor_id=agg.supervisor_id and
-        awc.awc_id = agg.awc_id AND
+        awc.doc_id = agg.awc_id AND
         awc.supervisor_id=agg.supervisor_id
     )
-    where agg.month = "%(month_execute)s"
-    and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5) TO '/tmp/%(month_execute)s/ap_%(month_execute)s_vhnd.csv' DELIMITER ',' CSV HEADER
+    where agg.month in ('%(month_1)s','%(month_2)s','%(month_3)s')
+    and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5) TO '/tmp/%(name)s/ap_data_pull_2.csv' DELIMITER ',' CSV HEADER
+
+--                                                                QUERY PLAN
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+--  Gather  (cost=298255.09..301832.65 rows=5 width=280)
+--    Workers Planned: 4
+--    ->  Merge Join  (cost=297255.09..300832.15 rows=1 width=280)
+--          Merge Cond: ((agg_15.supervisor_id = awc.supervisor_id) AND (agg_15.awc_id = awc.doc_id))
+--          ->  Sort  (cost=220727.65..221733.89 rows=402493 width=77)
+--                Sort Key: agg_15.supervisor_id, agg_15.awc_id
+--                ->  Parallel Append  (cost=0.00..162307.36 rows=402493 width=77)
+--                      ->  Parallel Seq Scan on "agg_awc_2019-06-01_5" agg_15  (cost=0.00..61171.33 rows=150715 width=78)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-05-01_5" agg_10  (cost=0.00..49717.43 rows=127958 width=78)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-04-01_5" agg_5  (cost=0.00..42845.22 rows=108743 width=78)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-06-01_4" agg_14  (cost=0.00..2095.27 rows=10783 width=49)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-05-01_4" agg_9  (cost=0.00..1931.26 rows=10346 width=49)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-04-01_4" agg_4  (cost=0.00..1635.53 rows=9275 width=49)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-06-01_3" agg_13  (cost=0.00..301.57 rows=1642 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-05-01_3" agg_8  (cost=0.00..278.47 rows=1562 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-04-01_3" agg_3  (cost=0.00..227.89 rows=1374 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-06-01_2" agg_12  (cost=0.00..28.21 rows=161 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-05-01_2" agg_7  (cost=0.00..26.09 rows=152 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-04-01_2" agg_2  (cost=0.00..21.89 rows=138 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-06-01_1" agg_11  (cost=0.00..5.25 rows=18 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-05-01_1" agg_6  (cost=0.00..5.24 rows=18 width=20)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on "agg_awc_2019-04-01_1" agg_1  (cost=0.00..4.24 rows=2 width=76)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                      ->  Parallel Seq Scan on agg_awc agg  (cost=0.00..0.00 rows=1 width=76)
+--                            Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--          ->  Materialize  (cost=76527.44..76806.60 rows=55832 width=151)
+--                ->  Sort  (cost=76527.44..76667.02 rows=55832 width=151)
+--                      Sort Key: awc.supervisor_id, awc.doc_id
+--                      ->  Index Scan using awc_location_local_pkey on awc_location_local awc  (cost=0.68..69665.42 rows=55832 width=151)
+--                            Index Cond: (state_id = 'f98e91aa003accb7b849a0f18ebd7039'::text)
+--                            Filter: (aggregation_level = 5)
--- custom/icds_reports/management/commands/sql_scripts/ap_data_pull_3.sql
+++ custom/icds_reports/management/commands/sql_scripts/ap_data_pull_3.sql
@@ -5,18 +5,47 @@ COPY(SELECT
     awc.supervisor_name,
     awc.awc_name,
     awc.awc_site_code,
-    SUM(CASE WHEN chm.num_rations_distributed>=21 THEN 1 ELSE 0 END) as thr_count_ccs,
-    SUM(CASE WHEN chm.thr_eligible=1 THEN 1 ELSE 0 END) as thr_eligible_ccs
+    SUM(CASE WHEN chm.num_rations_distributed>=21 AND chm.month='%(month_1)s' THEN 1 ELSE 0 END) as thr_count_ccs_%(column_1)s,
+    SUM(CASE WHEN chm.thr_eligible=1 AND chm.month='%(month_1)s' THEN 1 ELSE 0 END) as thr_eligible_ccs_%(column_1)s,
+
+    SUM(CASE WHEN chm.num_rations_distributed>=21 AND chm.month='%(month_2)s' THEN 1 ELSE 0 END) as thr_count_ccs_%(column_2)s,
+    SUM(CASE WHEN chm.thr_eligible=1 AND chm.month='%(month_2)s' THEN 1 ELSE 0 END) as thr_eligible_ccs_%(column_2)s,
+
+    SUM(CASE WHEN chm.num_rations_distributed>=21 AND chm.month='%(month_3)s' THEN 1 ELSE 0 END) as thr_count_ccs_%(column_3)s,
+    SUM(CASE WHEN chm.thr_eligible=1 AND chm.month='%(month_3)s' THEN 1 ELSE 0 END) as thr_eligible_ccs_%(column_3)s
 FROM
     "awc_location" awc LEFT JOIN "ccs_record_monthly" chm ON (
         chm.supervisor_id=awc.supervisor_id and
         chm.awc_id = awc.doc_id
     )
-    where chm.month = "%(month_execute)s"
+    where chm.month in ('%(month_1)s','%(month_2)s','%(month_3)s')
     and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5
 GROUP by awc.state_name,
     awc.district_name,
     awc.block_name,
     awc.supervisor_name,
     awc.awc_name,
-    awc.awc_site_code) TO '/tmp/%(month_execute)s/ap_%(month_execute)s_thr_ccs.csv' DELIMITER ',' CSV HEADER
+    awc.awc_site_code) TO '/tmp/%(name)s/ap_data_pull_3.csv' DELIMITER ',' CSV HEADER
+
+
+--                                                                                    QUERY PLAN
+-- ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--  HashAggregate  (cost=0.00..0.00 rows=0 width=0)
+--    Group Key: remote_scan.state_name, remote_scan.district_name, remote_scan.block_name, remote_scan.supervisor_name, remote_scan.awc_name, remote_scan.awc_site_code
+--    ->  Custom Scan (Citus Real-Time)  (cost=0.00..0.00 rows=0 width=0)
+--          Task Count: 64
+--          Tasks Shown: One of 64
+--          ->  Task
+--                Node: host=100.71.184.232 port=6432 dbname=icds_ucr
+--                ->  GroupAggregate  (cost=176133.36..176133.43 rows=1 width=136)
+--                      Group Key: awc.state_name, awc.district_name, awc.block_name, awc.supervisor_name, awc.awc_name, awc.awc_site_code
+--                      ->  Sort  (cost=176133.36..176133.36 rows=1 width=100)
+--                            Sort Key: awc.state_name, awc.district_name, awc.block_name, awc.supervisor_name, awc.awc_name, awc.awc_site_code
+--                            ->  Gather  (cost=1000.55..176133.35 rows=1 width=100)
+--                                  Workers Planned: 5
+--                                  ->  Nested Loop  (cost=0.55..175133.25 rows=1 width=100)
+--                                        ->  Parallel Seq Scan on ccs_record_monthly_102712 chm  (cost=0.00..137867.58 rows=29125 width=78)
+--                                              Filter: (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[]))
+--                                        ->  Index Scan using awc_location_indx6_102840 on awc_location_102840 awc  (cost=0.55..1.27 rows=1 width=151)
+--                                              Index Cond: (doc_id = chm.awc_id)
+--                                              Filter: ((state_id = 'f98e91aa003accb7b849a0f18ebd7039'::text) AND (aggregation_level = 5) AND (chm.supervisor_id = supervisor_id))
