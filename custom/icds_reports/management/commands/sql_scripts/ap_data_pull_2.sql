3f8a1de4a83b05a557875e5cfba1240387b617f6
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-07 14:00:38 +0530

    [fix]: minor fixes after review
    - Github Issue:

--- custom/icds_reports/management/commands/sql_scripts/ap_data_pull_2.sql
+++ custom/icds_reports/management/commands/sql_scripts/ap_data_pull_2.sql
@@ -6,13 +6,17 @@ COPY(SELECT
     awc.supervisor_name,
     awc.awc_name,
     awc.awc_site_code,
-    CASE WHEN (agg.vhnd_conducted IS NOT NULL AND agg.vhnd_conducted>0) THEN 'Y'
+    CASE WHEN (agg.num_awcs_conducted_vhnd IS NOT NULL AND agg.num_awcs_conducted_vhnd>0) THEN 'Y'
         ELSE 'N'
-    END as vhnd_conducted_month_executes
+    END as vhnd_conducted_month_executes,
+    CASE WHEN num_launched_awcs=1 THEN 'Launched'
+        ELSE 'Not Launched'
+    END as launched_status
 FROM
     "awc_location_local" awc LEFT JOIN "agg_awc" agg ON (
-        agg.supervisor_id=agg.supervisor_id and
-        agg.awc_id = agg.awc_id
+        awc.supervisor_id=agg.supervisor_id and
+        awc.awc_id = agg.awc_id AND
+        awc.supervisor_id=agg.supervisor_id
     )
-    where agg.month = '%(month_execute)s'
-    and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5) TO '/tmp/ap_%(month_execute)s_vhnd.csv' DELIMITER ',' CSV HEADER
+    where agg.month = "%(month_execute)s"
+    and awc.state_id='f98e91aa003accb7b849a0f18ebd7039' and awc.aggregation_level=5) TO '/tmp/%(month_execute)s/ap_%(month_execute)s_vhnd.csv' DELIMITER ',' CSV HEADER
