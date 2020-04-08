bfe000bcaad3b9da34e46353c7542b305ad3dcb5
Ethan Soergel <esoergel@gmail.com>
2020-04-08 17:46:46 -0400

    Implement gaps summary report

--- custom/inddex/reports/gaps_summary.py
+++ custom/inddex/reports/gaps_summary.py
@@ -1,10 +1,8 @@
-from memoized import memoized
+from collections import defaultdict
 
 from custom.inddex import filters
-from custom.inddex.ucr.data_providers.gaps_summary_data import (
-    ConvFactorGapsSummaryData,
-    FCTGapsSummaryData,
-)
+from custom.inddex.food import ConvFactorGaps, FctGaps, FoodData
+
 from .utils import MultiTabularReport
 
 
@@ -22,18 +20,49 @@ class GapsSummaryReport(MultiTabularReport):
         ]
 
     @property
-    @memoized
     def data_providers(self):
+        cf_gaps, fct_gaps = self._get_gap_counts()
         return [
-            ConvFactorGapsSummaryData(config=self.report_config),
-            FCTGapsSummaryData(config=self.report_config)
+            ConvFactorGapsData(cf_gaps),
+            FctGapsData(fct_gaps),
         ]
 
+    def _get_gap_counts(self):
+        cf_gaps = defaultdict(int)
+        fct_gaps = defaultdict(int)
+        food_data = FoodData.from_request(self.domain, self.request)
+        for row in food_data.rows:
+            cf_gaps[(row.conv_factor_gap_code, row.food_type)] += 1
+            fct_gaps[(row.fct_gap_code, row.food_type)] += 1
+        return cf_gaps, fct_gaps
+
+
+class GapsData:
+    def __init__(self, gaps):
+        self._gaps = gaps
+
     @property
-    def report_config(self):
-        report_config = {}  # TODO port to FoodData.from_request
-        report_config.update(
-            gap_type=self.request.GET.get('gap_type') or '',
-            recall_status=self.request.GET.get('recall_status') or '',
-        )
-        return report_config
+    def rows(self):
+        for (code, food_type), count in self._gaps.items():
+            description = self._gaps_descriptions[code]
+            yield [code, description, food_type, count]
+
+
+class ConvFactorGapsData(GapsData):
+    title = 'Conv Factor Gaps Summary'
+    slug = 'conv_factor_gaps_summary'
+    _gaps_descriptions = ConvFactorGaps.DESCRIPTIONS
+
+    @property
+    def headers(self):
+        return ['conv_factor_gap_code', 'conv_factor_gap_desc', 'food_type', 'conv_gap_food_type_total']
+
+
+class FctGapsData(GapsData):
+    title = 'FCT Gaps Summary'
+    slug = 'fct_gaps_summary'
+    _gaps_descriptions = FctGaps.DESCRIPTIONS
+
+    @property
+    def headers(self):
+        return ['fct_gap_code', 'fct_gap_desc', 'food_type', 'fct_gap_food_type_total']
