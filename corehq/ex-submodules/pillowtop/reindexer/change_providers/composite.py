b8f84825d744c81d0aabd5d056d0a3cc83b0029d
Sravan Reddy <sravfeyn@gmail.com>
2020-04-09 10:34:28 +0530

    Revert "Convert report form/case reindexers too"
    This reverts commit c785e32b018e8396f3309ec56256057b1a3a6bf0.

--- corehq/ex-submodules/pillowtop/reindexer/change_providers/composite.py
+++ corehq/ex-submodules/pillowtop/reindexer/change_providers/composite.py
@@ -1,6 +1,5 @@
 import itertools
 
-from corehq.util.doc_processor.interface import DocumentProvider
 from pillowtop.reindexer.change_providers.interface import ChangeProvider
 
 
@@ -13,15 +12,3 @@ class CompositeChangeProvider(ChangeProvider):
 
     def iter_all_changes(self, start_from=None):
         return itertools.chain(*[change_provider.iter_all_changes() for change_provider in self.change_providers])
-
-
-class CompositeDocProvider(DocumentProvider):
-
-    def __init__(self, doc_providers):
-        self.doc_providers = doc_providers
-
-    def get_document_iterator(self, *args, **kwargs):
-        return itertools.chain(*[doc_provider.get_document_iterator(*args, **kwargs) for doc_provider in self.doc_providers])
-
-    def get_total_document_count(self):
-        return sum([dc.get_total_document_count() for dc in self.doc_providers])
