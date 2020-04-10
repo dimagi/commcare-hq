e7a27b15540cfd8aea204a8b13049ee75a2434a1
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-10 07:51:28 +0530

    [fix]: updated the files
    - Github Issue:

--- custom/icds_reports/management/commands/fix_past_data.py
+++ custom/icds_reports/management/commands/fix_past_data.py
@@ -1,5 +1,3 @@
-from corehq.apps.locations.models import SQLLocation
-from custom.icds_reports.tasks import icds_state_aggregation_task
 import os
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -8,7 +6,6 @@ from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
 from corehq.util.argparse_types import date_type
 from django.db import connections, transaction
-from custom.icds_reports.tasks import _get_monthly_dates
 from corehq.sql_db.connections import get_icds_ucr_citus_db_alias
 
 
@@ -30,10 +27,17 @@ class Command(BaseCommand):
             help='The start date (inclusive). format YYYY-MM-DD'
         )
 
-    def build_data(self, date):
-        for i in range(0, 7):
-            path = os.path.join(os.path.dirname(__file__), 'sql_scripts',
-                                'fix_past_data_part_{}.sql'.format(i))
+    def build_data(self, monthly_date):
+        date = monthly_date['record_date']
+        print(f'\n======= Executing for month {date}======\n')
+        for i in range(1, 8):
+            print(f'==============Executing Script {i} =============')
+            if monthly_date["default"] is False:
+                path = os.path.join(os.path.dirname(__file__), 'sql_scripts',
+                                'fix_past_data_part_1_{}.sql'.format(i))
+            else:
+                path = os.path.join(os.path.dirname(__file__), 'sql_scripts',
+                                    'fix_past_data_part_{}.sql'.format(i))
             end_date = date + relativedelta(months=1)
             context = {
                 'start_date': date.strftime("%Y-%m-%d"),
@@ -42,16 +46,29 @@ class Command(BaseCommand):
             with open(path, "r", encoding='utf-8') as sql_file:
                 sql_to_execute = sql_file.read()
                 sql_to_execute = sql_to_execute % context
+                print(sql_to_execute)
                 _run_custom_sql_script(sql_to_execute)
 
     def handle(self, *args, **kwargs):
         date = kwargs['month']
-        date = datetime.strptime(date, "%Y-%m-%d")
         self.run_task(date)
 
+
     def run_task(self, date):
-        initial_date = datetime(2017, 3, 1, 0, 0)
-        intervals = date.month - initial_date.month + 12 * (date.year - initial_date.year) + 1
-        monthly_dates = _get_monthly_dates(date, total_intervals=intervals)
+        monthly_dates = []
+        initial = datetime(2017, 3, 1, 0, 0)
+        initial_2 = datetime(2017, 5, 1, 0, 0)
+        initial_3 = datetime(2019, 8, 1, 0, 0)
+        # date at which the partitioning breaks
+        date_breaks = [initial_2, initial_3]
+        # get the number of month intervals between the dates
+        intervals = date.month - initial.month + 12 * (date.year - initial.year) + 1
+        default = False
+        for i in range(0, intervals):
+            if initial in date_breaks:
+                default = not default
+            monthly_dates.append({"record_date": initial, "default": default})
+            initial = initial + relativedelta(months=1)
+
         for monthly_date in monthly_dates:
             self.build_data(monthly_date)
