9d847e7af8fed937cc56032cc0494a74016a9126
Jennifer Schweers <orange.jenny@gmail.com>
2020-04-10 13:46:17 -0400

    Added domain name and replaced functions that are no longer available

--- corehq/apps/appstore/management/commands/export_exchange.py
+++ corehq/apps/appstore/management/commands/export_exchange.py
@@ -11,30 +11,45 @@ from dimagi.utils.parsing import ISO_DATE_FORMAT, ISO_DATETIME_FORMAT
 
 class Command(BaseCommand):
     help = """
+        Pulls data on domain snapshots (projects in the late CommCare Exchange).
     """
 
     def add_arguments(self, parser):
         parser.add_argument('outfile')
 
     def handle(self, outfile, **options):
-        snapshots = Domain.get_db().view('domain/snapshots', startkey=[], endkey=[{}], reduce=False, include_docs=True,).all()
+        snapshots = Domain.get_db().view(
+            'domain/snapshots',
+            startkey=[],
+            endkey=[{}],
+            reduce=False,
+            include_docs=True,
+        ).all()
         snapshots = [s['doc'] for s in snapshots]
         snapshots = [s for s in snapshots if s['published'] and s['is_snapshot'] and s.get('snapshot_head')]
-        rows = [
-            (
+        rows = []
+        for s in snapshots:
+            domain = Domain.wrap(s)
+            user = None
+            if s['cda']['user_id']:
+                user = CouchUser.get_by_user_id(s['cda']['user_id'])
+            author = s['author']
+            if not author and user:
+                author = user.human_friendly_name
+            row = (
+                domain.copied_from,
                 s['title'],
-                s['author'] or Domain.wrap(s).name_of_publisher,
+                author,
                 s['short_description'],
                 s['project_type'],
-                CouchUser.get_by_user_id(s['cda']['user_id']).username if s['cda']['user_id'] and CouchUser.get_by_user_id(s['cda']['user_id']) else None,
+                user.username if user else None,
                 datetime.strptime(s['snapshot_time'], ISO_DATETIME_FORMAT).strftime('%b %d, %Y'),
-                Domain.wrap(s).readable_languages(),
+                ', '.join(set([lang for app in domain.applications() for lang in app.langs])),
                 s['license'],
             )
-            for s in snapshots
-        ]
+            rows.append(row)
 
         with open(outfile, 'w', encoding='utf-8') as out:
-            writer = csv.writer(out, delimiter="~")
+            writer = csv.writer(out, delimiter="\t")
             for row in rows:
                 writer.writerow(row)
