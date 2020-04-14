ccac9aee26abfb96fdd6569ad5a280fa07be3d08
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-14 17:24:19 +0530

    [refactor]: refactor the file to get the rows for all the three months
    - Github Issue:
    
    Authored-by: Shubham Bansal <shubhambansal17@hotmail.com>
    Signed-off-by: Shubham Bansal <shubhambansal17@hotmail.com>

--- custom/icds_reports/sqldata/exports/growth.py
+++ custom/icds_reports/sqldata/exports/growth.py
@@ -1,175 +1,177 @@
-from sqlagg.base import AliasColumn
-from sqlagg.columns import SimpleColumn
-from sqlagg.filters import EQ, RawFilter, ORFilter, LTE
-from sqlagg.sorting import OrderBy
-
-from corehq.apps.reports.sqlreport import DatabaseColumn
+from corehq.apps.locations.models import SQLLocation
+from custom.icds_reports.models import ChildHealthMonthlyView
 from custom.icds_reports.sqldata.base import IcdsSqlData
 from custom.icds_reports.utils.mixins import ExportableMixin
 from custom.icds_reports.utils import get_status, calculate_date_for_age, \
     current_month_stunting_column, \
     current_month_wasting_column, phone_number_function
+from dateutil import relativedelta
+
 
 class GrowthExport(ExportableMixin, IcdsSqlData):
     title = 'Child Growth Tracking'
     table_name = 'child_health_monthly_view'
 
-    def __int__(self, config=None, loc_level=1, show_test=False, beta=False):
-        config.update({
-            '5_years': 60,
-            'true': 1
-        })
-        super().__init__(config, loc_level, show_test, beta, use_excluded_states=False)
+    def get_excel_data(self, location):
 
-    @property
-    def group_by(self):
-        group_by_columns = self.get_columns_by_loc_level
-        group_by = []
-        for column in group_by_columns:
-            if column.slug != 'current_age':
-                group_by.append(column.slug)
-        return group_by
+        def _check_case_presence(case_id, column, data_dict):
+            return data_dict[case_id][column] if case_id in data_dict.keys() else "N/A"
 
-    def _map_filter_name_to_sql_filter(self, filter_name):
-        return {
-            'severely_underweight': RawFilter("current_month_nutrition_status = 'severely_underweight'"),
-            'moderately_underweight': RawFilter("current_month_nutrition_status = 'moderately_underweight'"),
-            'normal_wfa': RawFilter("current_month_nutrition_status = 'normal'"),
-            'severely_stunted': RawFilter("{} = 'severe'".format(current_month_stunting_column(self.beta))),
-            'moderately_stunted': RawFilter("{} = 'moderate'".format(current_month_stunting_column(self.beta))),
-            'normal_hfa': RawFilter("{} = 'normal'".format(current_month_stunting_column(self.beta))),
-            'severely_wasted': RawFilter("{} = 'severe'".format(current_month_wasting_column(self.beta))),
-            'moderately_wasted': RawFilter("{} = 'moderate'".format(current_month_wasting_column(self.beta))),
-            'normal_wfh': RawFilter("{} = 'normal'".format(current_month_wasting_column(self.beta))),
-        }[filter_name]
+        filters = {
+            "month": self.month,
+            "aggregation_level": 5,
+            "age_in_months__lte": 60,
+            "valid_in_month": 1
+        }
+        initial_month = self.month
 
-    def _build_additional_filters(self, filters):
-        if len(filters) == 1:
-            return self._map_filter_name_to_sql_filter(filter[0])
-        return ORFilter([
-            self._map_filter_name_to_sql_filter(filter_name)
-            for filter_name in filters
-        ])
+        if self.loc_level == 4:
+            filters['supervisor_id'] = location
+            order_by = ('awc_name',)
+        elif self.loc_level == 3:
+            filters['block_id'] = location
+            order_by = ('supervisor_name', 'awc_name')
+        elif self.loc_level == 2:
+            filters['district_id'] = location
+            order_by = ('block_name', 'supervisor_name', 'awc_name')
+        elif self.loc_level == 1:
+            filters['state_id'] = location
+            order_by = ('district_name', 'block_name', 'supervisor_name', 'awc_name')
+        else:
+            order_by = ('state_name', 'district_name', 'block_name', 'supervisor_name', 'awc_name')
 
-    @property
-    def filters(self):
-        filters = [LTE('age_in_months', '5_years'), EQ('valid_in_month', 'true')]
-        for key, value in self.config.items():
-            if key in ['domain', '5_years', 'true']
-                continue
-            elif key == 'filters':
-                filters.append(self._build_additional_filters(value))
-                continue
-            filters.append(EQ(key, key))
-        return filters
+        query_set_1 = ChildHealthMonthlyView.objects.filter(**filters).order_by(*order_by)
+        data_initial_month = query_set_1.values('state_name', 'district_name', 'block_name', 'supervisor_name',
+                                                'awc_name', 'awc_site_code', 'person_name', 'dob', 'mother_name',
+                                                'mother_phone_number', 'pse_days_attended', 'lunch_count',
+                                                'current_month_nutrition_status',
+                                                current_month_wasting_column(self.beta),
+                                                current_month_stunting_column(self.beta), 'case_id')
 
-    @property
-    def order_by(self):
-        return [OrderBy('person_name')]
+        filters["month"] = initial_month - relativedelta(months=1)
+        query_set_2 = ChildHealthMonthlyView.objects.filter(**filters).order_by(*order_by)
+        data_past_month = query_set_2.values('case_id', 'pse_days_attended', 'lunch_count',
+                                             'current_month_nutrition_status',
+                                             current_month_wasting_column(self.beta),
+                                             current_month_stunting_column(self.beta), 'case_id')
+        data_past_month = {item['case_id']: item for item in data_past_month}
 
-    @property
-    def get_columns_by_loc_level(self):
-        selected_month = self.config['month']
+        filters["month"] = initial_month - relativedelta(months=2)
+        query_set_3 = ChildHealthMonthlyView.objects.filter(**filters).order_by(*order_by)
+        data_past_month_2 = query_set_3.values('case_id', 'pse_days_attended', 'lunch_count',
+                                               'current_month_nutrition_status',
+                                               current_month_wasting_column(self.beta),
+                                               current_month_stunting_column(self.beta), 'case_id')
+        data_past_month_2 = {item['case_id']: item for item in data_past_month_2}
+        filters["month"] = initial_month
 
-        columns = [
-            DatabaseColumn(
-                'State',
-                SimpleColumn('state_name'),
-                slug='state_name'
-            ),
-            DatabaseColumn(
-                'District',
-                SimpleColumn('district_name'),
-                slug='district_name'
-            ),
-            DatabaseColumn(
-                'Block',
-                SimpleColumn('block_name'),
-                slug='block_name'
-            ),
-            DatabaseColumn(
-                'Sector',
-                SimpleColumn('supervisor_name'),
-                slug='supervisor_name'
-            ),
-            DatabaseColumn(
-                'AWC Name',
-                SimpleColumn('awc_name'),
-                slug='awc_name'
-            ),
-            DatabaseColumn(
-                'AWC Site Code',
-                SimpleColumn('awc_site_code'),
-                slug='awc_site_code'
-            ),
-            DatabaseColumn(
-                'Child Name',
-                SimpleColumn('person_name'),
-                slug='person_name'
-            ),
-            DatabaseColumn(
-                'Current Age (as of {})'.format(selected_month.isoformat()),
-                AliasColumn('dob'),
-                format_fn=lambda x: calculate_date_for_age(x, self.config['month']),
-                slug='current_age'
-            ),
-            DatabaseColumn(
-                'Mother name',
-                SimpleColumn('mother_name'),
-                slug='mother_name'
-            ),
-            DatabaseColumn(
-                'Mother Phone Number',
-                SimpleColumn('mother_phone_number'),
-                format_fn=phone_number_function,
-                slug='mother_phone_number'
-            )
-            DatabaseColumn(
-                'Days attended PSE (as of {})'.format(selected_month.isoformat()),
-                SimpleColumn('pse_days_attended'),
-                slug="pse_days_attended"
-            ),
-            DatabaseColumn(
-                'Rations Distributed (as of {})'.format(selected_month.isoformat()),
-                SimpleColumn('num_rations_distributed'),
-                slug="num_rations_distributed"
-            ),
-            DatabaseColumn(
-                'Weight-for-Age Status (in Month)',
-                SimpleColumn('current_month_nutrition_status'),
-                format_fn=lambda x: get_status(
-                    x,
-                    'underweight',
-                    'Normal weight for age',
-                    True
-                ),
-                slug='current_month_nutrition_status'
-            ),
-            DatabaseColumn(
-                'Weight-for-Height Status (in Month)',
-                SimpleColumn(current_month_wasting_column(self.beta)),
-                format_fn=lambda x: get_status(
-                    x,
-                    'wasted',
-                    'Normal weight for height',
-                    True
-                ),
-                slug=current_month_wasting_column(self.beta)
-            ),
-            DatabaseColumn(
-                'Height-for-Age status (in Month)',
-                SimpleColumn(current_month_stunting_column(self.beta)),
-                format_fn=lambda x: get_status(
-                    x,
+        month_1 = (initial_month - relativedelta(months=2)).strftime('%Y-%m-%d')
+        month_2 = (initial_month - relativedelta(months=1)).strftime('%Y-%m-%d')
+        month_3 = initial_month.strftime('%Y-%m-%d')
+
+        headers = ['State', 'District', 'Block', 'Sector', 'Awc Name', 'Child', 'Age', 'Mother Name',
+                   'Mother Phone Number', f'PSE_{month_1}', f'PSE_{month_2}', f'PSE_{month_3}', f'SN_{month_1}',
+                   f'SN_{month_2}', f'SN_{month_3}', f'Stunting_{month_1}', f'Stunting_{month_2}',
+                   f'Stunting_{month_3}', f'Wasting_month_1', f'Wasting_{month_2}', f'Wasting_{month_3}',
+                   f'underweight_{month_1}', f'underweight_{month_2}', f'underweight_{month_3}']
+
+        excel_rows = [headers]
+
+        for row in data_initial_month:
+            row_data = [
+                row['state_name'],
+                row['district_name'],
+                row['block_name'],
+                row['supervisor_name'],
+                row['awc_name'],
+                row['person_name'],
+                calculate_date_for_age(row['dob'], initial_month),
+                row['mother_name'],
+                phone_number_function(row['mother_phone_number']),
+                _check_case_presence(row['case_id'], 'pse_days_attended', data_past_month_2),
+                _check_case_presence(row['case_id'], 'pse_days_attended', data_past_month),
+                row['pse_days_attended'],
+                _check_case_presence(row['case_id'], 'lunch_count', data_past_month_2),
+                _check_case_presence(row['case_id'], 'lunch_count', data_past_month),
+                row['lunch_count'],
+                get_status(
+                    _check_case_presence(row['case_id'], current_month_stunting_column(self.beta),
+                                         data_past_month_2),
                     'stunted',
                     'Normal height for age',
                     True
                 ),
-                slug=current_month_stunting_column(self.beta)
-            )
-        ]
-        return columns
+                get_status(
+                    _check_case_presence(row['case_id'], current_month_stunting_column(self.beta),
+                                         data_past_month),
+                    'stunted',
+                    'Normal height for age',
+                    True
+                ),
+                get_status(
+                    row[current_month_stunting_column(self.beta)],
+                    'stunted',
+                    'Normal height for age',
+                    True
+                ),
+                get_status(
+                    _check_case_presence(row['case_id'], current_month_wasting_column(self.beta),
+                                         data_past_month_2),
+                    'wasted',
+                    'Normal weight for height',
+                    True
+                ),
+                get_status(
+                    _check_case_presence(row['case_id'], current_month_wasting_column(self.beta), data_past_month),
+                    'wasted',
+                    'Normal weight for height',
+                    True
+                ),
+                get_status(
+                    row[current_month_wasting_column(self.beta)],
+                    'wasted',
+                    'Normal weight for height',
+                    True
+                ),
+                get_status(
+                    _check_case_presence(row['case_id'], 'current_month_nutrition_status', data_past_month_2),
+                    'underweight',
+                    'Normal weight for age',
+                    True
+                ),
+                get_status(
+                    _check_case_presence(row['case_id'], 'current_month_nutrition_status', data_past_month),
+                    'underweight',
+                    'Normal weight for age',
+                    True
+                ),
+                get_status(
+                    row['current_month_nutrition_status'],
+                    'underweight',
+                    'Normal weight for age',
+                    True
+                )
+            ]
+            excel_rows.append(row_data)
+        filters = [['Generated at', india_now()]]
+        if location:
+            locs = SQLLocation.objects.get(location_id=location).get_ancestors(include_self=True)
+            for loc in locs:
+                filters.append([loc.location_type.name.title(), loc.name])
+        else:
+            filters.append(['Location', 'National'])
 
-    @property
-    def columns(self):
-        return self.get_columns_by_loc_level
+        date = self.month
+        filters.append(['Month', date.strftime("%B")])
+        filters.append(['Year', date.year])
+
+        return [
+            [
+                self.title,
+                excel_rows
+            ],
+            [
+                'Export Info',
+                filters
+            ]
+        ]
