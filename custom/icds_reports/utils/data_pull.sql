838bd945d00efb5a10924e281a69db0efe3f37e5
Shubham <shubhambansal17@hotmail.com>
2020-04-15 19:49:52 +0530

    Update data_pull.sql

--- custom/icds_reports/utils/data_pull.sql
+++ custom/icds_reports/utils/data_pull.sql
@@ -36,7 +36,7 @@ SELECT
     awc.num_launched_awcs as total_awcs,
     ucr.unique_visits as visited_awcs,
     CASE
-        WHEN total_awcs > visited_awcs THEN 'NO'
+        WHEN awc.num_launched_awcs > ucr.unique_visits THEN 'NO'
         ELSE 'YES'
     END as all_visited
     FROM "agg_awc_2020-03-01_4" awc
@@ -46,5 +46,18 @@ SELECT
         ON (t.supervisor_id = awc.supervisor_id
         AND t.aggregation_level=awc.aggregation_level
         AND t.aggregation_level=4);
+--  QUERY PLAN
+-- ------------------------------------------------------------------------------------------------------------------------------------
+--  Hash Right Join  (cost=4165.52..17406.48 rows=26824 width=83)
+--    Hash Cond: ((t.supervisor_id = awc.supervisor_id) AND (t.aggregation_level = awc.aggregation_level))
+--    ->  Index Scan using awc_location_local_aggregation_level_idx on awc_location_local t  (cost=0.42..12008.87 rows=27173 width=75)
+--          Index Cond: (aggregation_level = 4)
+--    ->  Hash  (cost=3500.74..3500.74 rows=26824 width=49)
+--          ->  Hash Right Join  (cost=2508.54..3500.74 rows=26824 width=49)
+--                Hash Cond: (ucr.supervisor_id = awc.supervisor_id)
+--                ->  Seq Scan on temp_visit_table ucr  (cost=0.00..290.40 rows=15840 width=40)
+--                ->  Hash  (cost=1937.24..1937.24 rows=26824 width=41)
+--                      ->  Seq Scan on "agg_awc_2020-03-01_4" awc  (cost=0.00..1937.24 rows=26824 width=41)
+-- (10 rows)
  
 DROP TABLE IF EXISTS temp_visit_table;
