7ed7549bb682cb060da23ad23cde70e95c19f1d8
challa <challarao@beehyv.com>
2020-04-16 11:24:09 +0530

    lint changes

--- custom/icds_reports/migrations/0178_rebuild_chm_view.py
+++ custom/icds_reports/migrations/0178_rebuild_chm_view.py
@@ -4,8 +4,6 @@ from __future__ import unicode_literals
 from django.db import migrations
 
 from corehq.sql_db.operations import RawSQLMigration
-from custom.icds_reports.utils.migrations import get_view_migrations
-
 
 migrator = RawSQLMigration(('custom', 'icds_reports', 'migrations', 'sql_templates', 'database_views'))
 
