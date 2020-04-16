589ba8d63ff8df95ed41a1326f1f457d3a4b0519
Rohit Negi <rohit25.negi@gmail.com>
2020-04-16 17:50:08 +0530

    added the data fetching function

--- custom/icds_reports/reports/bihar_api.py
+++ custom/icds_reports/reports/bihar_api.py
@@ -1,4 +1,5 @@
 from custom.icds_reports.models.views import BiharDemographicsView
+from custom.icds_reports.models.aggregate import BiharAPIMotherDetails
 from custom.icds_reports.const import CAS_API_PAGE_SIZE
 from custom.icds_reports.cache import icds_quickcache
 
@@ -6,9 +7,13 @@ from custom.icds_reports.cache import icds_quickcache
 # cache for 2 hours because it wont change atleast for 24 hours.
 # This API will be hit in a loop of something and they should be able to scrape all
 # the records in 2 hours.
-@icds_quickcache(['month', 'state_id'], timeout=60 * 60 * 2)
-def get_total_records_count(month, state_id):
-    return BiharDemographicsView.objects.filter(
+@icds_quickcache(['model_classname', 'month', 'state_id'], timeout=60 * 60 * 2)
+def get_total_records_count(model_classname, month, state_id):
+    classes = {
+        BiharDemographicsView.__name__: BiharDemographicsView,
+        BiharAPIMotherDetails.__name__: BiharAPIMotherDetails
+    }
+    return classes[model_classname].objects.filter(
         month=month,
         state_id=state_id
     ).count()
@@ -68,4 +73,30 @@ def get_api_demographics_data(month, state_id, last_person_case_id):
 
     # To apply pagination on database query with data size length
     limited_demographics_data = list(demographics_data_query[:CAS_API_PAGE_SIZE])
-    return limited_demographics_data,  get_total_records_count(month, state_id)
+    return limited_demographics_data,  get_total_records_count(BiharDemographicsView.__name__, month, state_id)
+
+
+def get_mother_details(month, state_id, last_ccs_case_id):
+    bihar_mother_details = BiharAPIMotherDetails.objects.filter(
+        month=month,
+        state_id=state_id,
+        ccs_case_id__gt=last_ccs_case_id
+    ).order_by('person_id').values(
+        'household_id',
+        'ccs_case_id',
+        'person_id',
+        'married',
+        'husband_name',
+        'husband_id',
+        'last_preg_year',
+        'last_preg_tt',
+        'is_pregnant',
+        'preg_reg_date',
+        'tt_1',
+        'tt_2',
+        'tt_booster',
+        'add',
+        'hb'
+    )
+    limited_mother_details_data = list(bihar_mother_details[:CAS_API_PAGE_SIZE])
+    return limited_mother_details_data, get_total_records_count(BiharAPIMotherDetails.__name__, month, state_id)
