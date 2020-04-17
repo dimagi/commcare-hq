a5b6f6aa8aaeb1b22148fce6cb7500ff012fed6b
Farid Rener <proteusvacuum@gmail.com>
2020-04-17 09:09:30 -0400

    Add template string parsing

--- corehq/messaging/smsbackends/turn/models.py
+++ corehq/messaging/smsbackends/turn/models.py
@@ -1,19 +1,23 @@
+from collections import namedtuple
+
 from corehq.apps.sms.models import SQLSMSBackend
 from corehq.apps.sms.util import clean_phone_number
+from corehq.messaging.smsbackends.turn.exceptions import WhatsAppTemplateStringException
 from corehq.messaging.smsbackends.turn.forms import TurnBackendForm
-from turn import TurnClient, TurnBusinessManagementClient
+from turn import TurnBusinessManagementClient, TurnClient
 from turn.exceptions import WhatsAppContactNotFound
+
+WA_TEMPLATE_STRING = "cc_wa_template"
 
 
 class SQLTurnWhatsAppBackend(SQLSMSBackend):
-
     class Meta(object):
         proxy = True
-        app_label = 'sms'
+        app_label = "sms"
 
     @classmethod
     def get_api_id(cls):
-        return 'TURN'
+        return "TURN"
 
     @classmethod
     def get_generic_name(cls):
@@ -22,9 +26,9 @@ class SQLTurnWhatsAppBackend(SQLSMSBackend):
     @classmethod
     def get_available_extra_fields(cls):
         return [
-            'client_auth_token',
-            'business_id',
-            'business_auth_token',
+            "client_auth_token",
+            "business_id",
+            "business_auth_token",
         ]
 
     @classmethod
@@ -39,14 +43,33 @@ class SQLTurnWhatsAppBackend(SQLSMSBackend):
         try:
             wa_id = client.contacts.get_whatsapp_id(to)
         except WhatsAppContactNotFound:
-            pass                # TODO: Fallback to SMS?
+            pass  # TODO: Fallback to SMS?
 
         try:
             message = client.messages.send_text(wa_id, msg.text)
-        except:                 # TODO: Add message exceptions to package
+        except:  # TODO: Add message exceptions to package
             raise
 
     def get_all_templates(self):
         config = self.config
         client = TurnBusinessManagementClient(config.business_id, config.business_auth_token)
         return client.message_templates.get_message_templates()
+
+
+def is_whatsapp_template_message(message_text):
+    return message_text.lower().startswith(WA_TEMPLATE_STRING)
+
+
+def get_template_hsm_parts(message_text):
+    HsmParts = namedtuple("hsm_parts", "template_name lang_code variables")
+    parts = message_text.split("~")[0].split(":")
+
+    try:
+        variables = [p.strip() for p in parts[3].split(",")]
+    except IndexError:
+        variables = []
+
+    try:
+        return HsmParts(template_name=parts[1], lang_code=parts[2], variables=variables,)
+    except IndexError:
+        raise WhatsAppTemplateStringException
