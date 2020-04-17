cd6e35530c0b0f22d008d25c718d7827ce74dea4
Sravan Reddy <sravfeyn@gmail.com>
2020-04-17 15:50:24 +0530

    space

--- custom/icds_reports/tasks.py
+++ custom/icds_reports/tasks.py
@@ -1755,7 +1755,7 @@ def get_valid_data_not_in_ucr(status_record):
             if doc_id in doc_id_and_inserted_in_ucr:
                 # This is to handle the cases which are outdated. This condition also handles the time drift of 1 sec
                 # between main db and ucr db. i.e  doc will even be included when inserted_at-sql_modified_on <= 1 sec
-                if sql_modified_on - doc_id_and_inserted_in_ucr[doc_id] >= timedelta(seconds=-1) :
+                if sql_modified_on - doc_id_and_inserted_in_ucr[doc_id] >= timedelta(seconds=-1):
                     yield (doc_id, doc_subtype, sql_modified_on.isoformat())
             else:
                 yield (doc_id, doc_subtype, sql_modified_on.isoformat())
