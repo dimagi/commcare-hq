32c9289aa6504ba557a114fe7c7949fe94df88a3
Cory Zue <czue@dimagi.com>
2020-04-21 08:17:51 +0200

    continue to return None for non-boolean values

--- corehq/apps/user_importer/helpers.py
+++ corehq/apps/user_importer/helpers.py
@@ -5,4 +5,7 @@ def spec_value_to_boolean_or_none(user_spec_dict, key):
     value = user_spec_dict.get(key, None)
     if value and isinstance(value, str):
         return string_to_boolean(value)
-    return value
+    elif isinstance(value, bool):
+        return value
+    else:
+        return None
