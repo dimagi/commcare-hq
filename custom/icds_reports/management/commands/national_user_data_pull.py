a7c07b608695a8a55e5616a8168e4b3753fc1e68
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-22 16:10:04 +0530

    [fix]: adding webusers also
    - Github Issue:

--- custom/icds_reports/management/commands/national_user_data_pull.py
+++ custom/icds_reports/management/commands/national_user_data_pull.py
@@ -1,7 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
-from corehq.apps.users.models import CommCareUser
+from corehq.apps.users.models import CommCareUser, WebUser
 from custom.icds_reports.const import INDIA_TIMEZONE
 from custom.icds_reports.models import ICDSAuditEntryRecord
 from django.db.models import Max
@@ -20,18 +20,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         users = CommCareUser.by_domain('icds-cas')
+        print(f"==========={len(users)} Commcare Users======\n")
+        web_users = WebUser.by_domain('icds-cas')
+        print(f"==========={len(users)} Web Users======\n")
         usernames = []
         for user in users:
-            if user.has_permission('icds-cas', 'access_all_locations'):
+            if ((len(user.assigned_location_ids)==0 or 'cpmu' in user.username.tolower() ) and user.has_permission('icds-cas', 'access_all_locations')):
                 usernames.append(user.username)
+        for user in web_users:
+            if ((len(user.assigned_location_ids)==0 or 'cpmu' in user.username.tolower() ) and user.has_permission('icds-cas', 'access_all_locations')):
+                usernames.append(user.username)
+        usernames = list(set(usernames))
+        print(f"==========={len(usernames)} Unique national users======\n")
         chunk_size = 100
         headers = ["username", "time"]
         rows = [headers]
         usernames_usage = []
         for user_chunk in chunked(usernames, chunk_size):
             usage_data = ICDSAuditEntryRecord.objects.filter(username__in=list(user_chunk)).values('username').annotate(time=Max('time_of_use'))
+            print(f"===========Executing chunk of {len(usage_data)}======\n")
             for usage in usage_data:
-                print(usage)
                 row_data = [
                     usage['username'],
                     self.convert_to_ist(usage['time'])
@@ -40,6 +48,7 @@ class Command(BaseCommand):
                 rows.append(row_data)
         users_not_logged_in = set(usernames) - set(usernames_usage)
         rows.extend([[user, 'N/A'] for user in users_not_logged_in])
+        print(f"===========Total rows of {len(rows)}======\n")
         fout = open('/home/cchq/National_users_data.csv', 'w')
         writer = csv.writer(fout)
         writer.writerows(rows)
