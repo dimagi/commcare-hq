968071ffac964e7005a192f7d58feea5878182a1
Shubham Bansal <shubhambansal17@hotmail.com>
2020-04-28 15:06:02 +0530

    [fix]: update the queries

--- custom/icds_reports/management/commands/sql_scripts/district_data_pull_1.sql
+++ custom/icds_reports/management/commands/sql_scripts/district_data_pull_1.sql
@@ -20,4 +20,25 @@ COPY(SELECT
         awc.district_id = agg.district_id AND
         awc.aggregation_level = agg.aggregation_level
     ) WHERE agg.aggregation_level = 2
-    AND agg.month in ('{month_1}', '{month_2}', '{month_3}') ) TO '/tmp/%(name)s/district_data_pull_1.csv' DELIMITER ',' CSV HEADER
+    AND agg.month in ('{month_1}', '{month_2}', '{month_3}') ) TO '/tmp/%(name)s/district_data_pull_2.csv' DELIMITER ',' CSV HEADER
+
+--   QUERY PLAN
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+--  Merge Left Join  (cost=303.22..341.88 rows=766 width=133)
+--    Merge Cond: (agg.district_id = awc.district_id)
+--    Join Filter: (awc.aggregation_level = agg.aggregation_level)
+--    ->  Sort  (cost=123.96..125.87 rows=766 width=90)
+--          Sort Key: agg.district_id
+--          ->  Append  (cost=0.00..87.26 rows=766 width=90)
+--                ->  Seq Scan on agg_awc agg  (cost=0.00..0.00 rows=1 width=88)
+--                      Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[])))
+--                ->  Seq Scan on "agg_awc_2019-04-01_2" agg_1  (cost=0.00..24.80 rows=234 width=90)
+--                      Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[])))
+--                ->  Seq Scan on "agg_awc_2019-05-01_2" agg_2  (cost=0.00..28.19 rows=258 width=90)
+--                      Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[])))
+--                ->  Seq Scan on "agg_awc_2019-06-01_2" agg_3  (cost=0.00..30.44 rows=273 width=90)
+--                      Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-04-01,2019-05-01,2019-06-01}'::date[])))
+--    ->  Sort  (cost=179.27..180.18 rows=367 width=55)
+--          Sort Key: awc.district_id
+--          ->  Index Scan using awc_location_local_aggregation_level_idx on awc_location_local awc  (cost=0.42..163.63 rows=367 width=55)
+--                Index Cond: (aggregation_level = 2)
--- custom/icds_reports/management/commands/sql_scripts/district_data_pull_2.sql
+++ custom/icds_reports/management/commands/sql_scripts/district_data_pull_2.sql
@@ -3,33 +3,39 @@ COPY(SELECT
     awc.state_name,
     agg.state_id,
     agg.district_id,
-    CASE WHEN agg.month='{month_1}' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_{column_1},
-    CASE WHEN agg.month='{month_2}' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_{column_2},
-    CASE WHEN agg.month='{month_3}' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_{column_3},
-    CASE WHEN agg.month='{month_1}' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_{column_1},
-    CASE WHEN agg.month='{month_2}' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_{column_2},
-    CASE WHEN agg.month='{month_3}' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_{column_3}
+    SUM(CASE WHEN agg.month='{month_1}' THEN agg.cf_initiation_in_month ELSE 0 END) as cf_initiation_in_month_{column_1},
+    SUM(CASE WHEN agg.month='{month_2}' THEN agg.cf_initiation_in_month ELSE 0 END) as cf_initiation_in_month_{column_2},
+    SUM(CASE WHEN agg.month='{month_3}' THEN agg.cf_initiation_in_month ELSE 0 END) as cf_initiation_in_month_{column_3},
+    SUM(CASE WHEN agg.month='{month_1}' THEN agg.cf_initiation_eligible ELSE 0 END) as cf_initiation_eligible_{column_1},
+    SUM(CASE WHEN agg.month='{month_2}' THEN agg.cf_initiation_eligible ELSE 0 END) as cf_initiation_eligible_{column_2},
+    SUM(CASE WHEN agg.month='{month_3}' THEN agg.cf_initiation_eligible ELSE 0 END) as cf_initiation_eligible_{column_3}
     FROM agg_child_health agg
     LEFT OUTER JOIN awc_location_local awc ON (
         awc.district_id = agg.district_id AND
         awc.aggregation_level = agg.aggregation_level
     ) WHERE agg.aggregation_level = 2
-    AND agg.month in ('{month_1}', '{month_2}', '{month_3}') ) TO '/tmp/%(name)s/district_data_pull_2.csv' DELIMITER ',' CSV HEADER
+    AND agg.month in ('{month_1}', '{month_2}', '{month_3}') GROUP BY agg.district_id, awc.district_name, awc.state_name) TO '/tmp/%(name)s/district_data_pull_2.csv' DELIMITER ',' CSV HEADER
 
-SELECT
-    awc.district_name,
-    awc.state_name,
-    agg.state_id,
-    agg.district_id,
-    CASE WHEN agg.month='2019-03-01' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_column_1,
-    CASE WHEN agg.month='2019-04-01' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_column_2,
-    CASE WHEN agg.month='2019-05-01' THEN agg.cf_initiation_in_month ELSE 0 END as cf_initiation_in_month_column_3,
-    CASE WHEN agg.month='2019-03-01' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_column_1,
-    CASE WHEN agg.month='2019-04-01' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_column_2,
-    CASE WHEN agg.month='2019-05-01' THEN agg.cf_initiation_eligible ELSE 0 END as cf_initiation_eligible_column_3
-    FROM agg_child_health agg
-    LEFT OUTER JOIN awc_location_local awc ON (
-        awc.district_id = agg.district_id AND
-        awc.aggregation_level = agg.aggregation_level
-    ) WHERE agg.aggregation_level = 2
-    AND agg.month in ('2019-03-01', '2019-04-01', '2019-05-01')
+
+-- QUERY PLAN
+-- ----------------------------------------------------------------------------------------------------------------------------------------------
+--  HashAggregate  (cost=1951.59..2033.17 rows=8158 width=100)
+--    Group Key: agg.district_id, awc.district_name, awc.state_name
+--    ->  Merge Left Join  (cost=1478.63..1645.67 rows=8158 width=64)
+--          Merge Cond: (agg.district_id = awc.district_id)
+--          Join Filter: (awc.aggregation_level = agg.aggregation_level)
+--          ->  Sort  (cost=1299.37..1319.76 rows=8158 width=49)
+--                Sort Key: agg.district_id
+--                ->  Append  (cost=0.00..769.34 rows=8158 width=49)
+--                      ->  Seq Scan on agg_child_health agg  (cost=0.00..0.00 rows=1 width=48)
+--                            Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-03-01,2019-04-01,2019-05-01}'::date[])))
+--                      ->  Seq Scan on "agg_child_health_2019-04-01_2" agg_1  (cost=0.00..198.67 rows=2441 width=49)
+--                            Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-03-01,2019-04-01,2019-05-01}'::date[])))
+--                      ->  Seq Scan on "agg_child_health_2019-03-01_2" agg_2  (cost=0.00..247.79 rows=2264 width=49)
+--                            Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-03-01,2019-04-01,2019-05-01}'::date[])))
+--                      ->  Seq Scan on "agg_child_health_2019-05-01_2" agg_3  (cost=0.00..282.10 rows=3452 width=49)
+--                            Filter: ((aggregation_level = 2) AND (month = ANY ('{2019-03-01,2019-04-01,2019-05-01}'::date[])))
+--          ->  Sort  (cost=179.27..180.18 rows=367 width=55)
+--                Sort Key: awc.district_id
+--                ->  Index Scan using awc_location_local_aggregation_level_idx on awc_location_local awc  (cost=0.42..163.63 rows=367 width=55)
+--                      Index Cond: (aggregation_level = 2)
