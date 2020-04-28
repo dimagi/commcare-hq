75a493de5ecdad50cd569a10369fa8d7ac62f1e0
Ethan Soergel <esoergel@gmail.com>
2020-04-28 17:45:38 -0400

    Switch case owner filter to ES-backed multiselect

--- custom/inddex/ucr_data.py
+++ custom/inddex/ucr_data.py
@@ -1,11 +1,13 @@
 from sqlagg.columns import SimpleColumn
-from sqlagg.filters import EQ, GTE, LT, LTE
+from sqlagg.filters import EQ, GTE, IN, LT, LTE
 
 from corehq.apps.reports.sqlreport import DatabaseColumn, SqlData
+from corehq.apps.reports.util import get_INFilter_bindparams
 from corehq.apps.userreports.util import get_table_name
 from corehq.sql_db.connections import UCR_ENGINE_ID
+from custom.utils.utils import clean_IN_filter_value
 
-from .const import FOOD_CONSUMPTION, AGE_RANGES
+from .const import AGE_RANGES, FOOD_CONSUMPTION
 
 
 class FoodCaseData(SqlData):
@@ -15,7 +17,7 @@ class FoodCaseData(SqlData):
     FILTERABLE_COLUMNS = [  # columns easily filtered by exact match
         'breastfeeding',
         'gender',
-        'owner_name',
+        'owner_id',
         'pregnant',
         'recall_status',
         'supplements'
@@ -39,6 +41,9 @@ class FoodCaseData(SqlData):
             if self.config.get(column):
                 if column == 'age_range':
                     filters.append(self._age_range_filter)
+                elif column == 'owner_id':
+                    infilter_bindparams = get_INFilter_bindparams('owner_id', self.config['owner_id'])
+                    filters.append(IN('owner_id', infilter_bindparams))
                 else:
                     filters.append(EQ(column, column))
         return filters
@@ -48,3 +53,7 @@ class FoodCaseData(SqlData):
         age_range = {age_range.slug: age_range for age_range in AGE_RANGES}[self.config['age_range']]
         return [GTE(age_range.column, age_range.lower_bound),
                 LT(age_range.column, age_range.upper_bound)]
+
+    @property
+    def filter_values(self):
+        return clean_IN_filter_value(super().filter_values, 'owner_id')
