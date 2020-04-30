542c1feb18d5e0b1e87aeb2c4a6b9c7e3cd8ea4e
Cal Ellowitz <calellowitz@gmail.com>
2020-04-30 14:29:58 -0400

    add help texts

--- corehq/apps/userreports/management/commands/queue_indicators.py
+++ corehq/apps/userreports/management/commands/queue_indicators.py
@@ -7,11 +7,14 @@ from corehq.apps.userreports.tasks import _queue_indicators
 
 
 class Command(BaseCommand):
-
+    help = "Queue all of the docs in the async indicators table for a given datasource"
+    
     def add_arguments(self, parser):
-        parser.add_argument('data_source_ids')
-        parser.add_argument('--agg-queue', dest='use_agg_queue', action='store_true')
+        parser.add_argument('data_source_id' help='id of data source to queue')
+        parser.add_argument('--agg-queue', dest='use_agg_queue', action='store_true'
+                            help='Use the aggregation queue instead of the async UCR queue to avoid competing with normal async UCR processing'
+        )
 
     def handle(self, data_source_ids, use_agg_queue, **options):
-        indicators = AsyncIndicator.objects.filter(indicator_config_ids__contains=[data_source_ids])
+        indicators = AsyncIndicator.objects.filter(indicator_config_ids__contains=[data_source_id])
         _queue_indicators(indicators, use_agg_queue=use_agg_queue)
