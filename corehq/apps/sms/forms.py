25d6da136f976a8286de10c4e73397da5020a6d5
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-03 21:25:02 -0400

    Renamed sql models (PR3)

--- corehq/apps/sms/forms.py
+++ corehq/apps/sms/forms.py
@@ -23,7 +23,7 @@ from dimagi.utils.django.fields import TrimmedCharField
 from corehq import toggles
 from corehq.apps.app_manager.dbaccessors import get_built_app_ids
 from corehq.apps.app_manager.models import Application
-from corehq.apps.commtrack.models import SQLAlertConfig
+from corehq.apps.commtrack.models import AlertConfig
 from corehq.apps.domain.models import DayTimeWindow
 from corehq.apps.groups.models import Group
 from corehq.apps.hqwebapp import crispy as hqcrispy
@@ -1349,7 +1349,7 @@ class SubscribeSMSForm(Form):
 
     def save(self, commtrack_settings):
         if not hasattr(commtrack_settings, 'sqlalertconfig'):
-            commtrack_settings.sqlalertconfig = SQLAlertConfig()
+            commtrack_settings.sqlalertconfig = AlertConfig()
         alert_config = commtrack_settings.sqlalertconfig
         alert_config.stock_out_facilities = self.cleaned_data.get("stock_out_facilities", False)
         alert_config.stock_out_commodities = self.cleaned_data.get("stock_out_commodities", False)
