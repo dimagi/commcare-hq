e91ee6748bca1d48b7314d80462b5765e973bf71
Simon Kelly <skelly@dimagi.com>
2020-05-04 14:49:15 +0200

    clean up prometheus metrics after pillow exists

--- corehq/ex-submodules/pillowtop/run_pillowtop.py
+++ corehq/ex-submodules/pillowtop/run_pillowtop.py
@@ -1,6 +1,13 @@
-import sys
-from pillowtop import get_all_pillow_instances
+import functools
+import glob
 import multiprocessing
+import os
+import signal
+import sys
+
+from pillowtop import get_all_pillow_instances
+
+from corehq.util.signals import SignalHandlerContext
 
 
 def _do_run_pillow(pillow_class):
@@ -17,24 +24,46 @@ def start_pillows(pillows=None):
     """
     run_pillows = pillows or get_all_pillow_instances()
 
-    try:
-        while True:
-            jobs = []
-            print("[pillowtop] Starting pillow processes")
-            for pillow_class in run_pillows:
-                p = multiprocessing.Process(target=pillow_class.run)
-                p.start()
-                jobs.append(p)
-            print("[pillowtop] all processes started, pids: %s" % ([x.pid for x in jobs]))
-            for j in jobs:
-                j.join()
-            print("[pillowtop] All processes complete, restarting")
-    except KeyboardInterrupt:
-        sys.exit()
+    while True:
+        pids = []
+        handler = functools.partial(_remove_prometheus_metric_files, pids)
+        with SignalHandlerContext([signal.SIGINT, signal.SIGTERM, signal.SIGHUP], handler):
+            try:
+                print("[pillowtop] Starting pillow processes")
+                jobs = []
+                for pillow_class in run_pillows:
+                    p = multiprocessing.Process(target=pillow_class.run)
+                    p.start()
+                    jobs.append(p)
+
+                pids.extend(x.pid for x in jobs)
+                print(f"[pillowtop] all processes started, pids: {pids}")
+
+                for j in jobs:
+                    j.join()
+            finally:
+                handler()
+
+        print("[pillowtop] All processes complete, restarting")
 
 
 def start_pillow(pillow_instance):
-    while True:
-        print("Starting pillow %s.run()" % pillow_instance.__class__)
-        pillow_instance.run()
-        print("Pillow %s.run() completed, restarting" % pillow_instance.__class__)
+    handler = functools.partial(_remove_prometheus_metric_files, [os.getpid()])
+    with SignalHandlerContext([signal.SIGINT, signal.SIGTERM, signal.SIGHUP], handler):
+        try:
+            while True:
+                print("Starting pillow %s.run()" % pillow_instance.__class__)
+                pillow_instance.run()
+                print("Pillow %s.run() completed, restarting" % pillow_instance.__class__)
+        finally:
+            handler()
+
+
+def _remove_prometheus_metric_files(pids):
+    path = os.environ.get('prometheus_multiproc_dir')
+    if not path:
+        return
+
+    for pid in pids:
+        for file in glob.glob(os.path.join(path, '*_{}.db'.format(pid))):
+            os.remove(file)
