4f0302ba795d66e7d0d2774d09b3e6842fe69502
Shubham Bansal <shubhambansal17@hotmail.com>
2020-05-05 18:21:01 +0530

    [fix]: updated the code for merging in the code base

--- custom/icds_reports/management/commands/national_user_data_pull.py
+++ custom/icds_reports/management/commands/national_user_data_pull.py
@@ -9,53 +9,62 @@ from dimagi.utils.chunked import chunked
 
 
 class Command(BaseCommand):
-    help = "Custom data pull"
+    help = "List of users with national level access on Dashboard along with usage"
 
     def convert_to_ist(self, date):
         if date is None:
             return 'N/A'
         date = date.astimezone(INDIA_TIMEZONE)
-        date_formatted = date.strftime("%d/%m/%Y, %I:%M %p")
+        date_formatted = date.strftime("%d-%m-%Y")
         return date_formatted
 
-    def _get_details(self, users, usernames, users_created_time):
+    def _get_details(self, users, usernames, user_details):
+        """
+        :param users: user objects of all commcare and web users
+        :param usernames: username array eg. [username1, username2]
+        :param user_details: details dict contains created and role name eg.{username: [created_on, role_name]}
+        :return: usernames and user_details dict
+        """
         for user in users:
             role = user.get_role('icds-cas')
             if (role in ('CPMU','Dashboard Only Access', 'TRP') or len(user.assigned_location_ids)==0) and user.has_permission('icds-cas', 'access_all_locations'):
                 usernames.append(user.username)
-                users_created_time.update({ user.username: user.created_on })
-        return usernames, users_created_time
+                user_details.update({ user.username: [ user.created_on, role.name ]})
+        return usernames, user_details
 
 
     def handle(self, *args, **options):
         users = CommCareUser.by_domain('icds-cas')
-        print(f"==========={len(users)} Commcare Users======\n")
         web_users = WebUser.by_domain('icds-cas')
-        print(f"==========={len(users)} Web Users======\n")
         usernames = []
-        users_created_time = {}
-        usernames, users_created_time = self._get_details(users, usernames, users_created_time)
-        usernames, users_created_time = self._get_details(web_users, usernames, users_created_time)
+        user_details = {}
+        usernames, user_details = self._get_details(users, usernames, user_details)
+        usernames, user_details = self._get_details(web_users, usernames, user_details)
         usernames = list(set(usernames))
-        print(f"==========={len(usernames)} Unique national users======\n")
         chunk_size = 100
-        headers = ["username", "last_access_time", "created_on"]
+        headers = ["username", "last_access_time", "created_on", "role"]
         rows = [headers]
         usernames_usage = []
         for user_chunk in chunked(usernames, chunk_size):
-            usage_data = ICDSAuditEntryRecord.objects.filter(username__in=list(user_chunk)).values('username').annotate(time=Max('time_of_use'))
-            print(f"===========Executing chunk of {len(usage_data)}======\n")
+            usage_data = ICDSAuditEntryRecord.objects.filter(
+                username__in=list(user_chunk)).values('username').annotate(time=Max('time_of_use'))
             for usage in usage_data:
                 row_data = [
                     usage['username'],
                     self.convert_to_ist(usage['time']),
-                    self.convert_to_ist(users_created_time[usage['username']])
+                    self.convert_to_ist(user_details[usage['username']][0]),
+                    user_details[usage['username']][1]
                 ]
                 usernames_usage.append(usage['username'])
                 rows.append(row_data)
         users_not_logged_in = set(usernames) - set(usernames_usage)
-        rows.extend([[user, 'N/A', self.convert_to_ist(users_created_time[usage['username']])] for user in users_not_logged_in])
-        print(f"===========Total rows of {len(rows)}======\n")
-        fout = open('/home/cchq/National_users_data.csv', 'w')
+        for user in users_not_logged_in:
+            rows.extend([
+                user,
+                'N/A',
+                self.convert_to_ist(user_details[usage['username']][0]),
+                user_details[usage['username']][1]
+            ])
+        fout = open('/home/cchq/National_users_usage_data.csv', 'w')
         writer = csv.writer(fout)
         writer.writerows(rows)
