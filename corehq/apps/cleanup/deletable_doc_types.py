5ded5ec24d8909d04972f0b93c67745a4a6f9dde
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-10 17:43:14 -0400

    Added MobileAuthKeyRecord to DELETABLE_COUCH_DOC_TYPES (PR3)

--- corehq/apps/cleanup/deletable_doc_types.py
+++ corehq/apps/cleanup/deletable_doc_types.py
@@ -27,6 +27,7 @@ DELETABLE_COUCH_DOC_TYPES = {
     'HqDeploy': (MAIN_DB,),
     'ILSGatewayConfig': (MAIN_DB,),
     'Invitation': (settings.USERS_GROUPS_DB,),
+    'MobileAuthKeyRecord': (MAIN_DB,),
     'RegistrationRequest': (MAIN_DB,),
     'StandaloneTranslationDoc': (MAIN_DB,),
     'WisePillDeviceEvent': (MAIN_DB,),
