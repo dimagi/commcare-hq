577caec01866b39ee57077685522ede58d8be81f
Shubham <shubhambansal17@hotmail.com>
2020-05-11 11:36:15 +0530

    :tada: added the sql file

---
+++ custom/icds_reports/utils/data_pull.sql
@@ -0,0 +1,24 @@
+SELECT
+    awc.state_name,
+    awc.district_name,
+    awc.block_name,
+    awc.supervisor_name,
+    awc.awc_name,
+    ccs.person_name,
+    ccs.ccs_status,
+    ccs.mobile_number
+    FROM ccs_record_monthly ccs
+    LEFT JOIN "awc_location" awc
+    ON (awc.doc_id = ccs.awc_id AND awc.supervisor_id = ccs.supervisor_id)
+    WHERE ccs.month='2020-05-01' AND (ccs.lactating=1 OR ccs.pregnant=1) AND ccs.mobile_number <> NULL AND ccs.mobile_number <> '';
+
+--                           QUERY PLAN
+-- --------------------------------------------------------------
+--  Custom Scan (Citus Router)  (cost=0.00..0.00 rows=0 width=0)
+--    Task Count: 1
+--    Tasks Shown: All
+--    ->  Task
+--          Node: host=100.71.184.226 port=6432 dbname=icds_ucr
+--          ->  Result  (cost=0.00..0.00 rows=0 width=256)
+--                One-Time Filter: false
+-- (7 rows)
