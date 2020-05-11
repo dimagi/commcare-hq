a92ee777a6b708ed50dccf148bdf27ef2c9a0f2b
Norman Hooper <nhooper@dimagi.com>
2020-05-11 14:12:57 +0200

    Add weekly frequency

--- corehq/motech/dhis2/tests/test_datasetmap.py
+++ corehq/motech/dhis2/tests/test_datasetmap.py
@@ -6,6 +6,7 @@ from nose.tools import assert_false, assert_true
 from corehq.motech.dhis2.const import (
     SEND_FREQUENCY_MONTHLY,
     SEND_FREQUENCY_QUARTERLY,
+    SEND_FREQUENCY_WEEKLY,
 )
 from corehq.motech.dhis2.models import DataSetMap
 
@@ -40,6 +41,30 @@ def test_send_quarterly_wrong_month():
     with quarterly_datasetmap() as quarterly_day_2:
         send_date = date(2020, 5, 2)
         assert_false(quarterly_day_2.should_send_on_date(send_date))
+
+
+def test_send_weekly_yes():
+    with weekly_datasetmap() as weekly_day_2:
+        send_date = date(2020, 5, 5)  # Tuesday ("2020-W19-2")
+        assert_true(weekly_day_2.should_send_on_date(send_date))
+
+
+def test_send_weekly_wrong_day():
+    with weekly_datasetmap() as weekly_day_2:
+        send_date = date(2020, 5, 2)  # Saturday ("2020-W18-6")
+        assert_false(weekly_day_2.should_send_on_date(send_date))
+
+
+@contextmanager
+def weekly_datasetmap():
+    try:
+        yield DataSetMap(
+            domain=DOMAIN,
+            frequency=SEND_FREQUENCY_WEEKLY,
+            day_to_send=2,
+        )
+    finally:
+        pass
 
 
 @contextmanager
