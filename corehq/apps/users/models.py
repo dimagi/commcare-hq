0f39f1d386784159f1c943bee29ceba099fba2bc
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-12 18:07:16 -0400

    Removed CouchUser.projects
    Grepping for "\.projects\b" turned up nothing.

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -1135,10 +1135,6 @@ class CouchUser(Document, DjangoUserMixin, IsMemberOfMixin, EulaMixin):
             raise NotImplementedError(f'Unrecognized user type {self.doc_type!r}')
 
     @property
-    def projects(self):
-        return list(map(Domain.get_by_name, self.get_domains()))
-
-    @property
     def full_name(self):
         return ("%s %s" % (self.first_name or '', self.last_name or '')).strip()
 
