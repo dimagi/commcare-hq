e8d79dbe0a4965e2adf4e27b1c1efa6c1b1dc1ee
Ethan Soergel <esoergel@gmail.com>
2020-05-12 16:57:20 -0400

    Disable signal from firing when transaction ends

--- corehq/apps/dump_reload/tests/test_sql_dump_load.py
+++ corehq/apps/dump_reload/tests/test_sql_dump_load.py
@@ -7,7 +7,7 @@ from io import StringIO
 
 from django.contrib.admin.utils import NestedObjects
 from django.core import serializers
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.test import TestCase
 from django.test.utils import override_settings
 
@@ -28,6 +28,9 @@ from corehq.apps.hqcase.utils import submit_case_blocks
 from corehq.apps.products.models import SQLProduct
 from corehq.apps.zapier.consts import EventTypes
 from corehq.apps.zapier.models import ZapierSubscription
+from corehq.apps.zapier.signals.receivers import (
+    zapier_subscription_post_delete,
+)
 from corehq.blobs.models import BlobMeta
 from corehq.form_processor.backends.sql.dbaccessors import LedgerAccessorSQL
 from corehq.form_processor.interfaces.dbaccessors import (
@@ -52,6 +55,7 @@ from corehq.messaging.scheduling.scheduling_partitioned.models import AlertSched
 class BaseDumpLoadTest(TestCase):
     @classmethod
     def setUpClass(cls):
+        post_delete.disconnect(zapier_subscription_post_delete, sender=ZapierSubscription)
         super(BaseDumpLoadTest, cls).setUpClass()
         cls.domain_name = uuid.uuid4().hex
         cls.domain = Domain(name=cls.domain_name)
@@ -65,6 +69,7 @@ class BaseDumpLoadTest(TestCase):
     def tearDownClass(cls):
         cls.domain.delete()
         super(BaseDumpLoadTest, cls).tearDownClass()
+        post_delete.connect(zapier_subscription_post_delete, sender=ZapierSubscription)
 
     def delete_sql_data(self):
         for model_class, builder in get_model_iterator_builders_to_dump(self.domain_name, []):
@@ -534,14 +539,13 @@ class TestSQLDumpLoad(BaseDumpLoadTest):
         self._dump_and_load(Counter({AlertSchedule: 1, AlertEvent: 2, SMSContent: 2}))
 
     def test_zapier_subscription(self):
-        subscription = ZapierSubscription.objects.create(
+        ZapierSubscription.objects.create(
             domain=self.domain_name,
             case_type='case_type',
             event_name=EventTypes.NEW_CASE,
             url='example.com',
             user_id='user_id',
         )
-        self.addCleanup(subscription.delete)
         self._dump_and_load(Counter({ZapierSubscription: 1}))
 
 
