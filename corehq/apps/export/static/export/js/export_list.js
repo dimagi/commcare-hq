c5dad8e1c1368ba5de41556373b72141a45180cf
Biyeun Buczyk <biyeun@dimagi.com>
2020-05-13 14:45:52 -0500

    add ability for users (with all loctions access) to update location restrictions of feeds (which is handled separately from filters)

--- corehq/apps/export/static/export/js/export_list.js
+++ corehq/apps/export/static/export/js/export_list.js
@@ -457,6 +457,7 @@ hqDefine("export/js/export_list", [
                 params: '^[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]$',
             },
         });
+        self.updateLocationRestriction = ko.observable(false);
 
         // Editing filters for a saved export
         self.selectedExportModelType = ko.observable();
@@ -567,6 +568,7 @@ hqDefine("export/js/export_list", [
                         days: self.days(),
                         start_date: self.startDate(),
                         end_date: self.endDate(),
+                        update_location_restriction: self.updateLocationRestriction(),
                     }),
                     is_deid: self.isDeid,
                     is_odata: self.isOData,
