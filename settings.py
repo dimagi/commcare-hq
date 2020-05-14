3e105f8fa1922a2a6919c2e55a7e61d305fb433b
Daniel Miller <dmiller@dimagi.com>
2020-05-14 14:58:01 -0400

    Avoid lint error
    E731 do not assign a lambda expression, use a def

--- settings.py
+++ settings.py
@@ -769,8 +769,8 @@ COMPRESS_PRECOMPILERS = AVAILABLE_COMPRESS_PRECOMPILERS = (
 )
 # if not overwritten in localsettings, these will be replaced by the value they return
 # using the local DEBUG value (which we don't have access to here yet)
-COMPRESS_ENABLED = lambda: not DEBUG and not UNIT_TESTING
-COMPRESS_OFFLINE = lambda: not DEBUG and not UNIT_TESTING
+COMPRESS_ENABLED = lambda: not DEBUG and not UNIT_TESTING  # noqa: E731
+COMPRESS_OFFLINE = lambda: not DEBUG and not UNIT_TESTING  # noqa: E731
 COMPRESS_JS_COMPRESSOR = 'corehq.apps.hqwebapp.uglify.JsUglifySourcemapCompressor'
 # use 'compressor.js.JsCompressor' for faster local compressing (will get rid of source maps)
 COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',
