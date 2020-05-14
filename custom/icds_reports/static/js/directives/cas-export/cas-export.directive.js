0044a5bf394d1055b6e5cb73da2ef3817c4b3de0
Cal Ellowitz <calellowitz@gmail.com>
2020-05-13 22:05:34 -0400

    use archived on not deprecated at

--- custom/icds_reports/static/js/directives/cas-export/cas-export.directive.js
+++ custom/icds_reports/static/js/directives/cas-export/cas-export.directive.js
@@ -155,7 +155,7 @@ function CasExportController($window, $location, locationHierarchy, locationsSer
 
     vm.showReassignmentMessage = function () {
         utcSelectedDate = Date.UTC(vm.selectedDate.getFullYear(), vm.selectedDate.getMonth());
-        return vm.selectedLocation && (Date.parse(vm.selectedLocation.deprecated_at) <= utcSelectedDate || Date.parse(vm.selectedLocation.deprecates_at) > utcSelectedDate);
+        return vm.selectedLocation && (Date.parse(vm.selectedLocation.archived_on) <= utcSelectedDate || Date.parse(vm.selectedLocation.deprecates_at) > utcSelectedDate);
     };
 }
 
--- custom/icds_reports/static/js/directives/location-filter/location-filter.directive.js
+++ custom/icds_reports/static/js/directives/location-filter/location-filter.directive.js
@@ -31,7 +31,7 @@ function LocationModalController($uibModalInstance, $location, locationsService,
 
     vm.showReassignmentMessage = function () {
         utcSelectedDate = Date.UTC(vm.selectedDate.getFullYear(), vm.selectedDate.getMonth());
-        return vm.selectedLocation() && (Date.parse(vm.selectedLocation().deprecated_at) <= utcSelectedDate || Date.parse(vm.selectedLocation().deprecates_at) > utcSelectedDate);
+        return vm.selectedLocation() && (Date.parse(vm.selectedLocation().archived_on) <= utcSelectedDate || Date.parse(vm.selectedLocation().deprecates_at) > utcSelectedDate);
     };
 
     vm.errors = function () {
