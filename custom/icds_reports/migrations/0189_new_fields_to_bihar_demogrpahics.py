2d0a4ee5923bae698adb2c556cb13b54d2e662ba
challa <challarao@beehyv.com>
2020-05-20 16:48:24 +0530

    migration fix

--- custom/icds_reports/migrations/0189_new_fields_to_bihar_demogrpahics.py
+++ custom/icds_reports/migrations/0189_new_fields_to_bihar_demogrpahics.py
@@ -10,7 +10,7 @@ from custom.icds_reports.utils.migrations import get_view_migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('icds_reports', '0188_rebuild_view_bihar_api'),
+        ('icds_reports', '0189_new_fields_to_bihar_demogrpahics'),
     ]
 
     operations = [
@@ -45,5 +45,3 @@ class Migration(migrations.Migration):
             field=models.SmallIntegerField(null=True),
         ),
     ]
-
-    operations.extend(get_view_migrations())
