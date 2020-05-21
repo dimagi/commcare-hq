e254a247056a565045de02399bb92f7e47b0439c
Farid Rener <proteusvacuum@gmail.com>
2020-05-20 23:08:49 -0400

    Remove unneeded copy/pasted code

--- corehq/apps/userreports/forms.py
+++ corehq/apps/userreports/forms.py
@@ -6,10 +6,7 @@ from crispy_forms import layout as crispy
 from crispy_forms.bootstrap import StrictButton
 from corehq.apps.hqwebapp.crispy import HQFormHelper
 
-from corehq import toggles
-from corehq.apps.domain.models import Domain
 from corehq.apps.hqwebapp import crispy as hqcrispy
-from corehq.apps.linked_domain.models import DomainLink
 from corehq.apps.linked_domain.dbaccessors import (
     get_linked_domains,
 )
@@ -34,31 +31,3 @@ class CopyReportForm(forms.Form):
             crispy.Hidden("report_id", report_id),
             hqcrispy.FormActions(StrictButton(_("Copy"), type="submit", css_class="btn-primary")),
         )
-
-    def clean_domain(self):
-        domain = self.cleaned_data["domain"]
-        domain_obj = Domain.get_by_name(domain)
-        if domain_obj is None:
-            raise forms.ValidationError("A valid project space is required.")
-        if toggles.MULTI_MASTER_BYPASS_VERSION_CHECK.enabled(domain):
-            raise forms.ValidationError(
-                """
-                Copying an app to a domain that uses multi-master linked apps and bypasses
-                the minimum CommCare version check requires developer intervention.
-            """
-            )
-        return domain
-
-    def clean(self):
-        domain = self.cleaned_data.get("domain")
-        if self.cleaned_data.get("linked"):
-            if not toggles.LINKED_DOMAINS.enabled(domain):
-                raise forms.ValidationError(
-                    "The target project space does not have linked apps enabled."
-                )
-            link = DomainLink.objects.filter(linked_domain=domain)
-            if link and link[0].master_domain != self.from_domain:
-                raise forms.ValidationError(
-                    "The target project space is already linked to a different domain"
-                )
-        return self.cleaned_data
