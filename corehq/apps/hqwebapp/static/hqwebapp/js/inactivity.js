126f2ed97e5269804b52988ea6e0167749125d78
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-25 13:39:17 -0400

    Added modal

---
+++ corehq/apps/hqwebapp/static/hqwebapp/js/inactivity.js
@@ -0,0 +1,16 @@
+/*
+ *  TODO
+ */
+hqDefine('hqwebapp/js/inactivity', [
+    'jquery',
+    'hqwebapp/js/initial_page_data',
+], function (
+    $,
+    initialPageData
+) {
+    $(function () {
+        $("#inactivityModal").modal('show');
+    });
+
+    return 1;
+});
