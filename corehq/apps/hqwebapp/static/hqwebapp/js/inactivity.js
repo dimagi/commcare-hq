21fe40e7ed972ce00433f9d174155cd93ec0c17c
Jennifer Schweers <orange.jenny@gmail.com>
2020-05-25 18:17:36 -0400

    Added polling

--- corehq/apps/hqwebapp/static/hqwebapp/js/inactivity.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/inactivity.js
@@ -9,7 +9,18 @@ hqDefine('hqwebapp/js/inactivity', [
     initialPageData
 ) {
     $(function () {
-        $("#inactivityModal").modal('show');
+        var interval = 60 * 1000;       // TODO: get timeout from domain
+        setInterval(function () {
+            $.ajax({
+                url: initialPageData.reverse('ping_login'),
+                type: 'GET',
+                success: function (data) {
+                    if (!data.success) {
+                        $("#inactivityModal").modal('show');
+                    }
+                },
+            });
+        }, interval);
     });
 
     return 1;
