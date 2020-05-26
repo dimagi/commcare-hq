7119cec70079e2aa105af7e8addd6e4dddbeaef0
Shubham Bansal <shubhambansal17@hotmail.com>
2020-05-27 00:58:38 +0530

    ✅ fixed and improved tests

--- custom/icds_reports/tests/agg_tests/reports/test_bihar_api_mother_details.py
+++ custom/icds_reports/tests/agg_tests/reports/test_bihar_api_mother_details.py
@@ -1,17 +1,19 @@
 import datetime
-from django.test import TestCase
-from custom.icds_reports.reports.bihar_api import get_mother_details
-from custom.icds_reports.tasks import  update_bihar_api_table
 from datetime import date
 
+from django.test import TestCase
 from mock import patch
 
+from custom.icds_reports.reports.bihar_api import get_mother_details
+from custom.icds_reports.tasks import update_bihar_api_table
 
-@patch('custom.icds_reports.utils.aggregation_helpers.distributed.bihar_api_demographics.BiharApiDemographicsHelper.bihar_state_id',
-       'st1')
+
+@patch(
+    'custom.icds_reports.utils.aggregation_helpers.distributed.bihar_api_demographics.BiharApiDemographicsHelper.bihar_state_id',
+    'st1')
 class BiharAPIMotherTest(TestCase):
 
-    def test_file_content(self):
+    def test_file_content_1(self):
         update_bihar_api_table(date(2017, 5, 1))
         data, count = get_mother_details(
             month=date(2017, 5, 1).strftime("%Y-%m-%d"),
@@ -51,6 +53,50 @@ class BiharAPIMotherTest(TestCase):
                 "ifa_consumed_7_days": 4,
                 "causes_for_ifa": "side_effects",
                 "maternal_complications": 'Discharge'
-             },
+            },
+            ccs_case_details
+        )
+
+    def test_file_content_2(self):
+        update_bihar_api_table(date(2017, 5, 1))
+        data, count = get_mother_details(
+            month=date(2017, 5, 1).strftime("%Y-%m-%d"),
+            state_id='st1',
+            last_ccs_case_id=''
+        )
+
+        for case in data:
+            if case['ccs_case_id'] == 'f491263a-4846-4ffd-a64d-a6653c03dd03':
+                ccs_case_details = case
+                break
+
+        self.assertEqual(
+            {
+                "household_id": None,
+                "person_id": "1d3e45d5-bd58-487c-9f93-7da2cf67c8d7",
+                "ccs_case_id": "f491263a-4846-4ffd-a64d-a6653c03dd03",
+                "married": None,
+                "husband_name": None,
+                "husband_id": None,
+                "last_preg_year": None,
+                "is_pregnant": None,
+                "preg_reg_date": None,
+                "tt_1": None,
+                'tt_2': None,
+                "tt_booster": None,
+                "hb": 2,
+                "add": datetime.date(2017, 3, 20),
+                "last_preg_tt": None,
+                "lmp": datetime.date(2016, 11, 1),
+                "anc_1": None,
+                "anc_2": None,
+                "anc_3": None,
+                "anc_4": None,
+                "edd": datetime.date(2017, 8, 8),
+                "total_ifa_tablets_received": -130,
+                "ifa_consumed_7_days": 2,
+                "causes_for_ifa": None,
+                "maternal_complications": None
+            },
             ccs_case_details
         )
