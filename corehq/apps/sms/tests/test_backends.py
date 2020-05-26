9ca3e7c81ad007098b730ed56da8844fcaa84c87
nemanja <nemanja.stancic.91@gmail.com>
2020-05-26 09:40:32 +0200

    adding aws pinpoint backed to settings file

--- corehq/apps/sms/tests/test_backends.py
+++ corehq/apps/sms/tests/test_backends.py
@@ -68,6 +68,7 @@ from corehq.messaging.smsbackends.unicel.models import (
 from corehq.messaging.smsbackends.vertex.models import VertexBackend
 from corehq.messaging.smsbackends.yo.models import SQLYoBackend
 from corehq.messaging.smsbackends.infobip.models import SQLInfobipBackend
+from corehq.messaging.smsbackends.amazon_pinpoint.models import SQLPinpointBackend
 from corehq.util.test_utils import create_test_case
 
 
@@ -232,6 +233,13 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
         )
         cls.infobip_backend.save()
 
+        cls.pinpoint_backend = SQLPinpointBackend(
+            name='PINPOINT',
+            is_global=True,
+            hq_api_id=SQLPinpointBackend.get_api_id()
+        )
+        cls.pinpoint_backend.save()
+
     @classmethod
     def tearDownClass(cls):
         cls.teardown_subscriptions()
@@ -258,6 +266,7 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
         cls.karix_backend.delete()
         cls.airtel_tcl_backend.delete()
         cls.infobip_backend.delete()
+        cls.pinpoint_backend.delete()
         clear_plan_version_cache()
         super(AllBackendTest, cls).tearDownClass()
 
@@ -353,8 +362,10 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
     @patch('corehq.messaging.smsbackends.karix.models.KarixBackend.send')
     @patch('corehq.messaging.smsbackends.airtel_tcl.models.AirtelTCLBackend.send')
     @patch('corehq.messaging.smsbackends.infobip.models.SQLInfobipBackend.send')
+    @patch('corehq.messaging.smsbackends.amazon_pinpoint.models.SQLPinpointBackend.send')
     def test_outbound_sms(
             self,
+            pinpoint_send,
             infobip_send,
             airtel_tcl_send,
             karix_send,
@@ -397,6 +408,7 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
         self._test_outbound_backend(self.karix_backend, 'karix test', karix_send)
         self._test_outbound_backend(self.airtel_tcl_backend, 'airtel tcl test', airtel_tcl_send)
         self._test_outbound_backend(self.infobip_backend, 'infobip test', infobip_send)
+        self._test_outbound_backend(self.pinpoint_backend, 'pinpoint test', pinpoint_send)
 
     @run_with_all_backends
     def test_unicel_inbound_sms(self):
@@ -540,7 +552,6 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
         self._verify_inbound_request(self.push_backend.get_api_id(), 'push test',
             backend_couch_id=self.push_backend.couch_id)
 
-
     @run_with_all_backends
     def test_infobip_inbound_sms(self):
         url = '/infobip/sms/%s' % self.infobip_backend.inbound_api_key
@@ -560,6 +571,27 @@ class AllBackendTest(DomainSubscriptionMixin, TestCase):
 
         self._verify_inbound_request(self.infobip_backend.get_api_id(), 'infobip test',
             backend_couch_id=self.infobip_backend.couch_id)
+
+    @run_with_all_backends
+    def test_pinpoint_inbound_sms(self):
+        url = '/pinpoint/sms/%s' % self.pinpoint_backend.inbound_api_key
+        payload = {
+            "results": [
+                {
+                    "from": self.test_phone_number,
+                    "messageId": "message_id",
+                    "message": {
+                        "type": "TEXT",
+                        "text": "pinpoint test"
+                    }
+                }
+            ]
+        }
+        self._simulate_inbound_request_with_payload(url, 'application/json', json.dumps(payload))
+
+        self._verify_inbound_request(self.pinpoint_backend.get_api_id(), 'pinpoint test',
+            backend_couch_id=self.pinpoint_backend.couch_id)
+
 
 class OutgoingFrameworkTestCase(DomainSubscriptionMixin, TestCase):
 
