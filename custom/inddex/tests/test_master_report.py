4fc1afe98f02e75e2503cd05e3054a821bcb816a
Ethan Soergel <esoergel@gmail.com>
2020-05-27 17:09:12 -0400

    Remove fixture row counts test

--- custom/inddex/tests/test_master_report.py
+++ custom/inddex/tests/test_master_report.py
@@ -110,18 +110,8 @@ class TestSetupUtils(TestCase):
         ))
 
     def test_fixtures_created(self):
-        # Note, this is actually quite slow - might want to drop
         data_types = get_fixture_data_types(DOMAIN)
         self.assertEqual(len(data_types), 6)
-        self.assertItemsEqual(
-            [(dt.tag, count_fixture_items(DOMAIN, dt._id)) for dt in data_types],
-            [('recipes', 384),
-             ('food_list', 1130),
-             ('food_composition_table', 1042),
-             ('conv_factors', 2995),
-             ('nutrients_lookup', 152),
-             ('languages', 2)]
-        )
 
 
 class TestUcrAdapter(TestCase):
