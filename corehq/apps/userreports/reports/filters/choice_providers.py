42fe310e779db113a0f57537c5a10adb02df84e0
Cory Zue <czue@dimagi.com>
2020-05-29 13:15:07 +0200

    show null/empty as options in dynamic choice list

--- corehq/apps/userreports/reports/filters/choice_providers.py
+++ corehq/apps/userreports/reports/filters/choice_providers.py
@@ -10,7 +10,7 @@ from corehq.apps.es import GroupES, UserES
 from corehq.apps.locations.models import SQLLocation
 from corehq.apps.reports_core.filters import Choice
 from corehq.apps.userreports.exceptions import ColumnNotFoundError
-from corehq.apps.userreports.reports.filters.values import SHOW_ALL_CHOICE
+from corehq.apps.userreports.reports.filters.values import SHOW_ALL_CHOICE, EMPTY_CHOICE, NONE_CHOICE
 from corehq.apps.userreports.util import get_indicator_adapter
 from corehq.apps.users.analytics import get_search_users_in_domain_es_query
 from corehq.apps.users.util import raw_username
@@ -187,6 +187,10 @@ class DataSourceColumnChoiceProvider(ChoiceProvider):
         return []
 
     def _make_choice_from_value(self, value):
+        if value is None:
+            return Choice(NONE_CHOICE, '[Missing]')
+        elif not value:
+            return Choice(EMPTY_CHOICE, '[Empty]')
         return Choice(value, value)
 
 
--- corehq/apps/userreports/reports/filters/values.py
+++ corehq/apps/userreports/reports/filters/values.py
@@ -31,8 +31,11 @@ from corehq.apps.reports.util import (
     get_INFilter_element_bindparam,
 )
 
-SHOW_ALL_CHOICE = '_all'  # todo: if someone wants to name an actually choice "_all" this will break
+# todo: if someone wants to name an actual choice any of these values, it will break
+SHOW_ALL_CHOICE = '_all'
 NONE_CHOICE = "\u2400"
+EMPTY_CHOICE = '_cchq_empty'
+
 CHOICE_DELIMITER = "\u001f"
 
 
