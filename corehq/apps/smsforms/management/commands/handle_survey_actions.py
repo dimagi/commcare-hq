950644583b1b3356ab227c74c09bea298a112041
Daniel Roberts <droberts@dimagi.com>
2020-06-01 10:12:07 -0500

    Release any stale session/channel claims in handle_survey_action
    This is a release valve for edge cases we can imagine but will almost never happen.
    When a session claim is still active but the session is no longer open,
    that means whatever was supposed to release it has failed to do so,
    and so we release it and move on as if the channel were unclaimed.

--- corehq/apps/smsforms/management/commands/handle_survey_actions.py
+++ corehq/apps/smsforms/management/commands/handle_survey_actions.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 
 from corehq import toggles
 from dimagi.utils.couch import get_redis_lock
-from dimagi.utils.logging import notify_exception
+from dimagi.utils.logging import notify_exception, notify_error
 
 from corehq.apps.domain_migration_flags.api import any_migrations_in_progress
 from corehq.apps.smsforms.models import SQLXFormsSession, XFormsSessionSynchronization
@@ -46,10 +46,16 @@ class Command(BaseCommand):
             current_action_due__lt=datetime.utcnow(),
         ).values_list('domain', 'connection_id', 'session_id', 'current_action_due', 'phone_number')
 
+    def get_open_session_ids(self):
+        return SQLXFormsSession.objects.filter(
+            session_is_open=True
+        ).values_list('session_id', flat=True)
+
     @handle_connection_failure()
     def create_tasks(self):
-        for domain, connection_id, session_id, current_action_due, phone_number \
-                in self.get_survey_sessions_due_for_action():
+        survey_sessions_due_for_action = self.get_survey_sessions_due_for_action()
+        all_open_session_ids = self.get_open_session_ids()
+        for domain, connection_id, session_id, current_action_due, phone_number in survey_sessions_due_for_action:
             if skip_domain(domain):
                 continue
 
@@ -60,7 +66,22 @@ class Command(BaseCommand):
                     phone_number=phone_number,
                 )
                 if not XFormsSessionSynchronization.could_maybe_claim_channel_for_session(fake_session):
-                    continue
+                    running_session_info = XFormsSessionSynchronization.get_running_session_info_for_channel()
+                    # First confirm the supposedly running session is even open
+                    # and if it's not (should be exceedingly rare) release it and act like it wasn't there
+                    if running_session_info.session_id \
+                            and running_session_info.session_id not in all_open_session_ids:
+                        notify_error(
+                            "The supposedly running session was not open and was released. "
+                            "No known way for this to happen, so worth investigating.",
+                            details={
+                                'running_session_info': running_session_info
+                            })
+                        XFormsSessionSynchronization.release_channel_for_session(session)
+                    # This is the 99% case: there's a running session for the channel
+                    # so leave this session/action in the queue for later and move on to the next one
+                    else:
+                        continue
 
             enqueue_lock = self.get_enqueue_lock(session_id, current_action_due)
             if enqueue_lock.acquire(blocking=False):
