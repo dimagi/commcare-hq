b4731a30905b803e7cfc858b5e8d594b37effe31
Ethan Soergel <esoergel@gmail.com>
2020-06-02 15:54:07 -0400

    Count only unique foods

--- custom/inddex/reports/r2a_gaps_summary.py
+++ custom/inddex/reports/r2a_gaps_summary.py
@@ -42,12 +42,12 @@ class GapsSummaryReport(MultiTabularReport):
 
 
 def get_gaps_data(domain, request):
-    cf_gaps = defaultdict(int)
-    fct_gaps = defaultdict(int)
+    cf_gaps = defaultdict(set)
+    fct_gaps = defaultdict(set)
     food_data = FoodData.from_request(domain, request)
     for row in food_data.rows:
-        cf_gaps[(row.conv_factor_gap_code, row.food_type or '')] += 1
-        fct_gaps[(row.fct_gap_code, row.food_type or '')] += 1
+        cf_gaps[(row.conv_factor_gap_code, row.food_type or '')].add(row.food_name)
+        fct_gaps[(row.fct_gap_code, row.food_type or '')].add(row.food_name)
 
     return (
         ConvFactorGapsData(cf_gaps),
@@ -64,7 +64,7 @@ class GapsData:
         for gap_code in self._gaps_descriptions:
             for food_type in [FOOD_ITEM, NON_STANDARD_FOOD_ITEM, STANDARD_RECIPE, NON_STANDARD_RECIPE]:
                 description = self._gaps_descriptions[gap_code]
-                count = self._gaps.get((gap_code, food_type), 0)
+                count = len(self._gaps[(gap_code, food_type)])
                 yield format_row([gap_code, description, food_type, count])
 
 
