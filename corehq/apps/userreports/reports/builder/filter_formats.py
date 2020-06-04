7034ca18ed182d5f2bc508344ce293060ff74ef2
Cory Zue <czue@dimagi.com>
2020-06-04 10:10:29 +0200

    add support for additional filter types

--- corehq/apps/userreports/reports/builder/filter_formats.py
+++ corehq/apps/userreports/reports/builder/filter_formats.py
@@ -6,4 +6,11 @@ def get_pre_filter_format(pre_filter_dict):
     See the `to_report_filter` function of DataSourceProperty in forms.py for this logic.
     This is basically the opposite.
     """
-    return 'Value' if pre_filter_dict['pre_value'] else 'Date'
+    if pre_filter_dict['pre_operator'] == '' and pre_filter_dict['pre_value'] == '':
+        return const.PRE_FILTER_VALUE_IS_EMPTY
+    elif pre_filter_dict['pre_operator'] == '!=' and pre_filter_dict['pre_value'] == '':
+        return const.PRE_FILTER_VALUE_EXISTS
+    elif pre_filter_dict['pre_operator'] == 'distinct from':
+        return const.PRE_FILTER_VALUE_NOT_EQUAL
+    else:
+        return 'Value' if pre_filter_dict['pre_value'] else 'Date'
