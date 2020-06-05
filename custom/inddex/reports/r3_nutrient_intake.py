96b870ab7f23e906b565462e80182dc67784cf82
Ethan Soergel <esoergel@gmail.com>
2020-06-05 16:53:20 -0400

    Move this to reflect the order in the report

--- custom/inddex/reports/r3_nutrient_intake.py
+++ custom/inddex/reports/r3_nutrient_intake.py
@@ -49,6 +49,39 @@ class NutrientIntakeReport(MultiTabularReport):
         ]
 
 
+class IntakeData:
+    title = 'Disaggregated Intake Data By Food'
+    slug = 'disaggr_intake_data_by_rspndnt'
+    _columns = [
+        'unique_respondent_id', 'location_id', 'respondent_id',
+        'recall_case_id', 'opened_by_username', 'owner_name', 'visit_date',
+        'recall_status', 'gender', 'age_years_calculated',
+        'age_months_calculated', 'age_range', 'supplements', 'urban_rural',
+        'pregnant', 'breastfeeding', 'food_code', 'base_term_food_code',
+        'reference_food_code', 'caseid', 'food_name', 'recipe_name',
+        'fao_who_gift_food_group_code', 'fao_who_gift_food_group_description',
+        'user_food_group', 'food_type', 'include_in_analysis', 'is_ingredient',
+        'total_grams', 'conv_factor_gap_code', 'conv_factor_gap_desc',
+        'fct_gap_code', 'fct_gap_desc',
+    ]
+
+    def __init__(self, food_data):
+        self._food_data = food_data
+        self._nutrient_names = self._food_data.fixtures.nutrient_names
+
+    @property
+    def headers(self):
+        return self._columns + list(self._nutrient_names)
+
+    @property
+    def rows(self):
+        for row in self._food_data.rows:
+            yield format_row(chain(
+                (getattr(row, col) for col in self._columns),
+                (na_for_None(row.get_nutrient_amt(name)) for name in self._nutrient_names),
+            ))
+
+
 class DailyIntakeData:
     title = 'Aggregated Daily Intake By Respondent'
     slug = 'aggr_daily_intake_by_rspndnt'
@@ -99,36 +132,3 @@ class DailyIntakeData:
 
 def _sum(items):
     return sum(filter(None, items))
-
-
-class IntakeData:
-    title = 'Disaggregated Intake Data By Food'
-    slug = 'disaggr_intake_data_by_rspndnt'
-    _columns = [
-        'unique_respondent_id', 'location_id', 'respondent_id',
-        'recall_case_id', 'opened_by_username', 'owner_name', 'visit_date',
-        'recall_status', 'gender', 'age_years_calculated',
-        'age_months_calculated', 'age_range', 'supplements', 'urban_rural',
-        'pregnant', 'breastfeeding', 'food_code', 'base_term_food_code',
-        'reference_food_code', 'caseid', 'food_name', 'recipe_name',
-        'fao_who_gift_food_group_code', 'fao_who_gift_food_group_description',
-        'user_food_group', 'food_type', 'include_in_analysis', 'is_ingredient',
-        'total_grams', 'conv_factor_gap_code', 'conv_factor_gap_desc',
-        'fct_gap_code', 'fct_gap_desc',
-    ]
-
-    def __init__(self, food_data):
-        self._food_data = food_data
-        self._nutrient_names = self._food_data.fixtures.nutrient_names
-
-    @property
-    def headers(self):
-        return self._columns + list(self._nutrient_names)
-
-    @property
-    def rows(self):
-        for row in self._food_data.rows:
-            yield format_row(chain(
-                (getattr(row, col) for col in self._columns),
-                (na_for_None(row.get_nutrient_amt(name)) for name in self._nutrient_names),
-            ))
