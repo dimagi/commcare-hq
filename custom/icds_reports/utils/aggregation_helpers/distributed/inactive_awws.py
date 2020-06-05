33e499cc4ebf920f05c546c32599f52f88a0f665
Shubham Bansal <shubhambansal17@hotmail.com>
2020-06-05 07:25:54 +0530

    🔧 fixed helper function for inactive awws

--- custom/icds_reports/utils/aggregation_helpers/distributed/inactive_awws.py
+++ custom/icds_reports/utils/aggregation_helpers/distributed/inactive_awws.py
@@ -94,11 +94,11 @@ class InactiveAwwsAggregationDistributedHelper(BaseICDSAggregationDistributedHel
                 last_submission = GREATEST(agg_table.last_submission, ut.last_submission),
                 no_of_days_since_start = CASE
                     WHEN LEAST(agg_table.first_submission, ut.first_submission) IS DISTINCT FROM NULL
-                    THEN DATE_PART('day', {now} - LEAST(agg_table.first_submission, ut.first_submission))
+                    THEN DATE_PART('day', '{now}'::DATE - LEAST(agg_table.first_submission, ut.first_submission))
                     ELSE NULL END,
                 no_of_days_inactive = CASE
                     WHEN GREATEST(agg_table.last_submission, ut.last_submission) IS DISTINCT FROM NULL
-                    THEN DATE_PART('day', {now} - GREATEST(agg_table.last_submission, ut.last_submission))
+                    THEN DATE_PART('day', '{now}'::DATE - GREATEST(agg_table.last_submission, ut.last_submission))
                     ELSE NULL END
             FROM (
               SELECT
