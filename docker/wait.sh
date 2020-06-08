2a3c73d019b2ca12dbf21bc1a961bdcab7642ef0
Sravan Reddy <sravfeyn@gmail.com>
2020-06-08 18:22:35 +0530

    skip wait

--- docker/wait.sh
+++ docker/wait.sh
@@ -21,16 +21,17 @@ for service in $SERVICES; do
     echo -n "Waiting for TCP connection to $svc:$port..."
 
     counter=0
-    while ! { exec 6<>/dev/tcp/${svc}/${port}; } 2>/dev/null
-    do
-      echo -n .
-      sleep 1
-      let counter=counter+1
-      if [ $counter -gt 90 ]; then
-        echo "TIMEOUT"
-        exit 1
-      fi
-    done
+    # Todo
+    # while ! { exec 6<>/dev/tcp/${svc}/${port}; } 2>/dev/null
+    # do
+    #   echo -n .
+    #   sleep 1
+    #   let counter=counter+1
+    #   if [ $counter -gt 90 ]; then
+    #     echo "TIMEOUT"
+    #     exit 1
+    #   fi
+    # done
 
     echo "$svc ok"
 done
