b6b1a8f11959fc9857c20f3afba1d077f110bd5d
Sravan Reddy <sravfeyn@gmail.com>
2020-06-10 17:41:05 +0530

    define service in all compose files

--- testsettings.py
+++ testsettings.py
@@ -6,8 +6,12 @@ import settingshelper as helper
 from settings import *
 
 # to enable v2 ES tests
-ELASTICSEARCH_PORT = int(os.environ.get('ELASTICSEARCH_2_PORT', ELASTICSEARCH_PORT))
-ELASTICSEARCH_MAJOR_VERSION = int(os.environ.get('ELASTICSEARCH_MAJOR_VERSION', 7))
+if os.environ.get('ELASTICSEARCH_2_PORT'):
+    ELASTICSEARCH_PORT = int(os.environ.get('ELASTICSEARCH_2_PORT'))
+
+if os.environ.get('ELASTICSEARCH_MAJOR_VERSION'):
+    ELASTICSEARCH_MAJOR_VERSION = int(os.environ.get('ELASTICSEARCH_MAJOR_VERSION'))
+print(ELASTICSEARCH_PORT, ELASTICSEARCH_MAJOR_VERSION, "using" )
 
 USING_CITUS = any(db.get('ROLE') == 'citus_master' for db in DATABASES.values())
 
