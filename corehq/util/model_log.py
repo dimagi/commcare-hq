e110b081ca675ff8886b2d6db1d034b16aabe27d
Manish Kangia <mkangia@dimagi.com>
2020-06-12 01:16:24 +0530

    Revert "adapt for couch models create"
    This reverts commit 1c619881c7c7e714c40f480af81e062dde25c873.

--- corehq/util/model_log.py
+++ corehq/util/model_log.py
@@ -1,18 +1,15 @@
 from django.contrib.admin.options import get_content_type_for_model
-from django.db.models import Model
 from django.utils.encoding import force_text
 
-from dimagi.ext.couchdbkit import Document
 
-
-def log_model_change(user, model_object, message=None, fields_changed=None, is_create=False):
+def log_model_change(user, model_object, message=None, fields_changed=None):
     """
     :param user: User making the change (couch user or django user)
     :param model_object: The object being changed (must be a Django model)
     :param message: Message text
     :param fields_changed: List of model field names that have changed
     """
-    from django.contrib.admin.models import LogEntry, CHANGE, ADDITION
+    from django.contrib.admin.models import LogEntry, CHANGE
     from corehq.apps.users.models import CouchUser
     if isinstance(user, CouchUser):
         user = user.get_django_user()
@@ -29,19 +26,11 @@ def log_model_change(user, model_object, message=None, fields_changed=None, is_c
 
         message = {'changed': {'fields': fields_changed}}
 
-    content_type_id = None
-    model_object_pk = None
-    if isinstance(model_object, Model):
-        content_type_id = get_content_type_for_model(model_object).pk
-        model_object_pk = model_object.pk
-    elif isinstance(model_object, Document):
-        model_object_pk = model_object.get_id
-
     return LogEntry.objects.log_action(
         user_id=user.pk,
-        content_type_id=content_type_id,
-        object_id=model_object_pk,
+        content_type_id=get_content_type_for_model(model_object).pk,
+        object_id=model_object.pk,
         object_repr=force_text(model_object),
-        action_flag=ADDITION if is_create else CHANGE,
+        action_flag=CHANGE,
         change_message=message,
     )
