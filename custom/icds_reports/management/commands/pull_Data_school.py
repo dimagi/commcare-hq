d240d86706e79e6a33db7daa2d9d6b0fb01d0b37
Rohit Negi <rohit25.negi@gmail.com>
2020-06-12 23:13:17 +0530

    simplified the code  fixed lynts

--- custom/icds_reports/management/commands/pull_Data_school.py
+++ custom/icds_reports/management/commands/pull_Data_school.py
@@ -2,7 +2,7 @@ import datetime
 from dateutil import parser
 from django.core.management.base import BaseCommand
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
-from django.db import connections, transaction
+from django.db import connections
 from dateutil.relativedelta import relativedelta
 from corehq.sql_db.connections import get_icds_ucr_citus_db_alias
 
@@ -35,7 +35,8 @@ class Command(BaseCommand):
                 count_joined_private_school_during_month = 0
                 count_stopped_private_school_during_month = 0
                 total_count = 0
-                person_case_ids = [item[0] for item in _run_custom_sql_script(query.format(age_group=age_group,month=month))]
+                person_case_ids = [item[0] for item in _run_custom_sql_script(query.format(age_group=age_group,
+                                                                                           month=month))]
 
                 for case in case_accessor.iter_cases(person_case_ids):
                     total_count += 1
@@ -44,30 +45,26 @@ class Command(BaseCommand):
                     date_last_private_admit = case.get_case_property('date_last_private_admit')
                     date_return_private = case.get_case_property('date_return_private')
                     next_month = month + relativedelta(months=1)
+
                     if primary_school_admit == 'yes' and parser.parse(date_primary_admit) < next_month:
                         count_primary_school_going += 1
+                        if parser.parse(date_primary_admit) >= month:
+                            count_joined_primary_school_during_month += 1
 
-                    if (date_last_private_admit is not None and parser.parse(date_last_private_admit) < next_month and
-                        (date_return_private is None or parser.parse(date_return_private) >= next_month)):
-                        count_private_school_going += 1
+                    if date_last_private_admit is not None and parser.parse(date_last_private_admit) < next_month:
+                        if (not date_return_private) or parser.parse(date_return_private) >= next_month:
+                            count_private_school_going += 1
+                        if parser.parse(date_last_private_admit) >= month:
+                            count_joined_private_school_during_month += 1
 
-                    if (primary_school_admit == 'yes' and parser.parse(date_primary_admit) >= month and
-                        parser.parse(date_primary_admit) < next_month):
-                        count_joined_primary_school_during_month += 1
-
-                    if (date_last_private_admit is not None and  parser.parse(date_last_private_admit) >= month and
-                        parser.parse(date_last_private_admit)< next_month):
-                        count_joined_private_school_during_month += 1
-
-                    if (date_return_private is not None and parser.parse(date_return_private) >= month and
-                        parser.parse(date_return_private) < next_month):
+                    if month <= parser.parse(date_return_private) < next_month:
                         count_stopped_private_school_during_month += 1
 
-                count_children_pse_in_awc = len(total_count) - count_primary_school_going - count_private_school_going
+                pse_in_awc = (len(total_count) - count_primary_school_going - count_private_school_going)
                 print("DATA FOR MONTH {} and age group {}".format(month, age_group))
                 print("Public School Going Children : {}".format(count_primary_school_going))
                 print("Private School Going Children: {}".format(count_private_school_going))
-                print("Children attending PSE in AWC: {}".format(count_children_pse_in_awc))
-                print("Children started going to Public School in month: {}".format(count_joined_primary_school_during_month))
-                print("Children started going to Private School in month: {}".format(count_joined_private_school_during_month))
-                print("Children stopped going to Private School in month: {}".format(count_stopped_private_school_during_month))
+                print("Children attending PSE in AWC: {}".format(pse_in_awc))
+                print("started going Public schl in month:{}".format(count_joined_primary_school_during_month))
+                print("started going Private schl in month:{}".format(count_joined_private_school_during_month))
+                print("stopped going Private schl in month:{}".format(count_stopped_private_school_during_month))
