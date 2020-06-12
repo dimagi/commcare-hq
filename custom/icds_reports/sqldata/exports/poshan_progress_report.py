2ff027b569a35b90bfea0209f9653a9efac1c75c
Shubham Bansal <shubhambansal17@hotmail.com>
2020-06-13 02:46:55 +0530

    👌 fixed code and tests as per review

--- custom/icds_reports/sqldata/exports/poshan_progress_report.py
+++ custom/icds_reports/sqldata/exports/poshan_progress_report.py
@@ -14,7 +14,8 @@ HEADERS_COMPREHENSIVE = [
     "Weighing efficiency", "Number of women in third trimester",
     "Number of trimester three women counselled on immediate and EBF",
     "% of trimester three women counselled on immediate and EBF",
-    "Children Eligible to have their height Measured", "Total number of children that were heighed in the month",
+    "Children Eligible to have their height Measured",
+    "Total number of children that had their height measured in the month",
     "Height Measurement Efficiency", "Number of children between 6 months -3 years, P&LW",
     "No of children between 6 months -3 years, P&LW provided THR for atleast 21+ days",
     "% of children between 6 months -3 years, P&LW provided THR for atleast 21+ days",
@@ -56,12 +57,16 @@ class PoshanProgressReport(object):
         self.beta = beta
         self.show_test = show_test
         self.layout = self.config['report_layout']
-        if self.config['data_format'] == 1:
-            self.report_type = 'month'
-        else:
-            self.report_type = 'quarter'
+        self.report_type = self.config['data_format']
+        self.row_constants = [HEADERS_COMPREHENSIVE[:], COLS_COMPREHENSIVE[:], HEADERS_SUMMARY[:], COLS_SUMMARY[:]]
+        if self.report_type == 'quarter':
             self.quarter = self.config['quarter']
             self.year = self.config['year']
+        if loc_level == 1:
+            self.row_constants[0].remove('District Name')
+            self.row_constants[1].remove('district_name')
+            self.row_constants[2].remove('District Name')
+            self.row_constants[3].remove('district_name')
 
     def _generate_quarter_years(self):
         months = []
@@ -70,111 +75,136 @@ class PoshanProgressReport(object):
             months.append(date(self.year, i, 1))
         return months
 
-    def structure_data(self, headers, cols, rows):
-        """
-        :param headers: headers of the excel file
-        :param cols: columns mapped with headers
-        :param rows: row data
-        :return: list of list contain the excel rows
-        """
-        excel_rows = [headers]
-        for row in rows:
-            row_data = []
-            for col in cols:
-                row_data.append(row[col])
-            excel_rows.append(row_data)
-        return excel_rows
+    def _indexes_to_remove(self):
+        _, cols_comprehensive, _, cols_summary = self.row_constants
+        extra_columns = list(set(cols_comprehensive) - set(cols_summary))
+        indexes_to_remove = [cols_comprehensive.index(col) for col in extra_columns]
+        return indexes_to_remove
 
-    def quarter_wise(self, filters, order_by):
+    def quarter_wise(self, filters, order_by, aggregation_level):
         """
         :param filters: quaterwise filter (months in [month array])
         :param order_by: order by columns
         :return: excel_rows
         """
+        headers_comprehensive, cols_comprehensive, headers_summary, cols_summary = self.row_constants
         query_set = PoshanProgressReportView.objects.filter(**filters).order_by(*order_by)
-        cols = COLS_COMPREHENSIVE[:]
-        cols.append('district_id')  # used as key for the dict
+
+        # it used to uniquely identify the row
+        # for district wise data we need district id
+        # for statewise we need state id
+        unique_id = 'district_id'
+        if aggregation_level == 1:
+            unique_id = 'state_id'
+        cols = cols_comprehensive[:]
+        cols.append(unique_id)  # used as key for the dict
         data = query_set.values(*cols)
         row_data_dict = {}
-        dummy_row = ['', ''] + [0 for _ in range(0, len(COLS_COMPREHENSIVE) - 2)]
-        headers = HEADERS_COMPREHENSIVE
+        dummy_row = ['' for _ in range(0, aggregation_level)] + [0 for _ in range(0, len(
+            headers_comprehensive) - aggregation_level)]
+        headers = headers_comprehensive
         # generate a dict
-        # {'disitrict_id': [contains the excel row with sum of col values for all months eg. m1+m2+m3]}
+        # {'unique_id': [contains the excel row with sum of col values for all months eg. m1+m2+m3]}
         for row in data:
             row_data = dummy_row
             for k, v in row.items():
                 if k == 'state_name' or k == 'district_name':
-                    row_data[COLS_COMPREHENSIVE.index(k)] = v
-                elif k != 'district_id':
-                    row_data[COLS_COMPREHENSIVE.index(k)] += v
-            row_data_dict[row['district_id']] = row_data
+                    row_data[cols_comprehensive.index(k)] = v
+                elif k != unique_id:
+                    row_data[cols_comprehensive.index(k)] += v if v else 0
+            row_data_dict[row[unique_id]] = row_data
 
-        # Calculating Average
+        # calculating average
         # m1+m2+m3/3
+        total_row = ['Total' for _ in range(0, aggregation_level)] + [0 for _ in range(0, len(
+            headers_comprehensive) - aggregation_level)]
         for k, v in row_data_dict.items():
-            for col in COLS_COMPREHENSIVE[2:]:
-                if col != 'state_name' and col != 'district_name':
-                    row_data_dict[k][COLS_COMPREHENSIVE.index(col)] = v[COLS_COMPREHENSIVE.index(col)] / 3
+            for col in cols_comprehensive[2:]:
+                if col != 'state_name' and col != 'district_name' and col != unique_id:
+                    val = v[cols_comprehensive.index(col)]
+                    row_data_dict[k][cols_comprehensive.index(col)] = val / 3 if val else 0
+                    total_row[cols_comprehensive.index(col)] += val if val else 0
 
-        # Calculating Percentage
+        row_data_dict["total_row"] = total_row
+
+        # calculating percentage
         # percent(current col) = 100 * (actual_value(prev col) / expected_value(prev prev col))
         for k, v in row_data_dict.items():
-            i = 8
-            while i < len(COLS_COMPREHENSIVE):
+            i = 6 + aggregation_level
+            while i < len(cols_comprehensive):
                 row_data_dict[k][i] = 100 * (v[i - 1] / v[i - 2]) if v[i - 2] else 0
                 i = i + 3
 
         if self.layout != 'comprehensive':
-            headers = HEADERS_SUMMARY
-            cols = COLS_SUMMARY[:]
-            extra_columns = list(set(COLS_COMPREHENSIVE) - set(COLS_SUMMARY))
-            indexes_to_remove = [COLS_COMPREHENSIVE.index(col) for col in extra_columns]
+            headers = headers_summary
             for k, v in row_data_dict.items():
-                row_data_dict[k] = [val for j, val in enumerate(v) if j not in indexes_to_remove]
+                row_data_dict[k] = [val for j, val in enumerate(v) if j not in self._indexes_to_remove()]
 
         excel_rows = [headers]
         for _, v in row_data_dict.items():
             excel_rows.append(v)
         return excel_rows
 
-    def month_wise(self, filters, order_by):
+    def month_wise(self, filters, order_by, aggregation_level):
         """
         :param filters: monthwise filter [month=month]
         :param order_by: order by columns
         :return: excel_rows
         """
         query_set = PoshanProgressReportView.objects.filter(**filters).order_by(*order_by)
-        if self.layout == 'comprehensive':
-            cols = COLS_COMPREHENSIVE
-            headers = HEADERS_COMPREHENSIVE
-        else:
-            cols = COLS_SUMMARY
-            headers = HEADERS_SUMMARY
-        data = query_set.values(*cols)
-        return self.structure_data(headers, cols, data)
+        headers_comprehensive, cols_comprehensive, headers_summary, cols_summary = self.row_constants
+        data = query_set.values(*cols_comprehensive)
+        excel_rows = [headers_comprehensive]
+        total_row = ['Total' for _ in range(0, aggregation_level)] + [0 for _ in range(0, len(
+            headers_comprehensive) - aggregation_level)]
+        for row in data:
+            row_data = []
+            for col in cols_comprehensive:
+                row_data.append(row[col])
+                if col != 'state_name' and col != 'district_name':
+                    total_row[cols_comprehensive.index(col)] += row[col] if row[col] else 0
+            excel_rows.append(row_data)
+        # calcuating percentage for total row
+        i = 6 + aggregation_level
+        while i < len(cols_comprehensive):
+            total_row[i] = 100 * (total_row[i - 1] / total_row[i - 2]) if total_row[i - 2] else 0
+            i = i + 3
+        excel_rows.append(total_row)
+
+        if self.layout != 'comprehensive':
+            excel_rows[0] = headers_summary
+            for i in range(0, len(excel_rows)):
+                excel_rows[i] = [val for j, val in enumerate(excel_rows[i]) if j not in self._indexes_to_remove()]
+        excel_rows[0] = headers_summary
+
+        return excel_rows
 
     def get_excel_data(self, location):
+        aggregation_level = self.loc_level
         filters = {}
-        filters['aggregation_level'] = 2  # this report is needed district wise only
-        if self.loc_level == 2:
-            filters['district_id'] = location
-        elif self.loc_level == 1:
-            filters['state_id'] = location
-        order_by = ('district_name', 'state_name')
+        filters['aggregation_level'] = aggregation_level  # this report is needed district wise only
+        export_filters = [['Generated at', india_now()]]
+        if location:
+            try:
+                locs = SQLLocation.objects.get(location_id=location).get_ancestors(include_self=True)
+                for loc in locs:
+                    export_filters.append([loc.location_type.name.title(), loc.name])
+                    location_key = '%s_id' % loc.location_type.code
+                    filters.update({
+                        location_key: loc.location_id,
+                    })
+            except SQLLocation.DoesNotExist:
+                pass
+        order_by = ('state_name', 'district_name')
         if self.report_type == 'month':
             filters['month'] = self.config['month']
-            excel_rows = self.month_wise(filters, order_by)
+            excel_rows = self.month_wise(filters, order_by, aggregation_level)
         else:
             filters['month__in'] = self._generate_quarter_years()
-            excel_rows = self.quarter_wise(filters, order_by)
+            excel_rows = self.quarter_wise(filters, order_by, aggregation_level)
 
-        filters = [['Generated at', india_now()]]
-        if location:
-            locs = SQLLocation.objects.get(location_id=location).get_ancestors(include_self=True)
-            for loc in locs:
-                filters.append([loc.location_type.name.title(), loc.name])
-        filters.append(['Report Layout', self.layout.title()])
-        filters.append(['Data Period', self.report_type.title()])
+        export_filters.append(['Report Layout', self.layout.title()])
+        export_filters.append(['Data Period', self.report_type.title()])
 
         return [
             [
@@ -183,6 +213,6 @@ class PoshanProgressReport(object):
             ],
             [
                 'Export Info',
-                filters
+                export_filters
             ]
         ]
