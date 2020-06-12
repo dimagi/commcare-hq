dbcae44df055f7ec3b4d4b04ff6131193a2f58ae
Rohit Negi <rohit25.negi@gmail.com>
2020-06-12 22:25:53 +0530

    converted to date

--- custom/icds_reports/management/commands/pull_Data_school.py
+++ custom/icds_reports/management/commands/pull_Data_school.py
@@ -1,17 +1,15 @@
 import datetime
-
+from dateutil import parser
 from django.core.management.base import BaseCommand
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from django.db import connections, transaction
 from dateutil.relativedelta import relativedelta
 from corehq.sql_db.connections import get_icds_ucr_citus_db_alias
 
+db_alias = get_icds_ucr_citus_db_alias()
+
 
 def _run_custom_sql_script(command):
-    db_alias = get_icds_ucr_citus_db_alias()
-    if not db_alias:
-        return
-
     with connections[db_alias].cursor() as cursor:
         cursor.execute(command)
         return cursor.fetchall()
@@ -36,32 +34,33 @@ class Command(BaseCommand):
                 count_joined_primary_school_during_month = 0
                 count_joined_private_school_during_month = 0
                 count_stopped_private_school_during_month = 0
-
+                total_count = 0
                 person_case_ids = [item[0] for item in _run_custom_sql_script(query.format(age_group=age_group,month=month))]
-                child_person_cases = case_accessor.get_cases(person_case_ids)
 
-                for case in child_person_cases:
+                for case in case_accessor.iter_cases(person_case_ids):
+                    total_count += 1
                     primary_school_admit = case.get_case_property('primary_school_admit')
                     date_primary_admit = case.get_case_property('date_primary_admit')
                     date_last_private_admit = case.get_case_property('date_last_private_admit')
                     date_return_private = case.get_case_property('date_return_private')
                     next_month = month + relativedelta(months=1)
-                    if primary_school_admit == 'yes' and date_primary_admit < next_month:
+                    if primary_school_admit == 'yes' and parser.parse(date_primary_admit) < next_month:
                         count_primary_school_going += 1
 
-                    if date_last_private_admit < next_month and (date_return_private is None or date_return_private >= next_month or date_return_private == ''):
+                    if date_last_private_admit is not None and parser.parse(date_last_private_admit) < next_month and \
+                        (date_return_private is None or parser.parse(date_return_private) >= next_month):
                         count_private_school_going += 1
 
-                    if primary_school_admit == 'yes' and date_primary_admit >= month and date_primary_admit < next_month:
+                    if primary_school_admit == 'yes' and parser.parse(date_primary_admit) >= month and parser.parse(date_primary_admit) < next_month:
                         count_joined_primary_school_during_month += 1
 
-                    if date_last_private_admit >= month and date_last_private_admit < next_month:
+                    if date_last_private_admit is not None and  parser.parse(date_last_private_admit) >= month and parser.parse(date_last_private_admit)< next_month:
                         count_joined_private_school_during_month += 1
 
-                    if date_return_private >= month and date_return_private < next_month:
+                    if date_return_private is not None and parser.parse(date_return_private) >= month and parser.parse(date_return_private) < next_month:
                         count_stopped_private_school_during_month += 1
 
-                count_children_pse_in_awc = len(child_person_cases) - count_primary_school_going - count_private_school_going
+                count_children_pse_in_awc = len(total_count) - count_primary_school_going - count_private_school_going
                 print("DATA FOR MONTH {} and age group {}".format(month, age_group))
                 print("Public School Going Children : {}".format(count_primary_school_going))
                 print("Private School Going Children: {}".format(count_private_school_going))
