7f63c6a3f7245a5ee91151c9814df5a20f54cf72
Biyeun Buczyk <biyeun@dimagi.com>
2020-06-15 22:12:25 -0500

    new custom test runner using mocha-headless-chrome :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,77 +1,122 @@
 module.exports = function(grunt) {
+    var headless = require('mocha-headless-chrome'),
+        _ = require('lodash'),
+        colors = require('colors');
+
     // use localhost unless we're running on travis
     var BASE_ADDRESS = process.env.WEB_TEST_PORT_8000_TCP_ADDR || 'localhost',
         BASE_URL = 'http://' + BASE_ADDRESS + ':8000/mocha/';
 
     /*
+     * To run all tests:
+     *    grunt test
+     *
+     * To run a single test:
+     *    grunt test:<app>
+     *
      * To add a new app to test:
      *  - Add the app name to this list
      *  - Create a test runner view at corehq/apps/<app>/templates/<app>/spec/mocha.html
      *  - Test in the browser at http://localhost:8000/mocha/<app>
      *
      * To add a new section to an existing app:
-     *  - Add <app>#<section> to this list
+     *  - Add <app>/<section> to this list
      *  - Create a test runner view at corehq/apps/<app>/templates/<app>/spec/<section>/mocha.html
      *  - Test in the browser at http://localhost:8000/mocha/<app>/<section>
      */
     var apps = [
         'app_manager',
-        'export#ko',
+        'export/ko',
         'notifications',
-        'reports_core#choiceListUtils',
+        'reports_core/choiceListUtils',
         'locations',
         'userreports',
         'cloudcare',
-        'cloudcare#form_entry',
+        'cloudcare/form_entry',
         'hqwebapp',
         'case_importer',
     ];
 
-    var customApps = [
+    var custom = [
         'icds_reports',
-        'champ'
+        'champ',
     ];
 
-    var mochaConfig = {},
-        watchConfig = {};
+    var runTest = function (queuedTests, taskPromise, finishedTests, failures) {
+        if (finishedTests === undefined) {
+            finishedTests = [];
+            failures = {};
+        }
 
-    var addToConfig = function(path) {
-        return function (app) {
-            var parts = app.split('#');
-            var appName = parts[0];
-            var config = parts[1];
+        if (queuedTests.length === 0) {
+            if (!_.isEmpty(failures)) {
+                printFailures(failures);
+                grunt.fail.fatal("Javascript tests failed.");
+            }
+            taskPromise();
+            return;
+        }
 
-            mochaConfig[app] = {
-                options: {
-                    urls: [BASE_URL + appName + '/' + (config ? config : '')],
-                    run: true,
-                    log: true,
-                    logErrors: true,
-                    reporter: 'Spec',
-                },
-            };
-            watchConfig[app] = {
-                files: [
-                    path + appName + '/static/' + appName + '/js/**/*.js',
-                    path + appName + '/static/' + appName + '/ko/**/*.js',
-                    path + appName + '/static/' + appName + '/spec/**/*.js',
-                ],
-                tasks: ['mocha:' + app],
-            };
-        };
+        var currentApp = queuedTests[0],
+            currentTestPath = BASE_URL + currentApp,
+            testText = "Running Test '" + currentApp + "'";
+
+        grunt.log.writeln("\n");
+        grunt.log.writeln(testText.bold);
+        grunt.log.write(currentTestPath.italic.cyan);
+
+        headless.runner({
+            file: currentTestPath,
+            visible: false,
+            timeout: 120000,
+            reporter: 'dot',
+        }).then(function (data) {
+            if (data.result.failures.length) {
+                failures[currentApp] = data.result.failures;
+            }
+            finishedTests.push(currentApp);
+            runTest(
+                _.without(queuedTests, currentApp),
+                taskPromise,
+                finishedTests,
+                failures
+            );
+        });
     };
 
-    apps.forEach(addToConfig('corehq/apps'));
-    customApps.forEach(addToConfig('custom/apps'));
+    var printFailures = function (failures) {
+        grunt.log.writeln("\n");
+        var numFailures = _.flatten(_.values(failures)).length + " test(s) failed.";
+        grunt.log.writeln(numFailures.bold.red);
 
-    grunt.initConfig({
-        mocha: mochaConfig,
-        watch: watchConfig,
-    });
+        _.forIn(failures, function (errors, appName) {
+            grunt.log.writeln("\n");
+            var failSummary = " has " + errors.length + " error(s)";
+            grunt.log.write(appName.bold);
+            grunt.log.writeln(failSummary.bold.red);
+            _.each(errors, function (error) {
+                grunt.log.write("\nERROR: ".bold.red);
+                grunt.log.writeln(error.fullTitle);
+                grunt.log.error(error.err.stack);
+                grunt.log.write("\n");
+            });
+        });
+    };
 
-    grunt.loadNpmTasks('grunt-mocha');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['mocha']);
+    grunt.task.registerTask(
+        'test',
+        'Runs Javascript Tests. Pass in an argument to run a specific test',
+        function (arg) {
+            var paths = _.concat(apps, custom);
+            if (arg) {
+                paths = [arg];
+            }
+            var testStatement = "Running tests: " + paths.join(', ');
+            grunt.log.writeln(testStatement.bold.green);
+            runTest(paths, this.async());
+        }
+    );
+
     grunt.registerTask('list', 'Lists all available apps to test', function() {
         apps.forEach(function(app) { console.log(app); });
     });
