3d0d5f6cb309bb1e6a9ae02d35596193854798bc
Dmitriy Chukhin <dchukhin@gmail.com>
2020-06-19 15:34:58 -0400

    convert strings to their types during CSV processing

--- corehq/util/workbook_reading/adapters/csv.py
+++ corehq/util/workbook_reading/adapters/csv.py
@@ -1,4 +1,7 @@
+import ast
 from contextlib import contextmanager
+import datetime
+from dateutil.parser import parse
 import csv
 import os
 
@@ -31,6 +34,74 @@ def open_csv_workbook(filename):
         raise SpreadsheetFileNotFound(str(error))
 
 
+def format_value(str_value):
+    """Attempt to format a str_value into its type."""
+    # Try to see if the str_value is a empty.
+    if str_value == "":
+        return None
+
+    # Try to see if the str_value is a number (float or integer).
+    try:
+        number = ast.literal_eval(str_value)
+        if int(number) == float(number):
+            return int(number)
+        else:
+            return float(number)
+    except (SyntaxError, ValueError):
+        pass
+
+    # Try to see if the str_value is a date or a time.
+    try:
+        parsed_datetime = parse(str_value)
+        # Because dateutil.parser.parse always returns a datetime, we do some guessing
+        # for whether the value is a datetime, a date, or a time.
+        today_midnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+        today_date = today_midnight.date()
+        # If the parsed_datetime time is midnight, then check if the user indicated
+        # midnight or not.
+        if parsed_datetime.time() == today_midnight.time():
+            if ("am" in str_value.lower() or "00:00" in str_value):
+                # The user has indicated to use midnight.
+                # If the str_value is long, then this must be a datetime.
+                if len(str_value) > 10:
+                    return parsed_datetime
+                else:
+                    # The str_value is short, so this must be a time.
+                    return parsed_datetime.time()
+            else:
+                # The user has not indicated to use midnight, so this must be a date.
+                return parsed_datetime.date()
+        elif parsed_datetime.date() == today_date:
+            # If the date is for today, but not for midnight, then it must either
+            # be a datetime or a time. We check its length to determine which one.
+            # If the str_value is long, then this must be a datetime.
+            if len(str_value) > 10:
+                return parsed_datetime
+            else:
+                # The str_value is short, so this must be a time.
+                return parsed_datetime.time()
+        else:
+            # Otherwise, this must be a datetime.
+            return parsed_datetime
+    except ValueError:
+        pass
+
+    # Try to see if the str_value is a boolean.
+    if str_value.lower() in ["true", "t", "yes", "y"]:
+        return True
+    elif str_value.lower() in ["false", "f", "no", "n"]:
+        return False
+
+    # Try to see if the str_value is a percent.
+    if "%" in str_value:
+        try:
+            return float(str_value.replace("%", "")) / 100
+        except ValueError:
+            pass
+
+    return str_value
+
+
 class _CSVWorkbookAdaptor(object):
 
     def __init__(self, csv_file):
@@ -38,7 +109,12 @@ class _CSVWorkbookAdaptor(object):
 
     def to_workbook(self):
         rows = []
+
+        # Loop through the rows, and change any string to its appropriate type.
         for row in csv.reader(self._file, delimiter=","):
-            rows.append(row)
+            formatted_row = []
+            for column in row:
+                formatted_row.append(format_value(column))
+            rows.append(formatted_row)
 
         return Workbook(worksheets=[make_worksheet(rows, title='Sheet1')])
