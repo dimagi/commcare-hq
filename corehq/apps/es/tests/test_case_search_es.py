a5c4e9270335c12d4704d97b120ad20b6ae866ec
Sravan Reddy <sravfeyn@gmail.com>
2020-06-22 16:47:41 +0530

    move case search es tests

--- corehq/apps/es/tests/test_case_search_es.py
+++ corehq/apps/es/tests/test_case_search_es.py
@@ -1,10 +1,24 @@
+import uuid
+
 from django.test.testcases import SimpleTestCase
+from django.test import TestCase
+from mock import MagicMock, patch
 from nose.plugins.attrib import attr
 
 from corehq.apps.case_search.const import RELEVANCE_SCORE
 from corehq.apps.es.case_search import CaseSearchES, flatten_result
 from corehq.apps.es.tests.utils import ElasticTestMixin
-from corehq.elastic import SIZE_LIMIT
+from corehq.apps.es.case_search import case_property_missing
+from corehq.elastic import get_es_new, SIZE_LIMIT
+from corehq.form_processor.tests.utils import FormProcessorTestUtils
+from corehq.pillows.case_search import CaseSearchReindexerFactory
+from corehq.pillows.mappings.case_search_mapping import (
+    CASE_SEARCH_INDEX,
+    CASE_SEARCH_INDEX_INFO,
+)
+from corehq.util.elastic import ensure_index_deleted
+from corehq.util.test_utils import create_and_save_a_case
+from pillowtop.es_utils import initialize_index_and_mapping
 
 
 @attr(es_test=True)
@@ -214,3 +228,115 @@ class TestCaseSearchES(ElasticTestMixin, SimpleTestCase):
                       }}},
                     'size': SIZE_LIMIT}
         self.checkQuery(query, expected)
+
+
+@attr(es_test=True)
+class TestCaseSearchLookups(TestCase):
+
+    def setUp(self):
+        self.domain = 'case_search_es'
+        super(TestCaseSearchLookups, self).setUp()
+        FormProcessorTestUtils.delete_all_cases()
+        self.elasticsearch = get_es_new()
+        ensure_index_deleted(CASE_SEARCH_INDEX)
+
+        # Bootstrap ES
+        initialize_index_and_mapping(get_es_new(), CASE_SEARCH_INDEX_INFO)
+
+    def tearDown(self):
+        ensure_index_deleted(CASE_SEARCH_INDEX)
+        super(TestCaseSearchLookups, self).tearDown()
+
+    def _make_case(self, domain, case_properties):
+        # make a case
+        case_properties = case_properties or {}
+        if '_id' in case_properties:
+            case_id = case_properties.pop('_id')
+        else:
+            case_id = uuid.uuid4().hex
+        case_name = 'case-name-{}'.format(uuid.uuid4().hex)
+        if 'owner_id' in case_properties:
+            owner_id = case_properties.pop('owner_id')
+        else:
+            owner_id = None
+        case = create_and_save_a_case(domain, case_id, case_name, case_properties, owner_id=owner_id)
+        return case
+
+    def _bootstrap_cases_in_es_for_domain(self, domain):
+        with patch('corehq.pillows.case_search.domains_needing_search_index',
+                   MagicMock(return_value=[domain])):
+            CaseSearchReindexerFactory(domain=domain).build().reindex()
+
+    def _assert_query_runs_correctly(self, domain, input_cases, query, output):
+        for case in input_cases:
+            self._make_case(domain, case)
+        self._bootstrap_cases_in_es_for_domain(domain)
+        self.elasticsearch.indices.refresh(CASE_SEARCH_INDEX)
+        self.assertItemsEqual(
+            query.get_ids(),
+            output
+        )
+
+    def test_simple_case_property_query(self):
+        self._assert_query_runs_correctly(
+            self.domain,
+            [
+                {'_id': 'c1', 'name': 'redbeard'},
+                {'_id': 'c2', 'name': 'blackbeard'},
+            ],
+            CaseSearchES().domain(self.domain).case_property_query("name", "redbeard"),
+            ['c1']
+        )
+
+    def test_multiple_case_search_queries(self):
+        query = (CaseSearchES().domain(self.domain)
+                 .case_property_query("name", "redbeard")
+                 .case_property_query("parrot_name", "polly"))
+        self._assert_query_runs_correctly(
+            self.domain,
+            [
+                {'_id': 'c1', 'name': 'redbeard', 'parrot_name': 'polly'},
+                {'_id': 'c2', 'name': 'blackbeard', 'parrot_name': 'polly'},
+                {'_id': 'c3', 'name': 'redbeard', 'parrot_name': 'molly'}
+            ],
+            query,
+            ['c1']
+        )
+
+    def test_multiple_case_search_queries_should_clause(self):
+        query = (CaseSearchES().domain(self.domain)
+                 .case_property_query("name", "redbeard")
+                 .case_property_query("parrot_name", "polly", clause="should"))
+        self._assert_query_runs_correctly(
+            self.domain,
+            [
+                {'_id': 'c1', 'name': 'redbeard', 'parrot_name': 'polly'},
+                {'_id': 'c2', 'name': 'blackbeard', 'parrot_name': 'polly'},
+                {'_id': 'c3', 'name': 'redbeard', 'parrot_name': 'molly'}
+            ],
+            query,
+            ['c1', 'c3']
+        )
+
+    def test_blacklisted_owner_ids(self):
+        self._assert_query_runs_correctly(
+            self.domain,
+            [
+                {'_id': 'c1', 'owner_id': '123'},
+                {'_id': 'c2', 'owner_id': '234'},
+            ],
+            CaseSearchES().domain(self.domain).blacklist_owner_id('123'),
+            ['c2']
+        )
+
+    def test_missing_case_property(self):
+        self._assert_query_runs_correctly(
+            self.domain,
+            [
+                {'_id': 'c2', 'name': 'blackbeard'},
+                {'_id': 'c3', 'name': ''},
+                {'_id': 'c4'},
+            ],
+            CaseSearchES().domain(self.domain).filter(case_property_missing('name')),
+            ['c3'] # todo; flag farid
+        )
--- testapps/test_pillowtop/tests/test_case_search_pillow.py
+++ testapps/test_pillowtop/tests/test_case_search_pillow.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.test import override_settings, TestCase
 from mock import MagicMock, patch
+from nose.plugins.attrib import attr
 
 from corehq.apps.case_search.const import SPECIAL_CASE_PROPERTIES_MAP
 from corehq.apps.case_search.exceptions import CaseSearchNotEnabledException
@@ -15,7 +16,6 @@ from corehq.apps.change_feed.tests.utils import get_test_kafka_consumer
 from corehq.apps.change_feed.topics import get_multi_topic_offset
 from corehq.apps.es import CaseSearchES
 from corehq.apps.userreports.tests.utils import doc_to_change
-from corehq.apps.es.case_search import case_property_missing
 from corehq.elastic import get_es_new
 from corehq.form_processor.tests.utils import FormProcessorTestUtils
 from corehq.pillows.case import get_case_pillow
@@ -30,7 +30,6 @@ from corehq.pillows.mappings.case_search_mapping import (
 )
 from corehq.util.elastic import ensure_index_deleted
 from corehq.util.test_utils import create_and_save_a_case
-from nose.plugins.attrib import attr
 from pillowtop.es_utils import initialize_index_and_mapping
 
 
@@ -146,19 +145,11 @@ class CaseSearchPillowTest(TestCase):
 
     def _make_case(self, domain=None, case_properties=None):
         # make a case
-        case_properties = case_properties or {}
-        if '_id' in case_properties:
-            case_id = case_properties.pop('_id')
-        else:
-            case_id = uuid.uuid4().hex
+        case_id = uuid.uuid4().hex
         case_name = 'case-name-{}'.format(uuid.uuid4().hex)
         if domain is None:
             domain = self.domain
-        if 'owner_id' in case_properties:
-            owner_id = case_properties.pop('owner_id')
-        else:
-            owner_id = None
-        case = create_and_save_a_case(domain, case_id, case_name, case_properties, owner_id=owner_id)
+        case = create_and_save_a_case(domain, case_id, case_name, case_properties)
         return case
 
     def _assert_case_in_es(self, domain, case):
@@ -188,87 +179,9 @@ class CaseSearchPillowTest(TestCase):
         results = CaseSearchES().run()
         self.assertEqual(0, results.total)
 
-    def _bootstrap_cases_in_es_for_domain(self, domain, create_case=True):
-        case = self._make_case(domain) if create_case else None
+    def _bootstrap_cases_in_es_for_domain(self, domain):
+        case = self._make_case(domain)
         with patch('corehq.pillows.case_search.domains_needing_search_index',
                    MagicMock(return_value=[domain])):
             CaseSearchReindexerFactory(domain=domain).build().reindex()
         return case
-
-    def _assert_query_runs_correctly(self, domain, input_cases, query, output):
-        for case in input_cases:
-            self._make_case(domain, case)
-        self._bootstrap_cases_in_es_for_domain(domain, create_case=False)
-        self.elasticsearch.indices.refresh(CASE_SEARCH_INDEX)
-        self.assertItemsEqual(
-            query.get_ids(),
-            output
-        )
-
-    @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
-    def test_simple_case_property_query(self):
-        self._assert_query_runs_correctly(
-            self.domain,
-            [
-                {'_id': 'c1', 'name': 'redbeard'},
-                {'_id': 'c2', 'name': 'blackbeard'},
-            ],
-            CaseSearchES().domain(self.domain).case_property_query("name", "redbeard"),
-            ['c1']
-        )
-
-    @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
-    def test_multiple_case_search_queries(self):
-        query = (CaseSearchES().domain(self.domain)
-                 .case_property_query("name", "redbeard")
-                 .case_property_query("parrot_name", "polly"))
-        self._assert_query_runs_correctly(
-            self.domain,
-            [
-                {'_id': 'c1', 'name': 'redbeard', 'parrot_name': 'polly'},
-                {'_id': 'c2', 'name': 'blackbeard', 'parrot_name': 'polly'},
-                {'_id': 'c3', 'name': 'redbeard', 'parrot_name': 'molly'}
-            ],
-            query,
-            ['c1']
-        )
-
-    @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
-    def test_multiple_case_search_queries_should_clause(self):
-        query = (CaseSearchES().domain(self.domain)
-                 .case_property_query("name", "redbeard")
-                 .case_property_query("parrot_name", "polly", clause="should"))
-        self._assert_query_runs_correctly(
-            self.domain,
-            [
-                {'_id': 'c1', 'name': 'redbeard', 'parrot_name': 'polly'},
-                {'_id': 'c2', 'name': 'blackbeard', 'parrot_name': 'polly'},
-                {'_id': 'c3', 'name': 'redbeard', 'parrot_name': 'molly'}
-            ],
-            query,
-            ['c1', 'c3']
-        )
-
-    @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
-    def test_blacklisted_owner_ids(self):
-        self._assert_query_runs_correctly(
-            self.domain,
-            [
-                {'_id': 'c1', 'owner_id': '123'},
-                {'_id': 'c2', 'owner_id': '234'},
-            ],
-            CaseSearchES().domain(self.domain).blacklist_owner_id('123'),
-            ['c2']
-        )
-
-    def test_missing_case_property(self):
-        self._assert_query_runs_correctly(
-            self.domain,
-            [
-                {'_id': 'c2', 'name': 'blackbeard'},
-                {'_id': 'c3', 'name': ''},
-                {'_id': 'c4'},
-            ],
-            CaseSearchES().domain(self.domain).filter(case_property_missing('name')),
-            ['c3'] # todo; flag farid
-        )
