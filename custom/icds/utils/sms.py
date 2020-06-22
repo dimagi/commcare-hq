fb3bc7fedb5537a693e09288138e5b2ab5fc14d5
Manish Kangia <mkangia@dimagi.com>
2020-06-23 03:43:18 +0530

    hard code time restrictions to not run task itself

--- custom/icds/utils/sms.py
+++ custom/icds/utils/sms.py
@@ -1,6 +1,10 @@
 import pytz
 from datetime import timedelta
 
+from corehq.apps.sms.api import get_utcnow
+from corehq.util.timezones.conversions import ServerTime
+
+
 def get_next_available_time(domain_object, domain_now):
     if domain_object.restricted_sms_times:
         restricted_sms_time = domain_object.restricted_sms_times[0]
@@ -21,3 +25,11 @@ def get_next_available_time(domain_object, domain_now):
                 second=0)
             next_available_time = next_available_time.astimezone(pytz.utc).replace(tzinfo=None)
         return next_available_time
+
+
+def can_send_sms_now():
+    # from 9am to 9pm IST
+    utcnow = get_utcnow()
+    india_timezone = pytz.timezone('Asia/Kolkata')
+    server_now = ServerTime(utcnow).user_time(india_timezone).done()
+    return 9 <= server_now.hour < 21
