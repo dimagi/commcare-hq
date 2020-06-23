bfe971697a02f3955b94496ef2670008f73fd527
Dmitriy Chukhin <dchukhin@gmail.com>
2020-06-23 09:26:15 -0400

    move format_value() to utils.py file, and add tests for it

--- corehq/util/workbook_reading/adapters/csv.py
+++ corehq/util/workbook_reading/adapters/csv.py
@@ -1,6 +1,4 @@
-import ast
 from contextlib import contextmanager
-import datetime
 import csv
 import os
 
@@ -9,8 +7,8 @@ from corehq.util.workbook_reading import (
     SpreadsheetFileNotFound,
     Workbook,
 )
-from dimagi.utils.parsing import string_to_boolean, string_to_datetime
 from .raw_data import make_worksheet
+from .utils import format_str_to_its_type
 
 
 @contextmanager
@@ -31,79 +29,6 @@ def open_csv_workbook(filename):
         raise SpreadsheetFileNotFound(str(error))
 
 
-def format_value(str_value):
-    """Attempt to format a str_value into its type."""
-    # Try to see if the str_value is a empty.
-    if str_value == "":
-        return None
-
-    # Try to see if the str_value is a number (float or integer).
-    try:
-        number = ast.literal_eval(str_value)
-        if int(number) == float(number):
-            return int(number)
-        else:
-            return float(number)
-    except (SyntaxError, ValueError):
-        pass
-
-    # Try to see if the str_value is a date or a time.
-    try:
-        parsed_datetime = string_to_datetime(str_value)
-        # Because dateutil.parser.parse always returns a datetime, we do some guessing
-        # for whether the value is a datetime, a date, or a time.
-        today_midnight = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-        today_date = today_midnight.date()
-        # If the parsed_datetime time is midnight, then check if the user indicated
-        # midnight or not.
-        if parsed_datetime.time() == today_midnight.time():
-            if ("am" in str_value.lower() or "00:00" in str_value):
-                # The user has indicated to use midnight.
-                # If the str_value is long, then this must be a datetime.
-                if len(str_value) > 10:
-                    return parsed_datetime
-                else:
-                    # The str_value is short, so this must be a time.
-                    return parsed_datetime.time()
-            else:
-                # The user has not indicated to use midnight, so this must be a date.
-                return parsed_datetime.date()
-        elif parsed_datetime.date() == today_date:
-            # We have already handled the case that the user specified a date (but
-            # not a time), so parsed_datetime is a datetime with a time of midnight.
-            # If the parsed_datetime has a date of today, but a time of not midnight,
-            # then either the user specified both the date and the time (meaning
-            # we should return the datetime), or the user specified only the time (so
-            # we should return only the time).
-
-            # If the str_value is long, then this must be a datetime.
-            if len(str_value) > 10:
-                return parsed_datetime
-            else:
-                # The str_value is short, so this must be a time.
-                return parsed_datetime.time()
-        else:
-            # Otherwise, this must be a datetime.
-            return parsed_datetime
-    except ValueError:
-        pass
-
-    # Try to see if the str_value is a boolean.
-    try:
-        return string_to_boolean(str_value)
-    except ValueError:
-        pass
-
-    # Try to see if the str_value is a percent.
-    if "%" in str_value:
-        try:
-            return float(str_value.replace("%", "")) / 100
-        except ValueError:
-            pass
-
-    return str_value
-
-
 class _CSVWorkbookAdaptor(object):
 
     def __init__(self, csv_file):
@@ -116,7 +41,7 @@ class _CSVWorkbookAdaptor(object):
         for row in csv.reader(self._file, delimiter=","):
             formatted_row = []
             for column in row:
-                formatted_row.append(format_value(column))
+                formatted_row.append(format_str_to_its_type(column))
             rows.append(formatted_row)
 
         return Workbook(worksheets=[make_worksheet(rows, title='Sheet1')])
