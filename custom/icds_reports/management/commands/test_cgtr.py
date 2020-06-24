058475ea13cdbb62f21b645b79a53b5187bdeabb
Shubham Bansal <shubhambansal17@hotmail.com>
2020-06-24 17:51:54 +0530

    ✏️ fix date

--- custom/icds_reports/management/commands/test_cgtr.py
+++ custom/icds_reports/management/commands/test_cgtr.py
@@ -13,7 +13,7 @@ class Command(BaseCommand):
         t = GrowthTrackerExport(
             config={
                 'domain': 'icds-cas',
-                'month': date(2017, 5, 1),
+                'month': date(2020, 1, 1),
                 'district_id': district_id
             },
             loc_level=2).get_excel_data('d1')
