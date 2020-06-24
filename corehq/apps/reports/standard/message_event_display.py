6a6afcae84d5063fc38d37698be44cb36744f300
Daniel Miller <dmiller@dimagi.com>
2020-06-24 17:50:11 -0400

    Remove superfluous else statements

--- corehq/apps/reports/standard/message_event_display.py
+++ corehq/apps/reports/standard/message_event_display.py
@@ -83,20 +83,16 @@ def get_sms_status_display(sms):
         if error:
             error_message = SMS.ERROR_MESSAGES.get(error, error)
             return '%s - %s' % (_('Error'), _(error_message))
-        else:
-            return _('Error')
-    elif not sms.processed:
+        return _('Error')
+    if not sms.processed:
         return _('Queued')
-    else:
-        if sms.direction == INCOMING:
-            return _('Received')
-        elif sms.direction == OUTGOING:
-            if sms.workflow == WORKFLOW_FORWARD:
-                return _('Forwarded')
-            else:
-                return _('Sent')
-        else:
-            return _('Unknown')
+    if sms.direction == INCOMING:
+        return _('Received')
+    if sms.direction == OUTGOING:
+        if sms.workflow == WORKFLOW_FORWARD:
+            return _('Forwarded')
+        return _('Sent')
+    return _('Unknown')
 
 
 def _get_keyword_display(keyword_id, content_cache):
