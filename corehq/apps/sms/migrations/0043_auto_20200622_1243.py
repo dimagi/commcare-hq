c07cba319c583f97df94448474f972722a508fae
Simon Kelly <skelly@dimagi.com>
2020-06-25 11:52:38 +0200

    remove duplicate migrations

--- corehq/apps/sms/migrations/0043_auto_20200622_1243.py
+++ corehq/apps/sms/migrations/0043_auto_20200622_1243.py
@@ -13,36 +13,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='InfobipBackend',
-            fields=[
-            ],
-            options={
-                'proxy': True,
-                'indexes': [],
-            },
-            bases=('sms.sqlsmsbackend',),
-        ),
-        migrations.CreateModel(
-            name='PinpointBackend',
-            fields=[
-            ],
-            options={
-                'proxy': True,
-                'indexes': [],
-            },
-            bases=('sms.sqlsmsbackend',),
-        ),
-        migrations.AlterField(
-            model_name='messagingevent',
-            name='status',
-            field=models.CharField(choices=[('PRG', 'In Progress'), ('CMP', 'Completed'), ('NOT', 'Not Completed'), ('ERR', 'Error'), ('SND', 'Email Sent'), ('DEL', 'Email Delivered')], max_length=3),
-        ),
-        migrations.AlterField(
-            model_name='messagingsubevent',
-            name='status',
-            field=models.CharField(choices=[('PRG', 'In Progress'), ('CMP', 'Completed'), ('NOT', 'Not Completed'), ('ERR', 'Error'), ('SND', 'Email Sent'), ('DEL', 'Email Delivered')], max_length=3),
-        ),
         migrations.AlterField(
             model_name='email',
             name='messaging_subevent',
