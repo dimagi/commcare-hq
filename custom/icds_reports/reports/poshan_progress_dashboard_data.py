15b3ae03024905232bb6af01a48b037f351f7292
Shubham Bansal <shubhambansal17@hotmail.com>
2020-06-29 19:23:54 +0530

    🐛 fix in the failing endpoint for aggregated rows in case of no rows

--- custom/icds_reports/reports/poshan_progress_dashboard_data.py
+++ custom/icds_reports/reports/poshan_progress_dashboard_data.py
@@ -20,14 +20,14 @@ from custom.icds_reports.utils import apply_exclude, generate_quarter_months, ca
 
 def calculate_percentage_single_row(row, truncate_out=True):
     for k, v in PPR_COLS_PERCENTAGE_RELATIONS.items():
-        num = row[v[0]]
-        den = row[v[1]]
+        num = row.get(v[0], 0)
+        den = row.get(v[1], 1)
         extra_number = v[2] if len(v) > 2 else None
         row[k] = calculate_percent(num, den, extra_number, truncate_out)
         # calculation is done on decimal values
         # and then round off to nearest integer
-        row[v[0]] = round(row[v[0]])
-        row[v[1]] = round(row[v[1]])
+        row[v[0]] = round(num)
+        row[v[1]] = round(den)
     return row
 
 
@@ -54,7 +54,7 @@ def calculate_aggregated_row(data, aggregation_level, data_period, unique_id):
     aggregated_row = calculate_percentage_single_row(deepcopy(aggregated_row))
     # rounding values
     for col in ['num_launched_districts', 'num_launched_blocks', 'num_launched_states']:
-        aggregated_row[col] = round(aggregated_row[col])
+        aggregated_row[col] = round(aggregated_row.get(col, 0))
     aggregated_row = prepare_structure_aggregated_row(deepcopy(aggregated_row),
                                                       aggregated_row['num_launched_states'],
                                                       aggregation_level)
