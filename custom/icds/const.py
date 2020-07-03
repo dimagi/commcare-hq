8d632f091ad0f3769943bf579a1dd4eb8e88f922
Manish Kangia <mkangia@dimagi.com>
2020-07-03 21:49:36 +0530

    update to new references

--- custom/icds/const.py
+++ custom/icds/const.py
@@ -6,10 +6,10 @@ IS_ICDS_ENVIRONMENT = settings.SERVER_ENVIRONMENT in settings.ICDS_ENVS
 SUPERVISOR_APP_ID = '48cc1709b7f62ffea24cc6634a00660c'
 VHND_SURVEY_XMLNS = "http://openrosa.org/formdesigner/A1C9EF1B-8B42-43AB-BA81-9484DB9D8293"
 
-CHILDREN_WEIGHED_REPORT_ID = 'static-icds-cas-static-ls_children_weighed'
+CHILDREN_WEIGHED_REPORT_ID = 'static-icds-cas-ls_v2_children_weighed'
 DAYS_AWC_OPEN_REPORT_ID = 'static-icds-cas-static-ls_awc_days_open'
 HOME_VISIT_REPORT_ID = 'static-icds-cas-static-ls_timely_home_visits'
-THR_REPORT_ID = 'static-icds-cas-static-ls_thr_30_days'
+THR_REPORT_ID = 'static-icds-cas-ls_v2_thr_30_days'
 
 AWC_LOCATION_TYPE_CODE = 'awc'
 SUPERVISOR_LOCATION_TYPE_CODE = 'supervisor'
