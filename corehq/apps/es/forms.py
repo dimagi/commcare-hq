93dff3f98c10fc5129addc123c94c932f30dbee8
Sravan Reddy <sravfeyn@gmail.com>
2020-07-10 17:07:26 +0530

    replace literals with orm

--- corehq/apps/es/forms.py
+++ corehq/apps/es/forms.py
@@ -11,7 +11,7 @@ from .es_query import HQESQuery
 class FormES(HQESQuery):
     index = 'forms'
     default_filters = {
-        'is_xform_instance': {"term": {"doc_type": "xforminstance"}},
+        'is_xform_instance': filters.term("doc_type", "xforminstance"),
         'has_xmlns': filters.field_exists("xmlns"),
         'has_user': filters.field_exists("form.meta.userID"),
         'has_domain': filters.field_exists("domain"),
