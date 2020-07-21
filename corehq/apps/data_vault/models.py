15796a4873853349f001c59045cd74b43ddd9109
Manish Kangia <mkangia@dimagi.com>
2020-07-21 16:21:10 +0530

    use default argument and set index using Meta - https://stackoverflow.com/a/50926644

--- corehq/apps/data_vault/models.py
+++ corehq/apps/data_vault/models.py
@@ -3,13 +3,18 @@ import secrets
 from django.db import models
 
 
+def _default_key():
+    return secrets.token_urlsafe(16)
+
+
 class VaultStore(models.Model):
     id = models.BigAutoField(primary_key=True)
-    key = models.CharField(max_length=25, unique=True)
+    key = models.CharField(max_length=25, default=_default_key)
     value = models.CharField(max_length=255)
     identifier = models.CharField(max_length=255, blank=True, null=True)
     date_created = models.DateTimeField()
 
-    def __init__(self, *args, **kwargs):
-        super(VaultStore, self).__init__(*args, **kwargs)
-        self.key = secrets.token_urlsafe(16)
+    class Meta:
+        indexes = [
+            models.Index(fields=['key'])
+        ]
