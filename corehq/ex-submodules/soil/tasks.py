4fe1892776406828c0997fb8885b0c6719e884fe
Farid Rener <proteusvacuum@gmail.com>
2020-07-24 13:56:10 -0400

    Remove unused soil demo view

--- corehq/ex-submodules/soil/tasks.py
+++ corehq/ex-submodules/soil/tasks.py
@@ -1,23 +1,8 @@
 from celery.task import task, periodic_task
-import time
-from django.core.cache import cache
-from soil import CachedDownload
-import uuid
 from celery.schedules import crontab
 from soil.heartbeat import write_file_heartbeat, write_cache_heartbeat
 from soil.util import expose_cached_download
 from django.conf import settings
-
-
-@task(serializer='pickle')
-def demo_sleep(download_id, howlong=5, expiry=1*60*60):
-    """
-    Demo the downloader, with sleep
-    """
-    time.sleep(howlong)
-    temp_id = uuid.uuid4().hex
-    cache.set(temp_id, "It works!", expiry)
-    cache.set(download_id, CachedDownload(temp_id), expiry)
 
 
 @task(serializer='pickle')
@@ -41,7 +26,7 @@ def prepare_download(download_id, payload_func, content_disposition,
 def heartbeat():
     """
     A heartbeat, used to confirm that celery is alive and kicking.
-    
+
     This heartbeat will stop if either celery or celeryd go down.
     """
     write_file_heartbeat()
