9db147ee5894c2e8d9fc559abe074f2f0647b0a0
Cal Ellowitz <calellowitz@gmail.com>
2020-07-28 14:57:42 -0400

    add test for custom opt keywords

--- corehq/apps/sms/tests/opt_tests.py
+++ corehq/apps/sms/tests/opt_tests.py
@@ -5,9 +5,10 @@ from corehq.apps.accounting.tests.base_tests import BaseAccountingTest
 from corehq.apps.accounting.tests.utils import DomainSubscriptionMixin
 from corehq.apps.accounting.utils import clear_plan_version_cache
 from corehq.apps.domain.models import Domain
+from corehq.messaging.smsbackends.test.models import SQLTestSMSBackend
 from corehq.apps.sms.api import incoming, send_sms_to_verified_number
 from corehq.apps.sms.messages import MSG_OPTED_IN, MSG_OPTED_OUT, get_message
-from corehq.apps.sms.models import SMS, PhoneBlacklist, PhoneNumber
+from corehq.apps.sms.models import SMS, PhoneBlacklist, PhoneNumber, SQLMobileBackendMapping, SQLMobileBackend
 from corehq.apps.sms.tests.util import (
     delete_domain_phone_numbers,
     setup_default_sms_test_backend,
@@ -30,11 +31,26 @@ class OptTestCase(DomainSubscriptionMixin, TestCase):
 
         cls.setup_subscription(cls.domain, SoftwarePlanEdition.ADVANCED)
         cls.backend, cls.backend_mapping = setup_default_sms_test_backend()
+        cls.custom_backend = SQLTestSMSBackend.objects.create(
+            name='MOBILE_BACKEND_CUSTOM_TEST',
+            is_global=True,
+            hq_api_id=SQLTestSMSBackend.get_api_id(),
+            opt_in_keywords=['RESTART'],
+            opt_out_keywords=['RESTOP']
+        )
+        cls.custom_backend_mapping = SQLMobileBackendMapping.objects.create(
+            is_global=True,
+            backend_type=SQLMobileBackend.SMS,
+            prefix='1',
+            backend=cls.custom_backend,
+        )
 
     @classmethod
     def tearDownClass(cls):
         cls.backend_mapping.delete()
         cls.backend.delete()
+        cls.custom_backend_mapping.delete()
+        cls.custom_backend.delete()
         FormProcessorTestUtils.delete_all_cases(cls.domain)
         cls.teardown_subscriptions()
         cls.domain_obj.delete()
@@ -113,6 +129,42 @@ class OptTestCase(DomainSubscriptionMixin, TestCase):
 
         send_sms_to_verified_number(v, 'hello')
         sms = self.get_last_sms('+99912345678')
+        self.assertEqual(sms.direction, 'O')
+        self.assertEqual(sms.text, 'hello')
+        self.assertFalse(sms.error)
+        self.assertIsNone(sms.system_error_message)
+
+    def test_custom_opt_keywords(self):
+        self.assertEqual(PhoneBlacklist.objects.count(), 0)
+
+        incoming('19912345678', 'join opt-test', 'TEST')
+        v = PhoneNumber.get_two_way_number('19912345678')
+        self.assertIsNotNone(v)
+
+        send_sms_to_verified_number(v, 'hello')
+        sms = self.get_last_sms('+19912345678')
+        self.assertEqual(sms.direction, 'O')
+        self.assertEqual(sms.text, 'hello')
+
+        incoming('99912345678', 'restop', 'TEST')
+        self.assertEqual(PhoneBlacklist.objects.count(), 1)
+        phone_number = PhoneBlacklist.objects.get(phone_number='19912345678')
+        self.assertFalse(phone_number.send_sms)
+
+        send_sms_to_verified_number(v, 'hello')
+        sms = self.get_last_sms('+19912345678')
+        self.assertEqual(sms.direction, 'O')
+        self.assertEqual(sms.text, 'hello')
+        self.assertTrue(sms.error)
+        self.assertEqual(sms.system_error_message, SMS.ERROR_PHONE_NUMBER_OPTED_OUT)
+
+        incoming('19912345678', 'restart', 'TEST')
+        self.assertEqual(PhoneBlacklist.objects.count(), 1)
+        phone_number = PhoneBlacklist.objects.get(phone_number='19912345678')
+        self.assertTrue(phone_number.send_sms)
+
+        send_sms_to_verified_number(v, 'hello')
+        sms = self.get_last_sms('+19912345678')
         self.assertEqual(sms.direction, 'O')
         self.assertEqual(sms.text, 'hello')
         self.assertFalse(sms.error)
