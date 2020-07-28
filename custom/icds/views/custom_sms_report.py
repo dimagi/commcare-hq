2e824f9b3f8d887ec4b7b22df8ae77d1df51dd3b
Amit Phulera <amitphulera999@gmail.com>
2020-07-28 09:51:49 +0530

    update: page_context for SMSUsageReport view

--- custom/icds/views/custom_sms_report.py
+++ custom/icds/views/custom_sms_report.py
@@ -8,6 +8,7 @@ from corehq.apps.sms.views import BaseMessagingSectionView
 from custom.icds.forms import CustomSMSReportRequestForm
 from custom.icds.tasks.sms import send_custom_sms_report
 from custom.icds_core.const import SMSUsageReport_urlname
+from custom.icds.utils.custom_sms_report import CustomSMSReportTracker
 
 
 @method_decorator(toggles.ICDS_CUSTOM_SMS_REPORT.required_decorator(), name='dispatch')
@@ -15,16 +16,27 @@ class SMSUsageReport(BaseMessagingSectionView):
     template_name = 'icds/sms/custom_sms_report.html'
     urlname = SMSUsageReport_urlname
     page_title = _('Custom SMS Usage Report')
+    report_tracker = CustomSMSReportTracker()
 
     @use_daterangepicker
     def dispatch(self, *args, **kwargs):
+        messages.info(self.request, _('Please note that this report takes a few hours to process.'))
         return super().dispatch(*args, **kwargs)
 
     @property
     def page_context(self):
+        reports_in_progress = self.report_tracker.active_reports
+        report_count = len(reports_in_progress)
+        disable_submit = True if report_count >= 3 else False
+        if disable_submit:
+            messages.info(self.request, 'Only 3 concurrent reports are allowed at a time.')
+        if report_count > 0:
+            display_message = _prepare_display_message(reports_in_progress, report_count)
+            messages.info(self.request, display_message)
         self.request_form = CustomSMSReportRequestForm()
         return {
             'request_form': self.request_form,
+            'disable_submit': disable_submit,
         }
 
     def post(self, request, *args, **kwargs):
@@ -47,3 +59,22 @@ class SMSUsageReport(BaseMessagingSectionView):
                 for message in message_list:
                     messages.error(self.request, message)
         return self.get(*args, **kwargs)
+
+
+def _prepare_display_message(reports_in_progress, report_count):
+    message = _('Currently {reports_count} {report_text} for duration\n').format(
+        reports_count=report_count,
+        report_text='reports' if report_count>1 else 'report',
+    )
+    for index, report in enumerate(reports_in_progress):
+        message+=_('{start_date}-{end_date}').format(
+            start_date=report['start_date'],
+            end_date=report['end_date']
+        )
+        if index!=report_count-1:
+            message+=', '
+    message+=_(' {is_or_are} in progress').format(
+        is_or_are='are' if report_count>1 else 'is',
+    )
+    return message
+
