4a6475638dc7fe050fd17fe37ff1175f0adf0d1e
Amit Phulera <amitphulera999@gmail.com>
2020-07-28 10:43:34 +0530

    add: the missing import in commit

--- custom/icds/tasks/sms.py
+++ custom/icds/tasks/sms.py
@@ -30,6 +30,7 @@ from corehq.apps.sms.models import (
 from corehq.util.celery_utils import periodic_task_on_envs, task
 from corehq.util.files import file_extention_from_filename
 from custom.icds_core.view_utils import is_icds_cas_project
+from custom.icds.utils.custom_sms_report import CustomSMSReportTracker
 
 
 @periodic_task_on_envs(settings.ICDS_ENVS, run_every=crontab(day_of_month='2', minute=0, hour=0), queue='sms_queue')
