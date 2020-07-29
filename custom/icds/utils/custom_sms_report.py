e0a81915f0f611aa4e5528204dcac56ebc6a119a
Amit Phulera <amitphulera999@gmail.com>
2020-07-29 22:25:08 +0530

    refactor: CustomSMSReportTracker

--- custom/icds/utils/custom_sms_report.py
+++ custom/icds/utils/custom_sms_report.py
@@ -3,39 +3,19 @@ from dimagi.utils.couch.cache.cache_core import get_redis_client
 
 
 class CustomSMSReportTracker(object):
-    def __init__(self):
+    def __init__(self, domain):
         self.client = get_redis_client()
+        self.domain = domain
 
     @property
     def report_key(self):
-        return 'custom-sms-report-tracker'
+        return '{domain}-custom-sms-report-tracker'.format(domain=self.domain)
 
     @property
     def key_expiry(self):
         return 48 * 60 * 60
 
-    @property
-    def active_reports(self):
-        unfiltered_report_list = self.client.get(self.report_key)
-        if(unfiltered_report_list):
-            reports = json.loads(unfiltered_report_list)
-        else:
-            reports = []
-        return reports
-
-    def clean_all_active_reports(self):
-        self.client.delete(self.report_key)
-
-    def remove_report_from_list(self, start_date, end_date):
-        reports = self.active_reports
-        updated_reports = [report for report in reports
-                    if report['start_date'] != start_date or report['end_date'] != end_date]
-        if len(updated_reports) == 0:
-            self.clean_all_active_reports()
-        else:
-            self.save_report_info(updated_reports)
-
-    def add_report_to_list(self, start_date, end_date):
+    def add_report_to_list(self, start_date: str, end_date: str):
         reports = self.active_reports
         reports.append({
             'start_date': start_date,
@@ -43,6 +23,26 @@ class CustomSMSReportTracker(object):
         })
         self.save_report_info(reports)
 
+    @property
+    def active_reports(self):
+        reports = []
+        unfiltered_report_list = self.client.get(self.report_key)
+        if(unfiltered_report_list):
+            reports = json.loads(unfiltered_report_list)
+        return reports
+
     def save_report_info(self, reports):
         self.client.set(self.report_key, json.dumps(reports))
         self.client.expire(self.report_key, self.key_expiry)
+
+    def remove_report_from_list(self, start_date: str, end_date: str):
+        reports = self.active_reports
+        updated_reports = [report for report in reports
+                    if report['start_date'] != start_date or report['end_date'] != end_date]
+        if len(updated_reports) == 0:
+            self.clear_all_reports()
+        else:
+            self.save_report_info(updated_reports)
+
+    def clear_all_reports(self):
+        self.client.delete(self.report_key)
