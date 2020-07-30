718c9274241fe414daeb709cc3548c3ffa3990cd
Simon Kelly <skelly@dimagi.com>
2020-07-30 12:32:59 +0200

    no need to load the extension points

--- corehq/extensions/app_config.py
+++ corehq/extensions/app_config.py
@@ -6,6 +6,5 @@ class ExtensionAppConfig(AppConfig):
     name = 'corehq.extensions'
 
     def ready(self):
-        from corehq.extensions import extension_points, extension_manager
-        extension_manager.add_extension_points(extension_points)
+        from corehq.extensions import extension_manager
         extension_manager.load_extensions(settings.COMMCARE_EXTENSIONS)
