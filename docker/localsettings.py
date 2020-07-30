844e18b9c432d98dbfc675006ced3e60037105e0
Sravan Reddy <sravfeyn@gmail.com>
2020-07-30 11:53:46 +0530

    swap 7/2 matrix

--- docker/localsettings.py
+++ docker/localsettings.py
@@ -114,10 +114,10 @@ WS4REDIS_CONNECTION = {
 ELASTICSEARCH_HOST = 'elasticsearch2'
 ELASTICSEARCH_PORT = 6200 # ES 2 port
 ELASTICSEARCH_MAJOR_VERSION = 2
-# to enable v2 ES tests
-if os.environ.get('ELASTICSEARCH_2_PORT'):
-    ELASTICSEARCH_HOST = 'elasticsearch7'
-    ELASTICSEARCH_PORT = int(os.environ.get('ELASTICSEARCH_2_PORT'))
+# to enable v7 ES tests
+if os.environ.get('ELASTICSEARCH_7_PORT'):
+    ELASTICSEARCH_HOST = 'elasticsearch'
+    ELASTICSEARCH_PORT = int(os.environ.get('ELASTICSEARCH_7_PORT'))
 
 if os.environ.get('ELASTICSEARCH_MAJOR_VERSION'):
     ELASTICSEARCH_MAJOR_VERSION = int(os.environ.get('ELASTICSEARCH_MAJOR_VERSION'))
--- docker/run.sh
+++ docker/run.sh
@@ -40,7 +40,7 @@ function setup() {
 
 function run_tests() {
     TEST="$1"
-    if [ "$TEST" != "javascript" -a "$TEST" != "python" -a "$TEST" != "python-sharded" -a "$TEST" != "python-sharded-and-javascript" -a "$TEST" != "python-elasticsearch-v2"]; then
+    if [ "$TEST" != "javascript" -a "$TEST" != "python" -a "$TEST" != "python-sharded" -a "$TEST" != "python-sharded-and-javascript" -a "$TEST" != "python-elasticsearch-v7"]; then
         echo "Unknown test suite: $TEST"
         exit 1
     fi
@@ -101,7 +101,7 @@ function _run_tests() {
         export USE_PARTITIONED_DATABASE=yes
         # TODO make it possible to run a subset of python-sharded tests
         TESTS="--attr=sql_backend"
-    elif [ "$TEST" == "python-elasticsearch-v2" ]; then
+    elif [ "$TEST" == "python-elasticsearch-v7" ]; then
         export ELASTICSEARCH_7_PORT=6200
         export ELASTICSEARCH_MAJOR_VERSION=2
         TESTS="--attr=es_test"
