122038696cc82be54109b4db3ac948c00ce2fe6f
Amit Phulera <amitphulera999@gmail.com>
2020-07-30 20:04:59 +0530

    refactor: util class

--- custom/icds/utils/custom_sms_report.py
+++ custom/icds/utils/custom_sms_report.py
@@ -15,34 +15,35 @@ class CustomSMSReportTracker(object):
     def key_expiry(self):
         return 48 * 60 * 60
 
-    def add_report_to_list(self, start_date: str, end_date: str):
+    def add_report(self, start_date: str, end_date: str):
         reports = self.active_reports
-        reports.append({
-            'start_date': start_date,
-            'end_date': end_date,
-        })
-        self.save_report_info(reports)
+        reports.append('{start_date}--{end_date}'.format(
+            start_date=start_date,
+            end_date=end_date,
+        ))
+        self.save_reports_info(reports)
 
     @property
     def active_reports(self):
         reports = []
-        unfiltered_report_list = self.client.get(self.report_key)
-        if(unfiltered_report_list):
-            reports = json.loads(unfiltered_report_list)
+        serialized_report_list = self.client.get(self.report_key)
+        if(serialized_report_list):
+            reports = json.loads(serialized_report_list)
         return reports
 
-    def save_report_info(self, reports):
+    def save_reports_info(self, reports):
         self.client.set(self.report_key, json.dumps(reports))
         self.client.expire(self.report_key, self.key_expiry)
 
-    def remove_report_from_list(self, start_date: str, end_date: str):
+    def remove_report(self, start_date: str, end_date: str):
         reports = self.active_reports
+        report_id = start_date + '--' + end_date
         updated_reports = [report for report in reports
-                    if report['start_date'] != start_date or report['end_date'] != end_date]
+                    if report != report_id]
         if len(updated_reports) == 0:
             self.clear_all_reports()
         else:
-            self.save_report_info(updated_reports)
+            self.save_reports_info(updated_reports)
 
     def clear_all_reports(self):
         self.client.delete(self.report_key)
