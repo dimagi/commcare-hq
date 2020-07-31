6c47e51ae482670405e44a5547b539fcd54a21ed
Jennifer Schweers <orange.jenny@gmail.com>
2020-07-31 13:12:30 -0400

    Updated tests to test release_domain task instead of ReleaseManager class

--- corehq/apps/linked_domain/tests/test_release_manager.py
+++ corehq/apps/linked_domain/tests/test_release_manager.py
@@ -10,7 +10,7 @@ from corehq.apps.linked_domain.const import (
     MODEL_USER_DATA,
 )
 from corehq.apps.linked_domain.models import AppLinkDetail, DomainLink
-from corehq.apps.linked_domain.tasks import ReleaseManager
+from corehq.apps.linked_domain.tasks import release_domain
 from corehq.apps.linked_domain.tests.test_linked_apps import BaseLinkedAppsTest
 from corehq.apps.users.models import WebUser
 from corehq.util.test_utils import flag_enabled
@@ -21,29 +21,14 @@ class TestReleaseManager(BaseLinkedAppsTest):
     def setUpClass(cls):
         super().setUpClass()
         cls.user = WebUser.create(cls.domain, 'fiona', 'secret', None, None)
-        cls.manager = ReleaseManager(cls.domain, cls.user.username)
         master1_module = cls.master1.add_module(Module.new_module('Module for master1', None))
         master1_module.new_form('Form for master1', 'en', get_blank_form_xml('Form for master1'))
-        cls.extra_domain = 'antarctica'
-        cls.extra_domain_link = DomainLink.link_domains(cls.extra_domain, cls.domain)
 
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-        cls.extra_domain_link.delete()
+    def _assert_domain_outcomes(self, result, domains):
+        self.assertEqual(set(result.get('text', {}).keys()), set(domains))
 
-    def _assert_domain_outcomes(self, success_domains, error_domains):
-        self.assertEqual(
-            set(self.manager.successes_by_domain.get('text', {}).keys()),
-            success_domains
-        )
-        self.assertEqual(
-            set(self.manager.errors_by_domain.get('text', {}).keys()),
-            error_domains
-        )
-
-    def _assert_error(self, domain, error):
-        for actual in self.manager.errors_by_domain.get('text', {}).get(domain, []):
+    def _assert_error(self, result, domain, error):
+        for actual in result.get('text', {}).get(domain, []):
             if error in actual:
                 self.assertTrue(True)
                 return
@@ -57,64 +42,72 @@ class TestReleaseManager(BaseLinkedAppsTest):
         }
 
     def test_success(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_USER_DATA),
-        ], [self.linked_domain])
-        self._assert_domain_outcomes({self.linked_domain}, set())
+        ])
+        self._assert_domain_outcomes(successes, {self.linked_domain})
+        self._assert_domain_outcomes(errors, set())
 
     def test_exception(self):
         with patch('corehq.apps.linked_domain.updates.update_custom_data_models', side_effect=Exception('Boom!')):
-            self.manager.release([
+            (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
                 self._model_status(MODEL_FLAGS),
                 self._model_status(MODEL_USER_DATA),
-            ], [self.linked_domain])
-        self._assert_domain_outcomes({self.linked_domain}, {self.linked_domain})
-        self._assert_error(self.linked_domain, 'Boom!')
+            ])
+        self._assert_domain_outcomes(successes, {self.linked_domain})
+        self._assert_domain_outcomes(errors, {self.linked_domain})
+        self._assert_error(errors, self.linked_domain, 'Boom!')
 
     @flag_enabled('SYNC_SEARCH_CASE_CLAIM')
     def test_case_claim_on(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_CASE_SEARCH),
         ], [self.linked_domain])
-        self._assert_domain_outcomes({self.linked_domain}, set())
+        self._assert_domain_outcomes(successes, {self.linked_domain})
+        self._assert_domain_outcomes(errors, set())
 
     def test_case_claim_off(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_CASE_SEARCH),
-        ], [self.linked_domain])
-        self._assert_domain_outcomes(set(), {self.linked_domain})
-        self._assert_error(self.linked_domain, 'Case claim flag is not on')
+        ])
+        self._assert_domain_outcomes(successes, set())
+        self._assert_domain_outcomes(errors, {self.linked_domain})
+        self._assert_error(errors, self.linked_domain, 'Case claim flag is not on')
 
     def test_bad_domain(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, 'not-a-domain', self.user.username, [
             self._model_status(MODEL_FLAGS),
-        ], [self.linked_domain, 'not-a-domain'])
-        self._assert_domain_outcomes({self.linked_domain}, {'not-a-domain'})
-        self._assert_error('not-a-domain', 'no longer linked')
+        ])
+        self._assert_domain_outcomes(successes, set())
+        self._assert_domain_outcomes(errors, {'not-a-domain'})
+        self._assert_error(errors, 'not-a-domain', 'no longer linked')
 
     def test_app_not_found(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_APP, detail=AppLinkDetail(app_id='123').to_json()),
-        ], [self.linked_domain])
-        self._assert_domain_outcomes(set(), {self.linked_domain})
-        self._assert_error(self.linked_domain, "Could not find app")
+        ])
+        self._assert_domain_outcomes(successes, set())
+        self._assert_domain_outcomes(errors, {self.linked_domain})
+        self._assert_error(errors, self.linked_domain, "Could not find app")
 
     @flag_enabled('MULTI_MASTER_LINKED_DOMAINS')
     def test_multi_master_app_fail(self):
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_APP, detail=AppLinkDetail(app_id='123').to_json()),
-        ], [self.linked_domain])
-        self._assert_domain_outcomes(set(), {self.linked_domain})
-        self._assert_error(self.linked_domain, "Multi master flag is in use")
+        ])
+        self._assert_domain_outcomes(successes, set())
+        self._assert_domain_outcomes(errors, {self.linked_domain})
+        self._assert_error(errors, self.linked_domain, "Multi master flag is in use")
 
     @patch('corehq.apps.app_manager.models.validate_xform', return_value=None)
     def test_app_build_and_release(self, *args):
         self._make_master1_build(True)
         original_version = self.linked_app.version
-        self.manager.release([
+        (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
             self._model_status(MODEL_APP, detail=AppLinkDetail(app_id=self.master1._id).to_json()),
-        ], [self.linked_domain], True)
-        self._assert_domain_outcomes({self.linked_domain}, set())
+        ], True)
+        self._assert_domain_outcomes(successes, {self.linked_domain})
+        self._assert_domain_outcomes(errors, set())
         self.linked_application = LinkedApplication.get(self.linked_app._id)
         self.assertEqual(original_version + 1, self.linked_application.version)
         self.assertTrue(self.linked_application.is_released)
@@ -123,13 +116,7 @@ class TestReleaseManager(BaseLinkedAppsTest):
         self._make_master1_build(True)
         # The missing validate_xform patch means this test will fail on travis, but make sure it also fails locally
         with patch('corehq.apps.app_manager.models.ApplicationBase.make_build', side_effect=Exception('Boom!')):
-            self.manager.release([
+            (successes, errors) = release_domain(self.domain, self.linked_domain, self.user.username, [
                 self._model_status(MODEL_APP, detail=AppLinkDetail(app_id=self.master1._id).to_json()),
-            ], [self.linked_domain], True)
-            self._assert_error(self.linked_domain, "Updated app but did not build or release: Boom!")
-
-    def test_multiple_domains(self):
-        self.manager.release([
-            self._model_status(MODEL_FLAGS),
-        ], [self.linked_domain, self.extra_domain])
-        self._assert_domain_outcomes({self.linked_domain, self.extra_domain}, set())
+            ], True)
+            self._assert_error(errors, self.linked_domain, "Updated app but did not build or release: Boom!")
