3cb515f0c87349f839f432e2e7a66cf115022c86
Sravan Reddy <sravfeyn@gmail.com>
2020-07-31 16:28:47 +0530

    refactor index/reindex/disallowed settings

--- corehq/ex-submodules/pillowtop/index_settings.py
+++ corehq/ex-submodules/pillowtop/index_settings.py
@@ -1,25 +1,59 @@
+from copy import copy
+
+from django.conf import settings
+
 from corehq.elastic import SIZE_LIMIT
 
-INDEX_REINDEX_SETTINGS = {
-    "index": {
-        "refresh_interval": "1800s",
-        # this property will be filtered out on ES 1
-        "max_result_window": SIZE_LIMIT,
-        # todo: remove these deprecated properties we are off ES 1
-        "merge.policy.merge_factor": 20,
-        "store.throttle.max_bytes_per_sec": "1mb",
-        "store.throttle.type": "merge",
-    }
+disallowed_settings_by_es_version = {
+    1: ['max_result_window'],
+    2: [
+        'merge.policy.merge_factor',
+        'store.throttle.max_bytes_per_sec',
+        'store.throttle.type'
+    ],
+    7: [
+        'merge.policy.merge_factor',
+        'store.throttle.max_bytes_per_sec',
+        'store.throttle.type'
+    ]
 }
-INDEX_STANDARD_SETTINGS = {
-    "index": {
-        "refresh_interval": "5s",
-        # this property will be filtered out on ES 1
-        "max_result_window": SIZE_LIMIT,
-        # todo: remove these deprecated properties we are off ES 1
-        "merge.policy.merge_factor": 10,
-        "store.throttle.max_bytes_per_sec": "5mb",
-        "store.throttle.type": "node",
 
-    }
-}
+def _get_es_settings(es_settings):
+    es_settings = copy(es_settings)
+    for setting in disallowed_settings_by_es_version[settings.ELASTICSEARCH_MAJOR_VERSION]:
+        es_settings['index'].pop(setting)
+    return es_settings
+
+
+def get_standard_es_settings():
+    return _get_es_settings(
+        {
+            "index": {
+                "refresh_interval": "1800s",
+                # this property will be filtered out on ES 1
+                "max_result_window": SIZE_LIMIT,
+                # todo: remove these deprecated properties we are off ES 1
+                "merge.policy.merge_factor": 20,
+                "store.throttle.max_bytes_per_sec": "1mb",
+                "store.throttle.type": "merge",
+            }
+        }
+    )
+
+
+def get_reindex_es_settings():
+    return _get_es_settings({
+        "index": {
+            "refresh_interval": "5s",
+            # this property will be filtered out on ES 1
+            "max_result_window": SIZE_LIMIT,
+            # todo: remove these deprecated properties we are off ES 1
+            "merge.policy.merge_factor": 10,
+            "store.throttle.max_bytes_per_sec": "5mb",
+            "store.throttle.type": "node",
+
+        }
+    })
+
+INDEX_STANDARD_SETTINGS = get_standard_es_settings()
+INDEX_REINDEX_SETTINGS = get_reindex_es_settings()
