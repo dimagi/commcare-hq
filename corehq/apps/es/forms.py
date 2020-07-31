c0175a1c50e6162180355b07a555f54e8b927b02
Sravan Reddy <sravfeyn@gmail.com>
2020-07-31 14:20:51 +0530

    Core ESQuery changes for 7

--- corehq/apps/es/forms.py
+++ corehq/apps/es/forms.py
@@ -11,10 +11,10 @@ from .es_query import HQESQuery
 class FormES(HQESQuery):
     index = 'forms'
     default_filters = {
-        'is_xform_instance': {"term": {"doc_type": "xforminstance"}},
-        'has_xmlns': {"not": {"missing": {"field": "xmlns"}}},
-        'has_user': {"not": {"missing": {"field": "form.meta.userID"}}},
-        'has_domain': {"not": {"missing": {"field": "domain"}}}
+        'is_xform_instance': filters.term("doc_type", "xforminstance"),
+        'has_xmlns': filters.field_exists("xmlns"),
+        'has_user': filters.field_exists("form.meta.userID"),
+        'has_domain': filters.field_exists("domain"),
     }
 
     @property
