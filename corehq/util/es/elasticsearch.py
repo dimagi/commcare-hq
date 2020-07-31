161011837f5f3daf6e639b766e14b548ba65fb0f
Sravan Reddy <sravfeyn@gmail.com>
2020-07-31 14:23:54 +0530

    ES7 lib and Docker ES7 test matrix

--- corehq/util/es/elasticsearch.py
+++ corehq/util/es/elasticsearch.py
@@ -38,8 +38,28 @@ elif settings.ELASTICSEARCH_MAJOR_VERSION == 2:
         SnapshotClient,
     )
     from elasticsearch2.helpers import bulk
+elif settings.ELASTICSEARCH_MAJOR_VERSION == 7:
+    import elasticsearch7 as elasticsearch
+    from elasticsearch7.exceptions import AuthorizationException
+    from elasticsearch7 import (
+        ConnectionError,
+        ConflictError,
+        ConnectionTimeout,
+        Elasticsearch,
+        ElasticsearchException,
+        NotFoundError,
+        SerializationError,
+        TransportError,
+        RequestError,
+    )
+    from elasticsearch7.client import (
+        IndicesClient,
+        SnapshotClient,
+    )
+    from elasticsearch7.helpers import bulk
 else:
-    raise ValueError("ELASTICSEARCH_MAJOR_VERSION must currently be 1 or 2")
+    version = settings.ELASTICSEARCH_MAJOR_VERSION
+    raise ValueError(f"ELASTICSEARCH_MAJOR_VERSION must currently be 1 or 2, given {version}")
 
 
 __all__ = [
