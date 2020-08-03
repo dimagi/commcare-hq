dba63e70cf230afc5a5ed4d91a664c99c32d2e8d
Manish Kangia <mkangia@dimagi.com>
2020-08-04 00:50:31 +0530

    remove underscore and be explicit about exposed functions

--- corehq/apps/data_vault/__init__.py
+++ corehq/apps/data_vault/__init__.py
@@ -1,4 +1,4 @@
-from corehq.apps.data_vault.utils import _get_tracked_vault_entries
+from corehq.apps.data_vault.utils import get_tracked_vault_entries
 
 
 def new_vault_entry(value):
@@ -7,10 +7,17 @@ def new_vault_entry(value):
 
 
 def has_tracked_vault_entries(on_model):
-    return bool(_get_tracked_vault_entries(on_model))
+    return bool(get_tracked_vault_entries(on_model))
 
 
 def save_tracked_vault_entries(on_model):
     from corehq.apps.data_vault.models import VaultEntry
-    values = _get_tracked_vault_entries(on_model)
+    values = get_tracked_vault_entries(on_model)
     return VaultEntry.objects.bulk_create(values)
+
+
+__all__ = [
+    'new_vault_entry',
+    'has_tracked_vault_entries',
+    'save_tracked_vault_entries'
+]
