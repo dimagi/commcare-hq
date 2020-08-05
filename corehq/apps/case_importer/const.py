5548d9102dd2b5d4bebe133de91b5f8e6b2bf79e
Tobias McNulty <tobias@caktusgroup.com>
2020-08-05 16:46:24 -0400

    Increase MAX_CASE_IMPORTER_COLUMNS 300 -> 1600

--- corehq/apps/case_importer/const.py
+++ corehq/apps/case_importer/const.py
@@ -3,9 +3,9 @@ from django.utils.translation import ugettext_noop
 # Each column results in 3 form fields so this must be true:
 #   num_columns * 3 < DATA_UPLOAD_MAX_NUMBER_FIELDS
 #
-# DATA_UPLOAD_MAX_NUMBER_FIELDS defaults to 1000 but there are
-# a few other fields as well. Also 300 is a nice round number.
-MAX_CASE_IMPORTER_COLUMNS = 300
+# DATA_UPLOAD_MAX_NUMBER_FIELDS defaults to 5000 but there are
+# a few other fields as well. Also 1600 is a nice round number.
+MAX_CASE_IMPORTER_COLUMNS = 1600
 
 
 class LookupErrors(object):
