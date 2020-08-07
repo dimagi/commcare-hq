283672232fbb9974559410b066dfe7908a22e06f
Farid Rener <proteusvacuum@gmail.com>
2020-08-07 16:21:55 -0400

    Don't duplicate shadow menus in new shadow menus

--- corehq/apps/app_manager/views/utils.py
+++ corehq/apps/app_manager/views/utils.py
@@ -460,6 +460,7 @@ def handle_shadow_child_modules(app, shadow_parent):
     source_module_children = [
         m for m in app.modules
         if m.root_module_id == shadow_parent.source_module_id
+        and m.module_type != 'shadow'
     ]
 
     shadow_parent_children = [
