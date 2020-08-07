a68737a95bbd9f980fdd2bd8dcec6555cfd06822
Norman Hooper <nhooper@dimagi.com>
2020-08-07 15:17:36 +0200

    Drop unused URL

--- corehq/motech/push/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-
-from corehq.motech.views import ConnectionSettingsListView
-
-urlpatterns = [
-    url(r'^conn/$', ConnectionSettingsListView.as_view(), name=ConnectionSettingsListView.urlname),
-]
