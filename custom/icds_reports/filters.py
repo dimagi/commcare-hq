0fc23027d08791976a718090bbf70366acb8f07d
Rohit Negi <rohit25.negi@gmail.com>
2020-08-11 19:29:00 +0530

    fixed the naming added support for multiple user location

--- custom/icds_reports/filters.py
+++ custom/icds_reports/filters.py
@@ -24,7 +24,7 @@ def location_hierarchy_config(domain, location_types=None):
 
 # copy/paste from corehq.apps.location.utils
 # added possibility to exclude test locations, test flag is custom added to the metadata in location object
-def load_locs_json(domain, selected_loc_id=None, user=None, show_test=False):
+def load_restricted_locs(domain, selected_loc_id=None, user=None, show_test=False):
 
     def loc_to_json(loc):
         return {
@@ -47,55 +47,58 @@ def load_locs_json(domain, selected_loc_id=None, user=None, show_test=False):
                 return parent_location
         return None
 
-    user_location = user.get_sql_location(domain)
+    user_locations = user.get_sql_locations(domain)
     user_has_all_location_access = user.has_permission(domain, 'access_all_locations')
 
     if user_has_all_location_access:
         accessible_root_locations = SQLLocation.root_locations(domain)
     else:
-        accessible_root_locations = (user_location
-                                     .get_ancestors(include_self=True)
-                                     .filter(parent_id__isnull=True))
+        accessible_root_locations = []
+        for user_loc in user_locations:
+            accessible_root_locations += list(user_loc
+                                              .get_ancestors(include_self=True)
+                                              .filter(parent_id__isnull=True))
+        # select only unique roots, in case when user is assigned different locations of
+        # same parent
+        accessible_root_locations = list({loc.location_id: loc for loc in accessible_root_locations}.values())
 
-    if not show_test:
-        accessible_root_locations = [loc for loc in accessible_root_locations
-                                     if loc.metadata.get('is_test_location', 'real') != 'test']
-
-    loc_json = [loc_to_json(loc) for loc in accessible_root_locations]
+    locations_list = [loc_to_json(loc) for loc in accessible_root_locations
+                      if show_test or loc.metadata.get('is_test_location', 'real') != 'test'
+                      ]
 
     # if a location is selected, we need to pre-populate its location hierarchy
     # so that the data is available client-side to pre-populate the drop-downs
+    accessible_location_ids = set()
     if selected_loc_id:
         if not user_has_all_location_access:
-            accessible_location_ids = _get_accessible_location_ids(user_location)
-        else:
-            accessible_location_ids = []
+            for user_loc in user_locations:
+                accessible_location_ids.update(_get_accessible_location_ids(user_loc))
 
         selected = SQLLocation.objects.get(
             domain=domain,
             location_id=selected_loc_id
         )
 
-        json_at_level = loc_json  # json in which we should find the ancestor in iteration
+        json_at_level = locations_list  # json in which we should find the ancestor in iteration
         for ancestor in selected.get_ancestors():  # this would start with top level ancestor first
-            ancestor_loc_dict_in_json = _get_ancestor_loc_dict(json_at_level, ancestor)
+            ancestor_loc_dict = _get_ancestor_loc_dict(json_at_level, ancestor)
 
             # could not find the ancestor at the level,
             # user should not have reached at this point to try and access an ancestor that is not permitted
-            if ancestor_loc_dict_in_json is None:
+            if ancestor_loc_dict is None:
                 break
 
             child_locations = ancestor.get_children()
 
-            ancestor_loc_dict_in_json['children'] = [
+            ancestor_loc_dict['children'] = [
                 loc_to_json(loc) for loc in child_locations
-                if loc.metadata.get('is_test_location', 'real') != 'test'
+                if (show_test or loc.metadata.get('is_test_location', 'real') != 'test')
                 and (user_has_all_location_access or loc.location_id in accessible_location_ids)
             ]
 
             # reset level to one level down to find ancestor in next iteration
-            json_at_level = ancestor_loc_dict_in_json['children']
-    return loc_json
+            json_at_level = ancestor_loc_dict['children']
+    return locations_list
 
 
 class ICDSTableauFilterMixin(object):
@@ -122,7 +125,7 @@ class IcdsLocationFilter(AsyncLocationFilter):
 
     def load_locations_json(self, loc_id):
         show_test = self.request.GET.get('include_test', False)
-        return load_locs_json(self.domain, loc_id, user=self.request.couch_user, show_test=show_test)
+        return load_restricted_locs(self.domain, loc_id, user=self.request.couch_user, show_test=show_test)
 
 
 class IcdsRestrictedLocationFilter(AsyncLocationFilter):
