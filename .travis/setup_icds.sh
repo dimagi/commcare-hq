139bcd1aed27a03696e0dba776d86771a9db364f
Simon Kelly <skelly@dimagi.com>
2020-08-11 16:29:39 +0200

    skip icds setup for fork builds

--- .travis/setup_icds.sh
+++ .travis/setup_icds.sh
@@ -5,6 +5,9 @@ RED='\033[0;31m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; COLOR_RESET='\033[0m
 echo "travis_fold:start:extension_setup"
 
 echo "${YELLOW}ICDS Setup{$COLOR_RESET}"
+if [[ -z "$encrypted_871d352bed27_key" || -z "$encrypted_871d352bed27_iv" ]]; then
+    echo "${RED}Encryption keys missing. Skipping ICDS extension setup.${COLOR_RESET}"
+    exit 0
 openssl aes-256-cbc -K $encrypted_871d352bed27_key -iv $encrypted_871d352bed27_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
 eval "$(ssh-agent -s)"
 chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key.pem
