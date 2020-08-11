0e98f2b5c961e730fc651d91ad7e1338b239aa21
Simon Kelly <skelly@dimagi.com>
2020-08-11 16:29:27 +0200

    move icds setup to script

---
+++ .travis/setup_icds.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; COLOR_RESET='\033[0m'
+
+echo "travis_fold:start:extension_setup"
+
+echo "${YELLOW}ICDS Setup{$COLOR_RESET}"
+openssl aes-256-cbc -K $encrypted_871d352bed27_key -iv $encrypted_871d352bed27_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
+eval "$(ssh-agent -s)"
+chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key.pem
+ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_key.pem
+mkdir -p $TRAVIS_BUILD_DIR/extensions/icds/
+git clone git@github.com:dimagi/commcare-icds.git $TRAVIS_BUILD_DIR/extensions/icds/ --depth=1
+cd $TRAVIS_BUILD_DIR/extensions/icds/ \
+    && git fetch origin $TRAVIS_PULL_REQUEST_BRANCH \
+    && git checkout $TRAVIS_PULL_REQUEST_BRANCH \
+    || echo "{$ORANGE}Branch $TRAVIS_PULL_REQUEST_BRANCH not found in ICDS repo. Defaulting to 'master'{$COLOR_RESET}"
+cd $TRAVIS_BUILD_DIR
+
+echo "travis_fold:stop:extension_setup"
