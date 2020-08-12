db6de6980607ab59d2e79ee27fea0591ad53be81
Simon Kelly <skelly@dimagi.com>
2020-08-12 09:31:28 +0200

    remove colors

--- .travis/setup_icds.sh
+++ .travis/setup_icds.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
-RED='\033[0;31m'; ORANGE='\033[0;33m'; YELLOW='\033[1;33m'; COLOR_RESET='\033[0m'
-
-echo "travis_fold:start:extension_setup"
-
-echo "${YELLOW}ICDS Setup{$COLOR_RESET}"
+echo "ICDS Setup"
 if [[ -z "$encrypted_871d352bed27_key" || -z "$encrypted_871d352bed27_iv" ]]; then
-    echo "${RED}Encryption keys missing. Skipping ICDS extension setup.${COLOR_RESET}"
+    echo "Encryption keys missing. Skipping ICDS extension setup."
     exit 0
+fi
 openssl aes-256-cbc -K $encrypted_871d352bed27_key -iv $encrypted_871d352bed27_iv -in .travis/deploy_key.pem.enc -out .travis/deploy_key.pem -d
 eval "$(ssh-agent -s)"
 chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key.pem
@@ -17,7 +15,5 @@ git clone git@github.com:dimagi/commcare-icds.git $TRAVIS_BUILD_DIR/extensions/i
 cd $TRAVIS_BUILD_DIR/extensions/icds/ \
     && git fetch origin $TRAVIS_PULL_REQUEST_BRANCH \
     && git checkout $TRAVIS_PULL_REQUEST_BRANCH \
-    || echo "{$ORANGE}Branch $TRAVIS_PULL_REQUEST_BRANCH not found in ICDS repo. Defaulting to 'master'{$COLOR_RESET}"
+    || echo "Branch $TRAVIS_PULL_REQUEST_BRANCH not found in ICDS repo. Defaulting to 'master'"
 cd $TRAVIS_BUILD_DIR
-
-echo "travis_fold:stop:extension_setup"
