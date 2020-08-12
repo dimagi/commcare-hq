ad49bc5c1502e12130667cba2b030e46dddc24d2
Farid Rener <proteusvacuum@gmail.com>
2020-08-12 10:09:33 -0400

    Add failing test

--- corehq/apps/linked_domain/tests/test_linked_userreports.py
+++ corehq/apps/linked_domain/tests/test_linked_userreports.py
@@ -4,6 +4,7 @@ from mock import patch
 
 from dimagi.utils.couch.undo import is_deleted, soft_delete
 
+from corehq.apps.app_manager.models import LinkedApplication
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.domain.tests.test_utils import delete_all_domains
 from corehq.apps.linked_domain.applications import link_app
@@ -129,6 +130,48 @@ class TestLinkedUCR(BaseLinkedAppsTest):
             linked_report_info.datasource.configured_filter['filters'][1]['property_value'],
             linked_app.get_id
         )
+
+    def test_linked_app_filter_maps_correctly_multiple_downstream(self):
+        first_linked_app = link_app(self.linked_app, self.domain, self.master1.get_id)
+
+        second_linked_app = LinkedApplication.new_app(self.linked_domain, "Second Linked Application")
+        second_linked_app.family_id = self.master1.get_id
+        second_linked_app.save()
+
+        self.data_source.configured_filter = {
+            "type": "and",
+            "filters": [
+                {
+                    "type": "boolean_expression",
+                    "operator": "eq",
+                    "expression": {
+                        "type": "property_name",
+                        "property_name": "xmlns",
+                        "datatype": None
+                    },
+                    "property_value": "http://openrosa.org/formdesigner/1A255365-DAA1-4815-906D-CBD8D3A462B1",
+                    "comment": None
+                },
+                {
+                    "type": "boolean_expression",
+                    "operator": "eq",
+                    "expression": {
+                        "type": "property_name",
+                        "property_name": "app_id",
+                        "datatype": None
+                    },
+                    "property_value": self.master1.get_id,
+                    "comment": None
+                }
+            ]
+        }
+        self.data_source.save()
+        linked_report_info = create_linked_ucr(self.domain_link, self.report.get_id)
+        self.assertEqual(
+            linked_report_info.datasource.configured_filter['filters'][1]['property_value'],
+            first_linked_app.get_id  # TODO: what is the correct behaviour? Get the user to choose?
+        )
+
 
     @patch('corehq.apps.linked_domain.ucr.remote_get_ucr_config')
     def test_remote_link_ucr(self, fake_ucr_getter):
