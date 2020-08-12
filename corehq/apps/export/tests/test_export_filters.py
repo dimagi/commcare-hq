e058b608c45ada9d7ddd577c74bc7162e10f38df
Daniel Miller <dmiller@dimagi.com>
2020-08-12 15:27:06 -0400

    Fix OR filter test

--- corehq/apps/export/tests/test_export_filters.py
+++ corehq/apps/export/tests/test_export_filters.py
@@ -45,10 +45,10 @@ class ExportFilterTest(SimpleTestCase):
         self.assertEqual(
             OR(OwnerFilter("foo"), OwnerFilter("bar")).to_es_filter(),
             {
-                'or': (
+                'bool': {'should': (
                     {'term': {'owner_id': 'foo'}},
                     {'term': {'owner_id': 'bar'}}
-                )
+                )}
             }
         )
 
