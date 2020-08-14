82cdc95b71b10e9ef48653ae5d8f220874bea954
Sravan Reddy <sravfeyn@gmail.com>
2020-08-14 15:33:56 +0530

    Use newer ES7 compatible bool/query  format in ESQuery
    remvoe comment

--- corehq/apps/es/filters.py
+++ corehq/apps/es/filters.py
@@ -38,31 +38,17 @@ def term(field, value):
 
 def OR(*filters):
     """Filter docs to match any of the filters passed in"""
-    return {"or": filters}
+    return {"bool": {"should": filters}}
 
 
 def AND(*filters):
     """Filter docs to match all of the filters passed in"""
-    return {"and": filters}
+    return {"bool": {"filter": filters}}
 
 
 def NOT(filter_):
     """Exclude docs matching the filter passed in"""
-    if 'or' in filter_:
-        # ES 2.4 appears not to accept {"not": {"or": [A, B]}} e.g. not (A or B)
-        # but accepts the same logic
-        # formulated as {"and": [{"not": A}, {"not": B}]} (e.g. not A and not B)
-        return AND(*(NOT(condition) for condition in filter_['or']))
-    elif 'and' in filter_:
-        # Same ES 2.4 issue as above.
-        # Rewrite "not (A and B)" as "not A or not B"
-        return OR(*(NOT(condition) for condition in filter_['and']))
-    elif 'not' in filter_:
-        # This may not be strictly necessary
-        # but prevents {'not': {'not': A}}, in favor of just A
-        return filter_['not']
-    else:
-        return {"not": filter_}
+    return {"bool": {"must_not": filter_}}
 
 
 def not_term(field, value):
@@ -106,12 +92,12 @@ def doc_id(doc_id):
 def missing(field, exist=True, null=True):
     """Only return docs missing a value for ``field``"""
     return {
-        "missing": {
-            "field": field,
-            "existence": exist,
-            "null_value": null
-        }
+        "bool": {"must_not": exists(field)}
     }
+
+
+def field_exists(field):
+    return exists(field)
 
 
 def exists(field):
@@ -135,7 +121,11 @@ def nested(path, filter_):
     return {
         "nested": {
             "path": path,
-            "filter": filter_
+            "query": {
+                "bool": {
+                    "filter": filter_
+                }
+            }
         }
     }
 
