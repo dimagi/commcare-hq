0b805d1ea81e56865319794b354761972be546ca
Cory Zue <czue@dimagi.com>
2020-08-21 14:23:07 +0200

    add requirepermission tests

--- corehq/apps/api/tests/test_auth.py
+++ corehq/apps/api/tests/test_auth.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import AnonymousUser
 from django.http import HttpResponse
 from django.test import TestCase, RequestFactory
 
-from corehq.apps.api.resources.auth import LoginAuthentication, LoginAndDomainAuthentication
+from corehq.apps.api.resources.auth import LoginAuthentication, LoginAndDomainAuthentication, \
+    RequirePermissionAuthentication
 from corehq.apps.domain.models import Domain
-from corehq.apps.users.models import WebUser, HQApiKey
+from corehq.apps.users.models import WebUser, HQApiKey, Permissions, UserRole
 
 
 class AuthenticationTestBase(TestCase):
@@ -82,3 +83,72 @@ class LoginAndDomainAuthenticationTest(AuthenticationTestBase):
         self.addCleanup(project.delete)
         self.assertAuthenticationFail(LoginAndDomainAuthentication(),
                                       self._get_request_with_api_key(domain=project.name))
+
+
+class RequirePermissionAuthenticationTest(AuthenticationTestBase):
+    require_edit_data = RequirePermissionAuthentication(Permissions.edit_data)
+
+    def test_login_no_auth_no_domain(self):
+        self.assertAuthenticationFail(self.require_edit_data, self._get_request())
+
+    def test_login_no_auth_with_domain(self):
+        self.assertAuthenticationFail(self.require_edit_data, self._get_request(domain=self.domain))
+
+    def test_login_with_wrong_domain(self):
+        project = Domain.get_or_create_with_name('api-test-fail', is_active=True)
+        self.addCleanup(project.delete)
+        self.assertAuthenticationFail(self.require_edit_data,
+                                      self._get_request_with_api_key(domain=project.name))
+
+    def test_login_with_domain_no_permissions(self):
+        self.assertAuthenticationFail(self.require_edit_data,
+                                      self._get_request_with_api_key(domain=self.domain))
+
+    def test_login_with_domain_admin(self):
+        user_with_permission = WebUser.create(self.domain, 'domain_admin', '***', None, None, is_admin=True)
+        api_key_with_permissions, _ = HQApiKey.objects.get_or_create(
+            user=WebUser.get_django_user(user_with_permission)
+        )
+        self.addCleanup(lambda: user_with_permission.delete(None))
+        self.assertAuthenticationSuccess(self.require_edit_data,
+                                         self._get_request(
+                                             domain=self.domain,
+                                             HTTP_AUTHORIZATION=self._contruct_api_auth_header(
+                                                 user_with_permission.username,
+                                                 api_key_with_permissions
+                                             )
+                                         ))
+
+    def test_login_with_explicit_permission(self):
+        role = UserRole.get_or_create_with_permissions(self.domain, Permissions(edit_data=True), 'edit-data')
+        self.addCleanup(role.delete)
+        user_with_permission = WebUser.create(self.domain, 'permission', '***', None, None, role_id=role.get_id)
+        api_key_with_permissions, _ = HQApiKey.objects.get_or_create(
+            user=WebUser.get_django_user(user_with_permission)
+        )
+        self.addCleanup(lambda: user_with_permission.delete(None))
+        self.assertAuthenticationSuccess(self.require_edit_data,
+                                         self._get_request(
+                                             domain=self.domain,
+                                             HTTP_AUTHORIZATION=self._contruct_api_auth_header(
+                                                 user_with_permission.username,
+                                                 api_key_with_permissions
+                                             )
+                                         ))
+
+    def test_login_with_wrong_permission(self):
+        role = UserRole.get_or_create_with_permissions(self.domain, Permissions(edit_data=False), 'edit-data')
+        self.addCleanup(role.delete)
+        user_with_permission = WebUser.create(self.domain, 'permission', '***', None, None, role_id=role.get_id)
+        api_key_with_permissions, _ = HQApiKey.objects.get_or_create(
+            user=WebUser.get_django_user(user_with_permission)
+        )
+        self.addCleanup(lambda: user_with_permission.delete(None))
+        self.assertAuthenticationFail(self.require_edit_data,
+                                         self._get_request(
+                                             domain=self.domain,
+                                             HTTP_AUTHORIZATION=self._contruct_api_auth_header(
+                                                 user_with_permission.username,
+                                                 api_key_with_permissions
+                                             )
+                                         ))
