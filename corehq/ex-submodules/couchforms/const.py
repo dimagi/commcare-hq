9df9b999765fae1cf13cff54f3643a35221cae32
mkangia <mkangia@dimagi.com>
2020-08-21 18:20:47 +0530

    add comment for extensions source

--- corehq/ex-submodules/couchforms/const.py
+++ corehq/ex-submodules/couchforms/const.py
@@ -22,6 +22,8 @@ RESERVED_WORDS = [TAG_TYPE, TAG_XML, TAG_VERSION, TAG_UIVERSION, TAG_NAMESPACE,
                   TAG_NAME, TAG_META, ATTACHMENT_NAME, 'case', MAGIC_PROPERTY]
 
 
+# Should be in sync with file extensions supported by CommCare
+# Copied from https://github.com/dimagi/commcare-android/blob/62ae0bde9ed623db17bcc44ad678d693e0b73cb6/app/src/org/commcare/utils/FormUploadUtil.java#L57
 SUPPORTED_MEDIA_FILE_EXTENSIONS = [
     "jpg", "jpeg", "3gpp", "3gp", "3ga", "3g2", "mp3", "wav", "amr",
     "mp4", "3gp2", "mpg4", "mpeg4", "m4v", "mpg", "mpeg", "qcp", "ogg"
