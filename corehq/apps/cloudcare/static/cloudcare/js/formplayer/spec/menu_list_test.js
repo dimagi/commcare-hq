aeaf806910aeb70b4a257a4c2021d2bb72f3ca4c
Jennifer Schweers <orange.jenny@gmail.com>
2020-08-21 22:49:24 -0400

    Migrated Menus.Views to hqDefine

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/menu_list_test.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/menu_list_test.js
@@ -13,24 +13,25 @@ describe('Render a case list', function () {
             server.restore();
         });
 
+        // TODO: Fix these
         it('Should parse a case list response to a CaseListView', function () {
             var caseListView = Menus.Util.getMenuView(fixtures.caseList);
-            assert.isTrue(caseListView instanceof Menus.Views.CaseListView);
+            assert.isTrue(caseListView instanceof hqImport("cloudcare/js/formplayer/menus/views").CaseListView);
         });
 
         it('Should parse a menu list response to a MenuListView', function () {
             var menuListView = Menus.Util.getMenuView(fixtures.menuList);
-            assert.isTrue(menuListView instanceof Menus.Views.MenuListView);
+            assert.isTrue(menuListView instanceof hqImport("cloudcare/js/formplayer/menus/views").MenuListView);
         });
 
         it('Should parse a case list response with tiles to a CaseTileListView', function () {
             var caseTileListView = Menus.Util.getMenuView(fixtures.caseTileList);
-            assert.isTrue(caseTileListView instanceof Menus.Views.CaseTileListView);
+            assert.isTrue(caseTileListView instanceof hqImport("cloudcare/js/formplayer/menus/views").CaseTileListView);
         });
 
         it('Should parse a case grid response with tiles to a GridCaseTileListView', function () {
             var caseTileGridView = Menus.Util.getMenuView(fixtures.caseGridList);
-            assert.isTrue(caseTileGridView instanceof Menus.Views.GridCaseTileListView);
+            assert.isTrue(caseTileGridView instanceof hqImport("cloudcare/js/formplayer/menus/views").CaseTileGridView);
         });
     });
 
