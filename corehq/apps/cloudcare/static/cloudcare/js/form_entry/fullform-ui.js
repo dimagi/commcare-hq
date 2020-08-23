e496aacdab0b7845ada3340f2399b0167050dd07
Jennifer Schweers <orange.jenny@gmail.com>
2020-08-23 12:27:43 -0400

    Added gettext

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/fullform-ui.js
@@ -628,12 +628,12 @@ Formplayer.Const = {
 };
 
 Formplayer.Errors = {
-    GENERIC_ERROR: "Something unexpected went wrong on that request. " +
+    GENERIC_ERROR: gettext("Something unexpected went wrong on that request. " +
         "If you have problems filling in the rest of your form please submit an issue. " +
-        "Technical Details: ",
-    TIMEOUT_ERROR: "CommCareHQ has detected a possible network connectivity problem. " +
+        "Technical Details: "),
+    TIMEOUT_ERROR: gettext("CommCareHQ has detected a possible network connectivity problem. " +
         "Please make sure you are connected to the " +
-        "Internet in order to submit your form.",
+        "Internet in order to submit your form."),
     LOCK_TIMEOUT_ERROR: gettext('Another process prevented us from servicing your request. ' +
         'Please try again later.'),
     NO_INTERNET_ERROR: gettext("We have detected an issue with your network. " +
