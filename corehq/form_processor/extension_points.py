951473c165d0dedc38f11d21f9105058c942ef4a
mkangia <mkangia@dimagi.com>
2020-08-24 20:38:29 +0530

    add doc

--- corehq/form_processor/extension_points.py
+++ corehq/form_processor/extension_points.py
@@ -3,4 +3,8 @@ from corehq.extensions import extension_point, ResultFormat
 
 @extension_point(result_format=ResultFormat.FIRST)
 def get_form_submission_context_class():
-    """docs"""
+    """Custom form submission context class
+
+    Returns:
+        A class to be instantiated, or None
+    """
