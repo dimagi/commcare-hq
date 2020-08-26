47594c54a0e807f0735278bb14c67196702507ea
Ethan Soergel <esoergel@gmail.com>
2020-08-26 14:23:36 -0400

    Log deletion from command line

--- corehq/apps/users/management/commands/delete_user.py
+++ corehq/apps/users/management/commands/delete_user.py
@@ -1,3 +1,5 @@
+import sys
+
 from django.core.management.base import BaseCommand
 
 from corehq.apps.users.models import WebUser
@@ -8,8 +10,19 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('username')
+        parser.add_argument('--deleted_by', required=True)
 
-    def handle(self, username, **options):
+    def handle(self, username, deleted_by, **options):
+        user_to_delete = _get_user(username)
+        deleted_by_user = _get_user(deleted_by)
         print("Deleting user %s" % username)
-        WebUser.get_by_username(username).delete()
+        user_to_delete.delete(deleted_by=deleted_by_user, deleted_via=__name__)
         print("Operation completed")
+
+
+def _get_user(username):
+    user = WebUser.get_by_username(username)
+    if not user:
+        print(f"User '{username}' not found")
+        sys.exit(1)
+    return user
