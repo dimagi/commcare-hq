ceccc3dcdd82fc5d15bbdb0b9bfe4ff588b71187
mkangia <mkangia@dimagi.com>
2020-08-28 14:52:47 +0530

    Revert "Log deletion from command line"
    This reverts commit 47594c54a0e807f0735278bb14c67196702507ea.

--- corehq/apps/users/management/commands/delete_user.py
+++ corehq/apps/users/management/commands/delete_user.py
@@ -1,5 +1,3 @@
-import sys
-
 from django.core.management.base import BaseCommand
 
 from corehq.apps.users.models import WebUser
@@ -10,19 +8,8 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('username')
-        parser.add_argument('--deleted_by', required=True)
 
-    def handle(self, username, deleted_by, **options):
-        user_to_delete = _get_user(username)
-        deleted_by_user = _get_user(deleted_by)
+    def handle(self, username, **options):
         print("Deleting user %s" % username)
-        user_to_delete.delete(deleted_by=deleted_by_user, deleted_via=__name__)
+        WebUser.get_by_username(username).delete()
         print("Operation completed")
-
-
-def _get_user(username):
-    user = WebUser.get_by_username(username)
-    if not user:
-        print(f"User '{username}' not found")
-        sys.exit(1)
-    return user
