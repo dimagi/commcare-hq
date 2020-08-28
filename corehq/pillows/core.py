2e9d25f2c3e061130c8808da35e0186810260ea5
Sravan Reddy <sravfeyn@gmail.com>
2020-08-28 17:57:45 +0530

    lint

--- corehq/pillows/core.py
+++ corehq/pillows/core.py
@@ -16,14 +16,14 @@ DATE_FORMATS_ARR = ["yyyy-MM-dd",
 ]
 
 if settings.ELASTICSEARCH_MAJOR_VERSION == 7:
-      DATE_FORMATS_ARR = DATE_FORMATS_ARR + [
-            # e.g. 2014-01-12T13:50:40.314+01
-            "yyyy-MM-dd'T'HH:mm:ss.SSSx",
-            # e.g. 2013-11-05T17:39:01+00:00Z
-            "yyyy-MM-dd'T'HH:mm:ssXXX'Z'",
-            # e.g. 2014-01-19T12:01:59.596+05:30
-            "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
-      ]
+    DATE_FORMATS_ARR = DATE_FORMATS_ARR + [
+        # e.g. 2014-01-12T13:50:40.314+01
+        "yyyy-MM-dd'T'HH:mm:ss.SSSx",
+        # e.g. 2013-11-05T17:39:01+00:00Z
+        "yyyy-MM-dd'T'HH:mm:ssXXX'Z'",
+        # e.g. 2014-01-19T12:01:59.596+05:30
+        "yyyy-MM-dd'T'HH:mm:ss.SSSXXX",
+    ]
 
 #https://github.com/elasticsearch/elasticsearch/issues/2132
 #elasticsearch Illegal pattern component: t
