ef2ef6784c6831708ed49f4c7cff0f2f5f38ca5b
mkangia <mkangia@dimagi.com>
2020-09-01 20:21:21 +0530

    return 422 when not permitted file extension

--- corehq/ex-submodules/couchforms/exceptions.py
+++ corehq/ex-submodules/couchforms/exceptions.py
@@ -1,4 +1,16 @@
-from couchforms.const import MAGIC_PROPERTY
+from couchforms.const import (
+    MAGIC_PROPERTY,
+    SUPPORTED_MEDIA_FILE_EXTENSIONS,
+)
+
+
+InvalidSubmissionFileExtensionErrorMessage = 'If you use multipart/form-data, please use xml file only for ' \
+                                             'submitting form xml. You may also do a normal (non-multipart) ' \
+                                             'with the xml submission as the request body instead.'
+
+InvalidAttachmentFileExtensionErrorMessage = "If you use multipart/form-data, please use the following " \
+                                             "supported file extensions for attachments:\n" \
+                                             f"{', '.join(SUPPORTED_MEDIA_FILE_EXTENSIONS)}"
 
 
 class CouchFormException(Exception):
@@ -21,8 +33,9 @@ class UnexpectedDeletedXForm(Exception):
 
 
 class BadSubmissionRequest(Exception):
-    def __init__(self, message):
+    def __init__(self, message, status_code=400):
         self.message = message
+        self.status_code = status_code
 
 
 class MultipartFilenameError(BadSubmissionRequest):
@@ -45,3 +58,19 @@ class MultipartEmptyPayload(BadSubmissionRequest):
 class EmptyPayload(BadSubmissionRequest):
     def __init__(self):
         super().__init__('Post may not have an empty body\n')
+
+
+class InvalidSubmissionFileExtensionError(BadSubmissionRequest):
+    def __init__(self):
+        super().__init__(
+            InvalidSubmissionFileExtensionErrorMessage,
+            422
+        )
+
+
+class InvalidAttachmentFileExtensionError(BadSubmissionRequest):
+    def __init__(self):
+        super().__init__(
+            InvalidAttachmentFileExtensionErrorMessage,
+            422
+        )
