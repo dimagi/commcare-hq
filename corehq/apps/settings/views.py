2263690f9a2f2a804df97c36de88300efd2a0523
Cory Zue <czue@dimagi.com>
2020-09-10 11:50:41 +0200

    add domain to the UI (and fix dangling column on new keys)

--- corehq/apps/settings/views.py
+++ corehq/apps/settings/views.py
@@ -529,6 +529,7 @@ class ApiKeyView(BaseMyAccountView, CRUDPaginatedViewMixin):
         return [
             _("Name"),
             _("API Key"),
+            _("Domain"),
             _("IP Allowlist"),
             _("Created"),
             _("Delete"),
@@ -547,6 +548,7 @@ class ApiKeyView(BaseMyAccountView, CRUDPaginatedViewMixin):
                     "id": api_key.id,
                     "name": api_key.name,
                     "key": redacted_key,
+                    "domain": api_key.domain or _('All Domains'),
                     "ip_allowlist": (
                         ", ".join(api_key.ip_allowlist)
                         if api_key.ip_allowlist else _("All IP Addresses")
@@ -577,6 +579,7 @@ class ApiKeyView(BaseMyAccountView, CRUDPaginatedViewMixin):
                 'id': new_api_key.id,
                 'name': new_api_key.name,
                 'key': f"{new_api_key.key} ({copy_key_message})",
+                "domain": new_api_key.domain or _('All Domains'),
                 'ip_allowlist': new_api_key.ip_allowlist,
                 'created': new_api_key.created.isoformat()
             },
