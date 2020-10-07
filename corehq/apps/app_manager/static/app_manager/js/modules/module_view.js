f5b4cead7ba01ddccff461ffbb607c7378b17c62
Adil <adilaryan786@gmail.com>
2020-10-07 15:42:32 +0530

    added kissmatrix track

--- corehq/apps/app_manager/static/app_manager/js/modules/module_view.js
+++ corehq/apps/app_manager/static/app_manager/js/modules/module_view.js
@@ -1,13 +1,15 @@
 hqDefine("app_manager/js/modules/module_view", function () {
     $(function () {
         $('.multiselect-caselist').select2();
+        $('#overwrite-danger').on("click", function(){
+            hqImport('analytix/js/kissmetrix').track.event("Overwrite Case Lists/Case Details");
+        });
         var initial_page_data = hqImport('hqwebapp/js/initial_page_data').get,
             moduleBrief = initial_page_data('module_brief'),
             moduleType = moduleBrief.module_type,
             options = initial_page_data('js_options') || {};
 
         hqImport('app_manager/js/app_manager').setAppendedPageTitle(django.gettext("Menu Settings"));
-        
         // Set up details
         if (moduleBrief.case_type) {
             var state = hqImport('app_manager/js/details/screen_config').state;
