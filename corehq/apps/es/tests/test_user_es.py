28754b437ba655863b20fd8dfaa5a1010fd3695b
Ethan Soergel <esoergel@gmail.com>
2020-10-28 11:49:30 -0400

    Flesh out tests a bit more

--- corehq/apps/es/tests/test_user_es.py
+++ corehq/apps/es/tests/test_user_es.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.test import TestCase
 
 from pillowtop.es_utils import initialize_index_and_mapping
@@ -26,6 +24,23 @@ class TestUserES(TestCase):
         cls.domain = 'test-user-es'
         cls.domain_obj = create_domain(cls.domain)
 
+        with sync_users_to_es():
+            cls._create_mobile_worker('stark', metadata={'sigil': 'direwolf', 'seat': 'Winterfell'})
+            cls._create_mobile_worker('lannister', metadata={'sigil': 'lion', 'seat': 'Casterly Rock'})
+            cls._create_mobile_worker('targaryen', metadata={'sigil': 'dragon', 'false_sigil': 'direwolf'})
+        cls.elasticsearch.indices.refresh(USER_INDEX)
+
+    @classmethod
+    def _create_mobile_worker(cls, username, metadata):
+        CommCareUser.create(
+            domain=cls.domain,
+            username=username,
+            password="*****",
+            created_by=None,
+            created_via=None,
+            metadata=metadata,
+        )
+
     @classmethod
     def tearDownClass(cls):
         delete_all_users()
@@ -33,20 +48,20 @@ class TestUserES(TestCase):
         ensure_index_deleted(USER_INDEX)
         super().tearDownClass()
 
-    def _create_mobile_worker(self, metadata):
-        CommCareUser.create(
-            domain=self.domain,
-            username=uuid.uuid4().hex,
-            password="*****",
-            created_by=None,
-            created_via=None,
-            metadata=metadata,
-        )
+    def test_basic_metadata_query(self):
+        direwolf_families = UserES().metadata('sigil', 'direwolf').values_list('username', flat=True)
+        self.assertEqual(direwolf_families, ['stark'])
 
-    def test_user_data_query(self):
-        with sync_users_to_es():
-            self._create_mobile_worker(metadata={'foo': 'bar'})
-            self._create_mobile_worker(metadata={'foo': 'baz'})
-            self._create_mobile_worker(metadata={'foo': 'womp', 'fu': 'bar'})
-        self.elasticsearch.indices.refresh(USER_INDEX)
-        self.assertEqual(UserES().metadata('foo', 'bar').count(), 1)
+    def test_chained_metadata_queries_where_both_match(self):
+        direwolf_families = (UserES()
+                             .metadata('sigil', 'direwolf')
+                             .metadata('seat', 'Winterfell')
+                             .values_list('username', flat=True))
+        self.assertEqual(direwolf_families, ['stark'])
+
+    def test_chained_metadata_queries_with_only_one_match(self):
+        direwolf_families = (UserES()
+                             .metadata('sigil', 'direwolf')
+                             .metadata('seat', 'Casterly Rock')
+                             .values_list('username', flat=True))
+        self.assertEqual(direwolf_families, [])
