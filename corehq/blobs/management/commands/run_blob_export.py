b3ba03aa1197f5b47404753c582143bfb836ae3a
Ethan Soergel <esoergel@gmail.com>
2020-10-30 14:08:43 -0400

    Restructure blob exporter to accept list of blobs to skip

--- corehq/blobs/management/commands/run_blob_export.py
+++ corehq/blobs/management/commands/run_blob_export.py
@@ -25,6 +25,11 @@ class Command(BaseCommand):
         ./manage.py run_blob_export -e sql_xforms --limit-to-db p0 domain
          ...
         ./manage.py run_blob_export -e sql_xforms --limit-to-db pN domain
+
+    To top-up an older blob dump, first extract a list of names from the archive:
+        $ tar --list -f blob_export.tar.gz > blob_export.list
+    Then provide this file to the `--already_exported` argument to skip over
+    those objects in this dump.
     """
     help = USAGE
 
@@ -40,16 +45,15 @@ class Command(BaseCommand):
         parser.add_argument('--limit-to-db', dest='limit_to_db',
                             help="When specifying a SQL importer use this to restrict "
                                  "the exporter to a single database.")
-        parser.add_argument('--extend', dest='extends', action='append', default=[],
-                            help='Extend a previous export file. '
-                                 'You can extend multiple files.')
+        parser.add_argument('--already_exported', dest='already_exported',
+                            help='Pass a file with a list of blob names already exported')
 
     @change_log_level('boto3', logging.WARNING)
     @change_log_level('botocore', logging.WARNING)
     def handle(self, domain=None, reset=False,
                chunk_size=100, all=None, limit_to_db=None, **options):
         exporters = options.get('exporters')
-        extends = options['extends']
+        already_exported = _get_names_from_file(options['already_exported'])
 
         if not domain:
             raise CommandError(USAGE)
@@ -64,7 +68,7 @@ class Command(BaseCommand):
                 raise CommandError(USAGE)
 
             self.stdout.write("\nRunning exporter: {}\n{}".format(exporter_slug, '-' * 50))
-            export_filename = _get_export_filename(exporter_slug, domain, extends)
+            export_filename = _get_export_filename(exporter_slug, domain, already_exported)
             if os.path.exists(export_filename):
                 raise CommandError(f"Export file '{export_filename}' exists. "
                                    f"Remove the file and re-run the command.")
@@ -74,13 +78,22 @@ class Command(BaseCommand):
                 export_filename,
                 chunk_size=chunk_size,
                 limit_to_db=limit_to_db,
-                extends=extends,
+                already_exported=already_exported,
             )
             if skips:
                 sys.exit(skips)
 
 
-def _get_export_filename(slug, domain, extends):
+def _get_names_from_file(filename):
+    if not filename:
+        return set()
+    with open(filename) as f:
+        names = {line.strip() for line in f}
+    print("Found {} existing blobs, these will be skipped".format(len(names)))
+    return names
+
+
+def _get_export_filename(slug, domain, already_exported):
     timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H.%M')
-    _extends = '-extended' if extends else ''
-    return f'{timestamp}-{domain}-{slug}{_extends}.tar.gz'
+    part = '-part' if already_exported else ''
+    return f'{timestamp}-{domain}-{slug}{part}.tar.gz'
