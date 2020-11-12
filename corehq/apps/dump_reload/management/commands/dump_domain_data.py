8e2a226dd9f92020d5aa3a2635116b2c95c83d7a
Ethan Soergel <esoergel@gmail.com>
2020-11-12 17:43:03 -0500

    Add meta.json to dump archive

--- corehq/apps/dump_reload/management/commands/dump_domain_data.py
+++ corehq/apps/dump_reload/management/commands/dump_domain_data.py
@@ -1,7 +1,7 @@
 import gzip
+import json
 import os
 import zipfile
-from collections import Counter
 from datetime import datetime
 
 from django.core.management.base import BaseCommand, CommandError
@@ -40,7 +40,7 @@ class Command(BaseCommand):
         zipname = 'data-dump-{}-{}.zip'.format(domain_name, self.utcnow)
 
         self.stdout.ending = None
-        stats = Counter()
+        meta = {}  # {dumper_slug: {model_name: count}}
         # domain dumper should be first since it validates domain exists
         for dumper in [DomainDumper, SqlDataDumper, CouchDataDumper, ToggleDumper]:
             if requested_dumpers and dumper.slug not in requested_dumpers:
@@ -49,7 +49,7 @@ class Command(BaseCommand):
             filename = _get_dump_stream_filename(dumper.slug, domain_name, self.utcnow)
             stream = self.stdout if console else gzip.open(filename, 'wt')
             try:
-                stats += dumper(domain_name, excludes).dump(stream)
+                meta[dumper.slug] = dumper(domain_name, excludes).dump(stream)
             except Exception as e:
                 if show_traceback:
                     raise
@@ -64,16 +64,24 @@ class Command(BaseCommand):
 
                 os.remove(filename)
 
-        self._print_stats(stats)
+        if not console:
+            with zipfile.ZipFile(zipname, mode='a', allowZip64=True) as z:
+                z.writestr('meta.json', json.dumps(meta))
+
+        self._print_stats(meta)
         self.stdout.write('\nData dumped to file: {}'.format(zipname))
 
-    def _print_stats(self, stats):
+    def _print_stats(self, meta):
         self.stdout.ending = '\n'
         self.stdout.write('{0} Dump Stats {0}'.format('-' * 32))
-        for model in sorted(stats):
-            self.stdout.write("{:<50}: {}".format(model, stats[model]))
+        for dumper, models in sorted(meta.items()):
+            self.stdout.write(dumper)
+            for model, count in sorted(models.items()):
+                self.stdout.write("  {:<50}: {}".format(model, count))
         self.stdout.write('{0}{0}'.format('-' * 38))
-        self.stdout.write('Dumped {} objects'.format(sum(stats.values())))
+        self.stdout.write('Dumped {} objects'.format(sum(
+            count for model in meta.values() for count in model.values()
+        )))
         self.stdout.write('{0}{0}'.format('-' * 38))
 
 
