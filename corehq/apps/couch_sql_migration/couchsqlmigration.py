cd1b6dd4d342756421e1515d370aca9beabbee02
Daniel Miller <dmiller@dimagi.com>
2020-12-02 16:48:50 -0500

    Fix more bad case modified date formats

--- corehq/apps/couch_sql_migration/couchsqlmigration.py
+++ corehq/apps/couch_sql_migration/couchsqlmigration.py
@@ -805,8 +805,17 @@ def patch_case_date_modified_fixer():
             if isinstance(datemod, str) and MALFORMED_DATE.match(datemod):
                 # fix modified date so subsequent validation (immediately
                 # after this function call) does not fail
-                assert datemod[8] == "0", datemod
-                case_block["@date_modified"] = datemod[:8] + datemod[9:]
+                if len(datemod) == 10:
+                    # format: MM-DD-YYYY -> YYYY-MM-DD
+                    case_block["@date_modified"] = datemod[6:] + "-" + datemod[:5]
+                elif len(datemod) == 11:
+                    # format: YYYY-MM-0DD -> YYYY-MM-DD
+                    assert datemod[8] == "0", datemod
+                    case_block["@date_modified"] = datemod[:8] + datemod[9:]
+                elif len(datemod) == 17:
+                    # MM/DD/YY HH:MM:SS -> YYYY-MM-DD HH:MM:SS
+                    old = datemod.replace("/", "-")
+                    case_block["@date_modified"] = f"20{old[6:8]}-{old[:5]} {old[9:]}"
         return has_case
     import casexml.apps.case.xform as module
     from casexml.apps.case.xform import has_case_id
