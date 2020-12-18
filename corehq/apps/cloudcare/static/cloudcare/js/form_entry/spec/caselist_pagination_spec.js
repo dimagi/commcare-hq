22f02b3f87b0282c3e84d1925eaa3ded4f88de0c
Adil <adilaryan786@gmail.com>
2020-12-18 13:30:10 +0530

    added pagination test case

---
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/caselist_pagination_spec.js
@@ -0,0 +1,23 @@
+describe('#paginateOptions', function () {
+    var paginateItems = hqImport("cloudcare/js/formplayer/menus/views");
+    it('Should determine if two answers are equal', function () {
+        var result = paginateItems.paginateOptions(0, 15)
+        /**
+         *   result: {stratPage:'', endPage:'', pageCount:''}
+         *   endPage: max number of pages to display at a time
+         *   pageCount: total number of pages
+         */
+
+        //Asserting equal
+        assert.equal(result.startPage, 1);
+        assert.equal(result.endPage, 5);
+        assert.equal(result.pageCount, 15)
+
+        //Assdrting not equal
+        assert.notEqual(result.startPage, 5)
+        assert.notEqual(result.endPage, 10);
+        assert.notEqual(result.pageCount, 20)
+
+
+    });
+ });
