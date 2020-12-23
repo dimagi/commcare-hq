e9b3a3be0f9561e8d6e047de302a0d4eae2ab3b0
Matt Riley <mriley@dimagi.com>
2020-12-23 16:54:30 -0500

    refactored case schema assembly

--- corehq/apps/app_manager/app_schemas/casedb_schema.py
+++ corehq/apps/app_manager/app_schemas/casedb_schema.py
@@ -42,9 +42,7 @@ def get_casedb_schema(form):
     subsets = [{
         "id": generation_names[i],
         "name": "{} ({})".format(generation_names[i], " or ".join(ctypes)) if i > 0 else base_case_type,
-        "structure": {
-            p: {"description": descriptions_dict.get(t, {}).get(p, '')}
-            for t in ctypes for p in map[t]},
+        "structure": generate_structure(ctypes, map, descriptions_dict),
         "related": {"parent": {
             "hashtag": "#case/" + generation_names[i + 1],
             "subset": generation_names[i + 1],
@@ -68,3 +66,26 @@ def get_casedb_schema(form):
         "structure": {},
         "subsets": subsets,
     }
+
+
+def generate_structure(case_types, case_to_property_mapping, descriptions):
+    structure = {}
+
+    for case_type in case_types:
+        case_properties = case_to_property_mapping[case_type]
+        case_descriptions = descriptions.get(case_type, {})
+        properties_with_metadata = {
+            prop: generate_property_metadata(prop, case_descriptions)
+            for prop in case_properties
+        }
+        structure.update(properties_with_metadata)
+
+    return structure
+
+
+def generate_property_metadata(prop, descriptions):
+    metadata = {
+        "description": descriptions.get(prop, '')
+    }
+
+    return metadata
