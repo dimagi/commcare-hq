3947adc0668857f51bf528778fb393e7931e9bcd
Biyeun Buczyk <biyeun@dimagi.com>
2020-12-29 15:41:26 -0500

    add x509 certificate handling (with tests)

--- corehq/apps/sso/models.py
+++ corehq/apps/sso/models.py
@@ -1,7 +1,13 @@
+from OpenSSL import crypto
+import uuid
+from dateutil.parser import parse
+
+from django.conf import settings
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 from corehq.apps.accounting.models import BillingAccount
+from corehq.apps.sso.exceptions import ServiceProviderCertificateError
 
 
 class IdentityProviderType(object):
@@ -9,6 +15,41 @@ class IdentityProviderType(object):
     CHOICES = (
         (AZURE_AD, "Azure AD"),
     )
+
+
+class ServiceProviderCertificate(object):
+    DEFAULT_EXPIRATION = 365 * 24 * 60 * 60  # one year in seconds
+
+    def __init__(self, identity_provider):
+        """
+        To increase the security with SAML transactions, we will provide the IdP
+        with our public key for an x509 certificate unique to our interactions with
+        a particular IdP. This certificate will be regenerated automatically by
+        a periodic task every 60 days.
+        """
+        # create a key pair
+        k = crypto.PKey()
+        k.generate_key(crypto.TYPE_RSA, 4096)
+
+        # create a self-signed cert
+        cert = crypto.X509()
+        cert.get_subject().C = "US"
+        cert.get_subject().ST = "MA"
+        cert.get_subject().L = "Cambridge"
+        cert.get_subject().O = "Dimagi Inc."
+        cert.get_subject().OU = "CommCareHQ"
+        cert.get_subject().CN = "CommCare"
+        cert.get_subject().emailAddress = settings.ACCOUNTS_EMAIL
+        cert.set_serial_number(uuid.uuid4().int)
+        cert.gmtime_adj_notBefore(0)
+        cert.gmtime_adj_notAfter(self.DEFAULT_EXPIRATION)
+        cert.set_issuer(cert.get_subject())
+        cert.set_pubkey(k)
+        cert.sign(k, "sha256")
+
+        self.public_key = crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8")
+        self.private_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, k).decode("utf-8")
+        self.date_expires = parse(cert.get_notAfter())
 
 
 class IdentityProvider(models.Model):
@@ -70,6 +111,35 @@ class IdentityProvider(models.Model):
 
     def __str__(self):
         return f"{self.name} IdP [{self.idp_type}]"
+
+    def create_service_provider_certificate(self):
+        sp_cert = ServiceProviderCertificate(self)
+        self.sp_cert_public = sp_cert.public_key
+        self.sp_cert_private = sp_cert.private_key
+        self.date_sp_cert_expiration = sp_cert.date_expires
+        self.save()
+
+    def create_rollover_service_provider_certificate(self):
+        sp_cert = ServiceProviderCertificate(self)
+        self.sp_rollover_cert_public = sp_cert.public_key
+        self.sp_rollover_cert_private = sp_cert.private_key
+        self.date_sp_rollover_cert_expiration = sp_cert.date_expires
+        self.save()
+
+    def renew_service_provider_certificate(self):
+        if not self.sp_rollover_cert_public:
+            raise ServiceProviderCertificateError(
+                "A rollover certificate for the Service Provider was never "
+                "generated. You should first create a rollover certificate and "
+                "leave it active for a few days to give the IdP a heads up."
+            )
+        self.sp_cert_public = self.sp_rollover_cert_public
+        self.sp_cert_private = self.sp_rollover_cert_private
+        self.date_sp_cert_expiration = self.date_sp_rollover_cert_expiration
+        self.sp_rollover_cert_public = None
+        self.sp_rollover_cert_private = None
+        self.date_sp_rollover_cert_expiration = None
+        self.save()
 
 
 class AuthenticatedEmailDomain(models.Model):
