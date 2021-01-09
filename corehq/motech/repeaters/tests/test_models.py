9eabf2619799617e37339bcd9668dc0c6088a78b
Norman Hooper <nhooper@dimagi.com>
2021-01-09 16:09:37 +0200

    ⚖ Model tests for process_repeater_stub()

--- corehq/motech/repeaters/tests/test_models.py
+++ corehq/motech/repeaters/tests/test_models.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from uuid import uuid4
 
 from django.conf import settings
-from django.test import TestCase
+from django.test import SimpleTestCase, TestCase
 from django.utils import timezone
 
 from nose.tools import assert_in
@@ -13,13 +13,22 @@ from corehq.motech.models import ConnectionSettings
 from corehq.motech.utils import b64_aes_encrypt
 
 from ..const import (
+    MAX_ATTEMPTS,
+    MIN_RETRY_WAIT,
     RECORD_CANCELLED_STATE,
     RECORD_FAILURE_STATE,
     RECORD_PENDING_STATE,
     RECORD_SUCCESS_STATE,
 )
-from ..models import FormRepeater, RepeaterStub, get_all_repeater_types
-from ..repeater_generators import FormRepeaterXMLPayloadGenerator
+from ..models import (
+    FormRepeater,
+    RepeaterStub,
+    format_response,
+    get_all_repeater_types,
+    is_response,
+)
+
+DOMAIN = 'test-domain'
 
 
 def test_get_all_repeater_types():
@@ -29,82 +38,81 @@ def test_get_all_repeater_types():
         assert_in(name, types)
 
 
-class RepeaterConnectionSettingsTests(TestCase):
-
-    def setUp(self):
-        self.rep = FormRepeater(
-            domain="greasy-spoon",
-            url="https://spam.example.com/api/",
-            format=FormRepeaterXMLPayloadGenerator.format_name,
-        )
-
-    def tearDown(self):
-        if self.rep.connection_settings_id:
-            ConnectionSettings.objects.filter(
-                pk=self.rep.connection_settings_id
-            ).delete()
-        self.rep.delete()
-
-    def test_create_connection_settings(self):
-        self.assertIsNone(self.rep.connection_settings_id)
-        conn = self.rep.connection_settings
-
-        self.assertIsNotNone(self.rep.connection_settings_id)
-        self.assertEqual(conn.name, self.rep.url)
-
-    def test_notify_addresses(self):
-        self.rep.notify_addresses_str = "admin@example.com"
-        conn = self.rep.connection_settings
-        self.assertEqual(conn.notify_addresses, ["admin@example.com"])
-
-    def test_notify_addresses_none(self):
-        self.rep.notify_addresses_str = None
-        conn = self.rep.connection_settings
-        self.assertEqual(conn.notify_addresses, [])
-
-    def test_password_encrypted(self):
-        self.rep.auth_type = BASIC_AUTH
-        self.rep.username = "terry"
-        self.rep.password = "Don't save me decrypted!"
-        conn = self.rep.connection_settings
-
-        self.assertEqual(self.rep.plaintext_password, conn.plaintext_password)
-        # rep.password was saved decrypted; conn.password is not:
-        self.assertNotEqual(self.rep.password, conn.password)
-
-    def test_password_bug(self):
-        self.rep.auth_type = BASIC_AUTH
-        self.rep.username = "terry"
-        plaintext = "Don't save me decrypted!"
-        ciphertext = b64_aes_encrypt(plaintext)
-        bytestring_repr = f"b'{ciphertext}'"  # bug fixed by commit 3a900068
-        self.rep.password = f'${ALGO_AES}${bytestring_repr}'
-        conn = self.rep.connection_settings
-
-        self.assertEqual(conn.plaintext_password, self.rep.plaintext_password)
-
-
-class TestSQLRepeatRecordOrdering(TestCase):
+class RepeaterFixtureMixin:
 
     def setUp(self):
         self.repeater = FormRepeater(
-            domain='eden',
-            url='https://spam.example.com/api/',
+            domain=DOMAIN,
+            url='https://www.example.com/api/',
         )
         self.repeater.save()
         self.repeater_stub = RepeaterStub.objects.create(
-            domain='eden',
+            domain=DOMAIN,
             repeater_id=self.repeater.get_id,
-        )
-        self.repeater_stub.repeat_records.create(
-            domain=self.repeater_stub.domain,
-            payload_id='eve',
-            registered_at='1970-02-01',
         )
 
     def tearDown(self):
         self.repeater_stub.delete()
         self.repeater.delete()
+
+
+class RepeaterConnectionSettingsTests(RepeaterFixtureMixin, TestCase):
+
+    def tearDown(self):
+        if self.repeater.connection_settings_id:
+            ConnectionSettings.objects.filter(
+                pk=self.repeater.connection_settings_id
+            ).delete()
+        super().tearDown()
+
+    def test_create_connection_settings(self):
+        self.assertIsNone(self.repeater.connection_settings_id)
+        conn = self.repeater.connection_settings
+
+        self.assertIsNotNone(self.repeater.connection_settings_id)
+        self.assertEqual(conn.name, self.repeater.url)
+
+    def test_notify_addresses(self):
+        self.repeater.notify_addresses_str = "admin@example.com"
+        conn = self.repeater.connection_settings
+        self.assertEqual(conn.notify_addresses, ["admin@example.com"])
+
+    def test_notify_addresses_none(self):
+        self.repeater.notify_addresses_str = None
+        conn = self.repeater.connection_settings
+        self.assertEqual(conn.notify_addresses, [])
+
+    def test_password_encrypted(self):
+        self.repeater.auth_type = BASIC_AUTH
+        self.repeater.username = "terry"
+        self.repeater.password = "Don't save me decrypted!"
+        conn = self.repeater.connection_settings
+
+        self.assertEqual(self.repeater.plaintext_password, conn.plaintext_password)
+        # repeater.password was saved decrypted; conn.password is not:
+        self.assertNotEqual(self.repeater.password, conn.password)
+
+    def test_password_bug(self):
+        self.repeater.auth_type = BASIC_AUTH
+        self.repeater.username = "terry"
+        plaintext = "Don't save me decrypted!"
+        ciphertext = b64_aes_encrypt(plaintext)
+        bytestring_repr = f"b'{ciphertext}'"  # bug fixed by commit 3a900068
+        self.repeater.password = f'${ALGO_AES}${bytestring_repr}'
+        conn = self.repeater.connection_settings
+
+        self.assertEqual(conn.plaintext_password, self.repeater.plaintext_password)
+
+
+class TestSQLRepeatRecordOrdering(RepeaterFixtureMixin, TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.repeater_stub.repeat_records.create(
+            domain=DOMAIN,
+            payload_id='eve',
+            registered_at='1970-02-01',
+        )
 
     def test_earlier_record_created_later(self):
         self.repeater_stub.repeat_records.create(
@@ -131,22 +139,7 @@ class TestSQLRepeatRecordOrdering(TestCase):
         self.assertEqual(repeat_records[1].payload_id, 'cain')
 
 
-class RepeaterStubManagerTests(TestCase):
-
-    def setUp(self):
-        self.repeater = FormRepeater(
-            domain="greasy-spoon",
-            url="https://spam.example.com/api/",
-        )
-        self.repeater.save()
-        self.repeater_stub = RepeaterStub.objects.create(
-            domain="greasy-spoon",
-            repeater_id=self.repeater.get_id,
-        )
-
-    def tearDown(self):
-        self.repeater_stub.delete()
-        self.repeater.delete()
+class RepeaterStubManagerTests(RepeaterFixtureMixin, TestCase):
 
     def test_all_ready_no_repeat_records(self):
         repeater_stubs = RepeaterStub.objects.all_ready()
@@ -230,3 +223,137 @@ def set_next_attempt_at(repeater_stub, when):
     finally:
         repeater_stub.next_attempt_at = None
         repeater_stub.save()
+
+
+class ResponseMock:
+    pass
+
+
+class IsResponseTests(SimpleTestCase):
+
+    def test_has_text(self):
+        resp = ResponseMock()
+        resp.text = '<h1>Hello World</h1>'
+        self.assertFalse(is_response(resp))
+
+    def test_has_status_code(self):
+        resp = ResponseMock()
+        resp.status_code = 504
+        self.assertFalse(is_response(resp))
+
+    def test_has_reason(self):
+        resp = ResponseMock()
+        resp.reason = 'Gateway Timeout'
+        self.assertFalse(is_response(resp))
+
+    def test_has_status_code_and_reason(self):
+        resp = ResponseMock()
+        resp.status_code = 504
+        resp.reason = 'Gateway Timeout'
+        self.assertTrue(is_response(resp))
+
+
+class FormatResponseTests(SimpleTestCase):
+
+    def test_non_response(self):
+        resp = ResponseMock()
+        self.assertIsNone(format_response(resp))
+
+    def test_no_text(self):
+        resp = ResponseMock()
+        resp.status_code = 504
+        resp.reason = 'Gateway Timeout'
+        self.assertEqual(format_response(resp), '504: Gateway Timeout')
+
+    def test_with_text(self):
+        resp = ResponseMock()
+        resp.status_code = 200
+        resp.reason = 'OK'
+        resp.text = '<h1>Hello World</h1>'
+        self.assertEqual(format_response(resp), '200: OK\n'
+                                                '<h1>Hello World</h1>')
+
+
+class AddAttemptsTests(RepeaterFixtureMixin, TestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.just_now = timezone.now()
+        self.repeater_stub.next_attempt_at = self.just_now
+        self.repeater_stub.save()
+        self.repeat_record = self.repeater_stub.repeat_records.create(
+            domain=DOMAIN,
+            payload_id='eggs',
+            registered_at=timezone.now(),
+        )
+
+    def tearDown(self):
+        self.repeater_stub.delete()
+        self.repeater.delete()
+
+    def test_add_success_attempt_true(self):
+        self.repeat_record.add_success_attempt(response=True)
+        self.assertEqual(self.repeat_record.state, RECORD_SUCCESS_STATE)
+        self.assertIsNone(self.repeater_stub.next_attempt_at)
+        self.assertEqual(self.repeat_record.num_attempts, 1)
+        self.assertEqual(self.repeat_record.attempts[0].state,
+                         RECORD_SUCCESS_STATE)
+        self.assertIsNone(self.repeat_record.attempts[0].message)
+
+    def test_add_success_attempt_200(self):
+        resp = ResponseMock()
+        resp.status_code = 200
+        resp.reason = 'OK'
+        resp.text = '<h1>Hello World</h1>'
+        self.repeat_record.add_success_attempt(response=resp)
+        self.assertEqual(self.repeat_record.state, RECORD_SUCCESS_STATE)
+        self.assertIsNone(self.repeater_stub.next_attempt_at)
+        self.assertEqual(self.repeat_record.num_attempts, 1)
+        self.assertEqual(self.repeat_record.attempts[0].state,
+                         RECORD_SUCCESS_STATE)
+        self.assertEqual(self.repeat_record.attempts[0].message,
+                         format_response(resp))
+
+    def test_add_failure_attempt_fail(self):
+        message = '409: Conflict\n'
+        self.repeat_record.add_failure_attempt(message=message)
+        self.assertEqual(self.repeat_record.state, RECORD_FAILURE_STATE)
+        self.assertGreater(self.repeater_stub.last_attempt_at, self.just_now)
+        self.assertEqual(self.repeater_stub.next_attempt_at,
+                         self.repeater_stub.last_attempt_at + MIN_RETRY_WAIT)
+        self.assertEqual(self.repeat_record.num_attempts, 1)
+        self.assertEqual(self.repeat_record.attempts[0].state,
+                         RECORD_FAILURE_STATE)
+        self.assertEqual(self.repeat_record.attempts[0].message, message)
+        self.assertIsNone(self.repeat_record.attempts[0].traceback)
+
+    def test_add_failure_attempt_cancel(self):
+        message = '409: Conflict\n'
+        for __ in range(MAX_ATTEMPTS + 1):
+            self.repeat_record.add_failure_attempt(message=message)
+        self.assertEqual(self.repeat_record.state, RECORD_CANCELLED_STATE)
+        self.assertGreater(self.repeater_stub.last_attempt_at, self.just_now)
+        self.assertEqual(self.repeater_stub.next_attempt_at,
+                         self.repeater_stub.last_attempt_at + MIN_RETRY_WAIT)
+        self.assertEqual(self.repeat_record.num_attempts, MAX_ATTEMPTS + 1)
+        attempts = list(self.repeat_record.attempts)
+        expected_states = ([RECORD_FAILURE_STATE] * MAX_ATTEMPTS
+                           + [RECORD_CANCELLED_STATE])
+        self.assertEqual([a.state for a in attempts], expected_states)
+        self.assertEqual(attempts[-1].message, message)
+        self.assertIsNone(attempts[-1].traceback)
+
+    def test_add_payload_exception_attempt(self):
+        message = 'ValueError: Schema validation failed'
+        tb_str = 'Traceback ...'
+        self.repeat_record.add_payload_exception_attempt(message=message,
+                                                         tb_str=tb_str)
+        self.assertEqual(self.repeat_record.state, RECORD_CANCELLED_STATE)
+        # Note: Our payload issues do not affect how we deal with their
+        #       server issues:
+        self.assertEqual(self.repeater_stub.next_attempt_at, self.just_now)
+        self.assertEqual(self.repeat_record.num_attempts, 1)
+        self.assertEqual(self.repeat_record.attempts[0].state,
+                         RECORD_CANCELLED_STATE)
+        self.assertEqual(self.repeat_record.attempts[0].message, message)
+        self.assertEqual(self.repeat_record.attempts[0].traceback, tb_str)
