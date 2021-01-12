ab93b5f1db03756d234e4b1d6c3ed3978d765a19
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-01-12 15:40:37 -0500

    debug data

--- corehq/apps/sso/decorators.py
+++ corehq/apps/sso/decorators.py
@@ -28,6 +28,8 @@ def use_saml2_auth(view_func):
                 'get_data': request.GET.copy(),
                 'post_data': request.POST.copy(),
             }
+            from corehq.apps.sso.views import sso_soft_assert
+            sso_soft_assert(False, request_data)
             request.saml2_auth = OneLogin_Saml2_Auth(request_data, get_saml2_config(request.idp))
             request.saml2_errors = None
         except Exception as e:
