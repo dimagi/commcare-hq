dde7ff6e3efa40b5311a92957b9b33b2c42f0ff1
Simon Kelly <skelly@dimagi.com>
2021-01-12 14:52:43 +0200

    use _post_data and check response

--- corehq/apps/formplayer_api/sync_db.py
+++ corehq/apps/formplayer_api/sync_db.py
@@ -1,4 +1,4 @@
-from corehq.apps.formplayer_api.smsforms.api import _get_response
+from corehq.apps.formplayer_api.smsforms.api import _post_data
 from corehq.apps.users.models import CouchUser
 from corehq.toggles import FORMPLAYER_USE_LIVEQUERY
 
@@ -9,10 +9,12 @@ def sync_db(domain, username, restore_as):
     user_id = user.user_id
     use_livequery = FORMPLAYER_USE_LIVEQUERY.enabled(domain)
     data = {
-        'action': 'change_locale',
+        'action': 'sync-db',
         'username': username,
         'domain': domain,
         'restoreAs': restore_as,
         'useLiveQuery': use_livequery,
     }
-    return _get_response(data, user_id=user_id)
+    response_json = _post_data(data, user_id)
+    if not response_json.get("status") == "accepted":
+        raise Exception(response_json)
