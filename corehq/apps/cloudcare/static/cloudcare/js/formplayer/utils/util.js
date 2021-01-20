fbee4138df4b0b92fc0b8bc9645f01948a6fd2bd
Jennifer Schweers <orange.jenny@gmail.com>
2021-01-20 13:16:56 -0500

    Updated itemset code to use setQueryData instead of setQuery

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/util.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/util.js
@@ -169,13 +169,13 @@ hqDefine("cloudcare/js/formplayer/utils/util", function () {
             this.sortIndex = null;
         };
 
-        this.setQueryData = function (queryDict) {
+        this.setQueryData = function (queryDict, execute) {
             if (!this.queryData) {
                 this.queryData = {};
             }
             this.queryData[sessionStorage.queryKey] = {
                 inputs: queryDict,
-                execute: true,
+                execute: execute,
             };
         };
 
