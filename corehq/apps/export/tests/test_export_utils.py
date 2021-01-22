4117389dcd658049dd8cf35b13b6f209964deda1
Graham Herceg <g.a.herceg@gmail.com>
2021-01-22 17:24:03 -0500

    finally fixed tests

--- corehq/apps/export/tests/test_export_utils.py
+++ corehq/apps/export/tests/test_export_utils.py
@@ -2,40 +2,42 @@ from datetime import date, timedelta
 
 from django.test import TestCase
 
-from corehq.apps.accounting.models import SoftwarePlanEdition, Subscription, DefaultProductPlan, BillingAccount
+from corehq.apps.accounting.models import SoftwarePlanEdition, Subscription, DefaultProductPlan, BillingAccount, \
+    SubscriptionAdjustment
 from corehq.apps.domain.models import Domain
 from corehq.apps.export.utils import get_or_create_default_export_settings_for_domain
-from corehq.util.test_utils import flag_enabled
+from corehq.util.test_utils import flag_enabled, generate_cases
 
 
 class TestExportUtils(TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super(TestExportUtils, cls).setUpClass()
-        cls.domain = Domain.get_or_create_with_name('test-export-utils', is_active=True)
-        cls.account, _ = BillingAccount.get_or_create_account_by_domain(
-            cls.domain.name,
+    def setUp(self):
+        super(TestExportUtils, self).setUp()
+        self.domain = Domain.get_or_create_with_name('test-export-utils', is_active=True)
+        self.account, _ = BillingAccount.get_or_create_account_by_domain(
+            self.domain.name,
             created_by='webuser@test.com'
         )
-        cls.account.save()
-        cls.subscription = Subscription.new_domain_subscription(
-            cls.account, cls.domain.name,
+        self.account.save()
+        subscription = Subscription.new_domain_subscription(
+            self.account, self.domain.name,
             DefaultProductPlan.get_default_plan_version(edition=SoftwarePlanEdition.ENTERPRISE),
             date_start=date.today() - timedelta(days=3)
         )
-        cls.subscription.is_active = True
-        cls.subscription.save()
+        subscription.is_active = True
+        subscription.save()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.domain.delete()
-        super(TestExportUtils, cls).tearDownClass()
+    def tearDown(self):
+        self.domain.delete()
+        SubscriptionAdjustment.objects.all().delete()
+        Subscription.visible_and_suppressed_objects.all().delete()
+        self.account.delete()
+        super(TestExportUtils, self).tearDown()
 
     def update_subscription(self, plan):
-        if self.subscription.plan_version.plan.edition != plan:
-            self.subscription.change_plan(DefaultProductPlan.get_default_plan_version(plan))
-            self.subscription.save()
+        current_subscription = Subscription.get_active_subscription_by_domain(self.domain)
+        if current_subscription.plan_version.plan.edition != plan:
+            current_subscription.change_plan(DefaultProductPlan.get_default_plan_version(plan))
 
     def test_default_export_settings_no_ff_enabled(self):
         """
@@ -47,22 +49,58 @@ class TestExportUtils(TestCase):
         self.assertIsNone(settings)
 
     @flag_enabled('DEFAULT_EXPORT_SETTINGS')
-    def test_default_export_settings_non_enterprise_domain(self):
+    def test_default_export_settings_community_domain(self):
         """
-        Verify software plan editions that do not have access to default export settings
-        are not able to create a DefaultExportSettings instance
+        Verify COMMUNITY software plans do not have access to default export settings
         """
-        def test(plan_edition):
-            self.update_subscription(plan_edition)
-            settings = get_or_create_default_export_settings_for_domain(self.domain)
-            self.assertIsNone(settings)
+        self.update_subscription(SoftwarePlanEdition.COMMUNITY)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
 
-        yield test, SoftwarePlanEdition.COMMUNITY
-        yield test, SoftwarePlanEdition.STANDARD
-        yield test, SoftwarePlanEdition.PRO
-        yield test, SoftwarePlanEdition.ADVANCED
-        yield test, SoftwarePlanEdition.RESELLER
-        yield test, SoftwarePlanEdition.MANAGED_HOSTING
+    @flag_enabled('DEFAULT_EXPORT_SETTINGS')
+    def test_default_export_settings_standard_domain(self):
+        """
+        Verify STANDARD software plans do not have access to default export settings
+        """
+        self.update_subscription(SoftwarePlanEdition.STANDARD)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
+
+    @flag_enabled('DEFAULT_EXPORT_SETTINGS')
+    def test_default_export_settings_pro_domain(self):
+        """
+        Verify PRO software plans do not have access to default export settings
+        """
+        self.update_subscription(SoftwarePlanEdition.PRO)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
+
+    @flag_enabled('DEFAULT_EXPORT_SETTINGS')
+    def test_default_export_settings_advanced_domain(self):
+        """
+        Verify ADVANCED software plans do not have access to default export settings
+        """
+        self.update_subscription(SoftwarePlanEdition.ADVANCED)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
+
+    @flag_enabled('DEFAULT_EXPORT_SETTINGS')
+    def test_default_export_settings_reseller_domain(self):
+        """
+        Verify RESELLER software plans do not have access to default export settings
+        """
+        self.update_subscription(SoftwarePlanEdition.RESELLER)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
+
+    @flag_enabled('DEFAULT_EXPORT_SETTINGS')
+    def test_default_export_settings_managed_hosting_domain(self):
+        """
+        Verify MANAGED_HOSTING software plans do not have access to default export settings
+        """
+        self.update_subscription(SoftwarePlanEdition.MANAGED_HOSTING)
+        settings = get_or_create_default_export_settings_for_domain(self.domain)
+        self.assertIsNone(settings)
 
     @flag_enabled('DEFAULT_EXPORT_SETTINGS')
     def test_default_export_settings_enterprise_domain(self):
