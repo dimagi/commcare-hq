0bee59de720d229f925f04c067987a830d3045d1
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-01-22 11:13:31 -0500

    python3-saml expects the "request_data" parameter to be a specifc-formatted dict, rather than a raw request object.

--- corehq/apps/sso/decorators.py
+++ corehq/apps/sso/decorators.py
@@ -5,6 +5,7 @@ from onelogin.saml2.auth import OneLogin_Saml2_Auth
 
 from corehq.apps.sso.models import IdentityProvider
 from corehq.apps.sso.configuration import get_saml2_config
+from corehq.apps.sso.utils import get_request_data
 
 
 def identity_provider_required(view_func):
@@ -19,7 +20,10 @@ def use_saml2_auth(view_func):
     @wraps(view_func)
     def _inner(request, idp_slug, *args, **kwargs):
         request.idp = _get_idp_or_404(idp_slug)
-        request.saml2_auth = OneLogin_Saml2_Auth(request, get_saml2_config(request.idp))
+        request.saml2_request_data = get_request_data(request)
+        request.saml2_auth = OneLogin_Saml2_Auth(
+            request.saml2_request_data, get_saml2_config(request.idp)
+        )
         return view_func(request, idp_slug, *args, **kwargs)
     return _inner
 
