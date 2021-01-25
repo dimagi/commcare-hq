02630ccd5793e338c029e4895ca411bd8915bf55
Amit Phulera <amitphulera999@gmail.com>
2021-01-25 18:50:18 +0530

    update test data

--- corehq/apps/reports/tests/filters/user_list.py
+++ corehq/apps/reports/tests/filters/user_list.py
@@ -1,56 +1,59 @@
+# test data for corehq.apps.reports.tests.test_filters.TestEMWFilterOutput
 dummy_user_list = [
     {
-        'domain': 'emfw-filter-output-test',
+        'domain': 'emwf-filter-output-test',
         'username': 'active-worker-1',
         'password': 'Some secret Pass',
         'created_by': None,
         'created_via': None,
         'email': 'activeworker@commcarehq.com',
-        'uuid': 'abcd11',
+        'uuid': 'active1',
         'is_active': True,
         'doc_type': 'CommcareUser'
     },
     {
-        'domain': 'emfw-filter-output-test',
+        'domain': 'emwf-filter-output-test',
         'username': 'active-worker-2',
         'password': 'Some secret Pass',
         'created_by': None,
         'created_via': None,
         'email': 'activeworker2@commcarehq.com',
-        'uuid': 'abcd21',
+        'uuid': 'active2',
         'is_active': True,
         'doc_type': 'CommcareUser'
     },
     {
-        'domain': 'emfw-filter-output-test',
+        'domain': 'emwf-filter-output-test',
         'username': 'deactivated-worker-1',
         'password': 'Some secret Pass',
         'created_by': None,
         'created_via': None,
         'email': 'deactiveworker1@commcarehq.com',
-        'uuid': 'abcd31',
+        'uuid': 'deactive1',
         'is_active': False,
         'doc_type': 'CommcareUser'
     },
     {
-        'domain': 'emfw-filter-output-test',
+        'domain': 'emwf-filter-output-test',
         'username': 'deactivated-worker-2',
         'password': 'Some secret Pass',
         'created_by': None,
         'created_via': None,
         'email': 'deactiveworker2@commcarehq.com',
-        'uuid': 'abcd41',
+        'uuid': 'deactive2',
         'is_active': False,
         'doc_type': 'CommcareUser'
     },
     {
-        'domain': 'emfw-filter-output-test',
+        'domain': 'emwf-filter-output-test',
         'username': 'web-worker-1',
         'password': 'Some secret Pass',
         'created_by': None,
         'created_via': None,
         'email': 'webworker@commcarehq.com',
-        'uuid': 'abcd5',
+        'uuid': 'web1',
+        'is_active': True,
+        "timezone": "UTC",
         'doc_type': 'WebUser'
     },
 ]
