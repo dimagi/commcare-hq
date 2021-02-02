55766d6367b0de3de1deed76effa38efd7d5686f
Matt Riley <mriley@dimagi.com>
2021-02-02 17:32:08 -0500

    Fixed a ResourceConflict when fetching failed logins

--- corehq/apps/hqwebapp/signals.py
+++ corehq/apps/hqwebapp/signals.py
@@ -28,7 +28,7 @@ def clear_failed_logins_and_unlock_account(sender, request, user, **kwargs):
 
 @receiver(user_login_failed)
 def add_failed_attempt(sender, credentials, token_failure=False, **kwargs):
-    user = CouchUser.get_by_username(credentials['username'])
+    user = CouchUser.get_by_username(credentials['username'], strict=True)
     if not user:
         metrics_counter('commcare.auth.invalid_user')
         return
