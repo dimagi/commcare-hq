bdadf3d2c78fa71703f388738c0d4806d83c104b
Simon Kelly <skelly@dimagi.com>
2021-02-04 10:40:19 +0200

    lint

--- corehq/apps/ota/views.py
+++ corehq/apps/ota/views.py
@@ -202,8 +202,8 @@ def get_restore_response(domain, couch_user, app_id=None, since=None, version='1
     :param openrosa_version:
     :param case_sync: Override default case sync algorithm
     :param skip_fixtures: Do not include fixtures in sync payload
-    :param auth_type: The type of auth that was used to authenticate the request. Used to determine if the request is
-                    coming from an actual user of as part of some automation.
+    :param auth_type: The type of auth that was used to authenticate the request.
+        Used to determine if the request is coming from an actual user of as part of some automation.
     :return: Tuple of (http response, timing context or None)
     """
 
