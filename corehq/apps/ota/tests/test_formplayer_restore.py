c59306c84fabe41cab0001829e4f288e86eafc9a
Simon Kelly <skelly@dimagi.com>
2021-02-04 15:55:26 +0200

    clean imports

--- corehq/apps/ota/tests/test_formplayer_restore.py
+++ corehq/apps/ota/tests/test_formplayer_restore.py
@@ -1,5 +1,3 @@
-import uuid
-
 import six
 from django.test import TestCase, override_settings
 from django.urls import reverse
@@ -8,7 +6,7 @@ from casexml.apps.phone.models import SyncLogSQL
 from corehq.apps.domain.auth import FORMPLAYER
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.domain.tests.test_utils import delete_all_domains
-from corehq.apps.users.models import CommCareUser, WebUser
+from corehq.apps.users.models import CommCareUser
 from corehq.util.hmac_request import get_hmac_digest
 
 
