fa6c6e4a095b46cafe7cb890a630c9eac3c2ea73
Cory Zue <czue@dimagi.com>
2021-02-08 15:48:16 +0200

    extract case references to their own model

--- corehq/apps/consumer_forms/admin.py
+++ corehq/apps/consumer_forms/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import AuthenticatedLink
+from .models import AuthenticatedLink, CaseReference
 
 
 @admin.register(AuthenticatedLink)
 class AuthenticatedLinkAdmin(admin.ModelAdmin):
     list_display = ('link_id', 'domain', 'created_on', 'expires_on')
     list_filter = ('domain', 'created_on', 'expires_on')
+
+
+@admin.register(CaseReference)
+class CaseReferenceAdmin(admin.ModelAdmin):
+    list_display = ('case_id', 'link')
--- corehq/apps/consumer_forms/models.py
+++ corehq/apps/consumer_forms/models.py
@@ -20,7 +20,6 @@ class ConsumerForm(models.Model):
 class AuthenticatedLink(models.Model):
     link_id = models.UUIDField(unique=True, db_index=True, default=uuid.uuid4)
     domain = models.CharField(max_length=126, null=False, db_index=True)
-    case_ids = JSONField(default=list, help_text=_('List of cases this link is allowed to access.'))
     created_on = models.DateTimeField(auto_now=True)
     expires_on = models.DateTimeField(null=True, blank=True)
     allows_submission = models.BooleanField(default=False, help_text=_('If the link allows data submission'))
@@ -36,7 +35,24 @@ class AuthenticatedLink(models.Model):
     def is_valid(self):
         return not self.is_used and self.expires_on is None or self.expires_on > datetime.utcnow()
 
+    @property
+    def case_ids(self):
+        # most of HQ that uses case ids expects them to be a list of of strings and not a
+        # queryset of uuids...
+        return [str(case_id) for case_id in self.case_data.values_list('case_id', flat=True)]
+
     def get_data(self):
         return [
             c.to_json() for c in CaseAccessors(self.domain).get_cases(self.case_ids)
         ]
+
+
+class CaseReference(models.Model):
+
+    link = models.ForeignKey(AuthenticatedLink, on_delete=models.CASCADE, related_name='case_data')
+    case_id = models.UUIDField()
+    # in the future could also attach metadata
+
+    class Meta:
+        unique_together = ('link', 'case_id')
+
