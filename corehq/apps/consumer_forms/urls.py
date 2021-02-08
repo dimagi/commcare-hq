f4e5e305e7fb5e5f6aaf1537ff5cef088b7e71bc
Cory Zue <czue@dimagi.com>
2021-02-08 16:41:06 +0200

    add stub url/views for consumer forms

--- corehq/apps/consumer_forms/urls.py
+++ corehq/apps/consumer_forms/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
+from .views import consumer_form_reference, consumer_form, consumer_form_with_link
 
 app_name = 'consumer_forms'
 
 urlpatterns = [
-    # path("link/<slug:link_id>/", access_authenticated_link, name='generate_data_dictionary'),
+    path("reference/<slug:link_id>/", consumer_form_reference, name='consumer_form_reference'),
+    path("s/<slug:form_id>/", consumer_form, name='consumer_form'),
+    path("s/<slug:form_id>/<slug:link_id>/", consumer_form_with_link, name='consumer_form_with_link'),
 ]
--- corehq/apps/consumer_forms/views.py
+++ corehq/apps/consumer_forms/views.py
@@ -1,2 +1,23 @@
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
+from corehq.apps.authenticated_links.models import AuthenticatedLink
+
+
+def consumer_form_reference(request, domain, link_id):
+    """
+    Reference / proof-of-concept hard-coded implementation of consumer forms.
+    """
+    link = get_object_or_404(AuthenticatedLink, domain=domain, link_id=link_id)
+    # print(link.get_data())
+    return JsonResponse({'link': link.get_data()})
+
+
+def consumer_form(request, domain, form_id):
+    # todo
+    pass
+
+
+def consumer_form_with_link(request, domain, form_id, link_id):
+    # todo
+    pass
