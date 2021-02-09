b1f8880193b59234eeb37c270a26a135c58279dd
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-02-09 14:14:41 -0500

    add edit form for identity provider in accounting admin

--- corehq/apps/sso/forms.py
+++ corehq/apps/sso/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.db import transaction
 from django.template.defaultfilters import slugify
+from django.urls import reverse
 from django.utils.translation import ugettext_lazy
 from django.utils.translation import ugettext as _
 
@@ -10,7 +11,18 @@ from crispy_forms import layout as crispy
 from corehq.apps.accounting.models import BillingAccount
 from corehq.apps.hqwebapp import crispy as hqcrispy
 from crispy_forms import bootstrap as twbscrispy
+from corehq.apps.hqwebapp.widgets import BootstrapCheckboxInput
 from corehq.apps.sso.models import IdentityProvider
+from corehq.apps.sso import utils
+
+
+def _validate_or_raise_slugify_error(slug):
+    slugified_test = slugify(slug)
+    if slug != slugified_test:
+        raise forms.ValidationError(
+            _("You did not enter a valid slug. Did you mean '{}'?").format(
+                slugified_test)
+        )
 
 
 class CreateIdentityProviderForm(forms.Form):
@@ -31,8 +43,6 @@ class CreateIdentityProviderForm(forms.Form):
         label=ugettext_lazy("Slug for SP Endpoints"),
         help_text=ugettext_lazy(
             "This will be the unique slug for this IdP's SAML2 urls. "
-            "You will NOT be able to edit this through the Admin UI once the "
-            "Identity Provider is created."
         ),
         max_length=256,
     )
@@ -64,11 +74,7 @@ class CreateIdentityProviderForm(forms.Form):
 
     def clean_slug(self):
         slug = self.cleaned_data['slug']
-        slugified_test = slugify(slug)
-        if slug != slugified_test:
-            raise forms.ValidationError(
-                _("You did not enter a valid slug. Did you mean '{}'?").format(slugified_test)
-            )
+        _validate_or_raise_slugify_error(slug)
         if IdentityProvider.objects.filter(slug=slug).exists():
             raise forms.ValidationError(
                 _("A Identity Provider already exists with this slug.")
@@ -87,3 +93,167 @@ class CreateIdentityProviderForm(forms.Form):
         )
         idp.create_service_provider_certificate()
         return idp
+
+
+class EditIdentityProviderAdminForm(forms.Form):
+    """This is the form used by Accounting admins to modify the IdentityProvider
+    configuration
+    """
+    owner = forms.CharField(
+        label=ugettext_lazy("Billing Account Owner"),
+        required=False,
+    )
+    sp_entity_id = forms.CharField(
+        label=ugettext_lazy("Entity ID"),
+        required=False,
+    )
+    sp_acs_url = forms.CharField(
+        label=ugettext_lazy("Assertion Consumer Service"),
+        required=False,
+    )
+    sp_logout_url = forms.CharField(
+        label=ugettext_lazy("Logout URL"),
+        required=False,
+    )
+    slug = forms.CharField(
+        label=ugettext_lazy("Slug"),
+        required=False,
+        help_text=ugettext_lazy(
+            "CAUTION: Changing this value will alter the SAML endpoint URLs "
+            "below and affect active SSO setups for the client!"
+        ),
+    )
+    sp_public_cert = forms.CharField(
+        label=ugettext_lazy("Public x509 Cert"),
+        required=False,
+    )
+    sp_public_cert_expiration = forms.CharField(
+        label=ugettext_lazy("Certificate Expires On"),
+        required=False,
+    )
+    sp_rollover_cert = forms.CharField(
+        label=ugettext_lazy("Rollover x509 Cert"),
+        required=False,
+    )
+    name = forms.CharField(
+        label=ugettext_lazy("Public Name"),
+        max_length=128,
+        help_text=ugettext_lazy(
+            "This text is what users will see when logging in with SSO."
+        )
+    )
+    is_editable = forms.BooleanField(
+        label=ugettext_lazy("Enterprise Dashboard"),
+        required=False,
+        widget=BootstrapCheckboxInput(
+            inline_label=ugettext_lazy(
+                "Allow Enterprise Admins to edit SSO Enterprise Settings"
+            ),
+        ),
+    )
+    is_active = forms.BooleanField(
+        label=ugettext_lazy("Active Status"),
+        required=False,
+        widget=BootstrapCheckboxInput(
+            inline_label=ugettext_lazy(
+                "Single Sign On is active"
+            ),
+        ),
+        help_text=ugettext_lazy(
+            "Force users with matching email domains to log in with SSO."
+        ),
+    )
+
+    def __init__(self, identity_provider, *args, **kwargs):
+        self.idp = identity_provider
+        kwargs['initial'] = {
+            'name': identity_provider.name,
+            'is_editable': identity_provider.is_editable,
+            'is_active': identity_provider.is_active,
+            'slug': identity_provider.slug,
+        }
+        super().__init__(*args, **kwargs)
+
+        from corehq.apps.accounting.views import ManageBillingAccountView
+        account_link = reverse(
+            ManageBillingAccountView.urlname,
+            args=(identity_provider.owner.id,)
+        )
+        sp_entity_id = utils.get_saml_entity_id(identity_provider)
+        sp_acs_url = utils.get_saml_acs_url(identity_provider)
+        sp_sls_url = utils.get_saml_sls_url(identity_provider)
+        download = _("Download")
+
+        self.helper = FormHelper()
+        self.helper.label_class = 'col-sm-3 col-md-2'
+        self.helper.field_class = 'col-sm-9 col-md-8 col-lg-6'
+        self.helper.layout = crispy.Layout(
+            crispy.Fieldset(
+                _('Primary Configuration'),
+                hqcrispy.B3TextField(
+                    'owner',
+                    f'<a href="{account_link}">'
+                    f'{identity_provider.owner.name}</a>'
+                ),
+                'name',
+                twbscrispy.PrependedText('is_editable', ''),
+                twbscrispy.PrependedText('is_active', ''),
+            ),
+            crispy.Fieldset(
+                _('Service Provider Settings'),
+                'slug',
+                hqcrispy.B3TextField(
+                    'sp_entity_id',
+                    f'<a href="{sp_entity_id}">{sp_entity_id}</a>',
+                ),
+                hqcrispy.B3TextField(
+                    'sp_acs_url',
+                    f'<a href="{sp_acs_url}">{sp_acs_url}</a>',
+                ),
+                hqcrispy.B3TextField(
+                    'sp_logout_url',
+                    f'<a href="{sp_sls_url}">{sp_sls_url}</a>',
+                ),
+                hqcrispy.B3TextField(
+                    'sp_public_cert',
+                    f'<a href="?sp_cert_public" target="_blank">{download}</a>',
+                ),
+                hqcrispy.B3TextField(
+                    'sp_public_cert_expiration',
+                    identity_provider.date_sp_cert_expiration.strftime(
+                        '%d %B %Y at %H:%M UTC'
+                    ),
+                ),
+                hqcrispy.B3TextField(
+                    'sp_rollover_cert',
+                    (f'<a href="?sp_rollover_cert_public" target="_blank">{download}</a>'
+                     if identity_provider.sp_rollover_cert_public
+                     else _("Not needed/generated yet.")),
+                ),
+            ),
+            hqcrispy.FormActions(
+                twbscrispy.StrictButton(
+                    ugettext_lazy("Update Primary Configuration"),
+                    type="submit",
+                    css_class="btn btn-primary",
+                )
+            )
+        )
+
+    def clean_slug(self):
+        slug = self.cleaned_data['slug']
+        _validate_or_raise_slugify_error(slug)
+        if IdentityProvider.objects.filter(slug=slug).exclude(id=self.idp.id).exists():
+            raise forms.ValidationError(
+                _("Another Identity Provider already exists with this slug.")
+            )
+        return slug
+
+    @transaction.atomic
+    def update_identity_provider(self, admin_user):
+        self.idp.slug = self.cleaned_data['slug']
+        self.idp.is_editable = self.cleaned_data['is_editable']
+        self.idp.is_active = self.cleaned_data['is_active']
+        self.idp.last_modified_by = admin_user.username
+        self.idp.save()
+        return self.idp
