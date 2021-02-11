a86e9f5131de882cf74eb484c30790eb1f2a2232
Matt Riley <mriley@dimagi.com>
2021-02-11 14:21:18 -0500

    Created a redis login failures model

---
+++ corehq/apps/users/login_record.py
@@ -0,0 +1,83 @@
+from datetime import datetime
+from django_redis import get_redis_connection
+
+ONE_DAY = 60 * 60 * 24
+EPOCH = datetime.utcfromtimestamp(0)
+
+
+class LoginRecord:
+    """A thread-safe class to record login failures."""
+
+    FIELD_FAILURES = 'failures'
+    FIELD_TS = 'ts'
+
+    @classmethod
+    def get(cls, username):
+        raw_failures, raw_ts = cls._get_raw_fields(username)
+        attempts_ts = float(raw_ts or 0)
+        num_failures = int(raw_failures or 0)
+
+        last_attempt_date = datetime.utcfromtimestamp(attempts_ts or 0)
+
+        return LoginRecord(username, num_failures, last_attempt_date)
+
+    @classmethod
+    def _get_raw_fields(cls, username):
+        redis_client = get_redis_connection()
+        return redis_client.hmget(cls._get_key(username), cls.FIELD_FAILURES, cls.FIELD_TS)
+
+    @staticmethod
+    def _get_key(username):
+        return f'attempts_{username}'
+
+    def __init__(self, username, failures=0, last_attempt_date=None):
+        self.username = username
+        self._reset_values(failures, last_attempt_date)
+
+        self.key = self._get_key(self.username)
+
+    def _reset_values(self, failures=0, last_attempt_date=None):
+        self.failures = failures
+        self.last_attempt_date = last_attempt_date or EPOCH
+
+    def clear(self):
+        redis_client = get_redis_connection()
+        redis_client.delete(self.key)
+
+        self._reset_values()
+
+    def add_failure(self, current_time):
+        redis_client = get_redis_connection()
+        transaction_results = redis_client.transaction(
+            self._create_failure_handler(current_time),
+            self.key)
+        num_failures = transaction_results[0]
+
+        if num_failures is True:
+            num_failures = 1
+
+        self._reset_values(failures=num_failures, last_attempt_date=current_time)
+
+        return num_failures
+
+    def _create_failure_handler(self, current_time):
+        def add_failure(pipe):
+            previous_attempt_ts = float(pipe.hget(self.key, self.FIELD_TS) or 0)
+            previous_attempt_date = datetime.utcfromtimestamp(previous_attempt_ts).date()
+
+            time_between_attempts = current_time.date() - previous_attempt_date
+
+            pipe.multi()
+
+            if time_between_attempts.days >= 1:
+                pipe.hset(self.key, self.FIELD_FAILURES, 1)
+            else:
+                pipe.hincrby(self.key, self.FIELD_FAILURES, 1)
+
+            pipe.hset(self.key, self.FIELD_TS, current_time.timestamp())
+            pipe.expire(self.key, ONE_DAY)
+
+        return add_failure
+
+    def __repr__(self):
+        return f'{self.username}: {self.attempts} failures, last at: {self.last_attempt_date}'
---
+++ corehq/apps/users/tests/test_login_record.py
@@ -0,0 +1,87 @@
+from datetime import datetime
+from django.test import SimpleTestCase, TestCase
+
+from ..login_record import LoginRecord, EPOCH
+
+
+class TestLoginRecord_Constructor(SimpleTestCase):
+    def test_creates_valid_object(self):
+        current_time = datetime(2020, 12, 5)
+        record = LoginRecord('test_user', failures=4, last_attempt_date=current_time)
+        self.assertEqual(record.username, 'test_user')
+        self.assertEqual(record.failures, 4)
+        self.assertEqual(record.last_attempt_date, current_time)
+
+    def test_handles_default_arguments(self):
+        record = LoginRecord('test_user')
+        self.assertEqual(record.failures, 0)
+        self.assertEqual(record.last_attempt_date, EPOCH)
+
+
+class TestLoginRecord(TestCase):
+    def setUp(self):
+        # ensure 'test_user' doesn't exist already
+        record = LoginRecord('test_user')
+        record.clear()
+
+    def tearDown(self):
+        record = LoginRecord('test_user')
+        record.clear()
+
+    def test_get_populates_empty_user(self):
+        record = LoginRecord.get('test_user')
+        self.assertEqual(record.failures, 0)
+        self.assertEqual(record.last_attempt_date, EPOCH)
+
+    def test_clear_handles_missing_user(self):
+        record = LoginRecord('missing_user')
+        record.clear()
+
+        self.assertEqual(record.failures, 0)
+        self.assertEqual(record.last_attempt_date, EPOCH)
+
+    def test_inserts_add_failure_increments_failure_count(self):
+        record = LoginRecord.get('test_user')
+        current_time = datetime(2020, 12, 5)
+        record.add_failure(current_time)
+
+        # ensure the record fields were updated after insert
+        self.assertEqual(record.failures, 1)
+        self.assertEqual(record.last_attempt_date, current_time)
+
+        # ensure the database roundtrip
+        inserted_record = LoginRecord.get('test_user')
+        self.assertEqual(inserted_record.failures, 1)
+        self.assertEqual(inserted_record.last_attempt_date, current_time)
+
+    def test_clear_resets_user(self):
+        record = LoginRecord.get('test_user')
+        record.add_failure(datetime.utcnow())
+
+        record.clear()
+
+        # ensure the record fields were updated after clear
+        self.assertEqual(record.failures, 0)
+        self.assertEqual(record.last_attempt_date, EPOCH)
+
+        cleared_record = LoginRecord.get('test_user')
+        self.assertEqual(cleared_record.failures, 0)
+        self.assertEqual(cleared_record.last_attempt_date, EPOCH)
+
+    def test_multiple_failures_increment_failure_counter(self):
+        record = LoginRecord.get('test_user')
+        record.add_failure(datetime.utcnow())
+        record.add_failure(datetime.utcnow())
+        record.add_failure(datetime.utcnow())
+        self.assertEqual(record.failures, 3)
+
+    def test_interleaved_inserts_update_correctly(self):
+        record_one = LoginRecord.get('test_user')
+        record_two = LoginRecord.get('test_user')
+
+        record_one.add_failure(datetime.utcnow())
+        record_one.add_failure(datetime.utcnow())
+        record_one.add_failure(datetime.utcnow())
+
+        record_two.add_failure(datetime.utcnow())
+        self.assertEqual(record_two.failures, 4)
