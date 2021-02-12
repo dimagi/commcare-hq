8a32196624020d548dd99862154d523e284f427a
Norman Hooper <nhooper@dimagi.com>
2021-02-12 17:47:55 +0200

    FHIRRepeater, form, views

--- corehq/motech/fhir/const.py
+++ corehq/motech/fhir/const.py
@@ -2,3 +2,9 @@ FHIR_VERSION_4_0_1 = '4.0.1'
 FHIR_VERSIONS = [
     (FHIR_VERSION_4_0_1, 'R4'),
 ]
+
+# The XMLNS to use for forms for data imported from a remote FHIR
+# service. e.g. Setting `external_id` on a case to its FHIR ID, or cases
+# created or updated in CommCare via our FHIR API. This is to prevent
+# FHIRRepeater from sending data from a FHIR service back again.
+XMLNS_FHIR = 'http://commcarehq.org/x/fhir/engine-read'
