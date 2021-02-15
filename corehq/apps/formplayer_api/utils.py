03dc2aa91fa1c1c795c8f5c351cf2d11cbd25f68
Simon Kelly <skelly@dimagi.com>
2021-02-15 11:09:22 +0200

    extract user check to function

--- corehq/apps/formplayer_api/utils.py
+++ corehq/apps/formplayer_api/utils.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 
+from corehq.apps.formplayer_api.exceptions import FormplayerAPIException
+from corehq.apps.users.models import CouchUser
 from dimagi.utils.web import get_url_base
 
 
@@ -8,3 +10,10 @@ def get_formplayer_url():
     if not formplayer_url.startswith('http'):
         formplayer_url = '{}{}'.format(get_url_base(), formplayer_url)
     return formplayer_url
+
+
+def check_user_access(domain, username, allow_mirroring=False):
+    user = CouchUser.get_by_username(username)
+    if not user.is_member_of(domain, allow_mirroring=allow_mirroring):
+        raise FormplayerAPIException(f"User '{username}' does not have access to domain '{domain}'")
+    return user
