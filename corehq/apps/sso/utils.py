27ab7ccc3bed14349e7d4eb468e940ab60810f93
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-02-16 18:29:02 -0500

    fix error when a linked_subscription isn't found

--- corehq/apps/sso/utils.py
+++ corehq/apps/sso/utils.py
@@ -55,8 +55,11 @@ def get_dashboard_link(identity_provider):
         is_active=True,
         account__is_active=True,
     ).first()
-    return reverse(
-        EditIdentityProviderEnterpriseView.urlname,
-        args=(linked_subscription.subscriber.domain, identity_provider.slug,)
-    )
+    if linked_subscription:
+        return reverse(
+            EditIdentityProviderEnterpriseView.urlname,
+            args=(linked_subscription.subscriber.domain, identity_provider.slug,)
+        )
+    # would only ever reach this in tests
+    return '#'
 
