9aa398b373aba8d32e3c755d3bb9fa480defecda
Graham Herceg <g.a.herceg@gmail.com>
2021-02-16 13:34:32 -0500

    change export_settings ff to username scope

--- corehq/apps/enterprise/views.py
+++ corehq/apps/enterprise/views.py
@@ -42,7 +42,7 @@ from corehq.apps.domain.decorators import (
 )
 
 from corehq.apps.accounting.utils.subscription import get_account_or_404
-from corehq.apps.export.utils import get_default_export_settings_for_domain
+from corehq.apps.export.utils import get_default_export_settings_for_user
 from corehq.apps.hqwebapp.views import CRUDPaginatedViewMixin
 from corehq.const import USER_DATE_FORMAT
 
@@ -113,13 +113,14 @@ def enterprise_dashboard_email(request, domain, slug):
 @login_and_domain_required
 def enterprise_settings(request, domain):
     account = get_account_or_404(request, domain)
-    export_settings = get_default_export_settings_for_domain(domain)
+    export_settings = get_default_export_settings_for_user(request.user.username, domain)
 
     if request.method == 'POST':
         form = EnterpriseSettingsForm(request.POST, domain=domain, account=account,
-                                      export_settings=export_settings)
+                                      username=request.user.username, export_settings=export_settings)
     else:
-        form = EnterpriseSettingsForm(domain=domain, account=account, export_settings=export_settings)
+        form = EnterpriseSettingsForm(domain=domain, account=account, username=request.user.username,
+                                      export_settings=export_settings)
 
     context = {
         'account': account,
@@ -139,8 +140,9 @@ def enterprise_settings(request, domain):
 @require_POST
 def edit_enterprise_settings(request, domain):
     account = get_account_or_404(request, domain)
-    export_settings = get_default_export_settings_for_domain(domain)
-    form = EnterpriseSettingsForm(request.POST, domain=domain, account=account, export_settings=export_settings)
+    export_settings = get_default_export_settings_for_user(request.user.username, domain)
+    form = EnterpriseSettingsForm(request.POST, username=request.user.username, domain=domain,
+                                  account=account, export_settings=export_settings)
 
     if form.is_valid():
         form.save(account)
