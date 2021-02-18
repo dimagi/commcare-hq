9866c0e77436c9edf7b86bd561e6387c086430a0
Ethan Soergel <esoergel@gmail.com>
2021-02-18 15:02:31 -0500

    Move new API into normal API url scheme

--- corehq/apps/hqcase/urls.py
+++ corehq/apps/hqcase/urls.py
@@ -6,9 +6,11 @@ from corehq.apps.hqcase.views import (
 )
 
 urlpatterns = [
-    url(r'^api/$', case_api, name='case_api'),
-    url(r'^api/(?P<case_id>[\w-]+)/$', case_api, name='case_api'),
-
     # for load testing
     url(r'explode/', ExplodeCasesView.as_view(), name=ExplodeCasesView.url_name),
+]
+
+case_api_urlpatterns = [
+    url(r'^$', case_api, name='case_api'),
+    url(r'^(?P<case_id>[\w-]+)/$', case_api, name='case_api'),
 ]
