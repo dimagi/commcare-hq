94b476ee7d43322e071abd02d33d5c5d6ebd25a4
Norman Hooper <nhooper@dimagi.com>
2021-02-19 18:32:54 +0200

    Cache `get_case_type_obj()`

--- corehq/apps/data_dictionary/util.py
+++ corehq/apps/data_dictionary/util.py
@@ -1,5 +1,6 @@
 from itertools import groupby
 from operator import attrgetter
+from typing import Optional
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext
@@ -169,3 +170,11 @@ def get_data_dict_props_by_case_type(domain):
 def get_data_dict_case_types(domain):
     case_types = CaseType.objects.filter(domain=domain).values_list('name', flat=True)
     return set(case_types)
+
+
+@quickcache(['domain', 'name'])
+def get_case_type_obj(domain, name) -> Optional[CaseType]:
+    try:
+        return CaseType.objects.get(domain=domain, name=name)
+    except CaseType.DoesNotExist:
+        return None
