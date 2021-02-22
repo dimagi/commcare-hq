61a99e2fd9c6f7db38fe897575ea8e3c2c682b8e
Norman Hooper <nhooper@dimagi.com>
2021-02-22 12:51:01 +0200

    deepmerge: Skip case if it has no data

--- corehq/motech/fhir/models.py
+++ corehq/motech/fhir/models.py
@@ -1,5 +1,6 @@
 import json
 import os
+from itertools import zip_longest
 from typing import Optional, Union
 
 from django.conf import settings
@@ -178,12 +179,14 @@ def build_fhir_resource_for_info(
 
     Used by ``FHIRRepeater``.
     """
-    return _build_fhir_resource(info, fhir_version)
+    return _build_fhir_resource(info, fhir_version, skip_empty=True)
 
 
 def _build_fhir_resource(
     info: CaseTriggerInfo,
     fhir_version: str,
+    *,
+    skip_empty: bool = False,
 ) -> Optional[dict]:
 
     case_type = get_case_type_obj(info.domain, info.type)
@@ -199,14 +202,18 @@ def _build_fhir_resource(
     except FHIRResourceType.DoesNotExist:
         return None
 
-    fhir_resource = {
-        **resource_type.template,
-        'id': info.case_id,
-        'resourceType': resource_type.name,  # Always required
-    }
+    fhir_resource = {}
     for prop in resource_type.properties.all():
         value_source = prop.get_value_source()
         value_source.set_external_value(fhir_resource, info)
+    if not fhir_resource and skip_empty:
+        return None
+
+    fhir_resource = deepmerge({
+        **resource_type.template,
+        'id': info.case_id,
+        'resourceType': resource_type.name,  # Always required
+    }, fhir_resource)
     resource_type.validate_resource(fhir_resource)
     return fhir_resource
 
@@ -229,3 +236,37 @@ def get_case_trigger_info(case):
         type=case.type,
         extra_fields={p: case.get_case_property(p) for p in prop_names},
     )
+
+
+def deepmerge(a, b):
+    """
+    Merges ``b`` into ``a``.
+
+    >>> foo = {'one': {'two': 2, 'three': 42}}
+    >>> bar = {'one': {'three': 3}}
+    >>> {**foo, **bar}
+    {'one': {'three': 3}}
+    >>> deepmerge(foo, bar)
+    {'one': {'two': 2, 'three': 3}}
+
+    Dicts and lists are recursed. Other data types are replaced.
+
+    >>> foo = {'one': [{'two': 2}, 42]}
+    >>> bar = {'one': [{'three': 3}]}
+    >>> deepmerge(foo, bar)
+    {'one': [{'two': 2, 'three': 3}, 42]}
+
+    """
+    if isinstance(a, dict) and isinstance(b, dict):
+        for key in b:
+            if key in a:
+                a[key] = deepmerge(a[key], b[key])
+            else:
+                a[key] = b[key]
+        return a
+    elif isinstance(a, list) and isinstance(b, list):
+        return list(deepmerge(aa, bb) for aa, bb in zip_longest(a, b))
+    elif b is None:
+        return a
+    else:
+        return b
