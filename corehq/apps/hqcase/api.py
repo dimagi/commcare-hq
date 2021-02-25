d74fa6dca2a1aedfac826827880ba96f81558e67
Ethan Soergel <esoergel@gmail.com>
2021-02-25 10:27:51 -0500

    Also support couch backend

--- corehq/apps/hqcase/api.py
+++ corehq/apps/hqcase/api.py
@@ -7,7 +7,7 @@ from memoized import memoized
 from casexml.apps.case.mock import CaseBlock, IndexAttrs
 
 from corehq.apps.hqcase.utils import CASEBLOCK_CHUNKSIZE, submit_case_blocks
-from corehq.form_processor.backends.sql.dbaccessors import CaseAccessorSQL
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 
 
 def serialize_case(case):
@@ -191,7 +191,7 @@ def _get_bulk_updates(domain, all_data, user):
 
 
 def _missing_cases(domain, case_ids):
-    real_case_ids = CaseAccessorSQL.get_case_ids_that_exist(domain, case_ids)
+    real_case_ids = CaseAccessors(domain).get_case_ids_that_exist(case_ids)
     return set(case_ids) - set(real_case_ids)
 
 
