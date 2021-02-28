fee3604624518bf49a5f08186275d36bb0cfe10c
Norman Hooper <nhooper@dimagi.com>
2021-02-28 22:59:50 +0200

    Build out repeater_helpers

--- corehq/motech/fhir/const.py
+++ corehq/motech/fhir/const.py
@@ -3,6 +3,19 @@ FHIR_VERSIONS = [
     (FHIR_VERSION_4_0_1, 'R4'),
 ]
 
+# See https://www.hl7.org/fhir/valueset-bundle-type.html
+FHIR_BUNDLE_TYPES = {
+    'document',
+    'message',
+    'transaction',
+    'transaction-response',
+    'batch',
+    'batch-response',
+    'history',
+    'searchset',
+    'collection',
+}
+
 # The XMLNS to use for forms for data imported from a remote FHIR
 # service. e.g. Setting `external_id` on a case to its FHIR ID, or cases
 # created or updated in CommCare via our FHIR API. This is to prevent
