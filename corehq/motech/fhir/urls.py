2c0638ba3759b957394229a13c98ad2ed6fa5561
mkangia <mkangia@dimagi.com>
2021-03-01 15:31:28 +0530

    support different fhir versions

--- corehq/motech/fhir/urls.py
+++ corehq/motech/fhir/urls.py
@@ -6,6 +6,7 @@ from corehq.motech.fhir.views import (
 )
 
 urlpatterns = [
-    url(r'^fhir/R4/(?P<resource_type>\w+)/$', search_view, name="fhir_search"),
-    url(r'^fhir/R4/(?P<resource_type>\w+)/(?P<resource_id>[\w\-]+)$', get_view, name="fhir_get_view"),
+    url(r'^fhir/(?P<fhir_version_name>\w+)/(?P<resource_type>\w+)/$', search_view, name="fhir_search"),
+    url(r'^fhir/(?P<fhir_version_name>\w+)/(?P<resource_type>\w+)/(?P<resource_id>[\w\-]+)/$', get_view,
+        name="fhir_get_view"),
 ]
