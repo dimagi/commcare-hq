2acde439131db86c71045508a301499a63989b26
Norman Hooper <nhooper@dimagi.com>
2021-03-01 10:36:41 +0200

    Drop unuseful comment

--- corehq/motech/models.py
+++ corehq/motech/models.py
@@ -223,7 +223,7 @@ class RequestLog(models.Model):
     #           for RequestLog instances.
     #       They will be ignored.
 
-    domain = models.CharField(max_length=126, db_index=True)  # 126 seems to be a popular length
+    domain = models.CharField(max_length=126, db_index=True)
     timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
     log_level = models.IntegerField(null=True)
     # payload_id is set for requests that are caused by a payload (e.g.
