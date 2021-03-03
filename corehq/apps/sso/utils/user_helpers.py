91f8be14a3a004e8ce74ba9cc812bc842d135961
Biyeun <biyeun@dimagi.com>
2021-03-03 15:07:55 -0600

    update docstring
    Co-authored-by: Daniel Miller <dmiller@dimagi.com>

--- corehq/apps/sso/utils/user_helpers.py
+++ corehq/apps/sso/utils/user_helpers.py
@@ -3,7 +3,7 @@ def get_email_domain_from_username(username):
     A quick utility for getting an Email Domain from a username
     (the last part of the email after the '@' sign)
     :param username: String
-    :return: String username
+    :return: Domain name string. `None` if there is no domain name.
     """
     split_username = username.split('@')
     if len(split_username) < 2:
