74b3d0c0803270750758cfb7819a8ed1c6c3c8f9
Ethan Soergel <esoergel@gmail.com>
2021-03-05 12:53:40 -0500

    make params an object, attempt #1

--- corehq/apps/hqcase/api/get_list.py
+++ corehq/apps/hqcase/api/get_list.py
@@ -1,6 +1,8 @@
 import datetime
+from functools import partial
 from itertools import chain
 
+from attr import attrib, attrs
 from dateutil.parser import parse
 
 from dimagi.utils.parsing import FALSE_STRINGS
@@ -12,18 +14,18 @@ from .core import UserError, serialize_es_case
 
 DEFAULT_PAGE_SIZE = 20
 MAX_PAGE_SIZE = 5000
-CUSTOM_PROPERTY_PREFIX = 'property.'
+CASE_PROPERTY_PREFIX = 'property.'
 
 
 def _to_boolean(val):
     return not (val == '' or val.lower() in FALSE_STRINGS)
 
 
-def _to_int(val, param_name):
+def _to_int(val, param):
     try:
         return int(val)
     except ValueError:
-        raise UserError(f"'{val}' is not a valid value for '{param_name}'")
+        raise UserError(f"'{val}' is not a valid value for '{param}'")
 
 
 def _make_date_filter(date_filter, param):
@@ -66,31 +68,57 @@ FILTERS.update(chain(*[
 ]))
 
 
-def get_list(domain, params):
-    start = _to_int(params.pop('offset', 0), 'offset')
-    page_size = _to_int(params.pop('limit', DEFAULT_PAGE_SIZE), 'limit')
-    if page_size > MAX_PAGE_SIZE:
-        raise UserError(f"You cannot request more than {MAX_PAGE_SIZE} cases per request.")
+@attrs(kw_only=True)
+class CaseListParams:
+    offset = attrib(converter=partial(_to_int, param='offset'))
+    limit = attrib(converter=partial(_to_int, param='limit'))
+    metadata_filters = attrib()
+    case_property_filters = attrib()
+
+    @classmethod
+    def from_querydict(cls, querydict):
+        self = CaseListParams(
+            offset=querydict.pop('offset', 0),
+            limit=querydict.pop('limit', DEFAULT_PAGE_SIZE),
+            metadata_filters=[],
+            case_property_filters=[],
+        )
+        for key, val in querydict.items():
+            if key.startswith(CASE_PROPERTY_PREFIX):
+                self.case_property_filters.append((key, val))
+            elif key in FILTERS:
+                self.metadata_filters.append((key, val))
+            else:
+                raise UserError(f"'{key}' is not a valid parameter.")
+
+        return self
+
+    @limit.validator
+    def validate_page_size(self, attribute, value):
+        if value > MAX_PAGE_SIZE:
+            raise UserError(f"You cannot request more than {MAX_PAGE_SIZE} cases per request.")
+
+
+def get_list(domain, querydict):
+    params = CaseListParams.from_querydict(querydict)
 
     query = (case_search.CaseSearchES()
              .domain(domain)
-             .size(page_size)
-             .start(start)
+             .size(params.limit)
+             .start(params.offset)
              .sort("@indexed_on"))
 
-    for key, val in params.items():
-        if key.startswith(CUSTOM_PROPERTY_PREFIX):
-            query = query.filter(_get_custom_property_filter(key, val))
-        elif key in FILTERS:
-            query = query.filter(FILTERS[key](val))
-        else:
-            raise UserError(f"'{key}' is not a valid parameter.")
+    for key, val in params.metadata_filters:
+        query = query.filter(FILTERS[key](val))
+
+    for key, val in params.case_property_filters:
+        query = query.filter(_get_custom_property_filter(key, val))
 
     return [serialize_es_case(case) for case in query.run().hits]
 
 
 def _get_custom_property_filter(key, val):
-    prop = key[len(CUSTOM_PROPERTY_PREFIX):]
+    prop = key[len(CASE_PROPERTY_PREFIX):]
     if val == "":
         return case_search.case_property_missing(prop)
     return case_search.exact_case_property_text_query(prop, val)
