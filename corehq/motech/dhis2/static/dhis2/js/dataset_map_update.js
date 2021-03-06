60b4631dcba11dff2bc96d920eb1a298d0131ada
Jennifer Schweers <orange.jenny@gmail.com>
2021-03-05 21:49:30 -0500

    Fixed datepicker references

--- corehq/motech/dhis2/static/dhis2/js/dataset_map_update.js
+++ corehq/motech/dhis2/static/dhis2/js/dataset_map_update.js
@@ -2,7 +2,7 @@ hqDefine("dhis2/js/dataset_map_update", [
     "jquery",
     "hqwebapp/js/crud_paginated_list_init",
     "hqwebapp/js/widgets",
-    'jquery-ui/ui/datepicker',
+    'jquery-ui/ui/widgets/datepicker',
 ], function ($) {
     $('.date-picker').datepicker({ dateFormat: "yy-mm-dd" });
 });
