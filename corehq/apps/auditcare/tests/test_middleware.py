3a94de683f02fc3ed353deeb49123bcdfbb4149c
Daniel Miller <dmiller@dimagi.com>
2021-03-09 10:26:06 -0500

    Move request into setUp

--- corehq/apps/auditcare/tests/test_middleware.py
+++ corehq/apps/auditcare/tests/test_middleware.py
@@ -10,56 +10,58 @@ from .. import middleware as mod
 
 class TestAuditMiddleware(SimpleTestCase):
 
+    def setUp(self):
+        self.request = Config(user="username")
+
     def test_generic_view_not_audited_with_default_settings(self):
-        req, func = make_view()
+        func = make_view()
         with configured_middleware() as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_no_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_no_audit(self.request)
 
     def test_admin_view_is_audited_with_default_settings(self):
-        req, func = make_view(module="django.contrib.admin")
+        func = make_view(module="django.contrib.admin")
         with configured_middleware() as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_audit(self.request)
 
     def test_generic_view_is_audited_with_audit_all_views_setting(self):
-        req, func = make_view()
+        func = make_view()
         settings = Settings(AUDIT_ALL_VIEWS=True)
         with configured_middleware(settings) as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_audit(self.request)
 
     def test_generic_view_class_is_audited_with_audit_all_views_setting(self):
-        req, func = make_view("TheView")
+        func = make_view("TheView")
         settings = Settings(AUDIT_ALL_VIEWS=True)
         with configured_middleware(settings) as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_audit(self.request)
 
     def test_audit_views_setting(self):
-        req, func = make_view("ChangeMyPasswordView", "corehq.apps.settings.views")
+        func = make_view("ChangeMyPasswordView", "corehq.apps.settings.views")
         with configured_middleware() as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_audit(self.request)
 
     def test_audit_modules_setting(self):
-        req, func = make_view("TheView", "corehq.apps.reports")
+        func = make_view("TheView", "corehq.apps.reports")
         with configured_middleware() as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_audit(self.request)
 
     def test_debug_media_view_not_audited(self):
-        req, func = make_view("debug_media", "debug_toolbar.views")
+        func = make_view("debug_media", "debug_toolbar.views")
         with configured_middleware() as ware:
-            ware.process_view(req, func, ARGS, KWARGS)
-        self.assert_no_audit(req)
+            ware.process_view(self.request, func, ARGS, KWARGS)
+        self.assert_no_audit(self.request)
 
     def test_staticfiles_not_audited(self):
         from django.contrib.staticfiles.views import serve
-        req = Config(user="username")
         with configured_middleware() as ware:
-            ware.process_view(req, serve, ARGS, KWARGS)
-        self.assert_no_audit(req)
+            ware.process_view(self.request, serve, ARGS, KWARGS)
+        self.assert_no_audit(self.request)
 
     def assert_audit(self, request):
         audit_doc = getattr(request, "audit_doc", None)
@@ -70,29 +72,25 @@ class TestAuditMiddleware(SimpleTestCase):
 
 
 def test_make_view_function():
-    req, func = make_view()
-    eq(req.user, "username")
+    func = make_view()
     eq(func.__name__, "the_view")
     eq(func.__module__, "corehq.apps.auditcare.views")
 
 
 def test_make_view_class():
-    req, func = make_view("TheView")
-    eq(req.user, "username")
+    func = make_view("TheView")
     eq(func.__class__.__name__, "TheView")
     eq(func.__module__, "corehq.apps.auditcare.views")
 
 
 def test_make_admin_view_function():
-    req, func = make_view("the_view", "django.contrib.admin")
-    eq(req.user, "username")
+    func = make_view("the_view", "django.contrib.admin")
     eq(func.__name__, "the_view")
     eq(func.__module__, "django.contrib.admin")
 
 
 def test_make_admin_view_class():
-    req, func = make_view("TheView", "django.contrib.admin")
-    eq(req.user, "username")
+    func = make_view("TheView", "django.contrib.admin")
     eq(func.__class__.__name__, "TheView")
     eq(func.__module__, "django.contrib.admin")
 
@@ -122,8 +120,7 @@ def make_view(name="the_view", module="corehq.apps.auditcare.views"):
             assert False, "unexpected call"
         view_func.__name__ = name
     view_func.__module__ = module
-    request = Config(user="username")
-    return request, view_func
+    return view_func
 
 
 def fake_audit(request, user, view_func, view_kwargs, extra={}):
