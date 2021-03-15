9682281a552b9a2d7795b88e7496b6d1abd0d614
Farid Rener <proteusvacuum@gmail.com>
2021-03-15 16:36:00 -0400

    Clarify logic for keeping invitations around

--- corehq/apps/consumer_user/signals.py
+++ corehq/apps/consumer_user/signals.py
@@ -3,16 +3,18 @@ from casexml.apps.case.const import CASE_INDEX_EXTENSION
 from corehq.form_processor.models import CommCareCaseSQL
 from corehq.form_processor.signals import sql_case_post_save
 
-from .const import CONSUMER_INVITATION_CASE_TYPE, CONSUMER_INVITATION_STATUS
+from .const import CONSUMER_INVITATION_CASE_TYPE
 from .tasks import create_new_consumer_user_invitation
 
 
 def send_email_case_changed_receiver(sender, case, **kwargs):
     if case.type != CONSUMER_INVITATION_CASE_TYPE:
         return
+
     email = case.get_case_property('email')
     if not email:
         return
+
     host_indices = [index for index in case.indices if index.relationship == CASE_INDEX_EXTENSION]
     try:
         demographic_case_id = host_indices[0].referenced_id
@@ -20,8 +22,7 @@ def send_email_case_changed_receiver(sender, case, **kwargs):
         return
 
     create_new_consumer_user_invitation.delay(
-        case.domain, case.case_id, demographic_case_id, case.closed,
-        case.get_case_property(CONSUMER_INVITATION_STATUS), case.opened_by, email
+        case.domain, case.case_id, demographic_case_id,
     )
 
 
