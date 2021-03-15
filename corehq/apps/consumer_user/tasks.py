04397b7942911caa2b8ff684d62334dcf9021a06
Farid Rener <proteusvacuum@gmail.com>
2021-03-15 16:36:00 -0400

    Clarify logic for keeping invitations around

--- corehq/apps/consumer_user/tasks.py
+++ corehq/apps/consumer_user/tasks.py
@@ -9,6 +9,7 @@ from corehq.apps.consumer_user.models import (
 )
 from corehq.apps.hqcase.utils import update_case
 from corehq.apps.hqwebapp.tasks import send_html_email_async
+from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.util.view_utils import absolute_reverse
 
 from .const import (
@@ -19,48 +20,48 @@ from .const import (
 
 
 @task
-def create_new_consumer_user_invitation(
-    domain, invitation_case_id, demographic_case_id, closed, status, opened_by, email
-):
-    invitation = ConsumerUserInvitation.objects.filter(
-        case_id=invitation_case_id,
-        domain=domain,
-        demographic_case_id=demographic_case_id,
-        active=True,
-    ).last()
+def create_new_consumer_user_invitation(domain, invitation_case_id, demographic_case_id):
+    invitation_case = CaseAccessors(domain).get_case(invitation_case_id)
+    email = invitation_case.get_case_property('email')
+    status = invitation_case.get_case_property(CONSUMER_INVITATION_STATUS)
+    keep_open_status = [CONSUMER_INVITATION_SENT, CONSUMER_INVITATION_ACCEPTED]
 
-    # Set invitation inactive when the invitation case is closed, and there is an invitation
-    if closed:
-        if invitation:
-            invitation.make_inactive()
-        return
-
-    # If the invitation is already "sent" or "accepted" and this is the same email address, do nothing
-    elif (
-        invitation and email == invitation.email
-        and status in [CONSUMER_INVITATION_SENT, CONSUMER_INVITATION_ACCEPTED]
-    ):
-        return
-
-    # Otherwise, close this invitation
-    elif invitation:
-        invitation.make_inactive()
-        if ConsumerUserCaseRelationship.objects.filter(case_id=demographic_case_id, domain=domain).exists():
-            # There is already a relationship with this case_id, so we can't invite someone new
+    try:
+        invitation = ConsumerUserInvitation.objects.get(
+            case_id=invitation_case_id,
+            domain=domain,
+            demographic_case_id=demographic_case_id,
+            active=True,
+        )
+        if invitation.email == email and status in keep_open_status and not invitation_case.closed:
+            # An invitation has already been sent to this address, and this invite has been created, so do nothing
             return
+        # For any other requests, we'll make a new invitation so we can keep track. Deactivate this one.
+        invitation.make_inactive()
+    except ConsumerUserInvitation.DoesNotExist:
+        pass
 
-    invitation = ConsumerUserInvitation.objects.create(
+    if invitation_case.closed:
+        # There is nothing left to do.
+        return
+
+    if ConsumerUserCaseRelationship.objects.filter(case_id=demographic_case_id, domain=domain).exists():
+        # There is already a relationship with this case_id, so don't invite anyone new
+        return
+
+    # Make a new invitation, and send an email to the user
+    new_invitation = ConsumerUserInvitation.objects.create(
         case_id=invitation_case_id,
         domain=domain,
         demographic_case_id=demographic_case_id,
-        invited_by=opened_by,
+        invited_by=invitation_case.opened_by,
         email=email,
     )
     email_context = {
         'link':
             absolute_reverse(
                 'consumer_user:consumer_user_register',
-                kwargs={'invitation': invitation.signature()},
+                kwargs={'invitation': new_invitation.signature()},
             ),
     }
     send_html_email_async.delay(
