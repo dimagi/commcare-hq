39e30408aaadd3898bae1e3a3390a78812188567
Jennifer Schweers <orange.jenny@gmail.com>
2021-03-15 12:16:38 -0400

    Fixed import

--- corehq/motech/fhir/utils.py
+++ corehq/motech/fhir/utils.py
@@ -1,6 +1,6 @@
-from corehq.motech.fhir.views import get_view
 from corehq.util.view_utils import absolute_reverse
 
 
 def resource_url(domain, resource_type, case_id):
+    from corehq.motech.fhir.views import get_view
     return absolute_reverse(get_view, args=(domain, resource_type, case_id))
