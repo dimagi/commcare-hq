7e91b396d0d01e827a0860fa857afe0c82998ee4
Farid Rener <proteusvacuum@gmail.com>
2021-03-17 21:56:57 -0400

    Remove extraneous views

--- corehq/apps/consumer_user/urls.py
+++ corehq/apps/consumer_user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
 from .views import (
+    ConsumerUserLoginView,
     change_contact_details_view,
     change_password_view,
     domains_and_cases_list_view,
-    login_view,
     logout_view,
     register_view,
     success_view,
@@ -14,8 +14,12 @@ app_name = 'consumer_user'
 
 urlpatterns = [
     path('signup/<signed_invitation_id>/', register_view, name='consumer_user_register'),
-    path('login/', login_view, name='consumer_user_login'),
-    path('login/<signed_invitation_id>/', login_view, name='consumer_user_login_with_invitation'),
+    path('login/', ConsumerUserLoginView.as_view(), name='consumer_user_login'),
+    path(
+        'login/<signed_invitation_id>/',
+        ConsumerUserLoginView.as_view(),
+        name='consumer_user_login_with_invitation',
+    ),
     path('logout/', logout_view, name='consumer_user_logout'),
     path('homepage/', success_view, name='consumer_user_homepage'),
     path('change-password/', change_password_view, name='change_password'),
