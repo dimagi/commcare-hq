cdb13774d626137dd70705a8181a6b46b413ed07
Farid Rener <proteusvacuum@gmail.com>
2021-04-02 17:06:34 -0400

    Refactor handle_consumer_user_invitations

--- corehq/apps/consumer_user/tasks.py
+++ corehq/apps/consumer_user/tasks.py
@@ -1,5 +1,5 @@
-from datetime import datetime, timedelta
 from collections import defaultdict
+from datetime import datetime, timedelta
 
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
@@ -11,7 +11,7 @@ from corehq.apps.consumer_user.models import (
     ConsumerUserCaseRelationship,
     ConsumerUserInvitation,
 )
-from corehq.apps.hqcase.utils import update_case, bulk_update_cases
+from corehq.apps.hqcase.utils import bulk_update_cases, update_case
 from corehq.apps.hqwebapp.tasks import send_html_email_async
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.util.view_utils import absolute_reverse
@@ -27,39 +27,19 @@ from .const import (
 @task
 def handle_consumer_user_invitations(domain, invitation_case_id, demographic_case_id):
     invitation_case = CaseAccessors(domain).get_case(invitation_case_id)
-    email = invitation_case.get_case_property('email')
-    status = invitation_case.get_case_property(CONSUMER_INVITATION_STATUS)
-    keep_open_status = [CONSUMER_INVITATION_SENT, CONSUMER_INVITATION_ACCEPTED]
 
-    if not invitation_case.closed and ConsumerUserInvitation.objects.filter(
+    unaccepted_invitation = ConsumerUserInvitation.objects.filter(
         demographic_case_id=demographic_case_id, active=True, accepted=False
-    ).exclude(case_id=invitation_case_id).exists():
-        # If there is currently an open, unaccepted invitation for the same demographic case, do nothing
-        update_case(
-            domain=domain,
-            case_id=invitation_case_id,
-            case_properties={
-                CONSUMER_INVITATION_STATUS: CONSUMER_INVITATION_ERROR,
-                CONSUMER_INVITATION_ERROR: "There is already an open invitation for this case",
-            },
-            device_id=__name__ + '.handle_consumer_user_invitations',
-        )
+    ).exclude(case_id=invitation_case_id)
+    if not invitation_case.closed and unaccepted_invitation.exists():
+        # If there is currently a different open, unaccepted invitation for the same
+        # demographic case, so this is an error
+        _set_case_error(domain, invitation_case_id, "There is already an open invitation for this case")
         return
 
-    try:
-        invitation = ConsumerUserInvitation.objects.get(
-            case_id=invitation_case_id,
-            domain=domain,
-            demographic_case_id=demographic_case_id,
-            active=True,
-        )
-        if invitation.email == email and status in keep_open_status and not invitation_case.closed:
-            # An invitation has already been sent to this address, and this invite has been created, so do nothing
-            return
-        # For any other requests, we'll make a new invitation so we can keep track. Deactivate this one.
-        invitation.make_inactive()
-    except ConsumerUserInvitation.DoesNotExist:
-        pass
+    invitation_already_accepted = _handle_existing_invitation(domain, invitation_case, demographic_case_id)
+    if invitation_already_accepted:
+        return
 
     if invitation_case.closed:
         # There is nothing left to do.
@@ -67,20 +47,55 @@ def handle_consumer_user_invitations(domain, invitation_case_id, demographic_cas
 
     if ConsumerUserCaseRelationship.objects.filter(case_id=demographic_case_id, domain=domain).exists():
         # There is already a relationship with this case_id, so don't invite anyone new
-        update_case(
-            domain=domain,
-            case_id=invitation_case_id,
-            case_properties={
-                CONSUMER_INVITATION_STATUS: CONSUMER_INVITATION_ERROR,
-                CONSUMER_INVITATION_ERROR: "Someone else has already created a user for this case."
-            },
-            device_id=__name__ + '.handle_consumer_user_invitations',
-        )
+        _set_case_error(domain, invitation_case_id, "Someone else has already created a user for this case.")
         return
 
-    # Make a new invitation, and send an email to the user
+    _create_new_invitation(domain, invitation_case, demographic_case_id)
+
+
+def _set_case_error(domain, case_id, error_message):
+    update_case(
+        domain=domain,
+        case_id=case_id,
+        case_properties={
+            CONSUMER_INVITATION_STATUS: CONSUMER_INVITATION_ERROR,
+            CONSUMER_INVITATION_ERROR: error_message,
+        },
+        device_id=__name__ + '.handle_consumer_user_invitations',
+    )
+
+
+def _handle_existing_invitation(domain, invitation_case, demographic_case_id):
+    """Fetches any existing invitations and deactivates them if necessary.
+    Returns whether the invitation has been accepted or not
+    """
+    keep_open_status = [CONSUMER_INVITATION_SENT, CONSUMER_INVITATION_ACCEPTED]
+    email = invitation_case.get_case_property('email')
+    status = invitation_case.get_case_property(CONSUMER_INVITATION_STATUS)
+    try:
+        invitation = ConsumerUserInvitation.objects.get(
+            case_id=invitation_case.case_id,
+            domain=domain,
+            demographic_case_id=demographic_case_id,
+            active=True,
+        )
+        if invitation.email == email and status in keep_open_status and not invitation_case.closed:
+            # An invitation has already been sent to this address, and this invite has been created, so do nothing
+            return True
+        # For any other requests, we'll make a new invitation so we can keep track. Deactivate this one.
+        invitation.make_inactive()
+        return False
+    except ConsumerUserInvitation.DoesNotExist:
+        return False
+
+
+def _create_new_invitation(domain, invitation_case, demographic_case_id):
+    """Make a new invitation, and send an email to the user with the details
+    """
+
+    email = invitation_case.get_case_property('email')
     new_invitation = ConsumerUserInvitation.objects.create(
-        case_id=invitation_case_id,
+        case_id=invitation_case.case_id,
         domain=domain,
         demographic_case_id=demographic_case_id,
         invited_by=invitation_case.opened_by,
@@ -102,7 +117,7 @@ def handle_consumer_user_invitations(domain, invitation_case_id, demographic_cas
 
     update_case(
         domain=domain,
-        case_id=invitation_case_id,
+        case_id=invitation_case.case_id,
         case_properties={
             CONSUMER_INVITATION_STATUS: CONSUMER_INVITATION_SENT,
             CONSUMER_INVITATION_ERROR: "",
