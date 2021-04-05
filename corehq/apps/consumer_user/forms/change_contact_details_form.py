f61a05f4cd7ba8f2efde76116ae2d8bee5d29afc
Farid Rener <proteusvacuum@gmail.com>
2021-04-05 17:17:44 -0400

    Remove unneeded save method from ChangeContactDetailsForm

--- corehq/apps/consumer_user/forms/change_contact_details_form.py
+++ corehq/apps/consumer_user/forms/change_contact_details_form.py
@@ -7,11 +7,3 @@ class ChangeContactDetailsForm(ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name']
-
-    def save(self, commit=True):
-        user = super(ChangeContactDetailsForm, self).save(commit=False)
-        user.first_name = self.cleaned_data["first_name"]
-        user.last_name = self.cleaned_data["last_name"]
-        if commit:
-            user.save()
-        return user
