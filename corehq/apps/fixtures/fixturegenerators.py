e1e3fa03fa149d1c96e5eced26a3a0cf8ba5afef
Sravan Reddy <sravfeyn@gmail.com>
2021-04-06 12:21:53 +0530

    correct

--- corehq/apps/fixtures/fixturegenerators.py
+++ corehq/apps/fixtures/fixturegenerators.py
@@ -68,7 +68,7 @@ def item_lists_by_app(app):
             'id': config.uuid,
             'uri': uri,
             'path': "/rows/row",
-            'name': app.default_language,
+            'name': config.header.get(app.default_language),
             'structure': {},
             'fixture_type': REPORT_FIXTURE,
         })
