29916dae2fa72985939d89038d80403245505ae7
Daniel Miller <dmiller@dimagi.com>
2021-04-09 09:23:00 -0400

    Remove auditcare.views.audited_views
    This view would try to load every navigation audit event from Couch, which is an absurdly large amount of data to load in a single request. Needless to say it is impractical and might even have a bad effect on Couch if it were run on a cluster with a lot of auditcare data.

--- corehq/apps/auditcare/views.py
+++ corehq/apps/auditcare/views.py
@@ -5,9 +5,7 @@ from argparse import ArgumentTypeError
 from django.conf import settings
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http import HttpResponse, HttpResponseBadRequest
-from django.shortcuts import render
 
-from .models import AccessAudit
 from .utils.export import write_export_from_all_log_events
 
 from corehq.util.argparse_types import date_type
@@ -33,13 +31,3 @@ def export_all(request):
     response['Content-Disposition'] = 'attachment; filename="AuditAll.csv"'
     write_export_from_all_log_events(response, start=start, end=end)
     return response
-
-
-@login_required
-@user_passes_test(lambda u: u.is_superuser)
-def audited_views(request, *args, **kwargs):
-    db = AccessAudit.get_db()
-    views = db.view('auditcare/urlpath_by_user_date', reduce=False).all()
-    template = "auditcare/audit_views.html"
-    context = {"audit_views": views}
-    return render(request, template, context)
