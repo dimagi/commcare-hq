b5587f802ad1208daf4f7df4948e5e2c8195ac94
Matt Riley <mriley@dimagi.com>
2021-04-12 22:50:28 -0400

    stick with default escaping for multi select2

--- corehq/apps/reports/static/reports/js/filters/select2s.js
+++ corehq/apps/reports/static/reports/js/filters/select2s.js
@@ -71,7 +71,6 @@ hqDefine("reports/js/filters/select2s", [
         if (!data.endpoint) {
             $filter.select2({
                 width: '100%',
-                escapeMarkup: function (m) { return m; },
             });
             return;
         }
@@ -120,7 +119,6 @@ hqDefine("reports/js/filters/select2s", [
                 },
             },
             multiple: true,
-            escapeMarkup: function (m) { return m; },
             width: '100%',
         });
 
