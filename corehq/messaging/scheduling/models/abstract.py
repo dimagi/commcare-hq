d6e0b9d0f43021cb72bcbce4d6a96947fbdbaa0e
Rynhard <rpietersen@dimagi.com>
2021-04-16 15:14:06 +0200

    added paused to Schedule class and migrated

--- corehq/messaging/scheduling/models/abstract.py
+++ corehq/messaging/scheduling/models/abstract.py
@@ -49,6 +49,7 @@ class Schedule(models.Model):
     domain = models.CharField(max_length=126, db_index=True)
     active = models.BooleanField(default=True)
     deleted = models.BooleanField(default=False)
+    paused = models.BooleanField(default=False)
 
     # Only matters when the recipient of a ScheduleInstance is a Location
     # If False, only include users at that location as recipients
