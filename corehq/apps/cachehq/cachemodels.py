93f198ef89c4f028714c907a5ee502a08296110a
Jennifer Schweers <orange.jenny@gmail.com>
2021-04-16 21:19:57 -0400

    Removed unused users_extra views

--- corehq/apps/cachehq/cachemodels.py
+++ corehq/apps/cachehq/cachemodels.py
@@ -17,9 +17,6 @@ class UserGenerationCache(GenerationCache):
     generation_key = '#gen#couch_user#'
     doc_types = ['CommCareUser', 'CouchUser', 'WebUser']
     views = [
-        "users_extra/phones_to_domains",
-        "users_extra/users_over_time",
-        "users_extra/emails",
         "users/by_domain",
         "users/phone_users_by_domain",
         "users/web_users_by_domain",
@@ -27,7 +24,6 @@ class UserGenerationCache(GenerationCache):
         "users/admins_by_domain",
         "users/by_username",
         "domain/old_users",
-        "users_extra/phones_to_domains",
     ]
 
 
