b0548ae692e7e21153cae8bb7013e81426b985d3
Jennifer Schweers <orange.jenny@gmail.com>
2021-04-19 14:17:47 -0400

    Incremented migrations

--- corehq/apps/commtrack/migrations/0007_rename_sql_models.py
+++ corehq/apps/commtrack/migrations/0007_rename_sql_models.py
@@ -8,7 +8,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('commtrack', '0005_remove_sqlcommtrackconfig_couch_id'),
+        ('commtrack', '0006_remove_sqlcommtrackconfig_couch_id'),
     ]
 
     operations = [
