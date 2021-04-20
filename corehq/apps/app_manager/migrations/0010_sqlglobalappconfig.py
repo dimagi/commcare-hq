663941ca9448560733937c197a73f47440ca8702
Jennifer Schweers <orange.jenny@gmail.com>
2021-04-20 10:57:02 -0400

    Removed GlobalAppConfig command

--- corehq/apps/app_manager/migrations/0010_sqlglobalappconfig.py
+++ corehq/apps/app_manager/migrations/0010_sqlglobalappconfig.py
@@ -3,15 +3,6 @@
 
 from django.db import migrations
 
-from corehq.apps.app_manager.management.commands.populate_sql_global_app_config import Command
-
-
-def _migrate_from_migration(apps, schema_editor):
-    sql_class = Command.sql_class()
-    sql_class.objects.model._meta.db_table = "app_manager_sqlglobalappconfig"
-    Command.migrate_from_migration(apps, schema_editor)
-    sql_class.objects.model._meta.db_table = "app_manager_globalappconfig"
-
 
 class Migration(migrations.Migration):
 
@@ -19,8 +10,4 @@ class Migration(migrations.Migration):
         ('app_manager', '0009_add_sqlglobalappconfig'),
     ]
 
-    operations = [
-        migrations.RunPython(_migrate_from_migration,
-                             reverse_code=migrations.RunPython.noop,
-                             elidable=True),
-    ]
+    operations = []
