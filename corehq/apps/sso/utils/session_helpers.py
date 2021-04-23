1cf60dcd25dbe39689020e182cb40057741b1249
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-04-23 16:16:53 -0400

    add way of storing and fetching invitation information during an sso sign in / sign up request

--- corehq/apps/sso/utils/session_helpers.py
+++ corehq/apps/sso/utils/session_helpers.py
@@ -1,3 +1,10 @@
+import logging
+
+from corehq.apps.users.models import Invitation
+
+log = logging.getLogger(__name__)
+
+
 def store_saml_data_in_session(request):
     """
     This stores SAML-related authentication data in the request's session
@@ -33,3 +40,27 @@ def get_sso_user_last_name_from_session(request):
     )
 
 
+def prepare_session_for_sso_invitation(request, invitation):
+    """
+    This prepares the Request's Session to store invitation details for use
+    when logging in as a new or existing user through SSO.
+    :param request: HttpRequest
+    :param invitation: Invitation
+    """
+    request.session['ssoInvitation'] = invitation.uuid
+
+
+def get_sso_invitation_from_session(request):
+    """
+    Fetches an Invitation object from the ssoInvitation information
+    (if available) stored in the request session.
+    :param request: HttpRequest
+    :return: Invitation or None
+    """
+    uuid = request.session.get('ssoInvitation')
+    try:
+        return Invitation.objects.get(uuid=uuid) if uuid else None
+    except Invitation.DoesNotExist:
+        log.exception(
+            f"Error fetching invitation from sso create user request: {uuid}"
+        )
