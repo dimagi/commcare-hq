3d5621e5196d5cbbc70eff5f0d7223c17f81f803
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-04-23 16:15:13 -0400

    add a way of quickly fetching first and last name user data from the SamlUserdata

--- corehq/apps/sso/utils/session_helpers.py
+++ corehq/apps/sso/utils/session_helpers.py
@@ -9,3 +9,27 @@ def store_saml_data_in_session(request):
     request.session['samlNameIdNameQualifier'] = request.saml2_auth.get_nameid_nq()
     request.session['samlNameIdSPNameQualifier'] = request.saml2_auth.get_nameid_spnq()
     request.session['samlSessionIndex'] = request.saml2_auth.get_session_index()
+
+
+def get_sso_user_first_name_from_session(request):
+    """
+    This gets the first name from sso user data stored in the session SAML data.
+    :param request: HttpRequest
+    :return: string or None
+    """
+    return request.session.get('samlUserdata', {}).get(
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname'
+    )
+
+
+def get_sso_user_last_name_from_session(request):
+    """
+    This gets the last name from sso user data stored in the session SAML data.
+    :param request: HttpRequest
+    :return: string or None
+    """
+    return request.session.get('samlUserdata', {}).get(
+        'http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname'
+    )
+
+
