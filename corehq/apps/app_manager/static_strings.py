b5bddeb9f45113459651f9208bc067290d2fbc3b
ShivamPokhriyal <shivampokhriyal1997@gmail.com>
2021-04-27 10:04:02 +0530

    Specify asterisk setting is only for mobile

--- corehq/apps/app_manager/static_strings.py
+++ corehq/apps/app_manager/static_strings.py
@@ -254,5 +254,5 @@ STATICALLY_ANALYZABLE_TRANSLATIONS = [
     ugettext_noop('Enable Text To Speech'),
     ugettext_noop('Adds a text to speech button for all questions to read out the question text aloud.'),
     ugettext_noop('Enable asterisk on required question'),
-    ugettext_noop('Adds a red asterisk to denote mandatory questions in a form.'),
+    ugettext_noop('Adds a red asterisk to denote mandatory questions in a form. This setting only works in mobile.'),
 ]
