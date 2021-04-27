6a123056d46ba0920cfd67ad342df3bd711a0e0a
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-04-27 12:35:50 -0400

    create new user from sso username information if no user exists matching the given username in the SsoBackend

--- corehq/apps/sso/backends.py
+++ corehq/apps/sso/backends.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
 
+from dimagi.utils.web import get_ip
+
+from corehq.apps.registration.utils import activate_new_user
 from corehq.apps.sso.models import IdentityProvider, AuthenticatedEmailDomain
+from corehq.apps.sso.utils.session_helpers import (
+    get_sso_invitation_from_session,
+    get_sso_user_first_name_from_session,
+    get_sso_user_last_name_from_session,
+)
 from corehq.apps.sso.utils.user_helpers import get_email_domain_from_username
+from corehq.apps.users.models import CouchUser
+from corehq.const import (
+    USER_CHANGE_VIA_SSO_INVITE,
+    USER_CHANGE_VIA_SSO_NEW_USER,
+)
 
 
 class SsoBackend(ModelBackend):
@@ -42,13 +55,38 @@ class SsoBackend(ModelBackend):
             )
             return None
 
+        invitation = get_sso_invitation_from_session(request)
+
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            # todo handle user creation based on information from request/session
-            #  do this prior to handling the invite scenario and new user scenario
-            request.sso_login_error = f"User {username} does not exist."
-            return None
+            user = self._create_new_user(request, username, invitation)
 
         request.sso_login_error = None
         return user
+
+    def _create_new_user(self, request, username, invitation):
+        """
+        This creates a new user in HQ based on information in the request.
+        :param request: HttpRequest
+        :param username: String (username)
+        :param invitation: Invitation
+        :return: User
+        """
+        created_via = (USER_CHANGE_VIA_SSO_INVITE if invitation
+                       else USER_CHANGE_VIA_SSO_NEW_USER)
+        created_by = (CouchUser.get_by_user_id(invitation.invited_by) if invitation
+                      else None)
+        domain = invitation.domain if invitation else None
+
+        new_web_user = activate_new_user(
+            username=username,
+            password=User.objects.make_random_password(),
+            created_by=created_by,
+            created_via=created_via,
+            first_name=get_sso_user_first_name_from_session(request),
+            last_name=get_sso_user_last_name_from_session(request),
+            domain=domain,
+            ip=get_ip(request),
+        )
+        return new_web_user
