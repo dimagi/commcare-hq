7bbdae2c102dded340e46ec35f9f3385e18c3f2a
Jennifer Schweers <orange.jenny@gmail.com>
2021-04-28 22:05:19 -0400

    Added tests

--- corehq/apps/auditcare/tasks.py
+++ corehq/apps/auditcare/tasks.py
@@ -8,11 +8,23 @@ from couchdbkit.ext.django.loading import get_db
 from dimagi.utils.dates import force_to_datetime
 from dimagi.utils.logging import notify_exception
 
-from corehq.apps.auditcare.models import AccessAudit, NavigationEventAudit
+from corehq.apps.auditcare.models import (
+    ACCESS_LOGIN,
+    ACCESS_LOGOUT,
+    ACCESS_FAILED,
+    AccessAudit,
+    NavigationEventAudit,
+)
 from corehq.apps.auditcare.utils.export import get_sql_start_date
 from corehq.util.soft_assert import soft_assert
 
 log = logging.getLogger(__name__)
+
+ACCESS_LOOKUP = {
+    "login": ACCESS_LOGIN,
+    "logout": ACCESS_LOGOUT,
+    "login_failed": ACCESS_FAILED,
+}
 
 
 @periodic_task(run_every=crontab(minute="30"), queue='background_queue')
@@ -21,7 +33,15 @@ def copy_events_to_sql(limit=1000):
     start_date = get_sql_start_date()
     log.info(f"Initial start date: {start_date}")
 
-    startkey = [start_date.year, start_date.month, start_date.day]
+    startkey = [
+        start_date.year,
+        start_date.month,
+        start_date.day,
+        start_date.hour,
+        start_date.minute,
+        start_date.second,
+        start_date.microsecond,
+    ]
     for doc in [r["doc"] for r in db.view(
         "auditcare/all_events",
         startkey=startkey,
@@ -31,18 +51,24 @@ def copy_events_to_sql(limit=1000):
         limit=limit,
     )]:
         try:
-            kwargs = _pick(doc, ["user", "domain", "path", "ip_address", "session_key",
+            kwargs = _pick(doc, ["user", "domain", "ip_address", "session_key",
                                  "headers", "status_code", "user_agent"])
             kwargs.update({
                 "event_date": force_to_datetime(doc.get("event_date")),
-                "path": doc.get("request_path"),
             })
             if doc["doc_type"] == "NavigationEventAudit":
                 kwargs.update(_pick(doc, ["params", "headers", "status_code", "view", "view_kwargs"]))
+                kwargs.update({
+                    "path": doc.get("request_path"),
+                })
                 NavigationEventAudit(**kwargs).save()
             elif doc["doc_type"] == "AccessAudit":
-                kwargs.update(_pick(doc, ["http_accept", "access_type", "trace_id"]))
-                AccessAudit().save()
+                kwargs.update(_pick(doc, ["http_accept", "trace_id"]))
+                kwargs.update({
+                    "access_type": ACCESS_LOOKUP.get(doc.get("access_type")),
+                    "path": doc.get("path_info"),
+                })
+                AccessAudit(**kwargs).save()
         except Exception:
             message = f"Error in copy_events_to_sql on doc {doc['_id']}"
             notify_exception(None, message=message)
---
+++ corehq/apps/auditcare/tests/test_tasks.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+
+from ..models import AccessAudit, NavigationEventAudit
+from ..tasks import copy_events_to_sql
+from .testutils import AuditcareTest, delete_couch_docs, save_couch_doc
+
+
+class TestCopyEventsToSQL(AuditcareTest):
+    username = "audit@care.com"
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        username = "couch@test.com"
+        cls.couch_ids = [
+            save_couch_doc(
+                "NavigationEventAudit",
+                username,
+                event_date=datetime(2021, 2, 1, 2).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                description='User Name',
+                extra={},
+                headers={
+                    'REQUEST_METHOD': 'GET',
+                },
+                ip_address='10.1.2.3',
+                request_path='/a/delmar/phone/restore/?version=2.0&since=...',
+                session_key='abc123',
+                status_code=200,
+                user_agent='okhttp/4.4.1',
+                view_kwargs={'domain': 'delmar'},
+                view='corehq.apps.ota.views.restore',
+            ),
+            save_couch_doc(
+                "AccessAudit",
+                username,
+                event_date=datetime(2021, 2, 1, 3).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                access_type='login',
+                description='Login Success',
+                failures_since_start=None,
+                get_data=[],
+                http_accept='text/html',
+                ip_address='10.1.3.2',
+                path_info='/a/delmar/login/',
+                post_data=[],
+                session_key='abc123',
+                user_agent='Mozilla/5.0',
+            ),
+            save_couch_doc(
+                "NavigationEventAudit",
+                username,
+                event_date=datetime(2021, 2, 1, 5).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                description='User Name',
+                extra={},
+                headers={
+                    'REQUEST_METHOD': 'GET',
+                },
+                ip_address='10.1.2.3',
+                request_path='/a/sandwich/phone/restore/?version=2.0&since=...',
+                session_key='abc123',
+                status_code=200,
+                user_agent='okhttp/4.4.1',
+                view_kwargs={'domain': 'sandwich'},
+                view='corehq.apps.ota.views.restore',
+            ),
+        ]
+
+    @classmethod
+    def tearDownClass(cls):
+        delete_couch_docs(cls.couch_ids)
+        super().tearDownClass()
+
+    def tearDown(self):
+        AccessAudit.objects.all().delete()
+        NavigationEventAudit.objects.all().delete()
+        super().tearDown()
+
+    def test_copy(self):
+        NavigationEventAudit(event_date=datetime(2021, 2, 1, 4), path="just/a/checkpoint").save()
+        copy_events_to_sql()
+
+        self.assertEqual(NavigationEventAudit.objects.count(), 2)
+        self.assertEqual(
+            [e.path for e in NavigationEventAudit.objects.order_by("-event_date").all()],
+            ["just/a/checkpoint", "/a/delmar/phone/restore/?version=2.0&since=..."]
+        )
+
+        self.assertEqual(AccessAudit.objects.count(), 1)
+        self.assertEqual(AccessAudit.objects.first().path, "/a/delmar/login/")
+
+    def test_limit(self):
+        NavigationEventAudit(event_date=datetime(2021, 2, 1, 4), path="just/a/checkpoint").save()
+        copy_events_to_sql(limit=1)
+        self.assertEqual(NavigationEventAudit.objects.count(), 1)
+        self.assertEqual(NavigationEventAudit.objects.first().path, "just/a/checkpoint")
+
+        self.assertEqual(AccessAudit.objects.count(), 1)
+        self.assertEqual(AccessAudit.objects.first().path, "/a/delmar/login/")
