556b0f7f5abf3e8f9798df5f84af658424e58f93
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-04-29 16:29:50 -0400

    get new user data from an AsyncSignupRequest rather than the session

--- corehq/apps/sso/backends.py
+++ corehq/apps/sso/backends.py
@@ -17,7 +17,6 @@ from corehq.apps.sso.models import IdentityProvider, AuthenticatedEmailDomain
 from corehq.apps.sso.utils.session_helpers import (
     get_sso_user_first_name_from_session,
     get_sso_user_last_name_from_session,
-    get_new_sso_user_data_from_session,
 )
 from corehq.apps.sso.utils.user_helpers import get_email_domain_from_username
 from corehq.apps.users.models import CouchUser, WebUser
@@ -114,24 +113,33 @@ class SsoBackend(ModelBackend):
         request.sso_new_user_messages['success'].append(
             _("User account for {} created.").format(new_web_user.username)
         )
-        self._process_new_user_data(request, new_web_user)
+        self._process_new_user_data(request, new_web_user, async_signup)
         return User.objects.get(username=username), new_web_user
 
     @staticmethod
-    def _process_new_user_data(request, new_web_user):
+    def _process_new_user_data(request, new_web_user, async_signup):
         """
         If available, this makes sure we apply any relevant user data
         :param request: HttpRequest
         :param new_web_user: WebUser
-        :return:
+        :param async_signup: AsyncSignupRequest
         """
-        user_data = get_new_sso_user_data_from_session(request)
-        if not user_data:
+        if not async_signup:
+            if settings.IS_SAAS_ENVIRONMENT:
+                track_workflow(
+                    new_web_user.username,
+                    "Requested New Account via SSO (Bypassed Signup Form)",
+                    {
+                        'environment': settings.SERVER_ENVIRONMENT,
+                    }
+                )
             return
 
-        phone_number = user_data['phone_number']
-        if phone_number:
-            new_web_user.phone_numbers.append(phone_number)
+        if async_signup.invitation:
+            return
+
+        if async_signup.phone_number:
+            new_web_user.phone_numbers.append(async_signup.phone_number)
             new_web_user.save()
 
         if settings.IS_SAAS_ENVIRONMENT:
@@ -142,19 +150,25 @@ class SsoBackend(ModelBackend):
                     'environment': settings.SERVER_ENVIRONMENT,
                 }
             )
-            track_workflow(
-                new_web_user.username,
-                "Persona Field Filled Out via SSO",
-                {
-                    'personachoice': user_data['persona'],
-                    'personaother': user_data['persona_other'],
-                }
-            )
-            track_web_user_registration_hubspot(
-                request,
-                new_web_user,
-                user_data['additional_hubspot_data'],
-            )
+            if async_signup.persona:
+                track_workflow(
+                    new_web_user.username,
+                    "Persona Field Filled Out via SSO",
+                    {
+                        'personachoice': async_signup.persona,
+                        'personaother': async_signup.persona_other,
+                    }
+                )
+                track_web_user_registration_hubspot(
+                    request,
+                    new_web_user,
+                    async_signup.additional_hubspot_data,
+                )
+            else:
+                track_workflow(
+                    new_web_user.username,
+                    "New User created through SSO, but Persona info missing"
+                )
 
     @staticmethod
     def _process_invitation(request, invitation, web_user, is_new_user=False):
