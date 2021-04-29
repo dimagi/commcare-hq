862c42cd53bbb7057e0477af7712b3f7210e612c
Jennifer Schweers <orange.jenny@gmail.com>
2021-04-28 21:23:06 -0400

    Added object properties

--- corehq/apps/auditcare/tasks.py
+++ corehq/apps/auditcare/tasks.py
@@ -1,3 +1,4 @@
+from dimagi.utils.dates import force_to_datetime
 from celery.schedules import crontab
 from celery.task import periodic_task
 from couchdbkit.ext.django.loading import get_db
@@ -18,13 +19,29 @@ def copy_events_to_sql():
         descending=True,
         limit=1000,
     )]:
-        if doc["doc_type"] == "NavigationEventAudit":
-            # TODO: create NavigationEventAudit in SQL
-            NavigationEventAudit().save()
-        elif doc["doc_type"] == "AccessAudit":
-            # TODO: create AccessAudit in SQL
-            AccessAudit().save()
+        try:
+            kwargs = _pick(doc, ["user", "domain", "path", "ip_address", "session_key",
+                                 "headers", "status_code", "user_agent"])
+            kwargs.update({
+                "event_date": force_to_datetime(doc.get("event_date")),
+                "path": doc.get("request_path"),
+            })
+            if doc["doc_type"] == "NavigationEventAudit":
+                kwargs.update(_pick(doc, ["params", "headers", "status_code", "view", "view_kwargs"]))
+                NavigationEventAudit(**kwargs).save()
+            elif doc["doc_type"] == "AccessAudit":
+                kwargs.update(_pick(doc, ["http_accept", "access_type", "trace_id"]))
+                AccessAudit().save()
+        except Exception:
+            message = f"Error in copy_events_to_sql on doc {doc['_id']}"
+            notify_exception(None, message=message)
+            _soft_assert = soft_assert(to="{}@{}.com".format('jschweers', 'dimagi'), notify_admins=False)
+            _soft_assert(False, message)
+            sys.exit(1)
+
+    final_start_date = get_sql_start_date()
+    log.info(f"Final start date: {final_start_date}")
 
 
-def _date_key(date):
-    return [date.year, date.month, date.day]
+def _pick(doc, keys):
+    return {key: doc.get(key) for key in keys if doc.get(key)}
