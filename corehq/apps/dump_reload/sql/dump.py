41002f8c4592bc56270e2bd1e2b7d5959845fc62
Amit Phulera <amitphulera999@gmail.com>
2021-05-10 14:31:39 +0530

    fix typo

--- corehq/apps/dump_reload/sql/dump.py
+++ corehq/apps/dump_reload/sql/dump.py
@@ -111,7 +111,7 @@ APP_LABELS_WITH_FILTER_KWARGS_TO_DUMP = defaultdict(list)
     FilteredModelIteratorBuilder('ota.DemoUserRestore', UserIDFilter('demo_user_id', include_web_users=False)),
     FilteredModelIteratorBuilder('domain_migration_flags.DomainMigrationProgress', SimpleFilter('domain')),
     FilteredModelIteratorBuilder('products.SQLProduct', SimpleFilter('domain')),
-    FilteredModelIteratorBuilder('saved_reports.SQLReportConfigß', SimpleFilter('domain')),
+    FilteredModelIteratorBuilder('saved_reports.SQLReportConfig', SimpleFilter('domain')),
     FilteredModelIteratorBuilder('smsforms.SQLXFormsSession', SimpleFilter('domain')),
     FilteredModelIteratorBuilder('sms.MessagingEvent', SimpleFilter('domain')),
     FilteredModelIteratorBuilder('sms.MessagingSubEvent', SimpleFilter('parent__domain')),
