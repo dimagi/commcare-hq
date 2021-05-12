8f3b4fd0fb42249e82d960fe28bd67ce894062d3
Charl Smit <csmit@dimagi.com>
2021-05-12 10:13:19 +0200

    Add exponential backoff to celery periodic task

--- custom/onse/const.py
+++ custom/onse/const.py
@@ -3,3 +3,6 @@ CASE_TYPE = 'facility_supervision'
 CONNECTION_SETTINGS_NAME = 'DHIS2 Facilities Import Server'
 MAPPINGS_FILE = 'custom/onse/case_property_map.csv'
 LAST_IMPORTED_PROPERTY = 'dhis2_last_imported'
+PING_SUCCESS_CODE = 0
+EXPONENTIAL_BACKOFF_FACTOR = 86400
+RETRY_BACKOFF_MAX_SECONDS = 1382400  # 16 days
