e509b7f7f33bd08a77b039d3da68bd838fdf4db9
Charl Smit <csmit@dimagi.com>
2021-05-18 15:33:37 +0200

    Add functional tests

--- custom/onse/tests.py
+++ custom/onse/tests.py
@@ -1,9 +1,13 @@
 import doctest
-from datetime import date
+from mock import patch
 
+from django.test import TestCase
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
 from nose.tools import assert_equal
-
 from custom.onse import tasks
+from custom.onse.tasks import _update_facility_cases_from_dhis2_data_elements, schedule_execution
+from corehq.motech.models import ConnectionSettings
 
 
 def test_previous_quarter():
@@ -24,3 +28,28 @@ def test_previous_quarter():
 def test_doctests():
     results = doctest.testmod(tasks)
     assert results.failed == 0
+
+
+class TestUpdateFromDhis2Task(TestCase):
+
+    @patch('custom.onse.tasks.domain_exists', return_value=True)
+    @patch('custom.onse.tasks.get_dhis2_server', return_value=ConnectionSettings())
+    @patch('custom.onse.tasks._server_is_online', return_value=False)
+    @patch('custom.onse.tasks.schedule_execution')
+    def test_retry(self, *args):
+        _update_facility_cases_from_dhis2_data_elements(None, False)
+
+        schedule_execution_function = args[0]
+        list_of_schedule_execution_function_calls = schedule_execution_function.call_args_list
+        first_call_with_args = list_of_schedule_execution_function_calls[0]
+        call_args = first_call_with_args[0]
+
+        callback_function = call_args[0]
+        callback_function_args = call_args[1]
+        callback_function_execution_date = call_args[2]
+
+        expected_execution_date = datetime.utcnow() + relativedelta(days=1)
+
+        assert callback_function == _update_facility_cases_from_dhis2_data_elements
+        assert callback_function_args == [None, False, 1]
+        assert callback_function_execution_date.date() == expected_execution_date.date()
