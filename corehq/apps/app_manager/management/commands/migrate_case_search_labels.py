eef1722df32c0490a57bfb926b4c3c706e5ef7c0
mkangia <mkangia@dimagi.com>
2021-05-21 15:13:10 +0530

    Reduce chunk size

--- corehq/apps/app_manager/management/commands/migrate_case_search_labels.py
+++ corehq/apps/app_manager/management/commands/migrate_case_search_labels.py
@@ -8,7 +8,7 @@ class Command(AppMigrationCommandBase):
     help = "One-time migration to replace CaseSearch command_label and again_label with " \
            "search_label and search_again_label"
 
-    chunk_size = 20
+    chunk_size = 10
     include_builds = True
     include_linked_apps = True
 
