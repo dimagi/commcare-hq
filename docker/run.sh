81e6979de35823efa99c92cf7019dfccd000c94c
Joel Miller <jmiller@dimagi.com>
2021-05-24 14:17:03 -0400

    PR Review: move bash utils to separate file

--- docker/run.sh
+++ docker/run.sh
@@ -17,78 +17,6 @@ fi
 DOCKER_OVERLAY_TEST_DEBUG='no'
 
 
-function logmsg {
-    local echo_args=( -e )
-    local script=$(basename "$0")
-    local levelname="$1"
-    shift
-    if [ "x${1}" == "x-n" ]; then
-        shift
-        echo_args+=( -n )
-    fi
-    local msg="$*"
-    local ccode=''
-    local reset=''
-    if [ -t 2 ]; then
-        # only define color codes when STDERR is a tty
-        local color='0' # default no color
-        case "$levelname" in
-            DEBUG)
-                color='1;35' # magenta
-                ;;
-            INFO)
-                color='1;32' # green
-                ;;
-            WARNING)
-                color='0;33' # gold
-                ;;
-            ERROR)
-                color='1;31' # red
-                ;;
-        esac
-        ccode="\\033[${color}m"
-        reset='\033[0m'
-    fi
-    echo "${echo_args[@]}" "[${script}] ${ccode}${levelname}${reset}: $msg" >&2
-}
-
-function func_text {
-    local func_name
-    for func_name in "$@"; do
-        if ! type "$func_name" 2>&1 | grep -E " is a function$" >/dev/null; then
-            logmsg ERROR "$func_name is not a function"
-            return 1
-        fi
-        type "$func_name" | tail -n +2
-        echo ''
-    done
-}
-
-function truthy {
-    function _usage {
-        logmsg ERROR "USAGE: truthy [false|true|no|yes|off|on|0|1]"
-        logmsg ERROR "$*"
-    }
-    if [ $# -gt 1 ]; then
-        _usage "too many arguments: $*"
-        exit 1
-    fi
-    local value="$1"
-    if [ -n "$value" ]; then
-        if echo "$value" | grep -Ei '^(t(rue)?|y(es)?|on|1)$' >/dev/null; then
-            return 0
-        elif echo "$value" | grep -Ei '^(f(alse)?|n(o)?|off|0)$' >/dev/null; then
-            return 1
-        else
-            _usage "invalid boolean string: '$value'"
-            exit 1
-        fi
-    else
-        # absent == false
-        return 1
-    fi
-}
-
 function setup {
     [ -n "$1" ] && TEST="$1"
     logmsg INFO "performing setup..."
@@ -248,6 +176,7 @@ function runserver {
 }
 
 source /mnt/commcare-hq-ro/scripts/datadog-utils.sh  # provides send_metric_to_datadog
+source /mnt/commcare-hq-ro/scripts/bash-utils.sh  # provides logmsg, func_text and truthy
 
 # build the run_tests script to be executed as cchq later
 func_text logmsg _run_tests  > /mnt/run_tests
