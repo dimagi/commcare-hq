556b697ed5d4a43e141f4a24e52c3b8728c3c973
Simon Kelly <skelly@dimagi.com>
2021-05-26 17:15:08 +0200

    Revert "Revert "static admin and default roles""

--- corehq/apps/users/views/__init__.py
+++ corehq/apps/users/views/__init__.py
@@ -100,6 +100,7 @@ from corehq.apps.users.models import (
     DomainRemovalRecord,
     DomainRequest,
     Invitation,
+    StaticRole,
     UserRole,
     WebUser,
 )
@@ -499,7 +500,7 @@ class BaseRoleAccessView(BaseUserSettingsView):
     @property
     @memoized
     def user_roles(self):
-        user_roles = [UserRole.admin_role(self.domain)]
+        user_roles = [StaticRole.domain_admin(self.domain)]
         user_roles.extend(sorted(
             UserRole.by_domain(self.domain),
             key=lambda role: role.name if role.name else '\uFFFF'
@@ -657,7 +658,7 @@ class ListRolesView(BaseRoleAccessView):
             'user_roles': self.user_roles,
             'non_admin_roles': self.user_roles[1:],
             'can_edit_roles': self.can_edit_roles,
-            'default_role': UserRole.get_default(),
+            'default_role': StaticRole.domain_default(self.domain),
             'report_list': get_possible_reports(self.domain),
             'is_domain_admin': self.couch_user.is_domain_admin,
             'domain_object': self.domain_object,
