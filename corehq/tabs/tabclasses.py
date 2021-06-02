f1c06f61db76e24e1a8846992564dd800a25838d
Rynhard <rpietersen@dimagi.com>
2021-06-02 10:58:57 +0200

    added a new smsbillable report for enterprise users

--- corehq/tabs/tabclasses.py
+++ corehq/tabs/tabclasses.py
@@ -1,3 +1,4 @@
+from corehq.apps.enterprise.dispatcher import EnterpriseInterfaceDispatcher
 from django.conf import settings
 from django.http import Http404
 from django.urls import reverse
@@ -1561,8 +1562,9 @@ class EnterpriseSettingsTab(UITab):
         enterprise_views.append({
             'title': _('Billing Statements'),
             'url': reverse('enterprise_billing_statements',
-                           args=[self.domain])
+                        args=[self.domain])
         })
+
         if toggles.ENTERPRISE_SSO.enabled_for_request(self._request):
             if IdentityProvider.domain_has_editable_identity_provider(self.domain):
                 from corehq.apps.sso.views.enterprise_admin import (
@@ -1580,6 +1582,9 @@ class EnterpriseSettingsTab(UITab):
                     ],
                 })
         items.append((_('Manage Enterprise'), enterprise_views))
+
+        items.extend(EnterpriseInterfaceDispatcher.navigation_sections(
+            request=self._request, domain=self.domain))
         return items
 
 
