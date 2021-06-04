4dfaeb6c55981714cac68a99e35e1f4cc3fddec8
Jennifer Schweers <orange.jenny@gmail.com>
2021-06-03 21:42:14 -0400

    Updated tests

--- corehq/apps/case_importer/tests/test_importer.py
+++ corehq/apps/case_importer/tests/test_importer.py
@@ -20,11 +20,12 @@ from corehq.apps.case_importer.util import ImporterConfig, WorksheetWrapper, \
     get_interned_exception
 from corehq.apps.commtrack.tests.util import make_loc
 from corehq.apps.domain.shortcuts import create_domain
+from corehq.apps.enterprise.tests.utils import create_enterprise_permissions
 from corehq.apps.groups.models import Group
 from corehq.apps.hqcase.dbaccessors import get_case_ids_in_domain
 from corehq.apps.locations.models import LocationType
 from corehq.apps.locations.tests.util import restrict_user_by_location
-from corehq.apps.users.models import CommCareUser, WebUser, DomainPermissionsMirror
+from corehq.apps.users.models import CommCareUser, WebUser
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.form_processor.tests.utils import run_with_all_backends
 from corehq.util.test_utils import flag_enabled, flag_disabled
@@ -430,17 +431,14 @@ class ImporterTest(TestCase):
     @override_settings(TESTS_SHOULD_USE_SQL_BACKEND=True)
     @flag_enabled('DOMAIN_PERMISSIONS_MIRROR')
     def test_multiple_domain_case_import(self):
-        mirror_domain1 = DomainPermissionsMirror(source=self.domain, mirror='mirrordomain1')
-        mirror_domain2 = DomainPermissionsMirror(source=self.domain, mirror='mirrordomain2')
-        mirror_domain1.save()
-        mirror_domain2.save()
+        create_enterprise_permissions(self.couch_user.username, self.domain, ["mirrordomain1", "mirrordomain2"])
         headers_with_domain = ['case_id', 'name', 'artist', 'domain']
         config_1 = self._config(headers_with_domain, create_new_cases=True, search_column='case_id')
         case_with_domain_file = make_worksheet_wrapper(
             ['case_id', 'name', 'artist', 'domain'],
             ['', 'name-0', 'artist-0', self.domain],
-            ['', 'name-1', 'artist-1', mirror_domain1.mirror],
-            ['', 'name-2', 'artist-2', mirror_domain2.mirror],
+            ['', 'name-1', 'artist-1', "mirrordomain1"],
+            ['', 'name-2', 'artist-2', "mirrordomain2"],
             ['', 'name-3', 'artist-3', self.domain],
             ['', 'name-4', 'artist-4', self.domain],
             ['', 'name-5', 'artist-5', 'not-existing-domain']
@@ -459,7 +457,7 @@ class ImporterTest(TestCase):
         self.assertEqual(cases['name-0'].get_case_property('artist'), 'artist-0')
 
         # Asserting mirror domain 1
-        md1_case_ids = CaseAccessors(mirror_domain1.mirror).get_case_ids_in_domain()
+        md1_case_ids = CaseAccessors("mirrordomain1").get_case_ids_in_domain()
         md1_cases = list(self.accessor.get_cases(md1_case_ids))
         self.assertEqual(1, len(md1_cases))
         # Asserting mirror domain 1 case property
@@ -467,7 +465,7 @@ class ImporterTest(TestCase):
         self.assertEqual(md1_cases_pro['name-1'].get_case_property('artist'), 'artist-1')
 
         # Asserting mirror domain 2
-        md2_case_ids = CaseAccessors(mirror_domain2.mirror).get_case_ids_in_domain()
+        md2_case_ids = CaseAccessors("mirrordomain2").get_case_ids_in_domain()
         md2_cases = list(self.accessor.get_cases(md2_case_ids))
         self.assertEqual(1, len(md2_cases))
         # Asserting mirror domain 2 case propperty
