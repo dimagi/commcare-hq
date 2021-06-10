6dc23679e04dbdf03ce205a7972a3881483e9f37
Jenny Schweers <orange.jenny@gmail.com>
2021-06-10 17:37:27 -0400

    Update corehq/apps/accounting/models.py
    Co-authored-by: Simon Kelly <skelly@dimagi.com>

--- corehq/apps/accounting/models.py
+++ corehq/apps/accounting/models.py
@@ -515,8 +515,7 @@ class BillingAccount(ValidateModelMixin, models.Model):
         return StripePaymentMethod.objects.get(web_user=self.auto_pay_user).get_autopay_card(self)
 
     def get_domains(self):
-        subscriptions = Subscription.visible_objects.filter(account_id=self.id, is_active=True)
-        return [s.subscriber.domain for s in subscriptions]
+        return list(Subscription.visible_objects.filter(account_id=self.id, is_active=True).values_list('subscriber__domain', flat=True))
 
     def has_enterprise_admin(self, email):
         return self.is_customer_billing_account and email in self.enterprise_admin_emails
