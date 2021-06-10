fd4fe8de74b7124a9d9b236dc6824b6c42090bf4
mkangia <mkangia@dimagi.com>
2021-06-10 15:31:12 +0530

    pass domain in tests calling unretire

--- corehq/apps/couch_sql_migration/tests/test_migration.py
+++ corehq/apps/couch_sql_migration/tests/test_migration.py
@@ -1137,19 +1137,19 @@ class MigrationTestCase(BaseMigrationTestCase):
         with self.assertRaises(NotAllowed):
             user.retire(self.domain_name, deleted_by=None)
         with self.assertRaises(NotAllowed):
-            user.unretire(unretired_by=None)
+            user.unretire(self.domain_name, unretired_by=None)
 
         clear_local_domain_sql_backend_override(self.domain_name)
         self.assert_backend("couch")
         with self.assertRaises(NotAllowed):
             user.retire(self.domain_name, deleted_by=None)
         with self.assertRaises(NotAllowed):
-            user.unretire(unretired_by=None)
+            user.unretire(self.domain_name, unretired_by=None)
 
         self.do_migration(finish=True)
         self.do_migration(COMMIT)
         user.retire(self.domain_name, deleted_by=None)
-        user.unretire(unretired_by=None)
+        user.unretire(self.domain_name, unretired_by=None)
 
     def test_delete_cases_during_migration(self):
         from corehq.apps.hqcase.tasks import delete_exploded_cases
