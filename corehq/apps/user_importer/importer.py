d6307838ffdaf5208eeb0ec4a2d66d5e49e0662f
Steph <stephherbers@gmail.com>
2021-06-10 11:07:14 -0400

    raise UserUploadError when domain_obj results in None so it's a row error instead of erroring the entire import

--- corehq/apps/user_importer/importer.py
+++ corehq/apps/user_importer/importer.py
@@ -343,6 +343,9 @@ def get_domain_info(domain, upload_domain, user_specs, domain_info_by_domain, up
         location_cache = SiteCodeToLocationCache(domain)
 
     domain_obj = Domain.get_by_name(domain)
+    if domain_obj is None:
+        raise UserUploadError(_("Domain cannot be set to '{domain}'".format(domain=domain)))
+
     allowed_group_names = [group.name for group in domain_group_memoizer.groups]
     profiles_by_name = {}
     domain_user_specs = [spec for spec in user_specs if spec.get('domain', upload_domain) == domain]
@@ -418,9 +421,10 @@ def create_or_update_users_and_groups(upload_domain, user_specs, upload_user, gr
                 'row': row,
             }
 
-            domain_info = get_domain_info(domain, upload_domain, user_specs, domain_info_by_domain, group_memoizer)
-
             try:
+                domain_info = get_domain_info(domain, upload_domain, user_specs, domain_info_by_domain,
+                                              group_memoizer)
+
                 for validator in domain_info.validators:
                     validator(row)
             except UserUploadError as e:
@@ -631,9 +635,9 @@ def create_or_update_web_users(upload_domain, user_specs, upload_user, update_pr
             'username': username,
             'row': row,
         }
-        domain_info = get_domain_info(domain, upload_domain, user_specs, domain_info_by_domain,
-                                      upload_user=upload_user, is_web_upload=True)
         try:
+            domain_info = get_domain_info(domain, upload_domain, user_specs, domain_info_by_domain,
+                                          upload_user=upload_user, is_web_upload=True)
             for validator in domain_info.validators:
                 validator(row)
         except UserUploadError as e:
