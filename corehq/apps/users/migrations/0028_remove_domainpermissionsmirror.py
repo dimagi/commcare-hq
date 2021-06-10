3f554cd7d3e9fcec9b27a6cb6be0dbd2b94b65e4
Jennifer Schweers <orange.jenny@gmail.com>
2021-06-10 14:52:02 -0400

    Moved permissions into their own model

--- corehq/apps/users/migrations/0028_remove_domainpermissionsmirror.py
+++ corehq/apps/users/migrations/0028_remove_domainpermissionsmirror.py
@@ -1,6 +1,7 @@
 from django.db import migrations
 
 from corehq.apps.accounting.models import BillingAccount
+from corehq.apps.enterprise.models import EnterprisePermissions
 from corehq.util.django_migrations import skip_on_fresh_install
 
 
@@ -10,17 +11,20 @@ def migrate_mirrors(apps, schema_editor):
     sources = {o.source for o in DomainPermissionsMirror.objects.all()}
     for source in sources:
         account = BillingAccount.get_account_by_domain(source)
-        account.permissions_source_domain = source
-        all_domains = set(account.get_domains())
+        account_domains = set(account.get_domains())
         mirror_domains = {o.mirror for o in DomainPermissionsMirror.objects.filter(source=source)}
-        account.permissions_ignore_domains = list(all_domains - mirror_domains)
-        account.save()
+        EnterprisePermissions(
+            account=account,
+            is_enabled=True,
+            source_domain=source,
+            domains=list(account_domains - mirror_domains - {source}),
+        ).save()
 
 
 class Migration(migrations.Migration):
     dependencies = [
         ('users', '0027_role_permission_unique'),
-        ('accounting', '0055_enterprise_permissions'),
+        ('enterprise', '0001_initial'),
     ]
 
     operations = [
