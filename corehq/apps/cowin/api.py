144581dabecf6b1209662db7f830f02dcf9a2063
mkangia <mkangia@dimagi.com>
2021-06-17 16:15:15 +0530

    add vaccinator apis

--- corehq/apps/cowin/api.py
+++ corehq/apps/cowin/api.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 import requests
 
@@ -27,6 +28,66 @@ def send_request_for_meta_beneficiary_via_public_api():
     return requests.get(url, headers=headers)
 
 
+# vaccinator apis
+def send_request_to_register_beneficiary_via_vaccinator_api(beneficiary_data):
+    if not beneficiary_data:
+        beneficiary_data = {
+            "name": "Apparao",
+            "birth_year": "1980",
+            "gender_id": 1,
+            "mobile_number": "8383909618",
+            "photo_id_type": 1,
+            "photo_id_number": "XXXXXXXX9999",
+            "consent_version": "1"
+        }
+    headers = {
+        'Accept-Language': 'en_US',
+        'User-Agent': '',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    url = API['vaccinator'] + "vaccinator/beneficiaries/new"
+    return requests.post(url, headers=headers, data=json.dumps(beneficiary_data))
+
+
+def send_request_to_find_beneficiaries_by_mobile_number_via_vaccinator_api(mobile_number):
+    if not mobile_number:
+        mobile_number = "8383909618"
+
+    headers = {
+        'Accept-Language': 'en_US',
+        'User-Agent': '',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+
+    url = API['vaccinator'] + f"vaccinator/beneficiaries/findByMobile?mobile_number={mobile_number}"
+    return requests.get(url, headers=headers)
+
+
+def send_request_to_notify_vaccination_via_vaccinator_api(vaccination_data):
+    if not vaccination_data:
+        vaccination_data = {
+            "beneficiary_reference_id": "62682606457710",
+            "center_id": 563048,
+            "vaccine": "COVISHIELD",
+            "vaccine_batch": "123456",
+            "dose": 1,
+            "dose1_date": datetime.date.today().strftime('%d-%m-%Y'),
+            "vaccinator_name": "Prabha Devi",
+        }
+
+    headers = {
+        'Accept-Language': 'en_US',
+        'User-Agent': '',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+    }
+    url = API['vaccinator'] + "vaccinator/beneficiaries/vaccinate"
+    return requests.post(url, headers=headers, data=json.dumps(vaccination_data))
+
+
 # protected apis
 def send_request_for_otp_via_protected_api(mobile_number):
     if not mobile_number:
@@ -42,7 +103,6 @@ def send_request_for_otp_via_protected_api(mobile_number):
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'X-Api-Key': API_KEYS['protected'],
-
     }
 
     url = API['protected'] + "auth/generateOTP"
