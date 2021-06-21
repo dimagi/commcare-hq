a3e53cbcdd14627543adbad3bc3cb2b8f60788bb
Steph <stephherbers@gmail.com>
2021-06-21 13:01:41 -0400

    added html and js files to add tableau viz config to project settings

---
+++ corehq/apps/reports/static/reports/js/tableau_visualization.js
@@ -0,0 +1,6 @@
+hqDefine("reports/js/tableau_visualization", [
+    "hqwebapp/js/crud_paginated_list_init",
+    "hqwebapp/js/widgets",
+], function () {
+    // No page-specific logic, just need to collect the dependencies above
+});
