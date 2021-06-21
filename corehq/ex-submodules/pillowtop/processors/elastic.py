1f34fbaf179c559d0b76e71ad9509b7f60f4b30c
Simon Kelly <skelly@dimagi.com>
2021-06-21 13:38:00 +0200

    ignore couch changes for sql domains in xform / case pillow

--- corehq/ex-submodules/pillowtop/processors/elastic.py
+++ corehq/ex-submodules/pillowtop/processors/elastic.py
@@ -51,7 +51,8 @@ class ElasticProcessor(PillowProcessor):
       - ES
     """
 
-    def __init__(self, elasticsearch, index_info, doc_prep_fn=None, doc_filter_fn=None):
+    def __init__(self, elasticsearch, index_info, doc_prep_fn=None, doc_filter_fn=None, change_filter_fn=None):
+        self.change_filter_fn = change_filter_fn or noop_filter
         self.doc_filter_fn = doc_filter_fn or noop_filter
         self.elasticsearch = elasticsearch
         self.es_interface = ElasticsearchInterface(self.elasticsearch)
@@ -62,6 +63,9 @@ class ElasticProcessor(PillowProcessor):
         return self.elasticsearch
 
     def process_change(self, change):
+        if self.change_filter_fn and self.change_filter_fn(change):
+            return
+
         if change.deleted and change.id:
             self._delete_doc_if_exists(change.id)
             return
