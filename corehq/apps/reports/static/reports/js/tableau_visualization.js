ec58f687bce9430571e0aa005298d6072af17ad9
Steph <stephherbers@gmail.com>
2021-06-22 11:07:50 -0400

    removed need for tableau_visualization.js and adding missing start tag for the delete button column

--- corehq/apps/reports/static/reports/js/tableau_visualization.js
+++
@@ -1,6 +0,0 @@
-hqDefine("reports/js/tableau_visualization", [
-    "hqwebapp/js/crud_paginated_list_init",
-    "hqwebapp/js/widgets",
-], function () {
-    // No page-specific logic, just need to collect the dependencies above
-});
