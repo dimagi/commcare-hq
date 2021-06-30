cea0fd18a2e77710b5e995bae7e61cb38f9d4ef4
Amit Phulera <amitphulera999@gmail.com>
2021-06-30 16:00:19 +0530

    fix tests as per updated data

--- corehq/apps/auditcare/tests/test_tasks.py
+++ corehq/apps/auditcare/tests/test_tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from ..models import AccessAudit, NavigationEventAudit
 from ..tasks import copy_events_to_sql
+from ..tests.data.auditcare_migration import task_docs
 from .testutils import AuditcareTest, delete_couch_docs, save_couch_doc
 
 
@@ -11,58 +12,7 @@ class TestCopyEventsToSQL(AuditcareTest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        username = "couch@test.com"
-        cls.couch_ids = [
-            save_couch_doc(
-                "NavigationEventAudit",
-                username,
-                event_date=datetime(2021, 2, 1, 2).strftime("%Y-%m-%dT%H:%M:%SZ"),
-                description='User Name',
-                extra={},
-                headers={
-                    'REQUEST_METHOD': 'GET',
-                },
-                ip_address='10.1.2.3',
-                request_path='/a/delmar/phone/restore/?version=2.0&since=...',
-                session_key='abc123',
-                status_code=200,
-                user_agent='okhttp/4.4.1',
-                view_kwargs={'domain': 'delmar'},
-                view='corehq.apps.ota.views.restore',
-            ),
-            save_couch_doc(
-                "AccessAudit",
-                username,
-                event_date=datetime(2021, 2, 1, 3).strftime("%Y-%m-%dT%H:%M:%SZ"),
-                access_type='login',
-                description='Login Success',
-                failures_since_start=None,
-                get_data=[],
-                http_accept='text/html',
-                ip_address='10.1.3.2',
-                path_info='/a/delmar/login/',
-                post_data=[],
-                session_key='abc123',
-                user_agent='Mozilla/5.0',
-            ),
-            save_couch_doc(
-                "NavigationEventAudit",
-                username,
-                event_date=datetime(2021, 2, 1, 5).strftime("%Y-%m-%dT%H:%M:%SZ"),
-                description='User Name',
-                extra={},
-                headers={
-                    'REQUEST_METHOD': 'GET',
-                },
-                ip_address='10.1.2.3',
-                request_path='/a/sandwich/phone/restore/?version=2.0&since=...',
-                session_key='abc123',
-                status_code=200,
-                user_agent='okhttp/4.4.1',
-                view_kwargs={'domain': 'sandwich'},
-                view='corehq.apps.ota.views.restore',
-            ),
-        ]
+        cls.couch_ids = [save_couch_doc(**doc) for doc in task_docs]
 
     @classmethod
     def tearDownClass(cls):
@@ -70,16 +20,15 @@ class TestCopyEventsToSQL(AuditcareTest):
         super().tearDownClass()
 
     def test_copy(self):
-
         def _assert():
-            self.assertEqual(NavigationEventAudit.objects.count(), 2)
+            self.assertEqual(NavigationEventAudit.objects.count(), 3)
             self.assertEqual(
                 [e.path for e in NavigationEventAudit.objects.order_by("-event_date").all()],
-                ["just/a/checkpoint", "/a/delmar/phone/restore/"]
+                ["just/a/checkpoint", '/a/random/phone/restore/', '/a/test-space/phone/restore/']
             )
             self.assertEqual(
                 [e.params for e in NavigationEventAudit.objects.order_by("-event_date").all()],
-                ["", "version=2.0&since=..."]
+                ["", "version=2.0&since=...", "version=2.0&since=..."]
             )
             self.assertEqual(AccessAudit.objects.count(), 1)
             self.assertEqual(AccessAudit.objects.first().path, "/a/delmar/login/")
