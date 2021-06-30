934efa8f340dc88b44ceeb5b35b685797c5518aa
Ethan Soergel <esoergel@gmail.com>
2021-06-30 15:37:24 +0200

    Use fancy URL pattern to handle trailing slashes
    With PUT and POST requests, omitting the trailing slash causes a
    redirect, which clients typically don't handle well (it strips the POST
    data).  Shout out to Norman and Daniel for the syntax.

--- corehq/apps/api/urls.py
+++ corehq/apps/api/urls.py
@@ -33,7 +33,6 @@ from corehq.apps.fixtures.resources.v0_1 import (
     LookupTableItemResource,
     LookupTableResource,
 )
-from corehq.apps.hqcase.urls import case_api_urlpatterns
 from corehq.apps.hqcase.views import case_api
 from corehq.apps.hqwebapp.decorators import waf_allow
 from corehq.apps.locations import resources as locations
@@ -120,8 +119,8 @@ def api_url_patterns():
               ODataFormMetadataView.as_view(), name=ODataFormMetadataView.table_urlname)
     yield url(r'v0.5/odata/forms/(?P<config_id>[\w\-:]+)/\$metadata$',
               ODataFormMetadataView.as_view(), name=ODataFormMetadataView.urlname)
-    yield url(r'v0.6/case$', case_api)  # We don't want Django to redirect to add a trailing slash
-    yield url(r'v0.6/case/?', include(case_api_urlpatterns))
+    # match v0.6/case/ AND v0.6/case/e0ad6c2e-514c-4c2b-85a7-da35bbeb1ff1/ trailing slash optional
+    yield url(r'v0\.6/case(?:/(?P<case_id>[\w-]+))?/?$', case_api, name='case_api')
     yield from versioned_apis(API_LIST)
     yield url(r'^case/attachment/(?P<case_id>[\w\-:]+)/(?P<attachment_id>.*)$', CaseAttachmentAPI.as_view(), name="api_case_attachment")
     yield url(r'^form/attachment/(?P<form_id>[\w\-:]+)/(?P<attachment_id>.*)$', FormAttachmentAPI.as_view(), name="api_form_attachment")
