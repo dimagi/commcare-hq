d2481f4dbed2d1d14f381ef3b9022e85e47c77b9
Norman Hooper <nhooper@dimagi.com>
2021-07-02 22:22:30 +0200

    Revert "Validate Bundle  # breaks test"
    This reverts commit d58571c274a2f19114165c0f7a8a112a56ffef02.
    
    It turns out that the [first Bundle example][1] in the FHIR reference
    fails its own schema validation.
    
      [1]: https://www.hl7.org/fhir/bundle-example.json.html

--- corehq/motech/fhir/tests/test_bundle.py
+++ corehq/motech/fhir/tests/test_bundle.py
@@ -1,42 +1,6 @@
 import doctest
-from unittest.mock import Mock
-
-from django.test import SimpleTestCase
-
-import attr
-
-from corehq.motech.exceptions import ConfigurationError
 
 from .. import bundle
-from ..bundle import get_bundle
-
-
-class GetBundleTest(SimpleTestCase):
-
-    def test_invalid_bundle(self):
-        response = Response({'resourceType': 'Bundle', 'entry': 'invalid'})
-        requests = Mock()
-        requests.get.return_value = response
-
-        with self.assertRaises(ConfigurationError):
-            get_bundle(requests, 'Patient/')
-
-    def test_valid_bundle(self):
-        valid_bundle = {'resourceType': 'Bundle', 'entry': []}
-        response = Response(valid_bundle)
-        requests = Mock()
-        requests.get.return_value = response
-
-        bundle_ = get_bundle(requests, 'Patient/')
-        self.assertEqual(bundle_, valid_bundle)
-
-
-@attr.s(auto_attribs=True)
-class Response:
-    data: dict
-
-    def json(self):
-        return self.data
 
 
 def test_doctests():
