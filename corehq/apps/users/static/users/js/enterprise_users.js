89be9f183d0c453bf3e86213b450eeed97ad4a0c
Jennifer Schweers <orange.jenny@gmail.com>
2021-07-06 14:35:48 -0400

    Combined javascript back into a single model

--- corehq/apps/users/static/users/js/enterprise_users.js
+++ corehq/apps/users/static/users/js/enterprise_users.js
@@ -1,12 +1,23 @@
-// TODO: extract webUserList and share with web_users.js?
-hqDefine("users/js/enterprise_users",[
-    'jquery',
-    'knockout',
-    'underscore',
-    "hqwebapp/js/assert_properties",
+/**
+ * This file controls the UI for the enterprise users page.
+ * This page is based on the main "Current Users" panel on
+ * the web users page, but it displays a different set of columns
+ * and adds in rows for mobile workers linked to the web users.
+ */
+hqDefine("users/js/enterprise_users", [
+    "jquery",
+    "knockout",
+    "underscore",
     "hqwebapp/js/initial_page_data",
-    'hqwebapp/js/components.ko',    // pagination and search box widgets
-], function ($, ko, _, assertProperties, initialPageData) {
+    "users/js/web_users_list",
+    "hqwebapp/js/components.ko",    // pagination and search box widgets
+], function (
+    $,
+    ko,
+    _,
+    initialPageData,
+    webUsersList,
+) {
     var UserModel = function (options) {
         var self = _.defaults(options, {
             profile: null,
@@ -23,59 +34,8 @@ hqDefine("users/js/enterprise_users",[
         return self;
     };
 
-    var webUsersList = function () {
-        var self = {};
-        self.users = ko.observableArray([]);
-
-        self.query = ko.observable('');
-
-        self.itemsPerPage = ko.observable();
-        self.totalItems = ko.observable();
-
-        self.error = ko.observable();
-        self.showLoadingSpinner = ko.observable(true);
-        self.showPaginationSpinner = ko.observable(false);
-        self.showUsers = ko.computed(function () {
-            return !self.showLoadingSpinner() && !self.error() && self.users().length > 0;
-        });
-
-        self.noUsersMessage = ko.computed(function () {
-            if (!self.showLoadingSpinner() && !self.error() && self.users().length === 0) {
-                if (self.query()) {
-                    return gettext("No users matched your search.");
-                }
-                return gettext("This project has no web users. Please invite a web user above.");   // TODO: remove or update
-            }
-            return "";
-        });
-
-        self.goToPage = function (page) {
-            self.showPaginationSpinner(true);
-            self.error('');
-            $.ajax({
-                method: 'GET',
-                url: initialPageData.reverse('paginate_enterprise_users'),
-                data: {
-                    page: page,
-                    query: self.query() || '',
-                    limit: self.itemsPerPage(),
-                },
-                success: function (data) {
-                    self.showLoadingSpinner(false);
-                    self.showPaginationSpinner(false);
-                    self.totalItems(data.total);
-                    self.users.removeAll();
-                    _.each(data.users, function (user) {
-                        self.users.push(UserModel(user));
-                    });
-                },
-                error: function () {
-                    self.showLoadingSpinner(false);
-                    self.showPaginationSpinner(false);
-                    self.error(gettext("Could not load users. Please try again later or report an issue if this problem persists."));
-                },
-            });
-        };
+    var enterpriseUsersList = function (options) {
+        var self = webUsersList(options);
 
         self.toggleLoginAsUsers = function (web_user, e) {
             web_user.expanded(!web_user.expanded());
@@ -86,14 +46,13 @@ hqDefine("users/js/enterprise_users",[
             });
         };
 
-        self.onPaginationLoad = function () {
-            self.goToPage(1);
-        };
-
         return self;
     };
 
     $(function () {
-        $("#web-users-panel").koApplyBindings(webUsersList());
+        $("#web-users-panel").koApplyBindings(enterpriseUsersList({
+            url: initialPageData.reverse("paginate_enterprise_users"),
+            userModel: UserModel,
+        }));
     });
 });
