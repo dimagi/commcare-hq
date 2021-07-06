aacba1c4b2878571fd8e95890f52d78704506fc3
Simon Kelly <skelly@dimagi.com>
2021-07-06 11:32:54 +0200

    add registry to Django admin

---
+++ corehq/apps/registry/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+
+from .models import DataRegistry, RegistryInvitation, RegistryGrant, RegistryPermission
+
+
+class RegistryInvitationInline(admin.TabularInline):
+    model = RegistryInvitation
+    autocomplete_fields = ['accepted_by', 'rejected_by']
+    extra = 1
+
+
+class RegistryGrantInline(admin.TabularInline):
+    model = RegistryGrant
+    extra = 1
+
+
+class RegistryPermissionInline(admin.TabularInline):
+    model = RegistryPermission
+    extra = 1
+
+
+@admin.register(DataRegistry)
+class DataRegistryAdmin(admin.ModelAdmin):
+    list_display = ['domain', 'name', 'slug', 'is_active', 'created_on']
+    list_filter = ['domain', 'name', 'is_active']
+    readonly_fields = ['slug']
+
+    inlines = [
+        RegistryInvitationInline,
+        RegistryGrantInline,
+        RegistryPermissionInline
+    ]
+
+
