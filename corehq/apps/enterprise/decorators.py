388fbe1883f772ff18db82ccc59517ede341fe2b
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-07-14 20:34:54 +0200

    lint

--- corehq/apps/enterprise/decorators.py
+++ corehq/apps/enterprise/decorators.py
@@ -13,7 +13,7 @@ def require_enterprise_admin(view_func):
     def _inner(request, domain, *args, **kwargs):
         if not hasattr(request, 'couch_user'):
             raise Http404()
-        
+
         account = get_account_or_404(domain)
         if not request_has_permissions_for_enterprise_admin(request, account):
             # we want these urls to remain ambiguous/not discoverable so
