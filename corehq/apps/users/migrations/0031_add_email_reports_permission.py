41f570c82cda580795c2cb32a6ed3b81764a885e
Steph <stephherbers@gmail.com>
2021-07-14 13:22:45 -0400

    add migration so email_reports is enabled for all roles by default

---
+++ corehq/apps/users/migrations/0031_add_email_reports_permission.py
@@ -0,0 +1,31 @@
+from django.db import migrations
+
+from dimagi.utils.couch.database import iter_docs
+
+from corehq.apps.users.models import UserRole
+from corehq.util.django_migrations import skip_on_fresh_install
+
+
+@skip_on_fresh_install
+def migrate_email_reports_permissions(apps, schema_editor):
+    roles = UserRole.view(
+        'users/roles_by_domain',
+        include_docs=False,
+        reduce=False
+    ).all()
+    for role_doc in iter_docs(UserRole.get_db(), [r['id'] for r in roles]):
+        role = UserRole.wrap(role_doc)
+
+        role.permissions.email_reports = True
+        role.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0030_userhistory_user_upload_record'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate_email_reports_permissions, migrations.RunPython.noop)
+    ]
