5430b51ca57ad08609f5d7895c2b9253be310420
Rynhard <rpietersen@dimagi.com>
2021-07-14 09:56:20 +0200

    wrote a test for my changes

---
+++ corehq/apps/enterprise/tests/test_interface.py
@@ -0,0 +1,31 @@
+from corehq.apps.domain.shortcuts import create_domain
+from django.test import TestCase
+from django.test.client import RequestFactory
+
+from corehq.apps.users.models import WebUser
+
+from ..interface import EnterpriseSMSBillablesInterface
+
+
+class TestEnterpriseSMSBillablesInterface(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.domain = 'test-domain'
+        cls.domain_obj = create_domain(cls.domain)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.domain_obj.delete()
+
+    def test_headers(self):
+        interface = self.create_interface()
+        test_headers = [['Date of Message', 'Project Space', 'Direction', 'SMS parts', 'Gateway',
+            'Gateway Charge', 'Usage Charge', 'Total Charge', 'Message Log ID', 'Is Valid?', 'Date Created']]
+        self.assertEqual(interface.headers.as_export_table, test_headers)
+
+    def create_interface(self):
+        request = RequestFactory().get('')
+        user = WebUser()
+        request.couch_user = user
+        return EnterpriseSMSBillablesInterface(request)
