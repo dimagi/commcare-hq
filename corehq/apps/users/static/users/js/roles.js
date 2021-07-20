47c85b594b47cd286d6491867b32173e01e32db6
Steph <stephherbers@gmail.com>
2021-07-20 12:53:20 -0400

    remove visibility constraint and modify description

--- corehq/apps/users/static/users/js/roles.js
+++ corehq/apps/users/static/users/js/roles.js
@@ -238,11 +238,12 @@ hqDefine('users/js/roles',[
                         checkboxText: gettext("Allow role to create and edit reports in report builder."),
                     },
                     {
-                        visibilityRestraint: self.permissions.access_all_locations,
+                        visibilityRestraint: true,
                         text: gettext("Download and Email Reports"),
                         checkboxLabel: "download-and-email-reports-checkbox",
                         checkboxPermission: self.permissions.download_reports,
-                        checkboxText: gettext("Allow role to download and email reports."),
+                        checkboxText: gettext("Allow role to download and email reports and " +
+                            "create scheduled reports."),
                     },
                     {
                         visibilityRestraint: true,
