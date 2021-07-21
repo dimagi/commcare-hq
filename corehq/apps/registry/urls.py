86f34bf54f7dc236d6165055cba7ea13e3706635
Simon Kelly <skelly@dimagi.com>
2021-07-21 14:50:09 +0200

    add reject and move actions to new JS module

--- corehq/apps/registry/urls.py
+++ corehq/apps/registry/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
 
-from corehq.apps.registry.views import data_registries, accept_registry_invitation
+from corehq.apps.registry.views import (
+    data_registries,
+    accept_registry_invitation,
+    reject_registry_invitation
+)
 
 urlpatterns = [
     url(r'^$', data_registries, name='data_registries'),
     url(r'^accept$', accept_registry_invitation, name='accept_registry_invitation'),
+    url(r'^reject$', reject_registry_invitation, name='reject_registry_invitation'),
 ]
--- corehq/apps/registry/views.py
+++ corehq/apps/registry/views.py
@@ -71,15 +71,31 @@ def _registry_list_context(domain, registry):
 @require_POST
 def accept_registry_invitation(request, domain):
     registry_slug = request.POST.get('registry_slug')
-    try:
-        invitation = RegistryInvitation.objects.get(
-            registry__slug=registry_slug,
-            domain=domain
-        )
-    except RegistryInvitation.DoesNotExist:
-        raise Http404
+    invitation = _get_invitation_or_404(domain, registry_slug)
 
     if not invitation.is_accepted:
         invitation.accept(request.user)
 
     return JsonResponse(_registry_list_context(domain, invitation.registry))
+
+
+@domain_admin_required
+@require_POST
+def reject_registry_invitation(request, domain):
+    registry_slug = request.POST.get('registry_slug')
+    invitation = _get_invitation_or_404(domain, registry_slug)
+
+    if not invitation.is_rejected:
+        invitation.reject(request.user)
+
+    return JsonResponse(_registry_list_context(domain, invitation.registry))
+
+
+def _get_invitation_or_404(domain, registry_slug):
+    try:
+        return RegistryInvitation.objects.get(
+            registry__slug=registry_slug,
+            domain=domain
+        )
+    except RegistryInvitation.DoesNotExist:
+        raise Http404
