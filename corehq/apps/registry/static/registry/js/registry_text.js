0b3bb08b746b328cc1827b7c3ee60d85bcbc1e01
Simon Kelly <skelly@dimagi.com>
2021-07-21 12:32:55 +0200

    move translation function to new module

---
+++ corehq/apps/registry/static/registry/js/registry_text.js
@@ -0,0 +1,57 @@
+hqDefine("registry/js/registry_text", ['moment'], function (moment) {
+    let getAcceptedBadgeText = function (registry) {
+        return interpolate(ngettext(
+            "%(count)s Invitation Accepted",
+            "%(count)s Invitations Accepted",
+            registry.accepted_invitation_count
+        ), {"count": registry.accepted_invitation_count}, true);
+    }
+    let getPendingBadgeText = function (registry) {
+        return interpolate(ngettext(
+            "%(count)s Invitation Pending",
+            "%(count)s Invitations Pending",
+            registry.pending_invitation_count
+        ), {"count": registry.pending_invitation_count}, true);
+    }
+    let getRejectedBadgeText = function (registry) {
+        return interpolate(ngettext(
+            "%(count)s Invitation Rejected",
+            "%(count)s Invitations Rejected",
+            registry.rejected_invitation_count
+        ), {"count": registry.rejected_invitation_count}, true);
+    }
+    let getParticipatorCountBadgeText = function (registry) {
+        return interpolate(ngettext(
+            "%(count)s Project Space Participating",
+            "%(count)s Project Spaces Participating",
+            registry.participator_count
+        ), {"count": registry.participator_count}, true);
+    }
+    let getStatusText = function (registry) {
+        if (registry.invitation.status === 'rejected') {
+            return gettext('Rejected');
+        } else if (registry.invitation.status === 'accepted') {
+            return gettext('Accepted');
+        } else {
+            return gettext('Pending');
+        }
+    }
+    let getRejectedText = function (registry) {
+        console.log(registry.invitation);
+        const text = gettext("Rejected by %(user)s on %(date)s"),
+            params = {
+                user: registry.invitation.rejected_by,
+                date: moment(registry.invitation.rejected_on).format("D MMM YYYY")
+            };
+        return interpolate(text, params, true);
+    }
+
+    return {
+        getAcceptedBadgeText: getAcceptedBadgeText,
+        getPendingBadgeText: getPendingBadgeText,
+        getRejectedBadgeText: getRejectedBadgeText,
+        getParticipatorCountBadgeText: getParticipatorCountBadgeText,
+        getStatusText: getStatusText,
+        getRejectedText: getRejectedText,
+    };
+});
