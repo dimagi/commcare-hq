e13cf96d7aac357c510e96563057710f124a4856
Amit Phulera <amitphulera999@gmail.com>
2021-07-22 12:07:55 +0530

    remove unused setting

--- settings.py
+++ settings.py
@@ -1162,8 +1162,6 @@ _location = lambda x: os.path.join(FILEPATH, x)
 
 IS_SAAS_ENVIRONMENT = SERVER_ENVIRONMENT in ('production', 'staging')
 
-IS_INDIA_ENVIRONMENT = SERVER_ENVIRONMENT == 'india'
-
 ALLOW_MAKE_SUPERUSER_COMMAND = True
 
 if 'KAFKA_URL' in globals():
