9d4026f33e1d2e148933e983b5ceb57ce697d0e7
Jennifer Schweers <orange.jenny@gmail.com>
2021-07-26 09:51:39 -0400

    Incremented migration

--- corehq/apps/users/migrations/0031_delete_domainpermissionsmirror.py
+++ corehq/apps/users/migrations/0031_delete_domainpermissionsmirror.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('users', '0028_remove_domainpermissionsmirror'),
+        ('users', '0030_userhistory_user_upload_record'),
     ]
 
     operations = [
