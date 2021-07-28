e9cb748d32d0a83e8b73c268f91543965d31c0cc
Simon Kelly <skelly@dimagi.com>
2021-07-28 09:47:48 +0200

    refactor alert JS

--- corehq/apps/hqwebapp/static/hqwebapp/js/alert_user.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/alert_user.js
@@ -21,53 +21,61 @@ function (
     $,
     ko
 ) {
-    var message_alert = function (message, tags) {
-        var alert_obj = {
+    var MessageAlert = function (message, tags) {
+        var self = {
             "message": ko.observable(message),
             "alert_class": ko.observable(
                 "alert fade in message-alert"
             ),
         };
         if (tags) {
-            alert_obj.alert_class(alert_obj.alert_class() + " " + tags);
+            self.alert_class(self.alert_class() + " " + tags);
         }
-        return alert_obj;
+        return self;
     };
-    var message_alerts = ko.observableArray();
 
-    var alert_user = function (message, emphasis, append) {
-        var tags = "alert-" + emphasis;
-        if (!append || message_alerts().length === 0) {
-            message_alerts.push(message_alert(message, tags));
-        } else {
-            var alert = message_alerts()[0];
-            alert.message(alert.message() + "<br>" + message);
-            if (!alert.alert_class().includes(tags)) {
-                alert.alert_class(alert.alert_class() + ' ' + tags);
+    const ViewModel = function() {
+        let self = this;
+        self.alerts = ko.observableArray();
+        self.removeAlert = (alertObj) => {
+            self.alerts.remove(alertObj);
+        };
+
+        self.alert_user = function (message, emphasis, append) {
+            var tags = "alert-" + emphasis;
+            if (!append || self.alerts().length === 0) {
+                self.alerts.push(MessageAlert(message, tags));
+            } else {
+                var alert = self.alerts()[0];
+                alert.message(alert.message() + "<br>" + message);
+                if (!alert.alert_class().includes(tags)) {
+                    alert.alert_class(alert.alert_class() + ' ' + tags);
+                }
             }
-        }
 
-        // Scroll to top of page to see alert
-        document.body.scrollTop = document.documentElement.scrollTop = 0;
+            // Scroll to top of page to see alert
+            document.body.scrollTop = document.documentElement.scrollTop = 0;
+        };
+        return self;
     };
+
+    const viewModel = ViewModel();
 
     $(function () {
         // remove closed alerts from backend model
         $(document).on('close.bs.alert','.message-alert', function () {
-            message_alerts.remove(ko.dataFor(this));
+            viewModel.removeAlert(ko.dataFor(this));
         });
 
         var message_element = $("#message-alerts").get(0);
         // this element is not available on templates like iframe_domain_login.html
         if (message_element) {
             ko.cleanNode(message_element);
-            $(message_element).koApplyBindings({
-                alerts: message_alerts,
-            });
+            $(message_element).koApplyBindings(viewModel);
         }
     });
 
     return {
-        alert_user: alert_user,
+        alert_user: viewModel.alert_user,
     };
 });
