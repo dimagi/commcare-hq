3f314790086c4d07f59a5b89bff230fb88090dd0
Steph <stephherbers@gmail.com>
2021-07-28 21:10:38 -0400

    modified migration to use new managment command that filters role to change by if role has permission

--- corehq/apps/users/migrations/0032_add_download_reports_permission.py
+++ corehq/apps/users/migrations/0032_add_download_reports_permission.py
@@ -1,18 +1,18 @@
+import traceback
 from django.db import migrations
 
-from corehq.apps.users.models_sql import SQLPermission, SQLUserRole
+from corehq.apps.couch_sql_migration.tests.test_migration import call_command
 from corehq.util.django_migrations import skip_on_fresh_install
 
 
 @skip_on_fresh_install
 def migrate_download_reports_permissions(apps, schema_editor):
-    new_permission_name = 'download_reports'
-    permission, created = SQLPermission.objects.get_or_create(value=new_permission_name)
-    for role in SQLUserRole.objects.all().iterator():
-        rp, created = role.rolepermission_set.get_or_create(permission_fk=permission,
-                                                            defaults={"allow_all": True})
-        if created:
-            role._migration_do_sync()
+    try:
+        call_command(
+            "add_download_reports_permission",
+        )
+    except Exception:
+        traceback.print_exc()
 
 
 class Migration(migrations.Migration):
