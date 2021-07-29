14c894c90786bd6a42137a5881e5081ff7f43d60
Steph <stephherbers@gmail.com>
2021-07-29 12:27:47 -0400

    added print statements to command

--- corehq/apps/users/management/commands/add_download_reports_permission.py
+++ corehq/apps/users/management/commands/add_download_reports_permission.py
@@ -8,8 +8,12 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         permission, created = SQLPermission.objects.get_or_create(value='download_reports')
+        num_roles_modified = 0
         for role in SQLUserRole.objects.exclude(rolepermission__permission_fk_id=permission.id).iterator():
             rp, created = role.rolepermission_set.get_or_create(permission_fk=permission,
                                                                 defaults={"allow_all": True})
             if created:
                 role._migration_do_sync()
+                num_roles_modified += 1
+            if num_roles_modified % 5000 == 0:
+                print("Updated {} roles".format(num_roles_modified))
