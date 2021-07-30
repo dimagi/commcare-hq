7c18e2d911a6da96a26ad0402ec7689e28d92eb3
Cal Ellowitz <calellowitz@gmail.com>
2021-07-30 15:00:19 -0400

    update prune_synclogs

--- corehq/ex-submodules/casexml/apps/phone/tasks.py
+++ corehq/ex-submodules/casexml/apps/phone/tasks.py
@@ -12,6 +12,8 @@ from casexml.apps.phone.cleanliness import set_cleanliness_flags_for_all_domains
 from casexml.apps.phone.models import SyncLogSQL
 from dimagi.utils.logging import notify_exception
 
+from corehq.util.metrics import metrics_gauge
+
 log = logging.getLogger(__name__)
 
 ASYNC_RESTORE_QUEUE = 'async_restore_queue'
@@ -92,10 +94,11 @@ def update_celery_state(sender=None, headers=None, **kwargs):
 )
 def prune_synclogs():
     """
-    Drops all partition tables containing data that's older than 63 days (7 weeks)
+    Drops all partition tables containing data that's older than 63 days (9 weeks)
     """
-    oldest_synclog = SyncLogSQL.objects.aggregate(Min('date'))['date__min']
-    while oldest_synclog and (datetime.today() - oldest_synclog).days > SYNCLOG_RETENTION_DAYS:
+    db = router.db_for_write(SyncLogSQL)
+    oldest_date = SyncLogSQL.objects.aggregate(Min('date'))['date__min']
+    while oldest_date and (datetime.today() - oldest_date).days > SYNCLOG_RETENTION_DAYS:
         year, week, _ = oldest_synclog.isocalendar()
         table_name = "{base_name}_y{year}w{week}".format(
             base_name=SyncLogSQL._meta.db_table,
@@ -103,7 +106,12 @@ def prune_synclogs():
             week="%02d" % week
         )
         drop_query = "DROP TABLE IF EXISTS {}".format(table_name)
-        db = router.db_for_write(SyncLogSQL)
         with connections[db].cursor() as cursor:
             cursor.execute(drop_query)
-        oldest_synclog = SyncLogSQL.objects.aggregate(Min('date'))['date__min']
+        oldest_date += timedelta(weeks=1)
+
+    # find and log synclogs for which the trigger did not function properly
+    with connections[db].cursor() as cursor:
+        cursor.execute("select count(*) from only phone_synclogsql")
+        orphaned_synclogs = cursor.fetchone()[0]
+        metrics_gauge('commcare.orphaned_synclogs', orphaned_synclogs)
