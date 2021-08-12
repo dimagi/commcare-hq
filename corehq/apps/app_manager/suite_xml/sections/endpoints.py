f701092a491a65342a8d4615acde3c115fd99830
Jennifer Schweers <orange.jenny@gmail.com>
2021-08-12 16:05:11 -0400

    Extracted helper methods

--- corehq/apps/app_manager/suite_xml/sections/endpoints.py
+++ corehq/apps/app_manager/suite_xml/sections/endpoints.py
@@ -46,30 +46,43 @@ class SessionEndpointContributor(SuiteContributorByModule):
         stack = Stack()
         helper = EndpointsHelper(self.suite, self.app)
         children = helper.get_frame_children(module, form)
-        for child in children:
-            if isinstance(child, WorkflowDatumMeta) and child.requires_selection:
-                frame = PushFrame()
-                stack.add_frame(frame)
-                frame.add_datum(
-                    StackDatum(id=child.id, value=f"${child.id}")
-                )
-                frame.add_command(f"'claim_command.{endpoint_id}.{child.id}'")
+        argument_ids = self._get_argument_ids(children)
+
+        # Add a claim request for each endpoint argument.
+        # This assumes that all arguments are case ids.
+        for arg_id in argument_ids:
+            self._add_claim_frame(stack, arg_id, endpoint_id)
+
+        # Add a frame to navigate to the endpoint
         frame = PushFrame()
         stack.add_frame(frame)
-        arguments = []
         for child in children:
-            if isinstance(child, WorkflowDatumMeta) and child.requires_selection:
-                arguments.append(Argument(id=child.id))
-                frame.add_datum(
-                    StackDatum(id=child.id, value=f"${child.id}")
-                )
-            elif isinstance(child, CommandId):
+            if isinstance(child, CommandId):
                 frame.add_command(child.to_command())
+            elif child.id in argument_ids:
+                self._add_datum_for_arg(frame, child.id)
 
         return SessionEndpoint(
             id=endpoint_id,
-            arguments=arguments,
+            arguments=[Argument(id=i) for i in argument_ids],
             stack=stack,
+        )
+
+    def _get_argument_ids(self, frame_children):
+        return [
+            child.id for child in frame_children
+            if isinstance(child, WorkflowDatumMeta) and child.requires_selection
+        ]
+
+    def _add_claim_frame(self, stack, arg_id, endpoint_id):
+        frame = PushFrame()
+        stack.add_frame(frame)
+        self._add_datum_for_arg(frame, arg_id)
+        frame.add_command(f"'claim_command.{endpoint_id}.{arg_id}'")
+
+    def _add_datum_for_arg(self, frame, arg_id):
+        frame.add_datum(
+            StackDatum(id=arg_id, value=f"${arg_id}")
         )
 
 
