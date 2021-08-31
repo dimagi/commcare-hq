607def2e19fa3bea6a7ea7cbddbfe147688fae88
Simon Kelly <skelly@dimagi.com>
2021-08-31 13:17:20 +0200

    build registry domains fixture

--- corehq/apps/registry/fixturegenerators.py
+++ corehq/apps/registry/fixturegenerators.py
@@ -1,5 +1,8 @@
 from lxml.builder import E
 
+from corehq.apps.domain.models import Domain
+from corehq.apps.registry.helper import DataRegistryHelper
+
 
 def _get_registry_list_fixture(registries):
     registry_elements = [
@@ -11,6 +14,13 @@ def _get_registry_list_fixture(registries):
     )
 
 
+def _get_registry_domains_fixture(current_domain, registry):
+    domains = _get_registry_domains(current_domain, registry)
+    return E.fixture(
+        E.domains(*domains),
+        id=f'registry:domains:{registry.slug}'
+    )
+
 def _get_registry_element(registry):
     return E.registry(
         *[E.name(registry.name), E.description(registry.description)],
@@ -18,3 +28,13 @@ def _get_registry_element(registry):
         owner=registry.domain,
         active='true' if registry.is_active else 'false'
     )
+
+
+def _get_registry_domains(current_domain, registry):
+    helper = DataRegistryHelper(current_domain, registry=registry)
+    domain_elements = []
+    for domain in sorted(list(helper.visible_domains)):
+        domain_obj = Domain.get_by_name(domain)
+        display_name = domain_obj.display_name() if domain_obj else domain
+        domain_elements.append(E.domain(display_name, name=domain))
+    return domain_elements
--- corehq/apps/registry/tests/test_fixtures.py
+++ corehq/apps/registry/tests/test_fixtures.py
@@ -1,9 +1,11 @@
+from unittest.mock import patch
 from xml.etree import ElementTree
 
 from django.test import SimpleTestCase
 
 from corehq.apps.app_manager.tests.util import TestXmlMixin
-from corehq.apps.registry.fixturegenerators import _get_registry_list_fixture
+from corehq.apps.domain.models import Domain
+from corehq.apps.registry.fixturegenerators import _get_registry_list_fixture, _get_registry_domains_fixture
 from corehq.apps.registry.models import DataRegistry
 
 
@@ -15,6 +17,7 @@ class RegistryFixtureTests(SimpleTestCase, TestXmlMixin):
             description="Registry of Malaria Patients",
             slug="reg1",
         )
+        self.registry1.get_granted_domains = _mock_get_granted_domain({"domain1", "domain2"})
 
         self.registry2 = DataRegistry(
             domain="domain_owner_1",
@@ -40,3 +43,27 @@ class RegistryFixtureTests(SimpleTestCase, TestXmlMixin):
            </registry_list>
         </fixture>"""
         self.assertXmlEqual(expected, ElementTree.tostring(fixture, encoding='unicode'))
+
+    def test_registry_domains_fixture(self):
+        expected = """
+        <fixture id="registry:domains:reg1">
+           <domains>
+               <domain name="domain1">domain1</domain>
+               <domain name="domain2">Domain Number 2</domain>
+           </domains>
+        </fixture>
+        """
+
+        def _mock_get_domain(name):
+            if name == "domain2":
+                return Domain(hr_name="Domain Number 2")
+
+        with patch.object(Domain, 'get_by_name', _mock_get_domain):
+            fixture = _get_registry_domains_fixture("domain1", self.registry1)
+        self.assertXmlEqual(expected, ElementTree.tostring(fixture, encoding='unicode'))
+
+
+def _mock_get_granted_domain(domains):
+    def _mock_fn(domain):
+        return domains
+    return _mock_fn
