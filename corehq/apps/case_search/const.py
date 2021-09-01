9dd1c4fe5495df34cb4974893bef916afb4d73cc
Ethan Soergel <esoergel@gmail.com>
2021-09-01 16:31:22 -0400

    Tag related cases so they can be filtered out

--- corehq/apps/case_search/const.py
+++ corehq/apps/case_search/const.py
@@ -14,6 +14,8 @@ CASE_SEARCH_MAX_RESULTS = 500
 
 # Added to each case response when case searches are performed
 RELEVANCE_SCORE = "commcare_search_score"
+# Added to secondary results in case search to filter them out of the case list
+IS_RELATED_CASE = "commcare_is_related_case"
 
 # Added to each case on the index for debugging when a case was added to ES
 INDEXED_ON = '@indexed_on'
