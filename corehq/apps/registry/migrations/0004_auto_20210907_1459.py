acab3cfdbbe13a4d9e266547773acfc1789ca22e
Simon Kelly <skelly@dimagi.com>
2021-09-08 12:30:01 +0200

    fix migration conflict

--- corehq/apps/registry/migrations/0004_auto_20210907_1459.py
+++ corehq/apps/registry/migrations/0004_auto_20210907_1459.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('registry', '0002_registry_slug_globally_unique'),
+        ('registry', '0003_schema_validation'),
     ]
 
     operations = [
