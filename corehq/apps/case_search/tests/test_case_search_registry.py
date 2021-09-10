8f2008a6b0153c1957147bc4e8ec2cf5dd091d43
Ethan Soergel <esoergel@gmail.com>
2021-09-10 12:16:31 -0400

    Add some more tests

--- corehq/apps/case_search/tests/test_case_search_registry.py
+++ corehq/apps/case_search/tests/test_case_search_registry.py
@@ -20,70 +20,100 @@ from corehq.apps.registry.tests.utils import (
 from corehq.form_processor.tests.utils import run_with_sql_backend
 
 
+def case(name, type_, properties):
+    return CaseBlock(
+        case_id=str(uuid.uuid4()),
+        case_type=type_,
+        case_name=name,
+        create=True,
+        update=properties,
+    )
+
+
 @es_test
 @run_with_sql_backend
 class TestCaseSearchRegistry(TestCase):
 
-    # TODO convert to setUpClass
-    def setUp(self):
-        self.user = create_user("admin", "123")
-        self.domain_1 = "jane-the-virgin"
-        self.setup_domain(self.domain_1, [
-            ("Jane", {"family": "Villanueva"}),
-            ("Xiomara", {"family": "Villanueva"}),
-            ("Alba", {"family": "Villanueva"}),
-            ("Rogelio", {"family": "de la Vega"}),
-            ("Jane", {"family": "Ramos"}),
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = create_user("admin", "123")
+        cls.domain_1 = "jane-the-virgin"
+        cls.setup_domain(cls.domain_1, [
+            case("Jane", 'person', {"family": "Villanueva"}),
+            case("Xiomara", 'person', {"family": "Villanueva"}),
+            case("Alba", 'person', {"family": "Villanueva"}),
+            case("Rogelio", 'person', {"family": "de la Vega"}),
+            case("Jane", 'person', {"family": "Ramos"}),
         ])
-        self.domain_2 = "jane-eyre"
-        self.setup_domain(self.domain_2, [
-            ("Jane", {"family": "Eyre"}),
-            ("Sarah", {"family": "Reed"}),
-            ("John", {"family": "Reed"}),
-            ("Eliza", {"family": "Reed"}),
-            ("Georgiana", {"family": "Reed"}),
+        cls.domain_2 = "jane-eyre"
+        cls.setup_domain(cls.domain_2, [
+            case("Jane", 'person', {"family": "Eyre"}),
+            case("Sarah", 'person', {"family": "Reed"}),
+            case("John", 'person', {"family": "Reed"}),
+            case("Eliza", 'person', {"family": "Reed"}),
+            case("Georgiana", 'person', {"family": "Reed"}),
         ])
-        self.domain_3 = "janes-addiction"
-        self.setup_domain(self.domain_3, [
-            ("Jane", {}),
-            ("Perry", {"family": "Farrell"}),
-            ("Dave", {"family": "Navarro"}),
-            ("Stephen", {"family": "Perkins"}),
-            ("Chris", {"family": "Chaney"}),
+        cls.domain_3 = "janes-addiction"
+        cls.setup_domain(cls.domain_3, [
+            case("Jane", 'person', {"family": "Villanueva"}),
+            case("Perry", 'person', {"family": "Farrell"}),
+            case("Dave", 'person', {"family": "Navarro"}),
+            case("Stephen", 'person', {"family": "Perkins"}),
+            case("Chris", 'person', {"family": "Chaney"}),
         ])
 
-        self.registry_slug = create_registry_for_test(
-            self.user,
-            self.domain_1,
+        cls.registry_slug = create_registry_for_test(
+            cls.user,
+            cls.domain_1,
             invitations=[
-                Invitation(self.domain_2),
-                Invitation(self.domain_3),
+                Invitation(cls.domain_2),
+                Invitation(cls.domain_3),
             ],
             grants=[
-                Grant(self.domain_1, [self.domain_2, self.domain_3]),
-                Grant(self.domain_2, [self.domain_1]),
-                Grant(self.domain_3, []),
+                Grant(cls.domain_1, [cls.domain_2]),
+                Grant(cls.domain_2, [cls.domain_1]),
+                Grant(cls.domain_3, [cls.domain_1]),
             ],
             name="reg1",
         ).slug
 
-    def setup_domain(self, domain, cases):
+    @classmethod
+    def setup_domain(cls, domain, cases):
         CaseSearchConfig.objects.create(pk=domain, enabled=True)
-        case_search_es_setup(domain, [
-            CaseBlock(
-                case_id=str(uuid.uuid4()),
-                case_type='person',
-                case_name=name,
-                create=True,
-                update=properties,
-            ) for name, properties in cases
-        ])
+        case_search_es_setup(domain, cases)
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         case_search_es_teardown()
+        super().tearDownClass()
 
-    def test_query_multiple_domains(self):
+    def test_query_all_domains_in_registry(self):
+        # Domain 1 has access to all three domains
         results = RegistryCaseSearchCriteria(self.domain_1, ['person'], {
+            "name": "Jane",
+        }, self.registry_slug).search_es.values_list("name", "domain")
+        self.assertItemsEqual([
+            ("Jane", self.domain_1),
+            ("Jane", self.domain_1),
+            ("Jane", self.domain_2),
+            ("Jane", self.domain_3),
+        ], results)
+
+    def test_case_property_query(self):
+        results = RegistryCaseSearchCriteria(self.domain_1, ['person'], {
+            "family": "Villanueva",
+        }, self.registry_slug).search_es.values_list("name", "domain")
+        self.assertItemsEqual([
+            ("Jane", self.domain_1),
+            ("Xiomara", self.domain_1),
+            ("Alba", self.domain_1),
+            ("Jane", self.domain_3),
+        ], results)
+
+    def test_subset_of_domains_accessible(self):
+        # Domain 2 has access only to domains 1 and itself
+        results = RegistryCaseSearchCriteria(self.domain_2, ['person'], {
             "name": "Jane",
         }, self.registry_slug).search_es.values_list("name", "domain")
         self.assertItemsEqual([
@@ -92,8 +122,14 @@ class TestCaseSearchRegistry(TestCase):
             ("Jane", self.domain_2),
         ], results)
 
-    def test_invalid_registry(self):
-        pass
+    def test_in_registry_but_no_grants(self):
+        # Domain 3 has access only to its own cases
+        results = RegistryCaseSearchCriteria(self.domain_3, ['person'], {
+            "name": "Jane",
+        }, self.registry_slug).search_es.values_list("name", "domain")
+        self.assertItemsEqual([
+            ("Jane", self.domain_3),
+        ], results)
 
     def test_case_type_not_in_registry(self):
         pass
