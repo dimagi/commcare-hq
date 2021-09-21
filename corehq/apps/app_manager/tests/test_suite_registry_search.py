51dd61792d8f9001a491ca0429684231bce21901
Simon Kelly <skelly@dimagi.com>
2021-09-21 12:23:27 +0200

    add test for form linking

--- corehq/apps/app_manager/tests/test_suite_registry_search.py
+++ corehq/apps/app_manager/tests/test_suite_registry_search.py
@@ -1,16 +1,17 @@
 from django.test import SimpleTestCase
 
+from corehq.apps.app_manager.const import WORKFLOW_FORM
 from corehq.apps.app_manager.models import (
     Application,
     CaseSearch,
     CaseSearchProperty,
     Module,
-    AdditionalRegistryQuery,
+    AdditionalRegistryQuery, DetailColumn, FormLink,
 )
 from corehq.apps.app_manager.tests.util import (
     SuiteMixin,
     TestXmlMixin,
-    patch_get_xform_resource_overrides,
+    patch_get_xform_resource_overrides, get_simple_form,
 )
 from corehq.apps.builds.models import BuildSpec
 from corehq.util.test_utils import flag_enabled
@@ -20,14 +21,14 @@ DOMAIN = 'test_domain'
 
 @patch_get_xform_resource_overrides()
 class RemoteRequestSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
-    file_path = ('data', 'suite')
+    file_path = ('data', 'suite_registry')
 
     def setUp(self):
         self.app = Application.new_app(DOMAIN, "Untitled Application")
         self.app._id = '123'
         self.app.build_spec = BuildSpec(version='2.35.0', build_number=1)
         self.module = self.app.add_module(Module.new_module("Untitled Module", None))
-        self.form = self.app.new_form(0, "Untitled Form", None)
+        self.form = self.app.new_form(0, "Untitled Form", None, attachment=get_simple_form("xmlns1.0"))
         self.form.requires = 'case'
         self.module.case_type = 'case'
 
@@ -36,16 +37,17 @@ class RemoteRequestSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
                 CaseSearchProperty(name='name', label={'en': 'Name'}),
                 CaseSearchProperty(name='dob', label={'en': 'Date of birth'})
             ],
+            data_registry="myregistry"
         )
 
         # wrap to have assign_references called
         self.app = Application.wrap(self.app.to_json())
         # reset to newly wrapped module
         self.module = self.app.modules[0]
+        self.form = self.module.forms[0]
 
     @flag_enabled('USH_CASE_CLAIM_UPDATES')
     def test_search_data_registry(self, *args):
-        self.module.search_config.data_registry = "myregistry"
         suite = self.app.create_suite()
 
         expected_entry_query = """
@@ -75,7 +77,6 @@ class RemoteRequestSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
 
     @flag_enabled('USH_CASE_CLAIM_UPDATES')
     def test_search_data_registry_additional_registry_query(self, *args):
-        self.module.search_config.data_registry = "myregistry"
         base_xpath = "instance('registry')/results/case[@case_id=instance('commcaresession')/session/data/case_id]"
         self.module.search_config.additional_registry_queries = [
             AdditionalRegistryQuery(
@@ -100,3 +101,14 @@ class RemoteRequestSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
         # assert that session and registry instances are added to the entry
         self.assertXmlHasXpath(suite, "./entry[1]/instance[@id='commcaresession']")
         self.assertXmlHasXpath(suite, "./entry[1]/instance[@id='registry']")
+
+    def test_form_linking_with_registry_module(self, *args):
+        self.form.post_form_workflow = WORKFLOW_FORM
+        self.form.form_links = [
+            FormLink(xpath="(today() - dob) &lt; 7", module_unique_id=self.module.unique_id)
+        ]
+        self.assertXmlPartialEqual(
+            self.get_xml('form_link_followup_module_registry'),
+            self.app.create_suite(),
+            "./entry[1]"
+        )
