2abccb3c70dc4eb4bf4ef69128785f00d003ac06
Amit Phulera <amitphulera999@gmail.com>
2021-09-22 12:13:52 +0530

    refactor: move other properties below columns and update max_length

--- corehq/motech/repeaters/models.py
+++ corehq/motech/repeaters/models.py
@@ -256,27 +256,27 @@ class SQLCaseRepeater(SyncSQLToCouchMixin, SQLRepeater):
     Record that cases should be repeated to a new url
 
     """
-
-    payload_generator_classes = (CaseRepeaterXMLPayloadGenerator, CaseRepeaterJsonPayloadGenerator)
-
     version = models.CharField(
         max_length=10,
         choices=list(zip(LEGAL_VERSIONS, LEGAL_VERSIONS)),
         default=V2,
     )
     white_listed_case_types = ArrayField(
-        models.CharField(max_length=128, default=list)
-    )
-    black_listed_users = ArrayField(
-        models.CharField(max_length=128, default=list)
+        models.CharField(max_length=255, default=list)
     )
     # users who caseblock submissions should be ignored
-    friendly_name = _("Forward Cases")
+    black_listed_users = ArrayField(
+        models.CharField(max_length=255, default=list)
+    )
 
     format = models.CharField(max_length=16)
 
     class Meta:
         db_table = 'repeaters_caserepeater'
+
+    friendly_name = _("Forward Cases")
+
+    payload_generator_classes = (CaseRepeaterXMLPayloadGenerator, CaseRepeaterJsonPayloadGenerator)
 
     def allowed_to_forward(self, payload):
         return self._allowed_case_type(payload) and self._allowed_user(payload)
