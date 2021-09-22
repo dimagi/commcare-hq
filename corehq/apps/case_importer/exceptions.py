0443705e2aee396de1013a172217feab1878b040
Karen Tracey <kmtracey@caktusgroup.com>
2021-09-22 10:56:33 -0400

    Fix typo in exception title.

--- corehq/apps/case_importer/exceptions.py
+++ corehq/apps/case_importer/exceptions.py
@@ -97,7 +97,7 @@ class InvalidDate(CaseRowError):
 
 
 class InvalidSelectValue(CaseRowError):
-    title = ugettext_noop('Unexpected muliple choice value')
+    title = ugettext_noop('Unexpected multiple choice value')
     message = ugettext_lazy(
         "Multiple choice values were specified that are not listed "
         "in the valid values defined in the property's data dictionary."
