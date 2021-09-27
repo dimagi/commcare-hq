0473702e275a19a9f776b8bffbbcb4a6986a9cc1
Steph <stephherbers@gmail.com>
2021-09-27 14:03:06 -0400

    run migration in chunks

--- corehq/apps/users/migrations/0037_add_edit_messaging_permission.py
+++ corehq/apps/users/migrations/0037_add_edit_messaging_permission.py
@@ -1,6 +1,7 @@
 
 from django.db import migrations
 
+from dimagi.utils.chunked import chunked
 from corehq.apps.users.models_role import SQLPermission, UserRole
 from corehq.util.django_migrations import skip_on_fresh_install
 
@@ -11,8 +12,9 @@ def migrate_edit_migrations_permissions(apps, schema_editor):
     edit_data_permission = SQLPermission.objects.get(value='edit_data')
     role_ids_with_edit_data = set(UserRole.objects.filter(rolepermission__permission_fk_id=edit_data_permission.id)
                                   .values_list("id", flat=True))
-    for role in UserRole.objects.filter(id__in=role_ids_with_edit_data):
-        role.rolepermission_set.get_or_create(permission_fk=permission, defaults={"allow_all": True})
+    for chunk in chunked(role_ids_with_edit_data, 50):
+        for role in UserRole.objects.filter(id__in=chunk):
+            role.rolepermission_set.get_or_create(permission_fk=permission, defaults={"allow_all": True})
 
 
 class Migration(migrations.Migration):
