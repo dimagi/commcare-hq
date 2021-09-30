2fae89d5a8969b16493b5fb9951859e6568940d5
Ethan Soergel <esoergel@gmail.com>
2021-09-30 09:49:06 -0400

    Include real data

--- corehq/apps/reports/standard/web_user_activity.py
+++ corehq/apps/reports/standard/web_user_activity.py
@@ -1,12 +1,13 @@
 from django.utils.translation import ugettext as _
 from django.utils.translation import ugettext_noop
 
+from corehq.apps.auditcare.models import NavigationEventAudit
 from corehq.apps.reports.datatables import DataTablesColumn, DataTablesHeader
 from corehq.apps.reports.generic import GenericTabularReport
-from corehq.apps.reports.standard import ProjectReport
+from corehq.apps.reports.standard import DatespanMixin, ProjectReport
 
 
-class WebUserActivityReport(GenericTabularReport, ProjectReport):
+class WebUserActivityReport(DatespanMixin, GenericTabularReport, ProjectReport):
     slug = 'web_user_activity'
     name = ugettext_noop("Web User Activity")
 
@@ -19,11 +20,33 @@ class WebUserActivityReport(GenericTabularReport, ProjectReport):
     def headers(self):
         return DataTablesHeader(
             DataTablesColumn(_("Username")),
-            DataTablesColumn(_("Activity")),
+            DataTablesColumn(_("Date")),
+            DataTablesColumn(_("Request Path")),
+            DataTablesColumn(_("View Name")),
+            DataTablesColumn(_("Params")),
+            DataTablesColumn(_("View Kwargs")),
+            DataTablesColumn(_("Status")),
         )
 
     @property
     def rows(self):
-        yield ['test1', 'downloaded something']
-        yield ['test1', 'downloaded something']
-        yield ['test1', 'exported something']
+        events = self._get_navigation_events()
+        for event in events:
+            yield [
+                event.user,
+                event.event_date,
+                event.path,
+                event.view,
+                event.params,
+                event.view_kwargs,
+                event.status_code,
+            ]
+
+    def _get_navigation_events(self):
+        username = self.request.GET.get('username')
+        return NavigationEventAudit.objects.filter(
+            domain=self.domain,
+            user=username,
+            event_date__gt=self.datespan.startdate,
+            event_date__lt=self.datespan.enddate_adjusted,
+        ).select_related('view_fk')
