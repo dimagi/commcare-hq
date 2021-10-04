6e99dcdd9e1cc52495e3e47e770f807a595d0504
Jenny Schweers <orange.jenny@gmail.com>
2021-10-04 14:25:21 -0400

    ugh

--- corehq/apps/cloudcare/views.py
+++ corehq/apps/cloudcare/views.py
@@ -595,7 +595,7 @@ def session_endpoint(request, domain, app_id, endpoint_id):
         from corehq.apps.linked_domain.applications import get_downstream_app_id_map
         id_map = get_downstream_app_id_map(domain)
         if app_id in id_map:
-            if len(id_map[app_id] == 1):
+            if len(id_map[app_id]) == 1:
                 build = _fetch_build(domain, request.couch_user.username, id_map[app_id][0])
         if not build:
             return _fail(_("Could not find application."))
