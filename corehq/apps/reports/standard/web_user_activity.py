5c87c129770788629c762b3da8edf76be951bbcf
Ethan Soergel <esoergel@gmail.com>
2021-10-05 17:07:30 -0400

    Commit to view mapping approach

--- corehq/apps/reports/standard/web_user_activity.py
+++ corehq/apps/reports/standard/web_user_activity.py
@@ -19,48 +19,23 @@ class WebUserActivityReport(GetParamsMixin, DatespanMixin, GenericTabularReport,
         'corehq.apps.reports.filters.simple.SimpleUsername',
     ]
 
-    @cached_property
-    def _reports_only(self):
-        return 'reports' in self.request.GET
-
     @property
     def headers(self):
-        if self._reports_only:
-            return DataTablesHeader(
-                DataTablesColumn(_("Username")),
-                DataTablesColumn(_("Date")),
-                DataTablesColumn(_("Report")),
-            )
         return DataTablesHeader(
             DataTablesColumn(_("Username")),
             DataTablesColumn(_("Date")),
-            DataTablesColumn(_("Request Path")),
-            DataTablesColumn(_("View Name")),
-            DataTablesColumn(_("Params")),
-            DataTablesColumn(_("View Kwargs")),
-            DataTablesColumn(_("Status")),
+            DataTablesColumn(_("Report")),
         )
 
     @property
     def rows(self):
         events = self._get_queryset()[self.pagination.start:self.pagination.end]
         for event in events:
-            if self._reports_only:
-                yield [
-                    event.user,
-                    ServerTime(event.event_date).user_time(self.timezone).ui_string(),
-                    self._get_report_name(event.view_kwargs.get('report_slug')),
-                ]
-            else:
-                yield [
-                    event.user,
-                    event.event_date,
-                    event.path,
-                    event.view,
-                    event.params,
-                    str(event.view_kwargs),
-                    event.status_code,
-                ]
+            yield [
+                event.user,
+                ServerTime(event.event_date).user_time(self.timezone).ui_string(),
+                self._get_report_name(event.view_kwargs.get('report_slug')),
+            ]
 
     @property
     def total_records(self):
@@ -68,16 +43,13 @@ class WebUserActivityReport(GetParamsMixin, DatespanMixin, GenericTabularReport,
 
     def _get_queryset(self):
         username = self.request.GET.get('username')
-        queryset = NavigationEventAudit.objects.filter(
+        return NavigationEventAudit.objects.filter(
             domain=self.domain,
             user=username,
             event_date__gt=self.datespan.startdate,
             event_date__lt=self.datespan.enddate_adjusted,
-        )
-        if self._reports_only:
-            queryset = queryset.filter(
-                view_fk__value= 'corehq.apps.reports.dispatcher.ProjectReportDispatcher')
-        return queryset.select_related('view_fk')
+            view_fk__value= 'corehq.apps.reports.dispatcher.ProjectReportDispatcher',
+        ).select_related('view_fk')
 
     def _get_report_name(self, slug):
         if slug and slug in self._report_names_by_slug:
