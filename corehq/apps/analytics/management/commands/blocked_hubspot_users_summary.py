391ad04d88f6513ef39b9ef567b8601062278f9c
Biyeun Buczyk <bbuczyk@dimagi.com>
2021-10-07 16:11:37 +0200

    command improvements to skip to a domain in the list and start the normal command from there. also option to only check against one domain

--- corehq/apps/analytics/management/commands/blocked_hubspot_users_summary.py
+++ corehq/apps/analytics/management/commands/blocked_hubspot_users_summary.py
@@ -15,50 +15,89 @@ class Command(BaseCommand):
     help = "Manually cleans up blocked Hubspot contacts"
     api_key = None
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-s',
+            '--skip',
+            action='store',
+            dest='skip',
+            help='Skip to this domain name and check domains from there.',
+        )
+        parser.add_argument(
+            '-s',
+            '--domain',
+            action='store',
+            dest='domain',
+            help='Only check this domain.',
+        )
+
     def handle(self, **options):
         self.api_key = settings.ANALYTICS_IDS.get('HUBSPOT_API_KEY', None)
 
         if not self.api_key:
             self.stdout.write("No HubSpot API key found.")
+            return
 
         blocked_domains = get_blocked_hubspot_domains()
-        for domain in blocked_domains:
-            users_not_blocked = {}
-            user_query = UserES().domain(domain).source(['email', 'username'])
 
-            total_users = user_query.count()
-            chunk_size = 30  # Hubspot recommends fewer than 100 emails per request
-            num_chunks = int(math.ceil(float(total_users) / float(chunk_size)))
+        check_domain = options.get('domain')
+        skip_to_domain = options.get('skip')
 
-            for chunk in range(num_chunks):
-                blocked_users = (user_query
-                                 .size(chunk_size)
-                                 .start(chunk * chunk_size)
-                                 .run()
-                                 .hits)
-                blocked_emails = []
-                for user in blocked_users:
-                    username = user.get('username')
-                    user_email = user.get('email')
-                    blocked_emails.append(username)
-                    if user_email and user_email != username:
-                        blocked_emails.append(user_email)
-                    users_not_blocked.update(self.get_blocked_status_for_emails(
-                        list(set(blocked_emails))
-                    ))
-            if users_not_blocked:
-                self.stdout.write(self.style.ERROR(
-                    f"\n\nFound {len(users_not_blocked)} users in "
-                    f"HubSpot who are members of the project {domain} "
-                    f"that is blocking HubSpot data:"
+        if check_domain:
+            if check_domain not in blocked_domains:
+                self.stdout.write(
+                    f"{check_domain} is not a blocked HubSpot domain."
+                )
+                return
+            self.print_domain_summary(check_domain)
+        else:
+            skip = skip_to_domain is not None
+
+            for domain in blocked_domains:
+                if domain == skip_to_domain:
+                    skip = False
+                if skip:
+                    continue
+                self.print_domain_summary(domain)
+
+    def print_domain_summary(self, domain):
+        users_not_blocked = {}
+        user_query = UserES().domain(domain).source(['email', 'username'])
+
+        total_users = user_query.count()
+        chunk_size = 30  # Hubspot recommends fewer than 100 emails per request
+        num_chunks = int(math.ceil(float(total_users) / float(chunk_size)))
+
+        for chunk in range(num_chunks):
+            blocked_users = (user_query
+                             .size(chunk_size)
+                             .start(chunk * chunk_size)
+                             .run()
+                             .hits)
+            blocked_emails = []
+            for user in blocked_users:
+                username = user.get('username')
+                user_email = user.get('email')
+                blocked_emails.append(username)
+                if user_email and user_email != username:
+                    blocked_emails.append(user_email)
+                users_not_blocked.update(self.get_blocked_status_for_emails(
+                    list(set(blocked_emails))
                 ))
-                self.stdout.write("\nEmail\tFirst Conversion")
-                for user, status in users_not_blocked:
-                    self.stdout.write(f"{user}\t{status}")
-            else:
-                self.stdout.write(self.style.SUCCESS(
-                    f"All users in project {domain} are absent on HubSpot."
-                ))
+        if users_not_blocked:
+            self.stdout.write(self.style.ERROR(
+                f"\n\nFound {len(users_not_blocked)} users in "
+                f"HubSpot who are members of the project {domain} "
+                f"that is blocking HubSpot data:"
+            ))
+            self.stdout.write("\nEmail\tFirst Conversion")
+            for user, status in users_not_blocked:
+                self.stdout.write(f"{user}\t{status}")
+            self.stdout.write('\n\n')
+        else:
+            self.stdout.write(self.style.SUCCESS(
+                f"All users in project {domain} are absent on HubSpot."
+            ))
 
     def get_blocked_status_for_emails(self, list_of_emails, retry_num=0):
         try:
