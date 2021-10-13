61a2350349c986785b22b6f0d8a561d7d45c9a90
Amit Phulera <amitphulera999@gmail.com>
2021-10-13 19:30:08 +0530

    initialize popover in JS

--- corehq/apps/userreports/static/userreports/js/widgets.js
+++ corehq/apps/userreports/static/userreports/js/widgets.js
@@ -2,6 +2,7 @@ hqDefine("userreports/js/widgets", [
     'jquery',
 ], function ($) {
     $(function () {
+        $('#data-source-rebuild-btn').popover();
         $(".submit-dropdown-form").click(function (e) {
             e.preventDefault();
             var $form = $("#dropdown-form");
