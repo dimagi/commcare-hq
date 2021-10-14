521aeac270513ca6ccb54e8e999ee740785706af
Daniel Miller <dmiller@dimagi.com>
2021-10-14 16:30:27 -0400

    @flaky_slow: decorator that waits between reruns

--- corehq/util/test_utils.py
+++ corehq/util/test_utils.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 from functools import wraps
 from io import StringIO, open
 from textwrap import indent, wrap
-from time import time
+from time import sleep, time
 from unittest import SkipTest, TestCase
 
 from django.conf import settings
@@ -18,6 +18,7 @@ from django.db import connections
 from django.db.backends import utils
 from django.test import TransactionTestCase
 from django.test.utils import CaptureQueriesContext
+from flaky import flaky
 
 import mock
 
@@ -426,6 +427,7 @@ def timelimit(limit):
         limit = timedelta(seconds=limit)
         return lambda func: timelimit((func, limit))
     func, limit = limit
+
     @wraps(func)
     def time_limit(*args, **kw):
         from corehq.tests.noseplugins.timing import add_time_limit
@@ -830,3 +832,15 @@ def disable_quickcache(test_case=None):
         return self.fn(*args, **kw)
     patch = mock.patch("quickcache.quickcache_helper.QuickCacheHelper.__call__", call)
     return patch if test_case is None else patch(test_case)
+
+
+def flaky_slow(test=None, max_runs=5, min_passes=1, rerun_filter=lambda *a: True):
+    """A flaky test decorator that waits between reruns
+
+    Use for tests that depend on eventual database consistency.
+    """
+    def rerun(*args):
+        sleep(0.5)
+        return rerun_filter(*args)
+    deco = flaky(max_runs=max_runs, min_passes=min_passes, rerun_filter=rerun)
+    return deco if test is None else deco(test)
