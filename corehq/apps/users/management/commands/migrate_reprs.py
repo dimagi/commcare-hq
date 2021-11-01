40f0877e4e9a2f83d22ddba72134e3c77cf22d72
robert-costello <robertcostello81@gmail..com>
2021-11-01 09:50:15 -0400

    fix variable name and check for None values for reprs

--- corehq/apps/users/management/commands/migrate_reprs.py
+++ corehq/apps/users/management/commands/migrate_reprs.py
@@ -13,23 +13,24 @@ class Command(BaseCommand):
     def handle(self):
         records = UserHistory.objects
         for user_history in records.order_by('pk').iterator():
-            try:
-                migrate(user_history)
-            except Exception as e:
-                logging.error(f"{user_history.pk}: {e}")
+            if user_history.changed_by_repr is None or user_history.user_repr is None:
+                try:
+                    migrate(user_history)
+                except Exception as e:
+                    logging.error(f"{user_history.pk}: {e}")
 
 
 def migrate(user_history):
     if user_history.changed_by == SYSTEM_USER_ID:
-        change_by_repr = SYSTEM_USER_ID
+        changed_by_repr = SYSTEM_USER_ID
     else:
-        change_by_repr = user_id_to_username(user_history.changed_by)
+        changed_by_repr = user_id_to_username(user_history.changed_by)
 
     user_repr = user_id_to_username(user_history.user_id)
 
-    if change_by_repr:
-        user_history.change_by_repr = change_by_repr
+    if changed_by_repr:
+        user_history.change_by_repr = changed_by_repr
     if user_repr:
         user_history.user_repr = user_repr
-    if change_by_repr or user_repr:
+    if changed_by_repr or user_repr:
         user_history.save()
