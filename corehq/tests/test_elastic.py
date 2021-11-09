f517150f9422128bc73b8b32b5cd8cc32a630287
Joel Miller <jmiller@dimagi.com>
2021-11-09 13:38:22 -0500

    Write test that confirms scroll query only returns up to 2000 docs.

--- corehq/tests/test_elastic.py
+++ corehq/tests/test_elastic.py
@@ -12,23 +12,21 @@ from corehq.elastic import get_es_new, scroll_query
 from corehq.util.es.interface import ElasticsearchInterface
 
 
-@es_test(index=TEST_ES_INFO, setup_class=True)
+@es_test(index=TEST_ES_INFO)
 class TestElastic(SimpleTestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.index = TEST_ES_INFO.alias
-        cls.doc_type = TEST_ES_INFO.type
-        cls.es = get_es_new()
+    def setUp(self):
+        super().setUp()
+        self.index = TEST_ES_INFO.alias
+        self.doc_type = TEST_ES_INFO.type
+        self.es = get_es_new()
         meta = {"mapping": TEST_ES_MAPPING}
-        if not cls.es.indices.exists(cls.index):
-            cls.es.indices.create(index=cls.index, body=meta)
+        if not self.es.indices.exists(self.index):
+            self.es.indices.create(index=self.index, body=meta)
 
-    @classmethod
-    def tearDownClass(cls):
-        super().tearDownClass()
-        cls.es.indices.delete(cls.index)
+    def tearDown(self):
+        super().tearDown()
+        self.es.indices.delete(self.index)
 
     def test_scroll_query(self):
         docs = [
@@ -43,6 +41,40 @@ class TestElastic(SimpleTestCase):
                 self.assertEqual(result["_source"], indexed[result["_id"]])
             self.assertEqual(len(results), len(indexed))
 
+    def test_scroll_query_returns_over_2x_size_docs(self):
+        """Test that all results are returned in very large scroll queries.
+
+        NOTE: this test is slow (~7sec on an i7-10610U)
+
+        Due to the slowness, this test indexes docs discretely rather than
+        using the `_index_test_docs()` helper in order to be more performant.
+
+        The combination of the following optimizations:
+
+        - Convert test setup/teardown to functions instead of classmethods
+          to allow this test to not clean up after itself (tearDown deletes
+          the entire index).
+        - Discrete document indexing in this method rather than using a
+          contextmanager that deletes the docs after the test completes.
+        - Calling `indices.refresh(...)` once after all docs are indexed rather
+          than specifying a refresh after each doc is indexed.
+
+        reduced the runtime of this test from ~25sec to ~7sec.
+        """
+        docs = []
+        interface = ElasticsearchInterface(self.es)
+        for integer in range((interface.SCROLL_SIZE * 2) + 1):
+            doc_id = uuid.uuid4().hex
+            doc = {"_id": doc_id, "number": integer}
+            interface.index_doc(self.index, self.doc_type, doc_id, doc)
+            docs.append(doc)
+        self.es.indices.refresh(self.index)
+        results = []
+        for result in scroll_query(self.index, {}):
+            results.append(result["_source"])
+        results.sort(key=lambda d: d["number"])
+        self.assertEqual(results, docs)
+
     @contextmanager
     def _index_test_docs(self, index, doc_type, docs):
         interface = ElasticsearchInterface(self.es)
@@ -53,10 +85,11 @@ class TestElastic(SimpleTestCase):
                 doc = dict(doc)  # make a copy
                 doc_id = doc["_id"] = uuid.uuid4().hex
             indexed[doc_id] = doc
-            interface.index_doc(index, doc_type, doc_id, doc,
-                                params={"refresh": True})
+            interface.index_doc(index, doc_type, doc_id, doc)
+        self.es.indices.refresh(index)
         try:
             yield indexed
         finally:
             for doc_id in indexed:
                 self.es.delete(index, doc_type, doc_id)
+            self.es.indices.refresh(index)
