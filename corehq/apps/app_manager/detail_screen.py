dimagi/commcare-hq

commit 691a86245386c04c72aeca81ab35ce6d29a81ccc

use correct instance name for detail tabs

corehq/apps/app_manager/detail_screen.py
@@ -593,8 +593,9 @@ class PropertyXpathGenerator(BaseXpathGenerator):
         if indexes and indexes[0] == 'user':
             case = CaseXPath(UsercaseXPath().case())
         else:
+            instance_name = self.detail.get_instance_name(self.module)
             for index in indexes:
-                case = case.index_id(index).case(instance_name=self.detail.instance_name)
+                case = case.index_id(index).case(instance_name=instance_name)
 
         if property == '#owner_name':
             return self.owner_name(case.property('@owner_id'))

corehq/apps/app_manager/models.py
@@ -138,7 +138,7 @@ from corehq.apps.app_manager.util import (
     module_offers_search,
     save_xform,
     update_form_unique_ids,
-    update_report_module_ids,
+    update_report_module_ids, module_loads_registry_case,
 )
 from corehq.apps.app_manager.xform import XForm
 from corehq.apps.app_manager.xform import parse_xml as _parse_xml
@@ -2010,6 +2010,12 @@ class Detail(IndexedSchema, CaseListLookupMixin):
     pull_down_tile = BooleanProperty()
 
     print_template = DictProperty()
+
+    def get_instance_name(self, module):
+        value_is_the_default = self.instance_name == 'casedb'
+        if value_is_the_default and module_loads_registry_case(module):
+            return RESULTS_INSTANCE
+        return self.instance_name
 
     def get_tab_spans(self):
         '''

corehq/apps/app_manager/suite_xml/sections/details.py
@@ -143,7 +143,7 @@ class DetailContributor(SectionContributor):
                     title=Text(locale_id=id_strings.detail_tab_title_locale(
                         module, detail_type, tab
                     )),
-                    nodeset=self._get_detail_tab_nodeset(detail, tab),
+                    nodeset=self._get_detail_tab_nodeset(module, detail, tab),
                     start=tab_spans[tab.id][0],
                     end=tab_spans[tab.id][1],
                     relevant=tab_relevant,
@@ -223,7 +223,7 @@ class DetailContributor(SectionContributor):
                 for e in custom_variable_elements
             ])
 
-    def _get_detail_tab_nodeset(self, detail, tab):
+    def _get_detail_tab_nodeset(self, module, detail, tab):
         if not tab.has_nodeset:
             return None
 
@@ -232,7 +232,7 @@ class DetailContributor(SectionContributor):
 
         if tab.nodeset_case_type:
             nodeset = CaseTypeXpath(tab.nodeset_case_type)
-            nodeset = nodeset.case(instance_name=detail.instance_name)
+            nodeset = nodeset.case(instance_name=detail.get_instance_name(module))
             nodeset = nodeset.select(CaseXPath().parent_id(),
                                      CaseXPath("current()").property("@case_id"))
             nodeset = nodeset.select("@status", "open")

commit 920cc7c61922f6318234b5e93ec10ad743fc66a1

Fixed test breaking due to DetailColumn.wrap nonsense
691a86245386c04c72aeca81ab35ce6d29a81ccc broke tests due to self.detail not always being defined in
detail_screen.py

corehq/apps/app_manager/detail_screen.py
@@ -592,7 +592,7 @@ class PropertyXpathGenerator(BaseXpathGenerator):
 
         if indexes and indexes[0] == 'user':
             case = CaseXPath(UsercaseXPath().case())
-        else:
+        elif indexes:
             instance_name = self.detail.get_instance_name(self.module)
             for index in indexes:
                 case = case.index_id(index).case(instance_name=instance_name)