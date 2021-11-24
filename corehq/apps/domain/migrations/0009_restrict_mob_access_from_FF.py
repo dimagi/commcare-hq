5d53296e6b3d1cdc06338378ca494a8778d8d291
Addison Dunn <addisondunn@dim-a083.myfiosgateway.com>
2021-11-24 10:27:09 -0500

    migrate flag to domain attr

---
+++ corehq/apps/domain/migrations/0009_restrict_mob_access_from_FF.py
@@ -0,0 +1,25 @@
+from django.db import migrations
+
+from corehq.apps.domain.models import Domain
+from corehq.util.django_migrations import skip_on_fresh_install
+
+from corehq.toggles import RESTRICT_MOBILE_ACCESS
+
+
+@skip_on_fresh_install
+def _enable_restrict_mobile_access(apps, schema_editor):
+    for domain in RESTRICT_MOBILE_ACCESS.get_enabled_domains():
+        domain_obj = Domain.get_by_name(domain)
+        domain_obj.restrict_mobile_access = True
+        domain_obj.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('domain', '0008_use_livequery'),
+    ]
+
+    operations = [
+        migrations.RunPython(_enable_restrict_mobile_access, migrations.RunPython.noop)
+    ]
