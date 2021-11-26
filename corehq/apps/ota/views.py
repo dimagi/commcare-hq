c85d1c5313fcac2f5222f4b5429a576a9acfe4f1
Simon Kelly <skelly@dimagi.com>
2021-11-26 09:16:41 +0200

    restict case search and DR endpoints using feature flags

--- corehq/apps/ota/views.py
+++ corehq/apps/ota/views.py
@@ -98,6 +98,7 @@ def restore(request, domain, app_id=None):
 @csrf_exempt
 @mobile_auth
 @check_domain_migration
+@toggles.SYNC_SEARCH_CASE_CLAIM.required_decorator()
 def search(request, domain):
     return app_aware_search(request, domain, None)
 
@@ -106,6 +107,7 @@ def search(request, domain):
 @csrf_exempt
 @mobile_auth
 @check_domain_migration
+@toggles.SYNC_SEARCH_CASE_CLAIM.required_decorator()
 def app_aware_search(request, domain, app_id):
     """
     Accepts search criteria as GET params, e.g. "https://www.commcarehq.org/a/domain/phone/search/?a=b&c=d"
@@ -129,6 +131,7 @@ def app_aware_search(request, domain, app_id):
 @require_POST
 @mobile_auth
 @check_domain_migration
+@toggles.SYNC_SEARCH_CASE_CLAIM.required_decorator()
 def claim(request, domain):
     """
     Allows a user to claim a case that they don't own.
@@ -408,6 +411,8 @@ def recovery_measures(request, domain, build_id):
 @location_safe_bypass
 @mobile_auth
 @require_GET
+@toggles.SYNC_SEARCH_CASE_CLAIM.required_decorator()
+@toggles.DATA_REGISTRY.required_decorator()
 def registry_case(request, domain, app_id):
     case_ids = request.GET.getlist("case_id")
     case_types = request.GET.getlist("case_type")
