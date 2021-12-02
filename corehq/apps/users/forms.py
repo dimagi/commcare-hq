2e3604a37ba5726bce7943ce3c7078470ef57951
Charl Smit <csmit@dimagi.com>
2021-12-02 11:48:43 +0200

    Parse to list

--- corehq/apps/users/forms.py
+++ corehq/apps/users/forms.py
@@ -1496,6 +1496,6 @@ class UserFilterForm(forms.Form):
             # 2) restrict user access
             domain_membership = self.couch_user.get_domain_membership(self.domain)
             if domain_membership.assigned_location_ids:
-                data['web_user_assigned_location_ids'] = domain_membership.assigned_location_ids
+                data['web_user_assigned_location_ids'] = list(domain_membership.assigned_location_ids)
 
         return data
