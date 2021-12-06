21c155dfd4bf52ada3417713939ddc50f81cb8a1
Steph <stephherbers@gmail.com>
2021-12-06 09:31:56 -0500

    add get_enterprise_controlled_domains function to utils and tests

--- corehq/apps/enterprise/tests/test_enterprise_utils.py
+++ corehq/apps/enterprise/tests/test_enterprise_utils.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.enterprise.tests.utils import create_enterprise_permissions
-from corehq.apps.enterprise.utils import get_enterprise_domains
+from corehq.apps.enterprise.utils import get_enterprise_domains, get_enterprise_controlled_domains
 
 
 class TestEnterpriseUtils(TestCase):
@@ -33,4 +33,14 @@ class TestEnterpriseUtils(TestCase):
         self.assertEqual(
             set(['domain', 'source-domain']),
             set(get_enterprise_domains(self.domain))
+        )
+
+    def test_get_enterprise_controlled_domains(self):
+        self.assertEqual(
+            set(['domain', 'source-domain']),
+            set(get_enterprise_controlled_domains(self.source_domain))
+        )
+        self.assertEqual(
+            set(['domain']),
+            set(get_enterprise_controlled_domains(self.domain))
         )
--- corehq/apps/enterprise/utils.py
+++ corehq/apps/enterprise/utils.py
@@ -8,3 +8,12 @@ def get_enterprise_domains(domain):
     if config.is_enabled and domain in config.domains:
         domain_list.append(config.source_domain)
     return domain_list
+
+
+def get_enterprise_controlled_domains(domain):
+    """Return list of domains containing the input domain and all domains controlled by the input domain"""
+    domain_list = [domain]
+    config = EnterprisePermissions.get_by_domain(domain)
+    if config.is_enabled:
+        domain_list += EnterprisePermissions.get_domains(domain)
+    return domain_list
