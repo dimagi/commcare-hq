dfa5363dcca8a18ae57df3b52849505080ec2d90
Joel Miller <jmiller@dimagi.com>
2021-12-06 20:15:18 -0500

    Improve logging clarity and add support for GitHub workflow grouping

--- scripts/test-django-migrations.sh
+++ scripts/test-django-migrations.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+
+GROUP='0;33'    # gold
+SUCCESS='0;32'  # green
+FAIL='0;31'     # red
+
+
 function main {
     local retcode=0
     check_makemigrations  || retcode=$(( $retcode + 1 ))
     check_migrations_list || retcode=$(( $retcode + 1 ))
+    trap - RETURN  # traps propogate up, clear the RETURN trap
     return $retcode
 }
 
@@ -12,14 +19,15 @@ function main {
 function check_makemigrations {
     # check makemigrations
     local cmd=(./manage.py makemigrations)
-    log_color '0;33' "Checking: ${cmd[*]}"
+    log_group "${cmd[*]}"
+    trap 'echo "::endgroup::"' RETURN
     if ! "${cmd[@]}" --noinput --check; then
         # Changes
-        log_color '0;31' "ERROR: Migrations are missing.  Did you run '${cmd[*]}'?"
+        log_fail "Migrations are missing.  Did you run '${cmd[*]}'?"
         return 1
     fi
     # No changes
-    log_color '0;32' "makemigrations ok"
+    log_success "makemigrations ok"
 }
 
 
@@ -27,17 +35,37 @@ function check_migrations_list {
     # ensure migrations lockfile is consistent with actual migrations list
     local lockfile=migrations.lock
     local cmd=(make migrations)
-    log_color '0;33' "Checking: ${cmd[*]}"
+    log_group "${cmd[*]}"
+    trap 'echo "::endgroup::"' RETURN
     "${cmd[@]}" >/dev/null  # don't output the diff twice
     git update-index -q --refresh
     if ! git diff --exit-code HEAD -- "$lockfile"; then
         # dirty
-        log_color '0;31' "ERROR: Frozen migrations are inconsistent.  Did you run '${cmd[*]}'?"
+        log_fail "Frozen migrations are inconsistent.  Did you run '${cmd[*]}'?"
         #git checkout "$lockfile"  # (don't) clean up
         return 1
     fi
     # clean
-    log_color '0;32' "frozen migrations ok"
+    log_success "frozen migrations ok"
+}
+
+
+function log_group {
+    local group_info="$*"
+    # use GitHub workflow grouping log lines
+    # see: https://docs.github.com/en/actions/learn-github-actions/workflow-commands-for-github-actions#grouping-log-lines
+    echo "::group::Check $group_info"
+    log_color "$GROUP" "Checking $group_info"
+}
+
+
+function log_success {
+    log_color "$SUCCESS" "$@"
+}
+
+
+function log_fail {
+    log_color "$FAIL" "ERROR:" "$@"
 }
 
 
