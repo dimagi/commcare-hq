73d534d55bd3f69e0677d1f61caf40e2f789157c
Simon Kelly <skelly@dimagi.com>
2021-12-14 15:36:44 +0200

    enterprise user filter

--- corehq/apps/reports/filters/controllers.py
+++ corehq/apps/reports/filters/controllers.py
@@ -1,5 +1,6 @@
 from memoized import memoized
 
+from corehq.apps.enterprise.models import EnterprisePermissions
 from corehq.apps.es import GroupES, UserES, groups
 from corehq.apps.locations.models import SQLLocation
 from corehq.apps.reports.const import DEFAULT_PAGE_LIMIT
@@ -277,3 +278,36 @@ class LocationGroupOptionsController(EmwfOptionsController):
             (self.get_groups_size, self.get_groups),
             (self.get_locations_size, self.get_locations),
         ]
+
+
+class EnterpriseUserOptionsController(EmwfOptionsController):
+    """Controller designed to return all users across all enterprise domains when the request
+    is made from the enterprise domain and the request is not location restricted.
+
+    If either of those conditions are false then only users from the current domain are returned
+    """
+    def __init__(self, request, domain, search):
+        super().__init__(request, domain, search, False)
+        # this will include all domains in the enterprise only if the current domain
+        # is the 'source'
+        self.enterprise_domains = list(set(EnterprisePermissions.get_domains(domain)) | {domain})
+
+    @property
+    @memoized
+    def utils(self):
+        return UsersUtils(self.domain)
+
+    @property
+    def data_sources(self):
+        return [
+            (self.get_all_users_size, self.get_all_users),
+        ]
+
+    def active_user_es_query(self, query):
+        if not self.request.can_access_all_locations:
+            return super().active_user_es_query(query)
+        else:
+            search_fields = ["first_name", "last_name", "base_username"]
+            return (UserES()
+                    .domains(self.enterprise_domains)
+                    .search_string_query(query, default_fields=search_fields))
