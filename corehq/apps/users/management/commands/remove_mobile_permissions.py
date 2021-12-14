26b0889e0b4b239a99a2a355544e14dfa22a4e37
Addison Dunn <addisondunn@dim-a083.myfiosgateway.com>
2021-12-14 11:02:35 -0500

    log deletions to CSV files

--- corehq/apps/users/management/commands/remove_mobile_permissions.py
+++ corehq/apps/users/management/commands/remove_mobile_permissions.py
@@ -1,20 +1,26 @@
+from datetime import datetime
+import logging
+import csv
+
 from django.core.management.base import BaseCommand
 
 from corehq.apps.domain.models import Domain
 from corehq.apps.users.models_role import RolePermission, UserRole
 from corehq.toggles import RESTRICT_MOBILE_ACCESS
 
-import logging
-
 logger = logging.getLogger('app_migration')
 logger.setLevel('INFO')
 
 
 class Command(BaseCommand):
-    help = "Removes the mobile access permission from roles not in Domains with" \
-           " the RESTRICT_MOBILE_ACCESS feature flag"
+    help = "Removes the mobile access permission from roles not in Domains with the" \
+           " RESTRICT_MOBILE_ACCESS feature flag and outputs details of deleted RolePermission" \
+           " objects to a csv file."
+
 
     def handle(self, **options):
+        self.outfile = f"Mobile_access_permission_deleted_{datetime.today().strftime('%m-%d-%Y')}.csv"
+
         domains_with_mobile_permission = self._get_domains_with_mobile_permission()
         num_domains_with_their_roles_changed = self._delete_mobile_permissions_from_domains(
             domains_with_mobile_permission)
@@ -23,20 +29,26 @@ class Command(BaseCommand):
 
     def _delete_mobile_permissions_from_domains(self, domains):
         num_domains_with_their_roles_changed = 0
+        csvfile = open(self.outfile, "wt")
+        self.writer = csv.writer(csvfile)
+        self.writer.writerow(["Mobile access permission has been deleted for the following roles."])
+        self.writer.writerow(["Related Role", "Domain"])
         for domain in domains:
             did_delete_permisisons_from_domain = self._delete_mobile_access_permissions_on_domain(domain)
             if did_delete_permisisons_from_domain:
                 num_domains_with_their_roles_changed += 1
+        csvfile.close()
         return num_domains_with_their_roles_changed
 
-    @staticmethod
-    def _delete_mobile_access_permissions_on_domain(domain):
+    def _delete_mobile_access_permissions_on_domain(self, domain):
         affected_roles = UserRole.objects.filter(
             domain=domain, rolepermission__permission_fk__value='access_mobile_endpoints')
         atleast_one_permission_deleted = 0 < len(affected_roles)
         for role in affected_roles:
-            logger.info("Deleting RolePermission object with permission=access_mobile_endpoints"
-            f" and related role {role.name} from domain \"{domain}\"...")
+            self.writer.writerow([
+                role.name,
+                domain
+            ])
         role_ids = [role.id for role in affected_roles]
         RolePermission.objects.filter(
             role__id__in=role_ids,
