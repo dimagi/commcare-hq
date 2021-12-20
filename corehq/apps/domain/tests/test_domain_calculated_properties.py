36a9294c9ca56c448823784f8a59d6957ad3cc95
Graham Herceg <g.a.herceg@gmail.com>
2021-12-20 10:30:08 -0500

    minor refactor for calculated properties related tests

--- corehq/apps/domain/tests/test_domain_calculated_properties.py
+++ corehq/apps/domain/tests/test_domain_calculated_properties.py
@@ -21,46 +21,75 @@ from corehq.elastic import send_to_elasticsearch
 
 
 @es_test
-class DomainCalculatedPropertiesTest(TestCase):
-
+class BaseCalculatedPropertiesTest(TestCase):
     @classmethod
     def setUpClass(cls):
-        super(DomainCalculatedPropertiesTest, cls).setUpClass()
+        super(BaseCalculatedPropertiesTest, cls).setUpClass()
         cls.es = [{
-            'info': i,
+            'info': index_info,
             'instance': get_es_new(),
-        } for i in [CASE_INDEX_INFO, SMS_INDEX_INFO, XFORM_INDEX_INFO, USER_INDEX_INFO]]
+        } for index_info in [CASE_INDEX_INFO, SMS_INDEX_INFO, XFORM_INDEX_INFO, USER_INDEX_INFO]]
+
+        cls.domain = Domain(name='test')
+        cls.domain.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.domain.delete()
+        super(BaseCalculatedPropertiesTest, cls).tearDownClass()
 
     def setUp(self):
-        self.domain = Domain(name='test-b9289e19d819')
-        self.domain.save()
         for es in self.es:
             ensure_index_deleted(es['info'].index)
             initialize_index_and_mapping(es['instance'], es['info'])
-        self._set_up_sms_es()
 
-    def _set_up_sms_es(self):
+    @staticmethod
+    def create_sms_in_es(domain_name, direction):
         sms_doc = {
             '_id': 'some_sms_id',
-            'domain': self.domain.name,
-            'direction': INCOMING,
+            'domain': domain_name,
+            'direction': direction,
             'date': json_format_datetime(datetime.datetime.utcnow()),
             'doc_type': SMS_INDEX_INFO.type,
         }
         send_to_elasticsearch("sms", sms_doc)
         refresh_elasticsearch_index('sms')
+        return sms_doc
 
-    def tearDown(self):
-        self.domain.delete()
+    @staticmethod
+    def delete_sms_in_es(sms_doc):
+        send_to_elasticsearch("sms", sms_doc, delete=True)
+        refresh_elasticsearch_index('sms')
 
-    def test_sanity(self):
+
+class DomainCalculatedPropertiesTest(BaseCalculatedPropertiesTest):
+
+    @classmethod
+    def setUpClass(cls):
+        super(DomainCalculatedPropertiesTest, cls).setUpClass()
+        cls.incoming_sms = cls.create_sms_in_es(cls.domain.name, INCOMING)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.delete_sms_in_es(cls.incoming_sms)
+        super(DomainCalculatedPropertiesTest, cls).tearDownClass()
+
+    def test_calculated_properties_are_serializable(self):
+        all_stats = all_domain_stats()
+        props = calced_props(self.domain, self.domain._id, all_stats)
+        json.dumps(props)
+
+    def test_domain_does_not_have_apps(self):
         all_stats = all_domain_stats()
         props = calced_props(self.domain, self.domain._id, all_stats)
         self.assertFalse(props['cp_has_app'])
-        # ensure serializable
-        json.dumps(props)
 
-    def test_sms(self):
+
+class SmsHelperTest(BaseCalculatedPropertiesTest):
+
+    def test_sms_count(self):
+        sms_doc = self.create_sms_in_es(self.domain.name, INCOMING)
+        self.addCleanup(self.delete_sms_in_es, sms_doc)
         self.assertEqual(SMSES().count(), 1)
         self.assertEqual(sms(self.domain.name, INCOMING), 1)
         self.assertEqual(sms(self.domain.name, OUTGOING), 0)
