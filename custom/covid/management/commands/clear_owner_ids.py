350a460b0795457fdd59045528c41af124f741c8
Addison Dunn <addisondunn@dim-a083.myfiosgateway.com>
2021-12-22 15:36:06 -0500

    refactoring and checking for blank owner IDs

--- custom/covid/management/commands/clear_owner_ids.py
+++ custom/covid/management/commands/clear_owner_ids.py
@@ -1,7 +1,6 @@
 from xml.etree import cElementTree as ElementTree
 
 from casexml.apps.case.mock import CaseBlock
-from dimagi.utils.chunked import chunked
 
 from corehq.apps.hqcase.utils import submit_case_blocks
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
@@ -16,7 +15,7 @@ class Command(CaseUpdateCommand):
 
     def case_block(self, case):
         blank_owner_id = {'owner_id': '-'}
-        return ElementTree.tostring(CaseBlock.deprecated_init(
+        return ElementTree.tostring(CaseBlock(
             create=False,
             case_id=case.case_id,
             update=blank_owner_id,
@@ -26,20 +25,27 @@ class Command(CaseUpdateCommand):
         case_ids = self.find_case_ids_by_type(domain, case_type)
         accessor = CaseAccessors(domain)
         case_blocks = []
-        errors = []
 
-        print(f"Found {len(case_ids)} {case_type} cases in {domain}")
-
-        for case in accessor.iter_cases(case_ids):
-            case_blocks.append(self.case_block(case))
+        print(f"Found {len(case_ids)} {case_type} cases in {domain}. Proceeding...")
 
         total_cases_updated = 0
-        for chunk in chunked(case_blocks, BATCH_SIZE):
-            submit_case_blocks(chunk, domain, device_id=DEVICE_ID, user_id=user_id)
-            total_cases_updated += len(chunk)
-            print("Updated {} cases on domain {}".format(total_cases_updated, domain))
+        count_already_blank = 0
+        for case in accessor.iter_cases(case_ids):
+            if(case.get_case_property('owner_id') != '-'):
+                case_blocks.append(self.case_block(case))
+            else:
+                count_already_blank += 1
+            if(len(case_blocks) >= BATCH_SIZE):
+                submit_case_blocks(case_blocks, domain, device_id=DEVICE_ID, user_id=user_id)
+                case_blocks = []
+                total_cases_updated += BATCH_SIZE
+
+        if(case_blocks):
+            submit_case_blocks(case_blocks, domain, device_id=DEVICE_ID, user_id=user_id)
+            total_cases_updated += len(case_blocks)
 
         print(f"{total_cases_updated} cases of case type {case_type} in domain {domain} had their owner ID"
-            " field cleared successfully.")
+            f" field cleared successfully. {count_already_blank} cases already had a blank owner ID and were"
+            " skipped.")
 
-        self.log_data(domain, "clear_owner_ids", case_type, len(case_ids), total_cases_updated, errors)
+        self.log_data(domain, "clear_owner_ids", case_type, len(case_ids), total_cases_updated, errors=[])
