eac850edfe03658dfc8c3ad921f6d58cb9d8f1c4
Ethan Soergel <esoergel@gmail.com>
2021-12-28 15:48:28 +0100

    Create test index on demand

--- corehq/apps/test_case_search/administer.py
+++ corehq/apps/test_case_search/administer.py
@@ -1,2 +1,40 @@
+from copy import deepcopy
+from datetime import datetime
+
+from pillowtop.es_utils import (
+    set_index_normal_settings,
+    set_index_reindex_settings,
+)
+
+from corehq.elastic import get_es_new
+from corehq.pillows.mappings.case_mapping import CASE_ES_TYPE
+from corehq.pillows.mappings.case_search_mapping import CASE_SEARCH_INDEX_INFO
+from corehq.pillows.mappings.utils import mapping_from_json
+
+from .const import TEST_INDEX_NAME
+
+
 def reset_test_index():
     print("resetting")
+    es = get_es_new()
+    _delete_and_recreate_index(es)
+
+    set_index_reindex_settings(es, TEST_INDEX_NAME)
+
+    set_index_normal_settings(es, TEST_INDEX_NAME)
+    es.indices.refresh(TEST_INDEX_NAME)
+
+
+def _delete_and_recreate_index(es):
+    if es.indices.exists(TEST_INDEX_NAME):
+        es.indices.delete(TEST_INDEX_NAME)
+
+    meta = deepcopy(CASE_SEARCH_INDEX_INFO.meta)
+    meta['mappings'] = {CASE_ES_TYPE: _get_mapping()}
+    es.indices.create(index=TEST_INDEX_NAME, body=meta)
+
+
+def _get_mapping():
+    mapping = mapping_from_json('test_case_search_mapping.json')
+    mapping['_meta']['created'] = datetime.utcnow().isoformat()
+    return mapping
---
+++ corehq/apps/test_case_search/const.py
@@ -0,0 +1,1 @@
+TEST_INDEX_NAME = "new_case_search_test"
