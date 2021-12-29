b0a72d4cd15b53003f11aa77a7e9ae48cd5318b9
Ethan Soergel <esoergel@gmail.com>
2021-12-29 13:14:21 +0100

    Add some basic fixture data for easier iteration

--- corehq/apps/test_case_search/administer.py
+++ corehq/apps/test_case_search/administer.py
@@ -15,7 +15,8 @@ from corehq.pillows.mappings.case_mapping import CASE_ES_TYPE
 from corehq.pillows.mappings.case_search_mapping import CASE_SEARCH_INDEX_INFO
 from corehq.pillows.mappings.utils import mapping_from_json
 
-from .const import TEST_INDEX_NAME
+from .const import TEST_DOMAIN_NAME, TEST_INDEX_NAME
+from .fixture_data import CASES_FIXTURE
 
 
 def reset_test_index():
@@ -26,6 +27,7 @@ def reset_test_index():
     meta = deepcopy(CASE_SEARCH_INDEX_INFO.meta)
     meta['mappings'] = {CASE_ES_TYPE: _get_mapping()}
     es.indices.create(index=TEST_INDEX_NAME, body=meta)
+    load_case_fixtures(es)
 
 
 def _get_mapping():
@@ -44,10 +46,7 @@ def load_domain(domain):
 
     with _bulk_indexing_settings(es):
         for case in accessor.iter_cases(case_ids):
-            doc_id = case.case_id
-            doc = transform_case_for_elasticsearch(case.to_json())
-            doc_id = doc.pop('_id')  # You can't send _id directly
-            es.index(TEST_INDEX_NAME, CASE_ES_TYPE, doc, id=doc_id)
+            transform_and_send(es, case.to_json())
 
 
 @contextmanager
@@ -58,7 +57,20 @@ def _bulk_indexing_settings(es):
     es.indices.refresh(TEST_INDEX_NAME)
 
 
+def transform_and_send(es, doc_dict):
+    doc = transform_case_for_elasticsearch(doc_dict)
+    doc_id = doc.pop('_id')  # You can't send _id directly
+    es.index(TEST_INDEX_NAME, CASE_ES_TYPE, doc, id=doc_id)
+
+
 def transform_case_for_elasticsearch(doc_dict):
     # This is where we'd make any just-in-time modifications in python before
     # sending cases to ES
     return old_transform(doc_dict)
+
+
+def load_case_fixtures(es):
+    print(f"loading fixture data into domain {TEST_DOMAIN_NAME}")
+    with _bulk_indexing_settings(es):
+        for case in CASES_FIXTURE:
+            transform_and_send(es, case.to_json())
--- corehq/apps/test_case_search/const.py
+++ corehq/apps/test_case_search/const.py
@@ -1,1 +1,2 @@
 TEST_INDEX_NAME = "new_case_search_test"
+TEST_DOMAIN_NAME = "new_case_search_test_domain"
