cb6e910ab1cdd4046632db8be8d8c7944f26bb54
mkangia <mkangia@dimagi.com>
2021-12-31 19:15:28 +0530

    sets up validators for samveg

---
+++ custom/samveg/commcare_extensions.py
@@ -0,0 +1,14 @@
+from corehq.apps.case_importer.extension_points import custom_case_upload_file_checks
+
+validators = [
+]
+
+
+@custom_case_upload_file_checks.extend(domains=["samveg"])
+def samveg_case_upload_checks(domain, case_upload):
+    errors = []
+    with case_upload.get_spreadsheet() as spreadsheet:
+        for validator in validators:
+            if hasattr(validator, 'validate'):
+                errors.extend(getattr(validator, 'validate')(spreadsheet))
+    return errors
--- settings.py
+++ settings.py
@@ -1088,7 +1088,8 @@ DEFAULT_COMMCARE_EXTENSIONS = [
     "custom.abt.commcare_extensions",
     "custom.eqa.commcare_extensions",
     "mvp.commcare_extensions",
-    "custom.nutrition_project.commcare_extensions"
+    "custom.nutrition_project.commcare_extensions",
+    "custom.samveg.commcare_extensions",
 ]
 COMMCARE_EXTENSIONS = []
 
