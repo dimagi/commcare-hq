01766f9239128f541d57df39e6522da3e2239974
Graham Herceg <g.a.herceg@gmail.com>
2022-01-03 10:54:18 -0800

    add proper setup/teardown for test_case_history class

--- corehq/ex-submodules/casexml/apps/case/tests/test_case_history.py
+++ corehq/ex-submodules/casexml/apps/case/tests/test_case_history.py
@@ -3,11 +3,35 @@ from django.test import TestCase
 from casexml.apps.case.mock import CaseFactory, CaseStructure
 from casexml.apps.case.tests.util import delete_all_cases, delete_all_xforms
 from casexml.apps.case.util import get_case_history
+from pillowtop.es_utils import initialize_index_and_mapping
+
+from corehq.apps.es.tests.utils import es_test
+from corehq.elastic import get_es_new
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+from corehq.pillows.mappings import CASE_INDEX_INFO, XFORM_INDEX_INFO
+from corehq.util.elastic import ensure_index_deleted
 
 
+@es_test
 class TestCaseHistory(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestCaseHistory, cls).setUpClass()
+        cls.es = [{
+            'info': index_info,
+            'instance': get_es_new(),
+        } for index_info in [CASE_INDEX_INFO, XFORM_INDEX_INFO]]
+
+    @classmethod
+    def tearDownClass(cls):
+        for es in cls.es:
+            ensure_index_deleted(es['info'].index)
+        super(TestCaseHistory, cls).tearDownClass()
+
     def setUp(self):
+        for es in self.es:
+            ensure_index_deleted(es['info'].index)
+            initialize_index_and_mapping(es['instance'], es['info'])
         self.domain = "isildur"
         self.factory = CaseFactory(self.domain)
         self.case = self.factory.create_case(owner_id='owner', case_name="Aragorn", update={"prop_1": "val1"})
