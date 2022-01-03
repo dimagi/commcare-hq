cc0c5ba6dbad9de0df937477ecbb9b7c3573eee3
Jennifer Schweers <orange.jenny@gmail.com>
2022-01-03 11:27:37 -0500

    Updated search and list fields for SyncLogSQLAdmin

--- corehq/ex-submodules/casexml/apps/phone/admin.py
+++ corehq/ex-submodules/casexml/apps/phone/admin.py
@@ -5,8 +5,8 @@ from .models import *
 class SyncLogSQLAdmin(admin.ModelAdmin):
     model = SyncLogSQL
     list_display = ['synclog_id', 'domain', 'user_id', 'date']
-    list_filter = ['domain', 'user_id', 'date']
-    search_fields = ['domain', 'user_id']
+    list_filter = ['date']
+    search_fields = ['domain', 'user_id', 'auth_type']
 
 
 admin.site.register(SyncLogSQL, SyncLogSQLAdmin)
