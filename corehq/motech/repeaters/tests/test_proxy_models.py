b1496d9f012e9559e30dc80733f6cbaaae5e7a79
Amit Phulera <amitphulera999@gmail.com>
2022-01-07 14:37:00 +0530

    add tests for remaining model classes

--- corehq/motech/repeaters/tests/test_proxy_models.py
+++ corehq/motech/repeaters/tests/test_proxy_models.py
@@ -106,11 +106,48 @@ class TestSQLCreateCaseRepeaterSubModels(RepeaterProxyTests):
         )
 
 
+class TestSQLRepeaterSubClasses(RepeaterProxyTests):
+    def setUp(self):
+        super().setUp()
+        appstructure_repeater_obj = SQLAppStructureRepeater(
+            domain=DOMAIN,
+            connection_settings=self.conn,
+            is_paused=False,
+            format='app_structure_xml',
+        )
+        shortform_repeater_obj = SQLShortFormRepeater(
+            domain=DOMAIN,
+            connection_settings=self.conn,
+            is_paused=False,
+            format='short_form_json',
+        )
+        user_repeater_obj = SQLUserRepeater(
+            domain=DOMAIN,
+            connection_settings=self.conn,
+            is_paused=False,
+            format='',
+        )
+        location_repeater_obj = SQLLocationRepeater(
+            domain=DOMAIN,
+            connection_settings=self.conn,
+            is_paused=False,
+            format='',
+        )
+        self.all_repeaters = [
+            appstructure_repeater_obj, shortform_repeater_obj, user_repeater_obj, location_repeater_obj
+        ]
+
+        for r in self.all_repeaters:
+            r.save()
+
+    def test_repeaters_are_synced_to_couch(self):
+        repeaters = get_all_repeater_docs()
+        self.assertEqual(len(repeaters), 4)
         self.assertEqual(
             {r['_id'] for r in repeaters},
-            {self.createcase_repeater_obj.repeater_id, self.case_repeater_obj.repeater_id}
+            {r.repeater_id for r in self.all_repeaters}
         )
         self.assertEqual(
             {Repeater.wrap(r).repeater_type for r in repeaters},
-            {self.createcase_repeater_obj.repeater_type, self.case_repeater_obj.repeater_type}
+            {r.repeater_type for r in self.all_repeaters}
         )
