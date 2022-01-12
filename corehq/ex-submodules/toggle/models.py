cbd12172100923984506cb6d8c3925ea88cee3e3
Jennifer Schweers <orange.jenny@gmail.com>
2022-01-12 11:20:47 -0500

    Updated approach to handling the Toggle ghost needed for caching

--- corehq/ex-submodules/toggle/models.py
+++ corehq/ex-submodules/toggle/models.py
@@ -1,7 +1,4 @@
-from dimagi.ext.couchdbkit import Document
-
-
-# Skeleton to avoid pickling errors due to caching that happened
-# before this class moved to corehq.apps.toggle_ui.models.
-class Toggle(Document):
-    pass
+# Import to avoid pickling errors due to caching of Toggle.get_cached that happened before the class moved.
+# This can be removed 24 hours after its deploy.
+# Practically, that means at least 6 weeks after merge, to allow self-hosting environments to update.
+from corehq.apps.toggle_ui.models import Toggle   # noqa
