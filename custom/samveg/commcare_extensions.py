178c9c1aa2baa98417ea98d33636378852a0a9f1
mkangia <mkangia@dimagi.com>
2022-01-12 18:24:03 +0530

    calls run directly

--- custom/samveg/commcare_extensions.py
+++ custom/samveg/commcare_extensions.py
@@ -27,7 +27,7 @@ def samveg_case_upload_checks(domain, case_upload):
     errors = []
     with case_upload.get_spreadsheet() as spreadsheet:
         for validator in validators:
-            errors.extend(getattr(validator, 'run')(spreadsheet))
+            errors.extend(validator.run(spreadsheet))
     return errors
 
 
@@ -35,8 +35,7 @@ def samveg_case_upload_checks(domain, case_upload):
 def samveg_case_upload_row_operations(domain, row_num, raw_row, fields_to_update, import_context):
     all_errors = []
     for operation in row_level_operations:
-        if hasattr(operation, 'run'):
-            fields_to_update, errors = getattr(operation, 'run')(row_num, raw_row, fields_to_update,
-                                                                 import_context=import_context)
-            all_errors.extend(errors)
+        fields_to_update, errors = operation.run(row_num, raw_row, fields_to_update,
+                                                 import_context=import_context)
+        all_errors.extend(errors)
     return fields_to_update, all_errors
