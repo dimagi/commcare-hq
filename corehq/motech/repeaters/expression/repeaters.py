daf2b3af06fb99b08909fb12ad20fb9c43250ede
Amit Phulera <amitphulera999@gmail.com>
2022-01-19 20:31:15 +0530

    add sql model for caseexpression repeater

--- corehq/motech/repeaters/expression/repeaters.py
+++ corehq/motech/repeaters/expression/repeaters.py
@@ -11,7 +11,7 @@ from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.motech.repeaters.expression.repeater_generators import (
     ExpressionPayloadGenerator,
 )
-from corehq.motech.repeaters.models import Repeater
+from corehq.motech.repeaters.models import OptionValue, Repeater, SQLRepeater
 from corehq.toggles import EXPRESSION_REPEATER
 
 
@@ -54,6 +54,57 @@ class BaseExpressionRepeater(Repeater):
 
 class CaseExpressionRepeater(BaseExpressionRepeater):
     friendly_name = _("Configurable Case Repeater")
+
+    @memoized
+    def payload_doc(self, repeat_record):
+        return CaseAccessors(repeat_record.domain).get_case(repeat_record.payload_id).to_json()
+
+
+class SQLBaseExpressionRepeater(SQLRepeater):
+    """Uses a UCR dict expression to send a generic json response
+    """
+    class Meta:
+        app_label = 'repeaters'
+        proxy = True
+
+    configured_filter = OptionValue()
+    configured_expression = OptionValue()
+    payload_generator_classes = (ExpressionPayloadGenerator,)
+
+    @property
+    @memoized
+    def parsed_filter(self):
+        return FilterFactory.from_spec(self.configured_filter, FactoryContext.empty())
+
+    @property
+    @memoized
+    def parsed_expression(self):
+        return ExpressionFactory.from_spec(self.configured_expression, FactoryContext.empty())
+
+    @classmethod
+    def available_for_domain(cls, domain):
+        return EXPRESSION_REPEATER.enabled(domain)
+
+    def allowed_to_forward(self, payload):
+        payload_json = payload.to_json()
+        return self.parsed_filter(payload_json, EvaluationContext(payload_json))
+
+    @memoized
+    def get_payload(self, repeat_record):
+        return self.generator.get_payload(
+            repeat_record,
+            self.payload_doc(repeat_record),
+            self.parsed_expression,
+        )
+
+
+class SQLCaseExpressionRepeater(SQLBaseExpressionRepeater):
+
+    friendly_name = _("Configurable Case Repeater")
+
+    class Meta:
+        app_label = 'repeaters'
+        proxy = True
 
     @memoized
     def payload_doc(self, repeat_record):
