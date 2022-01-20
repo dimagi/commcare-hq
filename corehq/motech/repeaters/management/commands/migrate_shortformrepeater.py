8500a5b977b33b04f9856a727f3c20536ffaf735
Amit Phulera <amitphulera999@gmail.com>
2022-01-20 21:58:17 +0530

    migrate shortformrepeater

---
+++ corehq/motech/repeaters/management/commands/migrate_shortformrepeater.py
@@ -0,0 +1,25 @@
+from corehq.motech.repeaters.models import SQLShortFormRepeater
+from corehq.motech.repeaters.utils import RepeaterMigrationHelper
+
+
+class Command(RepeaterMigrationHelper):
+
+    @classmethod
+    def couch_doc_type(cls):
+        return 'ShortFormRepeater'
+
+    @classmethod
+    def sql_class(cls):
+        return SQLShortFormRepeater
+
+    @classmethod
+    def _get_string_props(cls):
+        return ['version', 'format']
+
+    def get_sql_options_obj(self, doc):
+        return {
+            "options": {
+                "format": doc.get("format"),
+                "version": doc.get("version")
+            }
+        }
