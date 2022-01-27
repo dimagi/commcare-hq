b8da51bfc8e962751c1a8a513dc4b72524a4c25b
Dominic Ochoa <dominicochoa@dim-a067.myfiosgateway.com>
2022-01-27 12:40:16 -0500

    fixed operator flag logic

--- corehq/apps/case_search/management/commands/compare_case_properties.py
+++ corehq/apps/case_search/management/commands/compare_case_properties.py
@@ -37,17 +37,34 @@ class Command(BaseCommand):
         parser.add_argument('domain')
         parser.add_argument('-ct', '--type', dest='type', type=str)
         parser.add_argument('-cp', '--properties', dest='props', nargs='+', type=str)
-        parser.add_argument('-op', '--operator', dest='operator', type=str)
+        parser.add_argument('-eq', dest='equals', action='store_true')
+        parser.add_argument('-neq', dest='not_equals', action='store_true')
+        parser.add_argument('-gt', dest='greater_than', action='store_true')
+        parser.add_argument('-lt', dest='less_than', action='store_true')
+        parser.add_argument('-gteq', dest='greater_equal', action='store_true')
+        parser.add_argument('-lteq', dest='less_equal', action='store_true')
 
     def handle(self, *args, **kwargs):
         domain = kwargs['domain']
         case_type = kwargs['type'] if kwargs['type'] else None
         props = kwargs['props']
-        operator = kwargs['operator']
+        operators = {
+            "==": kwargs['equals'],
+            "!=": kwargs['not_equals'],
+            ">": kwargs['greater_than'],
+            "<": kwargs['less_than'],
+            ">=": kwargs['greater_equal'],
+            "<=": kwargs['less_equal']
+        }
+        operator_valuelist = list(operators.values())
+        operator_keylist = list(operators.keys())
 
+        if operator_valuelist.count(True) != 1:
+            raise Exception('Exception: only one operator flag may be invoked (-eq, -neq, -gt, -lt, -gteq, -lteq)')
         if len(props) != 2:
-            raise ValueError('Value Error exception thown: 2 arguments are needed for properties flag')
+            raise ValueError('Value Error exception thown: 2 arguments required for properties flag')
 
+        operator = operator_keylist[operator_valuelist.index(True)]
         query_obj = define_query(domain, props, operator, case_type)
         type_str = f"{case_type}s in" if case_type else "All cases in"
 
