8923fe59caaf4dbed7687a3eec495c05ee4766e1
Daniel Miller <dmiller@dimagi.com>
2022-01-28 07:16:02 -0500

    Undo overambitious XFormInstance.objects.get_forms

--- corehq/motech/management/commands/create_repeat_records.py
+++ corehq/motech/management/commands/create_repeat_records.py
@@ -4,7 +4,10 @@ import re
 from django.core.management import CommandError
 from django.core.management.base import BaseCommand
 
-from corehq.form_processor.backends.sql.dbaccessors import CaseAccessorSQL
+from corehq.form_processor.backends.sql.dbaccessors import (
+    CaseAccessorSQL,
+    FormAccessorSQL,
+)
 from corehq.form_processor.models import XFormInstance
 from corehq.motech.repeaters.management.commands.find_missing_repeat_records import (
     get_repeaters_for_type_in_domain,
@@ -77,7 +80,7 @@ class Command(BaseCommand):
                         logger.exception(f"Unable to fetch doc '{doc_id}'")
 
         forms = XFormInstance.objects
-        bulk_accessor = forms.get_forms if options['doc_type'] == 'form' else CaseAccessorSQL.get_cases
+        bulk_accessor = FormAccessorSQL.get_forms if options['doc_type'] == 'form' else CaseAccessorSQL.get_cases
         single_accessor = forms.get_form if options['doc_type'] == 'form' else CaseAccessorSQL.get_case
         for doc_ids in chunked(with_progress_bar(doc_ids), 100):
             for doc in doc_iterator(list(doc_ids)):
