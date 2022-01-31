93390037cdbd9c8ea103b1c295596d202307af41
Biyeun Buczyk <bbuczyk@dimagi.com>
2022-01-31 20:28:22 +0100

    add remaining tests

--- corehq/apps/analytics/tests/test_partner_analytics_utils.py
+++ corehq/apps/analytics/tests/test_partner_analytics_utils.py
@@ -13,6 +13,8 @@ from corehq.apps.analytics.models import (
 )
 from corehq.apps.analytics.utils.partner_analytics import (
     get_number_of_mobile_workers,
+    get_number_of_web_users,
+    get_number_of_submissions,
     generate_monthly_mobile_worker_statistics,
     track_partner_access,
     generate_monthly_web_user_statistics,
@@ -25,8 +27,18 @@ from corehq.apps.analytics.utils.partner_analytics import (
     ACCESS_ODATA,
 )
 from corehq.apps.domain.models import Domain
-from corehq.apps.es.tests.utils import es_test
+from corehq.apps.users.models import WebUser, Invitation
+from corehq.elastic import get_es_new, send_to_elasticsearch
+from corehq.form_processor.tests.utils import (
+    FormProcessorTestUtils,
+    create_form_for_test,
+)
+from corehq.pillows.mappings import USER_INDEX_INFO, XFORM_INDEX_INFO
+from corehq.pillows.user import transform_user_for_elasticsearch
+from corehq.pillows.xform import transform_xform_for_elasticsearch
+from corehq.util.elastic import ensure_index_deleted
 from dimagi.utils.dates import get_start_and_end_dates_of_month
+from pillowtop.es_utils import initialize_index_and_mapping
 
 
 def _get_fake_number_of_mobile_workers(domain, _year, _month):
@@ -53,38 +65,118 @@ def _get_fake_number_of_submissions(domain, _year, _month):
     }[domain]
 
 
-@es_test
 class TestPartnerAnalyticsDataUtils(TestCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
 
+        cls.es = get_es_new()
+        initialize_index_and_mapping(cls.es, USER_INDEX_INFO)
+        initialize_index_and_mapping(cls.es, XFORM_INDEX_INFO)
+
         cls.date_start, cls.date_end = get_start_and_end_dates_of_month(2021, 11)
         cls.domain = Domain.get_or_create_with_name('test-partner-analytics', is_active=True)
 
-        ## Data for Mobile Workers Tests
-        # test month
+        # Data for Mobile Workers Tests
         DomainUserHistory.objects.create(
             domain=cls.domain.name,
             record_date=cls.date_end,
             num_users=3
         )
-        # previous month
         DomainUserHistory.objects.create(
             domain=cls.domain.name,
             record_date=cls.date_start - datetime.timedelta(days=1),
             num_users=6
         )
-        # next month
         DomainUserHistory.objects.create(
             domain=cls.domain.name,
             record_date=cls.date_end + datetime.timedelta(days=1),
             num_users=9
         )
 
+        # Data for Web Users tests
+        cls.users = [
+            WebUser.create(
+                cls.domain.name, 'user1@partner.org', 'testpwd', None, None,
+                date=cls.date_start + datetime.timedelta(days=5)
+            ),
+            WebUser.create(
+                cls.domain.name, 'user2@partner.org', 'testpwd', None, None,
+                date=cls.date_start - datetime.timedelta(days=1)
+            ),
+            WebUser.create(
+                cls.domain.name, 'user3@partner.org', 'testpwd', None, None,
+                date=cls.date_end + datetime.timedelta(days=1)
+            ),
+            WebUser.create(
+                cls.domain.name, 'user4@partner.org', 'testpwd', None, None,
+                date=cls.date_start + datetime.timedelta(days=6)
+            ),
+        ]
+        for user in cls.users:
+            elastic_user = transform_user_for_elasticsearch(user.to_json())
+            send_to_elasticsearch('users', elastic_user)
+
+        invitations = [
+            Invitation.objects.create(
+                domain=cls.domain.name,
+                email='user5@partner.org',
+                invited_by='user1@partner.org',
+                invited_on=cls.date_start + datetime.timedelta(days=5),
+            ),
+            Invitation.objects.create(
+                domain=cls.domain.name,
+                email='user6@partner.org',
+                invited_by='user1@partner.org',
+                invited_on=cls.date_start - datetime.timedelta(days=1),
+            ),
+            Invitation.objects.create(
+                domain=cls.domain.name,
+                email='user7@partner.org',
+                invited_by='user1@partner.org',
+                invited_on=cls.date_end + datetime.timedelta(days=1),
+            ),
+        ]
+        for invitation in invitations:
+            invitation.is_accepted = True
+            invitation.save()
+
+        # Data for forms tests
+        forms = [
+            create_form_for_test(
+                cls.domain.name,
+                received_on=cls.date_start - datetime.timedelta(days=1)
+            ),
+            create_form_for_test(
+                cls.domain.name,
+                received_on=cls.date_end + datetime.timedelta(days=1)
+            ),
+            create_form_for_test(
+                cls.domain.name,
+                received_on=cls.date_start + datetime.timedelta(days=3)
+            ),
+            create_form_for_test(
+                cls.domain.name,
+                received_on=cls.date_start + datetime.timedelta(days=12)
+            ),
+        ]
+        for form in forms:
+            elastic_form = transform_xform_for_elasticsearch(form.to_json())
+            send_to_elasticsearch('forms', elastic_form)
+
+        cls.es.indices.refresh(USER_INDEX_INFO.alias)
+        cls.es.indices.refresh(XFORM_INDEX_INFO.alias)
+
     @classmethod
     def tearDownClass(cls):
+        ensure_index_deleted(USER_INDEX_INFO.alias)
+        ensure_index_deleted(XFORM_INDEX_INFO.alias)
+        for user in cls.users:
+            user.delete(cls.domain.name, None)
+        FormProcessorTestUtils.delete_all_sql_forms(cls.domain.name)
+        FormProcessorTestUtils.delete_all_sql_cases(cls.domain.name)
+        Invitation.objects.all().delete()
         DomainUserHistory.objects.all().delete()
         cls.domain.delete()
         super().tearDownClass()
@@ -95,6 +187,22 @@ class TestPartnerAnalyticsDataUtils(TestCase):
                 self.domain.name, self.date_start.year, self.date_start.month
             ),
             3
+        )
+
+    def test_get_number_of_web_users(self):
+        self.assertEqual(
+            get_number_of_web_users(
+                self.domain.name, self.date_start.year, self.date_start.month
+            ),
+            4
+        )
+
+    def test_get_number_of_submissions(self):
+        self.assertEqual(
+            get_number_of_submissions(
+                self.domain.name, self.date_start.year, self.date_start.month
+            ),
+            2
         )
 
 
