e2b58ab3c5d79d23953553a3f8b0d2fc2ab8eb4e
Graham Herceg <g.a.herceg@gmail.com>
2022-02-02 17:08:34 -0800

    fix TestAccessToLinkedProjects tests

--- corehq/tabs/tests.py
+++ corehq/tabs/tests.py
@@ -19,33 +19,53 @@ from corehq.util.test_utils import flag_enabled
 @patch('corehq.apps.users.models.CouchUser')
 class TestAccessToLinkedProjects(SimpleTestCase):
 
-    def test_get_feature_flag_items_returns_none(self, mock_user):
+    def setUp(self):
+        super().setUp()
+
+        access_patcher = patch('corehq.tabs.tabclasses.can_domain_access_release_management')
+        self.mock_can_access = access_patcher.start()
+        self.addCleanup(access_patcher.stop)
+
+        privilege_patcher = patch('corehq.tabs.tabclasses.domain_has_privilege')
+        self.mock_domain_has_privilege = privilege_patcher.start()
+        self.addCleanup(privilege_patcher.stop)
+
+    def test_returns_empty_if_no_release_management_access_and_is_admin_but_no_toggle(self, mock_user):
         mock_user.is_domain_admin.return_value = True
-        with patch('corehq.tabs.tabclasses.domain_has_privilege') as mock_domain_has_privilege:
-            mock_domain_has_privilege.return_value = False
-            items = _get_feature_flag_items("domain", mock_user)
+        self.mock_can_access.return_value = False
+
+        items = _get_feature_flag_items("domain", mock_user)
 
         self.assertFalse(items)
 
     @flag_enabled('LINKED_DOMAINS')
-    def test_get_feature_flag_items_returns_some(self, mock_user):
-        mock_user.is_domain_admin.return_value = True
-        with patch('corehq.tabs.tabclasses.domain_has_privilege') as mock_domain_has_privilege, \
-             patch('corehq.tabs.tabclasses.reverse') as mock_reverse:
-            mock_domain_has_privilege.return_value = False
-            mock_reverse.return_value = 'dummy_url'
-            items = _get_feature_flag_items("domain", mock_user)
+    def test_returns_empty_if_no_release_management_access_and_toggle_but_not_admin(self, mock_user):
+        mock_user.is_domain_admin.return_value = False
+        self.mock_can_access.return_value = False
 
-        self.assertTrue(items)
-
-    @flag_enabled('LINKED_DOMAINS')
-    def test_get_feature_flag_items_returns_none_if_domain_has_release_management_privilege(self, mock_user):
-        mock_user.is_domain_admin.return_value = True
-        with patch('corehq.tabs.tabclasses.domain_has_privilege') as mock_domain_has_privilege:
-            mock_domain_has_privilege.return_value = True
-            items = _get_feature_flag_items("domain", mock_user)
+        items = _get_feature_flag_items("domain", mock_user)
 
         self.assertFalse(items)
+
+    @flag_enabled('LINKED_DOMAINS')
+    def test_returns_empty_if_is_admin_and_has_toggle_but_can_access_release_management_privilege(self, mock_user):
+        mock_user.is_domain_admin.return_value = True
+        self.mock_can_access.return_value = True
+
+        items = _get_feature_flag_items("domain", mock_user)
+
+        self.assertFalse(items)
+
+    @flag_enabled('LINKED_DOMAINS')
+    def test_returns_items_if_toggle_enabled_with_no_release_management_access_and_is_admin(self, mock_user):
+        mock_user.is_domain_admin.return_value = True
+        self.mock_can_access.return_value = False
+
+        with patch('corehq.tabs.tabclasses.reverse', return_value='dummy_url'):
+            items = _get_feature_flag_items("domain", mock_user)
+
+        self.assertEqual(items[0]['title'], 'Linked Project Spaces')
+        self.assertEqual(items[1]['title'], 'Linked Project Space History')
 
 
 class TestAccessToReleaseManagementTab(SimpleTestCase):
