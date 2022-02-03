dimagi/commcare-hq

commit 5cdc537484fe6db18f533bafef70256caa861281

CommCareCase.objects.get_case(case_id[, domain])

corehq/apps/users/models.py
@@ -81,6 +81,7 @@ from corehq.apps.users.util import (
 from corehq.form_processor.exceptions import CaseNotFound
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.form_processor.interfaces.supply import SupplyInterface
+from corehq.form_processor.models import CommCareCase
 from corehq.util.dates import get_timestamp
 from corehq.util.models import BouncedEmail
 from corehq.util.quickcache import quickcache
@@ -2225,7 +2226,7 @@ class CommCareUser(CouchUser, SingleMembershipMixin, CommCareMobileContactMixin)
         """
         try:
             from corehq.apps.commtrack.util import location_map_case_id
-            return CaseAccessors(self.domain).get_case(location_map_case_id(self))
+            return CommCareCase.objects.get_case(location_map_case_id(self, self.domain))
         except CaseNotFound:
             return None
 

corehq/apps/api/models.py
@@ -12,7 +12,7 @@ from django.http import HttpResponse
 from couchforms import const
 
 from corehq.apps.api.resources import DictObject
-from corehq.form_processor.models import CommCareCaseIndex
+from corehq.form_processor.models import CommCareCase, CommCareCaseIndex
 from corehq.form_processor.models.cases import CaseToXMLMixin, get_index_map
 from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
 from corehq.form_processor.models import XFormInstance
@@ -233,18 +233,18 @@ class ESCase(DictObject, CaseToXMLMixin):
     @property
     def child_cases(self):
         from corehq.apps.api.util import case_to_es_case
-        accessor = CaseAccessors(self.domain)
         return {
-            index.case_id: case_to_es_case(accessor.get_case(index.case_id))
+            index.case_id: case_to_es_case(
+                CommCareCase.objects.get_case(index.case_id, self.domain))
             for index in self._reverse_indices
         }
 
     @property
     def parent_cases(self):
         from corehq.apps.api.util import case_to_es_case
-        accessor = CaseAccessors(self.domain)
         return {
-            index.identifier: case_to_es_case(accessor.get_case(index.referenced_id))
+            index.identifier: case_to_es_case(
+                CommCareCase.objects.get_case(index.referenced_id, self.domain))
             for index in self.indices if index.referenced_id
         }
 

corehq/apps/sms/models.py
@@ -26,7 +26,7 @@ from corehq.apps.sms.mixin import (
     apply_leniency,
 )
 from corehq.apps.users.models import CouchUser
-from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+from corehq.form_processor.models import CommCareCase
 from corehq.util.mixin import UUIDGeneratorMixin
 from corehq.util.quickcache import quickcache
 from corehq.util.view_utils import absolute_reverse
@@ -149,7 +149,7 @@ class Log(models.Model):
     @property
     def recipient(self):
         if self.couch_recipient_doc_type == 'CommCareCase':
-            return CaseAccessors(self.domain).get_case(self.couch_recipient)
+            return CommCareCase.objects.get_case(self.couch_recipient, self.domain)
         else:
             return CouchUser.get_by_user_id(self.couch_recipient)
 
@@ -658,8 +658,7 @@ class PhoneNumber(UUIDGeneratorMixin, models.Model):
     @property
     def owner(self):
         if self.owner_doc_type == 'CommCareCase':
-            from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
-            return CaseAccessors(self.domain).get_case(self.owner_id)
+            return CommCareCase.objects.get_case(self.owner_id, self.domain)
         elif self.owner_doc_type == 'CommCareUser':
             from corehq.apps.users.models import CommCareUser
             return CommCareUser.get(self.owner_id)

corehq/messaging/scheduling/scheduling_partitioned/models.py
@@ -12,7 +12,7 @@ from corehq.apps.users.models import CommCareUser, WebUser, CouchUser
 from corehq.apps.users.util import format_username
 from corehq.form_processor.models import DEFAULT_PARENT_IDENTIFIER
 from corehq.form_processor.exceptions import CaseNotFound
-from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
+from corehq.form_processor.models import CommCareCase
 from corehq.form_processor.utils import is_commcarecase
 from corehq.messaging.scheduling import util
 from corehq.messaging.scheduling.exceptions import UnknownRecipientType
@@ -69,7 +69,7 @@ class ScheduleInstance(PartitionedModel):
     def recipient(self):
         if self.recipient_type == self.RECIPIENT_TYPE_CASE:
             try:
-                case = CaseAccessors(self.domain).get_case(self.recipient_id)
+                case = CommCareCase.objects.get_case(self.recipient_id, self.domain)
             except CaseNotFound:
                 return None
 
@@ -562,7 +562,7 @@ class CaseScheduleInstanceMixin(object):
     @memoized
     def case(self):
         try:
-            return CaseAccessors(self.domain).get_case(self.case_id)
+            return CommCareCase.objects.get_case(self.case_id, self.domain)
         except CaseNotFound:
             return None
 

corehq/motech/repeaters/models.py
@@ -87,8 +87,7 @@ from corehq.apps.locations.models import SQLLocation
 from corehq.apps.users.models import CommCareUser
 from corehq.form_processor.backends.sql.dbaccessors import CaseAccessorSQL
 from corehq.form_processor.exceptions import XFormNotFound
-from corehq.form_processor.interfaces.dbaccessors import CaseAccessors
-from corehq.form_processor.models import XFormInstance
+from corehq.form_processor.models import CommCareCase, XFormInstance
 from corehq.motech.const import (
     ALGO_AES,
     BASIC_AUTH,
@@ -672,7 +671,7 @@ class CaseRepeater(Repeater):
 
     @memoized
     def payload_doc(self, repeat_record):
-        return CaseAccessors(repeat_record.domain).get_case(repeat_record.payload_id)
+        return CommCareCase.objects.get_case(repeat_record.payload_id, repeat_record.domain)
 
     @property
     def form_class_name(self):

commit 07659044f0211e06d42c9df87bf12fd28aa48696

Fix bad parens in get_location_map_case
Bug introduced in 5cdc537484fe6db18f533bafef70256caa861281

corehq/apps/users/models.py
@@ -2226,7 +2226,7 @@ class CommCareUser(CouchUser, SingleMembershipMixin, CommCareMobileContactMixin)
         """
         try:
             from corehq.apps.commtrack.util import location_map_case_id
-            return CommCareCase.objects.get_case(location_map_case_id(self, self.domain))
+            return CommCareCase.objects.get_case(location_map_case_id(self), self.domain)
         except CaseNotFound:
             return None
 