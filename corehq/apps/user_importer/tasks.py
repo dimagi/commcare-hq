e1a8d741797af94308260a4c7d07fa0057b1fba7
minhalee999 <minhalee999@gmail.com>
2022-02-03 13:18:47 -0500

    Update task by task id

--- corehq/apps/user_importer/tasks.py
+++ corehq/apps/user_importer/tasks.py
@@ -2,7 +2,6 @@ import functools
 
 from celery.exceptions import TimeoutError
 from celery.task import task
-from celery.result import AsyncResult
 
 from django.db import DEFAULT_DB_ALIAS
 
@@ -20,25 +19,20 @@ USER_UPLOAD_CHUNK_SIZE = 1000
 
 @task(bind=True)
 def import_users_and_groups(self, domain, user_specs, group_specs, upload_user_id, upload_record_id, is_web_upload,
-                            task=None):
+                            task_id=None):
     from corehq.apps.user_importer.importer import create_or_update_commcare_users_and_groups, \
         create_or_update_groups, create_or_update_web_users
-    if task is None:
-        task = self
-    else:
-        #parallel_import_task is the only place that passes a task argument
-        task = AsyncResult(task)
-    DownloadBase.set_progress(task, 0, 100)
+    DownloadBase.set_progress(self, 0, 100, task_id)
 
     total = len(user_specs) + len(group_specs)
-    DownloadBase.set_progress(task, 0, total)
+    DownloadBase.set_progress(self, 0, total, task_id)
 
     group_memoizer, group_results = create_or_update_groups(domain, group_specs)
 
-    DownloadBase.set_progress(task, len(group_specs), total)
+    DownloadBase.set_progress(self, len(group_specs), total, task_id)
 
     def _update_progress(value, start=0):
-        DownloadBase.set_progress(task, start + value, total)
+        DownloadBase.set_progress(self, start + value, total, task_id)
 
     upload_user = WebUser.get_by_user_id(upload_user_id)
     if is_web_upload:
@@ -63,10 +57,10 @@ def import_users_and_groups(self, domain, user_specs, group_specs, upload_user_i
         'rows': user_results['rows']
     }
     upload_record = UserUploadRecord.objects.using(DEFAULT_DB_ALIAS).get(pk=upload_record_id)
-    upload_record.task_id = task.request.id
+    upload_record.task_id = task_id
     upload_record.result = results
     upload_record.save()
-    DownloadBase.set_progress(task, total, total)
+    DownloadBase.set_progress(self, total, total, task_id)
     return {
         'messages': results
     }
@@ -75,9 +69,9 @@ def import_users_and_groups(self, domain, user_specs, group_specs, upload_user_i
 @task(queue='ush_background_tasks', bind=True)
 def parallel_import_task(self, domain, user_specs, group_specs, upload_user_id,
                          upload_record_id, is_web_user_upload=False):
-    task = self.request.id
+    task_id = self.request.id
     return import_users_and_groups(domain, user_specs, group_specs, upload_user_id, upload_record_id,
-                                   is_web_user_upload, task)
+                                   is_web_user_upload, task_id)
 
 
 @task(queue='ush_background_tasks')
