01bab062c6c4ddb5d1c41be54d71b230c91c1b40
Charl Smit <csmit@dimagi.com>
2022-02-08 22:28:13 +0200

    Revert "Merge to master and solve conflict"
    This reverts commit d1144816107a93505dc4f0bc405733e96b2358c6, reversing
    changes made to cbd41b9d7d55aa18ce6b200988fc1ca932e36f7a.

--- corehq/apps/saved_reports/tests/test_models.py
+++ corehq/apps/saved_reports/tests/test_models.py
@@ -1,25 +1,11 @@
-from django.test import SimpleTestCase, TestCase
+from django.test import SimpleTestCase
 from smtplib import SMTPSenderRefused
 from dimagi.utils.django.email import LARGE_FILE_SIZE_ERROR_CODE
 from unittest.mock import create_autospec, patch, PropertyMock, ANY
 from corehq.apps.reports import views
 from corehq.apps.users.models import WebUser
 from corehq.apps.saved_reports import models
-from ..models import ReportNotification, ReportConfig
-
-
-class TestReportConfig(TestCase):
-
-    def tearDown(self) -> None:
-        self.config.delete()
-
-    def test_report_is_shared_on_domain(self):
-        domain = 'test_domain'
-        self.config = ReportConfig(
-            domain=domain,
-        )
-        self.config.save()
-        self.assertFalse(self.config.is_shared_on_domain)
+from ..models import ReportNotification
 
 
 class TestReportNotification(SimpleTestCase):
