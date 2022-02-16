96d9c8e6998b1cafe863e771bc12bdb18bbb45ae
Daniel Miller <dmiller@dimagi.com>
2022-02-16 12:18:24 -0500

    Remove pygments from HqAdminEmailHandler

--- corehq/util/log.py
+++ corehq/util/log.py
@@ -1,14 +1,10 @@
 import sys
 from collections import defaultdict
-from itertools import islice
 from logging import Filter
 import traceback
 from datetime import timedelta, datetime
 
 from celery._state import get_current_task
-from pygments import highlight
-from pygments.lexers import PythonLexer
-from pygments.formatters import HtmlFormatter
 
 from dimagi.utils.django.email import send_HTML_email as _send_HTML_email
 from django.core import mail
@@ -75,7 +71,6 @@ class HqAdminEmailHandler(AdminEmailHandler):
         request_repr = get_sanitized_request_repr(request)
 
         tb_list = []
-        code = None
         if record.exc_info:
             etype, _value, tb = record.exc_info
             value = clean_exception(_value)
@@ -83,7 +78,6 @@ class HqAdminEmailHandler(AdminEmailHandler):
             formatted_exception = traceback.format_exception_only(etype, value)
             tb_list.extend(formatted_exception)
             extracted_tb = list(reversed(traceback.extract_tb(tb)))
-            code = self.get_code(extracted_tb)
             tb_list.extend(traceback.format_list(extracted_tb))
             stack_trace = '\n'.join(tb_list)
             subject = '%s: %s' % (record.levelname,
@@ -102,7 +96,6 @@ class HqAdminEmailHandler(AdminEmailHandler):
             'tb_list': tb_list,
             'request_repr': request_repr,
             'stack_trace': stack_trace,
-            'code': code,
         })
         if request:
             sanitized_url = sanitize_url(request.build_absolute_uri())
@@ -138,31 +131,6 @@ class HqAdminEmailHandler(AdminEmailHandler):
         if details:
             formatted = '\n'.join('{item[0]}: {item[1]}'.format(item=item) for item in details.items())
             return 'Details:\n{}'.format(formatted)
-
-    @staticmethod
-    def get_code(extracted_tb):
-        try:
-            trace = next((trace for trace in extracted_tb if 'site-packages' not in trace[0]), None)
-            if not trace:
-                return None
-
-            filename = trace[0]
-            lineno = trace[1]
-            offset = 10
-            with open(filename, encoding='utf-8') as f:
-                code_context = list(islice(f, lineno - offset, lineno + offset))
-
-            return highlight(''.join(code_context),
-                PythonLexer(),
-                HtmlFormatter(
-                    noclasses=True,
-                    linenos='table',
-                    hl_lines=[offset, offset],
-                    linenostart=(lineno - offset + 1),
-                )
-            )
-        except Exception as e:
-            return "Unable to extract code. {}".format(e)
 
     @classmethod
     def _clean_subject(cls, subject):
