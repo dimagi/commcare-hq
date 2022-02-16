29f7f1fa86efcc1b6409885f5c10d7eb20be62b2
Jennifer Schweers <orange.jenny@gmail.com>
2022-02-16 09:11:21 -0500

    Added migration

---
+++ corehq/apps/app_manager/management/commands/migrate_case_search_prompt_itemset_ids.py
@@ -0,0 +1,32 @@
+import re
+
+from corehq.apps.app_manager.management.commands.helpers import (
+    AppMigrationCommandBase,
+)
+from corehq.apps.app_manager.util import get_correct_app_class
+from corehq.apps.fixtures.fixturegenerators import ItemListsProvider
+
+
+class Command(AppMigrationCommandBase):
+    help = "One-time migration to add 'item-list:' prefix to instance ids in case search itemset prompts"
+
+    chunk_size = 1
+    include_builds = True
+    include_linked_apps = True
+
+    def migrate_app(self, app_doc):
+        should_save = False
+        for module in app_doc.get('modules', []):
+            if module.get('search_config'):
+                for prop in module.get('search_config').get('properties'):
+                    itemset = prop.get('itemset')
+                    if itemset.get('instance_uri', '').startswith(f'jr://fixture/{ItemListsProvider.id}:'):
+                        instance_id = itemset.get('instance_id')
+                        if instance_id and ItemListsProvider.id not in instance_id:
+                            should_save = True
+                            itemset['instance_id'] = f'{ItemListsProvider.id}:{instance_id}'
+                            itemset['nodeset'] = re.sub(r"instance\((.)" + instance_id,
+                                                     r"instance(\1" + ItemListsProvider.id + r":" + instance_id,
+                                                     itemset.get('nodeset', ''))
+
+        return get_correct_app_class(app_doc).wrap(app_doc) if should_save else None
