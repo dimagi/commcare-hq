d7cada6a13dc6f7277e67c6af924f839f3bd38a2
Graham Herceg <g.a.herceg@gmail.com>
2022-02-18 08:50:27 -0800

    ensure refresh_timed_schedule_instances is called with hex rep

--- corehq/apps/accounting/subscription_changes.py
+++ corehq/apps/accounting/subscription_changes.py
@@ -130,7 +130,7 @@ def get_refresh_alert_schedule_instances_call(broadcast):
 def get_refresh_timed_schedule_instances_call(broadcast):
     def refresh():
         refresh_timed_schedule_instances.delay(
-            broadcast.schedule_id,
+            broadcast.schedule_id.hex,
             broadcast.recipients,
             start_date_iso_string=json_format_date(broadcast.start_date)
         )
