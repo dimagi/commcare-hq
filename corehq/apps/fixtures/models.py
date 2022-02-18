8f5d9e30aa466e3b3f2a6c9e93a7ea801d0c8def
Daniel Miller <dmiller@dimagi.com>
2022-02-18 08:32:08 -0500

    Rename fixture SQL model: Fixture -> LookupTable

--- corehq/apps/fixtures/models.py
+++ corehq/apps/fixtures/models.py
@@ -14,21 +14,26 @@ from .couchmodels import (  # noqa: F401
 )
 
 
-class UserFixtureType:
+class UserLookupTableType:
     LOCATION = 1
     CHOICES = (
         (LOCATION, "Location"),
     )
 
 
-class UserFixtureStatus(models.Model):
+class UserLookupTableStatus(models.Model):
     """Keeps track of when a user needs to re-sync a fixture"""
     user_id = models.CharField(max_length=100, db_index=True)
-    fixture_type = models.PositiveSmallIntegerField(choices=UserFixtureType.CHOICES)
+    fixture_type = models.PositiveSmallIntegerField(choices=UserLookupTableType.CHOICES)
     last_modified = models.DateTimeField()
 
     DEFAULT_LAST_MODIFIED = datetime.min
 
     class Meta:
         app_label = 'fixtures'
+        db_table = 'fixtures_userfixturestatus'
         unique_together = ("user_id", "fixture_type")
+
+
+UserFixtureType = UserLookupTableType
+UserFixtureStatus = UserLookupTableStatus
