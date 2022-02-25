8af6f9102a08412add147aed9fb98bf8458023cf
Jennifer Schweers <orange.jenny@gmail.com>
2022-02-25 16:38:46 -0500

    Updated update_owner_ids to use base's update_cases implementation

--- custom/covid/management/commands/update_owner_ids.py
+++ custom/covid/management/commands/update_owner_ids.py
@@ -1,15 +1,8 @@
-from xml.etree import cElementTree as ElementTree
-
 from casexml.apps.case.mock import CaseBlock
-from dimagi.utils.chunked import chunked
 
-from corehq.apps.hqcase.utils import submit_case_blocks
-from corehq.form_processor.models import CommCareCase
 from corehq.apps.locations.models import SQLLocation
 from custom.covid.management.commands.update_cases import CaseUpdateCommand
 
-BATCH_SIZE = 100
-DEVICE_ID = __name__ + ".update_owner_ids"
 CHILD_LOCATION_TYPE = 'investigators'
 
 
@@ -17,52 +10,34 @@ class Command(CaseUpdateCommand):
     help = f"Changes the owner_id of a case to the location_id of the child location with type " \
            f"{CHILD_LOCATION_TYPE} of the current location"
 
-    def case_block(self, case, child_location):
-        return ElementTree.tostring(CaseBlock.deprecated_init(
-            create=False,
-            case_id=case.case_id,
-            owner_id=child_location.location_id,
-        ).as_xml(), encoding='utf-8').decode('utf-8')
+    logger_name = __name__
+    locations_objects = None
 
-    def update_cases(self, domain, user_id):
-        case_ids = self.find_case_ids(domain)
+    def get_location(self, owner_id):
+        if self.locations_objects is None:
+            self.locations_objects = {}
 
-        locations_objects = {}
-        case_blocks = []
-        errors = []
-        skip_count = 0
-        for case in CommCareCase.objects.iter_cases(case_ids, domain):
-            owner_id = case.get_case_property('owner_id')
-            if owner_id in locations_objects:
-                location_obj = locations_objects[owner_id]
-            else:
-                try:
-                    location_obj = SQLLocation.objects.get(location_id=owner_id)
-                except SQLLocation.DoesNotExist:
-                    errors.append("Location does not exist associated with the owner_id:{}. "
-                                  "Case:{}".format(owner_id, case.case_id))
-                    skip_count += 1
-                    continue
-                locations_objects[owner_id] = location_obj
-            if location_obj:
-                children = location_obj.get_children()
-                has_correct_child_location_type = False
-                for child_location in children:
-                    if child_location.location_type.code == CHILD_LOCATION_TYPE:
-                        case_blocks.append(self.case_block(case, child_location))
-                        has_correct_child_location_type = True
-                        break
-                if not has_correct_child_location_type:
-                    skip_count += 1
-            else:
-                skip_count += 1
-        print(f"{len(case_blocks)} to update in {domain}, {skip_count} cases have skipped due to unknown owner_id"
-              f" or has no child location of type {CHILD_LOCATION_TYPE}.")
+        if owner_id in self.locations_objects:
+            return self.locations_objects[owner_id]
 
-        total = 0
-        for chunk in chunked(case_blocks, BATCH_SIZE):
-            submit_case_blocks(chunk, domain, device_id=DEVICE_ID, user_id=user_id)
-            total += len(chunk)
-            print("Updated {} cases on domain {}".format(total, domain))
+        loc = SQLLocation.objects.get(location_id=owner_id)
+        self.locations_objects[owner_id] = loc
+        return loc
 
-        self.log_data(domain, "update_owner_ids", len(case_ids), total, errors)
+    def case_block(self, case):
+        owner_id = case.get_case_property('owner_id')
+        try:
+            location_obj = self.get_location(owner_id)
+        except SQLLocation.DoesNotExist:
+            self.logger.error("Location does not exist associated with the owner_id:{}. "
+                              "Case:{}".format(owner_id, case.case_id))
+            return None
+
+        children = location_obj.get_children()
+        for child_location in children:
+            if child_location.location_type.code == CHILD_LOCATION_TYPE:
+                return [CaseBlock.deprecated_init(
+                    create=False,
+                    case_id=case.case_id,
+                    owner_id=child_location.location_id,
+                )]
