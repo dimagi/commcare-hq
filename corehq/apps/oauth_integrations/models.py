d31d1a7aeb4e4c3121b41881dea9ce6359b2ee41
Rynhard <rpietersen@dimagi.com>
2022-02-28 12:50:23 +0200

    changed models.py to contain new model

--- corehq/apps/oauth_integrations/models.py
+++ corehq/apps/oauth_integrations/models.py
@@ -1,10 +1,17 @@
 from django.db import models
-from django.db.models.fields import CharField, DateField
 
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 
 class GoogleApiToken(models.Model):
     user = models.ForeignKey(User, related_name='google_api_tokens', on_delete=models.CASCADE)
-    token = CharField(max_length=700)
-    date_created = DateField(auto_now_add=True)
+    token = models.CharField(max_length=700)
+    date_created = models.DateField(auto_now_add=True)
+
+
+class LiveGoogleSheetSchedule(models.Model):
+    export_config_id = models.CharField(length=250, db_index=True)
+    is_active = models.BooleanField(default=True)
+    start_time = models.IntegerField(default=200)
+    google_sheet_id = CharField(length=250)
