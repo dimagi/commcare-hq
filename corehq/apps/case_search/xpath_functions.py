19748a78cde214fe0d7639b632085ad896c0d2fb
Addison Dunn <addisondunn@dim-a083.myfiosgateway.com>
2022-03-02 14:55:22 -0500

    add new selected XPath functions

--- corehq/apps/case_search/xpath_functions.py
+++ corehq/apps/case_search/xpath_functions.py
@@ -3,6 +3,9 @@ import datetime
 from django.utils.dateparse import parse_date
 from django.utils.translation import ugettext as _
 
+from corehq.apps.es.case_search import case_property_query
+from eulxml.xpath.ast import serialize
+
 
 class XPathFunctionException(Exception):
     pass
@@ -35,6 +38,36 @@ def date(node):
     )
 
 
+def selected(node, fuzzy):
+    if len(node.args) != 2:
+        raise XPathFunctionException(_("The \"selected\" function accepts exactly two arguments."))
+    property_name = serialize(node.args[0])
+    search_values = node.args[1]
+    search_mode = None
+    if 1 < len(search_values.split(' ')):
+        search_mode = 'OR'  # This will actually make the query the same as with selected_any
+    return case_property_query(property_name, search_values, fuzzy=fuzzy, mode=search_mode)
+
+
+def selected_any(node, fuzzy):
+    if len(node.args) != 2:
+        raise XPathFunctionException(_("The \"selected-any\" function accepts exactly two arguments."))
+    property_name = serialize(node.args[0])
+    search_values = node.args[1]
+    return case_property_query(property_name, search_values, fuzzy=fuzzy, mode='OR')
+
+
+def selected_all(node, fuzzy):
+    if len(node.args) != 2:
+        raise XPathFunctionException(_("The \"selected-all\" function accepts exactly two arguments."))
+    property_name = serialize(node.args[0])
+    search_values = node.args[1]
+    return case_property_query(property_name, search_values, fuzzy=fuzzy, mode='AND')
+
+
 XPATH_FUNCTIONS = {
     'date': date,
+    'selected': selected,
+    'selected-any': selected_any,
+    'selected-all': selected_all,
 }
