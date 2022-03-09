0fb8cd46496a05ea518e7889eaf539ea3b788cad
Dominic Ochoa <dominicochoa@dim-a067.myfiosgateway.com>
2022-03-09 18:05:09 -0500

    added multi _xpath_query field support for case search

--- corehq/apps/case_search/models.py
+++ corehq/apps/case_search/models.py
@@ -93,7 +93,7 @@ class SearchCriteria:
         disallowed_parameters = [
             CASE_SEARCH_BLACKLISTED_OWNER_ID_KEY,
             'owner_id',
-            CASE_SEARCH_XPATH_QUERY_KEY,
+            # CASE_SEARCH_XPATH_QUERY_KEY,
         ]
 
         if self.key in disallowed_parameters or self.is_daterange:
@@ -154,7 +154,6 @@ def extract_search_request_config(request_dict):
         config_name: params.pop(param_name, None)
         for param_name, config_name in CONFIG_KEYS_MAPPING.items()
     }
-
     criteria = criteria_dict_to_criteria_list(params)
     return CaseSearchRequestConfig(criteria=criteria, **kwargs_from_params)
 
