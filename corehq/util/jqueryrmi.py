cc37df9ad8983138487969bb8eabe9a09bc20ad8
Daniel Miller <dmiller@dimagi.com>
2022-03-15 10:03:46 -0400

    Remove unused and deprecated RMI logic

--- corehq/util/jqueryrmi.py
+++ corehq/util/jqueryrmi.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
+"""jQuery RMI adaptor for Django
+
+Used with [jquery.rmi](https://github.com/dimagi/jquery.rmi).
+
+Original source (dead project):
+https://github.com/jrief/django-angular/blob/666f4ee8e57a3c/djng/views/mixins.py
+"""
 import json
-import warnings
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 
@@ -8,16 +14,11 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbid
 def allow_remote_invocation(func, method='auto'):
     """
     All methods which shall be callable through a given Ajax 'action' must be
-    decorated with @allowed_action. This is required for safety reasons. It
-    inhibits the caller to invoke all available methods of a class.
+    decorated with ``@allow_remote_invocation``. This is required for safety
+    reasons. It inhibits the caller to invoke all available methods of a class.
     """
     setattr(func, 'allow_rmi', method)
     return func
-
-
-def allowed_action(func):
-    warnings.warn("Decorator `@allowed_action` is deprecated. Use `@allow_remote_invocation` instead.", DeprecationWarning)
-    return allow_remote_invocation(func)
 
 
 class JSONResponseException(Exception):
@@ -57,23 +58,13 @@ class JSONResponseMixin(JSONBaseMixin):
     def get(self, request, *args, **kwargs):
         if not request.is_ajax():
             return self._dispatch_super(request, *args, **kwargs)
-        if 'action' in kwargs:
-            warnings.warn("Using the keyword 'action' in URLresolvers is deprecated. Please use 'invoke_method' instead", DeprecationWarning)
-            remote_method = kwargs['action']
-        else:
-            remote_method = kwargs.get('invoke_method')
-        if remote_method:
-            # method for invocation is determined programmatically
-            handler = getattr(self, remote_method)
-        else:
-            # method for invocation is determined by HTTP header
-            remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
-            handler = remote_method and getattr(self, remote_method, None)
-            if not callable(handler):
-                return self._dispatch_super(request, *args, **kwargs)
-            if not hasattr(handler, 'allow_rmi'):
-                return HttpResponseForbidden("Method '{0}.{1}' has no decorator '@allow_remote_invocation'"
-                                             .format(self.__class__.__name__, remote_method))
+        remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
+        handler = remote_method and getattr(self, remote_method, None)
+        if not callable(handler):
+            return self._dispatch_super(request, *args, **kwargs)
+        if not hasattr(handler, 'allow_rmi'):
+            return HttpResponseForbidden("Method '{0}.{1}' has no decorator '@allow_remote_invocation'"
+                                            .format(self.__class__.__name__, remote_method))
         try:
             response_data = handler()
         except JSONResponseException as e:
@@ -87,11 +78,7 @@ class JSONResponseMixin(JSONBaseMixin):
             in_data = json.loads(request.body.decode('utf-8'))
         except ValueError:
             in_data = request.body.decode('utf-8')
-        if 'action' in in_data:
-            warnings.warn("Using the keyword 'action' inside the payload is deprecated. Please use 'djangoRMI' from module 'djng.forms'", DeprecationWarning)
-            remote_method = in_data.pop('action')
-        else:
-            remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
+        remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
         handler = remote_method and getattr(self, remote_method, None)
         if not callable(handler):
             return self._dispatch_super(request, *args, **kwargs)
@@ -109,5 +96,4 @@ class JSONResponseMixin(JSONBaseMixin):
         handler = getattr(base, request.method.lower(), None)
         if callable(handler):
             return handler(request, *args, **kwargs)
-        # HttpResponseNotAllowed expects permitted methods.
         return HttpResponseBadRequest('This view can not handle method {0}'.format(request.method), status=405)
