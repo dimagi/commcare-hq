ccf87513da3ea12130b45619da48b6cb35162b34
Daniel Miller <dmiller@dimagi.com>
2022-03-15 12:14:18 -0400

    Dedupe remote method invocation code

--- corehq/util/jqueryrmi.py
+++ corehq/util/jqueryrmi.py
@@ -56,37 +56,29 @@ class JSONResponseMixin(JSONBaseMixin):
     The returned HTTP responses are of kind ``application/json;charset=UTF-8``.
     """
     def get(self, request, *args, **kwargs):
-        if not request.is_ajax():
-            return self._dispatch_super(request, *args, **kwargs)
-        remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
-        handler = remote_method and getattr(self, remote_method, None)
-        if not callable(handler):
-            return self._dispatch_super(request, *args, **kwargs)
-        if not hasattr(handler, 'allow_rmi'):
-            return HttpResponseForbidden("Method '{0}.{1}' has no decorator '@allow_remote_invocation'"
-                                            .format(self.__class__.__name__, remote_method))
-        try:
-            response_data = handler()
-        except JSONResponseException as e:
-            return self.json_response({'message': e.args[0]}, e.status_code)
-        return self.json_response(response_data)
+        return self._invoke_remote_method(request, args, kwargs)
 
     def post(self, request, *args, **kwargs):
+        def data():
+            try:
+                return json.loads(request.body.decode('utf-8'))
+            except ValueError:
+                return request.body.decode('utf-8')
+        return self._invoke_remote_method(request, args, kwargs, data)
+
+    def _invoke_remote_method(self, request, args, kwargs, data=None):
         if not request.is_ajax():
             return self._dispatch_super(request, *args, **kwargs)
-        try:
-            in_data = json.loads(request.body.decode('utf-8'))
-        except ValueError:
-            in_data = request.body.decode('utf-8')
         remote_method = request.META.get('HTTP_DJNG_REMOTE_METHOD')
         handler = remote_method and getattr(self, remote_method, None)
         if not callable(handler):
             return self._dispatch_super(request, *args, **kwargs)
         if not hasattr(handler, 'allow_rmi'):
-            return HttpResponseForbidden("Method '{0}.{1}' has no decorator '@allow_remote_invocation'"
-                                         .format(self.__class__.__name__, remote_method), 403)
+            return HttpResponseForbidden(
+                f"Method '{type(self).__name__}.{remote_method}' has no decorator '@allow_remote_invocation'")
+        data_args = (data(),) if data is not None else ()
         try:
-            response_data = handler(in_data)
+            response_data = handler(*data_args)
         except JSONResponseException as e:
             return self.json_response({'message': e.args[0]}, e.status_code)
         return self.json_response(response_data)
