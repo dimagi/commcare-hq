dimagi/commcare-hq

commit af09c994818daa06e0b3f974ce03b7878ccc9724

combine celery checks so both print

corehq/apps/hqadmin/service_checks.py
@@ -157,8 +157,7 @@ def check_blobdb():
 
 
 def check_celery():
-    blocked_queues = []
-    slow_queues = []
+    bad_queues = []
 
     for queue, threshold in settings.CELERY_HEARTBEAT_THRESHOLDS.items():
         if threshold:
@@ -174,21 +173,17 @@ def check_celery():
                 # so to make actionable, we never alert on blockage under 5 minutes
                 # It is still counted as out of SLA for the celery uptime metric in datadog
                 if blockage_duration > max(threshold, datetime.timedelta(minutes=5)):
-                    blocked_queues.append((queue, blockage_duration, threshold))
+                    bad_queues.append(
+                        f"{queue} has been blocked for {blockage_duration} (max allowed is {threshold})"
+                    )
                 elif (heartbeat_time_to_start is not None and
                       heartbeat_time_to_start > max(threshold, datetime.timedelta(minutes=5))):
-                    slow_queues.append(queue, heartbeat_time_to_start, threshold)
+                    bad_queues.append(
+                        f"{queue} is delayed for {blockage_duration} (max allowed is {threshold})"
+                    )
 
-    if blocked_queues:
-        return ServiceStatus(False, '\n'.join(
-            "{} has been blocked for {} (max allowed is {})".format(
-                queue, blockage_duration, threshold
-            ) for queue, blockage_duration, threshold in blocked_queues))
-    elif slow_queues:
-        return ServiceStatus(False, '\n'.join(
-            "{} is delayed for {} (max allowed is {})".format(
-                queue, blockage_duration, threshold
-            ) for queue, blockage_duration, threshold in slow_queues))
+    if bad_queues:
+        return ServiceStatus(False, '\n'.join(bad_queues))
     else:
         return ServiceStatus(True, "OK")
 

commit cc64b6ba65a9aeed8055ba0150e4d13267fef3ba

fix celery service check
Broken in af09c994818daa06e0b3f974ce03b7878ccc9724

corehq/apps/hqadmin/service_checks.py
@@ -167,7 +167,7 @@ def check_celery():
                 blockage_duration = heartbeat.get_and_report_blockage_duration()
                 heartbeat_time_to_start = heartbeat.get_and_report_time_to_start()
             except HeartbeatNeverRecorded:
-                blocked_queues.append((queue, 'as long as we can see', threshold))
+                bad_queues.append(f"{queue} has been blocked as long as we can see (max allowed is {threshold})")
             else:
                 # We get a lot of self-resolving celery "downtime" under 5 minutes
                 # so to make actionable, we never alert on blockage under 5 minutes