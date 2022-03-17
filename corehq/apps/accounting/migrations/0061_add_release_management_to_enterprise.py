95b5cd9834e426436972ff63727fd59e4ff3fcdc
Graham Herceg <g.a.herceg@gmail.com>
2022-03-17 16:13:27 -0700

    create migration to add release management to enterprise plans

---
+++ corehq/apps/accounting/migrations/0061_add_release_management_to_enterprise.py
@@ -0,0 +1,24 @@
+from django.core.management import call_command
+from django.db import migrations
+
+from corehq.util.django_migrations import skip_on_fresh_install
+
+
+@skip_on_fresh_install
+def _add_release_management_to_enterprise(apps, schema_editor):
+    call_command(
+        'cchq_prbac_grandfather_privs',
+        'release management',
+        skip_edition='Paused,Community,Standard,Pro,Advanced',
+        noinput=True,
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('accounting', '0060_remove_enterprise_v1'),
+    ]
+
+    operations = [
+        migrations.RunPython(_add_release_management_to_enterprise),
+    ]
