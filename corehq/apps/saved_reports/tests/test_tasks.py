154d2ef0f3646e8427c6b02d802e0745449a0eab
Norman Hooper <nhooper@dimagi.com>
2022-03-22 14:19:33 +0000

    Add test

--- corehq/apps/saved_reports/tests/test_tasks.py
+++ corehq/apps/saved_reports/tests/test_tasks.py
@@ -1,13 +1,19 @@
-from unittest.mock import patch
-from django.test import SimpleTestCase, TestCase
-from freezegun import freeze_time
 import datetime
+from dataclasses import dataclass
+from unittest.mock import patch
 
+from django.test import SimpleTestCase, TestCase
+
+from freezegun import freeze_time
+
+from .. import tasks
 from ..exceptions import ReportNotFound
 from ..models import ScheduledReportLog
-from ..tasks import purge_old_scheduled_report_logs, queue_scheduled_reports
-from .. import tasks
-
+from ..tasks import (
+    purge_old_scheduled_report_logs,
+    queue_scheduled_reports,
+    send_email_report,
+)
 
 CURRENT_DATE = datetime.date(2020, 1, 2)
 
@@ -55,3 +61,60 @@ class QueueScheduledReportsTests(SimpleTestCase):
         ARG_INDEX = 0
         calls = set(call[ARG_INDEX][0] for call in mock_send.call_args_list)
         self.assertSetEqual(calls, {'a', 'b', 'c'})
+
+
+class TestSendEmailReport(SimpleTestCase):
+
+    def test_duplicates(self):
+        recipient_list = [
+            'alice@example.com',
+            'alice@example.com',
+            'alice@example.com',
+        ]
+        request_data = {
+            'couch_user': 'abc123',
+            'GET': {
+                'startdate': '2022-01-01T00:00:00Z',
+                'enddate': '2022-01-31T23:59:59Z',
+            }
+        }
+        cleaned_data = {
+            'subject': '',
+            'notes': ''
+        }
+        with patch('corehq.apps.saved_reports.tasks.CouchUser', CouchUser), \
+                patch('corehq.apps.reports.views._render_report_configs') as render_configs, \
+                patch('corehq.apps.reports.views.render_full_report_notification') as render_report, \
+                patch('corehq.apps.saved_reports.tasks.send_HTML_email') as send_html_email:
+            render_configs.return_value = ['report_text']
+            render_report.return_value = ReportNotification('content')
+
+            send_email_report(
+                recipient_list,
+                'test-domain',
+                'report_slug',
+                'report_type',
+                request_data,
+                is_once_off=True,
+                cleaned_data=cleaned_data,
+            )
+
+            send_html_email.assert_called_once()
+
+
+@dataclass
+class CouchUser:
+    user_id: str
+    language: str
+
+    @classmethod
+    def get_by_user_id(cls, user_id):
+        return cls(
+            user_id=user_id,
+            language='tnq',
+        )
+
+
+@dataclass
+class ReportNotification:
+    content: str
