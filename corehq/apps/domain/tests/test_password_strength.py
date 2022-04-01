cb705d45f8a407d91716da3886db8d456b30aca6
Daniel Roberts <droberts@dimagi.com>
2022-04-01 17:10:09 -0500

    Add a few more tests for password strength and sensitivity MINIMUM_ZXCVBN_SCORE setting

--- corehq/apps/domain/tests/test_password_strength.py
+++ corehq/apps/domain/tests/test_password_strength.py
@@ -18,9 +18,21 @@ class PasswordStrengthTest(SimpleTestCase):
     def test_score_2_password(self):
         self.assert_good_password(PASSWORDS_BY_STRENGTH[2])
 
+    @override_settings(MINIMUM_ZXCVBN_SCORE=2)
+    def test_score_3_password(self):
+        self.assert_good_password(PASSWORDS_BY_STRENGTH[3])
+
+    @override_settings(MINIMUM_ZXCVBN_SCORE=2)
+    def test_score_4_password(self):
+        self.assert_good_password(PASSWORDS_BY_STRENGTH[4])
+
     @override_settings(MINIMUM_ZXCVBN_SCORE=3)
-    def test_sensitivity_to_minimum_zxcvbn_score_setting(self):
+    def test_sensitivity_to_minimum_zxcvbn_score_setting_bad(self):
         self.assert_bad_password(PASSWORDS_BY_STRENGTH[2])
+
+    @override_settings(MINIMUM_ZXCVBN_SCORE=3)
+    def test_sensitivity_to_minimum_zxcvbn_score_setting_good(self):
+        self.assert_good_password(PASSWORDS_BY_STRENGTH[3])
 
     def assert_good_password(self, password):
         self.assertEqual(clean_password(password), password)
