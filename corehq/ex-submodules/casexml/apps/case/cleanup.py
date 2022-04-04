423f38be46b8cae9c9be2c890d1e348446459c1c
Dominic Ochoa <dominicochoa@dim-a067.fios-router.home>
2022-04-04 16:15:29 -0400

    removed whitespace

--- corehq/ex-submodules/casexml/apps/case/cleanup.py
+++ corehq/ex-submodules/casexml/apps/case/cleanup.py
@@ -149,7 +149,7 @@ def get_first_claims(domain, user_id, case_ids):
 
     potential_cases = CommCareCase.objects.get_reverse_indexed_cases(domain, case_ids_found, case_types=[CLAIM_CASE_TYPE], is_closed=False)
     # creates set of claimed case_ids where owner_id = user_id
-    previously_claimed_ids = set(map(lambda case: case.get_index('host').referenced_id if case.owner_id == user_id else None, 
+    previously_claimed_ids = set(map(lambda case: case.get_index('host').referenced_id if case.owner_id == user_id else None,
                                     potential_cases))
 
     return previously_claimed_ids
