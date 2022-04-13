4909f0bcae65ab9fc33fc0118bcc3c44c203d39f
Daniel Miller <dmiller@dimagi.com>
2022-04-13 13:10:12 -0400

    Reset Celery app created by gevent monkey patching
    This happens for workers run with a command like
    
        celery -A corehq worker -P gevent

--- corehq/__init__.py
+++ corehq/__init__.py
@@ -18,7 +18,8 @@ def _get_current_app():
     # raise an error if the current app is accessed before it is set,
     # but not thereafter?
     #
-    # Note no distinction between current and default app. They are the same.
+    # Eliminating the thread-local current app hooey should be fine.
+    # https://github.com/celery/celery/blob/ab3231dea14501c0159d3caa1fcf83689eb6db2d/celery/app/trace.py#L673-L680
     app = _state.default_app
     if app is None:
         raise RuntimeError("""Celery is not initialized.
@@ -34,7 +35,11 @@ def _get_current_app():
 # corehq.apps.celery._init_celery_app during Django setup.
 os.environ.setdefault("C_STRICT_APP", "1")
 from celery import _state  # noqa: E402
-assert _state.default_app is None, "Default app already created"
+if _state.default_app is not None:
+    # Reset default app, which can be initialized by gevent monkey
+    # patching, which traverses gc.get_objects() and calls
+    # isinstance(...) on each object.
+    _state.set_default_app(None)
 assert _state._tls.current_app is None, "Current app already created"
 assert hasattr(_state.current_app, "_Proxy__local")
 object.__setattr__(_state.current_app, "_Proxy__local", _get_current_app)
