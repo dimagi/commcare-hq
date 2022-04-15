112d1949789a644300da71a6caa4e61fb220f1bc
Daniel Miller <dmiller@dimagi.com>
2022-04-15 11:58:42 -0400

    Whitelist warnings triggered during test startup
    There are still deprecation warnings triggered by tests that now cause test failures. They will be addressed in a future commit.

--- corehq/warnings.py
+++ corehq/warnings.py
@@ -1,7 +1,25 @@
 import os
+import re
 import warnings
 
 
 def configure_warnings(is_testing):
     if is_testing and 'PYTHONWARNINGS' not in os.environ:
         warnings.simplefilter("error")
+    configure_deprecation_whitelist()
+
+
+def configure_deprecation_whitelist():
+    # warnings that may be resolved with a library upgrade
+    whitelist("celery", "'collections.abc'")
+    whitelist("couchdbkit.schema.properties", "'collections.abc'")
+    whitelist("kombu.utils.functional", "'collections.abc'")
+    whitelist("nose.importer", "the imp module is deprecated")
+
+    # warnings that can be resolved with HQ code changes
+    whitelist("corehq.util.validation", "metaschema specified by $schema was not found")
+
+
+def whitelist(module, message, category=DeprecationWarning):
+    msg = r".*" + re.escape(message)
+    warnings.filterwarnings("default", msg, category, re.escape(module))
