3ef6d2f1620488b1936106672651ae86674d5742
Daniel Miller <dmiller@dimagi.com>
2022-04-18 13:26:05 -0400

    Warnings whitelist as a static list of tuples

--- corehq/warnings.py
+++ corehq/warnings.py
@@ -2,6 +2,55 @@ import os
 import re
 import warnings
 
+from django.utils.deprecation import RemovedInDjango41Warning
+from sqlalchemy.exc import SAWarning
+
+
+WHITELIST = [
+    # Adding items here is a temporary workaround to prevent a warning
+    # from causing tests to fail when the warning cannot be easily
+    # avoided. There is a cost associated with adding items to this list
+    # (the warnings module does a linear search through the list with
+    # regex matching each time a warning is triggered), so it is best to
+    # avoid it if possible.
+    #
+    # Item format:
+    # (module_path, message_substring_or_regex, optional_warning_class)
+
+    # warnings that may be resolved with a library upgrade
+    ("captcha.fields", "ugettext_lazy() is deprecated"),
+    ("celery", "'collections.abc'"),
+    ("couchdbkit.schema.properties", "'collections.abc'"),
+    ("django.apps", re.compile(r"'(" + "|".join(re.escape(app) for app in [
+        "captcha",
+        "django_celery_results",
+        "oauth2_provider",
+        "statici18n",
+        "two_factor",
+    ]) + ")' defines default_app_config"), RemovedInDjango41Warning),
+    ("django_celery_results", "ugettext_lazy() is deprecated"),
+    ("django_digest", "The 'warn' method is deprecated"),
+    ("django_otp.plugins", "django.conf.urls.url() is deprecated"),
+    ("kombu.utils.functional", "'collections.abc'"),
+    ("logentry_admin.admin", "ugettext_lazy() is deprecated"),
+    ("nose.importer", "the imp module is deprecated"),
+    ("nose.util", "inspect.getargspec() is deprecated"),
+    ("tastypie", "django.conf.urls.url() is deprecated"),
+    ("tastypie", "request.is_ajax() is deprecated"),
+
+    # warnings that can be resolved with HQ code changes
+    ("", "json_response is deprecated.  Use django.http.JsonResponse instead."),
+    ("", "property_match are deprecated. Use boolean_expression instead."),
+    ("corehq.util.validation", "metaschema specified by $schema was not found"),
+
+    # other, resolution not obvious
+    ("sqlalchemy.", re.compile(r"^Predicate of partial index .* ignored during reflection"), SAWarning),
+    ("sqlalchemy.",
+        "Skipped unsupported reflection of expression-based index form_processor_xformattachmentsql_blobmeta_key",
+        SAWarning),
+    ("unittest.case", "TestResult has no addExpectedFailure method", RuntimeWarning),
+]
+
 
 def configure_warnings(is_testing):
     if is_testing:
@@ -9,57 +58,8 @@ def configure_warnings(is_testing):
         if 'PYTHONWARNINGS' not in os.environ:
             warnings.simplefilter("error")
     if is_testing or "CCHQ_WHITELISTED_WARNINGS" in os.environ:
-        configure_deprecation_whitelist()
-
-
-def configure_deprecation_whitelist():
-    """Whitelist known deprecated code paths
-
-    Adding items here is a temporary workaround to prevent a warning
-    from causing tests to fail when the warning cannot be easily
-    avoided. There is a cost associated with adding items to this list
-    (the warnings module does a linear search through the list with
-    regex matching each time a warning is triggered), so it is best to
-    avoid it if possible.
-    """
-    from django.utils.deprecation import RemovedInDjango41Warning
-    from sqlalchemy.exc import SAWarning
-
-    # warnings that may be resolved with a library upgrade
-    config_apps = "|".join(re.escape(app) for app in [
-        "captcha",
-        "django_celery_results",
-        "oauth2_provider",
-        "statici18n",
-        "two_factor",
-    ])
-    default_config_message = re.compile(fr"'.*({config_apps})' defines default_app_config")
-    whitelist("captcha.fields", "ugettext_lazy() is deprecated")
-    whitelist("celery", "'collections.abc'")
-    whitelist("couchdbkit.schema.properties", "'collections.abc'")
-    whitelist("django.apps", default_config_message, RemovedInDjango41Warning)
-    whitelist("django_celery_results", "ugettext_lazy() is deprecated")
-    whitelist("django_digest", "The 'warn' method is deprecated")
-    whitelist("django_otp.plugins", "django.conf.urls.url() is deprecated")
-    whitelist("kombu.utils.functional", "'collections.abc'")
-    whitelist("logentry_admin.admin", "ugettext_lazy() is deprecated")
-    whitelist("nose.importer", "the imp module is deprecated")
-    whitelist("nose.util", "inspect.getargspec() is deprecated")
-    whitelist("tastypie", "django.conf.urls.url() is deprecated")
-    whitelist("tastypie", "request.is_ajax() is deprecated")
-
-    # warnings that can be resolved with HQ code changes
-    whitelist("", "json_response is deprecated.  Use django.http.JsonResponse instead.")
-    whitelist("", "property_match are deprecated. Use boolean_expression instead.")
-    whitelist("corehq.util.validation", "metaschema specified by $schema was not found")
-
-    # other, resolution not obvious
-    ignored_predicates = re.compile(r"^Predicate of partial index .* ignored during reflection")
-    skipped_reflections = re.compile(
-        "^Skipped unsupported reflection of expression-based index form_processor_xformattachmentsql_blobmeta_key")
-    whitelist("sqlalchemy.", ignored_predicates, SAWarning)
-    whitelist("sqlalchemy.", skipped_reflections, SAWarning)
-    whitelist("unittest.case", "TestResult has no addExpectedFailure method", RuntimeWarning)
+        for args in WHITELIST:
+            whitelist(*args)
 
 
 def whitelist(module, message, category=DeprecationWarning):
@@ -161,11 +161,10 @@ Possible resolutions:
 
 - Best: Eliminate the deprecated code path.
 
-- Worse: add a targeted whitelist item to `corehq.warnings`
-  `.configure_deprecation_whitelist()`. Whitelist items should target
-  the specific module or package that triggers the warning, and should
-  uniquely match the deprecation message so as not to hide other
-  deprecation warnings in the same module or package.
+- Worse: add an item to `corehq.warnings.WHITELIST`. Whitelist items
+  should target the specific module or package that triggers the
+  warning, and should uniquely match the deprecation message so as not
+  to hide other deprecation warnings in the same module or package.
 
 - Last resort: if it is not possible to eliminate the deprecated code
   path or to add a whitelist item that uniquely matches the deprecation
