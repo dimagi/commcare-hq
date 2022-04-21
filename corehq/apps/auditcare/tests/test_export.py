7bad709d9ae121b8a783066ecb025cb4e989f61a
Amit Phulera <amitphulera999@gmail.com>
2022-04-21 17:12:21 +0530

    fix: tests

--- corehq/apps/auditcare/tests/test_export.py
+++ corehq/apps/auditcare/tests/test_export.py
@@ -20,7 +20,7 @@ from ..utils.export import (
     navigation_events_by_user,
     write_export_from_all_log_events,
 )
-from .testutils import AuditcareTest, save_couch_doc, delete_couch_docs
+from .testutils import AuditcareTest
 
 
 class TestNavigationEventsQueries(AuditcareTest):
@@ -31,76 +31,6 @@ class TestNavigationEventsQueries(AuditcareTest):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        username = "couch@test.com"
-        event_date = datetime(2021, 2, 1, 2).strftime("%Y-%m-%dT%H:%M:%SZ")
-        cls.couch_ids = [
-            # field structure based on real auditcare docs in Couch
-            save_couch_doc(
-                "NavigationEventAudit",
-                username,
-                event_date=event_date,
-                description='User Name',
-                extra={},
-                headers={
-                    'REQUEST_METHOD': 'GET',
-                    'QUERY_STRING': 'version=2.0&since=...',
-                    'HTTP_CONNECTION': 'close',
-                    'HTTP_COOKIE': 'sessionid=abc123',
-                    'SERVER_NAME': '0.0.0.0',
-                    'SERVER_PORT': '9010',
-                    'HTTP_ACCEPT': 'application/json, application/*+json, */*',
-                    'REMOTE_ADDR': '10.2.10.60',
-                    'HTTP_ACCEPT_ENCODING': 'gzip'
-                },
-                ip_address='10.1.2.3',
-                request_path='/a/delmar/phone/restore/?version=2.0&since=...',
-                session_key='abc123',
-                status_code=200,
-                user_agent='okhttp/4.4.1',
-                view_kwargs={'domain': 'delmar'},
-                view='corehq.apps.ota.views.restore',
-            ),
-            save_couch_doc(
-                "AccessAudit",
-                username,
-                event_date=event_date,
-                access_type='login',
-                description='Login Success',
-                failures_since_start=None,
-                get_data=[],
-                http_accept='text/html',
-                ip_address='10.1.3.2',
-                path_info='/a/delmar/login/',
-                post_data=[],
-                session_key='abc123',
-                user_agent='Mozilla/5.0',
-            ),
-            save_couch_doc(
-                "NavigationEventAudit",
-                cls.username,
-                event_date=event_date,
-                description='User Name',
-                extra={},
-                headers={
-                    'REQUEST_METHOD': 'GET',
-                    'QUERY_STRING': 'version=2.0&since=...',
-                    'HTTP_CONNECTION': 'close',
-                    'HTTP_COOKIE': 'sessionid=abc123',
-                    'SERVER_NAME': '0.0.0.0',
-                    'SERVER_PORT': '9010',
-                    'HTTP_ACCEPT': 'application/json, application/*+json, */*',
-                    'REMOTE_ADDR': '10.2.10.60',
-                    'HTTP_ACCEPT_ENCODING': 'gzip'
-                },
-                ip_address='10.1.2.3',
-                request_path='/a/delmar/phone/restore/?version=2.0&since=...',
-                session_key='abc123',
-                status_code=200,
-                user_agent='okhttp/4.4.1',
-                view_kwargs={'domain': 'delmar'},
-                view='corehq.apps.ota.views.restore',
-            )
-        ]
 
         def iter_events(model, username, **kw):
             for event_date in cls.event_dates:
@@ -119,15 +49,14 @@ class TestNavigationEventsQueries(AuditcareTest):
 
     @classmethod
     def tearDownClass(cls):
-        delete_couch_docs(cls.couch_ids)
         super().tearDownClass()
 
     def test_navigation_events_query(self):
         events = list(navigation_events_by_user(self.username))
         self.assertEqual({e.user for e in events}, {self.username})
-        expected_dates = [datetime(2021, 2, 1, 2, 0)] + self.event_dates
+        expected_dates = self.event_dates
         self.assertEqual([e.event_date for e in events], expected_dates)
-        self.assertEqual(len(events), 29)
+        self.assertEqual(len(events), 28)
 
     def test_navigation_events_date_range_query(self):
         start = datetime(2021, 2, 5)
@@ -136,16 +65,6 @@ class TestNavigationEventsQueries(AuditcareTest):
         self.assertEqual({e.user for e in events}, {self.username})
         self.assertEqual({e.event_date for e in events}, set(self.event_dates[4:15]))
         self.assertEqual(len(events), 11)
-
-    def test_navigation_events_querying_couch_and_sql(self):
-        couch_event_date = datetime(2021, 2, 1, 2)
-        start = datetime(2021, 1, 31)
-        end = datetime(2021, 2, 5)
-        events = list(navigation_events_by_user(self.username, start, end))
-        self.assertEqual({e.user for e in events}, {self.username})
-        expected_event_dates = set(self.event_dates[2:5] + [couch_event_date])
-        self.assertEqual({e.event_date for e in events}, expected_event_dates)
-        self.assertEqual(len(events), 4)
 
     def test_recent_all_log_events_query(self):
         start = datetime(2021, 2, 4)
@@ -168,10 +87,10 @@ class TestNavigationEventsQueries(AuditcareTest):
         start = datetime(2021, 2, 1)
         end = datetime(2021, 2, 1)
         events = list(get_all_log_events(start, end))
-        self.assertEqual({e.user for e in events}, {self.username, "other@test.com", "couch@test.com"})
+        self.assertEqual({e.user for e in events}, {self.username, "other@test.com"})
         self.assertEqual({e.doc_type for e in events}, {"AccessAudit", "NavigationEventAudit"})
-        self.assertEqual({e.event_date for e in events}, {datetime(2021, 2, 1, 2), datetime(2021, 2, 1, 3)})
-        self.assertEqual(len(events), 7)
+        self.assertEqual({e.event_date for e in events}, {datetime(2021, 2, 1, 3)})
+        self.assertEqual(len(events), 4)
 
     def test_write_export_from_all_log_events(self):
         def unpack(row):
@@ -188,24 +107,6 @@ class TestNavigationEventsQueries(AuditcareTest):
                 rows = DictReader(fh)
                 items = sorted([unpack(r) for r in rows], key=key)
                 expected_items = [
-                    {
-                        'Date': '2021-02-01 02:00:00',
-                        'Type': 'AccessAudit',
-                        'User': 'couch@test.com',
-                        'Description': 'Login Success',
-                    },
-                    {
-                        'Date': '2021-02-01 02:00:00',
-                        'Type': 'NavigationEventAudit',
-                        'User': 'couch@test.com',
-                        'Description': 'User Name',
-                    },
-                    {
-                        'Date': '2021-02-01 02:00:00',
-                        'Type': 'NavigationEventAudit',
-                        'User': 'test@test.com',
-                        'Description': 'User Name'
-                    },
                     {
                         'Date': '2021-02-01 03:00:00',
                         'Type': 'AccessAudit',
