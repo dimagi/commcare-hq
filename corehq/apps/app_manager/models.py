36f046aef499c3600ae77b7f3d8fac0849e52e12
Jennifer Schweers <orange.jenny@gmail.com>
2022-04-21 20:24:36 -0400

    Updated shadow modules to inherit multi-select property from source

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -3891,6 +3891,9 @@ class ShadowModule(ModuleBase, ModuleDetailsMixin):
             return []
         return self.source_module.all_forms_require_a_case()
 
+    def is_multi_select(self):
+        return self.source_module.is_multi_select()
+
     @classmethod
     def new_module(cls, name, lang, shadow_module_version=2):
         lang = lang or 'en'
