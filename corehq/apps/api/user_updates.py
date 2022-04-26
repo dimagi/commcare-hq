b074fc84bc4e4c51393999fe5085566fe94e0917
Graham Herceg <g.a.herceg@gmail.com>
2022-04-26 08:10:00 -0700

    consolidate logging code for phone numbers

--- corehq/apps/api/user_updates.py
+++ corehq/apps/api/user_updates.py
@@ -67,23 +67,15 @@ def _update_password(user, password, user_change_logger):
 
 def _update_default_phone_number(user, phone_number, user_change_logger):
     old_phone_numbers = set(user.phone_numbers)
+    new_phone_numbers = set(user.phone_numbers)
     if not isinstance(phone_number, str):
         raise InvalidFormatException('string')
     formatted_phone_number = strip_plus(phone_number)
+    new_phone_numbers.add(formatted_phone_number)
     user.set_default_phone_number(formatted_phone_number)
 
     if user_change_logger:
-        numbers_added, numbers_removed = find_differences_in_list(
-            target=[phone_number],
-            source=list(old_phone_numbers)
-        )
-
-        change_messages = {}
-        if numbers_added:
-            change_messages.update(
-                UserChangeMessage.phone_numbers_added(list(numbers_added))["phone_numbers"]
-            )
-            user_change_logger.add_change_message({'phone_numbers': change_messages})
+        _log_phone_number_change(new_phone_numbers, old_phone_numbers, user_change_logger)
 
 
 def _update_phone_numbers(user, phone_numbers, user_change_logger):
@@ -98,24 +90,7 @@ def _update_phone_numbers(user, phone_numbers, user_change_logger):
             user.set_default_phone_number(formatted_phone_number)
 
     if user_change_logger:
-        (numbers_added, numbers_removed) = find_differences_in_list(
-            target=list(new_phone_numbers),
-            source=list(old_phone_numbers)
-        )
-
-        change_messages = {}
-        if numbers_removed:
-            change_messages.update(
-                UserChangeMessage.phone_numbers_removed(list(numbers_removed))["phone_numbers"]
-            )
-
-        if numbers_added:
-            change_messages.update(
-                UserChangeMessage.phone_numbers_added(list(numbers_added))["phone_numbers"]
-            )
-
-        if change_messages:
-            user_change_logger.add_change_message({'phone_numbers': change_messages})
+        _log_phone_number_change(new_phone_numbers, old_phone_numbers, user_change_logger)
 
 
 def _update_groups(user, group_ids, user_change_logger):
@@ -139,3 +114,24 @@ def _simple_update(user, key, value, user_change_logger):
     if user_change_logger and getattr(user, key) != value:
         user_change_logger.add_changes({key: value})
     setattr(user, key, value)
+
+
+def _log_phone_number_change(new_phone_numbers, old_phone_numbers, user_change_logger):
+    numbers_added, numbers_removed = find_differences_in_list(
+        target=list(new_phone_numbers),
+        source=list(old_phone_numbers)
+    )
+
+    change_messages = {}
+    if numbers_removed:
+        change_messages.update(
+            UserChangeMessage.phone_numbers_removed(list(numbers_removed))["phone_numbers"]
+        )
+
+    if numbers_added:
+        change_messages.update(
+            UserChangeMessage.phone_numbers_added(list(numbers_added))["phone_numbers"]
+        )
+
+    if change_messages:
+        user_change_logger.add_change_message({'phone_numbers': change_messages})
