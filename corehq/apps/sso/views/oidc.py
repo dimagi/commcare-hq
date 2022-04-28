6370416f105e700f0ca1c2014df11248e1008eee
Biyeun Buczyk <biyeun@dimagi.com>
2022-04-28 19:25:14 +0200

    make sure that login_hint is added to the url in the saml view too

--- corehq/apps/sso/views/oidc.py
+++ corehq/apps/sso/views/oidc.py
@@ -6,13 +6,17 @@ from corehq.apps.sso.utils.oidc import (
     initialize_oidc_session,
     get_openid_provider_login_url,
 )
+from corehq.apps.sso.utils.url_helpers import add_username_hint_to_login_url
 
 
 @identity_provider_required
 def sso_oidc_login(request, idp_slug):
     client = get_client_for_identity_provider(request.idp)
     initialize_oidc_session(request)
-    login_url = get_openid_provider_login_url(client, request)
+    login_url = add_username_hint_to_login_url(
+        get_openid_provider_login_url(client, request),
+        request
+    )
     return HttpResponseRedirect(login_url)
 
 
