6fb4d05989997b30cbbc4278f1b30896708cf39d
Jennifer Schweers <orange.jenny@gmail.com>
2022-05-02 13:16:22 -0400

    Added UI

--- corehq/apps/app_manager/static/app_manager/js/details/detail_tab_nodeset.js
+++ corehq/apps/app_manager/static/app_manager/js/details/detail_tab_nodeset.js
@@ -10,6 +10,7 @@ hqDefine('app_manager/js/details/detail_tab_nodeset', function () {
 
         self.nodeset = ko.observable(options.nodeset);
         self.nodesetCaseType = ko.observable(options.nodesetCaseType);
+        self.nodesetFilter = ko.observable(options.nodesetFilter);
 
         self.dropdownOptions = [{name: gettext("Data Tab: Custom Expression"), value: ""}].concat(
             _.map(options.caseTypes, function (t) {
@@ -21,8 +22,14 @@ hqDefine('app_manager/js/details/detail_tab_nodeset', function () {
             return !self.nodesetCaseType();
         });
 
-        var ui = '<select class="form-control" data-bind="options: dropdownOptions, optionsText: \'name\', optionsValue: \'value\', value: nodesetCaseType"></select>';
-        ui += '<textarea type="text" class="form-control" data-bind="value: nodeset, visible: showXpath" style="margin-top: 5px" /></textarea>';
+        self.showFilter = ko.observable(!!self.nodesetFilter());
+
+        var ui = '<select class="form-control" data-bind="options: dropdownOptions, optionsText: \'name\', optionsValue: \'value\', value: nodesetCaseType" style="margin-bottom: 5px;"></select>';
+        ui += '<button class="btn btn-default btn-xs" data-bind="visible: !showFilter() && !showXpath(), click: function () { showFilter(true); }">'
+        ui += '<i class="fa fa-plus"></i>' + gettext("Add filter");
+        ui += '</button>';
+        ui += '<input type="text" class="form-control" data-bind="value: nodesetFilter, visible: showFilter" placeholder="' + gettext("referral = 'y'") + '" />';
+        ui += '<textarea class="form-control" data-bind="value: nodeset, visible: showXpath" /></textarea>';
         if (hqImport('hqwebapp/js/toggles').toggleEnabled('SYNC_SEARCH_CASE_CLAIM')) {
             ui += '<p data-bind="visible: showXpath() && nodeset()" class="help-block">' + gettext("This data will not be shown for case search results.") + '</p>';
         }
@@ -32,10 +39,13 @@ hqDefine('app_manager/js/details/detail_tab_nodeset', function () {
         hqImport("hqwebapp/js/main").eventize(self);
         self.nodeset.subscribe(function () {
             self.fire('change');
+            self.showFilter(false);
         });
         self.nodesetCaseType.subscribe(function () {
             self.fire('change');
             self.nodeset("");
+            self.showFilter(false);
+            self.nodesetFilter("");
         });
 
         return self;
