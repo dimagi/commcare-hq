576cc2a162dbcc585863f2b5f4cd89056a17c27b
Simon Kelly <skelly@dimagi.com>
2022-05-09 12:22:06 +0200

    combine util methods

--- corehq/apps/smsforms/signals.py
+++
@@ -1,8 +0,0 @@
-from corehq.apps.smsforms.util import process_sms_form_complete
-
-
-def handle_sms_form_complete(session_id, form):
-    from corehq.apps.smsforms.models import SQLXFormsSession
-    session = SQLXFormsSession.by_session_id(session_id)
-    if session:
-        process_sms_form_complete(session, form)
