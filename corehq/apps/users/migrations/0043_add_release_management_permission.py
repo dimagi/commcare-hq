5efe67ceab055a662daadc862b0f09448ca21f47
Graham Herceg <g.a.herceg@gmail.com>
2022-05-10 06:06:33 -0700

    use SQLPermission.create_all()

--- corehq/apps/users/migrations/0043_add_release_management_permission.py
+++ corehq/apps/users/migrations/0043_add_release_management_permission.py
@@ -1,12 +1,10 @@
 from django.db import migrations
 
 from corehq.apps.users.models_role import SQLPermission
-from corehq.util.django_migrations import skip_on_fresh_install
 
 
-@skip_on_fresh_install
 def create_release_management_permission(apps, schema_editor):
-    SQLPermission.objects.get_or_create(value='access_release_management')
+    SQLPermission.create_all()
 
 
 class Migration(migrations.Migration):
