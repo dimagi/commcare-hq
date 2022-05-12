a94f7124997eb3c228960e80930bf1f023a30a9a
Simon Kelly <skelly@dimagi.com>
2022-05-12 14:15:49 +0200

    fix SQLDataRegistryCaseUpdateRepeater 'allowed_to_forward'

--- corehq/motech/repeaters/models.py
+++ corehq/motech/repeaters/models.py
@@ -615,7 +615,15 @@ class SQLDataRegistryCaseUpdateRepeater(SQLCreateCaseRepeater):
         # Exclude extension cases where the host is also a case type that this repeater
         # would act on since they get forwarded along with their host
         host_index = payload.get_index(CASE_INDEX_IDENTIFIER_HOST)
-        return not host_index or host_index.referenced_type not in self.white_listed_case_types
+        if host_index and host_index.referenced_type in self.white_listed_case_types:
+            return False
+
+        transaction = CaseTransaction.objects.get_most_recent_form_transaction(payload.case_id)
+        if transaction:
+            # prevent chaining updates
+            return transaction.xmlns != DataRegistryCaseUpdatePayloadGenerator.XMLNS
+
+        return True
 
     @classmethod
     def _migration_get_couch_model_class(cls):
