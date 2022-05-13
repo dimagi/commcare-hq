4eba4e9e2072b0eae1b63aecdaf37c9167d43186
Simon Kelly <skelly@dimagi.com>
2022-05-13 14:13:00 +0200

    Adjust datum ID for child module with different case type

--- corehq/apps/app_manager/suite_xml/sections/entries.py
+++ corehq/apps/app_manager/suite_xml/sections/entries.py
@@ -898,6 +898,12 @@ class EntriesHelper(object):
         datum_ids = {d.datum.id: d for d in datums}
         datums_by_case_tag = _get_datums_by_case_tag(datums)
 
+        def set_id(datum_meta, new_id, datum_=None):
+            datum_ = datum_ or datum_meta.datum
+            case_tag = getattr(datum_meta.action, 'case_tag', 'basic')
+            _update_refs(datums_by_case_tag[case_tag], datum_.id, new_id)
+            datum_.id = new_id
+
         ret = []
         offset = 0
         for this_datum_meta, parent_datum_meta in zip_longest(datums, parent_datums):
@@ -910,16 +916,13 @@ class EntriesHelper(object):
                     ret.insert(head - offset, attr.evolve(parent_datum_meta, from_parent=True))
                     offset += 1
                 elif _same_case(this_datum_meta, parent_datum_meta) and this_datum_meta.action:
-                    def set_id(datum, new_id):
-                        case_tag = getattr(this_datum_meta.action, 'case_tag', 'basic')
-                        _update_refs(datums_by_case_tag[case_tag], datum.id, new_id)
-                        datum.id = new_id
-
                     if parent_datum_meta.datum.id in datum_ids:
                         datum = datum_ids[parent_datum_meta.datum.id]
-                        set_id(datum.datum, '_'.join((datum.datum.id, datum.case_type)))
+                        set_id(this_datum_meta, '_'.join((datum.datum.id, datum.case_type)), datum.datum)
 
-                    set_id(this_datum_meta.datum, parent_datum_meta.datum.id)
+                    set_id(this_datum_meta, parent_datum_meta.datum.id)
+                elif this_datum_meta.datum.id == parent_datum_meta.datum.id and this_datum_meta.action:
+                    set_id(this_datum_meta, '_'.join((this_datum_meta.datum.id, this_datum_meta.case_type)))
             if this_datum_meta:
                 ret.append(this_datum_meta)
         return ret
--- corehq/apps/app_manager/tests/test_child_module.py
+++ corehq/apps/app_manager/tests/test_child_module.py
@@ -9,12 +9,12 @@ from corehq.apps.app_manager.models import (
     PreloadAction,
 )
 from corehq.apps.app_manager.tests.app_factory import AppFactory
-from corehq.apps.app_manager.tests.util import TestXmlMixin, patch_get_xform_resource_overrides
+from corehq.apps.app_manager.tests.util import TestXmlMixin, patch_get_xform_resource_overrides, SuiteMixin
 
 DOMAIN = 'domain'
 
 
-class ModuleAsChildTestBase(TestXmlMixin):
+class ModuleAsChildTestBase(SuiteMixin):
     file_path = ('data', 'suite')
     child_module_class = None
 
@@ -381,6 +381,25 @@ class BasicModuleAsChildTest(ModuleAsChildTestBase, SimpleTestCase):
             self.get_xml('child-module-entry-datums-parent-select-other-same-case-type'),
             self.app.create_suite(),
             "./entry"
+        )
+
+    @patch_get_xform_resource_overrides()
+    def test_child_module_parent_no_parent_select_different_case_type(self, *args):
+        """A child module that does not use 'parent_select'.
+
+        If the case type of the child module differs then we need to update the datum ID to make sure
+        that the user is prompted to select a case.
+        """
+        m0f0 = self.module_0.get_form(0)
+        self.factory.form_requires_case(m0f0)
+
+        m1f0 = self.module_1.get_form(0)
+        self.factory.form_requires_case(m1f0)
+
+        self.assert_module_datums(
+            self.app.create_suite(),
+            self.module_1.id,
+            [('datum', 'case_id_guppy')]
         )
 
 
