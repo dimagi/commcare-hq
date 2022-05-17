7242ac0e03cbee924c08e3d58b4c043378b2de0b
Simon Kelly <skelly@dimagi.com>
2022-05-17 16:15:15 +0200

    more tests for inline search

--- corehq/apps/app_manager/tests/test_suite_inline_search.py
+++ corehq/apps/app_manager/tests/test_suite_inline_search.py
@@ -1,13 +1,23 @@
 from django.test import SimpleTestCase
 
+from corehq.apps.app_manager.const import WORKFLOW_FORM, WORKFLOW_PREVIOUS
 from corehq.apps.app_manager.models import (
     Application,
     CaseSearch,
     CaseSearchProperty,
+    ConditionalCaseUpdate,
     DetailColumn,
     DetailTab,
+    FormLink,
     Module,
+    OpenCaseAction,
+    ParentSelect,
+    ShadowModule,
 )
+from corehq.apps.app_manager.suite_xml.post_process.remote_requests import (
+    RESULTS_INSTANCE,
+)
+from corehq.apps.app_manager.tests.app_factory import AppFactory
 from corehq.apps.app_manager.tests.util import (
     SuiteMixin,
     TestXmlMixin,
@@ -15,6 +25,8 @@ from corehq.apps.app_manager.tests.util import (
     patch_get_xform_resource_overrides,
 )
 from corehq.apps.builds.models import BuildSpec
+from corehq.apps.case_search.const import EXCLUDE_RELATED_CASES_FILTER
+from corehq.tests.util.xml import parse_normalize
 from corehq.util.test_utils import flag_enabled
 
 DOMAIN = 'test_domain'
@@ -51,7 +63,20 @@ class InlineSearchSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
             inline_search=True,
         )
 
+        self.reg_module = self.app.add_module(Module.new_module("Registration", None))
+        self.reg_form = self.app.new_form(1, "Untitled Form", None, attachment=get_simple_form("xmlns1.0"))
+        self.reg_module.case_type = 'case'
+        self.reg_form.actions.open_case = OpenCaseAction(
+            name_update=ConditionalCaseUpdate(question_path="/data/question1")
+        )
+        self.reg_form.actions.open_case.condition.type = 'always'
+        self.reg_form.post_form_workflow = WORKFLOW_FORM
+        self.reg_form.form_links = [
+            FormLink(form_id=self.form.get_unique_id())
+        ]
+
         self.module.assign_references()
+        self.reg_module.assign_references()
         # reset to newly wrapped module
         self.module = self.app.modules[0]
         self.form = self.module.forms[0]
@@ -117,3 +142,219 @@ class InlineSearchSuiteTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
             self.get_xml("detail_tabs"),
             self.app.create_suite(),
             './detail[@id="m0_case_long"]')
+
+    def test_form_linking_to_inline_search_module_from_registration_form(self):
+        self.module.search_config.additional_case_types = ["other_case"]
+        suite = self.app.create_suite()
+        expected = """
+        <partial>
+          <create>
+            <command value="'m0'"/>
+            <query id="results" value="http://localhost:8000/a/test_domain/phone/registry_case/123/">
+              <data key="case_type" ref="'case'"/>
+              <data key="case_type" ref="'other_case'"/>
+              <data key="case_id" ref="instance('commcaresession')/session/data/case_id_new_case_0"/>
+            </query>
+            <datum id="case_id" value="instance('commcaresession')/session/data/case_id_new_case_0"/>
+            <command value="'m0-f0'"/>
+          </create>
+        </partial>"""
+        self.assertXmlPartialEqual(
+            expected,
+            suite,
+            "./entry[2]/stack/create"
+        )
+
+    def test_workflow_inline_search_previous_screen_after_case_list_form(self):
+        factory = AppFactory(DOMAIN, "App with DR", build_version='2.53.0')
+        m0, f0 = factory.new_basic_module("new case", "case")
+        factory.form_opens_case(f0, "case")
+
+        m1, f1 = factory.new_basic_module("update case", "case")
+        factory.form_requires_case(f1, "case")
+
+        m1.case_details.long.columns.append(
+            DetailColumn.wrap(dict(
+                header={"en": "name"},
+                model="case",
+                format="plain",
+                field="whatever",
+            ))
+        )
+
+        m1.search_config = CaseSearch(
+            properties=[
+                CaseSearchProperty(name='name', label={'en': 'Name'}),
+            ],
+            auto_launch=True,
+            inline_search=True,
+        )
+
+        m1.case_list_form.form_id = f0.get_unique_id()
+        m1.case_list_form.label = {'en': 'New Case'}
+
+        f1.post_form_workflow = WORKFLOW_PREVIOUS
+        app = Application.wrap(factory.app.to_json())
+        app._id = "123"
+        suite = app.create_suite()
+        self.assertXmlPartialEqual(
+            """
+            <partial>
+              <create>
+                <command value="'m1'"/>
+                <query id="results" value="http://localhost:8000/a/test_domain/phone/registry_case/123/">
+                  <data key="case_type" ref="'case'"/>
+                  <data key="case_id" ref="instance('commcaresession')/session/data/case_id"/>
+                </query>
+                <datum id="case_id" value="instance('commcaresession')/session/data/case_id"/>
+              </create>
+            </partial>
+            """,
+            suite,
+            "./entry[2]/stack/create"
+        )
+
+
+@patch_get_xform_resource_overrides()
+class InlineSearchShadowModuleTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
+    file_path = ('data', 'suite_inline_search')
+
+    def setUp(self):
+        self.app = Application.new_app(DOMAIN, "Application with Shadow")
+        self.app._id = '456'
+        self.app.build_spec = BuildSpec(version='2.53.0', build_number=1)
+        self.module = self.app.add_module(Module.new_module("Followup", None))
+        self.form = self.app.new_form(0, "Untitled Form", None, attachment=get_simple_form("xmlns1.0"))
+        self.form.requires = 'case'
+        self.module.case_type = 'case'
+
+        self.module.case_details.long.columns.append(
+            DetailColumn.wrap(dict(
+                header={"en": "name"},
+                model="case",
+                format="plain",
+                field="whatever",
+            ))
+        )
+
+        self.module.search_config = CaseSearch(
+            properties=[CaseSearchProperty(name='name', label={'en': 'Name'})],
+            auto_launch=True,
+            inline_search=True,
+        )
+
+        self.shadow_module = self.app.add_module(ShadowModule.new_module("Shadow", "en"))
+        self.shadow_module.source_module_id = self.module.unique_id
+
+        self.shadow_module.case_details.long.columns.append(
+            DetailColumn.wrap(dict(
+                header={"en": "name"},
+                model="case",
+                format="plain",
+                field="whatever",
+            ))
+        )
+
+        self.shadow_module.search_config = CaseSearch(
+            properties=[CaseSearchProperty(name='name', label={'en': 'Name'})],
+            auto_launch=True,
+            inline_search=True,
+        )
+
+        # wrap to have assign_references called
+        self.app = Application.wrap(self.app.to_json())
+
+        # reset to newly wrapped module
+        self.module = self.app.modules[0]
+        self.shadow_module = self.app.modules[1]
+
+    @flag_enabled('USH_CASE_CLAIM_UPDATES')
+    def test_suite(self):
+        suite = self.app.create_suite()
+        self.assertXmlPartialEqual(
+            self.get_xml('shadow_module_entry'),
+            suite,
+            "./entry[2]"
+        )
+
+    @flag_enabled('USH_CASE_CLAIM_UPDATES')
+    def test_additional_types(self):
+        another_case_type = "another_case_type"
+        self.module.search_config.additional_case_types = [another_case_type]
+        suite_xml = self.app.create_suite()
+        suite = parse_normalize(suite_xml, to_string=False)
+        self.assertEqual(
+            "instance('{}')/{}/case[@case_type='{}' or @case_type='{}'][@status='open']{}".format(
+                RESULTS_INSTANCE,
+                RESULTS_INSTANCE,
+                self.module.case_type,
+                another_case_type,
+                EXCLUDE_RELATED_CASES_FILTER
+            ),
+            suite.xpath("./entry[2]/session/datum/@nodeset")[0]
+        )
+        self.assertXmlPartialEqual(
+            """
+            <partial>
+              <data key="case_type" ref="'case'"/>
+              <data key="case_type" ref="'another_case_type'"/>
+            </partial>
+            """,
+            suite_xml,
+            "./entry[2]/session/query[1]/data[@key='case_type']"
+        )
+
+
+@patch_get_xform_resource_overrides()
+class InlineSearchFormLinkChildModuleTest(SimpleTestCase, TestXmlMixin, SuiteMixin):
+    file_path = ('data', 'suite_inline_search')
+
+    def setUp(self):
+        factory = AppFactory(DOMAIN, "App with inline search and child modules", build_version='2.53.0')
+        m0, f0 = factory.new_basic_module("case list", "case")
+        factory.form_requires_case(f0)
+
+        m1, f1 = factory.new_basic_module("child case list", "case", parent_module=m0)
+        m1.parent_select = ParentSelect(active=True, relationship="other", module_id=m0.get_or_create_unique_id())
+        f2 = factory.new_form(m1)
+
+        factory.form_requires_case(f1)
+        factory.form_requires_case(f2)
+
+        m1.search_config = CaseSearch(
+            properties=[CaseSearchProperty(name='name', label={'en': 'Name'})],
+            auto_launch=True,
+            inline_search=True,
+        )
+
+        # link from f1 to f2 (both in the child module)
+        f1.post_form_workflow = WORKFLOW_FORM
+        f1.form_links = [FormLink(form_id=f2.get_unique_id())]
+
+        factory.app._id = "123"
+        # wrap to have assign_references called
+        self.app = Application.wrap(factory.app.to_json())
+
+    def test_form_link_in_child_module_with_registry_search(self):
+        suite = self.app.create_suite()
+
+        expected = """
+        <partial>
+          <create>
+            <command value="'m0'"/>
+            <datum id="case_id" value="instance('commcaresession')/session/data/case_id"/>
+            <command value="'m1'"/>
+            <query id="results" value="http://localhost:8000/a/test_domain/phone/registry_case/123/">
+              <data key="case_type" ref="'case'"/>
+              <data key="case_id" ref="instance('commcaresession')/session/data/case_id_case"/>
+            </query>
+            <datum id="case_id_case" value="instance('commcaresession')/session/data/case_id_case"/>
+            <command value="'m1-f1'"/>
+          </create>
+        </partial>"""
+
+        self.assertXmlPartialEqual(
+            expected,
+            suite,
+            "./entry[2]/stack/create"
+        )
