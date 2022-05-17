38b5a859f9a529028005c395377f83d1b7ddfe34
Daniel Roberts <droberts@dimagi.com>
2022-05-17 18:34:50 -0500

    Make tests check that translations are synced from code

---
+++ scripts/test-make-translations.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+# source utils for logging functions log_group_{begin,end} and log_{success,fail}
+source ./scripts/bash-utils.sh
+
+log_group_begin "Check: translations"
+
+./scripts/make-translations.sh
+git --no-pager diff
+git update-index -q --refresh
+if ! git diff-index --quiet HEAD --; then
+    # Changes
+    log_fail "Translations are inconsistent with code.  Did you run './scripts/make-translations.sh'?"
+    git checkout -- locale/  # clean up
+    exit 1
+fi
+# No changes
+log_success "translations ok"
+log_group_end  # only log group end on success (prevents group collapse on failure)
