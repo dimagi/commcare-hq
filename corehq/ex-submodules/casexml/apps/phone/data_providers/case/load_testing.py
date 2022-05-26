1cfa7e85a8067004a5ab861d5384d82b7edf4481
Norman Hooper <nhooper@dimagi.com>
2022-05-26 19:48:47 +0100

    Drop type hints, rename param

--- corehq/ex-submodules/casexml/apps/phone/data_providers/case/load_testing.py
+++ corehq/ex-submodules/casexml/apps/phone/data_providers/case/load_testing.py
@@ -1,15 +1,9 @@
-from __future__ import annotations
-
 from copy import deepcopy
-from typing import TYPE_CHECKING
 
 from casexml.apps.phone.data_providers.case.utils import CaseSyncUpdate
 from casexml.apps.phone.xml import get_case_element, tostring
 
 from corehq.apps.app_manager.const import USERCASE_TYPE
-
-if TYPE_CHECKING:
-    from casexml.apps.phone.restore import RestoreState
 
 
 def transform_loadtest_update(update, factor):
@@ -27,25 +21,28 @@ def transform_loadtest_update(update, factor):
     return CaseSyncUpdate(case, update.sync_token, required_updates=update.required_updates)
 
 
-def get_xml_for_response(
-    update: CaseSyncUpdate,
-    restore_state: RestoreState,
-    total_cases: int,
-) -> list[str]:
+def get_xml_for_response(case_sync_update, restore_state, total_cases):
     """
     Adds the XML from the case_update to the restore response.
     If factor is > 1 it will append that many updates to the response for load testing purposes.
     """
     current_count = 0
-    original_update = update
+    original_update = case_sync_update
     elements = []
     loadtest_factor = restore_state.get_safe_loadtest_factor(total_cases)
     while current_count < loadtest_factor:
-        element = get_case_element(update.case, update.required_updates, restore_state.version)
+        element = get_case_element(
+            case_sync_update.case,
+            case_sync_update.required_updates,
+            restore_state.version,
+        )
         elements.append(tostring(element))
         current_count += 1
         if current_count < loadtest_factor:
-            update = transform_loadtest_update(original_update, current_count)
+            case_sync_update = transform_loadtest_update(
+                original_update,
+                current_count,
+            )
         # only add user case on the first iteration
         if original_update.case.type == USERCASE_TYPE:
             break
