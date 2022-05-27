883e384e1ad75d03f92b661f0aba027525c188bf
Daniel Miller <dmiller@dimagi.com>
2022-05-27 10:00:43 -0400

    Pull out function and add tests for functionality

---
+++ corehq/apps/notifications/tests/test_views.py
@@ -0,0 +1,51 @@
+from unittest.mock import patch
+
+from corehq.apps.accounting.models import Subscription
+from corehq.apps.groups.models import Group
+
+from ..views import NotificationsServiceRMIView
+
+
+def test_should_hide_feature_notifs_for_pro_with_groups():
+    with case_sharing_groups_patch(['agroupid']), active_service_type_patch("not_IMPLEMENTATION_or_SANDBOX"):
+        hide = NotificationsServiceRMIView._should_hide_feature_notifs("test", "pro")
+        assert hide, "notifications should be hidden for pro domain with groups"
+
+
+def test_should_hide_feature_notifs_for_pro_without_groups():
+    with case_sharing_groups_patch([]), active_service_type_patch("not_IMPLEMENTATION_or_SANDBOX"):
+        hide = NotificationsServiceRMIView._should_hide_feature_notifs("test", "pro")
+        assert not hide, "notifications should not be hidden for pro domain without groups"
+
+
+def test_should_hide_feature_notifs_for_non_pro_with_groups():
+    with case_sharing_groups_patch(['agroupid']), active_service_type_patch("not_IMPLEMENTATION_or_SANDBOX"):
+        hide = NotificationsServiceRMIView._should_hide_feature_notifs("test", None)
+        assert not hide, "notifications should not be hidden for pro domain without groups"
+
+
+def test_should_hide_feature_notifs_for_implementation_subscription():
+    with case_sharing_groups_patch([]), active_service_type_patch("IMPLEMENTATION"):
+        hide = NotificationsServiceRMIView._should_hide_feature_notifs("test", "pro")
+        assert hide, "notifications should be hidden for IMPLEMENTATION subscription"
+
+
+def test_should_hide_feature_notifs_for_sandbox_subscription():
+    with case_sharing_groups_patch([]), active_service_type_patch("SANDBOX"):
+        hide = NotificationsServiceRMIView._should_hide_feature_notifs("test", "pro")
+        assert hide, "notifications should be hidden for SANDBOX subscription"
+
+
+def active_service_type_patch(service_type=None):
+    def getter(domain):
+        return sub
+    sub = None if service_type is None else Subscription(service_type=service_type)
+    return patch.object(Subscription, "get_active_subscription_by_domain", getter)
+
+
+def case_sharing_groups_patch(groups):
+    # patch because quickcache makes this hard to test
+    def getter(domain, wrap):
+        assert not wrap, "expected wrap to be false"
+        return groups
+    return patch.object(Group, "get_case_sharing_groups", getter)
--- corehq/apps/notifications/views.py
+++ corehq/apps/notifications/views.py
@@ -47,19 +47,10 @@ class NotificationsServiceRMIView(JSONResponseMixin, View):
         else:
             plan_tier = 'basic'
 
-        def should_hide_feature_notifs(domain, plan):
-            groups = Group.get_case_sharing_groups(domain, wrap=False)
-            subscription_type = Subscription.get_active_subscription_by_domain(domain).service_type
-            if plan == 'pro' and groups != []:
-                return True
-            if subscription_type == 'IMPLEMENTATION' or subscription_type == 'SANDBOX':
-                return True
-            return False
-
         notifications = Notification.get_by_user(self.request.user,
                                                  self.request.couch_user,
                                                  plan_tier=plan_tier,
-                                                 hide_features=should_hide_feature_notifs(
+                                                 hide_features=self._should_hide_feature_notifs(
                                                      self.get_domain(), plan_tier))
         has_unread = len([x for x in notifications if not x['isRead']]) > 0
         last_seen_notification_date = LastSeenNotification.get_last_seen_notification_date_for_user(
@@ -70,6 +61,16 @@ class NotificationsServiceRMIView(JSONResponseMixin, View):
             'notifications': notifications,
             'lastSeenNotificationDate': last_seen_notification_date
         }
+
+    @staticmethod
+    def _should_hide_feature_notifs(domain, plan):
+        groups = Group.get_case_sharing_groups(domain, wrap=False)
+        subscription_type = Subscription.get_active_subscription_by_domain(domain).service_type
+        if plan == 'pro' and groups != []:
+            return True
+        if subscription_type == 'IMPLEMENTATION' or subscription_type == 'SANDBOX':
+            return True
+        return False
 
     @allow_remote_invocation
     def mark_as_read(self, in_data):
