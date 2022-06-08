61f3e75c53b240cda8bc4d03b51f8f1579f4b8f4
Amit Phulera <amitphulera999@gmail.com>
2022-06-08 14:05:26 +0530

    if user in not staff redirect to sms billables page

--- corehq/apps/sms/views.py
+++ corehq/apps/sms/views.py
@@ -27,6 +27,7 @@ from django.views.generic import View
 from couchdbkit import ResourceNotFound
 from django_prbac.utils import has_privilege
 from memoized import memoized
+from corehq.apps.smsbillables.dispatcher import SMSAdminInterfaceDispatcher
 
 from couchexport.export import export_raw
 from couchexport.models import Format
@@ -1387,6 +1388,13 @@ class GlobalSmsGatewayListView(CRUDPaginatedViewMixin, BaseAdminSectionView):
 
     @method_decorator(require_superuser)
     def dispatch(self, request, *args, **kwargs):
+        if not request.couch_user.is_staff:
+            return HttpResponseRedirect(
+                reverse(
+                    SMSAdminInterfaceDispatcher.name(),
+                    kwargs={'report_slug': 'sms_billables'}
+                )
+            )
         return super(GlobalSmsGatewayListView, self).dispatch(request, *args, **kwargs)
 
     @property
