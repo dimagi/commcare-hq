dc9f15b8816025312f259b844a27b1f1c82e219d
Jennifer Schweers <orange.jenny@gmail.com>
2022-06-13 17:09:36 -0400

    Extracted tableau js into file

---
+++ corehq/apps/reports/static/reports/js/tableau.js
@@ -0,0 +1,28 @@
+hqDefine("report/js/tableau", function () {
+    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        self = {};
+
+    self.initViz = function() {
+        var containerDiv = document.getElementById("vizContainer");
+        var url = _.template("https://<%- validate_hostname %>/<% if (is_server) { %>trusted/<%- ticket %>/<% } %><%- view_url %>")({
+            validate_hostname: initialPageData.get("validate_hostname"),
+            is_server: initialPageData.get("server_type") === "server",
+            ticket: initialPageData.get("ticket"),
+            view_url: initialPageData.get("view_url"),
+        });
+
+        var options = {
+            hideTabs: true,
+            width: "800px",
+            height: "700px",
+        };
+
+        var viz = new tableau.Viz(containerDiv, url, options);
+    };
+
+    $(document).ready(function() {
+        self.initViz();
+    });
+
+    return 1;
+});
