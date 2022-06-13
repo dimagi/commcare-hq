8cf503895a5dc7c11bbacfd29e632e046df4fbae
Matt Riley <mriley@dimagi.com>
2022-06-13 11:57:56 -0400

    Fixed translation issue with OData Report string

--- corehq/apps/enterprise/enterprise.py
+++ corehq/apps/enterprise/enterprise.py
@@ -2,7 +2,7 @@ import re
 from datetime import datetime, timedelta
 
 from django.utils.translation import gettext as _
-from django.utils.translation import gettext_lazy as _lazy
+from django.utils.translation import gettext_lazy
 
 from memoized import memoized
 
@@ -256,7 +256,7 @@ class EnterpriseFormReport(EnterpriseReport):
 
 
 class EnterpriseODataReport(EnterpriseReport):
-    title = _lazy('OData Feeds')
+    title = gettext_lazy('OData Feeds')
     MAXIMUM_EXPECTED_EXPORTS = 150
 
     def __init__(self, account, couch_user):
