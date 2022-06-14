c3f0074397f2d30bcedc1576a61e8d386ed40da6
Jennifer Schweers <orange.jenny@gmail.com>
2022-06-13 21:02:19 -0400

    WIP

--- corehq/apps/reports/static/reports/js/tableau.js
+++ corehq/apps/reports/static/reports/js/tableau.js
@@ -13,12 +13,14 @@ hqDefine("report/js/tableau", [
 
     self.initViz = function() {
         var containerDiv = document.getElementById("vizContainer");
+console.log("container: " + containerDiv);
         var url = _.template("https://<%- validate_hostname %>/<% if (is_server) { %>trusted/<%- ticket %>/<% } %><%- view_url %>")({
             validate_hostname: initialPageData.get("validate_hostname"),
             is_server: initialPageData.get("server_type") === "server",
             ticket: initialPageData.get("ticket"),
             view_url: initialPageData.get("view_url"),
         });
+console.log("url: " + url);
 
         var options = {
             hideTabs: true,
@@ -30,8 +32,12 @@ hqDefine("report/js/tableau", [
     };
 
     $(document).ready(function() {
+console.log("ready");
         self.initViz();
         $(window).resize(_.debounce(self.initViz, 300));
+        $("body").on("load", function () {
+console.log("body loaded");
+        });
     });
 
     return 1;
