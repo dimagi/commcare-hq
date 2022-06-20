a23ac85fd18891e90dc67a246d45be06e30d35eb
Dominic Ochoa <dochoa@dimagi.com>
2022-06-20 12:20:59 -0400

    added 'null=True' to migration

--- corehq/apps/reports/migrations/0008_tableauvisualization_title.py
+++ corehq/apps/reports/migrations/0008_tableauvisualization_title.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='tableauvisualization',
             name='title',
-            field=models.CharField(default='', max_length=32),
+            field=models.CharField(default='', max_length=32, null=True),
             preserve_default=False,
         ),
     ]
