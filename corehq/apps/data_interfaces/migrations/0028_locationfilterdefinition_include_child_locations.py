122709d4d39d05ce14dae3a734b1a227f57ea0cc
Charl Smit <csmit@dimagi.com>
2022-06-20 09:11:07 +0200

    Minimise migration files

--- corehq/apps/data_interfaces/migrations/0028_locationfilterdefinition_include_child_locations.py
+++ corehq/apps/data_interfaces/migrations/0028_locationfilterdefinition_include_child_locations.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='locationfilterdefinition',
             name='include_child_locations',
-            field=models.BooleanField(default=False),
+            field=models.BooleanField(default=True),
         ),
     ]
