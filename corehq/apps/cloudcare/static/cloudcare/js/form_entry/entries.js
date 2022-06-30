29737b7e60a242bc14b10f21e3897826c01b557c
Graham Herceg <gherceg@dimagi.com>
2022-06-30 14:13:12 -0700

    remove minimum column width

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
@@ -42,7 +42,6 @@ hqDefine("cloudcare/js/form_entry/entries", function () {
     // Checkbox questions in a Question List Group.
     Entry.prototype.getColStyle = function (numChoices) {
         var colWidth = parseInt(12 / (numChoices + 1)) || 1;
-        colWidth = colWidth >= 3 ? colWidth : 3;
         return 'col-xs-' + colWidth;
     };
 
