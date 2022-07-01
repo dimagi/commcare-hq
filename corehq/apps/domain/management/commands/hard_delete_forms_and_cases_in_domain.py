c42d70dad9ceec4cd42fe5598c076e3fb67b6152
Graham Herceg <gherceg@dimagi.com>
2022-07-01 12:44:04 -0700

    check if domain is active before deleting forms and cases

--- corehq/apps/domain/management/commands/hard_delete_forms_and_cases_in_domain.py
+++ corehq/apps/domain/management/commands/hard_delete_forms_and_cases_in_domain.py
@@ -3,6 +3,7 @@ import logging
 from django.core.management import BaseCommand
 
 from ...deletion import delete_all_cases, delete_all_forms
+from ...models import Domain
 
 logger = logging.getLogger(__name__)
 
@@ -18,8 +19,21 @@ class Command(BaseCommand):
             default=False,
             help='Skip important confirmation warnings.',
         )
+        parser.add_argument(
+            '--allow-active-domain',
+            action='store_true',
+            dest='allow_active_domain',
+            default=False,
+            help='Override deleted domain check.',
+        )
 
     def handle(self, domain, **options):
+        domain_obj = Domain.get_by_name(domain)
+        if not options['allow_active_domain'] and domain_obj and not domain_obj.doc_type.endswith('-Deleted'):
+            print(f'WARNING: Domain {domain} is an active domain. If your intention is to delete the domain, you '
+                  f'should use the `delete_domain` command instead. If you are sure you want to delete forms and'
+                  f'cases for this domain, use the "--allow-active-domain" argument.')
+
         if not options['noinput']:
             confirm = input(
                 """
