b119b12e25af966589f103c304cce583d6a4be3a
Graham Herceg <gherceg@dimagi.com>
2022-06-30 17:52:24 -0700

    ensure domain does not exist

--- corehq/apps/cleanup/management/commands/delete_es_docs_in_domain.py
+++ corehq/apps/cleanup/management/commands/delete_es_docs_in_domain.py
@@ -2,7 +2,7 @@ import sys
 
 from django.core.management import BaseCommand
 
-from corehq.apps.cleanup.tasks import _get_missing_domains
+from corehq.apps.domain.models import Domain
 from corehq.apps.es import AppES, CaseES, CaseSearchES, FormES, GroupES, UserES
 from corehq.apps.es.registry import registry_entry
 from corehq.apps.es.transient_util import doc_adapter_from_info
@@ -14,8 +14,8 @@ class Command(BaseCommand):
         parser.add_argument('domain')
 
     def handle(self, domain, **options):
-        deleted_domains = _get_missing_domains()
-        if domain not in deleted_domains:
+        domain_obj = Domain.get_by_name(domain)
+        if domain_obj:
             print(f"{domain} has not been deleted. This command is intended for use on deleted domains only.")
             sys.exit(1)
 
