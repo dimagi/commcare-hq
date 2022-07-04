9b896437fd0f39766442ac66863f300b2d913dd4
Jing Cheng <jcheng@dimagi.com>
2022-07-04 00:33:57 -0400

    improve test case

--- corehq/apps/domain/tests/test_password_strength.py
+++ corehq/apps/domain/tests/test_password_strength.py
@@ -7,6 +7,7 @@ from corehq.apps.domain.forms import clean_password
 @override_settings(MINIMUM_PASSWORD_LENGTH=0, MINIMUM_ZXCVBN_SCORE=2)
 class PasswordStrengthTest(SimpleTestCase):
 
+    # Test zxcvbn library strength
     def test_score_0_password(self):
         self.assert_bad_password(PASSWORDS_BY_STRENGTH[0])
 
@@ -16,17 +17,8 @@ class PasswordStrengthTest(SimpleTestCase):
     def test_score_2_password(self):
         self.assert_good_password(PASSWORDS_BY_STRENGTH[2])
 
-    @override_settings(MINIMUM_PASSWORD_LENGTH=8)
-    def test_length_2_password(self):
-        self.assert_bad_password(PASSWORDS_BY_STRENGTH[2])
-
-    @override_settings(MINIMUM_PASSWORD_LENGTH=8)
     def test_score_3_password(self):
         self.assert_good_password(PASSWORDS_BY_STRENGTH[3])
-
-    @override_settings(MINIMUM_PASSWORD_LENGTH=12)
-    def test_length_3_password(self):
-        self.assert_bad_password(PASSWORDS_BY_STRENGTH[3])
 
     def test_score_4_password(self):
         self.assert_good_password(PASSWORDS_BY_STRENGTH[4])
@@ -37,6 +29,26 @@ class PasswordStrengthTest(SimpleTestCase):
 
     @override_settings(MINIMUM_ZXCVBN_SCORE=3)
     def test_sensitivity_to_minimum_zxcvbn_score_setting_good(self):
+        self.assert_good_password(PASSWORDS_BY_STRENGTH[3])
+
+    # Test minimum password length
+    @override_settings(MINIMUM_PASSWORD_LENGTH=8)
+    def test_length_5_password(self):
+        self.assert_bad_password("e3r4f")
+
+    # Password has less than the minimum requirement
+    @override_settings(MINIMUM_PASSWORD_LENGTH=7)
+    def test_score_1_length_9_password(self):
+        self.assert_bad_password(PASSWORDS_BY_STRENGTH[1])
+
+    # Password has exactly the minimum requirement
+    @override_settings(MINIMUM_PASSWORD_LENGTH=7)
+    def test_score_2_length_7_password(self):
+        self.assert_good_password(PASSWORDS_BY_STRENGTH[2])
+
+    # Password has more than the minimum requirement
+    @override_settings(MINIMUM_SUBSCRIPTION_LENGTH=8)
+    def test_score_3_length_10_password(self):
         self.assert_good_password(PASSWORDS_BY_STRENGTH[3])
 
     def assert_good_password(self, password):
