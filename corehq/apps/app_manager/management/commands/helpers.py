a74d5b8e7a16131418c1fc4483301a4fb752ca06
AddisonDunn <dunnaddisonw@gmail.com>
2022-07-07 11:38:52 -0400

    WIP

--- corehq/apps/app_manager/management/commands/helpers.py
+++ corehq/apps/app_manager/management/commands/helpers.py
@@ -1,7 +1,10 @@
+from datetime import timedelta
 import logging
 from collections import namedtuple
+from time import time
 
 from django.core.management import BaseCommand
+from django.db import models
 
 from corehq.apps.app_manager.models import Application
 from corehq.util.couch import DocUpdate, iter_update
@@ -25,6 +28,19 @@ def get_all_app_ids(domain=None, include_builds=False):
 
 SaveError = namedtuple('SaveError', 'id error reason')
 
+
+APP_MIGRATION_CMD_DOMAIN_LIST_POSITION = 0
+
+def get_domain_list_position():
+    return APP_MIGRATION_CMD_DOMAIN_LIST_POSITION
+
+def increment_domain_list_position():
+    global APP_MIGRATION_CMD_DOMAIN_LIST_POSITION
+    APP_MIGRATION_CMD_DOMAIN_LIST_POSITION += 1
+
+def reset_domain_list_position():
+    global APP_MIGRATION_CMD_DOMAIN_LIST_POSITION
+    APP_MIGRATION_CMD_DOMAIN_LIST_POSITION = 0
 
 class AppMigrationCommandBase(BaseCommand):
     """
@@ -54,18 +70,36 @@ class AppMigrationCommandBase(BaseCommand):
             default=False,
             help="Perform the migration but don't save any changes",
         )
+        parser.add_argument(
+            '--restartable',
+            action='store_true',
+            default=False,
+            help='''If restarted, the command will continue at the place in the domains list where it left off 
+            when it was cancelled. This option is 'True' by default. Restartability does not persist through 
+            different Django instances. Should not be used with a single domain.''',
+        )
 
     def handle(self, **options):
+        start_time = time()
         self.options = options
         if self.options['domain']:
             domains = [self.options['domain']]
+        elif self.restartable:
+            domains = self.get_domains()[get_domain_list_position():] or [None]
         else:
             domains = self.get_domains() or [None]
         for domain in domains:
+            print(f'domain pos: {get_domain_list_position()}')
             app_ids = self.get_app_ids(domain)
             logger.info('migrating {} apps{}'.format(len(app_ids), f" in {domain}" if domain else ""))
             iter_update(Application.get_db(), self._migrate_app, app_ids, verbose=True, chunksize=self.chunk_size)
-        logger.info('done')
+            if self.restartable:
+                increment_domain_list_position()
+        end_time = time()
+        execution_time_seconds = end_time - start_time
+        logger.info(f"Completed in {timedelta(seconds=execution_time_seconds)}.")
+        if self.restartable:
+            reset_domain_list_position()
 
     @property
     def is_dry_run(self):
@@ -78,6 +112,10 @@ class AppMigrationCommandBase(BaseCommand):
     @property
     def log_debug(self):
         return self.options.get("verbosity", 0) > 2
+
+    @property
+    def restartable(self):
+         return self.options.get('restartable', False)
 
     def _doc_types(self):
         doc_types = ["Application", "Application-Deleted"]
@@ -98,7 +136,7 @@ class AppMigrationCommandBase(BaseCommand):
 
     @staticmethod
     def increment_app_version(app_doc):
-        if not app_doc.get('copy_of') and app_doc.get('version'):
+        if not getattr(app_doc, 'copy_of', False) and getattr(app_doc, 'version', False):
             app_doc['version'] = app_doc['version'] + 1
         return app_doc
 
