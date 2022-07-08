c063f653ff02aebb10559cb20c94b6c6ae227d16
Simon Kelly <skelly@dimagi.com>
2022-07-08 11:19:17 +0200

    fix test
    Co-authored-by: Ethan Soergel <esoergel@users.noreply.github.com>

--- corehq/apps/users/tests/test_signals.py
+++ corehq/apps/users/tests/test_signals.py
@@ -109,7 +109,7 @@ class TestUserSyncToEs(SimpleTestCase):
         self.assertEqual(len(results), 1)
         self.assertEqual(results[0], {
             '_id': user._id,
-            'domain': self.user.domain,
+            'domain': user.domain,
             'username': user.username,
             'is_active': True,
             'first_name': user.first_name,
