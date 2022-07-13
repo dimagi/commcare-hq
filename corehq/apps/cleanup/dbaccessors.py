ec0a03924da4a65a8080a37af26a8e1c9fe50520
Graham Herceg <gherceg@dimagi.com>
2022-07-13 11:08:40 -0700

    move to dbaccessors.py and add tests

---
+++ corehq/apps/cleanup/dbaccessors.py
@@ -0,0 +1,74 @@
+from collections import defaultdict
+
+from django.db import connections
+
+from corehq.apps.domain.models import Domain
+from corehq.apps.es import AppES, CaseES, CaseSearchES, FormES, GroupES, UserES
+from corehq.apps.userreports.util import (
+    LEGACY_UCR_TABLE_PREFIX,
+    UCR_TABLE_PREFIX,
+)
+from corehq.form_processor.models import CommCareCase, XFormInstance
+from corehq.sql_db.connections import UCR_ENGINE_ID, ConnectionManager
+from corehq.sql_db.util import get_db_aliases_for_partitioned_query
+
+
+def find_sql_forms_for_deleted_domains():
+    form_count_by_deleted_domain = {}
+    for domain in Domain.get_deleted_domain_names():
+        form_count = 0
+        for db_name in get_db_aliases_for_partitioned_query():
+            form_count += XFormInstance.objects.using(db_name).filter(domain=domain).count()
+        if form_count:
+            form_count_by_deleted_domain[domain] = form_count
+
+    return form_count_by_deleted_domain
+
+
+def find_sql_cases_for_deleted_domains():
+    case_count_by_deleted_domain = {}
+    for domain in Domain.get_deleted_domain_names():
+        case_count = 0
+        for db_name in get_db_aliases_for_partitioned_query():
+            case_count += CommCareCase.objects.using(db_name).filter(domain=domain).count()
+        if case_count:
+            case_count_by_deleted_domain[domain] = case_count
+
+    return case_count_by_deleted_domain
+
+
+def find_es_docs_for_deleted_domains():
+    es_doc_counts_by_deleted_domain = defaultdict(dict)
+    for domain in Domain.get_deleted_domain_names():
+        for hqESQuery in [AppES, CaseES, CaseSearchES, FormES, GroupES, UserES]:
+            query = hqESQuery().domain(domain)
+            count = query.count()
+            if count != 0:
+                es_doc_counts_by_deleted_domain[domain][hqESQuery.index] = count
+
+    return es_doc_counts_by_deleted_domain
+
+
+def find_ucr_tables_for_deleted_domains():
+    deleted_domain_names = Domain.get_deleted_domain_names()
+
+    connection_name = ConnectionManager().get_django_db_alias(UCR_ENGINE_ID)
+    table_names = connections[connection_name].introspection.table_names()
+    ucr_table_names = [name for name in table_names if
+                       name.startswith(UCR_TABLE_PREFIX) or name.startswith(LEGACY_UCR_TABLE_PREFIX)]
+
+    deleted_domains_to_tables = defaultdict(list)
+
+    for ucr_table_name in ucr_table_names:
+        table_domain = _get_domain_for_table_name(ucr_table_name)
+        if table_domain in deleted_domain_names:
+            deleted_domains_to_tables[table_domain].append(ucr_table_name)
+
+    return deleted_domains_to_tables
+
+
+def _get_domain_for_table_name(ucr_table_name):
+    if ucr_table_name.startswith(UCR_TABLE_PREFIX):
+        return ucr_table_name.split('_')[1]
+    else:
+        return ucr_table_name.split('_')[2]
--- corehq/apps/cleanup/tasks.py
+++ corehq/apps/cleanup/tasks.py
@@ -1,19 +1,18 @@
-from collections import defaultdict
 from datetime import datetime
 
 from django.conf import settings
 from django.core.management import call_command
-from django.db import connections
 
 from celery.schedules import crontab
 from celery.task import periodic_task
 
-from corehq.apps.domain.models import Domain
-from corehq.apps.es import AppES, CaseES, CaseSearchES, FormES, GroupES, UserES
+from corehq.apps.cleanup.dbaccessors import (
+    find_es_docs_for_deleted_domains,
+    find_sql_cases_for_deleted_domains,
+    find_sql_forms_for_deleted_domains,
+    find_ucr_tables_for_deleted_domains,
+)
 from corehq.apps.hqwebapp.tasks import mail_admins_async
-from corehq.form_processor.models import XFormInstance, CommCareCase
-from corehq.sql_db.connections import UCR_ENGINE_ID, ConnectionManager
-from corehq.sql_db.util import get_db_aliases_for_partitioned_query
 
 UNDEFINED_XMLNS_LOG_DIR = settings.LOG_HOME
 
@@ -25,17 +24,10 @@ def clear_expired_sessions():
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def check_for_sql_cases_without_existing_domain():
-    case_count_by_deleted_domain = {}
-    for domain in Domain.get_deleted_domain_names():
-        case_count = 0
-        for db_name in get_db_aliases_for_partitioned_query():
-            case_count += CommCareCase.objects.using(db_name).filter(domain=domain).count()
-        if case_count:
-            case_count_by_deleted_domain[domain] = case_count
-
+    case_count_by_deleted_domain = find_sql_cases_for_deleted_domains()
     if case_count_by_deleted_domain:
         mail_admins_async.delay(
-            'There exist SQL cases belonging to a deleted domain',
+            'There exist SQL cases belonging to deleted domain(s)',
             f'{case_count_by_deleted_domain}\nConsider hard_delete_forms_and_cases_in_domain'
         )
     elif _is_monday():
@@ -46,17 +38,10 @@ def check_for_sql_cases_without_existing_domain():
 
 @periodic_task(run_every=crontab(minute=0, hour=1), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def check_for_sql_forms_without_existing_domain():
-    form_count_by_deleted_domain = {}
-    for domain in Domain.get_deleted_domain_names():
-        form_count = 0
-        for db_name in get_db_aliases_for_partitioned_query():
-            form_count += XFormInstance.objects.using(db_name).filter(domain=domain).count()
-        if form_count:
-            form_count_by_deleted_domain[domain] = form_count
-
+    form_count_by_deleted_domain = find_sql_forms_for_deleted_domains()
     if form_count_by_deleted_domain:
         mail_admins_async.delay(
-            'There exist SQL forms belonging to a deleted domain',
+            'There exist SQL forms belonging to deleted domain(s)',
             f'{form_count_by_deleted_domain}\nConsider hard_delete_forms_and_cases_in_domain'
         )
     elif _is_monday():
@@ -67,19 +52,14 @@ def check_for_sql_forms_without_existing_domain():
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def check_for_elasticsearch_data_without_existing_domain():
-    issue_found = False
-    for domain_name in Domain.get_deleted_domain_names():
-        for hqESQuery in [AppES, CaseES, CaseSearchES, FormES, GroupES, UserES]:
-            query = hqESQuery().domain(domain_name)
-            count = query.count()
-            if query.count() != 0:
-                issue_found = True
-                mail_admins_async.delay(
-                    'ES index "%s" contains %s items belonging to a deleted domain "%s"' % (
-                        query.index, count, domain_name
-                    ), ''
-                )
-    if not issue_found and _is_monday():
+    es_docs_by_deleted_domain = find_es_docs_for_deleted_domains()
+    if es_docs_by_deleted_domain:
+        for domain, es_docs in es_docs_by_deleted_domain.items():
+            mail_admins_async.delay(
+                f'Deleted domain "{domain}" has remaining ES docs',
+                f'{es_docs}\nConsider delete_es_docs_for_domain'
+            )
+    elif _is_monday():
         mail_admins_async.delay(
             'All data in ES belongs to valid domains', ''
         )
@@ -87,19 +67,7 @@ def check_for_elasticsearch_data_without_existing_domain():
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
 def check_for_ucr_tables_without_existing_domain():
-    deleted_domain_names = Domain.get_deleted_domain_names()
-
-    connection_name = ConnectionManager().get_django_db_alias(UCR_ENGINE_ID)
-    table_names = connections[connection_name].introspection.table_names()
-    ucr_table_names = [name for name in table_names if name.startswith('config_report')]
-
-    deleted_domains_to_tables = defaultdict(list)
-
-    for ucr_table_name in ucr_table_names:
-        table_domain = ucr_table_name.split('_')[2]
-        if table_domain in deleted_domain_names:
-            deleted_domains_to_tables[table_domain].append(ucr_table_name)
-
+    deleted_domains_to_tables = find_ucr_tables_for_deleted_domains()
     if deleted_domains_to_tables:
         for deleted_domain in deleted_domains_to_tables:
             mail_admins_async.delay(
