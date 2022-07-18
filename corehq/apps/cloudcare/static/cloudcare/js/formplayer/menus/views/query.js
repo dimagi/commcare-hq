0b8d091aa9ad78d51b1d2d720c258af446f15468
robert-costello <robertcostello81@gmail..com>
2022-07-18 14:49:36 -0400

    set error to empty string on model on clear

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
@@ -233,6 +233,7 @@ hqDefine("cloudcare/js/formplayer/menus/views/query", function () {
         clear: function () {
             var self = this;
             self.model.set('value', '');
+            self.model.set('error', '');
             self.errorMessage = "";
             self.model.set('searchForBlank', false);
             if (self.ui.date.length) {
