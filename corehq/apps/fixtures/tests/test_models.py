09ed573074e77cc44729a5b431c3f12deb36fa79
Daniel Miller <dmiller@dimagi.com>
2022-07-19 10:44:00 -0400

    Test attrs field types instead of jsonobject ones

--- corehq/apps/fixtures/tests/test_models.py
+++ corehq/apps/fixtures/tests/test_models.py
@@ -7,9 +7,6 @@ from corehq.util.tests.test_jsonattrs import set_json_value
 
 from ..models import (
     Field,
-    FieldList,
-    FixtureItemField,
-    FixtureTypeField,
     LookupTable,
     LookupTableRow,
     LookupTableRowOwner,
@@ -287,80 +284,55 @@ class TestLookupTableRow(TestCase):
 class TestFieldTypes(SimpleTestCase):
 
     def test_type_field_equality(self):
-        f1 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=True)
-        f2 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=True)
+        f1 = TypeField(name="name", properties=["a", "b"], is_indexed=True)
+        f2 = TypeField(name="name", properties=["a", "b"], is_indexed=True)
         self.assertIsNot(f1, f2)
         self.assertEqual(f1, f2)
 
-        f3 = FixtureTypeField(field_name="name", properties=["a"], is_indexed=True)
+        f3 = TypeField(name="name", properties=["a"], is_indexed=True)
         self.assertNotEqual(f1, f3)
         self.assertNotEqual(f1, None)
 
-        f4 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=False)
+        f4 = TypeField(name="name", properties=["a", "b"], is_indexed=False)
         self.assertNotEqual(f1, f4)
 
-        f5 = FixtureTypeField(field_name="diff", properties=["a", "b"], is_indexed=True)
+        f5 = TypeField(name="diff", properties=["a", "b"], is_indexed=True)
         self.assertNotEqual(f1, f5)
 
     def test_type_field_hash(self):
-        f1 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=True)
-        f2 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=True)
+        f1 = TypeField(name="name", properties=["a", "b"], is_indexed=True)
+        f2 = TypeField(name="name", properties=["a", "b"], is_indexed=True)
         self.assertEqual(hash(f1), hash(f2))
 
-        f3 = FixtureTypeField(field_name="name", properties=["a"], is_indexed=True)
+        f3 = TypeField(name="name", properties=["a"], is_indexed=True)
         self.assertNotEqual(hash(f1), hash(f3))
 
-        f4 = FixtureTypeField(field_name="name", properties=["a", "b"], is_indexed=False)
+        f4 = TypeField(name="name", properties=["a", "b"], is_indexed=False)
         self.assertNotEqual(hash(f1), hash(f4))
 
-        f5 = FixtureTypeField(field_name="diff", properties=["a", "b"], is_indexed=True)
+        f5 = TypeField(name="diff", properties=["a", "b"], is_indexed=True)
         self.assertNotEqual(hash(f1), hash(f5))
 
     def test_field_equality(self):
-        f1 = FixtureItemField(field_value="val", properties={"a": "1", "b": "2"})
-        f2 = FixtureItemField(field_value="val", properties={"b": "2", "a": "1"})
+        f1 = Field(value="val", properties={"a": "1", "b": "2"})
+        f2 = Field(value="val", properties={"b": "2", "a": "1"})
         self.assertIsNot(f1, f2)
         self.assertEqual(f1, f2)
 
-        f3 = FixtureItemField(field_value="val", properties={"prop": "val"})
+        f3 = Field(value="val", properties={"prop": "val"})
         self.assertNotEqual(f1, f3)
         self.assertNotEqual(f1, None)
 
-        f4 = FixtureItemField(field_value="dif", properties={"a": "1", "b": "2"})
+        f4 = Field(value="dif", properties={"a": "1", "b": "2"})
         self.assertNotEqual(f1, f4)
 
     def test_field_hash(self):
-        f1 = FixtureItemField(field_value="val", properties={"a": "1", "b": "2"})
-        f2 = FixtureItemField(field_value="val", properties={"b": "2", "a": "1"})
+        f1 = Field(value="val", properties={"a": "1", "b": "2"})
+        f2 = Field(value="val", properties={"b": "2", "a": "1"})
         self.assertEqual(hash(f1), hash(f2))
 
-        f3 = FixtureItemField(field_value="val", properties={"prop": "val"})
+        f3 = Field(value="val", properties={"prop": "val"})
         self.assertNotEqual(hash(f1), hash(f3))
 
-        f4 = FixtureItemField(field_value="dif", properties={"a": "1", "b": "2"})
+        f4 = Field(value="dif", properties={"a": "1", "b": "2"})
         self.assertNotEqual(hash(f1), hash(f4))
-
-    def test_field_list_equality(self):
-        f1 = FixtureItemField(field_value="val", properties={})
-        f2 = FixtureItemField(field_value="val", properties={})
-        x1 = FieldList(field_list=[f1])
-        x2 = FieldList(field_list=[f2])
-        self.assertIsNot(x1, x2)
-        self.assertEqual(x1, x2)
-        self.assertEqual(hash(x1), hash(x2))
-
-        f3 = FixtureItemField(field_value="val", properties={"prop": "val"})
-        x3 = FieldList(field_list=[f3])
-        self.assertNotEqual(x1, x3)
-        self.assertNotEqual(x1, None)
-
-    def test_field_list_hash(self):
-        f1 = FixtureItemField(field_value="val", properties={})
-        f2 = FixtureItemField(field_value="val", properties={})
-        x1 = FieldList(field_list=[f1])
-        x2 = FieldList(field_list=[f2])
-        self.assertEqual(hash(x1), hash(x2))
-
-        f3 = FixtureItemField(field_value="val", properties={"prop": "val"})
-        x3 = FieldList(field_list=[f3])
-        self.assertNotEqual(hash(x1), hash(x3))
