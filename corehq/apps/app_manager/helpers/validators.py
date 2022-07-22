adecceca3ef942bdd49183a6dbb8b38f1d33e825
Clayton Sims <csims@dimagi.com>
2022-07-22 13:37:10 -0400

    add helpme doc tag

--- corehq/apps/app_manager/helpers/validators.py
+++ corehq/apps/app_manager/helpers/validators.py
@@ -1080,7 +1080,18 @@ class FormValidator(IndexedFormBaseValidator):
 
 
 class AdvancedFormValidator(IndexedFormBaseValidator):
-    def check_actions(self):
+    def check_actions(self):1
+        # HELPME
+        #
+        # This method has been flagged for refactoring due to its complexity and
+        # frequency of touches in changesets
+        #
+        # If you are writing code that touches this method, your changeset
+        # should leave the method better than you found it.
+        #
+        # Please remove this flag when this method no longer triggers an 'E' or 'F'
+        # classification from the radon code static analysis
+
         errors = []
 
         from corehq.apps.app_manager.models import AdvancedOpenCaseAction, LoadUpdateAction
