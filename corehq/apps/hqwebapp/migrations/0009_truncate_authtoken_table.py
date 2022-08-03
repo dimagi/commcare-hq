8af9752f2c6f9a704e2c4bd92fb25d0c034c7b69
Graham Herceg <gherceg@dimagi.com>
2022-08-03 14:02:34 -0700

    add migration to truncate authtoken_token table

---
+++ corehq/apps/hqwebapp/migrations/0009_truncate_authtoken_table.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+
+def _truncate_authtoken_table():
+    # NOTE: truncating to preserve migration records
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor.execute("TRUNCATE TABLE `authtoken_token`")
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('hqwebapp', '0008_hqoauthapplication'),
+    ]
+
+    operations = [
+        migrations.RunPython(_truncate_authtoken_table, reverse_code=migrations.RunPython.noop)
+    ]
