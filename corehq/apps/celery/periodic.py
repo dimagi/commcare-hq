ee609333c5717e3a8bfd74656f00ecef61a5257d
Amit Phulera <amitphulera999@gmail.com>
2022-08-04 14:32:42 +0530

    update: periodic_task to use the hq task

--- corehq/apps/celery/periodic.py
+++ corehq/apps/celery/periodic.py
@@ -1,5 +1,4 @@
 from celery import Task
-from django.conf import settings
 
 """
 @periodic_task has been removed from celery>=5.0.0.
@@ -34,10 +33,5 @@ def periodic_task(**options):
             - celery.schedules.crontab interval
         options (dict): https://docs.celeryq.dev/en/latest/reference/celery.app.task.html#celery.app.task.Task.apply_async # noqa E501
     """
-    options.setdefault('options', {})
-    options.setdefault('serializer', 'json')
-    if not options.get('queue'):
-        options['queue'] = getattr(settings, 'CELERY_MAIN_QUEUE', 'celery')
-    options['options']['queue'] = options.get('queue')
-    from corehq.apps.celery import app
-    return app.task(base=PeriodicTask, **options)
+    from corehq.apps.celery.shared_task import task
+    return task(base=PeriodicTask, **options)
