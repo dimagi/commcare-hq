e99b35b51353f1cf2be63bb39c41e6085f525313
Graham Herceg <gherceg@dimagi.com>
2022-08-05 06:49:41 -0700

    add add_if_exists_raw utility

--- corehq/apps/hqwebapp/migrations/0009_truncate_authtoken_table.py
+++ corehq/apps/hqwebapp/migrations/0009_truncate_authtoken_table.py
@@ -1,6 +1,6 @@
 from django.db import migrations
 
-from corehq.util.django_migrations import add_if_not_exists_raw
+from corehq.util.django_migrations import add_if_exists_raw
 
 
 class Migration(migrations.Migration):
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.RunSQL(
-            add_if_not_exists_raw("TRUNCATE TABLE authtoken_token", name='authtoken_table'),
+            add_if_exists_raw("TRUNCATE TABLE authtoken_token", name='authtoken_table'),
             reverse_sql=migrations.RunSQL.noop
         )
     ]
