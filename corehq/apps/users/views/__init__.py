49bad73443457e9bae442e51ac41885b3480b299
Rynhard <rpietersen@dimagi.com>
2022-08-11 14:51:58 +0200

    changes to get_role_users_count

--- corehq/apps/users/views/__init__.py
+++ corehq/apps/users/views/__init__.py
@@ -523,11 +523,7 @@ class BaseRoleAccessView(BaseUserSettingsView):
 
             try:
                 user_count = get_role_user_count(role.domain, role.couch_id)
-                invitations = Invitation.objects.filter(role="user-role:" + role_data["_id"], is_accepted=False)
-                if user_count or invitations:
-                    role_data["hasUsersAssigned"] = True
-                else:
-                    role_data["hasUsersAssigned"] = False
+                role_data["hasUsersAssigned"] = bool(user_count)
             except TypeError:
                 # when query_result['hits'] returns None due to an ES issue
                 show_es_issue = True
@@ -950,19 +946,17 @@ def delete_user_role(request, domain):
         return JsonResponse({})
     role_data = json.loads(request.body.decode('utf-8'))
     user_count = get_role_user_count(domain, role_data["_id"])
-    invitations = Invitation.objects.filter(role="user-role:" + role_data["_id"], is_accepted=False)
-    if user_count or invitations:
+    if user_count:
         return JsonResponse({
             "message": ngettext(
                 "Unable to delete role '{role}'. "
-                "It has one user and {invite_count} invitations still assigned to it. "
+                "It has one user and pending invitations still assigned to it. "
                 "Remove all users assigned to the role before deleting it.",
                 "Unable to delete role '{role}'. "
-                "It has {user_count} users and {invite_count} invitations still assigned to it. "
+                "It has {user_count} users and pending invitations still assigned to it. "
                 "Remove all users assigned to the role before deleting it.",
                 user_count,
-                invitations.count(),
-            ).format(role=role_data["name"], user_count=user_count, invite_count=invitations.count())
+            ).format(role=role_data["name"], user_count=user_count)
         }, status=400)
     try:
         role = UserRole.objects.by_couch_id(role_data["_id"], domain=domain)
