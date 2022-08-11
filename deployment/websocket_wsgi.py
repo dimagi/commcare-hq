1a511f23acc873c95ed60e8a918bff5c6ba68ebc
Daniel Roberts <droberts@dimagi.com>
2022-08-11 10:37:15 -0500

    Fix websockets process after celery upgrade
    make it do the same patching that manage.py does

--- deployment/websocket_wsgi.py
+++ deployment/websocket_wsgi.py
@@ -1,13 +1,12 @@
 import os
 import gevent.socket
 import redis.connection
-from manage import _set_source_root_parent, _set_source_root
+from manage import init_hq_python_path, run_patches
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-_set_source_root_parent('submodules')
-_set_source_root(os.path.join('corehq', 'ex-submodules'))
-_set_source_root(os.path.join('custom', '_legacy'))
+init_hq_python_path()
+run_patches()
 
 redis.connection.socket = gevent.socket
 from ws4redis.uwsgi_runserver import uWSGIWebsocketServer
