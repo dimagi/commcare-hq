2113ab525f33b6583b41836b4a284b78b1d52991
Simon Kelly <skelly@dimagi.com>
2022-08-12 12:26:29 +0200

    filter by domain

--- corehq/apps/hqcase/api/get_bulk.py
+++ corehq/apps/hqcase/api/get_bulk.py
@@ -4,7 +4,21 @@ from corehq.apps.hqcase.api.core import serialize_es_case
 
 def get_bulk(domain, case_ids):
     results = ElasticCaseSearch().get_docs(case_ids)
+    missing = 0
+    final_results = []
+    for doc in results:
+        if doc['domain'] != domain:
+            final_results.append(_get_error_doc(doc['_id']))
+            missing += 1
+        else:
+            final_results.append(serialize_es_case(doc))
+
     return {
-        'matching_records': len(results),
-        'cases': [serialize_es_case(case) for case in results]
+        'matching_records': len(final_results) - missing,
+        'missing_records': missing,
+        'cases': final_results
     }
+
+
+def _get_error_doc(case_id):
+    return {'case_id': case_id, 'error': 'not found'}
--- corehq/apps/hqcase/tests/test_case_api_bulk_get.py
+++ corehq/apps/hqcase/tests/test_case_api_bulk_get.py
@@ -4,11 +4,23 @@ from django.test import TestCase
 from django.urls import reverse
 
 from casexml.apps.case.mock import CaseBlock
+
 from corehq import privileges
 from corehq.apps.domain.shortcuts import create_domain
-from corehq.apps.es.tests.utils import es_test, case_search_es_setup, case_search_es_teardown
+from corehq.apps.es.tests.utils import (
+    case_search_es_setup,
+    case_search_es_teardown,
+    es_test,
+    populate_case_search_index,
+)
+from corehq.apps.hqcase.utils import submit_case_blocks
 from corehq.apps.users.models import HqPermissions, UserRole, WebUser
-from corehq.util.test_utils import disable_quickcache, flag_enabled, privilege_enabled
+from corehq.form_processor.tests.utils import FormProcessorTestUtils
+from corehq.util.test_utils import (
+    disable_quickcache,
+    flag_enabled,
+    privilege_enabled,
+)
 
 
 @es_test
@@ -17,7 +29,7 @@ from corehq.util.test_utils import disable_quickcache, flag_enabled, privilege_e
 @flag_enabled('CASE_API_V0_6')
 @flag_enabled('API_THROTTLE_WHITELIST')
 class TestCaseAPIBulkGet(TestCase):
-    domain = 'test-update-cases'
+    domain = 'test-bulk-get-cases'
     maxDiff = None
 
     @classmethod
@@ -36,16 +48,22 @@ class TestCaseAPIBulkGet(TestCase):
         case_search_es_setup(cls.domain, case_blocks)
         cls.case_ids = [b.case_id for b in case_blocks]
 
+        xform, [case] = submit_case_blocks(
+            cls._make_case_block('Vera Menchik', external_id='vera').as_text(), domain='other-domain'
+        )
+        populate_case_search_index([case])
+        cls.other_domain_case_id = case.case_id
+
     def setUp(self):
         self.client.login(username='netflix', password='password')
-
-    def tearDown(self):
-        case_search_es_teardown()
 
     @classmethod
     def tearDownClass(cls):
         cls.web_user.delete(cls.domain, deleted_by=None)
         cls.domain_obj.delete()
+        FormProcessorTestUtils.delete_all_cases(cls.domain)
+        FormProcessorTestUtils.delete_all_xforms(cls.domain)
+        case_search_es_teardown()
         super().tearDownClass()
 
     @classmethod
@@ -59,7 +77,19 @@ class TestCaseAPIBulkGet(TestCase):
 
     def test_bulk_get(self):
         case_ids = self.case_ids[0:2]
+        self._call_api_check_results(case_ids)
+
+    def test_bulk_get_domain_filter(self):
+        case_ids = self.case_ids[0:2] + [self.other_domain_case_id]
+        result = self._call_api_check_results(case_ids)
+        self.assertEqual(result['matching_records'], 2)
+        self.assertEqual(result['missing_records'], 1)
+        self.assertEqual(result['cases'][2]['error'], 'not found')
+
+    def _call_api_check_results(self, case_ids):
         res = self.client.get(reverse('case_api', args=(self.domain, ','.join(case_ids))))
         self.assertEqual(res.status_code, 200)
-        result_case_ids = [case['case_id'] for case in res.json()['cases']]
+        result = res.json()
+        result_case_ids = [case['case_id'] for case in result['cases']]
         self.assertEqual(result_case_ids, case_ids)
+        return result
