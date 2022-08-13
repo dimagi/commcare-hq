dce7ef3d444ea2c8b4208405c5d055bcfd519f7b
Jing Cheng <jcheng@dimagi.com>
2022-08-13 14:06:38 -0400

    remove case migrations app

--- corehq/apps/case_migrations/views.py
+++
@@ -1,8 +0,0 @@
-from corehq.apps.domain.auth import formplayer_auth
-from corehq.apps.ota.case_restore import get_case_restore_response
-
-
-@formplayer_auth
-def migration_restore(request, domain, case_id):
-    """Moved to `ota` app. Can be removed once Formplayer is updated."""
-    return get_case_restore_response(domain, case_id)
