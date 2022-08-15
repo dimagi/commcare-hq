0d1a000831b247c43f2e964cebc47799ba07524e
Biyeun Buczyk <biyeun@dimagi.com>
2022-08-15 17:25:22 +0200

    giving this another try

--- corehq/apps/sso/views/oidc.py
+++ corehq/apps/sso/views/oidc.py
@@ -79,9 +79,9 @@ def _get_authorization_url(request):
     initialize_oidc_session(request)
     nonce = request.session["oidc_nonce"]
     authorization_url = f"https://login.microsoftonline.com/{request.idp.entity_id}/oauth2/authorize?" \
-                        f"client_id={request.idp.client_id}&scope=openid" \
-                        f"&response_type=access_as_user&" \
-                        f"redirect_uri={redirect_url}&nonce={nonce}&resource=https://graph.windows.net&prompt=consent"
+                        f"client_id={request.idp.client_id}&scope=profile,openid,User.Read,email" \
+                        f"&response_type=code&" \
+                        f"redirect_uri={redirect_url}&nonce={nonce}&resource=https://graph.windows.net&prompt=login"
     return authorization_url
 
 
