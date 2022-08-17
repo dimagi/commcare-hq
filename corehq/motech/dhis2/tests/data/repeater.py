f61e7939af0bd7eedb077d676bb2c22d4568621d
Amit Phulera <amitphulera999@gmail.com>
2022-08-17 16:09:55 +0530

    add tests for config_dhis2_entity_repeater view

---
+++ corehq/motech/dhis2/tests/data/repeater.py
@@ -0,0 +1,89 @@
+dhis2_repeater_data = {
+    "dhis2_config":
+    {
+        "doc_type": "Dhis2Config",
+        "form_configs":
+        [
+            {
+                "doc_type": "Dhis2FormConfig",
+                "xmlns": "http://openrosa.org/formdesigner/3974C41B-0BC4-4B27-B6D2-6FE49E33D961",
+                "datavalue_maps":
+                [
+                    {
+                        "data_element_id": "S9zdlWB1xny",
+                        "value":
+                        {
+                            "doc_type": "FormQuestion",
+                            "form_question": "/data/dhis2_indicators/dhis2_avortement_bovin_ovin_caprin"
+                        },
+                        "doc_type": "FormDataValueMap"
+                    },
+                    {
+                        "data_element_id": "ENxf6cfFlFR",
+                        "categoryOptionCombo": "iaUZ0KXio4m",
+                        "value":
+                        {
+                            "doc_type": "FormQuestion",
+                            "form_question": "/data/dhis2_indicators/dhis2_suscption_rougeole_mort"
+                        },
+                        "doc_type": "FormDataValueMap"
+                    },
+                    {
+                        "data_element_id": "VOpvrwKrAoJ",
+                        "value":
+                        {
+                            "doc_type": "FormQuestion",
+                            "form_question": "/data/dhis2_indicators/dhis2_mention_particuliere"
+                        },
+                        "doc_type": "FormDataValueMap"
+                    }
+                ],
+                "org_unit_id":
+                {
+                    "location_field": "org_unit_id_simr",
+                    "doc_type": "FormUserAncestorLocationField"
+                },
+                "program_id": "lasd4I6jsbl",
+                "event_status":
+                {
+                    "value": "COMPLETED"
+                },
+                "dataSet": "WEu0IGoo8mU",
+                "completeDate":
+                {
+                    "form_question": "/data/dhis2_indicators/dates_calc/date",
+                    "doc_type": "FormQuestion"
+                },
+                'event_location': {},
+                "period":
+                {
+                    "form_question": "/data/dhis2_indicators/dhis2_real_date",
+                    "doc_type": "FormQuestion"
+                },
+                "event_date":
+                {
+                    "form_question": "/data/dhis2_indicators/dhis2_real_date",
+                    "doc_type": "FormQuestion"
+                },
+                "completed_date": None,
+                "enrollment_date":
+                {},
+                "incident_date":
+                {},
+                "program_stage_id":
+                {},
+                "program_status":
+                {
+                    "value": "ACTIVE"
+                }
+            }
+        ]
+    },
+    "format": "form_json",
+    "is_paused": False,
+    "repeater_type": "Dhis2Repeater",
+    "white_listed_form_xmlns":
+    [],
+    "dhis2_version": None,
+    "dhis2_version_last_modified": None,
+}
