c3a121748ae91de15c93e2e4fb2e6f37bc9ba252
Amit Phulera <amitphulera999@gmail.com>
2022-08-19 14:46:38 +0530

    update pause_repeater to use sqlrepeater

--- corehq/motech/repeaters/views/repeaters.py
+++ corehq/motech/repeaters/views/repeaters.py
@@ -307,7 +307,7 @@ def drop_repeater(request, domain, repeater_id):
 @require_can_edit_web_users
 @requires_privilege_with_fallback(privileges.DATA_FORWARDING)
 def pause_repeater(request, domain, repeater_id):
-    rep = Repeater.get(repeater_id)
+    rep = SQLRepeater.objects.get(repeater_id=repeater_id)
     rep.pause()
     messages.success(request, "Forwarding paused!")
     return HttpResponseRedirect(
