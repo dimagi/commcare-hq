0ef59a3c5e83f366653f53ec5c282d7ee79e9614
AddisonDunn <dunnaddisonw@gmail.com>
2022-08-24 10:31:12 -0400

    use non-admin role by default for tableau reports

--- corehq/apps/reports/standard/tableau.py
+++ corehq/apps/reports/standard/tableau.py
@@ -79,7 +79,7 @@ def tableau_visualization_ajax(request, domain):
     target_site = visualiation_data.pop('target_site')
 
     # An equivalent Tableau user with the username "HQ/{role name}" and proper permissions must exist.
-    tableau_username = f"HQ/{request.couch_user.get_role(domain).name}"
+    tableau_username = f"HQ/{request.couch_user.get_role(domain, checking_global_admin=False).name}"
     tabserver_url = 'https://{}/trusted/'.format(server_name)
     post_arguments = {'username': tableau_username}
 
