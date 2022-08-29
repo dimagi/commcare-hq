8dd6ce3496d584556b8e8bac5865b0aef051a128
Jennifer Schweers <orange.jenny@gmail.com>
2022-08-29 15:36:32 -0400

    Extracted questionDefaults

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures.js
@@ -1,4 +1,10 @@
 hqDefine("cloudcare/js/formplayer/spec/fixtures", function () {
+    var questionDefaults = {
+        "type": "question",
+        "required": 0,
+        "relevant": 0,
+        "datatype": "str",
+    };
     return {
         "caseList": {
           "notification": {
@@ -932,135 +938,100 @@ hqDefine("cloudcare/js/formplayer/spec/fixtures", function () {
           "appId": null,
           "appVersion": null,
           "tree": [
-            {
+            _.extend(questionDefaults, {
               "caption": "The following questions will go over basic question types allowed in a form.",
               "binding": "/data/intro",
-              "required": 0,
-              "relevant": 0,
               "datatype": "info",
-              "type": "question",
               "ix": "0",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should let you enter any form of text or special characters. Try different values.",
               "binding": "/data/q_text",
-              "required": 0,
-              "relevant": 0,
-              "datatype": "str",
-              "type": "question",
               "ix": "1",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should only let you enter an integer. [",
               "binding": "/data/q_int",
-              "required": 0,
-              "relevant": 0,
               "datatype": "int",
-              "type": "question",
               "ix": "2",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should only let you enter a decimal number. If you enter a whole number, proceed to the next question and then back to this one to make sure it was saved as a decimal number.",
               "binding": "/data/q_numeric",
-              "required": 0,
-              "relevant": 0,
               "datatype": "float",
-              "type": "question",
               "ix": "3",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should only allow you to enter a date.",
               "binding": "/data/q_date",
-              "required": 0,
-              "relevant": 0,
               "datatype": "date",
-              "type": "question",
               "ix": "4",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should allow you to enter a date and a time.",
               "binding": "/data/q_date_time",
-              "required": 0,
-              "relevant": 0,
               "datatype": "datetime",
-              "type": "question",
               "ix": "5",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "You should be able to choose one or more answers here.",
               "binding": "/data/q_multiselect",
-              "required": 0,
-              "relevant": 0,
               "datatype": "multiselect",
-              "type": "question",
               "ix": "6",
               "choices": [
                 "One",
                 "Two",
                 "Three"
               ],
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "You should be able to choose only one answer here.",
               "binding": "/data/q_singleselect",
-              "required": 0,
-              "relevant": 0,
               "datatype": "select",
-              "type": "question",
               "ix": "7",
               "choices": [
                 "One",
                 "Two",
                 "Three"
               ],
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should display with a label at the top and allow you to choose from either option on each row",
-              "required": 0,
-              "relevant": 0,
               "style": {},
               "type": "sub-group",
               "ix": "8",
               "repeatable": "false",
               "children": [
-                {
+                _.extend(questionDefaults, {
                   "caption": "Choose for each:",
                   "binding": "/data/list_view/list_label",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "select",
                   "style": {
                     "raw": "label"
                   },
-                  "type": "question",
                   "ix": "8,0",
                   "choices": [
                     "Yes",
                     "No"
                   ],
-                },
-                {
+                }),
+                _.extend(questionDefaults, {
                   "caption": "Red",
                   "binding": "/data/list_view/red",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "select",
                   "style": {
                     "raw": "list-nolabel"
                   },
-                  "type": "question",
                   "caption_markdown": "Red",
                   "ix": "8,1",
                   "choices": [
                     "Yes",
                     "No"
                   ],
-                },
-                {
+                }),
+                _.extend(questionDefaults, {
                   "caption": "Green",
                   "binding": "/data/list_view/Green",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "select",
                   "style": {
                     "raw": "list-nolabel"
@@ -1072,235 +1043,169 @@ hqDefine("cloudcare/js/formplayer/spec/fixtures", function () {
                     "Yes",
                     "No"
                   ],
-                },
-                {
+                }),
+                _.extend(questionDefaults, {
                   "caption": "Yellow",
                   "binding": "/data/list_view/Yellow",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "select",
                   "style": {
                     "raw": "list-nolabel"
                   },
-                  "type": "question",
                   "ix": "8,3",
                   "choices": [
                     "Yes",
                     "No"
                   ],
-                }
+                })
               ],
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "This question should only allow you to enter a time.",
               "binding": "/data/q_time",
-              "required": 0,
-              "relevant": 0,
               "datatype": "time",
-              "type": "question",
               "ix": "9",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "The value of this question should be hidden, but anything can be entered.",
               "binding": "/data/q_pass",
-              "required": 0,
-              "relevant": 0,
-              "datatype": "str",
-              "type": "question",
               "ix": "10",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "The value of this question should be hidden and only numbers are allowed.",
               "binding": "/data/q_pass_int",
-              "required": 0,
-              "relevant": 0,
-              "datatype": "str",
-              "type": "question",
               "ix": "11",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "You should be able to enter digits here. Enter multiple zeroes and navigate back and forth to make sure they remain.",
               "binding": "/data/numerictext",
-              "required": 0,
-              "relevant": 0,
-              "datatype": "str",
               "style": {
                 "raw": "numeric"
               },
-              "type": "question",
               "ix": "12",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "You should only see this message. It should have no confirmation box.",
               "binding": "/data/no_confirmation",
-              "required": 0,
-              "relevant": 0,
               "datatype": "info",
               "style": {
                 "raw": "minimal"
               },
-              "type": "question",
               "ix": "13",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "You should be able to see this message and a confirmation box. The next set of questions will go over complex question types and will only be available on some devices.",
               "binding": "/data/q_label",
-              "required": 0,
-              "relevant": 0,
               "datatype": "info",
-              "type": "question",
               "ix": "14",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If using an Android device, you should be able to capture a signature. Try it out!",
               "binding": "/data/sig_cap",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
               "style": {
                 "raw": "signature"
               },
-              "type": "question",
               "ix": "15",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If using an Android device, this question should allow you to scan a barcode.",
               "binding": "/data/q_barcode",
-              "required": 0,
-              "relevant": 0,
               "datatype": "barcode",
-              "type": "question",
               "ix": "16",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If using an Android device, this question should display a QR Code (similar to a barcode. If you scanned one in the previous question, it should contain its contents.)",
               "binding": "/data/qroutput",
-              "required": 0,
-              "relevant": 0,
               "datatype": "info",
-              "type": "question",
               "ix": "17",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If using an Android device, this question should allow you to capture a GPS location. Try it out.",
               "binding": "/data/q_gps",
-              "required": 0,
-              "relevant": 0,
               "datatype": "geo",
-              "type": "question",
               "ix": "18",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should allow you to take a picture or upload an image.",
               "binding": "/data/q_image",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
-              "type": "question",
               "ix": "19",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should allow you to record or upload audio, and then play it.",
               "binding": "/data/q_audio",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
-              "type": "question",
               "ix": "20",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should allow you to record or upload video, and then play it.",
               "binding": "/data/q_video",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
-              "type": "question",
               "ix": "21",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should only allow you to take a picture.",
               "binding": "/data/q_image_acquire",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
               "style": {
                 "raw": "acquire"
               },
-              "type": "question",
               "ix": "22",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should only allow you to record audio, and then play it.",
               "binding": "/data/q_audio_acquire",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
               "style": {
                 "raw": "acquire"
               },
-              "type": "question",
               "ix": "23",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "If available on your device, this question should only allow you to record a video, and then play it.",
               "binding": "/data/q_video_acquire",
-              "required": 0,
-              "relevant": 0,
               "datatype": "binary",
               "style": {
                 "raw": "acquire"
               },
-              "type": "question",
               "ix": "24",
-            },
-            {
+            }),
+            _.extend(questionDefaults, {
               "caption": "Group List Question",
-              "required": 0,
-              "relevant": 0,
               "style": {},
               "type": "sub-group",
               "ix": "25",
               "repeatable": "false",
               "children": [
-                {
+                _.extend(questionDefaults, {
                   "caption": "This question should let you enter any form of text or special characters. Try different values.",
                   "binding": "/data/group_list/q_text_2",
-                  "required": 0,
-                  "relevant": 0,
-                  "datatype": "str",
-                  "type": "question",
                   "ix": "25,0",
-                },
-                {
+                }),
+                _.extend(questionDefaults, {
                   "caption": "You should be able to choose one or more answers here.",
                   "binding": "/data/group_list/q_multiselect_2",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "multiselect",
-                  "type": "question",
                   "ix": "25,1",
                   "choices": [
                     "One",
                     "Two",
                     "Three"
                   ],
-                },
-                {
+                }),
+                _.extend(questionDefaults, {
                   "caption": "You should be able to choose only one answer here.",
                   "binding": "/data/group_list/q_singleselect_2",
-                  "required": 0,
-                  "relevant": 0,
                   "datatype": "select",
-                  "type": "question",
                   "ix": "25,2",
                   "choices": [
                     "One",
                     "Two",
                     "Three"
                   ],
-                }
+                })
               ],
-            }
+            }),
           ],
           "langs": [
             "en",
