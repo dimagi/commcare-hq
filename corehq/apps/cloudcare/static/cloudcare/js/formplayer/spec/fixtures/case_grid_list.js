8226408e8fcf518ca15bea032e6624e96da6cc75
Jennifer Schweers <orange.jenny@gmail.com>
2022-08-29 15:55:53 -0400

    Extracted each response into a file

---
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures/case_grid_list.js
@@ -0,0 +1,113 @@
+hqDefine("cloudcare/js/formplayer/spec/fixtures/case_grid_list", function () {
+    return {
+      "notification": {
+        "message": null,
+        "error": false
+      },
+      "title": "New Adherence Data",
+      "clearSession": false,
+      "appId": "5319fe096062b0e282bf37e6faa81566",
+      "appVersion": "CommCare Version: 2.27, App Version: 93",
+      "locales": [
+        "default",
+        "en"
+      ],
+      "breadcrumbs": [
+        "UATBC Calendar Testing",
+        "[hidden] Adherence Reg Form Shell",
+        "[hidden] Adherence Reg Form Shell",
+        "New Adherence Data"
+      ],
+      "menuSessionId": "af420f42-4171-45d6-82dc-21ddfce837a7",
+      "entities": [
+        {
+          "id": "16957",
+          "data": [
+            "5",
+            "",
+            "jr://file/commcare/image/data/question1.png"
+          ],
+          "details": null
+        },
+        {
+          "id": "16958",
+          "data": [
+            "6",
+            "",
+            "jr://file/commcare/image/data/question1.png"
+          ],
+          "details": null
+        },
+        {
+          "id": "17053",
+          "data": [
+            "9",
+            "",
+            "jr://file/commcare/image/data/question1.png"
+          ],
+          "details": null
+        }
+      ],
+      "action": null,
+      "styles": [
+        {
+          "fontSize": 12,
+          "widthHint": null,
+          "displayFormat": null
+        },
+        {
+          "fontSize": 12,
+          "widthHint": null,
+          "displayFormat": null
+        },
+        {
+          "fontSize": 0,
+          "widthHint": null,
+          "displayFormat": "Image"
+        }
+      ],
+      "headers": [
+        "Date",
+        "",
+        ""
+      ],
+      "tiles": [
+        {
+          "gridX": 9,
+          "gridY": 0,
+          "gridWidth": 3,
+          "gridHeight": 2,
+          "cssId": null,
+          "fontSize": "medium"
+        },
+        {
+          "gridX": 1,
+          "gridY": 0,
+          "gridWidth": 4,
+          "gridHeight": 2,
+          "cssId": null,
+          "fontSize": "medium"
+        },
+        {
+          "gridX": 0,
+          "gridY": 4,
+          "gridWidth": 7,
+          "gridHeight": 7,
+          "cssId": null,
+          "fontSize": null
+        }
+      ],
+      "widthHints": [
+        33,
+        33,
+        33
+      ],
+      "numEntitiesPerRow": 7,
+      "pageCount": 0,
+      "currentPage": 0,
+      "type": "entities",
+      "usesCaseTiles": true,
+      "maxWidth": 12,
+      "maxHeight": 11
+    };
+});
---
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures/menu_list.js
@@ -0,0 +1,55 @@
+hqDefine("cloudcare/js/formplayer/spec/fixtures/menu_list", function () {
+    return {
+      "notification": {
+        "message": null,
+        "error": false
+      },
+      "title": "Case Tests",
+      "clearSession": false,
+      "appId": "d8404850d060879c5e56e63e047dde09",
+      "appVersion": "CommCare Version: 2.27, App Version: 32",
+      "locales": [
+        "default",
+        "en",
+        "hin"
+      ],
+      "breadcrumbs": [
+        "Basic",
+        "Case Tests"
+      ],
+      "menuSessionId": "e9fad761-5239-4096-bb71-0aba1ebd7377",
+      "commands": [
+        {
+          "index": 0,
+          "displayText": "Create a Case",
+          "navigationState": "JUMP"
+        },
+        {
+          "index": 1,
+          "displayText": "Update a Case",
+          "navigationState": "NEXT"
+        },
+        {
+          "index": 2,
+          "displayText": "Close a Case",
+          "navigationState": "NEXT"
+        },
+        {
+          "index": 3,
+          "displayText": "Create a Sub Case",
+          "navigationState": "NEXT"
+        },
+        {
+          "index": 4,
+          "displayText": "Create Multiple Sub Cases",
+          "navigationState": "NEXT"
+        },
+        {
+          "index": 5,
+          "displayText": "Case list",
+          "navigationState": "NEXT"
+        }
+      ],
+      "type": "commands"
+    };
+});
