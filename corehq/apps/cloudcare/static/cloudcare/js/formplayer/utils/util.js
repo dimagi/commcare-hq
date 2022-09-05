5429f16efd07eed6914f20b7866c71a839d89f7a
Jennifer Schweers <orange.jenny@gmail.com>
2022-09-05 11:26:41 -0400

    Fixed assignment

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/util.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/util.js
@@ -186,7 +186,7 @@ hqDefine("cloudcare/js/formplayer/utils/util", function () {
 
         this.setQueryData = function (inputs, execute, forceManualSearch) {
             var selections = hqImport("cloudcare/js/formplayer/utils/util").currentUrlToObject().selections;
-            this.queryData = queryData || {};
+            this.queryData = this.queryData || {};
             this.queryData[sessionStorage.queryKey] = _.defaults({
                 inputs: inputs,
                 execute: execute,
