1d5ccfe35f94fab17224fbc49f569fd739735ac9
akashkj <akjain@dimagi.com>
2022-09-06 08:06:58 +0000

    refactor

--- custom/abdm/milestone_one/utils/abha_creation_util.py
+++ custom/abdm/milestone_one/utils/abha_creation_util.py
@@ -27,40 +27,6 @@ def generate_aadhar_otp(aadhaar_number):
     return resp.json()
 
 
-def generate_auth_otp(health_id, auth_method):
-    url = "https://healthidsbx.abdm.gov.in/api/v2/auth/init"
-    payload = {"authMethod": auth_method, "healthid": health_id}
-    print(payload)
-    headers = {"Content-Type": "application/json; charset=UTF-8"}
-    token = get_access_token()
-    headers.update({"Authorization": "Bearer {}".format(token)})
-    resp = requests.post(url=url, data=json.dumps(payload), headers=headers)
-    print(resp.content)
-    if resp.status_code == 200:
-        return resp.json()
-
-
-def confirm_with_mobile_otp(otp, txn_id):
-    url = "https://healthidsbx.abdm.gov.in/api/v1/auth/confirmWithMobileOTP"
-    payload = {"otp": otp, "txnId": txn_id}
-    headers = {"Content-Type": "application/json; charset=UTF-8"}
-    token = get_access_token()
-    headers.update({"Authorization": "Bearer {}".format(token)})
-    resp = requests.post(url=url, data=json.dumps(payload), headers=headers)
-    if resp.status_code == 200:
-        return resp.json()
-
-
-def get_account_information(x_token):
-    url = "https://healthidsbx.abdm.gov.in/api/v1/account/profile"
-    headers = {"Content-Type": "application/json; charset=UTF-8"}
-    token = get_access_token()
-    headers.update({"Authorization": "Bearer {}".format(token), "X-Token": f"Bearer {x_token}"})
-    resp = requests.get(url=url, headers=headers)
-    if resp.status_code == 200:
-        return resp.json()
-
-
 def generate_mobile_otp(mobile_number, txnid):
     generate_aadhar_otp = "v1/registration/aadhaar/generateMobileOTP"
     payload = {"mobile": str(mobile_number), "txnId": txnid}
@@ -115,17 +81,3 @@ def create_health_id(txnid):
     print(token)
     resp = requests.post(url=base_url + generate_aadhar_otp, data=json.dumps(payload), headers=headers)
     return resp.json()
-
-
-def search_by_health_id(health_id):
-    url = "https://healthidsbx.abdm.gov.in/api/v1/search/searchByHealthId"
-    payload = {"healthId": health_id}
-    headers = {"Content-Type": "application/json; charset=UTF-8"}
-    token = get_access_token()
-    headers.update({"Authorization": "Bearer {}".format(token)})
-    print(token)
-    resp = requests.post(url=url, data=json.dumps(payload), headers=headers)
-    print(resp)
-    print(resp.content)
-    if resp.status_code == 200:
-        return resp.json()
---
+++ custom/abdm/milestone_one/views/abha_creation_views.py
@@ -0,0 +1,55 @@
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import (HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND, HTTP_200_OK)
+from rest_framework.response import Response
+
+from custom.abdm import abdm_util
+
+
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'}, status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'}, status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return token.key
+
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def generate_aadhaar_otp(request):
+    aadhaar_number = request.data.get("aadhaar")
+    resp = abdm_util.generate_aadhar_otp(aadhaar_number)
+    return Response(resp, status=HTTP_200_OK)
+
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def generate_mobile_otp(request):
+    txn_id = request.data.get("txn_id")
+    mobile_number = request.data.get("mobile_number")
+    resp = abdm_util.generate_mobile_otp(mobile_number, txn_id)
+    return Response(resp, status=HTTP_200_OK)
+
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def verify_aadhaar_otp(request):
+    txn_id = request.data.get("txn_id")
+    otp = request.data.get("otp")
+    resp = abdm_util.verify_aadhar_otp(otp, txn_id)
+    return Response(resp, status=HTTP_200_OK)
+
+
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def verify_mobile_otp(request):
+    txn_id = request.data.get("txn_id")
+    otp = request.data.get("otp")
+    resp = abdm_util.verify_mobile_otp(otp, txn_id)
+    return Response(resp, status=HTTP_200_OK)
