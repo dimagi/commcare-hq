b418549d94fed779d17c50ddbb23479c2b87075f
akashkj <akjain@dimagi.com>
2022-09-14 13:16:18 +0000

    restore token fix

--- custom/abdm/milestone_one/utils/user_util.py
+++ custom/abdm/milestone_one/utils/user_util.py
@@ -1,13 +1,20 @@
+import logging
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 from rest_framework.authtoken.models import Token
 
+logger = logging.getLogger(__name__)
 
-def get_abdm_api_token(username, password):
-    print(f"Getting token for user {username}")
-    user = authenticate(username=username, password=password)
+
+def get_abdm_api_token(username, password, restore=False):
+    logger.info(f"Getting token for user {username}")
+    if restore:
+        user = User.objects.filter(username=username).first()
+    else:
+        user = authenticate(username=username, password=password)
     if not user:
         return None
     token, _ = Token.objects.get_or_create(user=user)
-    print(f"Received token {len(token.key)}")
+    logger.info(f"Received token {len(token.key)}")
     return token.key
