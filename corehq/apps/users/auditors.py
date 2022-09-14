4e8e39caeef2d9ea396fe85b2e2530acda763ffa
Joel Miller <jmiller@dimagi.com>
2022-09-14 10:09:42 -0400

    Fix HQAuditor bug -- fetch `couch_user` when missing from request

--- corehq/apps/users/auditors.py
+++ corehq/apps/users/auditors.py
@@ -10,7 +10,13 @@ class HQAuditor(BaseAuditor):
             return None
         info = {}
         if request.user.is_authenticated:
-            user = request.couch_user
+            try:
+                user = request.couch_user
+            except AttributeError:
+                # Fetch the couch user manually if it is not set on the request.
+                # Only known to happen during registration (sign up).
+                from corehq.apps.users.models import CouchUser
+                user = CouchUser.get_by_username(request.user.username)
             info["user_type"] = user.doc_type
             info["username"] = user.username
         domain = getattr(request, "domain", None)
--- corehq/apps/users/tests/test_auditors.py
+++ corehq/apps/users/tests/test_auditors.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from corehq.apps.users.auditors import HQAuditor
@@ -9,14 +11,25 @@ class TestHQAudit(TestCase):
         self.auditor = HQAuditor()
 
     def test_change_context_returns_none_when_not_authenticated(self):
-        request = MockRequest.without_auth("test@example.com", "WebUser")
+        request = MockRequest.without_auth("test@example.com")
         self.assertIsNone(self.auditor.change_context(request))
 
     def test_change_context_contains_user_name_and_type(self):
-        request = MockRequest.with_auth("test@example.com", "WebUser")
+        request = MockRequest.with_auth_and_couch_user("test@example.com", "WebUser")
         self.assertEqual(
             {"username": "test@example.com", "user_type": "WebUser"},
             self.auditor.change_context(request)
+        )
+
+    def test_change_context_fetches_couch_user_when_missing(self):
+        request = MockRequest.with_auth("test@example.com")
+        couch_user = MockCouchUser(request.user.username, "WebUser")
+        with patch("corehq.apps.users.models.CouchUser.get_by_username", return_value=couch_user) as mock:
+            change_context = self.auditor.change_context(request)
+            mock.assert_called_once_with(request.user.username)
+        self.assertEqual(
+            {"username": "test@example.com", "user_type": "WebUser"},
+            change_context,
         )
 
     def test_change_context_contains_domain_when_present(self):
@@ -29,18 +42,23 @@ class TestHQAudit(TestCase):
 
 class MockRequest:
 
-    def __init__(self, user, couch_user, domain=None):
+    def __init__(self, user, couch_user=None, domain=None):
         self.user = user
-        self.couch_user = couch_user
+        if couch_user is not None:
+            self.couch_user = couch_user
         if domain is not None:
             self.domain = domain
 
     @classmethod
-    def without_auth(cls, username, doc_type):
-        return cls(MockDjangoAuthUser(username, False), MockCouchUser(username, doc_type))
+    def without_auth(cls, username):
+        return cls(MockDjangoAuthUser(username, False))
 
     @classmethod
-    def with_auth(cls, username, doc_type):
+    def with_auth(cls, username):
+        return cls(MockDjangoAuthUser(username))
+
+    @classmethod
+    def with_auth_and_couch_user(cls, username, doc_type):
         return cls(MockDjangoAuthUser(username), MockCouchUser(username, doc_type))
 
     @classmethod
