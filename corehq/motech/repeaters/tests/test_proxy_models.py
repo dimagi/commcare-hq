92a4d1b57a1bf9b1c28befe842aeee42ce79a8ce
Amit Phulera <amitphulera999@gmail.com>
2022-09-16 10:27:56 +0530

    inform tests about new sql attrs

--- corehq/motech/repeaters/tests/test_proxy_models.py
+++ corehq/motech/repeaters/tests/test_proxy_models.py
@@ -291,9 +291,11 @@ class TestRepeaterModelsAttrEquality(ModelAttrEqualityHelper):
         return {
             'repeater_id', 'set_next_attempt', 'next_attempt_at',
             'is_ready', 'options', '_repeater_type', 'last_attempt_at', 'repeat_records_ready', 'repeat_records',
-            'all_objects', 'reset_next_attempt', 'is_deleted', 'PROXY_FIELD_NAME', 'Meta', 'repeater',
+            'all_objects', 'reset_next_attempt', 'is_deleted', 'PROXY_FIELD_NAME',
+            'Meta', 'repeater', 'last_modified', 'date_created',
             # added by django choicefield in models
-            'get_request_method_display',
+            'get_request_method_display', 'get_next_by_date_created', 'get_next_by_last_modified',
+            'get_previous_by_date_created', 'get_previous_by_last_modified',
             # other attrs
             'to_json', '_convert_to_serializable',
             '_optionvalue_fields', '_wrap_schema_attrs',
