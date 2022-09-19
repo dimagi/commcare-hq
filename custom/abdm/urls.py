0c755578c87fc3b0f676aded8547df8d1809b96d
akashkj <akjain@dimagi.com>
2022-09-19 15:35:43 +0530

    remove unsed url

--- custom/abdm/urls.py
+++ custom/abdm/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from custom.abdm.milestone_one.views import abha_creation_views, abha_verification_views
 
 abha_creation_urls = [
-    path('api/login', abha_creation_views.login),
     path('api/generate_aadhaar_otp', abha_creation_views.generate_aadhaar_otp),
     path('api/generate_mobile_otp', abha_creation_views.generate_mobile_otp),
     path('api/verify_aadhaar_otp', abha_creation_views.verify_aadhaar_otp),
