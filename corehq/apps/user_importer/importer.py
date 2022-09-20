08601c8bdd6bb5f736ab741ad8a0a11b875d2f08
Rynhard <rpietersen@dimagi.com>
2022-09-20 16:39:06 +0200

    re-added normalizing usernames without the validation

--- corehq/apps/user_importer/importer.py
+++ corehq/apps/user_importer/importer.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from django.db import DEFAULT_DB_ALIAS
 
 from corehq.apps.enterprise.models import EnterpriseMobileWorkerSettings
+from corehq.apps.users.util import get_complete_mobile_username
 from dimagi.utils.logging import notify_exception
 from django.utils.translation import gettext as _
 
@@ -486,6 +487,7 @@ def create_or_update_commcare_users_and_groups(upload_domain, user_specs, upload
 
         username = row.get('username')
         domain = row.get('domain') or upload_domain
+        username = get_complete_mobile_username(str(username), domain) if username else None
         status_row = {
             'username': username,
             'row': row,
