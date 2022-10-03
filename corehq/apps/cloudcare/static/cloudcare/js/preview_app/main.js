abdd207decd004ae6b25ab844bb2f3bc3ea6ee23
Jennifer Schweers <orange.jenny@gmail.com>
2022-10-03 15:10:21 -0400

    Updated preview_app/main.js

--- corehq/apps/cloudcare/static/cloudcare/js/preview_app/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/preview_app/main.js
@@ -1,9 +1,17 @@
 'use strict';
-hqDefine("cloudcare/js/preview_app/main", function () {
-    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
-        previewApp = hqImport("cloudcare/js/preview_app/preview_app"),
-        sentry = hqImport("cloudcare/js/sentry");
-
+hqDefine("cloudcare/js/preview_app/main", [
+    'jquery',
+    'underscore',
+    'hqwebapp/js/initial_page_data',
+    'cloudcare/js/sentry',
+    'cloudcare/js/preview_app/preview_app',
+], function (
+    $,
+    _,
+    initialPageData,
+    sentry,
+    previewApp
+) {
     $(function () {
         sentry.initSentry();
 
