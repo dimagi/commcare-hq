fdbd3dd355818178935aed3caf25223013aca30a
Jennifer Schweers <orange.jenny@gmail.com>
2022-10-04 17:34:50 -0400

    Updated Ethiopian calendar entry

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
@@ -12,6 +12,7 @@ hqDefine("cloudcare/js/form_entry/entries", [
     'cloudcare/js/form_entry/const',
     'cloudcare/js/form_entry/utils',
     'signature_pad/dist/signature_pad.umd.min',
+    'cloudcare/js/formplayer/utils/calendar-picker-translations',   // EthiopianDateEntry
     'select2/dist/js/select2.full.min',
     'mapbox.js/dist/mapbox',    // window.L
 ], function (
