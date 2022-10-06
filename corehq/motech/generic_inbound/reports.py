8bb59d12eb2ed6025baa8c2a2d0cdd091b7374aa
Ethan Soergel <esoergel@gmail.com>
2022-10-06 13:10:35 -0400

    Display real data in the report

--- corehq/motech/generic_inbound/reports.py
+++ corehq/motech/generic_inbound/reports.py
@@ -1,3 +1,4 @@
+from django.utils.functional import cached_property
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 
@@ -5,6 +6,8 @@ from corehq.apps.reports.datatables import DataTablesColumn, DataTablesHeader
 from corehq.apps.reports.dispatcher import DomainReportDispatcher
 from corehq.apps.reports.generic import GenericTabularReport
 from corehq.toggles import GENERIC_INBOUND_API
+
+from .models import RequestLog
 
 
 class ApiRequestLogReport(GenericTabularReport):
@@ -25,12 +28,31 @@ class ApiRequestLogReport(GenericTabularReport):
         return request.couch_user.is_domain_admin()
 
     @property
+    def total_records(self):
+        return self._queryset.count()
+
+    @property
     def headers(self):
         return DataTablesHeader(
-            DataTablesColumn(_("Foo")),
-            DataTablesColumn(_("Bar")),
+            DataTablesColumn(_("API")),
+            DataTablesColumn(_("Timestamp")),
+            DataTablesColumn(_("Status")),
+            DataTablesColumn(_("Response")),
+        )
+
+    @cached_property
+    def _queryset(self):
+        return RequestLog.objects.filter(
+            domain=self.domain,
         )
 
     @property
     def rows(self):
-        return []
+        status_labels = dict(RequestLog.Status.choices)
+        for log in self._queryset[self.pagination.start:self.pagination.end]:
+            yield [
+                log.api.name,
+                log.timestamp,
+                status_labels[log.status],
+                log.response_status,
+            ]
