69b5818d24714408cf7cbb69cebdf691a8219fb1
AddisonDunn <dunnaddisonw@gmail.com>
2022-11-01 10:21:04 -0400

    add translations for tableau roles

--- corehq/apps/reports/const.py
+++ corehq/apps/reports/const.py
@@ -1,12 +1,14 @@
+from django.utils.translation import gettext_lazy as _
+
 USER_QUERY_LIMIT = 5000
 DEFAULT_PAGE_LIMIT = 10
 
 TABLEAU_ROLES = (
-    ('Explorer - Can Publish', 'ExplorerCanPublish'),
-    ('Server Administrator', 'ServerAdministrator'),
-    ('Site Administrator - Explorer', 'SiteAdministratorExplorer'),
-    ('Site Administrator - Creator', 'SiteAdministratorCreator'),
-    ('Unlicensed', 'Unlicensed'),
-    ('Read Only', 'ReadOnly'),
-    ('Viewer', 'Viewer')
+    (_('Explorer - Can Publish'), 'ExplorerCanPublish'),
+    (_('Server Administrator'), 'ServerAdministrator'),
+    (_('Site Administrator - Explorer'), 'SiteAdministratorExplorer'),
+    (_('Site Administrator - Creator'), 'SiteAdministratorCreator'),
+    (_('Unlicensed'), 'Unlicensed'),
+    (_('Read Only'), 'ReadOnly'),
+    (_('Viewer'), 'Viewer')
 )
