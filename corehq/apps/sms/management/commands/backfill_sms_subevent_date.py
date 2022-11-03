29bb5364fbaff15e64db617280860e1495c485e4
Simon Kelly <skelly@dimagi.com>
2022-11-03 13:11:32 +0200

    backfill from xformsession

--- corehq/apps/sms/management/commands/backfill_sms_subevent_date.py
+++ corehq/apps/sms/management/commands/backfill_sms_subevent_date.py
@@ -10,11 +10,12 @@ class Command(BaseCommand):
     def handle(self, **options):
         update_subevent_date_from_emails(CHUNK_SIZE)
         update_subevent_date_from_sms(CHUNK_SIZE)
+        update_subevent_date_from_xform_session(CHUNK_SIZE)
 
 
 def update_subevent_date_from_emails(chunk_size):
     query = f"""
-        update sms_messagingsubevent set date_last_activity = greatest(em.date, em.date_modified)
+        update sms_messagingsubevent set date_last_activity = greatest(se.date, em.date, em.date_modified)
         from sms_messagingsubevent se join sms_email em on se.id = em.messaging_subevent_id
         where sms_messagingsubevent.id in (
             select se.id from sms_messagingsubevent se
@@ -28,7 +29,7 @@ def update_subevent_date_from_emails(chunk_size):
 
 def update_subevent_date_from_sms(chunk_size):
     query = f"""
-        update sms_messagingsubevent set date_last_activity = greatest(sms.date, sms.date_modified)
+        update sms_messagingsubevent set date_last_activity = greatest(se.date, sms.date, sms.date_modified)
         from sms_messagingsubevent se join sms_sms sms on se.id = sms.messaging_subevent_id
         where sms_messagingsubevent.id in (
             select se.id from sms_messagingsubevent se
@@ -38,6 +39,20 @@ def update_subevent_date_from_sms(chunk_size):
         )
     """
     return run_query_until_no_updates("sms", query)
+
+
+def update_subevent_date_from_xform_session(chunk_size):
+    query = f"""
+        update sms_messagingsubevent set date_last_activity = greatest(se.date, sess.modified_time)
+        from sms_messagingsubevent se join smsforms_sqlxformssession sess on se.xforms_session_id = sess.id
+        where sms_messagingsubevent.id in (
+            select id from sms_messagingsubevent where
+            sms_messagingsubevent.xforms_session_id is not null
+            and sms_messagingsubevent.date_last_activity is null
+            limit {chunk_size}
+        )
+    """
+    return run_query_until_no_updates("xform_session", query)
 
 
 def run_query_until_no_updates(slug, query):
