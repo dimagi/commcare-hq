05a506dff2298d8278c024b59617316dd7192c42
Simon Kelly <skelly@dimagi.com>
2022-11-03 13:59:57 +0200

    combine tests into a single class

--- corehq/apps/sms/tests/test_backfill_subevent.py
+++ corehq/apps/sms/tests/test_backfill_subevent.py
@@ -16,71 +16,55 @@ from corehq.apps.sms.tests.data_generator import (
 from corehq.apps.users.models import CommCareUser
 
 
-class TestBackfillSubeventDateEmail(TestCase):
+class TestBackfillSubeventDate(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.domain = Domain.get_or_create_with_name("backfill-test", is_active=True)
         cls.addClassCleanup(Subscription._get_active_subscription_by_domain.clear, Subscription, cls.domain.name)
         cls.addClassCleanup(cls.domain.delete)
-        user_ids = []
-        for i in range(2):
-            user = CommCareUser.create(
-                cls.domain.name, f"user {i}", "123", None, None, email=f"user{i}@email.com"
-            )
-            user_ids.append(user.get_id)
-            cls.addClassCleanup(user.delete, cls.domain.name, deleted_by=None)
 
-        for i in range(2):
-            make_email_event_for_test(cls.domain.name, "test broadcast", user_ids)
+    def test_update_from_email(self):
+        self._setup_email_data()
+        self._do_backfill_validate_result(update_subevent_date_from_emails)
 
-        update_ids = MessagingSubEvent.objects.all()[1:]
-        MessagingSubEvent.objects.filter(id__in=update_ids).update(date_last_activity=None)
+    def test_update_from_sms(self):
+        self._setup_sms_data()
+        self._do_backfill_validate_result(update_subevent_date_from_sms)
 
-    def test_update_date_last_activity(self):
-        self.assertEqual(3, MessagingSubEvent.objects.filter(date_last_activity=None).count())
-        rows_updated, iterations = update_subevent_date_from_emails(chunk_size=2)
+    def test_update_from_xform_session(self):
+        self._setup_survey_data()
+        self._do_backfill_validate_result(update_subevent_date_from_xform_session)
+
+    def _do_backfill_validate_result(self, update_fn):
+        rows_updated, iterations = update_fn(chunk_size=2)
         self.assertEqual(3, rows_updated)
         self.assertEqual(3, iterations)  # 2 with updates + 1 with no updates
         self.assertFalse(MessagingSubEvent.objects.filter(date_last_activity=None).exists())
 
-
-class TestBackfillSubeventDateSMS(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.domain = Domain.get_or_create_with_name("backfill-test", is_active=True)
-        cls.addClassCleanup(Subscription._get_active_subscription_by_domain.clear, Subscription, cls.domain.name)
-        cls.addClassCleanup(cls.domain.delete)
+    def _setup_email_data(self):
+        user = CommCareUser.create(
+            self.domain.name, "user 1", "123", None, None, email="user1@email.com"
+        )
+        self.addCleanup(user.delete, self.domain.name, deleted_by=None)
 
         for i in range(4):
-            create_fake_sms(cls.domain.name, randomize=True)
+            make_email_event_for_test(self.domain.name, "test broadcast", [user.get_id])
 
-        update_ids = MessagingSubEvent.objects.all()[1:]
-        MessagingSubEvent.objects.filter(id__in=update_ids).update(date_last_activity=None)
+        self._reset_dates_and_check()
 
-    def test_update_date_last_activity(self):
-        self.assertEqual(3, MessagingSubEvent.objects.filter(date_last_activity=None).count())
-        rows_updated, iterations = update_subevent_date_from_sms(chunk_size=2)
-        self.assertEqual(3, rows_updated)
-        self.assertEqual(3, iterations)  # 2 with updates + 1 with no updates
-        self.assertFalse(MessagingSubEvent.objects.filter(date_last_activity=None).exists())
-
-
-class TestBackfillSubeventDateXformSession(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.domain = Domain.get_or_create_with_name("backfill-test", is_active=True)
-        cls.addClassCleanup(Subscription._get_active_subscription_by_domain.clear, Subscription, cls.domain.name)
-        cls.addClassCleanup(cls.domain.delete)
-
+    def _setup_sms_data(self):
         for i in range(4):
-            make_survey_sms_for_test(cls.domain.name, "test survey")
+            create_fake_sms(self.domain.name, randomize=True)
 
+        self._reset_dates_and_check()
+
+    def _setup_survey_data(self):
+        for i in range(4):
+            make_survey_sms_for_test(self.domain.name, "test survey")
+
+        self._reset_dates_and_check()
+
+    def _reset_dates_and_check(self):
         update_ids = MessagingSubEvent.objects.all()[1:]
         MessagingSubEvent.objects.filter(id__in=update_ids).update(date_last_activity=None)
-
-    def test_update_date_last_activity(self):
         self.assertEqual(3, MessagingSubEvent.objects.filter(date_last_activity=None).count())
-        rows_updated, iterations = update_subevent_date_from_xform_session(chunk_size=2)
-        self.assertEqual(3, rows_updated)
-        self.assertEqual(3, iterations)  # 2 with updates + 1 with no updates
-        self.assertFalse(MessagingSubEvent.objects.filter(date_last_activity=None).exists())
