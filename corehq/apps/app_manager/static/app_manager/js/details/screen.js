0a0a4f2a6cf9416eb1c4fba5a5efe782f094c698
Steph <stephherbers@gmail.com>
2022-11-07 12:47:51 -0500

    enable save button with any change to the multiselect value

--- corehq/apps/app_manager/static/app_manager/js/details/screen.js
+++ corehq/apps/app_manager/static/app_manager/js/details/screen.js
@@ -80,6 +80,9 @@ hqDefine("app_manager/js/details/screen", function () {
             self.fireChange();
         });
         self.maxSelectValue = ko.observable(spec[self.columnKey].max_select_value) || hqImport("cloudcare/js/formplayer/constants").MULTI_SELECT_MAX_SELECT_VALUE;
+        self.maxSelectValue.subscribe(function () {
+            self.fireChange();
+        });
         self.autoSelectEnabled = ko.observable(spec[self.columnKey].auto_select);
         self.autoSelectEnabled.subscribe(function () {
             self.fireChange();
