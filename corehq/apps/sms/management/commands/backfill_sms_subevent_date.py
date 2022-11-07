0821b24810ddeb062391a9b432671202869a0de0
Simon Kelly <skelly@dimagi.com>
2022-11-07 14:47:01 +0200

    add more options for testing

--- corehq/apps/sms/management/commands/backfill_sms_subevent_date.py
+++ corehq/apps/sms/management/commands/backfill_sms_subevent_date.py
@@ -3,10 +3,14 @@ from django.db import connections, transaction
 
 from corehq.apps.domain_migration_flags.api import (
     ALL_DOMAINS,
-    migration_in_progress,
+    get_migration_status,
     set_migration_complete,
     set_migration_started,
 )
+from corehq.apps.domain_migration_flags.exceptions import (
+    DomainMigrationProgressError,
+)
+from corehq.apps.domain_migration_flags.models import MigrationStatus
 
 CHUNK_SIZE = 500000
 
@@ -18,20 +22,36 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--chunksize", type=int, default=CHUNK_SIZE)
+        parser.add_argument("--explain", action="store_true", default=False)
+        parser.add_argument("--force", action="store_true", default=False,
+                            help="Run the queries even if the migration has been marked complete.")
 
     def handle(self, **options):
-        if not migration_in_progress(ALL_DOMAINS, MIGRATION_SLUG):
-            set_migration_started(ALL_DOMAINS, MIGRATION_SLUG)
+        explain = options["explain"]
+        force = options["force"]
+        if not explain:
+            status = get_migration_status(ALL_DOMAINS, MIGRATION_SLUG)
+            if status == MigrationStatus.COMPLETE and not force:
+                print("Backfill has already been marked as complete")
+                return
 
-        chunk_size = options["chunk_size"]
-        update_subevent_date_from_emails(chunk_size)
-        update_subevent_date_from_sms(chunk_size)
-        update_subevent_date_from_xform_session(chunk_size)
+            if status not in (MigrationStatus.IN_PROGRESS, MigrationStatus.COMPLETE):
+                set_migration_started(ALL_DOMAINS, MIGRATION_SLUG)
 
-        set_migration_complete(ALL_DOMAINS, MIGRATION_SLUG)
+        chunk_size = options["chunksize"]
+        update_subevent_date_from_emails(chunk_size, explain)
+        update_subevent_date_from_sms(chunk_size, explain)
+        update_subevent_date_from_xform_session(chunk_size, explain)
+
+        if not explain:
+            try:
+                set_migration_complete(ALL_DOMAINS, MIGRATION_SLUG)
+            except DomainMigrationProgressError:
+                if not force:
+                    raise
 
 
-def update_subevent_date_from_emails(chunk_size):
+def update_subevent_date_from_emails(chunk_size, explain):
     query = f"""
         update sms_messagingsubevent set date_last_activity = greatest(se.date, em.date, em.date_modified)
         from sms_messagingsubevent se join sms_email em on se.id = em.messaging_subevent_id
@@ -42,24 +62,24 @@ def update_subevent_date_from_emails(chunk_size):
             limit {chunk_size}
         )
     """
-    return run_query_until_no_updates("email", query)
+    return run_query_until_no_updates("email", query, explain)
 
 
-def update_subevent_date_from_sms(chunk_size):
+def update_subevent_date_from_sms(chunk_size, explain):
     query = f"""
         update sms_messagingsubevent set date_last_activity = greatest(se.date, sms.date, sms.date_modified)
         from sms_messagingsubevent se join sms_sms sms on se.id = sms.messaging_subevent_id
         where sms_messagingsubevent.id in (
-            select se.id from sms_messagingsubevent se
+            select count(*) from sms_messagingsubevent se
                 join sms_sms sms on se.id = sms.messaging_subevent_id
             where se.date_last_activity is null
             limit {chunk_size}
         )
     """
-    return run_query_until_no_updates("sms", query)
+    return run_query_until_no_updates("sms", query, explain)
 
 
-def update_subevent_date_from_xform_session(chunk_size):
+def update_subevent_date_from_xform_session(chunk_size, explain):
     query = f"""
         update sms_messagingsubevent set date_last_activity = greatest(se.date, sess.modified_time)
         from sms_messagingsubevent se join smsforms_sqlxformssession sess on se.xforms_session_id = sess.id
@@ -70,23 +90,33 @@ def update_subevent_date_from_xform_session(chunk_size):
             limit {chunk_size}
         )
     """
-    return run_query_until_no_updates("xform_session", query)
+    return run_query_until_no_updates("xform_session", query, explain)
 
 
-def run_query_until_no_updates(slug, query):
+def run_query_until_no_updates(slug, query, explain):
     total_rows = 0
     iterations = 0
+    if explain:
+        print(f"Running 'explain' for {slug} query:\n")
+        query = f"explain {query}"
+    else:
+        print(f"Running backfill for {slug} query:\n")
     while True:
         with transaction.atomic(using='default'), connections["default"].cursor() as cursor:
             cursor.execute(query)
             rowcount = cursor.rowcount
+            if explain:
+                for row in cursor.fetchall():
+                    print(row)
+                print()
+                return
 
         total_rows += rowcount
         iterations += 1
 
+        print(f"[{slug}] Updated {rowcount} ({total_rows}) subevent rows")
+
         if rowcount == 0:
             break
-
-        print(f"[{slug}] Updated {rowcount} ({total_rows}) subevent rows")
 
     return total_rows, iterations
