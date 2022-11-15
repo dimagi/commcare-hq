bcc5153fc1159b1eaad34888546bc588a7f50518
Simon Kelly <skelly@dimagi.com>
2022-11-15 16:22:56 +0200

    udpate messaging-event API to sort by date_last_activity by default
    this will use the new index and be a more efficient query

--- corehq/apps/api/resources/messaging_event/utils.py
+++ corehq/apps/api/resources/messaging_event/utils.py
@@ -39,7 +39,7 @@ def get_order_by_field(request_params):
     order_by = request_params.get("order_by")
 
     if not order_by:
-        return "date"
+        return "date_last_activity"
 
     if order_by not in ("date", "-date", "date_last_activity", "-date_last_activity"):
         raise BadRequest(_("No matching '{field_name}' field for ordering").format(field_name=order_by))
