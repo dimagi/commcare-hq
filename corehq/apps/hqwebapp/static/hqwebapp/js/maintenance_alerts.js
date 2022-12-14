5975aed0829f380ede69746407aee4c50bc94e3c
nospame <mapson.evan@gmail.com>
2022-12-14 11:44:28 -0800

    Add knockout model for form and basic validation

--- corehq/apps/hqwebapp/static/hqwebapp/js/maintenance_alerts.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/maintenance_alerts.js
@@ -1,11 +1,24 @@
 hqDefine("hqwebapp/js/maintenance_alerts",[
     'jquery',
+    'knockout',
     'hqwebapp/js/initial_page_data',
     'hqwebapp/js/widgets',
-], function ($, initialPageData) {
+], function ($, ko, initialPageData) {
     $(function () {
-        $('#ko-alert-container').koApplyBindings({
+        var alertFormModel = {
+            text: ko.observable(),
+            domains: ko.observable(),
+            startTime: ko.observable(),
+            endTime: ko.observable(),
+            timezone: ko.observable(),
+        };
+
+        var alertViewModel = {
             alerts: initialPageData.get('alerts'),
-        });
+        };
+
+        $('#timezone').select2({ placeholder: 'UTC (default)' });
+        $('#ko-alert-form').koApplyBindings(alertFormModel);
+        $('#ko-alert-container').koApplyBindings(alertViewModel);
     });
 });
