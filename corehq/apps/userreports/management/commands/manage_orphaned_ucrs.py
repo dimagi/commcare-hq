484344118eda2b92f4427a1513cfcde2f442a83c
Graham Herceg <gherceg@dimagi.com>
2022-12-23 09:53:55 -0500

    rewrite delete orphaned ucr code
    big commit. I wasn't happy with my initial implementation
    as it was hard to share between the management command
    and other code. This aims to make it more user friendly.

--- corehq/apps/userreports/management/commands/manage_orphaned_ucrs.py
+++ corehq/apps/userreports/management/commands/manage_orphaned_ucrs.py
@@ -1,11 +1,9 @@
-from collections import defaultdict
-
 from django.core.management.base import BaseCommand
 
 from corehq.apps.userreports.dbaccessors import (
-    drop_ucr_tables,
-    get_deletable_ucrs,
-    get_orphaned_tables_by_engine_id,
+    drop_orphaned_ucrs,
+    get_all_data_sources,
+    get_orphaned_ucrs,
 )
 
 CMD_FIND = 'find'
@@ -28,10 +26,10 @@ class Command(BaseCommand):
             help='Only check this DB engine',
         )
         parser.add_argument(
-            '--force-delete',
+            '--ignore-active-domains',
             action='store_true',
-            default=False,
-            help='Drop orphaned tables on active domains'
+            default=True,
+            help='If True, only includes orphaned UCRs from deleted domains.'
         )
         parser.add_argument(
             '--domain',
@@ -40,50 +38,32 @@ class Command(BaseCommand):
         )
 
     def handle(self, **options):
-        orphaned_tables_by_engine_id = get_orphaned_tables_by_engine_id(options.get('engine_id'))
-        try:
-            ucrs_to_delete = get_deletable_ucrs(orphaned_tables_by_engine_id, force_delete=options['force_delete'],
-                                                domain=options['domain'])
-        except AssertionError as e:
-            suggestions = """
-            Use the '--domain' option to further inspect a specific domain.
-            Use the '--force-delete' option if you are sure you want to
-            delete all orphaned ucrs.
-            """
-            print(str(e))
-            print(suggestions)
-            exit(0)
+        """
+        """
+        command = options.get('command')
+        domain = options.get('domain')
 
-        log_orphaned_tables(ucrs_to_delete)
+        engine_ids = {ds.engine_id for ds in get_all_data_sources()}
+        for engine_id in engine_ids:
+            try:
+                orphaned_ucrs = get_orphaned_ucrs(
+                    engine_id,
+                    domain=domain,
+                    ignore_active_domains=options.get('ignore_active_domains'))
+            except AssertionError as e:
+                print(str(e))
+                continue
 
-        if options.get('command') == CMD_DELETE and confirm_deletion_with_user():
-            tablenames = get_tables_names(ucrs_to_delete)
-            drop_ucr_tables(tablenames)
+            if not orphaned_ucrs:
+                print(f"Did not find orphaned UCRs in the {engine_id} db.")
+                continue
 
+            print(f"Found the following orphaned UCRs in the {engine_id} db:")
+            for ucr in orphaned_ucrs:
+                print(f"\t{ucr}")
 
-def log_orphaned_tables(ucrs_to_delete):
-    if not ucrs_to_delete:
-        print("Did not find any orphaned UCRs.")
-        return
-
-    for engine_id, ucr_infos in ucrs_to_delete.items():
-        if len(ucr_infos) > 0:
-            print(f"Found orphaned UCRs in the {engine_id} database:")
-            for ucr_info in ucr_infos:
-                print(
-                    f"\t{ucr_info['tablename']}, {ucr_info['row_count']} rows."
-                )
-        else:
-            print(f"Did not find orphaned UCRs in the {engine_id} database.")
-
-
-def get_tables_names(ucrs_to_delete):
-    tablenames_to_drop = defaultdict(list)
-    for engine_id, ucr_infos in ucrs_to_delete.items():
-        for ucr_info in ucr_infos:
-            tablenames_to_drop[engine_id].append(ucr_info['tablename'])
-
-    return tablenames_to_drop
+            if command == CMD_DELETE and confirm_deletion_with_user():
+                drop_orphaned_ucrs(engine_id, orphaned_ucrs)
 
 
 def confirm_deletion_with_user():
