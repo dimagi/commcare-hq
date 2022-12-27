ac35c6e009898d9a1a5b6854fe4857a834c86255
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-27 16:29:06 -0500

    Added hqwebapp/js/mocha for mocha setup code

--- corehq/apps/hqwebapp/static/hqwebapp/spec/main.js
+++ corehq/apps/hqwebapp/static/hqwebapp/spec/main.js
@@ -1,39 +1,15 @@
 hqDefine("hqwebapp/spec/main", [
-    "analytix/js/google",
-    "analytix/js/kissmetrix",
-    "mocha/mocha",
-    "chai/chai",
-    "sinon/pkg/sinon",
+    "hqwebapp/js/mocha",
 ], function (
-    googleAnalytics,
-    kissAnalytics,
-    mocha,
-    chai,
-    sinon
+    hqMocha
 ) {
-    // TODO: DRY up with mocha/base.html
-    mocha.setup('bdd')
-    window.assert = chai.assert
-
-    function gettext(str) {
-        return str;
-    }
-    window.gettext = gettext;
-
-    googleAnalytics.track.event = sinon.spy();
-    googleAnalytics.track.click = sinon.spy();
-    kissAnalytics.track.event = sinon.spy();
-
     hqRequire([
         "hqwebapp/spec/assert_properties_spec",
         "hqwebapp/spec/inactivity_spec",
         "hqwebapp/spec/urllib_spec",
         "hqwebapp/spec/widgets_spec",
     ], function () {
-        // TODO: DRY up with mocha/base.html
-        if (navigator.userAgent.indexOf('PhantomJS') < 0) {
-            mocha.run();
-        }
+        hqMocha.run();
     });
 
     return 1;
