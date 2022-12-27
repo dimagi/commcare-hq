0a9b8af0d6b6d202526029bc742e47aa9febfff3
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-27 15:49:41 -0500

    Added requirejs_main to hqwebapp/mocha.html

---
+++ corehq/apps/hqwebapp/static/hqwebapp/spec/main.js
@@ -0,0 +1,35 @@
+hqDefine("hqwebapp/spec/main", [
+    "analytix/js/google",
+    "analytix/js/kissmetrix",
+    "mocha/mocha",
+    "chai/chai",
+    "sinon/pkg/sinon",
+], function (
+    googleAnalytics,
+    kissAnalytics,
+    mocha,
+    chai,
+    sinon
+) {
+    // TODO: DRY up with mocha/base.html
+    mocha.setup('bdd')
+    window.assert = chai.assert
+
+    function gettext(str) {
+        return str;
+    }
+    window.gettext = gettext;
+
+    googleAnalytics.track.event = sinon.spy();
+    googleAnalytics.track.click = sinon.spy();
+    kissAnalytics.track.event = sinon.spy();
+
+    hqRequire(["hqwebapp/spec/assert_properties_spec"], function () {
+        // TODO: DRY up with mocha/base.html
+        if (navigator.userAgent.indexOf('PhantomJS') < 0) {
+            mocha.run();
+        }
+    });
+
+    return 1;
+});
