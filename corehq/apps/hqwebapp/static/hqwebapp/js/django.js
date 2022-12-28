870433df22141d411483d9639eb2babe561a4f89
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-27 19:43:55 -0500

    Updated django.js to hopefully fix test

--- corehq/apps/hqwebapp/static/hqwebapp/js/django.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/django.js
@@ -3,5 +3,5 @@
     Allow usage of gettext, RequireJS-style, rather than accessing the global django.
 */
 define([], function () {
-    return { gettext: django.gettext };
+    return { gettext: window.django ? django.gettext : undefined };
 });
