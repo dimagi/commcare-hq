4c7df4edad70ec9077e94711c33234bfa417d79d
Jennifer Schweers <orange.jenny@gmail.com>
2022-12-28 10:12:18 -0500

    Added requirejs_main to form entry tests

---
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
@@ -0,0 +1,12 @@
+hqDefine("cloudcare/js/form_entry/spec/main", [
+    "hqwebapp/js/mocha",
+], function (
+    hqMocha
+) {
+    hqRequire([
+    ], function () {
+        hqMocha.run();
+    });
+
+    return 1;
+});
