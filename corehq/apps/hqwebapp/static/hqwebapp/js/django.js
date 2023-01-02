308eb861e23f2a0108ca0ba789ae6bcc7b2ae5eb
Jennifer Schweers <orange.jenny@gmail.com>
2023-01-02 17:09:54 -0500

    Moved hqwebapp/js/django into BASE_MAIN block

--- corehq/apps/hqwebapp/static/hqwebapp/js/django.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/django.js
@@ -3,5 +3,5 @@
     Allow usage of gettext, RequireJS-style, rather than accessing the global django.
 */
 define([], function () {
-    return { gettext: window.django ? django.gettext : undefined };
+    return { gettext: django.gettext };
 });
