86c649d6bbbbe3ef78395ab4cbf3398f8f7afa94
Graham Herceg <gherceg@dimagi.com>
2023-01-02 13:50:27 -0500

    migration to bootstrap audit events
    creates bootstrap audit events for AutomaticUpdateRule

---
+++ corehq/apps/data_interfaces/migrations/0032_bootstrap_audit_events_for_update_rules.py
@@ -0,0 +1,24 @@
+from django.core.management import call_command
+from django.db import migrations
+
+
+def bootstrap_audit_events_for_automatic_update_rule(apps, schema_editor):
+    call_command('bootstrap_field_audit_events',
+                 'top-up',
+                 ['AutomaticUpdateRule'])
+
+
+def noop(*args, **kwargs):
+    pass
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data_interfaces', '0031_add_domaincaserulerun_status_choices'),
+    ]
+
+    operations = [
+        migrations.RunPython(bootstrap_audit_events_for_automatic_update_rule,
+                             reverse_code=noop)
+    ]
