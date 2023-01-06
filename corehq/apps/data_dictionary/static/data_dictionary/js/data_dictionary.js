487eca0faa90ff793123731314e2ef8c0f297645
mkangia <mkangia@dimagi.com>
2023-01-06 19:00:57 +0530

    used select2 for FHIR resource type

--- corehq/apps/data_dictionary/static/data_dictionary/js/data_dictionary.js
+++ corehq/apps/data_dictionary/static/data_dictionary/js/data_dictionary.js
@@ -10,6 +10,7 @@ hqDefine("data_dictionary/js/data_dictionary", [
     "hqwebapp/js/toggles",
     "hqwebapp/js/knockout_bindings.ko",
     "data_interfaces/js/make_read_only",
+    'hqwebapp/js/select2_knockout_bindings.ko',
 ], function (
     $,
     ko,
@@ -342,5 +343,6 @@ hqDefine("data_dictionary/js/data_dictionary", [
         $('#download-dict').click(function () {
             googleAnalytics.track.event('Data Dictionary', 'downloaded data dictionary');
         });
+
     });
 });
