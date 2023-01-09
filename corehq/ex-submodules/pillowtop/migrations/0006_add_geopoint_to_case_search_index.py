94951fa7b43199b87b7a5de8d327bf8ce291899f
Joel Miller <jmiller@dimagi.com>
2023-01-09 14:10:51 -0500

    Use `prompt_for_historical_migration()` instead of an empty migration

--- corehq/ex-submodules/pillowtop/migrations/0006_add_geopoint_to_case_search_index.py
+++ corehq/ex-submodules/pillowtop/migrations/0006_add_geopoint_to_case_search_index.py
@@ -1,15 +1,21 @@
 from django.db import migrations
 
+from corehq.util.django_migrations import (
+    get_migration_name,
+    prompt_for_historical_migration,
+)
+
 
 class Migration(migrations.Migration):
-    """This migration no longer has any operations. In the past this migration
-    updated the mapping on the Elasticsearch `case_search` index. That operation
-    has been removed because it was merged further in the past than our
-    downstream hosters support window, making it safe to skip from now on.
-    """
 
     dependencies = [
         ('pillowtop', '0005_kafkacheckpoint_doc_modification_time'),
     ]
 
-    operations = []
+    operations = [
+        prompt_for_historical_migration(
+            'pillowtop', get_migration_name(__file__),
+            # the commit (on 'master') that this branch is currently based on
+            required_commit='08d594d1877cfe3ca20fcf8aa29155bce4ff26bf'
+        )
+    ]
