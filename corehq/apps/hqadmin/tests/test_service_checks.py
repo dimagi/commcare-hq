dea8ded550d4a2d6bdb1846e01ab0f89356f8d49
Joel Miller <jmiller@dimagi.com>
2023-01-10 17:02:26 -0500

    Update tests to work regardless of the Elastic container's actual status

--- corehq/apps/hqadmin/tests/test_service_checks.py
+++ corehq/apps/hqadmin/tests/test_service_checks.py
@@ -4,39 +4,47 @@ from unittest.mock import patch
 from corehq.apps.es.groups import group_adapter
 from corehq.apps.es.tests.utils import es_test
 
-from ..service_checks import check_elasticsearch
+from .. import service_checks
+
+
+def always_green():
+    """Patch ``check_es_cluster_health()`` with this function to ensure tests
+    get a deterministic (successful) value, regardless of the Elastic
+    container's actual status.
+    """
+    return "green"
 
 
 @es_test(requires=[group_adapter])
+@patch.object(service_checks, "check_es_cluster_health", always_green)
 class TestCheckElasticsearch(SimpleTestCase):
 
     class Fail(Exception):
         pass
 
     def test_check_elasticsearch(self):
-        status = check_elasticsearch()
-        self.assertTrue(status.success)
+        status = service_checks.check_elasticsearch()
         self.assertEqual(status.msg, "Successfully sent a doc to ES and read it back")
+        self.assertTrue(status.success)
         self.assertIsNone(status.exception)
 
     def test_check_elasticsearch_passes_for_cluster_yellow(self):
-        with patch("corehq.apps.hqadmin.service_checks.check_es_cluster_health", return_value="yellow") as mock:
-            from corehq.apps.hqadmin.service_checks import check_es_cluster_health
-            self.assertEqual("yellow", check_es_cluster_health())
+        with patch.object(service_checks, "check_es_cluster_health", return_value="yellow") as mock:
+            self.assertEqual("yellow", service_checks.check_es_cluster_health())
             mock.reset_mock()
-            self.assertTrue(check_elasticsearch().success)
+            self.assertTrue(service_checks.check_elasticsearch().success)
         mock.assert_called_once()
 
     def test_check_elasticsearch_fails_for_cluster_red(self):
-        with patch("corehq.apps.hqadmin.service_checks.check_es_cluster_health", return_value="red"):
-            status = check_elasticsearch()
+        with patch.object(service_checks, "check_es_cluster_health", return_value="red"):
+            status = service_checks.check_elasticsearch()
         self.assertFalse(status.success)
         self.assertEqual(status.msg, "Cluster health at red")
         self.assertIsNone(status.exception)
 
     def test_check_elasticsearch_fails_with_exc_if_group_doc_index_raises(self):
         with patch.object(group_adapter, "index", side_effect=self.Fail) as mock:
-            status = check_elasticsearch()
+            status = service_checks.check_elasticsearch()
         mock.assert_called_once()
         self.assertFalse(status.success)
         self.assertEqual(status.msg, "Something went wrong sending a doc to ES")
@@ -44,7 +52,7 @@ class TestCheckElasticsearch(SimpleTestCase):
 
     def test_check_elasticsearch_fails_with_exc_if_group_doc_delete_raises(self):
         with patch.object(group_adapter, "delete", side_effect=self.Fail) as mock:
-            status = check_elasticsearch()
+            status = service_checks.check_elasticsearch()
         mock.assert_called_once()
         self.assertFalse(status.success)
         self.assertEqual(status.msg, "Something went wrong sending a doc to ES")
@@ -52,7 +60,7 @@ class TestCheckElasticsearch(SimpleTestCase):
 
     def test_check_elasticsearch_fails_if_indexed_doc_is_missing(self):
         with patch.object(group_adapter, "index", return_value=None) as mock:
-            status = check_elasticsearch()
+            status = service_checks.check_elasticsearch()
         mock.assert_called_once()
         self.assertFalse(status.success)
         self.assertEqual(status.msg, "Something went wrong sending a doc to ES")
