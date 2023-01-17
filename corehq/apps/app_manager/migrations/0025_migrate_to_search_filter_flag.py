07362c512b617ef36e9f9e8aa69ef76dbe37b102
nospame <mapson.evan@gmail.com>
2023-01-17 15:04:42 -0800

    Let exceptions from flag_domains fail hard

--- corehq/apps/app_manager/migrations/0025_migrate_to_search_filter_flag.py
+++ corehq/apps/app_manager/migrations/0025_migrate_to_search_filter_flag.py
@@ -7,12 +7,9 @@ from corehq.util.django_migrations import skip_on_fresh_install
 
 @skip_on_fresh_install
 def migrate_to_search_filter_flag(apps, schema_editor):
-    try:
-        call_command(
-            "flag_domains_using_search_filter",
-        )
-    except Exception:
-        traceback.print_exc()
+    call_command(
+        "flag_domains_using_search_filter",
+    )
 
 
 class Migration(migrations.Migration):
