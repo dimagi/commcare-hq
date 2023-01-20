2d043425ee503060892f6b2e89cbfe619a680fe8
AddisonDunn <dunnaddisonw@gmail.com>
2023-01-20 12:21:17 -0500

    Revert "add couch view for deleted apps"
    This reverts commit df6afab7cce3a36f2350773254958cbe6aadad6c.

--- corehq/apps/app_manager/_design/views/deleted_applications/map.js
+++
@@ -1,8 +0,0 @@
-function(doc){
-    if(doc.doc_type == 'Application-Deleted' || doc.doc_type == 'RemoteApp-Deleted' || doc.doc_type == 'LinkedApplication-Deleted') {
-        emit([doc.domain, doc.copy_of, doc.version], null);
-        if (doc.is_released && doc.copy_of) {
-            emit(['^ReleasedApplications', doc.domain,  doc.copy_of, doc.version], null);
-        }
-    }
-}
