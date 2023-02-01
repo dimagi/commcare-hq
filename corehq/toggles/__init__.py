3948d7b745eafadb5e89dfeb5f309e899aef0fa2
Charl Smit <csmit@dimagi.com>
2023-02-01 09:20:02 +0200

    Add FrozenPrivilegeToggle

--- corehq/toggles/__init__.py
+++ corehq/toggles/__init__.py
@@ -2457,3 +2457,11 @@ FORM_LINK_WORKFLOW = FrozenPrivilegeToggle(
     [NAMESPACE_DOMAIN],
     help_link='https://confluence.dimagi.com/display/saas/Form+Link+Workflow+Feature+Flag',
 )
+
+PHONE_HEARTBEAT = FrozenPrivilegeToggle(
+    privileges.PHONE_APK_HEARTBEAT,
+    'phone_apk_heartbeat',
+    "Ability to configure a mobile feature to prompt users to update to latest CommCare app and apk",
+    TAG_SOLUTIONS_CONDITIONAL,
+    [NAMESPACE_DOMAIN]
+)
