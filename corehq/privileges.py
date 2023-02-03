0d221ae54d2eb704b2de4cd50fc330608792dc01
Norman Hooper <nhooper@dimagi.com>
2023-02-03 09:15:42 +0000

    Create DATA_FILE_DOWNLOAD privilege

--- corehq/privileges.py
+++ corehq/privileges.py
@@ -91,6 +91,9 @@ FORM_LINK_WORKFLOW = 'form_link_workflow'
 
 PHONE_APK_HEARTBEAT = 'phone_apk_heartbeat'
 
+# a.k.a. "File Dropzone", "Secure File Transfer"
+DATA_FILE_DOWNLOAD = 'data_file_download'
+
 MAX_PRIVILEGES = [
     LOOKUP_TABLES,
     API_ACCESS,
@@ -140,6 +143,7 @@ MAX_PRIVILEGES = [
     LOADTEST_USERS,
     FORM_LINK_WORKFLOW,
     PHONE_APK_HEARTBEAT,
+    DATA_FILE_DOWNLOAD,
 ]
 
 # These are special privileges related to their own rates in a SoftwarePlanVersion
@@ -204,4 +208,5 @@ class Titles(object):
             LOADTEST_USERS: _('Loadtest Users'),
             FORM_LINK_WORKFLOW: _("Link to other forms in End of Form Navigation"),
             PHONE_APK_HEARTBEAT: _("Phone heartbeat"),
+            DATA_FILE_DOWNLOAD: _('File Dropzone'),
         }.get(privilege, privilege)
