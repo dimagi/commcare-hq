4ae0bf5afbe5897b2b0f3fa32ad5147e0732f695
Amit Phulera <amitphulera999@gmail.com>
2023-02-16 19:54:18 +0530

    add tests

--- corehq/apps/es/tests/test_client.py
+++ corehq/apps/es/tests/test_client.py
@@ -1876,6 +1876,54 @@ class TestFromMultiInElasticUser(TestCase):
         self.assertEqual(es_user, commcare_user)
 
 
+@es_test(requires=[domain_adapter], setup_class=True)
+class TestFromMultiInDomain(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.domain = 'from-multi-domain-tests'
+        cls.domain_obj = create_domain(cls.domain)
+        cls.addClassCleanup(cls.domain_obj.delete)
+
+    def test_from_multi_works_with_domain_objects(self):
+        domain_adapter.from_multi(self.domain_obj)
+
+    def test_from_multi_works_with_user_dicts(self):
+        domain_adapter.from_multi(self.domain_obj.to_json())
+
+    def test_from_multi_raises_for_other_objects(self):
+        self.assertRaises(UnknownDocException, domain_adapter.from_multi, set)
+
+    def test_from_python_raises_for_other_objects(self):
+        self.assertRaises(UnknownDocException, domain_adapter.from_python, set)
+        self.assertRaises(UnknownDocException, domain_adapter.from_python, self.domain_obj.to_json())
+
+    def test_from_multi_is_same_as_transform_domain_for_es(self):
+        # this test can be safely removed when transform_domain_for_elasticsearch is removed
+        domain_id, domain = domain_adapter.from_multi(self.domain_obj)
+        domain['_id'] = domain_id
+        self.assertEqual(transform_domain_for_elasticsearch(self.domain_obj.to_json()), domain)
+
+    def test_index_can_handle_domain_dicts(self):
+        domain_dict = self.domain_obj.to_json()
+        domain_adapter.index(domain_dict, refresh=True)
+        self.addCleanup(domain_adapter.delete, self.domain_obj._id)
+
+        domain = domain_adapter.to_json(self.domain_obj)
+        es_domain = domain_adapter.search({})['hits']['hits'][0]['_source']
+
+        self.assertEqual(es_domain, domain)
+
+    def test_index_can_handle_domain_objects(self):
+        domain_adapter.index(self.domain_obj, refresh=True)
+        self.addCleanup(domain_adapter.delete, self.domain_obj._id)
+
+        domain = domain_adapter.to_json(self.domain_obj)
+        es_domain = domain_adapter.search({})['hits']['hits'][0]['_source']
+
+        self.assertEqual(es_domain, domain)
+
+
 class OneshotIterable:
 
     def __init__(self, items):
