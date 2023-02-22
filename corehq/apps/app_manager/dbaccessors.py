9bc6e893dc2abed6f1de2cf067a247d11c3a9a36
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-02-22 14:21:00 +0200

    code refactoring

--- corehq/apps/app_manager/dbaccessors.py
+++ corehq/apps/app_manager/dbaccessors.py
@@ -498,6 +498,16 @@ def get_version_build_id(domain, app_id, version):
     return build['id']
 
 
+def _get_save_to_case_updates(domain):
+    save_to_case_updates = set()
+    for app in get_apps_in_domain(domain):
+        for form in app.get_forms():
+            for update in form.get_save_to_case_updates():
+                save_to_case_updates.add(update)
+
+    return save_to_case_updates
+
+
 def get_case_types_from_apps(domain):
     """
     Get the case types of modules in applications in the domain.
@@ -506,10 +516,7 @@ def get_case_types_from_apps(domain):
     """
     save_to_case_updates = set()
     if VELLUM_SAVE_TO_CASE.enabled(domain):
-        for app in get_apps_in_domain(domain):
-            for form in app.get_forms():
-                for update in form.get_save_to_case_updates():
-                    save_to_case_updates.add(update)
+        save_to_case_updates = _get_save_to_case_updates(domain)
 
     case_types_agg = NestedAggregation('modules', 'modules').aggregation(
         TermsAggregation('case_types', 'modules.case_type.exact'))
@@ -518,7 +525,8 @@ def get_case_types_from_apps(domain):
          .is_build(False)
          .size(0)
          .aggregation(case_types_agg))
-    return set(q.run().aggregations.modules.case_types.keys).union(save_to_case_updates) - {''}
+    case_types = set(q.run().aggregations.modules.case_types.keys)
+    return (case_types.union(save_to_case_updates) - {''})
 
 
 @quickcache(['domain'], timeout=24 * 60 * 60)
