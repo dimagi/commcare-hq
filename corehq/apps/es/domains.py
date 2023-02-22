2c0e2322c05c90d1f2fc8a7b5c56597a3cfcde6d
Amit Phulera <amitphulera999@gmail.com>
2023-02-22 13:12:10 +0530

    add from_python in domain_adapter

--- corehq/apps/es/domains.py
+++ corehq/apps/es/domains.py
@@ -11,12 +11,16 @@ DomainES
              .created(gte=datespan.startdate, lte=datespan.enddate)
              .size(0))
 """
+from copy import copy
+
+from django_countries import Countries
+
 from . import filters
 from .client import ElasticDocumentAdapter, create_document_adapter
 from .es_query import HQESQuery
 from .index.analysis import COMMA_ANALYSIS
 from .index.settings import IndexSettingsKey
-from .transient_util import get_adapter_mapping, from_dict_with_possible_id
+from .transient_util import get_adapter_mapping
 
 
 class DomainES(HQESQuery):
@@ -55,9 +59,40 @@ class ElasticDomain(ElasticDocumentAdapter):
     def mapping(self):
         return get_adapter_mapping(self)
 
-    @classmethod
-    def from_python(cls, doc):
-        return from_dict_with_possible_id(doc)
+    def from_python(self, domain):
+        """
+        :param user: an instance of ``CouchUser`` or a user dict
+        :raises ``TypeError`` user is none of the above types
+        """
+        from corehq.apps.domain.models import Domain
+        if isinstance(domain, Domain):
+            domain_dict = domain.to_json()
+        elif isinstance(domain, dict):
+            domain_dict = domain
+        else:
+            raise TypeError(f"Unkown type {type(domain)}")
+        return self.from_dict(domain_dict)
+
+    def from_dict(self, domain):
+        """
+        Takes a domain dict and applies required transformation to make it suitable for ES.
+        The function is replica of ``transform_domain_for_elasticsearch``.
+        In future all references to  ``transform_domain_for_elasticsearch`` will be replaced by `from_python`
+
+        :param domain: an instance of ``dict`` which is result of ``Domain.to_json()``
+        """
+        from corehq.apps.accounting.models import Subscription
+
+        doc_ret = copy(domain)
+        sub = Subscription.visible_objects.filter(subscriber__domain=doc_ret['name'], is_active=True)
+        doc_ret['deployment'] = doc_ret.get('deployment', None) or {}
+        countries = doc_ret['deployment'].get('countries', [])
+        doc_ret['deployment']['countries'] = []
+        if sub:
+            doc_ret['subscription'] = sub[0].plan_version.plan.edition
+        for country in countries:
+            doc_ret['deployment']['countries'].append(Countries[country].upper())
+        return doc_ret.pop('_id', None), doc_ret
 
 
 domain_adapter = create_document_adapter(
