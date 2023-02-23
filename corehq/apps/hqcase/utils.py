4c9ef1b89ce76efd562cb0da128f6e1a22b4edf0
Charl Smit <csmit@dimagi.com>
2023-02-23 22:50:35 +0200

    Remove redundant changes in case utils

--- corehq/apps/hqcase/utils.py
+++ corehq/apps/hqcase/utils.py
@@ -120,9 +120,7 @@ def submit_case_block_from_template(domain, template, context, xmlns=None,
     )
 
 
-def _get_update_or_close_case_block(
-    case_id, case_properties=None, close=False, owner_id=None, domain=None, case_name=None
-):
+def _get_update_or_close_case_block(case_id, case_properties=None, close=False, owner_id=None, domain=None):
     kwargs = {
         'create': False,
         'user_id': SYSTEM_USER_ID,
@@ -134,14 +132,12 @@ def _get_update_or_close_case_block(
         kwargs['owner_id'] = owner_id
     if domain:
         kwargs['domain'] = domain
-    if case_name:
-        kwargs['case_name'] = case_name
 
     return CaseBlock.deprecated_init(case_id, **kwargs)
 
 
 def update_case(domain, case_id, case_properties=None, close=False,
-                xmlns=None, device_id=None, form_name=None, owner_id=None, max_wait=..., case_name=None):
+                xmlns=None, device_id=None, form_name=None, owner_id=None, max_wait=...):
     """
     Updates or closes a case (or both) by submitting a form.
     domain - the case's domain
@@ -156,9 +152,7 @@ def update_case(domain, case_id, case_properties=None, close=False,
                the project is over its submission rate limit.
                See the docstring for submit_form_locally for meaning of values
     """
-    caseblock = _get_update_or_close_case_block(
-        case_id, case_properties, close, owner_id, domain=domain, case_name=case_name,
-    )
+    caseblock = _get_update_or_close_case_block(case_id, case_properties, close, owner_id, domain=domain)
     return submit_case_blocks(
         ElementTree.tostring(caseblock.as_xml(), encoding='utf-8').decode('utf-8'),
         domain,
@@ -166,7 +160,7 @@ def update_case(domain, case_id, case_properties=None, close=False,
         xmlns=xmlns,
         device_id=device_id,
         form_name=form_name,
-        max_wait=max_wait,
+        max_wait=max_wait
     )
 
 
