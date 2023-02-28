386dcb58d5e40a8598f83adf1cc0210d1945df18
Akash Jain <akashjain@work.com>
2023-02-28 09:57:02 +0530

    Refactor - url constants

--- custom/abdm/milestone_one/utils/abha_verification_util.py
+++ custom/abdm/milestone_one/utils/abha_verification_util.py
@@ -8,36 +8,36 @@ from custom.abdm.milestone_one.utils.request_util import (
     get_response_http_post,
 )
 
-auth_otp_url = "v2/auth/init"
-confirm_with_mobile_otp_url = "v1/auth/confirmWithMobileOTP"
-confirm_with_aadhaar_otp_url = "v1/auth/confirmWithAadhaarOtp"
-account_information_url = "v1/account/profile"
-search_by_health_id_url = "v1/search/searchByHealthId"
+AUTH_OTP_URL = "v2/auth/init"
+CONFIRM_WITH_MOBILE_OTP_URL = "v1/auth/confirmWithMobileOTP"
+CONFIRM_WITH_AADHAAR_OTP_URL = "v1/auth/confirmWithAadhaarOtp"
+ACCOUNT_INFORMATION_URL = "v1/account/profile"
+SEARCH_BY_HEALTH_ID_URL = "v1/search/searchByHealthId"
 
 
 def generate_auth_otp(health_id, auth_method):
     payload = {"authMethod": auth_method, "healthid": health_id}
-    return get_response_http_post(auth_otp_url, payload)
+    return get_response_http_post(AUTH_OTP_URL, payload)
 
 
 def confirm_with_mobile_otp(otp, txn_id):
     payload = {"otp": otp, "txnId": txn_id}
-    return get_response_http_post(confirm_with_mobile_otp_url, payload)
+    return get_response_http_post(CONFIRM_WITH_MOBILE_OTP_URL, payload)
 
 
 def confirm_with_aadhaar_otp(otp, txn_id):
     payload = {"otp": otp, "txnId": txn_id}
-    return get_response_http_post(confirm_with_aadhaar_otp_url, payload)
+    return get_response_http_post(CONFIRM_WITH_AADHAAR_OTP_URL, payload)
 
 
 def get_account_information(x_token):
     headers = {"Content-Type": "application/json; charset=UTF-8"}
     token = get_access_token()
     headers.update({"Authorization": "Bearer {}".format(token), "X-Token": f"Bearer {x_token}"})
-    resp = requests.get(url=settings.ABDM_BASE_URL + account_information_url, headers=headers)
+    resp = requests.get(url=settings.ABDM_BASE_URL + ACCOUNT_INFORMATION_URL, headers=headers)
     return resp.json()
 
 
 def search_by_health_id(health_id):
     payload = {"healthId": health_id}
-    return get_response_http_post(search_by_health_id_url, payload)
+    return get_response_http_post(SEARCH_BY_HEALTH_ID_URL, payload)
