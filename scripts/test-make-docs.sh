e695445947765394c6fae310c4f9981a0b629b23
Graham Herceg <gherceg@dimagi.com>
2023-03-01 15:14:10 -0800

    whielist yacc table file version is out of date warning

--- scripts/test-make-docs.sh
+++ scripts/test-make-docs.sh
@@ -21,6 +21,7 @@
 WHITELIST_PATTERNS=(
     '^\s*$'  # ignore lines containing only whitespace
     'logger is being changed to' # ignore error when FIX_LOGGER_ERROR_OBFUSCATION is true
+    'yacc table file version is out of date' # warning whenever building docs on a freshly created virtual environment
     # Only whitelist docs build warnings/errors when absolutely necessary
 )
 
