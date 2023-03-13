1b6c230c68a3c1deb4a328c9661a5c8aed5c15da
Norman Hooper <nhooper@dimagi.com>
2023-03-13 17:41:21 +0000

    --wip-- Set up tests for ES

--- corehq/apps/hqcase/tests/test_case_api_permissions.py
+++ corehq/apps/hqcase/tests/test_case_api_permissions.py
@@ -1,24 +1,24 @@
 import uuid
 from contextlib import contextmanager
 
+from django.http import QueryDict
 from django.test import TestCase
 from django.urls import reverse
 
 from casexml.apps.case.mock import CaseBlock
 
 from corehq import privileges
+from corehq.apps.case_search.models import CaseSearchConfig
 from corehq.apps.commtrack.tests.util import bootstrap_domain
-from corehq.apps.es.tests.utils import (
-    case_search_es_setup,
-    case_search_es_teardown,
-    es_test,
-)
+from corehq.apps.es.case_search import case_search_adapter
+from corehq.apps.es.client import manager
+from corehq.apps.es.forms import form_adapter
+from corehq.apps.es.tests.utils import case_search_es_setup, es_test
+from corehq.apps.es.users import user_adapter
 from corehq.apps.hqcase.api.get_list import get_list
 from corehq.apps.locations.tests.util import setup_locations_and_types
 from corehq.apps.users.models import HqPermissions, UserRole, WebUser
-from corehq.elastic import get_es_new
-from corehq.pillows.mappings.user_mapping import USER_INDEX
-from corehq.util.elastic import ensure_index_deleted
+from corehq.form_processor.tests.utils import FormProcessorTestUtils
 from corehq.util.es.testing import sync_users_to_es
 from corehq.util.test_utils import (
     disable_quickcache,
@@ -27,7 +27,11 @@ from corehq.util.test_utils import (
 )
 
 
-@es_test
+@es_test(requires=[
+    case_search_adapter,
+    form_adapter,
+    user_adapter,
+], setup_class=True)
 @disable_quickcache
 @privilege_enabled(privileges.API_ACCESS)
 @flag_enabled('CASE_API_V0_6')
@@ -39,11 +43,12 @@ class TestCaseAPIPermissions(TestCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.domain_obj = bootstrap_domain(cls.domain)
+        CaseSearchConfig.objects.create(pk=cls.domain, enabled=True)
         location_types, locations = setup_locations_and_types(
             cls.domain,
             location_types=['country'],
             stock_tracking_types=[],
-            locations= [('USA', []), ('RSA', [])],
+            locations=[('USA', []), ('RSA', [])],
         )
 
         cls.user_location = locations['RSA']
@@ -58,8 +63,8 @@ class TestCaseAPIPermissions(TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        FormProcessorTestUtils.delete_all_cases()
         cls.domain_obj.delete()
-        case_search_es_teardown()
         super().tearDownClass()
 
     def test_case_api_list_happy_path(self):
@@ -76,6 +81,8 @@ class TestCaseAPIPermissions(TestCase):
             url = reverse('case_api', args=(self.domain,))
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
+            json = response.json()
+            self.assertEqual(json['matching_records'], 2)  # AssertionError: 0 != 2
 
     def test_case_api_list_requires_access_all_locations(self):
         permissions = {
@@ -111,6 +118,8 @@ class TestCaseAPIPermissions(TestCase):
                 content_type='application/json',
             )
             self.assertEqual(response.status_code, 200)
+            json = response.json()
+            self.assertEqual(json['matching_records'], 2)  # AssertionError: 0 != 2
 
     def test_case_api_bulk_fetch_requires_access_all_locations(self):
         permissions = {
@@ -144,9 +153,8 @@ class TestCaseAPIPermissions(TestCase):
             permissions,
             self.client,
         ) as web_user:
-
-            result = get_list(self.domain, web_user, params={})
-            self.assertEqual(result['matching_records'], 2)
+            result = get_list(self.domain, web_user, params=QueryDict())
+            self.assertEqual(result['matching_records'], 2)  # AssertionError: 0 != 2
             self.assertEqual(
                 {c['case_name'] for c in result['cases']},
                 {'Joe', 'Cyril'},
@@ -166,13 +174,9 @@ class TestCaseAPIPermissions(TestCase):
                 permissions,
                 self.client,
             ) as web_user:
-                get_es_new().indices.refresh(USER_INDEX)
-
-                result = get_list(self.domain, web_user, params={})
-                self.assertEqual(result['matching_records'], 1)
+                result = get_list(self.domain, web_user, params=QueryDict())
+                self.assertEqual(result['matching_records'], 1)  # AssertionError: 0 != 1
                 self.assertEqual(result['cases'][0]['case_name'], 'Cyril')
-
-                ensure_index_deleted(USER_INDEX)
 
 
 @contextmanager
@@ -194,6 +198,7 @@ def get_web_user(domain, location, permissions, client):
         role_id=role.get_id,
     )
     web_user.set_location(domain, location)
+    manager.index_refresh(user_adapter.index_name)
     client.login(username=username, password=password)
     try:
         yield web_user
