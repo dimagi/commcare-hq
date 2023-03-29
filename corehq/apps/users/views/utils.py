bbb0be6b52b4fcd895476c52e5a93acaba305e9c
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-03-29 17:02:00 +0200

    change structure of conditional statement

--- corehq/apps/users/views/utils.py
+++ corehq/apps/users/views/utils.py
@@ -177,23 +177,23 @@ def get_locations_with_orphaned_cases(domain, location_ids, user_id):
 
     # Get the assigned location ids that are in all_loc_ids but not in loc_ids_with_other_users
     orphaned_loc_ids = list(set(location_ids) - loc_ids_with_other_users)
-    if orphaned_loc_ids:
-        assigned_locs = SQLLocation.objects.filter(location_id__in=orphaned_loc_ids)
-        # We need to find all descendant locations for orphaned assigned locations that don't have a user
-        # assigned to them
-        desc_locs = SQLLocation.objects.get_queryset_descendants(
-            assigned_locs.filter(location_type__view_descendants=True)
-        )
-        desc_loc_ids = [loc.location_id for loc in desc_locs]
-        desc_loc_ids_with_other_users = _get_location_ids_with_other_users(domain, desc_loc_ids, user_id)
-        orphaned_desc_loc_ids = list(set(desc_loc_ids) - desc_loc_ids_with_other_users)
-
-        all_orphaned_loc_ids = set(orphaned_loc_ids + orphaned_desc_loc_ids)
-        all_locs = set(chain(assigned_locs, desc_locs))
-        case_count_per_loc = _get_location_case_counts(domain, all_orphaned_loc_ids)
-        return {
-            loc.get_path_display(): case_count_per_loc[loc.location_id] for loc in all_locs
-            if loc.location_id in case_count_per_loc
-        }
-    else:
+    if not orphaned_loc_ids:
         return {}
+
+    assigned_locs = SQLLocation.objects.filter(location_id__in=orphaned_loc_ids)
+    # We need to find all descendant locations for orphaned assigned locations that don't have a user
+    # assigned to them
+    desc_locs = SQLLocation.objects.get_queryset_descendants(
+        assigned_locs.filter(location_type__view_descendants=True)
+    )
+    desc_loc_ids = [loc.location_id for loc in desc_locs]
+    desc_loc_ids_with_other_users = _get_location_ids_with_other_users(domain, desc_loc_ids, user_id)
+    orphaned_desc_loc_ids = list(set(desc_loc_ids) - desc_loc_ids_with_other_users)
+
+    all_orphaned_loc_ids = set(orphaned_loc_ids + orphaned_desc_loc_ids)
+    all_locs = set(chain(assigned_locs, desc_locs))
+    case_count_per_loc = _get_location_case_counts(domain, all_orphaned_loc_ids)
+    return {
+        loc.get_path_display(): case_count_per_loc[loc.location_id] for loc in all_locs
+        if loc.location_id in case_count_per_loc
+    }
