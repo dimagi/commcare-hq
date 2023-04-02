c8fd447bf74ec619f8f2c8cc13a68b8f2b18161e
Akash Jain <akashjain@work.com>
2023-04-03 00:24:33 +0530

    Add UT for exists_by_health_id view function

--- custom/abdm/tests/test_abha_verification.py
+++ custom/abdm/tests/test_abha_verification.py
@@ -23,6 +23,7 @@ class TestABHAVerification(APITestCase):
             "v2/auth/init": abdm_txn_id_mock,
             "v1/auth/confirmWithMobileOTP": abdm_txn_id_mock,
             "v1/auth/confirmWithAadhaarOtp": abdm_txn_id_mock,
+            "v1/search/existsByHealthId": {"status": True}
         }.get(url)
 
     @staticmethod
@@ -104,6 +105,18 @@ class TestABHAVerification(APITestCase):
 
     def test_get_health_card_failure(self):
         response = self.client.post(reverse("get_health_card_png"))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(self.invalid_req_msg, response.json().get("message"))
+
+    def test_get_health_id_existence_check_success(self):
+        with patch('custom.abdm.milestone_one.views.abha_verification_views.abdm_util.get_response_http_post',
+                   side_effect=TestABHAVerification._mock_abdm_http_post):
+            response = self.client.post(reverse("exists_by_health_id"), {"health_id": "user@abdm"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json(), {'health_id': 'user@abdm', 'exists': True})
+
+    def test_get_health_id_existence_check_failure(self):
+        response = self.client.post(reverse("exists_by_health_id"))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(self.invalid_req_msg, response.json().get("message"))
 
