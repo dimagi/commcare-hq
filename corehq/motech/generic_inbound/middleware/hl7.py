9f170a002f60a93d25811d219c09f57dda12901f
Simon Kelly <skelly@dimagi.com>
2023-04-06 16:44:03 +0200

    sane variable names

--- corehq/motech/generic_inbound/middleware/hl7.py
+++ corehq/motech/generic_inbound/middleware/hl7.py
@@ -1,42 +1,38 @@
-"""
-Original source: http://www.prschmid.com/2016/11/converting-adt-hl7-message-to-json.html
-"""
-
 from hl7apy.parser import parse_message
 
 
-def hl7_str_to_dict(s, use_long_name=True):
+def hl7_str_to_dict(raw_hl7: str, use_long_name: bool = True) -> dict:
     """Convert an HL7 string to a dictionary
-    :param s: The input HL7 string
-    :param use_long_name: Whether or not to user the long names
+    :param raw_hl7: The input HL7 string
+    :param use_long_name: Whether to use the long names
                           (e.g. "patient_name" instead of "pid_5")
     :returns: A dictionary representation of the HL7 message
     """
-    s = s.replace("\n", "\r")
-    m = parse_message(s)
-    return hl7_message_to_dict(m, use_long_name=use_long_name)
+    raw_hl7 = raw_hl7.replace("\n", "\r")
+    message = parse_message(raw_hl7)
+    return hl7_message_to_dict(message, use_long_name=use_long_name)
 
 
-def hl7_message_to_dict(m, use_long_name=True):
+def hl7_message_to_dict(message_part, use_long_name: bool = True) -> dict:
     """Convert an HL7 message to a dictionary
-    :param m: The HL7 message as returned by :func:`hl7apy.parser.parse_message`
-    :param use_long_name: Whether to user the long names
-                          (e.g. "patient_name" instead of "pid_5")
+    :param message_part: The HL7 message as returned by :func:`hl7apy.parser.parse_message`
+    :param use_long_name: Whether to use the long names (e.g. "patient_name" instead of "pid_5")
     :returns: A dictionary representation of the HL7 message
     """
-    if m.children:
-        d = {}
-        for c in m.children:
-            name = str(c.name).lower()
+    if message_part.children:
+        data = {}
+        for child in message_part.children:
+            name = str(child.name)
             if use_long_name:
-                name = str(c.long_name).lower() if c.long_name else name
-            dictified = hl7_message_to_dict(c, use_long_name=use_long_name)
-            if name in d:
-                if not isinstance(d[name], list):
-                    d[name] = [d[name]]
-                d[name].append(dictified)
+                name = str(child.long_name).lower() if child.long_name else name
+
+            dictified = hl7_message_to_dict(child, use_long_name=use_long_name)
+            if name in data:
+                if not isinstance(data[name], list):
+                    data[name] = [data[name]]
+                data[name].append(dictified)
             else:
-                d[name] = dictified
-        return d
+                data[name] = dictified
+        return data
     else:
-        return m.to_er7() 
+        return message_part.to_er7()
