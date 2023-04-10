01506b92b5e68a0fbc635e9d1abcd444dfb94597
Biyeun Buczyk <biyeun@dimagi.com>
2023-04-10 20:16:37 +0200

    add management command to multi-thread the migration of domains to case search index

---
+++ corehq/apps/case_search/management/commands/move_parallel_domains_to_case_search.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from multiprocessing import cpu_count, Pool
+
+from django.core.management import BaseCommand
+from django.db.models import Sum
+
+from corehq.apps.case_search.models import DomainsNotInCaseSearchIndex
+from corehq.pillows.case_search import CaseSearchReindexerFactory, domain_needs_search_index
+
+
+class Command(BaseCommand):
+    """Moves domain case data to Case Search Index (multi threaded)
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('lowest_size')
+        parser.add_argument('highest_size')
+        parser.add_argument('max_domains')
+        parser.add_argument('max_threads')
+
+    def handle(self, lowest_size, highest_size, max_domains, max_threads, **options):
+        domains = DomainsNotInCaseSearchIndex.objects.filter(
+            estimated_size__gte=lowest_size
+        ).filter(
+            estimated_size__lte=highest_size
+        ).all()[:max_domains]
+
+        num_domains = domains.count()
+        num_cases = domains.aggregate(Sum('estimated_size'))['estimated_size__sum']
+
+        confirm = input(f'\nMigrate {num_domains} domains with {num_cases} cases to Case Search Index [y/n] ')
+        if confirm:
+            self.stdout.write("Migrating...\n")
+            time_started = datetime.utcnow()
+            pool = Pool(processes=cpu_count())
+            pool.map(self.migrate_domain, domains)
+            task_time = datetime.utcnow() - time_started
+            self.stdout.write(f'\nDone...\ntook {task_time.seconds} seconds\n\n\n\n')
+
+    def migrate_domain(self, domain):
+        domain_needs_search_index.clear(domain)
+        DomainsNotInCaseSearchIndex.objects.filter(domain=domain).delete()
+        CaseSearchReindexerFactory(domain=domain).build().reindex()
