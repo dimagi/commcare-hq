bfa30cf080604d4862ff92585d1a405a4c377199
nospame <mapson.evan@gmail.com>
2023-04-13 15:29:55 -0700

    Only check against multimedia_id
    It stays set now thanks to _update_multimedia_map

--- corehq/apps/app_manager/views/utils.py
+++ corehq/apps/app_manager/views/utils.py
@@ -206,6 +206,7 @@ def overwrite_app(app, master_build, report_map=None):
 
 def _update_multimedia_map(old_map, new_map):
     for path in new_map:
+        new_map[path]['upstream_media_id'] = None
         if path in old_map and old_map[path].get('upstream_media_id') == new_map[path]['multimedia_id']:
             new_map[path] = old_map[path]
         new_map[path]['version'] = None
@@ -359,10 +360,7 @@ def update_linked_app(app, master_app_id_or_build, user_id):
         or master_build.version > previous.upstream_version
         or toggles.MULTI_MASTER_LINKED_DOMAINS.enabled(app.domain)
     ):
-        old_multimedia_ids = set(
-            [getattr(media_info, 'upstream_media_id', None) or media_info.multimedia_id
-             for path, media_info in app.multimedia_map.items()]
-        )
+        old_multimedia_ids = set([media_info.multimedia_id for path, media_info in app.multimedia_map.items()])
         report_map = get_static_report_mapping(master_build.domain, app['domain'])
         report_map.update({
             c.report_meta.master_id: c._id
