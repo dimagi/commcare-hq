ef85c30bc8247c6cb47e0044be766bacec76813c
Pawan Verma <pverma@dimagi.com>
2023-04-18 18:13:53 +0530

    Added remove out of sync properties and empty groups

--- corehq/apps/data_dictionary/management/commands/populate_case_prop_groups.py
+++ corehq/apps/data_dictionary/management/commands/populate_case_prop_groups.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.db.models import Q, F
 from corehq.apps.data_dictionary.models import CaseProperty, CasePropertyGroup
 
 from corehq.apps.domain.models import Domain
@@ -13,6 +14,7 @@ class Command(BaseCommand):
             help="Domain name(s). If blank, will generate for all domains")
 
     def handle(self, **options):
+        remove_out_of_sync_prop_and_groups()
         domains = options['domains'] or [d['key'] for d in Domain.get_all(include_docs=False)]
         print("Populating groups for {} domains".format(len(domains)))
 
@@ -31,3 +33,17 @@ def populate_case_prop_groups(domain):
         )
         case_prop.group_obj = group
         case_prop.save()
+
+
+def remove_out_of_sync_prop_and_groups():
+    # Remove properties have different group and group_obj fields
+    properties_out_of_sync = (CaseProperty.objects
+                            .filter(group_obj__isnull=False)
+                            .filter(~Q(group_obj__name=F('group'))))
+    print("Remove out of sync groups for {} properties".format(len(properties_out_of_sync)))
+    properties_out_of_sync.update(group_obj=None)
+
+    # Remove groups which dont have any properties
+    group_without_properties = CasePropertyGroup.objects.filter(property__isnull=True)
+    print("Removing {} groups without properties".format(len(group_without_properties)))
+    group_without_properties.delete()
