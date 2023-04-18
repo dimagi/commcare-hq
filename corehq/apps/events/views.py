5ff0b77fa308bc52533bb35fffd659213b751444
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-04-18 11:18:14 +0200

    remove unused import

--- corehq/apps/events/views.py
+++ corehq/apps/events/views.py
@@ -4,7 +4,7 @@ from django.http import Http404, HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.decorators.http import require_GET, require_POST
+from django.views.decorators.http import require_GET
 
 from memoized import memoized
 
