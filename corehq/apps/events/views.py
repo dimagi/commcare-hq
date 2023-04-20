bfa7aed75d8e7d01a0c6104ef93fbf636a9ced13
Charl Smit <csmit@dimagi.com>
2023-04-20 12:56:23 +0200

    Remove unused import

--- corehq/apps/events/views.py
+++ corehq/apps/events/views.py
@@ -1,5 +1,3 @@
-import json
-
 from django.http import Http404, HttpResponseRedirect, JsonResponse, HttpResponseServerError
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
