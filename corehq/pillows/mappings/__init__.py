c90bacf84a0f2e3ae5b6ecbfb313bf34fd99322a
Amit Phulera <amitphulera999@gmail.com>
2023-04-21 20:18:20 +0530

    delete user mapping file in pillows

--- corehq/pillows/mappings/__init__.py
+++ corehq/pillows/mappings/__init__.py
@@ -3,10 +3,8 @@ from .case_search_mapping import CASE_SEARCH_INDEX_INFO
 from .domain_mapping import DOMAIN_INDEX_INFO
 from .group_mapping import GROUP_INDEX_INFO
 from .sms_mapping import SMS_INDEX_INFO
-from .user_mapping import USER_INDEX_INFO
 
 CANONICAL_NAME_INFO_MAP = {
-    "users": USER_INDEX_INFO,
     "domains": DOMAIN_INDEX_INFO,
     "groups": GROUP_INDEX_INFO,
     "sms": SMS_INDEX_INFO,
