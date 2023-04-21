16f76e8b4d630fa22cf00b7b0eeaf1724ccda0b0
Amit Phulera <amitphulera999@gmail.com>
2023-04-21 20:19:50 +0530

    delete domain mapping file in pillows

--- corehq/pillows/mappings/__init__.py
+++ corehq/pillows/mappings/__init__.py
@@ -1,11 +1,9 @@
 
 from .case_search_mapping import CASE_SEARCH_INDEX_INFO
-from .domain_mapping import DOMAIN_INDEX_INFO
 from .group_mapping import GROUP_INDEX_INFO
 from .sms_mapping import SMS_INDEX_INFO
 
 CANONICAL_NAME_INFO_MAP = {
-    "domains": DOMAIN_INDEX_INFO,
     "groups": GROUP_INDEX_INFO,
     "sms": SMS_INDEX_INFO,
     "case_search": CASE_SEARCH_INDEX_INFO,
