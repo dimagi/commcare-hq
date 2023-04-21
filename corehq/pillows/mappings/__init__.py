b85d71e44872db2ab9e762cb8132254f6d9d6415
Amit Phulera <amitphulera999@gmail.com>
2023-04-21 20:21:17 +0530

    delete group mapping file in pillows

--- corehq/pillows/mappings/__init__.py
+++ corehq/pillows/mappings/__init__.py
@@ -1,10 +1,8 @@
 
 from .case_search_mapping import CASE_SEARCH_INDEX_INFO
-from .group_mapping import GROUP_INDEX_INFO
 from .sms_mapping import SMS_INDEX_INFO
 
 CANONICAL_NAME_INFO_MAP = {
-    "groups": GROUP_INDEX_INFO,
     "sms": SMS_INDEX_INFO,
     "case_search": CASE_SEARCH_INDEX_INFO,
 }
