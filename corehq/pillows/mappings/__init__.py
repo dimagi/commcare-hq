c6f110016cc50b9ad4b81279adc3a9876029e364
Amit Phulera <amitphulera999@gmail.com>
2023-04-21 20:25:52 +0530

    delete sms mapping file in pillows

--- corehq/pillows/mappings/__init__.py
+++ corehq/pillows/mappings/__init__.py
@@ -1,8 +1,6 @@
 
 from .case_search_mapping import CASE_SEARCH_INDEX_INFO
-from .sms_mapping import SMS_INDEX_INFO
 
 CANONICAL_NAME_INFO_MAP = {
-    "sms": SMS_INDEX_INFO,
     "case_search": CASE_SEARCH_INDEX_INFO,
 }
