77e862b65543de182511b56d9378d3d3c6037d0c
Biyeun Buczyk <biyeun@dimagi.com>
2023-04-25 18:16:43 +0200

    split hq-bug-report into bootstrap 5 and bootstrap 3 versions

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/base_main.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/base_main.js
@@ -3,7 +3,7 @@ hqDefine("hqwebapp/js/bootstrap5/base_main", [
     'hqwebapp/js/prepaid_modal',
     'hqwebapp/js/bootstrap5/hq.helpers',
     'hqwebapp/js/layout',
-    'hqwebapp/js/hq-bug-report',
+    'hqwebapp/js/bootstrap5/hq-bug-report',
     'hqwebapp/js/bootstrap5/sticky_tabs',
     'hqwebapp/js/main',
     'hqwebapp/js/inactivity',
