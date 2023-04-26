73b84e4661c31624d7a3f05ebe4ad9772de66df9
Simon Kelly <skelly@dimagi.com>
2023-04-26 15:08:49 +0200

    extract base middleware class

---
+++ corehq/motech/generic_inbound/middleware/base.py
@@ -0,0 +1,81 @@
+from contextlib import contextmanager
+
+from corehq.apps.hqcase.api.core import SubmissionError, UserError
+from corehq.apps.userreports.exceptions import BadSpecError
+from corehq.motech.generic_inbound.exceptions import (
+    GenericInboundApiError,
+    GenericInboundRequestFiltered,
+    GenericInboundUserError,
+    GenericInboundValidationError,
+)
+from corehq.motech.generic_inbound.utils import get_evaluation_context, ApiResponse
+
+
+class BaseApiMiddleware:
+    """Base class for Generic API middleware.
+
+    Middleware is used to translate incoming data to JSON which the API can process. The response
+    is also translated from JSON to the appropriate data format."""
+    error_response = None
+
+    def get_context(self, request_data):
+        return get_evaluation_context(
+            request_data.restore_user,
+            request_data.request_method,
+            request_data.query,
+            request_data.headers,
+            self._get_body_for_eval_context(request_data)
+        )
+
+    @contextmanager
+    def handle_errors(self):
+        """Context manager for handling errors during execution of the API.
+
+        If an error is handled use ``get_error_response`` to get the ApiResponse.
+        """
+        try:
+            yield
+        except GenericInboundRequestFiltered:
+            self.error_response = ApiResponse(status=204)
+        except GenericInboundValidationError as e:
+            self.error_response = self._get_validation_error(400, 'validation error', e.errors)
+        except SubmissionError as e:
+            self.error_response = self._get_submission_error_response(400, e.form_id, str(e))
+        except Exception as e:
+            try:
+                status = {
+                    UserError: 400,
+                    BadSpecError: 500,
+                    GenericInboundUserError: 400,
+                    GenericInboundApiError: 500,
+                }[type(e)]
+            except KeyError:
+                raise e
+            self.error_response = self._get_generic_error(status, str(e))
+
+    def get_error_response(self):
+        """In an error is handled by the ``handle_errors`` context manager, the response will be
+        available here."""
+        return self.error_response
+
+    def _get_body_for_eval_context(self, request_data):
+        """Give the RequestData, return a dictionary of data which will be placed into the ``body``
+        attribute of the evaluation context.
+        """
+        raise NotImplementedError
+
+    def _get_success_response(self, response_json):
+        """Given a successful API response JSON, return an ``ApiResponse`` object."""
+        raise NotImplementedError
+
+    def _get_generic_error(self, status_code, message):
+        """Return an ApiResponse object with the given status code and message."""
+        raise NotImplementedError
+
+    def _get_submission_error_response(self, status_code, form_id, message):
+        """Return an ApiResponse object or a submission processing error."""
+        raise NotImplementedError
+
+    def _get_validation_error(self, status_code, message, errors):
+        """Return an ApiResponse object for validation errors."""
+        raise NotImplementedError
--- corehq/motech/generic_inbound/middleware/json.py
+++ corehq/motech/generic_inbound/middleware/json.py
@@ -1,72 +1,35 @@
 import json
-from contextlib import contextmanager
 
 from django.utils.translation import gettext
 
-from corehq.apps.hqcase.api.core import SubmissionError, UserError
-from corehq.apps.userreports.exceptions import BadSpecError
-from corehq.motech.generic_inbound.exceptions import GenericInboundUserError, GenericInboundRequestFiltered, \
-    GenericInboundValidationError, GenericInboundApiError
-from corehq.motech.generic_inbound.utils import get_evaluation_context, ApiResponse
+from corehq.motech.generic_inbound.exceptions import GenericInboundUserError
+from corehq.motech.generic_inbound.middleware.base import BaseApiMiddleware
+from corehq.motech.generic_inbound.utils import ApiResponse
 
 
-class JsonMiddleware:
-    error_response = None
+class JsonMiddleware(BaseApiMiddleware):
+    """API middleware for handling JSON payloads"""
 
-    def get_context(self, request_data):
-        return get_evaluation_context(
-            request_data.restore_user,
-            request_data.request_method,
-            request_data.query,
-            request_data.headers,
-            self.convert_data(request_data.data)
-        )
+    def _get_body_for_eval_context(self, request_data):
+        try:
+            return json.loads(request_data.data)
+        except json.JSONDecodeError:
+            raise GenericInboundUserError(gettext("Payload must be valid JSON"))
 
-    def get_success_response(self, response_json):
+    def _get_success_response(self, response_json):
         return ApiResponse(status=200, data=response_json)
 
-    def get_generic_error(self, status, message):
-        return ApiResponse(status=status, data={'error': message})
+    def _get_generic_error(self, status_code, message):
+        return ApiResponse(status=status_code, data={'error': message})
 
-    def get_submission_error_response(self, status, form_id, message):
-        return ApiResponse(status=status, data={
+    def _get_submission_error_response(self, status_code, form_id, message):
+        return ApiResponse(status=status_code, data={
             'error': message,
             'form_id': form_id,
         })
 
-    def get_validation_error(self, status, message, errors):
-        return ApiResponse(status=status, data={
+    def _get_validation_error(self, status_code, message, errors):
+        return ApiResponse(status=status_code, data={
             'error': message,
             'errors': errors,
         })
-
-    def convert_data(self, data):
-        try:
-            return json.loads(data)
-        except json.JSONDecodeError:
-            raise GenericInboundUserError(gettext("Payload must be valid JSON"))
-
-    @contextmanager
-    def handle_errors(self):
-        try:
-            yield
-        except GenericInboundRequestFiltered:
-            self.error_response = ApiResponse(status=204)
-        except GenericInboundValidationError as e:
-            self.error_response = self.get_validation_error(400, 'validation error', e.errors)
-        except SubmissionError as e:
-            self.error_response = self.get_submission_error_response(400, e.form_id, str(e))
-        except Exception as e:
-            try:
-                status = {
-                    UserError: 400,
-                    BadSpecError: 500,
-                    GenericInboundUserError: 400,
-                    GenericInboundApiError: 500,
-                }[type(e)]
-            except KeyError:
-                raise e
-            self.error_response = self.get_generic_error(status, str(e))
-
-    def get_error_response(self):
-        return self.error_response
