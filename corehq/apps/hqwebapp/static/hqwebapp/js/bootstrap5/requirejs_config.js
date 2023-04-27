f9bf7a018a21ef57613c557282f2b7724bc0ade9
Biyeun Buczyk <biyeun@dimagi.com>
2023-04-27 18:22:39 +0200

    modify bootstrap5 version of requirejs_config to now use bootstrap 5

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/requirejs_config.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/requirejs_config.js
@@ -2,9 +2,12 @@
 requirejs.config({
     baseUrl: '/static/',
     paths: {
+        "es6": "requirejs-babel7/es6",
+        "babel": "@babel/standalone/babel.min",
+        "babel-plugin-transform-modules-requirejs-babel": "babel-plugin-transform-modules-requirejs-babel/index",
         "jquery": "jquery/dist/jquery.min",
         "underscore": "underscore/underscore",
-        "bootstrap": "bootstrap/dist/js/bootstrap.min",
+        "bootstrap5": "bootstrap5/dist/js/bootstrap.bundle.min",
         "knockout": "knockout/build/output/knockout-latest.debug",
         "ko.mapping": "hqwebapp/js/lib/knockout_plugins/knockout_mapping.ko.min",
         "datatables": "datatables.net/js/jquery.dataTables.min",
@@ -15,9 +18,8 @@ requirejs.config({
     },
     shim: {
         "ace-builds/src-min-noconflict/ace": { exports: "ace" },
-        "bootstrap": { deps: ['jquery'] },
         "ko.mapping": { deps: ['knockout'] },
-        "hqwebapp/js/hq.helpers": { deps: ['jquery', 'bootstrap', 'knockout', 'underscore'] },
+        "hqwebapp/js/hq.helpers": { deps: ['jquery', 'knockout', 'underscore'] },
         "datatables.bootstrap": { deps: ['datatables'] },
         "jquery.rmi/jquery.rmi": {
             deps: ['jquery', 'knockout', 'underscore'],
