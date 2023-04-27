d2baf558f7f90761b7cbe04d19367f25d8b36138
Minha <mlee@dimagi.com>
2023-04-27 14:57:22 -0400

    Adjust user input date to UTC based on project timezone

--- corehq/apps/case_search/xpath_functions/comparison.py
+++ corehq/apps/case_search/xpath_functions/comparison.py
@@ -1,12 +1,17 @@
+from datetime import datetime, timedelta
+
 from django.utils.translation import gettext
 from eulxml.xpath import serialize
 from eulxml.xpath.ast import Step
 
 from corehq.apps.case_search.dsl_utils import unwrap_value
-from corehq.apps.case_search.exceptions import CaseFilterError
+from corehq.apps.case_search.exceptions import CaseFilterError, XPathFunctionException
+from corehq.apps.case_search.xpath_functions.value_functions import _value_to_date
 from corehq.apps.case_search.const import RANGE_OP_MAPPING, EQ, NEQ
 from corehq.apps.es import filters
 from corehq.apps.es.case_search import case_property_query, case_property_range_query
+from corehq.util.timezones.utils import get_timezone_for_domain
+from corehq.util.timezones.conversions import UserTime
 
 
 def property_comparison_query(context, case_property_name_raw, op, value_raw, node):
@@ -18,6 +23,20 @@ def property_comparison_query(context, case_property_name_raw, op, value_raw, no
 
     case_property_name = serialize(case_property_name_raw)
     value = unwrap_value(value_raw, context)
+    # adjust the user's input date based on project timezones
+    is_user_input = False
+    try:
+        date = _value_to_date(node, value)
+        date = datetime.combine(date, datetime.min.time())
+        if op == '>' or op == '<=':
+            # this might be inconsistent in daylight savings situations
+            date += timedelta(days=1)
+        project_timezone = get_timezone_for_domain(context.domain)
+        value = UserTime(date, tzinfo=project_timezone).server_time().done().isoformat()
+        is_user_input = True
+    except (XPathFunctionException, AssertionError):
+        # AssertionError is caused by tests that use domains without a valid timezone (in get_timezeone_for_domain)
+        pass
     if op in [EQ, NEQ]:
         query = case_property_query(case_property_name, value, fuzzy=context.fuzzy)
         if op == NEQ:
@@ -25,7 +44,8 @@ def property_comparison_query(context, case_property_name_raw, op, value_raw, no
         return query
     else:
         try:
-            return case_property_range_query(case_property_name, **{RANGE_OP_MAPPING[op]: value})
+            return case_property_range_query(case_property_name, is_user_input=is_user_input,
+                                             **{RANGE_OP_MAPPING[op]: value})
         except (TypeError, ValueError):
             raise CaseFilterError(
                 gettext("The right hand side of a comparison must be a number or date. "
