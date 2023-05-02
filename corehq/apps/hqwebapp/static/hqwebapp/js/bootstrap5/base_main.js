46bbd0d06b4c268a28d881876ea589f4391642e4
Biyeun Buczyk <biyeun@dimagi.com>
2023-05-02 19:50:08 +0200

    update sticky_tabs bootstrap5 version

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/base_main.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/base_main.js
@@ -4,7 +4,7 @@ hqDefine("hqwebapp/js/bootstrap5/base_main", [
     'hqwebapp/js/hq.helpers',
     'hqwebapp/js/layout',
     'hqwebapp/js/hq-bug-report',
-    'hqwebapp/js/sticky_tabs',
+    'hqwebapp/js/bootstrap5/sticky_tabs',
     'hqwebapp/js/main',
     'hqwebapp/js/inactivity',
     'notifications/js/notifications_service_main',
