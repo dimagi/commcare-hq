6a7d356d3864ed59fd6cba1be508e095dc3df878
Biyeun Buczyk <biyeun@dimagi.com>
2023-05-04 15:58:37 +0200

    add management command to verify data integrity

---
+++ corehq/apps/case_search/management/commands/check_cle_move_counts.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from django.core.management import BaseCommand
+
+from corehq.apps.case_search.models import DomainsNotInCaseSearchIndex
+from corehq.apps.domain.models import Domain
+from corehq.apps.es import CaseES, CaseSearchES
+from dimagi.utils.couch.database import iter_docs
+
+
+class Command(BaseCommand):
+    """Runs data integrity checks to verify domains
+    """
+
+    def handle(self, **options):
+        all_domain_ids = [d['id'] for d in Domain.get_all(include_docs=False)]
+        date_of_move = datetime(2023, 4, 1)
+        for domain_doc in iter_docs(Domain.get_db(), all_domain_ids):
+            domain_obj = Domain.wrap(domain_doc)
+            if domain_obj.date_created > date_of_move:
+                continue
+            total_case_es = CaseES().domain(domain_obj.name).count()
+            total_case_search = CaseSearchES().domain(domain_obj.name).count()
+
+            if (total_case_es != total_case_search
+                    and not DomainsNotInCaseSearchIndex.objects.filter(domain=domain_obj.name)):
+                difference = total_case_es - total_case_search
+                self.stdout.write(f"mismatch found:\t\t{domain_obj.name}\t{difference}")
+                DomainsNotInCaseSearchIndex.objects.create(domain=domain_obj.name, estimated_size=difference)
