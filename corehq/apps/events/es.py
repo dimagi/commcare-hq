75fe2e4b48a88b53a1794ddef73a88587b35cd8d
Norman Hooper <nhooper@dimagi.com>
2023-05-10 23:56:13 +0100

    attendee_location_filter()

--- corehq/apps/events/es.py
+++ corehq/apps/events/es.py
@@ -1,8 +1,37 @@
 from __future__ import annotations
 
-from corehq.apps.es import CaseES
-from corehq.apps.events.models import get_attendee_case_type
+from corehq.apps.es import CaseES, CaseSearchES
+from corehq.apps.es.case_search import case_property_text_query
+from corehq.apps.events.models import (
+    LOCATION_IDS_CASE_PROPERTY,
+    get_attendee_case_type,
+)
 from corehq.form_processor.models import CommCareCase
+
+
+class AttendeeSearchES(CaseSearchES):
+    """
+    Allows Attendee cases to be searched by location, like Users.
+    """
+    @property
+    def builtin_filters(self):
+        return [
+            attendee_location_filter,
+        ] + super().builtin_filters
+
+
+def attendee_location_filter(location_ids):
+    """
+    Return Attendee cases whose location IDs include at least one of
+    ``location_ids``.
+    """
+    if not isinstance(location_ids, str):
+        location_ids = ' '.join(location_ids)
+    return case_property_text_query(
+        LOCATION_IDS_CASE_PROPERTY,
+        location_ids,
+        operator='or',  # Any location ID will be a hit
+    )
 
 
 def get_paginated_attendees(domain, limit, page, query=None):
--- corehq/apps/events/tests/test_es.py
+++ corehq/apps/events/tests/test_es.py
@@ -3,15 +3,144 @@ from uuid import uuid4
 
 from django.test import TestCase
 
-from corehq.apps.es import case_adapter
+from corehq.apps.domain.shortcuts import create_domain
+from corehq.apps.es import case_adapter, case_search_adapter
 from corehq.apps.es.tests.utils import es_test
+from corehq.apps.locations.models import LocationType, SQLLocation
 from corehq.form_processor.models import CommCareCase
 from corehq.form_processor.tests.utils import create_case
 
-from ..es import get_paginated_attendees
-from ..models import get_attendee_case_type
+from ..es import AttendeeSearchES, get_paginated_attendees
+from ..models import LOCATION_IDS_CASE_PROPERTY, get_attendee_case_type
+from ...case_search.models import CaseSearchConfig
 
 DOMAIN = 'test-domain'
+
+
+@es_test(requires=[case_search_adapter], setup_class=True)
+class TestAttendeeLocationFilter(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.domain_obj = create_domain(DOMAIN)
+        cls.config = CaseSearchConfig.objects.create(
+            domain=DOMAIN,
+            enabled=True,
+        )
+
+        cls.country = LocationType.objects.create(
+            domain=DOMAIN,
+            name='Country',
+        )
+        cls.city = LocationType.objects.create(
+            domain=DOMAIN,
+            name='City',
+            parent_type=cls.country,
+        )
+        cls.suriname = SQLLocation.objects.create(
+            domain=DOMAIN,
+            name='Suriname',
+            location_id=str(uuid4()),
+            location_type=cls.country,
+        )
+        cls.paramaribo = SQLLocation.objects.create(
+            domain=DOMAIN,
+            name='Paramaribo',
+            location_id=str(uuid4()),
+            location_type=cls.city,
+            parent=cls.suriname,
+        )
+
+        case_type = get_attendee_case_type(DOMAIN)
+        cls.country_attendee = create_case(
+            DOMAIN,
+            case_id=uuid4().hex,
+            case_type=case_type,
+            name='Countryboy',
+            case_json={
+                LOCATION_IDS_CASE_PROPERTY: cls.suriname.location_id,
+            },
+            save=True,
+        )
+        cls.city_attendee = create_case(
+            DOMAIN,
+            case_id=uuid4().hex,
+            case_type=case_type,
+            name='Citygirl',
+            case_json={
+                LOCATION_IDS_CASE_PROPERTY: cls.paramaribo.location_id,
+            },
+            save=True,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        CommCareCase.objects.hard_delete_cases(DOMAIN, [
+            cls.city_attendee.case_id,
+            cls.country_attendee.case_id,
+        ])
+        cls.paramaribo.delete()
+        cls.suriname.delete()
+        cls.city.delete()
+        cls.country.delete()
+        cls.config.delete()
+        cls.domain_obj.delete()
+        super().tearDownClass()
+
+    def test_parent_location(self):
+        case_ids = (
+            AttendeeSearchES()
+            .attendee_location_filter(self.suriname.location_id)
+            .get_ids()
+        )
+        self.assertEqual(case_ids, [self.country_attendee.case_id])
+
+    def test_child_location(self):
+        case_ids = (
+            AttendeeSearchES()
+            .attendee_location_filter(self.paramaribo.location_id)
+            .get_ids()
+        )
+        self.assertEqual(case_ids, [self.city_attendee.case_id])
+
+    def test_location_ids_list(self):
+        location_ids = [
+            self.suriname.location_id,
+            self.paramaribo.location_id,
+        ]
+        case_ids = (
+            AttendeeSearchES()
+            .attendee_location_filter(location_ids)
+            .get_ids()
+        )
+        self.assertEqual(set(case_ids), {
+            self.country_attendee.case_id,
+            self.city_attendee.case_id,
+        })
+
+    def test_location_ids_str(self):
+        location_ids = ' '.join((
+            self.suriname.location_id,
+            self.paramaribo.location_id,
+        ))
+        case_ids = (
+            AttendeeSearchES()
+            .attendee_location_filter(location_ids)
+            .get_ids()
+        )
+        self.assertEqual(set(case_ids), {
+            self.country_attendee.case_id,
+            self.city_attendee.case_id,
+        })
+
+    def test_no_location_ids(self):
+        case_ids = (
+            AttendeeSearchES()
+            .attendee_location_filter('')
+            .get_ids()
+        )
+        self.assertEqual(case_ids, [])
 
 
 @es_test(requires=[case_adapter], setup_class=True)
