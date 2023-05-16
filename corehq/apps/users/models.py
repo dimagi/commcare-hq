c862144ee17e01d809925f8599cf468bf2e1736a
robert-costello <robertcostello81@gmail.com>
2023-05-16 19:00:14 -0400

    rearrange current version reference

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -112,7 +112,6 @@ COMMCARE_USER = 'commcare'
 MAX_WEB_USER_LOGIN_ATTEMPTS = 5
 MAX_COMMCARE_USER_LOGIN_ATTEMPTS = 500
 
-CURRENT_VERSION = '3.0'  # Set this to the most up to date version of the eula
 
 
 def _add_to_list(list, obj, default):
@@ -712,7 +711,7 @@ class DjangoUserMixin(DocumentSchema):
 
 
 class EulaMixin(DocumentSchema):
-    CURRENT_VERSION = CURRENT_VERSION
+    CURRENT_VERSION = '3.0'  # Set this to the most up to date version of the eula
     eulas = SchemaListProperty(LicenseAgreement)
 
     @classmethod
