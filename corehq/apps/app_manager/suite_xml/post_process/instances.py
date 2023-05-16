9e716974f48f76adee8a0b97c764467f580df29a
Steph <stephherbers@gmail.com>
2023-05-16 10:47:28 -0400

    add missing parentheses

--- corehq/apps/app_manager/suite_xml/post_process/instances.py
+++ corehq/apps/app_manager/suite_xml/post_process/instances.py
@@ -341,7 +341,7 @@ def search_input_instances(app, instance_name):
 
 @register_factory('selected_cases')
 def selected_cases_instances(app, instance_name):
-    return Instance(id=instance_name, src=f'jr://instance/selected-entities/{instance_name}'
+    return Instance(id=instance_name, src=f'jr://instance/selected-entities/{instance_name}')
 
 
 @register_factory('results')
