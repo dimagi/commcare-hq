c2b0d5d40cfc9ae302036f66df4bdb9b7b736669
Jennifer Schweers <orange.jenny@gmail.com>
2023-05-17 11:08:02 -0600

    Moved field options into javascript

--- corehq/apps/app_manager/static/app_manager/js/details/screen_config.js
+++ corehq/apps/app_manager/static/app_manager/js/details/screen_config.js
@@ -66,6 +66,7 @@ hqDefine('app_manager/js/details/screen_config', function () {
                     containsCustomXMLConfiguration: columnType === "short",
                     allowsTabs: columnType === 'long',
                     allowsEmptyColumns: columnType === 'long',
+                    caseTileFields: ["", "header", "top_left", "bottom_left", "date", "sex"],
                 }
             );
             self.screens.push(screen);
