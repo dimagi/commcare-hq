ebdff68cb8b3f45629c4bcde76042e9d4e3ecfdc
robert-costello <robertcostello81@gmail.com>
2023-05-18 14:19:00 -0400

    Revert "clean up is_eula_signed logic"
    This reverts commit 2dc02d114cb2a76f57029853dcfc31b21324763c.

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -728,9 +728,7 @@ class EulaMixin(DocumentSchema):
         current_eula = self.eula
         if current_eula.version == version:
             if toggles.FORCE_ANNUAL_TOS.enabled(current_domain):
-                if not current_eula.date:
-                    return False
-                elapsed = datetime.now() - current_eula.date
+                elapsed = datetime.now() - current_eula.date if current_eula.date else 365
                 return current_eula.signed and elapsed.days < 365
             return current_eula.signed
         return False
