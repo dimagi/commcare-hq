6b3b62a7113e2f18ac24dcc9e42fb12efd136600
robert-costello <robertcostello81@gmail.com>
2023-05-18 14:18:42 -0400

    Revert "make current version a constant"
    This reverts commit 485b84c827ab5867d994410001e6d9780497e139.

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -112,8 +112,6 @@ COMMCARE_USER = 'commcare'
 MAX_WEB_USER_LOGIN_ATTEMPTS = 5
 MAX_COMMCARE_USER_LOGIN_ATTEMPTS = 500
 
-CURRENT_VERSION = '3.0'  # Set this to the most up to date version of the eula
-
 
 def _add_to_list(list, obj, default):
     if obj in list:
@@ -712,6 +710,7 @@ class DjangoUserMixin(DocumentSchema):
 
 
 class EulaMixin(DocumentSchema):
+    CURRENT_VERSION = '3.0'  # Set this to the most up to date version of the eula
     eulas = SchemaListProperty(LicenseAgreement)
 
     @classmethod
