4c6f9c97a1ba770231c2a4f059f8cad94252f864
Jennifer Schweers <orange.jenny@gmail.com>
2023-05-19 23:28:06 -0600

    Moved to matrix-based logic that reconciles all affected parts of tile

--- corehq/apps/app_manager/static/app_manager/js/details/screen.js
+++ corehq/apps/app_manager/static/app_manager/js/details/screen.js
@@ -95,15 +95,170 @@ hqDefine("app_manager/js/details/screen", function () {
                 column.tileWidth(3);
             });
         };
-        self.adjustTileGridArea = function (columnIndex, rowDelta, columnDelta, widthDelta, heightDelta) {
-            const activeColumn = self.columns()[columnIndex];
-            activeColumn.tileRowStart(activeColumn.tileRowStart() + rowDelta);
-            activeColumn.tileColumnStart(activeColumn.tileColumnStart() + columnDelta);
-            activeColumn.tileWidth(activeColumn.tileWidth() + widthDelta);
-            activeColumn.tileHeight(activeColumn.tileHeight() + heightDelta);
 
-            // TODO: Reconcile other affected cells
+        self.adjustTileGridArea = function (activeColumnIndex, rowDelta, columnDelta, widthDelta, heightDelta) {
+            let matrix = self._buildMatrix();
+            matrix = self._adjustTileGridArea(matrix, activeColumnIndex, rowDelta, columnDelta, widthDelta, heightDelta);
+            self._parseMatrix(matrix);
         };
+
+        // TODO: replace rowDelta, columnDelta, widthDelta, heightDelta with a single action param
+        self._adjustTileGridArea = function (matrix, activeColumnIndex, rowDelta, columnDelta, widthDelta, heightDelta) {
+            let activeColumn = self.columns()[activeColumnIndex];
+
+            // Validate column still has size
+            if (activeColumn.tileWidth() + widthDelta < 1 || activeColumn.tileHeight() + heightDelta < 1) {
+                throw new Error("tile shrank to nothing");
+            }
+
+            // Validate boundaries
+            const newRowStart = activeColumn.tileRowStart() + rowDelta,
+                  newColumnStart = activeColumn.tileColumnStart() + columnDelta,
+                  newRowEnd = newRowStart + activeColumn.tileHeight() + heightDelta,
+                  newColumnEnd = newColumnStart + activeColumn.tileWidth() + widthDelta;
+            if (newRowStart < 1 || newRowEnd > 4 || newColumnStart < 1 || newColumnEnd > 13) {
+                throw new Error("cannot move tile out of bounds");
+            }
+
+            // Identify matrix points to null out
+            if (rowDelta === 1) {
+                matrix = self._nullMatrixRow(matrix, activeColumn.tileRowStart(), activeColumn.tileColumnStart(), activeColumn.tileColumnEnd());
+            } else if (rowDelta === -1 || heightDelta === -1) {
+                matrix = self._nullMatrixRow(matrix, activeColumn.tileRowEnd() - 1, activeColumn.tileColumnStart(), activeColumn.tileColumnEnd());
+            } else if (columnDelta === 1) {
+                matrix = self._nullMatrixColumn(matrix, activeColumn.tileColumnStart(), activeColumn.tileRowStart(), activeColumn.tileRowEnd());
+            } else if (columnDelta === -1 || widthDelta === -1) {
+                matrix = self._nullMatrixColumn(matrix, activeColumn.tileColumnEnd() - 1, activeColumn.tileRowStart(), activeColumn.tileRowEnd());
+            }
+
+            // Identify matrix points to fill in
+            if (rowDelta === 1 || heightDelta === 1) {
+                matrix = self._replaceMatrixRow(matrix, activeColumnIndex, activeColumn.tileRowEnd(), activeColumn.tileColumnStart(), activeColumn.tileColumnEnd());
+            } else if (rowDelta === -1) {
+                matrix = self._replaceMatrixRow(matrix, activeColumnIndex, activeColumn.tileRowStart() - 1, activeColumn.tileColumnStart(), activeColumn.tileColumnEnd());
+            } else if (columnDelta === 1 || widthDelta === 1) {
+                matrix = self._replaceMatrixColumn(matrix, activeColumnIndex, activeColumn.tileColumnEnd(), activeColumn.tileRowStart(), activeColumn.tileRowEnd());
+            } else if (columnDelta === -1) {
+                matrix = self._replaceMatrixColumn(matrix, activeColumnIndex, activeColumn.tileColumnStart() - 1, activeColumn.tileRowStart(), activeColumn.tileRowEnd());
+            }
+
+            return matrix;
+        };
+
+        // TODO: combine these into 2 functions instead of 4
+        self._nullMatrixRow = function (matrix, row, col1, col2) {
+            for (let i = col1 - 1; i < col2 - 1; i++) {
+                matrix[row - 1][i] = null;
+            }
+            return matrix;
+        }
+        self._nullMatrixColumn = function (matrix, column, row1, row2) {
+            for (let i = row1 - 1; i < row2 - 1; i++) {
+               matrix[i][column - 1] = null;
+            }
+            return matrix;
+        }
+
+        self._replaceMatrixRow = function (matrix, newValue, row, col1, col2) {
+            for (let i = col1 - 1; i < col2 - 1; i++) {
+                const oldValue = matrix[row - 1][i];
+                if (oldValue !== null) {
+                    try {
+                        // Attempt to move. If the row above is newValue, move down, otherwise move up.
+                        const rowDelta = row > 1 && matrix[row - 2][i] === newValue ? 1 : -1;
+                        console.log("Attempt to move #" + oldValue + " " + (rowDelta === 1 ? "down" : "up"));
+                        matrix = self._adjustTileGridArea(matrix, oldValue, rowDelta, 0, 0, 0);
+                    } catch (e) {
+                        try {
+                            // attempt to shrink height
+                            console.log("Attempt to shrink height of #" + oldValue);
+                            matrix = self._adjustTileGridArea(matrix, oldValue, 0, 0, 0, -1);
+                        } catch (e) {
+                            throw new Error("cannot _replaceMatrixRow");
+                        }
+                    }
+                }
+                matrix[row - 1][i] = newValue;
+            }
+            return matrix;
+        }
+        self._replaceMatrixColumn = function (matrix, newValue, column, row1, row2) {
+            for (let i = row1 - 1; i < row2 - 1; i++) {
+                const oldValue = matrix[i][column - 1];
+                if (oldValue !== null) {
+                    try {
+                        // Attempt to move. If the column to the left is newValue, move right, otherwise move left.
+                        const columnDelta = column > 1 && matrix[i][column - 2] === newValue ? 1 : -1;
+                        console.log("Attempt to move #" + oldValue + " " + (columnDelta === 1 ? "right" : "left"));
+                        matrix = self._adjustTileGridArea(matrix, oldValue, 0, columnDelta, 0, 0);
+                    } catch (e) {
+                        try {
+                            // attempt to shrink width
+                            console.log("Attempt to shrink width of #" + oldValue);
+                            matrix = self._adjustTileGridArea(matrix, oldValue, 0, 0, -1, 0);
+                        } catch (e) {
+                            throw new Error("cannot _replaceMatrixColumn");
+                        }
+                    }
+                }
+                matrix[i][column - 1] = newValue;
+            }
+            return matrix;
+        }
+
+        self._buildMatrix = function () {
+            let matrix = [
+                [null, null, null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null, null, null],
+                [null, null, null, null, null, null, null, null, null, null, null, null]
+            ];
+            _.each(self.columns(), function (column, columnIndex) {
+                if (!column.showInTilePreview()) {
+                    return;
+                }
+                for (let i = 0; i < column.tileHeight(); i++) {
+                    for (let j = 0; j < column.tileWidth(); j++) {
+                        matrix[i + column.tileRowStart() - 1][j + column.tileColumnStart() - 1] = columnIndex;
+                    }
+                }
+            });
+            return matrix;
+        };
+        self._parseMatrix = function (matrix) {
+            let columns = _.map(_.range(self.columns().length), function () { return {
+                rowStart: null,
+                rowEnd: null,
+                columnStart: null,
+                columnEnd: null,
+            }; });
+
+            for (let i = 0; i < 3; i++) {
+                for (let j = 0; j < 12; j++) {
+                    const columnIndex = matrix[i][j];
+                    if (columnIndex === null) {
+                        continue;
+                    }
+                    if (columns[columnIndex].rowStart === null) {
+                        columns[columnIndex].rowStart = i;
+                    }
+                    columns[columnIndex].rowEnd = i;
+                    if (columns[columnIndex].columnStart === null) {
+                        columns[columnIndex].columnStart = j;
+                    }
+                    columns[columnIndex].columnEnd = j;
+                }
+            }
+
+            _.each(columns, function (c, i) {
+                if (c.rowStart !== null) {
+                    self.columns()[i].tileRowStart(c.rowStart + 1);
+                    self.columns()[i].tileWidth(c.columnEnd - c.columnStart + 1);
+                    self.columns()[i].tileColumnStart(c.columnStart + 1);
+                    self.columns()[i].tileHeight(c.rowEnd - c.rowStart + 1);
+                }
+            });
+        };
+
         self.enableTilePullDown = ko.observable(spec[self.columnKey].pull_down_tile || false);
         self.allowsEmptyColumns = options.allowsEmptyColumns;
         self.persistentCaseTileFromModule = (
