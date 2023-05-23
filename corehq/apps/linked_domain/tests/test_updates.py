b104bdec73f261653cbe48a1b02ea7faf49a201f
Matt Riley <mriley@dimagi.com>
2023-05-23 11:43:34 -0400

    Removed redundant and erroring tests

--- corehq/apps/linked_domain/tests/test_updates.py
+++ corehq/apps/linked_domain/tests/test_updates.py
@@ -1,28 +1,6 @@
 from django.test import SimpleTestCase
 from corehq.apps.users.models import UserRole
-from corehq.apps.linked_domain.updates import _get_matching_downstream_entry, _copy_role_attributes
-
-
-class UpdateUserRolesTests(SimpleTestCase):
-    def test_get_downstream_entry_when_no_match_is_found_returns_none(self):
-        upstream_entry = {'name': 'test', 'id': '7', '_id': '8'}
-        downstream_entries = []
-        result = _get_matching_downstream_entry(upstream_entry, downstream_entries)
-        self.assertIsNone(result)
-
-    def test_get_downstream_entry_matches_on_upstream_id(self):
-        # upstream_entry = UserRole(name='test', id='7', couch_id='8')
-        upstream_entry = {'name': 'test', 'id': '7', '_id': '8'}
-        downstream_entries = [UserRole(name='test2', upstream_id='8')]
-        result = _get_matching_downstream_entry(upstream_entry, downstream_entries)
-        self.assertEqual(result.name, 'test2')
-
-    def test_get_downstream_entry_does_not_match_on_name(self):
-        # upstream_entry = UserRole(name='test', couch_id='8')
-        upstream_entry = {'name': 'test', '_id': '8'}
-        downstream_entries = [UserRole(name='test')]
-        result = _get_matching_downstream_entry(upstream_entry, downstream_entries)
-        self.assertIsNone(result)
+from corehq.apps.linked_domain.updates import _copy_role_attributes
 
 
 class RoleCopyTests(SimpleTestCase):
