65aaed67e50c44c8df53e0cda8d16f04dc8944ee
Graham Herceg <gherceg@dimagi.com>
2023-05-24 11:28:40 -0700

    add doc_type field

--- corehq/apps/cleanup/models.py
+++ corehq/apps/cleanup/models.py
@@ -2,5 +2,6 @@ from django.db import models
 
 
 class DeletedCouchDoc(models.Model):
-    doc_id = models.CharField(max_length=126)  # doc ids are unique across couch
+    doc_id = models.CharField(max_length=126)
+    doc_type = models.CharField(max_length=126)
     deleted_on = models.DateTimeField(db_index=True)
