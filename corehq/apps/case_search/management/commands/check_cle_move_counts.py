1dc0ecb17e7795cf8109fdcc385779a3d47363ea
Biyeun Buczyk <biyeun@dimagi.com>
2023-06-12 19:57:13 +0200

    modify threshold

--- corehq/apps/case_search/management/commands/check_cle_move_counts.py
+++ corehq/apps/case_search/management/commands/check_cle_move_counts.py
@@ -11,7 +11,18 @@ class Command(BaseCommand):
     """Runs data integrity checks to verify domains
     """
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-thresh',
+            action='store',
+            dest='thresh',
+            type=int,
+            default=100,
+            help='Difference threshold'
+        )
+
     def handle(self, **options):
+        thresh = options.get('thresh')
         all_domain_ids = [d['id'] for d in Domain.get_all(include_docs=False)]
         date_of_move = datetime(2023, 4, 1)
         for domain_doc in iter_docs(Domain.get_db(), all_domain_ids):
@@ -22,5 +33,5 @@ class Command(BaseCommand):
             total_case_search = CaseSearchES().domain(domain_obj.name).count()
             difference = total_case_es - total_case_search
 
-            if difference >= 100:
+            if difference >= thresh:
                 self.stdout.write(f"{domain_obj.name}\t{difference}\t{total_case_es}\t{total_case_search}")
