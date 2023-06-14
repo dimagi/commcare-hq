c8b3bb31f6edde349d2b6f34f60f017ad0ec85b4
Ethan Soergel <esoergel@gmail.com>
2023-06-14 11:27:48 +0200

    Move methods to avoid merge conflicts on staging

--- corehq/apps/app_manager/feature_support.py
+++ corehq/apps/app_manager/feature_support.py
@@ -217,13 +217,13 @@ class CommCareFeatureSupportMixin(object):
         )
 
     @property
+    def supports_menu_instances(self):
+        return self._require_minimum_version('2.54')
+
+    @property
     def supports_module_assertions(self):
         # form-level assertions have been supported longer
         return (
             toggles.CUSTOM_ASSERTIONS.enabled(self.domain)
             and self._require_minimum_version('2.54')
         )
-
-    @property
-    def supports_menu_instances(self):
-        return self._require_minimum_version('2.54')
