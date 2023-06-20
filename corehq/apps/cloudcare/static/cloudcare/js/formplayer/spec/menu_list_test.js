cfde4ed8fa244cafe3a61879d81f4fb951118aae
Steph <stephherbers@gmail.com>
2023-06-20 11:55:47 -0400

    set FF default to false for exisitng tests

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/menu_list_test.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/menu_list_test.js
@@ -9,7 +9,7 @@ describe('Render a case list', function () {
             "toggles_dict",
             {
                 CHANGE_FORM_LANGUAGE: false,
-                SPLIT_SCREEN_CASE_SEARCH: true,
+                SPLIT_SCREEN_CASE_SEARCH: false,
             }
         );
         sinon.stub(Utils, 'getCurrentQueryInputs').callsFake(function () { return {}; });
