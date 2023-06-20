807cbaa7d9a89f1f4e61fc462a2e5f0cafc8d0d3
Biyeun Buczyk <biyeun@dimagi.com>
2023-06-20 19:33:04 +0200

    remove irrelevant upgrade notice for dimagi users

--- corehq/apps/reports/standard/cases/basic.py
+++ corehq/apps/reports/standard/cases/basic.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.contrib import messages
 from django.utils.translation import gettext as _
 from django.utils.translation import gettext_lazy
 
@@ -25,7 +23,6 @@ from corehq.apps.reports.standard.cases.utils import (
 )
 from corehq.apps.reports.standard.inspect import ProjectInspectionReport
 from corehq.elastic import ESError
-from corehq.toggles import CASE_LIST_EXPLORER
 
 from .data_sources import CaseDisplayES
 
@@ -169,31 +166,6 @@ class CaseListReport(CaseListMixin, ProjectInspectionReport, ReportDataSource):
                 'urlname': CaseDataView.urlname,
             },
         ]
-
-    @property
-    def can_upgrade_to_case_list_explorer(self):
-        if settings.ENTERPRISE_MODE:
-            return False
-
-        if self.request.couch_user.is_dimagi and not CASE_LIST_EXPLORER.enabled(self.domain):
-            return True
-
-        return False
-
-    @property
-    def view_response(self):
-        if self.can_upgrade_to_case_list_explorer:
-            messages.warning(
-                self.request,
-
-                'Hey Dimagi User! Have you tried out the <a href="https://'
-                'confluence.dimagi.com/display/saas/Case+List+Explorer" '
-                'target="_blank">Case List Explorer</a> yet? It might be just '
-                'what you are looking for!',
-
-                extra_tags='html',
-            )
-        return super(CaseListReport, self).view_response
 
     @classmethod
     def display_in_dropdown(cls, domain=None, project=None, user=None):
