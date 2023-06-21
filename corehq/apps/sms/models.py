929d65be833e969e9851ee721029b41a66271663
Ajeet Yadav <ayadav@dimagi.com>
2023-06-21 23:12:26 +0530

    messaging error if  domain is not enabled for fcm

--- corehq/apps/sms/models.py
+++ corehq/apps/sms/models.py
@@ -1068,6 +1068,7 @@ class MessagingEvent(models.Model, MessagingStatusMixin):
     ERROR_FCM_UNSUPPORTED_RECIPIENT = 'FCM_UNSUPPORTED_RECIPIENT'
     ERROR_FCM_NO_ACTION = "FCM_NO_ACTION"
     ERROR_FCM_NOTIFICATION_FAILURE = "FCM_NOTIFICATION_FAILURE"
+    ERROR_FCM_DOMAIN_NOT_ENABLED = 'FCM_DOMAIN_NOT_ENABLED'
 
     ERROR_MESSAGES = {
         ERROR_NO_RECIPIENT:
@@ -1127,7 +1128,8 @@ class MessagingEvent(models.Model, MessagingStatusMixin):
         ERROR_FCM_NOT_AVAILABLE: gettext_noop("FCM not available on this environment."),
         ERROR_FCM_UNSUPPORTED_RECIPIENT: gettext_noop("FCM is supported for Mobile Workers only."),
         ERROR_FCM_NO_ACTION: gettext_noop("No action selected for the FCM Data message type."),
-        ERROR_FCM_NOTIFICATION_FAILURE: gettext_noop("Failure while sending FCM notifications to the devices.")
+        ERROR_FCM_NOTIFICATION_FAILURE: gettext_noop("Failure while sending FCM notifications to the devices."),
+        ERROR_FCM_DOMAIN_NOT_ENABLED: gettext_noop("Domain is not enabled for FCM Push Notifications"),
     }
 
     domain = models.CharField(max_length=126, null=False, db_index=True)
