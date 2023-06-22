55b5b741d8bec9e7350a9755709a1e18d44e41a0
nospame <mapson.evan@gmail.com>
2023-06-22 14:36:37 -0700

    Rename/organize tests for clarity

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/split_screen_case_search_spec.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/split_screen_case_search_spec.js
@@ -10,8 +10,9 @@ describe('Split Screen Case Search', function () {
         Toggles = hqImport('hqwebapp/js/toggles'),
         Utils = hqImport('cloudcare/js/formplayer/utils/utils');
 
-    const currentUrl = new Utils.CloudcareUrl({appId: 'abc123'}),
+    const currentUrl = new Utils.CloudcareUrl({ appId: 'abc123' }),
         sandbox = sinon.sandbox.create(),
+        clearSidebar = sandbox.spy(currentUrl, 'clearSidebar'),
         stubs = {},
         REGIONS = {
             main: 'main',
@@ -45,16 +46,17 @@ describe('Split Screen Case Search', function () {
             addRegions: function () { return; },
         };
         getRegion = sandbox.spy(FormplayerFrontend.regions, 'getRegion');
-        stubs.sidebarEnabled = sandbox.stub(Toggles, 'toggleEnabled').withArgs('SPLIT_SCREEN_CASE_SEARCH');
+        stubs.splitScreenToggleEnabled = sandbox.stub(Toggles, 'toggleEnabled').withArgs('SPLIT_SCREEN_CASE_SEARCH');
     });
 
     beforeEach(function () {
         FormplayerFrontend.currentUser.displayOptions.singleAppMode = false;
-        stubs.sidebarEnabled.returns(true);
+        stubs.splitScreenToggleEnabled.returns(true);
     });
 
     afterEach(function () {
         getRegion.reset();
+        clearSidebar.reset();
         sandbox.resetHistory();
     });
 
@@ -62,72 +64,75 @@ describe('Split Screen Case Search', function () {
         sandbox.restore();
     });
 
-    it('should show sidebar and main regions when using split screen case search', function () {
-        Controller.showMenu(splitScreenCaseListResponse);
+    describe('Controller actions', function () {
+        it('should show sidebar and main regions with entities type split screen case search', function () {
+            Controller.showMenu(splitScreenCaseListResponse);
 
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
 
-        assert.isTrue(getRegion.calledWith(REGIONS.main));
-        assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main));
+            assert.isTrue(getRegion.calledWith(REGIONS.main));
+            assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main));
+        });
+
+        it('should show sidebar and main regions with query type split screen case search', function () {
+            const responseWithTypeQuery = _.extend({}, splitScreenCaseListResponse, { 'type': 'query' });
+            Controller.showMenu(responseWithTypeQuery);
+
+            assert.isTrue(getRegion.calledWith(REGIONS.main));
+            assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main));
+
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+        });
+
+        it('should explicitly set sidebarEnabled and triggerEmptyCaseList with query type split screen case search', function () {
+            const responseWithTypeQuery = _.extend({}, splitScreenCaseListResponse, { 'type': 'query' });
+            Controller.showMenu(responseWithTypeQuery);
+
+            const showMain = _.find(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main);
+            assert.isTrue(showMain.args[0].options.sidebarEnabled);
+            assert.isTrue(showMain.args[0].options.triggerEmptyCaseList);
+        });
+
+        it('should empty sidebar if in app preview', function () {
+            FormplayerFrontend.currentUser.displayOptions.singleAppMode = true;
+            Controller.showMenu(splitScreenCaseListResponse);
+
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+        });
+
+        it('should empty sidebar if response type neither entities nor query', function () {
+            const responseWithTypeEmpty = _.extend({}, splitScreenCaseListResponse, { 'type': '' });
+            Controller.showMenu(responseWithTypeEmpty);
+
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+        });
+
+        it('should empty sidebar if no queryResponse present', function () {
+            const responseWithoutQueryResponse = _.omit(splitScreenCaseListResponse, 'queryResponse');
+            Controller.showMenu(responseWithoutQueryResponse);
+
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+        });
+
+        it('should empty sidebar if feature flag disabled', function () {
+            stubs.splitScreenToggleEnabled.returns(false);
+            Controller.showMenu(splitScreenCaseListResponse);
+
+            assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
+            assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
+        });
     });
 
-    it('should show sidebar and main regions even when response type query', function () {
-        const responseWithTypeQuery = _.extend({}, splitScreenCaseListResponse, {'type': 'query'});
-        Controller.showMenu(responseWithTypeQuery);
+    describe('FormplayerFrontend actions', function () {
+        it('should clear sidebar on menu:select', function () {
+            FormplayerFrontend.trigger('menu:select', 0);
 
-        assert.isTrue(getRegion.calledWith(REGIONS.main));
-        assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main));
-
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
-    });
-
-    it('should set sidebarEnabled and triggerEmptyCaseList when response type query', function () {
-        const responseWithTypeQuery = _.extend({}, splitScreenCaseListResponse, {'type': 'query'});
-        Controller.showMenu(responseWithTypeQuery);
-
-        const showMain = _.find(stubs.show.getCalls(), call => call.thisValue.region === REGIONS.main);
-        assert.isTrue(showMain.args[0].options.sidebarEnabled);
-        assert.isTrue(showMain.args[0].options.triggerEmptyCaseList);
-    });
-
-    it('should empty sidebar if in app preview', function () {
-        FormplayerFrontend.currentUser.displayOptions.singleAppMode = true;
-        Controller.showMenu(splitScreenCaseListResponse);
-
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
-    });
-
-    it('should empty sidebar if response type neither entities nor query', function () {
-        const responseWithTypeEmpty = _.extend({}, splitScreenCaseListResponse, {'type': ''});
-        Controller.showMenu(responseWithTypeEmpty);
-
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
-    });
-
-    it('should empty sidebar if no queryResponse present', function () {
-        const responseWithoutQueryResponse = _.omit(splitScreenCaseListResponse, 'queryResponse');
-        Controller.showMenu(responseWithoutQueryResponse);
-
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
-    });
-
-    it('should empty sidebar if feature flag disabled', function () {
-        stubs.sidebarEnabled.returns(false);
-        Controller.showMenu(splitScreenCaseListResponse);
-
-        assert.isTrue(getRegion.calledWith(REGIONS.sidebar));
-        assert.isTrue(_.some(stubs.empty.getCalls(), call => call.thisValue.region === REGIONS.sidebar));
-    });
-
-    it('should clear sidebar on menu:select', function () {
-        const clearSidebar = sandbox.spy(currentUrl, 'clearSidebar');
-        FormplayerFrontend.trigger("menu:select", 0);
-
-        assert.isTrue(clearSidebar.calledOnce);
+            assert.isTrue(clearSidebar.calledOnce);
+        });
     });
 });
