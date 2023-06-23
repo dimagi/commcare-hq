ccf8d0acf2f6758ccb8fd8a630c441680ca8c455
Minha <mlee@dimagi.com>
2023-06-23 14:39:14 -0400

    Check number of returned cases for ancestor query with the right es query

--- corehq/apps/case_search/xpath_functions/ancestor_functions.py
+++ corehq/apps/case_search/xpath_functions/ancestor_functions.py
@@ -1,5 +1,5 @@
 from django.utils.translation import gettext
-from eulxml.xpath import serialize
+from eulxml.xpath import serialize, parse
 from eulxml.xpath.ast import BinaryExpression, FunctionCall, Step
 
 from corehq.apps.case_search.const import OPERATOR_MAPPING, EQ
@@ -144,11 +144,21 @@ def _get_case_ids_from_ast_filter(context, filter_node):
         es_filter = build_filter_from_ast(filter_node, context)
 
         es_query = CaseSearchES().domain(context.domain).filter(es_filter)
-        if es_query.count() > MAX_RELATED_CASES:
-            new_query = serialize(filter_node)
-            raise TooManyRelatedCasesError(
-                gettext("The related case lookup you are trying to perform would return too many cases"),
-                new_query
-            )
 
         return es_query.scroll_ids()
+
+
+def is_ancestor_query_below_limit(query, xpath):
+    node = parse(xpath)
+    REDUCED_MAX_RELATED_CASES = 5000  # for testing purposes
+    if (is_ancestor_comparison(node) or 'ancestor-exists' in xpath) \
+       and query.count() > REDUCED_MAX_RELATED_CASES:
+        try:
+            query = BinaryExpression(node.left.right, node.op, node.right)
+        except AttributeError:
+            ancestor_path_node, ancestor_case_filter_node = node.args
+            query = ancestor_case_filter_node
+        raise TooManyRelatedCasesError(
+            gettext("The related case lookup you are trying to perform would return too many cases"),
+            serialize(query)
+        )
