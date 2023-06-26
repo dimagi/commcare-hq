36e960aacdf99a3feff8aeef024539d80ee75b79
Martin Riese <mriese@dimagi.com>
2023-06-26 10:52:37 -0500

    Remove feature flag from Address Popup

--- corehq/apps/app_manager/static/app_manager/js/details/utils.js
+++ corehq/apps/app_manager/static/app_manager/js/details/utils.js
@@ -36,19 +36,15 @@ hqDefine("app_manager/js/details/utils", function () {
             value: "address",
             label: gettext('Address'),
         }, {
+            value: "address-popup",
+            label: gettext('Address Popup (WebApp only)'),
+        }, {
             value: "distance",
             label: gettext('Distance from current location'),
         }, {
             value: "markdown",
             label: gettext('Markdown'),
         }];
-
-        if (hqImport('hqwebapp/js/toggles').toggleEnabled('CASE_LIST_TILE')) {
-            formats.push({
-            value: "address-popup",
-            label: gettext('Address Popup (WebApp only)'),
-            });
-        }
 
         if (hqImport('hqwebapp/js/toggles').toggleEnabled('MM_CASE_PROPERTIES')) {
             formats.push({
