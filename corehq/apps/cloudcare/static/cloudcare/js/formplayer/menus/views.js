fe5f782209ce2f0407bdbbe2eeb023206b600d7e
Martin Riese <mriese@dimagi.com>
2023-06-26 10:59:58 -0500

    Pan to marker when selected

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
@@ -571,6 +571,8 @@ hqDefine("cloudcare/js/formplayer/menus/views", function () {
                                         // -50 Stay clear of the breadcrumbs
                                         scrollTop: $(`#${rowId}`).offset().top - 50
                                     }, 500);
+
+                                    addressMap.panTo(latLng);
                                 });
                                 latLngs.push(latLng);
                             }
