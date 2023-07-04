d7f1d6bc8f8b5deef5902d7947d00a110b34ddfe
Jennifer Schweers <orange.jenny@gmail.com>
2023-07-04 09:50:58 -0400

    Moved current user dependencies from app.js to users models

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/sessions/views.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/sessions/views.js
@@ -5,6 +5,7 @@ hqDefine("cloudcare/js/formplayer/sessions/views", [
     'moment',
     'cloudcare/js/formplayer/constants',
     'cloudcare/js/formplayer/app',
+    'cloudcare/js/formplayer/users/models',
     'cloudcare/js/formplayer/utils/utils',
     'cloudcare/js/formplayer/sessions/api',     // deleteSession
 ], function (
@@ -14,6 +15,7 @@ hqDefine("cloudcare/js/formplayer/sessions/views", [
     moment,
     constants,
     FormplayerFrontend,
+    UsersModels,
     utils
 ) {
     var SessionView = Marionette.View.extend({
@@ -134,8 +136,8 @@ hqDefine("cloudcare/js/formplayer/sessions/views", [
             }
         },
         templateContext: function () {
-            var user = FormplayerFrontend.getChannel().request('currentUser');
-            var paginationConfig = utils.paginateOptions(
+            var user = UsersModels.getCurrentUser(),
+                paginationConfig = utils.paginateOptions(
                 this.options.pageNumber,
                 this.options.totalPages,
                 this.collection.totalSessions
