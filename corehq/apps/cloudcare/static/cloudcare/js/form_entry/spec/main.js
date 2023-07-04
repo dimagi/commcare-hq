597888e0634207f495ea3a81d9e1c935e91f966a
Jennifer Schweers <orange.jenny@gmail.com>
2023-07-04 10:15:40 -0400

    Restored form_ui_spec

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/main.js
@@ -5,6 +5,7 @@ hqDefine("cloudcare/js/form_entry/spec/main", [
     hqMocha
 ) {
     hqRequire([
+        "cloudcare/js/form_entry/spec/form_ui_spec",
         "cloudcare/js/form_entry/spec/integration_spec",
         "cloudcare/js/form_entry/spec/utils_spec",
         "cloudcare/js/form_entry/spec/web_form_session_spec",
