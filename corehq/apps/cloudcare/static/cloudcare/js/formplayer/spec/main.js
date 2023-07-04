4661f43a6eb9ad4f39ca992b1e4ffee9eca9d75f
Jennifer Schweers <orange.jenny@gmail.com>
2023-07-04 10:05:10 -0400

    Added formplayer integration_spec back

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/main.js
@@ -6,6 +6,7 @@ hqDefine("cloudcare/js/formplayer/spec/main", [
 ) {
     hqRequire([
         "cloudcare/js/formplayer/spec/hq_events_spec",
+        "cloudcare/js/formplayer/spec/integration_spec",
         "cloudcare/js/formplayer/spec/menu_list_spec",
         "cloudcare/js/formplayer/spec/session_middleware_spec",
         "cloudcare/js/formplayer/spec/user_spec",
