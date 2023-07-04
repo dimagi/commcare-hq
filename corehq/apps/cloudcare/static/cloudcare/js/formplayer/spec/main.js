87c7b7fb93c3e7ba5ee03e42b38a0a4c72c84301
Jennifer Schweers <orange.jenny@gmail.com>
2023-07-04 10:09:32 -0400

    Added split_screen_case_search_spec back

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/main.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/main.js
@@ -10,6 +10,7 @@ hqDefine("cloudcare/js/formplayer/spec/main", [
         "cloudcare/js/formplayer/spec/menu_list_spec",
         "cloudcare/js/formplayer/spec/query_spec",
         "cloudcare/js/formplayer/spec/session_middleware_spec",
+        "cloudcare/js/formplayer/spec/split_screen_case_search_spec",
         "cloudcare/js/formplayer/spec/user_spec",
         "cloudcare/js/formplayer/spec/utils_spec",
         "cloudcare/js/spec/utils_spec",
