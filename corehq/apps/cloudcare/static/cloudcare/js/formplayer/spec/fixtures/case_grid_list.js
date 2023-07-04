5360ef87f15f876efba993440f6a040e0f82d2ee
Jennifer Schweers <orange.jenny@gmail.com>
2023-07-04 09:57:13 -0400

    Added menu_list_test back

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures/case_grid_list.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/fixtures/case_grid_list.js
@@ -1,7 +1,9 @@
 'use strict';
-hqDefine("cloudcare/js/formplayer/spec/fixtures/case_grid_list", function () {
-    let FakeFormplayer = hqImport("cloudcare/js/formplayer/spec/fake_formplayer");
-
+hqDefine("cloudcare/js/formplayer/spec/fixtures/case_grid_list", [
+    "cloudcare/js/formplayer/spec/fake_formplayer",
+], function (
+    FakeFormplayer
+) {
     return FakeFormplayer.makeEntitiesResponse({
         "title": "New Adherence Data",
         "breadcrumbs": [
