4c6c5dc3abc19f910d559212b3c50d95a59a3123
Akash Jain <akashjain@work.com>
2023-07-05 01:27:51 +0530

    Get langauge code mapping from the value set in context

--- corehq/apps/cloudcare/views.py
+++ corehq/apps/cloudcare/views.py
@@ -87,6 +87,8 @@ from corehq.form_processor.models import CommCareCase
 from corehq.form_processor.models import XFormInstance
 from xml2json.lib import xml2json
 
+from langcodes import get_name
+
 
 @require_cloudcare_access
 def default(request, domain):
@@ -210,6 +212,19 @@ class FormplayerMain(View):
             "integrations": integration_contexts(domain),
             "has_geocoder_privs": has_geocoder_privs(domain),
         }
+
+        langs_by_code = {}
+
+        def get_lang_name(lang_code):
+            if not langs_by_code.get(lang_code):
+                langs_by_code[lang_code] = get_name(lang_code)
+            return langs_by_code[lang_code]
+
+        for app in apps:
+            lang_codes = app.get("langs", [])
+            lang_code_name_mapping = {code: get_lang_name(code) for code in lang_codes}
+            app["lang_code_name_mapping"] = lang_code_name_mapping
+
         return set_cookie(
             render(request, "cloudcare/formplayer_home.html", context)
         )
