79a3f57d080cbb4a55ec761b2f3032d3bb11791d
Norman Hooper <nhooper@dimagi.com>
2023-07-06 10:09:22 +0100

    Revert "Test empty case type in Case API v0.6"
    This reverts commit c35fd779cf8cd9a6dd2ede04e61ce4645ddbac8b.

--- corehq/apps/hqcase/tests/test_case_api_updates.py
+++ corehq/apps/hqcase/tests/test_case_api_updates.py
@@ -1,28 +1,6 @@
-import json
-from dataclasses import dataclass
-from typing import Literal
+from django.test import SimpleTestCase
 
-from django.test import SimpleTestCase, TestCase
-
-from ..api.updates import JsonCaseCreation
-from ..views import _handle_case_update
-
-DOMAIN = 'test-domain'
-
-
-@dataclass
-class FakeUser:
-    user_id: str
-    username: str
-
-
-@dataclass
-class FakeRequest:
-    method: Literal['GET', 'POST', 'PUT']
-    body: bytes
-    META: dict[str, str]
-    domain: str
-    couch_user: FakeUser
+from corehq.apps.hqcase.api.updates import JsonCaseCreation
 
 
 class GetCaseBlockTests(SimpleTestCase):
@@ -70,29 +48,3 @@ class GetCaseBlockTests(SimpleTestCase):
         self.assertIn('<owner_id>abc123</owner_id>', case_block)
         self.assertIn('<external_id>Ewan McGregor</external_id>', case_block)
         self.assertIn('<age>26</age>', case_block)
-
-
-class TestHandleCaseUpdate(TestCase):
-
-    def test_post_empty_type(self):
-        user = FakeUser(
-            user_id='abc123',
-            username=f'irvine@{DOMAIN}.commcarehq.org',
-        )
-        request = FakeRequest(
-            method='POST',
-            body=json.dumps({
-                'case_name': 'Mark Renton',
-                'case_type': '',  # Field is required, but value can be empty
-                'owner_id': 'abc123'
-            }).encode('utf-8'),
-            META={'HTTP_USER_AGENT': __file__},
-            domain=DOMAIN,
-            couch_user=user,
-        )
-        response = _handle_case_update(request, is_creation=True)
-
-        response_json = json.loads(response.content)
-        self.assertNotIn('error', response_json)
-        self.assertEqual(response_json['case']['case_name'], 'Mark Renton')
-        self.assertEqual(response_json['case']['case_type'], '')
