280da7d7284a0995ab2a326158f613ab0c5c12e4
Daniel Miller <dmiller@dimagi.com>
2023-07-20 17:23:43 -0400

    Implement populate_repeatrecords migration

--- corehq/motech/repeaters/management/commands/populate_repeatrecords.py
+++ corehq/motech/repeaters/management/commands/populate_repeatrecords.py
@@ -1,6 +1,9 @@
 from dimagi.utils.parsing import json_format_datetime
 
 from corehq.apps.cleanup.management.commands.populate_sql_model_from_couch_model import PopulateSQLCommand
+from corehq.util.couch_helpers import paginate_view
+
+from ...models import Repeater
 
 
 class Command(PopulateSQLCommand):
@@ -53,6 +56,53 @@ class Command(PopulateSQLCommand):
             json_format_datetime(sql.next_check) if sql.next_check else sql.next_check,
         ))
         return diffs
+
+    def get_ids_to_ignore(self, docs):
+        """Get ids of records that reference missing repeaters
+
+        May include repeaters that have been created since the migration
+        started, whose records are already migrated. Also ignore records
+        associated with deleted repeaters.
+
+        NOTE: there is a race condition between this repeaters existence
+        check and saving new records. A repeater could be deleted
+        between when this function is called and when the new records
+        are saved, which would cause the migration to fail with
+        IntegrityError on "repeater_id" column value. Since that is a
+        rare condition, it is not handled. It should be sufficient to
+        rerun the migration to recover from that error.
+        """
+        existing_ids = {id_.hex for id_ in Repeater.objects.filter(
+            id__in=list({d["repeater_id"] for d in docs})
+        ).values_list("id", flat=True)}
+        return {d["_id"] for d in docs if d["repeater_id"] not in existing_ids}
+
+    def _get_couch_doc_count_for_type(self):
+        from ...models import RepeatRecord
+        result = RepeatRecord.get_db().view(
+            'repeaters/repeat_records',
+            include_docs=False,
+            reduce=True,
+            descending=True,
+        ).one()
+        if not result:
+            return 0
+        # repeaters/repeat_records's map emits twice per doc, so its count is doubled
+        # repeaters/repeat_records_by_payload_id has no reduce, so cannot be used
+        assert result['value'] % 2 == 0, result['value']
+        return int(result['value'] / 2)
+
+    def _get_all_couch_docs_for_model(self, chunk_size):
+        from ...models import RepeatRecord
+        # repeaters/repeat_records_by_payload_id's map emits once per document
+        for result in paginate_view(
+            RepeatRecord.get_db(),
+            'repeaters/repeat_records_by_payload_id',
+            chunk_size=chunk_size,
+            include_docs=True,
+            reduce=False,
+        ):
+            yield result['doc']
 
 
 def get_state(doc):
--- corehq/motech/repeaters/tests/test_couchsqlmigration.py
+++ corehq/motech/repeaters/tests/test_couchsqlmigration.py
@@ -1,7 +1,15 @@
+from contextlib import contextmanager
 from datetime import datetime, timedelta
+from pathlib import Path
+from unittest.mock import patch
 from uuid import UUID, uuid4
 
+from django.db import connection, transaction
+from django.core.management import call_command
 from django.test import TestCase
+from django.utils.functional import cached_property
+
+from testil import tempdir
 
 from dimagi.utils.parsing import json_format_datetime
 
@@ -12,6 +20,7 @@ from ..models import ConnectionSettings, RepeatRecord, SQLRepeatRecord
 
 REPEATER_ID_1 = "5c739aaa0cb44a24a71933616258f3b6"
 REPEATER_ID_2 = "64b6bf1758ed4f2a8944d6f34c2811c2"
+REPEATER_ID_3 = "123b7a7008b447a4a0de61f6077a0353"
 
 
 class BaseRepeatRecordCouchToSQLTest(TestCase):
@@ -171,6 +180,76 @@ class TestRepeatRecordCouchToSQLMigration(BaseRepeatRecordCouchToSQLTest):
         self.assertEqual(obj.registered_at, hour_hence)
         self.assertEqual(obj.next_check, hour_hence)
 
+    def test_migration(self):
+        @property
+        def dont_lookup_repeater(self):
+            # fail if inefficient repeater lookup is attempted
+            raise Exception("this should not happen")
+
+        doc, obj = self.create_repeat_record(unwrap_doc=False)
+        doc.save(sync_to_sql=False)
+        with patch.object(type(doc), "repeater", dont_lookup_repeater):
+            call_command('populate_repeatrecords')
+        self.assertEqual(
+            self.diff(doc.to_json(), SQLRepeatRecord.objects.get(couch_id=doc._id)),
+            [],
+        )
+
+    def test_migration_fixup_diffs(self):
+        # Additional call should apply any updates
+        doc, obj = self.create_repeat_record(unwrap_doc=False)
+        doc.save()
+        doc.payload_id = payload_id = uuid4().hex
+        doc.repeater_id = REPEATER_ID_2
+        doc.failure_reason = "something happened"
+        doc.registered_on = datetime.utcnow() + timedelta(hours=1)
+        doc.save(sync_to_sql=False)
+
+        with templog() as log:
+            call_command('populate_repeatrecords', log_path=log.path)
+            self.assertIn(f'Doc "{doc._id}" has differences:\n', log.content)
+            self.assertIn(f"payload_id: couch value {payload_id!r} != sql value {obj.payload_id!r}\n", log.content)
+            self.assertIn(
+                f"repeater_id: couch value '{REPEATER_ID_2}' != sql value '{REPEATER_ID_1}'\n", log.content)
+            self.assertIn("state: couch value 'FAIL' != sql value 'PENDING'\n", log.content)
+            self.assertIn("registered_at: couch value '", log.content)
+
+            call_command('populate_repeatrecords', fixup_diffs=log.path)
+            self.assertEqual(
+                self.diff(doc.to_json(), SQLRepeatRecord.objects.get(couch_id=doc._id)),
+                [],
+            )
+
+    def test_migration_with_deleted_repeater(self):
+        doc, obj = self.create_repeat_record(unwrap_doc=False)
+        repeater1_id = self.repeater1.id
+        self.addCleanup(setattr, self.repeater1, "id", repeater1_id)
+        self.repeater1.delete()
+        doc_id = self.db.save_doc(doc.to_json())["id"]
+        assert RepeatRecord.get(doc_id) is not None, "missing record"
+        with templog() as log, patch.object(transaction, "atomic", atomic_check):
+            call_command('populate_repeatrecords', log_path=log.path)
+            self.assertIn(f"Ignored model for RepeatRecord with id {doc_id}\n", log.content)
+
+    def test_migration_with_repeater_added_after_start(self):
+        doc, obj = self.create_repeat_record(unwrap_doc=False)
+        repeater3 = models.FormRepeater(
+            id=UUID(REPEATER_ID_3),
+            domain="test",
+            connection_settings_id=self.repeater1.connection_settings_id,
+            include_app_id_param=False,
+        )
+        repeater3.save()
+        doc.repeater_id = repeater3.repeater_id
+        doc.save()
+        with (
+            templog() as log,
+            patch.object(Command, "get_ids_to_ignore", lambda *a: {}),
+            patch.object(transaction, "atomic", atomic_check),
+        ):
+            call_command('populate_repeatrecords', log_path=log.path)
+            assert not log.content, log.content  # doc should have been implicitly ignored (already migrated)
+
     def diff(self, doc, obj):
         return do_diff(Command, doc, obj)
 
@@ -178,3 +257,29 @@ class TestRepeatRecordCouchToSQLMigration(BaseRepeatRecordCouchToSQLTest):
 def do_diff(Command, doc, obj):
     result = Command.diff_couch_and_sql(doc, obj)
     return [x for x in result if x is not None]
+
+
+@contextmanager
+def atomic_check(using=None, savepoint='ignored'):
+    with _atomic(using=using):
+        yield
+        connection.check_constraints()
+
+
+_atomic = transaction.atomic
+
+
+@contextmanager
+def templog():
+    with tempdir() as tmp:
+        yield Log(tmp)
+
+
+class Log:
+    def __init__(self, tmp):
+        self.path = Path(tmp) / "log.txt"
+
+    @cached_property
+    def content(self):
+        with self.path.open() as lines:
+            return "".join(lines)
