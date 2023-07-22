89c6b6cc3f44843576c917917a3243bdda5c2eca
Jonathan Tang <Jonahtang738@gmail.com>
2023-07-22 09:39:56 -0700

    bug: timezone converts only datetime special properties
    Special Properties are stored as UTC so user inputs in local timezones
    need to be converted. However, case properties such "dob" are stored as just
    standalone dates which do not need to be converted

--- corehq/apps/case_search/xpath_functions/comparison.py
+++ corehq/apps/case_search/xpath_functions/comparison.py
@@ -7,7 +7,7 @@ from eulxml.xpath.ast import Step
 from corehq.apps.case_search.dsl_utils import unwrap_value
 from corehq.apps.case_search.exceptions import CaseFilterError, XPathFunctionException
 from corehq.apps.case_search.xpath_functions.value_functions import value_to_date
-from corehq.apps.case_search.const import RANGE_OP_MAPPING, EQ, NEQ
+from corehq.apps.case_search.const import RANGE_OP_MAPPING, EQ, NEQ, SPECIAL_CASE_PROPERTIES
 from corehq.apps.es import filters
 from corehq.apps.es.case_search import case_property_query, case_property_range_query
 from corehq.util.timezones.utils import get_timezone_for_domain
@@ -25,14 +25,16 @@ def property_comparison_query(context, case_property_name_raw, op, value_raw, no
     value = unwrap_value(value_raw, context)
     # adjust the user's input date based on project timezones
     is_user_input = False
-    try:
-        # this might be inconsistent in daylight savings situations
-        value = adjust_input_date_by_timezone(value_to_date(node, value),
-                                              get_timezone_for_domain(context.domain), op)
-        is_user_input = True
-    except (XPathFunctionException, AssertionError):
-        # AssertionError is caused by tests that use domains without a valid timezone (in get_timezeone_for_domain)
-        pass
+    if case_property_name in SPECIAL_CASE_PROPERTIES:
+        try:
+            # this might be inconsistent in daylight savings situations
+            value = adjust_input_date_by_timezone(value_to_date(node, value),
+                                                get_timezone_for_domain(context.domain), op)
+            is_user_input = True
+        except (XPathFunctionException, AssertionError):
+            # AssertionError is caused by tests that use domains without
+            # a valid timezone (in get_timezeone_for_domain)
+            pass
     if op in [EQ, NEQ]:
         query = case_property_query(case_property_name, value, fuzzy=context.fuzzy)
         if op == NEQ:
