81e29e894b54d5baf52392c22bbf1c32705293c8
Jing Cheng <jcheng@dimagi.com>
2023-07-24 17:31:00 -0400

    Remove unrelated code

--- corehq/apps/accounting/migrations/0077_add_web_user_feature.py
+++ corehq/apps/accounting/migrations/0077_add_web_user_feature.py
@@ -1,23 +1,7 @@
 from django.db import migrations
-from django.core.management import call_command
-
-from corehq.privileges import LOADTEST_USERS
-from corehq.util.django_migrations import skip_on_fresh_install
 
 from corehq.apps.accounting.bootstrap.utils import _ensure_feature_rates
 from corehq.apps.accounting.bootstrap.config.web_user_feature_rate import BOOTSTRAP_CONFIG
-
-
-@skip_on_fresh_install
-def _grandfather_basic_privs(apps, schema_editor):
-    call_command('cchq_prbac_bootstrap')
-    call_command(
-        'cchq_prbac_grandfather_privs',
-        LOADTEST_USERS,
-        skip_edition='Paused,Community',
-        noinput=True,
-    )
-
 
 def _add_web_user_feature(apps, schema_editor):
     Feature = apps.get_model('accounting', 'Feature')
@@ -34,9 +18,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(
-            _grandfather_basic_privs,
-            reverse_code=migrations.RunPython.noop,
-        ),
         migrations.RunPython(_add_web_user_feature, reverse_code=migrations.RunPython.noop),
     ]
