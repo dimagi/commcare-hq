be871d34ceaa6e6618fb2e044462fb29ad93011e
Biyeun Buczyk <biyeun@dimagi.com>
2023-07-25 21:26:21 +0200

    only look at active subs

--- corehq/apps/accounting/management/commands/domains_using_cle.py
+++ corehq/apps/accounting/management/commands/domains_using_cle.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "return privilege usage statistics"
 
     def handle(self, **options):
-        domains_with_priv = set(Subscription.visible_objects.exclude(
+        domains_with_priv = set(Subscription.visible_objects.filter(is_active=True).exclude(
             plan_version__plan__edition__in='Paused,Community,Standard'
         ).values_list('subscriber__domain', flat=True).distinct())
         domains_with_ff = set(toggles.CASE_LIST_EXPLORER.get_enabled_domains())
@@ -17,12 +17,12 @@ class Command(BaseCommand):
         domains_only_ff = domains_with_ff.difference(domains_with_priv)
         new_domains = domains_with_priv.difference(domains_with_ff)
 
-        self.stdout.write("\n\n\nPROJECTS UNCHANGED\n")
+        self.stdout.write("\n\n\nPROJECTS UNCHANGED -------------------------------------------------------\n")
         self.stdout.write("\n".join(domains_always_cle))
 
-        self.stdout.write("\n\n\n\nPROJECTS ONLY FF\n")
+        self.stdout.write("\n\n\n\nPROJECTS ONLY FF -------------------------------------------------------\n")
         self.stdout.write("\n".join(domains_only_ff))
 
-        self.stdout.write("\n\n\n\nNEW PROJECTS\n")
+        self.stdout.write("\n\n\n\nNEW PROJECTS -------------------------------------------------------\n")
         self.stdout.write("\n".join(new_domains))
         self.stdout.write("\n\n")
