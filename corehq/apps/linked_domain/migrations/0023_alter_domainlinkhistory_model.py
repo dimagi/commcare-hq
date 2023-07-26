74b40b53ad1463d7c462ae20aec00316fe999d05
Biyeun Buczyk <biyeun@dimagi.com>
2023-07-26 21:18:56 +0200

    remove comma

--- corehq/apps/linked_domain/migrations/0023_alter_domainlinkhistory_model.py
+++ corehq/apps/linked_domain/migrations/0023_alter_domainlinkhistory_model.py
@@ -15,4 +15,4 @@ class Migration(migrations.Migration):
             name='model',
             field=models.CharField(choices=[('app', 'Application'), ('fixture', 'Lookup Table'), ('report', 'Report'), ('keyword', 'Keyword'), ('ucr_expression', 'Data Expressions and Filters'), ('auto_update_rule', 'Automatic Update Rule'), ('custom_user_data', 'Custom User Data Fields'), ('custom_location_data', 'Custom Location Data Fields'), ('roles', 'User Roles'), ('previews', 'Feature Previews'), ('auto_update_rules', 'Automatic Update Rules'), ('case_search_data', 'Case Search Settings'), ('data_dictionary', 'Data Dictionary'), ('dialer_settings', 'Dialer Settings'), ('otp_settings', 'OTP Pass-through Settings'), ('hmac_callout_settings', 'Signed Callout'), ('tableau_server_and_visualizations', 'Tableau Server and Visualizations'), ('custom_product_data', 'Custom Product Data Fields'), ('toggles', 'Feature Flags')], max_length=128),
         ),
-    ],
+    ]
