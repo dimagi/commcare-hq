7084aa54e5b185617cf4276d1626d97b69d2e956
Ajeet Yadav <ayadav@dimagi.com>
2023-07-30 19:03:48 +0530

    models for HIU consents

--- custom/abdm/const.py
+++ custom/abdm/const.py
@@ -3,4 +3,30 @@ ABHA_IN_USE_ERROR_CODE = 1001
 ERROR_MESSAGES = {
     ABHA_IN_USE_ERROR_CODE: 'Provided ABHA is already linked to another beneficiary!'
 }
+
 SESSIONS_PATH = '/v0.5/sessions'
+GATEWAY_CALLBACK_URL_PREFIX = 'api/gateway/v0.5'
+
+CONSENT_PURPOSES = [('CAREMGT', 'Care Management'), ('BTG', 'Break the Glass'), ('PUBHLTH', 'Public Health'),
+                    ('HPAYMT', 'Healthcare Payment'), ('DSRCH', 'Disease Specific Healthcare Research'),
+                    ('PATRQT', 'Self Requested')]
+CONSENT_PURPOSES_REF_URI = 'http://terminology.hl7.org/ValueSet/v3-PurposeOfUse'
+
+HI_TYPES = [('OPConsultation', 'OP Consultation'), ('Prescription', 'Prescription'),
+            ('DischargeSummary', 'Discharge Summary'), ('DiagnosticReport', 'Diagnostic Report'),
+            ('ImmunizationRecord', 'Immunization Record'), ('HealthDocumentRecord', 'Record artifact'),
+            ('WellnessRecord', 'Wellness Record')]
+
+DATA_ACCESS_MODES = [(c, c) for c in ['VIEW', 'STORE', 'QUERY', 'STREAM']]
+TIME_UNITS = [(c, c) for c in ['HOUR', 'WEEK', 'DAY', 'MONTH', 'YEAR']]
+
+STATUS_PENDING = 'PENDING'
+STATUS_REQUESTED = 'REQUESTED'
+STATUS_GRANTED = 'GRANTED'
+STATUS_DENIED = 'DENIED'
+STATUS_ERROR = 'ERROR'
+STATUS_REVOKED = 'REVOKED'
+STATUS_EXPIRED = 'EXPIRED'
+
+GATEWAY_CONSENT_STATUS_CHOICES = [(c, c) for c in [STATUS_GRANTED, STATUS_DENIED, STATUS_REVOKED,
+                                                   STATUS_EXPIRED]]
---
+++ custom/abdm/hiu/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+
+from custom.abdm.const import (
+    STATUS_DENIED,
+    STATUS_ERROR,
+    STATUS_EXPIRED,
+    STATUS_GRANTED,
+    STATUS_PENDING,
+    STATUS_REQUESTED,
+    STATUS_REVOKED,
+)
+from custom.abdm.models import ABDMUser
+
+
+class HIUConsentRequest(models.Model):
+
+    STATUS = (
+        (STATUS_PENDING, 'Pending request from Gateway'),
+        (STATUS_REQUESTED, 'Requested'),
+        (STATUS_GRANTED, 'Granted'),
+        (STATUS_DENIED, 'Denied'),
+        (STATUS_REVOKED, 'Revoked'),
+        (STATUS_EXPIRED, 'Expired'),
+        (STATUS_ERROR, 'Error occurred'),
+    )
+
+    user = models.ForeignKey(ABDMUser, on_delete=models.PROTECT, related_name='consent_requests')
+    gateway_request_id = models.UUIDField(null=True, unique=True)
+    consent_request_id = models.UUIDField(null=True, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS, default=STATUS_PENDING, max_length=40)
+    details = models.JSONField(null=True)
+    error = models.JSONField(null=True)
+    # Below attributes correspond to ones that are accepted by Patient when consent is granted.
+    health_info_from_date = models.DateTimeField()
+    health_info_to_date = models.DateTimeField()
+    health_info_types = models.JSONField(default=list)
+    expiry_date = models.DateTimeField()
+
+    def update_status(self, status):
+        self.status = status
+        self.save()
+
+    def update_user_amendable_details(self, consent_permission, health_info_types):
+        self.health_info_from_date = consent_permission['dateRange']['from']
+        self.health_info_to_date = consent_permission['dateRange']['to']
+        self.expiry_date = consent_permission['dataEraseAt']
+        self.health_info_types = health_info_types
+        self.save()
+
+
+class HIUConsentArtefact(models.Model):
+    STATUS = (
+        (STATUS_GRANTED, 'Granted'),
+        (STATUS_REVOKED, 'Revoked'),
+        (STATUS_EXPIRED, 'Expired'),
+        (STATUS_ERROR, 'Error occurred'),
+    )
+
+    consent_request = models.ForeignKey(HIUConsentRequest, to_field='consent_request_id', on_delete=models.PROTECT,
+                                        related_name='artefacts')
+    gateway_request_id = models.UUIDField(null=True, unique=True)
+    artefact_id = models.UUIDField(unique=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    status = models.CharField(choices=STATUS, max_length=40)
+    details = models.JSONField(null=True)
+    error = models.JSONField(null=True)
+
+    def update_status(self, status):
+        self.status = status
+        self.save()
