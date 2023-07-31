c248ee276298550b7f4f0e98f7536b9e00dfdafb
AddisonDunn <dunnaddisonw@gmail.com>
2023-07-31 11:39:52 -0400

    move filtering to 'remote' method so that it applies to bulk upload

--- corehq/apps/reports/util.py
+++ corehq/apps/reports/util.py
@@ -555,11 +555,11 @@ def update_tableau_user(domain, username, role=None, groups=[], session=None):
     if role:
         user.role = role
     user.save()
-    groups = filter(lambda group: group.name in allowed_tableau_groups_for_domain(domain), groups)
-    _update_user_remote(session, user, groups)
+    _update_user_remote(session, user, domain, groups)
 
 
-def _update_user_remote(session, user, groups=[]):
+def _update_user_remote(session, user, domain, groups=[]):
+    groups = filter(lambda group: group.name in get_allowed_tableau_groups_for_domain(domain), groups)
     new_id = session.update_user(user.tableau_user_id, role=user.role, username=tableau_username(user.username))
     user.tableau_user_id = new_id
     user.save()
@@ -635,6 +635,7 @@ def sync_tableau_users_on_domain(domain):
                 _update_user_remote(
                     session,
                     local_user,
+                    domain,
                     groups=_group_json_to_tuples(session.get_groups_for_user_id(local_user.tableau_user_id))
                 )
 
