43ef81b653a7a111b89e2b7db3c32154fc7cdf0b
Biyeun Buczyk <biyeun@dimagi.com>
2023-08-01 19:56:49 +0200

    bootstrap 3 to 5 auto-migration for hqwebapp/iframe_sso_login_pending.html

--- corehq/apps/hqwebapp/views.py
+++ corehq/apps/hqwebapp/views.py
@@ -464,7 +464,7 @@ def iframe_domain_login(req, domain):
 @xframe_options_sameorigin
 @location_safe
 def iframe_sso_login_pending(request):
-    return TemplateView.as_view(template_name='hqwebapp/iframe_sso_login_pending.html')(request)
+    return TemplateView.as_view(template_name='hqwebapp/bootstrap3/iframe_sso_login_pending.html')(request)
 
 
 class HQLoginView(LoginView):
