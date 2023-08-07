97ac95fd1da073e7c694a980d0e7ae2434385377
nospame <mapson.evan@gmail.com>
2023-08-07 15:36:46 -0700

    Use temporary branch to save git state for js coverage

--- scripts/coverage-js.sh
+++ scripts/coverage-js.sh
@@ -1,7 +1,8 @@
 # Run this with local development server active to report Javascript test coverage
 
-# Save the local file state
-git commit -a -m "temp coverage-js commit"
+# Save the local file state in a temporary branch
+git checkout -b "temp-coverage-js"
+git commit --allow-empty --no-verify -a -m "temp commit"
 
 # Instrument HQ Javascript in place
 npx nyc instrument -x "**/sentry" -x "**/spec" -x "**/mocha" -x "**/lib" \
@@ -15,8 +16,11 @@ npx nyc merge "./coverage-js" "./coverage-js/merged/coverage.json"
 
 # Reset to previous git state before creating report
 # This is done before the report is built to get accurate line-by-line coverage
-git reset --hard && git reset HEAD~1
+git reset --hard
 
 # Build the report based on merged coverage data
 npx nyc report --temp-dir "./coverage-js/merged" --report-dir "./coverage-js" \
     --reporter "html" --reporter "text-summary"
+
+# Unstage current changes, switch to prior working branch, delete temporary branch
+git reset HEAD~1 && git switch - && git branch -D "temp-coverage-js"
