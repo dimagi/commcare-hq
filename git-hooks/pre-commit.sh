26785e0fdc99e9c49f0f8c1ac23ef16b4b759ce6
Matt Riley <mriley@dimagi.com>
2023-08-10 17:27:07 -0400

    Fixed typo in commit hook

--- git-hooks/pre-commit.sh
+++ git-hooks/pre-commit.sh
@@ -50,7 +50,7 @@ fi
 status=0
 git diff-index --check --cached $against --
 if [ $? != 0 ]; then status=1; fi
-pyfiles=$(git diff --staged --name-only | grep \*.py$)
+pyfiles=$(git diff --staged --name-only | grep "\.py$")
 if [ -n "$pyfiles" ]; then
     flake8 --show-source --config=.flake8 "$pyfiles"
 	if [ $? != 0 ]; then status=1; fi
