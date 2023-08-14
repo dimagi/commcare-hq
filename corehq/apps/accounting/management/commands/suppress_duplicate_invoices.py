0f2798d8262c2a636efc986d409e400eaea08289
Jing Cheng <jcheng@dimagi.com>
2023-08-14 16:45:26 -0400

    Add dry run version

--- corehq/apps/accounting/management/commands/suppress_duplicate_invoices.py
+++ corehq/apps/accounting/management/commands/suppress_duplicate_invoices.py
@@ -13,11 +13,16 @@ class Command(BaseCommand):
         parser.add_argument('month', type=int, help='The month of the statement period.')
         parser.add_argument('note', type=str,
                             help='A note to be added when crediting back the amount')
+        parser.add_argument('--dry-run', action='store_true', default=False)
+
 
     def handle(self, *args, **kwargs):
         year = kwargs['year']
         month = kwargs['month']
         note = kwargs['note']
+        dry_run = kwargs['dry_run']
+        dry_run_tag = '[DRY_RUN]' if dry_run else ''
+
 
         start_date, end_date = get_first_last_days(year, month)
 
@@ -37,23 +42,25 @@ class Command(BaseCommand):
             for invoice in related_invoices[:-1]:
                 custom_note = f"{note}. Referenced to invoice {first_created_invoice_id}."
                 if invoice.balance != invoice.subtotal:
-                    #credit them back
-                    self.revert_invoice_payment(invoice, custom_note)
-                #suppress invoice
-                invoice.is_hidden_to_ops = True
-                print(f"Suppressing invoice {invoice.id} for domain {invoice.subscription.subscriber.domain} ",
-                      end='')
-                invoice.save()
+                    # credit them back
+                    self.revert_invoice_payment(invoice, custom_note, dry_run)
+                # suppress invoice
+                print(f'{dry_run_tag}Suppressing invoice {invoice.id} for domain'
+                      f'{invoice.subscription.subscriber.domain} ', end='')
+                if not dry_run:
+                    invoice.is_hidden_to_ops = True
+                    invoice.save()
                 suppressed_count += 1
                 print("✓")
 
-        self.stdout.write(self.style.SUCCESS(
-            'Successfully suppressed {} duplicate invoices for Statement Period: {}-{}'
-            .format(suppressed_count, year, month)))
+        self.stdout.write(self.style.SUCCESS(f'{dry_run_tag}Successfully suppressed {suppressed_count} '
+                                             f'duplicate invoices for Statement Period: {year}-{month}'))
 
-    def revert_invoice_payment(self, invoice, note):
+    def revert_invoice_payment(self, invoice, note, dry_run=False):
+        dry_run_tag = '[DRY_RUN]' if dry_run else ''
 
-        print(f'Reverting payment for Invoice Id: {invoice.id}, Domain: {invoice.subscription.subscriber.domain}')
+        print(f'{dry_run_tag}Reverting payment for Invoice Id: {invoice.id}, '
+              f'Domain: {invoice.subscription.subscriber.domain}')
         # Total Credit
         payment_by_credit = -invoice.applied_credit
 
@@ -61,9 +68,10 @@ class Command(BaseCommand):
             invoice.lineitem_set.get_products().all()
         )
         if plan_deduction:
-            print(f'Adding plan credit: {plan_deduction}')
-            CreditLine.add_credit(amount=plan_deduction, subscription=invoice.subscription,
-                                  is_product=True, note=note)
+            print(f'{dry_run_tag}Adding plan credit: {plan_deduction}')
+            if not dry_run:
+                CreditLine.add_credit(amount=plan_deduction, subscription=invoice.subscription,
+                                    is_product=True, note=note)
             payment_by_credit -= plan_deduction
 
         for feature in FeatureType.CHOICES:
@@ -71,18 +79,21 @@ class Command(BaseCommand):
                 invoice.lineitem_set.get_feature_by_type(feature).all()
             )
             if feature_deduction:
-                print(f"Adding {feature[0]} credit: {feature_deduction}")
-                CreditLine.add_credit(amount=feature_deduction, subscription=invoice.subscription,
-                                    feature_type=feature[0], note=note)
+                print(f"{dry_run_tag}Adding {feature[0]} credit: {feature_deduction}")
+                if not dry_run:
+                    CreditLine.add_credit(amount=feature_deduction, subscription=invoice.subscription,
+                                        feature_type=feature[0], note=note)
                 payment_by_credit -= feature_deduction
 
         # After deducting plan credit and feature credit, the remained credit should be type Any
         if payment_by_credit:
-            print(f"Adding remaining credit (type Any): {payment_by_credit}")
-            CreditLine.add_credit(amount=payment_by_credit, subscription=invoice.subscription, note=note)
+            print(f"{dry_run_tag}Adding remaining credit (type Any): {payment_by_credit}")
+            if not dry_run:
+                CreditLine.add_credit(amount=payment_by_credit, subscription=invoice.subscription, note=note)
 
         # Calculate the amount paid not by credit
         payment_by_other = invoice.subtotal + invoice.applied_credit - invoice.balance
         if payment_by_other:
-            print(f"Adding credit for other payments: {payment_by_other}")
-            CreditLine.add_credit(amount=payment_by_other, subscription=invoice.subscription, note=note)
+            print(f"{dry_run_tag}Adding credit for other payments: {payment_by_other}")
+            if not dry_run:
+                CreditLine.add_credit(amount=payment_by_other, subscription=invoice.subscription, note=note)
