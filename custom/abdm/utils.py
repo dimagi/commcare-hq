8ba522590e301f7fef3df4d3677bcf8f2dc89c87
Ajeet Yadav <ayadav@dimagi.com>
2023-08-17 12:11:39 +0530

    fixes page size param name

--- custom/abdm/utils.py
+++ custom/abdm/utils.py
@@ -67,7 +67,7 @@ class GatewayRequestHelper:
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page'
+    page_size_query_param = 'page_size'
     max_page_size = 1000
 
 
