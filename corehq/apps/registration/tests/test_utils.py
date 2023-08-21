3684d36f07a1272f276138d3f310bdefe1fdf708
Biyeun Buczyk <biyeun@dimagi.com>
2023-08-21 16:44:19 +0200

    add mock noop for setup_subscription so that it doesn't throw a NoBrokerAvoilable error

--- corehq/apps/registration/tests/test_utils.py
+++ corehq/apps/registration/tests/test_utils.py
@@ -95,6 +95,7 @@ class TestRequestNewDomain(TestCase):
         domain = Domain.get_by_name('subscription-failed')
         self.assertIsNone(domain)
 
+    @mock.patch('corehq.apps.registration.utils._setup_subscription', _noop)
     @mock.patch('corehq.apps.registration.utils.initialize_domain_with_default_roles', _issue_initializing_domain)
     @mock.patch('corehq.apps.registration.utils.notify_exception', _noop)
     def test_default_roles_exception_raises_error(self):
