76600f33c62f4a3e202c3894ac5eb9b68cadb2cc
Biyeun Buczyk <biyeun@dimagi.com>
2023-08-22 15:53:07 +0200

    bootstrap 3 to 5 auto-migration for hqwebapp/js/crud_paginated_list_init.js

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/crud_paginated_list_init.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/crud_paginated_list_init.js
@@ -1,4 +1,4 @@
-hqDefine("hqwebapp/js/crud_paginated_list_init", [
+hqDefine("hqwebapp/js/bootstrap3/crud_paginated_list_init", [
     "jquery",
     "knockout",
     "hqwebapp/js/initial_page_data",
