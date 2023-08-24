e58da3d05403eb991e424782525fce7c4ae9d79c
Ethan Soergel <esoergel@gmail.com>
2023-08-24 12:07:04 -0400

    Add failing test

--- corehq/apps/receiverwrapper/tests/test_audit_logging.py
+++ corehq/apps/receiverwrapper/tests/test_audit_logging.py
@@ -21,8 +21,9 @@ def return_submission_run_resp(*args, **kwargs):
     return submission_post_run
 
 
-@patch('corehq.apps.receiverwrapper.views.couchforms.get_instance_and_attachment', return_value=(Mock(), Mock()))
-@patch('corehq.apps.receiverwrapper.views._record_metrics')
+@patch('corehq.apps.receiverwrapper.views.couchforms.get_instance_and_attachment',
+       new=Mock(return_value=(Mock(), Mock())))
+@patch('corehq.apps.receiverwrapper.views._record_metrics', new=Mock())
 @patch('corehq.apps.receiverwrapper.views.SubmissionPost.run', new=return_submission_run_resp)
 class TestAuditLoggingForFormSubmission(TestCase):
     domain = "submission-api-test"
@@ -37,10 +38,12 @@ class TestAuditLoggingForFormSubmission(TestCase):
     def tearDownClass(cls):
         cls.domain_obj.delete()
 
-    def _create_user(self, edit_data=False, access_api=False):
+    def _create_user(self, access_api, access_mobile_endpoints):
         role = UserRole.create(
             self.domain, 'api-user', permissions=HqPermissions(
-                edit_data=edit_data, access_api=access_api
+                edit_data=access_api,
+                access_api=access_api,
+                access_mobile_endpoints=access_mobile_endpoints,
             )
         )
         web_user = WebUser.create(self.domain, self.username, self.password,
@@ -52,16 +55,23 @@ class TestAuditLoggingForFormSubmission(TestCase):
         self.client.login(username=self.username, password=self.password)
         self.assertEqual(self.client.post(url).status_code, status)
 
-    def test_valid_request_not_logged(self, mock_record_metrics, mock_couchform_instance):
+    def test_api_user_api_endpoint(self):
         url = reverse(post_api, args=[self.domain])
-        self._create_user(edit_data=True, access_api=True)
+        self._create_user(access_api=True, access_mobile_endpoints=False)
         with patch('corehq.apps.receiverwrapper.views.notify_exception') as mock_notify_exception:
             self.assert_api_response(200, url)
             mock_notify_exception.assert_not_called()
 
-    def test_invalid_request_logged(self, mock_record_metrics, mock_couchform_instance):
+    def test_mobile_user_regular_submission(self):
         url = reverse(secure_post, args=[self.domain])
-        user = self._create_user(edit_data=True, access_api=True)
+        self._create_user(access_api=False, access_mobile_endpoints=True)
+        with patch('corehq.apps.receiverwrapper.views.notify_exception') as mock_notify_exception:
+            self.assert_api_response(200, url)
+            mock_notify_exception.assert_not_called()
+
+    def test_api_user_regular_submission_gets_logged(self):
+        url = reverse(secure_post, args=[self.domain])
+        user = self._create_user(access_api=True, access_mobile_endpoints=False)
         with patch('corehq.apps.receiverwrapper.views.notify_exception') as mock_notify_exception:
             self.assert_api_response(200, url)
             mock_notify_exception.assert_called_once_with(ANY, message=ANY)
