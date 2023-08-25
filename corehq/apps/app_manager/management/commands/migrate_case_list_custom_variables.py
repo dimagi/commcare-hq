ae52fe4db465f3b0e9537b2e0b93c6472ca60244
Martin Riese <mriese@dimagi.com>
2023-08-25 10:05:45 -0500

    Add migration command

---
+++ corehq/apps/app_manager/management/commands/migrate_case_list_custom_variables.py
@@ -0,0 +1,75 @@
+from corehq.apps.app_manager.management.commands.helpers import AppMigrationCommandBase
+from corehq.apps.domain.models import Domain
+
+from lxml import etree
+
+
+class Command(AppMigrationCommandBase):
+    help = """Migrate case list custom variables from xml to dict and back"""
+
+    include_builds = True
+    include_linked_apps = True
+    DOMAIN_LIST_FILENAME = 'migrate_case_list_custom_variables_domain.txt'
+    DOMAIN_PROGRESS_NUMBER_FILENAME = 'migrate_case_list_custom_variables_progress.txt'
+    chunk_size = 1
+
+    def add_arguments(self, parser):
+        super(Command, self).add_arguments(parser)
+
+        parser.add_argument(
+            '--reverse',
+            action='store_true',
+            default=False,
+            help="Perform the migration in reverse",
+        )
+
+    def get_domains(self):
+        # TODO: to many?
+        return Domain.get_all_names()
+
+    @staticmethod
+    def parse(xml):
+        elems = etree.fromstring(f"<w>{xml}</w>")
+        return {e.tag: e.get("function") for e in elems}
+
+    @staticmethod
+    def migrate_detail_forward(detail):
+        if detail and (detail.get('custom_variables') is not None):
+            detail['custom_variables_map'] = Command.parse(detail['custom_variables'])
+            del detail['custom_variables']
+            return True
+        return False
+
+    @staticmethod
+    def serialize(variables_dict):
+        return "\n".join([f"<{p[0]} function=\"{p[1]}\"/>" for p in variables_dict.items()])
+
+    @staticmethod
+    def migrate_detail_backward(detail):
+        if detail and (detail.get('custom_variables_map') is not None):
+            detail['custom_variables'] = Command.serialize(detail['custom_variables_map'])
+            del detail['custom_variables_map']
+            return True
+        return False
+
+    @staticmethod
+    def migrate_app_impl(app, reverse):
+        app_was_changed = False
+
+        migrate_detail = Command.migrate_detail_forward
+        if reverse:
+            migrate_detail = Command.migrate_detail_backward
+
+        for module in app['modules']:
+            detail_pair = module.get('case_details')
+            if detail_pair:
+                short_detail, long_detail = detail_pair['short'], detail_pair['long']
+                app_was_changed_short = migrate_detail(short_detail)
+                app_was_changed_long = migrate_detail(long_detail)
+                app_was_changed = app_was_changed_short or app_was_changed_long
+        if app_was_changed:
+            return app
+
+    def migrate_app(self, app):
+        reverse = self.options['reverse']
+        return Command.migrate_app_impl(app, reverse)
---
+++ corehq/apps/app_manager/tests/test_migrate_case_list_custom_variables.py
@@ -0,0 +1,79 @@
+from django.test import SimpleTestCase
+
+from corehq.apps.app_manager.management.commands.migrate_case_list_custom_variables import Command
+
+
+class CaseListCustomVariablesTests(SimpleTestCase):
+
+    custom_variables_xml = "<xml1 function=\"if(true(), 'text text', '')\"/>\n<xml2 function=\"text\"/>"
+    custom_variables_dict = {"xml1": "if(true(), 'text text', '')", "xml2": "text"}
+
+    def make_app(self, short_dict):
+        return {
+            "modules": [
+                {
+                    "case_details": {
+                        "short": short_dict,
+                        "long": {}
+                    }
+                }
+            ]
+        }
+
+    def test_parse(self):
+        var_dict = Command.parse(CaseListCustomVariablesTests.custom_variables_xml)
+        self.assertEqual(var_dict, CaseListCustomVariablesTests.custom_variables_dict)
+
+    def test_serialize(self):
+        xml = Command.serialize(CaseListCustomVariablesTests.custom_variables_dict)
+        self.assertEqual(xml, CaseListCustomVariablesTests.custom_variables_xml)
+
+    def test_migrate_details_forward(self):
+        detail = {
+            "custom_variables": CaseListCustomVariablesTests.custom_variables_xml,
+        }
+
+        did_migrate = Command.migrate_detail_forward(detail)
+        self.assertTrue(did_migrate)
+        self.assertIsNotNone(detail.get("custom_variables_map"))
+        self.assertTrue(detail.get("custom_variables_map"), CaseListCustomVariablesTests.custom_variables_dict)
+        self.assertIsNone(detail.get("custom_variables"))
+
+    def test_migrate_details_forward_no_variables(self):
+        detail = {}
+        did_migrate = Command.migrate_detail_forward(detail)
+        self.assertFalse(did_migrate)
+
+    def test_migrate_details_backward(self):
+        detail = {
+            "custom_variables_map": CaseListCustomVariablesTests.custom_variables_dict,
+        }
+
+        did_migrate = Command.migrate_detail_backward(detail)
+        self.assertTrue(did_migrate)
+        self.assertIsNotNone(detail.get("custom_variables"))
+        self.assertTrue(detail.get("custom_variables"), CaseListCustomVariablesTests.custom_variables_xml)
+        self.assertIsNone(detail.get("custom_variables_map"))
+
+    def test_migrate_app_impl(self):
+        app = self.make_app({"custom_variables": CaseListCustomVariablesTests.custom_variables_xml})
+        migrated_app = Command.migrate_app_impl(app, False)
+        self.assertIsNotNone(migrated_app)
+        self.assertEqual(
+            migrated_app,
+            self.make_app({"custom_variables_map": CaseListCustomVariablesTests.custom_variables_dict})
+        )
+
+    def test_migrate_app_impl_no_change(self):
+        app = self.make_app({})
+        migrated_app = Command.migrate_app_impl(app, False)
+        self.assertIsNone(migrated_app)
+
+    def test_migrate_app_impl_reverse(self):
+        app = self.make_app({"custom_variables_map": CaseListCustomVariablesTests.custom_variables_dict})
+        migrated_app = Command.migrate_app_impl(app, True)
+        self.assertIsNotNone(migrated_app)
+        self.assertEqual(
+            migrated_app,
+            self.make_app({"custom_variables": CaseListCustomVariablesTests.custom_variables_xml})
+        )
