1cfc074a09d429cda4a8054095c6070dfe2d5824
Jing Cheng <jcheng@dimagi.com>
2023-08-28 23:14:21 -0400

    Add need update column

--- corehq/apps/accounting/management/commands/list_customer_billing_account_software_plan.py
+++ corehq/apps/accounting/management/commands/list_customer_billing_account_software_plan.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from corehq.apps.accounting.models import BillingAccount, Subscription
+from corehq.apps.accounting.models import BillingAccount, Subscription, SubscriptionType
 import csv
 
 
@@ -11,23 +11,44 @@ class Command(BaseCommand):
         file_path = "/tmp/customer_billing_plans.csv"
         with open(file_path, 'w', newline='') as csvfile:
             csvwriter = csv.writer(csvfile)
-            headers = ["Account", "Domain", "Software Plan", "Type", "Do not invoice", "Start Date", "End Date"]
+            headers = ["Account", "Domain", "Software Plan", "Type", "Do not invoice",
+                       "Start Date", "End Date", "Main Subscription", "Need Update"]
             csvwriter.writerow(headers)
 
             for account in customer_billing_account:
                 subscriptions = Subscription.visible_objects.filter(account=account, is_active=True)
                 if len(subscriptions) == 0:
-                    csvwriter.writerow([account.name, "", "", "", ""])
-
+                    csvwriter.writerow([account.name, "", "", "", "", "", "", "", ""])
                 else:
+                    # Value for csv column Main Subscription
+                    csv_main_subscription = ""
+                    try:
+                        main_subscription = subscriptions.filter(do_not_invoice=False).get()
+                        csv_main_subscription = main_subscription.plan_version
+                    except Subscription.DoesNotExist:
+                        main_subscription = None
+                        csv_main_subscription = "No Main Subscription"
+                    except Subscription.MultipleObjectsReturned:
+                        main_subscription = None
+                        csv_main_subscription = "Multiple Main Subscriptions"
                     for subscription in subscriptions:
+                        need_update = False
+                        if (
+                            main_subscription
+                            and subscription.plan_version != main_subscription.plan_version
+                            and subscription.service_type in (SubscriptionType.PRODUCT,
+                                                              SubscriptionType.IMPLEMENTATION)
+                        ):
+                            need_update = True
                         csvwriter.writerow([
                             account.name,
                             subscription.subscriber.domain,
-                            str(subscription.plan_version),
+                            subscription.plan_version,
                             subscription.service_type,
                             subscription.do_not_invoice,
                             subscription.date_start,
-                            subscription.date_end
+                            subscription.date_end,
+                            csv_main_subscription,
+                            need_update
                         ])
         print(f"File has been saved to {file_path}")
