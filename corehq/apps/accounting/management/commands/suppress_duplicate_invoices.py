e7b3797ade22e4dd06445b9920325e3023953be5
Jing Cheng <jcheng@dimagi.com>
2023-09-02 00:55:59 -0400

    Generate report

--- corehq/apps/accounting/management/commands/suppress_duplicate_invoices.py
+++ corehq/apps/accounting/management/commands/suppress_duplicate_invoices.py
@@ -1,15 +1,17 @@
+import csv
 import datetime
 from datetime import timedelta
 from django.core.management.base import BaseCommand
 from django.db.models import Count
 from corehq.apps.accounting.interface import get_subtotal_and_deduction
-from corehq.apps.accounting.models import CreditAdjustment, Invoice, CreditLine, FeatureType
+from corehq.apps.accounting.models import (BillingRecord, CreditAdjustment, Invoice,
+                                           CreditLine, FeatureType, LineItem)
 from corehq.util.dates import get_first_last_days
 from dimagi.utils.dates import add_months
 
 
 class Command(BaseCommand):
-    help = 'Put credit back for duplicate invoices and suppress them'
+    help = 'Put credit back for duplicate invoices and suppress them, and generate a report'
 
     def add_arguments(self, parser):
         parser.add_argument('year', type=int, help='The year of the statement period.')
@@ -36,21 +38,42 @@ class Command(BaseCommand):
             count=Count('id')).filter(count__gt=1).values_list('subscription', flat=True)
 
         suppressed_count = 0
-        for sub_id in duplicate_subs_ids:
-            related_invoices = list(Invoice.objects.filter(is_hidden_to_ops=False,
-                subscription_id=sub_id, date_start__range=(start_date, end_date)).order_by('-date_created'))
-            first_created_invoice_id = related_invoices[0].id
-            for invoice in related_invoices[:-1]:
-                custom_note = f"{note}. Referenced to invoice {first_created_invoice_id}."
-                self.revert_invoice_payment(invoice, custom_note, year, month, dry_run)
-                # suppress invoice
-                print(f'{dry_run_tag}Suppressing invoice {invoice.id} for domain '
-                      f'{invoice.subscription.subscriber.domain} ', end='')
-                if not dry_run:
-                    invoice.is_hidden_to_ops = True
-                    invoice.save()
-                suppressed_count += 1
-                print("✓")
+
+        file_path = "/tmp/duplicate_invoices.csv"
+        with open(file_path, 'w', newline='') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            headers = ['Invoice ID', 'BillingAccount Name', 'Dimagi Contact', 'Domain', 'Software Plan Name',
+                       'Service Type', 'Subscription Id', 'tax_rate', 'balance', 'date_due', 'date_paid',
+                       'date_created', 'date_start', 'date_end', 'is_hidden', 'is_hidden_to_ops', 'last_modified',
+                       'Emailed To List', 'Plan Cost', 'Plan Credit', 'SMS Cost', 'SMS Credit', 'User Cost',
+                       'User Credit', 'Total Cost', 'Total Credit', 'Will Suppress', 'Will Revert',
+                       'Suppression Status', 'Revert Status']
+            csvwriter.writerow(headers)
+            for sub_id in duplicate_subs_ids:
+                related_invoices = list(Invoice.objects.filter(is_hidden_to_ops=False,
+                    subscription_id=sub_id, date_start__range=(start_date, end_date)).order_by('date_created'))
+                first_created_invoice_id = related_invoices[0].id
+                row_data = self.get_csv_row_data(related_invoices[0], False, False, "", "")
+                csvwriter.writerow(row_data)
+                for invoice in related_invoices[1:]:
+                    will_suppress = True
+                    suppression_status = ""
+                    custom_note = f"{note}. Referenced to invoice {first_created_invoice_id}."
+                    # credit back the paid amount
+                    will_revert, revert_status = self.revert_invoice_payment(
+                        invoice, custom_note, year, month, dry_run)
+                    # suppress invoice
+                    print(f'{dry_run_tag}Suppressing invoice {invoice.id} for domain '
+                        f'{invoice.subscription.subscriber.domain} ', end='')
+                    if not dry_run:
+                        invoice.is_hidden_to_ops = True
+                        invoice.save()
+                        suppression_status = True
+                    row_data = self.get_csv_row_data(
+                        invoice, will_suppress, will_revert, suppression_status, revert_status)
+                    csvwriter.writerow(row_data)
+                    suppressed_count += 1
+                    print("✓")
 
         self.stdout.write(self.style.SUCCESS(f'{dry_run_tag}Successfully suppressed {suppressed_count} '
                                              f'duplicate invoices for Statement Period: {year}-{month}'))
@@ -58,7 +81,8 @@ class Command(BaseCommand):
     def revert_invoice_payment(self, invoice, note, year, month, dry_run=False):
         dry_run_tag = '[DRY_RUN] ' if dry_run else ''
         payment_by_other = invoice.subtotal
-        reverted = False
+        will_revert = False
+        reverted = ""
 
         # Plan Credit
         if not invoice.subscription.auto_generate_credits:
@@ -67,14 +91,16 @@ class Command(BaseCommand):
             )
             plan_credit = -plan_deduction
             if plan_credit:
-                reverted = True
+                will_revert = True
                 print(f'{dry_run_tag}Adding plan credit: {plan_credit} to '
                     f'domain {invoice.subscription.subscriber.domain}')
                 if not dry_run:
                     CreditLine.add_credit(amount=plan_credit, subscription=invoice.subscription,
                                         is_product=True, note=note)
+                    reverted = True
         else:
             # Check if we generate duplicate product credit line
+            # We don't need to return those auto-generated credits
             current_year, current_month = add_months(year, month, 1)
 
             date_start = datetime.datetime(current_year, current_month, 1)
@@ -101,32 +127,82 @@ class Command(BaseCommand):
             )
             feature_credit = -feature_deduction
             if feature_credit:
-                reverted = True
+                will_revert = True
                 print(f"{dry_run_tag}Adding {feature[0]} credit: {feature_credit} to "
                       f"domain {invoice.subscription.subscriber.domain}")
                 if not dry_run:
                     CreditLine.add_credit(amount=feature_credit, subscription=invoice.subscription,
                                         feature_type=feature[0], note=note)
+                    reverted = True
 
         # Any Credit
         any_credit = -invoice.applied_credit
         if any_credit:
-            reverted = True
+            will_revert = True
             payment_by_other -= any_credit
             print(f"{dry_run_tag}Adding type Any credit: {any_credit} to "
                   f"domain {invoice.subscription.subscriber.domain}")
             if not dry_run:
                 CreditLine.add_credit(amount=any_credit, subscription=invoice.subscription, note=note)
+                reverted = True
 
         # Calculate the amount paid not by credit
         payment_by_other -= invoice.balance
         if payment_by_other:
-            reverted = True
+            will_revert = True
             print(f"{dry_run_tag}Adding credit for other payments: {payment_by_other} "
                   f"to domain {invoice.subscription.subscriber.domain}")
             if not dry_run:
                 CreditLine.add_credit(amount=payment_by_other, subscription=invoice.subscription, note=note)
+                reverted = True
 
-        if reverted:
+        if will_revert:
             print(f'{dry_run_tag}Successfully reverted payment for Invoice Id: {invoice.id}, '
               f'Domain: {invoice.subscription.subscriber.domain}')
+        return will_revert, reverted
+
+    def get_csv_row_data(self, invoice, will_suppress, will_revert, suppresion_status, revert_status):
+        billing_record = BillingRecord.objects.filter(invoice=invoice,
+                                                      date_created__gt=invoice.date_end).first() or None
+        emailed_to_combined = "; ".join(billing_record.emailed_to_list) if billing_record else ""
+
+        related_line_items = LineItem.objects.filter(subscription_invoice=invoice)
+
+        # Compute the subtotals and credits for the three categories
+        plan_subtotal, plan_credit = get_subtotal_and_deduction(
+            [item for item in related_line_items if item.product_rate])
+
+        sms_line_items = [item for item in related_line_items
+                          if item.feature_rate and item.feature_rate.feature.feature_type == FeatureType.SMS]
+        user_line_items = [item for item in related_line_items
+                           if item.feature_rate and item.feature_rate.feature.feature_type == FeatureType.USER]
+
+        sms_subtotal, sms_credit = get_subtotal_and_deduction(sms_line_items)
+        user_subtotal, user_credit = get_subtotal_and_deduction(user_line_items)
+        row_data = [
+            invoice.id,
+            invoice.subscription.account.name,
+            invoice.subscription.account.dimagi_contact,
+            invoice.subscription.subscriber.domain,
+            invoice.subscription.plan_version.plan.name,
+            invoice.subscription.service_type,
+            invoice.subscription_id,
+            invoice.tax_rate,
+            invoice.balance,
+            invoice.date_due,
+            invoice.date_paid,
+            invoice.date_created,
+            invoice.date_start,
+            invoice.date_end,
+            invoice.is_hidden,
+            invoice.is_hidden_to_ops,
+            invoice.last_modified,
+            emailed_to_combined,
+            plan_subtotal, plan_credit,
+            sms_subtotal, sms_credit,
+            user_subtotal, user_credit,
+            invoice.subtotal,
+            invoice.applied_credit
+        ]
+        row_data.extend([will_suppress, will_revert, suppresion_status, revert_status])
+        return row_data
