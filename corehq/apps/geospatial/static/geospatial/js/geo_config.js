29360183e28491e5b0ac40371766d1fb938e2f70
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-09-05 09:40:57 +0200

    fix js lint issues

--- corehq/apps/geospatial/static/geospatial/js/geo_config.js
+++ corehq/apps/geospatial/static/geospatial/js/geo_config.js
@@ -7,19 +7,17 @@ hqDefine("geospatial/js/geo_config", [
     ko,
     initialPageData
 ) {
-    const CUSTOM_USER_PROP = "custom_user_property";
-
     var geoConfigViewModel = function (configData) {
         'use strict';
         var self = {};
 
-        var data = configData.get('config')
+        var data = configData.get('config');
         self.customUserFieldName = ko.observable(data.user_location_property_name);
         self.geoCasePropertyName = ko.observable(data.case_location_property_name);
         self.isCasePropDeprecated = ko.observable(configData.get('is_case_prop_deprecated'));
 
         return self;
-    }
+    };
 
     $(function () {
         $('#geospatial-config-form').koApplyBindings(geoConfigViewModel(initialPageData));
