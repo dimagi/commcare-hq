85121a67346fc23f2dd6a4ed40c53716dbaa3f77
Amit Phulera <amitphulera999@gmail.com>
2023-09-06 15:16:23 +0530

    update string to text/keyword in group mappings

--- corehq/apps/es/mappings/group_mapping.py
+++ corehq/apps/es/mappings/group_mapping.py
@@ -14,38 +14,35 @@ GROUP_MAPPING = {
             "type": "boolean"
         },
         "doc_type": {
-            "index": "not_analyzed",
-            "type": "string"
+            "type": "keyword"
         },
         "domain": {
             "fields": {
                 "exact": {
-                    "index": "not_analyzed",
-                    "type": "string"
+                    "type": "keyword"
                 }
             },
-            "type": "string"
+            "type": "text"
         },
         "name": {
             "fields": {
                 "exact": {
-                    "index": "not_analyzed",
-                    "type": "string"
+                    "type": "keyword"
                 }
             },
-            "type": "string"
+            "type": "text"
         },
         "path": {
-            "type": "string"
+            "type": "text"
         },
         "removed_users": {
-            "type": "string"
+            "type": "text"
         },
         "reporting": {
             "type": "boolean"
         },
         "users": {
-            "type": "string"
+            "type": "text"
         },
         Tombstone.PROPERTY_NAME: {
             "type": "boolean"
