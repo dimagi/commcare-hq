c781b53a8ee8e700f064361a4a1a3e0f3e017d2e
Biyeun Buczyk <biyeun@dimagi.com>
2023-09-06 16:32:57 +0200

    Bootstrap 5 - Updated references to 'hqwebapp/js/crud_paginated_list_init.js'

--- corehq/apps/email/static/email/js/gateway_list.js
+++ corehq/apps/email/static/email/js/gateway_list.js
@@ -1,5 +1,5 @@
 hqDefine("email/js/gateway_list", [
-    "hqwebapp/js/crud_paginated_list_init",
+    "hqwebapp/js/bootstrap3/crud_paginated_list_init",
     "hqwebapp/js/widgets",
 ], function () {
     // No page-specific logic, just need to collect the dependencies above
