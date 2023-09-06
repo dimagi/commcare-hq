7914c099ed3e7d09d3ba8bc582ae91d2d750e415
Amit Phulera <amitphulera999@gmail.com>
2023-09-06 15:28:08 +0530

    remove remove geohash, geohash_prefix, geohash_precision, lat_lon

--- corehq/apps/es/mappings/xform_mapping.py
+++ corehq/apps/es/mappings/xform_mapping.py
@@ -113,10 +113,6 @@ XFORM_MAPPING = {
                             "type": "keyword"
                         },
                         "geo_point": {
-                            "geohash": True,
-                            "geohash_precision": "10m",
-                            "geohash_prefix": True,
-                            "lat_lon": True,
                             "type": "geo_point"
                         },
                         "instanceID": {
