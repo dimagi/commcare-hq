ffafcc4bdfb9f498ef1d745f2aab61a9455e6a37
Daniel Roberts <droberts@dimagi.com>
2023-09-11 14:31:27 -0500

    Add "smart" selection method option to remove_duplicate_domains

--- corehq/apps/domain/management/commands/remove_duplicate_domains.py
+++ corehq/apps/domain/management/commands/remove_duplicate_domains.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from memoized import memoized
 
 from corehq.apps.domain.models import Domain
 
@@ -7,9 +8,15 @@ class Command(BaseCommand):
     help = "Detects and removes duplicate domains"
 
     def add_arguments(self, parser):
+        parser.add_argument('--selection-method', choices=['default', 'smart'], default='default', help="""
+        'default' selects the domain doc currently given by Domain.get_by_name.
+        'smart' uses additional signals to attempt to determine the best domain doc.
+        Note that this may be different from the one currently given by Domain.get_by_name,
+        and thus this will result in a change in the domain doc actively used by the system.
+        """)
         parser.add_argument('--dry-run', action='store_true', default=False)
 
-    def handle(self, dry_run, **options):
+    def handle(self, selection_method, dry_run, **options):
         domains = Domain.get_all()
         seen = set([])
         dups = set([])
@@ -29,17 +36,48 @@ class Command(BaseCommand):
                 reduce=False,
                 include_docs=True,
             ).all()
-            other_domain_docs = [d for d in all_domain_docs if d.get_id != currently_chosen_domain_doc.get_id]
+
+            if selection_method == 'default':
+                chosen_domain_doc = currently_chosen_domain_doc
+            elif selection_method == 'smart':
+                chosen_domain_doc = sorted(all_domain_docs, key=lambda d: score_domain_doc(d, is_currently_chosen=(d.get_id == currently_chosen_domain_doc.get_id)))[-1]
+            else:
+                raise NotImplementedError()
+
+            other_domain_docs = [d for d in all_domain_docs if d.get_id != chosen_domain_doc.get_id]
+
 
             self.stdout.write(f'Found duplicate docs for domain: {domain}\n')
-            self.stdout.write(f'Chosen\t{"_id":32}\t{"name":16}\tis_active\t{"date_created":26}\t{"_rev":32}\n')
+            self.stdout.write(f'Chosen\t{"_id":32}\t{"name":16}\tis_active\t{"date_created":26}\t{"_rev":32}\tscore\n')
 
             for domain_doc in all_domain_docs:
-                chosen = domain_doc._id == currently_chosen_domain_doc._id
+                chosen = domain_doc._id == chosen_domain_doc._id
                 chosen_str = '-->' if chosen else ''
-                self.stdout.write(f'{chosen_str}\t{domain_doc._id}\t{domain_doc.name:16}\t{str(domain_doc.is_active):9}\t{domain_doc.date_created}\t{domain_doc._rev}\n')
+                self.stdout.write(f'{chosen_str}\t{domain_doc._id}\t{domain_doc.name:16}\t{str(domain_doc.is_active):9}\t{domain_doc.date_created}\t{domain_doc._rev}\t{score_domain_doc(domain_doc, is_currently_chosen=chosen)}\n')
 
             if not dry_run:
                 for dom in other_domain_docs:
                     dom.doc_type = 'Domain-DUPLICATE'
                     dom.save()
+
+
+@memoized
+def score_domain_doc(domain_doc, *, is_currently_chosen):
+    """
+    Score a domain doc's worthiness as the "real" domain doc
+
+    The score is based on three factors:
+    - Is it the one that is currently returned by Domain.get_by_name?
+      This is a strong prior, since it's what is currently being served
+    - Has the doc been updated many times?
+      This could mean that somehow in the past it was being treated as the real doc, even if not presently
+    - Is the domain active? If only one of the many domain docs is active, we probably want that one
+    """
+    score = 0
+    if is_currently_chosen:
+        score += 1
+    if domain_doc.is_active:
+        score += 1
+    # add to the score the square root of the number of times the doc has been updated
+    score += float(domain_doc._rev.split('-')[0])**(.5)
+    return score
