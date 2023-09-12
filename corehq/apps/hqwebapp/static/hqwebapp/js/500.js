31b9c07e7b278d6ba846e8b8a2189c1a371c049b
Biyeun Buczyk <biyeun@dimagi.com>
2023-09-12 21:52:44 +0200

    more debugging

--- corehq/apps/hqwebapp/static/hqwebapp/js/500.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/500.js
@@ -1,8 +1,10 @@
 hqDefine("hqwebapp/js/500",[
     'jquery',
-    'es6!hqwebapp/js/bootstrap5_loader',
+    'hqwebapp/js/bootstrap5_loader',
 ], function ($, bootstrap) {
     $(function () {
+        console.log('test bootstrap 500');
+        console.log(bootstrap);
         new bootstrap.Popover('#sad-danny', {
             title: gettext("This is Danny, one of our best developers."),
             content: gettext("Danny is pretty sad that you had to encounter this issue. He's making sure it gets fixed as soon as possible.")
