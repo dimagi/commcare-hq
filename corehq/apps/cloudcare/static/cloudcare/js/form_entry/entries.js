422f1e812f69698b0b046818d12ca5598955e667
nospame <mapson.evan@gmail.com>
2023-09-14 13:30:20 -0700

    Remove unused conditional

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/entries.js
@@ -1318,9 +1318,6 @@ hqDefine("cloudcare/js/form_entry/entries", function () {
                 } else {
                     switch (question.control()) {
                         case constants.CONTROL_IMAGE_CHOOSE:
-                            if (question.stylesContains(constants.SIGNATURE)) {
-                                break;
-                            }
                             entry = new ImageEntry(question, {});
                             break;
                         case constants.CONTROL_AUDIO_CAPTURE:
