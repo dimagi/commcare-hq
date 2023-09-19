7ef0209581e76caa845a2cec677862e0abcf3c4d
Jing Cheng <jcheng@dimagi.com>
2023-09-18 22:53:08 -0400

    Write raw sql commands to add index concurrently

--- corehq/apps/accounting/migrations/0087_invoice_unique_constraints.py
+++ corehq/apps/accounting/migrations/0087_invoice_unique_constraints.py
@@ -2,6 +2,19 @@
 
 from django.db import migrations, models
 
+# Raw SQL to add unique constraints to Invoice model
+INVOICE_TABLE_INDEX_NAME = 'unique_invoice_per_subscription_period'
+INVOICE_TABLE_COLUMNS = ['subscription_id', 'date_start', 'date_end']
+CREATE_INVOICE_INDEX_SQL = (f"CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS {INVOICE_TABLE_INDEX_NAME} ON accounting_invoice ({','.join(INVOICE_TABLE_COLUMNS)}) "
+"WHERE is_hidden_to_ops = False AND duplicate_invoice_id IS NULL")
+DROP_INVOICE_INDEX_SQL = f"DROP INDEX CONCURRENTLY IF EXISTS {INVOICE_TABLE_INDEX_NAME}"
+
+# Raw SQL to add unique constraints to Customer Invoice model
+CUSTOMER_INVOICE_TABLE_INDEX_NAME = 'unique_customer_invoice_per_subscription_period'
+CUSTOMER_INVOICE_TABLE_COLUMNS = ['account_id', 'date_start', 'date_end']
+CREATE_CUSTOMER_INVOICE_INDEX_SQL = f"CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS {CUSTOMER_INVOICE_TABLE_INDEX_NAME} ON accounting_customerinvoice ({','.join(CUSTOMER_INVOICE_TABLE_COLUMNS)}) WHERE is_hidden_to_ops = False"
+DROP_CUSTOMER_INVOICE_INDEX_SQL = f"DROP INDEX CONCURRENTLY IF EXISTS {CUSTOMER_INVOICE_TABLE_INDEX_NAME}"
+
 
 class Migration(migrations.Migration):
 
@@ -10,12 +23,24 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddConstraint(
-            model_name='customerinvoice',
-            constraint=models.UniqueConstraint(condition=models.Q(('is_hidden_to_ops', False)), fields=('account', 'date_start', 'date_end'), name='unique_invoice_per_account_period'),
+        migrations.RunSQL(
+            sql=CREATE_INVOICE_INDEX_SQL,
+            reverse_sql=DROP_INVOICE_INDEX_SQL,
+            state_operations=[
+                migrations.AddIndex(
+                    model_name='invoice',
+                    index=models.Index(fields=INVOICE_TABLE_COLUMNS, name=INVOICE_TABLE_INDEX_NAME),
+                )
+            ]
         ),
-        migrations.AddConstraint(
-            model_name='invoice',
-            constraint=models.UniqueConstraint(condition=models.Q(('is_hidden_to_ops', False)), fields=('subscription', 'date_start', 'date_end'), name='unique_invoice_per_subscription_period'),
-        ),
+        migrations.RunSQL(
+            sql=CREATE_CUSTOMER_INVOICE_INDEX_SQL,
+            reverse_sql=DROP_CUSTOMER_INVOICE_INDEX_SQL,
+            state_operations=[
+                migrations.AddIndex(
+                    model_name='customerinvoice',
+                    index=models.Index(fields=CUSTOMER_INVOICE_TABLE_COLUMNS, name=CUSTOMER_INVOICE_TABLE_INDEX_NAME),
+                )
+            ]
+        )
     ]
