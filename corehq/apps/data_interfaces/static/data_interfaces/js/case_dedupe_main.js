3315279f2ea73a12e8302ad949f728d851de7bcc
Matt Riley <mriley@dimagi.com>
2023-09-20 11:36:40 -0400

    Improve property selection for deduplication

--- corehq/apps/data_interfaces/static/data_interfaces/js/case_dedupe_main.js
+++ corehq/apps/data_interfaces/static/data_interfaces/js/case_dedupe_main.js
@@ -14,6 +14,131 @@ hqDefine("data_interfaces/js/case_dedupe_main", [
     casePropertyInput,
     CaseRuleCriteria
 ) {
+
+    /*
+    PropertyManager exists to make sure that available properties are updated and unique.
+    For example, if a case type contains A, B, and C properties, this class ensures
+    that if C is selected, further options will only show A and B.
+    */
+    const PropertyManager = function (caseType, selectedProperties, initialPropertyMap) {
+        const self = {};
+        self.caseType = caseType;
+        self.selectedProperties = selectedProperties;
+        self.availablePropertyMap = ko.observable(clonePropertyMap(initialPropertyMap));
+
+        self.addProperty = function (property) {
+            self.selectedProperties.push(property);
+
+            if (property.name()) {
+                let currentProperties = self.availablePropertyMap()[self.caseType()];
+                currentProperties = reserveProperties([property.name()], currentProperties);
+                rebuildPropertyMap(currentProperties);
+            }
+        };
+
+        self.removeProperty = function (property) {
+            self.selectedProperties.remove(property);
+
+            let currentProperties = self.availablePropertyMap()[self.caseType()];
+            currentProperties = restoreProperty(property.name(), currentProperties);
+
+            rebuildPropertyMap(currentProperties);
+        };
+
+        self.updatePropertyValue = function (oldValue, newValue) {
+            let currentProperties = self.availablePropertyMap()[self.caseType()];
+            currentProperties = reserveProperties([oldValue], currentProperties);
+            currentProperties = restoreProperty(newValue, currentProperties);
+
+            rebuildPropertyMap(currentProperties);
+        };
+
+        self.caseType.subscribe(handleCaseTypeChange);
+        function handleCaseTypeChange(caseType) {
+            if (!caseType) {
+                return;
+            }
+
+            resetAvailableProperties();
+
+            const selectedNames = self.selectedProperties().map(function (prop) {
+                return prop.name();
+            });
+
+            let currentProperties = self.availablePropertyMap()[caseType];
+            currentProperties = reserveProperties(selectedNames, currentProperties);
+            rebuildPropertyMap(currentProperties);
+        }
+
+        function resetAvailableProperties() {
+            const originalPropertyMap = clonePropertyMap(initialPropertyMap);
+            self.availablePropertyMap(originalPropertyMap);
+        }
+
+        function clonePropertyMap(originalMap) {
+            const clone = {};
+            for (const key in originalMap) {
+                clone[key] = originalMap[key].slice();
+            }
+
+            return clone;
+        }
+
+        function propertyExistsInOriginalMap(propName) {
+            const originalProperties = initialPropertyMap[self.caseType()];
+
+            return (originalProperties.indexOf(propName) !== -1);
+        }
+
+        function rebuildPropertyMap(changedCategory) {
+            const caseType = self.caseType();
+            const existingMap = self.availablePropertyMap();
+            changedCategory.sort();
+            existingMap[caseType] = changedCategory;
+            self.availablePropertyMap(existingMap);
+        }
+
+        function restoreProperty(propName, currentProperties) {
+            if (!propName || !propertyExistsInOriginalMap(propName)) {
+                // do not put custom properties back into the map
+                return currentProperties;
+            }
+
+            currentProperties.push(propName);
+
+            return currentProperties;
+        }
+
+        function reserveProperties(propertyNames, currentProperties) {
+            propertyNames.forEach(function (propName) {
+                const foundIndex = currentProperties.indexOf(propName);
+                if (foundIndex !== -1) {
+                    currentProperties.splice(foundIndex, 1);
+                }
+            });
+
+            return currentProperties;
+        }
+
+        return self;
+    };
+
+    var CaseProperty = function (name, propertyManager) {
+        var self = {};
+        self.name = ko.observable();
+        let prevValue = '';
+        self.name.subscribe(function (newValue) {
+            if (!newValue) {
+                return;
+            }
+
+            propertyManager.updatePropertyValue(newValue, prevValue);
+            prevValue = newValue;
+        });
+        self.name(name);
+        return self;
+    };
+
     var CaseDedupe = function (
         initialName,
         initialCaseType,
@@ -21,12 +146,24 @@ hqDefine("data_interfaces/js/case_dedupe_main", [
         initialMatchType,
         initialCaseProperties,
         initialIncludeClosed,
-        initialPropertiesToUpdate
+        initialPropertiesToUpdate,
+        allCaseProperties
     ) {
         var self = {};
         self.name = ko.observable(initialName);
+        self.caseType = ko.observable();
+        self.caseProperties = ko.observableArray();
+        self.includeClosed = ko.observable(initialIncludeClosed);
+        self.propertyManager = PropertyManager(self.caseType, self.caseProperties, allCaseProperties);
 
-        self.caseType = ko.observable(initialCaseType);
+        // Create a separate property for this so that outside callers do not need
+        // to be aware of this class's structure. Also functions as making the variable read-only
+        self.availablePropertyMap = ko.pureComputed(function () {
+            return self.propertyManager.availablePropertyMap();
+        });
+
+        self.caseType(initialCaseType);
+
         self.caseTypeOptions = caseTypeOptions;
 
         self.matchType = ko.observable(initialMatchType);
@@ -44,28 +181,22 @@ hqDefine("data_interfaces/js/case_dedupe_main", [
             return gettext("True when ALL of the case properties match");
         };
 
-        var caseProperty = function (name) {
-            var self = {};
-            self.name = ko.observable(name);
-            return self;
-        };
-        self.caseProperties = ko.observableArray(_.map(initialCaseProperties, function (name) {
-            return caseProperty(name);
+        self.caseProperties(_.map(initialCaseProperties, function (name) {
+            return CaseProperty(name, self.propertyManager);
         }));
         self.removeCaseProperty = function (item) {
-            self.caseProperties.remove(item);
+            self.propertyManager.removeProperty(item);
         };
         self.addCaseProperty = function () {
-            self.caseProperties.push(caseProperty(''));
+            self.propertyManager.addProperty(CaseProperty('', self.propertyManager));
         };
+
         if (self.caseProperties().length === 0) {
             self.addCaseProperty();
         }
         self.serializedCaseProperties = ko.computed(function () {
             return ko.toJSON(self.caseProperties);
         });
-
-        self.includeClosed = ko.observable(initialIncludeClosed);
 
         var propertyToUpdate = function (name, valueType, value) {
             var self = {};
@@ -105,7 +236,8 @@ hqDefine("data_interfaces/js/case_dedupe_main", [
             initialPageData.get('match_type'),
             initialPageData.get('case_properties'),
             initialPageData.get('include_closed'),
-            initialPageData.get('properties_to_update')
+            initialPageData.get('properties_to_update'),
+            initialPageData.get("all_case_properties")
         );
         $("#case-dedupe-rule-definition").koApplyBindings(caseDedupe);
 
