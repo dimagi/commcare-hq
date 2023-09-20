74fb08ba3362dd8492b32a57c6d8141043b0a5a3
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-09-20 10:58:53 +0200

    implement additional model fields

--- corehq/apps/geospatial/models.py
+++ corehq/apps/geospatial/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext as _
 from dataclasses import dataclass
 
 import geopandas as gpd
@@ -135,13 +136,40 @@ class GeoConfig(models.Model):
 
     CUSTOM_USER_PROPERTY = 'custom_user_property'
     ASSIGNED_LOCATION = 'assigned_location'
+    RADIAL_ALGORITHM = 'radial_algorithm'
+    ROAD_NETWORK_ALGORITHM = 'road_network_algorithm'
+    MIN_MAX_GROUPING = 'min_max_grouping'
+    TARGET_SIZE_GROUPING = 'target_size_grouping'
 
     VALID_LOCATION_SOURCES = [
         CUSTOM_USER_PROPERTY,
         ASSIGNED_LOCATION,
+    ]
+    VALID_DISBURSEMENT_ALGORITHMS = [
+        (RADIAL_ALGORITHM, _('Radial Algorithm')),
+        (ROAD_NETWORK_ALGORITHM, _('Road Network Algorithm')),
+    ]
+    VALID_GROUPING_METHODS = [
+        (MIN_MAX_GROUPING, _('Min/Max Grouping')),
+        (TARGET_SIZE_GROUPING, _('Target Size Grouping')),
     ]
 
     domain = models.CharField(max_length=256, db_index=True, primary_key=True)
     location_data_source = models.CharField(max_length=126, default=CUSTOM_USER_PROPERTY)
     user_location_property_name = models.CharField(max_length=256, default=GPS_POINT_CASE_PROPERTY)
     case_location_property_name = models.CharField(max_length=256, default=GPS_POINT_CASE_PROPERTY)
+
+    selected_grouping_method = models.CharField(
+        choices=VALID_GROUPING_METHODS,
+        default=MIN_MAX_GROUPING,
+        max_length=50
+    )
+    max_cases_per_group = models.IntegerField(null=True)
+    min_cases_per_group = models.IntegerField(null=True)
+    target_group_count = models.IntegerField(null=True)
+
+    selected_disbursement_algorithm = models.CharField(
+        choices=VALID_DISBURSEMENT_ALGORITHMS,
+        default=ROAD_NETWORK_ALGORITHM,
+        max_length=50
+    )
