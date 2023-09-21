a892a48e98687d49bb192bc5639d4b3854daf7ee
Norman Hooper <nhooper@dimagi.com>
2023-09-21 19:53:37 +0100

    Stubs for CaseGroupingMap

---
+++ corehq/apps/geospatial/es.py
@@ -0,0 +1,7 @@
+
+
+def get_geohashes(domain, *, case_property, coords=None, precision=None):
+    """
+    Queries Elasticsearch for geohash grid aggregations of cases.
+    """
+    return [], precision
--- corehq/apps/geospatial/urls.py
+++ corehq/apps/geospatial/urls.py
@@ -1,19 +1,22 @@
+# urls.py
+
 from django.conf.urls import re_path as url
 
-from .dispatchers import CaseManagementMapDispatcher
+from .dispatchers import CaseGroupingMapDispatcher, CaseManagementMapDispatcher
 from .views import (
-    geospatial_default,
     GeoPolygonView,
-    GPSCaptureView,
-    get_paginated_cases_or_users_without_gps,
-    MapboxOptimizationV2,
-    mapbox_routing_status,
     GeospatialConfigPage,
+    GPSCaptureView,
+    MapboxOptimizationV2,
+    geospatial_default,
+    get_paginated_cases_or_users_without_gps,
+    get_paginated_geohashes,
     get_users_with_gps,
+    mapbox_routing_status,
 )
 
-
 urlpatterns = [
+    url(r'^$', geospatial_default, name='geospatial_default'),
     url(r'^edit_geo_polygon/$', GeoPolygonView.as_view(),
         name=GeoPolygonView.urlname),
     url(r'^mapbox_routing/$',
@@ -22,11 +25,15 @@ urlpatterns = [
     url(r'^mapbox_routing_status/(?P<poll_id>[\w-]+)/',
         mapbox_routing_status,
         name="mapbox_routing_status"),
-    url(r'^settings/$', GeospatialConfigPage.as_view(), name=GeospatialConfigPage.urlname),
+    url(r'^settings/$', GeospatialConfigPage.as_view(),
+        name=GeospatialConfigPage.urlname),
     url(r'^gps_capture/json/$', get_paginated_cases_or_users_without_gps,
         name='get_paginated_cases_or_users_without_gps'),
     url(r'^gps_capture/$', GPSCaptureView.as_view(), name=GPSCaptureView.urlname),
     url(r'^users/json/$', get_users_with_gps, name='get_users_with_gps'),
-    url(r'^$', geospatial_default, name='geospatial_default'),
+    url(r'^geohashes/json/$', get_paginated_geohashes,
+        name='get_paginated_geohashes'),
+
     CaseManagementMapDispatcher.url_pattern(),
+    CaseGroupingMapDispatcher.url_pattern(),
 ]
