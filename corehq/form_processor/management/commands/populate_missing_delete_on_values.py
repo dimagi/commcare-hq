5550ddec8779a5ebab2df1420868611947a5cbf5
Minha <mlee@dimagi.com>
2023-09-22 11:53:10 -0400

    Modify command to target docs by old state value, use raw SQL to update values

--- corehq/form_processor/management/commands/populate_missing_delete_on_values.py
+++ corehq/form_processor/management/commands/populate_missing_delete_on_values.py
@@ -1,16 +1,39 @@
+from django.db.models import Q
 from django.core.management.base import BaseCommand
 
 from corehq.form_processor.models import XFormInstance
-from corehq.sql_db.util import get_db_aliases_for_partitioned_query
+from corehq.sql_db.util import paginate_query_across_partitioned_databases
+
+from dimagi.utils.chunked import chunked
 
 
 class Command(BaseCommand):
-    help = "One time use. Populate missing XFormInstance.deleted_on values"
+    help = "One time use to populate missing XFormInstance.deleted_on values"
 
     def handle(self, **options):
-        for db in get_db_aliases_for_partitioned_query():
-            queryset = XFormInstance.objects.using(db).filter(deleted_on__isnull=True)
-            for xform in queryset:
-                if xform.doc_type == 'XFormInstance-Deleted':
-                    xform.deleted_on = xform.server_modified_on
-                    xform.save()
+        chunk_size = 100
+        old_deleted_state = 64
+
+        def iter_all_ids():
+            rows = paginate_query_across_partitioned_databases(
+                XFormInstance,
+                Q(deleted_on__isnull=True),
+                values=['form_id'],
+                query_size=chunk_size,
+            )
+            for r in rows:
+                yield r[0]
+
+        form_ids = iter_all_ids()
+        for chunk in chunked(form_ids, chunk_size, list):
+            with XFormInstance.get_plproxy_cursor() as cursor:
+                cursor.execute(
+                    """
+                    UPDATE form_processor_xforminstancesql
+                    SET deleted_on = server_modified_on
+                    WHERE form_id = ANY(%s)
+                        AND deleted_on IS NULL
+                        AND state & %s = %s
+                    """,
+                    [chunk, old_deleted_state, old_deleted_state]
+                )
