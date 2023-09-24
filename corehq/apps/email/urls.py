f8b7f362ffea974e067513089a764a68a3d9685e
Akash Jain <akashjain@work.com>
2023-09-24 19:46:25 +0530

    Add default view and update context for email app main view

--- corehq/apps/email/urls.py
+++ corehq/apps/email/urls.py
@@ -1,6 +1,7 @@
-from corehq.apps.email.views import EmailSMTPSettingsView
+from corehq.apps.email.views import EmailSMTPSettingsView, default
 from django.conf.urls import re_path as url
 
 urlpatterns = [
     url(r'settings/', EmailSMTPSettingsView.as_view(), name=EmailSMTPSettingsView.urlname),
+    url(r'^$', default, name='email_default'),
 ]
--- corehq/apps/email/views.py
+++ corehq/apps/email/views.py
@@ -1,12 +1,20 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
 
 from corehq import toggles
-from corehq.apps.domain.decorators import domain_admin_required
+from corehq.apps.domain.decorators import domain_admin_required, login_and_domain_required
 from corehq.apps.domain.views import BaseDomainView
 from corehq.apps.email.forms import EmailSMTPSettingsForm
 from corehq.apps.email.models import EmailSettings
+
+
+@login_and_domain_required
+def default(request, domain):
+    from corehq.messaging.scheduling.views import MessagingDashboardView
+    return HttpResponseRedirect(reverse(MessagingDashboardView.urlname, args=[domain]))
 
 
 @method_decorator(domain_admin_required, name='dispatch')
@@ -15,6 +23,11 @@ class EmailSMTPSettingsView(BaseDomainView):
     template_name = 'email/email_settings.html'
     urlname = 'email_gateway_settings'
     page_title = _('Email Settings')
+    section_name = _('Messaging')
+
+    @property
+    def section_url(self):
+        return reverse("email_default", args=[self.domain])
 
     def get(self, request, *args, **kwargs):
         email_settings = EmailSettings.objects.filter(domain=self.domain).first()
@@ -23,7 +36,12 @@ class EmailSMTPSettingsView(BaseDomainView):
             form = EmailSMTPSettingsForm(instance=email_settings)
         else:
             form = EmailSMTPSettingsForm()
-        return render(request, self.template_name, {'form': form, 'domain': self.domain})
+        return render(request, self.template_name, {
+            'form': form,
+            'domain': self.domain,
+            'current_page': {'title': self.page_title},
+            'section': {'page_name': self.section_name, 'url': self.section_url}
+        })
 
     def post(self, request, *args, **kwargs):
         email_settings, _ = EmailSettings.objects.get_or_create(domain=self.domain)
