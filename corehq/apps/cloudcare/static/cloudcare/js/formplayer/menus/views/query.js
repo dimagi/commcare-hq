aac98194501abdca46051e97e4632a9fd016e9fa
Jonathan Tang <Jonahtang738@gmail.com>
2023-09-25 09:54:24 -0400

    refactor: makes renderMapboxInput use named parameter

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
@@ -157,12 +157,12 @@ hqDefine("cloudcare/js/formplayer/menus/views/query", function () {
                     );
                     return true;
                 }
-                formEntryUtils.renderMapboxInput(
-                    inputId,
-                    geocoderItemCallback(id, model),
-                    geocoderOnClearCallback(id),
-                    initialPageData
-                );
+                formEntryUtils.renderMapboxInput({
+                    divId: inputId,
+                    itemCallback: geocoderItemCallback(id, model),
+                    clearCallBack: geocoderOnClearCallback(id),
+                    initialPageData: initialPageData
+                });
                 var divEl = $field.find('.mapboxgl-ctrl-geocoder');
                 divEl.css("max-width", "none");
                 divEl.css("width", "100%");
