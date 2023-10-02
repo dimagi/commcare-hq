126acfd10aa8de19f57814a091af793611bc2b1e
Minha <mlee@dimagi.com>
2023-10-02 13:17:38 -0400

    Disable dry run mode (but keep the option)

--- corehq/apps/cleanup/tasks.py
+++ corehq/apps/cleanup/tasks.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 
 @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
-def permanently_delete_eligible_data(dry_run=True):
+def permanently_delete_eligible_data(dry_run=False):
     """
     Permanently delete database objects that are eligible for hard deletion.
     To be eligible means to have a ``deleted_on`` field with a value less than
