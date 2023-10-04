951e0ceec6681763815a9e6fa4a011c753486b37
Biyeun Buczyk <biyeun@dimagi.com>
2023-10-04 16:03:01 +0200

    add page on accessibility

--- corehq/apps/styleguide/views/bootstrap5.py
+++ corehq/apps/styleguide/views/bootstrap5.py
@@ -5,3 +5,8 @@ from corehq.apps.styleguide.context import get_navigation_context
 
 def styleguide_home(request):
     return render(request, 'styleguide/bootstrap5/home.html', get_navigation_context("styleguide_home_b5"))
+
+
+def styleguide_atoms_accessibility(request):
+    return render(request, 'styleguide/bootstrap5/atoms/accessibility.html',
+                  get_navigation_context("styleguide_atoms_accessibility_b5"))
