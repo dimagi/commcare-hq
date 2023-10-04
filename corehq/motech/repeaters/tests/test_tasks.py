08c29ce1e9151ffd49e9d8428d056632aeb4b4d2
Graham Herceg <gherceg@dimagi.com>
2023-10-04 11:17:17 -0400

    add tests for _process_repeat_record

--- corehq/motech/repeaters/tests/test_tasks.py
+++ corehq/motech/repeaters/tests/test_tasks.py
@@ -14,14 +14,13 @@ from corehq.form_processor.utils.xform import (
     TestFormMetadata,
 )
 from corehq.motech.models import ConnectionSettings, RequestLog
-
+from corehq.motech.repeaters.models import FormRepeater, RepeatRecord, Repeater
+from corehq.motech.repeaters.tasks import delete_old_request_logs, process_repeater, _process_repeat_record
 from ..const import (
     RECORD_CANCELLED_STATE,
     RECORD_FAILURE_STATE,
     RECORD_PENDING_STATE,
 )
-from ..models import FormRepeater
-from ..tasks import process_repeater, delete_old_request_logs
 
 DOMAIN = 'gaidhlig'
 PAYLOAD_IDS = ['aon', 'dha', 'trì', 'ceithir', 'coig', 'sia', 'seachd', 'ochd',
@@ -154,3 +153,147 @@ def form_context(form_ids):
         yield
     finally:
         XFormInstance.objects.hard_delete_forms(DOMAIN, form_ids)
+
+
+class TestProcessRepeatRecord(TestCase):
+
+    def test_returns_if_record_is_cancelled(self):
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=self.repeater.repeater_id,
+            next_check=None,
+            cancelled=True,
+        )
+
+        _process_repeat_record(repeat_record)
+
+        self.assertEqual(self.mock_fire.call_count, 0)
+        self.assertEqual(self.mock_postpone_by.call_count, 0)
+
+    def test_cancels_and_returns_if_domain_cannot_forward(self):
+        self.mock_domain_can_forward.return_value = False
+
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=self.repeater.repeater_id,
+        )
+
+        _process_repeat_record(repeat_record)
+
+        fetched_repeat_record = RepeatRecord.get(repeat_record._id)
+        self.assertTrue(fetched_repeat_record.cancelled)
+        self.assertEqual(self.mock_fire.call_count, 0)
+        self.assertEqual(self.mock_postpone_by.call_count, 0)
+
+    def test_cancels_and_returns_if_repeat_record_exceeds_max_retries(self):
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=self.repeater.repeater_id,
+            failure_reason='test',
+            overall_tries=1,
+            max_possible_tries=1
+        )
+
+        _process_repeat_record(repeat_record)
+
+        fetched_repeat_record = RepeatRecord.get(repeat_record._id)
+        self.assertTrue(fetched_repeat_record.cancelled)
+        self.assertEqual(self.mock_fire.call_count, 0)
+        self.assertEqual(self.mock_postpone_by.call_count, 0)
+
+    def test_deletes_repeat_record_cancels_and_returns_if_repeater_deleted(self):
+        deleted_repeater = Repeater.objects.create(
+            domain=self.domain,
+            connection_settings=self.conn_settings,
+            is_deleted=True
+        )
+
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=deleted_repeater.repeater_id,
+        )
+
+        _process_repeat_record(repeat_record)
+
+        fetched_repeat_record = RepeatRecord.get(repeat_record._id)
+        self.assertEqual(fetched_repeat_record.doc_type, 'RepeatRecord-Deleted')
+        self.assertTrue(fetched_repeat_record.cancelled)
+        self.assertEqual(self.mock_fire.call_count, 0)
+        self.assertEqual(self.mock_postpone_by.call_count, 0)
+
+    def test_postpones_record_if_repeater_is_paused(self):
+        paused_repeater = Repeater.objects.create(
+            domain=self.domain,
+            connection_settings=self.conn_settings,
+            is_paused=True
+        )
+
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=paused_repeater.repeater_id,
+        )
+
+        _process_repeat_record(repeat_record)
+
+        self.assertEqual(self.mock_fire.call_count, 0)
+        self.assertEqual(self.mock_postpone_by.call_count, 1)
+
+    def test_fires_record_if_repeater_is_not_paused(self):
+        paused_repeater = Repeater.objects.create(
+            domain=self.domain,
+            connection_settings=self.conn_settings,
+            is_paused=False
+        )
+
+        repeat_record = RepeatRecord(
+            domain=self.domain,
+            payload_id='abc123',
+            registered_at=datetime.utcnow(),
+            repeater_id=paused_repeater.repeater_id,
+        )
+
+        _process_repeat_record(repeat_record)
+
+        self.assertEqual(self.mock_fire.call_count, 1)
+        self.assertEqual(self.mock_postpone_by.call_count, 0)
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.domain = 'process-repeat-record-tests'
+        cls.conn_settings = ConnectionSettings.objects.create(
+            domain=cls.domain,
+            name='To Be Deleted',
+            url="http://localhost/api/"
+        )
+        cls.repeater = Repeater.objects.create(
+            domain=cls.domain,
+            connection_settings=cls.conn_settings,
+        )
+
+    def setUp(self):
+        self.patch()
+
+    def patch(self):
+        patch_fire = patch.object(RepeatRecord, 'fire')
+        self.mock_fire = patch_fire.start()
+        self.addCleanup(patch_fire.stop)
+
+        patch_postpone_by = patch.object(RepeatRecord, 'postpone_by')
+        self.mock_postpone_by = patch_postpone_by.start()
+        self.addCleanup(patch_postpone_by.stop)
+
+        patch_domain_can_forward = patch('corehq.motech.repeaters.tasks.domain_can_forward')
+        self.mock_domain_can_forward = patch_domain_can_forward.start()
+        self.mock_domain_can_forward.return_value = True
+        self.addCleanup(patch_domain_can_forward.stop)
