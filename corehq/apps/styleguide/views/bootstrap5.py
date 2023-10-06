59ed7cea7982fe3c8799860c60ed8405ab4b2f1a
Biyeun Buczyk <biyeun@dimagi.com>
2023-10-06 14:32:50 +0000

    add colors page in styleguide

--- corehq/apps/styleguide/views/bootstrap5.py
+++ corehq/apps/styleguide/views/bootstrap5.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
-from corehq.apps.styleguide.context import get_navigation_context
+from corehq.apps.styleguide.context import (
+    get_navigation_context,
+    get_interaction_colors,
+    get_neutral_colors,
+)
 
 
 def styleguide_home(request):
@@ -15,3 +19,12 @@ def styleguide_atoms_accessibility(request):
 def styleguide_atoms_typography(request):
     return render(request, 'styleguide/bootstrap5/atoms/typography.html',
                   get_navigation_context("styleguide_atoms_typography_b5"))
+
+
+def styleguide_atoms_colors(request):
+    context = get_navigation_context("styleguide_atoms_colors_b5")
+    context.update({
+        'interaction': get_interaction_colors(),
+        'neutral': get_neutral_colors(),
+    })
+    return render(request, 'styleguide/bootstrap5/atoms/colors.html', context)
