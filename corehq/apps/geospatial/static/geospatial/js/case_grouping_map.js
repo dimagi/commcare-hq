0f5f96e54092d099f7539f6a6fee493db69bfc45
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-10-09 14:46:22 +0200

    create helper function to parse case item array

--- corehq/apps/geospatial/static/geospatial/js/case_grouping_map.js
+++ corehq/apps/geospatial/static/geospatial/js/case_grouping_map.js
@@ -22,6 +22,15 @@ hqDefine("geospatial/js/case_grouping_map",[
     }
 
     $(function () {
+        // Parses a case row (which is an array of column values) to an object, using caseRowOrder as the order of the columns
+        function parseCaseItem(caseItem, caseRowOrder) {
+            let caseObj = {};
+            for (const propKey in caseRowOrder) {
+                const propIndex = caseRowOrder[propKey];
+                caseObj[propKey] = caseItem[propIndex];
+            }
+            return caseObj;
+        }
         $(document).ajaxComplete(function (event, xhr, settings) {
             const isAfterReportLoad = settings.url.includes('geospatial/async/case_grouping_map/');
             if (isAfterReportLoad) {
