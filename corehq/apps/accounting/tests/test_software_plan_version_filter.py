27e7d9f24b231919854313fe8020694b6bc513d3
Jing Cheng <jcheng@dimagi.com>
2023-10-10 17:37:37 -0400

    Improve test

--- corehq/apps/accounting/tests/test_software_plan_version_filter.py
+++ corehq/apps/accounting/tests/test_software_plan_version_filter.py
@@ -6,56 +6,46 @@ from corehq.apps.accounting.models import (SoftwarePlan,
                                            )
 from corehq.apps.accounting.tests.base_tests import BaseAccountingTest
 from django_prbac.models import Role
+import random
 
 
 class SoftwarePlanVersionTest(BaseAccountingTest):
 
     def setUp(self):
-        # Create 2 software plans with different versions
-        software_plan1 = SoftwarePlan.objects.create(
+        self.edition = SoftwarePlanEdition.ADVANCED
+        self.visibility = SoftwarePlanVisibility.PUBLIC
+        # Create 2 software plans
+        self.software_plan1 = SoftwarePlan.objects.create(
             name="Software Plan 1",
             description="A software plan have two versions",
-            edition=SoftwarePlanEdition.ADVANCED,
-            visibility=SoftwarePlanVisibility.PUBLIC,
+            edition=self.edition,
+            visibility=self.visibility,
         )
-        product_rate1 = SoftwareProductRate.objects.create(
-            monthly_fee=1000,
-            name="Advanced Product Rate 1"
-        )
-        self.plan1_version1 = SoftwarePlanVersion.objects.create(
-            plan=software_plan1,
-            product_rate=product_rate1,
-            role=Role.objects.first(),
-        )
-        product_rate2 = SoftwareProductRate.objects.create(
-            monthly_fee=1200,
-            name="Advanced Product Rate 2",
-        )
-        self.plan1_version2 = SoftwarePlanVersion.objects.create(
-            plan=software_plan1,
-            product_rate=product_rate2,
-            role=Role.objects.first(),
-        )
-
-        software_plan2 = SoftwarePlan.objects.create(
+        self.software_plan2 = SoftwarePlan.objects.create(
             name="Software Plan 2",
             description="A software plan have only one version",
-            edition=SoftwarePlanEdition.ADVANCED,
-            visibility=SoftwarePlanVisibility.PUBLIC,
-        )
-        self.plan2_version1 = SoftwarePlanVersion.objects.create(
-            plan=software_plan2,
-            product_rate=product_rate1,
-            role=Role.objects.first(),
+            edition=self.edition,
+            visibility=self.visibility,
         )
 
-    def test_get_most_recent_version(self):
-        edition = SoftwarePlanEdition.ADVANCED
-        visibility = SoftwarePlanVisibility.PUBLIC
+    def test_get_most_recent_version_is_most_recent(self):
+        plan1_old = _create_plan_version(self.software_plan1)
+        plan1_new = _create_plan_version(self.software_plan1)
+        plan2 = _create_plan_version(self.software_plan2)
 
         # Get most recent versions
-        recent_versions = SoftwarePlanVersion.get_most_recent_version(edition, visibility)
+        recent_versions = SoftwarePlanVersion.get_most_recent_version(self.edition, self.visibility)
 
-        self.assertIn(self.plan1_version2, recent_versions)
-        self.assertIn(self.plan2_version1, recent_versions)
-        self.assertNotIn(self.plan1_version1, recent_versions)
+        self.assertIn(plan1_new, recent_versions)
+        self.assertIn(plan2, recent_versions)
+        self.assertNotIn(plan1_old, recent_versions)
+
+
+def _create_plan_version(software_plan):
+    product_rate = SoftwareProductRate.objects.create(
+        monthly_fee=random.randint(1, 5000), name="Product Rate")
+    return SoftwarePlanVersion.objects.create(
+        plan=software_plan,
+        product_rate=product_rate,
+        role=Role.objects.first(),
+    )
