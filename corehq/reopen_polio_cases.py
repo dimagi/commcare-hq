cf4b4e79347fb602a0e676bca96d94822cdf2bec
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-10-10 12:30:36 +0200

    pass case_ids to func + map old->new case ids in logs

--- corehq/reopen_polio_cases.py
+++ corehq/reopen_polio_cases.py
@@ -27,32 +27,36 @@ def batch_split():
             batch_number += 1
 
 # Clone case data by creating new cases containing same data
-def process_chunk(rows):
+def process_chunk(case_ids):
     cases_to_create = []
-    case_ids = [case_row[0] for case_row in rows]
-    processed_case_ids = set()
+    processed_old_case_ids = set()
+    processed_cloned_case_ids = []
     for case_obj in CommCareCase.objects.iter_cases(case_ids, domain=DOMAIN):
+        case_props = case_obj.case_json
+        clone_case_id = uuid.uuid4().hex
         cases_to_create.append(
             CaseBlock(
                 create=True,
-                case_id=uuid.uuid4().hex,
+                case_id=clone_case_id,
                 case_name=case_obj.name,
                 case_type=case_obj.type,
                 date_opened=case_obj.opened_on,
                 owner_id=case_obj.owner_id,
-                update=case_obj.case_json,
+                user_id=case_obj.user_id,
+                update=case_props,
             ).as_text()
         )
-        processed_case_ids.add(case_obj.case_id)
+        processed_cloned_case_ids.append(f'{case_obj.case_id} -> {clone_case_id}')
+        processed_old_case_ids.add(case_obj.case_id)
     submit_case_blocks(cases_to_create, DOMAIN, device_id='system')
     
-    skipped_case_ids = set(case_ids) - processed_case_ids
-    return skipped_case_ids
+    skipped_case_ids = set(case_ids) - processed_old_case_ids
+    return skipped_case_ids, processed_cloned_case_ids
 
 
 def process_chunk_logs(rows, filename, message=''):
     with open(filename, mode='a') as logfile:
-        for case_id, _ in rows:
+        for case_id in rows:
             logfile.write(f'{case_id} - {message}\n')
 
 
@@ -64,19 +68,20 @@ def process_batch():
         total_fail_count = 0
         total_skip_count = 0
         for rows in chunked(reader, CHUNK_SIZE):
+            case_ids = [case_row[0] for case_row in rows]
             start_count = CHUNK_SIZE * (current_chunk - 1)
             print(f"Processing chunk {current_chunk} ({start_count + 1}-{start_count + len(case_ids)})")
             try:
-                skipped_case_ids = process_chunk(rows)
+                skipped_case_ids, processed_case_ids = process_chunk(case_ids)
                 if len(skipped_case_ids):
                     total_skip_count += len(skipped_case_ids)
                     process_chunk_logs(skipped_case_ids, FAILED_FILENAME, message='Case does not exist')
-                process_chunk_logs(rows, SUCCESS_FILENAME)
+                process_chunk_logs(processed_case_ids, SUCCESS_FILENAME)
             except Exception as e:
                 print(f"Failed processing chunk {current_chunk}!")
                 print(str(e))
-                process_chunk_logs(rows, FAILED_FILENAME, message=str(e))
-                total_fail_count += CHUNK_SIZE
+                process_chunk_logs(case_ids, FAILED_FILENAME, message=str(e))
+                total_fail_count += len(case_ids)
             current_chunk += 1
     print("Finished cloning batch of cases.")
     print(f"Failed: {total_fail_count} Skipped: {total_skip_count}")
