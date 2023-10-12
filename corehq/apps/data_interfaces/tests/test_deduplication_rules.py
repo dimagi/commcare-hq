d1a52fcf265a0112853c94ba70058457cadf3c18
Matt Riley <mriley@dimagi.com>
2023-10-12 17:58:32 -0400

    Updated and refactored dedupe edit view tests

--- corehq/apps/data_interfaces/tests/test_deduplication_rules.py
+++ corehq/apps/data_interfaces/tests/test_deduplication_rules.py
@@ -3,7 +3,8 @@ from unittest.mock import Mock, patch
 
 from django.test import RequestFactory, TestCase
 
-from corehq.apps.data_interfaces.models import AutomaticUpdateRule
+from corehq.apps.data_interfaces import views as dedupe_views
+from corehq.apps.data_interfaces.models import AutomaticUpdateRule, CaseDeduplicationActionDefinition
 from corehq.apps.data_interfaces.views import (
     DeduplicationRuleCreateView,
     DeduplicationRuleEditView,
@@ -11,11 +12,12 @@ from corehq.apps.data_interfaces.views import (
 )
 from corehq.apps.es.cases import case_adapter
 from corehq.apps.es.tests.utils import es_test
+from django.contrib.messages import get_messages
 from django.contrib.messages.storage import default_storage
 
 
 @es_test(requires=[case_adapter], setup_class=True)
-@patch('corehq.apps.data_interfaces.views.reverse', Mock(return_value='url'))
+@patch.object(dedupe_views, 'reverse', Mock(return_value='url'))
 class DeduplicationRuleCreateViewTest(TestCase):
 
     def setUp(self):
@@ -32,7 +34,7 @@ class DeduplicationRuleCreateViewTest(TestCase):
                                                workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertFalse(rule.active)
 
-    @patch('corehq.apps.data_interfaces.views.DataInterfaceSection.get')
+    @patch.object(dedupe_views.DataInterfaceSection, 'get')
     def test_creating_rule_with_existing_name_fails(self, *args):
         self._save_dedupe_rule('existing_name', 'test_type')
         existing_rule = AutomaticUpdateRule.objects.get(
@@ -44,9 +46,7 @@ class DeduplicationRuleCreateViewTest(TestCase):
         latest_rule = AutomaticUpdateRule.objects.get(
             domain=self.domain, workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertEqual(existing_rule, latest_rule)
-        # Hacky -- there are cleaner ways to look at messages, but that would
-        # require a valid response object, and we don't have that when mocking out 'GET'
-        self.assertEqual(request._messages._queued_messages[0].message,
+        self.assertEqual(list(get_messages(request))[0].message,
             'Deduplication rule not saved due to the following issues: '
             '<ul><li>A rule with name existing_name already exists</li></ul>')
 
@@ -92,7 +92,7 @@ class DeduplicationRuleCreateViewTest(TestCase):
         self.assertEqual(criteria.property_name, 'name')
         self.assertEqual(criteria.property_value, 'someName')
 
-    @patch('corehq.apps.data_interfaces.views.DataInterfaceSection.get')
+    @patch.object(dedupe_views.DataInterfaceSection, 'get')
     def test_duplicate_matching_parameters_fails(self, *args):
         request = self._create_creation_request(case_properties=['email', 'email'])
 
@@ -101,11 +101,11 @@ class DeduplicationRuleCreateViewTest(TestCase):
         rules = AutomaticUpdateRule.objects.filter(
             domain=self.domain, workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertEqual(0, rules.count())
-        self.assertEqual(request._messages._queued_messages[0].message,
+        self.assertEqual(list(get_messages(request))[0].message,
             'Deduplication rule not saved due to the following issues: '
             '<ul><li>Matching case properties must be unique</li></ul>')
 
-    @patch('corehq.apps.data_interfaces.views.DataInterfaceSection.get')
+    @patch.object(dedupe_views.DataInterfaceSection, 'get')
     def test_duplicate_updating_properties_fails(self, *args):
         request = self._create_creation_request(
             properties_to_update=[
@@ -119,11 +119,11 @@ class DeduplicationRuleCreateViewTest(TestCase):
         rules = AutomaticUpdateRule.objects.filter(
             domain=self.domain, workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertEqual(0, rules.count())
-        self.assertEqual(request._messages._queued_messages[0].message,
+        self.assertEqual(list(get_messages(request))[0].message,
             'Deduplication rule not saved due to the following issues: '
             '<ul><li>Action case properties must be unique</li></ul>')
 
-    @patch('corehq.apps.data_interfaces.views.DataInterfaceSection.get')
+    @patch.object(dedupe_views.DataInterfaceSection, 'get')
     def test_updating_reserved_property_fails(self, *args):
         request = self._create_creation_request(
             properties_to_update=[{'name': 'name', 'valueType': 'EXACT', 'value': 'test'}]
@@ -134,11 +134,11 @@ class DeduplicationRuleCreateViewTest(TestCase):
         rules = AutomaticUpdateRule.objects.filter(
             domain=self.domain, workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertEqual(0, rules.count())
-        self.assertEqual(request._messages._queued_messages[0].message,
+        self.assertEqual(list(get_messages(request))[0].message,
             'Deduplication rule not saved due to the following issues: '
             '<ul><li>You cannot update reserved property: name</li></ul>')
 
-    @patch('corehq.apps.data_interfaces.views.DataInterfaceSection.get')
+    @patch.object(dedupe_views.DataInterfaceSection, 'get')
     def test_updating_match_property_fails(self, *args):
         request = self._create_creation_request(
             case_properties=['email'],
@@ -150,14 +150,14 @@ class DeduplicationRuleCreateViewTest(TestCase):
         rules = AutomaticUpdateRule.objects.filter(
             domain=self.domain, workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE)
         self.assertEqual(0, rules.count())
-        self.assertEqual(request._messages._queued_messages[0].message,
+        self.assertEqual(list(get_messages(request))[0].message,
             'Deduplication rule not saved due to the following issues: '
             '<ul><li>You cannot update properties that are used to match a duplicate.</li></ul>')
 
     def _create_creation_request(self,
         name='TestRule',
         case_type='case',
-        case_properties=["email"],
+        case_properties=['email'],
         match_type='ALL',
         properties_to_update=None,
         match_filter=None,
@@ -165,7 +165,7 @@ class DeduplicationRuleCreateViewTest(TestCase):
         params = {
             'name': name,
             'case_type': case_type,
-            'case_properties': json.dumps([{"name": prop} for prop in case_properties]),
+            'case_properties': json.dumps([{'name': prop} for prop in case_properties]),
             'match_type': match_type,
             'case-filter-property_match_definitions': json.dumps(match_filter or []),
             'case-filter-location_filter_definition': json.dumps([])
@@ -199,54 +199,115 @@ class DeduplicationRuleCreateViewTest(TestCase):
 
 
 @es_test(requires=[case_adapter], setup_class=True)
+@patch.object(dedupe_views, 'reverse', Mock(return_value='url'))
 class DeduplicationRuleEditViewTest(TestCase):
+    def setUp(self):
+        self.domain = 'test-domain-edit'
 
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.domain = 'test-domain-edit'
+    def test_when_rule_edited_doesnt_throw_error(self):
+        rule = self._create_rule()
+        view = self._create_view(rule)
+        request = self._create_update_request(rule, name='UpdatedName')
 
-    @patch('corehq.apps.data_interfaces.views.messages')
-    @patch('corehq.apps.data_interfaces.views.DeduplicationRuleEditView.dedupe_action')
-    @patch('corehq.apps.data_interfaces.views.reverse', Mock(return_value='url'))
-    def test_when_rule_edited_doesnt_throw_error(self, *args):
-        rule_name = 'test_rule'
-        case_type = 'test_type'
-        rule_id = self._save_dummy_rule(rule_name, case_type)
-
-        view = DeduplicationRuleEditView()
-        view.args = []
-        view.kwargs = {"rule_id": rule_id, "domain": self.domain}
-        request = self._create_request(params={
-            'properties_to_update': json.dumps({}),
-            'case_properties': json.dumps({}),
-            'name': rule_name,
-            'case_type': case_type,
-            'match_type': 'ttype',
-            'case-filter-property_match_definitions': json.dumps([]),
-            'case-filter-location_filter_definition': json.dumps([]),
-        })
         resp = view.post(request)
+
         self.assertEqual(HttpResponseRedirect, type(resp))
 
-    def _save_dummy_rule(self, rule_name, case_type):
-        res = AutomaticUpdateRule.objects.create(
-            domain=self.domain,
-            name=rule_name,
-            case_type=case_type,
-            active=False,
-            deleted=False,
-            filter_on_server_modified=False,
-            server_modified_boundary=None,
-            workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE,
-        )
-        return res.id
+    def test_rule_can_update_properties(self):
+        rule = self._create_rule(name='TestRule1')
+        view = self._create_view(rule)
+        request = self._create_update_request(rule, name='TestRule2')
 
-    def _create_request(self, params=None, method='post'):
-        url = 'dummy_url'
-        if method == 'get':
-            request = RequestFactory().get(url, params)
-        else:
-            request = RequestFactory().post(url, params)
+        view.post(request)
+
+        updatedRule = AutomaticUpdateRule.objects.get(id=rule.id)
+        self.assertEqual(updatedRule.name, 'TestRule2')
+
+    def test_cannot_edit_backfilling_rule(self):
+        rule = self._create_rule(name='TestRule1', is_backfilling=True)
+        view = self._create_view(rule)
+        request = self._create_update_request(rule, name='TestRule2')
+
+        view.post(request)
+
+        updatedRule = AutomaticUpdateRule.objects.get(id=rule.id)
+        self.assertEqual(updatedRule.name, 'TestRule1')
+        self.assertEqual(list(get_messages(request))[0].message,
+            'Rule TestRule1 is currently backfilling and cannot be edited')
+
+    @patch.object(dedupe_views, 'reset_and_backfill_deduplicate_rule')
+    def test_updated_rule_starts_backfilling(self, mock_backfill):
+        rule = self._create_rule(name='TestRule1')
+        view = self._create_view(rule)
+        request = self._create_update_request(rule, name='TestRule2')
+
+        view.post(request)
+
+        mock_backfill.assert_called()
+        self.assertEqual(list(get_messages(request))[0].message,
+            'Rule TestRule2 was updated, and has been queued for backfilling')
+
+    def _create_view(self, rule):
+        view = DeduplicationRuleEditView()
+        view.args = []
+        view.kwargs = {'rule_id': rule.id, 'domain': self.domain}
+        return view
+
+    def _create_rule(
+        self,
+        name='TestRule',
+        case_type='case',
+        case_properties=['email'],
+        match_type='ALL',
+        properties_to_update=[],
+        is_backfilling=False,
+    ):
+        rule = AutomaticUpdateRule.objects.create(
+            name=name,
+            domain=self.domain,
+            case_type=case_type,
+            workflow=AutomaticUpdateRule.WORKFLOW_DEDUPLICATE,
+            locked_for_editing=is_backfilling,
+        )
+        rule.add_action(CaseDeduplicationActionDefinition,
+                        case_properties=case_properties,
+                        match_type=match_type,
+                        properties_to_update=properties_to_update)
+
+        return rule
+
+    def _create_update_request(
+        self,
+        rule,
+        name=None,
+        case_type=None,
+        case_properties=None,
+        match_type=None,
+        properties_to_update=None,
+    ):
+        action = CaseDeduplicationActionDefinition.from_rule(rule)
+        params = {
+            'name': name or rule.name,
+            'case_type': case_type or rule.case_type,
+            'match_type': match_type or action.match_type,
+            'case_properties': json.dumps(
+                self._transform_case_properties(case_properties or action.case_properties)),
+            'properties_to_update': json.dumps(
+                self._transform_update_properties(properties_to_update or action.properties_to_update)),
+            'case-filter-property_match_definitions': json.dumps([]),
+            'case-filter-location_filter_definition': json.dumps([])
+        }
+
+        request = RequestFactory().post('dummy_url', params)
         request.domain = self.domain
+        request._messages = default_storage(request)
         return request
+
+    def _transform_case_properties(self, case_properties):
+        return [{'name': prop} for prop in case_properties]
+
+    def _transform_update_properties(self, properties_to_update):
+        return [
+            {'name': prop['name'], 'valueType': prop['value_type'], 'value': prop['value']}
+            for prop in properties_to_update
+        ]
