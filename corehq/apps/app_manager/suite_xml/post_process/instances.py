c5dba28dbe1039bdec6a6e0cf9e1ec89bb255bbc
Jonathan Tang <Jonahtang738@gmail.com>
2023-10-18 15:58:10 -0500

    refactor: improves logic flow

--- corehq/apps/app_manager/suite_xml/post_process/instances.py
+++ corehq/apps/app_manager/suite_xml/post_process/instances.py
@@ -353,14 +353,12 @@ def generic_fixture_instances(app, instance_name):
 
 @register_factory('search-input')
 def search_input_instances(app, instance_name):
-    try:
-        _, query_datum_id = instance_name.split(':', 1)
+    if ":" not in instance_name:
+        return Instance(id=instance_name, src='jr://instance/search-input')  # legacy instance
+
+    _, query_datum_id = instance_name.split(':', 1)
+    if is_valid_results_instance_name(app, query_datum_id):
         src = f'jr://instance/search-input/{query_datum_id}'
-        is_valid_instance_name = is_valid_results_instance_name(app, query_datum_id)
-    except ValueError:
-        src = 'jr://instance/search-input'  # legacy instance
-        is_valid_instance_name = True
-    if is_valid_instance_name:
         return Instance(id=instance_name, src=src)
     return None
 
