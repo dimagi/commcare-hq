76f5580bad6e5266b2d39c4701bf99feaedc168a
Jonathan Tang <Jonahtang738@gmail.com>
2023-10-19 13:45:54 -0500

    tests: update to remove indexed choices

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/query_spec.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/query_spec.js
@@ -12,32 +12,17 @@ hqDefine("cloudcare/js/formplayer/spec/query_spec", function () {
                 keyModel = new QueryViewModel({
                     "itemsetChoicesKey": ["CA", "MA", "FL"],
                     "itemsetChoices": ["California", "Massachusetts", "Florida"],
-                }),
-                indexModel = new QueryViewModel({
-                    "itemsetChoices": ["California", "Massachusetts", "Florida"],
                 });
 
-            let keyViewCollection = new QueryViewCollection([keyModel]),
-                indexViewCollection = new QueryViewCollection([indexModel]);
+            let keyViewCollection = new QueryViewCollection([keyModel])
 
             sinon.stub(Utils, 'getStickyQueryInputs').callsFake(function () { return 'fake_value'; });
             let keyQueryListView =  QueryListView({ collection: keyViewCollection}),
-                keyQueryView = new keyQueryListView.childView({ parentView: keyQueryListView, model: keyModel}),
-                indexQueryListView =  QueryListView({ collection: indexViewCollection}),
-                indexQueryView = new keyQueryListView.childView({ parentView: indexQueryListView, model: indexModel});
+                keyQueryView = new keyQueryListView.childView({ parentView: keyQueryListView, model: keyModel})
 
-            it('should flag if options contains keys for itemset', function () {
-                assert.isTrue(keyQueryListView.selectValuesByKeys);
-                assert.isFalse(indexQueryListView.selectValuesByKeys);
-            });
-
-            it('should create dictionary with either keys if provided. Otherwise, with index as the key', function () {
+            it('should create dictionary with either keys', function () {
                 let expectedKeyItemsetChoicesDict = { "CA": "California", "MA": "Massachusetts", "FL": "Florida"};
                 assert.deepEqual(expectedKeyItemsetChoicesDict, keyQueryView.model.get("itemsetChoicesDict"));
-
-                let expectedIndexItemsetChoicesDict = {"0": "California", "1": "Massachusetts", "2": "Florida"};
-                assert.deepEqual(expectedIndexItemsetChoicesDict, indexQueryView.model.get("itemsetChoicesDict"));
-
             });
         });
     });
