f5cba690e3454e5d33b4812e3a81e364123a6a34
nospame <mapson.evan@gmail.com>
2023-10-19 14:48:10 -0700

    Sort missing values in alignment with empty values

--- corehq/apps/es/case_search.py
+++ corehq/apps/es/case_search.py
@@ -131,11 +131,13 @@ class CaseSearchES(CaseES):
     def sort_by_case_property(self, case_property_name, desc=False, sort_type=None, reset_sort=False):
         sort_filter = filters.term(PROPERTY_KEY, case_property_name)
         if sort_type:
+            sort_missing = '_last' if desc else '_first'
             return self.nested_sort(
                 CASE_PROPERTIES_PATH, "{}.{}".format(VALUE, sort_type),
                 sort_filter,
                 desc,
-                reset_sort
+                reset_sort,
+                sort_missing
             )
 
         return self.nested_sort(
