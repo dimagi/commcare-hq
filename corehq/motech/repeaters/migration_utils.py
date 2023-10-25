4d4d384597341bc3d3cdde6f00cb70e25bcaa131
Graham Herceg <gherceg@dimagi.com>
2023-10-25 17:38:39 -0400

    update tests to actually work
    run on deleted repeaters that are broken as well

--- corehq/motech/repeaters/migration_utils.py
+++ corehq/motech/repeaters/migration_utils.py
@@ -3,22 +3,21 @@ from corehq.motech.repeaters.models import Repeater
 
 def repair_repeaters_with_whitelist_bug():
     """
-    Used in 003_fix_whitelist_bug_repeaters.py
+    Used in 0004_fix_whitelist_bug_repeaters.py
     The whitelist bug resulted in the white_listed_form_xmlns key in a
     repeater's option field storing '[]' as a form id it would whitelist.
     :return: list of repeater ids that were fixed
     """
-    # ignores deleted repeaters
-    all_repeaters = Repeater.objects.all()
+    all_repeaters = Repeater.all_objects.all()
     broken_repeaters = [r for r in all_repeaters if
                         'white_listed_form_xmlns' in r.options and r.options.get(
                             'white_listed_form_xmlns') == ['[]']]
 
-    fixed_repeaters = []
+    fixed_repeater_ids = []
     for repeater in broken_repeaters:
         # reset back to an empty list
         repeater.options['white_listed_form_xmlns'] = []
         repeater.save()
-        fixed_repeaters.append(repeater.repeater_id)
+        fixed_repeater_ids.append(repeater.repeater_id)
 
-    return fixed_repeaters
+    return fixed_repeater_ids
--- corehq/motech/repeaters/tests/test_migration_utils.py
+++ corehq/motech/repeaters/tests/test_migration_utils.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
-from corehq.motech.repeaters.models import Repeater
+from corehq.motech.models import ConnectionSettings
+from corehq.motech.repeaters.models import FormRepeater
 from corehq.motech.repeaters.migration_utils import repair_repeaters_with_whitelist_bug
 
 
@@ -10,41 +11,55 @@ class TestRepairRepeatersWithWhitelistBug(TestCase):
     repeater's option storing '[]' as a form id it would whitelist.
     """
     def test_properly_configured_repeater_is_ignored(self):
-        repeater = Repeater.objects.create(
+        repeater = FormRepeater.objects.create(
             domain='test',
-            repeater_id='abc123',
+            connection_settings=self.connection_settings,
             name='properly-configured-repeater',
             options={'white_listed_form_xmlns': ['abc123']}
         )
 
         fixed_ids = repair_repeaters_with_whitelist_bug()
 
-        self.assertEqual(repeater.options, ['abc123'])
+        refetched_repeater = FormRepeater.objects.get(id=repeater.repeater_id)
+        self.assertEqual(refetched_repeater.options['white_listed_form_xmlns'], ['abc123'])
         self.assertEqual(fixed_ids, [])
 
     def test_impacted_repeater_is_fixed(self):
-        repeater = Repeater.objects.create(
+        repeater = FormRepeater.objects.create(
             domain='test',
-            repeater_id='abc123',
-            name='properly-configured-repeater',
+            connection_settings=self.connection_settings,
+            name='repeater-with-bug',
             options={'white_listed_form_xmlns': ['[]']}
         )
 
         fixed_ids = repair_repeaters_with_whitelist_bug()
 
-        self.assertEqual(repeater.options, [])
+        refetched_repeater = FormRepeater.objects.get(id=repeater.repeater_id)
+        self.assertEqual(refetched_repeater.options['white_listed_form_xmlns'], [])
         self.assertEqual(fixed_ids, [repeater.repeater_id])
 
     def test_impacted_but_deleted_repeater_is_fixed(self):
-        repeater = Repeater.objects.create(
+        repeater = FormRepeater.objects.create(
             domain='test',
-            repeater_id='abc123',
-            name='properly-configured-repeater',
+            connection_settings=self.connection_settings,
+            name='deletd-repeater-with-bug',
             options={'white_listed_form_xmlns': ['[]']},
             is_deleted=True,
         )
 
         fixed_ids = repair_repeaters_with_whitelist_bug()
 
-        self.assertEqual(repeater.options, [])
+        refetched_repeater = FormRepeater.all_objects.get(id=repeater.repeater_id)
+        self.assertEqual(refetched_repeater.options['white_listed_form_xmlns'], [])
         self.assertEqual(fixed_ids, [repeater.repeater_id])
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        conn = ConnectionSettings(
+            domain='test',
+            name="repeaters-bug-settings",
+            url="url",
+        )
+        conn.save()
+        cls.connection_settings = conn
