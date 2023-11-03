1a3d59b8d662ea5d6eb32ecb35c0a32ab798f974
Graham Herceg <gherceg@dimagi.com>
2023-11-03 13:50:47 -0400

    Upgrade django-phonenumber-field to 7.2.0
    - add a comment to the only usage of this dependency to explain why
      it is necessary.
    - add additional test cases and minor renaming for clarity

--- corehq/apps/settings/tests/test_validators.py
+++ corehq/apps/settings/tests/test_validators.py
@@ -4,25 +4,33 @@ from django.test import SimpleTestCase
 from corehq.apps.settings.validators import validate_international_phonenumber
 
 
-class GetTempFileTests(SimpleTestCase):
+class ValidateInternationalPhonenumberTests(SimpleTestCase):
 
-    def test_valid_us_number(self):
+    def test_empty_string_raises_error(self):
+        with self.assertRaises(ValidationError):
+            validate_international_phonenumber('')
+
+    def test_valid_us_number_does_not_raise_error(self):
         validate_international_phonenumber('+16175555555')
 
-    def test_invalid_international_number(self):
+    def test_invalid_us_number_raises_error(self):
         with self.assertRaises(ValidationError):
-            validate_international_phonenumber('6175555555')
+            validate_international_phonenumber('+1617555555')
 
-    def test_valid_south_africa_number(self):
+    def test_valid_south_africa_number_does_not_raise_error(self):
         validate_international_phonenumber('+27623555555')
 
-    def test_invalid_south_africa_number(self):
+    def test_invalid_south_africa_number_raises_error(self):
         with self.assertRaises(ValidationError):
             validate_international_phonenumber('+27555555555')
 
-    def test_valid_india_number(self):
+    def test_valid_india_number_does_not_raise_error(self):
         validate_international_phonenumber('+911125555555')
 
-    def test_invalid_india_number(self):
+    def test_invalid_india_number_raises_error(self):
         with self.assertRaises(ValidationError):
             validate_international_phonenumber('+915555555555')
+
+    def test_invalid_international_number_raises_error(self):
+        with self.assertRaises(ValidationError):
+            validate_international_phonenumber('6175555555')
