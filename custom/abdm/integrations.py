a9ec98fe69c16089fdcd4796f1d3243f08d886a2
Ajeet <ayadav@dimagi.com>
2023-11-05 21:39:17 +0530

    discover patient and care context

--- custom/abdm/integrations.py
+++ custom/abdm/integrations.py
@@ -2,6 +2,7 @@
 from abdm_integrator.integrations import HRPIntegration
 
 from corehq.form_processor.models.cases import CommCareCase
+from custom.abdm.discovery_request import discover_patient_and_care_contexts
 from custom.abdm.fhir.document_bundle import fhir_health_data_from_hq
 
 
@@ -14,3 +15,6 @@ class HRPIntegrationHQ(HRPIntegration):
     def fetch_health_data(self, care_context_reference, health_info_types, linked_care_context_details, **kwargs):
         domain = linked_care_context_details['additional_info']['domain']
         return fhir_health_data_from_hq(care_context_reference, health_info_types, domain)
+
+    def discover_patient_and_care_contexts(self, patient_details, hip_id, **kwargs):
+        return discover_patient_and_care_contexts(patient_details, hip_id)
