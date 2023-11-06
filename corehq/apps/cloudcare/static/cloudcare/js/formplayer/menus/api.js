9f6d7a1471dbafc6687d3c037c6367cc69203e6d
robert-costello <robertcostello81@gmail.com>
2023-11-06 12:40:01 -0500

    remove check for isBackground

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
@@ -47,7 +47,7 @@ hqDefine("cloudcare/js/formplayer/menus/api", function () {
 
                 options = {
                     success: function (parsedMenus, response) {
-                        if (params.isBackground && response.submitResponseMessage) {
+                        if (response.submitResponseMessage) {
                             FormplayerFrontend.trigger('showSuccess', gettext(response.submitResponseMessage));
                         }
                         if (response.status === 'retry') {
