c933dc44eacb59e296879abb49981c1e1e582dcc
Akash Jain <akashjain@work.com>
2023-11-07 15:14:40 +0530

    Modify conditional check

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
@@ -167,7 +167,7 @@ hqDefine("cloudcare/js/formplayer/menus/api", function () {
 
                 var callStartTime = performance.now();
                 menus.fetch($.extend(true, {}, options)).always(function () {
-                    if (data.query_data.results && data.query_data.results.initiated_by === "dynamicSearch") {
+                    if (data.query_data && data.query_data.results && data.query_data.results.initiated_by === "dynamicSearch") {
                         var callEndTime = performance.now();
                         var callResponseTime = callEndTime - callStartTime;
                         $.ajax(initialPageData.reverse('api_histogram_metrics'), {
