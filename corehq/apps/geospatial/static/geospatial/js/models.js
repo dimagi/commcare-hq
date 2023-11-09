f91c331e022407a449cc68affd7750895640e31c
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2023-11-09 07:56:37 +0200

    create map item models

--- corehq/apps/geospatial/static/geospatial/js/models.js
+++ corehq/apps/geospatial/static/geospatial/js/models.js
@@ -20,8 +20,69 @@ hqDefine('geospatial/js/models', [
         self.geoJson = polygon.geo_json;
     };
 
+    var MapItem = function (itemId, itemData, marker, markerColors) {
+        'use strict';
+        var self = this;
+        self.itemId = itemId;
+        self.itemData = itemData;
+        self.marker = marker;
+        self.selectCssId = "select" + itemId;
+        self.isSelected = ko.observable(false);
+        self.markerColors = markerColors;
+
+        self.groupId = null;
+        self.groupCoordinates = null;
+
+        self.setMarkerOpacity = function (opacity) {
+            let element = self.marker.getElement();
+            element.style.opacity = opacity;
+        };
+
+        function changeMarkerColor(selectedCase, newColor) {
+            let marker = selectedCase.marker;
+            let element = marker.getElement();
+            let svg = element.getElementsByTagName("svg")[0];
+            let path = svg.getElementsByTagName("path")[0];
+            path.setAttribute("fill", newColor);
+        }
+
+        self.getItemType = function () {
+            if (self.itemData.type === "user") {
+                return gettext("Mobile Worker");
+            }
+            return gettext("Case");
+        };
+
+        self.isSelected.subscribe(function () {
+            var color = self.isSelected() ? self.markerColors.selected : self.markerColors.default;
+            changeMarkerColor(self, color);
+        });
+    };
+
+    var ClusterMapItem = function (itemId, itemData, link) {
+        let self = this;
+        self.itemId = itemId;
+        self.itemData = itemData;
+        self.link = link;
+        self.groupId = null;
+        self.groupCoordinates = null;
+
+        self.toJson = function () {
+            const coordinates = (self.itemData.coordinates) ? `${self.itemData.coordinates.lng} ${self.itemData.coordinates.lat}` : "";
+            const groupCoordinates = (self.groupCoordinates) ? `${self.groupCoordinates.lng} ${self.groupCoordinates.lat}` : "";
+            return {
+                'groupId': self.groupId,
+                'groupCenterCoordinates': groupCoordinates,
+                'caseId': self.caseId,
+                'coordinates': coordinates,
+            };
+        };
+    };
+
     return {
         MissingGPSModel: MissingGPSModel,
         SavedPolygon: SavedPolygon,
+        MapItem: MapItem,
+        ClusterMapItem: ClusterMapItem,
     };
 });
