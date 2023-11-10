566c2f83da9dd69f495b24bfcf1c56b4718b7116
Charl Smit <csmit@dimagi.com>
2023-11-10 11:12:37 +0200

    Fix linting

--- corehq/apps/geospatial/static/geospatial/js/case_grouping_map.js
+++ corehq/apps/geospatial/static/geospatial/js/case_grouping_map.js
@@ -291,7 +291,7 @@ hqDefine("geospatial/js/case_grouping_map",[
                             "type": "Point",
                             "coordinates": [coordinates.lng, coordinates.lat],
                         },
-                    },
+                    }
                 );
             }
         });
@@ -422,7 +422,7 @@ hqDefine("geospatial/js/case_grouping_map",[
     function uuidv4() {
         // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid/2117523#2117523
         return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16),
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
         );
     }
 
