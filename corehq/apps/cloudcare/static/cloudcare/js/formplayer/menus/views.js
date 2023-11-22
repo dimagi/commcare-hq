08d68489ae3cd53e68e526b387b3fff6e13f75ca
Martin Riese <mriese@dimagi.com>
2023-11-22 07:24:10 -0600

    Border for case tile fields in manual config

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
@@ -146,6 +146,7 @@ hqDefine("cloudcare/js/formplayer/menus/views", function () {
     };
     // generate the case tile's style block and insert
     const buildCellLayout = function (tiles, styles, prefix) {
+        const borderInTile = Boolean(_.find(styles, s => s.showBorder));
         const tileModels = _.chain(tiles || [])
             .map(function (tile, idx) {
                 if (tile === null || tile === undefined) {
@@ -159,6 +160,7 @@ hqDefine("cloudcare/js/formplayer/menus/views", function () {
                     verticalAlign: getValidFieldAlignment(style.verticalAlign),
                     horizontalAlign: getValidFieldAlignment(style.horizontalAlign),
                     showBorder: style.showBorder,
+                    borderInTile: borderInTile,
                 };
             })
             .filter(function (tile) {
