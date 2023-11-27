e872114fd7ef5b7a55af7378100bc9676503cf09
Sravan Reddy <sravfeyn@gmail.com>
2023-11-27 15:32:23 +0530

    pass web_user data

--- corehq/apps/es/tests/test_user_es.py
+++ corehq/apps/es/tests/test_user_es.py
@@ -6,7 +6,7 @@ from corehq.apps.es.users import user_adapter
 from corehq.apps.es.client import manager
 from corehq.apps.es.tests.utils import es_test
 from corehq.apps.users.dbaccessors import delete_all_users
-from corehq.apps.users.models import CommCareUser
+from corehq.apps.users.models import CommCareUser, WebUser
 from corehq.util.es.testing import sync_users_to_es
 
 
@@ -19,6 +19,7 @@ class TestUserES(TestCase):
         cls.domain = 'test-user-es'
         cls.another_domain = 'another-test-user-es'
         cls.domain_obj = create_domain(cls.domain)
+        create_domain(cls.another_domain)
 
         with sync_users_to_es():
             cls._create_mobile_worker('stark',
@@ -30,6 +31,20 @@ class TestUserES(TestCase):
             cls._create_mobile_worker('another_stark',
                 domain=cls.another_domain,
                 user_data={'sigil': 'direwolf', 'seat': 'Winterfell', 'optional': 'ok'})
+            # https://github.com/dimagi/commcare-hq/pull/33688/commits/de687004867a5a37580b74d8b6de6e1ccf430c68
+            web_user = WebUser.create(cls.domain, 'webstark', '***', None, None, timezone="UTC")
+            web_user.add_domain_membership(cls.another_domain)
+            web_user.get_user_data(cls.domain)['start'] = 'some'
+            web_user.get_user_data(cls.another_domain).to_dict()
+            web_user.save()
+            web_user = WebUser.get_by_user_id(web_user.user_id)
+            web_user.get_user_data(cls.another_domain).update(
+                {'sigil': 'direwolf', 'seat': 'Winterfell', 'optional': 'ok'}
+            )
+            web_user.get_user_data(cls.domain).update(
+                {'sigil': 'dragon', 'false_sigil': 'direwolf'}
+            )
+            web_user.save()
         manager.index_refresh(user_adapter.index_name)
 
     @classmethod
@@ -51,19 +66,26 @@ class TestUserES(TestCase):
 
     def test_basic_user_data_query(self):
         direwolf_families = UserES().user_data('sigil', 'direwolf').values_list('username', flat=True)
-        self.assertEqual(direwolf_families, ['stark', 'another_stark'])
+        self.assertEqual(direwolf_families, ['stark', 'another_stark', 'webstark'])
+
+        direwolf_families = UserES().web_users().user_data('sigil', 'direwolf').values_list('username', flat=True)
+        self.assertEqual(direwolf_families, ['webstark'])
 
     def test_user_data_query_with_domain(self):
         direwolf_families = UserES().user_data(
             'sigil', 'direwolf', domain=self.domain).values_list('username', flat=True)
         self.assertEqual(direwolf_families, ['stark'])
+        web_users = UserES().web_users().user_data(
+            'sigil', 'direwolf', domain=self.another_domain).values_list('username', flat=True)
+        self.assertEqual(web_users, ['webstark'])
+
 
     def test_chained_user_data_queries_where_both_match(self):
         direwolf_families = (UserES()
                              .user_data('sigil', 'direwolf', domain=self.another_domain)
                              .user_data('seat', 'Winterfell')
                              .values_list('username', flat=True))
-        self.assertEqual(direwolf_families, ['another_stark'])
+        self.assertEqual(direwolf_families, ['another_stark', 'webstark'])
 
     def test_chained_user_data_queries_with_only_one_match(self):
         direwolf_families = (UserES()
@@ -78,5 +100,5 @@ class TestUserES(TestCase):
                             .values_list('username', flat=True))
         self.assertEqual(
             missing_optional,
-            ['lannister', 'targaryen']
+            ['lannister', 'targaryen', 'webstark']
         )
--- corehq/apps/es/users.py
+++ corehq/apps/es/users.py
@@ -106,7 +106,7 @@ class ElasticUser(ElasticDocumentAdapter):
         user_dict['__group_ids'] = [res.id for res in results]
         user_dict['__group_names'] = [res.name for res in results]
         user_dict['user_data_es'] = []
-        if user_dict.get('base_doc') == 'CouchUser' and user_dict['doc_type'] == 'CommCareUser':
+        if user_dict.get('base_doc') == 'CouchUser':
             user_obj = self.model_cls.wrap_correctly(user_dict)
             for domain in user_obj.domains:
                 user_data = user_obj.get_user_data(domain)
