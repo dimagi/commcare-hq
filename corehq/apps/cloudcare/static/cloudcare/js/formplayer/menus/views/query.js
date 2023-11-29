629e75208a03bf63490dc87a03752963da3743a6
Steph <stephherbers@gmail.com>
2023-11-29 10:42:55 -0500

    Revert "add toggle to flag if domain does not use Automatic Logoff off"
    This reverts commit 5d59652e08c69fa7dc52e3c47c2838858d1906cd.

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views/query.js
@@ -81,11 +81,9 @@ hqDefine("cloudcare/js/formplayer/menus/views/query", function () {
                 kissmetrics.track.event("Accessibility Tracking - Geocoder Interaction in Case Search");
                 model.set('value', item.place_name);
                 initMapboxWidget(model);
-                if (toggles.toggleEnabled('PERSISTENT_GEOCODER_SEARCH_FIELDS')) {
-                    let geocoderValues = JSON.parse(sessionStorage.geocoderValues);
-                    geocoderValues[model.id] = item.place_name;
-                    sessionStorage.geocoderValues = JSON.stringify(geocoderValues);
-                }
+                let geocoderValues = JSON.parse(sessionStorage.geocoderValues);
+                geocoderValues[model.id] = item.place_name;
+                sessionStorage.geocoderValues = JSON.stringify(geocoderValues);
                 var broadcastObj = formEntryUtils.getBroadcastObject(item);
                 $.publish(addressTopic, broadcastObj);
                 return item.place_name;
@@ -176,12 +174,9 @@ hqDefine("cloudcare/js/formplayer/menus/views/query", function () {
                 divEl.css("width", "100%");
             }
 
-
             let geocoderValues = JSON.parse(sessionStorage.geocoderValues);
-            if (geocoderValues[id] && toggles.toggleEnabled('PERSISTENT_GEOCODER_SEARCH_FIELDS')) {
+            if (geocoderValues[id]) {
                 $field.find('.mapboxgl-ctrl-geocoder--input').val(geocoderValues[id]);
-            } else if (model.get('value')) {
-                $field.find('.mapboxgl-ctrl-geocoder--input').val(model.get('value'));
             }
         };
 
