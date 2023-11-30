3ec5aa5f3b30e05a26415f04e501fcf2d8f89691
Chris Smit <chris.smit@dimagi.com>
2023-11-30 16:39:16 +0200

    update test

--- corehq/apps/userreports/tests/test_pillow.py
+++ corehq/apps/userreports/tests/test_pillow.py
@@ -44,7 +44,6 @@ from corehq.apps.userreports.tests.utils import (
 from corehq.apps.userreports.util import get_indicator_adapter
 from corehq.form_processor.models import CommCareCase
 from corehq.form_processor.signals import sql_case_post_save
-from corehq.motech.repeaters.dbaccessors import delete_all_repeat_records
 from corehq.motech.repeaters.models import (
     ConnectionSettings,
     DataSourceRepeater,
@@ -405,7 +404,6 @@ class IndicatorPillowTest(BaseRepeaterTest):
         super(IndicatorPillowTest, cls).tearDownClass()
 
     def tearDown(self):
-        delete_all_repeat_records()
         self.adapter.clear_table()
 
     @flaky_slow
