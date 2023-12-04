24acd2d49977d46aaed18c95a4ba7b092003db1f
Ethan Soergel <esoergel@gmail.com>
2023-12-04 11:44:09 -0500

    reference form_link for clarity

--- corehq/apps/app_manager/tests/views/test_get_form_datums.py
+++ corehq/apps/app_manager/tests/views/test_get_form_datums.py
@@ -5,7 +5,7 @@ from no_exceptions.exceptions import Http400
 
 from corehq.apps.app_manager.tests.app_factory import AppFactory
 from corehq.apps.app_manager.tests.util import get_simple_form
-from corehq.apps.app_manager.views.forms import _get_form_datums
+from corehq.apps.app_manager.views.forms import _get_form_link_datums
 
 
 class TestReleaseBuild(TestCase):
@@ -31,26 +31,26 @@ class TestReleaseBuild(TestCase):
         cls.app.delete()
         super().tearDownClass()
 
-    def test_get_form_datums(self):
-        datums = _get_form_datums(self.domain_name, self.app._id, self.disambiguated_form_id)
+    def test_get_form_link_datums(self):
+        datums = _get_form_link_datums(self.domain_name, self.app._id, self.disambiguated_form_id)
         self.assertEqual(datums, [{'name': 'case_id', 'case_type': 'cheeto'}])
 
-    def test_get_form_datums_wrong_domain(self):
+    def test_get_form_link_datums_wrong_domain(self):
         with self.assertRaises(Http404):
-            _get_form_datums('other', self.app._id, self.disambiguated_form_id)
+            _get_form_link_datums('other', self.app._id, self.disambiguated_form_id)
 
-    def test_get_form_datums_missing_app_id(self):
+    def test_get_form_link_datums_missing_app_id(self):
         with self.assertRaises(Http404):
-            _get_form_datums(self.domain_name, 'missing', self.disambiguated_form_id)
+            _get_form_link_datums(self.domain_name, 'missing', self.disambiguated_form_id)
 
-    def test_get_form_datums_missing_module_id(self):
+    def test_get_form_link_datums_missing_module_id(self):
         with self.assertRaises(Http404):
-            _get_form_datums(self.domain_name, self.app._id, f'missing.{self.form_id}')
+            _get_form_link_datums(self.domain_name, self.app._id, f'missing.{self.form_id}')
 
-    def test_get_form_datums_missing_form_id(self):
+    def test_get_form_link_datums_missing_form_id(self):
         with self.assertRaises(Http404):
-            _get_form_datums(self.domain_name, self.app._id, f'{self.module_id}.missing')
+            _get_form_link_datums(self.domain_name, self.app._id, f'{self.module_id}.missing')
 
-    def test_get_form_datums_missing_bad_form_id(self):
+    def test_get_form_link_datums_missing_bad_form_id(self):
         with self.assertRaises(Http400):
-            _get_form_datums(self.domain_name, self.app._id, 'missing')
+            _get_form_link_datums(self.domain_name, self.app._id, 'missing')
