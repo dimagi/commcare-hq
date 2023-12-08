437a6184f25616b7ca0884770f651abfec1982c0
Charl Smit <csmit@dimagi.com>
2023-12-08 10:17:02 +0200

    Disable activate action when more >= 3 active alerts

--- corehq/apps/domain/static/domain/js/manage_alerts.js
+++ corehq/apps/domain/static/domain/js/manage_alerts.js
@@ -4,11 +4,33 @@ hqDefine("domain/js/manage_alerts",[
     'hqwebapp/js/initial_page_data',
 ], function ($, ko, initialPageData) {
 
+    var alertModel = function (alertData, disabled) {
+        var self = alertData;
+        self.isDisabled = disabled;
+        return self;
+    }
+
     var alertsViewModel = function() {
         var self = {};
 
-        self.alerts = ko.observable(initialPageData.get('alerts'));
+        var totalActiveAlerts = 0
+        initialPageData.get('alerts').forEach(function (alert) {
+            if (alert.active) {
+                totalActiveAlerts += 1;
+            }
+        });
 
+        var alerts = [];
+        initialPageData.get('alerts').forEach(function (alert) {
+            disabled = false;
+            if (totalActiveAlerts >= 3 && !alert.active) {
+                disabled = true;
+            }
+            alerts.push(
+                alertModel(alert, disabled)
+            );
+        });
+        self.alerts = ko.observable(alerts);
         return self;
     }
 
