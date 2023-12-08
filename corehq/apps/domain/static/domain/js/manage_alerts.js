b61a78da3bac9dee80da7de2608f8711bc4b3005
Charl Smit <csmit@dimagi.com>
2023-12-08 09:45:53 +0200

    Convert alerts binding to view model

--- corehq/apps/domain/static/domain/js/manage_alerts.js
+++ corehq/apps/domain/static/domain/js/manage_alerts.js
@@ -1,10 +1,20 @@
 hqDefine("domain/js/manage_alerts",[
     'jquery',
+    'knockout',
     'hqwebapp/js/initial_page_data',
-], function ($, initialPageData) {
+], function ($, ko, initialPageData) {
+
+    var alertsViewModel = function() {
+        var self = {};
+
+        self.alerts = ko.observable(initialPageData.get('alerts'));
+
+        return self;
+    }
+
     $(function () {
-        $('#ko-alert-container').koApplyBindings({
-            'alerts': initialPageData.get('alerts'),
-        });
+        $('#ko-alert-container').koApplyBindings(
+            alertsViewModel()
+        );
     });
 });
