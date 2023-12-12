0b0d35e007344b446f85656bca248ae84f544550
Jonathan Tang <Jonahtang738@gmail.com>
2023-12-12 13:39:48 -0800

    refactor: improve logic readability for validation

--- corehq/apps/app_manager/helpers/validators.py
+++ corehq/apps/app_manager/helpers/validators.py
@@ -538,17 +538,15 @@ class ModuleBaseValidator(object):
                             "module": self.get_module_info(),
                             "details": search_config.get_instance_name()
                         }
-
-            contains_ungrouped_properties = any(not prop.group_key for prop in search_config.properties)
-            contains_grouping_property = any(prop.is_group for prop in search_config.properties)
-            if contains_ungrouped_properties and contains_grouping_property:
-                for prop in search_config.properties:
-                    if not prop.group_key:
-                        yield {
-                            "type": "invalid grouping from ungrouped search property",
-                            "module": self.get_module_info(),
-                            "property": prop.name,
-                        }
+            module_contains_grouping_property = any(prop.is_group for prop in search_config.properties)
+            if module_contains_grouping_property:
+                ungrouped_properties = [prop for prop in search_config.properties if not prop.group_key]
+                for prop in ungrouped_properties:
+                    yield {
+                        "type": "invalid grouping from ungrouped search property",
+                        "module": self.get_module_info(),
+                        "property": prop.name,
+                    }
 
     def validate_case_list_field_actions(self):
         if hasattr(self.module, 'case_details'):
