8f225018316a2b5d31b44ba554cd6da37c7af334
robert-costello <robertcostello81@gmail.com>
2023-12-12 15:53:18 -0500

    default content-container to full width

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/app.js
@@ -47,11 +47,9 @@ hqDefine("cloudcare/js/formplayer/app", function () {
         FormplayerFrontend.regions = new RegionContainer();
         let sidebar = FormplayerFrontend.regions.getRegion('sidebar');
         sidebar.on('show', function () {
-            $('#content-container').addClass('full-width');
             $('#menu-region').addClass('sidebar-push');
         });
         sidebar.on('hide empty', function () {
-            $('#content-container').removeClass('full-width');
             $('#menu-region').removeClass('sidebar-push');
         });
 
