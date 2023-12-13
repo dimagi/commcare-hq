c2a5479a97a1e00d0ed20cc9a4b482e3fe0ebad1
Biyeun Buczyk <biyeun@dimagi.com>
2023-12-13 15:56:54 -0500

    add migration guide

--- corehq/apps/styleguide/context.py
+++ corehq/apps/styleguide/context.py
@@ -19,6 +19,7 @@ def get_navigation_context(current_page):
                 pages=[
                     Page("Introduction", 'styleguide_home_b5'),
                     Page("Code Guidelines", 'styleguide_code_guidelines_b5'),
+                    Page("Migration Guide", 'styleguide_migration_guide_b5'),
                 ],
             ),
             NavigationGroup(
