1c39777f03a0d1cf04029a1a05779c7864f43015
Martin Riese <mriese@dimagi.com>
2023-12-18 12:28:15 -0600

    fix query_spec.js
    * Calling QueryListView in the describe block caused an error that did not get caught
    * Move it into the before block
    * Replace let with const where appropriate

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/query_spec.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/query_spec.js
@@ -4,24 +4,30 @@ hqDefine("cloudcare/js/formplayer/spec/query_spec", function () {
     describe('Query', function () {
 
         describe('itemset', function () {
-            let QueryListView = hqImport("cloudcare/js/formplayer/menus/views/query"),
-                Utils = hqImport("cloudcare/js/formplayer/utils/utils");
 
-            let QueryViewModel = Backbone.Model.extend(),
-                QueryViewCollection = Backbone.Collection.extend(),
-                keyModel = new QueryViewModel({
+            let keyQueryView;
+
+            before(function () {
+                const QueryListView = hqImport("cloudcare/js/formplayer/menus/views/query");
+                const Utils = hqImport("cloudcare/js/formplayer/utils/utils");
+
+                const QueryViewModel = Backbone.Model.extend();
+                const QueryViewCollection = Backbone.Collection.extend();
+                const keyModel = new QueryViewModel({
                     "itemsetChoicesKey": ["CA", "MA", "FL"],
                     "itemsetChoices": ["California", "Massachusetts", "Florida"],
                 });
 
-            let keyViewCollection = new QueryViewCollection([keyModel]);
+                const keyViewCollection = new QueryViewCollection([keyModel]);
 
-            sinon.stub(Utils, 'getStickyQueryInputs').callsFake(function () { return 'fake_value'; });
-            let keyQueryListView =  QueryListView({ collection: keyViewCollection}),
+                sinon.stub(Utils, 'getStickyQueryInputs').callsFake(function () { return 'fake_value'; });
+
+                const keyQueryListView = QueryListView({ collection: keyViewCollection});
                 keyQueryView = new keyQueryListView.childView({ parentView: keyQueryListView, model: keyModel});
+            });
 
             it('should create dictionary with either keys', function () {
-                let expectedKeyItemsetChoicesDict = { "CA": "California", "MA": "Massachusetts", "FL": "Florida"};
+                const expectedKeyItemsetChoicesDict = { "CA": "California", "MA": "Massachusetts", "FL": "Florida"};
                 assert.deepEqual(expectedKeyItemsetChoicesDict, keyQueryView.model.get("itemsetChoicesDict"));
             });
         });
