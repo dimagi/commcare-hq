9b9f4309ff65e067e863342415584f09d3d83ec6
Steph <stephherbers@gmail.com>
2023-12-18 20:11:47 -0500

    modify logic on upload to save the new submit_label to the form

--- corehq/apps/translations/app_translations/upload_form.py
+++ corehq/apps/translations/app_translations/upload_form.py
@@ -39,7 +39,6 @@ class BulkAppTranslationFormUpdater(BulkAppTranslationUpdater):
         # These attributes get populated by update
         self.markdowns = None
         self.markdown_vetoes = None
-        self.submit_label = None
 
     def _get_xform(self):
         if not isinstance(self.form, ShadowForm) and self.form.source:
@@ -86,9 +85,6 @@ class BulkAppTranslationFormUpdater(BulkAppTranslationUpdater):
         # Skip labels that have no translation provided
         label_ids_to_skip = self._get_label_ids_to_skip(rows)
 
-        if self.submit_label:
-            self._update_translation(self.submit_label, self.form.submit_label)
-
         # Update the translations
         for lang in self.langs:
             translation_node = self.itext.find("./{f}translation[@lang='%s']" % lang)
@@ -96,6 +92,12 @@ class BulkAppTranslationFormUpdater(BulkAppTranslationUpdater):
 
             for row in rows:
                 if row['label'] in label_ids_to_skip:
+                    continue
+                if row['label'] == 'submit_label':
+                    try:
+                        self.form.submit_label[lang] = row[self._get_col_key('default', lang)]
+                    except KeyError:
+                        pass
                     continue
                 try:
                     self._add_or_remove_translations(lang, row)
