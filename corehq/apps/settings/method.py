8594ad47c917085903aae8b6c284aa7f1fff5e09
Graham Herceg <gherceg@dimagi.com>
2023-12-27 09:33:32 -0500

    Fix LoginView

--- corehq/apps/settings/method.py
+++ corehq/apps/settings/method.py
@@ -1,3 +1,5 @@
+from django_otp.plugins.otp_totp.models import TOTPDevice
+
 from two_factor.plugins.phonenumber.method import PhoneCallMethod, SMSMethod
 from two_factor.plugins.registry import GeneratorMethod
 
@@ -11,6 +13,13 @@ class HQGeneratorMethod(GeneratorMethod):
     def get_setup_forms(self, *args):
         return {self.code: HQTOTPDeviceForm}
 
+    def get_token_form_class(self):
+        from corehq.apps.hqwebapp.forms import HQAuthenticationTokenForm
+        return HQAuthenticationTokenForm
+
+    def recognize_device(self, device):
+        return isinstance(device, TOTPDevice)
+
 
 class HQPhoneCallMethod(PhoneCallMethod):
 
@@ -19,6 +28,10 @@ class HQPhoneCallMethod(PhoneCallMethod):
     def get_setup_forms(self, *args):
         return {self.code: HQPhoneNumberForm}
 
+    def get_token_form_class(self):
+        from corehq.apps.hqwebapp.forms import HQAuthenticationTokenForm
+        return HQAuthenticationTokenForm
+
 
 class HQSMSMethod(SMSMethod):
 
@@ -26,3 +39,7 @@ class HQSMSMethod(SMSMethod):
 
     def get_setup_forms(self, *args):
         return {self.code: HQPhoneNumberForm}
+
+    def get_token_form_class(self):
+        from corehq.apps.hqwebapp.forms import HQAuthenticationTokenForm
+        return HQAuthenticationTokenForm
