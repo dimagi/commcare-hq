25f160925f811738194bd8f1bfb8731163eb2b23
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2024-01-03 13:28:11 +0200

    change where case type name validation is done

--- corehq/apps/data_dictionary/util.py
+++ corehq/apps/data_dictionary/util.py
@@ -210,9 +210,6 @@ def save_case_property(name, case_type, domain=None, data_type=None,
     if not is_case_type_or_prop_name_valid(name):
         return gettext('Invalid case property name. It should start with a letter, and only contain letters, '
                        'numbers, "-", and "_"')
-    if not is_case_type_or_prop_name_valid(case_type):
-        return gettext('Invalid case type name. It should start with a letter, and only contain letters, '
-                       'numbers, "-", and "_"')
 
     try:
         prop = CaseProperty.get_or_create(
