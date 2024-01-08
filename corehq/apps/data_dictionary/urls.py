6cb1ff8eb3c2e65b796b975d254aa3547ffa59c7
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2024-01-08 10:49:19 +0200

    lint fix

--- corehq/apps/data_dictionary/urls.py
+++ corehq/apps/data_dictionary/urls.py
@@ -19,8 +19,10 @@ urlpatterns = [
     url(r"^deprecate_or_restore_case_type/(?P<case_type_name>[\w-]+)/$", deprecate_or_restore_case_type,
         name='deprecate_or_restore_case_type'),
     url(r"^update_case_property/$", update_case_property, name='update_case_property'),
-    url(r"^update_case_property_description/$", update_case_property_description, name='update_property_description'),
+    url(r"^update_case_property_description/$", update_case_property_description,
+        name='update_property_description'),
     url(r"^export/$", ExportDataDictionaryView.as_view(), name=ExportDataDictionaryView.urlname),
     url(r"^$", DataDictionaryView.as_view(), name=DataDictionaryView.urlname),
-    url(r"^import$", waf_allow('XSS_BODY')(UploadDataDictionaryView.as_view()), name=UploadDataDictionaryView.urlname),
+    url(r"^import$", waf_allow('XSS_BODY')(UploadDataDictionaryView.as_view()),
+        name=UploadDataDictionaryView.urlname),
 ]
