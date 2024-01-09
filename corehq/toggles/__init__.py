296595110c7d048fd3e1b70b71f6e6e2f95002d2
Charl Smit <csmit@dimagi.com>
2024-01-09 10:18:20 +0200

    Add feature flag

--- corehq/toggles/__init__.py
+++ corehq/toggles/__init__.py
@@ -2708,3 +2708,11 @@ USE_LOGO_IN_SYSTEM_EMAILS = StaticToggle(
     namespaces=[NAMESPACE_DOMAIN],
     description='The project logo replaces the CommCare logo.',
 )
+
+SUPPORT_GEO_JSON_EXPORT = StaticToggle(
+    slug='support_geo_json_export',
+    label='Support geo json export in Case Exporter',
+    tag=TAG_SOLUTIONS_CONDITIONAL,
+    namespaces=[NAMESPACE_DOMAIN],
+    description='The Case Export page now supports the exporting of geo json data.',
+)
