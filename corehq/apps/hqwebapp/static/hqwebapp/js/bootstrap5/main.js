06f4418e342e0f95a233ed87f70e37c93cb52682
Jennifer Schweers <orange.jenny@gmail.com>
2024-01-11 15:39:29 -0500

    Linted

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/main.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/main.js
@@ -87,9 +87,9 @@ hqDefine('hqwebapp/js/bootstrap5/main', [
     ko.bindingHandlers.runOnInit = {
         // suggestion from https://github.com/knockout/knockout/issues/2446 to use
         // instead of an anonymous template
-        init: function(elem, valueAccessor) {
+        init: function (elem, valueAccessor) {
             valueAccessor();
-        }
+        },
     };
     ko.virtualElements.allowedBindings.runOnInit = true;
 
@@ -97,11 +97,11 @@ hqDefine('hqwebapp/js/bootstrap5/main', [
         // fixes an issue where we try to apply bindings to a parent element
         // that has a child element with existing bindings.
         // see: https://github.com/knockout/knockout/issues/1922
-        init: function(elem, valueAccessor) {
+        init: function (elem, valueAccessor) {
             // Let bindings proceed as normal *only if* my value is false
             var shouldAllowBindings = ko.unwrap(valueAccessor());
             return { controlsDescendantBindings: !shouldAllowBindings };
-        }
+        },
     };
     ko.virtualElements.allowedBindings.allowDescendantBindings = true;
 
