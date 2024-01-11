2543f363fb65820a1cf26a038e5b90e73aae1c56
Graham Herceg <gherceg@dimagi.com>
2024-01-11 14:13:08 -0500

    Move two_factor registry import to top to conform with PEP8
    two_factor_methods.py needs to be imported locally, otherwise
    that file attempts to import django_otp which for some reason
    is not ready, despite being defined before hqwebapp.

--- corehq/apps/hqwebapp/apps.py
+++ corehq/apps/hqwebapp/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+from two_factor.plugins.registry import registry
+
 
 class HqWebAppConfig(AppConfig):
     name = 'corehq.apps.hqwebapp'
@@ -8,13 +10,12 @@ class HqWebAppConfig(AppConfig):
     def ready(self):
         # Ensure the login signal handlers have been loaded
         from . import login_handlers, signals  # noqa
+
         # custom 2FA methods need to be registered on startup
         register_two_factor_methods()
 
 
 def register_two_factor_methods():
-    from two_factor.plugins.registry import registry
-
     from corehq.apps.hqwebapp.two_factor_methods import (
         HQGeneratorMethod,
         HQPhoneCallMethod,
