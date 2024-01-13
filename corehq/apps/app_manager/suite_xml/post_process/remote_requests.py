d52cc65e806b48f7bae9d51b4a4eac3444b315fe
Jonathan Tang <Jonahtang738@gmail.com>
2024-01-12 18:34:02 -0800

    feat: adds attribute to suite

--- corehq/apps/app_manager/suite_xml/post_process/remote_requests.py
+++ corehq/apps/app_manager/suite_xml/post_process/remote_requests.py
@@ -219,6 +219,9 @@ class RemoteRequestFactory(object):
             "default_search": self.module.search_config.default_search,
             "dynamic_search": self.app.split_screen_dynamic_search and not self.module.is_auto_select()
         }
+        if self.module.search_config.search_on_clear:
+            kwargs["search_on_clear"] = (self.module.search_config.search_on_clear
+                and not self.module.is_auto_select())
         return [
             RemoteRequestQuery(**kwargs)
         ]
