7e87d8368ba86113b1c486a1866906dcde5b8540
Jonathan Tang <Jonahtang738@gmail.com>
2024-01-16 15:40:17 -0800

    adds validation for incompatibility of autoselect with search on clear
    when both are configured, it will cause an infinite rerunning of searches in a loop

--- corehq/apps/app_manager/helpers/validators.py
+++ corehq/apps/app_manager/helpers/validators.py
@@ -545,6 +545,11 @@ class ModuleBaseValidator(object):
                         "module": self.get_module_info(),
                         "property": prop.name,
                     }
+            if search_config.search_on_clear and self.module.is_auto_select():
+                yield {
+                    "type": "search on clear with auto select",
+                    "module": self.get_module_info(),
+                }
 
     def validate_case_list_field_actions(self):
         if hasattr(self.module, 'case_details'):
