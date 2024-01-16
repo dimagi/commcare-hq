c5508237d3f82f144de468d3abb57590eba89224
robert-costello <robertcostello81@gmail.com>
2024-01-16 12:08:38 -0500

    lint

--- corehq/apps/app_manager/tests/app_factory.py
+++ corehq/apps/app_manager/tests/app_factory.py
@@ -173,7 +173,10 @@ class AppFactory(object):
                 if not parent_tag:
                     parent_tag = form.actions.load_update_cases[-1].case_tag
 
-                action.case_indices = [CaseIndex(tag=parent_tag, relationship='extension' if is_extension else 'child')]
+                action.case_indices = [CaseIndex(
+                    tag=parent_tag,
+                    relationship='extension' if is_extension else 'child'
+                )]
 
             form.actions.open_cases.append(action)
 
