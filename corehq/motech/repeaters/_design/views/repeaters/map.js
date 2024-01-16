778efb9a8dcaa8beb38cb53644762f206b642f14
Daniel Miller <dmiller@dimagi.com>
2024-01-16 15:47:50 -0500

    Remove unused repeaters Couch views

--- corehq/motech/repeaters/_design/views/repeaters/map.js
+++
@@ -1,6 +0,0 @@
-function(doc){
-    // grandfather in old FormRepeater docs
-    if(doc.base_doc === "Repeater" || doc.doc_type === "FormRepeater") {
-        emit([doc.domain, doc.doc_type], null);
-    }
-}
