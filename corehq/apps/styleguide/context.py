4c792de57c4583453b5c9ccf321d6fda5d3d1bd8
Biyeun Buczyk <biyeun@dimagi.com>
2024-01-23 14:32:03 +0100

    add dates and times section to styleguide

--- corehq/apps/styleguide/context.py
+++ corehq/apps/styleguide/context.py
@@ -42,6 +42,7 @@ def get_navigation_context(current_page):
                     Page("Searching & Filtering", 'styleguide_molecules_searching_b5'),
                     Page("Inline Editing", 'styleguide_molecules_inline_editing_b5'),
                     Page("Feedback", 'styleguide_molecules_feedback_b5'),
+                    Page("Dates & Times", 'styleguide_molecules_dates_times_b5'),
                 ],
             ),
             NavigationGroup(
