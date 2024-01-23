5951c16cde5aaf87b2929c7f01bbd3e645092568
Biyeun Buczyk <biyeun@dimagi.com>
2024-01-23 16:23:54 +0100

    update reference of datepicker to renamed datetimepicker

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/widgets.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/widgets.js
@@ -4,7 +4,7 @@ hqDefine("hqwebapp/js/bootstrap5/widgets",[
     '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min',
     'hqwebapp/js/initial_page_data',
     'select2/dist/js/select2.full.min',
-    'datepicker',  // jquery plugin for tempusDominus
+    'datetimepicker',  // jquery plugin for tempusDominus
 ], function ($, _, MapboxGeocoder, initialPageData) {
     var init = function () {
         var MAPBOX_ACCESS_TOKEN = initialPageData.get(
