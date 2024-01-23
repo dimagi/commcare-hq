a5d83bccd31ab92b9d8b9c880012f58d5851fc0d
Graham Herceg <gherceg@dimagi.com>
2024-01-22 19:04:30 -0500

    Fix failing JSONFieldSerialization test

--- corehq/apps/dump_reload/tests/test_serialization.py
+++ corehq/apps/dump_reload/tests/test_serialization.py
@@ -3,19 +3,22 @@ from django.test import SimpleTestCase
 
 
 class TestJSONFieldSerialization(SimpleTestCase):
-    """See https://github.com/bradjasper/django-jsonfield/pull/173"""
+    """
+    See https://github.com/bradjasper/django-jsonfield/pull/173
+    We just need to test that a model that uses jsonfield.JSONField is serialized correctly
+    """
 
     def test(self):
         serialized_model_with_primary_key = {
-            'model': 'form_processor.XFormInstance', 'pk': 1, 'fields': {'auth_context': '{}'}
+            'model': 'accounting.BillingContactInfo', 'pk': 1, 'fields': {'email_list': '{}'}
         }
         serialized_model_with_natural_key = {
-            'model': 'form_processor.XFormInstance', 'fields': {'auth_context': '{}'}
+            'model': 'accounting.BillingContactInfo', 'fields': {'email_list': '{}'}
         }
 
         def _test_json_field_after_serialization(serialized):
             for obj in Deserializer([serialized]):
-                self.assertIsInstance(obj.object.auth_context, dict)
+                self.assertIsInstance(obj.object.email_list, dict)
 
         _test_json_field_after_serialization(serialized_model_with_primary_key)
         _test_json_field_after_serialization(serialized_model_with_natural_key)
