2e000fa6c36487c3c24768c49ff0da9ed17e339f
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2024-01-26 09:23:08 +0200

    add model variables

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/email-request.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/email-request.js
@@ -10,6 +10,25 @@ hqDefine('hqwebapp/js/bootstrap3/email-request', [
 
         self.$element = $(`#${modalId}`);
         self.$formElement = $(`#${formId}`);
+        self.$submitBtn = self.$formElement.find("button[type='submit']");
+
+        self.subjectText = ko.observable('');
+        self.descriptionText = ko.observable('');
+        self.emailInput = ko.observable('');
+        self.recipientEmailsText = ko.observable('');
+
+        self.subjectHasFocus = ko.observable(false);
+        self.cancelBtnEnabled = ko.observable(true);
+
+        self.hasSubmitError = ko.observable(false);
+        self.hasSubjectError = ko.observable(false);
+        self.hasEmailInputError = ko.observable(false);
+        self.hasOtherEmailInputError = ko.observable(false);
+        self.hasRecipientsInputError = ko.observable(false);
+
+        self.isRequestReportSubmitting = false;
+
+        self.reportUrl = ko.observable('');
 
         return self;
     };
