35cceca0082d6293e38157500940d6621824e5ec
Jennifer Schweers <orange.jenny@gmail.com>
2024-01-31 13:32:57 -0500

    Fixed lint errors

--- corehq/apps/locations/static/locations/spec/types_spec.js
+++ corehq/apps/locations/static/locations/spec/types_spec.js
@@ -13,8 +13,8 @@ hqDefine("locations/spec/types_spec", [
         var locationSettingsViewModel = locationsTypes.locationSettingsViewModel,
             locationTypeModel = locationsTypes.locationTypeModel;
 
-        var extract_name = function (loc_type) {
-            return loc_type.name();
+        var extractName = function (locType) {
+            return locType.name();
         };
 
         describe('Linear Hierarchy', function () {
@@ -31,7 +31,7 @@ hqDefine("locations/spec/types_spec", [
                 this.supervisor_model = locationTypeModel(data.supervisor, false, this.view_model);
             });
 
-            var make_cycle = function () {
+            var makeCycle = function () {
                 this.location_types[0].parent_type = data.block.pk; // state.parent_type = block
                 this.state_model.parent_type(this.block_model.pk);
                 this.view_model = locationSettingsViewModel(this.location_types, false);
@@ -44,29 +44,29 @@ hqDefine("locations/spec/types_spec", [
 
             describe('expand_from_options', function () {
                 it('Provides all levels down to the current one, including root', function () {
-                    var returned_loc_types = _.map(
+                    var returnedLocTypes = _.map(
                             this.block_model.expand_from_options(),
-                            extract_name
+                            extractName
                         ),
-                        desired_loc_types_returned = _.map([this.state_model, this.district_model], extract_name);
-                    desired_loc_types_returned.push('root');
-                    assert.sameMembers(desired_loc_types_returned, returned_loc_types);
+                        desiredLocTypesReturned = _.map([this.state_model, this.district_model], extractName);
+                    desiredLocTypesReturned.push('root');
+                    assert.sameMembers(desiredLocTypesReturned, returnedLocTypes);
                 });
 
                 it('Returns only root if there are cycles', function () {
-                    make_cycle.call(this);
-                    var returned_loc_types = _.map(this.state_model.expand_from_options(), extract_name);
+                    makeCycle.call(this);
+                    var returnedLocTypes = _.map(this.state_model.expand_from_options(), extractName);
                     assert(this.view_model.has_cycles());
-                    assert.sameMembers(['root'], returned_loc_types);
+                    assert.sameMembers(['root'], returnedLocTypes);
                 });
             });
 
             describe('expand_to_options', function () {
                 it('Provides all levels beneath the current one', function () {
-                    var returned_loc_types = _.map(this.district_model.expand_to_options().children, extract_name),
-                        desired_loc_types_returned = _.map([this.district_model, this.block_model], extract_name);
+                    var returnedLocTypes = _.map(this.district_model.expand_to_options().children, extractName),
+                        desiredLocTypesReturned = _.map([this.district_model, this.block_model], extractName);
                     assert.isFalse(this.view_model.has_cycles());
-                    assert.sameMembers(desired_loc_types_returned, returned_loc_types);
+                    assert.sameMembers(desiredLocTypesReturned, returnedLocTypes);
                 });
 
                 it('Returns the outermost leaf', function () {
@@ -75,31 +75,31 @@ hqDefine("locations/spec/types_spec", [
                 });
 
                 it('Returns empty if there are cycles', function () {
-                    make_cycle.call(this);
-                    var returned_loc_types = _.map(this.state_model.expand_to_options().children, extract_name);
+                    makeCycle.call(this);
+                    var returnedLocTypes = _.map(this.state_model.expand_to_options().children, extractName);
                     assert(this.view_model.has_cycles());
-                    assert.sameMembers([], returned_loc_types);
+                    assert.sameMembers([], returnedLocTypes);
                 });
 
             });
 
             describe('include_without_expanding_options', function () {
                 it('Provides all levels', function () {
-                    var returned_loc_types = _.map(
+                    var returnedLocTypes = _.map(
                             this.block_model.include_without_expanding_options(),
-                            extract_name
+                            extractName
                         ),
-                        desired_loc_types_returned = _.map([this.state_model, this.district_model, this.block_model, this.supervisor_model], extract_name);
-                    assert.sameMembers(desired_loc_types_returned, returned_loc_types);
+                        desiredLocTypesReturned = _.map([this.state_model, this.district_model, this.block_model, this.supervisor_model], extractName);
+                    assert.sameMembers(desiredLocTypesReturned, returnedLocTypes);
                 });
 
                 it('Provides nothing if expand from is root', function () {
                     this.block_model.expand_from(-1);
-                    var returned_loc_types = _.map(
+                    var returnedLocTypes = _.map(
                         this.block_model.include_without_expanding_options(),
-                        extract_name
+                        extractName
                     );
-                    assert.equal(0, returned_loc_types.length);
+                    assert.equal(0, returnedLocTypes.length);
                 });
             });
         });
@@ -121,15 +121,15 @@ hqDefine("locations/spec/types_spec", [
 
             describe('expand_to_options', function () {
                 it('shows when types are at the same level', function () {
-                    var returned_loc_types = this.state_model.expand_to_options(),
-                        desired_children_returned = ["state", "county | region"],
-                        desired_leaf_returned = "city | town";
+                    var returnedLocTypes = this.state_model.expand_to_options(),
+                        desiredChildrenReturned = ["state", "county | region"],
+                        desiredLeafReturned = "city | town";
 
                     assert.isFalse(this.view_model.has_cycles());
-                    assert.sameMembers(desired_children_returned, _.map(
-                        returned_loc_types.children, extract_name
+                    assert.sameMembers(desiredChildrenReturned, _.map(
+                        returnedLocTypes.children, extractName
                     ));
-                    assert.equal(desired_leaf_returned, returned_loc_types.leaf.name());
+                    assert.equal(desiredLeafReturned, returnedLocTypes.leaf.name());
                 });
 
                 it('calculates the correct level', function () {
@@ -140,35 +140,35 @@ hqDefine("locations/spec/types_spec", [
 
                 it('shows correct levels when expand_from is above current fork', function () {
                     this.city_model.expand_from(this.state_model.pk);
-                    var returned_loc_types = this.city_model.expand_to_options(),
-                        desired_children_returned = ["state", "county | region"],
-                        desired_leaf_returned = "city | town";
-                    assert.sameMembers(desired_children_returned, _.map(
-                        returned_loc_types.children, extract_name
+                    var returnedLocTypes = this.city_model.expand_to_options(),
+                        desiredChildrenReturned = ["state", "county | region"],
+                        desiredLeafReturned = "city | town";
+                    assert.sameMembers(desiredChildrenReturned, _.map(
+                        returnedLocTypes.children, extractName
                     ));
-                    assert.equal(desired_leaf_returned, returned_loc_types.leaf.name());
+                    assert.equal(desiredLeafReturned, returnedLocTypes.leaf.name());
                 });
 
                 it('shows all levels when expand_from is root', function () {
                     this.city_model.expand_from(-1);
-                    var returned_loc_types = this.city_model.expand_to_options(),
-                        desired_children_returned = ['state', 'county | region'],
-                        desired_leaf_returned = "city | town";
-                    assert.sameMembers(desired_children_returned, _.map(
-                        returned_loc_types.children, extract_name
+                    var returnedLocTypes = this.city_model.expand_to_options(),
+                        desiredChildrenReturned = ['state', 'county | region'],
+                        desiredLeafReturned = "city | town";
+                    assert.sameMembers(desiredChildrenReturned, _.map(
+                        returnedLocTypes.children, extractName
                     ));
-                    assert.equal(desired_leaf_returned, returned_loc_types.leaf.name());
+                    assert.equal(desiredLeafReturned, returnedLocTypes.leaf.name());
                 });
             });
 
             describe('include_without_expanding_options', function () {
                 it('Provides all levels', function () {
-                    var returned_loc_types = _.map(
+                    var returnedLocTypes = _.map(
                             this.region_model.include_without_expanding_options(),
-                            extract_name
+                            extractName
                         ),
-                        desired_loc_types_returned = ['state', 'county | region', 'city | town'];
-                    assert.sameMembers(desired_loc_types_returned, returned_loc_types);
+                        desiredLocTypesReturned = ['state', 'county | region', 'city | town'];
+                    assert.sameMembers(desiredLocTypesReturned, returnedLocTypes);
                 });
             });
         });
