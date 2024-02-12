17db02c60c949f5b63fe8ab531485283689f39d3
Charl Smit <csmit@dimagi.com>
2024-02-12 11:37:57 +0200

    Remove redundant if statement

--- corehq/apps/geospatial/forms.py
+++ corehq/apps/geospatial/forms.py
@@ -211,6 +211,5 @@ class GeospatialConfigForm(forms.ModelForm):
         return cleaned_data
 
     def save(self, commit=True):
-        if self.cleaned_data.get('plaintext_api_token'):
-            self.instance.plaintext_api_token = self.cleaned_data.get('plaintext_api_token')
+        self.instance.plaintext_api_token = self.cleaned_data.get('plaintext_api_token')
         return super().save(commit)
