19a144da109c7b496f699793a5dbf60a0b0bcea6
Minha <mlee@dimagi.com>
2024-02-22 17:42:00 -0500

    Add model

--- corehq/apps/cleanup/models.py
+++ corehq/apps/cleanup/models.py
@@ -10,3 +10,17 @@ class DeletedCouchDoc(models.Model):
         constraints = [
             models.UniqueConstraint(fields=['doc_id', 'doc_type'], name='deletedcouchdoc_unique_id_and_type')
         ]
+
+
+class DeletedSQLDoc(models.Model):
+    doc_id = models.CharField(max_length=126)
+    doc_type = models.CharField(max_length=126)
+    deleted_on = models.DateTimeField(db_index=True)
+    domain = models.CharField(max_length=255)
+    deleted_by = models.CharField(max_length=126, null=True)
+
+    class Meta:
+        db_table = "cleanup_deletedsqldoc"
+        constraints = [
+            models.UniqueConstraint(fields=['doc_id', 'doc_type'], name='deletedsqldoc_unique_id_and_type')
+        ]
