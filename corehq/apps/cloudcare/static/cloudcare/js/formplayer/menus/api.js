954efbdf89ed8502a08f93a1324a0dab830d8478
Jonathan Tang <Jonahtang738@gmail.com>
2024-02-21 16:05:56 -0800

    bug: updates lastUserActivityTime upon formplayer query

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/api.js
@@ -93,7 +93,8 @@ hqDefine("cloudcare/js/formplayer/menus/api", function () {
                                 return;
                             }
 
-                            formplayerUtils.startIntervalSync(params.appId, parsedMenus.metaData.attemptRestore );
+                            formplayerUtils.startIntervalSync(params.appId, parsedMenus.metaData.attemptRestore);
+                            sessionStorage.setItem("lastUserActivityTime",  Date.now());
                             FormplayerFrontend.trigger('clearProgress');
                             defer.resolve(parsedMenus);
                             // Only configure menu debugger if we didn't get a form entry response
