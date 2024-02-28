792e5a44a0e09eee80e5a6c538ad55c5f63844f1
Graham Herceg <gherceg@dimagi.com>
2024-02-27 21:50:50 -0500

    Create and use is_logged_in to handle users with and without 2FA
    Users without 2FA setup can only be authenticated, however users with
    2FA setup have an additional is_verified method that ensures an active
    and valid 2FA device exists in the user's session. We should take this
    into account when checking if a user is logged in before redirecting
    them.

--- corehq/apps/hqwebapp/tests/test_login_utils.py
+++ corehq/apps/hqwebapp/tests/test_login_utils.py
@@ -1,6 +1,12 @@
-from django.test import SimpleTestCase, override_settings
+from django.contrib.auth.models import AnonymousUser, User
+from django.test import SimpleTestCase, TestCase, override_settings
 
-from corehq.apps.hqwebapp.login_utils import get_custom_login_page
+from django_otp.plugins.otp_totp.models import TOTPDevice
+
+from corehq.apps.hqwebapp.login_utils import (
+    get_custom_login_page,
+    is_logged_in,
+)
 
 
 class TestCustomLogin(SimpleTestCase):
@@ -26,3 +32,33 @@ class TestCustomLogin(SimpleTestCase):
     def test_dict_default(self):
         self.assertEqual('custom/login.html', get_custom_login_page('example.com'))
         self.assertEqual('normal/login.html', get_custom_login_page('commcarehq.org'))
+
+
+class TestIsLoggedIn(TestCase):
+
+    def test_returns_true_if_user_without_2fa_is_authenticated(self):
+        user = User(username="test-user", password="abc123")
+        self.assertTrue(is_logged_in(user))
+
+    def test_returns_false_if_user_without_2fa_is_not_authenticated(self):
+        user = AnonymousUser()
+        self.assertFalse(is_logged_in(user))
+
+    def test_returns_true_if_user_with_2fa_is_authenticated_and_verified(self):
+        user = User(username="test-user", password="abc123")
+        user.save()
+        user.is_verified = lambda: True
+        self.setup_two_factor_device(user)
+        self.assertTrue(is_logged_in(user))
+
+    def test_returns_false_if_user_with_2fa_is_authenticated_but_not_verified(self):
+        user = User(username="test-user", password="abc123")
+        user.save()
+        user.is_verified = lambda: False
+        self.setup_two_factor_device(user)
+        self.assertFalse(is_logged_in(user))
+
+    @staticmethod
+    def setup_two_factor_device(user):
+        device = TOTPDevice(user=user, name='default', confirmed=True)
+        device.save()
