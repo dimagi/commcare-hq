98fc97fc90291e9f1be052011421579c629388cc
Ethan Soergel <esoergel@gmail.com>
2024-02-28 15:13:39 -0500

    Rewrite migration to be faster

--- corehq/apps/users/management/commands/populate_sql_user_data.py
+++ corehq/apps/users/management/commands/populate_sql_user_data.py
@@ -3,7 +3,10 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
-from corehq.apps.users.models import CouchUser
+from dimagi.utils.chunked import chunked
+
+from corehq.apps.users.dbaccessors import get_user_docs_by_username
+from corehq.apps.users.user_data import SQLUserData
 from corehq.util.log import with_progress_bar
 from corehq.util.queries import queryset_to_iterator
 
@@ -13,16 +16,28 @@ class Command(BaseCommand):
 
     def handle(self, **options):
         queryset = get_users_without_user_data()
-        for user in with_progress_bar(queryset_to_iterator(queryset, User), queryset.count()):
-            populate_user_data(user)
+        users = with_progress_bar(queryset_to_iterator(queryset, User), queryset.count())
+        for chunk in chunked(users, 100):
+            users_by_username = {user.username: user for user in chunk}
+            for user_doc in get_user_docs_by_username(users_by_username.keys()):
+                populate_user_data(user_doc, users_by_username[user_doc['username']])
 
 
 def get_users_without_user_data():
     return User.objects.filter(sqluserdata__isnull=True)
 
 
-def populate_user_data(django_user):
-    user = CouchUser.from_django_user(django_user, strict=True)
-    if user:
-        for domain in user.get_domains():
-            user.get_user_data(domain).save()
+def populate_user_data(user_doc, django_user):
+    domains = user_doc.get('domains', [user_doc.get('domain')])
+    for domain in domains:
+        raw_user_data = user_doc.get('user_data', {})
+        raw_user_data.pop(COMMCARE_PROJECT, None)
+        profile_id = raw_user_data.pop(PROFILE_SLUG, None)
+        if raw_user_data or profile_id:
+            sql_data = SQLUserData.objects.create(
+                user_id=user_doc['_id'],
+                domain=domain,
+                data=raw_user_data,
+                django_user=django_user,
+                profile_id=profile_id,
+            )
