46d25c66c73d4e3df84518e194102d94290f4552
Biyeun Buczyk <biyeun@dimagi.com>
2024-03-04 11:05:03 +0100

    make sure that related TrustedIdentityProvider objects are deleted when deleting IdentityProvider to avoid throwing ProtectedError

--- corehq/apps/sso/views/accounting_admin.py
+++ corehq/apps/sso/views/accounting_admin.py
@@ -28,7 +28,12 @@ from corehq.apps.sso.async_handlers import (
     SSOExemptUsersAdminAsyncHandler,
     SsoTestUserAdminAsyncHandler,
 )
-from corehq.apps.sso.models import IdentityProvider, IdentityProviderProtocol, AuthenticatedEmailDomain
+from corehq.apps.sso.models import (
+    IdentityProvider,
+    IdentityProviderProtocol,
+    AuthenticatedEmailDomain,
+    TrustedIdentityProvider,
+)
 
 
 class IdentityProviderInterface(AddItemInterface):
@@ -207,6 +212,7 @@ class EditIdentityProviderAdminView(BaseIdentityProviderAdminView, AsyncHandlerM
             )
         elif self.is_deletion_request:
             AuthenticatedEmailDomain.objects.filter(identity_provider=self.identity_provider).delete()
+            TrustedIdentityProvider.objects.filter(identity_provider=self.identity_provider).delete()
             self.identity_provider.delete()
             messages.success(
                 request,
