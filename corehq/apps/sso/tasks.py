030eb789d288dd76863fe48a9ad0ac3c75329d38
Jing Cheng <jcheng@dimagi.com>
2024-03-07 18:31:50 -0500

    Wrong intendation

--- corehq/apps/sso/tasks.py
+++ corehq/apps/sso/tasks.py
@@ -129,26 +129,26 @@ def auto_deactivate_removed_sso_users():
             context = get_sso_deactivation_skip_email_context(idp)
             if not context["to"]:
                 notify_exception(None, f"no admin email addresses for IdP: {idp}")
-                try:
-                    for send_to in context["to"]:
-                        send_html_email_async.delay(
-                            context["subject"],
-                            send_to,
-                            context["html"],
-                            text_content=context["plaintext"],
-                            email_from=context["from"],
-                            bcc=context["bcc"],
-                        )
-                        log.info(
-                            "Sent sso user deactivation skipped notification"
-                            "email for %(idp_name)s to %(send_to)s." % {
-                                "idp_name": idp.name,
-                                "send_to": send_to,
-                            }
-                        )
-                except Exception as exc:
-                    notify_exception(None, f"Failed to send sso user deactivation skipped notification email for"
-                                     f" IdP {idp}: {exc!s}", exc_info=True)
+            try:
+                for send_to in context["to"]:
+                    send_html_email_async.delay(
+                        context["subject"],
+                        send_to,
+                        context["html"],
+                        text_content=context["plaintext"],
+                        email_from=context["from"],
+                        bcc=context["bcc"],
+                    )
+                    log.info(
+                        "Sent sso user deactivation skipped notification"
+                        "email for %(idp_name)s to %(send_to)s." % {
+                            "idp_name": idp.name,
+                            "send_to": send_to,
+                        }
+                    )
+            except Exception as exc:
+                notify_exception(None, f"Failed to send sso user deactivation skipped notification email for"
+                                       f" IdP {idp}: {exc!s}", exc_info=True)
             return
 
         usernames_to_deactivate = []
