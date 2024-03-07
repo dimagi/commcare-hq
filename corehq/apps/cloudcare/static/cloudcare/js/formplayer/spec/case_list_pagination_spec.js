kabass/commcare-hq-1

Restored case_list_pagination_spec

corehq/apps/cloudcare/static/cloudcare/js/form_entry/spec/case_list_pagination_spec.js
@@ -1,6 +1,9 @@
-hqDefine("cloudcare/js/form_entry/spec/case_list_pagination_spec", function () {
+hqDefine("cloudcare/js/form_entry/spec/case_list_pagination_spec", [
+    "cloudcare/js/formplayer/utils/utils",
+], function (
+    paginateItems
+) {
     describe('#paginateOptions', function () {
-        var paginateItems = hqImport("cloudcare/js/formplayer/utils/utils");
         it('Should return paginateOptions', function () {
             var case1 = paginateItems.paginateOptions(0, 15);
             /**

dimagi/commcare-hq

corehq/apps/cloudcare/static/cloudcare/js/formplayer/spec/case_list_pagination_spec.js
@@ -1,7 +1,10 @@
 'use strict';
-hqDefine("cloudcare/js/formplayer/spec/case_list_pagination_spec", function () {
+hqDefine("cloudcare/js/formplayer/spec/case_list_pagination_spec", [
+    "cloudcare/js/formplayer/utils/utils",
+], function (
+    paginateItems
+) {
     describe('#paginateOptions', function () {
-        var paginateItems = hqImport("cloudcare/js/formplayer/utils/utils");
         it('Should return paginateOptions', function () {
             var case1 = paginateItems.paginateOptions(0, 15, 3);
             /**