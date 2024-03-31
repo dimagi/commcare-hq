e3f1fed2836dca10dde9f6ede64050e95f8d4227
Jonathan Tang <Jonahtang738@gmail.com>
2024-03-30 23:49:18 -0700

    preparation to rename field since it is not specific to supply_point

--- corehq/apps/users/models.py
+++ corehq/apps/users/models.py
@@ -2683,6 +2683,7 @@ class Invitation(models.Model):
     role = models.CharField(max_length=100, null=True)  # role qualified ID
     program = models.CharField(max_length=126, null=True)   # couch id of a Program
     supply_point = models.CharField(max_length=126, null=True)  # couch id of a Location
+    location_id = models.CharField(max_length=126, null=True)  # couch id of a Location (to replace supply_point)
     profile = models.ForeignKey("custom_data_fields.CustomDataFieldsProfile",
                                 on_delete=models.CASCADE, null=True)
 
