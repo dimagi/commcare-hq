d5609d1c052a3bd7190e18cbe4cdc73b07e7710c
Matt Riley <mriley@dimagi.com>
2024-04-02 13:28:43 -0400

    Fixed display issue with OIDC provider forms

--- corehq/apps/sso/forms.py
+++ corehq/apps/sso/forms.py
@@ -1070,6 +1070,7 @@ class SsoOidcEnterpriseSettingsForm(BaseSsoEnterpriseSettingsForm):
             client_secret_toggles = crispy.Div()
 
         self.helper = FormHelper()
+        self.helper.form_class = 'form form-horizontal'
         self.helper.label_class = 'col-sm-3 col-md-2'
         self.helper.field_class = 'col-sm-9 col-md-8 col-lg-6'
         self.helper.layout = crispy.Layout(
