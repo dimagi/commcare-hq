664a1eb019fb2ae310af0b08180b9188e4bad608
Matt Riley <mriley@dimagi.com>
2024-04-02 15:32:13 -0400

    Changed method into property

--- corehq/apps/sso/views/enterprise_admin.py
+++ corehq/apps/sso/views/enterprise_admin.py
@@ -72,7 +72,7 @@ class EditIdentityProviderEnterpriseView(BaseEnterpriseAdminView, AsyncHandlerMi
             'idp_slug': self.idp_slug,
             'is_oidc': self.identity_provider.protocol == IdentityProviderProtocol.OIDC,
             'show_remote_user_management': self.show_remote_user_management,
-            'show_api_fields': self.uses_api_key_management(),
+            'show_api_fields': self.uses_api_key_management,
         }
 
     @property
@@ -111,27 +111,26 @@ class EditIdentityProviderEnterpriseView(BaseEnterpriseAdminView, AsyncHandlerMi
             else SsoOidcEnterpriseSettingsForm
         )
 
-        uses_api_key_management = self.uses_api_key_management()
-
         if self.request.method == 'POST':
             return form_class(
                 self.identity_provider,
                 self.request.POST,
                 self.request.FILES,
                 show_remote_user_management=self.show_remote_user_management,
-                uses_api_key_management=uses_api_key_management
+                uses_api_key_management=self.uses_api_key_management
             )
 
         return form_class(
             self.identity_provider,
             show_remote_user_management=self.show_remote_user_management,
-            uses_api_key_management=uses_api_key_management
+            uses_api_key_management=self.uses_api_key_management
         )
 
     @property
     def show_remote_user_management(self):
         return toggles.SSO_REMOTE_USER_MANAGEMENT.enabled_for_request(self.request)
 
+    @property
     def uses_api_key_management(self):
         return toggles.MULTI_VIEW_API_KEYS.enabled_for_request(self.request)
 
