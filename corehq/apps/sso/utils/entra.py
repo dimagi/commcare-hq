7ae2fbbc2e54f8af2a2c2478e84b58b3292be96d
Jing Cheng <jcheng@dimagi.com>
2024-04-02 17:16:58 -0400

    Return user in an application

--- corehq/apps/sso/utils/entra.py
+++ corehq/apps/sso/utils/entra.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 from corehq.apps.sso.exceptions import EntraVerificationFailed
@@ -19,7 +20,8 @@ def get_all_members_of_the_idp_from_entra(idp):
         "client_id": idp.api_id,
         "scope": ["https://graph.microsoft.com/.default"],
         "secret": idp.api_secret,
-        "endpoint": "https://graph.microsoft.com/v1.0/users"
+        "endpoint": f"https://graph.microsoft.com/v1.0/servicePrincipals(appId='{idp.api_id}')/"
+                    "appRoleAssignedTo?$select=principalId, principalType"
     }
 
     # Create a preferably long-lived app instance which maintains a token cache.
@@ -39,9 +41,38 @@ def get_all_members_of_the_idp_from_entra(idp):
             headers={'Authorization': 'Bearer ' + result['access_token']},
         )
         response.raise_for_status()  # Raises an error for bad status
-        graph_data = response.json()
+        assignments = response.json()
 
-        user_principal_names = [user["userPrincipalName"] for user in graph_data["value"]]
+        # microsoft.graph.appRoleAssignment's property doesn't  userPrincipalName
+        # Property principalType can either be User, Group or ServicePrincipal
+        principal_ids = {assignment["principalId"] for assignment in assignments["value"]
+                         if assignment["principalType"] == "User"}
+
+        # Prepare batch request
+        batch_payload = {
+            "requests": [
+                {
+                    "id": str(i),
+                    "method": "GET",
+                    "url": f"/users/{principal_id}?$select=userPrincipalName"
+                } for i, principal_id in enumerate(principal_ids)
+            ]
+        }
+
+        # Send batch request
+        batch_response = requests.post(
+            'https://graph.microsoft.com/v1.0/$batch',
+            headers={'Authorization': 'Bearer ' + result['access_token'], 'Content-Type': 'application/json'},
+            data=json.dumps(batch_payload)
+        )
+        batch_response.raise_for_status()
+        batch_result = batch_response.json()
+
+        # Extract userPrincipalName from batch response
+        user_principal_names = [
+            resp['body']['userPrincipalName'] for resp in batch_result['responses']
+            if 'body' in resp and 'userPrincipalName' in resp['body']
+        ]
         return user_principal_names
     else:
         raise EntraVerificationFailed(result.get('error', {}).get('code', 'Unknown'),
