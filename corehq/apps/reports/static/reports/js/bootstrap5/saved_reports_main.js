eb724e0cb18fbb42f12d34d70adb7733930047a8
Biyeun Buczyk <biyeun@dimagi.com>
2024-04-03 07:45:46 +0000

    "Bootstrap 5 Migration - initial auto-migration for reports/js/saved_reports_main.js, splitting templates"

---
+++ corehq/apps/reports/static/reports/js/bootstrap5/saved_reports_main.js
@@ -0,0 +1,32 @@
+hqDefine("reports/js/bootstrap5/saved_reports_main", [
+    'jquery',
+    'knockout',
+    'hqwebapp/js/initial_page_data',
+    'reports/js/bootstrap5/report_config_models',
+    'reports/js/scheduled_reports_list',
+    'hqwebapp/js/bootstrap3/knockout_bindings.ko', // modal binding
+], function (
+    $,
+    ko,
+    initialPageData,
+    reportConfigModels,
+    scheduledReports
+) {
+    $(function () {
+
+        var $configList = $("#ko-report-config-list");
+        if ($configList.length) {
+            $configList.koApplyBindings(reportConfigModels.reportConfigsViewModel({
+                items: initialPageData.get('configs'),
+                sharedItems: initialPageData.get('shared_saved_reports'),
+                saveUrl: initialPageData.reverse("add_report_config"),
+            }));
+        }
+
+        $("#js-scheduled_reports").koApplyBindings(scheduledReports.scheduledReportListModel({
+            scheduled_reports: initialPageData.get('scheduled_reports'),
+            other_scheduled_reports: initialPageData.get('others_scheduled_reports'),
+            is_admin: initialPageData.get('is_admin'),
+        }));
+    });
+});
