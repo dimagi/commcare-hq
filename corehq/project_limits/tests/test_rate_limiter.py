e331824b341091f9edd5d0e4cb96c3f524736cd5
Charl Smit <csmit@dimagi.com>
2024-04-04 10:43:44 +0200

    Add another basic test

--- corehq/project_limits/tests/test_rate_limiter.py
+++ corehq/project_limits/tests/test_rate_limiter.py
@@ -32,6 +32,23 @@ def test_rate_limit_interface():
 @patch('corehq.project_limits.rate_limiter.get_n_users_in_domain', lambda domain: 10)
 @patch('corehq.project_limits.rate_limiter.get_n_users_in_subscription', lambda domain: 10)
 @patch('corehq.project_limits.rate_limiter._get_account_name', lambda domain: 'test')
+def test_rate_limit_interface_wait_time_for_access():
+    """
+    Just test that very basic usage doesn't error
+    """
+    per_user_rate_def = RateDefinition(per_week=50000, per_day=13000, per_second=.001)
+    min_rate_def = RateDefinition(per_second=10)
+    per_user_rate_def = PerUserRateDefinition(per_user_rate_def, min_rate_def)
+    my_feature_rate_limiter = RateLimiter('my_feature', per_user_rate_def.get_rate_limits)
+
+    if my_feature_rate_limiter.get_wait_time_for_access('my_domain') == 0:
+        # ...do stuff...
+        my_feature_rate_limiter.report_usage('my_domain')
+
+
+@patch('corehq.project_limits.rate_limiter.get_n_users_in_domain', lambda domain: 10)
+@patch('corehq.project_limits.rate_limiter.get_n_users_in_subscription', lambda domain: 10)
+@patch('corehq.project_limits.rate_limiter._get_account_name', lambda domain: 'test')
 def test_get_window_of_first_exceeded_limit():
     per_user_rate_def = RateDefinition(per_second=10)
     min_rate_def = RateDefinition(per_second=100)
