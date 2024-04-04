3ccfeec75b9be26a908dfa27893c39f51e5927f6
Graham Herceg <gherceg@dimagi.com>
2024-04-04 15:23:27 -0400

    Serialize and deserialize prep_value to ensure it is valid

--- corehq/util/tests/test_jsonattrs.py
+++ corehq/util/tests/test_jsonattrs.py
@@ -176,7 +176,8 @@ def get_json_value(model, field_name):
     value = field.pre_save(model, False)
     from django import VERSION as django_version
     if django_version[:2] >= (4, 2):
-        return field.get_prep_value(value)
+        # load as json obj to ensure it serializes properly
+        return json.loads(json.dumps(field.get_prep_value(value)))
     else:
         return json.loads(field.get_prep_value(value))
 
