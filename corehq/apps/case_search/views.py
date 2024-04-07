b02a80df1149664653cd21f5df6c397dba241e03
Jing Cheng <jcheng@dimagi.com>
2024-04-07 00:30:56 -0400

    Bootstrap 5 Migration - initial auto-migration for case_search/profile_case_search.html, splitting templates

--- corehq/apps/case_search/views.py
+++ corehq/apps/case_search/views.py
@@ -99,7 +99,7 @@ class CaseSearchView(_BaseCaseSearchView):
 
 
 class ProfileCaseSearchView(_BaseCaseSearchView):
-    template_name = 'case_search/profile_case_search.html'
+    template_name = 'case_search/bootstrap3/profile_case_search.html'
     urlname = 'profile_case_search'
     page_title = gettext_lazy("Profile Case Search")
 
