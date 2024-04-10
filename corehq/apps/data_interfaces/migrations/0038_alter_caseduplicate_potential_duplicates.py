0e4bfab32e5c81669774e87a735a70106f006524
Graham Herceg <gherceg@dimagi.com>
2024-04-10 09:54:48 -0400

    Add hack to pass in related_name if Django < 4
    This allows this migration to exist on both Django 3.2 and 4.2, which
    makes the upgrade smoother.

--- corehq/apps/data_interfaces/migrations/0038_alter_caseduplicate_potential_duplicates.py
+++ corehq/apps/data_interfaces/migrations/0038_alter_caseduplicate_potential_duplicates.py
@@ -1,4 +1,12 @@
+from django import VERSION as django_version
 from django.db import migrations, models
+
+# Django < 4 specifies related_name in ManyToManyField, whereas Django >= 4 does not.
+# This results in a new migration being created on 4. This m2m_kwargs is designed to enable
+# upgrading to Django 4.2 from 3.2 smoothly.
+m2m_kwargs = {}
+if django_version[0] < 4:
+    m2m_kwargs['related_name'] = '_caseduplicate_potential_duplicates_+'
 
 
 class Migration(migrations.Migration):
@@ -11,7 +19,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name="caseduplicate",
             name="potential_duplicates",
-            field=models.ManyToManyField(to="data_interfaces.caseduplicate"),
+            field=models.ManyToManyField(to="data_interfaces.caseduplicate", **m2m_kwargs),
         ),
     ]
 
