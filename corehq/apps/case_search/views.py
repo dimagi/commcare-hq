61cdb7dc4286051deb404670ec350bdb3e6739f1
Jing Cheng <jcheng@dimagi.com>
2024-04-13 16:28:38 -0400

    migrate profile_case_search

--- corehq/apps/case_search/views.py
+++ corehq/apps/case_search/views.py
@@ -97,8 +97,9 @@ class CaseSearchView(_BaseCaseSearchView):
         })
 
 
+@method_decorator(use_bootstrap5, name='dispatch')
 class ProfileCaseSearchView(_BaseCaseSearchView):
-    template_name = 'case_search/bootstrap3/profile_case_search.html'
+    template_name = 'case_search/bootstrap5/profile_case_search.html'
     urlname = 'profile_case_search'
     page_title = gettext_lazy("Profile Case Search")
 
