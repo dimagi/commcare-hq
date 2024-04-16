c47c6cd3d01a2c1813f621500d6dade2b772e09b
Biyeun Buczyk <biyeun@dimagi.com>
2024-04-16 17:27:29 +0200

    add test for checking in progress status of app

--- corehq/apps/hqwebapp/tests/utils/test_bootstrap_status.py
+++ corehq/apps/hqwebapp/tests/utils/test_bootstrap_status.py
@@ -4,12 +4,19 @@ from corehq.apps.hqwebapp.utils.bootstrap.status import (
     is_app_completed,
     get_completed_templates_for_app,
     get_completed_javascript_for_app,
+    is_app_in_progress,
 )
 
 
 def test_is_app_completed_for_builds():
     is_completed = is_app_completed("builds")
     eq(is_completed, True)
+
+
+def test_is_app_in_progress_for_hqwebapp():
+    # we base this test on hqwebapp because it will be the final app to be completed in this migration
+    is_in_progress = is_app_in_progress("hqwebapp")
+    eq(is_in_progress, True)
 
 
 def test_get_completed_templates_for_app_relative_paths():
