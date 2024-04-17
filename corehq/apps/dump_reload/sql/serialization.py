a16fc511d0b025189289253970dccf1839c241c6
Graham Herceg <gherceg@dimagi.com>
2024-04-17 17:34:22 -0400

    Remove reset_encrypted_fields option

--- corehq/apps/dump_reload/sql/serialization.py
+++ corehq/apps/dump_reload/sql/serialization.py
@@ -12,31 +12,6 @@ class JsonLinesSerializer(JsonSerializer):
     Convert a queryset to JSON outputting one object per line
     """
 
-    def serialize(
-        self,
-        queryset,
-        *,
-        stream=None,
-        fields=None,
-        use_natural_foreign_keys=False,
-        use_natural_primary_keys=False,
-        progress_output=None,
-        object_count=0,
-        reset_encrypted_fields=True,
-        **options
-    ):
-        self.reset_encrypted_fields = reset_encrypted_fields
-        super().serialize(
-            queryset,
-            stream=stream,
-            fields=fields,
-            use_natural_foreign_keys=use_natural_foreign_keys,
-            use_natural_primary_keys=use_natural_primary_keys,
-            progress_output=progress_output,
-            object_count=object_count,
-            **options
-        )
-
     def start_serialization(self):
         self._init_options()
 
@@ -45,7 +20,7 @@ class JsonLinesSerializer(JsonSerializer):
 
     def get_dump_object(self, obj):
         dumped_obj = super().get_dump_object(obj)
-        if self.reset_encrypted_fields and hasattr(obj, 'encrypted_fields'):
+        if hasattr(obj, 'encrypted_fields'):
             for field in obj.encrypted_fields():
                 dumped_obj['fields'][field] = PASSWORD_PLACEHOLDER
         return dumped_obj
