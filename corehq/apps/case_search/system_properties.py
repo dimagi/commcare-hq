c20cc2c5c62b38e32e563139736cda7350037aba
Ethan Soergel <esoergel@gmail.com>
2024-04-17 21:40:25 -0400

    make system properties classes

--- corehq/apps/case_search/system_properties.py
+++ corehq/apps/case_search/system_properties.py
@@ -1,6 +1,3 @@
-from collections import namedtuple
-
-
 def get_field_lambda(field_name):
     return lambda model: model._meta.get_field(field_name)
 
@@ -9,66 +6,79 @@ def get_field_lambda(field_name):
 # expressions. We store these as case properties in the case search index so
 # they are easily searchable, then remove them when pulling the case source
 # from ES
-SpecialCaseProperty = namedtuple('SpecialCaseProperty', 'key value_getter sort_property field_getter')
-SPECIAL_CASE_PROPERTIES_MAP = {
-    '@case_id': SpecialCaseProperty(
-        '@case_id',
-        lambda doc: doc.get('_id'),
-        '_id',
-        get_field_lambda('case_id'),
-    ),
-    '@case_type': SpecialCaseProperty(
-        '@case_type',
-        lambda doc: doc.get('type'),
-        'type.exact',
-        get_field_lambda('type'),
-    ),
-    '@owner_id': SpecialCaseProperty(
-        '@owner_id',
-        lambda doc: doc.get('owner_id'),
-        'owner_id',
-        get_field_lambda('owner_id'),
-    ),
-    '@status': SpecialCaseProperty(
-        '@status',
-        lambda doc: 'closed' if doc.get('closed') else 'open',
-        'closed',
-        get_field_lambda('closed'),
-    ),
-    'name': SpecialCaseProperty(
-        'name',
-        lambda doc: doc.get('name'),
-        'name.exact',
-        get_field_lambda('name'),
-    ),
-    'case_name': SpecialCaseProperty(
-        'case_name',
-        lambda doc: doc.get('name'),
-        'name.exact',
-        get_field_lambda('name'),
-    ),
-    'external_id': SpecialCaseProperty(
-        'external_id',
-        lambda doc: doc.get('external_id', ''),
-        'external_id',
-        get_field_lambda('external_id'),
-    ),
-    'date_opened': SpecialCaseProperty(
-        'date_opened',
-        lambda doc: doc.get('opened_on'),
-        'opened_on',
-        get_field_lambda('opened_on'),
-    ),
-    'closed_on': SpecialCaseProperty(
-        'closed_on',
-        lambda doc: doc.get('closed_on'),
-        'closed_on',
-        get_field_lambda('closed_on'),
-    ),
-    'last_modified': SpecialCaseProperty(
-        'last_modified',
-        lambda doc: doc.get('modified_on'),
-        'modified_on',
-        get_field_lambda('modified_on'),
-    ),
-}
+
+class _SystemProperty:
+    ...
+
+
+class CaseID(_SystemProperty):
+    key = '@case_id'
+    value_getter = lambda doc: doc.get('_id')
+    sort_property = '_id'
+    field_getter = get_field_lambda('case_id')
+
+
+class CaseType(_SystemProperty):
+    key = '@case_type'
+    value_getter = lambda doc: doc.get('type')
+    sort_property = 'type.exact'
+    field_getter = get_field_lambda('type')
+
+
+class OwnerID(_SystemProperty):
+    key = '@owner_id'
+    value_getter = lambda doc: doc.get('owner_id')
+    sort_property = 'owner_id'
+    field_getter = get_field_lambda('owner_id')
+
+
+class Status(_SystemProperty):
+    key = '@status'
+    value_getter = lambda doc: 'closed' if doc.get('closed') else 'open'
+    sort_property = 'closed'
+    field_getter = get_field_lambda('closed')
+
+
+class Name(_SystemProperty):
+    key = 'name'
+    value_getter = lambda doc: doc.get('name')
+    sort_property = 'name.exact'
+    field_getter = get_field_lambda('name')
+
+
+class CaseName(_SystemProperty):
+    key = 'case_name'
+    value_getter = lambda doc: doc.get('name')
+    sort_property = 'name.exact'
+    field_getter = get_field_lambda('name')
+
+
+class ExternalID(_SystemProperty):
+    key = 'external_id'
+    value_getter = lambda doc: doc.get('external_id', '')
+    sort_property = 'external_id'
+    field_getter = get_field_lambda('external_id')
+
+
+class DateOpened(_SystemProperty):
+    key = 'date_opened'
+    value_getter = lambda doc: doc.get('opened_on')
+    sort_property = 'opened_on'
+    field_getter = get_field_lambda('opened_on')
+
+
+class ClosedOn(_SystemProperty):
+    key = 'closed_on'
+    value_getter = lambda doc: doc.get('closed_on')
+    sort_property = 'closed_on'
+    field_getter = get_field_lambda('closed_on')
+
+
+class LastModified(_SystemProperty):
+    key = 'last_modified'
+    value_getter = lambda doc: doc.get('modified_on')
+    sort_property = 'modified_on'
+    field_getter = get_field_lambda('modified_on')
+
+
+SPECIAL_CASE_PROPERTIES_MAP = {prop.key: prop for prop in _SystemProperty.__subclasses__()}
