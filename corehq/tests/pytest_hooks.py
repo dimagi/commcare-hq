18e41dcf3b0bf08b834f002a32bf7ed0b34c7e85
Daniel Miller <dmiller@dimagi.com>
2024-04-18 16:20:56 -0400

    Initial pytest implementation

---
+++ corehq/tests/pytest_hooks.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_load_initial_conftests():
+    assert not hasattr(pytest_load_initial_conftests, 'loaded'), "Already loaded"
+    pytest_load_initial_conftests.loaded = True
+    os.environ.setdefault('CCHQ_TESTING', '1')
+
+    from manage import init_hq_python_path, run_patches
+    init_hq_python_path()
+    run_patches()
+
+    from corehq.warnings import configure_warnings
+    configure_warnings(is_testing=True)
+
+    from .nosecompat import create_nose_virtual_package
+    create_nose_virtual_package()
+
+
+def assert_loaded():
+    # Not a pytest hook.
+    assert getattr(pytest_load_initial_conftests, "loaded", False), """
+        pytest requires commcare-hq test hooks. Recommended:
+
+        pip install -e .
+    """
