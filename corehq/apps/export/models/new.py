cac33c01fb78a61b202f806f0d1cdc66a7765a18
Charl Smit <csmit@dimagi.com>
2024-04-22 11:46:10 +0200

    Unhide checkbox

--- corehq/apps/export/models/new.py
+++ corehq/apps/export/models/new.py
@@ -832,12 +832,6 @@ class ExportInstance(BlobMixin, Document):
     @classmethod
     def wrap(cls, data):
         from corehq.apps.export.views.utils import clean_odata_columns
-        # This is a temporary solution to make sure geojson exports have split_multiselects set to false,
-        # otherwise it won't work. Ticket SC-3569 is for making geojson form exports compatible with
-        # the split_multiselects option.
-        if data.get('export_format', '') == 'geojson':
-            data['split_multiselects'] = False
-
         export_instance = super(ExportInstance, cls).wrap(data)
         if export_instance.is_odata_config:
             clean_odata_columns(export_instance)
