e5649025cf77a1f25f74360a8a6f700f5772a252
Ethan Soergel <esoergel@gmail.com>
2024-04-22 15:17:53 -0400

    Use dataclasses instead

--- corehq/apps/case_search/system_properties.py
+++ corehq/apps/case_search/system_properties.py
@@ -6,86 +6,75 @@ index.
 These are stored along with dynamic case properties in the case search index to
 be easily searchable, then removed when pulling the case source from ES.
 """
+from dataclasses import dataclass
+from typing import Callable
 
 
+@dataclass(frozen=True)
 class _SystemProperty:
-    key = None  # The user-facing property name
-    system_name = None  # The CommCareCase field name
-    _es_field_name = None  # Path to use for ES logic, if not system_name
-    is_datetime = False
+    key: str  # The user-facing property name
+    system_name: str  # The CommCareCase field name
+    is_datetime: bool = False
+    _es_field_name: str = None  # Path to use for ES logic, if not system_name
+    _value_getter: Callable = None
 
-    @classmethod
-    def get_value(cls, doc):
-        return doc.get(cls.system_name)
+    def get_value(self, doc):
+        if self._value_getter:
+            return self._value_getter(doc)
+        return doc.get(self.system_name)
 
-    @classmethod
     @property
-    def es_field_name(cls):
-        return cls._es_field_name or cls.system_name
+    def es_field_name(self):
+        return self._es_field_name or self.system_name
 
 
-class CaseID(_SystemProperty):
-    key = '@case_id'
-    system_name = '_id'
-
-
-class CaseType(_SystemProperty):
-    key = '@case_type'
-    system_name = 'type'
-    _es_field_name = 'type.exact'
-
-
-class OwnerID(_SystemProperty):
-    key = '@owner_id'
-    system_name = 'owner_id'
-
-
-class Status(_SystemProperty):
-    key = '@status'
-    system_name = 'closed'
-
-    @classmethod
-    def get_value(cls, doc):
-        return 'closed' if doc.get('closed') else 'open'
-
-
-class Name(_SystemProperty):
-    key = 'name'
-    system_name = 'name'
-    _es_field_name = 'name.exact'
-
-
-class CaseName(_SystemProperty):
-    key = 'case_name'
-    system_name = 'name'
-    sort_property = 'name.exact'
-
-
-class ExternalID(_SystemProperty):
-    key = 'external_id'
-    system_name = 'external_id'
-
-    @classmethod
-    def get_value(cls, doc):
-        return doc.get('external_id', '')
-
-
-class DateOpened(_SystemProperty):
-    key = 'date_opened'
-    system_name = 'opened_on'
-    is_datetime = True
-
-
-class ClosedOn(_SystemProperty):
-    key = 'closed_on'
-    system_name = 'closed_on'
-    is_datetime = True
-
-
-class LastModified(_SystemProperty):
-    key = 'last_modified'
-    system_name = 'modified_on'
-    is_datetime = True
-
-
-SPECIAL_CASE_PROPERTIES_MAP = {prop.key: prop for prop in _SystemProperty.__subclasses__()}
+SPECIAL_CASE_PROPERTIES_MAP = {prop.key: prop for prop in [
+    _SystemProperty(
+        key='@case_id',
+        system_name='_id',
+    ),
+    _SystemProperty(
+        key='@case_type',
+        system_name='type',
+        _es_field_name='type.exact',
+    ),
+    _SystemProperty(
+        key='@owner_id',
+        system_name='owner_id',
+    ),
+    _SystemProperty(
+        key='@status',
+        system_name='closed',
+        _value_getter=lambda doc: 'closed' if doc.get('closed') else 'open',
+    ),
+    _SystemProperty(
+        key='name',
+        system_name='name',
+        _es_field_name='name.exact',
+    ),
+    _SystemProperty(
+        key='case_name',
+        system_name='name',
+        _es_field_name='name.exact',
+    ),
+    _SystemProperty(
+        key='external_id',
+        system_name='external_id',
+        _value_getter=lambda doc: doc.get('external_id', ''),
+    ),
+    _SystemProperty(
+        key='date_opened',
+        system_name='opened_on',
+        is_datetime=True,
+    ),
+    _SystemProperty(
+        key='closed_on',
+        system_name='closed_on',
+        is_datetime=True,
+    ),
+    _SystemProperty(
+        key='last_modified',
+        system_name='modified_on',
+        is_datetime=True,
+    ),
+]}
