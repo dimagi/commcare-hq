92e99215251a095d4fe690aea888bdbe039dcf89
Martin Riese <mriese@dimagi.com>
2024-04-23 12:18:50 -0500

    USH-4329: Make anchored submit button sticky

--- corehq/apps/cloudcare/static/cloudcare/js/form_entry/form_ui.js
+++ corehq/apps/cloudcare/static/cloudcare/js/form_entry/form_ui.js
@@ -392,8 +392,6 @@ hqDefine("cloudcare/js/form_entry/form_ui", function () {
         self.hasSubmitAttempted = ko.observable(false);
         self.isSubmitting = ko.observable(false);
         self.isAnchoredSubmitStyle = hqImport('hqwebapp/js/toggles').toggleEnabled('WEB_APPS_ANCHORED_SUBMIT');
-        self.submitClass = constants.FULL_WIDTH + ' text-center' +
-          (self.isAnchoredSubmitStyle ? ' anchored-submit' : ' nonanchored-submit');
 
         self.currentIndex = ko.observable("0");
         self.atLastIndex = ko.observable(false);
