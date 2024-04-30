0825a78e1d1a0f41e0b4acc2cc07d6242b24e07d
Jenny Schweers <jschweers@dimagi.com>
2024-04-30 13:28:26 -0400

    Removed empty_module

--- corehq/apps/cloudcare/static/cloudcare/js/empty_module.js
+++
@@ -1,7 +0,0 @@
-/*
- *  This is a temporary measure for the bootstrap3/bootstrap5 migration,
- *  as a stub for the bootstrap5_loader.
- */
-hqDefine("cloudcare/js/empty_module", [ ], function () {
-    return 1;
-});
