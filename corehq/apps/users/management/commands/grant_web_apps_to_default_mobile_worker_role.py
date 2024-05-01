4489b44dce62e0fa6aab5103cfcf2a8add0af01c
Jenny Schweers <jschweers@dimagi.com>
2024-05-01 13:27:18 -0400

    Updated to only change roles that don't have web users assigned

--- corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
+++ corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 
+from corehq.apps.users.analytics import get_role_user_count
 from corehq.apps.users.models_role import Permission, UserRole
 
 
@@ -9,10 +10,13 @@ class Command(BaseCommand):
     def handle(self, **options):
         permission, created = Permission.objects.get_or_create(value='access_web_apps')
         roles_to_update = UserRole.objects.filter(is_commcare_user_default=True)
-        #roles_to_update = roles_to_update.exclude(rolepermission__permission_fk_id=permission.id)
         for role in roles_to_update:
             permissions = role.permissions
-            if not permissions.access_web_apps and not permissions.web_apps_list:
-                permissions.access_web_apps = True
-                permissions.normalize(previous=role.permissions)
-                role.set_permissions(permissions.to_list())
+            if not permissions.access_web_apps:
+                count = get_role_user_count(role.domain, role.couch_id, web_users_only=True)
+                if count == 0:
+                    # Only update if role is limited to mobile workers, who already have access to web apps
+                    if not permissions.web_apps_list:
+                        permissions.access_web_apps = True
+                        permissions.normalize(previous=role.permissions)
+                        role.set_permissions(permissions.to_list())
