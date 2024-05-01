dimagi/commcare-hq

commit 24412b11109b4511c27c58c80e46b315822749f3

RepeatRecord out of corehq/apps/data_interfaces

corehq/apps/data_interfaces/tasks.py
@@ -19,11 +19,6 @@ from corehq.apps.domain_migration_flags.api import any_migrations_in_progress
 from corehq.apps.hqcase.utils import AUTO_UPDATE_XMLNS
 from corehq.apps.users.models import CouchUser
 from corehq.form_processor.models import XFormInstance
-from corehq.motech.repeaters.dbaccessors import (
-    get_couch_repeat_record_ids_by_payload_id,
-    get_sql_repeat_records_by_payload_id,
-    iter_repeat_record_ids_by_repeater,
-)
 from corehq.apps.case_importer.do_import import SubmitCaseBlockHandler, RowAndCase
 from corehq.motech.repeaters.models import SQLRepeatRecord
 from corehq.sql_db.util import get_db_aliases_for_partitioned_query
@@ -208,7 +203,7 @@ def task_operate_on_payloads(
     record_ids: List[str],
     domain: str,
     action,  # type: Literal['resend', 'cancel', 'requeue']  # 3.8+
-    use_sql: bool,
+    use_sql: bool = True,
 ):
     return operate_on_payloads(record_ids, domain, action, use_sql,
                                task=task_operate_on_payloads)
@@ -220,37 +215,27 @@ def task_generate_ids_and_operate_on_payloads(
     repeater_id: Optional[str],
     domain: str,
     action,  # type: Literal['resend', 'cancel', 'requeue']  # 3.8+
-    use_sql: bool,
+    use_sql: bool = True,
 ) -> dict:
-    repeat_record_ids = _get_repeat_record_ids(payload_id, repeater_id, domain,
-                                               use_sql)
+    repeat_record_ids = _get_repeat_record_ids(payload_id, repeater_id, domain, use_sql)
     return operate_on_payloads(repeat_record_ids, domain, action, use_sql,
                                task=task_generate_ids_and_operate_on_payloads)
 
 
-def _get_repeat_record_ids(
-    payload_id: Optional[str],
-    repeater_id: Optional[str],
-    domain: str,
-    use_sql: bool,
-) -> List[str]:
-    if not payload_id and not repeater_id:
-        return []
+def _get_repeat_record_ids(payload_id, repeater_id, domain, use_sql=True):
     if payload_id:
-        if use_sql:
-            records = get_sql_repeat_records_by_payload_id(domain, payload_id)
-            return [r.id for r in records]
-        else:
-            return get_couch_repeat_record_ids_by_payload_id(domain, payload_id)
+        queryset = SQLRepeatRecord.objects.filter(
+            domain=domain,
+            payload_id=payload_id,
+        ).order_by('-registered_at')
+    elif repeater_id:
+        queryset = SQLRepeatRecord.objects.filter(
+            domain=domain,
+            repeater__id=repeater_id,
+        )
     else:
-        if use_sql:
-            queryset = SQLRepeatRecord.objects.filter(
-                domain=domain,
-                repeater__id=repeater_id,
-            )
-            return [r['id'] for r in queryset.values('id')]
-        else:
-            return list(iter_repeat_record_ids_by_repeater(domain, repeater_id))
+        return []
+    return list(queryset.values_list("id", flat=True))
 
 
 @task

corehq/apps/data_interfaces/tests/test_tasks.py
@@ -10,7 +10,7 @@ from corehq.apps.data_interfaces.tasks import (
 class TestTasks(TestCase):
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_task_operate_on_payloads_no_action(
         self,
         unused_1,
@@ -20,7 +20,6 @@ class TestTasks(TestCase):
             record_ids=['payload_id'],
             domain='test_domain',
             action='',
-            use_sql=False,
         )
         self.assertEqual(response, {
             'messages': {
@@ -38,13 +37,12 @@ class TestTasks(TestCase):
             record_ids=[],
             domain='test_domain',
             action='test_action',
-            use_sql=False,
         )
         self.assertEqual(response,
                          {'messages': {'errors': ['No payloads specified']}})
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     @patch('corehq.apps.data_interfaces.tasks._get_repeat_record_ids')
     def test_task_generate_ids_and_operate_on_payloads_no_action(
         self,
@@ -58,7 +56,6 @@ class TestTasks(TestCase):
             repeater_id=None,
             domain='test_domain',
             action='',
-            use_sql=False,
         )
         self.assertEqual(response, {
             'messages': {
@@ -79,7 +76,6 @@ class TestTasks(TestCase):
             repeater_id=None,
             domain='test_domain',
             action='',
-            use_sql=False,
         )
         self.assertEqual(response,
                          {'messages': {'errors': ['No payloads specified']}})

corehq/apps/data_interfaces/tests/test_utils.py
@@ -4,14 +4,12 @@ from uuid import uuid4
 
 from django.test import SimpleTestCase, TestCase
 
-from couchdbkit import ResourceNotFound
-
 from corehq.apps.data_interfaces.tasks import (
     _get_repeat_record_ids,
     task_generate_ids_and_operate_on_payloads,
 )
 from corehq.apps.data_interfaces.utils import (
-    _get_couch_repeat_record,
+    _get_sql_repeat_record,
     operate_on_payloads,
 )
 from corehq.motech.models import ConnectionSettings
@@ -20,6 +18,7 @@ from corehq.motech.repeaters.models import (
     RepeatRecord,
     SQLRepeatRecord,
 )
+from dimagi.utils.couch.migration import SyncSQLToCouchMixin
 
 DOMAIN = 'test-domain'
 
@@ -30,70 +29,67 @@ class TestUtils(SimpleTestCase):
         response = _get_repeat_record_ids(None, None, 'test_domain', False)
         self.assertEqual(response, [])
 
-    @patch('corehq.apps.data_interfaces.tasks.get_couch_repeat_record_ids_by_payload_id')
-    @patch('corehq.apps.data_interfaces.tasks.iter_repeat_record_ids_by_repeater')
-    def test__get_ids_payload_id_in_data(
-        self,
-        iter_by_repeater,
-        get_by_payload_id,
-    ):
+    @patch('corehq.apps.data_interfaces.tasks.SQLRepeatRecord.objects.filter')
+    def test__get_ids_payload_id_in_data(self, get_by_payload_id):
         payload_id = Mock()
         _get_repeat_record_ids(payload_id, None, 'test_domain', False)
 
         self.assertEqual(get_by_payload_id.call_count, 1)
-        get_by_payload_id.assert_called_with(
-            'test_domain', payload_id)
-        self.assertEqual(iter_by_repeater.call_count, 0)
+        get_by_payload_id.assert_called_with(domain='test_domain', payload_id=payload_id)
 
-    @patch('corehq.apps.data_interfaces.tasks.get_couch_repeat_record_ids_by_payload_id')
-    @patch('corehq.apps.data_interfaces.tasks.iter_repeat_record_ids_by_repeater')
-    def test__get_ids_payload_id_not_in_data(
-        self,
-        iter_by_repeater,
-        get_by_payload_id,
-    ):
+    @patch('corehq.apps.data_interfaces.tasks.SQLRepeatRecord.objects.filter')
+    def test__get_ids_payload_id_not_in_data(self, iter_by_repeater):
         REPEATER_ID = 'c0ffee'
         _get_repeat_record_ids(None, REPEATER_ID, 'test_domain', False)
 
-        get_by_payload_id.assert_not_called()
-        iter_by_repeater.assert_called_with(
-            'test_domain', REPEATER_ID)
+        iter_by_repeater.assert_called_with(domain='test_domain', repeater__id=REPEATER_ID)
         self.assertEqual(iter_by_repeater.call_count, 1)
 
-    @patch('corehq.motech.repeaters.models.RepeatRecord')
-    def test__validate_record_record_does_not_exist(self, mock_RepeatRecord):
-        mock_RepeatRecord.get.side_effect = [ResourceNotFound]
-        response = _get_couch_repeat_record('test_domain', 'id_1')
+    @patch('corehq.motech.repeaters.models.SQLRepeatRecord.objects')
+    def test__validate_record_record_does_not_exist(self, mock_objects):
+        mock_objects.get.side_effect = [SQLRepeatRecord.DoesNotExist]
+        response = _get_sql_repeat_record('test_domain', '1234')
 
-        mock_RepeatRecord.get.assert_called_once()
+        mock_objects.get.assert_called_once_with(domain='test_domain', id='1234')
         self.assertIsNone(response)
 
-    @patch('corehq.motech.repeaters.models.RepeatRecord')
-    def test__validate_record_invalid_domain(self, mock_RepeatRecord):
-        mock_payload = Mock()
-        mock_payload.domain = 'domain'
-        mock_RepeatRecord.get.return_value = mock_payload
-        response = _get_couch_repeat_record('test_domain', 'id_1')
+    @patch('corehq.motech.repeaters.models.SQLRepeatRecord.objects')
+    def test__validate_record_invalid_domain(self, mock_objects):
+        mock_objects.get.side_effect = SQLRepeatRecord.DoesNotExist
+        response = _get_sql_repeat_record('test_domain', '1234')
 
-        mock_RepeatRecord.get.assert_called_once()
+        mock_objects.get.assert_called_once_with(domain='test_domain', id='1234')
         self.assertIsNone(response)
 
-    @patch('corehq.motech.repeaters.models.RepeatRecord')
-    def test__validate_record_success(self, mock_RepeatRecord):
-        mock_payload = Mock()
-        mock_payload.domain = 'test_domain'
-        mock_RepeatRecord.get.return_value = mock_payload
-        response = _get_couch_repeat_record('test_domain', 'id_1')
+    @patch('corehq.motech.repeaters.models.SQLRepeatRecord.objects')
+    def test__validate_record_success(self, mock_objects):
+        mock_record = Mock()
+        mock_record.domain = 'test_domain'
+        mock_objects.get.return_value = mock_record
+        response = _get_sql_repeat_record('test_domain', '1234')
 
-        mock_RepeatRecord.get.assert_called_once()
-        self.assertEqual(response, mock_payload)
+        mock_objects.get.assert_called_once_with(domain='test_domain', id='1234')
+        self.assertEqual(response, mock_record)
+
+    @patch('corehq.motech.repeaters.models.SQLRepeatRecord.objects')
+    def test__validate_record_success_with_couch_id(self, mock_objects):
+        couch_id = 'b6859ae05fd94dccbc3dfd25cdc6cb2c'
+        mock_record = Mock()
+        mock_record.domain = 'test_domain'
+        mock_objects.get.return_value = mock_record
+        response = _get_sql_repeat_record('test_domain', couch_id)
+
+        mock_objects.get.assert_called_once_with(domain='test_domain', couch_id=couch_id)
+        self.assertEqual(response, mock_record)
+        assert issubclass(SQLRepeatRecord, SyncSQLToCouchMixin), \
+            "couch_id not supported? Should this test be removed?"
 
 
 class TestTasks(TestCase):
 
     def setUp(self):
-        self.mock_payload_one = Mock(id='id_1')
-        self.mock_payload_two = Mock(id='id_2')
+        self.mock_payload_one = Mock(id=1)
+        self.mock_payload_two = Mock(id=2)
         self.mock_payload_ids = [self.mock_payload_one.id,
                                  self.mock_payload_two.id]
 
@@ -104,6 +100,7 @@ class TestTasks(TestCase):
         mock_operate_on_payloads,
         mock__get_ids,
     ):
+        mock__get_ids.return_value = record_ids = [1, 2, 3]
         payload_id = 'c0ffee'
         repeater_id = 'deadbeef'
         task_generate_ids_and_operate_on_payloads(
@@ -113,15 +110,13 @@ class TestTasks(TestCase):
         mock__get_ids.assert_called_with(
             'c0ffee', 'deadbeef', 'test_domain', False)
 
-        mock_record_ids = mock__get_ids(
-            'c0ffee', 'deadbeef', 'test_domain', False)
         mock_operate_on_payloads.assert_called_once()
         mock_operate_on_payloads.assert_called_with(
-            mock_record_ids, 'test_domain', 'test_action', False,
+            record_ids, 'test_domain', 'test_action', False,
             task=task_generate_ids_and_operate_on_payloads)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_false_resend(
         self,
         mock__validate_record,
@@ -146,7 +141,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_true_resend(
         self,
         mock__validate_record,
@@ -167,7 +162,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_false_resend(
         self,
         mock__validate_record,
@@ -192,7 +187,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_true_resend(
         self,
         mock__validate_record,
@@ -214,7 +209,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_false_cancel(
         self,
         mock__validate_record,
@@ -239,7 +234,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_true_cancel(
         self,
         mock__validate_record,
@@ -260,7 +255,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_false_cancel(
         self,
         mock__validate_record,
@@ -285,7 +280,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_true_cancel(
         self,
         mock__validate_record,
@@ -307,7 +302,7 @@ class TestTasks(TestCase):
                            response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_false_requeue(
         self,
         mock__validate_record,
@@ -332,7 +327,7 @@ class TestTasks(TestCase):
                             response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_no_task_from_excel_true_requeue(
         self,
         mock__validate_record,
@@ -353,7 +348,7 @@ class TestTasks(TestCase):
                             response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_false_requeue(
         self,
         mock__validate_record,
@@ -378,7 +373,7 @@ class TestTasks(TestCase):
                             response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_with_task_from_excel_true_requeue(
         self,
         mock__validate_record,
@@ -400,7 +395,7 @@ class TestTasks(TestCase):
                             response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_throws_exception_resend(
         self,
         mock__validate_record,
@@ -426,7 +421,7 @@ class TestTasks(TestCase):
         self.assertEqual(response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_throws_exception_cancel(
         self,
         mock__validate_record,
@@ -454,7 +449,7 @@ class TestTasks(TestCase):
         self.assertEqual(response, expected_response)
 
     @patch('corehq.apps.data_interfaces.utils.DownloadBase')
-    @patch('corehq.apps.data_interfaces.utils._get_couch_repeat_record')
+    @patch('corehq.apps.data_interfaces.utils._get_sql_repeat_record')
     def test_operate_on_payloads_throws_exception_requeue(
         self,
         mock__validate_record,
@@ -476,9 +471,7 @@ class TestTasks(TestCase):
 
         self.assertEqual(mock_DownloadBase.set_progress.call_count, 3)
         self.assertEqual(self.mock_payload_one.requeue.call_count, 1)
-        self.assertEqual(self.mock_payload_one.save.call_count, 1)
         self.assertEqual(self.mock_payload_two.requeue.call_count, 1)
-        self.assertEqual(self.mock_payload_two.save.call_count, 0)
         self.assertEqual(response, expected_response)
 
     def _check_resend(self, mock_payload_one, mock_payload_two,
@@ -498,9 +491,7 @@ class TestTasks(TestCase):
     def _check_requeue(self, mock_payload_one, mock_payload_two,
                        response, expected_response):
         self.assertEqual(mock_payload_one.requeue.call_count, 1)
-        self.assertEqual(mock_payload_one.save.call_count, 1)
         self.assertEqual(mock_payload_two.requeue.call_count, 0)
-        self.assertEqual(mock_payload_two.save.call_count, 0)
         self.assertEqual(response, expected_response)
 
 
@@ -520,12 +511,10 @@ class TestGetRepeatRecordIDs(TestCase):
         cls.repeater.save()
         cls.create_repeat_records()
 
-    @classmethod
-    def tearDownClass(cls):
-        for record in cls.couch_records:
-            record.delete(sync_to_sql=False)
-        cls.repeater.delete()
-        super().tearDownClass()
+        def delete_couch_records():
+            for record in cls.couch_records:
+                record.delete(sync_to_sql=False)
+        cls.addClassCleanup(delete_couch_records)
 
     @classmethod
     def create_repeat_records(cls):
@@ -552,31 +541,14 @@ class TestGetRepeatRecordIDs(TestCase):
                                         domain=DOMAIN, use_sql=True)
         self.assertEqual(result, [])
 
-    def test_no_payload_id_no_repeater_id_couch(self):
-        result = _get_repeat_record_ids(payload_id=None, repeater_id=None,
-                                        domain=DOMAIN, use_sql=False)
-        self.assertEqual(result, [])
-
     def test_payload_id_sql(self):
         result = _get_repeat_record_ids(payload_id=self.instance_id,
                                         repeater_id=None,
                                         domain=DOMAIN, use_sql=True)
         self.assertEqual(set(result), {r.pk for r in self.sql_records})
 
-    def test_payload_id_couch(self):
-        result = _get_repeat_record_ids(payload_id=self.instance_id,
-                                        repeater_id=None,
-                                        domain=DOMAIN, use_sql=False)
-        self.assertEqual(set(result), {r._id for r in self.couch_records})
-
     def test_repeater_id_sql(self):
         result = _get_repeat_record_ids(payload_id=None,
                                         repeater_id=self.repeater.repeater_id,
                                         domain=DOMAIN, use_sql=True)
         self.assertEqual(set(result), {r.pk for r in self.sql_records})
-
-    def test_repeater_id_couch(self):
-        result = _get_repeat_record_ids(payload_id=None,
-                                        repeater_id=self.repeater.repeater_id,
-                                        domain=DOMAIN, use_sql=False)
-        self.assertEqual(set(result), {r._id for r in self.couch_records})

corehq/apps/data_interfaces/utils.py
@@ -13,7 +13,6 @@ from corehq.apps.casegroups.models import CommCareCaseGroup
 from corehq.apps.domain.models import Domain
 from corehq.apps.domain_migration_flags.api import any_migrations_in_progress
 from corehq.form_processor.models import CommCareCase, XFormInstance
-from corehq.motech.repeaters.const import RECORD_CANCELLED_STATE
 
 
 def add_cases_to_case_group(domain, case_group_id, uploaded_data, progress_tracker):
@@ -126,10 +125,10 @@ def property_references_parent(case_property):
 
 
 def operate_on_payloads(
-    repeat_record_ids: List[str],
+    repeat_record_ids: List,
     domain: str,
     action,  # type: Literal['resend', 'cancel', 'requeue']  # 3.8+
-    use_sql: bool,
+    use_sql: bool = True,
     task: Optional = None,
     from_excel: bool = False,
 ):
@@ -147,10 +146,7 @@ def operate_on_payloads(
         DownloadBase.set_progress(task, 0, len(repeat_record_ids))
 
     for record_id in repeat_record_ids:
-        if use_sql:
-            record = _get_sql_repeat_record(domain, record_id)
-        else:
-            record = _get_couch_repeat_record(domain, record_id)
+        record = _get_sql_repeat_record(domain, record_id)
 
         if record:
             try:
@@ -158,16 +154,11 @@ def operate_on_payloads(
                     record.fire(force_send=True)
                     message = _("Successfully resent repeat record (id={})").format(record_id)
                 elif action == 'cancel':
-                    if use_sql:
-                        record.state = RECORD_CANCELLED_STATE
-                    else:
-                        record.cancel()
+                    record.cancel()
                     record.save()
                     message = _("Successfully cancelled repeat record (id={})").format(record_id)
                 elif action == 'requeue':
                     record.requeue()
-                    if not use_sql:
-                        record.save()
                     message = _("Successfully requeued repeat record (id={})").format(record_id)
                 else:
                     raise ValueError(f'Unknown action {action!r}')
@@ -193,23 +184,12 @@ def operate_on_payloads(
     return {"messages": response}
 
 
-def _get_couch_repeat_record(domain, record_id):
-    from corehq.motech.repeaters.models import RepeatRecord
-
-    try:
-        couch_record = RepeatRecord.get(record_id)
-    except ResourceNotFound:
-        return None
-    if couch_record.domain != domain:
-        return None
-    return couch_record
-
-
 def _get_sql_repeat_record(domain, record_id):
-    from corehq.motech.repeaters.models import SQLRepeatRecord
+    from corehq.motech.repeaters.models import SQLRepeatRecord, is_sql_id
 
+    where = {"id": record_id} if is_sql_id(record_id) else {"couch_id": record_id}
     try:
-        return SQLRepeatRecord.objects.get(domain=domain, pk=record_id)
+        return SQLRepeatRecord.objects.get(domain=domain, **where)
     except SQLRepeatRecord.DoesNotExist:
         return None
 

commit 11865665fb6c676769c766dc29d9ba820bc80a18

Get all repeat records in domain
...when payload_id=None and repeater_id=None
Bug introduced in commit:24412b11109b4511c27c58c80e46b315822749f3

corehq/apps/data_interfaces/tasks.py
@@ -222,18 +222,11 @@ def task_generate_ids_and_operate_on_payloads(
 
 
 def _get_repeat_record_ids(payload_id, repeater_id, domain):
+    queryset = SQLRepeatRecord.objects.filter(domain=domain)
     if payload_id:
-        queryset = SQLRepeatRecord.objects.filter(
-            domain=domain,
-            payload_id=payload_id,
-        )
+        queryset = queryset.filter(payload_id=payload_id)
     elif repeater_id:
-        queryset = SQLRepeatRecord.objects.filter(
-            domain=domain,
-            repeater__id=repeater_id,
-        )
-    else:
-        return []
+        queryset = queryset.filter(repeater__id=repeater_id)
     return list(queryset.order_by().values_list("id", flat=True))
 
 

corehq/apps/data_interfaces/tests/test_tasks.py
@@ -4,7 +4,10 @@ from unittest.mock import patch
 from corehq.apps.data_interfaces.tasks import (
     task_generate_ids_and_operate_on_payloads,
     task_operate_on_payloads,
+    _get_repeat_record_ids,
 )
+from corehq.motech.repeaters.models import FormRepeater
+from corehq.motech.repeaters.tests.test_models import RepeaterTestCase
 
 
 class TestTasks(TestCase):
@@ -70,6 +73,7 @@ class TestTasks(TestCase):
             }
         })
 
+    @patch('corehq.apps.data_interfaces.tasks._get_repeat_record_ids', lambda *a: [])
     def test_task_generate_ids_and_operate_on_payloads_no_data(self):
         response = task_generate_ids_and_operate_on_payloads(
             payload_id=None,
@@ -79,3 +83,42 @@ class TestTasks(TestCase):
         )
         self.assertEqual(response,
                          {'messages': {'errors': ['No payloads specified']}})
+
+
+class TestRepeatRecordIdsQuery(RepeaterTestCase):
+
+    def setUp(self):
+        super().setUp()
+        self.repeater2 = FormRepeater(
+            domain="test",
+            connection_settings=self.conn,
+        )
+        self.repeater2.save()
+
+        self.record1 = self.new_record(payload_id='c0ffee')
+        self.record2 = self.new_record(payload_id='deadbeef')
+        self.record3 = self.new_record(payload_id='c0ffee', repeater=self.repeater2)
+
+    def test_get_repeat_record_ids_by_payload_id(self):
+        ids = _get_repeat_record_ids(
+            repeater_id=None,
+            payload_id="c0ffee",
+            domain="test",
+        )
+        self.assertEqual(set(ids), {self.record1.id, self.record3.id})
+
+    def test_get_repeat_record_ids_by_repeater_id(self):
+        ids = _get_repeat_record_ids(
+            repeater_id=self.repeater.id,
+            payload_id=None,
+            domain="test",
+        )
+        self.assertEqual(set(ids), {self.record1.id, self.record2.id})
+
+    def test_get_repeat_record_ids_by_domain(self):
+        ids = _get_repeat_record_ids(
+            repeater_id=None,
+            payload_id=None,
+            domain="test",
+        )
+        self.assertEqual(set(ids), {self.record1.id, self.record2.id, self.record3.id})