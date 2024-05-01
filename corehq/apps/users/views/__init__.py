f4b2bf36a2246dc259a7c5a5a43911702473b499
Jonathan Tang <Jonahtang738@gmail.com>
2024-05-01 00:05:51 -0700

    shows location on post request to invite page
    this occurs if there's an error upon sending invite and the
    invite web user view is called again

--- corehq/apps/users/views/__init__.py
+++ corehq/apps/users/views/__init__.py
@@ -1081,6 +1081,7 @@ class InviteWebUserView(BaseManageWebUserView):
                 role_choices=role_choices,
                 domain=self.domain,
                 is_add_user=is_add_user,
+                should_show_location=self.request.project.uses_locations
             )
         return AdminInvitesUserForm(
             initial=initial,
