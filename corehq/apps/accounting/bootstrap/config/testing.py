294ee4800def959727b1ae9db1cc6b897054a624
nospame <mapson.evan@gmail.com>
2024-05-08 10:26:20 -0700

    Add annual software plans to testing config

--- corehq/apps/accounting/bootstrap/config/testing.py
+++ corehq/apps/accounting/bootstrap/config/testing.py
@@ -56,4 +56,28 @@ BOOTSTRAP_CONFIG_TESTING = {
             FeatureType.SMS: dict(monthly_limit=UNLIMITED_FEATURE_USAGE),
         }
     },
+    (SoftwarePlanEdition.STANDARD, False, False, True): {
+        'role': 'standard_plan_v1',
+        'product_rate_monthly_fee': Decimal('250.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=125, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        },
+    },
+    (SoftwarePlanEdition.PRO, False, False, True): {
+        'role': 'pro_plan_v1',
+        'product_rate_monthly_fee': Decimal('500.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=250, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        },
+    },
+    (SoftwarePlanEdition.ADVANCED, False, False, True): {
+        'role': 'advanced_plan_v0',
+        'product_rate_monthly_fee': Decimal('1000.00'),
+        'feature_rates': {
+            FeatureType.USER: dict(monthly_limit=500, per_excess_fee=Decimal('2.00')),
+            FeatureType.SMS: dict(monthly_limit=0),
+        }
+    },
 }
