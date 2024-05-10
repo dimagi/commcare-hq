168e8f5c7a17be51eb45bb5a43fe0565566e8ff7
Simon Kelly <skelly@dimagi.com>
2024-05-10 16:01:42 +0200

    rate limit error emails

--- corehq/apps/app_execution/tasks.py
+++ corehq/apps/app_execution/tasks.py
@@ -9,6 +9,7 @@ from corehq.apps.app_execution.models import AppWorkflowConfig
 from corehq.apps.celery import periodic_task
 from corehq.util import reverse
 from corehq.util.log import send_HTML_email
+from dimagi.utils.rate_limit import rate_limit
 
 
 @periodic_task(run_every=crontab())  # run every minute
@@ -23,20 +24,9 @@ def run_app_workflows():
             log.success = False
             log.error = str(e)
 
-            url = reverse('app_execution:edit_workflow', args=[config.pk], absolute=True)
-            log_url = reverse('app_execution:workflow_log', args=[log.pk], absolute=True)
-            message = format_html("""Error executing workflow: <a href="url}">{name}</a>
-            <br><br>
-            <p>Log: {log_url}</p>
-            <p><pre>{traceback}</pre></p>
-            <p>Error:</p>
-            <p>{error}</p>
-            """, url=url, name=config.name, log_url=log_url, error=e, traceback=traceback.format_exc())
-            send_HTML_email(
-                f"App Execution Workflow Failure: {config.name}",
-                config.notification_emails,
-                message,
-            )
+            # rate limit to prevent spamming: 1 email per config per 10 minutes
+            if rate_limit(f"task-execution-error-{config.pk}", actions_allowed=1, how_often=10 * 60):
+                _email_error(config, e, log)
         else:
             log.success = True
         finally:
@@ -45,6 +35,33 @@ def run_app_workflows():
             log.output = session.log.getvalue()
             log.completed = timezone.now()
             log.save()
+
+
+def _email_error(config, e, log):
+    url = reverse('app_execution:edit_workflow', args=[config.pk], absolute=True)
+    log_url = reverse('app_execution:workflow_log', args=[log.pk], absolute=True)
+    all_logs_url = reverse('app_execution:workflow_logs', args=[config.pk], absolute=True)
+    message = format_html(
+        """Error executing workflow: <a href="url}">{name}</a>
+        <br><br>
+        <p>Log: {log_url}</p>
+        <p>All Logs: {all_logs_url}</p>
+        <p><pre>{traceback}</pre></p>
+        <p>Error:</p>
+        <p>{error}</p>
+        """,
+        url=url,
+        name=config.name,
+        log_url=log_url,
+        all_logs_url=all_logs_url,
+        error=e,
+        traceback=traceback.format_exc()
+    )
+    send_HTML_email(
+        f"App Execution Workflow Failure: {config.name}",
+        config.notification_emails,
+        message,
+    )
 
 
 @periodic_task(run_every=crontab(minute=0, hour=0))  # run every day at midnight
