b1863546bb9ed28146ae897e7096958d8781a864
Biyeun Buczyk <biyeun@dimagi.com>
2024-05-13 20:11:54 +0200

    add utils for bootstrap 5 paths

--- corehq/apps/hqwebapp/tests/utils/test_bootstrap_paths.py
+++ corehq/apps/hqwebapp/tests/utils/test_bootstrap_paths.py
@@ -1,5 +1,9 @@
 from testil import eq
-from corehq.apps.hqwebapp.utils.bootstrap.paths import is_ignored_path
+from corehq.apps.hqwebapp.utils.bootstrap.paths import (
+    is_ignored_path,
+    get_bootstrap5_path,
+    is_bootstrap3_path,
+)
 
 
 def test_is_ignored_path_true():
@@ -12,3 +16,22 @@ def test_is_ignored_path_false():
     path = "/path/to/corehq/apps/builds/templates/builds/base_builds.html"
     app_name = "builds"
     eq(is_ignored_path(app_name, path), False)
+
+
+def test_get_bootstrap5_path():
+    bootstrap3_path = "reports/bootstrap3/base_template.html"
+    bootstrap5_path = "reports/bootstrap5/base_template.html"
+    eq(get_bootstrap5_path(bootstrap3_path), bootstrap5_path)
+
+
+def test_get_bootstrap5_path_none():
+    eq(get_bootstrap5_path(None), None)
+
+
+def test_is_bootstrap3_path():
+    bootstrap3_path = "reports/bootstrap3/base_template.html"
+    eq(is_bootstrap3_path(bootstrap3_path), True)
+
+
+def test_is_bootstrap3_path_false_with_none():
+    eq(is_bootstrap3_path(None), False)
