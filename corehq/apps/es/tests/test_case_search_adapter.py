1449d036ab80d392f6675430216baddd36ff9dc1
Amit Phulera <amitphulera999@gmail.com>
2024-05-14 14:32:26 +0530

    add tests for index operation on case-search adapter

--- corehq/apps/es/tests/test_case_search_adapter.py
+++ corehq/apps/es/tests/test_case_search_adapter.py
@@ -1,6 +1,9 @@
+from unittest.mock import patch
+
 from django.test import TestCase
 
 from corehq.apps.es.case_search import case_search_adapter
+from corehq.apps.es.case_search_bha import case_search_bha_adapter
 from corehq.apps.es.tests.utils import es_test
 from corehq.form_processor.tests.utils import create_case
 
@@ -43,3 +46,46 @@ class TestFromPythonInCaseSearch(TestCase):
         es_case = case_search_adapter.search({})['hits']['hits'][0]['_source']
         es_case.pop('@indexed_on')
         self.assertEqual(es_case, case)
+
+
+@es_test(requires=[case_search_adapter, case_search_bha_adapter], setup_class=True)
+class TestCaseSearchAdapterAlsoWritesToAnotherIndex(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.domain = 'casesearch-dual-writetests'
+        cls.case = create_case(cls.domain, save=True)
+
+    def _get_normalized_cases_from_hits(self, cases):
+        normalised_cases = []
+        for hit in cases['hits']['hits']:
+            normalised_case = self._normalise_case(hit['_source'])
+            normalised_cases.append(normalised_case)
+        return normalised_cases
+
+    def _normalise_case(self, case):
+        case.pop('@indexed_on')
+        return case
+
+    def test_index_writes_to_both_adapters(self):
+        with patch('corehq.apps.es.case_search.multiplex_to_adapter', return_value=case_search_bha_adapter):
+            case_search_adapter.index(self.case, refresh=True)
+            self.addCleanup(case_search_bha_adapter.delete, self.case.case_id)
+            self.addCleanup(case_search_adapter.delete, self.case.case_id)
+
+        docs_in_bha = self._get_normalized_cases_from_hits(case_search_bha_adapter.search({}))
+        docs_in_case_search = self._get_normalized_cases_from_hits(case_search_adapter.search({}))
+
+        self.assertEqual(docs_in_bha, docs_in_case_search)
+
+    def test_index_not_writes_to_bha_adapter_if_not_required(self):
+        with patch('corehq.apps.es.case_search.multiplex_to_adapter', return_value=None):
+            case_search_adapter.index(self.case, refresh=True)
+            self.addCleanup(case_search_adapter.delete, self.case.case_id)
+
+        docs_in_bha = self._get_normalized_cases_from_hits(case_search_bha_adapter.search({}))
+        docs_in_case_search = self._get_normalized_cases_from_hits(case_search_adapter.search({}))
+
+        self.assertEqual(docs_in_bha, [])
+        self.assertEqual(len(docs_in_case_search), 1)
