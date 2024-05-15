036222b328b134d392cd957e540a49b82939eddf
Jenny Schweers <jschweers@dimagi.com>
2024-05-15 19:43:17 +0000

    "Bootstrap 5 Migration - initial auto-migration for dhis2/js/dataset_map_update.js, migrated in-place"

--- corehq/motech/dhis2/static/dhis2/js/dataset_map_update.js
+++ corehq/motech/dhis2/static/dhis2/js/dataset_map_update.js
@@ -1,7 +1,7 @@
 hqDefine("dhis2/js/dataset_map_update", [
     "jquery",
-    "hqwebapp/js/bootstrap3/crud_paginated_list_init",
-    "hqwebapp/js/bootstrap3/widgets",
+    "hqwebapp/js/bootstrap5/crud_paginated_list_init",
+    "hqwebapp/js/bootstrap5/widgets",
 ], function ($) {
     function showCompleteDateColumnInput(shouldShow) {
         var label = $('label[for="id_complete_date_column"]').hide();
