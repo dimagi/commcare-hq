8d1d8adf4883fc3059dc7e742f90d3a5eaad949c
Jonathan Tang <Jonahtang738@gmail.com>
2024-05-15 22:41:00 -0700

    provides instructions for manually migration upon failure
    if failure occurs from changes to the code (such as model changes)
    then linked changelog will provide instructions to properly manually migrate

--- corehq/apps/users/migrations/0065_transfer_to_tableau_config_permission.py
+++ corehq/apps/users/migrations/0065_transfer_to_tableau_config_permission.py
@@ -1,3 +1,6 @@
+import sys
+import traceback
+
 from django.db import migrations
 from django.db.models import Q
 
@@ -6,24 +9,76 @@ from corehq.util.django_migrations import skip_on_fresh_install
 from corehq.util.log import with_progress_bar
 from corehq.apps.users.models import HqPermissions
 from corehq.toggles import TABLEAU_USER_SYNCING
+from corehq.apps.domain_migration_flags.api import (
+    ALL_DOMAINS,
+    get_migration_status,
+    set_migration_complete,
+    set_migration_started,
+)
+from corehq.apps.domain_migration_flags.exceptions import (
+    DomainMigrationProgressError,
+)
+from corehq.apps.domain_migration_flags.models import MigrationStatus
 from corehq.apps.users.models import UserRole
+
+MIGRATION_SLUG = "mirror_web_users_permissions_to_tableau_config_permissions"
+AUTO_MIGRATE_FAILED_MESSAGE = """
+This migration cannot be performed automatically and must instead be run manually
+before this environment can be upgraded to the latest version of CommCare HQ.
+Instructions for running the migration can be found at this link:
+
+TODO: Update with changelog link
+"""
 
 
 @skip_on_fresh_install
-def transfer_web_user_permission_to_tableau_config_permission(apps, schema_editor):
-    user_role_ids_to_migrate = get_user_role_ids_to_migrate()
+def _assert_migrated(apps, schema_editor):
 
-    for chunk in with_progress_bar(chunked(user_role_ids_to_migrate, 1000),
-                                length=len(user_role_ids_to_migrate)):
-        for role in UserRole.objects.filter(id__in=chunk):
-            permissions = role.permissions
-            if permissions.edit_web_users:
-                permissions.edit_user_tableau_config = True
-                permissions.view_user_tableau_config = True
-            elif permissions.view_web_users:
-                permissions.view_user_tableau_config = True
+    def transfer_web_user_permission_to_tableau_config_permission():
+        user_role_ids_to_migrate = get_user_role_ids_to_migrate()
 
-            role.set_permissions(permissions.to_list())
+        status = get_migration_status(ALL_DOMAINS, MIGRATION_SLUG)
+        if status == MigrationStatus.COMPLETE:
+            print("Manage Tableau Configuration permissions have already been populated from Web Users permissons")
+            return
+        if status not in (MigrationStatus.IN_PROGRESS, MigrationStatus.COMPLETE):
+            set_migration_started(ALL_DOMAINS, MIGRATION_SLUG)
+
+        for chunk in with_progress_bar(chunked(user_role_ids_to_migrate, 1000),
+                                    length=len(user_role_ids_to_migrate)):
+            for role in UserRole.objects.filter(id__in=chunk):
+                permissions = role.permissions
+                if permissions.edit_web_users:
+                    permissions.edit_user_tableau_config = True
+                    permissions.view_user_tableau_config = True
+                elif permissions.view_web_users:
+                    permissions.view_user_tableau_config = True
+
+                role.set_permissions(permissions.to_list())
+
+        try:
+            set_migration_complete(ALL_DOMAINS, MIGRATION_SLUG)
+        except DomainMigrationProgressError:
+            raise
+
+    num_items = len(get_user_role_ids_to_migrate())
+
+    migrated = num_items == 0
+    if migrated:
+        return
+
+    try:
+        transfer_web_user_permission_to_tableau_config_permission()
+        migrated = len(get_user_role_ids_to_migrate()) == 0
+        if not migrated:
+            print("Automatic migration failed")
+    except Exception:
+        traceback.print_exc()
+
+    if not migrated:
+        print("")
+        print(AUTO_MIGRATE_FAILED_MESSAGE)
+        sys.exit(1)
 
 
 class Migration(migrations.Migration):
@@ -33,7 +88,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(transfer_web_user_permission_to_tableau_config_permission, migrations.RunPython.noop)
+        migrations.RunPython(_assert_migrated, migrations.RunPython.noop)
     ]
 
 
