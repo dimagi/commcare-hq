6aff3134527d5b2b228a3e60a788e91d96ae6ac7
Szymon Balasz <sbalasz@gmail.com>
2024-05-17 15:51:26 +0200

    unit test location upload

--- corehq/apps/locations/tests/test_views.py
+++ corehq/apps/locations/tests/test_views.py
@@ -1,16 +1,21 @@
 import json
 
 from django.contrib.messages import get_messages
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.handlers.wsgi import WSGIRequest
 from django.test import Client, TestCase
 from django.urls import reverse
 
+from io import BytesIO
+from openpyxl import Workbook
 from unittest import mock
 
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.locations.exceptions import LocationConsistencyError
 from corehq.apps.locations.models import LocationType
-from corehq.apps.locations.views import LocationTypesView
-from corehq.apps.users.models import WebUser
+from corehq.apps.locations.views import LocationTypesView, LocationImportView
+from corehq.apps.users.models import WebUser, HQApiKey
+from corehq.util.workbook_json.excel import WorkbookJSONError
 
 OTHER_DETAILS = {
     'expand_from': None,
@@ -98,3 +103,118 @@ class LocationTypesViewTest(TestCase):
         response = self.send_request(data)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, self.url)
+
+
+class BulkLocationUploadAPITest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(BulkLocationUploadAPITest, cls).setUpClass()
+        cls.domain_name = 'bulk-upload-domain'
+        cls.domain = create_domain(cls.domain_name)
+        cls.domain.save()
+        cls.addClassCleanup(cls.domain.delete)
+        cls.user = WebUser.create(cls.domain_name, 'test@test.com', 'password', created_by=None, created_via=None)
+        cls.addClassCleanup(cls.user.delete, cls.domain_name, deleted_by=None)
+        cls.api_key = HQApiKey.objects.create(user=cls.user.get_django_user(), domain=cls.domain_name)
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('bulk_location_upload_api', args=[self.domain_name])
+
+    @mock.patch('corehq.apps.locations.views.import_locations_async')
+    @mock.patch('corehq.apps.locations.views.LocationImportView.cache_file')
+    def test_success(self, mock_cache_file, mock_import_locations_async):
+        mock_file_ref = mock.MagicMock()
+        mock_file_ref.download_id = 'mock_download_id'
+        mock_file_ref.set_task = mock.MagicMock()
+        mock_cache_file.return_value = LocationImportView.Ref(mock_file_ref)
+        mock_import_locations_async.delay.return_value = None
+
+        workbook = Workbook()
+        file = BytesIO()
+        workbook.save(file)
+        file.seek(0)
+        file.name = 'mock_file.xlsx'
+
+        response = self.client.post(
+            self.url,
+            {'bulk_upload_file': file},
+            HTTP_AUTHORIZATION=f'ApiKey {self.user.username}:{self.api_key.key}',
+            format='multipart'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'success': True})
+
+        mock_cache_file.assert_called_once()
+        _, call_args, call_kwargs = mock_cache_file.mock_calls[0]
+        self.assertIsInstance(call_args[0], WSGIRequest)
+        self.assertEqual(call_args[1], self.domain_name)
+        self.assertIsInstance(call_args[2], InMemoryUploadedFile)
+        self.assertEqual(call_args[2].name, 'mock_file.xlsx')
+
+        mock_import_locations_async.delay.assert_called_once_with(
+            self.domain_name, 'mock_download_id', self.user.user_id
+        )
+        mock_file_ref.set_task.assert_called_once()
+
+    def test_api_no_authentication(self):
+        response = self.client.post(self.url, {'bulk_upload_file': 'mock_file'})
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.content.decode(), 'Authorization Required')
+
+    def test_api_invalid_authentication(self):
+        response = self.client.post(
+            self.url,
+            {'bulk_upload_file': 'mock_file'},
+            HTTP_AUTHORIZATION=f'ApiKey {self.user.username}:invalid_key'
+        )
+        self.assertEqual(response.status_code, 401)
+
+    def test_no_file_uploaded(self):
+        response = self.client.post(
+            self.url,
+            HTTP_AUTHORIZATION=f'ApiKey {self.user.username}:{self.api_key.key}',
+            format='multipart'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'success': False, 'message': 'no file uploaded'})
+
+    @mock.patch('corehq.apps.locations.views.get_workbook')
+    def test_invalid_file_format(self, mock_get_workbook):
+        mock_get_workbook.side_effect = WorkbookJSONError('Invalid file format')
+        file = BytesIO(b'invalid file content')
+        file.name = 'invalid_file.txt'
+        response = self.client.post(
+            self.url,
+            {'bulk_upload_file': file},
+            HTTP_AUTHORIZATION=f'ApiKey {self.user.username}:{self.api_key.key}',
+            format='multipart'
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'success': False, 'message': 'Invalid file format'})
+
+    @mock.patch('corehq.apps.locations.views.get_redis_lock')
+    def test_concurrent_update_in_progress(self, mock_get_redis_lock):
+        mock_lock = mock.MagicMock()
+        mock_lock.acquire.return_value = False
+        mock_get_redis_lock.return_value = mock_lock
+
+        workbook = Workbook()
+        file = BytesIO()
+        workbook.save(file)
+        file.seek(0)
+        file.name = 'file.xlsx'
+
+        response = self.client.post(
+            self.url,
+            {'bulk_upload_file': file},
+            HTTP_AUTHORIZATION=f'ApiKey {self.user.username}:{self.api_key.key}',
+            format='multipart'
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'success': False,
+            'message': 'Some of the location edits are still in progress, '
+                       'please wait until they finish and then try again'
+        })
