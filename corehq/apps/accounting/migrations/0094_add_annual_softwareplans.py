51137f2c3ffcea0a7773267b0baec00e9c877886
nospame <mapson.evan@gmail.com>
2024-05-20 11:53:41 -0700

    Make migration reversible

--- corehq/apps/accounting/migrations/0094_add_annual_softwareplans.py
+++ corehq/apps/accounting/migrations/0094_add_annual_softwareplans.py
@@ -9,6 +9,15 @@ def _bootstrap_new_annual_pricing(apps, schema_editor):
     ensure_plans(BOOTSTRAP_CONFIG, verbose=True, apps=apps)
 
 
+def _remove_annual_default_product_plans(apps, schema_editor):
+    # annual SoftwarePlans may have already existed, so only delete the DefaultProductPlans
+    DefaultProductPlan = apps.get_model('accounting', 'DefaultProductPlan')
+    annual_default_product_plans = DefaultProductPlan.objects.filter(is_annual_plan=True)
+
+    assert len(annual_default_product_plans) == 3
+    annual_default_product_plans.delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -22,5 +31,6 @@ class Migration(migrations.Migration):
                 ("edition", "is_trial", "is_report_builder_enabled", "is_annual_plan")
             },
         ),
-        migrations.RunPython(_bootstrap_new_annual_pricing),
+        migrations.RunPython(_bootstrap_new_annual_pricing,
+                             reverse_code=_remove_annual_default_product_plans),
     ]
