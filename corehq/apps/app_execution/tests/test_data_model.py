76c5dbd08853c5557653ce0e618770bed8618416
Simon Kelly <skelly@dimagi.com>
2024-05-22 15:02:35 +0200

    DSL

--- corehq/apps/app_execution/tests/test_data_model.py
+++ corehq/apps/app_execution/tests/test_data_model.py
@@ -1,4 +1,5 @@
 import itertools
+from itertools import zip_longest
 
 from django.test import SimpleTestCase
 from testil import eq
@@ -8,7 +9,6 @@ from corehq.apps.app_execution.data_model import STEP_MAP
 
 
 class DataModelTest(SimpleTestCase):
-
     def test_workflow_has_all_step_types(self):
         workflow = _get_workflow()
         steps = []
@@ -30,9 +30,39 @@ class DataModelTest(SimpleTestCase):
         workflow = data_model.AppWorkflow.__jsonattrs_from_json__(_get_workflow_json())
         eq(workflow, _get_workflow())
 
+    def test_to_dsl(self):
+        actual = _get_workflow().to_dsl()
+        if actual != _get_dsl():
+            for actual_line, expected_line in zip_longest(actual.splitlines(), _get_dsl().splitlines()):
+                eq(actual_line, expected_line)
 
-def _get_workflow():
-    return data_model.AppWorkflow(steps=[
+    def test_from_dsl(self):
+        workflow = data_model.AppWorkflow.from_dsl(_get_dsl(with_raw=False))
+        eq(workflow, _get_workflow(with_raw=False))
+
+
+def _get_dsl(with_raw=True):
+    lines = [
+        'select menu "Case Search"',
+        'update search parameters first_name="query value"',
+        'update search parameters last_name="query value"',
+        'search with parameters first_name="query value", last_name="query value"',
+        'select entity with id "123"',
+        'select entity at index 2',
+        'clear search',
+        'select menu "Followup Case"',
+        'select entities with ids "xyz, abc"',
+        'select entities at indexes "0, 2"',
+        'answer question "Name" with "str"',
+        'submit form',
+    ]
+    if with_raw:
+        lines = ['navigate using raw request data'] + lines
+    return '\n'.join(lines)
+
+
+def _get_workflow(with_raw=True):
+    steps = [
         data_model.CommandStep("Case Search"),
         data_model.QueryInputValidationStep({"first_name": "query value"}),
         data_model.QueryInputValidationStep({"last_name": "query value"}),
@@ -40,20 +70,23 @@ def _get_workflow():
         data_model.EntitySelectStep("123"),
         data_model.EntitySelectIndexStep(2),
         data_model.ClearQueryStep(),
-        data_model.RawNavigationStep(request_data={"selections": ["0", "1", "123abc"]}),
         data_model.CommandStep("Followup Case"),
         data_model.MultipleEntitySelectStep(values=["xyz", "abc"]),
         data_model.MultipleEntitySelectByIndexStep(values=[0, 2]),
         data_model.FormStep(children=[
-            data_model.AnswerQuestionStep(question_text='Name', question_id='name', value='str'),
+            data_model.AnswerQuestionStep(question_text='Name', value='str'),
             data_model.SubmitFormStep()
         ]),
-    ])
+    ]
+    if with_raw:
+        steps = [data_model.RawNavigationStep(request_data={"selections": ["0", "1", "123abc"]})] + steps
+    return data_model.AppWorkflow(steps=steps)
 
 
 def _get_workflow_json():
     return {
         "steps": [
+            {"type": "raw_navigation", "request_data": {"selections": ["0", "1", "123abc"]}},
             {"type": "command", "value": "Case Search"},
             {"type": "query_input_validation", "inputs": {"first_name": "query value"}},
             {"type": "query_input_validation", "inputs": {"last_name": "query value"}},
@@ -65,7 +98,6 @@ def _get_workflow_json():
             {"type": "entity_select", "value": "123"},
             {"type": "entity_select_index", "value": 2},
             {"type": "clear_query"},
-            {"type": "raw_navigation", "request_data": {"selections": ["0", "1", "123abc"]}},
             {"type": "command", "value": "Followup Case"},
             {"type": "multiple_entity_select", "values": ["xyz", "abc"]},
             {"type": "multiple_entity_select_by_index", "values": [0, 2]},
@@ -75,7 +107,7 @@ def _get_workflow_json():
                     {
                         "type": "answer_question",
                         "question_text": "Name",
-                        "question_id": "name",
+                        "question_id": None,
                         "value": "str",
                     },
                     {"type": "submit_form"}
