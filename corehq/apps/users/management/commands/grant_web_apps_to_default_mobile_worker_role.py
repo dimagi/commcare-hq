8b7383e18c703e54bade89a438e88f8c046f25b6
Jenny Schweers <jschweers@dimagi.com>
2024-05-23 14:07:47 -0400

    Moved permissions.web_apps_list check before counting users, which is potentially expensive

--- corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
+++ corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
@@ -12,11 +12,10 @@ class Command(BaseCommand):
         roles_to_update = UserRole.objects.filter(is_commcare_user_default=True)
         for role in roles_to_update:
             permissions = role.permissions
-            if not permissions.access_web_apps:
+            if not permissions.access_web_apps and not permissions.web_apps_list:
                 count = get_role_user_count(role.domain, role.couch_id, web_users_only=True)
                 if count == 0:
                     # Only update if role is limited to mobile workers, who already have access to web apps
-                    if not permissions.web_apps_list:
-                        permissions.access_web_apps = True
-                        permissions.normalize(previous=role.permissions)
-                        role.set_permissions(permissions.to_list())
+                    permissions.access_web_apps = True
+                    permissions.normalize(previous=role.permissions)
+                    role.set_permissions(permissions.to_list())
