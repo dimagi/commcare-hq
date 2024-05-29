8f650647dbdbd82b5dfcea67a0765ebbdabc0ce0
Jenny Schweers <jschweers@dimagi.com>
2024-05-29 11:47:03 -0400

    Bootstrap 5 Migration - initial auto-migration for dashboard/js/dashboard.js, migrated in-place

--- corehq/apps/dashboard/static/dashboard/js/dashboard.js
+++ corehq/apps/dashboard/static/dashboard/js/dashboard.js
@@ -3,8 +3,8 @@ hqDefine("dashboard/js/dashboard", [
     'knockout',
     'underscore',
     'hqwebapp/js/initial_page_data',
-    'hqwebapp/js/bootstrap3/components.ko',    // pagination widget
-    'hqwebapp/js/bootstrap3/main',     // post-link function
+    'hqwebapp/js/bootstrap5/components.ko',    // pagination widget
+    'hqwebapp/js/bootstrap5/main',     // post-link function
 ], function (
     $,
     ko,
