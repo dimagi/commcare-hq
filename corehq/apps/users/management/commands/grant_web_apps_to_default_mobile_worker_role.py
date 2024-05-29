239ffa36af29f0f6ce0b988ec6263f6567a41e4e
Jenny Schweers <jschweers@dimagi.com>
2024-05-29 11:09:14 -0400

    Added simple logging to user role script

--- corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
+++ corehq/apps/users/management/commands/grant_web_apps_to_default_mobile_worker_role.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from corehq.apps.users.analytics import get_role_user_count
 from corehq.apps.users.models_role import Permission, UserRole
+
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -10,7 +15,11 @@ class Command(BaseCommand):
     def handle(self, **options):
         permission, created = Permission.objects.get_or_create(value='access_web_apps')
         roles_to_update = UserRole.objects.filter(is_commcare_user_default=True)
-        for role in roles_to_update:
+        total = roles_to_update.count()
+        logger.info(f"Found {total} roles to update")
+        for index, role in enumerate(roles_to_update):
+            if index % 100 == 0:
+                logger.info(f"Examining role {index} of {total}")
             permissions = role.permissions
             if not permissions.access_web_apps and not permissions.web_apps_list:
                 count = get_role_user_count(role.domain, role.couch_id, web_users_only=True)
