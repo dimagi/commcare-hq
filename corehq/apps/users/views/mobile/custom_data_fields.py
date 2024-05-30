63f9256da4d138838f58fb2667d347a343dfe0c8
Jonathan Tang <Jonahtang738@gmail.com>
2024-05-30 15:23:58 -0700

    feat: adds function to fetch profiles based on role permission
    Other classes that inherits CustomDataModelMixin do not currently
    have profile restrictions so do not implement the "get_user_accesible_profiles" method
    and will use the one defined in the base class.

--- corehq/apps/users/views/mobile/custom_data_fields.py
+++ corehq/apps/users/views/mobile/custom_data_fields.py
@@ -2,7 +2,8 @@ from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
 
 from corehq.apps.custom_data_fields.edit_model import CustomDataModelMixin
-from corehq.apps.users.decorators import require_can_edit_commcare_users
+from corehq.apps.users.decorators import require_can_edit_commcare_users, get_permission_name
+from corehq.apps.users.models import HqPermissions
 from corehq.apps.users.tasks import remove_unused_custom_fields_from_users_task
 from corehq.apps.users.views import BaseUserSettingsView
 
@@ -23,3 +24,15 @@ class UserFieldsView(CustomDataModelMixin, BaseUserSettingsView):
 
     def update_existing_models(self):
         remove_unused_custom_fields_from_users_task.delay(self.domain)
+
+    @classmethod
+    def get_user_accessible_profiles(cls, domain, couch_user):
+        all_profiles = cls.get_definition_for_domain(domain).get_profiles()
+        if couch_user.has_permission(domain, get_permission_name(HqPermissions.edit_user_profile)):
+            return all_profiles
+
+        role = couch_user.get_role(domain)
+        permission = role.permissions if role else HqPermissions()
+        accessible_profile_ids = permission.edit_user_profile_list
+        accessible_profiles = {p for p in all_profiles if str(p.id) in accessible_profile_ids}
+        return accessible_profiles
