26498e9d2b893699e3fb9516277cf6869473f05a
mkangia <mkangia@dimagi.com>
2024-06-05 14:22:21 +0530

    add structure for new migration

--- custom/benin/management/commands/migrate_users_and_their_cases_to_new_rc_level.py
+++ custom/benin/management/commands/migrate_users_and_their_cases_to_new_rc_level.py
@@ -1,5 +1,11 @@
 from django.core.management.base import BaseCommand
 
+from corehq.apps.locations.models import SQLLocation
+from corehq.apps.users.user_data import prime_user_data_caches
+
+LOCATION_TYPE_VILLAGE = "Village"
+LOCATION_TYPE_RC = "RC"
+
 
 class Command(BaseCommand):
     help = 'Migrate benin project\'s users and their cases to new rc level locations'
@@ -35,4 +41,72 @@ class Command(BaseCommand):
                         1. Update owner to be the corresponding RC location
                     4. Update users location to corresponding RC location
         """
-        pass
+        villages = _find_locations(domain=domain, location_type_code=LOCATION_TYPE_VILLAGE)
+        for village in villages:
+            users = _find_rc_users_at_location(domain, village)
+            for user in users:
+                user_rc_number = user.user_data.get('rc_number')
+                if user_rc_number:
+                    new_user_rc_location = _find_child_location_with_name(
+                        parent_location=village,
+                        location_name=user_rc_number
+                    )
+                    if new_user_rc_location:
+                        _update_cases(user=user, current_owner_id=village.location_id,
+                                      new_owner_id=new_user_rc_location.location_id)
+                        _update_users_location(user=user, location=new_user_rc_location)
+                    else:
+                        log(f"User {user.username}:{user.user_id} rc {user_rc_number} location not found ")
+                else:
+                    log(f"User {user.username}:{user.user_id} missing rc number")
+
+
+def _find_locations(domain, location_type_code):
+    return SQLLocation.active_objects.filter(
+        domain=domain,
+        location_type__code=location_type_code
+    )
+
+
+def _find_rc_users_at_location(domain, location):
+    # return users with usertype as 'rc'
+    users = _find_users_at_location(location)
+    prime_user_data_caches(users, domain)
+
+    return [
+        user
+        for user in users
+        if user.user_data.get('usertype') == 'rc'
+    ]
+
+
+def _find_users_at_location(location):
+    # ToDo: return users at location
+    return []
+
+
+def _find_child_location_with_name(parent_location, location_name):
+    # ToDo: find location under parent location that has the name location_name
+    return None
+
+
+def _update_cases(user, current_owner_id, new_owner_id):
+    cases = _find_cases(owner_id=current_owner_id, opened_by_user_id=user.user_id)
+    log("fUpdating {len(cases)} cases for user {user.username}")
+    # ToDo: update cases with progress bar
+    return cases
+
+
+def log(message):
+    # ToDo: add logging to log message where ever needed
+    pass
+
+
+def _find_cases(owner_id, opened_by_user_id):
+    # ToDo: find open cases owned and opened by specific user
+    return []
+
+
+def _update_users_location(user, location):
+    # ToDo: update user's primary location
+    pass
