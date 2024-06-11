9b523cd2324d8ca64f7cbbb115a1cbba7d0b4a0a
Daniel Miller <dmiller@dimagi.com>
2024-06-11 16:45:27 -0400

    More nose.tools: assert_raises, assert_equal, ...

--- corehq/tests/nosecompat.py
+++ corehq/tests/nosecompat.py
@@ -1,5 +1,7 @@
 import sys
 
+from testil import assert_raises as _assert_raises
+
 from .tools import nottest as nottest_tool
 
 
@@ -11,3 +13,22 @@ class VirtualNose:
     """Legacy namespace for tests written before pytest"""
     class tools:
         nottest = nottest_tool
+        assert_raises = _assert_raises
+
+        def assert_equal(actual, expected):
+            assert actual == expected
+
+        def assert_false(value):
+            assert not value
+
+        def assert_true(value):
+            assert value
+
+        def assert_in(needle, haystack):
+            assert needle in haystack
+
+        def assert_list_equal(actual, expected):
+            assert actual == expected
+
+        def assert_is_none(value):
+            assert value is None
