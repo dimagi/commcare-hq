70c1c56de031abb047b9d22f99bd80f93c551489
Manish Kangia <mkangia@dimagi.com>
2024-06-12 23:52:42 +0530

    Simplify query; remove redundant select related
    Co-authored-by: Ethan Soergel <esoergel@users.noreply.github.com>

--- corehq/apps/data_dictionary/util.py
+++ corehq/apps/data_dictionary/util.py
@@ -420,12 +420,6 @@ def get_used_props_by_case_type(domain):
 
 
 def get_case_property_group_name_for_properties(domain, case_type_name):
-    case_group_name_for_property = {}
-    case_property_group_query = CaseProperty.objects.filter(
+    return dict(CaseProperty.objects.filter(
         case_type__name=case_type_name, case_type__domain=domain
-    ).select_related('case_type').select_related('group').values_list('name', 'group__name')
-
-    for case_property_name, group_name in case_property_group_query:
-        case_group_name_for_property[case_property_name] = group_name
-
-    return case_group_name_for_property
+    ).values_list('name', 'group__name'))
