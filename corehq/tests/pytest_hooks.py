b626ed78b239843391eba97cde47c9f584b70f85
Daniel Miller <dmiller@dimagi.com>
2024-06-14 15:15:33 -0400

    Detect tests masked by bad class decorations

--- corehq/tests/pytest_hooks.py
+++ corehq/tests/pytest_hooks.py
@@ -40,6 +40,27 @@ def pytest_load_initial_conftests():
     create_nose_virtual_package()
 
 
+def pytest_pycollect_makeitem(collector, name, obj):
+    """Fail on common mistake that results in masked tests"""
+    if (
+        "Test" in name
+        and not isinstance(obj, type)
+        and isinstance(wrapped := _get_wrapped(obj), type)
+        and any(n.startswith("test_") for n in dir(wrapped))
+    ):
+        return pytest.fail(
+            f"{obj.__module__}.{name} appears to be a test class that has "
+            "been wrapped with a decorator that masks its tests."
+        )
+    return None
+
+
+def _get_wrapped(obj):
+    while hasattr(obj, "__wrapped__"):
+        obj = obj.__wrapped__
+    return obj
+
+
 def assert_loaded():
     # Not a pytest hook.
     assert getattr(pytest_load_initial_conftests, "loaded", False), """
