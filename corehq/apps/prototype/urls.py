6932d7b87e0010fb1d3dd6e9107645be5ddcbcfc
Biyeun Buczyk <biyeun@dimagi.com>
2024-06-18 11:47:06 +0200

    add simple pagination of table example with HTMX

--- corehq/apps/prototype/urls.py
+++ corehq/apps/prototype/urls.py
@@ -1,9 +1,15 @@
 from django.urls import re_path as url
 from corehq.apps.prototype.views import example
+from corehq.apps.prototype.views.htmx.pagination import (
+    HtmxPaginationView,
+    PaginationDataView,
+)
 
 urlpatterns = [
     url(r'^example/$', example.knockout_pagination,
         name='prototype_example_knockout_pagination'),
     url(r'^example/data/$', example.example_paginated_data,
         name='prototype_example_paginated_data'),
+    url(r'^htmx/pagination/$', HtmxPaginationView.as_view(), name=HtmxPaginationView.urlname),
+    url(r'^htmx/pagination/data/$', PaginationDataView.as_view(), name=PaginationDataView.urlname),
 ]
