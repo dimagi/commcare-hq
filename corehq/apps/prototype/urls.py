4beaeeb097cfaa25baa0b5c1396aa47b4edd84ea
Biyeun Buczyk <biyeun@dimagi.com>
2024-06-20 13:42:56 +0200

    update formatting

--- corehq/apps/prototype/urls.py
+++ corehq/apps/prototype/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
         name='prototype_example_knockout_pagination'),
     url(r'^example/data/$', example.example_paginated_data,
         name='prototype_example_paginated_data'),
-    url(r'^htmx/pagination/$', HtmxPaginationView.as_view(), name=HtmxPaginationView.urlname),
-    url(r'^htmx/pagination/data/$', PaginationDataView.as_view(), name=PaginationDataView.urlname),
+    url(r'^htmx/pagination/$', HtmxPaginationView.as_view(),
+        name=HtmxPaginationView.urlname),
+    url(r'^htmx/pagination/data/$', PaginationDataView.as_view(),
+        name=PaginationDataView.urlname),
 ]
