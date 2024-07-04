1ca177a70882ac70c2956462dbb3160ebeb754d3
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2024-07-04 15:06:01 +0200

    update queue + remove unused script file

--- custom/benin/tasks.py
+++ custom/benin/tasks.py
@@ -1,7 +1,7 @@
 from corehq.apps.celery import task
 
 
-@task(queue='background_queue', ignore_result=True)
+@task(queue='soltech_custom_script_queue', ignore_result=True)
 def process_updates_for_village_async(domain, village, dry_run):
     from custom.benin.management.commands.migrate_users_and_their_cases_to_new_rc_level import \
         process_updates_for_village
