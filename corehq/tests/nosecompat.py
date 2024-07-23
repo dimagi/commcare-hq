47acdf5e618cab4941011a7ff3e55a5be22758bd
Daniel Miller <dmiller@dimagi.com>
2024-07-23 12:18:27 -0400

    Replace nose.plugins.attrib with pytest.mark

--- corehq/tests/nosecompat.py
+++ corehq/tests/nosecompat.py
@@ -7,7 +7,6 @@ from .tools import nottest as nottest_tool
 
 def create_nose_virtual_package():
     sys.modules['nose.tools'] = VirtualNose.tools
-    sys.modules['nose.plugins.attrib'] = VirtualNose.plugins.attrib
 
 
 class VirtualNose:
@@ -33,18 +32,3 @@ class VirtualNose:
 
         def assert_is_none(value):
             assert value is None
-
-    class plugins:
-        class attrib:
-            def attr(*args, **kwargs):
-                # TODO adapt to pytest
-                """Decorator that adds attributes to classes or functions
-                for use with the Attribute (-a) plugin.
-                """
-                def wrap_ob(ob):
-                    for name in args:
-                        setattr(ob, name, True)
-                    for name, value in kwargs.items():
-                        setattr(ob, name, value)
-                    return ob
-                return wrap_ob
