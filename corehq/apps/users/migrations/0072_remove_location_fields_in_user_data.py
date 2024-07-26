7f8b8f873204346524e335451d852fe469802346
Jing Cheng <jcheng@dimagi.com>
2024-07-25 22:40:56 -0400

    Add revert function

--- corehq/apps/users/migrations/0072_remove_location_fields_in_user_data.py
+++ corehq/apps/users/migrations/0072_remove_location_fields_in_user_data.py
@@ -1,5 +1,7 @@
 from django.db import migrations
 
+from corehq.apps.users.util import user_location_data
+from corehq.apps.users.models import CommCareUser
 from corehq.util.django_migrations import skip_on_fresh_install
 
 
@@ -22,6 +24,29 @@ def remove_keys_from_data(apps, schema_editor):
             user_data.save()
 
 
+def revert_keys_in_data(apps, schema_editor):
+    SQLUserData = apps.get_model('users', 'SQLUserData')
+    CommCareUserCouch = CommCareUser.get_db()
+
+    for user_data in SQLUserData.objects.all():
+        user_id = user_data.user_id
+
+        commcare_user = CommCareUserCouch.get(user_id)
+
+        data = user_data.data
+        location_id = commcare_user.get('location_id')
+        assigned_location_ids = commcare_user.get('assigned_location_ids', [])
+
+        if location_id:
+            data['commcare_location_id'] = location_id
+            data['commcare_primary_case_sharing_id'] = location_id
+        if assigned_location_ids:
+            data['commcare_location_ids'] = user_location_data(assigned_location_ids)
+
+        user_data.data = data
+        user_data.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -29,5 +54,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.RunPython(remove_keys_from_data),
+        migrations.RunPython(remove_keys_from_data, revert_keys_in_data),
     ]
