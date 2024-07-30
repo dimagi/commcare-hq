c983e02fc636b683f2d84c406e3e037e7649c054
Daniel Miller <dmiller@dimagi.com>
2024-07-30 11:51:05 -0400

    Parametrize test
    Make it easier to debug individual failing parameter sets.

--- corehq/tests/pytest_compat.py
+++ corehq/tests/pytest_compat.py
@@ -1,4 +1,5 @@
 import sys
+from functools import wraps
 
 
 def _install_pytest_compat():
@@ -13,6 +14,26 @@ class Marker:
             setattr(test_obj, name, True)
             return test_obj
         return set_attribute
+
+    def parametrize(self, arg_names, args_tuples):
+        def parametrize(func):
+            @wraps(func)
+            def test():
+                test_tuple = (func,)
+                test = (func,)
+                for args in args_tuples:
+                    if "," not in arg_names and (
+                        not isinstance(args, tuple)
+                        or len(args) != 1
+                    ):
+                        args = (args,)
+                    try:
+                        test_tuple = test + args
+                    except TypeError:
+                        raise TypeError(f"could not construct test tuple for {func} with args {args!r}")
+                    yield test_tuple
+            return test
+        return parametrize
 
 
 mark = Marker()
