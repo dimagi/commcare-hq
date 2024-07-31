230ab0b74982ab8184acc5c2058a9deb1feb0ee3
Simon Kelly <skelly@dimagi.com>
2024-07-31 10:10:48 +0200

    Remove es6 b5 loader

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/menus/views.js
@@ -5,7 +5,6 @@ hqDefine("cloudcare/js/formplayer/menus/views", [
     'backbone',
     'backbone.marionette',
     'DOMPurify/dist/purify.min',
-    'es6!hqwebapp/js/bootstrap5_loader',
     'hqwebapp/js/initial_page_data',
     'hqwebapp/js/toggles',
     'analytix/js/kissmetrix',
