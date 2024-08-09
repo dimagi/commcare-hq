369c96ed6b7b23e6774f80439794b9ebec503110
Norman Hooper <nhooper@dimagi.com>
2024-08-09 19:57:35 +0100

    A partial implementation of a lock using Postgres

--- settings.py
+++ settings.py
@@ -386,6 +386,7 @@ HQ_APPS = (
     'corehq.apps.zapier.apps.ZapierConfig',
     'corehq.apps.translations',
     'corehq.apps.app_execution',
+    'corehq.apps.pg_lock',
 
     # custom reports
     'custom.reports.mc',
