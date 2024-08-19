7db83430323c6734fb6b0251c7c37783c0bfb978
Matt Riley <mriley@dimagi.com>
2024-08-19 14:12:19 -0400

    Improved test naming for enterprise iterator tests

--- corehq/apps/enterprise/tests/test_iterators.py
+++ corehq/apps/enterprise/tests/test_iterators.py
@@ -4,16 +4,16 @@ from corehq.apps.enterprise.iterators import raise_after_max_elements
 
 
 class TestRaiseAfterMaxElements(SimpleTestCase):
-    def test_fully_iterating_will_raise_the_default_exception(self):
+    def test_iterating_beyond_max_items_will_raise_the_default_exception(self):
         it = raise_after_max_elements([1, 2, 3], 2)
         with self.assertRaisesMessage(Exception, 'Too Many Elements'):
             list(it)
 
-    def test_fully_iterating_will_raise_provided_exception(self):
+    def test_iterating_beyond_max_items_will_raise_provided_exception(self):
         it = raise_after_max_elements([1, 2, 3], 2, Exception('Test Message'))
         with self.assertRaisesMessage(Exception, 'Test Message'):
             list(it)
 
-    def test_slicing_iterator_can_avoid_error(self):
+    def test_can_iterate_through_all_elements_with_no_exception(self):
         it = raise_after_max_elements([1, 2, 3], 3)
         self.assertEqual(list(it), [1, 2, 3])
