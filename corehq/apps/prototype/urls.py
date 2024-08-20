9ab97c8291af2fa5f4db5b43ea18920d9a1d38e9
Biyeun Buczyk <biyeun@dimagi.com>
2024-08-20 20:43:01 +0200

    add page for testing loading of an AMD-formatted bootstrap 3 module

--- corehq/apps/prototype/urls.py
+++ corehq/apps/prototype/urls.py
@@ -5,6 +5,8 @@ from corehq.apps.prototype.views import webpack
 urlpatterns = [
     url(r'^webpack/b5_amd/$', webpack.bootstrap5_amd_example,
         name='webpack_bootstrap5_amd_example'),
+    url(r'^webpack/b3_amd/$', webpack.bootstrap3_amd_example,
+        name='webpack_bootstrap3_amd_example'),
     url(r'^example/$', example.knockout_pagination,
         name='prototype_example_knockout_pagination'),
     url(r'^example/data/$', example.example_paginated_data,
--- corehq/apps/prototype/views/webpack.py
+++ corehq/apps/prototype/views/webpack.py
@@ -10,3 +10,9 @@ from corehq.apps.hqwebapp.decorators import use_bootstrap5
 @toggles.SAAS_PROTOTYPE.required_decorator()
 def bootstrap5_amd_example(request):
     return render(request, 'prototype/webpack/bootstrap5_amd.html', {})
+
+
+@login_required
+@toggles.SAAS_PROTOTYPE.required_decorator()
+def bootstrap3_amd_example(request):
+    return render(request, 'prototype/webpack/bootstrap3_amd.html', {})
