efc087282fc87ecdb559e0a1695fc41f3e44e6e1
Matt Riley <mriley@dimagi.com>
2024-08-20 14:15:33 -0400

    Readability changes to row limits

--- corehq/apps/enterprise/iterators.py
+++ corehq/apps/enterprise/iterators.py
@@ -1,9 +1,7 @@
 def raise_after_max_elements(it, max_elements, exception=None):
-    total_yielded = 0
-    for ele in it:
+    for total_yielded, ele in enumerate(it):
         if total_yielded >= max_elements:
             exception = exception or Exception('Too Many Elements')
             raise exception
 
         yield ele
-        total_yielded += 1
