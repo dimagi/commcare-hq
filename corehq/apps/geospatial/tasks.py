17d38c06a70b5494267c8ed3bd1ce5e6698ac9b7
Ajeet <ayadav@dimagi.com>
2024-08-26 13:28:09 +0530

    util to check if a task is invoked but not completed

--- corehq/apps/geospatial/tasks.py
+++ corehq/apps/geospatial/tasks.py
@@ -1,3 +1,6 @@
+from celery import states
+from celery.result import AsyncResult
+
 from corehq.apps.celery import task
 from corehq.apps.geospatial.utils import update_cases_owner
 
@@ -5,3 +8,15 @@ from corehq.apps.geospatial.utils import update_cases_owner
 @task(queue="background_queue")
 def geo_cases_reassignment_update_owners(domain, case_id_to_owner_id):
     update_cases_owner(domain, case_id_to_owner_id)
+
+
+def is_task_invoked_and_not_completed(task_id):
+    """Returns True is a task is invoked and is executing or waiting to be executed.
+    NOTE: Only works for tasks that store results i.e. ignore_result must be False.
+    NOTE: Celery states.PENDING is a bit ambiguous as it could mean two things one, task was never invoked and two
+    task was invoked but still waiting to picked up.
+    However, in HQ we set state to `SENT` as soon as it is invoked.
+    See 'casexml.apps.phone.tasks.update_celery_state'
+    """
+    result = AsyncResult(task_id)
+    return result.state not in [states.PENDING, states.SUCCESS, states.FAILURE, states.REVOKED]
