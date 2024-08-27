aa44677f79f955c9ef07b2b6342d0e99c46dbf86
Ajeet <ayadav@dimagi.com>
2024-08-27 18:03:39 +0530

    doc for function

--- corehq/apps/geospatial/tasks.py
+++ corehq/apps/geospatial/tasks.py
@@ -11,12 +11,14 @@ def geo_cases_reassignment_update_owners(domain, case_id_to_owner_id):
 
 
 def is_task_invoked_and_not_completed(task_id):
-    """Returns True if a task is invoked and is executing or waiting to be executed.
+    """
     NOTE: Only works for tasks that store results i.e. ignore_result must be False.
-    NOTE: Celery states.PENDING is a bit ambiguous as it could mean two things one, task was never invoked and two
-    task was invoked but still waiting to picked up.
-    However, in HQ we set state to `SENT` as soon as it is invoked.
-    See 'casexml.apps.phone.tasks.update_celery_state'
+    Returns True if a task is invoked and is executing or waiting to be executed.
+    NOTE: In case the task with a given task_id is not invoked (i.e. does not exist in results backend), the state
+    returned is PENDING.
+    The state is also PENDING if the task was invoked and still waiting to be picked up. However, in HQ we set the
+    state to `SENT` as soon as it is invoked. See 'casexml.apps.phone.tasks.update_celery_state'.
+    Read more about Celery State at https://docs.celeryq.dev/en/stable/reference/celery.states.html
     """
     result = AsyncResult(task_id)
     return result.state not in [states.PENDING, states.SUCCESS, states.FAILURE, states.REVOKED]
