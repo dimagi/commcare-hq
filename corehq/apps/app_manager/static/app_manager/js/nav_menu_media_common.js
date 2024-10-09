b1a382bee1ec8e9fcdaf5f51f973a61da703f9f8
Jenny Schweers <jschweers@dimagi.com>
2024-10-09 18:11:55 -0400

    Updated menu media to use new code (WIP)

--- corehq/apps/app_manager/static/app_manager/js/nav_menu_media_common.js
+++ corehq/apps/app_manager/static/app_manager/js/nav_menu_media_common.js
@@ -1,10 +1,10 @@
-/* globals HQMediaFileUploadController */
 hqDefine("app_manager/js/nav_menu_media_common", function () {
-    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
-        uploaders = {};
+    const initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        uploadersModule = hqImport("hqmedia/js/hqmediauploaders");
+    let uploaders = {};
 
     _.each(initialPageData.get("multimedia_upload_managers"), function (uploader, type) {
-        uploaders[type] = new HQMediaFileUploadController(
+        uploaders[type] = uploadersModule.uploader(
             uploader.slug,
             uploader.media_type,
             uploader.options
