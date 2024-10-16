9a36a655aefeced6c1213d73d6d083349eaabbed
Norman Hooper <nhooper@dimagi.com>
2024-10-16 16:44:34 +0100

    Refactor: Unused imports

--- corehq/pillows/user.py
+++ corehq/pillows/user.py
@@ -1,8 +1,6 @@
-import copy
 from corehq.apps.change_feed.consumer.feed import KafkaChangeFeed, KafkaCheckpointEventHandler
 from corehq.apps.change_feed import topics
 from corehq.apps.es.users import user_adapter
-from corehq.apps.groups.dbaccessors import get_group_id_name_map_by_user
 from corehq.apps.users.models import CommCareUser, CouchUser, WebUser
 from corehq.apps.users.util import WEIRD_USER_IDS
 from corehq.apps.userreports.data_source_providers import DynamicDataSourceProvider, StaticDataSourceProvider
