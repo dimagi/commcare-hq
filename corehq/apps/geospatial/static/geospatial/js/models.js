c9a8139fdd64b5823e8bdd62dafd9eb29540a741
Ajeet <ayadav@dimagi.com>
2024-10-17 20:55:39 +0530

    fix: getext for unexpected error message

--- corehq/apps/geospatial/static/geospatial/js/models.js
+++ corehq/apps/geospatial/static/geospatial/js/models.js
@@ -21,7 +21,10 @@ hqDefine('geospatial/js/models', [
     const DISBURSEMENT_LAYER_PREFIX = 'route-';
     const saveGeoPolygonUrl = initialPageData.reverse('geo_polygons');
     const reassignCasesUrl = initialPageData.reverse('reassign_cases');
-    const unexpectedErrorMessage = "Oops! Something went wrong! Please report an issue if the problem persists.";
+    const unexpectedErrorMessage = gettext(
+        "Oops! Something went wrong!" +
+        " Please report an issue if the problem persists."
+    );
 
     var MissingGPSModel = function () {
         this.casesWithoutGPS = ko.observable([]);
@@ -692,7 +695,7 @@ hqDefine('geospatial/js/models', [
                     }, 2000);
                 },
                 error: function () {
-                    alertUser.alert_user(gettext(unexpectedErrorMessage), 'danger');
+                    alertUser.alert_user(unexpectedErrorMessage, 'danger');
                 },
             });
         };
@@ -800,7 +803,7 @@ hqDefine('geospatial/js/models', [
                         if (responseText) {
                             alertUser.alert_user(responseText, 'danger');
                         } else {
-                            alertUser.alert_user(gettext(unexpectedErrorMessage), 'danger');
+                            alertUser.alert_user(unexpectedErrorMessage, 'danger');
                         }
                     },
                 });
@@ -1040,7 +1043,7 @@ hqDefine('geospatial/js/models', [
                     if (responseText) {
                         alertUser.alert_user(responseText, 'danger');
                     } else {
-                        alertUser.alert_user(gettext(unexpectedErrorMessage), 'danger', false, true);
+                        alertUser.alert_user(unexpectedErrorMessage, 'danger', false, true);
                     }
                 },
                 complete: function () {
