811ba45373686ddb7838deb38f1407224267df7a
Jenny Schweers <jschweers@dimagi.com>
2024-10-21 14:29:18 -0400

    Removed ajaxSubmit and resetForm from email-request.js (Report an Issue + SolTech feature request)

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/email-request.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/email-request.js
@@ -2,7 +2,6 @@ hqDefine('hqwebapp/js/bootstrap5/email-request', [
     "jquery",
     "knockout",
     "es6!hqwebapp/js/bootstrap5_loader",
-    "jquery-form/dist/jquery.form.min",
     "hqwebapp/js/bootstrap5/hq.helpers",
 ], function ($, ko, bootstrap) {
     'use strict';
@@ -69,11 +68,15 @@ hqDefine('hqwebapp/js/bootstrap5/email-request', [
             } else if (!self.isRequestReportSubmitting) {
                 self.$submitBtn.changeButtonState('loading');
                 self.cancelBtnEnabled(false);
-                self.$formElement.ajaxSubmit({
-                    type: "POST",
+                self.reportUrl(location.href);
+                self.isRequestReportSubmitting = true;
+                $.ajax({
+                    method: "POST",
                     url: self.$formElement.attr('action'),
-                    beforeSerialize: hqwebappRequestReportBeforeSerialize,
-                    beforeSubmit: hqwebappRequestReportBeforeSubmit,
+                    data: new FormData(self.$formElement.get(0)),
+                    contentType: false,
+                    processData: false,
+                    enctype: 'multipart/form-data',
                     success: hqwebappRequestReportSucccess,
                     error: hqwebappRequestReportError,
                 });
@@ -87,7 +90,7 @@ hqDefine('hqwebapp/js/bootstrap5/email-request', [
 
         self.resetForm = function () {
             self.$formElement.find("button[type='submit']").changeButtonState('reset');
-            self.$formElement.resetForm();
+            self.$formElement.get(0).reset();
             self.cancelBtnEnabled(true);
             self.$submitBtn.changeButtonState('reset');
             resetErrors();
@@ -107,14 +110,6 @@ hqDefine('hqwebapp/js/bootstrap5/email-request', [
             self.hasSubjectError(false);
             self.hasEmailInputError(false);
             self.recipientsErrorMessage(null);
-        }
-
-        function hqwebappRequestReportBeforeSerialize() {
-            self.reportUrl(location.href);
-        }
-
-        function hqwebappRequestReportBeforeSubmit() {
-            self.isRequestReportSubmitting = true;
         }
 
         function hqwebappRequestReportSucccess() {
