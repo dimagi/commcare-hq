9a8a4b416001fc752b407f261bda5cd9e7580380
Jenny Schweers <jschweers@dimagi.com>
2024-10-21 14:40:34 -0400

    Removed ajaxSubmit from group members page

--- corehq/apps/groups/static/groups/js/group_members.js
+++ corehq/apps/groups/static/groups/js/group_members.js
@@ -109,7 +109,10 @@ hqDefine("groups/js/group_members", [
                     };
                 _showMembershipUpdating();
                 $(this).find(':button').prop('disabled', true);
-                $(this).ajaxSubmit({
+                $.ajax({
+                    url: $(this).attr("action"),
+                    method: "POST",
+                    data: Object.fromEntries(new FormData(this)),
                     success: outcome(true, "Group membership", "#edit_membership", "Edit Group Membership", _hideMembershipUpdating),
                     error: outcome(false, "Group membership", "#edit_membership", _hideMembershipUpdating),
                 });
@@ -117,7 +120,10 @@ hqDefine("groups/js/group_members", [
             });
             $('#edit-group-settings').submit(function () {
                 $(this).find('.modal-footer :button').disableButton();
-                $(this).ajaxSubmit({
+                $.ajax({
+                    url: $(this).attr("action"),
+                    method: "POST",
+                    data: Object.fromEntries(new FormData(this)),
                     success: outcome(true, "Group settings", "#edit-group-settings", "Edit Settings"),
                     error: outcome(false, "Group settings", "#edit-group-settings"),
                 });
@@ -132,7 +138,10 @@ hqDefine("groups/js/group_members", [
             });
             $('#group-data-form').submit(function () {
                 $(this).find(':button').prop('disabled', true);
-                $(this).ajaxSubmit({
+                $.ajax({
+                    url: $(this).attr("action"),
+                    method: "POST",
+                    data: Object.fromEntries(new FormData(this)),
                     success: outcome(true, "Group data", "#group-data-form", "Edit Group Data"),
                     error: outcome(false, "Group data", "#group-data-form"),
                 });
