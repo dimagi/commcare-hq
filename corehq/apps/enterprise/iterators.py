8320589d2d67842fa823a9df40677bdce3547d5a
Matt Riley <mriley@dimagi.com>
2024-10-23 11:18:47 -0400

    Added enterprise form iterators

--- corehq/apps/enterprise/iterators.py
+++ corehq/apps/enterprise/iterators.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from corehq.apps.es import filters
+from corehq.apps.es.forms import FormES
+from corehq.apps.enterprise.resumable_iterator_wrapper import ResumableIteratorWrapper
+
+
 def raise_after_max_elements(it, max_elements, exception=None):
     for total_yielded, ele in enumerate(it):
         if total_yielded >= max_elements:
@@ -5,3 +11,81 @@ def raise_after_max_elements(it, max_elements, exception=None):
             raise exception
 
         yield ele
+
+
+def get_enterprise_form_iterator(account, start_date, end_date, last_domain=None, last_time=None, last_id=None):
+    domains = account.get_domains()
+
+    it = multi_domain_form_generator(domains, start_date, end_date, last_domain, last_time, last_id)
+    return ResumableIteratorWrapper(it, lambda ele: {
+        'domain': ele['domain'],
+        'received_on': ele['received_on'],
+        'id': ele['form']['meta']['instanceID']
+    })
+
+
+def multi_domain_form_generator(domains, start_date, end_date, last_domain=None, last_time=None, last_id=None):
+    domain_index = domains.index(last_domain) if last_domain else 0
+
+    def _get_domain_iterator(last_time=None, last_id=None):
+        if domain_index >= len(domains):
+            return None
+        domain = domains[domain_index]
+        return domain_form_generator(domain, start_date, end_date, last_time, last_id)
+
+    current_iterator = _get_domain_iterator(last_time, last_id)
+
+    while current_iterator:
+        yield from current_iterator
+        domain_index += 1
+        if domain_index >= len(domains):
+            break
+        current_iterator = _get_domain_iterator()
+
+
+def domain_form_generator(domain, start_date, end_date, last_time=None, last_id=None):
+    if not last_time:
+        last_time = datetime.now()
+
+    while True:
+        query = create_domain_query(domain, start_date, end_date, last_time, last_id)
+        results = query.run()
+        for form in results.hits:
+            last_form_fetched = form
+            yield last_form_fetched
+
+        if len(results.hits) >= results.total:
+            break
+        else:
+            last_time = last_form_fetched['received_on']
+            last_id = last_form_fetched['_id']
+
+
+def create_domain_query(domain, start_date, end_date, last_time, last_id):
+    CURSOR_SIZE = 5
+
+    query = (
+        FormES()
+        .domain(domain)
+        .user_type('mobile')
+        .submitted(gte=start_date, lte=end_date)
+        .size(CURSOR_SIZE)
+    )
+
+    query.es_query['sort'] = [
+        {'received_on': {'order': 'desc'}},
+        {'form.meta.instanceID': 'asc'}
+    ]
+
+    if last_id:
+        query = query.filter(filters.OR(
+            filters.AND(
+                filters.term('received_on', last_time),
+                filters.range_filter('form.meta.instanceID', gt=last_id)
+            ),
+            filters.range_filter('received_on', lt=last_time)
+        ))
+    else:
+        query = query.submitted(lte=last_time)
+
+    return query
--- corehq/apps/enterprise/tests/test_iterators.py
+++ corehq/apps/enterprise/tests/test_iterators.py
@@ -1,6 +1,15 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from datetime import datetime
 
-from corehq.apps.enterprise.iterators import raise_after_max_elements
+from corehq.apps.es.forms import form_adapter
+from corehq.apps.es.tests.utils import es_test
+from corehq.apps.users.models import CommCareUser
+from corehq.form_processor.tests.utils import create_form_for_test
+from corehq.apps.enterprise.iterators import (
+    raise_after_max_elements,
+    domain_form_generator,
+    multi_domain_form_generator,
+)
 
 
 class TestRaiseAfterMaxElements(SimpleTestCase):
@@ -17,3 +26,121 @@ class TestRaiseAfterMaxElements(SimpleTestCase):
     def test_can_iterate_through_all_elements_with_no_exception(self):
         it = raise_after_max_elements([1, 2, 3], 3)
         self.assertEqual(list(it), [1, 2, 3])
+
+
+@es_test(requires=[form_adapter])
+class TestMultiDomainFormGenerator(TestCase):
+    def setUp(self):
+        self.user = CommCareUser.create('test-domain', 'test-user', 'password', None, None)
+        self.addCleanup(self.user.delete, None, None)
+
+    def test_iterates_through_multiple_domains(self):
+        form1 = self._create_form('domain1', form_id='1', received_on=datetime(year=2024, month=7, day=1))
+        form2 = self._create_form('domain2', form_id='2', received_on=datetime(year=2024, month=7, day=2))
+        form3 = self._create_form('domain3', form_id='3', received_on=datetime(year=2024, month=7, day=3))
+        form_adapter.bulk_index([form1, form2, form3], refresh=True)
+
+        it = multi_domain_form_generator(
+            ['domain1', 'domain2', 'domain3'],
+            start_date=datetime(year=2024, month=7, day=1),
+            end_date=datetime(year=2024, month=7, day=15)
+        )
+
+        form_ids = [form['_id'] for form in list(it)]
+        self.assertEqual(form_ids, ['1', '2', '3'])
+
+    def _create_form(self, domain, form_id=None, received_on=None):
+        form_data = {
+            '#type': 'fake-type',
+            'meta': {
+                'userID': self.user._id,
+                'instanceID': form_id,
+            },
+        }
+        return create_form_for_test(
+            domain,
+            user_id=self.user._id,
+            form_data=form_data,
+            form_id=form_id,
+            received_on=received_on
+        )
+
+
+@es_test(requires=[form_adapter])
+class TestDomainFormGenerator(TestCase):
+    def setUp(self):
+        self.user = CommCareUser.create('test-domain', 'test-user', 'password', None, None)
+        self.addCleanup(self.user.delete, None, None)
+
+    def test_iterates_through_all_forms_in_domain(self):
+        form1 = self._create_form('test-domain', form_id='1', received_on=datetime(year=2024, month=7, day=2))
+        form2 = self._create_form('test-domain', form_id='2', received_on=datetime(year=2024, month=7, day=3))
+        form3 = self._create_form('test-domain', form_id='3', received_on=datetime(year=2024, month=7, day=4))
+        form_adapter.bulk_index([form1, form2, form3], refresh=True)
+
+        it = domain_form_generator(
+            'test-domain',
+            start_date=datetime(year=2024, month=7, day=1),
+            end_date=datetime(year=2024, month=7, day=15),
+        )
+
+        form_ids = [form['_id'] for form in list(it)]
+        self.assertEqual(form_ids, ['3', '2', '1'])
+
+    def test_includes_inclusive_boundaries(self):
+        form1 = self._create_form('test-domain', form_id='1', received_on=datetime(year=2024, month=7, day=1))
+        form2 = self._create_form('test-domain', form_id='2', received_on=datetime(year=2024, month=7, day=2))
+        form_adapter.bulk_index([form1, form2], refresh=True)
+
+        it = domain_form_generator(
+            'test-domain',
+            start_date=datetime(year=2024, month=7, day=1),
+            end_date=datetime(year=2024, month=7, day=2)
+        )
+
+        form_ids = [form['_id'] for form in list(it)]
+        self.assertEqual(form_ids, ['2', '1'])
+
+    def test_ignores_form_in_another_domain(self):
+        form1 = self._create_form('test-domain', form_id='1', received_on=datetime(year=2024, month=7, day=2))
+        form2 = self._create_form('not-test-domain', form_id='2', received_on=datetime(year=2024, month=7, day=2))
+        form_adapter.bulk_index([form1, form2], refresh=True)
+
+        it = domain_form_generator(
+            'test-domain',
+            start_date=datetime(year=2024, month=7, day=1),
+            end_date=datetime(year=2024, month=7, day=15),
+        )
+
+        form_ids = [form['_id'] for form in list(it)]
+        self.assertEqual(form_ids, ['1'])
+
+    def test_sorts_by_date_then_id(self):
+        form2 = self._create_form('test-domain', form_id='2', received_on=datetime(year=2024, month=7, day=1))
+        form1 = self._create_form('test-domain', form_id='1', received_on=datetime(year=2024, month=7, day=1))
+        form_adapter.bulk_index([form2, form1], refresh=True)
+
+        it = domain_form_generator(
+            'test-domain',
+            start_date=datetime(year=2024, month=7, day=1),
+            end_date=datetime(year=2024, month=7, day=2),
+        )
+
+        form_ids = [form['_id'] for form in list(it)]
+        self.assertEqual(form_ids, ['1', '2'])
+
+    def _create_form(self, domain, form_id=None, received_on=None):
+        form_data = {
+            '#type': 'fake-type',
+            'meta': {
+                'userID': self.user._id,
+                'instanceID': form_id,
+            },
+        }
+        return create_form_for_test(
+            domain,
+            user_id=self.user._id,
+            form_data=form_data,
+            form_id=form_id,
+            received_on=received_on
+        )
