42df2b7e7c9815cc2d981222f71d68a24a5a6c16
Ethan Soergel <esoergel@gmail.com>
2024-10-24 14:03:43 -0400

    Create stubbed-out fixture generator

--- corehq/apps/case_search/fixtures.py
+++ corehq/apps/case_search/fixtures.py
@@ -1,3 +1,7 @@
+from lxml.builder import E
+
+from casexml.apps.phone.fixtures import FixtureProvider
+
 from corehq.messaging.templating import (
     MessagingTemplateRenderer,
     NestedDictTemplateParam,
@@ -16,3 +20,33 @@ def _get_template_renderer(restore_user):
     renderer = MessagingTemplateRenderer()
     renderer.set_context_param('user', NestedDictTemplateParam(_get_user_template_info(restore_user)))
     return renderer
+
+
+def _run_query(csql):
+    return "TODO"
+
+
+def _get_indicator_node(name, csql_template, renderer):
+    result = _run_query(renderer.render(csql_template))
+    return E.value(result, name=name)
+
+
+class CaseSearchFixtureProvider(FixtureProvider):
+    id = 'case-search-fixture'
+
+    def __call__(self, restore_state):
+        renderer = _get_template_renderer(restore_state.restore_user)
+        indicators = _get_indicators(restore_state.domain)
+        if not indicators:
+            return []
+        return E.fixture(E.values(*[
+            _get_indicator_node(name, csql_template, renderer)
+            for name, csql_template in indicators
+        ]), id=self.id)
+
+
+def _get_indicators(domain):
+    return []  # TODO
+
+
+case_search_fixture_generator = CaseSearchFixtureProvider()
--- corehq/apps/case_search/tests/test_fixtures.py
+++ corehq/apps/case_search/tests/test_fixtures.py
@@ -1,13 +1,20 @@
+from unittest.mock import patch
+
 from django.test import TestCase
+
+from lxml import etree
+
+from casexml.apps.phone.tests.utils import call_fixture_generator
 
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.users.models import WebUser
+from corehq.tests.util.xml import assert_xml_equal
 
-from ..fixtures import _get_template_renderer
+from ..fixtures import _get_template_renderer, case_search_fixture_generator
 
 
-class TestInterpolation(TestCase):
-    domain_name = 'test-interpolation'
+class TestCaseSearchFixtures(TestCase):
+    domain_name = 'test-case-search-fixtures'
 
     @classmethod
     def setUpClass(cls):
@@ -27,3 +34,22 @@ class TestInterpolation(TestCase):
     def test_user_id(self):
         res = self.render("@owner_id = '{user.uuid}'")
         self.assertEqual(res, f"@owner_id = '{self.user.user_id}'")
+
+    @patch('corehq.apps.case_search.fixtures._get_indicators')
+    @patch('corehq.apps.case_search.fixtures._run_query')
+    def test_fixture_generator(self, run_query, get_indicators):
+        run_query.return_value = "42"
+        get_indicators.return_value = [
+            ('pre_pandemic_births', "dob < '2020-01-01'"),
+            ('owned_by_user', "@owner_id = '{user.uuid}'"),
+        ]
+        res = call_fixture_generator(case_search_fixture_generator, self.restore_user, self.domain_obj)
+
+        expected = """
+        <fixture id="case-search-fixture">
+           <values>
+               <value name="pre_pandemic_births">42</value>
+               <value name="owned_by_user">42</value>
+           </values>
+        </fixture>"""
+        assert_xml_equal(expected, etree.tostring(res, encoding='utf-8'))
