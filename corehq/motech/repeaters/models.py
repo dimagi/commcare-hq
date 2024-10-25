d79cab2ef448733c7166468be70a692f165f38c8
Simon Kelly <skelly@dimagi.com>
2024-10-25 10:54:37 +0200

    add URL to attempt message

--- corehq/motech/repeaters/models.py
+++ corehq/motech/repeaters/models.py
@@ -1313,10 +1313,12 @@ def has_failed(record):
 def format_response(response):
     if not is_response(response):
         return ''
+    request_url = getattr(response, "url", "")
+    ret = f'{request_url}\n{response.status_code}: {response.reason}'
     response_text = getattr(response, "text", "")[:MAX_REQUEST_LOG_LENGTH]
     if response_text:
-        return f'{response.status_code}: {response.reason}\n{response_text}'
-    return f'{response.status_code}: {response.reason}'
+        return ret + f'\n{response_text}'
+    return ret
 
 
 def is_success_response(response):
