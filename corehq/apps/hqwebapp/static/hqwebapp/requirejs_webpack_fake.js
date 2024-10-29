d45a522d3a0d69815b1af0cd77ea84a9a711251f
Jenny Schweers <jschweers@dimagi.com>
2024-10-28 20:47:42 -0400

    Updated to work in both B3 and B5

--- corehq/apps/hqwebapp/static/hqwebapp/requirejs_webpack_fake.js
+++ corehq/apps/hqwebapp/static/hqwebapp/requirejs_webpack_fake.js
@@ -1,4 +1,4 @@
-hqDefine("hqwebapp/js/webpack_bootstrap3", [], function () {
+hqDefine("hqwebapp/js/requirejs_webpack_fake", [], function () {
     // do nothing, this is a dummy file so modules can be used in both webpack and requirejs
     // can be removed one USE_REQUIREJS is removed
 });
