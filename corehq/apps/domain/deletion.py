ab6684662373d8582fbd55809270237de58482d0
AddisonDunn <dunnaddisonw@gmail.com>
2024-10-29 14:54:00 -0400

    create models to store fixture config and history

--- corehq/apps/domain/deletion.py
+++ corehq/apps/domain/deletion.py
@@ -468,6 +468,7 @@ DOMAIN_DELETE_OPERATIONS = [
     ModelDeletion('couchforms', 'UnfinishedSubmissionStub', 'domain'),
     ModelDeletion('couchforms', 'UnfinishedArchiveStub', 'domain'),
     ModelDeletion('fixtures', 'LookupTable', 'domain'),
+    ModelDeletion('fixtures', 'CSQLFixtureExpression', 'domain'),
     CustomDeletion('ucr', delete_all_ucr_tables_for_domain, []),
     ModelDeletion('domain', 'OperatorCallLimitSettings', 'domain'),
     ModelDeletion('domain', 'SMSAccountConfirmationSettings', 'domain'),
