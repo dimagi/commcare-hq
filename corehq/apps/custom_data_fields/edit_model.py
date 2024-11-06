5463a1ed5bd0e28a1333a3d7e543dabc61c60855
Minha <mlee@dimagi.com>
2024-11-06 14:27:19 -0500

    Set FK'd profiles to null when deleted - only delete profiles if not associated with active users who also belong to domain

--- corehq/apps/custom_data_fields/edit_model.py
+++ corehq/apps/custom_data_fields/edit_model.py
@@ -17,6 +17,7 @@ from corehq.apps.callcenter.tasks import bulk_sync_usercases_if_applicable
 
 from corehq.apps.hqwebapp.decorators import use_bootstrap5, use_jquery_ui
 from corehq.apps.app_manager.helpers.validators import load_case_reserved_words
+from corehq.apps.users.models import SQLUserData, CouchUser
 
 from .models import (
     CustomDataFieldsDefinition,
@@ -371,10 +372,14 @@ class CustomDataModelMixin(object):
         errors = []
         for profile in self.get_profiles():
             if profile.id not in seen:
-                if profile.has_users_assigned:
-                    errors.append(_("Could not delete profile '{}' because it has users "
-                                    "assigned.").format(profile.name))
-                else:
+                user_data = SQLUserData.objects.filter(profile=profile)
+                for ud in user_data:
+                    user = CouchUser.get_by_user_id(ud.user_id)
+                    if user.is_active and self.domain in user.domains:
+                        errors.append(_("Could not delete profile '{}' because it has users "
+                                        "assigned.").format(profile.name))
+                        break
+                if not errors:
                     profile.delete()
 
         return errors
