bff5facbf3137ff0f7d757ab19538a65d1ce7b37
Matt Riley <mriley@dimagi.com>
2024-11-06 16:17:18 -0500

    Remove superuser permissions from Enterprise Forms API test

--- corehq/apps/enterprise/tests/test_apis.py
+++ corehq/apps/enterprise/tests/test_apis.py
@@ -1,25 +1,28 @@
 import json
 from django.test import TestCase, RequestFactory
-from django_prbac.models import Role, Grant, UserRole
-from corehq import privileges
+from corehq.apps.accounting.models import SoftwarePlanEdition, DefaultProductPlan
 import base64
 from datetime import datetime, timezone
-from corehq.apps.users.models import WebUser, CommCareUser, HQApiKey
+from corehq.apps.users.models import WebUser, CommCareUser, HQApiKey, HqPermissions
 from corehq.apps.accounting.tests import generator
 from corehq.apps.domain.models import Domain
 from corehq.apps.es.tests.utils import es_test
 from corehq.apps.es.forms import form_adapter
 from corehq.apps.enterprise.api.resources import FormSubmissionResource
 from corehq.form_processor.tests.utils import create_form_for_test
+from corehq.apps.users.models_role import UserRole
 
 
 @es_test(requires=[form_adapter])
 class FormSubmissionResourceTests(TestCase):
     def test_happy_path(self):
+        enterprise_account = self._create_enterprise_account_covering_domains(['test-domain-1', 'test-domain-2'])
 
-        self._create_enterprise_account_covering_domains(['test-domain-1', 'test-domain-2'])
-
-        enterprise_admin = self._create_enterprise_admin('test-admin@dimagi.com', 'test-domain-1')
+        enterprise_admin = self._create_enterprise_admin(
+            'test-admin@somedomain.com',
+            'test-domain-1',
+            enterprise_account
+        )
         django_user = enterprise_admin.get_django_user()
         api_key = HQApiKey.objects.create(user=django_user, key='1234', name='TestKey')
 
@@ -30,7 +33,12 @@ class FormSubmissionResourceTests(TestCase):
         form2 = self._create_form(mobile_user_2, form_id='2345', received_on=datetime(year=2004, month=10, day=12))
         form_adapter.bulk_index([form1, form2], refresh=True)
 
-        request = self._create_api_request(enterprise_admin, 'test-domain-1', api_key)
+        request = self._create_api_request(
+            enterprise_admin,
+            'test-domain-1',
+            api_key,
+            {'startdate': '2004-10-10', 'enddate': '2004-11-10'}
+        )
 
         resource = FormSubmissionResource()
         response = resource.dispatch_list(request)
@@ -57,9 +65,14 @@ class FormSubmissionResourceTests(TestCase):
         )
 
     def _create_enterprise_account_covering_domains(self, domains):
-        billing_account = generator.billing_account('test-admin@dimagi.com', 'test-admin@dimagi.com')
-        billing_account.enterprise_admin_emails = ['test-admin@dimagi.com']
-        billing_account.save()
+        billing_account = generator.billing_account(
+            'test-admin@dimagi.com',
+            'test-admin@dimagi.com',
+            is_customer_account=True
+        )
+
+        # Enterprise is needed to grant API and OData permissions
+        enterprise_plan = DefaultProductPlan.get_default_plan_version(SoftwarePlanEdition.ENTERPRISE)
 
         for domain in domains:
             domain_obj = Domain(name=domain, is_active=True)
@@ -67,26 +80,31 @@ class FormSubmissionResourceTests(TestCase):
             self.addCleanup(domain_obj.delete)
 
             generator.generate_domain_subscription(
-                billing_account, domain_obj, datetime.now(timezone.utc), None, is_active=True
+                billing_account,
+                domain_obj,
+                datetime.now(timezone.utc),
+                None,
+                plan_version=enterprise_plan,
+                is_active=True
             )
 
         return billing_account
 
-    def _create_enterprise_admin(self, email, domain):
+    def _create_enterprise_admin(self, email, domain, enterprise_account):
         user = WebUser.create(
             domain, email, 'test123', None, None, email)
-        user.is_superuser = True
+
+        # Users need to have permission to view OData reports on the specified domain to access these APIs
+        permissions = HqPermissions(view_reports=True)
+        role = UserRole.create(domain, email, permissions=permissions)
+        membership = user.get_domain_membership(domain)
+        membership.role_id = role.couch_id
         user.save()
 
-        self.addCleanup(user.delete, None, deleted_by=None)
+        enterprise_account.enterprise_admin_emails.append(email)
+        enterprise_account.save()
 
-        role = Role.objects.create(slug="test_role")
-        UserRole.objects.create(user=user.get_django_user(), role=role)
-        accounting_admin_role = Role.objects.get_or_create(
-            name="Accounting Admin",
-            slug=privileges.ACCOUNTING_ADMIN,
-        )[0]
-        Grant.objects.create(from_role=role, to_role=accounting_admin_role)
+        self.addCleanup(user.delete, None, deleted_by=None)
 
         return user
 
@@ -95,13 +113,13 @@ class FormSubmissionResourceTests(TestCase):
         self.addCleanup(user.delete, None, deleted_by=None)
         return user
 
-    def _create_api_request(self, user, domain, api_key):
+    def _create_api_request(self, user, domain, api_key, data=None):
         auth_string = f'{user.username}:{api_key.key}'
         factory = RequestFactory()
         encoded_auth = base64.b64encode(auth_string.encode()).decode()
         request = factory.get(
             '/',
-            {'startdate': '2004-10-10', 'enddate': '2004-11-10'},
+            data,
             HTTP_AUTHORIZATION=f'basic {encoded_auth}'
         )
         request.couch_user = user
