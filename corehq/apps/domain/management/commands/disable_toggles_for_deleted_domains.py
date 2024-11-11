b38e31da50c5eec112906a243766405054dcf3e0
Jenny Schweers <jschweers@dimagi.com>
2024-11-11 11:37:07 -0500

    Added options to specify domain and/or toggle

--- corehq/apps/domain/management/commands/disable_toggles_for_deleted_domains.py
+++ corehq/apps/domain/management/commands/disable_toggles_for_deleted_domains.py
@@ -1,7 +1,9 @@
-from django.core.management.base import BaseCommand
+from couchdbkit import ResourceNotFound
+from django.core.management.base import BaseCommand, CommandError
 
 from corehq.apps.domain.models import Domain
-from corehq.toggles import NAMESPACE_DOMAIN, all_toggles_by_name, set_toggle
+from corehq.toggles import NAMESPACE_DOMAIN, all_toggles_by_name, set_toggle, Toggle
+from corehq.toggles.shortcuts import find_domains_with_toggle_enabled
 
 
 class Command(BaseCommand):
@@ -13,12 +15,30 @@ class Command(BaseCommand):
             help="Actually disable toggles",
             action='store_true',
         )
+        parser.add_argument('--domain', help="Only update this domain")
+        parser.add_argument('--slug', help="Only update this toggle")
 
     def handle(self, **options):
-        all_toggles = all_toggles_by_name()
+        if options['slug']:
+            try:
+                toggles = [Toggle.get(options['slug'])]
+            except ResourceNotFound:
+                raise CommandError(f"Slug {options['slug']} not found - are you using the lowercase slug?")
+        else:
+            toggles = all_toggles_by_name().values()
+
+        domain = options['domain']
         domain_existence = {}
-        for toggle in all_toggles.values():
-            for domain in toggle.get_enabled_domains():
+        for toggle in toggles:
+            domains = []
+            if domain:
+                if domain in find_domains_with_toggle_enabled(toggle):
+                    domains = [domain]
+            else:
+                domains = find_domains_with_toggle_enabled(toggle)
+
+            print(f"Toggle {toggle.slug}, {len(domains)} domains")
+            for domain in domains:
                 if domain not in domain_existence:
                     domain_existence[domain] = bool(Domain.get_by_name(domain))
                 if not domain_existence[domain]:
