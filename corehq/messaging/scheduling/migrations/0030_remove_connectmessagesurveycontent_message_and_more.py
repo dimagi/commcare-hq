f29ebfc0691ce0ef228ece334f0b47b430d826c3
Cal Ellowitz <calellowitz@gmail.com>
2024-11-15 16:03:27 -0500

    fix default values

--- corehq/messaging/scheduling/migrations/0030_remove_connectmessagesurveycontent_message_and_more.py
+++ corehq/messaging/scheduling/migrations/0030_remove_connectmessagesurveycontent_message_and_more.py
@@ -23,13 +23,13 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='connectmessagesurveycontent',
             name='expire_after',
-            field=models.IntegerField(default=django.utils.timezone.now),
+            field=models.IntegerField(default=1),
             preserve_default=False,
         ),
         migrations.AddField(
             model_name='connectmessagesurveycontent',
             name='form_unique_id',
-            field=models.CharField(default=1, max_length=126),
+            field=models.CharField(default="1", max_length=126),
             preserve_default=False,
         ),
         migrations.AddField(
