4af8071051103b0c5246122d4037a32d817d6729
Biyeun Buczyk <biyeun@dimagi.com>
2024-11-21 20:46:39 +0100

    make data cleaning prototype views accessible in prototype app

--- corehq/apps/prototype/urls.py
+++ corehq/apps/prototype/urls.py
@@ -1,6 +1,19 @@
 from django.urls import re_path as url
 from corehq.apps.prototype.views import example
 from corehq.apps.prototype.views import webpack
+from corehq.apps.prototype.views.data_cleaning.main import (
+    CaseDataCleaningPrototypeView,
+    reset_data,
+    slow_simulator,
+)
+from corehq.apps.prototype.views.data_cleaning.forms import (
+    ConfigureColumnsFormView,
+    FilterColumnsFormView,
+    CleanDataFormView,
+)
+from corehq.apps.prototype.views.data_cleaning.tables import (
+    DataCleaningTableView,
+)
 
 urlpatterns = [
     url(r'^webpack/b5_amd/$', webpack.bootstrap5_amd_example,
@@ -13,4 +26,18 @@ urlpatterns = [
         name='prototype_example_knockout_pagination'),
     url(r'^example/data/$', example.example_paginated_data,
         name='prototype_example_paginated_data'),
+    url(r'^dc/$', CaseDataCleaningPrototypeView.as_view(),
+        name=CaseDataCleaningPrototypeView.urlname),
+    url(r'^dc/data/$', DataCleaningTableView.as_view(),
+        name=DataCleaningTableView.urlname),
+    url(r'^dc/forms/columns/$', ConfigureColumnsFormView.as_view(),
+        name=ConfigureColumnsFormView.urlname),
+    url(r'^dc/forms/filter/$', FilterColumnsFormView.as_view(),
+        name=FilterColumnsFormView.urlname),
+    url(r'^dc/forms/clean_data/$', CleanDataFormView.as_view(),
+        name=CleanDataFormView.urlname),
+    url(r'^dc/reset/$', reset_data,
+        name="data_cleaning_reset_data"),
+    url(r'^dc/slow/$', slow_simulator,
+        name="data_cleaning_slow"),
 ]
