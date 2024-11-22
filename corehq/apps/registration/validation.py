2873fcb29a2ff7551ebbfa0a004ba2734ba40e49
Jonathan Tang <Jonahtang738@gmail.com>
2024-11-21 16:19:29 -0800

    adds function to validate profile

--- corehq/apps/registration/validation.py
+++ corehq/apps/registration/validation.py
@@ -1,6 +1,9 @@
 from memoized import memoized
 
 from corehq.apps.user_importer.validation import RoleValidator
+from corehq.apps.user_importer.validation import ProfileValidator
+
+from corehq.apps.custom_data_fields.models import CustomDataFieldsDefinition
 
 
 class AdminInvitesUserValidator():
@@ -15,6 +18,25 @@ class AdminInvitesUserValidator():
         return {role[1]: role[0] for role in get_editable_role_choices(self.domain, self.upload_user,
                                                   allow_admin_role=True)}
 
+    @property
+    @memoized
+    def profiles_by_name(self):
+        from corehq.apps.users.views.mobile.custom_data_fields import UserFieldsView
+        definition = CustomDataFieldsDefinition.get(self.domain, UserFieldsView.field_type)
+        if definition:
+            profiles = definition.get_profiles()
+            return {
+                profile.name: profile
+                for profile in profiles
+            }
+        else:
+            return {}
+
     def validate_role(self, role):
         spec = {'role': role}
         return RoleValidator(self.domain, self.roles_by_name()).validate_spec(spec)
+
+    def validate_profile(self, new_profile_name):
+        profile_validator = ProfileValidator(self.domain, self.upload_user, True, self.profiles_by_name())
+        spec = {'user_profile': new_profile_name}
+        return profile_validator.validate_spec(spec)
