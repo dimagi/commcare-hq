a043006bb75c51753b8f3a3a91ad8fd40925e03d
Jonathan Tang <Jonahtang738@gmail.com>
2024-11-26 14:22:35 -0800

    perform permission and privilege check only if the parameter exists

--- corehq/apps/registration/validation.py
+++ corehq/apps/registration/validation.py
@@ -11,19 +11,19 @@ class AdminInvitesUserFormValidator():
 
     @staticmethod
     def validate_parameters(domain, upload_user, parameters):
-        can_edit_tableau_config = (upload_user.has_permission(domain, 'edit_user_tableau_config')
-                        and TABLEAU_USER_SYNCING.enabled(domain))
-        if (('tableau_role' in parameters or 'tableau_group_indices' in parameters)
-        and not can_edit_tableau_config):
-            return _("You do not have permission to edit Tableau Configuraion.")
+        if 'tableau_role' in parameters or 'tableau_group_indices' in parameters:
+            can_edit_tableau_config = (
+                upload_user.has_permission(domain, 'edit_user_tableau_config')
+                and TABLEAU_USER_SYNCING.enabled(domain)
+            )
+            if not can_edit_tableau_config:
+                return _("You do not have permission to edit Tableau Configuration.")
 
-        has_profile_privilege = domain_has_privilege(domain, privileges.APP_USER_PROFILES)
-        if 'profile' in parameters and not has_profile_privilege:
+        if 'profile' in parameters and not domain_has_privilege(domain, privileges.APP_USER_PROFILES):
             return _("This domain does not have user profile privileges.")
 
-        has_locations_privilege = domain_has_privilege(domain, privileges.LOCATIONS)
         if (('primary_location' in parameters or 'assigned_locations' in parameters)
-        and not has_locations_privilege):
+           and not domain_has_privilege(domain, privileges.LOCATIONS)):
             return _("This domain does not have locations privileges.")
 
     @staticmethod
