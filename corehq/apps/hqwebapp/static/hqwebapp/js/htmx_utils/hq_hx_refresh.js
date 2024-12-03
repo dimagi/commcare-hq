0d6eeed97d1b7b7668f7b06fa309ccfb8b86f3cb
Biyeun Buczyk <biyeun@dimagi.com>
2024-12-03 21:55:22 +0100

    update hq_hx_refresh to be a little clearer and add options for after request and after swap

--- corehq/apps/hqwebapp/static/hqwebapp/js/htmx_utils/hq_hx_refresh.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/htmx_utils/hq_hx_refresh.js
@@ -1,10 +1,32 @@
 /*
-    Sends an `hqRefresh` event to the selector (element) specified in the `hq-hx-refresh` attribute.
+    Used to chain an `hqRefresh` event to a related HTMX element making a request.
+
+    The attribute `hq-hx-refresh-after` sends the `hqRefresh` event to the target selector
+    element on `htmx:afterRequest`.
+
+    THe attribute `hq-hx-refresh-swap` sends the `hqRefresh` event to the target selector
+    element on `htmx:afterSwap`.
+
+    The value of the attributes should be a css selector--for example, `#element` where element is the CSS id.
+
+    The target element can then apply `hx-trigger="hqRefresh"`, effectively chaining a refresh event to the
+    original triggering request.
+
+    This is commonly used to trigger a refresh of tabular data with Django Tables using the `BaseHtmxTable`
+    subclass. However, it can be used to chain other HTMX elements together
  */
 import htmx from 'htmx.org';
 
-document.body.addEventListener('htmx:afterSwap', (evt) => {
-    if (evt.detail.elt.hasAttribute('hq-hx-refresh')) {
-        htmx.trigger(evt.detail.elt.getAttribute('hq-hx-refresh'), 'hqRefresh');
+const handleRefresh = (evt, attribute) => {
+    if (evt.detail.elt.hasAttribute(attribute)) {
+        htmx.trigger(evt.detail.elt.getAttribute(attribute), 'hqRefresh');
     }
+};
+
+document.body.addEventListener('htmx:afterRequest', (evt) => {
+    handleRefresh(evt, 'hq-hx-refresh-after');
+});
+
+document.body.addEventListener('htmx:afterSwap', (evt) => {
+    handleRefresh(evt, 'hq-hx-refresh-swap');
 });
