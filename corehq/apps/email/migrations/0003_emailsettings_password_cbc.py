fb4c15a4a5a3af50c1c79b29272b16e0b0901435
Jonathan Tang <Jonahtang738@gmail.com>
2024-12-05 12:33:25 -0800

    make migration idempotent

--- corehq/apps/email/migrations/0003_emailsettings_password_cbc.py
+++ corehq/apps/email/migrations/0003_emailsettings_password_cbc.py
@@ -2,7 +2,7 @@
 
 from django.db import migrations
 
-from corehq.motech.const import ALGO_AES
+from corehq.motech.const import ALGO_AES, ALGO_AES_CBC
 from corehq.util.django_migrations import skip_on_fresh_install
 from corehq.motech.utils import reencrypt_ecb_to_cbc_mode
 
@@ -12,8 +12,9 @@ def copy_and_reencrypt_password_to_password_cbc(apps, schema_editor):
     EmailSettings = apps.get_model('email', 'EmailSettings')
 
     for email_settings in EmailSettings.objects.all():
-        email_settings.password = reencrypt_ecb_to_cbc_mode(email_settings.password, f'${ALGO_AES}$')
-        email_settings.save()
+        if not email_settings.password.startswith(f'${ALGO_AES_CBC}$'):
+            email_settings.password = reencrypt_ecb_to_cbc_mode(email_settings.password, f'${ALGO_AES}$')
+            email_settings.save()
 
 
 class Migration(migrations.Migration):
