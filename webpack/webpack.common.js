601cf922e05ec543639dbb69b6c5e1c97a16bc30
Jenny Schweers <jschweers@dimagi.com>
2024-12-12 18:35:26 -0500

    Migrated view lookup tables page to webpack
    The non-minified version of datatables errors out.
    This is the B3 library so I'm not going to worry about it.
    Webpack will minify it.

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,6 +7,8 @@ const hqPlugins = require('./plugins');
 const aliases = {
     "commcarehq": path.resolve(utils.getStaticPathForApp('hqwebapp', 'js/bootstrap5/'),
         'commcarehq'),
+    "datatables.bootstrap": "datatables.net-bs5",
+    "datatables.fixedColumns": "datatables-fixedcolumns",
     "jquery": require.resolve('jquery'),
     "langcodes/js/langcodes": path.resolve("submodules/langcodes/static/langcodes/js/langcodes"),
 
