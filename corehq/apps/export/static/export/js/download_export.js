88c0f54e5d06a4f1c9c01f4fffe1470d960f987e
Jenny Schweers <jschweers@dimagi.com>
2024-12-12 18:32:43 -0500

    Migrated download export page

--- corehq/apps/export/static/export/js/download_export.js
+++ corehq/apps/export/static/export/js/download_export.js
@@ -20,6 +20,7 @@ hqDefine('export/js/download_export', [
     'reports/js/util',
     'export/js/utils',
     'jquery.cookie/jquery.cookie',      // for resuming export downloads on refresh
+    'commcarehq',
 ], function (
     $,
     ko,
