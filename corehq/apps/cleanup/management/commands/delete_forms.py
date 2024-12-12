9ca88fde78f5dbeb3f7ea32a277ccb5fe897df76
Matt Riley <mriley@dimagi.com>
2024-12-11 19:28:09 -0500

    Added form deletion script

---
+++ corehq/apps/cleanup/management/commands/delete_forms.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand
+import csv
+import itertools
+from dimagi.utils.chunked import chunked
+from corehq.form_processor.models import XFormInstance
+
+
+INDEX_FORM_ID = 1
+CHUNK_SIZE = 100
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('domain', help='Domain name that owns the forms to be deleted')
+        parser.add_argument('filename', help='path to the CSV file')
+        parser.add_argument('--resume_id', help='form ID to start at, within the CSV file')
+
+    def handle(self, domain, filename, resume_id=None, **options):
+        # expects the filename to have a CSV with a header containing "Deletion Status" and "Form ID" fields
+        with open(filename) as csvfile:
+            reader = csv.reader(csvfile, delimiter=',')
+            self._process_rows(reader, domain, resume_id)
+
+    def _process_rows(self, rows, domain, resume_id):
+        next(rows)   # skip header line
+
+        num_deleted = 0
+
+        if resume_id:
+            print('resuming at: ', resume_id)
+            rows = itertools.dropwhile(lambda row: row[INDEX_FORM_ID] != resume_id, rows)
+
+        print('Starting form deletion')
+        for chunk in chunked(rows, CHUNK_SIZE):
+            form_ids = [row[INDEX_FORM_ID] for row in chunk]
+
+            try:
+                deleted_form_ids = set(XFormInstance.objects.hard_delete_forms(
+                    domain, form_ids, return_ids=True))
+            except Exception:
+                print('failed during processing of: ', form_ids)
+                raise
+
+            for form_id in form_ids:
+                if form_id in deleted_form_ids:
+                    print('Deleted: ', form_id)
+                else:
+                    print('Not found:', form_id)
+
+            num_deleted += len(deleted_form_ids)
+
+        print(f'Complete -- removed {num_deleted} forms')
