b089038236fa418388e90f051093803baebfe9b9
Daniel Miller <dmiller@dimagi.com>
2024-12-12 11:27:43 -0500

    Remove unneeded B5 branching in crispy helper code

--- corehq/apps/hqwebapp/crispy.py
+++ corehq/apps/hqwebapp/crispy.py
@@ -1,4 +1,3 @@
-import re
 from contextlib import contextmanager
 from django.forms.widgets import DateTimeInput
 from django.template.loader import render_to_string
@@ -14,9 +13,7 @@ from crispy_forms.layout import LayoutObject
 from crispy_forms.utils import flatatt, get_template_pack, render_field
 
 CSS_LABEL_CLASS = 'field-label'
-CSS_LABEL_CLASS_BOOTSTRAP5 = 'field-label'
 CSS_FIELD_CLASS = 'field-control'
-CSS_FIELD_CLASS_BOOTSTRAP5 = 'field-control'
 CSS_ACTION_CLASS = CSS_FIELD_CLASS
 
 
@@ -28,12 +25,7 @@ class HQFormHelper(FormHelper):
     def __init__(self, *args, **kwargs):
         super(HQFormHelper, self).__init__(*args, **kwargs)
         from corehq.apps.hqwebapp.utils.bootstrap import get_bootstrap_version, BOOTSTRAP_5
-        bootstrap_version = get_bootstrap_version()
-        use_bootstrap5 = bootstrap_version == BOOTSTRAP_5
-        if use_bootstrap5:
-            self.label_class = CSS_LABEL_CLASS_BOOTSTRAP5
-            self.field_class = CSS_FIELD_CLASS_BOOTSTRAP5
-            self.use_bootstrap5 = use_bootstrap5
+        self.use_bootstrap5 = get_bootstrap_version() == BOOTSTRAP_5
 
         if 'autocomplete' not in self.attrs:
             self.attrs.update({
@@ -75,15 +67,7 @@ class ErrorsOnlyField(Field):
 
 
 def get_form_action_class():
-    """This is only valid for bootstrap 5"""
-    return CSS_FIELD_CLASS_BOOTSTRAP5
-
-
-def _get_offsets(context):
-    label_class = context.get('label_class', '')
-    use_bootstrap5 = context.get('use_bootstrap5')
-    return (label_class.replace('col', 'offset') if use_bootstrap5
-            else re.sub(r'(xs|sm|md|lg)-', r'\g<1>-offset-', label_class))
+    return CSS_FIELD_CLASS
 
 
 class FormActions(OriginalFormActions):
@@ -101,11 +85,10 @@ class FormActions(OriginalFormActions):
                 template_pack=template_pack,
             )
         fields_html = mark_safe(fields_html)  # nosec: just concatenated safe fields
-        offsets = _get_offsets(context)
         context.update({
             'formactions': self,
             'fields_output': fields_html,
-            'offsets': offsets,
+            'offsets': '',
             'field_class': context.get('field_class', '')
         })
         return render_to_string(self.template, context.flatten())
