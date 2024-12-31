74688fdc06b38817696cfe5d7912bb40e2f4097a
nospame <mapson.evan@gmail.com>
2024-12-31 14:01:47 -0800

    Remove dead 'choose your plan' and 'request a trial' code

--- corehq/apps/registration/static/registration/js/register_new_user.js
+++ corehq/apps/registration/static/registration/js/register_new_user.js
@@ -17,44 +17,6 @@ hqDefine('registration/js/register_new_user', [
 ) {
     'use strict';
 
-    $('#js-create-account').click(function (e) {
-        startCreateAccount(e);
-    });
-
-    $('#js-self-signup').click(function (e) {
-        regForm.isSelfSignup(true);
-        startCreateAccount(e);
-    });
-
-    var startCreateAccount = function (e) {
-        e.preventDefault();
-        $('#registration-choose-plan-container').hide();
-        $('#registration-form-container').fadeIn();
-
-        $('#back-to-start-btn').removeClass('hide');
-    };
-
-    $('#back-to-start-btn').click(function () {
-        $('#registration-form-container').hide();
-        $('#registration-choose-plan-container').fadeIn();
-    });
-
-    kissmetrics.whenReadyAlways(function () {
-
-        $('#js-start-trial').click(function () {
-            kissmetrics.track.event("Signup alt ux dec2018 - clicked start trial");
-        });
-
-        $('#js-get-tour').click(function () {
-            kissmetrics.track.event("Signup alt ux dec2018 - clicked get a tour");
-            kissmetrics.track.event("Demo Workflow - Get A Tour Button Clicked (new UX)");
-        });
-
-        $('#start-chat-cta-btn').click(function () {
-            kissmetrics.track.event("Signup alt ux dec2018 - clicked start chat");
-        });
-    });
-
     newUser.setOnModuleLoad(function () {
         $('.loading-form-step').fadeOut(500, function () {
             $('.step-1').fadeIn(500);
