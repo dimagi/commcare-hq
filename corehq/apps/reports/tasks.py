f942db6c00d6baf93ba35e01e5979366fc9a6887
nospame <mapson.evan@gmail.com>
2025-01-03 11:21:55 -0800

    Move update_domain_metrics and run_datadog_user_stats to data_analytics app

--- corehq/apps/reports/tasks.py
+++ corehq/apps/reports/tasks.py
@@ -1,9 +1,7 @@
 import os
 import uuid
 import zipfile
-from datetime import datetime, timedelta, timezone
-
-from django.db.models import Q
+from datetime import datetime, timedelta
 
 from celery.schedules import crontab
 from celery.utils.log import get_task_logger
@@ -16,12 +14,8 @@ from soil import DownloadBase
 from soil.util import expose_blob_download
 
 from corehq.apps.celery import periodic_task, task
-from corehq.apps.data_analytics.models import DomainMetrics
-from corehq.apps.domain.calculations import (
-    all_domain_stats,
-    calced_props,
-    domain_metrics,
-)
+from corehq.apps.data_analytics.tasks import datadog_report_user_stats
+from corehq.apps.domain.calculations import all_domain_stats, calced_props
 from corehq.apps.domain.models import Domain
 from corehq.apps.es import DomainES, FormES, filters
 from corehq.apps.es.domains import domain_adapter
@@ -33,7 +27,6 @@ from corehq.const import ONE_DAY
 from corehq.form_processor.models import XFormInstance
 from corehq.util.dates import get_timestamp_for_filename
 from corehq.util.files import TransientTempfile, safe_filename_header
-from corehq.util.metrics import metrics_gauge
 from corehq.util.view_utils import absolute_reverse
 
 from .analytics.esaccessors import (
@@ -109,114 +102,6 @@ def get_domains_to_update_es_filter():
             filters.term('name', domains_submitted_today)
         )
     )
-
-
-@periodic_task(run_every=crontab(hour="22", minute="0", day_of_week="*"), queue='background_queue')
-def update_domain_metrics():
-    domains = get_domains_to_update()
-    for chunk in chunked(domains, 5000):
-        update_domain_metrics_for_domains.delay(chunk)
-
-
-@task(queue='background_queue')
-def update_domain_metrics_for_domains(domains):
-    """
-    :param domains: list of domain names
-    """
-    # relying on caching for efficiency
-    all_stats = all_domain_stats()
-    active_users_by_domain = {}
-    for domain in domains:
-        metrics, __ = _update_or_create_domain_metrics(domain, all_stats)
-        if metrics:
-            active_users_by_domain[domain] = metrics['active_mobile_workers']
-
-    datadog_report_user_stats('commcare.active_mobile_workers.count', active_users_by_domain)
-
-
-def _update_or_create_domain_metrics(domain, all_stats):
-    try:
-        domain_obj = Domain.get_by_name(domain)
-        metrics_dict = domain_metrics(domain_obj, domain_obj['_id'], all_stats)
-        return DomainMetrics.objects.update_or_create(
-            **metrics_dict,
-        )
-    except Exception as e:
-        notify_exception(
-            None, message='Failed to create or update domain metrics for {domain}: {}'.format(e, domain=domain)
-        )
-
-
-def get_domains_to_update():
-    """
-    Returns a list of domains that are active and meet one or more
-    of the following criteria:
-     - never had DomainMetrics created
-     - DomainMetrics was updated over one week ago
-     - new form submissions within the last day
-    """
-    is_domain_active = filters.term('is_active', True)
-    active_domains = (
-        DomainES().filter(is_domain_active)
-        .terms_aggregation('name.exact', 'domain')
-        .size(0).run().aggregations.domain.keys
-    )
-
-    now = datetime.now(tz=timezone.utc)
-    yesterday = now - timedelta(days=1)
-    domains_submitted_within_day = (
-        FormES().submitted(gte=yesterday)
-        .terms_aggregation('domain.exact', 'domain')
-        .size(0).run().aggregations.domain.keys
-    )
-
-    last_week = now - timedelta(days=7)
-    domains_up_to_date = DomainMetrics.objects.filter(
-        Q(domain__in=active_domains)
-        & Q(last_modified__gte=last_week)
-        & ~Q(domain__in=domains_submitted_within_day)
-    ).values_list('domain', flat=True)
-
-    return set(active_domains) - set(domains_up_to_date)
-
-
-@periodic_task(run_every=timedelta(minutes=1), queue='background_queue')
-def run_datadog_user_stats():
-    all_stats = all_domain_stats()
-
-    datadog_report_user_stats(
-        'commcare.mobile_workers.count',
-        commcare_users_by_domain=all_stats['commcare_users'],
-    )
-
-
-def datadog_report_user_stats(metric_name, commcare_users_by_domain):
-    commcare_users_by_domain = summarize_user_counts(commcare_users_by_domain, n=50)
-    for domain, user_count in commcare_users_by_domain.items():
-        metrics_gauge(metric_name, user_count, tags={
-            'domain': '_other' if domain == () else domain
-        }, multiprocess_mode='max')
-
-
-def summarize_user_counts(commcare_users_by_domain, n):
-    """
-    Reduce (domain => user_count) to n entries, with all other entries summed to a single one
-
-    This allows us to report individual domain data to datadog for the domains that matter
-    and report a single number that combines the users for all other domains.
-
-    :param commcare_users_by_domain: the source data
-    :param n: number of domains to reduce the map to
-    :return: (domain => user_count) of top domains
-             with a single entry under () for all other domains
-    """
-    user_counts = sorted((user_count, domain) for domain, user_count in commcare_users_by_domain.items())
-    if n:
-        top_domains, other_domains = user_counts[-n:], user_counts[:-n]
-    else:
-        top_domains, other_domains = [], user_counts[:]
-    other_entry = (sum(user_count for user_count, _ in other_domains), ())
-    return {domain: user_count for user_count, domain in top_domains + [other_entry]}
 
 
 @task(serializer='pickle', ignore_result=True)
--- corehq/apps/reports/tests/test_tasks.py
+++ corehq/apps/reports/tests/test_tasks.py
@@ -4,7 +4,6 @@ from unittest.mock import patch
 from uuid import uuid4
 from zipfile import ZipFile
 
-from django.db.models import BooleanField, DateTimeField, IntegerField
 from django.test import SimpleTestCase, TestCase
 
 from attrs import define, field
@@ -12,7 +11,6 @@ from freezegun import freeze_time
 
 from dimagi.utils.parsing import json_format_datetime
 
-from corehq.apps.data_analytics.models import DomainMetrics
 from corehq.apps.domain.shortcuts import create_domain
 from corehq.apps.es import DomainES, form_adapter
 from corehq.apps.es.domains import domain_adapter
@@ -21,7 +19,6 @@ from corehq.apps.reports.tasks import (
     _get_question_id_for_attachment,
     _make_unique_filename,
     _write_attachments_to_file,
-    get_domains_to_update,
     get_domains_to_update_es_filter,
 )
 from corehq.form_processor.tests.utils import create_form_for_test
@@ -76,96 +73,6 @@ class GetQuestionIdTests(SimpleTestCase):
 
         result = _get_question_id_for_attachment(form, 'attachment.ext')
         self.assertIsNone(result)
-
-
-@es_test(requires=[domain_adapter, form_adapter], setup_class=True)
-class TestGetDomainsToUpdate(TestCase):
-    def test_domain_metrics_never_updated_is_included(self):
-        domain = self.index_domain('never-updated')
-        with self.assertRaises(DomainMetrics.DoesNotExist):
-            DomainMetrics.objects.get(domain=domain.name)
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, {domain.name})
-
-    @freeze_time('2024-01-10')
-    def test_domain_metrics_updated_over_one_week_ago_is_included(self):
-        domain = self.index_domain('cp-over-one-week')
-        self.create_domain_metrics(domain.name, last_modified=datetime(2024, 1, 2, 23, 59))
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, {domain.name})
-
-    @freeze_time('2024-01-10')
-    def test_domain_metrics_updated_exactly_one_week_ago_is_excluded(self):
-        domain = self.index_domain('cp-one-week')
-        self.create_domain_metrics(domain.name, last_modified=datetime(2024, 1, 3))
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, set())
-
-    @freeze_time('2024-01-10')
-    def test_domain_metrics_updated_less_than_one_week_ago_is_excluded(self):
-        domain = self.index_domain('cp-less-than-one-week')
-        self.create_domain_metrics(domain.name, last_modified=datetime(2024, 1, 4))
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, set())
-
-    @freeze_time('2024-01-10')
-    def test_form_submission_in_the_last_day_is_included(self):
-        domain = self.index_domain('form-from-today')
-        self.index_form(domain.name, received_on=datetime(2024, 1, 9, 0, 0))
-        self.create_domain_metrics(domain.name, last_modified=datetime(2024, 1, 9))
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, {domain.name})
-
-    @freeze_time('2024-01-10')
-    def test_form_submission_over_one_day_ago_is_excluded(self):
-        domain = self.index_domain('form-from-yesterday')
-        self.index_form(domain.name, received_on=datetime(2024, 1, 8, 23, 59))
-        self.create_domain_metrics(domain.name, last_modified=datetime(2024, 1, 9))
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, set())
-
-    @freeze_time('2024-01-10')
-    def test_inactive_domain_is_excluded(self):
-        domain = self.index_domain('inactive-domain', active=False)
-        self.create_domain_metrics(domain.name)
-
-        domains = get_domains_to_update()
-        self.assertEqual(domains, set())
-
-    def index_domain(self, name, active=True, cp_last_updated=None):
-        domain = create_domain(name, active)
-        self.addCleanup(domain.delete)
-        if cp_last_updated:
-            domain.cp_last_updated = json_format_datetime(cp_last_updated)
-        domain_adapter.index(domain, refresh=True)
-        self.addCleanup(domain_adapter.delete, domain._id, refresh=True)
-        return domain
-
-    def index_form(self, domain, received_on):
-        xform = create_form_for_test(domain, received_on=received_on)
-        form_adapter.index(xform, refresh=True)
-        self.addCleanup(form_adapter.delete, xform.form_id, refresh=True)
-        return xform
-
-    def create_domain_metrics(self, domain, last_modified=None):
-        metrics_dict = {}
-        for metrics_field in DomainMetrics._meta.get_fields():
-            if isinstance(metrics_field, BooleanField):
-                metrics_dict[metrics_field.name] = False
-            if isinstance(metrics_field, DateTimeField):
-                metrics_dict[metrics_field.name] = datetime(2024, 1, 1)
-            if isinstance(metrics_field, IntegerField):
-                metrics_dict[metrics_field.name] = 0
-        domain_metrics = DomainMetrics.objects.create(domain=domain, **metrics_dict)
-        self.addCleanup(domain_metrics.delete)
-        if last_modified:
-            DomainMetrics.objects.filter(domain=domain).update(last_modified=last_modified)
 
 
 @es_test(requires=[domain_adapter, form_adapter], setup_class=True)
