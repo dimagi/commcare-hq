6c1120861ccb2689875006048c481cddc55b68cc
Jenny Schweers <jschweers@dimagi.com>
2025-01-09 17:16:34 -0500

    WIP

--- custom/inddex/static/inddex/js/main.js
+++ custom/inddex/static/inddex/js/main.js
@@ -1,9 +1,10 @@
 hqDefine("inddex/js/main", function () {
-    const tabular = hqImport("reports/js/bootstrap3/tabular");
+    const initialPageData = hqImport("hqwebapp/js/initial_page_data"),
+        tabular = hqImport("reports/js/bootstrap3/tabular");
     $(document).on('ajaxSuccess', function (e, xhr, ajaxOptions, data) {
         $(".datatable[data-slug]").each(function (index, table) {
             const tableData = $(table).data();
-            tabular.renderPage(data.slug, data.js_options);
+            tabular.renderPage(data.slug, initialPageData.get("report_table_js_options"));
         });
     });
 });
