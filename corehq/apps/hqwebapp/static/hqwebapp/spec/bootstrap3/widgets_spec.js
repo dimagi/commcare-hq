f4e15861520b0c686c6d7be16e828f12db047e5b
Jenny Schweers <jschweers@dimagi.com>
2025-01-10 11:40:54 -0500

    Added test failure in hqwebapp/bootstrap3

--- corehq/apps/hqwebapp/static/hqwebapp/spec/bootstrap3/widgets_spec.js
+++ corehq/apps/hqwebapp/static/hqwebapp/spec/bootstrap3/widgets_spec.js
@@ -7,7 +7,7 @@ hqDefine("hqwebapp/spec/bootstrap3/widgets_spec", [
     describe('widgets', function () {
         describe('parseEmail', function () {
             it('should parse comma-separated input into individual emails', function () {
-                assert.deepEqual(widgets.parseEmails("abcdefghi"), ["abcdefghi"]);
+                assert.deepEqual(widgets2.parseEmails("abcdefghi"), ["abcdefghi"]);
                 assert.deepEqual(widgets.parseEmails("a@b.com, x@y.com"), ["a@b.com", "x@y.com"]);
                 assert.deepEqual(widgets.parseEmails("a@b.com,x@y.com"), ["a@b.com", "x@y.com"]);
                 assert.deepEqual(widgets.parseEmails("a@b.com  x@y.com"), ["a@b.com", "x@y.com"]);
