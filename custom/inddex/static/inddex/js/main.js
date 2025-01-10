a3f62298df6edf15a574ed5bb73cc9a730cffc84
Jenny Schweers <jschweers@dimagi.com>
2025-01-09 20:13:25 -0500

    Fixed lint error

--- custom/inddex/static/inddex/js/main.js
+++ custom/inddex/static/inddex/js/main.js
@@ -3,7 +3,6 @@ hqDefine("inddex/js/main", function () {
         tabular = hqImport("reports/js/bootstrap3/tabular");
     $(document).on('ajaxSuccess', function (e, xhr, ajaxOptions, data) {
         $(".datatable[data-slug]").each(function (index, table) {
-            const tableData = $(table).data();
             tabular.renderPage(data.slug, initialPageData.get("report_table_js_options"));
         });
     });
