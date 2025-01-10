fe5b3472cc21652c83f06777db45d9c4ddf3ec1b
nospame <mapson.evan@gmail.com>
2025-01-10 14:55:43 -0800

    Add cleanup to BaseInvoiceTestCase

--- corehq/apps/accounting/tests/base_tests.py
+++ corehq/apps/accounting/tests/base_tests.py
@@ -6,6 +6,19 @@ from dateutil.relativedelta import relativedelta
 from django_prbac.models import Role
 
 from corehq.apps.accounting import utils
+from corehq.apps.accounting.models import (
+    BillingAccount,
+    BillingRecord,
+    CreditAdjustment,
+    CreditLine,
+    CustomerBillingRecord,
+    CustomerInvoice,
+    Invoice,
+    InvoiceCommunicationHistory,
+    LineItem,
+    Subscription,
+    SubscriptionAdjustment,
+)
 from corehq.apps.accounting.tasks import (
     calculate_users_in_all_domains,
     calculate_web_users_in_all_billing_accounts,
@@ -39,12 +52,12 @@ class BaseInvoiceTestCase(BaseAccountingTest):
         cls.subscription_end_date = cls.subscription_start_date + relativedelta(months=cls.subscription_length)
         # make sure the subscription is still active when we count web users
         cls.subscription_is_active = cls.is_testing_web_user_feature
+        cls.currency = generator.init_default_currency()
 
     def setUp(self):
         super().setUp()
         self.billing_contact = generator.create_arbitrary_web_user_name()
         self.dimagi_user = generator.create_arbitrary_web_user_name(is_dimagi=True)
-        self.currency = generator.init_default_currency()
         self.account = generator.billing_account(self.dimagi_user, self.billing_contact)
         self.domain = generator.arbitrary_domain()
         self.subscription = generator.generate_domain_subscription(
@@ -54,13 +67,32 @@ class BaseInvoiceTestCase(BaseAccountingTest):
             date_end=self.subscription_end_date,
             is_active=self.subscription_is_active
         )
+        self.addCleanup(self.cleanUp)
 
-    def tearDown(self):
+    def cleanUp(self):
         for user in self.domain.all_users():
             user.delete(self.domain.name, deleted_by=None)
-
         self.domain.delete()
-        super().tearDown()
+
+        def delete_objects(obj_cls):
+            obj_cls.objects.all().delete()
+
+        # order is important as many of these have protected foreign keys on each other
+        for obj_cls in [
+            CreditAdjustment,
+            CreditLine,
+            SubscriptionAdjustment,
+            BillingRecord,
+            InvoiceCommunicationHistory,
+            LineItem,
+            Invoice,
+            CustomerBillingRecord,
+            CustomerInvoice,
+        ]:
+            delete_objects(obj_cls)
+
+        Subscription.visible_and_suppressed_objects.all().delete()
+        delete_objects(BillingAccount)
 
     @classmethod
     def tearDownClass(cls):
