29150957029615d5dc6e362efef57b27c026ec69
Norman Hooper <nhooper@dimagi.com>
2025-01-10 23:11:59 +0000

    Clean up tests

--- corehq/motech/repeaters/tests/test_tasks.py
+++ corehq/motech/repeaters/tests/test_tasks.py
@@ -1,37 +1,26 @@
 from collections import namedtuple
-from contextlib import contextmanager
 from datetime import datetime, timedelta
 from unittest.mock import MagicMock, patch
-from uuid import uuid4
 
 from django.test import SimpleTestCase, TestCase
 
-from nose.tools import assert_equal
+from freezegun import freeze_time
 
-from corehq.apps.receiverwrapper.util import submit_form_locally
-from corehq.form_processor.models import XFormInstance
-from corehq.form_processor.utils.xform import (
-    FormSubmissionBuilder,
-    TestFormMetadata,
-)
 from corehq.motech.models import ConnectionSettings, RequestLog
-from corehq.motech.repeaters.models import FormRepeater, Repeater, RepeatRecord
-from corehq.motech.repeaters.tasks import (
+from corehq.util.test_utils import flag_enabled
+
+from ..const import State
+from ..models import FormRepeater, Repeater, RepeatRecord
+from ..tasks import (
     _get_wait_duration_seconds,
     _process_repeat_record,
     delete_old_request_logs,
     get_repeater_ids_by_domain,
     iter_ready_repeater_ids,
-    process_repeater,
     update_repeater,
 )
-from corehq.util.test_utils import _create_case, flag_enabled
 
-from ..const import State
-
-DOMAIN = 'gaidhlig'
-PAYLOAD_IDS = ['aon', 'dha', 'trì', 'ceithir', 'coig', 'sia', 'seachd', 'ochd',
-               'naoi', 'deich']
+DOMAIN = 'test-tasks'
 
 
 ResponseMock = namedtuple('ResponseMock', 'status_code reason')
@@ -77,20 +66,6 @@ class TestDeleteOldRequestLogs(TestCase):
 
         count = RequestLog.objects.filter(domain=DOMAIN).count()
         self.assertEqual(count, 0)
-
-
-@contextmanager
-def form_context(form_ids):
-    for form_id in form_ids:
-        builder = FormSubmissionBuilder(
-            form_id=form_id,
-            metadata=TestFormMetadata(domain=DOMAIN),
-        )
-        submit_form_locally(builder.as_xml_string(), DOMAIN)
-    try:
-        yield
-    finally:
-        XFormInstance.objects.hard_delete_forms(DOMAIN, form_ids)
 
 
 class TestProcessRepeatRecord(TestCase):
@@ -256,7 +231,7 @@ class TestProcessRepeatRecord(TestCase):
         self.addCleanup(patch_domain_can_forward.stop)
 
 
-def test_iter_ready_repeater_ids_once():
+def test_iter_ready_repeater_ids():
     with (
         patch(
             'corehq.motech.repeaters.tasks.Repeater.objects.get_all_ready_ids_by_domain',
@@ -272,7 +247,7 @@ def test_iter_ready_repeater_ids_once():
         ),
     ):
         pairs = list(iter_ready_repeater_ids())
-        assert_equal(pairs, [
+        assert pairs == [
             # First round of domains
             ('domain1', 'repeater_id3'),
             ('domain2', 'repeater_id5'),
@@ -284,7 +259,7 @@ def test_iter_ready_repeater_ids_once():
 
             # Third round
             ('domain1', 'repeater_id1'),
-        ])
+        ]
 
 
 def test_get_repeater_ids_by_domain():
@@ -306,71 +281,10 @@ def test_get_repeater_ids_by_domain():
             side_effect=lambda dom, __: dom == 'domain3'),
     ):
         repeater_ids_by_domain = get_repeater_ids_by_domain()
-        assert_equal(repeater_ids_by_domain, {
+        assert repeater_ids_by_domain == {
             'domain2': ['repeater_id4', 'repeater_id5'],
             'domain3': ['repeater_id6'],
-        })
-
-
-@flag_enabled('PROCESS_REPEATERS')
-class TestProcessRepeater(TestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-
-        can_forward_now_patch = patch(
-            'corehq.motech.repeaters.tasks.domain_can_forward_now',
-            return_value=True,
-        )
-        can_forward_now_patch = can_forward_now_patch.start()
-        cls.addClassCleanup(can_forward_now_patch.stop)
-
-        cls.set_backoff_patch = patch.object(FormRepeater, 'set_backoff')
-        cls.set_backoff_patch.start()
-        cls.addClassCleanup(cls.set_backoff_patch.stop)
-
-        connection_settings = ConnectionSettings.objects.create(
-            domain=DOMAIN,
-            url='http://www.example.com/api/'
-        )
-        cls.repeater = FormRepeater.objects.create(
-            domain=DOMAIN,
-            connection_settings=connection_settings,
-        )
-
-    def test_process_repeater_sends_repeat_record(self):
-        payload, __ = _create_case(
-            domain=DOMAIN,
-            case_id=str(uuid4()),
-            case_type='case',
-            owner_id='abc123'
-        )
-        self.repeater.register(payload)
-
-        with patch('corehq.motech.repeaters.models.simple_request') as request_mock:
-            request_mock.return_value = ResponseMock(status_code=200, reason='OK')
-            process_repeater(DOMAIN, self.repeater.repeater_id, 'token')
-
-            request_mock.assert_called_once()
-
-    def test_process_repeater_updates_repeater(self):
-        payload, __ = _create_case(
-            domain=DOMAIN,
-            case_id=str(uuid4()),
-            case_type='case',
-            owner_id='abc123'
-        )
-        self.repeater.register(payload)
-
-        with patch('corehq.motech.repeaters.models.simple_request') as request_mock:
-            request_mock.return_value = ResponseMock(
-                status_code=429,
-                reason='Too Many Requests',
-            )
-            process_repeater(DOMAIN, self.repeater.repeater_id, 'token')
-
-        self.repeater.set_backoff.assert_called_once()
+        }
 
 
 @flag_enabled('PROCESS_REPEATERS')
@@ -412,6 +326,7 @@ class TestUpdateRepeater(SimpleTestCase):
         mock_repeater.reset_backoff.assert_not_called()
 
 
+@freeze_time('2025-01-01')
 class TestGetWaitDurationSeconds(TestCase):
 
     @classmethod
