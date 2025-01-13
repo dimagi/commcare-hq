92e9fd08004521ce764bf66aad87dfe78eab8aa1
Jenny Schweers <jschweers@dimagi.com>
2025-01-13 18:50:38 -0500

    Migrated locations tests to webpack

---
+++ corehq/apps/locations/static/locations/spec/main.js
@@ -0,0 +1,9 @@
+import hqMocha from "mocha/js/main";
+import "commcarehq";
+
+import "locations/spec/types_spec";
+import "locations/spec/location_drilldown_spec";
+
+$(function () {
+    hqMocha.run();
+});
