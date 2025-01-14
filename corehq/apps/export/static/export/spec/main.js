fa56510b5afdffb46816d984d379c9283a866ebd
Jenny Schweers <jschweers@dimagi.com>
2025-01-13 22:20:16 -0500

    Removed unnecessary import in export tests

--- corehq/apps/export/static/export/spec/main.js
+++ corehq/apps/export/static/export/spec/main.js
@@ -1,7 +1,6 @@
 import hqMocha from "mocha/js/main";
 import "commcarehq";
 
-import "export/spec/data/export_instances";
 import "export/spec/ExportInstance.spec";
 import "export/spec/ExportColumn.spec";
 import "export/spec/Exports.Utils.spec";
