96d5b9220ddec6ff91e02634166f7ef6d56666d5
Jenny Schweers <jschweers@dimagi.com>
2025-01-20 18:24:28 -0500

    Added explicit dependencies to app_manager/js/modules/module_view_report

--- corehq/apps/app_manager/static/app_manager/js/modules/module_view_report.js
+++ corehq/apps/app_manager/static/app_manager/js/modules/module_view_report.js
@@ -1,15 +1,23 @@
-hqDefine("app_manager/js/modules/module_view_report", function () {
+hqDefine("app_manager/js/modules/module_view_report", [
+    "jquery",
+    "underscore",
+    "hqwebapp/js/initial_page_data",
+    "app_manager/js/app_manager_media",
+    "app_manager/js/modules/report_module",
+    "reports_core/js/choice_list_utils",
+], function (
+    $,
+    _,
+    initialPageData,
+    appManagerMedia,
+    reportModule,
+    choiceListUtils,
+) {
     $(function () {
-        var initialPageData = hqImport("hqwebapp/js/initial_page_data");
-        var initNavMenuMedia = hqImport('app_manager/js/app_manager_media').initNavMenuMedia;
-        var reportModuleModel = hqImport('app_manager/js/modules/report_module').reportModuleModel;
-        var staticFilterDataModel = hqImport('app_manager/js/modules/report_module').staticFilterDataModel;
-        var choiceListUtils = hqImport('reports_core/js/choice_list_utils');
-
         // Hacky: report modules only deal with one kind of multimedia (the menu image/audio),
         // so assume nav_menu_media_specifics has one element.
         var navMenuMediaItem = initialPageData.get("nav_menu_media_specifics")[0];
-        var navMenuMedia = initNavMenuMedia(
+        var navMenuMedia = appManagerMedia.initNavMenuMedia(
             "",
             navMenuMediaItem.menu_refs.image,
             navMenuMediaItem.menu_refs.audio,
@@ -18,8 +26,8 @@ hqDefine("app_manager/js/modules/module_view_report", function () {
         );
 
         var saveURL = initialPageData.reverse("edit_report_module");
-        var staticData = staticFilterDataModel(initialPageData.get('static_data_options'));
-        var reportModule = reportModuleModel(_.extend({}, initialPageData.get("report_module_options"), {
+        var staticData = reportModule.staticFilterDataModel(initialPageData.get('static_data_options'));
+        var reportModule = reportModule.reportModuleModel(_.extend({}, initialPageData.get("report_module_options"), {
             lang: initialPageData.get('lang'),
             staticFilterData: staticData,
             saveURL: saveURL,
