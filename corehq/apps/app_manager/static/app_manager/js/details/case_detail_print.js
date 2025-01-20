5f71c517758aec3b982dc1f59fb6eb6c20549df2
Jenny Schweers <jschweers@dimagi.com>
2025-01-20 18:20:13 -0500

    Added explicit dependencies to app_manager/js/details/case_detail_print

--- corehq/apps/app_manager/static/app_manager/js/details/case_detail_print.js
+++ corehq/apps/app_manager/static/app_manager/js/details/case_detail_print.js
@@ -1,15 +1,20 @@
-"use strict";
-hqDefine("app_manager/js/details/case_detail_print", function () {
+hqDefine("app_manager/js/details/case_detail_print", [
+    "hqwebapp/js/initial_page_data",
+    "hqmedia/js/uploaders",
+    "hqmedia/js/media_reference_models",
+], function (
+    initialPageData,
+    uploaders,
+    mediaReferenceModels,
+) {
     var printRef, printTemplateUploader;
-    var initialPageData = hqImport("hqwebapp/js/initial_page_data"),
-        printUploader = initialPageData.get("print_uploader_js"),
-        uploaders = hqImport("hqmedia/js/uploaders");
+    const printUploader = initialPageData.get("print_uploader_js");
     if (printUploader) {
         printTemplateUploader = uploaders.uploader(
             printUploader.slug,
             printUploader.options
         );
-        printRef = hqImport('hqmedia/js/media_reference_models').BaseMediaReference(initialPageData.get('print_ref'));
+        printRef = mediaReferenceModels.BaseMediaReference(initialPageData.get('print_ref'));
         printRef.upload_controller = printTemplateUploader;
         printRef.setObjReference(initialPageData.get('print_media_info'));
         printTemplateUploader.currentReference = printRef;
