0a8bf9d107539acf20560550eb4a7b9c7da4afc0
Jenny Schweers <jschweers@dimagi.com>
2025-01-21 22:35:08 -0500

    Migrated import_app.html

--- corehq/apps/app_manager/static/app_manager/js/import_app.js
+++ corehq/apps/app_manager/static/app_manager/js/import_app.js
@@ -1,4 +1,10 @@
-hqDefine('app_manager/js/import_app', ['jquery'], function ($) {
+hqDefine('app_manager/js/import_app', [
+    'jquery',
+    'app_manager/js/apps_base',     // TODO: is this necessary?
+    'commcarehq',
+], function (
+    $,
+) {
     $(function () {
         $(".historyBack").click(function () {
             history.back();
