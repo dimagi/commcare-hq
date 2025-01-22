9ca6192916b55c4e011d646924095d98a2981787
Graham Herceg <gherceg@dimagi.com>
2025-01-22 10:03:48 -0500

    Only set CheckboxInputField for BooleanFields

--- corehq/apps/domain/forms.py
+++ corehq/apps/domain/forms.py
@@ -952,7 +952,7 @@ class PrivacySecurityForm(forms.Form):
             excluded_fields.append('secure_sessions_timeout')
 
         for field in self.fields.values():
-            if not isinstance(field.widget, BootstrapCheckboxInput):
+            if isinstance(field, BooleanField):
                 field.widget = BootstrapCheckboxInput()
 
         fields = [hqcrispy.CheckboxField(field_name)
