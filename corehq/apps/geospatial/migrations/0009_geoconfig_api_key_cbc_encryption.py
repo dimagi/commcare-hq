8779e6a45ba124ce57dc28717b4c243aab640759
Jonathan Tang <Jonahtang738@gmail.com>
2025-01-22 12:40:35 -0800

    filter for only values that have been migrated, indicated by the aes-cbc prefix

--- corehq/apps/geospatial/migrations/0009_geoconfig_api_key_cbc_encryption.py
+++ corehq/apps/geospatial/migrations/0009_geoconfig_api_key_cbc_encryption.py
@@ -2,7 +2,7 @@ from django.db import migrations
 from django.db.migrations import RunPython
 
 from corehq.util.django_migrations import skip_on_fresh_install
-from corehq.apps.geospatial.const import ALGO_AES, ALGO_AES_CBC
+from corehq.motech.const import ALGO_AES, ALGO_AES_CBC
 from corehq.motech.utils import (
     reencrypt_ecb_to_cbc_mode,
     reencrypt_cbc_to_ecb_mode,
@@ -31,14 +31,13 @@ def reencrypt_api_keys(apps, schema_editor):
 def reversion_api_keys(apps, schema_editor):
     GeoConfig = apps.get_model('geospatial', 'GeoConfig')
 
-    geo_configs_to_revert = GeoConfig.objects.exclude(
-        api_token__startswith=f'${ALGO_AES}$'
-    ).exclude(api_token=None)
+    geo_configs_to_revert = GeoConfig.objects.filter(
+        api_token__startswith=f'${ALGO_AES_CBC}$'
+    )
 
     for config in geo_configs_to_revert:
-        if config.api_token.startswith(f'${ALGO_AES_CBC}$'):
-            config.api_token = reencrypt_cbc_to_ecb_mode(config.api_token,
-                                                       f'${ALGO_AES_CBC}$')
+        config.api_token = reencrypt_cbc_to_ecb_mode(config.api_token,
+                                                    f'${ALGO_AES_CBC}$')
         config.save()
 
 
