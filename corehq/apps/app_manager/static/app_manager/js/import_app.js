3e82adfbb702d1f404d86e65943b79ec592b1abd
Jenny Schweers <jschweers@dimagi.com>
2025-01-22 15:42:43 -0500

    Migrated import_app.html to webpack

--- corehq/apps/app_manager/static/app_manager/js/import_app.js
+++ corehq/apps/app_manager/static/app_manager/js/import_app.js
@@ -1,14 +1,9 @@
-hqDefine('app_manager/js/import_app', [
-    'jquery',
-    'app_manager/js/apps_base',     // TODO: is this necessary?
-    'commcarehq',
-], function (
-    $,
-) {
-    $(function () {
-        $(".historyBack").click(function () {
-            history.back();
-            return false;
-        });
+import "commcarehq";
+import $ from "jquery";
+
+$(function () {
+    $(".historyBack").click(function () {
+        history.back();
+        return false;
     });
 });
