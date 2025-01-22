769babc181baed94be0d15e9d4d1ea2606bb7b50
Jenny Schweers <jschweers@dimagi.com>
2025-01-22 17:55:32 -0500

    Migrated module settings pages (normal, advanced, and reports) to webpack

--- corehq/apps/app_manager/static/app_manager/js/modules/module_view_report.js
+++ corehq/apps/app_manager/static/app_manager/js/modules/module_view_report.js
@@ -7,6 +7,8 @@ hqDefine("app_manager/js/modules/module_view_report", [
     "reports_core/js/choice_list_utils",
     "select2/dist/js/select2.full.min",
     "app_manager/js/app_manager",
+    // TODO: support xpathValidator
+    //{% include "app_manager/partials/xpathValidator.html" %}
 ], function (
     $,
     _,
