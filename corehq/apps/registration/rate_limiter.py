7f9d3289ddbf9854fbbf2b06b2a0a9d80989cc8d
nospame <mapson.evan@gmail.com>
2025-01-29 11:39:54 -0800

    Break out functions from rate_limit_check_username_availability and use constants

--- corehq/apps/registration/rate_limiter.py
+++ corehq/apps/registration/rate_limiter.py
@@ -11,6 +11,11 @@ from corehq.util.decorators import run_only_when, silence_and_report_error
 from corehq.util.global_request import get_request
 from corehq.util.metrics import metrics_counter
 
+STATUS_SESSION_RATE_LIMITED = 'session_rate_limited'
+STATUS_IP_RATE_LIMITED = 'ip_rate_limited'
+STATUS_BAD_REQUEST = 'bad_request'
+STATUS_ACCEPTED = 'accepted'
+
 
 @run_only_when(not settings.ENTERPRISE_MODE and not settings.UNIT_TESTING)
 @silence_and_report_error("Exception raised in the check username availability rate limiter",
@@ -22,51 +27,47 @@ def rate_limit_check_username_availability():
 
     Requests without both an IP and session ID are rejected.
     """
-    _status_session_rate_limited = 'session_rate_limited'
-    _status_ip_rate_limited = 'ip_rate_limited'
-    _status_bad_request = 'bad_request'
-    _status_accepted = 'accepted'
-
-    def get_session_and_ip():
-        request = get_request()
-        if request and request.session:
-            return request.session.session_key, get_ip(request)
-        else:
-            return None
-
-    def _check_for_exceeded_rate_limits(session, ip):
-        session_window = (
-            check_username_rate_limiter_per_session
-            .get_window_of_first_exceeded_limit('session:{}'.format(session))
-        )
-        ip_window = check_username_rate_limiter_per_ip.get_window_of_first_exceeded_limit('ip:{}'.format(ip))
-
-        # ensure that no rate limit windows have been exceeded
-        # order of priority (session, ip)
-        if session_window is not None:
-            return _status_session_rate_limited, session_window
-        elif ip_window is not None:
-            return _status_ip_rate_limited, ip_window
-        else:
-            return _status_accepted, None
-
-    session_id, ip_address = get_session_and_ip()
+    session_id, ip_address = _get_session_and_ip()
 
     if ip_address and session_id:
         status, window = _check_for_exceeded_rate_limits(session_id, ip_address)
-        if status == _status_accepted:
+        if status == STATUS_ACCEPTED:
             check_username_rate_limiter_per_session.report_usage('session:{}'.format(session_id))
             check_username_rate_limiter_per_ip.report_usage('ip:{}'.format(ip_address))
-
     else:
         window = None
-        status = _status_bad_request
+        status = STATUS_BAD_REQUEST
 
     metrics_counter('commcare.registration.check_username_attempts', 1, tags={
         'status': status,
         'window': window or 'none',
     })
-    return status != _status_accepted
+    return status != STATUS_ACCEPTED
+
+
+def _get_session_and_ip():
+    request = get_request()
+    if request and request.session:
+        return request.session.session_key, get_ip(request)
+    else:
+        return None
+
+
+def _check_for_exceeded_rate_limits(session, ip):
+    session_window = (
+        check_username_rate_limiter_per_session
+        .get_window_of_first_exceeded_limit('session:{}'.format(session))
+    )
+    ip_window = check_username_rate_limiter_per_ip.get_window_of_first_exceeded_limit('ip:{}'.format(ip))
+
+    # ensure that no rate limit windows have been exceeded
+    # order of priority (session, ip)
+    if session_window is not None:
+        return STATUS_SESSION_RATE_LIMITED, session_window
+    elif ip_window is not None:
+        return STATUS_IP_RATE_LIMITED, ip_window
+    else:
+        return STATUS_ACCEPTED, None
 
 
 check_username_rate_limiter_per_ip = RateLimiter(
