2e232cf760064c7f16813f63082e1d01410722c4
Daniel Miller <dmiller@dimagi.com>
2025-02-13 12:37:29 -0500

    Refactor experiment decorator to reduce complexity

--- corehq/apps/experiments/experiment.py
+++ corehq/apps/experiments/experiment.py
@@ -4,7 +4,6 @@ from functools import wraps
 from operator import eq
 
 from attrs import asdict, define, field
-
 from dimagi.utils.logging import notify_error, notify_exception
 
 from corehq.util.metrics import metrics_histogram
@@ -54,29 +53,8 @@ class Experiment:
         def wrapper(*args, **kwargs):
             enabled = is_enabled(self.campaign, self.path)
             metrics_enabled = enabled or should_record_metrics(self.campaign, self.path)
-            old_error = new_error = None
             start = time.time()
-            if enabled is not None:
-                try:
-                    old_result = func(*args, **kwargs, **self.old_args)
-                except Exception as err:
-                    old_error = err
-                mid = time.time()
-            if enabled or enabled is None:
-                try:
-                    new_result = func(*args, **kwargs, **self.new_args)
-                except Exception as err:
-                    if old_error is None and enabled:
-                        notify_exception(None, "new code path failed in experiment",
-                                        details=self.tags)
-                    new_error = err
-                if enabled is None:
-                    # run new code path and not old
-                    mid = time.time()
-                    if new_error is None:
-                        old_result = new_result
-                    else:
-                        old_error = new_error
+            old_result, new_result, old_error, new_error, mid = self._run(enabled, func, args, kwargs)
 
             if metrics_enabled:
                 end = time.time()
@@ -96,30 +74,58 @@ class Experiment:
                 bucket_tag='duration', buckets=self.percent_buckets, bucket_unit='%',
             )
 
-            if old_error is not None:
-                if new_error is None:
-                    new_repr = repr(new_result)
-                elif type(new_error) is type(old_error) and new_error.args == old_error.args:
-                    raise old_error
-                else:
-                    new_repr = f"raised {describe(type(new_error), new_error.args, {})}"
-                old_repr = describe(type(old_error), old_error.args, {})
-                notify_error(
-                    f"{describe(func, args, kwargs)}: raised {old_repr} != {new_repr}",
-                    details=self.tags,
-                )
-                raise old_error
-            if new_error is None and not self.is_equal(old_result, new_result):
-                notify_error(
-                    f"{describe(func, args, kwargs)}: {old_result!r} != {new_result!r}",
-                    details=self.tags,
-                )
+            self._compare(old_result, new_result, old_error, new_error, func, args, kwargs)
             return old_result
 
         self._warn_on_duplicate()
         over_the_top = self.percent_buckets[-1] + 1
         wrapper.experiment = self
         return wrapper
+
+    def _run(self, enabled, func, args, kwargs):
+        old_result = new_result = old_error = new_error = None
+        if enabled is not None:
+            try:
+                old_result = func(*args, **kwargs, **self.old_args)
+            except Exception as err:
+                old_error = err
+            mid = time.time()
+        if enabled or enabled is None:
+            try:
+                new_result = func(*args, **kwargs, **self.new_args)
+            except Exception as err:
+                if old_error is None and enabled:
+                    notify_exception(None, "new code path failed in experiment",
+                                     details=self.tags)
+                new_error = err
+            if enabled is None:
+                # run new code path and not old
+                mid = time.time()
+                if new_error is None:
+                    old_result = new_result
+                else:
+                    old_error = new_error
+        return old_result, new_result, old_error, new_error, mid
+
+    def _compare(self, old_result, new_result, old_error, new_error, func, args, kwargs):
+        if old_error is not None:
+            if new_error is None:
+                new_repr = repr(new_result)
+            elif type(new_error) is type(old_error) and new_error.args == old_error.args:
+                raise old_error
+            else:
+                new_repr = f"raised {describe(type(new_error), new_error.args, {})}"
+            old_repr = describe(type(old_error), old_error.args, {})
+            notify_error(
+                f"{describe(func, args, kwargs)}: raised {old_repr} != {new_repr}",
+                details=self.tags,
+            )
+            raise old_error
+        if new_error is None and not self.is_equal(old_result, new_result):
+            notify_error(
+                f"{describe(func, args, kwargs)}: {old_result!r} != {new_result!r}",
+                details=self.tags,
+            )
 
     def _warn_on_duplicate(self):
         key = self.campaign, self.path
