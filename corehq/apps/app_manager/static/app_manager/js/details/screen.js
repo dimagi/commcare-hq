a34f69c1aab5f0d86a9c9c69c9df267590f0b34d
mkangia <mkangia@dimagi.com>
2025-02-14 00:57:34 +0530

    show case list optimizations once set to show in settings

--- corehq/apps/app_manager/static/app_manager/js/details/screen.js
+++ corehq/apps/app_manager/static/app_manager/js/details/screen.js
@@ -14,7 +14,8 @@
 hqDefine("app_manager/js/details/screen", function () {
     const Utils = hqImport('app_manager/js/details/utils'),
         ColumnModel = hqImport("app_manager/js/details/column"),
-        uiMapList = hqImport("hqwebapp/js/ui_elements/bootstrap3/ui-element-key-val-list");
+        uiMapList = hqImport("hqwebapp/js/ui_elements/bootstrap3/ui-element-key-val-list"),
+        initialPageData = hqImport('hqwebapp/js/initial_page_data');
 
     const getPropertyTitle = function (property) {
         // Strip "<prefix>:" before converting to title case.
@@ -109,7 +110,8 @@ hqDefine("app_manager/js/details/screen", function () {
         });
         self.showCaseListOptimizations = (
             self.columnKey === 'short' &&
-            hqImport('hqwebapp/js/toggles').toggleEnabled('CASE_LIST_OPTIMIZATIONS')
+            hqImport('hqwebapp/js/toggles').toggleEnabled('CASE_LIST_OPTIMIZATIONS') &&
+            initialPageData.get('show_case_list_optimization_options')
         );
         self.persistCaseContext = ko.observable(detail.persist_case_context || false);
         self.persistentCaseContextXML = ko.observable(detail.persistent_case_context_xml || 'case_name');
