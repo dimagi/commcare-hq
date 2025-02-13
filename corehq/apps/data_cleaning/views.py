59ee5d28d0f02135bd87c31540cd46a8252479a6
Biyeun Buczyk <biyeun@dimagi.com>
2025-02-13 22:57:21 +0100

    add (in progress) data cleaning views to data tab

--- corehq/apps/data_cleaning/views.py
+++ corehq/apps/data_cleaning/views.py
@@ -1,19 +1,22 @@
 from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy
 
 from corehq import toggles
 from corehq.apps.data_cleaning.tables import CleanCaseTable
 from corehq.apps.domain.decorators import LoginAndDomainMixin
-from corehq.apps.domain.views import BaseDomainView, DomainViewMixin
+from corehq.apps.domain.views import DomainViewMixin
 from corehq.apps.es import CaseSearchES
 from corehq.apps.hqwebapp.decorators import use_bootstrap5
 from corehq.apps.hqwebapp.tables.pagination import SelectablePaginatedTableView
+from corehq.apps.settings.views import BaseProjectDataView
 
 
 @method_decorator([
     use_bootstrap5,
     toggles.DATA_CLEANING_CASES.required_decorator(),
 ], name='dispatch')
-class CleanCasesMainView(BaseDomainView):
+class CleanCasesMainView(BaseProjectDataView):
+    page_title = gettext_lazy("Clean Case Data")
     urlname = "data_cleaning_cases"
     template_name = "data_cleaning/cases.html"
 
@@ -22,7 +25,10 @@ class CleanCasesMainView(BaseDomainView):
         return ""
 
 
-@method_decorator(toggles.DATA_CLEANING_CASES.required_decorator(), name='dispatch')
+@method_decorator([
+    use_bootstrap5,
+    toggles.DATA_CLEANING_CASES.required_decorator(),
+], name='dispatch')
 class CleanCasesTableView(LoginAndDomainMixin, DomainViewMixin, SelectablePaginatedTableView):
     urlname = "data_cleaning_cases_table"
     table_class = CleanCaseTable
