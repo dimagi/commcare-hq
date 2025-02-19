86d618eb846aa57763a12759cde0640eb6ccb6db
Jonathan Tang <Jonahtang738@gmail.com>
2025-02-19 10:33:23 -0800

    bug: the encrypted_secret_value field was never prefixed and is always encrypted so on the migration, it needs to be directly reencrypted

--- corehq/apps/reports/migrations/0021_tableauconnectedapp_use_aes_cbc_encryption.py
+++ corehq/apps/reports/migrations/0021_tableauconnectedapp_use_aes_cbc_encryption.py
@@ -1,12 +1,11 @@
 from django.db import migrations, models
 from django.db.migrations import RunPython
 
-from corehq.motech.const import ALGO_AES, ALGO_AES_CBC
+from corehq.motech.const import ALGO_AES_CBC
 from corehq.util.django_migrations import skip_on_fresh_install
 from corehq.motech.utils import (
     reencrypt_ecb_to_cbc_mode,
     reencrypt_cbc_to_ecb_mode,
-    b64_aes_cbc_encrypt,
 )
 
 
@@ -20,13 +19,8 @@ def migrate_tableau_connected_app_secret_value(apps, schema_editor):
 
     for connected_app in connected_apps_to_update:
         encrypted_secret_value = connected_app.encrypted_secret_value
-        if encrypted_secret_value.startswith(f'${ALGO_AES}$'):
-            connected_app.encrypted_secret_value = reencrypt_ecb_to_cbc_mode(
-                encrypted_secret_value, f'${ALGO_AES}$'
-            )
-        else:
-            ciphertext = b64_aes_cbc_encrypt(encrypted_secret_value)
-            connected_app.encrypted_secret_value = f'${ALGO_AES_CBC}${ciphertext}'
+        connected_app.encrypted_secret_value = reencrypt_ecb_to_cbc_mode(
+            encrypted_secret_value)
         connected_app.save()
 
 
