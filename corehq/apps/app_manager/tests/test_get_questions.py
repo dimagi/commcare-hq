255cd3441968df8c8c4293fe0d1c6036251735ce
Norman Hooper <nhooper@dimagi.com>
2025-02-19 20:50:04 -0500

    Refactored by GitHub Copilot using DeepSeek V3

--- corehq/apps/app_manager/tests/test_get_questions.py
+++ corehq/apps/app_manager/tests/test_get_questions.py
@@ -1,10 +1,14 @@
 import os
+import unittest
 
 from django.template.loader import render_to_string
 from django.test.testcases import SimpleTestCase
 
+from lxml import etree
+
 from corehq.apps.app_manager.models import Application, Module
 from corehq.apps.app_manager.util import generate_xmlns
+from corehq.apps.app_manager.xform import XForm
 from corehq.util.test_utils import TestFileMixin
 
 QUESTIONS = [
@@ -318,3 +322,236 @@ class GetFormQuestionsTest(SimpleTestCase, TestFileMixin):
             "type": "Select",
             "value": "/data/lookup-table"
         })
+
+
+class TestXFormGetQuestions(unittest.TestCase):
+    # Added by GitHub Copilot using DeepSeek V3
+
+    def test_text_question(self):
+        xml_content = """
+        <xf:input label="Name" ref="/data/name">
+            <xf:bind nodeset="." type="xsd:string"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'text', 'label': 'Name', 'ref': '/data/name'}]
+        self.assertEqual(questions, expected)
+
+    def test_select_question(self):
+        xml_content = """
+        <xf:select label="Gender" ref="/data/gender">
+            <xf:item>
+                <xf:label>Male</xf:label>
+                <xf:value>M</xf:value>
+            </xf:item>
+            <xf:item>
+                <xf:label>Female</xf:label>
+                <xf:value>F</xf:value>
+            </xf:item>
+        </xf:select>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{
+            'type': 'select',
+            'label': 'Gender',
+            'ref': '/data/gender',
+            'options': [
+                {'label': 'Male', 'value': 'M'},
+                {'label': 'Female', 'value': 'F'}
+            ]
+        }]
+        self.assertEqual(questions, expected)
+
+    def test_multi_select_question(self):
+        xml_content = """
+        <xf:select1 label="Interests" ref="/data/interests">
+            <xf:item>
+                <xf:label>Sports</xf:label>
+                <xf:value>sports</xf:value>
+            </xf:item>
+            <xf:item>
+                <xf:label>Music</xf:label>
+                <xf:value>music</xf:value>
+            </xf:item>
+        </xf:select1>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{
+            'type': 'multi-select',
+            'label': 'Interests',
+            'ref': '/data/interests',
+            'options': [
+                {'label': 'Sports', 'value': 'sports'},
+                {'label': 'Music', 'value': 'music'}
+            ]
+        }]
+        self.assertEqual(questions, expected)
+
+    def test_group_question(self):
+        xml_content = """
+        <xf:group label="Personal Details" ref="/data/personal">
+        </xf:group>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'group', 'label': 'Personal Details', 'ref': '/data/personal'}]
+        self.assertEqual(questions, expected)
+
+    def test_repeat_question(self):
+        xml_content = """
+        <xf:repeat label="Children" ref="/data/children">
+        </xf:repeat>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'repeat', 'label': 'Children', 'ref': '/data/children'}]
+        self.assertEqual(questions, expected)
+
+    def test_date_question(self):
+        xml_content = """
+        <xf:input label="Birthdate" ref="/data/birthdate">
+            <xf:bind nodeset="." type="xsd:date"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'date', 'label': 'Birthdate', 'ref': '/data/birthdate'}]
+        self.assertEqual(questions, expected)
+
+    def test_time_question(self):
+        xml_content = """
+        <xf:input label="Wakeup Time" ref="/data/wakeup_time">
+            <xf:bind nodeset="." type="xsd:time"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'time', 'label': 'Wakeup Time', 'ref': '/data/wakeup_time'}]
+        self.assertEqual(questions, expected)
+
+    def test_datetime_question(self):
+        xml_content = """
+        <xf:input label="Event Time" ref="/data/event_time">
+            <xf:bind nodeset="." type="xsd:dateTime"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'datetime', 'label': 'Event Time', 'ref': '/data/event_time'}]
+        self.assertEqual(questions, expected)
+
+    def test_geopoint_question(self):
+        xml_content = """
+        <xf:input label="Location" ref="/data/location">
+            <xf:bind nodeset="." type="geopoint"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'geopoint', 'label': 'Location', 'ref': '/data/location'}]
+        self.assertEqual(questions, expected)
+
+    def test_image_question(self):
+        xml_content = """
+        <xf:upload label="Profile Picture" ref="/data/profile_picture" mediatype="image/*">
+        </xf:upload>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'image', 'label': 'Profile Picture', 'ref': '/data/profile_picture'}]
+        self.assertEqual(questions, expected)
+
+    def test_audio_question(self):
+        xml_content = """
+        <xf:upload label="Voice Note" ref="/data/voice_note" mediatype="audio/*">
+        </xf:upload>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'audio', 'label': 'Voice Note', 'ref': '/data/voice_note'}]
+        self.assertEqual(questions, expected)
+
+    def test_video_question(self):
+        xml_content = """
+        <xf:upload label="Intro Video" ref="/data/intro_video" mediatype="video/*">
+        </xf:upload>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'video', 'label': 'Intro Video', 'ref': '/data/intro_video'}]
+        self.assertEqual(questions, expected)
+
+    def test_barcode_question(self):
+        xml_content = """
+        <xf:upload label="Barcode Scan" ref="/data/barcode_scan" mediatype="barcode/*">
+        </xf:upload>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'barcode', 'label': 'Barcode Scan', 'ref': '/data/barcode_scan'}]
+        self.assertEqual(questions, expected)
+
+    def test_secret_question(self):
+        xml_content = """
+        <xf:input label="Password" ref="/data/password" secret="true">
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'secret', 'label': 'Password', 'ref': '/data/password'}]
+        self.assertEqual(questions, expected)
+
+    def test_android_app_question(self):
+        xml_content = """
+        <xf:input label="App Version" ref="/data/app_version" appearance="android-app">
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'android-app', 'label': 'App Version', 'ref': '/data/app_version'}]
+        self.assertEqual(questions, expected)
+
+    def test_phone_number_question(self):
+        xml_content = """
+        <xf:input label="Phone Number" ref="/data/phone_number" appearance="phone-number">
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'phone-number', 'label': 'Phone Number', 'ref': '/data/phone_number'}]
+        self.assertEqual(questions, expected)
+
+    def test_int_question(self):
+        xml_content = """
+        <xf:input label="Age" ref="/data/age">
+            <xf:bind nodeset="." type="xsd:int"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'int', 'label': 'Age', 'ref': '/data/age'}]
+        self.assertEqual(questions, expected)
+
+    def test_decimal_question(self):
+        xml_content = """
+        <xf:input label="Weight" ref="/data/weight">
+            <xf:bind nodeset="." type="xsd:decimal"/>
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'decimal', 'label': 'Weight', 'ref': '/data/weight'}]
+        self.assertEqual(questions, expected)
+
+    def test_note_question(self):
+        xml_content = """
+        <xf:input label="Comments" ref="/data/comments" appearance="note">
+        </xf:input>
+        """
+        xform = XForm(xml_content)
+        questions = xform.get_questions()
+        expected = [{'type': 'note', 'label': 'Comments', 'ref': '/data/comments'}]
+        self.assertEqual(questions, expected)
