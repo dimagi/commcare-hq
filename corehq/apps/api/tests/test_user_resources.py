a168fae726db2e8fa0b4889994ec5ffcb78ebea0
Jonathan Tang <Jonahtang738@gmail.com>
2025-02-20 23:34:58 -0800

    test: last_modified field is now included in CommCareUser list GET response

--- corehq/apps/api/tests/test_user_resources.py
+++ corehq/apps/api/tests/test_user_resources.py
@@ -153,7 +153,8 @@ class TestCommCareUserResource(APIResourceTest):
                           'commcare_location_ids': f'{self.loc1.location_id} {self.loc2.location_id}'},
             'username': 'fake_user',
             'primary_location': self.loc2.location_id,
-            'locations': [self.loc1.location_id, self.loc2.location_id]
+            'locations': [self.loc1.location_id, self.loc2.location_id],
+            'last_modified': commcare_user.last_modified.strftime('%Y-%m-%dT%H:%M:%S.%f')
         })
 
     def test_create(self):
