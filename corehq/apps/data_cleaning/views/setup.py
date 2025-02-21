bc5fa189024cb2bec6b34167712c233105e11f70
Biyeun Buczyk <biyeun@dimagi.com>
2025-02-21 15:04:01 +0100

    add ability to resume or restart existing session, if found

--- corehq/apps/data_cleaning/views/setup.py
+++ corehq/apps/data_cleaning/views/setup.py
@@ -5,7 +5,10 @@ from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
 
 from corehq import toggles
-from corehq.apps.data_cleaning.forms.setup import SelectCaseTypeForm
+from corehq.apps.data_cleaning.forms.setup import (
+    SelectCaseTypeForm,
+    ResumeOrRestartCaseSessionForm,
+)
 from corehq.apps.data_cleaning.models import BulkEditSession
 from corehq.apps.domain.decorators import LoginAndDomainMixin
 from corehq.apps.domain.views import DomainViewMixin
@@ -20,12 +23,13 @@ from corehq.util.htmx_action import HqHtmxActionMixin, hq_hx_action
 class SetupCaseSessionFormView(HqHtmxActionMixin, LoginAndDomainMixin, DomainViewMixin, TemplateView):
     urlname = "data_cleaning_select_case_type_form"
     template_name = "data_cleaning/partials/forms/next_action_form.html"
+    container_id = "setup-case-session"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update({
             "form": kwargs.pop('form', SelectCaseTypeForm(self.domain)),
-            "container_id": "setup-case-session",
+            "container_id": self.container_id,
             "next_action": kwargs.pop('next_action', 'validate_session'),
         })
         return context
@@ -44,6 +48,34 @@ class SetupCaseSessionFormView(HqHtmxActionMixin, LoginAndDomainMixin, DomainVie
                     request.user, self.domain, case_type
                 )
                 return self.render_session_redirect(new_session)
+            form = ResumeOrRestartCaseSessionForm(
+                self.domain, self.container_id, request.path_info, {
+                    'case_type': case_type,
+                    'next_step': 'resume',
+                },
+            )
+            next_action = 'resume_or_restart'
+        return self.get(request, form=form, next_action=next_action, *args, **kwargs)
+
+    @hq_hx_action('post')
+    def resume_or_restart(self, request, *args, **kwargs):
+        form = ResumeOrRestartCaseSessionForm(
+            self.domain, self.container_id, request.path_info, request.POST
+        )
+        next_action = 'resume_or_restart'
+        if form.is_valid():
+            case_type = form.cleaned_data['case_type']
+            next_step = form.cleaned_data['next_step']
+            get_session = {
+                'resume': lambda: BulkEditSession.get_active_case_session(
+                    request.user, self.domain, case_type
+                ),
+                'new': lambda: BulkEditSession.restart_case_session(
+                    request.user, self.domain, case_type
+                ),
+            }[next_step]
+            if get_session:
+                return self.render_session_redirect(get_session())
         return self.get(request, form=form, next_action=next_action, *args, **kwargs)
 
     def render_session_redirect(self, session):
