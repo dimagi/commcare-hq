42ddb408547d8c692e998ed0f11c12d24b683f05
Ajeet <ayadav@dimagi.com>
2025-02-25 21:32:17 +0530

    test cases for kyc user class

--- corehq/apps/integration/kyc/tests/test_models.py
+++ corehq/apps/integration/kyc/tests/test_models.py
@@ -6,7 +6,7 @@ import pytest
 from corehq.apps.app_manager.const import USERCASE_TYPE
 from corehq.apps.es.case_search import case_search_adapter
 from corehq.apps.es.tests.utils import es_test
-from corehq.apps.integration.kyc.models import KycConfig, UserDataStore
+from corehq.apps.integration.kyc.models import KycConfig, UserDataStore, KycUser
 from corehq.apps.users.models import CommCareUser
 from corehq.form_processor.tests.utils import create_case
 from corehq.motech.const import OAUTH2_CLIENT
@@ -192,3 +192,117 @@ class TestGetUserObjectsCases(TestCase):
         )
         with pytest.raises(AssertionError):
             config.get_user_objects()
+
+
+class TestKycUser(TestCase):
+
+    def setUp(self):
+        self.commcare_user = CommCareUser.create(
+            DOMAIN, f'test.user@{DOMAIN}.commcarehq.org', 'Passw0rd!',
+            None, None,
+            user_data={'custom_field': 'custom_value'},
+        )
+        self.addCleanup(self.commcare_user.delete, DOMAIN, deleted_by=None)
+        self.user_case = create_case(
+            DOMAIN,
+            case_type=USERCASE_TYPE,
+            user_id=self.commcare_user._id,
+            name='test.user',
+            external_id=self.commcare_user._id,
+            save=True,
+            case_json={'user_case_property': 'user_case_value'},
+        )
+        # self.addCleanup(self.user_case.delete, DOMAIN, deleted_by=None)
+        self.other_case = create_case(
+            DOMAIN,
+            case_type='other_case_type',
+            save=True,
+            case_json={'other_case_property': 'other_case_value'},
+        )
+
+    def test_custom_user_data(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.CUSTOM_USER_DATA,
+        )
+
+        kyc_user = KycUser(config, self.commcare_user)
+
+        assert kyc_user.user_data == {
+            'custom_field': 'custom_value',
+            'commcare_profile': '',
+        }
+        assert kyc_user.user_id == self.commcare_user.user_id
+
+    def test_user_case(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.USER_CASE,
+        )
+
+        kyc_user = KycUser(config, self.commcare_user)
+
+        assert kyc_user.user_data == {'user_case_property': 'user_case_value'}
+        assert kyc_user.user_id == self.commcare_user.user_id
+
+    def test_other_case_type(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.OTHER_CASE_TYPE,
+            other_case_type='other_case_type',
+        )
+
+        kyc_user = KycUser(config, self.other_case)
+
+        assert kyc_user.user_data == {'other_case_property': 'other_case_value'}
+        assert kyc_user.user_id == self.other_case.case_id
+
+    def test_kyc_verification_initial_values(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.CUSTOM_USER_DATA,
+        )
+
+        kyc_user = KycUser(config, self.commcare_user)
+
+        assert kyc_user.kyc_is_verified is None
+        assert kyc_user.kyc_last_verified_at is None
+        assert kyc_user.kyc_provider is None
+
+    def _assert_for_verification_status(self, kyc_user, expected_status, expected_provider):
+        assert kyc_user.kyc_is_verified is expected_status
+        assert kyc_user.kyc_last_verified_at is not None
+        assert kyc_user.kyc_provider == expected_provider
+
+    def test_update_verification_status_for_custom_user_data(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.CUSTOM_USER_DATA,
+        )
+
+        kyc_user = KycUser(config, self.commcare_user)
+        kyc_user.update_verification_status(True)
+
+        self._assert_for_verification_status(kyc_user, True, config.provider)
+
+    def test_update_verification_status_for_user_case(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.USER_CASE,
+        )
+
+        kyc_user = KycUser(config, self.commcare_user)
+        kyc_user.update_verification_status(True)
+
+        self._assert_for_verification_status(kyc_user, True, config.provider)
+
+    def test_update_verification_status_for_other_case_type(self):
+        config = KycConfig(
+            domain=DOMAIN,
+            user_data_store=UserDataStore.OTHER_CASE_TYPE,
+        )
+
+        kyc_user = KycUser(config, self.other_case)
+        kyc_user.update_verification_status(True)
+
+        self._assert_for_verification_status(kyc_user, True, config.provider)
