918c24a426bf239b8cdc5a58d1ef900a2a5e47f5
nospame <mapson.evan@gmail.com>
2025-02-27 11:29:51 -0800

    Add test_uses_domain_from_dict_or_object

--- corehq/apps/export/tests/test_deid_export.py
+++ corehq/apps/export/tests/test_deid_export.py
@@ -1,3 +1,5 @@
+from types import SimpleNamespace
+
 from django.test import TestCase
 
 from corehq.apps.export.models import DeIdHash
@@ -24,3 +26,13 @@ class TestDeIdHash(TestCase):
 
         deid = DeIdHash.get_deid(value, {'domain': 'test-domain'})
         self.assertIsNotNone(deid)
+
+    def test_uses_domain_from_dict_or_object(self):
+        value = 'somedatavalue'
+        domain = 'test-domain'
+        doc_dict = {'domain': domain}
+        doc_obj = SimpleNamespace(domain=domain)
+
+        deid_with_dict = DeIdHash.get_deid(value, doc_dict)
+        deid_with_obj = DeIdHash.get_deid(value, doc_obj)
+        self.assertEqual(deid_with_dict, deid_with_obj)
