85368a34ff3304b17cc4317a5db5c8913c45030e
Biyeun Buczyk <biyeun@dimagi.com>
2025-02-27 21:25:01 +0100

    export fixSelect2htmx utility for use in other modules

--- corehq/apps/hqwebapp/static/hqwebapp/js/alpinejs/directives/select2.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/alpinejs/directives/select2.js
@@ -2,6 +2,24 @@ import $ from 'jquery';
 import 'select2/dist/js/select2.full.min';
 import Alpine from 'alpinejs';
 
+const fixSelect2htmx = (event, selector) => {
+    const elems = event.target.querySelectorAll(selector);
+    if (elems.length) {
+        elems.forEach((el) => {
+            const activeSelect2 = $(el).data('select2');
+            if (activeSelect2) {
+                $(el).addClass('select2-hidden-accessible');
+                const validationClasses = ['is-valid', 'is-invalid'];
+                validationClasses.forEach((validationClass) => {
+                    if ($(el).hasClass(validationClass)) {
+                        activeSelect2.$container.addClass(validationClass);
+                    }
+                });
+            }
+        });
+    }
+};
+
 const select2Cleanup = (el) => {
     if ($(el).data('select2')) {
         $(el).select2('destroy');
@@ -47,20 +65,10 @@ document.body.addEventListener('htmx:afterSettle', (event) => {
      * Without this fix, you will get the **visible** <select> stacked on top of the select2, and no
      * validation classes are passed to the select2.
      */
-    event.target.querySelectorAll('[x-select2]').forEach((el) => {
-        const activeSelect2 = $(el).data('select2');
-        if (activeSelect2) {
-            $(el).addClass('select2-hidden-accessible');
-            const validationClasses = ['is-valid', 'is-invalid'];
-            validationClasses.forEach((validationClass) => {
-                if ($(el).hasClass(validationClass)) {
-                    activeSelect2.$container.addClass(validationClass);
-                }
-            });
-        }
-    });
+    fixSelect2htmx(event, '[x-select2]');
 });
 
 export default {
     select2Cleanup,
+    fixSelect2htmx,
 };
