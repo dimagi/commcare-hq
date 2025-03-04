3f15a67b4de464f4ea1c8a5199fdea886024493c
Ajeet <ayadav@dimagi.com>
2025-03-04 13:28:45 +0530

    adds view for the table

--- corehq/apps/integration/payments/urls.py
+++ corehq/apps/integration/payments/urls.py
@@ -1,10 +1,11 @@
 from django.urls import re_path as url
 
 from corehq.apps.integration.payments.views import (
-    PaymentsVerificationReportView,
+    PaymentsVerificationReportView, PaymentsVerificationTableView,
 )
 
 
 urlpatterns = [
     url(r'^verify/$', PaymentsVerificationReportView.as_view(), name=PaymentsVerificationReportView.urlname),
+    url(r'^verify/table/$', PaymentsVerificationTableView.as_view(), name=PaymentsVerificationTableView.urlname),
 ]
--- corehq/apps/integration/payments/views.py
+++ corehq/apps/integration/payments/views.py
@@ -3,8 +3,14 @@ from django.utils.decorators import method_decorator
 from django.utils.translation import gettext as _
 
 from corehq import toggles
+from corehq.apps.case_importer.const import MOMO_PAYMENT_CASE_TYPE
+from corehq.apps.domain.decorators import login_required
 from corehq.apps.domain.views.base import BaseDomainView
+from corehq.apps.es import CaseSearchES
 from corehq.apps.hqwebapp.decorators import use_bootstrap5
+from corehq.apps.hqwebapp.tables.pagination import SelectablePaginatedTableView
+from corehq.apps.integration.payments.tables import PaymentsVerifyTable
+from corehq.util.htmx_action import HqHtmxActionMixin
 
 
 @method_decorator(use_bootstrap5, name='dispatch')
@@ -18,3 +24,13 @@ class PaymentsVerificationReportView(BaseDomainView):
     @property
     def section_url(self):
         return reverse(self.urlname, args=(self.domain,))
+
+
+@method_decorator(login_required, name='dispatch')
+@method_decorator(toggles.MTN_MOBILE_WORKER_VERIFICATION.required_decorator(), name='dispatch')
+class PaymentsVerificationTableView(HqHtmxActionMixin, SelectablePaginatedTableView):
+    urlname = 'payments_verify_table'
+    table_class = PaymentsVerifyTable
+
+    def get_queryset(self):
+        return CaseSearchES().domain(self.request.domain).case_type(MOMO_PAYMENT_CASE_TYPE)
