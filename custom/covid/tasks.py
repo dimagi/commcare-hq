67c16aec5a5d3ea767de13b2166e011a7c8ba24e
Jonathan Tang <Jonahtang738@gmail.com>
2025-03-04 13:38:39 -0800

    this function was moved to a different file but the original function wasn't deleted

--- custom/covid/tasks.py
+++ custom/covid/tasks.py
@@ -1,22 +1,15 @@
 from datetime import datetime
 
-from celery.exceptions import MaxRetriesExceededError
 from celery.schedules import crontab
 from dateutil.relativedelta import relativedelta
 
-from dimagi.utils.logging import notify_exception
-
 from corehq.apps.celery import periodic_task
-from corehq.apps.formplayer_api.clear_user_data import clear_user_data
-from corehq.apps.formplayer_api.exceptions import FormplayerAPIException
 
 from corehq.toggles import PRIME_FORMPLAYER_DBS
-from corehq.util.celery_utils import no_result_task
 
 from custom.formplayer.restore_priming import (
     prime_formplayer_db_for_user,
     get_users_for_priming,
-    get_prime_restore_user_params,
 )
 
 
@@ -42,23 +35,3 @@ def prime_formplayer_dbs():
         users = get_users_for_priming(domain, date_window, date_cutoff, MIN_CASE_COUNT)
         for row in users:
             prime_formplayer_db_for_user.delay(domain, row[0], row[1], task_cutoff_hour=TASK_WINDOW_CUTOFF_HOUR)
-
-
-@no_result_task(queue='ush_background_tasks', max_retries=3, bind=True)
-def clear_formplayer_db_for_user(self, domain, request_user_id, sync_user_id):
-    request_user, as_username = get_prime_restore_user_params(request_user_id, sync_user_id)
-
-    try:
-        clear_user_data(domain, request_user, as_username)
-    except FormplayerAPIException:
-        notify_exception(None, "Error while clearing formplayer user DB", details={
-            'domain': domain,
-            'username': request_user,
-            'as_user': as_username,
-        })
-    except Exception as e:
-        # most likely an error contacting formplayer, try again
-        try:
-            raise self.retry(exc=e)
-        except MaxRetriesExceededError:
-            raise
