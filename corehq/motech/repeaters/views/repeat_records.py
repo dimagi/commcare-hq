e9bed3abbab25a546f0072f765473bb423aa91b7
Graham Herceg <gherceg@dimagi.com>
2025-03-03 21:54:26 -0500

    Require superuser access to resend records
    In addition to adding confusion to this page (resend vs requeue),
    resending gives users the ability to "skip the line" for data
    forwarding. Practically, requeueing gives a typical user everything they
    need to retry records. In a pinch, it is nice to skip the line and
    resend records immediately, but this should only be given to superusers.

--- corehq/motech/repeaters/views/repeat_records.py
+++ corehq/motech/repeaters/views/repeat_records.py
@@ -27,7 +27,7 @@ from corehq.apps.data_interfaces.tasks import (
     task_generate_ids_and_operate_on_payloads,
     task_operate_on_payloads,
 )
-from corehq.apps.domain.decorators import domain_admin_required
+from corehq.apps.domain.decorators import domain_admin_required, require_superuser
 from corehq.apps.hqwebapp.templatetags.hq_shared_tags import static
 from corehq.apps.reports.datatables import DataTablesColumn, DataTablesHeader
 from corehq.apps.reports.dispatcher import DomainReportDispatcher
@@ -225,6 +225,7 @@ class DomainForwardingRepeatRecords(GenericTabularReport):
             payload_id=self.payload_id,
             repeater_id=self.repeater_id,
             state=self.state.label.upper() if self.state else 'ALL',
+            is_superuser=self.request.user.is_superuser,
         )
         return context
 
@@ -296,6 +297,7 @@ class RepeatRecordView(View):
             'content_type': content_type,
         })
 
+    @method_decorator(require_superuser)
     def post(self, request, domain):
         # Retriggers a repeat record
         if _get_state(request):
