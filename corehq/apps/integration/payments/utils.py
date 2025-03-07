eafaa115f04eef4684a64ae0170e23c493eeb107
Charl Smit <csmit@dimagi.com>
2025-03-07 15:26:43 +0200

    Return if no case_ids

--- corehq/apps/integration/payments/utils.py
+++ corehq/apps/integration/payments/utils.py
@@ -5,6 +5,9 @@ from corehq.apps.hqcase.api.updates import handle_case_update
 
 
 def verify_payment_cases(domain, case_ids: list, verifying_user: WebUser) -> tuple[int, int]:
+    if not case_ids:
+        return 0, 0
+
     payment_properties_update = {
         'momo_payment_verified': str(True),
         'momo_payment_verified_on_utc': str(datetime.now()),
