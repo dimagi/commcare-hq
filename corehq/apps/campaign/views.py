e549f81ba3c4ba568957402983feb21f50a5cba1
mkangia <mkangia@dimagi.com>
2025-03-09 17:59:45 +0530

    limit access to new feature flag

--- corehq/apps/campaign/views.py
+++ corehq/apps/campaign/views.py
@@ -2,12 +2,15 @@ from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy
 
+from corehq import toggles
+
 from corehq.apps.domain.decorators import login_and_domain_required
 from corehq.apps.hqwebapp.decorators import use_bootstrap5
 from corehq.apps.reports.views import BaseProjectReportSectionView
 
 
 @method_decorator(login_and_domain_required, name='dispatch')
+@method_decorator(toggles.CAMPAIGN_DASHBOARD.required_decorator(), name='dispatch')
 @method_decorator(use_bootstrap5, name='dispatch')
 class DashboardView(BaseProjectReportSectionView):
     urlname = 'campaign_dashboard'
