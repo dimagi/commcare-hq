ce7549c85f3a59f9e5e2ffdf7f0d62d37d4c12b8
mkangia <mkangia@dimagi.com>
2025-03-09 16:22:55 +0530

    change other references

--- corehq/apps/campaign/apps.py
+++ corehq/apps/campaign/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CampDashConfig(AppConfig):
+class CampaignConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'corehq.apps.campaign'
