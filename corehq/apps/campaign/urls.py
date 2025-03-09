e74a8f6964765476f413e378635ce8a9b1bc0269
mkangia <mkangia@dimagi.com>
2025-03-09 17:40:39 +0530

    add campaign dashboard view & template

--- corehq/apps/campaign/urls.py
+++ corehq/apps/campaign/urls.py
@@ -1,1 +1,7 @@
-urlpatterns = []
+from django.urls import re_path as url
+
+from corehq.apps.campaign.views import DashboardView
+
+urlpatterns = [
+    url(r'dashboard/', DashboardView.as_view(), name=DashboardView.urlname),
+]
--- corehq/apps/campaign/views.py
+++ corehq/apps/campaign/views.py
@@ -1,1 +1,19 @@
-# Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.utils.translation import gettext_lazy
+
+from corehq.apps.domain.decorators import login_and_domain_required
+from corehq.apps.hqwebapp.decorators import use_bootstrap5
+from corehq.apps.reports.views import BaseProjectReportSectionView
+
+
+@method_decorator(login_and_domain_required, name='dispatch')
+@method_decorator(use_bootstrap5, name='dispatch')
+class DashboardView(BaseProjectReportSectionView):
+    urlname = 'campaign_dashboard'
+    page_title = gettext_lazy("Campaign Dashboard")
+    template_name = 'campaign/dashboard.html'
+
+    @property
+    def page_url(self):
+        return reverse(self.urlname, args=[self.domain])
