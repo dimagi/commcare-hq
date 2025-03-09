bd38f2fd458b535a1b9cf4f2bbf26ba5d22b77d8
mkangia <mkangia@dimagi.com>
2025-03-09 16:19:51 +0530

    rename app to campaign

--- corehq/apps/campaign/apps.py
+++ corehq/apps/campaign/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class CampDashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'corehq.apps.campdash'
+    name = 'corehq.apps.campaign'
