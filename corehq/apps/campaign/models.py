26e6c496af6efe153f46af9f6c1dcd9fe5ffe9d2
Norman Hooper <nhooper@dimagi.com>
2025-03-10 22:30:55 +0000

    Add DashboardMap and DashboardReport models

--- corehq/apps/campaign/models.py
+++ corehq/apps/campaign/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from corehq.apps.userreports.models import ReportConfiguration
+
 
 class Dashboard(models.Model):
     """
@@ -9,3 +11,42 @@ class Dashboard(models.Model):
 
     class Meta:
         app_label = 'campaign'
+
+
+class DashboardMap(models.Model):
+    """
+    Configuration for a map in a campaign dashboard
+    """
+    dashboard = models.ForeignKey(
+        Dashboard,
+        on_delete=models.CASCADE,
+        related_name='maps'
+    )
+    display_order = models.IntegerField(default=0)
+    case_type = models.CharField(max_length=255)
+    geo_case_property = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'campaign'
+        ordering = ['display_order']
+
+
+class DashboardReport(models.Model):
+    """
+    Configuration for a report in a campaign dashboard
+    """
+    dashboard = models.ForeignKey(
+        Dashboard,
+        on_delete=models.CASCADE,
+        related_name='reports'
+    )
+    display_order = models.IntegerField(default=0)
+    report_configuration_id = models.CharField(max_length=36)
+
+    class Meta:
+        app_label = 'campaign'
+        ordering = ['display_order']
+
+    @property
+    def report_configuration(self):
+        return ReportConfiguration.get(self.report_configuration_id)
