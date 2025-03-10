101fae653dfad98a346aa5d52f0648c44ee7601a
Zandre Engelbrecht <zengelbrecht@dimagi.com>
2025-03-10 11:43:59 +0200

    urls

--- corehq/apps/campdash/urls.py
+++ corehq/apps/campdash/urls.py
@@ -1,11 +1,19 @@
 from django.urls import re_path as url
 
-from corehq.apps.campdash.views import CampaignDashboardView
+from corehq.apps.campdash.views import (
+    CampaignDashboardView,
+    MapReportView,
+)
 
 urlpatterns = [
     url(
         r'^$',
         CampaignDashboardView.as_view(),
         name=CampaignDashboardView.urlname,
+    ),
+    url(
+        r'^map_report/(?P<subreport_slug>[\w-]+)/$',
+        MapReportView.as_view(),
+        name='campaign_map_report',
     ),
 ]
