8f7c509f023edec0ab6210c0708f5a5225743b5a
Norman Hooper <nhooper@dimagi.com>
2025-03-10 22:52:43 +0000

    --wip-- UI [skip ci]

--- corehq/apps/campaign/urls.py
+++ corehq/apps/campaign/urls.py
@@ -1,7 +1,12 @@
 from django.urls import re_path as url
 
-from corehq.apps.campaign.views import DashboardView
+from corehq.apps.campaign.views import DashboardView, MapReportView
 
 urlpatterns = [
     url(r'dashboard/', DashboardView.as_view(), name=DashboardView.urlname),
+    url(
+        r'^map_report/(?P<subreport_slug>[\w-]+)/$',
+        MapReportView.as_view(),
+        name='campaign_map_report',
+    ),
 ]
--- corehq/apps/campaign/views.py
+++ corehq/apps/campaign/views.py
@@ -3,10 +3,10 @@ from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy
 
 from corehq import toggles
-
 from corehq.apps.domain.decorators import login_and_domain_required
 from corehq.apps.hqwebapp.decorators import use_bootstrap5
 from corehq.apps.reports.views import BaseProjectReportSectionView
+from corehq.apps.userreports.reports.view import ConfigurableReportView
 
 
 @method_decorator(login_and_domain_required, name='dispatch')
@@ -20,3 +20,12 @@ class DashboardView(BaseProjectReportSectionView):
     @property
     def page_url(self):
         return reverse(self.urlname, args=[self.domain])
+
+
+@method_decorator(use_bootstrap5, name='dispatch')
+class MapReportView(ConfigurableReportView):
+    template_name = 'campaign/partials/report.html'  # 'campaign/partials/base_report.html'
+    slug = 'configurable'
+    prefix = slug
+
+    is_exportable = False
