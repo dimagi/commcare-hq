19dfa62d7a6fcb7c97d70ad2e50f52966d9ac749
Norman Hooper <nhooper@dimagi.com>
2025-03-10 16:56:53 +0000

    A little clean-up

--- corehq/motech/repeaters/views/repeat_record_display.py
+++ corehq/motech/repeaters/views/repeat_record_display.py
@@ -55,7 +55,8 @@ class RepeatRecordDisplay:
 
     @property
     def state(self):
-        return format_html('<span class="label label-{}">{}</span>', *_get_state_tuple(self.record))
+        label_cls, label_text = _get_state_tuple(self.record)
+        return format_html(f'<span class="label label-{label_cls}">{label_text}</span>')
 
     def _format_date(self, date):
         if not date:
@@ -64,26 +65,12 @@ class RepeatRecordDisplay:
 
 
 def _get_state_tuple(record):
-    if record.state == State.Success:
-        label_cls = 'success'
-        label_text = _('Success')
-    elif record.state == State.Pending:
-        label_cls = 'warning'
-        label_text = _('Pending')
-    elif record.state == State.Cancelled:
-        label_cls = 'danger'
-        label_text = _('Cancelled')
-    elif record.state == State.Fail:
-        label_cls = 'danger'
-        label_text = _('Failed')
-    elif record.state == State.Empty:
-        label_cls = 'success'
-        label_text = _('Empty')
-    elif record.state == State.InvalidPayload:
-        label_cls = 'danger'
-        label_text = _('Invalid Payload')
-    else:
-        label_cls = ''
-        label_text = ''
-
-    return label_cls, label_text
+    state_map = {
+        State.Success: ('success', _('Success')),
+        State.Pending: ('warning', _('Pending')),
+        State.Cancelled: ('danger', _('Cancelled')),
+        State.Fail: ('danger', _('Failed')),
+        State.Empty: ('success', _('Empty')),
+        State.InvalidPayload: ('danger', _('Invalid payload')),
+    }
+    return state_map.get(record.state, ('', ''))
