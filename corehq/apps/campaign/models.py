cb028fa077ec7c15da9166b08b9bb9d6513c8c32
mkangia <mkangia@dimagi.com>
2025-03-11 01:04:57 +0530

    add bare initial gauge model

--- corehq/apps/campaign/models.py
+++ corehq/apps/campaign/models.py
@@ -1,1 +1,15 @@
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy
+
+
+class Gauge(models.Model):
+    CASE = 'case'
+    MOBILE_WORKER = 'mobile_worker'
+
+    TYPE_CHOICES = (
+        (CASE, gettext_lazy('Case')),
+        (MOBILE_WORKER, gettext_lazy('Mobile Worker')),
+    )
+
+    type = models.CharField(null=False, blank=False, db_index=True, choices=TYPE_CHOICES)
+    case_type = models.CharField(null=True)
