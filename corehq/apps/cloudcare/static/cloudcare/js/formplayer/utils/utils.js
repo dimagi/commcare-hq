bcb306d9d5141f833a5feda775c05436dfa11b02
Jonathan Tang <Jonahtang738@gmail.com>
2025-03-12 18:47:58 -0400

    delete print

--- corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/utils.js
+++ corehq/apps/cloudcare/static/cloudcare/js/formplayer/utils/utils.js
@@ -459,7 +459,6 @@ hqDefine("cloudcare/js/formplayer/utils/utils", [
         }
 
         if (toggles.toggleEnabled('USH_DISABLE_INTERVAL_SYNC')) {
-            console.log("interval sync is disabled");
             return;
         }
         const useAggressiveSyncTiming = (customProperties[constants.POST_FORM_SYNC] === "yes");
