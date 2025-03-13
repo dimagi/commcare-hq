7fabb1508caa978df445f4487cb4e12aaa7e6128
Biyeun Buczyk <biyeun@dimagi.com>
2025-03-13 11:45:59 +0100

    add default translations to datepicker localization

--- corehq/apps/hqwebapp/static/hqwebapp/js/alpinejs/directives/datepicker.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/alpinejs/directives/datepicker.js
@@ -1,6 +1,37 @@
 import { TempusDominus } from '@eonasdan/tempus-dominus';
 import '@eonasdan/tempus-dominus/dist/css/tempus-dominus.css';
 import Alpine from 'alpinejs';
+import _ from 'underscore';
+
+const defaultTranslations = {
+    clear: gettext('Clear selection'),
+    close: gettext('Close the picker'),
+    dayViewHeaderFormat: { month: gettext('long'), year: gettext('2-digit') },
+    decrementHour: gettext('Decrement Hour'),
+    decrementMinute: gettext('Decrement Minute'),
+    decrementSecond: gettext('Decrement Second'),
+    incrementHour: gettext('Increment Hour'),
+    incrementMinute: gettext('Increment Minute'),
+    incrementSecond: gettext('Increment Second'),
+    nextCentury: gettext('Next Century'),
+    nextDecade: gettext('Next Decade'),
+    nextMonth: gettext('Next Month'),
+    nextYear: gettext('Next Year'),
+    pickHour: gettext('Pick Hour'),
+    pickMinute: gettext('Pick Minute'),
+    pickSecond: gettext('Pick Second'),
+    previousCentury: gettext('Previous Century'),
+    previousDecade: gettext('Previous Decade'),
+    previousMonth: gettext('Previous Month'),
+    previousYear: gettext('Previous Year'),
+    selectDate: gettext('Select Date'),
+    selectDecade: gettext('Select Decade'),
+    selectMonth: gettext('Select Month'),
+    selectTime: gettext('Select Time'),
+    selectYear: gettext('Select Year'),
+    today: gettext('Go to today'),
+    toggleMeridiem: gettext('Toggle Meridiem'),
+};
 
 Alpine.directive('datepicker', (el, { expression }, { cleanup }) => {
     /**
@@ -44,7 +75,7 @@ Alpine.directive('datepicker', (el, { expression }, { cleanup }) => {
             theme: 'light',
             components: components,
         },
-        localization: localization,
+        localization: _.extend(defaultTranslations, localization),
     });
 
     cleanup(() => {
