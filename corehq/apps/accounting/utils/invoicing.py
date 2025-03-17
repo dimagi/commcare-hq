2c1cef2c670d83460873f2074fe69278cc478d12
nospame <mapson.evan@gmail.com>
2025-03-17 08:24:17 -0700

    Refactor invoicing utils that get overdue invoices to use more generic helpers
    This is to allow the helpers to be used to also get invoices that are unpaid, but not yet due

--- corehq/apps/accounting/utils/invoicing.py
+++ corehq/apps/accounting/utils/invoicing.py
@@ -26,7 +26,12 @@ def _get_unpaid_saas_invoices_in_downgrade_daterange(today):
 
 
 def get_oldest_overdue_invoice_over_threshold(today, domain):
-    for overdue_invoice in _get_unpaid_saas_invoices_in_downgrade_daterange(today).filter(
+    invoices = _get_unpaid_saas_invoices_in_downgrade_daterange(today)
+    return _get_oldest_invoice_over_threshold(domain, invoices)
+
+
+def _get_oldest_invoice_over_threshold(domain, invoices):
+    for overdue_invoice in invoices.filter(
         subscription__subscriber__domain=domain
     ):
         total_overdue_by_domain_and_invoice_date = _get_all_unpaid_saas_invoices().filter(
@@ -40,38 +45,48 @@ def get_oldest_overdue_invoice_over_threshold(today, domain):
 
 
 def get_domains_with_subscription_invoices_overdue(today):
-    for domain in set(_get_unpaid_saas_invoices_in_downgrade_daterange(today).values_list(
+    invoices = _get_unpaid_saas_invoices_in_downgrade_daterange(today)
+    return _get_domains_over_threshold(invoices, today, get_oldest_overdue_invoice_over_threshold)
+
+
+def _get_domains_over_threshold(invoices, today, get_oldest_invoice_fn):
+    for domain in set(invoices.values_list(
         'subscription__subscriber__domain', flat=True
     )):
-        overdue_invoice, total_overdue_to_date = get_oldest_overdue_invoice_over_threshold(today, domain)
+        overdue_invoice, total_overdue_to_date = get_oldest_invoice_fn(today, domain)
         if overdue_invoice:
             yield domain, overdue_invoice, total_overdue_to_date
 
 
 def get_accounts_with_customer_invoices_overdue(today):
+    date_start = today - datetime.timedelta(days=61)
+    date_end = today - datetime.timedelta(days=1)
+    return _get_accounts_over_threshold(date_start, date_end)
+
+
+def _get_accounts_over_threshold(date_start, date_end):
     unpaid_customer_invoices = CustomerInvoice.objects.filter(
         is_hidden=False,
-        date_paid__isnull=True
+        date_paid__isnull=True,
     )
-
-    overdue_customer_invoices_in_downgrade_daterange = unpaid_customer_invoices.filter(
-        date_due__lte=today - datetime.timedelta(days=1),
-        date_due__gte=today - datetime.timedelta(days=61)
+    unpaid_customer_invoices_in_daterange = unpaid_customer_invoices.objects.filter(
+        date_due__gte=date_start,
+        date_due__lte=date_end,
     ).order_by('date_due').select_related('account')
 
     accounts = set()
-    for overdue_invoice in overdue_customer_invoices_in_downgrade_daterange:
-        account = overdue_invoice.account.name
-        plan = overdue_invoice.subscriptions.first().plan_version
+    for unpaid_invoice in unpaid_customer_invoices_in_daterange:
+        account = unpaid_invoice.account.name
+        plan = unpaid_invoice.subscriptions.first().plan_version
         if (account, plan) not in accounts:
             invoices = unpaid_customer_invoices.filter(
-                Q(date_due__lte=overdue_invoice.date_due)
-                | (Q(date_due__isnull=True) & Q(date_end__lte=overdue_invoice.date_end)),
+                Q(date_due__lte=unpaid_invoice.date_due)
+                | (Q(date_due__isnull=True) & Q(date_end__lte=unpaid_invoice.date_end)),
                 account__name=account
             )
             invoices = [invoice for invoice in invoices if invoice.subscriptions.first().plan_version == plan]
-            total_overdue_to_date = sum(invoice.balance for invoice in invoices)
+            total_unpaid_to_date = sum(invoice.balance for invoice in invoices)
 
-            if total_overdue_to_date >= UNPAID_INVOICE_THRESHOLD:
+            if total_unpaid_to_date >= UNPAID_INVOICE_THRESHOLD:
                 accounts.add((account, plan))
-                yield overdue_invoice, total_overdue_to_date
+                yield unpaid_invoice, total_unpaid_to_date
