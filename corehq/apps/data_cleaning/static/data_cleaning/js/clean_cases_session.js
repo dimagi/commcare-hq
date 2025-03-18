0d509a2f03178b2a404ce7f8a313b87da7485f7d
Biyeun Buczyk <biyeun@dimagi.com>
2025-03-18 13:52:38 +0100

    add alpine directive for tooltips ...and add tooltip to icon explaining data type

--- corehq/apps/data_cleaning/static/data_cleaning/js/clean_cases_session.js
+++ corehq/apps/data_cleaning/static/data_cleaning/js/clean_cases_session.js
@@ -3,6 +3,7 @@ import 'hqwebapp/js/htmx_base';
 import 'hqwebapp/js/alpinejs/directives/select2';
 import 'hqwebapp/js/alpinejs/directives/report_select2';
 import 'hqwebapp/js/alpinejs/directives/datepicker';
+import 'hqwebapp/js/alpinejs/directives/tooltip';
 import 'data_cleaning/js/directives/dynamic_options_select2';
 
 import Alpine from 'alpinejs';
