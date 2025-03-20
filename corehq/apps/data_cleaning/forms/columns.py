50c0b05ce2e8086a9aa04fb71fce69e1f5588029
Biyeun Buczyk <biyeun@dimagi.com>
2025-03-20 10:44:51 +0100

    auto-populate label with default value from case property details

--- corehq/apps/data_cleaning/forms/columns.py
+++ corehq/apps/data_cleaning/forms/columns.py
@@ -43,6 +43,10 @@ class AddColumnForm(forms.Form):
         is_initial_editable = (
             property_details[initial_prop_id]['is_editable'] if initial_prop_id else True
         )
+        default_label = (
+            property_details[initial_prop_id]['label'] if initial_prop_id else ""
+        )
+        initial_label = self.data.get('column_label', default_label)
 
         offcanvas_selector = "#offcanvas-configure-columns"
 
@@ -51,6 +55,7 @@ class AddColumnForm(forms.Form):
             "dataType": self.data.get(
                 'data_type', DataType.CASE_CHOICES[0][0]
             ),
+            'label': initial_label,
             "casePropertyDetails": property_details,
             "isEditable": is_initial_editable,
         }
@@ -68,11 +73,15 @@ class AddColumnForm(forms.Form):
                     **({
                         "@select2change": "propId = $event.detail; "
                                           "dataType = casePropertyDetails[$event.detail].data_type; "
+                                          "label = casePropertyDetails[$event.detail].label; "
                                           "isEditable = casePropertyDetails[$event.detail].is_editable;",
                     })
                 ),
                 crispy.Div(
-                    'column_label',
+                    crispy.Field(
+                        'column_label',
+                        x_model="label",
+                    ),
                     crispy.Div(
                         crispy.Field(
                             'column_data_type',
