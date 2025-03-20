14bd0286832f5c89ec5d07c5191c261a548c9501
Jonathan Tang <Jonahtang738@gmail.com>
2025-03-20 15:15:33 -0700

    handle exception where couch user is not found in elasticsearch
    These can be skipped so no update needs to occur if the user does not already exist in ES

--- corehq/ex-submodules/pillowtop/management/commands/sync_es_webusers.py
+++ corehq/ex-submodules/pillowtop/management/commands/sync_es_webusers.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from corehq.dbaccessors.couchapps.all_docs import get_all_docs_with_doc_types
 from corehq.apps.users.models import CouchUser
 from corehq.apps.users.signals import update_user_in_es
+from elasticsearch5.exceptions import NotFoundError
 
 
 class Command(BaseCommand):
@@ -11,4 +12,9 @@ class Command(BaseCommand):
         web_users_docs = get_all_docs_with_doc_types(db=CouchUser.get_db(), doc_types=["WebUser"])
 
         for user_doc in web_users_docs:
-            update_user_in_es(None, CouchUser.wrap_correctly(user_doc))
+            try:
+                update_user_in_es(None, CouchUser.wrap_correctly(user_doc))
+            except NotFoundError as e:
+                self.stdout.write(self.style.WARNING(
+                    f"User not found in Elasticsearch: {user_doc.get('_id')} - {str(e)}"
+                ))
