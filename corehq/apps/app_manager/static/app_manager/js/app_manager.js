7fd76d8f32a193ef90f0a0706504cd96e2a2ff2a
Jenny Schweers <jschweers@dimagi.com>
2025-03-20 09:35:56 -0400

    Folded managed_app.js into app_manager.js

--- corehq/apps/app_manager/static/app_manager/js/app_manager.js
+++ corehq/apps/app_manager/static/app_manager/js/app_manager.js
@@ -6,6 +6,7 @@ hqDefine('app_manager/js/app_manager', [
     'hqwebapp/js/initial_page_data',
     'hqwebapp/js/layout',
     'hqwebapp/js/toggles',
+    'hqwebapp/js/ui_elements/ui-element-langcode-button',
     'analytix/js/google',
     'analytix/js/kissmetrix',
     'hqwebapp/js/bootstrap3/alert_user',
@@ -20,6 +21,7 @@ hqDefine('app_manager/js/app_manager', [
     initialPageData,
     hqLayout,
     toggles,
+    uiElementLangcodeButton,
     google,
     kissmetrix,
     alertUser,
@@ -465,6 +467,57 @@ hqDefine('app_manager/js/app_manager', [
     };
 
     $(function () {
+        const app = initialPageData.get('app_subset');
+        module.init({
+            appVersion: app.version || -1,
+            commcareVersion: String(app.commcare_minor_release),
+            latestCommcareVersion: initialPageData.get('latest_commcare_version') || null,
+        });
+
+        $('.btn-langcode-preprocessed').each(function () {
+            uiElementLangcodeButton.new($(this), $(this).text());
+            if ($(this).hasClass('langcode-input')) {
+                var $langcodeInput = $(this).parent().find("input");
+                var that = this;
+                if ($langcodeInput) {
+                    $langcodeInput.change(function () {
+                        if ($(this).val() === "") {
+                            $(that).show();
+                        } else {
+                            $(that).hide();
+                        }
+                    });
+                }
+            }
+        });
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        // https://github.com/twitter/bootstrap/issues/6122
+        // this is necessary to get popovers to be able to extend
+        // outside the borders of their containing div
+        //
+        // http://manage.dimagi.com/default.asp?183618
+        // Firefox 40 considers hovering on a select a mouseleave event and thus kills the select
+        // dropdown. The focus and blur events are to ensure that we do not trigger overflow hidden
+        // if we are in a select
+        var inSelectElement = false,
+            $tabContent = $('.tab-content');
+        $tabContent.css('overflow', 'visible');
+        $tabContent.on('mouseenter', '.collapse', function () {
+            $(this).css('overflow','visible');
+        });
+        $tabContent.on('mouseleave', '.collapse', function () {
+            if (inSelectElement) { return; }
+            $(this).css('overflow','hidden');
+        });
+        $tabContent.on('focus', '.collapse', function () {
+            inSelectElement = true;
+        });
+        $tabContent.on('blur', '.collapse', function () {
+            inSelectElement = false;
+        });
+
         // Handling for popup displayed when accessing a deleted app
         $('#deleted-app-modal').modal({
             backdrop: 'static',
