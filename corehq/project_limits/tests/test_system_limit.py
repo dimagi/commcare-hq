861fc25547fcdb13b146f91ad23f99a4e3e05a64
Graham Herceg <gherceg@dimagi.com>
2025-03-20 13:22:34 -0400

    Add validation to prevent global to domain scope change

--- corehq/project_limits/tests/test_system_limit.py
+++ corehq/project_limits/tests/test_system_limit.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
+from corehq.project_limits.exceptions import SystemLimitIllegalScopeChange
 from corehq.project_limits.models import SystemLimit
 
 
@@ -22,3 +24,21 @@ class TestSystemLimitMethods(TestCase):
         SystemLimit.objects.create(key="general_limit", limit=20, domain="specific")
         self.assertEqual(SystemLimit.for_key("general_limit"), 10)
         self.assertEqual(SystemLimit.for_key("general_limit", domain="specific"), 20)
+
+    def test_raises_error_if_changing_scope_from_global_to_domain(self):
+        global_limit = SystemLimit.objects.create(key="general_limit", limit=10)
+        with self.assertRaises(SystemLimitIllegalScopeChange):
+            global_limit.domain = 'new-domain'  # this is changing scope from global to domain
+            global_limit.save()
+
+        # nothing stops you from changing a domain scope to a global scope apart from the db constraint
+        new_limit = SystemLimit.objects.create(key="domain_limit", limit=5, domain='test')
+        new_limit.domain = ''
+        new_limit.save()
+
+    def test_raises_db_error_if_conflicting_global_scopes(self):
+        SystemLimit.objects.create(key="general_limit", limit=10)
+        domain_limit = SystemLimit.objects.create(key="general_limit", limit=5, domain='test')
+        with self.assertRaises(IntegrityError):
+            domain_limit.domain = ''
+            domain_limit.save()
