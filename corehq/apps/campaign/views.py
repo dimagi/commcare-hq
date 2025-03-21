06383b1c2775a8a96ad9254429b05103900893b8
Ajeet <ayadav@dimagi.com>
2025-03-21 13:09:14 +0530

    adds view method to get edit widget form

--- corehq/apps/campaign/views.py
+++ corehq/apps/campaign/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.core.paginator import Paginator
 from django.forms.models import model_to_dict
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy
@@ -228,3 +229,22 @@ class DashboardWidgetView(HqHtmxActionMixin, BaseDomainView):
     @property
     def model_class(self):
         return WidgetType.get_model_class(self.widget_type)
+
+    @hq_hx_action('get')
+    def edit_widget(self, request, *args, **kwargs):
+        self._validate_request_widget_type()
+
+        widget = get_object_or_404(self.model_class, pk=self.widget_id)
+        context = {
+            'widget_form': self.form_class(self.domain, instance=widget),
+            'widget_type': self.widget_type,
+            'widget': widget,
+        }
+        return self.render_htmx_partial_response(request, self.form_template_partial_name, context)
+
+    @cached_property
+    def widget_id(self):
+        if self.request.method == "GET":
+            return self.request.GET.get('widget_id')
+        else:
+            return self.request.POST.get('widget_id')
