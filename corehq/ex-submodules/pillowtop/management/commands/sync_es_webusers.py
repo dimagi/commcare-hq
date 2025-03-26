400e62f7011dc8e43fa560fb6d6287ef051831eb
Jonathan Tang <Jonahtang738@gmail.com>
2025-03-26 13:06:59 -0700

    handle user not found in ES

--- corehq/ex-submodules/pillowtop/management/commands/sync_es_webusers.py
+++ corehq/ex-submodules/pillowtop/management/commands/sync_es_webusers.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from corehq.dbaccessors.couchapps.all_docs import get_all_docs_with_doc_types
 from corehq.apps.users.models import CouchUser
 from corehq.apps.users.signals import update_user_in_es
+from elasticsearch5.exceptions import NotFoundError
 
 
 class Command(BaseCommand):
@@ -11,4 +12,13 @@ class Command(BaseCommand):
         web_users_docs = get_all_docs_with_doc_types(db=CouchUser.get_db(), doc_types=["WebUser"])
 
         for user_doc in web_users_docs:
-            update_user_in_es(None, CouchUser.wrap_correctly(user_doc))
+            try:
+                user = CouchUser.wrap_correctly(user_doc)
+                update_user_in_es(None, user)
+            except NotFoundError as e:
+                if user.to_be_deleted():
+                    continue
+                else:
+                    self.stdout.write(self.style.WARNING(
+                        f"User not found in Elasticsearch: {user_doc.get('_id')} - {str(e)}"
+                    ))
