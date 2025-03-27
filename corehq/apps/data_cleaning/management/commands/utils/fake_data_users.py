76786ad58783bcc5aef00ccf1a511d37e45c1a5b
Biyeun Buczyk <biyeun@dimagi.com>
2025-03-27 13:03:01 +0100

    TEST DATA WIP - add utilities for creating fake data for data cleaning testing

---
+++ corehq/apps/data_cleaning/management/commands/utils/fake_data_users.py
@@ -0,0 +1,20 @@
+import random
+
+
+DATA_CLEANING_TEST_USER_PREFIX = 'dc.plantcare'
+
+
+def get_first_name():
+    return random.choice((
+        'Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Hannah', 'Isaac', 'Jane', 'Kevin', 'Linda',
+        'Michael', 'Nancy', 'Oscar', 'Pamela', 'Quincy', 'Rachel', 'Steve', 'Tina', 'Ulysses', 'Vivian', 'Walter',
+        'Xavier', 'Yvonne', 'Zach',
+    ))
+
+
+def get_last_name():
+    return random.choice((
+        'Rodriguez', 'Lewis', 'Lee', 'Walker', 'Hall', 'Allen', 'Young', 'Hernandez', 'King', 'Wright', 'Lopez',
+        'Smith', 'Johnson', 'Williams', 'Jones', 'Brown', 'Davis', 'Miller', 'Wilson', 'Moore', 'Taylor',
+        'Thomas', 'Jackson', 'White', 'Harris', 'Martin', 'Thompson', 'Garcia', 'Martinez', 'Robinson', 'Clark',
+    ))
