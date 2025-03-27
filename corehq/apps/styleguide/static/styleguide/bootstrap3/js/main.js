ba378cb5457206c5107be09d153535a9f3071de2
Jenny Schweers <jschweers@dimagi.com>
2025-03-27 11:16:25 -0400

    Moved inline script on organisms page into main.js

--- corehq/apps/styleguide/static/styleguide/bootstrap3/js/main.js
+++ corehq/apps/styleguide/static/styleguide/bootstrap3/js/main.js
@@ -1,10 +1,18 @@
 import "commcarehq";
+import $ from "jquery";
 
+// Molecules page
 import "hqwebapp/js/components/select_toggle";
-
 import "styleguide/bootstrap3/js/feedback";
 import "styleguide/bootstrap3/js/inline_edit";
 import "styleguide/bootstrap3/js/multiselect";
 import "styleguide/bootstrap3/js/pagination";
 import "styleguide/bootstrap3/js/search_box";
 import "styleguide/bootstrap3/js/select2";
+
+// Organisms page
+$(function () {
+  $('.styleguide-example form').submit(function (e) {
+    e.preventDefault();
+  });
+});
