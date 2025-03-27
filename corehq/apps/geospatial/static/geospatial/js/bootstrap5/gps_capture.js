435d14af284ad2b3e06b04b18ab09ba9f34cb197
Norman Hooper <nhooper@dimagi.com>
2025-03-27 18:41:50 +0000

    "Bootstrap 5 Migration - initial auto-migration for geospatial/js/gps_capture.js, splitting templates"

--- corehq/apps/geospatial/static/geospatial/js/bootstrap5/gps_capture.js
+++ corehq/apps/geospatial/static/geospatial/js/bootstrap5/gps_capture.js
@@ -1,4 +1,4 @@
-hqDefine("geospatial/js/gps_capture", [
+hqDefine("geospatial/js/bootstrap5/gps_capture", [
     "jquery",
     "knockout",
     'underscore',
@@ -7,7 +7,7 @@ hqDefine("geospatial/js/gps_capture", [
     '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min',
     "hqwebapp/js/components/pagination",
     "hqwebapp/js/components/search_box",
-    'reports/js/bootstrap3/base',
+    'reports/js/bootstrap5/base',
     'hqwebapp/js/select2_knockout_bindings.ko',
     'commcarehq',
 ], function (
