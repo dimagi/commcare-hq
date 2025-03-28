7243d50d676f44ee21a4285069e85e70c3e7c357
Jenny Schweers <jschweers@dimagi.com>
2025-03-27 20:21:23 -0400

    Migrated tables page to webpack

---
+++ corehq/apps/styleguide/static/styleguide/js/tables.js
@@ -0,0 +1,9 @@
+import 'commcarehq';
+import 'styleguide/js/main';
+import 'datatables.net/js/jquery.dataTables.min';
+import 'datatables.net-bs5/js/dataTables.bootstrap5.min';
+import 'datatables.net-fixedcolumns/js/dataTables.fixedColumns.min';
+import 'datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.min';
+
+import 'styleguide/js/examples/datatables';
+import 'styleguide/js/examples/paginated_table';
