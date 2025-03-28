0bc343dfbe5156ef68f471e499fdd66f7a875821
Minha <mlee@dimagi.com>
2025-03-28 15:18:57 -0400

    Add IP to session block list if is not allowed

--- corehq/apps/ip_access/middleware.py
+++ corehq/apps/ip_access/middleware.py
@@ -25,10 +25,16 @@ class IPAccessMiddleware(MiddlewareMixin):
 def is_valid_ip(request, domain):
     ip = get_ip(request)
     key = f"hq_session_ips-{domain}"
+    block_key = f"hq_session_blocked_ips-{domain}"
     if key not in request.session:
         request.session[key] = []
+    if block_key not in request.session:
+        request.session[block_key] = []
+
     if ip in request.session[key]:
         return True
+    elif ip in request.sessions[block_key]:
+        return False
     else:
         try:
             config = IPAccessConfig.objects.get(domain=domain)
@@ -37,4 +43,6 @@ def is_valid_ip(request, domain):
         if not config or config.is_allowed(ip):
             request.session[key].append(ip)
             return True
-        return False
+        else:
+            request.session[block_key].append(ip)
+            return False
