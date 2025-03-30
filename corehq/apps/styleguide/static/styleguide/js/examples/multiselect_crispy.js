42cdd28188b1dec2dc0158275c8d161481f84833
Jenny Schweers <jschweers@dimagi.com>
2025-03-30 12:17:37 -0400

    Removed double document ready handler from multiselect_crispy.js

--- corehq/apps/styleguide/static/styleguide/js/examples/multiselect_crispy.js
+++ corehq/apps/styleguide/static/styleguide/js/examples/multiselect_crispy.js
@@ -1,18 +1,16 @@
 import $ from 'jquery';
 import multiselectUtils from 'hqwebapp/js/multiselect_utils';
 
-$(function () {
-    let teamListener = function () {
-        console.log("Triggered willSelectAllListener");
-    };
+let teamListener = function () {
+    console.log("Triggered willSelectAllListener");
+};
 
-    $(function () {
-        multiselectUtils.createFullMultiselectWidget('id_team', {
-            selectableHeaderTitle: gettext("Benched"),
-            selectedHeaderTitle: gettext("Playing"),
-            searchItemTitle: gettext("Search Team..."),
-            disableModifyAllActions: false,
-            willSelectAllListener: teamListener,
-        });
+$(function () {
+    multiselectUtils.createFullMultiselectWidget('id_team', {
+        selectableHeaderTitle: gettext("Benched"),
+        selectedHeaderTitle: gettext("Playing"),
+        searchItemTitle: gettext("Search Team..."),
+        disableModifyAllActions: false,
+        willSelectAllListener: teamListener,
     });
 });
