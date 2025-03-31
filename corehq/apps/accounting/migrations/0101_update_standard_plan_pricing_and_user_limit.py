81458f8c4a4a14ff03a7d787fe925350a397e0be
nospame <mapson.evan@gmail.com>
2025-03-31 08:52:17 -0700

    Update Standard plan pricing and user limit

---
+++ corehq/apps/accounting/migrations/0101_update_standard_plan_pricing_and_user_limit.py
@@ -0,0 +1,21 @@
+from django.db import migrations
+
+from corehq.apps.accounting.bootstrap.config.standard_update_april_2025 import (
+    BOOTSTRAP_CONFIG,
+)
+from corehq.apps.accounting.bootstrap.utils import ensure_plans
+
+
+def _bootstrap_new_standard_pricing(apps, schema_editor):
+    ensure_plans(BOOTSTRAP_CONFIG, verbose=True, apps=apps)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('accounting', '0100_alter_customerinvoicecommunicationhistory_communication_type_and_more'),
+    ]
+
+    operations = [
+        migrations.RunPython(_bootstrap_new_standard_pricing),
+    ]
