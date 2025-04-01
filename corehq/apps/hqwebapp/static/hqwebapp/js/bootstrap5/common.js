92b6e09db821bb48b86c292f39c432e6ce750e3a
Jenny Schweers <jschweers@dimagi.com>
2025-03-31 20:15:42 -0400

    Removed comment

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/common.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap5/common.js
@@ -3,7 +3,6 @@ hqDefine("hqwebapp/js/bootstrap5/common", [
     'knockout',
     'ko.mapping',
     'underscore',
-    // the es6! loaders below (without the prefix) are necessary to fix build issues with these modules
     'hqwebapp/js/bootstrap5_loader',
 ], function () {
     // nothing to do, this is just to define the major common dependencies for HQ
