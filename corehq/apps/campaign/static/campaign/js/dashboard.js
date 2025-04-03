4f306d6a4ed6c04cc520851b220c4ae2ce41e5a2
Ajeet <ayadav@dimagi.com>
2025-04-03 14:40:28 +0530

    fix: show correct title on modal title

--- corehq/apps/campaign/static/campaign/js/dashboard.js
+++ corehq/apps/campaign/static/campaign/js/dashboard.js
@@ -246,10 +246,10 @@ var onHideWidgetModal = function () {
 
 var onShowWidgetModal = function (event) {
     const triggerSource = event.relatedTarget;
-    if (triggerSource.id === 'edit-widget-btn') {
-        $modalTitleElement.text(editWidgetText);
-    } else {
+    if (triggerSource.id === 'add-widget-btn') {
         $modalTitleElement.text(addWidgetText);
+    } else {
+        $modalTitleElement.text(editWidgetText);
     }
 };
 
