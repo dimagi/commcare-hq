46eaf47f0348c874d253ae71d3acc3734cb10bd6
mkangia <mkangia@dimagi.com>
2025-04-04 04:12:34 +0530

    better help doc

--- corehq/apps/users/management/commands/reset_demo_users_restores.py
+++ corehq/apps/users/management/commands/reset_demo_users_restores.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = "Reset restores for demo users"
+    help = "Reset restores for demo users in a domain"
 
     def add_arguments(self, parser):
         parser.add_argument('domain')
