25ba6e3dab7e6df14338290b3232176165765e2d
robert-costello <robertcostello81@gmail.com>
2025-04-07 15:01:27 -0400

    use background queue

--- custom/mgh_epic/sync_epic_appointments.py
+++ custom/mgh_epic/sync_epic_appointments.py
@@ -315,7 +315,7 @@ def sync_all_appointments_domain(domain):
 MGH_EPIC_DOMAINS = settings.CUSTOM_DOMAINS_BY_MODULE['custom.mgh_epic']
 
 
-@periodic_task(run_every=crontab(hour="*", minute=1), queue=settings.CELERY_PERIODIC_QUEUE)
+@periodic_task(run_every=crontab(hour="*", minute=1), queue="background_queue")
 def sync_all_epic_appointments():
     for domain in MGH_EPIC_DOMAINS:
         sync_all_appointments_domain(domain)
