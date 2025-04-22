904d3107a0eecf93c532cb976d7c7eb0330e7901
Jing Cheng <jcheng@dimagi.com>
2025-04-22 18:08:31 -0400

    use requests.get to replace Github()

--- corehq/apps/builds/tests.py
+++ corehq/apps/builds/tests.py
@@ -14,15 +14,3 @@ def test_extract_build_info_from_filename_error():
     msg = r"Could not find filename like 'CommCare_CommCare_.+_artifacts.zip' in 'foo'"
     with pytest.raises(ValueError, match=msg):
         extract_build_info_from_filename('foo')
-
-
-# NOTE: Don't run the test below because GitHub rate-limits unauthenticated API
-# request _very_ fast.
-#
-# See doctest in: corehq/apps/builds/management/commands/add_commcare_build.py
-#
-# def test_doctests():
-#     import doctest
-#     from .management.commands import add_commcare_build
-#     results = doctest.testmod(add_commcare_build, optionflags=doctest.ELLIPSIS)
-#     assert results.failed == 0, results
