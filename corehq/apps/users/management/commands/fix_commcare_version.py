bed3565a175942bea3e85f06368dda8cc9d0e133
Jing Cheng <jcheng@dimagi.com>
2025-04-28 00:14:07 -0400

    use iter_update to do bulk update

--- corehq/apps/users/management/commands/fix_commcare_version.py
+++ corehq/apps/users/management/commands/fix_commcare_version.py
@@ -1,57 +1,61 @@
+from itertools import chain
+
 from django.core.management.base import BaseCommand
-from corehq.apps.users.dbaccessors import get_all_user_rows, get_mobile_user_count
-from corehq.apps.users.models import CouchUser, CommCareUser
+from corehq.apps.users.models import CommCareUser
+from corehq.dbaccessors.couchapps.all_docs import (
+    get_doc_count_by_type,
+    iter_all_doc_ids,
+)
+from corehq.util.couch import DocUpdate, iter_update
 from corehq.util.log import with_progress_bar
-from corehq.apps.es import DomainES
 
 
 class Command(BaseCommand):
     help = 'Fix truncated commcare_version for mobile users'
 
     def handle(self, *args, **kwargs):
-        self.update_commcare_version()
+        db = CommCareUser.get_db()
+        count = get_doc_count_by_type(db, 'CommCareUser')
+        all_ids = chain(iter_all_doc_ids(db, 'CommCareUser'))
+        iter_update(db, update_commcare_version, with_progress_bar(all_ids, count), verbose=True)
 
-    def update_commcare_version(self):
-        total_update = 0
-        total_users = 0
-        active_domains = DomainES().is_active().values_list('name', flat=True)
 
-        for domain in with_progress_bar(active_domains, len(active_domains)):
-            total_users_in_domain = get_mobile_user_count(domain)
-            total_users += total_users_in_domain
-            users = get_all_user_rows(domain, include_web_users=False, include_mobile_users=True,
-                                      include_inactive=True, include_docs=True)
+def update_commcare_version(user_doc):
+    changes_made = False
+    if 'devices' in user_doc and user_doc['devices']:
+        if 'last_device' in user_doc:
+            changes_made = update_version(user_doc['last_device'], 'commcare_version')
+        for device in user_doc['devices']:
+            changes_made |= update_version(device, 'commcare_version')
+    if ('reporting_metadata' in user_doc
+            and 'last_submissions' in user_doc['reporting_metadata']
+            and user_doc['reporting_metadata']['last_submissions']):
+        if 'last_submission_for_user' in user_doc['reporting_metadata']:
+            changes_made |= update_version(
+                user_doc['reporting_metadata']['last_submission_for_user'],
+                'commcare_version'
+            )
+        for submission in user_doc['reporting_metadata']['last_submissions']:
+            changes_made |= update_version(submission, 'commcare_version')
+    if changes_made:
+        return DocUpdate(user_doc)
+    else:
+        return None
 
-            for user in users:
-                user = CouchUser.wrap_correctly(user['doc'])
 
-                changes_made = False
-                if getattr(user, 'devices', None):
-                    changes_made |= self.update_version(user.last_device, 'commcare_version')
-                    for device in user.devices:
-                        changes_made |= self.update_version(device, 'commcare_version')
-                if getattr(user.reporting_metadata, 'last_submissions', None):
-                    changes_made |= self.update_version(user.reporting_metadata.last_submission_for_user,
-                                                        'commcare_version')
-                    for submission in user.reporting_metadata.last_submissions:
-                        changes_made |= self.update_version(submission, 'commcare_version')
-                if changes_made:
-                    CommCareUser.save_docs([user])
-                    total_update += 1
-        print(f"Updated {total_update} users out of {total_users}")
+def update_version(obj, attr_name):
+    original_version = obj.get(attr_name)
+    new_version = _fix_commcare_version(original_version)
+    if new_version != original_version:
+        obj[attr_name] = new_version
+        return True
+    return False
 
-    def update_version(self, obj, attr_name):
-        original_version = getattr(obj, attr_name, None)
-        new_version = self._fix_commcare_version(original_version)
-        if new_version != original_version:
-            setattr(obj, attr_name, new_version)
-            return True
-        return False
 
-    def _fix_commcare_version(self, version):
-        if version and version.count('.') == 1:
-            return f"{version}.0"
-        elif version and version.count('.') == 0:
-            return f"{version}.0.0"
-        else:
-            return version
+def _fix_commcare_version(version):
+    if version and version.count('.') == 1:
+        return f"{version}.0"
+    elif version and version.count('.') == 0:
+        return f"{version}.0.0"
+    else:
+        return version
