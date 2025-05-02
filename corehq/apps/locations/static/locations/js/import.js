c91f85487c6afb78901a18b73506a3ce0f0f3c58
Jenny Schweers <jschweers@dimagi.com>
2025-05-02 13:40:30 -0400

    Updated locations to use ESM

--- corehq/apps/locations/static/locations/js/import.js
+++ corehq/apps/locations/static/locations/js/import.js
@@ -1,41 +1,35 @@
-hqDefine('locations/js/import', [
-    'jquery',
-    'knockout',
-    'analytix/js/google',
-    'commcarehq',
-], function (
-    $,
-    ko,
-    googleAnalytics,
-) {
-    $(function () {
-        googleAnalytics.track.click($('#download_link'), 'Organization Structure', 'Bulk Import', 'Download');
-        $("button[type='submit']").click(function () {
-            googleAnalytics.track.event('Organization Structure', 'Bulk Import', 'Upload');
-        });
+import "commcarehq";
+import $ from "jquery";
+import ko from "knockout";
+import googleAnalytics from "analytix/js/google";
 
-        if ($("#bulk_upload_form").get(0)) {
-            $("#bulk_upload_form").koApplyBindings({
-                file: ko.observable(null),
-            });
-        }
-
-        // modify download url to pass extra options
-        function consumptionOptionsViewModel(baseUrl) {
-            var self = {};
-            self.base_url = baseUrl;
-            self.include_consumption = ko.observable(false);
-            self.url = ko.computed(function () {
-                return (
-                    self.base_url + "?"
-                    + (self.include_consumption() ? "include_consumption=true" : "")
-                );
-            });
-            return self;
-        }
-
-        $("#download_block").koApplyBindings(
-            consumptionOptionsViewModel($("#download_link").get(0).href),
-        );
+$(function () {
+    googleAnalytics.track.click($('#download_link'), 'Organization Structure', 'Bulk Import', 'Download');
+    $("button[type='submit']").click(function () {
+        googleAnalytics.track.event('Organization Structure', 'Bulk Import', 'Upload');
     });
+
+    if ($("#bulk_upload_form").get(0)) {
+        $("#bulk_upload_form").koApplyBindings({
+            file: ko.observable(null),
+        });
+    }
+
+    // modify download url to pass extra options
+    function consumptionOptionsViewModel(baseUrl) {
+        var self = {};
+        self.base_url = baseUrl;
+        self.include_consumption = ko.observable(false);
+        self.url = ko.computed(function () {
+            return (
+                self.base_url + "?"
+                + (self.include_consumption() ? "include_consumption=true" : "")
+            );
+        });
+        return self;
+    }
+
+    $("#download_block").koApplyBindings(
+        consumptionOptionsViewModel($("#download_link").get(0).href),
+    );
 });
