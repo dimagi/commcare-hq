0eed916f2f632009560c43dbb24f84a8c0aec4ce
Jenny Schweers <jschweers@dimagi.com>
2025-05-02 21:56:36 -0400

    Migrated hqadmin app

--- corehq/apps/hqadmin/static/hqadmin/js/app_build_timings.js
+++ corehq/apps/hqadmin/static/hqadmin/js/app_build_timings.js
@@ -1,22 +1,20 @@
-hqDefine('hqadmin/js/app_build_timings', [
-    "jquery",
-    "jquery-treetable/jquery.treetable",
-    "commcarehq",
-], function ($) {
-    $(function () {
-        $("#timingTable").treetable({
-            expandable: true,
-            expanderTemplate: '<i class="fa fa-angle-double-down" /> ',
-            initialState: 'expanded',
-            clickableNodeNames: true,
-            onNodeExpand: function () {
-                $(this.expander).addClass("fa-angle-double-down");
-                $(this.expander).removeClass("fa-angle-double-right");
-            },
-            onNodeCollapse: function () {
-                $(this.expander).removeClass("fa-angle-double-down");
-                $(this.expander).addClass("fa-angle-double-right");
-            },
-        });
+import "commcarehq";
+import $ from "jquery";
+import "jquery-treetable/jquery.treetable";
+
+$(function () {
+    $("#timingTable").treetable({
+        expandable: true,
+        expanderTemplate: '<i class="fa fa-angle-double-down" /> ',
+        initialState: 'expanded',
+        clickableNodeNames: true,
+        onNodeExpand: function () {
+            $(this.expander).addClass("fa-angle-double-down");
+            $(this.expander).removeClass("fa-angle-double-right");
+        },
+        onNodeCollapse: function () {
+            $(this.expander).removeClass("fa-angle-double-down");
+            $(this.expander).addClass("fa-angle-double-right");
+        },
     });
 });
