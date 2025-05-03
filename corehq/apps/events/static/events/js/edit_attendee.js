ca5146f99c564a671699d6e9c7289a18acf328c2
Jenny Schweers <jschweers@dimagi.com>
2025-05-03 09:36:17 -0400

    Migrated events to ESM

--- corehq/apps/events/static/events/js/edit_attendee.js
+++ corehq/apps/events/static/events/js/edit_attendee.js
@@ -1,9 +1,7 @@
-hqDefine('events/js/edit_attendee', [
-    'jquery',
-    'knockout',
-    'hqwebapp/js/initial_page_data',
-    'locations/js/widgets',
-    "commcarehq",
-], function () {
-    // Placeholder for Location & Primary location widgets
-});
+import "commcarehq";
+import "jquery";
+import "knockout";
+import "hqwebapp/js/initial_page_data";
+import "locations/js/widgets";
+
+// Placeholder for Location & Primary location widgets
