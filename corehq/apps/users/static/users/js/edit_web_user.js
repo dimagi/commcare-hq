0d241ddc66d00aec35b57f9f675842a6cc9bc158
Jenny Schweers <jschweers@dimagi.com>
2025-05-03 11:10:39 -0400

    Migrated users app to ESM

--- corehq/apps/users/static/users/js/edit_web_user.js
+++ corehq/apps/users/static/users/js/edit_web_user.js
@@ -1,28 +1,20 @@
+import "commcarehq";
+import $ from "jquery";
+import initialPageData from "hqwebapp/js/initial_page_data";
+import customDataFields from "users/js/custom_data_fields";
+import "locations/js/widgets";
 
-
-hqDefine('users/js/edit_web_user', [
-    'jquery',
-    'hqwebapp/js/initial_page_data',
-    'users/js/custom_data_fields',
-    'locations/js/widgets',
-    'commcarehq',
-], function (
-    $,
-    initialPageData,
-    customDataFields,
-) {
-    var $customDataFieldsForm = $(".custom-data-fieldset");
-    if ($customDataFieldsForm.length) {
-        $customDataFieldsForm.koApplyBindings(function () {
-            return {
-                custom_fields: customDataFields.customDataFieldsEditor({
-                    user_data: initialPageData.get('user_data'),
-                    profiles: initialPageData.get('custom_fields_profiles'),
-                    profile_slug: initialPageData.get('custom_fields_profile_slug'),
-                    slugs: initialPageData.get('custom_fields_slugs'),
-                    can_edit_original_profile: initialPageData.get('can_edit_original_profile'),
-                }),
-            };
-        });
-    }
-});
+var $customDataFieldsForm = $(".custom-data-fieldset");
+if ($customDataFieldsForm.length) {
+    $customDataFieldsForm.koApplyBindings(function () {
+        return {
+            custom_fields: customDataFields.customDataFieldsEditor({
+                user_data: initialPageData.get('user_data'),
+                profiles: initialPageData.get('custom_fields_profiles'),
+                profile_slug: initialPageData.get('custom_fields_profile_slug'),
+                slugs: initialPageData.get('custom_fields_slugs'),
+                can_edit_original_profile: initialPageData.get('can_edit_original_profile'),
+            }),
+        };
+    });
+}
