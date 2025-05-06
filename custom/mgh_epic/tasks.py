6b772baa6ad5daacbc7667324519ac78c8a8c2bf
robert-costello <robertcostello81@gmail.com>
2025-05-06 14:32:25 -0400

    handle exceptions per domain

--- custom/mgh_epic/tasks.py
+++ custom/mgh_epic/tasks.py
@@ -12,4 +12,7 @@ MGH_EPIC_DOMAINS = settings.CUSTOM_DOMAINS_BY_MODULE['custom.mgh_epic']
 @periodic_task(run_every=crontab(hour="*", minute=1), queue="background_queue")
 def sync_all_epic_appointments():
     for domain in MGH_EPIC_DOMAINS:
-        sync_all_appointments_domain(domain)
+        try:
+            sync_all_appointments_domain(domain)
+        except Exception as e:
+            raise Exception(f"Error syncing appointments for domain {domain}: {e}")
