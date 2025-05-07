44cace47fae9e9793a9c32f9e2cc07fcf068a31c
Jenny Schweers <jschweers@dimagi.com>
2025-05-07 16:39:55 -0400

    Migrated accounting

--- corehq/apps/accounting/static/accounting/js/credits_tab.js
+++ corehq/apps/accounting/static/accounting/js/credits_tab.js
@@ -1,24 +1,19 @@
 
-hqDefine("accounting/js/credits_tab", [
-    'jquery',
-    'knockout',
-], function (
-    $,
-    ko,
-) {
-    $(function () {
-        var $form = $('#credit-form');
-        if ($form.length) {
-            var creditFormModel = function () {
-                var self = {};
-                self.rateType = ko.observable("");
-                self.showFeature = ko.computed(function () {
-                    return self.rateType() === 'Feature';
-                }, self);
-                return self;
-            };
+import $ from "jquery";
+import ko from "knockout";
 
-            $form.koApplyBindings(creditFormModel());
-        }
-    });
+$(function () {
+    var $form = $('#credit-form');
+    if ($form.length) {
+        var creditFormModel = function () {
+            var self = {};
+            self.rateType = ko.observable("");
+            self.showFeature = ko.computed(function () {
+                return self.rateType() === 'Feature';
+            }, self);
+            return self;
+        };
+
+        $form.koApplyBindings(creditFormModel());
+    }
 });
