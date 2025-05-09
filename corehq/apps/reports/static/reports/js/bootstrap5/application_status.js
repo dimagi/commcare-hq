4ab6ef59f85b6861a4f2c0918025f6aa98ca8c20
Jenny Schweers <jschweers@dimagi.com>
2025-05-09 09:38:17 -0400

    Migrate some bootstrap-specific reports

--- corehq/apps/reports/static/reports/js/bootstrap5/application_status.js
+++ corehq/apps/reports/static/reports/js/bootstrap5/application_status.js
@@ -1,15 +1,11 @@
 
-hqDefine("reports/js/bootstrap5/application_status", [
-    "jquery",
-], function (
-    $,
-) {
-    $(function () {
-        $('#report-content').on('click', '.toggle-all-locations', function (e) {
-            $(this).prevAll('.locations-list').toggle();
-            $(this).children('.loc-view-control').toggle();
-            $(this).prevAll('.all-locations-list').toggle();
-            e.preventDefault();
-        });
+import $ from "jquery";
+
+$(function () {
+    $('#report-content').on('click', '.toggle-all-locations', function (e) {
+        $(this).prevAll('.locations-list').toggle();
+        $(this).children('.loc-view-control').toggle();
+        $(this).prevAll('.all-locations-list').toggle();
+        e.preventDefault();
     });
 });
