e4653c34506573f8b64a713830af2c483923b0c4
robert-costello <robertcostello81@gmail.com>
2025-05-09 10:38:57 -0400

    log exceptions

--- custom/mgh_epic/tasks.py
+++ custom/mgh_epic/tasks.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.conf import settings
 
 from celery.schedules import crontab
@@ -8,11 +10,13 @@ from custom.mgh_epic.sync_epic_appointments import sync_all_appointments_domain
 
 MGH_EPIC_DOMAINS = settings.CUSTOM_DOMAINS_BY_MODULE['custom.mgh_epic']
 
+logger = logging.getLogger("sync_epic_appointments")
+
 
 @periodic_task(run_every=crontab(hour="*", minute=1), queue="background_queue")
 def sync_all_epic_appointments():
     for domain in MGH_EPIC_DOMAINS:
         try:
             sync_all_appointments_domain(domain)
-        except Exception as e:
-            raise Exception(f"Error syncing appointments for domain {domain}: {e}")
+        except Exception:
+            logger.exception("Error syncing epic appointments", extra={"domain": domain})
