a0649b188a54d9ecf9e3d8e2c381e4ab8142262f
Jenny Schweers <jschweers@dimagi.com>
2025-05-09 09:32:23 -0400

    Migrated a few non-bootstrap reports

--- corehq/apps/reports/static/reports/js/case_activity.js
+++ corehq/apps/reports/static/reports/js/case_activity.js
@@ -1,13 +1,12 @@
+import $ from "jquery";
 
-hqDefine("reports/js/case_activity", ['jquery'], function ($) {
 
-    $(document).ajaxSuccess(function (event, xhr, settings) {
-        if (settings.url.match(/reports\/async\/case_activity/)) {
-            const $specialNotice = $("#report-special-notice");
-            if ($specialNotice) {
-                const urlParams = new URLSearchParams(window.location.search);
-                (urlParams.get('view_by') === 'groups') ? $specialNotice.show() : $specialNotice.hide();
-            }
+$(document).ajaxSuccess(function (event, xhr, settings) {
+    if (settings.url.match(/reports\/async\/case_activity/)) {
+        const $specialNotice = $("#report-special-notice");
+        if ($specialNotice) {
+            const urlParams = new URLSearchParams(window.location.search);
+            (urlParams.get('view_by') === 'groups') ? $specialNotice.show() : $specialNotice.hide();
         }
-    });
+    }
 });
