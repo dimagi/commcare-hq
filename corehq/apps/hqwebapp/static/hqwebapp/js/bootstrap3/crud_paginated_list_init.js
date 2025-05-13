f159b8dc898a8602852c3929cd738df5804e4b21
Jenny Schweers <jschweers@dimagi.com>
2025-05-12 20:12:29 -0400

    Migrated crud paginated list files
    DHIS2 had the last non-ESM usages of these files

--- corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/crud_paginated_list_init.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/bootstrap3/crud_paginated_list_init.js
@@ -1,31 +1,24 @@
-hqDefine("hqwebapp/js/bootstrap3/crud_paginated_list_init", [
-    "jquery",
-    "knockout",
-    "hqwebapp/js/initial_page_data",
-    "hqwebapp/js/bootstrap3/crud_paginated_list",
-    "commcarehq",
-], function (
-    $,
-    ko,
-    initialPageData,
-    CRUDPaginatedList,
-) {
-    var paginatedListModel = CRUDPaginatedList.CRUDPaginatedListModel(
-        initialPageData.get('total'),
-        initialPageData.get('limit'),
-        initialPageData.get('page'),
-        {
-            statusCodeText: initialPageData.get('status_codes'),
-            allowItemCreation: initialPageData.get('allow_item_creation'),
-            createItemForm: initialPageData.get('create_item_form'),
-            createItemFormClass: initialPageData.get('create_item_form_class'),
-        },
-    );
+import "commcarehq";
+import $ from "jquery";
+import ko from "knockout";
+import initialPageData from "hqwebapp/js/initial_page_data";
+import CRUDPaginatedList from "hqwebapp/js/bootstrap3/crud_paginated_list";
 
-    $(function () {
-        ko.applyBindings(paginatedListModel, $('#editable-paginated-list').get(0));
-        paginatedListModel.init();
-    });
+var paginatedListModel = CRUDPaginatedList.CRUDPaginatedListModel(
+    initialPageData.get('total'),
+    initialPageData.get('limit'),
+    initialPageData.get('page'),
+    {
+        statusCodeText: initialPageData.get('status_codes'),
+        allowItemCreation: initialPageData.get('allow_item_creation'),
+        createItemForm: initialPageData.get('create_item_form'),
+        createItemFormClass: initialPageData.get('create_item_form_class'),
+    },
+);
 
-    return {'paginatedListModel': paginatedListModel};
+$(function () {
+    ko.applyBindings(paginatedListModel, $('#editable-paginated-list').get(0));
+    paginatedListModel.init();
 });
+
+export default {'paginatedListModel': paginatedListModel};
