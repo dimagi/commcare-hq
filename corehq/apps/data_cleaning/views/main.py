8ac58d7e34c331a9b4e400d11edf94c6a81c0d14
Biyeun Buczyk <biyeun@dimagi.com>
2025-05-14 19:08:07 +0200

    rename: remove Bulk - if we won't remove the bulk from the frontend we might as well do it on the backend :P

--- corehq/apps/data_cleaning/views/main.py
+++ corehq/apps/data_cleaning/views/main.py
@@ -26,7 +26,7 @@ from corehq.util.view_utils import set_file_download
     use_bootstrap5,
     require_bulk_data_cleaning_cases,
 ], name='dispatch')
-class BulkEditCasesMainView(BaseProjectDataView):
+class EditCasesMainView(BaseProjectDataView):
     page_title = gettext_lazy("Bulk Edit Case Data")
     urlname = "bulk_edit_cases_main"
     template_name = "data_cleaning/bulk_edit_main.html"
@@ -56,7 +56,7 @@ class EditCasesSessionView(BulkEditSessionViewMixin, BaseProjectDataView):
     redirect_on_session_exceptions = True
 
     def get_redirect_url(self):
-        return reverse(BulkEditCasesMainView.urlname, args=(self.domain, ))
+        return reverse(EditCasesMainView.urlname, args=(self.domain, ))
 
     @property
     def case_type(self):
@@ -75,8 +75,8 @@ class EditCasesSessionView(BulkEditSessionViewMixin, BaseProjectDataView):
     @property
     def parent_pages(self):
         return [{
-            'title': BulkEditCasesMainView.page_title,
-            'url': reverse(BulkEditCasesMainView.urlname, args=(self.domain,)),
+            'title': EditCasesMainView.page_title,
+            'url': reverse(EditCasesMainView.urlname, args=(self.domain,)),
         }]
 
     @property
@@ -111,7 +111,7 @@ def clear_session_caches(request, domain, session_id):
     session = BulkEditSession.objects.get(session_id=session_id)
     clear_caches_case_data_cleaning(session.domain, session.identifier)
     messages.success(request, _("Caches successfully cleared."))
-    return redirect(reverse(BulkEditCasesMainView.urlname, args=(domain,)))
+    return redirect(reverse(EditCasesMainView.urlname, args=(domain,)))
 
 
 @require_GET
