edcc9285c4e26e310c5226ffdec951b1d51e306a
Biyeun Buczyk <biyeun@dimagi.com>
2025-05-22 18:53:36 +0200

    flag hq-hx-action requests + middleware to remove - address issue where logs don't have insight into which htmx partial is being served as only the top-level url is shown - address issues with browser caches (especially fast back/forward button navigation) where the browser caches the response for the last htmx fragment served from the top level URL as the cached response for that url. this is a problem if that url has several partial responses. this creates a unique key for the browser so caching behaves as expected.

--- corehq/apps/hqwebapp/static/hqwebapp/js/htmx_utils/hq_hx_action.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/htmx_utils/hq_hx_action.js
@@ -17,7 +17,21 @@
 document.body.addEventListener('htmx:configRequest', (evt) => {
     // Require that the hq-hx-action attribute is present
     if (evt.detail.elt.hasAttribute('hq-hx-action')) {
+        const action = evt.detail.elt.getAttribute('hq-hx-action');
         // insert HQ-HX-Action in the header to be processed by the `HqHtmxActionMixin`
-        evt.detail.headers['HQ-HX-Action'] = evt.detail.elt.getAttribute('hq-hx-action');
+        evt.detail.headers['HQ-HX-Action'] = action;
+
+        /*
+            Below we namespace the URL with a flag in the querystring so that
+            the browser keys the cache for each partial separately.
+
+            It also creates distinct log paths for each hq-hx-action--helpful for troubleshooting.
+
+            This querystring flag will be removed by `HqHtmxActionMiddleware` so that it
+            doesn't accidentally get passed around to other requests.
+        */
+        const url = new URL(evt.detail.path, window.location.origin);
+        url.searchParams.set('_hq-hx-action', action);
+        evt.detail.path = url.pathname + url.search;
     }
 });
