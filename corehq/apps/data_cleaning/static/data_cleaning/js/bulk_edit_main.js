864ad89d07323295ae970054013213532d05a368
Biyeun Buczyk <biyeun@dimagi.com>
2025-05-28 14:13:20 +0200

    trigger GTM events with alpine store

--- corehq/apps/data_cleaning/static/data_cleaning/js/bulk_edit_main.js
+++ corehq/apps/data_cleaning/static/data_cleaning/js/bulk_edit_main.js
@@ -3,6 +3,7 @@ import 'hqwebapp/js/htmx_base';
 import 'hqwebapp/js/htmx_utils/htmx_gtx';
 import 'hqwebapp/js/alpinejs/directives/select2';
 import 'hqwebapp/js/alpinejs/directives/tooltip';
+import 'hqwebapp/js/alpinejs/stores/gtx';
 
 import Alpine from 'alpinejs';
 Alpine.start();
