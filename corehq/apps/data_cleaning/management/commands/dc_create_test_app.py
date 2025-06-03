92a85aaa65e5eb81559aeda6dccd9c148659da08
Biyeun Buczyk <biyeun@dimagi.com>
2025-06-03 16:30:16 +0200

    FAKE DATA - squash - ruff format

--- corehq/apps/data_cleaning/management/commands/dc_create_test_app.py
+++ corehq/apps/data_cleaning/management/commands/dc_create_test_app.py
@@ -7,8 +7,7 @@ from corehq.apps.app_manager.exceptions import AppValidationError
 from corehq.apps.app_manager.models import import_app
 from corehq.apps.data_cleaning.management.commands.utils import input_validation
 
-
-APP_JSON_DIR = Path(__file__).resolve().parent / "utils" / "apps"
+APP_JSON_DIR = Path(__file__).resolve().parent / 'utils' / 'apps'
 
 
 def get_plant_app_template():
@@ -17,8 +16,10 @@ def get_plant_app_template():
 
 
 class Command(BaseCommand):
-    help = (f"Creates a copy of the {input_validation.DATA_CLEANING_TEST_APP_NAME} "
-            f"app for generating fake data for the data cleaning tool.")
+    help = (
+        f'Creates a copy of the {input_validation.DATA_CLEANING_TEST_APP_NAME} '
+        f'app for generating fake data for the data cleaning tool.'
+    )
 
     def add_arguments(self, parser):
         parser.add_argument('domain')
@@ -30,19 +31,24 @@ class Command(BaseCommand):
             return
         existing_app = input_validation.get_fake_app(domain)
         if existing_app:
-            self.stderr.write(f"Domain {domain} already has the "
-                              f"'{input_validation.DATA_CLEANING_TEST_APP_NAME} app.")
+            self.stderr.write(
+                f"Domain {domain} already has the '{input_validation.DATA_CLEANING_TEST_APP_NAME} app."
+            )
             return
         template = get_plant_app_template()
-        app = import_app(template, domain, {
-            'created_from_template': 'plant_care_app',
-        })
+        app = import_app(
+            template,
+            domain,
+            {
+                'created_from_template': 'plant_care_app',
+            },
+        )
         app.name = input_validation.DATA_CLEANING_TEST_APP_NAME
         try:
-            app_copy = app.make_build(comment="This is for generating fake data for the data cleaning tool.")
+            app_copy = app.make_build(comment='This is for generating fake data for the data cleaning tool.')
         except AppValidationError as e:
-            self.stderr.write(f"Error creating build for Plant Care: {e}")
+            self.stderr.write(f'Error creating build for Plant Care: {e}')
             return
         app_copy.is_released = True
         app_copy.save(increment_version=False)
-        self.stdout.write(f"Created app {app_copy.id} for domain {domain}.")
+        self.stdout.write(f'Created app {app_copy.id} for domain {domain}.')
--- corehq/apps/data_cleaning/management/commands/utils/issues.py
+++ corehq/apps/data_cleaning/management/commands/utils/issues.py
@@ -2,25 +2,16 @@ import random
 
 
 def is_mostly_false():
-    return bool(random.choices(
-        [0, 1],
-        weights=[0.8, 0.2]
-    )[0])
+    return bool(random.choices([0, 1], weights=[0.8, 0.2])[0])
 
 
 def get_random_spaces():
-    num_spaces = random.choices(
-        range(1, 5),
-        weights=[0.5, 0.2, 0.2, 0.1]
-    )[0]
+    num_spaces = random.choices(range(1, 5), weights=[0.5, 0.2, 0.2, 0.1])[0]
     return ' ' * num_spaces
 
 
 def simulate_capitalization_issues(value):
-    trigger = random.choices(
-        range(4),
-        weights=[0.7, 0.1, 0.1, 0.1]
-    )[0]
+    trigger = random.choices(range(4), weights=[0.7, 0.1, 0.1, 0.1])[0]
     transform_fn = [
         lambda x: x,
         lambda x: x.upper(),
@@ -33,18 +24,15 @@ def simulate_capitalization_issues(value):
 def simulate_whitespace_issues(value):
     value = get_random_spaces() + value + get_random_spaces()
     if is_mostly_false():
-        value = value + "\n"
+        value = value + '\n'
     if is_mostly_false():
-        value = "\t" + value
+        value = '\t' + value
     return value
 
 
 def simulate_missing_value_issues(value):
-    trigger = random.choices(
-        range(3),
-        weights=[0.7, 0.2, 0.1]
-    )[0]
-    return [value, "", None][trigger]
+    trigger = random.choices(range(3), weights=[0.7, 0.2, 0.1])[0]
+    return [value, '', None][trigger]
 
 
 def simulate_free_input_issues(value, is_missing=True, whitespace=True, caps=True):
