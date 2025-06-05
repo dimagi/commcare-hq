633fef7af6e0fd8d85fee22730389c2fab1d71c4
Biyeun Buczyk <biyeun@dimagi.com>
2025-06-05 16:19:00 +0200

    update another campaigns test

--- corehq/apps/campaign/tests/test_views.py
+++ corehq/apps/campaign/tests/test_views.py
@@ -575,8 +575,8 @@ class TestDeleteWidget(TestDashboardWidgetView):
             headers={'hq-hx-action': self.HQ_ACTION_DELETE_WIDGET},
         )
 
-        assert response.context['htmx_error'].status_code == 400
-        assert response.context['htmx_error'].message == "Requested widget type is not supported"
+        assert response.status_code == 400
+        assert response.content == b'Requested widget type is not supported'
         assert DashboardMap.objects.filter(pk=map_widget.id).exists() is True
 
 
