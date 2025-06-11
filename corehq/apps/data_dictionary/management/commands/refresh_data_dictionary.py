95a84b7b41c2278f995889f436c8c47d70695566
Jing Cheng <jcheng@dimagi.com>
2025-06-10 23:21:10 -0400

    Set migration status for all domains

--- corehq/apps/data_dictionary/management/commands/refresh_data_dictionary.py
+++ corehq/apps/data_dictionary/management/commands/refresh_data_dictionary.py
@@ -7,10 +7,13 @@ from corehq.apps.data_cleaning.utils.cases import (
 )
 from corehq.apps.domain.models import Domain
 from corehq.apps.domain_migration_flags.api import (
+    ALL_DOMAINS,
     get_migration_complete,
+    get_migration_status,
     set_migration_complete,
     set_migration_started,
 )
+from corehq.apps.domain_migration_flags.models import MigrationStatus
 from corehq.util.log import with_progress_bar
 
 
@@ -23,7 +26,15 @@ class Command(BaseCommand):
             help="Domain name(s). If blank, will refresh for all domains")
 
     def handle(self, **options):
+        if get_migration_complete(ALL_DOMAINS, 'refresh_data_dictionary'):
+            print("Migration already complete")
+            return
+
+        if get_migration_status(ALL_DOMAINS, 'refresh_data_dictionary') == MigrationStatus.NOT_STARTED:
+            set_migration_started(ALL_DOMAINS, 'refresh_data_dictionary')
+
         domains = options['domains'] or Domain.get_all_names()
+        success = True
 
         for domain in with_progress_bar(domains):
             if get_migration_complete(domain, 'refresh_data_dictionary'):
@@ -36,8 +47,14 @@ class Command(BaseCommand):
                         _refresh_data_dictionary_from_app(domain, app.get_id)
                     except Exception as e:
                         print(f'Failed to refresh app {app.get_id} in domain {domain}: {str(e)}')
+                        success = False
                 clear_caches_case_data_cleaning(domain)
 
             except Exception as e:
                 print(f'Failed to get apps in domain {domain}: {str(e)}')
+                success = False
             set_migration_complete(domain, 'refresh_data_dictionary')
+
+        if success:
+            set_migration_complete(ALL_DOMAINS, 'refresh_data_dictionary')
+            print("All domains and apps processed successfully!")
