d5e06cd9b815ce59592881af783814bae0d152d7
Jing Cheng <jcheng@dimagi.com>
2025-06-12 12:47:11 -0400

    Automatically rerun the command if migration is not completed for all domains

--- corehq/apps/data_dictionary/migrations/0017_refresh_data_dictionary.py
+++ corehq/apps/data_dictionary/migrations/0017_refresh_data_dictionary.py
@@ -1,14 +1,17 @@
 from django.db import migrations
-
 from django.core.management import call_command
+from corehq.apps.data_dictionary.management.commands.refresh_data_dictionary import MIGRATION_SLUG
 from corehq.util.django_migrations import skip_on_fresh_install
+from corehq.apps.domain_migration_flags.api import (
+    ALL_DOMAINS,
+    get_migration_complete,
+)
 
 
 @skip_on_fresh_install
 def refresh_data_dictionary(apps, schema_editor):
-    call_command(
-        "refresh_data_dictionary",
-    )
+    while not get_migration_complete(ALL_DOMAINS, MIGRATION_SLUG):
+        call_command("refresh_data_dictionary")
 
 
 class Migration(migrations.Migration):
