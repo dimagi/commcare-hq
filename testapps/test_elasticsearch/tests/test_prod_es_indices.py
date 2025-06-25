2ac7f457d298e929f6232193eb41b237956b0b6a
Minha <mlee@dimagi.com>
2025-06-25 10:49:02 -0400

    Another test fix

--- testapps/test_elasticsearch/tests/test_prod_es_indices.py
+++ testapps/test_elasticsearch/tests/test_prod_es_indices.py
@@ -143,6 +143,43 @@ EXPECTED_PROD_INDICES = [
         }
     },
     {
+        "index": "test_case-search-cc-perf-2025-06-19",
+        "type": "case",
+        "hq_index_name": "case_search_cc_perf",
+        "meta": {
+            "settings": {
+                "analysis": {
+                    "analyzer": {
+                        "default": {
+                            "type": "custom",
+                            "tokenizer": "whitespace",
+                            "filter": [
+                                "lowercase"
+                            ]
+                        },
+                        "phonetic": {
+                            "filter": [
+                                "standard",
+                                "lowercase",
+                                "soundex"
+                            ],
+                            "tokenizer": "standard"
+                        }
+                    },
+                    "filter": {
+                        "soundex": {
+                            "replace": "true",
+                            "type": "phonetic",
+                            "encoder": "soundex"
+                        }
+                    }
+                },
+                "number_of_replicas": 0,
+                "number_of_shards": 1,
+            }
+        }
+    },
+    {
         "hq_index_name": "hqapps",
         "index": "test_apps-20230524",
         "type": "app",
