aa9676cef59f6499f3e610362a926dbdef6f1362
Daniel Miller <dmiller@dimagi.com>
2025-06-26 15:33:25 -0400

    DownloadBase.set_progress -> set_task_progress
    Add load source for metrics tracking. Previously it had an "unknown" source.

--- corehq/apps/hqcase/case_helper.py
+++ corehq/apps/hqcase/case_helper.py
@@ -1,7 +1,7 @@
 import uuid
 from dataclasses import dataclass
 from django.utils.translation import gettext_lazy as _
-from soil import DownloadBase
+from soil.progress import set_task_progress
 
 from corehq.apps.users.models import CouchUser
 from casexml.apps.case.mock import CaseBlock, IndexAttrs
@@ -290,8 +290,7 @@ class CaseCopier:
                 self.processed_cases[orig_case.case_id] = caseblock
                 self._add_to_submission_handler(caseblock)
 
-            if progress_task is not None:
-                DownloadBase.set_progress(progress_task, idx, len(case_ids))
+            set_task_progress(progress_task, idx, len(case_ids), src="copy_cases")
 
         self._commit_cases()
 
