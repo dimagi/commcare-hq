3314afe07c99656b2d0bfd0cc49bd748dd09f98f
Cal Ellowitz <calellowitz@gmail.com>
2025-07-07 15:25:30 -0400

    renumber migration

--- corehq/motech/repeaters/migrations/0019_connectformrepeater.py
+++ corehq/motech/repeaters/migrations/0019_connectformrepeater.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('repeaters', '0017_add_indexes'),
+        ('repeaters', '0018_datasourceupdate'),
     ]
 
     operations = [
