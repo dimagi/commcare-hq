cb3b71726eb6551f8603312cffe6a769b23a5baa
Jing Cheng <jcheng@dimagi.com>
2025-07-24 18:21:21 -0400

    assert instead of self.assert...

--- corehq/apps/accounting/tests/test_utils.py
+++ corehq/apps/accounting/tests/test_utils.py
@@ -7,66 +7,66 @@ from corehq.apps.accounting.utils import is_date_range_overlapping
 
 class TestIsDateRangeOverlapping(SimpleTestCase):
     def test_first_range_is_contained_in_second_range(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 3), date(2025, 1, 6),
-                                                  date(2025, 1, 1), date(2025, 1, 10)))
+        assert is_date_range_overlapping(date(2025, 1, 3), date(2025, 1, 6),
+                                         date(2025, 1, 1), date(2025, 1, 10))
 
     def test_second_range_is_contained_in_first_range(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                  date(2025, 1, 5), date(2025, 1, 7)))
+        assert is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                         date(2025, 1, 5), date(2025, 1, 7))
 
     def test_partial_overlap_start(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                  date(2024, 12, 20), date(2025, 1, 2)))
+        assert is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                         date(2024, 12, 20), date(2025, 1, 2))
 
     def test_partial_overlap_end(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                  date(2025, 1, 9), date(2025, 1, 20)))
+        assert is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                         date(2025, 1, 9), date(2025, 1, 20))
 
     def test_exact_overlap(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                  date(2025, 1, 1), date(2025, 1, 10)))
+        assert is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                         date(2025, 1, 1), date(2025, 1, 10))
 
     def test_no_overlap_before(self):
-        self.assertFalse(is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
-                                                   date(2025, 1, 1), date(2025, 1, 9)))
+        assert not is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
+                                             date(2025, 1, 1), date(2025, 1, 9))
 
     def test_no_overlap_after(self):
-        self.assertFalse(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 9),
-                                                   date(2025, 1, 10), date(2025, 1, 20)))
+        assert not is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 9),
+                                             date(2025, 1, 10), date(2025, 1, 20))
 
     def test_adjacent_ranges_do_not_overlap(self):
         # Two ranges that touch at a boundary is not considered an overlap.
         # This is a special case for our accounting system
-        self.assertFalse(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                   date(2025, 1, 10), date(2025, 1, 20)))
+        assert not is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                             date(2025, 1, 10), date(2025, 1, 20))
 
-        self.assertFalse(is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
-                                                   date(2025, 1, 1), date(2025, 1, 10)))
+        assert not is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
+                                             date(2025, 1, 1), date(2025, 1, 10))
 
     def test_same_start_date_is_overlap(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 5), date(2025, 1, 10),
-                                                  date(2025, 1, 5), date(2025, 1, 15)))
+        assert is_date_range_overlapping(date(2025, 1, 5), date(2025, 1, 10),
+                                         date(2025, 1, 5), date(2025, 1, 15))
 
     def test_same_end_date_is_overlap(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
-                                                  date(2025, 1, 5), date(2025, 1, 10)))
+        assert is_date_range_overlapping(date(2025, 1, 1), date(2025, 1, 10),
+                                         date(2025, 1, 5), date(2025, 1, 10))
 
     def test_first_range_infinite_end(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), None,
-                                                  date(2025, 1, 10), date(2025, 1, 20)))
+        assert is_date_range_overlapping(date(2025, 1, 1), None,
+                                         date(2025, 1, 10), date(2025, 1, 20))
 
     def test_second_range_infinite_end(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
-                                                  date(2025, 1, 1), None))
+        assert is_date_range_overlapping(date(2025, 1, 10), date(2025, 1, 20),
+                                         date(2025, 1, 1), None)
 
     def test_both_ranges_infinite_end(self):
-        self.assertTrue(is_date_range_overlapping(date(2025, 1, 1), None,
-                                                  date(2025, 2, 1), None))
+        assert is_date_range_overlapping(date(2025, 1, 1), None,
+                                         date(2025, 2, 1), None)
 
     def test_first_range_infinite_end_but_start_after_second_range_end(self):
-        self.assertFalse(is_date_range_overlapping(date(2025, 1, 1), None,
-                                                   date(2024, 1, 1), date(2024, 12, 31)))
+        assert not is_date_range_overlapping(date(2025, 1, 1), None,
+                                             date(2024, 1, 1), date(2024, 12, 31))
 
     def test_second_range_infinite_end_but_start_after_first_range_end(self):
-        self.assertFalse(is_date_range_overlapping(date(2024, 1, 1), date(2024, 12, 31),
-                                                   date(2025, 1, 1), None))
+        assert not is_date_range_overlapping(date(2024, 1, 1), date(2024, 12, 31),
+                                             date(2025, 1, 1), None)
