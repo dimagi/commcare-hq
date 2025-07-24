25852b44179f22ffdc863c94d8987c0bbc63b700
Biyeun Buczyk <biyeun@dimagi.com>
2025-07-24 19:13:41 +0700

    modernize assert statements

--- corehq/apps/data_cleaning/tests/test_session.py
+++ corehq/apps/data_cleaning/tests/test_session.py
@@ -48,37 +48,37 @@ class BulkEditSessionTest(TestCase):
         active_session = BulkEditSession.objects.active_case_session(
             self.django_user, self.domain_name, self.case_type
         )
-        self.assertIsNone(active_session)
+        assert active_session is None
 
     def test_has_no_active_form_session(self):
         active_session = BulkEditSession.objects.active_form_session(
             self.django_user, self.domain_name, self.form_xmlns
         )
-        self.assertIsNone(active_session)
+        assert active_session is None
 
     def test_new_case_session(self):
         new_session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
-        self.assertEqual(new_session.session_type, BulkEditSessionType.CASE)
-        self.assertEqual(new_session.columns.count(), 6)
-        self.assertEqual(new_session.filters.count(), 0)
-        self.assertEqual(new_session.pinned_filters.count(), 2)
-        self.assertEqual(new_session.records.count(), 0)
-        self.assertEqual(new_session.changes.count(), 0)
+        assert new_session.session_type == BulkEditSessionType.CASE
+        assert new_session.columns.count() == 6
+        assert new_session.filters.count() == 0
+        assert new_session.pinned_filters.count() == 2
+        assert new_session.records.count() == 0
+        assert new_session.changes.count() == 0
 
     def test_has_active_case_session(self):
         BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
         active_session = BulkEditSession.objects.active_case_session(
             self.django_user, self.domain_name, self.case_type
         )
-        self.assertIsNotNone(active_session)
-        self.assertEqual(active_session.user, self.django_user)
-        self.assertEqual(active_session.domain, self.domain_name)
-        self.assertEqual(active_session.identifier, self.case_type)
+        assert active_session is not None
+        assert active_session.user == self.django_user
+        assert active_session.domain == self.domain_name
+        assert active_session.identifier == self.case_type
 
     def test_has_no_active_case_session_other_type(self):
         BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
         active_session = BulkEditSession.objects.active_case_session(self.django_user, self.domain_name, 'other')
-        self.assertIsNone(active_session)
+        assert active_session is None
 
     def test_has_no_active_case_session_after_committed(self):
         case_session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -87,7 +87,7 @@ class BulkEditSessionTest(TestCase):
         active_session = BulkEditSession.objects.active_case_session(
             self.django_user, self.domain_name, self.case_type
         )
-        self.assertIsNone(active_session)
+        assert active_session is None
 
     def test_has_no_active_case_session_after_completed(self):
         case_session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -97,7 +97,7 @@ class BulkEditSessionTest(TestCase):
         active_session = BulkEditSession.objects.active_case_session(
             self.django_user, self.domain_name, self.case_type
         )
-        self.assertIsNone(active_session)
+        assert active_session is None
 
     def test_restart_case_session(self):
         old_session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -107,7 +107,7 @@ class BulkEditSessionTest(TestCase):
             self.domain_name,
             self.case_type,
         )
-        self.assertNotEqual(old_session_id, new_session.session_id)
+        assert old_session_id != new_session.session_id
 
 
 @es_test(requires=[case_search_adapter, user_adapter, group_adapter], setup_class=True)
@@ -142,8 +142,8 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
         session.add_filter('height_cm', DataType.DECIMAL, FilterMatchType.LESS_THAN_EQUAL, '11.0')
         filters = session.filters.all()
         for index, prop_id in enumerate(['watered_on', 'name', 'num_leaves', 'pot_type', 'height_cm']):
-            self.assertEqual(filters[index].prop_id, prop_id)
-            self.assertEqual(filters[index].index, index)
+            assert filters[index].prop_id == prop_id
+            assert filters[index].index == index
 
     def test_remove_filters(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -153,24 +153,24 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
         session.add_filter('pot_type', DataType.DATE, FilterMatchType.IS_EMPTY)
         session.add_filter('height_cm', DataType.DECIMAL, FilterMatchType.LESS_THAN_EQUAL, '11.0')
         filter_to_remove = session.filters.all()[1]  # name
-        self.assertEqual(filter_to_remove.prop_id, 'name')
+        assert filter_to_remove.prop_id == 'name'
         session.remove_filter(filter_to_remove.filter_id)
         filters = session.filters.all()
-        self.assertEqual(len(filters), 4)
+        assert len(filters) == 4
         for index, prop_id in enumerate(['watered_on', 'num_leaves', 'pot_type', 'height_cm']):
-            self.assertEqual(filters[index].prop_id, prop_id)
-            self.assertEqual(filters[index].index, index)
+            assert filters[index].prop_id == prop_id
+            assert filters[index].index == index
 
     def test_remove_column(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
         column_to_remove = session.columns.all()[1]  # owner_name
-        self.assertEqual(column_to_remove.prop_id, 'owner_name')
+        assert column_to_remove.prop_id == 'owner_name'
         session.remove_column(column_to_remove.column_id)
         columns = session.columns.all()
-        self.assertEqual(len(columns), 5)
+        assert len(columns) == 5
         for index, prop_id in enumerate(['name', 'date_opened', 'opened_by_username', 'last_modified', '@status']):
-            self.assertEqual(columns[index].prop_id, prop_id)
-            self.assertEqual(columns[index].index, index)
+            assert columns[index].prop_id == prop_id
+            assert columns[index].index == index
 
     def test_reorder_wrong_number_of_filter_ids_raises_error(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -206,7 +206,7 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
         ]
         session.update_filter_order(new_order)
         reordered_prop_ids = [c.prop_id for c in session.filters.all()]
-        self.assertEqual(reordered_prop_ids, ['name', 'watered_on', 'num_leaves', 'height_cm', 'pot_type'])
+        assert reordered_prop_ids == ['name', 'watered_on', 'num_leaves', 'height_cm', 'pot_type']
 
     def test_reorder_wrong_number_of_column_ids_raises_error(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -233,10 +233,14 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
         ]
         session.update_column_order(new_order)
         reordered_prop_ids = [c.prop_id for c in session.columns.all()]
-        self.assertEqual(
-            reordered_prop_ids,
-            ['owner_name', 'name', 'date_opened', 'last_modified', '@status', 'opened_by_username'],
-        )
+        assert reordered_prop_ids == [
+            'owner_name',
+            'name',
+            'date_opened',
+            'last_modified',
+            '@status',
+            'opened_by_username',
+        ]
 
     def test_get_queryset_multiple_filters(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -256,7 +260,7 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
                 self.domain_name, "phonetic-match(name, 'lowkey') and num_leaves > 2 and height_cm <= 11.1"
             )
         )
-        self.assertEqual(query.es_query, expected_query.es_query)
+        assert query.es_query == expected_query.es_query
 
     def test_get_queryset_filters_no_xpath(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -268,7 +272,7 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
             .case_type(self.case_type)
             .NOT(case_property_missing('watered_on'))
         )
-        self.assertEqual(query.es_query, expected_query.es_query)
+        assert query.es_query == expected_query.es_query
 
     def test_get_queryset_filters_xpath_only(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
@@ -280,39 +284,39 @@ class BulkEditSessionFilteredQuerysetTests(TestCase):
             .case_type(self.case_type)
             .xpath_query(self.domain_name, 'num_leaves > 2')
         )
-        self.assertEqual(query.es_query, expected_query.es_query)
+        assert query.es_query == expected_query.es_query
 
     def test_has_filters_and_reset(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
-        self.assertFalse(session.has_filters)
+        assert not session.has_filters
         session.add_filter('num_leaves', DataType.INTEGER, FilterMatchType.GREATER_THAN, '2')
-        self.assertTrue(session.has_filters)
+        assert session.has_filters
         session.reset_filters()
-        self.assertFalse(session.has_filters)
+        assert not session.has_filters
 
     def test_has_pinned_values_and_reset(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
-        self.assertFalse(session.has_pinned_values)
+        assert not session.has_pinned_values
         pinned_filter = session.pinned_filters.all()[0]
         pinned_filter.value = ['t__1']
         pinned_filter.save()
-        self.assertTrue(session.has_pinned_values)
+        assert session.has_pinned_values
         session.reset_pinned_filters()
-        self.assertFalse(session.has_pinned_values)
+        assert not session.has_pinned_values
 
     def test_has_any_filtering_and_reset(self):
         session = BulkEditSession.objects.new_case_session(self.django_user, self.domain_name, self.case_type)
-        self.assertFalse(session.has_any_filtering)
+        assert not session.has_any_filtering
         pinned_filter = session.pinned_filters.all()[0]
         pinned_filter.value = ['t__1']
         pinned_filter.save()
-        self.assertTrue(session.has_any_filtering)
+        assert session.has_any_filtering
         session.reset_filtering()
-        self.assertFalse(session.has_any_filtering)
+        assert not session.has_any_filtering
         session.add_filter('num_leaves', DataType.INTEGER, FilterMatchType.GREATER_THAN, '2')
-        self.assertTrue(session.has_any_filtering)
+        assert session.has_any_filtering
         session.reset_filtering()
-        self.assertFalse(session.has_any_filtering)
+        assert not session.has_any_filtering
 
 
 class BaseBulkEditSessionTest(TestCase):
@@ -344,23 +348,23 @@ class BulkEditSessionCaseColumnTests(BaseBulkEditSessionTest):
     domain_name = 'session-test-case-columns'
 
     def test_add_column(self):
-        self.assertEqual(self.session.columns.count(), 6)
+        assert self.session.columns.count() == 6
         new_column = self.session.add_column('num_leaves', 'Number of Leaves', DataType.INTEGER)
-        self.assertEqual(new_column.index, 6)
-        self.assertEqual(self.session.columns.count(), 7)
-        self.assertEqual(new_column.prop_id, 'num_leaves')
-        self.assertEqual(new_column.label, 'Number of Leaves')
-        self.assertEqual(new_column.data_type, DataType.INTEGER)
-        self.assertFalse(new_column.is_system)
+        assert new_column.index == 6
+        assert self.session.columns.count() == 7
+        assert new_column.prop_id == 'num_leaves'
+        assert new_column.label == 'Number of Leaves'
+        assert new_column.data_type == DataType.INTEGER
+        assert not new_column.is_system
 
     def test_add_system_column(self):
         new_column = self.session.add_column('@owner_id', 'Owner ID', DataType.INTEGER)
-        self.assertEqual(new_column.index, 6)
-        self.assertEqual(self.session.columns.count(), 7)
-        self.assertEqual(new_column.prop_id, '@owner_id')
-        self.assertEqual(new_column.label, 'Owner ID')
-        self.assertEqual(new_column.data_type, DataType.TEXT)
-        self.assertTrue(new_column.is_system)
+        assert new_column.index == 6
+        assert self.session.columns.count() == 7
+        assert new_column.prop_id == '@owner_id'
+        assert new_column.label == 'Owner ID'
+        assert new_column.data_type == DataType.TEXT
+        assert new_column.is_system
 
 
 class BulkEditSessionSelectionTests(BaseBulkEditSessionTest):
@@ -376,7 +380,7 @@ class BulkEditSessionSelectionTests(BaseBulkEditSessionTest):
             is_selected=False,
         )
         num_selected_records = self.session.get_num_selected_records()
-        self.assertEqual(num_selected_records, 2)
+        assert num_selected_records == 2
 
 
 class BulkEditSessionChangesTests(BaseBulkEditSessionTest):
