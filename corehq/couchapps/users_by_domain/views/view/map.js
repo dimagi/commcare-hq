22ad7b665f3d0e339606b0202826fef531e2b951
Martin Riese <mriese@dimagi.com>
2025-07-30 12:19:16 -0500

    Add view and migration

--- corehq/couchapps/users_by_domain/views/view/map.js
+++ corehq/couchapps/users_by_domain/views/view/map.js
@@ -1,13 +1,12 @@
 function (doc) {
+    var i, active;
     if (doc.base_doc === "CouchUser") {
+        active = doc.is_active ? "active" : "inactive";
         if (doc.doc_type === "WebUser") {
-            for (let i = 0; i < doc.domain_memberships.length; i += 1) {
-                const domainMembershipActive = doc.domain_memberships[i].is_active !== false;
-                const active = (doc.is_active && domainMembershipActive) ? "active" : "inactive";
+            for (i = 0; i < doc.domain_memberships.length; i += 1) {
                 emit([active, doc.domain_memberships[i].domain, doc.doc_type, doc.username],  null);
             }
         } else if (doc.doc_type === "CommCareUser") {
-            const active = doc.is_active ? "active" : "inactive";
             emit([active, doc.domain, doc.doc_type, doc.username], null);
         }
     }
