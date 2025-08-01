b7199771b3485cfad47c5247ee44d7d13fe1115c
Jonathan Tang <Jonahtang738@gmail.com>
2025-07-31 17:16:37 -0700

    store processed ids so it can handle retries

--- corehq/ex-submodules/pillowtop/management/commands/sync_es_users.py
+++ corehq/ex-submodules/pillowtop/management/commands/sync_es_users.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management.base import BaseCommand
 from elasticsearch6.exceptions import NotFoundError
 
@@ -13,6 +15,8 @@ from corehq.util.log import with_progress_bar
 class Command(BaseCommand):
     help = "Sync users in Elasticsearch with Couchdb (WebUser or CommCareUser)"
 
+    SYNC_ES_USERS_PROGRESS_FILENAME = 'sync_es_users_processed_doc_ids.txt'
+
     def add_arguments(self, parser):
         parser.add_argument(
             '--doc_types',
@@ -25,10 +29,30 @@ class Command(BaseCommand):
         doc_type = options['doc_types']
         user_docs = get_all_docs_with_doc_types(db=CouchUser.get_db(), doc_types=[doc_type])
         doc_count = get_doc_count_by_type(CouchUser.get_db(), doc_type)
-        for user_doc in with_progress_bar(user_docs, doc_count):
-            try:
-                update_user_in_es(None, CouchUser.wrap_correctly(user_doc))
-            except NotFoundError as e:
-                self.stdout.write(self.style.WARNING(
-                    f"User not found in Elasticsearch: {user_doc.get('_id')} - {str(e)}"
-                ))
+
+        processed_ids = set()
+        processed_count = 0
+        try:
+            with open(self.SYNC_ES_USERS_PROGRESS_FILENAME, 'r') as f:
+                for line in f:
+                    processed_ids.add(line.strip())
+        except FileNotFoundError:
+            pass
+
+        with open(self.SYNC_ES_USERS_PROGRESS_FILENAME, 'a') as f:
+            for user_doc in with_progress_bar(user_docs, doc_count):
+                user_id = user_doc.get('_id')
+                if user_id in processed_ids:
+                    continue
+                try:
+                    update_user_in_es(None, CouchUser.wrap_correctly(user_doc))
+                    f.write(user_id + '\n')
+                    processed_count += 1
+                    if processed_count % 100 == 0:
+                        f.flush()
+                except NotFoundError as e:
+                    self.stdout.write(self.style.WARNING(
+                        f"User not found in Elasticsearch: {user_doc.get('_id')} - {str(e)}"
+                    ))
+            f.flush()
+        os.remove(self.SYNC_ES_USERS_PROGRESS_FILENAME)
