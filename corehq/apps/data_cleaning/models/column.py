e6b3c10d9a049f0050b6d8fa90a84e2aef8ca4f0
Biyeun Buczyk <biyeun@dimagi.com>
2025-08-04 16:02:50 -0400

    fix copy_to_session behavior for existing columns with the same prop_id - two columns with the same prop_id should never be a part of the same session

--- corehq/apps/data_cleaning/models/column.py
+++ corehq/apps/data_cleaning/models/column.py
@@ -44,14 +44,24 @@ class BulkEditColumnManager(models.Manager):
 
     def copy_to_session(self, source_session, dest_session):
         for column in self.filter(session=source_session):
-            self.model.objects.create(
+            existing = self.model.objects.filter(
                 session=dest_session,
-                index=column.index,
                 prop_id=column.prop_id,
-                label=column.label,
-                data_type=column.data_type,
-                is_system=column.is_system,
-            )
+            ).first()
+            if existing:
+                existing.label = column.label
+                existing.data_type = column.data_type
+                existing.index = column.index
+                existing.save()
+            else:
+                self.model.objects.create(
+                    session=dest_session,
+                    index=column.index,
+                    prop_id=column.prop_id,
+                    label=column.label,
+                    data_type=column.data_type,
+                    is_system=column.is_system,
+                )
 
     def create_for_session(self, session, prop_id, label, data_type=None):
         is_system_property = self.model.is_system_property(prop_id)
