9b3ad008f82a7bf17d4ba732fcff144fbb0f59f1
Ajeet <ayadav@dimagi.com>
2025-08-20 16:55:55 +0530

    optmise batch_number filter to use aggregation instead of fetching all documents from ES

--- corehq/apps/integration/payments/filters.py
+++ corehq/apps/integration/payments/filters.py
@@ -3,25 +3,9 @@ from django.utils.translation import gettext as _
 from corehq.apps.case_importer.const import MOMO_PAYMENT_CASE_TYPE
 from corehq.apps.es.case_search import get_case_property_unique_values
 from corehq.apps.integration.payments.const import PaymentStatus, PaymentProperties
-from corehq.apps.integration.payments.services import (
-    get_payment_batch_numbers_for_domain,
-)
 from corehq.apps.reports.filters.base import BaseSingleOptionFilter
 from corehq.apps.reports.filters.case_list import CaseListFilter
 from corehq.apps.reports.filters.users import WebUserFilter
-
-
-class BatchNumberFilter(BaseSingleOptionFilter):
-    slug = "batch_number"
-    label = _("Batch number")
-    default_text = _("Show all")
-
-    @property
-    def options(self):
-        batch_numbers = get_payment_batch_numbers_for_domain(self.domain)
-        return [
-            (batch_number, batch_number) for batch_number in batch_numbers
-        ]
 
 
 class PaymentVerifiedByFilter(WebUserFilter):
@@ -54,6 +38,12 @@ class BaseCasePropertyFilter(BaseSingleOptionFilter):
             self.case_property,
         )
         return [(value, value) for value in sorted(values)]
+
+
+class BatchNumberFilter(BaseCasePropertyFilter):
+    slug = "batch_number"
+    label = _("Batch number")
+    case_property = PaymentProperties.BATCH_NUMBER
 
 
 class CampaignFilter(BaseCasePropertyFilter):
