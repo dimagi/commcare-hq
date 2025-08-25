138c4eee467bd2f43ff855b2f7bdaf091720661c
Jing Cheng <jcheng@dimagi.com>
2025-08-25 17:28:51 -0400

    Add javascript to handle modal interaction

---
+++ corehq/apps/domain/static/domain/js/ocs_chatbot_enable_modal.js
@@ -0,0 +1,50 @@
+import "commcarehq";
+import $ from "jquery";
+import ko from 'knockout';
+import "hqwebapp/js/bootstrap5/knockout_bindings.ko";
+
+function ToSModalViewModel() {
+    const self = this;
+
+    self.isTosConfirmed = ko.observable(false);
+    self.isChatbotEnabled = ko.observable(false);
+
+    self.isEnableButtonEnabled = ko.computed(() => {
+        return self.isTosConfirmed();
+    });
+
+    self.enableChatbot = function () {
+        if (self.isTosConfirmed()) {
+            self.isChatbotEnabled(true);
+            $('#ocs_chatbot_checkbox').prop('checked', true);
+            $('#ocsChatbotModal').modal('hide');
+        }
+    };
+
+    self.resetModal = function () {
+        self.isTosConfirmed(false);
+    };
+}
+
+function initModalBindings() {
+    const viewModel = new ToSModalViewModel();
+
+    ko.applyBindings(viewModel, document.getElementById('ocsChatbotModal'));
+
+    const checkbox = $('#ocs_chatbot_checkbox');
+    viewModel.isChatbotEnabled(checkbox.prop('checked'));
+
+    $('#ocsChatbotModal').on('hidden.bs.modal', function () {
+        viewModel.resetModal();
+    });
+
+    $('#ocs_chatbot_checkbox').on('mousedown', function () {
+        if (!this.checked) {
+            $('#ocsChatbotModal').modal('show');
+        }
+    });
+}
+
+$(function () {
+    initModalBindings();
+});
