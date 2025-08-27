5a966a4b5d58ec7697018e592c816bff43ed9c47
Jing Cheng <jcheng@dimagi.com>
2025-08-27 12:04:54 -0400

    Update test

--- corehq/apps/app_manager/tests/test_views.py
+++ corehq/apps/app_manager/tests/test_views.py
@@ -688,6 +688,7 @@ class TestViewGeneric(ViewsBase):
         'commcare_hq_names', 'langs', 'title_context_block', 'timezone', 'has_mobile_workers',
         'multimedia_state', 'bulk_app_translation_upload', 'show_training_modules', 'forloop', 'secure_cookies',
         'IS_ANALYTICS_ENVIRONMENT', 'module_type', 'icon_class', 'form_submit_history_url', 'btn_style',
+        'chat_widget_config',
     }
 
     expected_keys_module = {
@@ -746,7 +747,7 @@ class TestViewGeneric(ViewsBase):
         'env', 'False', 'ANALYTICS_IDS', 'STATIC_URL', 'selected_module', 'role_version', 'is_usercase_in_use',
         'module_loads_registry_case', 'EULA_COMPLIANCE', 'sentry', 'show_shadow_modules', 'show_custom_ref',
         'block', 'IS_ANALYTICS_ENVIRONMENT', 'module_type', 'icon_class', 'case_property_warning',
-        'form_submit_history_url', 'btn_style',
+        'form_submit_history_url', 'btn_style', 'chat_widget_config',
     }
 
 
