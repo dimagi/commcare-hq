debf6a2ca6917215d3aea92a36db3042870343c3
Norman Hooper <nhooper@dimagi.com>
2025-09-05 16:57:32 +0100

    Better ES filter. Delete apps with bad forms.

--- subs_app_versions.py
+++ subs_app_versions.py
@@ -4,8 +4,10 @@ invalid build versions in Application.built_with.version.
 
 Context: https://dimagi.atlassian.net/browse/SAAS-18157
 """
+from datetime import datetime
+from typing import NamedTuple
 from couchdbkit.exceptions import BadValueError
-from corehq.apps.app_manager.models import Application
+from corehq.apps.app_manager.models import Application, DeleteApplicationRecord
 from corehq.apps.builds.models import SemanticVersionProperty
 from corehq.apps.es import filters
 from corehq.apps.es.apps import AppES
@@ -17,35 +19,64 @@ VERSION_SUBSTITUTIONS = {
     '1.3.0alpha': '1.3.999',
     '2.25.w': '2.25.999',
 }
-APP_IDS = [
-    # (domain, app_id)
-    # Output from previous runs
-]
+
+
+class AppTuple(NamedTuple):
+    domain: str
+    app_id: str
+    copy_of: str  # Empty string for non-builds
 
 
 def subs_app_versions(*, dry_run=True):
     print('Dry run' if dry_run else 'Live')
     modified = []
-    for domain, app_id in APP_IDS:
-        for hit in (
-            AppES()
-            .doc_type('Application')
-            .domain(domain)
-            .app_id(app_id)  # copy_of
-            .OR(
-                filters.NOT(filters.empty('build_spec.version')),
-                filters.NOT(filters.empty('built_with.version')),
-            )
-            .scroll()
-        ):
-            if (
-                not is_version_ok(get_by_path(hit, ('build_spec', 'version')))
-                or not is_version_ok(get_by_path(hit, ('built_with', 'version')))
-            ):
-                modified.append((hit['domain'], hit['doc_id'], hit['copy_of']))
-                if not dry_run:
-                    subs_bad_version(hit['doc_id'])
+    apps = get_apps_with_bad_versions()
+    for app_tuple in apps:
+        modified.append(app_tuple)
+        builds = get_builds_with_bad_versions(app_tuple.domain, app_tuple.app_id)
+        modified.extend(builds)
+        if not dry_run:
+            subs_bad_version(app_tuple.app_id)
+            for build_tuple in builds:
+                subs_bad_version(build_tuple.app_id)
     print_table(modified)
+
+
+def get_apps_with_bad_versions():
+    bad_versions = list(VERSION_SUBSTITUTIONS.keys())
+    apps = []
+    for hit in (
+        AppES()
+        .doc_type('Application')
+        .is_build(build=False)
+        .OR(
+            filters.term('build_spec.version', bad_versions),
+            filters.term('built_with.version', bad_versions),
+        )
+        .run()
+        .hits
+    ):
+        apps.append(AppTuple(hit['domain'], hit['doc_id'], ''))
+    return apps
+
+
+def get_builds_with_bad_versions(domain, app_id):
+    bad_versions = list(VERSION_SUBSTITUTIONS.keys())
+    builds = []
+    for hit in (
+        AppES()
+        .doc_type('Application')
+        .domain(domain)
+        .app_id(app_id)  # copy_of
+        .OR(
+            filters.term('build_spec.version', bad_versions),
+            filters.term('built_with.version', bad_versions),
+        )
+        .run()
+        .hits
+    ):
+        builds.append(AppTuple(domain, hit['doc_id'], app_id))
+    return builds
 
 
 def is_version_ok(version):
@@ -87,19 +118,34 @@ def subs_bad_version(app_id):
         app_dict['built_with']['version'] = subs
     try:
         app = Application.wrap(app_dict)
-    except BadValueError as err:
-        print(f"Skipping {app_dict['domain']} | {app_id}:\n{err}\n")
-        return
-    app.save()
+        app.save()
+        print(f'{app_id} saved')
+    except BadValueError:
+        # App has multiple validation errors. (Invariably means that a
+        # form has been stored as a string and not JSON.)
+        delete_app(app_dict)
+        print(f'{app_id} deleted')
 
 
-def print_table(app_id_tuples):
-    if not app_id_tuples:
+def delete_app(app_dict):
+    # See ApplicationBase.delete_app()
+    app_dict['doc_type'] += '-Deleted'
+    Application.get_db().save_doc(app_dict)
+    record = DeleteApplicationRecord(
+        domain=app_dict['domain'],
+        app_id=app_dict['_id'],
+        datetime=datetime.utcnow()
+    )
+    record.save()
+
+
+def print_table(app_tuples):
+    if not app_tuples:
         return
     print('Affected apps:\n')
     print('| Domain       | App ID                           | Copy of                          |')
     print('| ------------ | -------------------------------- | -------------------------------- |')
-    for domain, app_id, copy_of in app_id_tuples:
+    for domain, app_id, copy_of in app_tuples:
         print(f'| {domain:<12} | {app_id:<32} | {copy_of:<32} |')
 
 
