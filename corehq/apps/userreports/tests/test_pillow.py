dimagi/commcare-hq

commit f47f94c55c13bb4840de669c310e3323a607493e

Use generators rather than allocating a new list for each filter
Unclear if performance or memory footprint is improved (didn't measure),
but generators are so concise. They make the code easier to read.

corehq/apps/userreports/pillow.py
@@ -83,22 +83,19 @@ def time_ucr_process_change(method):
 def _filter_by_hash(configs, ucr_division):
     ucr_start = ucr_division[0]
     ucr_end = ucr_division[-1]
-    filtered_configs = []
     for config in configs:
         table_hash = hashlib.md5(config.table_id.encode('utf-8')).hexdigest()[0]
         if ucr_start <= table_hash <= ucr_end:
-            filtered_configs.append(config)
-    return filtered_configs
+            yield config
 
 
 def _filter_domains_to_skip(configs):
     """Return a list of configs whose domain exists on this environment"""
     domain_names = list({config.domain for config in configs if config.is_static})
     existing_domains = list(get_domain_ids_by_names(domain_names))
-    return [
-        config for config in configs
-        if not config.is_static or config.domain in existing_domains
-    ]
+    for config in configs:
+        if not config.is_static or config.domain in existing_domains:
+            yield config
 
 
 def _filter_invalid_config(configs):
@@ -109,14 +106,12 @@ def _filter_invalid_config(configs):
     # cache. It is unclear if it is by design that subsequent changes
     # continue to be processed against the outdated (valid) version of
     # the config/adapter.
-    valid_configs = []
     for config in configs:
         try:
             config.validate()
-            valid_configs.append(config)
+            yield config
         except Exception:
             pillow_logging.warning("Invalid config found during bootstrap: %s", config._id)
-    return valid_configs
 
 
 def _get_indicator_adapter_for_pillow(config):
@@ -282,7 +277,7 @@ class ConfigurableReportTableManager(UcrTableManager):
     def iter_configs(self, domain):
         for provider in self.data_source_providers:
             configs = provider.get_data_sources(domain)
-            for config in self.get_filtered_configs(configs):
+            for config in self._filter_configs(configs):
                 yield (config.domain, config)
 
     def iter_configs_since(self, timestamp):
@@ -294,16 +289,16 @@ class ConfigurableReportTableManager(UcrTableManager):
         # view: userreports/data_sources_by_last_modified
         for provider in self.data_source_providers:
             configs = provider.get_data_sources_modified_since(timestamp)
-            for config in self.get_filtered_configs(configs):
+            for config in self._filter_configs(configs):
                 yield (config.domain, config)
 
-    def get_filtered_configs(self, configs):
+    def _filter_configs(self, configs):
         if configs:
             if self.exclude_ucrs:
-                configs = [config for config in configs if config.table_id not in self.exclude_ucrs]
+                configs = (config for config in configs if config.table_id not in self.exclude_ucrs)
 
             if self.include_ucrs:
-                configs = [config for config in configs if config.table_id in self.include_ucrs]
+                configs = (config for config in configs if config.table_id in self.include_ucrs)
             elif self.ucr_division:
                 configs = _filter_by_hash(configs, self.ucr_division)
 

commit 7d418e64e62c9f9441a8aa2dcad2b88ddd544073

Fix _filter_domains_to_skip(<generator>)
It returned no items because generator was exhausted on first pass.
Bug introduced in commit:f47f94c55c13bb4840de669c310e3323a607493e

corehq/apps/userreports/pillow.py
@@ -91,6 +91,7 @@ def _filter_by_hash(configs, ucr_division):
 
 def _filter_domains_to_skip(configs):
     """Return a list of configs whose domain exists on this environment"""
+    configs = list(configs)  # convert generator to list for multiple passes
     domain_names = list({config.domain for config in configs if config.is_static})
     existing_domains = list(get_domain_ids_by_names(domain_names))
     for config in configs:

corehq/apps/userreports/tests/test_pillow.py
@@ -152,6 +152,22 @@ class ConfigurableReportTableManagerDbTest(TestCase):
         adapters = table_manager.get_adapters(ds_1_domain)
         assert [a.config for a in adapters] == [data_source_1]
 
+    def test_table_adapters_for_migration_process(self):
+        # this test is for a regression in _filter_domains_to_skip
+        skip_domain_filter_patch.stop()  # unpatch _filter_domains_to_skip
+        self.addCleanup(skip_domain_filter_patch.start)
+        data_source_1 = get_sample_data_source()
+        ds_1_domain = data_source_1.domain
+        data_source_1.save()
+
+        table_manager = ConfigurableReportTableManager([MockDataSourceProvider({
+            ds_1_domain: [data_source_1]
+        })], run_migrations=True, ucr_division='0f')
+        table_manager.configure_dedicated_migration_process()
+
+        adapters = table_manager.migration_cache.get_adapters()
+        assert [a.config for a in adapters] == [data_source_1]
+
     @patch("corehq.apps.userreports.pillow.rebuild_sql_tables")
     def test_complete_integration(self, mock_rebuild_sql_tables):
         data_source_1 = get_sample_data_source()