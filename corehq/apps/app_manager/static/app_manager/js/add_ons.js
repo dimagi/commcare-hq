1ba54072618e28f8896e1f765b25e3c596af6e2e
Norman Hooper <nhooper@dimagi.com>
2025-09-18 15:57:11 +0100

    Modernize add_ons.js

--- corehq/apps/app_manager/static/app_manager/js/add_ons.js
+++ corehq/apps/app_manager/static/app_manager/js/add_ons.js
@@ -1,41 +1,42 @@
 import $ from "jquery";
-import _ from "underscore";
 import initialPageData from "hqwebapp/js/initial_page_data";
 import hqMain from "hqwebapp/js/bootstrap3/main";
 import sectionChanger from "app_manager/js/section_changer";
 
-function EditAddOns(addOns, layout, saveUrl) {
-    var self = this;
-
-    self.addOns = addOns;
-    self.sections = _.map(layout, function (s) {
-        return _.extend(s, {
+class EditAddOns {
+    constructor(addOns, layout, saveUrl) {
+        this.addOns = addOns;
+        this.sections = layout.map(s => ({
+            ...s,
             collapse: sectionChanger.shouldCollapse("add-ons", s.slug, s.collapse),
+        }));
+        this.saveButton = hqMain.initSaveButton({
+            unsavedMessage: gettext("You have unsaved changes."),
+            save: () => {
+                // Send server map of slug => enabled
+                const data = Object.fromEntries(
+                    Object.entries(this.addOns).map(([slug, enabled]) => [slug, enabled ? 'on' : ''])
+                );
+                this.saveButton.ajax({
+                    url: saveUrl,
+                    type: 'post',
+                    data,
+                    error: () => {
+                        throw gettext("There was an error saving.");
+                    },
+                });
+            },
         });
-    });
-    self.saveButton = hqMain.initSaveButton({
-        unsavedMessage: gettext("You have unsaved changes."),
-        save: function () {
-            // Send server map of slug => enabled
-            var data = _.mapObject(self.addOns, function (a) { return a ? 'on' : ''; });
-            self.saveButton.ajax({
-                url: saveUrl,
-                type: 'post',
-                data: data,
-                error: function () {
-                    throw gettext("There was an error saving.");
-                },
-            });
-        },
-    });
-    self.update = function (addOn, e) {
-        self.addOns[addOn.slug] = e.currentTarget.checked;
-        self.saveButton.fire('change');
-    };
+    }
+
+    update(addOn, e) {
+        this.addOns[addOn.slug] = e.currentTarget.checked;
+        this.saveButton.fire('change');
+    }
 }
 
-$(function () {
-    var $addOns = $("#add-ons");
+$(() => {
+    const $addOns = $("#add-ons");
     if ($addOns.length) {
         $addOns.koApplyBindings(new EditAddOns(
             initialPageData.get("add_ons"),
