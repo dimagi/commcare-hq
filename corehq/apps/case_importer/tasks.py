5d452e6a0aed6748f1e0bca862c463d3e83eba79
Graham Herceg <gherceg@dimagi.com>
2025-09-19 09:59:12 -0400

    Make bulk_import_async a serial task by domain
    Retry every 5 minutes for up to 62 times, which would last 10 to 15
    minutes longer than the task timeout

--- corehq/apps/case_importer/tasks.py
+++ corehq/apps/case_importer/tasks.py
@@ -5,6 +5,7 @@ from corehq.apps.hqadmin.tasks import (
     AbnormalUsageAlert,
     send_abnormal_usage_alert,
 )
+from corehq.util.decorators import serial_task
 from corehq.util.metrics import metrics_gauge_task
 from corehq.util.metrics.const import MPM_MAX
 from corehq.apps.data_dictionary.util import get_data_dict_deprecated_case_types
@@ -22,7 +23,10 @@ from .util import (
 )
 
 
-@task(queue='case_import_queue')
+# an import shouldn't last longer than 5 hours (capped at 100k cases)
+@serial_task(
+    '{domain}', default_retry_delay=60 * 5, max_retries=62, timeout=5 * 60 * 60, queue='case_import_queue'
+)
 def bulk_import_async(config_list_json, domain, excel_id):
     case_upload = CaseUpload.get(excel_id)
     result_stored = False
