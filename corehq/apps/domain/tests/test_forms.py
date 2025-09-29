e4261098999a26dd8af4d8946ff8378d0821eb18
Matt Riley <mriley@dimagi.com>
2025-09-29 17:42:11 -0400

    Refactored ExtractAppInfoForm tests to use override_settings

--- corehq/apps/domain/tests/test_forms.py
+++ corehq/apps/domain/tests/test_forms.py
@@ -1,7 +1,7 @@
 from datetime import date, timedelta
 from unittest.mock import Mock, patch, MagicMock
 
-from django.test import SimpleTestCase, TestCase
+from django.test import SimpleTestCase, TestCase, override_settings
 
 from dateutil.relativedelta import relativedelta
 
@@ -648,14 +648,14 @@ class TestExtractAppInfoForm(SimpleTestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('The URL must be from a valid CommCare server', str(form.errors['app_url']))
 
-    @patch('corehq.apps.domain.forms.settings.SERVER_ENVIRONMENT', 'production')
+    @override_settings(SERVER_ENVIRONMENT='production')
     def test_clean_app_url_same_server_validation(self):
         url = 'https://www.commcarehq.org/a/test-domain/apps/view/62891a383516c656850cc9c7e7b8d459/'
         form = forms.ExtractAppInfoForm(data={'app_url': url})
         self.assertFalse(form.is_valid())
         self.assertIn('The source app url matches the current server', str(form.errors['app_url']))
 
-    @patch('corehq.apps.domain.forms.settings.SERVER_ENVIRONMENT', 'india')
+    @override_settings(SERVER_ENVIRONMENT='india')
     def test_clean_app_url_different_server_validation(self):
         url = 'https://www.commcarehq.org/a/test-domain/apps/view/62891a383516c656850cc9c7e7b8d459/'
         form = forms.ExtractAppInfoForm(data={'app_url': url})
