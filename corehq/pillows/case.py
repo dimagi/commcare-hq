6e729b2a30058ef5c3bf9f4e293cb1ce424e2af8
Graham Herceg <gherceg@dimagi.com>
2025-10-01 11:18:47 -0400

    Add demo case pillow and topic

--- corehq/pillows/case.py
+++ corehq/pillows/case.py
@@ -18,7 +18,7 @@ from corehq.apps.change_feed.consumer.feed import (
     KafkaChangeFeed,
     KafkaCheckpointEventHandler,
 )
-from corehq.apps.change_feed.topics import CASE_TOPICS
+from corehq.apps.change_feed.topics import CASE_TOPICS, DEMO_CASE_SQL
 from corehq.apps.es.cases import case_adapter
 from corehq.apps.userreports.data_source_providers import (
     DynamicDataSourceProvider,
@@ -67,6 +67,15 @@ def get_case_to_elasticsearch_pillow(pillow_id='CaseToElasticsearchPillow', num_
     )
 
 
+def get_demo_case_pillow(pillow_id='demo-case-pillow', **kwargs):
+    return get_case_pillow(
+        pillow_id=pillow_id,
+        topics=[DEMO_CASE_SQL],
+        num_processes=kwargs.get('num_processes', 1),
+        processor_chunk_size=kwargs.get('processor_chunk_size', 1),
+    )
+
+
 def get_case_pillow(
     pillow_id='case-pillow',
     ucr_division=None,
@@ -78,6 +87,7 @@ def get_case_pillow(
     processor_chunk_size=DEFAULT_PROCESSOR_CHUNK_SIZE,
     topics=None,
     dedicated_migration_process=False,
+    include_demo_topic=False,
     **kwargs,
 ):
     """Return a pillow that processes cases. The processors include, UCR and elastic processors
@@ -89,8 +99,13 @@ def get_case_pillow(
       - :py:class:`corehq.messaging.pillow.CaseMessagingSyncProcessor`
     """
     if topics:
-        assert set(topics).issubset(CASE_TOPICS), "This is a pillow to process cases only"
-    topics = topics or CASE_TOPICS
+        expected_topics = set(CASE_TOPICS + (DEMO_CASE_SQL,))  # the demo case sql topic is only used here
+        assert set(topics).issubset(expected_topics), "This is a pillow to process cases only"
+    else:
+        if include_demo_topic:
+            topics = CASE_TOPICS + (DEMO_CASE_SQL,)
+        else:
+            topics = CASE_TOPICS
     change_feed = KafkaChangeFeed(
         topics, client_id=pillow_id, num_processes=num_processes, process_num=process_num,
         dedicated_migration_process=dedicated_migration_process
