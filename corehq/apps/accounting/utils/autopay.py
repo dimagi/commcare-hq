bd763dca651d893c087366be2becca235bf09dfd
Biyeun Buczyk <biyeun@dimagi.com>
2025-10-03 15:39:09 +0800

    squash: fix autopay util

--- corehq/apps/accounting/utils/autopay.py
+++ corehq/apps/accounting/utils/autopay.py
@@ -3,9 +3,9 @@ from stripe import Card
 from corehq.apps.accounting.models import BillingAccount, StripePaymentMethod
 
 
-def get_autopay_card_and_owner_for_billing_account(account: BillingAccount) -> None | Card:
+def get_autopay_card_and_owner_for_billing_account(account: BillingAccount) -> tuple[None | Card, None | str]:
     if not account.auto_pay_enabled:
-        return None
+        return None, None
 
     auto_payer = account.auto_pay_user
     payment_method = StripePaymentMethod.objects.get(web_user=auto_payer)
