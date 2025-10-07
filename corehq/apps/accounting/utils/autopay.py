f343b81ecf2f2bf3bc7677e2f8b68aed434b7399
Biyeun Buczyk <biyeun@dimagi.com>
2025-10-08 00:36:11 +0800

    removing typehints because controversy - :shrug_cat:

--- corehq/apps/accounting/utils/autopay.py
+++ corehq/apps/accounting/utils/autopay.py
@@ -1,25 +1,18 @@
-from stripe import Card
-
-from corehq.apps.accounting.models import BillingAccount, StripePaymentMethod
+from corehq.apps.accounting.models import StripePaymentMethod
 
 
-def get_autopay_card_and_owner_for_billing_account(account: BillingAccount) -> tuple[None | Card, None | str]:
+def get_autopay_card_and_owner_for_billing_account(account):
     if not account.auto_pay_enabled:
         return None, None
 
     auto_payer = account.auto_pay_user
     payment_method = StripePaymentMethod.objects.get(web_user=auto_payer)
 
-    # makes API call to Stripe:
+    # makes API call to Stripe. Returns a stripe.Card object
     autopay_card = payment_method.get_autopay_card(account)
     return autopay_card, auto_payer
 
 
-def set_card_as_autopay_for_billing_account(
-    payment_method: StripePaymentMethod,
-    card_token: str,
-    account: BillingAccount,
-    domain: str,
-) -> None:
+def set_card_as_autopay_for_billing_account(payment_method, card_token, account, domain):
     card = payment_method.get_card(card_token)
     payment_method.set_autopay(card, account, domain)
