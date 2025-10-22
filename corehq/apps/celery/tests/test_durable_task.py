ff8bbc38d73ae0744a9bdfb7f7a35c3bf86599b1
Graham Herceg <gherceg@dimagi.com>
2025-10-22 09:27:00 -0400

    Implement post task run updates to task records

--- corehq/apps/celery/tests/test_durable_task.py
+++ corehq/apps/celery/tests/test_durable_task.py
@@ -11,6 +11,7 @@ from celery.exceptions import OperationalError
 from django.test import TestCase
 
 from corehq.apps.celery import task
+from corehq.apps.celery.durable import update_task_record
 from corehq.apps.celery.models import TaskRecord
 
 
@@ -109,3 +110,57 @@ class TestDurableTask(TestCase):
         durable_task.delay()
         args, kwargs = self.mock_apply_async.call_args
         assert kwargs['headers']['durable']
+
+
+@attr.s(auto_attribs=True)
+class MockRequest:
+    id: str
+    headers: dict
+
+
+@attr.s(auto_attribs=True)
+class MockTask:
+    request: MockRequest
+
+
+class TestUpdateTaskRecord(TestCase):
+    def test_empty_task_does_not_raise_error(self):
+        update_task_record(task={})
+
+    def test_task_without_durable_flag(self):
+        task = MockTask(request=MockRequest(id=uuid.uuid4(), headers={}))
+        update_task_record(task=task, state=celery_states.SUCCESS)
+
+    def test_durable_task_record_is_deleted_when_in_ready_state(self):
+        # as defined by celery at the time of writing this test
+        ready_states = [
+            celery_states.SUCCESS,
+            celery_states.FAILURE,
+            celery_states.REVOKED,
+        ]
+        for state in ready_states:
+            task_id = uuid.uuid4()
+            TaskRecord.objects.create(task_id=task_id, name='test-task', sent=True, args={}, kwargs={})
+            task = MockTask(request=MockRequest(id=task_id, headers={'durable': True}))
+            update_task_record(task=task, state=state)
+            with pytest.raises(TaskRecord.DoesNotExist):
+                TaskRecord.objects.get(task_id=task_id)
+
+    def test_durable_task_record_is_not_deleted_when_in_unready_state(self):
+        # as defined by celery at the time of writing this test
+        unready_states = [
+            celery_states.PENDING,
+            celery_states.RECEIVED,
+            celery_states.STARTED,
+            celery_states.REJECTED,
+            celery_states.RETRY,
+        ]
+        for state in unready_states:
+            task_id = uuid.uuid4()
+            TaskRecord.objects.create(
+                task_id=task_id, name='test-task', sent=True, args={}, kwargs={}
+            )
+            task = MockTask(request=MockRequest(id=task_id, headers={'durable': True}))
+            update_task_record(task=task, state=state)
+            record = TaskRecord.objects.get(task_id=task_id)
+            assert record.sent
