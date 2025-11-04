484fe5c8a53cc93a71ec9de347c57ccfafb1fb17
Graham Herceg <gherceg@dimagi.com>
2025-11-04 13:59:22 -0500

    Minor improvements to TaskRecord class

--- corehq/apps/celery/models.py
+++ corehq/apps/celery/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class TaskRecord(models.Model):
+    """
+    Record to track a celery task/message prior to being processed by a worker.
+    Once processed by a worker, whether it fails or succeeds, this record is deleted.
+    """
     task_id = models.UUIDField(null=True)
     name = models.CharField(max_length=255)
     args = models.JSONField()
@@ -16,3 +20,6 @@ class TaskRecord(models.Model):
                 fields=["task_id"], condition=models.Q(task_id__isnull=False), name="unique_nonnull_task_id"
             )
         ]
+
+    def __str__(self):
+        return f"<{self.name}> {self.task_id} {self.date_created}"
