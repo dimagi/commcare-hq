df22ffede0bcd55d56a3f0713fcb1889de9cbb7e
Graham Herceg <gherceg@dimagi.com>
2025-11-04 13:58:34 -0500

    Replace TaskRecordState with boolean
    We really only need to track whether or not the task was sent to the
    broker since the record is deleted once the task is processed.

--- corehq/apps/celery/models.py
+++ corehq/apps/celery/models.py
@@ -1,26 +1,12 @@
 from django.db import models
 
 
-class TaskRecordState(models.TextChoices):
-    # the following are meant to replicate celery task states
-    # https://docs.celeryq.dev/en/latest/reference/celery.states.html
-    FAILURE = "FAILURE", "Failure"
-    PENDING = "PENDING", "Pending"  # task was created, but not yet sent to the broker
-    RECEIVED = "RECEIVED", "Received"  # task was received by a worker, not a broker
-    REJECTED = "REJECTED", "Rejected"  # not in docs, but set when task cannot be processed by worker
-    RETRY = "RETRY", "Retry"
-    REVOKED = "REVOKED", "Revoked"
-    SENT = "SENT", "Sent"  # commcare adds this state in a receiver for the after_task_publish signal
-    STARTED = "STARTED", "Started"
-    SUCCESS = "SUCCESS", "Success"
-
-
 class TaskRecord(models.Model):
     task_id = models.UUIDField(null=True)
     name = models.CharField(max_length=255)
     args = models.JSONField()
     kwargs = models.JSONField()
-    state = models.CharField(choices=TaskRecordState.choices, default=TaskRecordState.PENDING)
+    sent = models.BooleanField()  # if True, task was successfully sent to the broker
     date_created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
 
