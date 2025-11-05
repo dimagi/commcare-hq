74a7899b073e19f1ddc3b37dcd0adb8a874d830e
Graham Herceg <gherceg@dimagi.com>
2025-11-05 15:13:19 -0500

    Support existing TaskRecords for retried tasks
    If a task fails during processing and has logic to retry, the TaskRecord
    will be preserved and apply_async will be called again. So apply_async
    needs to handle this condition to update the existing record rather than
    attempt to create a new one, which would conflict on task_id.

--- corehq/apps/celery/durable.py
+++ corehq/apps/celery/durable.py
@@ -2,6 +2,7 @@ import kombu.utils.json as kombu_json
 from celery import Task
 from celery import states as celery_states
 from celery.signals import task_postrun
+from dimagi.utils.logging import notify_exception
 
 
 class UnsupportedSerializationError(Exception):
@@ -31,12 +32,27 @@ class DurableTask(Task):
         headers = opts.pop('headers', {}) or {}
         headers['durable'] = True
 
-        record = TaskRecord(
-            name=self.name,
-            args=kombu_json.dumps(args),
-            kwargs=kombu_json.dumps(kwargs),
-            sent=False,
-        )
+        record = None
+        task_id = opts.get('task_id', None)
+        if task_id:
+            # if a task calls `self.retry()`, it triggers another apply_async call
+            try:
+                record = TaskRecord.objects.get(task_id=task_id)
+            except TaskRecord.DoesNotExist as e:
+                notify_exception(
+                    None,
+                    f'Unexpectedly could not find TaskRecord object for id {task_id}',
+                    details={'error': str(e)},
+                )
+
+        if not record:
+            record = TaskRecord(
+                name=self.name,
+                args=kombu_json.dumps(args),
+                kwargs=kombu_json.dumps(kwargs),
+                sent=False,
+            )
+
         try:
             result = super().apply_async(args=args, kwargs=kwargs, headers=headers, **opts)
             record.task_id = result.task_id
