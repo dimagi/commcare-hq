8a2c984b0a0c4be38f95c6504fa80938ca272783
Manish Kangia <mkangia@dimagi.com>
2025-11-09 14:57:53 +0530

    limit just to pk

--- corehq/apps/reports/standard/deployments.py
+++ corehq/apps/reports/standard/deployments.py
@@ -381,7 +381,7 @@ class ApplicationStatusReport(GetParamsMixin, PaginatedReportMixin, DeploymentsR
         where = Q(domain=self.domain, location_id__in=location_ids)
         # include_self is ignored when Q is passed but added for code clarity
         locations_and_ancestors = SQLLocation.objects.get_ancestors(where, include_self=True)
-        location_by_location_id = {location.location_id: location for location in locations_and_ancestors}
+        location_pk_by_location_id = {location.location_id: location.pk for location in locations_and_ancestors}
         location_by_pk = {location.pk: location for location in locations_and_ancestors}
 
         hierarchy_cache = {}
@@ -399,8 +399,8 @@ class ApplicationStatusReport(GetParamsMixin, PaginatedReportMixin, DeploymentsR
 
         grouped_location = {}
         for location_id in location_ids:
-            if location_id in location_by_location_id:
-                pk = location_by_location_id[location_id].pk
+            if location_id in location_pk_by_location_id:
+                pk = location_pk_by_location_id[location_id]
                 grouped_location[location_id] = get_hierarchy(pk)
             else:
                 grouped_location[location_id] = []
