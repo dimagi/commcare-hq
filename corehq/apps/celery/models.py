7ac741e6c658be7b2114e7320635926b2d72a3f8
Graham Herceg <gherceg@dimagi.com>
2025-11-13 13:45:13 -0500

    Unique constraints in pg allow multiple null rows
    https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-UNIQUE-CONSTRAINTS

--- corehq/apps/celery/models.py
+++ corehq/apps/celery/models.py
@@ -6,7 +6,9 @@ class TaskRecord(models.Model):
     Record to track a celery task/message prior to being processed by a worker.
     Once processed by a worker, whether it fails or succeeds, this record is deleted.
     """
-    task_id = models.UUIDField(null=True)
+
+    # by default, multiple null values are allowed with unique constraint
+    task_id = models.UUIDField(unique=True, null=True)
     name = models.CharField(max_length=255)
     args = models.JSONField()
     kwargs = models.JSONField()
@@ -14,13 +16,6 @@ class TaskRecord(models.Model):
     error = models.TextField(blank=True, default='')  # optional - captures issues queueing task
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=["task_id"], condition=models.Q(task_id__isnull=False), name="unique_nonnull_task_id"
-            )
-        ]
 
     def __str__(self):
         return f"<{self.name}> {self.task_id} {self.date_created}"
