b04ede096078cf71f0cfc72e05e0584285dd6c49
Graham Herceg <gherceg@dimagi.com>
2025-11-18 08:53:32 -0500

    Fix docstring indentation

--- corehq/apps/celery/shared_task.py
+++ corehq/apps/celery/shared_task.py
@@ -11,10 +11,10 @@ def task(*args, **kwargs):
     This decorator serves multiple purposes:
 
     - Adds "durable" task functionality that optionally creates a permanent
-    record of incomplete tasks
+      record of incomplete tasks
     - enforces the default task serializer as JSON, which is needed until
-    https://github.com/celery/celery/issues/6759 is resolved, at which point
-    CELERY_TASK_SERIALIZER can be set back to json.
+      https://github.com/celery/celery/issues/6759 is resolved, at which point
+      CELERY_TASK_SERIALIZER can be set back to json.
 
     Parameters:
         serializer (string): Serialization method to use.
