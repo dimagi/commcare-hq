5c18806efc0714c4ce504ea3715c6af877b62399
Graham Herceg <gherceg@dimagi.com>
2025-12-05 12:10:50 -0800

    Add support for soft and hard deletion

--- corehq/apps/cleanup/management/commands/hard_delete_forms.py
+++ corehq/apps/cleanup/management/commands/hard_delete_forms.py
@@ -14,43 +14,67 @@ class Command(BaseCommand):
         parser.add_argument('domain', help='Domain name that owns the forms to be deleted')
         parser.add_argument('filename', help='path to the CSV file')
         parser.add_argument('--resume_id', help='form ID to start at, within the CSV file')
+        parser.add_argument(
+            '--permanent', action='store_true', default=False, help='Hard delete forms. Defaults to soft deletion.'
+        )
 
-    def handle(self, domain, filename, resume_id=None, **options):
+    def handle(self, domain, filename, resume_id=None, permanent=False, **options):
         # expects the filename to have a CSV with a header containing a "Form ID" field
         with open(filename, mode='r', encoding='utf-8-sig') as csvfile:
             reader = csv.reader(csvfile, delimiter=',')
-            self._process_rows(reader, domain, resume_id)
+            self._process_rows(reader, domain, resume_id, permanent)
 
-    def _process_rows(self, rows, domain, resume_id):
+    def _process_rows(self, rows, domain, resume_id, permanent):
         header_row = next(rows)   # skip header line
         if header_row[INDEX_FORM_ID] != 'Form ID':
             raise CommandError(
                 f'Expected Column {INDEX_FORM_ID} to be "Form ID", found "{header_row[INDEX_FORM_ID]}". Exiting'
             )
 
-        num_deleted = 0
-
         if resume_id:
             print('resuming at: ', resume_id)
             rows = itertools.dropwhile(lambda row: row[INDEX_FORM_ID] != resume_id, rows)
 
-        print('Starting form deletion')
+        if permanent:
+            self.hard_delete_forms(domain, rows)
+        else:
+            self.soft_delete_forms(domain, rows)
+
+    def hard_delete_forms(self, domain, rows):
+        print('Starting hard form deletion')
+        num_deleted = 0
         for chunk in chunked(rows, CHUNK_SIZE):
             form_ids = [row[INDEX_FORM_ID] for row in chunk]
 
             try:
                 deleted_form_ids = set(XFormInstance.objects.hard_delete_forms(
                     domain, form_ids, return_ids=True))
-            except Exception:
-                print('failed during processing of: ', form_ids)
-                raise
 
-            for form_id in form_ids:
-                if form_id in deleted_form_ids:
-                    print('Deleted: ', form_id)
-                else:
-                    print('Not found:', form_id)
+                for form_id in form_ids:
+                    if form_id in deleted_form_ids:
+                        print('Hard deleted: ', form_id)
+                    else:
+                        print('Not found:', form_id)
+            except Exception:
+                print('Failed while attempting to delete: ', form_ids)
+                raise
 
             num_deleted += len(deleted_form_ids)
 
-        print(f'Complete -- removed {num_deleted} forms')
+        print(f'Complete -- hard deleted {num_deleted} forms')
+
+    def soft_delete_forms(self, domain, rows):
+        print('Starting soft form deletion')
+        num_deleted = 0
+        for chunk in chunked(rows, CHUNK_SIZE):
+            form_ids = [row[INDEX_FORM_ID] for row in chunk]
+            try:
+                num_deleted += XFormInstance.objects.soft_delete_forms(
+                    domain, form_ids, deletion_id='delete_forms_cmd'
+                )
+                print("Soft deleted chunk: ", form_ids)
+            except Exception:
+                print('Failed whilte attempting to delete: ', form_ids)
+                raise
+
+        print(f'Complete -- soft deleted {num_deleted} forms')
