e430e21b8ed2581cf14ce22835ca2c3d9825bb85
nospame <mapson.evan@gmail.com>
2025-12-10 23:26:26 +0000

    "Bootstrap 5 Migration - initial auto-migration for app_manager/js/visit_scheduler.js, splitting templates"

--- corehq/apps/app_manager/static/app_manager/js/forms/advanced/bootstrap5/case_config_ui.js
+++ corehq/apps/app_manager/static/app_manager/js/forms/advanced/bootstrap5/case_config_ui.js
@@ -9,7 +9,7 @@ import google from "analytix/js/google";
 import initialPageData from "hqwebapp/js/initial_page_data";
 import main from "hqwebapp/js/bootstrap5/main";
 import appManager from "app_manager/js/bootstrap5/app_manager";
-import visitSchedulerModel from "app_manager/js/visit_scheduler";
+import visitSchedulerModel from "app_manager/js/bootstrap5/visit_scheduler";
 
 $(function () {
     if (initialPageData.get('module_doc_type') !== "AdvancedModule") {
