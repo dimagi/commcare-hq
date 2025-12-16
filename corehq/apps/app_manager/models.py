5f33ae095c072c1f2350392b3df5b51c3a37e9a9
nospame <mapson.evan@gmail.com>
2025-12-16 12:59:38 -0800

    Remove HUBSPOT_SAVED_APP_FORM_ID and uses

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -4882,7 +4882,7 @@ class ApplicationBase(LazyBlobDoc, SnapshotMixin,
         return record
 
     def save(self, response_json=None, increment_version=None, **params):
-        from corehq.apps.analytics.tasks import track_workflow_noop, send_hubspot_form, HUBSPOT_SAVED_APP_FORM_ID
+        from corehq.apps.analytics.tasks import track_workflow_noop
         from corehq.apps.case_search.utils import get_app_context_by_case_type
         self.last_modified = datetime.datetime.utcnow()
         if not self._rev and not domain_has_apps(self.domain):
@@ -4903,7 +4903,6 @@ class ApplicationBase(LazyBlobDoc, SnapshotMixin,
         user = getattr(request, 'couch_user', None)
         if user and user.days_since_created == 0:
             track_workflow_noop(user.get_email(), 'Saved the App Builder within first 24 hours')
-        send_hubspot_form(HUBSPOT_SAVED_APP_FORM_ID, request)
         if self.copy_of:
             cache.delete('app_build_cache_{}_{}'.format(self.domain, self.get_id))
 
