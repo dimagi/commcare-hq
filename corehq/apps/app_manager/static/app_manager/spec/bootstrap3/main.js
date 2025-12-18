b8edf04b610a210fe059ca0e17ca78c12b7315d2
nospame <mapson.evan@gmail.com>
2025-12-18 20:33:54 +0000

    "Bootstrap 5 Migration - initial auto-migration for app_manager/spec/releases_spec.js, splitting templates"

--- corehq/apps/app_manager/static/app_manager/spec/bootstrap3/main.js
+++ corehq/apps/app_manager/static/app_manager/spec/bootstrap3/main.js
@@ -2,6 +2,6 @@ import hqMocha from "mocha/js/main";
 
 import "app_manager/spec/download_async_modal_spec";
 import "app_manager/spec/bootstrap3/form_workflow_spec";
-import "app_manager/spec/releases_spec";
+import "app_manager/spec/bootstrap3/releases_spec";
 
 hqMocha.run();
