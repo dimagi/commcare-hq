0f3b2d2cf9158eb8657b47b728b3366724e6a1cd
Graham Herceg <gherceg@dimagi.com>
2025-12-19 14:10:07 -0500

    Lint file

--- corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
+++ corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
@@ -1,13 +1,12 @@
+from celery import Celery
 from django.conf import settings
 from django.core.management.base import BaseCommand
-
-from celery import Celery
 
 from corehq.apps.hqadmin.utils import parse_celery_pings
 
 
 class Command(BaseCommand):
-    help = "Gracefully shutsdown a celery worker"
+    help = "Gracefully shuts down a celery worker"
 
     def add_arguments(self, parser):
         parser.add_argument('hostname')
@@ -16,7 +15,9 @@ class Command(BaseCommand):
         celery = Celery()
         celery.config_from_object(settings)
         celery.control.broadcast('shutdown', destination=[hostname])
-        worker_responses = celery.control.ping(timeout=10, destination=[hostname])
+        worker_responses = celery.control.ping(
+            timeout=10, destination=[hostname]
+        )
         pings = parse_celery_pings(worker_responses)
         if hostname in pings:
             print('Did not shutdown worker')
