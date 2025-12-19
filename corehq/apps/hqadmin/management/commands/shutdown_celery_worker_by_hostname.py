d65b697a10c4646b175eced34c5ec2a6ac116aa9
Graham Herceg <gherceg@dimagi.com>
2025-12-19 15:09:01 -0500

    Ping worker before attempting shutdown

--- corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
+++ corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
@@ -16,7 +16,10 @@ class Command(BaseCommand):
         celery.config_from_object(settings)
         succeeded = self._shutdown(celery, hostname)
         if succeeded:
-            print('Successfully initiated warm shutdown')
+            print(
+                f'Successfully initiated warm shutdown of {hostname} '
+                f'via broker {celery.conf.broker_url}'
+            )
             return
 
         # try old broker if it is set
@@ -26,18 +29,31 @@ class Command(BaseCommand):
             old_celery_app.conf.broker_url = settings.OLD_BROKER_URL
             succeeded = self._shutdown(old_celery_app, hostname)
             if succeeded:
-                print('Successfully initiated warm shutdown via old broker')
+                print(
+                    f'Successfully initiated warm shutdown of {hostname} '
+                    f'via old broker {settings.OLD_BROKER_URL}'
+                )
                 return
 
         print(f'Did not shutdown worker {hostname}')
         exit(1)
 
     def _shutdown(self, celery_app, hostname):
+        if not self._is_worker_up(celery_app, hostname):
+            # worker not found with this celery config
+            return False
+
         celery_app.control.broadcast('shutdown', destiation=[hostname])
+        if self.is_worker_up(celery_app, hostname):
+            # if worker is still up, the shutdown likely did not succeed
+            # or it is just a slow shutdown
+            return False
+
+        return True
+
+    def _is_worker_up(self, celery_app, hostname):
         worker_responses = celery_app.control.ping(
             timeout=10, destination=[hostname]
         )
         pings = parse_celery_pings(worker_responses)
-        if hostname in pings:
-            return False
-        return True
+        return hostname in pings
