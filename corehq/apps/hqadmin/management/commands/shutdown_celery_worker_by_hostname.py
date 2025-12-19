1fbd1d58a9bf48f28c82917d5369cd77370e2c15
Graham Herceg <gherceg@dimagi.com>
2025-12-19 14:23:59 -0500

    Initiate shutdown via old broker if read url is set

--- corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
+++ corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
@@ -1,6 +1,7 @@
 from celery import Celery
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from kombu import Connection
 
 from corehq.apps.hqadmin.utils import parse_celery_pings
 
@@ -12,14 +13,36 @@ class Command(BaseCommand):
         parser.add_argument('hostname')
 
     def handle(self, hostname, **options):
-        celery = Celery()
-        celery.config_from_object(settings)
-        celery.control.broadcast('shutdown', destination=[hostname])
-        worker_responses = celery.control.ping(
-            timeout=10, destination=[hostname]
-        )
-        pings = parse_celery_pings(worker_responses)
-        if hostname in pings:
-            print('Did not shutdown worker')
-            exit(1)
-        print('Successfully initiated warm shutdown')
+        self.celery = Celery()
+        self.celery.config_from_object(settings)
+
+        broker_conn = None
+        # explicit read and write urls are only set when a broker
+        # migration is in progress
+        if old_broker_url := getattr(settings, 'CELERY_BROKER_READ_URL', None):
+            broker_conn = Connection(old_broker_url)
+
+        succeeded = self._shutdown(hostname, broker_conn)
+        if succeeded:
+            print('Successfully initiated warm shutdown')
+            return
+
+        print(f'Did not shutdown worker {hostname}')
+        exit(1)
+
+    def _shutdown(self, hostname, broker_conn=None):
+        kwargs = {'destination': [hostname]}
+        if broker_conn is not None:
+            # use a custom broker connection
+            kwargs['broker'] = broker_conn
+        self.celery.control.broadcast('shutdown', **kwargs)
+
+        if broker_conn is None:
+            worker_responses = self.celery.control.ping(
+                timeout=10, destination=[hostname]
+            )
+            pings = parse_celery_pings(worker_responses)
+            if hostname in pings:
+                return False
+            return True
+        return True
