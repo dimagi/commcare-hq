b74f6ec59f73acae6fc05c7714034aaac246c895
Graham Herceg <gherceg@dimagi.com>
2026-01-02 13:37:57 -0500

    Support sending shutdown via old broker if set

--- corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
+++ corehq/apps/hqadmin/management/commands/shutdown_celery_worker_by_hostname.py
@@ -1,6 +1,9 @@
+from contextlib import contextmanager
+
 from celery import Celery
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from kombu import Connection
 
 from corehq.apps.hqadmin.utils import parse_celery_pings
 
@@ -15,22 +18,49 @@ class Command(BaseCommand):
         self.celery = Celery()
         self.celery.config_from_object(settings)
 
-        if not self._ping_worker(hostname):
-            print(f'Failed to ping {hostname}. Aborting shutdown.')
+        current_broker_url = getattr(settings, 'CELERY_BROKER_URL', None)
+        assert current_broker_url is not None, "CELERY_BROKER_URL is not set"
+        broker_urls = [current_broker_url]
+        if old_broker_url := getattr(settings, 'OLD_BROKER_URL', None):
+            broker_urls.append(old_broker_url)
+
+        success = False
+        for broker_url in broker_urls:
+            with self._connection(broker_url) as conn:
+                success = self._shutdown_worker(hostname, conn)
+            if success:
+                break
+
+        if not success:
+            print(f'Failed to shutdown {hostname}.')
             exit(1)
+
+    def _shutdown_worker(self, hostname, conn):
+        if not self._ping_worker(hostname, conn):
+            print(f'Failed to ping {hostname} via broker at {conn.hostname}')
+            return False
 
         self.celery.control.broadcast('shutdown', destination=[hostname])
 
-        if self._ping_worker(hostname):
+        if self._ping_worker(hostname, conn):
             print(
                 f'Sent shutdown to {hostname} but worker is still '
                 'responding to ping'
             )
         print(f'Successfully shutdown {hostname}')
+        return True
 
-    def _ping_worker(self, hostname):
+    def _ping_worker(self, hostname, conn):
         worker_responses = self.celery.control.ping(
-            timeout=10, destination=[hostname]
+            timeout=10, destination=[hostname], connection=conn
         )
         pings = parse_celery_pings(worker_responses)
         return hostname in pings
+
+    @contextmanager
+    def _connection(self, url):
+        conn = Connection(url)
+        try:
+            yield conn
+        finally:
+            conn.release()
