07af233d2281adea0a772f0b4384c21bd5ca7c29
Ajeet <ayadav@dimagi.com>
2026-01-07 16:30:36 +0530

    make payment status method dynamic based on provider

--- corehq/apps/integration/payments/services.py
+++ corehq/apps/integration/payments/services.py
@@ -364,7 +364,8 @@ def request_payment_status(payment_case: CommCareCase, config: MoMoConfig):
         return _get_status_details(PaymentStatus.ERROR, PaymentStatusErrorCode.MISSING_TRANSACTION_ID)
 
     try:
-        response = _make_payment_status_request(transaction_id, config)
+        payment_status_api_method = config.get_payment_status_api_method()
+        response = payment_status_api_method(transaction_id, config)
         response_data = response.json()
 
         status = response_data.get('status', '').lower()
@@ -406,7 +407,7 @@ def request_payment_status(payment_case: CommCareCase, config: MoMoConfig):
     return _get_status_details(status, error_code)
 
 
-def _make_payment_status_request(reference_id, config):
+def make_mtn_payment_status_request(reference_id, config):
     connection_settings = config.connection_settings
     requests = connection_settings.get_requests()
     response = requests.get(
