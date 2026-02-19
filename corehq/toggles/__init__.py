8925bf1aee390ec309ce6801052884815aae4d0c
Graham Herceg <gherceg@dimagi.com>
2026-02-19 09:10:53 -0500

    Comment out NON_COMMTRACK_LEDGERS

--- corehq/toggles/__init__.py
+++ corehq/toggles/__init__.py
@@ -1342,16 +1342,16 @@ COMMTRACK = StaticToggle(
     save_fn=_commtrackify,
 )
 
-NON_COMMTRACK_LEDGERS = StaticToggle(
-    'non_commtrack_ledgers',
-    "Enable ledgers for projects not using Supply.",
-    TAG_DEPRECATED,
-    description=(
-        'Turns on the ledger fixture and ledger transaction question types in '
-        'the form builder. ONLY WORKS ON SQL DOMAINS!'
-    ),
-    namespaces=[NAMESPACE_DOMAIN],
-)
+# NON_COMMTRACK_LEDGERS = StaticToggle(
+#     'non_commtrack_ledgers',
+#     "Enable ledgers for projects not using Supply.",
+#     TAG_DEPRECATED,
+#     description=(
+#         'Turns on the ledger fixture and ledger transaction question types in '
+#         'the form builder. ONLY WORKS ON SQL DOMAINS!'
+#     ),
+#     namespaces=[NAMESPACE_DOMAIN],
+# )
 
 CUSTOM_INSTANCES = StaticToggle(
     'custom_instances',
