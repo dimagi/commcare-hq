0700d91a4710c972c67970a8555ecb556c1c5363
Evan Joseph-Pinero <ejosephpinero@gmail.com>
2026-04-02 09:07:57 -0700

    Rework offboard_staff_user to avoid creating redirect directly with POST
    - return early if no web user is found
    - pull username off web user to construct redirect

--- corehq/apps/hqadmin/views/users.py
+++ corehq/apps/hqadmin/views/users.py
@@ -546,69 +546,71 @@ def offboard_staff_user(request):
     if not username:
         return HttpResponseBadRequest("Missing username")
 
+    base_url = reverse("web_user_lookup")
     web_user = WebUser.get_by_username(username)
     if web_user is None:
         messages.error(request, _("User '{username}' not found.").format(username=username))
-    else:
-        removed = []
-        fields_changed = {}
-        django_user = web_user.get_django_user()
+        return redirect(base_url)
 
-        if web_user.domains:
-            count = len(web_user.domains)
-            for domain in list(web_user.domains):
-                web_user.delete_domain_membership(domain)
-                log_user_change(
-                    by_domain=None, for_domain=domain, couch_user=web_user,
-                    changed_by_user=request.couch_user,
-                    changed_via=USER_CHANGE_VIA_WEB,
-                    change_messages=UserChangeMessage.domain_removal(domain),
-                    by_domain_required_for_log=False,
-                )
-            web_user.save()
-            removed.append(ngettext(
-                "{count} domain membership",
-                "{count} domain memberships",
-                count,
-            ).format(count=count))
+    removed = []
+    fields_changed = {}
+    django_user = web_user.get_django_user()
 
-        if django_user.is_superuser:
-            django_user.is_superuser = False
-            django_user.save()
-            fields_changed['is_superuser'] = False
-            removed.append(_("superuser access"))
-
-        if is_accounting_admin(django_user):
-            ops_role = Role.objects.get(slug=privileges.OPERATIONS_TEAM)
-            Grant.objects.filter(
-                from_role=django_user.prbac_role.role,
-                to_role=ops_role,
-            ).delete()
-            Role.update_cache()
-            fields_changed['is_accounting_admin'] = False
-            removed.append(_("accounting admin access"))
-
-        if fields_changed:
+    if web_user.domains:
+        count = len(web_user.domains)
+        for domain in list(web_user.domains):
+            web_user.delete_domain_membership(domain)
             log_user_change(
-                by_domain=None, for_domain=None, couch_user=web_user,
+                by_domain=None, for_domain=domain, couch_user=web_user,
                 changed_by_user=request.couch_user,
                 changed_via=USER_CHANGE_VIA_WEB,
-                fields_changed=fields_changed,
+                change_messages=UserChangeMessage.domain_removal(domain),
                 by_domain_required_for_log=False,
-                for_domain_required_for_log=False,
             )
+        web_user.save()
+        removed.append(ngettext(
+            "{count} domain membership",
+            "{count} domain memberships",
+            count,
+        ).format(count=count))
 
-        if removed:
-            messages.success(
-                request,
-                _("Removed {items} from user '{username}'.").format(items=", ".join(removed), username=username)
-            )
-        else:
-            messages.info(
-                request, _("User '{username}' has no access to remove.").format(username=username)
-            )
+    if django_user.is_superuser:
+        django_user.is_superuser = False
+        django_user.save()
+        fields_changed['is_superuser'] = False
+        removed.append(_("superuser access"))
 
-    redirect_url = '{}?{}'.format(reverse('web_user_lookup'), urllib.parse.urlencode({'q': username}))
+    if is_accounting_admin(django_user):
+        ops_role = Role.objects.get(slug=privileges.OPERATIONS_TEAM)
+        Grant.objects.filter(
+            from_role=django_user.prbac_role.role,
+            to_role=ops_role,
+        ).delete()
+        Role.update_cache()
+        fields_changed['is_accounting_admin'] = False
+        removed.append(_("accounting admin access"))
+
+    if fields_changed:
+        log_user_change(
+            by_domain=None, for_domain=None, couch_user=web_user,
+            changed_by_user=request.couch_user,
+            changed_via=USER_CHANGE_VIA_WEB,
+            fields_changed=fields_changed,
+            by_domain_required_for_log=False,
+            for_domain_required_for_log=False,
+        )
+
+    if removed:
+        messages.success(
+            request,
+            _("Removed {items} from user '{username}'.").format(items=", ".join(removed), username=username)
+        )
+    else:
+        messages.info(
+            request, _("User '{username}' has no access to remove.").format(username=username)
+        )
+
+    redirect_url = '{}?{}'.format(base_url, urllib.parse.urlencode({'q': web_user.username}))
     return redirect(redirect_url)
 
 
