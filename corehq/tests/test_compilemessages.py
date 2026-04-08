691cc7a60b85e45370f4e37b701282454eec6cc5
Norman Hooper <nhooper@dimagi.com>
2026-04-08 17:57:39 -0400

    Catch CommandError

--- corehq/tests/test_compilemessages.py
+++ corehq/tests/test_compilemessages.py
@@ -1,8 +1,8 @@
-from io import StringIO
 from pathlib import Path
 
+import pytest
 from django.conf import settings
-from django.core.management import call_command
+from django.core.management import call_command, CommandError
 
 LOCALE_DIR = Path(settings.BASE_DIR) / 'locale'
 
@@ -12,8 +12,7 @@ def test_compilemessages_succeeds():
     for mo_file in LOCALE_DIR.rglob('*.mo'):
         mo_file.unlink()
 
-    out = StringIO()
-    err = StringIO()
-    call_command('compilemessages', stdout=out, stderr=err)
-    errors = err.getvalue()
-    assert errors == '', f'compilemessages produced errors:\n{errors}'
+    try:
+        call_command('compilemessages', '-v', '0')
+    except CommandError as err:
+        pytest.fail(str(err))
