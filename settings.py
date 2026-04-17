d54cbbb47d340df968c1c01ad01915ce595e8f7e
Graham Herceg <gherceg@dimagi.com>
2026-04-17 14:29:38 -0400

    Rename PERMANENT_DELETION_WINDOW
    Since this is really the beginning of our data rention policy, this name
    change makes sense to me.

--- settings.py
+++ settings.py
@@ -1167,8 +1167,8 @@ CONNECTID_ADD_USER_ANALYTICS_URL = 'http://localhost:8080/users/add_user_analyti
 MAX_MOBILE_UCR_LIMIT = 300  # used in corehq.apps.cloudcare.util.should_restrict_web_apps_usage
 MAX_MOBILE_UCR_SIZE = 100000  # max number of rows allowed when syncing a mobile UCR
 
-# used by periodic tasks that delete soft deleted data older than PERMANENT_DELETION_WINDOW days
-PERMANENT_DELETION_WINDOW = 30  # days
+# days to commit to keeping soft deleted data in databases
+DATA_RETENTION_WINDOW = 30  # days
 
 #### Chatbot configuration
 # Override in localsettings.py
