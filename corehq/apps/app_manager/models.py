c82e2fb21e01a94cb1cbb17c1309d81534cbbdb2
Martin Riese <mriese@dimagi.com>
2026-04-21 13:18:32 -0500

    remove command_label field and dead check in update_case_search_toggles

--- corehq/apps/app_manager/models.py
+++ corehq/apps/app_manager/models.py
@@ -2240,7 +2240,6 @@ class CaseSearch(DocumentSchema):
     - search_label: Removed; search button always uses default label (Apr 2026)
       These fields may still exist in CouchDB documents but are no longer used.
     """
-    command_label = LabelProperty(default={'en': 'Search All Cases'})
     properties = SchemaListProperty(CaseSearchProperty)
     auto_launch = BooleanProperty(default=False)        # if true, skip the casedb case list
     default_search = BooleanProperty(default=False)     # if true, skip the search fields screen
