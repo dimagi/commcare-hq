7067adc59169379e23c4ece7d20d156c40648b0d
Graham Herceg <gherceg@dimagi.com>
2026-04-24 12:05:35 -0400

    Leave dry_run on permanently_delete_eligible_data
    I shouldn't have removed this in the first place, and removing it has
    test implications that I should have realized. Not worth touching for
    now so just putting this back.

--- corehq/apps/cleanup/tasks.py
+++ corehq/apps/cleanup/tasks.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 
 
 # @periodic_task(run_every=crontab(minute=0, hour=0), queue=getattr(settings, 'CELERY_PERIODIC_QUEUE', 'celery'))
-def permanently_delete_eligible_data():
+def permanently_delete_eligible_data(dry_run=False):
     """
     Permanently delete database objects that are eligible for hard deletion.
     To be eligible an object must have a ``deleted_on`` value
