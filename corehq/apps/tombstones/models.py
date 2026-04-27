1d7f1dbf15750475854cd79234b1794e17392b59
Graham Herceg <gherceg@dimagi.com>
2026-04-27 11:29:01 -0400

    Copy DeletedSQLDoc to Tombstone
    corehq.apps.cleanup.models.DeletedSQLDoc -> corehq.apps.tombstones.models.Tombstone

--- corehq/apps/tombstones/models.py
+++ corehq/apps/tombstones/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Tombstone(models.Model):
+    doc_id = models.CharField(max_length=126)
+    object_class_path = models.CharField(max_length=255)
+    domain = models.CharField(max_length=255)
+    deleted_on = models.DateTimeField(db_index=True)
+    deleted_by = models.CharField(max_length=126, null=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['doc_id', 'object_class_path'],
+                name='tombstone_unique_id_and_type',
+            )
+        ]
