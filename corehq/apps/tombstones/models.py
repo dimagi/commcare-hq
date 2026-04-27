85b6745772e8f12139907448f922cd48d8525d65
Graham Herceg <gherceg@dimagi.com>
2026-04-27 11:30:01 -0400

    Remove deleted_by field
    We can't meaningfully make use of this field right now, and while it is
    a nice idea I don't think it is worth having in code.

--- corehq/apps/tombstones/models.py
+++ corehq/apps/tombstones/models.py
@@ -6,7 +6,6 @@ class Tombstone(models.Model):
     object_class_path = models.CharField(max_length=255)
     domain = models.CharField(max_length=255)
     deleted_on = models.DateTimeField(db_index=True)
-    deleted_by = models.CharField(max_length=126, null=True)
 
     class Meta:
         constraints = [
