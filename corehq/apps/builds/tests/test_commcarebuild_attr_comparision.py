595a9ab71b1bb1d6b50631448b2e6bfa44a3491d
jingcheng16 <jcheng@dimagi.com>
2026-05-14 10:59:58 -0400

    complete test_commcarebuild_attr_comparision.py
    'get_next_by_time', 'get_previous_by_time' is auto-generated by Django 5.2, see https://docs.djangoproject.com/en/5.2/ref/models/instances/#django.db.models.Model.get_next_by_FOO. So we have to manually add them in sql_only_attrs
    
    Also explain auto-generated DateTimeField attrs

--- corehq/apps/builds/tests/test_commcarebuild_attr_comparision.py
+++ corehq/apps/builds/tests/test_commcarebuild_attr_comparision.py
@@ -12,11 +12,21 @@ class TestCommCareBuildModelsAttrEquality(ModelAttrEqualityHelper):
 
     couch_only_attrs = {*[
         # removed attrs
+        'create_without_artifacts',  # can be replaced by CommCareMobileBuild.objects.create
+        'major_release',  # only usage is via BuildSpec. Will consolidate after migration
+        'minor_release',  # only usage is via BuildSpec. Will consolidate after migration
+        'all_builds',  # can be replaced by CommCareMobileBuild.objects.all()
         # renamed attrs
         # not used attrs
+        'external_blobs',
+        'j2me_enabled',
     ]}
 
     sql_only_attrs = {*[
         # new attrs
+        'couch_id',
         # renamed attrs
+        # auto generated attrs
+        'get_next_by_time',
+        'get_previous_by_time',
     ]}
