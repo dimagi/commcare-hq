dd7366c34dd350c4386b840a184863d664f6f734
Norman Hooper <nhooper@dimagi.com>
2026-06-10 17:38:01 -0400

    Parse datetime strings

--- corehq/apps/hqwebapp/static/hqwebapp/js/lib/userflow.js
+++ corehq/apps/hqwebapp/static/hqwebapp/js/lib/userflow.js
@@ -6,7 +6,7 @@ const userId = initialPageData.get("userflow_user_id");
 if (token && userId) {
     userflow.init(token);
     userflow.identify(userId, {
-        signed_up_at: initialPageData.get("userflow_signed_up_at"),
+        signed_up_at: JSON.parse(initialPageData.get("userflow_signed_up_at")),
         server_env: initialPageData.get("userflow_server_env"),
         domain: initialPageData.get("userflow_domain"),
         subs_plan: initialPageData.get("userflow_subs_plan"),
