82744f492c6db728ccf81383bf24a19a988afc09
jingcheng16 <jcheng@dimagi.com>
2026-06-15 09:56:51 -0400

    fix local imports

--- corehq/apps/users/views/my_role.py
+++ corehq/apps/users/views/my_role.py
@@ -2,8 +2,15 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 
 from corehq.apps.accounting.models import BillingAccount
+from corehq.apps.cloudcare.dbaccessors import get_cloudcare_apps
+from corehq.apps.custom_data_fields.models import CustomDataFieldsDefinition
 from corehq.apps.domain.decorators import login_and_domain_required
+from corehq.apps.registry.utils import get_data_registry_dropdown_options
+from corehq.apps.reports.models import TableauVisualization
+from corehq.apps.reports.util import get_possible_reports
 from corehq.apps.users.models import PARAMETERIZED_PERMISSIONS, HqPermissions
+from corehq.apps.users.views import _commcare_analytics_roles_options
+from corehq.apps.users.views.mobile.custom_data_fields import CUSTOM_USER_DATA_FIELD_TYPE
 from corehq.util.quickcache import quickcache
 
 
@@ -62,33 +69,24 @@ def _resolve_item_names(domain, list_field, raw_items):
 def _name_map_for(domain, list_field):
     """Build the {raw_id: friendly_name} map for one parameterized list, or None."""
     if list_field == 'view_report_list':
-        from corehq.apps.reports.util import get_possible_reports
         return {r['path']: r['name'] for r in get_possible_reports(domain)}
 
     if list_field == 'view_tableau_list':
-        from corehq.apps.reports.models import TableauVisualization
         return {
             str(viz.id): viz.name
             for viz in TableauVisualization.objects.filter(domain=domain)
         }
 
     if list_field == 'web_apps_list':
-        from corehq.apps.cloudcare.dbaccessors import get_cloudcare_apps
         return {app._id: app.name for app in get_cloudcare_apps(domain)}
 
     if list_field in ('manage_data_registry_list', 'view_data_registry_contents_list'):
-        from corehq.apps.registry.utils import get_data_registry_dropdown_options
         return {r['slug']: r['name'] for r in get_data_registry_dropdown_options(domain)}
 
     if list_field == 'commcare_analytics_roles_list':
-        from corehq.apps.users.views import _commcare_analytics_roles_options
         return {r['slug']: r['name'] for r in _commcare_analytics_roles_options()}
 
     if list_field == 'edit_user_profile_list':
-        from corehq.apps.custom_data_fields.models import CustomDataFieldsDefinition
-        from corehq.apps.users.views.mobile.custom_data_fields import (
-            CUSTOM_USER_DATA_FIELD_TYPE,
-        )
         definition = CustomDataFieldsDefinition.get(domain, CUSTOM_USER_DATA_FIELD_TYPE)
         if definition is None:
             return {}
