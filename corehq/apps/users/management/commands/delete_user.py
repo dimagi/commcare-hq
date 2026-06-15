d2146b960cac3c94ff15da14396282b4341a3b08
Daniel Miller <dmiller@dimagi.com>
2026-06-15 08:53:40 -0400

    Fix delete_user management command
    Fixes TypeError: CouchUser.delete() missing 2 required positional arguments: 'deleted_by_domain' and 'deleted_by'

--- corehq/apps/users/management/commands/delete_user.py
+++ corehq/apps/users/management/commands/delete_user.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 
+from corehq.const import USER_CHANGE_VIA_USER_REQUEST
 from corehq.apps.users.models import WebUser
+from corehq.apps.users.util import SYSTEM_USER_ID
 
 
 class Command(BaseCommand):
@@ -11,5 +13,9 @@ class Command(BaseCommand):
 
     def handle(self, username, **options):
         print("Deleting user %s" % username)
-        WebUser.get_by_username(username).delete()
+        WebUser.get_by_username(username).delete(
+            deleted_by_domain=None,
+            deleted_by=SYSTEM_USER_ID,
+            deleted_via=USER_CHANGE_VIA_USER_REQUEST,
+        )
         print("Operation completed")
