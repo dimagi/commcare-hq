d8b33789ef9bb8b36b8b453ff5c8c20d9d79891d
Norman Hooper <nhooper@dimagi.com>
2026-06-16 17:58:57 +0100

    Nit: Linebreaks

--- corehq/apps/app_manager/views/utils.py
+++ corehq/apps/app_manager/views/utils.py
@@ -111,7 +111,10 @@ def back_to_main(request, domain, app_id, module_id=None, form_id=None,
 def get_langs(request, app):
     lang = request.GET.get(
         'lang',
-        request.COOKIES.get('lang', app.langs[0] if hasattr(app, 'langs') and app.langs else '')
+        request.COOKIES.get(
+            'lang',
+            app.langs[0] if hasattr(app, 'langs') and app.langs else ''
+        )
     )
     langs = None
     if app and hasattr(app, 'langs'):
