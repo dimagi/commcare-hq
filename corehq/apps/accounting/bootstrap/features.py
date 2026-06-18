8f9ee6dc44eab72d9c98c2ca9d7f25fa77ef36d9
Evan Joseph-Pinero <ejosephpinero@gmail.com>
2026-06-18 14:04:33 -0700

    Add software plan privilege for public webforms

--- corehq/apps/accounting/bootstrap/features.py
+++ corehq/apps/accounting/bootstrap/features.py
@@ -167,6 +167,7 @@ advanced_v0 = pro_v1 + [
     privileges.LOCATION_COLUMNS_IN_USER_LAST_ACTIVITY_REPORT,
     privileges.LOCKED_ADMIN_QUESTIONS,
     privileges.VELLUM_SAVE_TO_CASE,
+    privileges.PUBLIC_WEBFORMS,
 ]
 
 enterprise_v0 = advanced_v0 + [
