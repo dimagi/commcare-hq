e583fd7d6dbfc9631079e472e57ca46af662c167
Graham Herceg <gherceg@dimagi.com>
2026-06-24 16:19:35 -0400

    Ignore imported but unused lint error

--- corehq/apps/hqadmin/views/__init__.py
+++ corehq/apps/hqadmin/views/__init__.py
@@ -1,24 +1,24 @@
-from corehq.apps.hqadmin.views.data import (
+from corehq.apps.hqadmin.views.data import (  # noqa F401;
     doc_in_es,
     raw_doc,
 )
-from corehq.apps.hqadmin.views.operations import (
+from corehq.apps.hqadmin.views.operations import (  # noqa F401;
     CallcenterUCRCheck,
     ReprocessMessagingCaseUpdatesView,
     mass_email,
 )
-from corehq.apps.hqadmin.views.reports import (
+from corehq.apps.hqadmin.views.reports import (  # noqa F401;
     DownloadGIRView,
     DownloadMALTView,
 )
-from corehq.apps.hqadmin.views.system import (
+from corehq.apps.hqadmin.views.system import (  # noqa F401;
     SystemInfoView,
     branches_on_staging,
     check_services,
     pillow_operation_api,
     system_ajax,
 )
-from corehq.apps.hqadmin.views.users import (
+from corehq.apps.hqadmin.views.users import (  # noqa F401;
     AdminRestoreView,
     DisableTwoFactorView,
     DisableUserView,
@@ -27,7 +27,7 @@ from corehq.apps.hqadmin.views.users import (
     WebUserDataView,
     web_user_lookup,
 )
-from corehq.apps.hqadmin.views.utils import (
+from corehq.apps.hqadmin.views.utils import (  # noqa F401;
     BaseAdminSectionView,
     default,
 )
