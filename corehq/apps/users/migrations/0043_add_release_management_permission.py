7ab996ec1bc6494fdc250062c99e0e4dbdd38509
Graham Herceg <gherceg@dimagi.com>
2026-06-30 13:10:13 -0400

    Guarantee all permissions are created on fresh env
    We need to eagerly create the Permission rows in postgres on fresh
    installs, and we have designated this migration as the one responsible
    for doing so. Any other migration introducing a new permission can be
    skipped on fresh installs.

--- corehq/apps/users/migrations/0043_add_release_management_permission.py
+++ corehq/apps/users/migrations/0043_add_release_management_permission.py
@@ -1,11 +1,16 @@
 from django.db import migrations
 
 from corehq.apps.users.models_role import Permission
-from corehq.util.django_migrations import skip_on_fresh_install
 
 
-@skip_on_fresh_install
 def create_release_management_permission(apps, schema_editor):
+    """
+    This is the only permission-related migration that intentionally does not
+    get skipped on fresh install  (no @skip_on_fresh_install decorator). This
+    is to ensure that we have a migration that will run Permission.create_all()
+    on a fresh install to eagerly populate all HqPermissions in the postgres
+    table.
+    """
     Permission.create_all()
 
 
@@ -13,6 +18,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('users', '0042_deactivatemobileworkertrigger'),
+        # Permission is an audited table that will create audit events
+        ('field_audit', '0002_add_is_bootstrap_column'),
     ]
 
     operations = [
