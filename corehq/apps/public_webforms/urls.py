193792f7636cc067ccd08158a5eb8272b136768a
Evan Joseph-Pinero <ejosephpinero@gmail.com>
2026-08-06 14:22:42 -0700

    Add stub CreatePublicWebformView

--- corehq/apps/public_webforms/urls.py
+++ corehq/apps/public_webforms/urls.py
@@ -1,7 +1,11 @@
 from django.urls import re_path as url
 
-from corehq.apps.public_webforms.views import ManagePublicWebformsView
+from corehq.apps.public_webforms.views import (
+    CreatePublicWebformView,
+    ManagePublicWebformsView,
+)
 
 urlpatterns = [
     url(r'^$', ManagePublicWebformsView.as_view(), name=ManagePublicWebformsView.urlname),
+    url(r'^create/$', CreatePublicWebformView.as_view(), name=CreatePublicWebformView.urlname),
 ]
