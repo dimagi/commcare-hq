1e460974509de82156de089b094102ccb3e4c4f4
Evan Joseph-Pinero <ejosephpinero@gmail.com>
2026-08-12 13:06:13 -0700

    Add EditPublicWebformView to tabclasses

--- corehq/tabs/tabclasses.py
+++ corehq/tabs/tabclasses.py
@@ -1214,6 +1214,7 @@ class ApplicationsTab(UITab):
     def public_webforms_urls(self):
         from corehq.apps.public_webforms.views import (
             CreatePublicWebformView,
+            EditPublicWebformView,
             ManagePublicWebformsView,
         )
         if not (
@@ -1233,6 +1234,10 @@ class ApplicationsTab(UITab):
                 {
                     'title': _(CreatePublicWebformView.page_title),
                     'urlname': CreatePublicWebformView.urlname,
+                },
+                {
+                    'title': _(EditPublicWebformView.page_title),
+                    'urlname': EditPublicWebformView.urlname,
                 },
             ],
         }]
