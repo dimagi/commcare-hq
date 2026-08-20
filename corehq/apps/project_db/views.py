f86655643d4b761903a741dd2fc319af09d4d73e
Ethan Soergel <esoergel@gmail.com>
2026-08-20 12:07:11 +0200

    Render translated SQL for inspection

--- corehq/apps/project_db/views.py
+++ corehq/apps/project_db/views.py
@@ -2,6 +2,9 @@ from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy
 
+import sqlglot
+from sqlalchemy.dialects import postgresql
+
 from corehq import toggles
 from corehq.apps.domain.decorators import domain_admin_required
 from corehq.apps.hqwebapp.decorators import use_bootstrap5
@@ -14,14 +17,6 @@ from corehq.apps.settings.views import BaseProjectDataView
 from corehq.util.htmx_action import HqHtmxActionMixin, hq_hx_action
 
 MAX_ROWS = 100
-
-
-def run_user_sql(domain, sql):
-    """Run user-supplied SQL against ``domain``'s tables, returning columns and rows"""
-    query = translate(sql, get_domain_tables(domain))
-    with get_project_db_engine().connect() as conn:
-        result = conn.execute(query)
-        return list(result.keys()), result.fetchmany(MAX_ROWS)
 
 
 @method_decorator([
@@ -46,10 +41,34 @@ class QueryProjectDBView(HqHtmxActionMixin, BaseProjectDataView):
     def run_query(self, request, *args, **kwargs):
         context = {'sql': request.POST.get('sql', '')}
         try:
-            columns, rows = run_user_sql(self.domain, context['sql'])
+            query_result = run_user_sql(self.domain, context['sql'])
         except UnsupportedSQL as error:
             context['error'] = str(error)
         else:
-            context.update({'columns': columns, 'rows': rows, 'max_rows': MAX_ROWS})
+            context.update({
+                'result': query_result,
+                'max_rows': MAX_ROWS,
+            })
         return self.render_htmx_partial_response(
             request, 'project_db/partials/query_results.html', context)
+
+
+def run_user_sql(domain, user_sql):
+    query = translate(user_sql, get_domain_tables(domain))
+    with get_project_db_engine().connect() as conn:
+        result = conn.execute(query)
+        return {
+            'query': compile_query(query),
+            'columns': list(result.keys()),
+            'rows': result.fetchmany(MAX_ROWS),
+        }
+
+
+def compile_query(query):
+    """Render a SQLAlchemy selectable as pretty-printed PostgreSQL and its bound params"""
+    compiled = query.compile(dialect=postgresql.dialect(paramstyle='named'))
+    sql = sqlglot.transpile(str(compiled), read='postgres', write='postgres', pretty=True)[0]
+    return {
+        'sql': sql,
+        'params': compiled.params
+    }
