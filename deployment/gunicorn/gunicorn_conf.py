c13232b109762d8ea16295fe89f916b27f782141
Graham Herceg <gherceg@dimagi.com>
2026-08-28 19:26:55 -0400

    Lint

--- deployment/gunicorn/gunicorn_conf.py
+++ deployment/gunicorn/gunicorn_conf.py
@@ -11,7 +11,7 @@ timeout = 900
 max_requests = 240
 max_requests_jitter = int(max_requests * 0.5)
 # defaults to 30 sec, setting to 5 minutes to fight `GreenletExit`s
-graceful_timeout = 5*60
+graceful_timeout = 5 * 60
 # defaults to 4094, increasing to avoid https://manage.dimagi.com/default.asp?283517#1532884
 limit_request_line = 4500
 
@@ -20,6 +20,7 @@ def post_fork(server, worker):
     # hacky way to address gunicorn gevent requests hitting django too early before urls are loaded
     # see: https://github.com/benoitc/gunicorn/issues/527#issuecomment-19601046
     from django.urls import resolve
+
     resolve('/')
 
 
@@ -27,14 +28,14 @@ def on_starting(server):
     try:
         _remove_prometheus_metric_files()
     except Exception:
-        server.log.exception("Error clearing Prometheus metrics")
+        server.log.exception('Error clearing Prometheus metrics')
 
 
 def child_exit(server, worker):
     try:
         _mark_prometheus_worker_dead(worker)
     except Exception:
-        server.log.exception("Error clearing Prometheus live metrics")
+        server.log.exception('Error clearing Prometheus live metrics')
 
 
 def _remove_prometheus_metric_files():
@@ -52,4 +53,5 @@ def _mark_prometheus_worker_dead(worker):
         return
 
     from prometheus_client import multiprocess
+
     multiprocess.mark_process_dead(worker.pid, path)
