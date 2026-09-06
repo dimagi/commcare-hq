d75fdfd0c7998d066d2f09a0f7a8470f96345775
jingcheng16 <jcheng@dimagi.com>
2026-09-06 18:01:35 +0800

    Refactor: do not rename to_privs to upgraded_privs because it is misleading
    "downgraded_privs" is the privileges that the project space will lose, thus "upgraded_privs" should mean all privileges that the project space will newly gain. While currently "upgraded_privs" is actually all privileges the new software plan has.

--- corehq/apps/accounting/utils/__init__.py
+++ corehq/apps/accounting/utils/__init__.py
@@ -100,7 +100,6 @@ def get_change_status(from_plan_version, to_plan_version):
     to_privs = get_privileges(to_plan_version) if to_plan_version is not None else set()
 
     downgraded_privs = from_privs.difference(to_privs)
-    upgraded_privs = to_privs
 
     from corehq.apps.accounting.models import \
         SubscriptionAdjustmentReason as Reason
@@ -108,11 +107,11 @@ def get_change_status(from_plan_version, to_plan_version):
         adjustment_reason = Reason.CREATE
     else:
         adjustment_reason = Reason.SWITCH
-        if len(downgraded_privs) == 0 and len(upgraded_privs) > 0:
+        if len(downgraded_privs) == 0 and len(to_privs) > 0:
             adjustment_reason = Reason.UPGRADE
-        elif len(upgraded_privs) == 0 and len(downgraded_privs) > 0:
+        elif len(to_privs) == 0 and len(downgraded_privs) > 0:
             adjustment_reason = Reason.DOWNGRADE
-    return ChangeStatusResult(adjustment_reason, downgraded_privs, upgraded_privs)
+    return ChangeStatusResult(adjustment_reason, downgraded_privs, to_privs)
 
 
 def domain_has_privilege_cache_args(domain, privilege_slug, **assignment):
