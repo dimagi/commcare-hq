7abaff516161d642bc81c9c8664e279999c21610
Graham Herceg <gherceg@dimagi.com>
2026-09-23 08:51:55 -0400

    Render alerts in the django template
    There is no need for knockout here since data does not update
    dynamically.

--- corehq/apps/notifications/static/notifications/js/manage_notifications.js
+++
@@ -1,9 +0,0 @@
-import "commcarehq";
-import $ from "jquery";
-import initialPageData from "hqwebapp/js/initial_page_data";
-
-$(function () {
-    $('#ko-alert-container').koApplyBindings({
-        alerts: initialPageData.get('alerts'),
-    });
-});
